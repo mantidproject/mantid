81df55774a29196c1cd347d38134947175fc718f
Russell Taylor <taylorrj@ornl.gov>
2007-09-19 13:47:07 +0000

    Adding Build directory, which includes makefile. Also adding a script to run tests. Re #28.

---
+++ Code/Mantid/Kernel/test/runTests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Simple script to build and run the tests.
+# Have kept seperate from the makefile since that's automatically generated
+#   by Eclipse.
+#
+# Author: Russell Taylor, 19/09/07
+#
+
+echo
+echo "Making sure that the Mantid library is built and up-to-date..."
+echo
+make -C ../../Build
+echo
+
+echo "Generating the source from the test header files..."
+cxxtestgen.pl --error-printer -o runner.cpp *.h
+echo
+
+echo "Compiling the test executable..."
+g++ -o runner.exe runner.cpp -L ../../Build -lMantid
+echo
+
+echo "Running the tests..."
+./runner.exe
+
+# Remove the generated files to ensure that they're not inadvertently run
+#   when something in the chain has failed.
+rm -rf runner.*
