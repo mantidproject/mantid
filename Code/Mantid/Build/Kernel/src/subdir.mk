9cdc1a5c097811e72473bb6f028e97fd21b762c6
Russell Taylor <taylorrj@ornl.gov>
2007-09-19 15:06:14 +0000

    Really adding the Build directory and makefiles this time. Fixes #28.

---
+++ Code/Mantid/Build/Kernel/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Kernel/src/Algorithm.cpp 
+
+OBJS += \
+./Kernel/src/Algorithm.o 
+
+CPP_DEPS += \
+./Kernel/src/Algorithm.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Kernel/src/%.o: ../Kernel/src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
