38fe0c66d18fc01a033590c245b78ff3cc8f1f75
Matt Clarke <matt.clarke@stfc.ac.uk>
2007-10-23 09:28:06 +0000

    Small changes to the scripts. Refs #31

--- Code/Mantid/LinuxBuildScripts/doTests.py
+++ Code/Mantid/LinuxBuildScripts/doTests.py
@@ -6,6 +6,7 @@ for test in f:
 	name = test.split('/')
 	os.popen('echo ' + name[-1].strip().rstrip('.h') + ' >> ../logs/testResults.log')
 	os.popen('./Build/Tests/' + name[-1].strip().rstrip('.h') + ' >> ../logs/testResults.log')
+	os.popen('rm ./Build/Tests/' + name[-1].strip().rstrip('.h') + '.*')
 	
 
 	
