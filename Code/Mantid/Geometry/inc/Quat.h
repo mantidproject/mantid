76f0311d7287623953d107d0816a396c65271232
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2007-11-07 10:17:59 +0000

    re #35 Modifications of the V3D classes and Quat Classes Check-in base object for Geometry description : Component and CompAssembly classes

---
+++ Code/Mantid/Geometry/inc/Quat.h
@@ -0,0 +1,112 @@
+#ifndef MANTID_QUAT_H_
+#define MANTID_QUAT_H_
+
+#include <iostream> 
+
+namespace Mantid
+{
+namespace Geometry
+{
+//Forward declarations
+class V3D; 
+class M33;
+
+class Quat
+{
+	/** @class Quat Quat.h Geometry/Quat.h
+			@brief Class for quaternions 
+		    @version 1.0
+		    @author Laurent C Chapon, ISIS RAL
+		    @date 10/10/2007
+		    
+		    Templated class for quaternions. 
+		    Quaternions are the 3D generalization of complex numbers
+		    Quaternions are used for roations in 3D spaces and  
+		    often implemented for computer graphics applications.
+		    Quaternion can be written q=W+ai+bj+ck where 
+		    w is the scalar part, and a, b, c the 3 imaginary parts.
+		    Quaternion multiplication is non-commutative.
+		    i*j=-j*i=k
+		    j*k=-k*j=i
+		    k*i=-i*k=j
+		    Rotation of an angle theta around a normalized axis (u,v,w) can be simply
+		    written W=cos(theta/2), a=u*sin(theta/2), b=v*sin(theta/2), c=w*sin(theta/2)
+		    This class support all arithmetic operations for quaternions
+		    @ignore 
+		    Copyright � 2007 ???RAL???
+
+		    This file is part of Mantid.
+
+		    Mantid is free software; you can redistribute it and/or modify
+		    it under the terms of the GNU General Public License as published by
+		    the Free Software Foundation; either version 3 of the License, or
+		    (at your ption) any later version.
+
+		    Mantid is distributed in the hope that it will be useful,
+		    but WITHOUT ANY WARRANTY; without even the implied warranty of
+		    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		    GNU General Public License for more details.
+
+		    You should have received a copy of the GNU General Public License
+		    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+		    
+		    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+		*/
+	public:
+		Quat();       
+		Quat(const double, const double, const double, const double); 
+		Quat(const Quat&);
+		//! Set quaternion form an angle in degrees and an axis
+		Quat(const double, const V3D&);
+		//Quat(const M33&);
+		~Quat();
+		Quat& operator=(const Quat&);
+		void operator()(const double, const double, const double, const double);
+		void operator()(const double, const V3D&);
+		//! Set quaternion from a 3x3 matrix
+		//void operator()(const M33&);
+		void set(const double, const double, const double, const double);
+		void setAngleAxis(const double, const V3D&);
+		//void setRotMatrix(const M33&);
+		//! Norm of a quaternion
+		double len() const;
+		//! Norm squared
+		double len2() const;
+		//! Initialized to identity
+		void init();
+		//! Nornmalize
+		void normalize();
+		//! Take the complex conjugate
+		void conjugate();  
+		//! Inverse a quaternion (in the sense of rotayion inversion)
+		void inverse();
+		//! Convert quaternion rotation to an OpenGL matrix [4x4] matrix 
+		//! stored as an linear array of 16 double
+		//! The function glRotated must be called
+		void GLMatrix(double[16]);
+		//! Rotate a vector
+		void rotate(V3D&) const;
+		//! Overload operators
+		Quat  operator+(const Quat&) const;
+		Quat& operator+=(const Quat&);
+		Quat  operator-(const Quat&) const;
+		Quat& operator-=(const Quat&);
+		Quat  operator*(const Quat&) const;
+		Quat& operator*=(const Quat&);
+		bool   operator==(const Quat&) const;
+		bool   operator!=(const Quat&) const;
+		const double& operator[](int) const;
+		double& operator[](int);
+		void printSelf(std::ostream&) const;
+	private:
+		double w, a, b, c;
+};
+	
+		//! Overload operator <<. This calls Quat::printself()
+ std::ostream& operator<<(std::ostream&, const Quat&);
+
+} // Namespace Mantid
+
+} // Namespace Geometry
+
+#endif /*MANTID_QUAT_H_*/
---
+++ Code/Mantid/Geometry/src/V3D.cpp
@@ -0,0 +1,368 @@
+#include <ostream>
+#include <stdexcept>
+#include <cmath> 
+#include <vector>
+
+
+// THIS IS VERY ERROR PRONE :: USE "V3D.h" and -I flag.
+#include "V3D.h"
+
+namespace Mantid
+{
+namespace Geometry
+{
+
+const double precision(1e-7);   
+
+V3D::V3D():x(0),y(0),z(0)
+  /// Constructor [Null]
+{}
+
+V3D::V3D(const double xx, const double yy, const double zz) :
+  x(xx),y(yy),z(zz)
+  /// Value constructor
+{}
+
+V3D::V3D(const V3D& v):x(v.x),y(v.y),z(v.z)
+  /// Copy constructor
+{}
+void V3D::spherical(double R, double theta, double phi)
+{
+	double deg2rad=M_PI/180.0;
+	z=R*sin(theta*deg2rad);
+	double ct=cos(theta*deg2rad);
+	x=R*ct*cos(phi*deg2rad);
+	y=R*ct*sin(phi*deg2rad);
+}
+V3D& 
+V3D::operator=(const V3D& A)
+  /*!
+    Assignment operator
+    \param v :: V3D to copy 
+    \return *this
+  */
+{
+  if (this!=&A)
+    {
+      x=A.x;
+      y=A.y;
+      z=A.z;
+    }
+  return *this;
+}
+
+V3D::V3D(const double* vPtr)  
+  /*!
+    Constructor from a pointer.
+    requires that the point is assigned after this has
+    been allocated since vPtr[x] may throw.
+  */
+{
+  if (vPtr)
+    {
+      x=vPtr[0];
+      y=vPtr[1];
+      z=vPtr[2];
+    }
+}
+
+V3D::~V3D()
+  /// Destructor
+{}
+
+V3D 
+V3D::operator+(const V3D& v) const
+  /*
+    Addtion operator
+     \param v :: Vector to add
+     \return *this+v;
+  */
+{
+  V3D out(*this);
+  out+=v;
+  return out;
+}
+
+V3D 
+V3D::operator-(const V3D& v) const
+  /*
+    Subtraction operator
+    \param v :: Vector to sub.
+    \return *this-v;
+  */
+{
+  V3D out(*this);
+  out-=v;
+  return out;
+}
+
+V3D 
+V3D::operator*(const V3D& v) const
+  /*
+    Inner product
+    \param v :: Vector to sub.
+    \return *this * v;
+  */
+{
+  V3D out(*this);
+  out*=v;
+  return out;
+}
+
+V3D 
+V3D::operator/(const V3D& v) const
+  /*
+    Inner division
+    \param v :: Vector to divide
+    \return *this * v;
+  */
+{
+  V3D out(*this);
+  out/=v;
+  return out;
+}
+
+V3D& 
+V3D::operator+=(const V3D& v) 
+  /*
+    Self-Addition operator
+    \param v :: Vector to add.
+    \return *this+=v;
+  */
+{
+  x+=v.x;
+  y+=v.y;
+  z+=v.z;
+  return *this;
+}
+
+V3D& 
+V3D::operator-=(const V3D& v) 
+  /*
+    Self-Subtraction operator
+    \param v :: Vector to sub.
+    \return *this-v;
+  */
+{
+  x-=v.x;
+  y-=v.y;
+  z-=v.z;
+  return *this;
+}
+
+V3D& 
+V3D::operator*=(const V3D& v) 
+  /*
+    Self-Inner product
+    \param v :: Vector to multiply
+    \return *this*=v;
+  */
+{
+  x*=v.x;
+  y*=v.y;
+  z*=v.z;
+  return *this;
+}
+
+V3D& 
+V3D::operator/=(const V3D& v) 
+  /*
+    Self-Inner division
+    \param v :: Vector to divide
+    \return *this*=v;
+  */
+{
+  x/=v.x;
+  y/=v.y;
+  z/=v.z;
+  return *this;
+}
+
+V3D 
+V3D::operator*(const double D) const
+  /*!
+    Scalar product
+    \param D :: value to scale
+    \return this * D
+   */
+{
+  V3D out(*this);
+  out*=D;
+  return out;
+}
+
+V3D 
+V3D::operator/(const double D) const
+  /*!
+    Scalar divsion
+    \param D :: value to scale
+    \return this / D
+  */
+{
+  V3D out(*this);
+  out/=D;
+  return out;
+}
+
+V3D& 
+V3D::operator*=(const double D)
+  /*!
+    Scalar product
+    \param D :: value to scale
+    \return this *= D
+  */
+{
+  x*=D;
+  y*=D;
+  z*=D;
+  return *this;
+}
+
+V3D& 
+V3D::operator/=(const double D) 
+  /*!
+    Scalar division
+    \param D :: value to scale
+    \return this /= D
+    \todo ADD PRECISION
+  */
+{
+  if (D!=0.0)
+    {
+      x/=D;
+      y/=D;
+      z/=D;
+    }
+  return *this;
+}
+
+bool 
+V3D::operator==(const V3D& v) const
+  /*!
+    Equals operator with tolerance factor
+    \param v :: V3D for comparison
+  */
+{
+  return (fabs(x-v.x)>precision ||
+	  fabs(y-v.y)>precision ||
+	  fabs(z-v.z)>precision)  ?
+    false : true;
+}
+
+bool 
+V3D::operator<(const V3D& V) const
+  /*!
+    \tood ADD PRCESSION
+   */
+{
+  if (x!=V.x)
+    return x<V.x;
+  if (y!=V.y)
+    return y<V.y;
+  return z<V.z;
+}
+
+void 
+V3D::operator()(const double xx, const double yy, const double zz)
+{
+  x=xx;
+  y=yy;
+  z=zz;
+  return;
+}
+
+const double&
+V3D::operator[](const int Index) const
+{
+  switch (Index)
+    {
+    case 0: return x;
+    case 1: return y;
+    case 2: return z;
+    default:
+      throw std::runtime_error("V3D::operator[] range error");
+    }
+}
+
+double&
+V3D::operator[](const int Index)
+{
+  switch (Index)
+    {
+    case 0: return x;
+    case 1: return y;
+    case 2: return z;
+    default:
+      throw std::runtime_error("V3D::operator[] range error");
+    }
+}
+
+double 
+V3D::norm() const
+  /*!
+    Vector length
+    \return vec.length()
+  */
+{
+  return sqrt(x*x+y*y+z*z);
+}
+
+double 
+V3D::norm2() const
+{
+	return (x*x+y*y+z*z);
+}
+
+double
+V3D::normalize()
+  /*!
+    Normalises the vector and 
+    then returns the scalar value of the vector
+    \return Norm
+  */
+{
+  const double ND(norm());
+  this->operator/=(ND);
+  return ND;
+}
+
+double 
+V3D::scalar_prod(const V3D& V) const
+{
+  return (x*V.x+y*V.y+z*V.z);
+}
+
+V3D 
+V3D::cross_prod(const V3D& v) const
+{
+  V3D out;
+  out.x=y*v.z-z*v.y;
+  out.y=z*v.x-x*v.z;
+  out.z=x*v.y-y*v.x;
+  return out;
+}
+
+double 
+V3D::distance(const V3D& v) const
+{
+  V3D dif(*this);
+  dif-=v;
+  return dif.norm();
+}
+
+void 
+V3D::printSelf(std::ostream& os) const
+{
+  os << "[" << x << "," << y << "," << z << "]";
+  return;
+}
+
+std::ostream& 
+operator<<(std::ostream& os, const V3D& v)
+{
+  v.printSelf(os);
+  return os;
+}
+
+} // Namespace Geometry
+} // Namespace Mantid
