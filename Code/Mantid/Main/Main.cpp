93529feed409e5fb6c4c7a25074515750e2a1aab
Nick Draper <nick.draper@stfc.ac.uk>
2007-11-14 16:40:46 +0000

    closes #61

---
+++ Code/Mantid/Main/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+#include "SimpleIntegration.h"
+#include "Exception.h"
+#include "FrameworkManager.h"
+#include "Algorithm.h"
+
+
+using namespace Mantid::Kernel;
+int
+main()
+{
+#if defined _DEBUG
+	//NOTE:  Any code in here is temporory for debugging purposes only, nothing is safe!
+	FrameworkManager manager;
+	manager.initialize();
+
+	try
+	{
+		manager.createAlgorithm("NotThere");
+	}
+	catch (Exception::NotFoundError &ex)
+	{
+	  std::cerr << ex.what() <<std::endl;
+	}
+
+	manager.clear();
+
+#endif
+	exit(0);
+}
