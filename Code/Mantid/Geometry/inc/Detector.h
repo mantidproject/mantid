ab7f7f150a5442a7f905f3ebc26dcf894db719d9
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2007-11-15 09:37:44 +0000

    re #35 Checking in the ObjComponent Class, Detector class and read_HET utilities

---
+++ Code/Mantid/Geometry/inc/Detector.h
@@ -0,0 +1,26 @@
+#ifndef DETECTOR_H_
+#define DETECTOR_H_
+#include "ObjComponent.h"
+#include <string>
+namespace Mantid
+{
+namespace Geometry
+{
+class Detector : public ObjComponent
+{
+public:
+	virtual std::string type() const {return "DetectorComponent";}
+	Detector();
+	Detector(const std::string&, Component* reference=0);
+	virtual ~Detector();
+	virtual Component* clone() const {return new Detector(*this);}
+	void setID(int);
+	int getID() const;
+private:
+	// The detector id
+	int id;
+};
+
+}
+}
+#endif /*DETECTOR_H_*/
