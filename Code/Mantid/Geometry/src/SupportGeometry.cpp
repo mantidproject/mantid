46c018c7631ae86cb6e0581eb28808ce16d7a385
Stuart Ansell <stuart.ansell@stfc.ac.uk>
2007-11-23 10:50:04 +0000

    re #35
         Part 2 : Template for support

---
+++ Code/Mantid/Geometry/src/SupportGeometry.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+#include "AuxException.h"
+#include "Matrix.h"
+#include "Vec3D.h"
+#include "Support.h"
+#include "SupportTempCode.h"
+
+namespace Mantid
+{
+namespace  StrFunc
+{
+
+/// \cond TEMPLATE 
+
+template DLLExport int section(std::string&,Geometry::Vec3D&);
+
+template DLLExport int convert(const std::string&,Geometry::Vec3D&);
+
+/// \endcond TEMPLATE 
+
+}  // NAMESPACE StrFunc
+
+}  // NAMESPACE Mantid
