c27da8f438d0185d286e5ec985662fb49caebb0d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2007-11-30 18:05:08 +0000

    Use locally installed scons if it is present. Refs #31.

--- Code/Mantid/build.sh
+++ Code/Mantid/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #
-# Build the Mantid library using scons-local in the Third_Party directory.
-# Passes through command line arguments to Scons.
+# Build the Mantid library using scons-local in the Third_Party directory
+# if scons is not installed on the local machine.
 #
-python ../Third_Party/src/scons-local/scons.py $*
+# Also Passes through command line arguments to Scons.
+#
+sc=`which scons 2>/dev/null`
+if test x$sc = x; then 
+    python ../Third_Party/src/scons-local/scons.py $*
+else
+    $sc $*	# use installed scone
+fi
