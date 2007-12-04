f3f6be4cfe0bfa70b3501f06526f30105a538cf9
Nick Draper <nick.draper@stfc.ac.uk>
2007-12-04 14:13:27 +0000

    Re #65 added missing tests

---
+++ Code/Mantid/Kernel/test/MandatoryValidatorTest.h
@@ -0,0 +1,32 @@
+#ifndef MANDATORYVALIDATORTEST_H_
+#define MANDATORYVALIDATORTEST_H_
+
+#include <string>
+#include <cxxtest/TestSuite.h>
+#include "MantidKernel/MandatoryValidator.h"
+
+using namespace Mantid::Kernel;
+
+class MandatoryValidatorTest : public CxxTest::TestSuite
+{
+public:
+
+	void testConstructor()
+	{
+    TS_ASSERT_THROWS_NOTHING(
+    MandatoryValidator nsv;
+    );
+	}
+	
+	void testMandatoryValidator()
+	{
+		MandatoryValidator p;
+		TS_ASSERT_EQUALS(p.isValid("AZ"), true);
+		TS_ASSERT_EQUALS(p.isValid("B"), true);
+		TS_ASSERT_EQUALS(p.isValid(""), false);
+		TS_ASSERT_EQUALS(p.isValid("ta"), true);
+	}
+
+};
+
+#endif /*MANDATORYVALIDATORTEST_H_*/
---
+++ Code/Mantid/Kernel/test/NullValidatorTest.h
@@ -0,0 +1,51 @@
+#ifndef NULLVALIDATORTEST_H_
+#define NULLVALIDATORTEST_H_
+
+#include <string>
+#include <cxxtest/TestSuite.h>
+#include "MantidKernel/NullValidator.h"
+
+using namespace Mantid::Kernel;
+
+class NullValidatorTest : public CxxTest::TestSuite
+{
+public:
+
+	void testConstructor()
+	{
+    TS_ASSERT_THROWS_NOTHING(
+		NullValidator<int> niv;
+		NullValidator<double> ndv;
+    NullValidator<std::string> nsv;
+    );
+	}
+	void testIntNullValidator()
+	{
+		NullValidator<int> p;
+		TS_ASSERT_EQUALS(p.isValid(0), true);
+		TS_ASSERT_EQUALS(p.isValid(1), true);
+		TS_ASSERT_EQUALS(p.isValid(10), true);
+		TS_ASSERT_EQUALS(p.isValid(-11), true);
+	}
+
+	void testDoubleNullValidator()
+	{
+		NullValidator<double> p;
+		TS_ASSERT_EQUALS(p.isValid(0.0), true);
+		TS_ASSERT_EQUALS(p.isValid(1.0), true);
+		TS_ASSERT_EQUALS(p.isValid(10.0), true);
+		TS_ASSERT_EQUALS(p.isValid(-10.1), true);
+	}
+	
+	void testStringNullValidator()
+	{
+		NullValidator<std::string> p;
+		TS_ASSERT_EQUALS(p.isValid("AZ"), true);
+		TS_ASSERT_EQUALS(p.isValid("B"), true);
+		TS_ASSERT_EQUALS(p.isValid(""), true);
+		TS_ASSERT_EQUALS(p.isValid("ta"), true);
+	}
+
+};
+
+#endif /*NULLVALIDATORTEST_H_*/
