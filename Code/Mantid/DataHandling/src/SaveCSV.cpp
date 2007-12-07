65f0f6f6b5ef45383e87531fbb8c68cb035b5828
Dickon Champion <dickon.champion@stfc.ac.uk>
2007-12-07 14:45:12 +0000

    init/initialise() exec/execute() final/finalize() are all now void functions AnalysisDataService functions add/addOrReplace/retrieve/remove() are also now void functions all these functions throw exceptions and have been put in try/catch loops where appropriate, sometimes they add to the logger sometimes they do not
    refs #69

--- Code/Mantid/DataHandling/src/SaveCSV.cpp
+++ Code/Mantid/DataHandling/src/SaveCSV.cpp
@@ -1,22 +1,22 @@
 /*    
-    Copyright &copy; 2007 ???RAL???
+Copyright &copy; 2007 ???RAL???
 
-    This file is part of Mantid.
+This file is part of Mantid.
 
-    Mantid is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+Mantid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
-    Mantid is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+Mantid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>    
+File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>    
 */
 
 //----------------------------------------------------------------------
@@ -34,210 +34,192 @@ DECLARE_NAMESPACED_ALGORITHM(Mantid::DataHandling, SaveCSV)
 
 /* @class SaveCSV 
 
-    @author Anders J. Markvardsen, ISIS, RAL
-    @date 15/10/2007
+@author Anders J. Markvardsen, ISIS, RAL
+@date 15/10/2007
 */
 
 namespace Mantid
 {
-namespace DataHandling
-{
-
-  using namespace Kernel;
-  using DataObjects::Workspace1D;
-	using DataObjects::Workspace2D;
-
-  Logger& SaveCSV::g_log = Logger::get("SaveCSV");
-
-
-  /// Empty default constructor
-  SaveCSV::SaveCSV() {}
-
-
-  /** Initialisation method. Does nothing at present.
-   * 
-   *  @return A StatusCode object indicating whether the operation was successful
-   */
-  StatusCode SaveCSV::init()
-  {	
-    declareProperty("Filename","",new Kernel::MandatoryValidator);
-    declareProperty("Separator",",");
-    declareProperty("LineSeparator","\n");
-    return StatusCode::SUCCESS;
-  }
-  
-  
-  /** Executes the algorithm. Retrieve the Filename, separator and Lineseparator
-   *  properties and save workspace to Filename. 
-   * 
-   *  @return A StatusCode object indicating whether the operation was successful
-	 *  @throw NotImplementedError Thrown if workspace to save is not a 1D or 2D workspace
-   */
-  StatusCode SaveCSV::exec()
+  namespace DataHandling
   {
-    // Gets the name of the file to save the workspace to; and the
-    // separator and Lineseparator properties if they are provided by the user.
 
-    // Retrieve the filename from the properties
-    m_filename = getPropertyValue("Filename");
-    
-    // Get the values of the optional parameters 
-    m_separator = getPropertyValue("Separator");
-    m_lineSeparator = getPropertyValue("LineSeparator");
+    using namespace Kernel;
+    using DataObjects::Workspace1D;
+    using DataObjects::Workspace2D;
 
-    
-    // prepare to save to file
-    
-    std::ofstream outCSV_File(m_filename.c_str());
-  
-  
-    if (!outCSV_File)
-    {
-      g_log.error("Failed to open file:" + m_filename);
-      return StatusCode::FAILURE;
+    Logger& SaveCSV::g_log = Logger::get("SaveCSV");
+
+
+    /// Empty default constructor
+    SaveCSV::SaveCSV() {}
+
+
+    /** Initialisation method. Does nothing at present.
+    * 
+    */
+    void SaveCSV::init()
+    {	
+      declareProperty("Filename","",new Kernel::MandatoryValidator);
+      declareProperty("Separator",",");
+      declareProperty("LineSeparator","\n");
     }
 
 
-    // get workspace ID string. Used to differentiate between
-		// workspace1D and workspace2D in the if statement below
-		
-    const std::string workspaceID = m_inputWorkspace->id();
-		
-		
-		// seperating out code depending on the workspace ID
-
-		
-    if ( workspaceID == "Workspace1D" )
-		{
-		
-		  const Workspace1D *localworkspace = dynamic_cast<Workspace1D*>(m_inputWorkspace);
-
-
-      // Get info from 1D workspace
-
-      const std::vector<double>& xValue = localworkspace->dataX();
-      const std::vector<double>& yValue = localworkspace->dataY();
-      const std::vector<double>& eValue = localworkspace->dataE();
-  
-    
-      // write to file
-    
-      for (int i = 0; i < (int)xValue.size(); i++)
-      {
-        outCSV_File << std::setw(15) << xValue[i] << m_separator << std::setw(15) << yValue[i] 
-          << m_separator << std::setw(15) << eValue[i] << m_lineSeparator;
-      }    
-	  }
-    else if ( workspaceID == "Workspace2D" )
+    /** Executes the algorithm. Retrieve the Filename, separator and Lineseparator
+    *  properties and save workspace to Filename. 
+    * 
+    *  @throw NotImplementedError Thrown if workspace to save is not a 1D or 2D workspace
+    *  @throw Mantid::Kernel::Exception::FileError  Thrown if errors with file opening and existence 
+    */
+    void SaveCSV::exec()
     {
-		  
-			const Workspace2D *localworkspace = dynamic_cast<Workspace2D*>(m_inputWorkspace);
-			
-			
-			// Get info from 2D workspace
-			
-			const int numberOfHist = localworkspace->getHistogramNumber();
+      // Gets the name of the file to save the workspace to; and the
+      // separator and Lineseparator properties if they are provided by the user.
+
+      // Retrieve the filename from the properties
+      m_filename = getPropertyValue("Filename");
+
+      // Get the values of the optional parameters 
+      m_separator = getPropertyValue("Separator");
+      m_lineSeparator = getPropertyValue("LineSeparator");
 
 
-      // Add first x-axis line to output file
+      // prepare to save to file
 
+      std::ofstream outCSV_File(m_filename.c_str());  
+
+      if (!outCSV_File)
       {
-        const std::vector<double>& xValue = localworkspace->getX(0);
-			
-			  outCSV_File << "A";
-			
-        for (int j = 0; j < (int)xValue.size(); j++)
-			  {
-			    outCSV_File << std::setw(15) << xValue[j] << m_separator;
-			  }
-	
-	      outCSV_File << m_lineSeparator;	
-	    }
+        g_log.error("Failed to open file:" + m_filename);
+        throw Exception::FileError("Failed to open file:" , m_filename);	  
+      }
 
 
-		  for (int i = 0; i < numberOfHist; i++)
-			{
-			  // check if x-axis has changed. If yes print out new x-axis line
-				
-				if (i > 0)
-				{
-				  const std::vector<double>& xValue = localworkspace->getX(i);
-					const std::vector<double>& xValuePrevious = localworkspace->getX(i-1);
-					
-				  if ( xValue != xValuePrevious )
-				  {
-			      outCSV_File << "A";
-			
-            for (int j = 0; j < (int)xValue.size(); j++)
-			      {
-			        outCSV_File << std::setw(15) << xValue[j] << m_separator;
-			      }
-	
-	          outCSV_File << m_lineSeparator;
-				  }
-				}
-				
-				
-				// add y-axis line for histogram (detector) i
-			
-        const std::vector<double>& yValue = localworkspace->getY(i);
-				
-				outCSV_File << i;
-				
-				for (int j = 0; j < (int)yValue.size(); j++)
+      // get workspace ID string. Used to differentiate between
+      // workspace1D and workspace2D in the if statement below
+
+      const std::string workspaceID = m_inputWorkspace->id();
+
+
+      // seperating out code depending on the workspace ID
+
+
+      if ( workspaceID == "Workspace1D" )
+      {
+
+        const Workspace1D *localworkspace = dynamic_cast<Workspace1D*>(m_inputWorkspace);
+
+
+        // Get info from 1D workspace
+
+        const std::vector<double>& xValue = localworkspace->dataX();
+        const std::vector<double>& yValue = localworkspace->dataY();
+        const std::vector<double>& eValue = localworkspace->dataE();
+
+
+        // write to file
+
+        for (int i = 0; i < (int)xValue.size(); i++)
         {
-          outCSV_File << std::setw(15) << yValue[j] << m_separator;
-        }  
-				
-				outCSV_File << m_lineSeparator;
-		  }
-			
-			
-			// print out errors
-			
-			outCSV_File << "\nERRORS\n";			
-			
-		  for (int i = 0; i < numberOfHist; i++)
-			{
-        const std::vector<double>& eValue = localworkspace->getE(i);
-				
-				outCSV_File << i;
-				
-				for (int j = 0; j < (int)eValue.size(); j++)
+          outCSV_File << std::setw(15) << xValue[i] << m_separator << std::setw(15) << yValue[i] 
+          << m_separator << std::setw(15) << eValue[i] << m_lineSeparator;
+        }    
+      }
+      else if ( workspaceID == "Workspace2D" )
+      {
+
+        const Workspace2D *localworkspace = dynamic_cast<Workspace2D*>(m_inputWorkspace);
+
+
+        // Get info from 2D workspace
+
+        const int numberOfHist = localworkspace->getHistogramNumber();
+
+
+        // Add first x-axis line to output file
+
         {
-          outCSV_File << std::setw(15) << eValue[j] << m_separator;
-        }  
-				
-				outCSV_File << m_lineSeparator;
-		  }			
+          const std::vector<double>& xValue = localworkspace->getX(0);
 
-		
-		
-		}
-		else
-		{
-		  outCSV_File.close();  // and should probably delete file from disk as well
-			
-			throw Exception::NotImplementedError("SaveCSV currently only works for 1D and 2D workspaces.");
-		}
-		
-		
-		
-    outCSV_File.close();
-    
+          outCSV_File << "A";
 
-    return StatusCode::SUCCESS;
-  }
+          for (int j = 0; j < (int)xValue.size(); j++)
+          {
+            outCSV_File << std::setw(15) << xValue[j] << m_separator;
+          }
 
-  
-  /** Finalisation method. Does nothing at present.
-   *
-   *  @return A StatusCode object indicating whether the operation was successful
-   */  
-  StatusCode SaveCSV::final()
-  {
-    return StatusCode::SUCCESS;
-  }
-  
-} // namespace DataHandling
+          outCSV_File << m_lineSeparator;	
+        }
+
+
+        for (int i = 0; i < numberOfHist; i++)
+        {
+          // check if x-axis has changed. If yes print out new x-axis line
+
+          if (i > 0)
+          {
+            const std::vector<double>& xValue = localworkspace->getX(i);
+            const std::vector<double>& xValuePrevious = localworkspace->getX(i-1);
+
+            if ( xValue != xValuePrevious )
+            {
+              outCSV_File << "A";
+
+              for (int j = 0; j < (int)xValue.size(); j++)
+              {
+                outCSV_File << std::setw(15) << xValue[j] << m_separator;
+              }
+
+              outCSV_File << m_lineSeparator;
+            }
+          }
+
+
+          // add y-axis line for histogram (detector) i
+
+          const std::vector<double>& yValue = localworkspace->getY(i);
+
+          outCSV_File << i;
+
+          for (int j = 0; j < (int)yValue.size(); j++)
+          {
+            outCSV_File << std::setw(15) << yValue[j] << m_separator;
+          }  
+
+          outCSV_File << m_lineSeparator;
+        }						
+        // print out errors
+
+        outCSV_File << "\nERRORS\n";			
+
+        for (int i = 0; i < numberOfHist; i++)
+        {
+          const std::vector<double>& eValue = localworkspace->getE(i);
+
+          outCSV_File << i;
+
+          for (int j = 0; j < (int)eValue.size(); j++)
+          {
+            outCSV_File << std::setw(15) << eValue[j] << m_separator;
+          }  				
+          outCSV_File << m_lineSeparator;
+        }			
+      }
+      else
+      {
+        outCSV_File.close();  // and should probably delete file from disk as well			
+        throw Exception::NotImplementedError("SaveCSV currently only works for 1D and 2D workspaces.");
+      }		
+      outCSV_File.close();
+      //only gets here if everything happened normally
+      return;
+    }  
+    /** Finalisation method. Does nothing at present.
+    *
+    */  
+    void SaveCSV::final()
+    {
+    }  
+
+  } // namespace DataHandling
 } // namespace Mantid
