bbf3f704dd38faf72dfefe66800378d42d8f90a5
Matt Clarke <matt.clarke@stfc.ac.uk>
2008-01-11 10:00:11 +0000

    Forgot some new files! Refs #34

---
+++ Code/Mantid/Kernel/inc/MantidKernel/LibraryWrapper.h
@@ -0,0 +1,64 @@
+#ifndef MANTID_KERNEL_LIBRARY_WRAPPER_H_
+#define MANTID_KERNEL_LIBRARY_WRAPPER_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include <string>
+
+#include "MantidKernel/System.h"
+
+namespace Mantid
+{
+namespace Kernel
+{
+
+/** @class LibraryWrapper LibraryWrapper.h Kernel/LibraryWrapperr.h
+
+    Class for wrapping a shared library.
+    
+    @author Matt Clarke
+    @date 10/01/2008
+    
+    Copyright &copy; 2008 STFC Rutherford Appleton Laboratories
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport LibraryWrapper
+{
+public:
+	LibraryWrapper();
+	virtual ~LibraryWrapper();
+
+	//Returns true if DLL is opened or already open
+	bool OpenLibrary(const std::string&);
+
+	bool OpenLibrary(const std::string&, const std::string&);
+
+private:
+	/** An untyped pointer to the loaded library.
+	* This is created and deleted by this class.
+	**/
+	void* module;
+};
+
+} // namespace Kernel
+} // namespace Mantid
+
+#endif //MANTID_KERNEL_LIBRARY_WRAPPER_H_
---
+++ Code/Mantid/Kernel/src/LibraryWrapper.cpp
@@ -0,0 +1,67 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidKernel/DllOpen.h"
+#include "MantidKernel/LibraryWrapper.h"
+
+namespace Mantid
+{
+namespace Kernel
+{
+	/// Constructor
+	LibraryWrapper::LibraryWrapper() : module(0)
+	{}
+
+	/// Destructor
+	LibraryWrapper::~LibraryWrapper()
+	{
+		//Close lib
+		if (module)
+		{
+			DllOpen::CloseDll(module);
+			module = 0;
+		}
+	}
+
+	/** Opens a DLL
+	 *  @param libName The name of the file to open (not including the lib/so/dll)
+	 *  @return True if DLL is opened or already open
+	 */
+	bool LibraryWrapper::OpenLibrary(const std::string& libName)
+	{
+		if (!module)
+		{		
+			//Load dynamically loaded library
+			module = DllOpen::OpenDll(libName);
+			if (!module) 
+			{
+				return false;
+			}
+		}
+	
+		return true;
+	}
+	
+	/** Opens a DLL
+	 *  @param libName The name of the file to open (not including the lib/so/dll)
+	 *  @param filePath The filepath to the directory where the library lives
+	 *  @return True if DLL is opened or already open
+	 */
+	bool LibraryWrapper::OpenLibrary(const std::string& libName, const std::string& filePath)
+	{
+		if (!module)
+		{		
+			//Load dynamically loaded library
+			module = DllOpen::OpenDll(libName, filePath);
+			if (!module) 
+			{
+				return false;
+			}
+		}
+	
+		return true;
+	}
+
+} // namespace Kernel
+} // namespace Mantid
+
