438132eafc26b7cf711b67fb5ce6ab8bc84e7c99
Dickon Champion <dickon.champion@stfc.ac.uk>
2008-01-29 00:55:54 +0000

    WorkspaceHistory needs to be added to prevent all hell breaking loose!!!
    re #58

---
+++ Code/Mantid/API/inc/MantidAPI/WorkspaceHistory.h
@@ -0,0 +1,69 @@
+#ifndef MANTID_DATAOBJECTS_WORKSPACEHISTORY_H_
+#define MANTID_DATAOBJECTS_WORKSPACEHISTORY_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/AlgorithmHistory.h"
+#include "MantidAPI/EnvironmentHistory.h"
+#include <ctime>
+#include <vector>
+
+namespace Mantid
+{
+namespace API
+{
+
+/** @class WorkspaceHistory WorkspaceHistory.h API/MAntidAPI/WorkspaceHistory.h
+
+    This class stores information about the Workspace History used by algorithms
+    on a workspace and the environment history
+
+
+    @author Dickon Champion, ISIS, RAL
+    @date 21/01/2008
+    
+    Copyright &copy; 2007 STFC Rutherford Appleton Laboratories
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport WorkspaceHistory
+{
+public:
+
+  WorkspaceHistory();
+  WorkspaceHistory(const EnvironmentHistory&, const std::vector<AlgorithmHistory>&);
+	virtual ~WorkspaceHistory();
+  WorkspaceHistory(const WorkspaceHistory&);
+  WorkspaceHistory& operator=(const WorkspaceHistory& );
+  
+  std::vector<AlgorithmHistory>& getAlgorithms() { return m_algorithms; }
+  const std::vector<AlgorithmHistory>& getAlgorithms() const { return m_algorithms; }
+
+private:
+  /// The execution duration of the algorithm
+  EnvironmentHistory m_environment;
+  std::vector<AlgorithmHistory> m_algorithms;
+
+};
+
+} // namespace API
+} // namespace Mantid
+
+#endif /*MANTID_DATAOBJECTS_WORKSPACEHISTORY_H_*/
---
+++ Code/Mantid/API/src/WorkspaceHistory.cpp
@@ -0,0 +1,49 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/WorkspaceHistory.h"
+
+namespace Mantid
+{
+namespace API
+{
+
+  WorkspaceHistory::WorkspaceHistory()
+{}
+
+/// Constructor
+WorkspaceHistory::WorkspaceHistory(const EnvironmentHistory& environment, 
+                                   const std::vector<AlgorithmHistory>& algorithms)
+:
+m_environment(environment),m_algorithms(algorithms)
+{  
+}
+
+/// Destructor
+WorkspaceHistory::~WorkspaceHistory()
+{
+}
+/*!
+  Standard Copy Constructor
+  \param A :: WorkspaceHistory Item to copy
+*/
+
+WorkspaceHistory::WorkspaceHistory(const WorkspaceHistory& A)
+:
+  m_environment(A.m_environment),m_algorithms(A.m_algorithms)
+{}
+
+WorkspaceHistory& WorkspaceHistory::operator=(const WorkspaceHistory& A)
+{
+  if (this!=&A)
+  {
+    m_environment=A.m_environment;
+    m_algorithms=A.m_algorithms;
+  }
+  return *this;
+}
+
+
+} // namespace API
+} // namespace Mantid
+
