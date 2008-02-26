0570d4ba37d7e5970f50c6c1157c373dea7c9ba3
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2008-02-26 10:44:27 +0000

    Update scons to 12/12/07 version. Fixes #98.

--- Code/Mantid/build.sh
+++ Code/Mantid/build.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 #
 # Build the Mantid library using scons-local in the Third_Party directory
-# if scons is not installed on the local machine.
 #
 # Also Passes through command line arguments to Scons.
 #
-sc=`which scons 2>/dev/null`
-if test x$sc = x; then 
-    python ../Third_Party/src/scons-local/scons.py $*
-else
-    $sc $*	# use installed scone
-fi
+#sc=`which scons 2>/dev/null`
+python ../Third_Party/src/scons-local/scons.py $*
