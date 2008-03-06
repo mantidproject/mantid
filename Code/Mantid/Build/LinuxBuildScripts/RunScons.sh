8ffa96bba37c426d4d991e93d7dff2c3f54572bc
Nick Draper <nick.draper@stfc.ac.uk>
2008-03-06 16:39:38 +0000

    re #88 seperate test from framework building on linux build server

--- Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
+++ Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
@@ -7,5 +7,5 @@ svn log -v -rBASE > ../logs/svn.log
 cd ..
 svn up Third_Party/
 cd checkout/Build
-sh build.sh >> ../../logs/scons.log 2> ../../logs/sconsErr.log
+sh build.sh "skiptest=1" >> ../../logs/scons.log 2> ../../logs/sconsErr.log
 doxygen Mantid.doxyfile 2> ../../logs/doxy.log
