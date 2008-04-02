a45be0d41dd3e73da6a6855cd302dba53c775b71
Nick Draper <nick.draper@stfc.ac.uk>
2008-04-02 14:37:52 +0000

    re #64 Adjust linux build server to update test area of svn

--- Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
+++ Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
@@ -4,8 +4,8 @@ rm ../logs/sconsErr.log
 rm ../logs/svn.log
 rm ../logs/doxy.log
 svn log -v -rBASE > ../logs/svn.log
-cd ..
-svn up Third_Party/
-cd checkout/Build
+svn up ../Third_Party
+svn up ../../Test
+cd Build
 sh build.sh "skiptest=1" >> ../../logs/scons.log 2> ../../logs/sconsErr.log
 doxygen Mantid.doxyfile 2> ../../logs/doxy.log
