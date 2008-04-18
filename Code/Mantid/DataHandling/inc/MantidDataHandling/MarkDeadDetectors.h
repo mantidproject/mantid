620872d64165302c2e7a02cd2b6b4538ac4f2201
Russell Taylor <taylorrj@ornl.gov>
2008-04-18 16:03:58 +0000

    First version of an algorithm for marking detectors as dead and zeroing their associated spectra. Re #104.

---
+++ Code/Mantid/DataHandling/inc/MantidDataHandling/MarkDeadDetectors.h
@@ -0,0 +1,80 @@
+#ifndef MANTID_DATAHANDLING_MARKDEADDETECTORS_H_
+#define MANTID_DATAHANDLING_MARKDEADDETECTORS_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "DataHandlingCommand.h"
+#include "MantidDataObjects/Workspace2D.h"
+
+namespace Mantid
+{
+namespace DataHandling
+{
+/** An algorithm to mark a detector, or set or range of detectors, as dead.
+    The workspace spectra associated with those detectors are zeroed.
+
+    Required Properties:
+    <UL>
+    <LI> Workspace - The name of the (input & output) Workspace2D on which to perform the algorithm </LI>
+    </UL>
+
+    Optional Properties (one or the other should be set or nothing will happen!):
+    <UL>
+    <LI> WorkspaceIndexList - An ArrayProperty containing a list of workspace indices whose detectors should be marked as dead </LI>
+    <LI> WorkspaceIndexMin  - The lower bound of a range of workspace indices whose detectors should be marked as dead </LI>
+    <LI> WorkspaceIndexMax  - The upper bound of a range of workspace indices whose detectors should be marked as dead </LI>
+    </UL>
+
+    @author Russell Taylor, Tessella Support Services plc
+    @date 15/04/2008
+
+    Copyright &copy; 2008 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>. 
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class MarkDeadDetectors : public DataHandlingCommand
+{
+public:
+  MarkDeadDetectors();
+  virtual ~MarkDeadDetectors();
+	
+  /// Algorithm's name for identification overriding a virtual method
+  virtual const std::string name() const { return "MarkDeadDetectors";};
+  /// Algorithm's version for identification overriding a virtual method
+  virtual const int version() const { return 1;};
+
+private:
+  // Implement abstract Algorithm methods
+  void init();
+  void exec();
+  
+  void clearSpectrum(const int& index, const int& vectorSize);
+  
+  /// Pointer to the local workspace
+  DataObjects::Workspace2D_sptr m_localWorkspace;  
+  
+  /// Static reference to the logger class
+  static Kernel::Logger& g_log;
+};
+
+} // namespace DataHandling
+} // namespace Mantid
+
+#endif /*MANTID_DATAHANDLING_MARKDEADDETECTORS_H_*/
---
+++ Code/Mantid/DataHandling/src/MarkDeadDetectors.cpp
@@ -0,0 +1,103 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidDataHandling/MarkDeadDetectors.h"
+#include "MantidAPI/WorkspaceProperty.h"
+#include "MantidKernel/ArrayProperty.h"
+#include "MantidDataObjects/Workspace2D.h"
+
+namespace Mantid
+{
+namespace DataHandling
+{
+
+// Register the algorithm into the algorithm factory
+DECLARE_ALGORITHM(MarkDeadDetectors)
+
+using namespace Kernel;
+using namespace API;
+using DataObjects::Workspace2D;
+using DataObjects::Workspace2D_sptr;
+
+// Initialise the logger
+Kernel::Logger& MarkDeadDetectors::g_log = Kernel::Logger::get("MarkDeadDetectors");
+
+/// (Empty) Constructor
+MarkDeadDetectors::MarkDeadDetectors() {}
+
+/// Destructor
+MarkDeadDetectors::~MarkDeadDetectors() {}
+
+void MarkDeadDetectors::init()
+{
+  declareProperty(new WorkspaceProperty<Workspace2D>("Workspace","", Direction::InOut));
+  declareProperty(new ArrayProperty<int>("WorkspaceIndexList"));
+  
+  BoundedValidator<int> *mustBePositive = new BoundedValidator<int>();
+  mustBePositive->setLower(0);
+  declareProperty("WorkspaceIndexMin",0, mustBePositive);
+  // As the property takes ownership of the validator pointer, have to take care to pass in a unique
+  // pointer to each property.
+  declareProperty("WorkspaceIndexMax",0, mustBePositive->clone());
+}
+
+void MarkDeadDetectors::exec()
+{
+  // Get the input workspace
+  const Workspace2D_sptr m_localWorkspace = getProperty("Workspace");
+  // Get the size of the vectors
+  const int vectorSize = m_localWorkspace->blocksize();
+
+  Property *DeadDetectorList = getProperty("WorkspaceIndexList");
+  // This is an optional property - only do something if it's actually been set.
+  if ( ! DeadDetectorList->isDefault() )
+  {
+    std::vector<int> detList = getProperty("WorkspaceIndexList");
+    std::vector<int>::const_iterator it;
+    for (it = detList.begin(); it != detList.end(); ++it) 
+    {
+      clearSpectrum(*it,vectorSize);
+    }
+  }
+  
+  Property *minDet = getProperty("WorkspaceIndexMin");
+  Property *maxDet = getProperty("WorkspaceIndexMax");
+  // These are optional properties - only do something if they've actually been set.
+  if ( !minDet->isDefault() && !maxDet->isDefault() )
+  {
+    int min = getProperty("WorkspaceIndexMin");
+    int max = getProperty("WorkspaceIndexMax");
+    // Check validity of min/max properties
+    if ( min > max )
+    {
+      g_log.error() << "WorkspaceIndexMin (" << min << ") cannot be greater than WorkspaceIndexMax (" << max << ")";
+      throw std::invalid_argument("WorkspaceIndexMin cannot be greater than WorkspaceIndexMax");
+    }
+    // If max goes beyond end of workspace, print warning and set to highest index
+    if (max >= m_localWorkspace->getHistogramNumber() )
+    {
+      g_log.warning("WorkspaceIndexMax is greater than workspace size - will go to end of workspace");
+      max = m_localWorkspace->getHistogramNumber()-1;
+    }
+    
+    for (int i = min; i <= max; ++i) 
+    {
+      clearSpectrum(i,vectorSize);
+    }
+  }
+  
+}
+
+void MarkDeadDetectors::clearSpectrum(const int& index, const int& vectorSize)
+{
+  // Zero the workspace spectra (data and errors, not X values)
+  m_localWorkspace->dataY(index).assign(vectorSize,0.0);
+  m_localWorkspace->dataE(index).assign(vectorSize,0.0);
+  m_localWorkspace->dataE2(index).assign(vectorSize,0.0);
+
+  // Mark associated detector as dead
+  m_localWorkspace->getInstrument().getDetector(m_localWorkspace->spectraNo(index))->markDead();
+}
+
+} // namespace DataHandling
+} // namespace Mantid
