5d95151d8201c2510335c78d26cb37894f89a8ae
Lakshmi Sastry <lakshmi.sastry@stfc.ac.uk>
2008-05-08 15:26:27 +0000

    Refs #115 Removed Deprecated directory & cleaned up mathSupport

---
+++ Code/Mantid/Geometry/inc/MantidGeometry/mathSupport.h
@@ -0,0 +1,86 @@
+#ifndef mathSupport_h
+#define mathSupport_h
+
+#include <functional>
+
+namespace Mantid
+{
+
+#define fabs(x) std::fabs((x)*1.0)
+
+/// Create an index of the vector in sorted order.
+template<typename T> 
+void indexSort(const std::vector<T>&,std::vector<int>&);
+
+/// Solve a Quadratic equation
+template<typename InputIter>
+int solveQuadratic(InputIter,
+	     std::pair<std::complex<double>,std::complex<double> >&);
+
+/// Solve a Cubic equation
+template<typename InputIter>
+int solveCubic(const InputIter,std::complex<double>&,
+	       std::complex<double>&,std::complex<double>&);
+
+/*!
+  \namespace mathSupport
+  \brief Holds functors to do numerical operations
+  \version 1.0
+  \author S. Ansell
+  \date August 2005
+  
+  Holds all the numberical operations for comparison
+  and checking needed for containers of objects.
+*/
+
+namespace mathSupport
+{
+
+/*!
+  \class PIndex 
+  \author S. Ansell
+  \date Aug 2005
+  \version 1.0
+  \brief Class  to fill an index with a progressive count
+*/
+
+template<typename T>
+class PIndex
+{
+private:
+
+  int count;    ///< counter
+
+public:
+
+  /// Constructor
+  PIndex() : count(0) { }
+
+  /// functional
+  std::pair<T,int> operator()(const T& A) {  return std::pair<T,int>(A,count++); }
+
+};
+
+/*!
+  \class PSep 
+  \author S. Ansell
+  \date Aug 2005
+  \version 1.0
+  \brief Class to access the second object in index pair. 
+*/
+template<typename T>
+class PSep
+{
+public:
+
+  /// Functional to the second object
+  int operator()(const std::pair<T,int>& A) {  return A.second; }
+
+};
+
+}   // NAMESPACE  mathSupport
+
+}   // NAMESPACE Mantid
+
+#endif
+
