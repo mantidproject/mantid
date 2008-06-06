b7c21d166141e28cf659b08fc91d20d7bc7175b5
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2008-06-06 15:48:54 +0000

    re #151

---
+++ Code/Mantid/Kernel/test/SignalChannelTest.h
@@ -0,0 +1,67 @@
+#ifndef MANTID_SIGNALCHANNELTEST_H_
+#define MANTID_SIGNALCHANNELTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidKernel/SignalChannel.h"
+#include "MantidKernel/ConfigService.h"
+#include "Poco/Channel.h"
+#include "Poco/Message.h"
+#include "Poco/LoggingFactory.h"
+#include "Poco/LoggingRegistry.h"
+#include <string>
+
+using namespace Mantid::Kernel;
+
+std::string str = "";
+void tst(const Poco::Message& msg)
+{
+    str = msg.getText();
+}
+
+class SignalChannelTest : public CxxTest::TestSuite
+{
+public: 
+
+  void testContructor()
+  {
+    TS_ASSERT_THROWS_NOTHING
+    (
+      Poco::SignalChannel a;
+    )
+  }
+
+  void testCreateThroughFactory()
+  {
+    //Ensure that the ConfigService has started as this registers the SignalChannel into the factory
+    ConfigService::Instance();
+
+    Poco::SignalChannel* empty=0;
+   
+    Poco::Channel* createdChannel = Poco::LoggingFactory::defaultFactory().createChannel("SignalChannel");
+    Poco::SignalChannel* castedSignalChannel = dynamic_cast<Poco::SignalChannel*>(createdChannel);
+	  TS_ASSERT_DIFFERS(castedSignalChannel,empty);
+  }
+
+  void testConnect()
+  {
+    Poco::SignalChannel a;
+    TS_ASSERT_THROWS_NOTHING
+    (
+       a.connect(tst);
+    )
+  }
+
+  void testSendMessage()
+  {
+    Poco::SignalChannel a;
+    a.connect(tst);
+    Poco::Message msg;
+    msg.setText("TesT");
+    a.log(msg);
+    TS_ASSERT_EQUALS(str, "TesT")
+  }
+
+};
+
+#endif /*MANTID_SIGNALCHANNELTEST_H_*/
