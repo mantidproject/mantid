6edd8ebd74b20e02e62bd7dea8d187b7b840636e
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2008-07-21 14:11:44 +0000

    Changed some friend method declarations to standard function declarations to better cross-platform compiling.... Refs #139.

--- Code/Mantid/Algorithms/inc/MantidAlgorithms/GaussLeastSquaresFit.h
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/GaussLeastSquaresFit.h
@@ -103,12 +103,6 @@ namespace Mantid
       /// The X bin to finish the fitting at
       int m_maxX;
 
-      /// Gaussian function and derivative function in GSL format
-      // defined as friend do get access to function pointers
-      friend int gauss_fdf (const gsl_vector * x, void *params, gsl_vector * f, gsl_matrix * J);
-      friend int gauss_df (const gsl_vector * x, void *params, gsl_matrix * J);
-      friend int gauss_f (const gsl_vector * x, void *params,gsl_vector * f);
-
       /// function which guesses initial parameter values
       void guessInitialValues(const FitData& data, gsl_vector* param_init);
 
@@ -116,82 +110,12 @@ namespace Mantid
       static Mantid::Kernel::Logger& g_log;
     };
 
-
-    
-    /** Gaussian function in GSL format
-* @param x Input function arguments  
-* @param params Input data
-* @param f Output function value
-* @return A GSL status information
-*/
-    /*
-int gauss_f (const gsl_vector * x, void *params, gsl_vector * f) {
-    size_t n = ((struct FitData *)params)->n;
-    double *X = ((struct FitData *)params)->X;
-    double *Y = ((struct FitData *)params)->Y;
-    double *sigma = ((struct FitData *)params)->sigma;
-    double Y0 = gsl_vector_get (x, 0);
-    double A = gsl_vector_get (x, 1);
-    double C = gsl_vector_get (x, 2);
-    double w = gsl_vector_get (x, 3);
-    size_t i;
-    for (i = 0; i < n; i++) {
-        double diff=X[i]-C;
-        double Yi = A*exp(-0.5*diff*diff/(w*w))+Y0;
-        gsl_vector_set (f, i, (Yi - Y[i])/sigma[i]);
-    }
-    return GSL_SUCCESS;
-}*/
-
-/** Calculates Gaussian derivatives in GSL format
-* @param x Input function arguments  
-* @param params Input data
-* @param J Output derivatives
-* @return A GSL status information
-*/
-/*
-int gauss_df (const gsl_vector * x, void *params,
-              gsl_matrix * J) 
-{
-    size_t n = ((struct FitData *)params)->n;
-    double *X = ((struct FitData *)params)->X;
-    double *sigma = ((struct FitData *)params)->sigma;
-    double A = gsl_vector_get (x, 1);
-    double C = gsl_vector_get (x, 2);
-    double w = gsl_vector_get (x, 3);
-    size_t i;
-    for (i = 0; i < n; i++) {
-        // Jacobian matrix J(i,j) = dfi / dxj,	 
-        // where fi = Yi - yi,					
-        // Yi = y=A*exp[-(Xi-xc)^2/(2*w*w)]+B		
-        // and the xj are the parameters (B,A,C,w) 
-        double s = sigma[i];
-        double diff = X[i]-C;
-        double e = exp(-0.5*diff*diff/(w*w))/s;
-        gsl_matrix_set (J, i, 0, 1/s);
-        gsl_matrix_set (J, i, 1, e);
-        gsl_matrix_set (J, i, 2, diff*A*e/(w*w));
-        gsl_matrix_set (J, i, 3, diff*diff*A*e/(w*w*w));
-    }
-    return GSL_SUCCESS;
-} */
-
-/** Calculates Gaussian derivatives and function value in GSL format
-* @param x Input function arguments  
-* @param params Input data
-* @param f Output function value
-* @param J Output derivatives
-* @return A GSL status information
-*/
-
-/*int gauss_fdf (const gsl_vector * x, void *params,
-               gsl_vector * f, gsl_matrix * J) {
-    gauss_f (x, params, f);
-    gauss_df (x, params, J);
-    return GSL_SUCCESS;
-} */
-
-
+    /// Gaussian function and derivative function in GSL format
+    // defined as friend do get access to function pointers
+    int gauss_fdf (const gsl_vector * x, void *params, gsl_vector * f, gsl_matrix * J);
+    int gauss_df (const gsl_vector * x, void *params, gsl_matrix * J);
+    int gauss_f (const gsl_vector * x, void *params,gsl_vector * f);
+ 
 
   } // namespace Algorithm
 } // namespace Mantid
