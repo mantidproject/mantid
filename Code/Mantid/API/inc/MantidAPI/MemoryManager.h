d5271d8fe35822b6850b79b9b3c06944718fcb35
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2008-08-06 08:24:57 +0000

    re #189. Forgot to add the files

---
+++ Code/Mantid/API/inc/MantidAPI/MemoryManager.h
@@ -0,0 +1,89 @@
+#ifndef MANTID_API_MEMORYMANAGER_H_
+#define MANTID_API_MEMORYMANAGER_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+//#include "boost/shared_ptr.hpp"
+#include <vector>
+#include <string>
+#include "MantidAPI/DllExport.h"
+#include "MantidKernel/Logger.h"
+#include "MantidKernel/SingletonHolder.h"
+
+namespace Mantid
+{
+	namespace API
+	{
+
+        struct MemoryInfo
+        {
+            int totalMemory;// total physical memory in KB
+            int availMemory;// available physical memory in KB
+            int freeRatio;// percentage of the available memory ( 0 - 100 )
+        };
+
+		/** @class MemoryManagerImpl MemoryManager.h API/MemoryManager.h
+
+		The MemoryManagerImpl class is responsible for memory management.
+
+		@author Roman Tolchenov, Tessella Support Services plc
+		@date 01/08/2008
+
+		Copyright &copy; 2007 STFC Rutherford Appleton Laboratories
+
+		This file is part of Mantid. 
+
+		Mantid is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation; either version 3 of the License, or
+		(at your option) any later version.
+
+		Mantid is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+		File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+		Code Documentation is available at: <http://doxygen.mantidproject.org>
+		*/
+
+		class EXPORT_OPT_MANTID_API MemoryManagerImpl 
+		{
+		public:
+
+            ///Returns available physical memory in the system in KB.
+            MemoryInfo getMemoryInfo();
+
+		private:
+            friend struct Mantid::Kernel::CreateUsingNew<MemoryManagerImpl>;
+
+			///Class cannot be instantiated by normal means
+			MemoryManagerImpl();
+            ///destructor
+			~MemoryManagerImpl();
+            ///Copy contrstrutor
+			MemoryManagerImpl(const MemoryManagerImpl&);
+    
+			/// Standard Assignment operator    
+			MemoryManagerImpl& operator = (const MemoryManagerImpl&);
+
+			/// Static reference to the logger class
+			Kernel::Logger& g_log;
+
+		};
+
+		///Forward declaration of a specialisation of SingletonHolder for AlgorithmManagerImpl (needed for dllexport/dllimport) and a typedef for it.
+#ifdef _WIN32
+		// this breaks new namespace declaraion rules; need to find a better fix
+		template class EXPORT_OPT_MANTID_API Mantid::Kernel::SingletonHolder<MemoryManagerImpl>;
+#endif /* _WIN32 */
+		typedef Mantid::Kernel::SingletonHolder<MemoryManagerImpl> MemoryManager;
+
+	} // namespace API
+}  //Namespace Mantid
+
+#endif /* MANTID_API_MEMORYMANAGER_H_ */
---
+++ Code/Mantid/API/src/MemoryManager.cpp
@@ -0,0 +1,58 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <limits>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "MantidAPI/MemoryManager.h"
+#include "MantidKernel/Exception.h"
+
+using namespace Mantid::Kernel;
+
+namespace Mantid
+{
+  namespace API
+  {
+
+    /// Private Constructor for singleton class
+    MemoryManagerImpl::MemoryManagerImpl(): g_log(Kernel::Logger::get("MemoryManager"))
+    {
+      std::cerr << "Memory Manager created." << std::endl;
+      g_log.debug() << "Memory Manager created." << std::endl;
+    }
+
+    /** Private destructor
+    *  Prevents client from calling 'delete' on the pointer handed 
+    *  out by Instance
+    */
+    MemoryManagerImpl::~MemoryManagerImpl(){}
+
+    MemoryInfo MemoryManagerImpl::getMemoryInfo()
+    {
+        MemoryInfo mi;
+#ifdef _WIN32
+        MEMORYSTATUSEX memStatus;
+        memStatus.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx( &memStatus );
+        mi.availMemory = memStatus.ullAvailPhys/1024;
+        mi.totalMemory = memStatus.ullTotalPhys/1024;
+        mi.freeRatio = int(100*double(mi.availMemory)/mi.totalMemory);
+#else
+        long int totPages = sysconf(_SC_PHYS_PAGES);
+        long int avPages = sysconf(_SC_AVPHYS_PAGES);
+        long int pageSize = sysconf(_SC_PAGESIZE);
+        mi.availMemory = avPages/1024*pageSize;
+        mi.totalMemory = totPages/1024*pageSize;
+        mi.freeRatio = int(100*double(mi.availMemory)/mi.totalMemory);
+#endif
+        return mi;
+    }
+
+
+  } // namespace API
+} // namespace Mantid
