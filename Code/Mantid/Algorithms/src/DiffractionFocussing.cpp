501f45abb9287dffccb1b53b7fe53aedb7342f52
Russell Taylor <taylorrj@ornl.gov>
2008-09-04 16:43:01 +0000

    I'd inadvertently broken DiffractionFocussing. Re #186.

--- Code/Mantid/Algorithms/src/DiffractionFocussing.cpp
+++ Code/Mantid/Algorithms/src/DiffractionFocussing.cpp
@@ -326,7 +326,7 @@ namespace Mantid
             double offset;
             istr >> n >> udet >> offset >> sel >> group;
             // Check the line wasn't badly formatted - return a failure if it is
-            if ( ! istr.good() ) return false;
+            //if ( ! istr.good() ) return false;
             if (sel) detectorGroups.insert(std::make_pair(group,udet));
         }
         return true;
