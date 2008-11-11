912f3e18cdef7ebf3185b2851ffed20533fea12e
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2008-11-11 17:44:13 +0000

    Added a basic algorithm to save a 2D workspace to a VTK format for reading into Paraview. Re #283

---
+++ Code/Mantid/DataHandling/inc/MantidDataHandling/SaveVTK.h
@@ -0,0 +1,97 @@
+#ifndef MANTID_DATAHANDLING_SAVEVTP_H_
+#define MANTID_DATAHANDLING_SAVEVTP_H_
+
+//---------------------------------------------------
+// Includes
+//---------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid 
+{
+namespace DataHandling
+{
+  /**
+     Saves a 1D or 2D workspace using the vtk file format described in the "file formats"
+     document at http://www.vtk.org/documents.php .
+     This version saves the information in the newer XML format using a <I>.vtp</I> 
+     file extension. The class is implemented as an algorithm and overrides the init() 
+     and exec() functions.
+
+     Required properties:
+     <UL>
+     <LI> Filename - The name of the used to store the workspace, without an extension</LI>
+     <LI> InputWorkspace - The name of the workspace to save.
+     </UL>
+
+     Optional properties:
+     <UL>
+     <LI> Xminimum - The minimum value of the histogram x-axis to print
+     <LI> Xmaximum - The maximum value of the histogram x-axis to print
+     </UL>
+
+     @author Martyn Gigg, Tessella Support Services plc
+     @date 05/11/2008
+     
+     Copyright &copy; 2008 STFC Rutherford Appleton Laboratories
+     
+     This file is part of Mantid.
+     
+     Mantid is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 3 of the License, or
+     (at your option) any later version.
+     
+     Mantid is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+     
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     
+     File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+     Code Documentation is available at: <http://doxygen.mantidproject.org>    
+  */
+  class DLLExport SaveVTK : public API::Algorithm 
+  {
+    
+  public:
+    /// Default constructor
+    SaveVTK();
+
+    /// Algorithm's name 
+    virtual const std::string name() const { return "SaveVTK";};
+    /// Algorithm's version 
+    virtual const int version() const { return 1;};
+    /// Algorithm's category 
+    virtual const std::string category() const { return "DataHandling";}
+
+  private:
+    
+    /// Override virtual init function
+    void init();
+
+    /// Override virtual exec function
+    void exec();
+
+    /// Check the optional properties
+    void checkOptionalProperties();
+
+    /// Write a histogram to the file
+    void writeVTKPiece(std::ostream & outXML, const std::vector<double> & dataX, const std::vector<double> & dataY, const std::vector<double> & dataE, int index) const; 
+
+    /// The x-axis minimum
+    double m_Xmin;
+
+    /// The x-axis minimum
+    double m_Xmax;
+    
+    /// A reference to the logger class
+    static Kernel::Logger & g_log;
+  };
+
+}
+
+}
+
+#endif //MANTID_DATAHANDLING_SAVEVTP_H_
---
+++ Code/Mantid/DataHandling/src/SaveVTK.cpp
@@ -0,0 +1,223 @@
+//---------------------------------------------------
+// Includes
+//---------------------------------------------------
+#include "MantidDataHandling/SaveVTK.h"
+#include "MantidKernel/MandatoryValidator.h"
+#include "MantidKernel/BoundedValidator.h"
+#include "MantidDataObjects/Workspace1D.h"
+#include "MantidDataObjects/Workspace2D.h"
+#include "boost/filesystem.hpp"
+
+#include <string>
+#include <fstream>
+
+namespace Mantid
+{
+namespace DataHandling
+{
+
+  // Register algorithm with AlgorithmFactory
+  DECLARE_ALGORITHM(SaveVTK)
+
+  using namespace Kernel;
+  using namespace DataObjects;
+  using namespace API;
+
+  // Set the reference to the logger 
+  Logger& SaveVTK::g_log = Logger::get("SaveVTK");
+
+  /// Default constructor
+  SaveVTK::SaveVTK() : m_Xmin(0), m_Xmax(0) {}
+
+  /**
+   * Initilisation method.
+   * Simply declares the properties that this algorithm possesses
+   */
+  void SaveVTK::init()
+  {
+    //Declare mandatory properties
+    declareProperty("Filename", "", new MandatoryValidator<std::string>(), "", Direction::Input);
+    declareProperty(new WorkspaceProperty<Workspace>("InputWorkspace", "", Direction::Input));
+    //Declare optional properties
+    BoundedValidator<double> * mustBePositive = new BoundedValidator<double>();
+    mustBePositive->setLower(0.0);
+    declareProperty("Xminimum", 0.0, mustBePositive);
+    declareProperty("Xmaximum", 0.0, mustBePositive->clone());
+  }
+
+  /**
+   * Executes the algorithm.
+   * Saves the workspace specified by the user to tje VTK XML format
+   */
+  void SaveVTK::exec()
+  {
+    std::string filename = getProperty("Filename");
+    g_log.debug() << "Parameters: Filename='" << filename << "'" << std::endl;
+    //add extension
+    filename += ".vtu";
+    
+    Workspace_sptr inputWorkspace = getProperty("InputWorkspace");
+    if( !inputWorkspace )
+    {
+      g_log.error("Failed to retrieve inputWorkspace.");
+      throw Exception::NullPointerException("SaveVTK::exec()", "inputWorkspace");
+    }
+
+    checkOptionalProperties();
+
+    //Open file for writing
+      std::ofstream outVTP(filename.c_str());
+    if( !outVTP )
+    {
+      g_log.error("Failed to open file: " + filename);
+      throw Exception::FileError("Failed to open file ", filename);
+    }
+    
+    // First write document level XML header
+    outVTP << "<?xml version=\"1.0\"?>\n"
+      "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" byte_order=\"LittleEndian\">\n"
+      "<UnstructuredGrid>\n";
+    
+    const std::string workspaceID = inputWorkspace->id();    
+    if( workspaceID == "Workspace2D" )
+    {
+      const Workspace2D_sptr localWorkspace = 
+	boost::dynamic_pointer_cast<Workspace2D>(inputWorkspace);
+      //      const int numberOfHist = localWorkspace->getNumberHistograms();
+
+      //Write out whole range
+      bool xMin(m_Xmin > 0.0), xMax(m_Xmax > 0.0);
+      if( !xMin && !xMax )
+      {
+	for( int hNum = 2; hNum < 100; ++hNum )
+	{
+	  writeVTKPiece(outVTP, localWorkspace->dataX(hNum), localWorkspace->dataY(hNum), 
+			localWorkspace->dataE(hNum), hNum);
+      	}
+      }
+      else
+      {
+	for( int hNum = 2; hNum < 100; ++hNum )
+	{
+	  std::vector<double> xValue, yValue, errors;
+	  std::vector<double>::size_type nVals(localWorkspace->dataY(hNum).size());
+	  for( int i = 0; i < (int)nVals; ++i )
+	  {
+	    if( xMin && localWorkspace->dataX(hNum)[i] < m_Xmin ) continue;
+	    if( xMax && localWorkspace->dataX(hNum)[i+1] > m_Xmax)
+	    {
+	      xValue.push_back(localWorkspace->dataX(hNum)[i]);
+	      break;
+	    }
+	    xValue.push_back(localWorkspace->dataX(hNum)[i]);
+	    if( i == (int)nVals - 1 )
+	    {
+	      xValue.push_back(localWorkspace->dataX(hNum)[i+1]);
+	    } 	    
+	    yValue.push_back(localWorkspace->dataY(hNum)[i]);
+	    errors.push_back(localWorkspace->dataE(hNum)[i]);
+	  }
+	  //sanity check
+	  assert( (int)xValue.size() == (int)yValue.size() + 1 );
+	  
+	  writeVTKPiece(outVTP, xValue, yValue, errors, hNum);
+	}
+      }
+    }
+    else
+    {
+      outVTP.close();
+      boost::filesystem::remove_all(filename);
+      throw Exception::NotImplementedError("SaveVTK only implemented for Workspace2D\n");
+    }
+    
+    // Final XML end block tags
+    outVTP << "</UnstructuredGrid>\n</VTKFile>\n";
+    outVTP.close();
+  }
+
+  /**
+   * Check the validity of the optional parameters
+   */
+  void SaveVTK::checkOptionalProperties()
+  {
+    m_Xmin = getProperty("Xminimum");
+    m_Xmax = getProperty("Xmaximum");
+
+    if( m_Xmin > 0. && m_Xmax > 0 && m_Xmin > m_Xmax )
+    {
+      g_log.error("Invalid range specification.");
+      throw std::invalid_argument("SaveVTK: Inconsistent range values");
+    }
+
+  }
+
+  /**
+   * Write a histogram as a VTK <Piece .../> block
+   * @param outVTP The output stream
+   * @param xValue The x data
+   * @param yValue The y data
+   * @param errors The error data
+   * @param index The histogram number
+   */
+  void SaveVTK::writeVTKPiece(std::ostream & outVTP, const std::vector<double> & xValue, const std::vector<double> & yValue, const std::vector<double> & errors, int index) const
+  {
+    std::vector<double>::size_type nY = yValue.size();
+    int nPoints(8*nY); 
+    outVTP << "<Piece NumberOfPoints=\"" << nPoints << "\" NumberOfCells=\"" << nY << "\">";
+    outVTP << "<CellData Scalars=\"counts\">"
+	   << "<DataArray type=\"Float32\" Name=\"counts\" NumberOfComponents=\"1\" format=\"ascii\">\n";
+    for( int i = 0; i < (int)nY; ++i )
+    {
+	outVTP << yValue[i] << "\n";
+    }
+    outVTP << "</DataArray></CellData>\n";
+    
+    outVTP << "<Points>"
+	   << "<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">\n";
+    
+    double deltaZ(100.);
+    for( int i = 0; i <(int)nY; ++i )
+    {
+      //first face
+      double xLow(xValue[i]), xUpp(xValue[i+1]), ypos(yValue[i]), zpos(-index*deltaZ);
+      outVTP << xLow << " " << 0.0 << " " << zpos << "\n";
+      outVTP << xUpp << " " << 0.0 << " " << zpos << "\n";
+      outVTP << xLow << " " << ypos << " " << zpos  << "\n";
+      outVTP << xUpp << " " << ypos << " " << zpos  << "\n";
+      //second face
+      zpos = -(index + 1)*deltaZ;
+      outVTP << xLow << " " << 0.0 << " " << zpos << "\n";
+      outVTP << xUpp << " " << 0.0 << " " << zpos << "\n";
+      outVTP << xLow << " " << ypos << " " << zpos  << "\n";
+      outVTP << xUpp << " " << ypos << " " << zpos  << "\n";
+
+    }
+    outVTP << "</DataArray></Points>\n";
+    outVTP << "<Cells>\n"
+	   << "<DataArray type=\"Int32\" Name =\"connectivity\" format=\"ascii\">\n";
+    for( int i = 0; i < nPoints; ++i )
+    {
+      outVTP << i << "\n";
+    }
+    outVTP << "</DataArray>\n";
+    outVTP << "<DataArray type=\"Int32\" Name =\"offsets\" format=\"ascii\">\n";
+    for( int i = 8; i <= nPoints;  i += 8 )
+    {
+	outVTP << i << "\n";
+    }
+    outVTP << "</DataArray>\n";
+    outVTP << "<DataArray type=\"UInt8\" Name =\"types\" format=\"ascii\">\n";
+    for( int i = 0; i < nPoints; ++i)
+      {
+	outVTP << "11\n";
+      }
+    outVTP << "</DataArray>\n";
+    
+    outVTP << "</Cells>\n";
+    //End of this piece
+    outVTP << "</Piece>\n";
+  }
+
+}
+}
