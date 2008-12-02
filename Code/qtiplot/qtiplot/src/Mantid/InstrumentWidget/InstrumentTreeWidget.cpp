806583f4c46246365be50d7caa48cef21134920e
Sri Nagella <srikanth.nagella@stfc.ac.uk>
2008-12-02 17:06:26 +0000

    refs #292 Added Instrument tree widget, forgot to add classes

---
+++ Code/qtiplot/qtiplot/src/Mantid/InstrumentWidget/InstrumentTreeWidget.cpp
@@ -0,0 +1,58 @@
+#include "InstrumentTreeWidget.h"
+#ifdef WIN32
+#include <windows.h>
+#endif
+#include "MantidObject.h"
+#include "GLActor.h"
+#include <queue>
+
+void InstrumentTreeWidget::setInstrument(Mantid::API::Instrument* ins)
+{
+	mInstrument=ins;
+	ParseInstrumentGeometry();
+}
+
+void InstrumentTreeWidget::ParseInstrumentGeometry()
+{
+	clear();
+	std::queue<Mantid::Geometry::Component *> CompList;
+	std::queue<QTreeWidgetItem *> itemQueue;
+	CompList.push(mInstrument);
+	//Add top level instrument
+	QTreeWidgetItem *topItem = new QTreeWidgetItem(QStringList(mInstrument->getName().c_str()));
+	itemQueue.push(topItem);              
+    addTopLevelItem(topItem);
+
+	while(!CompList.empty())
+	{
+		Mantid::Geometry::Component* tmp = CompList.front();
+		QTreeWidgetItem* item=itemQueue.front();
+		item->setFlags(Qt::ItemIsEnabled | Qt::ItemIsEditable | Qt::ItemIsUserCheckable);
+		item->setCheckState(0, Qt::Unchecked);
+		CompList.pop();
+		itemQueue.pop();
+		//std::cout<<" Component: "<<tmp->getName()<<std::endl;
+		//std::cout<<" Component Type:"<<tmp->type()<<std::endl;
+		if(tmp->type()=="PhysicalComponent" ||tmp->type()=="DetectorComponent"){
+			//boost::shared_ptr<MantidObject> obj(new MantidObject(dynamic_cast<Mantid::Geometry::ObjComponent*>(tmp)));
+		} else if(tmp->type()=="Instrument"){
+			Mantid::API::Instrument *tmpIns=dynamic_cast<Mantid::API::Instrument*>(tmp);
+			for(int idx=0;idx<tmpIns->nelements();idx++)
+			{
+				CompList.push((*tmpIns)[idx]);
+				QTreeWidgetItem* newItem=new QTreeWidgetItem(QStringList((*tmpIns)[idx]->getName().c_str()));
+				item->addChild(newItem);
+				itemQueue.push(newItem);
+			}
+		} else if(tmp->type()=="CompAssembly"){
+			Mantid::Geometry::CompAssembly *tmpAssem=dynamic_cast<Mantid::Geometry::CompAssembly*>(tmp);
+			for(int idx=0;idx<tmpAssem->nelements();idx++)
+			{
+				CompList.push((*tmpAssem)[idx]);
+				QTreeWidgetItem* newItem=new QTreeWidgetItem(QStringList((*tmpAssem)[idx]->getName().c_str()));
+				item->addChild(newItem);
+				itemQueue.push(newItem);
+			}
+		} 
+	}	
+}
---
+++ Code/qtiplot/qtiplot/src/Mantid/InstrumentWidget/InstrumentTreeWidget.h
@@ -0,0 +1,18 @@
+#ifndef INSTRUMENTTREEWIDGET_H
+#define INSTRUMENTTREEWIDGET_H
+
+#include <QTreeWidget>
+#include "MantidAPI/Instrument.h"
+
+class InstrumentTreeWidget:public QTreeWidget
+{
+    Q_OBJECT
+public:
+    InstrumentTreeWidget(QWidget *w):QTreeWidget(w){};
+	void setInstrument(Mantid::API::Instrument*);
+private:
+	void ParseInstrumentGeometry();
+	Mantid::API::Instrument* mInstrument;
+};
+
+#endif
