a8adbd874a094f36f52a62617a125bae8262e53d
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2008-12-11 20:36:27 +0000

    re #274 Modify error propagation in divide algorithm in the case of the error for the nominator being zero.

--- Code/Mantid/API/src/GaussianErrorHelper.cpp
+++ Code/Mantid/API/src/GaussianErrorHelper.cpp
@@ -12,12 +12,12 @@ namespace Mantid
     GaussianErrorHelper* GaussianErrorHelper::m_instance = 0;
 
     /** A static method which retrieves the single instance of the Algorithm Manager
-    * 
+    *
     *  @returns A pointer to the Algorithm Manager instance
     */
     GaussianErrorHelper* GaussianErrorHelper::Instance()
     {
-      if (!m_instance) m_instance = new GaussianErrorHelper;	 		
+      if (!m_instance) m_instance = new GaussianErrorHelper;
       return m_instance;
     }
 
@@ -32,7 +32,7 @@ namespace Mantid
     * @param result The result with value and error caluculated, all other values wil be passed through from the lhs value
     */
     const void GaussianErrorHelper::plus (const value_type& lhs,const value_type& rhs, value_type& result) const
-    { 
+    {
       result.Y() = lhs.Y()+rhs.Y();
       result.E() = sqrt((lhs.E()*lhs.E())+(rhs.E()*rhs.E()));
     }
@@ -57,12 +57,12 @@ namespace Mantid
     {
       result.Y() = lhs.Y()*rhs.Y();
       //  gaussian errors
-      // (Sa/a)2 + (Sb/b)2 = (Sc/c)2 
-      //  So after taking proportions, squaring, summing, 
+      // (Sa/a)2 + (Sb/b)2 = (Sc/c)2
+      //  So after taking proportions, squaring, summing,
       //  and taking the square root, you get a proportional error to the product c.
-      //  Multiply that proportional error by c to get the actual standard deviation Sc.  
-      result.E() = result.Y()*sqrt(pow((lhs.E()/lhs.Y()),2) + pow((rhs.E()/rhs.Y()),2));   
-    } 
+      //  Multiply that proportional error by c to get the actual standard deviation Sc.
+      result.E() = result.Y()*sqrt(pow((lhs.E()/lhs.Y()),2) + pow((rhs.E()/rhs.Y()),2));
+    }
 
     /**Division
     * @param lhs The lhs value
@@ -73,12 +73,17 @@ namespace Mantid
     {
       result.Y() = lhs.Y()/rhs.Y();
       //  gaussian errors
-      // (Sa/a)2 + (Sb/b)2 = (Sc/c)2 
-      //  So after taking proportions, squaring, summing, 
+      // (Sa/a)2 + (Sb/b)2 = (Sc/c)2
+      //  So after taking proportions, squaring, summing,
       //  and taking the square root, you get a proportional error to the product c.
-      //  Multiply that proportional error by c to get the actual standard deviation Sc.  
-      result.E() = result.Y()*sqrt(pow((lhs.E()/lhs.Y()),2) + pow((rhs.E()/rhs.Y()),2));   
-    } 
+      //  Multiply that proportional error by c to get the actual standard deviation Sc.
+      if (fabs(lhs.E())<1e-7)
+      {
+    	  result.E()=0;
+    	  return;
+      }
+      result.E() = result.Y()*sqrt(pow((lhs.E()/lhs.Y()),2) + pow((rhs.E()/rhs.Y()),2));
+    }
 
   } // namespace API
 } // namespace Mantid
