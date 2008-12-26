bdc7204d3036c7832041a069d5b1242c064b134f
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2008-12-26 21:24:51 +0000

    re#274 Added an algorithm to compute the generalised second difference of spectra. This will be used by the peak-finding algorithm.

---
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/GeneralisedSecondDifference.h
@@ -0,0 +1,84 @@
+#ifndef MANTID_ALGORITHMS_GENERALISEDSECONDDIFFERENCE_H_
+#define MANTID_ALGORITHMS_GENERALISEDSECONDDIFFERENCE_H_
+
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+/** Compute the generalised second difference of a spectrum or several spectra based on the method
+ * described by M.A. Mariscotti., Nuclear Instruments and Methods 50, 309 (1967).
+ * This is used for example by a peak-finding algorithm.
+ * Given a spectrum with value yi (0<=i<n), the generalised second difference corresponds to
+ * the second difference curve, smoothed by averaging each point in the interval [-m,+m],
+ * and applying this procedure z times.
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the workspace to take as input. </LI>
+    <LI> OutputWorkspace - The name under which to store the output workspace. </LI>
+    <LI> m               - The number of points for averaging, i.e. summing will be done in the range [y(i-m),y(i+m)] </LI>
+    <LI> z               - The number of iteration steps in the averaging procedure </LI>
+    <LI> Spectra_min  - Lower bound of the spectra range </LI>
+    <LI> Spectra_max  - Upper bound of the spectra range  </LI>
+    </UL>
+
+    @author Laurent C Chapon, ISIS Facility Rutherford Appleton Laboratory
+    @date 26/12/2008
+
+    Copyright &copy; 2008 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport GeneralisedSecondDifference : public API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  GeneralisedSecondDifference();
+  /// Virtual destructor
+  virtual ~GeneralisedSecondDifference();
+  /// Algorithm's name
+  virtual const std::string name() const { return "GeneralisedSecondDifference"; }
+  /// Algorithm's version
+  virtual const int version() const { return (1); }
+  /// Algorithm's category for identification
+  virtual const std::string category() const { return "General"; }
+
+private:
+  /// Initialisation code
+  void init();
+  ///Execution code
+  void exec();
+  ///
+  void computePrefactors();
+  /// Static reference to the logger class
+  static Mantid::Kernel::Logger& g_log;
+  /// Vector that contains the prefactor coefficients Cij in the range [-zm-1,zm+1]
+  std::vector<double> Cij;
+  /// Vector that contains the prefactor coefficients Cij^2 in the range [-zm-1,zm+1]
+  std::vector<double> Cij2;
+  ///
+  int z,m;
+};
+
+} // Namespace Algorithms
+} // Namespace Mantid
+
+#endif /* MANTID_ALGORITHMS_GENERALIZEDSECONDDIFFERENCE_H_ */
---
+++ Code/Mantid/Algorithms/src/GeneralisedSecondDifference.cpp
@@ -0,0 +1,176 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+
+#include <sstream>
+#include <numeric>
+#include "MantidKernel/VectorHelper.h"
+
+
+#include "MantidAlgorithms/GeneralisedSecondDifference.h"
+
+
+
+namespace Mantid
+{
+  namespace Algorithms
+  {
+
+    // Register the class into the algorithm factory
+    DECLARE_ALGORITHM(GeneralisedSecondDifference)
+
+    using namespace Kernel;
+    using namespace API;
+
+    // Get a reference to the logger
+    Logger& GeneralisedSecondDifference::g_log = Logger::get("GeneralisedSecondDifference");
+
+    /// Constructor
+    GeneralisedSecondDifference::GeneralisedSecondDifference(): Algorithm(),Cij(0),Cij2(0),z(0),m(0)
+    {
+    }
+    /// Destructor
+    GeneralisedSecondDifference::~GeneralisedSecondDifference()
+    {
+    }
+    /// Initialisation method.
+    void GeneralisedSecondDifference::init()
+    {
+
+      //Input and output workspaces
+      declareProperty(new WorkspaceProperty<Workspace>("InputWorkspace","",Direction::Input));
+      declareProperty(new WorkspaceProperty<Workspace>("OutputWorkspace","",Direction::Output));
+
+      BoundedValidator<int> *mustBePositive = new BoundedValidator<int>();
+      mustBePositive->setLower(0);
+      declareProperty("z",0,mustBePositive);
+      declareProperty("m",0,mustBePositive->clone());
+      declareProperty("spectra_min",0,mustBePositive->clone());
+      declareProperty("spectra_max",0,mustBePositive->clone());
+     }
+
+    /** Executes the algorithm
+     *
+     *  @throw runtime_error Thrown if algorithm cannot execute
+     */
+void GeneralisedSecondDifference::exec()
+{
+	// Message stream used for logger
+	std::ostringstream message;
+
+	// Get some properties
+  	Workspace_const_sptr inputWS=getProperty("InputWorkspace");
+  	int spec_min=getProperty("spectra_min");
+  	int spec_max=getProperty("spectra_max");
+	if (spec_min>spec_max)
+		std::swap(spec_min,spec_max);
+
+	if (spec_max>inputWS->getNumberHistograms())
+	{
+		message << "The value of spectra_max "<< spec_max << "is not valid";
+		throw std::runtime_error(message.str());
+		message.str("");
+	}
+
+	// Get some more input fields
+  	z=getProperty("z");
+  	m=getProperty("m");
+  	const int n_av=z*m+1;
+
+  	// Calculate thre Cij and Cij^2 coefficients
+  	computePrefactors();
+
+	const int n_specs=spec_max-spec_min+1;
+	const int n_points=inputWS->dataY(0).size()-2*n_av;
+  	//Create OuputWorkspace
+  	Workspace_sptr out= WorkspaceFactory::Instance().create(inputWS,n_specs,n_points+1,n_points);
+
+	const int nsteps=2*n_av+1;
+
+  	for (int i=spec_min;i<=spec_max;i++)
+  	{
+  		int out_index=i-spec_min;
+  		out->getAxis(1)->spectraNo(out_index)=inputWS->getAxis(1)->spectraNo(i);
+  		const std::vector<double>& refX=inputWS->dataX(i);
+  		const std::vector<double>& refY=inputWS->dataY(i);
+  		const std::vector<double>& refE=inputWS->dataE(i);
+  		std::vector<double>& outX=out->dataX(out_index);
+  		std::vector<double>& outY=out->dataY(out_index);
+  		std::vector<double>& outE=out->dataE(out_index);
+
+  		std::copy(refX.begin()+n_av,refX.end()-n_av,outX.begin());
+  		std::vector<double>::const_iterator itInY=refY.begin();
+  		std::vector<double>::iterator itOutY=outY.begin();
+  		std::vector<double>::const_iterator itInE=refE.begin();
+  		std::vector<double>::iterator itOutE=outE.begin();
+  		double err2;
+  		for (;itOutY!=outY.end();itOutY++,itInY++,itOutE++,itInE++)
+  		{
+  			//Calculate \sum_{j}Cij.Y(j)
+  			(*itOutY)=std::inner_product(itInY,itInY+nsteps,Cij.begin(),0.0);
+  			//Calculate the error bars sqrt(\sum_{j}Cij^2.E^2)
+  			err2=std::inner_product(itInE,itInE+nsteps,Cij2.begin(),0.0);
+  			(*itOutE)=sqrt(err2);
+  		}
+  	}
+  	setProperty("OutputWorkspace",out);
+  	return;
+}
+/** Compute the Cij
+ *
+ */
+void GeneralisedSecondDifference::computePrefactors()
+{
+	int zz=0;
+	int max_index_prev=1;
+	int n_el_prev=3;
+	std::vector<double> previous(n_el_prev);
+	previous[0]=1;
+	previous[1]=-2;
+	previous[2]=1;
+
+	if (z==0) //
+	{
+		Cij.resize(3);
+		std::copy(previous.begin(),previous.end(),Cij.begin());
+		Cij2.resize(3);
+		std::transform(Cij.begin(),Cij.end(),Cij2.begin(),Squares<double>());
+		return;
+	}
+	std::vector<double> next;
+	// Calculate the Cij iteratively.
+	do
+	{
+	zz++;
+	int max_index=zz*m+1;
+	int n_el=2*max_index+1;
+	next.resize(n_el);
+	std::fill(next.begin(),next.end(),0.0);
+	for (int i=0;i<n_el;++i)
+	{
+		int delta=-max_index+i;
+		for (int l=delta-m;l<=delta+m;l++)
+		{
+			int index=l+max_index_prev;
+			if (index>=0 && index<n_el_prev)
+				next[i]+=previous[index];
+		}
+	}
+	previous.resize(n_el);
+	std::copy(next.begin(),next.end(),previous.begin());
+	max_index_prev=max_index;
+	n_el_prev=n_el;
+	}while(zz!=z);
+
+
+	Cij.resize(2*z*m+3);
+	std::copy(previous.begin(),previous.end(),Cij.begin());
+	Cij2.resize(2*z*m+3);
+	std::transform(Cij.begin(),Cij.end(),Cij2.begin(),Squares<double>());
+	return;
+}
+
+
+
+  } // namespace Algorithm
+} // namespace Mantid
