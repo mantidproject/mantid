48f822bc91ed9a1c68452209be0e69db3aff5770
Ronald Fowler <ronald.fowler@stfc.ac.uk>
2009-02-04 17:23:03 +0000

    Another minor fix for the fix, for Linux in the header file Re #401

--- Code/Mantid/Nexus/inc/MantidNexus/NexusFileIO.h
+++ Code/Mantid/Nexus/inc/MantidNexus/NexusFileIO.h
@@ -125,7 +125,7 @@ namespace Mantid
       /// check if the gievn item exists in the current level
       bool checkEntryAtLevel(const std::string& item) const;
       /// check if given attribute name is in currently opened entry
-      bool NexusFileIO::checkAttributeName(const std::string& target) const;
+      bool checkAttributeName(const std::string& target) const;
       /// write test field
       int writeNexusTextField( const NXhandle& h, const std::string& name, const std::string& value);
       /// search for exisiting MantidWorkpace_n entries in opened file
