8d162f535542d22a16de5a1a51ca46cc39c1fdb4
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2009-02-13 11:51:58 +0000

    Updated the HRPD demo script. Re #407

--- Code/Mantid/PythonAPI/scripts/HRPD-Demo.py
+++ Code/Mantid/PythonAPI/scripts/HRPD-Demo.py
@@ -1,41 +1,36 @@
-# First some function definitions for the Loading algorithms
-def LoadData(outputarea):
-	LoadRawDialog(OutputWorkspace=outputarea,message="Enter path to data file")
-
-def LoadEmpty(outputarea):
-	LoadRawDialog(OutputWorkspace=outputarea, message="Enter path to empty data")
-
-def LoadSample(outputarea):
-	LoadRawDialog(OutputWorkspace=outputarea,message="Enter path to sample file")
-
+# First a function definition for the Loading algorithms which loads the data and immediately aligns the detectors
+def Load(type, outputArea):
+	LoadRawDialog(OutputWorkspace=outputArea, message = "Enter path to the file containing the " + type)
+	AlignDetectorsDialog(InputWorkspace=outputArea, OutputWorkspace=outputArea,message="Enter path to calibration file")
+		
 # ========== The script starts here ============
 dataWorkspace = "Vanadium"
-LoadData(dataWorkspace)
-LoadEmpty("Empty")
+Load("data", dataWorkspace)
+Load("empty run", "Empty")
 
 # ==== Just want pure Vanadium so subtract the empty instrument ===
 Minus(InputWorkspace1=dataWorkspace,InputWorkspace2="Empty",OutputWorkspace=dataWorkspace)
 # Calculate absorption and correct for it
 transWorkspace="Transmission"
+#The input workspace needs to be in units of wavelength for the CorrectForAttentuation algorithm
+ConvertUnits(InputWorkspace=dataWorkspace, OutputWorkspace=dataWorkspace, Target="Wavelength")
 CorrectForAttenuationDialog(InputWorkspace=dataWorkspace, OutputWorkspace=transWorkspace,SampleNumberDensity="0.072",ScatteringXSection="5.08",AttenuationXSection="5.1",message="Enter size parameters")
 Divide(InputWorkspace1=dataWorkspace, InputWorkspace2=transWorkspace, OutputWorkspace=dataWorkspace)
 
 # === Save as a Nexus file ===
-SaveNexusProcessDialog(InputWorkspace=sampleWorkspace, message="Enter path to save Nexus file")
+#SaveNexusProcessDialog(InputWorkspace=sampleWorkspace, message="Enter path to save Nexus file")
 
 # === Load the sample data file ===
 sampleWorkspace="Sample"
-LoadSample(sampleWorkspace)
+Load("sample", sampleWorkspace)
 
-# === Align and focus the data ===
-alignAlg = AlignDetectorsDialog(InputWorkspace=dataWorkspace,OutputWorkspace=dataWorkspace,message="Enter path to calibration file")
-calibFile = alignAlg.getPropertyValue("CalibrationFile")
-DiffractionFocussing(InputWorkspace=dataWorkspace,OutputWorkspace=dataWorkspace, GroupingFileName=calibFile)
+# === Focus the data ===
+alg = DiffractionFocussingDialog(InputWorkspace=dataWorkspace,OutputWorkspace=dataWorkspace)
+calibFile = alg.getPropertyValue("GroupingFileName")
 StripPeaks(InputWorkspace=dataWorkspace, OutputWorkspace=dataWorkspace)
 
-# === Align and focus the sample ===
-AlignDetectors(InputWorkspace=sampleWorkspace,OutputWorkspace=sampleWorkspace, CalibrationFile=calibFile)
-DiffractionFocussing(InputWorkspace=sampleWorkspace,OutputWorkspace=sampleWorkspace, GroupingFileName=calibFile)
+# === Focus the sample ===
+DiffractionFocussing(InputWorkspace=sampleWorkspace,OutputWorkspace=sampleWorkspace,GroupingFileName=calibFile)
 
 # === The result is the sample corrected by the vandium ===
 resultSpace="Result"
