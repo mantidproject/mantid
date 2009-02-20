56da7f25d477be3776e322419ddf069336500317
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2009-02-20 10:22:51 +0000

    Slight alteration to instrument script. Re #420

--- Code/Mantid/PythonAPI/scripts/InstrumentViewTest.py
+++ Code/Mantid/PythonAPI/scripts/InstrumentViewTest.py
@@ -5,15 +5,19 @@ LoadRaw("LOQ sans configuration/LOQ48094.raw","LOQTest")
 
 insView = getInstrumentView("LOQTest")
 
-insView.changeColorMap("../../Code/qtiplot/colormaps/_standard.map")
+#insView.changeColorMap("../../Code/qtiplot/colormaps/_standard.map")
 
-insView.setColorMapRange(0.,195)
+#insView.setColorMapRange(0.,195)
 # Or can set a minimum and maximum separately
 #insView.setColorMapMinValue(1.)
 #insView.setColorMapMaxValue(10.)
 
 # Alter the binning range
-insView.setBinRange(10000,15000)
+#insView.setBinRange(10000,15000)
 
 insView.showWindow()
 
+insView.selectComponent("hab-module")
+
+#insView.selectComponent("main-detector-pixel")
+#insView.selectComponent("LOQ")
