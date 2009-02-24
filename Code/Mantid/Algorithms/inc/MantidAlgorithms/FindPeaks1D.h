cdf31ecadd11d94aef5bb45904fb8dd36e92cbcf
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2009-02-24 18:33:15 +0000

    re#432 Put the peak detection routine in place. Works and has been tested for various data-sets. Still need to address unresolved multiple peaks.

--- Code/Mantid/Algorithms/inc/MantidAlgorithms/FindPeaks1D.h
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/FindPeaks1D.h
@@ -11,7 +11,47 @@ namespace Mantid
 {
 namespace Algorithms
 {
+/** This algorithm searches for peaks in a dataset.
+    The method used is based on the method detailed in: M.A.Mariscotti, NIM 50 (1967) 309.
 
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the Workspace to search for peaks. </LI>
+    <LI> PeaksList      - The name of the TableWorkspace in which to store the list of peaks found. </LI>
+    <LI> spectrum       - The spectrum number in the InputWorkspace.</LI>
+	<LI> spectrum       - The spectrum number in the InputWorkspace.</LI>
+	<LI> spectrum       - The spectrum number in the InputWorkspace.</LI>
+	<LI> smoothNpts     - The number of points for averaging, i.e. summing will be done in the range [y(i-m),y(i+m)] when
+						  calculating the second difference <\LI>
+	<LI> smoothIter     - The number of iteration iterations in the averaging procedure <\LI>
+	<LI> threashold     - The threahsold value for peak detection, i.e only points with Intensity/sigma>threashold
+	                      will be considered as peaks <\LI>
+    </UL>
+
+
+    @author Laurent Chapon, ISIS, Rutherford Appleton Laboratory
+    @date 25/11/2008
+
+    Copyright &copy; 2008-9 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
 class DLLExport FindPeaks1D : public API::Algorithm
 {
 public:
--- Code/Mantid/Algorithms/src/FindPeaks1D.cpp
+++ Code/Mantid/Algorithms/src/FindPeaks1D.cpp
@@ -24,6 +24,12 @@ FindPeaks1D::FindPeaks1D() : API::Algorithm(), peaks(new DataObjects::TableWorks
 void FindPeaks1D::init()
 {
   declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input));
+  // The found peaks in a table
+  declareProperty(new WorkspaceProperty<DataObjects::TableWorkspace>("PeaksList","",Direction::Output));
+  // Set up the columns for the TableWorkspace holding the peak information
+  peaks->createColumn("double","centre");
+  peaks->createColumn("double","width");
+  peaks->createColumn("double","height");
   BoundedValidator<int> *min = new BoundedValidator<int>();
   min->setLower(0);
   declareProperty("spectrum",0,min);
@@ -33,14 +39,9 @@ void FindPeaks1D::init()
   declareProperty("smoothIter",1,min->clone());
   BoundedValidator<double> *mind = new BoundedValidator<double>();
   mind->setLower(1.0);
-  declareProperty("Threashold",3.0,mind);
+  declareProperty("threashold",3.0,mind);
 
-  // The found peaks in a table
-  declareProperty(new WorkspaceProperty<DataObjects::TableWorkspace>("PeaksList","",Direction::Output));
-  // Set up the columns for the TableWorkspace holding the peak information
-  peaks->createColumn("double","centre");
-  peaks->createColumn("double","width");
-  peaks->createColumn("double","height");
+
   return;
 }
 
@@ -62,49 +63,104 @@ void FindPeaks1D::exec()
   retrieveProperties();
   // Now run generalisedsecondDifference sub-algo
   generalisedSecondDifference();
+  // Now find peaks in the vector
   analyseVector();
+  // Assign output
   setProperty("PeaksList",peaks);
   return;
 }
 
 void FindPeaks1D::generalisedSecondDifference()
 {
-	Algorithm_sptr second_diff;
+	Algorithm_sptr second_diff_alg;
 	  try
 	  {
-	    second_diff = createSubAlgorithm("GeneralisedSecondDifference");
+	    second_diff_alg = createSubAlgorithm("GeneralisedSecondDifference");
 	  }
 	  catch (Exception::NotFoundError)
 	  {
 	    g_log.error("Can't locate GeneralisedSecondDifference");
 	    throw;
 	  }
-	  second_diff->setProperty("InputWorkspace",input);
-	  second_diff->setProperty("z",smooth_iter);
-	  second_diff->setProperty("m",smooth_npts);
-	  second_diff->setProperty("spectra_min",spec_number);
-	  second_diff->setProperty("spectra_max",spec_number);
+	  second_diff_alg->setProperty("InputWorkspace",input);
+	  second_diff_alg->setProperty("z",smooth_iter);
+	  second_diff_alg->setProperty("m",smooth_npts);
+	  second_diff_alg->setProperty("spectra_min",spec_number);
+	  second_diff_alg->setProperty("spectra_max",spec_number);
 
 	    try {
-	      second_diff->execute();
+	      second_diff_alg->execute();
 	    } catch (std::runtime_error) {
 	      g_log.error("Unable to successfully run GeneralizedSecondDifference sub-algorithm");
 	      throw;
 	    }
 
-	    if ( ! second_diff->isExecuted() )
+	    if ( ! second_diff_alg->isExecuted() )
 	    {
 	      g_log.error("Unable to successfully run GeneralizedSecondDifference sub-algorithm");
 	      throw std::runtime_error("Unable to successfully run GeneralizedSecondDifference sub-algorithm");
 	    }
-	    second_diff_spec=second_diff->getProperty("OutputWorkspace");
-  return;
+	    // Everything is fine, get the second_difference
+	    second_diff_spec=second_diff_alg->getProperty("OutputWorkspace");
+	    return;
 }
 
 void FindPeaks1D::analyseVector()
 {
+	typedef std::vector<double> Vector;
 
-	return;
+	Vector& X=second_diff_spec->dataX(0);
+	Vector& Y=second_diff_spec->dataY(0);
+	Vector& E=second_diff_spec->dataE(0);
+	Vector& obs=input->dataY(spec_number);
+
+	// Number of points chopped at the beginning and end of the second-difference spectrum with
+	// respect to the initial data(obs).
+	int n_chop=smooth_iter*smooth_npts+1;
+
+	// Calculate Y divided by E.
+	Vector YoE(Y.size());
+	std::transform(Y.begin(),Y.end(),E.begin(),YoE.begin(),std::divides<double>());
+
+	Vector::iterator left=YoE.begin(),right=YoE.begin(),center;
+	typedef Vector::reverse_iterator rit;
+	rit rleft;
+	Vector::difference_type d;
+	double peak_center, peak_width, peak_amplitude;
+	int counter=0;
+	std::ostringstream mess;
+
+	for(;;) // Find the peaks
+	{
+		left=std::find_if(left,YoE.end(),std::bind2nd(std::less_equal<double>(),-threashold));
+		if (left==YoE.end())
+			break; // No more peaks
+		rleft=std::find_if(rit(left),rit(right),std::bind2nd(std::greater<double>(),1.0));
+		if (rleft==rit(right))
+			continue; // Not a peak
+		d=std::distance(rleft,YoE.rend());
+		peak_width=-*(X.begin()+d+1); //Left FWHM
+		right=std::find_if(left,YoE.end(),std::bind2nd(std::greater<double>(),1.0));
+		if (right==YoE.end())
+			break; //Not a peak
+		d=std::distance(YoE.begin(),right);
+		peak_width+=*(X.begin()+d); //Right FWHM
+		center=std::min_element(left,right);
+		d=std::distance(YoE.begin(),center);
+		peak_center=*(X.begin()+d);
+		peak_amplitude=*(obs.begin()+d+n_chop);
+		mess << "Find one peak at Pos:" << peak_center << ",A=" <<  peak_amplitude << "W=" << peak_width;
+		g_log.information(mess.str());
+		mess.str("");
+		DataObjects::TableRow newrow = peaks->appendRow();
+		newrow << peak_center << peak_amplitude << peak_width;
+		left=right;
+		counter++;
+	}
+	mess.str("");
+	mess << " Total number of peaks " << counter;
+	g_log.information(mess.str());
+return;
 
 }
 } // namespace Algorithms
