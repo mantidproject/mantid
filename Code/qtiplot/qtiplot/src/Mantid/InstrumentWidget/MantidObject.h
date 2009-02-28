661ce3d280b4c0c8e3b81428e9ba5030ac1cfa1b
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2009-02-28 00:12:47 +0000

    re#474 Remove warning

--- Code/qtiplot/qtiplot/src/Mantid/InstrumentWidget/MantidObject.h
+++ Code/qtiplot/qtiplot/src/Mantid/InstrumentWidget/MantidObject.h
@@ -1,6 +1,6 @@
 #ifndef MANTIDOBJECT_H_
 #define MANTIDOBJECT_H_
-#include "GLObject.h" 
+#include "GLObject.h"
 /*!
   \class  MantidObject
   \brief  Mantid ObjComponent OpenGL GLObject class
@@ -13,23 +13,27 @@
   Copyright &copy; 2007 STFC Rutherford Appleton Laboratories
 
   This file is part of Mantid.
- 	
+
   Mantid is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
-  
+
   Mantid is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  
+
   File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
 */
-class Mantid::Geometry::IObjComponent;
+namespace Mantid{
+	namespace Geometry{
+		class IObjComponent;
+	}
+}
 
 class MantidObject : public GLObject
 {
