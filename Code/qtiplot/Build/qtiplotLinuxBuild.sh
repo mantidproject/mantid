4fc51c637c46f1e2f81df7854a638e6daf04e25e
Matt Clarke <matt.clarke@stfc.ac.uk>
2009-03-04 12:07:52 +0000

    Refs #100. Modified build script to build MantidQt

--- Code/qtiplot/Build/qtiplotLinuxBuild.sh
+++ Code/qtiplot/Build/qtiplotLinuxBuild.sh
@@ -2,6 +2,10 @@
 pwd
 rm ../../../../logs/qtiplot.log
 rm ../../../../logs/qtiplotErr.log
+cd MantidQt
+qmake-qt4
+make
+cd ..
 cd qtiplot/
 qmake-qt4 
 make >> ../../../../../logs/qtiplot.log 2> ../../../../../logs/qtiplotErr.log
