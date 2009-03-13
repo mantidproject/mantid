32fed81767ac1816f6517bb71210470325ae8561
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2009-03-13 17:46:11 +0000

    re#498 First part of the unit-test of new Rbin function

---
+++ Code/Mantid/Kernel/test/RebinHistogramTest.h
@@ -0,0 +1,71 @@
+#ifndef REBINHISTOGRAM_TEST_H_
+#define REBINHISTOGRAM_TEST_H_
+
+#include <cxxtest/TestSuite.h>
+#include <vector>
+#include "MantidKernel/VectorHelper.h"
+
+using namespace Mantid::Kernel;
+
+
+
+/// @author Laurent C Chapon, ISIS Facility, Rutherford Appleton Laboratory
+/// 13/03/2009
+/// This is testing the validity of the rebinHistogram function in vectorHelper Class
+
+class RebinHistogramTest : public CxxTest::TestSuite
+{
+	public:
+  /// Size of vectors
+  int size1, size2;
+  /// Some vectors
+  std::vector<double> xin,yin,ein,xout,yout,eout;
+
+  RebinHistogramTest(){}
+
+  /// Create a new X vector where the steps are half the size of the old one.
+  /// Perform rebin and check the values
+  /// Y data should now contains half the intensity
+  /// E data should contains the
+  /// Perform another rebin in the opposite direction and check that the data are identical to initial values
+  void TestRebinSmallerSteps()
+  {
+	size1=12;size2=23;
+  	xin.resize(size1);
+  	yin.resize(size1-1);
+  	ein.resize(size1-1);
+  	xout.resize(size2);
+  	yout.resize(size2-1);
+  	eout.resize(size2-1);
+  	for (int i=0;i<size1-1;i++)
+  	{
+  		xin[i]=(double)(i);
+  		yin[i]=1.0;
+  		ein[i]=1.0;
+  	}
+  	xin[size1-1]=size1-1;
+  	for (int i=0;i<size2;i++)
+  		xout[i]=0.5*i;
+  	rebinHistogram(xin,yin,ein,xout,yout,eout,false);
+  	for (int i=0;i<size2-1;i++)
+  	{
+  		TS_ASSERT_DELTA(yout[i],0.5,1e-7);
+  		TS_ASSERT_DELTA(eout[i],1.0/sqrt(2.0),1e-7);
+
+  	}
+  std::vector<double> returnX(xin), returnY(size1-1), returnE(size1-1);
+
+  rebinHistogram(xout,yout,eout,returnX,returnY,returnE,false);
+  for (int i=0;i<size1-1;i++)
+  {
+	  TS_ASSERT_DELTA(returnY[i],yin[i],1e-7);
+	  TS_ASSERT_DELTA(returnE[i],ein[i],1e-7);
+  }
+
+  }
+
+
+};
+
+
+#endif //REBINHISTOGRAM_TEST_H_/
