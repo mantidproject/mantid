285e00f90b167511da7a672c092d0ad609d7dbe7
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2009-04-20 16:31:29 +0000

    Add in option for passing Mantid version and subversion revision. These can be obtained using programs like svnversion / SubWCRev Fixes #551

--- Code/qtiplot/release_date.py
+++ Code/qtiplot/release_date.py
@@ -1,7 +1,24 @@
+import sys
 from datetime import date
-f = open('qtiplot/src/Mantid/MantidPlotReleaseDate.h','w')
-f.write('#ifndef MANTIDPLOT_RELEASE_DATE\n')
-f.write('#define MANTIDPLOT_RELEASE_DATE "')
-f.write(date.today().strftime("%d %b %Y"))
-f.write('"\n#endif\n')
-f.close()
+
+# argv[1] can optionally be a version number
+# argv[2] can optionally be a subversion revision
+
+def main(argv=None) :
+    if argv is None :
+        argv = sys.argv
+    f = open('qtiplot/src/Mantid/MantidPlotReleaseDate.h','w')
+    f.write('#ifndef MANTIDPLOT_RELEASE_DATE\n')
+    f.write('#define MANTIDPLOT_RELEASE_DATE "')
+    f.write(date.today().strftime("%d %b %Y"))
+    if len(sys.argv) > 1 :
+        f.write(' (Version '+argv[1])
+        if len(sys.argv) > 2 :
+            f.write(', SVN R'+argv[2])
+        f.write(')')
+    f.write('"\n#endif\n')
+    f.close()
+    return 0
+
+if __name__ == "__main__" :
+    sys.exit(main())
