2239522b27c9f1326d2bcca3485dd8e7255eb7d8
Russell Taylor <taylorrj@ornl.gov>
2009-06-08 13:58:58 +0000

    Meant to say the Test directory. Re #100.

--- Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
+++ Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
@@ -4,7 +4,7 @@ rm ../../../../logs/sconsErr.log
 rm ../../../../logs/svn.log
 rm ../../../../logs/doxy.log
 svn log -v -rBASE > ../../../../logs/svn.log
-svn update ../Third_Party
+svn update ../../Test
 cd Build
 sh build.sh "skiptest=1" >> ../../../../../logs/scons.log 2> ../../../../../logs/sconsErr.log
 doxygen Mantid.doxyfile 2> ../../../../../logs/doxy.log
