a5c183eeeb6063594f6efca1850ab2de8516347a
Russell Taylor <taylorrj@ornl.gov>
2009-06-08 13:45:10 +0000

    Add update of Third_Party directory to linux buildserver build. Re #100.

--- Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
+++ Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
@@ -4,6 +4,7 @@ rm ../../../../logs/sconsErr.log
 rm ../../../../logs/svn.log
 rm ../../../../logs/doxy.log
 svn log -v -rBASE > ../../../../logs/svn.log
+svn update ../Third_Party
 cd Build
 sh build.sh "skiptest=1" >> ../../../../../logs/scons.log 2> ../../../../../logs/sconsErr.log
 doxygen Mantid.doxyfile 2> ../../../../../logs/doxy.log
