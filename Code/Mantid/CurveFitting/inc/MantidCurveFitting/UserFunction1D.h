8773c6bf686c82f3cc0498c18af9926c1f0b9996
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2009-07-14 17:32:42 +0000

    Fix muParser path. Refs #745.

--- Code/Mantid/CurveFitting/inc/MantidCurveFitting/UserFunction1D.h
+++ Code/Mantid/CurveFitting/inc/MantidCurveFitting/UserFunction1D.h
@@ -5,7 +5,7 @@
 // Includes
 //----------------------------------------------------------------------
 #include "MantidCurveFitting/Fit1D.h"
-#include <muParser/muParser.h>
+#include <muParser.h>
 #include <boost/shared_array.hpp>
 
 namespace Mantid
