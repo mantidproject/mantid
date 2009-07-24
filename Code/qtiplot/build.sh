b6f42d81ddaf0d5e8d5b250ca7ae1cd6515dc5e6
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2009-07-24 10:57:49 +0000

    A few corrections and improvements for the Linux qtiplot build script. Re #100

--- Code/qtiplot/build.sh
+++ Code/qtiplot/build.sh
@@ -6,16 +6,29 @@
 python release_date.py
 
 # The top-level directory 
-ROOTDIR=pwd
+ROOTDIR=`pwd`
 
 # First, build dialog library
-cd $ROOTDIR\MantidQt
+cd $ROOTDIR/MantidQt
 qmake
-nmake clean
-nmake
+make clean
+make
+ERRORCODE=$?
+if [ $ERRORCODE != 0 ]; then
+    echo "Error: MantidQt build failed with error code $ERRORCODE"
+    exit $ERRORCODE
+fi
 
 # Now build qtiplot
-cd $ROOTDIR\qtiplot
-nmake clean
+cd $ROOTDIR/qtiplot
 qmake
-nmake
+make clean
+make
+ERRORCODE=$?
+if [ $ERRORCODE? != 0 ]; then
+    echo "Error: MantidPlot build failed with error code $ERRORCODE"
+    exit $ERRORCODE
+fi
+
+# If we reached this point then the build was successful
+echo "MantidQt and MantidPlot build succeeded"
