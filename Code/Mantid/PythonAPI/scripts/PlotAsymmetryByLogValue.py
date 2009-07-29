1a1b4696578c463867bf98e42f7b442ed4c22ba3
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2009-07-29 12:07:49 +0000

    Added python script for running PlotAsymmetryByLogValue. re #748

---
+++ Code/Mantid/PythonAPI/scripts/PlotAsymmetryByLogValue.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------
+#  Runs PlotAssymetryByLogValue algorithm
+#  and plots the output in MantidPlot
+#------------------------------------------------------
+
+# Execute the algorithm
+alg = PlotAsymmetryByLogValueDialog()
+
+# Get the output workspace
+ws = alg.getPropertyValue("OutputWorkspace")
+
+g1 = plotSpectrum(ws,0)
+g1.activeLayer().setCurveTitle(0,"Difference")
+
+green = int(alg.getPropertyValue("Green"))
+
+# This should check if Green is set
+if green < 1000:
+	mergePlots(g1, plotSpectrum(ws,1))
+	g1.activeLayer().setCurveTitle(1,"Red")
+	mergePlots(g1, plotSpectrum(ws,2))
+	g1.activeLayer().setCurveTitle(2,"Green")
+	mergePlots(g1, plotSpectrum(ws,3))
+	g1.activeLayer().setCurveTitle(3,"Sum")
