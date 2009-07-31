acdf848cd4360fe7b0d70cd6ab547d6be16036b3
Matt Clarke <matt.clarke@stfc.ac.uk>
2009-07-31 09:56:45 +0000

    Refs #721. Added test scripts for win64.

---
+++ Code/Mantid/Build/WinBuildScripts/ExecTests64.bat
@@ -0,0 +1,3 @@
+cd Build\Tests
+python ..\WinBuildScripts\doWinTests64.py
+TestsScript.bat 2> ..\..\..\..\..\..\logs\Mantid\testsRunErr.log
---
+++ Code/Mantid/Build/WinBuildScripts/doWinTests64.py
@@ -0,0 +1,19 @@
+import os
+
+f=open('../TestsToRun', 'r')
+
+g=open('TestsScript.bat', 'w')
+
+logfile = '../../../../../../logs/Mantid/testResults.log'
+g.write('set PATH=../../../Third_Party\lib\win64;%PATH%\n')
+g.write('set PATH=../../Bin/Shared;%PATH%\n')
+
+for test in f:
+	name = test.split('/')
+	g.write('echo ' + name[-1].strip().rstrip('.h') + ' >> ' + logfile + '\n')
+	g.write(name[-1].strip().rstrip('.h') + ' >> ' + logfile + '\n')
+	g.write('del ' + name[-1].strip().rstrip('.h') + '.cpp\n')
+	
+f.close()
+g.close()
+	
