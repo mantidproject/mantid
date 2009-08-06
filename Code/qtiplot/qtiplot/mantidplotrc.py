bf0b891ba043a411934e273fcfffe9cf5438770c
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2009-08-06 11:48:54 +0000

    Small fix to include the plotBin function correctly in the Python API. Fixes #787

--- Code/qtiplot/qtiplot/mantidplotrc.py
+++ Code/qtiplot/qtiplot/mantidplotrc.py
@@ -26,6 +26,7 @@ MantidUIImports = [
     'importTableWorkspace',
     'plotSpectrum',
     'plotTimeBin',
+    'plotBin',
     'getMantidMatrix',
     'getInstrumentView', 
     'getSelectedWorkspaceName',
