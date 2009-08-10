bab6656ecb14028203233d1db8d440eb8654ab92
Matt Clarke <matt.clarke@stfc.ac.uk>
2009-08-10 15:37:08 +0000

    Refs #721. Modified script to skip muon nexus tests.

--- Code/Mantid/Build/WinBuildScripts/doWinTests64.py
+++ Code/Mantid/Build/WinBuildScripts/doWinTests64.py
@@ -4,15 +4,29 @@ f=open('../TestsToRun', 'r')
 
 g=open('TestsScript.bat', 'w')
 
+#tests not to run
+h=open('../TestsNotToRun64', 'r')
+notRuns = []
+for file in h:
+	(filepath, filename) = os.path.split(file)
+	notRuns.append(filename.strip())
+
+
 logfile = '../../../../../../logs/Mantid/testResults.log'
 g.write('set PATH=../../../Third_Party\lib\win64;%PATH%\n')
 g.write('set PATH=../../Bin/Shared;%PATH%\n')
 
 for test in f:
 	name = test.split('/')
-	g.write('echo ' + name[-1].strip().rstrip('.h') + ' >> ' + logfile + '\n')
-	g.write(name[-1].strip().rstrip('.h') + ' >> ' + logfile + '\n')
-	g.write('del ' + name[-1].strip().rstrip('.h') + '.cpp\n')
+	
+	try:
+		notRuns.index(name[-1].strip())
+		print "Not Running: ", name[-1].strip()
+	except ValueError:
+		g.write('echo ' + name[-1].strip().rstrip('.h') + ' >> ' + logfile + '\n')
+		g.write(name[-1].strip().rstrip('.h') + ' >> ' + logfile + '\n')
+		g.write('del ' + name[-1].strip().rstrip('.h') + '.cpp\n')
+	
 	
 f.close()
 g.close()
