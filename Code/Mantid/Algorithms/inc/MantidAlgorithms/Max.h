22d1b54cbf611dfa3786eba326e352218b68db85
Laurent Chapon <laurent.chapon@stfc.ac.uk>
2009-08-11 12:39:17 +0000

    re#809 Max algorithm

---
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/Max.h
@@ -0,0 +1,86 @@
+#ifndef MANTID_ALGORITHMS_MAX_H_
+#define MANTID_ALGORITHMS_MAX_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+/** Takes a 2D workspace as input and find the maximum in each 1D spectrum.
+ * The algorithm creates a new 1D workspace containing all maxima as well as their X boundaries
+ * and error. This is used in particular for single crystal as a quick way to find strong peaks.
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the Workspace2D to take as input </LI>
+    <LI> OutputWorkspace - The name of the workspace in which to store the result </LI>
+    </UL>
+
+    Optional Properties (assume that you count from zero):
+    <UL>
+    <LI> Range_lower - The X value to search from (default 0)</LI>
+    <LI> Range_upper - The X value to search to (default max)</LI>
+    <LI> StartSpectrum - Start spectrum number (default 0)</LI>
+    <LI> EndSpectrum - End spectrum number  (default max)</LI>
+    </UL>
+
+	@author L C Chapon, ISIS, Rutherford Appleton Laboratory
+    @date 11/08/2009
+    Copyright &copy; 2007-8 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+ */
+class DLLExport Max : public API::Algorithm
+{
+public:
+  /// Default constructor
+  Max() : API::Algorithm() {};
+  /// Destructor
+  virtual ~Max() {};
+  /// Algorithm's name for identification overriding a virtual method
+  virtual const std::string name() const { return "Max";}
+  /// Algorithm's version for identification overriding a virtual method
+  virtual const int version() const { return (1);}
+  /// Algorithm's category for identification overriding a virtual method
+  virtual const std::string category() const { return "General";}
+
+private:
+  // Overridden Algorithm methods
+  void init();
+  void exec();
+
+  /// The value in X to start the search from
+  double m_MinRange;
+  /// The value in X to finish the search at
+  double m_MaxRange;
+  /// The spectrum to start the integration from
+  int m_MinSpec;
+  /// The spectrum to finish the integration at
+  int m_MaxSpec;
+
+};
+
+} // namespace Algorithm
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_MAX_H_*/
---
+++ Code/Mantid/Algorithms/src/Max.cpp
@@ -0,0 +1,130 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/Max.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidKernel/VectorHelper.h"
+#include "MantidAPI/Progress.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+// Register the class into the algorithm factory
+DECLARE_ALGORITHM(Max)
+
+using namespace Kernel;
+using namespace API;
+
+
+/** Initialisation method.
+ *
+ */
+void Max::init()
+{
+  declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input,new HistogramValidator<>));
+  declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output));
+
+  declareProperty("RangeLower",EMPTY_DBL());
+  declareProperty("RangeUpper",EMPTY_DBL());
+  BoundedValidator<int> *mustBePositive = new BoundedValidator<int>();
+  mustBePositive->setLower(0);
+  declareProperty("StartWorkspaceIndex",0, mustBePositive);
+  // As the property takes ownership of the validator pointer, have to take care to pass in a unique
+  // pointer to each property.
+  declareProperty("EndWorkspaceIndex",0, mustBePositive->clone());
+}
+
+/** Executes the algorithm
+ *
+ *  @throw runtime_error Thrown if algorithm cannot execute
+ */
+void Max::exec()
+{
+  // Try and retrieve the optional properties
+  m_MinRange = getProperty("RangeLower");
+  m_MaxRange = getProperty("RangeUpper");
+  //m_MinSpec = getProperty("StartSpectrum");
+  //m_MaxSpec = getProperty("EndSpectrum");
+  m_MinSpec = getProperty("StartWorkspaceIndex");
+  m_MaxSpec = getProperty("EndWorkspaceIndex");
+
+
+  // Get the input workspace
+  MatrixWorkspace_const_sptr localworkspace = getProperty("InputWorkspace");
+
+  const int numberOfSpectra = localworkspace->getNumberHistograms();
+
+  // Check 'StartSpectrum' is in range 0-numberOfSpectra
+  if ( m_MinSpec > numberOfSpectra )
+  {
+    g_log.warning("StartSpectrum out of range! Set to 0.");
+    m_MinSpec = 0;
+  }
+  if ( !m_MaxSpec ) m_MaxSpec = numberOfSpectra-1;
+  if ( m_MaxSpec > numberOfSpectra-1 || m_MaxSpec < m_MinSpec )
+  {
+    g_log.warning("EndSpectrum out of range! Set to max detector number");
+    m_MaxSpec = numberOfSpectra;
+  }
+  if ( m_MinRange > m_MaxRange )
+  {
+    g_log.warning("Range_upper is less than Range_lower. Will integrate up to frame maximum.");
+    m_MaxRange = 0.0;
+  }
+
+  // Create the 1D workspace for the output
+  MatrixWorkspace_sptr outputWorkspace = API::WorkspaceFactory::Instance().create(localworkspace,m_MaxSpec-m_MinSpec+1,2,1);
+
+
+  Progress progress(this,0,1,m_MinSpec,m_MaxSpec,1);
+
+  // Loop over spectra
+  for (int i = m_MinSpec,j=0; i <= m_MaxSpec; ++i,++j)
+  {
+    if (localworkspace->axes() > 1)
+    {
+      outputWorkspace->getAxis(1)->spectraNo(j) = localworkspace->getAxis(1)->spectraNo(i);
+    }
+
+    // Retrieve the spectrum into a vector
+    const MantidVec& X = localworkspace->readX(i);
+    const MantidVec& Y = localworkspace->readY(i);
+
+    // Find the range [min,max]
+    MantidVec::const_iterator lowit, highit;
+    if (m_MinRange == EMPTY_DBL()) lowit=X.begin();
+    else lowit=std::lower_bound(X.begin(),X.end(),m_MinRange);
+
+    if (m_MaxRange == EMPTY_DBL()) highit=X.end();
+    else highit=std::find_if(lowit,X.end(),std::bind2nd(std::greater<double>(),m_MaxRange));
+
+    // If range specified doesn't overlap with this spectrum then bail out
+    if ( lowit == X.end() || highit == X.begin() ) continue;
+
+    highit--; // Upper limit is the bin before, i.e. the last value smaller than MaxRange
+
+    MantidVec::difference_type distmin=std::distance(X.begin(),lowit);
+    MantidVec::difference_type distmax=std::distance(X.begin(),highit);
+
+    if (distmin0 != distmin || distmax0 != distmax)
+        g_log.debug()<<"Starting with spectrum "<<i<<" bins selected: from "<<distmin<<" ("<<*(X.begin()+distmin)
+        <<") to "<<distmax<<" ("<<*(X.begin()+distmax)<<")\n";
+
+    MantidVec::const_iterator maxY=std::max_element(Y.begin()+distmin,Y.begin()+distmax);
+    MantidVec::difference_type d=std::distance(Y.begin(),maxY);
+    outputWorkspace->dataX(i)[0]=*(X.begin()+d);
+    outputWorkspace->dataX(i)[1]=*(X.begin()+d+1); //This is safe since X is of dimension Y+1
+    outputWorkspace->dataY(i)[0]=*maxY;
+    progress.report();
+  }
+
+  // Assign it to the output workspace property
+  setProperty("OutputWorkspace",outputWorkspace);
+
+  return;
+}
+
+} // namespace Algorithms
+} // namespace Mantid
