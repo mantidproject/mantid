87b76f697472e034bcc7d418d316d769c17124f4
Russell Taylor <taylorrj@ornl.gov>
2009-09-08 16:00:11 +0000

    Remove (incorrect) hard-coded IP address from qtiplot build emails. Re #868.

--- Code/qtiplot/Build/sendEmail.py
+++ Code/qtiplot/Build/sendEmail.py
@@ -6,10 +6,15 @@ from time import strftime
 
 #Email settings
 smtpserver = 'outbox.rl.ac.uk'
-localServerName = 'http://130.246.49.183/'
-#localServerName = 'file://c|/Program Files/CruiseControl/'
+localServerName = 'http://' 
+if (os.name =='nt'):
+     SENDER = 'Win' + SENDER
+     localServerName = localServerName + os.getenv('COMPUTERNAME') + '/'
 
-#RECIPIENTS = ['r.tolchenov@rl.ac.uk']
+else:
+     SENDER = 'Linux' + SENDER
+     localServerName = localServerName + os.getenv('HOSTNAME') + '/'
+
 RECIPIENTS = ['mantid-buildserver@mantidproject.org']
 SENDER = 'BuildServer1@mantidproject.org'
 if (os.name =='nt'):
