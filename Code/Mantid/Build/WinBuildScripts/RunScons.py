4cb6dbe60d675ad6adeb7c2b4dcd1d7c90eacdbe
Russell Taylor <taylorrj@ornl.gov>
2009-09-09 13:29:31 +0000

    Improvements to build scripts. Re #454

--- Code/Mantid/Build/WinBuildScripts/RunScons.py
+++ Code/Mantid/Build/WinBuildScripts/RunScons.py
@@ -1,7 +1,30 @@
 import os
+import subprocess as sp
+import platform
 import buildNotification
 
-print 'Current path == ',os.getcwd()
 buildNotification.sendBuildStarted("Mantid")
-os.system('Build\Winbuildscripts\RunScons.bat')
+
+sp.call("svn up", shell=True)
+svnlog = open("../../../../logs/Mantid/svn.log","w")
+sp.call("svn log -v -rBASE",stdout=svnlog,shell=True)
+svnlog.close()
+
+sp.call("svn up ../Third_Party", shell=True)
+sp.call("svn up ../../Test", shell=True)
+sp.call("svn up ../../Images", shell=True)
+
+doxylog = open("../../../../logs/Mantid/doxy.log","w")
+sp.call("doxygen Mantid.doxyfile",stderr=doxylog,shell=True,cwd="Build")
+doxylog.close()
+
+sconslog = open("../../../../logs/Mantid/scons.log","w")
+sconserr = open("../../../../logs/Mantid/sconsErr.log","w")
+buildargs = ["-j2","skiptest=1"]
+if platform.system() != 'Darwin':
+    buildargs.append("matlab=1")
+sp.call("python build.py "+' '.join(buildargs),stdout=sconslog,stderr=sconserr,shell=True)
+sconslog.close()
+sconserr.close()
+
 buildNotification.sendBuildCompleted("Mantid")
---
+++ Code/Mantid/build.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+import sys
+
+execfile("release_version.py")
+
+retcode = subprocess.call("scons "+' '.join(sys.argv[1:]), shell=True)
+if retcode != 0:
+    p = subprocess.call("python ../Third_Party/src/scons-local/scons.py "+' '.join(sys.argv[1:]), shell=True)
