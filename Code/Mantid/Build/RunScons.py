a629bb8aabe187139225b5a9ee6b260e363df555
Russell Taylor <taylorrj@ornl.gov>
2009-09-11 12:10:41 +0000

    Add some output to the build scripts so that progress can be seen on CruiseControl dashboard. Re #454.

--- Code/Mantid/Build/RunScons.py
+++ Code/Mantid/Build/RunScons.py
@@ -14,10 +14,13 @@ sp.call("svn up ../Third_Party", shell=True)
 sp.call("svn up ../../Test", shell=True)
 sp.call("svn up ../../Images", shell=True)
 
+print "Building documentation..."
 doxylog = open("../../../../logs/Mantid/doxy.log","w")
 sp.call("doxygen Mantid.doxyfile",stderr=doxylog,shell=True,cwd="Build")
 doxylog.close()
+print "Done building documentation"
 
+print "Building Mantid framework..."
 sconslog = open("../../../../logs/Mantid/scons.log","w")
 sconserr = open("../../../../logs/Mantid/sconsErr.log","w")
 buildargs = ["-j2","skiptest=1"]
@@ -26,5 +29,6 @@ if platform.system() != 'Darwin':
 sp.call("python build.py "+' '.join(buildargs),stdout=sconslog,stderr=sconserr,shell=True)
 sconslog.close()
 sconserr.close()
+print "Mantid build complete."
 
 buildNotification.sendBuildCompleted("Mantid")
--- Code/Mantid/Build/WinBuildScripts/RunTests.py
+++ Code/Mantid/Build/WinBuildScripts/RunTests.py
@@ -7,7 +7,9 @@ buildNotification.sendTestBuildStarted("Mantid")
 
 buildlog = open("../../../../logs/Mantid/testsBuild.log","w")
 builderr = open("../../../../logs/Mantid/testsBuildErr.log","w")
+print "Building the tests..."
 sp.call("python build.py",stdout=buildlog,stderr=builderr,shell=True)
+print "Tests build complete."
 buildlog.close()
 builderr.close()
 
@@ -16,20 +18,25 @@ buildNotification.sendTestBuildCompleted("Mantid")
 # Then run them
 buildNotification.sendTestStarted("Mantid")
 
+print "Running the tests..."
 runlog = open("../../../../logs/Mantid/testResults.log","w")
+runerr = open("../../../../logs/Mantid/testRunErr.log","w")
 testDir = "Build/Tests"
 testsToRun = os.listdir(testDir)
 for test in testsToRun:
     if test.endswith("cpp"):
         test = test.split(".")[0]
+        print "Running " + test
         runlog.write(test+"\n")
         runlog.flush()
         if os.name == 'nt':
             test += ".exe"
         else:
             test = "./" + test
-        sp.call(test,stdout=runlog,shell=True,cwd=testDir)
+        sp.call(test,stdout=runlog,stderr=runerr,shell=True,cwd=testDir)
         runlog.flush()
 
 runlog.close()
+runerr.close()
+print "Done running tests."
 
