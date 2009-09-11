aeb44370b7c2c73df822cf1f0eaf77f3e89e3cd7
Russell Taylor <taylorrj@ornl.gov>
2009-09-11 14:57:42 +0000

    Try again. Re #454.

--- Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
+++ Code/Mantid/Build/LinuxBuildScripts/RunScons.sh
@@ -5,6 +5,6 @@ rm ../../../../logs/svn.log
 rm ../../../../logs/doxy.log
 svn log -v -rBASE > ../../../../logs/svn.log
 svn update ../../Test
+sh build.sh "skiptest=1" >> ../../../../logs/scons.log 2> ../../../../logs/sconsErr.log
 cd Build
-sh ../build.sh "skiptest=1" >> ../../../../../logs/scons.log 2> ../../../../../logs/sconsErr.log
 doxygen Mantid.doxyfile 2> ../../../../../logs/doxy.log
--- Code/Mantid/Build/LinuxBuildScripts/RunTests.sh
+++ Code/Mantid/Build/LinuxBuildScripts/RunTests.sh
@@ -4,9 +4,8 @@ rm ../../../../logs/testsBuildErr.log
 rm ../../../../logs/testResults.log
 rm ../../../../logs/testsRunErr.log
 rm ../../../../logs/PythonResults.log
+sh build.sh >> ../../../../logs/testsBuild.log 2> ../../../../logs/testsBuildErr.log
 cd Build/Tests
-pwd
-sh ../../build.sh >> ../../../../../../logs/testsBuild.log 2> ../../../../../../logs/testsBuildErr.log
 python ../LinuxBuildScripts/doTests.py
 sh TestsScript.sh 2> ../../../../../../logs/testsRunErr.log
 
