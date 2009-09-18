bf14e56940044e186f600ed43b03f86f20eef5cb
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2009-09-18 07:59:29 +0000

    Missing files added. re #750

---
+++ Code/qtiplot/qtiplot/src/Mantid/MantidCurve.cpp
@@ -0,0 +1,252 @@
+#include "MantidCurve.h"
+#include "MantidUI.h"
+#include <MantidAPI/MatrixWorkspace.h>
+#include <MantidAPI/AnalysisDataService.h>
+
+#include "../Graph.h"
+
+#include <qpainter.h>
+#include <stdexcept>
+
+/**
+ *  @param name The curve's name - shown in the legend
+ *  @param workspace The source workspace for the curve's data
+ *  @param g The Graph widget which will display the curve
+ *  @param type The type of the QwtData: "spectra" for MantidQwtDataSpectra or
+ *              "bin" for MantidQwtDataBin
+ *  @param index The index of the spectrum or bin in the workspace
+ *  @param err True if the errors are to be plotted
+ */
+MantidCurve::MantidCurve(const QString& name,boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace,Graph* g,
+                         const QString& type,int index,bool err)
+:PlotCurve(name),m_drawErrorBars(err),m_wsName("")
+{
+  init(workspace,g,type,index);
+}
+
+/**
+ *  @param wsName The workspace name.
+ *  @param g The Graph widget which will display the curve
+ *  @param type The type of the QwtData: "spectra" for MantidQwtDataSpectra or
+ *              "bin" for MantidQwtDataBin
+ *  @param index The index of the spectrum or bin in the workspace
+ *  @param err True if the errors are to be plotted
+ */
+MantidCurve::MantidCurve(const QString& wsName,Graph* g,
+                         const QString& type,int index,bool err)
+:PlotCurve(createCurveName(wsName,type,index)),m_drawErrorBars(err),m_wsName(wsName)
+{
+  Mantid::API::MatrixWorkspace_sptr ws = 
+    boost::dynamic_pointer_cast<Mantid::API::MatrixWorkspace>(
+       Mantid::API::AnalysisDataService::Instance().retrieve(wsName.toStdString())
+    );
+  init(ws,g,type,index);
+  connect(mantidUI,SIGNAL(workspace_removed(const QString&)),this,SLOT(workspaceRemoved(const QString&)));
+  connect(mantidUI,SIGNAL(workspace_replaced(const QString &, Mantid::API::Workspace_sptr))
+    ,this,SLOT(workspaceReplaced(const QString &, Mantid::API::Workspace_sptr)));
+
+}
+
+MantidCurve::MantidCurve(const MantidCurve& c)
+:PlotCurve(c.title().text()),m_drawErrorBars(c.m_drawErrorBars),m_wsName(c.m_wsName)
+{
+  setData(c.data());
+  connect(mantidUI,SIGNAL(workspace_removed(const QString&)),this,SLOT(workspaceRemoved(const QString&)));
+}
+
+/**
+ *  @param workspace The source workspace for the curve's data
+ *  @param g The Graph widget which will display the curve
+ *  @param type The type of the QwtData: "spectra" for MantidQwtDataSpectra or
+ *              "bin" for MantidQwtDataBin
+ *  @param index The index of the spectrum or bin in the workspace
+ */
+void MantidCurve::init(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace,Graph* g,
+                         const QString& type,int index)
+{
+  if (type == "spectra")
+  {
+    MantidQwtDataSpectra data(workspace,index);
+    setData(data);
+  }
+  else
+    throw std::runtime_error("Unrecognized MuntidCurve type " + type.toStdString());
+  if (workspace->isHistogramData())
+  {
+    setStyle(QwtPlotCurve::Steps);
+    setCurveAttribute(Inverted,true);// this is the Steps style modifier that makes horizontal steps
+  }
+  else
+    setStyle(QwtPlotCurve::Lines);
+  if (g)
+  {
+    g->insertCurve(this);
+  }
+}
+
+/** If the removed workspace is the one displayed by this curve remove this curve.
+ *  @param wsName The workspace name.
+ */
+void MantidCurve::workspaceRemoved(const QString& wsName)
+{
+  if (!mantidData()) return;
+  Mantid::API::MatrixWorkspace_sptr ws = 
+    boost::dynamic_pointer_cast<Mantid::API::MatrixWorkspace>(
+       Mantid::API::AnalysisDataService::Instance().retrieve(wsName.toStdString())
+    );
+  if (mantidData()->sameWorkspace(ws))
+  {
+    emit removeMe(this);// this doesnot delete the curve ?
+  }
+}
+
+void MantidCurve::workspaceReplaced(const QString &, boost::shared_ptr<const Mantid::API::Workspace>)
+//void MantidCurve::workspaceReplaced(const QString &, Mantid::API::Workspace_sptr)
+{
+  std::cerr<<"MantidCurve: workspace replaced\n";
+}
+
+MantidCurve::~MantidCurve()
+{
+  //std::cerr<<"MantidCurve deleted\n";
+}
+
+void MantidCurve::setData(const QwtData &data)
+{
+  if (!dynamic_cast<const MantidQwtData*>(&data)) 
+    throw std::runtime_error("Only MantidQwtData can be set to a MantidCurve");
+  PlotCurve::setData(data);
+}
+
+void MantidCurve::draw(QPainter *p, 
+          const QwtScaleMap &xMap, const QwtScaleMap &yMap,
+          const QRect &rect) const
+{
+  PlotCurve::draw(p,xMap,yMap,rect);
+
+  if (m_drawErrorBars)// drawing error bars
+  {
+    const MantidQwtData* d = dynamic_cast<const MantidQwtData*>(&data());
+    if (!d)
+      throw std::runtime_error("Only MantidQwtData can be set to a MantidCurve");
+    for (int i = 0; i < dataSize(); i++)
+    {
+      const int xi = xMap.transform(d->ex(i));
+      const double Y = y(i);
+      const double E = d->e(i);
+      const int ei1 = yMap.transform(Y - E);
+      const int ei2 = yMap.transform(Y + E);
+
+      p->setPen(pen());
+      p->drawLine(xi,ei1,xi,ei2);
+      p->drawLine(xi-5,ei1,xi+5,ei1);
+      p->drawLine(xi-5,ei2,xi+5,ei2);
+    }
+  }
+}
+
+void MantidCurve::itemChanged()
+{
+  MantidQwtDataSpectra* d = dynamic_cast<MantidQwtDataSpectra*>(&data());
+  if (d && d->m_isHistogram)
+  {
+    if (style() == Steps) d->m_binCentres = false;
+    else
+      d->m_binCentres = true;
+  }
+  PlotCurve::itemChanged();
+}
+
+/** Create the name for a curve from the following input:
+ *  @param wsName The workspace name
+ *  @param type   The data type ("spectra", "bin",...)
+ *  @param index  The spectra (bin) index
+ */
+QString MantidCurve::createCurveName(const QString& wsName,const QString& type,int index)
+{
+  QString name = wsName + "-";
+  if (type == "spectra")
+    name += "sp-";
+  name += QString::number(index);
+  return name;
+}
+
+//==========================================
+//
+//  MantdQwtData methods
+//
+//==========================================
+MantidQwtData* MantidCurve::mantidData()
+{
+  MantidQwtData* d = dynamic_cast<MantidQwtData*>(&data());
+  return d;
+}
+
+const MantidQwtData* MantidCurve::mantidData()const
+{
+  const MantidQwtData* d = dynamic_cast<const MantidQwtData*>(&data());
+  return d;
+}
+
+bool MantidQwtData::sameWorkspace(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace)const
+{
+  return workspace.get() == m_workspace.get();
+}
+
+/// Constructor
+MantidQwtDataSpectra::MantidQwtDataSpectra(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace,int specIndex)
+:MantidQwtData(workspace),m_spec(specIndex),
+m_X(workspace->readX(specIndex)),
+m_Y(workspace->readY(specIndex)),
+m_E(workspace->readE(specIndex)),
+m_isHistogram(workspace->isHistogramData()),
+m_binCentres(false)
+{}
+
+/// Copy constructor
+MantidQwtDataSpectra::MantidQwtDataSpectra(const MantidQwtDataSpectra& data)
+:MantidQwtData(data),m_spec(data.m_spec),
+m_X(data.m_workspace->readX(data.m_spec)),
+m_Y(data.m_workspace->readY(data.m_spec)),
+m_E(data.m_workspace->readE(data.m_spec)),
+m_isHistogram(m_workspace->isHistogramData()),
+m_binCentres(false)
+{}
+
+/** Size of the data set
+ */
+size_t MantidQwtDataSpectra::size() const
+{
+  return m_Y.size();
+}
+
+/**
+Return the x value of data point i
+\param i Index
+\return x X value of data point i
+*/
+double MantidQwtDataSpectra::x(size_t i) const
+{
+  return m_binCentres ? (m_X[i] + m_X[i+1])/2 : m_X[i];
+}
+
+/**
+Return the y value of data point i
+\param i Index
+\return y Y value of data point i
+*/
+double MantidQwtDataSpectra::y(size_t i) const
+{
+  return m_Y[i];
+}
+
+double MantidQwtDataSpectra::ex(size_t i) const
+{
+  return m_isHistogram ? (m_X[i] + m_X[i+1])/2 : m_X[i];
+}
+
+double MantidQwtDataSpectra::e(size_t i) const
+{
+  return m_E[i];
+}
+
---
+++ Code/qtiplot/qtiplot/src/Mantid/MantidCurve.h
@@ -0,0 +1,191 @@
+#ifndef MANTID_CURVE_H
+#define MANTID_CURVE_H
+
+#include "../PlotCurve.h"
+#include "boost/shared_ptr.hpp"
+
+// Forward definitions
+namespace Mantid
+{
+  typedef std::vector<double> MantidVec;
+  namespace API
+  {
+    class MatrixWorkspace;
+    class Workspace;
+  }
+}
+
+class MantidQwtData;
+class Graph;
+class MantidUI;
+
+/** 
+    This class is for plotting spectra or bins from a Mantid MatrixWorkspace in a 
+    QtiPlot's Graph widget.
+    
+    @author Roman Tolchenov, Tessella plc
+    @date 09/09/2009
+
+    Copyright &copy; 2009 STFC Rutherford Appleton Laboratories
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>    
+*/
+
+class MantidCurve:public PlotCurve
+{
+  Q_OBJECT
+public:
+
+  /// Constructor of "empty" MantidCurve
+  //MantidCurve(const QString& name) :PlotCurve(name)   {}
+
+  /// More complex constructor setting some defaults for the curve
+  MantidCurve(const QString& name,boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace,Graph* g,
+              const QString& type,int index,bool err=false);
+
+  /// More complex constructor setting some defaults for the curve
+  MantidCurve(const QString& wsName,Graph* g,
+              const QString& type,int index,bool err=false);
+
+  /// Copy constructor 
+  MantidCurve(const MantidCurve& c);
+
+  ~MantidCurve();
+
+  PlotCurve* clone()const{return new MantidCurve(*this);}
+
+  /// Curve type. Used in the QtiPlot API.
+  int rtti() const{return Rtti_PlotUserItem;}
+
+  /// Overrides qwt_plot_curve::setData to make sure only data of MantidQwtData type can  be set
+  void setData(const QwtData &data);
+
+  /// Return pointer to the data if it of the right type or 0 otherwise
+  MantidQwtData* mantidData();
+  /// Return pointer to the data if it of the right type or 0 otherwise, const version
+  const MantidQwtData* mantidData()const;
+
+  /// Enables/disables drawing of error bars
+  void setErrorBars(bool yes=true){m_drawErrorBars = yes;}
+
+  virtual void draw(QPainter *p, 
+    const QwtScaleMap &xMap, const QwtScaleMap &yMap,
+    const QRect &) const;
+
+  /// Overriden virtual method
+  void itemChanged();
+
+public slots:
+
+  /// Respond to workspace removal.
+  void workspaceRemoved(const QString&);
+  /// Respont to workspace replacement
+  void workspaceReplaced(const QString &, boost::shared_ptr<const Mantid::API::Workspace>);
+
+private:
+  /// Init the curve
+  void init(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace,Graph* g,
+              const QString& type,int index);
+  /// Make the curve name
+  static QString createCurveName(const QString& wsName,const QString& type,int index);
+  bool m_drawErrorBars;///< True for drawing error bars
+  QString m_wsName;///< Workspace name. If empty the ws isn't in the data service
+};
+
+/**  This class implements QwtData with direct access to a MatrixWorkspace.
+ */
+class MantidQwtData: public QwtData
+{
+public:
+  /// Constructor
+  MantidQwtData(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace)
+    :m_workspace(workspace){}
+
+  /// Copy constructor
+  MantidQwtData(const MantidQwtData& data)
+    :m_workspace(data.m_workspace){}
+
+  bool sameWorkspace(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace)const;
+
+  /// Returns the error of the i-th data point
+  virtual double e(size_t i)const = 0;
+  /// Returns the x position of the error bar for the i-th data point (bin)
+  virtual double ex(size_t i)const = 0;
+protected:
+  /// Pointer to the Mantid workspace
+  boost::shared_ptr<const Mantid::API::MatrixWorkspace> m_workspace;
+};
+
+/**  This class implements QwtData with direct access to a spectrum in a MatrixWorkspace.
+ */
+class MantidQwtDataSpectra: public MantidQwtData
+{
+public:
+  /// Constructor
+  MantidQwtDataSpectra(boost::shared_ptr<const Mantid::API::MatrixWorkspace> workspace,int specIndex);
+
+  /// Copy constructor
+  MantidQwtDataSpectra(const MantidQwtDataSpectra& data);
+
+    //! \return Pointer to a copy (virtual copy constructor)
+  virtual QwtData *copy() const {return new MantidQwtDataSpectra(*this);}
+
+  //! \return Size of the data set
+  virtual size_t size() const;
+
+  /*!
+  Return the x value of data point i
+  \param i Index
+  \return x X value of data point i
+  */
+  virtual double x(size_t i) const;
+  /*!
+  Return the y value of data point i
+  \param i Index
+  \return y Y value of data point i
+  */
+  virtual double y(size_t i) const;
+
+  /// Returns the error of the i-th data point
+  double e(size_t i)const;
+  /// Returns the x position of the error bar for the i-th data point (bin)
+  double ex(size_t i)const;
+
+private:
+
+  friend class MantidCurve;
+
+  /// Spectrum index in the workspace
+  int m_spec;
+  /// Reference to the X vector
+  const Mantid::MantidVec& m_X;
+  /// Reference to the Y vector
+  const Mantid::MantidVec& m_Y;
+  /// Reference to the E vector
+  const Mantid::MantidVec& m_E;
+  /// Is the spectrum a histogram?
+  bool m_isHistogram;
+  /// This field can be set true for a histogram worspace. If it's true x(i) returns (X[i]+X[i+1])/2
+  bool m_binCentres;
+
+};
+
+
+#endif // MANTID_CURVE_H
