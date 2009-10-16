965863aa10fd1557e3bba99ef79acb0ba3b4cc86
Nick Draper <nick.draper@stfc.ac.uk>
2009-10-16 14:04:45 +0000

    re #981
    Added PowerLawCorrection written by Rob Dalgliesh

---
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/PowerLawCorrection.h
@@ -0,0 +1,75 @@
+#ifndef MANTID_ALGORITHMS_POWERLAWCORRECTION_H_
+#define MANTID_ALGORITHMS_POWERLAWCORRECTION_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/UnaryOperation.h"
+
+namespace Mantid
+{
+  namespace Algorithms
+  {
+    /** 
+    Corrects the data and error values on a workspace by the value of an exponential function
+    which is evaluated at the X value of each data point: c0*x^C1. 
+    The data and error values are either divided or multiplied by the value of this function.
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace  - The name of the workspace to correct</LI>
+    <LI> OutputWorkspace - The name of the corrected workspace (can be the same as the input one)</LI>
+    <LI> c0              - The value by which the entire calculation is multiplied (see above)</LI>
+    <LI> c1              - The power by which the x value is raised (see above)</LI>
+    <LI> Operation       - Whether to divide (the default) or multiply the data by the correction function</LI>
+    </UL>
+
+    @author Robert Dalgliesh ISIS, STFC
+    @date 14/10/2009
+
+    Copyright &copy; 2009 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>    
+    */
+    class DLLExport PowerLawCorrection : public UnaryOperation
+    {
+    public:
+      /// Default constructor
+      PowerLawCorrection() : UnaryOperation() {};
+      /// Destructor
+      virtual ~PowerLawCorrection() {};
+      /// Algorithm's name for identification
+      virtual const std::string name() const { return "PowerLawCorrection";}
+      /// Algorithm's version for identification
+      virtual const int version() const { return 1;}
+
+    private:
+      // Overridden UnaryOperation methods
+      void defineProperties();
+      void retrieveProperties();
+      void performUnaryOperation(const double& XIn, const double& YIn, const double& EIn, double& YOut, double& EOut);
+
+      double m_c0;   ///< The constant by which to multiply 
+      double m_c1;   ///< The power to raise by.
+      
+    };
+
+  } // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_POWERLAWCORRECTION_H_*/
---
+++ Code/Mantid/Algorithms/test/PowerLawCorrectionTest.h
@@ -0,0 +1,97 @@
+#ifndef POWERLAWCORRECTIONTEST_H_
+#define POWERLAWCORRECTIONTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "WorkspaceCreationHelper.hh"
+#include "MantidAlgorithms/PowerLawCorrection.h"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidKernel/ArrayProperty.h"
+
+using namespace Mantid::API;
+using namespace Mantid::Kernel;
+
+class PowerLawCorrectionTest : public CxxTest::TestSuite
+{
+public:
+  void testName()
+  {
+    TS_ASSERT_EQUALS( expon.name(), "PowerLawCorrection" )
+  }
+  
+  void testVersion()
+  {
+    TS_ASSERT_EQUALS( expon.version(), 1 )
+  }
+  
+  void testCategory()
+  {
+    TS_ASSERT_EQUALS( expon.category(), "CorrectionFunctions" )
+  }
+  
+  void testInit()
+  {
+    Mantid::Algorithms::PowerLawCorrection expon2;
+    TS_ASSERT_THROWS_NOTHING( expon2.initialize() )
+    TS_ASSERT( expon2.isInitialized() )
+
+    const std::vector<Property*> props = expon2.getProperties();
+    TS_ASSERT_EQUALS( props.size(), 4 )
+
+    TS_ASSERT_EQUALS( props[0]->name(), "InputWorkspace" )
+    TS_ASSERT( props[0]->isDefault() )
+    TS_ASSERT( dynamic_cast<WorkspaceProperty<MatrixWorkspace>* >(props[0]) )
+
+    TS_ASSERT_EQUALS( props[1]->name(), "OutputWorkspace" )
+    TS_ASSERT( props[1]->isDefault() )
+    TS_ASSERT( dynamic_cast<WorkspaceProperty<MatrixWorkspace>* >(props[1]) )
+
+    TS_ASSERT_EQUALS( props[2]->name(), "C0")
+    TS_ASSERT( props[2]->isDefault() )
+    TS_ASSERT( dynamic_cast<PropertyWithValue<double>* >(props[2]) )
+
+    TS_ASSERT_EQUALS( props[3]->name(), "C1")
+    TS_ASSERT( props[3]->isDefault() )
+    TS_ASSERT( dynamic_cast<PropertyWithValue<double>* >(props[3]) )
+  }
+  
+  void testMultiply()
+  {
+    MatrixWorkspace_sptr inputWS = WorkspaceCreationHelper::Create2DWorkspaceBinned(2,3,0.5);
+    AnalysisDataService::Instance().add("PowerLawCorrectionInputWS",inputWS);
+    
+    Mantid::Algorithms::PowerLawCorrection expon3;
+    expon3.initialize();
+    TS_ASSERT_THROWS_NOTHING( expon3.setPropertyValue("InputWorkspace","PowerLawCorrectionInputWS") )
+    TS_ASSERT_THROWS_NOTHING( expon3.setPropertyValue("OutputWorkspace","PowerLawCorrectionWSCor") )
+    TS_ASSERT_THROWS_NOTHING( expon3.setPropertyValue("c0","3.0") )
+    TS_ASSERT_THROWS_NOTHING( expon3.setPropertyValue("c1","2.0") )
+    
+    TS_ASSERT_THROWS_NOTHING( expon3.execute() )
+    TS_ASSERT( expon3.isExecuted() )
+    
+    MatrixWorkspace_sptr result;
+    TS_ASSERT_THROWS_NOTHING( result = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve("PowerLawCorrectionWSCor")) )
+    TS_ASSERT( result )
+    
+    for (int i = 0; i < result->getNumberHistograms(); ++i)
+    {
+      for (int j = 0; j < 3; ++j)
+      {
+        double factor = 3.0*pow(j+1,2.0);
+        TS_ASSERT_EQUALS( result->dataX(i)[j], inputWS->dataX(i)[j] )
+        TS_ASSERT_DELTA( result->dataY(i)[j], inputWS->dataY(i)[j]*factor, 0.0001 )
+        TS_ASSERT_DELTA( result->dataE(i)[j], inputWS->dataE(i)[j]*factor, 0.0001 )
+      }
+    }
+    
+    AnalysisDataService::Instance().remove("PowerLawCorrectionInputWS");
+    AnalysisDataService::Instance().remove("PowerLawCorrectionWSCor");
+  }
+  
+private:
+  Mantid::Algorithms::PowerLawCorrection expon;
+
+};
+
+#endif /*POWERLAWCORRECTIONTEST_H_*/
