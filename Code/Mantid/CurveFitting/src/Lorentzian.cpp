b141b49ea767159e46290a0d466ae09d86b279c9
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2009-10-27 10:15:09 +0000

    Add Lorentzian peak shape function. Refs #983

---
+++ Code/Mantid/CurveFitting/src/Lorentzian.cpp
@@ -0,0 +1,55 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidCurveFitting/Lorentzian.h"
+#include <cmath>
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+
+using namespace Kernel;
+using namespace API;
+
+void Lorentzian::init()
+{
+  declareParameter("Height", 0.0);
+  declareParameter("PeakCentre", 0.0);
+  declareParameter("HWHM", 1.0);
+}
+
+
+void Lorentzian::function(double* out, const double* xValues, const int& nData)
+{
+    const double& height = getParameter("Height");
+    const double& peakCentre = getParameter("PeakCentre");
+    const double& hwhm = getParameter("HWHM");
+
+    for (int i = 0; i < nData; i++) {
+        double diff=xValues[i]-peakCentre;
+        out[i] = height*( hwhm*hwhm/(diff*diff+hwhm*hwhm) );
+    }
+
+}
+
+void Lorentzian::functionDeriv(Jacobian* out, const double* xValues, const int& nData)
+{
+    const double& height = getParameter("Height");
+    const double& peakCentre = getParameter("PeakCentre");
+    const double& hwhm = getParameter("HWHM");
+
+
+    for (int i = 0; i < nData; i++) {
+        double diff = xValues[i]-peakCentre;
+        double invDenominator =  1/((diff*diff+hwhm*hwhm));
+        out->set(i,0, hwhm*hwhm*invDenominator);
+        out->set(i,1, 2.0*height*diff*hwhm*hwhm*invDenominator*invDenominator);
+        out->set(i,2, height*(-hwhm*hwhm*invDenominator+1)*2.0*hwhm*invDenominator);
+    }
+
+}
+
+
+} // namespace CurveFitting
+} // namespace Mantid
