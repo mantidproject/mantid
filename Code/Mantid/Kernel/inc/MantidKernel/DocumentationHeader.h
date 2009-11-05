d39c007d7f1f50e7fb397b61abbf7de8fee9c6d9
Nick Draper <nick.draper@stfc.ac.uk>
2009-11-05 16:09:20 +0000

    re #100 Add Doxygen Main page

---
+++ Code/Mantid/Kernel/inc/MantidKernel/DocumentationHeader.h
@@ -0,0 +1,38 @@
+#ifndef MANTID_KERNEL_DOCUMENTATIONHEADER_H_
+#define MANTID_KERNEL_DOCUMENTATIONHEADER_H_
+
+// The following stuff is for the doxygen title page
+/*!  \mainpage Mantid - Manipulation and Analysis Toolkit for ISIS Data - API documentation
+  \image html "http://svn.mantidproject.org/mantid/trunk/Images/Mantid Logo.png"
+  \section description Program description:
+The Mantid application framework will provide a platform to supports high-performance computing on neutron data. The framework will provide a set of common services, algorithms and data objects that can be extended further by specialised applications or directly by users if required. The main aims of the project are as follows 
+<ul>
+<li>To provide a framework for Data Analysis that is not instrument or technique/dependent. </li>
+<li>To support multiple target platforms (Windows, Linux, mac OSX).  </li>
+<li>The framework must be easily extensible by Instruments Scientists/Users.  </li>
+<li>The framework must be freely redistributable to visiting scientists.  </li>
+<li>The framework should provide low-level functionalities for Scripting, Visualization, Data transformation, Implementing Algorithms, Virtual Instrument Geometry.  </li>
+<li>This will be bound to Data reduction and analysis, not Instrument Control.  </li>
+</ul>
+<br>
+
+ The Mantid web page can be found at<br>
+ <a href="http://www.mantidproject.org">http://www.mantidproject.org</a><br>
+
+  \section libs Mantid uses the following libraries:
+  <a href="http://www.boost.org">Boost</a>,
+  <a href="http://pocoproject.org/">POCO</a>,
+  <a href="http://www.gnu.org/software/gsl/">GSL</a>,
+  <a href="http://www.opencascade.org/">OpenCascade</a>,
+  <a href="http://muparser.sourceforge.net/">muParser</a>,
+  <a href="http://www.zlib.net/">zlib (1.2.3)</a>,
+  and <a href="http://www.nexusformat.org/Main_Page">libNexus</a>.
+  <br>
+
+  \page style Coding Style
+  
+  Refer to the conding standards page on the project wiki <a href="http://www.mantidproject.org/Coding_Standards">here</a>.
+*/
+
+
+#endif /*MANTID_KERNEL_DOCUMENTATIONHEADER_H_*/
