8d69346f48ce0f60ba22d4d72cde92f3ec7fd2a3
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2009-12-02 13:39:33 +0000

    Fixing FitTest. re #961

--- Code/Mantid/CurveFitting/test/FitTest.h
+++ Code/Mantid/CurveFitting/test/FitTest.h
@@ -24,6 +24,112 @@ public:
   {return 1+0.3*x+exp(-0.5*(x-4)*(x-4)*2)+2*exp(-0.5*(x-6)*(x-6)*3);}
 };
 
+class FitTest_Gauss: public IPeakFunction
+{
+public:
+  FitTest_Gauss()
+  {
+    declareParameter("c");
+    declareParameter("h",1.);
+    declareParameter("s",1.);
+  }
+
+  std::string name()const{return "Gauss";}
+
+  void function(double* out, const double* xValues, const int& nData)
+  {
+    double c = getParameter("c");
+    double h = getParameter("h");
+    double w = getParameter("s");
+    for(int i=0;i<nData;i++)
+    {
+      double x = xValues[i] - c;
+      out[i] = h*exp(-0.5*x*x*w);
+    }
+  }
+  void functionDeriv(Jacobian* out, const double* xValues, const int& nData)
+  {
+    //throw Mantid::Kernel::Exception::NotImplementedError("");
+    double c = getParameter("c");
+    double h = getParameter("h");
+    double w = getParameter("s");
+    for(int i=0;i<nData;i++)
+    {
+      double x = xValues[i] - c;
+      double e = h*exp(-0.5*x*x*w);
+      out->set(i,0,x*h*e*w);
+      out->set(i,1,e);
+      out->set(i,2,-0.5*x*x*h*e);
+    }
+  }
+
+  double centre()const
+  {
+    return parameter(0);
+  }
+
+  double height()const
+  {
+    return parameter(1);
+  }
+
+  double width()const
+  {
+    return parameter(2);
+  }
+
+  void setCentre(const double c)
+  {
+    parameter(0) = c;
+  }
+  void setHeight(const double h)
+  {
+    parameter(1) = h;
+  }
+
+  void setWidth(const double w)
+  {
+    parameter(2) = w;
+  }
+
+};
+
+
+class FitTest_Linear: public Function
+{
+public:
+  FitTest_Linear()
+  {
+    declareParameter("a");
+    declareParameter("b");
+  }
+
+  std::string name()const{return "Linear";}
+
+  void function(double* out, const double* xValues, const int& nData)
+  {
+    double a = getParameter("a");
+    double b = getParameter("b");
+    for(int i=0;i<nData;i++)
+    {
+      out[i] = a + b * xValues[i];
+    }
+  }
+  void functionDeriv(Jacobian* out, const double* xValues, const int& nData)
+  {
+    //throw Mantid::Kernel::Exception::NotImplementedError("");
+    for(int i=0;i<nData;i++)
+    {
+      out->set(i,0,1.);
+      out->set(i,1,xValues[i]);
+    }
+  }
+
+};
+
+DECLARE_FUNCTION(FitTest_Gauss);
+DECLARE_FUNCTION(FitTest_Linear);
+
 class FitTest : public CxxTest::TestSuite
 {
 public:
@@ -45,12 +151,11 @@ public:
     alg.setPropertyValue("WorkspaceIndex","0");
     alg.setPropertyValue("Output","out");
     std::string params = "";
-    params += "name=LinearBackground,A0=1,A1=0;";
-    params += "name=Gaussian, PeakCentre=4.1,Height=1.1,Sigma=0.5;";
-    params += "name=Gaussian, PeakCentre=6.1,Height=3.1,Sigma=0.5;";
+    params += "name=FitTest_Linear,a=1,b=0;";
+    params += "name=FitTest_Gauss, c=4.1,h=1.1,s=0.5;";
+    params += "name=FitTest_Gauss, c=6.1,h=3.1,s=0.5;";
 
     alg.setPropertyValue("Function",params);
-    //alg.setPropertyValue("Ties","f1.Sigma=f2.Sigma/3");
 
     TS_ASSERT_THROWS_NOTHING(alg.execute());
     TS_ASSERT(alg.isExecuted());
@@ -75,36 +180,36 @@ public:
     TS_ASSERT_EQUALS(outParams->columnCount(),2);
 
     TableRow row = outParams->getFirstRow();
-    TS_ASSERT_EQUALS(row.String(0),"f0.A0");
+    TS_ASSERT_EQUALS(row.String(0),"f0.a");
     TS_ASSERT_DELTA(row.Double(1),1,0.00001);
 
     row = outParams->getRow(1);
-    TS_ASSERT_EQUALS(row.String(0),"f0.A1");
+    TS_ASSERT_EQUALS(row.String(0),"f0.b");
     TS_ASSERT_DELTA(row.Double(1),0.3,0.00001);
 
     row = outParams->getRow(2);
-    TS_ASSERT_EQUALS(row.String(0),"f1.Height");
-    TS_ASSERT_DELTA(row.Double(1),1,0.00001);
-
-    row = outParams->getRow(3);
-    TS_ASSERT_EQUALS(row.String(0),"f1.PeakCentre");
+    TS_ASSERT_EQUALS(row.String(0),"f1.c");
     TS_ASSERT_DELTA(row.Double(1),4,0.00001);
 
-    row = outParams->getRow(4);
-    TS_ASSERT_EQUALS(row.String(0),"f1.Sigma");
-    TS_ASSERT_DELTA(row.Double(1),0.7071,0.00001);
+    row = outParams->getRow(3);
+    TS_ASSERT_EQUALS(row.String(0),"f1.h");
+    TS_ASSERT_DELTA(row.Double(1),1,0.00001);
 
-    row = outParams->getRow(5);
-    TS_ASSERT_EQUALS(row.String(0),"f2.Height");
+    row = outParams->getRow(4);
+    TS_ASSERT_EQUALS(row.String(0),"f1.s");
     TS_ASSERT_DELTA(row.Double(1),2,0.00001);
 
-    row = outParams->getRow(6);
-    TS_ASSERT_EQUALS(row.String(0),"f2.PeakCentre");
+    row = outParams->getRow(5);
+    TS_ASSERT_EQUALS(row.String(0),"f2.c");
     TS_ASSERT_DELTA(row.Double(1),6,0.00001);
 
+    row = outParams->getRow(6);
+    TS_ASSERT_EQUALS(row.String(0),"f2.h");
+    TS_ASSERT_DELTA(row.Double(1),2,0.0001);
+
     row = outParams->getRow(7);
-    TS_ASSERT_EQUALS(row.String(0),"f2.Sigma");
-    TS_ASSERT_DELTA(row.Double(1),0.57735,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f2.s");
+    TS_ASSERT_DELTA(row.Double(1),3,0.0005);
 
     removeWS("Exp");
     removeWS("out_Workspace");
@@ -124,12 +229,12 @@ public:
     alg.setPropertyValue("WorkspaceIndex","0");
     alg.setPropertyValue("Output","out");
     std::string params = "";
-    params += "name=LinearBackground,A0=1,A1=0;";
-    params += "name=Gaussian, PeakCentre=4.1,Height=1.1,Sigma=0.5;";
-    params += "name=Gaussian, PeakCentre=6.1,Height=3.1,Sigma=0.5;";
+    params += "name=FitTest_Linear,a=1,b=0;";
+    params += "name=FitTest_Gauss, c=4.1,h=1.1,s=2.2;";
+    params += "name=FitTest_Gauss, c=6.1,h=3.1,s=3.3;";
 
     alg.setPropertyValue("Function",params);
-    alg.setPropertyValue("Ties","f1.Sigma=f2.Sigma/3");
+    alg.setPropertyValue("Ties","f1.s=f2.s/3");
 
     TS_ASSERT_THROWS_NOTHING(alg.execute());
     TS_ASSERT(alg.isExecuted());
@@ -143,36 +248,40 @@ public:
     TS_ASSERT_EQUALS(outParams->columnCount(),2);
 
     TableRow row = outParams->getFirstRow();
-    TS_ASSERT_EQUALS(row.String(0),"f0.A0");
-    TS_ASSERT_DELTA(row.Double(1),1.11869,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f0.a");
+    TS_ASSERT_DELTA(row.Double(1),0.9677,0.0001);
 
     row = outParams->getRow(1);
-    TS_ASSERT_EQUALS(row.String(0),"f0.A1");
-    TS_ASSERT_DELTA(row.Double(1),0.287874,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f0.b");
+    TS_ASSERT_DELTA(row.Double(1),0.3036,0.0001);
 
     row = outParams->getRow(2);
-    TS_ASSERT_EQUALS(row.String(0),"f1.Height");
-    TS_ASSERT_DELTA(row.Double(1),1.1314,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f1.c");
+    TS_ASSERT_DELTA(row.Double(1),4.1274,0.0001);
 
     row = outParams->getRow(3);
-    TS_ASSERT_EQUALS(row.String(0),"f1.PeakCentre");
-    TS_ASSERT_DELTA(row.Double(1),3.88745,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f1.h");
+    TS_ASSERT_DELTA(row.Double(1),0.9456,0.0001);
 
     row = outParams->getRow(4);
-    TS_ASSERT_EQUALS(row.String(0),"f1.Sigma");
-    TS_ASSERT_DELTA(row.Double(1),0.247012,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f1.s");
+    double s1 = row.Double(1);
+    TS_ASSERT_DELTA(row.Double(1),1.1476,0.0001);
 
     row = outParams->getRow(5);
-    TS_ASSERT_EQUALS(row.String(0),"f2.Height");
-    TS_ASSERT_DELTA(row.Double(1),1.81892,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f2.c");
+    TS_ASSERT_DELTA(row.Double(1),6.0547,0.0001);
 
     row = outParams->getRow(6);
-    TS_ASSERT_EQUALS(row.String(0),"f2.PeakCentre");
-    TS_ASSERT_DELTA(row.Double(1),5.87412,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f2.h");
+    TS_ASSERT_DELTA(row.Double(1),1.9206,0.0001);
 
     row = outParams->getRow(7);
-    TS_ASSERT_EQUALS(row.String(0),"f2.Sigma");
-    TS_ASSERT_DELTA(row.Double(1),0.741036,0.00001);
+    TS_ASSERT_EQUALS(row.String(0),"f2.s");
+    TS_ASSERT_DELTA(row.Double(1),3.443,0.0001);
+    double s2 = row.Double(1);
+
+    TS_ASSERT_DELTA(s1,s2/3,1e-12);
 
     removeWS("Exp");
     removeWS("out_Workspace");
