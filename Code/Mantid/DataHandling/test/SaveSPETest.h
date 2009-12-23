10fdf41113ad541f0f5ecbdeac469425215670f6
Steve Williams <stephen.williams@stfc.ac.uk>
2009-12-23 12:31:54 +0000

    The ?printf functions now print floating point numbers with two digits in their exponent throughout Mantid, in both Windows and Linux. .SPE files created by Mantid can now be read my the Excitations Group Matlab application HOMER refs #1071

--- Code/Mantid/DataHandling/test/SaveSPETest.h
+++ Code/Mantid/DataHandling/test/SaveSPETest.h
@@ -6,50 +6,57 @@
 #include "MantidDataHandling/SaveSPE.h"
 #include "MantidKernel/UnitFactory.h"
 #include "../../Algorithms/test/WorkspaceCreationHelper.hh"
+#include "MantidAPI/FrameworkManager.h"
 #include "Poco/File.h"
 #include <fstream>
+
+using namespace Mantid::API;
 
 class SaveSPETest : public CxxTest::TestSuite
 {
 public:  
+  SaveSPETest()
+  {// the functioning of SaveSPE is affected by a function call in the FrameworkManager's constructor, creating the algorithm in this way ensures that function is executed
+    saver = FrameworkManager::Instance().createAlgorithm("SaveSPE");
+  }
+
   void testName()
   {
-    TS_ASSERT_EQUALS( saver.name(), "SaveSPE" )
+    TS_ASSERT_EQUALS( saver->name(), "SaveSPE" )
   }
 
   void testVersion()
   {
-    TS_ASSERT_EQUALS( saver.version(), 1 )
+    TS_ASSERT_EQUALS( saver->version(), 1 )
   }
 
   void testCategory()
   {
-    TS_ASSERT_EQUALS( saver.category(), "DataHandling" )
+    TS_ASSERT_EQUALS( saver->category(), "DataHandling" )
   }
 
   void testInit()
   {
-    TS_ASSERT_THROWS_NOTHING( saver.initialize() )
-    TS_ASSERT( saver.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( saver->initialize() )
+    TS_ASSERT( saver->isInitialized() )
     
-    TS_ASSERT_EQUALS( saver.getProperties().size(), 2 )
+    TS_ASSERT_EQUALS( saver->getProperties().size(), 2 )
   }
 
   void testExec()
   {
-    using namespace Mantid::API;
     // Create a small test workspace
     MatrixWorkspace_sptr inputWS = WorkspaceCreationHelper::Create2DWorkspaceBinned(2,10,1.0);
     inputWS->getAxis(0)->unit() = Mantid::Kernel::UnitFactory::Instance().create("DeltaE");
     const std::string input("input");
     AnalysisDataService::Instance().add(input,inputWS);
     
-    TS_ASSERT_THROWS_NOTHING( saver.setPropertyValue("InputWorkspace",input) )
+    TS_ASSERT_THROWS_NOTHING( saver->setPropertyValue("InputWorkspace",input) )
     const std::string outputFile("testSPE.spe");
-    TS_ASSERT_THROWS_NOTHING( saver.setPropertyValue("Filename",outputFile) )
-    
-    TS_ASSERT_THROWS_NOTHING( saver.execute() )
-    TS_ASSERT( saver.isExecuted() )
+    TS_ASSERT_THROWS_NOTHING( saver->setPropertyValue("Filename",outputFile) )
+
+    TS_ASSERT_THROWS_NOTHING( saver->execute() )
+    TS_ASSERT( saver->isExecuted() )
     
     TS_ASSERT( Poco::File(outputFile).exists() )
     std::ifstream file(outputFile.c_str());
@@ -116,7 +123,7 @@ public:
   }
   
 private:
-  Mantid::DataHandling::SaveSPE saver;
+  IAlgorithm* saver;
 };
 
 #endif /*SAVESPETEST_H_*/
