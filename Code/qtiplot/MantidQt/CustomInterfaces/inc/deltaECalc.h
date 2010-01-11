fc02272459d2016121fe5527d95ca06e85833ce6
Steve Williams <stephen.williams@stfc.ac.uk>
2010-01-11 09:56:14 +0000

    Adding the convert unit units calculation files forgotten from in the last check in refs #1071

---
+++ Code/qtiplot/MantidQt/CustomInterfaces/inc/deltaECalc.h
@@ -0,0 +1,58 @@
+#include "MantidQtMantidWidgets/pythonCalc.h"
+#include "MantidQtCustomInterfaces/ui_Excitations.h"
+#include <vector>
+#include <QString>
+#include <string>
+
+namespace MantidQt
+{
+  namespace CustomInterfaces
+  {
+    using namespace MantidWidgets;
+	
+    class deltaECalc : public pythonCalc
+    {
+    public:
+      //??STEVES? move this into the file widget
+      class FileInput
+      {
+      private:
+        QLineEdit &m_lineEdit;
+	    QComboBox &m_box;
+        std::vector<std::string> m_files;
+      public:
+        FileInput(QLineEdit &num, QComboBox &instr);
+        const std::vector<std::string>& getRunFiles();
+        void readComasAndHyphens(const std::string &in, std::vector<std::string> &out);
+      };
+  
+      deltaECalc(const Ui::Excitations &userSettings, FileInput &runFiles);
+	  void maskDetects(QString &maskWS);
+	  /** removes the path from the filename passed and replaces extensions with .spe
+      * @param inputFilename name of the file that the .SPE file is based on
+      */
+      static QString SPEFileName(const std::string &inputFilename)
+      {
+        std::string root = Poco::Path(inputFilename).getBaseName();
+        return root.empty() ? "" : QString::fromStdString(root)+".spe";
+      }
+      std::string insertNumber(const std::string &filename, const int number) const;
+    private:
+      /// the form that ws filled in by the user
+      const Ui::Excitations &m_sets;
+
+	  QString createProcessingScript(const std::string &inFiles, const std::string &oName);
+	  void createGetEIStatmens(QString &newScr);
+	  void createNormalizationStatmens(QString &newScr);
+      void createOutputStatmens(QString &WSName, QString &newScr);
+      QString getScaling() const;
+      QString getNormalization() const;
+      void LEChkCpIn(QString &text, QString pythonMark, QLineEdit * userVal, Property * const check);
+	  std::string replaceInErrsFind(QString &text, QString pythonMark, const QString &setting, Property * const check) const;
+      void renameWorkspace(const QString &name);
+  
+      // holds the prefix that we give to output workspaces that will be deleted in the Python
+      static const QString tempWS;
+    };
+  }
+}
---
+++ Code/qtiplot/MantidQt/CustomInterfaces/src/deltaECalc.cpp
@@ -0,0 +1,379 @@
+#include "MantidQtCustomInterfaces/deltaECalc.h"
+#include "MantidKernel/ConfigService.h"
+#include "MantidAPI/IAlgorithm.h"
+#include "MantidAPI/AlgorithmManager.h"
+#include "MantidKernel/FileProperty.h"
+#include <QDir>
+#include "boost/lexical_cast.hpp"
+#include "Poco/StringTokenizer.h"
+
+using namespace MantidQt::CustomInterfaces;
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+const QString deltaECalc::tempWS = "_ConvertToETrans_tempory_workspace_";
+
+/** Read the data the user supplied to create Python code to do their calculation
+* @param userSettings the form that the user filled in
+* @throw invalid_argument where problems with user data prevent the calculation from proceeding
+*/
+deltaECalc::deltaECalc(const Ui::Excitations &userSettings, deltaECalc::FileInput &runFiles) :
+  m_sets(userSettings), pythonCalc()
+{
+  QDir scriptsdir(QString::fromStdString(ConfigService::Instance().getString("pythonscripts.directory")));
+  // load a template for the Python script that we will contain in a string
+  QString pythonFileName =
+    scriptsdir.absoluteFilePath("Excitations/DetectorEfficiency/ConvertToETrans.py");
+  readFile(pythonFileName);
+  
+  // the run file selection has a profound effect on the script, we might once on a single file, ilterate the process over many files or sum the list of files and run just once
+  const std::vector<std::string> &inputFiles = runFiles.getRunFiles();
+  std::vector<std::string>::const_iterator inFile = inputFiles.begin();
+  
+  // ckSumSpec->isChecked() == true means sum all the files
+  if (m_sets.ckSumSpecs->isChecked() || inputFiles.size() == 1)
+  {// this is the easy case; the analysis is done just once on the sum of all the input spaces
+	std::string fileList;
+	for( ; inFile != inputFiles.end(); ++inFile)
+	{// there will be a spare ',' at the end of the list but Python accepts this without error and requires it if there is only one member in the list
+	  fileList += "'"+(*inFile)+"',";
+	}
+	QString WS = createProcessingScript(fileList,
+	                              m_sets.leNameSPE->text().toStdString());
+	renameWorkspace(WS);
+  }
+  //no summing, the analysis is done once for _each_ input file, createProcessingScript() is run many times to make one long (possibly very long) script
+  else if( m_sets.leNameSPE->text().isEmpty())
+  {// here the names of the output files are based on the names of the input files, these names are likely to be unique but two files can have the same name if there are in different directories. Create a set to check on duplication
+	std::map<std::string, int> oldNames;
+	for(std::vector<std::string>::const_iterator end = inputFiles.end(); ; )
+	{
+	  std::string saveName = SPEFileName(*inFile).toStdString();
+	  if ( oldNames.find(saveName) == oldNames.end() )
+	  {// this is the first time this name has been used, record the name and say that it hasn't been used before
+	    oldNames[saveName] = 1;
+	  }
+	  else
+	  {// duplicate names!
+	    saveName = insertNumber(saveName, oldNames[saveName]++);
+	  }
+      QString WS = createProcessingScript("'"+(*inFile)+"',", saveName);
+	  // the last workspace will retained for the user to examine
+	  if (end == ++inFile)
+	  {// rename it to something they'll recognise
+	    renameWorkspace(WS);
+	    break;
+	  }
+	  // all the other workspaces will be deleted
+	  m_pyScript.append("mantid.deleteWorkspace(");
+	  m_pyScript.append("'"+tempWS+WS+"')\n");
+	}
+  }
+  else
+  {// we have a base name to use, distingish the multiple output files using numbers
+	for( int i = 0, end = inputFiles.size(); ; )
+	{
+	  // the coma at the end is required that so the Python interpretor will see the string as a list of strings, a list with one member
+	  QString WS = createProcessingScript("'"+inputFiles[i]+"',", 
+	    insertNumber(m_sets.leNameSPE->text().toStdString(), i+1));
+
+	  // the last workspace will retained for the user to examine
+	  if (end == ++i)
+	  {// rename it to something they'll recognise
+	    renameWorkspace(WS);
+	    break;
+	  }
+	  m_pyScript.append("mantid.deleteWorkspace(");
+	  m_pyScript.append("'"+tempWS+WS+"')\n");
+	}
+  }
+}
+/** reads leScale from the form to calculate the scaling factor
+* @throw invalid_argument if the leScale text can't be converted to an integer
+*/
+QString deltaECalc::getScaling() const
+{
+  bool status = false;
+  QString exponetial;
+  int power = m_sets.leScale->text().toInt(&status);
+  exponetial.setNum(pow(10.0, power));
+  if ( ! status ) throw std::invalid_argument("leScale");
+  return exponetial;
+}
+/** reads cbNormal and returns the user setting, unless it is set to monitor
+* in which case it returns the text in cbMonitors
+* @throw invalid_argument if normalise to monitor is set but no monitor is selected 
+*/
+QString deltaECalc::getNormalization() const
+{
+  if ( m_sets.cbNormal->currentText() != "monitor" )
+  {
+    return m_sets.cbNormal->currentText();
+  }
+  if (m_sets.cbMonitors->currentText().isEmpty()) std::invalid_argument("cbMonitors");
+  return m_sets.cbMonitors->currentText();
+}
+/** Use the detector masking present in the workspace whose name was passed in
+*  the input workspace/s
+*  @param maskWS name of the workspace whose detector masking will be copied
+*/
+void deltaECalc::maskDetects(QString &maskWS)
+{
+  m_pyScript.replace("''#+|MASK_WORKSPACE|", "'"+maskWS+"'");
+}
+/** Insert the number before the dot of the extension
+* @param filename the a full path or partial path or just the name of a file
+* @param number the number to insert
+*/
+std::string deltaECalc::insertNumber(const std::string &filename, const int number) const
+{
+  // we're going to break up the file name to insert a number into it
+  Poco::Path f(filename);
+  // check if the path is given in the filename
+  if ( f.depth() > 0 )
+  {// get the directory name, the full path of the file minus its name and add it back to the result so that we don't lose the path
+    return f.directory(f.depth()-1) + f.getBaseName() +
+	  boost::lexical_cast<std::string>(number) + f.getExtension();
+  }
+  return f.getBaseName() + boost::lexical_cast<std::string>(number) +
+    f.getExtension();
+}
+  
+QString deltaECalc::createProcessingScript(const std::string &inFiles, const std::string &oName)
+{	
+  // we make a copy of code we read from the file because we might replace some terms and we might need to repeat this operation
+  QString newScr = m_templateB;
+  // validate the user's values using the validator from the algorithm that will be run
+  IAlgorithm_sptr rebin = AlgorithmManager::Instance().createUnmanaged("Rebin");
+  rebin->initialize();
+  IAlgorithm_sptr group = AlgorithmManager::Instance().createUnmanaged("GroupDetectors");
+  group->initialize();
+  QString err;
+
+  // here we are placing code directly into specified parts of the Python that will get run
+  newScr.replace("|GUI_SET_RAWFILE_LIST|", QString::fromStdString(inFiles));
+  
+  createGetEIStatmens(newScr);
+
+  QString rebinStr = m_sets.leELow->text()+","+m_sets.leEWidth->text()+","+m_sets.leEHigh->text();
+  newScr.replace("|GUI_SET_BIN_BOUNDS|", "'"+rebinStr+"'");
+  Property * const params = rebin->getProperty("Params");
+  std::string error = params->setValue(rebinStr.toStdString());
+  if ( ! error.empty() )
+  {
+    m_fails[m_sets.leEHigh] = error;
+  }
+  
+  createNormalizationStatmens(newScr);
+
+  LEChkCpIn(newScr, "|GUI_SET_MAP_FILE|", m_sets.map_fileInput_leName,
+    group->getProperty("MapFile"));
+  
+  QString WSName = QString::fromStdString(oName);
+  createOutputStatmens(WSName, newScr);
+  
+  newScr.replace("|GUI_SET_SCALING|", getScaling());
+
+  if (m_pyScript.isEmpty())
+  {// put the import statement at the top of the script, only do this once
+    m_pyScript.append(m_templateH);
+  }
+  m_pyScript.append(newScr);
+
+  return WSName;
+}
+
+void deltaECalc::createGetEIStatmens(QString &newScr)
+{
+  // create this algorithm for doing the validation, actually, depending on the user settings, getEi might not get run but the validation is good anyway
+  IAlgorithm_sptr gEi = AlgorithmManager::Instance().createUnmanaged("GetEi");
+  gEi->initialize();
+  // run getEi, unless the user checked the box 
+  if (m_sets.ckFixEi->isChecked())
+  {// the function returns a string that is placed in the python source code where the python intepretor reads it as a number
+    LEChkCpIn(newScr, "|GUI_SET_E|", m_sets.leEGuess,
+	  gEi->getProperty("EnergyEstimate"));
+  }
+  else
+  {
+    newScr.replace("|GUI_SET_E|", "'Run GetEi'");
+  }
+  //this e guess (approximate Ei value) doesn't always get used, it depends on the tests above. However, Pyhton requires that the '|' in the code is replaced by something
+  LEChkCpIn(newScr, "|GUI_SET_E_GUESS|", m_sets.leEGuess,
+    gEi->getProperty("EnergyEstimate"));
+}
+
+void deltaECalc::createNormalizationStatmens(QString &newScr)
+{
+  newScr.replace("|GUI_SET_NORM|", "'"+getNormalization()+"'");
+  
+  std::vector<std::string> exts;
+  exts.push_back("raw"); exts.push_back("RAW");
+  exts.push_back("NXS"); exts.push_back("nxs");
+  FileProperty loadData("Filename", "", FileProperty::Load, exts);
+  LEChkCpIn(newScr, "|GUI_SET_WBV|", m_sets.leWBV0, &loadData);
+  
+  if ( ! m_sets.leWBV0->text().isEmpty() )
+  {
+    newScr.replace("|GUI_SET_WBVLow|", m_sets.leWBV0Low->text());
+    newScr.replace("|GUI_SET_WBVHigh|", m_sets.leWBV0High->text());
+	bool isNumber;
+	m_sets.leWBV0Low->text().toDouble(&isNumber);
+	if ( ! isNumber )
+	{
+	  m_fails[m_sets.leWBV0Low] = "Must be a number";
+	}
+	m_sets.leWBV0High->text().toDouble(&isNumber);
+	if ( ! isNumber )
+	{
+	  m_fails[m_sets.leWBV0High] = "Must be a number";
+	}
+  }
+  else
+  {// inserdt dummy values that Python will accept 
+    newScr.replace("|GUI_SET_WBVLow|", "-1");
+    newScr.replace("|GUI_SET_WBVHigh|", "-1");
+  }
+}
+void deltaECalc::createOutputStatmens(QString &WSName, QString &newScr)
+{// we are forcing the name to have contain .spe, but it doesn't have be right at the end of the file, if the user sees fit to have a different extension
+  if ( ! WSName.contains(".spe") )
+  {
+	WSName += ".spe";
+  }  
+  newScr.replace("|GUI_SET_OUTPUT|","'"+WSName+"'");
+  // if a full path was given check it exists, is what the test below amounts to
+  IAlgorithm_sptr saveSPE = AlgorithmManager::Instance().createUnmanaged("SaveSPE");
+  saveSPE->initialize();
+  Property * const Filename = saveSPE->getProperty("Filename");
+  std::string error = Filename->setValue(WSName.toStdString());
+  if ( ! error.empty() )
+  {
+    m_fails[m_sets.leNameSPE] = error;
+  }
+
+  // the name of the workspace which may remain in Mantid after this interface has finished will be the same as the saved file
+  newScr.replace("|GUI_SET_OUTWS|", "'"+tempWS+WSName+"'");
+}
+/** Replaces the marker word in the given text with the text in the QLineEdit
+*  checking if the value will fail validation, storing any error in m_fails
+* @param text the string in which the string will be replaced
+* @param pythonMark the word to search for and replace
+* @param userVal points to the QLineEdit containing the user value
+* @param check the property that will be used to for validity checking
+*/
+void deltaECalc::LEChkCpIn(QString &text, QString pythonMark, QLineEdit * userVal, Property * const check)
+{
+  const QString setting = userVal->text();
+  std::string error = replaceInErrsFind(text, pythonMark, setting, check);
+  if ( ! error.empty() )
+  {
+    m_fails[userVal] = error;
+  }
+}
+std::string deltaECalc::replaceInErrsFind(QString &text, QString pythonMark, const QString &setting, Property * const check) const
+{
+  text.replace(pythonMark, "'"+setting+"'");
+  return check->setValue(setting.toStdString());
+}
+/** appends Python commands that will remove the tempWS string from the
+* workspace name that will create a workspace with the name that was passed
+* @param name the name that the workspace will be renamed to (orginal name was tempWS+name)
+*/
+void deltaECalc::renameWorkspace(const QString &name)
+{
+  m_pyScript.append("RenameWorkspace(");
+  m_pyScript.append("'"+tempWS+name+"', '"+name+"')\n");
+}
+
+deltaECalc::FileInput::FileInput(QLineEdit &num, QComboBox &instr)
+  : m_lineEdit(num), m_box(instr), m_files()
+{}
+/** Convert integers into filenames, leaves all non-integers values untouched
+*/
+const std::vector<std::string>& deltaECalc::FileInput::getRunFiles()
+{
+  readComasAndHyphens(m_lineEdit.text().toStdString(), m_files);
+  std::vector<std::string>::iterator i = m_files.begin(), end = m_files.end();
+  for ( ; i != end; ++i )
+  {
+    try
+	{
+	  const unsigned int runNumber = boost::lexical_cast<unsigned int>(*i);
+	  *i = m_box.currentText().toStdString() + //instrument code
+	    boost::lexical_cast<std::string>(runNumber) +
+		".raw";                                      //only raw files are supported at the moment
+	}
+	catch ( boost::bad_lexical_cast )
+	{// the entry doesn't read as a run number
+	}// the alternative is that they entered a filename and we leave that as it is
+  }
+  return m_files;
+}
+
+//??STEVES? move this function into the file widget
+void deltaECalc::FileInput::readComasAndHyphens(const std::string &in, std::vector<std::string> &out)
+{
+  if ( in.empty() )
+  {// it is not an error to have an empty line but it would cause problems with an error check a the end of this function
+    return;
+  }
+  
+  Poco::StringTokenizer ranges(in, "-");
+  Poco::StringTokenizer::Iterator aList = ranges.begin();
+
+  out.clear();
+  do
+  {
+    Poco::StringTokenizer preHyp(*aList, ",", Poco::StringTokenizer::TOK_TRIM);
+    Poco::StringTokenizer::Iterator readPos = preHyp.begin();
+    if ( readPos == preHyp.end() )
+    {// can only mean that there was only an empty string or white space the '-'
+      throw std::invalid_argument("'-' found at the start of a list, can't interpret range specification");
+    }
+	out.reserve(out.size()+preHyp.count());
+    for ( ; readPos != preHyp.end(); ++readPos )
+    {
+      out.push_back(*readPos);
+    }
+    if (aList == ranges.end()-1)
+    {// there is no more input
+      break;
+    }
+
+    Poco::StringTokenizer postHy(*(aList+1),",",Poco::StringTokenizer::TOK_TRIM);
+    readPos = postHy.begin();
+    if ( readPos == postHy.end() )
+    {
+      throw std::invalid_argument("A '-' follows straight after another '-', can't interpret range specification");
+    }
+	
+    try
+    {//we've got the point in the string where there was a hyphen, first get the stuff that is after it
+      // the tokenizer will always return at least on string
+      const int rangeEnd = boost::lexical_cast<int>(*readPos);
+      // the last string before the hyphen should be an int, here we get the it
+      const int rangeStart = boost::lexical_cast<int>(out.back()); 
+      // counting down isn't supported
+      if ( rangeStart > rangeEnd )
+      {
+        throw std::invalid_argument("A range where the first integer is larger than the second is not allowed");
+      }
+      // expand the range
+      for ( int j = rangeStart+1; j < rangeEnd; j++ )
+      {
+        out.push_back(boost::lexical_cast<std::string>(j));
+      }
+	}
+	catch (boost::bad_lexical_cast)
+	{// the hyphen wasn't between two numbers, don't intepret it as a range instaed reconstruct the hyphenated string and add it to the list
+	  out.back() += "-" + *readPos;
+	}
+  }
+  while( ++aList != ranges.end() );
+
+  if ( *(in.end()-1) == '-' )
+  {
+    throw std::invalid_argument("'-' found at the end of a list, can't interpret range specification");
+  }
+}
