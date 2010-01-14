04bc58c8a591ec3181c37bd7ed9dd050ea8fa31b
Russell Taylor <taylorrj@ornl.gov>
2010-01-14 18:17:50 +0000

    Add WeightedMean algorithm from Rob Dalgliesh. Re #1084.

---
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/WeightedMean.h
@@ -0,0 +1,59 @@
+#ifndef MANTID_ALGORITHMS_WEIGHTEDMEAN_H_
+#define MANTID_ALGORITHMS_WEIGHTEDMEAN_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+/** An algorithm to calculate the weighted mean of two workspaces.
+
+    @author Robert Dalgliesh, ISIS, RAL
+    @date 12/1/2010
+
+    Copyright &copy; 2010 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+ */
+class DLLExport WeightedMean : public API::Algorithm
+{
+public:
+  /// Empty Constructor
+  WeightedMean() : API::Algorithm() {}
+  /// Empty Destructor
+  virtual ~WeightedMean() {}
+
+  virtual const std::string name() const { return "WeightedMean"; }
+  virtual const int version() const { return (1); }
+  virtual const std::string category() const { return "Arithmetic"; }
+
+private:
+  void init();
+  void exec();
+};
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_WEIGHTEDMEAN_H_*/
+
---
+++ Code/Mantid/Algorithms/src/WeightedMean.cpp
@@ -0,0 +1,99 @@
+#include "MantidAlgorithms/WeightedMean.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+// Algorithm must be declared
+DECLARE_ALGORITHM(WeightedMean)
+
+using namespace Kernel;
+using namespace API;
+
+/**  Initialization code
+ *
+ *   Properties have to be declared here before they can be used
+*/
+void WeightedMean::init()
+{
+  // Declare a 2D input workspace property.
+  declareProperty(new WorkspaceProperty<>("InputWorkspace1","",Direction::Input));
+
+  // Declare a 2D input workspace property.
+  declareProperty(new WorkspaceProperty<>("InputWorkspace2","",Direction::Input));
+
+  // Declare a 2D output workspace property.
+  declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output));
+}
+
+/** Executes the algorithm
+ */
+void WeightedMean::exec()
+{
+  g_log.information() << "Running algorithm " << name() << " version " << version() << std::endl;
+
+  // Get the input workspace2
+  MatrixWorkspace_const_sptr inputW1 = getProperty("InputWorkspace1");
+  MatrixWorkspace_const_sptr inputW2 = getProperty("InputWorkspace2");
+
+  // make output Workspace the same type and size as the input one
+  MatrixWorkspace_sptr outputW = boost::dynamic_pointer_cast<MatrixWorkspace>(WorkspaceFactory::Instance().create(inputW1));
+
+  // Get the count of histograms in the input workspace
+  int histogramCount = inputW1->getNumberHistograms();
+  // Loop over spectra
+  for (int i = 0; i < histogramCount; ++i)
+  {
+    // Retrieve the data into a vector
+    const MantidVec& XValues1 = inputW1->readX(i);
+    const MantidVec& YValues1 = inputW1->readY(i);
+    const MantidVec& EValues1 = inputW1->readE(i);
+    const MantidVec& XValues2 = inputW2->readX(i);
+    const MantidVec& YValues2 = inputW2->readY(i);
+    const MantidVec& EValues2 = inputW2->readE(i);
+    MantidVec& newX = outputW->dataX(i);
+    MantidVec& newY = outputW->dataY(i);
+    MantidVec& newE = outputW->dataE(i);
+
+    int j=0;
+    double err1,err2;
+    // Iterate over i-th spectrum and modify the data
+    for(j=0;j<inputW1->blocksize();j++)
+    {
+      newX[j] = XValues1[j];
+      if (EValues1[j] > 0.0 && EValues2[j] > 0.0)
+      {
+        err1=EValues1[j]*EValues1[j];
+        err2=EValues2[j]*EValues2[j];
+        newY[j] = (YValues1[j]/err1)+(YValues2[j]/err2);
+        newE[j] = 1.0/((1.0/err1)+(1.0/err2));
+        newY[j] *= newE[j];
+        newE[j]=sqrt(newE[j]); 
+      }
+      else if (EValues1[j] > 0.0 && EValues2[j] <= 0.0)
+      {
+        newY[j]=YValues1[j];
+        newE[j]=EValues1[j];
+      }
+      else if (EValues1[j] <= 0.0 && EValues2[j] > 0.0)
+      {
+        newY[j]=YValues2[j];
+        newE[j]=EValues2[j];
+      }
+      else
+      {
+        newY[j]=0.0;
+        newE[j]=0.0;
+      }
+    }
+    newX[j] = XValues1[j];
+
+    }
+
+    // Assign it to the output workspace property
+    setProperty("OutputWorkspace",outputW);
+}
+
+} // namespace Algorithms
+} // namespace Mantid
