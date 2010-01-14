4b2e40724c65293b3142bb3fd1d2e86bb33aaf4c
Nick Draper <nick.draper@stfc.ac.uk>
2010-01-14 16:10:29 +0000

    re #1000 attempt to fix win64 build

--- Code/Mantid/Build/WinBuildScripts/BuildTests64.bat
+++ Code/Mantid/Build/WinBuildScripts/BuildTests64.bat
@@ -2,4 +2,6 @@ del ..\..\..\..\logs\Mantid\testsBuild.log
 del ..\..\..\..\logs\Mantid\testsBuildErr.log
 del ..\..\..\..\logs\Mantid\testResults.log
 del ..\..\..\..\logs\Mantid\testsRunErr.log
+rem must set to be 64 bit
+CALL "%VCINSTALLDIR%"\vcvarsall.bat amd64
 build.bat "win64=1" 1>..\..\..\..\logs\Mantid\testsBuild.log 2> ..\..\..\..\logs\Mantid\testsBuildErr.log
