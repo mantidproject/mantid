15e774d94a5cff0f109deabd5b524d14ffad5256
Alex Buts <alex.buts@stfc.ac.uk>
2010-05-14 12:54:06 +0000

    refs #1239 names of logarithm functions and files have been changed to upper case

---
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/Logarithm.h
@@ -0,0 +1,75 @@
+#ifndef MANTID_ALGORITHM_LOG_H_
+#define MANTID_ALGORITHM_LOG_H_
+
+#include "MantidAlgorithms/UnaryOperation.h"
+namespace Mantid{
+namespace Algorithms {
+/** Takes a workspace as input and calculates the natural logarithm of number of counts for each 1D spectrum.
+    The algorithm creates a new workspace containing logarithms of signals (numbers of counts) in
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the workspace to take as input </LI>
+    <LI> OutputWorkspace - The name of the workspace in which to store the result </LI>
+    </UL>
+
+    Optional Properties (assume that you count from zero):
+    <UL>
+	<LI> filler  -- A workspace can normally have zeros in it and the logarithm goes to minys infinity for zeros.
+	                This field keeps value, that should be placed in the workspace instead of minus infinity </LI>
+    <LI> natural -- Natural or base 10 logarithm. Default is natural </LI>
+    </UL>
+
+    @author AB,    ISIS, Rutherford Appleton Laboratory
+    @date 12/05/2010
+    Copyright &copy; 2007-2010 STFC Rutherford Appleton Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+ */
+class DLLExport Logarithm : public UnaryOperation
+{
+public:
+    Logarithm(void):UnaryOperation(),log_Min(0),is_natural(true){};
+    virtual ~Logarithm(void){};
+    /// Algorithm's name for identification
+	virtual const std::string name() const { return "log";}
+   /// Algorithm's version for identification
+	virtual const int version() const { return (1);}
+    /// Algorithm's category for identification
+    virtual const std::string category() const { return "Arithmetic";}
+private:
+  // Overridden Algorithm methods
+  void init();
+  //void exec();
+  /// The value to replace ln(0)
+  double log_Min;
+  /// If the logarithm natural or 10-based
+  bool   is_natural;
+  ///
+
+  /// get properties from GUI
+  virtual void retrieveProperties();
+ /// Actually the function, which is run on values when the operation is performed
+  virtual void performUnaryOperation(const double& XIn, const double& YIn, const double& EIn, double& YOut, double& EOut);
+
+
+};
+} // End namespace Algorithms
+} // End namespace Mantid
+#endif
---
+++ Code/Mantid/Algorithms/test/LogarithmTest.h
@@ -0,0 +1,102 @@
+#ifndef MANTID_ALGORITHM_LOGTEST_H_
+#define MANTID_ALGORITHM_LOGTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "WorkspaceCreationHelper.hh"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/WorkspaceFactory.h"
+#include "MantidDataObjects/Workspace2D.h"
+#include "MantidDataObjects/Workspace1D.h"
+#include "MantidAPI/WorkspaceProperty.h"
+#include "MantidAPI/WorkspaceOpOverloads.h"
+
+#include "MantidAlgorithms/Logarithm.h"
+
+using namespace Mantid::Kernel;
+using namespace Mantid::DataObjects;
+using namespace Mantid::API;
+using namespace Mantid::Algorithms;
+
+
+class LogarithmTest : public CxxTest::TestSuite
+{
+public:
+  void testInit(void)
+  {
+      Mantid::Algorithms::Logarithm alg;
+      TS_ASSERT_THROWS_NOTHING(alg.initialize());
+      TS_ASSERT(alg.isInitialized());
+
+  }
+
+  void testExec1D(void)
+  {
+	  int sizex = 10;
+
+		// Register the workspace in the data service
+	  MatrixWorkspace_sptr work_in1 = WorkspaceCreationHelper::Create1DWorkspaceFib(sizex);
+	  AnalysisDataService::Instance().add("test_inLn", work_in1);
+
+
+      Logarithm alg;
+
+      alg.initialize();
+
+      TS_ASSERT_THROWS_NOTHING(
+          alg.setPropertyValue("InputWorkspace","test_inLn");
+          alg.setPropertyValue("OutputWorkspace","test_inLn");
+          alg.setPropertyValue("filler","10");
+          alg.setPropertyValue("natural","0");
+       );
+
+      alg.execute();
+
+     MatrixWorkspace_sptr work_out1;
+     TS_ASSERT_THROWS_NOTHING(work_out1 = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve("test_inLn")));
+
+     TS_ASSERT_THROWS_NOTHING(
+      AnalysisDataService::Instance().remove("test_outLn");
+      AnalysisDataService::Instance().remove("test_inLn");
+	  );
+
+  }
+
+  void testExec2D(void)
+  {
+
+	  int sizex = 10,sizey=20;
+		// Register the workspace in the data service
+		MatrixWorkspace_sptr work_in2 = WorkspaceCreationHelper::Create2DWorkspace154(sizex,sizey);
+		Workspace2D_sptr     work_ou2 = WorkspaceCreationHelper::Create2DWorkspace(sizex, sizey);
+
+
+        Logarithm alg;
+
+		AnalysisDataService::Instance().add("test_inLn2", work_in2);
+		AnalysisDataService::Instance().add("test_outLn2", work_ou2);
+
+		alg.initialize();
+       TS_ASSERT_THROWS_NOTHING(
+        alg.setPropertyValue("InputWorkspace","test_inLn2");
+	    alg.setPropertyValue("OutputWorkspace","test_outLn2");
+	 	alg.setPropertyValue("natural","1");
+
+		);
+
+		TS_ASSERT_THROWS_NOTHING(alg.execute());
+        TS_ASSERT( alg.isExecuted() );
+
+	     MatrixWorkspace_sptr work_out2;
+         TS_ASSERT_THROWS_NOTHING(work_out2 = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve("test_outLn2")));
+
+   //    checkData(work_in1, work_in2, work_out1);
+
+        AnalysisDataService::Instance().remove("test_inLn2");
+        AnalysisDataService::Instance().remove("test_outLn2");
+
+  }
+
+};
+
+#endif           /* MANTID_ALGORITHM_LOGTEST_H_ */
