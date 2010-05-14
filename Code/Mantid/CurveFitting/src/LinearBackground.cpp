1e8696df313af7ce0f62f18b6e4248f1a0b547db
Russell Taylor <taylorrj@ornl.gov>
2010-05-14 13:44:43 +0000

    Compiler warning flagged bug in LinearBackground - loop counter wasn't being initialised. Re #1184.

--- Code/Mantid/CurveFitting/src/LinearBackground.cpp
+++ Code/Mantid/CurveFitting/src/LinearBackground.cpp
@@ -66,7 +66,7 @@ void LinearBackground::fit(const std::vector<double>& X,const std::vector<double
   double y_mean = 0;
   double x2_mean = 0;
   double xy_mean = 0;
-  for(int i;i < n; i++)
+  for(int i = 0; i < n; i++)
   {
     double x = X[i];
     double y = Y[i];
