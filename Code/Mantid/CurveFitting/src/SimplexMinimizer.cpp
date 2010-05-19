d4fde6e6a7230c0fd3371be89b8487a99e0543e6
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2010-05-19 10:28:42 +0000

    missed out this file in previous commit to this ticket. Refs #1254.

--- Code/Mantid/CurveFitting/src/SimplexMinimizer.cpp
+++ Code/Mantid/CurveFitting/src/SimplexMinimizer.cpp
@@ -3,28 +3,80 @@
 //----------------------------------------------------------------------
 #include "MantidCurveFitting/SimplexMinimizer.h"
 #include "MantidKernel/Exception.h"
+#include "MantidCurveFitting/GSLFunctions.h"
 
 namespace Mantid
 {
 namespace CurveFitting
 {
 
-SimplexMinimizer::SimplexMinimizer( gsl_multimin_function& gslContainer, 
-  gsl_vector* startGuess, const double& size) : m_name("Simplex") 
+// Get a reference to the logger
+Kernel::Logger& SimplexMinimizer::g_log = Kernel::Logger::get("SimplexMinimizer");
+
+void SimplexMinimizer::initialize(double* X, const double* Y, double *sqrtWeight, 
+                                   const int& nData, const int& nParam, 
+                                   gsl_vector* startGuess,
+                                   Fit* fit, const std::string& costFunction) 
 {
   const gsl_multimin_fminimizer_type *T = gsl_multimin_fminimizer_nmsimplex;
 
   // step size for simplex
-  m_simplexStepSize = gsl_vector_alloc(gslContainer.n);
-  gsl_vector_set_all (m_simplexStepSize, size);  // is this always a sensible starting step size?
+  m_simplexStepSize = gsl_vector_alloc(nParam);
+  gsl_vector_set_all (m_simplexStepSize, m_size);  
+
+  // set-up GSL container to be used with GSL simplex algorithm
+  m_data = new GSL_FitData(fit);  //,X, Y, sqrtWeight, nData, nParam);
+  m_data->p = nParam;
+  m_data->n = nData; 
+  m_data->X = X;
+  m_data->Y = Y;
+  m_data->sqrtWeightData = sqrtWeight;
+  m_data->holdCalculatedData = new double[nData];
+  m_data->holdCalculatedJacobian =  gsl_matrix_alloc (nData, nParam);
+
+  if ( costFunction.compare("Least squares") == 0 )
+    m_data->costFunc = new CostFuncLeastSquares();
+  else if ( costFunction.compare("Ignore positive peaks") == 0 )
+    m_data->costFunc = new CostFuncIgnorePosPeaks();
+  else
+  {
+    g_log.error("Unrecognised cost function. Default to Least squares\n");
+    m_data->costFunc = new CostFuncLeastSquares();
+  }
+
+  // setup simplex container
+  gslContainer.n = nParam;  
+  gslContainer.f = &gsl_costFunction;
+  gslContainer.params = m_data;
 
   // setup minimizer
-  m_gslSolver = gsl_multimin_fminimizer_alloc(T, gslContainer.n);
+  m_gslSolver = gsl_multimin_fminimizer_alloc(T, nParam);
   gsl_multimin_fminimizer_set(m_gslSolver, &gslContainer, startGuess, m_simplexStepSize);
+}
+
+void SimplexMinimizer::resetSize(double* X, const double* Y, double *sqrtWeight, 
+                                   const int& nData, const int& nParam, 
+                                   gsl_vector* startGuess, const double& size,
+                                   Fit* fit, const std::string& costFunction) 
+{
+  m_size = size;
+  clearMemory();
+  initialize(X, Y, sqrtWeight, nData, nParam, startGuess, fit, costFunction);
 }
 
 SimplexMinimizer::~SimplexMinimizer()
 {
+  clearMemory();
+}
+
+/// clear memory
+void SimplexMinimizer::clearMemory()
+{
+  delete [] m_data->holdCalculatedData;
+  delete m_data->costFunc;
+  gsl_matrix_free (m_data->holdCalculatedJacobian);
+  delete m_data;
+
   gsl_vector_free(m_simplexStepSize);
   gsl_multimin_fminimizer_free(m_gslSolver);
 }
