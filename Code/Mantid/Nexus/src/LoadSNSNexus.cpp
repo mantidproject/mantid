f6122ce95a259254d2039c6a270d8a082a1f8043
Peter Peterson <petersonpf@ornl.gov>
2010-05-19 21:37:29 +0000

    Lots of print statements, but fixed the direction of the indices in the block read. Refs #1131.

--- Code/Mantid/Nexus/src/LoadSNSNexus.cpp
+++ Code/Mantid/Nexus/src/LoadSNSNexus.cpp
@@ -15,6 +15,7 @@
 #include "MantidKernel/PhysicalConstants.h"
 
 #include <cmath>
+#include <iostream>
 #include <sstream>
 #include <fstream>
 #include <limits>
@@ -35,6 +36,7 @@ LoadSNSNexus::LoadSNSNexus():m_L1(0) {}
 /// Initialisation method.
 void LoadSNSNexus::init()
 {
+    std::cerr << "00:" << std::endl; // REMOVE
     std::vector<std::string> exts;
     exts.push_back("NXS");
     exts.push_back("nxs");
@@ -57,29 +59,31 @@ void LoadSNSNexus::init()
 */
 void LoadSNSNexus::exec()
 {
+    std::cerr << "00:" << std::endl; // REMOVE
     // Create the root Nexus class
     NXRoot root(getPropertyValue("Filename"));
 
     int nPeriods = root.groups().size();
     WorkspaceGroup_sptr sptrWSGrp= WorkspaceGroup_sptr(new WorkspaceGroup);
-
+    std::cerr << "01:" << std::endl; // REMOVE
     if (nPeriods > 1)
     {
         setProperty("OutputWorkspace",boost::dynamic_pointer_cast<Workspace>(sptrWSGrp));
     }
-
+    std::cerr << "02:" << std::endl; // REMOVE
     double progress_step = 1.0/nPeriods;
     int period = 0;
     // Loop over entries loading each entry into a separate workspace
     for(std::vector<NXClassInfo>::const_iterator it=root.groups().begin();it!=root.groups().end();it++)
     if (it->nxclass == "NXentry")
     {
+        std::cerr << "03a:" << std::endl; // REMOVE
         ++period;
         double progress_start = (period-1)*progress_step;
         double progress_end = progress_start + progress_step;
-
+        std::cerr << "04:" << std::endl; // REMOVE
         Workspace_sptr ws = loadEntry(root.openEntry(it->nxname),period,progress_start,progress_end);
-
+        std::cerr << "05:" << nPeriods << std::endl; // REMOVE
         // Save the workspace property
         if (nPeriods == 1)
             setProperty("OutputWorkspace",ws);
@@ -107,31 +111,33 @@ void LoadSNSNexus::exec()
  */
 API::Workspace_sptr LoadSNSNexus::loadEntry(NXEntry entry,int period, double progress_start, double progress_end)
 {
+    std::cerr << "loadEntry00:" << std::endl;// REMOVE
     // To keep sorted bank names
     std::set<std::string,CompareBanks> banks;
     std::set<std::string> monitors;
-
+    std::cerr << "loadEntry01:" << std::endl;// REMOVE
     // Calculate the workspace dimensions
     int nSpectra = 0;
     int nBins = 0;
-    
+        std::cerr << "loadEntry02:" << std::endl;// REMOVE
     for(std::vector<NXClassInfo>::const_iterator it=entry.groups().begin();it!=entry.groups().end();it++)
     if (it->nxclass == "NXdata") // Count detectors
-    {
+    {    std::cerr << "loadEntry03:" << it->nxname << std::endl;// REMOVE
             NXData dataGroup = entry.openNXData(it->nxname);
-            NXFloat data = dataGroup.openFloatData();
+            NXInt data = dataGroup.openIntData();
             if (data.rank() != 3) throw std::runtime_error("SNS NXdata is expected to be a 3D array");
             if (nBins == 0) nBins = data.dim2();
             nSpectra += data.dim0() * data.dim1();
             banks.insert(it->nxname); // sort the bank names
     }
     else if (it->nxclass == "NXmonitor") // Count monitors
-    {
+    {    std::cerr << "loadEntry04:" << std::endl;// REMOVE
         nSpectra += 1;
         monitors.insert(it->nxname);
     }
 
     // Create the output workspace
+    std::cerr << "loadEntry05:" << std::endl;// REMOVE
     DataObjects::Workspace2D_sptr ws = boost::dynamic_pointer_cast<DataObjects::Workspace2D>
         (WorkspaceFactory::Instance().create("Workspace2D",nSpectra,nBins+1,nBins));
     ws->setTitle(entry.name());
@@ -142,6 +148,7 @@ API::Workspace_sptr LoadSNSNexus::loadEntry(NXEntry entry,int period, double pro
     int spec = 0;
 
     // Load monitor readings
+    std::cerr << "loadEntry06:" << std::endl;// REMOVE
     for(std::set<std::string>::const_iterator it=monitors.begin();it!=monitors.end();it++)
     {
         NXData dataGroup = entry.openNXData(*it);
@@ -157,7 +164,7 @@ API::Workspace_sptr LoadSNSNexus::loadEntry(NXEntry entry,int period, double pro
         std::transform(Y.begin(), Y.end(), E.begin(), dblSqrt);
         spec++;
     }
-
+    std::cerr << "loadEntry07:" << std::endl;// REMOVE
     int specData = spec;
 
     // Load detector readings
@@ -173,16 +180,18 @@ API::Workspace_sptr LoadSNSNexus::loadEntry(NXEntry entry,int period, double pro
             X.assign(timeBins(),timeBins()+nBins+1);
         }
         NXInt data = dataGroup.openIntData();
+    std::cerr << "loadEntry08:" << data.dim0() << ", " << data.dim1() << std::endl;// REMOVE
         for(int i = 0;i<data.dim0();i++)
             for(int j = 0; j < data.dim1(); j++)
             {
+
                 if (spec > specData)
                 {
                     MantidVec& newX = ws->dataX(spec);
                     MantidVec& oldX = ws->dataX(specData);
                     newX.assign(oldX.begin(),oldX.end());
                 }
-                data.load(i,j);
+                data.load(j,i);
                 ////-- simulate input --
                 //for(int k=0;k<nBins;k++)
                 //    data()[k] = float(i*1000 + j*100 + k);
@@ -195,7 +204,7 @@ API::Workspace_sptr LoadSNSNexus::loadEntry(NXEntry entry,int period, double pro
                 progress.report();
             }
     }
-
+    std::cerr << "loadEntry09:" << std::endl;// REMOVE
     boost::shared_array<int> spectra(new int[nSpectra]);
     for(int i=0;i<nSpectra;i++)
     {
@@ -203,14 +212,14 @@ API::Workspace_sptr LoadSNSNexus::loadEntry(NXEntry entry,int period, double pro
         ws->getAxis(1)->spectraNo(i)= i+1;
     }
     ws->mutableSpectraMap().populate(spectra.get(),spectra.get(),nSpectra);
-
+    std::cerr << "loadEntry10:" << std::endl;// REMOVE
     NXFloat proton_charge = entry.openNXFloat("proton_charge");
     proton_charge.load();
     //ws->getSample()->setProtonCharge(*proton_charge());
 	ws->mutableSample().setProtonCharge(*proton_charge());
-
+    std::cerr << "loadEntry11:" << std::endl;// REMOVE
     loadInstrument(ws,entry,banks,monitors);
-
+    std::cerr << "loadEntry12:" << std::endl;// REMOVE
     return ws;
 }
 
