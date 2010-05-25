c34a05e849fa553c5b425b1febcf8cb0e077c7b3
Russell Taylor <taylorrj@ornl.gov>
2010-05-25 11:33:49 +0000

    Fix test on Windows. Re #100.

--- Code/Mantid/Geometry/test/QuatTest.h
+++ Code/Mantid/Geometry/test/QuatTest.h
@@ -16,173 +16,188 @@ private:
   Mantid::Geometry::Quat q,p;
 
 public:
-	void testoperatorbracket()
-	{
-		p[0]=0;
-		p[1]=1;
-		p[2]=2;
-		p[3]=3;
-		TS_ASSERT_EQUALS(p[0],0.0);
-		TS_ASSERT_EQUALS(p[1],1.0);
-		TS_ASSERT_EQUALS(p[2],2.0);
-		TS_ASSERT_EQUALS(p[3],3.0);
-	}
-	void testEmptyConstructor()
-	{
-		TS_ASSERT_EQUALS(q[0],1.0);
-		TS_ASSERT_EQUALS(q[1],0.0);
-		TS_ASSERT_EQUALS(q[2],0.0);
-		TS_ASSERT_EQUALS(q[3],0.0);
-	}
-	void testValueConstructor()
-	{
-		Mantid::Geometry::Quat q1(1,2,3,4);
-		TS_ASSERT_EQUALS(q1[0],1.0);
-		TS_ASSERT_EQUALS(q1[1],2.0);
-		TS_ASSERT_EQUALS(q1[2],3.0);
-		TS_ASSERT_EQUALS(q1[3],4.0);
-	}
-	void testAngleAxisConstructor()
-	{
-		Mantid::Geometry::V3D v(1,1,1);
-		// Construct quaternion to represent rotation
-		// of 45 degrees around the 111 axis.
-		Mantid::Geometry::Quat q1(90.0,v);
-		double c=1.0/sqrt(2.0);
-		double s=c/sqrt(3.0);
-		TS_ASSERT_DELTA(q1[0],c,0.000001);
-		TS_ASSERT_DELTA(q1[1],s,0.000001);
-		TS_ASSERT_DELTA(q1[2],s,0.000001);
-		TS_ASSERT_DELTA(q1[3],s,0.000001);
-	}
+  void testoperatorbracket()
+  {
+    p[0]=0;
+    p[1]=1;
+    p[2]=2;
+    p[3]=3;
+    TS_ASSERT_EQUALS(p[0],0.0);
+    TS_ASSERT_EQUALS(p[1],1.0);
+    TS_ASSERT_EQUALS(p[2],2.0);
+    TS_ASSERT_EQUALS(p[3],3.0);
+  }
 
-	void testoperatorassignmentfromdouble()
-	{
-		q(2,3,4,5);
-		TS_ASSERT_EQUALS(q[0],2.0);
-		TS_ASSERT_EQUALS(q[1],3.0);
-		TS_ASSERT_EQUALS(q[2],4.0);
-		TS_ASSERT_EQUALS(q[3],5.0);
-	}
-	void testoperatorassignmentfromangleaxis()
-	{
-		Mantid::Geometry::V3D v(1,1,1);
-		q(90.0,v);
-	        double c=1.0/sqrt(2.0);
-		double s=c/sqrt(3.0);
-		TS_ASSERT_DELTA(q[0],c,0.000001);
-		TS_ASSERT_DELTA(q[1],s,0.000001);
-		TS_ASSERT_DELTA(q[2],s,0.000001);
-		TS_ASSERT_DELTA(q[3],s,0.000001);
+  void testEmptyConstructor()
+  {
+    TS_ASSERT_EQUALS(q[0],1.0);
+    TS_ASSERT_EQUALS(q[1],0.0);
+    TS_ASSERT_EQUALS(q[2],0.0);
+    TS_ASSERT_EQUALS(q[3],0.0);
+  }
+
+  void testValueConstructor()
+  {
+    Mantid::Geometry::Quat q1(1,2,3,4);
+    TS_ASSERT_EQUALS(q1[0],1.0);
+    TS_ASSERT_EQUALS(q1[1],2.0);
+    TS_ASSERT_EQUALS(q1[2],3.0);
+    TS_ASSERT_EQUALS(q1[3],4.0);
+  }
+
+  void testAngleAxisConstructor()
+  {
+    Mantid::Geometry::V3D v(1,1,1);
+    // Construct quaternion to represent rotation
+    // of 45 degrees around the 111 axis.
+    Mantid::Geometry::Quat q1(90.0,v);
+    double c=1.0/sqrt(2.0);
+    double s=c/sqrt(3.0);
+    TS_ASSERT_DELTA(q1[0],c,0.000001);
+    TS_ASSERT_DELTA(q1[1],s,0.000001);
+    TS_ASSERT_DELTA(q1[2],s,0.000001);
+    TS_ASSERT_DELTA(q1[3],s,0.000001);
+  }
+
+  void testoperatorassignmentfromdouble()
+  {
+    q(2,3,4,5);
+    TS_ASSERT_EQUALS(q[0],2.0);
+    TS_ASSERT_EQUALS(q[1],3.0);
+    TS_ASSERT_EQUALS(q[2],4.0);
+    TS_ASSERT_EQUALS(q[3],5.0);
+  }
+
+  void testoperatorassignmentfromangleaxis()
+  {
+    Mantid::Geometry::V3D v(1,1,1);
+    q(90.0,v);
+    double c=1.0/sqrt(2.0);
+    double s=c/sqrt(3.0);
+    TS_ASSERT_DELTA(q[0],c,0.000001);
+    TS_ASSERT_DELTA(q[1],s,0.000001);
+    TS_ASSERT_DELTA(q[2],s,0.000001);
+    TS_ASSERT_DELTA(q[3],s,0.000001);
 
     //Now rotate 45 degrees around y
     q(45, V3D(0,1,0));
     V3D X(1,0,0);
     q.rotate(X);
-    double a = sqrt(2)/2;
+    double a = sqrt(2.0)/2;
     TS_ASSERT(X==V3D(a,0,-a));
     //Now rotate -45 degrees around y
     q(-45, V3D(0,1,0));
     X(1,0,0);
     q.rotate(X);
     TS_ASSERT(X==V3D(a,0,a));
-	}
+  }
 
-	void testoperatorequal()
-	{
-		q=p;
-		TS_ASSERT_EQUALS(q[0],p[0]);
-		TS_ASSERT_EQUALS(q[1],p[1]);
-		TS_ASSERT_EQUALS(q[2],p[2]);
-		TS_ASSERT_EQUALS(q[3],p[3]);
-	}
-	void testlenmethod()
-	{
-		q(1,2,3,4);
-		TS_ASSERT_EQUALS(q.len(),sqrt(30.0));
-	}
-	void testlen2method()
-	{
-		q(1,2,3,4);
-		TS_ASSERT_EQUALS(q.len2(),30.0);
-	}
-	void testinitmehtod()
-	{
-		q.init();
-		TS_ASSERT_EQUALS(q[0],1);
-		TS_ASSERT_EQUALS(q[1],0);
-		TS_ASSERT_EQUALS(q[2],0);
-		TS_ASSERT_EQUALS(q[3],0);
-	}
-	void testnormalizemethod()
-	{
-		q(2,2,2,2);
-		q.normalize();
-		TS_ASSERT_DELTA(q[0],0.125,0.000001);
-		TS_ASSERT_DELTA(q[1],0.125,0.000001);
-		TS_ASSERT_DELTA(q[2],0.125,0.000001);
-		TS_ASSERT_DELTA(q[3],0.125,0.000001);
-	}
-	void testconjugatemethod()
-	{
-		q(1,1,1,1);
-		q.conjugate();
-		TS_ASSERT_EQUALS(q[0],1);
-		TS_ASSERT_EQUALS(q[1],-1);
-		TS_ASSERT_EQUALS(q[2],-1);
-		TS_ASSERT_EQUALS(q[3],-1);
-	}
-	void testinversemethod()
-	{
-		q(2,3,4,5);
-		Mantid::Geometry::Quat qinv(q);
-		qinv.inverse();
-		q*=qinv;
-		TS_ASSERT_DELTA(q[0],1,0.000001);
-		TS_ASSERT_DELTA(q[1],0,0.000001);
-		TS_ASSERT_DELTA(q[2],0,0.000001);
-		TS_ASSERT_DELTA(q[3],0,0.000001);
-	}
-	void testoperatorplus()
-	{
-		q(1,1,1,1);
-		p(-1,2,1,3);
-		Mantid::Geometry::Quat res;
-		res=p+q;
-		TS_ASSERT_EQUALS(res[0],0);
-		TS_ASSERT_EQUALS(res[1],3);
-		TS_ASSERT_EQUALS(res[2],2);
-		TS_ASSERT_EQUALS(res[3],4);
-	}
-	void testoperatorminus()
-	{
-		q(1,1,1,1);
-		p(-1,2,1,3);
-		Mantid::Geometry::Quat res;
-		res=p-q;
-		TS_ASSERT_EQUALS(res[0],-2);
-		TS_ASSERT_EQUALS(res[1],1);
-		TS_ASSERT_EQUALS(res[2],0);
-		TS_ASSERT_EQUALS(res[3],2);
-	}
-	void testoperatortimes()
-	{
-		q(1,1,1,1);
-		p(-1,2,1,3);
-		Mantid::Geometry::Quat res;
-		res=p*q;
-		TS_ASSERT_EQUALS(res[0],-7);
-		TS_ASSERT_EQUALS(res[1],-1);
-		TS_ASSERT_EQUALS(res[2],1);
-		TS_ASSERT_EQUALS(res[3],3);
-	}
-	void testoperatordoublequal()
-	{
-		p=q;
+  void testoperatorequal()
+  {
+    q=p;
+    TS_ASSERT_EQUALS(q[0],p[0]);
+    TS_ASSERT_EQUALS(q[1],p[1]);
+    TS_ASSERT_EQUALS(q[2],p[2]);
+    TS_ASSERT_EQUALS(q[3],p[3]);
+  }
+
+  void testlenmethod()
+  {
+    q(1,2,3,4);
+    TS_ASSERT_EQUALS(q.len(),sqrt(30.0));
+  }
+
+  void testlen2method()
+  {
+    q(1,2,3,4);
+    TS_ASSERT_EQUALS(q.len2(),30.0);
+  }
+
+  void testinitmehtod()
+  {
+    q.init();
+    TS_ASSERT_EQUALS(q[0],1);
+    TS_ASSERT_EQUALS(q[1],0);
+    TS_ASSERT_EQUALS(q[2],0);
+    TS_ASSERT_EQUALS(q[3],0);
+  }
+
+  void testnormalizemethod()
+  {
+    q(2,2,2,2);
+    q.normalize();
+    TS_ASSERT_DELTA(q[0],0.125,0.000001);
+    TS_ASSERT_DELTA(q[1],0.125,0.000001);
+    TS_ASSERT_DELTA(q[2],0.125,0.000001);
+    TS_ASSERT_DELTA(q[3],0.125,0.000001);
+  }
+
+  void testconjugatemethod()
+  {
+    q(1,1,1,1);
+    q.conjugate();
+    TS_ASSERT_EQUALS(q[0],1);
+    TS_ASSERT_EQUALS(q[1],-1);
+    TS_ASSERT_EQUALS(q[2],-1);
+    TS_ASSERT_EQUALS(q[3],-1);
+  }
+
+  void testinversemethod()
+  {
+    q(2,3,4,5);
+    Mantid::Geometry::Quat qinv(q);
+    qinv.inverse();
+    q*=qinv;
+    TS_ASSERT_DELTA(q[0],1,0.000001);
+    TS_ASSERT_DELTA(q[1],0,0.000001);
+    TS_ASSERT_DELTA(q[2],0,0.000001);
+    TS_ASSERT_DELTA(q[3],0,0.000001);
+  }
+
+  void testoperatorplus()
+  {
+    q(1,1,1,1);
+    p(-1,2,1,3);
+    Mantid::Geometry::Quat res;
+    res=p+q;
+    TS_ASSERT_EQUALS(res[0],0);
+    TS_ASSERT_EQUALS(res[1],3);
+    TS_ASSERT_EQUALS(res[2],2);
+    TS_ASSERT_EQUALS(res[3],4);
+  }
+
+  void testoperatorminus()
+  {
+    q(1,1,1,1);
+    p(-1,2,1,3);
+    Mantid::Geometry::Quat res;
+    res=p-q;
+    TS_ASSERT_EQUALS(res[0],-2);
+    TS_ASSERT_EQUALS(res[1],1);
+    TS_ASSERT_EQUALS(res[2],0);
+    TS_ASSERT_EQUALS(res[3],2);
+  }
+
+  void testoperatortimes()
+  {
+    q(1,1,1,1);
+    p(-1,2,1,3);
+    Mantid::Geometry::Quat res;
+    res=p*q;
+    TS_ASSERT_EQUALS(res[0],-7);
+    TS_ASSERT_EQUALS(res[1],-1);
+    TS_ASSERT_EQUALS(res[2],1);
+    TS_ASSERT_EQUALS(res[3],3);
+  }
+
+  void testoperatordoublequal()
+  {
+    p=q;
     TS_ASSERT(p==q);
-		q(1,4,5,6);
-		TS_ASSERT(p!=q);
-	}
+    q(1,4,5,6);
+    TS_ASSERT(p!=q);
+  }
+
   void testoperatornotequal()
   {
     q(1,2,3,4);
@@ -193,7 +208,7 @@ public:
 
   void testRotateVector()
   {
-    double a = sqrt(2)/2;
+    double a = sqrt(2.0)/2;
 
     //Trivial
     p(1,0,0,0);//Identity quaternion
@@ -221,7 +236,7 @@ public:
     v = V3D(1,0,0);
     p(30., V3D(0,0,1));
     p.rotate(v);
-    TS_ASSERT(v==V3D(sqrt(3)/2, 0.5, 0));
+    TS_ASSERT(v==V3D(sqrt(3.0)/2, 0.5, 0));
 
     v = V3D(1,0,0);
     p(125., V3D(1,0,0));
@@ -260,7 +275,7 @@ public:
   void testSetFromDirectionCosineMatrix2b()
   {
     //Rotate -45 deg around Y
-    double a = sqrt(2)/2;
+    double a = sqrt(2.0)/2;
     V3D rX(a,0,a);
     V3D rY(0,1,0);
     V3D rZ(-a,0,a);
@@ -326,9 +341,9 @@ public:
     TS_ASSERT(oZ==rZ);
     TS_ASSERT(rotQ==q);
 
-//    std::cout << "\nRotated coordinates are " << rX << rY << rZ << "\n";
-//    std::cout << "Expected (p) is" << p << "; got " << q << "\n";
-//    std::cout << "Re-Rotated coordinates are " << oX << oY << oZ << "\n";
+    //    std::cout << "\nRotated coordinates are " << rX << rY << rZ << "\n";
+    //    std::cout << "Expected (p) is" << p << "; got " << q << "\n";
+    //    std::cout << "Re-Rotated coordinates are " << oX << oY << oZ << "\n";
   }
 
   void testSetFromDirectionCosineMatrix_arbitrary()
@@ -345,7 +360,7 @@ public:
 
   void testConstructorFromDirectionCosine()
   {
-    double a = sqrt(2)/2;
+    double a = sqrt(2.0)/2;
     V3D rX(a,0,a);
     V3D rY(0,1,0);
     V3D rZ(-a,0,a);
