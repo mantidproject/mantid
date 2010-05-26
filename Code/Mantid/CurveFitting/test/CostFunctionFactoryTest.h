608db26238f2ac899ff83591bffec1224edc0f99
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2010-05-26 09:11:17 +0000

    Tests for factories. Refs #1254.

---
+++ Code/Mantid/CurveFitting/test/CostFunctionFactoryTest.h
@@ -0,0 +1,52 @@
+#ifndef COSTFUNCTIONFACTORYTEST_H_
+#define COSTFUNCTIONFACTORYTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidCurveFitting/CostFunctionFactory.h"
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidCurveFitting/ICostFunction.h"
+
+#include <sstream>
+
+using namespace Mantid;
+using namespace Mantid::CurveFitting;
+using namespace Mantid::API;
+
+class CostFunctionFactoryTest_A: public ICostFunction
+{
+  int m_attr;
+public:
+  CostFunctionFactoryTest_A() {}
+
+  std::string name()const {return "fido";}
+
+  double val(const double* yData, const double* inverseError, double* yCal, const int& n) { return 0.0;}
+  void deriv(const double* yData, const double* inverseError, const double* yCal, 
+    const double* jacobian, double* outDerivs, const int& p, const int& n) {}
+
+};
+
+DECLARE_COSTFUNCTION(CostFunctionFactoryTest_A, nedtur);
+
+
+class CostFunctionFactoryTest : public CxxTest::TestSuite
+{
+public:
+  CostFunctionFactoryTest()
+  {
+    Mantid::API::FrameworkManager::Instance();
+  }
+
+  void testCreateFunction()
+  {
+    ICostFunction* cfA = CostFunctionFactory::Instance().createUnwrapped("nedtur");
+    TS_ASSERT(cfA);
+    TS_ASSERT(cfA->name().compare("fido") == 0);
+
+    delete cfA;
+  }
+
+};
+
+#endif /*COSTFUNCTIONFACTORYTEST_H_*/
---
+++ Code/Mantid/CurveFitting/test/FuncMinimizerFactoryTest.h
@@ -0,0 +1,56 @@
+#ifndef FUNCMINIMIZERFACTORYTEST_H_
+#define FUNCMINIMIZERFACTORYTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidCurveFitting/FuncMinimizerFactory.h"
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidCurveFitting/IFuncMinimizer.h"
+#include "MantidCurveFitting/Fit.h"
+#include "MantidCurveFitting/GSLFunctions.h"
+
+#include <sstream>
+
+using namespace Mantid;
+using namespace Mantid::CurveFitting;
+using namespace Mantid::API;
+
+class FuncMinimizerFactoryTest_A: public IFuncMinimizer
+{
+  int m_attr;
+public:
+  FuncMinimizerFactoryTest_A() {}
+
+  /// Overloading base class methods
+  std::string name()const {return "Boevs";}
+  int iterate() {return 1000;}
+  int hasConverged() {return 101;}
+  double costFunctionVal() {return 5.0;}
+  void calCovarianceMatrix(double epsrel, gsl_matrix * covar) {}
+  void initialize(double* X, const double* Y, double *sqrtWeight, const int& nData, const int& nParam, 
+    gsl_vector* startGuess, Fit* fit, const std::string& costFunction) {}
+};
+
+DECLARE_FUNCMINIMIZER(FuncMinimizerFactoryTest_A, nedtur);
+
+
+class FuncMinimizerFactoryTest : public CxxTest::TestSuite
+{
+public:
+  FuncMinimizerFactoryTest()
+  {
+    Mantid::API::FrameworkManager::Instance();
+  }
+
+  void testCreateFunction()
+  {
+    IFuncMinimizer* minimizerA = FuncMinimizerFactory::Instance().createUnwrapped("nedtur");
+    TS_ASSERT(minimizerA);
+    TS_ASSERT(minimizerA->name().compare("Boevs") == 0);
+
+    delete minimizerA;
+  }
+
+};
+
+#endif /*FUNCMINIMIZERFACTORYTEST_H_*/
