5cf4149f73231ebe81d69c700aa18f7f1fcf4080
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2010-07-16 14:29:21 +0000

    Update the detector effic test to include a small test workspace that includes grouped detectors as the original MARI test doesn't test this. Re #1330

--- Code/Mantid/Algorithms/test/DetectorEfficiencyCorTest.h
+++ Code/Mantid/Algorithms/test/DetectorEfficiencyCorTest.h
@@ -6,22 +6,22 @@
 #include "MantidAlgorithms/DetectorEfficiencyCor.h"
 #include "MantidAlgorithms/ConvertUnits.h"
 #include "MantidAlgorithms/SimpleRebin.h"
-#include "MantidDataHandling/GroupDetectors2.h"
 #include "MantidDataHandling/LoadRaw3.h"
 #include "MantidDataHandling/LoadDetectorInfo.h"
 #include "MantidDataObjects/Workspace2D.h"
 #include "MantidKernel/UnitFactory.h"
 #include "MantidGeometry/Instrument/Detector.h"
 #include "MantidGeometry/Instrument/DetectorGroup.h"
+#include "MantidGeometry/Objects/ShapeFactory.h"
 #include "MantidAPI/SpectraDetectorMap.h"
+
+#include "Poco/DOM/DOMParser.h"
+#include "Poco/DOM/Document.h"
+#include "Poco/DOM/Element.h"
 #include "Poco/Path.h"
 #include "Poco/File.h"
-#include <boost/lexical_cast.hpp>
-#include <algorithm>
+
 #include <fstream>
-#include <vector>
-#include <iostream>
-#include <string>
 #include <iomanip>
 
 using namespace Mantid::Kernel;
@@ -48,11 +48,6 @@ public:
     // create a .dat file in the current directory that we'll load later
     writeDatFile();
   }
-
-  void tearDown()
-  {
-    Poco::File(m_DatFile).remove();    
-  }
   
   void testInit()
   {
@@ -64,7 +59,6 @@ public:
     TS_ASSERT_THROWS_NOTHING( grouper.initialize() )
     TS_ASSERT( grouper.isInitialized() )
   }
-
 
   void testFromRaw()
   {
@@ -105,7 +99,42 @@ public:
     AnalysisDataService::Instance().remove(inName);
   }
 
-private:
+  void testDataWithGroupedDetectors()
+  {
+    const std::string wsName = "testInput";
+    makeTestWS(wsName);
+
+    DetectorEfficiencyCor grouper;
+    grouper.setChild(true);
+    TS_ASSERT_THROWS_NOTHING(grouper.initialize());
+    TS_ASSERT(grouper.isInitialized());
+    TS_ASSERT_THROWS_NOTHING(grouper.setPropertyValue("InputWorkspace", wsName));
+    TS_ASSERT_THROWS_NOTHING(grouper.setPropertyValue("OutputWorkspace", wsName));
+    TS_ASSERT_THROWS_NOTHING(grouper.setProperty("IncidentEnergy", 2.1));
+    grouper.execute();
+    TS_ASSERT( grouper.isExecuted() );
+    
+    MatrixWorkspace_sptr result = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve(wsName));
+
+    TS_ASSERT_EQUALS(result->getNumberHistograms(), 10);
+
+    TS_ASSERT_DELTA(result->readY(0).front(), 5.036868, 1e-6);
+    TS_ASSERT_DELTA(result->readY(0).back(), 0.0, 1e-6);
+    //Errors
+    TS_ASSERT_DELTA(result->readE(0).front(), 2.252556, 1e-6);
+    TS_ASSERT_DELTA(result->readE(0).back(), 0.0, 1e-6);
+
+
+    TS_ASSERT_DELTA(result->readY(4)[2], 7.026159, 1e-6);
+    TS_ASSERT_DELTA(result->readY(4).back(), 0.0, 1e-6);
+    //Errors
+    TS_ASSERT_DELTA(result->readE(4)[2], 2.244424, 1e-6);
+    TS_ASSERT_DELTA(result->readE(4).back(), 0.0, 1e-6);
+
+  }
+
+
+
 
   void loadRawFile(std::string WSName, std::string file, bool small_set=false)
   {
@@ -152,6 +181,107 @@ private:
     TS_ASSERT( loader.isExecuted() );
   }
 
+  void tearDown()
+  {
+    Poco::File(m_DatFile).remove();    
+  }
+
+
+  // Set up a small workspace for testing
+  void makeTestWS(std::string WSName)
+  {
+    MatrixWorkspace_sptr space = WorkspaceFactory::Instance().create("Workspace2D", NSpectra, NBins + 1, NBins);
+    space->getAxis(0)->unit() = UnitFactory::Instance().create("DeltaE");
+    Workspace2D_sptr space2D = boost::dynamic_pointer_cast<Workspace2D>(space);
+    
+    Histogram1D::RCtype x,y,e;
+    x.access().resize(NBins+1);
+    y.access().resize(NBins);
+    e.access().resize(NBins);
+    for (int i = 0; i < NBins; ++i)
+    {
+      x.access()[i] = static_cast<double>((1 + i)/100);
+      y.access()[i] = 5 + i;
+      e.access()[i] = std::sqrt(5);
+    }
+    x.access()[NBins] = static_cast<double>(NBins);
+    // Fill a couple of zeros just as a check that it doesn't get changed
+    y.access()[NBins-1] = 0.0;
+    e.access()[NBins-1] = 0.0;
+    
+    int specNums[NSpectra];
+    int detIDs[NSpectra];
+    for (int i=0; i< NSpectra; i++)
+    {
+      space2D->setX(i,x);
+      space2D->setData(i,y,e);
+      space2D->getAxis(1)->spectraNo(i) = i+1;
+      
+      specNums[i] = i+1;
+      specNums[i+1] = i+1;
+      specNums[i+2] = i+1;
+      detIDs[i] = i+1;
+      detIDs[i+1] = i+2;
+      detIDs[i+2] = i+3;
+    }
+    space2D->mutableSpectraMap().populate(specNums, detIDs, 3*NSpectra);
+    
+    
+
+    std::string xmlShape = "<cylinder id=\"shape\"> ";
+    xmlShape +=	"<centre-of-bottom-base x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ; 
+    xmlShape +=	"<axis x=\"0.0\" y=\"1.0\" z=\"0\" /> " ;
+    xmlShape +=	"<radius val=\"0.0127\" /> " ;
+    xmlShape +=	"<height val=\"1\" /> " ;
+    xmlShape +=	"</cylinder>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";  
+    
+    boost::shared_ptr<Object> shape = getObject(xmlShape);
+    space2D->setInstrument(boost::shared_ptr<Instrument>(new Instrument));
+    boost::shared_ptr<Instrument> instrument = space2D->getBaseInstrument();
+    ObjComponent *sample = new ObjComponent("sample", shape, NULL);
+    sample->setPos(0,0,0);
+    instrument->markAsSamplePos(sample);
+    
+
+    ParameterMap &pmap = space2D->instrumentParameters();
+    //Detector info
+    for( int i = 0; i < NSpectra; ++i)
+    {
+      for( int j = 0; j < 3; ++j)
+      {
+	Detector *detector = new Detector("det",shape, NULL);
+	detector->setPos(j*10,i*10,5);
+	detector->setID(i+j+1);
+	pmap.add("double", detector, "3He(atm)", 10.0);
+	pmap.add("double", detector, "wallT(m)", 0.0008);
+	instrument->markAsDetector(detector);
+      }
+    }
+	
+
+    AnalysisDataService::Instance().add(WSName, space2D);
+  }
+
+  boost::shared_ptr<Object> getObject(std::string xmlShape)
+  {
+    std::string shapeXML = "<type name=\"userShape\"> " + xmlShape + " </type>";
+    
+    // Set up the DOM parser and parse xml string
+    Poco::XML::DOMParser pParser;
+    Poco::XML::Document* pDoc;
+    
+    pDoc = pParser.parseString(shapeXML);
+    
+    // Get pointer to root element
+    Poco::XML::Element* pRootElem = pDoc->documentElement();
+    
+    //convert into a Geometry object
+    ShapeFactory sFactory;
+    boost::shared_ptr<Object> shape_sptr = sFactory.createShape(pRootElem);
+    pDoc->release();
+    return shape_sptr;
+  }
 
 
   private:
