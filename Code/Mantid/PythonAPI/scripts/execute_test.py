6d2ac81669559fde9455c79033b41407dc4a945e
Mathieu Doucet <doucetm@ornl.gov>
2010-07-22 13:59:23 +0000

    Now that we know what the problem is we don't need those examples. Adding the file directory to the python path when loading from MantidPlot seems like the solution.  Re #1415

--- Code/Mantid/PythonAPI/scripts/execute_test.py
+++
@@ -1,9 +0,0 @@
-"""
-    When run within Mantiplot, this script should print SUCCESS
-"""
-from TestClass import *
-
-if TestClass.CLASS_DATA_MEMBER==1:
-    print "SUCCESS"
-else: 
-    print "Complete failure"
