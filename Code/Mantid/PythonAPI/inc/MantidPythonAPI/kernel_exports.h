c3daff67b497534faa59635f71afaa0f3bdc706e
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2010-08-12 10:48:20 +0000

    Missed adding a new file on last checkin. Re #1395

---
+++ Code/Mantid/PythonAPI/inc/MantidPythonAPI/kernel_exports.h
@@ -0,0 +1,69 @@
+#ifndef MANTIDPYTHONAPI_KERNEL_EXPORTS_H_
+#define MANTIDPYTHONAPI_KERNEL_EXPORTS_H_
+
+#include <MantidKernel/Exception.h>
+#include <MantidKernel/ConfigService.h>
+#include <MantidKernel/FacilityInfo.h>
+
+namespace Mantid
+{
+  namespace PythonAPI
+  {
+
+    /**
+    * A wrapper for the ConfigService Singleton
+    *
+    * Simple forwards calls onto the real config service. Inheritance cannot be used 
+    * as the constructor is private
+    */
+    struct ConfigServiceWrapper
+    {
+      /**
+      * Retrieve a setting from the ConfigService
+      * @param name The name of the property
+      * @returns The current value of the property
+      */
+      std::string getProperty(const std::string & name) const
+      {
+        return Mantid::Kernel::ConfigService::Instance().getString(name);
+      }
+
+      /**
+      * Update a setting in the ConfigService
+      * @param name The name of the property
+      * @param value The new value of the property
+      */
+      void setProperty(const std::string & name, const std::string & value)
+      {
+        Mantid::Kernel::ConfigService::Instance().setString(name, value);
+      }
+
+      /**
+       * Access the facilities information
+       * @param name The name of a facility. If empty the default facility is returned. (default = "")
+       */
+      Kernel::FacilityInfo facility(const std::string & name = "")
+      {
+        if( name.empty() )
+        {
+          return Mantid::Kernel::ConfigService::Instance().Facility();
+        }
+        else
+        {
+          try
+          {
+            return Mantid::Kernel::ConfigService::Instance().Facility(name);
+          }
+          catch(Kernel::Exception::NotFoundError&)
+          {
+            // Throw a better error to Python users
+            throw std::runtime_error("Facility \"" + name + "\" not defined.") ;
+          }
+        }
+      }
+    };
+
+  }
+}
+
+#endif //MANTIDPYTHONAPI_KERNEL_EXPORTS_H_
