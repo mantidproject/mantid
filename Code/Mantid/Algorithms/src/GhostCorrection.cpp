af133038d2b0bab83a16d9d177659a326e9cc2d8
Janik Zikovsky <zikovskyjl@ornl.gov>
2010-08-17 20:06:40 +0000

    Refs #1478: GhostCorrection algorithm seems to be working well; is parallelized. There may be an issue with focussed no. of spectra not matching.

---
+++ Code/Mantid/Algorithms/src/GhostCorrection.cpp
@@ -0,0 +1,309 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/GhostCorrection.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidAPI/FileProperty.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidDataObjects/Workspace2D.h"
+#include "MantidKernel/ArrayProperty.h"
+#include "MantidKernel/RebinParamsValidator.h"
+#include "MantidKernel/VectorHelper.h"
+#include "MantidKernel/BinaryFile.h"
+#include "MantidAPI/WorkspaceFactory.h"
+
+namespace Mantid
+{
+  namespace Algorithms
+  {
+
+    // Register the class into the algorithm factory
+    DECLARE_ALGORITHM(GhostCorrection)
+
+    using namespace Kernel;
+    using namespace API;
+    using namespace DataObjects;
+    using std::string;
+
+    /** Initialisation method. Declares properties to be used in algorithm.
+    *
+    */
+    void GhostCorrection::init()
+    {
+      nGroups = 0;
+      this->g_log.setName("Algorithms::GhostCorrection");
+
+      //Input workspace must be in dSpacing and be an EventWorkspace
+      API::CompositeValidator<MatrixWorkspace> *wsValidator = new API::CompositeValidator<MatrixWorkspace>;
+      wsValidator->add(new API::WorkspaceUnitValidator<MatrixWorkspace>("dSpacing"));
+      wsValidator->add(new API::RawCountValidator<MatrixWorkspace>);
+      wsValidator->add(new API::EventWorkspaceValidator<MatrixWorkspace>);
+
+      declareProperty(
+        new WorkspaceProperty<>("InputWorkspace", "",Direction::Input, wsValidator),
+        "EventWorkspace from which to make a ghost correction histogram.");
+
+      declareProperty(
+        new WorkspaceProperty<>("OutputWorkspace","",Direction::Output),
+        "The name to give the output workspace; it will be a Workspace2D");
+
+      declareProperty(
+        new ArrayProperty<double>("BinParams", new RebinParamsValidator),
+        "A comma separated list of first bin boundary, width, last bin boundary. Optionally\n"
+        "this can be followed by a comma and more widths and last boundary pairs.\n"
+        "Negative width values indicate logarithmic binning.");
+
+      declareProperty(new FileProperty("GroupingFileName", "", FileProperty::Load, ".cal"),
+          "The name of the CalFile with grouping data" );
+
+      declareProperty(new FileProperty("GhostCorrectionFileName", "", FileProperty::Load, "dat"),
+          "The name of the file containing the ghost correction mapping." );
+
+    }
+
+
+    //---------------------------------------------------------------------------------
+    /** Executes the rebin algorithm
+    *
+    *  @throw runtime_error Thrown if the bin range does not intersect the range of the input workspace
+    */
+    void GhostCorrection::exec()
+    {
+      // Get the input workspace
+      this->inputW = getProperty("InputWorkspace");
+
+      //Now, determine if the input workspace is actually an EventWorkspace
+      EventWorkspace_const_sptr eventW = boost::dynamic_pointer_cast<const EventWorkspace>(inputW);
+      if (eventW == NULL)
+        throw std::runtime_error("Invalid workspace type provided to GhostCorrection. Only EventWorkspaces work with this algorithm.");
+
+      //Load the grouping file
+      this->readGroupingFile( getProperty("GroupingFileName") );
+      if (this->nGroups <= 0)
+        throw std::runtime_error("The # of groups found in the Grouping file is 0.");
+
+      //Make the X axis to bin to.
+      MantidVecPtr XValues_new;
+      // create new output X axis
+      const int numbins = VectorHelper::createAxisFromRebinParams(getProperty("BinParams"), XValues_new.access());
+
+      //Create an output Workspace2D with group # of output spectra
+      MatrixWorkspace_sptr outputW = WorkspaceFactory::Instance().create("Workspace2D", this->nGroups-1, numbins, numbins-1);
+      WorkspaceFactory::Instance().initializeFromParent(inputW, outputW,  true);
+
+      //Set the X bins in the output WS.
+      Workspace2D_sptr outputWS2D = boost::dynamic_pointer_cast<Workspace2D>(outputW);
+      for (int i=0; i < outputWS2D->getNumberHistograms(); i++)
+        outputWS2D->setX(i, XValues_new);
+
+      //Load the ghostmapping file
+      this->loadGhostMap( getProperty("GhostCorrectionFileName") );
+
+
+      //Initialize progress reporting.
+      int numsteps = 0; //count how many steps
+      for (int gr=1; gr < this->nGroups; ++gr)
+        numsteps += this->groupedGhostMaps[gr]->size();
+      numsteps += eventW->getNumberHistograms();
+      Progress prog(this, 0.0, 1.0, numsteps);
+
+      //For parallel processing, it would be safest to pre-sort all events by TOF if not done already
+      PARALLEL_FOR1(eventW)
+      for (int i=0; i < eventW->getNumberHistograms(); i++)
+      {
+        PARALLEL_START_INTERUPT_REGION
+        eventW->getEventListAtWorkspaceIndex(i).sortTof();
+        prog.report();
+        PARALLEL_END_INTERUPT_REGION
+      }
+      PARALLEL_CHECK_INTERUPT_REGION
+
+
+      //Go through the groups, starting at #1!
+      PARALLEL_FOR2(eventW, outputW)
+      for (int gr=1; gr < this->nGroups; ++gr)
+      {
+        PARALLEL_START_INTERUPT_REGION
+
+        //TODO: Convert between group # and workspace index. Sigh.
+        int outputWorkspaceIndex = gr-1;
+
+        //Start by making sure the Y and E values are 0.
+        MantidVec& Y = outputW->dataY(outputWorkspaceIndex);
+        MantidVec& E = outputW->dataE(outputWorkspaceIndex);
+        Y.assign(Y.size(), 0.0);
+        E.assign(E.size(), 0.0);
+
+        //Perform the GhostCorrection
+
+        //Ok, this map has as keys the source workspace indices, and as values
+        GhostSourcesMap * thisGroupsGhostMap = this->groupedGhostMaps[gr];
+        GhostSourcesMap::iterator it;
+        for (it = thisGroupsGhostMap->begin(); it != thisGroupsGhostMap->end(); it++)
+        {
+          //Get the values.
+          int inputWorkspaceIndex = it->first;
+          double weight = it->second;
+          //This is the histogram of the pixel CAUSING the ghost.
+          EventList sourceEvents = eventW->getEventListAtWorkspaceIndex(inputWorkspaceIndex);
+
+          //Histogram it using the binning parameters of the output workspace.
+          MantidVec sourceY;
+          sourceEvents.generateCountsHistogram(*XValues_new, sourceY);
+
+          //Add it, with a weight, to the group's Y histogram
+          for (int i=0; i<numbins-1; i++)
+            Y[i] += sourceY[i] * weight;
+
+          //Report progress
+          prog.report();
+        }
+
+        PARALLEL_END_INTERUPT_REGION
+      }
+      PARALLEL_CHECK_INTERUPT_REGION
+
+
+      // Assign the workspace to the output workspace property
+      setProperty("OutputWorkspace", outputW);
+
+    }
+
+
+    //-----------------------------------------------------------------------------
+    /*
+     * Loads a ghost mapping file from disk. Does a lot of the
+     * work of mapping what ghost pixels should go were.
+     * this->inputW needs to be set first!
+     */
+    void GhostCorrection::loadGhostMap(std::string ghostMapFile)
+    {
+
+      //Open the file
+      BinaryFile<GhostDestinationValue> ghostFile(ghostMapFile);
+
+      //Ghost pixels per input pixel. Should this be a parameter?
+      const int num_ghosts = 16;
+
+      //Load all the ghost corrections
+      std::vector<GhostDestinationValue> * rawMap = ghostFile.loadAll();
+
+      if (rawMap->size() % num_ghosts != 0)
+      {
+        delete rawMap;
+        throw std::runtime_error("The ghost correction file specified is not of the expected size.");
+      }
+
+      //Prepare the groups' ghost sources lists
+      this->groupedGhostMaps.clear();
+      for (int i=0; i < this->nGroups; i++)
+        this->groupedGhostMaps.push_back( new GhostSourcesMap() );
+
+      //Prepare the maps you need
+      IndexToIndexMap * detectorIDToWorkspaceIndexMap = inputW->getDetectorIDToWorkspaceIndexMap();
+
+      size_t numInPixels = rawMap->size() / num_ghosts;
+      size_t fileIndex = 0;
+
+      for (size_t inPixelId = 0; inPixelId < numInPixels; inPixelId++)
+      {
+        //Find the input workspace index corresponding to this input Pixel ID
+        IndexToIndexMap::iterator it;
+        it = detectorIDToWorkspaceIndexMap->find(inPixelId);
+        if (it != detectorIDToWorkspaceIndexMap->end())
+        {
+          //A valid workspace index was found, this one:
+          int inputWorkspaceIndex = it->second;
+//          std::cout << "inputWorkspaceIndex " << inputWorkspaceIndex << " for inPixelId " << inPixelId << "\n";
+
+          for (int g = 0; g < num_ghosts; g++)
+          {
+            //Calculate the index into the file
+            fileIndex = inPixelId * num_ghosts + g;
+
+            //This is where the ghost ends up
+            GhostDestinationValue ghostVal = (*rawMap)[fileIndex];
+            size_t ghostPixelId = ghostVal.pixelId;
+
+            //Don't do anything if the weight is 0
+            if (ghostVal.weight > 0.0)
+            {
+//              std::cout << "ghostPixelId " << ghostPixelId << " and weight " << ghostVal.weight << "\n";
+
+              //Find the group # for this ghost pixel id
+              int ghostGroup = this->udet2group[ghostPixelId];
+
+              //And get the list of sources from it.
+              GhostSourcesMap * thisGroupsGhostMap = this->groupedGhostMaps[ghostGroup];
+
+              //Look if the inputWorkspaceIndex is already in this list
+              GhostSourcesMap::iterator gsmit = thisGroupsGhostMap->find(inputWorkspaceIndex);
+              if (gsmit == thisGroupsGhostMap->end())
+              {
+                //That workspace index was not in the map before.
+                //Add the entry
+                (*thisGroupsGhostMap)[inputWorkspaceIndex] = ghostVal.weight;
+//                std::cout << "setting " << ghostVal.weight << " to index " << inputWorkspaceIndex << ", in group " << ghostGroup << ".\n";
+              }
+              else
+              {
+                //Add this new weight to the old one
+                (*thisGroupsGhostMap)[inputWorkspaceIndex] += ghostVal.weight;
+//                std::cout << "adding " << ghostVal.weight << " to index " << inputWorkspaceIndex << ", in group " << ghostGroup << ".\n";
+              }
+            } //non-zero weight
+
+          } //for each group
+
+        } //(valid WS index)
+      }
+
+      //Free some data.
+      delete rawMap;
+    }
+
+
+    //=============================================================================
+    /** Reads in the file with the grouping information
+     * @param groupingFileName The file that contains the group information
+     * TODO: Abstract this out into its own class, to reuse code better.
+     */
+    void GhostCorrection::readGroupingFile(const std::string& groupingFileName)
+    {
+      std::ifstream grFile(groupingFileName.c_str());
+      if (!grFile.is_open())
+      {
+        g_log.error() << "Unable to open grouping file " << groupingFileName << std::endl;
+        throw Exception::FileError("Error reading .cal file",groupingFileName);
+      }
+
+      int maxGroup = -1;
+
+      udet2group.clear();
+      std::string str;
+      while(getline(grFile,str))
+      {
+        //Comment
+        if (str.empty() || str[0] == '#') continue;
+        std::istringstream istr(str);
+        int n,udet,sel,group;
+        double offset;
+        istr >> n >> udet >> offset >> sel >> group;
+        if ((sel) && (group>0))
+        {
+          udet2group[udet]=group; //Register this udet
+          if (group > maxGroup)
+            maxGroup = group;
+        }
+      }
+      grFile.close();
+
+      //Count the # of groups this way.
+      nGroups = maxGroup+1;
+
+      return;
+    }
+
+
+  } // namespace Algorithm
+} // namespace Mantid
---
+++ Code/Mantid/Algorithms/test/GhostCorrectionTest.h
@@ -0,0 +1,249 @@
+#ifndef GHOSTCORRECTIONTEST_H_
+#define GHOSTCORRECTIONTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAlgorithms/GhostCorrection.h"
+#include "MantidAPI/WorkspaceProperty.h"
+#include "MantidDataObjects/Workspace2D.h"
+#include "MantidDataHandling/LoadEventPreNeXus.h"
+#include "MantidDataHandling/AlignDetectors.h"
+#include "WorkspaceCreationHelper.hh"
+
+using namespace Mantid;
+using namespace Mantid::Kernel;
+using namespace Mantid::DataObjects;
+using namespace Mantid::API;
+using namespace Mantid::Algorithms;
+using namespace Mantid::DataHandling;
+
+
+class GhostCorrectionTest : public CxxTest::TestSuite
+{
+public:
+  double BIN_DELTA;
+  int NUMPIXELS, NUMBINS;
+
+  GhostCorrectionTest()
+  {
+    BIN_DELTA = 2.0;
+    NUMPIXELS = 20;
+    NUMBINS = 50;
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  void testBasics()
+  {
+    //12 bytes per record
+    TS_ASSERT_EQUALS( sizeof(GhostDestinationValue), 12);
+
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  void testBadInputs()
+  {
+    std::string wsName("dummy");
+    EventWorkspace_sptr test_in = WorkspaceCreationHelper::CreateEventWorkspace(NUMBINS, NUMPIXELS);
+    AnalysisDataService::Instance().add(wsName, test_in);
+
+    GhostCorrection gc;
+    TS_ASSERT_THROWS_NOTHING(gc.initialize() );
+
+    //Not d-spacing units.
+    TS_ASSERT_THROWS( gc.setPropertyValue("InputWorkspace",wsName), std::invalid_argument);
+
+    AnalysisDataService::Instance().remove(wsName);
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  void makeFakeEventWorkspace(std::string wsName)
+  {
+    //Make an event workspace with 2 events in each bin.
+    EventWorkspace_sptr test_in = WorkspaceCreationHelper::CreateEventWorkspace(NUMPIXELS, NUMBINS, NUMBINS, 0.0, BIN_DELTA, 2);
+    //Fake a d-spacing unit in the data.
+    test_in->getAxis(0)->unit() =UnitFactory::Instance().create("dSpacing");
+    //Add it to the workspace
+    AnalysisDataService::Instance().add(wsName, test_in);
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  /** Generate a fake ghost correction file **/
+  void makeFakeGhostFile(std::string ghostFileName)
+  {
+    //Open new file
+    std::ofstream * handle = new std::ofstream(ghostFileName.c_str(), std::ios::binary);
+
+    GhostDestinationValue ghost;
+    for (int pix=0; pix < NUMPIXELS; pix++)
+      for (int g=0; g < 16; g++)
+      {
+        ghost.pixelId = g;
+        ghost.weight = g * 1.0;
+        //Write to file
+        handle->write(reinterpret_cast<char*>(&ghost), sizeof(ghost));
+      }
+    handle->close();
+    delete handle;
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  /** Generate a fake ghost correction file **/
+  void makeFakeGroupingFile(std::string groupingFile)
+  {
+    //Open new file, text mode
+    std::ofstream * handle = new std::ofstream(groupingFile.c_str(), std::ios::out);
+    *handle << "# Fake detector file\n";
+    *handle << "# Format: number    UDET    offset    select    group\n";
+
+    //Groups will range from 1 to 5.
+    for (int pix=0; pix < NUMPIXELS; pix++)
+    {
+      *handle << pix << " " << pix << "  0.000   1  " << 1+(pix/4) << "\n";
+    }
+    handle->close();
+    delete handle;
+  }
+
+
+  //--------------------------------------------------------------------------------------------------------
+  void testExecDummy()
+  {
+    std::string wsName("dummy");
+    std::string outwsName("ghost_correction");
+    std::string ghostFileName("FakeGhostMapFile.dat");
+    std::string groupingFile("FakeGroupingFile.cal");
+
+    //Make up event workspace in d-spacing units
+    this->makeFakeEventWorkspace(wsName);
+
+    //Make the ghost file
+    makeFakeGhostFile(ghostFileName);
+    //And grouping file
+    makeFakeGroupingFile(groupingFile);
+
+    //Checks on the workspace
+    EventWorkspace_const_sptr inputW = boost::dynamic_pointer_cast<const EventWorkspace>(AnalysisDataService::Instance().retrieve(wsName));
+    TS_ASSERT_EQUALS( inputW->getNumberHistograms(), NUMPIXELS);
+    IndexToIndexMap * m;
+    m = inputW->getDetectorIDToWorkspaceIndexMap();
+    TS_ASSERT_EQUALS( m->size(), NUMPIXELS);
+    //2 events per bin
+    TS_ASSERT_EQUALS( inputW->dataY(0)[0], 2);
+
+    //----- Now do ghost correction ------
+    GhostCorrection gc;
+    gc.initialize();
+    gc.setPropertyValue("InputWorkspace",wsName);
+    gc.setPropertyValue("OutputWorkspace",outwsName);
+    //Make the same bin parameters
+    std::stringstream params;
+    params << "0.0," << BIN_DELTA << "," << BIN_DELTA*NUMBINS;
+    gc.setPropertyValue("BinParams", params.str());
+    gc.setPropertyValue("GroupingFileName", groupingFile);
+    gc.setPropertyValue("GhostCorrectionFileName", ghostFileName);
+
+    TS_ASSERT(gc.execute());
+    TS_ASSERT(gc.isExecuted());
+
+    //Get the output workspace and check it
+    Workspace2D_sptr outWS = boost::dynamic_pointer_cast<Workspace2D>(AnalysisDataService::Instance().retrieve(outwsName));
+    TS_ASSERT( outWS );
+
+    TS_ASSERT_EQUALS( outWS->getNumberHistograms(), 5 );
+    for (int group=1; group < 6; group++)
+    {
+      int workspaceIndex = group-1;
+
+      //The way the ghost weights are done will give this expected value
+      double expected_value = 0;
+      for (int i= (group-1)*4; i < group*4; i++)
+        expected_value += i;
+      //2 events per input WS bin
+      expected_value *= 2.0;
+      //And all of the 20 pixels add up in the same group - this is the focussing
+      expected_value *= NUMPIXELS;
+      //But group #5 is past the 16 ghost indices, so it is 0
+      if (group==5) expected_value = 0;
+
+      //Get the data
+      MantidVec Y = outWS->dataY(workspaceIndex);
+      TS_ASSERT_EQUALS( Y.size(), NUMBINS ); //Proper size
+      for (int i=0; i<NUMBINS; i++)
+        TS_ASSERT_EQUALS( Y[i], expected_value ); //Proper size
+    }
+
+
+    AnalysisDataService::Instance().remove(wsName);
+    AnalysisDataService::Instance().remove(outwsName);
+  }
+
+
+
+  //--------------------------------------------------------------------------------------------------------
+  void xtestExecPG3()
+  {
+    std::string wsName("pg3_732");
+    std::string outwsName("ghost_correction");
+
+    //----- Load some event data --------
+    Mantid::DataHandling::LoadEventPreNeXus * eventLoader;
+    eventLoader = new LoadEventPreNeXus();
+    eventLoader->initialize();
+    eventLoader->setPropertyValue("EventFilename", "../../../../Test/Data/sns_event_prenexus/PG3_732_neutron_event.dat");
+//    eventLoader->setPropertyValue("MappingFilename", "../../../../Test/Data/sns_event_prenexus/CNCS_TS_2008_08_18.dat");
+    eventLoader->setPropertyValue("OutputWorkspace", wsName);
+    eventLoader->setMaxEventsToLoad(1000000);
+    TS_ASSERT( eventLoader->execute() );
+    std::cout << "Event loading complete.\n";
+
+    std::string groupingFile("../../../../Test/Data/sns_event_prenexus/pg3_mantid_det.cal");
+
+    //----- Align them using fake calib --------
+    Mantid::DataHandling::AlignDetectors align;
+    align.initialize();
+    align.setPropertyValue("InputWorkspace", wsName);
+    align.setPropertyValue("OutputWorkspace", wsName);
+    align.setPropertyValue("CalibrationFile", groupingFile);
+    TS_ASSERT_THROWS_NOTHING( align.execute() );
+    TS_ASSERT( align.isExecuted() );
+    std::cout << "AlignDetectors complete.\n";
+
+    //----- Now do ghost correction ------
+    GhostCorrection gc;
+    gc.initialize();
+    gc.setPropertyValue("InputWorkspace",wsName);
+    gc.setPropertyValue("OutputWorkspace",outwsName);
+    gc.setPropertyValue("BinParams", "0.0, 0.1, 5.0");
+    gc.setPropertyValue("GroupingFileName", groupingFile);
+    gc.setPropertyValue("GhostCorrectionFileName", "../../../../Test/Data/sns_event_prenexus/PG3_D664_ghostmap_2010_03_17.dat");
+
+    TS_ASSERT(gc.execute());
+    TS_ASSERT(gc.isExecuted());
+
+    //Get the output workspace and check it
+    Workspace2D_sptr outWS = boost::dynamic_pointer_cast<Workspace2D>(AnalysisDataService::Instance().retrieve(outwsName));
+    TS_ASSERT( outWS );
+
+    TS_ASSERT_EQUALS( outWS->getNumberHistograms(), 4);
+    MantidVec Y = outWS->dataY(1);
+    TS_ASSERT_EQUALS( Y.size(), 50 ); //Proper size
+    for (int i=0; i<50; i++)
+      std::cout << i << ": " << Y[i] << "\n";
+
+
+    AnalysisDataService::Instance().remove(wsName);
+    AnalysisDataService::Instance().remove(outwsName);
+  }
+
+
+
+private:
+
+
+
+};
+
+
+#endif /* GHOSTCORRECTIONTEST_H_ */
