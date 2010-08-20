7db179e36c8e7b0f49db56d65f8ff9bee44419bf
Stuart Campbell <campbellsi@ornl.gov>
2010-08-20 21:38:48 +0000

    Initial version of monitor loading algorithm. refs #1506

---
+++ Code/Mantid/DataHandling/inc/MantidDataHandling/LoadPreNeXusMonitors.h
@@ -0,0 +1,84 @@
+#ifndef LOADPRENEXUSMONITORS_H_
+#define LOADPRENEXUSMONITORS_H_
+
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/MatrixWorkspace.h"
+
+namespace Mantid
+{
+namespace DataHandling
+{
+/** @class Mantid::DataHandling::LoadPreNeXusMonitors
+
+    A data loading routine for SNS PreNeXus beam monitor (histogram) files.
+
+    @author Stuart Campbell, SNS ORNL
+    @date 20/08/2010
+
+    Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+*/
+class LoadPreNeXusMonitors: public Mantid::API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  LoadPreNeXusMonitors() :
+    Mantid::API::Algorithm()
+  {
+  }
+  /// Virtual destructor
+  virtual ~LoadPreNeXusMonitors()
+  {
+  }
+  /// Algorithm's name
+  virtual const std::string name() const
+  {
+    return "LoadPreNeXusMonitors";
+  }
+  /// Algorithm's version
+  virtual int version() const
+  {
+    return (1);
+  }
+  /// Algorithm's category for identification
+  virtual const std::string category() const
+  {
+    return "DataHandling";
+  }
+
+private:
+  /// Initialisation code
+  void init();
+  ///Execution code
+  void exec();
+
+  /// Number of monitors
+  size_t nMonitors;
+
+  /// Set to true when instrument geometry was loaded.
+  bool instrument_loaded_correctly;
+
+  void runLoadInstrument(const std::string& instrument, API::MatrixWorkspace_sptr localWorkspace);
+
+};
+
+} // namespace DataHandling
+} // namespace Mantid
+
+#endif /*LOADPRENEXUSMONITORS_H_*/
---
+++ Code/Mantid/DataHandling/src/LoadPreNeXusMonitors.cpp
@@ -0,0 +1,327 @@
+#include "MantidDataHandling/LoadPreNeXusMonitors.h"
+#include "MantidAPI/FileProperty.h"
+#include "MantidKernel/ConfigService.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidAPI/SpectraDetectorMap.h"
+#include "MantidKernel/BinaryFile.h"
+
+#include "Poco/Path.h"
+#include "Poco/File.h"
+#include "Poco/DOM/DOMParser.h"
+#include "Poco/DOM/Document.h"
+#include "Poco/DOM/Element.h"
+#include "Poco/DOM/NodeIterator.h"
+#include "Poco/DOM/NodeFilter.h"
+#include "Poco/DOM/NodeList.h"
+#include "Poco/DOM/AutoPtr.h"
+#include "Poco/SAX/InputSource.h"
+
+#include "boost/lexical_cast.hpp"
+#include <boost/shared_array.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <iterator>
+
+
+namespace Mantid
+{
+namespace DataHandling
+{
+
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM( LoadPreNeXusMonitors)
+
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+using namespace Mantid::Geometry;
+
+// Some constants for property names
+static const std::string RUNINFO_FILENAME("RunInfoFilename");
+static const std::string WORKSPACE_OUT("OutputWorkspace");
+
+// A reference to the logger is provided by the base class, it is called g_log.
+// It is used to print out information, warning and error messages
+
+void LoadPreNeXusMonitors::init()
+{
+
+  // Reset the logger's name
+  this->g_log.setName("DataHandling::LoadPreNeXusMonitors");
+
+  // Filename for the runinfo file.
+  declareProperty(new FileProperty(RUNINFO_FILENAME, "", FileProperty::Load, "_runinfo.xml"));
+
+  // The output workspace
+  declareProperty(new WorkspaceProperty<MatrixWorkspace> (WORKSPACE_OUT, "", Direction::Output));
+
+  // Make sure things are initialised.
+  nMonitors = 0;
+
+}
+
+void LoadPreNeXusMonitors::exec()
+{
+  // time of flight channel parameters
+  double_t tmin = -1;
+  double_t tmax = -1;
+  double_t tstep = -1;
+  int tchannels = -1;
+  std::string instrumentName;
+
+  // Vectors to store monitor parameters
+  std::vector < std::string > monitorFilenames;
+  std::vector < std::string > monitorBinMode;
+  std::vector < size_t > monitorDims;
+
+  // Get the Runinfo filename from the property
+  std::string runinfo_filename = this->getPropertyValue(RUNINFO_FILENAME);
+
+  // TODO: Extract the directory that the runinfo file is in.
+  // Create a Poco Path object for runinfo filename
+  Poco::Path runinfoPath(runinfo_filename, Poco::Path::PATH_GUESS);
+  // Now lets get the directory
+  Poco::Path dirPath(runinfoPath.parent());
+
+  this->g_log.information("Monitor File Dir: " + dirPath.toString());
+
+  // Some XML parsing magic...
+  std::ifstream in(runinfo_filename.c_str());
+  Poco::XML::InputSource src(in);
+
+  Poco::XML::DOMParser parser;
+  Poco::AutoPtr<Poco::XML::Document> pDoc = parser.parse(&src);
+
+  Poco::XML::NodeIterator it(pDoc, Poco::XML::NodeFilter::SHOW_ELEMENT);
+  Poco::XML::Node* pNode = it.nextNode();
+  while (pNode)
+  {
+
+    // Get the beamline name.
+    if (pNode->nodeName() == "RunInfo")
+    {
+      Poco::XML::Element* pRunInfoElement = static_cast<Poco::XML::Element*> (pNode);
+      instrumentName = pRunInfoElement->getAttribute("instrument");
+    }
+
+    // Look for the section that contains the binning parameters.
+    // This will end up with using the binning for the last monitor that is found
+    // TODO: Modify to store (and use) the TCBs for each monitor separately.
+    if (pNode->nodeName() == "BeamMonitorInfo")
+    {
+      // Increment the number of monitors we've found
+      ++nMonitors;
+
+      Poco::XML::Element* pE = static_cast<Poco::XML::Element*> (pNode);
+      g_log.information() << "Beam Monitor " << pE->getAttribute("id") << std::endl;
+      g_log.information() << "\tname: " << pE->getAttribute("name") << std::endl;
+      g_log.information() << "\tdescription: " << pE->getAttribute("description") << std::endl;
+
+      // Now lets get the tof binning settings
+      Poco::XML::Element* pTimeChannels = pE->getChildElement("NumTimeChannels");
+      tmin = boost::lexical_cast<double_t>(pTimeChannels->getAttribute("startbin"));
+      tmax = boost::lexical_cast<double_t>(pTimeChannels->getAttribute("endbin"));
+      tstep = boost::lexical_cast<double_t>(pTimeChannels->getAttribute("width"));
+
+    }
+
+    // Look for the 'DataList' node to get the monitor dims.
+    // TODO: Again we will only use the mast monitor value.
+    if (pNode->nodeName() == "DataList")
+    {
+      // Get a list of the child elements
+      Poco::XML::NodeList* pDataListChildren = pNode->childNodes();
+      for (size_t i = 0; i < pDataListChildren->length(); ++i)
+      {
+        // We only care about monitors
+        if (pDataListChildren->item(i)->nodeName() == "monitor")
+        {
+          monitorFilenames.push_back(
+              static_cast<Poco::XML::Element*> (pDataListChildren->item(i))->getAttribute("name"));
+        }
+      }
+      // Release the NodeList
+      pDataListChildren->release();
+
+    }
+
+    // Get the size of the files
+    if (pNode->nodeName() == "FileFormats")
+    {
+      // Get a list of the child elements
+      Poco::XML::NodeList* pDataListChildren = pNode->childNodes();
+      for (size_t i = 0; i < pDataListChildren->length(); ++i)
+      {
+        // We only care about monitors
+        if (pDataListChildren->item(i)->nodeName() == "monitor")
+        {
+          std::string dims =
+              static_cast<Poco::XML::Element*> (pDataListChildren->item(i))->getAttribute("dims");
+          tchannels = boost::lexical_cast<size_t>(dims);
+        }
+      }
+      // Release the NodeList
+      pDataListChildren->release();
+    }
+
+    pNode = it.nextNode();
+    //  std::cout << pNode->nodeName() << ":" << pNode->getNodeValue() << std::endl;
+  }
+
+  g_log.information() << "Found " << nMonitors << " beam monitors." << std::endl;
+
+  //tchannels = static_cast<size_t> ((tmax - tmin) / tstep);
+
+  std::cout << "tchannels = " << tchannels << std::endl;
+
+  // Now lets create the time of flight array.
+  MantidVec time_bins(tchannels + 1);
+  for (int i = 0; i < tchannels + 2; ++i)
+  {
+    time_bins[i] = tmin + (i) * tstep;
+  }
+
+  std::cout << "Finished generating TCBs." << std::endl;
+
+  std::cout << "Making fake data..." << std::endl;
+
+  // FIXME: temp hack to get some counts into an array!
+  MantidVec counts(tchannels);
+  MantidVec error(tchannels);
+  for (int i = 0; i < tchannels + 1; ++i)
+  {
+    counts[i] = i;
+    error[i] = sqrt(i);
+  }
+
+  std::cout << "Finished making fake data..." << std::endl;
+
+  std::cout << "Creating workspace..." << std::endl;
+  // Create the new workspace
+  MatrixWorkspace_sptr localWorkspace = WorkspaceFactory::Instance().create("Workspace2D", nMonitors,
+      tchannels + 1, tchannels);
+
+  std::cout << "Finished!" << std::endl;
+
+  // a temporary place to put the spectra/detector numbers
+  boost::shared_array<int> spectra_numbers(new int[nMonitors]);
+
+  std::vector<uint32_t> buffer;
+
+  for (size_t i = 0; i < nMonitors; i++)
+  {
+    // Now lets actually read the monitor files..
+    Poco::Path pMonitorFilename(dirPath, monitorFilenames[i]);
+
+    //Kernel::BinaryFile<uint32_t> monitorFile();
+    Kernel::BinaryFile<uint32_t> monitorFile(pMonitorFilename.toString());
+    monitorFile.loadAllInto(buffer);
+
+    MantidVec intensity(buffer.begin(), buffer.end());
+
+   // std::ifstream fileHandle(pMonitorFilename.toString().c_str(), std::ios::in | std::ios::binary);
+   // if (!(fileHandle.is_open()))
+   // {
+   //   g_log.error() << "Cannot open monitor file: " << monitorFilenames[i] << std::endl;
+   // }
+
+    // Make sure we are at the beginning
+    //fileHandle.seekg(std::ios::beg);
+
+  //  std::vector<double> monitorCounts;
+    //    std::istream_iterator<double> numbers(fileHandle);
+    //    std::copy(numbers, std::istream_iterator<double>(), counts.begin());
+
+    // Actually read the data
+    //fileHandle.read(reinterpret_cast<char *> (&counts[0]), tchannels * sizeof(uint32_t));
+
+    for (int j = 46000; j < 46050; j++)
+    {
+      std::cout << intensity[j] << std::endl;
+    }
+
+//    fileHandle.close();
+
+    //    counts.assign(buffer, buffer+sizeof(buffer));
+
+    localWorkspace->dataX(i) = time_bins;
+    localWorkspace->dataY(i) = intensity;
+    localWorkspace->dataE(i) = error;
+    // Just have spectrum number be the same as the monitor number but -ve.
+    spectra_numbers[i] = -(i + 1);
+    localWorkspace->getAxis(1)->spectraNo(i) = -(i + 1);
+  }
+
+  std::cout << "Setting Units" << std::endl;
+
+  // Set the units
+  localWorkspace->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
+  localWorkspace->setYUnit("Counts");
+  // TODO localWorkspace->setTitle(title);
+
+  std::cout << "Loading instrument" << std::endl;
+
+  // Actually load the instrument
+  this->runLoadInstrument(instrumentName, localWorkspace);
+
+  // Populate the Spectra Map
+  localWorkspace->mutableSpectraMap().populate(spectra_numbers.get(), spectra_numbers.get(),
+      static_cast<int> (nMonitors));
+
+  // Set the property
+  setProperty("OutputWorkspace", localWorkspace);
+}
+
+//-----------------------------------------------------------------------------
+/** Load the instrument geometry File
+ *  @param instrument instrument name.
+ *  @param localWorkspace MatrixWorkspace in which to put the instrument geometry
+ */
+void LoadPreNeXusMonitors::runLoadInstrument(const std::string &instrument,
+    MatrixWorkspace_sptr localWorkspace)
+{
+  std::string filename = Mantid::Kernel::ConfigService::Instance().getInstrumentFilename(instrument);
+  if (filename.empty())
+    return;
+  if (!Poco::File(filename).exists())
+    return;
+
+  // do the actual work
+  IAlgorithm_sptr loadInst = createSubAlgorithm("LoadInstrument");
+
+  // Now execute the sub-algorithm. Catch and log any error, but don't stop.
+  bool executionSuccessful(true);
+  try
+  {
+    loadInst->setPropertyValue("Filename", filename);
+    loadInst->setProperty<MatrixWorkspace_sptr> ("Workspace", localWorkspace);
+    loadInst->execute();
+
+    // Populate the instrument parameters in this workspace - this works around a bug
+    localWorkspace->populateInstrumentParameters();
+  } catch (std::invalid_argument& e)
+  {
+    g_log.information() << "Invalid argument to LoadInstrument sub-algorithm : " << e.what()
+        << std::endl;
+    executionSuccessful = false;
+  } catch (std::runtime_error& e)
+  {
+    g_log.information() << "Unable to successfully run LoadInstrument sub-algorithm : " << e.what()
+        << std::endl;
+    executionSuccessful = false;
+  }
+
+  // If loading instrument definition file fails
+  if (!executionSuccessful)
+  {
+    g_log.error() << "Error loading Instrument definition file\n";
+  }
+  else
+  {
+    this->instrument_loaded_correctly = true;
+  }
+}
+
+} // namespace DataHandling
+} // namespace Mantid
