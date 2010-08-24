1f0d3c920ca2ecda0b027f282989fdd9a45dc72c
Mathieu Doucet <doucetm@ornl.gov>
2010-08-24 15:01:43 +0000

    Simple unit test for numpy integration. Re #948

---
+++ Code/Mantid/PythonAPI/scripts/utest_numpy.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+from mantidsimple import *
+
+# import Numpy to check that it's on the system
+import numpy
+
+TEST_DIR = "../../../Test/Data/"
+class TestCommands(unittest.TestCase):
+    """
+        Simple test to check the numpy integration
+    """
+    
+    def setUp(self):
+        pass        
+        
+    def test_array_output(self):
+        filepath = os.path.join(TEST_DIR, "LOQ_CANSAS1D.xml")
+        loader = LoadCanSAS1D(filepath, "test")
+        
+        # Get the Y array
+        x = mtd["test"].dataY(0)
+        
+        # Check that we got an ndarray
+        self.assertEqual(x.__class__, numpy.ndarray)
+        
+        # Some sanity check to verify that we have the right file
+        self.assertEqual(len(x), 102)
+        self.assertEqual(x[20], 2895.0)
+        
+        # Try modifying the data
+        x[0] = 10.0
+        
+        # Try to get it back again
+        y = mtd["test"].dataY(0)
+        self.assertEqual(y[0], 10.0)
+
+if __name__ == '__main__':
+    unittest.main()
