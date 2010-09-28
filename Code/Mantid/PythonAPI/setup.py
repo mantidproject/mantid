f8762f0909c703acc4b3834fbd9093a552f898ac
Mathieu Doucet <doucetm@ornl.gov>
2010-09-28 20:05:33 +0000

    Added installer that allows importing mantid into vanilla python. Set MANTIDPATH first. Re #1594

---
+++ Code/Mantid/PythonAPI/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+  Python install script for Mantid. Assumes that Mantid has been properly compiled.
+  Users will need to have their environment variables set properly:
+
+    MANTIDPATH should be set to the Mantid release directory
+    LD_LIBRARY_PATH should also contain the Mantid release directory
+"""
+from distutils.core import setup
+import os
+
+# Python module for Mantid has a different name on Windows
+if os.name == 'nt':
+    python_lib = "MantidPythonAPI.pyd"
+else:
+    python_lib = "libMantidPythonAPI.so"
+
+setup(name='Mantid',
+      version='1.0',
+      description='Python bindings for Mantid',
+      url='http://www.mantidproject.org',
+      packages=['MantidFramework'],
+      package_dir={'MantidFramework' : ''},
+      package_data={'MantidFramework' : [python_lib]},
+     )
