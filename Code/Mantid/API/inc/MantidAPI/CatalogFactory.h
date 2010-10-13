be9609174ae618695ae04487bb91efd66029bb0f
Sofia Antony <sofia.antony@stfc.ac.uk>
2010-10-13 10:09:32 +0000

    re#1699 - Added Catalog factory class to create different catalogs

---
+++ Code/Mantid/API/inc/MantidAPI/CatalogFactory.h
@@ -0,0 +1,109 @@
+#ifndef MANTID_API_CATALOGFACTORYIMPL_H_
+#define MANTID_API_CATALOGFACTORYIMPL_H_
+
+/* Used to register Catalog classes into the factory. creates a global object in an
+ * anonymous namespace. The object itself does nothing, but the comma operator
+ * is used in the call to its constructor to effect a call to the factory's
+ * subscribe method.
+ *
+ * The second operation that this macro performs is to provide the definition
+ * of the CatalogID method for the concrete Catalog.
+ */
+#define DECLARE_CATALOG(classname) \
+  namespace { \
+    Mantid::Kernel::RegistrationHelper register_alg_##classname( \
+       ((Mantid::API::CatalogFactory::Instance().subscribe<classname>(#classname)) \
+       , 0)); \
+  } 
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/DllExport.h"
+#include "MantidKernel/DynamicFactory.h"
+#include "MantidKernel/SingletonHolder.h"
+
+namespace Mantid
+{
+//----------------------------------------------------------------------
+// Forward declaration
+//----------------------------------------------------------------------
+namespace kernel
+{
+  class Logger;
+}
+namespace API
+{
+
+//----------------------------------------------------------------------
+// Forward declaration
+//----------------------------------------------------------------------
+
+class ICatalog;
+
+/** Creates instances of concrete Catalog.
+    The factory is a singleton that hands out shared pointers to the base Catalog class.
+    It overrides the base class DynamicFactory::create method so that only a single
+    instance of a given Catalog is ever created, and a pointer to that same instance
+    is passed out each time the Catalog is requested.
+
+    @author Sofia Antony
+    @date 01/10/2010
+
+    Copyright &copy; 2008 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class EXPORT_OPT_MANTID_API CatalogFactoryImpl : public Kernel::DynamicFactory<ICatalog>
+{
+public:
+	/// create an instance of the catalog specified by the calssName
+  virtual boost::shared_ptr<ICatalog> create(const std::string& className) const;
+
+private:
+  friend struct Kernel::CreateUsingNew<CatalogFactoryImpl>;
+
+  /// Private Constructor for singleton class
+  CatalogFactoryImpl();
+  /// Private copy constructor 
+  CatalogFactoryImpl(const CatalogFactoryImpl&);
+  /// Private assignment operator 
+  CatalogFactoryImpl& operator = (const CatalogFactoryImpl&);
+  ///Private Destructor
+  virtual ~CatalogFactoryImpl();
+
+  /// Stores pointers to already created Catalog instances, with their name as the key
+  mutable std::map< std::string, boost::shared_ptr<ICatalog> > m_createdCatalogs;
+
+  /// Reference to the logger class
+  Kernel::Logger& m_log;
+};
+
+///Forward declaration of a specialisation of SingletonHolder for AlgorithmFactoryImpl (needed for dllexport/dllimport) .
+#ifdef _WIN32
+// this breaks new namespace declaraion rules; need to find a better fix
+  template class EXPORT_OPT_MANTID_API Mantid::Kernel::SingletonHolder<CatalogFactoryImpl>;
+#endif /* _WIN32 */
+/// The specialisation of the SingletonHolder class that holds the CatalogFactory
+typedef Mantid::Kernel::SingletonHolder<CatalogFactoryImpl> CatalogFactory;
+
+} // namespace API
+} // namespace Mantid
+
+#endif /*MANTID_API_CatalogFACTORYIMPL_H_*/
---
+++ Code/Mantid/API/src/CatalogFactory.cpp
@@ -0,0 +1,42 @@
+#include "MantidAPI/CatalogFactory.h"
+#include "MantidKernel/Logger.h"
+
+namespace Mantid
+{
+namespace API
+{
+
+/// Constructor
+CatalogFactoryImpl::CatalogFactoryImpl() :
+  DynamicFactory<ICatalog>(), m_createdCatalogs(), m_log(Kernel::Logger::get("CatalogFactory"))
+{
+}
+
+ /// Destructor
+CatalogFactoryImpl::~CatalogFactoryImpl()
+{
+}
+
+/** Returns an instance of the class with the given name. Overrides the base class method.
+ *  If an instance already exists, a pointer to it is returned, otherwise
+ *  a new instance is created by the DynamicFactory::create method.
+ *  @param className The name of the class to be created
+ *  @return A shared pointer to the instance of the requested unit
+ */
+boost::shared_ptr<ICatalog> CatalogFactoryImpl::create(const std::string& className) const
+{
+  std::map< std::string, boost::shared_ptr<ICatalog> >::const_iterator it = m_createdCatalogs.find(className);
+  if ( it != m_createdCatalogs.end() )
+  {
+    // If an instance has previously been created, just return a pointer to it
+    return it->second;
+  }
+  else
+  {
+    // Otherwise create & return a new instance and store the pointer in the internal map for next time
+    return m_createdCatalogs[className] = DynamicFactory<API::ICatalog>::create(className);
+  }
+}
+
+} // namespace Kernel
+} // namespace Mantid
