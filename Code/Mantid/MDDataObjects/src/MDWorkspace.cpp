17a8e14e685c0f90b626cbb77472b27d5b38bd4f
Alex Buts <alex.buts@stfc.ac.uk>
2010-10-18 13:37:42 +0000

    refs #1637 missing piece of the code has been added

---
+++ Code/Mantid/MDDataObjects/src/MDWorkspace.cpp
@@ -0,0 +1,13 @@
+#include "MDDataObjects/stdafx.h"
+#include "MDDataObjects/MDWorkspace.h"
+
+
+DECLARE_MD_WORKSPACE(MDWorkspace)
+
+namespace Mantid{
+    namespace MDDataObjects{
+        using namespace Mantid::Kernel;
+
+
+}
+}
