1ae8417fc0ac9f66f618441583b06349a44e8257
Mathieu Doucet <doucetm@ornl.gov>
2010-10-21 18:17:10 +0000

    Modified SANS UI start script for new Interfaces menu. Re #1736

--- Code/Mantid/PythonAPI/scripts/HFIR_Reduction.py
+++ Code/Mantid/PythonAPI/scripts/HFIR_Reduction.py
@@ -1,7 +1,8 @@
 """
     Script used to start the HFIR SANS reduction gui from Mantidplot
 """
-from reduction_gui.reduction_application import ReductionGUI
+#from reduction_gui.reduction_application import ReductionGUI
+from Interface.reduction_application import ReductionGUI
 from PyQt4 import QtCore, uic
 
 reducer = ReductionGUI()
