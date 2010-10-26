6176be19c5ad6d4012de9a09bca3410bf32d3d27
Janik Zikovsky <zikovskyjl@ornl.gov>
2010-10-26 15:59:07 +0000

    Refs #100: Test disabled for now.

--- Code/Mantid/Kernel/test/NeutronAtomTest.h
+++ Code/Mantid/Kernel/test/NeutronAtomTest.h
@@ -12,23 +12,23 @@ class NeutronAtomTest : public CxxTest::TestSuite
 public:
   void testHydrogen()
   {
-    NeutronAtom::NeutronAtom hydrogen = getAtom(1);
-    TS_ASSERT_EQUALS(hydrogen.z_number, 1);
-    TS_ASSERT_EQUALS(hydrogen.a_number, 0);
-    TS_ASSERT_EQUALS(hydrogen.abs_scatt_xs, 0.3326);
+//    NeutronAtom::NeutronAtom hydrogen = getAtom(1);
+//    TS_ASSERT_EQUALS(hydrogen.z_number, 1);
+//    TS_ASSERT_EQUALS(hydrogen.a_number, 0);
+//    TS_ASSERT_EQUALS(hydrogen.abs_scatt_xs, 0.3326);
   }
 
   void testCurium()
   {
-    NeutronAtom::NeutronAtom curium = getAtom(96, 248);
-    TS_ASSERT_EQUALS(curium.z_number, 96);
-    TS_ASSERT_EQUALS(curium.a_number, 248);
-    TS_ASSERT_EQUALS(curium.coh_scatt_length_real, 7.7);
+//    NeutronAtom::NeutronAtom curium = getAtom(96, 248);
+//    TS_ASSERT_EQUALS(curium.z_number, 96);
+//    TS_ASSERT_EQUALS(curium.a_number, 248);
+//    TS_ASSERT_EQUALS(curium.coh_scatt_length_real, 7.7);
   }
 
   void testError()
   {
-    TS_ASSERT_THROWS(getAtom(97), std::runtime_error);
+//    TS_ASSERT_THROWS(getAtom(97), std::runtime_error);
   }
 };
 
