e0438ae2a2796585d12612c1348ad74ecefeb7bb
Russell Taylor <taylorrj@ornl.gov>
2010-10-28 15:06:44 +0000

    It's just been noticed that paths to a couple of libraries were still wrong. Re #1602.

--- Code/MacInstaller/deploy.sh
+++ Code/MacInstaller/deploy.sh
@@ -132,7 +132,7 @@ PLUGINLIBS='MantidPlot.app/plugins/*.dylib'
 update_lib_paths "$PLUGINLIBS" "$MACOSLIBS" "$QTLIBS" ".."
 PLUGINSLIBS='MantidPlot.app/plugins/qtplugins/mantid/*.dylib'
 update_lib_paths "$PLUGINSLIBS" "$MACOSLIBS" "$QTLIBS" "../../.."
-install_name_tool -change libQtPropertyBrowser.1.dylib @loader_path/../../../Frameworks/libQtPropertyBrowser.1.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomInterfaces.dylib
-install_name_tool -change libQtPropertyBrowser.1.dylib @loader_path/../../../Frameworks/libQtPropertyBrowser.1.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomDialogs.dylib
-install_name_tool -change libqwt.5.dylib @loader_path/../../../Frameworks/libqwt.5.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomInterfaces.dylib
-install_name_tool -change libqwt.5.dylib @loader_path/../../../Frameworks/libqwt.5.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomDialogs.dylib
+install_name_tool -change libQtPropertyBrowser.1.dylib @loader_path/../../../Contents/Frameworks/libQtPropertyBrowser.1.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomInterfaces.dylib
+install_name_tool -change libQtPropertyBrowser.1.dylib @loader_path/../../../Contents/Frameworks/libQtPropertyBrowser.1.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomDialogs.dylib
+install_name_tool -change libqwt.5.dylib @loader_path/../../../Contents/Frameworks/libqwt.5.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomInterfaces.dylib
+install_name_tool -change libqwt.5.dylib @loader_path/../../../Contents/Frameworks/libqwt.5.dylib MantidPlot.app/plugins/qtplugins/mantid/libMantidQtCustomDialogs.dylib
