054de9b1a91119c3731222f100a4902556f34140
Steve Williams <stephen.williams@stfc.ac.uk>
2010-11-12 12:05:50 +0000

    PythonRunner always the runPythonCode() method to be called from the different MantidPlot UI elements that contain one of these objects re #1747

---
+++ Code/qtiplot/MantidQt/API/inc/PythonRunner.h
@@ -0,0 +1,59 @@
+#ifndef MANTIDQTAPI_PYTHONRUNNER_H_
+#define MANTIDQTAPI_PYTHONRUNNER_H_
+
+#include <QObject>
+#include "DllOption.h"
+#include <QString>
+
+/** The ase class from which mantid custom widgets are derived it contains
+*  some useful functions
+*/
+namespace MantidQt
+{
+  namespace API
+  {
+    /** 
+    This is the base class all customised user interfaces that do not wish to be tied
+    to a specific Mantid algorithm but rather customised for user's requirements
+
+    @author Steve Williams, Rutherford Appleton Laboratory
+    @date 10/11/2010
+
+    Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>    
+    */
+    class EXPORT_OPT_MANTIDQT_API PythonRunner : public virtual QObject
+    {
+    Q_OBJECT
+
+    public:
+      /// Default constructor
+      PythonRunner() : QObject(){}
+
+      /// Run python code
+      QString runPythonCode(const QString & code, bool no_output = false);
+
+    signals:
+      void runAsPythonScript(const QString& code);
+    };
+  }
+}
+
+#endif //MANTIDQTAPI_PYTHONRUNNER_H_
---
+++ Code/qtiplot/MantidQt/API/src/PythonRunner.cpp
@@ -0,0 +1,45 @@
+#include "MantidQtAPI/PythonRunner.h"
+#include <QTemporaryFile>
+#include <QDir>
+#include <QTextStream>
+
+using namespace MantidQt::API;
+
+/** Run a piece of python code and return any output that it writes to stdout
+*  @param code the Python commands to execute
+*  @param no_output if set to true this method returns an empty string, if false it returns the output from any Python print statements
+*  @return output from Python print statements unless no_output is false
+*/
+QString PythonRunner::runPythonCode(const QString & code, bool no_output)
+{
+  if( no_output )
+  {
+    emit runAsPythonScript(code);
+    return QString();
+  }
+
+  // Otherwise we need to gather the information from stdout. This is achieved by redirecting the stdout stream
+  // to a temproary file and then reading its contents
+  // A QTemporaryFile object is used since the file is automatically deleted when the object goes out of scope
+  QTemporaryFile tmp_file;
+  if( !tmp_file.open() )
+  {
+    throw std::runtime_error("An error occurred opening a temporary file in " + QDir::tempPath().toStdString());
+  }
+  //The file name is only valid when the file is open
+   QString tmpstring = tmp_file.fileName();
+   tmp_file.close();
+   QString code_to_run = "import sys; sys.stdout = open('" + tmpstring + "', 'w')\n" + code;
+
+   emit runAsPythonScript(code_to_run);
+
+   //Now get the output
+   tmp_file.open();
+   QTextStream stream(&tmp_file);
+   tmpstring.clear();
+   while( !stream.atEnd() )
+   {
+     tmpstring.append(stream.readLine().trimmed() + "\n");
+   }
+   return tmpstring;
+}
