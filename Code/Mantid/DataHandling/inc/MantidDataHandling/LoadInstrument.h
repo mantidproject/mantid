143676c0c7ba5f2ab6ddce0631d7aa4ffaf4a9c6
Michael Reuter <reuterma@ornl.gov>
2010-11-16 19:00:07 +0000

    Fixing doxygen errors. This refs #100.

--- Code/Mantid/DataHandling/inc/MantidDataHandling/LoadInstrument.h
+++ Code/Mantid/DataHandling/inc/MantidDataHandling/LoadInstrument.h
@@ -211,8 +211,8 @@ namespace Mantid
       /// get name of location element
       std::string getNameOfLocationElement(Poco::XML::Element* pElem);
 
-	  /// Calculate the position of comp relative to its parent from info provided by <location> element
-	  Geometry::V3D getRelativeTranslation(const Geometry::IComponent* comp, const Poco::XML::Element* pElem);
+      /// Calculate the position of comp relative to its parent from info provided by \verbatim <location> \endverbatim element
+      Geometry::V3D getRelativeTranslation(const Geometry::IComponent* comp, const Poco::XML::Element* pElem);
     };
 
   } // namespace DataHandling
