2d01a2992a693f85b994603b1cddc80662633d94
Alex Buts <alex.buts@stfc.ac.uk>
2010-11-17 20:31:01 +0000

    refs #1637 MDDataPoint (MD-pixel) and test for it added to MDDataObjects

--- Code/Mantid/MDDataObjects/inc/MDDataObjects/MDDataPoints.h
+++ Code/Mantid/MDDataObjects/inc/MDDataObjects/MDDataPoints.h
@@ -31,22 +31,97 @@
 
 namespace Mantid{
     namespace MDDataObjects{
-      /// path-through class which provide transformation of the data from pixel buffer format to data fields format;
+     /** 
+      * path-through class which provide transformation of the data from pixel buffer format to the data fields format; 
+      * It is hard-coded in the class that the run indexes and the detector indexes are present in the data and are located 
+      * after two double sized image fields namely signal and error. All other types of data e.g. events data should be processed
+      * using similar specialized class
+      *
+      * CODED FOR LITTLE ENDIAN -> big endian is not supported at the moment
+      */
       template<class T>
       class MDDataPoint{
       public:
-        MDDataPoint(const char * buf, const std::vector<unsigned int> &dim_field_lengths,const std::vector<unsigned int> &img_field_length):
-            pBuffer(buf)
+        MDDataPoint(char * buf, unsigned int n_dimensions,unsigned int nSignals0,unsigned int nIntFields):
+            pDataBuffer(buf),
+            nDims(n_dimensions),
+            nIndexes(nIntFields),
+            nSignals(nSignals0)
         {
-           this->field_lengths.reserve(dim_field_lengths.size()+img_field_length.size());
+          pWorkingBuf = new uint16_t[nIndexes];
+          // arrange array for all data fields length-es which can be present in the data structure and set its initial values to sizeof(T);
+           this->field_lengths.assign(n_dimensions+nSignals+nIntFields,sizeof(T));
+           unsigned int i;
+           // set field sizes of signals to size of double;
+           for(i=n_dimensions;i<n_dimensions+nSignals;i++){
+             this->field_lengths[i]=sizeof(double);
+           }
+           // set field sizes of all integer indicators to 2; Some fields will be combined in a partucular way
+           for(i=n_dimensions+nSignals;i<n_dimensions+nSignals+nIntFields;i++){
+             this->field_lengths[i]=2;
+           }
+          // allocate and calculate the field locations
+          unsigned int n_fields      = n_dimensions+nSignals+nIntFields;
+          PixIndex                   = n_dimensions+nSignals;
+          field_loc                  = new unsigned int[n_fields];
+
+          field_loc[0] = 0;
+          for(unsigned int i=1;i<n_fields;i++){
+             field_loc[i]=field_loc[i-1]+field_lengths[i-1];
+          }
+        // set up specific pointers for fast accessors;
+          MDPointStride= field_loc[n_fields-1]+field_lengths[n_fields-1];
+          pSignal      = field_loc[n_dimensions];
+          pError       = field_loc[n_dimensions+1];
+          pPixIndex    = field_loc[PixIndex];
+  
+
         }
-        ~MDDataPoint(void);
-        T getDataField(unsigned int nField,size_t n_point)const{return (T)(*(pBuffer+n_point*MDPointStride+field_locations[nField]));}
+        // Accessors:;
+        T getDataField(unsigned int nField,size_t n_point)const{return *(reinterpret_cast<T *>(pDataBuffer+n_point*MDPointStride+field_loc[nField]));}
+        double getSignal(size_t n_point)const{return   *(reinterpret_cast<double *>(pDataBuffer+ n_point*MDPointStride+pSignal));}
+        double getError  (size_t n_point)const{return *(reinterpret_cast<double *>(pDataBuffer+ n_point*MDPointStride +pError));}
+        uint16_t getIndex(unsigned int nf,size_t n_point)const{return *(reinterpret_cast<uint16_t *>(pDataBuffer+n_point*MDPointStride+field_loc[PixIndex+nf]));}
+        // these two are hardcoded for particular type of experiments -> number of runs <~2^9, number of pixels <~ 2^(23)
+        uint32_t getNrun(size_t n_point)const{return   0x01FF&(*(reinterpret_cast<uint32_t *>(pDataBuffer+n_point*MDPointStride+pPixIndex)));}
+        uint32_t getNPix(size_t n_point)const{return ((0x07FFFF)&(*(reinterpret_cast<uint32_t *>(pDataBuffer+n_point*MDPointStride +pPixIndex)))>>9);}
+        // function specialized for Horace data e.g. expetcs no more than 2^9 runs and no more than 2^23 pixels (unique detectors)
+        void setData(unsigned int ind,T dim_fields[],double SignalFields[],int iFiels[]){
+          unsigned int i,i0;
+      
+          char *const base = pDataBuffer+ind*MDPointStride;
+
+          memcpy(base,dim_fields,sizeof(T)*nDims);
+          i0 = nDims;
+          memcpy(base+field_loc[i0],SignalFields,sizeof(double)*nSignals);
+          // this part is specialized for particular type of data (indexes ~<2^16)
+          i0=PixIndex;
+          // compress n_runs and n_pixels into single 32 bit word
+          uint32_t *pBuf = (uint32_t *)pWorkingBuf;
+          uint32_t  w1   = (uint32_t)iFiels[0];
+          uint32_t  w2   = (uint32_t)iFiels[1];
+          *pBuf = (w1&0x01FF)|((w2<<9)&(~0x01FF));
+          // add proper number of other 16-bit indexes
+          for(i=2;i<nIndexes;i++){
+            pWorkingBuf[i]=(uint16_t)iFiels[i];
+          }
+          memcpy(base + field_loc[i0],pWorkingBuf,sizeof(uint16_t)*nIndexes);
+
+        }
+        //
+        ~MDDataPoint(){
+          delete [] field_loc;
+          delete [] pWorkingBuf;
+        }
       private:
-        const char *pBuffer;
+        unsigned int nDims,nSignals,nIndexes;
+        char *const  pDataBuffer;
         unsigned int MDPointStride;
-        std::vector<unsigned int> field_locations;
+        unsigned int *field_loc;
+        uint16_t     *pWorkingBuf;
         std::vector<unsigned int> field_lengths;
+        unsigned int pSignal,pError,pPixIndex,PixIndex;
+
       };
 
 
---
+++ Code/Mantid/MDDataObjects/test/MDDataPointTest.h
@@ -0,0 +1,135 @@
+#ifndef MDDATA_POINT_TEST_H
+#define MDDATA_POINT_TEST_H
+#include <cxxtest/TestSuite.h>
+#include "MDDataObjects/MDDataPoints.h"
+
+using namespace Mantid;
+using namespace MDDataObjects;
+struct data4D{
+  float q1,q2,q3,En;
+  double S,Err;
+  int   iRun,iPix,iEn;
+};
+struct data5D{
+  float q1,q2,q3,En,T;
+  double S,Err;
+  int   iRun,iPix,iEn,iT;
+};
+
+
+
+
+class testMDDataPoints :    public CxxTest::TestSuite
+{
+public:
+  void test4DAccess(void){
+    const int nPix = 10;
+    int i;
+    data4D testData[nPix];
+    char   testBuffer[nPix*(4*sizeof(float)+2*sizeof(double)+sizeof(uint32_t)+2)];
+    for(i=0;i<10;i++){
+      testData[i].q1  =(float)(i+1);
+      testData[i].q2  =(float)(i+1)*2;
+      testData[i].q3  =(float)(i+1)*3;
+      testData[i].En  =(float)(i+1)*4;
+      testData[i].S   =(double)(i+1)*5;
+      testData[i].Err =(double)(i+1)*6;
+      testData[i].iRun=(i+1)*7;
+      testData[i].iPix=(i+1)*8;
+      testData[i].iEn =(i+1)*9; 
+    }
+    MDDataPoint<float>  PackUnpacker(testBuffer,4,2,3);
+
+    float  Dim[4];
+    double SE[2];
+    int    Ind[3];
+    for(i=0;i<nPix;i++){
+      Dim[0]=testData[i].q1;
+      Dim[1]=testData[i].q2;
+      Dim[2]=testData[i].q3;
+      Dim[3]=testData[i].En;
+      SE[0] =testData[i].S ;
+      SE[1] =testData[i].Err ;
+      Ind[0] =testData[i].iRun ;
+      Ind[1] =testData[i].iPix ;
+      Ind[2] =testData[i].iEn ;
+      PackUnpacker.setData(i,Dim,SE,Ind);
+
+    }
+
+    for(i=0;i<nPix;i++){
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(0,i),testData[i].q1);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(1,i),testData[i].q2);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(2,i),testData[i].q3);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(3,i),testData[i].En);
+        TS_ASSERT_EQUALS(PackUnpacker.getSignal(i),testData[i].S);
+        TS_ASSERT_EQUALS(PackUnpacker.getError(i), testData[i].Err);
+        TS_ASSERT_EQUALS(PackUnpacker.getNrun(i), testData[i].iRun);
+        TS_ASSERT_EQUALS(PackUnpacker.getNPix(i), testData[i].iPix);
+
+        TS_ASSERT_EQUALS(PackUnpacker.getIndex(2,i), testData[i].iEn);
+
+    }
+  }
+         
+  void test5DAccess(void){
+    const int nPix = 10;
+    int i;
+    data5D testData[nPix];
+    char   testBuffer[nPix*(5*sizeof(float)+2*sizeof(double)+sizeof(uint32_t)+2*sizeof(uint16_t))];
+    for(i=0;i<10;i++){
+      testData[i].q1  =(float)(i);
+      testData[i].q2  =(float)(i)*2;
+      testData[i].q3  =(float)(i)*3;
+      testData[i].En  =(float)(i)*4;
+      testData[i].T  =(float)(i)*5;
+      testData[i].S   =(double)(i)*6;
+      testData[i].Err =(double)(i)*7;
+      testData[i].iRun=(i)*8;
+      testData[i].iPix=(i)*9;
+      testData[i].iEn =(i)*10; 
+      testData[i].iT  =(i)*11; 
+    }
+    MDDataPoint<float>  PackUnpacker(testBuffer,5,2,4);
+
+    float  Dim[5];
+    double SE[2];
+    int    Ind[4];
+    for(i=0;i<nPix;i++){
+      Dim[0]=testData[i].q1;
+      Dim[1]=testData[i].q2;
+      Dim[2]=testData[i].q3;
+      Dim[3]=testData[i].En;
+      Dim[4]=testData[i].T;
+      SE[0] =testData[i].S ;
+      SE[1] =testData[i].Err ;
+      Ind[0] =testData[i].iRun ;
+      Ind[1] =testData[i].iPix ;
+      Ind[2] =testData[i].iEn ;
+      Ind[3] =testData[i].iT ;
+      PackUnpacker.setData(i,Dim,SE,Ind);
+
+    }
+
+    for(i=0;i<nPix;i++){
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(0,i),testData[i].q1);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(1,i),testData[i].q2);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(2,i),testData[i].q3);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(3,i),testData[i].En);
+        TS_ASSERT_EQUALS(PackUnpacker.getDataField(4,i),testData[i].T);
+        TS_ASSERT_EQUALS(PackUnpacker.getSignal(i),testData[i].S);
+        TS_ASSERT_EQUALS(PackUnpacker.getError(i), testData[i].Err);
+        TS_ASSERT_EQUALS(PackUnpacker.getNrun(i), testData[i].iRun);
+        TS_ASSERT_EQUALS(PackUnpacker.getNPix(i), testData[i].iPix);
+
+        TS_ASSERT_EQUALS(PackUnpacker.getIndex(2,i), testData[i].iEn);
+        TS_ASSERT_EQUALS(PackUnpacker.getIndex(3,i), testData[i].iT);
+    }
+         
+
+
+  }
+
+};
+
+#endif
