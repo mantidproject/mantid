53e2a7686078fa8cd7bac684320fa4892125c618
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2010-12-02 10:15:00 +0000

    Fix method call after name change. Re #1212

--- Code/Mantid/DataHandling/test/CreateSampleShapeTest.h
+++ Code/Mantid/DataHandling/test/CreateSampleShapeTest.h
@@ -75,7 +75,7 @@ public:
     //Get the created object
     Mantid::API::MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<Mantid::API::MatrixWorkspace>(Mantid::API::AnalysisDataService::Instance().retrieve("TestWorkspace"));
     
-    const Mantid::Geometry::Object& sample = ws->sample().getShapeObject();
+    const Mantid::Geometry::Object& sample = ws->sample().getShape();
     Mantid::Geometry::V3D point(x,y,z);
     
     if( inside )
