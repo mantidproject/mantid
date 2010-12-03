1ac7d3259e4c68b3d18e71ff84cea2aea712337b
Vickie Lynch <lynchve@ornl.gov>
2010-12-03 20:20:51 +0000

    Refs #2008 Reads ISAW detcal files for any instrument with rectangular detectors

--- Code/Mantid/Algorithms/src/DiffractionEventReadDetCal.cpp
+++ Code/Mantid/Algorithms/src/DiffractionEventReadDetCal.cpp
@@ -11,6 +11,7 @@
 #include "MantidKernel/UnitFactory.h"
 #include "MantidKernel/ArrayProperty.h"
 #include "MantidKernel/Exception.h"
+#include "MantidGeometry/Instrument/RectangularDetector.h"
 #include "MantidGeometry/V3D.h"
 #include <Poco/File.h>
 #include <sstream>
@@ -144,6 +145,36 @@ namespace Algorithms
     std::ifstream input(filename.c_str(), std::ios_base::in);
     std::string line;
     std::string detname;
+    //Build a list of Rectangular Detectors
+    std::vector<boost::shared_ptr<RectangularDetector> > detList;
+    for (int i=0; i < inst->nelements(); i++)
+    {
+      boost::shared_ptr<RectangularDetector> det;
+      boost::shared_ptr<ICompAssembly> assem;
+
+      det = boost::dynamic_pointer_cast<RectangularDetector>( (*inst)[i] );
+     if (det)
+        detList.push_back(det);
+      else
+      {
+        //Also, look in the first sub-level for RectangularDetectors (e.g. PG3).
+        // We are not doing a full recursive search since that will be very long for lots of pixels.
+        assem = boost::dynamic_pointer_cast<ICompAssembly>( (*inst)[i] );
+        if (assem)
+        {
+          for (int j=0; j < assem->nelements(); j++)
+          {
+            det = boost::dynamic_pointer_cast<RectangularDetector>( (*assem)[j] );
+            if (det) detList.push_back(det);
+          }
+        }
+      }
+    }
+
+    if (detList.size() == 0)
+      throw std::runtime_error("This instrument does not have any RectangularDetector's. SumNeighbors cannot operate on this instrument at this time.");
+
+    int i=0;
     while(std::getline(input, line)) 
     {
       if(line[0] != '5') continue;
@@ -154,32 +185,27 @@ namespace Algorithms
       x = x * 0.01;
       y = y * 0.01;
       z = z * 0.01;
-      if (id <= 9)
+      boost::shared_ptr<RectangularDetector> det;
+      det = detList[i];
+      i++;
+      if (det)
       {
-        std::ostringstream oss;
-        oss << id;
-        detname = "E" + oss.str();
-      }
-      else
-      {
-        std::ostringstream oss;
-        oss << id-9;
-        detname = "W" + oss.str();
-      }
-      V3D base = V3D(base_x, base_y, base_z);
-      base.normalize();
-      V3D up = V3D(up_x, up_y, up_z);
-      up.normalize();
-      Quat cr3 = Quat(base, up);
-      cr3.getAngleAxis(roty, ax, ay, az);
-      //Added 180 just like IDF
-      roty += 180.0;
-      V3D out  = V3D(ax, ay, az);
-      V3D ra3 = out.cross_prod(V3D(0,0,1));
-      ax = ra3.X();
-      ay = ra3.Y();
-      az = ra3.Z();
-      intensity(x, y, z, ax, ay, az, roty, detname, inname);
+        detname = det->getName();
+        V3D base = V3D(base_x, base_y, base_z);
+        base.normalize();
+        V3D up = V3D(up_x, up_y, up_z);
+        up.normalize();
+        Quat cr3 = Quat(base, up);
+        cr3.getAngleAxis(roty, ax, ay, az);
+        //Added 180 just like IDF
+        roty += 180.0;
+        V3D out  = V3D(ax, ay, az);
+        V3D ra3 = out.cross_prod(V3D(0,0,1));
+        ax = ra3.X();
+        ay = ra3.Y();
+        az = ra3.Z();
+        intensity(x, y, z, ax, ay, az, roty, detname, inname);
+      } 
     } 
 
 
