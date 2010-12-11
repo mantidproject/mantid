369a0961b92c0d148bd0a4f0c2ac82b881c9d0d5
Peter Peterson <petersonpf@ornl.gov>
2010-12-11 00:18:35 +0000

    Refs #2055

---
+++ Code/Mantid/Algorithms/inc/MantidAlgorithms/SetUncertaintiesToZero.h
@@ -0,0 +1,71 @@
+#ifndef MANTID_ALGORITHMS_CONVERTTOMATRIXWORKSPACE_H_
+#define MANTID_ALGORITHMS_CONVERTTOMATRIXWORKSPACE_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+/** Set the uncertainties of the data to zero.
+
+ Required Properties:
+ <UL>
+ <LI> InputWorkspace  - The name of the input workspace. </LI>
+ <LI> OutputWorkspace - The name of the output workspace. </LI>
+ </UL>
+
+ @date 10/12/2010
+
+ Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+ This file is part of Mantid.
+
+ Mantid is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ Mantid is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+ Code Documentation is available at: <http://doxygen.mantidproject.org>
+ */
+class DLLExport SetUncertaintiesToZero : public API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  SetUncertaintiesToZero();
+
+  /// Virtual destructor
+  virtual ~SetUncertaintiesToZero();
+
+  /// Algorithm's name
+  virtual const std::string name() const;
+
+  /// Algorithm's version
+  virtual int version() const;
+
+  /// Algorithm's category for identification
+  virtual const std::string category() const;
+
+private:
+  /// Initialisation code
+  void init();
+  /// Execution code
+  void exec();
+};
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_CONVERTTOMATRIXWORKSPACE_H_*/
---
+++ Code/Mantid/Algorithms/src/SetUncertaintiesToZero.cpp
@@ -0,0 +1,77 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/SetUncertaintiesToZero.h"
+#include <vector>
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM(SetUncertaintiesToZero)
+
+using namespace Kernel;
+using namespace API;
+
+/// (Empty) Constructor
+SetUncertaintiesToZero::SetUncertaintiesToZero() : API::Algorithm()
+{}
+
+/// Virtual destructor
+SetUncertaintiesToZero::~SetUncertaintiesToZero()
+{}
+
+/// Algorithm's name
+const std::string SetUncertaintiesToZero::name() const
+{ return "SetUncertaintiesToZero";}
+
+/// Algorithm's version
+int SetUncertaintiesToZero::version() const
+{ return (1);}
+
+/// Algorithm's category for identification
+const std::string SetUncertaintiesToZero::category() const
+{ return "General";}
+
+void SetUncertaintiesToZero::init()
+{
+  declareProperty(new WorkspaceProperty<API::MatrixWorkspace>("InputWorkspace","",
+                                                              Direction::Input));
+  declareProperty(new WorkspaceProperty<API::MatrixWorkspace>("OutputWorkspace","",
+                                                              Direction::Output));
+}
+
+void SetUncertaintiesToZero::exec()
+{
+  MatrixWorkspace_const_sptr inputWorkspace = getProperty("InputWorkspace");
+
+  // Create the output workspace. This will copy many aspects from the input one.
+  MatrixWorkspace_sptr outputWorkspace = WorkspaceFactory::Instance().create(inputWorkspace);
+
+  // ...but not the data, so do that here.
+  const int numHists = inputWorkspace->getNumberHistograms();
+  Progress prog(this,0.0,1.0,numHists);
+
+  PARALLEL_FOR2(inputWorkspace,outputWorkspace)
+  for (int i = 0; i < numHists; ++i)
+  {
+    PARALLEL_START_INTERUPT_REGION
+
+    outputWorkspace->setX(i,inputWorkspace->refX(i));
+    outputWorkspace->dataY(i) = inputWorkspace->readY(i);
+    outputWorkspace->dataE(i) = std::vector<double>(inputWorkspace->readE(i).size(), 0.);
+
+    prog.report();
+
+    PARALLEL_END_INTERUPT_REGION
+  }
+  PARALLEL_CHECK_INTERUPT_REGION
+
+  setProperty("OutputWorkspace", outputWorkspace);
+}
+
+} // namespace Algorithms
+} // namespace Mantid
+
