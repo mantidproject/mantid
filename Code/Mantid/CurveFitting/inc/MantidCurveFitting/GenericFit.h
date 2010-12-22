fa68d264d6fed1d59c4eef26c02864e2bc1973cb
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2010-12-22 16:51:58 +0000

    Added GenericFit algorithm (not working yet). re #2132

---
+++ Code/Mantid/CurveFitting/inc/MantidCurveFitting/GenericFit.h
@@ -0,0 +1,99 @@
+#ifndef MANTID_CURVEFITTING_GENERICFIT_H_
+#define MANTID_CURVEFITTING_GENERICFIT_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/IFitFunction.h"
+#include "MantidCurveFitting/CostFuncLeastSquares.h"
+#include "MantidCurveFitting/CostFuncIgnorePosPeaks.h"
+
+namespace Mantid
+{
+  namespace CurveFitting
+  {
+    /**
+    Abstract base class for fitting functions.
+
+    Properties common for all fitting functions:
+    <UL>
+    <LI> InputWorkspace - The name of the Workspace2D to take as input </LI>
+
+    <LI> SpectrumNumber - The spectrum to fit, using the workspace numbering of the spectra (default 0)</LI>
+    <LI> StartX - Lowest value of x data array </LI>
+    <LI> EndX - Highest value of x data array </LI>
+
+    <LI> Function - 
+
+    <LI> MaxIterations - The spectrum to fit (default 500)</LI>
+    <LI> Output Status - whether the fit was successful. Direction::Output</LI>
+    <LI> Output Chi^2/DoF - returns how good the fit was (default 0.0). Direction::Output</LI>
+    </UL>
+
+    @author Anders Markvardsen, ISIS, RAL
+    @date 15/5/2009
+    @author Roman Tolchenov, Tessella plc
+    @date 22/12/2010
+
+    Copyright &copy; 2007-8 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
+    class DLLExport GenericFit : public API::Algorithm
+    {
+    public:
+      /// Default constructor
+      GenericFit() : API::Algorithm(),m_function(NULL) {};
+      /// Destructor
+      virtual ~GenericFit();
+      /// Algorithm's name for identification overriding a virtual method
+      virtual const std::string name() const { return "GenericFit";}
+      /// Algorithm's version for identification overriding a virtual method
+      virtual int version() const { return (1);}
+      /// Algorithm's category for identification overriding a virtual method
+      virtual const std::string category() const { return "CurveFitting";}
+
+      /// Function you want to fit to.
+      virtual void function(const double* in, double* out, const double* xValues, const int& nData);
+      /// Derivatives of function with respect to parameters you are trying to fit
+      virtual void functionDeriv(const double* in, API::Jacobian* out, const double* xValues, const int& nData);
+
+      /// Get the function for fitting
+      API::IFitFunction* getFunction()const{return m_function;}
+
+    protected:
+      // Overridden Algorithm methods
+      void init();
+      void exec();
+
+      /// calculates the derivative of a declared parameter over active parameter i
+      double transformationDerivative(int i);
+
+      /// Pointer to the fitting function
+      API::IFitFunction* m_function;
+
+      friend struct FitData1;
+    };
+
+    
+  } // namespace CurveFitting
+} // namespace Mantid
+
+#endif /*MANTID_CURVEFITTING_GENERICFIT_H_*/
---
+++ Code/Mantid/CurveFitting/src/GenericFit.cpp
@@ -0,0 +1,499 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidCurveFitting/GenericFit.h"
+#include "MantidCurveFitting/BoundaryConstraint.h"
+#include "MantidCurveFitting/SimplexMinimizer.h"
+#include "MantidAPI/CompositeFunction.h"
+#include "MantidAPI/IPeakFunction.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidAPI/TextAxis.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidKernel/ArrayProperty.h"
+#include "MantidKernel/Exception.h"
+
+#include <sstream>
+#include <numeric>
+#include <cmath>
+#include <iomanip>
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+
+  // Register the class into the algorithm factory
+  //DECLARE_ALGORITHM(GenericFit)
+
+  using namespace Kernel;
+  using API::WorkspaceProperty;
+  using API::Axis;
+  using API::MatrixWorkspace;
+  using API::Algorithm;
+  using API::Progress;
+  using API::Jacobian;
+
+
+  ///Destructor
+  GenericFit::~GenericFit()
+  {
+    if (m_function) delete m_function;
+  }
+
+  /** Initialisation method
+  */
+  void GenericFit::init()
+  {
+    declareProperty("Input","","Dataset identifier. Must be consistent with the Function type (see FitFunction::setWorkspace).");
+
+    declareProperty("Function","",Direction::InOut );
+
+    BoundedValidator<int> *mustBePositive = new BoundedValidator<int>();
+    mustBePositive->setLower(0);
+    declareProperty("MaxIterations", 500, mustBePositive,
+      "Stop after this number of iterations if a good GenericFit is not found" );
+    declareProperty("Output Status","", Direction::Output);
+    declareProperty("Output Chi^2/DoF",0.0, Direction::Output);
+
+    // Disable default gsl error handler (which is to call abort!)
+    gsl_set_error_handler_off();
+
+    declareProperty("Output","","If not empty OutputParameters TableWorksace and OutputWorkspace will be created.");
+
+    std::vector<std::string> minimizerOptions = FuncMinimizerFactory::Instance().getKeys();
+
+    declareProperty("Minimizer","Levenberg-Marquardt",new ListValidator(minimizerOptions),
+      "The minimizer method applied to do the GenericFit, default is Levenberg-Marquardt", Direction::InOut);
+
+    std::vector<std::string> costFuncOptions = CostFunctionFactory::Instance().getKeys();
+    declareProperty("CostFunction","Least squares",new ListValidator(costFuncOptions),
+      "The cost function to be used for the GenericFit, default is Least squares", Direction::InOut);
+  }
+
+
+  /** Executes the algorithm
+  *
+  *  @throw runtime_error Thrown if algorithm cannot execute
+  */
+  void GenericFit::exec()
+  {
+    // Try to retrieve optional properties
+    const int maxInterations = getProperty("MaxIterations");
+
+    std::string funIni = getProperty("Function");
+    m_function = API::FunctionFactory::Instance().createInitialized(funIni);
+
+    if (m_function == NULL)
+    {
+      throw std::runtime_error("Function was not set.");
+    }
+
+    std::string input = getProperty("Input");
+
+    m_function->setWorkspace(input);
+
+    // force initial parameters to satisfy constraints of function
+    m_function->setParametersToSatisfyConstraints();
+
+    // check if derivative defined in derived class
+    bool isDerivDefined = true;
+    //  const std::vector<double> inTest(nActive(),1.0);
+    //  std::vector<double> outTest(nActive());
+    //  const double xValuesTest = 0;
+    //  JacobianImpl1 J;
+    //  gsl_matrix* M( gsl_matrix_alloc(1,nActive()) );
+    //  J.setJ(M);
+    //  try
+    //  {
+    //  // note nData set to zero (last argument) hence this should avoid further memory problems
+    //  functionDeriv(NULL, &J, &xValuesTest, 0);
+    //}
+    //catch (Exception::NotImplementedError&)
+    //{
+    //  isDerivDefined = false;
+    //}
+    //gsl_matrix_free(M);
+
+    // What minimizer to use
+    std::string methodUsed = getProperty("Minimizer");
+    if ( !isDerivDefined && methodUsed.compare("Simplex") != 0 )
+    {
+      methodUsed = "Simplex";
+      g_log.information() << "No derivatives available for this GenericFitting function"
+                          << " therefore Simplex method used for GenericFitting\n";
+    }
+
+    // create and populate data containers. Warn user if nData < nParam 
+    // since as a rule of thumb this is required as a minimum to obtained 'accurate'
+    // GenericFitting parameter values.
+
+    const int nParam = m_function->nActive();
+    const int nData = m_function->dataSize();
+    if (nParam == 0)
+    {
+      g_log.error("There are no active parameters.");
+      throw std::runtime_error("There are no active parameters.");
+    }
+    if (nData == 0)
+    {
+      g_log.error("The data set is empty.");
+      throw std::runtime_error("The data set is empty.");
+    }
+    if (nData < nParam)
+    {
+      g_log.error("Number of data points less than number of parameters to be GenericFitted.");
+      throw std::runtime_error("Number of data points less than number of parameters to be GenericFitted.");
+    }
+
+    double* X = new double[nData];
+    double* sqrtWeightData = new double[nData];
+
+
+    // set-up initial guess for GenericFit parameters
+    gsl_vector *initFuncArg;
+    initFuncArg = gsl_vector_alloc(nParam);
+
+    for (size_t i = 0; i < m_function->nActive(); i++)
+    {
+        gsl_vector_set(initFuncArg, i, m_function->activeParameter(i));
+    }
+
+
+    // set-up minimizer
+
+    std::string costFunction = getProperty("CostFunction");
+    IFuncMinimizer* minimizer = FuncMinimizerFactory::Instance().createUnwrapped(methodUsed);
+    //minimizer->initialize(X, Y, sqrtWeightData, nData, nParam, 
+    //                 initFuncArg, this, costFunction);
+    
+
+    // finally do the GenericFitting
+
+    int iter = 0;
+    int status = 0;
+    double finalCostFuncVal = 0.0;
+    double dof = nData - nParam;  // dof stands for degrees of freedom
+
+    // Standard least-squares used if derivative function defined otherwise simplex
+    Progress prog(this,0.0,1.0,maxInterations);
+    if ( methodUsed.compare("Simplex") != 0 )
+    {
+      status = GSL_CONTINUE;
+      while (status == GSL_CONTINUE && iter < maxInterations)
+      {
+        iter++;
+        status = minimizer->iterate();
+        if (status != GSL_SUCCESS)  
+        { 
+          // From experience it is found that gsl_multiGenericFit_fdfsolver_iterate occasionally get
+          // stock - even after having achieved a sensible GenericFit. This seem in particular to be a
+          // problem on Linux. For now only fall back to Simplex if iter = 1 or 2, i.e.   
+          // gsl_multiGenericFit_fdfsolver_iterate has failed on the first or second hurdle
+          if (iter < 3)
+          {
+            g_log.warning() << "GenericFit algorithm using " << methodUsed << " failed "
+              << "reporting the following: " << gsl_strerror(status) << "\n"
+              << "Try using Simplex method instead\n";
+            methodUsed = "Simplex";
+            delete minimizer;
+            minimizer = FuncMinimizerFactory::Instance().createUnwrapped(methodUsed);
+            //minimizer->initialize(X, Y, sqrtWeightData, nData, nParam, 
+            //                      initFuncArg, this, costFunction);
+            iter = 0;
+          }
+          break;
+        }
+
+        status = minimizer->hasConverged();
+        prog.report();
+      }
+
+      finalCostFuncVal = minimizer->costFunctionVal() / dof;
+    }
+
+    if ( methodUsed.compare("Simplex") == 0 )
+    {
+      status = GSL_CONTINUE;
+      while (status == GSL_CONTINUE && iter < maxInterations)
+      {
+        iter++;
+        status = minimizer->iterate();
+
+        if (status)  // break if error
+        {
+          // if failed at first iteration try reducing the initial step size
+          if (iter == 1)
+          { 
+            g_log.information() << "Simplex step size reduced to 0.1\n";
+            delete minimizer;
+            SimplexMinimizer* sm = new SimplexMinimizer;
+            //sm->initialize(X, Y, sqrtWeightData, nData, nParam, 
+            //               initFuncArg, this, costFunction);
+            //sm->resetSize(X, Y, sqrtWeightData, nData, nParam, 
+            //               initFuncArg, 0.1, this, costFunction);
+            minimizer = sm;
+            status = GSL_CONTINUE;
+            continue;
+          }
+          break;
+        }
+
+        status = minimizer->hasConverged();
+        prog.report();
+      }
+
+      finalCostFuncVal = minimizer->costFunctionVal() / dof;
+    }
+
+    // Output summary to log file
+
+    std::string reportOfGenericFit = gsl_strerror(status);
+
+    g_log.information() << "Method used = " << methodUsed << "\n" <<
+      "Iteration = " << iter << "\n" <<
+      "Status = " << reportOfGenericFit << "\n" <<
+      "Chi^2/DoF = " << finalCostFuncVal << "\n";
+    for (int i = 0; i < m_function->nParams(); i++)
+    {
+      g_log.information() << m_function->parameterName(i) << " = " << m_function->getParameter(i) << "  \n";
+    }
+
+
+    // also output summary to properties
+
+    setProperty("Output Status", reportOfGenericFit);
+    setProperty("Output Chi^2/DoF", finalCostFuncVal);
+    setProperty("Minimizer", methodUsed);
+    setPropertyValue("Function",*m_function);
+    
+
+    // if Output property is specified output additional workspaces
+
+    std::vector<double> standardDeviations;
+    std::string output = getProperty("Output");
+    gsl_matrix *covar(NULL);
+
+    // only if derivative is defined for GenericFitting function create covariance matrix output workspace
+    if ( methodUsed.compare("Simplex") != 0 )    
+    {
+      // calculate covariance matrix
+      covar = gsl_matrix_alloc (nParam, nParam);
+      minimizer->calCovarianceMatrix( 0.0, covar);
+
+      // take standard deviations to be the square root of the diagonal elements of
+      // the covariance matrix
+      int iPNotFixed = 0;
+      for(int i=0; i < m_function->nParams(); i++)
+      {
+        standardDeviations.push_back(1.0);
+        if (m_function->isActive(i))
+        {
+          standardDeviations[i] = sqrt(gsl_matrix_get(covar,iPNotFixed,iPNotFixed));
+          if (m_function->activeParameter(iPNotFixed) != m_function->getParameter(m_function->indexOfActive(iPNotFixed)))
+          {// it means the active param is not the same as declared but transformed
+            standardDeviations[i] *= fabs(transformationDerivative(iPNotFixed));
+          }
+          iPNotFixed++;
+        }
+      }
+    }
+
+    if (!output.empty())
+    {
+      // only if derivative is defined for GenericFitting function create covariance matrix output workspace
+      if ( methodUsed.compare("Simplex") != 0 )    
+      {
+        // Create covariance matrix output workspace
+        declareProperty(
+          new WorkspaceProperty<API::ITableWorkspace>("OutputNormalisedCovarianceMatrix","",Direction::Output),
+          "The name of the TableWorkspace in which to store the final covariance matrix" );
+        setPropertyValue("OutputNormalisedCovarianceMatrix",output+"_NormalisedCovarianceMatrix");
+
+        Mantid::API::ITableWorkspace_sptr m_covariance = Mantid::API::WorkspaceFactory::Instance().createTable("TableWorkspace");
+        m_covariance->addColumn("str","Name");
+        std::vector<std::string> paramThatAreGenericFitted; // used for populating 1st "name" column
+        for(int i=0; i < m_function->nParams(); i++) 
+        {
+          if (m_function->isActive(i)) 
+          {
+            m_covariance->addColumn("double",m_function->parameterName(i));
+            paramThatAreGenericFitted.push_back(m_function->parameterName(i));
+          }
+        }
+
+        for(int i=0; i<nParam; i++)
+        {
+          Mantid::API::TableRow row = m_covariance->appendRow();
+          row << paramThatAreGenericFitted[i];
+          for(int j=0; j<nParam; j++)
+          {
+            if (j == i)
+              row << 1.0;
+            else
+            {
+              row << 100.0*gsl_matrix_get(covar,i,j)/sqrt(gsl_matrix_get(covar,i,i)*gsl_matrix_get(covar,j,j));
+            }
+          }
+        }
+
+        setProperty("OutputNormalisedCovarianceMatrix",m_covariance);
+      }
+
+      // create output parameter table workspace to store final GenericFit parameters 
+      // including error estimates if derivative of GenericFitting function defined
+
+      declareProperty(
+        new WorkspaceProperty<API::ITableWorkspace>("OutputParameters","",Direction::Output),
+        "The name of the TableWorkspace in which to store the final GenericFit parameters" );
+      declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output),
+        "Name of the output Workspace holding resulting simlated spectrum");
+
+      setPropertyValue("OutputParameters",output+"_Parameters");
+      setPropertyValue("OutputWorkspace",output+"_Workspace");
+
+      Mantid::API::ITableWorkspace_sptr m_result = Mantid::API::WorkspaceFactory::Instance().createTable("TableWorkspace");
+      m_result->addColumn("str","Name");
+      m_result->addColumn("double","Value");
+      if ( methodUsed.compare("Simplex") != 0 ) 
+        m_result->addColumn("double","Error");
+
+      for(int i=0;i<m_function->nParams();i++)
+      {
+        Mantid::API::TableRow row = m_result->appendRow();
+        row << m_function->parameterName(i) << m_function->getParameter(i);
+        if ( methodUsed.compare("Simplex") != 0 && m_function->isActive(i)) 
+        {
+          row << standardDeviations[i];
+        }
+      }
+      setProperty("OutputParameters",m_result);
+
+
+
+      if ( methodUsed.compare("Simplex") != 0 ) 
+        gsl_matrix_free(covar);
+
+    // Add Parameters, Errors and ParameterNames properties to output so they can be queried on the algorithm.
+    declareProperty(new ArrayProperty<double> ("Parameters",new NullValidator<std::vector<double> >,Direction::Output));
+    declareProperty(new ArrayProperty<double> ("Errors",new NullValidator<std::vector<double> >,Direction::Output));
+    declareProperty(new ArrayProperty<std::string> ("ParameterNames",new NullValidator<std::vector<std::string> >,Direction::Output));
+    std::vector<double> params,errors;
+    std::vector<std::string> parNames;
+
+    for(int i=0;i<m_function->nParams();i++)
+    {
+      parNames.push_back(m_function->parameterName(i));
+      params.push_back(m_function->getParameter(i));
+      if (!standardDeviations.empty())
+      {
+        errors.push_back(standardDeviations[i]);
+      }
+      else
+      {
+        errors.push_back(0.);
+      }
+    }
+    setProperty("Parameters",params);
+    setProperty("Errors",errors);
+    setProperty("ParameterNames",parNames);
+    }
+    
+    // minimizer may have dynamically allocated memory hence make sure this memory is freed up
+    delete minimizer;
+
+    // clean up dynamically allocated gsl stuff
+    delete [] X;
+    delete [] sqrtWeightData;
+    gsl_vector_free (initFuncArg);
+    
+    return;
+  }
+
+/** Calculate the GenericFitting function.
+ *  @param in A pointer ot the input active function parameters
+ *  @param out A pointer to the output GenericFitting function buffer. The buffer must be large enough to receive nData double values.
+ *        The GenericFitting procedure will try to minimise Sum(out[i]^2)
+ *  @param xValues The array of nData x-values.
+ *  @param nData The size of the GenericFitted data.
+ */
+  void GenericFit::function(const double* in, double* out, const double* xValues, const int& nData)
+  {
+
+    if (in) m_function->updateActive(in);
+    //m_function->function(out,xValues,nData);
+  
+    // Add penalty factor to function if any constraint is violated
+
+    double penalty = 0.;
+    for(int i=0;i<m_function->nParams();++i)
+    {
+      API::IConstraint* c = m_function->getConstraint(i);
+      if (c)
+      {
+        penalty += c->check();
+      }
+    }
+
+    // add penalty to first and last point and every 10th point in between
+    if ( penalty != 0.0 )
+    {
+      out[0] += penalty;
+      out[nData-1] += penalty;
+
+      for (int i = 9; i < nData-1; i+=10)
+      {
+        out[i] += penalty;
+      }
+    }
+  }
+
+  /** Calculate derivates of GenericFitting function
+  *
+  * @param in Input GenericFitting parameter values
+  * @param out Derivatives
+  * @param xValues X values for data points
+  * @param nData Number of data points
+  */
+  void GenericFit::functionDeriv(const double* in, Jacobian* out, const double* xValues, const int& nData)
+  {
+    if (in) m_function->updateActive(in);
+    //m_function->functionDeriv(out,xValues,nData);
+
+    if (nData <= 0) return;
+
+    for(int i=0;i<m_function->nParams();++i)
+    {  
+      API::IConstraint* c = m_function->getConstraint(i);
+      if (c)
+      {
+        double penalty = c->checkDeriv();
+        out->addNumberToColumn(penalty, m_function->activeIndex(i));
+      }
+    }
+
+    //std::cerr<<"-------------- Jacobian ---------------\n";
+    //for(int i=0;i<nActive();i++)
+    //  for(int j=0;j<nData;j++)
+    //    std::cerr<<i<<' '<<j<<' '<<gsl_matrix_get(((JacobianImpl1*)out)->m_J,j,i)<<'\n';
+  }
+    
+  /**
+   * If i-th parameter is transformed the derivative will be != 1.0.
+   * The derivative is calculated numerically.
+   * @param i The index of an active parameter
+   * @return The transformation derivative
+   */
+  double GenericFit::transformationDerivative(int i)
+  {
+    int j = m_function->indexOfActive(i);
+    double p0 = m_function->getParameter(j);
+    double ap0 = m_function->activeParameter(i);
+    double dap = ap0 != 0.0? ap0 * 0.001 : 0.001;
+    m_function->setActiveParameter(i,ap0 + dap);
+    double deriv = ( m_function->getParameter(j) - p0 ) / dap;
+    m_function->setParameter(j,p0,false);
+    return deriv;
+  }
+
+} // namespace Algorithm
+} // namespace Mantid
