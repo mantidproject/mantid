ecf8cd8703f5f74c5b6e310dd86b90f2f0cda515
Michael Reuter <reuterma@ornl.gov>
2011-01-05 20:34:24 +0000

    Attempt to get GMock/GTest into CMake for real. Headers and libgmock generate a huge number of linker errors that look to be self-inflicted, so test compilation fails. No working solution found. This refs #1881.

--- Code/Mantid/Build/CMake/FindGMock.cmake
+++ Code/Mantid/Build/CMake/FindGMock.cmake
@@ -1,4 +1,22 @@
 # Find the Google Mock headers and libraries
+# GMOCK_INCLUDE_DIR where to find gmock.h
 # GMOCK_FOUND If false, do not try to use Google Mock
 
-set ( GMOCK_FOUND FALSE )
+find_path ( GMOCK_INCLUDE_DIR gmock/gmock.h 
+  PATHS ${PROJECT_SOURCE_DIR}/TestingTools/include
+        ${PROJECT_SOURCE_DIR}/../TestingTools/include
+)
+
+find_library ( GMOCK_LIB NAMES gmock
+  PATHS ${PROJECT_SOURCE_DIR}/TestingTools/lib
+        ${PROJECT_SOURCE_DIR}/../TestingTools/lib
+)
+
+set ( GMOCK_LIBRARIES optimized ${GMOCK_LIB} )
+
+# handle the QUIETLY and REQUIRED arguments and set GMOCK_FOUND to TRUE if 
+# all listed variables are TRUE
+include ( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( GMOCK DEFAULT_MSG GMOCK_INCLUDE_DIR 
+  GMOCK_LIBRARIES
+)
--- Code/Mantid/Build/CMake/FindGTest.cmake
+++ Code/Mantid/Build/CMake/FindGTest.cmake
@@ -1,4 +1,13 @@
 # Find the Google Test headers and libraries
+# GTEST_INCLUDE_DIR where to find gtest.h
 # GTEST_FOUND If false, do not try to use Google Test
 
-set ( GTEST_FOUND FALSE )
+find_path ( GTEST_INCLUDE_DIR gtest/gtest.h
+  PATHS ${PROJECT_SOURCE_DIR}/TestingTools/include
+        ${PROJECT_SOURCE_DIR}/../TestingTools/include
+)
+
+# handle the QUIETLY and REQUIRED arguments and set GTEST_FOUND to TRUE if 
+# all listed variables are TRUE
+include ( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( GTest DEFAULT_MSG GTEST_INCLUDE_DIR )
