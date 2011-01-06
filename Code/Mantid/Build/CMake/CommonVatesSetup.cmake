ff9613b22022fc3c40de068fbbcefc9f1a566ef5
Michael Reuter <reuterma@ornl.gov>
2011-01-06 18:20:59 +0000

    Consolidating Vates setup. This refs #1881.

---
+++ Code/Mantid/Build/CMake/CommonVatesSetup.cmake
@@ -0,0 +1,13 @@
+# Setup common things for the Vates subprojects
+
+include ( SetMantidSubprojects )
+
+set_mantid_subprojects (
+Framework/API
+Framework/Geometry
+Framework/Kernel
+Framework/MDAlgorithms
+Framework/MDDataObjects
+)
+
+set ( COMMONVATES_SETUP_DONE TRUE )
