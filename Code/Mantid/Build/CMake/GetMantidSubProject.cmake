546176bac679977f34da9c2445676a1a78b1f48c
Michael Reuter <reuterma@ornl.gov>
2011-01-06 16:26:28 +0000

    Building of VisIt dependent projects is now available in CMake system. This refs #1881.

--- Code/Mantid/Build/CMake/GetMantidSubProject.cmake
+++ Code/Mantid/Build/CMake/GetMantidSubProject.cmake
@@ -1,11 +1,21 @@
 # Set information for a given Mantid subproject
-macro ( get_mantid_subproject SUBPROJECT )
-  set ( TMP Mantid_${SUBPROJECT} )
-  string ( TOUPPER ${TMP} SUBPROJECT_TAG )
+macro ( get_mantid_subproject _subproject_path)
+
+  string ( REGEX MATCH "/.+$" _subproject ${_subproject_path} )
+  string ( REGEX REPLACE "/" "" _subproject ${_subproject} )
+
+  set ( _tmp Mantid_${_subproject} )
+
+  string ( TOUPPER ${_tmp} SUBPROJECT_TAG )
+
+  #message(STATUS "Q: ${ARGN}")
+
   set ( ${SUBPROJECT_TAG}_INCLUDE_DIR 
-    ${CMAKE_SOURCE_DIR}/Framework/${SUBPROJECT}/inc/Mantid${SUBPROJECT}
+    "${CMAKE_SOURCE_DIR}/${_subproject_path}/inc"
     )
+
   set( ${SUBPROJECT_TAG}_LIBRARY 
-    "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/Mantid${SUBPROJECT}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+    "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/Mantid${_subproject}${CMAKE_SHARED_LIBRARY_SUFFIX}"
     )
-endmacro ( get_mantid_subproject SUBPROJECT )
+
+endmacro ( get_mantid_subproject _subproject_path )
