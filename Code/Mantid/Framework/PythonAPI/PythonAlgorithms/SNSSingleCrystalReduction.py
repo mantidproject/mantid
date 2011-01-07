1f9daf035854478a0d7902b970ba2ebc4a3a2198
Vickie Lynch <lynchve@ornl.gov>
2011-01-07 20:56:07 +0000

    Refs #2023 Vanadium data focussed for each detector before division in SNSSingleCrystalReduction

--- Code/Mantid/Framework/PythonAPI/PythonAlgorithms/SNSSingleCrystalReduction.py
+++ Code/Mantid/Framework/PythonAPI/PythonAlgorithms/SNSSingleCrystalReduction.py
@@ -16,7 +16,7 @@ class SNSSingleCrystalReduction(PythonAlgorithm):
         #types = ["Event preNeXus", "Event NeXus"]
         #self.declareProperty("FileType", "Event NeXus",
         #                     Validator=ListValidator(types))
-        self.declareListProperty("RunNumber", [0], Validator=BoundedValidator(Lower=0))
+        self.declareProperty("RunNumber", 0, Validator=BoundedValidator(Lower=0))
         self.declareProperty("BackgroundNumber", 0, Validator=BoundedValidator(Lower=0))
         self.declareProperty("EmptyInstrumentNumber", 0, Validator=BoundedValidator(Lower=0))
         self.declareProperty("VanadiumNumber", 0, Validator=BoundedValidator(Lower=0))
@@ -115,15 +115,51 @@ class SNSSingleCrystalReduction(PythonAlgorithm):
         
         Sort(InputWorkspace=wksp, SortBy="Time of Flight")
         #ConvertUnits(InputWorkspace=wksp, OutputWorkspace=wksp, Target="TOF")
-        Rebin(InputWorkspace=wksp, OutputWorkspace="temp", Params=[self._TOFMin, self._delta, self._TOFMax])
-        RenameWorkspace(InputWorkspace="temp", OutputWorkspace=wksp)
-        NormaliseByCurrent(InputWorkspace=wksp, OutputWorkspace=wksp)
+        Rebin(InputWorkspace=wksp, OutputWorkspace=wksp, Params=[self._TOFMin, self._delta, self._TOFMax])
+        #NormaliseByCurrent(InputWorkspace=wksp, OutputWorkspace=wksp)
+
+        return wksp
+
+    def _vanbin(self, wksp, filterLogs=None):
+        if wksp is None:
+            return None
+        # take care of filtering events
+        if self._filterBadPulses:
+            pcharge = wksp.getRun()['proton_charge']
+            pcharge = pcharge.getStatistics().mean
+            FilterByLogValue(InputWorkspace=wksp, OutputWorkspace=wksp, LogName="proton_charge",
+                             MinimumValue=.95*pcharge, MaximumValue=2.*pcharge)
+        if filterLogs is not None:
+            try:
+                logparam = wksp.getRun()[filterLogs[0]]
+                if logparam is not None:
+                    FilterByLogValue(InputWorkspace=wksp, OutputWorkspace=wksp, LogName=filterLogs[0],
+                                     MinimumValue=filterLogs[1], MaximumValue=filterLogs[2])
+            except KeyError, e:
+                raise RuntimeError("Failed to find log '%s' in workspace '%s'" \
+                                   % (filterLogs[0], str(wksp)))
+        
+        Sort(InputWorkspace=wksp, SortBy="Time of Flight")
+        # Remove old calibration files
+        cmd = "rm temp.cal*"
+        os.system(cmd)
+        CreateCalFileByNames(self._instrument, "temp.cal","bank1,bank2,bank3,bank4,bank5,bank6,bank7,bank8,bank9,bank10,bank11,bank12,bank13,bank14,bank15")
+        # Align detectors using new calibration file with offsets
+        AlignDetectors(InputWorkspace=wksp, OutputWorkspace=wksp,CalibrationFile="temp.cal")
+        # Diffraction focusing using new calibration file with offsets
+        DiffractionFocussing(InputWorkspace=wksp, OutputWorkspace=wksp,GroupingFileName="temp.cal")
+        StripVanadiumPeaks(InputWorkspace=wksp, OutputWorkspace="temp", PeakWidthPercent=self._vanPeakWidthPercent)
+        RenameWorkspace(InputWorkspace="temp", OutputWorkspace=str(wksp))
+        ConvertUnits(InputWorkspace=wksp, OutputWorkspace=wksp, Target="TOF")
+        Rebin(InputWorkspace=wksp, OutputWorkspace=wksp, Params=[self._TOFMin, self._delta, self._TOFMax])
+        SmoothData(InputWorkspace=wksp, OutputWorkspace=wksp, NPoints=self._vanSmoothPoints)
+        #NormaliseByCurrent(InputWorkspace=wksp, OutputWorkspace=wksp)
 
         return wksp
 
     def _save(self, wksp, normalized):
         filename = os.path.join(self._outDir, str(wksp))
-        nxsfile = self._findData(self.getProperty("RunNumber")[0], ".nxs")
+        nxsfile = self._findData(self.getProperty("RunNumber"), ".nxs")
         self.log().information(nxsfile)
         if "nxs" in self._outTypes:
             SaveSNSNexus(InputFilename=nxsfile,InputWorkspace=wksp, OutputFilename=filename+"_mantid.nxs")
@@ -156,62 +192,67 @@ class SNSSingleCrystalReduction(PythonAlgorithm):
         self._vanSmoothPoints = self.getProperty("VanadiumSmoothNumPoints")
         self._outDir = self.getProperty("OutputDirectory")
         self._outTypes = self.getProperty("SaveAs")
-        samRuns = self.getProperty("RunNumber")
+        sam = self.getProperty("RunNumber")
         bkg = self.getProperty("BackgroundNumber")
         van = self.getProperty("VanadiumNumber")
         empty = self.getProperty("EmptyInstrumentNumber")
 
-        for samRun in samRuns:
-            # first round of processing the sample 
-            samRun = self._loadData(samRun, SUFFIX)
-            samRun = self._bin(samRun, filterLogs)
+        # first round of processing the sample 
+        samRun = self._loadData(sam, SUFFIX)
+        samRun = self._bin(samRun, filterLogs)
 
-            # process the background
-            if bkg > 0:
-                bkgRun = mtd["%s_%d" % (self._instrument, bkg)]
-                if bkgRun is None:
-                    bkgRun = self._loadData(bkg, SUFFIX)
-                    bkgRun = self._bin(bkgRun)
-                    samRun -= bkgRun
-                    mtd.deleteWorkspace("%s_%d" % (self._instrument, bkg))
-            else:
-                bkgRun = None 
+        # process the background
+        if bkg > 0:
+            bkgRun = mtd["%s_%d" % (self._instrument, bkg)]
+            bkgRun = self._loadData(bkg, SUFFIX)
+            bkgRun = self._bin(bkgRun)
+            samRun -= bkgRun
+            mtd.deleteWorkspace("%s_%d" % (self._instrument, bkg))
+        else:
+            bkgRun = None 
 
-            # process the empty instrument
-            if empty > 0:
-                emptyRun = mtd["%s_%d" % (self._instrument, empty)]
-                if emptyRun is None:
-                    emptyRun = self._loadData(empty, SUFFIX)
-                    emptyRun = self._bin(emptyRun)
-                    samRun -= emptyRun
-                    mtd.deleteWorkspace("%s_%d" % (self._instrument, empty))
-            else:
-                emptyRun = None 
+        # process the empty instrument
+        if empty > 0:
+            emptyRun = mtd["%s_%d" % (self._instrument, empty)]
+            emptyRun = self._loadData(empty, SUFFIX)
+            emptyRun = self._bin(emptyRun)
+            samRun -= emptyRun
+            mtd.deleteWorkspace("%s_%d" % (self._instrument, empty))
+        else:
+            emptyRun = None 
 
-            # process the vanadium run
-            if van > 0:
-                vanRun = mtd["%s_%d" % (self._instrument, van)]
-                if vanRun is None:
-                    vanRun = self._loadData(van, SUFFIX)
-                    vanRun = self._bin(vanRun)
-                    ConvertUnits(InputWorkspace=vanRun, OutputWorkspace=vanRun, Target="dSpacing")
-                    StripVanadiumPeaks(InputWorkspace=vanRun, OutputWorkspace="temp", PeakWidthPercent=self._vanPeakWidthPercent)
-                    RenameWorkspace(InputWorkspace="temp", OutputWorkspace=str(vanRun))
-                    ConvertUnits(InputWorkspace=vanRun, OutputWorkspace=vanRun, Target="TOF")
-                    SmoothData(InputWorkspace=vanRun, OutputWorkspace=vanRun, NPoints=self._vanSmoothPoints)
-            else:
-                vanRun = None
+        # process the vanadium run
+        if van > 0:
+            vanRun = mtd["%s_%d" % (self._instrument, van)]
+            vanRun = self._loadData(van, SUFFIX)
+            vanRun = self._vanbin(vanRun)
+        else:
+            vanRun = None
 
-            # the final bit of math
-            if vanRun is not None:
-                samRun /= vanRun
-                mtd.deleteWorkspace("%s_%d" % (self._instrument, van))
-                normalized = True
-            else:
-                normalized = False
+        # the final bit of math
+        samH = samRun.getNumberHistograms()
+        samRunX = []
+        samRunY = []
+        samRunE = []
+        if vanRun is not None:
+            for num in xrange(0,samH-1):
+                det = str(samRun.getDetector(num).getID())
+                for i in xrange(0,vanRun.getNumberHistograms()-1):
+                    vdet = str(vanRun.getDetector(i).getID())
+                    if vdet == det:
+                        vnum = i
+                samRunX.append(samRun.readX(num))
+                samRunY.append(samRun.readY(num)/vanRun.readY(vnum))
+                samRunE.append(samRun.readE(num))
+            mtd.deleteWorkspace("%s_%d" % (self._instrument, van))
+            normalized = True
+        else:
+            normalized = False
 
-            # write out the files
-            ReplaceSpecialValues(InputWorkspace=samRun, OutputWorkspace=samRun, NaNValue="0.0", InfinityValue="0.0")
-            self._save(samRun, normalized)
+        # write out the files
+        unitX = samRun.getAxis(0).getUnit().name()
+        mtd.deleteWorkspace(samRun)
+        CreateWorkspace(samRun, samRunX, samRunY, samRunE, NSpec=samH, UnitX=unitX)
+        self._save(samRun, normalized)
 
 mtd.registerPyAlgorithm(SNSSingleCrystalReduction())
