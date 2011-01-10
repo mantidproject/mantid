2de6fff0e8b3d24bbfdf656e42ad7b5aedeabfc0
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-01-10 10:01:52 +0000

    Updated ConvertToMatrixWorkspace so that it doesn't do anything if it doesn't need to as in some circumstances wihin the inelastic scripts it was taking a couple of seconds doing a copy unnecessarily. Re #1762

--- Code/Mantid/Framework/Algorithms/src/ConvertToMatrixWorkspace.cpp
+++ Code/Mantid/Framework/Algorithms/src/ConvertToMatrixWorkspace.cpp
@@ -2,6 +2,7 @@
 // Includes
 //----------------------------------------------------------------------
 #include "MantidAlgorithms/ConvertToMatrixWorkspace.h"
+#include "MantidDataObjects/EventWorkspace.h"
 
 namespace Mantid
 {
@@ -23,30 +24,41 @@ void ConvertToMatrixWorkspace::init()
 void ConvertToMatrixWorkspace::exec()
 {
   MatrixWorkspace_const_sptr inputWorkspace = getProperty("InputWorkspace");
-
-  // Create the output workspace. This will copy many aspects fron the input one.
-  MatrixWorkspace_sptr outputWorkspace = WorkspaceFactory::Instance().create(inputWorkspace);
-
-  // ...but not the data, so do that here.
-
-  const int numHists = inputWorkspace->getNumberHistograms();
-  Progress prog(this,0.0,1.0,numHists);
-
-  PARALLEL_FOR2(inputWorkspace,outputWorkspace)
-  for (int i = 0; i < numHists; ++i)
+  // Let's see if we have to do anything first. Basically we want to avoid the data copy if we can
+  DataObjects::EventWorkspace_const_sptr eventW = 
+    boost::dynamic_pointer_cast<const DataObjects::EventWorkspace>(inputWorkspace);
+  MatrixWorkspace_sptr outputWorkspace;
+  if( eventW )
   {
-    PARALLEL_START_INTERUPT_REGION
+    g_log.information() << "Converting EventWorkspace to Workspace2D.\n";
+    // Create the output workspace. This will copy many aspects fron the input one.
+    MatrixWorkspace_sptr outputWorkspace = WorkspaceFactory::Instance().create(inputWorkspace);
 
-    outputWorkspace->setX(i,inputWorkspace->refX(i));
-    outputWorkspace->dataY(i) = inputWorkspace->readY(i);
-    outputWorkspace->dataE(i) = inputWorkspace->readE(i);
+    // ...but not the data, so do that here.
+    
+    const int numHists = inputWorkspace->getNumberHistograms();
+    Progress prog(this,0.0,1.0,numHists);
+    
+    PARALLEL_FOR2(inputWorkspace,outputWorkspace)
+    for (int i = 0; i < numHists; ++i)
+    {
+      PARALLEL_START_INTERUPT_REGION
+	  
+      outputWorkspace->setX(i,inputWorkspace->refX(i));
+      outputWorkspace->dataY(i) = inputWorkspace->readY(i);
+      outputWorkspace->dataE(i) = inputWorkspace->readE(i);
+      
+      prog.report();
 
-    prog.report();
-
-    PARALLEL_END_INTERUPT_REGION
+      PARALLEL_END_INTERUPT_REGION
+    }
+    PARALLEL_CHECK_INTERUPT_REGION
   }
-  PARALLEL_CHECK_INTERUPT_REGION
-
+  else
+  {
+    g_log.information() << "Input workspace does not need converting. Pointing OutputWorkspace property to input.\n";
+    outputWorkspace = boost::const_pointer_cast<MatrixWorkspace>(inputWorkspace);
+  }
 
   setProperty("OutputWorkspace", outputWorkspace);
 }
