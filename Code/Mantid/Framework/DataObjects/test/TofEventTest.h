ce9703fc43c0764642aec165dc8d2586205ff345
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-01-11 19:06:50 +0000

    Refs #2222: Split off EventList test file into 3.

---
+++ Code/Mantid/Framework/DataObjects/test/TofEventTest.h
@@ -0,0 +1,60 @@
+#ifndef TOFEVENTTEST_H_
+#define TOFEVENTTEST_H_ 1
+
+#include <cxxtest/TestSuite.h>
+#include "MantidDataObjects/EventList.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidKernel/Timer.h"
+#include <cmath>
+#include <boost/math/special_functions/fpclassify.hpp>
+
+using namespace Mantid;
+using namespace Mantid::Kernel;
+using namespace Mantid::DataObjects;
+
+using std::runtime_error;
+using std::size_t;
+using std::vector;
+
+//==========================================================================================
+class TofEventTest : public CxxTest::TestSuite
+{
+private:
+  TofEvent e;
+
+public:
+  TofEventTest()
+  {
+    e = TofEvent(123, 456);
+  }
+
+  void testInit()
+  {
+    TS_ASSERT_EQUALS(e.tof(), 123);
+    TS_ASSERT_EQUALS(e.pulseTime(), 456);
+  }
+
+  void testAssign()
+  {
+    TofEvent e2;
+    e2 = e;
+    TS_ASSERT_EQUALS(e2.tof(), 123);
+    TS_ASSERT_EQUALS(e2.pulseTime(), 456);
+  }
+
+  void testConstructors()
+  {
+    TofEvent e2 = TofEvent(e);
+    TS_ASSERT_EQUALS(e2.tof(), 123);
+    TS_ASSERT_EQUALS(e2.pulseTime(), 456);
+
+    TofEvent e3 = TofEvent(890.234, 321);
+    TS_ASSERT_EQUALS(e3.tof(), 890.234);
+    TS_ASSERT_EQUALS(e3.pulseTime(), 321);
+  }
+
+};
+
+
+#endif
+
---
+++ Code/Mantid/Framework/DataObjects/test/WeightedEventTest.h
@@ -0,0 +1,84 @@
+#ifndef WEIGHTEDEVENTTEST_H_
+#define WEIGHTEDEVENTTEST_H_ 1
+
+#include <cxxtest/TestSuite.h>
+#include "MantidDataObjects/EventList.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidKernel/Timer.h"
+#include <cmath>
+#include <boost/math/special_functions/fpclassify.hpp>
+
+using namespace Mantid;
+using namespace Mantid::Kernel;
+using namespace Mantid::DataObjects;
+
+using std::runtime_error;
+using std::size_t;
+using std::vector;
+
+
+//==========================================================================================
+class WeightedEventTest : public CxxTest::TestSuite
+{
+private:
+
+public:
+  WeightedEventTest()
+  {
+  }
+
+  void testConstructors()
+  {
+    TofEvent e(123, 456);
+    WeightedEvent we, we2;
+
+    //Empty
+    we = WeightedEvent();
+    TS_ASSERT_EQUALS(we.tof(), 0);
+    TS_ASSERT_EQUALS(we.pulseTime(), 0);
+    TS_ASSERT_EQUALS(we.weight(), 1.0);
+    TS_ASSERT_EQUALS(we.error(), 1.0);
+
+    //Default one weight
+    we = WeightedEvent(e);
+    TS_ASSERT_EQUALS(we.tof(), 123);
+    TS_ASSERT_EQUALS(we.pulseTime(), 456);
+    TS_ASSERT_EQUALS(we.weight(), 1.0);
+    TS_ASSERT_EQUALS(we.error(), 1.0);
+
+    //TofEvent + weights
+    we = WeightedEvent(e, 3.5, 0.5*0.5);
+    TS_ASSERT_EQUALS(we.tof(), 123);
+    TS_ASSERT_EQUALS(we.pulseTime(), 456);
+    TS_ASSERT_EQUALS(we.weight(), 3.5);
+    TS_ASSERT_EQUALS(we.error(), 0.5);
+
+    //Full constructor
+    we = WeightedEvent(456, 789, 2.5, 1.5*1.5);
+    TS_ASSERT_EQUALS(we.tof(), 456);
+    TS_ASSERT_EQUALS(we.pulseTime(), 789);
+    TS_ASSERT_EQUALS(we.weight(), 2.5);
+    TS_ASSERT_EQUALS(we.error(), 1.5);
+  }
+
+  void testAssignAndCopy()
+  {
+    WeightedEvent we, we2;
+
+    //Copy constructor
+    we = WeightedEvent();
+    we2 = WeightedEvent(456, 789, 2.5, 1.5*1.5);
+    we = we2;
+    TS_ASSERT_EQUALS(we.tof(), 456);
+    TS_ASSERT_EQUALS(we.pulseTime(), 789);
+    TS_ASSERT_EQUALS(we.weight(), 2.5);
+    TS_ASSERT_EQUALS(we.error(), 1.5);
+  }
+
+
+};
+
+
+
+#endif /// EVENTLISTTEST_H_
+
