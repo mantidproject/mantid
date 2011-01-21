b565e0f5768304986ee4129a7c481b2b814876ed
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-01-21 17:46:00 +0000

    Added the /MP flag to WindowsSetup.cmake to enable multiprocessor builds within Visual Studio. Re #1881

--- Code/Mantid/Build/CMake/WindowsSetup.cmake
+++ Code/Mantid/Build/CMake/WindowsSetup.cmake
@@ -4,6 +4,8 @@ add_definitions ( -DGSL_DLL )
 
 set ( CMAKE_INCLUDE_PATH "${THIRD_PARTY}/include" )
 set ( BOOST_INCLUDEDIR "${THIRD_PARTY}/include" )
+# Multiprocessor compilation
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP" )
 
 set (Boost_NO_SYSTEM_PATHS TRUE)
 
