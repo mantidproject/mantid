02c4a51ca57c248a954fb4730d3977a231fe49e3
Russell Taylor <taylorrj@ornl.gov>
2011-01-21 18:53:34 +0000

    Fix tests by removing ASSERTS that aren't testing the class in question anyway. Re #2221.

--- Code/Mantid/Framework/DataHandling/test/FindDetectorsInShapeTest.h
+++ Code/Mantid/Framework/DataHandling/test/FindDetectorsInShapeTest.h
@@ -5,23 +5,6 @@
 
 #include "MantidDataHandling/FindDetectorsInShape.h"
 #include "MantidDataHandling/LoadEmptyInstrument.h"
-#include "MantidAPI/WorkspaceFactory.h"
-#include "MantidGeometry/Instrument/Instrument.h"
-#include "MantidDataObjects/Workspace2D.h"
-#include "MantidAPI/AnalysisDataService.h"
-#include "MantidKernel/Exception.h"
-#include "MantidAPI/FrameworkManager.h"
-#include "MantidAPI/Workspace.h"
-#include "MantidAPI/Algorithm.h"
-#include "MantidAPI/SpectraDetectorMap.h"
-#include "MantidGeometry/Instrument/Component.h"
-#include <vector>
-
-using namespace Mantid::API;
-using namespace Mantid::Kernel;
-using namespace Mantid::Geometry;
-using namespace Mantid::DataHandling;
-using namespace Mantid::DataObjects;
 
 class FindDetectorsInShapeTest : public CxxTest::TestSuite
 {
@@ -32,151 +15,142 @@ public:
 
   FindDetectorsInShapeTest()
   {
-		loadTestWS();
+    loadTestWS();
   }
 
-	void testCuboidMiss()
-	{
-		std::string xmlShape = "<cuboid id=\"shape\"> ";
-		xmlShape +=	"<left-front-bottom-point x=\"0.005\" y=\"-0.1\" z=\"0.0\" /> " ;
-  	xmlShape +=	"<left-front-top-point x=\"0.005\" y=\"-0.1\" z=\"0.0001\" />  " ;
-  	xmlShape +=	"<left-back-bottom-point x=\"-0.005\" y=\"-0.1\" z=\"0.0\" />  " ;
-  	xmlShape +=	"<right-front-bottom-point x=\"0.005\" y=\"0.1\" z=\"0.0\" />  " ;
-  	xmlShape +=	"</cuboid> ";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-		
-		runTest(xmlShape,"");
-	}
-
-	void testCuboidHit()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<cuboid id=\"shape\"> ";
-		xmlShape +=	"<left-front-bottom-point x=\"0.674291\" y=\"0.335987\" z=\"1.30628\" /> " ;
-  	xmlShape +=	"<left-front-top-point x=\"0.674291\" y=\"0.335987\" z=\"1.34783\" />  " ;
-  	xmlShape +=	"<left-back-bottom-point x=\"0.673866\" y=\"0.335987\" z=\"1.30628\" />  " ;
-  	xmlShape +=	"<right-front-bottom-point x=\"0.674291\" y=\"0.336189\" z=\"1.30628\" />  " ;
-  	xmlShape +=	"</cuboid>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-		
-		runTest(xmlShape,"977,978,1017,1018");
-	}
-	
-	void testSphereMiss()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<sphere id=\"shape\"> ";
-		xmlShape +=	"<centre x=\"4.1\"  y=\"2.1\" z=\"8.1\" /> " ;
-  	xmlShape +=	"<radius val=\"3.2\" /> " ;
-  	xmlShape +=	"</sphere>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-
-		runTest(xmlShape,"");
-	}
-
-	void testSphereHit()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<sphere id=\"shape\"> ";
-		xmlShape +=	"<centre x=\"0.67\"  y=\"0.33\" z=\"1.32\" /> " ;
-  	xmlShape +=	"<radius val=\"0.05\" /> " ;
-  	xmlShape +=	"</sphere>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-
-		runTest(xmlShape,"976,977,978,979,980,1016,1017,1018,1019,1020");
-	}
-
-	void testCylinderHit()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<cylinder id=\"shape\"> ";
-		xmlShape +=	"<centre-of-bottom-base x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ; 
-  	xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"1\" /> " ;
-  	xmlShape +=	"<radius val=\"0.1\" /> " ;
-  	xmlShape +=	"<height val=\"3\" /> " ;
-  	xmlShape +=	"</cylinder>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-
-		runTest(xmlShape,"1,3");
-	}
-
-	void testInfiniteCylinderHit()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<infinite-cylinder id=\"shape\"> ";
-		xmlShape +=	"<centre x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ; 
-  	xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"1\" /> " ;
-  	xmlShape +=	"<radius val=\"0.1\" /> " ;
-  	xmlShape +=	"</infinite-cylinder>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-
-		runTest(xmlShape,"1,2,3");
-	}
-
-	void testConeHitNoMonitors()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<cone id=\"shape\"> ";
-		xmlShape +=	"<tip-point x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ; 
-  	xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"-1\" /> " ;
-  	xmlShape +=	"<angle val=\"8.1\" /> " ;
-  	xmlShape +=	"<height val=\"4\" /> " ;
-  	xmlShape +=	"</cone>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-
-		runTest(xmlShape,"320,340,360,380",false);
-	}
-
-	void runTest(std::string xmlShape, std::string expectedHits,bool includeMonitors = true)
+  ~FindDetectorsInShapeTest()
   {
-		FindDetectorsInShape alg;
-		TS_ASSERT_THROWS_NOTHING(alg.initialize());
+    Mantid::API::AnalysisDataService::Instance().clear();
+  }
+
+  void testCuboidMiss()
+  {
+    std::string xmlShape = "<cuboid id=\"shape\"> ";
+    xmlShape +=	"<left-front-bottom-point x=\"0.005\" y=\"-0.1\" z=\"0.0\" /> " ;
+    xmlShape +=	"<left-front-top-point x=\"0.005\" y=\"-0.1\" z=\"0.0001\" />  " ;
+    xmlShape +=	"<left-back-bottom-point x=\"-0.005\" y=\"-0.1\" z=\"0.0\" />  " ;
+    xmlShape +=	"<right-front-bottom-point x=\"0.005\" y=\"0.1\" z=\"0.0\" />  " ;
+    xmlShape +=	"</cuboid> ";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"");
+  }
+
+  void testCuboidHit()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<cuboid id=\"shape\"> ";
+    xmlShape +=	"<left-front-bottom-point x=\"0.674291\" y=\"0.335987\" z=\"1.30628\" /> " ;
+    xmlShape +=	"<left-front-top-point x=\"0.674291\" y=\"0.335987\" z=\"1.34783\" />  " ;
+    xmlShape +=	"<left-back-bottom-point x=\"0.673866\" y=\"0.335987\" z=\"1.30628\" />  " ;
+    xmlShape +=	"<right-front-bottom-point x=\"0.674291\" y=\"0.336189\" z=\"1.30628\" />  " ;
+    xmlShape +=	"</cuboid>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"977,978,1017,1018");
+  }
+
+  void testSphereMiss()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<sphere id=\"shape\"> ";
+    xmlShape +=	"<centre x=\"4.1\"  y=\"2.1\" z=\"8.1\" /> " ;
+    xmlShape +=	"<radius val=\"3.2\" /> " ;
+    xmlShape +=	"</sphere>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"");
+  }
+
+  void testSphereHit()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<sphere id=\"shape\"> ";
+    xmlShape +=	"<centre x=\"0.67\"  y=\"0.33\" z=\"1.32\" /> " ;
+    xmlShape +=	"<radius val=\"0.05\" /> " ;
+    xmlShape +=	"</sphere>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"976,977,978,979,980,1016,1017,1018,1019,1020");
+  }
+
+  void testCylinderHit()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<cylinder id=\"shape\"> ";
+    xmlShape +=	"<centre-of-bottom-base x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ;
+    xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"1\" /> " ;
+    xmlShape +=	"<radius val=\"0.1\" /> " ;
+    xmlShape +=	"<height val=\"3\" /> " ;
+    xmlShape +=	"</cylinder>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"1,3");
+  }
+
+  void testInfiniteCylinderHit()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<infinite-cylinder id=\"shape\"> ";
+    xmlShape +=	"<centre x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ;
+    xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"1\" /> " ;
+    xmlShape +=	"<radius val=\"0.1\" /> " ;
+    xmlShape +=	"</infinite-cylinder>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"1,2,3");
+  }
+
+  void testConeHitNoMonitors()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<cone id=\"shape\"> ";
+    xmlShape +=	"<tip-point x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ;
+    xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"-1\" /> " ;
+    xmlShape +=	"<angle val=\"8.1\" /> " ;
+    xmlShape +=	"<height val=\"4\" /> " ;
+    xmlShape +=	"</cone>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"320,340,360,380",false);
+  }
+
+  void runTest(std::string xmlShape, std::string expectedHits,bool includeMonitors = true)
+  {
+    Mantid::DataHandling::FindDetectorsInShape alg;
+    TS_ASSERT_THROWS_NOTHING(alg.initialize());
     TS_ASSERT( alg.isInitialized() );
 
     alg.setPropertyValue("Workspace", wsName);
     alg.setPropertyValue("ShapeXML", xmlShape);
-		if (includeMonitors)
-		{
-			alg.setPropertyValue("IncludeMonitors", "1");
-		}
-    
+    if (includeMonitors)
+    {
+      alg.setPropertyValue("IncludeMonitors", "1");
+    }
+
     TS_ASSERT_THROWS_NOTHING(alg.execute());
 
     TS_ASSERT( alg.isExecuted() );
 
-		TS_ASSERT_EQUALS(alg.getPropertyValue("DetectorList"),expectedHits);
+    TS_ASSERT_EQUALS(alg.getPropertyValue("DetectorList"),expectedHits);
   }
 
-	std::string loadTestWS()
-	{
-    LoadEmptyInstrument loaderSLS;
+  std::string loadTestWS()
+  {
+    Mantid::DataHandling::LoadEmptyInstrument loaderSLS;
 
     TS_ASSERT_THROWS_NOTHING(loaderSLS.initialize());
     TS_ASSERT( loaderSLS.isInitialized() );
     inputFile = "SANDALS_Definition.xml";
     loaderSLS.setPropertyValue("Filename", inputFile);
-    wsName = "FindDetectorsInShapeTestSLS";
+    wsName = "FindDetectorsInShapeTest_FindDetectorsInShapeTestSLS";
     loaderSLS.setPropertyValue("OutputWorkspace", wsName);
 
-    std::string result;
-    TS_ASSERT_THROWS_NOTHING( result = loaderSLS.getPropertyValue("Filename") )
-    TS_ASSERT( ! result.compare(inputFile));
-
-    TS_ASSERT_THROWS_NOTHING( result = loaderSLS.getPropertyValue("OutputWorkspace") )
-    TS_ASSERT( ! result.compare(wsName));
-
     TS_ASSERT_THROWS_NOTHING(loaderSLS.execute());
-
     TS_ASSERT( loaderSLS.isExecuted() );
 
-
-    MatrixWorkspace_sptr output;
-    output = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve(wsName));
-    // Check the total number of elements in the map for SLS
-    TS_ASSERT_EQUALS(output->spectraMap().nElements(),683);
-	
-		return wsName;
-	}
+    return wsName;
+  }
 
 
 private:
--- Code/Mantid/Framework/DataHandling/test/MaskDetectorsInShapeTest.h
+++ Code/Mantid/Framework/DataHandling/test/MaskDetectorsInShapeTest.h
@@ -5,132 +5,109 @@
 
 #include "MantidDataHandling/MaskDetectorsInShape.h"
 #include "MantidDataHandling/LoadEmptyInstrument.h"
-#include "MantidAPI/WorkspaceFactory.h"
-#include "MantidGeometry/Instrument/Instrument.h"
-#include "MantidDataObjects/Workspace2D.h"
-#include "MantidAPI/AnalysisDataService.h"
-#include "MantidKernel/Exception.h"
 #include "MantidKernel/ArrayProperty.h"
-#include "MantidAPI/FrameworkManager.h"
-#include "MantidAPI/Workspace.h"
-#include "MantidAPI/Algorithm.h"
-#include "MantidAPI/SpectraDetectorMap.h"
-#include "MantidGeometry/Instrument/Component.h"
-#include <vector>
-
-using namespace Mantid::API;
-using namespace Mantid::Kernel;
-using namespace Mantid::Geometry;
-using namespace Mantid::DataHandling;
-using namespace Mantid::DataObjects;
 
 class MaskDetectorsInShapeTest : public CxxTest::TestSuite
 {
 public:
   static MaskDetectorsInShapeTest *createSuite() { return new MaskDetectorsInShapeTest(); }
   static void destroySuite(MaskDetectorsInShapeTest *suite) { delete suite; }
-  
+
   MaskDetectorsInShapeTest()
   {
-		loadTestWS();
+    loadTestWS();
   }
 
-	void testCuboidMiss()
-	{
-		std::string xmlShape = "<cuboid id=\"shape\"> ";
-		xmlShape +=	"<left-front-bottom-point x=\"0.005\" y=\"-0.1\" z=\"0.0\" /> " ;
-  	xmlShape +=	"<left-front-top-point x=\"0.005\" y=\"-0.1\" z=\"0.0001\" />  " ;
-  	xmlShape +=	"<left-back-bottom-point x=\"-0.005\" y=\"-0.1\" z=\"0.0\" />  " ;
-  	xmlShape +=	"<right-front-bottom-point x=\"0.005\" y=\"0.1\" z=\"0.0\" />  " ;
-  	xmlShape +=	"</cuboid> ";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-		
-		runTest(xmlShape,"");
-	}
-
-	void testConeHitNoMonitors()
-	{
-		//algebra line is essential
-		std::string xmlShape = "<cone id=\"shape\"> ";
-		xmlShape +=	"<tip-point x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ; 
-  	xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"-1\" /> " ;
-  	xmlShape +=	"<angle val=\"8.1\" /> " ;
-  	xmlShape +=	"<height val=\"4\" /> " ;
-  	xmlShape +=	"</cone>";
-		xmlShape +=	"<algebra val=\"shape\" /> ";  
-
-		runTest(xmlShape,"320,340,360,380",false);
-	}
-
-	void runTest(std::string xmlShape, std::string expectedHits,bool includeMonitors = true)
+  ~MaskDetectorsInShapeTest()
   {
-		MaskDetectorsInShape alg;
-		TS_ASSERT_THROWS_NOTHING(alg.initialize());
+    Mantid::API::AnalysisDataService::Instance().clear();
+  }
+
+  void testCuboidMiss()
+  {
+    std::string xmlShape = "<cuboid id=\"shape\"> ";
+    xmlShape +=	"<left-front-bottom-point x=\"0.005\" y=\"-0.1\" z=\"0.0\" /> " ;
+    xmlShape +=	"<left-front-top-point x=\"0.005\" y=\"-0.1\" z=\"0.0001\" />  " ;
+    xmlShape +=	"<left-back-bottom-point x=\"-0.005\" y=\"-0.1\" z=\"0.0\" />  " ;
+    xmlShape +=	"<right-front-bottom-point x=\"0.005\" y=\"0.1\" z=\"0.0\" />  " ;
+    xmlShape +=	"</cuboid> ";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"");
+  }
+
+  void testConeHitNoMonitors()
+  {
+    //algebra line is essential
+    std::string xmlShape = "<cone id=\"shape\"> ";
+    xmlShape +=	"<tip-point x=\"0.0\" y=\"0.0\" z=\"0.0\" /> " ;
+    xmlShape +=	"<axis x=\"0.0\" y=\"0.0\" z=\"-1\" /> " ;
+    xmlShape +=	"<angle val=\"8.1\" /> " ;
+    xmlShape +=	"<height val=\"4\" /> " ;
+    xmlShape +=	"</cone>";
+    xmlShape +=	"<algebra val=\"shape\" /> ";
+
+    runTest(xmlShape,"320,340,360,380",false);
+  }
+
+  void runTest(std::string xmlShape, std::string expectedHits,bool includeMonitors = true)
+  {
+    using namespace Mantid::API;
+
+    Mantid::DataHandling::MaskDetectorsInShape alg;
+    TS_ASSERT_THROWS_NOTHING(alg.initialize());
     TS_ASSERT( alg.isInitialized() );
 
     alg.setPropertyValue("Workspace", wsName);
     alg.setPropertyValue("ShapeXML", xmlShape);
-		if (includeMonitors)
-		{
-			alg.setPropertyValue("IncludeMonitors", "1");
-		}
-    
+    if (includeMonitors)
+    {
+      alg.setPropertyValue("IncludeMonitors", "1");
+    }
+
     TS_ASSERT_THROWS_NOTHING(alg.execute());
 
     TS_ASSERT( alg.isExecuted() );
 
-		Workspace2D_sptr outWS = boost::dynamic_pointer_cast<Workspace2D>(AnalysisDataService::Instance().retrieve(wsName));
+    MatrixWorkspace_const_sptr outWS =
+        boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve(wsName));
 
-		checkDeadDetectors(outWS, expectedHits);
-	}
-
-	void checkDeadDetectors(Workspace2D_sptr outWS,std::string expectedHits)
-	{
-		//check that the detectors have actually been marked dead
-		std::vector<int> expectedDetectorArray = convertStringToVector(expectedHits);
-		boost::shared_ptr<IInstrument> i = outWS->getInstrument();
-		for (std::vector<int>::iterator it = expectedDetectorArray.begin(); it!=expectedDetectorArray.end(); ++it) 
-		{
-			TS_ASSERT( i->getDetector((*it))->isMasked() )
-		}
+    checkDeadDetectors(outWS, expectedHits);
   }
 
-	std::vector<int> convertStringToVector(const std::string input)
-	{
-		ArrayProperty<int> arrayProp("name",input);
-		return arrayProp();
-	}
+  void checkDeadDetectors(Mantid::API::MatrixWorkspace_const_sptr outWS,std::string expectedHits)
+  {
+    //check that the detectors have actually been marked dead
+    std::vector<int> expectedDetectorArray = convertStringToVector(expectedHits);
+    Mantid::Geometry::IInstrument_const_sptr i = outWS->getInstrument();
+    for (std::vector<int>::iterator it = expectedDetectorArray.begin(); it!=expectedDetectorArray.end(); ++it)
+    {
+      TS_ASSERT( i->getDetector((*it))->isMasked() )
+    }
+  }
 
-	std::string loadTestWS()
-	{
-    LoadEmptyInstrument loaderSLS;
+  std::vector<int> convertStringToVector(const std::string input)
+  {
+    Mantid::Kernel::ArrayProperty<int> arrayProp("name",input);
+    return arrayProp();
+  }
+
+  std::string loadTestWS()
+  {
+    Mantid::DataHandling::LoadEmptyInstrument loaderSLS;
 
     TS_ASSERT_THROWS_NOTHING(loaderSLS.initialize());
     TS_ASSERT( loaderSLS.isInitialized() );
     inputFile = "SANDALS_Definition.xml";
     loaderSLS.setPropertyValue("Filename", inputFile);
-    wsName = "MaskDetectorsInShapeTestSLS";
+    wsName = "MaskDetectorsInShapeTest_MaskDetectorsInShapeTestSLS";
     loaderSLS.setPropertyValue("OutputWorkspace", wsName);
 
-    std::string result;
-    TS_ASSERT_THROWS_NOTHING( result = loaderSLS.getPropertyValue("Filename") )
-    TS_ASSERT( ! result.compare(inputFile));
-
-    TS_ASSERT_THROWS_NOTHING( result = loaderSLS.getPropertyValue("OutputWorkspace") )
-    TS_ASSERT( ! result.compare(wsName));
-
     TS_ASSERT_THROWS_NOTHING(loaderSLS.execute());
-
     TS_ASSERT( loaderSLS.isExecuted() );
 
-
-    MatrixWorkspace_sptr output;
-    output = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve(wsName));
-    
-    // Check the total number of elements in the map for SLS
-    TS_ASSERT_EQUALS(output->spectraMap().nElements(),683);
-	return wsName;
-	}
+    return wsName;
+  }
 
 
 private:
