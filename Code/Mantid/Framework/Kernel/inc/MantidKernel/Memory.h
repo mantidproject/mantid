837ae9964ecc4f3d28643c1878aedb33b774e20f
Peter Peterson <petersonpf@ornl.gov>
2011-01-26 16:37:27 +0000

    Trying to get windoze build working again. Refs #2282.

--- Code/Mantid/Framework/Kernel/inc/MantidKernel/Memory.h
+++ Code/Mantid/Framework/Kernel/inc/MantidKernel/Memory.h
@@ -24,13 +24,13 @@ public:
   std::size_t totalMem() const;
   std::size_t availMem() const;
   double getFreeRatio() const;
-  friend std::ostream& operator<<(std::ostream& out, const MemoryStats &stats);
 private:
   MemoryStatsIgnore ignore; /// What fields to ignore.
   std::size_t vm_usage; /// Virtual memory usage by process in kiB.
   std::size_t res_usage; /// Resident memory usage by process in kiB.
   std::size_t total_memory; /// Total physical memory of system in kiB.
   std::size_t avail_memory; /// Available memory of system in kiB.
+  friend std::ostream& operator<<(std::ostream& out, const MemoryStats &stats);
 };
 
 DLLExport std::ostream& operator<<(std::ostream& out, const MemoryStats &stats);
