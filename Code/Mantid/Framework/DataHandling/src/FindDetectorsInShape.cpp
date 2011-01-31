22932ef78830352d0dd7ea5f89fd1ee85182edfa
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-01-31 11:53:45 +0000

    Checked in some changes accidentally, reverting them. Re #2276

--- Code/Mantid/Framework/DataHandling/src/FindDetectorsInShape.cpp
+++ Code/Mantid/Framework/DataHandling/src/FindDetectorsInShape.cpp
@@ -16,93 +16,92 @@ using Poco::XML::Element;
 
 namespace Mantid
 {
-  namespace DataHandling
-  {
-    // Register the algorithm into the algorithm factory
-    DECLARE_ALGORITHM(FindDetectorsInShape)
+	namespace DataHandling
+	{
+		// Register the algorithm into the algorithm factory
+		DECLARE_ALGORITHM(FindDetectorsInShape)
 
-      using namespace Kernel;
-    using namespace API;
+		using namespace Kernel;
+		using namespace API;
     using namespace Geometry;
 
-    /// (Empty) Constructor
-    FindDetectorsInShape::FindDetectorsInShape() {}
+		/// (Empty) Constructor
+		FindDetectorsInShape::FindDetectorsInShape() {}
 
-    /// Destructor
-    FindDetectorsInShape::~FindDetectorsInShape() {}
+		/// Destructor
+		FindDetectorsInShape::~FindDetectorsInShape() {}
 
-    void FindDetectorsInShape::init()
-    {
-      declareProperty(
+		void FindDetectorsInShape::init()
+		{
+			declareProperty(
         new WorkspaceProperty<MatrixWorkspace>("Workspace","",Direction::Input),
         "Name of the input workspace" );
-      declareProperty("ShapeXML", "", new MandatoryValidator<std::string>(),
-		      "The XML definition of the shape");
-      declareProperty("IncludeMonitors", false,
-		      "Whether monitors should be included if they are contained in the\n"
-		      "shape (default false)");
-      declareProperty("DetectorList", std::vector<int>(),
-		      new NullValidator< std::vector<int> >,                                      //the null validator always returns valid, there is no validation
-		      "The list of detector ids included within the shape",
-		      Direction::Output);
-    }
+			declareProperty("ShapeXML", "", new MandatoryValidator<std::string>(),
+        "The XML definition of the shape");
+			declareProperty("IncludeMonitors", false,
+        "Whether monitors should be included if they are contained in the\n"
+        "shape (default false)");
+			declareProperty("DetectorList", std::vector<int>(),
+        new NullValidator< std::vector<int> >,                                      //the null validator always returns valid, there is no validation
+        "The list of detector ids included within the shape",
+        Direction::Output);
+		}
 
-    void FindDetectorsInShape::exec()
-    {
-      // Get the input workspace
-      const MatrixWorkspace_sptr WS = getProperty("Workspace");
+		void FindDetectorsInShape::exec()
+		{
+			// Get the input workspace
+			const MatrixWorkspace_sptr WS = getProperty("Workspace");
 			
-      bool includeMonitors = getProperty("IncludeMonitors");
+			bool includeMonitors = getProperty("IncludeMonitors");
 
-      std::string shapeXML = getProperty("ShapeXML");
+			std::string shapeXML = getProperty("ShapeXML");
 
-      //convert into a Geometry object
-      Geometry::ShapeFactory sFactory;
-      boost::shared_ptr<Geometry::Object> shape_sptr = sFactory.createShape(shapeXML);
+			//convert into a Geometry object
+			Geometry::ShapeFactory sFactory;
+			boost::shared_ptr<Geometry::Object> shape_sptr = sFactory.createShape(shapeXML);
 
-      //get the instrument out of the workspace
-      IInstrument_sptr instrument_sptr = WS->getInstrument();
+			//get the instrument out of the workspace
+			IInstrument_sptr instrument_sptr = WS->getInstrument();
       IInstrument::plottables_const_sptr objCmptList = instrument_sptr->getPlottable();
 
-      std::vector<int> foundDets;
+			std::vector<int> foundDets;
 
-      //progress
-      int objCmptCount = objCmptList->size();
-      int iprogress_step = objCmptCount / 100;
-      if (iprogress_step == 0) iprogress_step = 1;
+			//progress
+			int objCmptCount = objCmptList->size();
+			int iprogress_step = objCmptCount / 100;
+			if (iprogress_step == 0) iprogress_step = 1;
 
-      //for every plottable item
-      for (IInstrument::plottables::const_iterator it = objCmptList->begin(); it!=objCmptList->end(); ++it) 
-      {
-	//attempt to dynamic cast up to an IDetector
-	boost::shared_ptr<const Geometry::IDetector> detector_sptr =
-	  boost::dynamic_pointer_cast<const Geometry::IDetector>((*it));
+			//for every plottable item
+			for (IInstrument::plottables::const_iterator it = objCmptList->begin(); it!=objCmptList->end(); ++it) 
+			{
+				//attempt to dynamic cast up to an IDetector
+				boost::shared_ptr<const Geometry::IDetector> detector_sptr =
+					boost::dynamic_pointer_cast<const Geometry::IDetector>((*it));
 
-	if (detector_sptr)
-	{
-
-	  if ((includeMonitors) || (!detector_sptr->isMonitor()))
-	  {
-	    //check if the centre of this item is within the user defined shape
-	    if (shape_sptr->isValid(detector_sptr->getPos()))
-	    {
-	      //shape encloses this objectComponent
+				if (detector_sptr)
+				{
+					if ((includeMonitors) || (!detector_sptr->isMonitor()))
+					{
+						//check if the centre of this item is within the user defined shape
+						if (shape_sptr->isValid(detector_sptr->getPos()))
+						{
+							//shape encloses this objectComponent
               g_log.debug()<<"Detector contained in shape " << detector_sptr->getID() << std::endl;
-	      foundDets.push_back(detector_sptr->getID());
-	    }
-	  }
-	}
+							foundDets.push_back(detector_sptr->getID());
+						}
+					}
+				}
 
-	int i=objCmptList->end()-it;
-	if (i % iprogress_step == 0)
-	{
-	  progress(double(i)/objCmptCount);
-	  interruption_point();
-	}
-      }
-      setProperty("DetectorList",foundDets);
-    }
+				int i=objCmptList->end()-it;
+				if (i % iprogress_step == 0)
+				{
+					progress(double(i)/objCmptCount);
+					interruption_point();
+				}
+			}
+			setProperty("DetectorList",foundDets);
+		}
 
-  } // namespace DataHandling
+	} // namespace DataHandling
 } // namespace Mantid
 
