5119c1a75bf8d858137e4364cd790d715f22f92a
Sofia Antony <sofia.antony@stfc.ac.uk>
2011-02-04 09:49:11 +0000

    re-#2155 created a new class UserStringParser to parse the user given string.This class gives a method parse which takes a string as input and returns a vector of vectors of numbers by splitting the string to different tokens using separators +,-,:

---
+++ Code/Mantid/Framework/Kernel/src/UserStringParser.cpp
@@ -0,0 +1,235 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidKernel/UserStringParser.h"
+#include "boost/lexical_cast.hpp"
+
+namespace Mantid
+{
+  namespace Kernel
+  {
+    ///constructor
+    UserStringParser::UserStringParser()
+    {
+    }
+    ///Destructor
+    UserStringParser::~UserStringParser()
+    {
+
+    }
+  /**This method parses a given string of numbers and returns a vector of vector of numbers.
+    *@param userString - the string to parse
+    *@returns  a vector containing vectors of numbers.
+   */
+    std::vector<std::vector<unsigned int>> UserStringParser::parse(const std::string& userString)
+    {
+      std::vector<std::vector<unsigned int>> numbers;
+      //first separate commas
+      std::vector<std::string> commaseparatedstrings;
+      if(userString.find(",")!=std::string::npos)
+      {
+        commaseparatedstrings=separateComma(userString);
+      }
+
+      if(!commaseparatedstrings.empty())
+      {     
+        std::vector<std::string>::const_iterator citr;
+        for(citr=commaseparatedstrings.begin();citr!=commaseparatedstrings.end();++citr)
+        { 
+          convertToNumbers((*citr),numbers);
+        }
+
+      }
+      else
+      {
+        convertToNumbers(userString,numbers);
+
+      }
+      return numbers;
+      
+    }
+
+  /**This method parses a given string of numbers and returns a vector of vector of numbers.
+    *@param userString - the input  string to parse
+    *@param numbers- a vector containing vectors of numbers.
+   */
+    void  UserStringParser::convertToNumbers(const std::string& userString,
+                                                             std::vector<std::vector<unsigned int>>& numbers)
+    {
+
+      //look for  separators
+      std::string separators("-+:");
+      std::vector<unsigned int>value;
+      //if input contains no separator string 
+      if(userString.find_first_of(separators)==std::string::npos)
+      { 
+        numbers.push_back(std::vector<unsigned int>(1,toUInt(userString)));
+      }
+      else if(Contains(userString,'-'))
+      {
+        std::vector<unsigned int>value =separateDelimiters(userString,"-:"); 
+        if(!value.empty())
+        {
+          numbers.push_back(value);
+        }
+      }
+      else if (Contains(userString,'+'))
+      {
+        std::vector<unsigned int>value =separateDelimiters(userString,"+");
+        if(!value.empty())
+        {
+          numbers.push_back(value);
+        }
+      }
+      else if (Contains(userString,':'))
+      {
+        std::vector<std::vector<unsigned int>>colonseparated= separateColon(userString);
+        std::vector<std::vector<unsigned int>>::const_iterator citr1;
+        for(citr1=colonseparated.begin();citr1!=colonseparated.end();++citr1)
+        {
+          numbers.push_back((*citr1));
+        }
+      }
+    
+    }
+
+  /** This method checks input string contains character ch 
+    * @input - the input string
+    * @input - character ch to search
+    * @return - true if the string contains character ch.
+    */
+    bool UserStringParser::Contains(const std::string& input,char ch)
+    {
+      std::string::size_type pos = input.find(ch);
+      return (pos==std::string::npos?false:true);
+    }
+
+  /**This method parses a given string of numbers into comma separated tokens.
+    *@param input - the string to parse
+    *@returns  a vector containing comma separated tokens.
+    */
+    std::vector<std::string> UserStringParser::separateComma(const std::string& input)
+    {
+     
+      typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+      boost::char_separator<char> commasep(",");
+       std::vector<std::string> commaseparatedvalues;
+      
+      tokenizer tokens(input,commasep);
+      for(tokenizer::iterator tokItr=tokens.begin();tokItr!=tokens.end();++tokItr)
+      {       
+          commaseparatedvalues.push_back(*tokItr);
+      }
+      return commaseparatedvalues;
+         
+    }
+
+   /**This method parses a given string of numbers into colon separated tokens.
+     *@param input - the string to parse
+     *@returns  a vector of vector containing colon separated tokens.
+    */
+    std::vector<std::vector<unsigned int>> UserStringParser::separateColon(const std::string& input)
+    {     
+      unsigned int startNum=0;
+      unsigned int endNum=0;
+      unsigned int step=1;
+      std::vector<std::vector<unsigned int>> separatedValues;
+      Tokenize(input,":",startNum,endNum,step);
+      for(unsigned int num=startNum;num<=endNum;num+=step)
+      {
+        separatedValues.push_back(std::vector<unsigned int>(1,num));
+      }
+
+      return separatedValues;
+
+    }
+
+   /**This method parses a given string of numbers into tokens using the separator character symbol.
+     *@param input - the string to parse
+     *@param delimiters - the string used as separator
+     *@returns  a vector of vector containing colon separated tokens.
+    */
+    std::vector<unsigned int> UserStringParser::separateDelimiters(const std::string& input,const std::string& delimiters)
+    {     
+      unsigned int startNum=0;
+      unsigned int endNum=0;
+      unsigned int step=1;
+      std::vector<unsigned int> separatedValues;
+      Tokenize(input,delimiters,startNum,endNum,step);
+      
+      for(unsigned int num=startNum;num<=endNum;num+=step)
+      {
+        separatedValues.push_back(num);
+      }
+
+      return separatedValues;
+
+    }
+  
+  /**This method parses a given string of numbers into tokens based on the given separator
+    *returns numbers corresponding to separated tokens and separators if any
+    *@param input - the string to parse
+    *@param delimiter - character to separate
+    *@param start - a number corresponding to the string before delimiter
+    *@param end -  a number corresponding to the string after delimiter
+    *@param step - number used to increment from the start num generate the series of numbers . 
+    */
+    void UserStringParser::Tokenize(const std::string& input,const std::string& delimiter,
+      unsigned int& start, unsigned int& end,unsigned int& step)
+    {
+      typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+      
+      boost::char_separator<char> seps(delimiter.c_str());
+      std::vector<unsigned int> separatedValues;
+      std::vector<std::string> temp;
+      tokenizer tokens(input,seps);
+      for(tokenizer::const_iterator tokItr=tokens.begin();tokItr!=tokens.end();++tokItr)
+      {
+        temp.push_back(*tokItr);
+        
+      }
+      if(temp.empty())
+      {
+        return ;
+      }
+      try
+      {
+        start = toUInt(temp.at(0)); 
+        end= toUInt(temp.at(1)); 
+        if(temp.size()==3)
+        {   
+          //at this point the input string contains a step string like 2010:2020:2
+          step=toUInt(temp.at(2)); 
+        }
+
+      }
+      catch(boost::bad_lexical_cast&)
+      {
+        throw std::runtime_error("Error when parsing the string "+input);
+      }
+      catch(std::out_of_range&)
+      {
+        throw std::runtime_error("Error when parsing the string "+input);
+      }
+    }
+    
+
+  /**This method converts a string to unsigned int
+    *@param input - the string to parse
+    *@returns an unsigned number.
+    */
+    unsigned int  UserStringParser::toUInt(const std::string& input)
+    {
+      try
+      {
+        return boost::lexical_cast<unsigned int>(input);
+      }
+      catch(boost::bad_lexical_cast&)
+      {
+        throw std::runtime_error("Error when parsing the range string"+input);
+      }
+    }
+
+    
+  }
+}
---
+++ Code/Mantid/Framework/Kernel/test/UserStringParserTest.h
@@ -0,0 +1,190 @@
+#ifndef RANGESTRINGPARSERTEST_H_
+#define RANGESTRINGPARSERTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidKernel/UserStringParser.h"
+#include <vector>
+
+using namespace Mantid::Kernel;
+
+class UserStringParserTest : public CxxTest::TestSuite
+{
+public:
+
+  void testColonSeparatedStrings()
+  {
+    /*std::string s;
+    std::getline(std::cin,s);*/
+
+    UserStringParser parser;
+    std::string str="2010:2020:2";
+    unsigned int startNum=2010;
+
+
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    std::vector<std::vector<unsigned int>>::const_iterator parsedValcitr;
+    for(parsedValcitr=parsedVals.begin();parsedValcitr!=parsedVals.end();++parsedValcitr)
+    {
+      std::vector<unsigned int>::const_iterator citr;
+      for(citr=(*parsedValcitr).begin();citr!=(*parsedValcitr).end();++citr)
+      {
+       TS_ASSERT_EQUALS(*citr, startNum )
+       startNum+=2;
+      }
+    }
+   
+  }
+
+  void testMinusSeparatedStrings()
+  {    
+    UserStringParser parser;
+    std::string str="60-61";
+    unsigned int startNum=60;
+    
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    std::vector<std::vector<unsigned int>>::const_iterator parsedValcitr;
+    for(parsedValcitr=parsedVals.begin();parsedValcitr!=parsedVals.end();++parsedValcitr)
+    {
+      std::vector<unsigned int>::const_iterator citr;
+      for(citr=(*parsedValcitr).begin();citr!=(*parsedValcitr).end();++citr)
+      {
+       TS_ASSERT_EQUALS(*citr, startNum )
+       startNum+=1;
+      }
+    }
+
+  }
+
+  void testMinusSeparatedRangeofStrings()
+  {
+    /*std::string s;
+    std::getline(std::cin,s);*/
+
+    UserStringParser parser;
+    std::string str="60-85";
+    unsigned int startNum=60;
+           
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    std::vector<std::vector<unsigned int>>::const_iterator parsedValcitr;
+    for(parsedValcitr=parsedVals.begin();parsedValcitr!=parsedVals.end();++parsedValcitr)
+    {
+      std::vector<unsigned int>::const_iterator citr;
+      for(citr=(*parsedValcitr).begin();citr!=(*parsedValcitr).end();++citr)
+      {
+        TS_ASSERT_EQUALS(*citr, startNum )
+        startNum+=1;
+      }
+               
+    }
+
+  }
+
+   void testPlusSeparatedStrings()
+  {    
+    UserStringParser parser;
+    std::string str="62+63";
+    unsigned int startNum=62;
+
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    std::vector<std::vector<unsigned int>>::const_iterator parsedValcitr;
+    for(parsedValcitr=parsedVals.begin();parsedValcitr!=parsedVals.end();++parsedValcitr)
+    {
+      std::vector<unsigned int>::const_iterator citr;
+      for(citr=(*parsedValcitr).begin();citr!=(*parsedValcitr).end();++citr)
+      {
+       TS_ASSERT_EQUALS(*citr, startNum )
+       startNum+=1;
+      }
+    }
+    
+   
+  }
+
+  void testMinusandColonSeparatedStrings()
+  { 
+    std::string s;
+    std::getline(std::cin,s);
+    UserStringParser parser;
+    std::string str="4010-4020:2";
+    unsigned int startNum=4010;
+
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    std::vector<std::vector<unsigned int>>::const_iterator parsedValcitr;
+    for(parsedValcitr=parsedVals.begin();parsedValcitr!=parsedVals.end();++parsedValcitr)
+    {
+      std::vector<unsigned int>::const_iterator citr;
+      for(citr=(*parsedValcitr).begin();citr!=(*parsedValcitr).end();++citr)
+      {
+        TS_ASSERT_EQUALS(*citr, startNum )
+          startNum+=2;
+      }
+    }
+    
+  }
+
+  void testCommaSeparatedComplexStrings1()
+  {
+        
+    UserStringParser parser;
+    std::string str="60-61,62+63,64-65";
+   
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    TS_ASSERT_EQUALS(parsedVals.size(), 3 )
+
+    TS_ASSERT_EQUALS((parsedVals[0][0]),60)
+    TS_ASSERT_EQUALS((parsedVals[0][1]),61)
+    TS_ASSERT_EQUALS((parsedVals[1][0]),62)
+    TS_ASSERT_EQUALS((parsedVals[1][1]),63)
+    TS_ASSERT_EQUALS((parsedVals[2][0]),64)
+    TS_ASSERT_EQUALS((parsedVals[2][1]),65)
+    
+  }
+
+  void testCommaSeparatedComplexStrings2()
+  {
+        
+    UserStringParser parser;
+    std::string str="60,61+62,64-66,68-74:2";
+         
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    TS_ASSERT_EQUALS(parsedVals.size(), 4 )
+
+    TS_ASSERT_EQUALS((parsedVals[0][0]),60)
+    TS_ASSERT_EQUALS((parsedVals[1][0]),61)
+    TS_ASSERT_EQUALS((parsedVals[1][1]),62)
+    TS_ASSERT_EQUALS((parsedVals[2][0]),64)
+    TS_ASSERT_EQUALS((parsedVals[2][1]),65)
+    TS_ASSERT_EQUALS((parsedVals[2][2]),66)
+
+    TS_ASSERT_EQUALS((parsedVals[3][0]),68)
+    TS_ASSERT_EQUALS((parsedVals[3][1]),70)
+    TS_ASSERT_EQUALS((parsedVals[3][2]),72)
+
+   
+  }
+
+  void testCommaSeparatedComplexStrings3()
+  {
+        
+    UserStringParser parser;
+    std::string str="60,61,62:68";
+         
+    std::vector<std::vector<unsigned int>> parsedVals=parser.parse(str);
+    TS_ASSERT_EQUALS(parsedVals.size(),9 )
+
+    TS_ASSERT_EQUALS((parsedVals[0][0]),60)
+    TS_ASSERT_EQUALS((parsedVals[1][0]),61)
+   /* TS_ASSERT_EQUALS((parsedVals[2]0]),62)
+    TS_ASSERT_EQUALS((parsedVals[3][0]),64)
+    TS_ASSERT_EQUALS((parsedVals[4][0]),65)
+    TS_ASSERT_EQUALS((parsedVals[5][0]),66)*/
+       
+  }
+
+
+private:
+ 
+};
+
+#endif 
