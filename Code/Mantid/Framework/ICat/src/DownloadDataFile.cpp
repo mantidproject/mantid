b33e117f0607c094105affdf027b3ebbac0e6aec
Stuart Campbell <campbellsi@ornl.gov>
2011-02-05 02:06:16 +0000

    refs #1349

--- Code/Mantid/Framework/ICat/src/DownloadDataFile.cpp
+++ Code/Mantid/Framework/ICat/src/DownloadDataFile.cpp
@@ -11,12 +11,12 @@
 #include "MantidKernel/FacilityInfo.h"
 
 
-#include "Poco/Net/HTTPClientSession.h"
-#include "Poco/Net/HTTPRequest.h"
-#include "Poco/Net/HTTPResponse.h"
-#include "Poco/StreamCopier.h"
-#include "Poco/Path.h"
-#include "Poco/URI.h"
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Path.h>
+#include <Poco/URI.h>
 #include <fstream>
 #include <iomanip>
 
