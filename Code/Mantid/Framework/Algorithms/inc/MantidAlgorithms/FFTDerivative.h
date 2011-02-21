d3bec142f13552d4362d1fd5266f3e8d4021174b
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2011-02-21 15:28:24 +0000

    Added algorithm FFTDerivative. re #2517

---
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/FFTDerivative.h
@@ -0,0 +1,61 @@
+#ifndef FFTDERIVATIVE_H_
+#define FFTDERIVATIVE_H_
+
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+/** Calculates derivatives of the spectra in a MatrixWorkspace using a Fast Fourier Transform.
+
+    @author Roman Tolchenov
+    @date 21/02/2011
+
+    Copyright &copy; 2008 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+ */
+class FFTDerivative : public Mantid::API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  FFTDerivative() : Mantid::API::Algorithm() {}
+  /// Virtual destructor
+  virtual ~FFTDerivative() {}
+  /// Algorithm's name
+  virtual const std::string name() const { return "FFTDerivative"; }
+  /// Algorithm's version
+  virtual int version() const { return (1); }
+  /// Algorithm's category for identification
+  virtual const std::string category() const { return "General"; }
+
+private:
+  /// Initialisation code
+  void init();
+  ///Execution code
+  void exec();
+
+};
+
+} // FFT
+} // Mandid
+
+#endif /*DERIVATIVE_H_*/
---
+++ Code/Mantid/Framework/Algorithms/test/FFTDerivativeTest.h
@@ -0,0 +1,89 @@
+#ifndef FFT_DERIVATIVE_TEST_H_
+#define FFT_DERIVATIVE_TEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidAlgorithms/FFTDerivative.h"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidDataObjects/Workspace2D.h"
+
+using namespace Mantid;
+using namespace Mantid::API;
+
+class FFTDerivativeTest : public CxxTest::TestSuite
+{
+public:
+  FFTDerivativeTest()
+  {
+  }
+  ~FFTDerivativeTest()
+  {
+  }
+
+  void testGaussianDerivative()
+  {
+    const int N = 100;
+
+    createWS(N,0,"exp");
+
+    IAlgorithm* fft = Mantid::API::FrameworkManager::Instance().createAlgorithm("FFTDerivative");
+    fft->initialize();
+    fft->setPropertyValue("InputWorkspace","FFTDerivative_WS_exp");
+    fft->setPropertyValue("OutputWorkspace","FFTDerivative_out");
+    fft->execute();
+
+    MatrixWorkspace_sptr fWS = boost::dynamic_pointer_cast<MatrixWorkspace>
+      (AnalysisDataService::Instance().retrieve("FFTDerivative_out"));
+
+    const MantidVec& X = fWS->readX(0);
+    const MantidVec& Y = fWS->readY(0);
+
+    TS_ASSERT_EQUALS(Y.size(),101);
+
+    for(int i=0; i < Y.size(); ++i)
+    {
+      double xx = X[i] - 5.1;
+      TS_ASSERT_DELTA( Y[i],(-2 * xx * exp(-(xx*xx)*2)*2),0.000001);
+    }
+
+    FrameworkManager::Instance().deleteWorkspace("FFTDerivative_WS_exp");
+    FrameworkManager::Instance().deleteWorkspace("FFTDerivative_out");
+  }
+
+
+private:
+
+  MatrixWorkspace_sptr createWS(int n,int dn,const std::string& name)
+  {
+    FrameworkManager::Instance();
+    Mantid::DataObjects::Workspace2D_sptr ws = boost::dynamic_pointer_cast<Mantid::DataObjects::Workspace2D>
+      (WorkspaceFactory::Instance().create("Workspace2D",1,n+dn,n));
+
+    const double dX = 10.0 / (n-1);
+    const double x0 = 0.;
+    const double c = 5.1;
+    Mantid::MantidVec& X = ws->dataX(0);
+    Mantid::MantidVec& Y = ws->dataY(0);
+    Mantid::MantidVec& E = ws->dataE(0);
+
+    for(int i=0;i<n;i++)
+    {
+      double x = x0 + dX*(i);
+      X[i] = x;
+      Y[i] = exp(-(x-c)*(x-c)*2);
+      E[i] = 1.;
+    }
+
+    if (dn > 0)
+      X[n] = X[n-1] + dX;
+
+    AnalysisDataService::Instance().add("FFTDerivative_WS_"+name,ws);
+
+    return ws;
+  }
+
+};
+
+#endif /*FFT_DERIVATIVE_TEST_H_*/
