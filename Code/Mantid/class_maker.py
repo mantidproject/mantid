73a99ab9ece0eea968f28228793e2946da2c6243
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-02-25 16:56:56 +0000

    Refs #2501: class_maker.py update to create an algorithm stub.

--- Code/Mantid/class_maker.py
+++ Code/Mantid/class_maker.py
@@ -9,18 +9,42 @@ import datetime
 
 
 #======================================================================
-def write_header(subproject, classname, filename):
+def write_header(subproject, classname, filename, algorithm):
     """Write a class header file"""
     print "Writing header file to %s" % filename
     f = open(filename, 'w')
     
     guard = "MANTID_%s_%s_H_" % (subproject.upper(), classname.upper())
     
+    # Create an Algorithm header; will not use it if 
+    algorithm_header = """
+    /// Algorithm's name for identification 
+    virtual const std::string name() const { return "%s";};
+    /// Algorithm's version for identification 
+    virtual int version() const { return 1;};
+    /// Algorithm's category for identification
+    virtual const std::string category() const { return "General";}
+    
+  private:
+    void init();
+    void exec();
+
+""" % classname
+        
+    alg_class_declare = " : public API::Algorithm"
+    alg_include = """#include "MantidAPI/Algorithm.h" """
+    
+    if not algorithm:
+        algorithm_header = ""
+        alg_class_declare = ""
+        alg_include = ""
+    
+    # The full text
     s = """#ifndef %s
 #define %s
     
 #include "MantidKernel/System.h"
-
+%s
 
 namespace Mantid
 {
@@ -29,14 +53,15 @@ namespace %s
 
   /** %s : TODO: DESCRIPTION
    * 
-   * @author:
-   * @date: %s
+   * @author
+   * @date %s
    */
-  class DLLExport %s
+  class DLLExport %s %s
   {
   public:
     %s();
     ~%s();
+    %s
   };
 
 
@@ -44,19 +69,55 @@ namespace %s
 } // namespace %s
 
 #endif  /* %s */
-""" % (guard, guard, subproject, classname, datetime.datetime.now(), classname,classname,classname, subproject, guard)
+""" % (guard, guard, alg_include, subproject, classname, datetime.datetime.now(), classname, alg_class_declare, classname,classname, algorithm_header, subproject, guard)
     f.write(s)
     f.close()
-    
+
 
 
 
 
 #======================================================================
-def write_source(subproject, classname, filename):
+def write_source(subproject, classname, filename, algorithm):
     """Write a class source file"""
     print "Writing source file to %s" % filename
     f = open(filename, 'w')
+    
+    algorithm_top = """
+  // Register the algorithm into the AlgorithmFactory
+  DECLARE_ALGORITHM(%s)
+  
+  using namespace Mantid::Kernel;
+  using namespace Mantid::API;
+""" % (classname)
+
+    algorithm_source = """
+  //----------------------------------------------------------------------------------------------
+  /** Initialize the algorithm's properties.
+   */
+  void %s::init()
+  {
+    setOptionalMessage("TODO: Enter a quick description of your algorithm.");
+    declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input), "An input workspace.");
+    declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output), "An output workspace.");
+  }
+
+
+  //----------------------------------------------------------------------------------------------
+  /** Execute the algorithm.
+   */
+  void %s::exec()
+  {
+    // TODO Auto-generated execute stub
+  }
+
+"""% (classname, classname)   
+
+    if not algorithm:
+        algorithm_top = ""
+        algorithm_source = ""
+        
+    
     s = """#include "Mantid%s/%s.h"
 #include "MantidKernel/System.h"
 
@@ -64,6 +125,7 @@ namespace Mantid
 {
 namespace %s
 {
+%s
 
   //----------------------------------------------------------------------------------------------
   /** Constructor
@@ -80,24 +142,38 @@ namespace %s
   {
     // TODO Auto-generated destructor stub
   }
+  
+%s
 
 } // namespace Mantid
 } // namespace %s
 
-""" % (subproject, classname, subproject, classname,classname,classname,classname, subproject)
+""" % (subproject, classname, subproject, algorithm_top, classname,classname,classname,classname,  algorithm_source, subproject)
     f.write(s)
     f.close()
 
 
     
 #======================================================================
-def write_test(subproject, classname, filename):
+def write_test(subproject, classname, filename, algorithm):
     """Write a class test file"""
     print "Writing test file to %s" % filename
     f = open(filename, 'w')
 
     guard = "MANTID_%s_%sTEST_H_" % (subproject.upper(), classname.upper())
-
+    algorithm_test = """
+    
+  void test_Init()
+  {
+    %s alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+  }
+  """ % classname
+  
+    if not algorithm:
+        algorithm_test = ""
+  
     s = """#ifndef %s
 #define %s
 
@@ -114,8 +190,8 @@ using namespace Mantid::%s;
 class %sTest : public CxxTest::TestSuite
 {
 public:
-
-  void test()
+%s
+  void test_Something()
   {
   }
 
@@ -125,13 +201,13 @@ public:
 
 #endif /* %s */
 
-""" % (guard,guard, subproject, classname,subproject, classname, guard)
+""" % (guard,guard, subproject, classname,subproject, classname, algorithm_test, guard)
     f.write(s)
     f.close()
     
 
 #======================================================================
-def generate(subproject, classname, overwrite, test_only):
+def generate(subproject, classname, overwrite, test_only, algorithm):
     # Directory at base of subproject
     basedir = os.path.join(os.path.curdir, "Framework/"+subproject)
     
@@ -151,15 +227,15 @@ def generate(subproject, classname, overwrite, test_only):
       
     print
     if not test_only:
-        write_header(subproject, classname, headerfile)
-        write_source(subproject, classname, sourcefile)
-    write_test(subproject, classname, testfile)
+        write_header(subproject, classname, headerfile, algorithm)
+        write_source(subproject, classname, sourcefile, algorithm)
+    write_test(subproject, classname, testfile, algorithm)
     
     print "\n   Don't forget to add your class to CMakeLists.txt:"
     print 
     if not test_only:
         print "\tsrc/%s.cpp" % (classname)
-        print "\tinc/Mantid%s/%s.cpp" % (subproject, classname)
+        print "\tinc/Mantid%s/%s.h" % (subproject, classname)
     print "\ttest/%sTest.h" % (classname)
     print  
 
@@ -177,11 +253,15 @@ if __name__ == "__main__":
     parser.add_argument('--test', dest='test', action='store_const',
                         const=True, default=False,
                         help='Create only the test file.')
+    parser.add_argument('--alg', dest='alg', action='store_const',
+                        const=True, default=False,
+                        help='Create an Algorithm stub. This adds some methods common to algorithms.')
      
     args = parser.parse_args()
     subproject = args.subproject
     classname = args.classname
     overwrite = args.force
     test_only = args.test
+    algorithm = args.alg
     
-    generate(subproject, classname, overwrite, test_only)
+    generate(subproject, classname, overwrite, test_only, algorithm)
