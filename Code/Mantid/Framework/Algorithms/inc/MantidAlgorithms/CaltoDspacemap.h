a263a1e29af8986223c0f2217388e01a314b5892
Vickie Lynch <lynchve@ornl.gov>
2011-03-04 15:16:53 +0000

    Refs #2581 Creating new algorithm CaltoDspacemap for writing binary dspacemap

---
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/CaltoDspacemap.h
@@ -0,0 +1,93 @@
+#ifndef MANTID_ALGORITHMS_CALTODSPACEMAP_H_
+#define MANTID_ALGORITHMS_CALTODSPACEMAP_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h"
+#include "MantidDataObjects/EventWorkspace.h"
+
+namespace Mantid
+{
+using DataObjects::EventList;
+using DataObjects::EventWorkspace;
+using DataObjects::EventWorkspace_sptr;
+using DataObjects::EventWorkspace_const_sptr;
+
+namespace Algorithms
+{
+
+/** Performs a unit change from TOF to dSpacing, correcting the X values to account for small
+    errors in the detector positions.
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the Workspace whose detectors are to be aligned </LI>
+    <LI> OutputWorkspace - The name of the Workspace in which the result of the algorithm will be stored </LI>
+    <LI> CalibrationFile - The file containing the detector offsets </LI>
+    </UL>
+
+    @author Vickie Lynch ORNL
+    @date 04/01/2011
+
+    Copyright &copy; 2008 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport CaltoDspacemap : public API::Algorithm
+{
+public:
+  CaltoDspacemap();
+  virtual ~CaltoDspacemap();
+
+  /// Algorithm's name for identification overriding a virtual method
+  virtual const std::string name() const { return "CaltoDspacemap";};
+  /// Algorithm's version for identification overriding a virtual method
+  virtual int version() const { return 1;};
+  /// Algorithm's category for identification overriding a virtual method
+  virtual const std::string category() const { return "Diffraction";}
+
+  static void WriteCalibrationFile(std::string calFileName, const std::map<int, Geometry::IDetector_sptr> & allDetectors ,
+                                    const std::map<int,double> &offsets, const std::map<int,bool> &selects, std::map<int,int> &groups);
+
+private:
+  /// Sets documentation strings for this algorithm
+  virtual void initDocs();
+  // Implement abstract Algorithm methods
+  void init();
+  void exec();
+
+  void execEvent();
+
+  void CalculateDspaceFromCal(Mantid::API::MatrixWorkspace_const_sptr inputWS,
+                                    const std::string DFileName,
+                                    std::map<int,double> &offsets);
+
+  /// Pointer for an event workspace
+  EventWorkspace_const_sptr eventW;
+
+};
+
+
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /* MANTID_ALGORITHMS_CALTODSPACEMAP_H_ */
---
+++ Code/Mantid/Framework/Algorithms/src/CaltoDspacemap.cpp
@@ -0,0 +1,164 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/AlignDetectors.h"
+#include "MantidAlgorithms/CaltoDspacemap.h"
+#include "MantidAPI/FileProperty.h"
+#include "MantidAPI/SpectraDetectorMap.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidGeometry/V3D.h"
+#include "MantidKernel/BinaryFile.h"
+#include "MantidKernel/PhysicalConstants.h"
+#include "MantidKernel/UnitFactory.h"
+#include <cmath>
+#include <fstream>
+
+namespace Mantid
+{
+namespace Algorithms
+{
+// Register the algorithm into the algorithm factory
+DECLARE_ALGORITHM(CaltoDspacemap)
+
+/// Sets documentation strings for this algorithm
+void CaltoDspacemap::initDocs()
+{
+  this->setWikiSummary("Creates a calibration file with offsets calculated from Dspacemap file. ");
+  this->setOptionalMessage("Creates a calibration file with offsets calculated from Dspacemap file.");
+}
+
+
+using namespace Kernel;
+using namespace API;
+using Geometry::IInstrument_const_sptr;
+using DataObjects::EventList;
+using DataObjects::EventWorkspace;
+using DataObjects::EventWorkspace_sptr;
+using DataObjects::EventWorkspace_const_sptr;
+
+
+
+
+//========================================================================
+//========================================================================
+/// (Empty) Constructor
+CaltoDspacemap::CaltoDspacemap()
+{
+}
+
+/// Destructor
+CaltoDspacemap::~CaltoDspacemap()
+{
+}
+
+//-----------------------------------------------------------------------
+void CaltoDspacemap::init()
+{
+  declareProperty(
+    new WorkspaceProperty<API::MatrixWorkspace>("InputWorkspace","",Direction::Input),
+    "A workspace with units of TOF" );
+
+  declareProperty(new FileProperty("CalibrationFile", "", FileProperty::Load, ".cal"),
+     "The CalFile on input contains the groups; on output contains the offsets");
+
+  declareProperty(new FileProperty("DspacemapFile", "", FileProperty::Save, ".dat"),
+     "The DspacemapFile containing the d-space mapping");
+
+}
+
+
+
+
+
+//-----------------------------------------------------------------------
+/** Executes the algorithm
+ *  @throw Exception::FileError If the calibration file cannot be opened and read successfully
+ *  @throw Exception::InstrumentDefinitionError If unable to obtain the source-sample distance
+ */
+void CaltoDspacemap::exec()
+{
+  // Get the input workspace
+  const MatrixWorkspace_const_sptr inputWS = getProperty("InputWorkspace");
+
+  // Read in the calibration data
+  const std::string DFileName = getProperty("DspacemapFile");
+
+  const std::string calFileName = getProperty("CalibrationFile");
+  std::map<int,double> offsets;
+  std::map<int,int> groups;
+  progress(0.0,"Reading calibration file");
+  if ( ! AlignDetectors::readCalFile(calFileName, offsets, groups) )
+    throw Exception::FileError("Problem reading calibration file", calFileName);
+
+  // generate map of the tof->d conversion factors
+  CalculateDspaceFromCal(inputWS, DFileName, offsets);
+
+}
+//-----------------------------------------------------------------------
+/**
+ * Make a map of the conversion factors between tof and D-spacing
+ * for all pixel IDs in a workspace.
+ * @param inputWS the workspace containing the instrument geometry
+ *    of interest.
+ * @param DFileName name of dspacemap file
+ * @param offsets map between pixelID and offset (from the calibration file)
+ */
+void CaltoDspacemap::CalculateDspaceFromCal(Mantid::API::MatrixWorkspace_const_sptr inputWS,
+                                  std::string DFileName, 
+                                  std::map<int,double> &offsets)
+{
+  const char * filename = DFileName.c_str();
+  // Get a pointer to the instrument contained in the workspace
+  IInstrument_const_sptr instrument = inputWS->getInstrument();
+  double l1;
+  Geometry::V3D beamline,samplePos;
+  double beamline_norm;
+  AlignDetectors::getInstrumentParameters(instrument,l1,beamline,beamline_norm, samplePos);
+
+  //To get all the detector ID's
+  const std::map<int, Geometry::IDetector_sptr> allDetectors = instrument->getDetectors();
+
+  // Selects (empty, will default to true)
+  std::map<int, bool> selects;
+
+  std::map<int, Geometry::IDetector_sptr>::const_iterator it;
+  int maxdetID = 0;
+  for (it = allDetectors.begin(); it != allDetectors.end(); it++)
+  {
+    int detectorID = it->first;
+    if(detectorID > maxdetID) maxdetID = detectorID;
+  }
+
+  // Now write the POWGEN-style Dspace mapping file
+  std::ofstream fout(filename, std::ios_base::out|std::ios_base::binary);
+  Progress prog(this,0.0,1.0,maxdetID);
+
+  for (int i = 0; i != maxdetID; i++)
+  {
+    //Compute the factor
+    double factor;
+    Geometry::IDetector_sptr det;
+    for (it = allDetectors.begin(); it != allDetectors.end(); it++)
+    {
+      if(it->first == i) break;
+    }
+    det = it->second;
+    if(det)
+    {
+      factor = AlignDetectors::calcConversion(l1, beamline, beamline_norm, samplePos, det, offsets[i], false);
+      //Factor of 10 between ISAW and Mantid
+      factor *= 0.1 ;
+      fout.write( reinterpret_cast<char*>( &factor ), sizeof(double) );
+    }
+    else
+    {
+      factor = 0;
+      fout.write( reinterpret_cast<char*>( &factor ), sizeof(double) );
+    }
+  }
+  fout.close();
+}
+
+} // namespace Algorithms
+} // namespace Mantid
