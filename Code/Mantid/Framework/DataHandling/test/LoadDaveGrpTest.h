5b5e5231ef6f17719cb647fde61ab240275e1e38
Michael Reuter <reuterma@ornl.gov>
2011-03-04 19:46:08 +0000

    Trying to make Windows build happy. This refs #2288.

--- Code/Mantid/Framework/DataHandling/test/LoadDaveGrpTest.h
+++ Code/Mantid/Framework/DataHandling/test/LoadDaveGrpTest.h
@@ -36,9 +36,9 @@ public:
       TS_ASSERT_EQUALS(outputWS->readX(0).size(), 60);
       TS_ASSERT_EQUALS(outputWS->readX(0)[0], 655.0);
       TS_ASSERT_EQUALS((*(outputWS->getAxis(1)))(1), 0.625);
-      TS_ASSERT_EQUALS(outputWS->readY(0)[1], 0.000106102311091);
-      TS_ASSERT_EQUALS(outputWS->readY(11)[59], 0.0116074689604);
-      TS_ASSERT_EQUALS(outputWS->readE(27)[7], 0.0187950781228);
+      TS_ASSERT_DELTA(outputWS->readY(0)[1], 0.000106102311091, 1e-6);
+      TS_ASSERT_DELTA(outputWS->readY(11)[59], 0.0116074689604, 1e-6);
+      TS_ASSERT_DELTA(outputWS->readE(27)[7], 0.0187950781228, 1e-6);
 
       dataStore.remove(outputWSName);
     }
