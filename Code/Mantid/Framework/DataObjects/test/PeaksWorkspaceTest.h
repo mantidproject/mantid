8eacbed7744097660177a0f986d65a5c6a83ece7
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-03-14 15:13:58 +0000

    Refs #2379: PeaksWorkspace test stub.

---
+++ Code/Mantid/Framework/DataObjects/test/PeaksWorkspaceTest.h
@@ -0,0 +1,27 @@
+#ifndef MANTID_DATAOBJECTS_PEAKSWORKSPACETEST_H_
+#define MANTID_DATAOBJECTS_PEAKSWORKSPACETEST_H_
+
+#include <cxxtest/TestSuite.h>
+#include "MantidKernel/Timer.h"
+#include "MantidKernel/System.h"
+#include <iostream>
+#include <iomanip>
+
+#include "MantidDataObjects/PeaksWorkspace.h"
+
+using namespace Mantid::DataObjects;
+
+class PeaksWorkspaceTest : public CxxTest::TestSuite
+{
+public:
+
+  void test_Something()
+  {
+  }
+
+
+};
+
+
+#endif /* MANTID_DATAOBJECTS_PEAKSWORKSPACETEST_H_ */
+
