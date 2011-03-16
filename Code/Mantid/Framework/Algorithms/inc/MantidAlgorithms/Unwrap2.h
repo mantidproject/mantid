e3c916646ceb7d80ebe9da3924600d922d14a2a1
Peter Peterson <petersonpf@ornl.gov>
2011-03-16 16:09:55 +0000

    Fairly well working of new unwrap function. Refs #2502.

---
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/Unwrap2.h
@@ -0,0 +1,83 @@
+#ifndef MANTID_ALGORITHMS_UNWRAP_H_
+#define MANTID_ALGORITHMS_UNWRAP_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+#include "MantidDataObjects/EventWorkspace.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+/** Takes an input Workspace2D that contains 'raw' data, unwraps the data according to
+    the reference flightpath provided and converts the units to wavelength.
+    The output workspace will have common bins in the maximum theoretical wavelength range.
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace  - The name of the input workspace. </LI>
+    <LI> OutputWorkspace - The name of the output workspace. </LI>
+    <LI> LRef            - The 'reference' flightpath (in metres). </LI>
+    </UL>
+
+    @author Russell Taylor, Tessella Support Services plc
+    @date 25/07/2008
+
+    Copyright &copy; 2008-9 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport Unwrap2 : public API::Algorithm
+{
+public:
+  Unwrap2();
+  virtual ~Unwrap2();
+  virtual const std::string name() const;
+  virtual int version() const;
+  virtual const std::string category() const;
+
+private:
+  /// Sets documentation strings for this algorithm
+  virtual void initDocs();
+  void init();
+  void exec();
+  void execEvent();
+
+  double calculateFlightpath(const std::size_t& spectrum, bool& isMonitor) const;
+  size_t unwrapX(const MantidVec&, MantidVec&, const double& Ld);
+  double m_conversionConstant; ///< The constant used in the conversion from TOF to wavelength
+  API::MatrixWorkspace_const_sptr m_inputWS; ///< Pointer to the input workspace
+  DataObjects::EventWorkspace_const_sptr m_inputEvWS; ///< Pointer to the input event workspace
+  double m_LRef; ///< The 'reference' flightpath
+  double m_L1; ///< The instrument initial flightpath
+  double m_Tmin; ///< The start of the time-of-flight frame
+  double m_Tmax; ///< The end of the time-of-flight frame
+  double m_frameWidth; ///< The width of the frame cached to speed up things
+  std::size_t m_numberOfSpectra; ///< The number of spectra in the workspace
+  std::size_t m_XSize; ///< The size of the X vectors in the input workspace
+  /// Progress reporting
+  API::Progress* m_progress;
+};
+
+} // namespace Algorithm
+} // namespace Mantid
+
+#endif /* MANTID_ALGORITHMS_UNWRAP_H_ */
---
+++ Code/Mantid/Framework/Algorithms/src/Unwrap2.cpp
@@ -0,0 +1,361 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/Unwrap2.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidDataObjects/EventList.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidKernel/PhysicalConstants.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+DECLARE_ALGORITHM(Unwrap2)
+
+/// Sets documentation strings for this algorithm
+void Unwrap2::initDocs()
+{
+  this->setWikiSummary("Takes an input [[workspace]] that contains 'raw' data, unwraps the data according to the reference flightpath provided and converts the units to wavelength. The output workspace will have common bins in the maximum theoretical wavelength range. ");
+  this->setOptionalMessage("Takes an input workspace that contains 'raw' data, unwraps the data according to the reference flightpath provided and converts the units to wavelength. The output workspace will have common bins in the maximum theoretical wavelength range.");
+}
+
+
+using namespace Kernel;
+using namespace API;
+using DataObjects::EventList;
+using DataObjects::EventWorkspace;
+using Geometry::IInstrument_const_sptr;
+using Kernel::Exception::InstrumentDefinitionError;
+using Kernel::Exception::NotFoundError;
+using std::size_t;
+
+/// Default constructor
+Unwrap2::Unwrap2():m_progress(NULL)
+{}
+
+/// Destructor
+Unwrap2::~Unwrap2()
+{
+  if(m_progress)
+    delete m_progress;
+  m_progress=NULL;
+}
+
+/// Algorithm's name for identification overriding a virtual method
+const std::string Unwrap2::name() const
+{
+  return "Unwrap";
+}
+
+/// Algorithm's version for identification overriding a virtual method
+int Unwrap2::version() const
+{
+  return 2;
+}
+
+/// Algorithm's category for identification overriding a virtual method
+const std::string Unwrap2::category() const
+{
+  return "Units";
+}
+
+/// Initialisation method
+void Unwrap2::init()
+{
+  CompositeValidator<> *wsValidator = new CompositeValidator<>;
+  wsValidator->add(new WorkspaceUnitValidator<>("TOF"));
+  wsValidator->add(new HistogramValidator<>);
+  wsValidator->add(new RawCountValidator<>);
+  wsValidator->add(new InstrumentValidator<>);
+  declareProperty(
+    new WorkspaceProperty<MatrixWorkspace>("InputWorkspace","",Direction::Input,wsValidator),
+    "A workspace with x values in units of TOF and y values in counts" );
+  declareProperty(
+    new WorkspaceProperty<MatrixWorkspace>("OutputWorkspace","",Direction::Output),
+    "The name of the workspace to be created as the output of the algorithm" );
+
+  BoundedValidator<double> *validator = new BoundedValidator<double>;
+  validator->setLower(0.01);
+  declareProperty("LRef", 0.0, validator,
+    "The length of the reference flight path (in metres)" );
+  validator = new BoundedValidator<double>;
+  validator->setLower(0.01);
+  declareProperty("Tmin", Mantid::EMPTY_DBL(), validator,
+                  "The minimum time-of-flight of the frame (in microseconds). If not set the data range will be used.");
+  validator = new BoundedValidator<double>;
+  validator->setLower(0.01);
+  declareProperty("Tmax", Mantid::EMPTY_DBL(), validator,
+                  "The minimum time-of-flight of the frame (in microseconds). If not set the data range will be used.");
+  declareProperty("ForceHist", false);
+
+  // Calculate and set the constant factor for the conversion to wavelength
+  const double TOFisinMicroseconds = 1e6;
+  const double toAngstroms = 1e10;
+  m_conversionConstant = (PhysicalConstants::h * toAngstroms) / (PhysicalConstants::NeutronMass * TOFisinMicroseconds);
+}
+
+/** Executes the algorithm
+ *  @throw std::runtime_error if the workspace is invalid or a child algorithm fails
+ *  @throw Kernel::Exception::InstrumentDefinitionError if detector, source or sample positions cannot be calculated
+ *
+ */
+void Unwrap2::exec()
+{
+  // Get the input workspace
+  m_inputWS = getProperty("InputWorkspace");
+
+  // without the primary flight path the algorithm cannot work
+  try {
+    IInstrument_const_sptr instrument = m_inputWS->getInstrument();
+    Geometry::IObjComponent_const_sptr sample = instrument->getSample();
+    m_L1 = instrument->getSource()->getDistance(*sample);
+  }
+  catch (NotFoundError e)
+  {
+    throw InstrumentDefinitionError("Unable to calculate source-sample distance",
+        m_inputWS->getTitle());
+  }
+
+
+  // Get the "reference" flightpath (currently passed in as a property)
+  m_LRef = getProperty("LRef");
+
+  // Get the min & max frame values
+  {
+    double temp;
+    double empty = Mantid::EMPTY_DBL();
+
+    temp = this->getProperty("Tmin");
+    if (temp != empty)
+      m_Tmin = temp;
+    else
+      m_Tmin = m_inputWS->dataX(0).front();
+
+    temp = this->getProperty("Tmax");
+    if (temp != empty)
+      m_Tmax = temp;
+    else
+      m_Tmax = m_inputWS->dataX(0).back();
+  }
+  m_frameWidth = m_Tmax - m_Tmin;
+
+  g_log.debug() << "Frame range in microseconds is: " << m_Tmin << " - " << m_Tmax << "\n";
+  g_log.information() << "Wavelength cuttoff is : " << (m_conversionConstant * m_Tmin / m_LRef)
+                      << "Angstrom, Frame width is: " << m_frameWidth << "microseconds\n";
+
+  m_XSize = m_inputWS->dataX(0).size();
+  m_numberOfSpectra = m_inputWS->getNumberHistograms();
+  g_log.debug() << "Number of spectra in input workspace: " << m_numberOfSpectra << "\n";
+
+  // go off and do the event version if appropriate
+  m_inputEvWS = boost::dynamic_pointer_cast<const EventWorkspace>(m_inputWS);
+  if ((m_inputEvWS != NULL) && ! this->getProperty("ForceHist"))
+  {
+    this->execEvent();
+    return;
+  }
+
+  // set up the progress bar
+  m_progress = new Progress(this, 0.0, 1.0, m_numberOfSpectra);
+
+  MatrixWorkspace_sptr outputWS = getProperty("OutputWorkspace");
+  if (outputWS != m_inputWS)
+  {
+    outputWS = WorkspaceFactory::Instance().create(m_inputWS, m_numberOfSpectra,
+                                                   m_XSize,m_XSize-1);
+    setProperty("OutputWorkspace", outputWS);
+  }
+
+  PARALLEL_FOR2(m_inputWS,outputWS)
+  for (size_t workspaceIndex = 0; workspaceIndex < m_numberOfSpectra; workspaceIndex++)
+  {
+    PARALLEL_START_INTERUPT_REGION
+    // get the total flight path
+    bool isMonitor;
+    double Ld = this->calculateFlightpath(workspaceIndex, isMonitor);
+    if (Ld < 0.)
+    {
+      // If the detector flightpath is missing, zero the data
+       g_log.debug() << "Detector information for workspace index " << workspaceIndex
+                     << " is not available.\n";
+       outputWS->dataX(workspaceIndex) = m_inputWS->dataX(workspaceIndex);
+       outputWS->dataY(workspaceIndex).assign(m_XSize-1, 0.0);
+       outputWS->dataE(workspaceIndex).assign(m_XSize-1, 0.0);
+    }
+    else {
+      // fix the x-axis
+      size_t pivot = this->unwrapX(m_inputWS->readX(workspaceIndex),
+                                   outputWS->dataX(workspaceIndex), Ld);
+
+      // fix the counts using the pivot point
+      const MantidVec& yIn = m_inputWS->readY(workspaceIndex);
+      MantidVec& yOut = outputWS->dataY(workspaceIndex);
+      yOut.clear();
+      yOut.insert(yOut.begin(), yIn.begin() + pivot, yIn.end());
+      yOut.insert(yOut.end(), yIn.begin(), yIn.begin() + pivot);
+
+      // fix the uncertainties using the pivot point
+      const MantidVec& eIn = m_inputWS->readE(workspaceIndex);
+      MantidVec& eOut = outputWS->dataE(workspaceIndex);
+      eOut.clear();
+      eOut.insert(eOut.begin(), eIn.begin() + pivot, eIn.end());
+      eOut.insert(eOut.end(),   eIn.begin(),         eIn.begin() + pivot);
+    }
+    m_progress->report();
+    PARALLEL_END_INTERUPT_REGION
+  }
+  PARALLEL_CHECK_INTERUPT_REGION
+
+  m_inputWS.reset();
+}
+
+void Unwrap2::execEvent()
+{
+  // set up the output workspace
+  MatrixWorkspace_sptr matrixOutW = this->getProperty("OutputWorkspace");
+  DataObjects::EventWorkspace_sptr outW;
+  if (matrixOutW == m_inputWS)
+    outW = boost::dynamic_pointer_cast<EventWorkspace>(matrixOutW);
+  else
+  {
+    outW = boost::dynamic_pointer_cast<EventWorkspace>(
+                API::WorkspaceFactory::Instance().create("EventWorkspace",m_numberOfSpectra,2,1) );
+    //Copy required stuff from it
+    API::WorkspaceFactory::Instance().initializeFromParent(m_inputWS, outW, false);
+    outW->copyDataFrom( (*m_inputEvWS) );
+
+    // cast to the matrixoutput workspace and save it
+    matrixOutW = boost::dynamic_pointer_cast<MatrixWorkspace>(outW);
+    this->setProperty("OutputWorkspace", matrixOutW);
+  }
+
+  // set up the progress bar
+  m_progress = new Progress(this,0.0,1.0,m_numberOfSpectra*2);
+
+  // algorithm assumes the data is sorted so it can jump out early
+  outW->sortAll(Mantid::DataObjects::TOF_SORT, m_progress);
+
+  // do the actual work
+  PARALLEL_FOR2(m_inputWS,outW)
+  for (size_t workspaceIndex = 0; workspaceIndex < m_numberOfSpectra; workspaceIndex++)
+  {
+    PARALLEL_START_INTERUPT_REGION
+    std::size_t numEvents = outW->getEventList(workspaceIndex).getNumberEvents();
+    bool isMonitor;
+    double Ld = this->calculateFlightpath(workspaceIndex, isMonitor);
+    MantidVec time_bins;
+    this->unwrapX(m_inputWS->dataX(workspaceIndex), time_bins, Ld);
+    outW->setX(workspaceIndex, time_bins);
+    if (numEvents > 0)
+    {
+      MantidVec times(numEvents);
+      outW->getEventList(workspaceIndex).getTofs(times);
+      double filterVal = m_Tmin * Ld / m_LRef;
+      for (size_t j = 0; j < numEvents; j++)
+      {
+        if (times[j] < filterVal)
+          times[j] += m_frameWidth;
+        else
+          break; // stop filtering
+      }
+      outW->getEventList(workspaceIndex).setTofs(times);
+    }
+    m_progress->report();
+    PARALLEL_END_INTERUPT_REGION
+  }
+  PARALLEL_CHECK_INTERUPT_REGION
+
+  outW->clearMRU();
+}
+
+/** Calculates the total flightpath for the given detector.
+ *  This is L1+L2 normally, but is the source-detector distance for a monitor.
+ *  @param spectrum ::  The workspace index
+ *  @param L1 ::        The primary flightpath
+ *  @param isMonitor :: Output: true is this detector is a monitor
+ *  @return The flightpath (Ld) for the detector linked to spectrum
+ *  @throw Kernel::Exception::InstrumentDefinitionError if the detector position can't be obtained
+ */
+double Unwrap2::calculateFlightpath(const size_t& spectrum, bool& isMonitor) const
+{
+  double Ld = -1.0;
+  try
+  {
+    // Get the detector object for this histogram
+    Geometry::IDetector_const_sptr det = m_inputWS->getDetector(spectrum);
+    // Get the sample-detector distance for this detector (or source-detector if a monitor)
+    // This is the total flightpath
+    isMonitor = det->isMonitor();
+    // Get the L2 distance if this detector is not a monitor
+    if ( !isMonitor )
+    {
+      double L2 = det->getDistance(*(m_inputWS->getInstrument()->getSample()));
+      Ld = m_L1 + L2;
+    }
+    // If it is a monitor, then the flightpath is the distance to the source
+    else
+    {
+      Ld = det->getDistance(*(m_inputWS->getInstrument()->getSource()));
+    }
+  }
+  catch (Exception::NotFoundError)
+  {
+    // If the detector information is missing, return a negative number
+  }
+
+  return Ld;
+}
+
+size_t Unwrap2::unwrapX(const MantidVec& datain, MantidVec& dataout, const double& Ld)
+{
+//  std::cout << "datain[" << datain.size() << "] ";
+
+  // Calculate cut-off times
+//  std::cout << "Ld/Lref[" << (Ld/m_LRef) << "="<< Ld << "/" << m_LRef << "] ";
+
+  MantidVec tempX_L; // lower half - to be frame wrapped
+  tempX_L.reserve(m_XSize);
+  tempX_L.clear();
+  MantidVec tempX_U; // upper half - to not be frame wrapped
+  tempX_U.reserve(m_XSize);
+  tempX_U.clear();
+
+  double filterVal = m_Tmin * Ld / m_LRef;
+  dataout.clear();
+  size_t specialBin = 0;
+  for (size_t bin = 0; bin < m_XSize; ++bin)
+  {
+    // This is the time-of-flight value under consideration in the current iteration of the loop
+    const double tof = datain[bin];
+    if (tof < filterVal) {
+      tempX_L.push_back(tof + m_frameWidth);
+      // Record the bins that fall in this range for copying over the data & errors
+      if (specialBin < bin)
+        specialBin = bin;
+    }
+    else {
+      tempX_U.push_back(tof);
+
+    }
+  } // loop over X values
+
+  // now put it back into the vector supplied
+//  std::cout << "tempX_U " << tempX_U.size() << " tempX_L " << tempX_L.size() << std::endl;
+  dataout.clear();
+  dataout.insert(dataout.begin(), tempX_U.begin(), tempX_U.end());
+  dataout.insert(dataout.end(), tempX_L.begin(),tempX_L.end());
+  assert(datain.size() == dataout.size());
+
+  // REMOVE
+//  std::cout << " DATAOUT[" << dataout.size() << "] " << *(dataout.begin()) << " -> " << *(dataout.rbegin());
+//  for(size_t i = 0; i < dataout.size(); i++)
+//    std::cout << dataout[i] << " ";
+//  std::cout << std::endl;
+
+  return specialBin;
+}
+
+} // namespace Algorithm
+} // namespace Mantid
