a4a8a6b99d5429b139eeda90c9d530ea45d68cb4
Stuart Campbell <campbellsi@ornl.gov>
2011-03-22 15:43:18 +0000

    Initial version of Orbiter Search. refs #1404

---
+++ Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/OrbiterDataArchive.h
@@ -0,0 +1,53 @@
+#ifndef MANTID_DATAHANDLING_ORBITERDATAARCHIVE_H_
+#define MANTID_DATAHANDLING_ORBITERDATAARCHIVE_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidKernel/System.h"
+#include "MantidAPI/IArchiveSearch.h"
+
+#include <string>
+
+namespace Mantid
+{
+  namespace DataHandling
+  {
+
+  /**
+   This class is for searching the Orbiter data archive
+
+   @author Stuart Campbell, ORNL
+   @date 18/03/2010
+
+   Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+   This file is part of Mantid.
+
+   Mantid is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Mantid is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+   Code Documentation is available at: <http://doxygen.mantidproject.org>
+   */
+
+    class  DLLExport OrbiterDataArchive: public API::IArchiveSearch
+    {
+      public:
+      std::string getPath(const std::string& fName) const;
+    };
+
+  }
+}
+
+#endif /* MANTID_DATAHANDLING_ORBITERDATAARCHIVE_H_ */
---
+++ Code/Mantid/Framework/DataHandling/src/OrbiterDataArchive.cpp
@@ -0,0 +1,82 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidDataHandling/OrbiterDataArchive.h"
+#include "MantidAPI/ArchiveSearchFactory.h"
+
+#include <Poco/Net/HTTPSClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/Context.h>
+#include <Poco/URI.h>
+
+#include <iostream>
+
+using Poco::Net::HTTPSClientSession;
+using Poco::Net::HTTPRequest;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPMessage;
+using Poco::URI;
+
+namespace Mantid
+{
+  namespace DataHandling
+  {
+
+    DECLARE_ARCHIVESEARCH(OrbiterDataArchive,OrbiterDataSearch);
+
+    /**
+     * Calls a web service to get a full path to a file
+     * @param fName :: The file name.
+     * @return The path to the file or empty string in case of error.
+     */
+    std::string OrbiterDataArchive::getPath(const std::string& fName) const
+    {
+
+      std::string baseURL(
+          "https://orbiter.sns.gov/orbiter/service/webservice/OrbiterFindFileService.php");
+      std::string findFileWS("/operation/findFile/format/space/filename/");
+      std::string URL;
+
+      std::string out;
+
+      //#ifdef _WIN32
+      //	// Return an empty string
+      //#else
+      //
+      //#endif
+
+      Poco::URI uri(baseURL + findFileWS + fName);
+      std::string path(uri.getPathAndQuery());
+
+      Poco::Net::Context::Ptr context = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "",
+          "", Poco::Net::Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+
+      Poco::Net::HTTPSClientSession session(uri.getHost(), uri.getPort(), context);
+      HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+      session.sendRequest(req);
+
+      HTTPResponse res;
+      std::istream& rs = session.receiveResponse(res);
+
+      char buff[300];
+      std::streamsize n;
+
+      do
+      {
+        rs.read(&buff[0], 300);
+        n = rs.gcount();
+        out.append(&buff[0], n);
+      } while (n == 300);
+
+      //	if (!out.empty()) {
+      //		return out.substr(5);
+      //	}
+
+      std::cout << "Returned Filename = " << out << std::endl;
+
+      return out;
+    }
+
+  } // namespace DataHandling
+} // namespace Mantid
