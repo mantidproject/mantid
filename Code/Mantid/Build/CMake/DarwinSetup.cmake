e4878439ad969231885c49101efd2867989f7349
Russell Taylor <taylorrj@ornl.gov>
2011-03-24 20:30:06 +0000

    CMake build file changes for Mac - 32 bit builds are no longer supported. Re #1133.

--- Code/Mantid/Build/CMake/DarwinSetup.cmake
+++ Code/Mantid/Build/CMake/DarwinSetup.cmake
@@ -1,11 +1,11 @@
 set ( CMAKE_INCLUDE_PATH "${THIRD_PARTY}/include" )
 set ( BOOST_INCLUDEDIR "${THIRD_PARTY}/include" )
 
-set ( CMAKE_LIBRARY_PATH "${THIRD_PARTY}/lib/mac" )
-set ( BOOST_LIBRARYDIR  "${THIRD_PARTY}/lib/mac" )
+set ( CMAKE_LIBRARY_PATH "${THIRD_PARTY}/lib/mac64" )
+set ( BOOST_LIBRARYDIR  "${THIRD_PARTY}/lib/mac64" )
 
-# For now force 32 bit compile as those are the libraries we've got
-set ( CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -m32 )
-set ( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -m32 )
+# Force 64 bit compile
+set ( CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -m64 )
+set ( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -m64 )
 
 set ( CMAKE_INSTALL_NAME_DIR ${CMAKE_LIBRARY_PATH} )
