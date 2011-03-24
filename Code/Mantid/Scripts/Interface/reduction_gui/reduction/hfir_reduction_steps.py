ded2056fb114e47e1b8e0378fa6a11cafbfdf186
Mathieu Doucet <doucetm@ornl.gov>
2011-03-24 14:28:25 +0000

    Pop up I(q) in MantidPlot once reduction is complete. Re #2438

--- Code/Mantid/Scripts/Interface/reduction_gui/reduction/hfir_reduction_steps.py
+++ Code/Mantid/Scripts/Interface/reduction_gui/reduction/hfir_reduction_steps.py
@@ -17,6 +17,14 @@ try:
 except:
     HAS_MANTID = False  
 
+# Check whether we are running in MantidPlot
+IS_IN_MANTIDPLOT = False
+try:
+    import mantidplot
+    IS_IN_MANTIDPLOT = True
+except:
+    pass
+
 class Transmission(BaseScriptElement):
 
     class DirectBeam(BaseScriptElement):
@@ -739,4 +747,8 @@ class Output(BaseScriptElement):
         if HAS_MANTID and ReductionSingleton()._azimuthal_averager is not None:
             self.data = ReductionSingleton()._azimuthal_averager.get_data(ReductionSingleton()._data_files.keys()[0])
     
-       
+            if IS_IN_MANTIDPLOT:
+                try:
+                    mantidplot.plotSpectrum(ReductionSingleton()._data_files.keys()[0]+ReductionSingleton()._azimuthal_averager._suffix, 0, True)
+                except:
+                    raise RuntimeError, "Could not plot resulting I(Q)\n  %s" % sys.exc_value
