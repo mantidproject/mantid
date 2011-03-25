d1f52e92c55dde37b3aabe4504abbbbe04010123
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-03-25 16:13:40 +0000

    Fixes #2719: IMDDimension got back its methods, as empty virtual methods.

--- Code/Mantid/Framework/Geometry/inc/MantidGeometry/MDGeometry/IMDDimension.h
+++ Code/Mantid/Framework/Geometry/inc/MantidGeometry/MDGeometry/IMDDimension.h
@@ -1,46 +1,52 @@
 #ifndef I_MD_DIMENSION_H
 #define I_MD_DIMENSION_H
+
 #include <vector>
-
-/** The class discribes one dimension of multidimensional dataset representing an ortogonal dimension and linear axis. 
-*
-*   Abstract type for a multi dimensional dimension. Gives a read-only layer to the concrete implementation.
-
-    @author Owen Arnold, RAL ISIS
-    @date 12/11/2010
-
-    Copyright &copy; 2007-10 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
-
-    This file is part of Mantid.
-
-    Mantid is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    Mantid is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
-    Code Documentation is available at: <http://doxygen.mantidproject.org>
-    */
+#include <stdexcept>
 #include "MantidKernel/System.h"
 #include "MantidGeometry/V3D.h"
 #include <boost/shared_ptr.hpp>
+
+
 
 namespace Mantid
 {
   namespace Geometry
   {
+  /** The class discribes one dimension of multidimensional dataset representing an ortogonal dimension and linear axis.
+  *
+  *   Abstract type for a multi dimensional dimension. Gives a read-only layer to the concrete implementation.
+
+      @author Owen Arnold, RAL ISIS
+      @date 12/11/2010
+
+      Copyright &copy; 2007-10 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+      This file is part of Mantid.
+
+      Mantid is free software; you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation; either version 3 of the License, or
+      (at your option) any later version.
+
+      Mantid is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+      File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+      Code Documentation is available at: <http://doxygen.mantidproject.org>
+      */
 
   class DLLExport IMDDimension
   {
   public:
+    /// Destructor
+    virtual ~IMDDimension(){};
+
     /// the name of the dimennlsion as can be displayed along the axis
     virtual std::string getName() const = 0;
 
@@ -63,37 +69,49 @@ namespace Mantid
     ///  Get coordinate for index;
     virtual double getX(size_t ind)const = 0;
 
-
-//    /// if the dimension is integrated (e.g. have single bin)
-//    virtual bool getIsIntegrated() const = 0;
-//
-//    // it is sometimes convinient to shift image data by some number along specific dimension
-//    virtual double getDataShift()const = 0;
-//
-//    /// the change of the location in the multidimensional image array, which occurs if the index of this dimension changes by one.
-//    virtual size_t      getStride()const = 0;
-//
-//    /// defines if the dimension is reciprocal or not. The reciprocal dimensions are treated differently from an orthogonal one
-//    virtual bool isReciprocal() const = 0;
-//
-//    /// defines the dimension scale in physical units. ->TODO: it is unclear if we need this here
-//    // virtual double getScale()const = 0;
-//    /** function returns a direction of the dimension in the system of coordinates described by the MDBasis;
-//     *  Orthogonal dimensions always have direction 1, but we set direction of this to 0  (e.g. direction={0,0,0})? questionable, 1 may be better;
-//     *  while reciprocal dimension can be directed anywhere withing the reciprocal space;
-//	     Norm of the vector, returned by this function has to be 1    */
-//    virtual V3D getDirection(void)const = 0;
-//    /** Return direction in the crystallogrpahical sence, e.g. output V3D is normalized in such a way that the size of
-//	   smallest (by module) non-0 component of the vector is 1; In this case, all vectors representing valid crystallographical axis would
-//	   have integer values; */
-//    virtual V3D getDirectionCryst(void)const = 0;
-//
-//    /** the function returns the center points of the axis bins; There are nBins of such points
-//     (when axis has nBins+1 points with point 0 equal rMin and nBins+1 equal rMax) */
-//    virtual void getAxisPoints(std::vector<double>  &)const=0;
+    /// if the dimension is integrated (e.g. have single bin)
+    virtual bool getIsIntegrated() const
+    {
+      return getNBins() == 1;
+    }
 
 
-    virtual ~IMDDimension(){};
+
+    //===============================================================================================
+    //========== Virtual Methods used in MDDimension  ===============================================
+    //===============================================================================================
+
+    // it is sometimes convinient to shift image data by some number along specific dimension
+    virtual double getDataShift()
+    { return 0; }
+
+    /// the change of the location in the multidimensional image array, which occurs if the index of this dimension changes by one.
+    virtual size_t getStride()const
+    { return 0; }
+
+    /// defines if the dimension is reciprocal or not. The reciprocal dimensions are treated differently from an orthogonal one
+    virtual bool isReciprocal() const
+    { return false; }
+
+    /** function returns a direction of the dimension in the system of coordinates described by the MDBasis;
+     *  Orthogonal dimensions always have direction 1, but we set direction of this to 0  (e.g. direction={0,0,0})? questionable, 1 may be better;
+     *  while reciprocal dimension can be directed anywhere withing the reciprocal space;
+	     Norm of the vector, returned by this function has to be 1    */
+    virtual V3D getDirection(void)const
+    { throw std::runtime_error("Not Implemented.");  }
+
+    /** Return direction in the crystallogrpahical sence, e.g. output V3D is normalized in such a way that the size of
+	   smallest (by module) non-0 component of the vector is 1; In this case, all vectors representing valid crystallographical axis would
+	   have integer values; */
+    virtual V3D getDirectionCryst(void)const
+    { throw std::runtime_error("Not Implemented.");  }
+
+    /** the function returns the center points of the axis bins; There are nBins of such points
+     (when axis has nBins+1 points with point 0 equal rMin and nBins+1 equal rMax) */
+    virtual void getAxisPoints(std::vector<double>  &)const
+    { throw std::runtime_error("Not Implemented."); }
+
+
   };
 
   /// Shared Pointer for IMDDimension. Frequently used type in framework.
