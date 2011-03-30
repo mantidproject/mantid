e8b34badefb3b77dd433ceb81e298134765352ec
Michael Whitty <michael.whitty@stfc.ac.uk>
2011-03-30 11:15:41 +0000

    refs #2731 - changes to parameter file caused test failure

--- Code/Mantid/Framework/DataHandling/test/LoadRaw3Test.h
+++ Code/Mantid/Framework/DataHandling/test/LoadRaw3Test.h
@@ -418,7 +418,7 @@ public:
     TS_ASSERT_EQUALS( ptrDet->getID(), 60);
 
     Mantid::Geometry::ParameterMap& pmap = output2D->instrumentParameters();
-    TS_ASSERT_EQUALS( static_cast<int>(pmap.size()), 148);
+    TS_ASSERT_EQUALS( static_cast<int>(pmap.size()), 152);
 	AnalysisDataService::Instance().remove("parameterIDF");
   }
 
