686e7468a9b5da3e25b8ba8793082f7c23b79ec1
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-04-12 11:33:01 +0000

    Fixes #2806. V3D and Quat were already exported to Python as contructable objects. This just tidies up their constructors so that they are documented all of the C++ ones are exposed, not just the number related ones.

--- Code/Mantid/Framework/PythonAPI/src/geometry_exports.cpp
+++ Code/Mantid/Framework/PythonAPI/src/geometry_exports.cpp
@@ -37,7 +37,8 @@ namespace Mantid
     void export_utils()
     {
       //V3D class
-      class_< Geometry::V3D >("V3D", init<double, double, double>())
+      class_< Geometry::V3D >("V3D",init<>("Construct a V3D at 0,0,0"))
+        .def(init<double, double, double>("Construct a V3D with X,Y,Z coordinates"))
         .def("getX", &Geometry::V3D::X, return_value_policy< copy_const_reference >())
         .def("getY", &Geometry::V3D::Y, return_value_policy< copy_const_reference >())
         .def("getZ", &Geometry::V3D::Z, return_value_policy< copy_const_reference >())
@@ -66,7 +67,11 @@ namespace Mantid
         ;
 
       //Quat class
-      class_< Geometry::Quat >("Quaternion", init<double, double, double, double>())
+      class_< Geometry::Quat >("Quat", init<>("Construct a default Quat that will perform no transformation."))
+        .def(init<double, double, double, double>("Constructor with values"))
+        .def(init<V3D, V3D>("Construct a Quat between two vectors"))
+        .def(init<V3D, V3D, V3D>("Construct a Quaternion that performs a reference frame rotation.\nThe initial X,Y,Z vectors are aligned as expected: X=(1,0,0), Y=(0,1,0), Z=(0,0,1)"))
+        .def(init<double,V3D>("Constructor from an angle(degrees) and an axis."))
         .def("rotate", &Geometry::Quat::rotate)
         .def("real", &Geometry::Quat::real)
         .def("imagI", &Geometry::Quat::imagI)
