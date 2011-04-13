7d75b65562a9bd6acc2f9cf74f6f2b4c05bbc068
Russell Taylor <taylorrj@ornl.gov>
2011-04-13 01:58:17 +0000

    Re #2501

--- Code/Mantid/Framework/Kernel/test/TimerTest.h
+++ Code/Mantid/Framework/Kernel/test/TimerTest.h
@@ -16,17 +16,21 @@ public:
     Mantid::Kernel::Timer timer;
     #ifdef _WIN32
       Sleep(200);
+      TS_ASSERT_DELTA( timer.elapsed(), 0.2, 0.02 );
     #else
       usleep(200000);
+      TS_ASSERT_LESS_THAN( 0.2, timer.elapsed() );
     #endif
-    TS_ASSERT_LESS_THAN( 0.2, timer.elapsed() );
+
+    // Calling elapsed above should reset the timer
     #ifdef _WIN32
       Sleep(100);
+      TS_ASSERT_DELTA( timer.elapsed(), 0.1, 0.02 );
     #else
       usleep(100000);
+      TS_ASSERT_LESS_THAN( 0.1, timer.elapsed() );
     #endif
-    // Calling elapsed above should reset the timer
-    TS_ASSERT_LESS_THAN( 0.1, timer.elapsed() );
+
   }
 };
 
