14be9b73f92c1ef8466dfa23c0a8600de7022904
Andrei Savici <saviciat@ornl.gov>
2011-04-15 22:23:56 +0000

    OrientedLattice class implements UB matrix. Refs#455

---
+++ Code/Mantid/Framework/Geometry/inc/MantidGeometry/Crystal/OrientedLattice.h
@@ -0,0 +1,62 @@
+#ifndef MANTID_GEOMETRY_ORIENTEDLATTICE_H_
+#define MANTID_GEOMETRY_ORIENTEDLATTICE_H_
+#include <MantidGeometry/Crystal/UnitCell.h>
+
+namespace Mantid
+{
+namespace Geometry
+{  
+    /** @class OrientedLattice OrientedLattice.h Geometry/Crystal/OrientedLattice.h
+    Class to implement UB matrix. 
+    See documentation about UB matrix in the Mantid repository.\n
+  
+    @author Andrei Savici, SNS, ORNL
+    @date 2011-04-15    
+    Copyright &copy; 2007-8 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+   */
+  class OrientedLattice: public UnitCell
+  {
+    public:
+      // Default constructor. a = b = c = 1, alpha = beta = gamma = 90 degrees
+      OrientedLattice(MantidMat Umatrix=MantidMat(3,3,true)); 
+      //Copy constructor
+      OrientedLattice(const OrientedLattice& other); 
+      // a,b,c constructor
+      OrientedLattice(const double _a,const double _b,const double _c,MantidMat Umatrix=MantidMat(3,3,true)); 
+      //a,b,c,alpha,beta,gamma constructor
+      OrientedLattice(const double _a,const double _b,const double _c,const double _alpha,const double _beta,const double _gamma,MantidMat Umatrix=MantidMat(3,3,true),const int angleunit=angDegrees);
+      //UnitCell constructor
+      OrientedLattice(UnitCell uc ,MantidMat Umatrix=MantidMat(3,3,true));
+      // Destructor
+      virtual ~OrientedLattice();  
+
+      // Access private variables
+      const MantidMat& getU() const;
+      const MantidMat getUB() const;
+      void setU(MantidMat& newU);
+      void setUB(MantidMat& newUB);
+
+    private:
+      MantidMat U;
+  };
+} // namespace Mantid
+} // namespace Geometry
+#endif  /* MANTID_GEOMETRY_UNITCELL_H_ */
---
+++ Code/Mantid/Framework/Geometry/src/Crystal/OrientedLattice.cpp
@@ -0,0 +1,81 @@
+#include <MantidGeometry/Crystal/OrientedLattice.h>
+namespace Mantid
+{
+namespace Geometry
+{
+  /** Default constructor
+  @param Umatrix :: orientation matrix U. By default this will be identity matrix
+  */
+  OrientedLattice::OrientedLattice(MantidMat Umatrix):UnitCell(),U(Umatrix)
+  {
+  }
+
+  /** Copy constructor
+  @param other :: The OrientedLattice from which to copy information
+  */    
+  OrientedLattice::OrientedLattice(const OrientedLattice& other):UnitCell(other),U(other.U)
+  {
+  }
+
+  /** Constructor
+  @param _a, _b, _c :: lattice parameters \f$ a, b, c \f$ \n
+  with \f$\alpha = \beta = \gamma = 90^\circ \f$*/      
+  OrientedLattice::OrientedLattice(const double _a,const double _b,const double _c,MantidMat Umatrix):UnitCell(_a,_b,_c),U(Umatrix)
+  {
+  }
+
+  /** Constructor
+  @param _a, _b, _c, _alpha, _beta, _gamma :: lattice parameters\n
+  @param angleunit :: units for angle, of type #AngleUnits. Default is degrees.
+  */
+  OrientedLattice::OrientedLattice(const double _a,const double _b,const double _c,const double _alpha,const double _beta,const double _gamma,MantidMat Umatrix, const int angleunit):UnitCell(_a,_b,_c,_alpha,_beta,_gamma,angleunit),U(Umatrix)
+  {
+  }
+
+  /** UnitCell constructor
+  @param uc :: UnitCell
+  @param Umatrix :: orientation matrix U. By default this will be identity matrix
+  */
+  OrientedLattice::OrientedLattice(UnitCell uc,MantidMat Umatrix):UnitCell(uc),U(Umatrix)
+  {
+  }
+
+  /// Destructor
+  OrientedLattice::~OrientedLattice()
+  {
+  } 
+
+
+  /// Get the U matrix
+  /// @return U :: U orientation matrix
+  const MantidMat& OrientedLattice::getU() const
+  {
+    return U;
+  }
+  
+  /// Get the UB matrix
+  /// @return UB :: UB orientation matrix
+  const MantidMat OrientedLattice::getUB() const
+  {
+    MantidMat UB;
+    UB=U*this->getB();
+    return UB;
+  }
+
+  void OrientedLattice::setU(MantidMat& newU)
+  {
+    U=newU;
+  }
+
+
+  void OrientedLattice::setUB(MantidMat& newUB)
+  {
+    MantidMat newGstar,B;
+    newGstar=newUB.Tprime()*newUB;
+    this->recalculateFromGstar(newGstar);
+    B=this->getB();
+    B.Invert();
+    U=newUB*B;
+  }
+}//Namespace Geometry
+}//Namespace Mantid
