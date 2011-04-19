669c879b0c114126a4951261e18d785fada7b78e
Steve Williams <stephen.williams@stfc.ac.uk>
2011-04-19 08:08:08 +0000

    Last matlab compiler warnings from mwdebg.c re #2818

--- Code/Mantid/Framework/MatlabAPI/src/mwdebug_silent.c
+++ Code/Mantid/Framework/MatlabAPI/src/mwdebug_silent.c
@@ -4,4 +4,6 @@
 #pragma warning(disable:4996)
 // matlab isn't using the return keyword after some exceptions
 #pragma warning(disable:4715)
+// signed unsigned warnings that they have
+#pragma warning(disable:4018)
 #include "mwdebug.c"
