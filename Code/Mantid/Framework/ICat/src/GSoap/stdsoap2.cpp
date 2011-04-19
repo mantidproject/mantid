2d02826834e823be829e2df099d30128b346be6f
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-04-19 09:30:42 +0000

    Refs #2818. Easy win, 85 warnings gone.

--- Code/Mantid/Framework/ICat/src/GSoap/stdsoap2.cpp
+++ Code/Mantid/Framework/ICat/src/GSoap/stdsoap2.cpp
@@ -71,6 +71,9 @@ when locally allocated data exceeds 64K.
 
 #ifdef __BORLANDC__
 # pragma warn -8060
+#elif defined(__GNUC__)
+  #pragma GCC diagnostic ignored "-Wcast-qual"
+  #pragma GCC diagnostic ignored "-Wconversion"
 #else
 # ifdef WIN32
 #  ifdef UNDER_CE
