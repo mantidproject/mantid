ce131ead903e196cc4998567255bd7d8f14604c8
Russell Taylor <taylorrj@ornl.gov>
2011-04-27 15:55:35 +0000

    Fix error in script. Re #861.

--- Code/Mantid/scripts/PlotAsymmetryByLogValue_Example.py
+++ Code/Mantid/scripts/PlotAsymmetryByLogValue_Example.py
@@ -12,9 +12,9 @@ ws = alg.getPropertyValue("OutputWorkspace")
 
 spectra_plot = { 0 : 'Difference' }
 if int(alg.getPropertyValue("Green")) < 1000:
-    spectraplot[1] = 'Red'
-    spectraplot[2] = 'Green'
-    spectraplot[3] = 'Sum'
+    spectra_plot[1] = 'Red'
+    spectra_plot[2] = 'Green'
+    spectra_plot[3] = 'Sum'
 
 gs = plotSpectrum(ws, spectra_plot.keys())
 for key, value in spectra_plot.iteritems():
