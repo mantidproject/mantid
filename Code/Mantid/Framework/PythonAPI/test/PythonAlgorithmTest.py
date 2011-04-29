5b0fe5c136fa05bd5fc0cf3c11376f04801b1617
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-04-29 18:32:09 +0000

    Refs #2635: Added a python unit test where an algorithm property is obtained in a Python algo and then executed.

--- Code/Mantid/Framework/PythonAPI/test/PythonAlgorithmTest.py
+++ Code/Mantid/Framework/PythonAPI/test/PythonAlgorithmTest.py
@@ -6,6 +6,28 @@ from MantidFramework import Direction
 mtd.initialise()
 from mantidsimple import *
 
+class DummyAlg3(PythonAlgorithm):
+    """
+        Dummy algorithm that runs an algorithm property.
+    """
+     
+    def category(self):
+        return "SANS"
+    
+    def name(self):
+        return "DummyAlg"
+    
+    def PyInit(self):
+        self.declareWorkspaceProperty("OutputWorkspace", "", Direction = Direction.Output, Description = '')
+        self.declareAlgorithmProperty("Algo")
+    
+    def PyExec(self):
+        # Get the algorithm property
+        sub_alg = self._getAlgorithmProperty("Algo")
+        sub_alg.execute()
+        self.setPropertyValue("OutputWorkspace", sub_alg.getPropertyValue("OutputWorkspace"))
+
+
 class DummyAlg(PythonAlgorithm):
     """
         Dummy algorithm that uses executeSubAlg()
@@ -25,6 +47,7 @@ class DummyAlg(PythonAlgorithm):
         output_ws = self.getPropertyValue("OutputWorkspace")
         a = self.executeSubAlg(CreateWorkspace, output_ws, [0,1,2], [0,1,2], [0,0,0])
         self._setWorkspaceProperty("OutputWorkspace", a._getWorkspaceProperty("OutputWorkspace"))
+
 
 class DummyAlg2(DummyAlg):
     def name(self):
@@ -47,7 +70,7 @@ class PythonAlgorithmTest(unittest.TestCase):
             Check that we can execute a sub-algorithm and pass
             ownership of an output workspace to the parent algo. 
         """
-        algm = DummyAlg()
+        algm = DummyAlg2()
         algm.initialize()
         algm.setPropertyValue("OutputWorkspace", "subalgtest")
         algm.setRethrows(True)
@@ -66,6 +89,7 @@ class PythonAlgorithmTest(unittest.TestCase):
         algm.execute()
         self.assertTrue(mtd.workspaceExists("subalgtest2"))
         mtd.deleteWorkspace("subalgtest2")
+        
         
     def test_cpp_alg_property_using_python_alg(self):
         """
@@ -120,6 +144,28 @@ class PythonAlgorithmTest(unittest.TestCase):
                          "CreateWorkspace.1(OutputWorkspace=test,DataX=1,DataY=1,DataE=1)")
         self.assertTrue(mtd.workspaceExists("subalgtest"))
         mtd.deleteWorkspace("subalgtest")
+                
         
+    def test_cpp_alg_property_using_python_alg_and_execute_the_propertied_algorithm(self):
+        """
+            Declare, seta C++ algorithm from a python algorithm.
+            Then, in the "super" python algorithm, run the "sub" algorithm that
+            was passed as a parameter
+        """
+        algm_par = mtd._createAlgProxy("CreateWorkspace")
+        algm_par.setPropertyValues(OutputWorkspace="test", DataX=1, DataY=1, DataE=1)
+        algm_par.initialize()
+        
+        algm = DummyAlg3()
+        algm.initialize()
+        algm.setPropertyValue("OutputWorkspace", "subalgtest")
+        algm._setAlgorithmProperty("Algo", algm_par._getHeldObject())
+        algm.execute()
+        # THe algorithm created the workspace name given in the PROPERTY ...
+        self.assertTrue(mtd.workspaceExists("test"))
+        mtd.deleteWorkspace("test")
+        # ... not the one given as a parameter of DummyAlg3, because that's not the way that algo was written
+        self.assertFalse(mtd.workspaceExists("subalgtest"))
+                
 if __name__ == '__main__':
     unittest.main()
