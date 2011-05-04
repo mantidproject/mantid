027ee9c09050362f683aa528c1c457b8d4f9f4f8
Michael Reuter <reuterma@ornl.gov>
2011-05-04 21:22:35 +0000

    Correcting the ki/kf keyword. This fixes #2896.

--- Code/Mantid/scripts/Inelastic/DirectEnergyConversion.py
+++ Code/Mantid/scripts/Inelastic/DirectEnergyConversion.py
@@ -593,7 +593,7 @@ class DirectEnergyConversion(object):
             elif ext == '.nxs':
                 SaveNexus(workspace, filename)
             elif ext == '.nxspe':
-                SaveNXSPE(workspace, filename, kioverkfscaling=self.apply_kikf_correction,psi=self.psi)
+                SaveNXSPE(workspace, filename, ki_over_kf_scaling=self.apply_kikf_correction,psi=self.psi)
             else:
                 self.log('Unknown file format "%s" encountered while saving results.')
     
