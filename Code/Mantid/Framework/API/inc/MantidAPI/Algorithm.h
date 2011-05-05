c03c8e0a2548e4135aa1cd4380c4b731335dcd66
Vickie Lynch <lynchve@ornl.gov>
2011-05-05 13:20:20 +0000

    Refs #2808 Properties file variable MultiThreaded.MaxCores sets the maximum number of threads for OpenMP and ThreadPool to use

--- Code/Mantid/Framework/API/inc/MantidAPI/Algorithm.h
+++ Code/Mantid/Framework/API/inc/MantidAPI/Algorithm.h
@@ -211,6 +211,9 @@ public:
   /// Execute as a sub-algorithm
   void executeAsSubAlg();
 
+  /// Set the maximum number of cores from Properties files
+  void getOpenMPCores();
+
   /// Add an observer for a notification
   void addObserver(const Poco::AbstractObserver& observer)const;
 
