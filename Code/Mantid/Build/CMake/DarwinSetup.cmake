a3b4bc80b9e8665d7ebe06aa37158de8060c87ae
Russell Taylor <taylorrj@ornl.gov>
2011-05-06 15:53:49 +0000

    Add a pre-install step to the mac installer that wipes out the old installation. Re #2933.

--- Code/Mantid/Build/CMake/DarwinSetup.cmake
+++ Code/Mantid/Build/CMake/DarwinSetup.cmake
@@ -45,6 +45,7 @@ set ( MACOSX_BUNDLE_ICON_FILE MantidPlot.icns )
 
 set ( CPACK_SYSTEM_NAME SnowLeopard )
 set ( CPACK_OSX_PACKAGE_VERSION 10.6 )
+set ( CPACK_PREFLIGHT_SCRIPT ${CMAKE_SOURCE_DIR}/Installers/MacInstaller/installer_hooks/preflight )
 set ( CPACK_POSTFLIGHT_SCRIPT ${CMAKE_SOURCE_DIR}/Installers/MacInstaller/installer_hooks/postflight )
 
 set ( CPACK_GENERATOR PackageMaker )
