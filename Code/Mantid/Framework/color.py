1e7e7da4a83393b13545d341479312d070b14d63
Russell Taylor <taylorrj@ornl.gov>
2011-05-09 20:07:19 +0000

    More tidy-ups. Re #2937.

--- Code/Mantid/Framework/color.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Silly script colorizes output of any command line."""
-
-import MantidBuild
-import sys
-
-if __name__=="__main__":
-    MantidBuild.color_output( " ".join(sys.argv[1:]), "." )
