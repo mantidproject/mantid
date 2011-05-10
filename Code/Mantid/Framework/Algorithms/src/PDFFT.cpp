cc70745d4748dd1d9267e4a3f46ee13a290ee6bc
Russell Taylor <taylorrj@ornl.gov>
2011-05-10 15:30:57 +0000

    Make these available again in the algorithm factory now the release is behind us. I'm not that keen on the algorithm name 'DampSq' though. Re #2709.

--- Code/Mantid/Framework/Algorithms/src/PDFFT.cpp
+++ Code/Mantid/Framework/Algorithms/src/PDFFT.cpp
@@ -7,7 +7,7 @@ namespace Algorithms
 {
 
   // Register the algorithm into the AlgorithmFactory
-  //DECLARE_ALGORITHM(PDFFT)
+  DECLARE_ALGORITHM(PDFFT)
   
   using namespace Mantid::Kernel;
   using namespace Mantid::API;
