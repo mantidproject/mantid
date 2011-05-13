3dc65baceb07e1686fef02c487883dbe40cb90d9
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-05-13 19:28:47 +0000

    Refs #3008: OpenMP on windows. Also TimerTest reliability fix.

--- Code/Mantid/Framework/Kernel/test/TimerTest.h
+++ Code/Mantid/Framework/Kernel/test/TimerTest.h
@@ -16,19 +16,19 @@ public:
     Mantid::Kernel::Timer timer;
     #ifdef _WIN32
       Sleep(200);
-      TS_ASSERT_DELTA( timer.elapsed(), 0.2, 0.02 );
+      TS_ASSERT_LESS_THAN( 0.19, timer.elapsed() );
     #else
       usleep(200000);
-      TS_ASSERT_LESS_THAN( 0.2, timer.elapsed() );
+      TS_ASSERT_LESS_THAN( 0.19, timer.elapsed() );
     #endif
 
     // Calling elapsed above should reset the timer
     #ifdef _WIN32
       Sleep(100);
-      TS_ASSERT_DELTA( timer.elapsed(), 0.1, 0.02 );
+      TS_ASSERT_LESS_THAN( 0.09, timer.elapsed() );
     #else
       usleep(100000);
-      TS_ASSERT_LESS_THAN( 0.1, timer.elapsed() );
+      TS_ASSERT_LESS_THAN( 0.09, timer.elapsed() );
     #endif
 
   }
