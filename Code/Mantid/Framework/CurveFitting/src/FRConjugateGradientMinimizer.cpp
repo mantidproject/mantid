3c78d1ead011b74edb1ce230146b9306896df93f
Michael Reuter <reuterma@ornl.gov>
2011-05-19 15:38:22 +0000

    CurveFitting warnings. This refs #2897.

--- Code/Mantid/Framework/CurveFitting/src/FRConjugateGradientMinimizer.cpp
+++ Code/Mantid/Framework/CurveFitting/src/FRConjugateGradientMinimizer.cpp
@@ -5,6 +5,7 @@
 #include "MantidCurveFitting/CostFunctionFactory.h"
 #include "MantidKernel/Exception.h"
 #include "MantidKernel/Logger.h"
+#include "MantidKernel/System.h"
 
 namespace Mantid
 {
@@ -22,6 +23,9 @@ void FRConjugateGradientMinimizer::initialize(double* X, const double* Y,
                                               const int& nParam, gsl_vector* startGuess, API::IFitFunction* function, 
                                               const std::string& costFunction) 
 {
+  UNUSED_ARG(X);
+  UNUSED_ARG(Y);
+  UNUSED_ARG(sqrtWeight);
   // set-up GSL container to be used with GSL simplex algorithm
   m_data = new GSL_FitData(function,CostFunctionFactory::Instance().createUnwrapped(costFunction));
 
