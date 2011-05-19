dff2d33aa4e31eb84dee9ffb1d95c0c84a7cd755
Stuart Campbell <campbellsi@ornl.gov>
2011-05-19 18:47:46 +0000

    refs #2897

--- Code/Mantid/Framework/DataHandling/test/SaveSPETest.h
+++ Code/Mantid/Framework/DataHandling/test/SaveSPETest.h
@@ -169,7 +169,7 @@ private:
     inputWS = setUpWorkspace(input, inputWS);
     API::Axis * axisOne = inputWS->getAxis(1);
     API::NumericAxis *newAxisOne = new NumericAxis(axisOne->length());
-    for( int i = 0; i < axisOne->length(); ++i)
+    for( std::size_t i = 0; i < axisOne->length(); ++i)
     {
       newAxisOne->setValue(i, axisOne->operator()((i)));
     }
