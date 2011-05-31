419593d2d0c69bcd692a3e46db5bdf1ad85a04ad
Peter Peterson <petersonpf@ornl.gov>
2011-05-31 17:38:14 +0000

    Put a try/catch around the bit that breaks on cleanup for ubuntu. Refs #3086.

--- Code/Mantid/Framework/DataHandling/src/OrbiterDataArchive.cpp
+++ Code/Mantid/Framework/DataHandling/src/OrbiterDataArchive.cpp
@@ -59,23 +59,26 @@ namespace Mantid
       Poco::Net::Context::Ptr context = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "",
           "", Poco::Net::Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
 
-      Poco::Net::HTTPSClientSession session(uri.getHost(), uri.getPort(), context);
-      HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
-      session.sendRequest(req);
+      try { // workaround for ubuntu 11.04
+        Poco::Net::HTTPSClientSession session(uri.getHost(), uri.getPort(), context); // this line is broken
+        HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+        session.sendRequest(req);
 
-      HTTPResponse res;
-      std::istream& rs = session.receiveResponse(res);
+        HTTPResponse res;
+        std::istream& rs = session.receiveResponse(res);
 
-      char buff[300];
-      std::streamsize n;
+        char buff[300];
+        std::streamsize n;
 
-      do
-      {
-        rs.read(&buff[0], 300);
-        n = rs.gcount();
-        wsResult.append(&buff[0], n);
-      } while (n == 300);
-
+        do
+        {
+          rs.read(&buff[0], 300);
+          n = rs.gcount();
+          wsResult.append(&buff[0], n);
+        } while (n == 300);
+      } catch(Poco::IOException &e) {
+        g_log.debug() << "Poco::IOException: \'" << e.message() << "\'\n"; // REMOVE
+      }
 
       //Look for spaces, and split on them.
       std::vector<std::string> filenames;
