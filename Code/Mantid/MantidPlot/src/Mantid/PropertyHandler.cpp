052a7830dde2fc8cf153eb5a5f64cf10054a9001
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2011-05-31 08:17:09 +0000

    fixes #3113

--- Code/Mantid/MantidPlot/src/Mantid/PropertyHandler.cpp
+++ Code/Mantid/MantidPlot/src/Mantid/PropertyHandler.cpp
@@ -206,6 +206,11 @@ void PropertyHandler::initParameters()
     QString parName = QString::fromStdString(function()->parameterName(i));
     if (parName.contains('.')) continue;
     QtProperty* prop = m_browser->addDoubleProperty(parName);
+    QString toolTip = QString::fromStdString(function()->parameterDescription(i));
+    if (!toolTip.isEmpty())
+    {
+      prop->setToolTip(toolTip);
+    }
     m_browser->m_doubleManager->setValue(prop,function()->getParameter(i));
     m_item->property()->addSubProperty(prop);
     m_parameters << prop;
