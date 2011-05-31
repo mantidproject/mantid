834e0262ae68c85c923059e9b58648d75535fce0
Stuart Campbell <campbellsi@ornl.gov>
2011-05-31 15:43:25 +0000

    Added test for PG3. refs #1404

--- Code/Mantid/Framework/DataHandling/test/OrbiterDataArchiveTest.h
+++ Code/Mantid/Framework/DataHandling/test/OrbiterDataArchiveTest.h
@@ -15,21 +15,23 @@ public:
 
   void xtestSearch()
   {
-	OrbiterDataArchive arch;
+      OrbiterDataArchive arch;
+      std::string path = arch.getPath("cncs_23412_event.nxs");
+      //std::cout << "(cncs23412)= " << path << std::endl;
+      TS_ASSERT_EQUALS(path, "/SNS/CNCS/IPTS-4545/0/23412/NeXus/CNCS_23412_event.nxs");
 
-	  std::string path = arch.getPath("cncs_23412_event.nxs");
-    //std::cout << "(cncs23412)= " << path << std::endl;
-    TS_ASSERT_EQUALS(path, "/SNS/CNCS/IPTS-4545/0/23412/NeXus/CNCS_23412_event.nxs ");
+      // PG3 Test case
+      TS_ASSERT_EQUALS(arch.getPath("PG3_2795_event.nxs"), "/SNS/PG3/IPTS-1582/0/2795/NeXus/PG3_2795_event.nxs");
 
-    // Test a non-existent file
-    path = arch.getPath("mybeamline_666.nxs");
-    TS_ASSERT(path.empty());
+      // Test a non-existent file
+      path = arch.getPath("mybeamline_666.nxs");
+      TS_ASSERT(path.empty());
   }
 
   void testFactory()
   {
-    boost::shared_ptr<IArchiveSearch> arch = ArchiveSearchFactory::Instance().create("OrbiterDataSearch");
-    TS_ASSERT(arch);
+      boost::shared_ptr<IArchiveSearch> arch = ArchiveSearchFactory::Instance().create("OrbiterDataSearch");
+      TS_ASSERT(arch);
   }
   
 };
