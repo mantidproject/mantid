9432e0c7de1806db83b66ecdd6e2381b04ffdd01
Mathieu Doucet <doucetm@ornl.gov>
2011-06-02 17:28:10 +0000

    Added Dialog version of algorithms. Re #2663

--- Code/Mantid/Framework/PythonAPI/mantidsimple_runtime.py
+++ Code/Mantid/Framework/PythonAPI/mantidsimple_runtime.py
@@ -78,6 +78,44 @@ def create_algorithm(algorithm, version):
     
     return algorithm_wrapper
     
+def create_algorithm_dialog(algorithm, version):
+    """
+        Create a function that will set up and execute an algorithm dialog.
+        The help that will be displayed is that of the most recent version.
+        @param algorithm: name of the algorithm
+    """
+    def algorithm_wrapper(*args, **kwargs):
+        _version = version
+        if "Version" in kwargs:
+            _version = kwargs["Version"]
+            kwargs.pop(kwargs.index("Version"))
+        for item in ["Message", "Enable", "Disable"]:
+            if item not in kwargs:
+                kwargs[item] = ""
+            
+        algm = mtd.createAlgorithm(algorithm, _version)
+        algm.setPropertiesDialog(*args, **kwargs)
+        algm.execute()
+        return algm
+    
+    algorithm_wrapper.__name__ = "%sDialog" % algorithm
+    algorithm_wrapper.__doc__ = "\n\n%s dialog" % algorithm
+
+    # Dark magic to get the correct function signature
+    _algm_object = mtd.createUnmanagedAlgorithm(algorithm, version)
+    arg_list = []
+    for p in mtd._getPropertyOrder(_algm_object):
+        arg_list.append("%s=None" % p)
+    arg_str = ', '.join(arg_list)
+    signature = "\b%s" % arg_str
+    f = algorithm_wrapper.func_code
+    c = f.__new__(f.__class__, f.co_argcount, f.co_nlocals, f.co_stacksize, f.co_flags, f.co_code, f.co_consts, f.co_names,\
+       (signature, "\b\bMessage=\"\", Enable=\"\", Disable=\"\", Version=%d" % version), \
+       f.co_filename, f.co_name, f.co_firstlineno, f.co_lnotab, f.co_freevars)
+    algorithm_wrapper.func_code = c  
+    
+    return algorithm_wrapper
+
 def translate():
     """
         Loop through the algorithms and register a function call 
@@ -85,6 +123,7 @@ def translate():
     """
     for algorithm in mtd._getRegisteredAlgorithms():
         globals()[algorithm[0]] = create_algorithm(algorithm[0], max(algorithm[1]))
+        globals()["%sDialog" % algorithm[0]] = create_algorithm_dialog(algorithm[0], max(algorithm[1]))
             
 translate()
 
