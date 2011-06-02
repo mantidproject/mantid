a25a1074f3632a2f623c44082f208f0f4f09a13e
Mathieu Doucet <doucetm@ornl.gov>
2011-06-02 21:51:51 +0000

    Added Load and LoadDialog Re #2663

--- Code/Mantid/Framework/PythonAPI/mantidsimple_runtime.py
+++ Code/Mantid/Framework/PythonAPI/mantidsimple_runtime.py
@@ -116,12 +116,120 @@ def create_algorithm_dialog(algorithm, version):
     
     return algorithm_wrapper
 
+def Load(*args, **kwargs):
+    """
+    Load is a more flexible algorithm than other Mantid algorithms.
+    It's aim is to discover the correct loading algorithm for a
+    given file. This flexibility comes at the expense of knowing the
+    properties out right before the file is specified.
+    
+    The argument list for the Load function has to be more flexible to
+    allow this searching to occur. Two arguments must be specified:
+    
+      - Filename :: The name of the file,
+      - OutputWorkspace :: The name of the workspace,
+    
+    either as the first two arguments in the list or as keywords. Any other
+    properties that the Load algorithm has can be specified by keyword only.
+    
+    Some common keywords are:
+     - SpectrumMin,
+     - SpectrumMax,
+     - SpectrumList,
+     - EntryNumber
+    
+    Example:
+      # Simple usage, ISIS NeXus file
+      Load('INSTR00001000.nxs', 'run_ws')
+      
+      # ISIS NeXus with SpectrumMin and SpectrumMax = 1
+      Load('INSTR00001000.nxs', 'run_ws', SpectrumMin=1,SpectrumMax=1)
+      
+      # SNS Event NeXus with precount on
+      Load('INSTR_1000_event.nxs', 'event_ws', Precount=True)
+      
+      # A mix of keyword and non-keyword is also possible
+      Load('event_ws', Filename='INSTR_1000_event.nxs',Precount=True)
+    """
+    # Small inner function to grab the mandatory arguments and translate possible
+    # exceptions
+    def get_argument_value(key, kwargs):
+        try:
+            value = kwargs[key]
+            del kwargs[key]
+            return value
+        except KeyError:
+            raise RuntimeError('%s argument not supplied to Load function' % str(key))
+    
+    if len(args) == 2:
+        filename = args[0]
+        wkspace = args[1]
+    elif len(args) == 1:
+        if 'Filename' in kwargs:
+            wkspace = args[0]
+            filename = get_argument_value('Filename', kwargs)
+        elif 'OutputWorkspace' in kwargs:
+            filename = args[0]
+            wkspace = get_argument_value('OutputWorkspace', kwargs)
+        else:
+            raise RuntimeError('Cannot find "Filename" or "OutputWorkspace" in key word list. '
+                               'Cannot use single positional argument.')
+    elif len(args) == 0:
+        filename = get_argument_value('Filename', kwargs)
+        wkspace = get_argument_value('OutputWorkspace', kwargs)
+    else:
+        raise RuntimeError('Load() takes at most 2 positional arguments, %d found.' % len(args))
+    
+    # Create and execute
+    algm = mtd.createAlgorithm('Load')
+    algm.setPropertyValue('Filename', filename) # Must be set first
+    algm.setPropertyValue('OutputWorkspace', wkspace)
+    for key, value in kwargs.iteritems():
+        algm.setPropertyValue(key, _makeString(value).lstrip('? '))
+    algm.execute()
+    return algm
+
+def LoadDialog(*args, **kwargs):
+    """Popup a dialog for the Load algorithm. More help on the Load function
+    is available via help(Load).
+
+    Additional arguments available here (as keyword only) are:
+      - Enable :: A CSV list of properties to keep enabled in the dialog
+      - Disable :: A CSV list of properties to keep enabled in the dialog
+      - Message :: An optional message string
+    """
+    arguments = {}
+    filename = None
+    wkspace = None
+    if len(args) == 2:
+        filename = args[0]
+        wkspace = args[1]
+    elif len(args) == 1:
+        if 'Filename' in kwargs:
+            filename = kwargs['Filename']
+            wkspace = args[0]
+        elif 'OutputWorkspace' in kwargs:
+            wkspace = kwargs['OutputWorkspace']
+            filename = args[0]
+    arguments['Filename'] = filename
+    arguments['OutputWorkspace'] = wkspace
+    arguments.update(kwargs)
+    if 'Enable' not in arguments: arguments['Enable']=''
+    if 'Disable' not in arguments: arguments['Disable']=''
+    if 'Message' not in arguments: arguments['Message']=''
+    algm = mtd.createAlgorithm('Load')
+    algm.setPropertiesDialog(**arguments)
+    algm.execute()
+    return algm
+
 def translate():
     """
         Loop through the algorithms and register a function call 
         for each of them
     """
     for algorithm in mtd._getRegisteredAlgorithms():
+        if algorithm[0] == "Load":
+            continue
         globals()[algorithm[0]] = create_algorithm(algorithm[0], max(algorithm[1]))
         globals()["%sDialog" % algorithm[0]] = create_algorithm_dialog(algorithm[0], max(algorithm[1]))
             
