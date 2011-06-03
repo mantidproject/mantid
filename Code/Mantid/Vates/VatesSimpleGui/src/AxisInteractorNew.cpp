b011e6d014029f3c1eba9b9a73340206505ed4ac
Michael Reuter <reuterma@ornl.gov>
2011-06-03 13:48:07 +0000

    Creating new axis widget in parallel. Not in build yet since I'm still putting it together, but want to checkpoint work. This refs #2965.

---
+++ Code/Mantid/Vates/VatesSimpleGui/src/AxisInteractorNew.cpp
@@ -0,0 +1,256 @@
+#include "AxisInteractorNew.h"
+#include "Indicator.h"
+#include "ScalePicker.h"
+
+#include <qwt_scale_draw.h>
+#include <qwt_scale_engine.h>
+#include <qwt_scale_map.h>
+#include <qwt_scale_widget.h>
+
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsView>
+#include <QGridLayout>
+#include <QList>
+#include <QMouseEvent>
+#include <QString>
+
+#include <cmath>
+#include <iostream>
+AxisInteractorNew::AxisInteractorNew(QWidget *parent) : QWidget(parent)
+{
+  this->orientation = Qt::Vertical;
+  this->scalePos = AxisInteractorNew::LeftScale;
+
+  this->graphicsView = new QGraphicsView(this);
+  this->gridLayout = new QGridLayout(this);
+  this->scaleWidget = new QwtScaleWidget(this);
+
+	this->scene = new QGraphicsScene(this);
+	this->scene->setItemIndexMethod(QGraphicsScene::NoIndex);
+	this->isSceneGeomInit = false;
+
+	this->widgetLayout();
+
+	this->graphicsView->setScene(this->scene);
+	this->graphicsView->installEventFilter(this);
+
+	this->engine = new QwtLinearScaleEngine;
+	this->transform = new QwtScaleTransformation(QwtScaleTransformation::Linear);
+	this->scalePicker = new ScalePicker(this->scaleWidget);
+	QObject::connect(this->scalePicker, SIGNAL(makeIndicator(const QPoint &)),
+			this, SLOT(createIndicator(const QPoint &)));
+}
+
+void AxisInteractorNew::widgetLayout()
+{
+  if (!this->gridLayout->isEmpty())
+  {
+    for (int i = 0; i < this->gridLayout->count(); ++i)
+    {
+      this->gridLayout->removeItem(this->gridLayout->itemAt(i));
+    }
+  }
+
+  // All set for vertical orientation
+  QRect scaleRect(75, 150);
+  QRect gvRect(50, 150);
+  QSizePolicy policy(QSizePolicy::Fixed, QSizePolicy::MinimumExpanding);
+
+  if (this->orientation == Qt::Vertical)
+  {
+    switch (this->scalePos)
+    {
+    case RightScale:
+    {
+      this->scaleWidget->setAlignment(QwtScaleDraw::RightScale);
+      break;
+    }
+    case LeftScale:
+    default:
+    {
+      this->scaleWidget->setAlignment(QwtScaleDraw::LeftScale);
+      break;
+    }
+    }
+  }
+  else // Qt::Horizontal
+  {
+    policy.transpose();
+    switch (this->scalePos)
+    {
+    {
+    case TopScale:
+    {
+      this->scaleWidget->setAlignment(QwtScaleDraw::TopScale);
+      break;
+    }
+    case BottomScale:
+    default:
+    {
+      this->scaleWidget->setAlignment(QwtScaleDraw::BottomScale);
+      break;
+    }
+    }
+  }
+  this->setSizePolicy(policy);
+}
+
+void AxisInteractorNew::setInformation(QString title, double min, double max)
+{
+	this->scaleWidget->setTitle(title);
+	this->scaleWidget->setScaleDiv(this->transform,
+			this->engine->divideScale(std::floor(min), std::ceil(max), 10, 0));
+}
+
+void AxisInteractorNew::createIndicator(const QPoint &point)
+{
+	QRect gv_rect = this->graphicsView->geometry();
+	if (! this->isSceneGeomInit)
+	{
+		this->scene->setSceneRect(gv_rect);
+		this->isSceneGeomInit = true;
+	}
+	Indicator *tri = new Indicator();
+	tri->setPoints(point, gv_rect);
+	this->scene->addItem(tri);
+}
+
+void AxisInteractorNew::setIndicatorName(const QString &name)
+{
+	QList<QGraphicsItem *> list = this->scene->items();
+	for (int i = 0; i < list.count(); ++i)
+	{
+		QGraphicsItem *item = list.at(i);
+		if (item->type() == IndicatorItemType)
+		{
+			if (item->toolTip().isEmpty())
+			{
+				// This must be the most recently added
+				item->setToolTip(name);
+			}
+		}
+	}
+}
+
+void AxisInteractorNew::selectIndicator(const QString &name)
+{
+	this->clearSelections();
+	QList<QGraphicsItem *> list = this->scene->items();
+	for (int i = 0; i < list.count(); ++i)
+	{
+		QGraphicsItem *item = list.at(i);
+		if (item->type() == IndicatorItemType)
+		{
+			if (item->toolTip() == name)
+			{
+				item->setSelected(true);
+			}
+		}
+	}
+}
+
+bool AxisInteractorNew::hasIndicator()
+{
+	QList<QGraphicsItem *> list = this->scene->selectedItems();
+	if (list.count() > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void AxisInteractorNew::clearSelections()
+{
+	QList<QGraphicsItem *> list = this->scene->selectedItems();
+	for (int i = 0; i < list.count(); ++i)
+	{
+		QGraphicsItem *item = list.at(i);
+		if (item->type() == IndicatorItemType)
+		{
+			item->setSelected(false);
+		}
+	}
+}
+
+void AxisInteractorNew::updateIndicator(double value)
+{
+	QPoint *pos = this->scalePicker->getLocation(value);
+	QList<QGraphicsItem *> list = this->scene->selectedItems();
+	if (list.count() > 0)
+	{
+		Indicator *item = static_cast<Indicator *>(list.at(0));
+		item->updatePos(*pos);
+	}
+}
+
+bool AxisInteractorNew::eventFilter(QObject *obj, QEvent *event)
+{
+	if (obj == this->graphicsView)
+	{
+		if (event->type() == QEvent::MouseButtonPress ||
+				event->type() == QEvent::MouseButtonDblClick)
+		{
+			QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+			if (mouseEvent->button() == Qt::RightButton)
+			{
+				// Want to eat these so users don't add the indicators
+				// via the QGraphicsView (Yum!)
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return AxisInteractorNew::eventFilter(obj, event);
+	}
+}
+
+ScalePos AxisInteractorNew::scalePosition()
+{
+  return this->scalePos;
+}
+
+void AxisInteractorNew::setOrientation(Qt::Orientation orient, ScalePos scalePos)
+{
+  if ( orient == this->orientation && scalePos == this->scalePos )
+  {
+      return;
+  }
+
+  this->scalePos = scalePos;
+
+  if ( orient != this->orientation )
+  {
+    QSizePolicy sp = this->sizePolicy();
+    sp.transpose();
+    this->setSizePolicy(sp);
+  }
+
+  this->orientation = orient;
+  this->widgetLayout();
+}
+
+void AxisInteractorNew::setScalePosition(ScalePos scalePos)
+{
+    if ((scalePos == BottomScale) || (scalePos == TopScale))
+    {
+        this->setOrientation(Qt::Horizontal, scalePos);
+    }
+    else if ((scalePos == LeftScale) || (scalePos == RightScale))
+    {
+        this->setOrientation(Qt::Vertical, scalePos);
+    }
+}
