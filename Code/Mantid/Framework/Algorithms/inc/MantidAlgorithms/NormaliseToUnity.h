55b32aac806e2959de7d45b12699c2569e0cdf7e
Mathieu Doucet <doucetm@ornl.gov>
2011-06-09 21:31:40 +0000

    Re #3163 Added NormaliseToUnity and unit tests. Needs wiki page.

---
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/NormaliseToUnity.h
@@ -0,0 +1,76 @@
+#ifndef MANTID_ALGORITHMS_NORMALISETOUNITY_H_
+#define MANTID_ALGORITHMS_NORMALISETOUNITY_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+/** Takes a workspace as input and normalises it to 1.
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the workspace to take as input. Must be a histogram. </LI>
+    <LI> OutputWorkspace - The name of the workspace in which to store the result </LI>
+    </UL>
+
+    Optional Properties (assume that you count from zero):
+    <UL>
+    <LI> RangeLower - The X value to integrate from (default 0)</LI>
+    <LI> RangeUpper - The X value to integrate to (default max)</LI>
+    <LI> StartWorkspaceIndex - Workspace index number to integrate from (default 0)</LI>
+    <LI> EndWorkspaceIndex - Workspace index number to integrate to (default max)</LI>
+    <LI> IncludePartialBins - If true then partial bins from the beginning and end of the input range are also included in the integration (default false)</LI>
+    <LI> IncludeMonitors - Whether to include monitor spectra in the sum (default yes)
+    </UL>
+
+    Copyright &copy; 2007-2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+ */
+class DLLExport NormaliseToUnity : public API::Algorithm
+{
+public:
+  /// Default constructor
+  NormaliseToUnity() : API::Algorithm() {};
+  /// Destructor
+  virtual ~NormaliseToUnity() {};
+  /// Algorithm's name for identification overriding a virtual method
+  virtual const std::string name() const { return "NormaliseToUnity";}
+  /// Algorithm's version for identification overriding a virtual method
+  virtual int version() const { return (1);}
+  /// Algorithm's category for identification overriding a virtual method
+  virtual const std::string category() const { return "General";}
+
+private:
+  /// Sets documentation strings for this algorithm
+  virtual void initDocs();
+  // Overridden Algorithm methods
+  void init();
+  void exec();
+};
+
+} // namespace Algorithm
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_NORMALISETOUNITY_H_*/
---
+++ Code/Mantid/Framework/Algorithms/src/NormaliseToUnity.cpp
@@ -0,0 +1,104 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAlgorithms/NormaliseToUnity.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidAPI/Progress.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+// Register the class into the algorithm factory
+DECLARE_ALGORITHM(NormaliseToUnity)
+
+/// Sets documentation strings for this algorithm
+void NormaliseToUnity::initDocs()
+{
+  this->setWikiSummary("NormaliseToUnity takes a 2D [[workspace]] or an [[EventWorkspace]] as input and normalises it to 1. Optionally, the range summed can be restricted in either dimension. ");
+  this->setOptionalMessage("NormalizeToUnity takes a 2D workspace or an EventWorkspace as input and normalises it to 1. Optionally, the range summed can be restricted in either dimension.");
+}
+
+
+using namespace Kernel;
+using namespace API;
+
+
+/** Initialisation method.
+ *
+ */
+void NormaliseToUnity::init()
+{
+  CompositeValidator<> *wsValidator = new CompositeValidator<>;
+  wsValidator->add(new HistogramValidator<>);
+  wsValidator->add(new CommonBinsValidator<>);
+
+  declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input, wsValidator));
+  declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output));
+
+  declareProperty("RangeLower",EMPTY_DBL());
+  declareProperty("RangeUpper",EMPTY_DBL());
+  BoundedValidator<int> *mustBePositive = new BoundedValidator<int>();
+  mustBePositive->setLower(0);
+  declareProperty("StartWorkspaceIndex", 0, mustBePositive);
+  // As the property takes ownership of the validator pointer, have to take care to pass in a unique
+  // pointer to each property.
+  declareProperty("EndWorkspaceIndex", EMPTY_INT(), mustBePositive->clone());
+  declareProperty("IncludePartialBins", false, "If true then partial bins from the beginning and end of the input range are also included in the integration.");
+  declareProperty("IncludeMonitors",true,"Whether to include monitor spectra in the sum (default: yes)");
+}
+
+/** Executes the algorithm
+ *
+ *  @throw runtime_error Thrown if algorithm cannot execute
+ */
+void NormaliseToUnity::exec()
+{
+  // Try and retrieve the optional properties
+  double m_MinRange = getProperty("RangeLower");
+  double m_MaxRange = getProperty("RangeUpper");
+  int m_MinSpec = getProperty("StartWorkspaceIndex");
+  int m_MaxSpec = getProperty("EndWorkspaceIndex");
+  const bool keepMonitors = getProperty("IncludeMonitors");
+  const bool incPartBins = getProperty("IncludePartialBins");
+
+  Progress progress(this,0.0,1.0,3);
+
+  // Get the input workspace
+  MatrixWorkspace_sptr localworkspace = getProperty("InputWorkspace");
+
+  // Sum up all the wavelength bins
+  IAlgorithm_sptr integrateAlg = createSubAlgorithm("Integration");
+  integrateAlg->setProperty<MatrixWorkspace_sptr>("InputWorkspace", localworkspace);
+  integrateAlg->setProperty<double>("RangeLower", m_MinRange);
+  integrateAlg->setProperty<double>("RangeUpper", m_MaxRange);
+  integrateAlg->setProperty<int>("StartWorkspaceIndex", m_MinSpec);
+  integrateAlg->setProperty<int>("EndWorkspaceIndex", m_MaxSpec);
+  integrateAlg->setProperty<bool>("IncludePartialBins", incPartBins);
+  integrateAlg->executeAsSubAlg();
+  progress.report(" Normalising to unity");
+
+  MatrixWorkspace_sptr integrated = integrateAlg->getProperty("OutputWorkspace");
+
+  //Sum all the spectra of the integrated workspace
+  IAlgorithm_sptr sumAlg = createSubAlgorithm("SumSpectra");
+  sumAlg->setProperty<MatrixWorkspace_sptr>("InputWorkspace", integrated);
+  sumAlg->setProperty<bool>("IncludeMonitors", keepMonitors);
+  sumAlg->executeAsSubAlg();
+  progress.report(" Normalising to unity");
+
+  MatrixWorkspace_sptr summed = sumAlg->getProperty("OutputWorkspace");
+
+  // Divide by the sum
+  MatrixWorkspace_sptr result = localworkspace/summed;
+  progress.report(" Normalising to unity");
+
+  // Assign it to the output workspace property
+  setProperty("OutputWorkspace", result);
+
+  return;
+}
+
+} // namespace Algorithms
+} // namespace Mantid
