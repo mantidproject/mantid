d3fa54d49799d9d250e04ad924ec0081435594ea
Andrei Savici <saviciat@ornl.gov>
2011-06-10 21:55:46 +0000

    Fix for numpy. Refs#455

--- Code/Mantid/Framework/PythonAPI/src/PythonWrapper.cpp
+++ Code/Mantid/Framework/PythonAPI/src/PythonWrapper.cpp
@@ -7,7 +7,7 @@
 #include "MantidKernel/MantidVersion.h"
 #include "MantidPythonAPI/stl_proxies.h"
 #include "MantidPythonAPI/MantidVecHelper.h"
-#include "Numeric/arrayobject.h"
+#include <numpy/arrayobject.h>
 // Noisy MSVC compiler that complains about Boost stuff we can do nothing about. For some reason
 // it wouldn't accept it in the BoostPython_Silent.h header
 #ifdef _MSC_VER
