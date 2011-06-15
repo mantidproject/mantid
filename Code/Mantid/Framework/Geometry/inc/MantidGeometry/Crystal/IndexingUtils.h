3194dba3ace525e9f8010056fc6e39f745a09ba2
Dennis Mikkelson <mikkelsond@uwstout.edu>
2011-06-15 19:01:51 +0000

    Minor changes to conform to Mantid style. Moved method documentation from .h to .cpp file. Added GPL to header file. refs #3178

--- Code/Mantid/Framework/Geometry/inc/MantidGeometry/Crystal/IndexingUtils.h
+++ Code/Mantid/Framework/Geometry/inc/MantidGeometry/Crystal/IndexingUtils.h
@@ -3,52 +3,59 @@
 #ifndef MANTID_GEOMETRY_INDEXING_UTILS_H_
 #define MANTID_GEOMETRY_INDEXING_UTILS_H_
 
-#include <MantidKernel/System.h>
-#include <MantidGeometry/V3D.h>
-#include <MantidGeometry/Math/Matrix.h>
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidKernel/System.h"
+#include "MantidGeometry/V3D.h"
+#include "MantidGeometry/Math/Matrix.h"
 
 namespace Mantid
 {
 namespace Geometry
 {
-/** This class contains static utility methods for indexing peaks and
- *  finding the UB matrix.  Currently there is only one method, BestFit_UB
- *  that finds the best UB matrix given some indexed peaks.
- *  @author Dennis Mikkelson 
- *  @date 2011-06-14 
+/**
+    @class IndexingUtils
+  
+    This class contains static utility methods for indexing peaks and
+    finding the UB matrix.  Currently there is only one method, BestFit_UB
+    that finds the best UB matrix given some indexed peaks.
+  
+    @author Dennis Mikkelson 
+    @date   2011-06-14 
+     
+    Copyright © 2011 ORNL, STFC Rutherford Appleton Laboratories
+  
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    File change history is stored at: 
+                 <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+
+    Code Documentation is available at 
+                 <http://doxygen.mantidproject.org>
  */
 
 class MANTID_GEOMETRY_DLL IndexingUtils
 {
   public:
 
-  // STATIC method BestFit_UB: Calculates the matrix that most nearly maps
-  // the specified hkl_vectors to the specified q_vectors.  The calculated
-  // UB minimizes the sum squared differences between UB*(h,k,l) and the
-  // corresponding (qx,qy,qz) for all of the specified hkl and Q vectors.
-  // The sum of the squares of the residual errors is returned.
-  //
-  // @param  UB           3x3 matrix that will be set to the UB matrix
-  // @param  hkl_vectors  std::vector of V3D objects that contains the 
-  //                      list of hkl values
-  // @param  q_vectors    std::vector of V3D objects that contains the list of 
-  //                      q_vectors that are indexed by the corresponding hkl
-  //                      vectors.
-  // NOTE: The number of hkl_vectors and q_vectors must be the same, and must
-  //       be at least 3.
-  //
-  // @return  This will return the sum of the squares of the residual errors.
-  //
-  // @throws  std::invalid_argument exception if there are not at least 3
-  //                                hkl and q vectors, or if the numbers of
-  //                                hkl and q vectors are not the same.
-  //
-  // @throws  std::runtime_error    exception if the QR factorization fails or
-  //                                the UB matrix can't be calculated or if 
-  //                                UB is a singular matrix.
-  static double BestFit_UB(     Matrix<double>    & UB,
-                          const std::vector<V3D>  & hkl_vectors, 
-                          const std::vector<V3D>  & q_vectors );
+  /// Find the UB matrix that best fits 3 or more indexed peaks
+  static double BestFit_UB(      Matrix<double>    & UB,
+                           const std::vector<V3D>  & hkl_vectors, 
+                           const std::vector<V3D>  & q_vectors   );
 };
 
 } // namespace Geometry
--- Code/Mantid/Framework/Geometry/src/Crystal/IndexingUtils.cpp
+++ Code/Mantid/Framework/Geometry/src/Crystal/IndexingUtils.cpp
@@ -1,9 +1,9 @@
 /* File: IndexingUtils.cpp */
 
-#include <MantidGeometry/Crystal/IndexingUtils.h>
-#include <MantidKernel/System.h>
-#include <MantidGeometry/V3D.h>
-#include <MantidGeometry/Math/Matrix.h>
+#include "MantidGeometry/Crystal/IndexingUtils.h"
+#include "MantidKernel/System.h"
+#include "MantidGeometry/V3D.h"
+#include "MantidGeometry/Math/Matrix.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -19,16 +19,45 @@ extern "C"
 using namespace std;
 using namespace Mantid::Geometry;
 
-
+/** 
+    STATIC method BestFit_UB: Calculates the matrix that most nearly maps
+    the specified hkl_vectors to the specified q_vectors.  The calculated
+    UB minimizes the sum squared differences between UB*(h,k,l) and the
+    corresponding (qx,qy,qz) for all of the specified hkl and Q vectors.
+    The sum of the squares of the residual errors is returned.
+  
+    @param  UB           3x3 matrix that will be set to the UB matrix
+    @param  hkl_vectors  std::vector of V3D objects that contains the 
+                         list of hkl values
+    @param  q_vectors    std::vector of V3D objects that contains the list of 
+                         q_vectors that are indexed by the corresponding hkl
+                         vectors.
+    NOTE: The number of hkl_vectors and q_vectors must be the same, and must
+          be at least 3.
+  
+    @return  This will return the sum of the squares of the residual errors.
+  
+    @throws  std::invalid_argument exception if there are not at least 3
+                                   hkl and q vectors, or if the numbers of
+                                   hkl and q vectors are not the same.
+   
+    @throws  std::runtime_error    exception if the QR factorization fails or
+                                   the UB matrix can't be calculated or if 
+                                   UB is a singular matrix.
+*/  
 double IndexingUtils::BestFit_UB(      Matrix<double> & UB,
                                  const vector<V3D>    & hkl_vectors, 
                                  const vector<V3D>    & q_vectors )
 {
   if ( hkl_vectors.size() < 3 ) 
+  {
    throw std::invalid_argument("Three or more indexed peaks needed to find UB");
+  }
 
   if ( hkl_vectors.size() != q_vectors.size() )
+  {
    throw std::invalid_argument("Number of hkl_vectors != number of q_vectors");
+  }
 
   gsl_matrix *H_transpose = gsl_matrix_alloc( hkl_vectors.size(), 3 );
   gsl_vector *tau         = gsl_vector_alloc( 3 );
@@ -37,8 +66,12 @@ double IndexingUtils::BestFit_UB(      Matrix<double> & UB,
                                       // Make the H-transpose matrix from the
                                       // hkl vectors and form QR factorization
   for ( size_t row = 0; row < hkl_vectors.size(); row++ )
+  {
     for ( size_t col = 0; col < 3; col++ )
+    {
       gsl_matrix_set( H_transpose, row, col, (hkl_vectors[row])[col] );
+    }
+  }
 
   int returned_flag = gsl_linalg_QR_decomp( H_transpose, tau );
 
@@ -60,11 +93,15 @@ double IndexingUtils::BestFit_UB(      Matrix<double> & UB,
   for ( size_t row = 0; row < 3; row++ )
   {
     for ( size_t i = 0; i < q_vectors.size(); i++ )
+    {
       gsl_vector_set( q, i, (q_vectors[i])[row] );
+    }
 
     returned_flag = gsl_linalg_QR_lssolve(H_transpose,tau,q,UB_row,residual);
     if ( returned_flag != 0 )
+    {
       found_UB = false;
+    }
 
     for ( size_t i = 0; i < 3; i++ )
     {
@@ -79,7 +116,9 @@ double IndexingUtils::BestFit_UB(      Matrix<double> & UB,
     UB.setRow( row, row_values );
 
     for ( size_t i = 0; i < q_vectors.size(); i++ )
+    {
       sum_sq_error += gsl_vector_get(residual, i) * gsl_vector_get(residual, i);
+    }
   }
 
   gsl_matrix_free( H_transpose );
@@ -89,10 +128,14 @@ double IndexingUtils::BestFit_UB(      Matrix<double> & UB,
   gsl_vector_free( residual );
 
   if ( !found_UB )
+  {
     throw std::runtime_error("Failed to find UB, invalid hkl or Q values");
-  
+  }
+ 
   if ( fabs ( UB.determinant() ) < 1.0e-10 )
+  {
     throw std::runtime_error("UB is singular, invalid hkl or Q values");
+  }
 
   return sum_sq_error;
 }
