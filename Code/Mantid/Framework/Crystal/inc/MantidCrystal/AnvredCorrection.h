87b024e46251c36f0ea8f564c816392e976fe393
Vickie Lynch <lynchve@ornl.gov>
2011-06-16 11:48:04 +0000

    Refs #3193 Anvred correction are implemented for historgrams, but not finished for events; have not compared results with ISAW

---
+++ Code/Mantid/Framework/Crystal/inc/MantidCrystal/AnvredCorrection.h
@@ -0,0 +1,138 @@
+#ifndef MANTID_CRYSTAL_ANVREDCORRECTION_H_
+#define MANTID_CRYSTAL_ANVREDCORRECTION_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+#include "MantidDataObjects/EventWorkspace.h"
+
+namespace Mantid
+{
+namespace Crystal
+{
+/** A base class for absorption correction algorithms.
+
+    Common Properties:
+    <UL>
+    <LI> InputWorkspace  - The name of the input workspace. </LI>
+    <LI> OutputWorkspace - The name of the output workspace. Can be the same as the input one. </LI>
+    <LI> AttenuationXSection - The attenuation cross-section for the sample material in barns. </LI>
+    <LI> ScatteringXSection - The scattering cross-section for the sample material in barns. </LI>
+    <LI> SampleNumberDensity - The number density of the sample in Angstrom^-3.</LI>
+    <LI> NumberOfWavelengthPoints - The number of wavelength points for which numerical integral is calculated (default: all points). </LI>
+    <LI> ExpMethod - The method to calculate exponential function (Normal of Fast approximation). </LI>
+    </UL>
+
+    This class, which must be overridden to provide the specific sample geometry and integration
+    elements, uses a numerical integration method to calculate attenuation factors resulting 
+    from absorption and single scattering in a sample. Factors are calculated for each spectrum
+    (i.e. detector position) and wavelength point, as defined by the input workspace.
+    Path lengths through the sample are then calculated for the centre-point of each element 
+    and a numerical integration is carried out using these path lengths over the volume elements.
+
+    This algorithm assumes that the beam comes along the Z axis, that Y is up
+    and that the sample is at the origin.
+
+    @author Vickie Lynch, Dennis Mikkelson SNS
+    @date 06/14/2011
+
+    Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+  const double pc[4][19] =
+  {    {0.9369, 0.9490, 0.9778, 1.0083, 1.0295, 1.0389, 1.0392, 1.0338,
+        1.0261, 1.0180, 1.0107, 1.0046, 0.9997, 0.9957, 0.9929, 0.9909,
+        0.9896, 0.9888, 0.9886},
+       {2.1217, 2.0149, 1.7559, 1.4739, 1.2669, 1.1606, 1.1382, 1.1724,
+        1.2328, 1.3032, 1.3706, 1.4300, 1.4804, 1.5213, 1.5524, 1.5755,
+        1.5913, 1.6005, 1.6033},
+       {-0.1304, 0.0423, 0.4664, 0.9427, 1.3112, 1.5201, 1.5844, 1.5411,
+        1.4370, 1.2998, 1.1543, 1.0131, 0.8820, 0.7670, 0.6712, 0.5951,
+        0.5398, 0.5063, 0.4955},
+       {1.1717, 1.0872, 0.8715, 0.6068, 0.3643, 0.1757, 0.0446, -0.0375,
+       -0.0853, -0.1088, -0.1176, -0.1177, -0.1123, -0.1051, -0.0978,
+       -0.0914, -0.0868, -0.0840, -0.0833}};
+  const double MAX_WAVELENGTH = 50.0;    // max in lamda_weight table
+
+  const double STEPS_PER_ANGSTROM = 100;  // resolution of lamda table 
+
+  const int NUM_WAVELENGTHS = std::ceil( MAX_WAVELENGTH * STEPS_PER_ANGSTROM);
+
+  const double radtodeg_half = 180.0/M_PI/2.;
+
+
+class DLLExport AnvredCorrection : public API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  AnvredCorrection();
+  /// Virtual destructor
+  virtual ~AnvredCorrection() {}
+  /// Algorithm's name for identification overriding a virtual method
+  virtual const std::string name() const { return "AnvredCorrection"; }
+  /// Algorithm's version for identification overriding a virtual method
+  virtual int version() const { return 1; }
+  /// Algorithm's category for identification overriding a virtual method
+  virtual const std::string category() const { return "Crystal"; }
+
+protected:
+  /** A virtual function in which additional properties of an algorithm should be declared. 
+   *  Called by init().
+   */
+  virtual void defineProperties() { /*Empty in base class*/ }
+  /// A virtual function in which additional properties should be retrieved into member variables. 
+  virtual void retrieveProperties() { /*Empty in base class*/ }
+
+  API::MatrixWorkspace_sptr m_inputWS;     ///< A pointer to the input workspace
+  /// Shared pointer to the event workspace
+  DataObjects::EventWorkspace_sptr eventW;
+
+private:
+  /// Initialisation code
+  void init();
+  /// Execution code
+  void exec();
+  void execEvent();
+  void cleanup();
+
+
+  void retrieveBaseProperties();
+  void constructSample(API::Sample& sample);
+  double getEventWeight( double lamda, double two_theta);
+  void BuildLamdaWeights(std::string spectrum_file_name);
+  double absor_sphere(double& twoth, double& wl) ;
+  void GetSpectrumWeights(std::string spectrum_file_name, std::vector<double> lamda_weight);
+  
+  double smu; // in 1/cm
+  double amu; // in 1/cm
+  double radius; // in cm
+  double power_th;  //Power of lamda in BuildLamdaWeights
+  std::vector<double> lamda_weight;
+
+  int64_t n_lambda;         ///< The number of points in wavelength, the rest is interpolated linearly
+  int64_t x_step;           ///< The step in bin number between adjacent points
+
+};
+
+} // namespace Crystal
+} // namespace Mantid
+
+#endif /* MANTID_CRYSTAL_ANVREDCORRECTION_H_*/
---
+++ Code/Mantid/Framework/Crystal/src/AnvredCorrection.cpp
@@ -0,0 +1,400 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidCrystal/AnvredCorrection.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidGeometry/Objects/ShapeFactory.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidKernel/Fast_Exponential.h"
+#include "MantidKernel/VectorHelper.h"
+
+namespace Mantid
+{
+namespace Crystal
+{
+
+// Register the class into the algorithm factory
+DECLARE_ALGORITHM(AnvredCorrection)
+
+using namespace Kernel;
+using namespace Geometry;
+using namespace API;
+using namespace DataObjects;
+
+AnvredCorrection::AnvredCorrection() : API::Algorithm(), m_inputWS(),
+  n_lambda(0), x_step(0)
+{
+}
+
+void AnvredCorrection::init()
+{
+  // The input workspace must have an instrument and units of wavelength
+  CompositeValidator<> * wsValidator = new CompositeValidator<>;
+  wsValidator->add(new WorkspaceUnitValidator<> ("Wavelength"));
+  wsValidator->add(new InstrumentValidator<>());
+
+  declareProperty(new WorkspaceProperty<> ("InputWorkspace", "", Direction::Input,wsValidator),
+    "The X values for the input workspace must be in units of wavelength");
+  declareProperty(new WorkspaceProperty<> ("OutputWorkspace", "", Direction::Output),
+    "Output workspace name");
+   declareProperty("PreserveEvents", true, "Keep the output workspace as an EventWorkspace, if the input has events (default).\n"
+      "If false, then the workspace gets converted to a Workspace2D histogram.");
+   declareProperty("VanadiumSubtracted", false, "All corrections done if false (default).\n"
+      "If true, only the spherical absorption correction.");
+
+  BoundedValidator<double> *mustBePositive = new BoundedValidator<double> ();
+  mustBePositive->setLower(0.0);
+  declareProperty("LinearScatteringCoef", -1.0, mustBePositive,
+    "Linear scattering coefficient in 1/cm");
+  declareProperty("LinearAbsorptionCoef", -1.0, mustBePositive->clone(),
+    "Linear absorption coefficient at 1.8 Angstroms in 1/cm");
+  declareProperty("Radius", -1.0, mustBePositive->clone(),
+    "Radius of the sample in centimeters");
+  declareProperty("PowerLambda", 3.0,
+    "Power of lamda ");
+
+  defineProperties();
+}
+
+void AnvredCorrection::exec()
+{
+  BuildLamdaWeights("");
+  // Retrieve the input workspace
+  m_inputWS = getProperty("InputWorkspace");
+
+  // Get the input parameters
+  retrieveBaseProperties();
+
+  eventW = boost::dynamic_pointer_cast<EventWorkspace>( m_inputWS );
+  if ((getProperty("PreserveEvents")) && (eventW != NULL))
+  {
+    //Input workspace is an event workspace. Use the other exec method
+    this->execEvent();
+    this->cleanup();
+    return;
+  }
+
+  MatrixWorkspace_sptr correctionFactors = WorkspaceFactory::Instance().create(m_inputWS);
+
+  const int64_t numHists = static_cast<int64_t>(m_inputWS->getNumberHistograms());
+  const int64_t specSize = static_cast<int64_t>(m_inputWS->blocksize());
+
+  // If the number of wavelength points has not been given, use them all
+  if ( n_lambda == 0 ) n_lambda = specSize;
+  x_step = specSize / n_lambda; // Bin step between points to calculate
+
+  if (x_step == 0) //Number of wavelength points >number of histogram points
+    x_step = 1;
+
+  std::ostringstream message;
+  message << "Correction every " << x_step << " wavelength points" << std::endl;
+  g_log.information(message.str());
+  message.str("");
+
+  const bool isHist = m_inputWS->isHistogramData();
+
+  // If sample not at origin, shift cached positions.
+  const V3D samplePos = m_inputWS->getInstrument()->getSample()->getPos();
+
+  Progress prog(this,0.0,1.0,numHists);
+  // Loop over the spectra
+  PARALLEL_FOR2(m_inputWS,correctionFactors)
+  for (int64_t i = 0; i < int64_t(numHists); ++i)
+  {
+    PARALLEL_START_INTERUPT_REGION
+
+    // Copy over bin boundaries
+    const MantidVec& Xin = m_inputWS->readX(i);
+    correctionFactors->dataX(i) = Xin;
+    const MantidVec& Yin = m_inputWS->readY(i);
+
+    // Get detector position
+    IDetector_sptr det;
+    try
+    {
+      det = m_inputWS->getDetector(i);
+    } catch (Exception::NotFoundError&)
+    {
+      // Catch if no detector. Next line tests whether this happened - test placed
+      // outside here because Mac Intel compiler doesn't like 'continue' in a catch
+      // in an openmp block.
+    }
+    // If no detector found, skip onto the next spectrum
+    if ( !det ) continue;
+
+    // Get a reference to the Y's in the output WS for storing the factors
+    MantidVec& Y = correctionFactors->dataY(i);
+
+    // This is the scattered beam direction
+    IInstrument_sptr inst = m_inputWS->getInstrument();
+    V3D dir = det->getPos() - samplePos;
+    // Two-theta = polar angle = scattering angle = between +Z vector and the scattered beam
+    double scattering = dir.angle( V3D(0.0, 0.0, 1.0) );
+
+    // Loop through the bins in the current spectrum every x_step
+    for (int64_t j = 0; j < specSize; j = j + x_step)
+    {
+      const double lambda = (isHist ? (0.5 * (Xin[j] + Xin[j + 1])) : Xin[j]);
+
+      Y[j] = Yin[j]*this->getEventWeight(scattering, lambda);
+
+      // Make certain that last point is calculates
+      if ( x_step > 1 && j+x_step >= specSize && j+1 != specSize)
+      {
+        j = specSize - x_step - 1;
+      }
+    }
+
+    if (x_step > 1) // Interpolate linearly between points separated by x_step, last point required
+    {
+      VectorHelper::linearlyInterpolateY(Xin, Y, static_cast<double>(x_step));
+    }
+
+    prog.report();
+
+    PARALLEL_END_INTERUPT_REGION
+  }
+  PARALLEL_CHECK_INTERUPT_REGION
+
+  setProperty("OutputWorkspace", correctionFactors);
+
+}
+
+void AnvredCorrection::cleanup()
+{
+  //Clear vectors to free up memory.
+  lamda_weight.clear();
+}
+
+void AnvredCorrection::execEvent()
+{
+
+  const int64_t numHists = static_cast<int64_t>(m_inputWS->getNumberHistograms());
+  //Create a new outputworkspace with not much in it
+  DataObjects::EventWorkspace_sptr correctionFactors;
+  correctionFactors = boost::dynamic_pointer_cast<EventWorkspace>(
+      API::WorkspaceFactory::Instance().create("EventWorkspace",numHists,2,1) );
+  //Copy required stuff from it
+  API::WorkspaceFactory::Instance().initializeFromParent(m_inputWS, correctionFactors, true);
+
+
+  // If sample not at origin, shift cached positions.
+  const V3D samplePos = m_inputWS->getInstrument()->getSample()->getPos();
+
+  Progress prog(this,0.0,1.0,numHists);
+  // Loop over the spectra
+  PARALLEL_FOR2(eventW,correctionFactors)
+  for (int64_t i = 0; i < int64_t(numHists); ++i)
+  {
+    PARALLEL_START_INTERUPT_REGION
+
+    // Copy over bin boundaries
+    const MantidVec& X = m_inputWS->readX(i);
+    correctionFactors->dataX(i) = X;
+
+    // Get detector position
+    IDetector_sptr det;
+    try
+    {
+      det = m_inputWS->getDetector(i);
+    } catch (Exception::NotFoundError&)
+    {
+      // Catch if no detector. Next line tests whether this happened - test placed
+      // outside here because Mac Intel compiler doesn't like 'continue' in a catch
+      // in an openmp block.
+    }
+    // If no detector found, skip onto the next spectrum
+    if ( !det ) continue;
+
+    // This is the scattered beam direction
+    IInstrument_sptr inst = m_inputWS->getInstrument();
+    V3D dir = det->getPos() - samplePos;
+    // Two-theta = polar angle = scattering angle = between +Z vector and the scattered beam
+    double scattering = dir.angle( V3D(0.0, 0.0, 1.0) );
+
+      EventWorkspace_const_sptr event_correctionFactors = boost::dynamic_pointer_cast<const EventWorkspace>( correctionFactors);
+      EventList el = eventW->getEventList(i);
+      el.switchTo(WEIGHTED_NOTIME);
+      std::vector<WeightedEventNoTime> events = el.getWeightedEventsNoTime();
+
+      //std::vector<TofEvent>::iterator itev;
+      //std::vector<TofEvent>::iterator itev_end = events.end();
+
+      // multiplying an event list by a scalar value
+      /*for (itev = events.begin(); itev != itev_end; itev++)
+      {
+        const double lambda = itev->tof();
+        double value = this->getEventWeight(scattering, lambda);
+        itev->m_errorSquared = static_cast<float>(itev->m_errorSquared * value*value);
+        itev->m_weight *= static_cast<float>(value);
+      }*/
+
+    prog.report();
+
+    PARALLEL_END_INTERUPT_REGION
+  }
+  PARALLEL_CHECK_INTERUPT_REGION
+
+  setProperty("OutputWorkspace", boost::dynamic_pointer_cast<MatrixWorkspace>(correctionFactors));
+
+  // Now do some cleaning-up since destructor may not be called immediately
+  this->cleanup();
+}
+
+/// Fetch the properties and set the appropriate member variables
+void AnvredCorrection::retrieveBaseProperties()
+{
+  smu = getProperty("LinearScatteringCoef"); // in 1/cm
+  amu = getProperty("LinearAbsorptionCoef"); // in 1/cm
+  radius = getProperty("Radius"); // in cm
+  power_th = getProperty("PowerLambda"); // in cm
+
+  // Call the virtual function for any further properties
+  retrieveProperties();
+}
+
+ /**
+  *  Get the weight factor that would be used for an event occuring 
+  *  at the specified wavelength, with the specified two_theta value.
+  *
+  *  @param  lamda      The wavelength of an event.
+  *  @param  two_theta  The scattering angle of the event.
+  *
+  *  @return The weight factor for the specified position and wavelength.
+  */
+double AnvredCorrection::getEventWeight( double lamda, double two_theta )
+  {
+    double transinv = 1;
+    if ( radius > 0 )
+       transinv = absor_sphere(two_theta, lamda);
+    // Only Spherical absorption correction if vanadium subtracted
+    if (getProperty("VanadiumSubtracted")) return transinv;
+
+    // Resolution of the lambda table
+    size_t lamda_index = static_cast<size_t>( STEPS_PER_ANGSTROM * lamda );
+
+    if ( lamda_index >= lamda_weight.size() )
+      lamda_index = lamda_weight.size() - 1;
+
+    double lamda_w     = lamda_weight[ lamda_index ];
+
+    double sin_theta = std::sin( two_theta/2 );
+    double pix_weight = sin_theta * sin_theta;
+
+    double event_weight = pix_weight * lamda_w * transinv;
+
+    return event_weight;
+  }
+
+ /**
+  *       function to calculate a spherical absorption correction
+  *       and tbar. based on values in:
+  *
+  *       c. w. dwiggins, jr., acta cryst. a31, 395 (1975).
+  *
+  *       in this paper, a is the transmission and a* = 1/a is
+  *       the absorption correction.
+  *
+  *       input are the smu (scattering) and amu (absorption at 1.8 ang.)
+  *       linear absorption coefficients, the radius r of the sample
+  *       the theta angle and wavelength.
+  *       the absorption (absn) and tbar are returned.
+  *
+  *       a. j. schultz, june, 2008
+  */
+double AnvredCorrection::absor_sphere(double& twoth, double& wl)
+  {
+    int i;
+    double mu, mur;         //mu is the linear absorption coefficient,
+                            //r is the radius of the spherical sample.
+    double theta,astar1,astar2,frac,astar;
+//  double trans;
+//  double tbar;
+
+//  For each of the 19 theta values in dwiggins (theta = 0.0 to 90.0
+//  in steps of 5.0 deg.), the astar values vs.mur were fit to a third
+//  order polynomial in excel. these values are given in the static array
+//  pc[][]
+
+    mu = smu + (amu/1.8f)*wl;
+
+    mur = mu*radius;
+
+    theta = twoth*radtodeg_half;
+
+//  using the polymial coefficients, calulate astar (= 1/transmission) at
+//  theta values below and above the actual theta value.
+
+    i = (int)(theta/5.);
+    astar1 = pc[0][i] + mur * (pc[1][i] + mur * (pc[2][i] + pc[3][i] * mur));
+
+    i = i+1;
+    astar2 = pc[0][i] + mur * (pc[1][i] + mur * (pc[2][i] + pc[3][i] * mur));
+
+//  do a linear interpolation between theta values.
+
+    frac = theta - static_cast<double>(static_cast<int>( theta / 5. )) * 5.;//theta%5.
+    frac = frac/5.;
+
+    astar = astar1*(1-frac) + astar2*frac;       // astar is the correction
+//  trans = 1.f/astar;                           // trans is the transmission
+                                                 // trans = exp(-mu*tbar)
+
+//  calculate tbar as defined by coppens.
+//  tbar = -(double)Math.log(trans)/mu;
+
+    return astar;
+  }
+  /**
+   *  Build the list of weights corresponding to different wavelengths.
+   *  Although the spectrum file need not have a fixed number of
+   *  points, it MUST have the spectrum recorded as a histogram with one
+   *  more bin boundary than the number of bins.
+   *    The entries in the table produced are:
+   *
+   *     1/( lamda^power * spec(lamda) )
+   *
+   *  Where power was chosen to give a relatively uniform intensity display
+   *  in 3D.  The power is currently 3 if an incident spectrum is present
+   *  and 2.4 if no incident spectrum is used.
+   */
+  void AnvredCorrection::BuildLamdaWeights( std::string spectrum_file_name )
+  {
+                                             // Theoretically correct value 3.0;
+                                             // if we have an incident spectrum
+//  double power_ns = 2.4;                   // lower power needed to find
+                                             // peaks in ARCS data with no
+                                             // incident spectrum
+    double   lamda;
+
+    bool use_incident_spectrum = true;
+    double power = power_th;
+
+    //GetSpectrumWeights( spectrum_file_name, lamda_weight);
+
+    if ( lamda_weight.size() == 0 )              // loading spectrum failed so use
+    {                                        // array of 1's
+      use_incident_spectrum = false;
+//    power = power_ns;                      // This is commented out, so we
+                                             // don't override user specified 
+                                             // value.
+      for ( int i = 0; i < NUM_WAVELENGTHS; i++ )
+        lamda_weight.push_back(1.);
+    }
+
+    for ( size_t i = 0; i < lamda_weight.size(); i++ )
+    {
+      lamda = static_cast<double>(i) / STEPS_PER_ANGSTROM;
+      lamda_weight[i] *= (double)(1/std::pow(lamda,power));
+    }
+
+    if ( use_incident_spectrum )
+      std::cout << "Built weights using incident spectrum from " <<
+                          spectrum_file_name << " with wl power " << power ;
+    else
+      std::cout << "Built APPROXIMATE weights with wl power " << power ;
+  }
+
+
+} // namespace Crystal
+} // namespace Mantid
