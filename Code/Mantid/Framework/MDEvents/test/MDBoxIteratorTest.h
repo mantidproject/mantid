78870c7f42fe39d8520806bc9be870af4dfd4186
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-06-23 21:23:27 +0000

    Refs #3134: Let's try that again.

--- Code/Mantid/Framework/MDEvents/test/MDBoxIteratorTest.h
+++ Code/Mantid/Framework/MDEvents/test/MDBoxIteratorTest.h
@@ -44,93 +44,93 @@ public:
                   D210 D211 D212 D213
 
    */
-  void xtest_iterator()
+  void test_iterator()
   {
-    // Top level grid box
-    gbox_t * A = MDEventsTestHelper::makeMDGridBox<1>(4, 1, 0.0, 64.0);
-    A->splitContents(0); // Split B0 into C00 to C03
-    A->splitContents(2); // Split B2 into C20 to C23
-    gbox_t * B0 = dynamic_cast<gbox_t *>(A->getChild(0));
-    gbox_t * B2 = dynamic_cast<gbox_t *>(A->getChild(2));
-    B2->splitContents(1); // Split C21 into D210 to D212
-
-    ibox_t * B1 = A->getChild(1);
-    ibox_t * B3 = A->getChild(3);
-    TS_ASSERT_EQUALS( B1, A->getChild(1));
-    ibox_t * C00 = B0->getChild(0);
-    ibox_t * C01 = B0->getChild(1);
-    ibox_t * C02 = B0->getChild(2);
-    ibox_t * C03 = B0->getChild(3);
-    ibox_t * C20 = B2->getChild(0);
-    gbox_t * C21 = dynamic_cast<gbox_t *>(B2->getChild(1));
-    ibox_t * C22 = B2->getChild(2);
-    ibox_t * C23 = B2->getChild(3);
-    ibox_t * D210 = C21->getChild(0);
-    ibox_t * D211 = C21->getChild(1);
-    ibox_t * D212 = C21->getChild(2);
-    ibox_t * D213 = C21->getChild(3);
-
-    // Create an iterator
-    MDBoxIterator<MDEvent<1>,1> * it = new MDBoxIterator<MDEvent<1>,1>(A, 20, false);
-
-    // Start with the top one
-    TS_ASSERT_EQUALS( it->getBox(), A);
-    TS_ASSERT( nextIs(it, B0) );
-    TS_ASSERT( nextIs(it, C00) );
-    TS_ASSERT( nextIs(it, C01) );
-    TS_ASSERT( nextIs(it, C02) );
-    TS_ASSERT( nextIs(it, C03) );
-    TS_ASSERT( nextIs(it, B1) );
-    TS_ASSERT( nextIs(it, B2) );
-    TS_ASSERT( nextIs(it, C20) );
-    TS_ASSERT( nextIs(it, C21) );
-    TS_ASSERT( nextIs(it, D210) );
-    TS_ASSERT( nextIs(it, D211) );
-    TS_ASSERT( nextIs(it, D212) );
-    TS_ASSERT( nextIs(it, D213) );
-    TS_ASSERT( nextIs(it, C22) );
-    TS_ASSERT( nextIs(it, C23) );
-    TS_ASSERT( nextIs(it, B3) );
-    // No more!
-    TS_ASSERT( !it->next() );
-    // Calling next again does not cause problems.
-    TS_ASSERT( !it->next() );
-    TS_ASSERT( !it->next() );
-
-    // Limit depth to 1 (the B level)
-    it = new MDBoxIterator<MDEvent<1>,1>(A, 1, false);
-    TS_ASSERT_EQUALS( it->getBox(), A);
-    TS_ASSERT( nextIs(it, B0) );
-    TS_ASSERT( nextIs(it, B1) );
-    TS_ASSERT( nextIs(it, B2) );
-    TS_ASSERT( nextIs(it, B3) );
-    TS_ASSERT( !it->next() );
+//    // Top level grid box
+//    gbox_t * A = MDEventsTestHelper::makeMDGridBox<1>(4, 1, 0.0, 64.0);
+//    A->splitContents(0); // Split B0 into C00 to C03
+//    A->splitContents(2); // Split B2 into C20 to C23
+//    gbox_t * B0 = dynamic_cast<gbox_t *>(A->getChild(0));
+//    gbox_t * B2 = dynamic_cast<gbox_t *>(A->getChild(2));
+//    B2->splitContents(1); // Split C21 into D210 to D212
+//
+//    ibox_t * B1 = A->getChild(1);
+//    ibox_t * B3 = A->getChild(3);
+//    TS_ASSERT_EQUALS( B1, A->getChild(1));
+//    ibox_t * C00 = B0->getChild(0);
+//    ibox_t * C01 = B0->getChild(1);
+//    ibox_t * C02 = B0->getChild(2);
+//    ibox_t * C03 = B0->getChild(3);
+//    ibox_t * C20 = B2->getChild(0);
+//    gbox_t * C21 = dynamic_cast<gbox_t *>(B2->getChild(1));
+//    ibox_t * C22 = B2->getChild(2);
+//    ibox_t * C23 = B2->getChild(3);
+//    ibox_t * D210 = C21->getChild(0);
+//    ibox_t * D211 = C21->getChild(1);
+//    ibox_t * D212 = C21->getChild(2);
+//    ibox_t * D213 = C21->getChild(3);
+//
+//    // Create an iterator
+//    MDBoxIterator<MDEvent<1>,1> * it = new MDBoxIterator<MDEvent<1>,1>(A, 20, false);
+//
+//    // Start with the top one
+//    TS_ASSERT_EQUALS( it->getBox(), A);
+//    TS_ASSERT( nextIs(it, B0) );
+//    TS_ASSERT( nextIs(it, C00) );
+//    TS_ASSERT( nextIs(it, C01) );
+//    TS_ASSERT( nextIs(it, C02) );
+//    TS_ASSERT( nextIs(it, C03) );
+//    TS_ASSERT( nextIs(it, B1) );
+//    TS_ASSERT( nextIs(it, B2) );
+//    TS_ASSERT( nextIs(it, C20) );
+//    TS_ASSERT( nextIs(it, C21) );
+//    TS_ASSERT( nextIs(it, D210) );
+//    TS_ASSERT( nextIs(it, D211) );
+//    TS_ASSERT( nextIs(it, D212) );
+//    TS_ASSERT( nextIs(it, D213) );
+//    TS_ASSERT( nextIs(it, C22) );
+//    TS_ASSERT( nextIs(it, C23) );
+//    TS_ASSERT( nextIs(it, B3) );
+//    // No more!
+//    TS_ASSERT( !it->next() );
+//    // Calling next again does not cause problems.
+//    TS_ASSERT( !it->next() );
+//    TS_ASSERT( !it->next() );
+//
+//    // Limit depth to 1 (the B level)
+//    it = new MDBoxIterator<MDEvent<1>,1>(A, 1, false);
+//    TS_ASSERT_EQUALS( it->getBox(), A);
+//    TS_ASSERT( nextIs(it, B0) );
+//    TS_ASSERT( nextIs(it, B1) );
+//    TS_ASSERT( nextIs(it, B2) );
+//    TS_ASSERT( nextIs(it, B3) );
+//    TS_ASSERT( !it->next() );
 
   }
 
 
 };
 
-//
-//class MDBoxIteratorTestPerformace : public CxxTest::TestSuite
-//{
-//public:
-//  MDGridBox<MDEvent<3>,3> * top;
-//
-//  void setUp()
-//  {
-//    // About 2 mi1968876
-//    top = MDEventsTestHelper::makeRecursiveMDGridBox<3>(5, 2);
-//  }
-//
-//  void tearDown()
-//  {
-//    delete top;
-//  }
-//
-//  /** Make a simple iterator that will go through all the boxes */
-//  void test_iterator()
-//  {
+
+class MDBoxIteratorTestPerformace : public CxxTest::TestSuite
+{
+public:
+  MDGridBox<MDEvent<3>,3> * top;
+
+  void setUp()
+  {
+    // About 2 mi1968876
+    top = MDEventsTestHelper::makeRecursiveMDGridBox<3>(5, 2);
+  }
+
+  void tearDown()
+  {
+    delete top;
+  }
+
+  /** Make a simple iterator that will go through all the boxes */
+  void test_iterator()
+  {
 //    // Count the top level box.
 //    size_t counter = 1;
 //    IMDBox<MDEvent<3>,3> * box;
@@ -144,13 +144,13 @@ public:
 //    }
 //    TS_ASSERT( box );
 //    TS_ASSERT_EQUALS( counter, 125*125*125 + 125*125 + 125 + 1);
-//  }
-//
-//  // ---------------------------------------------------------------
-//  /** For comparison, let's use getBoxes() that fills a vector
-//   */
-//  void test_getBoxes()
-//  {
+  }
+
+  // ---------------------------------------------------------------
+  /** For comparison, let's use getBoxes() that fills a vector
+   */
+  void test_getBoxes()
+  {
 //    std::vector< IMDBox<MDEvent<3>,3> * > boxes;
 //    top->getBoxes(boxes, 20, false);
 //    TS_ASSERT_EQUALS( boxes.size(), 125*125*125 + 125*125 + 125 + 1);
@@ -167,9 +167,9 @@ public:
 //    }
 //
 //    TS_ASSERT_EQUALS( counter, 125*125*125 + 125*125 + 125 + 1);
-//  }
-//
-//};
+  }
+
+};
 
 
 #endif /* MANTID_MDEVENTS_MDBOXITERATORTEST_H_ */
