c40b02573cb3773122749b641b0bd6708fae7010
Vickie Lynch <lynchve@ornl.gov>
2011-06-27 18:29:09 +0000

    Refs #3229 Reference pixel should be input as workspace index

--- Code/Mantid/Framework/PythonAPI/PythonAlgorithms/CalibrateRectangularDetectors.py
+++ Code/Mantid/Framework/PythonAPI/PythonAlgorithms/CalibrateRectangularDetectors.py
@@ -186,7 +186,6 @@ class CalibrateRectangularDetectors(PythonAlgorithm):
             if y_s[midBin] > ymax:
                     refpixel = s
                     ymax = y_s[midBin]
-        refpixel = temp.getAxis(1).spectraNumber(refpixel)
         print "Reference spectra=",refpixel
         # Remove old calibration files
         cmd = "rm "+self._outDir+str(wksp)+".cal*"
@@ -214,7 +213,6 @@ class CalibrateRectangularDetectors(PythonAlgorithm):
                 if y_s[midBin] > ymax:
                         refpixel = s
                         ymax = y_s[midBin]
-            refpixel = temp.getAxis(1).spectraNumber(refpixel)
             print "Reference spectra=",refpixel
             CrossCorrelate(InputWorkspace=temp, OutputWorkspace=str(wksp)+"cc2", ReferenceSpectra=refpixel,
                 WorkspaceIndexMin=self._lastpixel+1, WorkspaceIndexMax=temp.getNumberHistograms()-1, XMin=self._peakmin2, XMax=self._peakmax2)
