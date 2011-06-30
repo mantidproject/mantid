a23beb8dda54d521acb3204640366c33b130b4c7
Mathieu Doucet <doucetm@ornl.gov>
2011-06-30 21:18:47 +0000

    Fixes #3295

--- Code/Mantid/Framework/PythonAPI/test/ImportTest.py
+++ Code/Mantid/Framework/PythonAPI/test/ImportTest.py
@@ -1,6 +1,11 @@
 import unittest
+import types
 
 class ImportTest(unittest.TestCase):
     def test_import(self):
         from MantidFramework import mtd
         mtd.initialise()
+        
+    def test_alias(self):
+        from mantidsimple import *
+        self.assertEqual(type(UpdateInstrumentFromRaw), types.FunctionType)
