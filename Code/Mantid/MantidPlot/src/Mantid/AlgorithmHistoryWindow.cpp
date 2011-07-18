0f7bee75b672ed03d79d91eac49d1c500fa5b760
Stuart Campbell <campbellsi@ornl.gov>
2011-07-18 21:41:31 +0000

    fixes #3368

--- Code/Mantid/MantidPlot/src/Mantid/AlgorithmHistoryWindow.cpp
+++ Code/Mantid/MantidPlot/src/Mantid/AlgorithmHistoryWindow.cpp
@@ -346,7 +346,7 @@ QString AlgorithmHistoryWindow::generateScript()
         {
           std::string sanitisedname=sanitizePropertyName(name);
           algParam+=sanitisedname;
-          algParam+="'";//"=\"";
+          algParam+="='";//"=\"";
           algParam+=value;
           algParam+="',";//"\",";
         }
