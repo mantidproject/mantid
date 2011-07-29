6b2ae6772c743cc62b8ac21b3448238b290ab1ce
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-07-29 15:37:43 +0000

    Refs #3448 Add a switch for precompiled headers and turn it off while I figure out why the tests fail.

--- Code/Mantid/Build/CMake/PrecompiledHeaderCommands.cmake
+++ Code/Mantid/Build/CMake/PrecompiledHeaderCommands.cmake
@@ -1,7 +1,11 @@
 #
 # - Defines macros for dealing with precompiled header files on platforms that
 #   support them
-#
+
+# A switch to turn them on and off
+# 
+SET ( USE_PRECOMPILED_HEADERS OFF CACHE BOOL "If true, will use precompiled headers on those platforms that support it."  )
+
 # MACRO ( ADD_PRECOMPILED_HEADER HEADER SOURCE BIN_NAME SOURCE_FILES )
 #    Adds the given header and source file as precompiled header files 
 #       HEADER - The relative path from the current CMakeLists.txt file to the header file
@@ -10,31 +14,33 @@
 #       SOURCE_FILES - The list of source files from the project
 #       HEADER_FILES - The list of header files from the project (mainly for MSVC so that they show up in the tree)
 MACRO ( ADD_PRECOMPILED_HEADER HEADER INC_PREFIX BIN_NAME SOURCE_FILES HEADER_FILES )
-    IF ( MSVC )
-        SET ( PRECOMPILED_BIN "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${BIN_NAME}.pch" )
-        GET_FILENAME_COMPONENT( HEADER_FILE ${HEADER} NAME )
-        IF ( "${INC_PREFIX}" STREQUAL "" )
-            SET ( PRECOMPILED_HDR "${HEADER_FILE}" )
-        ELSE ()
-            SET ( PRECOMPILED_HDR "${INC_PREFIX}/${HEADER_FILE}" )
-        ENDIF ()
-        SET ( PRECOMPILED_SRC "${CMAKE_CURRENT_BINARY_DIR}/${BIN_NAME}PrecompiledHeader.cpp" )
-        # Generate a source file
-        ADD_CUSTOM_COMMAND( OUTPUT ${PRECOMPILED_SRC}
-                            DEPENDS ${PRECOMPILED_HDR}
-                            COMMAND ${CMAKE_COMMAND} 
-                            ARGS -E touch ${PRECOMPILED_SRC} 
-        )
-        SET_SOURCE_FILES_PROPERTIES ( ${PRECOMPILED_SRC} PROPERTIES GENERATED true )
-        SET_SOURCE_FILES_PROPERTIES ( ${PRECOMPILED_SRC}
-                                      PROPERTIES COMPILE_FLAGS "/Yc\"${PRECOMPILED_HDR}\" /Fp\"${PRECOMPILED_BIN}\" /FI\"${PRECOMPILED_HDR}\""
-                                      OBJECT_OUTPUTS "${PRECOMPILED_BIN}"
-        )
-        SET_SOURCE_FILES_PROPERTIES ( ${${SOURCE_FILES}}
-                                      PROPERTIES COMPILE_FLAGS "/Yu\"${PRECOMPILED_HDR}\" /FI\"${PRECOMPILED_HDR}\" /Fp\"${PRECOMPILED_BIN}\""
-                                      OBJECT_DEPENDS "${PRECOMPILED_BIN}" 
-        )
-        LIST ( APPEND ${SOURCE_FILES} ${PRECOMPILED_SRC} )
-        LIST ( APPEND ${HEADER_FILES} ${HEADER} )
-    ENDIF ( MSVC )
+    IF ( USE_PRECOMPILED_HEADERS )
+        IF ( MSVC )
+            SET ( PRECOMPILED_BIN "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${BIN_NAME}.pch" )
+            GET_FILENAME_COMPONENT( HEADER_FILE ${HEADER} NAME )
+            IF ( "${INC_PREFIX}" STREQUAL "" )
+                SET ( PRECOMPILED_HDR "${HEADER_FILE}" )
+            ELSE ()
+                SET ( PRECOMPILED_HDR "${INC_PREFIX}/${HEADER_FILE}" )
+            ENDIF ()
+            SET ( PRECOMPILED_SRC "${CMAKE_CURRENT_BINARY_DIR}/${BIN_NAME}PrecompiledHeader.cpp" )
+            # Generate a source file
+            ADD_CUSTOM_COMMAND( OUTPUT ${PRECOMPILED_SRC}
+                                DEPENDS ${PRECOMPILED_HDR}
+                                COMMAND ${CMAKE_COMMAND} 
+                                ARGS -E touch ${PRECOMPILED_SRC} 
+            )
+            SET_SOURCE_FILES_PROPERTIES ( ${PRECOMPILED_SRC} PROPERTIES GENERATED true )
+            SET_SOURCE_FILES_PROPERTIES ( ${PRECOMPILED_SRC}
+                                          PROPERTIES COMPILE_FLAGS "/Yc\"${PRECOMPILED_HDR}\" /Fp\"${PRECOMPILED_BIN}\" /FI\"${PRECOMPILED_HDR}\""
+                                          OBJECT_OUTPUTS "${PRECOMPILED_BIN}"
+            )
+            SET_SOURCE_FILES_PROPERTIES ( ${${SOURCE_FILES}}
+                                          PROPERTIES COMPILE_FLAGS "/Yu\"${PRECOMPILED_HDR}\" /FI\"${PRECOMPILED_HDR}\" /Fp\"${PRECOMPILED_BIN}\""
+                                          OBJECT_DEPENDS "${PRECOMPILED_BIN}" 
+            )
+            LIST ( APPEND ${SOURCE_FILES} ${PRECOMPILED_SRC} )
+            LIST ( APPEND ${HEADER_FILES} ${HEADER} )
+        ENDIF ( MSVC )
+    ENDIF()
 ENDMACRO( ADD_PRECOMPILED_HEADER )
