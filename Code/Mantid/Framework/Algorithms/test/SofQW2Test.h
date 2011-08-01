8bfdfca59162bd0567e73aef432d17643e60c73e
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-08-01 10:13:55 +0000

    Refs #2233 Update tests values.

--- Code/Mantid/Framework/Algorithms/test/SofQW2Test.h
+++ Code/Mantid/Framework/Algorithms/test/SofQW2Test.h
@@ -65,26 +65,19 @@ public:
     TS_ASSERT_EQUALS( (*(result->getAxis(1)))(3), 1.25 );
     TS_ASSERT_EQUALS( (*(result->getAxis(1)))(6), 2.0 );
 
-    int indices[6] = {1160, 1145, 1200, 99, 1654, 1025};
-
-    for( size_t i = 0; i < 6; ++i )
-    {
-      std::cerr << std::setprecision(12) << result->readY(i)[indices[i]] << "  " << result->readE(i)[indices[i]] << "\n";
-    }
-
     const double delta(1e-08);
     TS_ASSERT_DELTA( result->readY(0)[1160], 13.71406100 , delta);
     TS_ASSERT_DELTA( result->readE(0)[1160], 0.062926550, delta);
     TS_ASSERT_DELTA( result->readY(1)[1145], 5.681229516, delta);
     TS_ASSERT_DELTA( result->readE(1)[1145], 0.040755760, delta);
-    TS_ASSERT_DELTA( result->readY(2)[1200], 1.662663838, delta);
-    TS_ASSERT_DELTA( result->readE(2)[1200], 0.021415982, delta);
-    TS_ASSERT_DELTA( result->readY(3)[99],   0.042476730, delta);
-    TS_ASSERT_DELTA( result->readE(3)[99],   0.007617287, delta);
+    TS_ASSERT_DELTA( result->readY(2)[1200], 1.666488499, delta);
+    TS_ASSERT_DELTA( result->readE(2)[1200], 0.021459611, delta);
+    TS_ASSERT_DELTA( result->readY(3)[99],   0.042326574, delta);
+    TS_ASSERT_DELTA( result->readE(3)[99],   0.007609629, delta);
     TS_ASSERT_DELTA( result->readY(4)[1654], 0.01732786, delta);
     TS_ASSERT_DELTA( result->readE(4)[1654], 0.00182863, delta);
-    TS_ASSERT_DELTA( result->readY(5)[1025], 0.05377700, delta);
-    TS_ASSERT_DELTA( result->readE(5)[1025], 0.00408489, delta);
+    TS_ASSERT_DELTA( result->readY(5)[1025], 0.05412063, delta);
+    TS_ASSERT_DELTA( result->readE(5)[1025], 0.00410309, delta);
 
     AnalysisDataService::Instance().remove(inputWS);
     AnalysisDataService::Instance().remove(outputWS);    
