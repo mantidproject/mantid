7b658cde032b9d76ea7a0fcc0376b6599ca3c6d5
Vickie Lynch <lynchve@ornl.gov>
2011-08-03 19:38:32 +0000

    Refs #2379 Change category to Crystal

--- Code/Mantid/Framework/Crystal/inc/MantidCrystal/PeakIntegration.h
+++ Code/Mantid/Framework/Crystal/inc/MantidCrystal/PeakIntegration.h
@@ -48,7 +48,7 @@ public:
   /// Algorithm's version for identification overriding a virtual method
   virtual int version() const { return 1; }
   /// Algorithm's category for identification overriding a virtual method
-  virtual const std::string category() const { return "Diffraction"; }
+  virtual const std::string category() const { return "Crystal"; }
 
 private:
   API::MatrixWorkspace_sptr inputW;  ///< A pointer to the input workspace
