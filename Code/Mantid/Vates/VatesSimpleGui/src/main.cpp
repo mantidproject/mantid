035268f2f56436b8032959a9b063fdeb5603a89e
Michael Reuter <reuterma@ornl.gov>
2011-08-04 20:22:52 +0000

    Creating and using wrapper class to handle exception notification. This refs #3298.

--- Code/Mantid/Vates/VatesSimpleGui/src/main.cpp
+++ Code/Mantid/Vates/VatesSimpleGui/src/main.cpp
@@ -1,11 +1,9 @@
-#include <QApplication>
-#include <pqPVApplicationCore.h>
+#include "SimpleGuiApp.h"
 #include "MpMainWindow.h"
 
 int main(int argc, char** argv)
 {
-  QApplication app(argc, argv);
-  pqPVApplicationCore appCore(argc, argv);
+  SimpleGuiApp app(argc, argv);
   mpMainWindow window;
   window.show();
   return app.exec();
