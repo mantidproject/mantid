21e3a578e5f172c82f0aa4983bc903cb0ece1cd2
Russell Taylor <taylorrj@ornl.gov>
2011-08-04 15:24:24 +0000

    Re-jig SavePHXTest to see if it ends mystery test failures on ORNL mac build. Re #3495.

--- Code/Mantid/Framework/DataHandling/test/SavePHXTest.h
+++ Code/Mantid/Framework/DataHandling/test/SavePHXTest.h
@@ -4,32 +4,27 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidDataHandling/SavePHX.h"
-#include "MantidAPI/FrameworkManager.h"
 
-class SavePHXTest: public CxxTest::TestSuite {
+class SavePHXTest: public CxxTest::TestSuite
+{
 private:
-	Mantid::API::IAlgorithm* phxSaver;
+  Mantid::DataHandling::SavePHX phxSaver;
 public:
-    static SavePHXTest *createSuite() { return new SavePHXTest(); }
+  static SavePHXTest *createSuite() { return new SavePHXTest(); }
   static void destroySuite(SavePHXTest *suite) { delete suite; }
 
-	SavePHXTest() {
-		phxSaver = Mantid::API::FrameworkManager::Instance().createAlgorithm(
-				"SavePHX");
-	}
+  void testAlgorithmName()
+  {
+    TS_ASSERT_EQUALS(phxSaver.name(), "SavePHX");
+  }
 
-	void testAlgorithmName()
-	{
-		TS_ASSERT_EQUALS(phxSaver->name(), "SavePHX");
-	}
+  void testInit()
+  {
+    TS_ASSERT_THROWS_NOTHING(phxSaver.initialize());
+    TS_ASSERT(phxSaver.isInitialized());
+  }
 
-	void testInit()
-	{
-		TS_ASSERT_THROWS_NOTHING(phxSaver->initialize());
-		TS_ASSERT(phxSaver->isInitialized());
-	}
-
-	// TODO: Add proper testing...
+  // TODO: Add proper testing...
 };
 
 #endif /*SAVEPHXTEST_H_*/
