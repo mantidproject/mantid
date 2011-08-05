105441ff37eca9fde527e50c3a9b12a4d10b3613
Vickie Lynch <lynchve@ornl.gov>
2011-08-05 14:16:17 +0000

    Refs #3508 Changed order of input workspaces

--- Code/Mantid/Framework/Crystal/src/PeakIntegration.cpp
+++ Code/Mantid/Framework/Crystal/src/PeakIntegration.cpp
@@ -50,9 +50,9 @@ namespace Mantid
     void PeakIntegration::init()
     {
 
+      declareProperty(new WorkspaceProperty<PeaksWorkspace>("InPeaksWorkspace", "", Direction::Input), "Name of the peaks workspace.");
       declareProperty(new WorkspaceProperty<>("InputWorkspace", "", Direction::Input)
           , "A 2D workspace with X values of time of flight");
-      declareProperty(new WorkspaceProperty<PeaksWorkspace>("InPeaksWorkspace", "", Direction::Input), "Name of the peaks workspace.");
       declareProperty(new WorkspaceProperty<PeaksWorkspace>("OutPeaksWorkspace", "", Direction::Output), "Name of the output peaks workspace with integrated intensities.");
       declareProperty("FitSlices", true, "Integrate slices using IntegratePeakTimeSlices algorithm (default).\n"
         "If false, then next 6 variables are used for shoebox with pixels masked that are outside peak cluster.");
