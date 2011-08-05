3766814876fafe35bd8bf877d41a019aaffdee56
Michael Reuter <reuterma@ornl.gov>
2011-08-05 15:57:31 +0000

    Adding extra traps in main program. This refs #3298.

--- Code/Mantid/Vates/VatesSimpleGui/src/main.cpp
+++ Code/Mantid/Vates/VatesSimpleGui/src/main.cpp
@@ -1,10 +1,28 @@
 #include "SimpleGuiApp.h"
 #include "MpMainWindow.h"
 
+#include <QMessageBox>
+
 int main(int argc, char** argv)
 {
   SimpleGuiApp app(argc, argv);
-  mpMainWindow window;
-  window.show();
-  return app.exec();
+  try
+  {
+    mpMainWindow window;
+    window.show();
+    return app.exec();
+  }
+  catch(std::exception& e)
+  {
+    QMessageBox::critical(0, "VatesSimpleGui - Error",
+                          QString("An unhandled exception has been caught. "\
+                                  "VatesSimpleGui will have to close. "\
+                                  "Details:\n\n")+e.what());
+  }
+  catch(...)
+  {
+    QMessageBox::critical(0, "VatesSimpleGui - Error",
+                          "An unhandled exception has been caught. "\
+                          "VatesSimpleGui will have to close.");
+  }
 }
