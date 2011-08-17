e4c17ad2968ce1c80b70c5cf5260a75c4b0cb8a8
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2011-08-17 10:26:38 +0000

    There seems to be a problem with precompiled headers on the 32-bit windows build. re #3487

--- Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/PrecompiledHeader.h
+++ Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/PrecompiledHeader.h
@@ -1,10 +1,8 @@
 #ifndef MANTID_CURVEFITTING_PRECOMPILEDHEADER_H_
 #define MANTID_CURVEFITTING_PRECOMPILEDHEADER_H_
 
-#include "MantidAPI/Algorithm.h"
-#include "MantidAPI/IFitFunction.h"
-#include "MantidAPI/IFunctionMW.h"
-#include "MantidAPI/IPeakFunction.h"
-#include "MantidCurveFitting/BackgroundFunction.h"
+#include <vector>
+#include <string>
+#include <set>
 
 #endif //MANTID_CURVEFITTING_PRECOMPILEDHEADER_H_
