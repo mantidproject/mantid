313055f5584477435c302e86f02affa36b0927e0
Wenduo Zhou <zhouw@ornl.gov>
2011-08-22 18:32:33 +0000

    Add class EditTOFPowderDiffractomerGeometry.  Refs #3599.

---
+++ Code/Mantid/Framework/Algorithms/src/EditTOFPowderDiffractomerGeometry.cpp
@@ -0,0 +1,33 @@
+#include "MantidAlgorithms/EditTOFPowderDiffractomerGeometry.h"
+#include "MantidKernel/System.h"
+
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+
+  //----------------------------------------------------------------------------------------------
+  /** Constructor
+   */
+  EditTOFPowderDiffractomerGeometry::EditTOFPowderDiffractomerGeometry()
+  {
+    // TODO Auto-generated constructor stub
+  }
+    
+  //----------------------------------------------------------------------------------------------
+  /** Destructor
+   */
+  EditTOFPowderDiffractomerGeometry::~EditTOFPowderDiffractomerGeometry()
+  {
+    // TODO Auto-generated destructor stub
+  }
+  
+
+
+} // namespace Mantid
+} // namespace Algorithms
+
---
+++ Code/Mantid/Framework/Algorithms/test/EditTOFPowderDiffractomerGeometryTest.h
@@ -0,0 +1,29 @@
+#ifndef MANTID_ALGORITHMS_EDITTOFPOWDERDIFFRACTOMERGEOMETRYTEST_H_
+#define MANTID_ALGORITHMS_EDITTOFPOWDERDIFFRACTOMERGEOMETRYTEST_H_
+
+#include <cxxtest/TestSuite.h>
+#include "MantidKernel/Timer.h"
+#include "MantidKernel/System.h"
+#include <iostream>
+#include <iomanip>
+
+#include "MantidAlgorithms/EditTOFPowderDiffractomerGeometry.h"
+
+using namespace Mantid;
+using namespace Mantid::Algorithms;
+using namespace Mantid::API;
+
+class EditTOFPowderDiffractomerGeometryTest : public CxxTest::TestSuite
+{
+public:
+
+  void test_Something()
+  {
+  }
+
+
+};
+
+
+#endif /* MANTID_ALGORITHMS_EDITTOFPOWDERDIFFRACTOMERGEOMETRYTEST_H_ */
+
