cc8964ca1436e6fcdb5a72af369f4049b8bab5b7
Dennis Mikkelson <mikkelsond@uwstout.edu>
2011-08-25 15:56:19 +0000

    Changed name of FindUB_* algorithms to FindUB* at Nick's request. refs #3577 #3578 #3579

---
+++ Code/Mantid/Framework/Crystal/inc/MantidCrystal/FindUBUsingIndexedPeaks.h
@@ -0,0 +1,77 @@
+#ifndef MANTID_CRYSTAL_FIND_UB_USING_INDEXED_PEAKS_H_
+#define MANTID_CRYSTAL_FIND_UB_USING_INDEXED_PEAKS_H_
+    
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h" 
+#include "MantidKernel/Logger.h"
+
+namespace Mantid
+{
+namespace Crystal
+{
+/** FindUBUsingIndexedPeaks : Algorithm to calculate a UB matrix,
+    given a list of peaks that have already been indexed by some means.
+    
+    @author Dennis Mikkelson(adapted from Andrei Savici's CalculateUMatrix)
+    @date   2011-08-17
+
+    Copyright &copy; 2011 ISIS Rutherford Appleton Laboratory & 
+                     NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: 
+    <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+  */
+  class DLLExport FindUBUsingIndexedPeaks : public API::Algorithm
+  {
+  public:
+    FindUBUsingIndexedPeaks();
+    ~FindUBUsingIndexedPeaks();
+    
+    /// Algorithm's name for identification 
+    virtual const std::string name() const 
+            { return "FindUBUsingIndexedPeaks";};
+
+    /// Algorithm's version for identification 
+    virtual int version() const 
+            { return 1;};
+
+    /// Algorithm's category for identification
+    virtual const std::string category() const 
+            { return "Crystal";}
+    
+  private:
+
+    /// Sets documentation strings for this algorithm
+    virtual void initDocs();
+
+    /// Initialise the properties
+    void init();
+
+    /// Run the algorithm
+    void exec();
+
+    /// Static reference to the logger class
+        static Kernel::Logger& g_log;
+  };
+
+
+} // namespace Crystal
+} // namespace Mantid
+
+#endif  /* MANTID_CRYSTAL_FIND_UB_USING_INDEXED_PEAKS */
---
+++ Code/Mantid/Framework/Crystal/src/FindUBUsingIndexedPeaks.cpp
@@ -0,0 +1,152 @@
+#include "MantidCrystal/FindUBUsingIndexedPeaks.h"
+#include "MantidKernel/System.h"
+#include "MantidDataObjects/PeaksWorkspace.h"
+#include "MantidDataObjects/Peak.h"
+#include "MantidGeometry/Crystal/IndexingUtils.h"
+#include "MantidGeometry/Crystal/OrientedLattice.h"
+
+namespace Mantid
+{
+namespace Crystal
+{
+  Kernel::Logger& FindUBUsingIndexedPeaks::g_log = 
+                        Kernel::Logger::get("FindUBUsingIndexedPeaks");
+
+  // Register the algorithm into the AlgorithmFactory
+  DECLARE_ALGORITHM(FindUBUsingIndexedPeaks)
+
+  using namespace Mantid::Kernel;
+  using namespace Mantid::API;
+  using namespace Mantid::DataObjects;
+  using namespace Mantid::Geometry;
+
+#define round(x) ((x)>=0?(int)((x)+0.5):(int)((x)-0.5))
+
+  //--------------------------------------------------------------------------
+  /** Constructor
+   */
+  FindUBUsingIndexedPeaks::FindUBUsingIndexedPeaks()
+  {
+  }
+    
+  //--------------------------------------------------------------------------
+  /** Destructor
+   */
+  FindUBUsingIndexedPeaks::~FindUBUsingIndexedPeaks()
+  {
+  }
+  
+
+  //--------------------------------------------------------------------------
+  /// Sets documentation strings for this algorithm
+  void FindUBUsingIndexedPeaks::initDocs()
+  {
+    this->setWikiSummary("Calculate the UB matrix from a peaks workspace in which (h,k,l) indices have already been set on at least three linearly independent Q vectors.");
+    this->setOptionalMessage("Calculate the UB matrix from a peaks workspace, containing indexed peaks.");
+    this->setWikiDescription("Given a set of peaks at least three of which have been assigned Miller indices, this algorithm will find the UB matrix, that best fits maps the integer (h,k,l) values to the corresponding Q vectors.  The set of indexed peaks must include three linearly independent Q vectors.  The (h,k,l) values from the peaks are first rounded to form integer (h,k,l) values.  The algorithm then forms a possibly over-determined linear system of equations representing the mapping from (h,k,l) to Q for each indexed peak.  The system of linear equations is then solved in the least squares sense, using QR factorization.");
+  }
+
+  //--------------------------------------------------------------------------
+  /** Initialize the algorithm's properties.
+   */
+  void FindUBUsingIndexedPeaks::init()
+  {
+    this->declareProperty(new WorkspaceProperty<PeaksWorkspace>(
+          "PeaksWorkspace","",Direction::InOut), "Input Peaks Workspace");
+  }
+
+  //--------------------------------------------------------------------------
+  /** Execute the algorithm.
+   */
+  void FindUBUsingIndexedPeaks::exec()
+  {
+    PeaksWorkspace_sptr ws;
+    ws = boost::dynamic_pointer_cast<PeaksWorkspace>(
+         AnalysisDataService::Instance().retrieve(this->getProperty("PeaksWorkspace")) );
+
+    if (!ws)
+    {
+      throw std::runtime_error("Could not read the peaks workspace");
+    }
+
+    std::vector<Peak> peaks = ws->getPeaks();
+    size_t n_peaks = ws->getNumberPeaks();
+
+    std::vector<V3D>  q_vectors;
+    std::vector<V3D>  hkl_vectors;
+
+    q_vectors.reserve( n_peaks );
+    hkl_vectors.reserve( n_peaks );
+
+    size_t indexed_count = 0;
+    for ( size_t i = 0; i < n_peaks; i++ )
+    {
+      V3D hkl( peaks[i].getH(), peaks[i].getK(), peaks[i].getL() );    // ##### KEEP
+      if ( IndexingUtils::ValidIndex( hkl, 1.0 ) )    // use tolerance == 1 to 
+                                                      // just check for (0,0,0) 
+      {
+        q_vectors.push_back( peaks[i].getQSampleFrame() );
+        V3D miller_ind( round(hkl[0]), round(hkl[1]), round(hkl[2]) );
+        hkl_vectors.push_back( V3D(miller_ind) );
+        indexed_count++;
+      }
+    }
+
+    if ( indexed_count < 3 ) 
+    { 
+      throw std::runtime_error(
+            "At least three linearly independent indexed peaks are needed.");
+    }
+
+    Matrix<double> UB(3,3,false);
+    double error = IndexingUtils::Optimize_UB( UB, hkl_vectors, q_vectors );
+
+    std::cout << "Error = " << error << std::endl;
+    std::cout << "UB = " << UB << std::endl;
+
+    char logInfo[200];
+    if ( ! IndexingUtils::CheckUB( UB ) ) // UB not found correctly
+    {
+      g_log.notice( std::string(
+         "Found Invalid UB...peaks used might not be linearly independent") );
+      g_log.notice( std::string(
+         "UB NOT SAVED.") );
+    }
+    else                                 // tell user how many would be indexed
+    {                                    // from the full list of peaks, and  
+      q_vectors.clear();                 // save the UB in the sample
+      q_vectors.reserve( n_peaks );
+      for ( size_t i = 0; i < n_peaks; i++ )
+      {
+        q_vectors.push_back( peaks[i].getQSampleFrame() );
+      }
+      double tolerance = 0.1;
+      int num_indexed = IndexingUtils::NumberIndexed(UB, q_vectors, tolerance);
+
+      sprintf( logInfo,
+               std::string("New UB will index %1d Peaks out of %1d with tolerance %5.3f").c_str(),
+               num_indexed, n_peaks, tolerance);
+      g_log.notice( std::string(logInfo) );
+
+      OrientedLattice o_lattice;
+      o_lattice.setUB( UB );
+      double calc_a = o_lattice.a();
+      double calc_b = o_lattice.b();
+      double calc_c = o_lattice.c();
+      double calc_alpha = o_lattice.alpha();
+      double calc_beta  = o_lattice.beta();
+      double calc_gamma = o_lattice.gamma();
+                                       // Show the modified lattice parameters
+      sprintf( logInfo, 
+               std::string("Lattice Parameters: %8.3f %8.3f %8.3f %8.3f %8.3f %8.3f").c_str(),
+               calc_a, calc_b, calc_c, calc_alpha, calc_beta, calc_gamma);
+      g_log.notice( std::string(logInfo) );
+
+      ws->mutableSample().setOrientedLattice( new OrientedLattice(o_lattice) );
+    }
+  }
+
+
+} // namespace Mantid
+} // namespace Crystal
+
