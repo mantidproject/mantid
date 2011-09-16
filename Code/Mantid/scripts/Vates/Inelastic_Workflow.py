7a7806290e434c0d3aaa1d02ca5dc681db74dd7f
Owen Arnold <owen.arnold@stfc.ac.uk>
2011-09-16 16:43:44 +0000

    refs #3722. Simple script exercising aspects of the Inelastic workflow that we have working.

---
+++ Code/Mantid/scripts/Vates/Inelastic_Workflow.py
@@ -0,0 +1,26 @@
+#Common names
+filename = 'fe_demo_30.sqw'
+ws_in ='fe_demo_30' 
+
+#Load an SQW file and internally convert to a Multidimensional event workspace (MDEW)
+if not mtd.workspaceExists(ws_in):
+ LoadSQW(filename, OutputWorkspace=ws_in)
+
+#Bin the workspace in an axis aligned manner. Creates a Histogrammed MD workspace.
+BinToMDHistoWorkspace(InputWorkspace=ws_in,OutputWorkspace='binned_axis_aligned',AxisAligned=True,
+AlignedDimX='Q_\\zeta,-1.5,5,100',
+AlignedDimY='Q_\\xi,-6,6,100',
+AlignedDimZ='Q_\\eta,-6,6,100',
+AlignedDimT='E,0,150,30')
+
+#Bin the workpace using a coordinate transformation to rotate the output.. Creates a Histogrammed MD workspace.
+BinToMDHistoWorkspace(InputWorkspace=ws_in,OutputWorkspace='binned_rotated',AxisAligned=False,
+BasisVectorX='Qx,Ang,1,0.5,0,0,1,100',
+BasisVectorY='Qy,Ang,-0.5,1,0,0,1,100',
+BasisVectorZ='Qz,Ang,0,0,1.25,0,1,100',
+Origin='0,0,0,0')
+
+#Save the MDEW workspace in the MDEW nexus format.
+SaveMD(ws_in, Filename='MDEW_fe_demo_30.nxs')
+
+#Could reload the MDEW at this point.
