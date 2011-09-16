18f4e7afb227750d7bb43e36e7b123c85fb7b55d
Jean Bilheux <bilheuxjm@ornl.gov>
2011-09-16 16:56:44 +0000

    Implemented the Q widgets of the Settings tab. This refs #3570.

--- Code/Mantid/scripts/Interface/ui/reflectometer/ui_data_refl.py
+++ Code/Mantid/scripts/Interface/ui/reflectometer/ui_data_refl.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/reflectometer/data_refl.ui'
 #
-# Created: Thu Sep 15 13:54:08 2011
+# Created: Fri Sep 16 07:48:58 2011
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
--- Code/Mantid/scripts/Interface/ui/ui_instrument_dialog.py
+++ Code/Mantid/scripts/Interface/ui/ui_instrument_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/instrument_dialog.ui'
 #
-# Created: Thu Sep 15 13:54:08 2011
+# Created: Fri Sep 16 07:48:57 2011
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
