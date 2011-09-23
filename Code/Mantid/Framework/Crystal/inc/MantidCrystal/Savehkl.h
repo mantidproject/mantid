efb3be11e9e617aef2405e9e905f37604b703b75
Vickie Lynch <lynchve@ornl.gov>
2011-09-23 19:33:05 +0000

    Refs #3497 Savehkl output for single crystal data

---
+++ Code/Mantid/Framework/Crystal/inc/MantidCrystal/Savehkl.h
@@ -0,0 +1,76 @@
+#ifndef MANTID_CRYSTAL_SAVEHKL_H_
+#define MANTID_CRYSTAL_SAVEHKL_H_
+/*WIKI* 
+
+Save a PeaksWorkspace to a ISAW-style ASCII .peaks file.
+*WIKI*/
+    
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h" 
+
+namespace Mantid
+{
+namespace Crystal
+{
+
+  /** Save a PeaksWorkspace to a Gsas-style ASCII .hkl file.
+   * 
+   * @author Vickie Lynch, SNS
+   * @date 2011-09-28
+   */
+  const double pc[4][19] =
+  {    {0.9369, 0.9490, 0.9778, 1.0083, 1.0295, 1.0389, 1.0392, 1.0338,
+        1.0261, 1.0180, 1.0107, 1.0046, 0.9997, 0.9957, 0.9929, 0.9909,
+        0.9896, 0.9888, 0.9886},
+       {2.1217, 2.0149, 1.7559, 1.4739, 1.2669, 1.1606, 1.1382, 1.1724,
+        1.2328, 1.3032, 1.3706, 1.4300, 1.4804, 1.5213, 1.5524, 1.5755,
+        1.5913, 1.6005, 1.6033},
+       {-0.1304, 0.0423, 0.4664, 0.9427, 1.3112, 1.5201, 1.5844, 1.5411,
+        1.4370, 1.2998, 1.1543, 1.0131, 0.8820, 0.7670, 0.6712, 0.5951,
+        0.5398, 0.5063, 0.4955},
+       {1.1717, 1.0872, 0.8715, 0.6068, 0.3643, 0.1757, 0.0446, -0.0375,
+       -0.0853, -0.1088, -0.1176, -0.1177, -0.1123, -0.1051, -0.0978,
+       -0.0914, -0.0868, -0.0840, -0.0833}};
+  const double MAX_WAVELENGTH = 50.0;    // max in lamda_weight table
+
+  const double STEPS_PER_ANGSTROM = 100;  // resolution of lamda table
+
+  const int NUM_WAVELENGTHS = std::ceil( MAX_WAVELENGTH * STEPS_PER_ANGSTROM);
+
+  const double radtodeg_half = 180.0/M_PI/2.;
+
+  class DLLExport Savehkl  : public API::Algorithm
+  {
+  public:
+    Savehkl();
+    ~Savehkl();
+    
+    /// Algorithm's name for identification 
+    virtual const std::string name() const { return "Savehkl";};
+    /// Algorithm's version for identification 
+    virtual int version() const { return 1;};
+    /// Algorithm's category for identification
+    virtual const std::string category() const { return "Crystal";}
+    
+  private:
+    /// Sets documentation strings for this algorithm
+    virtual void initDocs();
+    /// Initialise the properties
+    void init();
+    /// Run the algorithm
+    void exec();
+
+    double absor_sphere(double& twoth, double& wl, double& tbar) ;
+    double smu; // in 1/cm
+    double amu; // in 1/cm
+    double radius; // in cm
+
+
+
+  };
+
+
+} // namespace Mantid
+} // namespace Crystal
+
+#endif  /* MANTID_CRYSTAL_SAVEHKL_H_ */
---
+++ Code/Mantid/Framework/Crystal/src/Savehkl.cpp
@@ -0,0 +1,305 @@
+#include "MantidAPI/FileProperty.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidCrystal/Savehkl.h"
+#include "MantidDataObjects/Peak.h"
+#include "MantidDataObjects/PeaksWorkspace.h"
+#include "MantidGeometry/Instrument/RectangularDetector.h"
+#include "MantidKernel/Strings.h"
+#include "MantidKernel/System.h"
+#include "MantidKernel/Utils.h"
+#include "MantidKernel/V3D.h"
+#include <fstream>
+
+using namespace Mantid::Geometry;
+using namespace Mantid::DataObjects;
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+namespace Mantid
+{
+namespace Crystal
+{
+
+  // Register the algorithm into the AlgorithmFactory
+  DECLARE_ALGORITHM(Savehkl)
+
+
+  //----------------------------------------------------------------------------------------------
+  /** Constructor
+   */
+  Savehkl::Savehkl()
+  {
+  }
+    
+  //----------------------------------------------------------------------------------------------
+  /** Destructor
+   */
+  Savehkl::~Savehkl()
+  {
+  }
+  
+
+  //----------------------------------------------------------------------------------------------
+  /// Sets documentation strings for this algorithm
+  void Savehkl::initDocs()
+  {
+    this->setWikiSummary("Save a PeaksWorkspace to a ASCII .hkl file.");
+    this->setOptionalMessage("Save a PeaksWorkspace to a ASCII .hkl file.");
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Initialize the algorithm's properties.
+   */
+  void Savehkl::init()
+  {
+    declareProperty(new WorkspaceProperty<PeaksWorkspace>("InputWorkspace","",Direction::Input, new InstrumentValidator<PeaksWorkspace>()),
+        "An input PeaksWorkspace with an instrument.");
+
+    BoundedValidator<double> *mustBePositive = new BoundedValidator<double> ();
+    mustBePositive->setLower(0.0);
+    declareProperty("LinearScatteringCoef", -1.0, mustBePositive,
+      "Linear scattering coefficient in 1/cm");
+    declareProperty("LinearAbsorptionCoef", -1.0, mustBePositive->clone(),
+      "Linear absorption coefficient at 1.8 Angstroms in 1/cm");
+    declareProperty("Radius", -1.0, mustBePositive->clone(),
+      "Radius of the sample in centimeters");
+
+    declareProperty("AppendFile", false, "Append to file if true.\n"
+      "If false, new file (default).");
+
+    std::vector<std::string> exts;
+    exts.push_back(".hkl");
+
+    declareProperty(new FileProperty("Filename", "", FileProperty::Save, exts),
+        "Path to an hkl file to save.");
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Execute the algorithm.
+   */
+  void Savehkl::exec()
+  {
+
+    std::string filename = getPropertyValue("Filename");
+    PeaksWorkspace_sptr ws = getProperty("InputWorkspace");
+    smu = getProperty("LinearScatteringCoef"); // in 1/cm
+    amu = getProperty("LinearAbsorptionCoef"); // in 1/cm
+    radius = getProperty("Radius"); // in cm
+
+    std::vector<Peak> peaks = ws->getPeaks();
+
+    int runcount = 1;
+
+    std::fstream out;
+    bool append = getProperty("AppendFile");
+    if (append)
+    {
+      out.open( filename.c_str(), std::ios::in|std::ios::out|std::ios::ate);
+      long pos = out.tellp();
+      out.seekp (pos - 73);
+      runcount ++;
+    }
+    else
+    {
+      out.open( filename.c_str(), std::ios::out);
+    }
+
+    Instrument_const_sptr inst = ws->getInstrument();
+    if (!inst) throw std::runtime_error("No instrument in PeaksWorkspace. Cannot save peaks file.");
+
+    double l1; V3D beamline; double beamline_norm; V3D samplePos;
+    inst->getInstrumentParameters(l1, beamline, beamline_norm, samplePos);
+
+
+    // We must sort the peaks first by run, then bank #, and save the list of workspace indices of it
+    typedef std::map<int, std::vector<size_t> > bankMap_t;
+    typedef std::map<int, bankMap_t> runMap_t;
+    std::set<int> uniqueBanks;
+    runMap_t runMap;
+
+    for (size_t i=0; i < peaks.size(); ++i)
+    {
+      Peak & p = peaks[i];
+      int run = p.getRunNumber();
+      int bank = 0;
+      std::string bankName = p.getBankName();
+      if (bankName.size() <= 4)
+      {
+        g_log.information() << "Could not interpret bank number of peak " << i << "(" << bankName << ")\n";
+        continue;
+      }
+      // Take out the "bank" part of the bank name and convert to an int
+      bankName = bankName.substr(4, bankName.size()-4);
+      Strings::convert(bankName, bank);
+
+      // Save in the map
+      runMap[run][bank].push_back(i);
+      // Track unique bank numbers
+      uniqueBanks.insert(bank);
+    }
+
+
+
+    // ============================== Save all Peaks =========================================
+    // Sequence number
+    int seqNum = 1;
+
+    // Go in order of run numbers
+    runMap_t::iterator runMap_it;
+    for (runMap_it = runMap.begin(); runMap_it != runMap.end(); runMap_it++)
+    {
+      // Start of a new run
+      int run = runMap_it->first;
+      bankMap_t & bankMap = runMap_it->second;
+
+      bankMap_t::iterator bankMap_it;
+      for (bankMap_it = bankMap.begin(); bankMap_it != bankMap.end(); bankMap_it++)
+      {
+        // Start of a new bank.
+        int bank = bankMap_it->first;
+        std::vector<size_t> & ids = bankMap_it->second;
+
+        if (ids.size() > 0)
+        {
+          // Go through each peak at this run / bank
+          for (size_t i=0; i < ids.size(); i++)
+          {
+            size_t wi = ids[i];
+            Peak & p = peaks[wi];
+            if (p.getIntensity() == 0.0) continue;
+
+            double tbar = 0;
+            // This is the scattered beam direction
+            V3D dir = p.getDetPos() - inst->getSample()->getPos();
+
+            // Two-theta = polar angle = scattering angle = between +Z vector and the scattered beam
+            double scattering = dir.angle( V3D(0.0, 0.0, 1.0) );
+            double lambda =  p.getWavelength();
+            double transmission = absor_sphere(scattering, lambda, tbar);
+
+            // Anvred write from Art Schultz
+            //hklFile.write('%4d%4d%4d%8.2f%8.2f%4d%8.4f%7.4f%7d%7d%7.4f%4d%9.5f%9.4f\n' 
+            //    % (H, K, L, FSQ, SIGFSQ, hstnum, WL, TBAR, CURHST, SEQNUM, TRANSMISSION, DN, TWOTH, DSP))
+            // HKL is flipped by -1 due to different q convention in ISAW vs mantid.
+            out <<  std::setw( 4 ) << Utils::round(-p.getH())
+                <<  std::setw( 4 ) << Utils::round(-p.getK())
+                <<  std::setw( 4 ) << Utils::round(-p.getL());
+
+            out << std::setw( 8 ) <<  std::fixed << std::setprecision( 2 ) << p.getIntensity();
+
+            out << std::setw( 8 ) << std::fixed << std::setprecision( 2 ) << p.getSigmaIntensity();
+
+            out << std::setw( 4 ) << runcount;
+
+            out << std::setw( 8 ) << std::fixed << std::setprecision( 4 ) << lambda;
+
+            out << std::setw( 7 ) <<  std::fixed << std::setprecision( 4 ) << tbar;
+
+            out <<  std::setw( 7 ) <<  run;
+
+            out <<  std::setw( 7 ) <<  seqNum;
+
+            out << std::setw( 7 ) <<  std::fixed << std::setprecision( 4 ) << transmission;
+
+            out <<  std::setw( 4 ) << std::right <<  bank;
+
+            out << std::setw( 9 ) << std::fixed << std::setprecision( 5 )
+              << scattering; //two-theta scattering
+
+            out << std::setw( 9 ) << std::fixed << std::setprecision( 4 )
+              << p.getDSpacing();
+
+            out << std::endl;
+
+            // Count the sequence
+            seqNum++;
+          }
+        }
+      }
+    }
+    out << "   0   0   0    0.00    0.00   0  0.0000 0.0000      0      0 0.0000   0";
+    out << std::endl;
+
+    out.flush();
+    out.close();
+
+
+  }
+ /**
+  *       function to calculate a spherical absorption correction
+  *       and tbar. based on values in:
+  *
+  *       c. w. dwiggins, jr., acta cryst. a31, 395 (1975).
+  *
+  *       in this paper, a is the transmission and a* = 1/a is
+  *       the absorption correction.
+  *
+  *       input are the smu (scattering) and amu (absorption at 1.8 ang.)
+  *       linear absorption coefficients, the radius r of the sample
+  *       the theta angle and wavelength.
+  *       the absorption (absn) and tbar are returned.
+  *
+  *       a. j. schultz, june, 2008
+  */
+  double Savehkl::absor_sphere(double& twoth, double& wl, double& tbar)
+  {
+    int i;
+    double mu, mur;         //mu is the linear absorption coefficient,
+                            //r is the radius of the spherical sample.
+    double theta,astar1,astar2,frac,astar;
+    double trans;
+
+//  For each of the 19 theta values in dwiggins (theta = 0.0 to 90.0
+//  in steps of 5.0 deg.), the astar values vs.mur were fit to a third
+//  order polynomial in excel. these values are given in the static array
+//  pc[][]
+
+    mu = smu + (amu/1.8f)*wl;
+
+    mur = mu*radius;
+    if (mur < 0. || mur > 2.5)
+    {
+      std::ostringstream s;
+      s << mur;
+      throw std::runtime_error("muR is not in range of Dwiggins' table :" + s.str());
+    }
+
+    theta = twoth*radtodeg_half;
+    if (theta < 0. || theta > 90.)
+    {
+      std::ostringstream s;
+      s << theta;
+      throw std::runtime_error("theta is not in range of Dwiggins' table :" + s.str());
+    }
+
+//  using the polymial coefficients, calulate astar (= 1/transmission) at
+//  theta values below and above the actual theta value.
+
+    i = (int)(theta/5.);
+    astar1 = pc[0][i] + mur * (pc[1][i] + mur * (pc[2][i] + pc[3][i] * mur));
+
+    i = i+1;
+    astar2 = pc[0][i] + mur * (pc[1][i] + mur * (pc[2][i] + pc[3][i] * mur));
+
+//  do a linear interpolation between theta values.
+
+    frac = theta - static_cast<double>(static_cast<int>( theta / 5. )) * 5.;//theta%5.
+    frac = frac/5.;
+
+    astar = astar1*(1-frac) + astar2*frac;       // astar is the correction
+    trans = 1.f/astar;                           // trans is the transmission
+                                                 // trans = exp(-mu*tbar)
+
+//  calculate tbar as defined by coppens.
+    tbar = -(double)std::log(trans)/mu;
+
+    return trans;
+  }
+
+
+
+
+
+} // namespace Mantid
+} // namespace Crystal
+
