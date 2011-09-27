1351bd3ad29bfd9407f2423348faed08be185620
Mathieu Doucet <doucetm@ornl.gov>
2011-09-27 16:00:17 +0000

    Deprecate LoadSNSNexus. Re #3227

--- Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/LoadSNSNexus.h
+++ Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/LoadSNSNexus.h
@@ -1,18 +1,11 @@
 #ifndef MANTID_DATAHANDLING_LOADSNSNEXUS_H_
 #define MANTID_DATAHANDLING_LOADSNSNEXUS_H_
-/*WIKI* 
-
-
-
-*WIKI*/
-
 //----------------------------------------------------------------------
 // Includes
 //----------------------------------------------------------------------
 #include "MantidAPI/Algorithm.h"
-#include "MantidDataObjects/Workspace2D.h"
-#include "MantidNexus/NexusClasses.h"
-#include "MantidAPI/IDataFileChecker.h"
+#include "MantidAPI/DeprecatedAlgorithm.h"
+#include "MantidDataHandling/LoadTOFRawNexus.h"
 #include <napi.h>
 #include <climits>
 
@@ -27,31 +20,10 @@ namespace Mantid
 {
 namespace DataHandling
 {
-
     /** @class LoadSNSNexus LoadSNSNexus.h NeXus/LoadSNSNexus.h
-
-    Loads a file in a Nexus format and stores it in a 2D workspace 
-    (Workspace2D class). LoadSNSNexus is an algorithm and as such inherits
-    from the Algorithm class, via DataHandlingCommand, and overrides
+    Loads a NeXus file that conforms to the TOFRaw instrument definition format and stores it in a 2D workspace.
+    LoadTOFRawNeXus is an algorithm and as such inherits  from the Algorithm class, via DataHandlingCommand, and overrides
     the init() & exec() methods.
-
-    Required Properties:
-    <UL>
-    <LI> Filename - The name of and path to the input Nexus file </LI>
-    <LI> OutputWorkspace - The name of the workspace in which to store the imported data 
-    (a multiperiod file will store higher periods in workspaces called OutputWorkspace_PeriodNo)
-    [ not yet implemented for Nexus ]</LI>
-    </UL>
-
-    Optional Properties: (note that these options are not available if reading a multiperiod file)
-    <UL>
-    <LI> spectrum_min  - The spectrum to start loading from</LI>
-    <LI> spectrum_max  - The spectrum to load to</LI>
-    <LI> spectrum_list - An ArrayProperty of spectra to load</LI>
-    </UL>
-
-    @author Roman Tolchenov, Tessella plc
-    @date 09/07/09
 
     Copyright &copy; 2007-9 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
 
@@ -73,7 +45,7 @@ namespace DataHandling
     File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>. 
     Code Documentation is available at: <http://doxygen.mantidproject.org>
     */
-    class DLLExport LoadSNSNexus : public API::IDataFileChecker
+    class DLLExport LoadSNSNexus : public LoadTOFRawNexus, public API::DeprecatedAlgorithm
     {
     public:
         /// Default constructor
@@ -81,53 +53,12 @@ namespace DataHandling
         /// Destructor
         ~LoadSNSNexus() {}
         /// Algorithm's name for identification overriding a virtual method
-        virtual const std::string name() const { return "LoadSNSNexus"; }
+        virtual const std::string name() const;
+
         /// Algorithm's version for identification overriding a virtual method
-        virtual int version() const { return 1; }
-        /// Algorithm's category for identification overriding a virtual method
-        virtual const std::string category() const { return "DataHandling"; }
-       /// do a quick check that this file can be loaded 
-      virtual bool quickFileCheck(const std::string& filePath,size_t nread,const file_header& header);
-      /// check the structure of the file and  return a value between 0 and 100 of how much this file can be loaded
-      virtual int fileCheck(const std::string& filePath);
+        virtual int version() const;
 
-    private:
-      /// Sets documentation strings for this algorithm
-      virtual void initDocs();
-        /// Overwrites Algorithm method.
-        void init();
-        /// Overwrites Algorithm method
-        void exec();
-
-        /// The l1
-        double m_L1;
-
-        /// Class for comparing bank names in format "bank123" according to the numeric part of the name
-        class CompareBanks
-        {
-        public:
-            /** Compare operator
-            *  @param s1 :: First argument
-            *  @param s2 :: Second argument
-            *  @return true if i1<i2
-            */
-            bool operator()(const std::string& s1, const std::string& s2)
-            {
-                int i1 = atoi( s1.substr(4,s1.size()-4).c_str() );
-                int i2 = atoi( s2.substr(4,s2.size()-4).c_str() );
-                return i1 < i2;
-            }
-        };
-
-        /// User input spectra selection
-        std::vector<int> getSpectraSelection();
-
-        /// Read in an entry (period).
-        API::Workspace_sptr loadEntry(Mantid::NeXus::NXEntry entry,int period, double progress_start, double progress_end);
-
-        /// Personal wrapper for sqrt to allow msvs to compile
-        static double dblSqrt(double in);
-
+        int fileCheck(const std::string& filePath);
     };
 
 } // namespace DataHandling
