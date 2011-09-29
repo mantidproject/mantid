4bbfba0f96fbada779b265780d5155da9518c8a0
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-09-29 20:57:48 +0000

    Refs #3882: A little more in the slice viewer, add to CMake

--- Code/Mantid/MantidPlot/src/Mantid/SliceViewer/SliceViewer.cpp
+++ Code/Mantid/MantidPlot/src/Mantid/SliceViewer/SliceViewer.cpp
@@ -1,12 +1,28 @@
 #include "SliceViewer.h"
+#include <qwt_plot_spectrogram.h>
 
 SliceViewer::SliceViewer(QWidget *parent)
     : QWidget(parent)
 {
 	ui.setupUi(this);
+
+	// Create the plot
+  m_spectLayout = new QVBoxLayout(ui.frmPlot);
+	m_plot = new QwtPlot();
+  m_spectLayout->addWidget(m_plot);
+
+	// Add a spectrograph
+	m_spect = new QwtPlotSpectrogram();
+	m_spect->attach(m_plot);
+
 }
 
 SliceViewer::~SliceViewer()
 {
 
+}
+
+
+void SliceViewer::initLayout()
+{
 }
--- Code/Mantid/MantidPlot/src/Mantid/SliceViewer/SliceViewer.h
+++ Code/Mantid/MantidPlot/src/Mantid/SliceViewer/SliceViewer.h
@@ -3,17 +3,34 @@
 
 #include <QtGui/QWidget>
 #include "ui_SliceViewer.h"
+#include <QtGui/qdialog.h>
+#include <qwt_plot_spectrogram.h>
+#include <qwt_plot.h>
 
 class SliceViewer : public QWidget
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    SliceViewer(QWidget *parent = 0);
-    ~SliceViewer();
+  SliceViewer(QWidget *parent = 0);
+  ~SliceViewer();
 
 private:
-    Ui::SliceViewerClass ui;
+  void initLayout();
+
+private:
+  /// Auto-generated UI controls.
+  Ui::SliceViewerClass ui;
+
+  /// Main plot object
+  QwtPlot * m_plot;
+
+  /// Spectrogram plot
+  QwtPlotSpectrogram * m_spect;
+
+  /// Layout containing the spectrogram
+  QVBoxLayout * m_spectLayout;
+
 };
 
 #endif // SLICEVIEWER_H
