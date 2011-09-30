195a703b48458379b99b9dad53e4038ad907226d
Wenduo Zhou <zhouw@ornl.gov>
2011-09-30 18:07:07 +0000

    Modify the algorithm to convert the offset for VULCAN's IDL code to Mantid's offset.  Refs #3696.

--- Code/Mantid/Framework/DataHandling/src/LoadDspacemap.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadDspacemap.cpp
@@ -11,6 +11,7 @@
 #include "MantidKernel/PhysicalConstants.h"
 #include "MantidKernel/System.h"
 #include "MantidKernel/UnitFactory.h"
+#include "MantidGeometry/IDetector.h"
 
 using namespace Mantid::Kernel;
 using namespace Mantid::API;
@@ -180,10 +181,13 @@ namespace DataHandling
 
   }
 
+  const double CONSTANT = (PhysicalConstants::h * 1e10) / (2.0 * PhysicalConstants::NeutronMass * 1e6);
+
   //-----------------------------------------------------------------------
   /**
    * Make a map of the conversion factors between tof and D-spacing
    * for all pixel IDs in a workspace.
+   * map vulcan should contain the module/module and stack/stack offset
    *
    * @param vulcan :: map between detector ID and vulcan correction factor.
    * @param offsetsWS :: OffsetsWorkspace to be filled.
@@ -192,9 +196,13 @@ namespace DataHandling
       Mantid::DataObjects::OffsetsWorkspace_sptr offsetsWS)
   {
     // Get a pointer to the instrument contained in the workspace
+    // At this point, instrument VULCAN has been created?
     Instrument_const_sptr instrument = offsetsWS->getInstrument();
 
-    //To get all the detector ID's
+    g_log.notice() << "Name of instrument = " << instrument->getName() << std::endl;
+    g_log.notice() << "Input map (dict):  size = " << vulcan.size() << std::endl;
+
+    // To get all the detector ID's
     detid2det_map allDetectors;
     instrument->getDetectors(allDetectors);
 
@@ -202,33 +210,142 @@ namespace DataHandling
     std::map<detid_t, bool> selects;
 
     detid2det_map::const_iterator it;
+    int numfinds = 0;
+    int nummisses = 0;
+    g_log.notice() << "Input number of detectors = " << allDetectors.size() << std::endl;
+
+    // Get detector information
+    double l1, beamline_norm;
+    Kernel::V3D beamline,samplePos;
+    instrument->getInstrumentParameters(l1,beamline,beamline_norm, samplePos);
+
+    /*** A survey of parent detector
+    std::map<detid_t, bool> parents;
+    for (it = allDetectors.begin(); it != allDetectors.end(); it++){
+      int32_t detid = it->first;
+
+      // def boost::shared_ptr<const Mantid::Geometry::IDetector> IDetector_const_sptr;
+
+      std::string parentname = it->second->getParent()->getComponentID()->getName();
+      g_log.notice() << "Name = " << parentname << std::endl;
+      // parents.insert(parentid, true);
+    }
+    ***/
+
+    /*** Here some special configuration for VULCAN is hard-coded here!
+     *   Including (1) Super-Parent Information
+     ***/
+    Kernel::V3D referencePos;
+    detid_t anydetinrefmodule = 21*1250+5;
+
+    std::map<detid_t, Geometry::IDetector_const_sptr>::iterator det_iter = allDetectors.find(anydetinrefmodule);
+
+    if (det_iter == allDetectors.end()){
+      throw std::invalid_argument("Any Detector ID is Instrument's detector");
+    }
+    referencePos = det_iter->second->getParent()->getPos();
+    double refl2 = referencePos.norm();
+    double halfcosTwoThetaRef = referencePos.scalar_prod(beamline)/(refl2*beamline_norm);
+    double sinThetaRef = sqrt(0.5-halfcosTwoThetaRef);
+    double difcRef = sinThetaRef*(l1+refl2)/CONSTANT;
+
+    // Loop over all detectors in instrument to find the offset
     for (it = allDetectors.begin(); it != allDetectors.end(); it++)
     {
       int detectorID = it->first;
       Geometry::IDetector_const_sptr det = it->second;
-
       double offset = 0.0;
 
       // Find the vulcan factor;
       double vulcan_factor = 0.0;
       std::map<detid_t,double>::const_iterator vulcan_iter = vulcan.find(detectorID);
-      if( vulcan_iter != vulcan.end() )
+      if( vulcan_iter != vulcan.end() ){
         vulcan_factor = vulcan_iter->second;
+        numfinds ++;
+      }
+
+      // g_log.notice() << "Selected Detector with ID = " << detectorID << "  ID2 = " << id2 << std::endl; proved to be same
+
+      double intermoduleoffset = 0;
+      double interstackoffset = 0;
+
+      detid_t intermoduleid = detid_t(detectorID/1250)*1250+1250-2;
+      vulcan_iter = vulcan.find(intermoduleid);
+      if (vulcan_iter == vulcan.end()){
+        g_log.error() << "Cannot find inter-module offset ID = " << intermoduleid << std::endl;
+      } else {
+        intermoduleoffset = vulcan_iter->second;
+      }
+
+      detid_t interstackid = detid_t(detectorID/1250)*1250+1250-1;
+      vulcan_iter = vulcan.find(interstackid);
+      if (vulcan_iter == vulcan.end()){
+        g_log.error() << "Cannot find inter-module offset ID = " << intermoduleid << std::endl;
+      } else {
+        interstackoffset = vulcan_iter->second;
+      }
+
+      /***  This is the previous way to correct upon DIFC[module center pixel]
       // The actual factor is 10^(-value_in_the_file)
       vulcan_factor = pow(10.0,-vulcan_factor);
-
       // At this point, tof_corrected = vulcan_factor * tof_input
       // So this is the offset
       offset = vulcan_factor - 1.0;
+      ***/
+
+      /*** New approach to correct based on DIFC of each pixel
+       *   Equation:  offset = DIFC^(pixel)/DIFC^(parent)*(1+vulcan_offset)-1
+       *   offset should be close to 0
+       ***/
+      // 1. calculate DIFC
+      Kernel::V3D detPos;
+      detPos = det->getPos();
+
+      // Now detPos will be set with respect to samplePos
+      detPos -= samplePos;
+      double l2 = detPos.norm();
+      double halfcosTwoTheta = detPos.scalar_prod(beamline) / (l2 * beamline_norm);
+      double sinTheta = sqrt(0.5 - halfcosTwoTheta);
+      double difc_pixel = sinTheta*(l1+l2)/CONSTANT;
+
+      // Kernel::V3D parentPos = det->getParent()->getPos();
+      // parentPos -= samplePos;
+      // double l2parent = parentPos.norm();
+      // double halfcosTwoThetaParent = parentPos.scalar_prod(beamline)/(l2 * beamline_norm);
+      // double sinThetaParent = sqrt(0.5 - halfcosTwoThetaParent);
+      // double difc_parent = sinThetaParent*(l1+l2parent)/CONSTANT;
+
+      /*** Offset Replicate Previous Result
+      offset = difc_pixel/difc_parent*(pow(10.0, -vulcan_factor))-1.0;
+      ***/
+
+      offset = difc_pixel/difcRef*(pow(10.0, -(vulcan_factor+intermoduleoffset+interstackoffset)))-1.0;
 
       // Save in the map
       try
       {
         offsetsWS->setValue(detectorID, offset);
+
+        if (intermoduleid != 27498 &&
+            intermoduleid != 28748 &&
+            intermoduleid != 29998 &&
+            intermoduleid != 33748 &&
+            intermoduleid != 34998 &&
+            intermoduleid != 36248){
+          g_log.error() << "Detector ID = " << detectorID << "  Inter-Module ID = " << intermoduleid << std::endl;
+          throw std::invalid_argument("Indexing error!");
+        }
+
       }
       catch (std::invalid_argument & )
-      {}
-    }
+      {
+        nummisses += 1;
+        g_log.notice() << "Misses Detector ID = " << detectorID << std::endl;
+      }
+    } // for
+
+    g_log.notice() << "Number of matched detectors =" << numfinds << std::endl;
+
   }
 
 
@@ -251,6 +368,7 @@ namespace DataHandling
     }
     vulcan.clear();
     std::string str;
+    int numentries = 0;
     while(getline(grFile,str))
     {
       if (str.empty() || str[0] == '#') continue;
@@ -259,7 +377,11 @@ namespace DataHandling
       double correction;
       istr >> udet >> correction;
       vulcan.insert(std::make_pair(udet,correction));
+      numentries ++;
     }
+
+    g_log.notice() << "Read Vulcan ASCII File:  # Entry = " << numentries << std::endl;
+
   }
 
   /** Structure of the vulcan binary file */
