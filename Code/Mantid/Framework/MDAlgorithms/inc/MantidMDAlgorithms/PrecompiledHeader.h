4cf3a6ea2135284b71e758a43ced15178e960ba4
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-10-05 15:03:30 +0000

    Refs #3907: Win precompiled headers again

--- Code/Mantid/Framework/MDAlgorithms/inc/MantidMDAlgorithms/PrecompiledHeader.h
+++ Code/Mantid/Framework/MDAlgorithms/inc/MantidMDAlgorithms/PrecompiledHeader.h
@@ -2,7 +2,6 @@
 #define MANTID_MDALGORITHMS_PRECOMPILEDHEADER_H_
 
 #include "MantidAPI/Algorithm.h"
-#include "MantidGeometry/MDGeometry/MDGeometryDescription.h"
 #include "MantidKernel/Exception.h"
 
 #endif // MANTID_MDALGORITHMS_PRECOMPILEDHEA
