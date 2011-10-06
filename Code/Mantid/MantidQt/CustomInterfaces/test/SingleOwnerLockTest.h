ec3a6190d53ceb2f6582b5b57e69f56cf4915f96
Owen Arnold <owen.arnold@stfc.ac.uk>
2011-10-06 11:59:45 +0000

    Test SingleOwnerLock. refs #3641

--- Code/Mantid/MantidQt/CustomInterfaces/test/SingleOwnerLockTest.h
+++ Code/Mantid/MantidQt/CustomInterfaces/test/SingleOwnerLockTest.h
@@ -2,12 +2,9 @@
 #define CUSTOM_INTERFACES_SINGLE_OWNER_LOCK_TEST_H_
 
 #include <cxxtest/TestSuite.h>
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
 
-//#include "MantidQtCustomInterfaces/WorkspaceMemento.h"
-//using namespace MantidQt::CustomInterfaces;
-//using namespace testing;
+#include "MantidQtCustomInterfaces/SingleOwnerLock.h"
+using namespace MantidQt::CustomInterfaces;
 
 class SingleOwnerLockTest : public CxxTest::TestSuite
 {
@@ -17,9 +14,66 @@ public:
 //=====================================================================================
 // Functional tests
 //=====================================================================================
-   void testsTODO()
+   void testLock()
    {
-     //TSM_ASSERT("No tests!", false); TODO
+     SingleOwnerLock lock("One");
+     TS_ASSERT_THROWS_NOTHING(lock.lock());
+     TS_ASSERT(lock.locked());
+   }
+
+   void testUnlock()
+   {
+     SingleOwnerLock lock("One");
+
+     TS_ASSERT_THROWS_NOTHING(lock.lock());
+     TS_ASSERT(lock.locked());
+     TSM_ASSERT("Should unlock", lock.unlock());
+     TS_ASSERT(!lock.locked());
+     TSM_ASSERT("Should already be unlocked", !lock.unlock());
+     TS_ASSERT(!lock.locked());
+   }
+   
+   //Test that many single owner locks can operatate independently
+   void testCreateThenFreeMany()
+   {
+     SingleOwnerLock a("a");
+     SingleOwnerLock b("b");
+     SingleOwnerLock c("c");
+
+     a.lock();
+     b.lock();
+     c.lock();
+
+     TS_ASSERT(a.locked());
+     TS_ASSERT(b.locked());
+     TS_ASSERT(c.locked());
+     TS_ASSERT(a.unlock());
+     TS_ASSERT(b.unlock());
+     TS_ASSERT(c.unlock());
+     TS_ASSERT(!a.locked());
+     TS_ASSERT(!b.locked());
+     TS_ASSERT(!c.locked());
+   }
+
+   void testLockSameTwiceThrows()
+   {
+     SingleOwnerLock a("One");
+     TS_ASSERT_THROWS_NOTHING(a.lock());
+     
+     //Now try to lock the same object (keyed by name)
+     SingleOwnerLock b("One");
+     TSM_ASSERT_THROWS("Was already locked. Should have thrown.", b.lock(), std::runtime_error);
+   }
+
+   void testFreeOnDestruction()
+   {
+     {
+       SingleOwnerLock a("One");
+       a.lock();
+     } //Now out of scope.
+
+     SingleOwnerLock b("One");
+     TSM_ASSERT_THROWS_NOTHING("First lock should have 'let go' on destruction.", b.lock());
    }
 };
 
