2e22bcad674c27614022c130419ea4849566118d
mathieu <doucetm@ornl.gov>
2011-10-24 17:25:02 -0400

    Adding the reduction setup code. Re #3988

---
+++ Code/Mantid/Framework/WorkflowAlgorithms/inc/MantidWorkflowAlgorithms/SetupHFIRReduction.h
@@ -0,0 +1,68 @@
+#ifndef MANTID_ALGORITHMS_SETUPHFIRREDUCTION_H_
+#define MANTID_ALGORITHMS_SETUPHFIRREDUCTION_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidAPI/MatrixWorkspace.h"
+
+namespace Mantid
+{
+namespace WorkflowAlgorithms
+{
+/**
+
+    Set up the reduction options for HFIR reduction.
+
+    Copyright &copy; 2011 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+
+class DLLExport SetupHFIRReduction : public API::Algorithm
+{
+public:
+  /// Constructor
+  SetupHFIRReduction() : API::Algorithm() {}
+  /// Virtual destructor
+  virtual ~SetupHFIRReduction() {}
+  /// Algorithm's name
+  virtual const std::string name() const { return "SetupHFIRReduction"; }
+  /// Algorithm's version
+  virtual int version() const { return (1); }
+  /// Algorithm's category for identification
+  virtual const std::string category() const { return "Workflow\\SANS"; }
+
+private:
+
+  /// Sets documentation strings for this algorithm
+  virtual void initDocs();
+  /// Initialisation code
+  void init();
+  /// Execution code
+  void exec();
+
+};
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_SETUPHFIRREDUCTION_H_*/
---
+++ Code/Mantid/Framework/WorkflowAlgorithms/src/SetupHFIRReduction.cpp
@@ -0,0 +1,57 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidWorkflowAlgorithms/SetupHFIRReduction.h"
+#include "MantidDataObjects/TableWorkspace.h"
+#include "MantidWorkflowAlgorithms/ReductionTableHandler.h"
+
+namespace Mantid
+{
+namespace WorkflowAlgorithms
+{
+
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM(SetupHFIRReduction)
+
+/// Sets documentation strings for this algorithm
+void SetupHFIRReduction::initDocs()
+{
+  this->setWikiSummary("Set up HFIR SANS reduction options.");
+  this->setOptionalMessage("Set up HFIR SANS reduction options.");
+}
+
+using namespace Kernel;
+using namespace API;
+using namespace Geometry;
+using namespace DataObjects;
+
+void SetupHFIRReduction::init()
+{
+  declareProperty("OutputMessage","",Direction::Output);
+  declareProperty(new WorkspaceProperty<TableWorkspace>("ReductionTableWorkspace","", Direction::Output, true));
+}
+
+void SetupHFIRReduction::exec()
+{
+  TableWorkspace_sptr reductionTable = getProperty("ReductionTableWorkspace");
+  const std::string reductionTableName = getPropertyValue("ReductionTableWorkspace");
+
+  ReductionTableHandler reductionHandler(reductionTable);
+  if (!reductionTable && reductionTableName.size()>0)
+    setProperty("ReductionTableWorkspace", reductionHandler.getTable());
+
+  // Store name of the instrument
+  reductionHandler.addEntry("InstrumentName", "BIOSANS", true);
+
+  // Store dark current algorithm algorithm
+  IAlgorithm_sptr darkAlg = createSubAlgorithm("HFIRDarkCurrentSubtraction");
+  darkAlg->setProperty("OutputDarkCurrentWorkspace", "");
+  darkAlg->setPropertyValue("ReductionTableWorkspace", reductionTableName);
+  reductionHandler.addEntry("DefaultDarkCurrentAlgorithm", darkAlg->toString());
+
+  setPropertyValue("OutputMessage", "HFIR reduction options set");
+}
+
+} // namespace WorkflowAlgorithms
+} // namespace Mantid
+
