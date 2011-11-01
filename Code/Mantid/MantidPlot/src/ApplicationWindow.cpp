f8738887de4d1c5f1df71f56e3a1f05cf24640c6
Robert Whitley <robert.whitley@stfc.ac.uk>
2011-11-01 15:51:52 +0000

    Refs #3806. Fixed the bug that only allowed for one fitting with a specified curve type.

--- Code/Mantid/MantidPlot/src/ApplicationWindow.cpp
+++ Code/Mantid/MantidPlot/src/ApplicationWindow.cpp
@@ -16435,21 +16435,24 @@ void ApplicationWindow::setPlotType(const QString & plotDetails)
                 int curveNum(-1);
 
                 if (plotDetailsList[1] == "Data")
+                {
                   curveNum = g->curveIndex(plotDetailsList[2]); //workspaceName
+                }
                 else if (plotDetailsList[1] == "Fit")
-                  curveNum = g->curveIndex(plotDetailsList[2] + "-sp-1-Calc"); //workspaceName+"-"+axisLabel+QString("-Diff");
-
+                {
+                  curveNum = g->curveIndex(plotDetailsList[2] + "-sp-1-Calc"); //workspaceName+"-"+axisLabel+QString("-Diff") doesnt work when axis label is 33;
+                    if (curveNum == -1)
+                      curveNum = g->curveIndex(plotDetailsList[2] + "-sp-33-Calc");
+                }
                 if (curveNum > -1) // If one of the curves has been changed 
                 {
-                  pd->selectCurve(g->curveIndex(curveNum+1));
-
                   // line(0) scatter(1) line+symbol(2)
                   if (plotType >= 0 && plotType <= 2)
                   {
                     if (plotDetailsList.size() > 3)
-                      pd->setPlotType(plotType, plotDetailsList[3]);
+                      pd->setPlotType(plotType, curveNum, plotDetailsList[3]);
                     else
-                      pd->setPlotType(plotType);            
+                      pd->setPlotType(plotType, curveNum);            
                   }
                   g->activateGraph();
                 }
