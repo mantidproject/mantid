7671abb942a4c9e194f56c18658dbdf9f86eb0ae
Alex Buts <Alex.Buts@stfc.ac.uk>
2011-11-03 14:11:48 +0000

    refs #3763 fixing Unix error

--- Code/Mantid/Framework/Kernel/inc/MantidKernel/ValidatorSignalChange.h
+++ Code/Mantid/Framework/Kernel/inc/MantidKernel/ValidatorSignalChange.h
@@ -42,11 +42,12 @@ namespace Kernel
     Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
 template <typename TYPE>
-class DLLExport ValidatorSignalChange : public IValidator<TYPE>, public boost::signal<void (const Property *)>
+class DLLExport ValidatorSignalChange : public IValidator< TYPE >, public boost::signal<void (const Property *)>
 {
 public:
   /// Constructor
   ValidatorSignalChange(const Property *pProp):
+      IValidator<TYPE>() ,
       pPropObserved(pProp)
   {}
 
