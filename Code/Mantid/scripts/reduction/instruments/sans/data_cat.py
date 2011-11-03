491e8fdea1290f56cb2816d287ae3a060fb1881b
Mathieu Doucet <doucetm@ornl.gov>
2011-11-03 16:31:46 -0400

    Re #4048 Fleshing out demo data set listing

--- Code/Mantid/scripts/reduction/instruments/sans/data_cat.py
+++ Code/Mantid/scripts/reduction/instruments/sans/data_cat.py
@@ -1,59 +1,105 @@
 import os
 import sqlite3
 import re
+import time
 
-def list_all():
-	data_dir =  os.path.join(os.path.expanduser("~"), "data/eqsans")
-	log_ws = "__log"
+class DataSet(object):
+	def __init__(self, run_number, title, run_start, duration, ssd):
+		self.run_number = run_number
+		self.title = title
+		self.run_start = run_start
+		self.duration = duration
+		self.ssd = ssd
+		
+	@classmethod
+	def header(cls):
+		return "%-6s %-60s %-16s %-7s %-10s" % ("Run", "Title", "Start", "Time[s]", "SDD[mm]")
+	def __str__(self):
+		return "%-6s %-60s %-16s %-7g %-10.0f" % (self.run_number, self.title, self.run_start, self.duration, self.ssd)
+	
+	def from_file(self, filepath):
+		pass
+	
+	def store(self):
+		pass
+	
+	@classmethod
+	def find(cls, file_path, cursor):
+		r_re = re.search("EQSANS_([0-9]+)_event", file_path)
+		if r_re is None:
+			return None
+		
+		t = (int(r_re.group(1)),)
+		cursor.execute('select * from dataset where run=?', t)
+		rows = cursor.fetchall()
 
-	def read_prop(prop):
-		try:
-			return mtd[log_ws].getRun().getProperty(prop).value
-		except:
-			return ""
+		if len(rows) == 0:
+			log_ws = "__log"
+			def read_prop(prop):
+				try:
+					return mtd[log_ws].getRun().getProperty(prop).value
+				except:
+					return ""
+			def read_series(prop):
+				try:
+					return mtd[log_ws].getRun().getProperty(prop).getStatistics().mean
+				except:
+					print sys.exc_value
+					return -1
+			
+			LoadEventNexus(file_path, OutputWorkspace=log_ws, MetaDataOnly=True)
+			runno = int(read_prop("run_number"))
+			title = read_prop("run_title")
+			t_str = read_prop("start_time")
+			t = time.strptime(t_str, '%Y-%m-%dT%H:%M:%S')
+			run_start = time.strftime('%y-%m-%d %H:%M', t)
+			duration = float(read_prop("duration"))
+			sdd = float(read_series("detectorZ"))
 
-	db = sqlite3.connect("/home/m2d/data_catalog")
+			t = (runno, title, run_start, duration, sdd)
+			cursor.execute('insert into dataset values (?,?,?,?,?)', t)
+			return DataSet(runno, title, run_start, duration, sdd)
+		else:
+			row = rows[0]
+			return DataSet(row[0], row[1], row[2], row[3], row[4])
+
+
+def list_all(data_dir=None, replace_db=False):
+	"""
+		Returns a list of the data sets as text
+	"""
+	
+	# Data directory
+	if data_dir is None:
+		data_dir =  os.path.join(os.path.expanduser("~"), "data/eqsans")
+
+	# Connect/create to DB
+	db_path = os.path.join(os.path.expanduser("~"), ".mantid_data_sets")
+	db_exists = False
+	if os.path.isfile(db_path):
+		if replace_db:
+			os.remove(db_path)
+		else: 
+			db_exists = True
+		
+	db = sqlite3.connect(db_path)
 	c = db.cursor()
-	#c.execute("""create table dataset (run int, title text, start text, duration real)""")
+	
+	if not db_exists:
+		c.execute("""create table dataset (run int, title text, start text, duration real, ssd real)""")
 
-	print "%-6s %-60s %-22s %-10s" % ("Run", "Title", "Start", "Duraction [s]")
+	output = "%s\n" % DataSet.header()
 
-	icount = 0
 	for f in os.listdir(data_dir):
 		if f.endswith("nxs"):
-			r_re = re.search("EQSANS_([0-9]+)_event", f)
-			if r_re is None:
-				continue
+			path = os.path.join(data_dir, f)
+			d = DataSet.find(path, c)
+			if d is not None:
+				output += "%s\n" % str(d)
 
-			run_number = int(r_re.group(1))
-			t = (run_number,)
-			c.execute('select * from dataset where run=?', t)
-			rows = c.fetchall()
-
-			if len(rows) == 0:
-				path = os.path.join(data_dir, f)
-				LoadEventNexus(path, OutputWorkspace=log_ws, MetaDataOnly=True)
-				runno = int(read_prop("run_number"))
-				title = read_prop("run_title")
-				run_start = read_prop("start_time")		
-				duration = float(read_prop("duration"))
-
-				t = (runno, title, run_start, duration)
-				print "%-6s %-60s %-22s %-10s" % (runno, title, run_start, duraction)
-				print "inserting", runno, title, run_start, duration
-
-				c.execute('insert into dataset values (?,?,?,?)', t)
-			else:
-				row = rows[0]
-				print "%-6s %-60s %-22s %-10s" % (row[0], row[1], row[2], row[3])
-
-			icount += 1
-			#if icount>2:
-			#	break
 	db.commit()
 	c.close()
+	return output
 	
-        
-    
 if __name__ == '__main__':
-    list_all()
+    print list_all(replace_db=False)
