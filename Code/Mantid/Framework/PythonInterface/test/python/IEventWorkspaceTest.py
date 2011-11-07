7e09b4466e48d5553e3ecf7a3847819e300b069c
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-11-07 15:34:17 +0000

    Refs #3987. Fix IEventWorkspace tests.

--- Code/Mantid/Framework/PythonInterface/test/python/IEventWorkspaceTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/IEventWorkspaceTest.py
@@ -11,17 +11,16 @@ class IEventWorkspaceTest(unittest.TestCase):
     def setUp(self):
         if self._test_ws is None:
             alg = run_algorithm("Load", Filename='CNCS_7860_event.nxs', OutputWorkspace='CNCS_7860',
-                                FilterByTimeStart=60.0,FilterByTimeStop=60.5,LoadMonitors=False,
-                                child=True)
-            _test_ws = alg.get_property("OutputWorkspace").value
+                                FilterByTimeStart=60.0,FilterByTimeStop=60.5,LoadMonitors=False,child=True)
+            self.__class__._test_ws = alg.get_property("OutputWorkspace").value
 
     def test_that_it_cannot_be_directly_instantiated(self):
         self.assertFalse(can_be_instantiated(IEventWorkspace))
 
     def test_meta_information(self):
-        self.assertEquals(self._test_ws.get_number_events(), 146)
-        self.assertEquals(self._test_ws.get_minimum_tof(), 44704.9)
-        self.assertEquals(self._test_ws.get_maximum_tof(), 55612.4)
+        self.assertEquals(self._test_ws.get_number_events(), 164)
+        self.assertAlmostEquals(self._test_ws.get_tof_min(), 44704.8984375)
+        self.assertAlmostEquals(self._test_ws.get_tof_max(), 55612.3984375)
 
     def test_that_clearing_mru_does_not_raise_an_error(self):
         try:
