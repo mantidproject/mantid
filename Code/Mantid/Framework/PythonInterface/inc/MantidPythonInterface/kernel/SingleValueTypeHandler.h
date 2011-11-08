b1c5715a4592c3a4c39e1449609e04fac4fecc5a
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-11-08 09:07:22 +0000

    Refs #3987. A better name for the current type handler.

--- Code/Mantid/Framework/PythonInterface/inc/MantidPythonInterface/kernel/SingleValueTypeHandler.h
+++ Code/Mantid/Framework/PythonInterface/inc/MantidPythonInterface/kernel/SingleValueTypeHandler.h
@@ -34,7 +34,7 @@ namespace Mantid
        * DECLARE_TYPEHANDLER macro whenever a new class is exported that will be used with PropertyWithValue
        */
       template<typename BaseType, typename DerivedType=BaseType>
-      struct DLLExport TypedHandler : public PropertyHandler
+      struct DLLExport SingleValueTypeHandler : public PropertyHandler
       {
         /**
          * Set function to handle Python -> C++ calls and get the correct type
@@ -64,7 +64,7 @@ namespace Mantid
        * assigned polymorphically. This can be removed when the bug is fixed
        */
       template<>
-      struct DLLExport TypedHandler<std::string> : public PropertyHandler
+      struct DLLExport SingleValueTypeHandler<std::string> : public PropertyHandler
       {
         /**
          * Set function to handle Python -> C++ calls and get the correct type
@@ -95,7 +95,7 @@ namespace Mantid
        * is of type double but an integer is passed.
        */
       template<>
-      struct DLLExport TypedHandler<int> : public PropertyHandler
+      struct DLLExport SingleValueTypeHandler<int> : public PropertyHandler
       {
         /**
          * Set function to handle Python -> C++ calls and get the correct type
@@ -140,6 +140,6 @@ namespace Mantid
   * @param export_type :: The C++ type that is to be converted
   * @param base_type :: The C++ type that the export_type is to be treated as
   */
-#define DECLARE_TYPEHANDLER(export_type, base_type) \
+#define DECLARE_SINGLEVALUETYPEHANDLER(export_type, base_type) \
   const boost::python::converter::registration *reg = boost::python::converter::registry::query(typeid(export_type));\
-  Mantid::PythonInterface::PropertyMarshal::registerHandler(reg->get_class_object(), new Mantid::PythonInterface::PropertyMarshal::TypedHandler<base_type, export_type>());
+  Mantid::PythonInterface::PropertyMarshal::registerHandler(reg->get_class_object(), new Mantid::PythonInterface::PropertyMarshal::SingleValueTypeHandler<base_type, export_type>());
