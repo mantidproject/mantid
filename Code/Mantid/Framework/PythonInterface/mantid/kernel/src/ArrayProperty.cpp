30249fce62fe11611907e16f9f0185aa225a5ea7
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-11-08 22:11:32 +0000

    Refs #4071. More property type exports.

---
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/src/ArrayProperty.cpp
@@ -0,0 +1,19 @@
+#include "MantidKernel/ArrayProperty.h"
+#include <boost/python/class.hpp>
+
+using Mantid::Kernel::ArrayProperty;
+using Mantid::Kernel::PropertyWithValue;
+using namespace boost::python;
+
+#define EXPORT_ARRAY_PROP(type, suffix) \
+    class_<ArrayProperty<type>, \
+           bases<PropertyWithValue<std::vector<type> > >, boost::noncopyable>("ArrayProperty_"#suffix, no_init);
+
+
+void export_ArrayProperty()
+{
+  EXPORT_ARRAY_PROP(double,dbl);
+  EXPORT_ARRAY_PROP(int,int);
+  EXPORT_ARRAY_PROP(size_t,size_t);
+}
+
--- Code/Mantid/Framework/PythonInterface/mantid/kernel/src/SequenceTypeHandler.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/src/SequenceTypeHandler.cpp
@@ -20,17 +20,8 @@ namespace Mantid
                                     const std::string &name, boost::python::object value)
       {
         UNUSED_ARG(alg); UNUSED_ARG(name); UNUSED_ARG(value);
-
         // We can't avoid a copy here as the final values will have reside in an array allocated
         // by the C++ new operator
-
-        // There are several issues to deal with here.
-        //  1) The sequence type could be one of many: list, tuple + others
-        //  2) We need to know the item type so that we can use the correct C++ container type
-        // Currently all are mapped to a std::vector
-
-        // How to handle each C++ type ??
-
 
       }
 
