dd92d51a2b77234ebf43df9c42dfec975c8a8cec
Mathieu Doucet <doucetm@ornl.gov>
2011-11-11 18:16:29 -0500

    Re #4114 First cut at data stitcher

--- Code/Mantid/scripts/Interface/compile_sans_ui.py
+++ Code/Mantid/scripts/Interface/compile_sans_ui.py
@@ -26,6 +26,7 @@ try:
     os.system("pyuic4 -o ui/ui_trans_spreader.py ui/trans_spreader.ui")
     os.system("pyuic4 -o ui/ui_instrument_dialog.py ui/instrument_dialog.ui")
     os.system("pyuic4 -o ui/ui_data_catalog.py ui/data_catalog.ui")
+    os.system("pyuic4 -o ui/ui_stitcher.py ui/stitcher.ui")
         
     # Example
     #os.system("pyuic4 -o ui/ui_example.py ui/example.ui")
