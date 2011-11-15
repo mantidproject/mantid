9b09b96f22a376be76c548d3867f1c6b847d2a32
Peter Peterson <petersonpf@ornl.gov>
2011-11-15 11:37:38 -0500

    Parallelize AlignDetectors even if unsorted. Refs #3890.
    Since it doesn't matter if the events are sorted for a unit
    conversion, don't check for threadsafe.

--- Code/Mantid/Framework/Algorithms/src/AlignDetectors.cpp
+++ Code/Mantid/Framework/Algorithms/src/AlignDetectors.cpp
@@ -318,7 +318,7 @@ void AlignDetectors::execEvent()
   // Initialise the progress reporting object
   Progress progress(this,0.0,1.0,numberOfSpectra);
 
-  PARALLEL_FOR2(inputWS,outputWS)
+  PARALLEL_FOR_NO_WSP_CHECK()
   for (int64_t i = 0; i < int64_t(numberOfSpectra); ++i)
   {
     PARALLEL_START_INTERUPT_REGION
