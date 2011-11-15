5691e854178f543d297f2e854770d510ccd4a2f8
Jose Borreguero <borreguerojm@ornl.gov>
2011-11-15 14:15:31 -0500

    Refs #4123 stretched exponential fit function files
    - modified:   CurveFitting/CMakeLists.txt
    - new file:   CurveFitting/inc/MantidCurveFitting/StretchExp.h
    - new file:   CurveFittingsrc/StretchExp.cpp

---
+++ Code/Mantid/Framework/CurveFitting/src/StretchExp.cpp
@@ -0,0 +1,56 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidCurveFitting/StretchExp.h"
+#include <cmath>
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+
+using namespace Kernel;
+using namespace API;
+
+DECLARE_FUNCTION(StretchExp)
+
+StretchExp::StretchExp()
+{
+  declareParameter("Height", 1.0);
+  declareParameter("Lifetime", 1.0);
+  declareParameter("Stretching",1.0);
+}
+
+
+void StretchExp::functionMW(double* out, const double* xValues, const size_t nData)const
+{
+    const double& h = getParameter("Height");
+    const double& t = getParameter("Lifetime");
+    const double& b = getParameter("Stretching");
+
+    for (size_t i = 0; i < nData; i++) 
+    {
+        out[i] = h*exp( -pow(xValues[i]/t,b) );
+    }
+}
+
+void StretchExp::functionDerivMW(Jacobian* out, const double* xValues, const size_t nData)
+{
+    const double& h = getParameter("Height");
+    const double& t = getParameter("Lifetime");
+    const double& b = getParameter("Stretching");
+
+    for (size_t i = 0; i < nData; i++) {
+        double x = xValues[i];
+        double a = pow( x/t, b);
+        double e = exp(-a);
+        out->set(i,0, e);              //derivative with respect to h
+        out->set(i,1, h*a*b*e/t);      //derivative with respect to t
+        out->set(i,2,-h*a*e*log(x/t)); //derivative with respect to b
+    }
+
+}
+
+
+} // namespace CurveFitting
+} // namespace Mantid
