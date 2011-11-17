26e499e2800061ed4d697678e0c0ff18d8c9c459
Owen Arnold <owen.arnold@stfc.ac.uk>
2011-11-17 10:05:50 +0000

    refs #4116. Clean up existing base class. No new features added yet.

---
+++ Code/Mantid/MantidPlot/src/Mantid/MantidCurve.cpp
@@ -0,0 +1,21 @@
+#include "MantidCurve.h"
+
+/**
+Constructor
+@param wsName : Name of the workspace
+*/
+MantidCurve::MantidCurve(const QString& wsName) : PlotCurve(wsName), WorkspaceObserver()
+{
+}
+
+/**
+Constructor default
+*/
+MantidCurve::MantidCurve() : PlotCurve(), WorkspaceObserver()
+{
+}
+
+/// Destructor
+MantidCurve::~MantidCurve()
+{
+}
--- Code/Mantid/MantidPlot/src/Mantid/MantidCurve.h
+++ Code/Mantid/MantidPlot/src/Mantid/MantidCurve.h
@@ -4,17 +4,40 @@
 #include "../PlotCurve.h"
 #include "MantidQtAPI/WorkspaceObserver.h"
 
+/** Base class for MantidCurve types. 
+    
+    @date 17/11/2011
+
+    Copyright &copy; 2011 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>    
+*/
 class MantidCurve :public PlotCurve, public MantidQt::API::WorkspaceObserver
 {
 public:
-  MantidCurve(const QString& wsName):PlotCurve(wsName), 
-  WorkspaceObserver()
-  {
-  }
-  MantidCurve():PlotCurve(), 
-  WorkspaceObserver()
-  {
-  }
+  /// Constructor
+  MantidCurve(const QString& wsName);
+  /// Default constructor
+  MantidCurve();
+  /// Destructor
+  virtual ~MantidCurve();
 };
 
 #endif
