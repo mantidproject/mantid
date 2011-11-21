afbfdaf150188c256438d4ff83e66e6a830c98ac
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-11-21 11:21:20 +0000

    Refs #2650. ADS in Python now upcasts its returned pointers.
    The ADS in Python now both returns weak_ptrs to objects, to avoid holding
    on to memory and also upcasts these pointers to the correct type.

--- Code/Mantid/Framework/PythonInterface/test/python/AnalysisDataServiceTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/AnalysisDataServiceTest.py
@@ -9,11 +9,11 @@ class AnalysisDataServiceTest(unittest.TestCase):
     def test_len_returns_correct_value(self):
         self.assertEquals(len(analysis_data_svc), 0)
         
-    def test_retrieval_of_non_existent_data_raises_error(self):
+    def test_retrieval_of_non_existent_data_raises_KeyError(self):
         try:
             analysis_data_svc['NotHere']
             self.fail('AnalysisDataService did not throw when object does not exist')
-        except RuntimeError:
+        except KeyError:
             pass
         
     def test_len_increases_when_item_added(self):
@@ -37,15 +37,31 @@ class AnalysisDataServiceTest(unittest.TestCase):
         ws_from_op = analysis_data_svc[wsname]
         ws_from_method = analysis_data_svc.retrieve(wsname)
         
+        # Type check
+        self.assertTrue(isinstance(ws_from_op, MatrixWorkspace))
+        self.assertTrue(isinstance(ws_from_method, MatrixWorkspace))
+
         self.assertNotEquals(ws_from_op.name(), '')
         self.assertEquals(ws_from_op.name(), ws_from_method.name())
         self.assertEquals(ws_from_op.get_memory_size(), ws_from_method.get_memory_size())
 
-        # Type check
-        #self.assertTrue(isinstance(ws_from_op, MatrixWorkspace))
-        #self.assertTrue(isinstance(ws_from_method, MatrixWorkspace))
-
         # Remove to clean the test up
         analysis_data_svc.remove(wsname)
 
+    def test_removing_item_invalidates_extracted_handles(self):
+        # If a reference to a DataItem has been extracted from the ADS
+        # and it is then removed. The extracted handle should no longer
+        # be able to access the DataItem
+        wsname = 'ADSTest_test_removing_item_invalidates_extracted_handles'
+        run_algorithm('LoadRaw', Filename='LOQ48127.raw', OutputWorkspace=wsname, SpectrumMax=1)
+        ws_handle = analysis_data_svc[wsname]
+        succeeded = False
+        try:
+            ws_handle.id() # Should be okay
+            succeeded = True
+        except RuntimeError:
+            pass
+        self.assertTrue(succeeded, "DataItem handle should be valid and allow function calls")
+        analysis_data_svc.remove(wsname)
+        self.assertRaises(RuntimeError, ws_handle.id)
         
