d0a422def4d9d3efe7061096085a786e4b82934c
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-11-24 09:54:35 +0000

    Refs #2650. Make the mantid python module a little quieter on import.
    This also avoids warnings from boost python that could crop up with some
    import configurations, i.e. from mantid import simpleapi.
    Also added a better docstring to the mantid module __init__.

--- Code/Mantid/Framework/PythonInterface/mantid/kernel/__init__.py
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/__init__.py
@@ -1,6 +1,15 @@
-"""Defines the kernel sub-package
 """
-import dlopen
-flags = dlopen.setup_dlopen() # Ensure the library is open with the correct flags
+kernel
+=============
+
+Defines Python objects that wrap the C++ Kernel namespace.
+
+"""
+
+###############################################################################
+# The _api C extension depends on exports defined in the _kernel extension
+###############################################################################
+import dlopen as _dlopen
+flags = _dlopen.setup_dlopen() # Ensure the library is open with the correct flags
 from _kernel import *
 dlopen.restore_flags(flags)
