c6d20520683af741a48724d86b70edfe6ba3ca47
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-12-05 12:00:33 -0500

    Refs #4265 Added SignalOverError unary operation

---
+++ Code/Mantid/Framework/Algorithms/test/SignalOverErrorTest.h
@@ -0,0 +1,60 @@
+#ifndef MANTID_ALGORITHMS_SIGNALOVERERRORTEST_H_
+#define MANTID_ALGORITHMS_SIGNALOVERERRORTEST_H_
+
+#include "MantidAlgorithms/SignalOverError.h"
+#include "MantidDataObjects/Workspace2D.h"
+#include "MantidKernel/System.h"
+#include "MantidKernel/Timer.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+#include <cxxtest/TestSuite.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace Mantid;
+using namespace Mantid::Algorithms;
+using namespace Mantid::API;
+using Mantid::DataObjects::Workspace2D_sptr;
+
+class SignalOverErrorTest : public CxxTest::TestSuite
+{
+public:
+  void test_Init()
+  {
+    SignalOverError alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+  }
+  
+  void test_exec()
+  {
+    // Name of the output workspace.
+    std::string outWSName("SignalOverErrorTest_OutputWS");
+
+    Workspace2D_sptr inWS = WorkspaceCreationHelper::Create2DWorkspace(2, 10);
+  
+    SignalOverError alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", boost::dynamic_pointer_cast<MatrixWorkspace>(inWS)) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+    
+    // Retrieve the workspace from data service.
+    MatrixWorkspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = boost::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve(outWSName)) );
+    TS_ASSERT(ws);
+    if (!ws) return;
+    
+    // Check the results
+    TS_ASSERT_DELTA(ws->readY(0)[0], sqrt(2.0), 1e-5);
+    TS_ASSERT_DELTA(ws->readE(0)[0], 0.0, 1e-5);
+    
+    // Remove workspace from the data service.
+    AnalysisDataService::Instance().remove(outWSName);
+  }
+
+};
+
+
+#endif /* MANTID_ALGORITHMS_SIGNALOVERERRORTEST_H_ */
