b2d1abca2c9d812e1025a54127f8b499c30f3c11
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2011-12-07 10:51:58 +0000

    Re #4281. Added a warning dialog if instrument view is not unwrapped

--- Code/Mantid/MantidPlot/src/Mantid/InstrumentWidget/InstrumentWindow.h
+++ Code/Mantid/MantidPlot/src/Mantid/InstrumentWidget/InstrumentWindow.h
@@ -145,6 +145,7 @@ public slots:
   void setWireframe(bool);
 
   void clearPeakOverlays();
+  //void setPeakLabelSignificantDigits(int n);
 
 signals:
   void plotSpectra(const QString&,const std::set<int>&);
