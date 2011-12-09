87ca30ced7f553f518b73e758af23c40c5532491
Alex Buts <Alex.Buts@stfc.ac.uk>
2011-12-09 21:03:09 +0000

    refs #4201 Stupid warning fixed

--- Code/Mantid/Framework/MDAlgorithms/test/ConvertToMDEventsTest.h
+++ Code/Mantid/Framework/MDAlgorithms/test/ConvertToMDEventsTest.h
@@ -119,7 +119,7 @@ void testTOFConversionFails()
     TS_ASSERT_THROWS_NOTHING(pSourceWSUnit->initialize(L1,L2,TwoTheta,emode,efix,delta));
      
     double X0(5);
-    double tof ,k_tr;
+    double tof(0) ,k_tr(0);
     TS_ASSERT_THROWS_NOTHING(tof  = pSourceWSUnit->singleToTOF(X0));
     TS_ASSERT_THROWS_NOTHING(k_tr = pWSUnit->singleFromTOF(tof));
 }
