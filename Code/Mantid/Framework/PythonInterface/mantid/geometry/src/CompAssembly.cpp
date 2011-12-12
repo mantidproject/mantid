06f9a81b2f9d1aa8ebe5c66125fa7b0585475db2
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2011-12-12 22:49:49 +0000

    Refs #4315. Export most of geometry namespace to new Python interface

---
+++ Code/Mantid/Framework/PythonInterface/mantid/geometry/src/CompAssembly.cpp
@@ -0,0 +1,18 @@
+#include "MantidGeometry/Instrument/CompAssembly.h"
+#include <boost/python/class.hpp>
+
+using Mantid::Geometry::CompAssembly;
+using Mantid::Geometry::ICompAssembly;
+using Mantid::Geometry::Component;
+using namespace boost::python;
+
+/**
+ * Enables boost.python to automatically "cast" an object up to the
+ * appropriate CompAssembly leaf type 
+ */
+void export_CompAssembly()
+{
+  class_<CompAssembly, bases<ICompAssembly, Component>, boost::noncopyable>("CompAssembly", no_init)
+    ;
+}
+
