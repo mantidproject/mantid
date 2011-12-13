b046c00655525d03de0ceba1fcb584a3015f87f0
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-12-13 12:09:51 -0500

    Refs #4316 added SliceViewer test to ctest
    Created a GUITests target to hold this and future GUI tests.
    Test python script runs from python stand alone. Rudimentary test for now, can use QtTest in the future for more sophisticated tests.

--- Code/Mantid/MantidQt/SliceViewer/test/SliceViewerPythonInterfaceTest.py
+++ Code/Mantid/MantidQt/SliceViewer/test/SliceViewerPythonInterfaceTest.py
@@ -6,7 +6,15 @@ import time
 from PyQt4 import Qt
 from PyQt4 import QtTest
 from PyQt4.QtTest import QTest
+
+# Import the Mantid framework
+import MantidFramework
+from MantidFramework import mtd
+from mantidsimple import *
 import libmantidqtpython
+
+
+app = Qt.QApplication(sys.argv)
 
 # Create a test data set
 CreateMDWorkspace(Dimensions='3',Extents='0,10,0,10,0,10',Names='x,y,z', 
@@ -20,9 +28,13 @@ class SliceViewerPythonInterfaceTest(unittest.TestCase):
     """Test for accessing SliceViewer widgets from MantidPlot
     python interpreter"""
     
+    def __init__(self, *args):
+        """ Constructor: builda QApplication """
+        unittest.TestCase.__init__(self, *args)
+       
+    
     def setUp(self):
         """ Set up and create a SliceViewer widget """
-        global libmantidqtpython
         self.sv = libmantidqtpython.MantidQt.SliceViewer.SliceViewer()
         pass
     
@@ -31,26 +43,14 @@ class SliceViewerPythonInterfaceTest(unittest.TestCase):
         self.sv.close()
 	
     def test_setWorkspace(self):
-        print "test_setWorkspace"
         sv = self.sv
         sv.setWorkspace('uniform')
         sv.show()
-        global QTest
-        #QTest.mouseClick(sv)
     
-    def test_other(self):
-        print "test_other"
+    def test_set_MDEventWorkspace(self):
         sv = self.sv
         sv.setWorkspace('mdw')
         sv.show()
     
     
-
-# ----- Create and run the unit test ----------------------    
-sys.path.append("/home/8oz/Code/Mantid/Code/Mantid/TestingTools/unittest-xml-reporting/src")
-import xmlrunner
-suite = unittest.makeSuite(SliceViewerPythonInterfaceTest)
-runner = xmlrunner.XMLTestRunner(output='Testing')
-runner.run(suite)
-print "Done!"
-
+    
