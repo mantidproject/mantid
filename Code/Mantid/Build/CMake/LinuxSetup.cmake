a93e8df19d159db5a90f2862e671a2297b0572b3
Peter Peterson <petersonpf@ornl.gov>
2011-12-19 12:04:33 -0500

    Moved environment scripts to inside the install directory. Refs #4264.
    By default they now go in /opt/mantid/etc and get symbolic links in RHEL. The
    links will need to be done in ubuntu as well, but that isn't top priority.

--- Code/Mantid/Build/CMake/LinuxSetup.cmake
+++ Code/Mantid/Build/CMake/LinuxSetup.cmake
@@ -34,6 +34,7 @@ endif ( USE_TCMALLOC )
 ###########################################################################
 
 set ( BIN_DIR bin )
+set ( ETC_DIR etc )
 set ( LIB_DIR lib )
 set ( PLUGINS_DIR plugins )
 set ( PVPLUGINS_DIR pvplugins )
@@ -56,11 +57,24 @@ file ( WRITE ${CMAKE_CURRENT_BINARY_DIR}/mantid.csh  "#!/bin/csh\n"
                                                     "setenv PATH \"\${PATH}:\${MANTIDPATH}\"\n"
 )
 
+file ( WRITE ${CMAKE_CURRENT_BINARY_DIR}/rpm_post_install.sh "#!/bin/sh\n"
+                                                         "ln -s $RPM_INSTALL_PREFIX0/${ETC_DIR}/mantid.sh /etc/profile.d/mantid.sh\n"
+                                                         "ln -s $RPM_INSTALL_PREFIX0/${ETC_DIR}/mantid.csh /etc/profile.d/mantid.csh\n"
+)
+
+file ( WRITE ${CMAKE_CURRENT_BINARY_DIR}/rpm_post_uninstall.sh "#!/bin/sh\n"
+                                                         "rm /etc/profile.d/mantid.sh\n"
+                                                         "rm /etc/profile.d/mantid.csh\n"
+)
+
+# Note: On older versions of CMake, this line may mean that to do a "make package" without being root
+# you will need to set the cache variable CPACK_SET_DESTDIR to ON.
+install ( PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/mantid.sh ${CMAKE_CURRENT_BINARY_DIR}/mantid.csh
+          DESTINATION ${CMAKE_INSTALL_PREFIX}/${ETC_DIR}
+)
+
 set ( ENVVARS_ON_INSTALL ON CACHE BOOL "Whether to include the scripts in /etc/profile.d to set the MANTIDPATH variable and add it to PATH. Turning this off allows installing locally without being root." )
 if ( ENVVARS_ON_INSTALL )
-  # Note: On older versions of CMake, this line may mean that to do a "make package" without being root
-  # you will need to set the cache variable CPACK_SET_DESTDIR to ON.
-  install ( PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/mantid.sh ${CMAKE_CURRENT_BINARY_DIR}/mantid.csh
-            DESTINATION /etc/profile.d 
-  )
+  set ( CPACK_RPM_POST_INSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/rpm_post_install.sh )
+  set ( CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/rpm_post_uninstall.sh )
 endif ()
