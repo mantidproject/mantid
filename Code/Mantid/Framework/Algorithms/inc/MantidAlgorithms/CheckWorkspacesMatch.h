57d108797b87cb4d501a2532dda7637718fb7c26
Stuart Campbell <campbellsi@ornl.gov>
2011-12-21 08:56:33 -0500

    Fix pelling error in category names. refs #3866

--- Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/CheckWorkspacesMatch.h
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/CheckWorkspacesMatch.h
@@ -73,7 +73,7 @@ public:
   /// Algorithm's version
   virtual int version() const { return (1); }
   /// Algorithm's category for identification
-  virtual const std::string category() const { return "Uility\\Workspaces"; }
+  virtual const std::string category() const { return "Utility\\Workspaces"; }
 
   /** Return the string output when comparison is successful.
    */
