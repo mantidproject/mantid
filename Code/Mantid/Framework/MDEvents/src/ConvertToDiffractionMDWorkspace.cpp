5edb693b57c2829e6653fd6facdbabe433343d0d
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-12-28 11:20:45 -0500

    Refs #4350: Make HKL correct in ConvertToDiffractionMDWorkspace

--- Code/Mantid/Framework/MDEvents/src/ConvertToDiffractionMDWorkspace.cpp
+++ Code/Mantid/Framework/MDEvents/src/ConvertToDiffractionMDWorkspace.cpp
@@ -305,10 +305,12 @@ namespace MDEvents
       // Set the matrix based on UB etc.
       Kernel::Matrix<double> ub = in_ws->mutableSample().getOrientedLattice().getUB();
       Kernel::Matrix<double> gon = in_ws->mutableRun().getGoniometerMatrix();
-      // As per Busing and Levy 1967, q_lab_frame = Goniometer * UB * HKL
-      // Therefore, HKL = (Goniometer * UB)^-1 * q_lab_frame
+      // As per Busing and Levy 1967, q_lab_frame = 2pi * Goniometer * UB * HKL
+      // Therefore, HKL = (2*pi * Goniometer * UB)^-1 * q_lab_frame
       mat = gon * ub;
       mat.Invert();
+      // Divide by 2 PI to account for our new convention, |Q| = 2pi / wl (December 2011, JZ)
+      mat /= (2 * M_PI);
       dimensionNames[0] = "H";
       dimensionNames[1] = "K";
       dimensionNames[2] = "L";
