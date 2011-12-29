12ad9edc7f2187f8ce344119759f49bbbcfacd52
Janik Zikovsky <zikovskyjl@ornl.gov>
2011-12-29 15:38:12 -0500

    Refs #4375 close the plots to help test pass

--- Code/Mantid/MantidPlot/test/MantidPlot1DPlotTest.py
+++ Code/Mantid/MantidPlot/test/MantidPlot1DPlotTest.py
@@ -5,7 +5,7 @@ import mantidplottests
 from mantidplottests import *
 import time
 import numpy as np
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 # =============== Create a fake workspace to plot =======================
 X1 = np.linspace(0,10, 100)
@@ -20,25 +20,34 @@ X = np.append(X1, X2)
 Y = np.append(Y1, Y2)
 E = np.sqrt(Y)
 
-CreateWorkspace("fake", X, Y, E, NSpec=2, UnitX="TOF", YUnitLabel="Counts",  WorkspaceTitle="Faked data Workspace")
+CreateWorkspace("fake", list(X), list(Y), list(E), NSpec=2, UnitX="TOF", YUnitLabel="Counts",  WorkspaceTitle="Faked data Workspace")
 #CreateWorkspace("fake2", X, Y, E, NSpec=2, UnitX="TOF", YUnitLabel="Counts",  WorkspaceTitle="Faked data Workspace")
 
 class MantidPlot1DPlotTest(unittest.TestCase):
     
     def setUp(self):
         pass
+    
+    def tearDown(self):
+        """Clean up by closing the created window """
+        self.g.confirmClose(False)
+        self.g.close()
+        QtCore.QCoreApplication.processEvents()
 
     def test_plotSpectrum_errorBars(self):
         g = plotSpectrum("fake", 0, error_bars=True)
         screenshot(g, "plotSpectrum_errorBars", "Call to plotSpectrum() of 1 spectrum, with error bars.")
+        self.g = g
 
     def test_plotSpectrum_fromWorkspaceProxy(self):
         ws = mtd["fake"]
-        plotSpectrum(ws, 0, error_bars=True)
+        g = plotSpectrum(ws, 0, error_bars=True)
+        self.g = g
         
     def test_plotSpectrum_severalSpectra(self):
         g = plotSpectrum("fake", [0, 1])
         screenshot(g, "plotSpectrum_severalSpectra", "Call to plotSpectrum() of 2 spectra, no error bars.")
+        self.g = g
         
     def test_Customized1DPlot(self):
         g = plotSpectrum("fake", 0, error_bars=True)
@@ -52,7 +61,7 @@ class MantidPlot1DPlotTest(unittest.TestCase):
         l.setScale(2, 0, 3.0)
         l.setAntialiasing(True)
         screenshot(g, "Customized1DPlot", "1D plot of a spectrum, with error bars, an orange line of width 2, a custom title in red Arial font, with X from 0 to 3")
-        
+        self.g = g
 
 
 # Run the unit tests
