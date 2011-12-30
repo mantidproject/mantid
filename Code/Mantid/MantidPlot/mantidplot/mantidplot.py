4564706275d7d74ae5272cb89356fd4965979b46
Russell Taylor <taylorrj@ornl.gov>
2011-12-30 17:33:04 -0500

    Split up mantidplot.py and move it into a directory. Re #1037.
    mantidplot.py is starting to get big, and once all the proxy business
    for qti objects is added is will really swell. So the proxy objects
    will go in a separate file and everything is in a mantidplot directory
    that is imported. It shouldn't matter if an old mantidplot.py is lying
    around in the old place - python will prefer to import the directory.
    Fingers crossed that I've got the installation settings right.

--- Code/Mantid/MantidPlot/mantidplot/mantidplot.py
+++ Code/Mantid/MantidPlot/mantidplot/mantidplot.py
@@ -10,6 +10,7 @@ except ImportError:
 
 # Grab a few Mantid things so that we can recognise workspace variables
 from MantidFramework import WorkspaceProxy, WorkspaceGroup, MatrixWorkspace, mtd, ProxyObject
+import proxies
 from PyQt4 import QtCore
 
 #-------------------------- Wrapped MantidPlot functions -----------------
@@ -142,10 +143,10 @@ def plotSlice(source, label="", xydim=None, slicepoint=None,
            xydim=xydim, slicepoint=slicepoint, colormin=colormin,
            colormax=colormax, colorscalelog=colorscalelog, limits=limits, 
            **kwargs) 
-        pxy = SliceViewerWindowProxy(window)
+        pxy = proxies.SliceViewerWindowProxy(window)
         out.append(pxy)
         
-    # Returh the widget alone if only 1
+    # Return the widget alone if only 1
     if len(out) == 1:
         return out[0]
     else:
@@ -207,118 +208,6 @@ Layer.Left = qti.GraphOptions.Left
 Layer.Right = qti.GraphOptions.Right
 Layer.Bottom = qti.GraphOptions.Bottom
 Layer.Top = qti.GraphOptions.Top
-
-
-#-----------------------------------------------------------------------------
-#--------------------------- Proxy Objects -----------------------------------
-#-----------------------------------------------------------------------------
-
-
-
-#-----------------------------------------------------------------------------
-class QtProxyObject(QtCore.QObject):
-    """Generic Proxy object for wrapping Qt C++ Qobjects.
-    This holds the QObject internally and passes methods to it.
-    When the underlying object is deleted, the reference is set
-    to None to avoid segfaults.
-    """
-    def __init__(self, toproxy):
-        QtCore.QObject.__init__(self)
-        self.__obj = toproxy
-        # Connect to track the destroyed
-        QtCore.QObject.connect( self.__obj, QtCore.SIGNAL("destroyed()"),
-                                self._heldObjectDestroyed)
-        
-    def _heldObjectDestroyed(self):
-        """Slot called when the held object is destroyed.
-        Sets it to None, preventing segfaults """
-        self._kill_object()
-
-    def __getattr__(self, attr):
-        """
-        Reroute a method call to the the stored object
-        """
-        return getattr(self._getHeldObject(), attr)
-
-    def __str__(self):
-        """
-        Return a string representation of the proxied object
-        """
-        return str(self._getHeldObject())
-
-    def __repr__(self):
-        """
-        Return a string representation of the proxied object
-        """
-        return `self._getHeldObject()`
-
-    def _getHeldObject(self):
-        """
-        Returns a reference to the held object
-        """
-        return self.__obj
-
-    def _kill_object(self):
-        """
-        Release the stored instance
-        """
-        self.__obj = None
-
-    def _swap(self, obj):
-        """
-        Swap an object so that the proxy now refers to this object
-        """
-        self.__obj = obj
-
-
-
-#-----------------------------------------------------------------------------
-class SliceViewerWindowProxy(QtProxyObject):
-    """Proxy for a C++ SliceViewerWindow object.
-    
-    It will pass-through method calls that can be applied to the
-    SliceViewer widget contained within.
-    """
-    def __init__(self, toproxy):
-        QtProxyObject.__init__(self, toproxy)
-        # List of methods in slicer to pass-through
-        self.slicer_methods = ["setWorkspace", "getWorkspaceName", "showControls", "openFromXML", "saveImage", "setXYDim", "setXYDim", "getDimX", "getDimY", "setSlicePoint", "setSlicePoint", "getSlicePoint", "getSlicePoint", "setXYLimits", "getXLimits", "getYLimits", "zoomBy", "setXYCenter", "resetZoom", "loadColorMap", "setColorScale", "setColorScaleMin", "setColorScaleMax", "setColorScaleLog", "getColorScaleMin", "getColorScaleMax", "getColorScaleLog", "setColorScaleAutoFull", "setColorScaleAutoSlice", "setFastRender", "getFastRender"]
-
-    def __getattr__(self, attr):
-        """
-        Reroute a method call to the the stored object
-        """
-        if self._getHeldObject() is None:
-            raise Exception("Error! The SliceViewerWindow has been deleted.")
-        
-        # Pass-through to the contained SliceViewer widget.
-        sv = self._getHeldObject().getSlicer()
-        if attr in self.slicer_methods:
-            return getattr(sv, attr)
-        else:
-            return getattr(self._getHeldObject(), attr)
-
-    def __str__(self):
-        """
-        Return a string representation of the proxied object
-        """
-        if self._getHeldObject() is None:
-            return "None"
-        else:
-            return 'SliceViewerWindow(workspace="%s")' % self._getHeldObject().getSlicer().getWorkspaceName()
-
-    def __repr__(self):
-        """
-        Return a string representation of the proxied object
-        """
-        return `self._getHeldObject()`
-    
-    def __dir__(self):
-        """
-        Returns the list of attributes for this object.
-        Might allow tab-completion to work under ipython
-        """
-        return self.slicer_methods
 
 
 
