7cbd12acee681019bf6a7ad9a929c38694cb1ff1
Andrei Savici <saviciat@ornl.gov>
2012-01-04 14:00:52 -0500

    Detailed balance algorithm. Refs #3659

--- Code/Mantid/Framework/Algorithms/src/ApplyDetailedBalance.cpp
+++ Code/Mantid/Framework/Algorithms/src/ApplyDetailedBalance.cpp
@@ -1,5 +1,16 @@
 /*WIKI* 
+ The fluctuation dissipation theorem [1,2] relates the dynamic susceptibility to the scattering function
 
+ <math>\left(1-e^{-\frac{E}{k_B T}}\right) S(\mathbf{q}, E) = \frac{1}{\pi} \chi'' (\mathbf{q}, E) </math>
+
+ where <math>E</math> is the energy transfer to the system. The algorithm assumes that the y axis of the
+ input workspace contains the scattering function <math>S</math>. The y axis of the output workspace will
+ contain the dynamic susceptibility. The temperature is extracted from a log attached to the workspace, as the mean
+ value. Alternatively, the temperature can be directly specified. The algorithm will fail if neither option is
+ valid.
+
+ [1] S. W. Lovesey - Theory of Neutron Scattering from Condensed Matter, vol 1
+ [2] I. A. Zaliznyak and S. H. Lee - Magnetic Neutron Scattering in "Modern techniques for characterizing magnetic materials"
 
 *WIKI*/
 #include "MantidAlgorithms/ApplyDetailedBalance.h"
@@ -31,7 +42,6 @@ namespace Algorithms
    */
   ApplyDetailedBalance::ApplyDetailedBalance()
   {
-    // TODO Auto-generated constructor stub
   }
     
   //----------------------------------------------------------------------------------------------
@@ -39,7 +49,6 @@ namespace Algorithms
    */
   ApplyDetailedBalance::~ApplyDetailedBalance()
   {
-    // TODO Auto-generated destructor stub
   }
   
 
@@ -47,8 +56,8 @@ namespace Algorithms
   /// Sets documentation strings for this algorithm
   void ApplyDetailedBalance::initDocs()
   {
-    this->setWikiSummary("TODO: Enter a quick description of your algorithm.");
-    this->setOptionalMessage("TODO: Enter a quick description of your algorithm.");
+    this->setWikiSummary("Transform scattering intensity to dynamic susceptibility.");
+    this->setOptionalMessage("Transform scattering intensity to dynamic susceptibility.");
   }
 
   //----------------------------------------------------------------------------------------------
@@ -60,7 +69,7 @@ namespace Algorithms
     wsValidator->add(new WorkspaceUnitValidator<>("DeltaE"));
     declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input,wsValidator), "An input workspace.");
     declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output), "An output workspace.");
-    declareProperty(new PropertyWithValue<string>("Temperature","",Direction::Input),"SampleLog variable name that contains the temperature or a number");
+    declareProperty(new PropertyWithValue<string>("Temperature","",Direction::Input),"SampleLog variable name that contains the temperature, or a number");
   }
 
   //----------------------------------------------------------------------------------------------
@@ -78,10 +87,19 @@ namespace Algorithms
 
      std::string Tstring=getProperty("Temperature");
      double Temp;
-     if (inputWS->run().hasProperty(Tstring))
-       Temp=(dynamic_cast<Kernel::TimeSeriesProperty<double> *>(inputWS->run().getProperty(Tstring)))->getStatistics().mean;
-     else
-       Temp=boost::lexical_cast<double>(Tstring);
+     try
+     {
+        if (inputWS->run().hasProperty(Tstring))
+          Temp=(dynamic_cast<Kernel::TimeSeriesProperty<double> *>(inputWS->run().getProperty(Tstring)))->getStatistics().mean;
+        else
+          Temp=boost::lexical_cast<double>(Tstring);
+     }
+     catch (...)
+     {
+       Tstring+= " is not a valid log, nor is it a number";
+       throw std::invalid_argument(Tstring);
+     }
+
      double oneOverT=PhysicalConstants::meVtoKelvin/Temp;
      // Run the exponential correction algorithm explicitly to enable progress reporting
      IAlgorithm_sptr expcor = createSubAlgorithm("OneMinusExponentialCor",0.0,1.0);
