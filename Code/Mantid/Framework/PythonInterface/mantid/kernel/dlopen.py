1cca624b28222c51d5144fb40d2f9eac2490df82
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-01-09 09:46:22 +0000

    Refs #4333. Do the same dlopen stuff for the new-style API.
    This is required so that the old one can be imported second on Linux/Mac.

--- Code/Mantid/Framework/PythonInterface/mantid/kernel/dlopen.py
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/dlopen.py
@@ -36,7 +36,7 @@ os.environ[_var] = os.environ['MANTIDPATH'] + _sep + _oldpath
 # Public api
 #######################################################################
         
-def setup_dlopen():
+def setup_dlopen(library, depends=[]):
     """Set the flags for a call to import a shared library
     such that all symbols are imported.
     
@@ -48,23 +48,62 @@ def setup_dlopen():
     copy of any singleton, which is not the correct
     behaviour
     
+    Args:
+      library - The path to the library we are opening
+      depends - A list of dependents to open (default=[])
+    
     Returns the original flags
     """
-    if platform.system() != "Linux": return None
+    if os.name == 'nt': return None
     old_flags = sys.getdlopenflags()
-    try:
-        import DLFCN as dynload
-    except:
-        # Try older module
-        try:
-            import dl as dynload
-        except:
-            # If neither is available then this platform is unsupported
-            print "Both the DLFCN and dl modules are unavailable."
-            print "Cannot run Mantid from stand-alone Python on this platform."
-            sys.exit(1)
+
+    import _dlopen
+    dlloader = _dlopen.loadlibrary
+    import subprocess
     
-    sys.setdlopenflags(dynload.RTLD_NOW | dynload.RTLD_GLOBAL)
+    def get_libpath(mainlib, dependency):
+        if platform.system() == 'Linux':
+            cmd = 'ldd %s | grep %s' % (mainlib, dependency)
+            part = 2
+        else:
+            cmd = 'otool -L %s | grep %s' % (mainlib, dependency)
+            part = 0
+        subp = subprocess.Popen(cmd,stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,shell=True)
+        out = subp.communicate()[0]
+        # ldd produces a string that always has 4 columns. The full path
+        # is in the 3rd column
+        return out.split()[part]
+    # stdc++ has to be loaded first or exceptions don't get translated 
+    # properly across bounadries
+    # NeXus has to be loaded as well as there seems to be an issue with
+    # the thread-local storage not being initialized properly unles
+    # it is loaded before other libraries.
+    _bin = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../')
+    library_var = "LD_LIBRARY_PATH"
+    if platform.system() == 'Darwin':
+        library_var = 'DY' + library_var
+    ldpath = os.environ.get(library_var, "")
+    ldpath += ":" + _bin
+    os.environ[library_var] = ldpath
+
+    pythonlib = library
+    dlloader(get_libpath(pythonlib, 'stdc++'))
+    dlloader(get_libpath(pythonlib, 'libNeXus'))
+    for dep in depends:
+        dlloader(get_libpath(pythonlib, dep))
+
+    oldflags = sys.getdlopenflags()
+    if platform.system() == "Darwin":
+        try:
+            import dl
+            RTLD_LOCAL = dl.RTLD_LOCAL
+            RTLD_NOW = dl.RTLD_NOW
+        except ImportError:
+            RTLD_LOCAL = 0x4
+            RTLD_NOW = 0x2
+        sys.setdlopenflags(RTLD_LOCAL|RTLD_NOW)
+    
     return old_flags
     
 def restore_flags(flags):
