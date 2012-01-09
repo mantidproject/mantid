124ed820763b2d0debb183137c02551cf4ef9054
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-01-09 10:08:04 +0000

    Refs #4333. Fix for mac library endings

--- Code/Mantid/Framework/PythonInterface/mantid/geometry/__init__.py
+++ Code/Mantid/Framework/PythonInterface/mantid/geometry/__init__.py
@@ -12,6 +12,6 @@ Defines Python objects that wrap the C++ Geometry namespace.
 from mantid.kernel import dlopen as _dlopen
 import os as _os
 clib = _os.path.join(_os.path.dirname(__file__), '_geometry.so')
-flags = _dlopen.setup_dlopen(clib, ['libMantidKernel.so', 'libMantidGeometry.so']) # Ensure the library is open with the correct flags
+flags = _dlopen.setup_dlopen(clib, ['libMantidKernel', 'libMantidGeometry']) # Ensure the library is open with the correct flags
 from _geometry import *
 _dlopen.restore_flags(flags)
