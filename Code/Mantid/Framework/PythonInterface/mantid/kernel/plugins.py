84abd6b4fe4784adfe449125ad28c3be15131113
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-01-09 14:30:09 +0000

    Refs #4423. Ensure Python algorithms are always loaded.
    They use the old API at the moment so ensure they are loaded
    regardless.

--- Code/Mantid/Framework/PythonInterface/mantid/kernel/plugins.py
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/plugins.py
@@ -78,4 +78,96 @@ def load_from_dir(directory):
             loaded.extend(load(filename))
             
     return loaded
+    
+###############################################################################
+# Backwards compatible loader for PythonAlgorithms written in old style
+# 
+# This will be removed when the old-style API is removed.
+#
+###############################################################################
+class PyAlgLoader(object):
+
+    __CHECKLINES__ = 100
+    
+    def __init__(self):
+        self._logger = Logger.get("PyAlgLoader")
+
+    def load_modules(self, refresh=False):
+        """
+        Import Python modules containing Python algorithms
+        """
+        from mantid.kernel import ConfigService
+        dir_list = ConfigService.Instance()["pythonalgorithms.directories"].split(';')
+       
+        # Check defined Python algorithm directories and load any modules
+        changes = False
+        for path in dir_list:
+            if path == '':
+                continue
+            if self._importAlgorithms(path, refresh):
+                changes = True
+
+#
+# ------- Private methods --------------
+#
+    def _importAlgorithms(self, path, refresh):
+        # Make sure the directory doesn't contain a trailing slash
+        path = path.rstrip("/").rstrip("\\")
+        try:
+            files = _os.listdir(path)
+        except(OSError):
+            return False
+        changes = False
+        
+        def _process_file(file_path, modname):
+            import sys
+            pyext = '.py'
+            if not modname.endswith(pyext):
+                return
+            original = _os.path.join(file_path, modname)
+            modname = modname[:-len(pyext)]
+            compiled = _os.path.join(file_path, modname + '.pyc')
+            if modname in sys.modules and \
+               _os.path.exists(compiled) and \
+               _os.path.getmtime(compiled) >= _os.path.getmtime(original):
+                return
+            try:               
+                if self._containsPyAlgorithm(original):
+                    # Temporarily insert into path
+                    sys.path.insert(0, file_path)
+                    if modname in sys.modules:
+                        reload(sys.modules[modname])
+                    else:
+                        __import__(modname)
+                    changes = True
+                    # Cleanup system path
+                    del sys.path[0]
+            except(StandardError), exp:
+                self._logger.warning('Error: Importing module "%s" failed". %s' % (modname,str(exp)))
+            except:
+                self._logger.warning('Error: Unknown error on Python algorithm module import. "%s" skipped' % modname)
+
+        # Find sub-directories     
+        for root, dirs, files in _os.walk(path):
+            for f in files:
+                _process_file(root, f)
+            
+        return changes
+
+    def _containsPyAlgorithm(self, modfilename):
+        file = open(modfilename,'r')
+        line_count = 0
+        alg_found = False
+        while line_count < self.__CHECKLINES__:
+            line = file.readline()
+            # EOF
+            if line == '':
+                alg_found = False
+                break
+            if line.rfind('PythonAlgorithm') >= 0:
+                alg_found = True
+                break
+            line_count += 1
+        file.close()
+        return alg_found
    
--- Code/Mantid/MantidPlot/mantidplotrc.py
+++ Code/Mantid/MantidPlot/mantidplotrc.py
@@ -42,6 +42,10 @@ if __name__ == '__main__':
     elif _api == 2:
         from mantid import *
         from mantid.simpleapi import *
+        from mantid.kernel.plugins import PyAlgLoader
+        # Make sure the Python algorithms are loaded as these all have to use the old API at the moment
+        loader = PyAlgLoader()
+        loader.load_modules()
     else:
         raise Runtime("Unknown Python API version requested: %d" % _api)
 
