f3d8eb99b4144996cc11db086a78d3ff1b01e54a
Janik Zikovsky <zikovskyjl@ornl.gov>
2012-01-10 11:10:43 -0500

    Refs #4265 make sure eventworkspaces become histos in
    SignalOverError

--- Code/Mantid/Framework/Algorithms/src/SignalOverError.cpp
+++ Code/Mantid/Framework/Algorithms/src/SignalOverError.cpp
@@ -25,6 +25,8 @@ namespace Algorithms
   SignalOverError::SignalOverError()
   : UnaryOperation()
   {
+    // Flag that EventWorkspaces will become histograms
+    this->useHistogram=true;
   }
     
   //----------------------------------------------------------------------------------------------
