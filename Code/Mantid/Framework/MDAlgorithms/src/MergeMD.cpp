ac3f422c0cdb366a54e50b3e15423a1073933d6a
Janik Zikovsky <zikovskyjl@ornl.gov>
2012-01-17 15:42:36 -0500

    Refs #3867 wiki docs

--- Code/Mantid/Framework/MDAlgorithms/src/MergeMD.cpp
+++ Code/Mantid/Framework/MDAlgorithms/src/MergeMD.cpp
@@ -1,6 +1,13 @@
 /*WIKI*
 
-Merge several [[MDWorkspace]]s into one.
+This algorithm merges several [[MDWorkspace]]s together into one by adding their events together.
+
+The algorithm starts by going through the list of [[MDWorkspace]]s to find the extents
+that fully encompass all input workspaces in each dimension.
+The number and names of dimensions must match for all input workspaces.
+
+The output workspace is created with these dimensions and the box parameters specified above. Then the events
+from each input workspace are appended to the output.
 
 See also: [[MergeMDFiles]], for merging when system memory is too small to keep the entire workspace in memory.
 
