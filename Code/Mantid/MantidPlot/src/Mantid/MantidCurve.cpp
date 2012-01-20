963d6c6ee14f97b0edfe49470aa35ae50760e141
Russell Taylor <taylorrj@ornl.gov>
2012-01-20 13:28:27 -0500

    Revert "Re #4573. Use curve's pen to draw the errors"
    This reverts commit 2850ba62f4e51e7b247e4aec77073335d700b2a7.
    Broke ability to customise error bars on Mantid curves (#2657.)

--- Code/Mantid/MantidPlot/src/Mantid/MantidCurve.cpp
+++ Code/Mantid/MantidPlot/src/Mantid/MantidCurve.cpp
@@ -158,9 +158,8 @@ void MantidCurve::doDraw(QPainter *p,
 
   int xi0 = 0;
   QPen mypen(pen());
-  // why do we want to draw the curve and the errors in different colors?
-  //mypen.setColor(m_errorSettings->color());
-  //mypen.setWidthF(m_errorSettings->width());
+  mypen.setColor(m_errorSettings->color());
+  mypen.setWidthF(m_errorSettings->width());
   p->setPen(mypen);
   const int dx = m_errorSettings->capLength()/2;
   const int dx2 = 2*dx;
