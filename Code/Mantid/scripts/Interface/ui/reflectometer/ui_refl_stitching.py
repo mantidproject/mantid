89c3275f590ca641bb07e60d62e6285e3d98dd92
Mathieu Doucet <doucetm@ornl.gov>
2012-01-23 15:55:54 -0500

    Re #4303 Create REFL menu option in MantidPlot

--- Code/Mantid/scripts/Interface/ui/reflectometer/ui_refl_stitching.py
+++ Code/Mantid/scripts/Interface/ui/reflectometer/ui_refl_stitching.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/reflectometer/refl_stitching.ui'
 #
-# Created: Mon Jan 23 11:49:33 2012
+# Created: Mon Jan 23 15:45:44 2012
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
