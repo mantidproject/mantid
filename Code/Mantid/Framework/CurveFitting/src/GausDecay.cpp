a3b7f222978b90bc089381dc04ceb30d799060ac
Karl Palmen <karl.palmen@stfc.ac.uk>
2012-01-23 16:32:17 +0000

    Added fit function GausDecay re #4413
    Unit test needs to be added.
    
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

---
+++ Code/Mantid/Framework/CurveFitting/src/GausDecay.cpp
@@ -0,0 +1,43 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidCurveFitting/GausDecay.h"
+#include <cmath>
+
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+
+using namespace Kernel;
+using namespace API;
+
+DECLARE_FUNCTION(GausDecay)
+
+void GausDecay::init()
+{
+   declareParameter("A", 0.2); 
+   declareParameter("Sigma", 0.2);  
+}
+
+
+void GausDecay::functionMW(double* out, const double* xValues, const size_t nData)const
+{
+  const double& A = getParameter("A"); 
+  const double& G = getParameter("Sigma");  
+
+  for (size_t i = 0; i < nData; i++) {
+    double x = xValues[i];
+    out[i] = A*exp(-G*G*x*x);
+  } 
+}
+
+void GausDecay::functionDerivMW(Jacobian* out, const double* xValues, const size_t nData)
+{
+  calNumericalDeriv(out, xValues, nData);
+}
+
+
+} // namespace CurveFitting
+} // namespace Mantid
