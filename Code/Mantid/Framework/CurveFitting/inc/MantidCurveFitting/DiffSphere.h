91f360aa425db5ded41292596e3871ef185c8d64
Jose Borreguero <borreguerojm@ornl.gov>
2012-01-25 16:36:53 -0500

    Refs #4394 Updated main fitting funtion
     - lookup tables alpha and linearJlist.
     - numerical interpolation when close to numerical divergence
       for factor J.

--- Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/DiffSphere.h
+++ Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/DiffSphere.h
@@ -36,6 +36,7 @@ namespace Mantid
     Code Documentation is available at: <http://doxygen.mantidproject.org>
     */
 
+<<<<<<< HEAD
   	// simple structure for the constant xnl coefficients
     struct xnlc{
       double x;
@@ -71,6 +72,51 @@ namespace Mantid
       "26.283265  4  6", "26.516603  9  4", "26.552589  2  7", "26.666054  0  8",
       "26.735177 15  2", "26.758685 24  0", "26.837518 12  3");
 
+=======
+  //simple structure for the constant xnl coefficients
+
+  struct xnlc{
+    double x;
+    unsigned n;
+    unsigned l;
+  };
+
+    // List of 98 coefficients sorted by increasing value (F.Volino,Mol. Phys. 41,271-279,1980)
+    // For each coefficient, the triad (coeff, n, l) is defined
+    double xnlist[]={
+       2.081576,  1,  0,  3.342094,  2,  0,  4.493409,  0,  1,  4.514100,  3,  0,
+       5.646704,  4,  0,  5.940370,  1,  1,  6.756456,  5,  0,  7.289932,  2,  1,
+       7.725252,  0,  2,  7.851078,  6,  0,  8.583755,  3,  1,  8.934839,  7,  0,
+       9.205840,  1,  2,  9.840446,  4,  1, 10.010371,  8,  0, 10.613855,  2,  2,
+      10.904122,  0,  3, 11.070207,  5,  1, 11.079418,  9,  0, 11.972730,  3,  2,
+      12.143204, 10,  0, 12.279334,  6,  1, 12.404445,  1,  3, 13.202620, 11,  0,
+      13.295564,  4,  2, 13.472030,  7,  1, 13.846112,  2,  3, 14.066194,  0,  4,
+      14.258341, 12,  0, 14.590552,  5,  2, 14.651263,  8,  1, 15.244514,  3,  3,
+      15.310887, 13,  0, 15.579236,  1,  4, 15.819216,  9,  1, 15.863222,  6,  2,
+      16.360674, 14,  0, 16.609346,  4,  3, 16.977550, 10,  1, 17.042902,  2,  4,
+      17.117506,  7,  2, 17.220755,  0,  5, 17.408034, 15,  0, 17.947180,  5,  3,
+      18.127564, 11,  1, 18.356318,  8,  2, 18.453241, 16,  0, 18.468148,  3,  4,
+      18.742646,  1,  5, 19.262710,  6,  3, 19.270294, 12,  1, 19.496524, 17,  0,
+      19.581889,  9,  2, 19.862424,  4,  4, 20.221857,  2,  5, 20.371303,  0,  6,
+      20.406581, 13,  1, 20.538074, 18,  0, 20.559428,  7,  3, 20.795967, 10,  2,
+      21.231068,  5,  4, 21.537120, 14,  1, 21.578053, 19,  0, 21.666607,  3,  5,
+      21.840012,  8,  3, 21.899697,  1,  6, 21.999955, 11,  2, 22.578058,  6,  4,
+      22.616601, 20,  0, 22.662493, 15,  1, 23.082796,  4,  5, 23.106568,  9,  3,
+      23.194996, 12,  2, 23.390490,  2,  6, 23.519453,  0,  7, 23.653839, 21,  0,
+      23.783192, 16,  1, 23.906450,  7,  4, 24.360789, 10,  3, 24.382038, 13,  2,
+      24.474825,  5,  5, 24.689873, 22,  0, 24.850085,  3,  6, 24.899636, 17,  1,
+      25.052825,  1,  7, 25.218652,  8,  4, 25.561873, 14,  2, 25.604057, 11,  3,
+      25.724794, 23,  0, 25.846084,  6,  5, 26.012188, 18,  1, 26.283265,  4,  6,
+      26.516603,  9,  4, 26.552589,  2,  7, 26.666054,  0,  8, 26.735177, 15,  2,
+      26.758685, 24,  0, 26.837518, 12,  3};
+
+    // simple structure to hold a linear interpolation of factor J around its numerical divergence point
+    struct linearJ{
+      double slope;
+      double intercept;
+    };
+
+>>>>>>> Refs #4394 Updated main fitting funtion
     class DLLExport DiffSphere : public API::ParamFunction, public API::IFunctionMW
     {
     public:
@@ -86,9 +132,22 @@ namespace Mantid
       virtual void functionMW(double* out, const double* xValues, const size_t nData)const;
       virtual void functionDerivMW(API::Jacobian* out, const double* xValues, const size_t nData);
       std::vector<xnlc> xnl; //xnl coefficients
+<<<<<<< HEAD
 
     private:
       void initXnlCoeff(std::vector<std::string> coeffList);
+=======
+      std::vector<double> alpha; //certain coefficients invariant during fitting
+      size_t lmax=8;         //maximum value of l in xnlist
+      size_t ncoeff=98;      //number of coefficients
+      double divZone=0.05;   //linear interpolatio zone around the numerical divergence of factor J
+      std::vector<linearJ> linearJlist;
+
+    private:
+      void initXnlCoeff(double *coeffList);
+      void initAlphaCoeff();
+      void initLinJlist();
+>>>>>>> Refs #4394 Updated main fitting funtion
 
     };
 
--- Code/Mantid/Framework/CurveFitting/src/DiffSphere.cpp
+++ Code/Mantid/Framework/CurveFitting/src/DiffSphere.cpp
@@ -3,6 +3,10 @@
 //----------------------------------------------------------------------
 #include "MantidCurveFitting/DiffSphere.h"
 #include <cmath>
+<<<<<<< HEAD
+=======
+#include <boost/math/special_functions/bessel.hpp>
+>>>>>>> Refs #4394 Updated main fitting funtion
 
 namespace Mantid
 {
@@ -14,6 +18,7 @@ using namespace API;
 
 DECLARE_FUNCTION(DiffSphere)
 
+<<<<<<< HEAD
 DiffSphere::DiffSphere()
 {
   declareParameter("Radius", 1.0);
@@ -69,5 +74,113 @@ void DiffSphere::functionDerivMW(Jacobian* out, const double* xValues, const siz
 }
 
 
+=======
+// initialize class attribute xnl with a list of coefficients in string format
+void DiffSphere::initXnlCoeff(double *coeffList){
+  xnlc coeff;
+  for(size_t i=0; i<ncoeff; i+=3){
+    coeff.x = coeffList[i];                            //value of the coefficient
+    coeff.n = dynamic_cast<unsigned>(coeffList[i+1]);  //corresponding n
+    coeff.l = dynamic_cast<unsigned>(coeffList[i+2]);  //corresponding l
+    }
+  xnl.push_back(coeff);
+}
+
+//initialize a set of coefficients that will remain constant during fitting
+void DiffSphere::initAlphaCoeff(){
+  for(std::vector<xnlc>::const_iterator it=xnl.begin(); it!=xnl.end(); ++it){
+    unsigned l = it->l;
+    double c = it->x * it->x;
+    alpha.push_back( (2*l+1) * ( 6*c/(c-l*(l+1)) ) / M_PI );
+  }
+}
+
+//initialize linear interpolation of factor J around its numerical divergence point a = it->x
+void DiffSphere::initLinJlist(){
+  linearJ abJ;
+  unsigned l;
+  double a,c;
+  double J0,J1;
+  for(std::vector<xnlc>::const_iterator it=xnl.begin(); it!=xnl.end(); ++it){
+    l = it->l;
+    c = it->x * it->x;
+    a = it->x - divZone; //left of the numerical divergence point
+    J0 = ( a*boost::math::sph_bessel(l+1,a)-l*boost::math::sph_bessel(l,a) ) / (a*a - c);
+    a = it->x - divZone; //right of the numerical divergence point
+    J1 = ( a*boost::math::sph_bessel(l+1,a)-l*boost::math::sph_bessel(l,a) ) / (a*a - c);
+    abJ.slope = (J1-J0)/(2*divZone);  //slope of the linear interpolation
+    abJ.intercept = J1 - abJ.slope * (it->x - divZone); //intercept of the linear interpolation
+
+    linearJlist.push_back(abJ); //store the parameters of the linear interpolation for this it->x
+  }
+}
+
+DiffSphere::DiffSphere(){
+  declareParameter("Radius", 1.0, "Sphere radius");
+  declareParameter("Diffusion", 1.0, "Diffusion coefficient, in units of");
+  declareParameter("Q",1.0, "Momentum transfer");
+
+  this->initXnlCoeff(xnlist); // initialize this->xnl with the list of coefficients xnlist
+  this->initAlphaCoeff();     // initialize this->alpha, certain factors constant over the fit
+  this->initLinJlist();       // initialize this->linJlist, linear interpolation around numerical divergence
+}
+
+
+void DiffSphere::functionMW(double* out, const double* xValues, const size_t nData)const{
+  const double& R = getParameter("Radius");
+  const double& D = getParameter("Diffusion");
+  const double& Q = getParameter("Q");
+
+  double a=Q*R, w=D/(R*R); //active parameters, "scaled" radius and diffusion
+
+  //precompute the 2+lmax spherical bessel functions
+  double jl[2+lmax];
+  for(size_t l=0; l<=1+lmax; l++) jl[l] = boost::math::sph_bessel(l,a);
+
+  unsigned l;
+  double c,cw;   //coefficient related
+  double A,J,L;  //factors of the fitting function A_i*J_i(a,x)*L_i(w,x)
+
+  for (size_t i = 0; i < nData; i++){
+
+    double x = xValues[i];
+
+    //loop over all coefficients
+    out[i] = 0.0;
+    std::vector<double>::const_iterator italpha=alpha.begin();
+    std::vector<linearJ>::const_iterator itlinJ=linearJlist.begin();
+    for(std::vector<xnlc>::const_iterator it=xnl.begin(); it!=xnl.end(); ++it){
+
+      //only to make expressions for A, J, and L more readable
+      c  = it->x * it->x;
+      cw = c * w;
+      l  = it->l;
+
+      //compute the three factors A, J, and L that go into each term
+      A = *italpha; //A is independent of parameters a and w, and independent of data x
+
+      /*J is dependent on parameter a and independent of data x
+       * cannot be computed when active parameter a obeys a*a-c=0. Thus for each it->x
+       * we stored J(it->x-divZone) and J(it->x_divZone), and use linear interpolation
+       * */
+      if(a - it->x > divZone ){
+        J = ( a*jl[l+1]-l*jl[l] ) / (a*a - c);
+      }else{
+          J = itlinJ->slope*a + itlinJ->intercept;
+      }
+      J *= J;
+
+      //Lorentzian. L is dependent on parameter w and data x
+      L = cw/(cw*cw+x*x);
+
+      out[i] += A * J * L;
+
+      ++italpha; //retrieve next alpha coefficient
+      ++itlinJ;  //retrieve next linear interpolation
+    }//end of for(std::vector<xnlc>::const_iterator it....
+  }//end of for (size_t i...
+} //end of void DiffSphere::functionMW
+
+>>>>>>> Refs #4394 Updated main fitting funtion
 } // namespace CurveFitting
 } // namespace Mantid
