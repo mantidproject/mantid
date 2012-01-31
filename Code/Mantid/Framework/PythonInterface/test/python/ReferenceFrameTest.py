23c5a1cfd3aab45c8b7c9ac20fcaaf76bd0ac974
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-01-31 14:21:26 +0000

    refs #4680. Fix New Python API Test.

--- Code/Mantid/Framework/PythonInterface/test/python/ReferenceFrameTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/ReferenceFrameTest.py
@@ -2,8 +2,12 @@ import unittest
 from mantid import ReferenceFrame
 
 class ReferenceFrameTest(object):
+    def test_ReferenceFrame_cannot_be_instantiated(self):
+        self.assertFalse(can_be_instantiated(Instrument))
 
-    def test_something(self):
-
+    def test_ReferenceFrame_has_expected_attrs(self):
+        expected_attrs = ["pointingAlongBeam", "pointingUp"]
+        for att in expected_attrs:
+            self.assertTrue(att in attrs)
 if __name__ == '__main__':
     unittest.main()
