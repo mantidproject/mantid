f22713b3c65b315cc0845c81b2b0b4a5d008d0ca
Vickie Lynch <lynchve@ornl.gov>
2012-02-01 08:41:17 -0500

    Refs #4693 tofExtinction algorithm added

---
+++ Code/Mantid/Framework/Crystal/inc/MantidCrystal/tofExtinction.h
@@ -0,0 +1,76 @@
+#ifndef MANTID_CRYSTAL_TOFEXTINCTION_H_
+#define MANTID_CRYSTAL_TOFEXTINCTION_H_
+    
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h" 
+
+namespace Mantid
+{
+namespace Crystal
+{
+
+  /** Save a PeaksWorkspace to a Gsas-style ASCII .hkl file.
+   * 
+   * @author Vickie Lynch, SNS
+   * @date 2012-01-20
+   */
+  const double pc[4][19] =
+  {    {0.9369, 0.9490, 0.9778, 1.0083, 1.0295, 1.0389, 1.0392, 1.0338,
+        1.0261, 1.0180, 1.0107, 1.0046, 0.9997, 0.9957, 0.9929, 0.9909,
+        0.9896, 0.9888, 0.9886},
+       {2.1217, 2.0149, 1.7559, 1.4739, 1.2669, 1.1606, 1.1382, 1.1724,
+        1.2328, 1.3032, 1.3706, 1.4300, 1.4804, 1.5213, 1.5524, 1.5755,
+        1.5913, 1.6005, 1.6033},
+       {-0.1304, 0.0423, 0.4664, 0.9427, 1.3112, 1.5201, 1.5844, 1.5411,
+        1.4370, 1.2998, 1.1543, 1.0131, 0.8820, 0.7670, 0.6712, 0.5951,
+        0.5398, 0.5063, 0.4955},
+       {1.1717, 1.0872, 0.8715, 0.6068, 0.3643, 0.1757, 0.0446, -0.0375,
+       -0.0853, -0.1088, -0.1176, -0.1177, -0.1123, -0.1051, -0.0978,
+       -0.0914, -0.0868, -0.0840, -0.0833}};
+  const double MAX_WAVELENGTH = 50.0;    // max in lamda_weight table
+
+  const double STEPS_PER_ANGSTROM = 100;  // resolution of lamda table
+
+  const int NUM_WAVELENGTHS = std::ceil( MAX_WAVELENGTH * STEPS_PER_ANGSTROM);
+
+  const double radtodeg_half = 180.0/M_PI/2.;
+
+
+  class DLLExport tofExtinction  : public API::Algorithm
+  {
+  public:
+    tofExtinction();
+    ~tofExtinction();
+    
+    /// Algorithm's name for identification 
+    virtual const std::string name() const { return "tofExtinction";};
+    /// Algorithm's version for identification 
+    virtual int version() const { return 1;};
+    /// Algorithm's category for identification
+    virtual const std::string category() const { return "Crystal;DataHandling\\Text";}
+    
+  private:
+    /// Sets documentation strings for this algorithm
+    virtual void initDocs();
+    /// Initialise the properties
+    void init();
+    /// Run the algorithm
+    void exec();
+    double getEg(double mosaic);
+    double getXqt(double Eg, double cellV, double wl, double twoth, double tbar, double fsq);
+    double getTypeIZachariasen(double Xqt);
+    double getTypeIGaussian(double Xqt, double twoth);
+    double getTypeILorentzian(double Xqt, double twoth);
+    double getRg(double Eg, double r_crystallite, double wl, double twoth);
+    double getXqtII(double Rg, double cellV, double wl, double twoth, double tbar, double fsq);
+    double getTypeIIZachariasen(double XqtII);
+    double getTypeIIGaussian(double XqtII, double twoth);
+    double getTypeIILorentzian(double XqtII, double twoth);
+    double absor_sphere(double& twoth, double& wl, double& tbar) ;
+  };
+
+
+} // namespace Mantid
+} // namespace Crystal
+
+#endif  /* MANTID_CRYSTAL_TOFEXTINCTION_H_ */
---
+++ Code/Mantid/Framework/Crystal/src/tofExtinction.cpp
@@ -0,0 +1,312 @@
+/*WIKI* 
+
+
+*WIKI*/
+#include "MantidAPI/FileProperty.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidCrystal/tofExtinction.h"
+#include "MantidDataObjects/Peak.h"
+#include "MantidDataObjects/PeaksWorkspace.h"
+#include "MantidGeometry/Instrument/RectangularDetector.h"
+#include "MantidKernel/Strings.h"
+#include "MantidKernel/System.h"
+#include "MantidKernel/Utils.h"
+#include "MantidKernel/V3D.h"
+#include "MantidKernel/Statistics.h"
+#include <boost/math/special_functions/fpclassify.hpp>
+#include "MantidDataObjects/TableWorkspace.h"
+#include "MantidAPI/TableRow.h"
+#include <fstream>
+
+using namespace Mantid::Geometry;
+using namespace Mantid::DataObjects;
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+namespace Mantid
+{
+namespace Crystal
+{
+
+  // Register the algorithm into the AlgorithmFactory
+  DECLARE_ALGORITHM(tofExtinction)
+
+
+  //----------------------------------------------------------------------------------------------
+  /** Constructor
+   */
+  tofExtinction::tofExtinction()
+  {
+  }
+    
+  //----------------------------------------------------------------------------------------------
+  /** Destructor
+   */
+  tofExtinction::~tofExtinction()
+  {
+  }
+  
+
+  //----------------------------------------------------------------------------------------------
+  /// Sets documentation strings for this algorithm
+  void tofExtinction::initDocs()
+  {
+    this->setWikiSummary("Sorts a PeaksWorkspace by HKL.");
+    this->setOptionalMessage("Sorts a PeaksWorkspace by HKL.");
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Initialize the algorithm's properties.
+   */
+  void tofExtinction::init()
+  {
+    declareProperty(new WorkspaceProperty<PeaksWorkspace>("InputWorkspace","",Direction::InOut),
+        "An input PeaksWorkspace with an instrument.");
+    declareProperty(new WorkspaceProperty<PeaksWorkspace>("OutputWorkspace","",Direction::Output));
+    BoundedValidator<double> *mustBePositive = new BoundedValidator<double> ();
+    mustBePositive->setLower(0.0);
+    declareProperty("LinearScatteringCoef", -1.0, mustBePositive,
+      "Linear scattering coefficient in 1/cm");
+    declareProperty("LinearAbsorptionCoef", -1.0, mustBePositive->clone(),
+      "Linear absorption coefficient at 1.8 Angstroms in 1/cm");
+    declareProperty("Radius", 0.10, "Radius of spherical crystal in cm");
+    declareProperty("Mosaic", 0.262, "Mosaic Spread (FWHM) (Degrees)");
+    declareProperty("Cell", 255.0, "Unit Cell Volume (Angstroms^3)");
+    declareProperty("RCrystallite", 6.0, "Becker-Coppens Crystallite Radius (micron)");
+    declareProperty("MinD", 0.40, "Minimum d-spacing (Angstroms)");
+    declareProperty("ScaleFactor", 1.2, "Multiply FSQ and sig(FSQ) by scaleFactor");
+    declareProperty("MinWL", 0.45, "Minimum wavelength (Angstroms)");
+    declareProperty("MaxWL", 3.6, "Maximum wavelength (Angstroms)");
+
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Execute the algorithm.
+   */
+  void tofExtinction::exec()
+  {
+
+    PeaksWorkspace_sptr peaksW = getProperty("InputWorkspace");
+
+    int NumberPeaks = peaksW->getNumberPeaks();
+    double mosaic = getProperty("Mosaic");
+    double cell = getProperty("Cell");
+    double r_crystallite = getProperty("RCrystallite");
+    double dMin = getProperty("MinD");
+    double scaleFactor = getProperty("ScaleFactor");
+    double wlMin = getProperty("MinWL");
+    double wlMax = getProperty("MaxWL");
+    double Eg = getEg(mosaic); // defined by Zachariasen, W. H. (1967). Acta Cryst. A23, 558
+    for (int i = 0; i < NumberPeaks; i++)
+    {
+      Peak & peak1 = peaksW->getPeaks()[i];
+      double h = peak1.getH();
+      double k = peak1.getK();
+      double l = peak1.getL();
+      double fsq = peak1.getIntensity()*scaleFactor;
+      double sigfsq = peak1.getSigmaIntensity()*scaleFactor;
+      double wl = peak1.getWavelength();
+      double dsp = peak1.getDSpacing();
+      double twoth = peak1.getScattering();
+      double tbar = 0.0;
+      double transmission = absor_sphere(twoth, wl, tbar);
+      std::cout << twoth<<"  "<<wl <<"  "<<tbar<<"\n";
+      //if(dsp < dMin || wl < wlMin || wl > wlMax) continue;
+      // Extinction Correction
+      // Use measured Fo_squared in first iteration
+      // y_corr = extinctionCorrection(mosaic, wl, fsq, tbar, twoth, cell, r_crystallite)
+
+      double Xqt = getXqt(Eg, cell, wl, twoth, tbar, fsq);
+
+      double y_corr = getTypeIZachariasen(Xqt);
+      //double y_corrI_G =getTypeIGaussian(Xqt, twoth);
+      //double y_corrI_L = getTypeILorentzian(Xqt, twoth);
+
+      // Test for Type-II
+      //double Rg = getRg(Eg, r_crystallite, wl, twoth); // defined by Zachariasen, W. H. (1967). Acta Cryst. A23, 558
+      //double XqtII = getXqtII(Rg, cell, wl, twoth, tbar, fsq);
+      //double y_corrII_Z = getTypeIIZachariasen(XqtII);
+      //double y_corrII_G = getTypeIIGaussian(XqtII, twoth);
+      //double y_corrII_L = getTypeIILorentzian(XqtII, twoth);
+      // Apply correction to fsq with Type-I Z for testing
+      //double y_corr = y_corrI_Z;
+      double fsq_ys = fsq * y_corr;
+      double sigfsq_ys = sigfsq * y_corr;
+      sigfsq_ys = std::sqrt(1+sigfsq_ys*sigfsq_ys+std::pow(0.005*sigfsq_ys,2));   // Ad a constant 1 for background and 0.5% of Fsq for instrument error
+
+      peak1.setIntensity(fsq_ys);
+      peak1.setSigmaIntensity(sigfsq_ys);
+
+      // output reflection to log file and to hkl file
+      /*printf('%4d%4d%4d%8.2f%8.2f%4d%8.4f%7.4f%7d%7d%7.4f%4d%9.5f%9.5f%9.5f%9.5f%9.5f%9.5f%9.5f%9.5f%9.5f\n' 
+                , (h, k, l, fsq_ys, sigfsq_ys, hstnum, wl, tbar, curhst, seqnum, transmission, dn, twoth, dsp, 
+                y_corr, y_corrI_Z, y_corrI_G, y_corrI_L, y_corrII_Z, y_corrII_G, y_corrII_L));
+
+        // last record all zeros for shelx
+        std::string lastLine = '   0   0   0    0.00    0.00   0  0.0000 0.0000      0      0 0.0000   0  0.0000   0.00000  0.00000';
+
+        printf(lastLine);*/
+
+    }
+    setProperty("OutputWorkspace",peaksW);
+
+  }
+  double tofExtinction::getEg(double mosaic)
+  {
+        double Eg = 2.0*std::sqrt(log(2)/(2*M_PI))/(mosaic*M_PI/180.0);
+        return Eg;
+  }
+;
+  double tofExtinction::getXqt(double Eg, double cellV, double wl, double twoth, double tbar, double fsq)
+  {
+        // Xqt calculated from measured Fsqr;
+        double beta = Eg / std::pow(cellV,2) * std::pow(wl,4) / std::pow((std::sin(twoth/2)),2) * tbar * fsq/10;
+        double Xqt = std::pow(beta,2) + beta * std::sqrt(std::pow(beta,2) + 1);
+        return Xqt;
+  }
+  double tofExtinction::getTypeIZachariasen(double Xqt)
+  {
+        // TYPE-I, Zachariasen, W. H. (1967). Acta Cryst. A23, 558 ;
+        double y_ext = std::sqrt(1 + 2 * Xqt);
+        return y_ext;
+  }
+;
+  double tofExtinction::getTypeIGaussian(double Xqt, double twoth)
+  {
+        // Type-I, Gaussian, Becker, P. J. & Coppens, P. (1974). Acta Cryst. A30, 129;
+        double y_ext =std::sqrt(1 + 2*Xqt + (0.58 + 0.48*std::cos(twoth) + 0.24*std::pow((std::cos(twoth)),2))*std::pow(Xqt,2)/(1 + (0.02 - 0.025*std::cos(twoth))*Xqt));
+        return y_ext;
+  }
+;
+  double tofExtinction::getTypeILorentzian(double Xqt, double twoth)
+  {
+        //TYPE-I Lorentzian, Becker, P. J. & Coppens, P. (1974). Acta Cryst. A30, 129;
+        double y_ext;
+        if (twoth < M_PI)
+             y_ext =std::sqrt(1+2*Xqt+(0.025+0.285*std::cos(twoth))*std::pow(Xqt,2)/(1 + 0.15*Xqt-0.2*std::pow((0.75-std::cos(twoth)),2)*Xqt));
+        else
+             y_ext =std::sqrt(1+2*Xqt+(0.025+0.285*std::cos(twoth))*std::pow(Xqt,2)/(1-0.45*Xqt*std::cos(twoth)));
+        return y_ext;
+  }
+;
+        // Type-II extinction correction;
+  double tofExtinction::getRg(double Eg, double r_crystallite, double wl, double twoth)
+  {
+        double r = r_crystallite; // micron
+        // Two-theta dependence by Becker & Coppens, Acta Cryst A 30, 129 (1974)
+        // The factor is std::pow((std::sin(twoth/2)/wl),2) for tof neutron 
+        double Es = r*std::pow((1000.0*std::sin(twoth/2)/wl),2);
+        double Rg = Es/std::sqrt(1+std::pow((Es/Eg),2));
+        return Rg;
+  }
+;
+  double tofExtinction::getXqtII(double Rg, double cellV, double wl, double twoth, double tbar, double fsq)
+  {
+        double betaII = Rg / std::pow(cellV,2) * std::pow(wl,4) / std::pow((std::sin(twoth/2)),2) * tbar * fsq/10;
+        double XqtII = std::pow(betaII,2) + betaII * std::sqrt(std::pow(betaII,2) + 1);
+        return XqtII;
+  }
+;
+  double tofExtinction::getTypeIIZachariasen(double XqtII)
+  {
+        // TYPE-II, Zachariasen, W. H. (1967). Acta Cryst. A23, 558 ;
+        double y_ext_II = std::sqrt(1 + 2 * XqtII);
+        return y_ext_II;
+  }
+;
+  double tofExtinction::getTypeIIGaussian(double XqtII, double twoth)
+  {
+        //Becker, P. J. & Coppens, P. (1974). Acta Cryst. A30, 129;
+        double y_ext_II =std::sqrt(1 + 2*XqtII + (0.58 + 0.48*std::cos(twoth) + 0.24*std::pow((std::cos(twoth)),2))*std::pow(XqtII,2)/(1 + (0.02 - 0.025*std::cos(twoth))*XqtII));
+        return y_ext_II;
+  }
+;
+  double tofExtinction::getTypeIILorentzian(double XqtII, double twoth)
+  {
+        //TYPE-II Lorentzian, Becker, P. J. & Coppens, P. (1974). Acta Cryst. A30, 129;
+        double y_ext_II;
+        if (twoth < M_PI)
+             y_ext_II =std::sqrt(1+2*XqtII+(0.025+0.285*std::cos(twoth))*std::pow(XqtII,2)/(1 + 0.15*XqtII-0.2*std::pow((0.75-std::cos(twoth)),2)*XqtII));
+        else
+             y_ext_II =std::sqrt(1+2*XqtII+(0.025+0.285*std::cos(twoth))*std::pow(XqtII,2)/(1-0.45*XqtII*std::cos(twoth)));
+        return y_ext_II;
+  }
+ /**
+  *       function to calculate a spherical absorption correction
+  *       and tbar. based on values in:
+  *
+  *       c. w. dwiggins, jr., acta cryst. a31, 395 (1975).
+  *
+  *       in this paper, a is the transmission and a* = 1/a is
+  *       the absorption correction.
+  *
+  *       input are the smu (scattering) and amu (absorption at 1.8 ang.)
+  *       linear absorption coefficients, the radius r of the sample
+  *       the theta angle and wavelength.
+  *       the absorption (absn) and tbar are returned.
+  *
+  *       a. j. schultz, june, 2008
+  */
+  double tofExtinction::absor_sphere(double& twoth, double& wl, double& tbar)
+  {
+    int i;
+    double mu, mur;         //mu is the linear absorption coefficient,
+                            //r is the radius of the spherical sample.
+    double theta,astar1,astar2,frac,astar;
+    double trans;
+
+//  For each of the 19 theta values in dwiggins (theta = 0.0 to 90.0
+//  in steps of 5.0 deg.), the astar values vs.mur were fit to a third
+//  order polynomial in excel. these values are given in the static array
+//  pc[][]
+
+    double smu = getProperty("LinearScatteringCoef");
+    double amu = getProperty("LinearAbsorptionCoef");
+    double radius = getProperty("Radius");
+    mu = smu + (amu/1.8f)*wl;
+
+    mur = mu*radius;
+    if (mur < 0. || mur > 2.5)
+    {
+      std::ostringstream s;
+      s << mur;
+      throw std::runtime_error("muR is not in range of Dwiggins' table :" + s.str());
+    }
+
+    theta = twoth*radtodeg_half;
+    if (theta < 0. || theta > 90.)
+    {
+      std::ostringstream s;
+      s << theta;
+      throw std::runtime_error("theta is not in range of Dwiggins' table :" + s.str());
+    }
+
+//  using the polymial coefficients, calulate astar (= 1/transmission) at
+//  theta values below and above the actual theta value.
+
+    i = (int)(theta/5.);
+    astar1 = pc[0][i] + mur * (pc[1][i] + mur * (pc[2][i] + pc[3][i] * mur));
+
+    i = i+1;
+    astar2 = pc[0][i] + mur * (pc[1][i] + mur * (pc[2][i] + pc[3][i] * mur));
+
+//  do a linear interpolation between theta values.
+
+    frac = theta - static_cast<double>(static_cast<int>( theta / 5. )) * 5.;//theta%5.
+    frac = frac/5.;
+
+    astar = astar1*(1-frac) + astar2*frac;       // astar is the correction
+    trans = 1.f/astar;                           // trans is the transmission
+                                                 // trans = exp(-mu*tbar)
+
+//  calculate tbar as defined by coppens.
+    tbar = -(double)std::log(trans)/mu;
+
+    return trans;
+  }
+
+
+
+} // namespace Mantid
+} // namespace Crystal
+
