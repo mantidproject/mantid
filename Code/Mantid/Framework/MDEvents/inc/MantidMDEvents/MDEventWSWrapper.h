2aa3339a681eb512f1b8858e52d7d40a2b741410
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-02-14 13:14:57 +0000

    refs #4401 MDEventWSWrapper made reenterable
    As boost function pointers do funny things when stings supplied as function arguments and when the number of arguments is high, custom vftable has been build with functions, which use 0 or 1 argument. As the result, internal memory for the class was needed.
    
    Reverting to classic old function pointers allows to use multiple function arguments and eliminate the need for internal class memory. This allows to deploy const methods and makes class reenterable (mainly)

--- Code/Mantid/Framework/MDEvents/inc/MantidMDEvents/MDEventWSWrapper.h
+++ Code/Mantid/Framework/MDEvents/inc/MantidMDEvents/MDEventWSWrapper.h
@@ -40,7 +40,13 @@ typedef  std::vector<std::string> Strings;
 
 /// signature for a function which implement void method of this workspace
 class MDEventWSWrapper;
-typedef boost::function<void () > fpVoidMethod;
+/// signature to void templated function 
+typedef void (MDEventWSWrapper::*fpVoidMethod)();
+/// signature for the internal templated function pointer to add data to the existing workspace
+typedef void(MDEventWSWrapper::*fpAddData)(float *,uint16_t *,uint32_t*,coord_t*,size_t)const;
+/// signature for the internal templated function pointer to create workspace
+typedef  void(MDEventWSWrapper::*fpCreateWS)(const Strings &,const Strings &t,const Strings &,
+          const std::vector<double> &, const std::vector<double> &, size_t );
 
 
 class DLLExport MDEventWSWrapper
@@ -56,56 +62,42 @@ public:
     /** function creates empty MD event workspace with given parameters (workspace factory) and stores internal pointer to this workspace for further usage */
     API::IMDEventWorkspace_sptr createEmptyMDWS(const MDWSDescription &WSD);
     /// add the data to the internal workspace. The workspace has to exist and be initiated 
-    void  addMDData(std::vector<float> &sig_err,std::vector<uint16_t> &run_index,std::vector<uint32_t> &det_id,std::vector<coord_t> &Coord,size_t data_size);
+    void  addMDData(std::vector<float> &sig_err,std::vector<uint16_t> &run_index,std::vector<uint32_t> &det_id,std::vector<coord_t> &Coord,size_t data_size)const;
     /// releases the function pointer of the workspace, stored by the class and makes the class instance undefined; 
     void releaseWorkspace();
     /// get access to the internal workspace
     API::IMDEventWorkspace_sptr pWorkspace(){return workspace;}
-    //
+    // should it be moved to the IMDEvents?
     void refreshCentroid(){
-        mdCalCentroid[n_dimensions]();
+        (this->*(mdCalCentroid[n_dimensions]))();
     };
 private:
-    /// maximal nuber of dimensions, currently supported by the class;
+   /// maximal nuber of dimensions, currently supported by the class;
    static const size_t MAX_N_DIM=8;
    /// actual number of dimensions, initiated in current MD workspace 0 if not initated;
    size_t n_dimensions;
    /// logger -> to provide logging, for MD dataset file operations
-    static Mantid::Kernel::Logger& g_log;
+   //static Mantid::Kernel::Logger& g_log;
    /// taret workspace:
    API::IMDEventWorkspace_sptr workspace;
-   
-   /// minimal and maximal values for the workspace dimensions:
-   std::vector<double>      dim_min,dim_max;
-   /// the ID-s for the target workspace dimensions, which allow to address these dimesnions by name
-   Strings targ_dim_ID;
-   /// the names for the target workspace dimensions and properties of input MD workspace
-   Strings targ_dim_names;
-   /// the units of target workspace dimensions and properties of input MD workspace dimensions
-   Strings targ_dim_units;
-   /// helper variables to transfer data between external storage and function, describbed by the boost function pointer (some uncertainty in template instanciation resolved)
-   float    *sig_err;
-   uint16_t *run_index;
-   uint32_t *det_id; 
-   coord_t  *Coord;
-  // how many datas are in the input vectors above
-   size_t data_size;
- 
+  
    /// helper class to generate methaloop on MD workspaces dimensions:
    template< size_t i>
    friend class LOOP;
-
+   ///
    /// vector holding function pointers to the code, creating different number of dimension worspace as function of dimensions number
-   std::vector<fpVoidMethod> wsCreator;
+   std::vector<fpCreateWS> wsCreator;
    /// vector holding function pointers to the code, which adds diffrent dimension number events to the workspace
-   std::vector<fpVoidMethod> mdEvSummator;
-   /// vector holding function pointers to the code, which refreshes centroid
+   std::vector<fpAddData> mdEvSummator;
+   /// vector holding function pointers to the code, which refreshes centroid (could it be moved to IMD?)
    std::vector<fpVoidMethod> mdCalCentroid;
 
-    /// helper function to create empty MDEventWorkspace with nd dimensions 
+  
+   /// helper function to create empty MDEventWorkspace with nd dimensions 
    template<size_t nd>
-    void  createEmptyEventWS(void)
-    {
+   void  createEmptyEventWS(const Strings &targ_dim_names,const Strings &targ_dim_ID,const Strings &targ_dim_units,
+                             const std::vector<double> &dim_min, const std::vector<double> &dim_max, size_t numBins)
+   {
 
            boost::shared_ptr<MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>,nd> > ws = 
            boost::shared_ptr<MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>, nd> >(new MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>, nd>());
@@ -113,32 +105,30 @@ private:
         // Give all the dimensions
         for (size_t d=0; d<nd; d++)
         {
-             Geometry::MDHistoDimension * dim = new Geometry::MDHistoDimension(this->targ_dim_names[d], this->targ_dim_ID[d], this->targ_dim_units[d], 
-                 coord_t(this->dim_min[d]), coord_t(this->dim_max[d]), 10);
-              ws->addDimension(Geometry::MDHistoDimension_sptr(dim));
+             Geometry::MDHistoDimension * dim = new Geometry::MDHistoDimension(targ_dim_names[d], targ_dim_ID[d], targ_dim_units[d], 
+                 coord_t(dim_min[d]), coord_t(dim_max[d]), numBins);
+                 ws->addDimension(Geometry::MDHistoDimension_sptr(dim));
         }
         ws->initialize();
 
       // Build up the box controller
       // bc = ws->getBoxController();
       // Build up the box controller, using the properties in BoxControllerSettingsAlgorithm 
-//     this->setBoxController(bc);
-    // We always want the box to be split (it will reject bad ones)
+     //     this->setBoxController(bc);
+      // We always want the box to be split (it will reject bad ones)
      //ws->splitBox();
         this->workspace = ws;
-    }
+   }
     /// templated by number of dimesnions function to add multidimensional data to the workspace
-    template<size_t nd>
-    void add_MDData()
-    {
+   template<size_t nd>
+   void add_MDData(float *sig_err,uint16_t *run_index,uint32_t* det_id,coord_t* Coord,size_t data_size)const
+   {
   
         MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>,nd> *const pWs = dynamic_cast<MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>,nd> *>(this->workspace.get());
         if(!pWs){
-            g_log.error()<<"MDEventWSWrapper: can not cast  worspace pointer into pointer to proper target workspace\n"; 
+           // g_log.error()<<"MDEventWSWrapper: can not cast  worspace pointer into pointer to proper target workspace\n"; 
             throw(std::bad_cast());
-        }
-      
-
+        }      
         for(size_t i=0;i<data_size;i++){
             pWs->addEvent(MDEvents::MDEvent<nd>(*(sig_err+2*i),*(sig_err+2*i+1),*(run_index+i),*(det_id+i),(Coord+i*nd)));
         }
@@ -150,6 +140,7 @@ private:
         //tp.joinAll();        
  
     }
+   //
   /// helper function to create empty MDEventWorkspace with nd dimensions 
     template<size_t nd>
     void  calc_Centroid(void)
@@ -157,15 +148,19 @@ private:
 
        MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>,nd> *const pWs = dynamic_cast<MDEvents::MDEventWorkspace<MDEvents::MDEvent<nd>,nd> *>(this->workspace.get());
         if(!pWs){
-            g_log.error()<<"MDEventWSWrapper: can not cast  worspace pointer into pointer to proper target workspace\n"; 
+           // g_log.error()<<"MDEventWSWrapper: can not cast  worspace pointer into pointer to proper target workspace\n"; 
             throw(std::bad_cast());
         }
         pWs->getBox()->refreshCentroid(NULL);
     }
- 
-    /// function called if the workspace has not been initated;
-    void throwNotInitiatedError();
-};
+   /// terminator for 0 dimensions, will throw
+   template<>void  calc_Centroid<0>(void);
+   /// terminator for 0 dimensions, will throw
+   template<>void createEmptyEventWS<0>(const Strings &,const Strings &,const Strings &,
+                                        const std::vector<double> &, const std::vector<double> &, size_t );
+   template<>void add_MDData<0>(float *,uint16_t *,uint32_t*,coord_t* ,size_t)const;
+
+ };
 
 
 } // endnamespace MDEvents
--- Code/Mantid/Framework/MDEvents/src/MDEventWSWrapper.cpp
+++ Code/Mantid/Framework/MDEvents/src/MDEventWSWrapper.cpp
@@ -5,7 +5,26 @@ namespace Mantid
 namespace MDEvents
 {
 // logger for loading workspaces  
-   Kernel::Logger& MDEventWSWrapper::g_log =Kernel::Logger::get("MD-Algorithms");
+ //  Kernel::Logger& MDEventWSWrapper::g_log =Kernel::Logger::get("MD-Algorithms");
+
+//
+template<>
+void  MDEventWSWrapper::calc_Centroid<0>(void){
+     //   g_log.error()<<" MDEventWSWrapper:calc_Centroid MDEventWSWrapper class has not been initiated with any number of dimensions\n";
+        throw(std::invalid_argument(" class has not been initiated"));
+}
+   //
+template<>
+void MDEventWSWrapper::createEmptyEventWS<0>(const Strings &,const Strings &,const Strings &,
+                                             const std::vector<double> &, const std::vector<double> &, size_t )
+{
+       throw(std::invalid_argument("MDEventWSWrapper:createEmptyEventWS can not be initated with 0 dimensions"));
+}
+template<>
+void MDEventWSWrapper::add_MDData<0>(float *,uint16_t *,uint32_t*,coord_t* ,size_t)const{
+    ///   g_log.error()<<" MDEvent WSWrapper class has not been initiated with any number of dimensions, can not add anything to 0-dimensional ws\n";
+       throw(std::invalid_argument(" class has not been initiated, can not add data to 0-dimensional workspace"));
+}
 
 
 /**function returns the number of dimensions in current MDEvent workspace or throws if the workspace has not been defined */
@@ -13,7 +32,7 @@ size_t
 MDEventWSWrapper::nDimensions()const
 {
     if(n_dimensions==0){
-        g_log.error()<<" attempt to obtain number of dimensions from not-initated undefined workspace wrapper\n";
+      //  g_log.error()<<" attempt to obtain number of dimensions from not-initated undefined workspace wrapper\n";
         throw(std::invalid_argument("The workspace has not been initated yet"));
     }
     return size_t(n_dimensions);
@@ -23,47 +42,25 @@ API::IMDEventWorkspace_sptr
 MDEventWSWrapper::createEmptyMDWS(const MDWSDescription &WSD)
 {
     if(WSD.n_dims<1||WSD.n_dims>MAX_N_DIM){
-        g_log.error()<< " Number of requested dimensions: "<<WSD.n_dims<<" exceeds the maxumal allowed numed or dimensions: "<<MAX_N_DIM<<std::endl;
+   //     g_log.error()<< " Number of requested dimensions: "<<WSD.n_dims<<" exceeds the maxumal allowed numed or dimensions: "<<MAX_N_DIM<<std::endl;
         throw(std::invalid_argument(" Numer of requested dimensions exceeds maximal allowed number of dimensions"));
     }
     this->n_dimensions = (int)WSD.n_dims;
-    size_t n_dim = WSD.n_dims;
+ 
 
-    // store input dimensions;
-    this->dim_min.resize(n_dim);
-    this->dim_max.resize(n_dim);
-    this->targ_dim_names.resize(n_dim);
-    this->targ_dim_units.resize(n_dim);
-    this->targ_dim_ID.resize(n_dim);
-
-    for(size_t i=0;i<n_dim;i++){
-        this->dim_min[i]=WSD.dim_min[i];
-        this->dim_max[i]=WSD.dim_max[i];
-        this->targ_dim_names[i]= WSD.dim_names[i];
-        this->targ_dim_units[i]= WSD.dim_units[i];
-        this->targ_dim_ID[i]   = WSD.dim_IDs[i];
-    }
-
-    wsCreator[n_dimensions]();
+    (this->*(wsCreator[n_dimensions]))(WSD.dim_names,WSD.dim_units,WSD.dim_IDs,WSD.dim_min,WSD.dim_max,10);
 
     return workspace;
 }
 
 /// method adds the data to the workspace which was initiated before;
 void  
-MDEventWSWrapper::addMDData(std::vector<float> &sig_err,std::vector<uint16_t> &run_index,std::vector<uint32_t> &det_id,std::vector<coord_t> &Coord,size_t data_size)
+MDEventWSWrapper::addMDData(std::vector<float> &sig_err,std::vector<uint16_t> &run_index,std::vector<uint32_t> &det_id,std::vector<coord_t> &Coord,size_t data_size)const
 {
     if(data_size==0)return;
-  // transfer pointers to data:
-   this->sig_err = &sig_err[0];
-   this->run_index=&run_index[0];
-   this->det_id   =&det_id[0]; 
-   this->Coord    =&Coord[0];
-
-   this->data_size = data_size;
 
    // run the actual addition 
-   mdEvSummator[n_dimensions]();
+   (this->*(mdEvSummator[n_dimensions]))(&sig_err[0],&run_index[0],&det_id[0],&Coord[0],data_size);
 
 }
   
@@ -79,21 +76,16 @@ MDEventWSWrapper::releaseWorkspace()
      n_dimensions=0;
 }
 
-void 
-MDEventWSWrapper::throwNotInitiatedError()
-{
-    g_log.error()<<" MDEvent WSWrapper class has not been initiated with any number of dimensions\n";
-    throw(std::invalid_argument(" class has not been initiated"));
-}
+
 //
 template<size_t i>
 class LOOP{
   public:
     static inline void EXEC(MDEventWSWrapper *pH){
             LOOP< i-1 >::EXEC(pH);
-            pH->wsCreator.push_back(boost::bind(std::mem_fun(&MDEventWSWrapper::createEmptyEventWS<i>),pH));
-            pH->mdEvSummator.push_back(boost::bind(std::mem_fun(&MDEventWSWrapper::add_MDData<i>),pH));
-            pH->mdCalCentroid.push_back(boost::bind(std::mem_fun(&MDEventWSWrapper::calc_Centroid<i>),pH));
+            pH->wsCreator[i]    = &MDEventWSWrapper::createEmptyEventWS<i>;
+            pH->mdEvSummator[i] = &MDEventWSWrapper::add_MDData<i>;
+            pH->mdCalCentroid[i]= &MDEventWSWrapper::calc_Centroid<i>;
 
     }
 };
@@ -101,18 +93,17 @@ template<>
 class LOOP<0>{
   public:
     static inline void EXEC(MDEventWSWrapper *pH){           
-            fpVoidMethod fp = (boost::bind(std::mem_fun(&MDEventWSWrapper::throwNotInitiatedError),pH));
-            pH->wsCreator.push_back(fp);
-            pH->mdEvSummator.push_back(fp);
-            pH->mdCalCentroid.push_back(fp);
+            pH->wsCreator[0]    =&MDEventWSWrapper::createEmptyEventWS<0>;
+            pH->mdEvSummator[0] =&MDEventWSWrapper::add_MDData<0>;
+            pH->mdCalCentroid[0]=&MDEventWSWrapper::calc_Centroid<0>;
       }
 };
 
 MDEventWSWrapper::MDEventWSWrapper():n_dimensions(0)
 {
-    wsCreator.reserve(MAX_N_DIM+1);
-    mdEvSummator.reserve(MAX_N_DIM+1);
-    mdCalCentroid.reserve(MAX_N_DIM+1);
+    wsCreator.resize(MAX_N_DIM+1);
+    mdEvSummator.resize(MAX_N_DIM+1);
+    mdCalCentroid.resize(MAX_N_DIM+1);
     LOOP<MAX_N_DIM>::EXEC(this);
 }
 
