8f615e2d60bf31e6c30e72581676730497139eb9
Janik Zikovsky <zikovskyjl@ornl.gov>
2012-02-15 10:00:49 -0500

    Refs #4474 wiki docs

--- Code/Mantid/Framework/Crystal/src/LoadIsawUB.cpp
+++ Code/Mantid/Framework/Crystal/src/LoadIsawUB.cpp
@@ -1,9 +1,13 @@
 /*WIKI* 
 
+Saves the UB matrix in a workspace to an ISAW-style UB matrix ASCII file.
 
+You can use the [[SaveIsawUB]] algorithm to save to this format.
 
-The ISAW UB Matrix file format is: TODO
-
+The matrix saved is the transpose of the UB Matrix.
+The UB matrix maps the column vector (h,k,l ) to the column vector (q'x,q'y,q'z).
+|Q'|=1/dspacing and its coordinates are a right-hand coordinate system where x
+is the beam direction and z is vertically upward. (IPNS convention)
 
 
 *WIKI*/
--- Code/Mantid/Framework/Crystal/src/SaveIsawUB.cpp
+++ Code/Mantid/Framework/Crystal/src/SaveIsawUB.cpp
@@ -1,5 +1,14 @@
 /*WIKI* 
 
+This saves a workspace's UB matrix to an ISAW-style UB matrix text file.
+
+The resulting file can be loaded again into another workspace by using the [[LoadIsawUB]] algorithm.
+
+The matrix saved is the transpose of the UB Matrix.
+The UB matrix maps the column vector (h,k,l ) to the column vector (q'x,q'y,q'z).
+|Q'|=1/dspacing and its coordinates are a right-hand coordinate system where x
+is the beam direction and z is vertically upward. (IPNS convention)
+
 
 *WIKI*/
 #include "MantidAPI/FileProperty.h"
