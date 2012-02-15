943e71063a62fccb766cd0824987cf3b5b06571b
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2012-02-15 14:49:26 -0600

    Update Wiki documentation and parameter documentation.
    Refs #4657

--- Code/Mantid/Framework/Crystal/src/IntegratePeakTimeSlices.cpp
+++ Code/Mantid/Framework/Crystal/src/IntegratePeakTimeSlices.cpp
@@ -1,29 +1,29 @@
 /*WIKI* 
 
 
-This algorithm fits a bivariate normal distribution( plus background) to the 
-data on each time slice. This algorithm only works for peaks on a Rectangular  
-Detector.  The rectangular area used for the fitting is calculated based on 
-the dQ parameter.  A good value for dQ is .1667/largest unit cell length.
+This algorithm fits a bivariate normal distribution( plus background) to the data on each time slice.  The Fit program uses [[BivariateNormal]] for the Fit Function.
 
-The table workspace is also a result. Each line contains information on the fit
-for each good time slice.  The column names( and information) in the table are:
- Time, Channel, Background, Intensity,Mcol,Mrow,SScol,SSrow,SSrc,NCells,
- ChiSqrOverDOF,TotIntensity,BackgroundError,FitIntensityError,ISAWIntensity,
- ISAWIntensityError,TotalBoundary,NBoundaryCells,Start Row,End Row,Start Col,End Col
+The area used for the fitting is calculated based on the dQ parameter.  A good value for dQ is .1667/largest unit cell length. This parameter dictates the size of the area used to approximate the intensity of the peak. The estimate .1667/ max(a,b,c) assumes |Q|=1/d.
+
+The result is returned in this algorithm's output "Intensity" and "SigmaIntensity" properties. The peak object is NOT CHANGED.
 
 
-The final Peak intensity is the sum of the IsawIntensity for each time slice.
-The error is the square root of the sum of squares of the IsawIntensityError values.
+The table workspace is also a result. Each line contains information on the fit for each good time slice.  The column names( and information) in the table are:
+  Time, Channel, Background, Intensity, Mcol, Mrow, SScol,SSrow, SSrc, NCells,
+  ChiSqrOverDOF, TotIntensity, BackgroundError, FitIntensityError, ISAWIntensity,
+   ISAWIntensityError,TotalBoundary, NBoundaryCells, Start Row, End Row,Start Col, and End Col
 
-The columns whose names are  Background, Intensity, Mcol, Mrow, SScol, SSrow, and SSrc
-are the parameters for the BivariateNormal curve fitting function.  
 
-This algorithm has been carefully tweaked to give good results for interior peaks only. 
-Peaks close to the edge of the detector may not give good results.
+The final Peak intensity is the sum of the IsawIntensity for each time slice. The error is the square root of the sum of squares of the IsawIntensityError values.
 
-This Algorithm is also used by the PeakIntegration algorithm when the Fit tag is selected.
+The columns whose names are  Background, Intensity, Mcol, Mrow, SScol, SSrow, and SSrc correspond to the parameters for the BivariateNormal curve fitting function.
+
+This algorithm has been carefully tweaked to give good results for interior peaks only. Peaks close to the edge of the detector may not give good results.
+
+This Algorithm is also used by the [[PeakIntegration]] algorithm when the Fit tag is selected.
+
 *WIKI*/
+
 /*
  * IntegratePeakTimeSlices.cpp
  *
@@ -170,7 +170,7 @@ namespace Mantid
 
     void IntegratePeakTimeSlices::initDocs()
     {
-      this->setWikiSummary("Integrates each time slice around a peak adding the results to the peak object");
+      this->setWikiSummary("Integrates each time slice around a peak ");
       this->setOptionalMessage("The algorithm uses CurveFitting::BivariateNormal for fitting a time slice");
     }
 
@@ -181,7 +181,7 @@ namespace Mantid
           "A 2D workspace with X values of time of flight");
 
       declareProperty(new WorkspaceProperty<TableWorkspace> ("OutputWorkspace", "", Direction::Output),
-          "Name of the output workspace with Log info");
+          "Name of the output table workspace with Log info");
 
       declareProperty(new WorkspaceProperty<PeaksWorkspace> ("Peaks", "", Direction::Input),
           "Workspace of Peaks");
