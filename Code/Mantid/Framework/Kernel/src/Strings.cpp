69f214382969323c5c5320718caa9718b50f400e
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-02-16 17:02:44 +0000

    Refs #4472. Add template instanstiation for uint64_t on 32bit gcc

--- Code/Mantid/Framework/Kernel/src/Strings.cpp
+++ Code/Mantid/Framework/Kernel/src/Strings.cpp
@@ -990,7 +990,7 @@ template MANTID_KERNEL_DLL std::string toString(const float value);
 template MANTID_KERNEL_DLL std::string toString(const int value);
 template MANTID_KERNEL_DLL std::string toString(const uint16_t value);
 template MANTID_KERNEL_DLL std::string toString(const size_t value); // Matches uint64_t on Linux 64 & Win 64
-#if defined(__APPLE__) || ( defined(_WIN32) && !defined(_WIN64) ) // Mac or 42-bit Windows
+#if defined(__APPLE__) || ( defined(_WIN32) && !defined(_WIN64)) || (defined(__GNUC__) && !defined(__LP64__)) // Mac or 32-bit compiler
   template MANTID_KERNEL_DLL std::string toString(const uint64_t value);
 #endif
 template MANTID_KERNEL_DLL std::string toString(const std::string value);
