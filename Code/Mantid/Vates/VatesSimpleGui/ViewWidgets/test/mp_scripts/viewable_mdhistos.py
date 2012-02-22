101e2ebe0fd1d58ab5a690124314e1c944bda455
Michael Reuter <reuterma@ornl.gov>
2012-02-22 16:54:04 -0500

    Refs #4232. Fix script.

--- Code/Mantid/Vates/VatesSimpleGui/ViewWidgets/test/mp_scripts/viewable_mdhistos.py
+++ Code/Mantid/Vates/VatesSimpleGui/ViewWidgets/test/mp_scripts/viewable_mdhistos.py
@@ -3,4 +3,4 @@ LoadMD(Filename='SEQ_MDEW.nxs', OutputWorkspace='SEQ')
 BinMD(InputWorkspace='SEQ', AlignedDim0='[Qh,0,0],0,0.5,20', AlignedDim1='[0.5Qh,0.866Qk,0],-0.75,-0.25,20', AlignedDim2='[0,0,Ql],-0.9,-0.55,20', AlignedDim3='DeltaE,17,22,20', OutputWorkspace='SEQ_4D_rebin')
 # Rebinned 4D->3D MDHistoWorkspace
 BinMD(InputWorkspace='SEQ', AlignedDim0='[Qh,0,0],0,0.5,20', AlignedDim1='DeltaE,17,22,20', AlignedDim2='[0.5Qh,0.866Qk,0],-0.75,-0.25,20', OutputWorkspace='SEQ_3D_rebin')
-BinMD(InputWorkspace='SEQ', AlignedDim0='[Qh,0,0],0,0.5,20', AlignedDim1='DeltaE,17,22,20', AlignedDim2='[0.5Qh,0.866Qk,0],-0.75,-0.25,20', AlignedDimT='[0,0,Ql],-0.9,-0.55,1', OutputWorkspace='SEQ_3D_int')
+BinMD(InputWorkspace='SEQ', AlignedDim0='[Qh,0,0],0,0.5,20', AlignedDim1='DeltaE,17,22,20', AlignedDim2='[0.5Qh,0.866Qk,0],-0.75,-0.25,20', AlignedDim3='[0,0,Ql],-0.9,-0.55,1', OutputWorkspace='SEQ_3D_int')
