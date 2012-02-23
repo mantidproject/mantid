0f77dcad72036f357808462879872f7418edc918
Janik Zikovsky <zikovskyjl@ornl.gov>
2012-02-23 14:47:14 -0500

    Refs #4803: MonitorLiveData runs, but segfaults after a while
    Even when using the TestDataListener.
    Added TestDataListener to build (temporarily).

--- Code/Mantid/Framework/DataHandling/src/MonitorLiveData.cpp
+++ Code/Mantid/Framework/DataHandling/src/MonitorLiveData.cpp
@@ -4,6 +4,8 @@ TODO: Enter a full wiki-markup description of your algorithm here. You can then 
 
 #include "MantidDataHandling/MonitorLiveData.h"
 #include "MantidKernel/System.h"
+#include <unistd.h>
+#include "MantidDataHandling/LoadLiveData.h"
 
 using namespace Mantid::Kernel;
 using namespace Mantid::API;
@@ -64,9 +66,50 @@ namespace DataHandling
    */
   void MonitorLiveData::exec()
   {
+    double UpdateEvery = getProperty("UpdateEvery");
+    if (UpdateEvery <= 0)
+      throw std::runtime_error("UpdateEvery must be > 0");
+
+    // Get the listener (and start listening) as early as possible
+    ILiveListener_sptr listener = this->getLiveListener();
+
     // The last time we called LoadLiveData.
     // Since StartLiveData _just_ called it, use the current time.
     DateAndTime lastTime = DateAndTime::getCurrentTime();
+
+    size_t chunk = 0;
+
+    while (true)
+    {
+      // Keep going until you get cancelled
+      this->interruption_point();
+
+      // Sleep for 50 msec
+      usleep(50000);
+      DateAndTime now = DateAndTime::getCurrentTime();
+      double seconds = DateAndTime::secondsFromDuration( now - lastTime );
+      if (seconds > UpdateEvery)
+      {
+        lastTime = now;
+        g_log.notice() << "Loading live data chunk " << chunk << " at " << now.toFormattedString("%H:%M:%S") << std::endl;
+        // Time to run LoadLiveData again
+        LoadLiveData loadAlg;
+        loadAlg.initialize();
+        // So the output gets put into the ADS
+        loadAlg.setChild(false);
+        // Too much logging
+        loadAlg.setLogging(false);
+        // Copy settings from THIS to LoadAlg
+        loadAlg.copyPropertyValuesFrom(*this);
+        // Give the listener directly to LoadLiveData (don't re-create it)
+        loadAlg.setLiveListener(listener);
+
+        // Run the LoadLiveData
+        loadAlg.executeAsSubAlg();
+
+        chunk++;
+      }
+    }
   }
 
 
