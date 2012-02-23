e1fa073506ea34bbec82d235e8f38ed12a339b4d
Russell Taylor <taylorrj@ornl.gov>
2012-02-23 15:18:41 -0500

    Widen tolerance for lousy win32 box. Re #4812.

--- Code/Mantid/Framework/DataHandling/test/FakeEventDataListenerTest.h
+++ Code/Mantid/Framework/DataHandling/test/FakeEventDataListenerTest.h
@@ -52,7 +52,7 @@ public:
     TS_ASSERT_EQUALS( evbuf->getNumberHistograms(), 2 )
     // Should be around 200 events
     TS_ASSERT_LESS_THAN( evbuf->getNumberEvents(), 25 )
-    TS_ASSERT_LESS_THAN( 15, evbuf->getNumberEvents() )
+    TS_ASSERT_LESS_THAN( 10, evbuf->getNumberEvents() )
 
     Poco::Thread::sleep(100);
     // Call it again, and check things again
@@ -68,7 +68,7 @@ public:
     TS_ASSERT_EQUALS( evbuf->getNumberHistograms(), 2 )
     // Should be around 200 events
     TS_ASSERT_LESS_THAN( evbuf->getNumberEvents(), 25 )
-    TS_ASSERT_LESS_THAN( 15, evbuf->getNumberEvents() )
+    TS_ASSERT_LESS_THAN( 10, evbuf->getNumberEvents() )
   }
 
 private:
