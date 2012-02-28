d8b2176e3e738ae8071143bb62c4a26be8d42711
Wenduo Zhou <zhouw@ornl.gov>
2012-02-28 10:17:07 -0500

    Add python algorithm InvertMask.  Refs #4757.

---
+++ Code/Mantid/Framework/PythonAPI/PythonAlgorithms/InvertMask.py
@@ -0,0 +1,51 @@
+"""*WIKI* 
+
+This algorithm will invert a masking workspace.  It is just a 'NOT' operation on SpecialWorkspace2D.
+
+*WIKI*"""
+
+
+from MantidFramework import *
+from mantidsimple import *
+import os
+
+class InvertMask(PythonAlgorithm):
+    """ Invert a masking workspace.
+    """
+   
+    def category(self):
+        return "Diagnostic;PythonAlgorithms"
+
+    def name(self):
+        return "InvertMask"
+
+    def PyInit(self):
+        self.declareWorkspaceProperty("InputWorkspace","", Direction=Direction.Input,
+                Description="Masking Workspace (SpecialWorkspace2D) to be inverted")
+        self.declareWorkspaceProperty("OutputWorkspace", "", Direction=Direction.Output, 
+                Description="Output Masking Workspace as the inverted one of the input workspace.")
+      
+      
+    def PyExec(self):
+        """ Main body of execution
+        """
+        # 1. get parameter values
+        wsInputName = self.getPropertyValue("InputWorkspace")
+        wsOutputName = self.getPropertyValue("OutputWorkspace")
+
+        wsIn = mtd[wsInputName]
+        if wsIn is None:
+            raise NotImplementedError("Input Workspace does not exist")
+
+        # 2. Invert
+        BinaryOperateMasks(InputWorkspace1=wsInputName, OperationType="NOT",
+                OutputWorkspace=wsOutputName)
+
+        # 3. Set result
+        wsOut = mtd[wsOutputName]
+        self.setProperty("OutputWorkspace",wsOut)
+
+        return
+        
+        
+mtd.registerPyAlgorithm(InvertMask())
