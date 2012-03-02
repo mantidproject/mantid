065f674fe155631610a7d5e141e5c1c859c3d02d
Michael Reuter <reuterma@ornl.gov>
2012-03-02 11:46:31 -0500

    Refs #4232. Adding test for no SplatterPlot for MDHW.

---
+++ Code/Mantid/Vates/VatesSimpleGui/ViewWidgets/test/suite_MDHistoWorkspace/tst_MDHW_Cant_Switch_to_SplatterPlot/test.py
@@ -0,0 +1,12 @@
+def main():
+    source(findFile("scripts", "test_helpers.py"))
+    source(findFile("scripts", "common_checks.py"))
+    startApplication("MantidPlot")
+    run_script("mdhistos_from_3D.py")
+    
+    get_workspace("TOPAZ_3680_3D")
+    activate_vsi()
+    check_mode_button_state("splatterPlot", False, "SplatterPlot disabled for MDHW")
+    close_vsi()
+    
+    quit_program()
