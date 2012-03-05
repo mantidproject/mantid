5d5edc65443eee3c6f61c2f08b6dd90f74f7fcc1
Janik Zikovsky <zikovskyjl@ornl.gov>
2012-03-05 17:39:04 -0500

    Refs #4931 attempt to make a qt designer plugin
    but none of it works for me, including the old ones.

--- Code/Mantid/MantidQt/DesignerPlugins/src/PluginCollectionInterface.cpp
+++ Code/Mantid/MantidQt/DesignerPlugins/src/PluginCollectionInterface.cpp
@@ -8,6 +8,7 @@
 #include "MantidQtDesignerPlugins/WorkspaceSelectorPlugin.h"
 #include "MantidQtDesignerPlugins/FitBrowserPlugin.h"
 #include "MantidQtDesignerPlugins/MuonFitBrowserPlugin.h"
+#include "MantidQtDesignerPlugins/ScriptEditorPlugin.h"
 
 
 Q_EXPORT_PLUGIN2(LIBRARY_NAME, PluginCollectionInterface)
@@ -21,11 +22,15 @@ PluginCollectionInterface::PluginCollectionInterface(QObject *parent) : QObject(
   m_widgets.append(new FileFinderPlugin(this));
   m_widgets.append(new InstrumentSelectorPlugin(this));
   m_widgets.append(new WorkspaceSelectorPlugin(this));
+
+  //  m_widgets.append(new ScriptEditorPlugin(this));
+
   // for now adding the fit browser to qt-designer is in 
   // development stage - only uncomment once they plugin
   // can be can be dragged in without breaking designer
   m_widgets.append(new FitBrowserPlugin(this));
   m_widgets.append(new MuonFitBrowserPlugin(this));
+
 }
 
 /**
