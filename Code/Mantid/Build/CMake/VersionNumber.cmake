3a6040c7a166f030b44ce925db5cc40e632646ba
Russell Taylor <taylorrj@ornl.gov>
2012-03-12 10:42:53 -0400

    Make it possible to fix the patch version number. Re #4952.
    This is instead of taking it from 'git describe'.
    This commit fixes the version number at 2.1.1 - REMEMBER TO COMMENT OUT THE
    LINE ADDED TO VersionNumber.cmake BEFORE MERGING BACK INTO MASTER.
    
    This commit has been cherry-picked from the release-2.0.x branch (c5efe707)
    with just the numbers changed.

--- Code/Mantid/Build/CMake/VersionNumber.cmake
+++ Code/Mantid/Build/CMake/VersionNumber.cmake
@@ -4,3 +4,7 @@ set ( VERSION_MAJOR 2 )
 
 # The minor version number is the iteration number
 set ( VERSION_MINOR 1 )
+
+# UNCOMMENT the next 'set' line to 'force' the patch version number to
+# a value (instead of using the count coming out of 'git describe')
+set ( MtdVersion_WC_LAST_CHANGED_REV 1 )
