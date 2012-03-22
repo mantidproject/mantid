c340ceb85b82e3238ea832f5c422823b23fd4d0c
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-03-22 16:21:28 +0000

    refs #5015. Testing of output metadata.

--- Code/Mantid/Framework/MDEvents/test/ConvertToReflectometryQTest.h
+++ Code/Mantid/Framework/MDEvents/test/ConvertToReflectometryQTest.h
@@ -9,6 +9,7 @@
 #include "MantidGeometry/Instrument/ReferenceFrame.h"
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
 #include "MantidMDEvents/ConvertToReflectometryQ.h"
+#include "MantidAPI/IMDHistoWorkspace.h"
 
 using namespace Mantid;
 using namespace Mantid::MDEvents;
@@ -17,6 +18,22 @@ using namespace Mantid::Geometry;
 
 class ConvertToReflectometryQTest : public CxxTest::TestSuite
 {
+private:
+
+  void doExecute(const std::string& outWSName)
+  {
+    MatrixWorkspace_sptr in_ws = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(10, 10);
+    in_ws->getAxis(0)->setUnit("TOF");
+  
+    ConvertToReflectometryQ alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", in_ws) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+  }
+
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
@@ -31,51 +48,44 @@ public:
     TS_ASSERT_THROWS_NOTHING( alg.initialize() )
     TS_ASSERT( alg.isInitialized() )
   }
-  
-  void test_exec()
+
+  void test_check_output_metadata()
   {
-    using Geometry::ReferenceFrame;
-
-    MatrixWorkspace_sptr in_ws = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(10, 10);
-    in_ws->getAxis(0)->setUnit("TOF");
-    
-    //ConvertUnits convertUnits;
-    //convertUnits.initialize();
-    //convertUnits.setPropertyValue("Target", "TOF");
-    //convertUnits.setPropertyValue("EMode", "Indirect");
-    //convertUnits.setProperty("InputWorkspace", in_ws);
-    //convertUnits.setPropertyValue("OutputWorkspace", convertUnits.getPropertyValue("InputWorkspace"));
-    //convertUnits.execute();
-    //in_ws = convertUnits.getProperty("OutputWorkspace");
-
-    // Name of the output workspace.
     std::string outWSName("ConvertToReflectometryQTest_OutputWS");
-  
-    ConvertToReflectometryQ alg;
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", in_ws) );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
-    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
-    TS_ASSERT( alg.isExecuted() );
-    
-    // Retrieve the workspace from data service. TODO: Change to your desired type
-    Workspace_sptr ws;
-    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
+
+    doExecute(outWSName);
+
+    IMDHistoWorkspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<IMDHistoWorkspace>(outWSName) );
     TS_ASSERT(ws);
-    if (!ws) return;
-    
-    
-    // TODO: Check the results
-    
-    // Remove workspace from the data service.
+
+    //Quick metadata checks associated with the ouput workspace
+    TSM_ASSERT_EQUALS("Wrong number of bins", 100, ws->getNPoints());
+    TSM_ASSERT_EQUALS("Wrong number of dimensions", 2, ws->getNumDims());
+    TSM_ASSERT_EQUALS("Wrong dimension names", "Qx", ws->getDimension(0)->getName());
+    TSM_ASSERT_EQUALS("Wrong dimension names", "Qz", ws->getDimension(1)->getName());
+    TSM_ASSERT_EQUALS("Wrong units on qx", "(Ang^-1)", ws->getDimension(0)->getUnits());
+    TSM_ASSERT_EQUALS("Wrong units on qz", "(Ang^-1)", ws->getDimension(1)->getUnits());
+    TSM_ASSERT_EQUALS("Wrong number of bins in qx", 10, ws->getDimension(0)->getNBins());
+    TSM_ASSERT_EQUALS("Wrong number of bins in qy", 10, ws->getDimension(1)->getNBins());
+
     AnalysisDataService::Instance().remove(outWSName);
   }
-  
-  void test_Something()
-  {
-  }
 
+  void test_check_output_data()
+  {
+    std::string outWSName("ConvertToReflectometryQTest_OutputWS");
+
+    doExecute(outWSName);
+
+    IMDHistoWorkspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<IMDHistoWorkspace>(outWSName) );
+    TS_ASSERT(ws);
+
+    //Detailed checks associated with the output workspace.
+
+    AnalysisDataService::Instance().remove(outWSName);
+  }
 
 };
 
