8aa1b08c0b385aff4990e8611e311fe2ef8400c9
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-03-23 11:42:16 +0000

    refs #5016. Characterise current behaviour.
    Pause while we consider whether its possible or better to implement this algorithm in terms of others.

--- Code/Mantid/Framework/MDEvents/test/ConvertToReflectometryQTest.h
+++ Code/Mantid/Framework/MDEvents/test/ConvertToReflectometryQTest.h
@@ -26,12 +26,12 @@ private:
     in_ws->getAxis(0)->setUnit("TOF");
   
     ConvertToReflectometryQ alg;
+    alg.setRethrows(true);
     TS_ASSERT_THROWS_NOTHING( alg.initialize() )
     TS_ASSERT( alg.isInitialized() )
     TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", in_ws) );
     TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
-    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
-    TS_ASSERT( alg.isExecuted() );
+    TSM_ASSERT_THROWS("Not Implemented Yet", alg.execute(), std::runtime_error );
   }
 
 public:
@@ -49,42 +49,11 @@ public:
     TS_ASSERT( alg.isInitialized() )
   }
 
-  void test_check_output_metadata()
+  void test_check_shouldnt_be_working()
   {
     std::string outWSName("ConvertToReflectometryQTest_OutputWS");
 
     doExecute(outWSName);
-
-    IMDHistoWorkspace_sptr ws;
-    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<IMDHistoWorkspace>(outWSName) );
-    TS_ASSERT(ws);
-
-    //Quick metadata checks associated with the ouput workspace
-    TSM_ASSERT_EQUALS("Wrong number of bins", 100, ws->getNPoints());
-    TSM_ASSERT_EQUALS("Wrong number of dimensions", 2, ws->getNumDims());
-    TSM_ASSERT_EQUALS("Wrong dimension names", "Qx", ws->getDimension(0)->getName());
-    TSM_ASSERT_EQUALS("Wrong dimension names", "Qz", ws->getDimension(1)->getName());
-    TSM_ASSERT_EQUALS("Wrong units on qx", "(Ang^-1)", ws->getDimension(0)->getUnits());
-    TSM_ASSERT_EQUALS("Wrong units on qz", "(Ang^-1)", ws->getDimension(1)->getUnits());
-    TSM_ASSERT_EQUALS("Wrong number of bins in qx", 10, ws->getDimension(0)->getNBins());
-    TSM_ASSERT_EQUALS("Wrong number of bins in qy", 10, ws->getDimension(1)->getNBins());
-
-    AnalysisDataService::Instance().remove(outWSName);
-  }
-
-  void test_check_output_data()
-  {
-    std::string outWSName("ConvertToReflectometryQTest_OutputWS");
-
-    doExecute(outWSName);
-
-    IMDHistoWorkspace_sptr ws;
-    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<IMDHistoWorkspace>(outWSName) );
-    TS_ASSERT(ws);
-
-    //Detailed checks associated with the output workspace.
-
-    AnalysisDataService::Instance().remove(outWSName);
   }
 
 };
