dc300f62c6c9b4392587abf4da239e3bffd914ea
Stuart Campbell <campbellsi@ornl.gov>
2012-03-30 10:55:06 -0400

    Initial implementation of MaskWorkspace. refs #5045
    Functional version - but no specific methods yet.
    Not used anywhere yet.

---
+++ Code/Mantid/Framework/DataObjects/test/MaskWorkspaceTest.h
@@ -0,0 +1,18 @@
+#ifndef MANTID_DATAOBJECTS_MASKWORKSPACETEST_H
+#define MANTID_DATAOBJECTS_MASKWORKSPACETEST_H
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidDataObjects/MaskWorkspace.h"
+
+class MaskWorkspaceTest : public CxxTest::TestSuite
+{
+public:
+
+    void test_Nothing()
+    {
+        //TODO: Test Something!
+    }
+};
+
+#endif // MANTID_DATAOBJECTS_MASKWORKSPACETEST_H
