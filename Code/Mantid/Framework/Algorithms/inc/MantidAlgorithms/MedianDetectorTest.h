cab131caf42b900cf28799a2452b299b7a9a5ca1
Stuart Campbell <campbellsi@ornl.gov>
2012-04-02 13:51:13 -0400

    refs #4472. Updated copyright.

--- Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/MedianDetectorTest.h
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/MedianDetectorTest.h
@@ -36,10 +36,7 @@ namespace Mantid
     <LI> RangeUpper - Stop the integration at the bin before the one that contains this x value (default: the end of each spectrum)</LI>
     </UL>
 
-    @author Steve D Williams, ISIS Facility Rutherford Appleton Laboratory
-    @date 15/06/2009
-
-    Copyright &copy; 2009 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+    Copyright &copy; 2012 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
 
     This file is part of Mantid.
 
