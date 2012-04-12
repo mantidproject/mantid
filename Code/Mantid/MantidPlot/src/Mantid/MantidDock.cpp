10f68123ac7e6a51fc09025c7e95702bed0f7d22
Stuart Campbell <campbellsi@ornl.gov>
2012-04-12 13:50:01 -0400

    refs #5045. Added use of MaskWorkspace.
    Added to Property Workspace lists
    Added to MantidDock

--- Code/Mantid/MantidPlot/src/Mantid/MantidDock.cpp
+++ Code/Mantid/MantidPlot/src/Mantid/MantidDock.cpp
@@ -419,7 +419,8 @@ void MantidDockWidget::populateChildData(QTreeWidgetItem* item)
   // Experiment info data
   ExperimentInfo_sptr experimentInfo_ws = boost::dynamic_pointer_cast<ExperimentInfo>(workspace);
   bool specialWorkspace = false;
-  specialWorkspace = (workspace->id() == "SpecialWorkspace2D" || workspace->id() == "OffsetsWorkspace" || workspace->id() == "GroupingWorkspace");
+  specialWorkspace = (workspace->id() == "SpecialWorkspace2D" || workspace->id() == "MaskWorkspace"
+                      || workspace->id() == "OffsetsWorkspace" || workspace->id() == "GroupingWorkspace");
   if (experimentInfo_ws && (!specialWorkspace))
     populateExperimentInfoData(experimentInfo_ws, item);
 
