dc41b58f81eb0a92f423f2685a2cbc8cd1e82c12
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-04-17 21:38:57 +0100

    Fix export declaration. Refs #4399

--- Code/Mantid/Framework/Kernel/src/IValidator.cpp
+++ Code/Mantid/Framework/Kernel/src/IValidator.cpp
@@ -5,6 +5,6 @@ namespace Mantid
   namespace Kernel
   {
     /// initialize static logger
-    Logger & IValidator::g_log = Logger::get("IValidator");
+    DLLExport Logger & IValidator::g_log = Logger::get("IValidator");
   }
 }
