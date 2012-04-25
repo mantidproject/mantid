f9c4c332110333a7c1aa29cf34f04ccea7d24eee
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-04-25 09:18:28 +0100

    Add Rob's Offspec scripts to the distribution. Refs #4975
    Until the script repository is up & running this is the best way to
    distribute Rob's scripts

---
+++ Code/Mantid/scripts/Reflectometry/offspec/__init__.py
@@ -0,0 +1,7 @@
+"""Defines functions & procedures specific to OFFSPEC at ISIS
+"""
+
+###############################################################################
+# Make all procedures available at package level
+###############################################################################
+from _procedures import *
