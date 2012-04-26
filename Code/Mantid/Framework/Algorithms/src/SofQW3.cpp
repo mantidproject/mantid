5c6f50bf50677c3e2fc1317662d3ffb87ac41603
Michael Reuter <reuterma@ornl.gov>
2012-04-26 12:06:54 -0400

    Refs #5070. Adding wiki docs.

--- Code/Mantid/Framework/Algorithms/src/SofQW3.cpp
+++ Code/Mantid/Framework/Algorithms/src/SofQW3.cpp
@@ -1,12 +1,19 @@
 /*WIKI*
+Converts a 2D workspace from units of spectrum number, energy transfer to the
+intensity as a function of momentum transfer and energy. The rebinning is done
+as a weighted sum of overlapping polygons with fractional area tracking. The
+result is stored in a new workspace type: '''RebinnedOutput'''. The new workspace
+presents the data as the fractional counts divided by the fractional area.
+The biggest consequence of this method is that in places where there are no
+counts and no acceptance (no fractional areas), '''nan'''s will result.
 
-
-
-Converts a 2D workspace from units of spectrum number/energy transfer to the intensity as a function of momentum transfer and energy. The rebinning is done as a weighted  sum of overlapping polygons.
-
-
-
- *WIKI*/
+The algorithm operates in non-PSD mode by default. This means that all azimuthal
+angles and widths are forced to zero. PSD mode will determine the azimuthal
+angles and widths from the instrument geometry. This mode is activated by
+placing the following named parameter in a Parameter file: ''detector-neighbour-offset''.
+The integer value of this parameter should be the number of pixels that separates
+two pixels at the same vertical position in adjacent tubes.
+*WIKI*/
 //----------------------------------------------------------------------
 // Includes
 //----------------------------------------------------------------------
