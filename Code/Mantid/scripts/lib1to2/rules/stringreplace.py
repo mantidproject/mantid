4ca6c33fdf0f02c07caa162ffcaaa8d50106b866
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-05-01 11:58:31 +0100

    Separate rules out in migration package. Refs #5197

---
+++ Code/Mantid/scripts/lib1to2/rules/stringreplace.py
@@ -0,0 +1,44 @@
+"""Defines the rules that simple string replacements that can be applied
+to the whole text
+"""
+import rules
+import re
+
+__STRINGREPLACEMENTS__ = [
+    (re.compile("from MantidFramework import \*"), "from mantid import *"),
+    (re.compile("import MantidFramework"), "import mantid"),
+    (re.compile("from mantidsimple import \*"), "from mantid.simpleapi import *"),
+    (re.compile("from mantidsimple import"), "from mantid.simpleapi import"),
+    (re.compile("import mantidsimple as"), "import mantid.simpleapi as"),
+    (re.compile("import mantidsimple"), "import mantid.simpleapi as simpleapi"),
+    (re.compile("mantidsimple\."), "simpleapi."),   
+    (re.compile("(MantidFramework\.|)(mtd|mantid)\.initiali[z,s]e\(\)"), ""),
+    (re.compile("MantidFramework\."), "mantid."),
+    (re.compile("\.getSampleDetails"), ".getRun"),
+    (re.compile("mtd\.settings"), "config"),
+    (re.compile("mtd\.getConfigProperty"), "config.getString"),
+    (re.compile("(mtd|mantid).sendLogMessage"), "logger.notice")
+]
+
+class SimpleStringReplace(rules.Rules):
+    """
+        Implements any rules that are simply a matter of matching a pattern
+        and replacing it with a fixed string
+    """
+    _rules = None
+    
+    def __init__(self):
+        rules.Rules.__init__(self)
+        self._rules = __STRINGREPLACEMENTS__
+        
+    def apply(self, text):
+        """
+        Returns a replacement string for the input text
+        with the simple string relpacements definined by the regexes 
+            @param text An input string to match
+            @returns A string containing the replacement or the original text
+                     if no replacement was needed
+        """
+        for pattern, replacement in self._rules:
+            text = pattern.sub(replacement, text)            
+        return text
