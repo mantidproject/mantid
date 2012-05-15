80a12b517c462cafaf2fbd2c58e67c6cf2491127
Peter Peterson <petersonpf@ornl.gov>
2012-05-15 16:47:30 -0400

    Refs #5335. Fixed uninitialized variable issue.
    I looked at an old version of the code and copied in an important line
    to the right place.

--- Code/Mantid/Framework/PythonAPI/PythonAlgorithms/SNSPowderReduction.py
+++ Code/Mantid/Framework/PythonAPI/PythonAlgorithms/SNSPowderReduction.py
@@ -543,6 +543,7 @@ class SNSPowderReduction(PythonAlgorithm):
             for temp in samRuns:
                 temp = self._focusChunks(temp, SUFFIX, filterWall, calib, filterLogs,
                                preserveEvents=preserveEvents, normByCurrent=normbycurrent)
+                tempinfo = self._getinfo(temp)
                 if samRun is None:
                     samRun = temp
                     info = tempinfo
