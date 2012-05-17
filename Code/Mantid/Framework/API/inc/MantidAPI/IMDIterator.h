59374f5081591efcd92046a2f71ef066f351d51b
Janik Zikovsky <zikovskyjl@ornl.gov>
2012-05-17 13:56:12 -0400

    Refs #5307 fix SliceViewer python test that failed kinda mysteriously
    In reality the destructor SHOULD be virtual but I did not get it to work.

--- Code/Mantid/Framework/API/inc/MantidAPI/IMDIterator.h
+++ Code/Mantid/Framework/API/inc/MantidAPI/IMDIterator.h
@@ -50,7 +50,7 @@ class IMDWorkspace;
   {
   public:
     IMDIterator();
-    virtual ~IMDIterator(){}
+    ~IMDIterator(){}
 
     void setNormalization(Mantid::API::MDNormalization normalization);
     Mantid::API::MDNormalization getNormalization() const;
