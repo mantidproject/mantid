0303a1488254081fe656daa369d7e7f39d26ea4f
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-05-17 14:25:29 +0100

    refs #5336 TransfModQ compiles

--- Code/Mantid/Framework/MDEvents/inc/MantidMDEvents/MDTransfInterface.h
+++ Code/Mantid/Framework/MDEvents/inc/MantidMDEvents/MDTransfInterface.h
@@ -1,8 +1,6 @@
 #ifndef H_IMD_TRANSFORMATION
 #define H_IMD_TRANSFORMATION
-#include "MantidGeometry/MDGeometry/MDTypes.h"
-#include "MantidKernel/cow_ptr.h"
-#include "ManditMDEvents/MDTransfDEHelper.h"
+
 
 namespace Mantid
 {
@@ -44,19 +42,29 @@ namespace MDEvents
         File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
         Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
-// forvard declaration for a class, which would provide all necessary parameters for the transformation; should be refactored;
+// forvard declaration for a class, which provides all necessary parameters for the workspace
 class ConvToMDEventsBase;
+
+namespace ConvertToMD
+{
+    /* enum describes known eneergy conversion/analysis modes
+    *  It is important to assign enums proper numbers, as direct correspondence between enums and their emodes 
+    *  used by the external units conversion algorithms within the Mantid, so the agreement should be the stame     
+    */
+    enum EModes
+    {
+          Elastic = 0,  //< int emode = 0; Elastic analysis
+          Direct  = 1,  //< emode=1; Direct inelastic analysis mode
+          Indir   = 2,  //< emode=2; InDirect inelastic analysis mode
+          No_DE         //< couples with NoNonentum analysis, means just copying existing data (may be doing units conversion)
+    };
+}
 
 
 class MDTransfInterface
 {
 public:
-    /** the function returns the unit ID for the input units, the particular transformation expects. 
-     if one wants the transformation to be meaningful, Input X-coordinates
-     used by the transformation have to be expressed in the uinits  specified */
-    virtual const std::string usedUnitID()const=0;
-    /**The function returns the name, under which the transformation would be known to user */
-    virtual const std::string transfID()const=0;
+      
     /** Method deployed out of the loop and calculates all variables needed within the loop.
      * In addition it calculates the property-dependant coordinates, which do not depend on workspace
      *
@@ -92,7 +100,7 @@ public:
     virtual bool calcMatrixCoord(const MantidVec& X,size_t i,size_t j,std::vector<coord_t> &Coord)const
     {
        UNUSED_ARG(i);
-       double X_ev =double(0.5*(X[j]+X[j+1])); // ! POSSIBLE FACTORY HERE !!! if the histogram interpolation is different
+       double X_ev =double(0.5*(X[j]+X[j+1])); // ! POSSIBLE FACTORY HERE !!! if this histohram interpolation is different
        return calcMatrixCoord(X_ev,Coord);
     }
   
@@ -106,21 +114,11 @@ public:
     virtual bool calcMatrixCoord(const double & X,std::vector<coord_t> &Coord)const=0;
 
     /** set up transformation and retrieve the pointer to the incorporating class, which runs the transformation and can provide
-      * all variables necessary for the conversion */
+      * all necessary variables necessary for the conversion */
     virtual void initialize(const ConvToMDEventsBase &)=0;
   
-    /** MD transformation can often be used together with energy analysis mode; This function should be overloaded 
-       if the transformation indeed can do the energy conversion */
-    virtual std::vector<std::string> getEmodes()const{return std::vector<std::string>(1,std::string("No dE"));}
-    virtual std::string getEmode(ConvertToMD::EModes Mode)const{UNUSED_ARG(Mode); return std::string("No dE");}
-
-    /** return the number of dimensions, calculated by the transformation from the workspace. This numebr is usually varies from 1 to 4*/
-    virtual int getNMatrixDimensions()const=0;
-
     virtual ~MDTransfInterface(){};
 }; 
-
-typedef boost::shared_ptr<MDTransfInterface> MDTransf_sptr; 
 
 } // End MDAlgorighms namespace
 } // End Mantid namespace
