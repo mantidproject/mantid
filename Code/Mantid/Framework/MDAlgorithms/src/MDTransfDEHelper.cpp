d039d02a237ac66f34042a510e30793d3155ee66
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-05-28 12:28:27 +0100

    refs #5336 Added Energy transfer analysis helper
    which describes possible energy transfer modes, which can be supported by an Q-converter

---
+++ Code/Mantid/Framework/MDAlgorithms/src/MDTransfDEHelper.cpp
@@ -0,0 +1,23 @@
+#include "MantidMDEvents/MDTransfDEHelper.h"
+
+
+namespace Mantid
+{
+namespace MDEvents
+{
+
+MDTransfDEHelper::MDTransfDEHelper()
+EmodesList(ConvertToMD::No_DE,"")
+{
+   EmodesList[ConvertToMD::Elastic]="Elastic";
+   EmodesList[ConvertToMD::Direct] ="Direct";
+   EmodesList[ConvertToMD::Indir]  ="Indirect";
+
+}
+ConvertToMD::Emodes getEmode(const std::string &Mode)const
+{
+    return ConvertToMD::No_DE;
+}
+
+} // endnamespace MDEvents
+} // endnamespace Mantid
