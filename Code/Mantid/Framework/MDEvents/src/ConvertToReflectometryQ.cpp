4f1972b5d24dccfc286d20634b3af60a802c7f2d
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-05-28 12:29:18 +0100

    refs #5016. A different approach

--- Code/Mantid/Framework/MDEvents/src/ConvertToReflectometryQ.cpp
+++ Code/Mantid/Framework/MDEvents/src/ConvertToReflectometryQ.cpp
@@ -12,6 +12,10 @@ TODO: Enter a full wiki-markup description of your algorithm here. You can then 
 #include "MantidAPI/IMDHistoWorkspace.h"
 #include "MantidMDEvents/MDEventWorkspace.h"
 #include "MantidMDEvents/MDHistoWorkspace.h"
+#include "MantidKernel/EnabledWhenProperty.h"
+#include "MantidKernel/TimeSeriesProperty.h"
+#include "MantidKernel/ListValidator.h"
+#include "MantidAPI/FrameworkManager.h"
 
 #include <boost/shared_ptr.hpp>
 
@@ -59,8 +63,8 @@ namespace MDEvents
   /// Sets documentation strings for this algorithm
   void ConvertToReflectometryQ::initDocs()
   {
-    this->setWikiSummary("TODO: Enter a quick description of your algorithm.");
-    this->setOptionalMessage("TODO: Enter a quick description of your algorithm.");
+    this->setWikiSummary("Transforms from real-space to Q or momentum space for reflectometry workspaces");
+    this->setOptionalMessage("Transforms from real-space to Q or momentum space for reflectometry workspaces");
   }
 
   //----------------------------------------------------------------------------------------------
@@ -68,11 +72,44 @@ namespace MDEvents
    */
   void ConvertToReflectometryQ::init()
   {
-    auto validator = boost::make_shared<API::WorkspaceUnitValidator>("TOF");
-    declareProperty(new WorkspaceProperty<MatrixWorkspace>("InputWorkspace","",Direction::Input, validator),
+    auto unitValidator = boost::make_shared<API::WorkspaceUnitValidator>("Wavelength");
+    auto compositeValidator = boost::make_shared<CompositeValidator>();
+    compositeValidator->add(unitValidator);
+
+    declareProperty(new WorkspaceProperty<MatrixWorkspace>("InputWorkspace","",Direction::Input, compositeValidator),
         "An input workspace in time-of-flight");
 
+    std::vector<std::string> propOptions;
+    propOptions.push_back("Q (lab frame)");
+    propOptions.push_back("P (lab frame)");
+    propOptions.push_back("K (initial, final)");
+    
+    declareProperty("OutputDimensions", "Q (lab frame)" ,boost::make_shared<StringListValidator>(propOptions),
+      "What will be the dimensions of the output workspace?\n"
+      "  Q (lab frame): Wave-vector change of the lattice in the lab frame.\n"
+      "  P (lab frame): Momentum in the sample frame.\n"
+      "  K initial and final vectors in the z plane."
+       );
+
+    declareProperty(new Kernel::PropertyWithValue<bool>("OverrideIncidentTheta", false),
+        "Use the provided incident theta value.");
+
+    declareProperty(new PropertyWithValue<double>("IncidentTheta", -1),
+        "An input incident theta value specified in degrees."
+        "Optional input value for the incident theta specified in degrees.");
+
+    setPropertySettings("IncidentTheta", new Kernel::EnabledWhenProperty("OverrideIncidentTheta", IS_EQUAL_TO, "1") );
+
     declareProperty(new WorkspaceProperty<IMDWorkspace>("OutputWorkspace","",Direction::Output), "Output 2D Workspace.");
+  }
+
+  bool incidentThetaInRange(const double& theta)
+  {
+    if(theta < 0 || theta >  90)
+    {
+      return false;
+    }
+    return true;
   }
 
   //----------------------------------------------------------------------------------------------
@@ -80,8 +117,102 @@ namespace MDEvents
    */
   void ConvertToReflectometryQ::exec()
   {
-    //Impelementation details still to workout here. May be best/possible to implement this algorithm in terms of other algorithms.
-    throw std::runtime_error("Not Yet Implemented.");
+    Mantid::API::MatrixWorkspace_const_sptr inputWs = getProperty("InputWorkspace");
+    bool bUseOwnIncidentTheta = getProperty("OverrideIncidentTheta"); 
+    double incidentTheta = getProperty("IncidentTheta");
+    std::string outputDimensions = getPropertyValue("OutputDimensions");
+
+    if(bUseOwnIncidentTheta)
+    {
+      if(!incidentThetaInRange(incidentTheta))
+      {
+        throw std::logic_error("Overriding incident theta is out of range");
+      }
+    }
+    else
+    {
+      const Mantid::API::Run& run = inputWs->run();
+      try
+      {
+        Property* p = run.getLogData("stheta");
+        auto incidentThetas = dynamic_cast<Mantid::Kernel::TimeSeriesProperty<double>*>(p);
+        incidentTheta = incidentThetas->valuesAsVector().back(); //Not quite sure what to do with the time series for stheta
+      }
+      catch(Exception::NotFoundError& ex)
+      {
+        throw std::runtime_error("The workspace does not have a stheta log value.");
+      }
+    }
+
+    if(outputDimensions != "Q (lab frame)")
+    {
+      throw std::runtime_error("Transforms other than to Q have not been implemented yet");
+    }
+
+    auto spectraAxis = inputWs->getAxis(1);
+    auto wavelengthAxis = inputWs->getAxis(0);
+    const std::string caption = spectraAxis->unit()->caption();
+    const std::string label = spectraAxis->unit()->label();
+    const std::string expectedCaption = "Scattering angle";
+    const std::string expectedLabel = "degrees";
+
+    const double qzmin = -1;
+    const double qxmin = -1;
+    const double qxmax = 1;
+    const double qzmax = 1;
+    const size_t nbinsx = 100;
+    const size_t nbinsz = 100;
+
+    MDHistoDimension_sptr qxDim = MDHistoDimension_sptr(new MDHistoDimension("Qx","qx","(Ang^-1)", qxmin, qxmax, nbinsx)); 
+    MDHistoDimension_sptr qzDim = MDHistoDimension_sptr(new MDHistoDimension("Qz","qz","(Ang^-1)", qzmin, qzmax, nbinsz)); 
+    MDHistoWorkspace_sptr ws = MDHistoWorkspace_sptr(new MDHistoWorkspace(qxDim, qzDim));
+  
+    const double two_pi = 6.28318531;
+    const double c_cos_theta_i = cos(incidentTheta);
+    const double  c_sin_theta_i = sin(incidentTheta);
+    for(size_t index = 0; index < inputWs->getNumberHistograms(); ++index)
+    {
+
+      auto counts = inputWs->readY(index);
+      auto wavelengths = inputWs->readX(index);
+      auto errors = inputWs->readE(index);
+      size_t nInputBins = inputWs->isHistogramData() ? wavelengths.size() -1 : wavelengths.size();
+      const double theta_final = spectraAxis->getValue(index)/2;
+      const double c_sin_theta_f = sin(theta_final);
+      const double c_cos_theta_f = cos(theta_final);
+      for(size_t j = 0; j < nInputBins; ++j)
+      {
+        double lambda = wavelengths[j];
+        double wavenumber = two_pi/lambda;
+        double _qx = wavenumber * (c_cos_theta_f - c_cos_theta_i);
+        double _qz = wavenumber * (c_sin_theta_f + c_sin_theta_i);
+       
+        /// If q-max and min are known a-prori, these boundrary case truncations are not required. See top of method for comment.
+       _qx = _qx < qxmin ? qxmin : _qx;
+       _qz = _qz < qzmin ? qzmin : _qz;
+       _qx = _qx > qxmax ? qxmax : _qx;
+       _qz = _qz > qzmax ? qzmax : _qz;
+
+       //Set up for linear transformation qi -> dimension index.
+ 
+       double mx = (nbinsx / (qxmax - qxmin));
+ 	     double mz = (nbinsz / (qzmax - qzmin));
+       double cx = (nbinsx - mx * (qxmin + qxmax))/2;
+       double cz = (nbinsz - mz * (qzmin + qzmax))/2;
+
+       size_t posIndexX = mx*_qx + cx;
+       size_t posIndexZ = mz*_qz + cz;
+       size_t linearindex = (posIndexX * nbinsx) + posIndexZ;
+
+       double error = errors[j];
+
+       ws->setSignalAt(linearindex, ws->getSignalAt(linearindex) + counts[j]);
+       ws->setErrorSquaredAt(linearindex, error*error);
+
+      }
+    }
+    
+    setProperty("OutputWorkspace", ws);
   }
 
 
--- Code/Mantid/Framework/MDEvents/test/ConvertToReflectometryQTest.h
+++ Code/Mantid/Framework/MDEvents/test/ConvertToReflectometryQTest.h
@@ -10,6 +10,7 @@
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
 #include "MantidMDEvents/ConvertToReflectometryQ.h"
 #include "MantidAPI/IMDHistoWorkspace.h"
+#include "MantidAPI/NumericAxis.h"
 
 using namespace Mantid;
 using namespace Mantid::MDEvents;
@@ -20,18 +21,41 @@ class ConvertToReflectometryQTest : public CxxTest::TestSuite
 {
 private:
 
+  /*
+  Boiler plate to produce a ConvertToReflectometryQ algorithm with a working set of input values.
+
+  Each test can customise with properties it wishes to override over these standard values.
+
+  Makes the tests much more readable like this.
+  */
+  boost::shared_ptr<ConvertToReflectometryQ> make_standard_algorithm()
+  {
+    MatrixWorkspace_sptr in_ws = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(10, 10);
+    in_ws->getAxis(0)->setUnit("TOF");
+
+    Mantid::API::NumericAxis* const newAxis = new Mantid::API::NumericAxis(in_ws->getAxis(1)->length());
+    in_ws->replaceAxis(1,newAxis);
+    newAxis->unit() = boost::make_shared<Mantid::Kernel::Units::Degrees>();
+
+    auto alg = boost::make_shared<ConvertToReflectometryQ>();
+    alg->setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING( alg->initialize() )
+    TS_ASSERT( alg->isInitialized() )
+    alg->setProperty("InputWorkspace", in_ws);
+    alg->setProperty("OutputDimensions", "Q (lab frame)");
+    alg->setPropertyValue("OutputWorkspace", "OutputTransformedWorkspace");
+    alg->setProperty("OverrideIncidentTheta", true);
+    alg->setProperty("IncidentTheta", 0.5);
+    return alg;
+  }
+
   void doExecute(const std::string& outWSName)
   {
     MatrixWorkspace_sptr in_ws = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(10, 10);
     in_ws->getAxis(0)->setUnit("TOF");
   
-    ConvertToReflectometryQ alg;
-    alg.setRethrows(true);
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", in_ws) );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
-    TSM_ASSERT_THROWS("Not Implemented Yet", alg.execute(), std::runtime_error );
+    auto alg = make_standard_algorithm();
+    TSM_ASSERT_THROWS("Not Implemented Yet", alg->execute(), std::runtime_error );
   }
 
 public:
@@ -49,12 +73,39 @@ public:
     TS_ASSERT( alg.isInitialized() )
   }
 
+  void test_theta_initial_negative()
+  {
+    auto alg = make_standard_algorithm();
+    alg->setProperty("IncidentTheta", -0.0001);
+    TSM_ASSERT_THROWS("Incident theta is negative, should throw", alg->execute(), std::logic_error);
+  }
+
+  void test_theta_initial_too_large()
+  {
+    auto alg = make_standard_algorithm();
+    alg->setProperty("IncidentTheta", 90.001);
+    TSM_ASSERT_THROWS("Incident theta is too large, should throw", alg->execute(), std::logic_error);
+  }
+
+  void test_invalid_theta_axis()
+  {
+  }
+
+  //Characterisation test for the current state of the algorithm
+  void test_only_support_q_conversion()
+  {
+    auto alg = make_standard_algorithm();
+    alg->setProperty("OutputDimensions", "P (lab frame)");
+    TSM_ASSERT_THROWS("Should throw as this mode is not supported yet", alg->execute(), std::runtime_error);
+  }
+
   void test_check_shouldnt_be_working()
   {
     std::string outWSName("ConvertToReflectometryQTest_OutputWS");
-
     doExecute(outWSName);
   }
+
+
 
 };
 
