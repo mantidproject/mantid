65d4443800c830f65e48058edba0c4fc1df4a2ad
Wenduo Zhou <zhouw@ornl.gov>
2012-06-01 15:49:04 -0400

    Rewrite InvertMask in C++.  Refs #5186.

---
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/InvertMask.h
@@ -0,0 +1,63 @@
+#ifndef MANTID_ALGORITHMS_INVERTMASK_H_
+#define MANTID_ALGORITHMS_INVERTMASK_H_
+
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+  /** InvertMask : TODO: DESCRIPTION
+    
+    @date 2012-06-01
+
+    Copyright &copy; 2012 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+  */
+  class DLLExport InvertMask : public API::Algorithm
+  {
+  public:
+    InvertMask();
+    virtual ~InvertMask();
+    
+    /// Algorithm's name for identification overriding a virtual method
+    virtual const std::string name() const { return "InvertMask";};
+    /// Algorithm's version for identification overriding a virtual method
+    virtual int version() const { return 2;};
+    /// Algorithm's category for identification overriding a virtual method
+    virtual const std::string category() const { return "Mask";}
+
+  private:
+    /// Sets documentation strings for this algorithm
+    virtual void initDocs();
+    // Implement abstract Algorithm methods
+    void init();
+    // Implement abstract Algorithm methods
+    void exec();
+
+  };
+
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif  /* MANTID_ALGORITHMS_INVERTMASK_H_ */
---
+++ Code/Mantid/Framework/Algorithms/src/InvertMask.cpp
@@ -0,0 +1,94 @@
+/*WIKI*
+
+
+Invert a MaskWorkspace
+
+
+*WIKI*/
+
+#include "MantidAlgorithms/InvertMask.h"
+#include "MantidKernel/System.h"
+#include "MantidAPI/WorkspaceProperty.h"
+#include "MantidDataObjects/MaskWorkspace.h"
+
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+  DECLARE_ALGORITHM(InvertMask)
+
+  //----------------------------------------------------------------------------------------------
+  /** Constructor
+   */
+  InvertMask::InvertMask()
+  {
+  }
+    
+  //----------------------------------------------------------------------------------------------
+  /** Destructor
+   */
+  InvertMask::~InvertMask()
+  {
+  }
+  
+  void InvertMask::initDocs()
+  {
+    this->setWikiSummary("This algorithm inverts every mask bit in a MaskWorkspace. ");
+    this->setOptionalMessage("This algorithm inverts every mask bit in a MaskWorkspace.");
+
+    return;
+  }
+
+  void InvertMask::init()
+  {
+    this->declareProperty(new API::WorkspaceProperty<DataObjects::MaskWorkspace>("InputWorkspace", "Anonymous", Direction::Input),
+        "MaskWorkspace to be inverted. ");
+
+    this->declareProperty(new API::WorkspaceProperty<DataObjects::MaskWorkspace>("OutputWorkspace", "AnonynmousOutput", Direction::Output),
+        "MaskWorkspace has inverted bits from input MaskWorkspace.");
+
+    return;
+  }
+
+  void InvertMask::exec()
+  {
+    // 1. Get input
+    DataObjects::MaskWorkspace_const_sptr inWS = this->getProperty("InputWorkspace");
+    if (!inWS)
+    {
+      throw std::invalid_argument("InputWorkspace is not a MaskWorkspace.");
+    }
+
+    // 2. Do Invert by calling sub algorithm
+    API::IAlgorithm_sptr invert = createSubAlgorithm("BinaryOperateMasks", 0.0, 1.0, true);
+    invert->setPropertyValue("InputWorkspace1", inWS->name());
+    invert->setProperty("OperationType", "NOT");
+    invert->setProperty("OutputWorkspace", "tempws");
+
+    invert->execute();
+
+    if (!invert->isExecuted())
+    {
+      g_log.error() << "Subalgorithm BinaryOperateMask() cannot be executed. " << std::endl;
+      throw std::runtime_error("Subalgorithm BinaryOperateMask() cannot be executed. ");
+    }
+
+    DataObjects::MaskWorkspace_sptr outputws = invert->getProperty("OutputWorkspace");
+    if (!outputws)
+    {
+      throw std::runtime_error("Output Workspace is not a MaskWorkspace. ");
+    }
+
+    // 3. Set
+    this->setProperty("OutputWorkspace", outputws);
+
+    return;
+  }
+
+
+} // namespace Mantid
+} // namespace Algorithms
