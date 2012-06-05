cf45b952dce5fbc6bba9644b707644a9425fb516
Wenduo Zhou <zhouw@ornl.gov>
2012-06-05 11:32:20 -0400

    Implement MaskDetectorBins. Refs #5450.

---
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/MaskDetectorBins.h
@@ -0,0 +1,64 @@
+#ifndef MANTID_ALGORITHMS_MASKDETECTORBINS_H_
+#define MANTID_ALGORITHMS_MASKDETECTORBINS_H_
+
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h"
+#include "MantidDataObjects/TableWorkspace.h"
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+  /** MaskDetectorBins : TODO: DESCRIPTION
+    
+    @date 2012-06-04
+
+    Copyright &copy; 2012 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+  */
+  class DLLExport MaskDetectorBins : public API::Algorithm
+  {
+  public:
+    MaskDetectorBins();
+    virtual ~MaskDetectorBins();
+    
+    /// Algorithm's name for identification overriding a virtual method
+    virtual const std::string name() const { return "MaskDetectorBins";};
+    /// Algorithm's version for identification overriding a virtual method
+    virtual int version() const { return 1;};
+    /// Algorithm's category for identification overriding a virtual method
+    virtual const std::string category() const { return "Transforms\\Masking";}
+
+  private:
+    /// Sets documentation strings for this algorithm
+    virtual void initDocs();
+    // Implement abstract Algorithm methods
+    void init();
+    // Implement abstract Algorithm methods
+    void exec();
+
+  };
+
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif  /* MANTID_ALGORITHMS_MASKDETECTORBINS_H_ */
---
+++ Code/Mantid/Framework/Algorithms/src/MaskDetectorBins.cpp
@@ -0,0 +1,109 @@
+#include "MantidAlgorithms/MaskDetectorBins.h"
+#include "MantidKernel/System.h"
+#include "MantidAPI/WorkspaceProperty.h"
+#include "MantidAPI/WorkspaceValidators.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/TableRow.h"
+
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+namespace Mantid
+{
+namespace Algorithms
+{
+
+
+  //----------------------------------------------------------------------------------------------
+  /** Constructor
+   */
+  MaskDetectorBins::MaskDetectorBins()
+  {
+  }
+    
+  //----------------------------------------------------------------------------------------------
+  /** Destructor
+   */
+  MaskDetectorBins::~MaskDetectorBins()
+  {
+  }
+  
+  void MaskDetectorBins::initDocs()
+  {
+    this->setWikiSummary("Mask bins from a table workspace. ");
+    this->setOptionalMessage("Mask bins from a table workspace. ");
+  }
+
+  void MaskDetectorBins::init()
+  {
+    this->declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input, boost::make_shared<HistogramValidator>()),
+        "Input Workspace to mask bins. ");
+    this->declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output),
+        "Output Workspace with bins masked.");
+    this->declareProperty(new WorkspaceProperty<DataObjects::TableWorkspace>("MaskingInformation", "", Direction::Input),
+        "Input TableWorkspace containing parameters, SpectraList, XMin and XMax.");
+
+    return;
+  }
+
+  void MaskDetectorBins::exec()
+  {
+    MatrixWorkspace_const_sptr inputWS = getProperty("InputWorkspace");
+    DataObjects::TableWorkspace_sptr paramWS = getProperty("MaskingInformation");
+
+    // 1. Check input table workspace
+    g_log.debug() << "Lines of parameters workspace = " << paramWS->rowCount() << std::endl;
+
+    // 2. Loop over all rows
+    bool firstloop = true;
+    API::MatrixWorkspace_sptr outputws;
+
+    for (size_t ib = 0; ib < paramWS->rowCount(); ++ib)
+    {
+      API::TableRow therow = paramWS->getRow(ib);
+      double xmin, xmax;
+      std::string speclist;
+      therow >> xmin >> xmax >> speclist;
+
+      g_log.debug() << "Row " << ib << " XMin = " << xmin << "  XMax = " << xmax << " SpectraList = " << speclist << std::endl;
+
+      API::IAlgorithm_sptr maskbins = this->createSubAlgorithm("MaskBins", 0, 0.3, true);
+      maskbins->initialize();
+      if (firstloop)
+      {
+        maskbins->setPropertyValue("InputWorkspace", this->getPropertyValue("InputWorkspace"));
+        firstloop = false;
+      }
+      else
+      {
+        maskbins->setProperty("InputWorkspace", outputws);
+      }
+      maskbins->setProperty("OutputWorkspace", this->getPropertyValue("OutputWorkspace"));
+      maskbins->setPropertyValue("SpectraList", speclist);
+      maskbins->setProperty("XMin", xmin);
+      maskbins->setProperty("XMax", xmax);
+
+      bool isexec = maskbins->execute();
+      if (!isexec)
+      {
+        g_log.error() << "MaskBins() is not executed for row " << ib << std::endl;
+        throw std::runtime_error("MaskBins() is not executed");
+      }
+
+      outputws = maskbins->getProperty("OutputWorkspace");
+      if (!outputws)
+      {
+        g_log.error() << "OutputWorkspace is not retrieved for row " << ib << ". " << std::endl;
+        throw std::runtime_error("OutputWorkspace is not got from MaskBins");
+      }
+    }
+
+    setProperty("OutputWorkspace", outputws);
+
+    return;
+  }
+
+
+
+} // namespace Mantid
+} // namespace Algorithms
