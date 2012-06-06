6a6299c211c4824daedddea354d1fbc066a20d61
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-06-06 15:41:11 +0100

    refs #5452. Diff and Sum P transforms.

--- Code/Mantid/Framework/MDEvents/inc/MantidMDEvents/ReflectometryTransformP.h
+++ Code/Mantid/Framework/MDEvents/inc/MantidMDEvents/ReflectometryTransformP.h
@@ -2,12 +2,71 @@
 #define MANTID_MDEVENTS_REFLECTOMETRYTRANSFORMP_H_
 
 #include "MantidKernel/System.h"
-
+#include "MantidMDEvents/ReflectometryMDTransform.h"
 
 namespace Mantid
 {
 namespace MDEvents
 {
+
+  /**
+  class CalculateReflectometryPBase: Base class for p-type transforms.
+  */
+  class CalculateReflectometryPBase
+  {
+  protected:
+    const double to_radians_factor;
+    const double two_pi;
+    double m_sinThetaInitial;   
+    double m_sinThetaFinal;
+
+    CalculateReflectometryPBase(const double& thetaIncident) : to_radians_factor(3.14159265/180), two_pi(6.28318531)
+    {
+      m_sinThetaInitial = sin(to_radians_factor*thetaIncident);
+    }
+    ~CalculateReflectometryPBase(){}    
+  public:
+    void setThetaFinal(const double& thetaFinal)
+    {
+      m_sinThetaFinal = sin(to_radians_factor*thetaFinal);
+    }
+  };
+
+  /**
+  class CalculateReflectometryDiffP: Calculates difference between ki and kf.
+  */
+  class CalculateReflectometryDiffP : public CalculateReflectometryPBase
+  {
+    public:
+    CalculateReflectometryDiffP(const double& thetaInitial) : CalculateReflectometryPBase(thetaInitial){}
+    ~CalculateReflectometryDiffP(){};
+    double execute(const double& wavelength)
+    {
+      double wavenumber = two_pi/wavelength;
+      double ki = wavenumber * m_sinThetaInitial;
+      double kf = wavenumber * m_sinThetaFinal;
+      return ki - kf;
+    }
+  };
+
+  /**
+  class CalculateReflectometrySumP: Calculates sum of ki and kf.
+  */
+  class CalculateReflectometrySumP : public CalculateReflectometryPBase
+  {
+    public:
+    CalculateReflectometrySumP(const double& thetaInitial) : CalculateReflectometryPBase(thetaInitial){}
+    ~CalculateReflectometrySumP(){};
+    double execute(const double& wavelength)
+    {
+      double wavenumber = two_pi/wavelength;
+      double ki = wavenumber * m_sinThetaInitial;
+      double kf = wavenumber * m_sinThetaFinal;
+      return ki + kf;
+    }
+  };
+
+
 
   /** ReflectometryTransformP : TODO: DESCRIPTION
     
@@ -33,11 +92,22 @@ namespace MDEvents
     File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
     Code Documentation is available at: <http://doxygen.mantidproject.org>
   */
-  class DLLExport ReflectometryTransformP 
+  class DLLExport ReflectometryTransformP : public ReflectometryMDTransform
   {
+  private:
+    const double m_kiMin;
+    const double m_kiMax;
+    const double m_kfMin;
+    const double m_kfMax;
+    /// Object performing raw calculation to determine pzi + pzf
+    mutable CalculateReflectometrySumP m_pSumCalculation;
+    /// Object performing raw calculation to determine pzi - pzf
+    mutable CalculateReflectometryDiffP m_pDiffCalculation;
+
   public:
-    ReflectometryTransformP();
+    ReflectometryTransformP(double kiMin, double kiMax, double kfMin, double kfMax, double incidentTheta);
     virtual ~ReflectometryTransformP();
+    virtual Mantid::API::IMDEventWorkspace_sptr execute(Mantid::API::MatrixWorkspace_const_sptr inputWs) const;
     
   };
 
