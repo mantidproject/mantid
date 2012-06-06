ac55f18c1c0130e4b71d884e39a69804ed743858
Michael Reuter <reuterma@ornl.gov>
2012-06-06 14:55:14 -0400

    Refs #5453. Stubbing top-level workflow algorithm.

---
+++ Code/Mantid/scripts/DGS_Reduction.py
@@ -0,0 +1,9 @@
+"""
+    Script used to start the DGS reduction GUI from MantidPlot
+"""
+from reduction_application import ReductionGUI
+from PyQt4 import QtCore, uic
+
+reducer = ReductionGUI(instrument_list=["SEQ"])
+reducer.setup_layout(load_last=True)
+reducer.show()
