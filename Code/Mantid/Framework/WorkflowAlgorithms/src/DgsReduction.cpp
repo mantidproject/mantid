fc52eae41c0260d6f029fc22b6e067ee68e40ac8
Michael Reuter <reuterma@ornl.gov>
2012-06-08 15:13:24 -0400

    Refs #5453. Adding properties.

--- Code/Mantid/Framework/WorkflowAlgorithms/src/DgsReduction.cpp
+++ Code/Mantid/Framework/WorkflowAlgorithms/src/DgsReduction.cpp
@@ -8,6 +8,7 @@ parameters and generating calls to other workflow or standard algorithms.
 
 #include "MantidWorkflowAlgorithms/DgsReduction.h"
 #include "MantidKernel/System.h"
+#include "MantidKernel/ListValidator.h"
 
 using namespace Mantid::Kernel;
 using namespace Mantid::API;
@@ -39,13 +40,13 @@ namespace WorkflowAlgorithms
 
   //----------------------------------------------------------------------------------------------
   /// Algorithm's name for identification. @see Algorithm::name
-  const std::string DgsReduction::name() const { return "DgsReduction";};
+  const std::string DgsReduction::name() const { return "DgsReduction"; };
   
   /// Algorithm's version for identification. @see Algorithm::version
-  int DgsReduction::version() const { return 1;};
+  int DgsReduction::version() const { return 1; };
   
   /// Algorithm's category for identification. @see Algorithm::category
-  const std::string DgsReduction::category() const { return "Workflow";}
+  const std::string DgsReduction::category() const { return "Workflow\\Inelastic"; }
 
   //----------------------------------------------------------------------------------------------
   /// Sets documentation strings for this algorithm
@@ -60,8 +61,19 @@ namespace WorkflowAlgorithms
    */
   void DgsReduction::init()
   {
-    declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input), "An input workspace.");
-    declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output), "An output workspace.");
+    //declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input), "An input workspace.");
+    //declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output), "An output workspace.");
+    declareProperty("SampleData", "", "Run numbers, files or workspaces of the data sets to be reduced");
+    declareProperty("FilterBadPulses", false, "If true, filter bad pulses from data.");
+    std::vector<std::string> incidentBeamNormOptions;
+    incidentBeamNormOptions.push_back("None");
+    incidentBeamNormOptions.push_back("ByCurrent");
+    incidentBeamNormOptions.push_back("ToMonitor");
+    declareProperty("IncidentBeamNormalisation", "None",
+        boost::make_shared<StringListValidator>(incidentBeamNormOptions),
+        "Options for incident beam normalisation on data.");
+    declareProperty("TimeIndepBackgroundRemoval", false,
+        "If true, time-independent background will be calculated and removed.");
   }
 
   //----------------------------------------------------------------------------------------------
