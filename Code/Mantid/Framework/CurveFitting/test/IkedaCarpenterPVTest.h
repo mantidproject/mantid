e2bf4fb693b0eb0bb16abe5d30506c6f742d37a7
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-06-12 12:26:06 +0100

    Enable IkedaCarpenterPVTest on OS X. Refs #5461
    Check if compiler upgrade has solved the problem.

--- Code/Mantid/Framework/CurveFitting/test/IkedaCarpenterPVTest.h
+++ Code/Mantid/Framework/CurveFitting/test/IkedaCarpenterPVTest.h
@@ -112,9 +112,6 @@ public:
   // here tries to fit an IC peak to a Gaussian mock data peak
   void testAgainstMockData()
   {
-    // Needs fixing on OS X, but keep alive for other platforms so we know if it fails for some other reason
-#ifndef __APPLE__
-
     Fit alg2;
     TS_ASSERT_THROWS_NOTHING(alg2.initialize());
     TS_ASSERT( alg2.isInitialized() );
@@ -206,7 +203,6 @@ public:
     TS_ASSERT( categories[0] == "Peak" );
 
     AnalysisDataService::Instance().remove(wsName);
-#endif
   }
 
 
