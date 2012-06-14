bffd7ff035438a7d2d8eb0251bb4fa86b488eb32
Michael Reuter <reuterma@ornl.gov>
2012-06-14 13:39:47 -0400

    Refs #5454. Rounding out data corrections tab.

--- Code/Mantid/scripts/Interface/reduction_gui/widgets/inelastic/dgs_data_corrections.py
+++ Code/Mantid/scripts/Interface/reduction_gui/widgets/inelastic/dgs_data_corrections.py
@@ -39,11 +39,21 @@ class DataCorrectionsWidget(BaseWidget):
         self._monitor_intrange_widgets_state(self._content.monitor1_rb.isChecked())
         self.connect(self._content.monitor1_rb, QtCore.SIGNAL("toggled(bool)"), 
                      self._monitor_intrange_widgets_state)
+
+        self._det_van_intrange_widgets_state(self._content.van_int_cb.isChecked())
+        self.connect(self._content.van_int_cb, QtCore.SIGNAL("toggled(bool)"),
+                     self._det_van_intrange_widgets_state)
         
     def _monitor_intrange_widgets_state(self, state=False):
         self._content.monint_label.setEnabled(state)
         self._content.monint_low_edit.setEnabled(state)
-        self._content.monint_high_edit.setEnabled(state)        
+        self._content.monint_high_edit.setEnabled(state)
+        
+    def _det_van_intrange_widgets_state(self, state=False):
+        self._content.van_int_range_label.setEnabled(state)
+        self._content.van_int_range_low_edit.setEnabled(state)
+        self._content.van_int_range_high_edit.setEnabled(state)
+        self._content.van_int_range_units_cb.setEnabled(state)
     
     def set_state(self, state):
         """
@@ -54,6 +64,17 @@ class DataCorrectionsWidget(BaseWidget):
         button_index = DataCorrectionsScript.INCIDENT_BEAM_NORM_TYPES.index(state.incident_beam_norm)
         cbutton = self.incident_beam_norm_grp.button(button_index)
         cbutton.setChecked(True)
+        self._content.monint_low_edit.setText(state.monitor_int_low)
+        self._content.monint_high_edit.setText(state.monitor_int_high)
+        self._content.background_sub_gb.setChecked(state.tib_subtraction)
+        self._content.tof_start_edit.setText(state.tib_tof_start)
+        self._content.tof_end_edit.setText(state.tib_tof_end)
+        self._content.van_input_edit.setText(state.detector_vanadium)
+        self._content.van_int_cb.setChecked(state.det_van_integration)
+        self._content.van_int_range_low_edit.setText(state.det_van_int_range_low)
+        self._content.van_int_range_high_edit.setText(state.det_van_int_range_high)
+        entry_index = self._content.van_int_range_units_cb.findText(state.det_van_int_range_units)
+        self._content.van_int_range_units_cb.setCurrentIndex(entry_index)
 
     def get_state(self):
         """
@@ -62,4 +83,15 @@ class DataCorrectionsWidget(BaseWidget):
         d = DataCorrectionsScript()
         d.filter_bad_pulses = self._content.filter_bad_pulses_chkbox.isChecked()
         d.incident_beam_norm = DataCorrectionsScript.INCIDENT_BEAM_NORM_TYPES[self.incident_beam_norm_grp.checkedId()]
+        d.monitor_int_low = self._content.monint_low_edit.text()
+        d.monitor_int_high = self._content.monint_high_edit.text()
+        d.tib_subtraction = self._content.background_sub_gb.isChecked()
+        d.tib_tof_start = self._content.tof_start_edit.text()
+        d.tib_tof_end = self._content.tof_end_edit.text()
+        d.detector_vanadium = self._content.van_input_edit.text()
+        d.det_van_integration = self._content.van_int_cb.isChecked()
+        d.det_van_int_range_low = self._content.van_int_range_low_edit.text()
+        d.det_van_int_range_high = self._content.van_int_range_high_edit.text()
+        d.det_van_int_range_units = self._content.van_int_range_units_cb.currentText()
         return d
+    
