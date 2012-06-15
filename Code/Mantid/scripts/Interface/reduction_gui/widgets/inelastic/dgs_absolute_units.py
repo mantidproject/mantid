fc64bab09acf0ff1d23c283675a9a162aaafb431
Michael Reuter <reuterma@ornl.gov>
2012-06-15 14:21:45 -0400

    Refs #5454. Finishing out absolute units tab work.

--- Code/Mantid/scripts/Interface/reduction_gui/widgets/inelastic/dgs_absolute_units.py
+++ Code/Mantid/scripts/Interface/reduction_gui/widgets/inelastic/dgs_absolute_units.py
@@ -30,7 +30,33 @@ class AbsoluteUnitsWidget(BaseWidget):
             self.set_state(AbsoluteUnitsScript())
 
     def initialize_content(self):
-        pass
+        # Constraints
+        dvp = QtGui.QDoubleValidator()
+        dvp.setBottom(0.0)
+        self._content.ei_edit.setValidator(dvp)
+        
+        # Connections
+        self.connect(self._content.absunits_van_browse, QtCore.SIGNAL("clicked()"), 
+                     self._absunits_van_browse)
+        self.connect(self._content.absunits_detvan_browse, QtCore.SIGNAL("clicked()"), 
+                     self._absunits_detvan_browse)
+        self.connect(self._content.grouping_file_browse, QtCore.SIGNAL("clicked()"), 
+                     self._grouping_file_browse)
+
+    def _absunits_van_browse(self):
+        fname = self.data_browse_dialog()
+        if fname:
+            self._content.absunits_van_edit.setText(fname)   
+
+    def _absunits_detvan_browse(self):
+        fname = self.data_browse_dialog()
+        if fname:
+            self._content.absunits_detvan_edit.setText(fname)   
+
+    def _grouping_file_browse(self):
+        fname = self.data_browse_dialog()
+        if fname:
+            self._content.grouping_file_edit.setText(fname)   
     
     def set_state(self, state):
         """
@@ -38,6 +64,15 @@ class AbsoluteUnitsWidget(BaseWidget):
             @param state: AbsoluteUnitsScript object
         """
         self._content.absunits_gb.setChecked(state.do_absolute_units)
+        self._content.absunits_van_edit.setText(state.absunits_vanadium)
+        self._content.grouping_file_edit.setText(state.grouping_file)
+        self._content.absunits_detvan_edit.setText(state.absunits_detector_vanadium)
+        self._content.ei_edit.setText(state.incident_energy)
+        self._content.emin_edit.setText(QtCore.QString(str(state.emin)))
+        self._content.emax_edit.setText(QtCore.QString(str(state.emax)))
+        self._content.van_mass_edit.setText(QtCore.QString(str(state.vandium_mass)))
+        self._content.sample_mass_edit.setText(QtCore.QString(str(state.sample_mass)))
+        self._content.sample_rmm_edit.setText(QtCore.QString(str(state.sample_rmm)))
         
     def get_state(self):
         """
@@ -45,5 +80,14 @@ class AbsoluteUnitsWidget(BaseWidget):
         """
         a = AbsoluteUnitsScript()
         a.do_absolute_units = self._content.absunits_gb.isChecked()
+        a.absunits_vanadium = self._content.absunits_van_edit.text()
+        a.grouping_file = self._content.grouping_file_edit.text()
+        a.absunits_detector_vanadium = self._content.absunits_detvan_edit.text()
+        a.incident_energy = self._content.ei_edit.text()
+        a.emin = util._check_and_get_float_line_edit(self._content.emin_edit)
+        a.emax = util._check_and_get_float_line_edit(self._content.emax_edit)
+        a.vanadium_mass = util._check_and_get_float_line_edit(self._content.van_mass_edit)
+        a.sample_mass = util._check_and_get_float_line_edit(self._content.sample_mass_edit)
+        a.sample_rmm = util._check_and_get_float_line_edit(self._content.sample_rmm_edit)
         return a
         
