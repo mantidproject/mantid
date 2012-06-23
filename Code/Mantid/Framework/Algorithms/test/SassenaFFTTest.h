25ae97d2050069192f637f5f52a9e92c30b83b83
Jose Borreguero <borreguero@gmail.com>
2012-06-23 19:32:54 -0400

    Refs #5431 finished test_ZeroImaginary
    modified: SassenaFFTTest.h

--- Code/Mantid/Framework/Algorithms/test/SassenaFFTTest.h
+++ Code/Mantid/Framework/Algorithms/test/SassenaFFTTest.h
@@ -3,6 +3,13 @@
 
 #include <cxxtest/TestSuite.h>
 #include "MantidAlgorithms/SassenaFFT.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidDataHandling/SaveAscii.h"
+#include "MantidAPI/FileProperty.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 using namespace Mantid;
 
@@ -22,62 +29,116 @@ public:
     TS_ASSERT( m_alg.isInitialized() )
   }
 
-  void test_exec()
+  /// FFT of a real symmetric Gaussian
+  void test_ZeroImaginary()
   {
-    if( !m_alg.isInitialized() ) m_alg.initialize();
-
-    // register a Sassena-like group workspace
+    const double params[] = {1.0, 1.0, 0.0, 0.1, 0.1, 2.0}; // three pairs of (Heigth,sigma) values
+    if( !m_alg.isInitialized() ){ m_alg.initialize(); }
     const std::string gwsName("Sassena");
-    this->createGroupWorkspace( gwsName );
+    this->createGroupWorkspace( params, gwsName );
     m_alg.setPropertyValue("InputWorkspace", gwsName);
-
     // execute the algorithm
     TS_ASSERT_THROWS_NOTHING( m_alg.execute() );
     TS_ASSERT( m_alg.isExecuted() );
-
-    // compare the results
+    //this->printWorkspace2D("/tmp/sqw.dat",gwsName +"_sqw");
+    // The input real part was an exponential h*exp(-x^2/(2*s^2) with h=1.0,s=1.0
+    // The Fourier transform is an exponential h'*exp(-x^2/(2*s'^2) with h'=sqrt(2*pi*s)=2.507 and s=2*pi/s=0.159
+    DataObjects::Workspace2D_const_sptr ws = API::AnalysisDataService::Instance().retrieveWS<DataObjects::Workspace2D>(gwsName +"_sqw");
+    checkHeigth(ws, sqrt(2*M_PI) );
+    checkSigma(ws, 1.0/(2.0*M_PI) );
   }
 
 private:
+
+  /**
+   * Check the maximum value stored in the workspace
+   * @param wsName name of the workspace
+   * @param value compare to the maximum value stored in the Y-vector of the workspace
+   */
+  void checkHeigth(DataObjects::Workspace2D_const_sptr &ws, const double &value)
+  {
+    const double frErr=1E-04; //allowed fractional error
+    const size_t nspectra = ws->getNumberHistograms();
+    MantidVec yv;
+    for(size_t i=0; i<nspectra; i++)
+    {
+      double goldStandard = value/(1+ static_cast<double>(i) );
+      yv = ws->readY(i);
+      double h = *std::max_element( yv.begin(), yv.end() );
+      TS_ASSERT_DELTA( h, goldStandard, frErr );
+    }
+  }
+
+  /**
+   * Check the standar deviation
+   * @param wsName name of the workspace
+   * @param value compare to the standard deviation
+   */
+  void checkSigma(DataObjects::Workspace2D_const_sptr &ws, const double &value)
+  {
+    const double frErr=1E-04; //allowed fractional error
+    const size_t nspectra = ws->getNumberHistograms();
+    MantidVec yv,xv;
+    for(size_t i=0; i<nspectra; i++)
+    {
+      double goldStandard = (1+ static_cast<double>(i) )*value;
+      double dx = (-2.0) * ws->readX(i).at(0); // extent along the X-axis
+      yv = ws->readY(i);
+      double h = *std::max_element( yv.begin(), yv.end() );
+      size_t nbins = yv.size();
+      double sum = std::accumulate(yv.begin(), yv.end(), 0.0) * dx / static_cast<double>(nbins);
+      double sigma = sum / (h * std::sqrt(2*M_PI));
+      TS_ASSERT_DELTA( sigma, goldStandard, frErr );
+    }
+  }
+
   /**
    * Gaussian centered at zero and with positive times only
    * @param sigma standard deviation
    * @param nbins number of time points
    */
-  void Gaussian( MantidVec& yv, const double& sigma, const size_t& nbins)
+  void Gaussian( MantidVec& xv, MantidVec& yv, const double& Heigth, const double& sigma)
   {
-    const double& Height = 1.0;
-    const double& dt = 1.0;
     double z;
-    for(size_t i=0; i<nbins; i++)
+    for(size_t i=0; i<xv.size(); i++)
     {
-      z = i * dt / sigma;
-      yv.push_back( Height*exp(-z*z/2.0) );
+      z = xv[i]/sigma;
+      yv[i] = Heigth*exp(-z*z/2.0);
     }
   } // void Gaussian
 
+  /// print workspace to an Ascii file
+  void printWorkspace2D( const std::string & fName, const std::string & wName)
+  {
+    Mantid::DataHandling::SaveAscii sa;
+    if( !sa.isInitialized() ){ sa.initialize();}
+    sa.setProperty("Filename",fName);
+    sa.setProperty("InputWorkspace", wName);
+    sa.execute();
+  }
+
   /// generate a Workspace2D, each spectra is half a gaussian
-  void createWorkspace( const std::string& wsName, const double& sigma0, const size_t &nbins, const size_t& nspectra)
+  void createWorkspace2D( const std::string& wsName, const double& Heigth, const double& sigma0, const size_t &nbins, const size_t& nspectra)
   {
     DataObjects::Workspace2D_sptr ws(new DataObjects::Workspace2D);
     ws->initialize(nspectra, nbins, nbins);
     ws->getAxis(0)->unit() = Kernel::UnitFactory::Instance().create("TOF");
 
-    const double dt=1.0;
+    const double dt=0.01;
     MantidVec xv;
     for(size_t i=0; i<nbins; i++)
     {
-      xv.push_back( dt*i );
+      int j = -static_cast<int>(nbins)/2 + static_cast<int>(i);
+      xv.push_back( dt*static_cast<double>(j) );
     }
 
-    double scaling = (1.*nspectra)/sigma0;
     double sigma;
-    MantidVec yv;
+    MantidVec yv(nbins);
     for (size_t i=0; i<nspectra; i++)
     {
       ws->dataX(i) = xv;
-      sigma = sigma0 - i * scaling;
-      this->Gaussian(yv, sigma, nbins);
+      sigma = sigma0/(1+ static_cast<double>(i) );
+      this->Gaussian(xv, yv, Heigth, sigma);
       ws->dataY(i) = yv;
     }
 
@@ -85,21 +146,31 @@ private:
   } // void createWorkspace
 
   /// create a group workspace with 'real' and 'imaginary' workspaces
-  void  createGroupWorkspace( const std::string& gwsName)
+  void  createGroupWorkspace( const double * params, const std::string& gwsName)
   {
-    API::WorkspaceGroup_sptr gws = new(API::WorkspaceGroup);
+    API::WorkspaceGroup_sptr gws(new API::WorkspaceGroup);
 
-    const size_t nbins(100);
-    const size_t nspectra(10);
+    const size_t nbins(2000);
+    const size_t nspectra(4);
 
     std::string wsName = gwsName +"_fqt.Re";
-    double sigma0 = 10.0;
-    this->createWorkspace( wsName, sigma0, nbins, nspectra);
+    double Heigth = params[0];
+    double sigma = params[1];
+    this->createWorkspace2D( wsName, Heigth, sigma, nbins, nspectra);
+    //this->printWorkspace2D("/tmp/fqt.Re.dat",wsName);
     gws->add(wsName);
 
     wsName = gwsName +"_fqt.Im";
-    sigma0 = 1.0; // assume a comparatively smaller imaginary part
-    this->createWorkspace( wsName, sigma0, nbins, nspectra);
+    Heigth = params[2];
+    sigma = params[3];
+    this->createWorkspace2D( wsName, Heigth, sigma, nbins, nspectra);
+    //this->printWorkspace2D("/tmp/fqt.Im.dat",wsName);
+    gws->add(wsName);
+
+    wsName = gwsName + "_fqt0";
+    Heigth = params[4];
+    sigma = params[5];
+    this->createWorkspace2D( wsName, Heigth, sigma, nbins, 1);
     gws->add(wsName);
 
     API::AnalysisDataService::Instance().add( gwsName, gws);
@@ -110,4 +181,4 @@ private:
 
 
 
-#endif MANTID_ALGORITHMS_SASSENAFFTTEST_H_
+#endif // MANTID_ALGORITHMS_SASSENAFFTTEST_H_
