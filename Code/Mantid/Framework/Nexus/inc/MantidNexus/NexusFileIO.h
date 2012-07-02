2ebeaaa78d438622ab3e82cb3082e8341669e92c
Peter Peterson <petersonpf@ornl.gov>
2012-07-02 13:24:54 -0400

    Refs #5282. Changing casts to make gcc4.7 happy.

--- Code/Mantid/Framework/Nexus/inc/MantidNexus/NexusFileIO.h
+++ Code/Mantid/Framework/Nexus/inc/MantidNexus/NexusFileIO.h
@@ -225,9 +225,9 @@ namespace Mantid
       if( NXopendata(fileID, name.c_str()) == NX_ERROR )return false;
       for(unsigned int it=0; it < attributes.size(); ++it)
       {
-        NXputattr(fileID, attributes[it].c_str(), (void*)avalues[it].c_str(), static_cast<int>(avalues[it].size()+1), NX_CHAR);
+        NXputattr(fileID, attributes[it].c_str(), reinterpret_cast<void*>(const_cast<char*>(avalues[it].c_str())), static_cast<int>(avalues[it].size()+1), NX_CHAR);
       }
-      NXputdata(fileID, (void*)nxstr.c_str());
+      NXputdata(fileID, reinterpret_cast<void*>(const_cast<char*>(nxstr.c_str())));
       NXclosedata(fileID);
       return true;
     }
@@ -286,9 +286,9 @@ namespace Mantid
       if( NXopendata(fileID, "value") == NX_ERROR )return false;
       for(unsigned int it=0; it < attributes.size(); ++it)
       {
-        NXputattr(fileID, attributes[it].c_str(), (void*)avalues[it].c_str(), static_cast<int>(avalues[it].size()+1), NX_CHAR);
+        NXputattr(fileID, attributes[it].c_str(), reinterpret_cast<void*>(const_cast<char*>(avalues[it].c_str())), static_cast<int>(avalues[it].size()+1), NX_CHAR);
       }
-      NXputdata(fileID, (void*)nxstr.c_str());
+      NXputdata(fileID, reinterpret_cast<void*>(const_cast<char*>(nxstr.c_str())));
       NXclosedata(fileID);
       NXclosegroup(fileID);
       return true;
