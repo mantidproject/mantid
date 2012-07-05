5b1519eccbe8a7b3289fbb9813418926d9cd96c8
Wenduo Zhou <zhouw@ornl.gov>
2012-07-05 18:13:56 -0400

    More on LeBailFunction.  Refs #5306.

--- Code/Mantid/Framework/CurveFitting/src/LeBailFunction.cpp
+++ Code/Mantid/Framework/CurveFitting/src/LeBailFunction.cpp
@@ -1,7 +1,8 @@
 #include "MantidCurveFitting/LeBailFunction.h"
 #include "MantidKernel/System.h"
-#include "MantidCurveFitting/ThermoNeutronBackToBackExpPV.h"
 #include "MantidAPI/FunctionFactory.h"
+
+#define DEFAULTPEAKWIDTHFACTOR 8.0
 
 using namespace Mantid::Kernel;
 using namespace Mantid::API;
@@ -24,8 +25,9 @@ namespace CurveFitting
     mL1 = 1.0;
     mL2 = 0.0;
 
-    mPeak = new CurveFitting::ThermoNeutronBackToBackExpPV();
-    mPeak->initialize();
+    g_log.warning() << "LeBailFunction.function1D(): Need a good algorithm to calculate a proper range for each peak. " << std::endl;
+
+    return;
   }
     
   //----------------------------------------------------------------------------------------------
@@ -68,11 +70,15 @@ namespace CurveFitting
     declareParameter("Gam1", 0.0);
     declareParameter("Gam2", 0.0);
 
-    g_log.warning() << "In function1D(), it is wrong to use a user given height for peak function. " << std::endl;
+    declareParameter("LatticeConstant", 10.0);
 
     return;
   }
 
+  /*
+   * Calculate peak parameters for a peak at d (d-spacing value)
+   * Output will be written to parameter map too.
+   */
   void LeBailFunction::calPeakParametersForD(double dh, double& alpha, double& beta, double &Tof_h,
       double &sigma_g2, double &gamma_l, std::map<std::string, double>& parmap) const
   {
@@ -98,14 +104,60 @@ namespace CurveFitting
     sigma_g2 = Sig0 + Sig1*std::pow(dh, 2) + Sig2*std::pow(dh, 4);
     gamma_l = Gam0 + Gam1*dh + Gam2*std::pow(dh, 2);
 
-    // 2. Add ...
+    // 3. Add to parameter map
     parmap.insert(std::make_pair("Alpha", alpha));
     parmap.insert(std::make_pair("Beta", beta));
     parmap.insert(std::make_pair("Sigma2", sigma_g2));
     parmap.insert(std::make_pair("Gamma", gamma_l));
     parmap.insert(std::make_pair("TOF_h", Tof_h));
 
-    std::cout << "DB1214 D = " << dh << ", TOF = " << Tof_h << std::endl;
+    g_log.debug() << "DB1214 D = " << dh << ", TOF = " << Tof_h << std::endl;
+
+    return;
+  }
+
+  /*
+   * Calculate all peaks' parameters
+   */
+  void LeBailFunction::calPeaksParameters()
+  {
+    // 1. Get parameters (class)
+    Alph0 = getParameter("Alph0");
+    Alph1 = getParameter("Alph1");
+    Beta0 = getParameter("Beta0");
+    Beta1 = getParameter("Beta1");
+    Alph0t = getParameter("Alph0t");
+    Alph1t = getParameter("Alph1t");
+    Beta0t = getParameter("Beta0t");
+    Beta1t = getParameter("Beta1t");
+    Dtt1 = getParameter("Dtt1");
+    Dtt1t = getParameter("Dtt1t");
+    Dtt2t = getParameter("Dtt2t");
+    Zero = getParameter("Zero");
+    Zerot = getParameter("Zerot");
+    Sig0 = getParameter("Sig0");
+    Sig1 = getParameter("Sig1");
+    Sig2 = getParameter("Sig2");
+    Gam0 = getParameter("Gam0");
+    Gam1 = getParameter("Gam1");
+    Gam2 = getParameter("Gam2");
+
+    // 2. Calcualte peak parameters for all peaks
+    for (size_t id = 0; id < dvalues.size(); ++id)
+    {
+      double dh = dvalues[id];
+      // a) Calculate all the parameters
+      double alpha, beta, tof_h, sigma2, gamma;
+      calPeakParametersForD(dh, alpha, beta, tof_h, sigma2, gamma, mPeakParameters[id]);
+
+      // b) Set peak parameters
+      mPeaks[id]->setParameter("TOF_h", tof_h);
+      mPeaks[id]->setParameter("height", heights[id]);
+      mPeaks[id]->setParameter("Alpha", alpha);
+      mPeaks[id]->setParameter("Beta", beta);
+      mPeaks[id]->setParameter("Sigma2", sigma2);
+      mPeaks[id]->setParameter("Gamma", gamma);
+    }
 
     return;
   }
@@ -132,35 +184,63 @@ namespace CurveFitting
     Gam0 = getParameter("Gam0");
     Gam1 = getParameter("Gam1");
     Gam2 = getParameter("Gam2");
+    double latticeconstant = getParameter("LatticeConstant");
+
+    /*
+    std::cout << " \n-------------------------  being visited -----------------------\n" << std::endl;
+    std::cout << "Alph0  = " << Alph0 << std::endl;
+    std::cout << "Alph1  = " << Alph1 << std::endl;
+    std::cout << "Alph0t = " << Alph0t << std::endl;
+    std::cout << "Alph1t = " << Alph1t << std::endl;
+    std::cout << "Zero   = " << Zero << std::endl;
+    std::cout << "Zerot  = " << Zerot << std::endl;
+    std::cout << "Lattice= " << latticeconstant << " Number of Peaks = " << mPeakHKLs.size() << std::endl;
+    */
 
     // 2.
     double *tempout = new double[nData];
-
-    for (size_t id = 0; id < dvalues.size(); ++id)
+    for (size_t iy = 0; iy < nData; ++iy)
     {
-      double dh = dvalues[id];
+      out[iy] = 0.0;
+    }
+
+    for (size_t id = 0; id < mPeakHKLs.size(); ++id)
+    {
+      int h = mPeakHKLs[id][0];
+      int k = mPeakHKLs[id][1];
+      int l = mPeakHKLs[id][2];
+      double dh = calCubicDSpace(latticeconstant, h, k, l);
+      dvalues[id] = dh;
+
       // a) Calculate all the parameters
       double alpha, beta, tof_h, sigma2, gamma;
       calPeakParametersForD(dh, alpha, beta, tof_h, sigma2, gamma, mPeakParameters[id]);
 
       // b) Set peak parameters
-      mPeak->setParameter("TOF_h", tof_h);
-      mPeak->setParameter("height", heights[id]);
-      mPeak->setParameter("Alpha", alpha);
-      mPeak->setParameter("Beta", beta);
-      mPeak->setParameter("Sigma2", sigma2);
-      mPeak->setParameter("Gamma", gamma);
+      g_log.debug() << "DB546 Peak @ d = " << dh << " Set Height = " << dh << std::endl;
+      mPeaks[id]->setParameter("TOF_h", tof_h);
+      mPeaks[id]->setParameter("height", heights[id]);
+      mPeaks[id]->setParameter("Alpha", alpha);
+      mPeaks[id]->setParameter("Beta", beta);
+      mPeaks[id]->setParameter("Sigma2", sigma2);
+      mPeaks[id]->setParameter("Gamma", gamma);
 
-      // c) Calculate range
-      g_log.error() << "Need a good algorithm to calculate a proper range for each peak. " << std::endl;
+      // c) FIXME Implement "Calculate individual peak range"
+      double fwhm = mPeaks[id]->fwhm();
+      double tof_low = tof_h - DEFAULTPEAKWIDTHFACTOR*fwhm;
+      double tof_upper = tof_h + DEFAULTPEAKWIDTHFACTOR*fwhm;
+      mPeaks[id]->setCalculationRange(tof_low, tof_upper);
 
       // d) Calculate peak
-      mPeak->function1D(tempout, xValues, nData);
+      mPeaks[id]->function1D(tempout, xValues, nData);
       for (size_t iy = 0; iy < nData; ++iy)
       {
         out[iy] += tempout[iy];
       }
-    }
+    } // END-FOR D-values
+
+    for (size_t n = 0; n < nData; ++n)
+      g_log.debug() << "DB327 " << xValues[n] << "\t\t" << out[n] << std::endl;
 
     // 3. Clean
     delete tempout;
@@ -168,6 +248,9 @@ namespace CurveFitting
     return;
   }
 
+  /*
+   * Using numerical derivative
+   */
   void LeBailFunction::functionDeriv(const API::FunctionDomain &domain, API::Jacobian &jacobian)
   {
     calNumericalDeriv(domain, jacobian);
@@ -179,20 +262,109 @@ namespace CurveFitting
    */
   void LeBailFunction::functionDeriv1D(API::Jacobian *out, const double* xValues, const size_t nData)
   {
+    UNUSED_ARG(out);
+    UNUSED_ARG(xValues);
+    UNUSED_ARG(nData);
+
     throw std::runtime_error("LeBailFunction does not support analytical derivative. ");
   }
 
   /*
    * Add a peak with its d-value
    */
-  void LeBailFunction::setPeak(double dh, double height)
+  void LeBailFunction::addPeak(double dh, double height)
   {
     dvalues.push_back(dh);
     heights.push_back(height);
+
+    // API::IPeakFunction* tpeak = new CurveFitting::ThermoNeutronBackToBackExpPV();
+    CurveFitting::ThermoNeutronBackToBackExpPV* peakptr = new CurveFitting::ThermoNeutronBackToBackExpPV();
+    CurveFitting::ThermoNeutronBackToBackExpPV_sptr tpeak(peakptr);
+    tpeak->setPeakRadius(8);
+
+    tpeak->initialize();
+    mPeaks.push_back(tpeak);
+
     std::map<std::string, double> parmap;
     mPeakParameters.push_back(parmap);
 
     return;
+  }
+
+  /*
+   * Add a peak (HKL)
+   */
+  void LeBailFunction::addPeaks(std::vector<std::vector<int> > peakhkls, std::vector<double> peakheights)
+  {
+    // 1. Check
+    if (peakhkls.size() != peakheights.size())
+    {
+      g_log.error() << "SetPeaks().  Input number of (HKL) is not equal to peak heights. " << std::endl;
+      throw std::invalid_argument("Peak's HKL and height do not match. ");
+    }
+
+    // 2. Calculate peak positions
+    double lattice = getParameter("LatticeConstant");
+    for (size_t ipk = 0; ipk < peakhkls.size(); ++ ipk)
+    {
+      if (peakhkls[ipk].size() != 3)
+      {
+        throw std::invalid_argument("Vector for (HKL) must have three and only three integers.");
+      }
+      int h = peakhkls[ipk][0];
+      int k = peakhkls[ipk][1];
+      int l = peakhkls[ipk][2];
+      double peak_d = calCubicDSpace(lattice, h, k, l);
+
+      this->addPeak(peak_d, peakheights[ipk]);
+      mPeakHKLs.push_back(peakhkls[ipk]);
+    }
+
+    return;
+  } // END Function
+
+  /*
+   * Reset all peaks' height
+   */
+  void LeBailFunction::setPeakHeights(std::vector<double> inheights)
+  {
+    if (inheights.size() != heights.size())
+    {
+      g_log.error() << "Input number of peaks (height) is not same as peaks. " << std::endl;
+      throw std::logic_error("Input number of peaks (height) is not same as peaks. ");
+    }
+
+    for (size_t ih = 0; ih < inheights.size(); ++ih)
+      heights[ih] = inheights[ih];
+
+    return;
+  }
+
+
+  CurveFitting::ThermoNeutronBackToBackExpPV_sptr LeBailFunction::getPeak(size_t peakindex)
+  {
+    if (peakindex >= mPeaks.size())
+    {
+      g_log.error() << "Try to access peak " << peakindex << " out of range [0, " << mPeaks.size() << ")." << std::endl;
+      throw std::invalid_argument("getPeak() out of boundary");
+    }
+
+    CurveFitting::ThermoNeutronBackToBackExpPV_sptr rpeak = mPeaks[peakindex];
+
+    return rpeak;
+  }
+
+
+  /*
+   * Calculate d = a/sqrt(h**2+k**2+l**2)
+   */
+  double LeBailFunction::calCubicDSpace(double a, int h, int k, int l) const
+  {
+    double hklfactor = sqrt(double(h*h)+double(k*k)+double(l*l));
+    double d = a/hklfactor;
+    g_log.debug() << "DB143 a = " << a << " (HKL) = " << h << ", " << k << ", " << l << ": d = " << d << std::endl;
+
+    return d;
   }
 
   /*
@@ -205,6 +377,9 @@ namespace CurveFitting
     return;
   }
 
+  /*
+   * Return peak parameters
+   */
   double LeBailFunction::getPeakParameter(size_t index, std::string parname) const
   {
     if (index >= mPeakParameters.size())
@@ -213,6 +388,11 @@ namespace CurveFitting
       throw std::runtime_error("Index out of range");
     }
 
+    CurveFitting::ThermoNeutronBackToBackExpPV_sptr peak = mPeaks[index];
+
+    double value = peak->getParameter(parname);
+
+    /*
     std::map<std::string, double>::iterator mit;
     mit = mPeakParameters[index].find(parname);
     double value = 0.0;
@@ -225,11 +405,10 @@ namespace CurveFitting
       g_log.error() << "Unable to find parameter " << parname << " in PeakParameters[" << index << "]" << std::endl;
       throw std::invalid_argument("Non-existing parameter name.");
     }
+    */
 
     return value;
   }
-
-
 
 
 } // namespace Mantid
--- Code/Mantid/Framework/CurveFitting/test/LeBailFunctionTest.h
+++ Code/Mantid/Framework/CurveFitting/test/LeBailFunctionTest.h
@@ -22,12 +22,6 @@ public:
   static LeBailFunctionTest *createSuite() { return new LeBailFunctionTest(); }
   static void destroySuite( LeBailFunctionTest *suite ) { delete suite; }
 
-
-  void test_Something()
-  {
-    TSM_ASSERT( "You forgot to write a test!", 0);
-  }
-
   /*
    * Goal: test function1D() of LeBailFunction by plotting 2 adjacent peaks
    * Input
@@ -43,10 +37,13 @@ public:
    * ...../Tests/Peaks/Jason-Powgen/HR_10Hz/B_mods/pg10b1.irf, LB4917b1.hkl
    *
    */
-  void test_Plot2Peaks()
+  void test_CalculatePeakParameters()
   {
     LeBailFunction fitalg;
     fitalg.initialize();
+
+    // TS_ASSERT_EQUALS(fitalg.asString(),
+    // "name=LeBailFunction,Dtt1=1,Dtt2=1,Dtt1t=1,Dtt2t=1,Zero=0,Zerot=0,Width=1,Tcross=1,Alph0=1.6,Alph1=1.5,Beta0=1.6,Beta1=1.5,Alph0t=1.6,Alph1t=1.5,Beta0t=1.6,Beta1t=1.5,Sig0=1,Sig1=1,Sig2=1,Gam0=0,Gam1=0,Gam2=0");
 
     // 1. Set up parameters
     fitalg.setParameter("Dtt1", 29671.7500);
@@ -78,19 +75,32 @@ public:
     fitalg.setParameter("Gam1", 0.0);
     fitalg.setParameter("Gam2", 0.0);
 
-    double d1 = 2.399981; // 1 1 1
+    fitalg.setParameter("LatticeConstant", 4.156890);
+
+    // double d1 = 2.399981; // 1 1 1
     double h1 = 1370.0/0.008;
-    double d2 = 2.939365; // 1 1 0
+    // double d2 = 2.939365; // 1 1 0
     double h2 = 660.0/0.0064;
-    fitalg.setPeak(d1, h1);
-    fitalg.setPeak(d2, h2);
+
+    std::vector<int> p111;
+    p111.push_back(1); p111.push_back(1); p111.push_back(1);
+    std::vector<int> p110;
+    p110.push_back(1); p110.push_back(1); p110.push_back(0);
+    std::vector<std::vector<int> > peaks;
+    peaks.push_back(p111); peaks.push_back(p110);
+
+    std::vector<double> peakheights;
+    peakheights.push_back(h1); peakheights.push_back(h2);
+
+    fitalg.addPeaks(peaks, peakheights);
 
     // 2. Calculate
     std::vector<double> vecX;
     std::vector<double> vecY;
     std::vector<double> vecE;
-    std::string filename("/home/wzz/Mantid/mantid/Code/release/LB4917b1_unittest.dat");
-    importDataFromColumnFile(filename, vecX, vecY,  vecE);
+    // std::string filename("/home/wzz/Mantid/mantid/Code/release/LB4917b1_unittest.dat");
+    // importDataFromColumnFile(filename, vecX, vecY,  vecE);
+    generateData(vecX, vecY, vecE);
 
     size_t nData = vecX.size();
     double* xvalues = new double[nData];
@@ -103,6 +113,7 @@ public:
 
     fitalg.calPeaks(out, xvalues, nData);
 
+    /*
     std::stringstream outstring;
     for (size_t id = 0; id < nData; ++id)
     {
@@ -112,6 +123,7 @@ public:
     ofile.open("peaks_gen.dat");
     ofile << outstring.str();
     ofile.close();
+    */
 
     // 3. Evaluate
     double tof_h_d1 = fitalg.getPeakParameter(0, "TOF_h");
@@ -136,8 +148,17 @@ public:
     TS_ASSERT_DELTA(sigma2_d2, 952.39972, 0.1);
     TS_ASSERT_DELTA(gamma_d2, 0.0, 0.01);
 
-    delete xvalues;
-    delete out;
+    // 4. Calcualte data
+    double y25 = 1360.27;
+    double y59 = 0.285529;
+    double y86 = 648.998;
+
+    TS_ASSERT_DELTA(out[25], y25, 0.01);
+    TS_ASSERT_DELTA(out[59], y59, 0.0001);
+    TS_ASSERT_DELTA(out[86], y86, 0.001);
+
+    delete[] xvalues;
+    delete[] out;
 
     return;
   }
@@ -147,7 +168,7 @@ public:
     std::ifstream ins;
     ins.open(filename.c_str());
     char line[256];
-    std::cout << "File " << filename << " isOpen = " << ins.is_open() << std::endl;
+    // std::cout << "File " << filename << " isOpen = " << ins.is_open() << std::endl;
     while(ins.getline(line, 256))
     {
       if (line[0] != '#')
@@ -165,8 +186,152 @@ public:
         vecE.push_back(e);
       }
     }
-
   }
+
+  /*
+   * Generate a set of powder diffraction data with 2 peaks
+   */
+  void generateData(std::vector<double>& vecX, std::vector<double>& vecY, std::vector<double>& vecE)
+  {
+    vecX.push_back(70931.750);    vecY.push_back(    0.0000000    );
+    vecX.push_back(70943.609);    vecY.push_back(    0.0000000    );
+    vecX.push_back(70955.477);    vecY.push_back(   0.69562334    );
+    vecX.push_back(70967.336);    vecY.push_back(   0.99016321    );
+    vecX.push_back(70979.203);    vecY.push_back(    1.4097446    );
+    vecX.push_back(70991.063);    vecY.push_back(    2.0066566    );
+    vecX.push_back(71002.930);    vecY.push_back(    2.8569770    );
+    vecX.push_back(71014.789);    vecY.push_back(    4.0666742    );
+    vecX.push_back(71026.656);    vecY.push_back(    5.7899261    );
+    vecX.push_back(71038.516);    vecY.push_back(    8.2414885    );
+    vecX.push_back(71050.383);    vecY.push_back(    11.733817    );
+    vecX.push_back(71062.242);    vecY.push_back(    16.702133    );
+    vecX.push_back(71074.109);    vecY.push_back(    23.779659    );
+    vecX.push_back(71085.969);    vecY.push_back(    33.848408    );
+    vecX.push_back(71097.836);    vecY.push_back(    48.191662    );
+    vecX.push_back(71109.695);    vecY.push_back(    68.596909    );
+    vecX.push_back(71121.563);    vecY.push_back(    97.664757    );
+    vecX.push_back(71133.430);    vecY.push_back(    139.04889    );
+    vecX.push_back(71145.289);    vecY.push_back(    197.90808    );
+    vecX.push_back(71157.156);    vecY.push_back(    281.60803    );
+    vecX.push_back(71169.016);    vecY.push_back(    399.65021    );
+    vecX.push_back(71180.883);    vecY.push_back(    562.42670    );
+    vecX.push_back(71192.742);    vecY.push_back(    773.34192    );
+    vecX.push_back(71204.609);    vecY.push_back(    1015.2813    );
+    vecX.push_back(71216.469);    vecY.push_back(    1238.3613    );
+    vecX.push_back(71228.336);    vecY.push_back(    1374.9380    );
+    vecX.push_back(71240.195);    vecY.push_back(    1380.5173    );
+    vecX.push_back(71252.063);    vecY.push_back(    1266.3978    );
+    vecX.push_back(71263.922);    vecY.push_back(    1086.2141    );
+    vecX.push_back(71275.789);    vecY.push_back(    894.75891    );
+    vecX.push_back(71287.648);    vecY.push_back(    723.46112    );
+    vecX.push_back(71299.516);    vecY.push_back(    581.04535    );
+    vecX.push_back(71311.375);    vecY.push_back(    465.93588    );
+    vecX.push_back(71323.242);    vecY.push_back(    373.45383    );
+    vecX.push_back(71335.102);    vecY.push_back(    299.35800    );
+    vecX.push_back(71346.969);    vecY.push_back(    239.92720    );
+    vecX.push_back(71358.836);    vecY.push_back(    192.29497    );
+    vecX.push_back(71370.695);    vecY.push_back(    154.14153    );
+    vecX.push_back(71382.563);    vecY.push_back(    123.54013    );
+    vecX.push_back(71394.422);    vecY.push_back(    99.028404    );
+    vecX.push_back(71406.289);    vecY.push_back(    79.368507    );
+    vecX.push_back(71418.148);    vecY.push_back(    63.620914    );
+    vecX.push_back(71430.016);    vecY.push_back(    50.990391    );
+    vecX.push_back(71441.875);    vecY.push_back(    40.873333    );
+    vecX.push_back(71453.742);    vecY.push_back(    32.758839    );
+    vecX.push_back(71465.602);    vecY.push_back(    26.259121    );
+    vecX.push_back(71477.469);    vecY.push_back(    21.045954    );
+    vecX.push_back(71489.328);    vecY.push_back(    16.870203    );
+    vecX.push_back(71501.195);    vecY.push_back(    13.520998    );
+    vecX.push_back(71513.055);    vecY.push_back(    10.838282    );
+    vecX.push_back(71524.922);    vecY.push_back(    8.6865807    );
+    vecX.push_back(71536.781);    vecY.push_back(    6.9630671    );
+    vecX.push_back(71548.648);    vecY.push_back(    5.5807042    );
+    vecX.push_back(71560.508);    vecY.push_back(    4.4734306    );
+    vecX.push_back(71572.375);    vecY.push_back(    3.5853302    );
+    vecX.push_back(71584.242);    vecY.push_back(    2.8735423    );
+    vecX.push_back(71596.102);    vecY.push_back(    2.3033996    );
+    vecX.push_back(71607.969);    vecY.push_back(    1.8461106    );
+    vecX.push_back(71619.828);    vecY.push_back(    0.0000000    );
+    vecX.push_back(86911.852);    vecY.push_back(   0.28651541    );
+    vecX.push_back(86923.719);    vecY.push_back(   0.39156997    );
+    vecX.push_back(86935.578);    vecY.push_back(   0.53503412    );
+    vecX.push_back(86947.445);    vecY.push_back(   0.73121130    );
+    vecX.push_back(86959.305);    vecY.push_back(   0.99911392    );
+    vecX.push_back(86971.172);    vecY.push_back(    1.3654519    );
+    vecX.push_back(86983.039);    vecY.push_back(    1.8661126    );
+    vecX.push_back(86994.898);    vecY.push_back(    2.5498226    );
+    vecX.push_back(87006.766);    vecY.push_back(    3.4847479    );
+    vecX.push_back(87018.625);    vecY.push_back(    4.7614965    );
+    vecX.push_back(87030.492);    vecY.push_back(    6.5073609    );
+    vecX.push_back(87042.352);    vecY.push_back(    8.8915405    );
+    vecX.push_back(87054.219);    vecY.push_back(    12.151738    );
+    vecX.push_back(87066.078);    vecY.push_back(    16.603910    );
+    vecX.push_back(87077.945);    vecY.push_back(    22.691912    );
+    vecX.push_back(87089.805);    vecY.push_back(    31.005537    );
+    vecX.push_back(87101.672);    vecY.push_back(    42.372311    );
+    vecX.push_back(87113.531);    vecY.push_back(    57.886639    );
+    vecX.push_back(87125.398);    vecY.push_back(    79.062233    );
+    vecX.push_back(87137.258);    vecY.push_back(    107.82082    );
+    vecX.push_back(87149.125);    vecY.push_back(    146.58661    );
+    vecX.push_back(87160.984);    vecY.push_back(    197.83006    );
+    vecX.push_back(87172.852);    vecY.push_back(    263.46185    );
+    vecX.push_back(87184.711);    vecY.push_back(    343.08966    );
+    vecX.push_back(87196.578);    vecY.push_back(    432.57846    );
+    vecX.push_back(87208.445);    vecY.push_back(    522.64124    );
+    vecX.push_back(87220.305);    vecY.push_back(    600.01373    );
+    vecX.push_back(87232.172);    vecY.push_back(    651.22260    );
+    vecX.push_back(87244.031);    vecY.push_back(    667.17743    );
+    vecX.push_back(87255.898);    vecY.push_back(    646.90039    );
+    vecX.push_back(87267.758);    vecY.push_back(    597.38873    );
+    vecX.push_back(87279.625);    vecY.push_back(    530.12573    );
+    vecX.push_back(87291.484);    vecY.push_back(    456.83890    );
+    vecX.push_back(87303.352);    vecY.push_back(    386.05295    );
+    vecX.push_back(87315.211);    vecY.push_back(    322.58456    );
+    vecX.push_back(87327.078);    vecY.push_back(    267.96231    );
+    vecX.push_back(87338.938);    vecY.push_back(    222.04863    );
+    vecX.push_back(87350.805);    vecY.push_back(    183.80043    );
+    vecX.push_back(87362.664);    vecY.push_back(    152.11101    );
+    vecX.push_back(87374.531);    vecY.push_back(    125.85820    );
+    vecX.push_back(87386.391);    vecY.push_back(    104.14707    );
+    vecX.push_back(87398.258);    vecY.push_back(    86.170067    );
+    vecX.push_back(87410.117);    vecY.push_back(    71.304932    );
+    vecX.push_back(87421.984);    vecY.push_back(    58.996807    );
+    vecX.push_back(87433.844);    vecY.push_back(    48.819309    );
+    vecX.push_back(87445.711);    vecY.push_back(    40.392483    );
+    vecX.push_back(87457.578);    vecY.push_back(    33.420235    );
+    vecX.push_back(87469.438);    vecY.push_back(    27.654932    );
+    vecX.push_back(87481.305);    vecY.push_back(    22.881344    );
+    vecX.push_back(87493.164);    vecY.push_back(    18.934097    );
+    vecX.push_back(87505.031);    vecY.push_back(    15.665835    );
+    vecX.push_back(87516.891);    vecY.push_back(    12.963332    );
+    vecX.push_back(87528.758);    vecY.push_back(    10.725698    );
+    vecX.push_back(87540.617);    vecY.push_back(    8.8754158    );
+    vecX.push_back(87552.484);    vecY.push_back(    7.3434072    );
+    vecX.push_back(87564.344);    vecY.push_back(    6.0766010    );
+    vecX.push_back(87576.211);    vecY.push_back(    5.0277033    );
+    vecX.push_back(87588.070);    vecY.push_back(    4.1603775    );
+    vecX.push_back(87599.938);    vecY.push_back(    3.4422443    );
+    vecX.push_back(87611.797);    vecY.push_back(    2.8484249    );
+    vecX.push_back(87623.664);    vecY.push_back(    2.3567512    );
+    vecX.push_back(87635.523);    vecY.push_back(    1.9501896    );
+    vecX.push_back(87647.391);    vecY.push_back(    1.6135623    );
+    vecX.push_back(87659.250);    vecY.push_back(    1.3352078    );
+    vecX.push_back(87671.117);    vecY.push_back(    1.1047342    );
+    vecX.push_back(87682.984);    vecY.push_back(   0.91404319    );
+    vecX.push_back(87694.844);    vecY.push_back(   0.75636220    );
+    vecX.push_back(87706.711);    vecY.push_back(    0.0000000    );
+
+    for (size_t i = 0; i < vecY.size(); ++i)
+    {
+      double e = 1.0;
+      if (vecY[i] > 1.0)
+        e = sqrt(vecY[i]);
+      vecE.push_back(e);
+    }
+
+    return;
+  }
+
 
 };
 
