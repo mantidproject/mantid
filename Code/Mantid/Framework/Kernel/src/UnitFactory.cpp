cb846171f4bbc5c264397af5794a1656c344e1a7
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-07-08 17:43:22 +0100

    The UnitFactory no longer caches an object for each unit. Refs #5584
    Updating the map of objects was not thread-safe and the units now have
    state meaning that it is best to have a new object for each create call.

--- Code/Mantid/Framework/Kernel/src/UnitFactory.cpp
+++ Code/Mantid/Framework/Kernel/src/UnitFactory.cpp
@@ -7,33 +7,12 @@ namespace Kernel
 {
 
 UnitFactoryImpl::UnitFactoryImpl() :
-  DynamicFactory<Unit>(), m_createdUnits(), m_log(Kernel::Logger::get("UnitFactory"))
+  DynamicFactory<Unit>(), m_log(Kernel::Logger::get("UnitFactory"))
 {
 }
 
 UnitFactoryImpl::~UnitFactoryImpl()
 {
-}
-
-/** Returns an instance of the class with the given name. Overrides the base class method.
- *  If an instance already exists, a pointer to it is returned, otherwise
- *  a new instance is created by the DynamicFactory::create method.
- *  @param className :: The name of the class to be created
- *  @return A shared pointer to the instance of the requested unit
- */
-boost::shared_ptr<Unit> UnitFactoryImpl::create(const std::string& className) const
-{
-  std::map< std::string, boost::shared_ptr<Unit> >::const_iterator it = m_createdUnits.find(className);
-  if ( it != m_createdUnits.end() )
-  {
-    // If an instance has previously been created, just return a pointer to it
-    return it->second;
-  }
-  else
-  {
-    // Otherwise create & return a new instance and store the pointer in the internal map for next time
-    return m_createdUnits[className] = DynamicFactory<Unit>::create(className);
-  }
 }
 
 } // namespace Kernel
