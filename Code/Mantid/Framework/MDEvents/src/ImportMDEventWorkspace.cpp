dee7c44ef13f4cdf8724dfa68be6853dad29afe6
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-07-16 15:13:03 +0100

    refs #5493. Wiki docs.

--- Code/Mantid/Framework/MDEvents/src/ImportMDEventWorkspace.cpp
+++ Code/Mantid/Framework/MDEvents/src/ImportMDEventWorkspace.cpp
@@ -1,5 +1,97 @@
 /*WIKI*
-TODO: Enter a full wiki-markup description of your algorithm here. You can then use the Build/wiki_maker.py script to generate your full wiki page.
+
+
+
+Creates an MDEventWorkspace from a plain ASCII file. Uses a simple format for the file described below. This algorithm is suitable for importing small volumes of data only. This algorithm does not scale well for large input workspaces. The purpose of this algorithm is to allow users to quickly import data from existing applications for purposes of comparison.
+
+== Format ==
+
+The file must contain a '''DIMENSIONS''' section listing the dimensionality of the workspace. Each input line is taken as a new dimension after the '''DIMENSIONS''' flag, and before the '''MDEVENTS''' flag. Input arguments must be separated by a space or tab. They are provided in the order Name, ID, Units, NumberOfBins. See the Usage examples below.
+
+The file must contain a '''MDEVENTS''' flag after the '''DIMENSIONS''' flag. Again, inputs are separated by a space or tab, each line represents a unique MDEvent. There must be either NDims + 2 or NDims + 4 columns in this section. If there are NDims + 2, columns, then ''Lean'' MDEvents will be used (Signal, Error and Dimensionality only). If there are NDims + 4 columns, then ''Full'' MDEvents will be used (Signal, Error, RunNo, DetectorId and Dimensionality). If you have provided NDims + 2 columns, then each row is interpreted as follows:
+
+ Signal Error {Dimensionality}
+
+where the Dimensionality is an array of coordinates in each of the dimensions listed in the '''DIMENSIONS''' section, and in that order. IF there are NDims + 4 columns, then ''Full'' MDEvents will be used. Each row is interpreted as follows:
+
+ Signal Error RunNumber DetectorId {Dimensionality}
+
+The usage example below shows demo files with both of these formats.
+
+Comments are denoted by lines starting with '''#'''. There is no multi-line comment. 
+
+== Usage ==
+
+The following example creates a 2D MDEventWorkspace called ''demo'' with 10 * 2 bins.
+
+
+ ImportMDEventWorkspace(Filename=r'demo.txt',OutputWorkspace='demo')
+
+demo.txt looks like:
+
+    # MANDATORY BLOCK. Dimensions are written in the format Id, Name, Units, number of bins
+    DIMENSIONS
+    a A U 10
+    b B U 2
+    # MANDATORY BLOCK. Events are written in the format Signal, Error, DetectorId, RunId, coord1, coord2, ... to end of coords
+    # or  Signal, Error, coord1, coord2, ... to end of coords
+    MDEVENTS
+    1.0	2.90	-1.0	-1
+    1.1	2.80	-0.9	-1
+    1.2	2.70	-0.8	-1
+    1.3	2.60	-0.7	-1
+    1.4	2.50	-0.6	-1
+    1.5	2.40	-0.5	-1
+    1.6	2.30	-0.4	-1
+    1.7	2.20	-0.3	-1
+    1.8	2.10	-0.2	-1
+    1.9	2.00	-0.1	-1
+    2.0	1.80	-1.0	1
+    2.1	1.70	-0.9	1
+    2.2	1.60	-0.8	1
+    2.3	1.50	-0.7	1
+    2.4	1.40	-0.6	1
+    2.5	1.30	-0.5	1
+    2.6	1.20	-0.4	1
+    2.7	1.10	-0.3	1
+    2.8	1.00	-0.2	1
+    2.9	0.90	-0.1	1
+
+The equivalent with run numbers and detector ids specified is:
+
+
+    # MANDATORY BLOCK. Dimensions are written in the format Id, Name, Units, number of bins
+    DIMENSIONS
+    a A U 10
+    b B U 2
+    # MANDATORY BLOCK. Events are written in the format Signal, Error, DetectorId, RunId, coord1, coord2, ... to end of coords
+    # or  Signal, Error, RunNumber, DetectorId, coord1, coord2, ... to end of coords
+    MDEVENTS
+    1.0	2.90	1	1	-1.0	-1
+    1.1	2.80	1	2	-0.9	-1
+    1.2	2.70	1	3	-0.8	-1
+    1.3	2.60	1	4	-0.7	-1
+    1.4	2.50	1	5	-0.6	-1
+    1.5	2.40	1	6	-0.5	-1
+    1.6	2.30	1	7	-0.4	-1
+    1.7	2.20	1	8	-0.3	-1
+    1.8	2.10	1	9	-0.2	-1
+    1.9	2.00	1	10	-0.1	-1
+    2.0	1.80	1	12	-1.0	1
+    2.1	1.70	1	13	-0.9	1
+    2.2	1.60	1	14	-0.8	1
+    2.3	1.50	1	15	-0.7	1
+    2.4	1.40	1	16	-0.6	1
+    2.5	1.30	1	17	-0.5	1
+    2.6	1.20	1	18	-0.4	1
+    2.7	1.10	1	19	-0.3	1
+    2.8	1.00	1	20	-0.2	1
+    2.9	0.90	1	20	-0.1	1
+
+== Alternatives ==
+Other alternatives to importing/creating MDWorkspaces are [[ImportMDHistoWorkspace]], [[CreateMDHistoWorkspace]] and [[CreateMDWorkspace]]
+
+
 *WIKI*/
 
 #include "MantidMDEvents/ImportMDEventWorkspace.h"
@@ -302,7 +394,7 @@ namespace MDEvents
     // Extract Dimensions and add to the output workspace.
     DataCollectionType::iterator dimEntriesIterator = m_posDimStart;
     for(size_t i = 0; i < m_nDimensions; ++i)
-    {
+    { 
       std::string id = convert<std::string>(*(++dimEntriesIterator));
       std::string name = convert<std::string>(*(++dimEntriesIterator));
       std::string units = convert<std::string>(*(++dimEntriesIterator));
