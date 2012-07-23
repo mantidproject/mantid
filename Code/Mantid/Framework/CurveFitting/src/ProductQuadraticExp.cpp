e708079ff54f8ce3a35dea87151ec14387b67a9b
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-07-23 15:32:43 +0100

    refs #5634. Working with tests.

--- Code/Mantid/Framework/CurveFitting/src/ProductQuadraticExp.cpp
+++ Code/Mantid/Framework/CurveFitting/src/ProductQuadraticExp.cpp
@@ -25,24 +25,38 @@ namespace Mantid
 
     void ProductQuadraticExp::functionDeriv1D(API::Jacobian* out, const double* xValues, const size_t nData)
     {
-      throw std::runtime_error("Not Implemented");
+      const double A0 = getParameter("A0");
+      const double A1 = getParameter("A1");
+      const double A2 = getParameter("A2");
+      const double Height = getParameter("Height");
+      const double Lifetime = getParameter("Lifetime");
+
+      for (size_t i = 0; i < nData; i++)
+      {
+        double x = xValues[i];
+        double expComponent = Height*std::exp(-x/Lifetime);
+        double linearComponent = (A1 * x) + A0;
+
+        out->set(i, 0, A1 * x * expComponent );
+        out->set(i, 1, (x + A0) * expComponent);
+        out->set(i, 2, ((x*x) + A1*x + A0) * expComponent);
+        out->set(i, 3, linearComponent * expComponent / Height);
+        out->set(i, 4, linearComponent * expComponent * x / (Lifetime * Lifetime));
+      }
     }
 
     void ProductQuadraticExp::function1D(double* out, const double* xValues, const size_t nData) const
     {
-      double A0 = getParameter("A0");
-      double A1 = getParameter("A1");
-      double A2 = getParameter("A2");
-      double Height = getParameter("Height");
-      double Lifetime = getParameter("Lifetime");
+      const double A0 = getParameter("A0");
+      const double A1 = getParameter("A1");
+      const double A2 = getParameter("A2");
+      const double Height = getParameter("Height");
+      const double Lifetime = getParameter("Lifetime");
 
-      UNUSED_ARG(A0);
-      UNUSED_ARG(A1);
-      UNUSED_ARG(A2);
-      UNUSED_ARG(Height);
-      UNUSED_ARG(Lifetime);
-
-      throw std::runtime_error("Not Implemented");
+      for(size_t i = 0; i < nData; ++i)
+      {
+        out[i] = (A0 + (A1*xValues[i]) + (A2*xValues[i]*xValues[i])) * Height * std::exp(-xValues[i]/Lifetime);
+      }
     }
 
   } // namespace CurveFitting
--- Code/Mantid/Framework/CurveFitting/test/ProductQuadraticExpTest.h
+++ Code/Mantid/Framework/CurveFitting/test/ProductQuadraticExpTest.h
@@ -4,11 +4,98 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidCurveFitting/ProductQuadraticExp.h"
+#include "MantidCurveFitting/ExpDecay.h"
+#include "MantidCurveFitting/QuadraticBackground.h"
+#include "MantidCurveFitting/ProductFunction.h"
+#include "MantidCurveFitting/Jacobian.h"
+#include "MantidAPI/FunctionDomain1D.h" 
+#include "MantidAPI/FunctionValues.h" 
+#include <algorithm>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 
-using Mantid::CurveFitting::ProductQuadraticExp;
+using namespace Mantid::CurveFitting;
+using namespace Mantid::API;
 
 class ProductQuadraticExpTest : public CxxTest::TestSuite
 {
+
+private:
+
+   /// Helper type to generate number for a std::generate call.
+  class LinearIncrementingAssignment
+  {
+  private:
+    double m_current;
+    const double m_step;
+  public:
+    LinearIncrementingAssignment(double min, double step) : m_current(min), m_step(step)
+    {
+    }
+    double operator() () 
+    {
+      double temp = m_current;
+      m_current += m_step;
+      return temp;
+    }
+  };
+
+    /** Helper method
+  With the input arguments
+  1) Creates the target ProductQuadraticFunction.
+  2) Creates and equavalent Product function using other Fit function framework types.
+  3) Manually calculates the expected output given that the underlying equation is so simple.
+  4) For each point on the domain, compares the outputs of (1) (2) and (3) above to check that the results are equal.
+  */
+  void do_test_function_calculation(const double& A0, const double& A1, const double& A2, const double& Height, const double& Lifetime)
+  {
+    // Create the Product linear exponential function
+    ProductQuadraticExp func;
+    func.setParameter("A0", A0);
+    func.setParameter("A1", A1);
+    func.setParameter("A2", A2);
+    func.setParameter("Height", Height);
+    func.setParameter("Lifetime", Lifetime);
+
+    // Create the equivalent Product Function
+    IFunction_sptr quadraticFunction = boost::make_shared<QuadraticBackground>();
+    quadraticFunction->initialize();
+    quadraticFunction->setParameter("A0", A0);
+    quadraticFunction->setParameter("A1", A1);
+    quadraticFunction->setParameter("A2", A2);
+    IFunction_sptr expFunction =  boost::make_shared<ExpDecay>();
+    expFunction->initialize();
+    expFunction->setParameter("Height", Height);
+    expFunction->setParameter("Lifetime", Lifetime);
+    ProductFunction benchmark;
+    benchmark.initialize();
+    benchmark.addFunction(quadraticFunction);
+    benchmark.addFunction(expFunction);
+
+    const size_t nResults = 10;
+    typedef std::vector<double> VecDouble;
+    VecDouble xValues(nResults);
+    std::generate(xValues.begin(), xValues.end(), LinearIncrementingAssignment(0, 0.0001));
+ 
+    FunctionDomain1DVector domain(xValues);
+    FunctionValues valuesBenchmark(domain);
+    FunctionValues valuesLinExpDecay(domain);
+    // Peform function evaluations.
+    benchmark.function(domain, valuesBenchmark);
+    func.function(domain, valuesLinExpDecay);
+
+    for(size_t i = 0; i < nResults; ++i)
+    {
+      double x = xValues[i];
+      // Mimic workings of ProductLinearExp function to product comparison output.
+      double expected = ((A2 * x * x) + (A1 * x) + A0) * Height * std::exp(-x/Lifetime); 
+      TS_ASSERT_DELTA(expected, valuesLinExpDecay[i], 0.0001);
+
+      // Verify that the output is also the same for the Linear algorithm.
+      TS_ASSERT_DELTA(valuesBenchmark[i], valuesLinExpDecay[i], 0.0001);
+    }
+  }
+
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
@@ -48,6 +135,87 @@ public:
     TS_ASSERT_EQUALS(A2, func.getParameter("A2"));
     TS_ASSERT_EQUALS(Height, func.getParameter("Height"));
     TS_ASSERT_EQUALS(Lifetime, func.getParameter("Lifetime"));
+  }
+
+
+  void test_execution_with_exp_components_unity()
+  {
+    // A1 is set to zero, so the ProductLinearExp function should just reduce to an exp decay function.
+    const double A0 = 1;
+    const double A1 = 0;
+    const double A2 = 0;
+    const double Height = 2;
+    const double Lifetime = 0.1;
+    
+    ProductQuadraticExp func;
+    func.setParameter("A0", A0);
+    func.setParameter("A1", A1);
+    func.setParameter("A2", A2);
+    func.setParameter("Height", Height);
+    func.setParameter("Lifetime", Lifetime);
+
+    ExpDecay benchmark;
+    benchmark.setParameter("Height", Height);
+    benchmark.setParameter("Lifetime", Lifetime);
+
+    const size_t nResults = 10;
+    typedef std::vector<double> VecDouble;
+    VecDouble xValues(nResults);
+    std::generate(xValues.begin(), xValues.end(), LinearIncrementingAssignment(0, 0.1));
+
+    FunctionDomain1DVector domain(xValues);
+    FunctionValues valuesExpDecay(domain);
+    FunctionValues valuesLinExpDecay(domain);
+    benchmark.function(domain,valuesExpDecay);
+    func.function(domain, valuesLinExpDecay);
+
+    for(size_t i = 0; i < nResults; ++i)
+    {
+      double x = xValues[i];
+      // Mimic workings of ProductLinearExp function to product comparison output.
+      double expected = ((A1 * x) + A0)* Height * std::exp(-x/Lifetime); 
+      TS_ASSERT_DELTA(expected, valuesLinExpDecay[i], 0.0001);
+      // As a complete check, verify that the output is also the same for the ExpDecay algorithm.
+      TS_ASSERT_DELTA(valuesExpDecay[i], valuesLinExpDecay[i], 0.0001);
+    }
+  }
+
+  void test_calculate_derivative_throws_nothing()
+  {
+    const size_t nResults = 10;
+    typedef std::vector<double> VecDouble;
+    VecDouble xValues(nResults);
+    std::generate(xValues.begin(), xValues.end(), LinearIncrementingAssignment(0, 0.1));
+    FunctionDomain1DVector domain(xValues);
+
+    Mantid::CurveFitting::Jacobian jacobian(nResults, 5);
+
+    ProductQuadraticExp func;
+    TS_ASSERT_THROWS_NOTHING(func.functionDeriv(domain, jacobian));
+  }
+
+  void test_with_low_contribution_from_expdecay()
+  {
+    // Setup the test to for low contribution from exponential component.
+    const double A0 = 0.2;
+    const double A1 = 0.1;
+    const double A2 = 0.05;
+    const double Height = 1;
+    const double Lifetime = 100;
+   
+    do_test_function_calculation(A0, A1, A2, Height, Lifetime);
+  }
+
+  void test_with_high_contribution_from_expdecay()
+  {
+    // Setup the test to for high contribution from exponential component.
+    const double A0 = 0.2;
+    const double A1 = 0.1;
+    const double A2 = 0.05;
+    const double Height = 1;
+    const double Lifetime = 0.01;
+   
+    do_test_function_calculation(A0, A1, A2, Height, Lifetime);
   }
 
 
