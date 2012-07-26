180742dd0fd34f84de85069af786b2c893168696
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-07-26 14:02:45 +0100

    refs #5646. Update wiki documentation.

--- Code/Mantid/Framework/Algorithms/src/MergeRuns.cpp
+++ Code/Mantid/Framework/Algorithms/src/MergeRuns.cpp
@@ -1,18 +1,29 @@
 /*WIKI* 
 
-
 Combines the data contained in an arbitrary number of input workspaces. If the input workspaces do not have common binning, the bins in the output workspace will cover the entire range of all the input workspaces, with the largest bin widths used in regions of overlap.
 
 ==== Restrictions on the input workspace ====
 
 The input workspaces must contain histogram data with the same number of spectra and matching units and instrument name in order for the algorithm to succeed. 
 
-'''For [[Workspace2D]]s''': Each input workspace must have common internal binning across all its spectra.
+'''For [[Workspace2D]]s''': Each input workspace must have common binning for all its spectra. 
 
 '''For [[EventWorkspace]]s''': This algorithm is Event-aware; it will append event lists from common spectra. Binning parameters need not be compatible; the output workspace will use the first workspaces' X bin boundaries.
 
+'''For [[WorkspaceGroup]]s''': Each nested has to be one of the above.
+
 Other than this it is currently left to the user to ensure that the combination of the workspaces is a valid operation.
 
+=== Processing Group Workspaces ===
+
+==== Multi-period Group Workspaces ====
+
+Group workspaces will be merged respecting the periods within each group. For example if you have two multiperiod group workspaces A and B and an output workspace C. A contains matrix workspaces A_1 and A_2, and B contains matrix workspaces B_1 and B2. Since this
+is multiperiod data, A_1 and B_1 share the same period, as do A_2 and B_2. So merging must be with respect to workspaces of equivalent periods. Therefore,
+merging is conducted such that A_1 + B_1 = C_1 and A_2 + B_2 = C_2.
+
+==== Group Workspaces that are not multiperiod ====
+If group workspaces are provided that are not multi-period, this algorithm will merge across all nested workspaces, to give a singe output matrix workspace. 
 
 ==== Subalgorithms used ====
 
