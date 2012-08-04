32081fdff2fec6fc95e9c527173ae3d41036a87d
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-08-04 16:30:27 +0100

    refs #5553 trying to revert changes which cause tests to fail

--- Code/Mantid/Framework/API/inc/MantidAPI/BoxController.h
+++ Code/Mantid/Framework/API/inc/MantidAPI/BoxController.h
@@ -34,8 +34,8 @@ namespace API
      * @return BoxController instance
      */
     BoxController(size_t nd)
-    :nd(nd), m_maxId(0), m_numSplit(1), m_file(NULL), m_diskBuffer(), m_useWriteBuffer(true),
-     m_SplitThreshold(128)
+    :nd(nd), m_maxId(0), m_numSplit(1), m_file(NULL), m_diskBuffer(), m_useWriteBuffer(true)
+     //m_SplitThreshold(128),
     {
       // TODO: Smarter ways to determine all of these values
       m_maxDepth = 5;
