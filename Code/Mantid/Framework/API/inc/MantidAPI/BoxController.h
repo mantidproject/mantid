b1994d0f531c46ed8c2f7d8ea3d3332a5b0add17
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-08-04 16:50:20 +0100

    refs #5552 check if revert decreases number of errors

--- Code/Mantid/Framework/API/inc/MantidAPI/BoxController.h
+++ Code/Mantid/Framework/API/inc/MantidAPI/BoxController.h
@@ -34,9 +34,8 @@ namespace API
      * @return BoxController instance
      */
     BoxController(size_t nd)
-    :nd(nd), m_maxId(0), m_numSplit(1), m_file(NULL), m_diskBuffer(), m_useWriteBuffer(true)
-     //m_SplitThreshold(128),
-    {
+    :nd(nd), m_maxId(0), m_numSplit(1), m_file(NULL), m_diskBuffer(), m_SplitThreshold(128),m_useWriteBuffer(true)
+      {
       // TODO: Smarter ways to determine all of these values
       m_maxDepth = 5;
       m_addingEvents_eventsPerTask = 1000;
