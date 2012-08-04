482607f13aace3f30056f9e11b195e91555f015a
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-08-04 18:03:29 +0100

    refs #5687 The test does not fail

--- Code/Mantid/Framework/MDEvents/test/MDWSDescrTest.h
+++ Code/Mantid/Framework/MDEvents/test/MDWSDescrTest.h
@@ -14,13 +14,18 @@ public:
   void testBuildFromMatrixWS()
   {
     MDWSDescription WSD;
+    // dimensions (min-max) have not been set
+    TS_ASSERT_THROWS(WSD.buildFromMatrixWS(ws2D,"|Q|","Direct"),std::invalid_argument);
+    std::vector<double> dimMin(2,-1);
+    std::vector<double> dimMax(2, 1);
+    WSD.setMinMax(dimMin,dimMax);   
     TS_ASSERT_THROWS_NOTHING(WSD.buildFromMatrixWS(ws2D,"|Q|","Direct"));
 
   }
 
 MDWSDescrTest()
 {
-     Mantid::API::MatrixWorkspace_sptr ws2D =WorkspaceCreationHelper::createProcessedWorkspaceWithCylComplexInstrument(4,10,true);
+     ws2D =WorkspaceCreationHelper::createProcessedWorkspaceWithCylComplexInstrument(4,10,true);
     // rotate the crystal by twenty degrees back;
      ws2D->mutableRun().mutableGoniometer().setRotationAngle(0,20);
      // add workspace energy
