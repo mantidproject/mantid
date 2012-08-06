06766ab04f50762f962d9eaa47e674d6f7c7d46f
Stuart Campbell <campbellsi@ornl.gov>
2012-08-06 15:52:33 -0400

    refs #5269. Added extra search directory
    To support openSUSE

--- Code/Mantid/Build/CMake/FindQwt.cmake
+++ Code/Mantid/Build/CMake/FindQwt.cmake
@@ -8,7 +8,7 @@
 ###############################################################################
 
 find_path ( QWT_INCLUDE_DIR qwt.h 
-            /usr/include/qwt-qt4 /usr/include/qwt 
+            /usr/include/qwt-qt4 /usr/include/qwt /usr/include/qwt5
 	    ${CMAKE_INCLUDE_PATH}/qwt 
 )
 find_library ( QWT_LIBRARY NAMES qwt-qt4 qwt )
