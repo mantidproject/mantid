1c4d0dffb8dd7a9dc4dce63c4f6edacbfd91f2f6
Russell Taylor <taylorrj@ornl.gov>
2012-08-14 13:15:16 -0400

    Re #5742. Add wiki text to source files.

--- Code/Mantid/Framework/Crystal/src/CombinePeaksWorkspaces.cpp
+++ Code/Mantid/Framework/Crystal/src/CombinePeaksWorkspaces.cpp
@@ -1,5 +1,5 @@
 /*WIKI*
-TODO: Enter a full wiki-markup description of your algorithm here. You can then use the Build/wiki_maker.py script to generate your full wiki page.
+This algorithm can be used to combine lists of single crystal peaks, possibly obtained by different methods, in to a single list (contained in a PeaksWorkspace). With the default options, this will simply append the lists of peaks. If CombineMatchingPeaks is selected then an attempt will be made to identify identical peaks by matching them in Q within the specified tolerance. The peaks in each workspace are traversed in the order they are found in the workspace (RHSWorkspace first) and if a match is found (the search stops at the first match for each RHSWorkspace peak) then the peak in the LHSWorkspace is retained.
 *WIKI*/
 
 #include "MantidCrystal/CombinePeaksWorkspaces.h"
--- Code/Mantid/Framework/Crystal/src/DiffPeaksWorkspaces.cpp
+++ Code/Mantid/Framework/Crystal/src/DiffPeaksWorkspaces.cpp
@@ -1,5 +1,5 @@
 /*WIKI*
-TODO: Enter a full wiki-markup description of your algorithm here. You can then use the Build/wiki_maker.py script to generate your full wiki page.
+This algorithm takes an input workspace (the LHSWorkspace) and removes from it's list of peaks any that are also found in a second workspace (the RHSWorkspace) in the same position in Q space within the specified tolerance. Each peak in the RHSWorkspace is taken in turn and compared to each peak in the LHSWorkspace in turn. The first match encountered is used, and the matching peak removed from the output before moving onto the next RHSWorkspace peak.
 *WIKI*/
 
 #include "MantidCrystal/DiffPeaksWorkspaces.h"
