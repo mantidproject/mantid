0760387f83999412362c9b3c59df26255b2bc88d
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-08-22 11:37:24 +0100

    refs #5785 removed redundant test and renamed another one

--- Code/Mantid/Framework/MDEvents/test/MDWSDescriptionTest.h
+++ Code/Mantid/Framework/MDEvents/test/MDWSDescriptionTest.h
@@ -9,7 +9,7 @@
 using namespace Mantid;
 using namespace Mantid::MDEvents;
 
-class MDWSDescrTest : public CxxTest::TestSuite
+class MDWSDescriptionTest : public CxxTest::TestSuite
 {
     Mantid::API::MatrixWorkspace_sptr ws2D;
 public:
@@ -39,11 +39,40 @@ public:
     PropNamews[1]="H";
     TS_ASSERT_THROWS_NOTHING(WSD.buildFromMatrixWS(ws2D,"|Q|","Indirect",PropNamews));
     TS_ASSERT_EQUALS(4,WSD.nDimensions());
-
-
   }
+  void testGetWS4DimIDFine()
+  {
+    Mantid::API::MatrixWorkspace_sptr ws2D =WorkspaceCreationHelper::createProcessedWorkspaceWithCylComplexInstrument(4,10,true);
+    ws2D->mutableRun().addProperty("Ei",12.,"meV",true);
 
-MDWSDescrTest()
+    MDEvents::MDWSDescription TWS;
+    std::vector<double> min(4,-10),max(4,10);
+    TWS.setMinMax(min,max);
+
+    std::vector<std::string> other_dim_names;
+ 
+
+    TS_ASSERT_THROWS_NOTHING(TWS.buildFromMatrixWS(ws2D,"Q3D","Direct",other_dim_names));
+
+    TSM_ASSERT_EQUALS("Inelastic workspace will produce 4 dimensions",4,TWS.nDimensions());
+    std::vector<std::string> dim_units = TWS.getDimUnits();
+    TSM_ASSERT_EQUALS("Last dimension of Inelastic transformation should be DeltaE","DeltaE",dim_units[3]);
+    TSM_ASSERT_EQUALS("Alg ID would be: ","Q3D",TWS.AlgID);
+    TSM_ASSERT("detector infromation should be present in the workspace ",!TWS.isDetInfoLost());
+
+    TS_ASSERT_THROWS_NOTHING(TWS.buildFromMatrixWS(ws2D,TWS.AlgID,"Indirect",other_dim_names));
+
+    //std::vector<std::string> dimID= TWS.getDefaultDimIDQ3D(1);
+    //for(size_t i=0;i<4;i++)
+    //{
+    //    TS_ASSERT_EQUALS(dimID[i],TWS.dimIDs[i]);
+    //    TS_ASSERT_EQUALS(dimID[i],TWS.dimNames[i]);
+    //}
+
+   }
+  
+
+MDWSDescriptionTest()
 {
      ws2D =WorkspaceCreationHelper::createProcessedWorkspaceWithCylComplexInstrument(4,10,true);
     // rotate the crystal by twenty degrees back;
