59aec9a436e685cfcbaac6602abc0d13156649e3
Jose Borreguero <borreguero@gmail.com>
2012-09-03 18:27:25 -0400

    Refs #5827 added logger retrieval

---
+++ Code/Mantid/Framework/API/src/ITransformScale.cpp
@@ -0,0 +1,14 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+
+#include "MantidAPI/ITransformScale.h"
+
+namespace Mantid
+{
+namespace API
+{
+Kernel::Logger& g_log = Kernel::Logger::get("ITransformScale");
+
+} // namespace API
+} // namespace Mantid
