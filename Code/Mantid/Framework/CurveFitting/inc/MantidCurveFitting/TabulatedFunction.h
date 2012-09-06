ac45146f97eef929cc62f24f7757d8f2eb4eded9
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2012-09-06 14:31:04 +0100

    Re #5822. Added TabulatedFunction

---
+++ Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/TabulatedFunction.h
@@ -0,0 +1,138 @@
+#ifndef MANTID_CURVEFITTING_TABULATEDFUNCTION_H_
+#define MANTID_CURVEFITTING_TABULATEDFUNCTION_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/ParamFunction.h"
+#include "MantidAPI/IFunction1D.h"
+#include "MantidKernel/System.h"
+#include <cmath>
+
+namespace Mantid
+{
+
+//----------------------------------------------------------------------
+// Forward declaration
+//----------------------------------------------------------------------
+namespace API
+{
+  class MatrixWorkspace;
+}
+
+namespace CurveFitting
+{
+/**
+
+A function which takes its values from a file or a workspace. The values atr tabulated as
+x,y pairs. Liear interpolation is used for points between the tabulated values. The function
+returns zero for points outside the tabulated values.
+
+The function has two attributes: FileName and Workspace. They define a data set to take the values from.
+Setting one of the attributes clears the other. 
+
+The files can be either ascii text files or nexus files. The ascii files must contain two column
+of real numbers separated by spaces. The first column are the x-values and the second one is for y.
+
+If a nexus file is used its first spectrum provides the data for the function. The same is true for 
+a workspace which must be a MatrixWorkspace.
+
+The function has a signle parameter - a scaling factor "Scaling". 
+
+@author Roman Tolchenov, Tessella plc
+@date 4/09/2012
+
+Copyright &copy; 2007-8 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+This file is part of Mantid.
+
+Mantid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Mantid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport TabulatedFunction : public API::ParamFunction, public API::IFunction1D
+{
+public:
+  /// Constructor
+  TabulatedFunction();
+
+  /// overwrite IFunction base class methods
+  std::string name()const{return "TabulatedFunction";}
+  virtual const std::string category() const { return "General";}
+  void function1D(double* out, const double* xValues, const size_t nData)const;
+  ///  function derivatives
+  void functionDeriv1D(API::Jacobian* out, const double* xValues, const size_t nData);
+
+  /// Returns the number of attributes associated with the function
+  size_t nAttributes()const{return 2;}
+  /// Returns a list of attribute names
+  std::vector<std::string> getAttributeNames()const;
+  /// Return a value of attribute attName
+  IFunction::Attribute getAttribute(const std::string& attName)const;
+  /// Set a value to attribute attName
+  void setAttribute(const std::string& attName,const IFunction::Attribute& value);
+  /// Check if attribute attName exists
+  bool hasAttribute(const std::string& attName)const{return attName == "FileName";}
+
+private:
+
+  /// Call the appropriate load function
+  void load(const std::string& fname);
+
+  /// Load the points from an ASCII file
+  void loadAscii(const std::string& fname);
+
+  /// Load the points from a NeXuS file
+  void loadNexus(const std::string& fname);
+
+  /// Load the points from a MatrixWorkspace
+  void loadWorkspace(const std::string& wsName);
+
+  /// Load the points from a MatrixWorkspace
+  void loadWorkspace(boost::shared_ptr<API::MatrixWorkspace> ws);
+
+  /// Size of the data
+  size_t size()const{return m_yData.size();}
+
+  /// Clear all data
+  void clear();
+
+  /// Evaluate the function for a list of arguments and given scaling factor
+  void eval(double scaling, double* out, const double* xValues, const size_t nData)const;
+
+  /// The file name
+  std::string m_fileName;
+
+  /// The workspace name
+  std::string m_wsName;
+
+  /// Stores x-values
+  std::vector<double> m_xData;
+
+  /// Stores y-values
+  std::vector<double> m_yData;
+
+  /// The first x
+  double m_xStart;
+
+  /// The lasst x
+  double m_xEnd;
+
+};
+
+} // namespace CurveFitting
+} // namespace Mantid
+
+#endif /*MANTID_CURVEFITTING_TABULATEDFUNCTION_H_*/
---
+++ Code/Mantid/Framework/CurveFitting/src/TabulatedFunction.cpp
@@ -0,0 +1,307 @@
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidCurveFitting/TabulatedFunction.h"
+#include "MantidKernel/FileValidator.h"
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/FunctionFactory.h"
+#include "MantidAPI/AnalysisDataService.h"
+
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+
+using namespace Kernel;
+using namespace API;
+
+DECLARE_FUNCTION(TabulatedFunction)
+
+/// Constructor
+TabulatedFunction::TabulatedFunction():
+m_xStart(0),m_xEnd(0)
+{
+  declareParameter("Scaling",1.0,"A scaling factor");
+}
+
+/// Evaluate the function for a list of arguments and given scaling factor
+void TabulatedFunction::eval(double scaling, double* out, const double* xValues, const size_t nData)const
+{
+  if (nData == 0 || size() == 0) return;
+
+  if (m_xStart >= xValues[nData-1] || m_xEnd <= xValues[0]) return;
+
+  size_t i = 0;
+  while(i < nData - 1 && xValues[i] < m_xStart)
+  {
+    out[i] = 0;
+    i++;
+  }
+  size_t j = 0;
+  for(;i<nData;i++)
+  {
+    if (j >= size()-1)
+    {
+      out[i] = 0;
+    }
+    else
+    {
+      double xi = xValues[i];
+      while(j < size()-1 && xi > m_xData[j]) j++;
+      if (xi == m_xData[j])
+      {
+        out[i] = m_yData[j] * scaling;
+      }
+      else if (j == size()-1)
+      {
+        out[i] = 0;
+      }
+      else if (j > 0)
+      {
+        double x0 = m_xData[j-1];
+        double x1 = m_xData[j];
+        double y0 = m_yData[j-1];
+        double y1 = m_yData[j];
+        out[i] = y0 + (y1 - y0)*(xi - x0)/(x1 - x0);
+        out[i] *= scaling;
+      }
+      else
+      {
+        out[i] = 0;
+      }
+    }
+  }
+}
+
+/**
+ * Calculate the function values.
+ * @param out :: The output buffer for the calculated values.
+ * @param xValues :: The array of x-values.
+ * @param nData :: The size of the data.
+ */
+void TabulatedFunction::function1D(double* out, const double* xValues, const size_t nData)const
+{
+  const double scaling = getParameter(0);
+  eval(scaling, out, xValues, nData);
+}
+
+/**
+ * function derivatives
+ * @param out :: The output Jacobian matrix: function derivatives over its parameters.
+ * @param xValues :: The function arguments
+ * @param nData :: The size of xValues.
+ */
+void TabulatedFunction::functionDeriv1D(API::Jacobian* out, const double* xValues, const size_t nData)
+{
+  std::vector<double> tmp( nData );
+  eval(1.0, tmp.data(), xValues, nData);
+  for(size_t i = 0; i < nData; ++i)
+  {
+    out->set( i, 0, tmp[i] );
+  }
+}
+
+
+/// Clear all data
+void TabulatedFunction::clear()
+{
+  m_fileName.clear();
+  m_wsName.clear();
+  m_xData.clear();
+  m_yData.clear();
+  m_xStart = 0;
+  m_xEnd = 0;
+}
+
+/// Returns a list of attribute names
+std::vector<std::string> TabulatedFunction::getAttributeNames()const
+{
+  std::vector<std::string> res(1,"FileName");
+  return res;
+}
+
+/** Set a value to attribute attName
+ * @param attName :: The attribute name
+ * @param value :: The new value
+ */
+void TabulatedFunction::setAttribute(const std::string& attName,const IFunction::Attribute& value)
+{
+  std::string aName( attName );
+  std::transform(aName.begin(),aName.end(),aName.begin(),toupper);
+  if (aName == "FILENAME")
+  {
+    std::string fileName = value.asUnquotedString();
+    FileValidator fval;
+    std::string error = fval.isValid(fileName);
+    if (error == "")
+    {
+      m_fileName = fileName;
+      m_wsName.clear();
+    }
+    else
+    {
+      return; // allow initialization with invalid attribute (for editing)
+    }
+    load(m_fileName);
+  }
+  else if (aName == "WORKSPACE") 
+  {
+    loadWorkspace( value.asString() );
+  }
+  else
+  {
+    IFunction::setAttribute(attName,value);
+  }
+}
+
+/// Return a value of attribute attName
+IFunction::Attribute TabulatedFunction::getAttribute(const std::string& attName)const
+{
+  std::string aName( attName );
+  std::transform(aName.begin(),aName.end(),aName.begin(),toupper);
+  if ( aName == "FILENAME" )
+  {
+    return Attribute( m_fileName, true );
+  }
+  else if ( aName == "WORKSPACE" )
+  {
+    return Attribute( m_wsName );
+  }
+  return IFunction::getAttribute( attName );
+}
+
+/**
+ * Decide whether to load the file as an ASCII file or as a Nexus file.
+ * @param fname :: The file name
+ */
+void TabulatedFunction::load(const std::string& fname)
+{
+  m_xData.clear();
+  m_yData.clear();
+
+  size_t format = fname.find(".nxs");
+  if ( format != std::string::npos )
+  {
+    loadNexus(fname);
+  }
+  else
+  {
+    loadAscii(fname);
+  }
+
+}
+
+/**
+ * Load input file as an Ascii file.
+ * @param fname :: The file name
+ */
+void TabulatedFunction::loadAscii(const std::string& fname)
+{
+  std::ifstream fil(fname.c_str());
+  std::string str;
+
+  while(getline(fil,str))
+  {
+    str += ' ';
+    std::istringstream istr(str);
+    double x,y;
+    istr >> x >> y;
+    if (!istr.good())
+    {
+      break;
+    }
+    m_xData.push_back(x);
+    m_yData.push_back(y);
+  }
+
+  if (m_xData.size() < 2)
+  {
+    m_xData.clear();
+    m_yData.clear();
+    throw std::runtime_error("TabulatedFunction: too few data points");
+  }
+  m_xStart = m_xData.front();
+  m_xEnd   = m_xData.back();
+}
+
+/**
+ * Load input file as a Nexus file.
+ * @param fname :: The file name
+ */
+void TabulatedFunction::loadNexus(const std::string& fname)
+{
+  IAlgorithm_sptr loadNxs = Mantid::API::AlgorithmFactory::Instance().create("LoadNexus", -1);
+  loadNxs->initialize();
+  loadNxs->setChild(true);
+  loadNxs->setLogging(false);
+  try
+  {
+    loadNxs->setPropertyValue("Filename", fname);
+    loadNxs->setPropertyValue("OutputWorkspace", "_TabulatedFunction_fit_data_");
+    loadNxs->execute();
+  }
+  catch ( std::runtime_error & )
+  {
+    throw std::runtime_error("Unable to load Nexus file for TabulatedFunction function.");
+  }
+  
+  Workspace_sptr ws = loadNxs->getProperty("OutputWorkspace");
+  MatrixWorkspace_sptr resData = boost::dynamic_pointer_cast<Mantid::API::MatrixWorkspace>(ws);
+  loadWorkspace( resData );
+  
+}
+
+/**
+ * Load the points from a MatrixWorkspace
+ * @param wsName :: The workspace to load from
+ */
+void TabulatedFunction::loadWorkspace(const std::string& wsName)
+{
+  try
+  {
+    auto ws = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>( wsName );
+    loadWorkspace( ws );
+    m_wsName = wsName;
+    m_fileName.clear();
+  }
+  catch( ... )
+  {
+    g_log.error() << "Workspace " << wsName << " not found \n";
+    clear();
+  }
+}
+
+/**
+ * Load the points from a MatrixWorkspace
+ * @param ws :: The workspace to load from
+ */
+void TabulatedFunction::loadWorkspace(boost::shared_ptr<MatrixWorkspace> ws)
+{
+  const bool hist = ws->isHistogramData();
+  const size_t nbins = ws->blocksize();
+
+  double first = ws->readX(0)[0];
+  double last;
+  if ( hist ) { last = ws->readX(0)[nbins]; }
+  else { last = ws->readX(0)[nbins-1]; }
+
+  for ( size_t i = 0; i < nbins; i++ )
+  {
+    double x = 0.0;
+    m_yData.push_back(ws->readY(0)[i]);
+    if ( hist ) x = ( ws->readX(0)[i] + ws->readX(0)[i+1] ) / 2;
+    else x = ws->readX(0)[i];
+    m_xData.push_back( x );
+  }
+  m_xStart = m_xData.front();
+  m_xEnd   = m_xData.back();
+}
+
+
+} // namespace CurveFitting
+} // namespace Mantid
