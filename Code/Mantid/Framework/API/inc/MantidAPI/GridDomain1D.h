934ab9fbe15c996d1f2e0a88d25d470fec2bc304
Jose Borreguero <borreguero@gmail.com>
2012-09-10 17:06:58 -0400

    Refs #5827 Define destructor for GridDomain1D

--- Code/Mantid/Framework/API/inc/MantidAPI/GridDomain1D.h
+++ Code/Mantid/Framework/API/inc/MantidAPI/GridDomain1D.h
@@ -45,7 +45,7 @@ class MANTID_API_DLL GridDomain1D: public API::GridDomain
 {
 public:
   GridDomain1D() {};
-  virtual ~GridDomain1D();
+  virtual ~GridDomain1D() {};
   /// initialize
   void initialize(double &startX, double &endX, size_t &n, std::string scaling);
   /// number of grid point	s
