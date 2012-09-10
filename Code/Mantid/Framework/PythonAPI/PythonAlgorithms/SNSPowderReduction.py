c23ac20817b099addd33895f98eb56f6f4256f5f
Vickie Lynch <lynchve@ornl.gov>
2012-09-10 10:24:04 -0400

    Refs #5740 binning in wrong units for final Rebin

--- Code/Mantid/Framework/PythonAPI/PythonAlgorithms/SNSPowderReduction.py
+++ Code/Mantid/Framework/PythonAPI/PythonAlgorithms/SNSPowderReduction.py
@@ -364,7 +364,7 @@ class SNSPowderReduction(PythonAlgorithm):
             api.SaveFocusedXYE(InputWorkspace=wksp, StartAtBankNumber=info.bank, Filename=filename+".dat")          
         if "NeXus" in self._outTypes:
             api.ConvertUnits(InputWorkspace=wksp, OutputWorkspace=wksp, Target=self.getProperty("FinalDataUnits").value)
-            api.Rebin(InputWorkspace=wksp, OutputWorkspace=wksp, Params=self._binning) # crop edges
+            #api.Rebin(InputWorkspace=wksp, OutputWorkspace=wksp, Params=self._binning) # crop edges
             api.SaveNexus(InputWorkspace=wksp, Filename=filename+".nxs")
 
         # always save python script
