e49d587b0ebb2819de87062ff5600b133aeaf1dd
Jean Bilheux <bilheuxjm@ornl.gov>
2012-09-26 11:22:23 -0400

    Removed code that does not work yet to link python to c++. This refs #5801

---
+++ Code/Mantid/MantidQt/RefDetectorViewer/inc/MantidQtRefDetectorViewer/RefMatrixWSImageView.h
@@ -0,0 +1,60 @@
+#ifndef REF_MATRIX_WS_IMAGE_VIEW_H
+#define REF_MATRIX_WS_IMAGE_VIEW_H
+
+#include "MantidQtRefDetectorViewer/DllOptionIV.h"
+#include "MantidAPI/MatrixWorkspace.h"
+
+#include "MantidQtRefDetectorViewer/ImageView.h"
+
+/**
+    @class RefMatrixWSImageView 
+  
+       This is the top level class for showing a matrix workspace
+    using an ImageViewer.
+ 
+    @author Dennis Mikkelson 
+    @date   2012-05-08 
+     
+    Copyright © 2012 ORNL, STFC Rutherford Appleton Laboratories
+  
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Code Documentation is available at 
+                 <http://doxygen.mantidproject.org>
+ */
+
+namespace MantidQt
+{
+namespace RefDetectorViewer
+{
+
+class EXPORT_OPT_MANTIDQT_IMAGEVIEWER RefMatrixWSImageView 
+{
+  public:
+
+    /// Construct an image viewer for the specifed MatrixWorkspace
+    RefMatrixWSImageView( Mantid::API::MatrixWorkspace_sptr mat_ws );
+
+   ~RefMatrixWSImageView();
+
+  private:
+    ImageView *image_view;
+};
+
+} // namespace MantidQt 
+} // namespace ImageView 
+
+#endif // REF_MATRIX_WS_IMAGE_VIEW_H
---
+++ Code/Mantid/MantidQt/RefDetectorViewer/src/RefMatrixWSImageView.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "MantidQtRefDetectorViewer/RefMatrixWSImageView.h"
+#include "MantidQtRefDetectorViewer/RefMatrixWSDataSource.h"
+
+using Mantid::API::MatrixWorkspace_sptr;
+using namespace MantidQt;
+using namespace RefDetectorViewer;
+
+/**
+ * Construct an ImageView for the specified matrix workspace
+ */
+RefMatrixWSImageView::RefMatrixWSImageView( MatrixWorkspace_sptr mat_ws )
+{
+  RefMatrixWSDataSource* source = new RefMatrixWSDataSource( mat_ws );
+
+  image_view = new ImageView( source );  // this is the QMainWindow
+                                         // for the viewer.  It is
+                                         // deleted when the window
+                                         // is closed
+}
+
+RefMatrixWSImageView::~RefMatrixWSImageView()
+{
+  // nothing to do here, since image_view is deleted when the window closes
+}
+
