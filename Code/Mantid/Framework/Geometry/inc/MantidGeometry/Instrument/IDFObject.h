70ee19bfc72071b5b639c85ae53566b060764022
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-10-03 15:19:51 +0100

    refs #5900. Virtual File Object.

---
+++ Code/Mantid/Framework/Geometry/inc/MantidGeometry/Instrument/IDFObject.h
@@ -0,0 +1,58 @@
+#ifndef MANTID_GEOMETRY_IDFOBJECT_H_
+#define MANTID_GEOMETRY_IDFOBJECT_H_
+
+#include "MantidKernel/System.h"
+#include <Poco/Timestamp.h>
+#include <Poco/File.h>
+#include <Poco/Path.h>
+
+namespace Mantid
+{
+namespace Geometry
+{
+
+  /** IDFObject : File object wrapper over an IDF file.
+
+  This is essentially an adapter for a Poco::File, with some extra convenience methods allowing easy access to the parent directory path.
+  This type has the last modified date and the exists method as a virtual methods to facilite testing.
+    
+    Copyright &copy; 2012 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://svn.mantidproject.org/mantid/trunk/Code/Mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+  */
+  class DLLExport IDFObject 
+  {
+  public:
+    IDFObject(const std::string& fileName);
+    Poco::Path getParentDirectory() const;
+    Poco::Path getFileFullPath() const;
+    std::string getFileNameOnly() const;
+    virtual Poco::Timestamp  getLastModified() const;
+    virtual bool exists() const;
+    virtual ~IDFObject();
+
+  private:
+    Poco::File m_defFile;
+  };
+
+
+} // namespace Geometry
+} // namespace Mantid
+
+#endif  /* MANTID_GEOMETRY_IDFOBJECT_H_ */
---
+++ Code/Mantid/Framework/Geometry/test/IDFObjectTest.h
@@ -0,0 +1,69 @@
+#ifndef MANTID_GEOMETRY_IDFOBJECTTEST_H_
+#define MANTID_GEOMETRY_IDFOBJECTTEST_H_
+
+#include <cxxtest/TestSuite.h>
+#include "MantidKernel/ConfigService.h"
+#include "MantidGeometry/instrument/IDFObject.h"
+#include <Poco/Path.h>
+
+using Mantid::Geometry::IDFObject;
+using Mantid::Kernel::ConfigService;
+
+class IDFObjectTest : public CxxTest::TestSuite
+{
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static IDFObjectTest *createSuite() { return new IDFObjectTest(); }
+  static void destroySuite( IDFObjectTest *suite ) { delete suite; }
+
+  void testExists()
+  {
+    const std::string filename = ConfigService::Instance().getInstrumentDirectory() + "/IDFs_for_UNIT_TESTING/IDF_for_UNIT_TESTING.xml";
+    IDFObject obj(filename);
+    TS_ASSERT(obj.exists());
+  }
+
+  void testDoesntExist()
+  {
+    const std::string filename = "made_up_file.xml";
+    IDFObject obj(filename);
+    TS_ASSERT(!obj.exists());
+  }
+
+  void testGetParentDirectory()
+  {
+    const Poco::Path expectedDir = Poco::Path( ConfigService::Instance().getInstrumentDirectory() + "/IDFs_for_UNIT_TESTING/" );
+    std::string filename = expectedDir.toString() + "IDF_for_UNIT_TESTING.xml";
+    IDFObject obj(filename);
+    TS_ASSERT_EQUALS(expectedDir.toString(), obj.getParentDirectory().toString());
+  }
+
+  void testGetFullPath()
+  {
+    const std::string filename = ConfigService::Instance().getInstrumentDirectory() + "/IDFs_for_UNIT_TESTING/IDF_for_UNIT_TESTING.xml";
+    IDFObject obj(filename);
+    TS_ASSERT_EQUALS(Poco::Path(filename).toString(), obj.getFileFullPath().toString());
+  }
+
+  void testGetLastModified()
+  {
+    const std::string filenameonly = "IDF_for_UNIT_TESTING.xml";
+    const std::string filename = ConfigService::Instance().getInstrumentDirectory() + "/IDFs_for_UNIT_TESTING/" + filenameonly;
+    IDFObject obj(filename);
+    TS_ASSERT_EQUALS(filenameonly, obj.getFileNameOnly());
+  }
+
+  void testGetModifiedTimestamp()
+  {
+    const std::string filename = ConfigService::Instance().getInstrumentDirectory() + "/IDFs_for_UNIT_TESTING/IDF_for_UNIT_TESTING.xml";
+    Poco::File file(filename);
+
+    IDFObject obj(filename);
+    TS_ASSERT_EQUALS(file.getLastModified(), obj.getLastModified());
+  }
+
+};
+
+
+#endif /* MANTID_GEOMETRY_IDFOBJECTTEST_H_ */
