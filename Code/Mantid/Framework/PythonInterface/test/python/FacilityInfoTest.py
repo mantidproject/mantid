a7ff3a64347610b89de74d1baf396b926a6b8535
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-10-10 13:39:37 +0100

    refs #5821. Modify python test.

--- Code/Mantid/Framework/PythonInterface/test/python/FacilityInfoTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/FacilityInfoTest.py
@@ -19,7 +19,7 @@ class FacilityInfoTest(unittest.TestCase):
         self.assertEquals(test_facility.preferredExtension(), ".nxs")
         self.assertEquals(test_facility.getSoapEndPoint(), "https://facilities01.esc.rl.ac.uk:443/ICATService/ICAT")
         self.assertEquals(len(test_facility.archiveSearch()), 1)
-        self.assertEquals(len(test_facility.instruments()), 31)
+        self.assertEquals(len(test_facility.instruments()), 32)
         self.assertEquals(len(test_facility.instruments("Neutron Diffraction")), 11)
         self.assertTrue(isinstance(test_facility.instrument("WISH"), InstrumentInfo))
         self.assertEquals(test_facility.catalogName(), "ICat3Catalog")
