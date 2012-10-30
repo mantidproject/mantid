4aca604779ffc8bca9c7d248c73319e9377760eb
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-10-30 21:54:05 +0000

    Clean orphaned pyc files. Refs #6052
    Make sure those deleted from git are truely gone.

--- Code/Mantid/Build/CMake/MantidUtils.cmake
+++ Code/Mantid/Build/CMake/MantidUtils.cmake
@@ -109,3 +109,25 @@ function( INSTALL_PYTHON_FILES SRC_DIR PY_FILES DEST_DIR )
   endforeach( PYFILE )
   install ( FILES ${PY_INSTALL_FILES} DESTINATION ${DEST_DIR} )
 endfunction( INSTALL_PYTHON_FILES )
+
+#######################################################################
+
+# NAME: CLEAN_ORPHANED_PYC_FILES
+# Given a directory, glob for .pyc files and if there is not a .py file
+# next to it. Assumes that the .pyc was not under source control. Note
+# that the removal happens when cmake is run
+#   Parameters:
+#      ROOT_DIR - The root directory to start the search (recursive)
+function ( CLEAN_ORPHANED_PYC_FILES ROOT_DIR )
+  file ( GLOB_RECURSE PYC_FILES ${ROOT_DIR}/*.pyc )
+  if ( PYC_FILES )
+    foreach ( PYC_FILE ${PYC_FILES} )
+      get_filename_component ( DIR_COMP ${PYC_FILE} PATH )
+      get_filename_component ( STEM ${PYC_FILE} NAME_WE )
+      set ( PY_FILE ${DIR_COMP}/${STEM}.py )
+      if ( NOT EXISTS ${PY_FILE} )
+        execute_process ( COMMAND ${CMAKE_COMMAND} -E remove -f ${PYC_FILE} )
+      endif()
+    endforeach()
+  endif()
+endfunction()
