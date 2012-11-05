bc343009b0eb257cb13e2d7ac99103192dbcd55c
Michael Reuter <reuterma@ornl.gov>
2012-11-05 17:04:19 -0500

    Refs #6094. Fix category for workflow algorithm.

--- Code/Mantid/Framework/WorkflowAlgorithms/src/DgsAbsoluteUnitsReduction.cpp
+++ Code/Mantid/Framework/WorkflowAlgorithms/src/DgsAbsoluteUnitsReduction.cpp
@@ -47,7 +47,7 @@ namespace Mantid
     int DgsAbsoluteUnitsReduction::version() const { return 1; };
 
     /// Algorithm's category for identification. @see Algorithm::category
-    const std::string DgsAbsoluteUnitsReduction::category() const { return "General"; }
+    const std::string DgsAbsoluteUnitsReduction::category() const { return "Workflow\\Inelastic"; }
 
     //----------------------------------------------------------------------------------------------
     /// Sets documentation strings for this algorithm
