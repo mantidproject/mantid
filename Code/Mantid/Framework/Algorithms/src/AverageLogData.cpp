5654537739d7e028eece7180e178db9fffd6c090
Andrei Savici <saviciat@ornl.gov>
2012-11-12 16:05:51 -0500

    Fix typo in wiki section. Refs #460

--- Code/Mantid/Framework/Algorithms/src/AverageLogData.cpp
+++ Code/Mantid/Framework/Algorithms/src/AverageLogData.cpp
@@ -1,5 +1,5 @@
 /*WIKI*
-The algorithm will calculate a proton_charge weighted averaf=ge and standard deviation of any log value of numeric series type.
+The algorithm will calculate a proton_charge weighted average and standard deviation of any log value of numeric series type.
 All proton charges earlier than the first data are ignored. Any proton pulse is counted for the log value on the right. This
 means that if all proton pulses happen before the first value, and FixZero is false, the average and standard deviations are NANs.
 If all the proton pulses occur after the last value, and FixZero is false, the average is equal to the last value, and the
