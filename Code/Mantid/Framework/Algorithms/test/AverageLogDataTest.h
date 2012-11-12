4d150d52aebd9d93473680a0f906371705f422bc
Andrei Savici <saviciat@ornl.gov>
2012-11-12 12:51:42 -0500

    AverageLogData, test is just a placeholder. Refs #460

---
+++ Code/Mantid/Framework/Algorithms/test/AverageLogDataTest.h
@@ -0,0 +1,56 @@
+#ifndef MANTID_ALGORITHMS_AverageLogDATATEST_H_
+#define MANTID_ALGORITHMS_AverageLogDATATEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidAlgorithms/AverageLogData.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+using Mantid::Algorithms::AverageLogData;
+
+class AverageLogDataTest : public CxxTest::TestSuite
+{
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static AverageLogDataTest *createSuite() { return new AverageLogDataTest(); }
+  static void destroySuite( AverageLogDataTest *suite ) { delete suite; }
+
+
+  void test_Init()
+  {
+    AverageLogData alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+  }
+  
+  void test_exec()
+  {
+    // Name of the output workspace.
+   /* std::string WSName("AverageLogDataTest_OutputWS");
+
+    AverageLogData alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("REPLACE_PROPERTY_NAME_HERE!!!!", "value") );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+    /*
+    // Retrieve the workspace from data service. TODO: Change to your desired type
+    Workspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
+    TS_ASSERT(ws);
+    if (!ws) return;
+    
+    // TODO: Check the results
+    
+    // Remove workspace from the data service.
+    AnalysisDataService::Instance().remove(outWSName);*/
+  }
+  
+
+
+};
+
+
+#endif /* MANTID_ALGORITHMS_AverageLogDATATEST_H_ */
