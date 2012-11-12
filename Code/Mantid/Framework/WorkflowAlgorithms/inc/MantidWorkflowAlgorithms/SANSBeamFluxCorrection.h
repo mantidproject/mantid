f37d01242298407dff7fff72709d8c626b32650a
Mathieu Doucet <doucetm@ornl.gov>
2012-11-12 16:45:49 -0500

    Re #6077 First cut for EQSANS beam flux corr

---
+++ Code/Mantid/Framework/WorkflowAlgorithms/inc/MantidWorkflowAlgorithms/SANSBeamFluxCorrection.h
@@ -0,0 +1,49 @@
+#ifndef MANTID_ALGORITHMS_SANSBEAMFLUXCORRECTION_H_
+#define MANTID_ALGORITHMS_SANSBEAMFLUXCORRECTION_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/DataProcessorAlgorithm.h"
+
+namespace Mantid
+{
+namespace WorkflowAlgorithms
+{
+/**
+    Performs beam flux correction on TOF SANS data.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport SANSBeamFluxCorrection : public API::DataProcessorAlgorithm
+{
+public:
+  /// (Empty) Constructor
+	SANSBeamFluxCorrection() : API::DataProcessorAlgorithm() {}
+  /// Virtual destructor
+  virtual ~SANSBeamFluxCorrection() {}
+  /// Algorithm's name
+  virtual const std::string name() const { return "SANSBeamFluxCorrection"; }
+  /// Algorithm's version
+  virtual int version() const { return (1); }
+  /// Algorithm's category for identification
+  virtual const std::string category() const { return "Workflow\\SANS;CorrectionFunctions\\InstrumentCorrections"; }
+
+private:
+  /// Sets documentation strings for this algorithm
+  virtual void initDocs();
+  /// Initialisation code
+  void init();
+  /// Execution code
+  void exec();
+  void execEvent();
+  API::MatrixWorkspace_sptr loadReference();
+  boost::shared_ptr<Kernel::PropertyManager> m_reductionManager;
+  std::string m_output_message;
+};
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_SANSBEAMFLUXCORRECTION_H_*/
