0acc9b0532b7c41a4ad5963079eca1c25c3cfb6c
Peter Peterson <petersonpf@ornl.gov>
2012-11-12 14:59:28 -0500

    Refs #6101. Autogenerate fit function index.

--- Code/Mantid/docs/qtassistant/make_fitfunctions_help.py
+++ Code/Mantid/docs/qtassistant/make_fitfunctions_help.py
@@ -19,24 +19,8 @@ def addWikiDir(helpsrcdir):
     wikitoolsloc = os.path.abspath(wikitoolsloc)
     sys.path.append(wikitoolsloc)
 
-def genCatElement(category):
-    category = category.split('/')
-    text = "<li>"
-    filename = 'AlgoCat_' + category[0] + '.html'
-    url = filename
-    if len(category) > 1:
-        text += '/'.join(category[:-1]) + '/'
-        url += '#' + '_'.join(category[1:])
-    text += '<a href="%s">%s</a></li>' % (url, category[-1])
-    return lxml.html.fragment_fromstring(text)
-
-def genAlgoElement(name, versions):
-    text = '<li><a href="%s">%s' % (WEB_BASE+name, name)
-    text += ' v%d</a>' % versions[-1]
-
-    if len(versions) > 1:
-        text += ', ' + ', '.join(['v'+str(version) for version in versions[:-1]])
-    text += '</li>'
+def genFuncElement(name):
+    text = '<a href="%s">%s</a>' % (WEB_BASE+name, name)
     return lxml.html.fragment_fromstring(text)
 
 def processCategories(categories, qhp, outputdir):
@@ -69,7 +53,8 @@ def processCategories(categories, qhp, outputdir):
             #body.append(lh.H2(subcategory))
             ul = le.SubElement(body, "ul")
             for (name, versions) in categories[subcategory]:
-                ul.append(genAlgoElement(name, versions))
+                li = le.SubElement(ul, "li")
+                li.append(genAlgoElement(name, versions))
 
         filename = "AlgoCat_%s.html" % page_name
         qhp.addFile(filename, page_name+" Algorithm Category")
@@ -77,87 +62,48 @@ def processCategories(categories, qhp, outputdir):
         handle = open(filename, 'w')
         handle.write(le.tostring(root, pretty_print=True, xml_declaration=False))
 
-def process(algos, qhp, outputdir):
-    from MantidFramework import mtd
+def process(functionsalgos, qhp, outputdir):
+    import mantid.api
 
     # sort algorithms into categories
     categories = {}
-    for (name, versions) in algos:
-        alg = mtd.createAlgorithm(name, versions[-1])
-        for category in alg.categories():
+    for name in functions:
+        func = mantid.api.FunctionFactory.createFunction(name)
+        for category in func.categories():
             category = category.replace('\\', '/')
             if not categories.has_key(category):
                 categories[category] = []
-            categories[category].append((name, versions))
+            categories[category].append(name)
     categories_list = categories.keys()
     categories_list.sort()
-
-    # sort algorithms into letter based groups
-    letter_groups = {}
-    for (name, versions) in algos:
-        letter = str(name)[0].upper()
-        if not letter_groups.has_key(letter):
-            letter_groups[letter] = []
-        letter_groups[letter].append((str(name), versions))
 
     ##### put together the top of the html document
     root = le.Element("html")
     head = le.SubElement(root, "head")
-    head.append(lh.META(lh.TITLE("Algorithms Index")))
+    head.append(lh.META(lh.TITLE("Fit Functions Index")))
     body = le.SubElement(root, "body")
-    body.append(lh.CENTER(lh.H1("Algorithms Index")))
+    body.append(lh.CENTER(lh.H1("Fit Functions Index")))
 
     ##### section for categories 
-    div_cat = le.SubElement(body, "div", **{"id":"alg_cats"})
-    div_cat.append(lh.H2("Subcategories"))
-    above = None
-    ul = le.SubElement(div_cat, "ul")
+    div_cat = le.SubElement(body, "div", **{"id":"function_cats"})
     for category in categories_list:
-        ul.append(genCatElement(category))
+        div_cat.append(lh.H2(category + " Category"))
+        ul = le.SubElement(div_cat, "ul")
+        funcs = categories[category]
+        for func in funcs:
+            li = le.SubElement(ul, "li")
+            li.append(genFuncElement(func))
 
-    ##### section for alphabetical 
-    div_alpha = le.SubElement(body, "div", **{"id":"alg_alpha"})
-    div_alpha.append(lh.H2("Alphabetical"))
-
-    letters = letter_groups.keys()
-    letters.sort()
-
-    # print an index within the page
-    para_text = "<p>"
-    for letter in map(chr, range(65, 91)):
-        if letter in letters:
-            para_text += "<a href='#algo%s'>%s</a> " % (letter, letter)
-        else:
-            para_text += letter + ' '
-    para_text += "</p>"
-    div_alpha.append( lxml.html.fragment_fromstring(para_text))
-
-    # print the list of algorithms by name
-    for letter in letters:
-        temp = le.SubElement(div_alpha, 'h3')
-        le.SubElement(temp, 'a', **{"name":'algo'+letter})
-        temp.text = letter
-
-        ul = le.SubElement(div_alpha, "ul")
-        for (name, versions) in letter_groups[letter]:
-            ul.append(genAlgoElement(name, versions))
-
-    # print an index within the page
-    div_alpha.append( lxml.html.fragment_fromstring(para_text))
-
-    filename = os.path.join(outputdir, "algorithms_index.html")
+    filename = os.path.join(outputdir, "fitfunctions_index.html")
     handle = open(filename, 'w')
     handle.write(le.tostring(root, pretty_print=True, xml_declaration=False))
 
     shortname = os.path.split(filename)[1]
-    qhp.addFile(shortname, "Algorithms Index")
-
-    # create all of the category pages
-    processCategories(categories, qhp, outputdir)
+    qhp.addFile(shortname, "Fit Functions Index")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate qtassistant docs " \
-                  + "for the algorithms")
+                  + "for the fit functions")
     defaultmantidpath = ""
     parser.add_argument('-m', '--mantidpath', dest='mantidpath',
                         default=defaultmantidpath,
@@ -174,10 +120,11 @@ if __name__ == "__main__":
     # initialize mantid
     import wiki_tools
     wiki_tools.initialize_Mantid(args.mantidpath)
-    algos = wiki_tools.get_all_algorithms(True)
+    import mantid.api
+    functions = mantid.api.FunctionFactory.getFunctionNames()
 
     # setup the qhp file
-    qhp = QHPFile("org.mantidproject.algorithms")
+    qhp = QHPFile("org.mantidproject.fitfunctions")
 
-    process(algos, qhp, helpoutdir)
-    qhp.write(os.path.join(helpoutdir, "algorithms.qhp"))
+    process(functions, qhp, helpoutdir)
+    qhp.write(os.path.join(helpoutdir, "fitfunctions.qhp"))
