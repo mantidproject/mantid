567d19fc0b841616a9678ee7c6470314b8404929
Michael Reuter <reuterma@ornl.gov>
2012-11-14 12:06:43 -0500

    Refs #6119. Getting UI wired up to run.

--- Code/Mantid/scripts/Interface/reduction_gui/widgets/inelastic/dgs_pd_sc_conversion.py
+++ Code/Mantid/scripts/Interface/reduction_gui/widgets/inelastic/dgs_pd_sc_conversion.py
@@ -1,7 +1,9 @@
 from PyQt4 import QtGui, uic, QtCore
+from functools import partial
 from reduction_gui.widgets.base_widget import BaseWidget
 from reduction_gui.reduction.inelastic.dgs_pd_sc_conversion_script import PdAndScConversionScript
 import ui.inelastic.ui_dgs_pd_sc_conversion
+import reduction_gui.widgets.util as util
 
 class PdAndScConversionWidget(BaseWidget):
     """
@@ -22,27 +24,83 @@ class PdAndScConversionWidget(BaseWidget):
                 
         self._content = PdAndScConversionFrame(self)
         self._layout.addWidget(self._content)
-        self.initialize_content()
         self._instrument_name = settings.instrument_name
-        
+        self.initialize_content()
+                
         if state is not None:
             self.set_state(state)
         else:
             self.set_state(PdAndScConversionScript(self._instrument_name))
     
-    def initalize_content(self):
-        pass
+    def initialize_content(self):
+        # Constraints
+        self._content.q_low_edit.setValidator(QtGui.QDoubleValidator(self._content.q_low_edit))
+        self._content.q_width_edit.setValidator(QtGui.QDoubleValidator(self._content.q_width_edit))
+        self._content.q_high_edit.setValidator(QtGui.QDoubleValidator(self._content.q_high_edit))
+        
+        # Default states
+        self._save_powder_nxs_state(self._content.save_procnexus_cb.isChecked())
+        
+        # Connections
+        self.connect(self._content.save_procnexus_save, QtCore.SIGNAL("clicked()"),
+                     self._save_powder_nxs_save)
+        self.connect(self._content.save_procnexus_cb, QtCore.SIGNAL("toggled(bool)"),
+                     self._save_powder_nxs_state)
+        
+        # Validate widgets
+        self._connect_validated_lineedit(self._content.q_low_edit)
+        self._connect_validated_lineedit(self._content.q_width_edit)
+        self._connect_validated_lineedit(self._content.q_high_edit)
+            
+    def _check_and_set_lineedit_content(self, lineedit, content):
+        lineedit.setText(content)
+        util.set_valid(lineedit, not lineedit.text().isEmpty())
+
+    def _connect_validated_lineedit(self, ui_ctrl):
+        call_back = partial(self._validate_edit, ctrl=ui_ctrl)
+        self.connect(ui_ctrl, QtCore.SIGNAL("editingFinished()"), call_back)
+        self.connect(ui_ctrl, QtCore.SIGNAL("textEdited(QString)"), call_back)
+        self.connect(ui_ctrl, QtCore.SIGNAL("textChanged(QString)"), call_back)
+
+    def _validate_edit(self, ctrl=None):
+        is_valid = True
+        if ctrl.text().isEmpty():
+            is_valid = False
+        util.set_valid(ctrl, is_valid)   
+    
+    def _save_powder_nxs_state(self, state):
+        self._content.save_procnexus_edit.setEnabled(state)
+        self._content.save_procnexus_save.setEnabled(state)        
+    
+    def _save_powder_nxs_save(self):
+        fname = self.data_save_dialog("*.nxs")
+        if fname:
+            self._content.save_procnexus_edit.setText(fname)   
     
     def set_state(self, state):
         """
             Populate the UI elements with the data from the given state.
             @param state: PdAndScConversionScript object
-        """    
-        pass
+        """  
+        self._content.powder_gb.setChecked(state.do_pd_convert)
+        self._check_and_set_lineedit_content(self._content.q_low_edit, 
+                                             state.pd_q_range_low)
+        self._check_and_set_lineedit_content(self._content.q_width_edit, 
+                                             state.pd_q_range_width)
+        self._check_and_set_lineedit_content(self._content.q_high_edit,
+                                             state.pd_q_range_high)
+        self._content.save_procnexus_cb.setChecked(state.save_powder_nxs)
+        self._content.save_procnexus_edit.setText(state.save_powder_nxs_file)
     
     def get_state(self):
         """
             Returns an object with the state of the interface
         """
         p = PdAndScConversionScript(self._instrument_name)
+        p.do_pd_convert = self._content.powder_gb.isChecked()
+        p.pd_q_range_low = self._content.q_low_edit.text()
+        p.pd_q_range_width = self._content.q_width_edit.text()
+        p.pd_q_range_high = self._content.q_high_edit.text()
+        p.save_powder_nxs = self._content.save_procnexus_cb.isChecked()
+        p.save_powder_nxs_file = self._content.save_procnexus_edit.text()
         return p
