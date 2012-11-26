a5daeafb1db399b6badf91f4570edc929a53ccd3
Mathieu Doucet <doucetm@ornl.gov>
2012-11-26 15:07:46 -0500

    Re #6151 Ensure backward compatibility

--- Code/Mantid/Framework/PythonAPI/PythonAlgorithms/WorkflowAlgorithms/HFIRSANSReduction.py
+++ Code/Mantid/Framework/PythonAPI/PythonAlgorithms/WorkflowAlgorithms/HFIRSANSReduction.py
@@ -7,6 +7,8 @@ from mantid.kernel import *
 class HFIRSANSReduction(PythonAlgorithm):
 
     def PyInit(self):
+        #TODO: allow for multiple files to be summed 
+        #TODO: allow for input workspace instead of file
         self.declareProperty(FileProperty("Filename", "",
                                           action=FileAction.Load, extensions=['xml']))
         self.declareProperty("ReductionProperties", "__sans_reduction_properties", 
@@ -25,9 +27,17 @@ class HFIRSANSReduction(PythonAlgorithm):
         property_manager = PropertyManagerDataService.retrieve(property_manager_name)
         
         property_list = [p.name for p in property_manager.getProperties()]
-
+        
+        output_msg = ""
         # Find the beam center
-        self.find_beam_center(property_manager)
+        if "SANSBeamFinderAlgorithm" in property_list:
+            p=property_manager.getProperty("SANSBeamFinderAlgorithm")
+            alg=Algorithm.fromString(p.valueAsStr)
+            if alg.existsProperty("ReductionProperties"):
+                alg.setProperty("ReductionProperties", property_manager_name)
+            alg.execute()
+            if alg.existsProperty("OutputMessage"):
+                output_msg += alg.getProperty("OutputMessage").value+'\n'
         
         # Load the sample data
         if "LoadAlgorithm" not in property_list:
@@ -38,7 +48,7 @@ class HFIRSANSReduction(PythonAlgorithm):
         alg.setProperty("OutputWorkspace", output_ws)
         alg.setProperty("ReductionProperties", property_manager_name)
         alg.execute()
-        output_msg = "Loaded %s\n" % filename
+        output_msg += "Loaded %s\n" % filename
         if alg.existsProperty("OutputMessage"):
             output_msg += alg.getProperty("OutputMessage").value
 
@@ -48,6 +58,18 @@ class HFIRSANSReduction(PythonAlgorithm):
         # Sample data transmission correction
         
         # Process background data
+        if "BackgroundFiles" in property_list:
+            background = property_manager.getProperty("LoadAlgorithm").valueAsStr
+            background_ws = "__background_%s" % output_ws
+            p=property_manager.getProperty("LoadAlgorithm")
+            alg=Algorithm.fromString(p.valueAsStr)
+            alg.setProperty("Filename", background)
+            alg.setProperty("OutputWorkspace", background_ws)
+            alg.setProperty("ReductionProperties", property_manager_name)
+            alg.execute()
+            output_msg += "Loaded background %s\n" % background
+            if alg.existsProperty("OutputMessage"):
+                output_msg += alg.getProperty("OutputMessage").value
         
         # Background transmission correction
         
@@ -56,7 +78,20 @@ class HFIRSANSReduction(PythonAlgorithm):
         # Absolute scale correction
         
         # Compute I(q)
-        
+        print "----------------------\n\n\n\n"
+        print property_list
+        if "IQAlgorithm" in property_list:
+            print "TEST"
+            iq_output = output_ws+'_Iq'
+            p=property_manager.getProperty("IQAlgorithm")
+            alg=Algorithm.fromString(p.valueAsStr)
+            alg.setProperty("InputWorkspace", output_ws)
+            alg.setProperty("OutputWorkspace", iq_output)
+            alg.setProperty("ReductionProperties", property_manager_name)
+            alg.execute()
+            if alg.existsProperty("OutputMessage"):
+                output_msg += alg.getProperty("OutputMessage").value            
+
         # Compute I(qx,qy)
         
         # Save data
@@ -65,9 +100,6 @@ class HFIRSANSReduction(PythonAlgorithm):
         self.setPropertyValue("OutputWorkspace", output_ws)
         self.setProperty("OutputMessage", output_msg)
         
-    def find_beam_center(self, property_manager):
-        pass
-    
     def process_data_file(self, workspace):
         output_msg = ""
         property_manager_name = self.getProperty("ReductionProperties").value
@@ -113,6 +145,17 @@ class HFIRSANSReduction(PythonAlgorithm):
                 output_msg += alg.getProperty("OutputMessage").value+'\n'
         
         # Sensitivity correction
+        if "SensitivityAlgorithm" in property_list:
+            p=property_manager.getProperty("SensitivityAlgorithm")
+            alg=Algorithm.fromString(p.valueAsStr)
+            alg.setProperty("InputWorkspace", workspace)
+            alg.setProperty("OutputWorkspace", workspace)
+            if alg.existsProperty("ReductionProperties"):
+                alg.setProperty("ReductionProperties", property_manager_name)
+            alg.execute()
+            if alg.existsProperty("OutputMessage"):
+                output_msg += alg.getProperty("OutputMessage").value+'\n'
+
         return output_msg
         
 
