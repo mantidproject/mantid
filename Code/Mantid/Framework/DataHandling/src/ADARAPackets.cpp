mantidproject/mantid

commit d5de7a2329b84f498a574afc740df05a2314a1f2

Updateing ADARA packet parsing code
This is the latest code from the ADARA repo, including the new stream
annotation packet type.

Refs #6235

Code/Mantid/Framework/DataHandling/src/ADARAPackets.cpp
@@ -82,40 +82,50 @@ Packet::~Packet()
 /* ------------------------------------------------------------------------ */
 
 RawDataPkt::RawDataPkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len < (6 * sizeof(uint32_t)))
 		throw invalid_packet("RawDataPacket is too short");
 }
 
 RawDataPkt::RawDataPkt(const RawDataPkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload())
+	Packet(pkt), m_fields((const uint32_t *)payload())
 {}
 
 /* ------------------------------------------------------------------------ */
 
 RTDLPkt::RTDLPkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len != 120)
 		throw invalid_packet("RTDL Packet is incorrect length");
+
+	if ((m_fields[4] >> 24) != 4)
+		throw invalid_packet("Missing ring period");
 }
 
 RTDLPkt::RTDLPkt(const RTDLPkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload())
+	Packet(pkt), m_fields((const uint32_t *)payload())
+{}
+
+/* ------------------------------------------------------------------------ */
+
+SourceListPkt::SourceListPkt(const uint8_t *data, uint32_t len) :
+	Packet(data, len)
+{}
+
+SourceListPkt::SourceListPkt(const SourceListPkt &pkt) :
+	Packet(pkt)
 {}
 
 /* ------------------------------------------------------------------------ */
 
 BankedEventPkt::BankedEventPkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len < (4 * sizeof(uint32_t)))
 		throw invalid_packet("BankedEvent packet is too short");
-
-        m_lastFieldIndex = (payload_length() / 4) - 1;
 }
-
 
 BankedEventPkt::BankedEventPkt(const BankedEventPkt &pkt) :
   Packet(pkt), m_fields((const uint32_t *)payload()),
@@ -241,14 +251,14 @@ void BankedEventPkt::firstEventInBank() const
 /* ------------------------------------------------------------------------ */
 
 BeamMonitorPkt::BeamMonitorPkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len < (4 * sizeof(uint32_t)))
 		throw invalid_packet("BeamMonitor packet is too short");
 }
 
 BeamMonitorPkt::BeamMonitorPkt(const BeamMonitorPkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload())
+	Packet(pkt), m_fields((const uint32_t *)payload())
 {}
 
 /* ------------------------------------------------------------------------ */
@@ -264,14 +274,14 @@ PixelMappingPkt::PixelMappingPkt(const PixelMappingPkt &pkt) :
 /* ------------------------------------------------------------------------ */
 
 RunStatusPkt::RunStatusPkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len != (3 * sizeof(uint32_t)))
 		throw invalid_packet("RunStatus packet is incorrect size");
 }
 
 RunStatusPkt::RunStatusPkt(const RunStatusPkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload())
+	Packet(pkt), m_fields((const uint32_t *)payload())
 {}
 
 /* ------------------------------------------------------------------------ */
@@ -279,7 +289,7 @@ RunStatusPkt::RunStatusPkt(const RunStatusPkt &pkt) :
 RunInfoPkt::RunInfoPkt(const uint8_t *data, uint32_t len) :
 	Packet(data, len)
 {
-        uint32_t size = *(const uint32_t *) payload();
+	uint32_t size = *(const uint32_t *) payload();
 	const char *xml = (const char *) payload() + sizeof(uint32_t);
 
 	if (m_payload_len < sizeof(uint32_t))
@@ -302,9 +312,10 @@ RunInfoPkt::RunInfoPkt(const RunInfoPkt &pkt) :
 TransCompletePkt::TransCompletePkt(const uint8_t *data, uint32_t len) :
 	Packet(data, len)
 {
-        uint32_t size = *(const uint32_t *) payload();
+	uint32_t size = *(const uint32_t *) payload();
 	const char *reason = (const char *) payload() + sizeof(uint32_t);
 
+	m_status = (uint16_t) (size >> 16);
 	size &= 0xffff;
 	if (m_payload_len < sizeof(uint32_t))
 		throw invalid_packet("TransComplete packet is too short");
@@ -330,7 +341,7 @@ ClientHelloPkt::ClientHelloPkt(const uint8_t *data, uint32_t len) :
 	if (m_payload_len != sizeof(uint32_t))
 		throw invalid_packet("ClientHello packet is incorrect size");
 
-        m_reqStart = *(const uint32_t *) payload();
+	m_reqStart = *(const uint32_t *) payload();
 }
 
 ClientHelloPkt::ClientHelloPkt(const ClientHelloPkt &pkt) :
@@ -339,15 +350,20 @@ ClientHelloPkt::ClientHelloPkt(const ClientHelloPkt &pkt) :
 
 /* ------------------------------------------------------------------------ */
 
-StatsResetPkt::StatsResetPkt(const uint8_t *data, uint32_t len) :
-	Packet(data, len)
+AnnotationPkt::AnnotationPkt(const uint8_t *data, uint32_t len) :
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
-	if (m_payload_len)
-		throw invalid_packet("StatsReset packet is incorrect size");
+	if (m_payload_len < (2 * sizeof(uint32_t)))
+		throw invalid_packet("AnnotationPkt packet is incorrect size");
+
+	uint16_t size = m_fields[0] & 0xffff;
+	if (m_payload_len < (size + (2 * sizeof(uint32_t))))
+		throw invalid_packet("AnnotationPkt packet has oversize "
+				     "string");
 }
 
-StatsResetPkt::StatsResetPkt(const StatsResetPkt &pkt) :
-	Packet(pkt)
+AnnotationPkt::AnnotationPkt(const AnnotationPkt &pkt) :
+	Packet(pkt), m_fields((const uint32_t *)payload())
 {}
 
 /* ------------------------------------------------------------------------ */
@@ -355,11 +371,11 @@ StatsResetPkt::StatsResetPkt(const StatsResetPkt &pkt) :
 SyncPkt::SyncPkt(const uint8_t *data, uint32_t len) :
 	Packet(data, len)
 {
-        const uint32_t *size = (const uint32_t *)(payload() + 24);
+	uint32_t size = *(const uint32_t *)(payload() + 24);
 
 	if (m_payload_len < 28)
 		throw invalid_packet("Sync packet is too small");
-	if (m_payload_len < (*size + 28))
+	if (m_payload_len < (size + 28))
 		throw invalid_packet("Sync packet has oversize string");
 }
 
@@ -385,7 +401,7 @@ HeartbeatPkt::HeartbeatPkt(const HeartbeatPkt &pkt) :
 GeometryPkt::GeometryPkt(const uint8_t *data, uint32_t len) :
 	Packet(data, len)
 {
-        uint32_t size = *(const uint32_t *) payload();
+	uint32_t size = *(const uint32_t *) payload();
 	const char *xml = (const char *) payload() + sizeof(uint32_t);
 
 	if (m_payload_len < sizeof(uint32_t))
@@ -409,7 +425,7 @@ BeamlineInfoPkt::BeamlineInfoPkt(const uint8_t *data, uint32_t len) :
 	Packet(data, len)
 {
 	const char *info = (const char *) payload() + sizeof(uint32_t);
-        uint32_t sizes = *(const uint32_t *) payload();
+	uint32_t sizes = *(const uint32_t *) payload();
 	uint32_t id_len, shortName_len, longName_len, info_len;
 
 	if (m_payload_len < sizeof(uint32_t))
@@ -441,7 +457,7 @@ BeamlineInfoPkt::BeamlineInfoPkt(const BeamlineInfoPkt &pkt) :
 DeviceDescriptorPkt::DeviceDescriptorPkt(const uint8_t *data, uint32_t len) :
 	Packet(data, len)
 {
-        const uint32_t *fields = (const uint32_t *) payload();
+	const uint32_t *fields = (const uint32_t *) payload();
 	uint32_t size;
 
 	if (m_payload_len < (2 * sizeof(uint32_t)))
@@ -465,7 +481,7 @@ DeviceDescriptorPkt::DeviceDescriptorPkt(const DeviceDescriptorPkt &pkt) :
 /* ------------------------------------------------------------------------ */
 
 VariableU32Pkt::VariableU32Pkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len != (4 * sizeof(uint32_t))) {
 		std::string msg("VariableValue (U32) packet is incorrect "
@@ -488,13 +504,13 @@ VariableU32Pkt::VariableU32Pkt(const uint8_t *data, uint32_t len) :
 }
 
 VariableU32Pkt::VariableU32Pkt(const VariableU32Pkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload())
+	Packet(pkt), m_fields((const uint32_t *)payload())
 {}
 
 /* ------------------------------------------------------------------------ */
 
 VariableDoublePkt::VariableDoublePkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	if (m_payload_len != (sizeof(double) + (3 * sizeof(uint32_t)))) {
 		std::string msg("VariableValue (double) packet is incorrect "
@@ -517,13 +533,13 @@ VariableDoublePkt::VariableDoublePkt(const uint8_t *data, uint32_t len) :
 }
 
 VariableDoublePkt::VariableDoublePkt(const VariableDoublePkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload())
+	Packet(pkt), m_fields((const uint32_t *)payload())
 {}
 
 /* ------------------------------------------------------------------------ */
 
 VariableStringPkt::VariableStringPkt(const uint8_t *data, uint32_t len) :
-        Packet(data, len), m_fields((const uint32_t *)payload())
+	Packet(data, len), m_fields((const uint32_t *)payload())
 {
 	uint32_t size;
 
@@ -562,5 +578,5 @@ VariableStringPkt::VariableStringPkt(const uint8_t *data, uint32_t len) :
 }
 
 VariableStringPkt::VariableStringPkt(const VariableStringPkt &pkt) :
-        Packet(pkt), m_fields((const uint32_t *)payload()), m_val(pkt.m_val)
+	Packet(pkt), m_fields((const uint32_t *)payload()), m_val(pkt.m_val)
 {}

Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/ADARA.h
@@ -11,6 +11,7 @@ namespace PacketType {
 	enum Enum {
 		RAW_EVENT_V0		= ADARA_PKT_TYPE(0x0000, 0),
 		RTDL_V0			= ADARA_PKT_TYPE(0x0001, 0),
+		SOURCE_LIST_V0		= ADARA_PKT_TYPE(0x0002, 0),
 		BANKED_EVENT_V0		= ADARA_PKT_TYPE(0x4000, 0),
 		BEAM_MONITOR_EVENT_V0	= ADARA_PKT_TYPE(0x4001, 0),
 		PIXEL_MAPPING_V0	= ADARA_PKT_TYPE(0x4002, 0),
@@ -18,7 +19,7 @@ namespace PacketType {
 		RUN_INFO_V0		= ADARA_PKT_TYPE(0x4004, 0),
 		TRANS_COMPLETE_V0	= ADARA_PKT_TYPE(0x4005, 0),
 		CLIENT_HELLO_V0		= ADARA_PKT_TYPE(0x4006, 0),
-		STATS_RESET_V0		= ADARA_PKT_TYPE(0x4007, 0),
+		STREAM_ANNOTATION_V0	= ADARA_PKT_TYPE(0x4007, 0),
 		SYNC_V0			= ADARA_PKT_TYPE(0x4008, 0),
 		HEARTBEAT_V0		= ADARA_PKT_TYPE(0x4009, 0),
 		GEOMETRY_V0		= ADARA_PKT_TYPE(0x400A, 0),
@@ -96,14 +97,21 @@ namespace VariableSeverity {
 	};
 }
 
+namespace MarkerType {
+	enum Enum {
+		GENERIC,
+		SCAN_START,
+		SCAN_STOP,
+		PAUSE,
+		RESUME,
+		OVERALL_RUN_COMMENT,
+	};
+}
+
 struct Event {
 	uint32_t tof;
 	uint32_t pixel;
 };
-
-typedef uint32_t EventBank;
-
-typedef uint32_t SourceSection;
 
 struct Header {
 	uint32_t payload_len;

Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/ADARAPackets.h
@@ -15,21 +15,29 @@ public:
 		m_payload_len = field[0];
 		m_type = (PacketType::Enum) field[1];
 
-                m_pulseId = ((uint64_t) field[2]) << 32;
-                m_pulseId |= field[3];
+		/* Convert EPICS epoch to Unix epoch,
+		 * Jan 1, 1990 ==> Jan 1, 1970
+		 */
+		m_timestamp.tv_sec = field[2] + EPICS_EPOCH_OFFSET;
+		m_timestamp.tv_nsec = field[3];
+
+		m_pulseId = ((uint64_t) field[2]) << 32;
+		m_pulseId |= field[3];
 	}
 
 	PacketType::Enum type(void) const { return m_type; }
 	uint32_t payload_length(void) const { return m_payload_len; }
-        uint64_t pulseId(void) const { return m_pulseId; }
-        uint32_t packet_length(void) const { return m_payload_len + 16; }
+	const struct timespec &timestamp(void) const { return m_timestamp; }
+	uint64_t pulseId(void) const { return m_pulseId; }
+	uint32_t packet_length(void) const { return m_payload_len + 16; }
 
 	static uint32_t header_length(void) { return 16; }
 
 protected:
 	uint32_t m_payload_len;
 	PacketType::Enum m_type;
-        uint64_t m_pulseId;
+	struct timespec m_timestamp;
+	uint64_t m_pulseId;
 
 	/* Don't allow the default constructor */
 	PacketHeader();
@@ -83,13 +91,13 @@ public:
 	uint32_t tofOffset(void) const { return m_fields[5] & 0x7fffffff; }
 	uint32_t tofField(void) const { return m_fields[5]; }
 
-        const Event *events(void) const { return (const Event *) &m_fields[6]; }
+	const Event *events(void) const { return (const Event *) &m_fields[6]; }
 	uint32_t num_events(void) const {
 		return (m_payload_len - 24) / (2 * sizeof (uint32_t));
 	}
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 
 	RawDataPkt(const uint8_t *data, uint32_t len);
 
@@ -115,14 +123,29 @@ public:
 	uint32_t intraPulseTime(void) const { return m_fields[2]; }
 	bool tofCorrected(void) const { return !!(m_fields[3] & 0x80000000); }
 	uint32_t tofOffset(void) const { return m_fields[3] & 0x7fffffff; }
-	uint32_t ringPeriod(void) const { return m_fields[4]; }
+	uint32_t ringPeriod(void) const { return m_fields[4] & 0xffffff; }
 
 	// TODO implement accessor for optional fields
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 
 	RTDLPkt(const uint8_t *data, uint32_t len);
+
+	friend class Parser;
+};
+
+class SourceListPkt : public Packet {
+public:
+	SourceListPkt(const SourceListPkt &pkt);
+
+	const uint32_t *ids(void) const { return (const uint32_t *) payload(); }
+	uint32_t num_ids(void) const {
+		return payload_length() / sizeof (uint32_t);
+	}
+
+private:
+	SourceListPkt(const uint8_t *data, uint32_t len);
 
 	friend class Parser;
 };
@@ -199,7 +222,7 @@ public:
 	// TODO implment monitor/event accessors
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 
 	BeamMonitorPkt(const uint8_t *data, uint32_t len);
 
@@ -229,7 +252,7 @@ public:
 	}
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 
 	RunStatusPkt(const uint8_t *data, uint32_t len);
 
@@ -280,12 +303,30 @@ private:
 	friend class Parser;
 };
 
-class StatsResetPkt : public Packet {
+class AnnotationPkt : public Packet {
 public:
-	StatsResetPkt(const StatsResetPkt &pkt);
+	AnnotationPkt(const AnnotationPkt &pkt);
+
+	bool resetHint(void) const { return !!(m_fields[0] & 0x80000000); }
+	MarkerType::Enum type(void) const {
+		uint16_t type = (m_fields[0] >> 16) & 0x7fff;
+		return static_cast<MarkerType::Enum>(type);
+	}
+	uint32_t scanIndex(void) const { return m_fields[1]; }
+	const std::string &comment(void) const {
+		if (!m_comment.length() && (m_fields[0] & 0xffff)) {
+			m_comment.assign((const char *) &m_fields[2],
+					 m_fields[0] & 0xffff);
+		}
+
+		return m_comment;
+	}
 
 private:
-	StatsResetPkt(const uint8_t *data, uint32_t len);
+	const uint32_t *m_fields;
+	mutable std::string m_comment;
+
+	AnnotationPkt(const uint8_t *data, uint32_t len);
 
 	friend class Parser;
 };
@@ -375,7 +416,7 @@ public:
 	uint32_t value(void) const { return m_fields[3]; }
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 
 	VariableU32Pkt(const uint8_t *data, uint32_t len);
 
@@ -395,10 +436,10 @@ public:
 		return static_cast<VariableSeverity::Enum>
 							(m_fields[2] & 0xffff);
 	}
-        double value(void) const { return *(const double *) &m_fields[3]; }
+	double value(void) const { return *(const double *) &m_fields[3]; }
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 
 	VariableDoublePkt(const uint8_t *data, uint32_t len);
 
@@ -421,7 +462,7 @@ public:
 	const std::string &value(void) const { return m_val; }
 
 private:
-        const uint32_t *m_fields;
+	const uint32_t *m_fields;
 	std::string m_val;
 
 	VariableStringPkt(const uint8_t *data, uint32_t len);

Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/ADARAParser.h
@@ -73,6 +73,7 @@ protected:
 	 */
 	virtual bool rxPacket(const RawDataPkt &pkt);
 	virtual bool rxPacket(const RTDLPkt &pkt);
+	virtual bool rxPacket(const SourceListPkt &pkt);
 	virtual bool rxPacket(const BankedEventPkt &pkt);
 	virtual bool rxPacket(const BeamMonitorPkt &pkt);
 	virtual bool rxPacket(const PixelMappingPkt &pkt);
@@ -80,7 +81,7 @@ protected:
 	virtual bool rxPacket(const RunInfoPkt &pkt);
 	virtual bool rxPacket(const TransCompletePkt &pkt);
 	virtual bool rxPacket(const ClientHelloPkt &pkt);
-	virtual bool rxPacket(const StatsResetPkt &pkt);
+	virtual bool rxPacket(const AnnotationPkt &pkt);
 	virtual bool rxPacket(const SyncPkt &pkt);
 	virtual bool rxPacket(const HeartbeatPkt &pkt);
 	virtual bool rxPacket(const GeometryPkt &pkt);

Code/Mantid/Framework/DataHandling/src/ADARAParser.cpp
@@ -41,14 +41,29 @@ bool Parser::read(int fd, unsigned int max_read)
 	while (!max_read || bytes_read < max_read) {
 		rc = ::read(fd, m_buffer + m_len, m_size - m_len);
 		if (rc < 0) {
-			if (errno == EINTR || errno == EAGAIN ||
-						errno == EWOULDBLOCK)
+			switch (errno) {
+			case EINTR:
+			case EAGAIN:
+				/* We didn't get any data, but we're OK */
 				return true;
-
-			int err = errno;
-			std::string msg("Parser::read(): ");
-			msg += strerror(err);
-			throw std::runtime_error(msg);
+			case EPIPE:
+			case ECONNRESET:
+			case ETIMEDOUT:
+			case EHOSTUNREACH:
+			case ENETUNREACH:
+				/* The host went away, but this shouldn't
+				 * be fatal.
+				 */
+				return false;
+			default:
+				/* TODO consider if we should throw an
+				 * exception at all.
+				 */
+				int err = errno;
+				std::string msg("Parser::read(): ");
+				msg += strerror(err);
+				throw std::runtime_error(msg);
+			}
 		}
 
 		if (rc == 0)
@@ -122,7 +137,7 @@ bool Parser::parseBuffer(void)
 		p += chunk_len;
 	}
 
-	while (!stopped && m_len > PacketHeader::header_length()) {
+	while (!stopped && m_len >= PacketHeader::header_length()) {
 		PacketHeader hdr(p);
 
 		if (hdr.payload_length() % 4)
@@ -148,11 +163,11 @@ bool Parser::parseBuffer(void)
 			 * resized, return to our caller as we obviously
 			 * don't have the full packet yet.
 			 */
-			unsigned int new_size = m_size * 2;
+			unsigned int new_size = m_size;
 			uint8_t *new_buffer;
 
 			do {
-				new_size = m_size * 2;
+				new_size *= 2;
 			} while (new_size < hdr.packet_length());
 
 			if (new_size > m_max_size)
@@ -201,6 +216,7 @@ bool Parser::rxPacket(const Packet &pkt)
 	switch (pkt.type()) {
 		MAP_TYPE(PacketType::RAW_EVENT_V0, RawDataPkt);
 		MAP_TYPE(PacketType::RTDL_V0, RTDLPkt);
+		MAP_TYPE(PacketType::SOURCE_LIST_V0, SourceListPkt);
 		MAP_TYPE(PacketType::BANKED_EVENT_V0, BankedEventPkt);
 		MAP_TYPE(PacketType::BEAM_MONITOR_EVENT_V0, BeamMonitorPkt);
 		MAP_TYPE(PacketType::PIXEL_MAPPING_V0, PixelMappingPkt);
@@ -208,7 +224,7 @@ bool Parser::rxPacket(const Packet &pkt)
 		MAP_TYPE(PacketType::RUN_INFO_V0, RunInfoPkt);
 		MAP_TYPE(PacketType::TRANS_COMPLETE_V0, TransCompletePkt);
 		MAP_TYPE(PacketType::CLIENT_HELLO_V0, ClientHelloPkt);
-		MAP_TYPE(PacketType::STATS_RESET_V0, StatsResetPkt);
+		MAP_TYPE(PacketType::STREAM_ANNOTATION_V0, AnnotationPkt);
 		MAP_TYPE(PacketType::SYNC_V0, SyncPkt);
 		MAP_TYPE(PacketType::HEARTBEAT_V0, HeartbeatPkt);
 		MAP_TYPE(PacketType::GEOMETRY_V0, GeometryPkt);
@@ -245,6 +261,7 @@ bool Parser::rxPacket(const type &) { return false; }
 
 EXPAND_HANDLER(RawDataPkt)
 EXPAND_HANDLER(RTDLPkt)
+EXPAND_HANDLER(SourceListPkt)
 EXPAND_HANDLER(BankedEventPkt)
 EXPAND_HANDLER(BeamMonitorPkt)
 EXPAND_HANDLER(PixelMappingPkt)
@@ -252,7 +269,7 @@ EXPAND_HANDLER(RunStatusPkt)
 EXPAND_HANDLER(RunInfoPkt)
 EXPAND_HANDLER(TransCompletePkt)
 EXPAND_HANDLER(ClientHelloPkt)
-EXPAND_HANDLER(StatsResetPkt)
+EXPAND_HANDLER(AnnotationPkt)
 EXPAND_HANDLER(SyncPkt)
 EXPAND_HANDLER(HeartbeatPkt)
 EXPAND_HANDLER(GeometryPkt)

commit 67f91e1f16db0f21699ec2b37653c45ad246363a

Fix constructor for BankedEventPkt
Squash bug introduced in previous commit
(d5de7a2329b84f498a574afc740df05a2314a1f2)

Refs #6235

Code/Mantid/Framework/DataHandling/src/ADARAPackets.cpp
@@ -125,6 +125,8 @@ BankedEventPkt::BankedEventPkt(const uint8_t *data, uint32_t len) :
 {
 	if (m_payload_len < (4 * sizeof(uint32_t)))
 		throw invalid_packet("BankedEvent packet is too short");
+
+        m_lastFieldIndex = (payload_length() / 4) - 1;
 }
 
 BankedEventPkt::BankedEventPkt(const BankedEventPkt &pkt) :