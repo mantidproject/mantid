1024eeaf5e834e9c312bb441b14d3201ba469b2b
Alex Buts <Alex.Buts@stfc.ac.uk>
2012-12-05 15:22:03 +0000

    refs #5697 enabling tests failing due to volume problems

--- Code/Mantid/Framework/MDEvents/test/MDGridBoxTest.h
+++ Code/Mantid/Framework/MDEvents/test/MDGridBoxTest.h
@@ -326,7 +326,7 @@ public:
 
   //-------------------------------------------------------------------------------------
   /** Start with a grid box, split some of its contents into sub-gridded boxes. */
-  void xest_splitContents()
+  void test_splitContents()
   {
     MDGridBox<MDLeanEvent<2>,2> * superbox = MDEventsTestHelper::makeMDGridBox<2>();
     MDGridBox<MDLeanEvent<2>,2> * gb;
@@ -343,7 +343,7 @@ public:
     boxes = superbox->getBoxes();
     b = dynamic_cast<MDBox<MDLeanEvent<2>,2> *>(boxes[0]);
     TS_ASSERT( b );
-   // TS_ASSERT_DELTA( b->getVolume(), 1.0, 1e-5 );
+    TS_ASSERT_DELTA( b->getVolume(), 1.0, 1e-5 );
 
     // It is the first child, so ID is 1
     TS_ASSERT_EQUALS( b->getId(), 1 );
@@ -356,7 +356,7 @@ public:
     boxes = superbox->getBoxes();
     gb = dynamic_cast<MDGridBox<MDLeanEvent<2>,2> *>(boxes[0]);
     TS_ASSERT( gb );
-   // TS_ASSERT_DELTA( gb->getVolume(), 1.0, 1e-5 );
+    TS_ASSERT_DELTA( gb->getVolume(), 1.0, 1e-5 );
 
     // ID of first child remains unchanged at 1
     TS_ASSERT_EQUALS( gb->getId(), 1 );
@@ -727,7 +727,7 @@ public:
    *
    * Tests that bad events are thrown out when using addEvents.
    * */
-  void xest_addEvents_2D()
+  void test_addEvents_2D()
   {
     MDGridBox<MDLeanEvent<2>,2> * b = MDEventsTestHelper::makeMDGridBox<2>();
     std::vector< MDLeanEvent<2> > events;
@@ -748,8 +748,8 @@ public:
     TS_ASSERT_EQUALS( b->getNPoints(), 100);
     TS_ASSERT_EQUALS( b->getSignal(), 100*2.0);
     TS_ASSERT_EQUALS( b->getErrorSquared(), 100*2.0);
-    //TS_ASSERT_DELTA( b->getSignalNormalized(), 100*2.0 / 100.0, 1e-5);
-//    TS_ASSERT_DELTA( b->getErrorSquaredNormalized(), 100*2.0 / 100.0, 1e-5);
+   // TS_ASSERT_DELTA( b->getSignalNormalized(), 100*2.0 / 100.0, 1e-5);
+    //TS_ASSERT_DELTA( b->getErrorSquaredNormalized(), 100*2.0 / 100.0, 1e-5);
 
     // Get all the boxes contained
     std::vector<MDBoxBase<MDLeanEvent<2>,2>*> boxes = b->getBoxes();
