266e92aab4a706c6525158f0010bb7b3033d86a8
Ricardo Leal <ricardo.leal@ill.fr>
2012-12-07 11:17:32 +0100

    Refs #6287. Still missing distance corrections.

--- Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/LoadILL.h
+++ Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/LoadILL.h
@@ -8,70 +8,93 @@
 #include "MantidAPI/IDataFileChecker.h"
 #include "MantidNexus/NexusClasses.h"
 
-namespace Mantid
-{
-namespace DataHandling
-{
+namespace Mantid {
+namespace DataHandling {
 /**
-  Loads an ILL nexus file into a Mantid workspace.
+ Loads an ILL nexus file into a Mantid workspace.
 
-  Required properties:
-  <UL>
-  <LI> Filename - The ILL nexus file to be read </LI>
-  <LI> Workspace - The name to give to the output workspace </LI>
-  </UL>
+ Required properties:
+ <UL>
+ <LI> Filename - The ILL nexus file to be read </LI>
+ <LI> Workspace - The name to give to the output workspace </LI>
+ </UL>
 
-  Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+ Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
 
-  This file is part of Mantid.
+ This file is part of Mantid.
 
-  Mantid is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
+ Mantid is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-  Mantid is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ Mantid is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  File change history is stored at: <https://github.com/mantidproject/mantid>
-  Code Documentation is available at: <http://doxygen.mantidproject.org>    
+ File change history is stored at: <https://github.com/mantidproject/mantid>
+ Code Documentation is available at: <http://doxygen.mantidproject.org>
  */
-class DLLExport LoadILL : public API::IDataFileChecker 
-{
+class DLLExport LoadILL: public API::IDataFileChecker {
 public:
-  /// Constructor
-  LoadILL() : API::IDataFileChecker() {}
-  /// Virtual destructor
-  virtual ~LoadILL() {}
-  /// Algorithm's name
-  virtual const std::string name() const { return "LoadILL"; }
-  /// Algorithm's version
-  virtual int version() const { return (1); }
-  /// Algorithm's category for identification
-  virtual const std::string category() const { return "DataHandling;Inelastic"; }
-  ///checks the file can be loaded by reading 1st 100 bytes and looking at the file extension.
-  bool quickFileCheck(const std::string& filePath,size_t nread,const file_header& header);
-  /// check the structure of the file and if this file can be loaded return a value between 1 and 100
-  int fileCheck(const std::string& filePath);
+	/// Constructor
+	LoadILL() :
+			API::IDataFileChecker(),m_instrumentName("") {
+	}
+	/// Virtual destructor
+	virtual ~LoadILL() {
+	}
+	/// Algorithm's name
+	virtual const std::string name() const {
+		return "LoadILL";
+	}
+	/// Algorithm's version
+	virtual int version() const {
+		return (1);
+	}
+	/// Algorithm's category for identification
+	virtual const std::string category() const {
+		return "DataHandling;Inelastic";
+	}
+	///checks the file can be loaded by reading 1st 100 bytes and looking at the file extension.
+	bool quickFileCheck(const std::string& filePath, size_t nread,
+			const file_header& header);
+	/// check the structure of the file and if this file can be loaded return a value between 1 and 100
+	int fileCheck(const std::string& filePath);
 private:
-  /// Sets documentation strings for this algorithm
-  virtual void initDocs();
-  // Initialisation code
-  void init();
-  // Execution code
-  void exec();
-  /// Load the counts
-  void loadData(NeXus::NXData& dataGroup, API::MatrixWorkspace_sptr& workspace);
-  /// Load the instrument form the IDF
-  void runLoadInstrument(API::MatrixWorkspace_sptr workspace);
+	/// Sets documentation strings for this algorithm
+	virtual void initDocs();
+	// Initialisation code
+	void init();
+	// Execution code
+	void exec();
+	/// Load the counts
+	void loadData(NeXus::NXData& dataGroup,
+			API::MatrixWorkspace_sptr& workspace);
+	/// Load Nexus Files Parameters
+	/// Load the instrument form the IDF
+	void runLoadInstrument(API::MatrixWorkspace_sptr workspace);
 
-  std::string m_filename;       ///< The file to load
-  std::string m_instrumentName; ///< Name of the instrumen
+	// Load all the nexus file information
+	NeXus::NXData loadNexusFile(NeXus::NXEntry&);
+
+	void correctData(API::MatrixWorkspace_sptr&);
+
+	std::string m_filename; ///< The file to load
+	std::string m_instrumentName; ///< Name of the instrumen
+
+	/* Values parsed from the nexus file */
+	int elasticPeakPosition;
+	double wavelength;
+	double channelWidth;
+	double timeOfFlightDelay;
+	double timePickupToOpening;
+	std::string title;
+
 };
 
 } // namespace DataHandling
--- Code/Mantid/Framework/DataHandling/src/LoadILL.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadILL.cpp
@@ -1,9 +1,9 @@
 /*WIKI* 
 
-Loads an ILL nexus file into a [[Workspace2D]] with the given name. 
+ Loads an ILL nexus file into a [[Workspace2D]] with the given name.
 
 
-*WIKI*/
+ *WIKI*/
 //---------------------------------------------------
 // Includes
 //---------------------------------------------------
@@ -13,11 +13,10 @@ Loads an ILL nexus file into a [[Workspace2D]] with the given name.
 //#include "MantidAPI/NumericAxis.h"
 //#include "MantidDataObjects/Histogram1D.h"
 #include "MantidAPI/LoadAlgorithmFactory.h"
+#include "MantidAPI/Progress.h"
 
-namespace Mantid
-{
-namespace DataHandling
-{
+namespace Mantid {
+namespace DataHandling {
 
 using namespace Kernel;
 using namespace API;
@@ -30,12 +29,10 @@ DECLARE_ALGORITHM(LoadILL)
 DECLARE_LOADALGORITHM(LoadILL)
 
 /// Sets documentation strings for this algorithm
-void LoadILL::initDocs()
-{
-  this->setWikiSummary("Loads a ILL nexus file. ");
-  this->setOptionalMessage("Loads a ILL nexus file.");
+void LoadILL::initDocs() {
+	this->setWikiSummary("Loads a ILL nexus file. ");
+	this->setOptionalMessage("Loads a ILL nexus file.");
 }
-
 
 //---------------------------------------------------
 // Private member functions
@@ -44,41 +41,109 @@ void LoadILL::initDocs()
 /**
  * Initialise the algorithm
  */
-void LoadILL::init()
-{
-  declareProperty(new FileProperty("Filename","", FileProperty::Load, ".nxs"),
-                  "Name of the SPE file to load" );
-  declareProperty(new WorkspaceProperty<>("OutputWorkspace", "", Direction::Output),
-    "The name to use for the output workspace" );
+void LoadILL::init() {
+	declareProperty(
+			new FileProperty("Filename", "", FileProperty::Load, ".nxs"),
+			"Name of the SPE file to load");
+	declareProperty(
+			new WorkspaceProperty<>("OutputWorkspace", "", Direction::Output),
+			"The name to use for the output workspace");
+}
+
+/**
+ * tostring operator to print the contents of NXClassInfo
+ */
+std::ostream& operator<<(std::ostream &strm, const NXClassInfo &c) {
+	return strm << "NXClassInfo :: nxname: " << c.nxname << " , nxclass: "
+			<< c.nxclass;
 }
 
 /**
  * Execute the algorithm
  */
-void LoadILL::exec()
-{
-  // Retrieve filename and try to open the file
-  m_filename = getPropertyValue("Filename");
+void LoadILL::exec() {
+	// Retrieve filename and try to open the file
+	m_filename = getPropertyValue("Filename");
 
-  // open the root node
-  NXRoot root(m_filename);
-  // find the first entry
-  NXEntry entry = root.openFirstEntry();
-  std::cerr << "Entry " << entry.name() << std::endl;
-  // find out the instrument name. If other instruments at ILL have this format
-  // I assume their instrument group will have the name of the instrument
-  m_instrumentName = "IN5";
+	MatrixWorkspace_sptr workspace;
 
-  // read in the data
-  NXData dataGroup = entry.openNXData("data");
-  MatrixWorkspace_sptr workspace;
-  loadData( dataGroup, workspace );
+	// open the root node
+	NXRoot root(m_filename);
+	// find the first entry
+	NXEntry entry = root.openFirstEntry();
 
-  // load the instrument from the IDF if it exists
-  runLoadInstrument( workspace );
+	NXData dataGroup = loadNexusFile(entry);
 
-  // Set the output workspace property
-  setProperty("OutputWorkspace", workspace);
+	loadData(dataGroup, workspace);
+
+	// load the instrument from the IDF if it exists
+	runLoadInstrument(workspace);
+
+	// Correct the data
+	correctData(workspace);
+
+	// Set the output workspace property
+	setProperty("OutputWorkspace", workspace);
+}
+
+/**
+ * Load the nexus details
+ * @param entry :: first NXEntry entry in the nexus file
+ */
+NeXus::NXData LoadILL::loadNexusFile(NXEntry& entry) {
+
+	// Ugly way of getting Instrument Name
+	// Instrument name is in: entry0/<NXinstrument>/name
+	std::vector<NXClassInfo> v = entry.groups();
+	for (auto it = v.begin(); it < v.end(); it++) {
+		if (it->nxclass == "NXinstrument") {
+			std::string insNamePath = it->nxname + "/name";
+			m_instrumentName = entry.getString(insNamePath);
+			g_log.debug() << "Instrument Name: " << m_instrumentName
+					<< std::endl;
+			break;
+		}
+	}
+	if (m_instrumentName == "") {
+		g_log.error();
+		throw std::runtime_error(
+				"Cannot read the instrument name from the Nexus file!");
+	}
+
+	title = entry.getString("title");
+	wavelength = entry.getFloat("wavelength");
+	elasticPeakPosition = entry.getInt("monitor/elasticpeak");
+
+	NXDiskChopper chopper =
+			entry.openNXInstrument(m_instrumentName).openNXDiskChopper("FO");
+	NXFloat f0ChopperSpeedRPM = chopper.openRotationSpeed();
+	f0ChopperSpeedRPM.load();
+
+	// Angular velocity [deg/s] and period [s]
+	double f0ChopperAngularVelocity = 360. * (f0ChopperSpeedRPM[0] / 60.);
+	double pickupOffsetAngle = 90.; // Pick-up to windows offset angle [deg.]
+	// Time from pickup to the window opening [mu-sec]
+	timePickupToOpening = pickupOffsetAngle / f0ChopperAngularVelocity * 1e6;
+
+	NXFloat time_of_flight_data = entry.openNXFloat("monitor/time_of_flight");
+	time_of_flight_data.load();
+
+	// channel width , number of channels, Time of flight delay
+	channelWidth = time_of_flight_data[0];
+	timeOfFlightDelay = time_of_flight_data[2];
+
+	g_log.debug("Nexus Data:");
+	g_log.debug() << "NchannelWidth: " << channelWidth << std::endl;
+	g_log.debug() << "wavelength: " << wavelength << std::endl;
+	g_log.debug() << "elasticPeakPosition: " << elasticPeakPosition
+			<< std::endl;
+	g_log.debug() << "timeOfFlightDelay: " << timeOfFlightDelay << std::endl;
+	g_log.debug() << "timePickupToOpening: " << timePickupToOpening
+			<< std::endl;
+
+	// read in the data
+	NXData dataGroup = entry.openNXData("data");
+	return dataGroup;
 }
 
 /**
@@ -86,48 +151,154 @@ void LoadILL::exec()
  * @param data :: The dataset with the counts (signal=1).
  * @param workspace :: The workspace to write the data in.
  */
-void LoadILL::loadData(NeXus::NXData& dataGroup, API::MatrixWorkspace_sptr& workspace)
-{
-  NXInt data = dataGroup.openIntData();
+void LoadILL::loadData(NeXus::NXData& dataGroup,
+		API::MatrixWorkspace_sptr& workspace) {
+	NXInt data = dataGroup.openIntData();
 
-  size_t dim0 = static_cast<size_t>( data.dim0() );
-  size_t dim1 = static_cast<size_t>( data.dim1() );
-  // dim0 * dim1 is the total number of detectors
-  size_t nhist = dim0 * dim1;
-  // dim2 is the number of time bins
-  size_t nbins = data.dim2();
+	size_t dim0 = static_cast<size_t>(data.dim0());
+	size_t dim1 = static_cast<size_t>(data.dim1());
+	// dim0 * dim1 is the total number of detectors
+	size_t nhist = dim0 * dim1;
+	// dim2 is the number of time bins
+	size_t nbins = data.dim2();
 
-  // Now create the output workspace
-  workspace = WorkspaceFactory::Instance().create("Workspace2D",nhist,nbins+1,nbins);
-  workspace->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
-  workspace->setYUnitLabel("Counts");
+	// Now create the output workspace
+	workspace = WorkspaceFactory::Instance().create("Workspace2D", nhist,
+			nbins + 1, nbins);
+	workspace->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
+	workspace->setYUnitLabel("Counts");
+	workspace->setTitle(title);
 
-  // put the time of flight data into the x vector
-  std::vector<double> tof(nbins + 1);
-  // I am making them up because I cannot find them in the file.
-  for(auto t = tof.begin(); t != tof.end(); ++t)
-  {
-    *t = static_cast<double>( t - tof.begin() );
-  }
-  workspace->dataX(0).assign(tof.begin(), tof.end());
+	// put the time of flight data into the x vector
+	std::vector<double> tof(nbins + 1);
+	// Calculate the time of flight from channel number and channel width
+	// Neutron velocity: v = (6.626e-34 m² kg / s) / (1.675e-27 kg) (\lambda m)
+	// t = distance / v
 
-  // load the counts from the file into memory
-  data.load();
-  size_t spec = 0;
-  for(size_t i = 0; i < dim0; ++i)
-  for(size_t j = 0; j < dim1; ++j)
-  {
-    if ( spec > 0 )
-    {
-      workspace->dataX(spec) = workspace->readX(0);
-    }
-    int* data_p = &data(static_cast<int>(i), static_cast<int>(j), 0);
-    workspace->dataY(spec).assign( data_p, data_p + nbins );
-    ++spec;
-  }
+	//TODO: get this from parameters file: 0.8656
+//	double distanceSourceSample = 2.10855; //meters
+//	double distanceSampleDetector = 4;
+//
+//	double tElastSampleDetector = (distanceSampleDetector)
+//			/ (6.626e-34 / (1.675e-27 * wavelength * 1e-10));
+//	double tElastSourceSample = (distanceSourceSample)
+//			/ (6.626e-34 / (1.675e-27 * wavelength * 1e-10));
 
+	//distance must be t1+t2: 865.6mm + 4m ????
+	//Mantid uses t1 (source in IDF) for calculations
+	// double telast = wavelength * (distanceSampleDetector + 0.8656) / 3956.035;
+
+//	// convert seconds to microseconds
+//	tElastSampleDetector *= 1e6;
+//	tElastSourceSample *= 1e6;
+//
+//	g_log.debug("Theoretical Values:");
+//	g_log.debug() << "tElastSourceSample: " << tElastSourceSample
+//			<< " microsecs" << std::endl;
+//	g_log.debug() << "tElastSampleDetector: " << tElastSampleDetector
+//			<< " microsecs" << std::endl;
+
+	double tElastChannel = static_cast<double>(elasticPeakPosition)
+			* channelWidth;
+	double tPickupToDetectorElastic = timeOfFlightDelay + tElastChannel;
+
+	g_log.debug("Calculated Values:");
+	g_log.debug() << "tElastChannel: " << tElastChannel << std::endl;
+	g_log.debug() << "timePickupToDetectorElastic: " << tPickupToDetectorElastic
+			<< std::endl;
+
+	// load the counts from the file into memory
+	data.load();
+
+	// have to find the real elastic peak
+	// Select a h = middle detector
+	// sweap across theta and find the average position
+
+	for (size_t j = 0; j <= nbins; ++j) {
+		// EPP channel number for which the elastic peak coincides
+		tof[j] = tPickupToDetectorElastic - timePickupToOpening
+				+ channelWidth
+						* static_cast<double>(static_cast<int>(j)
+								- elasticPeakPosition);
+
+		// This WORKS
+		/*tof[j] = channelWidth
+		 * static_cast<double>(static_cast<int>(j) - elasticPeakPosition)
+		 + tElastSourceSample + tElastSampleDetector;*/
+
+	}
+	workspace->dataX(0).assign(tof.begin(), tof.end());
+
+	Progress progress(this, 0, 1, dim0 * dim1);
+
+	size_t spec = 0;
+	for (size_t i = 0; i < dim0; ++i)
+		for (size_t j = 0; j < dim1; ++j) {
+			if (spec > 0) {
+				workspace->dataX(spec) = workspace->readX(0);
+			}
+			int* data_p = &data(static_cast<int>(i), static_cast<int>(j), 0);
+			workspace->dataY(spec).assign(data_p, data_p + nbins);
+			++spec;
+			progress.report();
+		}
 }
 
+/**
+ * Apply specific ILL corrections to the data: top/bottom detector correction + find the fitted Elastic Peak Position
+ *
+ * TODO: This has be implemented in a separate algorithm
+ * TODO: Still incomplete
+ *
+ *
+ */
+void LoadILL::correctData(API::MatrixWorkspace_sptr& workspace) {
+	detid_t minId = 0;
+	detid_t maxId = 0;
+	workspace->getInstrument()->getMinMaxDetectorIDs(minId, maxId);
+
+	size_t start = static_cast<size_t>(floor(static_cast<double>(minId + 256) / 2.0));
+
+	// Move along the equatorial line of the detector
+	// todo: I have to get these values from somewhere
+	// theres 384 detectors let's use 200
+	for (size_t i = start; i < 200*256 ; i=i+256 ) {
+
+		const MantidVec& y = workspace->readY(i);
+		// Dummy log to avoid warning: unused variable
+		g_log.debug() << y.size();
+
+
+	}
+//
+//
+//	 const int numberOfSpectra = workspace->getNumberHistograms();
+//	 for (int i = 0; i < numberOfSpectra; ++i)
+//	 {
+//	   MantidVec& x = workspace->readX(i); // Getting a reference (including the &) is more efficient
+//	   MantidVec& y = workspace->readY(i);
+//	   MantidVec& e = workspace->readE(i);
+//	   // ... do something to the data ...
+//	 }
+
+	size_t testSpec = 48082;
+	const MantidVec& x = workspace->readX(testSpec); // Getting a reference (including the &) is more efficient
+	const MantidVec& y = workspace->readY(testSpec);
+	const MantidVec& e = workspace->readE(testSpec);
+
+	std::cout << "x size: " << x.size() << std::endl;
+	std::cout << "x Max: " << *std::max_element(x.begin(), x.end()) << std::endl;
+	std::cout << "x Min: " << *std::min_element(x.begin(), x.end()) << std::endl;
+
+	std::cout << "y size: " << y.size() << std::endl;
+	std::cout << "y Max: " << *std::max_element(y.begin(), y.end()) << std::endl;
+	std::cout << "y Min: " << *std::min_element(y.begin(), y.end()) << std::endl;
+
+	std::cout << "e size: " << e.size() << std::endl;
+	std::cout << "e Max: " << *std::max_element(e.begin(), e.end()) << std::endl;
+	std::cout << "e Min: " << *std::min_element(e.begin(), e.end()) << std::endl;
+
+}
 
 /**This method does a quick file check by checking the no.of bytes read nread params and header buffer
  *  @param filePath- path of the file including name.
@@ -135,63 +306,61 @@ void LoadILL::loadData(NeXus::NXData& dataGroup, API::MatrixWorkspace_sptr& work
  *  @param header :: The first 100 bytes of the file as a union
  *  @return true if the given file is of type which can be loaded by this algorithm
  */
-bool LoadILL::quickFileCheck(const std::string& filePath,size_t nread,const file_header& header)
-{
-  std::string extn=extension(filePath);
-  bool bnexs(false);
-  (!extn.compare("nxs")||!extn.compare("nx5"))?bnexs=true:bnexs=false;
-  /*
-  * HDF files have magic cookie in the first 4 bytes
-  */
-  if ( ((nread >= sizeof(unsigned)) && (ntohl(header.four_bytes) == g_hdf_cookie)) || bnexs )
-  {
-    //hdf
-    return true;
-  }
-  else if ( (nread >= sizeof(g_hdf5_signature)) && 
-    (!memcmp(header.full_hdr, g_hdf5_signature, sizeof(g_hdf5_signature))) )
-  { 
-    //hdf5
-    return true;
-  }
-  return false;
+bool LoadILL::quickFileCheck(const std::string& filePath, size_t nread,
+		const file_header& header) {
+	std::string extn = extension(filePath);
+	bool bnexs(false);
+	(!extn.compare("nxs") || !extn.compare("nx5")) ? bnexs = true : bnexs =
+																false;
+	/*
+	 * HDF files have magic cookie in the first 4 bytes
+	 */
+	if (((nread >= sizeof(unsigned))
+			&& (ntohl(header.four_bytes) == g_hdf_cookie)) || bnexs) {
+		//hdf
+		return true;
+	} else if ((nread >= sizeof(g_hdf5_signature))
+			&& (!memcmp(header.full_hdr, g_hdf5_signature,
+					sizeof(g_hdf5_signature)))) {
+		//hdf5
+		return true;
+	}
+	return false;
 }
 
 /**checks the file by opening it and reading few lines 
  * @param filePath :: name of the file inluding its path
  * @return an integer value how much this algorithm can load the file 
  */
-int LoadILL::fileCheck(const std::string& filePath)
-{  
-  // Create the root Nexus class
-  NXRoot root(filePath);
-  NXEntry entry = root.openFirstEntry();
-  if ( entry.containsGroup("IN5") ) return 80;
+int LoadILL::fileCheck(const std::string& filePath) {
+	// Create the root Nexus class
+	NXRoot root(filePath);
+	NXEntry entry = root.openFirstEntry();
+	if (entry.containsGroup("IN5"))
+		return 80;
 
-  return 0;
+	return 0;
 }
 
 /**
  * Run the sub-algorithm LoadInstrument.
  * @param workspace :: The workspace to assign the loaded instrument to.
  */
-void LoadILL::runLoadInstrument(API::MatrixWorkspace_sptr workspace)
-{
+void LoadILL::runLoadInstrument(API::MatrixWorkspace_sptr workspace) {
 
-  IAlgorithm_sptr loadInst = createSubAlgorithm("LoadInstrument");
+	IAlgorithm_sptr loadInst = createSubAlgorithm("LoadInstrument");
 
-  // Now execute the sub-algorithm. Catch and log any error, but don't stop.
-  try
-  {
-    loadInst->setPropertyValue("InstrumentName", m_instrumentName);
-    loadInst->setProperty<MatrixWorkspace_sptr> ("Workspace", workspace);
-    loadInst->execute();
-  }
-  catch( ... )
-  {
-    g_log.information("Cannot load the instrument definition.");
-  }
+	// Now execute the sub-algorithm. Catch and log any error, but don't stop.
+	try {
+		loadInst->setPropertyValue("InstrumentName", m_instrumentName);
+		loadInst->setProperty<MatrixWorkspace_sptr>("Workspace", workspace);
+		loadInst->execute();
+	} catch (...) {
+		g_log.information("Cannot load the instrument definition.");
+	}
 }
 
-} // namespace DataHandling
-} // namespace Mantid
+}
+
+// namespace DataHandling
+}// namespace Mantid
