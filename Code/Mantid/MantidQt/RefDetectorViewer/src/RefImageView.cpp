481bb98629fe8190bc31a96c91dbcf46576c097e
Jean Bilheux <bilheuxjm@ornl.gov>
2012-12-10 14:14:57 -0500

    Done for the python->C++ communication. This refs #5801

--- Code/Mantid/MantidQt/RefDetectorViewer/src/RefImageView.cpp
+++ Code/Mantid/MantidQt/RefDetectorViewer/src/RefImageView.cpp
@@ -85,15 +85,56 @@ RefImageView::RefImageView( RefImageDataSource* data_source, double peak_min, do
     image_display->setPeakLeft(static_cast<int>(peak_min));
     QString peak_min_value = QString::fromStdString(s_peak_min);
     ui->lineEdit_peakLeft->setText(peak_min_value);
+
+    std::string s_peak_max;
+    std::stringstream ss_peak_max;
+    ss_peak_max << peak_max;
+    ss_peak_max >> s_peak_max;
+    image_display->setPeakRight(static_cast<int>(peak_max));
+    QString peak_max_value = QString::fromStdString(s_peak_max);
+    ui->lineEdit_peakRight->setText(peak_max_value);
+
+    std::string s_back_min;
+    std::stringstream ss_back_min;
+    ss_back_min << back_min;
+    ss_back_min >> s_back_min;
+    image_display->setBackLeft(static_cast<int>(back_min));
+    QString back_min_value = QString::fromStdString(s_back_min);
+    ui->lineEdit_backLeft->setText(back_min_value);
+
+    std::string s_back_max;
+    std::stringstream ss_back_max;
+    ss_back_max << back_max;
+    ss_back_max >> s_back_max;
+    image_display->setBackRight(static_cast<int>(back_max));
+    QString back_max_value = QString::fromStdString(s_back_max);
+    ui->lineEdit_backRight->setText(back_max_value);
     
+    std::string s_tof_min;
+    std::stringstream ss_tof_min;
+    ss_tof_min << tof_min;
+    ss_tof_min >> s_tof_min;
+    image_display->setTOFmin(static_cast<int>(tof_min));
+    QString tof_min_value = QString::fromStdString(s_tof_min);
+    ui->lineEdit_TOFmin->setText(tof_min_value);
     
-    
+    std::string s_tof_max;
+    std::stringstream ss_tof_max;
+    ss_tof_max << tof_max;
+    ss_tof_max >> s_tof_max;
+    image_display->setTOFmax(static_cast<int>(tof_max));
+    QString tof_max_value = QString::fromStdString(s_tof_max);
+    ui->lineEdit_TOFmax->setText(tof_max_value);
     
     
 //  ui->lineEdit_peakLeft.
     
   saved_iv_connections = iv_connections;
 
+    image_display->UpdateImage();
+    iv_connections->peak_back_tof_range_update();
+
+    
   image_display->SetDataSource( data_source );
 }
     
