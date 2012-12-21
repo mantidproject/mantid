7eaaa1988f59564fced77e01eb75d998a6e54a8a
Stuart Campbell <campbellsi@ornl.gov>
2012-12-21 12:25:48 -0500

    Refs #6098 - revert to fix build.

--- Code/Mantid/MantidQt/SliceViewer/src/PhysicalCrossPeak.cpp
+++ Code/Mantid/MantidQt/SliceViewer/src/PhysicalCrossPeak.cpp
@@ -64,7 +64,7 @@ namespace MantidQt
   */
   MantidQt::SliceViewer::CrossPeakPrimitives PhysicalCrossPeak::draw(const double& windowHeight, const double& windowWidth) const
   {
-    CrossPeakPrimitives drawingObjects;
+    CrossPeakPrimitives drawingObjects = {};
     if(isViewable())
     {
       const int halfCrossHeight = int(windowHeight * m_crossViewFraction);
