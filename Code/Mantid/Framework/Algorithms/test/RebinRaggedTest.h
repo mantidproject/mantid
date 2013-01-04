e944ad9b729c85d76cb5ab48ccc71749657c1535
Peter Peterson <petersonpf@ornl.gov>
2013-01-04 11:07:17 -0500

    Re #6318. Added working test for log resampling.

--- Code/Mantid/Framework/Algorithms/test/RebinRaggedTest.h
+++ Code/Mantid/Framework/Algorithms/test/RebinRaggedTest.h
@@ -27,7 +27,6 @@ public:
   
   void test_linear_binning()
   {
-
     RebinRagged alg;
     TS_ASSERT_THROWS_NOTHING( alg.initialize() );
     TS_ASSERT( alg.isInitialized() );
@@ -47,30 +46,65 @@ public:
     TS_ASSERT_EQUALS(0., xValues[0]);
     TS_ASSERT_EQUALS(0.1, xValues[1]);
     TS_ASSERT_EQUALS(300., xValues[3000]);
+  }
 
+  void test_log_binning()
+  {
+    RebinRagged alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() );
+    TS_ASSERT( alg.isInitialized() );
 
-//    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("InputWorkspace", "value") );
-//    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    //
+    int numBins(3000);
+    MantidVec xValues;
+    double delta;
+
+    alg.setOptions(numBins, true, false);
+
+    // first check that using zero for a border doesn't work
+    TS_ASSERT_THROWS_ANYTHING(delta = alg.determineBinning(xValues, 0, 300));
+    TS_ASSERT_THROWS_ANYTHING(delta = alg.determineBinning(xValues, -300, 0));
+
+    // do an actual run
+    delta = alg.determineBinning(xValues, 0.1, 1.0);
+//    std::cout << "**** " << numBins << " **** " << xValues.front() << ", " << delta << ", " << xValues.back() << " ****" << std::endl;
+//    std::cout << "000> " << xValues.size() << std::endl;
+//    std::cout << "001> " << xValues[0] << ", " << xValues[1] << ", ..., " << xValues.back() << std::endl;
+//    TS_ASSERT_EQUALS(numBins, xValues.size()-1);
+    TS_ASSERT_EQUALS(0.1, xValues[0]);
+    TS_ASSERT_EQUALS(1., xValues[3000]);
+    TS_ASSERT_DELTA(-.00077, delta, .00001);
+  }
+
+  void xtest_exec()
+  {
+    std::string outWSName("RebinRagged_out");
+
+    RebinRagged alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() );
+    TS_ASSERT( alg.isInitialized() );
+
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("InputWorkspace", "value") );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
 //    "XMin"
 //    "XMax"
 //    "NumberBins"
 //    "LogBinning"
 //    "PreserveEvents"
-//    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
-//    TS_ASSERT( alg.isExecuted() );
-    
-    // Retrieve the workspace from data service. TODO: Change to your desired type
-//    Workspace_sptr ws;
-//    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
-//    TS_ASSERT(ws);
-//    if (!ws) return;
-    
-    // TODO: Check the results
-    
-    // Remove workspace from the data service.
-//    AnalysisDataService::Instance().remove(outWSName);
-  }
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
 
+    // Retrieve the workspace from data service. TODO: Change to your desired type
+    Workspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
+    TS_ASSERT(ws);
+    if (!ws) return;
+
+    // TODO: Check the results
+
+    // Remove workspace from the data service.
+    AnalysisDataService::Instance().remove(outWSName);
+  }
 
 };
 
