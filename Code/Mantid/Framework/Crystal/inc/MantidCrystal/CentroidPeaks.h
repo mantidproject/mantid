887d01198fb16baf30b31860a59dc30b5b527d65
Vickie Lynch <lynchve@ornl.gov>
2013-01-24 13:17:34 -0500

    Refs #6031 better centroiding for histograms

--- Code/Mantid/Framework/Crystal/inc/MantidCrystal/CentroidPeaks.h
+++ Code/Mantid/Framework/Crystal/inc/MantidCrystal/CentroidPeaks.h
@@ -44,7 +44,8 @@ namespace Crystal
     void exec();
     void integrate();
     void integrateEvent();
-
+    int findPixelID(std::string bankName, int col, int row);
+    bool edgePixel(std::string bankName, int col, int row, int Edge);
 
     /// Input 2D Workspace
     API::MatrixWorkspace_sptr inWS;
--- Code/Mantid/Framework/Crystal/src/CentroidPeaks.cpp
+++ Code/Mantid/Framework/Crystal/src/CentroidPeaks.cpp
@@ -22,7 +22,6 @@ namespace Crystal
   using namespace Mantid::Kernel;
   using namespace Mantid::Crystal;
 
-
   //----------------------------------------------------------------------------------------------
   /** Constructor
    */
@@ -102,7 +101,8 @@ namespace Crystal
          if(peak.getRunNumber() == inWS->getRunNumber() && wi < Numberwi) MaxPeaks = i;
       }
     }
-    Geometry::Instrument_const_sptr Iptr = inWS->getInstrument();
+
+    int Edge = getProperty("EdgePixels");
     Progress prog(this, MinPeaks, 1.0, MaxPeaks);
     PARALLEL_FOR2(inWS,peakWS)
     for (int i = MinPeaks; i<= MaxPeaks; i++)
@@ -124,110 +124,25 @@ namespace Crystal
       int chan = Kernel::VectorHelper::getBinIndex(X, TOFPeakd);
       std::string bankName = peak.getBankName();
 
-      boost::shared_ptr<const IComponent> parent = Iptr->getComponentByName(bankName);
-      if (!parent) continue;
-      if (parent->type().compare("RectangularDetector") != 0)
-      {
-          int nPixels = std::max<int>(0, getProperty("EdgePixels"));
-          std::vector<Geometry::IComponent_const_sptr> children;
-          boost::shared_ptr<const Geometry::ICompAssembly> asmb = boost::dynamic_pointer_cast<const Geometry::ICompAssembly>(parent);
-          asmb->getChildren(children, false);
-          boost::shared_ptr<const Geometry::ICompAssembly> asmb2 = boost::dynamic_pointer_cast<const Geometry::ICompAssembly>(children[0]);
-          std::vector<Geometry::IComponent_const_sptr> grandchildren;
-          asmb2->getChildren(grandchildren,false);
-          int NROWS = static_cast<int>(grandchildren.size());
-          int NCOLS = static_cast<int>(children.size());
-    	  if (row < nPixels || col < nPixels || NROWS-row < nPixels || NCOLS-col < nPixels) continue;
-    	  //Only works for WISH for non-RectangularDetector  TODO-make more general
-    	  //if (bankName.compare("WISH") != 0)continue;
-          IAlgorithm_sptr slice_alg = createChildAlgorithm("IntegratePeakTimeSlices");
-          slice_alg->setProperty<MatrixWorkspace_sptr>("InputWorkspace", inWS);
-          std::ostringstream tab_str;
-          tab_str << "LogTable" << i;
+	  double intensity = 0.0;
+	  double chancentroid = 0.0;
 
-          slice_alg->setPropertyValue("OutputWorkspace", tab_str.str());
-          slice_alg->setProperty<PeaksWorkspace_sptr>("Peaks", peakWS);
-          slice_alg->setProperty("PeakIndex", i);
-          double qspan = 0.12;
-		  if (peakWS->mutableSample().hasOrientedLattice())
-		  {
-		    OrientedLattice latt = peakWS->mutableSample().getOrientedLattice();
-		    qspan = 1./std::max(latt.a(), std::max(latt.b(),latt.c()));//1/6*2Pi about 1
-		  }
-          slice_alg->setProperty("PeakQspan", qspan);
-
-
-          slice_alg->setProperty("NBadEdgePixels", nPixels);
-          try
-          {
-          slice_alg->executeAsChildAlg();
-          Mantid::API::MemoryManager::Instance().releaseFreeMemory();
-
-          TableWorkspace_sptr logtable = slice_alg->getProperty("OutputWorkspace");
-
-          double Imax = 0;
-          for (int iTOF=0; iTOF < static_cast<int>(logtable->rowCount()); iTOF++)
-          {
-              double intensity = logtable->getRef<double>(std::string("Intensity"), iTOF);
-              if (intensity > Imax)
-              {
-                                  Imax = intensity;
-				  int irow = static_cast<int>(logtable->getRef<double>(std::string("Mrow"), iTOF));
-				  int icol = static_cast<int>(logtable->getRef<double>(std::string("Mcol"), iTOF));
-				  std::string bankName0 = bankName;
-				  bankName0.erase(0,4);
-				  std::ostringstream pixelString;
-				  pixelString << Iptr->getName() << "/" << bankName0 << "/" <<bankName
-				  << "/tube" << std::setw(3) << std::setfill('0')<< icol
-				  << "/pixel" << std::setw(4) << std::setfill('0')<< irow;
-				  boost::shared_ptr<const Geometry::IComponent> component = Iptr->getComponentByName(pixelString.str());
-                                  boost::shared_ptr<const Detector> pixel = boost::dynamic_pointer_cast<const Detector>(component);
-				  if (pixel)
-                                  {
-                                       pixelID = pixel->getID();
-                                  }
-				  chan = static_cast<int>(logtable->getRef<double>(std::string("Channel"), iTOF));
-              }
-          }
-          } catch (...)
-          {
-
-             g_log.debug("Error in IntegratePeakTimeSlices");
-          }
-      }
-	  else
-      {
-      boost::shared_ptr<const RectangularDetector> RDet = boost::shared_dynamic_cast<
-                const RectangularDetector>(parent);
-      double intensity = 0.0;
-      double chancentroid = 0.0;
-      boost::shared_ptr<Detector> pixel = RDet->getAtXY(col, row);
-      it = wi_to_detid_map->find(pixel->getID());
-      if ( it == wi_to_detid_map->end() )
-      {
-        continue;
-      }
-      workspaceIndex = it->second;
-
-      const MantidVec & X = inWS->readX(workspaceIndex);
-
-      chan = Kernel::VectorHelper::getBinIndex(X, TOFPeakd);
       int chanstart = std::max(0,chan-PeakRadius);
       int chanend = std::min(static_cast<int>(X.size()),chan+PeakRadius);
       double rowcentroid = 0.0;
       int rowstart = std::max(0,row-PeakRadius);
-      int rowend = std::min(RDet->ypixels()-1,row+PeakRadius);
+      int rowend = row+PeakRadius;
       double colcentroid = 0.0;
-      int colstart = std::max(0,col-PeakRadius);
-      int colend = std::min(RDet->xpixels()-1,col+PeakRadius);
+      int colstart = col-PeakRadius;
+      int colend = col+PeakRadius;
       for (int ichan=chanstart; ichan<=chanend; ++ichan)
       {
         for (int irow=rowstart; irow<=rowend; ++irow)
         {
           for (int icol=colstart; icol<=colend; ++icol)
           {
-            boost::shared_ptr<Detector> pixel = RDet->getAtXY(icol, irow);
-            detid2index_map::const_iterator it = wi_to_detid_map->find(pixel->getID());
+            if (edgePixel(bankName, icol, irow, Edge))continue;
+            detid2index_map::const_iterator it = wi_to_detid_map->find(findPixelID(bankName, icol, irow));
             if ( it == wi_to_detid_map->end() ) continue;
             size_t workspaceIndex = (it->second);
     
@@ -241,32 +156,33 @@ namespace Crystal
         }
       }
     // Set pixelID to change row and col
-      row = std::min(RDet->ypixels()-1,int(rowcentroid/intensity));
+      row = int(rowcentroid/intensity);
       row = std::max(0,row);
-      col = std::min(RDet->xpixels()-1,int(colcentroid/intensity));
+      col = int(colcentroid/intensity);
       col = std::max(0,col);
-      pixel = RDet->getAtXY(col, row);
-      pixelID = pixel->getID();
       chan = int(chancentroid/intensity);
       chan = std::max(0,chan);
       chan = std::min(static_cast<int>(inWS->blocksize()),chan);
-      }
-      peak.setDetectorID(pixelID);
-    // Set wavelength to change tof for peak object
-      it = wi_to_detid_map->find(pixelID);
-      workspaceIndex = (it->second);
-      Mantid::Kernel::Units::Wavelength wl;
-      std::vector<double> timeflight;
-      timeflight.push_back(inWS->readX(workspaceIndex)[chan]);
-      double scattering = peak.getScattering();
-      double L1 = peak.getL1();
-      double L2 = peak.getL2();
-      wl.fromTOF(timeflight, timeflight, L1, L2, scattering, 0, 0, 0);
-      const double lambda = timeflight[0];
-      timeflight.clear();
 
-      peak.setWavelength(lambda);
-      peak.setBinCount(inWS->readY(workspaceIndex)[chan]);
+      peak.setDetectorID(findPixelID(bankName, col, row));
+    // Set wavelength to change tof for peak object
+      if (!edgePixel(bankName, col, row, Edge))
+      {
+          it = wi_to_detid_map->find(findPixelID(bankName, col, row));
+          workspaceIndex = (it->second);
+          Mantid::Kernel::Units::Wavelength wl;
+          std::vector<double> timeflight;
+          timeflight.push_back(inWS->readX(workspaceIndex)[chan]);
+          double scattering = peak.getScattering();
+          double L1 = peak.getL1();
+          double L2 = peak.getL2();
+          wl.fromTOF(timeflight, timeflight, L1, L2, scattering, 0, 0, 0);
+          const double lambda = timeflight[0];
+          timeflight.clear();
+
+          peak.setWavelength(lambda);
+          peak.setBinCount(inWS->readY(workspaceIndex)[chan]);
+      }
       PARALLEL_END_INTERUPT_REGION
     }
     PARALLEL_CHECK_INTERUPT_REGION
@@ -279,18 +195,12 @@ namespace Crystal
       int row = peak.getRow();
       std::string bankName = peak.getBankName();
 
-      boost::shared_ptr<const IComponent> parent = Iptr->getComponentByName(bankName);
-
-      if (!parent) continue;
-      if (parent->type().compare("RectangularDetector") != 0) continue;
-      boost::shared_ptr<const RectangularDetector> RDet = boost::shared_dynamic_cast<
-                const RectangularDetector>(parent);
-      int Edge = getProperty("EdgePixels");
-      if (col < Edge || col > (RDet->xpixels()-Edge) || row < Edge || row > (RDet->ypixels()-Edge))
+      if (edgePixel(bankName, col, row, Edge))
       {
         peakWS->removePeak(i);
       }
     }
+
     // Save the output
     setProperty("OutPeaksWorkspace", peakWS);
 
@@ -330,7 +240,8 @@ namespace Crystal
          if(peak.getRunNumber() == inWS->getRunNumber() && wi < Numberwi) MaxPeaks = i;
       }
     }
-    Geometry::Instrument_const_sptr Iptr = inWS->getInstrument();
+
+    int Edge = getProperty("EdgePixels");
     Progress prog(this, MinPeaks, 1.0, MaxPeaks);
     PARALLEL_FOR2(inWS,peakWS)
     for (int i = MinPeaks; i<= MaxPeaks; i++)
@@ -343,34 +254,27 @@ namespace Crystal
       double TOFPeakd = peak.getTOF();
       std::string bankName = peak.getBankName();
 
-      boost::shared_ptr<const IComponent> parent = Iptr->getComponentByName(bankName);
-
-      if (!parent) continue;
-      if (parent->type().compare("RectangularDetector") != 0) continue;
-      boost::shared_ptr<const RectangularDetector> RDet = boost::shared_dynamic_cast<
-                const RectangularDetector>(parent);
-
       double intensity = 0.0;
       double tofcentroid = 0.0;
-      boost::shared_ptr<Detector> pixel = RDet->getAtXY(col, row);
+      if (edgePixel(bankName, col, row, Edge))continue;
       Mantid::detid2index_map::iterator it;
-      it = (*wi_to_detid_map).find(pixel->getID());
+      it = (*wi_to_detid_map).find(findPixelID(bankName, col, row));
 
       double tofstart = TOFPeakd*std::pow(1.004,-PeakRadius);
       double tofend = TOFPeakd*std::pow(1.004,PeakRadius);
       double rowcentroid = 0.0;
       int rowstart = std::max(0,row-PeakRadius);
-      int rowend = std::min(RDet->ypixels()-1,row+PeakRadius);
+      int rowend = row+PeakRadius;
       double colcentroid = 0.0;
       int colstart = std::max(0,col-PeakRadius);
-      int colend = std::min(RDet->xpixels()-1,col+PeakRadius);
+      int colend = col+PeakRadius;
       for (int irow=rowstart; irow<=rowend; ++irow)
       {
         for (int icol=colstart; icol<=colend; ++icol)
         {
-          boost::shared_ptr<Detector> pixel = RDet->getAtXY(icol, irow);
           Mantid::detid2index_map::iterator it;
-          it = wi_to_detid_map->find(pixel->getID());
+          if (edgePixel(bankName, icol, irow, Edge))continue;
+          it = wi_to_detid_map->find(findPixelID(bankName, icol, irow));
           size_t workspaceIndex = (it->second);
           EventList el = eventW->getEventList(workspaceIndex);
           el.switchTo(WEIGHTED_NOTIME);
@@ -395,27 +299,29 @@ namespace Crystal
         }
       }
     // Set pixelID to change row and col
-      row = std::min(RDet->ypixels()-1,int(rowcentroid/intensity));
+      row = int(rowcentroid/intensity);
       row = std::max(0,row);
-      col = std::min(RDet->xpixels()-1,int(colcentroid/intensity));
+      col = int(colcentroid/intensity);
       col = std::max(0,col);
-      pixel = RDet->getAtXY(col, row);
-      peak.setDetectorID(pixel->getID());
+      if (!edgePixel(bankName, col, row, Edge))
+      {
+          peak.setDetectorID(findPixelID(bankName, col, row));
 
-    // Set wavelength to change tof for peak object
-      double tof = tofcentroid/intensity;
-      Mantid::Kernel::Units::Wavelength wl;
-      std::vector<double> timeflight;
-      timeflight.push_back(tof);
-      double scattering = peak.getScattering();
-      double L1 = peak.getL1();
-      double L2 = peak.getL2();
-      wl.fromTOF(timeflight, timeflight, L1, L2, scattering, 0, 0, 0);
-      const double lambda = timeflight[0];
-      timeflight.clear();
+          // Set wavelength to change tof for peak object
+          double tof = tofcentroid/intensity;
+          Mantid::Kernel::Units::Wavelength wl;
+          std::vector<double> timeflight;
+          timeflight.push_back(tof);
+          double scattering = peak.getScattering();
+          double L1 = peak.getL1();
+          double L2 = peak.getL2();
+          wl.fromTOF(timeflight, timeflight, L1, L2, scattering, 0, 0, 0);
+          const double lambda = timeflight[0];
+          timeflight.clear();
 
-      peak.setWavelength(lambda);
-      peak.setBinCount(intensity);
+          peak.setWavelength(lambda);
+          peak.setBinCount(intensity);
+      }
       PARALLEL_END_INTERUPT_REGION
     }
     PARALLEL_CHECK_INTERUPT_REGION
@@ -428,14 +334,7 @@ namespace Crystal
       int row = peak.getRow();
       std::string bankName = peak.getBankName();
 
-      boost::shared_ptr<const IComponent> parent = Iptr->getComponentByName(bankName);
-
-      if (!parent) continue;
-      if (parent->type().compare("RectangularDetector") != 0) continue;
-      boost::shared_ptr<const RectangularDetector> RDet = boost::shared_dynamic_cast<
-                const RectangularDetector>(parent);
-      int Edge = getProperty("EdgePixels");
-      if (col < Edge || col > (RDet->xpixels()-Edge) || row < Edge || row > (RDet->ypixels()-Edge))
+      if (edgePixel(bankName, col, row, Edge))
       {
         peakWS->removePeak(i);
       }
@@ -466,6 +365,60 @@ namespace Crystal
       this->integrate();
     }
   }
+  int CentroidPeaks::findPixelID(std::string bankName, int col, int row)
+  {
+	  Geometry::Instrument_const_sptr Iptr = inWS->getInstrument();
+	  boost::shared_ptr<const IComponent> parent = Iptr->getComponentByName(bankName);
+	  if (parent->type().compare("RectangularDetector") == 0)
+	  {
+		  boost::shared_ptr<const RectangularDetector> RDet = boost::shared_dynamic_cast<
+					const RectangularDetector>(parent);
+
+		  boost::shared_ptr<Detector> pixel = RDet->getAtXY(col, row);
+		  return pixel->getID();
+	  }
+	  else
+	  {
+		  std::string bankName0 = bankName;
+		  bankName0.erase(0,4);
+		  std::ostringstream pixelString;
+		  pixelString << Iptr->getName() << "/" << bankName0 << "/" <<bankName
+		  << "/tube" << std::setw(3) << std::setfill('0')<< col
+		  << "/pixel" << std::setw(4) << std::setfill('0')<< row;
+		  boost::shared_ptr<const Geometry::IComponent> component = Iptr->getComponentByName(pixelString.str());
+		  boost::shared_ptr<const Detector> pixel = boost::dynamic_pointer_cast<const Detector>(component);
+		  return pixel->getID();
+	  }
+  }
+  bool CentroidPeaks::edgePixel(std::string bankName, int col, int row, int Edge)
+  {
+	  Geometry::Instrument_const_sptr Iptr = inWS->getInstrument();
+	  boost::shared_ptr<const IComponent> parent = Iptr->getComponentByName(bankName);
+	  if (parent->type().compare("RectangularDetector") == 0)
+	  {
+		  boost::shared_ptr<const RectangularDetector> RDet = boost::shared_dynamic_cast<
+					const RectangularDetector>(parent);
+
+	      if (col < Edge || col > (RDet->xpixels()-Edge) || row < Edge || row > (RDet->ypixels()-Edge)) return true;
+	      else return false;
+	  }
+	  else
+	  {
+          int nPixels = std::max<int>(0, Edge);
+          std::vector<Geometry::IComponent_const_sptr> children;
+          boost::shared_ptr<const Geometry::ICompAssembly> asmb = boost::dynamic_pointer_cast<const Geometry::ICompAssembly>(parent);
+          asmb->getChildren(children, false);
+          boost::shared_ptr<const Geometry::ICompAssembly> asmb2 = boost::dynamic_pointer_cast<const Geometry::ICompAssembly>(children[0]);
+          std::vector<Geometry::IComponent_const_sptr> grandchildren;
+          asmb2->getChildren(grandchildren,false);
+          int NROWS = static_cast<int>(grandchildren.size());
+          int NCOLS = static_cast<int>(children.size());
+    	  if (row < nPixels || col < nPixels || NROWS-row < nPixels || NCOLS-col < nPixels) return true;
+    	  else return false;
+	  }
+	  return false;
+  }
+
 
 } // namespace Mantid
 } // namespace Crystal
