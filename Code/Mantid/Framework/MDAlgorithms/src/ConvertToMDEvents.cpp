fcec6798ad985a094c86a99a223f7b4fd062cbad
Alex Buts <Alex.Buts@stfc.ac.uk>
2013-01-27 16:31:53 +0000

    refs #6433 This should fix it.

--- Code/Mantid/Framework/MDAlgorithms/src/ConvertToMDEvents.cpp
+++ Code/Mantid/Framework/MDAlgorithms/src/ConvertToMDEvents.cpp
@@ -351,7 +351,9 @@ void ConvertToMDEvents::exec()
 
 /** Constructor */
 ConvertToMDEvents::ConvertToMDEvents()
-{}
+{ 
+  this->useAlgorithm("ConvertToMD");
+}
 /** Constructor
 ConvertToMDEvents::ConvertToMDEvents()
 {
