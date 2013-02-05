4d9d79469bbcd912351bf9c650f5e6a181a0249f
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-05 10:25:47 +0000

    fetch_third_party.bat now checks for presence of git.cmd/git.exe
    Newer versions of git on Windows have no git.cmd. Refs #6486

--- Code/fetch_Third_Party.bat
+++ Code/fetch_Third_Party.bat
@@ -4,6 +4,22 @@
 
 @echo off
 
+:: Check for git. Older versions used %GitCmd%, newer just git
+echo Checking for git.cmd
+where /Q git.cmd
+if ERRORLEVEL 1 (
+    echo Not Found. Checking for git.exe
+    where /Q git.exe
+    if ERRORLEVEL 1 (
+        echo Cannot find git. Make sure the cmd folder is in your path.
+        exit /b 1
+    ) else (
+        set GitCmd=git.exe
+    )
+) else (
+    set GitCmd=git.cmd
+)
+
 :: Check whether we're 64 or 32 bit. Store it in the 'arch' variable
 Set RegQry=HKLM\Hardware\Description\System\CentralProcessor\0
  
@@ -22,7 +38,7 @@ del CheckOS.txt
 del StringCheck.txt
 
 :: Find out the url where mantid came from so we use the same location & protocol
-FOR /F %%I IN ('git.cmd config --get remote.origin.url') DO SET url=%%I
+FOR /F %%I IN ('%GitCmd% config --get remote.origin.url') DO SET url=%%I
 echo Mantid repository URL: %url%
 
 :: Check if includes are already there - if so, just update
@@ -31,7 +47,7 @@ IF EXIST Third_Party/include GOTO UpdateInc
 set incs=%url:mantid.git=%3rdpartyincludes.git
 :: Otherwise we need to clone
 echo Cloning Third_Party includes from %incs%
-call git.cmd clone %incs% Third_Party/include
+call %GitCmd% clone %incs% Third_Party/include
 
 :DoLibs
 :: Check is libs are already there - if so, just update
@@ -42,14 +58,14 @@ echo %libs%
 
 :: Otherwise we need to clone
 echo Cloning Third_Party libraries from %libs%
-call git.cmd clone %libs% Third_Party/lib/%arch%
+call %GitCmd% clone %libs% Third_Party/lib/%arch%
 exit 0
 
 :: Just making sure what we have is up to date
 :UpdateInc
 echo Updating Third_Party includes...
 cd Third_Party/include
-call git.cmd pull
+call %GitCmd% pull
 :: Be sure to end up back where we started
 cd ../..
 GOTO :DoLibs
@@ -57,7 +73,7 @@ GOTO :DoLibs
 :UpdateLib
 echo Updating Third_Party libraries...
 cd Third_Party/lib/%arch%
-call git.cmd pull
+call %GitCmd% pull
 :: Be sure to end up back where we started
 cd ../../..
 exit 0
