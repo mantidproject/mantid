934e7b1bd2cbed7a7b6c73f92639fa5bdd6112ad
Jose Borreguero <borreguero@gmail.com>
2013-02-20 12:00:50 -0500

    Refs #6597 Fixed the header

--- Code/Mantid/Framework/Algorithms/src/ExtractFFTSpectrum.cpp
+++ Code/Mantid/Framework/Algorithms/src/ExtractFFTSpectrum.cpp
@@ -1,6 +1,48 @@
-/*WIKI* This algorithm iterates the [[FFT]] algorithm on each spectrum of InputWorkspace, computing the Fourier Transform and storing the transformed spectrum in OutputWorkspace. If InputImagWorkspace is also passed, then the pair spectrum ''i'' of InputWorkspace (real) and spectrum ''i'' of InputImagWorkspace (real) are taken together as spectrum ''i'' of a complex workspace, on which [[FFT]] is applied.
+/*WIKI*
 
-The FFTPart parameter specifies which spectra is selected from the output of the [[FFT]] algorithm. Allowed values are: 3 - the real part of the transform, 4 - the imaginary part, 5 - the modulus. Values 0,1, and 2 repeat the spectra for values 3,4, and 5 respectively, but for the positive frequencies only. Consult the [[FFT]] for more details.
+This algorithm iterates the [[FFT]] algorithm on each spectrum of InputWorkspace, computing the Fourier Transform and storing the transformed spectrum in OutputWorkspace. If InputImagWorkspace is also passed, then the pair spectrum ''i'' of InputWorkspace (real) and spectrum ''i'' of InputImagWorkspace (real) are taken together as spectrum ''i'' of a complex workspace, on which [[FFT]] is applied.
+
+The FFTPart parameter specifies which transform is selected from the output of the [[FFT]] algorithm:
+
+For the case of input containing real and imaginary workspaces:
+{| border="1" cellpadding="5" cellspacing="0"
+!FFTPart
+!Description
+|-
+|0
+|Complete real part
+|-
+|1
+|Complete imaginary part
+|-
+|2
+|Complete transform modulus
+|}
+
+For the case of input containing no imaginary workspace:
+{| border="1" cellpadding="5" cellspacing="0"
+!FFTPart
+!Description
+|-
+|0
+|Real part, positive frequencies
+|-
+|1
+|Imaginary part, positive frequencies
+|-
+|2
+|Modulus, positive frequencies
+|-
+|3
+|Complete real part
+|-
+|4
+|Complete imaginary part
+|-
+|5
+|Complete transform modulus
+|}
+
 *WIKI*/
 //----------------------------------------------------------------------
 // Includes
