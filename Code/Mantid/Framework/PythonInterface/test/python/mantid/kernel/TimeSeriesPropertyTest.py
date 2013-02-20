d2a724a94f812bdd281d905b2bbe02af0e0f9bf0
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-20 09:33:52 +0000

    Be less strict on element types Refs #6508
    Windows dislikes the element type comparison even though it looks the
    same when you print them out...

--- Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/TimeSeriesPropertyTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/TimeSeriesPropertyTest.py
@@ -14,34 +14,34 @@ class TimeSeriesPropertyTest(unittest.TestCase):
 
     def test_time_series_double_can_be_extracted(self):
         log_series = self._test_ws.getRun()["TEMP1"]
-        self._check_has_time_series_attributes(log_series, np.float64)
+        self._check_has_time_series_attributes(log_series)
         self.assertEquals(log_series.size(), 63)
         self.assertAlmostEqual(log_series.nthValue(0), -0.00161)
 
-    #def test_time_series_int_can_be_extracted(self):
-    #    log_series = self._test_ws.getRun()["raw_frames"]
-    #    self._check_has_time_series_attributes(log_series, np.int32)
-    #    self.assertEquals(log_series.size(), 172)
-    #    self.assertEquals(log_series.nthValue(0), 17)
+    def test_time_series_int_can_be_extracted(self):
+        log_series = self._test_ws.getRun()["raw_frames"]
+        self._check_has_time_series_attributes(log_series)
+        self.assertEquals(log_series.size(), 172)
+        self.assertEquals(log_series.nthValue(0), 17)
 
     def test_time_series_string_can_be_extracted(self):
         log_series = self._test_ws.getRun()["icp_event"]
-        self._check_has_time_series_attributes(log_series, str)
+        self._check_has_time_series_attributes(log_series, list)
         self.assertEquals(log_series.size(), 11)
         self.assertEquals(log_series.nthValue(0).strip(), 'CHANGE_PERIOD 1')
         
     def test_time_series_bool_can_be_extracted(self):
         log_series = self._test_ws.getRun()["period 1"]
-        self._check_has_time_series_attributes(log_series, np.bool_)
+        self._check_has_time_series_attributes(log_series)
         self.assertEquals(log_series.size(), 1)
     
-    def _check_has_time_series_attributes(self, log, element_type):
+    def _check_has_time_series_attributes(self, log, values_type=np.ndarray):
         self.assertTrue(hasattr(log, "value"))
         self.assertTrue(hasattr(log, "times"))
         self.assertTrue(hasattr(log, "getStatistics"))
 
         values = log.value
-        self.assertEquals(element_type, type(values[0]))
+        self.assertTrue(isinstance(values, values_type))
         self.assertEquals(log.size(), len(values))
 
 
