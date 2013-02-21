9b8d38492fc9c13df58110777728366ff0fa3546
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-02-21 14:43:16 +0000

    refs #6588 skip test on darwin

--- Code/Mantid/MantidPlot/test/AlgorithmDialogTest.py
+++ Code/Mantid/MantidPlot/test/AlgorithmDialogTest.py
@@ -4,14 +4,16 @@ Test generation of Algorithm Dialogs
 import mantidplottests
 from mantidplottests import *
 import mantidqtpython
+import sys
 
 class AlgorithmDialogTest(unittest.TestCase):
     
     def test_OpenDialog(self):
-        interface_manager = mantidqtpython.MantidQt.API.InterfaceManager()
-        dialog = threadsafe_call( interface_manager.createDialogFromName, "CreateMDWorkspace", False)
-        is_instance_of_alg_dialog = isinstance(dialog, mantidqtpython.MantidQt.API.AlgorithmDialog)
-        self.assertEqual(is_instance_of_alg_dialog, True)
+        if not sys.platform == 'darwin':
+            interface_manager = mantidqtpython.MantidQt.API.InterfaceManager()
+            dialog = threadsafe_call( interface_manager.createDialogFromName, "CreateMDWorkspace", False)
+            is_instance_of_alg_dialog = isinstance(dialog, mantidqtpython.MantidQt.API.AlgorithmDialog)
+            self.assertEqual(is_instance_of_alg_dialog, True)
     
 # Run the unit tests
 mantidplottests.runTests(AlgorithmDialogTest) 
