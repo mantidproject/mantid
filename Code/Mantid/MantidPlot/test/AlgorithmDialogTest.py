8683d276678d0d7dea6db0be70bc07a3f2d99457
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-02-21 15:02:43 +0000

    refs #6588 explicitly pass.

--- Code/Mantid/MantidPlot/test/AlgorithmDialogTest.py
+++ Code/Mantid/MantidPlot/test/AlgorithmDialogTest.py
@@ -14,6 +14,8 @@ class AlgorithmDialogTest(unittest.TestCase):
             dialog = threadsafe_call( interface_manager.createDialogFromName, "CreateMDWorkspace", False)
             is_instance_of_alg_dialog = isinstance(dialog, mantidqtpython.MantidQt.API.AlgorithmDialog)
             self.assertEqual(is_instance_of_alg_dialog, True)
+        else:
+            pass
     
 # Run the unit tests
 mantidplottests.runTests(AlgorithmDialogTest) 
