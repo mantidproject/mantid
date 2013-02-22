522627f3a3c9f92e40d181495c36fec9ea396235
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-02-22 10:47:01 +0000

    refs #6601. Add to and refactor python tests.

--- Code/Mantid/MantidPlot/test/MantidPlotAlgorithmDialogTest.py
+++ Code/Mantid/MantidPlot/test/MantidPlotAlgorithmDialogTest.py
@@ -5,18 +5,33 @@ import mantidplottests
 from mantidplottests import *
 import mantidqtpython
 import sys
+import tempfile
 
 class MantidPlotAlgorithmDialogTest(unittest.TestCase):
     
+    __target_algorithm__ = "CreateMDWorkspace"
+    __clean_properties__ = True
+        
     def test_OpenDialog(self):
-        if not sys.platform == 'darwin':
-            interface_manager = mantidqtpython.MantidQt.API.InterfaceManager()
-            dialog = threadsafe_call( interface_manager.createDialogFromName, "CreateMDWorkspace", False)
-            is_instance_of_alg_dialog = isinstance(dialog, mantidqtpython.MantidQt.API.AlgorithmDialog)
-            self.assertEqual(is_instance_of_alg_dialog, True)
-        else:
-            pass
-    
+        interface_manager = mantidqtpython.MantidQt.API.InterfaceManager()
+        dialog = threadsafe_call( interface_manager.createDialogFromName, self.__target_algorithm__, self.__clean_properties__)
+        is_instance_of_alg_dialog = isinstance(dialog, mantidqtpython.MantidQt.API.AlgorithmDialog)
+        self.assertEqual(is_instance_of_alg_dialog, True, "Did not get an AlgorithmDialog instance back.")
+        threadsafe_call( dialog.close )
+        
+    def test_ScreenShotDialog(self):
+        interface_manager = mantidqtpython.MantidQt.API.InterfaceManager()
+        dialog = threadsafe_call( interface_manager.createDialogFromName, self.__target_algorithm__, self.__clean_properties__)
+        screenshotdir = tempfile.gettempdir();
+        file = "CreateMDWorkspace_screenshot"
+        screenshot(widget=dialog, filename=file, screenshot_dir=screenshotdir)
+        threadsafe_call(dialog.close)
+        file_abs = os.path.join(screenshotdir, file + ".png")
+        file_exists = os.path.isfile(file_abs)
+        self.assertEquals(file_exists, True, "Screenshot was not written out as expected.")
+        if file_exists:
+            os.remove(file_abs)
+        
 # Run the unit tests
 mantidplottests.runTests(MantidPlotAlgorithmDialogTest) 
     
