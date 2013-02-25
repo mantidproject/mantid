279ec8f806ebf5d541eee9a27ab3b4997ee085b0
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-25 09:27:02 +0000

    Upgrade embedded scons for windows to 2.2 to work with VS 2012
    Refs #6610

--- Code/Mantid/Installers/WinInstaller/scons-local/scons.py
+++ Code/Mantid/Installers/WinInstaller/scons-local/scons.py
@@ -2,7 +2,7 @@
 #
 # SCons - a Software Constructor
 #
-# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010 The SCons Foundation
+# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -22,22 +22,20 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
 
-__revision__ = "src/script/scons.py 4720 2010/03/24 03:14:11 jars"
+__revision__ = "src/script/scons.py issue-2856:2676:d23b7a2f45e8 2012/08/05 15:38:28 garyo"
 
-__version__ = "1.3.0"
+__version__ = "2.2.0"
 
-__build__ = "r4720"
+__build__ = "issue-2856:2676:d23b7a2f45e8[MODIFIED]"
 
-__buildsys__ = "jars-desktop"
+__buildsys__ = "oberbrunner-dev"
 
-__date__ = "2010/03/24 03:14:11"
+__date__ = "2012/08/05 15:38:28"
 
-__developer__ = "jars"
+__developer__ = "garyo"
 
 import os
-import os.path
 import sys
 
 ##############################################################################
@@ -61,7 +59,8 @@ import sys
 # If so exit with error message
 try:
     if  sys.version_info >= (3,0,0):
-        msg = "scons: *** SCons version %s does not run under Python version %s.\n"
+        msg = "scons: *** SCons version %s does not run under Python version %s.\n\
+Python 3.0 and later are not yet supported.\n"
         sys.stderr.write(msg % (__version__, sys.version.split()[0]))
         sys.exit(1)
 except AttributeError:
@@ -76,7 +75,7 @@ if script_dir in sys.path:
 
 libs = []
 
-if os.environ.has_key("SCONS_LIB_DIR"):
+if "SCONS_LIB_DIR" in os.environ:
     libs.append(os.environ["SCONS_LIB_DIR"])
 
 local_version = 'scons-local-' + __version__
@@ -89,7 +88,21 @@ libs.append(os.path.abspath(local))
 
 scons_version = 'scons-%s' % __version__
 
+# preferred order of scons lookup paths
 prefs = []
+
+try:
+    import pkg_resources
+except ImportError:
+    pass
+else:
+    # when running from an egg add the egg's directory 
+    try:
+        d = pkg_resources.get_distribution('scons')
+    except pkg_resources.DistributionNotFound:
+        pass
+    else:
+        prefs.append(d.location)
 
 if sys.platform == 'win32':
     # sys.prefix is (likely) C:\Python*;
@@ -137,12 +150,11 @@ else:
         # check only /foo/lib/scons*.
         prefs.append(sys.prefix)
 
-    temp = map(lambda x: os.path.join(x, 'lib'), prefs)
-    temp.extend(map(lambda x: os.path.join(x,
+    temp = [os.path.join(x, 'lib') for x in prefs]
+    temp.extend([os.path.join(x,
                                            'lib',
                                            'python' + sys.version[:3],
-                                           'site-packages'),
-                           prefs))
+                                           'site-packages') for x in prefs])
     prefs = temp
 
     # Add the parent directory of the current python's library to the
@@ -160,23 +172,10 @@ else:
         # Check /usr/libfoo/scons*.
         prefs.append(libpath)
 
-    try:
-        import pkg_resources
-    except ImportError:
-        pass
-    else:
-        # when running from an egg add the egg's directory 
-        try:
-            d = pkg_resources.get_distribution('scons')
-        except pkg_resources.DistributionNotFound:
-            pass
-        else:
-            prefs.append(d.location)
-
 # Look first for 'scons-__version__' in all of our preference libs,
 # then for 'scons'.
-libs.extend(map(lambda x: os.path.join(x, scons_version), prefs))
-libs.extend(map(lambda x: os.path.join(x, 'scons'), prefs))
+libs.extend([os.path.join(x, scons_version) for x in prefs])
+libs.extend([os.path.join(x, 'scons') for x in prefs])
 
 sys.path = libs + sys.path
 
