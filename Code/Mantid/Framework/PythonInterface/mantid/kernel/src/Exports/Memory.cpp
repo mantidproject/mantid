2f43257855b04cd7b185eb90b133bfd1ec1df2d5
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-25 16:35:02 +0000

    Export MemoryStats class to Python. Refs #6614

---
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/src/Exports/Memory.cpp
@@ -0,0 +1,22 @@
+#include "MantidKernel/Memory.h"
+#include <boost/python/class.hpp>
+
+using Mantid::Kernel::MemoryStats;
+using namespace boost::python;
+
+void export_MemoryStats()
+{
+
+  class_< MemoryStats>("MemoryStats", init<>("Construct MemoryStats object."))
+    .def("update", &MemoryStats::update)
+    .def("totalMem", &MemoryStats::totalMem)
+    .def("availMem", &MemoryStats::availMem)
+    .def("residentMem", &MemoryStats::residentMem)
+    .def("virtualMem", &MemoryStats::virtualMem)
+    .def("reservedMem", &MemoryStats::reservedMem)
+    .def("getFreeRatio", &MemoryStats::getFreeRatio)
+    ;
+
+}
+
+
