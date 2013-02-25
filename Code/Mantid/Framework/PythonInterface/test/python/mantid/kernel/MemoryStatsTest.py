8bb437dac9fab81de767fe61899a20a2c1057f98
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-02-25 16:46:04 +0000

    Fix MemoryStats test for older unittest modules. Refs #6614

--- Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/MemoryStatsTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/MemoryStatsTest.py
@@ -8,12 +8,12 @@ class MemoryStatsTest(unittest.TestCase):
         mem = MemoryStats()
 
         self.assertTrue(hasattr(mem, "update"))
-        self.assertGreater(mem.availMem, 0.0, "Value should be larger than 0.0")
-        self.assertGreater(mem.totalMem, 0.0, "Value should be larger than 0.0")
-        self.assertGreater(mem.residentMem, 0.0, "Value should be larger than 0.0")
-        self.assertGreater(mem.virtualMem, 0.0, "Value should be larger than 0.0")
-        self.assertGreater(mem.reservedMem, 0.0, "Value should be larger than 0.0")
-        self.assertGreater(mem.getFreeRatio, 0.0, "Value should be larger than 0.0")
+        self.assertTrue(mem.availMem > 0.0, "Value should be larger than 0.0")
+        self.assertTrue(mem.totalMem > 0.0, "Value should be larger than 0.0")
+        self.assertTrue(mem.residentMem > 0.0, "Value should be larger than 0.0")
+        self.assertTrue(mem.virtualMem > 0.0, "Value should be larger than 0.0")
+        self.assertTrue(mem.reservedMem > 0.0, "Value should be larger than 0.0")
+        self.assertTrue(mem.getFreeRatio > 0.0, "Value should be larger than 0.0")
 
 if __name__ == '__main__':
     unittest.main()
