96531e653372e4f86e90bb341e738c1a455473f6
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-02-27 14:55:28 +0000

    Simple test in ApplyCalibrationTest.h written re #5797
    This test is passed by current version of ApplyCalibration.
    A more demanding test is needed in which a tube is rotated before calibration.
    
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/Framework/Algorithms/test/ApplyCalibrationTest.h
+++ Code/Mantid/Framework/Algorithms/test/ApplyCalibrationTest.h
@@ -22,6 +22,7 @@ using namespace Mantid::Algorithms;
 using namespace Mantid::API;
 using namespace Mantid::Kernel;
 using namespace Mantid::DataObjects;
+using Mantid::Geometry::IDetector_const_sptr;
 
 class ApplyCalibrationTest : public CxxTest::TestSuite
 {
@@ -40,8 +41,11 @@ public:
 
   void testExec()
   {
+
+     int ndets = 3; 
+
      // Create workspace with paremeterised instrument and put into data store
-     Workspace2D_sptr ws = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(3, 10, true);
+     Workspace2D_sptr ws = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(ndets, 10, true);
      const std::string wsName("ApplyCabrationWs");
      AnalysisDataServiceImpl & dataStore = AnalysisDataService::Instance();
      dataStore.add(wsName, ws);
@@ -51,17 +55,32 @@ public:
      posTableWs->addColumn("int","Detector ID");
      posTableWs->addColumn("V3D","Detector Position");
 
-     for(int i=0; i < 3; ++i)
+     for(int i=0; i < ndets; ++i)
      {
        TableRow row = posTableWs->appendRow();
-       row << i+1 << V3D(1.0,0.01*i,1.0); 
+       row << i+1 << V3D(1.0,0.01*i,2.0); 
      }
      TS_ASSERT_THROWS_NOTHING(appCalib.setPropertyValue("Workspace", wsName ));
      TS_ASSERT_THROWS_NOTHING(appCalib.setProperty<ITableWorkspace_sptr>("PositionTable", posTableWs ));
      TS_ASSERT_THROWS_NOTHING(appCalib.execute());
 
      TS_ASSERT( appCalib.isExecuted() );
+     
+     IDetector_const_sptr det = ws->getDetector(0);
+     int id = det->getID();
+     V3D newPos = det->getPos();
+     TS_ASSERT_EQUALS( id, 1);
+     TS_ASSERT_DELTA( newPos.X() , 1.0, 0.0001);
+     TS_ASSERT_DELTA( newPos.Y() , 0.0, 0.0001);
+     TS_ASSERT_DELTA( newPos.Z() , 2.0, 0.0001);
 
+     det = ws->getDetector(ndets-1);
+     id = det->getID();
+     newPos = det->getPos();
+     TS_ASSERT_EQUALS( id, ndets);
+     TS_ASSERT_DELTA( newPos.X() , 1.0, 0.0001);
+     TS_ASSERT_DELTA( newPos.Y() , 0.01*(ndets-1), 0.0001);
+     TS_ASSERT_DELTA( newPos.Z() , 2.0, 0.0001);
   }
 
 private:
