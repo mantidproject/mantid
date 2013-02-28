70392b38712d44b750e0f6c2a4f9a85d74d362e1
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-02-28 16:48:25 +0000

    Wikimaker fixes for ResampleX
    re #6485

--- Code/Mantid/Framework/Algorithms/src/ResampleX.cpp
+++ Code/Mantid/Framework/Algorithms/src/ResampleX.cpp
@@ -1,5 +1,9 @@
 /*WIKI*
-TODO: Enter a full wiki-markup description of your algorithm here. You can then use the Build/wiki_maker.py script to generate your full wiki page.
+
+This method will resample the x-axis with the number of specified bins. If the XMin and XMax parameters are supplied it will use those as the range, they can be supplied as a comma delimited list or as a single value.
+ 
+The LogBinning option calculates constant delta-X/X binning and rebins using that.
+
 *WIKI*/
 
 #include <sstream>
