0707ad228b2e4bbfba3c28f005405fd03650e0d8
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-03-05 13:08:46 +0000

    Move the ISISCommandInterface to Python V2
    re #6578

--- Code/Mantid/scripts/SANS/ISISCommandInterface.py
+++ Code/Mantid/scripts/SANS/ISISCommandInterface.py
@@ -9,7 +9,7 @@ import isis_reduction_steps
 import isis_reducer
 from centre_finder import CentreFinder as CentreFinder
 #import SANSReduction
-import MantidFramework
+import mantid
 import copy
 from SANSadd2 import *
 
@@ -37,7 +37,7 @@ def SetVerboseMode(state):
 # Print a message and log it if the 
 def _printMessage(msg, log = True, no_console=False):
     if log == True and _VERBOSE_ == True:
-        MantidFramework.mtd.sendLogMessage('::SANS::' + msg)
+        mantid.logger.notice('::SANS::' + msg)
     if not no_console:
         print msg
     
@@ -491,7 +491,7 @@ def WavRangeReduction(wav_start=None, wav_end=None, full_trans_wav=None, name_su
             if consider_can:
                 mergedQ -= (Cf_can+Cr_can)/(Nf_can/scale + Nr_can)
             
-            RenameWorkspace(mergedQ, retWSname_merged)
+            RenameWorkspace(InputWorkspace=mergedQ,OutputWorkspace= retWSname_merged)
         else:
             issueWarning('rear and front data has no overlapping q-region. Merged workspace no calculated')
         
@@ -511,7 +511,7 @@ def WavRangeReduction(wav_start=None, wav_end=None, full_trans_wav=None, name_su
     if reduce_front_flag:
         frontWS = mtd[retWSname_front]
         frontWS = (frontWS+shift)*scale
-        RenameWorkspace(frontWS, retWSname_front)
+        RenameWorkspace(InputWorkspace=frontWS,OutputWorkspace= retWSname_front)
 
     # finished calculating cross section so can restore these value
     ReductionSingleton().to_Q.outputParts = toRestoreOutputParts
@@ -628,7 +628,7 @@ def _WavRangeReduction(name_suffix=None):
     if name_suffix:
         old = result
         result += name_suffix
-        RenameWorkspace(old, result)
+        RenameWorkspace(InputWorkspace=old,OutputWorkspace= result)
         
     return result
 
@@ -643,9 +643,9 @@ def delete_workspaces(workspaces):
             workspaces = [workspaces]
 
     for wksp in workspaces:
-        if wksp and mtd.workspaceExists(wksp):
+        if wksp and wksp in mtd:
             try:
-                DeleteWorkspace(wksp)
+                DeleteWorkspace(Workspace=wksp)
             except:
                 #we're only deleting to save memory, if the workspace really won't delete leave it
                 pass
@@ -933,15 +933,15 @@ def DisplayMask(mask_worksp=None):
         
         if samp:
             counts_data = '__DisplayMasked_tempory_wksp'
-            Integration(samp, counts_data)
-            CloneWorkspace(samp, mask_worksp)
+            Integration(InputWorkspace=samp,OutputWorkspace= counts_data)
+            CloneWorkspace(InputWorkspace=samp,OutputWorkspace= mask_worksp)
         else:
             instrument.load_empty(mask_worksp)
             instrument.set_up_for_run('emptyInstrument')
         
     ReductionSingleton().mask.display(mask_worksp, ReductionSingleton(), counts_data)
     if counts_data:
-        mantid.deleteWorkspace(counts_data)
+        DeleteWorkspace(counts_data)
         
     return mask_worksp
 
@@ -1032,7 +1032,7 @@ def FindBeamCentre(rlow, rupp, MaxIter = 10, xstart = None, ystart = None):
     xstart = beamcoords[0]
     ystart = beamcoords[1]
     
-    mantid.sendLogMessage("::SANS:: xstart,ystart="+str(XNEW*1000.)+" "+str(YNEW*1000.)) 
+    logger.notice("::SANS:: xstart,ystart="+str(XNEW*1000.)+" "+str(YNEW*1000.)) 
     _printMessage("Starting centre finding routine ...")
 
     #remove this if we know running the Reducer() doesn't change i.e. all execute() methods are const
@@ -1045,7 +1045,7 @@ def FindBeamCentre(rlow, rupp, MaxIter = 10, xstart = None, ystart = None):
     centre_reduction = copy.deepcopy(ReductionSingleton().reference())
     LimitsR(str(float(rlow)), str(float(rupp)), quiet=True, reducer=centre_reduction)
 
-    mantid.sendLogMessage(centre.status_str(0, resX_old, resY_old))
+    logger.notice(centre.status_str(0, resX_old, resY_old))
     
     # take first trial step
     XNEW = xstart + XSTEP
@@ -1061,7 +1061,7 @@ def FindBeamCentre(rlow, rupp, MaxIter = 10, xstart = None, ystart = None):
         centre_reduction = copy.deepcopy(ReductionSingleton().reference())
         LimitsR(str(float(rlow)), str(float(rupp)), quiet=True, reducer=centre_reduction)
 
-        mantid.sendLogMessage(centre.status_str(it, resX, resY))
+        logger.notice(centre.status_str(it, resX, resY))
         
         try :
             if not graph_handle:
@@ -1081,7 +1081,7 @@ def FindBeamCentre(rlow, rupp, MaxIter = 10, xstart = None, ystart = None):
             YSTEP = -YSTEP/2.
         if abs(XSTEP) < 0.1251/1000. and abs(YSTEP) < 0.1251/1000. :
             # this is the success criteria, we've close enough to the center
-            mantid.sendLogMessage("::SANS:: Converged - check if stuck in local minimum!")
+            logger.notice("::SANS:: Converged - check if stuck in local minimum!")
             break
         
         resX_old = resX
@@ -1090,7 +1090,7 @@ def FindBeamCentre(rlow, rupp, MaxIter = 10, xstart = None, ystart = None):
         YNEW += YSTEP
     
     if it == MaxIter:
-        mantid.sendLogMessage("::SANS:: Out of iterations, new coordinates may not be the best!")
+        logger.notice("::SANS:: Out of iterations, new coordinates may not be the best!")
         XNEW -= XSTEP
         YNEW -= YSTEP
     
