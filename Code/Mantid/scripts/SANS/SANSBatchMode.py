b1dfe746c762c9e2347c18f21f03a6cf1b34d7e6
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-03-05 12:51:04 +0000

    Move the SANSBatchMode to Python V2
    re #6578

--- Code/Mantid/scripts/SANS/SANSBatchMode.py
+++ Code/Mantid/scripts/SANS/SANSBatchMode.py
@@ -29,7 +29,7 @@
 
 #Make the reduction module available
 from ISISCommandInterface import *
-from mantidsimple import *
+from mantid.simpleapi import *
 import copy
 import sys
 
@@ -178,7 +178,7 @@ def BatchReduce(filename, format, plotresults=False, saveAlgs={'SaveRKH':'txt'},
                 exec(algor+'(reduced,file+ext)')
 
         if verbose:
-            mantid.sendLogMessage('::SANS::' + createColetteScript(run, format, reduced, centreit, plotresults, filename))
+            logger.notice('::SANS::' + createColetteScript(run, format, reduced, centreit, plotresults, filename))
         # Rename the final workspace
         final_name = run['output_as']
         if final_name == '':
@@ -188,18 +188,18 @@ def BatchReduce(filename, format, plotresults=False, saveAlgs={'SaveRKH':'txt'},
         names = [final_name]
         if combineDet == 'rear':
             names = [final_name+'_rear']
-            RenameWorkspace(reduced, final_name+'_rear')
+            RenameWorkspace(InputWorkspace=reduced,OutputWorkspace= final_name+'_rear')
         elif combineDet == 'front':
             names = [final_name+'_front']
-            RenameWorkspace(reduced, final_name+'_front')
+            RenameWorkspace(InputWorkspace=reduced,OutputWorkspace= final_name+'_front')
         elif combineDet == 'both':
             names = [final_name+'_front', final_name+'_rear']
             if ins_name == 'SANS2D':
                 rear_reduced = reduced.replace('front','rear')
             else: #if ins_name == 'lOQ':
                 rear_reduced = reduced.replace('HAB','main')
-            RenameWorkspace(reduced,final_name+'_front')
-            RenameWorkspace(rear_reduced,final_name+'_rear')
+            RenameWorkspace(InputWorkspace=reduced,OutputWorkspace=final_name+'_front')
+            RenameWorkspace(InputWorkspace=rear_reduced,OutputWorkspace=final_name+'_rear')
         elif combineDet == 'merged':            
             names = [final_name + '_merged', final_name + '_rear',  final_name+'_front']
             if ins_name == 'SANS2D':
@@ -208,11 +208,11 @@ def BatchReduce(filename, format, plotresults=False, saveAlgs={'SaveRKH':'txt'},
             else:
                 rear_reduced = reduced.replace('_merged','')
                 front_reduced = rear_reduced.replace('main','HAB')
-            RenameWorkspace(reduced, final_name + '_merged')
-            RenameWorkspace(rear_reduced, final_name + '_rear')
-            RenameWorkspace(front_reduced, final_name+'_front')            
+            RenameWorkspace(InputWorkspace=reduced,OutputWorkspace= final_name + '_merged')
+            RenameWorkspace(InputWorkspace=rear_reduced,OutputWorkspace= final_name + '_rear')
+            RenameWorkspace(InputWorkspace=front_reduced,OutputWorkspace= final_name+'_front')            
         else:            
-            RenameWorkspace(reduced,final_name)
+            RenameWorkspace(InputWorkspace=reduced,OutputWorkspace=final_name)
 
         if plotresults == 1:
             for final_name in names:
@@ -307,9 +307,9 @@ def delete_workspaces(workspaces):
             workspaces = [workspaces]
 
     for wksp in workspaces:
-        if wksp and mtd.workspaceExists(wksp):
+        if wksp and wksp in mtd:
             try:
-                DeleteWorkspace(wksp)
+                DeleteWorkspace(Workspace=wksp)
             except:
                 #we're only deleting to save memory, if the workspace really won't delete leave it
                 pass
