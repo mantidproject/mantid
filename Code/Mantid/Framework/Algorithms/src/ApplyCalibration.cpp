f39efb527fa44f5b34fc2c175e1ba5f442e18502
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-03-06 15:31:41 +0000

    Fix and tidy ApplyCalibration.cpp re #6344
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/Framework/Algorithms/src/ApplyCalibration.cpp
+++ Code/Mantid/Framework/Algorithms/src/ApplyCalibration.cpp
@@ -102,35 +102,6 @@ namespace Mantid
        // TransformType positionType = Relative;
        Geometry::ComponentHelper::moveComponent(*det, *m_pmap, pos, positionType);
 
-       
-      /*
-       const Geometry::IDetector_const_sptr det = instrument->getDetector(detID);
-       // Then find the corresponding relative position
-       boost::shared_ptr<const Geometry::IComponent> parent = det->getParent();
-       if (parent)
-       {
-         pos -= parent->getPos();
-         Quat rot = parent->getRelativeRot();
-         rot.inverse();
-         rot.rotate(pos);
-       }      
-       boost::shared_ptr<const Geometry::IComponent>grandparent = parent->getParent();
-       if (grandparent)
-       {
-         Quat rot = grandparent->getRelativeRot();
-         rot.inverse();
-         rot.rotate(pos);
-         boost::shared_ptr<const Geometry::IComponent>greatgrandparent = grandparent->getParent();
-         if (greatgrandparent) {
-           Quat rot2 = greatgrandparent->getRelativeRot();
-            rot2.inverse();
-            rot2.rotate(pos);
-         }
-       } 
-       */
-
-       // Add a parameter for the new position
-       m_pmap->addV3D(det.get(), "pos", pos);
     }
 
   } // namespace Algorithms
