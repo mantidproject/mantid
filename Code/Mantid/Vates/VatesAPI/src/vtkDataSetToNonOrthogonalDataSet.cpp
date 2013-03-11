b2586f3447fbcfd08d4ec9fb9cdf75b04d942b9c
Michael Reuter <reuterma@ornl.gov>
2013-03-11 16:48:06 -0400

    Refs #6697. Adding more working code, but using some hardcoded values.

--- Code/Mantid/Vates/VatesAPI/src/vtkDataSetToNonOrthogonalDataSet.cpp
+++ Code/Mantid/Vates/VatesAPI/src/vtkDataSetToNonOrthogonalDataSet.cpp
@@ -2,6 +2,9 @@
 
 #include <vtkDataSet.h>
 #include <vtkFieldData.h>
+#include <vtkFloatArray.h>
+#include <vtkMatrix3x3.h>
+#include <vtkNew.h>
 #include <vtkPoints.h>
 #include <vtkUnstructuredGrid.h>
 
@@ -44,14 +47,38 @@ void vtkDataSetToNonOrthogonalDataSet::execute()
   }
   // Get the original points
   vtkPoints *points = data->GetPoints();
-  double *point;
+  double *inPoint;
+  double outPoint[3];
   vtkPoints *newPoints = vtkPoints::New();
   newPoints->Allocate(points->GetNumberOfPoints());
+
+  /*
+  // Gd2, HKLE
+  double convert[9] = {0.475446282, 0.158482094, 0.0,
+                       0.274499039, -0.274499039, 0.0,
+                       0.0, 0.0, 0.172980453};
+  */
+  /*
+  // Gd, HKLE
+  double convert[9] = {0.316964188, 0.158482094, 0.0,
+                       0.0, 0.274499039, 0.0,
+                       0.0, 0.0, 0.172980453};
+  */
+  // Gd, HEKL
+  double convert[9] = {0.316964188, 0.0, 0.158482094,
+                       0.0, 1.0, 0.0,
+                       0.0, 0.0, 0.274499039};
+  /*
+  // Gd2, HEKL
+  double convert[9] = {0.475446282, 0.0, 0.158482094,
+                       0.0, 1.0, 0.0,
+                       0.274499039, 0.0, -0.274499039};
+  */
   for(int i = 0; i < points->GetNumberOfPoints(); i++)
   {
-    point = points->GetPoint(i);
-
-    newPoints->InsertNextPoint(point);
+    inPoint = points->GetPoint(i);
+    vtkMatrix3x3::MultiplyPoint(convert, inPoint, outPoint);
+    newPoints->InsertNextPoint(outPoint);
   }
   data->SetPoints(newPoints);
   this->updateMetaData(data);
@@ -59,7 +86,51 @@ void vtkDataSetToNonOrthogonalDataSet::execute()
 
 void vtkDataSetToNonOrthogonalDataSet::updateMetaData(vtkUnstructuredGrid *ugrid)
 {
-  vtkFieldData *fd = ugrid->GetFieldData();
+  /*
+  // Gd2, HKLE
+  double baseX[3] = {0.8660254, 0.5, 0.0};
+  double baseY[3] = {0.5, -0.8660254, 0.0};
+  double baseZ[3] = {0.0, 0.0, 1.0};
+  */
+  /*
+  // Gd, HKLE
+  double baseX[3] = {1.0, 0.0, 0.0};
+  double baseY[3] = {0.5, 0.8660254, 0.0};
+  double baseZ[3] = {0.0, 0.0, 1.0};
+  */
+  // Gd, HEKL
+  double baseX[3] = {1.0, 0.0, 0.0};
+  double baseY[3] = {0.0, 1.0, 0.0};
+  double baseZ[3] = {0.5, 0.0, 0.8660254};
+  /*
+  // Gd2, HEKL
+  double baseX[3] = {0.8660254, 0.0, 0.5};
+  double baseY[3] = {0.0, 1.0, 0.0};
+  double baseZ[3] = {0.5, 0.0, -0.8660254};
+  */
+
+  vtkFieldData *fieldData = ugrid->GetFieldData();
+
+  vtkNew<vtkFloatArray> uBase;
+  uBase->SetNumberOfComponents(3);
+  uBase->SetNumberOfTuples(1);
+  uBase->SetName("AxisBaseForX");
+  uBase->SetTuple(0, baseX);
+  fieldData->AddArray(uBase.GetPointer());
+
+  vtkNew<vtkFloatArray> vBase;
+  vBase->SetNumberOfComponents(3);
+  vBase->SetNumberOfTuples(1);
+  vBase->SetName("AxisBaseForY");
+  vBase->SetTuple(0, baseY);
+  fieldData->AddArray(vBase.GetPointer());
+
+  vtkNew<vtkFloatArray> wBase;
+  wBase->SetNumberOfComponents(3);
+  wBase->SetNumberOfTuples(1);
+  wBase->SetName("AxisBaseForZ");
+  wBase->SetTuple(0, baseZ);
+  fieldData->AddArray(wBase.GetPointer());
 }
 
 } // namespace VATES
