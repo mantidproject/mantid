2f76197ff9028d730610108a1bfa9f7d5b37657e
Ricardo Leal <ricardo.leal@ill.fr>
2013-03-13 10:50:48 +0100

    Re #6614 Updated IN4 definition

--- Code/Mantid/Framework/DataHandling/src/LoadILL.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadILL.cpp
@@ -462,10 +462,18 @@ int LoadILL::getDetectorElasticPeakPosition(const NeXus::NXInt &data) {
 		//calculatedDetectorElasticPeakPosition = *it;
 		calculatedDetectorElasticPeakPosition = static_cast<int>(std::distance(cumulatedSumOfSpectras.begin(), it));
 
-		g_log.debug() << "Calculated Detector EPP: "
-				<< calculatedDetectorElasticPeakPosition;
-		g_log.debug() << " :: Read EPP from the nexus file: "
-				<< m_monitorElasticPeakPosition << std::endl;
+		if (calculatedDetectorElasticPeakPosition == 0) {
+			g_log.warning()
+							<< "Elastic peak position is ZERO Assuming the EPP in the Nexus file: "
+							<< m_monitorElasticPeakPosition << std::endl;
+			calculatedDetectorElasticPeakPosition = m_monitorElasticPeakPosition;
+
+		} else {
+			g_log.debug() << "Calculated Detector EPP: "
+					<< calculatedDetectorElasticPeakPosition;
+			g_log.debug() << " :: Read EPP from the nexus file: "
+					<< m_monitorElasticPeakPosition << std::endl;
+		}
 	}
 	return calculatedDetectorElasticPeakPosition;
 
