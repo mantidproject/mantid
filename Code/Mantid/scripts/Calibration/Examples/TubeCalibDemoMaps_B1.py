2da18ac5d460eb01138515c742760f1471adf7cd
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-03-27 11:29:19 +0000

    Final improvement to TubCalibDemoMaps_B1.py re #6752
    Now I can make similar changes to other scripts.
    
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/scripts/Calibration/Examples/TubeCalibDemoMaps_B1.py
+++ Code/Mantid/scripts/Calibration/Examples/TubeCalibDemoMaps_B1.py
@@ -21,7 +21,7 @@ rangeUpper = 10000 #
 
 # Set initial parameters for peak finding
 ExpectedHeight = -1000.0 # Expected Height of Gaussian Peaks (initial value of fit parameter)
-ExpectedWidth = 8.0 # Expected width of peaks in pixels  (initial value of fit parameter)
+ExpectedWidth = 8.0 # Expected width of Gaussian peaks in pixels  (initial value of fit parameter)
 ExpectedPositions = [4.0, 85.0, 128.0, 161.0, 252.0] # Expected positions of the edges and peak in pixels (initial values of fit parameters)
 
 # Set what we want to calibrate (e.g whole intrument or one door )
