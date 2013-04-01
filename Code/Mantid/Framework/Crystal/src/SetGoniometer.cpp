5dc9cf37bc898ebbb4beedd2d05ea55803e875b7
Andrei Savici <saviciat@ornl.gov>
2013-04-01 12:15:11 -0400

    Change wiki section. Refs #6793

--- Code/Mantid/Framework/Crystal/src/SetGoniometer.cpp
+++ Code/Mantid/Framework/Crystal/src/SetGoniometer.cpp
@@ -5,7 +5,7 @@
 Use this algorithm to define your goniometer. Enter each axis in the order of rotation, starting with the one closest to the sample. 
 
 You may enter up to 6 axes, for which you must define (separated by commas): 
-* The name of the axis, which MUST match the name in your sample logs.
+* The name of the axis, which MUST match the name in your sample logs. You can specify a number, and a log value will be created (GoniometerAxis?_FixedValue, where ? is the axis number)
 * The X, Y, Z components of the vector of the axis of rotation. Right-handed coordinates with +Z=beam direction; +Y=Vertically up (against gravity); +X to the left.
 * The sense of rotation as 1 or -1: 1 for counter-clockwise, -1 for clockwise rotation.
 
