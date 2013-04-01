6c6811fdae8a15a79ca5c91d6729fef882db0d6f
Wenduo Zhou <zhouw@ornl.gov>
2013-04-01 12:08:27 -0400

    Rename interfact name.  Refs #6555.

--- Code/Mantid/scripts/Powder_Diffraction_Reduction.py
+++ Code/Mantid/scripts/Powder_Diffraction_Reduction.py
@@ -4,7 +4,6 @@
 from reduction_application import ReductionGUI
 from PyQt4 import QtCore, uic
 
-#reducer = ReductionGUI(instrument_list=["VULCAN"])
 reducer = ReductionGUI(instrument_list=["PG3", "NOM", "VULCAN"])
 reducer.setup_layout(load_last=True)
 reducer.show()
