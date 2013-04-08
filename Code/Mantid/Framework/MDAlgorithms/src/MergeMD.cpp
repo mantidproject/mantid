ec5b7b8948ac6e783d3cd1e822bdc530b8660890
Alex Buts <Alex.Buts@stfc.ac.uk>
2013-04-08 19:23:31 +0100

    refs #6841 now try to restore EI deleted earlier.

--- Code/Mantid/Framework/MDAlgorithms/src/MergeMD.cpp
+++ Code/Mantid/Framework/MDAlgorithms/src/MergeMD.cpp
@@ -167,19 +167,20 @@ namespace MDAlgorithms
     // Perform the initial box splitting
     out->splitBox();
 
-  // copy experiment infos
-    uint16_t nExperiments(0);
-    if(m_workspaces.size()>std::numeric_limits<uint16_t>::max())
+    // copy experiment infos
+     uint16_t nExperiments(0);
+     if(m_workspaces.size()>std::numeric_limits<uint16_t>::max())
         throw std::invalid_argument("currently we can not combine more then 65535 experiments");
-    else
+     else
         nExperiments = static_cast<uint16_t>(m_workspaces.size());
 
-    for (uint16_t i=0; i < nExperiments; i++)
-    {
-      API::ExperimentInfo_sptr ei = API::ExperimentInfo_sptr(m_workspaces[i]->getExperimentInfo(i)->cloneExperimentInfo());
-      out->addExperimentInfo(ei);
-    }
+     for (uint16_t i=0; i < nExperiments; i++)
+     {
+       API::ExperimentInfo_sptr ei = API::ExperimentInfo_sptr(m_workspaces[i]->getExperimentInfo(i)->cloneExperimentInfo());
+       out->addExperimentInfo(ei);
+     }
 
+ 
   }
 
 
