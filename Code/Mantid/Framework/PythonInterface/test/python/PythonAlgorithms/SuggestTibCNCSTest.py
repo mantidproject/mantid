85f6b2e0d08564ab70cacd49994b610002811d5e
Andrei Savici <saviciat@ornl.gov>
2013-04-15 16:52:20 -0400

    Fix tests for python 2.6. Refs #6857

--- Code/Mantid/Framework/PythonInterface/test/python/PythonAlgorithms/SuggestTibCNCSTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/PythonAlgorithms/SuggestTibCNCSTest.py
@@ -5,11 +5,11 @@ import mantid
 class SuggestTibCNCSTest(unittest.TestCase):
 	def test_simple(self):
 		result=mantid.simpleapi.SuggestTibCNCS(3.)
-		self.assertAlmostEqual(result[0],44201.,delta=1)
-		self.assertAlmostEqual(result[1],47314.,delta=1)
+		self.assertAlmostEqual(result[0]*0.1,4420.1,0)
+		self.assertAlmostEqual(result[1]*0.1,4731.4,0)
 		result=mantid.simpleapi.SuggestTibCNCS(1.)
-		self.assertAlmostEqual(result[0],96621.,delta=1)
-		self.assertAlmostEqual(result[1],99021.,delta=1)
+		self.assertAlmostEqual(result[0]*0.1,9662.1,0)
+		self.assertAlmostEqual(result[1]*0.1,9902.1,0)
 
 if __name__=="__main__":
 	unittest.main()
--- Code/Mantid/Framework/PythonInterface/test/python/PythonAlgorithms/SuggestTibHYSPECTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/PythonAlgorithms/SuggestTibHYSPECTest.py
@@ -5,11 +5,11 @@ import mantid
 class SuggestTibHYSPECTest(unittest.TestCase):
 	def test_simple(self):
 		result=mantid.simpleapi.SuggestTibHYSPEC(5.)
-		self.assertAlmostEqual(result[0],39515.,delta=1)
-		self.assertAlmostEqual(result[1],41515.,delta=1)
+		self.assertAlmostEqual(result[0]*.1,3951.5,0)
+		self.assertAlmostEqual(result[1]*.1,4151.5,0)
 		result=mantid.simpleapi.SuggestTibHYSPEC(40.)
-		self.assertAlmostEqual(result[0],11898.,delta=1)
-		self.assertAlmostEqual(result[1],13898.,delta=1)
+		self.assertAlmostEqual(result[0]*.1,1189.8,0)
+		self.assertAlmostEqual(result[1]*.1,1389.8,0)
 
 if __name__=="__main__":
 	unittest.main()
