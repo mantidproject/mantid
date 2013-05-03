cfeef769fd2649573064d8dc2356b2c9632f5165
Andrei Savici <saviciat@ornl.gov>
2013-05-02 21:02:47 -0400

    Fix small bugs. Refs #6965

--- Code/Mantid/docs/qtassistant/parseLinks.py
+++ Code/Mantid/docs/qtassistant/parseLinks.py
@@ -10,6 +10,10 @@ class fixLinks:
         lines=wikilinksfile.readlines()
         wikilinksfile.close()
         self.wikilinks=[l.strip() for l in lines]
+        for l in lines:
+          if l.count("_")!=0:
+            self.wikilinks.append(l.replace("_","").strip())      
+        
     def linkAlgorithms(self):
         for item in self.results:
           #check for |
@@ -43,7 +47,13 @@ class fixLinks:
           else:
               name=item
               link=item
-          if "".join((link[0].upper()+link[1:]).split("#")[0].split()) in self.wikilinks:
+          #split on #
+          linkPieces=link.split("#")
+          for i in range(len(linkPieces)):
+              linkPieces[i]=linkPieces[i].lstrip().rstrip().replace(' ','_')
+          linkPieces[0]=linkPieces[0][0].upper()+ linkPieces[0][1:] 
+          link="#".join(linkPieces)
+          if linkPieces[0] in self.wikilinks:
               formatted="<a href=\""+WEB_BASE+link+"\">"+name+"</a>"
               self.text=self.text.replace("[["+item+"]]",formatted)
 
@@ -70,14 +80,14 @@ class fixLinks:
               link=item   
           if link==":Category:Fit_functions":
               formatted="<a href=\"fitfunctions_index.html\">"+name+"</a>"  
-              self.text=self.text.replace("["+item+"]",formatted) 
+              self.text=self.text.replace("[["+item+"]]",formatted) 
           elif link==":Category:Algorithms":
               formatted="<a href=\"algorithms_index.html\">"+name+"</a>"  
-              self.text=self.text.replace("["+item+"]",formatted)   
+              self.text=self.text.replace("[["+item+"]]",formatted)   
           elif link.count("ategory:")==1:
               linkpiece=link.split("ategory:")[1].strip(":")
               formatted="<a href=\"AlgoCat_"+linkpiece+".html\">"+name+"</a>"  
-              self.text=self.text.replace("["+item+"]",formatted) 
+              self.text=self.text.replace("[["+item+"]]",formatted) 
                      
     def parse(self):
         self.linkAlgorithms()
