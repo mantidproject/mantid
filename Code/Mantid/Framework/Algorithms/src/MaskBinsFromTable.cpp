a900ed5ab9547c2ad7e55bb714bed5981f26ab6a
Wenduo Zhou <zhouw@ornl.gov>
2013-05-08 11:04:05 -0400

    Add wiki.  Refs #6994.

--- Code/Mantid/Framework/Algorithms/src/MaskBinsFromTable.cpp
+++ Code/Mantid/Framework/Algorithms/src/MaskBinsFromTable.cpp
@@ -1,3 +1,16 @@
+/*WIKI*
+
+Masks bins in a workspace.  The user specified masking parameters, including spectra, xmin and xmax are given via a TableWorkspace.
+
+It calls algorithm MaskBins underneath.
+
+==Related Algorithms==
+===MaskBins==
+[[MaskBins]] Masks bins in a workspace. Masked bins should properly be regarded as having been completely removed from the workspace. Bins falling within the range given (even partially) are masked, i.e. their data and error values are set to zero and the bin is added to the list of masked bins. This range is masked for all spectra in the workspace (though the workspace does not have to have common X values in all spectra).
+
+
+*WIKI*/
+
 #include "MantidAlgorithms/MaskBinsFromTable.h"
 #include "MantidKernel/System.h"
 #include "MantidAPI/WorkspaceProperty.h"
