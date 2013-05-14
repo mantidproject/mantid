a6f751f15cd790de4bcf6f4fc182c236085313c7
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-05-14 15:23:52 +0100

    Remove the creation of TableWorkspace
    I've discussed with Martyn this feature and hi suggested to remove it.
    
    re #6923

--- Code/Mantid/Framework/Algorithms/src/UpdateScriptRepository.cpp
+++ Code/Mantid/Framework/Algorithms/src/UpdateScriptRepository.cpp
@@ -5,10 +5,7 @@ and download all the files marked for AutoUpdate.
 
 #include "MantidAlgorithms/UpdateScriptRepository.h"
 #include "MantidAPI/ScriptRepositoryFactory.h"
-#include "MantidAPI/ITableWorkspace.h"
 #include "MantidAPI/ScriptRepository.h"
-#include "MantidAPI/TableRow.h"
-#include "MantidDataObjects/TableWorkspace.h"
 
 namespace Mantid
 {
@@ -50,7 +47,7 @@ namespace Algorithms
   void UpdateScriptRepository::initDocs()
   {
     this->setWikiSummary("Update the local instance of [[ScriptRepository]].");
-    this->setOptionalMessage("Update the local instance of [[ScriptRepository]].");
+    this->setOptionalMessage("Update the local instance of ScriptRepository.");
   }
 
   //----------------------------------------------------------------------------------------------
@@ -72,26 +69,19 @@ namespace Algorithms
     using Mantid::API::ScriptRepository;
     auto repo_ptr = ScriptRepositoryFactory::Instance().create("ScriptRepositoryImpl");
     
-    try{
-      std::vector<std::string> f_list = repo_ptr->check4Update();
-      if (f_list.size() > 0){
-        declareProperty(new API::WorkspaceProperty<API::ITableWorkspace>("UpdatedFileList","DownloadedFiles",Kernel::Direction::Output), "The list containing all the files whose new version was downloaded.");
-        auto m_list = API::WorkspaceFactory::Instance().createTable("TableWorkspace");
-        m_list->addColumn("str","file path");
-        std::stringstream info; 
-        info << "ScriptRepository Update Info:\n" 
-             << " The following files were updated:\n";
-        for(unsigned short i = 0; i< f_list.size();i++){
-          API::TableRow t = m_list->appendRow();
-          t << f_list[i];
-          info << "  * " << f_list[i] << "\n"; 
-        }
-        g_log.notice() << info.str() << std::endl; 
-        setProperty("UpdatedFileList", m_list);      
+    std::vector<std::string> f_list = repo_ptr->check4Update();
+    if (f_list.size() > 0){
+      std::stringstream info; 
+      info << "Information about ScriptRepository:\n" 
+           << " A more recent version of the following files was installed:\n"
+      for(unsigned short i = 0; i< f_list.size();i++){
+        info << "  * " << f_list[i] << "\n"; 
       }
-    }catch(API::ScriptRepoException & ex){
-      g_log.error() << ex.what() << std::endl; 
+      info << "Please check these files before using them. "
+           << "Note: These files were configured for AutoUpdate.";
+      g_log.warning() << info.str() << std::endl; 
     }
+
   }
 
 
