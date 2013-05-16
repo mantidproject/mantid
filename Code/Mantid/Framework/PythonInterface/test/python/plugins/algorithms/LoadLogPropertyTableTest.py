18e3aa2101894517cd9fd5bcac9c9216f11e1d34
Nick Draper <nick.draper@stfc.ac.uk>
2013-05-16 18:09:12 +0100

    re #6669 damn mixture of tabs and spaces

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/LoadLogPropertyTableTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/LoadLogPropertyTableTest.py
@@ -9,10 +9,10 @@ import os
 
 class LoadLogPropertyTableTest(unittest.TestCase):
 
-	def test_LoadValidFilesComments(self):
-        autoTestDir="../../../../../Test/AutoTestData/"
-		outputWorskapceName = "LoadLogPropertyTableTest_Test1"
-		
+    def test_LoadValidFilesComments(self):
+        autoTestDir = "../../../../../Test/AutoTestData/"
+        outputWorskapceName = "LoadLogPropertyTableTest_Test1"
+        
         alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = autoTestDir + "MUSR00015189.nxs", 
                 LastFile = autoTestDir + "MUSR00015194.nxs", LogNames="comment", OutputWorkspace = outputWorskapceName)
 
@@ -21,20 +21,20 @@ class LoadLogPropertyTableTest(unittest.TestCase):
         #Verify some values
         tablews = AnalysisDataService.retrieve(outputWorskapceName)
         self.assertEqual(6, tablews.rowCount())
-		self.assertEqual(2, tablews.columnCount())
-		
-		self.assertEqual("18.95MHz 100W", print output.cell(0,1))
-		self.assertEqual("15189", print output.cell(0,0))
-		self.assertEqual("15194", print output.cell(5,0))
+        self.assertEqual(2, tablews.columnCount())
+        
+        self.assertEqual("18.95MHz 100W", print output.cell(0,1))
+        self.assertEqual("15189", print output.cell(0,0))
+        self.assertEqual("15194", print output.cell(5,0))
 
-		run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
+        run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
         
         return
-		
-	def test_LoadPartiallyValidFilesLogValues(self):
+        
+    def test_LoadPartiallyValidFilesLogValues(self):
         autoTestDir="../../../../../Test/AutoTestData/"
-		outputWorskapceName = "LoadLogPropertyTableTest_Test2"
-		
+        outputWorskapceName = "LoadLogPropertyTableTest_Test2"
+        
         alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = autoTestDir + "argus0026287.nxs", 
                 LastFile = autoTestDir + "argus0026577.nxs", LogNames="temperature_1_log", OutputWorkspace = outputWorskapceName)
 
@@ -43,14 +43,14 @@ class LoadLogPropertyTableTest(unittest.TestCase):
         #Verify some values
         tablews = AnalysisDataService.retrieve(outputWorskapceName)
         self.assertEqual(2, tablews.rowCount())
-		self.assertEqual(2, tablews.columnCount())
-		
-		self.assertEqual("26287", print output.cell(0,0))
-		self.assertEqual("180", print output.cell(0,1))
-		self.assertEqual("26287", print output.cell(1,0))
-		self.assertEqual("7.32055", print output.cell(1,1))
+        self.assertEqual(2, tablews.columnCount())
+        
+        self.assertEqual("26287", print output.cell(0,0))
+        self.assertEqual("180", print output.cell(0,1))
+        self.assertEqual("26287", print output.cell(1,0))
+        self.assertEqual("7.32055", print output.cell(1,1))
 
-		run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
+        run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
         
         return
 
