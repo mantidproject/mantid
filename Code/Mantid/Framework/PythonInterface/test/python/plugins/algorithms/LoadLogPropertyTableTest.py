d637308b5ba2f28145d111f5f67e9ebe8e0d6412
Nick Draper <nick.draper@stfc.ac.uk>
2013-05-17 09:52:50 +0100

    re #6669 improve unit tests

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/LoadLogPropertyTableTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/LoadLogPropertyTableTest.py
@@ -8,13 +8,14 @@ from mantid.api import AnalysisDataService
 import os
 
 class LoadLogPropertyTableTest(unittest.TestCase):
-
+    _autotestDir=None
+    def setUp(self):
+        self._autotestDir="../../../Test/AutoTestData/"
     def test_LoadValidFilesComments(self):
-        autoTestDir = "../../../../../Test/AutoTestData/"
         outputWorskapceName = "LoadLogPropertyTableTest_Test1"
         
-        alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = autoTestDir + "MUSR00015189.nxs", 
-                LastFile = autoTestDir + "MUSR00015194.nxs", LogNames="comment", OutputWorkspace = outputWorskapceName)
+        alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = self._autotestDir + "MUSR00015189.nxs", 
+                LastFile = self._autotestDir + "MUSR00015194.nxs", LogNames="comment", OutputWorkspace = outputWorskapceName)
 
         self.assertTrue(alg_test.isExecuted())
 
@@ -23,20 +24,19 @@ class LoadLogPropertyTableTest(unittest.TestCase):
         self.assertEqual(6, tablews.rowCount())
         self.assertEqual(2, tablews.columnCount())
         
-        self.assertEqual("18.95MHz 100W", output.cell(0,1))
-        self.assertEqual("15189", output.cell(0,0))
-        self.assertEqual("15194", output.cell(5,0))
+        self.assertEqual("18.95MHz 100W", tablews.cell(0,1))
+        self.assertEqual(15189, tablews.cell(0,0))
+        self.assertEqual(15194, tablews.cell(5,0))
 
         run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
         
         return
         
     def test_LoadPartiallyValidFilesLogValues(self):
-        autoTestDir="../../../../../Test/AutoTestData/"
         outputWorskapceName = "LoadLogPropertyTableTest_Test2"
         
-        alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = autoTestDir + "argus0026287.nxs", 
-                LastFile = autoTestDir + "argus0026577.nxs", LogNames="temperature_1_log", OutputWorkspace = outputWorskapceName)
+        alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = self._autotestDir + "emu00006473.nxs", 
+                LastFile = self._autotestDir + "emu00006475.nxs", LogNames="Temp_Sample", OutputWorkspace = outputWorskapceName)
 
         self.assertTrue(alg_test.isExecuted())
 
@@ -45,12 +45,62 @@ class LoadLogPropertyTableTest(unittest.TestCase):
         self.assertEqual(2, tablews.rowCount())
         self.assertEqual(2, tablews.columnCount())
         
-        self.assertEqual("26287", output.cell(0,0))
-        self.assertEqual("180", output.cell(0,1))
-        self.assertEqual("26287", output.cell(1,0))
-        self.assertEqual("7.32055", output.cell(1,1))
+        self.assertEqual(6473, tablews.cell(0,0))
+        self.assertAlmostEqual(200.078, tablews.cell(0,1),3)
+        self.assertEqual(6475, tablews.cell(1,0))
+        self.assertAlmostEqual(283.523, tablews.cell(1,1),3)
 
         run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
+        
+        return
+        
+    def test_LoadValidFilesBlankLog(self):
+        outputWorskapceName = "LoadLogPropertyTableTest_Test3"
+        
+        alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = self._autotestDir + "MUSR00015189.nxs", 
+                LastFile = self._autotestDir + "MUSR00015194.nxs", OutputWorkspace = outputWorskapceName)
+
+        self.assertTrue(alg_test.isExecuted())
+
+        #Verify some values
+        tablews = AnalysisDataService.retrieve(outputWorskapceName)
+        self.assertEqual(6, tablews.rowCount())
+        self.assertEqual(1, tablews.columnCount())
+        
+        self.assertEqual(15189, tablews.cell(0,0))
+        self.assertEqual(15194, tablews.cell(5,0))
+
+        run_algorithm("DeleteWorkspace", Workspace = outputWorskapceName)
+        
+        return
+        
+    def test_LoadInValidValues(self):
+        outputWorskapceName = "LoadLogPropertyTableTest_Test4"
+        
+        #invalid log name
+        alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = self._autotestDir + "emu00006473.nxs", 
+                LastFile = self._autotestDir + "emu00006475.nxs", LogNames="WrongTemp", OutputWorkspace = outputWorskapceName)
+        self.assertFalse(alg_test.isExecuted())
+        
+        #invalid first file
+        ExecptionThrownOnBadFileParameter = False
+        try:
+            alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = self._autotestDir + "emu0000000.nxs", 
+                LastFile = self._autotestDir + "emu00006475.nxs", LogNames="Temp_Sample", OutputWorkspace = outputWorskapceName)
+            self.assertFalse(alg_test.isExecuted())
+        except:
+            ExecptionThrownOnBadFileParameter = True
+        self.assertEqual(True,ExecptionThrownOnBadFileParameter)
+        
+        #invalid last file
+        ExecptionThrownOnBadFileParameter = False
+        try:
+            alg_test = run_algorithm("LoadLogPropertyTable", FirstFile = self._autotestDir + "emu00006473.nxs", 
+                LastFile = self._autotestDir + "emu9999999.nxs", LogNames="Temp_Sample", OutputWorkspace = outputWorskapceName)
+            self.assertFalse(alg_test.isExecuted())
+        except:
+            ExecptionThrownOnBadFileParameter = True
+        self.assertEqual(True,ExecptionThrownOnBadFileParameter)
         
         return
 
