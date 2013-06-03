abfd65d01ab6fbfaec038fac4c3e72a9c84ce697
Andrei Savici <saviciat@ornl.gov>
2013-06-03 15:40:15 -0400

    Add to wiki description. Refs #2710

--- Code/Mantid/Framework/Algorithms/src/IntegrateByComponent.cpp
+++ Code/Mantid/Framework/Algorithms/src/IntegrateByComponent.cpp
@@ -2,6 +2,8 @@
 The algorithm integrates up the instrument hierarchy, and each pixel will contain the average value for the component. For example,
 assuming that for a particular instrument on workspace w1 a "tube" is made out of "pixels", w=IntegrateByComponent(w1,1) will integrate values of w1,
 calculate the average along the tube (LevelsUp=1) (for non-masked pixels), and replace the value of each spectrum in a tube with the average value for that tube.
+
+Note that if the detectors are grouped before, this algorithm won't run except with LevelsUp=0 (integrate over all detectors).
 *WIKI*/
 
 #include "MantidAlgorithms/IntegrateByComponent.h"
