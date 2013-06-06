39ff44181b83c92be4046784341383037ec5beee
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-06-06 15:31:14 +0100

    refs #7252. Read metadata into md histoworkspace.

--- Code/Mantid/Vates/VatesAPI/src/LoadVTK.cpp
+++ Code/Mantid/Vates/VatesAPI/src/LoadVTK.cpp
@@ -1,4 +1,14 @@
 #include "MantidVatesAPI/LoadVTK.h"
+#include "MantidAPI/FileProperty.h"
+#include "MantidMDEvents/MDHistoWorkspace.h"
+#include <boost/make_shared.hpp>
+#include <vtkStructuredPointsReader.h>
+#include <vtkStructuredPoints.h>
+
+
+using namespace Mantid::API;
+using namespace Mantid::MDEvents;
+using namespace Mantid::Geometry;
 
 namespace Mantid
 {
@@ -23,12 +33,40 @@ namespace Mantid
 
      void LoadVTK::init()
      {
-       //TODO
+       std::vector<std::string> exts;
+       exts.push_back("vtk");
+       this->declareProperty(new FileProperty("Filename", "", FileProperty::Load, exts), "VTK file to load");
+       declareProperty(new WorkspaceProperty<IMDHistoWorkspace>("OutputWorkspace","", Mantid::Kernel::Direction::Output), "MDHistoWorkspace equivalent of vtkRectilinearInput.");
      }
 
      void LoadVTK::exec()
      {
-       //TODO
+       std::string filename = getProperty("Filename");
+
+       auto reader = vtkStructuredPointsReader::New();
+       reader->SetFileName(filename.c_str());
+       reader->Update();
+       vtkStructuredPoints* output = reader->GetOutput();
+
+       int nPoints = output->GetNumberOfPoints();
+       int nCellx = output->GetNumberOfCells();
+
+       int dimensions[3];
+       output->GetDimensions(dimensions);
+       double bounds[6];
+       output->ComputeBounds();
+       output->GetBounds(bounds);
+
+       auto dimX = boost::make_shared<MDHistoDimension>("X", "X", "", bounds[0], bounds[1], dimensions[0]);
+       auto dimY = boost::make_shared<MDHistoDimension>("Y", "Y", "", bounds[2], bounds[3], dimensions[1]);
+       auto dimZ = boost::make_shared<MDHistoDimension>("Z", "Z", "", bounds[4], bounds[5], dimensions[2]);
+       
+       MDHistoWorkspace_sptr outputWS = boost::make_shared<MDHistoWorkspace>(dimX, dimY, dimZ);
+
+       this->setProperty("OutputWorkspace", outputWS);
+       
+       output->Delete();
+       return;
      }
   }
 }
--- Code/Mantid/Vates/VatesAPI/test/LoadVTKTest.h
+++ Code/Mantid/Vates/VatesAPI/test/LoadVTKTest.h
@@ -3,10 +3,13 @@
 
 #include <cxxtest/TestSuite.h>
 #include "MantidVatesAPI/LoadVTK.h"
+#include "MantidAPI/IMDHistoWorkspace.h"
+#include "MantidGeometry/MDGeometry/IMDDimension.h"
 
 
 using namespace Mantid::API;
 using namespace Mantid::VATES;
+using namespace Mantid::Geometry;
 
 class LoadVTKTest: public CxxTest::TestSuite
 {
@@ -23,6 +26,43 @@ public:
   {
     LoadVTK loadVTK;
     TS_ASSERT_EQUALS(1, loadVTK.version());
+  }
+
+  void test_Properties()
+  {
+    LoadVTK loadVTK;
+    loadVTK.initialize();
+    TS_ASSERT_THROWS_NOTHING(loadVTK.setPropertyValue("Filename", "iron_protein.vtk"));
+    TS_ASSERT_THROWS_NOTHING(loadVTK.setPropertyValue("OutputWorkspace", "OutWS"));
+    TS_ASSERT( loadVTK.isInitialized() )
+  }
+
+  void do_check_dimension(IMDDimension_const_sptr dimension, const std::string& expectedName, const double expectedMin, const double expectedMax, const int expectedNBins)
+  {
+    TSM_ASSERT_EQUALS("Name is wrong.", dimension->getName(), expectedName);
+    TSM_ASSERT_EQUALS("Id is wrong.", dimension->getDimensionId(), expectedName);
+    TSM_ASSERT_DELTA("Minimum is wrong.", dimension->getMinimum(), expectedMin, 0.01);
+    TSM_ASSERT_DELTA("Maximum is wrong.", dimension->getMaximum(), expectedMax, 0.01);
+    TSM_ASSERT_EQUALS("Number of bins is wrong.", dimension->getNBins(), expectedNBins);
+  }
+
+  void test_LoadVTKFile()
+  {
+    const std::string outWSName = "OutWS";
+
+    LoadVTK loadVTK;
+    loadVTK.setRethrows(true);
+    loadVTK.initialize();
+    loadVTK.setPropertyValue("Filename", "iron_protein.vtk");
+    loadVTK.setPropertyValue("OutputWorkspace", outWSName);
+    loadVTK.execute();
+
+    IMDHistoWorkspace_sptr outWS = AnalysisDataService::Instance().retrieveWS<IMDHistoWorkspace>(outWSName);
+
+    TS_ASSERT_EQUALS(3, outWS->getNumDims());
+    do_check_dimension(outWS->getDimension(0), "X", 0, 67, 68); // These numbers are expected min, max, and nbins known from the input file for dim x.
+    do_check_dimension(outWS->getDimension(1), "Y", 0, 67, 68); // These numbers are expected min, max, and nbins known from the input file for dim y.
+    do_check_dimension(outWS->getDimension(2), "Z", 0, 67, 68); // These numbers are expected min, max, and nbins known from the input file for dim z.
   }
 
 };
