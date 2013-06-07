ef0855b456675bf1c5ce53019c4ab0d2dc42fc7f
Mathieu Doucet <doucetm@ornl.gov>
2013-06-07 11:40:27 -0400

    Re #7240 Fix import issue and API v1 code

--- Code/Mantid/scripts/Interface/reduction_gui/widgets/reflectometer/refm_reduction.py
+++ Code/Mantid/scripts/Interface/reduction_gui/widgets/reflectometer/refm_reduction.py
@@ -14,6 +14,7 @@ import ui.reflectometer.ui_refm_reduction
 IS_IN_MANTIDPLOT = False
 try:
     import mantidplot
+    from mantid.api import *
     #TODO: this will need to change once we get rid of the old python API
     from reduction.instruments.reflectometer import data_manipulation
     IS_IN_MANTIDPLOT = True
@@ -909,40 +910,40 @@ class DataReflWidget(BaseWidget):
         ws_base = "__%s" % basename
     
         ws_output_base_ff = "Counts vs X pixel - " + basename + " Off_Off" 
-        if mtd.workspaceExists(ws_output_base_ff):
-            mtd.deleteWorkspace(ws_output_base_ff)
+        if AnalysisDataService.doesExist(ws_output_base_ff):
+            AnalysisDataService.remove(ws_output_base_ff)
             ws_output_base_1 = ws_output_base_ff + "_2D"
-            mtd.deleteWorkspace(ws_output_base_1)            
+            AnalysisDataService.remove(ws_output_base_1)            
             ws_output_base_2 = ws_base + " - Off_Off"
-            mtd.deleteWorkspace(ws_output_base_2)
+            AnalysisDataService.remove(ws_output_base_2)
         
         ws_output_base_nf = "Counts vs X pixel - " + basename + " On_Off" 
-        if mtd.workspaceExists(ws_output_base_nf):
-            mtd.deleteWorkspace(ws_output_base_nf)
+        if AnalysisDataService.doesExist(ws_output_base_nf):
+            AnalysisDataService.remove(ws_output_base_nf)
             ws_output_base_1 = ws_output_base_nf + "_2D"
-            mtd.deleteWorkspace(ws_output_base_1)            
+            AnalysisDataService.remove(ws_output_base_1)            
             ws_output_base_2 = ws_base + " - On_Off"
-            mtd.deleteWorkspace(ws_output_base_2)
+            AnalysisDataService.remove(ws_output_base_2)
         
         ws_output_base_fn = "Counts vs X pixel - " + basename + " Off_On" 
-        if mtd.workspaceExists(ws_output_base_fn):
-            mtd.deleteWorkspace(ws_output_base_fn)
+        if AnalysisDataService.doesExist(ws_output_base_fn):
+            AnalysisDataService.remove(ws_output_base_fn)
             ws_output_base_1 = ws_output_base_fn + "_2D"
-            mtd.deleteWorkspace(ws_output_base_1)            
+            AnalysisDataService.remove(ws_output_base_1)            
             ws_output_base_2 = ws_base + " - Off_On"
-            mtd.deleteWorkspace(ws_output_base_2)
+            AnalysisDataService.remove(ws_output_base_2)
 
         ws_output_base_nn = "Counts vs X pixel - " + basename + " On_On" 
-        if mtd.workspaceExists(ws_output_base_nn):
-            mtd.deleteWorkspace(ws_output_base_nn)
+        if AnalysisDataService.doesExist(ws_output_base_nn):
+            AnalysisDataService.remove(ws_output_base_nn)
             ws_output_base_1 = ws_output_base_nn + "_2D"
-            mtd.deleteWorkspace(ws_output_base_1)            
+            AnalysisDataService.remove(ws_output_base_1)            
             ws_output_base_2 = ws_base + " - On_On"
-            mtd.deleteWorkspace(ws_output_base_2)
+            AnalysisDataService.remove(ws_output_base_2)
 
         ws_output_base_all = ws_base + '_all' 
-        if mtd.workspaceExists(ws_output_base_all):
-            mtd.deleteWorkspace(ws_output_base_all)
+        if AnalysisDataService.doesExist(ws_output_base_all):
+            AnalysisDataService.remove(ws_output_base_all)
 
         data_manipulation.counts_vs_pixel_distribution(file_path, 
                                                        is_pixel_y=False, 
