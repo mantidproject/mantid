3d07ec2d6dcf7773ebbac4c427c1189d005c73ef
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-06-18 15:12:45 +0100

    refs #7310. Fix test.

--- Code/Mantid/MantidQt/SliceViewer/test/PeakTransformQLabTest.h
+++ Code/Mantid/MantidQt/SliceViewer/test/PeakTransformQLabTest.h
@@ -201,7 +201,7 @@ void test_getFriendlyName()
 {
   PeakTransformQLab transform;
   TS_ASSERT_EQUALS(PeakTransformQLab::name(), transform.getFriendlyName());
-  TS_ASSERT_EQUALS("QLab", transform.getFriendlyName());
+  TS_ASSERT_EQUALS("Q (lab frame)", transform.getFriendlyName());
 }
 
 void test_getCoordinateSystem()
