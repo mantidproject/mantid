94aa5a5ec426839bec8d92dbf925d9f0e7e16192
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-06-21 15:40:46 +0100

    Improve wiki re #7338
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/Framework/Algorithms/src/RenameWorkspaces.cpp
+++ Code/Mantid/Framework/Algorithms/src/RenameWorkspaces.cpp
@@ -1,10 +1,14 @@
 /*WIKI* 
 
-Renames a list of workspaces to a different name in the data service.
+Renames a list of workspaces in the data service.
 This renaming is done by either replacing with new names in a list or adding a prefix, suffix or both prefix and suffix.
 The Renaming is implemented by calling RenameWorkspace as a child algorithm having defined the output workspace appropriately.
 
-If run on a group workspace, the members of the group will be renamed in the same manner as done by RemameWorkspace
+If run on a group workspace, the members of the group will be renamed in the same manner as done by RemameWorkspace.
+
+The new names can be either explicitly defined by a comma separated list or by adding a prefix, suffix or both a prefix and suffix.
+
+'''Warning:''' No new name can be the same as any existing workspace, even if that existing workspace is also being renamed. Duplicate names may cause the loss of a workspace.
 
 *WIKI*/
 //----------------------------------------------------------------------
