2d09303e0f2de456d184bcf2fee66f941645fbd7
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-07-03 15:32:11 +0100

    Tidy wiki description format re #7365
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/Framework/DataHandling/src/LoadInstrumentFromNexus.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadInstrumentFromNexus.cpp
@@ -1,11 +1,12 @@
 /*WIKI* 
-    Attempts to load information about the instrument from a ISIS NeXus file. In particular attempt to
-    read L2 and 2-theta detector position values and add detectors which are positioned relative
-    to the sample in spherical coordinates as (r,theta,phi)=(L2,2-theta,0.0). Also adds dummy source
-    and samplepos components to instrument.
+Attempts to load information about the instrument from a ISIS NeXus file. In particular attempt to
+read L2 and 2-theta detector position values and add detectors which are positioned relative
+to the sample in spherical coordinates as (r,theta,phi)=(L2,2-theta,0.0). Also adds dummy source
+and samplepos components to instrument.
 
-    LoadInstrumentFromNexus is intended to be used as a child algorithm of
-    other Loadxxx algorithms, rather than being used directly.
+LoadInstrumentFromNexus is intended to be used as a child algorithm of
+other Loadxxx algorithms, rather than being used directly.
+It is used by LoadMuonNexus version 1.
 *WIKI*/
 //----------------------------------------------------------------------
 // Includes
