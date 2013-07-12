ff4dabf8e285258cf39ffbd25a39038480ed7f36
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-07-12 12:10:47 +0100

    Simple translation of the previous example to the new interface
    re #7387

--- Code/Mantid/scripts/Calibration/Examples/TubeCalibDemoMaps_C4C3C2.py
+++ Code/Mantid/scripts/Calibration/Examples/TubeCalibDemoMaps_C4C3C2.py
@@ -4,32 +4,21 @@
 # Here we run the calibration of a selected part of MAPS consisting of several components
 # specifying them in an array of strings. 
 
-#
-from mantid.api import WorkspaceFactory  # For table worskspace of calibrations
-from tube_calib_fit_params import * # To handle fit parameters
-from ideal_tube import * # For ideal tube
-from tube_calib import *  # For tube calibration functions
-from tube_spec import * # For tube specification class
-
+import tube
 
 # == Set parameters for calibration ==
 
-path = r"C:/Temp/" # Path name of folder containing input and output files
-filename = 'MAPS14919.raw' # Name of calibration run
+filename = 'MAP14919.raw' # Name of calibration run
 rangeLower = 2000 # Integrate counts in each spectra from rangeLower to rangeUpper 
 rangeUpper = 10000 #
 
-
-# Set initial parameters for peak finding
-ExpectedHeight = -1000.0 # Expected Height of Peaks (initial value of fit parameter)
-ExpectedWidth = 8.0 # Expected width of centre peak (initial value of fit parameter)
-ExpectedPositions = [4.0, 85.0, 128.0, 161.0, 252.0] # Expected positions of the edges and peak (initial values of fit parameters)
-
+# Set what we want to calibrate (e.g whole intrument or one door )
 # Set what we want to calibrate (e.g whole intrument or one door )
 CalibratedComponents = ['C4_window','C3_window','C2_window']  # Calibrate three C windows
+ 
     
 # Get calibration raw file and integrate it    
-rawCalibInstWS = Load(path+filename)  #'raw' in 'rawCalibInstWS' means unintegrated.
+rawCalibInstWS = Load(filename)  #'raw' in 'rawCalibInstWS' means unintegrated.
 print "Integrating Workspace"
 CalibInstWS = Integration( rawCalibInstWS, RangeLower=rangeLower, RangeUpper=rangeUpper )
 DeleteWorkspace(rawCalibInstWS)
@@ -37,27 +26,16 @@ print "Created workspace (CalibInstWS) with integrated data from run and instrum
 
 # == Create Objects needed for calibration ==
 
-#Create Calibration Table
-calibrationTable = CreateEmptyTableWorkspace(OutputWorkspace="CalibTable")
-calibrationTable.addColumn(type="int",name="Detector ID")  # "Detector ID" column required by ApplyCalbration
-calibrationTable.addColumn(type="V3D",name="Detector Position")  # "Detector Position" column required by ApplyCalbration
-
-# Specify components to calibrate
-thisTubeSet = TubeSpec(CalibInstWS)
-thisTubeSet.setTubeSpecByStringArray(CalibratedComponents)
-
-# Get ideal tube
-iTube = IdealTube()
-iTube.setPositionsAndForm([-0.50,-0.16,-0.00, 0.16, 0.50 ],[2,1,1,1,2])
-
-# Get fitting parameters
-fitPar = TubeCalibFitParams( ExpectedPositions, ExpectedHeight, ExpectedWidth )
+# Specify the known positions
+knownPos = [-0.50,-0.16,-0.00, 0.16, 0.50 ]
+funcForm = [2,1,1,1,2]
 
 print "Created objects needed for calibration."
 
 # == Get the calibration and put results into calibration table ==
-# also put peaks into PeakFile
-getCalibration( CalibInstWS, thisTubeSet, calibrationTable,  fitPar, iTube, PeakFile=path+'TubeDemoMaps01.txt' )
+
+calibrationTable, peakTable = tube.calibrate(CalibInstWS, CalibratedComponents, knownPos, funcForm, 
+	outputPeak=True, margin=15)
 print "Got calibration (new positions of detectors) "
 
 # == Apply the Calibation ==
@@ -66,6 +44,7 @@ print "Applied calibration"
 
 
 # == Save workspace ==
-SaveNexusProcessed( CalibInstWS, path+'TubeCalibDemoMapsResult.nxs',"Result of Running TCDemoMaps.py")
+#SaveNexusProcessed( CalibInstWS, path+'TubeCalibDemoMapsResult.nxs',"Result of Running TCDemoMaps.py")
 print "saved calibrated workspace (CalibInstWS) into Nexus file TubeCalibDemoMapsResult.nxs"
+
 
