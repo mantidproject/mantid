55a8cfa25a1368b05d28f85f29b6abf683748e29
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-07-22 13:56:11 +0100

    Add all testing but contents of output file re #6986
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/Framework/DataHandling/test/ModifyDetectorDotDatFileTest.h
+++ Code/Mantid/Framework/DataHandling/test/ModifyDetectorDotDatFileTest.h
@@ -8,11 +8,13 @@
 #include <iomanip>
 
 #include "MantidDataHandling/ModifyDetectorDotDatFile.h"
+#include "MantidDataHandling/LoadEmptyInstrument.h"
 #include <Poco/File.h>
 
 using namespace Mantid;
 using namespace Mantid::DataHandling;
 using namespace Mantid::API;
+
 
 class ModifyDetectorDotDatFileTest : public CxxTest::TestSuite
 {
@@ -21,6 +23,20 @@ public:
   // This means the constructor isn't called when running other tests
   static ModifyDetectorDotDatFileTest *createSuite() { return new ModifyDetectorDotDatFileTest(); }
   static void destroySuite( ModifyDetectorDotDatFileTest *suite ) { delete suite; }
+
+  
+  // Helper to set up a simple workspace for testing
+  void makeTestWorkspace(const std::string & ads_name)
+  {
+    IAlgorithm* loader;
+    loader = new Mantid::DataHandling::LoadEmptyInstrument;
+    loader->initialize();
+    TS_ASSERT_THROWS_NOTHING(loader->setPropertyValue("Filename", "IDFs_for_UNIT_TESTING/MAPS_Definition_Reduced.xml"));
+    loader->setPropertyValue("OutputWorkspace", ads_name);
+    TS_ASSERT_THROWS_NOTHING( loader->execute() );
+    TS_ASSERT( loader->isExecuted() );
+    delete loader;
+  }
 
 
   void test_Init()
@@ -37,21 +53,25 @@ public:
     TS_ASSERT( alg.isInitialized() )
 
     // Create input workspace
-
+    std::string wsName = "ModifyDetectorDotDatFileTestWorkspace";
+    makeTestWorkspace(wsName);
 
     // Test Properties
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("InputWorkspace",wsName) );
     TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("InputFilename", "detector_few_maps.dat") );
     TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputFilename", "detector_few_maps_result.dat") );
 
-    //TS_ASSERT_THROWS_NOTHING( alg.setProperty("ChunkSize", 10) );
-    //TS_ASSERT_THROWS_NOTHING( alg.setProperty("NumChunks", 20) );
-    //TS_ASSERT_THROWS_NOTHING( alg.execute(); );
-   // TS_ASSERT( alg.isExecuted() );
+    // Test execution
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+
+    // Check output file
+    std::string fullFilename = alg.getPropertyValue("OutputFilename"); //Get absolute path
+    // has the algorithm written the output file to disk?
+    TS_ASSERT( Poco::File(fullFilename).exists() );
     
-   // Poco::File file(fullFile);
-   // TS_ASSERT( file.exists() );
-   // if (file.exists())
-  //    file.remove();
+    // Remove output file
+    Poco::File(fullFilename).remove();
   }
   
 
