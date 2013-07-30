8c05210fb4abba8ab003de7ab8109d423c94c852
Wenduo Zhou <zhouw@ornl.gov>
2013-07-30 13:52:43 -0400

    Cleaned the code.  Refs #6863.

--- Code/Mantid/Framework/CurveFitting/src/LeBailFunction.cpp
+++ Code/Mantid/Framework/CurveFitting/src/LeBailFunction.cpp
@@ -215,6 +215,11 @@ namespace CurveFitting
       if (!isvalid)
       {
         arevalid = false;
+
+        int h, k, l;
+        peak->getMillerIndex(h, k, l);
+        g_log.information() << "Peak [" << h << ", " << k << ", " << l << "] @ TOF = " << peak->centre()
+                            << " has unphysical parameters. " << ".\n";
         break;
       }
     }
@@ -605,8 +610,8 @@ namespace CurveFitting
       else if (intensity < 0.0)
       {
         // No negative intensity
-        g_log.information() << "[Wx134] Set peak @ " << peak->centre() << "'s intensity to 0.0 instead of "
-                            << intensity << ".\n";
+        g_log.debug() << "[Fx134] Set peak @ " << peak->centre() << "'s intensity to 0.0 instead of "
+                      << intensity << ".\n";
         intensity = 0.0;
       }
       g_log.debug() << "[Fx407] Peak @ " << peak->centre() << ": Set Intensity = " << intensity << "\n";
@@ -729,9 +734,14 @@ namespace CurveFitting
         for (size_t ipk = 0; ipk < m_numPeaks; ++ipk)
         {
           IPowderDiffPeakFunction_sptr peak = m_vecPeaks[ipk];
-          peak->setParameter(i, newvalue);
+          peak->setParameter(i, newvalue);          
         }
       } // If parameter name is a profile parameter
+      else
+      {
+        g_log.debug() << "Parameter " << parname << " is not a profile parameter. Length of string = "
+                      << parname.size() << "\n";
+      }
     } // ENDFOR [All profile parameter]
 
     // Set the flag to indicate that client has input parameters
@@ -1011,228 +1021,9 @@ namespace CurveFitting
     return;
   }
 
-  /*
-   * Calculate peak parameters for a peak at d (d-spacing value)
-   * Output will be written to parameter map too.
-
-  void LeBailFunction::calPeakParametersForD(double dh, double& alpha, double& beta, double &Tof_h,
-      double &sigma_g2, double &gamma_l, std::map<std::string, double>& parmap) const
-  {
-    // 1. Get some parameters
-    double wcross = getParameter("Width");
-    double Tcross = getParameter("Tcross");
-
-    // 2. Start to calculate alpha, beta, sigma2, gamma,
-    double n = 0.5*gsl_sf_erfc(wcross*(Tcross-1/dh));
-
-    double alpha_e = Alph0 + Alph1*dh;
-    double alpha_t = Alph0t - Alph1t/dh;
-    alpha = 1/(n*alpha_e + (1-n)*alpha_t);
-
-    double beta_e = Beta0 + Beta1*dh;
-    double beta_t = Beta0t - Beta1t/dh;
-    beta = 1/(n*beta_e + (1-n)*beta_t);
-
-    double Th_e = Zero + Dtt1*dh;
-    double Th_t = Zerot + Dtt1t*dh - Dtt2t/dh;
-    Tof_h = n*Th_e + (1-n)*Th_t;
-
-    sigma_g2 = Sig0 + Sig1*std::pow(dh, 2) + Sig2*std::pow(dh, 4);
-    gamma_l = Gam0 + Gam1*dh + Gam2*std::pow(dh, 2);
-
-    // 3. Add to parameter map
-    parmap.insert(std::make_pair("Alpha", alpha));
-    parmap.insert(std::make_pair("Beta", beta));
-    parmap.insert(std::make_pair("Sigma2", sigma_g2));
-    parmap.insert(std::make_pair("Gamma", gamma_l));
-    parmap.insert(std::make_pair("TOF_h", Tof_h));
-
-    g_log.debug() << "DB1214 D = " << dh << ", TOF = " << Tof_h << std::endl;
-
-    return;
-  }
-     */
-
-  /*
-   * Calculate all peaks' parameters
-  void LeBailFunction::calPeaksParameters()
-  {
-    // 1. Get parameters (class)
-    Alph0 = getParameter("Alph0");
-    Alph1 = getParameter("Alph1");
-    Beta0 = getParameter("Beta0");
-    Beta1 = getParameter("Beta1");
-    Alph0t = getParameter("Alph0t");
-    Alph1t = getParameter("Alph1t");
-    Beta0t = getParameter("Beta0t");
-    Beta1t = getParameter("Beta1t");
-    Dtt1 = getParameter("Dtt1");
-    Dtt1t = getParameter("Dtt1t");
-    Dtt2t = getParameter("Dtt2t");
-    Zero = getParameter("Zero");
-    Zerot = getParameter("Zerot");
-    Sig0 = getParameter("Sig0");
-    Sig1 = getParameter("Sig1");
-    Sig2 = getParameter("Sig2");
-    Gam0 = getParameter("Gam0");
-    Gam1 = getParameter("Gam1");
-    Gam2 = getParameter("Gam2");
-
-    // 2. Calcualte peak parameters for all peaks
-    for (size_t id = 0; id < dvalues.size(); ++id)
-    {
-      double dh = dvalues[id];
-      // a) Calculate all the parameters
-      double alpha, beta, tof_h, sigma2, gamma;
-      calPeakParametersForD(dh, alpha, beta, tof_h, sigma2, gamma, mPeakParameters[id]);
-
-      // b) Set peak parameters
-      m_peakVec[id]->setParameter("TOF_h", tof_h);
-      m_peakVec[id]->setParameter("Height", heights[id]);
-      m_peakVec[id]->setParameter("Alpha", alpha);
-      m_peakVec[id]->setParameter("Beta", beta);
-      m_peakVec[id]->setParameter("Sigma2", sigma2);
-      m_peakVec[id]->setParameter("Gamma", gamma);
-    }
-
-    return;
-  }
-     */
-
-  /*
-  void LeBailFunction::function1D(double *out, const double *xValues, size_t nData) const
-  {
-    // 1. Get parameters (class)
-    Alph0 = getParameter("Alph0");
-    Alph1 = getParameter("Alph1");
-    Beta0 = getParameter("Beta0");
-    Beta1 = getParameter("Beta1");
-    Alph0t = getParameter("Alph0t");
-    Alph1t = getParameter("Alph1t");
-    Beta0t = getParameter("Beta0t");
-    Beta1t = getParameter("Beta1t");
-    Dtt1 = getParameter("Dtt1");
-    Dtt1t = getParameter("Dtt1t");
-    Dtt2t = getParameter("Dtt2t");
-    Zero = getParameter("Zero");
-    Zerot = getParameter("Zerot");
-    Sig0 = getParameter("Sig0");
-    Sig1 = getParameter("Sig1");
-    Sig2 = getParameter("Sig2");
-    Gam0 = getParameter("Gam0");
-    Gam1 = getParameter("Gam1");
-    Gam2 = getParameter("Gam2");
-    double latticeconstant = getParameter("LatticeConstant");
-
-
-    std::cout << " \n-------------------------  being visited -----------------------\n" << std::endl;
-    std::cout << "Alph0  = " << Alph0 << std::endl;
-    std::cout << "Alph1  = " << Alph1 << std::endl;
-    std::cout << "Alph0t = " << Alph0t << std::endl;
-    std::cout << "Alph1t = " << Alph1t << std::endl;
-    std::cout << "Zero   = " << Zero << std::endl;
-    std::cout << "Zerot  = " << Zerot << std::endl;
-    std::cout << "Lattice= " << latticeconstant << " Number of Peaks = " << mPeakHKLs.size() << std::endl;
-
-
-    // 2.
-    double *tempout = new double[nData];
-    for (size_t iy = 0; iy < nData; ++iy)
-    {
-      out[iy] = 0.0;
-    }
-
-    for (size_t id = 0; id < m_peakHKLVec.size(); ++id)
-    {
-      int h = m_peakHKLVec[id][0];
-      int k = m_peakHKLVec[id][1];
-      int l = m_peakHKLVec[id][2];
-      double dh = calCubicDSpace(latticeconstant, h, k, l);
-      dvalues[id] = dh;
-
-      // a) Calculate all the parameters
-      double alpha, beta, tof_h, sigma2, gamma;
-      calPeakParametersForD(dh, alpha, beta, tof_h, sigma2, gamma, mPeakParameters[id]);
-
-      // b) Set peak parameters
-      g_log.debug() << "DB546 Peak @ d = " << dh << " Set Height = " << dh << std::endl;
-      m_peakVec[id]->setParameter("TOF_h", tof_h);
-      m_peakVec[id]->setParameter("Height", heights[id]);
-      m_peakVec[id]->setParameter("Alpha", alpha);
-      m_peakVec[id]->setParameter("Beta", beta);
-      m_peakVec[id]->setParameter("Sigma2", sigma2);
-      m_peakVec[id]->setParameter("Gamma", gamma);
-
-      // c) Calculate individual peak range
-      m_peakVec[id]->setPeakRadius(PEAKRADIUS);
-
-      // d) Calculate peak
-      m_peakVec[id]->function1D(tempout, xValues, nData);
-      for (size_t iy = 0; iy < nData; ++iy)
-      {
-        out[iy] += tempout[iy];
-      }
-    } // END-FOR D-values
-
-    for (size_t n = 0; n < nData; ++n)
-      g_log.debug() << "DB327 " << xValues[n] << "\t\t" << out[n] << std::endl;
-
-    // 3. Clean
-    delete[] tempout;
-
-    return;
-  }
-  */
-
-  /*
-   * Using numerical derivative
-
-  void LeBailFunction::functionDeriv(const API::FunctionDomain &domain, API::Jacobian &jacobian)
-  {
-    calNumericalDeriv(domain, jacobian);
-    return;
-  }
-   */
-
-  /*
-   * Analytical
-
-  void LeBailFunction::functionDeriv1D(API::Jacobian *out, const double* xValues, const size_t nData)
-  {
-    UNUSED_ARG(out);
-    UNUSED_ARG(xValues);
-    UNUSED_ARG(nData);
-
-    throw std::runtime_error("LeBailFunction does not support analytical derivative. ");
-  }
-    */
-
-  /** Add a peak with its d-value
-
-  void LeBailFunction::addPeak(double dh, double height)
-  {
-    dvalues.push_back(dh);
-    heights.push_back(height);
-
-    // API::IPeakFunction* tpeak = new CurveFitting::Bk2BkExpConvPV();
-    CurveFitting::Bk2BkExpConvPV* peakptr = new CurveFitting::Bk2BkExpConvPV();
-    CurveFitting::Bk2BkExpConvPV_sptr tpeak(peakptr);
-    tpeak->setPeakRadius(8);
-
-    tpeak->initialize();
-    m_peakVec.push_back(tpeak);
-
-    std::map<std::string, double> parmap;
-    mPeakParameters.push_back(parmap);
-
-    return;
-  }
-     */
-
-
-  /*
-   * Reset all peaks' height
-       * @param peakheights :: list of peak heights corresponding to each peak
+  //----------------------------------------------------------------------------------------------
+  /** Reset all peaks' height
+    * @param peakheights :: list of peak heights corresponding to each peak
    */
   void LeBailFunction::setPeakHeights(std::vector<double> inheights)
   {
