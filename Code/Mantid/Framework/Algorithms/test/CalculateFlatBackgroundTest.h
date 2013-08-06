7c57db5edd78ee4f7353d1a5336f3c0574cad3d9
Keith Brown <keith.brown@stfc.ac.uk>
2013-08-06 16:32:58 +0100

    Added WithReturnBackground tests and refactored tests in general
    testExec and testMeanFirst now have WithReturnBackground versions.
    
    Code has been refactored so that the algorithm executing is now in a separate method that sets the properties accordingly for that test.
    
    A lot of TS_ASSERT_EQUALS tests have been chaged to TS_ASSERT_DELTA tests.
    
    Refs #2057

--- Code/Mantid/Framework/Algorithms/test/CalculateFlatBackgroundTest.h
+++ Code/Mantid/Framework/Algorithms/test/CalculateFlatBackgroundTest.h
@@ -71,26 +71,16 @@ public:
     TS_ASSERT_EQUALS( flatBG.version(), 1 )
   }
 
+
+
   void testExec()
   {
-    Mantid::Algorithms::CalculateFlatBackground flatBG;
-    TS_ASSERT_THROWS_NOTHING( flatBG.initialize() )
-    TS_ASSERT( flatBG.isInitialized() )
-    
-    TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("InputWorkspace","calcFlatBG") )
-    TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("OutputWorkspace","Removed") )
-    TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("WorkspaceIndexList","0") )
-    TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("StartX","9.5") )
-    TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("EndX","20.5") )
-    TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("Mode","Linear Fit") )
+    runCalculateFlatBackground(1);
 
-    TS_ASSERT_THROWS_NOTHING( flatBG.execute() )
-    TS_ASSERT( flatBG.isExecuted() )
-    
     MatrixWorkspace_sptr inputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calcFlatBG");
     MatrixWorkspace_sptr outputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("Removed");
     // The X vectors should be the same
-    TS_ASSERT_EQUALS( inputWS->readX(0), outputWS->readX(0) )
+    TS_ASSERT_DELTA( inputWS->readX(0), outputWS->readX(0) , 1e-6 )
     // Just do a spot-check on Y & E
     const Mantid::MantidVec &Y = outputWS->readY(0);
     for (unsigned int i=0; i<Y.size(); ++i)
@@ -99,27 +89,31 @@ public:
     }
   }
 
+  void testExecWithReturnBackground()
+  {
+    runCalculateFlatBackground(2);
+
+    MatrixWorkspace_sptr inputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calcFlatBG");
+    MatrixWorkspace_sptr outputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("Removed");
+    // The X vectors should be the same
+    TS_ASSERT_DELTA( inputWS->readX(0), outputWS->readX(0) , 1e-6 )
+    // Just do a spot-check on Y & E
+    const Mantid::MantidVec &Y = outputWS->readY(0);
+    for (unsigned int i=0; i<Y.size(); ++i)
+    {
+      TS_ASSERT( 100.3431 > Y[i]);
+    }
+  }
+
+
   void testMeanFirst()
   {
-    Mantid::Algorithms::CalculateFlatBackground back;
-    TS_ASSERT_THROWS_NOTHING( back.initialize() )
-    TS_ASSERT( back.isInitialized() )
-    
-    back.setPropertyValue("InputWorkspace","calculateflatbackgroundtest_ramp");
-    back.setPropertyValue("OutputWorkspace","calculateflatbackgroundtest_first");
-    back.setPropertyValue("WorkspaceIndexList","");
-    back.setPropertyValue("Mode","Mean");
-    // remove the first half of the spectrum
-    back.setPropertyValue("StartX","0");
-    back.setPropertyValue("EndX","15");
+    runCalculateFlatBackground(3);
 
-    TS_ASSERT_THROWS_NOTHING( back.execute() )
-    TS_ASSERT( back.isExecuted() )
-    
     MatrixWorkspace_sptr inputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_ramp");
     MatrixWorkspace_sptr outputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_first");
     // The X vectors should be the same
-    TS_ASSERT_EQUALS( inputWS->readX(0), outputWS->readX(0) )
+    TS_ASSERT_DELTA( inputWS->readX(0), outputWS->readX(0) , 1e-6 )
 
     for (int j = 0; j < NUMSPECS; ++j)
     {
@@ -135,45 +129,69 @@ public:
         backError += EIn[k]*EIn[k];
       }
       background /= 15.0;
+
       backError = std::sqrt(backError)/15.0;
       for (int i = 0; i < NUMBINS; ++i)
       {
         double correct = ( YIn[i] - background ) > 0 ? YIn[i]-background : 0;
-        TS_ASSERT_EQUALS( YOut[i], correct )
+        TS_ASSERT_DELTA( YOut[i], correct , 1e-6 )
 
         if ( YIn[i] - background < 0 )
         {
-          TS_ASSERT_EQUALS(EOut[i], background)
+          TS_ASSERT_DELTA(EOut[i], background, 1e-6 )
         }
         else
         {
-          TS_ASSERT_EQUALS(EOut[i], std::sqrt((EIn[i]*EIn[i])+(backError*backError)) )
+          TS_ASSERT_DELTA(EOut[i], std::sqrt((EIn[i]*EIn[i])+(backError*backError)), 1e-6 )
         }
+      }
+    }
+  }
+
+void testMeanFirstWithReturnBackground()
+  {
+    runCalculateFlatBackground(4);
+
+    MatrixWorkspace_sptr inputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_ramp");
+    MatrixWorkspace_sptr outputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_first");
+    // The X vectors should be the same
+    TS_ASSERT_DELTA( inputWS->readX(0), outputWS->readX(0) , 1e-6 )
+
+    for (int j = 0; j < NUMSPECS; ++j)
+    {
+      const Mantid::MantidVec &YIn = inputWS->readY(j);
+      const Mantid::MantidVec &EIn = inputWS->readE(j);
+      const Mantid::MantidVec &YOut = outputWS->readY(j);
+      const Mantid::MantidVec &EOut = outputWS->readE(j);
+      // do our own calculation of the background and its error to check with later
+      double background = 0, backError = 0;
+
+      for( int k = 0; k < 15; ++k)
+      {
+        background += YIn[k];
+        backError += EIn[k]*EIn[k];
+      }
+      background /= 15.0;
+      backError = std::sqrt(backError)/15.0;
+      for (int i = 0; i < NUMBINS; ++i)
+      {
+        double correct = ( YIn[i] - background ) > 0 ? YIn[i]-background : 0;
+
+        TS_ASSERT_DELTA(YOut[i], background, 1e-6 )
+
+        TS_ASSERT_DELTA(EOut[i], std::sqrt((EIn[i]*EIn[i])+(backError*backError)), 1e-6 )
       }
     }
   }
 
   void testMeanSecond()
   {
-    Mantid::Algorithms::CalculateFlatBackground back;
-    TS_ASSERT_THROWS_NOTHING( back.initialize() )
-    TS_ASSERT( back.isInitialized() )
-    
-    back.setPropertyValue("InputWorkspace","calculateflatbackgroundtest_ramp");
-    back.setPropertyValue("OutputWorkspace","calculateflatbackgroundtest_second");
-    back.setPropertyValue("WorkspaceIndexList","");
-    back.setPropertyValue("Mode","Mean");
-    // remove the last half of the spectrum
-    back.setProperty("StartX", 2*double(NUMBINS)/3);
-    back.setProperty("EndX", double(NUMBINS));
+    runCalculateFlatBackground(5);
 
-    TS_ASSERT_THROWS_NOTHING( back.execute() )
-    TS_ASSERT( back.isExecuted() )
-    
     MatrixWorkspace_sptr inputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_ramp");
     MatrixWorkspace_sptr outputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_second");
     // The X vectors should be the same
-    TS_ASSERT_EQUALS( inputWS->readX(0), outputWS->readX(0) )
+    TS_ASSERT_DELTA( inputWS->readX(0), outputWS->readX(0) , 1e-6 )
 
     for (int j = 0; j < NUMSPECS; ++j)
     {
@@ -195,15 +213,15 @@ public:
       for (int i = 0; i < NUMBINS; ++i)
       {
         double correct = ( YIn[i] - background ) > 0 ? YIn[i]-background : 0;
-        TS_ASSERT_EQUALS( YOut[i], correct )
+        TS_ASSERT_DELTA( YOut[i], correct  , 1e-6 )
 
         if ( YIn[i] - background < 0 && EIn[i] < background )
         {
-          TS_ASSERT_EQUALS(EOut[i], background)
+          TS_ASSERT_DELTA(EOut[i], background , 1e-6 )
         }
         else
         {
-          TS_ASSERT_EQUALS(EOut[i], std::sqrt((EIn[i]*EIn[i])+(backError*backError)) )
+          TS_ASSERT_DELTA(EOut[i], std::sqrt((EIn[i]*EIn[i])+(backError*backError))  , 1e-6 )
         }
       }
     }
@@ -240,14 +258,14 @@ public:
     
     MatrixWorkspace_sptr outputWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("calculateflatbackgroundtest_third");
     // The X vectors should be the same
-    TS_ASSERT_EQUALS( WS->readX(0), outputWS->readX(0) )
+    TS_ASSERT_DELTA( WS->readX(0), outputWS->readX(0) , 1e-6 )
 
     const Mantid::MantidVec &YOut = outputWS->readY(0);
     const Mantid::MantidVec &EOut = outputWS->readE(0);
 
-    TS_ASSERT_EQUALS( YOut[5], 50.0 )
-    TS_ASSERT_EQUALS( YOut[25], 50.0 )
-    TS_ASSERT_EQUALS( YOut[NUMBINS-1], 0.0 )
+    TS_ASSERT_DELTA( YOut[5], 50.0  , 1e-6 )
+    TS_ASSERT_DELTA( YOut[25], 50.0  , 1e-6 )
+    TS_ASSERT_DELTA( YOut[NUMBINS-1], 0.0  , 1e-6 )
 
     TS_ASSERT_DELTA( EOut[10], 37.2677, 0.001 )
     TS_ASSERT_DELTA( EOut[20], 37.2677, 0.001 )
@@ -256,6 +274,77 @@ public:
 
 private:
   double bg;
+
+  double round( double value )
+  {
+    return floor( value*100000 + 0.5 )/100000;
+  }
+
+  ///Run CalculateFlatBackground wiht options specific to the function calling it. Each function has a preset
+  ///@param functindex - an integer identifying the function running the algorithm in order to set the properties
+  void runCalculateFlatBackground(int functindex)
+  {
+    // functindex key
+    // 1 = exec
+    // 2 = execwithreturnbg
+    // 3 = testMeanFirst
+    // 4 = testMeanFirstWithReturnBackground
+    // 5 = testMeanSecond
+
+    //common beginning
+    Mantid::Algorithms::CalculateFlatBackground flatBG;
+    TS_ASSERT_THROWS_NOTHING( flatBG.initialize() )
+    TS_ASSERT( flatBG.isInitialized() )
+
+    if (functindex == 1 || functindex == 2)
+    {
+      //exec or execWithReturnBackground
+      TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("InputWorkspace","calcFlatBG") )
+      TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("OutputWorkspace","Removed") )
+      TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("WorkspaceIndexList","0") )
+      TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("StartX","9.5") )
+      TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("EndX","20.5") )
+      TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("Mode","Linear Fit") )
+
+      if(functindex == 2)
+      {
+        //execWithReturnBackground
+        TS_ASSERT_THROWS_NOTHING( flatBG.setPropertyValue("OutputMode","Return Background") )
+      }
+    }
+    else if (functindex == 3 || functindex == 4 || functindex == 5)
+    {
+      flatBG.setPropertyValue("InputWorkspace","calculateflatbackgroundtest_ramp");
+      flatBG.setPropertyValue("WorkspaceIndexList","");
+      flatBG.setPropertyValue("Mode","Mean");
+
+      if (functindex == 3 || functindex == 4)
+      {
+        //testMeanFirst or testMeanFirstWithReturnBackground
+        flatBG.setPropertyValue("OutputWorkspace","calculateflatbackgroundtest_first");
+        // remove the first half of the spectrum
+        flatBG.setPropertyValue("StartX","0");
+        flatBG.setPropertyValue("EndX","15");
+        if (functindex == 4)
+        {
+          //testMeanFirstWithReturnBackground
+          flatBG.setPropertyValue("OutputMode","Return Background"); 
+        }
+      }
+      else if (functindex == 5)
+      {
+        //testMeanSecond
+        flatBG.setPropertyValue("OutputWorkspace","calculateflatbackgroundtest_second");
+        // remove the last half of the spectrum
+        flatBG.setProperty("StartX", 2*double(NUMBINS)/3);
+        flatBG.setProperty("EndX", double(NUMBINS));
+      }
+    }
+
+    // common ending
+    TS_ASSERT_THROWS_NOTHING( flatBG.execute() )
+    TS_ASSERT( flatBG.isExecuted() )
+  }
 };
 
 #endif /*FlatBackgroundTest_H_*/
