0dab2805731eeeeb972b3df5a5e4669856474764
Keith Brown <keith.brown@stfc.ac.uk>
2013-08-09 09:08:37 +0100

    Progress being made with test suite
    The test suite is coming along, only a few more tests to write
    
    Refs #3344

--- Code/Mantid/Framework/API/test/AlgorithmFactoryTest.h
+++ Code/Mantid/Framework/API/test/AlgorithmFactoryTest.h
@@ -23,31 +23,116 @@ public:
 
   void testSubscribe()
   {
-    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().subscribe<ToyAlgorithm>())
     Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>* newTwo = new Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>;
-    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().subscribe(newTwo))
+    
+    //get the nubmer of algorithms it already has
+    std::vector<std::string> keys = AlgorithmFactory::Instance().getKeys();
+    int noOfAlgs = keys.size();
 
-    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().subscribe<ToyAlgorithm>())
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().subscribe<ToyAlgorithm>());
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().subscribe(newTwo));
+
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().subscribe<ToyAlgorithm>());
+   
+    //get the nubmer of algorithms it has now
+    keys = AlgorithmFactory::Instance().getKeys();
+    int noOfAlgsAfter = keys.size();
+    TS_ASSERT_EQUALS(noOfAlgsAfter, noOfAlgs + 2);
+
     AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",1);
     AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",2);
   }
 
   void testUnsubscribe()
   {
-  }
-  
-  void testCreate()
-  {
+    Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>* newTwo = new Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>;
+
+    //get the nubmer of algorithms it already has
+    std::vector<std::string> keys = AlgorithmFactory::Instance().getKeys();
+    int noOfAlgs = keys.size();
+
+    AlgorithmFactory::Instance().subscribe<ToyAlgorithm>();
+    AlgorithmFactory::Instance().subscribe(newTwo);
+   
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",1));
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",2));
+    
+    //get the nubmer of algorithms it has now
+    keys = AlgorithmFactory::Instance().getKeys();
+    int noOfAlgsAfter = keys.size();
+
+    TS_ASSERT_EQUALS(noOfAlgsAfter, noOfAlgs)
+
+    //try unsubscribing them again
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",1));
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",2));
+    
+    //make sure the number hasn't changed
+    keys = AlgorithmFactory::Instance().getKeys();
+    int noOfAlgsAgain = keys.size();
+
+    TS_ASSERT_EQUALS(noOfAlgsAfter, noOfAlgsAgain);
   }
   
   void testExists()
   {
+    Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>* newTwo = new Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>;
+    
+    AlgorithmFactory::Instance().subscribe<ToyAlgorithm>();
+    AlgorithmFactory::Instance().subscribe(newTwo);
+    
+    TS_ASSERT(AlgorithmFactory::Instance().exists("ToyAlgorithm",1));
+    TS_ASSERT(AlgorithmFactory::Instance().exists("ToyAlgorithm",2));
+    TS_ASSERT(!AlgorithmFactory::Instance().exists("ToyAlgorithm",3));
+    TS_ASSERT(!AlgorithmFactory::Instance().exists("ToyAlgorithm",4));
+    TS_ASSERT(AlgorithmFactory::Instance().exists("ToyAlgorithm",-1));
+
+    AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",1);
+    AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",2);
   }
   
   void testGetKeys()
   {
+    std::vector<std::string> keys;
+
+    TS_ASSERT_EQUALS(0, keys.size());
+    AlgorithmFactory::Instance().subscribe<ToyAlgorithm>();
+
+    TS_ASSERT_THROWS_NOTHING(keys = AlgorithmFactory::Instance().getKeys());
+    int noOfAlgs = keys.size();
+    TS_ASSERT(noOfAlgs > 1);
+
+    AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",1);
+
+    TS_ASSERT_THROWS_NOTHING(keys = AlgorithmFactory::Instance().getKeys());
+    TS_ASSERT_EQUALS(noOfAlgs - 1, keys.size());
   }
   
+  void testCreate()
+  {
+    Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>* newTwo = new Mantid::Kernel::Instantiator<ToyAlgorithmTwo, Algorithm>;
+    
+    AlgorithmFactory::Instance().subscribe<ToyAlgorithm>();
+    AlgorithmFactory::Instance().subscribe(newTwo);
+    
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().create("ToyAlgorithm",-1));
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("AlgorithmDoesntExist",-1));
+
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().create("ToyAlgorithm",1));
+    TS_ASSERT_THROWS_NOTHING(AlgorithmFactory::Instance().create("ToyAlgorithm",2));
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("AlgorithmDoesntExist",1));
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("AlgorithmDoesntExist",2));
+
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("",1));
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("",-1));
+
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("ToyAlgorithm",3));
+    TS_ASSERT_THROWS_ANYTHING(AlgorithmFactory::Instance().create("ToyAlgorithm",4));
+
+    AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",1);
+    AlgorithmFactory::Instance().unsubscribe("ToyAlgorithm",2);
+  }
+
   void testGetCategories()
   {
   }
