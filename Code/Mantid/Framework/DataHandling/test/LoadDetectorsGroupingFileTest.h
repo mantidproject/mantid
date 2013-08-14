304876e0b457258644cd3f7b0a8d5bb931b04998
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2013-08-14 17:03:07 +0100

    Make the test use ScopedFile instead of creating/deleting manually.
    Refs #7715

--- Code/Mantid/Framework/DataHandling/test/LoadDetectorsGroupingFileTest.h
+++ Code/Mantid/Framework/DataHandling/test/LoadDetectorsGroupingFileTest.h
@@ -1,20 +1,24 @@
 #ifndef MANTID_DATAHANDLING_LOADDETECTORSGROUPINGFILETEST_H_
 #define MANTID_DATAHANDLING_LOADDETECTORSGROUPINGFILETEST_H_
 
-#include <cxxtest/TestSuite.h>
 #include "MantidKernel/Timer.h"
 #include "MantidKernel/System.h"
+
+#include "MantidDataHandling/LoadDetectorsGroupingFile.h"
+#include "MantidDataObjects/GroupingWorkspace.h"
+#include "MantidTestHelpers/ScopedFileHelper.h"
+
+#include <cxxtest/TestSuite.h>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 
-#include "MantidDataHandling/LoadDetectorsGroupingFile.h"
-#include "MantidDataObjects/GroupingWorkspace.h"
-#include "Poco/File.h"
-
 using namespace Mantid;
 using namespace Mantid::DataHandling;
+using namespace Mantid::DataObjects;
 using namespace Mantid::API;
+
+using ScopedFileHelper::ScopedFile;
 
 class LoadDetectorsGroupingFileTest : public CxxTest::TestSuite
 {
@@ -68,10 +72,9 @@ public:
     LoadDetectorsGroupingFile load;
     load.initialize();
 
-    std::string xmlfilename = "testautoidgroup.xml";
-    generateAutoGroupIDGroupXMLFile(xmlfilename);
+    ScopedFile f = generateAutoGroupIDGroupXMLFile("testautoidgroup.xml");
 
-    TS_ASSERT(load.setProperty("InputFile", xmlfilename));
+    TS_ASSERT(load.setProperty("InputFile", f.getFileName()));
     TS_ASSERT(load.setProperty("OutputWorkspace", "Vulcan_Group2"));
 
     load.execute();
@@ -83,34 +86,26 @@ public:
     TS_ASSERT_DELTA(gws->dataY(3695)[0], 1.0, 1.0E-5);
     TS_ASSERT_DELTA(gws->dataY(3696)[0], 2.0, 1.0E-5);
     TS_ASSERT_DELTA(gws->dataY(7000)[0], 2.0, 1.0E-5);
-
-    // Clean
-    Poco::File cleanfile(xmlfilename);
-    cleanfile.remove(false);
-
-    return;
   }
 
-  void generateAutoGroupIDGroupXMLFile(std::string xmlfilename)
+  ScopedFile generateAutoGroupIDGroupXMLFile(std::string xmlfilename)
   {
-    std::ofstream ofs;
-    ofs.open(xmlfilename.c_str(), std::ios::out);
+    std::ostringstream os;
 
-    ofs << "<?xml version=\"1.0\"?>" << std::endl;
-    ofs << "<detector-grouping instrument=\"VULCAN\">" << std::endl;
-    ofs << "  <group>" << std::endl;
-    ofs << "    <detids>26250-27481,27500-28731,28750-29981</detids>" << std::endl;
-    ofs << "  </group>" << std::endl;
-    ofs << "  <group>" << std::endl;
-    ofs << "    <component>bank26</component>" << std::endl;
-    ofs << "    <component>bank27</component>" << std::endl;
-    ofs << "    <component>bank28</component>" << std::endl;
-    ofs << "  </group>" << std::endl;
-    ofs << "</detector-grouping>" << std::endl;
+    os << "<?xml version=\"1.0\"?>" << std::endl;
+    os << "<detector-grouping instrument=\"VULCAN\">" << std::endl;
+    os << "  <group>" << std::endl;
+    os << "    <detids>26250-27481,27500-28731,28750-29981</detids>" << std::endl;
+    os << "  </group>" << std::endl;
+    os << "  <group>" << std::endl;
+    os << "    <component>bank26</component>" << std::endl;
+    os << "    <component>bank27</component>" << std::endl;
+    os << "    <component>bank28</component>" << std::endl;
+ 
+    os << "  </group>" << std::endl;
+    os << "</detector-grouping>" << std::endl;
 
-    ofs.close();
-
-    return;
+    return ScopedFile(os.str(), xmlfilename);
   }
 
   /*
@@ -121,10 +116,9 @@ public:
     LoadDetectorsGroupingFile load;
     load.initialize();
 
-    std::string xmlfilename = "testnoinstrumentgroup.xml";
-    generateSpectrumIDXMLFile(xmlfilename);
+    ScopedFile f = generateSpectrumIDXMLFile("testnoinstrumentgroup.xml");
 
-    TS_ASSERT(load.setProperty("InputFile", xmlfilename));
+    TS_ASSERT(load.setProperty("InputFile", f.getFileName()));
     TS_ASSERT(load.setProperty("OutputWorkspace", "Vulcan_Group3"));
 
     load.execute();
@@ -136,35 +130,26 @@ public:
     TS_ASSERT_DELTA(gws->dataY(1)[0], 1.0, 1.0E-5);
     TS_ASSERT_DELTA(gws->dataY(5)[0], 2.0, 1.0E-5);
     TS_ASSERT_DELTA(gws->dataY(16)[0], 2.0, 1.0E-5);
-
-    // Clean
-    Poco::File cleanfile(xmlfilename);
-    cleanfile.remove(false);
-
-    return;
   }
 
-  void generateSpectrumIDXMLFile(std::string xmlfilename)
+  ScopedFile generateSpectrumIDXMLFile(std::string xmlfilename)
   {
-    std::ofstream ofs;
-    ofs.open(xmlfilename.c_str(), std::ios::out);
+    std::ostringstream os;
 
-    ofs << "<?xml version=\"1.0\"?>" << std::endl;
-    ofs << "<detector-grouping>" << std::endl;
-    ofs << "  <group>" << std::endl;
-    ofs << "    <ids>30-36,12-16,100-111</ids>" << std::endl;
-    ofs << "  </group>" << std::endl;
-    ofs << "  <group>" << std::endl;
-    ofs << "    <ids>38</ids>" << std::endl;
-    ofs << "    <ids>291</ids>" << std::endl;
-    ofs << "    <ids>22-25</ids>" << std::endl;
-    ofs << "  </group>" << std::endl;
-    ofs << "  <group name=\"bwd2\"><ids val=\"333,444,555\"/>334,557</group>" << std::endl;
-    ofs << "</detector-grouping>" << std::endl;
+    os << "<?xml version=\"1.0\"?>" << std::endl;
+    os << "<detector-grouping>" << std::endl;
+    os << "  <group>" << std::endl;
+    os << "    <ids>30-36,12-16,100-111</ids>" << std::endl;
+    os << "  </group>" << std::endl;
+    os << "  <group>" << std::endl;
+    os << "    <ids>38</ids>" << std::endl;
+    os << "    <ids>291</ids>" << std::endl;
+    os << "    <ids>22-25</ids>" << std::endl;
+    os << "  </group>" << std::endl;
+    os << "  <group name=\"bwd2\"><ids val=\"333,444,555\"/>334,557</group>" << std::endl;
+    os << "</detector-grouping>" << std::endl;
 
-    ofs.close();
-
-    return;
+    return ScopedFile(os.str(), xmlfilename);
   }
 
   /*
@@ -175,10 +160,9 @@ public:
     LoadDetectorsGroupingFile load;
     load.initialize();
 
-    std::string xmlfilename = "testoldformat.xml";
-    generateOldSpectrumIDXMLFile(xmlfilename);
+    ScopedFile f = generateOldSpectrumIDXMLFile("testoldformat.xml");
 
-    TS_ASSERT(load.setProperty("InputFile", xmlfilename));
+    TS_ASSERT(load.setProperty("InputFile", f.getFileName()));
     TS_ASSERT(load.setProperty("OutputWorkspace", "Random_Group_Old"));
 
     load.execute();
@@ -191,28 +175,19 @@ public:
     TS_ASSERT_DELTA(gws->dataY(31)[0], 1.0, 1.0E-5);
     TS_ASSERT_DELTA(gws->dataY(32)[0], 2.0, 1.0E-5);
     TS_ASSERT_DELTA(gws->dataY(39)[0], 2.0, 1.0E-5);
-
-    // Clean
-    Poco::File cleanfile(xmlfilename);
-    cleanfile.remove(false);
-
-    return;
   }
 
-  void generateOldSpectrumIDXMLFile(std::string xmlfilename)
+  ScopedFile generateOldSpectrumIDXMLFile(std::string xmlfilename)
   {
-    std::ofstream ofs;
-    ofs.open(xmlfilename.c_str(), std::ios::out);
+    std::ostringstream os;
 
-    ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" << std::endl;
-    ofs << "<detector-grouping>" << std::endl;
-    ofs << "  <group name=\"fwd1\"> <ids val=\"1-32\"/> </group>" << std::endl;
-    ofs << "  <group name=\"bwd1\"> <ids val=\"33,36,38,60-64\"/> </group>" << std::endl;
-    ofs << "</detector-grouping>" << std::endl;
+    os << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" << std::endl;
+    os << "<detector-grouping>" << std::endl;
+    os << "  <group name=\"fwd1\"> <ids val=\"1-32\"/> </group>" << std::endl;
+    os << "  <group name=\"bwd1\"> <ids val=\"33,36,38,60-64\"/> </group>" << std::endl;
+    os << "</detector-grouping>" << std::endl;
 
-    ofs.close();
-
-    return;
+    return ScopedFile(os.str(), xmlfilename);
   }
 
 };
