ad7a75f0529bebcfd4b8d3f38512353bbe8ef2ea
Wenduo Zhou <zhouw@ornl.gov>
2013-08-19 15:32:22 -0400

    Added some unit test (incomplete).  Refs #7653.

--- Code/Mantid/Framework/CurveFitting/test/NeutronBk2BkExpConvPVoigtTest.h
+++ Code/Mantid/Framework/CurveFitting/test/NeutronBk2BkExpConvPVoigtTest.h
@@ -16,9 +16,36 @@ public:
   static void destroySuite( NeutronBk2BkExpConvPVoigtTest *suite ) { delete suite; }
 
 
-  void test_Something()
+  //----------------------------------------------------------------------------------------------
+  /** Set and get parameter
+    */
+  void test_accessParameter()
   {
-    TSM_ASSERT( "You forgot to write a test!", 0);
+    NeutronBk2BkExpConvPVoigt func;
+    func.setParameter("A", 1.0);
+    double a = func.getParameter("A");
+    TS_ASSERT_EQUALS(a, 1.0);
+
+    // Blabla
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Calculate peak positions
+    */
+  void test_calculatePeakPositions()
+  {
+    NeutronBk2BkExpConvPVoigt func;
+
+    func.setParameter("Dtt1", 0.0);
+    func.setParameter("Dtt2", 0.0);
+    func.setParameter("Zero", 0.0);
+    func.setParameter("LatticeConstant", 0.0);
+
+    func.setMillerIndex(1, 1, 0);
+    func.calculateParameters(false);
+    double tofh1 = func.centre();
+    TS_ASSERT_DELTA(tofh1, 0.0, 0.01);
+
   }
 
 
