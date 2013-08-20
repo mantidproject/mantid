3a998164239cf7c4e37467db05d6595799e9e52d
Jay Rainey <jawrainey@gmail.com>
2013-08-20 12:11:48 +0100

    Login and logout functionality implemented. Refs #7639.
    - Encapsulated SSL authentication scheme into own function to prevent duplication of code.
    - Error message method ensures user friendly and accurate gSoap errors are output.

---
+++ Code/Mantid/Framework/ICat/inc/MantidICat/ICat4/ICat4Catalog.h
@@ -0,0 +1,76 @@
+#ifndef MANTID_ICAT_ICAT4CATALOG_H_
+#define MANTID_ICAT_ICAT4CATALOG_H_
+
+#include "MantidAPI/ICatalog.h"
+#include "MantidICat/CatalogSearchParam.h"
+
+namespace Mantid
+{
+  namespace ICat
+  {
+    /**
+    This class is responsible for the implementation of ICat4 version based information catalogs
+    @author Jay Rainey, ISIS Rutherford Appleton Laboratory 
+    @date 12/08/2013
+    Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
+    class  ICat4Catalog : public Mantid::API::ICatalog
+    {
+      public:
+        /// Constructor
+        ICat4Catalog();
+        /// Destructor
+        virtual ~ICat4Catalog();
+        /// Log the user into the catalog system.
+        virtual void login(const std::string& username,const std::string& password,const std::string& url);
+        /// Log the user out of the catalog system.
+        virtual void logout();
+        /// Search the catalog for data.
+        virtual void search(const CatalogSearchParam& inputs, Mantid::API::ITableWorkspace_sptr& ws_sptr);
+        /// Show the logged in user's investigations search results.
+        virtual void myData(Mantid::API::ITableWorkspace_sptr& mydataws_sptr);
+        /// Get datasets.
+        virtual void getDataSets(const long long&investigationId,Mantid::API::ITableWorkspace_sptr& datasetsws_sptr);
+        /// Get datafiles
+        virtual void getDataFiles(const long long&investigationId,Mantid::API::ITableWorkspace_sptr& datafilesws_sptr);
+        /// Get instruments list
+        virtual void listInstruments(std::vector<std::string>& instruments);
+        /// Get investigationtypes list
+        virtual void listInvestigationTypes(std::vector<std::string>& invstTypes);
+        /// Get file location strings
+        virtual void getFileLocation(const long long&fileid,std::string& filelocation);
+        /// Get urls
+        virtual void getDownloadURL(const long long& fileid,std::string & fileLocation);
+        /// Keep current session alive
+        virtual void keepAlive();
+        /// Keep alive in minutes
+        virtual int keepAliveinminutes();
+
+      private:
+        // Ensures human friendly error messages are provided to the user.
+        void throwErrorMessage(ICat4::ICATPortBindingProxy& icat);
+        // Defines the SSL authentication scheme.
+        void setSSLContext(ICat4::ICATPortBindingProxy& icat);
+    };
+  }
+}
+
+#endif
---
+++ Code/Mantid/Framework/ICat/src/ICat4/ICat4Catalog.cpp
@@ -0,0 +1,240 @@
+#include "MantidICat/ICat4/GSoapGenerated/ICat4ICATPortBindingProxy.h"
+#include "MantidICat/ICat4/ICat4Catalog.h"
+#include "MantidAPI/CatalogFactory.h"
+#include "MantidICat/Session.h"
+#include "MantidAPI/Progress.h"
+
+namespace Mantid
+{
+  namespace ICat
+  {
+    using namespace ICat4;
+
+    DECLARE_CATALOG(ICat4Catalog)
+
+    /// constructor
+    ICat4Catalog::ICat4Catalog() { }
+
+    /// destructor
+    ICat4Catalog::~ICat4Catalog() { }
+
+    /**
+     * Uses ICat API login to connect to catalog.
+     * @param username :: login name(E.g. federal id) of the user
+     * @param password :: password of the user
+     * @param url      :: endpoint url of the catalog
+     */
+    void ICat4Catalog::login(const std::string& username, const std::string& password, const std::string& url)
+    {
+      UNUSED_ARG(url)
+      ICATPortBindingProxy icat;
+
+      // Define ssl authentication scheme
+      setSSLContext(icat);
+
+      // Used to authenticate the user.
+      ns1__login login;
+      ns1__loginResponse loginResponse;
+
+      // Used to add entries to the login class.
+      _ns1__login_credentials_entry entry;
+
+      // Name of the authentication plugin in use.
+      std::string plugin("ldap");
+      login.plugin = &plugin;
+
+      // Making string as cannot convert from const.
+      std::string userName(username);
+      std::string passWord(password);
+
+      std::string usernameKey("username");
+      std::string passwordKey("password");
+
+      // Instantiate an instance of an entry to prevent null pointer.
+      // This then allows us to push entries as required below.
+      std::vector<_ns1__login_credentials_entry> entries;
+      login.credentials.entry = &entries;
+
+      // Setting the username and pass credentials to the login class.
+      entry.key   = &usernameKey;
+      entry.value = &userName;
+      entries.push_back(entry);
+
+      entry.key   = &passwordKey;
+      entry.value = &passWord;
+      entries.push_back(entry);
+
+      // Is the login successful? (0 if yes, 12 if no).
+      int query_id = icat.login(&login, &loginResponse);
+
+      if (query_id == 0)
+      {
+        std::string session_id = *(loginResponse.return_);
+        ICat::Session::Instance().setSessionId(session_id);
+        ICat::Session::Instance().setUserName(userName);
+      }
+      else
+      {
+        throwErrorMessage(icat);
+      }
+    }
+
+    /**
+     * Disconnects the client application from ICat4 based catalog services.
+     */
+    void ICat4Catalog::logout()
+    {
+      ICATPortBindingProxy icat;
+      setSSLContext(icat);
+
+      ns1__logout request;
+      ns1__logoutResponse response;
+
+      //Get the session ID to log the user out.
+      std::string sessionID  = Session::Instance().getSessionId();
+      request.sessionId = &sessionID;
+
+      int logoutResponse = icat.logout(&request,&response);
+
+      //If session is not valid (expired, or not logged in)
+      if(logoutResponse != 0)
+      {
+        //then throw an error.
+        throwErrorMessage(icat);
+      }
+      //otherwise we clear the session.
+      Session::Instance().setSessionId("");
+    }
+
+    /**
+     * Searches for the investigation for the user.
+     * @param inputs  :: reference to a class contains search inputs
+     * @param ws_sptr :: shared pointer to search results workspace
+     */
+    void ICat4Catalog::search(const CatalogSearchParam& inputs, Mantid::API::ITableWorkspace_sptr& ws_sptr)
+    {
+    }
+
+    /**
+     * Returns the logged in user's investigations data.
+     * @param mydataws_sptr :: pointer to table workspace which stores the data
+     */
+    void ICat4Catalog::myData(Mantid::API::ITableWorkspace_sptr& mydataws_sptr)
+    {
+    }
+
+    /**
+     * Returns the datasets associated to the given investigation id.
+     * @param investigationId :: unique identifier of the investigation
+     * @param datasetsws_sptr :: shared pointer to datasets
+     */
+    void ICat4Catalog::getDataSets(const long long& investigationId, Mantid::API::ITableWorkspace_sptr& datasetsws_sptr)
+    {
+    }
+
+    /**
+     * Returns the datafiles associated to the given investigation id.
+     * @param investigationId  :: unique identifier of the investigation
+     * @param datafilesws_sptr :: shared pointer to datasets
+     */
+    void ICat4Catalog::getDataFiles(const long long& investigationId, Mantid::API::ITableWorkspace_sptr& datafilesws_sptr)
+    {
+    }
+
+    /**
+     * Returns the list of instruments.
+     * @param instruments :: instruments list
+     */
+    void ICat4Catalog::listInstruments(std::vector<std::string>& instruments)
+    {
+    }
+
+    /**
+     * Returns the list of investigation types.
+     * @param invstTypes :: investigation types list
+     */
+    void ICat4Catalog::listInvestigationTypes(std::vector<std::string>& invstTypes)
+    {
+    }
+
+    /**
+     * Gets the file location string from the archives.
+     * @param fileid       :: id of the file
+     * @param filelocation :: location string  of the file
+     */
+    void ICat4Catalog::getFileLocation(const long long & fileid, std::string & filelocation)
+    {
+    }
+
+    /**
+     * Downloads a file from the given url.
+     * @param fileid :: id of the file
+     * @param url    :: url  of the file
+     */
+    void ICat4Catalog::getDownloadURL(const long long & fileid, std::string& url)
+    {
+    }
+
+    /**
+     * Keep the current session alive.
+     */
+    void ICat4Catalog::keepAlive()
+    {
+    }
+
+    /**
+     * Keep the current session alive in minutes.
+     * @return The number of minutes to keep session alive for.
+     */
+    int ICat4Catalog::keepAliveinminutes()
+    {
+      return (0);
+    }
+
+    /**
+     * Defines the SSL authentication scheme.
+     * @param icat :: ICATPortBindingProxy object.
+     */
+    void ICat4Catalog::setSSLContext(ICATPortBindingProxy& icat)
+    {
+      if (soap_ssl_client_context(&icat,
+          SOAP_SSL_NO_AUTHENTICATION, /* use SOAP_SSL_DEFAULT in production code */
+          NULL,       /* keyfile: required only when client must authenticate to
+              server (see SSL docs on how to obtain this file) */
+          NULL,       /* password to read the keyfile */
+          NULL,      /* optional cacert file to store trusted certificates */
+          NULL,      /* optional capath to directory with trusted certificates */
+          NULL      /* if randfile!=NULL: use a file with random data to seed randomness */
+      ))
+      {
+        throwErrorMessage(icat);
+      }
+    }
+
+    /**
+     * Throws an error message (returned by gsoap) to Mantid upper layer.
+     * @param icat :: ICATPortBindingProxy object.
+     */
+    void ICat4Catalog::throwErrorMessage(ICATPortBindingProxy& icat)
+    {
+      char buf[600];
+      const int len = 600;
+      icat.soap_sprint_fault(buf,len);
+      std::string error(buf);
+      std::string begmsg("<message>");
+      std::string endmsg("</message>");
+
+      std::basic_string <char>::size_type start = error.find(begmsg);
+      std::basic_string <char>::size_type end   = error.find(endmsg);
+      std::string exception;
+
+      if(start != std::string::npos && end != std::string::npos)
+      {
+        exception = error.substr(start + begmsg.length(), end - (start + begmsg.length()) );
+      }
+
+      throw std::runtime_error(exception);
+    }
+
+  }
+}
