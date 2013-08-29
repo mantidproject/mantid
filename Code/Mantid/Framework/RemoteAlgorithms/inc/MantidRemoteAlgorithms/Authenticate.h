3f3e37d8341088180c417458dc165bda6e9963f7
Ross Miller <rgmiller@ornl.gov>
2013-08-29 15:09:46 -0400

    Add the Authenticate algorithm
    Refs #7769

---
+++ Code/Mantid/Framework/RemoteAlgorithms/inc/MantidRemoteAlgorithms/Authenticate.h
@@ -0,0 +1,70 @@
+#ifndef AUTHENTICATE_H_
+#define AUTHENTICATE_H_
+
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid {
+namespace RemoteAlgorithms {
+/*** Authenticate to the remote compute resource.
+    
+    Input Properties:
+    <UL>
+    <LI> ComputeResource  - The name of the compute resource that will execute the job </LI>
+    <LI> UserName         - User name on the compute resource </LI>
+    <LI> Password         - Password for the compute resource </LI>
+    </UL>
+
+    Output Properties: None.
+    If the authentication is successfull, a cookie is received that is stored internally and
+    re-used for all subsequent interactions with the compute resource.
+
+    
+    @author Ross Miller, ORNL
+    @date 04/30/2013
+
+    Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
+
+class Authenticate : public Mantid::API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  Authenticate() : Mantid::API::Algorithm() {}
+  /// Virtual destructor
+  virtual ~Authenticate() {}
+  /// Algorithm's name
+  virtual const std::string name() const { return "Authenticate"; }
+  /// Algorithm's version
+  virtual int version() const { return (1); }
+  /// Algorithm's category for identification
+  virtual const std::string category() const { return "Remote"; }
+
+private:
+  /// Initialisation code
+  void init();
+  ///Execution code
+  void exec();
+
+};
+
+} // end namespace RemoteAlgorithms
+} // end namespace Mantid
+#endif /*AUTHENTICATE_H_*/
---
+++ Code/Mantid/Framework/RemoteAlgorithms/src/Authenticate.cpp
@@ -0,0 +1,84 @@
+/*WIKI*
+
+Authenticate to the remote compute resource.  This must be executed before calling any
+other remote algorithms.
+
+*WIKI*/
+
+#include "MantidRemoteAlgorithms/Authenticate.h"
+#include "MantidKernel/BoundedValidator.h"
+#include "MantidKernel/MandatoryValidator.h"
+#include "MantidKernel/FacilityInfo.h"
+#include "MantidKernel/Exception.h"
+#include "MantidKernel/MaskedProperty.h"
+#include "MantidKernel/ListValidator.h"
+#include "MantidKernel/RemoteJobManager.h"
+
+#include "MantidRemote/RemoteTask.h"
+#include "MantidRemote/SimpleJSON.h"
+
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+namespace Mantid
+{
+namespace RemoteAlgorithms
+{
+
+
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM(Authenticate)
+
+using namespace Mantid::Kernel;
+//using namespace Mantid::API;
+//using namespace Mantid::Geometry;
+
+// A reference to the logger is provided by the base class, it is called g_log.
+// It is used to print out information, warning and error messages
+
+void Authenticate::init()
+{
+  // Unlike most algorithms, this wone doesn't deal with workspaces....
+
+  auto mustBePositive = boost::make_shared<BoundedValidator<int> >();
+  mustBePositive->setLower(0);
+
+  auto requireValue = boost::make_shared<MandatoryValidator<std::string> >();
+
+  // Compute Resources
+  std::vector<std::string> computes = Mantid::Kernel::ConfigService::Instance().getFacility().computeResources();
+  declareProperty( "ComputeResource", "", boost::make_shared<StringListValidator>(computes), "", Direction::Input);
+
+  // Say who we are (or at least, who we want to execute the remote python code)
+  declareProperty( "UserName", "", requireValue, "", Direction::Input);
+
+  // Password doesn't get echoed to the screen...
+  declareProperty( new MaskedProperty<std::string>( "Password", "", requireValue, Direction::Input), "");
+
+}
+
+void Authenticate::exec()
+{
+  boost::shared_ptr<RemoteJobManager> jobManager = ConfigService::Instance().getFacility().getRemoteJobManager( getPropertyValue("ComputeResource"));
+
+  // jobManager is a boost::shared_ptr...
+  if (! jobManager)
+  {
+    // Requested compute resource doesn't exist
+    // TODO: should we create our own exception class for this??
+    throw( std::runtime_error( std::string("Unknown create a compute resource named " + getPropertyValue("ComputeResource"))));
+  }
+
+  std::istream &respStream = jobManager->httpGet( "/authenticate", "", getPropertyValue("UserName"), getPropertyValue("Password"));
+  if ( jobManager->lastStatus() != Poco::Net::HTTPResponse::HTTP_OK)
+  {
+    JSONObject resp;
+    initFromStream( resp, respStream);
+    std::string errMsg;
+    resp["Err_Msg"].getValue( errMsg);
+    throw( std::runtime_error( errMsg));
+  }
+}
+
+} // end namespace RemoteAlgorithms
+} // end namespace Mantid
