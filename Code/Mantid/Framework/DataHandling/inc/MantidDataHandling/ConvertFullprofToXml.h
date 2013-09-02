f25c76724517463fdc36d142961c794b93b68427
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-09-02 15:51:00 +0100

    Add non-functioning ConvertFullprofToXml algorithm re #7617
    I've left some code in it from LoadFullprofResolution, which could be useful. If I end up using a lot of the code without much modification, then it should go in a class for use by both algorithms.
    
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

---
+++ Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/ConvertFullprofToXml.h
@@ -0,0 +1,75 @@
+#ifndef MANTID_DATAHANDLING_CONVERTFULLPROFTOXML_H_
+#define MANTID_DATAHANDLING_CONVERTFULLPROFTOXML_H_
+
+#include "MantidKernel/System.h"
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid
+{
+namespace DataHandling
+{
+
+  /** LoadFullprofResolution : Load Fullprof resolution (.irf) file to TableWorkspace(s)
+    
+    Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+  */
+  class DLLExport ConvertFullprofToXml : public API::Algorithm
+  {
+  public:
+    ConvertFullprofToXml();
+    virtual ~ConvertFullprofToXml();
+
+    /// Algorithm's name for identification overriding a virtual method
+    virtual const std::string name() const { return "ConvertFullprofToXml";}
+
+    /// Algorithm's version for identification overriding a virtual method
+    virtual int version() const { return 1;}
+
+    /// Algorithm's category for identification overriding a virtual method
+    virtual const std::string category() const { return "Diffraction";}
+
+  private:
+    /// Sets documentation strings for this algorithm
+    virtual void initDocs();
+    /// Implement abstract Algorithm methods
+    void init();
+    /// Implement abstract Algorithm methods
+    void exec();
+
+    /// Load file to a vector of strings
+    void loadFile(std::string filename, std::vector<std::string>& lines);
+
+    /// Scan imported file for bank information
+    void scanBanks(const std::vector<std::string>& lines, std::vector<int>& banks,
+                   std::map<int, int> &bankstartindexmap, std::map<int, int> &bankendindexmap);
+
+    /// Parse .irf file to a map
+    void parseResolutionStrings(std::map<std::string, double>& parammap, const std::vector<std::string>& lines, int bankid, int startlineindex, int endlineindex);
+    
+    void parseBankLine(std::string line, double& cwl, int& bankid);
+
+  };
+
+
+} // namespace DataHandling
+} // namespace Mantid
+
+#endif  /* MANTID_DATAHANDLING_CONVERTFULLPROFTOXML_H_ */
