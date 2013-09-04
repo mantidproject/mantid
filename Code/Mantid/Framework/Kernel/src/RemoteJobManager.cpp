e0e3bfe3c59c4b4b6faf62d23e146a72e15e5ba8
Ross Miller <rgmiller@ornl.gov>
2013-09-04 17:57:10 -0400

    Hack to work around problem with winsock.h
    It looks like the Poco headers are trying to include both winsock.h and
    winsock2.h (possibly because windows.h includes winsock.h by default).
    Even if this works (and I won't know until I push it into the develop
    branch), it's still a lousy solution.
    
    Refs #7769

--- Code/Mantid/Framework/Kernel/src/RemoteJobManager.cpp
+++ Code/Mantid/Framework/Kernel/src/RemoteJobManager.cpp
@@ -1,3 +1,8 @@
+// HACK! Workaround for Poco headers trying to include both winsock.h and winsock2.h
+// If this actually works, I'll see if I can get on an actual Windows machine and
+// try to sort out a better solution
+#define WIN32_LEAN_AND_MEAN
+
 #include "MantidKernel/ConfigService.h"
 #include "MantidKernel/Logger.h"
 #include "MantidKernel/RemoteJobManager.h"
