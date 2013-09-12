abcea42457e9020f92d9399d1023e1744ff713aa
Russell Taylor <taylorrj@ornl.gov>
2013-09-12 10:59:32 -0400

    Re #5939. Run mantidplotrc on IPython startup.
    This is in place of importing the individual mantid & mantidplot
    modules and means that code completion in the 'F3 script window'
    still works.

--- Code/Mantid/MantidPlot/ipython_widget/mantid_ipython_widget.py
+++ Code/Mantid/MantidPlot/ipython_widget/mantid_ipython_widget.py
@@ -12,10 +12,15 @@ class MantidIPythonWidget(RichIPythonWidget):
         kernel = kernel_manager.kernel
         kernel.gui = 'qt4'
 
-        import mantidplot
-        kernel.shell.push(mantidplot.__dict__)
-        import mantid.simpleapi
-        kernel.shell.push(mantid.simpleapi.__dict__)
+        # Figure out the full path to the mantidplotrc.py file and then %run it
+        from os import path
+        mantidplotpath = path.split(path.dirname(__file__))[0]
+        if not mantidplotpath:
+            # If the the file is in the cwd, then path.dirname returns an empty string
+            mantidplotrc = 'mantidplotrc.py'
+        else:
+            mantidplotrc = mantidplotpath + '/mantidplotrc.py'
+        kernel.shell.run_line_magic('run',mantidplotrc)
     
         kernel_client = kernel_manager.client()
         kernel_client.start_channels()
