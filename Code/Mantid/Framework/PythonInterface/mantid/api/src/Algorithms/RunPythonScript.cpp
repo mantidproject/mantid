fb72c2375c708111c4cc7c09abd0d4e88aabc6f4
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-09-12 13:07:22 +0100

    Rewrite RunPythonScript in C++.
    This is a core algorithm for the LiveData so needs away from possible user modification
    and allows custom behaviour for WorkspaceGroups.
    Refs #7019

---
+++ Code/Mantid/Framework/PythonInterface/mantid/api/src/Algorithms/RunPythonScript.cpp
@@ -0,0 +1,217 @@
+/*WIKI*
+Algorithm that will run a snippet of python code.
+This is meant to be used by [[LoadLiveData]] to perform some processing.
+
+The input & output workspaces can be accessed from the Python code using the variable
+names 'input' & 'output' respectively.
+
+*WIKI*/
+
+#include "MantidPythonInterface/api/Algorithms/RunPythonScript.h"
+#include "MantidPythonInterface/kernel/Environment/Threading.h"
+#include "MantidKernel/MandatoryValidator.h"
+
+#include <boost/python/exec.hpp>
+#include <boost/python/extract.hpp>
+#include <boost/python/import.hpp>
+
+#include <Poco/RegularExpression.h>
+
+namespace Mantid
+{
+  namespace PythonInterface
+  {
+
+    /// Algorithm's name for identification. @see Algorithm::name
+    const std::string RunPythonScript::name() const { return "RunPythonScript";}
+
+    /// Algorithm's version for identification. @see Algorithm::version
+    int RunPythonScript::version() const { return 1;}
+
+    /// Algorithm's category for identification. @see Algorithm::category
+    const std::string RunPythonScript::category() const { return "DataHandling\\LiveData\\Support"; }
+
+    /// Sets documentation strings for this algorithm
+    void RunPythonScript::initDocs()
+    {
+      this->setWikiSummary("Executes a snippet of Python code");
+      this->setOptionalMessage("Executes a snippet of Python code");
+    }
+
+    /** 
+     * Override standard group behaviour so that the algorithm is only
+     * called once for the whole group
+     */
+    bool RunPythonScript::checkGroups()
+    {
+      return false;
+    }
+
+    /** 
+     * Initialize the algorithm's properties.
+     */
+    void RunPythonScript::init()
+    {
+      using namespace API;
+      using namespace Kernel;
+
+      declareProperty(new WorkspaceProperty<Workspace>("InputWorkspace","",Direction::Input, PropertyMode::Optional),
+          "An input workspace that the python code will modify."
+          "The workspace will be in the python variable named 'input'.");
+      declareProperty("Code", "", "Python code (can be on multiple lines).", 
+                      boost::make_shared<MandatoryValidator<std::string> >());
+      declareProperty(new WorkspaceProperty<Workspace>("OutputWorkspace","",Direction::Output, PropertyMode::Optional),
+          "An output workspace to be produced by the python code."
+          "The workspace will be in the python variable named 'output'.");
+    }
+
+    //----------------------------------------------------------------------------------------------
+    /** Execute the algorithm.
+     */
+    void RunPythonScript::exec()
+    {
+      using namespace API;
+
+      Workspace_sptr outputWS = executeScript(scriptCode());
+      setProperty<Workspace_sptr>("OutputWorkspace", outputWS);
+    }
+
+    /**
+     * Builds the code string from the user input. The user script is wrapped
+     * in a PyExec function to 'fool' the Python FrameworkManager into
+     * creating a child algorithm for each algorithm that is run. See
+     * PythonInterface/mantid/api/src/Exports/FrameworkManager.cpp
+     * This has to be the case to get the workspace locking correct.
+     *
+     * The code assumes that the scope in which it is executed has defined
+     * the variables input & output.
+     *
+     * @return A string containing the code ready to execute
+     */
+    std::string RunPythonScript::scriptCode() const
+    {
+      std::string userCode = getPropertyValue("Code");
+      // Unify line endings
+      Poco::RegularExpression nonUnixLineEndings("(\r\n|\r)", Poco::RegularExpression::RE_NEWLINE_ANYCRLF);
+      nonUnixLineEndings.subst(userCode, "\n", Poco::RegularExpression::RE_GLOBAL);
+
+      // Wrap and indent the user code (see method documentation)
+      std::istringstream is(userCode);
+      std::ostringstream os;
+      os << "from mantid.simpleapi import *\n"
+         << "def PyExec(input=None,output=None):\n";
+      std::string line;
+      while(getline(is, line))
+      {
+        os << "  " << line << "\n";
+      }
+      os << "  return input,output\n"; // When executed the global scope needs to know about input,output so we return them
+      os << "input,output = PyExec(input,output)";
+      return os.str();
+    }
+
+    /**
+     * Sets up the code context & executes it.
+     * A python dictionary of local attributes is setup to contain a reference to the input workspace
+     * & the output workspace. This together with the __main__ global dictionary defines the execution
+     * context
+     * @param script A string containing a read-to-execute script
+     * @return A pointer to the output workspace if one was generated. If one was not then this is an empty pointer
+     */
+    boost::shared_ptr<API::Workspace> RunPythonScript::executeScript(const std::string & script) const
+    {
+      using namespace API;
+      using namespace boost::python;
+
+      // Local context (no GIL required to build)
+      auto locals = buildLocals();
+      executeScript(script, locals);
+
+      return extractOutputWorkspace(locals);
+    }
+
+    /**
+     * Creates a Python dictionary containing definitions of the 'input' & 'output' variable
+     * references that the script may use
+     * @return A Python dictionary that can be used as the locals argument for the script execution
+     */
+    boost::python::dict RunPythonScript::buildLocals() const
+    {
+      // Define the local variable names required by the script, in this case
+      // - input: Points to input workspace if one has been given
+      // - output: Will point to the output workspace if one has been given
+      using namespace boost::python;
+
+      dict locals;
+      locals["input"] = object(); // default to None
+      locals["output"] = object();
+
+      API::Workspace_sptr inputWS = getProperty("InputWorkspace");
+      if(inputWS)
+      {
+        const converter::registration *entry = converter::registry::query(typeid(API::Workspace_sptr));
+        locals["input"] = object(handle<>(entry->to_python((const void *)&inputWS)));
+      }
+      std::string outputWSName = getPropertyValue("OutputWorkspace");
+      if(!outputWSName.empty())
+      {
+        locals["output"] = object(handle<>(to_python_value<const std::string&>()(outputWSName)));
+      }
+      return locals;
+    }
+
+    /**
+     * Uses the __main__ object to define the globals context and together with the given locals
+     * dictionary executes the script. The GIL is acquired and released during this call
+     * @param script The script code
+     * @param locals A dictionary defining the input & output variables (this may get modified)
+     * @return A pointer giving the output workspace
+     */
+    void RunPythonScript::executeScript(const std::string & script, boost::python::dict & locals) const
+    {
+      // Execution
+      Environment::GlobalInterpreterLock gil;
+      // Retrieve the main module.
+      auto main = boost::python::import("__main__");
+      // Retrieve the main module's namespace
+      boost::python::object globals(main.attr("__dict__"));
+      boost::python::exec(script.c_str(), globals, locals);
+    }
+
+    /**
+     * If an output workspace was created then extract it from the given dictionary
+     * @param locals A dictionary possibly containing an 'output' reference
+     * @return A pointer to the output workspace if created, otherwise an empty pointer
+     */
+    boost::shared_ptr<API::Workspace> RunPythonScript::extractOutputWorkspace(const boost::python::dict & locals) const
+    {
+      using namespace API;
+      using namespace boost::python;
+
+      // Might be None, string or a workspace object
+      object pyoutput = locals["output"];
+      if(pyoutput.is_none()) return Workspace_sptr();
+
+      extract<Workspace_sptr> workspaceExtractor(pyoutput);
+      if(workspaceExtractor.check())
+      {
+        return workspaceExtractor();
+      }
+      else
+      {
+        extract<std::string> stringExtractor(pyoutput);
+        if(stringExtractor.check())
+        {
+          // Will raise an error if the workspace does not exist as the user requested an output workspace
+          // but didn't create one.
+          return AnalysisDataService::Instance().retrieve(stringExtractor());
+        }
+        else
+        {
+          throw std::runtime_error("Invalid type assigned to 'output' variable. Must be a string or a Workspace object");
+        }
+      }
+    }
+
+  } // namespace PythonInterface
+} // namespace Mantid
---
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/api/RunPythonScriptTest.py
@@ -0,0 +1,92 @@
+import unittest
+import os
+
+from mantid.api import *
+from mantid.kernel import *
+from mantid.simpleapi import *
+
+
+class RunPythonScriptTest(unittest.TestCase):
+    """
+    Try out RunPythonScript
+    """
+    
+    def setUp(self):
+        CreateWorkspace(OutputWorkspace='ws',DataX='1,2,3,4',DataY='1,2,3',DataE='1,2,3')
+
+    def tearDown(self):
+        mtd.clear()
+
+    # ======================== Success cases =====================================================
+    
+    def test_Code_Without_Workspaces_Is_Successful_If_Code_Does_Not_Use_Workspace_References(self):
+        code = "x = 5 + 2"
+        RunPythonScript(Code=code)
+        
+    def test_simplePlus(self):
+        code = "Plus(LHSWorkspace=input, RHSWorkspace=input, OutputWorkspace=output)"
+        RunPythonScript(InputWorkspace="ws", Code=code, OutputWorkspace='ws_out')
+        ws_out = mtd['ws_out']
+        
+        self.assertAlmostEqual(ws_out.dataY(0)[0], 2.0, 3)
+        self.assertAlmostEqual(ws_out.dataY(0)[1], 4.0, 3)
+        self.assertAlmostEqual(ws_out.dataY(0)[2], 6.0, 3)
+
+    # Use an operation that sets 'output' to a workspace proxy
+    def test_usingOperators(self):
+        code = "output = input * 5.0"
+        RunPythonScript(InputWorkspace="ws", Code=code, OutputWorkspace='ws_out')
+        ws_out = mtd['ws_out']
+        
+        self.assertAlmostEqual(ws_out.dataY(0)[0], 5.0, 3)
+        self.assertAlmostEqual(ws_out.dataY(0)[1],10.0, 3)
+        self.assertAlmostEqual(ws_out.dataY(0)[2],15.0, 3)
+
+    # Properties handle MDWorkspace types.
+    def test_withMDWorkspace(self):
+        CreateMDWorkspace(OutputWorkspace="ws", Dimensions='1', Extents='-10,10', Names='x', Units='m')
+        code = "output = input"
+        RunPythonScript(InputWorkspace="ws", Code=code, OutputWorkspace='ws_out')
+        ws_out = mtd['ws_out']
+        self.assertTrue(isinstance(ws_out, IMDWorkspace))
+        
+    def test_withNoInputWorkspace(self):
+        c = RunPythonScript(Code="output = CreateSingleValuedWorkspace(DataValue='1')")
+        self.assertEqual(c.readY(0)[0], 1)
+
+    def test_algorithm_executes_once_for_whole_input_group_and_not_once_per_group_member(self):
+        DeleteWorkspace('ws')
+        CreateWorkspace(OutputWorkspace='ws_1',DataX='1,2,3,4',DataY='1,2,3',DataE='1,2,3')
+        CreateWorkspace(OutputWorkspace='ws_2',DataX='1,2,3,4',DataY='1,2,3',DataE='1,2,3')
+        GroupWorkspaces(InputWorkspaces='ws_1,ws_2',OutputWorkspace='ws')
+        self.assertEquals(3,mtd.size())
+
+        code = "Scale(input,OutputWorkspace=output,Factor=5)"
+        RunPythonScript(InputWorkspace='ws',Code=code,OutputWorkspace='ws')
+
+        self.assertEquals(3,mtd.size())
+        group = mtd['ws']
+        self.assertTrue(isinstance(group, WorkspaceGroup))
+        self.assertAlmostEqual(5.0, mtd['ws_1'].readY(0)[0], 8)
+
+    def test_code_with_a_mixture_of_line_endings_succeeds(self):
+        code = "Scale(input,OutputWorkspace=output,Factor=5)\n"
+        code += "Scale(input,OutputWorkspace=output,Factor=10,Operation='Add')\r"
+        code += "Scale(input,OutputWorkspace=output,Factor=20)\r\n"
+
+        RunPythonScript(InputWorkspace='ws',Code=code,OutputWorkspace='ws')
+        ws = mtd['ws']
+        self.assertAlmostEqual(300.0, mtd['ws'].readY(0)[0], 8)
+
+    # ======================== Failure cases =====================================================
+    def test_Code_Without_Workspaces_Fails_If_Code_Uses_Workspace_References(self):
+        code = "output = input*5"
+        self.assertRaises(TypeError,RunPythonScript,Code=code)
+
+    def test_Code_Not_Creating_Output_Workspace_Fails_If_OutputWS_Is_Specified(self):
+        code = "x = 5"
+        self.assertRaises(RuntimeError, RunPythonScript, Code=code, OutputWorkspace='ws_out')
+
+if __name__ == '__main__':
+    unittest.main()
+
