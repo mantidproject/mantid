62d8717427c98f20c6ab0bb89d4904a04792579a
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-09-16 12:11:15 +0100

    Remove PythonAPI directory and most references
    The link to the system installed/3rd party version of boost python
    is now restored.
    
    This required an additional helper function: isNone. boost python
    only introduced object::is_none in version 1.43 so the helper
    uses it if it is present but falls back to checking manually if not.
    
    Refs #7607

---
+++ Code/Mantid/Framework/PythonInterface/inc/MantidPythonInterface/kernel/IsNone.h
@@ -0,0 +1,43 @@
+#ifndef MANTID_PYTHONINTERFACE_KERNEL_ISNONE_H_
+#define MANTID_PYTHONINTERFACE_KERNEL_ISNONE_H_
+
+#include <boost/python/object.hpp>
+
+/**
+ * Defines a helper function to check whether an object is of Python
+ * type None.
+ * 
+ * The boost::python::object method is_none was only
+ * added in version 1.43 and we still support some versions
+ * prior to this
+ */
+namespace Mantid
+{
+  namespace PythonInterface
+  {
+
+    /**
+     * @param ptr A * to a raw PyObject
+     * @returns true if the given object is of type None
+     */
+    inline bool isNone(PyObject * ptr)
+    {
+      return (ptr == Py_None);
+    }
+
+    /**
+     * @param obj A const reference to boost python object wrapper
+     * @returns true if the given boost python object is of type None
+     */
+    inline bool isNone(const boost::python::object & obj)
+    {
+#ifdef BOOST_PYTHON_OBJECT_HAS_IS_NONE
+      return obj.is_none();
+#else
+      return isNone(obj.ptr());
+#endif
+    }
+  }
+}
+
+#endif /* #ifndef MANTID_PYTHONINTERFACE_KERNEL_ISNONE_H_ */
