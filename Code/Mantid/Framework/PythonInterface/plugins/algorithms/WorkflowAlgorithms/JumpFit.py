45e473bc3ce3ee1e2cf8a9c76d14fa5e5dc899c1
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2013-09-18 12:26:41 +0100

    Refs #7966 Updated JumpFit to use Fit algorithm.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/JumpFit.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/JumpFit.py
@@ -3,7 +3,8 @@ from mantid.api import PythonAlgorithm, AlgorithmFactory
 from mantid.kernel import StringListValidator, StringMandatoryValidator
 from mantid.simpleapi import *
 from mantid import config, logger, mtd
-import os
+import os.path
+from IndirectBayes import JumpRun
 
 class JumpFit(PythonAlgorithm):
  
@@ -16,7 +17,7 @@ class JumpFit(PythonAlgorithm):
 		self.declareProperty(name='Analyser',defaultValue='graphite002',validator=StringListValidator(['graphite002','graphite004']), doc='Analyser & reflection')
 		self.declareProperty(name='QLprogram',defaultValue='QLr',validator=StringListValidator(['QLr','QLd']), doc='QL output from Res or Data')
 		self.declareProperty(name='Fit',defaultValue='CE',validator=StringListValidator(['CE','SS']), doc='Chudley-Elliott or Singwi-Sjolander')
-		self.declareProperty(name='Width',defaultValue='FW11',validator=StringListValidator(['FW11','FW21','FW22']), doc='FullWidth type')
+		self.declareProperty(name='Width',defaultValue='width11',validator=StringListValidator(['width11','width21','width22']), doc='FullWidth type')
 		self.declareProperty(name='SamNumber',defaultValue='',validator=StringMandatoryValidator(), doc='Sample run number')
 		self.declareProperty(name='CropQ',defaultValue=False, doc='Switch CropQ Off/On')
 		self.declareProperty(name='Qmin',defaultValue='', doc='Qmin for cropping')
@@ -26,12 +27,6 @@ class JumpFit(PythonAlgorithm):
 		self.declareProperty(name='Save',defaultValue=False, doc='Switch Save result to nxs file Off/On')
  
 	def PyExec(self):
-                from IndirectImport import run_f2py_compatibility_test, is_supported_f2py_platform
-
-                if is_supported_f2py_platform():
-                        import IndirectBayes as Main
-
-		run_f2py_compatibility_test()
 		
 		self.log().information('Jump input')
 		inType = self.getPropertyValue('InputType')
@@ -39,30 +34,52 @@ class JumpFit(PythonAlgorithm):
 		ana = self.getPropertyValue('Analyser')
 		prog = self.getPropertyValue('QLprogram')
 		jump = self.getPropertyValue('Fit')
-		fw = self.getPropertyValue('Width')
+		width = self.getPropertyValue('Width')
+		if width == 'width11':
+			index = 0
+		if width == 'width21':
+			index = 2
+		if width == 'width22':
+			index = 4
 		sam = self.getPropertyValue('SamNumber')
 
-		sname = prefix+sam+'_'+ana+'_'+prog
+		samWS = prefix+sam+'_'+ana+'_'+prog
 		cropOp = self.getProperty('CropQ').value
-		if cropOp:
-			qmin = self.getPropertyValue('Qmin')
-			qmax = self.getPropertyValue('Qmax')
-			qrange = [qmin, qmax]
-		else:
-			qrange = [0.0, 5.0]
 		verbOp = self.getProperty('Verbose').value
 		plotOp = self.getProperty('Plot').value
 		saveOp = self.getProperty('Save').value
 
 		workdir = config['defaultsave.directory']
 		if inType == 'File':
-			path = os.path.join(workdir, sname+'_Parameters.nxs')					# path name for nxs file
-			LoadNexusProcessed(Filename=path, OutputWorkspace=sname+'_Parameters')
+			path = os.path.join(workdir, samWS+'_Workspace.nxs')					# path name for nxs file
+			LoadNexusProcessed(Filename=path, OutputWorkspace=samWS)
 			message = 'Input from File : '+path
 		else:
-			message = 'Input from Workspace : '+sname
+			message = 'Input from Workspace : '+samWS
+		ExtractSingleSpectrum(InputWorkspace=samWS, OutputWorkspace=samWS, WorkspaceIndex=index)		
+		inGR = mtd[samWS].getRun()
+		val = inGR.getLogData('Fit Program').value
 		if verbOp:
 			logger.notice(message)
-		Main.JumpRun(sname,jump,prog,fw,cropOp,qrange,verbOp,plotOp,saveOp)
+			logger.notice('Fit program was : '+val)
+		x = mtd[samWS].readX(0)
+		xmin = x[0]
+		xmax = x[len(x)-1]
+		if cropOp:
+			Crop = False
+			qmin = self.getPropertyValue('Qmin')
+			qmin = float(qmin)
+			qmax = self.getPropertyValue('Qmax')
+			qmax = float(qmax)
+			if qmin > xmin:
+				Crop = True
+			if qmax < xmax:
+				Crop = True
+			if Crop:
+				CropWorkspace(InputWorkspace=samWS, OutputWorkspace=samWS,
+					XMin=qmin, XMax=qmax)
+				if verbOp:
+					logger.notice('Cropping from Q= ' + str(qmin) +' to '+ str(qmax))
+		JumpRun(samWS,jump,verbOp,plotOp,saveOp)
 
 AlgorithmFactory.subscribe(JumpFit)         # Register algorithm with Mantid
