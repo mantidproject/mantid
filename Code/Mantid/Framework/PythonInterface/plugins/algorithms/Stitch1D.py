9ef471367613700f8f5ff56a94f5a627caf3e9b5
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-09-20 13:08:04 +0100

    refs #7709. Stitching seems to be working ok.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/Stitch1D.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/Stitch1D.py
@@ -3,7 +3,7 @@
 Stitches single histogram [[MatrixWorkspace|Matrix Workspaces]] together outputing a stitched Matrix Workspace. This algorithm is a wrapper over [[Stitch1DMD]].
 
 *WIKI*"""
-import mantid.simpleapi as api
+from mantid.simpleapi import *
 
 from mantid.api import *
 from mantid.kernel import *
@@ -24,10 +24,9 @@ class Stitch1D(PythonAlgorithm):
         
         overlap_validator = FloatMandatoryValidator() 
 
-        self.declareProperty(name="StartOverlap", defaultValue=-1.0, validator=overlap_validator, doc="Overlap in Q.");
-        self.declareProperty(name="EndOverlap", defaultValue=-1.0, validator=overlap_validator, doc="End overlap in Q.");
-        self.declareProperty(name="Qmin", defaultValue=-1.0, doc="Minimum Q of output workspace. This is optional.")
-        self.declareProperty(name="Qmax", defaultValue=-1.0, doc="Maximum Q of output workspace. This is optional.")
+        self.declareProperty(name="StartOverlap", defaultValue=-1.0, validator=overlap_validator, doc="Overlap in Q.")
+        self.declareProperty(name="EndOverlap", defaultValue=-1.0, validator=overlap_validator, doc="End overlap in Q.")
+        self.declareProperty(name="Params", defaultValue="0.1", doc="Rebinning Parameters. See Rebin for format.")
         self.declareProperty(name="ScaleRHSWorkspace", defaultValue=True, doc="Scaling either with respect to workspace 1 or workspace 2.")
         self.declareProperty(name="UseManualScaleFactor", defaultValue=False, doc="True to use a provided value for the scale factor.")
         self.declareProperty(name="ManualScaleFactor", defaultValue=1.0, doc="Provided value for the scale factor.")
@@ -45,18 +44,60 @@ class Stitch1D(PythonAlgorithm):
         useManualScaleFactor = self.getProperty('UseManualScaleFactor').value
         manualScaleFactor = self.getProperty('ManualScaleFactor').value
         outScaleFactor = self.getProperty('OutScaleFactor').value
-        qMin = self.getProperty('Qmin').value
-        qMax = self.getProperty('Qmax').value
-       
-        factory = api.WorkspaceFactoryImpl.Instance()
-        outws = factory.create("Workspace2D", NVectors=1, XLength=len(x), YLength=len(y))
-        outws.setX(0, x)
-        outws.setY(0, y)
-        outws.setE(0, e)
-        outws.getAxis(0).setUnit("MomentumTransfer")
+        params = self.getProperty("Params").value
         
-        self.setProperty('OutputWorkspace', outws)
-        self.setProperty('OutScaleFactor', out_properties[1])
+        lhs_rebinned = Rebin(InputWorkspace=lhs_workspace, Params=params)
+        rhs_rebinned = Rebin(InputWorkspace=rhs_workspace, Params=params)
+        
+        a1=lhs_rebinned.binIndexOf(startOverlap)
+        a2=lhs_rebinned.binIndexOf(endOverlap)
+    
+        if not useManualScaleFactor:
+            lhsOverlapIntegrated = Integration(InputWorkspace=lhs_rebinned, RangeLower=startOverlap, RangeUpper=endOverlap)
+            rhsOverlapIntegrated = Integration(InputWorkspace=rhs_rebinned, RangeLower=startOverlap, RangeUpper=endOverlap)
+            if scaleRHSWorkspace:
+                rhs_rebinned *= (lhsOverlapIntegrated/rhsOverlapIntegrated)
+            else:
+                lhs_rebined *= (rhsOverlapIntegrated/lhsOverlapIntegrated)
+            
+            y1=lhsOverlapIntegrated.readY(0)
+            y2=rhsOverlapIntegrated.readY(0)
+            scalefactor = y1[0]/y2[0]
+        else:
+            rhs_rebinned = MultiplyRange(InputWorkspace=w2, StartBin=0, Factor=manualScaleFactor)
+            scalefactor = manualScaleFactor
+        
+        # Mask out everything BUT the overlap region as a new workspace.
+        overlap1 = MultiplyRange(InputWorkspace=lhs_rebinned, StartBin=0,EndBin=a1-1,Factor=0)
+        overlap1 = MultiplyRange(InputWorkspace=overlap1,StartBin=a2,Factor=0)
+    
+        # Mask out everything BUT the overlap region as a new workspace.
+        overlap2 = MultiplyRange(InputWorkspace=rhs_rebinned,StartBin=0,EndBin=a1,Factor=0)#-1
+        overlap2 = MultiplyRange(InputWorkspace=overlap2,StartBin=a2+1,Factor=0)
+    
+        # Mask out everything AFTER the start of the overlap region
+        lhs_rebinned=MultiplyRange(InputWorkspace=lhs_rebinned,StartBin=a1, Factor=0)
+        # Mask out everything BEFORE the end of the overlap region
+        rhs_rebinned=MultiplyRange(InputWorkspace=rhs_rebinned,StartBin=0, EndBin=a2,Factor=0)
+        
+        # Calculate a weighted mean for the overlap region
+        overlapave = WeightedMean(InputWorkspace1=overlap1,InputWorkspace2=overlap2)
+        # Add the Three masked workspaces together to create a complete x-range
+        result = lhs_rebinned + overlapave + rhs_rebinned
+        
+        # Cleanup
+        '''
+        DeleteWorkspace(w1)
+        DeleteWorkspace(w2)
+        DeleteWorkspace(w1_overlap_int)
+        DeleteWorkspace(w2_overlap_int)
+        DeleteWorkspace(overlap1)
+        DeleteWorkspace(overlap2)
+        DeleteWorkspace(overlapave)
+        '''
+        self.setProperty('OutputWorkspace', result)
+        self.setProperty('OutScaleFactor', scalefactor)
+        
         return None
         
 
