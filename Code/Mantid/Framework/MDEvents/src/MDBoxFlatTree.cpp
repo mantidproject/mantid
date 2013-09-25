c57be2cf00d41717edd2ad6d056d867c464fe3be
Alex Buts <Alex.Buts@stfc.ac.uk>
2013-09-25 16:15:00 +0100

    refs #5872 Increased accuracy of Box tree loaders and
    modify the CompareMDWorkspaces to check reller rather then abserr on large data.
    
    This is because system test requesting 10-5 accuracy was failing on comparing boxes volumes.
    
    The ultimate solution would be the changes in MDBox structure but it is subject to another ticket.

--- Code/Mantid/Framework/MDEvents/src/MDBoxFlatTree.cpp
+++ Code/Mantid/Framework/MDEvents/src/MDBoxFlatTree.cpp
@@ -498,7 +498,7 @@ namespace Mantid
       ibox->calcVolume();
       double vol = m_InverseVolume[i];
       if(vol<=FLT_EPSILON)vol=1;
-      if(std::fabs((ibox->getInverseVolume()-vol)/vol)>1.e-4)
+      if(std::fabs((ibox->getInverseVolume()-vol)/vol)>1.e-5)
       {
           g_log.debug()<<" Accuracy warning for box N "<<i<<" as stored inverse volume is : "<<m_InverseVolume[i]<<" and calculated from extents: "<<ibox->getInverseVolume()<<std::endl;
           ibox->setInverseVolume(coord_t(m_InverseVolume[i]));
