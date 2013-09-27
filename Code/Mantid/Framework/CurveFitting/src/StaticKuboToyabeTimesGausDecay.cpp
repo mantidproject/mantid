e9d941b9aa159901b7d4af40007acb85cd208c5e
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2013-09-27 10:51:07 +0100

    Added the function and test for it.
    Refs #7756

---
+++ Code/Mantid/Framework/CurveFitting/src/StaticKuboToyabeTimesGausDecay.cpp
@@ -0,0 +1,47 @@
+/*WIKI*
+Fitting function for use by Muon scientists defined by:
+
+<math> \mbox{A}\times ( \exp(-{Delta}^2 \times {x}^2 / 2 ) \times ( 1 - ( {Delta}^2 \times {x}^2 ) ) \times  \frac 2 3 + \frac 1 3 ) \times \exp(-{Sigma}^2 \times {x}^2 ) </math>
+ *WIKI*/
+#include "MantidCurveFitting/StaticKuboToyabeTimesGausDecay.h"
+#include "MantidAPI/FunctionFactory.h"
+#include <cmath>
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+  using namespace Kernel;
+  using namespace API;
+  
+  DECLARE_FUNCTION(StaticKuboToyabeTimesGausDecay)
+  
+  void StaticKuboToyabeTimesGausDecay::init() 
+  {
+    declareParameter("A", 0.2, "Amplitude at time 0");
+    declareParameter("Delta", 0.2, "Decay rate");
+    declareParameter("Sigma", 0.2, "Decay rate");
+  }
+  
+  void StaticKuboToyabeTimesGausDecay::function1D(double* out, const double* xValues, const size_t nData) const
+  {
+    const double A = getParameter("A"); 
+    const double D = getParameter("Delta"); 
+    const double S = getParameter("Sigma");
+
+    // Precalculate squares
+    const double D2 = pow(D, 2);
+    const double S2 = pow(S, 2);
+  
+    // Precalculate constants
+    const double C1 = 2.0/3;
+    const double C2 = 1.0/3;
+  
+    for (size_t i = 0; i < nData; i++) {
+      double x2 = pow(xValues[i], 2);
+      out[i] = A*(exp(-(x2*D2)/2)*(1-x2*D2)*C1 + C2)*exp(-S2*x2);
+    } 
+  
+}
+} // namespace CurveFitting
+} // namespace Mantid
