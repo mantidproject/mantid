945c8d4d4b9a23474350cf08b0e924974247ed8b
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2013-09-27 09:33:57 +0100

    Added the function and test for it.
    Refs #7755

---
+++ Code/Mantid/Framework/CurveFitting/src/StaticKuboToyabeTimesExpDecay.cpp
@@ -0,0 +1,40 @@
+#include "MantidCurveFitting/StaticKuboToyabeTimesExpDecay.h"
+#include "MantidAPI/FunctionFactory.h"
+#include <cmath>
+
+namespace Mantid
+{
+namespace CurveFitting
+{
+  
+using namespace Kernel;
+using namespace API;
+
+DECLARE_FUNCTION(StaticKuboToyabeTimesExpDecay)
+
+void StaticKuboToyabeTimesExpDecay::init() 
+{
+  declareParameter("A", 0.2, "Amplitude at time 0");
+  declareParameter("Delta", 0.2, "Decay rate");
+  declareParameter("Lambda", 0.2, "Decay rate");
+}
+
+void StaticKuboToyabeTimesExpDecay::function1D(double* out, const double* xValues, const size_t nData) const
+{
+  const double A = getParameter("A"); 
+  const double D = getParameter("Delta"); 
+  const double L = getParameter("Lambda");
+
+  const double C1 = 2.0/3;
+  const double C2 = 1.0/3;
+
+  for (size_t i = 0; i < nData; i++) {
+    double x = xValues[i];
+    double DSquared = pow(D*x,2);
+    out[i] = A*(exp(-DSquared/2)*(1-DSquared)*C1 + C2)*exp(-L*x);
+  } 
+
+}
+
+} // namespace CurveFitting
+} // namespace Mantid
