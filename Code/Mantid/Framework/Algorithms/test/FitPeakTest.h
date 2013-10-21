0a32556326cc1cdcf42b7d55a760619ddc8532dc
Wenduo Zhou <zhouw@ornl.gov>
2013-10-21 16:32:23 -0400

    Added 1 more unit test.  Refs #7788.

--- Code/Mantid/Framework/Algorithms/test/FitPeakTest.h
+++ Code/Mantid/Framework/Algorithms/test/FitPeakTest.h
@@ -293,46 +293,29 @@ public:
   //----------------------------------------------------------------------------------------------
   /** Test on fit a peak with 1 step
     */
-  void Xtest_FitPeakOneStep()
+  void test_FitPeakOneStep()
   {
     // Generate input workspace
-    // FIXME - Find a new set of data!
-    MatrixWorkspace_sptr dataws = gen_4866P5Data();
-    AnalysisDataService::Instance().addOrReplace("PG3_4866Peak5", dataws);
-
-    // Generate peak and background parameters
-    vector<string> peakparnames, bkgdparnames;
-    vector<double> peakparvalues, bkgdparvalues;
-
-    gen_linearBkgdParameters(bkgdparnames, bkgdparvalues);
-    gen_PeakParameters(peakparnames, peakparvalues);
-
-#if 0
-    const MantidVec& vecx = dataws->readX(0);
-    const MantidVec& vecy = dataws->readY(0);
-    for (size_t i = 0; i < vecx.size(); ++i)
-      cout << vecx[i] << "\t\t" << vecy[i] << "\n";
-    TS_ASSERT_EQUALS(1, 100);
-    return;
-#endif
+    MatrixWorkspace_sptr dataws = gen_PG3DiamondData();
+    AnalysisDataService::Instance().addOrReplace("PG3_Si_Peak", dataws);
 
     // Initialize FitPeak
     FitPeak fitpeak;
     fitpeak.initialize();
 
     // Set up properties
-    fitpeak.setPropertyValue("InputWorkspace", "PG3_4866Peak5");
-    fitpeak.setPropertyValue("OutputWorkspace", "FittedPeak");
-    fitpeak.setPropertyValue("ParameterTableWorkspace", "Fitted_Peak5_Parameters");
+    fitpeak.setPropertyValue("InputWorkspace", "PG3_Si_Peak");
+    fitpeak.setPropertyValue("OutputWorkspace", "FittedPeak2");
+    fitpeak.setPropertyValue("ParameterTableWorkspace", "Fitted_Si_Parameters");
     fitpeak.setProperty("WorkspaceIndex", 0);
     TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("PeakFunctionType", "Gaussian"));
-    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("PeakParameterNames", peakparnames));
-    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("PeakParameterValues", peakparvalues));
+    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("PeakParameterNames", "Height, PeakCentre, Sigma"));
+    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("PeakParameterValues", "40.0, 2.0658, 0.001"));
     TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("BackgroundType", "Linear"));
-    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("BackgroundParameterNames", bkgdparnames));
-    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("BackgroundParameterValues", bkgdparvalues));
-    fitpeak.setPropertyValue("FitWindow", "0.586, 0.604");
-    fitpeak.setPropertyValue("PeakRange", "0.591, 0.597");
+    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("BackgroundParameterNames", "A0, A1"));
+    TS_ASSERT_THROWS_NOTHING(fitpeak.setProperty("BackgroundParameterValues", "0.5, 0.0"));
+    fitpeak.setPropertyValue("FitWindow", "2.051, 2.077");
+    fitpeak.setPropertyValue("PeakRange", "2.055, 2.08");
     fitpeak.setProperty("FitBackgroundFirst", false);
     fitpeak.setProperty("RawParams", true);
 
@@ -348,10 +331,96 @@ public:
     TS_ASSERT_EQUALS(fittedbkgdvalues.size(), 2);
 
     double peakheight = fittedpeakvalues[0];
-    TS_ASSERT_DELTA(peakheight, 1200., 200.);
+    TS_ASSERT_DELTA(peakheight, 26., 1.);
 
+    // Clean
+    AnalysisDataService::Instance().remove("PG3_Si_Peak");
+    AnalysisDataService::Instance().remove("FittedPeak2");
+    AnalysisDataService::Instance().remove("Fitted_Si_Parameters");
 
     return;
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Generate a workspace contains PG3_4866 5-th peak
+    */
+  MatrixWorkspace_sptr gen_PG3DiamondData()
+  {
+    vector<double> vecx, vecy, vece;
+
+    vecx.push_back(2.050678);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.051498);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.052319);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.053140);    vecy.push_back(2.000000 ); vece.push_back(1.41421E+00);
+    vecx.push_back(2.053961);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.054783);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.055605);    vecy.push_back(2.000000 ); vece.push_back(1.41421E+00);
+    vecx.push_back(2.056427);    vecy.push_back(2.000000 ); vece.push_back(1.41421E+00);
+    vecx.push_back(2.057250);    vecy.push_back(3.000000 ); vece.push_back(1.73205E+00);
+    vecx.push_back(2.058072);    vecy.push_back(4.000000 ); vece.push_back(2.00000E+00);
+    vecx.push_back(2.058896);    vecy.push_back(5.000000 ); vece.push_back(2.23607E+00);
+    vecx.push_back(2.059719);    vecy.push_back(16.000000); vece.push_back(4.00000E+00);
+    vecx.push_back(2.060543);    vecy.push_back(20.000000); vece.push_back(4.47214E+00);
+    vecx.push_back(2.061367);    vecy.push_back(31.000000); vece.push_back(5.56776E+00);
+    vecx.push_back(2.062192);    vecy.push_back(26.000000); vece.push_back(5.09902E+00);
+    vecx.push_back(2.063017);    vecy.push_back(28.000000); vece.push_back(5.29150E+00);
+    vecx.push_back(2.063842);    vecy.push_back(29.000000); vece.push_back(5.38516E+00);
+    vecx.push_back(2.064668);    vecy.push_back(41.000000); vece.push_back(6.40312E+00);
+    vecx.push_back(2.065493);    vecy.push_back(40.000000); vece.push_back(6.32456E+00);
+    vecx.push_back(2.066320);    vecy.push_back(38.000000); vece.push_back(6.16441E+00);
+    vecx.push_back(2.067146);    vecy.push_back(40.000000); vece.push_back(6.32456E+00);
+    vecx.push_back(2.067973);    vecy.push_back(34.000000); vece.push_back(5.83095E+00);
+    vecx.push_back(2.068800);    vecy.push_back(35.000000); vece.push_back(5.91608E+00);
+    vecx.push_back(2.069628);    vecy.push_back(18.000000); vece.push_back(4.24264E+00);
+    vecx.push_back(2.070456);    vecy.push_back(21.000000); vece.push_back(4.58258E+00);
+    vecx.push_back(2.071284);    vecy.push_back(9.000000 ); vece.push_back(3.00000E+00);
+    vecx.push_back(2.072112);    vecy.push_back(6.000000 ); vece.push_back(2.44949E+00);
+    vecx.push_back(2.072941);    vecy.push_back(6.000000 ); vece.push_back(2.44949E+00);
+    vecx.push_back(2.073770);    vecy.push_back(11.000000); vece.push_back(3.31662E+00);
+    vecx.push_back(2.074600);    vecy.push_back(10.000000); vece.push_back(3.16228E+00);
+    vecx.push_back(2.075430);    vecy.push_back(4.000000 ); vece.push_back(2.00000E+00);
+    vecx.push_back(2.076260);    vecy.push_back(7.000000 ); vece.push_back(2.64575E+00);
+    vecx.push_back(2.077090);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.077921);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.078752);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.079584);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.080416);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.081248);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.082080);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.082913);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.083746);    vecy.push_back(2.000000 ); vece.push_back(1.41421E+00);
+    vecx.push_back(2.084580);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.085414);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.086248);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.087082);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.087917);    vecy.push_back(1.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.088752);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.089588);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.090424);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.091260);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.092096);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.092933);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+    vecx.push_back(2.093770);    vecy.push_back(0.000000 ); vece.push_back(1.00000E+00);
+
+    size_t NVectors = 1;
+    size_t sizex = vecx.size();
+    size_t sizey = vecy.size();
+    MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>(
+            WorkspaceFactory::Instance().create("Workspace2D", NVectors, sizex, sizey));
+
+    MantidVec& vecX = ws->dataX(0);
+    MantidVec& vecY = ws->dataY(0);
+    MantidVec& vecE = ws->dataE(0);
+
+    for (size_t i = 0; i < sizex; ++i)
+        vecX[i] = vecx[i];
+    for (size_t i = 0; i < sizey; ++i)
+    {
+        vecY[i] = vecy[i];
+        vecE[i] = vece[i];
+    }
+
+    return ws;
   }
 
   //----------------------------------------------------------------------------------------------
