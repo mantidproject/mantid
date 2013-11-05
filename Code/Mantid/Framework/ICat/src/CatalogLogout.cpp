6048e15f8716a911203876c8c6921d474918dffe
Jay Rainey <jawrainey@gmail.com>
2013-11-05 10:46:06 +0000

    Refactored catalog logout algorithm. Refs #8322.
    - Removed unimplemented method `doLogout()`.

--- Code/Mantid/Framework/ICat/src/CatalogLogout.cpp
+++ Code/Mantid/Framework/ICat/src/CatalogLogout.cpp
@@ -5,17 +5,12 @@ This algorithm disconnects the logged in user from the information catalog.
 *WIKI*/
 
 #include "MantidICat/CatalogLogout.h"
-#include "MantidAPI/CatalogFactory.h"
-#include "MantidKernel/ConfigService.h"
-#include "MantidKernel/FacilityInfo.h"
-#include "MantidAPI/ICatalog.h"
+#include "MantidICat/CatalogAlgorithmHelper.h"
 
 namespace Mantid
 {
   namespace ICat
   {
-    using namespace Kernel;
-    using namespace API;
     DECLARE_ALGORITHM(CatalogLogout)
 
     /// Sets documentation strings for this algorithm
@@ -26,29 +21,13 @@ namespace Mantid
     }
 
     /// Init method to declare algorithm properties
-    void CatalogLogout::init()
-    {
-    }
+    void CatalogLogout::init() {}
 
     /// execute the algorithm
     void CatalogLogout::exec()
     {
-      ICatalog_sptr catalog_sptr;
-      try
-      {
-        catalog_sptr=CatalogFactory::Instance().create(ConfigService::Instance().getFacility().catalogInfo().catalogName());
-      }
-      catch(Kernel::Exception::NotFoundError&)
-      {
-        throw std::runtime_error("Error when getting the catalog information from the Facilities.xml file.");
-      }
-      if(!catalog_sptr)
-      {
-        throw std::runtime_error("Error when getting the catalog information from the Facilities.xml file");
-      }
-      catalog_sptr->logout();
-
+      API::ICatalog_sptr catalog = CatalogAlgorithmHelper().createCatalog();
+      catalog->logout();
     }
-
   }
 }
