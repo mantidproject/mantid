7b3f698e4814bd394be10ecadedf118549d9c1e1
Jose Borreguero <borreguero@gmail.com>
2013-11-05 15:10:12 -0500

    Refs #8373 First pass at the unit test
    modified: StretchedExpFT.py
    new file:   StretchedExpFTTest.py

--- Code/Mantid/Framework/PythonInterface/plugins/functions/StretchedExpFT.py
+++ Code/Mantid/Framework/PythonInterface/plugins/functions/StretchedExpFT.py
@@ -1,4 +1,4 @@
-"""
+'''
 Provide the Fourier Transform of the Symmetrized Stretched Exponential Function
   Fourier{ height * exp( - |t/tau|**beta ) }
     
@@ -24,26 +24,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 File change history is stored at: <https://github.com/mantidproject/mantid>
 Code Documentation is available at: <http://doxygen.mantidproject.org>
-"""
-from mantid.api import IFunction1D, FunctionFactory
+'''
+from mantid.api import IFunction1D, FunctionFactory #, BoundaryConstraint
 from mantid import logger
-from numpy import exp
+import numpy as np
 from scipy.fftpack import fft
 
 class StretchedExpFT(IFunction1D):
     
     def category(self):
-        return "QENS"
+        return 'QENS'
 
     def init(self):
-        """Declare parameters that participate in the fitting"""
+        '''Declare parameters that participate in the fitting'''
         # Active fitting parameters
-        self.declareParameter("height", 1.0, "Intensity at the origin")
-        self.declareParameter("tau", 1.0, "Relaxation time of the standard exponential")
-        self.declareParameter("beta",1.0, "Stretching exponent")
+        self.declareParameter('height', 1.0, 'Intensity at the origin')
+        self.declareParameter('tau', 1.0, 'Relaxation time of the standard exponential')
+        self.declareParameter('beta',1.0, 'Stretching exponent')
+        #self.addConstraints() # constraints not yet exposed to python
+        self.validateParams()
+    
+    '''
+    def addConstraints(self):
+        for name in ('height', 'tau', 'beta'):
+            constraint = BoundaryConstraint(self, name, 0, True)
+            self.addConstraint(constraint);
+    '''
+
+    def validateParams(self,):
+        '''Check parameters are positive'''
+        height = self.getParameterValue('height')
+        tau = self.getParameterValue('tau')
+        beta = self.getParameterValue('beta')
+        for name,parm in {'height':height, 'tau':tau, 'beta':beta}.items():
+            if parm <=0:
+                message = 'Parameter {} in StretchedExpFT must be positive'.format(name)
+                logger.error(message)
+                raise ValueError(message)
+        return {'height':height, 'tau':tau, 'beta':beta}
        
     def function1D(self, xvals):
-        """
+        '''
         Computes the Fourier transform of the Symmetrized Stretched Exponential
         
         The Symmetrized Stretched Exponential:
@@ -51,22 +72,17 @@ class StretchedExpFT(IFunction1D):
         Its Fourier transform is real by definition, thus we return the real part
         of the Fast Fourier Transform (FFT). The FFT step is meant to produce
         some residual imaginary part due to numerical inaccuracies which we ignore
-        """
-        height = self.getParameterValue("height")
-        tau = self.getParameterValue("tau")
-        beta = self.getParameterValue("beta")
-        if beta <= 0:
-            logger.error("Parameter beta in StretchedExpFT must be positive")
-            raise ValueError()
-        exponent = -(abs(xvals)/tau)**beta
-        return fft( exp(exponent) ).real
+        '''
+        p=self.validateParams()
+        exponent = -(np.abs(xvals)/p['tau'])**p['beta']
+        return p['height']*fft( np.exp(exponent) ).real
     
-    """The derivative of the symmetrized stretched exponential is ill-defined at
+    '''The derivative of the symmetrized stretched exponential is ill-defined at
     the origin. Thus, we prefer the numerical derivative which will return a
     near-zero derivative close to the origin
     def functionDeriv1D(self, xvals, jacobian):
         pass
-    """
+    '''
 
 # Required to have Mantid recognise the new function
 FunctionFactory.subscribe(StretchedExpFT)
---
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/functions/StretchedExpFTTest.py
@@ -0,0 +1,72 @@
+import unittest
+from mantid.kernel import *
+from mantid.api import *
+from mantid.simpleapi import Fit
+import testhelpers
+
+
+class _InternalMakeSEFTData(PythonAlgorithm):
+
+    def PyInit(self):
+        self.declareProperty('height', 1.0, validator=FloatBoundedValidator(lower=0))
+        self.declareProperty('tau',    1.0, validator=FloatBoundedValidator(lower=0))
+        self.declareProperty('beta',   1.0, validator=FloatBoundedValidator(lower=0))
+        self.declareProperty('nhalfbins', 100)
+        self.declareProperty(MatrixWorkspaceProperty('OutputWorkspace', '', direction = Direction.Output))
+        
+    def PyExec(self):
+        import numpy as np
+        from scipy.fftpack import fft
+        
+        nhalfbins = self.getProperty('nhalfbins').value
+        nbins = 1 + 2*nhalfbins
+        wspace = WorkspaceFactory.create('Workspace2D', NVectors=1, XLength=nbins, YLength=nbins)
+        
+        height = self.getProperty('height').value
+        tau    = self.getProperty('tau').value
+        beta   = self.getProperty('beta').value
+
+        xvals = np.arange(-nhalfbins, 1+nhalfbins) * (1./nhalfbins) # from -1 to 1
+        exponent = -(np.abs(xvals)/tau)**beta
+        ynominal = height*fft( np.exp(exponent) ).real
+        # white noise in the [0.2, 1) range, average is 0.6
+        noise = 0.2 + 0.8*np.random.random(nbins)
+        sign = np.random.random(nbins)
+        sign = np.where(sign>0.5, 1, -1) # random sign
+        # average noise is 6% of the signal local intensity
+        yvals = (1+0.1*noise*sign) * ynominal
+        
+        wspace.dataX(0)[:] = xvals
+        wspace.dataY(0)[:] = yvals
+        # average error is 12% of the signal local intensity
+        wspace.dataE(0)[:] = 0.2*noise*ynominal
+
+        self.setProperty('OutputWorkspace', wspace) # Stores the workspace as the given name
+
+class StretchedExpFTTest(unittest.TestCase):
+
+    def test_registered(self):
+        try:
+            FunctionFactory.createFunction('StretchedExpFT')
+        except RuntimeError, exc:
+            self.fail('Could not create StretchedExpFT function: %s' % str(exc))
+            
+    def test_fit(self):
+        AlgorithmFactory.subscribe(_InternalMakeSEFTData)
+        alg = testhelpers.run_algorithm('_InternalMakeSEFTData', height=0.83, tau=2.45, beta=0.43,
+                                        nhalfbins=4000, OutputWorkspace='_test_seft_data')
+        input_ws = alg.getProperty('OutputWorkspace').value
+        
+        func_string='name=StretechedExpFT,height=1.0,tau=0.9,beta=0.5'
+        Fit(Function=func_string, InputWorkspace='_test_data',
+            StartX=-1, EndX=1, CreateOutput=1, MaxIterations=20)
+        
+        mtd['_test_seft_data_Parameters']
+        
+        mtd.remove('_test_seft_data')
+        mtd.remove('_test_seft_data_NormalisedCovarianceMatrix')
+        mtd.remove('_test_seft_data_Parameters')
+        mtd.remove('_test_seft_data_Workspace')
+
+if __name__ == '__main__':
+    unittest.main()
