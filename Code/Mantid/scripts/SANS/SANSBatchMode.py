8af0b8373e155eeedf6a188cbcd19f996d7f999f
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-11-08 12:08:42 +0000

    re #8395: Correct SaveCanSAS support for LARMOR instrument

--- Code/Mantid/scripts/SANS/SANSBatchMode.py
+++ Code/Mantid/scripts/SANS/SANSBatchMode.py
@@ -127,6 +127,11 @@ def BatchReduce(filename, format, plotresults=False, saveAlgs={'SaveRKH':'txt'},
     ins_name = ReductionSingleton().instrument.name()
     # is used for SaveCanSAS1D go give the detectors names
     detnames = ', '.join(ReductionSingleton().instrument.listDetectors())
+
+    # LARMOR has just one detector, but, it defines two because ISISInstrument is defined as two banks! #8395
+    if ins_name == 'LARMOR': 
+        detnames = ReductionSingleton().instrument.cur_detector().name()
+
     scale_shift = {'scale':1.0000, 'shift':0.0000}
     #first copy the user settings in case running the reductionsteps can change it
     settings = copy.deepcopy(ReductionSingleton().reference())
