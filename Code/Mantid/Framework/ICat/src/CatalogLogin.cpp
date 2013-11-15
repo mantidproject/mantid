53cbf6baeb883bc772ce1b023c1b862bd33ceafe
Jay Rainey <jawrainey@gmail.com>
2013-11-15 12:21:55 +0000

    Improved login message to include catalog version. Refs #8201.

--- Code/Mantid/Framework/ICat/src/CatalogLogin.cpp
+++ Code/Mantid/Framework/ICat/src/CatalogLogin.cpp
@@ -40,7 +40,8 @@ namespace Mantid
       std::string username = getProperty("Username");
       std::string password = getProperty("Password");
 
-      g_log.notice() << "Verifying user credentials..." << std::endl;
+      g_log.notice() << "Attempting to verify user credentials against " <<
+          Mantid::Kernel::ConfigService::Instance().getFacility().catalogInfo().catalogName() << std::endl;
       progress(0.5, "Verifying user credentials...");
 
       catalog->login(username,password,"");
