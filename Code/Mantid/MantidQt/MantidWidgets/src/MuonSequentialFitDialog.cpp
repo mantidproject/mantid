4d2ba844a01749f297442f96f6f6d43975b885d2
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2013-11-18 16:58:03 +0000

    Refs #6473. New class for the dialog.

---
+++ Code/Mantid/MantidQt/MantidWidgets/src/MuonSequentialFitDialog.cpp
@@ -0,0 +1,22 @@
+#include "MantidQtMantidWidgets/MuonSequentialFitDialog.h"
+
+namespace Mantid
+{
+namespace MantidWidgets
+{
+
+  /** 
+   * Constructor
+   */
+  MuonSequentialFitDialog::MuonSequentialFitDialog(QWidget* parent) :
+    QDialog(parent)
+  {}
+    
+  /**
+   * Destructor
+   */
+  MuonSequentialFitDialog::~MuonSequentialFitDialog()
+  {}
+
+} // namespace MantidWidgets
+} // namespace Mantid
