507ab4c0ff326189586173838866831d9208240c
Karl Palmen <karl.palmen@stfc.ac.uk>
2013-11-25 15:15:33 +0000

    Use lexical_cast to get integer from string re #8397
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Code/Mantid/Framework/DataHandling/src/LoadFullprofResolution.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadFullprofResolution.cpp
@@ -232,7 +232,7 @@ namespace DataHandling
       size_t nNumber = lines[1].find("=", nStart) + 1;
       size_t nEnd = lines[1].find(" ",nStart); // Assume the NRPOF number is followed by space
       if(nNumber == string::npos + 1 || nEnd == string::npos ) return (-1);
-      return( std::stoi(lines[1].substr(nNumber,nEnd-nNumber)) );
+      return( boost::lexical_cast<int> (lines[1].substr(nNumber,nEnd-nNumber)) );
     }
 
     return(0);
