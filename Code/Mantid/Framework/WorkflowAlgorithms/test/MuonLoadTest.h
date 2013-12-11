f8b41819012053d2ea02171f340dbbaf01462e41
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2013-12-11 17:24:30 +0000

    Refs #8494. Test for basic functionality.

--- Code/Mantid/Framework/WorkflowAlgorithms/test/MuonLoadTest.h
+++ Code/Mantid/Framework/WorkflowAlgorithms/test/MuonLoadTest.h
@@ -4,8 +4,15 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidWorkflowAlgorithms/MuonLoad.h"
+#include "MantidAPI/ScopedWorkspace.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidDataObjects/TableWorkspace.h"
 
 using Mantid::WorkflowAlgorithms::MuonLoad;
+
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+using namespace Mantid::DataObjects;
 
 class MuonLoadTest : public CxxTest::TestSuite
 {
@@ -23,34 +30,56 @@ public:
     TS_ASSERT( alg.isInitialized() )
   }
   
-  void test_exec()
+  void test_simpleLoad()
   {
-    // Name of the output workspace.
-    std::string outWSName("MuonLoadTest_OutputWS");
+    ScopedWorkspace output;
+
+    TableWorkspace_sptr grouping = createGroupingTable();
   
     MuonLoad alg;
     TS_ASSERT_THROWS_NOTHING( alg.initialize() )
     TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("REPLACE_PROPERTY_NAME_HERE!!!!", "value") );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("Filename", "emu00006473.nxs") );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("DetectorGroupingTable", grouping) );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("OutputType", "GroupCounts") );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("GroupIndex", 0) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", output.name()) );
     TS_ASSERT_THROWS_NOTHING( alg.execute(); );
     TS_ASSERT( alg.isExecuted() );
     
-    // Retrieve the workspace from data service. TODO: Change to your desired type
-    Workspace_sptr ws;
-    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
+    // Retrieve the workspace from data service.
+    MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>( output.retrieve() );
+
     TS_ASSERT(ws);
-    if (!ws) return;
-    
-    // TODO: Check the results
-    
-    // Remove workspace from the data service.
-    AnalysisDataService::Instance().remove(outWSName);
+    if (ws)
+    {
+      TSM_ASSERT( "You forgot to check the results!", 0);
+    }
   }
-  
-  void test_Something()
+
+  TableWorkspace_sptr createGroupingTable()
   {
-    TSM_ASSERT( "You forgot to write a test!", 0);
+    auto t = boost::make_shared<TableWorkspace>();
+
+    t->addColumn("vector_int", "Detectors");
+
+    std::vector<int> group1;
+    for ( int i = 33; i <= 64; ++i )
+    {
+      group1.push_back(i);
+    }
+    TableRow row1 = t->appendRow();
+    row1 << group1;
+
+    std::vector<int> group2;
+    for ( int i = 1; i <= 32; ++i )
+    {
+      group2.push_back(i);
+    }
+    TableRow row2 = t->appendRow();
+    row2 << group2;
+
+    return t;
   }
 
 
