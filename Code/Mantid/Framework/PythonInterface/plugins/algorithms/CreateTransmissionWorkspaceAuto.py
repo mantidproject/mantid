4eb962c5788ba83f21536d37ab34115025faac40
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-12-16 11:49:52 +0000

    refs #8616. Add remaining properties.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/CreateTransmissionWorkspaceAuto.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/CreateTransmissionWorkspaceAuto.py
@@ -1,3 +1,4 @@
+import sys
 import mantid.simpleapi
 from mantid.api import *
 from mantid.kernel import *
@@ -12,43 +13,26 @@ class CreateTransmissionWorkspaceAuto(PythonAlgorithm):
     
     def PyInit(self):
         
-        '''
-        
-        declareProperty(
-          new ArrayProperty<double>("Params", boost::make_shared<RebinParamsValidator>(true)),
-          "A comma separated list of first bin boundary, width, last bin boundary. "
-              "These parameters are used for stitching together transmission runs. "
-              "Values are in wavelength (angstroms). This input is only needed if a SecondTransmission run is provided.");
-
-      declareProperty(
-          new PropertyWithValue<double>("StartOverlap", Mantid::EMPTY_DBL(), Direction::Input),
-          "Start wavelength for stitching transmission runs together");
-
-      declareProperty(
-          new PropertyWithValue<double>("EndOverlap", Mantid::EMPTY_DBL(), Direction::Input),
-          "End wavelength (angstroms) for stitching transmission runs together");
-        
-        
-      this->initStitchingInputs();
-      this->initIndexInputs();
-      this->initWavelengthInputs();
-
-      declareProperty(new WorkspaceProperty<MatrixWorkspace>("OutputWorkspace", "", Direction::Output),
-          "Output Workspace IvsQ.");
-
-        '''
-        
         
         input_validator = WorkspaceUnitValidator("TOF")
         self.declareProperty(MatrixWorkspaceProperty("FirstTransmissionRun", "", Direction.Input, validator=input_validator), "Input workspace")
         self.declareProperty(MatrixWorkspaceProperty("SecondTransmissionRun", "", optional=PropertyMode.Optional,  validator=input_validator, direction=Direction.Input), "Second transmission run workspace in TOF")
         self.declareProperty(MatrixWorkspaceProperty("OutputWorkspace", "", Direction.Output), "Output transmission workspace in wavelength")
-        self.declareProperty(FloatArrayProperty(name="Params", values=[0.1]), doc="Rebinning Parameters. See Rebin for format.")
-        
-        self.declareProperty(name="StartOverlap", defaultValue=-1.0, doc="Overlap in Q.")
-        self.declareProperty(name="EndOverlap", defaultValue=-1.0, doc="End overlap in Q.")
-        
-        
+
+        self.declareProperty(FloatArrayProperty(name="Params", values=[sys.maxint]), doc="Rebinning Parameters. See Rebin for format.")
+        self.declareProperty(name="StartOverlap", defaultValue=sys.maxint, doc="Overlap in Q.")
+        self.declareProperty(name="EndOverlap", defaultValue=sys.maxint, doc="End overlap in Q.")
+        index_bounds = IntBoundedValidator()
+        index_bounds.setLower(0)
+        self.declareProperty(name="I0MonitorIndex", defaultValue=sys.maxint, validator=index_bounds, doc="I0 monitor index" )
+        self.declareProperty(IntArrayProperty(name="WorkspaceIndexList", values=[sys.maxint]), doc="Workspace index list")
+        self.declareProperty(name="WavelengthMin", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Wavelength Min in angstroms")
+        self.declareProperty(name="WavelengthMax", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Wavelength Max in angstroms")
+        self.declareProperty(name="WavelengthStep", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Wavelength step in angstroms")
+        self.declareProperty(name="MonitorBackgroundWavelengthMin", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Monitor wavelength background min in angstroms")
+        self.declareProperty(name="MonitorBackgroundWavelengthMax", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Monitor wavelength background max in angstroms")
+        self.declareProperty(name="MonitorIntegrationWavelengthMin", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Monitor integral min in angstroms")
+        self.declareProperty(name="MonitorIntegrationWavelengthMax", direction=Direction.Input, defaultValue=sys.float_info.max, doc="Monitor integral max in angstroms")
         
 
     def PyExec(self):
