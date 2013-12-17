540b827eb0d7222208144f7ec6bafcc65f9ff3fc
Peter Peterson <petersonpf@ornl.gov>
2013-12-17 09:02:50 -0500

    Re #8623. Adding properties to the algorithm and making it compile.

--- Code/Mantid/Framework/DataHandling/src/LoadPDCharacterizations.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadPDCharacterizations.cpp
@@ -3,6 +3,17 @@ TODO: Enter a full wiki-markup description of your algorithm here. You can then 
 *WIKI*/
 
 #include "MantidDataHandling/LoadPDCharacterizations.h"
+#include "MantidAPI/FileProperty.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidAPI/WorkspaceFactory.h"
+#include "MantidKernel/ArrayProperty.h"
+#include "MantidKernel/Property.h"
+
+using Mantid::API::FileProperty;
+using Mantid::API::ITableWorkspace;
+using Mantid::API::WorkspaceProperty;
+using Mantid::Kernel::ArrayProperty;
+using Mantid::Kernel::Direction;
 
 namespace Mantid
 {
@@ -37,7 +48,7 @@ namespace DataHandling
   int LoadPDCharacterizations::version() const { return 1;};
   
   /// Algorithm's category for identification. @see Algorithm::category
-  const std::string LoadPDCharacterizations::category() const { return TODO: FILL IN A CATEGORY;}
+  const std::string LoadPDCharacterizations::category() const { return "DataHandling";}
 
   //----------------------------------------------------------------------------------------------
   /// Sets documentation strings for this algorithm
@@ -52,8 +63,26 @@ namespace DataHandling
    */
   void LoadPDCharacterizations::init()
   {
-    declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input), "An input workspace.");
-    declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output), "An output workspace.");
+    std::vector<std::string> exts;
+    exts.push_back(".txt");
+    declareProperty(new FileProperty("Filename", "", FileProperty::Load, exts),
+                    "Characterizations file");
+
+    declareProperty(new WorkspaceProperty<ITableWorkspace>("CharacterizationWorkspace", "",
+                                                                     Direction::Output),
+                    "Output for the information of characterizations and runs");
+
+    declareProperty("IParmFilename", std::string(""), "Name of the gsas instrument parameter file.", Direction::Output);
+    declareProperty("PrimaryFlightPath", EMPTY_DBL(), "Primary flight path L1 of the powder diffractomer. ", Direction::Output);
+    declareProperty(new ArrayProperty<int32_t>("SpectrumIDs", Direction::Output),
+                    "Spectrum IDs (note that it is not detector ID or workspace indices). The list must be either empty or have a size equal to input workspace's histogram number. ");
+    declareProperty(new ArrayProperty<double>("L2", Direction::Output),
+                    "Seconary flight (L2) paths for each detector.  Number of L2 given must be same as number of histogram.");
+    declareProperty(new ArrayProperty<double>("Polar", Direction::Output),
+                    "Polar angles (two thetas) for detectors. Number of 2theta given must be same as number of histogram.");
+    declareProperty(new ArrayProperty<double>("Azimuthal", Direction::Output),
+                    "Azimuthal angles (out-of-plane) for detectors. "
+                    "Number of azimuthal angles given must be same as number of histogram.");
   }
 
   //----------------------------------------------------------------------------------------------
