a1c3b36f241fd99c7486207c707bf33d062b4cd5
Jose Borreguero <borreguero@gmail.com>
2013-12-20 11:46:15 -0500

    Refs #4394 Moved some initialization steps to init
    These changes for the elastic part

--- Code/Mantid/Framework/CurveFitting/src/DiffSphere.cpp
+++ Code/Mantid/Framework/CurveFitting/src/DiffSphere.cpp
@@ -23,22 +23,25 @@ using namespace API;
 
 DECLARE_FUNCTION(DiffSphere);
 
-ElasticDiffSphere::ElasticDiffSphere(){
+ElasticDiffSphere::ElasticDiffSphere()
+{
   //declareParameter("Height", 1.0); //parameter "Height" already declared in constructor of base class DeltaFunction
   declareParameter("Radius", 1.0, "Sphere radius");
+  declareAttribute( "Q", API::IFunction::Attribute(1.0) );
+}
 
+void ElasticDiffSphere::init()
+{
   // Ensure positive values for Height and Radius
   BoundaryConstraint* HeightConstraint = new BoundaryConstraint(this,"Height",0,true);
   addConstraint(HeightConstraint);
 
   BoundaryConstraint* RadiusConstraint = new BoundaryConstraint(this,"Radius",0,true);
   addConstraint(RadiusConstraint);
-
-  declareAttribute( "Q", API::IFunction::Attribute(1.0) );
-
 }
 
-double ElasticDiffSphere::HeightPrefactor() const{
+double ElasticDiffSphere::HeightPrefactor() const
+{
   const double R = getParameter("Radius");
   const double Q = getAttribute("Q").asDouble();
   return pow(3*boost::math::sph_bessel(1,Q*R)/(Q*R),2);
