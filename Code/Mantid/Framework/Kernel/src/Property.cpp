8ddfbd265408819ade173581d2a724970bb4f7fe
Jose Borreguero <borreguero@gmail.com>
2013-12-31 16:08:13 -0500

    Refs #8661 Added IPeaksWorkspace to list
    modified: Property.cpp

--- Code/Mantid/Framework/Kernel/src/Property.cpp
+++ Code/Mantid/Framework/Kernel/src/Property.cpp
@@ -228,6 +228,7 @@ namespace API
   class IMDEventWorkspace;
   class IMDWorkspace;
   class IEventWorkspace;
+  class IPeaksWorkspace;
   class IMDHistoWorkspace;
   class IFunction;
   class IAlgorithm;
@@ -297,6 +298,8 @@ std::string getUnmangledTypeName(const std::type_info& type)
                                       string("EventWorkspace")));
     typestrings.insert(make_pair(typeid(boost::shared_ptr<PeaksWorkspace>).name(), 
                                       string("PeaksWorkspace")));
+    typestrings.insert(make_pair(typeid(boost::shared_ptr<IPeaksWorkspace>).name(),
+                                      string("IPeaksWorkspace")));
     typestrings.insert(make_pair(typeid(boost::shared_ptr<GroupingWorkspace>).name(), 
                                       string("GroupingWorkspace")));
     typestrings.insert(make_pair(typeid(boost::shared_ptr<OffsetsWorkspace>).name(),
