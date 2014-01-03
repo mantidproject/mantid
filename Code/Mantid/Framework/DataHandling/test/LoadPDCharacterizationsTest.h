e239b05ce4214f392afaf077b81e870387ff5bcc
Peter Peterson <petersonpf@ornl.gov>
2014-01-03 16:37:21 -0500

    Re #8623. Refactored test to remove some duplicate code.

--- Code/Mantid/Framework/DataHandling/test/LoadPDCharacterizationsTest.h
+++ Code/Mantid/Framework/DataHandling/test/LoadPDCharacterizationsTest.h
@@ -34,23 +34,54 @@ public:
     TS_ASSERT(wksp);
   }
 
-  void test_Init()
+  // checks the focus positions for NOMAD
+  void checkNOMAD(LoadPDCharacterizations &alg)
   {
-    LoadPDCharacterizations alg;
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_EQUALS(alg.getPropertyValue("IParmFilename"), std::string("NOMAD_11_22_11.prm"));
+    double l1 = alg.getProperty("PrimaryFlightPath");
+    TS_ASSERT_EQUALS(l1, 19.5);
+
+    const int NUM_SPEC = 6;
+
+    std::vector<int32_t> specIds = alg.getProperty("SpectrumIDs");
+    TS_ASSERT_EQUALS(specIds.size(), NUM_SPEC);
+    if (!specIds.empty())
+    {
+      for (int i = 0; i < NUM_SPEC; ++i)
+        TS_ASSERT_EQUALS(specIds[i], i+1);
+    }
+
+    std::vector<double> l2 = alg.getProperty("L2");
+    TS_ASSERT_EQUALS(l2.size(), NUM_SPEC);
+    if (!l2.empty())
+    {
+      for (int i = 0; i < NUM_SPEC; ++i)
+        TS_ASSERT_EQUALS(l2[i], 2.);
+    }
+
+    std::vector<double> polar = alg.getProperty("Polar");
+    TS_ASSERT_EQUALS(polar.size(), NUM_SPEC);
+    if (!polar.empty())
+    {
+      TS_ASSERT_EQUALS(polar[0], 15.);
+      TS_ASSERT_EQUALS(polar[1], 31.);
+      TS_ASSERT_EQUALS(polar[2], 67.);
+      TS_ASSERT_EQUALS(polar[3], 122.);
+      TS_ASSERT_EQUALS(polar[4], 154.);
+      TS_ASSERT_EQUALS(polar[5], 7.);
+    }
+
+    std::vector<double> azi = alg.getProperty("Azimuthal");
+    TS_ASSERT_EQUALS(azi.size(), NUM_SPEC);
+    if (!azi.empty())
+    {
+      for (int i = 0; i < NUM_SPEC; ++i)
+        TS_ASSERT_EQUALS(azi[0], 0.);
+    }
   }
 
-  void test_FocusAndChar()
+  void checkPG3(ITableWorkspace_sptr &wksp)
   {
-    const std::string CHAR_FILE("Test_characterizations_focus_and_char.txt");
-    ITableWorkspace_sptr wksp;
-
-    // initialize and run the algorithm
-    LoadPDCharacterizations alg;
-    runAlg(alg, wksp, CHAR_FILE);
-
-    // test the table workspace
     TS_ASSERT_EQUALS(wksp->columnCount(), 10);
     TS_ASSERT_EQUALS(wksp->rowCount(), 6);
 
@@ -77,6 +108,26 @@ public:
     TS_ASSERT_EQUALS(wksp->String(5,7), "15.40");
     TS_ASSERT_EQUALS(wksp->Double(5,8), 0.);
     TS_ASSERT_EQUALS(wksp->Double(5,9), 100000.);
+  }
+
+  void test_Init()
+  {
+    LoadPDCharacterizations alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+  }
+
+  void test_FocusAndChar()
+  {
+    const std::string CHAR_FILE("Test_characterizations_focus_and_char.txt");
+    ITableWorkspace_sptr wksp;
+
+    // initialize and run the algorithm
+    LoadPDCharacterizations alg;
+    runAlg(alg, wksp, CHAR_FILE);
+
+    // test the table workspace
+    checkPG3(wksp);
 
     // test the other output properties
     TS_ASSERT_EQUALS(alg.getPropertyValue("IParmFilename"), std::string("dummy.iparm"));
@@ -130,47 +181,7 @@ public:
     TS_ASSERT_EQUALS(wksp->Double(0,9), 16666.67);
 
     // test the other output properties
-    TS_ASSERT_EQUALS(alg.getPropertyValue("IParmFilename"), std::string("NOMAD_11_22_11.prm"));
-    double l1 = alg.getProperty("PrimaryFlightPath");
-    TS_ASSERT_EQUALS(l1, 19.5);
-
-    const int NUM_SPEC = 6;
-
-    std::vector<int32_t> specIds = alg.getProperty("SpectrumIDs");
-    TS_ASSERT_EQUALS(specIds.size(), NUM_SPEC);
-    if (!specIds.empty())
-    {
-      for (int i = 0; i < NUM_SPEC; ++i)
-        TS_ASSERT_EQUALS(specIds[i], i+1);
-    }
-
-    std::vector<double> l2 = alg.getProperty("L2");
-    TS_ASSERT_EQUALS(l2.size(), NUM_SPEC);
-    if (!l2.empty())
-    {
-      for (int i = 0; i < NUM_SPEC; ++i)
-        TS_ASSERT_EQUALS(l2[i], 2.);
-    }
-
-    std::vector<double> polar = alg.getProperty("Polar");
-    TS_ASSERT_EQUALS(polar.size(), NUM_SPEC);
-    if (!polar.empty())
-    {
-      TS_ASSERT_EQUALS(polar[0], 15.);
-      TS_ASSERT_EQUALS(polar[1], 31.);
-      TS_ASSERT_EQUALS(polar[2], 67.);
-      TS_ASSERT_EQUALS(polar[3], 122.);
-      TS_ASSERT_EQUALS(polar[4], 154.);
-      TS_ASSERT_EQUALS(polar[5], 7.);
-    }
-
-    std::vector<double> azi = alg.getProperty("Azimuthal");
-    TS_ASSERT_EQUALS(azi.size(), NUM_SPEC);
-    if (!azi.empty())
-    {
-      for (int i = 0; i < NUM_SPEC; ++i)
-        TS_ASSERT_EQUALS(azi[0], 0.);
-    }
+    checkNOMAD(alg);
   }
 
   void test_Focus()
@@ -187,47 +198,7 @@ public:
     TS_ASSERT_EQUALS(wksp->rowCount(), 0);
 
     // test the other output properties
-    TS_ASSERT_EQUALS(alg.getPropertyValue("IParmFilename"), std::string("NOMAD_11_22_11.prm"));
-    double l1 = alg.getProperty("PrimaryFlightPath");
-    TS_ASSERT_EQUALS(l1, 19.5);
-
-    const int NUM_SPEC = 6;
-
-    std::vector<int32_t> specIds = alg.getProperty("SpectrumIDs");
-    TS_ASSERT_EQUALS(specIds.size(), NUM_SPEC);
-    if (!specIds.empty())
-    {
-      for (int i = 0; i < NUM_SPEC; ++i)
-        TS_ASSERT_EQUALS(specIds[i], i+1);
-    }
-
-    std::vector<double> l2 = alg.getProperty("L2");
-    TS_ASSERT_EQUALS(l2.size(), NUM_SPEC);
-    if (!l2.empty())
-    {
-      for (int i = 0; i < NUM_SPEC; ++i)
-        TS_ASSERT_EQUALS(l2[i], 2.);
-    }
-
-    std::vector<double> polar = alg.getProperty("Polar");
-    TS_ASSERT_EQUALS(polar.size(), NUM_SPEC);
-    if (!polar.empty())
-    {
-      TS_ASSERT_EQUALS(polar[0], 15.);
-      TS_ASSERT_EQUALS(polar[1], 31.);
-      TS_ASSERT_EQUALS(polar[2], 67.);
-      TS_ASSERT_EQUALS(polar[3], 122.);
-      TS_ASSERT_EQUALS(polar[4], 154.);
-      TS_ASSERT_EQUALS(polar[5], 7.);
-    }
-
-    std::vector<double> azi = alg.getProperty("Azimuthal");
-    TS_ASSERT_EQUALS(azi.size(), NUM_SPEC);
-    if (!azi.empty())
-    {
-      for (int i = 0; i < NUM_SPEC; ++i)
-        TS_ASSERT_EQUALS(azi[0], 0.);
-    }
+    checkNOMAD(alg);
   }
 
   void test_Char()
@@ -240,32 +211,7 @@ public:
     runAlg(alg, wksp, CHAR_FILE);
 
     // test the table workspace
-    TS_ASSERT_EQUALS(wksp->columnCount(), 10);
-    TS_ASSERT_EQUALS(wksp->rowCount(), 6);
-
-    // check all of the contents of row 0
-    TS_ASSERT_EQUALS(wksp->Double(0,0), 60.);
-    TS_ASSERT_EQUALS(wksp->Double(0,1), 0.900);
-    TS_ASSERT_EQUALS(wksp->Int(0,2), 1);
-    TS_ASSERT_EQUALS(wksp->Int(0,3), 15030);
-    TS_ASSERT_EQUALS(wksp->Int(0,4), 15039);
-    TS_ASSERT_EQUALS(wksp->Int(0,5), 0);
-    TS_ASSERT_EQUALS(wksp->String(0,6), "0.20");
-    TS_ASSERT_EQUALS(wksp->String(0,7), "4.12");
-    TS_ASSERT_EQUALS(wksp->Double(0,8), 4700.);
-    TS_ASSERT_EQUALS(wksp->Double(0,9), 21200.);
-
-    // check all of the contents of row 5
-    TS_ASSERT_EQUALS(wksp->Double(5,0), 10.);
-    TS_ASSERT_EQUALS(wksp->Double(5,1), 3.198);
-    TS_ASSERT_EQUALS(wksp->Int(5,2), 1);
-    TS_ASSERT_EQUALS(wksp->Int(5,3), 15033);
-    TS_ASSERT_EQUALS(wksp->Int(5,4), 15042);
-    TS_ASSERT_EQUALS(wksp->Int(5,5), 0);
-    TS_ASSERT_EQUALS(wksp->String(5,6), "0.05");
-    TS_ASSERT_EQUALS(wksp->String(5,7), "15.40");
-    TS_ASSERT_EQUALS(wksp->Double(5,8), 0.);
-    TS_ASSERT_EQUALS(wksp->Double(5,9), 100000.);
+    checkPG3(wksp);
 
     // test the other output properties
     TS_ASSERT_EQUALS(alg.getPropertyValue("IParmFilename"), std::string(""));
