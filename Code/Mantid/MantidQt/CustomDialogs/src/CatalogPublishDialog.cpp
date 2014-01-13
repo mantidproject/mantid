138897463fd9d9cb04b914ce17f02670a1175451
Jay Rainey <jawrainey@gmail.com>
2014-01-13 14:38:36 +0000

    Override generated form layout. Refs #8537.

--- Code/Mantid/MantidQt/CustomDialogs/src/CatalogPublishDialog.cpp
+++ Code/Mantid/MantidQt/CustomDialogs/src/CatalogPublishDialog.cpp
@@ -4,19 +4,39 @@ namespace MantidQt
 {
   namespace CustomDialogs
   {
-    // Declare the dialog. Name must match the class name.
     DECLARE_DIALOG(CatalogPublishDialog);
 
     /**
      * Default constructor.
      * @param parent :: Parent dialog.
      */
-    CatalogPublishDialog::CatalogPublishDialog(QWidget *parent) : MantidQt::API::AlgorithmDialog(parent) {}
+    CatalogPublishDialog::CatalogPublishDialog(QWidget *parent) : MantidQt::API::AlgorithmDialog(parent), m_uiForm() {}
 
     /// Destructor
     CatalogPublishDialog::~CatalogPublishDialog() {}
 
     /// Initialise the layout
-    void CatalogPublishDialog::initLayout() {}
+    void CatalogPublishDialog::initLayout()
+    {
+      m_uiForm.setupUi(this);
+      this->setWindowTitle(m_algName);
+
+      tie(m_uiForm.fileNameTxt,"FileName");
+      tie(m_uiForm.inputWorkspaceCb,"InputWorkspace");
+      tie(m_uiForm.nameInCatalogTxt,"NameInCatalog");
+      tie(m_uiForm.investigationNumberCb,"InvestigationNumber");
+
+      // Open a browsing dialog when the browse button is pressed.
+      connect(m_uiForm.browseBtn,SIGNAL(clicked()),this,SLOT(onBrowse()));
+
+      // Allows the combo box to show workspaces when they are loaded into Mantid.
+      m_uiForm.inputWorkspaceCb->setValidatingAlgorithm(m_algName);
+
+      // Add the automatically generated buttons (help, run, and cancel) to the ui.
+      m_uiForm.buttonGrid->addLayout(createDefaultButtonLayout());
+
+      // This allows the user NOT to select a workspace if there are any loaded into Mantid.
+      m_uiForm.inputWorkspaceCb->insertItem("", 0);
+
   }
 }
