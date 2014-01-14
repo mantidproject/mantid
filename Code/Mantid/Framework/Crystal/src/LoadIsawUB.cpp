29898838fea56b58f00ad9004224d3c3e9877bbc
Andrei Savici <saviciat@ornl.gov>
2014-01-14 17:24:16 -0500

    Fix windows build and wiki. Refs #8707

--- Code/Mantid/Framework/Crystal/src/LoadIsawUB.cpp
+++ Code/Mantid/Framework/Crystal/src/LoadIsawUB.cpp
@@ -1,14 +1,15 @@
 /*WIKI* 
 
-Saves the UB matrix in a workspace to an ISAW-style UB matrix ASCII file.
+Loads the UB matrix into a workspace from an ISAW-style UB matrix ASCII file.
 
 You can use the [[SaveIsawUB]] algorithm to save to this format.
 
-The matrix saved is the transpose of the UB Matrix.
+The matrix in the file is the transpose of the UB Matrix.
 The UB matrix maps the column vector (h,k,l ) to the column vector (q'x,q'y,q'z).
 |Q'|=1/dspacing and its coordinates are a right-hand coordinate system where x
 is the beam direction and z is vertically upward. (IPNS convention)
 
+Note: for an MDEventWorkspace, all experimentInfo objects will contain teh oriented lattice loaded from the IsawUB file
 
 *WIKI*/
 #include "MantidAPI/FileProperty.h"
@@ -164,7 +165,7 @@ namespace Crystal
     //Save it to every experiment info in MD workspaces
     if ((MDWS != NULL) && (MDWS->getNumExperimentInfo()>1))
     {
-        for(u_int16_t i=1;i<MDWS->getNumExperimentInfo();i++)
+        for(uint16_t i=1;i<MDWS->getNumExperimentInfo();i++)
         {
            ws = MDWS->getExperimentInfo(i);
            ws->mutableSample().setOrientedLattice(latt);
