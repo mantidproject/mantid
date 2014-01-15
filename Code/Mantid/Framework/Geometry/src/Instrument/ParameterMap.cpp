f089fdc94b29954b3a44e0794c68a7f523378ce8
Ricardo Leal <ricardo.leal@ill.fr>
2014-01-15 17:05:28 +0100

    Re #8710 new function copyFromParameterMap

--- Code/Mantid/Framework/Geometry/src/Instrument/ParameterMap.cpp
+++ Code/Mantid/Framework/Geometry/src/Instrument/ParameterMap.cpp
@@ -972,6 +972,25 @@ namespace Mantid
 //    }
 
 
+    /**
+     * Copy pairs (oldComp->id,Parameter) to the m_map
+     * assigning the new newComp->id
+     *
+     */
+    void ParameterMap::copyFromParameterMap(const IComponent* oldComp,
+		const IComponent* newComp, const ParameterMap *oldPMap) {
+
+		std::set<std::string> oldParameterNames = oldPMap->names(oldComp);
+
+		for(auto it = oldParameterNames.begin(); it != oldParameterNames.end(); ++it) {
+			Parameter_sptr thisParameter = oldPMap->get(oldComp,*it);
+			// Insert the fecthed parameter in the m_map
+			m_map.insert(std::make_pair(newComp->getComponentID(),thisParameter));
+		}
+    }
+
+
+
   } // Namespace Geometry
 } // Namespace Mantid
 
