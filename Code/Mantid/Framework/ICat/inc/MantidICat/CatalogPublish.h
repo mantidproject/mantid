644db73038d54139ff2b87ce85e653e855a535d1
Jay Rainey <jawrainey@gmail.com>
2014-01-17 09:45:11 +0000

    Added missing DLLExport. Refs #8725.

--- Code/Mantid/Framework/ICat/inc/MantidICat/CatalogPublish.h
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CatalogPublish.h
@@ -33,7 +33,7 @@ namespace Mantid
      File change history is stored at: <https://github.com/mantidproject/mantid>.
      Code Documentation is available at: <http://doxygen.mantidproject.org>
     */
-    class CatalogPublish : public API::Algorithm
+    class DLLExport CatalogPublish : public API::Algorithm
     {
       public:
         /// constructor
