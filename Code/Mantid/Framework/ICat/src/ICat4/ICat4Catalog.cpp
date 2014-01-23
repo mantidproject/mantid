8b241f19a254db1f3d98691c295de0c1319b89fe
Jay Rainey <jawrainey@gmail.com>
2014-01-23 09:48:09 +0000

    Throw an exception if soapEndPoint is empty. Refs #8719.

--- Code/Mantid/Framework/ICat/src/ICat4/ICat4Catalog.cpp
+++ Code/Mantid/Framework/ICat/src/ICat4/ICat4Catalog.cpp
@@ -898,6 +898,10 @@ namespace Mantid
      */
     void ICat4Catalog::setICATProxySettings(ICat4::ICATPortBindingProxy& icat)
     {
+      // The soapEndPoint is only set when the user logs into the catalog.
+      // Instead, Inform the user that they need to log into the catalog as no soapEndPoint is set.
+      if (ICat::Session::Instance().getSoapEndPoint().empty())
+        throw std::runtime_error("Session ID cannot be null or empty. Please log into the catalog.");
       // Set the soap-endpoint of the catalog we want to use.
       icat.soap_endpoint = ICat::Session::Instance().getSoapEndPoint().c_str();
       // Sets SSL authentication scheme
