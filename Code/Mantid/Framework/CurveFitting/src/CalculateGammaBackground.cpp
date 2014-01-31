201607b63ceefed6a4ed2e5cbac109b0f57c6b6f
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-01-31 16:06:36 +0000

    Docs for CalculateGammaBackground
    Refs #8737

--- Code/Mantid/Framework/CurveFitting/src/CalculateGammaBackground.cpp
+++ Code/Mantid/Framework/CurveFitting/src/CalculateGammaBackground.cpp
@@ -1,8 +1,13 @@
 /*WIKI*
+This algorithm is currently used by the Vesuvio spectrometer at ISIS to correct for background produced by photons
+that are produced when the neutrons are absorbed by the shielding on the instrument. It only corrects the forward scattering
+detector banks.
 
-
-
- *WIKI*/
+Two workspaces are produced: the calculated background and a corrected workspace where the input workspace has been
+corrected by the background. The background is computed by a simple simulation of the expected count across all of the foils. The
+corrected workspace counts are computed by calculating a ratio of the expected counts at the detector to the integrated foil counts (<math>\beta</math>)
+and then the final corrected count rate <math>\displaystyle c_f</math> is defined as <math>\displaystyle c_f = c_i - \beta c_b</math>.
+*WIKI*/
 #include "MantidCurveFitting/CalculateGammaBackground.h"
 #include "MantidCurveFitting/ComptonProfile.h"
 #include "MantidCurveFitting/ConvertToYSpace.h"
@@ -86,7 +91,7 @@ namespace Mantid
 
     void CalculateGammaBackground::initDocs()
     {
-      this->setWikiSummary("Calculates and the background due to gamma rays produced when neutrons are absorbed by shielding");
+      this->setWikiSummary("Calculates the background due to gamma rays produced when neutrons are absorbed by shielding");
       this->setOptionalMessage("Calculates the background due to gamma rays produced when neutrons are absorbed by shielding.");
     }
 
