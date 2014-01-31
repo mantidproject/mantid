674daed9ad744f0aa9221c0ed647ed372e377e41
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-01-31 16:14:25 +0000

    Docs for NormaliseByPeakArea.
    Refs #8737

--- Code/Mantid/Framework/CurveFitting/src/NormaliseByPeakArea.cpp
+++ Code/Mantid/Framework/CurveFitting/src/NormaliseByPeakArea.cpp
@@ -1,8 +1,16 @@
 /*WIKI*
 Takes an input TOF spectrum and converts it to Y-space using the [[ConvertToYSpace]] algorithm. The result is
-then fitted using the ComptonPeakProfile function to produce an estimate of the peak area. The input data is
+then fitted using the ComptonPeakProfile function using the given mass to produce an estimate of the peak area. The input data is
 normalised by this value.
- *WIKI*/
+
+The algorithm has 4 outputs:
+* the input data normalised by the fitted peak area;
+* the input data (without normalisation) converted Y-space;
+* the fitted peak in Y-space;
+* the input data converted to Y and then symmetrised about Y=0.
+
+If the sum option is requested then all input spectra are rebinned, in steps of 0.5 <math>A^-1</math>, to a common Y grid and then summed to give a single spectrum.
+*WIKI*/
 
 #include "MantidCurveFitting/NormaliseByPeakArea.h"
 
