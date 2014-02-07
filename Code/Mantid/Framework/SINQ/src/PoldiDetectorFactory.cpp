d5fda7114908f83a09a5aaf2ff46dd3ec2f930a2
MichaelWedel <michael.wedel@psi.ch>
2014-02-07 17:29:44 +0100

    Added detector factory for POLDI

---
+++ Code/Mantid/Framework/SINQ/src/PoldiDetectorFactory.cpp
@@ -0,0 +1,31 @@
+#include "MantidSINQ/PoldiDetectorFactory.h"
+
+#include "MantidSINQ/PoldiHeliumDetector.h"
+
+namespace Mantid
+{
+namespace Poldi
+{
+
+PoldiDetectorFactory::PoldiDetectorFactory() :
+    m_newDetectorDate(from_string(std::string("2016/01/01")))
+{
+}
+
+PoldiAbstractDetector *PoldiDetectorFactory::createDetector(std::string detectorType)
+{
+    return new PoldiHeliumDetector();
+}
+
+PoldiAbstractDetector *PoldiDetectorFactory::createDetector(date experimentDate)
+{
+    if(experimentDate < m_newDetectorDate) {
+        return new PoldiHeliumDetector();
+    }
+
+    return 0;
+}
+
+
+} // namespace SINQ
+} // namespace Mantid
