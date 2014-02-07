ae5f46634828e749aae819737fc5554206337917
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2014-02-07 13:22:48 +0000

    Re #8908. Catch and handle the Rename notification.

--- Code/Mantid/MantidQt/API/src/WorkspaceObserver.cpp
+++ Code/Mantid/MantidQt/API/src/WorkspaceObserver.cpp
@@ -32,6 +32,11 @@ namespace MantidQt
       m_observer->afterReplaceHandle(name, workspace);
     }
 
+    void ObserverCallback::handleRename(const std::string &oldName, const std::string &newName)
+    {
+      m_observer->renameHandle(oldName, newName);
+    }
+
     void ObserverCallback::handleClearADS()
     {
       m_observer->clearADSHandle();
@@ -47,9 +52,10 @@ namespace MantidQt
         m_postDeleteObserver(*this,&WorkspaceObserver::_postDeleteHandle),
       m_addObserver(*this,&WorkspaceObserver::_addHandle),
       m_afterReplaceObserver(*this,&WorkspaceObserver::_afterReplaceHandle),
+      m_renameObserver(*this,&WorkspaceObserver::_renameHandle),
       m_clearADSObserver(*this,&WorkspaceObserver::_clearADSHandle),
       m_predel_observed(false), m_postdel_observed(false), m_add_observed(false), m_repl_observed(false),
-      m_clr_observed(false)
+      m_rename_observed(false), m_clr_observed(false)
     {
       m_proxy = new ObserverCallback(this);
     }
@@ -61,6 +67,7 @@ namespace MantidQt
       observePostDelete(false);
       observeAdd(false);
       observeAfterReplace(false);
+      observeRename(false);
       observeADSClear(false);
 
       m_proxy->disconnect();
@@ -144,6 +151,32 @@ namespace MantidQt
           SLOT(handleAfterReplace(const std::string &, Mantid::API::Workspace_sptr)));
       }
       m_repl_observed = turnOn;
+    }
+
+    /**
+    * Turn on observations of workspace renaming notifications from the ADS
+    * @param turnOn :: If true observe the notifications, otherwise disable observation [default=true]
+    */
+    void WorkspaceObserver::observeRename(bool turnOn)
+    {
+      if( turnOn && !m_rename_observed )
+      {
+        Mantid::API::AnalysisDataService::Instance().notificationCenter.addObserver(m_renameObserver);
+        m_proxy->connect(m_proxy, 
+          SIGNAL(renamed(const std::string &,const std::string &)),
+          SLOT(handleRename(const std::string &, const std::string &)),
+          Qt::QueuedConnection
+          );
+      }
+      else if( !turnOn && m_rename_observed )
+      {
+        Mantid::API::AnalysisDataService::Instance().notificationCenter.removeObserver(m_renameObserver);
+        m_proxy->disconnect(m_proxy, 
+          SIGNAL(renamed(const std::string &,const std::string &)),
+          m_proxy,
+          SLOT(handleRename(const std::string &, const std::string &)));
+      }
+      m_rename_observed = turnOn;
     }
 
     /**
