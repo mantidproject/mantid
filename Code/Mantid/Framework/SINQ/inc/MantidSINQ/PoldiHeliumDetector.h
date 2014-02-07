5513466c0219955301f2b506658757a852bb71aa
MichaelWedel <michael.wedel@psi.ch>
2014-02-07 13:52:55 +0100

    Added license blocks to header files

--- Code/Mantid/Framework/SINQ/inc/MantidSINQ/PoldiHeliumDetector.h
+++ Code/Mantid/Framework/SINQ/inc/MantidSINQ/PoldiHeliumDetector.h
@@ -9,6 +9,33 @@
 namespace Mantid {
 namespace Poldi {
 
+/** PoldiHeliumDetector :
+ *
+  Implementation of PoldiAbstractDetector for the currently (2014) installed He3-based
+  detector at the POLDI instrument.
+
+    @author Michael Wedel, Paul Scherrer Institut - SINQ
+    @date 07/02/2014
+
+    Copyright © 2014 PSI-MSS
+
+  Mantid is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  Mantid is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  File change history is stored at: <https://github.com/mantidproject/mantid>
+  Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+
 class MANTID_SINQ_DLL PoldiHeliumDetector : public PoldiAbstractDetector
 {
 public:
