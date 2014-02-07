38e63ef03b269fe56b08f98a26a7ca67a5a6a737
MichaelWedel <michael.wedel@psi.ch>
2014-02-07 17:30:07 +0100

    Started integrating detector code into autocorrelate

--- Code/Mantid/Framework/SINQ/src/PoldiAutoCorrelation5.cpp
+++ Code/Mantid/Framework/SINQ/src/PoldiAutoCorrelation5.cpp
@@ -18,7 +18,11 @@ wiki page of [[PoldiProjectRun]].
 #include "MantidAPI/ITableWorkspace.h"
 #include "MantidAPI/TableRow.h"
 
+#include "MantidSINQ/PoldiDetectorFactory.h"
+
 #include <boost/shared_ptr.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/bind.hpp>
 
 
 using namespace std;
@@ -249,9 +253,32 @@ void PoldiAutoCorrelation5::exec()
 	double ang_beta_min = ang_beta_max - static_cast<double>(nb_det_channel) * ang_wire_apperture;
 	g_log.debug() << "_Poldi -        ang_beta_min                       " <<  ang_beta_min*rad2deg          << " deg" << std::endl;
 
+    PoldiDetectorFactory detectorFactory;
+    boost::shared_ptr<PoldiAbstractDetector> detector(detectorFactory.createDetector(std::string("helium3-detector")));
+    detector->loadConfiguration(ws_poldi_IPP);
 
+    // Create detector elements
+    auto elements = boost::irange(0, static_cast<int>(detector->elementCount()), 1);
 
+    // Map element indices to 2Theta-Values
+    std::vector<double> twoThetas;
+    twoThetas.reserve(detector->elementCount());
+    std::transform(elements.begin(), elements.end(), twoThetas.begin(), boost::bind<double>(&PoldiAbstractDetector::twoTheta, detector, _1));
 
+    // We will need sin(Theta) anyway, so we might just calculate those as well
+    std::vector<double> sinThetas;
+    sinThetas.reserve(detector->elementCount());
+    std::transform(twoThetas.cbegin(), twoThetas.cend(), sinThetas.begin(), [](double twoTheta) { return sin(twoTheta / 2.0); });
+
+    // Same goes for distances - map element index to distance, using detector object
+    std::vector<double> distances;
+    distances.reserve(detector->elementCount());
+    std::transform(elements.begin(), elements.end(), distances.begin(), boost::bind<double>(&PoldiAbstractDetector::distanceFromSample, detector, _1));
+
+    // Time of flight for neutrons with a wavelength of 1 Angstrom for each element
+    std::vector<double> tofFor1Angstrom;
+    tofFor1Angstrom.reserve(detector->elementCount());
+    std::transform(distances.cbegin(), distances.cend(), sinThetas.cbegin(), tofFor1Angstrom.begin(), [this, dist_chopper_sample] (const double distance, const double sinTheta) { return 2./CONVLAMV *1.e-7 * (dist_chopper_sample + distance) * sinTheta; });
 
 
 	////////////////////////////////////////////////////////////////////////
