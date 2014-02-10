af4686391fc7bab07a66e4dca3a0d513a4a3792d
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-02-10 10:41:47 +0000

    Refs #7838 Make the output options in C2E consistent.

--- Code/Mantid/MantidQt/CustomInterfaces/src/Indirect.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/Indirect.cpp
@@ -606,6 +606,12 @@ void Indirect::createRESfile(const QString& file)
   if ( m_uiForm.cal_ckPlotResult->isChecked() ) { pyInput +=	"plot = True\n"; }
   else { pyInput += "plot = False\n"; }
 
+  if ( m_uiForm.cal_ckVerbose->isChecked() ) { pyInput +=  "verbose = True\n"; }
+  else { pyInput += "verbose = False\n"; }
+
+  if ( m_uiForm.cal_ckSave->isChecked() ) { pyInput +=  "save = True\n"; }
+  else { pyInput += "save = False\n"; }
+
   QString rebinParam = QString::number(m_calDblMng->value(m_calResProp["ELow"])) + "," +
     QString::number(m_calDblMng->value(m_calResProp["EWidth"])) + "," +
     QString::number(m_calDblMng->value(m_calResProp["EHigh"]));
@@ -617,7 +623,7 @@ void Indirect::createRESfile(const QString& file)
     "background = " + background + "\n"
     "rebinParam = '" + rebinParam + "'\n"
     "file = " + file + "\n"
-    "ws = resolution(file, iconOpt, rebinParam, background, instrument, analyser, reflection, plotOpt = plot, factor="+scaleFactor+")\n"
+    "ws = resolution(file, iconOpt, rebinParam, background, instrument, analyser, reflection, Verbose=verbose, Plot=plot, Save=save, factor="+scaleFactor+")\n"
     "scaled = "+ scaled +"\n"
     "scaleFactor = "+m_uiForm.cal_leIntensityScaleMultiplier->text()+"\n"
     "backStart = "+QString::number(m_calDblMng->value(m_calCalProp["BackMin"]))+"\n"
@@ -1549,8 +1555,13 @@ void Indirect::calibCreate()
 
     reducer += "calib.execute(None, None)\n"
       "result = calib.result_workspace()\n"
-      "print result\n"
-      "SaveNexus(InputWorkspace=result, Filename=result+'.nxs')\n";
+      "print result\n";
+
+    if( m_uiForm.cal_ckSave->isChecked() )
+    {
+      reducer +=
+        "SaveNexus(InputWorkspace=result, Filename=result+'.nxs')\n";
+    }
 
     if ( m_uiForm.cal_ckPlotResult->isChecked() )
     {
@@ -1821,6 +1832,11 @@ void Indirect::sOfQwClicked()
     if ( m_uiForm.sqw_ckSave->isChecked() )
     {
       pyInput += "SaveNexus(InputWorkspace=sqwOutput, Filename=sqwOutput+'.nxs')\n";
+
+      if (m_uiForm.sqw_ckVerbose->isChecked())
+      {
+        pyInput += "logger.notice(\"Resolution file saved to default save directory.\")\n";
+      }
     }
 
     if ( m_uiForm.sqw_cbPlotType->currentText() == "Contour" )
