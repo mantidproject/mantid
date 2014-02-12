0d7fe3664215ff36a979ead545df35fba036ee93
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2014-02-12 15:21:39 +0000

    Refs #8839. Remove unused variable declarations.

--- Code/Mantid/Framework/DataHandling/src/LoadMuonNexus1.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadMuonNexus1.cpp
@@ -200,9 +200,6 @@ namespace Mantid
         }
       }
 
-      // Need to extract the user-defined output workspace name
-      Property *ws = getProperty("OutputWorkspace");
-      std::string localWSName = ws->value();
       // If multiperiod, will need to hold the Instrument & Sample for copying
       boost::shared_ptr<Instrument> instrument;
       boost::shared_ptr<Sample> sample;
