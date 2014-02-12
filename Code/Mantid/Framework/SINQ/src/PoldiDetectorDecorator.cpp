d76974a3e66e4382ac303114d7ba31e259bd0274
MichaelWedel <michael.wedel@psi.ch>
2014-02-12 10:02:13 +0100

    Added decorator interface for PoldiAbstractDetector

---
+++ Code/Mantid/Framework/SINQ/src/PoldiDetectorDecorator.cpp
@@ -0,0 +1,95 @@
+#include "MantidSINQ/PoldiDetectorDecorator.h"
+
+namespace Mantid
+{
+namespace Poldi
+{
+
+PoldiDetectorDecorator::PoldiDetectorDecorator(boost::shared_ptr<PoldiAbstractDetector> decoratedDetector) :
+    PoldiAbstractDetector(),
+    m_decoratedDetector()
+{
+    setDecoratedDetector(decoratedDetector);
+}
+
+void PoldiDetectorDecorator::setDecoratedDetector(boost::shared_ptr<PoldiAbstractDetector> detector)
+{
+    m_decoratedDetector = detector;
+
+    detectorSetHook();
+}
+
+boost::shared_ptr<PoldiAbstractDetector> PoldiDetectorDecorator::decoratedDetector()
+{
+    return m_decoratedDetector;
+}
+
+void PoldiDetectorDecorator::loadConfiguration(DataObjects::TableWorkspace_sptr detectorConfigurationWorkspace)
+{
+    UNUSED_ARG(detectorConfigurationWorkspace)
+}
+
+double PoldiDetectorDecorator::twoTheta(int elementIndex)
+{
+    if(m_decoratedDetector) {
+        return m_decoratedDetector->twoTheta(elementIndex);
+    } else {
+        throw std::runtime_error("No detector decorated!");
+    }
+}
+
+double PoldiDetectorDecorator::distanceFromSample(int elementIndex)
+{
+    if(m_decoratedDetector) {
+        return m_decoratedDetector->distanceFromSample(elementIndex);
+    } else {
+        throw std::runtime_error("No detector decorated!");
+    }
+}
+
+size_t PoldiDetectorDecorator::elementCount()
+{
+    if(m_decoratedDetector) {
+        return m_decoratedDetector->elementCount();
+    } else {
+        throw std::runtime_error("No detector decorated!");
+    }
+}
+
+size_t PoldiDetectorDecorator::centralElement()
+{
+    if(m_decoratedDetector) {
+        return m_decoratedDetector->centralElement();
+    } else {
+        throw std::runtime_error("No detector decorated!");
+    }
+}
+
+std::vector<int> PoldiDetectorDecorator::availableElements()
+{
+    if(m_decoratedDetector) {
+        return m_decoratedDetector->availableElements();
+    } else {
+        throw std::runtime_error("No detector decorated!");
+    }
+}
+
+std::pair<double, double> PoldiDetectorDecorator::qLimits(double lambdaMin, double lambdaMax)
+{
+    if(m_decoratedDetector) {
+        return m_decoratedDetector->qLimits(lambdaMin, lambdaMax);
+    } else {
+        throw std::runtime_error("No detector decorated!");
+    }
+}
+
+void PoldiDetectorDecorator::detectorSetHook()
+{
+
+}
+
+
+
+
+} // namespace Poldi
+} // namespace Mantid
