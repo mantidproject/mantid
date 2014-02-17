1f86385a2cd101989ccb90165eb7f894192b4efb
Jay Rainey <jawrainey@gmail.com>
2014-02-17 09:22:13 +0000

    Use fullName in search query. Refs #8823.

--- Code/Mantid/Framework/ICat/src/ICat4/ICat4Catalog.cpp
+++ Code/Mantid/Framework/ICat/src/ICat4/ICat4Catalog.cpp
@@ -172,7 +172,7 @@ namespace Mantid
       {
         joinClause.push_back("JOIN inves.investigationInstruments invInst");
         joinClause.push_back("JOIN invInst.instrument inst");
-        whereClause.push_back("inst.name = '" + inputs.getInstrument() + "'");
+        whereClause.push_back("inst.fullName = '" + inputs.getInstrument() + "'");
       }
 
       // Keywords
@@ -612,7 +612,7 @@ namespace Mantid
       std::string sessionID = Session::Instance().getSessionId();
       request.sessionId     = &sessionID;
 
-      std::string query = "Instrument.fullName ORDER BY name";
+      std::string query = "Instrument.fullName ORDER BY fullName";
       request.query     = &query;
 
       int result = icat.search(&request, &response);
