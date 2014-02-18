a24c1fd76fb85af13157e0394cefc320c1775d27
Jay Rainey <jawrainey@gmail.com>
2014-02-18 12:04:17 +0000

    Updated documentation. Refs #9026.

--- Code/Mantid/Framework/ICat/inc/MantidICat/CompositeCatalog.h
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CompositeCatalog.h
@@ -7,6 +7,31 @@ namespace Mantid
 {
   namespace ICat
   {
+    /**
+     CompositeCatalog is responsible for storing and performing options on multiple catalogues.
+
+     @author Jay Rainey, ISIS Rutherford Appleton Laboratory
+     @date 18/02/2014
+     Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+     This file is part of Mantid.
+
+     Mantid is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 3 of the License, or
+     (at your option) any later version.
+
+     Mantid is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     File change history is stored at: <https://github.com/mantidproject/mantid>.
+     Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
     class DLLExport CompositeCatalog : public API::ICatalog
     {
       public:
@@ -53,4 +78,4 @@ namespace Mantid
     };
   }
 }
-#endif
+#endif /* MANTID_ICAT_COMPOSITECATALOG_H_ */
