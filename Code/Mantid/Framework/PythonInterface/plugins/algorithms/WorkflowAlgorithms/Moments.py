1668c360957a697effc8261c80e60f037d73afa5
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-02-21 09:12:44 +0000

    Refs #5300 Tidying code.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/Moments.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/Moments.py
@@ -1,4 +1,4 @@
-"""*WIKI* 
+"""*WIKI*
 
 Calculates the <math>n^{th}</math> moment <math>M_n</math> of <math>y(Q,w)</math> where <math>M_n</math> is the integral of <math>w^n*y(Q,w)</math> over all w for <math>n=0</math> to 4.
 
@@ -7,11 +7,13 @@ Calculates the <math>n^{th}</math> moment <math>M_n</math> of <math>y(Q,w)</math
 from mantid.simpleapi import *
 from mantid.api import PythonAlgorithm, AlgorithmFactory, MatrixWorkspaceProperty, WorkspaceGroupProperty
 from mantid.kernel import Direction
-from mantid import config, logger
-import sys, os.path, numpy as np
+from mantid import logger
+
+import os.path
+import numpy as np
 
 class Moments(PythonAlgorithm):
- 
+
 	def category(self):
 		return "Workflow\\MIDAS;PythonAlgorithms"
 
@@ -28,10 +30,10 @@ class Moments(PythonAlgorithm):
 		self.declareProperty(name='Save', defaultValue=False, doc='Switch Save result to nxs file Off/On')
 
 		self.declareProperty(WorkspaceGroupProperty("OutputWorkspace", "", Direction.Output), doc="group_workspace workspace that includes all calculated moments.")
- 
+
 	def PyExec(self):
 		from IndirectCommon import CheckHistZero, CheckElimits, StartTime, EndTime, getDefaultWorkingDirectory
-		
+
 		sample_workspace = self.getPropertyValue('Sample')
 		output_workspace = self.getPropertyValue('OutputWorkspace')
 		factor = self.getProperty('Scale').value
@@ -42,7 +44,7 @@ class Moments(PythonAlgorithm):
 		Verbose = self.getProperty('Verbose').value
 		Plot = self.getProperty('Plot').value
 		Save = self.getProperty('Save').value
-		
+
 		StartTime('Moments')
 		num_spectra,num_w = CheckHistZero(sample_workspace)
 
@@ -73,12 +75,12 @@ class Moments(PythonAlgorithm):
 		for index in range(num_spectra):
 		    if Verbose:
 		        logger.notice('group_workspace %d at Q = %d' % (index+1, index))
-		    
+
 		    y = np.asarray(mtd[samWS].readY(index))
 
 		    S0 = y * delta_x
 		    m0 = np.sum(S0)
-		    
+
 		    S1 = (x * S0)
 		    m1 = np.sum(S1) / m0
 		    S2 = x * S1
@@ -87,31 +89,30 @@ class Moments(PythonAlgorithm):
 		    m3 = np.sum(S3) / m0
 		    S4 = x * S3
 		    m4 = np.sum(S4) / m0
-		    
+
 		    if Verbose:
 			    text = 'M0 = %f ; M2 = %f ; M4 = %f' % (m0, m2, m4)
 			    logger.notice(text)
-		    
+
 		    yM0.append(m0)
 		    yM1.append(m1)
 		    yM2.append(m2)
 		    yM4.append(m4)
 
 		output_workspace = output_workspace + '_Moments'
-		
+
 		#create output workspace
 		Q = np.arange(num_spectra)
 		extensions = ['_M0', '_M1', '_M2', '_M4']
 		y_data = [yM0, yM1, yM2, yM4]
 
 		for ext, data in zip(extensions, y_data):
-			CreateWorkspace(OutputWorkspace=output_workspace+ext, DataX=Q, DataY=data,
-		  	  Nspec=1, UnitX='MomentumTransfer')
+			CreateWorkspace(OutputWorkspace=output_workspace+ext, DataX=Q, DataY=data,Nspec=1, UnitX='MomentumTransfer')
 			CopyLogs(InputWorkspace=sample_workspace, OutputWorkspace=output_workspace+ext)
 			AddSampleLog(Workspace=output_workspace+ext, LogName="energy_min", LogType="Number", LogText=str(emin))
 			AddSampleLog(Workspace=output_workspace+ext, LogName="energy_max", LogType="Number", LogText=str(emax))
 			AddSampleLog(Workspace=output_workspace+ext, LogName="scale_factor", LogType="Number", LogText=str(factor))
-		
+
 		#group ouput workspace
 		group_workspaces = ','.join([output_workspace+ext for ext in extensions])
 		GroupWorkspaces(InputWorkspaces=group_workspaces,OutputWorkspace=output_workspace)
@@ -129,14 +130,14 @@ class Moments(PythonAlgorithm):
 		    self._plot_moments(output_workspace)
 
 		self.setProperty("OutputWorkspace", output_workspace)
-		
+
 		EndTime('Moments')
-	
+
 	def _plot_moments(self, inputWS):
 		from IndirectImport import import_mantidplot
 		mp = import_mantidplot()
-		
-		m0_plot=mp.plotSpectrum(inputWS+'_M0',0)
-		m2_plot=mp.plotSpectrum([inputWS+'_M2',inputWS+'_M4'],0)
+
+		mp.plotSpectrum(inputWS+'_M0',0)
+		mp.plotSpectrum([inputWS+'_M2',inputWS+'_M4'],0)
 
 AlgorithmFactory.subscribe(Moments)         # Register algorithm with Mantid
