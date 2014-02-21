53baf578459049085c0843577b39212ce1329907
Alex Buts <Alex.Buts@stfc.ac.uk>
2014-02-21 09:59:31 +0000

    refs #9048 GCC warnings

--- Code/Mantid/Framework/MDEvents/src/MDTransfModQ.cpp
+++ Code/Mantid/Framework/MDEvents/src/MDTransfModQ.cpp
@@ -361,8 +361,8 @@ namespace Mantid
 
     /// constructor;
     MDTransfModQ::MDTransfModQ():
-      m_Det(NULL),
       m_ex(0),m_ey(0),m_ez(1), 
+      m_Det(NULL),
       m_NMatrixDim(0), //uninitialized
       m_Emode(Kernel::DeltaEMode::Undefined), // uninitialized
       m_Ki(1.),m_Ei(1.),
