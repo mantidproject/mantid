b3270cdcc0b4f98bbe379b993055b04b532a592c
Jay Rainey <jawrainey@gmail.com>
2014-02-25 11:07:19 +0000

    Improve compsiteCatalog tests. Refs #9026.
    - I now check the functionality of each test in relation to how they are implemented in the catalog. That is, each method should append data to a workspace or vector.

--- Code/Mantid/Framework/ICat/test/CompositeCatalogTest.h
+++ Code/Mantid/Framework/ICat/test/CompositeCatalogTest.h
@@ -3,6 +3,7 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidAPI/WorkspaceFactory.h"
 #include "MantidICat/CatalogSearchParam.h"
 #include "MantidICat/CompositeCatalog.h"
 
@@ -29,39 +30,47 @@ class DummyCatalog : public ICatalog
       m_counter++;
     }
 
-    void search(const CatalogSearchParam&,ITableWorkspace_sptr&,const int &,const int &)
+    void search(const CatalogSearchParam&,ITableWorkspace_sptr& ws,const int &,const int &)
     {
+      // Each implementation of the ICatalog methods should always append data to the workspace.
+      ws->appendRow();
       m_counter++;
     }
 
     int64_t getNumberOfSearchResults(const CatalogSearchParam&)
     {
       m_counter++;
-      return 0;
+      // Return 5 to verify that the NumberOfResults gets added together across catalogs as expected.
+      return 5;
     }
 
-    void myData(ITableWorkspace_sptr&)
+    void myData(ITableWorkspace_sptr& ws)
     {
+      ws->appendRow();
       m_counter++;
     }
 
-    void getDataSets(const std::string& ,ITableWorkspace_sptr&)
+    void getDataSets(const std::string& ,ITableWorkspace_sptr& ws)
     {
+      ws->appendRow();
       m_counter++;
     }
 
-    void getDataFiles(const std::string&,ITableWorkspace_sptr&)
+    void getDataFiles(const std::string&,ITableWorkspace_sptr& ws)
     {
+      ws->appendRow();
       m_counter++;
     }
 
-    void listInstruments(std::vector<std::string>&)
+    void listInstruments(std::vector<std::string>& instruments)
     {
+      instruments.push_back("");
       m_counter++;
     }
 
-    void listInvestigationTypes(std::vector<std::string>&)
+    void listInvestigationTypes(std::vector<std::string>& investigations)
     {
+      investigations.push_back("");
       m_counter++;
     }
 
@@ -73,12 +82,6 @@ class DummyCatalog : public ICatalog
     void keepAlive()
     {
       m_counter++;
-    }
-
-    int keepAliveinminutes()
-    {
-      m_counter++;
-      return 0;
     }
 
     /// The counter used to verify each method works when
@@ -113,55 +116,62 @@ class CompositeCatalogTest : public CxxTest::TestSuite
     void testSearch()
     {
       CatalogSearchParam params;
-      ITableWorkspace_sptr ws;
-      int limit = 0;
-      int offset = 0;
-      createCompositeCatalog()->search(params,ws,offset,limit);
+      auto ws = WorkspaceFactory::Instance().createTable("TableWorkspace");
+      createCompositeCatalog()->search(params,ws,0,0);
+      // Check that the search results are appended correctly to the same table.
+      TS_ASSERT_EQUALS(ws->rowCount(),2);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
     void testGetNumberOfSearchResults()
     {
       CatalogSearchParam params;
-      createCompositeCatalog()->getNumberOfSearchResults(params);
+      // The number of results returned by getNumberOfSearchResults is hard-coded to 5
+      int64_t numberOfResults = createCompositeCatalog()->getNumberOfSearchResults(params);
+      // As two catalogs are created in createCompositeCatalog we verify that getNumberOfSearchResults functions correctly.
+      TS_ASSERT_EQUALS(numberOfResults,10);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
     void testMyData()
     {
-      ITableWorkspace_sptr ws;
+      auto ws = WorkspaceFactory::Instance().createTable("TableWorkspace");
       createCompositeCatalog()->myData(ws);
+      TS_ASSERT_EQUALS(ws->rowCount(),2);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
     void testGetDataSets()
     {
-      ITableWorkspace_sptr ws;
+      auto ws = WorkspaceFactory::Instance().createTable("TableWorkspace");
       std::string id = "";
       createCompositeCatalog()->getDataSets(id,ws);
+      TS_ASSERT_EQUALS(ws->rowCount(),2);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
     void testGetDataFiles()
     {
-      ITableWorkspace_sptr ws;
+      auto ws = WorkspaceFactory::Instance().createTable("TableWorkspace");
       std::string id = "";
       createCompositeCatalog()->getDataFiles(id,ws);
+      TS_ASSERT_EQUALS(ws->rowCount(),2);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
     void testListInstruments()
     {
-      std::vector<std::string> vector;
-      createCompositeCatalog()->listInstruments(vector);
-
+      std::vector<std::string> instruments;
+      createCompositeCatalog()->listInstruments(instruments);
+      TS_ASSERT_EQUALS(instruments.size(),2);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
     void testListInvestigationTypes()
     {
-      std::vector<std::string> vector;
-      createCompositeCatalog()->listInvestigationTypes(vector);
+      std::vector<std::string> investigations;
+      createCompositeCatalog()->listInvestigationTypes(investigations);
+      TS_ASSERT_EQUALS(investigations.size(),2);
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
@@ -179,21 +189,15 @@ class CompositeCatalogTest : public CxxTest::TestSuite
       TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
     }
 
-    void testKeepAliveinminutes()
-    {
-      createCompositeCatalog()->keepAliveinminutes();
-      TS_ASSERT_EQUALS(DummyCatalog::m_counter,2);
-    }
-
   private:
 
     /**
      * Create a compositeCatalog and add two DummyCatalogs to it.
      * @return A shared pointer to a CompositeCatalog.
      */
-    boost::shared_ptr<CompositeCatalog> createCompositeCatalog()
+    const boost::shared_ptr<CompositeCatalog> createCompositeCatalog()
     {
-      boost::shared_ptr<CompositeCatalog>compositeCatalog(new CompositeCatalog());
+      const boost::shared_ptr<CompositeCatalog>compositeCatalog(new CompositeCatalog());
       DummyCatalog::m_counter = 0;
 
       compositeCatalog->add(boost::make_shared<DummyCatalog>());
