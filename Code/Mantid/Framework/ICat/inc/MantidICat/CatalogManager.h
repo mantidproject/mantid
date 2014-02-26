e5412a801c360af922713c06fb2fe129dd0087b3
Jay Rainey <jawrainey@gmail.com>
2014-02-26 14:46:31 +0000

    Added initial structure of catalogManager. Refs #9084.

---
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CatalogManager.h
@@ -0,0 +1,71 @@
+#ifndef MANTID_ICAT_CATALOGMANAGERIMPL_H_
+#define MANTID_ICAT_CATALOGMANAGERIMPL_H_
+
+#include "MantidKernel/SingletonHolder.h"
+#include "MantidICat/CompositeCatalog.h"
+
+namespace Mantid
+{
+  namespace ICat
+  {
+    /**
+     This class is a singleton and is responsible for creating, destroying, and managing catalogs.
+
+     @author Jay Rainey, ISIS Rutherford Appleton Laboratory
+     @date 26/02/2014
+     Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+
+     This file is part of Mantid.
+
+     Mantid is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 3 of the License, or
+     (at your option) any later version.
+
+     Mantid is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     File change history is stored at: <https://github.com/mantidproject/mantid>.
+     Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
+    class DLLExport CatalogManagerImpl
+    {
+      public:
+        /// Create a new catalog, and add it to the list of active catalogs.
+        API::ICatalog_sptr create(const std::string facilityName);
+        /// Get a specific catalog using the sessionID.
+        API::ICatalog_sptr getCatalog(const std::string sessionID);
+        /// Get a list of all active catalogs.
+        boost::shared_ptr<CompositeCatalog> getCatalogs();
+        /// Destroy and remove a specific catalog from the active catalogs list.
+        void destroyCatalog(const std::string sessionID);
+        /// Destroy all active catalogs.
+        void destroyCatalogs();
+
+      private:
+        /// These methods are required to create a singleton.
+        friend struct Kernel::CreateUsingNew<CatalogManagerImpl>;
+        CatalogManagerImpl();
+        CatalogManagerImpl(const CatalogManagerImpl&);
+        CatalogManagerImpl& operator = (const CatalogManagerImpl&);
+        virtual ~CatalogManagerImpl();
+
+        // Holds a list of active catalogs and uses their sessionId as unique identifier.
+        std::map<std::string, API::ICatalog_sptr> m_activeCatalogs;
+        // Used to return the compositeCatalog that holds the created catalogs.
+        boost::shared_ptr<CompositeCatalog> m_compositeCatalog;
+    };
+
+    #ifdef _WIN32
+        template class DLLExport Kernel::SingletonHolder<CatalogManagerImpl>;
+    #endif
+        typedef Kernel::SingletonHolder<CatalogManagerImpl> CatalogManager;
+
+  }
+}
+#endif /* MANTID_ICAT_CATALOGMANAGERIMPL_H_ */
