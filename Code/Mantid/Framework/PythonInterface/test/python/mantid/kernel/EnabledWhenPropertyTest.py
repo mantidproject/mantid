5d3b45078f27dd0ac8ee71390b56ed26861ae3ca
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-03-03 11:11:17 +0000

    Export EnabledWhenProperty to Python.
    Refs #8947

---
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/EnabledWhenPropertyTest.py
@@ -0,0 +1,25 @@
+import unittest
+from mantid.kernel import EnabledWhenProperty, PropertyCriterion
+
+class EnabledWhenPropertyTest(unittest.TestCase):
+
+    def test_construction_with_name_criterion_only_succeeds(self):
+        p = EnabledWhenProperty("OtherProperty", PropertyCriterion.IsDefault)
+
+    def test_construction_with_name_criterion_value_succeeds(self):
+        p = EnabledWhenProperty("OtherProperty", PropertyCriterion.IsEqualTo, "value")
+
+    #------------ Failure cases ------------------
+    
+    def test_default_construction_raises_error(self):
+        try:
+            EnabledWhenProperty()
+            self.fail("Expected default constructor to raise an error")
+        except Exception, e:
+            # boost.python.ArgumentError are not catchable
+            if "Python argument types in" not in str(e):
+                raise RuntimeError("Unexpected exception type raised")
+
+
+if __name__ == '__main__':
+    unittest.main()
