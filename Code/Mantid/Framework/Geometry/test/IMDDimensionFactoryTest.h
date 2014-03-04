271efdf72040a7c9bc1635b83a19637dd27edfed
Russell Taylor <taylorrj@ornl.gov>
2014-03-04 14:53:31 -0500

    Re #9118. Update test to use factory in way I expect it to end up.
    Obviously, this won't compile until I've refactored the class itself.

--- Code/Mantid/Framework/Geometry/test/IMDDimensionFactoryTest.h
+++ Code/Mantid/Framework/Geometry/test/IMDDimensionFactoryTest.h
@@ -5,6 +5,7 @@
 #include "MantidGeometry/MDGeometry/IMDDimensionFactory.h"
 #include <Poco/DOM/DOMParser.h>
 #include <Poco/DOM/Document.h>
+#include <Poco/AutoPtr.h>
 
 using namespace Mantid::Geometry;
 
@@ -12,7 +13,7 @@ class IMDDimensionFactoryTest: public CxxTest::TestSuite
 {
 private:
 
-  static Poco::XML::Element* constructDimensionWithUnits()
+  std::string constructDimensionWithUnitsXMLString()
   {
     std::string xmlToParse = std::string("<Dimension ID=\"qz\">") + "<Name>Qz</Name>"
         + "<Units>Cubits</Units>"
@@ -20,77 +21,66 @@ private:
         + "<NumberOfBins>8</NumberOfBins>"
         + "<ReciprocalDimensionMapping>q3</ReciprocalDimensionMapping>" + "</Dimension>";
 
-    Poco::XML::DOMParser pParser;
-    Poco::XML::Document* pDoc = pParser.parseString(xmlToParse);
-    return pDoc->documentElement();
+    return xmlToParse;
   }
 
-  static Poco::XML::Element* constructDimensionWithoutUnits()
+  std::string constructDimensionWithoutUnitsXMLString()
   {
     std::string xmlToParse = std::string("<Dimension ID=\"qz\">") + "<Name>Qz</Name>"
         + "<UpperBounds>3</UpperBounds>" + "<LowerBounds>-3</LowerBounds>"
         + "<NumberOfBins>8</NumberOfBins>"
         + "<ReciprocalDimensionMapping>q3</ReciprocalDimensionMapping>" + "</Dimension>";
 
-    Poco::XML::DOMParser pParser;
-    Poco::XML::Document* pDoc = pParser.parseString(xmlToParse);
-    return pDoc->documentElement();
+    return xmlToParse;
   }
 
-  static std::string constructNonReciprocalDimensionXMLString()
+  std::string constructNonReciprocalDimensionXMLString()
   {
     return std::string("<Dimension ID=\"en\">") + "<Name>Energy</Name>"
         + "<UpperBounds>150</UpperBounds>" + "<LowerBounds>0</LowerBounds>"
         + "<NumberOfBins>4</NumberOfBins>" + "</Dimension>";
   }
 
-  static Poco::XML::Element* constructNonReciprocalDimensionXML()
+  Poco::AutoPtr<Poco::XML::Document> constructNonReciprocalDimensionXML()
   {
     std::string xmlToParse = constructNonReciprocalDimensionXMLString();
     Poco::XML::DOMParser pParser;
-    Poco::XML::Document* pDoc = pParser.parseString(xmlToParse);
-    return pDoc->documentElement();
+    return pParser.parseString(xmlToParse);
   }
 
 public:
 
   void testCorrectGeneration()
   {
-    using namespace Mantid::Geometry;
-    IMDDimensionFactory factory(constructDimensionWithUnits());
-    IMDDimension* dimension = factory.create();
+    IMDDimension_const_sptr dimension = IMDDimensionFactory::create(constructDimensionWithUnitsXMLString());
     TS_ASSERT_EQUALS("Cubits", dimension->getUnits());
     TS_ASSERT_EQUALS("Qz", dimension->getName());
     TS_ASSERT_EQUALS("qz", dimension->getDimensionId());
     TS_ASSERT_EQUALS(-3, dimension->getMinimum());
     TS_ASSERT_EQUALS(3, dimension->getMaximum());
     TS_ASSERT_EQUALS(8, dimension->getNBins());
-    delete dimension;
   }
 
   void testCorrectGenerationWithoutUnits()
   {
-    using namespace Mantid::Geometry;
-    IMDDimensionFactory factory(constructDimensionWithoutUnits());
-    IMDDimension* dimension = factory.create();
+    IMDDimension_const_sptr dimension = IMDDimensionFactory::create(constructDimensionWithoutUnits());
     TS_ASSERT_EQUALS("None", dimension->getUnits());
     TS_ASSERT_EQUALS("Qz", dimension->getName());
     TS_ASSERT_EQUALS("qz", dimension->getDimensionId());
     TS_ASSERT_EQUALS(-3, dimension->getMinimum());
     TS_ASSERT_EQUALS(3, dimension->getMaximum());
     TS_ASSERT_EQUALS(8, dimension->getNBins());
-    delete dimension;
   }
 
   void testStaticCreation()
   {
     std::string xmlToParse = constructNonReciprocalDimensionXMLString();
-
-    IMDDimensionFactory factoryA = IMDDimensionFactory::createDimensionFactory(xmlToParse);
-    IMDDimensionFactory factoryB(constructNonReciprocalDimensionXML());
+    IMDDimension_const_sptr viaString = IMDDimensionFactory::create(xmlToParse);
+    auto document = constructNonReciprocalDimensionXML();
+    IMDDimension_const_sptr viaXML = IMDDimensionFactory::create(document->documentElement());
 
     //Constructed either way, the products should be equivalent
-    TSM_ASSERT_EQUALS("Created through either route, the products should be equal", factoryA.create()->getDimensionId(), factoryB.create()->getDimensionId());
+    TSM_ASSERT_EQUALS("Created through either route, the products should be equal", viaString->getDimensionId(), viaXML->getDimensionId());
 
   }
 };
