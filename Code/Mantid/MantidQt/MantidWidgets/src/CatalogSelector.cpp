fa50a4f499626b1f7d6eebf39f40f26950a4be9a
Jay Rainey <jawrainey@gmail.com>
2014-03-10 15:38:44 +0000

    Display the facility names of catalogs to search through. Refs #9112.

--- Code/Mantid/MantidQt/MantidWidgets/src/CatalogSelector.cpp
+++ Code/Mantid/MantidQt/MantidWidgets/src/CatalogSelector.cpp
@@ -1,6 +1,7 @@
 #include "MantidQtMantidWidgets/CatalogSelector.h"
 
 #include <QDesktopWidget>
+#include <QListWidgetItem>
 
 namespace MantidQt
 {
@@ -18,6 +19,22 @@ namespace MantidQt
       // Centre the GUI on screen.
       this->setGeometry(QStyle::alignedRect(Qt::LeftToRight,Qt::AlignCenter,
           this->window()->size(),QDesktopWidget().availableGeometry()));
+    }
+
+    /**
+     * Populate the ListWidget with the facilities of the catalogs the user is logged in to.
+     */
+    void CatalogSelector::populateTable()
+    {
+      auto session = Mantid::API::CatalogManager::Instance().getActiveSessions();
+
+      for (unsigned row = 0; row < session.size(); ++row)
+      {
+        QListWidgetItem* item = new QListWidgetItem(QString::fromStdString(session.at(row)->getFacility()));
+        // Set sessionID to user specific meta-data to easily obtain it later.
+        item->setData(Qt::UserRole,QVariant(QString::fromStdString(session.at(row)->getSessionId())));
+        m_uiForm.selectedCatalogs->insertItem(row,item);
+      }
     }
 
   } // namespace MantidWidgets
