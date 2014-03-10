0bf86f34320044ff136004f99f6817f868d92e3e
Keith Brown <keith.brown@stfc.ac.uk>
2014-03-10 14:28:22 +0000

    Refs #8958 Added wiki information
    Added some wiki information. Basically enough to show that this is a specific-use save algorithm as well as explicitly mentioning that the result is not meant to be loaded back into mantid

--- Code/Mantid/Framework/DataHandling/src/SaveANSTOAscii.cpp
+++ Code/Mantid/Framework/DataHandling/src/SaveANSTOAscii.cpp
@@ -1,7 +1,7 @@
 /*WIKI* 
-
+SaveANSTOAscii is an export-only Acii-based save format with no associated loader. It is based on a python script by Maximilian Skoda, written for the ISIS Reflectometry GUI
 ==== Limitations ====
-
+While Files saved with SaveANSTOAscii can be loaded back into mantid using LoadAscii, the resulting workspaces won't be usful as the data written by SaveANSTOAscii is not in the normal X,Y,E,DX format.
 *WIKI*/
 //----------------------------------------------------------------------
 // Includes
