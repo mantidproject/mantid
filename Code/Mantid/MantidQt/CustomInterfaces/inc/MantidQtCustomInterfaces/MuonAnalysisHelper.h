7325e87e810656ce40bdbedf7900c3439eb6ac44
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2014-03-13 15:07:12 +0000

    Refs #8547. Use the proper DLL export macro
    Should fix windows builds.

--- Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/MuonAnalysisHelper.h
+++ Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/MuonAnalysisHelper.h
@@ -1,6 +1,7 @@
 #ifndef MANTID_CUSTOMINTERFACES_MUONANALYSISHELPER_H_
 #define MANTID_CUSTOMINTERFACES_MUONANALYSISHELPER_H_
 
+#include "MantidQtCustomInterfaces/DllConfig.h"
 #include "MantidKernel/System.h"
 #include "MantidAPI/MatrixWorkspace.h"
 
@@ -20,35 +21,35 @@ using namespace Mantid::API;
 using namespace Mantid::Kernel;
 
 /// Sets double validator for specified field
-DLLExport void setDoubleValidator(QLineEdit* field, bool allowEmpty = false);
+MANTIDQT_CUSTOMINTERFACES_DLL void setDoubleValidator(QLineEdit* field, bool allowEmpty = false);
 
 /// Returns a first period MatrixWorkspace in a run workspace
-DLLExport MatrixWorkspace_sptr firstPeriod(Workspace_sptr ws);
+MANTIDQT_CUSTOMINTERFACES_DLL MatrixWorkspace_sptr firstPeriod(Workspace_sptr ws);
 
 /// Validates the field and returns the value
-DLLExport double getValidatedDouble(QLineEdit* field, const QString& defaultValue,
+MANTIDQT_CUSTOMINTERFACES_DLL double getValidatedDouble(QLineEdit* field, const QString& defaultValue,
                                     const QString& valueDescr, Logger& log);
 
 /// Returns a number of periods in a run workspace
-DLLExport size_t numPeriods(Workspace_sptr ws);
+MANTIDQT_CUSTOMINTERFACES_DLL size_t numPeriods(Workspace_sptr ws);
 
 /// Print various information about the run
-DLLExport void printRunInfo(MatrixWorkspace_sptr runWs, std::ostringstream& out);
+MANTIDQT_CUSTOMINTERFACES_DLL void printRunInfo(MatrixWorkspace_sptr runWs, std::ostringstream& out);
 
 /// Get a run label for the workspace
-DLLExport std::string getRunLabel(const Workspace_sptr& ws);
+MANTIDQT_CUSTOMINTERFACES_DLL std::string getRunLabel(const Workspace_sptr& ws);
 
 /// Get a run label for a list of workspaces
-DLLExport std::string getRunLabel(std::vector<Workspace_sptr> wsList);
+MANTIDQT_CUSTOMINTERFACES_DLL std::string getRunLabel(std::vector<Workspace_sptr> wsList);
 
 /// Sums a list of workspaces together
-DLLExport Workspace_sptr sumWorkspaces(const std::vector<Workspace_sptr>& workspaces);
+MANTIDQT_CUSTOMINTERFACES_DLL Workspace_sptr sumWorkspaces(const std::vector<Workspace_sptr>& workspaces);
 
 /**
  * A class which deals with auto-saving the widget values. Widgets are registered and then on any
  * change, their value is stored using QSettings.
  */
-class Q_DECL_EXPORT WidgetAutoSaver : QObject
+class MANTIDQT_CUSTOMINTERFACES_DLL WidgetAutoSaver : QObject
 {
   Q_OBJECT
 
@@ -102,7 +103,7 @@ private:
 };
 
 /// Validator which accepts valid doubles OR empty strings
-class Q_DECL_EXPORT DoubleOrEmptyValidator : public QDoubleValidator
+class MANTIDQT_CUSTOMINTERFACES_DLL DoubleOrEmptyValidator : public QDoubleValidator
 {
   Q_OBJECT
 
