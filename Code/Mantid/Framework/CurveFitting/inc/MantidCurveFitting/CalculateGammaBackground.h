0b44233e6593a3665bbc567f912b404d571775ae
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-03-13 14:56:18 +0000

    Formatting changes to put the methods back in the right order
    Refs #9166

--- Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/CalculateGammaBackground.h
+++ Code/Mantid/Framework/CurveFitting/inc/MantidCurveFitting/CalculateGammaBackground.h
@@ -61,6 +61,8 @@ namespace Mantid
         double gaussWidth;
       };
 
+      /// Avoid nested try catch in openmp loop by returning boolean if spectrum was corrceted
+      bool calculateBackground(size_t inputIndex, size_t ouptutIndex);
       /// Calculate & correct the given index of the input workspace
       void applyCorrection(const size_t inputIndex,const size_t outputIndex);
       /// Compute the expected spectrum from a given detector
@@ -118,8 +120,6 @@ namespace Mantid
 
       /// Pointer to progress reporting
       API::Progress *m_progress;
-      // internal method called within the loop to avoid true-catch operations which broke Microsoft compiler compiling in parallel
-      bool calculateBackground(size_t inputIndex, size_t ouptutIndex);
     };
 
 
