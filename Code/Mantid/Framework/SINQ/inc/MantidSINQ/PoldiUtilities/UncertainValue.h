68fcd8d557681469cb1d4732dcd92807bb16f82a
MichaelWedel <michael.wedel@psi.ch>
2014-03-15 18:49:50 +0100

    Added license headers to helper classes

--- Code/Mantid/Framework/SINQ/inc/MantidSINQ/PoldiUtilities/UncertainValue.h
+++ Code/Mantid/Framework/SINQ/inc/MantidSINQ/PoldiUtilities/UncertainValue.h
@@ -9,6 +9,37 @@ namespace Mantid
 {
 namespace Poldi
 {
+
+/** UncertainValue :
+
+    A value with error. This class is convenient when numbers with uncertainties
+    have to be stored. It supports conversion to double (then only the value
+    is supplied, without the uncertainty) and arithmetic operations with doubles,
+    preserving the errors.
+
+        @author Michael Wedel, Paul Scherrer Institut - SINQ
+        @date 11/03/2014
+
+    Copyright © 2014 PSI-MSS
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+  */
 class MANTID_SINQ_DLL UncertainValue
 {
 public:
@@ -42,7 +73,6 @@ UncertainValue operator*(double d, const UncertainValue &v);
 UncertainValue operator/(double d, const UncertainValue &v);
 UncertainValue operator+(double d, const UncertainValue &v);
 UncertainValue operator-(double d, const UncertainValue &v);
-
 
 }
 }
