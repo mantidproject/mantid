fffb57e0c422f2e0f52a34dba61e1750559b0c22
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-04-03 13:38:39 +0100

    Export Unit::utf8Label to Python.
    It returns a standard Python unicode string.
    Refs #9252

--- Code/Mantid/Framework/PythonInterface/mantid/kernel/src/Exports/Unit.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/src/Exports/Unit.cpp
@@ -18,6 +18,16 @@ namespace
     PyErr_Warn(PyExc_DeprecationWarning, "'name' is deprecated, use 'caption' instead.");
     return self.caption();
   }
+
+  /**
+   * @param self A reference to the calling object
+   * @return A new Python unicode string with the contents of the utf8Label
+   */
+  PyObject * utf8LabelToUnicode(Unit & self)
+  {
+    const auto label = self.utf8Label();
+    return PyUnicode_FromWideChar(label.c_str(), label.size());
+  }
 }
 
 void export_Unit()
@@ -28,6 +38,7 @@ void export_Unit()
     .def("name", &deprecatedName, "Return the full name of the unit (deprecated, use caption)")
     .def("caption", &Unit::caption, "Return the full name of the unit")
     .def("label", &Unit::label, "Returns a label to be printed on the axis")
+    .def("utf8Label", &utf8LabelToUnicode, "Returns a unicode string to be printed on the axis")
     .def("unitID", &Unit::unitID, "Returns the string ID of the unit. This may/may not match its name")
   ;
 
--- Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/UnitsTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/kernel/UnitsTest.py
@@ -1,5 +1,6 @@
 import unittest
 from mantid.kernel import UnitFactory, Unit, Label
+import types
 
 class UnitsTest(unittest.TestCase):
 
@@ -10,6 +11,13 @@ class UnitsTest(unittest.TestCase):
         label_unit.setLabel("Temperature", "K")
         self.assertEquals("Temperature", label_unit.caption())
         self.assertEquals("K", label_unit.label())
+
+    def test_utf8Label_is_converted_to_unicode_object(self):
+        tof = UnitFactory.Instance().create("TOF")
+        unit_lbl = tof.utf8Label()
+        self.assertTrue(isinstance(unit_lbl, types.UnicodeType))
+        self.assertEquals(u"\u03bcs", unit_lbl)
+
 
 if __name__ == '__main__':
     unittest.main()
