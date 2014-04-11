dc9870993067909f40857b92b9c3f9afb0d9f0bc
Russell Taylor <taylorrj@ornl.gov>
2014-04-11 14:31:06 -0400

    Re #8358. Remove deletes that have been replicated in another ticket.
    #8685 changed this test and handled the delete in an alternative way.

--- Code/Mantid/Framework/DataObjects/test/EventListTest.h
+++ Code/Mantid/Framework/DataObjects/test/EventListTest.h
@@ -1476,7 +1476,6 @@ public:
 
     //No events in the first ouput 0-99
     TS_ASSERT_EQUALS( outputs[0]->getNumberEvents(), 0);
-    delete outputs[0];
 
     for (size_t i=1; i<10; i++)
     {
@@ -1492,7 +1491,6 @@ public:
         //Odd
         TS_ASSERT_EQUALS( myOut->getNumberEvents(), 0);
       }
-      delete myOut;
     }
   }
 
