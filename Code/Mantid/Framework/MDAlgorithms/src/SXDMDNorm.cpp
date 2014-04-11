052654e6812c5fcd84d9aa9f04caa46839095cbb
Andrei Savici <saviciat@ornl.gov>
2014-04-11 17:06:52 -0400

    Start calculating intersections. Refs #9202

--- Code/Mantid/Framework/MDAlgorithms/src/SXDMDNorm.cpp
+++ Code/Mantid/Framework/MDAlgorithms/src/SXDMDNorm.cpp
@@ -81,12 +81,25 @@ namespace MDAlgorithms
 
       declareProperty(new WorkspaceProperty<IMDWorkspace>("InputWorkspace","",Direction::Input), "An input MDWorkspace.");
 
-      this->initSlicingProps();
+      //this->initSlicingProps();
+      //this->m_axisAligned=true;
+
+      std::string dimChars = getDimensionChars();
+
+      // --------------- Axis-aligned properties ---------------------------------------
+      for (size_t i=0; i<dimChars.size(); i++)
+      {
+        std::string dim(" "); dim[0] = dimChars[i];
+        std::string propName = "AlignedDim" + dim;
+        declareProperty(new PropertyWithValue<std::string>(propName,"",Direction::Input),
+            "Binning parameters for the " + Strings::toString(i) + "th dimension.\n"
+            "Enter it as a comma-separated list of values with the format: 'name,minimum,maximum,number_of_bins'. Leave blank for NONE.");
+      }
 
       auto wsValidator = boost::make_shared<CompositeValidator>();
       wsValidator->add<WorkspaceUnitValidator>("Momentum");
 
-      declareProperty(new WorkspaceProperty<>("InputVanadiumWorkspace","",Direction::Input,wsValidator), "An input workspace containing vanadium data.");
+      //declareProperty(new WorkspaceProperty<>("InputVanadiumWorkspace","",Direction::Input,wsValidator), "An input workspace containing vanadium data.");
 
       declareProperty(new WorkspaceProperty<Workspace>("OutputWorkspace","",Direction::Output), "A name for the output data MDHistoWorkspace.");
       declareProperty(new WorkspaceProperty<MDHistoWorkspace>("OutputNormalizationWorkspace","",Direction::Output), "A name for the output normalization MDHistoWorkspace.");
@@ -97,54 +110,75 @@ namespace MDAlgorithms
    */
   void SXDMDNorm::exec()
   {
+      m_inputWS=getProperty("InputWorkspace");
+      hMin=m_inputWS->getDimension(0)->getMinimum();
+      kMin=m_inputWS->getDimension(1)->getMinimum();
+      lMin=m_inputWS->getDimension(2)->getMinimum();
+      hMax=m_inputWS->getDimension(0)->getMaximum();
+      kMax=m_inputWS->getDimension(1)->getMaximum();
+      lMax=m_inputWS->getDimension(2)->getMaximum();
+      for(size_t i=3;i<m_inputWS->getNumDims();i++)
+      {
+          otherDimsMin.push_back(m_inputWS->getDimension(i)->getMinimum());
+          otherDimsMax.push_back(m_inputWS->getDimension(i)->getMaximum());
+      }
       // Run BinMD
       Workspace_sptr outputWS = getProperty("OutputWorkspace");
       auto props=getProperties();
       IAlgorithm_sptr bin = createChildAlgorithm("BinMD",0.0,0.9);
+      bin->setPropertyValue("AxisAligned","1");
       for(auto it=props.begin();it!=props.end();++it)
       {
-          if((*it)->name()!="InputVanadiumWorkspace")
+          if(((*it)->name()!="InputVanadiumWorkspace")&&((*it)->name()!="OutputNormalizationWorkspace"))
               bin->setPropertyValue((*it)->name(),(*it)->value());
       }
       bin->executeAsChildAlg();
       outputWS=bin->getProperty("OutputWorkspace");
-      setProperty("OutputWorkspace", outputWS);
+      this->setProperty("OutputWorkspace", outputWS);
 
       //copy the MDHisto workspace, and change signals and errors to 0.
       m_normWS=MDHistoWorkspace_sptr(new MDHistoWorkspace(*(boost::dynamic_pointer_cast<MDHistoWorkspace>(outputWS))));
       m_normWS->setTo(0.,0.,0.);
+
+      //get indices of the original dimensions in the output workspace, and if not found, the corresponding dimension is integrated
+      Mantid::Kernel::Matrix<coord_t> mat=m_normWS->getTransformFromOriginal(0)->makeAffineMatrix();
+
+
+
       for (uint16_t expi=0;expi<m_normWS->getNumExperimentInfo();expi++)
       {
           //get detector ID, no monitors
           std::vector<detid_t> detIDS=m_normWS->getExperimentInfo(expi)->getInstrument()->getDetectorIDs(true);
           //TODO make parallel
-          for(int i=0;i<static_cast<int>(detIDS.size());i++)
+          for(int i=0;i<1;i++)//static_cast<int>(detIDS.size());i++)
           {
               Mantid::Geometry::IDetector_const_sptr detector=m_normWS->getExperimentInfo(expi)->getInstrument()->getDetector(detIDS[i]);
-            if(!detector->isMonitor())
-            {
-                std::vector<Mantid::Kernel::VMD> intersections=calculateIntersections(expi,detector);
-                if(!intersections.empty())
-                {
+              if(!detector->isMonitor()&&!detector->isMasked())
+              {
+                  std::vector<Mantid::Kernel::VMD> intersections=calculateIntersections(expi,detector);
+                  if(!intersections.empty())
+                  {
                     //calculate indices
                     //add to the correct signal at that particular index
                     //NOTE: if parallel it has to be atomic
-                }
-            }
+                  }
+              }
           }
       }
-      setProperty("OutputNormalizationWorkspace",m_normWS);
 
+
+      this->setProperty("OutputNormalizationWorkspace",m_normWS);
 
   }
 
 
   std::vector<Mantid::Kernel::VMD> SXDMDNorm::calculateIntersections(uint16_t expIndex, Mantid::Geometry::IDetector_const_sptr detector)
   {
-
-     // VMD smallestMomentum(m_nDims+1), largestMomentum(m_nDims+1);
-     //m_normWS->getExperimentInfo(0)->run().getGoniometer().getR()
-
+      // VMD smallestMomentum(m_nDims+1), largestMomentum(m_nDims+1);
+      //m_normWS->getExperimentInfo(0)->run().getGoniometer().getR()
+      Mantid::Kernel::Matrix<coord_t> mat=m_normWS->getTransformFromOriginal(0)->makeAffineMatrix();
+      size_t NDims=mat.size().second;
+      g_log.warning()<<mat;
       std::vector<Mantid::Kernel::VMD> intersections;
       return intersections;
   }
