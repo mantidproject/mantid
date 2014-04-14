eb06e215bafb5396752bd7fdbca705a7e4fbd9ab
Pete Peterson <petersonpf@ornl.gov>
2014-04-14 15:15:12 -0400

    Added gomp to the module linking flags.
    Re #9328. It appears that make appends the compile flags when
    calling the linker and ninja does not.

--- Code/Mantid/Build/CMake/CommonSetup.cmake
+++ Code/Mantid/Build/CMake/CommonSetup.cmake
@@ -214,6 +214,12 @@ include ( VersionNumber )
 ###########################################################################
 
 find_package ( OpenMP )
+if ( OPENMP_FOUND )
+  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}" )
+  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}" )
+  set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${OpenMP_CXX_FLAGS}" )
+endif ()
+
 
 ###########################################################################
 # Add linux-specific things
