2c4811eb11769454b665adbeb948a551d9de82a7
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-04-29 12:10:06 +0100

    Refs #8913 Export Workspace/Algorithm/Property histories to python

--- Code/Mantid/Framework/PythonInterface/mantid/api/src/Exports/WorkspaceHistory.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/api/src/Exports/WorkspaceHistory.cpp
@@ -1,21 +1,61 @@
 #include "MantidAPI/WorkspaceHistory.h"
+#include "MantidAPI/AlgorithmHistory.h"
 #include "MantidAPI/IAlgorithm.h"
+
 #include <boost/python/class.hpp>
-#include <boost/python/self.hpp>
+#include <boost/python/copy_const_reference.hpp>
+#include <boost/python/list.hpp>
 #include <boost/python/operators.hpp>
 #include <boost/python/register_ptr_to_python.hpp>
+#include <boost/python/self.hpp>
 
 using Mantid::API::WorkspaceHistory;
+using Mantid::API::AlgorithmHistory;
 using Mantid::API::IAlgorithm;
 using namespace boost::python;
+
+/**
+ * Return a Python list of history objects from the workspace history as this is
+ * far easier to work with than a set
+ * @param self :: A reference to the WorkspaceHistory that called this method
+ * @returns A python list created from the set of algorithm histories
+ */
+boost::python::object getHistoriesAsList(WorkspaceHistory& self)
+{
+  boost::python::list names;
+  const auto histories = self.getAlgorithmHistories();
+  std::set<AlgorithmHistory>::const_iterator iend = histories.end();
+  for(std::set<AlgorithmHistory>::const_iterator itr = histories.begin(); itr != iend; ++itr)
+  {
+    names.append(*itr);
+  }
+  return names;
+}
 
 void export_WorkspaceHistory()
 {
   register_ptr_to_python<WorkspaceHistory*>();
 
   class_<WorkspaceHistory, boost::noncopyable>("WorkspaceHistory", no_init)
+
+    .def("getAlgorithmHistories", &getHistoriesAsList,
+         "Returns a list of algorithm histories for this workspace history.")
+    
+    .def("getAlgorithmHistory", &WorkspaceHistory::getAlgorithmHistory, 
+          arg("index"),
+          return_value_policy<copy_const_reference>(),
+         "Returns the algorithm history at the given index in the history")
+    
+    .def("size", &WorkspaceHistory::size, 
+         "Returns the number of algorithms in the immediate history")    
+
+    .def("empty", &WorkspaceHistory::empty, 
+         "Returns whether the history has any entries")
+    
     .def("lastAlgorithm", &WorkspaceHistory::lastAlgorithm, "Returns the last algorithm run on this workspace so that its properties can be accessed")
+    
     .def("getAlgorithm", &WorkspaceHistory::getAlgorithm, "Returns the algorithm at the given index in the history")
+    
     // ----------------- Operators --------------------------------------
     .def("__getitem__", &WorkspaceHistory::getAlgorithm)
     .def(self_ns::str(self))
