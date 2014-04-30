0efcd8774fa5e3b6f9e1013e921a9aa30984e98e
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-04-30 16:38:42 +0100

    Refs #8913 Fix broken unit tests.

--- Code/Mantid/Framework/PythonInterface/test/python/mantid/api/AlgorithmHistoryTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/api/AlgorithmHistoryTest.py
@@ -1,6 +1,39 @@
 import unittest
-from mantid.simpleapi import Load, CreateTransmissionWorkspaceAuto, _set_properties
-from mantid import AlgorithmManager, mtd
+from mantid.simpleapi import Load, CreateTransmissionWorkspaceAuto, CreateWorkspace, _set_properties
+from mantid.api import *
+from mantid.kernel import *
+
+
+class ChildAlg(PythonAlgorithm):
+
+    def PyInit(self):
+        pass
+
+    def PyExec(self):
+        pass
+
+AlgorithmFactory.subscribe(ChildAlg)
+
+
+class ParentAlg(DataProcessorAlgorithm):
+
+    def PyInit(self):
+        self.declareProperty(MatrixWorkspaceProperty('OutputWorkspace', '', Direction.Output),
+                             doc="Name to give the output workspace.")
+
+    def PyExec(self):
+        ws_name = self.getProperty("OutputWorkspace").value
+        alg = self.createChildAlgorithm('ChildAlg', recordHistory=False)
+        alg.initialize()
+        args = {}
+        kwargs = {}
+        _set_properties(alg, *args, **kwargs)
+        alg.execute()
+
+        ws = CreateWorkspace([0, 1, 2], [0, 1, 2], OutputWorkspace=ws_name)
+        self.setProperty('OutputWorkspace', ws)
+
+AlgorithmFactory.subscribe(ParentAlg)
 
 
 class AlgorithmHistoryTest(unittest.TestCase):
@@ -33,28 +66,17 @@ class AlgorithmHistoryTest(unittest.TestCase):
         self.assertGreater(len(basic_child_algs), 0)
 
     def test_disable_history(self):
-        trans1 = Load('INTER00013463.nxs', OutputWorkspace="trans1")
         ws_name = '__tmp_test_algorithm_history'
 
-        # create and run algorithm without nested history capturing
-        alg = AlgorithmManager.createChildAlgorithm('CreateTransmissionWorkspaceAuto', recordHistory=False)
+        alg = AlgorithmManager.createUnmanaged('ParentAlg')
         alg.initialize()
-        args = {}
-        kwargs = {'FirstTransmissionRun': trans1, 'AnalysisMode': "PointDetectorAnalysis",
-                  'OutputWorkspace': ws_name}
-        _set_properties(alg, *args, **kwargs)
+        alg.setProperty("OutputWorkspace", ws_name)
         alg.execute()
-
         history = mtd[ws_name].getHistory()
 
         alg_hists = history.getAlgorithmHistories()
-        self.assertEquals(history.size(), 2)
-        self.assertEquals(len(alg_hists), 2)
-
-        parent_alg = history.getAlgorithmHistory(1)
-        self.assertEquals(parent_alg.name(), 'CreateTransmissionWorkspaceAuto')
-        self.assertEquals(parent_alg.childHistorySize(), 0)
-
+        self.assertEquals(history.size(), 1)
+        self.assertEquals(len(alg_hists), 1)
 
 if __name__ == '__main__':
     unittest.main()
