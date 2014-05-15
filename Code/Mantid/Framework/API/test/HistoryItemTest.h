3e0c8c8741ab811ccb5851387ae9e5ee3d13a776
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-05-15 17:00:45 +0100

    Refs #9369 Add unit tests for HistoryItem and HistoryView

---
+++ Code/Mantid/Framework/API/test/HistoryItemTest.h
@@ -0,0 +1,30 @@
+#ifndef HISTORYITEMTEST_H_
+#define HISTORYITEMTEST_H_
+
+#include <cxxtest/TestSuite.h>
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/HistoryView.h"
+
+using namespace Mantid::API;
+using namespace Mantid::Kernel;
+
+class HistoryItemTest : public CxxTest::TestSuite
+{
+  
+public:
+
+  void test_Minimum()
+  {
+    //not really much to test
+    AlgorithmHistory algHist("AnAlg", 1);
+    HistoryItem item(boost::make_shared<AlgorithmHistory>(algHist));
+    item.unrolled(true);
+
+    TS_ASSERT_EQUALS( *(item.getAlgorithmHistory()), algHist )
+    TS_ASSERT( item.isUnrolled() )
+    TS_ASSERT_EQUALS( item.numberOfChildren(), 0)
+  }
+
+};
+
+#endif
