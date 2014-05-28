85598b641222abd3721b7a7f300df623086c7fef
Wenduo Zhou <zhouw@ornl.gov>
2014-05-27 22:04:44 -0400

    Removed workspaces generated in unit tests. Refs #9358.

--- Code/Mantid/Framework/Algorithms/src/GeneratePeaks.cpp
+++ Code/Mantid/Framework/Algorithms/src/GeneratePeaks.cpp
@@ -322,7 +322,8 @@ namespace Algorithms
       double chi2 = m_funcParamWS->cell<double>(ipeak, icolchi2);
       if (chi2 > m_maxChi2)
       {
-        g_log.notice() << "Skip Peak " << ipeak << " (chi^2 " << chi2 << " > " << m_maxChi2 << "\n";
+        g_log.notice() << "Skip Peak " << ipeak << " (chi^2 " << chi2 << " > " << m_maxChi2
+                          << ".) " << "\n";
         continue;
       }
       else if (chi2 < 0.)
@@ -424,8 +425,8 @@ namespace Algorithms
     if (m_vecPeakParamValues.size() != numpeakparams)
     {
       std::stringstream errss;
-      errss << "Number of input peak parameters' value (" << numpeakparams << ") is not correct (should be "
-            << m_vecPeakParamValues.size() << " for peak of type " << m_peakFunction->name() << "). ";
+      errss << "Number of input peak parameters' value (" << m_vecPeakParamValues.size() << ") is not correct (should be "
+            << numpeakparams << " for peak of type " << m_peakFunction->name() << "). ";
       throw std::runtime_error(errss.str());
     }
     else
