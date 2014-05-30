159341a524b627a1683c2d98909adef0c7e86e60
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-05-30 12:44:13 +0100

    refs #9440. Remove deprecated alg method usage.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/Stitch1DMany.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/Stitch1DMany.py
@@ -36,8 +36,6 @@ class Stitch1DMany(DataProcessorAlgorithm):
         self.declareProperty(name="InputWorkspaces", defaultValue="", direction=Direction.Input, validator=input_validator, doc="Input workspaces")
         self.declareProperty(WorkspaceProperty("OutputWorkspace", "", Direction.Output), "Output stitched workspace")
         
-        self.setWikiSummary("Stitches single histogram matrix workspaces together")
-        self.setOptionalMessage("Stitches single histogram matrix workspaces together")
         self.declareProperty(FloatArrayProperty(name="StartOverlaps", values=[]), doc="Start overlaps for stitched workspaces.")
         self.declareProperty(FloatArrayProperty(name="EndOverlaps", values=[]), doc="End overlap for stitched workspaces.")
         self.declareProperty(FloatArrayProperty(name="Params", validator=FloatArrayMandatoryValidator()), doc="Rebinning Parameters. See Rebin for format.")
