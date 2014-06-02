fd94b6d9d18f3024be892c300f93889e21e805f6
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-06-02 09:16:30 +0100

    Refs #9369 Fix for OSX/RHEL6 extra qualification error.

--- Code/Mantid/Framework/Kernel/inc/MantidKernel/PropertyHistory.h
+++ Code/Mantid/Framework/Kernel/inc/MantidKernel/PropertyHistory.h
@@ -6,6 +6,7 @@
 //----------------------------------------------------------------------
 #include "MantidKernel/DllConfig.h"
 #include <string>
+#include <vector>
 #include <boost/shared_ptr.hpp>
 
 namespace Mantid
