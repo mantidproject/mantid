53d50fb66103e2ca1fe801c700d64d88d4146c55
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2014-06-03 12:46:30 +0100

    Rename aliases directive to alias
    Refs #9562

--- Code/Mantid/docs/sphinxext/mantiddoc/directives/alias.py
+++ Code/Mantid/docs/sphinxext/mantiddoc/directives/alias.py
@@ -1,24 +1,24 @@
 from base import BaseDirective
 
 
-class AliasesDirective(BaseDirective):
+class AliasDirective(BaseDirective):
 
     """
-    Obtains the aliases for a given algorithm based on it's name.
+    Obtains the alias for a given algorithm based on it's name.
     """
 
     required_arguments, optional_arguments = 0, 0
 
     def run(self):
         """
-        Called by Sphinx when the ..aliases:: directive is encountered.
+        Called by Sphinx when the ..alias:: directive is encountered.
         """
         alg = self.create_mantid_algorithm(self.algorithm_name(), self.algorithm_version())
         alias = alg.alias()
         if len(alias) == 0:
             return []
 
-        self.add_rst(self.make_header("Aliases"))
+        self.add_rst(self.make_header("Alias"))
         format_str = "This algorithm is also known as: **%s**"
         self.add_rst(format_str % alias)
         self.commit_rst()
@@ -32,4 +32,4 @@ def setup(app):
     Args:
       app: The main Sphinx application object
     """
-    app.add_directive('aliases', AliasesDirective)
+    app.add_directive('alias', AliasDirective)
