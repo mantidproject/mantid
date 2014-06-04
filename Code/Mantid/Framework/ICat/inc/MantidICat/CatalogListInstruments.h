696ae43ba181821a5b7621a5a0311fa7cc931d18
Jay Rainey <jawrainey@gmail.com>
2014-06-04 16:24:37 +0100

    Added usage to CatalogListInstruments & Investigations. Refs #9585.
    - Updated the summary for these algorithms to state difference between single and multiple catalogs.

--- Code/Mantid/Framework/ICat/inc/MantidICat/CatalogListInstruments.h
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CatalogListInstruments.h
@@ -42,16 +42,17 @@ namespace Mantid
       ~CatalogListInstruments(){}
       /// Algorithm's name for identification overriding a virtual method
       virtual const std::string name() const { return "CatalogListInstruments"; }
-    ///Summary of algorithms purpose
-    virtual const std::string summary() const {return "Lists the name of instruments from Information catalog.";}
-
+      /// Summary of algorithms purpose.
+      virtual const std::string summary() const
+      {
+        return "Lists the name of instruments from all catalogs or a specific catalog based on session information.";
+      }
       /// Algorithm's version for identification overriding a virtual method
       virtual int version() const { return 1; }
       /// Algorithm's category for identification overriding a virtual method
       virtual const std::string category() const { return "DataHandling\\Catalog"; }
 
     private:
-      
       /// Overwrites Algorithm init method.
       void init();
       /// Overwrites Algorithm exec method
--- Code/Mantid/Framework/ICat/inc/MantidICat/CatalogListInvestigationTypes.h
+++ Code/Mantid/Framework/ICat/inc/MantidICat/CatalogListInvestigationTypes.h
@@ -43,16 +43,14 @@ namespace Mantid
       ~CatalogListInvestigationTypes(){}
       /// Algorithm's name for identification overriding a virtual method
       virtual const std::string name() const { return "CatalogListInvestigationTypes"; }
-    ///Summary of algorithms purpose
-    virtual const std::string summary() const {return "Lists the name of investigation types from the Information catalog.";}
-
+      /// Summary of algorithms purpose.
+      virtual const std::string summary() const { return "Obtains a list of investigation types for active catalogs."; }
       /// Algorithm's version for identification overriding a virtual method
       virtual int version() const { return 1; }
       /// Algorithm's category for identification overriding a virtual method
       virtual const std::string category() const { return "DataHandling\\Catalog"; }
 
     private:
-      
       /// Overwrites Algorithm init method.
       void init();
       /// Overwrites Algorithm exec method
