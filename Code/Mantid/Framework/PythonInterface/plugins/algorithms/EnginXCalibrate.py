beea211cde96063cb7cd3bfaa2be45dc6d2204ab
Arturs Bekasovs <arturs.bekasovs@stfc.ac.uk>
2014-06-12 13:19:47 +0100

    Refs #8760. Initial versions of algorithms for EnginX calibration

---
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/EnginXCalibrate.py
@@ -0,0 +1,60 @@
+from mantid.kernel import *
+from mantid.api import *
+
+import csv
+import numpy as np
+import math
+
+class EnginXCalibrate(PythonAlgorithm):
+	def category(self):
+		return "Diffraction\Engineering;PythonAlgorithms"
+
+	def name(self):
+		return "EnginXCalibrateDetectors"
+		
+	def PyInit(self):
+		self.declareProperty(FileProperty("CalibrationRun", "", FileAction.Load),
+			"Calibration run to use")
+
+		self.declareProperty(FloatArrayProperty("ExpectedPeaks", ""),
+			"A list of dSpacing values where peaks are expected.")
+			
+		self.declareProperty("Bank", 1, "Which bank to calibrate")
+		
+		self.declareProperty(ITableWorkspaceProperty("DetectorPositions", "", Direction.Input, PropertyMode.Optional),
+			"Calibrated detector positions. If not specified, default ones are used.")
+		
+		self.declareProperty("Difc", 0.0, direction = Direction.Output,
+			doc = "Calibrated Difc value for the bank")
+
+		self.declareProperty("Zero", 0.0, direction = Direction.Output,
+			doc = "Calibrated Zero value for the bank")
+		
+	def PyExec(self):
+		
+		ws = self._focusRun()
+		
+		fitPeaksAlg = self.createChildAlgorithm('EnginXFitPeaks')
+		fitPeaksAlg.setProperty('InputWorkspace', ws)
+		fitPeaksAlg.setProperty('WorkspaceIndex', 0) # There should be only one index anyway
+		fitPeaksAlg.setProperty('ExpectedPeaks', self.getProperty('ExpectedPeaks').value)
+		fitPeaksAlg.execute()
+		
+		self.setProperty('Difc', fitPeaksAlg.getProperty('Difc').value)
+		self.setProperty('Zero', fitPeaksAlg.getProperty('Zero').value)
+		
+	def _focusRun(self):
+		alg = self.createChildAlgorithm('EnginXFocus')
+		alg.setProperty('Run', self.getProperty('CalibrationRun').value)
+		alg.setProperty('Bank', self.getProperty('Bank').value)
+		
+		detPos = self.getProperty('DetectorPositions').value
+		if detPos:
+			alg.setProperty('DetectorPositions', detPos)
+			
+		alg.execute()
+		
+		return alg.getProperty('OutputWorkspace').value
+		
+			
+AlgorithmFactory.subscribe(EnginXCalibrate)
