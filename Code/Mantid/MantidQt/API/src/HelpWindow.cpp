2dcef450c9980824fdfdc5b9d1dfb5e750b712ce
Pete Peterson <petersonpf@ornl.gov>
2014-06-17 13:14:19 -0400

    Re #6972. Adding implementation in HelpWindow.

--- Code/Mantid/MantidQt/API/src/HelpWindow.cpp
+++ Code/Mantid/MantidQt/API/src/HelpWindow.cpp
@@ -1,4 +1,6 @@
 #include "MantidQtAPI/HelpWindow.h"
+#include "MantidQtAPI/MantidHelpInterface.h"
+#include "MantidQtAPI/InterfaceManager.h"
 #include "MantidKernel/ConfigService.h"
 #include "MantidKernel/Exception.h"
 #include "MantidKernel/Logger.h"
@@ -20,25 +22,103 @@ namespace API
   using std::string;
 
   void HelpWindow::showPage(const std::string & url)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showPage(url);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for page " << url << "\n";
+    }
+  }
 
   void HelpWindow::showPage(const QString & url)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showPage(url);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for page " << url.toStdString() << "\n";
+    }
+  }
 
   void HelpWindow::showPage(const QUrl & url)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showPage(url);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for page " << url.toString().toStdString() << "\n";
+    }
+  }
 
   void HelpWindow::showWikiPage(const std::string &page)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showWikiPage(page);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for wiki page " << page << "\n";
+    }
+  }
 
   void HelpWindow::showAlgorithm(const std::string &name, const int version)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showAlgorithm(name, version);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for algorithm " << name << " v" << version << "\n";
+    }
+  }
 
   void HelpWindow::showAlgorithm(const QString &name, const int version)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showAlgorithm(name, version);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for algorithm " << name.toStdString()
+                    << " v" << version << "\n";
+    }
+  }
 
   void HelpWindow::showFitFunction(const std::string &name)
-  {}
+  {
+    InterfaceManager interfaceManager;
+    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+    if (gui)
+    {
+      gui->showFitFunction(name);
+    }
+    else
+    {
+      g_log.error() << "Failed to launch help for fit function " << name << "\n";
+    }
+  }
 
 } // namespace API
 } // namespace MantidQt
