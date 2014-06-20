a7a428625890b65dc1f93cd973bc5c97c8ba35d4
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-06-20 09:04:16 +0100

    Refs #9209 Add post processing step to diffraction.

--- Code/Mantid/scripts/Inelastic/IndirectDiffractionReduction.py
+++ Code/Mantid/scripts/Inelastic/IndirectDiffractionReduction.py
@@ -1,10 +1,14 @@
 import mantid
 from msg_reducer import MSGReducer
 import inelastic_indirect_reduction_steps as steps
+from  mantid.simpleapi import *
 
 class MSGDiffractionReducer(MSGReducer):
     """Reducer for Diffraction on IRIS and TOSCA.
     """
+
+    _container_workspace = None
+    _container_scale_factor = None
 
     def __init__(self):
         super(MSGDiffractionReducer, self).__init__()
@@ -44,6 +48,29 @@ class MSGDiffractionReducer(MSGReducer):
             step = steps.SaveItem()
             step.set_formats(self._save_formats)
             self.append_step(step)
+
+    def post_process(self):
+        #if we're using a can, normalise by beam current and subtact from the sample
+        if self._container_workspace is not None:
+            NormaliseByCurrent(self._container_workspace, OutputWorkspace=self._container_workspace)
+
+            #scale if required
+            if self._container_scale_factor is not None:
+                Scale(self._container_workspace, Factor=self._container_scale_factor, 
+                      OutputWorkspace=self._container_workspace)
+
+            workspaces = self.get_result_workspaces()
+            for ws in workspaces:
+                if ws != self._container_workspace:
+                    NormaliseByCurrent(ws, OutputWorkspace=ws)
+                    Minus(ws, self._container_workspace, OutputWorkspace=ws)
+
+    def set_container(self, container):
+        self._container_workspace = container
+
+    def set_container_scale_factor(self, factor):
+        self._container_scale_factor = factor
+
 
 def getStringProperty(workspace, property):
     """This function is used in the interface.
