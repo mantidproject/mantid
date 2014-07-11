7d61d7f843a9e7fd2013fed5e05a20179879c973
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2014-07-11 12:00:08 +0100

    Re #9468. Added SortTableWorkspace algorithm.

---
+++ Code/Mantid/Framework/DataHandling/src/SortTableWorkspace.cpp
@@ -0,0 +1,71 @@
+#include "MantidDataHandling/SortTableWorkspace.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidKernel/ArrayProperty.h"
+
+namespace Mantid
+{
+namespace DataHandling
+{
+
+  using Mantid::Kernel::Direction;
+  using Mantid::API::WorkspaceProperty;
+
+  // Register the algorithm into the AlgorithmFactory
+  DECLARE_ALGORITHM(SortTableWorkspace)
+  
+
+
+  //----------------------------------------------------------------------------------------------
+  /** Constructor
+   */
+  SortTableWorkspace::SortTableWorkspace()
+  {
+  }
+    
+  //----------------------------------------------------------------------------------------------
+  /** Destructor
+   */
+  SortTableWorkspace::~SortTableWorkspace()
+  {
+  }
+  
+
+  //----------------------------------------------------------------------------------------------
+
+  
+  /// Algorithm's version for identification. @see Algorithm::version
+  int SortTableWorkspace::version() const { return 1;};
+  
+  /// Algorithm's category for identification. @see Algorithm::category
+  const std::string SortTableWorkspace::category() const { return "General";}
+
+  /// Algorithm's summary for use in the GUI and help. @see Algorithm::summary
+  const std::string SortTableWorkspace::summary() const { return "Sort a TableWorkspace.";};
+
+  //----------------------------------------------------------------------------------------------
+  /** Initialize the algorithm's properties.
+   */
+  void SortTableWorkspace::init()
+  {
+    declareProperty(new WorkspaceProperty<API::ITableWorkspace>("InputWorkspace","",Direction::Input), "An input workspace.");
+    declareProperty(new WorkspaceProperty<API::ITableWorkspace>("OutputWorkspace","",Direction::Output), "An output workspace.");
+    declareProperty(new Kernel::ArrayProperty<std::string>("Columns"), "Column names to sort by.");
+    declareProperty(new Kernel::ArrayProperty<bool>("Ascending"), "List of bools for each column: true for ascending order, false for descending. "
+      "If contains a single value it applies to all columns.");
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Execute the algorithm.
+   */
+  void SortTableWorkspace::exec()
+  {
+    API::ITableWorkspace_sptr ws = getProperty("InputWorkspace");
+    std::vector<std::string> columns = getProperty("Columns");
+    std::vector<bool> ascending = getProperty("Ascending");
+    std::vector< std::pair<std::string,bool> > sortingOptions;
+  }
+
+
+
+} // namespace DataHandling
+} // namespace Mantid
---
+++ Code/Mantid/Framework/DataHandling/test/SortTableWorkspaceTest.h
@@ -0,0 +1,81 @@
+#ifndef MANTID_DATAHANDLING_SORTTABLEWORKSPACETEST_H_
+#define MANTID_DATAHANDLING_SORTTABLEWORKSPACETEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidDataHandling/SortTableWorkspace.h"
+#include "MantidAPI/WorkspaceFactory.h"
+#include "MantidAPI/TableRow.h"
+
+using Mantid::DataHandling::SortTableWorkspace;
+using namespace Mantid::API;
+
+class SortTableWorkspaceTest : public CxxTest::TestSuite
+{
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static SortTableWorkspaceTest *createSuite() { return new SortTableWorkspaceTest(); }
+  static void destroySuite( SortTableWorkspaceTest *suite ) { delete suite; }
+
+
+  void test_Init()
+  {
+    SortTableWorkspace alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+  }
+  
+  void test_exec()
+  {
+
+    auto ws = WorkspaceFactory::Instance().createTable();
+    ws->addColumn("int","Number1");
+    ws->addColumn("int","Number2");
+    ws->addColumn("int","Number3");
+
+    TableRow row = ws->appendRow();
+    row << 1 << 11 << 111;
+    row = ws->appendRow();
+    row << 2 << 22 << 222;
+    row = ws->appendRow();
+    row << 3 << 33 << 333;
+    row = ws->appendRow();
+    row << 4 << 44 << 444;
+    row = ws->appendRow();
+    row << 5 << 55 << 555;
+    row = ws->appendRow();
+    row << 6 << 66 << 666;
+    row = ws->appendRow();
+    row << 7 << 77 << 777;
+    row = ws->appendRow();
+    row << 8 << 88 << 888;
+
+
+    // Name of the output workspace.
+    std::string outWSName("SortTableWorkspaceTest_OutputWS");
+  
+    SortTableWorkspace alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", ws) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+    
+    // Retrieve the workspace from data service. TODO: Change to your desired type
+    Workspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
+    TS_ASSERT(ws);
+    if (!ws) return;
+    
+    // TODO: Check the results
+    
+    // Remove workspace from the data service.
+    AnalysisDataService::Instance().remove(outWSName);
+  }
+  
+};
+
+
+#endif /* MANTID_DATAHANDLING_SORTTABLEWORKSPACETEST_H_ */
