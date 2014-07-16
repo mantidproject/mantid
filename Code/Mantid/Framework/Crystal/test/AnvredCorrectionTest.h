5caebaab135bfea56dcfa60aa78db965889e7caf
Vickie Lynch <lynchve@ornl.gov>
2014-07-16 11:41:23 -0400

    Refs #9894 use helper for event workspace

--- Code/Mantid/Framework/Crystal/test/AnvredCorrectionTest.h
+++ Code/Mantid/Framework/Crystal/test/AnvredCorrectionTest.h
@@ -39,35 +39,8 @@ public:
    */
   EventWorkspace_sptr createDiffractionEventWorkspace(int numEvents)
   {
-    FacilityHelper::ScopedFacilities loadTESTFacility("IDFs_for_UNIT_TESTING/UnitTestFacilities.xml", "TEST");
-
-    int numPixels = 100;
-    int numBins = 4;
-    double binDelta = 0.10;
-    boost::mt19937 rng;
-    boost::uniform_real<double> u2(0, 1.0); // Random from 0 to 1.0
-    boost::variate_generator<boost::mt19937&, boost::uniform_real<double> > genUnit(rng, u2);
-    int randomSeed = 1;
-    rng.seed((unsigned int)(randomSeed));
-    size_t nd = 1;
-    // Make a random generator for each dimensions
-    typedef boost::variate_generator<boost::mt19937&, boost::uniform_real<double> >   gen_t;
-    gen_t * gens[1];
-    for (size_t d=0; d<nd; ++d)
-    {
-      double min = -1.;
-      double max = 1.;
-      if (max <= min) throw std::invalid_argument("UniformParams: min must be < max for all dimensions.");
-      boost::uniform_real<double> u(min,max); // Range
-      gen_t * gen = new gen_t(rng, u);
-      gens[d] = gen;
-    }
-
-
-
-    EventWorkspace_sptr retVal(new EventWorkspace);
-    retVal->initialize(numPixels,1,1);
-
+	// setup the test workspace
+	EventWorkspace_sptr retVal = WorkspaceCreationHelper::CreateEventWorkspace2(1, 100);
     // --------- Load the instrument -----------
     LoadInstrument * loadInst = new LoadInstrument();
     loadInst->initialize();
@@ -77,53 +50,6 @@ public:
     delete loadInst;
     // Populate the instrument parameters in this workspace - this works around a bug
     retVal->populateInstrumentParameters();
-
-    DateAndTime run_start("2010-01-01T00:00:00");
-
-    for (int pix = 0; pix < numPixels; pix++)
-    {
-      EventList & el = retVal->getEventList(pix);
-      el.setSpectrumNo(pix);
-      el.addDetectorID(pix);
-      //Background
-      for (int i=0; i<numBins; i++)
-      {
-        //Two events per bin
-        el += TofEvent((i+0.5)*binDelta, run_start+double(i));
-        el += TofEvent((i+0.5)*binDelta, run_start+double(i));
-      }
-
-      //Peak
-      int r = static_cast<int>(numEvents/std::sqrt((pix/100-50.5)*(pix/100-50.5) + (pix%100-50.5)*(pix%100-50.5)));
-      for (int i=0; i<r; i++)
-      {
-        el += TofEvent(0.75+binDelta*(((*gens[0])()+(*gens[0])()+(*gens[0])())*2.-3.), run_start+double(i));
-      }
-
-    }
-
-    /// Clean up the generators
-    for (size_t d=0; d<nd; ++d)
-      delete gens[d];
-
-
-    //Create the x-axis for histogramming.
-    MantidVecPtr x1;
-    MantidVec& xRef = x1.access();
-    xRef.resize(numBins);
-    for (int i = 0; i < numBins; ++i)
-    {
-      xRef[i] = i*binDelta;
-    }
-
-    //Set all the histograms at once.
-    retVal->setAllX(x1);
-
-    // Some sanity checks
-    TS_ASSERT_EQUALS( retVal->getInstrument()->getName(), "MINITOPAZ");
-    std::map<int, Geometry::IDetector_const_sptr> dets;
-    retVal->getInstrument()->getDetectors(dets);
-    TS_ASSERT_EQUALS( dets.size(), 100*100);
 
     return retVal;
   }
@@ -151,9 +77,9 @@ public:
     alg.setProperty("OutputWorkspace", "TOPAZ");
     alg.setProperty("PreserveEvents", ev);
     alg.setProperty("OnlySphericalAbsorption", false);
-    alg.setProperty("LinearScatteringCoef", 1.302);
-    alg.setProperty("LinearAbsorptionCoef", 1.686);
-    alg.setProperty("Radius", 0.17);
+    alg.setProperty("LinearScatteringCoef", 0.369);
+    alg.setProperty("LinearAbsorptionCoef", 0.011);
+    alg.setProperty("Radius", 0.05);
     alg.setProperty("PowerLambda", 3.0);
     TS_ASSERT_THROWS_NOTHING( alg.execute(); )
     TS_ASSERT( alg.isExecuted() )
@@ -163,7 +89,12 @@ public:
         ws = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>("TOPAZ") );
     TS_ASSERT(ws);
     if (!ws) return;
-    TS_ASSERT_DELTA( ws->readY(50)[1], 229.0, 0.5);
+    // do the final comparison - this is done by bounding
+    const MantidVec & y_actual = ws->readY(0);
+    TS_ASSERT_DELTA(y_actual[0], 4.8477, 0.4);
+    TS_ASSERT_DELTA(y_actual[1], 0.1796, 0.02);
+    TS_ASSERT_DELTA(y_actual[2], 0.0388, 0.004);
+
     AnalysisDataService::Instance().remove("TOPAZ");
   }
 
