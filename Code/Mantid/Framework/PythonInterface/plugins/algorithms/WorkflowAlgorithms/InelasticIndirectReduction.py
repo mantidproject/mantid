6a1e8397a19826022980b34d0d2e8da32159e165
Dan Nixon <dan@dan-nixon.com>
2014-08-08 10:47:04 +0100

    More refactoring, added sample log saving and plotting
    Refs #10083

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/InelasticIndirectReduction.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/InelasticIndirectReduction.py
@@ -8,36 +8,40 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
         return "Workflow\\Inelastic;PythonAlgorithms;Inelastic"
 
     def summary(self):
-        return ""  ##TODO
+        return "Runs a reduction for an inelastic indirect geometry instrument."
 
     def PyInit(self):
         self.declareProperty(WorkspaceProperty('OutputWorkspace', '',
-            direction=Direction.Output))
+            direction=Direction.Output, optional=PropertyMode.Optional), doc='Optionally override the name for the output workspace')
 
-        self.declareProperty(FileProperty('InputFiles', '', action=FileAction.Load,
+        self.declareProperty(FileProperty('InputFiles', 'Comma separated list of input files', action=FileAction.Load,
             extensions=['nxs', 'raw', 'sav', 'add', 'nxspe', 'n*', 's*']))
 
-        self.declareProperty(name='SumFiles', defaultValue=False, doc='')
-        self.declareProperty(name='LoadLogs', defaultValue=False, doc='')
+        self.declareProperty(name='SumFiles', defaultValue=False, doc='Toggle input file summing or sequential processing')
+        self.declareProperty(name='LoadLogs', defaultValue=False, doc='Load sample logs from input files')
 
-        self.declareProperty(name='Instrument', defaultValue='', doc='',
+        ##TODO: Add other indirect instruments and configurations (only covers ISIS)
+        self.declareProperty(name='Instrument', defaultValue='', doc='Instrument used during run',
                 validator=StringListValidator(['IRIS', 'OSIRIS', 'TOSCA']))
-        self.declareProperty(name='Analyser', defaultValue='', doc='',
+        self.declareProperty(name='Analyser', defaultValue='', doc='Analyser used during run',
                 validator=StringListValidator(['graphite', 'mica', 'fmica', 'diffraction']))
-        self.declareProperty(name='Reflection', defaultValue='', doc='',
+        self.declareProperty(name='Reflection', defaultValue='', doc='Reflection used during run',
                 validator=StringListValidator(['002', '004', '006', 'diffspec', 'diffonly']))
 
         self.declareProperty(WorkspaceProperty('CalibrationWorkspace', '',
-            direction=Direction.Input, optional=PropertyMode.Optional))
+            direction=Direction.Input, optional=PropertyMode.Optional), doc='Workspace contining calibration data')
 
-        self.declareProperty(name='DetectorRange', defaultValue='0,1', doc='')
+        self.declareProperty(name='DetectorRange', defaultValue='0,1', doc='Comma separated range of detectors to use')
         self.declareProperty(name='BackgroundRange', defaultValue='', doc='')
-        self.declareProperty(name='RebinString', defaultValue='', doc='')
+        self.declareProperty(name='RebinString', defaultValue='', doc='Rebin string parameters')
         self.declareProperty(name='DetailedBalance', defaultValue=-1.0, doc='')
         self.declareProperty(name='ScaleFactor', defaultValue=1.0, doc='')
         self.declareProperty(name='MappingFile', defaultValue='', doc='')
         self.declareProperty(name='Fold', defaultValue=False, doc='')
         self.declareProperty(name='SaveCM1', defaultValue=False, doc='')
+
+        self.declareProperty(name='Plot', defaultValue='none', doc='Type of plot to output after reduction',
+                validator=StringListValidator(['none', 'spectra', 'contour']))
 
     def PyExec(self):
         from mantid import config, logger
@@ -65,7 +69,7 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
         calib_ws = self.getProperty('CalibrationWorkspace')
 
         detailed_balance = self.getProperty('DetailedBalance').value
-    
+
         rebin_string = self.getPropertyValue('RebinString')
 
         scale_factor = self.getPropertyValue('ScaleFactor')
@@ -75,7 +79,7 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
         ## Setup reducer
         reducer = irr.IndirectReducer()
 
-        reducer.set_rename(False)
+        reducer.set_rename(True)
 
         reducer.set_instrument_name(instrument)
         reducer.set_parameter_file(param_file)
@@ -87,7 +91,8 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
 
         reducer.set_detector_range(int(detector_range[0])-1, int(detector_range[1])-1)
 
-        if calib_ws.value != None:
+        use_calib_ws = calib_ws.value != None
+        if use_calib_ws:
             logger.debug('Using calibration workspace')
             reducer.set_calibration_workspace(calib_ws.valueAsStr)
 
@@ -95,7 +100,8 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
             logger.debug('Using background range: ' + str(background_range))
             reducer.set_background(float(background_range[0]), float(background_range[1]))
 
-        if detailed_balance != -1.0:
+        use_detailed_balance = detailed_balance != -1.0
+        if use_detailed_balance:
             logger.debug('Using detailed balance: ' + str(detailed_balance))
             reducer.set_detailed_balance(detailed_balance)
 
@@ -103,7 +109,8 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
             logger.debug('Using rebin string: ' + rebin_string)
             reducer.set_rebin_string(rebin_string)
 
-        if scale_factor != 1.0:
+        use_scale_factor = scale_factor != 1.0
+        if use_scale_factor:
             logger.debug('Using scale factor: ' + str(scale_factor))
             reducer.set_scale_factor(scale_factor)
 
@@ -118,11 +125,48 @@ class InelasticIndirectReduction(DataProcessorAlgorithm):
         reducer.reduce()
         ws_list = reducer.get_result_workspaces()
 
-        RenameWorkspace(InputWorkspace=ws_list[0], OutputWorkspace=out_ws)
+        if len(ws_list) < 1:
+            logger.error('Failed to complete reduction')
+            return
+
+        ## Add sample logs to output workspace(s)
+        for workspace in ws_list:
+            AddSampleLog(Workspace=workspace, LogName='use_calib_wokspace', LogType='String', LogText=str(use_calib_ws))
+            if use_calib_ws:
+                AddSampleLog(Workspace=workspace, LogName='calib_workspace_name', LogType='String', LogText=str(calib_ws))
+
+            AddSampleLog(Workspace=workspace, LogName='use_detailed_balance', LogType='String', LogText=str(use_detailed_balance))
+            if use_detailed_balance:
+                AddSampleLog(Workspace=workspace, LogName='detailed_balance', LogType='Number', LogText=str(detailed_balance))
+
+            AddSampleLog(Workspace=workspace, LogName='use_scale_factor', LogType='String', LogText=str(use_scale_factor))
+            if use_detailed_balance:
+                AddSampleLog(Workspace=workspace, LogName='scale_factor', LogType='Number', LogText=str(scale_factor))
+
+        ## Rename output workspace
+        ## Only renames first workspace, but used in this way the reucer should only output one
+        use_provided_out_ws = self.getPropertyValue('OutputWorkspace') != ''
+        if use_provided_out_ws:
+            logger.information('Renaming output workspace ' + str(ws_list[0]) + ' to ' + str(out_ws))
+            RenameWorkspace(InputWorkspace=ws_list[0], OutputWorkspace=out_ws)
+        else:
+            out_ws = ws_list[0]
 
         self.setProperty('OutputWorkspace', out_ws)
 
-        ##TODO: Add sample logs
+        ## Do plotting
+        plot_type = self.getPropertyValue('Plot')
+
+        if plot_type != 'none':
+            if plot_type == 'spectra':
+                from mantidplot import plotSpectrum
+                num_spectra = mtd[out_ws].getNumberHistograms()
+                plotSpectrum(out_ws, range(0, num_spectra))
+
+            if plot_type == 'contour':
+                from mantidplot import importMatrixWorkspace
+                plot_workspace = importMatrixWorkspace(ws_list[0])
+                plot_workspace.plotGraph2D()
 
         EndTime('InelasticIndirectReduction')
 
