9778c8e835226c4f929d564576b8d5bf5ab592dd
Dan Nixon <dan@dan-nixon.com>
2014-08-13 09:15:30 +0100

    Improved documentation and unit test
    Refs #10057

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/IndirectTransmissionMonitorTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/IndirectTransmissionMonitorTest.py
@@ -4,18 +4,25 @@ from mantid.simpleapi import *
 
 class IndirectTransmissionMonitorTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        ##TODO: Create sample workspaces
+        sample_workspace = Load('IRS26176.RAW')
+        can_workspace = Load('IRS26173.RAW')
+
+        cls._sample_workspace = sample_workspace
+        cls._can_workspace = can_workspace
+
     def setUp(self):
         self.kwargs = {}
-        self.kwargs['SampleFile'] = 'IRS26176.RAW'
-        self.kwargs['CanFile'] = 'IRS26173.RAW'
+        self.kwargs['SampleWorkspace'] = self._sample_workspace
+        self.kwargs['CanWorkspace'] = self._can_workspace
         self.kwargs['Verbose'] = True
         self.kwargs['Plot'] = False
 
-        self.run_number, _ = os.path.splitext(self.kwargs['SampleFile'].split('_')[0])
-
     def tearDown(self):
         # Clean up saved nexus files
-        path = os.path.join(config['defaultsave.directory'], self.run_number + '_Transmission.nxs')
+        path = os.path.join(config['defaultsave.directory'], 'IndirectTransmissionMonitorTest.nxs')
         if os.path.isfile(path):
             try:
                 os.remove(path)
@@ -23,30 +30,19 @@ class IndirectTransmissionMonitorTest(unittest.TestCase):
                 pass
 
     def test_basic(self):
-        IndirectTransmissionMonitor(**self.kwargs)
-
-        trans_workspace = mtd[self.run_number + '_Transmission']
+        trans_workspace = IndirectTransmissionMonitor(**self.kwargs)
 
         self.assertTrue(isinstance(trans_workspace, mantid.api.WorkspaceGroup), msg='Result should be a workspace group')
         self.assertEqual(trans_workspace.size(), 3, msg='Transmission workspace group should have 3 workspaces: sample, can and transfer')
 
     def test_nexus_save(self):
         self.kwargs['Save'] = True
+        self.kwargs['OutputWorkspace'] = 'IndirectTransmissionMonitorTest'
 
         IndirectTransmissionMonitor(**self.kwargs)
 
-        path = os.path.join(config['defaultsave.directory'], self.run_number + '_Transmission.nxs')
+        path = os.path.join(config['defaultsave.directory'], 'IndirectTransmissionMonitorTest.nxs')
         self.assertTrue(os.path.isfile(path), msg='Transmission workspace should be saved to default save directory')
-
-    def test_empty_filenames(self):
-        self.kwargs['CanFile'] = ''
-
-        self.assertRaises(ValueError, IndirectTransmissionMonitor, **self.kwargs)
-
-    def test_nonexistent_file(self):
-        self.kwargs['CanFile'] = 'NoFile.raw'
-
-        self.assertRaises(ValueError, IndirectTransmissionMonitor, **self.kwargs)
 
 if __name__ == '__main__':
     unittest.main()
