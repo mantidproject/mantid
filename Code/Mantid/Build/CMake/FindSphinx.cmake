890969aac08229e8dbd1367d5c4dcccfa911fd3a
Pete Peterson <petersonpf@ornl.gov>
2014-08-18 16:43:43 -0400

    Re #10133. Making sphinx 1.2 the only version that matters.

--- Code/Mantid/Build/CMake/FindSphinx.cmake
+++ Code/Mantid/Build/CMake/FindSphinx.cmake
@@ -17,6 +17,24 @@ find_program( SPHINX_EXECUTABLE NAME sphinx-build
   DOC "Sphinx documentation generator"
 )
 
+if (SPHINX_EXECUTABLE)
+    # run sphinx-build to attempt to get the version
+    execute_process (COMMAND ${SPHINX_EXECUTABLE} --version
+                     OUTPUT_STRIP_TRAILING_WHITESPACE
+                     OUTPUT_VARIABLE version_string
+                     ERROR_VARIABLE version_error_string
+                     ERROR_STRIP_TRAILING_WHITESPACE)
+
+    # if it wasn't successful it is hiding in stderr
+    if (NOT version_string)
+        string (REGEX REPLACE "\n" ";" version_string ${version_error_string})
+        list (GET version_string 0 version_string)
+    endif (NOT version_string)
+
+    # chop out the version number
+    string (REGEX REPLACE "^Sphinx v?" "" SPHINX_VERSION ${version_string})
+endif (SPHINX_EXECUTABLE)
+
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE )
