0ead94d9aaf8feaa171d59ea083a775a86d321f0
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-09-08 14:46:08 +0100

    refs #10207. Remaining tests.

--- Code/Mantid/MantidPlot/test/squish_test_suites/refl_gui_tests/tst_basic_operation/test.py
+++ Code/Mantid/MantidPlot/test/squish_test_suites/refl_gui_tests/tst_basic_operation/test.py
@@ -91,6 +91,9 @@ def do_test_minimal(test_harness):
     test.verify('TOF' in out_workspaces, 'Should have a TOF group in the output')
     test.compare(len(out_workspaces), len(out_workspaces_from_run)+1, "All workspaces in output list should be accounted for")
     
+    btn = waitForObject(':tableMain.Plot_QPushButton')
+    test.verify(btn.isEnabled(), 'Should be able to plot the result')
+    
 def do_test_transmission(test_harness):
     '''Enter a single run with transmission correction applied.'''
     run_number = 13460
@@ -152,8 +155,84 @@ def do_test_reuse_transmission(test_harness):
     test.verify(str(run_number2) + '_IvsQ' in out_workspaces,'{0}_IvsQ should exist in output'.format(run_number2))
     test.verify(str(run_number2) +'_IvsLam' in out_workspaces,'{0}_IvsLam should exist in output'.format(run_number2))
     test.verify('TRANS_{0}_{1}'.format(transmission_run1, transmission_run2) in out_workspaces,'Transmission workspace should exist in output')
-   
+    
+def do_test_stitch_output(test_harness):
+    '''Check stitching'''
 
+    run_number1 = 13460
+    run_number2 = 13462
+    transmission_run1 = 13463
+    transmission_run2 = 13464
+    row_index = 0
+    
+    # Fetch the table
+    tbl = waitForObject(":splitterList.tableMain_QTableWidget")
+    
+    # Set the stitched state
+    ck_stitched = waitForObject(':tableMain_QCheckBox')
+    ck_stitched.setChecked(True)
+    
+    # Set the first run number
+    tbl.item(row_index, 0).setText(str(run_number1))
+    
+    # Set the second run number
+    tbl.item(row_index, 5).setText(str(run_number2))
+    # Set the transmission runs AS their workspace name
+    tbl.item(row_index, 2).setText('TRANS_{0}_{1}'.format(transmission_run1, transmission_run2))
+    # Set the transmission runs AS their run numbers. Should be recycled anyway as the transmission workspace already exists.
+    tbl.item(row_index, 7).setText('TRANS_{0}_{1}'.format(transmission_run1, transmission_run2))
+    
+    # Process everything
+    test_harness.process_everything()
+    
+    # Get a handle to the workspace list
+    out_workspaces = test_harness.list_from_workspace_list()
+    
+    # Check the output workspaces are in the workspace list
+    test.verify(str(run_number1) + '_IvsQ' in out_workspaces,'{0}_IvsQ should exist in output'.format(run_number1))
+    test.verify(str(run_number1) +'_IvsLam' in out_workspaces,'{0}_IvsLam should exist in output'.format(run_number1))
+    test.verify(str(run_number2) + '_IvsQ' in out_workspaces,'{0}_IvsQ should exist in output'.format(run_number2))
+    test.verify(str(run_number2) +'_IvsLam' in out_workspaces,'{0}_IvsLam should exist in output'.format(run_number2))
+    test.verify('TRANS_{0}_{1}'.format(transmission_run1, transmission_run2) in out_workspaces,'Transmission workspace should exist in output')    
+    # Test that the stitched workspace is generated.
+    test.verify('13460_62' in out_workspaces,'Stitched workspace should be generated')
+    
+def do_test_three_runs(test_harness):
+    '''Check stitching'''
+
+    run_number1 = 13460
+    run_number2 = 13462
+    run_number3 = 13463
+    
+    row_index = 0
+    
+    # Fetch the table
+    tbl = waitForObject(":splitterList.tableMain_QTableWidget")
+    
+    # Set the first run number
+    tbl.item(row_index, 0).setText(str(run_number1))
+    
+    # Set the second run number
+    tbl.item(row_index, 5).setText(str(run_number2))
+    
+    # Set the thrid run number
+    tbl.item(row_index, 10).setText(str(run_number3))
+    # Give the third run a theta value
+    tbl.item(row_index, 11).setText(str(1.0))
+    
+    # Process everything
+    test_harness.process_everything()
+    
+    # Get a handle to the workspace list
+    out_workspaces = test_harness.list_from_workspace_list()
+    
+    # Check the output workspaces are in the workspace list
+    test.verify(str(run_number1) + '_IvsQ' in out_workspaces,'{0}_IvsQ should exist in output'.format(run_number1))
+    test.verify(str(run_number1) +'_IvsLam' in out_workspaces,'{0}_IvsLam should exist in output'.format(run_number1))
+    test.verify(str(run_number2) + '_IvsQ' in out_workspaces,'{0}_IvsQ should exist in output'.format(run_number2))
+    test.verify(str(run_number2) +'_IvsLam' in out_workspaces,'{0}_IvsLam should exist in output'.format(run_number2))
+    test.verify(str(run_number3) + '_IvsQ' in out_workspaces,'{0}_IvsQ should exist in output'.format(run_number3))
+    test.verify(str(run_number3) +'_IvsLam' in out_workspaces,'{0}_IvsLam should exist in output'.format(run_number3))
 def main():
     # Create the test harness
     test_app = ReflTestHarness()
@@ -166,6 +245,8 @@ def main():
     test_app.run_test(do_test_minimal)
     test_app.run_test(do_test_transmission)
     test_app.run_test(do_test_reuse_transmission)
+    test_app.run_test(do_test_stitch_output)
+    test_app.run_test(do_test_three_runs)
     
     # Clean up
     test_app.tear_down_application()
