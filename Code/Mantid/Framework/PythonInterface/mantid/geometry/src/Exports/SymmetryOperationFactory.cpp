e7718f2ca61c2c638c055e1cf72e3c994b5bca47
Michael Wedel <michael.wedel@psi.ch>
2014-09-11 11:01:01 +0200

    Refs #10135. Exporting SymmetryOperation and -Factory to Python

---
+++ Code/Mantid/Framework/PythonInterface/mantid/geometry/src/Exports/SymmetryOperationFactory.cpp
@@ -0,0 +1,21 @@
+#include "MantidGeometry/Crystal/SymmetryOperationFactory.h"
+#include "MantidPythonInterface/kernel/PythonObjectInstantiator.h"
+
+#include <boost/python/class.hpp>
+
+using namespace Mantid::Geometry;
+using namespace boost::python;
+
+void export_SymmetryOperationFactory()
+{
+
+    class_<SymmetryOperationFactoryImpl,boost::noncopyable>("SymmetryOperationFactoryImpl", no_init)
+            .def("exists", &SymmetryOperationFactoryImpl::exists)
+            .def("createSymOp", &SymmetryOperationFactoryImpl::createSymOp)
+            .def("getKeys", &SymmetryOperationFactoryImpl::getKeys, "Returns all registered symmetry operations")
+            .def("Instance", &SymmetryOperationFactory::Instance, return_value_policy<reference_existing_object>(),
+                 "Returns a reference to the SymmetryOperationFactory singleton")
+            .staticmethod("Instance")
+            ;
+}
+
