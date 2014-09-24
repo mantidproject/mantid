4e69cb76d0509924447b540d3b013219caa22dc6
Ross Whitfield <whitfieldre@ornl.gov>
2014-09-24 10:14:00 -0400

    Refs #9984 Unit test added.

--- Code/Mantid/Framework/Algorithms/test/CorelliCrossCorrelateTest.h
+++ Code/Mantid/Framework/Algorithms/test/CorelliCrossCorrelateTest.h
@@ -4,9 +4,17 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidAlgorithms/CorelliCrossCorrelate.h"
+#include "MantidKernel/DateAndTime.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidKernel/TimeSeriesProperty.h"
+#include <iostream>
+#include <iomanip>
+
 
 using Mantid::Algorithms::CorelliCrossCorrelate;
 using namespace Mantid::API;
+using namespace Mantid::DataObjects;
+using namespace Mantid::Kernel;
 
 class CorelliCrossCorrelateTest : public CxxTest::TestSuite
 {
@@ -28,33 +36,68 @@ public:
   {
     // Name of the output workspace.
     std::string outWSName("CorelliCrossCorrelateTest_OutputWS");
-  
+
+    IAlgorithm_sptr lei = AlgorithmFactory::Instance().create("LoadEmptyInstrument", 1);
+    lei->initialize();
+    lei->setPropertyValue("Filename", "CORELLI_Definition.xml");
+    lei->setPropertyValue("OutputWorkspace", "CorelliCrossCorrelateTest_OutputWS");
+    lei->setPropertyValue("MakeEventWorkspace", "1");
+    lei->execute();
+
+    EventWorkspace_sptr ws;
+    ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>("CorelliCrossCorrelateTest_OutputWS");
+    ws->sortAll(TOF_SORT, NULL);
+
+    DateAndTime startTime("2007-11-30T16:17:00");
+    EventList *evlist = ws->getEventListPtr(0);
+
+    evlist->addEventQuickly( TofEvent(double(10.0),startTime+double(0.007)));
+    evlist->addEventQuickly( TofEvent(double(100.0),startTime+double(0.012)));
+    evlist->addEventQuickly( TofEvent(double(1000.0),startTime+double(0.012)));
+    evlist->addEventQuickly( TofEvent(double(10000.0),startTime+double(0.012)));
+    evlist->addEventQuickly( TofEvent(double(1222.0),startTime+double(0.03)));
+
+    ws->getAxis(0)->setUnit("TOF");
+
+    double freq = 293.383;
+    double period = 1/freq;
+    auto tdc = new TimeSeriesProperty<int>("chopper4_TDC");
+    for (int i=0; i<10; i++){
+      double tdcTime=i*period;
+      tdc->addValue(startTime+tdcTime,1);
+    }
+    ws->mutableRun().addLogData(tdc);
+
+    auto motorSpeed = new TimeSeriesProperty<double>("BL9:Chop:Skf4:MotorSpeed");
+    motorSpeed->addValue(startTime,freq);
+    ws->mutableRun().addLogData(motorSpeed);
+
     CorelliCrossCorrelate alg;
     TS_ASSERT_THROWS_NOTHING( alg.initialize() )
     TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("REPLACE_PROPERTY_NAME_HERE!!!!", "value") );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("InputWorkspace", "CorelliCrossCorrelateTest_OutputWS") );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", "CorelliCrossCorrelateTest_OutputWS") );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("TimingOffset", "20000") );
     TS_ASSERT_THROWS_NOTHING( alg.execute(); );
     TS_ASSERT( alg.isExecuted() );
     
     // Retrieve the workspace from data service. TODO: Change to your desired type
-    Workspace_sptr ws;
-    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<Workspace>(outWSName) );
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>("CorelliCrossCorrelateTest_OutputWS") );
     TS_ASSERT(ws);
     if (!ws) return;
     
-    // TODO: Check the results
-    
+    std::vector<WeightedEvent>& events = evlist->getWeightedEvents();
+
+    TS_ASSERT_DELTA(events[0].weight(),-1.99392,0.00001)
+    TS_ASSERT_DELTA(events[1].weight(),-1.99392,0.00001)
+    TS_ASSERT_DELTA(events[2].weight(),2.00612,0.00001)
+    TS_ASSERT_DELTA(events[3].weight(),2.00612,0.00001)
+    TS_ASSERT_DELTA(events[4].weight(),-1.99392,0.00001)
+
     // Remove workspace from the data service.
     AnalysisDataService::Instance().remove(outWSName);
   }
   
-  void test_Something()
-  {
-    TSM_ASSERT( "You forgot to write a test!", 0);
-  }
-
-
 };
 
 
