0bb4520f77114e8e4a4676a73ef189788a607510
Michael Wedel <michael.wedel@psi.ch>
2014-09-30 21:45:37 +0200

    Refs #10280. Changed python exports

--- Code/Mantid/Framework/PythonInterface/mantid/geometry/src/Exports/SymmetryOperationFactory.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/geometry/src/Exports/SymmetryOperationFactory.cpp
@@ -6,24 +6,11 @@
 using namespace Mantid::Geometry;
 using namespace boost::python;
 
-namespace {
-    using namespace Mantid::PythonInterface;
-
-    SymmetryOperation_sptr createSymOpPython(SymmetryOperationFactoryImpl & self, const std::string &identifier)
-    {
-        SymmetryOperation_const_sptr constSymOp = self.createSymOp(identifier);
-
-        return boost::const_pointer_cast<SymmetryOperation>(constSymOp);
-    }
-}
-
 void export_SymmetryOperationFactory()
 {
-
     class_<SymmetryOperationFactoryImpl,boost::noncopyable>("SymmetryOperationFactoryImpl", no_init)
-            .def("exists", &SymmetryOperationFactoryImpl::exists)
-            .def("createSymOp", &createSymOpPython)
-            .def("getKeys", &SymmetryOperationFactoryImpl::getKeys, "Returns all registered symmetry operations")
+            .def("exists", &SymmetryOperationFactoryImpl::isSubscribed)
+            .def("createSymOp", &SymmetryOperationFactoryImpl::createSymOp)
             .def("Instance", &SymmetryOperationFactory::Instance, return_value_policy<reference_existing_object>(),
                  "Returns a reference to the SymmetryOperationFactory singleton")
             .staticmethod("Instance")
