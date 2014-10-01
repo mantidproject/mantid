ad5c5e5d89bf36e2f3453cd51f4c32dd15ca49e0
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-10-01 12:38:38 +0100

    refs #10187. More init order warning fixes.

--- Code/Mantid/Framework/API/src/Algorithm.cpp
+++ Code/Mantid/Framework/API/src/Algorithm.cpp
@@ -83,8 +83,8 @@ namespace Mantid
       m_progressObserver(NULL),
       m_isInitialized(false), m_isExecuted(false),m_isChildAlgorithm(false), m_recordHistoryForChild(false),
       m_alwaysStoreInADS(false),m_runningAsync(false),
-      m_running(false),m_rethrow(false),m_algorithmID(this),
-      m_groupSize(0), m_singleGroup(-1), m_groupsHaveSimilarNames(false)
+      m_running(false),m_rethrow(false),
+      m_groupSize(0), m_algorithmID(this), m_singleGroup(-1), m_groupsHaveSimilarNames(false)
     {
     }
 
