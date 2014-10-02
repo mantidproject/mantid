0b757cd1ecc1b1ea703ceb943911efcfb8c4b2ef
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2014-10-02 10:26:01 +0100

    Refs #10235 Add unit test for Refl UI autofilling

--- Code/Mantid/MantidQt/CustomInterfaces/test/ReflLoadedMainViewPresenterTest.h
+++ Code/Mantid/MantidQt/CustomInterfaces/test/ReflLoadedMainViewPresenterTest.h
@@ -541,6 +541,66 @@ public:
     AnalysisDataService::Instance().remove("TRANS_13463_13464");
   }
 
+  /*
+   * Test autofilling workspace values.
+   */
+  void testAutofill()
+  {
+    auto ws = createWorkspace("TestWorkspace");
+    //Autofill two theta
+    TableRow row = ws->appendRow();
+    row << "13460" << "" << "13463,13464" << "0.01" << "0.06" << "0.04" << "1" << 1;
+
+    //Autofill dqq
+    row = ws->appendRow();
+    row << "13460" << "0.7" << "13463,13464" << "0.01" << "0.06" << "" << "1" << 2;
+
+    //Autofill two theta and dqq
+    row = ws->appendRow();
+    row << "13460" << "" << "13463,13464" << "0.01" << "0.06" << "" << "1" << 3;
+
+    MockView mockView;
+    ReflLoadedMainViewPresenter presenter(ws,&mockView);
+    std::vector<size_t> rowlist;
+    rowlist.push_back(0);
+    rowlist.push_back(1);
+    rowlist.push_back(2);
+
+    //We should not receive any errors
+    EXPECT_CALL(mockView,  giveUserCritical(_,_)).Times(0);
+
+    //The user hits the "process" button with the first three rows selected
+    EXPECT_CALL(mockView, getSelectedRowIndexes()).Times(1).WillRepeatedly(Return(rowlist));
+    EXPECT_CALL(mockView, getProcessInstrument()).WillRepeatedly(Return("INTER"));
+    EXPECT_CALL(mockView, setProgressRange(_,_));
+    EXPECT_CALL(mockView, setProgress(_)).Times(7);
+    presenter.notify(ProcessFlag);
+
+    //The user hits the "save" button
+    presenter.notify(SaveFlag);
+
+    //Check the calls were made as expected
+    TS_ASSERT(Mock::VerifyAndClearExpectations(&mockView));
+
+    //Check the table was updated as expected
+    ws = AnalysisDataService::Instance().retrieveWS<ITableWorkspace>("TestWorkspace");
+    TS_ASSERT_EQUALS(ws->String(0, ThetaCol), "0.70002");
+    TS_ASSERT_EQUALS(ws->String(0,   DQQCol), "0.04");
+    TS_ASSERT_EQUALS(ws->String(1, ThetaCol), "0.7");
+    TS_ASSERT_EQUALS(ws->String(1,   DQQCol), "0.0340301");
+    TS_ASSERT_EQUALS(ws->String(2, ThetaCol), "0.70002");
+    TS_ASSERT_EQUALS(ws->String(2,   DQQCol), "0.0340292");
+
+    //Tidy up
+    AnalysisDataService::Instance().remove("TestWorkspace");
+    AnalysisDataService::Instance().remove("TRANS_13463_13464");
+    AnalysisDataService::Instance().remove("TOF_13460");
+    AnalysisDataService::Instance().remove("TOF_13463");
+    AnalysisDataService::Instance().remove("TOF_13464");
+    AnalysisDataService::Instance().remove("IvsQ_13460");
+    AnalysisDataService::Instance().remove("IvsLam_13460");
+  }
+
   void testBadWorkspaceName()
   {
     MockView mockView;
