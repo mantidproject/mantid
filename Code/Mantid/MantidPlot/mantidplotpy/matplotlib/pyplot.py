e1c233e04e3eba9b47d155b9e99826dea0e73005
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-10-09 10:09:16 +0100

    refs #8912. Array to workspaces
    Conflicts:
    	Code/Mantid/MantidPlot/CMakeLists.txt
    
    issues with indentation and new (Future) test

--- Code/Mantid/MantidPlot/mantidplotpy/matplotlib/pyplot.py
+++ Code/Mantid/MantidPlot/mantidplotpy/matplotlib/pyplot.py
@@ -1,5 +1,7 @@
 import numpy as np
 from mantid.api import MatrixWorkspace as MatrixWorkspace
+from mantid.simpleapi import CreateWorkspace as CreateWorkspace
+import mantidplot  
 
 def __is_array(arg):
     return isinstance(arg, list) or isinstance(arg, np.ndarray) 
@@ -12,16 +14,23 @@ def __is_array_of_workspaces(arg):
 
 
 def __plot_as_workspace(arg):
-    print "TODO plot_as_workspace"
-    pass
+    mantidplot.plotSpectrum(arg, 0)
 
 def __plot_as_workspaces(arg):
-    print "TODO plot as workspaces"
-    pass
+    # plotSpectrum can already handle 1 or more input workspaces.
+    return __plot_as_workspace(arg)
+        
 
-def __plot_as_array(arg):
-    print "TODO plot as array"
-    pass
+def __plot_as_array(y, *args, **kwargs):
+    if len(args) != 0:
+        if __is_array(args):
+            ws = CreateWorkspace(DataX=args, DataY=y)     
+        else:
+            raise ValueError("Inputs are of type: " + str(type(args)) + ". Not plottable." )
+    else:
+        x = range(0, len(y), 1) # 0 to n, incremented by 1.
+        ws = CreateWorkspace(DataX=x, DataY=y)   
+    return __plot_as_workspace(ws)
 
 
 
@@ -30,10 +39,10 @@ def plot (y, x=None, marker=None):
     if __is_array(y):
         if __is_array_of_workspaces(y):
             __plot_as_workspaces(y)
-        elif __is_workspace(y):
-            __plot_as_workspace(y)
         else:
             __plot_as_array(y)
+    elif __is_workspace(y):
+        __plot_as_workspace(y)
     else:
         raise ValueError("Cannot plot argument of type " + str(type(y)) )
          
