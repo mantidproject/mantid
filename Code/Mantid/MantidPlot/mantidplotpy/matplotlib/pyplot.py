7ba73fe5472365352bd3f123dc7ba0928d91af5d
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-10-09 10:45:39 +0100

    refs #8912. Add doc strings
    This is not yet complient with the matplotlib pyplot plot function. That will be the next step.

--- Code/Mantid/MantidPlot/mantidplotpy/matplotlib/pyplot.py
+++ Code/Mantid/MantidPlot/mantidplotpy/matplotlib/pyplot.py
@@ -4,24 +4,62 @@ from mantid.simpleapi import CreateWorkspace as CreateWorkspace
 import mantidplot  
 
 def __is_array(arg):
+    """
+        Is the argument a python or numpy list?
+        @param arg :: argument
+        
+        Returns :: True if the argument a python or numpy list
+    """
     return isinstance(arg, list) or isinstance(arg, np.ndarray) 
 
 def __is_workspace(arg):
+    """
+        Is the argument a Mantid MatrixWorkspace?
+        @param arg :: argument
+        
+        Returns :: True if the argument a MatrixWorkspace
+    """
     return isinstance(arg, MatrixWorkspace)
 
 def __is_array_of_workspaces(arg):
+    """
+        Is the argument a sequence of Mantid MatrixWorkspaces?
+        @param arg :: argument
+        
+        Returns :: True if the argument is a sequence of  MatrixWorkspace
+    """
     return __is_array(arg) and len(arg) > 0 and __is_workspace(arg[0])
 
 
-def __plot_as_workspace(arg):
-    mantidplot.plotSpectrum(arg, 0)
+def __plot_as_workspace(workspaces):
+    """
+        plotSpectrum via qti plotting framework to plot a workspace.
+        @param workspaces :: workspace or list of workspaces
+        
+        Returns :: A plot handle
+    """
+    return mantidplot.plotSpectrum(workspaces, 0)
 
-def __plot_as_workspaces(arg):
+def __plot_as_workspaces(workspaces):
+    """
+        Plot a series of workspaces
+        @param workspaces :: workspaces to plot
+        
+        Returns :: A plot handle
+    """
     # plotSpectrum can already handle 1 or more input workspaces.
-    return __plot_as_workspace(arg)
+    return __plot_as_workspace(workspaces)
         
 
 def __plot_as_array(y, *args, **kwargs):
+    """
+        Plot from an array
+        @param y :: y array
+        @param Args :: x array
+        @param Kwargs :: Matplot lib style options
+        
+        Returns :: A plot handle
+    """
     if len(args) != 0:
         if __is_array(args):
             ws = CreateWorkspace(DataX=args, DataY=y)     
@@ -34,15 +72,23 @@ def __plot_as_array(y, *args, **kwargs):
 
 
 
-def plot (y, x=None, marker=None):
+def plot (y, *args, **kwargs):
+    """
+        Plot the data.
+        @param y :: argument
+        @param args :: curve inputs
+        @param kwargs :: plot line options
+        
+        Returns :: A plot handle.
+    """
     print type(y)
     if __is_array(y):
         if __is_array_of_workspaces(y):
-            __plot_as_workspaces(y)
+            return __plot_as_workspaces(y)
         else:
-            __plot_as_array(y)
+            return __plot_as_array(y)
     elif __is_workspace(y):
-        __plot_as_workspace(y)
+        return __plot_as_workspace(y)
     else:
         raise ValueError("Cannot plot argument of type " + str(type(y)) )
          
