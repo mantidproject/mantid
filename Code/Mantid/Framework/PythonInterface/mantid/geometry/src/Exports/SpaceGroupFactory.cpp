fd637c9e7a815f287836fbe3bf609d02666f81c2
Michael Wedel <michael.wedel@psi.ch>
2014-10-10 14:03:55 +0200

    Refs #10281. Exporting SpaceGroup and Factory to python

---
+++ Code/Mantid/Framework/PythonInterface/mantid/geometry/src/Exports/SpaceGroupFactory.cpp
@@ -0,0 +1,50 @@
+#include "MantidGeometry/Crystal/SpaceGroupFactory.h"
+#include "MantidPythonInterface/kernel/PythonObjectInstantiator.h"
+
+#include <boost/python/class.hpp>
+
+using namespace Mantid::Geometry;
+using namespace boost::python;
+
+namespace
+{
+    using namespace Mantid::PythonInterface;
+
+    std::vector<std::string> allSpaceGroupSymbols(SpaceGroupFactoryImpl &self)
+    {
+        return self.subscribedSpaceGroupSymbols();
+    }
+
+    std::vector<std::string> spaceGroupSymbolsForNumber(SpaceGroupFactoryImpl &self, size_t number)
+    {
+        return self.subscribedSpaceGroupSymbols(number);
+    }
+
+    bool isSubscribedSymbol(SpaceGroupFactoryImpl &self, const std::string &symbol)
+    {
+        return self.isSubscribed(symbol);
+    }
+
+    bool isSubscribedNumber(SpaceGroupFactoryImpl &self, size_t number)
+    {
+        return self.isSubscribed(number);
+    }
+
+}
+
+void export_SpaceGroupFactory()
+{
+
+    class_<SpaceGroupFactoryImpl,boost::noncopyable>("SpaceGroupFactoryImpl", no_init)
+            .def("isSubscribedSymbol", &isSubscribedSymbol)
+            .def("isSubscribedNumber", &isSubscribedNumber)
+            .def("createSpaceGroup", &SpaceGroupFactoryImpl::createSpaceGroup)
+            .def("allSubscribedSpaceGroupSymbols", &allSpaceGroupSymbols)
+            .def("subscribedSpaceGroupSymbols", &spaceGroupSymbolsForNumber)
+            .def("subscribedSpaceGroupNumbers", &SpaceGroupFactoryImpl::subscribedSpaceGroupNumbers)
+            .def("Instance", &SpaceGroupFactory::Instance, return_value_policy<reference_existing_object>(),
+                 "Returns a reference to the SpaceGroupFactory singleton")
+            .staticmethod("Instance")
+            ;
+}
+
