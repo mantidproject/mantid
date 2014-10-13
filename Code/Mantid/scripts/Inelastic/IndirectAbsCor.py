6c4f3e9ca942be9e6c124d4cd3927a76d96b1436
Dan Nixon <dan@dan-nixon.com>
2014-10-13 11:05:20 +0100

    Correct diffraction run axis unit check
    Refs #10094

--- Code/Mantid/scripts/Inelastic/IndirectAbsCor.py
+++ Code/Mantid/scripts/Inelastic/IndirectAbsCor.py
@@ -90,7 +90,7 @@ def AbsRun(inputWS, geom, beam, ncan, size, density, sigs, siga, avar, Verbose, 
     CheckSize(size, geom, ncan, Verbose)
     CheckDensity(density,ncan)
 
-    diffraction_run = checkUnits(inputWS, 'dSpacing')
+    diffraction_run = checkUnitIs(inputWS, 'dSpacing')
 
     det = GetWSangles(inputWS)
     ndet = len(det)
