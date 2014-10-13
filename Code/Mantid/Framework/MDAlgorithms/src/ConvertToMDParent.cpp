f36f3ad3d88b4800e71c3d106e465de44ba47035
Alex Buts <Alex.Buts@stfc.ac.uk>
2014-10-13 17:08:04 +0100

    refs #10332 Allow incident energy to be defined as eFixed log name

--- Code/Mantid/Framework/MDAlgorithms/src/ConvertToMDParent.cpp
+++ Code/Mantid/Framework/MDAlgorithms/src/ConvertToMDParent.cpp
@@ -190,16 +190,25 @@ namespace Mantid
           { 
             // a direct mode instrument can be unchanged but incident energy can be different.
             // It is cheap operation so we should always replace incident energy on the target workspace
-            try
+            bool hasEi= InWS2D->run().hasProperty("Ei");
+            bool hasEfix=InWS2D->run().hasProperty("eFixed");
+            if (hasEi||hasEfix)
             {
-              double Ei =  InWS2D->run().getPropertyValueAsType<double>("Ei");
+
+              double Ei;
+              if (hasEi) Ei=InWS2D->run().getPropertyValueAsType<double>("Ei");
+              if (hasEfix) Ei=InWS2D->run().getPropertyValueAsType<double>("eFixed");
+
               TargTableWS->logs()->addProperty<double>("Ei",Ei,true);           
             }
-            catch(Kernel::Exception::NotFoundError &)
+            else
             {
               Emode = Kernel::DeltaEMode().fromString(dEModeRequested);
               if(Emode==Kernel::DeltaEMode::Direct)
-                  throw(std::invalid_argument("Input neutron's energy has to be attached to the workspace as Ei number log in direct inelastic mode"));
+                  throw(std::invalid_argument("Input neutron's energy has to be present at the workspace as Ei or eFixed number log in Direct inelastic mode"));
+              if(Emode==Kernel::DeltaEMode::Indirect && !hasEfix)
+                  throw(std::invalid_argument("Input neutron's energy has to be present at the workspace as eFixed number log in Indirect inelastic mode"));
+
             }
  
 
