9bc29d88e038352755718221b984f6aea40dd634
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2014-10-29 11:28:25 +0000

    Refs #10408 Use QLabel to show hints
    Instead of relying on QToolTip (which has problems on OSX), create a
    QLabel widget and style it to look like a tooltip.

--- Code/Mantid/MantidQt/MantidWidgets/src/HintingLineEdit.cpp
+++ Code/Mantid/MantidQt/MantidWidgets/src/HintingLineEdit.cpp
@@ -9,6 +9,18 @@ namespace MantidQt
   {
     HintingLineEdit::HintingLineEdit(QWidget *parent, const std::map<std::string,std::string> &hints) : QLineEdit(parent), m_hints(hints), m_dontComplete(false)
     {
+      m_hintLabel = new QLabel(this, Qt::ToolTip);
+      m_hintLabel->setMargin(1 + style()->pixelMetric(QStyle::PM_ToolTipLabelFrameWidth, 0, m_hintLabel));
+      m_hintLabel->setFrameStyle(QFrame::StyledPanel);
+      m_hintLabel->setAlignment(Qt::AlignLeft);
+      m_hintLabel->setWordWrap(true);
+      m_hintLabel->setIndent(1);
+      m_hintLabel->setAutoFillBackground(true);
+      m_hintLabel->setPalette(QToolTip::palette());
+      m_hintLabel->setForegroundRole(QPalette::ToolTipText);
+      m_hintLabel->setBackgroundRole(QPalette::ToolTipBase);
+      m_hintLabel->ensurePolished();
+
       connect(this, SIGNAL(textEdited(const QString&)), this, SLOT(updateHints(const QString&)));
     }
 
@@ -103,7 +115,17 @@ namespace MantidQt
           hintList += QString::fromStdString(mIt->second) + "<br />\n";
       }
 
-      QToolTip::showText(mapToGlobal(QPoint(0, 5)), hintList.trimmed());
+      if(!hintList.trimmed().isEmpty())
+      {
+        m_hintLabel->show();
+        m_hintLabel->setText(hintList.trimmed());
+        m_hintLabel->adjustSize();
+        m_hintLabel->move(mapToGlobal(QPoint(0, height())));
+      }
+      else
+      {
+        m_hintLabel->hide();
+      }
     }
 
     /** Insert an auto completion suggestion beneath the user's cursor and select it */
