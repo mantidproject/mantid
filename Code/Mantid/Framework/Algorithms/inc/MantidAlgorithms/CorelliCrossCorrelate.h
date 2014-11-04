1fffa1a174c827125c111bc19d5154bed4267068
Ross Whitfield <whitfieldre@ornl.gov>
2014-11-04 15:25:23 -0500

    Refs #9984 clang-format it.

--- Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/CorelliCrossCorrelate.h
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/CorelliCrossCorrelate.h
@@ -5,57 +5,55 @@
 #include "MantidAPI/Algorithm.h"
 #include "MantidDataObjects/EventWorkspace.h"
 
-namespace Mantid
-{
-namespace Algorithms
-{
+namespace Mantid {
+namespace Algorithms {
 
-  /** CorelliCrossCorrelate : TODO: DESCRIPTION
-    
-    Copyright &copy; 2014 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge National Laboratory
+/** CorelliCrossCorrelate : TODO: DESCRIPTION
 
-    This file is part of Mantid.
+  Copyright &copy; 2014 ISIS Rutherford Appleton Laboratory & NScD Oak Ridge
+  National Laboratory
 
-    Mantid is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+  This file is part of Mantid.
 
-    Mantid is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  Mantid is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  Mantid is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    File change history is stored at: <https://github.com/mantidproject/mantid>
-    Code Documentation is available at: <http://doxygen.mantidproject.org>
-  */
-  class DLLExport CorelliCrossCorrelate  : public API::Algorithm
-  {
-  public:
-    CorelliCrossCorrelate();
-    virtual ~CorelliCrossCorrelate();
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    virtual const std::string name() const {return "CorelliCrossCorrelate"; };
-    virtual int version() const { return 1;};
-    virtual const std::string category() const { return "Diffraction;Events";};
-    virtual const std::string summary() const { return "Cross-correlation calculation for the elastic signal from Corelli.";};
+  File change history is stored at: <https://github.com/mantidproject/mantid>
+  Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport CorelliCrossCorrelate : public API::Algorithm {
+public:
+  CorelliCrossCorrelate();
+  virtual ~CorelliCrossCorrelate();
 
-  private:
-    std::map<std::string, std::string> validateInputs();
-    void init();
-    void exec();
-
-    /// Input workspace
-    DataObjects::EventWorkspace_const_sptr inputWS;
-    DataObjects::EventWorkspace_sptr outputWS;
-
+  virtual const std::string name() const { return "CorelliCrossCorrelate"; };
+  virtual int version() const { return 1; };
+  virtual const std::string category() const { return "Diffraction;Events"; };
+  virtual const std::string summary() const {
+    return "Cross-correlation calculation for the elastic signal from Corelli.";
   };
 
+private:
+  std::map<std::string, std::string> validateInputs();
+  void init();
+  void exec();
+
+  /// Input workspace
+  DataObjects::EventWorkspace_const_sptr inputWS;
+  DataObjects::EventWorkspace_sptr outputWS;
+};
 
 } // namespace Algorithms
 } // namespace Mantid
 
-#endif  /* MANTID_ALGORITHMS_CORELLICROSSCORRELATE_H_ */
+#endif /* MANTID_ALGORITHMS_CORELLICROSSCORRELATE_H_ */
