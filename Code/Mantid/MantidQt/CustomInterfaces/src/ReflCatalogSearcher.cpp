76482adb784efa95ae4b697d5c3e1bea847a55ed
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2014-11-05 16:28:08 +0000

    Refs #10392 Clean up Catalog search results

--- Code/Mantid/MantidQt/CustomInterfaces/src/ReflCatalogSearcher.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/ReflCatalogSearcher.cpp
@@ -28,9 +28,45 @@ namespace MantidQt
       algSearch->setProperty("InvestigationId", text);
       algSearch->setProperty("OutputWorkspace", "_ReflSearchResults");
       algSearch->execute();
-      auto results = algSearch->getProperty("OutputWorkspace");
+      ITableWorkspace_sptr results = algSearch->getProperty("OutputWorkspace");
 
-      //TODO: Postprocess results (remove non .raw files, etc.)
+      //Now, tidy up the data
+      std::set<size_t> toRemove;
+
+      //Traverse backwards so removals don't trip us up
+      for(size_t i = 0; i < results->rowCount(); ++i)
+      {
+        std::string& run = results->String(i,0);
+
+        //Too short to be more than ".raw"
+        if(run.size() < 5)
+        {
+          toRemove.insert(i);
+        }
+        //If this isn't the right instrument, remove it
+        else if(run.substr(0, instrument.size()) != instrument)
+        {
+          toRemove.insert(i);
+        }
+        //If it's not a raw file, remove it
+        else if(run.substr(run.size() - 4, 4) != ".raw")
+        {
+          toRemove.insert(i);
+        }
+
+        //It's a valid run, so let's trim the instrument prefix and ".raw" suffix
+        run = run.substr(instrument.size(), run.size() - (instrument.size() + 4));
+
+        //Let's also get rid of any leading zeros
+        size_t numZeros = 0;
+        while(run[numZeros] == '0')
+          numZeros++;
+        run = run.substr(numZeros, run.size() - numZeros);
+      }
+
+      //Sets are sorted so if we go from back to front we won't trip over ourselves
+      for(auto row = toRemove.rbegin(); row != toRemove.rend(); ++row)
+        results->removeRow(*row);
 
       return results;
     }
