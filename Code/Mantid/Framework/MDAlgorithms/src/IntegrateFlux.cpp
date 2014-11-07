380a85ef96708c319d1191972ca8fbbc6812aa9a
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2014-11-07 17:02:28 +0000

    Re #10462. Added IntegrateFlux algorithm.

---
+++ Code/Mantid/Framework/MDAlgorithms/src/IntegrateFlux.cpp
@@ -0,0 +1,144 @@
+#include "MantidMDAlgorithms/IntegrateFlux.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/WorkspaceFactory.h"
+
+namespace Mantid
+{
+namespace MDAlgorithms
+{
+
+  using Mantid::Kernel::Direction;
+  using Mantid::API::WorkspaceProperty;
+
+  // Register the algorithm into the AlgorithmFactory
+  DECLARE_ALGORITHM(IntegrateFlux)
+
+  //----------------------------------------------------------------------------------------------
+
+  /// Algorithms name for identification. @see Algorithm::name
+  const std::string IntegrateFlux::name() const { return "IntegrateFlux"; }
+
+  /// Algorithm's version for identification. @see Algorithm::version
+  int IntegrateFlux::version() const { return 1;};
+
+  /// Algorithm's category for identification. @see Algorithm::category
+  const std::string IntegrateFlux::category() const { return "MDAlgorithms";}
+
+  /// Algorithm's summary for use in the GUI and help. @see Algorithm::summary
+  const std::string IntegrateFlux::summary() const { return "Interates spectra in a matrix workspace at a set of points.";};
+
+  //----------------------------------------------------------------------------------------------
+  /** Initialize the algorithm's properties.
+   */
+  void IntegrateFlux::init()
+  {
+    declareProperty(new WorkspaceProperty<DataObjects::EventWorkspace>("InputWorkspace","",Direction::Input), "An input workspace.");
+    declareProperty("NPoints", 1000, "Number of points per output spectrum.");
+    declareProperty(new WorkspaceProperty<API::Workspace>("OutputWorkspace","",Direction::Output), "An output workspace.");
+  }
+
+  //----------------------------------------------------------------------------------------------
+  /** Execute the algorithm.
+   */
+  void IntegrateFlux::exec()
+  {
+    DataObjects::EventWorkspace_sptr inputWS = getProperty("InputWorkspace");
+    size_t nX = static_cast<size_t>( (int)getProperty("NPoints") );
+
+    auto outputWS = createOutputWorkspace( *inputWS, nX );
+
+    integrateSpectra( *inputWS, *outputWS );
+
+    setProperty("OutputWorkspace",outputWS);
+  }
+
+  /**
+   * Create an empty output workspace with required dimensions and defined x-values
+   * @param eventWS :: The input event workspace.
+   * @param nX :: Suggested size of the output spectra. It can change in the actual output.
+   */
+  boost::shared_ptr<API::MatrixWorkspace> IntegrateFlux::createOutputWorkspace( const DataObjects::EventWorkspace& eventWS, size_t nX ) const
+  {
+    size_t nSpec = eventWS.getNumberHistograms();
+
+    if ( nSpec == 0 )
+    {
+      throw std::runtime_error("Input workspace has no data.");
+    }
+
+    // make sure the output spectrum size isn't too large
+    auto nEvents = eventWS.getEventList(0).getNumberEvents();
+    if ( nX > nEvents )
+    {
+      nX = nEvents;
+    }
+
+    // and not 0
+    if ( nX == 0 )
+    {
+      throw std::runtime_error("Zero size of output spectra.");
+    }
+
+    // crate empty output workspace
+    API::MatrixWorkspace_sptr ws = API::WorkspaceFactory::Instance().create( "Workspace2D", nSpec, nX, nX );
+
+    // claculate the integration points and save them in the x-vactors of integrFlux
+    double xMin = eventWS.getEventXMin();
+    double xMax = eventWS.getEventXMax();
+    double dx = ( xMax - xMin ) / static_cast<double>( nX - 1 );
+    auto &X = ws->dataX(0);
+    auto ix = X.begin();
+    // x-values are equally spaced between the min and max tof in the first flux spectrum
+    for(double x = xMin; ix != X.end(); ++ix, x += dx)
+    {
+      *ix = x;
+    }
+
+    // share the xs for all spectra
+    auto xRef = ws->refX(0);
+    for(size_t sp = 1; sp < nSpec; ++sp)
+    {
+      ws->setX(sp,xRef);
+    }
+
+    return ws;
+  }
+
+  /**
+   * Integrate spectra in eventWS at x-values in integrWS and save the results in y-vectors of integrWS.
+   * @param eventWS :: A workspace to integrate. The events have to be weighted-no-time.
+   * @param integrWS :: A workspace to store the results.
+   */
+  void IntegrateFlux::integrateSpectra( const DataObjects::EventWorkspace& eventWS, API::MatrixWorkspace &integrWS )
+  {
+    size_t nSpec = eventWS.getNumberHistograms();
+    assert( nSpec == integrWS.getNumberHistograms() );
+
+    auto &X = integrWS.readX(0);
+    // loop overr the spectra and integrate
+    for(size_t sp = 0; sp < nSpec; ++sp)
+    {
+      std::vector<Mantid::DataObjects::WeightedEventNoTime> el = eventWS.getEventList(sp).getWeightedEventsNoTime();
+      auto &outY = integrWS.dataY(sp);
+      double sum = 0;
+      auto x = X.begin() + 1;
+      size_t i = 1;
+      // the integral is a running sum of the event weights in the spectrum
+      for(auto evnt = el.begin(); evnt != el.end(); ++evnt)
+      {
+        double tof = evnt->tof();
+        while( x != X.end() && *x < tof )
+        {
+          outY[i] = sum;
+          ++x; ++i;
+        }
+        if ( x == X.end() ) break;
+        sum += evnt->weight();
+        outY[i] = sum;
+      }
+    }
+  }
+
+} // namespace MDAlgorithms
+} // namespace Mantid
---
+++ Code/Mantid/Framework/MDAlgorithms/test/IntegrateFluxTest.h
@@ -0,0 +1,146 @@
+#ifndef MANTID_MDALGORITHMS_INTEGRATEFLUXTEST_H_
+#define MANTID_MDALGORITHMS_INTEGRATEFLUXTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidMDAlgorithms/IntegrateFlux.h"
+#include "MantidAPI/AlgorithmManager.h"
+
+using Mantid::MDAlgorithms::IntegrateFlux;
+using namespace Mantid::API;
+
+class IntegrateFluxTest : public CxxTest::TestSuite
+{
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static IntegrateFluxTest *createSuite() { return new IntegrateFluxTest(); }
+  static void destroySuite( IntegrateFluxTest *suite ) 
+  { 
+    delete suite; 
+  }
+
+
+  void test_Init()
+  {
+    IntegrateFlux alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+  }
+
+  void test_exec()
+  {
+    // Name of the input workspace.
+    std::string inWSName("IntegrateFluxTest_InputWS");
+    // Name of the output workspace.
+    std::string outWSName("IntegrateFluxTest_OutputWS");
+
+    // Create an input workspace
+    auto inputWS = createInputWorkspace(inWSName);
+
+    IntegrateFlux alg;
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("InputWorkspace", inWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", outWSName) );
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+
+    // Retrieve the workspace from data service. TODO: Change to your desired type
+    MatrixWorkspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(outWSName) );
+    TS_ASSERT(ws);
+    if (!ws) return;
+
+    TS_ASSERT_EQUALS( ws->getNumberHistograms(), 4 );
+
+    auto &x = ws->readX(0);
+    auto &y = ws->readY(0);
+
+    TS_ASSERT_EQUALS( x.size(), y.size() );
+
+    for(size_t i = 0; i < x.size(); ++i)
+    {
+      double t = x[i];
+      TS_ASSERT_DELTA( y[i], t*(t+1.0), 300.0 );
+    }
+
+    // Remove workspace from the data service.
+    AnalysisDataService::Instance().clear();
+  }
+  
+  void test_one_interpolation_point()
+  {
+    // Name of the input workspace.
+    std::string inWSName("IntegrateFluxTest_InputWS");
+    // Name of the output workspace.
+    std::string outWSName("IntegrateFluxTest_OutputWS");
+
+    // Create an input workspace
+    auto inputWS = createInputWorkspace(inWSName);
+
+    IntegrateFlux alg;
+    alg.initialize();
+    alg.setPropertyValue("InputWorkspace", inWSName);
+    alg.setPropertyValue("OutputWorkspace", outWSName);
+    alg.setProperty("NPoints", 1);
+    alg.execute();
+    TS_ASSERT( alg.isExecuted() );
+
+    // Retrieve the workspace from data service. TODO: Change to your desired type
+    MatrixWorkspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING( ws = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(outWSName) );
+    TS_ASSERT(ws);
+    if (!ws) return;
+
+    TS_ASSERT_EQUALS( ws->getNumberHistograms(), 4 );
+
+    auto &x = ws->readX(0);
+    auto &y = ws->readY(0);
+
+    TS_ASSERT_EQUALS( x.size(), y.size() );
+
+    for(size_t i = 0; i < x.size(); ++i)
+    {
+      double t = x[i];
+      TS_ASSERT_DELTA( y[i], t*(t+1.0), 300.0 );
+    }
+
+    // Remove workspace from the data service.
+    AnalysisDataService::Instance().clear();
+  }
+  
+private:
+
+  Workspace_sptr createInputWorkspace(const std::string& wsName)
+  {
+    auto alg = Mantid::API::AlgorithmManager::Instance().create("CreateSampleWorkspace");
+    alg->initialize();
+    alg->setPropertyValue("WorkspaceType","Event");
+    alg->setPropertyValue("Function","User Defined");
+    alg->setPropertyValue("UserDefinedFunction","name=LinearBackground,A0=1,A1=2");
+    alg->setProperty("NumEvents",10000);
+    alg->setProperty("NumBanks",1);
+    alg->setProperty("BankPixelWidth",2);
+    alg->setProperty("XMin",0.0);
+    alg->setProperty("XMax",100.0);
+    alg->setProperty("BinWidth",1.0);
+    alg->setProperty("OutputWorkspace",wsName);
+    alg->execute();
+
+    alg = Mantid::API::AlgorithmManager::Instance().create("CompressEvents");
+    alg->initialize();
+    alg->setPropertyValue("InputWorkspace",wsName);
+    alg->setPropertyValue("OutputWorkspace",wsName);
+    alg->setProperty("Tolerance",1.0);
+    alg->execute();
+
+    auto ws = AnalysisDataService::Instance().retrieveWS<Workspace>(wsName);
+    return ws;
+  }
+
+
+};
+
+
+#endif /* MANTID_MDALGORITHMS_INTEGRATEFLUXTEST_H_ */
