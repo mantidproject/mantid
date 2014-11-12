2729b82e7cce69c567d6738d5a8ff989af665ad8
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-11-12 11:45:19 +0000

    refs #10530. More TDD around these features.
    Particular focus on projection inputs. Also started adding binning information.

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/CutMD.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/CutMD.py
@@ -15,6 +15,17 @@ class CutMD(DataProcessorAlgorithm):
     def PyInit(self):
         self.declareProperty(IMDEventWorkspaceProperty('InputWorkspace', '', direction=Direction.Input),
                              doc='MDWorkspace to slice')
+        
+        self.declareProperty(ITableWorkspaceProperty('Projection', '', direction=Direction.Input, optional = PropertyMode.Optional), doc='Projection')
+        
+        self.declareProperty(FloatArrayProperty(name='P1Bin', values=[]),
+                             doc='Projection 1 binning')
+        
+        self.declareProperty(FloatArrayProperty(name='P2Bin', values=[]),
+                             doc='Projection 2 binning')
+        
+        self.declareProperty(FloatArrayProperty(name='P3Bin', values=[]),
+                             doc='Projection 3 binning')
 
         self.declareProperty(IMDWorkspaceProperty('OutputWorkspace', '',
                              direction=Direction.Output),
@@ -26,6 +37,19 @@ class CutMD(DataProcessorAlgorithm):
         coord_system = to_cut.getSpecialCoordinateSystem()
         if not coord_system == SpecialCoordinateSystem.HKL:
             raise ValueError("Input Workspace must be in reciprocal lattice dimensions (HKL)")
+        
+        
+        projection = self.getProperty("Projection").value
+        if isinstance(projection, ITableWorkspace):
+            column_names = set(projection.getColumnNames())
+            logger.warning(str(column_names))
+            if not column_names == set(['u', 'v']):
+                if not column_names == set(['u', 'v', 'offsets']):
+                    if not column_names == set(['u', 'v', 'w', 'offsets']):
+                        raise ValueError("Projection table schema is wrong")
+            if projection.rowCount() != 3:
+                raise ValueError("Projection table expects 3 rows")
+        
         
         clone_alg = AlgorithmManager.Instance().create("CloneMDWorkspace")
         clone_alg.setChild(True)
--- Code/Mantid/Framework/PythonInterface/test/python/mantid/api/CutMDTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/api/CutMDTest.py
@@ -1,21 +1,22 @@
 import unittest
 import testhelpers
 
-from mantid.simpleapi import CutMD, CreateMDWorkspace, SetSpecialCoordinates, CompareMDWorkspaces, FakeMDEventData
+from mantid.simpleapi import *
 
 
 class CutMDTest(unittest.TestCase):
     
 
-    def __init__(self, methodName='runTest'):
-        unittest.TestCase.__init__(self, methodName=methodName)
+    def setUp(self):
         # Create a workspace
         data_ws = CreateMDWorkspace(Dimensions=3, Extents=[-10,10,-10,10,-10,10], Names="A,B,C", Units="U,U,U")
+        # Mark the workspace as being in HKL
+        SetSpecialCoordinates(InputWorkspace=data_ws, SpecialCoordinates='HKL')
         # Add some data to the workspace
         FakeMDEventData(InputWorkspace=data_ws, PeakParams=[10000,0,0,0,1])
         self.__in_md  = data_ws
         
-    def __del__(self):
+    def tearDown(self):
         DeleteWorkspace(self.__in_md )
 
     def test_exec_throws_if_not_a_hkl_workspace(self):
@@ -25,11 +26,25 @@ class CutMDTest(unittest.TestCase):
         self.assertRaises(RuntimeError, CutMD, InputWorkspace=test_md, OutputWorkspace="out_ws")
         
     def test_slice_to_original(self):
-        
-        SetSpecialCoordinates(InputWorkspace=self.__in_md, SpecialCoordinates='HKL')
         out_md = CutMD(self.__in_md)
         equals, result = CompareMDWorkspaces(Workspace1=self.__in_md, Workspace2=out_md)
         self.assertTrue(equals, "Input and output workspaces should be identical" )
+        
+    def test_wrong_projection_workspace_format_wrong_column_numbers(self):
+        projection = CreateEmptyTableWorkspace()
+        projection.addColumn("double", "u")
+        # missing other columns
+        self.assertRaises(RuntimeError, CutMD, InputWorkspace=self.__in_md, Projection=projection, OutputWorkspace="out_ws")
+        
+    def test_wrong_table_workspace_format_wrong_row_numbers(self):
+        projection = CreateEmptyTableWorkspace()
+        # Correct number of columns, and names
+        projection.addColumn("double", "u")
+        projection.addColumn("double", "v")
+        projection.addColumn("double", "w")
+        projection.addColumn("double", "offset")
+        # Incorrect number of rows i.e. zero in this case as none added.
+        self.assertRaises(RuntimeError, CutMD, InputWorkspace=self.__in_md, Projection=projection, OutputWorkspace="out_ws")
         
     def test_run_it(self):
         pass
