128ec77e01c7097c355cd77de454bda7dcc1fcfd
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2014-11-13 14:26:46 +0000

    Refs #10238 Remove unneeded virtual keywords
    These won't be inherted. They don't need to be virtual.

--- Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/ReflMainViewPresenter.h
+++ Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/ReflMainViewPresenter.h
@@ -71,7 +71,7 @@ namespace MantidQt
       boost::shared_ptr<ReflTransferStrategy> m_transferStrategy;
 
       //process selected rows
-      virtual void process();
+      void process();
       //Reduce a row
       void reduceRow(int rowNo);
       //prepare a run or list of runs for processing
@@ -95,34 +95,34 @@ namespace MantidQt
       //Stitch some rows
       void stitchRows(std::set<int> rows);
       //insert a row in the model before the given index
-      virtual void insertRow(int index);
+      void insertRow(int index);
       //add row(s) to the model
-      virtual void appendRow();
-      virtual void prependRow();
+      void appendRow();
+      void prependRow();
       //delete row(s) from the model
-      virtual void deleteRow();
+      void deleteRow();
       //clear selected row(s) in the model
-      virtual void clearSelected();
+      void clearSelected();
       //copy selected rows to clipboard
-      virtual void copySelected();
+      void copySelected();
       //copy selected rows to clipboard and then delete them
-      virtual void cutSelected();
+      void cutSelected();
       //paste clipboard into selected rows
-      virtual void pasteSelected();
+      void pasteSelected();
       //group selected rows together
-      virtual void groupRows();
+      void groupRows();
       //expand selection to group
-      virtual void expandSelection();
+      void expandSelection();
       //table io methods
-      virtual void newTable();
-      virtual void openTable();
-      virtual void saveTable();
-      virtual void saveTableAs();
-      virtual void importTable();
-      virtual void exportTable();
+      void newTable();
+      void openTable();
+      void saveTable();
+      void saveTableAs();
+      void importTable();
+      void exportTable();
       //searching
-      virtual void search();
-      virtual void transfer();
+      void search();
+      void transfer();
       //options
       void showOptionsDialog();
       void initOptions();
