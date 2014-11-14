ac61d93e5c006f3a4da6a38c96842c2584d7b092
Ian Bush <ian.bush@tessella.com>
2014-11-14 13:47:36 +0000

    Refs #6428 Replaced hardcoded string with appropriate variable.

--- Code/Mantid/Framework/MDEvents/src/BoxControllerNeXusIO.cpp
+++ Code/Mantid/Framework/MDEvents/src/BoxControllerNeXusIO.cpp
@@ -469,7 +469,7 @@ void BoxControllerNeXusIO::loadBlock(std::vector<double> & Block, const uint64_t
                      std::vector<int64_t> free_dims(2,2);
                      free_dims[0] = int64_t(freeSpaceBlocks.size()/2);
 
-                     m_File->writeUpdatedData("free_space_blocks", freeSpaceBlocks, free_dims);
+                     m_File->writeUpdatedData(g_DBDataName, freeSpaceBlocks, free_dims);
                  }
              }
 
