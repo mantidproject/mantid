8dff93c102cb6f25602831332fd16e511073bea6
John Hill <john.r.hill@stfc.ac.uk>
2014-11-19 18:19:28 +0000

    Refs #10591 creating base files for algorithm

---
+++ Code/Mantid/Framework/RemoteAlgorithms/inc/MantidRemoteAlgorithms/SCARFTomoReconstruction.h
@@ -0,0 +1,81 @@
+#ifndef REMOTE_SCARFTOMORECONSTRUCTION_H_
+#define REMOTE_SCARFTOMORECONSTRUCTION_H_
+
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid {
+namespace RemoteAlgorithms {
+/***
+    Algorithm to initiate a tomographic reconstruction on SCARF at RAL.
+    The algorithm can also be used to to retrieve information about a reconstruction job or to cancel it.
+
+    Input Properties:
+    <UL>
+    <LI> ComputeResource  - The name of the compute resource that will execute the job </LI>
+    <LI> UserName         - User name on the compute resource </LI>
+    <LI> Password         - Password for the compute resource </LI>
+    </UL>
+
+    Output Properties: None.
+    If the authentication is successfull, a cookie is received that is stored internally and
+    re-used for all subsequent interactions with the compute resource.
+
+    
+    @author John R Hill, RAL
+    @date 19/11/2014
+
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+    */
+
+
+class SCARFTomoReconstruction : public Mantid::API::Algorithm
+{
+public:
+  /// (Empty) Constructor
+  SCARFTomoReconstruction() : Mantid::API::Algorithm() {}
+  /// Virtual destructor
+  virtual ~SCARFTomoReconstruction() {}
+  /// Algorithm's name
+  virtual const std::string name() const { return "SCARFTomoReconstruction"; }
+  ///Summary of algorithms purpose
+  virtual const std::string summary() const {return "Perform a tomographic reconstruction action on SCARF at RAL";}
+  /// Algorithm's version
+  virtual int version() const { return (1); }
+  /// Algorithm's category for identification
+  virtual const std::string category() const { return "Remote"; }
+
+private:
+  void init();
+  ///Execution code
+  void exec();
+
+  // ***********
+  // Member vars
+  std::string m_userName;
+  std::string m_password;
+  std::string m_operation;
+  std::string m_nxTomoPath;
+  std::string m_jobID;
+  std::string m_parameterPath;
+};
+
+} // end namespace RemoteAlgorithms
+} // end namespace Mantid
+#endif /*REMOTE_SCARFTOMORECONSTRUCTION_H_*/
---
+++ Code/Mantid/Framework/RemoteAlgorithms/src/SCARFTomoReconstruction.cpp
@@ -0,0 +1,83 @@
+#include "MantidRemoteAlgorithms/SCARFTomoReconstruction.h"
+#include "MantidAPI/FileProperty.h"
+#include "MantidKernel/ListValidator.h"
+#include "MantidKernel/MandatoryValidator.h"
+#include "MantidKernel/MaskedProperty.h"
+#include <boost/assign/std/vector.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+namespace Mantid
+{
+namespace RemoteAlgorithms
+{
+
+
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM(SCARFTomoReconstruction)
+
+using namespace boost::assign;
+using namespace Mantid::Kernel;
+
+void SCARFTomoReconstruction::init()
+{
+  auto requireValue = boost::make_shared<MandatoryValidator<std::string> >(); 
+  
+  std::vector<std::string> reconstructionOps;
+  reconstructionOps += "CreateJob","JobStatus","JobDelete";
+  auto listValue =  boost::make_shared<StringListValidator>(reconstructionOps);
+  
+  std::vector<std::string> exts;
+  exts.push_back(".nxs");
+  exts.push_back(".*");
+
+  // User
+  declareProperty( "UserName", "", requireValue, "Name of the user to authenticate as", Direction::Input);
+
+  // Password
+  declareProperty( new MaskedProperty<std::string>( "Password", "", requireValue, Direction::Input), "The password for the user");
+
+  // Operation to perform : Update description as enum changes
+  declareProperty( "Operation", "", listValue, "Choose the operation to perform on SCARF; [CreateJob,JobStatus,JobDelete]", Direction::Input),
+
+  // NXTomo File path on SCARF
+  declareProperty( new PropertyWithValue<std::string>( "RemoteNXTomoPath", "",  Direction::Input),
+    "The path on SCARF to the NXTomo file to reconstruct");
+    
+  // Job ID on SCARF
+  declareProperty( new PropertyWithValue<std::string>( "JobID", "", Direction::Input) , 
+    "The ID for a currently running job on SCARF");
+
+  // Path to parameter file for reconstruction  
+  declareProperty(new API::FileProperty("ParameterFilePath", "", API::FileProperty::OptionalLoad, exts, Direction::Input), 
+    "Parameter file for the reconstruction job");
+
+}
+
+void SCARFTomoReconstruction::exec()
+{
+  m_userName = getProperty("UserName");
+  m_password = getProperty("Password");
+  m_operation = getProperty("Operation");
+  m_nxTomoPath = getProperty("RemoteNXTomoPath");
+  m_jobID = getProperty("JobID");
+  m_parameterPath = getProperty("ParameterFilePath");
+
+  if(m_operation == "CreateJob")
+  {
+
+  }
+  else if(m_operation == "JobStatus")
+  {
+
+  }
+  else if(m_operation == "JobDelete")
+  {
+
+  }
+
+  g_log.information("Run SCARFTomoReconstruction");
+}
+
+} // end namespace RemoteAlgorithms
+} // end namespace Mantid
