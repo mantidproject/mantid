7cdd1f5408a00917022d7197f3a9889624ca8c0d
Wenduo Zhou <zhouw@ornl.gov>
2014-11-18 22:09:44 -0500

    Refs #10554.  Added a unit test and test file.

---
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/LoadSPICEAsciiTest.py
@@ -0,0 +1,49 @@
+import unittest
+import numpy
+from numpy import *
+from mantid.kernel import *
+from mantid.api import *
+import mantid.kernel as kernel
+from testhelpers import run_algorithm
+from mantid.api import AnalysisDataService
+import os
+
+class LoadSPICEAsciiTest(unittest.TestCase):
+
+    def test_LoadHB2AData(self):
+        """ Test to Load HB2A SPICE data
+        """
+        # Test algorithm
+        alg_test = run_algorithm("LoadSPICEAscii",
+            Filename = "HB2A_exp0231_scan0001.dat", 
+	    OutputWorkspace = "HB2A_0231_0001_Data", 
+	    RunInfoWorkspace = "HB2A_0231_Info")
+
+        # Validate
+        self.assertTrue(alg_test.isExecuted())
+
+        # Output workspace
+	datatbws = AnalysisDataService.retrieve("HB2A_0231_0001_Data")
+	self.assertTrue(datatbws is not None)
+
+	numcols = datatbws.columnCount()
+	self.assertEquals(numcols, 70)
+	
+	colnames = datatbws.getColumnNames()
+	self.assertEquals(colnames[0], "Pt.")
+
+	runinfows = AnalysisDataService.retrieve("HB2A_0231_Info")
+	self.assertTrue(runinfows is not None)
+
+	# Clean
+        AnalysisDataService.remove("HB2A_0231_0001_Data")
+	AnalysisDataService.remove("HB2A_0231_Info")
+
+        return
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
