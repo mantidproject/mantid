42178e4f3491fad82d9c67a16ba5c6264153b372
Steven Hahn <hahnse@ornl.gov>
2014-12-05 12:11:23 -0500

    Refs #10694. attempting to update windows build script

--- Code/Mantid/Build/Jenkins/buildscript.bat
+++ Code/Mantid/Build/Jenkins/buildscript.bat
@@ -61,7 +61,7 @@ if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 :: Build step
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-msbuild /nologo /m:%BUILD_THREADS% /nr:false /p:Configuration=%BUILD_CONFIG% Mantid.sln
+"C:\Program Files (x86)\CMake 2.8\bin\cmake.exe" --build . -- /nologo /m:%BUILD_THREADS% /nr:false /p:Configuration=%BUILD_CONFIG% Mantid.sln
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -75,7 +75,7 @@ if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 if "%CLEANBUILD%" EQU "yes" (
     :: Build offline documentation
-    msbuild /nologo /nr:false /p:Configuration=%BUILD_CONFIG% docs/docs-qthelp.vcxproj
+    C:\Program Files (x86)\CMake 2.8\bin\cmake.exe" --build . -- /nologo /nr:false /p:Configuration=%BUILD_CONFIG% docs/docs-qthelp.vcxproj
 
     :: ignore errors as the exit code of the build isn't correct
     ::if ERRORLEVEL 1 exit /B %ERRORLEVEL%
