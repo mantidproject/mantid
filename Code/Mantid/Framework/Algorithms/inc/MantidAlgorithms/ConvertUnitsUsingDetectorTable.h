5568135e80e5b5ca657e06464cc67b227f7ad237
Stuart Campbell <campbellsi@ornl.gov>
2014-12-10 15:17:07 -0500

    Put into hidden category for now.  refs #10748

--- Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/ConvertUnitsUsingDetectorTable.h
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/ConvertUnitsUsingDetectorTable.h
@@ -73,7 +73,7 @@ public:
   /// Algorithm's version for identification overriding a virtual method
   virtual int version() const { return 1; }
   /// Algorithm's category for identification overriding a virtual method
-  virtual const std::string category() const { return "Transforms\\Units";}
+  virtual const std::string category() const { return "Utility\\Development";}
 
 private:
   const std::string workspaceMethodName() const { return "convertUnitsUsingDetectorTable"; }
