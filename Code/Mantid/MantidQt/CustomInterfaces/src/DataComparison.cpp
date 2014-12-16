d6aa3c1b9ab042e3c8593e13c153654bb8fdd72a
Dan Nixon <dan@dan-nixon.com>
2014-12-16 12:27:26 +0000

    Added basic UI layout and some skeleton functions
    Refs #10802

--- Code/Mantid/MantidQt/CustomInterfaces/src/DataComparison.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/DataComparison.cpp
@@ -19,7 +19,8 @@ using namespace MantidQt::CustomInterfaces;
 //----------------------
 ///Constructor
 DataComparison::DataComparison(QWidget *parent) :
-  UserSubWindow(parent)
+  UserSubWindow(parent),
+  m_plot(new QwtPlot(parent))
 {
 }
 
@@ -27,5 +28,57 @@ DataComparison::DataComparison(QWidget *parent) :
 void DataComparison::initLayout()
 {
   m_uiForm.setupUi(this);
+
+  // Add the plot to the UI
+  m_plot->setCanvasBackground(Qt::white);
+  m_uiForm.loPlot->addWidget(m_plot);
+
+  // Connect push buttons
+  connect(m_uiForm.pbAddData, SIGNAL(clicked()), this, SLOT(addData()));
+
+  connect(m_uiForm.pbRemoveSelectedData, SIGNAL(clicked()), this, SLOT(removeSelectedData()));
+  connect(m_uiForm.pbRemoveAllData, SIGNAL(clicked()), this, SLOT(removeAllData()));
+
+  connect(m_uiForm.pbDiffSelected, SIGNAL(clicked()), this, SLOT(diffSelected()));
+  connect(m_uiForm.pbClearDiff, SIGNAL(clicked()), this, SLOT(clearDiff()));
 }
 
+/**
+ * Adds the data currently selected by the data selector to the plot.
+ */
+void DataComparison::addData()
+{
+}
+
+/**
+ * Removes the data currently selected in the table from the plot.
+ */
+void DataComparison::removeSelectedData()
+{
+}
+
+/**
+ * Removed all loaded data from the plot.
+ */
+void DataComparison::removeAllData()
+{
+}
+
+/**
+ * Creates a diff workspace of the two currently selected workspaces in the table
+ * and plots it on the plot.
+ *
+ * Does nothing if there are not 2 workspaces selected.
+ */
+void DataComparison::diffSelected()
+{
+}
+
+/**
+ * Removes the diff workspace form the plot.
+ *
+ * Does not remove it from ADS.
+ */
+void DataComparison::clearDiff()
+{
+}
