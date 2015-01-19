689f3e6a9895fe1544351d00341d12e884145acd
Martyn Gigg <martyn.gigg@gmail.com>
2015-01-19 13:39:51 +0000

    Unfix patch number for possible future patches

--- Code/Mantid/Build/CMake/VersionNumber.cmake
+++ Code/Mantid/Build/CMake/VersionNumber.cmake
@@ -6,5 +6,5 @@ set ( VERSION_MINOR 3 )
 # UNCOMMENT the next 'set' line to 'force' the patch version number to
 # a value (instead of using the count coming out of 'git describe')
 # DO NOT COMMIT THIS TO MASTER UNCOMMENTED, ONLY TO A RELEASE BRANCH
-set ( VERSION_PATCH 0 )
+#set ( VERSION_PATCH 0 )
 
