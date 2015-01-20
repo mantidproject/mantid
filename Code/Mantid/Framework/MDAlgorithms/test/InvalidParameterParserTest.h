2059c3d27b5d024b2d56a875f3456fc35cf2495b
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-01-20 12:29:22 +0000

    clang-format them to fix conflict issues, re #8491

--- Code/Mantid/Framework/MDAlgorithms/test/InvalidParameterParserTest.h
+++ Code/Mantid/Framework/MDAlgorithms/test/InvalidParameterParserTest.h
@@ -11,28 +11,30 @@
 #include <Poco/DOM/Document.h>
 #include <Poco/DOM/Element.h>
 
-class  InvalidParameterParserTest : public CxxTest::TestSuite
-{
+class InvalidParameterParserTest : public CxxTest::TestSuite {
 public:
+  void testParseInvalidParameterFragment() {
+    using namespace Poco::XML;
+    using namespace Mantid::MDAlgorithms;
 
-    void testParseInvalidParameterFragment()
-    {
-        using namespace Poco::XML;
-        using namespace Mantid::MDAlgorithms;
+    DOMParser pParser;
+    std::string xmlToParse = "<?xml version=\"1.0\" "
+                             "encoding=\"utf-8\"?><Parameter><Type>"
+                             "SomeUnknownParameter</Type><Value>x</Value></"
+                             "Parameter>";
+    Poco::AutoPtr<Document> pDoc = pParser.parseString(xmlToParse);
 
-        DOMParser pParser;
-        std::string xmlToParse = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Parameter><Type>SomeUnknownParameter</Type><Value>x</Value></Parameter>";
-        Poco::AutoPtr<Document> pDoc = pParser.parseString(xmlToParse);
+    InvalidParameterParser parser;
+    Mantid::API::ImplicitFunctionParameter *iparam =
+        parser.createParameter(pDoc->documentElement());
+    InvalidParameter *pInvalidParam = dynamic_cast<InvalidParameter *>(iparam);
+    boost::scoped_ptr<InvalidParameter> invalparam(pInvalidParam);
 
-        InvalidParameterParser parser;
-        Mantid::API::ImplicitFunctionParameter* iparam = parser.createParameter(pDoc->documentElement());
-        InvalidParameter* pInvalidParam = dynamic_cast<InvalidParameter*>(iparam);
-        boost::scoped_ptr<InvalidParameter> invalparam(pInvalidParam);
-
-        TSM_ASSERT("The paramter generated should be an InvalidParamter", NULL != pInvalidParam);
-        TSM_ASSERT_EQUALS("The invalid parameter has not been parsed correctly.", "x", invalparam->getValue());
-    }
-
+    TSM_ASSERT("The paramter generated should be an InvalidParamter",
+               NULL != pInvalidParam);
+    TSM_ASSERT_EQUALS("The invalid parameter has not been parsed correctly.",
+                      "x", invalparam->getValue());
+  }
 };
 
 #endif
