05474b662d5cf3d4760f8d2a28a6863a4234ad2c
Andrei Savici <saviciat@ornl.gov>
2015-01-27 12:05:25 -0500

    Fix for SuggestTibCNCS. Refs #10979

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/SuggestTibCNCSTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/SuggestTibCNCSTest.py
@@ -1,15 +1,24 @@
 import unittest,os
 import mantid
+import numpy
 
 
 class SuggestTibCNCSTest(unittest.TestCase):
     def test_simple(self):
-    	result=mantid.simpleapi.SuggestTibCNCS(3.)
-    	self.assertAlmostEqual(result[0]*0.1,4491.5,0)
-    	self.assertAlmostEqual(result[1]*0.1,4731.5,0)
-    	result=mantid.simpleapi.SuggestTibCNCS(1.)
-    	self.assertAlmostEqual(result[0]*0.1,9562.1,0)
-    	self.assertAlmostEqual(result[1]*0.1,9902.1,0)
+        result=mantid.simpleapi.SuggestTibCNCS(3.)
+        self.assertAlmostEqual(result[0]*0.1,4491.5,0)
+        self.assertAlmostEqual(result[1]*0.1,4731.5,0)
+        result=mantid.simpleapi.SuggestTibCNCS(1.)
+        self.assertAlmostEqual(result[0]*0.1,9562.1,0)
+        self.assertAlmostEqual(result[1]*0.1,9902.1,0)
+        result=mantid.simpleapi.SuggestTibCNCS(6.)
+        self.assertAlmostEqual(result[0]*0.1,2983.3,0)
+        self.assertAlmostEqual(result[1]*0.1,3323.3,0)
+
+    def test_someresult(self):
+        for en in numpy.arange(1.,30.,0.1):
+            result=mantid.simpleapi.SuggestTibCNCS(en)
+            self.assertGreater(result[1]-result[0],1000.)
 
 if __name__=="__main__":
     unittest.main()
