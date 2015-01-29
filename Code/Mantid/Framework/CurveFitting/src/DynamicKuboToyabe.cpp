06a5cb2fc14eb76dc3f9fdf0100eab2ca4f9d35c
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2015-01-29 16:40:10 +0000

    Re #9556 Fitting function improvements

--- Code/Mantid/Framework/CurveFitting/src/DynamicKuboToyabe.cpp
+++ Code/Mantid/Framework/CurveFitting/src/DynamicKuboToyabe.cpp
@@ -5,7 +5,8 @@
 #include "MantidAPI/Jacobian.h"
 #include "MantidAPI/FunctionFactory.h"
 #include <cmath>
-#include <boost/math/special_functions/fpclassify.hpp> // TODO Remove
+#include <vector>
+//#include <boost/math/special_functions/fpclassify.hpp> // TODO Remove
 
 #define EPS 1e-6
 #define JMAX 14
@@ -40,35 +41,21 @@ void DynamicKuboToyabe::init()
   declareParameter("Delta", 0.2, "Local field");
   declareParameter("Field", 0.0, "External field");
   declareParameter("Nu",    0.0, "Hopping rate");
-  //declareParameter("EndX",15);
 }
 
 
 //------------------------------------------------------------------------------------------------
-double *vector(long nl, long nh)
-/* allocate a double vector with subscript range v[nl..nh] */
-{
-	double *v;
+// Routines from Numerical Recipes
 
-	v=(double *)malloc((size_t) ((nh-nl+1+NR_END)*sizeof(double)));
-  if (!v) throw std::runtime_error("allocation failure in dvector()");
-	return v-nl+NR_END;
-}
-void free_vector(double *v, long nl, long nh)
-/* free a double vector allocated with dvector() */
-{
-	free((FREE_ARG) (v+nl-NR_END));
-}
 double midpnt(double func(const double, const double, const double),
 	const double a, const double b, const int n, const double g, const double w0) {
 // quote & modified from numerical recipe 2nd edtion (page147)	
 	
 	static double s;
 
-	if (n==1) { 
-    double s1 = 0.5*(b-a)*func(a,g,w0)+func(b,g,w0);
-    double s2 = (b-a)*func(0.5*(a+b),g,w0);
-    return (s2);
+	if (n==1) {
+    double s1 = (b-a)*func(0.5*(a+b),g,w0);
+    return (s1);
 	} else {
 		double x, tnm, sum, del, ddel;
 		int it, j;
@@ -93,11 +80,9 @@ void polint (double xa[], double ya[], int n, double x, double& y, double& dy) {
 	int i, m, ns = 1;
   double den, dif, dift, ho, hp, w;
 
-  double *c,*d;
-
   dif = fabs(x-xa[1]);
-  c = vector(1,n);
-  d = vector(1,n);
+  std::vector<double> c(n+1);
+  std::vector<double> d(n+1);
 	for (i=1;i<=n;i++){
 		if((dift=fabs(x-xa[i]))<dif) {
 			ns=i;
@@ -122,8 +107,6 @@ void polint (double xa[], double ya[], int n, double x, double& y, double& dy) {
 
 	}
 
-  free_vector(d,1,n);
-  free_vector(c,1,n);
 }
 
 
@@ -150,26 +133,34 @@ double integrate (double func(const double, const double, const double),
 
 
 double f1(const double x, const double G, const double w0) {
+
   return( exp(-G*G*x*x/2)*sin(w0*x));
 }
 
+// Zero Field Kubo Toyabe relaxation function
 double ZFKT (double q){
-  // In zero field: 
-  // g(t) = 1/3 + 2/3 exp( -q/2 ) ( 1 - q )
-  // q = t *sigma
+
   return (0.3333333333 + 0.6666666667*exp(-0.5*q)*(1-q));
 }
+
+// Relaxation function
 double gz (const double x, const double G, const double F) 
 {
 	double w0 = 2.0*3.1415926536*0.01355342*F;
 	const double q = G*G*x*x;
 	
 	if (w0 == 0.0) {
+    // Zero field
 		return (ZFKT(q)); 
 	}
 	else {
-		
-		if (F>2.0*G) { w0 = 2*3.1415926*0.01355342*F ;} else { w0 =2*3.1415926*0.01355342*2.0*G; }
+    // Non-zero field
+
+    if (F>2.0*G) { 
+      w0 = 2*3.1415926*0.01355342*F ;
+    } else { 
+      w0 =2*3.1415926*0.01355342*2.0*G; 
+    }
 			
 		double p = G*G/(w0*w0);
 		double HKT = 1.0-2.0*p*(1-exp(-q/2.0)*cos(w0*x))+2.0*p*p*w0*integrate(f1,0.0,x,G,w0);
@@ -179,7 +170,7 @@ double gz (const double x, const double G, const double F)
    }
 }
 
-// Original function by mark telling
+// Dynamic Kubo Toyabe function
 void DynamicKuboToyabe::function1D(double* out, const double* xValues, const size_t nData)const
 {
   const double& A = getParameter("Asym");
@@ -197,20 +188,41 @@ void DynamicKuboToyabe::function1D(double* out, const double* xValues, const siz
 
   // Non-zero hopping rate
 	else {
-    const int n = 1000;
+
+    // Make sure stepsize is smaller than spacing between xValues
+    int n = 1000;
+    //while (n<nData) {
+    //   n=n*2; 
+    //}
     const double stepsize = fabs(xValues[nData-1]/n);
-    // do{stepsizeTemp=stepsizeTemp/10;nTemp=nTemp*10; }while (xValues[0]<stepsizeTemp);
-    //make sure stepsize is smaller than spacing between xValues.
+
     std::vector<double> funcG(n);
+
+    // Mark's implementation
     for (int i = 0; i < n; i++) {
 
-      double Integral=0.0;
-      for (int c = 1; c <= i; c++) {
-        Integral= gz(c*stepsize,G,F)*exp(-v*c*stepsize)*funcG[i-c]*(stepsize) + Integral; 
+      double efac1=exp(-v*stepsize); // survival prob for step
+      double efac2=(1.0-efac1);      // hop prob ~ hoprate*step
+
+      funcG[0]=gz(0,G,F);
+      funcG[1]=gz(stepsize,G,F);
+
+      for(i=1; i<n; i++) {
+        double gdi=gz(i*stepsize,G,F);
+        for(int j=1; j<i; j++) {
+          gdi= gdi*efac1 + funcG[j]*gz((i-j)*stepsize,G,F)*efac2;
+        }
+        funcG[i]=gdi;
       }
-      funcG[i] = (gz(i*stepsize,G,F)*exp(-v*i*stepsize) + v*Integral);
     }
 
+    //for (int i = 0; i < n; i++) {
+    //  double Integral=0.0;
+    //  for (int c = 1; c <= i; c++) {
+    //    Integral= gz(c*stepsize,G,F)*exp(-v*c*stepsize)*funcG[i-c]*(stepsize) + Integral; 
+    //  }
+    //  funcG[i] = (gz(i*stepsize,G,F)*exp(-v*i*stepsize) + v*Integral);
+    //}
 
 		for (size_t i = 0; i < nData; i++) {
 			double a =xValues[i]/stepsize;
@@ -237,12 +249,6 @@ void DynamicKuboToyabe::functionDeriv1D(API::Jacobian* , const double* , const s
 void DynamicKuboToyabe::setActiveParameter(size_t i, double value) {
 
   setParameter( i, fabs(value), false);
-
-  if (parameterName(i)=="Nu"){
-    if (value<1e-10){
-      setParameter(i,0,false);
-    }
-  }
 
 }
 
