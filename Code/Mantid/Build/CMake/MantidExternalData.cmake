3a54237cbab2e2e3f960a19c3fc54b6016b79e93
Martyn Gigg <martyn.gigg@gmail.com>
2015-01-29 15:52:50 +0000

    Use real remote for external data URL template
    Refs #10869

--- Code/Mantid/Build/CMake/MantidExternalData.cmake
+++ Code/Mantid/Build/CMake/MantidExternalData.cmake
@@ -27,5 +27,5 @@ set(ExternalData_URL_TEMPLATES "" CACHE STRING
 file:///var/bigharddrive/%(algo)/%(hash)")
 mark_as_advanced(ExternalData_URL_TEMPLATES)
 list(APPEND ExternalData_URL_TEMPLATES
-  "file://insertrealservershere.org/%(algo)/%(hash)"
+  "http://198.74.56.37/ftp/external-data/%(algo)/%(hash)"
 )
