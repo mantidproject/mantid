5a48e0ca006a3a241fd6c0feb4aad58d21cea940
Michael Wedel <michael.wedel@psi.ch>
2015-02-06 17:02:18 +0100

    Refs #11035. Remove CurveFitting dependency from SINQ
    Refactored PoldiSpectrumConstantBackground to have FlatBackground as a class member instead of inheriting from it.

--- Code/Mantid/Framework/SINQ/src/PoldiUtilities/PoldiSpectrumConstantBackground.cpp
+++ Code/Mantid/Framework/SINQ/src/PoldiUtilities/PoldiSpectrumConstantBackground.cpp
@@ -12,10 +12,28 @@ DECLARE_FUNCTION(PoldiSpectrumConstantBackground)
 
 /// Default constructor
 PoldiSpectrumConstantBackground::PoldiSpectrumConstantBackground()
-    : FlatBackground(), IPoldiFunction1D(), m_timeBinCount(0) {}
+    : IFunction1D(), IPoldiFunction1D(), m_timeBinCount(0), m_flatBackground() {
+}
 
 /// Destructor
 PoldiSpectrumConstantBackground::~PoldiSpectrumConstantBackground() {}
+
+void PoldiSpectrumConstantBackground::function1D(double *out,
+                                                 const double *xValues,
+                                                 const size_t nData) const {
+
+  if (m_flatBackground) {
+    m_flatBackground->function1D(out, xValues, nData);
+  }
+}
+
+void PoldiSpectrumConstantBackground::functionDeriv1D(Jacobian *out,
+                                                      const double *xValues,
+                                                      const size_t nData) {
+  if (m_flatBackground) {
+    m_flatBackground->functionDeriv1D(out, xValues, nData);
+  }
+}
 
 void PoldiSpectrumConstantBackground::setWorkspace(
     boost::shared_ptr<const API::Workspace> ws) {
@@ -34,15 +52,39 @@ size_t PoldiSpectrumConstantBackground::getTimeBinCount() const {
 void PoldiSpectrumConstantBackground::poldiFunction1D(
     const std::vector<int> &indices, const API::FunctionDomain1D &domain,
     API::FunctionValues &values) const {
-  double backgroundDetector = getParameter(0);
-  double wireCount = static_cast<double>(indices.size());
-  double distributionFactor = wireCount * static_cast<double>(m_timeBinCount) /
-                              static_cast<double>(domain.size());
-  double backgroundD = backgroundDetector * distributionFactor;
+  if (m_flatBackground) {
+    double backgroundDetector = m_flatBackground->getParameter(0);
+    double wireCount = static_cast<double>(indices.size());
+    double distributionFactor = wireCount *
+                                static_cast<double>(m_timeBinCount) /
+                                static_cast<double>(domain.size());
+    double backgroundD = backgroundDetector * distributionFactor;
 
-  for (size_t i = 0; i < values.size(); ++i) {
-    values.addToCalculated(i, backgroundD);
+    for (size_t i = 0; i < values.size(); ++i) {
+      values.addToCalculated(i, backgroundD);
+    }
   }
+}
+
+void PoldiSpectrumConstantBackground::setParameter(const std::string &name,
+                                                   const double &value,
+                                                   bool explicitlySet) {
+  ParamFunction::setParameter(name, value, explicitlySet);
+}
+
+void PoldiSpectrumConstantBackground::setParameter(size_t i,
+                                                   const double &value,
+                                                   bool explicitlySet) {
+  if (m_flatBackground) {
+    m_flatBackground->setParameter(i, value, explicitlySet);
+  }
+}
+
+void PoldiSpectrumConstantBackground::init() {
+  m_flatBackground = boost::dynamic_pointer_cast<IFunction1D>(
+      FunctionFactory::Instance().createFunction("FlatBackground"));
+
+  declareParameter("A0");
 }
 
 } // namespace SINQ
