d6df8a13b5cb974c4b6671eff17b056469e44f3e
Pete Peterson <petersonpf@ornl.gov>
2015-02-12 12:29:55 -0500

    Refactored slave launching script
    Everything is a command line parameter. Also look multiple
    places for `slave.jar` to exist and print exact shell
    commands being write.

--- Code/Mantid/Build/Jenkins/jenkins-slave.sh
+++ Code/Mantid/Build/Jenkins/jenkins-slave.sh
@@ -11,39 +11,71 @@
 # User configuration
 #####################################################################
 # Main url for Jenkins
-JENKINS_URL=
+JENKINS_URL=http://builds.mantidproject.org
 # URL for proxy (if required)
-PROXY_HOST=
+PROXY_HOST=${3}
 # Port for proxy (if required)
-PROXY_PORT=
-# URL of jnlp file for slave. Replace [NODENAME] with the slave name on Jenkins
-SLAVE_AGENT_URL="${JENKINS_URL}/computer/[NODENAME]/slave-agent.jnlp"
+PROXY_PORT=${4}
+# Name of the node as it appears on jenkins
+NODE_NAME=${1}
 # Copy the secret from the command line given at the above URL
-SECRET=
+SECRET=${2}
+
+#####################################################################
+# Constants
+#####################################################################
+# URL of jnlp file for slave
+SLAVE_AGENT_URL="${JENKINS_URL}/computer/${NODE_NAME}/slave-agent.jnlp"
+# name of the slave jar - full path is determined later
+JAR_FILE=slave.jar
 
 #####################################################################
 # Script
 #####################################################################
-JAR_FILE=/tmp/slave.jar
-RUNNING=$(ps aux | grep java | grep $JAR_FILE)
+# exit if it is already running
+RUNNING=$(ps aux | grep java | grep ${JAR_FILE})
 if [ ! -z "${RUNNING}" ]; then
   echo "Slave process is already running"
   exit 0
 else
-  echo "Slave process is not running, starting..."
+  echo "Slave process is not running"
 fi
 
+# error out if there isn't a node name and secret
+if [ "$#" -lt 2 ]; then
+  echo "Usage: `basename ${0}` <NODE_NAME> <SECRET> [PROXY_HOST] [PROXY_PORT]"
+  exit -1
+fi
+
+# setup the proxy
 if [ ! -z "${PROXY_HOST}" ]; then
   PROXY_ARGS="-Dhttp.proxyHost=${PROXY_HOST} -Dhttp.proxyPort=${PROXY_PORT}"
+  echo "using proxy ${PROXY_HOST} ${PROXY_PORT}"
   # For curl
   export http_proxy=http://$PROXY_HOST:$PROXY_PORT
 fi
 
-if [ ! -f ${JAR_FILE} ]; then
-  echo "Downloading slave jar file to ${JAR_FILE}"
-  curl -o ${JAR_FILE} ${JENKINS_URL}/jnlpJars/slave.jar
+# find the jar file if it exists
+if [ -f ${HOME}/jenkins-linode/${JAR_FILE} ]; then
+  JAR_FILE=${HOME}/jenkins-linode/${JAR_FILE}
+elif [ -f ${HOME}/Jenkins/${JAR_FILE} ]; then
+  JAR_FILE=${HOME}/Jenkins/${JAR_FILE}
+else
+  JAR_FILE=/tmp/${JAR_FILE}
+  if [ ! -f ${JAR_FILE} ]; then
+    echo "Downloading slave jar file to ${JAR_FILE}"
+    if [ $(command -v curl) ]; then
+      echo "curl -o ${JAR_FILE} ${JENKINS_URL}/jnlpJars/slave.jar"
+      curl -o ${JAR_FILE} ${JENKINS_URL}/jnlpJars/slave.jar
+    else
+      echo "Need curl to download ${JENKINS_URL}/jnlpJars/slave.jar"
+      exit -1
+    fi
+  fi
 fi
 
-# Start
+echo "starting ..."
+JAVA=`which java`
 JAVA_ARGS="${PROXY_ARGS} -jar ${JAR_FILE} -jnlpUrl ${SLAVE_AGENT_URL} -secret ${SECRET}"
-java ${JAVA_ARGS}
+echo "${JAVA} ${JAVA_ARGS}"
+#${JAVA} ${JAVA_ARGS}
