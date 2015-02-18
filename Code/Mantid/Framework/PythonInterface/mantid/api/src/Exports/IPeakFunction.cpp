6a32a8324a1d49e858ff034f0b864cfcba4e5adc
Michael Wedel <michael.wedel@psi.ch>
2015-02-18 20:42:09 +0100

    Refs #11104. Reverting clang-format on python export
    The clang-format command seemed to cause a compilation error on all systems. Going back to the old format fixes it.

--- Code/Mantid/Framework/PythonInterface/mantid/api/src/Exports/IPeakFunction.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/api/src/Exports/IPeakFunction.cpp
@@ -7,18 +7,16 @@ using Mantid::API::IPeakFunction;
 using Mantid::PythonInterface::IPeakFunctionAdapter;
 using namespace boost::python;
 
-void export_IPeakFunction() {
-  class_<IPeakFunction, bases<IFunction1D>,
-         boost::shared_ptr<IPeakFunctionAdapter>, boost::noncopyable>(
-      "IPeakFunction")
-      .def("functionLocal",
-           (object (IPeakFunctionAdapter::*)(const object &)const) &
-               IPeakFunction::functionLocal,
-           "Calculate the values of the function for the given x values. The "
-           "output should be stored in the out array")
+void export_IPeakFunction()
+{
+  class_<IPeakFunction, bases<IFunction1D>, boost::shared_ptr<IPeakFunctionAdapter>,
+          boost::noncopyable>("IPeakFunction")
+      .def("functionLocal", (object (IPeakFunctionAdapter::*)(const object &)const)&IPeakFunction::functionLocal,
+           "Calculate the values of the function for the given x values. The output should be stored in the out array")
       .def("intensity", &IPeakFunction::intensity,
            "Returns the integral intensity of the peak function.")
       .def("setIntensity", &IPeakFunction::setIntensity,
            "Changes the integral intensity of the peak function by setting its "
-           "height.");
+           "height.")
+          ;
 }
