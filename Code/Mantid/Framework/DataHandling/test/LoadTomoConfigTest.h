ce46c1f8127fa5dc889954eb5918c7fb2a1696ac
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-02-23 12:35:11 +0000

    unit and doc tests now decently complete, re #10889

--- Code/Mantid/Framework/DataHandling/test/LoadTomoConfigTest.h
+++ Code/Mantid/Framework/DataHandling/test/LoadTomoConfigTest.h
@@ -4,10 +4,11 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/ITableWorkspace.h"
 #include "MantidDataHandling/LoadTomoConfig.h" 
 
-#include <Poco/File.h>
-
+using namespace Mantid::API;
 using Mantid::DataHandling::LoadTomoConfig;
 
 class LoadTomoConfigTest : public CxxTest::TestSuite
@@ -15,17 +16,17 @@ class LoadTomoConfigTest : public CxxTest::TestSuite
 public: 
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
-  static SCARFTomoReconstructionTest *createSuite() { return new SCARFTomoReconstructionTest(); }
-  static void destroySuite(SCARFTomoReconstructionTest *suite) { delete suite; }
+  static LoadTomoConfigTest *createSuite() { return new LoadTomoConfigTest(); }
+  static void destroySuite(LoadTomoConfigTest *suite) { delete suite; }
 
   /// Tests casting, general algorithm properties: name, version, etc.
   void test_algorithm()
   {
     testAlg =
       Mantid::API::AlgorithmManager::Instance().create("LoadTomoConfig" /*, 1*/);
-    TS_ASSERT(testAlg);
-    TS_ASSERT_EQUALS(testAlg->name(), "LoadTomoConfig" );
-    TS_ASSERT_EQUALS(testAlg->version(), 1 );
+    TS_ASSERT( testAlg );
+    TS_ASSERT_EQUALS( testAlg->name(), "LoadTomoConfig" );
+    TS_ASSERT_EQUALS( testAlg->version(), 1 );
   }
 
   void test_init()
@@ -33,45 +34,103 @@ public:
     if (!testAlg->isInitialized())
       testAlg->initialize();
 
-    TS_ASSERT_THROWS_NOTHING(test.initialize());
-    TS_ASSERT(alg.isInitialized());
+    TS_ASSERT_THROWS_NOTHING( testAlg->initialize() );
+    TS_ASSERT( testAlg->isInitialized() );
   }
 
   void test_wrongExec()
   {
-    Mantid::API::IAlgorithm_sptr testAlg =
+    IAlgorithm_sptr testFail =
       Mantid::API::AlgorithmManager::Instance().create("LoadTomoConfig" /*, 1*/);
-    TS_ASSERT(testAlg);
-    TS_ASSERT_THROWS_NOTHING(testAlg->initialize());
+    TS_ASSERT(testFail);
+    TS_ASSERT_THROWS_NOTHING(testFail->initialize());
     // exec without filename -> should throw
-    TS_ASSERT_THROWS(testAlg->execute(), std::runtime_error);
+    TS_ASSERT_THROWS(testFail->execute(), std::runtime_error);
     // try to set empty filename
-    TS_ASSERT_THROWS(testAlg->setPropertyValue("Filename",""), std::invalid_argument);
+    TS_ASSERT_THROWS(testFail->setPropertyValue("Filename",""), std::invalid_argument);
   }
 
   // one file with errors/unrecognized content
   void test_wrongContentsFile()
   {
-
+    // TODO: wait until we have a final spec of the format
   }
 
   // one example file that should load fine
   void test_loadOK()
   {
-    //TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("InputWorkspace", m_filename));
+    // Uses examples from the savu repository:
+    // https://github.com/DiamondLightSource/Savu/tree/master/test_data
+    // At the moment load just one file to test basic functionality. Probably more files
+    // should be added here as we have more certainty about the format
+    std::string fname = "savu_test_data_process03.nxs";
+    std::string outWSName = "LoadTomoConfig_test_ws";
+    // Load examples from https://github.com/DiamondLightSource/Savu/tree/master/test_data
+    TS_ASSERT_THROWS_NOTHING(testAlg->setPropertyValue("Filename", fname));
+    TS_ASSERT_THROWS_NOTHING(testAlg->setPropertyValue("OutputWorkspace", outWSName));
 
-    //if (!alg->isInitialized())
-    //  alg->initialize();
-    //TS_ASSERT(alg->isInitialized());
+    if (!testAlg->isInitialized())
+      testAlg->initialize();
+    TS_ASSERT(testAlg->isInitialized());
 
-    //TS_ASSERT_THROWS_NOTHING(alg.execute());
-    //TS_ASSERT(alg.isExecuted());
+    TS_ASSERT_THROWS_NOTHING( testAlg->execute() );
+    TS_ASSERT( testAlg->isExecuted() );
+
+    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
+
+    TS_ASSERT( ads.doesExist(outWSName) );
+    ITableWorkspace_sptr ws;
+    TS_ASSERT_THROWS_NOTHING(ws = ads.retrieveWS<ITableWorkspace>(outWSName) );
+
+    // general format: 3 columns (data, id, name)
+    TS_ASSERT_EQUALS( ws->columnCount(), 3) ;
+    TS_ASSERT_EQUALS( ws->rowCount(), 2 );
+
+    checkColumns(ws);
+
+    // this example has 3 plugins: savu.plugins.timeseries_fields_corrections, savu.median_filter,
+    // savu.plugins.simple_recon
+    // ID
+    TS_ASSERT_EQUALS( ws->cell<std::string>(0, 0), "savu.plugins.timeseries_fields_corrections" );
+    TS_ASSERT_EQUALS( ws->cell<std::string>(1, 0), "savu.plugins.median_filter" );
+    TS_ASSERT_EQUALS( ws->cell<std::string>(2, 0), "savu.plugins.simple_recon" );
+
+    // data entry in NeXus file (Params column)
+    TS_ASSERT_EQUALS( ws->cell<std::string>(0, 1), "{}" );
+    TS_ASSERT_EQUALS( ws->cell<std::string>(1, 1), "{\"kernel_size\": [1, 3, 3]}" );
+    TS_ASSERT_EQUALS( ws->cell<std::string>(2, 1), "{\"center_of_rotation\": 86}" );
+
+    // name entry in NeXus file
+    TS_ASSERT_EQUALS( ws->cell<std::string>(0, 2), "Timeseries Field Corrections" );
+    TS_ASSERT_EQUALS( ws->cell<std::string>(1, 2), "Median Filter" );
+    TS_ASSERT_EQUALS( ws->cell<std::string>(2, 2), "Simple Reconstruction" );
+
+    // cite information, not presently available in example files
+    for (size_t i=0; i<nRows; i++) {
+      TS_ASSERT_EQUALS( ws->cell<std::string>(i, 3), "" );
+    }
   }
 
 private:
-  IAlgorithm* testAlg;
+
+  void checkColumns(ITableWorkspace_sptr& table)
+  {
+    // each row of the workspace should have: ID, Params, Name, Cite
+    std::vector<std::string> names = table->getColumnNames();
+    TS_ASSERT_EQUALS( names.size(), 4 );
+    TS_ASSERT_EQUALS( names[0], "ID" );
+    TS_ASSERT_EQUALS( names[1], "Params" );
+    TS_ASSERT_EQUALS( names[2], "Name" );
+    TS_ASSERT_EQUALS( names[3], "Cite" );
+  }
+
+  IAlgorithm_sptr testAlg;
   LoadTomoConfig alg;
-  std::string m_filename;
+  static const size_t nRows;
+  static const size_t nCols;
 };
+
+const size_t LoadTomoConfigTest::nRows = 3;
+const size_t LoadTomoConfigTest::nCols = 4;
 
 #endif /* LOADTOMOCONFIGTEST_H__*/
--- Code/Mantid/docs/source/algorithms/LoadTomoConfig-v1.rst
+++ Code/Mantid/docs/source/algorithms/LoadTomoConfig-v1.rst
@@ -12,13 +12,18 @@ Description
 This algorithm reads a tomographic reconstruction parameterization
 (configuration) file and stores the configuration in a `TableWorkspace
 <http://www.mantidproject.org/TableWorkspace>`_. The file is expected
-to follow the following format. It contains a sequence of plugins to
-be used for tomographic reconstruction. For each plugin four fields
-are specificed in this order: id, parameters, name, and cite. All
-fields are character strings. The parameters field is formatted as a
-JSON string of name,value pairs. The workspace produced has one row
-for every plugin found in the input file, and four columns of string
-type.
+to follow the format used in the savu tomography reconstruction
+pipeline `<https://github.com/DiamondLightSource/Savu>`__. These files
+specify a sequence of plugins to be used for tomographic
+reconstruction. For each plugin four fields are given in this order:
+id, parameters, name, and cite. All fields are character strings. The
+parameters field is formatted as a JSON string of name,value
+pairs. The workspace produced has one row for every plugin found in
+the input file, and four columns of string type.
+
+This algorithm is used by the IMAT tomography reconstruction interface
+(GUI) to load and display configurations that can then be edited and
+saved.
 
 Usage
 -----
@@ -27,14 +32,17 @@ Usage
 
 .. testcode:: LoadTomoConfig
 
-    # TODO: check, put the example file, and finalize
-    tws = LoadNexusMonitors("CNCS_7860_event.nxs")
+    tws = LoadNexusMonitors("savu_test_data_process03.nxs", OutputWorkspace='savu_tomo_config')
     print "Number of columns: ", tws.columnCount()
     print "Number of rows / processing plugins: ", tws.rowCount()
     print "Cell 0,0: ", tws.cell(0,0)
     print "Cell 0,1: ", tws.cell(0,1)
     print "Cell 0,2: ", tws.cell(0,2)
     print "Cell 0,3: ", tws.cell(0,3)
+    print "Cell 1,0: ", tws.cell(2,0)
+    print "Cell 1,1: ", tws.cell(2,1)
+    print "Cell 1,2: ", tws.cell(2,2)
+    print "Cell 1,3: ", tws.cell(2,3)
     print "Cell 2,0: ", tws.cell(2,0)
     print "Cell 2,1: ", tws.cell(2,1)
     print "Cell 2,2: ", tws.cell(2,2)
@@ -44,16 +52,19 @@ Output:
 
 .. testoutput:: LoadTomoConfig
 
-    # TODO: check and finalize
     Number of columns: 4
     Number of rows / processing plugins: 3
-    Cell 0,0: id-string
-    Cell 0,1: parameters-string
-    Cell 0,2: pluging name
-    Cell 0,3: cite
-    Cell 1,0: id-string
-    Cell 1,1: parameters-string
-    Cell 1,2: pluging name
-    Cell 1,3: cite
+    Cell 0,0: savu.plugins.timeseries_fields_corrections
+    Cell 0,1: {}
+    Cell 0,2: Timeseries Field Corrections
+    Cell 0,3: Not available
+    Cell 1,0: savu.plugins.median_filter
+    Cell 1,1: {"kernel_size": [1, 3, 3]}
+    Cell 1,2: Median Filter
+    Cell 1,3: Not available
+    Cell 1,0: savu.plugins.simple_recon
+    Cell 1,1: {"center_of_rotation": 86}
+    Cell 1,2: Simple Reconstruction
+    Cell 1,3: Not available
 
 .. categories::
