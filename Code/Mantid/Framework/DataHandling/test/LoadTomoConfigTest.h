6a66f59ac140ab9f638de174a3f8aee723ac75ea
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-02-23 09:41:40 +0000

    use algorithm factory in test, re #10889

--- Code/Mantid/Framework/DataHandling/test/LoadTomoConfigTest.h
+++ Code/Mantid/Framework/DataHandling/test/LoadTomoConfigTest.h
@@ -13,18 +13,28 @@ using Mantid::DataHandling::LoadTomoConfig;
 class LoadTomoConfigTest : public CxxTest::TestSuite
 {
 public: 
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static SCARFTomoReconstructionTest *createSuite() { return new SCARFTomoReconstructionTest(); }
+  static void destroySuite(SCARFTomoReconstructionTest *suite) { delete suite; }
 
   /// Tests casting, general algorithm properties: name, version, etc.
   void test_algorithm()
   {
-    Mantid::API::IAlgorithm_sptr testAlg =
+    testAlg =
       Mantid::API::AlgorithmManager::Instance().create("LoadTomoConfig" /*, 1*/);
     TS_ASSERT(testAlg);
+    TS_ASSERT_EQUALS(testAlg->name(), "LoadTomoConfig" );
+    TS_ASSERT_EQUALS(testAlg->version(), 1 );
+  }
+
+  void test_init()
+  {
     if (!testAlg->isInitialized())
       testAlg->initialize();
-    TS_ASSERT_EQUALS(testAlg->version(), 1 );
-    TS_ASSERT_EQUALS(testAlg->name(), "LoadTomoConfig" );
-    
+
+    TS_ASSERT_THROWS_NOTHING(test.initialize());
+    TS_ASSERT(alg.isInitialized());
   }
 
   void test_wrongExec()
@@ -37,12 +47,6 @@ public:
     TS_ASSERT_THROWS(testAlg->execute(), std::runtime_error);
     // try to set empty filename
     TS_ASSERT_THROWS(testAlg->setPropertyValue("Filename",""), std::invalid_argument);
-  }
-
-  void test_init()
-  {
-    TS_ASSERT_THROWS_NOTHING(alg.initialize());
-    TS_ASSERT(alg.isInitialized());
   }
 
   // one file with errors/unrecognized content
@@ -65,7 +69,7 @@ public:
   }
 
 private:
-
+  IAlgorithm* testAlg;
   LoadTomoConfig alg;
   std::string m_filename;
 };
