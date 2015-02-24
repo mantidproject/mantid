4fc36494dccaa56360192531079283fddf92b438
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-02-24 20:38:02 +0000

    unit test starts to look good, more coming, re #10776

--- Code/Mantid/Framework/DataHandling/test/SaveTomoConfigTest.h
+++ Code/Mantid/Framework/DataHandling/test/SaveTomoConfigTest.h
@@ -3,11 +3,12 @@
 
 #include <cxxtest/TestSuite.h>
 
-#include "MantidAPI/WorkspaceFactory.h"
+#include "MantidAPI/AlgorithmManager.h"
 #include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidAPI/WorkspaceFactory.h"
 #include "MantidDataHandling/SaveTomoConfig.h"
-#include "MantidTestHelpers/WorkspaceCreationHelper.h"
 
 #include <Poco/File.h>
 
@@ -17,60 +18,106 @@ using namespace Mantid::DataHandling;
 class SaveTomoConfigTest : public CxxTest::TestSuite
 {
 public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static SaveTomoConfigTest *createSuite() { return new SaveTomoConfigTest(); }
+  static void destroySuite(SaveTomoConfigTest *suite) { delete suite; }
 
+  /// Tests casting, general algorithm properties: name, version, etc.
+  void test_algorithm()
+  {
+    testSave =
+      Mantid::API::AlgorithmManager::Instance().create("SaveTomoConfig" /*, 1*/);
+    TS_ASSERT( testSave );
+    TS_ASSERT_EQUALS( testSave->name(), "SaveTomoConfig" );
+    TS_ASSERT_EQUALS( testSave->version(), 1 );
+  }
 
   void test_init()
   {
-    TS_ASSERT_THROWS_NOTHING(alg.initialize());
-    TS_ASSERT( alg.isInitialized() );
+    if (!testSave->isInitialized())
+      testSave->initialize();
+
+    TS_ASSERT_THROWS_NOTHING( testSave->initialize() );
+    TS_ASSERT( testSave->isInitialized() );
   }
 
-  void test_save_reload()
+  void test_wrongExec()
   {
-    // Mantid::DataHandling::SaveTomoConfig tcSave;
+    std::string wsName = "simple_table";
+    ITableWorkspace_sptr ws = makeTableWorkspace(wsName);
 
-    if (!alg.isInitialized())
-      alg.initialize();
+    IAlgorithm_sptr testFail =
+      Mantid::API::AlgorithmManager::Instance().create("SaveTomoConfig" /*, 1*/);
+    TS_ASSERT( testFail );
+    TS_ASSERT_THROWS_NOTHING( testFail->initialize() );
+    // exec without InputWorkspaces property set -> should throw
+    TS_ASSERT_THROWS( testFail->execute(), std::runtime_error );
+    // try to set empty InputWorkspaces
+    TS_ASSERT_THROWS( testFail->setPropertyValue("InputWorkspaces",""), std::invalid_argument );
+    TS_ASSERT( !testFail->isExecuted() );
 
-    // TODO: sort out save/load files in tests
-    /* std::string outputSpace,inputFile; */
-    /* nxLoad.initialize(); */
-    /* // Now set required filename and output workspace name */
-    /* inputFile = "emu00006473.nxs"; */
-    /* nxLoad.setPropertyValue("Filename", inputFile); */
-    /* outputSpace="outer"; */
-    /* nxLoad.setPropertyValue("OutputWorkspace", outputSpace);      */
+    // exec with InputWorkspaces but empty Filename -> should throw
+    IAlgorithm_sptr fail2 =
+      Mantid::API::AlgorithmManager::Instance().create("SaveTomoConfig" /*, 1*/);
+    TS_ASSERT_THROWS_NOTHING( fail2->initialize() );
+    TS_ASSERT_THROWS_NOTHING( fail2->setPropertyValue("InputWorkspaces", wsName) );
+    TS_ASSERT_THROWS( fail2->setPropertyValue("Filename", ""), std::invalid_argument );
+    TS_ASSERT_THROWS( fail2->execute(), std::runtime_error );
+    TS_ASSERT( !fail2->isExecuted() );
 
-    /* TS_ASSERT_THROWS_NOTHING(alg.execute()); */
-    /* TS_ASSERT(alg.isExecuted()); */
-
-    /* Workspace_sptr out; */
-    /* TS_ASSERT_THROWS_NOTHING(out = AnalysisDataService::Instance().retrieve(outputSpace));     */
-
-    /* ITableWorkspace_sptr tws = boost::dynamic_pointer_cast<ITableWorkspace>(out); */
+    // exec with InputWorkspaces but no Filename -> should throw
+    IAlgorithm_sptr fail3 =
+      Mantid::API::AlgorithmManager::Instance().create("SaveTomoConfig" /*, 1*/);
+    TS_ASSERT_THROWS_NOTHING( fail3->initialize() );
+    TS_ASSERT_THROWS_NOTHING( fail3->setPropertyValue("InputWorkspaces", wsName) );
+    TS_ASSERT_THROWS( fail3->execute(), std::runtime_error );
   }
 
-  void test_pass_inputworkspace_as_pointer()
+  void test_wrongTableFormat()
   {
-    Workspace_sptr ws = WorkspaceCreationHelper::Create2DWorkspace123(2,5);
+    std::string wsName = "bad_table";
+    ITableWorkspace_sptr ws = makeTableWorkspace(wsName);
 
-    // TODO: sort out save/load files in tests
-    /* SaveNexus alg; */
-    /* alg.initialize(); */
-    /* alg.setProperty("InputWorkspace",ws); */
-    /* alg.setProperty("Filename", "out.nxs"); */
-
-    /* std::string outputFile = alg.getPropertyValue("Filename"); */
-
-    /* const bool executed = alg.execute(); */
-    /* TSM_ASSERT( "SaveNexus did not execute successfully", executed ) */
-    /* if ( executed ) Poco::File(outputFile).remove(); */
+    // TODO: should throw
   }
-  
+
 private:
 
-  SaveTomoConfig alg;
+  ITableWorkspace_sptr makeTableWorkspace(std::string &name)
+  {
+    ITableWorkspace_sptr ws = WorkspaceFactory::Instance().createTable();
+    AnalysisDataService::Instance().addOrReplace(name, ws);
+    ws->addColumn("str","ID");
+    ws->addColumn("str","Name");
+    ws->addColumn("str","Parameters");
+    ws->addColumn("str","Cite");
+
+    Mantid::API::TableRow row = ws->appendRow();
+    row << "savu.id1" << "{\"param1\": val1}" << "name 1" << "cite 1";
+    row = ws->appendRow();
+    row << "savu.id2" << "{\"param2\": val2}" << "name 2" << "cite 2";
+
+    return ws;
+  }
+
+  // intentionally forgets to add some columns
+  ITableWorkspace_sptr makeWrongTableWorkspace(std::string &name)
+  {
+    ITableWorkspace_sptr ws = WorkspaceFactory::Instance().createTable();
+    AnalysisDataService::Instance().addOrReplace(name, ws);
+    ws->addColumn("str","ID");
+    ws->addColumn("str","Name");
+
+    Mantid::API::TableRow row = ws->appendRow();
+    row << "savu.id1" << "{\"param1\": val1}";
+    row = ws->appendRow();
+    row << "savu.id2" << "{\"param2\": val2}";
+
+    return ws;
+  }
+
+  IAlgorithm_sptr testSave;
   std::string outFilename;
-  std::string title;
 };
-#endif /*SAVETOMOCONFIGTEST_H*/
+#endif /* SAVETOMOCONFIGTEST_H */
