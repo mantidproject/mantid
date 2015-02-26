1e043fcb2cd290d6e040141d5e9bd84cb0f4e3f3
Michael Wedel <michael.wedel@psi.ch>
2015-02-26 16:58:59 +0100

    Refs #10305. Introduced aliases for geometry factories

--- Code/Mantid/Framework/PythonInterface/test/python/mantid/geometry/SymmetryElementTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/mantid/geometry/SymmetryElementTest.py
@@ -1,26 +1,26 @@
 import unittest
-from mantid.geometry import SymmetryOperation, SymmetryOperationFactoryImpl
-from mantid.geometry import SymmetryElement, SymmetryElementFactoryImpl
+from mantid.geometry import SymmetryOperation, SymmetryOperationFactory
+from mantid.geometry import SymmetryElement, SymmetryElementFactory
 from mantid.kernel import V3D
 
 class SymmetryElementTest(unittest.TestCase):
 
     def test_creation_axis(self):
-        symOp = SymmetryOperationFactoryImpl.Instance().createSymOp("x,y,-z")
-        symEle = SymmetryElementFactoryImpl.Instance().createSymElement(symOp)
+        symOp = SymmetryOperationFactory.createSymOp("x,y,-z")
+        symEle = SymmetryElementFactory.createSymElement(symOp)
 	
 	self.assertEquals(symEle.hmSymbol(), "m")
 	self.assertEquals(symEle.getAxis(), V3D(0,0,1))
 	
-	rotation = SymmetryOperationFactoryImpl.Instance().createSymOp("x,-y,-z")
-        rotationElement = SymmetryElementFactoryImpl.Instance().createSymElement(rotation)
+	rotation = SymmetryOperationFactory.createSymOp("x,-y,-z")
+        rotationElement = SymmetryElementFactory.createSymElement(rotation)
 	
 	self.assertEquals(rotationElement.hmSymbol(), "2")
 	self.assertEquals(rotationElement.getAxis(), V3D(1,0,0))
 
     def test_creation_no_axis(self):
-        symOp = SymmetryOperationFactoryImpl.Instance().createSymOp("-x,-y,-z")
-        symEle = SymmetryElementFactoryImpl.Instance().createSymElement(symOp)
+        symOp = SymmetryOperationFactory.createSymOp("-x,-y,-z")
+        symEle = SymmetryElementFactory.createSymElement(symOp)
 	
 	self.assertEquals(symEle.hmSymbol(), "-1")
 	self.assertEquals(symEle.getAxis(), V3D(0,0,0))
