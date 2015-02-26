593836588c161566b9044d425be4a25123c67fcc
Dan Nixon <dan@dan-nixon.com>
2015-02-26 14:17:43 +0000

    Run a reduction and validation
    Refs #11162

--- Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ILLEnergyTransfer.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ILLEnergyTransfer.cpp
@@ -20,13 +20,13 @@ namespace CustomInterfaces
   {
     m_uiForm.setupUi(parent);
 
-    // SIGNAL/SLOT CONNECTIONS
-    // Update instrument information when a new instrument config is selected
     connect(this, SIGNAL(newInstrumentConfiguration()), this, SLOT(setInstrumentDefault()));
+    connect(m_batchAlgoRunner, SIGNAL(batchComplete(bool)), this, SLOT(algorithmComplete(bool)));
 
     // Validate to remove invalid markers
     validateTab();
   }
+
 
   //----------------------------------------------------------------------------------------------
   /** Destructor
@@ -43,27 +43,80 @@ namespace CustomInterfaces
 
   bool ILLEnergyTransfer::validate()
   {
-    //TODO
-    return false;
+    UserInputValidator uiv;
+
+    // Validate run file
+    if(!m_uiForm.rfInput->isValid())
+      uiv.addErrorMessage("Run File is invalid.");
+
+    // Validate map file if it is being used
+    bool useMapFile = m_uiForm.cbGroupingType->currentText() == "Map File";
+    if(useMapFile && !m_uiForm.rfInput->isValid())
+      uiv.addErrorMessage("Map File is invalid.");
+
+    // Show error message for errors
+    if(!uiv.isAllInputValid())
+      showMessageBox(uiv.generateErrorMessage());
+
+    return uiv.isAllInputValid();
   }
 
 
   void ILLEnergyTransfer::run()
   {
-    //TODO
+    IAlgorithm_sptr reductionAlg = AlgorithmManager::Instance().create("IndirectILLReduction");
+    reductionAlg->initialize();
+
+    reductionAlg->setProperty("Analyser", getInstrumentConfiguration()->getAnalyserName().toStdString());
+    reductionAlg->setProperty("Reflection", getInstrumentConfiguration()->getReflectionName().toStdString());
+
+    // Handle einput files
+    QString filename = m_uiForm.rfInput->getFirstFilename();
+    reductionAlg->setProperty("Run", filename.toStdString());
+
+    // Handle mapping file
+    bool useMapFile = m_uiForm.cbGroupingType->currentText() == "Map File";
+    if(useMapFile)
+    {
+      QString mapFilename = m_uiForm.rfMapFile->getFirstFilename();
+      reductionAlg->setProperty("MapFile", mapFilename.toStdString());
+    }
+
+    // Set mirror mode option
+    bool mirrorMode = m_uiForm.ckMirrorMode->isChecked();
+    reductionAlg->setProperty("MirrorMode", mirrorMode);
+
+    // Set left and right workspaces when using mirror mode
+    if(mirrorMode)
+    {
+      reductionAlg->setProperty("LeftWorkspace", "l"); //TODO
+      reductionAlg->setProperty("RightWorkspace", "r"); //TODO
+    }
+
+    // Set output workspace properties
+    reductionAlg->setProperty("RawWorkspace", "raw"); //TODO
+    reductionAlg->setProperty("ReducedWorkspace", "red"); //TODO
+
+    // Set output options
+    reductionAlg->setProperty("Plot", m_uiForm.ckPlot->isChecked());
+    reductionAlg->setProperty("Save", m_uiForm.ckSave->isChecked());
+
+    m_batchAlgoRunner->addAlgorithm(reductionAlg);
+    m_batchAlgoRunner->executeBatchAsync();
   }
 
 
   /**
    * Handles completion of the algorithm.
    *
-   * Sets result workspace for Python export and ungroups result WorkspaceGroup.
-   *
    * @param error True if the algorithm was stopped due to error, false otherwise
    */
   void ILLEnergyTransfer::algorithmComplete(bool error)
   {
-    //TODO
+    if(error)
+      return;
+
+    // Nothing to do here
   }
 
 
@@ -72,7 +125,11 @@ namespace CustomInterfaces
    */
   void ILLEnergyTransfer::setInstrumentDefault()
   {
-    //TODO
+    std::map<QString, QString> instDetails = getInstrumentDetails();
+
+    // Set instrument in run file widgets
+    m_uiForm.rfInput->setInstrumentOverride(instDetails["instrument"]);
+    m_uiForm.rfMapFile->setInstrumentOverride(instDetails["instrument"]);
   }
 
 
