61c81c35e98609b9b9aadd3db1320338220c467e
Pete Peterson <petersonpf@ornl.gov>
2015-03-04 14:32:19 -0500

    Re #11263. Added algorithm that segfaults mantid.

--- Code/Mantid/Framework/Algorithms/src/Segfault.cpp
+++ Code/Mantid/Framework/Algorithms/src/Segfault.cpp
@@ -1,65 +1,61 @@
 #include "MantidAlgorithms/Segfault.h"
 
-namespace Mantid
-{
-namespace Algorithms
-{
+namespace Mantid {
+namespace Algorithms {
 
-  using Mantid::Kernel::Direction;
-  using Mantid::API::WorkspaceProperty;
+using Mantid::Kernel::Direction;
+using Mantid::API::WorkspaceProperty;
 
-  // Register the algorithm into the AlgorithmFactory
-  DECLARE_ALGORITHM(Segfault)
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM(Segfault)
 
+//----------------------------------------------------------------------------------------------
+/** Constructor
+ */
+Segfault::Segfault() {}
 
+//----------------------------------------------------------------------------------------------
+/** Destructor
+ */
+Segfault::~Segfault() {}
 
-  //----------------------------------------------------------------------------------------------
-  /** Constructor
-   */
-  Segfault::Segfault()
-  {
+//----------------------------------------------------------------------------------------------
+
+/// Algorithms name for identification. @see Algorithm::name
+const std::string Segfault::name() const { return "Segfault"; }
+
+/// Algorithm's version for identification. @see Algorithm::version
+int Segfault::version() const { return 1; }
+
+/// Algorithm's category for identification. @see Algorithm::category
+const std::string Segfault::category() const { return "Utility\\Development"; }
+
+/// Algorithm's summary for use in the GUI and help. @see Algorithm::summary
+const std::string Segfault::summary() const {
+  return "WARNING: THIS CRASHES MANTID";
+}
+
+//----------------------------------------------------------------------------------------------
+/** Initialize the algorithm's properties.
+ */
+void Segfault::init() {
+  declareProperty("DryRun", true,
+                  "Just log to the error channel but don't crash mantid");
+}
+
+//----------------------------------------------------------------------------------------------
+/** Execute the algorithm.
+ */
+void Segfault::exec() {
+  bool dryrun = getProperty("DryRun");
+  g_log.error("Crashing mantid now");
+
+  if (!dryrun) {
+    // writing to read-only memory
+    char *s = "hello world";
+    *s = 'H';
   }
-
-  //----------------------------------------------------------------------------------------------
-  /** Destructor
-   */
-  Segfault::~Segfault()
-  {
-  }
-
-
-  //----------------------------------------------------------------------------------------------
-
-  /// Algorithms name for identification. @see Algorithm::name
-  const std::string Segfault::name() const { return "Segfault"; }
-
-  /// Algorithm's version for identification. @see Algorithm::version
-  int Segfault::version() const { return 1;};
-
-  /// Algorithm's category for identification. @see Algorithm::category
-  const std::string Segfault::category() const { return TODO: FILL IN A CATEGORY;}
-
-  /// Algorithm's summary for use in the GUI and help. @see Algorithm::summary
-  const std::string Segfault::summary() const { return TODO: FILL IN A SUMMARY;};
-
-  //----------------------------------------------------------------------------------------------
-  /** Initialize the algorithm's properties.
-   */
-  void Segfault::init()
-  {
-    declareProperty(new WorkspaceProperty<>("InputWorkspace","",Direction::Input), "An input workspace.");
-    declareProperty(new WorkspaceProperty<>("OutputWorkspace","",Direction::Output), "An output workspace.");
-  }
-
-  //----------------------------------------------------------------------------------------------
-  /** Execute the algorithm.
-   */
-  void Segfault::exec()
-  {
-    // TODO Auto-generated execute stub
-  }
-
-
+}
 
 } // namespace Algorithms
 } // namespace Mantid
--- Code/Mantid/docs/source/algorithms/Segfault-v1.rst
+++ Code/Mantid/docs/source/algorithms/Segfault-v1.rst
@@ -10,35 +10,6 @@
 Description
 -----------
 
-TODO: Enter a full rst-markup description of your algorithm here.
-
-
-Usage
------
-..  Try not to use files in your examples,
-    but if you cannot avoid it then the (small) files must be added to
-    autotestdata\UsageData and the following tag unindented
-    .. include:: ../usagedata-note.txt
-
-**Example - Segfault**
-
-.. testcode:: SegfaultExample
-
-   # Create a host workspace
-   ws = CreateWorkspace(DataX=range(0,3), DataY=(0,2))
-   or
-   ws = CreateSampleWorkspace()
-
-   wsOut = Segfault()
-
-   # Print the result
-   print "The output workspace has %i spectra" % wsOut.getNumberHistograms()
-
-Output:
-
-.. testoutput:: SegfaultExample
-
-  The output workspace has ?? spectra
-
-.. categories::
-
+The purpose of this algorithm is to crash mantid. Do **not** run it
+unless you want that to happen. This runs a variation the example code
+on `wikipedia <https://en.wikipedia.org/wiki/Segmentation_fault>`_.
