bbf4dcb2a67640385093582df0806cad36509053
Wenduo Zhou <zhouw@ornl.gov>
2015-03-06 11:49:09 -0500

    Added more to unit tests. Refs #11098.

--- Code/Mantid/Framework/DataHandling/test/LoadSpiceXML2DDetTest.h
+++ Code/Mantid/Framework/DataHandling/test/LoadSpiceXML2DDetTest.h
@@ -4,9 +4,11 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidDataHandling/LoadSpiceXML2DDet.h"
+#include "MantidAPI/MatrixWorkspace.h"
 
-using Mantid::DataHandling::LoadSpiceXML2DDet;
+using namespace Mantid;
 using namespace Mantid::API;
+using Mantid::DataHandling::LoadSpiceXML2DDet;
 
 class LoadSpiceXML2DDetTest : public CxxTest::TestSuite {
 public:
@@ -38,6 +40,57 @@ public:
 
     loader.execute();
     TS_ASSERT(loader.isExecuted());
+
+    // Get data
+    MatrixWorkspace_sptr outws = boost::dynamic_pointer_cast<MatrixWorkspace>(
+        AnalysisDataService::Instance().retrieve("Exp0335_S0038"));
+    TS_ASSERT(outws);
+
+    size_t numspec = outws->getNumberHistograms();
+    TS_ASSERT_EQUALS(numspec, 256);
+    size_t numx = outws->readX(0).size();
+    size_t numy = outws->readY(0).size();
+    TS_ASSERT_EQUALS(numx, 256);
+    TS_ASSERT_EQUALS(numy, 256);
+
+    // Check the value
+    double totalcounts = 0;
+    for (size_t i = 0; i < numspec; ++i) {
+      const MantidVec &vecY = outws->readY(i);
+      size_t numy = vecY.size();
+      for (size_t j = 0; j < numy; ++j)
+        totalcounts += vecY[j];
+    }
+    TS_ASSERT_DELTA(totalcounts, 8049.00, 0.000001);
+
+    // check max count
+    TS_ASSERT_DELTA(outws->readY(135)[120], 4., 0.000001);
+
+    // Check the sample logs
+    TS_ASSERT(outws->run().hasProperty("_monitor"));
+    int monitorcount =
+        atoi(outws->run().getProperty("_monitor")->value().c_str());
+    TS_ASSERT_EQUALS(monitorcount, 29);
+
+    // Check motor angles
+    TS_ASSERT(outws->run().hasProperty("_2theta"));
+    double _2theta = atof(outws->run().getProperty("_2theta")->value().c_str());
+    TS_ASSERT_DELTA(_2theta, 42.709750, 0.0000001);
+
+    TS_ASSERT(outws->run().hasProperty("_omega"));
+    double _omega = atof(outws->run().getProperty("_omega")->value().c_str());
+    TS_ASSERT_DELTA(_omega, 21.354500, 0.0000001);
+
+    TS_ASSERT(outws->run().hasProperty("_chi"));
+    double _chi = atof(outws->run().getProperty("_chi")->value().c_str());
+    TS_ASSERT_DELTA(_chi, 1.215250, 0.0000001);
+
+    TS_ASSERT(outws->run().hasProperty("_phi"));
+    double _phi = atof(outws->run().getProperty("_phi")->value().c_str());
+    TS_ASSERT_DELTA(_phi, 144.714218, 0.0000001);
+
+    // Clean
+    AnalysisDataService::Instance().remove("xp0335_S0038");
   }
 };
 
