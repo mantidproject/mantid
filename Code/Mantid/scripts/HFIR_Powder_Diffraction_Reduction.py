0a318508ba4793385237a91a90a110503fb4fcee
Wenduo Zhou <zhouw@ornl.gov>
2015-03-09 13:26:27 -0400

    Refs #11289.  Added scripts and qt design file to work.

---
+++ Code/Mantid/scripts/HFIR_Powder_Diffraction_Reduction.py
@@ -0,0 +1,20 @@
+#pylint: disable=invalid-name
+"""
+    Script used to start the DGS reduction GUI from MantidPlot
+"""
+from HFIRPowderReduction import HfirPDReductionGUI
+from PyQt4 import QtGui
+
+def qapp():
+    if QtGui.QApplication.instance():
+        _app = QtGui.QApplication.instance()
+    else:
+        _app = QtGui.QApplication(sys.argv)
+    return _app
+
+app = qapp()
+
+reducer = HfirPDReductionGUI.MainWindow() #the main ui class in this file is called MainWindow
+reducer.show()
+
+app.exec_()
