3a54017b7c69e8dd11c7edcfe9a78187a89a2a6d
Wenduo Zhou <zhouw@ornl.gov>
2015-03-11 22:44:17 -0400

    Added an out-of-mantid test script. Refs #11289.

---
+++ Code/Mantid/scripts/HFIRPowderReduction/testGUI.py
@@ -0,0 +1,31 @@
+####################
+# Tester 
+####################
+
+""" Test main """
+import sys
+
+import HfirPDReductionGUI
+from PyQt4 import QtGui
+
+def qapp():
+    if QtGui.QApplication.instance():
+        _app = QtGui.QApplication.instance()
+    else:
+        _app = QtGui.QApplication(sys.argv)
+    return _app
+
+app = qapp()
+
+reducer = HfirPDReductionGUI.MainWindow() #the main ui class in this file is called MainWindow
+reducer.show()
+
+# example: 'http://neutron.ornl.gov/user_data/hb2a/exp400/Datafiles/HB2A_exp0400_scan0001.dat'
+print "Set to exp 400, scan 1"
+reducer.ui.lineEdit_expNo.setText('400')
+reducer.ui.lineEdit_scanNo.setText('1')
+reducer.ui.lineEdit_cache.setText('/Users/wzz/Temp/')
+reducer.doLoadData()
+
+app.exec_()
+
