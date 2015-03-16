0da4a984e4849ca38c2d176d1473cbfa08949342
Dan Nixon <dan@dan-nixon.com>
2015-03-16 11:34:03 +0000

    Further refactoring of algorithm
    Refs #10753

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/ApplyPaalmanPingsCorrection.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/ApplyPaalmanPingsCorrection.py
@@ -1,136 +1,214 @@
 from mantid.simpleapi import *
 from mantid.api import PythonAlgorithm, AlgorithmFactory, MatrixWorkspaceProperty, WorkspaceGroupProperty, \
-                       FileProperty, FileAction, PropertyMode
-from mantid.kernel import StringListValidator, StringMandatoryValidator, Direction, logger
-from mantid import config
-import math, os.path, numpy as np
+                       PropertyMode
+from mantid.kernel import Direction, logger
+
 
 class ApplyPaalmanPingsCorrection(PythonAlgorithm):
 
     _sample_ws_name = None
     _corrections_ws_name = None
-    _usecan = False
+    _use_can = False
     _can_ws_name = None
-    _can_corrections = False
+    _use_corrections = False
+    _can_scale_factor = 1.0
     _scale_can = False
-    _scale_factor = 1.0
     _output_ws_name = None
+    _corrections = None
+    _scaled_container = None
+
 
     def category(self):
         return "Workflow\\MIDAS;PythonAlgorithms"
 
+
+    def summary(self):
+        return "Applies a calculated absorption correction in the Paalman and Pings factor style."
+
+
     def PyInit(self):
-        self.declareProperty(MatrixWorkspaceProperty('SampleWorkspace', '', direction=Direction.Input),
-                             doc="Name for the input Sample workspace.")
-        self.declareProperty(WorkspaceGroupProperty('CorrectionsWorkspace', '', optional=PropertyMode.Optional,
-                             direction=Direction.Input),doc="Name for the input Corrections workspace.")
+        self.declareProperty(MatrixWorkspaceProperty('SampleWorkspace', '',
+                             direction=Direction.Input),
+                             doc='Name for the input Sample workspace.')
 
-        self.declareProperty(MatrixWorkspaceProperty('CanWorkspace', '', optional=PropertyMode.Optional,
-                             direction=Direction.Input),doc="Name for the input Can workspace.")
-        self.declareProperty(name='CanScaleFactor', defaultValue='', doc = 'Factor to scale the can data')
+        self.declareProperty(WorkspaceGroupProperty('CorrectionsWorkspace', '',
+                             optional=PropertyMode.Optional, direction=Direction.Input),
+                             doc='Name for the input Corrections workspace.')
 
-        self.declareProperty(MatrixWorkspaceProperty('OutputWorkspace', '', direction=Direction.Output),
+        self.declareProperty(MatrixWorkspaceProperty('CanWorkspace', '',
+                             optional=PropertyMode.Optional, direction=Direction.Input),
+                             doc='Name for the input Can workspace.')
+
+        self.declareProperty(name='CanScaleFactor', defaultValue=1.0,
+                             doc='Factor to scale the can data')
+
+        self.declareProperty(MatrixWorkspaceProperty('OutputWorkspace', '',
+                             direction=Direction.Output),
                              doc='The output corrections workspace.')
 
+
     def PyExec(self):
-
-        workdir = config['defaultsave.directory']
         self._setup()
+
+        if not self._use_corrections:
+            logger.information('Not using corrections')
+        if not self._use_can:
+            logger.information('Not using container')
+
         instrument = mtd[self._sample_ws_name].getInstrument()
-        axis = mtd[self._sample_ws_name].getAxis(0)
-        unit = axis.getUnit()
-        logger.information('x-unit is ' + unit.unitID())
-#       this temporary WS is needed because ConvertUnits does not like named WS in a Group
-        self._corrections = '__converted_corrections'
+        unit_id = mtd[self._sample_ws_name].getAxis(0).getUnit().unitID()
+        logger.information('x-unit is ' + unit_id)
 
-        if self._usecan:
-            if unit.unitID() != 'Wavelength':
-                if unit.unitID() == 'dSpacing':
-                    self._emode = 'Elastic'
-                    target = 'dSpacing'
-                    self._efixed = 0.0
-                elif unit.unitID() == 'DeltaE':
-                    self._emode = 'Indirect'
-                    target = 'DeltaE'
-                    self._efixed = instrument.getNumberParameter("efixed-val")[0]
+        # Apply container scale factor if needed
+        if self._use_can:
+            if self._scale_can:
+                # Use temp workspace so we don't modify original data
+                Scale(InputWorkspace=self._can_ws_name,
+                      OutputWorkspace=self._scaled_container,
+                      Factor=self._can_scale_factor,
+                      Operation='Multiply')
+                logger.information('Container scaled by %f' % self._can_scale_factor)
+            else:
+                CloneWorkspace(InputWorkspace=self._can_ws_name,
+                               OutputWorkspace=self._scaled_container)
+
+        if self._use_corrections:
+            # If the sample is not in wavelength then convert the corrections to
+            # whatever units the sample is in
+            if unit_id != 'Wavelength':
+                target = unit_id
+                if unit_id == 'dSpacing':
+                    emode = 'Elastic'
+                    efixed = 0.0
+                elif unit_id == 'DeltaE':
+                    emode = 'Indirect'
+                    efixed = instrument.getNumberParameter('efixed-val')[0]
                 else:
-                    raise ValueError('Unit ' + unit.unitID() + ' is not supported')
-                ConvertUnits(InputWorkspace=self._corrections_ws_name, OutputWorkspace=self._corrections,
-			                 Target=target, EMode=self._emode, EFixed=self._efixed)
-            else:
-                CloneWorkspace(InputWorkspace=self._corrections_ws_name, OutputWorkspace=self._corrections)
+                    raise ValueError('Unit %s in sample workspace is not supported' % unit_id)
 
-            self._scaled_container = "__scaled_container"
-            if self.scale_can:
-            # Use temp workspace so we don't modify original data
-                Scale(InputWorkspace=self._can_ws_name, OutputWorkspace=self._scaled_container,
-                      Factor=factor, Operation='Multiply')
-                logger.information('Container scaled by %f' % factor)
-
+                ConvertUnits(InputWorkspace=self._corrections_ws_name,
+                             OutputWorkspace=self._corrections,
+                             Target=target,
+                             EMode=emode,
+                             EFixed=efixed)
             else:
-                CloneWorkspace(InputWorkspace=self._can_ws_name, OutputWorkspace=self._scaled_container)
+                CloneWorkspace(InputWorkspace=self._corrections_ws_name,
+                               OutputWorkspace=self._corrections)
 
-            if self._can_corrections:
-                self._correctSampleCan()
+            if self._use_can:
+                # Use container factors
+                self._correct_sample_can()
             else:
-                self._subtract()
+                # Use sample factor only
+                self._correct_sample()
 
         else:
-            self._correctSample()
+            # Do simple subtraction
+            self._subtract()
+
         self.setPropertyValue('OutputWorkspace', self._output_ws_name)
 
+
+    def validateInputs(self):
+        """
+        Validate user input.
+        """
+
+        self._setup()
+        issues = dict()
+
+        # Need something to get corrections from
+        if not (self._use_can or self._use_corrections):
+            error_msg = 'Must provide either CorrectionsWorkspace or CanWorkspace or both'
+            issues['CorrectionsWorkspace'] = error_msg
+            issues['CanWorkspace'] = error_msg
+
+        sample_unit_id = mtd[self._sample_ws_name].getAxis(0).getUnit().unitID()
+
+        # Check sample and container X axis units match
+        if self._use_can:
+            can_unit_id = mtd[self._can_ws_name].getAxis(0).getUnit().unitID()
+            if can_unit_id != sample_unit_id:
+                issues['CanWorkspace'] = 'X axis unit must match SampleWorkspace'
+
+        return issues
+
+
     def _setup(self):
+        """
+        Get properties and setup instance variables.
+        """
+
         self._sample_ws_name = self.getPropertyValue('SampleWorkspace')
-        logger.information('Sample is ' + self._sample_ws_name)
-        self._corrections_ws_name = self.getPropertyValue('CorrectionsWorkspace')
-        if self._corrections_ws_name == '':
-            self._can_corrections = False
-            logger.information('NO corrections')
-        else:
-            self._can_corrections = True
-            logger.information('Corrections is ' + self._corrections_ws_name)
-        self._can_ws_name = self.getPropertyValue('CanWorkspace')
-        if self._can_ws_name == '':
-            self._usecan = False
-            logger.information('NO can')
-        else:
-            self._usecan = True
-            logger.information('Can is ' + self._can_ws_name)
-        if self._usecan:
-            scale_factor = float(self.getProperty('CanScaleFactor').value)
-            if scale_factor == 1.0:
-                self.scale_can = False
-            else:
-                self.scale_can = True
-
-        if self._usecan == False and self._can_corrections == False:
-            raise ValueError('Nothing to do!')
-
         self._output_ws_name = self.getPropertyValue('OutputWorkspace')
 
+        # Get corrections workspace
+        self._corrections_ws_name = self.getPropertyValue('CorrectionsWorkspace')
+        self._use_corrections = self._corrections_ws_name != ''
+
+        # Get container workspace
+        self._can_ws_name = self.getPropertyValue('CanWorkspace')
+        self._use_can = self._can_ws_name != ''
+
+        self._can_scale_factor = self.getProperty('CanScaleFactor').value
+        self._scale_can = self._can_scale_factor != 1.0
+
+        # This temporary WS is needed because ConvertUnits does not like named WS in a Group
+        self._corrections = '__converted_corrections'
+        self._scaled_container = '__scaled_container'
+
+
     def _subtract(self):
-        Minus(LHSWorkspace=self._sample_ws_name, RHSWorkspace=self._scaled_container,
+        """
+        Do a simple container subtraction (when no corrections are given).
+        """
+
+        logger.information('Using simple container subtraction')
+
+        Minus(LHSWorkspace=self._sample_ws_name,
+              RHSWorkspace=self._scaled_container,
               OutputWorkspace=self._output_ws_name)
 
-    def _correctSample(self):
-#       Ass is group 1
-        Divide(LHSWorkspace=self._sample_ws_name, RHSWorkspace=self._corrections+'_1',
+
+    def _correct_sample(self):
+        """
+        Correct for sample only (when no container is given).
+        """
+
+        logger.information('Correcting sample')
+
+        # Ass
+        Divide(LHSWorkspace=self._sample_ws_name,
+               RHSWorkspace=self._corrections + '_ass',
+               OutputWorkspace=self._output_ws_name)
+
+
+    def _correct_sample_can(self):
+        """
+        Correct for sample and container.
+        """
+
+        logger.information('Correcting sample and container')
+        corrected_can_ws = '__corrected_can'
+
+        # Acc
+        Divide(LHSWorkspace=self._scaled_container,
+               RHSWorkspace=self._corrections + '_acc',
+               OutputWorkspace=corrected_can_ws)
+
+        # Acsc
+        Multiply(LHSWorkspace=corrected_can_ws,
+                 RHSWorkspace=self._corrections + '_acsc',
+                 OutputWorkspace=corrected_can_ws)
+        Minus(LHSWorkspace=self._sample_ws_name,
+              RHSWorkspace=corrected_can_ws,
               OutputWorkspace=self._output_ws_name)
 
-    def _correctSampleCan(self):
-        CorrectedCanWS = '__corrected_can'
-#       Acc	is group 4
-        Divide(LHSWorkspace=self._scaled_container, RHSWorkspace=self._corrections+'_4',
-              OutputWorkspace=CorrectedCanWS)
-#       Acsc is group 3
-        Multiply(LHSWorkspace=CorrectedCanWS, RHSWorkspace=self._corrections+'_3',
-              OutputWorkspace=CorrectedCanWS)
-        Minus(LHSWorkspace=self._sample_ws_name, RHSWorkspace=CorrectedCanWS,
-              OutputWorkspace=self._output_ws_name)
-#       Assc is group 2
-        Divide(LHSWorkspace=self._output_ws_name, RHSWorkspace=self._corrections+'_2',
-              OutputWorkspace=self._output_ws_name)
+        # Assc
+        Divide(LHSWorkspace=self._output_ws_name,
+               RHSWorkspace=self._corrections + '_assc',
+               OutputWorkspace=self._output_ws_name)
+
 
 # Register algorithm with Mantid
 AlgorithmFactory.subscribe(ApplyPaalmanPingsCorrection)
-#
