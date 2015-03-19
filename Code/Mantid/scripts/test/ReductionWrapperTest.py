14af060915f4b4ee276fd60c3ae6dee8fcc79e09
Alex Buts <Alex.Buts@stfc.ac.uk>
2015-03-19 21:30:28 +0000

    Re #11397 Unit test for custom file name

--- Code/Mantid/scripts/test/ReductionWrapperTest.py
+++ Code/Mantid/scripts/test/ReductionWrapperTest.py
@@ -1,16 +1,50 @@
 import os,sys
-#os.environ["PATH"] = r"c:/Mantid/Code/builds/br_master/bin/Release;"+os.environ["PATH"]
+os.environ["PATH"] = r"c:/Mantid/Code/builds/br_master/bin/Release;"+os.environ["PATH"]
 
 from mantid.simpleapi import *
 from mantid import api,config
 
-from Direct.ReductionWrapper import ReductionWrapper
+from Direct.ReductionWrapper import *
 import MariReduction as mr
 
 
 #
 import unittest
 
+class test_helper(ReductionWrapper):
+    def __init__(self,web_var=None):
+       """ sets properties defaults for the instrument with Name"""
+       ReductionWrapper.__init__(self,'MAR',web_var)
+
+    def set_custom_output_filename(self):
+        """define custom name of output files if standard one is not satisfactory
+          In addition to that, example of accessing reduction properties
+          Changing them if necessary
+        """
+        def custom_name(prop_man):
+            """ sample function which builds filename from
+              incident energy and run number and adds some auxiliary information
+              to it.
+            """
+            
+            # Note -- properties have the same names  as the list of advanced and
+            # main properties
+            ei = PropertyManager.incident_energy.get_current()
+            # sample run is more then just list of runs, so we use
+            # the formalization below to access its methods
+            run_num = prop_man.sample_run
+            name = "SOMETHING{0}_{1:<3.2f}meV_rings".format(run_num ,ei)
+            return name
+
+        # Uncomment this to use custom filename function
+        # Note: the properties are stored in prop_man class accessed as
+        # below.
+        return custom_name(self.reducer.prop_man)
+        # use this method to use standard file name generating function
+        #return None
+    @iliad
+    def reduce(self, input_file = None, output_directory = None):
+        return ''
 #-----------------------------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------------------------
@@ -34,8 +68,6 @@ class ReductionWrapperTest(unittest.TestCase):
         self.assertRaises(NotImplementedError,red.def_advanced_properties)
         self.assertTrue('reduce' in dir(red))
 
-
- 
     def test_export_advanced_values(self):
         red = mr.ReduceMARI()
 
@@ -125,6 +157,16 @@ class ReductionWrapperTest(unittest.TestCase):
         self.assertFalse(ok)
         self.assertEqual(level,1)
         self.assertEqual(len(errors),1)
+
+    def test_custom_print_name(self):
+        th=test_helper()
+        th.reducer.prop_man.sample_run = 100
+        th.reducer.prop_man.incident_energy=[10.01,20]
+
+        th.reduce()
+
+        save_file = th.reducer.prop_man.save_file_name
+        self.assertEqual(save_file,'SOMETHING100_10.01meV_rings')
 
 
 
