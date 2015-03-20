099fb4a9feef267384e5166aa0979960935fad37
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2015-03-20 11:36:26 +0000

    Refs #11355 Add C++ CutMD stub

---
+++ Code/Mantid/Framework/MDAlgorithms/src/CutMD.cpp
@@ -0,0 +1,26 @@
+#include "MantidMDAlgorithms/CutMD.h"
+#include "MantidKernel/System.h"
+
+namespace Mantid {
+namespace MDAlgorithms {
+
+// Register the algorithm into the AlgorithmFactory
+DECLARE_ALGORITHM(CutMD)
+
+//----------------------------------------------------------------------------------------------
+/** Constructor
+ */
+CutMD::CutMD() {}
+
+//----------------------------------------------------------------------------------------------
+/** Destructor
+ */
+CutMD::~CutMD() {}
+
+//----------------------------------------------------------------------------------------------
+
+void CutMD::init() {}
+void CutMD::exec() {}
+
+} // namespace Mantid
+} // namespace MDAlgorithms
