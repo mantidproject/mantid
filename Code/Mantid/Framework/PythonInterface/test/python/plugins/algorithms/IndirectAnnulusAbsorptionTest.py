2cff93e9e257b2079d5af9c8daace095413bbf24
Dan Nixon <dan@dan-nixon.com>
2015-03-24 09:02:14 +0000

    Update annulus unit test for algorithm changes
    Refs #11413

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/IndirectAnnulusAbsorptionTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/IndirectAnnulusAbsorptionTest.py
@@ -17,22 +17,25 @@ class IndirectAnnulusAbsorptionTest(unittest.TestCase):
         self._red_ws = red_ws
 
 
-    def _test_workspaces(self, corrected, ass):
+    def _test_workspaces(self, corrected, factor_group):
         """
         Checks the units of the Ass and corrected workspaces.
 
         @param corrected Corrected workspace
-        @param ass Assc corrections workspace
+        @param factor_group WorkspaceGroup containing factors
         """
 
+        # Test units of corrected workspace
         corrected_x_unit = corrected.getAxis(0).getUnit().unitID()
         self.assertEqual(corrected_x_unit, 'DeltaE')
 
-        ass_x_unit = ass.getAxis(0).getUnit().unitID()
-        self.assertEquals(ass_x_unit, 'Wavelength')
+        # Test units of factor workspaces
+        for ws in factor_group:
+            x_unit = ws.getAxis(0).getUnit().unitID()
+            self.assertEquals(x_unit, 'Wavelength')
 
-        ass_y_unit = ass.YUnitLabel()
-        self.assertEqual(ass_y_unit, 'Attenuation factor')
+            y_unit = ws.YUnitLabel()
+            self.assertEqual(y_unit, 'Attenuation factor')
 
 
     def test_sample_corrections_only(self):
@@ -40,15 +43,13 @@ class IndirectAnnulusAbsorptionTest(unittest.TestCase):
         Tests corrections for the sample only.
         """
 
-        corrected, ass = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
-                                                   ChemicalFormula='H2-O',
-                                                   CanInnerRadius=0.2,
-                                                   SampleInnerRadius=0.15,
-                                                   SampleOuterRadius=0.16,
-                                                   CanOuterRadius=0.22,
-                                                   Events=200)
+        corrected, fact = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
+                                                    SampleChemicalFormula='H2-O',
+                                                    Events=200,
+                                                    UseCanCorrections=False)
 
-        self._test_workspaces(corrected, ass)
+        self.assertEqual(fact.size(), 1)
+        self._test_workspaces(corrected, fact)
 
 
     def test_sample_and_can_subtraction(self):
@@ -56,16 +57,14 @@ class IndirectAnnulusAbsorptionTest(unittest.TestCase):
         Tests corrections for the sample and simple container subtraction.
         """
 
-        corrected, ass = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
-                                                   CanWorkspace=self._can_ws,
-                                                   ChemicalFormula='H2-O',
-                                                   CanInnerRadius=0.2,
-                                                   SampleInnerRadius=0.15,
-                                                   SampleOuterRadius=0.16,
-                                                   CanOuterRadius=0.22,
-                                                   Events=200)
+        corrected, fact = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
+                                                    SampleChemicalFormula='H2-O',
+                                                    CanWorkspace=self._can_ws,
+                                                    Events=200,
+                                                    UseCanCorrections=False)
 
-        self._test_workspaces(corrected, ass)
+        self.assertEqual(fact.size(), 1)
+        self._test_workspaces(corrected, fact)
 
 
     def test_sample_and_can_subtraction_with_scale(self):
@@ -74,17 +73,32 @@ class IndirectAnnulusAbsorptionTest(unittest.TestCase):
         with can scale.
         """
 
-        corrected, ass = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
-                                                   CanWorkspace=self._can_ws,
-                                                   CanScaleFactor=0.8,
-                                                   ChemicalFormula='H2-O',
-                                                   CanInnerRadius=0.2,
-                                                   SampleInnerRadius=0.15,
-                                                   SampleOuterRadius=0.16,
-                                                   CanOuterRadius=0.22,
-                                                   Events=200)
+        corrected, fact = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
+                                                    SampleChemicalFormula='H2-O',
+                                                    CanWorkspace=self._can_ws,
+                                                    CanScaleFactor=0.8,
+                                                    Events=200,
+                                                    UseCanCorrections=False)
 
-        self._test_workspaces(corrected, ass)
+        self.assertEqual(fact.size(), 1)
+        self._test_workspaces(corrected, fact)
+
+
+    def test_sample_and_can_corrections(self):
+        """
+        Tests corrections for the sample and container.
+        """
+
+        corrected, fact = IndirectAnnulusAbsorption(SampleWorkspace=self._red_ws,
+                                                    SampleChemicalFormula='H2-O',
+                                                    CanWorkspace=self._can_ws,
+                                                    CanChemicalFormula='V',
+                                                    CanScaleFactor=0.8,
+                                                    Events=200,
+                                                    UseCanCorrections=True)
+
+        self.assertEqual(fact.size(), 2)
+        self._test_workspaces(corrected, fact)
 
 
 if __name__ == '__main__':
