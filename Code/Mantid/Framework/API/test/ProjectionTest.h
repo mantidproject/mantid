7ed94f66c657657e19f7e6d08e96f05742aee469
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2015-04-01 10:29:33 +0100

    Refs #11455 Unit test Projection from table constructor

--- Code/Mantid/Framework/API/test/ProjectionTest.h
+++ Code/Mantid/Framework/API/test/ProjectionTest.h
@@ -4,9 +4,13 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidAPI/Projection.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidDataObjects/TableWorkspace.h"
 
 using namespace Mantid;
 using namespace Mantid::API;
+using namespace Mantid::DataObjects;
 
 class ProjectionTest : public CxxTest::TestSuite {
 public:
@@ -30,6 +34,69 @@ public:
     TS_ASSERT_EQUALS(p.U(), u);
     TS_ASSERT_EQUALS(p.V(), v);
     TS_ASSERT_EQUALS(p.W(), w);
+  }
+
+  void test_construct_null_workspace() {
+    TS_ASSERT_THROWS(new Projection(ITableWorkspace_sptr()), std::runtime_error)
+  }
+
+  void test_construct_bad_workspace_columns() {
+    auto proj = ITableWorkspace_sptr(new TableWorkspace());
+    TS_ASSERT_THROWS(new Projection(proj), std::runtime_error)
+  }
+
+  void test_construct_bad_workspace_no_rows() {
+    auto proj = ITableWorkspace_sptr(new TableWorkspace());
+    proj->addColumn("str", "name");
+    proj->addColumn("str", "value");
+    proj->addColumn("double", "offset");
+    proj->addColumn("str", "type");
+
+    TS_ASSERT_THROWS(new Projection(proj), std::runtime_error)
+  }
+
+  void test_construct_bad_workspace_too_many_rows() {
+    auto proj = ITableWorkspace_sptr(new TableWorkspace());
+    proj->addColumn("str", "name");
+    proj->addColumn("str", "value");
+    proj->addColumn("double", "offset");
+    proj->addColumn("str", "type");
+
+    proj->appendRow();
+    proj->appendRow();
+    proj->appendRow();
+    proj->appendRow();
+
+    TS_ASSERT_THROWS(new Projection(proj), std::runtime_error)
+  }
+
+  void test_construct_good_workspace() {
+    auto proj = ITableWorkspace_sptr(new TableWorkspace());
+    proj->addColumn("str", "name");
+    proj->addColumn("str", "value");
+    proj->addColumn("double", "offset");
+    proj->addColumn("str", "type");
+
+    TableRow u = proj->appendRow();
+    TableRow v = proj->appendRow();
+    TableRow w = proj->appendRow();
+
+    u << "u" << "1,0,0" << 0.5 << "r";
+    v << "v" << "0,1,0" << 1.25 << "a";
+    w << "w" << "0,0,1" << -10.0 << "r";
+
+    boost::shared_ptr<Projection> p;
+    TS_ASSERT_THROWS_NOTHING(p.reset(new Projection(proj)))
+
+    TS_ASSERT_EQUALS(p->U(), V3D(1, 0, 0));
+    TS_ASSERT_EQUALS(p->V(), V3D(0, 1, 0));
+    TS_ASSERT_EQUALS(p->W(), V3D(0, 0, 1));
+    TS_ASSERT_EQUALS(p->getOffset(0), 0.5);
+    TS_ASSERT_EQUALS(p->getOffset(1), 1.25);
+    TS_ASSERT_EQUALS(p->getOffset(2), -10.0);
+    TS_ASSERT_EQUALS(p->getUnit(0), RLU);
+    TS_ASSERT_EQUALS(p->getUnit(1), INV_ANG);
+    TS_ASSERT_EQUALS(p->getUnit(2), RLU);
   }
 
   void test_throw_out_of_range_access() {
