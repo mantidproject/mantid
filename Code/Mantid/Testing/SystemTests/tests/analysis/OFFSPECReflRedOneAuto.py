5f5166c0ad4dfd735d19e24b73335de712cb9aa9
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2015-04-07 13:28:45 +0100

    Refs #10847 Systemtest ReflRedOneAuto with OFFSPEC

---
+++ Code/Mantid/Testing/SystemTests/tests/analysis/OFFSPECReflRedOneAuto.py
@@ -0,0 +1,45 @@
+#pylint: disable=no-init,invalid-name
+"""
+This system test verifies that OFFSPEC data is processed correctly by
+ReflectometryReductionOneAuto
+"""
+
+import stresstesting
+from mantid.simpleapi import *
+
+class OFFSPECReflRedOneAuto(stresstesting.MantidStressTest):
+    def runTest(self):
+        offspec75 = Load("OFFSPEC00027575.nxs") #th=0.35
+        offspec76 = Load("OFFSPEC00027576.nxs") #th=1.00
+        offspec78 = Load("OFFSPEC00027578.nxs") #th=1.70
+        offspec85 = Load("OFFSPEC00027585.nxs") #transmission run
+
+        #Process using ReflectometryReductionOneAuto
+        ivq_75, __, __ = ReflectometryReductionOneAuto(
+                offspec75,
+                ThetaIn=0.35,
+                FirstTransmissionRun=offspec85)
+
+        ivq_76, __, __ = ReflectometryReductionOneAuto(
+                offspec76,
+                ThetaIn=1.00,
+                FirstTransmissionRun=offspec85)
+
+        ivq_78, __, __ = ReflectometryReductionOneAuto(
+                offspec78,
+                ThetaIn=1.70,
+                FirstTransmissionRun=offspec85)
+
+        ivq_75_76, __ = Stitch1D(ivq_75, ivq_76, Params="1e-3")
+        ivq_75_76_78, __ = Stitch1D(ivq_75_76, ivq_78, Params="0,1e-3,0.08")
+        return True
+
+    def validate(self):
+        return ("ivq_75_76_78","OFFSPECReflRedOneAuto_good.nxs")
+
+    def requiredFiles(self):
+        return ["OFFSPEC00027575.nxs",
+                "OFFSPEC00027576.nxs",
+                "OFFSPEC00027578.nxs",
+                "OFFSPEC00027585.nxs",
+                "OFFSPECReflRedOneAuto_good.nxs"]
