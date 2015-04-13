aaa35d9cf080a69bc202d2a8333587545934b86a
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2015-04-13 11:17:48 +0100

    Revert "Refs #11515 Fix dead code in RemovePromptPulse"
    This reverts commit ca2906a1239e4e9a15f93fcc87b8e4d31bbb7457.

--- Code/Mantid/Framework/Algorithms/src/RemovePromptPulse.cpp
+++ Code/Mantid/Framework/Algorithms/src/RemovePromptPulse.cpp
@@ -71,7 +71,9 @@ double getMedian(const API::Run &run, const std::string &name) {
 void getTofRange(MatrixWorkspace_const_sptr wksp, double &tmin, double &tmax) {
   DataObjects::EventWorkspace_const_sptr eventWksp =
       boost::dynamic_pointer_cast<const DataObjects::EventWorkspace>(wksp);
-  if (eventWksp) {
+
+  bool isEvent = false;
+  if (isEvent) {
     eventWksp->getEventXMinMax(tmin, tmax);
   } else {
     wksp->getXMinMax(tmin, tmax);
