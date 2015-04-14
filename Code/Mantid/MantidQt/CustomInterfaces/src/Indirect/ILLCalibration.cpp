c704e0283a2ea9e7f7f0e7fcfe67158ed82fdb65
Dan Nixon <dan@dan-nixon.com>
2015-04-14 11:00:22 +0100

    Added algorithm to interface
    Refs #11163

--- Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ILLCalibration.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ILLCalibration.cpp
@@ -1,5 +1,7 @@
 #include "MantidQtCustomInterfaces/Indirect/ILLCalibration.h"
 
+#include <QFileInfo>
+
 using namespace Mantid::API;
 using MantidQt::API::BatchAlgorithmRunner;
 
@@ -25,33 +27,96 @@ namespace CustomInterfaces
   {
   }
 
+
   void ILLCalibration::setup()
   {
   }
 
+
   void ILLCalibration::run()
   {
-    //TODO
+    std::map<QString, QString> instDetails = getInstrumentDetails();
+
+    IAlgorithm_sptr calibrationAlg = AlgorithmManager::Instance().create("ILLIN16BCalibration");
+    calibrationAlg->initialize();
+
+    // Handle einput files
+    QString runFilename = m_uiForm.rfRunFile->getFirstFilename();
+    calibrationAlg->setProperty("Run", runFilename.toStdString());
+
+    // Set options
+    bool mirrorMode = m_uiForm.ckMirrorMode->isChecked();
+    calibrationAlg->setProperty("MirrorMode", mirrorMode);
+
+    std::vector<long> specRange;
+    specRange.push_back(m_uiForm.spSpecMin->value());
+    specRange.push_back(m_uiForm.spSpecMax->value());
+    calibrationAlg->setProperty("SpectraRange", specRange);
+
+    std::vector<double> peakRange;
+    peakRange.push_back(m_uiForm.spPeakLower->value());
+    peakRange.push_back(m_uiForm.spPeakUpper->value());
+    calibrationAlg->setProperty("PeakRange", peakRange);
+
+    double scaleFactor = m_uiForm.spScaleFactor->value();
+    calibrationAlg->setProperty("ScaleFactor", scaleFactor);
+
+    // Get the name format for output files
+    QFileInfo runFileInfo(runFilename);
+    QString outputWsName = runFileInfo.baseName() +
+                           "_" + instDetails["analyser"] +
+                           "_" + instDetails["reflection"] +
+                           "_calib";
+
+    // Set output workspace properties
+    calibrationAlg->setProperty("OutputWorkspace", outputWsName.toStdString());
+
+    m_batchAlgoRunner->addAlgorithm(calibrationAlg);
+
+    bool save = m_uiForm.ckSave->isChecked();
+    if(save)
+    {
+      //TODO: saving
+    }
 
     m_batchAlgoRunner->executeBatchAsync();
+    m_pythonExportWsName = outputWsName.toStdString();
   }
+
 
   void ILLCalibration::algorithmComplete(bool error)
   {
     if(error)
       return;
 
-    //TODO
+    // Handle plotting
+    bool plot = m_uiForm.ckPlot->isChecked();
+    if(plot)
+      plotTimeBin(QString::fromStdString(m_pythonExportWsName), 0);
   }
+
 
   bool ILLCalibration::validate()
   {
     MantidQt::CustomInterfaces::UserInputValidator uiv;
 
-    //TODO
-    return false;
+    // Validate spectra range
+    auto specRange = std::make_pair<double, double>(m_uiForm.spSpecMin->value(), m_uiForm.spSpecMax->value());
+    uiv.checkValidRange("Spectra Range", specRange);
 
-    /* return uiv.isAllInputValid(); */
+    // Validate peak range
+    auto peakRange = std::make_pair<double, double>(m_uiForm.spPeakLower->value(), m_uiForm.spPeakUpper->value());
+    uiv.checkValidRange("Peak Range", peakRange);
+
+    // Validate run file
+    if(!m_uiForm.rfRunFile->isValid())
+      uiv.addErrorMessage("Run File is invalid.");
+
+    // Show error for failed validation
+    if(!uiv.isAllInputValid())
+      emit showMessageBox(uiv.generateErrorMessage());
+
+    return uiv.isAllInputValid();
   }
 
 } // namespace CustomInterfaces
