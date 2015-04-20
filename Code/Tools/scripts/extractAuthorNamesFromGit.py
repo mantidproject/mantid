c406027c2794378c73c7c75fca0482d0c9ca344e
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2015-04-20 13:31:34 +0100

    stu pointed out that I can't spell to author:-) re #9811

--- Code/Tools/scripts/extractAuthorNamesFromGit.py
+++ Code/Tools/scripts/extractAuthorNamesFromGit.py
@@ -1,7 +1,7 @@
 # This python script is run after running extractAlgorithmNames.py, where
 # the output of that script is saved to a *.txt
 #
-# This script then query github for arthur of each algorithm. This script
+# This script then query github for author of each algorithm. This script
 # was original used in connection with creating usage examples for algorithms
 # and creating an algorithm list for each developer to have a go at
 
