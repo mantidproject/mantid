3ce7aa48c5ecb67cdc36ca42374a560ad239d6f5
Stuart Campbell <campbellsi@ornl.gov>
2015-04-21 16:23:29 -0400

    Commit changes from crashed os! refs #10748

--- Code/Mantid/Framework/Algorithms/test/ConvertUnitsUsingDetectorTableTest.h
+++ Code/Mantid/Framework/Algorithms/test/ConvertUnitsUsingDetectorTableTest.h
@@ -3,10 +3,25 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidKernel/UnitFactory.h"
+
+
+
 #include "MantidAlgorithms/ConvertUnitsUsingDetectorTable.h"
 
+
+
 using Mantid::Algorithms::ConvertUnitsUsingDetectorTable;
+using namespace Mantid;
+using namespace Mantid::Kernel;
 using namespace Mantid::API;
+using namespace Mantid::Algorithms;
+using namespace Mantid::DataObjects;
+using namespace Mantid::Geometry;
 
 class ConvertUnitsUsingDetectorTableTest : public CxxTest::TestSuite
 {
@@ -24,7 +39,7 @@ public:
     TS_ASSERT( alg.isInitialized() )
   }
 
-  void test_exec()
+  void xtest_exec()
   {
     // Name of the output workspace.
     std::string outWSName("ConvertUnitsUsingDetectorTableTest_OutputWS");
@@ -48,10 +63,58 @@ public:
     // Remove workspace from the data service.
     AnalysisDataService::Instance().remove(outWSName);
   }
-  
-  void test_Something()
+
+  // TODO: Make this test useful
+  void test_TofToEnergy()
   {
-    TSM_ASSERT( "You forgot to write a test!", 0);
+     ConvertUnitsUsingDetectorTable myAlg;
+     myAlg.initialize();
+     TS_ASSERT(myAlg.isInitialized());
+
+     const std::string workspaceName("_ws_testConvertUsingDetectorTable");
+     int nBins = 10;
+     MatrixWorkspace_sptr WS = WorkspaceCreationHelper::Create2DWorkspaceBinned(2, nBins, 5.0, 50.0);
+     WS->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
+
+     AnalysisDataService::Instance().add(workspaceName,WS);
+
+      // Create TableWorkspace with values in it
+
+      ITableWorkspace_sptr pars = WorkspaceFactory::Instance().createTable("TableWorkspace");
+      pars->addColumn("int", "spectra");
+      pars->addColumn("double", "l1");
+      pars->addColumn("double", "l2");
+      pars->addColumn("double", "twotheta");
+      pars->addColumn("double", "efixed");
+      pars->addColumn("int", "emode");
+
+      API::TableRow row0 = pars->appendRow();
+      row0 << 1 << 100.0 << 10.0 << 90.0 << 7.0 << 0;
+
+      API::TableRow row1 = pars->appendRow();
+      row1 << 2 << 100.0 << 10.0 << 90.0 << 7.0 << 1;
+
+      // Set the properties
+      myAlg.setRethrows(true);
+      myAlg.setPropertyValue("InputWorkspace", workspaceName);
+      myAlg.setPropertyValue("OutputWorkspace", workspaceName);
+      myAlg.setPropertyValue("Target", "Energy");
+      myAlg.setProperty("DetectorParameters", pars);
+
+      myAlg.execute();
+
+      auto outWS = AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(workspaceName);
+
+      for (int j=0; j < outWS->getNumberHistograms(); ++j) {
+          for (int i=0; i < outWS->blocksize(); ++i) {
+              std::cout << "dataX[" << j << "]["<< i << "] = " << outWS->dataX(j)[i] << std::endl;
+          }
+      }
+
+      TS_ASSERT_DELTA( outWS->dataX(1)[1], 25.3444, 0.01 );
+      // TODO: Add more checks.
+
+      AnalysisDataService::Instance().remove(workspaceName);
   }
 
 
