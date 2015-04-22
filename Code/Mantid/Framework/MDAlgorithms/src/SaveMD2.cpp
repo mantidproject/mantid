f468a0d7cf4b0ff2aa59f63e0d6c26ffa2fca572
Ross Whitfield <whitfieldre@ornl.gov>
2015-04-22 15:19:48 -0400

    Refs #11362 If MDEventWorkspace then use SaveMD Version 1.

--- Code/Mantid/Framework/MDAlgorithms/src/SaveMD2.cpp
+++ Code/Mantid/Framework/MDAlgorithms/src/SaveMD2.cpp
@@ -81,142 +81,6 @@ void SaveMD2::init() {
 }
 
 //----------------------------------------------------------------------------------------------
-/** Save the MDEventWorskpace to a file.
- * Based on the Intermediate Data Format Detailed Design Document, v.1.R3 found
- *in SVN.
- *
- * @param ws :: MDEventWorkspace of the given type
- */
-template <typename MDE, size_t nd>
-void SaveMD2::doSaveEvents(typename MDEventWorkspace<MDE, nd>::sptr ws) {
-  std::string filename = getPropertyValue("Filename");
-  bool update = getProperty("UpdateFileBackEnd");
-  bool MakeFileBacked = getProperty("MakeFileBacked");
-
-  bool wsIsFileBacked = ws->isFileBacked();
-  if (update && MakeFileBacked)
-    throw std::invalid_argument(
-        "Please choose either UpdateFileBackEnd or MakeFileBacked, not both.");
-
-  if (MakeFileBacked && wsIsFileBacked)
-    throw std::invalid_argument(
-        "You picked MakeFileBacked but the workspace is already file-backed!");
-
-  BoxController_sptr bc = ws->getBoxController();
-
-  if (!wsIsFileBacked) { // Erase the file if it exists
-    Poco::File oldFile(filename);
-    if (oldFile.exists())
-      oldFile.remove();
-  }
-
-  Progress *prog = new Progress(this, 0.0, 0.05, 1);
-  if (update) // workspace has its own file and ignores any changes to the
-              // algorithm parameters
-  {
-    if (!ws->isFileBacked())
-      throw std::runtime_error(" attempt to update non-file backed workspace");
-    filename = bc->getFileIO()->getFileName();
-  }
-
-  //-----------------------------------------------------------------------------------------------------
-  // create or open WS group and put there additional information about WS and
-  // its dimensions
-  int nDims = static_cast<int>(nd);
-  bool data_exist;
-  auto file = file_holder_type(MDBoxFlatTree::createOrOpenMDWSgroup(
-      filename, nDims, MDE::getTypeName(), false, data_exist));
-
-  // Save each NEW ExperimentInfo to a spot in the file
-  MDBoxFlatTree::saveExperimentInfos(file.get(), ws);
-  if (!update || !data_exist) {
-    MDBoxFlatTree::saveWSGenericInfo(file.get(), ws);
-  }
-  file->closeGroup();
-  file->close();
-
-  MDBoxFlatTree BoxFlatStruct;
-  //-----------------------------------------------------------------------------------------------------
-  if (update) // the workspace is already file backed;
-  {
-    // remove all boxes from the DiskBuffer. DB will calculate boxes positions
-    // on HDD.
-    bc->getFileIO()->flushCache();
-    // flatten the box structure; this will remember boxes file positions in the
-    // box structure
-    BoxFlatStruct.initFlatStructure(ws, filename);
-  } else // not file backed;
-  {
-    // the boxes file positions are unknown and we need to calculate it.
-    BoxFlatStruct.initFlatStructure(ws, filename);
-    // create saver class
-    auto Saver = boost::shared_ptr<API::IBoxControllerIO>(
-        new DataObjects::BoxControllerNeXusIO(bc.get()));
-    Saver->setDataType(sizeof(coord_t), MDE::getTypeName());
-    if (MakeFileBacked) {
-      // store saver with box controller
-      bc->setFileBacked(Saver, filename);
-      // get access to boxes array
-      std::vector<API::IMDNode *> &boxes = BoxFlatStruct.getBoxes();
-      // calculate the position of the boxes on file, indicating to make them
-      // saveable and that the boxes were not saved.
-      BoxFlatStruct.setBoxesFilePositions(true);
-      prog->resetNumSteps(boxes.size(), 0.06, 0.90);
-      for (size_t i = 0; i < boxes.size(); i++) {
-        auto saveableTag = boxes[i]->getISaveable();
-        if (saveableTag) // only boxes can be saveable
-        {
-          // do not spend time on empty boxes
-          if (boxes[i]->getDataInMemorySize() == 0)
-            continue;
-          // save boxes directly using the boxes file postion, precalculated in
-          // boxFlatStructure.
-          saveableTag->save();
-          // remove boxes data from memory. This will actually correctly set the
-          // tag indicatin that data were not loaded.
-          saveableTag->clearDataFromMemory();
-          // put boxes into write buffer wich will save them when necessary
-          // Saver->toWrite(saveTag);
-          prog->report("Saving Box");
-        }
-      }
-      // remove everything from diskBuffer;  (not sure if it really necessary
-      // but just in case , should not make any harm)
-      Saver->flushCache();
-      // drop NeXus on HDD (not sure if it really necessary but just in case )
-      Saver->flushData();
-    } else // just save data, and finish with it
-    {
-      Saver->openFile(filename, "w");
-      BoxFlatStruct.setBoxesFilePositions(false);
-      std::vector<API::IMDNode *> &boxes = BoxFlatStruct.getBoxes();
-      std::vector<uint64_t> &eventIndex = BoxFlatStruct.getEventIndex();
-      prog->resetNumSteps(boxes.size(), 0.06, 0.90);
-      for (size_t i = 0; i < boxes.size(); i++) {
-        if (eventIndex[2 * i + 1] == 0)
-          continue;
-        boxes[i]->saveAt(Saver.get(), eventIndex[2 * i]);
-        prog->report("Saving Box");
-      }
-      Saver->closeFile();
-    }
-  }
-
-  // -------------- Save Box Structure  -------------------------------------
-  // OK, we've filled these big arrays of data representing flat box structrre.
-  // Save them.
-  progress(0.91, "Writing Box Data");
-  prog->resetNumSteps(8, 0.92, 1.00);
-
-  // Save box structure;
-  BoxFlatStruct.saveBoxStructure(filename);
-
-  delete prog;
-
-  ws->setFileNeedsUpdating(false);
-}
-
-//----------------------------------------------------------------------------------------------
 /** Save a MDHistoWorkspace to a .nxs file
  *
  * @param ws :: MDHistoWorkspace to save
@@ -320,8 +184,13 @@ void SaveMD2::exec() {
       boost::dynamic_pointer_cast<MDHistoWorkspace>(ws);
 
   if (eventWS) {
-    // Wrapper to cast to MDEventWorkspace then call the function
-    CALL_MDEVENT_FUNCTION(this->doSaveEvents, eventWS);
+    // If event workspace use SaveMD version 1.
+    IAlgorithm_sptr saveMDv1 = createChildAlgorithm("SaveMD",-1,-1,true,1);
+    saveMDv1->setProperty<IMDWorkspace_sptr>("InputWorkspace",ws);
+    saveMDv1->setProperty<std::string>("Filename",getProperty("Filename"));
+    saveMDv1->setProperty<bool>("UpdateFileBackEnd",getProperty("UpdateFileBackEnd"));
+    saveMDv1->setProperty<bool>("MakeFileBacked",getProperty("MakeFileBacked"));
+    saveMDv1->execute();
   } else if (histoWS) {
     this->doSaveHisto(histoWS);
   } else
