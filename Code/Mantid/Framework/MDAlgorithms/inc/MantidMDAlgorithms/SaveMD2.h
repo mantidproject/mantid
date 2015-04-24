3c50ade1674fdb46b92da9d60cbf6c1a80ac84b2
Ross Whitfield <whitfieldre@ornl.gov>
2015-04-24 09:49:43 -0400

    Refs #11362 Remove unneeded stuff from header.

--- Code/Mantid/Framework/MDAlgorithms/inc/MantidMDAlgorithms/SaveMD2.h
+++ Code/Mantid/Framework/MDAlgorithms/inc/MantidMDAlgorithms/SaveMD2.h
@@ -54,20 +54,9 @@ private:
   /// Run the algorithm
   void exec();
 
-  /// Helper method
-  template <typename MDE, size_t nd>
-  void doSaveEvents(typename DataObjects::MDEventWorkspace<MDE, nd>::sptr ws);
-
   /// Save the MDHistoWorkspace.
   void doSaveHisto(Mantid::DataObjects::MDHistoWorkspace_sptr ws);
 
-  /// Save all the affine matricies
-  void saveAffineTransformMatricies(::NeXus::File *const file,
-                                    API::IMDWorkspace_const_sptr ws);
-  /// Save a given affine matrix
-  void saveAffineTransformMatrix(::NeXus::File *const file,
-                                 API::CoordTransform *transform,
-                                 std::string entry_name);
   /// Save a generic matrix
   template <typename T>
   void saveMatrix(::NeXus::File *const file, std::string name,
