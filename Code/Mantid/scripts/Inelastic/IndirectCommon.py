cda8566e3fb3d357a2c6ac7eb64465773643abbb
Dan Nixon <dan@dan-nixon.com>
2015-04-28 10:57:04 +0100

    Fix trivial Python error
    Refs #11638

--- Code/Mantid/scripts/Inelastic/IndirectCommon.py
+++ Code/Mantid/scripts/Inelastic/IndirectCommon.py
@@ -89,14 +89,14 @@ def getWSprefix(wsname):
 def getEfixed(workspace, detIndex=0):
     inst = mtd[workspace].getInstrument()
 
-    if inst.hasParameter('Efixed')
+    if inst.hasParameter('Efixed'):
         return inst.getNumberParameter('EFixed')[0]
 
     if inst.hasParameter('analyser'):
         analyser_name = inst.getStringParameter('analyser')[0]
         analyser_comp = inst.getComponentByName(analyser_name)
 
-        if analyser_comp.hasParameter('Efixed')
+        if analyser_comp.hasParameter('Efixed'):
             return analyser_comp.getNumberParameter('EFixed')[0]
 
     raise ValueError('No Efixed parameter found')
