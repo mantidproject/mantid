0a0a8c128ba083496c6dca9a45ad733c3f664d02
Alex Buts <Alex.Buts@stfc.ac.uk>
2015-04-30 13:58:27 +0100

    Re #11628 documentation and correct good frame rate for event
    workspace.

--- Code/Mantid/scripts/Inelastic/Direct/diagnostics.py
+++ Code/Mantid/scripts/Inelastic/Direct/diagnostics.py
@@ -373,14 +373,14 @@ def do_bleed_test(sample_run, max_framerate, ignored_pixels):
     # Check if all necessary logs present in the workspace,as nxs workspace log names are different
     #  from a raw file workspace logs.
     try:
-        nFrames= data_ws.run().getLogData('goodfrm').value
+        nFrames= data_ws.getRun().getLogData('goodfrm').value
     except RuntimeError:
         try:
-            nFrames = len(data_ws.run().getLogData('good_frame_log').value)
+            nFrames = data_ws.getRun().getLogData('good_frames').lastValue()
             AddSampleLog(Workspace=data_ws, LogName='goodfrm', LogText=str(nFrames), LogType='Number')
         except RuntimeError:
             raise RuntimeError("""Can not run bleed test as no appropriate good frame log is found in the workspace: {0}\n
-                                  Disable bleed test by setting diag_bleed_test=False or add 'goodfrm' log value to the workspace\n"""\
+                                  Disable bleed test by setting diag_bleed_test=False or add 'goodfrm' log to the workspace\n"""\
                                   .format(data_ws.name()))
 
 
