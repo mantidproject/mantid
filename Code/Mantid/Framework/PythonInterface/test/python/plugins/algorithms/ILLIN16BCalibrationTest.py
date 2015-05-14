04aa40db17829fc7868575930de70bf18b803eaa
Dan Nixon <dan@dan-nixon.com>
2015-05-14 13:37:24 +0100

    Use grouping XML in test
    Refs #11163

--- Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/ILLIN16BCalibrationTest.py
+++ Code/Mantid/Framework/PythonInterface/test/python/plugins/algorithms/ILLIN16BCalibrationTest.py
@@ -1,6 +1,18 @@
 import unittest
 import mantid
 from mantid.simpleapi import ILLIN16BCalibration
+from testhelpers.tempfile_wrapper import TemporaryFileHelper
+
+
+SAMPLE_GROUPING_XML = \
+'''<?xml version="1.0" encoding="utf-8"?>
+<detector-grouping instrument="IN16B">
+  <group name="psd1"> <ids val="2-129"/> </group>
+  <group name="psd2"> <ids val="130-257"/> </group>
+  <group name="psd3"> <ids val="258-385"/> </group>
+  <group name="psd4"> <ids val="386-513"/> </group>
+</detector-grouping>
+'''
 
 
 class ILLIN16BCalibrationTest(unittest.TestCase):
@@ -23,17 +35,16 @@ class ILLIN16BCalibrationTest(unittest.TestCase):
         self.assertEqual(calib_ws.blocksize(), 1)
 
 
-    def test_spectra_range(self):
+    def test_map_file(self):
+        temp_map = TemporaryFileHelper(SAMPLE_GROUPING_XML, extension='.xml')
+
         calib_ws = ILLIN16BCalibration(Run='ILLIN16B_034745.nxs',
                                        MirrorMode=True,
-                                       SpectraRange=[4, 10],
+                                       MapFile=temp_map.getName(),
                                        PeakRange=[-0.001, 0.002])
 
-        self.assertEqual(calib_ws.getNumberHistograms(), 7)
+        self.assertEqual(calib_ws.getNumberHistograms(), 4)
         self.assertEqual(calib_ws.blocksize(), 1)
-
-        self.assertEqual(calib_ws.getSpectrum(0).getSpectrumNo(), 4)
-        self.assertEqual(calib_ws.getSpectrum(6).getSpectrumNo(), 10)
 
 
 if __name__=="__main__":
