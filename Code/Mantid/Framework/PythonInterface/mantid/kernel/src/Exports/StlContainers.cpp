fb06545972f316dd3265889a3b9bbca1a230697f
Pete Peterson <petersonpf@ornl.gov>
2015-05-19 10:56:46 -0400

    Re #766. Exposing shared_ptr<Quat> and vector<Quat> in python.

--- Code/Mantid/Framework/PythonInterface/mantid/kernel/src/Exports/StlContainers.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/kernel/src/Exports/StlContainers.cpp
@@ -1,6 +1,7 @@
 #include "MantidPythonInterface/kernel/StlExportDefinitions.h"
 
 #include "MantidKernel/DateAndTime.h"
+#include "MantidKernel/Quat.h"
 
 using Mantid::PythonInterface::std_vector_exporter;
 using Mantid::PythonInterface::std_set_exporter;
@@ -18,6 +19,7 @@ void exportStlContainers()
   std_vector_exporter<bool>::wrap("std_vector_bool");
   std_vector_exporter<std::string>::wrap("std_vector_str");
   std_vector_exporter<Mantid::Kernel::DateAndTime>::wrap("std_vector_dateandtime");
+  std_vector_exporter<Mantid::Kernel::Quat>::wrap("std_vector_quat");
   //std::set
   std_set_exporter<int>::wrap("std_set_int");
   std_set_exporter<std::string>::wrap("std_set_str");
