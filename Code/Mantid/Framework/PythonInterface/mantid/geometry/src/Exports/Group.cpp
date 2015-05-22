ad2b54586fb729b5a5de92dc08973899b4e92690
Michael Wedel <michael.wedel@psi.ch>
2015-05-22 16:55:04 +0200

    Refs #11830. Exporting additional group members

--- Code/Mantid/Framework/PythonInterface/mantid/geometry/src/Exports/Group.cpp
+++ Code/Mantid/Framework/PythonInterface/mantid/geometry/src/Exports/Group.cpp
@@ -32,11 +32,20 @@ void export_Group()
       .value("Orthogonal", Group::Orthogonal)
       .value("Hexagonal", Group::Hexagonal);
 
+  enum_<Group::GroupAxiom>("GroupAxiom")
+      .value("Closure", Group::Closure)
+      .value("Identity", Group::Identity)
+      .value("Inversion", Group::Inversion)
+      .value("Associativity", Group::Associativity);
+
   class_<Group>("Group")
       .def(init<std::string>("Construct a group from the provided initializer string."))
       .def(init<std::vector<SymmetryOperation> >("Construct a group from the provided symmetry operation list."))
       .def("getOrder", &Group::order, "Returns the order of the group.")
       .def("getCoordinateSystem", &Group::getCoordinateSystem, "Returns the type of coordinate system to distinguish groups with hexagonal system definition.")
       .def("getSymmetryOperations", &Group::getSymmetryOperations, "Returns the symmetry operations contained in the group.")
-      .def("getSymmetryOperationStrings", &getSymmetryOperationStrings, "Returns the x,y,z-strings for the contained symmetry operations.");
+      .def("getSymmetryOperationStrings", &getSymmetryOperationStrings, "Returns the x,y,z-strings for the contained symmetry operations.")
+      .def("containsOperation", &Group::containsOperation, "Checks whether a SymmetryOperation is included in Group.")
+      .def("isGroup", &Group::isGroup, "Checks whether the contained symmetry operations fulfill the group axioms.")
+      .def("fulfillAxiom", &Group::fulfillsAxiom, "Checks if the contained symmetry operations fulfill the specified group axiom.");
 }
