ff46c63f318cfb1a053d815fe0b93c935f620e9a
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-05-26 22:18:04 +0100

    fix type (dbl->ushort in constructor, re #11826

--- Code/Mantid/MantidPlot/src/origin/OPJFile.h
+++ Code/Mantid/MantidPlot/src/origin/OPJFile.h
@@ -304,19 +304,19 @@ struct vectorProperties
 	int const_magnitude;
 
 	vectorProperties()
-	:	color(0)
-	,	width(0.0)
-	,	arrow_lenght(0.0)
+        :	color(0)
+        ,	width(0.0)
+        ,	arrow_lenght(0)
         ,	arrow_angle(0)
-	,	arrow_closed(false)
-	,	endXColName()
-	,	endYColName()
+        ,	arrow_closed(false)
+        ,	endXColName()
+        ,	endYColName()
         ,	position(0)
         ,	angleColName()
         ,	magnitudeColName(0)
         ,	multiplier(1.0)
-	,	const_angle(0)
-	,	const_magnitude(0)
+        ,	const_angle(0)
+        ,	const_magnitude(0)
 	{};
 };
 
