191ad691d07193331863c4c9b5974cc53ede07a6
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2015-05-26 12:21:25 +0100

    Refs #11814 make unit test more efficient

--- Code/Mantid/Vates/VatesAPI/test/BoxInfoTest.h
+++ Code/Mantid/Vates/VatesAPI/test/BoxInfoTest.h
@@ -1,12 +1,11 @@
-#ifndef COMOPOSITE_PEAKS_BOX_INFO_TEST_H_
-#define COMOPOSITE_PEAKS_BOX_INFO_TEST_H_
+#ifndef VATES_API_BOX_INFO_TEST_H_
+#define VATES_API_BOX_INFO_TEST_H_
 
 #include <cxxtest/TestSuite.h>
 #include "MantidVatesAPI/BoxInfo.h"
 #include "MantidAPI/AnalysisDataService.h"
-#include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/BoxController.h"
 #include "MantidKernel/PropertyWithValue.h"
-#include "MantidMDAlgorithms/ConvertToMD.h"
 #include "MantidTestHelpers/MDEventsTestHelper.h"
 #include "MantidDataObjects/MDLeanEvent.h"
 
@@ -46,7 +45,11 @@ public:
   void test_initial_recursion_depth_is_1_for_MD_Event_w_split() {
     // Arrange
     const std::string wsName = "MD_EVENT_WS_WITH_SPLITTING";
-    provideMDEventWorspaceWithTopLevelSplitting(wsName);
+    auto ws = makeAnyMDEW<MDLeanEvent<3>, 3>(10, 0.0, 10.0, 1, wsName);
+    BoxController_sptr boxController =  ws->getBoxController();
+    boxController->setSplitTopInto(0,10);
+    boxController->setSplitTopInto(1, 20);
+    boxController->setSplitTopInto(2,30);
 
     // Act
     auto result = Mantid::VATES::findRecursionDepthForTopLevelSplitting(wsName);
@@ -56,40 +59,6 @@ public:
 
     // Clean up
     AnalysisDataService::Instance().remove(wsName);
-  }
-
-private:
-  void provideMDEventWorspaceWithTopLevelSplitting(std::string wsName) {
-
-    auto alg = Mantid::API::AlgorithmManager::Instance().create(
-        "CreateSampleWorkspace");
-    alg->initialize();
-    alg->setChild(true);
-    alg->setProperty("WorkspaceType", "Event");
-    alg->setPropertyValue("OutputWorkspace", wsName);
-    alg->execute();
-
-    Mantid::API::MatrixWorkspace_sptr ws = alg->getProperty("OutputWorkspace");
-    Mantid::API::Run &run = ws->mutableRun();
-    auto eiLog = new PropertyWithValue<double>("Ei", 12.0);
-    run.addLogData(eiLog);
-
-    Mantid::MDAlgorithms::ConvertToMD convertAlg;
-    convertAlg.setChild(true);
-    convertAlg.initialize();
-    convertAlg.setPropertyValue("OutputWorkspace",wsName);
-    convertAlg.setProperty("InputWorkspace", ws);
-    convertAlg.setProperty("QDimensions", "Q3D");
-    convertAlg.setProperty("dEAnalysisMode", "Direct");
-    convertAlg.setPropertyValue("MinValues","-10,-10,-10, 0");
-    convertAlg.setPropertyValue("MaxValues"," 10, 10, 10, 1");
-    convertAlg.setPropertyValue("TopLevelSplitting", "1");
-    convertAlg.execute();
-
-    if (!AnalysisDataService::Instance().doesExist(wsName)) {
-      IMDEventWorkspace_sptr  ws_new = convertAlg.getProperty("OutputWorkspace");
-      AnalysisDataService::Instance().addOrReplace(wsName, ws_new);
-    }
   }
 };
 
