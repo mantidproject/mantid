9eeaff3d8d51e575d5c05c72b644b0e8f38d3c41
Owen Arnold <owen.arnold@stfc.ac.uk>
2015-05-27 14:09:04 +0100

    refs #11755. Ignore ui files autogenerated.

--- Code/Mantid/Build/CMake/UiToPy.cmake
+++ Code/Mantid/Build/CMake/UiToPy.cmake
@@ -1,10 +1,15 @@
-function(UiToPy py_exec py_uic_py ui_files)
+###########################################################################
+# Function to convert ui files into PyQt4 python files
+###########################################################################
 
-  
+function(UiToPy ui_files)
+  set(py_exec ${PYTHON_EXECUTABLE})
+  set(py_uic_py ${PYQT4_PYUIC} ) # From FindPyQt4
   set(ui_dir ${CMAKE_CURRENT_SOURCE_DIR})
   foreach(ui_file ${${ui_files}})
-    # Establish the parent directory
+    # Get the filename
     get_filename_component(ui_name ${ui_file}  NAME_WE)
-    execute_process(COMMAND ${py_exec} ${py_uic_py} ${ui_dir}/${ui_name}.ui -o ${ui_dir}/${ui_name}.py)
+    # Run the converter
+    execute_process(COMMAND ${py_exec} ${py_uic_py} ${ui_dir}/${ui_name}.ui -o ${ui_dir}/ui_${ui_name}.py)
   endforeach(ui_file)
 endfunction(UiToPy)
