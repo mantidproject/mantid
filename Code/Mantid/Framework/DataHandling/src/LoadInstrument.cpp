455e782e08b63666b9fcf0a09674f0ff7e741f15
Nick Draper <nick.draper@stfc.ac.uk>
2015-05-28 18:11:30 +0100

    Error 4: from above the instrument parameter loading is fixed
    re #11818

--- Code/Mantid/Framework/DataHandling/src/LoadInstrument.cpp
+++ Code/Mantid/Framework/DataHandling/src/LoadInstrument.cpp
@@ -6,19 +6,8 @@
 #include "MantidAPI/Progress.h"
 #include "MantidDataHandling/LoadInstrument.h"
 #include "MantidGeometry/Instrument.h"
-#include "MantidGeometry/Instrument/Component.h"
-#include "MantidGeometry/Instrument/Detector.h"
-#include "MantidGeometry/Instrument/ObjCompAssembly.h"
-#include "MantidGeometry/Instrument/RectangularDetector.h"
-#include "MantidGeometry/Objects/ShapeFactory.h"
-#include "MantidGeometry/Rendering/vtkGeometryCacheReader.h"
-#include "MantidGeometry/Rendering/vtkGeometryCacheWriter.h"
 #include "MantidKernel/ArrayProperty.h"
 #include "MantidKernel/ConfigService.h"
-#include "MantidKernel/DateAndTime.h"
-#include "MantidKernel/Interpolation.h"
-#include "MantidKernel/PhysicalConstants.h"
-#include "MantidKernel/UnitFactory.h"
 
 #include <Poco/DOM/DOMParser.h>
 #include <Poco/DOM/Document.h>
@@ -272,10 +261,11 @@ void LoadInstrument::runLoadParameterFile() {
 /// found, else return "".
 //  directoryName must include a final '/'.
 std::string LoadInstrument::getFullPathParamIDF(std::string directoryName) {
+  Poco::Path directoryPath (directoryName);
+  directoryPath.makeDirectory();
   // Remove the path from the filename
-  const std::string::size_type stripPath = m_filename.find_last_of("\\/");
-  std::string instrumentFile =
-      m_filename.substr(stripPath + 1, m_filename.size());
+  Poco::Path filePath (m_filename);
+  std::string instrumentFile = filePath.getFileName();
 
   // First check whether there is a parameter file whose name is the same as the
   // IDF file,
@@ -295,11 +285,11 @@ std::string LoadInstrument::getFullPathParamIDF(std::string directoryName) {
 
   // Assemble parameter file name
   std::string fullPathParamIDF =
-      directoryName + prefix + "_Parameters" + suffix;
+      directoryPath.setFileName(prefix + "_Parameters" + suffix).toString();
   if (Poco::File(fullPathParamIDF).exists() ==
       false) { // No such file exists, so look for file based on instrument ID
                // given by the prefix
-    fullPathParamIDF = directoryName + "/" + prefix + "_Parameters.xml";
+    fullPathParamIDF = directoryPath.setFileName(prefix + "_Parameters.xml").toString();
   }
 
   if (Poco::File(fullPathParamIDF).exists() ==
