9669ae2bfa2001fbd252e49c7795715770a120f0
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-05-29 16:49:39 +0100

    circumvent a pylint warning with simpleapi, re #10967

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/EnginXCalibrate.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/EnginXCalibrate.py
@@ -1,7 +1,7 @@
 #pylint: disable=no-init,invalid-name
 from mantid.kernel import *
 from mantid.api import *
-from mantid.simpleapi import CreateEmptyTableWorkspace
+import mantid.simpleapi as sapi
 
 
 class EnginXCalibrate(PythonAlgorithm):
@@ -102,7 +102,7 @@ class EnginXCalibrate(PythonAlgorithm):
 
         @param name :: the name to use for the table workspace that is created here
         """
-        tbl = CreateEmptyTableWorkspace(OutputWorkspace=name)
+        tbl = sapi.CreateEmptyTableWorkspace(OutputWorkspace=name)
         tbl.addColumn('double', 'difc')
         tbl.addColumn('double', 'zero')
         tbl.addRow([float(self.getPropertyValue('difc')), float(self.getPropertyValue('zero'))])
