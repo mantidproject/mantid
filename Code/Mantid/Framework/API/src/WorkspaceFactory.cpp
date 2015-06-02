717d2abf4d6b3889cb88f4f0ce3c98ec62365eda
Dan Nixon <dan@dan-nixon.com>
2015-06-02 13:37:09 +0100

    Correct previous fix Refs #11882

--- Code/Mantid/Framework/API/src/WorkspaceFactory.cpp
+++ Code/Mantid/Framework/API/src/WorkspaceFactory.cpp
@@ -59,16 +59,16 @@ MatrixWorkspace_sptr
 WorkspaceFactoryImpl::create(const MatrixWorkspace_const_sptr &parent,
                              size_t NVectors, size_t XLength,
                              size_t YLength) const {
-  bool differentSize(true);
   // Use the parent sizes if new ones are not specified
   if (NVectors == size_t(-1))
     NVectors = parent->getNumberHistograms();
   if (XLength == size_t(-1))
     XLength = parent->dataX(0).size();
-  if (YLength == size_t(-1)) {
-    differentSize = false;
+  if (YLength == size_t(-1))
     YLength = parent->blocksize();
-  }
+
+  bool differentSize = (NVectors != parent->getNumberHistograms()) ||
+                       (YLength != parent->blocksize());
 
   // If the parent is an EventWorkspace, we want it to spawn a Workspace2D (or
   // managed variant) as a child
@@ -132,7 +132,7 @@ void WorkspaceFactoryImpl::initializeFromParent(
     const size_t newAxisLength = child->getAxis(i)->length();
     const size_t oldAxisLength = parent->getAxis(i)->length();
 
-    if (!differentSize && newAxisLength == oldAxisLength) {
+    if (!differentSize || newAxisLength == oldAxisLength) {
       // Need to delete the existing axis created in init above
       delete child->m_axes[i];
       // Now set to a copy of the parent workspace's axis
