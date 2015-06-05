67bda8ee2b1bc9d9e9a1f7a471a0cfa28bb2bc3b
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2015-06-05 16:19:19 +0100

    Re #7820 Fix the bug

--- Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/LoadMuonNexus1.h
+++ Code/Mantid/Framework/DataHandling/inc/MantidDataHandling/LoadMuonNexus1.h
@@ -107,6 +107,7 @@ private:
   void runLoadLog(DataObjects::Workspace2D_sptr);
   void loadRunDetails(DataObjects::Workspace2D_sptr localWorkspace);
   void addPeriodLog(DataObjects::Workspace2D_sptr localWorkspace, int64_t period);
+  void addGoodFrames(DataObjects::Workspace2D_sptr localWorkspace, int64_t period);
 
   /// Loads dead time table for the detector
   void loadDeadTimes(Mantid::NeXus::NXRoot &root);
