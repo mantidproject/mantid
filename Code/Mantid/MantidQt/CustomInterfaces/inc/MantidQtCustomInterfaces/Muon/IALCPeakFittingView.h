6343dc61ca52492eccf93d3e834f9b0ef9662ae4
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2015-06-15 14:52:11 +0100

    Re #12398 Display errors in PeakFitting step

--- Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Muon/IALCPeakFittingView.h
+++ Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Muon/IALCPeakFittingView.h
@@ -62,7 +62,8 @@ namespace CustomInterfaces
 
     /// Update the data curve displayed
     /// @param data :: New curve data
-    virtual void setDataCurve(const QwtData& data) = 0;
+    virtual void setDataCurve(const QwtData &data,
+                              const std::vector<double> &errors) = 0;
 
     /// Update the fitted curve displayed
     /// @param data :: New curve data
