f9856957f00c3b0509841814ee2677d376928c72
Harry Jeffery <henry.jeffery@stfc.ac.uk>
2015-06-23 15:55:57 +0100

    Refs #12788 Fix RHEL build

--- Code/Mantid/Framework/DataObjects/inc/MantidDataObjects/FractionalRebinning.h
+++ Code/Mantid/Framework/DataObjects/inc/MantidDataObjects/FractionalRebinning.h
@@ -61,7 +61,8 @@ getIntersectionRegion(API::MatrixWorkspace_const_sptr outputWS,
 MANTID_DATAOBJECTS_DLL void
 normaliseOutput(API::MatrixWorkspace_sptr outputWS,
                 API::MatrixWorkspace_const_sptr inputWS,
-                boost::shared_ptr<API::Progress> progress = 0);
+                boost::shared_ptr<API::Progress> progress =
+                    boost::shared_ptr<API::Progress>());
 
 /// Rebin the input quadrilateral to to output grid
 MANTID_DATAOBJECTS_DLL void
