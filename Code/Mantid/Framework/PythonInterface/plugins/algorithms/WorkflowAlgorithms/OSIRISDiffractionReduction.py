884c248eb478debc01040806145fc9d9a2e7b6b4
Dan Nixon <dan@dan-nixon.com>
2015-07-03 13:53:19 +0100

    Refactoring      Re #13015

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/OSIRISDiffractionReduction.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/OSIRISDiffractionReduction.py
@@ -85,12 +85,14 @@ class DRangeToWsMap(object):
         return self._map
 
 
-def averageWsList(wsList):
+def average_ws_list(wsList):
     """
     Returns the average of a list of workspaces.
     """
     # Assert we have some ws in the list, and if there is only one then return it.
-    assert len(wsList) > 0, "getAverageWs: Trying to take an average of nothing."
+    if len(wsList) == 0:
+        raise RuntimeError("getAverageWs: Trying to take an average of nothing")
+
     if len(wsList) == 1:
         return wsList[0]
 
@@ -113,28 +115,28 @@ def averageWsList(wsList):
     return avName
 
 
-def findIntersectionOfTwoRanges(rangeA, rangeB):
-
-    assert rangeA[0] < rangeA[1], "Malformed range."
-    assert rangeB[0] < rangeB[1], "Malformed range."
+def find_intersection_of_ranges(rangeA, rangeB):
+    if rangeA[0] >= rangeA[1] or rangeB[0] >= rangeB[1]:
+        raise RuntimeError("Malformed range")
 
     if rangeA[0] <= rangeA[1] <= rangeB[0] <= rangeB[1]:
         return
     if rangeB[0] <= rangeB[1] <= rangeA[0] <= rangeA[1]:
         return
-    if rangeA[0] <= rangeB[0] <= rangeB[1] <= rangeA[1] :
+    if rangeA[0] <= rangeB[0] <= rangeB[1] <= rangeA[1]:
         return rangeB
-    if rangeB[0] <= rangeA[0] <= rangeA[1] <= rangeB[1] :
+    if rangeB[0] <= rangeA[0] <= rangeA[1] <= rangeB[1]:
         return rangeA
-    if rangeA[0] <= rangeB[0] <= rangeA[1] <= rangeB[1] :
+    if rangeA[0] <= rangeB[0] <= rangeA[1] <= rangeB[1]:
         return [rangeB[0], rangeA[1]]
-    if rangeB[0] <= rangeA[0] <= rangeB[1] <= rangeA[1] :
+    if rangeB[0] <= rangeA[0] <= rangeB[1] <= rangeA[1]:
         return [rangeA[0], rangeB[1]]
 
-    assert False, "We should never reach here ..."
+    # Should never reach here
+    raise RuntimeError()
 
 
-def getIntersectionsOfRanges(rangeList):
+def get_intersetcion_of_ranges(range_list):
     """
     Get the intersections of a list of ranges.  For example, given the ranges:
     [1, 3], [3, 5] and [4, 6], the intersections would be a single range of [4,
@@ -144,14 +146,15 @@ def getIntersectionsOfRanges(rangeList):
     the same point.  Also, all ranges should obey range[0] <= range[1].
     """
     # Sanity check.
-    for myrange in rangeList:
-        assert len(myrange) == 2, "Unable to find the intersection of a malformed range."
+    for myrange in range_list:
+        if len(myrange) != 2:
+            raise RuntimeError("Unable to find the intersection of a malformed range")
 
     # Find all combinations of ranges, and see where they intersect.
-    rangeCombos = list(itertools.combinations(rangeList, 2))
+    rangeCombos = list(itertools.combinations(range_list, 2))
     intersections = []
     for rangePair in rangeCombos:
-        intersection = findIntersectionOfTwoRanges(rangePair[0], rangePair[1])
+        intersection = find_intersection_of_ranges(rangePair[0], rangePair[1])
         if intersection is not None:
             intersections.append(intersection)
 
@@ -160,8 +163,8 @@ def getIntersectionsOfRanges(rangeList):
     return intersections
 
 
-def isInRanges(rangeList, n):
-    for myrange in rangeList:
+def is_in_ranges(range_list, n):
+    for myrange in range_list:
         if myrange[0] < n < myrange[1]:
             return True
     return False
@@ -173,11 +176,11 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
     """
 
     _cal = None
-    _outputWsName = None
-    _sams = None
-    _vans = None
-    _samMap = None
-    _vanMap = None
+    _output_ws_name = None
+    _sample_runs = None
+    _vanadium_runs = None
+    _sam_ws_map = None
+    _van_ws_map = None
 
 
     def category(self):
@@ -204,98 +207,113 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
                                  "one will be generated based on the run numbers.")
 
         self._cal = None
-        self._outputWsName = None
+        self._output_ws_name = None
 
-        self._samMap = DRangeToWsMap()
-        self._vanMap = DRangeToWsMap()
+        self._sam_ws_map = DRangeToWsMap()
+        self._van_ws_map = DRangeToWsMap()
 
 
     def PyExec(self):
         # Set OSIRIS as default instrument.
-        config["default.instrument"] = 'OSIRIS'
-
+        # config["default.instrument"] = 'OSIRIS'
         self._cal = self.getProperty("CalFile").value
-        self._outputWsName = self.getPropertyValue("OutputWorkspace")
+        self._output_ws_name = self.getPropertyValue("OutputWorkspace")
 
-        sampleRuns = self.findRuns(self.getPropertyValue("Sample"))
+        self._sample_runs = self._find_runs(self.getPropertyValue("Sample"))
+        self._vanadium_runs = self._find_runs(self.getPropertyValue("Vanadium"))
 
-        self.execDiffOnly(sampleRuns)
+        self.execDiffOnly()
 
 
     #pylint: disable=too-many-branches
-    def execDiffOnly(self, sampleRuns):
+    def execDiffOnly(self):
         """
-            Execute the algorithm in diffraction-only mode
-            @param sampleRuns A list of files pointing to the sample runs
+        Execute the algorithm in diffraction-only mode
         """
-        self._sams = sampleRuns
-        self._vans = self.findRuns(self.getPropertyValue("Vanadium"))
-
         # Load all sample and vanadium files, and add the resulting workspaces to the DRangeToWsMaps.
-        for fileName in self._sams + self._vans:
-            Load(Filename=fileName, OutputWorkspace=fileName, SpectrumMin=3)#, SpectrumMax=962)
-        for sam in self._sams:
-            self._samMap.addWs(sam)
-        for van in self._vans:
-            self._vanMap.addWs(van)
+        for fileName in self._sample_runs + self._vanadium_runs:
+            Load(Filename=fileName,
+                 OutputWorkspace=fileName,
+                 SpectrumMin=3)
+                 # SpectrumMax=962)
+
+        for sam in self._sample_runs:
+            self._sam_ws_map.addWs(sam)
+        for van in self._vanadium_runs:
+            self._van_ws_map.addWs(van)
 
         # Check to make sure that there are corresponding vanadium files with the same DRange for each sample file.
-        for dRange in self._samMap.getMap().iterkeys():
-            if dRange not in self._vanMap.getMap():
+        for dRange in self._sam_ws_map.getMap().iterkeys():
+            if dRange not in self._van_ws_map.getMap():
                 raise RuntimeError("There is no van file that covers the " + str(dRange) + " DRange.")
 
         # Average together any sample workspaces with the same DRange.  This will mean our map of DRanges
         # to list of workspaces becomes a map of DRanges, each to a *single* workspace.
         tempSamMap = DRangeToWsMap()
-        for dRange, wsList in self._samMap.getMap().iteritems():
-            tempSamMap.setItem(dRange, averageWsList(wsList))
-        self._samMap = tempSamMap
+        for dRange, wsList in self._sam_ws_map.getMap().iteritems():
+            tempSamMap.setItem(dRange, average_ws_list(wsList))
+        self._sam_ws_map = tempSamMap
 
         # Now do the same to the vanadium workspaces.
         tempVanMap = DRangeToWsMap()
-        for dRange, wsList in self._vanMap.getMap().iteritems():
-            tempVanMap.setItem(dRange, averageWsList(wsList))
-        self._vanMap = tempVanMap
+        for dRange, wsList in self._van_ws_map.getMap().iteritems():
+            tempVanMap.setItem(dRange, average_ws_list(wsList))
+        self._van_ws_map = tempVanMap
 
         # Run necessary algorithms on BOTH the Vanadium and Sample workspaces.
-        for dRange, ws in self._samMap.getMap().items() + self._vanMap.getMap().items():
-            NormaliseByCurrent(InputWorkspace=ws,OutputWorkspace=ws)
-            AlignDetectors(InputWorkspace=ws, OutputWorkspace=ws, CalibrationFile=self._cal)
-            DiffractionFocussing(InputWorkspace=ws, OutputWorkspace=ws, GroupingFileName=self._cal)
-            CropWorkspace(InputWorkspace=ws, OutputWorkspace=ws, XMin=dRange[0], XMax=dRange[1])
+        for dRange, ws in self._sam_ws_map.getMap().items() + self._van_ws_map.getMap().items():
+            NormaliseByCurrent(InputWorkspace=ws,
+                               OutputWorkspace=ws)
+            AlignDetectors(InputWorkspace=ws,
+                           OutputWorkspace=ws,
+                           CalibrationFile=self._cal)
+            DiffractionFocussing(InputWorkspace=ws,
+                                 OutputWorkspace=ws,
+                                 GroupingFileName=self._cal)
+            CropWorkspace(InputWorkspace=ws,
+                          OutputWorkspace=ws,
+                          XMin=dRange[0],
+                          XMax=dRange[1])
 
         # Divide all sample files by the corresponding vanadium files.
-        for dRange in self._samMap.getMap().iterkeys():
-            samWs = self._samMap.getMap()[dRange]
-            vanWs = self._vanMap.getMap()[dRange]
-            samWs, vanWs = self.rebinToSmallest(samWs, vanWs)
-            Divide(LHSWorkspace=samWs, RHSWorkspace=vanWs, OutputWorkspace=samWs)
-            ReplaceSpecialValues(InputWorkspace=samWs, OutputWorkspace=samWs, NaNValue=0.0, InfinityValue=0.0)
+        for dRange in self._sam_ws_map.getMap().iterkeys():
+            samWs = self._sam_ws_map.getMap()[dRange]
+            vanWs = self._van_ws_map.getMap()[dRange]
+            samWs, vanWs = self._rebin_to_smallest(samWs, vanWs)
+            Divide(LHSWorkspace=samWs,
+                   RHSWorkspace=vanWs,
+                   OutputWorkspace=samWs)
+            ReplaceSpecialValues(InputWorkspace=samWs,
+                                 OutputWorkspace=samWs,
+                                 NaNValue=0.0,
+                                 InfinityValue=0.0)
 
         # Create a list of sample workspace NAMES, since we need this for MergeRuns.
         samWsNamesList = []
-        for sam in self._samMap.getMap().itervalues():
+        for sam in self._sam_ws_map.getMap().itervalues():
             samWsNamesList.append(sam)
 
         if len(samWsNamesList) > 1:
             # Merge the sample files into one.
-            MergeRuns(InputWorkspaces=samWsNamesList, OutputWorkspace=self._outputWsName)
+            MergeRuns(InputWorkspaces=samWsNamesList,
+                      OutputWorkspace=self._output_ws_name)
             for name in samWsNamesList:
                 DeleteWorkspace(Workspace=name)
         else:
-            RenameWorkspace(InputWorkspace=samWsNamesList[0],OutputWorkspace=self._outputWsName)
+            RenameWorkspace(InputWorkspace=samWsNamesList[0],
+                            OutputWorkspace=self._output_ws_name)
 
-        result = mtd[self._outputWsName]
+        result = mtd[self._output_ws_name]
 
         # Create scalar data to cope with where merge has combined overlapping data.
-        intersections = getIntersectionsOfRanges(self._samMap.getMap().keys())
+        intersections = get_intersetcion_of_ranges(self._sam_ws_map.getMap().keys())
 
         dataX = result.dataX(0)
         dataY = []
         dataE = []
         for i in range(0, len(dataX)-1):
             x = ( dataX[i] + dataX[i+1] ) / 2.0
-            if isInRanges(intersections, x):
+            if is_in_ranges(intersections, x):
                 dataY.append(2)
                 dataE.append(2)
             else:
@@ -314,13 +332,13 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
             result.setE(i,resultE)
 
         # Delete all workspaces we've created, except the result.
-        for ws in self._vanMap.getMap().values():
+        for ws in self._van_ws_map.getMap().values():
             DeleteWorkspace(Workspace=ws)
 
         self.setProperty("OutputWorkspace", result)
 
 
-    def findRuns(self, run_str):
+    def _find_runs(self, run_str):
         """
         Use the FileFinder to find search for the runs given by the string of
         comma-separated run numbers.
@@ -339,7 +357,7 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
         return run_files
 
 
-    def rebinToSmallest(self, samWS, vanWS):
+    def _rebin_to_smallest(self, samWS, vanWS):
         """
         At some point a change to the control program meant that the raw data
         got an extra bin. This prevents runs past this point being normalised
