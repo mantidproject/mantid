0c1eaa382b1cb4918f8eff4c6d8d29dc2544fa3d
Dan Nixon <dan@dan-nixon.com>
2015-07-03 12:13:45 +0100

    Make drange detection more rebost
    Re #13015

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/OSIRISDiffractionReduction.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/OSIRISDiffractionReduction.py
@@ -5,7 +5,8 @@ from mantid.simpleapi import *
 
 import itertools
 
-timeRegimeToDRange = {\
+
+time_regime_to_d_range = {\
      1.17e4: tuple([ 0.7,  2.5]),\
      2.94e4: tuple([ 2.1,  3.3]),\
      4.71e4: tuple([ 3.1,  4.3]),\
@@ -18,35 +19,49 @@ timeRegimeToDRange = {\
     17.09e4: tuple([10.4, 11.6]),\
     18.86e4: tuple([11.0, 12.5])}\
 
-# A "wrapper" class for a map, which maps workspaces from their corresponding
-#time regimes.
 
 class DRangeToWsMap(object):
+    """
+    A "wrapper" class for a map, which maps workspaces from their corresponding
+    time regimes.
+    """
 
     def __init__(self):
         self._map = {}
 
-    def addWs(self, wsname):
-        """ Takes in the given workspace and lists it alongside its time regime value.
-        If the time regime has yet to be created, it will create it, and if there is already
-        a workspace listed beside the time regime, then the new ws will be appended
-        to that list.
+    def addWs(self, ws_name):
         """
-        ws=mtd[wsname]
+        Takes in the given workspace and lists it alongside its time regime
+        value.  If the time regime has yet to be created, it will create it,
+        and if there is already a workspace listed beside the time regime, then
+        the new ws will be appended to that list.
+        """
+        ws = mtd[ws_name]
+
         # Get the time regime of the workspace, and use it to find the DRange.
-        timeRegime = ws.dataX(0)[0]
-        try:
-            dRange = timeRegimeToDRange[timeRegime]
-        except KeyError:
-            raise RuntimeError("Unable to identify the DRange of " + wsname +\
-                ", which has a time regime of " + str(timeRegime))
+        time_regime = ws.dataX(0)[0]
+        time_regimes = time_regime_to_d_range.keys()
+        time_regimes.sort()
+
+        for idx in range(len(time_regimes)):
+            if idx == len(time_regimes) - 1:
+                if time_regimes[idx] < time_regime:
+                    time_regime = time_regimes[idx]
+                    break
+            else:
+                if time_regimes[idx] < time_regime < time_regimes[idx + 1]:
+                    time_regime = time_regimes[idx]
+                    break
+
+        d_range = time_regime_to_d_range[time_regime]
+        logger.information('dRange for workspace %s is %s' % (ws_name, str(d_range)))
 
         # Add the workspace to the map, alongside its DRange.
-        if dRange not in self._map:
-            self._map[dRange] = [wsname]
+        if d_range not in self._map:
+            self._map[d_range] = [ws_name]
         else:
             #check if x ranges matchs and existing run
-            for ws_name in self._map[dRange]:
+            for ws_name in self._map[d_range]:
                 map_lastx = mtd[ws_name].readX(0)[-1]
                 ws_lastx = ws.readX(0)[-1]
 
@@ -55,20 +70,24 @@ class DRangeToWsMap(object):
                     DeleteWorkspace(ws)
                     return
 
-            self._map[dRange].append(wsname)
+            self._map[d_range].append(ws_name)
 
-    def setItem(self, dRange, wsname):
-        """ Set a dRange and corresponding *single* ws.
+    def setItem(self, d_range, ws_name):
         """
-        self._map[dRange] = wsname
+        Set a dRange and corresponding *single* ws.
+        """
+        self._map[d_range] = ws_name
 
     def getMap(self):
-        """ Get access to wrapped map.
+        """
+        Get access to wrapped map.
         """
         return self._map
 
+
 def averageWsList(wsList):
-    """ Returns the average of a list of workspaces.
+    """
+    Returns the average of a list of workspaces.
     """
     # Assert we have some ws in the list, and if there is only one then return it.
     assert len(wsList) > 0, "getAverageWs: Trying to take an average of nothing."
@@ -93,32 +112,36 @@ def averageWsList(wsList):
     RenameWorkspace(InputWorkspace=__temp_avg, OutputWorkspace=avName)
     return avName
 
+
 def findIntersectionOfTwoRanges(rangeA, rangeB):
 
     assert rangeA[0] < rangeA[1], "Malformed range."
     assert rangeB[0] < rangeB[1], "Malformed range."
 
-    if  rangeA[0] <= rangeA[1] <= rangeB[0] <= rangeB[1]:
+    if rangeA[0] <= rangeA[1] <= rangeB[0] <= rangeB[1]:
         return
-    if  rangeB[0] <= rangeB[1] <= rangeA[0] <= rangeA[1]:
+    if rangeB[0] <= rangeB[1] <= rangeA[0] <= rangeA[1]:
         return
-    if  rangeA[0] <= rangeB[0] <= rangeB[1] <= rangeA[1] :
+    if rangeA[0] <= rangeB[0] <= rangeB[1] <= rangeA[1] :
         return rangeB
-    if  rangeB[0] <= rangeA[0] <= rangeA[1] <= rangeB[1] :
+    if rangeB[0] <= rangeA[0] <= rangeA[1] <= rangeB[1] :
         return rangeA
-    if  rangeA[0] <= rangeB[0] <= rangeA[1] <= rangeB[1] :
+    if rangeA[0] <= rangeB[0] <= rangeA[1] <= rangeB[1] :
         return [rangeB[0], rangeA[1]]
-    if  rangeB[0] <= rangeA[0] <= rangeB[1] <= rangeA[1] :
+    if rangeB[0] <= rangeA[0] <= rangeB[1] <= rangeA[1] :
         return [rangeA[0], rangeB[1]]
 
     assert False, "We should never reach here ..."
 
-def getIntersectionsOfRanges(rangeList):
-    """ Get the intersections of a list of ranges.  For example, given the ranges:
-    [1, 3], [3, 5] and [4, 6], the intersections would be a single range of [4, 5].
 
-    NOTE: Assumes that no more than a maximum of two ranges will ever cross
-    at the same point.  Also, all ranges should obey range[0] <= range[1].
+def getIntersectionsOfRanges(rangeList):
+    """
+    Get the intersections of a list of ranges.  For example, given the ranges:
+    [1, 3], [3, 5] and [4, 6], the intersections would be a single range of [4,
+    5].
+
+    NOTE: Assumes that no more than a maximum of two ranges will ever cross at
+    the same point.  Also, all ranges should obey range[0] <= range[1].
     """
     # Sanity check.
     for myrange in rangeList:
@@ -136,14 +159,17 @@ def getIntersectionsOfRanges(rangeList):
     intersections.sort()
     return intersections
 
+
 def isInRanges(rangeList, n):
     for myrange in rangeList:
         if myrange[0] < n < myrange[1]:
             return True
     return False
 
+
 class OSIRISDiffractionReduction(PythonAlgorithm):
-    """ Handles the reduction of OSIRIS Diffraction Data.
+    """
+    Handles the reduction of OSIRIS Diffraction Data.
     """
 
     _cal = None
@@ -153,19 +179,23 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
     _samMap = None
     _vanMap = None
 
+
     def category(self):
         return 'Diffraction;PythonAlgorithms'
 
     def summary(self):
         return "This Python algorithm performs the operations necessary for the reduction of diffraction data "+\
-                "from the Osiris instrument at ISIS "+\
-                "into dSpacing, by correcting for the monitor and linking the various d-ranges together."
+               "from the Osiris instrument at ISIS "+\
+               "into dSpacing, by correcting for the monitor and linking the various d-ranges together."
+
 
     def PyInit(self):
         runs_desc='The list of run numbers that are part of the sample run. '+\
                   'There should be five of these in most cases. Enter them as comma separated values.'
+
         self.declareProperty('Sample', '', doc=runs_desc)
         self.declareProperty('Vanadium', '', doc=runs_desc)
+
         self.declareProperty(FileProperty('CalFile', '', action=FileAction.Load),
                              doc='Filename of the .cal file to use in the [[AlignDetectors]] and '+\
                                  '[[DiffractionFocussing]] child algorithms.')
@@ -179,6 +209,7 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
         self._samMap = DRangeToWsMap()
         self._vanMap = DRangeToWsMap()
 
+
     def PyExec(self):
         # Set OSIRIS as default instrument.
         config["default.instrument"] = 'OSIRIS'
@@ -189,6 +220,7 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
         sampleRuns = self.findRuns(self.getPropertyValue("Sample"))
 
         self.execDiffOnly(sampleRuns)
+
 
     #pylint: disable=too-many-branches
     def execDiffOnly(self, sampleRuns):
@@ -201,7 +233,7 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
 
         # Load all sample and vanadium files, and add the resulting workspaces to the DRangeToWsMaps.
         for fileName in self._sams + self._vans:
-            Load(Filename=fileName, OutputWorkspace=fileName, SpectrumMin=3, SpectrumMax=962)
+            Load(Filename=fileName, OutputWorkspace=fileName, SpectrumMin=3)#, SpectrumMax=962)
         for sam in self._sams:
             self._samMap.addWs(sam)
         for van in self._vans:
@@ -287,11 +319,14 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
 
         self.setProperty("OutputWorkspace", result)
 
+
     def findRuns(self, run_str):
         """
-           Use the FileFinder to find search for the runs given by the string of comma-separated run numbers
-           @param run_str A string of run numbers to find
-           @returns A list of filepaths
+        Use the FileFinder to find search for the runs given by the string of
+        comma-separated run numbers.
+
+        @param run_str A string of run numbers to find
+        @returns A list of filepaths
         """
         runs = run_str.split(",")
         run_files = []
@@ -303,26 +338,32 @@ class OSIRISDiffractionReduction(PythonAlgorithm):
 
         return run_files
 
+
     def rebinToSmallest(self, samWS, vanWS):
         """
-            At some point a change to the control program
-            meant that the raw data got an extra bin. This
-            prevents runs past this point being normalised
-            with a vanadium from an earlier point.
-            Here we simply rebin to the smallest workspace if
-            the sizes don't match
-            @param samWS A workspace object containing the sample run
-            @param vanWS A workspace object containing the vanadium run
-            @returns samWS, vanWS rebinned  to the smallest if necessary
+        At some point a change to the control program meant that the raw data
+        got an extra bin. This prevents runs past this point being normalised
+        with a vanadium from an earlier point.  Here we simply rebin to the
+        smallest workspace if the sizes don't match
+
+        @param samWS A workspace object containing the sample run
+        @param vanWS A workspace object containing the vanadium run
+        @returns samWS, vanWS rebinned  to the smallest if necessary
         """
         sample_size, van_size = mtd[samWS].blocksize(), mtd[vanWS].blocksize()
         if sample_size == van_size:
             return samWS, vanWS
 
         if sample_size < van_size:
-            RebinToWorkspace(WorkspaceToRebin=vanWS, WorkspaceToMatch=samWS,OutputWorkspace=vanWS)
+            # Rebin vanadium to match sample
+            RebinToWorkspace(WorkspaceToRebin=vanWS,
+                             WorkspaceToMatch=samWS,
+                             OutputWorkspace=vanWS)
         else:
-            RebinToWorkspace(WorkspaceToRebin=samWS, WorkspaceToMatch=vanWS,OutputWorkspace=samWS)
+            # Rebin sample to match vanadium
+            RebinToWorkspace(WorkspaceToRebin=samWS,
+                             WorkspaceToMatch=vanWS,
+                             OutputWorkspace=samWS)
 
         return samWS, vanWS
 
