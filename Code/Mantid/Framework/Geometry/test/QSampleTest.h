45b960a63f8c56a849c347604b14af7f745a7bd8
Owen Arnold <owen.arnold@stfc.ac.uk>
2015-07-13 17:21:17 +0100

    refs #12185. QSample MDFrame added.

--- Code/Mantid/Framework/Geometry/test/QSampleTest.h
+++ Code/Mantid/Framework/Geometry/test/QSampleTest.h
@@ -1,41 +1,43 @@
-#ifndef MANTID_GEOMETRY_QLABTEST_H_
-#define MANTID_GEOMETRY_QLABTEST_H_
+#ifndef MANTID_GEOMETRY_QSAMPLETEST_H_
+#define MANTID_GEOMETRY_QSAMPLETEST_H_
 
 #include <cxxtest/TestSuite.h>
-
 #include "MantidKernel/MDUnit.h"
-#include "MantidGeometry/MDGeometry/QLab.h"
+#include "MantidKernel/UnitLabelTypes.h"
+#include "MantidGeometry/MDGeometry/QSample.h"
 
-using Mantid::Geometry::QLab;
+using Mantid::Geometry::QSample;
 using namespace Mantid::Kernel;
 
-class QLabTest : public CxxTest::TestSuite {
+class QSampleTest : public CxxTest::TestSuite {
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
-  static QLabTest *createSuite() { return new QLabTest(); }
-  static void destroySuite( QLabTest *suite ) { delete suite; }
+  static QSampleTest *createSuite() { return new QSampleTest(); }
+  static void destroySuite( QSampleTest *suite ) { delete suite; }
 
 
   void test_name()
   {
-    QLab frame;
-    TS_ASSERT_EQUALS(QLab::QLabName, frame.name());
+    QSample frame;
+    TS_ASSERT_EQUALS(QSample::QSampleName, frame.name());
   }
 
   void test_canConvertTo_unit(){
-    QLab frame;
+    QSample frame;
     InverseAngstromsUnit unit;
     TSM_ASSERT("Same unit type as is used for QLab", frame.canConvertTo(unit));
   }
 
   void test_cannotConvertTo_unit(){
-    QLab frame;
+    QSample frame;
     ReciprocalLatticeUnit unit;
     TSM_ASSERT("Not same unit type as is used for QLab", !frame.canConvertTo(unit));
   }
 
+
+
 };
 
 
-#endif /* MANTID_GEOMETRY_QLABTEST_H_ */
+#endif /* MANTID_GEOMETRY_QSAMPLETEST_H_ */
