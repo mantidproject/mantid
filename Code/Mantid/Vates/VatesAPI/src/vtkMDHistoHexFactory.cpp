5d940c3d9ad10cf631fda2040bc472afbbea321d
Steven Hahn <hahnse@ornl.gov>
2015-07-14 16:45:33 -0400

    Refs #12396. Reuse MDHistoWorkspace signal array.

--- Code/Mantid/Vates/VatesAPI/src/vtkMDHistoHexFactory.cpp
+++ Code/Mantid/Vates/VatesAPI/src/vtkMDHistoHexFactory.cpp
@@ -3,6 +3,7 @@
 #include "MantidDataObjects/MDHistoWorkspace.h"
 #include "MantidDataObjects/MDHistoWorkspaceIterator.h"
 
+#include "MantidVatesAPI/vtkMDHWSignalArray.h"
 #include "MantidVatesAPI/vtkStructuredPointsArray.h"
 #include "MantidVatesAPI/Common.h"
 #include "MantidVatesAPI/Normalization.h"
@@ -106,29 +107,13 @@ vtkMDHistoHexFactory::create3Dor4D(size_t timestep,
   const int nBinsY = static_cast<int>(m_workspace->getYDimension()->getNBins());
   const int nBinsZ = static_cast<int>(m_workspace->getZDimension()->getNBins());
 
-  const coord_t maxX = m_workspace->getXDimension()->getMaximum();
-  const coord_t minX = m_workspace->getXDimension()->getMinimum();
-  const coord_t maxY = m_workspace->getYDimension()->getMaximum();
-  const coord_t minY = m_workspace->getYDimension()->getMinimum();
-  const coord_t maxZ = m_workspace->getZDimension()->getMaximum();
-  const coord_t minZ = m_workspace->getZDimension()->getMinimum();
-
-  coord_t incrementX = (maxX - minX) / static_cast<coord_t>(nBinsX);
-  coord_t incrementY = (maxY - minY) / static_cast<coord_t>(nBinsY);
-  coord_t incrementZ = (maxZ - minZ) / static_cast<coord_t>(nBinsZ);
-
   const int imageSize = (nBinsX) * (nBinsY) * (nBinsZ);
   
   vtkNew<vtkStructuredGrid> visualDataSet;
   visualDataSet->SetDimensions(nBinsX+1,nBinsY+1,nBinsZ+1);
 
-  const int nPointsX = nBinsX+1;
-  const int nPointsY = nBinsY+1;
-  const int nPointsZ = nBinsZ+1;
-
   // Array with true where the voxel should be shown
   double progressFactor = 0.5 / double(imageSize);
-  double progressOffset = 0.5;
 
   std::size_t offset = 0;
   if (nDims == 4)
@@ -136,101 +121,30 @@ vtkMDHistoHexFactory::create3Dor4D(size_t timestep,
     offset = timestep * indexMultiplier[2];
   }
 
-  if(m_normalizationOption == VATES::NoNormalization)
-  {
-    vtkNew<vtkDoubleArray> signal;
-    signal->SetName(vtkDataSetFactory::ScalarName.c_str());
-    signal->SetArray(m_workspace->getSignalArray()+offset,imageSize,1);
+  std::unique_ptr<MDHistoWorkspaceIterator> iterator(
+      dynamic_cast<MDHistoWorkspaceIterator *>(createIteratorWithNormalization(
+          m_normalizationOption, m_workspace.get())));
 
-    for (vtkIdType index = 0; index < imageSize; ++index) {
-      progressUpdate.eventRaised(double(index) * progressFactor);
-      double signalScalar = signal->GetValue(index);
-      bool maskValue = (isSpecial( signalScalar ) || !m_thresholdRange->inRange(signalScalar));
-      if (maskValue)
-      {
-        visualDataSet->BlankCell(index);
-      }
+  vtkNew<vtkMDHWSignalArray<double>> signal;
+
+  signal->SetName(vtkDataSetFactory::ScalarName.c_str());
+  signal->InitializeArray(std::move(iterator), offset, imageSize);
+  visualDataSet->GetCellData()->SetScalars(signal.GetPointer());
+
+  for (vtkIdType index = 0; index < imageSize; ++index) {
+    progressUpdate.eventRaised(double(index) * progressFactor);
+    double signalScalar = signal->GetValue(index);
+    bool maskValue =
+        (isSpecial(signalScalar) || !m_thresholdRange->inRange(signalScalar));
+    if (maskValue) {
+      visualDataSet->BlankCell(index);
     }
-
-    visualDataSet->GetCellData()->SetScalars(signal.GetPointer());
   }
-  else
-  {
-    vtkNew<vtkFloatArray> signal;
-    signal->SetName(vtkDataSetFactory::ScalarName.c_str());
-    signal->Allocate(static_cast<int>(imageSize));
-
-    boost::scoped_ptr<MDHistoWorkspaceIterator> iterator(
-           dynamic_cast<MDHistoWorkspaceIterator *>(createIteratorWithNormalization(
-           m_normalizationOption, m_workspace.get())));
-    iterator->jumpTo(offset);
-    for (vtkIdType index = 0; index < imageSize; ++index) {
-      progressUpdate.eventRaised(double(index) * progressFactor);
-      // Normalized by the requested method applied above.
-      const double signalScalar = iterator->getNormalizedSignal();
-      signal->InsertNextValue(signalScalar);
-      bool maskValue = (isSpecial( signalScalar ) || !m_thresholdRange->inRange(signalScalar));
-      if (maskValue)
-      {
-        visualDataSet->BlankCell(index);
-      }
-      iterator->next();
-    }
-    visualDataSet->GetCellData()->SetScalars(signal.GetPointer());
-  }
-
-  /*vtkNew<vtkPoints> points;
-  points->Allocate(static_cast<int>(imageSize));
-  //Get the transformation that takes the points in the TRANSFORMED space back
-  //into the ORIGINAL (not-rotated) space.
-  Mantid::API::CoordTransform const *transform = NULL;
-  if (m_useTransform)
-    transform = m_workspace->getTransformToOriginal();
-
-  Mantid::coord_t in[3];
-  Mantid::coord_t out[3];
-
-  progressFactor = 0.5 / static_cast<double>(nPointsZ);
-
-  for (int z = 0; z < nPointsZ; z++) {
-    // Report progress updates for the last 50%
-    progressUpdate.eventRaised(double(z) * progressFactor + progressOffset);
-    in[2] = (minZ + (static_cast<coord_t>(z) *
-                     incrementZ)); // Calculate increment in z;
-    for (int y = 0; y < nPointsY; y++) {
-      in[1] = (minY + (static_cast<coord_t>(y) *
-                       incrementY)); // Calculate increment in y;
-      for (int x = 0; x < nPointsX; x++) {
-        // Create the point only when needed
-        in[0] = (minX + (static_cast<coord_t>(x) * incrementX)); // Calculate increment in x;
-        if (transform)
-        {
-            transform->apply(in, out);
-            points->InsertNextPoint(out);
-        }
-        else
-        {
-            points->InsertNextPoint(in);
-        }
-      }
-    }
-  }*/
     
   vtkNew<vtkStructuredPointsArray<double>> implicitPoints;
   implicitPoints->InitializeArray(m_workspace.get(),m_useTransform);
   vtkNew<vtkPoints> newPoints;
   newPoints->SetData(implicitPoints.GetPointer());
-  /*for ( auto i = 0; i < points->GetNumberOfPoints();++i)
-    {
-      double* oldPoint = points->GetPoint(i);
-      double* newPoint = newPoints->GetPoint(i);
-      for (auto j = 0; j < 3;++j)
-      {
-        if (std::abs(newPoint[j]-oldPoint[j])>1.0e-4)
-          std::cout << "error at point #" << i << std::endl;
-      }
-    }*/
-  
     
   visualDataSet->SetPoints(newPoints.GetPointer());
   visualDataSet->Register(NULL);
