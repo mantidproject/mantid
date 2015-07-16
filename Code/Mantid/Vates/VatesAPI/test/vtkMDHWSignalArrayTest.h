e94d871510dfeda0ae8c5b43d7605888c95bc2bb
Steven Hahn <hahnse@ornl.gov>
2015-07-16 18:40:55 -0400

    Refs #12396. Work on vtkMDHWSignalArrayTest.

--- Code/Mantid/Vates/VatesAPI/test/vtkMDHWSignalArrayTest.h
+++ Code/Mantid/Vates/VatesAPI/test/vtkMDHWSignalArrayTest.h
@@ -1,8 +1,11 @@
-#ifndef VTKNULLSTRUCTUREDPOINTSARRAY_TEST_H_
-#define VTKNULLSTRUCTUREDPOINTSARRAY_TEST_H_
+#ifndef VTKNULLMDHWSIGNALARRAY_TEST_H_
+#define VTKNULLMDHWSIGNALARRAY_TEST_H_
 
+#include "MantidVatesAPI/Normalization.h"
 #include "MantidDataObjects/MDHistoWorkspace.h"
+#include "MantidDataObjects/MDHistoWorkspaceIterator.h"
 #include "MantidTestHelpers/MDEventsTestHelper.h"
+#include "MantidVatesAPI/Normalization.h"
 #include "MantidVatesAPI/vtkMDHWSignalArray.h"
 
 #include "MockObjects.h"
@@ -21,196 +24,168 @@
 using namespace Mantid::VATES;
 using namespace Mantid::DataObjects;
 
+/*
+ void InitializeArray(
+ std::unique_ptr<Mantid::DataObjects::MDHistoWorkspaceIterator> iterator,
+ std::size_t offset, vtkIdType size);
+
+ // Reimplemented virtuals -- see superclasses for descriptions:
+ void Initialize();
+ void GetTuples(vtkIdList *ptIds, vtkAbstractArray *output);
+ void GetTuples(vtkIdType p1, vtkIdType p2, vtkAbstractArray *output);
+ void Squeeze();
+ vtkArrayIterator *NewIterator();
+ vtkIdType LookupValue(vtkVariant value);
+ void LookupValue(vtkVariant value, vtkIdList *ids);
+ DONE: vtkVariant GetVariantValue(vtkIdType idx);
+ NOT_IMPLEMENTED void ClearLookup();
+ DONE: double *GetTuple(vtkIdType i);
+ DONE: void GetTuple(vtkIdType i, double *tuple);
+ vtkIdType LookupTypedValue(Scalar value);
+ void LookupTypedValue(Scalar value, vtkIdList *ids);
+ DONE: Scalar GetValue(vtkIdType idx);
+ DONE: Scalar &GetValueReference(vtkIdType idx);
+ DONE: void GetTupleValue(vtkIdType idx, Scalar *t);
+ */
+
 class vtkMDHWSignalArrayTest : public CxxTest::TestSuite {
 public:
   void testGetTuple() {
     MDHistoWorkspace_sptr ws_sptr =
         MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
-    vtkNew<vtkMDHWSignalArray<double>> spa;
-    spa->InitializeArray(ws_sptr.get());
+    vtkNew<vtkMDHWSignalArray<double>> signal;
+    std::size_t offset = 0;
+    const int nBinsX = static_cast<int>(ws_sptr->getXDimension()->getNBins());
+    const int nBinsY = static_cast<int>(ws_sptr->getYDimension()->getNBins());
+    const int nBinsZ = static_cast<int>(ws_sptr->getZDimension()->getNBins());
+    const int imageSize = (nBinsX) * (nBinsY) * (nBinsZ);
 
-    long long dims[3];
-    dims[0] = ws_sptr->getXDimension()->getNBins() + 1;
-    dims[1] = ws_sptr->getYDimension()->getNBins() + 1;
-    dims[2] = ws_sptr->getZDimension()->getNBins() + 1;
+    std::unique_ptr<MDHistoWorkspaceIterator> iterator(
+        dynamic_cast<MDHistoWorkspaceIterator *>(
+            createIteratorWithNormalization(Mantid::VATES::NoNormalization,
+                                            ws_sptr.get())));
+    signal->InitializeArray(std::move(iterator), offset, imageSize);
 
-    auto index = 0;
-    for (auto k = 0; k < dims[2]; ++k) {
-      for (auto j = 0; j < dims[1]; ++j) {
-        for (auto i = 0; i < dims[0]; ++i) {
-          double output1[3];
-          spa->GetTuple(index, output1);
-          TS_ASSERT_DELTA(double(i), output1[0], 0.0001);
-          TS_ASSERT_DELTA(double(j), output1[1], 0.0001);
-          TS_ASSERT_DELTA(double(k), output1[2], 0.0001);
+    for (auto index = 0; index < imageSize; ++index) {
+      double output1[1];
+      signal->GetTuple(index, output1);
+      TS_ASSERT_DELTA(1.0, output1[0], 0.0001);
 
-          // test alternate member function.
-          double *output2 = spa->GetTuple(index);
-          TS_ASSERT_DELTA(double(i), output2[0], 0.0001);
-          TS_ASSERT_DELTA(double(j), output2[1], 0.0001);
-          TS_ASSERT_DELTA(double(k), output2[2], 0.0001);
+      // test alternate member function.
+      double *output2 = signal->GetTuple(index);
+      TS_ASSERT_DELTA(1.0, output2[0], 0.0001);
 
-          // test alternate member function.
-          double output3[3];
-          spa->GetTupleValue(index, output3);
-          TS_ASSERT_DELTA(double(i), output3[0], 0.0001);
-          TS_ASSERT_DELTA(double(j), output3[1], 0.0001);
-          TS_ASSERT_DELTA(double(k), output3[2], 0.0001);
-          index++;
-        }
-      }
-    }
-  }
+      // test alternate member function.
+      double output3[1];
+      signal->GetTupleValue(index, output3);
+      TS_ASSERT_DELTA(1.0, output3[0], 0.0001);
 
-  void testGetValue() {
-    MDHistoWorkspace_sptr ws_sptr =
-        MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
-    vtkNew<vtkMDHWSignalArray<double>> spa;
-    spa->InitializeArray(ws_sptr.get());
+      // test alternate member function.
+      TS_ASSERT_DELTA(1.0, signal->GetValue(index), 0.0001);
 
-    long long dims[3];
-    dims[0] = ws_sptr->getXDimension()->getNBins() + 1;
-    dims[1] = ws_sptr->getYDimension()->getNBins() + 1;
-    dims[2] = ws_sptr->getZDimension()->getNBins() + 1;
+      // test alternate member function.
+      TS_ASSERT_DELTA(1.0, signal->GetValueReference(index), 0.0001);
 
-    auto index = 0;
-    for (auto k = 0; k < dims[2]; ++k) {
-      for (auto j = 0; j < dims[1]; ++j) {
-        for (auto i = 0; i < dims[0]; ++i) {
-          double value = spa->GetValue(index);
-          TS_ASSERT_DELTA(double(i), value, 0.0001);
-          index++;
-          value = spa->GetValue(index);
-          TS_ASSERT_DELTA(double(j), value, 0.0001);
-          index++;
-          value = spa->GetValue(index);
-          TS_ASSERT_DELTA(double(k), value, 0.0001);
-          index++;
-        }
-      }
-    }
-
-    index = 0;
-    for (auto k = 0; k < dims[2]; ++k) {
-      for (auto j = 0; j < dims[1]; ++j) {
-        for (auto i = 0; i < dims[0]; ++i) {
-          double value = spa->GetValueReference(index);
-          TS_ASSERT_DELTA(double(i), value, 0.0001);
-          index++;
-          value = spa->GetValueReference(index);
-          TS_ASSERT_DELTA(double(j), value, 0.0001);
-          index++;
-          value = spa->GetValueReference(index);
-          TS_ASSERT_DELTA(double(k), value, 0.0001);
-          index++;
-        }
-      }
-    }
-
-    index = 0;
-    for (auto k = 0; k < dims[2]; ++k) {
-      for (auto j = 0; j < dims[1]; ++j) {
-        for (auto i = 0; i < dims[0]; ++i) {
-          vtkVariant value = spa->GetVariantValue(index);
-          TS_ASSERT_DELTA(double(i), value.ToDouble(), 0.0001);
-          index++;
-          value = spa->GetVariantValue(index);
-          TS_ASSERT_DELTA(double(j), value.ToDouble(), 0.0001);
-          index++;
-          value = spa->GetVariantValue(index);
-          TS_ASSERT_DELTA(double(k), value.ToDouble(), 0.0001);
-          index++;
-        }
-      }
+      // test alternate member function.
+      vtkVariant value = signal->GetVariantValue(index);
+      TS_ASSERT_DELTA(1.0, value.ToDouble(), 0.0001);
     }
   }
 
   void testGetTuplesPtIds() {
+
     MDHistoWorkspace_sptr ws_sptr =
         MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
-    vtkNew<vtkMDHWSignalArray<double>> spa;
-    spa->InitializeArray(ws_sptr.get());
+    vtkNew<vtkMDHWSignalArray<double>> signal;
+    std::size_t offset = 0;
+    const int nBinsX = static_cast<int>(ws_sptr->getXDimension()->getNBins());
+    const int nBinsY = static_cast<int>(ws_sptr->getYDimension()->getNBins());
+    const int nBinsZ = static_cast<int>(ws_sptr->getZDimension()->getNBins());
+    const int imageSize = (nBinsX) * (nBinsY) * (nBinsZ);
 
-    long long dims[3];
-    dims[0] = ws_sptr->getXDimension()->getNBins() + 1;
-    dims[1] = ws_sptr->getYDimension()->getNBins() + 1;
-    dims[2] = ws_sptr->getZDimension()->getNBins() + 1;
+    std::unique_ptr<MDHistoWorkspaceIterator> iterator(
+        dynamic_cast<MDHistoWorkspaceIterator *>(
+            createIteratorWithNormalization(Mantid::VATES::NoNormalization,
+                                            ws_sptr.get())));
+    signal->InitializeArray(std::move(iterator), offset, imageSize);
 
     vtkNew<vtkIdList> ptIds;
 
-    for (auto idx = 0; idx < dims[0] * dims[1] * dims[2]; idx = idx + 4) {
+    for (auto idx = 0; idx < imageSize; idx = idx + 4) {
       ptIds->InsertNextId(idx);
     }
 
     vtkNew<vtkDoubleArray> doubleArray;
-    doubleArray->SetNumberOfComponents(3);
-    doubleArray->Allocate(333);
-    spa->GetTuples(ptIds.GetPointer(), doubleArray.GetPointer());
+    doubleArray->SetNumberOfComponents(1);
+    doubleArray->Allocate(imageSize / 4);
+    signal->GetTuples(ptIds.GetPointer(), doubleArray.GetPointer());
 
-    for (auto idx = 0; idx < dims[0] * dims[1] * dims[2] / 4; ++idx) {
-      double output1[3], output2[3];
-      spa->GetTupleValue(idx * 4, output1);
+    for (auto idx = 0; idx < imageSize / 4; ++idx) {
+      double output1[1], output2[1];
+      signal->GetTupleValue(idx * 4, output1);
       doubleArray->GetTupleValue(idx, output2);
       TS_ASSERT_DELTA(output1[0], output2[0], 0.0001);
-      TS_ASSERT_DELTA(output1[1], output2[1], 0.0001);
-      TS_ASSERT_DELTA(output1[2], output2[2], 0.0001);
     }
   }
 
   void testGetTuplesRange() {
     MDHistoWorkspace_sptr ws_sptr =
         MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
-    vtkNew<vtkMDHWSignalArray<double>> spa;
-    spa->InitializeArray(ws_sptr.get());
+    vtkNew<vtkMDHWSignalArray<double>> signal;
+    std::size_t offset = 0;
+    const int nBinsX = static_cast<int>(ws_sptr->getXDimension()->getNBins());
+    const int nBinsY = static_cast<int>(ws_sptr->getYDimension()->getNBins());
+    const int nBinsZ = static_cast<int>(ws_sptr->getZDimension()->getNBins());
+    const int imageSize = (nBinsX) * (nBinsY) * (nBinsZ);
 
-    long long dims[3];
-    dims[0] = ws_sptr->getXDimension()->getNBins() + 1;
-    dims[1] = ws_sptr->getYDimension()->getNBins() + 1;
-    dims[2] = ws_sptr->getZDimension()->getNBins() + 1;
+    std::unique_ptr<MDHistoWorkspaceIterator> iterator(
+        dynamic_cast<MDHistoWorkspaceIterator *>(
+            createIteratorWithNormalization(Mantid::VATES::NoNormalization,
+                                            ws_sptr.get())));
+    signal->InitializeArray(std::move(iterator), offset, imageSize);
 
     vtkNew<vtkDoubleArray> doubleArray;
-    doubleArray->SetNumberOfComponents(3);
+    doubleArray->SetNumberOfComponents(1);
     doubleArray->Allocate(100);
-    spa->GetTuples(0, 100, doubleArray.GetPointer());
+    signal->GetTuples(0, 100, doubleArray.GetPointer());
 
     for (auto idx = 0; idx < 100; ++idx) {
-      double output1[3], output2[3];
-      spa->GetTupleValue(idx, output1);
+      double output1[1], output2[1];
+      signal->GetTupleValue(idx, output1);
       doubleArray->GetTupleValue(idx, output2);
       TS_ASSERT_DELTA(output1[0], output2[0], 0.0001);
-      TS_ASSERT_DELTA(output1[1], output2[1], 0.0001);
-      TS_ASSERT_DELTA(output1[2], output2[2], 0.0001);
     }
   }
-
-  void testLookupOneValue() {
-    MDHistoWorkspace_sptr ws_sptr =
-        MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
-    vtkNew<vtkMDHWSignalArray<double>> spa;
-    spa->InitializeArray(ws_sptr.get());
-    TS_ASSERT(spa->LookupValue(1.0) == 3);
-    TS_ASSERT(spa->LookupTypedValue(1.0) == 3);
-  }
-
-  void testLookupAllValues() {
-    MDHistoWorkspace_sptr ws_sptr =
-        MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
-    vtkNew<vtkMDHWSignalArray<double>> spa;
-    spa->InitializeArray(ws_sptr.get());
-    vtkNew<vtkIdList> idList1, idList2;
-    spa->LookupValue(0.0, idList1.GetPointer());
-    for (vtkIdType i = 0; i < idList1->GetNumberOfIds(); ++i) {
-      TS_ASSERT(spa->GetValue(idList1->GetId(i)) == 0.0);
+  /*
+    void xtestLookupOneValue() {
+      MDHistoWorkspace_sptr ws_sptr =
+          MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
+      vtkNew<vtkMDHWSignalArray<double>> spa;
+      spa->InitializeArray(ws_sptr.get());
+      TS_ASSERT(spa->LookupValue(1.0) == 3);
+      TS_ASSERT(spa->LookupTypedValue(1.0) == 3);
     }
 
-    spa->LookupTypedValue(1.0, idList2.GetPointer());
-    for (vtkIdType i = 0; i < idList2->GetNumberOfIds(); ++i) {
-      TS_ASSERT(spa->GetValue(idList2->GetId(i)) == 1.0);
+    void xtestLookupAllValues() {
+      MDHistoWorkspace_sptr ws_sptr =
+          MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, 3);
+      vtkNew<vtkMDHWSignalArray<double>> spa;
+      spa->InitializeArray(ws_sptr.get());
+      vtkNew<vtkIdList> idList1, idList2;
+      spa->LookupValue(0.0, idList1.GetPointer());
+      for (vtkIdType i = 0; i < idList1->GetNumberOfIds(); ++i) {
+        TS_ASSERT(spa->GetValue(idList1->GetId(i)) == 0.0);
+      }
+
+      spa->LookupTypedValue(1.0, idList2.GetPointer());
+      for (vtkIdType i = 0; i < idList2->GetNumberOfIds(); ++i) {
+        TS_ASSERT(spa->GetValue(idList2->GetId(i)) == 1.0);
+      }
     }
-  }
+    */
 };
 
-class vtkMDHWSignalArrayTestPerformance : public CxxTest::TestSuite {
+/*class vtkMDHWSignalArrayTestPerformance : public CxxTest::TestSuite {
 public:
   long long dims[3];
   MDHistoWorkspace_sptr ws_sptr;
@@ -226,7 +201,7 @@ public:
 
   void tearDown() {}
 
-  void testGetTupleValuePerformance() {
+  void xtestGetTupleValuePerformance() {
     auto index = 0;
     for (auto k = 0; k < dims[2]; ++k) {
       for (auto j = 0; j < dims[1]; ++j) {
@@ -241,4 +216,5 @@ public:
     }
   }
 };
+ */
 #endif
