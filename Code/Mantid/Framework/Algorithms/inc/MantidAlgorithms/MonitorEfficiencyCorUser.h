57e62192d5732ef3908a92afee5e60eaee556860
Celine Durniak <c.durniak@fz-juelich.de>
2015-07-20 20:50:55 +0200

    Update category of MonitorEfficiencyCorUser

--- Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/MonitorEfficiencyCorUser.h
+++ Code/Mantid/Framework/Algorithms/inc/MantidAlgorithms/MonitorEfficiencyCorUser.h
@@ -24,7 +24,7 @@ public:
   /// Algorithm's version
   virtual int version() const { return 1; }
   /// Algorithm's category for identification
-  virtual const std::string category() const { return "MLZ;CorrectionFunctions\\NormalisationCorrections"; }
+  virtual const std::string category() const { return "CorrectionFunctions\\NormalisationCorrections"; }
 
 private:
   /// Initialisation code
