92d5449e9ad8e4630e2598869b4fb45c4ad5bb78
Matt King <matthew.king@stfc.ac.uk>
2015-07-21 12:23:05 +0100

    Changed code to comply with pyLint warnings
    changed variable names that caused complaints,
    trailing white space that caused warnings
    and variable declarations that were on one line to
    be on multiple lines.
    
    Refs #13145

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/MuonicXRD.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/MuonicXRD.py
@@ -2,13 +2,17 @@ from mantid.api import * # PythonAlgorithm, registerAlgorithm, WorkspaceProperty
 
 class GetNegMuMuonicXRD(PythonAlgorithm):
  #Dictionary of <element>:<peaks> easily extendible by user.  
-    Muonic_XR={ 'Au' : [8135.2,8090.6,8105.4,8069.4,5764.89,5594.97,3360.2,3206.8,2474.22,2341.21,2304.44,1436.05,1391.58,1104.9,899.14,869.98,405.654,400.143],
-                       'Ag' : [3184.7,3147.7,901.2,869.2,308.428,304.759],
-                       'Cu' : [1512.78,1506.61,334.8,330.26],
-                       'Zn' : [1600.15,1592.97,360.75,354.29],
-                       'Pb' : [8523.3,8442.11,5966.0,5780.1,2641.8,2499.7,2459.7,1511.63,1214.12,1028.83,972.3,938.4,437.687,431.285],
-                       'As' : [1866.9,1855.8,436.6,427.5],
-                       'Sn' : [3457.3,3412.8,1022.6,982.5,349.953,345.226]}   
+    Muonic_XR={ 'Au' : [8135.2,8090.6,8105.4,8069.4,5764.89,5594.97,3360.2,
+                        3206.8,2474.22,2341.21,2304.44,1436.05,1391.58,1104.9,
+                        899.14,869.98,405.654,400.143],
+                'Ag' : [3184.7,3147.7,901.2,869.2,308.428,304.759],
+                'Cu' : [1512.78,1506.61,334.8,330.26],
+                'Zn' : [1600.15,1592.97,360.75,354.29],
+                'Pb' : [8523.3,8442.11,5966.0,5780.1,2641.8,2499.7,
+                        2459.7,1511.63,1214.12,1028.83,972.3,938.4,
+                        437.687,431.285],
+                'As' : [1866.9,1855.8,436.6,427.5],
+                'Sn' : [3457.3,3412.8,1022.6,982.5,349.953,345.226]}
                        
     def get_Muonic_XR(self, element):
         #retrieve peak values from dictionary Muonic_XR
@@ -19,9 +23,9 @@ class GetNegMuMuonicXRD(PythonAlgorithm):
         #retrieve the values from Muonic_XR
         xr_peak_values = self.get_Muonic_XR(element)
         #Calibrate y-axis for workspace
-        YPos_WS=[YPos]*len(xr_peak_values)
-        X = xr_peak_values
-        WS_Muon_XR= CreateWorkspace(X, YPos_WS[:])
+        YPos_WS = [YPos]*len(xr_peak_values)
+        xvalues = xr_peak_values
+        WS_Muon_XR = CreateWorkspace(xvalues, YPos_WS[:])
         RenameWorkspaces(WS_Muon_XR, WorkspaceNames="MuonXRWorkspace_"+element)
         return WS_Muon_XR
 
@@ -42,7 +46,7 @@ class GetNegMuMuonicXRD(PythonAlgorithm):
 
     def PyExec(self):
         element = self.getProperty("Element").value
-        yposition = self.getProperty("YAxis Position").value 
-        ws=self.Create_MuonicXR_WS(elemenet,yposition)
+        yposition = self.getProperty("YAxis Position").value
+        output_workspace = self.Create_MuonicXR_WS(element,yposition)
 
 AlgorithmFactory.subscribe(GetNegMuMuonicXRD)
