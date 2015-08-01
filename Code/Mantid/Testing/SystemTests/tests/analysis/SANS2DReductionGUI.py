2f007cbd307eb49b0052a3ba173584426b1f954b
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2015-08-01 20:02:30 +0100

    Refs #13213 Fix system test because of IP file change

--- Code/Mantid/Testing/SystemTests/tests/analysis/SANS2DReductionGUI.py
+++ Code/Mantid/Testing/SystemTests/tests/analysis/SANS2DReductionGUI.py
@@ -49,7 +49,7 @@ class SANS2DMinimalBatchReduction(stresstesting.MantidStressTest):
         fit_settings = batch.BatchReduce(BATCHFILE,'.nxs', combineDet='rear')
 
     def validate(self):
-
+        self.disableChecking.append('Instrument')
         return "trans_test_rear","SANSReductionGUI.nxs"
 
 
