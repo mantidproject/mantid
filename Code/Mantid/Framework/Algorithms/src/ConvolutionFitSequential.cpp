edd67fd5a2d21beaef74f081f0b7ac6fa0cc6a3b
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-08-03 09:26:29 +0100

    Algorithm basic credentials and properties defined
    Refs #12419

--- Code/Mantid/Framework/Algorithms/src/ConvolutionFitSequential.cpp
+++ Code/Mantid/Framework/Algorithms/src/ConvolutionFitSequential.cpp
@@ -1,10 +1,11 @@
 #include "MantidAlgorithms/ConvolutionFitSequential.h"
+#include "MantidKernel/MandatoryValidator.h"
 
 namespace Mantid {
 namespace Algorithms {
 
-using Mantid::Kernel::Direction;
-using Mantid::API::WorkspaceProperty;
+using namespace API;
+using namespace Kernel;
 
 // Register the algorithm into the AlgorithmFactory
 DECLARE_ALGORITHM(ConvolutionFitSequential)
@@ -22,19 +23,21 @@ ConvolutionFitSequential::~ConvolutionFitSequential() {}
 //----------------------------------------------------------------------------------------------
 
 /// Algorithms name for identification. @see Algorithm::name
-const std::string ConvolutionFitSequential::name() const { return "ConvolutionFitSequential"; }
+const std::string ConvolutionFitSequential::name() const {
+  return "ConvolutionFitSequential";
+}
 
 /// Algorithm's version for identification. @see Algorithm::version
 int ConvolutionFitSequential::version() const { return 1; }
 
 /// Algorithm's category for identification. @see Algorithm::category
 const std::string ConvolutionFitSequential::category() const {
-  return "TODO: FILL IN A CATEGORY";
+  return "Workflow\\MIDAS";
 }
 
 /// Algorithm's summary for use in the GUI and help. @see Algorithm::summary
 const std::string ConvolutionFitSequential::summary() const {
-  return "TODO: FILL IN A SUMMARY";
+  return "Performs a sequential fit for a convolution workspace";
 }
 
 //----------------------------------------------------------------------------------------------
@@ -43,10 +46,40 @@ const std::string ConvolutionFitSequential::summary() const {
 void ConvolutionFitSequential::init() {
   declareProperty(
       new WorkspaceProperty<>("InputWorkspace", "", Direction::Input),
-      "An input workspace.");
+      "The input workspace for the fit.");
+
+  declareProperty("Function", "",
+                  boost::make_shared<MandatoryValidator<std::string>>(),
+                  "The function that describes the parameters of the fit.",
+                  Direction::Input);
+
+  declareProperty(
+      "Start X", "", boost::make_shared<MandatoryValidator<double>>(),
+      "The start of the range for the fit function.", Direction::Input);
+
+  declareProperty("End X", "", boost::make_shared<MandatoryValidator<double>>(),
+                  "The end of the range for the fit function.",
+                  Direction::Input);
+  //Needs validation
+  declareProperty("Temperature", "", "The Temperature correction for the fit.", Direction::Input);
+
+  declareProperty("Spec Min", 0, boost::make_shared<MandatoryValidator<int>>(),
+                  "The first spectrum to be used in the fit.", Direction::Input);
+
+  declareProperty("Spec Max", 0, boost::make_shared<MandatoryValidator<int>>(),
+                  "The final spectrum to be used in the fit.", Direction::Input);
+
+  declareProperty("Convolve", true,
+                  "If true, the fit is treated as a convolution workspace.",
+                  Direction::Input);
+
+  declareProperty(
+      "Maximum Iterations", 500, boost::make_shared<MandatoryValidator<int>>(),
+      "The maximum number of iterations permitted", Direction::Input);
+
   declareProperty(
       new WorkspaceProperty<>("OutputWorkspace", "", Direction::Output),
-      "An output workspace.");
+      "The ouput workspace for the fit.");
 }
 
 //----------------------------------------------------------------------------------------------
