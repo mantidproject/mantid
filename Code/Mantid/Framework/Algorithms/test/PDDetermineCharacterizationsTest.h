a3e67790bf3252fb8f8024350cc68c0ff646ee7b
Pete Peterson <petersonpf@ornl.gov>
2015-08-04 10:49:27 -0400

    Added test for standard NOMAD characterizations workspace

--- Code/Mantid/Framework/Algorithms/test/PDDetermineCharacterizationsTest.h
+++ Code/Mantid/Framework/Algorithms/test/PDDetermineCharacterizationsTest.h
@@ -31,7 +31,7 @@ public:
     delete suite;
   }
 
-  void createLogWksp() {
+  void createLogWksp(const std::string &frequency, const std::string &wavelength) {
     m_logWSName = "_det_char_log";
 
     {
@@ -47,7 +47,7 @@ public:
     {
       auto alg = FrameworkManager::Instance().createAlgorithm("AddSampleLog");
       alg->setPropertyValue("LogName", "frequency");
-      alg->setPropertyValue("LogText", "60.");
+      alg->setProperty("LogText", frequency);
       alg->setPropertyValue("LogUnit", "Hz");
       alg->setPropertyValue("LogType", "Number");
       alg->setPropertyValue("Workspace", m_logWSName);
@@ -56,7 +56,7 @@ public:
     {
       auto alg = FrameworkManager::Instance().createAlgorithm("AddSampleLog");
       alg->setPropertyValue("LogName", "LambdaRequest");
-      alg->setPropertyValue("LogText", "0.533");
+      alg->setPropertyValue("LogText", wavelength);
       alg->setPropertyValue("LogUnit", "Angstrom");
       alg->setPropertyValue("LogType", "Number");
       alg->setPropertyValue("Workspace", m_logWSName);
@@ -80,29 +80,41 @@ public:
     row << tofmax;
   }
 
-  ITableWorkspace_sptr createTableWksp(bool full) {
-    ITableWorkspace_sptr wksp = WorkspaceFactory::Instance().createTable();
-    wksp->addColumn("double", "frequency");
-    wksp->addColumn("double", "wavelength");
-    wksp->addColumn("int", "bank");
-    wksp->addColumn("int", "vanadium");
-    wksp->addColumn("int", "container");
-    wksp->addColumn("int", "empty");
-    wksp->addColumn("str", "d_min"); // b/c it is an array for NOMAD
-    wksp->addColumn("str", "d_max"); // b/c it is an array for NOMAD
-    wksp->addColumn("double", "tof_min");
-    wksp->addColumn("double", "tof_max");
+  ITableWorkspace_sptr createEmptyTableWksp() {
+      ITableWorkspace_sptr wksp = WorkspaceFactory::Instance().createTable();
+      wksp->addColumn("double", "frequency");
+      wksp->addColumn("double", "wavelength");
+      wksp->addColumn("int", "bank");
+      wksp->addColumn("int", "vanadium");
+      wksp->addColumn("int", "container");
+      wksp->addColumn("int", "empty");
+      wksp->addColumn("str", "d_min"); // b/c it is an array for NOMAD
+      wksp->addColumn("str", "d_max"); // b/c it is an array for NOMAD
+      wksp->addColumn("double", "tof_min");
+      wksp->addColumn("double", "tof_max");
 
-    if (full) {
-      addRow(wksp, 60., 0.533, 1, 17702, 17711, 0, "0.05", "2.20", 0000.00,
-             16666.67);
-      addRow(wksp, 60., 1.333, 3, 17703, 17712, 0, "0.43", "5.40", 12500.00,
-             29166.67);
-      addRow(wksp, 60., 2.665, 4, 17704, 17713, 0, "1.15", "9.20", 33333.33,
-             50000.00);
-      addRow(wksp, 60., 4.797, 5, 17705, 17714, 0, "2.00", "15.35", 66666.67,
-             83333.67);
-    }
+      return wksp;
+  }
+
+  ITableWorkspace_sptr createTableWkspPG3() {
+    ITableWorkspace_sptr wksp = createEmptyTableWksp();
+
+    addRow(wksp, 60., 0.533, 1, 17702, 17711, 0, "0.05", "2.20", 0000.00,
+           16666.67);
+    addRow(wksp, 60., 1.333, 3, 17703, 17712, 0, "0.43", "5.40", 12500.00,
+           29166.67);
+    addRow(wksp, 60., 2.665, 4, 17704, 17713, 0, "1.15", "9.20", 33333.33,
+           50000.00);
+    addRow(wksp, 60., 4.797, 5, 17705, 17714, 0, "2.00", "15.35", 66666.67,
+           83333.67);
+
+    return wksp;
+  }
+
+  ITableWorkspace_sptr createTableWkspNOM() {
+    ITableWorkspace_sptr wksp = createEmptyTableWksp();
+
+    addRow(wksp, 60., 1.4, 1, 0, 0, 0, ".31,.25,.13,.13,.13,.42", "13.66,5.83,3.93,2.09,1.57,31.42", 300.00, 16666.67);
 
     return wksp;
   }
@@ -159,7 +171,7 @@ public:
   }
 
   void testNoChar() {
-    createLogWksp();
+    createLogWksp("60.", "0.533");
     // don't create characterization table
 
     PDDetermineCharacterizations alg;
@@ -178,8 +190,8 @@ public:
   }
 
   void testEmptyChar() {
-    createLogWksp();
-    auto tableWS = createTableWksp(false);
+    createLogWksp("60.", "0.533");
+    auto tableWS = createEmptyTableWksp();
 
     PDDetermineCharacterizations alg;
     TS_ASSERT_THROWS_NOTHING(alg.initialize());
@@ -198,8 +210,8 @@ public:
   }
 
   void testFullChar() {
-    createLogWksp();
-    auto tableWS = createTableWksp(true);
+    createLogWksp("60.", "0.533");
+    auto tableWS = createTableWkspPG3();
 
     PDDetermineCharacterizations alg;
     TS_ASSERT_THROWS_NOTHING(alg.initialize());
@@ -219,8 +231,8 @@ public:
   }
 
   void testFullCharDisableChar() {
-    createLogWksp();
-    auto tableWS = createTableWksp(true);
+    createLogWksp("60.", "0.533");
+    auto tableWS = createTableWkspPG3();
 
     PDDetermineCharacterizations alg;
     TS_ASSERT_THROWS_NOTHING(alg.initialize());
@@ -240,6 +252,27 @@ public:
 
     compareResult(expectedInfo, PropertyManagerDataService::Instance().retrieve(
                                     PROPERTY_MANAGER_NAME));
+  }
+
+  void testFullCharNom() {
+      createLogWksp("60.", "1.4");
+      auto tableWS = createTableWkspNOM();
+
+      PDDetermineCharacterizations alg;
+      TS_ASSERT_THROWS_NOTHING(alg.initialize());
+      TS_ASSERT_THROWS_NOTHING(
+          alg.setPropertyValue("InputWorkspace", m_logWSName));
+      TS_ASSERT_THROWS_NOTHING(alg.setProperty("Characterizations", tableWS));
+      TS_ASSERT_THROWS_NOTHING(
+          alg.setPropertyValue("ReductionProperties", PROPERTY_MANAGER_NAME));
+      TS_ASSERT_THROWS_NOTHING(alg.execute(););
+      TS_ASSERT(alg.isExecuted());
+
+      auto expectedInfo = createExpectedInfo(60., 1.4, 1, 0, 0, 0,
+                                             ".31,.25,.13,.13,.13,.42", "13.66,5.83,3.93,2.09,1.57,31.42", 300.00, 16666.67);
+
+      compareResult(expectedInfo, PropertyManagerDataService::Instance().retrieve(
+                                      PROPERTY_MANAGER_NAME));
   }
 };
 
