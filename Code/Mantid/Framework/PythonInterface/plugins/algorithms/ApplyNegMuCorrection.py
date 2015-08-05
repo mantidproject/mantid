3b1b162ef8fb92575974943ce3e646ddc4fcc71b
Matt King <matthew.king@stfc.ac.uk>
2015-08-05 14:14:07 +0100

    Changed invalid variable names and other violations in pyLint
    Refs #13201

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/ApplyNegMuCorrection.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/ApplyNegMuCorrection.py
@@ -1,21 +1,22 @@
 from mantid.api import *  # PythonAlgorithm, registerAlgorithm, WorkspaceProperty
 
+#pylint: disable=no-init
 class ApplyNegMuCorrection(PythonAlgorithm):
 
     #Combining work spaces and normalising the correction.
-    def combine(self,dd,runno,A2000,B2000,A3000,B3000,C,C1,spec):
+    def combine(self,dataDir,runno,A2000,B2000,A3000,B3000,spec):
         if spec<10:
-            s='0'+str(spec)
+            specNo='0'+str(spec)
         else:
-            s=str(spec)
-        print dd+'ral0'+runno+'.rooth30'+s+'.dat'
+            specNo=str(spec)
+        print dataDir+'ral0'+runno+'.rooth30'+specNo+'.dat'
         #loading data
         try:
-            ws3000=Load(Filename=dd+r'\ral0'+runno+'.rooth30'+s+'.dat', OutputWorkspace='ws3000')
+            ws3000=Load(Filename=dataDir+r'\ral0'+runno+'.rooth30'+specNo+'.dat', OutputWorkspace='ws3000')
             print 'hello'
-            ws2000=Load(Filename=dd+r'\ral0'+runno+'.rooth20'+s+'.dat', OutputWorkspace='ws2000')
+            ws2000=Load(Filename=dataDir+r'\ral0'+runno+'.rooth20'+specNo+'.dat', OutputWorkspace='ws2000')
         except RuntimeError:
-            print runno+' '+s+"not found"
+            print runno+' '+specNo+"not found"
 
         #Correcting for Gain and offset of the detectors
         ws2000_corr=CreateWorkspace(A2000*ws2000.readX(0)[:]+B2000,ws2000.readY(0)[:])
@@ -24,24 +25,20 @@ class ApplyNegMuCorrection(PythonAlgorithm):
         #Summing total counts for normalisation
         ws2000_total=0
         ws3000_total=0
-        for x in range (0,8000):
-            try:
-                ws2000_total=ws2000_corr.readY(0)[x]+ws2000_total
-                ws3000_total=ws3000_corr.readY(0)[x]+ws3000_total
-            except:
-                continue
-
+        for count in range (0,8000):
+            ws2000_total=ws2000_corr.readY(0)[count]+ws2000_total
+            ws3000_total=ws3000_corr.readY(0)[count]+ws3000_total
         print ws2000_total
         print ws3000_total
         #normalising
         ws2000_corr=ws2000_corr/ws2000_total
         ws3000_corr=ws3000_corr/ws3000_total
 
-        #rebinning to add detectors together
-        bin=[100,ws2000.readX(0)[2]-ws2000.readX(0)[1],8000]
+        #rebinning to adataDir detectors together
+        data =[100,ws2000.readX(0)[2]-ws2000.readX(0)[1],8000]
 
-        ws2000_corr_rebin=Rebin(ws2000_corr,bin)
-        ws3000_corr_rebin=Rebin(ws3000_corr,bin)
+        ws2000_corr_rebin=Rebin(ws2000_corr,data)
+        ws3000_corr_rebin=Rebin(ws3000_corr,data)
 
         ws_ral=Plus(ws2000_corr_rebin,ws3000_corr_rebin)
 
@@ -75,7 +72,7 @@ class ApplyNegMuCorrection(PythonAlgorithm):
     def PyExec(self):
 
         spec=1
-        dd = self.getProperty("Data Directory").value
+        dataDir = self.getProperty("Data Directory").value
         first = self.getProperty("First Run Number").value
         last = self.getProperty("Last Run Number").value+1
         GRHE=self.getProperty("Gain RIKEN High E").value
@@ -85,11 +82,11 @@ class ApplyNegMuCorrection(PythonAlgorithm):
         GILE=self.getProperty("Gain ISIS Low E").value
         OILE=self.getProperty("Offset ISIS Low E").value
 
-        for x in range(first,last):
-           for spec in range(0,3):
-               runno=str(x)
-               self.combine(dd,runno,GRHE,ORHE,GIHE,OIHE,GILE,OILE,spec)
-               self.combine(dd,runno,GRHE,ORHE,GIHE,OIHE,GILE,OILE,10)
+        for run in range(first,last):
+            for spec in range(0,3):
+                runno=str(run)
+                self.combine(self,dataDir,runno,GRHE,ORHE,GIHE,OIHE,spec)
+                self.combine(self,dataDir,runno,GRHE,ORHE,GIHE,OIHE,10)
 
 
 
