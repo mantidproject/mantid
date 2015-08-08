7cde9908e9627af5185f2bc9deb45dd8090971b0
celinedurniak <c.durniak@fz-juelich.de>
2015-08-08 16:37:22 +0200

    Modified algorithms and documentation for MonitorEfficiencyCorUser

--- Code/Mantid/Framework/Algorithms/src/MonitorEfficiencyCorUser.cpp
+++ Code/Mantid/Framework/Algorithms/src/MonitorEfficiencyCorUser.cpp
@@ -16,88 +16,86 @@ using namespace Geometry;
 // Register the algorithm into the AlgorithmFactory
 DECLARE_ALGORITHM(MonitorEfficiencyCorUser)
 
-
 //----------------------------------------------------------------------------------------------
 /** Constructor
  */
-MonitorEfficiencyCorUser::MonitorEfficiencyCorUser() {
-}
+MonitorEfficiencyCorUser::MonitorEfficiencyCorUser() {}
 
 //----------------------------------------------------------------------------------------------
 /** Destructor
  */
-MonitorEfficiencyCorUser::~MonitorEfficiencyCorUser() {
-}
+MonitorEfficiencyCorUser::~MonitorEfficiencyCorUser() {}
 
 //----------------------------------------------------------------------------------------------
 /** Initialize the algorithm's properties.
  */
 
-void MonitorEfficiencyCorUser::init()
-{
-    declareProperty(
-            new WorkspaceProperty<>("InputWorkspace", "", Direction::Input,boost::make_shared<InstrumentValidator>()
-                        ), "The workspace to correct for monitor efficiency");
-    declareProperty(
-                new WorkspaceProperty<>("OutputWorkspace", "", Direction::Output),
-                "The name of the workspace in which to store the result.");
+void MonitorEfficiencyCorUser::init() {
+  declareProperty(
+      new WorkspaceProperty<>("InputWorkspace", "", Direction::Input,
+                              boost::make_shared<InstrumentValidator>()),
+      "The workspace to correct for monitor efficiency");
+  declareProperty(
+      new WorkspaceProperty<>("OutputWorkspace", "", Direction::Output),
+      "The name of the workspace in which to store the result.");
 }
 
+void MonitorEfficiencyCorUser::exec() {
+  m_inputWS = this->getProperty("InputWorkspace");
 
-void MonitorEfficiencyCorUser::exec()
-{
-    m_inputWS = this->getProperty("InputWorkspace");
+  m_outputWS = this->getProperty("OutputWorkspace");
 
-    m_outputWS = this->getProperty("OutputWorkspace");
+  if (m_inputWS->getInstrument()->getName() != "TOFTOF") {
+    std::string message("The input workspace does not come from TOFTOF");
+    g_log.error(message);
+    throw std::invalid_argument(message);
+  }
 
-    if ( m_inputWS->getInstrument()->getName() != "TOFTOF" )
-    { std::string message("The input workspace does not come from TOFTOF");
-      g_log.error(message);
-      throw std::invalid_argument(message);
-    }
+  // If input and output workspaces are not the same, create a new workspace for
+  // the output
+  if (m_outputWS != this->m_inputWS) {
+    m_outputWS = API::WorkspaceFactory::Instance().create(m_inputWS);
+  }
+  double val;
+  Strings::convert(m_inputWS->run().getProperty("Ei")->value(), val);
+  m_Ei = val;
+  Strings::convert(m_inputWS->run().getProperty("monitor_counts")->value(),
+                   m_monitorCounts);
 
-    // If input and output workspaces are not the same, create a new workspace for the output
-    if (m_outputWS != this->m_inputWS) {
-        m_outputWS = API::WorkspaceFactory::Instance().create(m_inputWS);
-    }
-    double val;
-    Strings::convert(m_inputWS->run().getProperty("Ei")->value(),val);
-    m_Ei = val;
-    Strings::convert(m_inputWS->run().getProperty("monitor_counts")->value(),m_monitorCounts);
+  // get Efficiency formula from the IDF - Parameters file
+  const std::string effFormula = getValFromInstrumentDef("formula_mon_eff");
 
-    // get Efficiency formula from the IDF - Parameters file
-    const std::string effFormula = getValFromInstrumentDef("formula_mon_eff");
+  // Calculate Efficiency for E = Ei
+  const double eff0 = m_monitorCounts * calculateFormulaValue(effFormula, m_Ei);
 
-    // Calculate Efficiency for E = Ei
-    const double eff0 = m_monitorCounts*calculateFormulaValue(effFormula, m_Ei);
+  const size_t numberOfChannels = this->m_inputWS->blocksize();
+  // Calculate the number of spectra in this workspace
+  const int numberOfSpectra =
+      static_cast<int>(this->m_inputWS->getNumberHistograms());
+  API::Progress prog(this, 0.0, 1.0, numberOfSpectra);
+  int64_t numberOfSpectra_i =
+      static_cast<int64_t>(numberOfSpectra); // cast to make openmp happy
 
-    const size_t numberOfChannels = this->m_inputWS->blocksize();
-    // Calculate the number of spectra in this workspace
-    const int numberOfSpectra = static_cast<int>(this->m_inputWS->getNumberHistograms());
-    API::Progress prog(this, 0.0, 1.0, numberOfSpectra);
-    int64_t numberOfSpectra_i = static_cast<int64_t>(numberOfSpectra); // cast to make openmp happy
+  // Loop over the histograms (detector spectra)
+  PARALLEL_FOR2(m_outputWS, m_inputWS)
+  for (int64_t i = 0; i < numberOfSpectra_i; ++i) {
+    PARALLEL_START_INTERUPT_REGION
+    // MantidVec& xOut = m_outputWS->dataX(i);
+    MantidVec &yOut = m_outputWS->dataY(i);
+    MantidVec &eOut = m_outputWS->dataE(i);
+    const MantidVec &yIn = m_inputWS->readY(i);
+    const MantidVec &eIn = m_inputWS->readE(i);
+    m_outputWS->setX(i, m_inputWS->refX(i));
 
-    // Loop over the histograms (detector spectra)
-    PARALLEL_FOR2(m_outputWS,m_inputWS)
-    for (int64_t i = 0; i < numberOfSpectra_i; ++i) {
-        PARALLEL_START_INTERUPT_REGION
-        //MantidVec& xOut = m_outputWS->dataX(i);
-        MantidVec& yOut = m_outputWS->dataY(i);
-        MantidVec& eOut = m_outputWS->dataE(i);
-        const MantidVec& yIn = m_inputWS->readY(i);
-        const MantidVec& eIn = m_inputWS->readE(i);
-        m_outputWS->setX(i, m_inputWS->refX(i));
+    applyMonEfficiency(numberOfChannels, yIn, eIn, eff0, yOut, eOut);
 
-        applyMonEfficiency(numberOfChannels, yIn, eIn, eff0, yOut, eOut);
-
-        prog.report("Detector Efficiency correction...");
+    prog.report("Detector Efficiency correction...");
     PARALLEL_END_INTERUPT_REGION
-    } //end for i
-PARALLEL_CHECK_INTERUPT_REGION
+  } // end for i
+  PARALLEL_CHECK_INTERUPT_REGION
 
-    setProperty("OutputWorkspace", m_outputWS);
+  setProperty("OutputWorkspace", m_outputWS);
 }
-
 
 /**
  * Apply the monitor efficiency to a single spectrum
@@ -109,17 +107,14 @@ PARALLEL_CHECK_INTERUPT_REGION
  * @param eOut corrected spectrum errors
  */
 void MonitorEfficiencyCorUser::applyMonEfficiency(
-        const size_t numberOfChannels, const MantidVec& yIn,
-        const MantidVec& eIn, const double effVec, MantidVec& yOut,
-        MantidVec& eOut) {
+    const size_t numberOfChannels, const MantidVec &yIn, const MantidVec &eIn,
+    const double effVec, MantidVec &yOut, MantidVec &eOut) {
 
-    for (unsigned int j = 0; j < numberOfChannels; ++j) {
-        yOut[j] = yIn[j] / effVec;
-        eOut[j] = eIn[j] / effVec;
-    }
-
+  for (unsigned int j = 0; j < numberOfChannels; ++j) {
+    yOut[j] = yIn[j] / effVec;
+    eOut[j] = eIn[j] / effVec;
+  }
 }
-
 
 /**
  * Calculate the value of a formula
@@ -127,23 +122,24 @@ void MonitorEfficiencyCorUser::applyMonEfficiency(
  * @param energy :: value of the energy to use in the formula
  * @return calculated corrected value of the monitor count
  */
-double MonitorEfficiencyCorUser::calculateFormulaValue(
-        const std::string &formula, double energy) {
-    try {
-        mu::Parser p;
-        p.DefineVar("e", &energy);
-        p.SetExpr(formula);
-        double eff = p.Eval();
-        g_log.debug() << "Formula: " << formula <<  " with: " << energy << "evaluated to: "<< eff  << std::endl;
-        return eff;
+double
+MonitorEfficiencyCorUser::calculateFormulaValue(const std::string &formula,
+                                                double energy) {
+  try {
+    mu::Parser p;
+    p.DefineVar("e", &energy);
+    p.SetExpr(formula);
+    double eff = p.Eval();
+    g_log.debug() << "Formula: " << formula << " with: " << energy
+                  << "evaluated to: " << eff << std::endl;
+    return eff;
 
-    } catch (mu::Parser::exception_type &e) {
-        throw Kernel::Exception::InstrumentDefinitionError(
-                "Error calculating formula from string. Muparser error message is: "
-                        + e.GetMsg());
-    }
+  } catch (mu::Parser::exception_type &e) {
+    throw Kernel::Exception::InstrumentDefinitionError(
+        "Error calculating formula from string. Muparser error message is: " +
+        e.GetMsg());
+  }
 }
-
 
 /**
  * Returns the value associated to a parameter name in the IDF
@@ -151,22 +147,21 @@ double MonitorEfficiencyCorUser::calculateFormulaValue(
  * @return the value associated to the parameter name
  */
 std::string MonitorEfficiencyCorUser::getValFromInstrumentDef(
-        const std::string& parameterName) {
+    const std::string &parameterName) {
 
-    const ParameterMap& pmap = m_inputWS->constInstrumentParameters();
-    Instrument_const_sptr instrument = m_inputWS->getInstrument();
-    Parameter_sptr par = pmap.getRecursive(instrument->getChild(0).get(),
-            parameterName);
-    if (par) {
-        std::string ret = par->asString();
-        g_log.debug() << "Parsed parameter " << parameterName << ": " << ret
-                << "\n";
-        return ret;
-    } else {
-        throw Kernel::Exception::InstrumentDefinitionError(
-                "There is no <" + parameterName
-                        + "> in the instrument definition!");
-    }
+  const ParameterMap &pmap = m_inputWS->constInstrumentParameters();
+  Instrument_const_sptr instrument = m_inputWS->getInstrument();
+  Parameter_sptr par =
+      pmap.getRecursive(instrument->getChild(0).get(), parameterName);
+  if (par) {
+    std::string ret = par->asString();
+    g_log.debug() << "Parsed parameter " << parameterName << ": " << ret
+                  << "\n";
+    return ret;
+  } else {
+    throw Kernel::Exception::InstrumentDefinitionError(
+        "There is no <" + parameterName + "> in the instrument definition!");
+  }
 }
 
 } // namespace Algorithms
--- Code/Mantid/docs/source/algorithms/MonitorEfficiencyCorUser-v1.rst
+++ Code/Mantid/docs/source/algorithms/MonitorEfficiencyCorUser-v1.rst
@@ -9,20 +9,24 @@
 Description
 -----------
 
-Normalizes the counts by monitor counts with additional efficiency correction.
-
-Corrects the monitor efficiency according to the formula contained in TOFTOF's Parameters file.
-
-The values of the monitor counts and incident energy are stored in the SampleLogs of the input workspace.
+This algorithm normalises the neutron counts by monitor counts with an additional efficiency correction.
 
 To date this algorithm only supports the TOFTOF instrument.
+
+The monitor counts is the total count and it is stored in the SampleLogs of the input workspace.
+
+This count is corrected taking into account the monitor efficiency. The formula used for the correction is stored in the Parameters file and requires the incident energy (Ei), which is stored in the SampleLogs of the input workspace.
+
+The corrected value of the monitor counts is used to normalise the input workspace.
 
 
 Restrictions
 ###################################
 
 A formula named "formula\_eff" must be defined in the instrument
-parameters file.
+parameters file. It is defined as "monitor counts * sqrt(Ei/25.3)"
+
+The incident energy Ei and the monitor counts are read in the SampleLogs of the input workspace.
 
 Usage
 -----
@@ -32,27 +36,32 @@ Usage
 .. testcode:: ExMonitorEfficiencyCorUser
 
     import numpy as np
-    #Load data
-   
     wsSample = LoadMLZ(Filename='TOFTOFTestdata.nxs')
-
-    wsNorm = MonitorEfficiencyCorUser(wsSample)
-    # Check calculation of normalization coefficient
+    wsNorm = MonitorEfficiencyCorUser(wsSample )
+    # Input and output workspaces have the same structure
+    print 'Number of histograms of the input and output workspaces:'
+    print wsSample.getNumberHistograms(), wsNorm.getNumberHistograms()
+    print 'Number of time channels of the input an output workspaces:'
+    print wsSample.blocksize(), wsNorm.blocksize()
+    # Check calculation of normalisation coefficient between input and output workspaces
     wsCheck = Divide(wsSample,wsNorm)
-    print 'Coefficient of proportionality between Input and Output of MonitorEfficiencyCorUser algorithm:'
-    print wsCheck.readY(102)[1]
+    print "Coefficient of proportionality between Input and Output of MonitorEfficiencyCorUser algorithm: %5.3f" % wsCheck.readY(102)[1]
+    # Read the values of the incident energy and of the monitor counts from the SampleLogs of wsSample
     monitor_counts = float(mtd['wsSample'].getRun().getLogData('monitor_counts').value)
     Ei = float(mtd['wsSample'].getRun().getLogData('Ei').value)
-    print 'Coefficient from theoretical formula = monitor_counts * sqrt(Ei/25.3) ', monitor_counts*np.sqrt(Ei/25.3)
-  
-
+    print "Coefficient from theoretical formula = monitor_counts * sqrt(Ei/25.3): %5.3f" % (monitor_counts*np.sqrt(Ei/25.3))
+ 
+ 
 Output:
 
 .. testoutput:: ExMonitorEfficiencyCorUser
 
-    Coefficient of proportionality between Input and Output of MonitorEfficiencyCorUser algorithm:
-    41038.4323645
-    Coefficient from theoretical formula = monitor_counts * sqrt(Ei/25.3)  41038.4323645
+    Number of histograms of the input and output workspaces:
+    1006 1006
+    Number of time channels of the input an output workspaces:
+    1024 1024
+    Coefficient of proportionality between Input and Output of MonitorEfficiencyCorUser algorithm: 41038.432
+    Coefficient from theoretical formula = monitor_counts * sqrt(Ei/25.3): 41038.432 
 
 .. categories::
 
