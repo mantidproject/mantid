e95efbc7d342733deb3ed0a7ccd198827f47b57e
Matthew D Jones <Matthew.D.Jones@tessella.com>
2015-08-12 13:40:31 +0100

    Re #11422 Rename scale option from "power (X^n)" to "power"

--- Code/Mantid/MantidPlot/src/ScaleDetails.cpp
+++ Code/Mantid/MantidPlot/src/ScaleDetails.cpp
@@ -95,7 +95,7 @@ ScaleDetails::ScaleDetails(ApplicationWindow* app, Graph* graph, int mappedaxis,
   m_cmbScaleType = new QComboBox();
   m_cmbScaleType->addItem(tr("linear"));
   m_cmbScaleType->addItem(tr("logarithmic"));
-  m_cmbScaleType->addItem(tr("power (X^n)"));
+  m_cmbScaleType->addItem(tr("power"));
   middleLayout->addWidget(m_lblScaleTypeLabel, 2, 0);
   middleLayout->addWidget(m_cmbScaleType, 2, 1);
 
