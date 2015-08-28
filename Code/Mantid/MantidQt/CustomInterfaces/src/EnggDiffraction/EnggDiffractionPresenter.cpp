533cd67a3a36035aa57e5effce71827a18e60669
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-08-28 08:40:41 +0100

    Fix coverity issue 1320626, re #13375
    Sneaked in as InstrumentChange always raises an exception (for now).

--- Code/Mantid/MantidQt/CustomInterfaces/src/EnggDiffraction/EnggDiffractionPresenter.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/EnggDiffraction/EnggDiffractionPresenter.cpp
@@ -88,6 +88,7 @@ void EnggDiffractionPresenter::notify(
 
   case IEnggDiffractionPresenter::InstrumentChange:
     processInstChange();
+    break;
 
   case IEnggDiffractionPresenter::ShutDown:
     processShutDown();
