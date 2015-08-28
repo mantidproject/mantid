2f8151318e0a73e9fe0fe568485af8597791ab2f
Martyn Gigg <martyn.gigg@gmail.com>
2015-08-28 08:54:00 +0100

    Switch to use gsl_multifit_linear and weight the input matrix
    It seems more stable.
    Refs #13366

--- Code/Mantid/Framework/Kernel/src/Math/ChebyshevPolyFit.cpp
+++ Code/Mantid/Framework/Kernel/src/Math/ChebyshevPolyFit.cpp
@@ -29,45 +29,42 @@ private:
 // ChebyshevPolyFitImpl Public Members
 //-----------------------------------------------------------------------------
 // See doc for main class for parameter descriptions
-std::vector<double> ChebyshevPolyFitImpl::fit(const std::vector<double> &xs,
-                                              const std::vector<double> &ys,
-                                              const std::vector<double> &wgts) {
-  assert(xs.size() == ys.size());
-  assert(ys.size() == wgts.size());
+std::vector<double> ChebyshevPolyFitImpl::fit(const std::vector<double> &x,
+                                              const std::vector<double> &y,
+                                              const std::vector<double> &w) {
+  assert(x.size() == y.size());
+  assert(y.size() == w.size());
 
-  const size_t npoints = ys.size();
-  auto gslY = gsl_vector_const_view_array(ys.data(), npoints);
-  auto gslW = gsl_vector_const_view_array(wgts.data(), npoints);
+  const size_t npts(y.size()), degp1(m_order + 1);
+  const double xmin(x.front()), xmax(x.back());
 
-  // Fit kth-order chebyshev polynomial to each point essentially solving
-  //   y = Xc
-  // where c_i are the expansion cofficients
-  const double xmin(xs.front()), xmax(xs.back());
-  const size_t nparams = m_order + 1;
-  auto *X = gsl_matrix_alloc(npoints, nparams);
+  // Compute the weighted pseudo Vandermonde matrix
+  // and y*w
+  gsl_matrix *MX = gsl_matrix_alloc(npts, degp1);
+  auto *yw = gsl_vector_alloc(npts);
   ChebyshevPolynomial chebyp;
-  for (size_t i = 0; i < npoints; ++i) {
-    const double xi = xs[i];
-    // Map to the applicable domain [-1,1] for the Chebyshev polynomial
-    const double xp = ((xi - xmin) - (xmax - xi)) / (xmax - xmin);
-    for (size_t j = 0; j < nparams; ++j) {
-      gsl_matrix_set(X, i, j, chebyp(j, xp));
+  for(size_t i = 0; i < npts; ++i) {
+    const auto xi = x[i];
+    const auto wi = w[i];
+    const auto xbar = ((xi - xmin) - (xmax - xi))/(xmax-xmin);
+    gsl_vector_set(yw, i, wi*y[i]);
+    for(size_t j = 0; j < degp1; ++j) {
+      gsl_matrix_set(MX, i, j, wi*chebyp(j, xbar));
     }
   }
-  auto *coef = gsl_vector_alloc(nparams);
-  auto *covar = gsl_matrix_alloc(nparams, nparams);
-  // output variables
+
+  // Least-squares fit to determine c
+  auto *c = gsl_vector_alloc(degp1);
+  auto *cov = gsl_matrix_alloc(degp1, degp1);
+  auto *work = gsl_multifit_linear_alloc (npts, degp1);
   double chisq(0.0);
-  auto *work = gsl_multifit_linear_alloc(npoints, nparams);
-  gsl_multifit_wlinear(X, &gslW.vector, &gslY.vector, coef, covar, &chisq,
-                       work);
+  gsl_multifit_linear (MX, yw, c, cov, &chisq, work);
+  gsl_matrix_free(cov);
+  gsl_matrix_free(MX);
   gsl_multifit_linear_free(work);
 
-  gsl_matrix_free(X);
-  gsl_matrix_free(covar);
-
-  std::vector<double> coeffs(coef->data, coef->data + nparams);
-  gsl_vector_free(coef);
+  std::vector<double> coeffs(c->data, c->data + degp1);
+  gsl_vector_free(c);
   return coeffs;
 }
 
@@ -88,7 +85,12 @@ ChebyshevPolyFit::ChebyshevPolyFit(const size_t n)
 ChebyshevPolyFit::~ChebyshevPolyFit() {}
 
 /**
- * Perform the fit for the given data set
+ * Find coefficients of polynomial that minimizes the sum
+ * of the squares of the residuals:
+ *      e_r = w_r(y_r - f_r)
+ * Solves the overdetermined equation
+ *      w * V(x) * c = w * y
+ *
  * @param xs X values
  * @param ys Y data points
  * @param wgts Weights for each Y point
