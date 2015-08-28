c80ff0820c57ba91a2885055674ffb9dabd5a8c1
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2015-08-28 16:50:16 +0100

    Working system test for ISISPowderDiffraction
    Refs #13407

--- Code/Mantid/Testing/SystemTests/tests/analysis/ISISPowderDiffractionTest.py
+++ Code/Mantid/Testing/SystemTests/tests/analysis/ISISPowderDiffractionTest.py
@@ -1,11 +1,18 @@
 # pylint: disable=no-init
 import stresstesting
 import os
+import sys
+
+sys.path.append('C:\Users\RYQ25391\Documents\GitHub\mantid\Code\Mantid\scripts\PowderISIS')
+import cry_ini
+import cry_focus
 from mantid.simpleapi import *
+from mantid.api import FileFinder
+
+from IndirectImport import is_supported_f2py_platform
 
 
-#==============================================================================
-
+# ==============================================================================
 def _cleanup_files(dirname, filenames):
     """
        Attempts to remove each filename from
@@ -17,30 +24,63 @@ def _cleanup_files(dirname, filenames):
             os.remove(path)
         except OSError:
             pass
+            print(OSError, "Did not DELETE")
+            #mantid.logger.warning("Unable to remove created file '%s'" % filename)
+# ==============================================================================
 
-#==============================================================================
+    def cleanup(self):
+        Files = ["hrpd/test/cycle_09_2/Calibration/hrpd_new_072_01_corr.cal",
+                     "hrpd/test/cycle_09_2/tester/hrp43022_s1_old.gss",
+                     "hrpd/test/cycle_09_2/tester/hrp43022_s1_old.nxs",
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b1_D.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b1_TOF.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b2_D.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b2_TOF.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b3_D.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b3_TOF.dat',
+                     'hrpd/test/cycle_09_2/hrpd_new_072_01_corr.cal']
+        for filename in Files:
+            absfile = FileFinder.getFullPath(filename)
+            if os.path.exists(absfile):
+                os.remove(absfile)
+        return True
+
+
 
 # Simply tests that our LoadRaw and LoadISISNexus algorithms produce the same workspace
-class ISISPowderDiffractionTest(stresstesting.MantidStressTest):
+class ISISPowderDiffraction(stresstesting.MantidStressTest):
+    def skipTests(self):
+        if is_supported_f2py_platform():
+            return False
+        else:
+            return True
+
     def requiredFiles(self):
-        return ["hrpd_new_072_01.cal", "hrpd_new_072_01.cal_corr.cal", "van_s1_old-0.nxs", "van_s1_old-1.nxs",
-                "van_s1_old-1.nxs", "mtd.pref"]
+        return {"HRP39191.raw", "HRP39187.raw", "HRP43022.raw", "hrpd/test/GrpOff/hrpd_new_072_01.cal",
+                "hrpd/test/GrpOff/hrpd_new_072_01_corr.cal", "hrpd/test/cycle_09_2/Calibration/van_s1_old-0.nxs",
+                "hrpd/test/cycle_09_2/Calibration/van_s1_old-1.nxs",
+                'hrpd/test/cycle_09_2/Calibration/van_s1_old-1.nxs', "hrpd/test/cycle_09_2/tester/mtd.pref"}
 
     def runTest(self):
-        import cry_init as Main
-        config['datasearch.searcharchive'] = 'On'
-        expt = cry_ini.Files("hrpd", Analysisdir='test')
+
+        dirs = config['datasearch.directories'].split(';')
+
+        print "this is dirs: ", dirs
+        expt = cry_ini.Files('hrpd', RawDir=(dirs[0]), Analysisdir='test', forceRootDirFromScripts=False,
+                             inputInstDir=dirs[0])
         expt.initialize('cycle_09_2', user='tester', prefFile='mtd.pref')
         expt.tell()
         cry_focus.focus_all(expt, "43022")
 
-    def validate(self):
-
-     def cleanup(self):
-    filenames = ['hrp43022_s1_old.gss','hrp43022_s1_old.nxs','hrp43022_s1_old_b1_D.dat','hrp43022_s1_old_b1_TOF.dat',
-                         'hrp43022_s1_old_b2_D.dat','hrp43022_s1_old_b2_TOF.dat',
-                         'hrp43022_s1_old_b3_D.dat','hrp43022_s1_old_b3_TOF.dat',
-                         'hrpd_new_072_01_corr.cal' ]
+    def cleanup(self):
+        filenames = {"hrpd/test/cycle_09_2/Calibration/hrpd_new_072_01_corr.cal",
+                     "hrpd/test/cycle_09_2/tester/hrp43022_s1_old.gss",
+                     "hrpd/test/cycle_09_2/tester/hrp43022_s1_old.nxs",
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b1_D.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b1_TOF.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b2_D.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b2_TOF.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b3_D.dat',
+                     'hrpd/test/cycle_09_2/tester/hrp43022_s1_old_b3_TOF.dat',
+                     'hrpd/test/cycle_09_2/hrpd_new_072_01_corr.cal'}
         _cleanup_files(config['defaultsave.directory'], filenames)
-
-
