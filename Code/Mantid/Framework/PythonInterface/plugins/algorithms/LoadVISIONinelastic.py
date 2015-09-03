09c4d63db50ab3089bf236d4ba17422d89f11265
Stuart Campbell <campbellsi@ornl.gov>
2015-09-03 12:14:07 -0400

    Initial version. refs #13534
    Added initial functionality.  Now to test it!

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/LoadVISIONinelastic.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/LoadVISIONinelastic.py
@@ -1,13 +1,15 @@
 #pylint: disable=no-init,invalid-name
+#from mantid.api import AlgorithmFactory
+#from mantid.simpleapi import PythonAlgorithm, WorkspaceProperty
+# from mantid.kernel import Direction
 from mantid.api import *
-from mantid.simpleapi import *
 from mantid.kernel import *
-import os
+import mantid.simpleapi
 
 class LoadVISIONinelastic(PythonAlgorithm):
 
-    __forward = ""
-    __backward = ""
+    __forward = "bank1,bank2,bank3,bank4,bank5,bank6,bank7"
+    __backward = "bank8,bank9,bank10,bank11,bank12,bank13,bank14"
 
     def category(self):
         return "DataHandling;PythonAlgorithms"
@@ -19,10 +21,32 @@ class LoadVISIONinelastic(PythonAlgorithm):
         return "This algorithm loads only the inelastic detectors on VISION."
 
     def PyInit(self):
-        self.declareProperty("Filename", "")
-        self.declareProperty("Banks", "All")
+        self.declareProperty(FileProperty("Filename", "", action=FileAction.Load, extensions=["*.nxs.h5"]))
+        self.declareProperty("Banks", "all")
+        self.declareProperty(WorkspaceProperty("OutputWorkspace", "", direction=Direction.Output))
+
 
     def PyExec(self):
         filename = self.getProperty("Filename").value
+        banks = ""
         banks = self.getProperty("Banks").value
+
+        # First lets replace 'All' with 'forward,backward'
+        banks = banks.lower().replace("all", "forward,backward")
+        banks = banks.lower().replace("forward", self.__forward)
+        banks = banks.lower().replace("backward", self.__backward)
+
+        self.log.information('Loading data from banks:' + banks.replace("bank", ""))
+
+        ws = None
+        wksp_name = "__tmp"
+
+        ws = mantid.simpleapi.LoadEventNexus(Filename=filename, BankName=banks, OutputWorkspace=wksp_name)
+
+        self.setProperty("OutputWorkspace", ws)
+        mantid.simpleapi.DeleteWorkspace(wksp_name)
+
+# Register
+AlgorithmFactory.subscribe(LoadVISIONinelastic)
+
 
