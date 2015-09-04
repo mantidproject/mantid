bf7ba9f8c41c0ddb73e9e168029d98f06564ab57
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-09-04 11:42:47 +0100

    Added initial content to classes (ui is clone of applyPaalmans)
    Refs #13110

---
+++ Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Indirect/ContainerSubtraction.h
@@ -0,0 +1,24 @@
+#ifndef MANTIDQTCUSTOMINTERFACESIDA_CONTAINERSUBTRACTION_H_
+#define MANTIDQTCUSTOMINTERFACESIDA_CONTAINERSUBTRACTION_H_
+
+#include "ui_ContainerSubtraction.h"
+#include "CorrectionsTab.h"
+
+namespace MantidQt {
+namespace CustomInterfaces {
+class DLLExport ContainerSubtraction : public CorrectionsTab {
+  Q_OBJECT
+
+public:
+  ContainerSubtraction(QWidget *parent = 0);
+
+private slots:
+
+private:
+  Ui::ContainerSubtraction m_uiForm;
+};
+
+} // namespace CustomInterfaces
+} // namespace MantidQt
+
+#endif /* MANTIDQTCUSTOMINTERFACESIDA_CONTAINERSUBTRACTION_H_ */
---
+++ Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ContainerSubtraction.cpp
@@ -0,0 +1,16 @@
+#include "MantidQtCustomInterfaces/Indirect/ContainerSubtraction.h"
+
+using namespace Mantid::API;
+
+namespace {
+Mantid::Kernel::Logger g_log("ContainerSubtraction");
+}
+
+namespace MantidQt {
+namespace CustomInterfaces {
+ContainerSubtraction::ContainerSubtraction(QWidget *parent)
+    : CorrectionsTab(parent) {
+  m_uiForm.setupUi(parent);
+}
+}
+}
