4144a7a704fbe0a5fbfd7560f85c935d448b1339
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-09-07 08:45:47 +0100

    Added Slots and signals for ui changes
    Refs #13110

--- Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Indirect/ContainerSubtraction.h
+++ Code/Mantid/MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Indirect/ContainerSubtraction.h
@@ -13,6 +13,16 @@ public:
   ContainerSubtraction(QWidget *parent = 0);
 
 private slots:
+  /// Handles the geometry being changed
+  void handleGeometryChange(int index);
+  /// Handles a new sample being loaded
+  void newData(const QString &dataName);
+  /// Updates the preview mini plot
+  void plotPreview(int specIndex);
+  /// Handle abs. correction algorithm completion
+  void absCorComplete(bool error);
+  /// Handle convert units and save algorithm completion
+  void postProcessComplete(bool error);
 
 private:
   virtual void setup();
--- Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ContainerSubtraction.cpp
+++ Code/Mantid/MantidQt/CustomInterfaces/src/Indirect/ContainerSubtraction.cpp
@@ -11,6 +11,17 @@ namespace CustomInterfaces {
 ContainerSubtraction::ContainerSubtraction(QWidget *parent)
     : CorrectionsTab(parent) {
   m_uiForm.setupUi(parent);
+
+  // Connect slots
+  connect(m_uiForm.cbGeometry, SIGNAL(currentIndexChanged(int)), this,
+          SLOT(handleGeometryChanged(int)));
+  connect(m_uiForm.dsSample, SIGNAL(dataReady(const QString &)), this,
+          SLOT(newData(const QString &)));
+  connect(m_uiForm.spPreviewSpec, SIGNAL(valueChanged(int)), this,
+          SLOT(plotPreview(int)));
+
+  m_uiForm.spPreviewSpec->setMinimum(0);
+  m_uiForm.spPreviewSpec->setMaximum(0);
 }
 
 void ContainerSubtraction::setup() {}
