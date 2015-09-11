4ecb6c2a733e735fbd63b64f047a0f6bf2d8be91
Wenduo Zhou <zhouw@ornl.gov>
2015-09-11 16:22:56 -0400

    Refs #13614. Created files.

---
+++ Code/Mantid/Framework/MDAlgorithms/src/ConvertCWSDMDtoHKL.cpp
@@ -0,0 +1,17 @@
+#include "MantidMDAlgorithms/ConvertCWSDMDtoHKL.h"
+
+namespace Mantid {
+namespace MDAlgorithms {
+
+//----------------------------------------------------------------------------------------------
+/** Constructor
+ */
+ConvertCWSDMDtoHKL::ConvertCWSDMDtoHKL() {}
+
+//----------------------------------------------------------------------------------------------
+/** Destructor
+ */
+ConvertCWSDMDtoHKL::~ConvertCWSDMDtoHKL() {}
+
+} // namespace MDAlgorithms
+} // namespace Mantid
---
+++ Code/Mantid/Framework/MDAlgorithms/test/ConvertCWSDMDtoHKLTest.h
@@ -0,0 +1,27 @@
+#ifndef MANTID_MDALGORITHMS_CONVERTCWSDMDTOHKLTEST_H_
+#define MANTID_MDALGORITHMS_CONVERTCWSDMDTOHKLTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidMDAlgorithms/ConvertCWSDMDtoHKL.h"
+
+using Mantid::MDAlgorithms::ConvertCWSDMDtoHKL;
+
+class ConvertCWSDMDtoHKLTest : public CxxTest::TestSuite {
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static ConvertCWSDMDtoHKLTest *createSuite() { return new ConvertCWSDMDtoHKLTest(); }
+  static void destroySuite( ConvertCWSDMDtoHKLTest *suite ) { delete suite; }
+
+
+  void test_Something()
+  {
+    TS_FAIL( "You forgot to write a test!");
+  }
+
+
+};
+
+
+#endif /* MANTID_MDALGORITHMS_CONVERTCWSDMDTOHKLTEST_H_ */
