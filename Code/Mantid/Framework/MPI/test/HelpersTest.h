9849e1c21c4925cb2d1d8c0d18397618f67c15ce
Simon Heybrock <simon.heybrock@esss.se>
2015-09-14 11:52:58 +0200

    Re #14864 Tests for MPI helpers functions (only for single-rank run).
    These tests do not work/pass when run with more than a single MPI rank.

---
+++ Code/Mantid/Framework/MPI/test/HelpersTest.h
@@ -0,0 +1,19 @@
+#ifndef MPI_HELPERS_TEST_H_
+#define MPI_HELPERS_TEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidMPI/Helpers.h"
+
+using namespace Mantid::MPI;
+
+class MPIHelpersTest : public CxxTest::TestSuite {
+public:
+  void testCommunicator() { TS_ASSERT_THROWS_NOTHING(communicator()); }
+  void testRank() { TS_ASSERT_EQUALS(rank(), 0); }
+  void testRootRank() { TS_ASSERT_EQUALS(rootRank(), 0); }
+  void testIsRoot() { TS_ASSERT(isRoot()); }
+  void testNumberOfRanks() { TS_ASSERT_EQUALS(numberOfRanks(), 1); }
+};
+
+#endif /* MPI_HELPERS_TEST_H_ */
