768b7cec8c662c4eb807d3be792ba3e599551039
Stuart Campbell <campbellsi@ornl.gov>
2015-09-17 17:12:32 -0400

    Added ability to naturally sort the bank list

--- Code/Mantid/Framework/PythonInterface/plugins/algorithms/LoadVisionInelastic.py
+++ Code/Mantid/Framework/PythonInterface/plugins/algorithms/LoadVisionInelastic.py
@@ -6,6 +6,34 @@ from mantid.api import *
 from mantid.kernel import *
 import mantid.simpleapi
 
+def try_int(s):
+    "Convert to integer if possible."
+    try: return int(s)
+    except: return s
+
+def natsort_key(s):
+    "Used internally to get a tuple by which s is sorted."
+    import re
+    return map(try_int, re.findall(r'(\d+|\D+)', s))
+
+def natcmp(a, b):
+    "Natural string comparison, case sensitive."
+    return cmp(natsort_key(a), natsort_key(b))
+
+def natcasecmp(a, b):
+    "Natural string comparison, ignores case."
+    return natcmp(a.lower(), b.lower())
+
+def natsort(seq, cmp=natcmp):
+    "In-place natural string sort."
+    seq.sort(cmp)
+
+def natsorted(seq, cmp=natcmp):
+    "Returns a copy of seq, sorted by natural string sort."
+    import copy
+    temp = copy.copy(seq)
+    natsort(temp, cmp)
+    return temp
 
 class LoadVisionInelastic(PythonAlgorithm):
 
@@ -35,9 +63,11 @@ class LoadVisionInelastic(PythonAlgorithm):
         banks = banks.lower().replace("forward", self.__forward)
         banks = banks.lower().replace("backward", self.__backward)
 
-        # Let's make sure we have a unique list
+        # Let's make sure we have a unique and naturally sorted list
         banks_list = banks.split(",")
-        banks = ",".join(sorted(set(banks_list)))
+        banks = ",".join(natsorted(list(set(banks_list))))
+
+        print banks
 
         self.getLogger().information('Loading data from banks:' + banks.replace("bank", ""))
 
