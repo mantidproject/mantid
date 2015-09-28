073fa52a91910d751aca479bf5511ce3419a25cb
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2015-09-28 16:20:08 +0100

    Re #11868 Added necessary properties

--- Code/Mantid/Framework/Algorithms/src/CalMuonDetectorPhases.cpp
+++ Code/Mantid/Framework/Algorithms/src/CalMuonDetectorPhases.cpp
@@ -1,5 +1,7 @@
 #include "MantidAlgorithms/CalMuonDetectorPhases.h"
 
+#include "MantidAPI/ITableWorkspace.h"
+
 namespace Mantid {
 namespace Algorithms {
 
@@ -13,12 +15,31 @@ DECLARE_ALGORITHM(CalMuonDetectorPhases)
 /** Initialize the algorithm's properties.
  */
 void CalMuonDetectorPhases::init() {
+
   declareProperty(
       new API::WorkspaceProperty<>("InputWorkspace", "", Direction::Input),
-      "An input workspace.");
-  declareProperty(
-      new API::WorkspaceProperty<>("OutputWorkspace", "", Direction::Output),
-      "An output workspace.");
+      "Name of the reference input workspace");
+
+  declareProperty("FirstGoodData", EMPTY_DBL(),
+                  "The first good data point in units of "
+                  "micro-seconds as measured from time "
+                  "zero",
+                  Direction::Input);
+
+  declareProperty("LastGoodData", EMPTY_DBL(),
+                  "The last good data point in units of "
+                  "micro-seconds as measured from time "
+                  "zero",
+                  Direction::Input);
+
+  declareProperty("Frequency", EMPTY_DBL(), "Starting hing for the frequency",
+                  Direction::Input);
+
+  declareProperty(new API::WorkspaceProperty<API::ITableWorkspace>(
+                      "DetectorTable", "", Direction::Output),
+                  "The name of the TableWorkspace in which to store the list "
+                  "of phases and asymmetries for each detector");
+
 }
 
 //----------------------------------------------------------------------------------------------
@@ -28,7 +49,8 @@ void CalMuonDetectorPhases::exec() {
 
   API::MatrixWorkspace_sptr inputWS = getProperty("InputWorkspace");
 
-  setProperty("OutputWorkspace",inputWS);
+  API::ITableWorkspace_sptr tab = API::WorkspaceFactory::Instance().createTable("TableWorkspace");
+  setProperty("DetectorTable",tab);
 }
 
 } // namespace Algorithms
