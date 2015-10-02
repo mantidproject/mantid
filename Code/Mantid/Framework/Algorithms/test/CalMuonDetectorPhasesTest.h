b7f8c42e04b8132c6dced5acb71349b9e98a21c3
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2015-10-02 12:27:45 +0100

    Re #11868 Update unit test

--- Code/Mantid/Framework/Algorithms/test/CalMuonDetectorPhasesTest.h
+++ Code/Mantid/Framework/Algorithms/test/CalMuonDetectorPhasesTest.h
@@ -5,6 +5,7 @@
 
 #include "MantidAPI/FrameworkManager.h"
 #include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/ITableWorkspace.h"
 #include <boost/assign/list_of.hpp>
 
 using namespace Mantid::API;
@@ -34,46 +35,64 @@ public:
 
   void testExecute()
   {
-    auto ws = createWorkspace("Microseconds");
+    auto ws = createWorkspace(4, 100, "Microseconds");
     auto calc = AlgorithmManager::Instance().create("CalMuonDetectorPhases");
     calc->initialize();
     calc->setChild(true);
     calc->setProperty("InputWorkspace", ws);
-    calc->setProperty("Frequency", 25.);
+    calc->setPropertyValue("Frequency", "25");
     calc->setPropertyValue("DataFitted", "fit");
     calc->setPropertyValue("DetectorTable", "tab");
 
     TS_ASSERT_THROWS_NOTHING(calc->execute());
 
     WorkspaceGroup_sptr fitResults = calc->getProperty("DataFitted");
-    ITableWorkspace_sptr detTab = calc->getProperty("DetectorTable");
+    ITableWorkspace_sptr tab = calc->getProperty("DetectorTable");
+
+    // Check the table workspace
+    TS_ASSERT_EQUALS(tab->rowCount(), 4);
+    TS_ASSERT_EQUALS(tab->columnCount(), 3);
+    // Test asymmetries
+    TS_ASSERT_DELTA(tab->Double(0,1), 0.099, 0.001);
+    TS_ASSERT_DELTA(tab->Double(1,1), 0.099, 0.001);
+    TS_ASSERT_DELTA(tab->Double(2,1), 0.099, 0.001);
+    TS_ASSERT_DELTA(tab->Double(3,1), 0.100, 0.001);
+    // Test phases
+    TS_ASSERT_DELTA(tab->Double(0,2), 6.281, 0.001);
+    TS_ASSERT_DELTA(tab->Double(1,2), 0.785, 0.001);
+    TS_ASSERT_DELTA(tab->Double(2,2), 1.570, 0.001);
+    TS_ASSERT_DELTA(tab->Double(3,2), 2.354, 0.001);
+
   }
 
-  MatrixWorkspace_sptr createWorkspace(std::string units) {
+  MatrixWorkspace_sptr createWorkspace(size_t nspec, size_t maxt, std::string units) {
 
     // Create a fake muon dataset
     double a = 0.1; // Amplitude of the oscillations
     double w = 25.; // Frequency of the oscillations
     double tau = 2.2; // Muon life time
-    size_t maxt = 100; // Maximum time
 
-    MantidVec X(maxt);
-    MantidVec Y(maxt);
-    for (size_t s = 0; s < 4; s++) {
+    MantidVec X;
+    MantidVec Y;
+    MantidVec E;
+    for (size_t s = 0; s < nspec; s++) {
       for (size_t t = 0; t < maxt; t++) {
         double x = static_cast<double>(t) / maxt;
         double e = exp(-x / tau);
         X.push_back(x);
-        Y.push_back(a * sin(w * t + s * M_PI / 4.) * e + e);
+        Y.push_back(a * sin(w * x + s * M_PI / nspec) * e + e);
+        E.push_back(0.005);
       }
     }
+
     auto createWS = AlgorithmManager::Instance().create("CreateWorkspace");
     createWS->initialize();
     createWS->setChild(true);
     createWS->setProperty("UnitX", units);
     createWS->setProperty("DataX", X);
     createWS->setProperty("DataY", Y);
-    createWS->setProperty("NSpec", 4);
+    createWS->setProperty("DataE", E);
+    createWS->setProperty("NSpec", static_cast<int>(nspec));
     createWS->setPropertyValue("OutputWorkspace", "ws");
     createWS->execute();
     MatrixWorkspace_sptr ws = createWS->getProperty("OutputWorkspace");
