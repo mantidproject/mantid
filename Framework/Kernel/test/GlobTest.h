1c11489eab947f7ec823cf37b9161aad9679a762
Ross Whitfield <whitfieldre@ornl.gov>
2015-10-05 15:34:24 -0400

    Fix Kernel Glob test.

--- Framework/Kernel/test/GlobTest.h
+++ Framework/Kernel/test/GlobTest.h
@@ -27,7 +27,7 @@ public:
   }
 
   void test_Glob() {
-    TS_ASSERT_EQUALS(base[base.depth() - 1], "Mantid");
+    TS_ASSERT_EQUALS(base[base.depth() - 1], "mantid");
     std::string pattern = base.toString() + "Framework/*/CMakeLists.*t";
 
     std::set<std::string> files;
@@ -90,9 +90,9 @@ public:
   }
 
   void test_double_dots_in_pattern() {
-    TS_ASSERT_EQUALS(base[base.depth() - 1], "Mantid");
+    TS_ASSERT_EQUALS(base[base.depth() - 1], "mantid");
     std::string pattern =
-        base.toString() + "../Mantid/Framework/*/CMakeLists.*t";
+        base.toString() + "Framework/*/CMakeLists.*t";
 
     std::set<std::string> files;
     Glob::glob(pattern, files);
