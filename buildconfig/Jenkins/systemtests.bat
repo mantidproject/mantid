8a46d826ecc3923e335f2bf56b7cb16c5f2f62b9
Stuart Campbell <campbellsi@ornl.gov>
2015-10-05 17:41:23 -0400

    fixed systemtest paths

--- buildconfig/Jenkins/systemtests.bat
+++ buildconfig/Jenkins/systemtests.bat
@@ -37,7 +37,7 @@ cd %WORKSPACE%\build
 :: We use the special flag that only creates the targets for the data
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 if not EXIST %WORKSPACE%\build\CMakeCache.txt (
-  "%CMAKE_BIN_DIR%\cmake" -DMANTID_DATA_STORE=!MANTID_DATA_STORE! -DDATA_TARGETS_ONLY=ON ..\Code\Mantid
+  "%CMAKE_BIN_DIR%\cmake" -DMANTID_DATA_STORE=!MANTID_DATA_STORE! -DDATA_TARGETS_ONLY=ON ..
 ) else (
   :: This ensures that any new data files are picked up by the cmake globbing
   "%CMAKE_BIN_DIR%\cmake" .
@@ -63,5 +63,5 @@ echo usagereports.enabled = 0 >> %USERPROPS%
 :: Run
 set PKGDIR=%WORKSPACE%\build
 set PATH=C:\MantidInstall\bin;C:\MantidInstall\plugins;%PATH%
-python %WORKSPACE%\Code\Mantid\Testing\SystemTests\scripts\InstallerTests.py -o -d %PKGDIR%
+python %WORKSPACE%\Testing\SystemTests\scripts\InstallerTests.py -o -d %PKGDIR%
 
