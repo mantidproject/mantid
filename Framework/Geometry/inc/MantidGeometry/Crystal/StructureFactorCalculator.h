7c1cc8f07592ef4d8b7a90139e4ce548965764bc
Michael Wedel <michael.wedel@esss.se>
2015-10-06 21:42:44 +0200

    Refs #13537. Re-adding files after repo-move

---
+++ Framework/Geometry/inc/MantidGeometry/Crystal/StructureFactorCalculator.h
@@ -0,0 +1,82 @@
+#ifndef MANTID_GEOMETRY_STRUCTUREFACTORCALCULATOR_H_
+#define MANTID_GEOMETRY_STRUCTUREFACTORCALCULATOR_H_
+
+#include "MantidGeometry/DllConfig.h"
+#include "MantidGeometry/Crystal/CrystalStructure.h"
+
+namespace Mantid {
+namespace Geometry {
+
+/** StructureFactorCalculator
+
+ This is a base class for concrete structure factor calculators. It is used
+ to logically separate this calculation from CrystalStructure so that different
+ methods of calculation can be used. For actual implementations please consult
+ the available sub-classes.
+
+      @author Michael Wedel, ESS
+      @date 05/09/2015
+
+  Copyright &copy; 2015 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
+  National Laboratory & European Spallation Source
+
+  This file is part of Mantid.
+
+  Mantid is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  Mantid is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  File change history is stored at: <https://github.com/mantidproject/mantid>
+  Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class MANTID_GEOMETRY_DLL StructureFactorCalculator {
+public:
+  StructureFactorCalculator();
+  virtual ~StructureFactorCalculator() {}
+
+  void setCrystalStructure(const CrystalStructure &crystalStructure);
+
+  /// In implementations this method should return the structure factor for the
+  /// specified HKL.
+  virtual StructureFactor getF(const Kernel::V3D &hkl) const = 0;
+  virtual double getFSquared(const Kernel::V3D &hkl) const;
+
+  virtual std::vector<StructureFactor>
+  getFs(const std::vector<Kernel::V3D> &hkls) const;
+  virtual std::vector<double>
+  getFsSquared(const std::vector<Kernel::V3D> &hkls) const;
+
+protected:
+  virtual void
+  crystalStructureSetHook(const CrystalStructure &crystalStructure);
+};
+
+typedef boost::shared_ptr<StructureFactorCalculator>
+    StructureFactorCalculator_sptr;
+
+namespace StructureFactorCalculatorFactory {
+/// Small templated factory function that creates the desired calculator
+/// and initializes it by setting the crystal structure.
+template <typename T>
+StructureFactorCalculator_sptr
+create(const CrystalStructure &crystalStructure) {
+  boost::shared_ptr<T> calculator = boost::make_shared<T>();
+  calculator->setCrystalStructure(crystalStructure);
+
+  return calculator;
+}
+}
+
+} // namespace Geometry
+} // namespace Mantid
+
+#endif /* MANTID_GEOMETRY_STRUCTUREFACTORCALCULATOR_H_ */
---
+++ Framework/PythonInterface/test/python/mantid/geometry/ReflectionGeneratorTest.py
@@ -0,0 +1,69 @@
+# pylint: disable=no-init,invalid-name,too-many-public-methods
+import unittest
+from mantid.geometry import CrystalStructure, ReflectionGenerator, ReflectionConditionFilter
+from mantid.kernel import V3D
+import numpy as np
+
+
+class CrystalStructureTest(unittest.TestCase):
+    crystalStructure = CrystalStructure("5.431 5.431 5.431", "F d -3 m", "Si 0 0 0 1.0 0.02")
+
+    def test_create(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        generator = ReflectionGenerator(self.crystalStructure, ReflectionConditionFilter.Centering)
+
+    def test_getHKLs(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        hkls = generator.getHKLs(1.0, 10.0)
+
+        self.assertEqual(len(hkls), 138)
+
+        # default is filtering by space group reflection condition
+        self.assertTrue(V3D(2, 2, 2) in hkls)
+        self.assertFalse(V3D(1, 0, 0) in hkls)
+
+    def test_getHKLsUsingFilter(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        hkls = generator.getHKLsUsingFilter(1.0, 10.0, ReflectionConditionFilter.StructureFactor)
+
+        self.assertEqual(len(hkls), 130)
+
+        # The 222 is gone now
+        self.assertFalse(V3D(2, 2, 2) in hkls)
+
+    def test_getUniqueHKLs(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        hkls = generator.getUniqueHKLs(1.0, 10.0)
+
+        self.assertEqual(len(hkls), 9)
+
+        self.assertTrue(V3D(2, 2, 2) in hkls)
+        self.assertFalse(V3D(1, 0, 0) in hkls)
+
+    def test_getUniqueHKLsUsingFilter(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        hkls = generator.getUniqueHKLsUsingFilter(1.0, 10.0, ReflectionConditionFilter.StructureFactor)
+
+        self.assertEqual(len(hkls), 8)
+        self.assertFalse(V3D(2, 2, 2) in hkls)
+
+    def test_getDValues(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        hkls = [V3D(1, 0, 0), V3D(1, 1, 1)]
+
+        dValues = generator.getDValues(hkls)
+
+        self.assertEqual(len(hkls), len(dValues))
+        self.assertAlmostEqual(dValues[0], 5.431, places=10)
+        self.assertAlmostEqual(dValues[1], 5.431 / np.sqrt(3.), places=10)
+
+    def test_getFsSquared(self):
+        generator = ReflectionGenerator(self.crystalStructure)
+        hkls = generator.getUniqueHKLs(1.0, 10.0)
+
+        fsSquared = generator.getFsSquared(hkls)
+
+        self.assertEqual(len(fsSquared), len(hkls))
+
+if __name__ == '__main__':
+    unittest.main()
