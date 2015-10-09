37fff918f486a9a3e06bbfa36238e66902e9be8b
Wenduo Zhou <zhouw@ornl.gov>
2015-10-08 21:33:10 -0400

    Refs #12122. Enabled ConvertCWSDExpToMomentum to copy properties.

--- Framework/MDAlgorithms/src/ConvertCWSDExpToMomentum.cpp
+++ Framework/MDAlgorithms/src/ConvertCWSDExpToMomentum.cpp
@@ -412,6 +412,13 @@ void ConvertCWSDExpToMomentum::convertSpiceMatrixToMomentumMDEvents(
   }
   expinfo->mutableRun().setGoniometer(dataws->run().getGoniometer(), false);
   expinfo->mutableRun().addProperty("run_number", runnumber);
+  // Add all the other propertys from original data workspace
+  const std::vector<Kernel::Property *> vec_property =
+      dataws->run().getProperties();
+  for (size_t i = 0; i < vec_property.size(); ++i) {
+    expinfo->mutableRun().addProperty(vec_property[i]->clone());
+  }
+
   m_outputWS->addExperimentInfo(expinfo);
 
   return;
