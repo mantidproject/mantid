7898e200b8973cf78b1727ef2e0f730594c03d29
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2015-10-13 18:03:00 +0100

    check retval from json parser + fix test example, 1311473, re #13951

--- Framework/DataHandling/src/CheckMantidVersion.cpp
+++ Framework/DataHandling/src/CheckMantidVersion.cpp
@@ -107,7 +107,14 @@ void CheckMantidVersion::exec() {
   if (!json.empty()) {
     Json::Reader r;
     Json::Value root;
-    r.parse(json, root);
+    bool parseOK = r.parse(json, root);
+    if (!parseOK) {
+      g_log.error() << "Error trying to parse this JSON string from GitHub: "
+                    << json << std::endl;
+      throw std::runtime_error("Error found when parsing version information "
+                               "retrieved from GitHub as a JSON string." +
+                               r.getFormattedErrorMessages());
+    }
 
     std::string gitHubVersionTag = root["tag_name"].asString();
     mostRecentVersion = cleanVersionTag(gitHubVersionTag);
