1c2d65374dc7975b251e6336ae0a79ae69971911
Matt King <matthew.king@stfc.ac.uk>
2015-10-22 11:34:55 +0100

    Now works for old refl gui
    Refs #13758

--- scripts/Interface/ui/reflectometer/refl_gui.py
+++ scripts/Interface/ui/reflectometer/refl_gui.py
@@ -211,7 +211,8 @@ class ReflGui(QtGui.QMainWindow, ui_refl_window.Ui_windowRefl):
 
     def _show_slit_calculator(self):
         calc = mantidqtpython.MantidQt.MantidWidgets.SlitCalculator(self)
-        calc.setCurrentInstrumentName(str(self.current_instrument))
+        calc.setCurrentInstrumentName(self.current_instrument)
+        calc.processInstrumentHasBeenChanged()
         calc.exec_()
 
     def _polar_corr_selected(self):
