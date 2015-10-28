4b06067cd58e8eb323f4ded122abf741d876cb81
Michael Wedel <michael.wedel@esss.se>
2015-10-28 17:54:14 +0100

    Refs #13906. Fixing pylint issue

--- Testing/SystemTests/tests/analysis/PredictPeaksTest.py
+++ Testing/SystemTests/tests/analysis/PredictPeaksTest.py
@@ -1,4 +1,4 @@
-# pylint: disable=no-init
+# pylint: disable=no-init,too-few-public-methods
 import stresstesting
 from mantid.simpleapi import *
 from mantid.geometry import CrystalStructure
@@ -12,12 +12,12 @@ from mantid.geometry import CrystalStructure
 # Instead of using the CheckWorkspacesMatch, only H, K and L are compared.
 class PredictPeaksTestWISH(stresstesting.MantidStressTest):
     def runTest(self):
-        ws = CreateSimulationWorkspace(Instrument='WISH',
-                                       BinParams='0,1,2',
-                                       UnitX='TOF')
+        simulationWorkspace = CreateSimulationWorkspace(Instrument='WISH',
+                                                        BinParams='0,1,2',
+                                                        UnitX='TOF')
 
-        SetUB(ws, a=5.5, b=6.5, c=8.1, u='12,1,1', v='0,4,9')
-        peaks = PredictPeaks(ws,
+        SetUB(simulationWorkspace, a=5.5, b=6.5, c=8.1, u='12,1,1', v='0,4,9')
+        peaks = PredictPeaks(simulationWorkspace,
                              WavelengthMin=0.5, WavelengthMax=6,
                              MinDSpacing=0.5, MaxDSpacing=10)
 
@@ -31,11 +31,11 @@ class PredictPeaksTestWISH(stresstesting.MantidStressTest):
         self.assertEquals(lists_match, True, message)
 
     def _get_hkls(self, peaksWorkspace):
-        h = peaksWorkspace.column('h')
-        k = peaksWorkspace.column('k')
-        l = peaksWorkspace.column('l')
+        h_list = peaksWorkspace.column('h')
+        k_list = peaksWorkspace.column('k')
+        l_list = peaksWorkspace.column('l')
 
-        return [(x, y, z) for x, y, z in zip(h, k, l)]
+        return [(x, y, z) for x, y, z in zip(h_list, k_list, l_list)]
 
     def _compare_hkl_lists(self, lhs, rhs):
         if len(lhs) != len(rhs):
@@ -53,36 +53,36 @@ class PredictPeaksTestWISH(stresstesting.MantidStressTest):
 
 class PredictPeaksTestTOPAZ(stresstesting.MantidStressTest):
     def runTest(self):
-        ws = CreateSimulationWorkspace(Instrument='TOPAZ',
-                                       BinParams='0,1,2',
-                                       UnitX='TOF')
+        simulationWorkspace = CreateSimulationWorkspace(Instrument='TOPAZ',
+                                                        BinParams='0,1,2',
+                                                        UnitX='TOF')
 
-        SetUB(ws, a=5.5, b=6.5, c=8.1, u='12,1,1', v='0,4,9')
-        peaks = PredictPeaks(ws,
+        SetUB(simulationWorkspace, a=5.5, b=6.5, c=8.1, u='12,1,1', v='0,4,9')
+        peaks = PredictPeaks(simulationWorkspace,
                              WavelengthMin=0.5, WavelengthMax=6,
                              MinDSpacing=0.5, MaxDSpacing=10)
 
         reference = LoadNexus('predict_peaks_test_random_ub_topaz.nxs')
 
-        wsMatch = CheckWorkspacesMatch(peaks, reference)
+        simulationWorkspaceMatch = CheckWorkspacesMatch(peaks, reference)
 
-        self.assertEquals(wsMatch, 'Success!')
+        self.assertEquals(simulationWorkspaceMatch, 'Success!')
 
 
 class PredictPeaksCalculateStructureFactorsTest(stresstesting.MantidStressTest):
     def runTest(self):
-        ws = CreateSimulationWorkspace(Instrument='WISH',
-                                       BinParams='0,1,2',
-                                       UnitX='TOF')
+        simulationWorkspace = CreateSimulationWorkspace(Instrument='WISH',
+                                                        BinParams='0,1,2',
+                                                        UnitX='TOF')
 
-        SetUB(ws, a=5.5, b=6.5, c=8.1, u='12,1,1', v='0,4,9')
+        SetUB(simulationWorkspace, a=5.5, b=6.5, c=8.1, u='12,1,1', v='0,4,9')
 
         # Setting some random crystal structure. Correctness of structure factor calculations is ensured in the
         # test suite of StructureFactorCalculator and does not need to be tested here.
-        ws.sample().setCrystalStructure(
+        simulationWorkspace.sample().setCrystalStructure(
             CrystalStructure('5.5 6.5 8.1', 'P m m m', 'Fe 0.121 0.234 0.899 1.0 0.01'))
 
-        peaks = PredictPeaks(ws,
+        peaks = PredictPeaks(simulationWorkspace,
                              WavelengthMin=0.5, WavelengthMax=6,
                              MinDSpacing=0.5, MaxDSpacing=10,
                              CalculateStructureFactors=True)
@@ -93,7 +93,7 @@ class PredictPeaksCalculateStructureFactorsTest(stresstesting.MantidStressTest):
             peak = peaks.getPeak(i)
             self.assertLessThan(0.0, peak.getIntensity())
 
-        peaks_no_sf = PredictPeaks(ws,
+        peaks_no_sf = PredictPeaks(simulationWorkspace,
                                    WavelengthMin=0.5, WavelengthMax=6,
                                    MinDSpacing=0.5, MaxDSpacing=10,
                                    CalculateStructureFactors=False)
