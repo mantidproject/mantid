e965c7e75bba66fb7941a143ff537dc80d62a2bc
Steven Hahn <hahnse@ornl.gov>
2015-10-30 10:18:23 -0400

    Refs #14169. Change make_unique to match gcc implementation.

--- Framework/Kernel/inc/MantidKernel/make_unique.h
+++ Framework/Kernel/inc/MantidKernel/make_unique.h
@@ -14,7 +14,7 @@ namespace Mantid {
 
 namespace Kernel {
 
-#if __cplusplus == 201402L // C++14
+#if __cplusplus > 201103L // C++14
 
 using std::make_unique;
 
@@ -29,7 +29,7 @@ template <class T> struct _Unique_if<T[]> {
 };
 
 template <class T, size_t N> struct _Unique_if<T[N]> {
-  typedef void _Known_bound;
+  struct __invalid_type {};
 };
 
 template <class T>
@@ -41,48 +41,49 @@ typename _Unique_if<T>::_Unknown_bound make_unique(size_t n) {
 #if !defined(_MSC_VER) || _MSC_VER > 1700
 
 template <class T, class... Args>
-typename _Unique_if<T>::_Single_object make_unique(Args &&... args) {
+inline typename _Unique_if<T>::_Single_object make_unique(Args &&... args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 template <class T, class... Args>
-typename _Unique_if<T>::_Known_bound make_unique(Args &&...) = delete;
+inline typename _Unique_if<T>::_Known_bound make_unique(Args &&...) = delete;
 #else
 // workaround for MSVC 2012
 
-template <class T> typename _Unique_if<T>::_Single_object make_unique() {
+template <class T> inline typename _Unique_if<T>::_Single_object make_unique() {
   return std::unique_ptr<T>(new T());
 }
 
 template <class T, class Arg1>
-typename _Unique_if<T>::_Single_object make_unique(Arg1 param1) {
+inline typename _Unique_if<T>::_Single_object make_unique(Arg1 param1) {
   return std::unique_ptr<T>(new T(std::forward<Arg1>(param1)));
 }
 
 template <class T, class Arg1, class Arg2>
-typename _Unique_if<T>::_Single_object make_unique(Arg1 param1, Arg2 param2) {
+inline typename _Unique_if<T>::_Single_object make_unique(Arg1 param1,
+                                                          Arg2 param2) {
   return std::unique_ptr<T>(
       new T(std::forward<Arg1>(param1), std::forward<Arg2>(param2)));
 }
 
 template <class T, class Arg1, class Arg2, class Arg3>
-typename _Unique_if<T>::_Single_object make_unique(Arg1 param1, Arg2 param2,
-                                                   Arg3 param3) {
+inline typename _Unique_if<T>::_Single_object
+make_unique(Arg1 param1, Arg2 param2, Arg3 param3) {
   return std::unique_ptr<T>(new T(std::forward<Arg1>(param1),
                                   std::forward<Arg2>(param2),
                                   std::forward<Arg3>(param3)));
 }
 
 template <class T, class Arg1, class Arg2, class Arg3, class Arg4>
-typename _Unique_if<T>::_Single_object make_unique(Arg1 param1, Arg2 param2,
-                                                   Arg3 param3, Arg4 param4) {
+inline typename _Unique_if<T>::_Single_object
+make_unique(Arg1 param1, Arg2 param2, Arg3 param3, Arg4 param4) {
   return std::unique_ptr<T>(
       new T(std::forward<Arg1>(param1), std::forward<Arg2>(param2),
             std::forward<Arg3>(param3), std::forward<Arg4>(param4)));
 }
 
 template <class T, class Arg1, class Arg2, class Arg3, class Arg4, class Arg5>
-typename _Unique_if<T>::_Single_object
+inline typename _Unique_if<T>::_Single_object
 make_unique(Arg1 param1, Arg2 param2, Arg3 param3, Arg4 param4, Arg5 param5) {
   return std::unique_ptr<T>(
       new T(std::forward<Arg1>(param1), std::forward<Arg2>(param2),
@@ -90,22 +91,23 @@ make_unique(Arg1 param1, Arg2 param2, Arg3 param3, Arg4 param4, Arg5 param5) {
             std::forward<Arg5>(param5)));
 }
 
-template <class T> typename _Unique_if<T>::_Known_bound make_unique() = delete;
+template <class T>
+inline typename _Unique_if<T>::_Known_bound make_unique() = delete;
 template <class T, class Arg1>
-typename _Unique_if<T>::_Known_bound make_unique(Arg1 param1) = delete;
+inline typename _Unique_if<T>::_Known_bound make_unique(Arg1 param1) = delete;
 template <class T, class Arg1, class Arg2>
-typename _Unique_if<T>::_Known_bound make_unique(Arg1 param1,
-                                                 Arg2 param2) = delete;
+inline typename _Unique_if<T>::_Known_bound make_unique(Arg1 param1,
+                                                        Arg2 param2) = delete;
 template <class T, class Arg1, class Arg2, class Arg3>
-typename _Unique_if<T>::_Known_bound make_unique(Arg1 param1, Arg2 param2,
-                                                 Arg3 param3) = delete;
+inline typename _Unique_if<T>::_Known_bound
+make_unique(Arg1 param1, Arg2 param2, Arg3 param3) = delete;
 template <class T, class Arg1, class Arg2, class Arg3, class Arg4>
-typename _Unique_if<T>::_Known_bound
+inline typename _Unique_if<T>::_Known_bound
 make_unique(Arg1 param1, Arg2 param2, Arg3 param3, Arg4 param4) = delete;
 template <class T, class Arg1, class Arg2, class Arg3, class Arg4, class Arg5>
-typename _Unique_if<T>::_Known_bound make_unique(Arg1 param1, Arg2 param2,
-                                                 Arg3 param3, Arg4 param4,
-                                                 Arg5 param5) = delete;
+inline typename _Unique_if<T>::_Known_bound
+make_unique(Arg1 param1, Arg2 param2, Arg3 param3, Arg4 param4,
+            Arg5 param5) = delete;
 #endif // MSVC
 #endif // __cplusplus == 201402L
 }
