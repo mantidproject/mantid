452055f8d9d22f9a702f6ca4310e1384632d9d16
Michael Wedel <michael.wedel@esss.se>
2015-11-08 23:31:47 +0100

    Refs #14204. Finishing up MatrixVectorPairParser

--- Framework/Geometry/inc/MantidGeometry/Crystal/MatrixVectorPairParser.h
+++ Framework/Geometry/inc/MantidGeometry/Crystal/MatrixVectorPairParser.h
@@ -8,11 +8,15 @@
 #include "MantidKernel/Exception.h"
 
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_auto.hpp>
 
 namespace Mantid {
 namespace Geometry {
 
-typedef boost::fusion::vector<int, boost::optional<int>> ParsedRationalNumber;
+typedef boost::fusion::vector<int, boost::optional<int> > ParsedRationalNumber;
+
+using boost::spirit::qi::grammar;
+using boost::spirit::qi::rule;
 
 /** MatrixVectorPairParser
 
@@ -27,7 +31,7 @@ typedef boost::fusion::vector<int, boost::optional<int>> ParsedRationalNumber;
 
   To reuse the internally constructed boost::spirit-based parser,
   simply instantiate MatrixVectorPairParser once and use the
-  parseMatrix
+  parse-method.
 
       @author Michael Wedel, ESS
       @date 02/11/2015
@@ -53,22 +57,80 @@ typedef boost::fusion::vector<int, boost::optional<int>> ParsedRationalNumber;
   File change history is stored at: <https://github.com/mantidproject/mantid>
   Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
-class MANTID_GEOMETRY_DLL MatrixVectorPairParser {
+
+class MANTID_GEOMETRY_DLL MatrixVectorPairParser
+    : public grammar<std::string::const_iterator,
+                     boost::spirit::qi::space_type> {
 public:
-  MatrixVectorPairParser();
-  ~MatrixVectorPairParser() {}
+  MatrixVectorPairParser()
+      : MatrixVectorPairParser::base_type(m_parser), m_directions(),
+        m_matrixRows(3), m_vector(), m_currentFactor(1),
+        m_currentDirection(0, 0, 0), m_currentSign(1), m_currentRow(0) {
+    m_directions.insert(std::make_pair("x", V3R(1, 0, 0)));
+    m_directions.insert(std::make_pair("y", V3R(0, 1, 0)));
+    m_directions.insert(std::make_pair("z", V3R(0, 0, 1)));
+
+    namespace qi = boost::spirit::qi;
+
+    using std::placeholders::_1;
+    using qi::int_;
+    using qi::uint_;
+    using qi::lit;
+
+    // Switch sign in the builder
+    m_sign =
+        lit('+')
+            [std::bind(&MatrixVectorPairParser::setCurrentSignPositive, this)] |
+        lit('-')
+            [std::bind(&MatrixVectorPairParser::setCurrentSignNegative, this)];
+
+    // This matches a rational number (also things like -1/-2 -> 1/2)
+    m_rational = (int_ >> -('/' >> int_))
+        [std::bind(&MatrixVectorPairParser::setCurrentFactor, this, _1)];
+
+    // Matches x, y or z.
+    m_direction = (qi::string("x") | qi::string("y") | qi::string("z"))
+        [std::bind(&MatrixVectorPairParser::setCurrentDirection, this, _1)];
+
+    /* A "component", which is either a rational number possibly followed by a
+     * vector
+     * definition. Examples:
+     *    2/3, -2/3, -2/-3, -4, 2, -2x, 3y, 4/5z, ...
+     * Or a vector possibly preceded by a sign:
+     *    z, -y, +x
+     */
+    m_component = (m_rational >> -m_direction) | (-m_sign >> m_direction);
+
+    // One row of the matrix/vector pair can have many such "components".
+    m_componentSeries =
+        m_component[std::bind(&MatrixVectorPairParser::addCurrentStateToResult,
+                              this)] >>
+        *(m_component[std::bind(
+             &MatrixVectorPairParser::addCurrentStateToResult, this)]);
+
+    // The entire matrix/vector pair is defined by three comma separated of
+    // those
+    // component strings.
+    m_parser =
+        (m_componentSeries >>
+         lit(',')[std::bind(&MatrixVectorPairParser::advanceRow, this)] >>
+         m_componentSeries >>
+         lit(',')[std::bind(&MatrixVectorPairParser::advanceRow, this)] >>
+         m_componentSeries);
+  }
 
   /// Tries to parse the given string. Throws a ParseError-exception if there is
   /// unparsable string left at the end.
   template <typename T>
   MatrixVectorPair<T, V3R> parse(const std::string &matrixVectorString) {
+    reset();
+
     namespace qi = boost::spirit::qi;
 
     std::string::const_iterator strIterator = matrixVectorString.begin();
     std::string::const_iterator strEnd = matrixVectorString.end();
 
-    m_builder.reset();
-    qi::phrase_parse(strIterator, strEnd, m_parser, qi::space);
+    qi::phrase_parse(strIterator, strEnd, *this, qi::space);
 
     if (std::distance(strIterator, strEnd) > 0) {
       throw Kernel::Exception::ParseError(
@@ -77,93 +139,129 @@ public:
           "", 0);
     }
 
-    return m_builder.getMatrixVectorPair<T>();
+    return getMatrixVectorPair<T>();
   }
 
 private:
-  /// Helper class to build up matrix/vector pair in parser.
-  class MatrixVectorPairBuilder {
-  public:
-    MatrixVectorPairBuilder();
-
-    /// Construct and return the actual matrix/vector pair, the rational matrix
-    /// components are castd to T.
-    template <typename T> MatrixVectorPair<T, V3R> getMatrixVectorPair() const {
-      if (m_currentRow < 2) {
-        throw Kernel::Exception::ParseError(
-            "Less than three rows were processed by MatrixVectorPairBuilder.",
-            "", 0);
-      }
-
-      std::vector<T> dbl;
-      for (auto rb : m_matrixRows) {
-        dbl.push_back(boost::rational_cast<T>((rb).x()));
-        dbl.push_back(boost::rational_cast<T>((rb).y()));
-        dbl.push_back(boost::rational_cast<T>((rb).z()));
-      }
-      Kernel::Matrix<T> mat(dbl);
-
-      return MatrixVectorPair<T, V3R>(mat, m_vector);
+  /// Construct and return the actual matrix/vector pair, the rational matrix
+  /// components are castd to T.
+  template <typename T> MatrixVectorPair<T, V3R> getMatrixVectorPair() const {
+    if (m_currentRow < 2) {
+      throw Kernel::Exception::ParseError(
+          "Less than three rows were processed by MatrixVectorPairBuilder.", "",
+          0);
     }
 
-    /// Returns the parsed vector.
-    const V3R &getVector() const { return m_vector; }
+    std::vector<T> dbl;
+    for (auto rb : m_matrixRows) {
+      dbl.push_back(boost::rational_cast<T>((rb).x()));
+      dbl.push_back(boost::rational_cast<T>((rb).y()));
+      dbl.push_back(boost::rational_cast<T>((rb).z()));
+    }
+    Kernel::Matrix<T> mat(dbl);
 
-    void setCurrentFactor(ParsedRationalNumber rationalNumberComponents);
-    void setCurrentDirection(std::string vector);
+    return MatrixVectorPair<T, V3R>(mat, m_vector);
+  }
 
-    /// Make the current sign negative.
-    void setCurrentSignNegative() { m_currentSign = -1; }
+  /// Set the current factor, which is a rational number. Depending on whether a
+  /// direction definition follows, it's processed differently later on.
+  void setCurrentFactor(ParsedRationalNumber rationalNumberComponents) {
+    int numerator = boost::fusion::at_c<0>(rationalNumberComponents);
+    boost::optional<int> denominator =
+        boost::fusion::at_c<1>(rationalNumberComponents);
 
-    /// Make the current sign positive.
-    void setCurrentSignPositive() { m_currentSign = 1; }
+    if (denominator.is_initialized()) {
+      m_currentFactor = RationalNumber(numerator, denominator.get());
+    } else {
+      m_currentFactor = RationalNumber(numerator);
+    }
+  }
 
-    void addCurrentStateToResult();
+  /// Set the direction vector to one of the stored values that correspond to x,
+  /// y
+  /// or z.
+  void setCurrentDirection(std::string vector) {
+    m_currentDirection = m_directions[vector];
+  }
 
-    /// Advance to the next row of the matrix/vector pair.
-    void advanceRow() {
-      ++m_currentRow;
-      resetState();
+  /// Make the current sign negative.
+  void setCurrentSignNegative() { m_currentSign = -1; }
+
+  /// Make the current sign positive.
+  void setCurrentSignPositive() { m_currentSign = 1; }
+
+  /**
+   * Adds currently stored state to the parse result
+   *
+   * This function takes the current factor, sign and direction that were
+   *assigned
+   * using the respective functions and adds to the stored intermediate result.
+   *
+   * If the current direction is (0,0,0) then the currently processed component
+   *is
+   * only a rational number that should go into the vector component of the row
+   * that's currently being assembled. Otherwise the direction vector should be
+   * multiplied by the rational number and added to the matrix row.
+   *
+   * After that, the current state is reset so that the next part of the current
+   * component can be processed.
+   */
+  void addCurrentStateToResult() {
+    if (m_currentRow > m_matrixRows.size()) {
+      throw Kernel::Exception::ParseError(
+          "MatrixVectorPair can not have more than 3 rows.", "", 0);
     }
 
-    /// Completely reset the builder, including stored preliminary results.
-    void reset() {
-      resetState();
-      resetAccumulatedResults();
+    m_currentFactor *= m_currentSign;
+
+    if (m_currentDirection != V3R(0, 0, 0)) {
+      m_matrixRows[m_currentRow] += (m_currentDirection * m_currentFactor);
+    } else {
+      m_vector[m_currentRow] += m_currentFactor;
     }
 
-  private:
-    /// Reset the current state, i.e. the state representing the current row.
-    void resetState() {
-      m_currentFactor = RationalNumber(1);
-      m_currentDirection = V3R(0, 0, 0);
-      m_currentSign = 1;
-    }
+    resetState();
+  }
 
-    /// Reset all accumulated results.
-    void resetAccumulatedResults() {
-      m_matrixRows = std::vector<V3R>(3);
-      m_vector = V3R(0, 0, 0);
-      m_currentRow = 0;
-    }
+  /// Advance to the next row of the matrix/vector pair.
+  void advanceRow() {
+    ++m_currentRow;
+    resetState();
+  }
 
-    std::map<std::string, V3R> m_directions;
+  /// Completely reset the builder, including stored preliminary results.
+  void reset() {
+    resetState();
+    resetAccumulatedResults();
+  }
 
-    std::vector<V3R> m_matrixRows;
-    V3R m_vector;
+  /// Reset the current state, i.e. the state representing the current row.
+  void resetState() {
+    m_currentFactor = RationalNumber(1);
+    m_currentDirection = V3R(0, 0, 0);
+    m_currentSign = 1;
+  }
 
-    RationalNumber m_currentFactor;
-    V3R m_currentDirection;
-    int m_currentSign;
+  /// Reset all accumulated results.
+  void resetAccumulatedResults() {
+    m_matrixRows = std::vector<V3R>(3);
+    m_vector = V3R(0, 0, 0);
+    m_currentRow = 0;
+  }
 
-    size_t m_currentRow;
-  };
+  rule<std::string::const_iterator, boost::spirit::qi::space_type> m_sign,
+      m_rational, m_direction, m_component, m_componentSeries, m_parser;
 
-  void initializeParser();
+  std::map<std::string, V3R> m_directions;
 
-  MatrixVectorPairBuilder m_builder;
-  boost::spirit::qi::rule<std::string::const_iterator,
-                          boost::spirit::qi::space_type> m_parser;
+  std::vector<V3R> m_matrixRows;
+  V3R m_vector;
+
+  RationalNumber m_currentFactor;
+  V3R m_currentDirection;
+  int m_currentSign;
+
+  size_t m_currentRow;
 };
 
 } // namespace Geometry
--- Framework/Geometry/src/Crystal/MatrixVectorPairParser.cpp
+++ Framework/Geometry/src/Crystal/MatrixVectorPairParser.cpp
@@ -1,134 +1,16 @@
 #include "MantidGeometry/Crystal/MatrixVectorPairParser.h"
 #include <functional>
+#include <boost/make_shared.hpp>
 
 namespace Mantid {
 namespace Geometry {
 
-MatrixVectorPairParser::MatrixVectorPairBuilder::MatrixVectorPairBuilder()
-    : m_directions(), m_matrixRows(3), m_vector(), m_currentFactor(1),
-      m_currentDirection(0, 0, 0), m_currentSign(1), m_currentRow(0) {
-  m_directions.insert(std::make_pair("x", V3R(1, 0, 0)));
-  m_directions.insert(std::make_pair("y", V3R(0, 1, 0)));
-  m_directions.insert(std::make_pair("z", V3R(0, 0, 1)));
-}
 
-/// Set the current factor, which is a rational number. Depending on whether a
-/// direction definition follows, it's processed differently later on.
-void MatrixVectorPairParser::MatrixVectorPairBuilder::setCurrentFactor(
-    ParsedRationalNumber rationalNumberComponents) {
-  int numerator = boost::fusion::at_c<0>(rationalNumberComponents);
-  boost::optional<int> denominator =
-      boost::fusion::at_c<1>(rationalNumberComponents);
 
-  if (denominator.is_initialized()) {
-    m_currentFactor = RationalNumber(numerator, denominator.get());
-  } else {
-    m_currentFactor = RationalNumber(numerator);
-  }
-}
 
-/// Set the direction vector to one of the stored values that correspond to x, y
-/// or z.
-void MatrixVectorPairParser::MatrixVectorPairBuilder::setCurrentDirection(
-    std::string vector) {
-  m_currentDirection = m_directions[vector];
-}
 
-/**
- * Adds currently stored state to the parse result
- *
- * This function takes the current factor, sign and direction that were assigned
- * using the respective functions and adds to the stored intermediate result.
- *
- * If the current direction is (0,0,0) then the currently processed component is
- * only a rational number that should go into the vector component of the row
- * that's currently being assembled. Otherwise the direction vector should be
- * multiplied by the rational number and added to the matrix row.
- *
- * After that, the current state is reset so that the next part of the current
- * component can be processed.
- */
-void MatrixVectorPairParser::MatrixVectorPairBuilder::
-    addCurrentStateToResult() {
-  if (m_currentRow > m_matrixRows.size()) {
-    throw Kernel::Exception::ParseError(
-        "MatrixVectorPair can not have more than 3 rows.", "", 0);
-  }
 
-  m_currentFactor *= m_currentSign;
 
-  if (m_currentDirection != V3R(0, 0, 0)) {
-    m_matrixRows[m_currentRow] += (m_currentDirection * m_currentFactor);
-  } else {
-    m_vector[m_currentRow] += m_currentFactor;
-  }
-
-  resetState();
-}
-
-/// Constructor, initializes the parser.
-MatrixVectorPairParser::MatrixVectorPairParser() : m_builder(), m_parser() {
-  initializeParser();
-}
-
-/// Initialize the boost::spirit-based parser.
-void MatrixVectorPairParser::initializeParser() {
-  namespace qi = boost::spirit::qi;
-
-  using std::placeholders::_1;
-  using qi::int_;
-  using qi::uint_;
-  using qi::lit;
-
-  // Switch sign in the builder
-  auto sign =
-      lit('+')[std::bind(&MatrixVectorPairParser::MatrixVectorPairBuilder::
-                             setCurrentSignPositive,
-                         &m_builder)] |
-      lit('-')[std::bind(&MatrixVectorPairParser::MatrixVectorPairBuilder::
-                             setCurrentSignNegative,
-                         &m_builder)];
-
-  // This matches a rational number (also things like -1/-2 -> 1/2)
-  auto rational = (int_ >> -('/' >> int_))[std::bind(
-      &MatrixVectorPairParser::MatrixVectorPairBuilder::setCurrentFactor,
-      &m_builder, _1)];
-
-  // Matches x, y or z.
-  auto vector = (qi::string("x") | qi::string("y") | qi::string("z"))[std::bind(
-      &MatrixVectorPairParser::MatrixVectorPairBuilder::setCurrentDirection,
-      &m_builder, _1)];
-
-  /* A "component", which is either a rational number possibly followed by a
-   * vector
-   * definition. Examples:
-   *    2/3, -2/3, -2/-3, -4, 2, -2x, 3y, 4/5z, ...
-   * Or a vector possibly preceded by a sign:
-   *    z, -y, +x
-   */
-  auto component = (rational >> -vector) | (-sign >> vector);
-
-  // One row of the matrix/vector pair can have many such "components".
-  auto componentSeries =
-      component[std::bind(&MatrixVectorPairParser::MatrixVectorPairBuilder::
-                              addCurrentStateToResult,
-                          &m_builder)] >>
-      *(component[std::bind(&MatrixVectorPairParser::MatrixVectorPairBuilder::
-                                addCurrentStateToResult,
-                            &m_builder)]);
-
-  // The entire matrix/vector pair is defined by three comma separated of those
-  // component strings.
-  m_parser = (componentSeries >>
-              lit(',')[std::bind(
-                  &MatrixVectorPairParser::MatrixVectorPairBuilder::advanceRow,
-                  &m_builder)] >>
-              componentSeries >>
-              lit(',')[std::bind(
-                  &MatrixVectorPairParser::MatrixVectorPairBuilder::advanceRow,
-                  &m_builder)] >>
-              componentSeries);
-}
 
 } // namespace Geometry
 } // namespace Mantid
