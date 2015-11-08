c7d7063bf4b7d9ebdca82db596d192c30568540f
Martyn Gigg <martyn.gigg@gmail.com>
2015-11-08 14:35:14 +0000

    Fix doxygen warnings.

--- MantidQt/API/inc/MantidQtAPI/GraphOptions.h
+++ MantidQt/API/inc/MantidQtAPI/GraphOptions.h
@@ -1,9 +1,8 @@
 #ifndef MANTIDGRAPHOPTIONS_H_
 #define MANTIDGRAPHOPTIONS_H_
-#include "DllOption.h"
 
 /**
- * This file contains declarations of options such as scale types that are 
+ * This file contains declarations of options such as scale types that are
  * shared between the colormaps and instrument window
  */
 namespace GraphOptions
@@ -12,12 +11,12 @@ namespace GraphOptions
 /**
  * Scale type enumeration
  */
-  enum EXPORT_OPT_MANTIDQT_API ScaleType { Linear = 0, Log10, Power };
+  enum ScaleType { Linear = 0, Log10, Power };
 
   /**
    * Axis choice
    */
-  enum EXPORT_OPT_MANTIDQT_API Axis { Left, Right, Bottom, Top };
+  enum Axis { Left, Right, Bottom, Top };
 }
 
 #endif //MANTIDSCALETYPE_H_
