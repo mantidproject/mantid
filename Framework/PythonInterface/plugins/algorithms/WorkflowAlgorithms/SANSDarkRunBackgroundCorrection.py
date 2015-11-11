a45807d2fcb37870791811508fb35be24cda0143
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2015-11-11 13:52:11 +0000

    Refs #10193 Add SANSDarkRunBackgroundCorrection with test

---
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SANSDarkRunBackgroundCorrection.py
@@ -0,0 +1,181 @@
+﻿#pylint: disable=no-init,invalid-name,too-many-locals,too-many-branches
+from mantid.simpleapi import *
+from mantid.kernel import *
+from mantid.api import *
+
+class SANSDarkRunBackgroundCorrection(PythonAlgorithm):
+
+    def category(self):
+        return "Workflow\\SANS\\UsesPropertyManager"
+
+    def name(self):
+        return "SANSDarkRunBackgroundCorrection"
+
+    def summary(self):
+        return "Correct SANS data with a dark run measurement."
+
+    def PyInit(self):
+        self.declareProperty(MatrixWorkspaceProperty("InputWorkspace", "",
+                                                     validator=CommonBinsValidator(),
+                                                     direction=Direction.Input))
+        self.declareProperty(MatrixWorkspaceProperty("DarkRun", "",
+                                                     validator=CommonBinsValidator(),
+                                                     direction=Direction.Input))
+        self.declareProperty(MatrixWorkspaceProperty("OutputWorkspace", "",
+                                                     direction = Direction.Output),
+                                                     "The corrected SANS workspace.")
+        self.declareProperty("NormalizationRatio", 1.0, "Number to scale the dark run in order"
+                                                       "to make it comparable to the SANS run")
+        self.declareProperty("Mean", False, "If True then a mean value of all spectra is used to" 
+                                             "calculate the value to subtract")
+        self.declareProperty("Uniform", True, "If True then we treat the treat the tim ebins a")
+
+    def PyExec(self):
+        # Get the workspaces
+        workspace = self.getProperty("InputWorkspace").value
+        dark_run = self.getProperty("DarkRun").value
+        output_ws_name = self.getPropertyValue("OutputWorkspace")
+
+        # Get other properties
+        do_mean = self.getProperty("Mean").value
+        do_uniform = self.getProperty("Uniform").value
+        normalization_ratio = self.getProperty("NormalizationRatio").value
+
+        dark_run_normalized = None
+        # Apply normalization. Uniform means here that the time over which the data was measured is uniform, there are
+        # no particular spikes to be expected. In the non-uniform case we assume that it matters, when the data was taken
+        if do_uniform:
+            dark_run_normalized = self._prepare_uniform_correction(workspace = workspace,
+                                                                   dark_run = dark_run,
+                                                                   normalization_ratio = normalization_ratio,
+                                                                   do_mean = do_mean)
+        else:
+            dark_run_normalized = self._prepare_non_uniform_correction(workspace = workspace,
+                                                                       dark_run = dark_run,
+                                                                       normalization_ratio = normalization_ratio)
+
+        # Subtract the normalizaed dark run from the SANS workspace
+        output_ws = self._subtract_dark_run_from_sans_data(workspace, dark_run_normalized)
+
+        self.setProperty("OutputWorkspace", output_ws)
+
+    def _subtract_dark_run_from_sans_data(self, workspace, dark_run):
+        # Subtract the dark_run from the workspace
+        subtracted_ws_name = "_dark_run_corrected_ws"
+        alg_minus = AlgorithmManager.create("Minus")
+        alg_minus.initialize()
+        alg_minus.setChild(True)
+        alg_minus.setProperty("LHSWorkspace", workspace)
+        alg_minus.setProperty("RHSWorkspace", dark_run)
+        alg_minus.setProperty("OutputWorkspace", subtracted_ws_name)
+        alg_minus.execute()
+        ws = alg_minus.getProperty("OutputWorkspace").value
+        return alg_minus.getProperty("OutputWorkspace").value
+
+    def _prepare_non_uniform_correction(self, workspace, dark_run, normalization_ratio):
+        # Make sure that the binning is the same for the scattering data and the dark run
+        dark_run_clone = dark_run.clone()
+
+        dark_run_rebin_name = "_dark_run_rebinned"
+        alg_rebin = AlgorithmManager.create("RebinToWorkspace")
+        alg_rebin.initialize()
+        alg_rebin.setChild(True)
+        alg_rebin.setProperty("WorkspaceToRebin", dark_run_clone)
+        alg_rebin.setProperty("WorkspaceToMatch", workspace)
+        alg_rebin.setProperty("OutputWorkspace", dark_run_rebin_name)
+        alg_rebin.execute()
+        dark_run_rebinned = alg_rebin.getProperty("OutputWorkspace").value
+
+        # Scale with the normalization factor
+        return self._scale_dark_run(dark_run_rebinned, normalization_ratio)
+
+    def _prepare_uniform_correction(self, workspace, dark_run, normalization_ratio, do_mean):
+        # First we need to integrate from the dark_run. This happens in each bin
+        dark_run_integrated = self._integarate_dark_run(dark_run)
+
+        # If the mean of all detectors is required then we need to average them as well
+        if do_mean:
+            dark_run_integrated = self._perform_average_over_all_pixels(dark_run_integrated)
+
+        # The workspace needs to be scaled to match the SANS data. This is done by the normalization_factor
+        # In addition we need to spread the integrated signal evenly over all bins of the SANS data set.
+        # Note that we assume here a workspace with common bins.
+        num_bins = len(workspace.dataY(0))
+        scale_factor = normalization_ratio/float(num_bins)
+
+        return self._scale_dark_run(dark_run_integrated, scale_factor)
+
+    def _integarate_dark_run(self, dark_run):
+        '''
+        Sum up all bins for each pixel
+        @param dark_run: a bare dark run
+        @returns an integrated dark run
+        '''
+        dark_run_integrated_name = "_dark_run_integrated"
+        alg_integrate = AlgorithmManager.create("Integration")
+        alg_integrate.initialize()
+        alg_integrate.setChild(True)
+        alg_integrate.setProperty("InputWorkspace", dark_run)
+        alg_integrate.setProperty("OutputWorkspace", dark_run_integrated_name)
+        alg_integrate.execute()
+        return alg_integrate.getProperty("OutputWorkspace").value
+
+    def _scale_dark_run(self, dark_run, scale_factor):
+        '''
+        Scales the dark run.
+        @param dark_run: The dark run to be scaled
+        @param scale_factor: The scaling factor
+        @returns a scaled dark run
+        '''
+        dark_run_scaled_name = "_dark_run_scaled"
+        alg_scale  = AlgorithmManager.create("Scale")
+        alg_scale.initialize()
+        alg_scale.setChild(True)
+        alg_scale.setProperty("InputWorkspace", dark_run)
+        alg_scale.setProperty("OutputWorkspace", dark_run_scaled_name)
+        alg_scale.setProperty("Operation", "Multiply")
+        alg_scale.setProperty("Factor", scale_factor)
+        alg_scale.execute()
+        return alg_scale.getProperty("OutputWorkspace").value
+
+    def _perform_average_over_all_pixels(self, dark_run_integrated):
+        '''
+        At this point we expect a dark run workspace with one entry for each pixel,ie
+        after integration. The average value of all pixels is calculated. This value
+        replaces the current value
+        @param dark_run_integrated: a dark run with integrated pixels
+        @returns an averaged, integrated dark run
+        '''
+        dark_run_summed_name= "_summed_spectra"
+        alg_sum  = AlgorithmManager.create("SumSpectra")
+        alg_sum.initialize()
+        alg_sum.setChild(True)
+        alg_sum.setProperty("InputWorkspace",  dark_run_integrated)
+        alg_sum.setProperty("OutputWorkspace", dark_run_summed_name)
+        alg_sum.execute()
+        dark_run_summed = alg_sum.getProperty("OutputWorkspace").value
+
+        # Get the single value out of the summed workspace and divide it
+        # by the number of pixels
+        summed_value = dark_run_summed.dataY(0)[0]
+        num_pixels = dark_run_integrated.getNumberHistograms()
+        averaged_value = summed_value/float(num_pixels)
+
+        # Apply the averaged value to all pixels. Set values to zero and 
+        # multiply by the averaged value.
+        dark_run_divide_name = "_unity_spectra"
+        alg_divide  = AlgorithmManager.create("Divide")
+        alg_divide.initialize()
+        alg_divide.setChild(True)
+        alg_divide.setProperty("LHSWorkspace",  dark_run_integrated)
+        alg_divide.setProperty("RHSWorkspace",  dark_run_integrated)
+        alg_divide.setProperty("OutputWorkspace", dark_run_divide_name)
+        alg_divide.execute()
+        dark_run_unity= alg_divide.getProperty("OutputWorkspace").value
+
+        # Now that we have a unity workspace multiply with the unit value
+        return self._scale_dark_run(dark_run_unity,averaged_value)
+
+#############################################################################################
+
+AlgorithmFactory.subscribe(SANSDarkRunBackgroundCorrection)
---
+++ Framework/PythonInterface/test/python/plugins/algorithms/SANSDarkRunBackgroundCorrectionTest.py
@@ -0,0 +1,215 @@
+import unittest
+
+from mantid.kernel import *
+from mantid.api import *
+from testhelpers import run_algorithm
+import time
+
+class SANSDarkRunBackgroundCorrectionTest(unittest.TestCase):
+    #-----
+    # Workspace2D tests
+    def test_dark_run_correction_with_uniform_and_not_mean_for_workspace2D_input(self):
+        # Arrange
+        spectra = 4
+
+        bin_boundaries_scatter = 5
+        y_value_scatter_run = 4.
+        e_value_scatter_run = 1.
+        name_scatter = "_scatter_SANS_test"
+        self._provide_workspace2D(bin_boundaries_scatter, y_value_scatter_run,
+                                  e_value_scatter_run,name_scatter, spectra)
+
+        bin_boundaries_dark_run = 20
+        y_value_dark_run = 0.3
+        e_value_dark_run = 0.
+        name_dark_run = "_dark_run_SANS_test"
+        self._provide_workspace2D(bin_boundaries_dark_run, y_value_dark_run,
+                                  e_value_dark_run,name_dark_run, spectra)
+        # Algorithm configuration
+        mean = False
+        uniform = True
+        normalization_ratio = 0.5
+
+        # Act
+        out_ws_name = "out_test"
+        alg = run_algorithm(
+                    'SANSDarkRunBackgroundCorrection',
+                    InputWorkspace= name_scatter,
+                    DarkRun = name_dark_run,
+                    Mean = mean,
+                    Uniform =uniform,
+                    NormalizationRatio=normalization_ratio, 
+                    OutputWorkspace = out_ws_name,
+                    rethrow = True)
+        
+        # Assert
+        # We should sum up all bins in the dark run (all y values, hence bin_boundaries_dark_run - 1).
+        # Then multpliy by the normalization ratio 
+        # Then divide by the bins in the scatterer.
+        expected_integration = y_value_dark_run* float(bin_boundaries_dark_run - 1)
+        expected_correction_value = (normalization_ratio*expected_integration/float(bin_boundaries_scatter - 1))
+
+        self.assertTrue(AnalysisDataService.doesExist(out_ws_name))
+        self._check_output_workspace(mtd[name_scatter],
+                                     mtd[out_ws_name],
+                                     expected_correction_value)
+
+        # Clean up
+        ws_to_clean = [out_ws_name, name_dark_run, name_scatter]
+        self._clean_up(ws_to_clean)
+
+    def test_dark_run_correction_with_uniform_and_mean_for_workspace2D_input(self):
+        # Arrange
+        spectra = 4
+
+        bin_boundaries_scatter = 5
+        y_value_scatter_run = 4.
+        e_value_scatter_run = 1.
+        name_scatter = "_scatter_SANS_test"
+        self._provide_workspace2D(bin_boundaries_scatter, y_value_scatter_run,
+                                    e_value_scatter_run,name_scatter, spectra)
+
+        bin_boundaries_dark_run = 20
+        y_value_spectra_even_dark_run = [0.3 for element in xrange(bin_boundaries_dark_run - 1)]
+        y_value_spectra_odd_dark_run = [0.2 for element in xrange(bin_boundaries_dark_run - 1)]
+        y_value_dark_run = (y_value_spectra_even_dark_run + y_value_spectra_odd_dark_run + 
+                            y_value_spectra_even_dark_run + y_value_spectra_odd_dark_run)
+        e_value_dark_run = 0
+        name_dark_run = "_dark_run_SANS_test"
+        self._provide_workspace2D(bin_boundaries_dark_run, y_value_dark_run,
+                                                e_value_dark_run,name_dark_run, spectra, True)
+        # Algorithm configuration
+        mean = True
+        uniform = True
+        normalization_ratio = 0.5
+
+        # Act
+        out_ws_name = "out_test"
+        alg = run_algorithm(
+                    'SANSDarkRunBackgroundCorrection',
+                    InputWorkspace= name_scatter,
+                    DarkRun = name_dark_run,
+                    Mean = mean,
+                    Uniform =uniform,
+                    NormalizationRatio=normalization_ratio,
+                    OutputWorkspace = out_ws_name,
+                    rethrow = True)
+
+        # Assert
+        # We should sum up all bins in the dark run (all y values, hence bin_boundaries_dark_run - 1).
+        # Then multpliy by the normalization ratio
+        # Then divide by the bins in the scatterer.
+        expected_integration = sum(y_value_dark_run)/float(mtd[name_dark_run].getNumberHistograms())
+        expected_correction_value = (normalization_ratio*expected_integration/float(bin_boundaries_scatter - 1))
+
+        self.assertTrue(AnalysisDataService.doesExist(out_ws_name))
+        self._check_output_workspace(mtd[name_scatter],
+                                     mtd[out_ws_name],
+                                     expected_correction_value)
+
+        # Clean up
+        ws_to_clean = [out_ws_name, name_dark_run, name_scatter]
+        self._clean_up(ws_to_clean)
+
+    def test_dark_run_correction_with_non_uniform_and_not_mean_for_workspace2D_input(self):
+        # Arrange
+        spectra = 4
+
+        bin_boundaries= 5
+        y_value_scatter_run = spectra*[element for element in xrange(bin_boundaries-1)]
+        e_value_scatter_run = 1.
+        name_scatter = "_scatter_SANS_test"
+        self._provide_workspace2D(bin_boundaries, y_value_scatter_run,
+                                    e_value_scatter_run,name_scatter, spectra, True)
+
+        y_value_dark_run = spectra*[element*0.2 for element in xrange(bin_boundaries - 1)]
+        e_value_dark_run = 0
+        name_dark_run = "_dark_run_SANS_test"
+        self._provide_workspace2D(bin_boundaries, y_value_dark_run,
+                                  e_value_dark_run, name_dark_run, spectra, True)
+        # Algorithm configuration
+        mean = False
+        uniform = False
+        normalization_ratio = 0.6
+
+        # Act
+        out_ws_name = "out_test"
+        alg = run_algorithm(
+                    'SANSDarkRunBackgroundCorrection',
+                    InputWorkspace= name_scatter,
+                    DarkRun = name_dark_run,
+                    Mean = mean,
+                    Uniform =uniform,
+                    NormalizationRatio=normalization_ratio, 
+                    OutputWorkspace = out_ws_name,
+                    rethrow = True)
+        
+        # Assert
+        # We should sum up all bins in the dark run (all y values, hence bin_boundaries_dark_run - 1).
+        # Then multpliy by the normalization ratio 
+        # Then divide by the bins in the scatterer.
+        expected_correction_value = normalization_ratio
+        self.assertTrue(AnalysisDataService.doesExist(out_ws_name))
+        self._check_output_workspace_non_uniform(mtd[name_scatter],
+                                                 mtd[out_ws_name],
+                                                 mtd[name_dark_run],
+                                                 expected_correction_value)
+
+        # Clean up
+        ws_to_clean = [out_ws_name, name_dark_run, name_scatter]
+        self._clean_up(ws_to_clean)
+
+    #------
+    # Helper methods
+    def _create_test_workspace(self, name, x, y, error, number_of_spectra):
+        alg = run_algorithm('CreateWorkspace',
+                            DataX = x,
+                            DataY = y,
+                            DataE = error,
+                            NSpec = number_of_spectra,
+                            OutputWorkspace= name)
+        return alg.getPropertyValue("OutputWorkspace")
+
+    def _check_output_workspace(self, original_ws, corrected_ws, expected_correction_value):
+        # Iterate over all spectra
+        num_spectra = original_ws.getNumberHistograms()
+
+        for index in range(0, num_spectra):
+            y_original = original_ws.dataY(index)
+            y_corrected = corrected_ws.dataY(index)
+            for elem in range(0, len(y_original)):
+                expected = y_original[elem] - expected_correction_value
+                self.assertAlmostEqual(expected,
+                                       y_corrected[elem], 4)
+
+    def _check_output_workspace_non_uniform(self, original_ws, corrected_ws,
+                                            dark_ws, expected_correction_value):
+        # Iterate over all spectra
+        num_spectra = original_ws.getNumberHistograms()
+
+        for index in range(0, num_spectra):
+            y_original = original_ws.dataY(index)
+            y_dark = dark_ws.dataY(index)
+            y_corrected = corrected_ws.dataY(index)
+            for elem in range(0, len(y_original)):
+                expected = y_original[elem] - y_dark[elem]*expected_correction_value
+                self.assertAlmostEqual(expected, y_corrected[elem], 4)
+
+    def _provide_workspace2D(self, bin_boundaries, y_value, e_value, name, spectra, use_y_list = False):
+        x = spectra*[element for element in xrange(bin_boundaries)]
+        y = None
+        if use_y_list:
+            y = y_value
+        else:
+            y = spectra*[y_value for element in xrange(bin_boundaries - 1)]
+        e = spectra*[e_value for element in xrange(bin_boundaries - 1)]
+        self._create_test_workspace(name, x, y, e, spectra)
+
+    def _clean_up(self, ws_to_clean):
+        for ws in ws_to_clean:
+            if AnalysisDataService.doesExist(ws):
+                AnalysisDataService.remove(ws)
+            
+
+if __name__ == '__main__':
+    unittest.main()
