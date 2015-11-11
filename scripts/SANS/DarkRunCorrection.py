bf84a498161c94c5fb0368c89591d0cae9fb1fd7
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2015-11-11 16:22:05 +0000

    Refs #10193 Extract normalization ratio

---
+++ scripts/SANS/DarkRunCorrection.py
@@ -0,0 +1,97 @@
+﻿#pylint: disable=invalid-name
+from mantid.simpleapi import *
+from mantid.kernel import time_duration
+
+class DarkRunCorrection(object):
+    '''
+    This class performs the dark run correction for ISIS SANS instruments
+    '''
+    def __init__(self):
+        super(DarkRunCorrection, self).__init__()
+        self._normalization_extractor = DarkRunNormalizationExtractor()
+
+        # Should we treat the dark run signal as uniform,
+        #ie constant (stat. fluctuations) over time
+        self._use_uniform = True
+
+        # Should we look at a mean value of the dark count over all pixels.
+        # Only applicable if the data is uniform
+        self._use_mean = False
+
+        # Should we use time logs or uamph logs to calculat the normalization ratio.
+        self._use_time = True
+        self._normalization_ratio = 1.
+
+    def set_use_mean(self, use_mean):
+        self._use_mean = use_mean
+
+    def set_use_uniform(self, use_uniform):
+        self._use_uniform = use_uniform
+
+    def set_use_time(self, use_time):
+        self._use_time
+
+    def execute(self, scatter_workspace, dark_run):
+        '''
+        Perform the dark run correction.
+        '''
+        # Get the normalization ratio from the workspaces
+        self._normalization_ratio = self._normalization_extractor.extract_normalization(scatter_workspace,
+                                                                                        dark_run)
+        # Run the correction algorithm with the user settings
+        corrected_ws_name = scatter_workspace.name() + "_dark_workspace_corrected"
+        alg_dark = AlgorithmManager.create("Minus")
+        alg_dark.initialize()
+        alg_dark.setChild(True)
+        alg_dark.setProperty("LHSWorkspace", workspace)
+        alg_dark.setProperty("RHSWorkspace", dark_run)
+        alg_dark.setProperty("OutputWorkspace", subtracted_ws_name)
+        alg_dark.execute()
+
+        return alg_dark.getProperty("OutputWorkspace").value
+
+class DarkRunNormalizationExtractor(object):
+    '''
+    Extrats the normalization ratio from the scatter workspace
+    and the dark run workspace depending. The normalization ratio
+    can be either calculated as a ratio of good proton charges or
+    a ratio of measurement times
+    '''
+    def __init__(self):
+        super(DarkRunNormalizationExtractor, self).__init__()
+
+    def extract_normalization(self, scatter_workspace, dark_run, use_time = True):
+        '''
+        Extract the normalization by either looking at the time duration of the measurement (good_frames)
+        or by looking at the time of the good charge (good_uah_log)
+        '''
+        log_entry = None
+        if use_time:
+            log_entry = "good_frames"
+        else:
+            log_entry = "good_uah_log"
+        return self._extract_normalization(scatter_workspace, dark_run, log_entry)
+
+    def _extract_normalization(self, scatter_workspace, dark_run, log_entry):
+        '''
+        Create a normalization ratio based on the duration.
+        '''
+        scatter_duration = self._get_time_duration_from_logs(scatter_workspace, log_entry)
+        dark_run_duration = self._get_time_duration_from_logs(dark_run, log_entry)
+        return scatter_duration/dark_run_duration
+
+    def _get_time_duration_from_logs(self, workspace, log_entry):
+        '''
+        Extract the time duration from the logs.
+        '''
+        run = workspace.getRun()
+        if not run.hasProperty(log_entry):
+            raise RuntimeError("DarkRunCorrection: The workspace does not have a" + log_entry +
+                               "log entry. This is required for calculating the noramlization"
+                               "of the dark run.")
+
+        entry = run.getProperty(log_entry)
+        first_time = entry.firstTime()
+        last_time = entry.lastTime()
+        return time_duration.total_nanoseconds(last_time- first_time)/1e9
+
---
+++ scripts/test/SANSDarkRunCorrectionTest.py
@@ -0,0 +1,79 @@
+﻿import unittest
+import mantid
+from mantid.simpleapi import *
+
+import DarkRunCorrection as dc
+
+def create_sample_workspace_with_log(x_start, x_end, bin_width,
+                                     log_name, start_time, number_of_times):
+    ws = CreateSampleWorkspace(NumBanks=1,
+                               BankPixelWidth=1,
+                               XMin=x_start,
+                               XMax = x_end,
+                               BinWidth = bin_width)
+    for i in range(0, number_of_times):
+        val = random.randrange(0, 10, 1)
+        date = DateAndTime(start_time)
+        date +=  int(i*1e9)
+        AddTimeSeriesLog(ws, Name=log_name, Time=date.__str__().strip(), Value=val)
+    return ws
+
+
+class SANSDarkRunCorrectionTest(unittest.TestCase):
+    def test_standard_dark_run_correction(self):
+        self.assertTrue(False)
+
+class DarkRunNormalizationExtractorTest(unittest.TestCase):
+    def test_good_frames_logs_can_be_extracted(self):
+        # Arrange
+        x_start = 0
+        bin_width = 1
+        log_name = "good_frame"
+
+        start_time_scatter = 3
+        number_of_times_scatter = 20
+
+        start_time_dark_run = 7
+        number_of_times_dark_run = 45
+
+        ws_scatter = create_sample_workspace_with_log(x_start, x_end, bin_width,
+                                            log_name, start_time_scatter, number_of_times_scatter)
+        ws_dark = create_sample_workspace_with_log(x_start, x_end, bin_width,
+                                            log_name, start_time_dark_run, number_of_times_dark_run)
+
+        extractor = dc.DarkRunNormalizationExtractor()
+        use_time = True
+
+        # Act
+        ratio = extractor.extract_normalization(ws_scatter, dark_run, use_time)
+        print "###########"
+        print ratio
+        # Assert
+        expected_ratio = number_of_times_scatter/number_of_times_dark_run -1
+        self.assertEqual(ratio, expected_ratio, "Should have the same ratio")
+
+    def test_that_throws_runtime_exception_for_missing_log(self):
+         # Arrange
+        x_start = 0
+        bin_width = 1
+        log_name = "alsdkfjslkdf"
+
+        start_time_scatter = 3
+        number_of_times_scatter = 20
+
+        start_time_dark_run = 7
+        number_of_times_dark_run = 45
+
+        ws_scatter = create_sample_workspace_with_log(x_start, x_end, bin_width,
+                                            log_name, start_time_scatter, number_of_times_scatter)
+        ws_dark = create_sample_workspace_with_log(x_start, x_end, bin_width,
+                                            log_name, start_time_dark_run, number_of_times_dark_run)
+
+        extractor = dc.DarkRunNormalizationExtractor()
+        use_time = True
+
+        # Act + Assert
+        self.assertRaises(RuntimeError, extractor.extract_normalization, [ws_scatter, dark_run, use_time])
+
+
+
