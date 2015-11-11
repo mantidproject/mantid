cd8cc59843ac9404bebc0800659bc36609ae49ef
Tom Perkins <thomas.perkins@stfc.ac.uk>
2015-11-11 13:36:27 +0000

    Re #12247 Make IMDEventWorkspace have consistent behaviour
    All other classes with this behaviour work in the same way - a sptr
    property can be retrieved as const_sptr or sptr. IMDEventWorkspace was
    different, so has been changed to work the same way as the others.

--- Framework/API/src/IMDEventWorkspace.cpp
+++ Framework/API/src/IMDEventWorkspace.cpp
@@ -105,10 +105,20 @@ IPropertyManager::getValue<Mantid::API::IMDEventWorkspace_const_sptr>(
   if (prop) {
     return prop->operator()();
   } else {
-    std::string message =
-        "Attempt to assign property " + name +
-        " to incorrect type. Expected const shared_ptr<IMDEventWorkspace>.";
-    throw std::runtime_error(message);
+    // Every other class with this behaviour allows you to get a shared_ptr<T>
+    // property as a shared_ptr<const T>. This class should be consistent, so
+    // try that:
+    PropertyWithValue<Mantid::API::IMDEventWorkspace_sptr> *nonConstProp =
+        dynamic_cast<PropertyWithValue<Mantid::API::IMDEventWorkspace_sptr> *>(
+            getPointerToProperty(name));
+    if (nonConstProp) {
+      return nonConstProp->operator()();
+    } else {
+      std::string message =
+          "Attempt to assign property " + name +
+          " to incorrect type. Expected const shared_ptr<IMDEventWorkspace>.";
+      throw std::runtime_error(message);
+    }
   }
 }
 
