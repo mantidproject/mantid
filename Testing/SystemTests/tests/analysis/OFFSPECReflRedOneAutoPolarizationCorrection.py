6ad2efac876f0136ddd987abb95e2674f7e3d2f3
Matt King <matthew.king@stfc.ac.uk>
2015-11-23 15:17:41 +0000

    Fix pylint warnings for system test
    Refs
    Refs #14488;

--- Testing/SystemTests/tests/analysis/OFFSPECReflRedOneAutoPolarizationCorrection.py
+++ Testing/SystemTests/tests/analysis/OFFSPECReflRedOneAutoPolarizationCorrection.py
@@ -1,4 +1,4 @@
-#pylint: disable=no-init,invalid-name
+#pylint: disable=no-init,invalid-name,line-too-long
 
 """
 This system test verifies that OFFSPEC data is processed correctly
@@ -14,21 +14,29 @@ class OFFSPECReflRedOneAutoPolarizationCorrection(stresstesting.MantidStressTest
         inputWorkspace = Load("OFFSPEC00033767.nxs")
         transmissionGroup = Load("OFFSPEC00033772.nxs")
         #set up our transmission workspace
-        transWorkspace = CreateTransmissionWorkspaceAuto(transmissionGroup, AnalysisMode="MultiDetectorAnalysis", ProcessingInstructions="110-120",WavelengthMin=2.0,WavelengthMax=12.0)
+        transWorkspace = CreateTransmissionWorkspaceAuto(transmissionGroup,
+                                                         AnalysisMode="MultiDetectorAnalysis",
+                                                         ProcessingInstructions="110-120",
+                                                         WavelengthMin=2.0,
+                                                         WavelengthMax=12.0)
         # set up our efficiency constants
         CRho=[1]
         CAlpha=[1]
         CAp=[1]
         CPp=[1]
         #run reflectometryReductionOneAuto
-        __, IvsLam_polCorr,__ = ReflectometryReductionOneAuto(inputWorkspace,AnalysisMode="MultiDetectorAnalysis",ProcessingInstructions="110-120", 
-                                                              FirstTransmissionRun=transWorkspace, ThetaIn="1.2",WavelengthMin=2.0,
-                                                              WavelengthMax=12.0,CorrectionAlgorithm='None', PolarizationAnalysis='PA',
+        __, _IvsLam_polCorr,__ = ReflectometryReductionOneAuto(inputWorkspace, AnalysisMode="MultiDetectorAnalysis",
+                                                              ProcessingInstructions="110-120",
+                                                              FirstTransmissionRun=transWorkspace,
+                                                              ThetaIn="1.2",WavelengthMin=2.0,
+                                                              WavelengthMax=12.0,CorrectionAlgorithm='None',
+                                                              PolarizationAnalysis='PA',
                                                               CPp=CPp,CAp=CAp,CRho=CRho,CAlpha=CAlpha)
         return True
-        
+
     def validate(self):
-        return ("IvsLam_polCorr", "OFFSPECReflRedOneAutoPolarizationCorrection_good.nxs")
+        self.tolerance = 1e-5
+        return ("_IvsLam_polCorr", "OFFSPECReflRedOneAutoPolarizationCorrection_good.nxs")
     def requiredFiles(self):
         return ["OFFSPEC00033767.nxs",
                 "OFFSPEC00033772.nxs",
