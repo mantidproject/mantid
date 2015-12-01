f205ee5619dedc52276cfc6ff630bb8aec35929f
Tom Perkins <thomas.perkins@stfc.ac.uk>
2015-12-01 14:45:23 +0000

    Re #10057 Fix further gcc warnings

--- MantidQt/MantidWidgets/src/MWRunFiles.cpp
+++ MantidQt/MantidWidgets/src/MWRunFiles.cpp
@@ -191,8 +191,8 @@ MWRunFiles::MWRunFiles(QWidget *parent)
       m_allowMultipleFiles(false), m_isOptional(false), m_multiEntry(false),
       m_buttonOpt(Text), m_fileProblem(""), m_entryNumProblem(""),
       m_algorithmProperty(""), m_fileExtensions(), m_extsAsSingleOption(true),
-      m_liveButtonState(Hide), m_foundFiles(), m_lastFoundFiles(), m_lastDir(),
-      m_showValidator(true), m_fileFilter() {
+      m_liveButtonState(Hide), m_showValidator(true), m_foundFiles(),
+      m_lastFoundFiles(), m_lastDir(), m_fileFilter() {
   m_thread = new FindFilesThread(this);
 
   m_uiForm.setupUi(this);
