45abf9cffc65a377d11bd212b4964afcd3e6fbfe
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-12-02 11:27:04 +0000

    Add SwapWidths algorithm
    Refs #14669

---
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SwapWidths.py
@@ -0,0 +1,103 @@
+#pylint: disable=no-init
+from mantid.kernel import *
+from mantid.api import (WorkspaceProperty, FileProperty, FileAction, TextAxis,
+                        DataProcessorAlgorithm, AlgorithmFactory, mtd)
+from mantid.simpleapi import *
+import numpy as np
+
+class SwapWidths(DataProcessorAlgorithm):
+
+    _input_ws = None
+    _output_ws = None
+    _swap_point = None
+
+
+    def category(self):
+        return 'Workflow\\MIDAS;PythonAlgorithms;Inelastic'
+
+
+    def summary(self):
+        return 'Creates a calibration workspace in energy trnasfer for IN16B.'
+
+
+    def PyInit(self):
+        self.declareProperty(WorkspaceProperty('InputWorkspace', '',
+                                               direction=Direction.Input),
+                             doc='Input workspace')
+
+
+        self.declareProperty(name='SwapPoint', defaultValue=1,
+                             doc='Swap point')
+
+        self.declareProperty(WorkspaceProperty('OutputWorkspace', '',
+                                               direction=Direction.Output),
+                             doc='Output workspace')
+
+
+    def PyExec(self):
+        self._setup()
+
+        label_1 = "f2.f1.FWHM"
+        index_1 = self._indexOf(mtd[self._input_ws].getAxis(1), label_1)
+        width1_x = mtd[self._input_ws].readX(index_1)
+        width1_y = mtd[self._input_ws].readY(index_1)
+        width1_e = mtd[self._input_ws].readE(index_1)
+        label_2 = "f2.f2.FWHM"
+        index_2 = self._indexOf(mtd[self._input_ws].getAxis(1), label_2)
+        width2_x = mtd[self._input_ws].readX(index_2)
+        width2_y = mtd[self._input_ws].readY(index_2)
+        width2_e = mtd[self._input_ws].readE(index_2)
+        number_points = len(width1_y)
+        logger.information('Number of points is %i' % number_points)
+        logger.information('Swap point is %i' % self._swap_point)
+
+        x1 = width1_x
+        y1 = width1_y[:self._swap_point]
+        y1 = np.append(y1,width2_y[self._swap_point:])
+        e1 = width1_e[:self._swap_point]
+        e1 = np.append(e1,width2_e[self._swap_point:])
+        x2 = width2_x
+        y2 = width2_y[:self._swap_point]
+        y2 = np.append(y2,width1_y[self._swap_point:])
+        e2 = width2_e[:self._swap_point]
+        e2 = np.append(e2,width1_e[self._swap_point:])
+		
+        dataX = x1                              #create data for WS
+        dataY = y1
+        dataE = e1
+        dataX = np.append(dataX,x2)
+        dataY = np.append(dataY,y2)
+        dataE = np.append(dataE,e2)
+        names = label_1 + ', ' + label_2           #names for WS
+        CreateWorkspace(OutputWorkspace=self._output_ws,
+                        DataX=dataX,
+                        DataY=dataY,
+                        DataE=dataE,
+                        Nspec=2,
+                        UnitX='MomentumTransfer',
+                        VerticalAxisUnit='Text',
+                        VerticalAxisValues=names)
+
+        self.setProperty('OutputWorkspace', self._output_ws)
+
+
+    def _setup(self):
+        """
+        Gets properties.
+        """
+
+        self._input_ws = self.getPropertyValue('InputWorkspace')
+        self._output_ws = self.getPropertyValue('OutputWorkspace')
+        self._swap_point = self.getProperty('SwapPoint').value
+        self._swap_point = int(self._swap_point) + 1
+
+    def _indexOf(self, text_axis, label):
+        nvals = text_axis.length()
+        for idx in range(nvals):
+            if label == text_axis.label(idx):
+                return idx
+    # If we reach here we didn't find it
+        raise LookupError("Label '%s' not found on text axis" % label)
+
+# Register algorithm with Mantid
+AlgorithmFactory.subscribe(SwapWidths)
