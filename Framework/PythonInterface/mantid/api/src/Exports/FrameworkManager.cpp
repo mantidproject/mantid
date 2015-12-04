6d6dcb8eccba5b88acc858adbb33186cfda4c5fc
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-12-04 15:22:18 +0000

    Remove hand-written exit clean up code in Python
    Use the standard atexit module instead.
    Refs #14657

--- Framework/PythonInterface/mantid/api/src/Exports/FrameworkManager.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/FrameworkManager.cpp
@@ -1,5 +1,4 @@
 #include "MantidAPI/FrameworkManager.h"
-#include <MantidPythonInterface/kernel/TrackingInstanceMethod.h>
 
 #include <boost/python/class.hpp>
 #include <boost/python/return_value_policy.hpp>
@@ -10,43 +9,46 @@ using Mantid::API::FrameworkManager;
 using namespace boost::python;
 
 void export_FrameworkManager() {
-  typedef class_<FrameworkManagerImpl, boost::noncopyable>
-      FrameworkManagerPythonType;
-  auto pythonClass =
-      FrameworkManagerPythonType("FrameworkManagerImpl", no_init)
-          .def("setNumOMPThreadsToConfigValue",
-               &FrameworkManagerImpl::setNumOMPThreadsToConfigValue,
-               arg("self"), "Sets the number of OpenMP threads to the value "
-                            "specified in the "
-                            "config file")
+  class_<FrameworkManagerImpl, boost::noncopyable>("FrameworkManagerImpl",
+                                                   no_init)
+      .def("setNumOMPThreadsToConfigValue",
+           &FrameworkManagerImpl::setNumOMPThreadsToConfigValue, arg("self"),
+           "Sets the number of OpenMP threads to the value "
+           "specified in the "
+           "config file")
 
-          .def("setNumOMPThreads", &FrameworkManagerImpl::setNumOMPThreads,
-               (arg("self"), arg("nthread")),
-               "Set the number of OpenMP threads to the given value")
+      .def("setNumOMPThreads", &FrameworkManagerImpl::setNumOMPThreads,
+           (arg("self"), arg("nthread")),
+           "Set the number of OpenMP threads to the given value")
 
-          .def("getNumOMPThreads", &FrameworkManagerImpl::getNumOMPThreads,
-               arg("self"),
-               "Returns the number of OpenMP threads that will be used.")
+      .def("getNumOMPThreads", &FrameworkManagerImpl::getNumOMPThreads,
+           arg("self"),
+           "Returns the number of OpenMP threads that will be used.")
 
-          .def("clear", &FrameworkManagerImpl::clear, arg("self"),
-               "Clear all memory held by Mantid")
+      .def("clear", &FrameworkManagerImpl::clear, arg("self"),
+           "Clear all memory held by Mantid")
 
-          .def("clearAlgorithms", &FrameworkManagerImpl::clearAlgorithms,
-               arg("self"),
-               "Clear memory held by algorithms (does not include workspaces)")
+      .def("clearAlgorithms", &FrameworkManagerImpl::clearAlgorithms,
+           arg("self"),
+           "Clear memory held by algorithms (does not include workspaces)")
 
-          .def("clearData", &FrameworkManagerImpl::clearData, arg("self"),
-               "Clear memory held by the data service (essentially all "
-               "workspaces, "
-               "including hidden)")
+      .def("clearData", &FrameworkManagerImpl::clearData, arg("self"),
+           "Clear memory held by the data service (essentially all "
+           "workspaces, "
+           "including hidden)")
 
-          .def("clearInstruments", &FrameworkManagerImpl::clearInstruments,
-               arg("self"), "Clear memory held by the cached instruments")
+      .def("clearInstruments", &FrameworkManagerImpl::clearInstruments,
+           arg("self"), "Clear memory held by the cached instruments")
 
-          .def("clearPropertyManagers",
-               &FrameworkManagerImpl::clearPropertyManagers, arg("self"),
-               "Clear memory held by the PropertyManagerDataService");
-  // Instance method
-  Mantid::PythonInterface::TrackingInstanceMethod<
-      FrameworkManager, FrameworkManagerPythonType>::define(pythonClass);
+      .def("clearPropertyManagers",
+           &FrameworkManagerImpl::clearPropertyManagers, arg("self"),
+           "Clear memory held by the PropertyManagerDataService")
+
+      .def("shutdown", &FrameworkManagerImpl::shutdown, arg("self"),
+           "Effectively shutdown this service")
+
+      .def("Instance", &FrameworkManager::Instance,
+           return_value_policy<reference_existing_object>(),
+           "Return a reference to the singleton instance")
+      .staticmethod("Instance");
 }
