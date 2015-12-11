3a4d910a950f8f2da34a0053e49eabfdcdee190b
Lamar Moore <lamar.moore@stfc.ac.uk>
2015-12-11 14:24:24 +0000

    Re #14725 GroupDetectors2 now using std initializer list

--- Framework/DataHandling/src/GroupDetectors2.cpp
+++ Framework/DataHandling/src/GroupDetectors2.cpp
@@ -46,11 +46,9 @@ void GroupDetectors2::init() {
                                                          Direction::Output),
                   "The name of the output workspace");
 
-  std::vector<std::string> fileExts(2);
-  fileExts[0] = ".map";
-  fileExts[1] = ".xml";
   declareProperty(
-      new FileProperty("MapFile", "", FileProperty::OptionalLoad, fileExts),
+      new FileProperty("MapFile", "", FileProperty::OptionalLoad,
+                       {".map", ".xml"}),
       "A file that consists of lists of spectra numbers to group. See the "
       "help\n"
       "for the file format");
@@ -359,7 +357,8 @@ void GroupDetectors2::getGroups(API::MatrixWorkspace_const_sptr workspace,
       if (*it > maxIn) {
         g_log.error() << "Spectra index " << *it
                       << " doesn't exist in the input workspace, the highest "
-                         "possible index is " << maxIn << std::endl;
+                         "possible index is "
+                      << maxIn << std::endl;
         throw std::out_of_range("One of the spectra requested to group does "
                                 "not exist in the input workspace");
       }
