ee2e221a1f661daa3549f388c906c63ed6da8ec3
Tom Perkins <thomas.perkins@stfc.ac.uk>
2015-12-14 11:09:17 +0000

    Re #14739 Add new dialog for surface plots

---
+++ MantidPlot/src/Mantid/MantidSurfacePlotDialog.cpp
@@ -0,0 +1,1 @@
+#include "MantidSurfacePlotDialog.h"
---
+++ MantidPlot/src/Mantid/MantidSurfacePlotDialog.h
@@ -0,0 +1,7 @@
+#ifndef MANTIDSURFACEPLOTDIALOG_H_
+#define MANTIDSURFACEPLOTDIALOG_H_
+
+#include <QComboBox>
+#include "MantidWSIndexDialog.h"
+
+#endif
