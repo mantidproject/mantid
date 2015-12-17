46dd55c442b655926d8c9685a186373a3b1c897f
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2015-12-17 08:26:31 +0000

    Refs #14803 add tests to file copier

--- Framework/PythonInterface/plugins/algorithms/SANSCopyUserFileDependency.py
+++ Framework/PythonInterface/plugins/algorithms/SANSCopyUserFileDependency.py
@@ -1,11 +1,12 @@
-#pylint: disable=no-init,invalid-name
+﻿#pylint: disable=no-init,invalid-name
 from mantid.api import *
 from mantid.kernel import Direction, FloatBoundedValidator
 import mantid.simpleapi
 import os
 import re
+import shutil
 
-class SANSCopyUserIFleDependency(PythonAlgorithm):
+class SANSCopyUserFileDependency(PythonAlgorithm):
 
     def category(self):
         return "SANS"
@@ -40,11 +41,55 @@ class SANSCopyUserIFleDependency(PythonAlgorithm):
             pass
 
         # Read all dependencies
-        dependencies = self._get_all_dependencies(full_user_file_path)
+        dependency_extractor = UserFileDependencyExtractor()
+        dependencies = dependency_extractor.get_all_dependencies(full_user_file_path)
 
         # Find all dependencies
+        dependencies_full = self._find_all_dependencies(dependencies)
+
+        # Get target names
+        targets_full = self._get_targets(dependencies, output_dir)
 
         # Copy each file into the new directory
+        self._copy_all_dependencies_to_new_directory(dependencies_full, targets_full)
+
+    def _get_targets(self, dependencies, output_dir):
+        targets = []
+        for dependency in dependencies:
+            targets.append(os.path.join(output_dir, dependency))
+        return targets
+
+    def _copy_all_dependencies_to_new_directory(self, dependencies_full, targets):
+        '''
+        @param dependencies_full: a list of full file paths to the dependencies
+        @param depdendencies: a list of dependency names
+        @param targets: a list of targets
+        '''
+        if len(dependencies_full) != len(targets):
+            pass # TODO handle correctly
+
+        soure_target = zip(dependencies_full, targets)
+
+        for src, trg in soure_target:
+            try:
+                shutil.copyfile(src, trg)
+            except IOError:
+                pass # TODO handle correctlry here
+
+    def _find_all_dependencies(self, dependencies):
+        '''
+        @param dependencies: a list of bare dependency names
+        @return full paths to dependencies
+        @throws RuntimeError: if a file cannot be found
+        '''
+        full_dependencies = []
+        for dependency in dependencies:
+            try:
+                full_dependencies.append(self._get_file_path(dependency))
+            except:
+                #TODO Track error
+                pass
+        return full_dependencies
 
     def _get_full_user_file_path(self, user_file):
         user_file_path = None
@@ -65,10 +110,20 @@ class SANSCopyUserIFleDependency(PythonAlgorithm):
             # if list, get first value
             if hasattr(this_path, '__iter__'):
                 this_path = this_path[0]
-
         return this_path
 
-    def _get_all_dependencies(self, file_path):
+
+class UserFileDependencyExtractor(object):
+    '''
+    Extracts all depenency file names from a user file
+    '''
+    def __init__(self):
+        super(UserFileDependencyExtractor, self).__init__()
+        file_types = [".nxs", ".xml", ".txt"]
+        regexes_front = ["=[\s]*[\w]+", ",[\s]*[\w]+"]
+        self.regex_collection = [front + back for front in regexes_front for back in file_types]
+
+    def get_all_dependencies(self, file_path):
         # The dependencies can have the following forms
         # 1. xxxx=Dependency.fileType as a direct assignment
         # 3. xxxx,Dependency.fileType as part of a list
@@ -76,12 +131,16 @@ class SANSCopyUserIFleDependency(PythonAlgorithm):
         with open(file_path) as f:
             for line in f:
                 dependencies_from_line = self._get_dependencies_from_line(line)
-                dependencies.expand(dependencies_from_line)
+                dependencies.extend(dependencies_from_line)
+        # Remove all commas, equal signs and whitespaces
+        dependencies = [el.replace("=", "").replace(",","").replace(" ","") for el in dependencies]
+        return set(dependencies)
 
     def _get_dependencies_from_line(self, line):
-        file_types = [".nxs", ".xml", ".txt"]
-        regexes_front = ["=[\s]*[\w]+", ",[\s]*[\w]+"]
-        regex_collection = [front + back for front in regexes_front for back in file_types]
+        found_items = []
+        for element in self.regex_collection:
+            found_items.extend(re.findall(element, line))
+        return found_items
 
-
-
+# Register algorithm with Mantid.
+AlgorithmFactory.subscribe(SANSCopyUserFileDependency)
---
+++ Framework/PythonInterface/test/python/plugins/algorithms/SANSCopyUserFileDependencyTest.py
@@ -0,0 +1,137 @@
+﻿import unittest
+from mantid.simpleapi import *
+from mantid.api import AlgorithmManager, MatrixWorkspace
+from SANSCopyUserFileDependency import UserFileDependencyExtractor
+import shutil
+
+class SANSCopyUserFileDependency(unittest.TestCase):
+    def _save_set_of_files(self, name):
+        text_in_file = ("TUBECALIBFILE=A.nxs\n"
+                        "MON/DIRECT=B.txt\n"
+                        "! use for front, this command has to come second!\n"
+                        "MON/TRANS/SPECTRUM=2\n"
+                        "TRANS/TRANSPEC=4/SHIFT=-55\n"
+                        "MASKFILE=C.xml,D.xml\n"
+                        "!TRANS/TRANSPEC=3\n"
+                        "MON/SPECTRUM=2\n")
+
+        expected_hits = ["A.nxs",
+                         "B.txt",
+                         "C.xml",
+                         "D.xml"]
+        user_file = self._create_file_path(name)
+
+        with open(user_file, 'w') as f:
+            f.write(text_in_file)
+
+        dependencies = []
+        # Also save the dependencies
+        for expected_hit in expected_hits:
+            dependencies.append(self._create_file_path(expected_hit))
+
+        for dependency in dependencies:
+            with open(dependency, 'w') as f:
+                f.write("test")
+
+        to_remove = dependencies
+        to_remove.append(user_file)
+        return set(expected_hits), to_remove
+
+    def _create_file_path(self, name):
+        temp_save_dir = config['defaultsave.directory']
+
+        if (temp_save_dir == ''):
+            temp_save_dir = os.getcwd()
+        full_path = os.path.join(temp_save_dir, name)
+        return full_path
+
+    def _create_target_directory(self, name):
+        temp_save_dir = config['defaultsave.directory']
+        if (temp_save_dir == ''):
+            temp_save_dir = os.getcwd()
+        directory = os.path.join(temp_save_dir, name)
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        return directory
+
+    def _remove_files(self, file_paths):
+        for file_path in file_paths:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+    def _remove_target_directory(self, name):
+        if os.path.isdir(name):
+            shutil.rmtree(name)
+
+    def test_that_files_are_copied(self):
+        # Arrange
+        user_file_name = "user_file_test_for_copy.txt"
+        expected, to_remove = self._save_set_of_files(user_file_name)
+        target_directory = self._create_target_directory("copy_user_file_sans_unit_test")
+
+        # Act
+        copier = AlgorithmManager.createUnmanaged("SANSCopyUserFileDependency")
+        copier.initialize()
+        copier.setChild(True)
+        copier.setProperty("Filename", user_file_name)
+        copier.setProperty("OutputDirectory", target_directory)
+        copier.execute()
+
+        # Assert
+        expected_files = [os.path.join(target_directory, file_name) for file_name in expected]
+        for expected_file in expected_files:
+            self.assertTrue(os.path.exists(expected_file), "New file in new folder should exist")
+
+        # Clean up
+        self._remove_files(to_remove)
+        self._remove_target_directory(target_directory)
+
+class UserFileDependencyExtractorTest(unittest.TestCase):
+    def _save_user_file(self, file_path):
+        text_in_file = ("TUBECALIBFILE=TUBE_SANS2D_BOTH_31681_25Sept15.nxs\n"
+                        "MON/DIRECT=DIRECT_SANS2D_REAR_33117_4m_14mm_15Nov15.txt\n"
+                        "! use for front, this command has to come second!\n"
+                        "MON/DIRECT/FRONT=DIRECT_SANS2D_REAR_33117_4m_14mm_15Nov15.txt\n"
+                        "MON/TRANS/SPECTRUM=2\n"
+                        "TRANS/TRANSPEC=4/SHIFT=-55\n"
+                        "MASKFILE=MASK_SANS2D_REAR_Edges_16Mar2015.xml,MASK_SANS2D_FRONT_Edges_16Mar2015.xml\n"
+                        "!TRANS/TRANSPEC=3\n"
+                        "MON/SPECTRUM=2\n"
+                        "FIT/TRANS/LOG 1.75 16.5\n"
+                        "!PRINT FIT/TRANS/OFF\n"
+                        "!FIT/TRANS/OFF\n")
+        expected_hits = ["TUBE_SANS2D_BOTH_31681_25Sept15.nxs",
+                         "DIRECT_SANS2D_REAR_33117_4m_14mm_15Nov15.txt",
+                         "MASK_SANS2D_REAR_Edges_16Mar2015.xml",
+                         "MASK_SANS2D_FRONT_Edges_16Mar2015.xml"]
+
+        with open(file_path, 'w') as f:
+            f.write(text_in_file)
+
+        return set(expected_hits)
+
+    def test_that_find_all_files_in_user_file(self):
+        # Arrange
+        temp_save_dir = config['defaultsave.directory']
+        if (temp_save_dir == ''):
+            temp_save_dir = os.getcwd()
+        full_path = os.path.join(temp_save_dir, "dummy_user_file.txt")
+        expected = self._save_user_file(full_path)
+
+        # Act
+        extractor = UserFileDependencyExtractor()
+        dependencies = extractor.get_all_dependencies(full_path)
+
+        # Assert
+        # Make sure that all dependencies are found
+        for dependency in dependencies:
+            self.assertTrue(dependency in expected)
+
+        # Make sure that no duplicates are found
+        self.assertTrue(len(set(dependencies)) == len(expected))
+
+        if os.path.exists(full_path):
+            os.remove(full_path)
+
+if __name__ == '__main__':
+    unittest.main()
