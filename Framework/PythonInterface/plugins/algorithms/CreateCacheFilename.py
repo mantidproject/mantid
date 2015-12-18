33e41e5eb7f6a5d60fef9e3a4629b60cb9a36506
Jiao Lin <linjiao@ornl.gov>
2015-12-18 08:35:14 -0500

    Refs #14716. added docs. computed def cache. added empty _calculate

--- Framework/PythonInterface/plugins/algorithms/CreateCacheFilename.py
+++ Framework/PythonInterface/plugins/algorithms/CreateCacheFilename.py
@@ -22,7 +22,51 @@ class CreateCacheFilename(PythonAlgorithm):
     def summary(self):
         """ Return summary
         """
-        return "Create cache filename"
+        return """Create cache filename
+
+The purpose of this algorithm is to create a unique
+filename for a cache so that a workflow can reuse
+results from previous computations.
+
+The algorithm will accept a prefix, PropertyManager, list of properties 
+to use from the property manager (empty is use all), and 
+a string array (or List) of other properties to use, and 
+a directory for cache files to exist in (default described below).
+
+The list of property names will be used to select which of the properties 
+in the PropertyManager will be used to calculate the hash 
+and will be interpreted as globbing.
+
+The string array of other_properties will be key/value pairs of properties 
+that should be considered, but are not in the provided PropertyManager.
+
+If a directory is not specified, cache files will go into a cache 
+subdirectory of ConfigService::getUserPropertiesDir().
+On unix this will be ~/.mantid/cache.
+
+The algorithm will convert all properties to strings as 
+"%s=%s" % (property.name, property.valueAsStr), sort the list, 
+then convert it to a sha1.
+
+A filename with the form <location>/<prefix>_<sha1>.nxs 
+will be returned as the output property.
+If no prefix is specified then file result will be <location>/<sha1>.nxs.
+
+property_manager: an instance of PropertyManager from which property values
+        can be retrieved. None means we don't care about property manager 
+        -- all properties will come from other_properties
+properties: a list of strings. each string is a property managed by the 
+        given property_manager, or it can be glob pattern to match prop
+        names too. but empty list means taking all properties 
+        from the property_manager
+other_properties: a list of strings. each string is in the form of
+        "key=value" for one property not managed by the property_manager.
+        no globbing here.
+prefix: prefix to the output hash name. when it is empty, just the hash.
+        when it is not empty, it will be <prefix>_<sha1>
+cache_dir: the directory in which the cach file will be created. 
+        empty string means default as described above
+"""
 
     def require(self):
         return
@@ -62,20 +106,25 @@ class CreateCacheFilename(PythonAlgorithm):
         if not prop_manager and not other_props:
             raise ValueError("Either PropertyManager or OtherProperties should be supplied")
         prop_manager = mantid.PropertyManagerDataService.retrieve(prop_manager)
-
-        props = self.getPropertyValue("Properties")
         # default to all properties in the manager
+        props = self.getPropertyValue("Properties")
         if not props:
             props = prop_manager.keys()
-
+        # output settings
         prefix = self.getPropertyValue("Prefix")
         cache_dir = self.getPropertyValue("CacheDir")
         if not cache_dir:
-            cache_dir = 
-        
-        # Generate Json file
-        self._save(inputws, outfilename, plotname)
+            cache_dir = os.path.join(
+                ConfigService.getUserPropertiesDir(),
+                "cache"
+                )
+        # calculate
+        self._calculate(prop_manager, props, other_props, prefix, cache_dir)
         return
+
+    def _calculate(self, prop_manager, props, other_props, prefix, cache_dir):
+        return
+
 
 # Register algorithm with Mantid
 AlgorithmFactory.subscribe(CreateCacheFilename)
