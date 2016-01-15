b173e5cc49a54c7cb3a6d8f06f2246568e85fc23
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-01-15 16:03:18 +0000

    Re #14977 Clang-format

--- Framework/Algorithms/test/RemoveExpDecayTest.h
+++ Framework/Algorithms/test/RemoveExpDecayTest.h
@@ -146,8 +146,8 @@ public:
   MatrixWorkspace_sptr createWorkspace(size_t nspec, size_t maxt) {
 
     // Create a fake muon dataset
-    double a = 0.1;   // Amplitude of the oscillations
-    double w = 25.;   // Frequency of the oscillations
+    double a = 0.1; // Amplitude of the oscillations
+    double w = 25.; // Frequency of the oscillations
     double tau = Mantid::PhysicalConstants::MuonLifetime *
                  1e6; // Muon life time in microseconds
 
