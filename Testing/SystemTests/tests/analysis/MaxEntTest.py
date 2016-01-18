ce948c8c56d06787ce30e66477d6f7fbc879e45e
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2016-01-18 12:44:53 +0000

    Re #14995 Added a system test

---
+++ Testing/SystemTests/tests/analysis/MaxEntTest.py
@@ -0,0 +1,19 @@
+﻿#pylint: disable=no-init,attribute-defined-outside-init
+import stresstesting
+from mantid.simpleapi import *
+
+class MaxEntTest(stresstesting.MantidStressTest):
+    '''Tests the MaxEnt algorithm on a MUSR workspace'''
+
+    def runTest(self):
+        Load(Filename='MUSR00022725.nxs', OutputWorkspace='MUSR00022725')
+        CropWorkspace(InputWorkspace='MUSR00022725', OutputWorkspace='MUSR00022725', XMin=0.11, XMax=4, EndWorkspaceIndex=0)
+        RemoveExpDecay(InputWorkspace='MUSR00022725', OutputWorkspace='MUSR00022725')
+        Rebin(InputWorkspace='MUSR00022725', OutputWorkspace='MUSR00022725', Params='0.016')
+        evolChi, evolAngle, image, data = MaxEnt(InputWorkspace='MUSR00022725', A=0.005, ChiTarget=200)
+        GroupWorkspaces(InputWorkspaces='evolChi,evolAngle,image,data',
+                        OutputWorkspace='MaxEntResults')
+
+    def validate(self):
+        self.tolerance = 1E-3
+        return ('MaxEntResults','MaxEntMUSR00022725.nxs')
