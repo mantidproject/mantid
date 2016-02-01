7dbadf0e432183bf16aba5aac6f79bdb19765102
Jose Borreguero <borreguero@gmail.com>
2016-02-01 16:51:49 -0500

    Refs #14936 do not build BackgroundRemover

--- MantidQt/CustomInterfaces/src/DynamicPDF/BackgroundRemover.cpp
+++ MantidQt/CustomInterfaces/src/DynamicPDF/BackgroundRemover.cpp
@@ -13,12 +13,14 @@ namespace CustomInterfaces
 namespace DynamicPDF
 {
 
-  BackgroundRemover::BackgroundRemover(SliceSelector *parent){
+  BackgroundRemover::BackgroundRemover(SliceSelector *parent) : QDialog(parent) {
     m_uiForm.setupUi(this);
   }
 
   void BackgroundRemover::refreshSlice(const boost::shared_ptr<WorkspaceRecord> loadedWorkspace,
                               const size_t &workspaceIndex){
+    UNUSED_ARG(loadedWorkspace);
+    UNUSED_ARG(workspaceIndex);
     std::cout << "hello world";
   }
 
