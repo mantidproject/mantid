1b2f101d4d514ddbcfd6bdd4ba29c8db74f04d37
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-02-03 10:49:45 +0000

    Refs #14326 Add more unit tests

--- Vates/VatesAPI/test/SaveMDWorkspaceToVTKImplTest.h
+++ Vates/VatesAPI/test/SaveMDWorkspaceToVTKImplTest.h
@@ -6,6 +6,9 @@
 #include "MantidVatesAPI/Normalization.h"
 #include "MantidVatesAPI/IgnoreZerosThresholdRange.h"
 #include "MantidTestHelpers/MDEventsTestHelper.h"
+#include "MantidKernel/ConfigService.h"
+
+#include <Poco/File.h>
 
 using namespace Mantid::DataObjects;
 
@@ -17,7 +20,7 @@ public:
     Mantid::VATES::SaveMDWorkspaceToVTKImpl saveMDToVTK;
 
     // Act
-    auto normalizations = saveMDToVTK.getAllowedNormalizationsInStringRepresentation();
+    const auto normalizations = saveMDToVTK.getAllowedNormalizationsInStringRepresentation();
 
     // Assert
     TSM_ASSERT_EQUALS("There should be 4 normalization options.", normalizations.size(), 4);
@@ -30,13 +33,13 @@ public:
   void test_string_representation_converts_to_visual_normalization() {
     // Arrange
     Mantid::VATES::SaveMDWorkspaceToVTKImpl saveMDToVTK;
-    auto normalizations = saveMDToVTK.getAllowedNormalizationsInStringRepresentation();
+    const auto normalizations = saveMDToVTK.getAllowedNormalizationsInStringRepresentation();
 
     // Act
-    auto autoSelect = saveMDToVTK.translateStringToVisualNormalization(normalizations[0]);
-    auto noNormalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[1]);
-    auto numEventsNormalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[2]);
-    auto volumeNormalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[3]);
+    const auto autoSelect = saveMDToVTK.translateStringToVisualNormalization(normalizations[0]);
+    const auto noNormalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[1]);
+    const auto numEventsNormalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[2]);
+    const auto volumeNormalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[3]);
 
     // Assert
     TSM_ASSERT_EQUALS("The visual normalization should be AutoSelect.", autoSelect, Mantid::VATES::AutoSelect);
@@ -50,7 +53,7 @@ public:
     Mantid::VATES::SaveMDWorkspaceToVTKImpl saveMDToVTK;
 
     // Act
-    auto thresholds = saveMDToVTK.getAllowedThresholdsInStringRepresentation();
+    const auto thresholds = saveMDToVTK.getAllowedThresholdsInStringRepresentation();
 
     // Assert
     TSM_ASSERT_EQUALS("There should be 2 normalization options", thresholds.size(), 2);
@@ -77,7 +80,7 @@ public:
     auto workspace = MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, numDims);
 
     // Act
-    auto is4D = saveMDToVTK.is4DWorkspace(workspace);
+    const auto is4D = saveMDToVTK.is4DWorkspace(workspace);
 
     // Assert
     TSM_ASSERT("Detects a 4D MD workspace", is4D);
@@ -86,11 +89,11 @@ public:
   void test_detects_when_not_4D_workspace() {
     // Arrange
     Mantid::VATES::SaveMDWorkspaceToVTKImpl saveMDToVTK;
-    size_t numDims = 2;
+    const size_t numDims = 2;
     auto workspace = MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, numDims);
 
     // Act
-    auto is4D = saveMDToVTK.is4DWorkspace(workspace);
+    const auto is4D = saveMDToVTK.is4DWorkspace(workspace);
 
     // Assert
     TSM_ASSERT("Detects that not a 4D MD workspace", !is4D);
@@ -98,14 +101,54 @@ public:
 
   void test_that_saves_MD_Histo_workspace_to_vts_file() {
     // Arrange
-    //Mantid::VATES::SaveMDWorkspaceToVTKImpl saveMDToVTK;
+    Mantid::VATES::SaveMDWorkspaceToVTKImpl saveMDToVTK;
+    const size_t numDims = 3;
+    const size_t numBins = 5;
+    auto workspace = MDEventsTestHelper::makeFakeMDHistoWorkspace(1.0, numDims, numBins);
 
-    // TODO: FINISH with fake data here, also write a system test with real data
+    const int recursionDepth = 5; // Note that this does not matter since we are testing MDHisto
+
+    const auto normalizations = saveMDToVTK.getAllowedNormalizationsInStringRepresentation();
+    const auto normalization = saveMDToVTK.translateStringToVisualNormalization(normalizations[0]);
+
+    const auto thresholds = saveMDToVTK.getAllowedThresholdsInStringRepresentation();
+    const auto threshold = saveMDToVTK.translateStringToThresholdRange(thresholds[0]);
+
+    const std::string filenameBare = "SaveMDWorkspaceToVTKImplTestFile";
+    const std::string filenameWithExtension = filenameBare + ".vts";
+
+    auto filenameExpected = getTemporaryFilename(filenameWithExtension);
+    removeTemporaryFile(filenameExpected);
+
+    auto filename = getTemporaryFilename(filenameBare);
 
     // Act
+    saveMDToVTK.saveMDWorkspace(workspace, filename, normalization, threshold, recursionDepth);
 
-    // Assert
+    // Assert -- at this point we can only check that a vtk (xml) structured grid file (vts) has been created
+    auto fileExists = doesFileExist(filenameExpected);
+    TSM_ASSERT("The .vts file should have been saved to the default save directory", fileExists);
 
+    // Cleanup
+    removeTemporaryFile(filenameExpected);
   }
+
+private:
+  std::string getTemporaryFilename(std::string filenameWithoutPath) const {
+    auto default_save_directory = Mantid::Kernel::ConfigService::Instance().getString("defaultsave.directory");
+    std::string filenameWithPath(default_save_directory + filenameWithoutPath);
+    return filenameWithPath;
+  }
+
+  void removeTemporaryFile(std::string fileNameWithPath) const {
+    if (Poco::File(fileNameWithPath).exists()) {
+        Poco::File(fileNameWithPath).remove();
+     }
+  }
+
+  bool doesFileExist(std::string filename) {
+    return Poco::File(filename).exists();
+  }
+
 };
 #endif
