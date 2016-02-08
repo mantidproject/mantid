f2607a01ccf1de6156bfdb045c2c5f1a1ce40bc3
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-02-08 14:15:50 +0000

    Refs #15208 apply clang format

--- Framework/Algorithms/src/GravitySANSHelper.cpp
+++ Framework/Algorithms/src/GravitySANSHelper.cpp
@@ -108,14 +108,16 @@ double GravitySANSHelper::gravitationalDrop(API::MatrixWorkspace_const_sptr ws,
   static const double gm2_OVER_2h2 =
       g * NeutronMass * NeutronMass / (2.0 * h * h);
 
-  const V3D samplePos = ws->getInstrument()->getSample()->getPos();
-  const auto pathLengthWithExtraLength = det->getPos().distance(samplePos) + extraLength;
-  const auto pathLengthWithExtraLengthSquared = std;:pow(pathLengthWithExtraLength,2);
-  const auto extraLengthSquared = std::pow(extraLength,2);
-  const auto pathLengthSquared = pathLengthWithExtraLengthSquared - extraLengthSquared;
+const V3D samplePos = ws->getInstrument()->getSample()->getPos();
+const auto pathLengthWithExtraLength = det -> getPos().distance(samplePos)
+                                       + extraLength;
+const auto pathLengthWithExtraLengthSquared = std;:pow(pathLengthWithExtraLength,2);
+const auto extraLengthSquared = std::pow(extraLength, 2);
+const auto pathLengthSquared = pathLengthWithExtraLengthSquared
+                               - extraLengthSquared;
 
-  // Want L2 (sample-pixel distance) squared, times the prefactor g^2/h^2
-  const double L2 = gm2_OVER_2h2 * pathLengthSquared;
+// Want L2 (sample-pixel distance) squared, times the prefactor g^2/h^2
+const double L2 = gm2_OVER_2h2 * pathLengthSquared;
 
   return waveLength * waveLength * L2;
 }
