61be96862b3139c34fc000a39767f170cc13f477
Steven Hahn <hahnse@ornl.gov>
2016-02-09 18:02:14 -0500

    clang-format.

--- Framework/Algorithms/test/BinaryOperationTest.h
+++ Framework/Algorithms/test/BinaryOperationTest.h
@@ -247,8 +247,8 @@ public:
 
   void test_buildBinaryOperationTable_groupedLHS_by_groupedRHS() {
     // two detectors per pixel in lhs
-    std::vector<std::vector<int>> lhs{{0, 1}, {2, 3},   {4, 5},   {6, 7},
-                                      {8, 9}, {10, 11}, {12, 13}, {14, 15}};
+    std::vector<std::vector<int>> lhs{
+        {0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}, {12, 13}, {14, 15}};
     // 4 detectors in each on the rhs
     std::vector<std::vector<int>> rhs{
         {0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
@@ -261,9 +261,12 @@ public:
   void
   test_buildBinaryOperationTable_groupedLHS_by_groupedRHS_bad_overlap_throws() {
     // 4 detectors per pixel in lhs
-    std::vector<std::vector<int>> lhs{{0, 1, 2, 3},     {4, 5, 6, 7},
-                                      {8, 9, 10, 11},   {12, 13, 14, 15},
-                                      {16, 17, 18, 19}, {20, 21, 22, 23}};
+    std::vector<std::vector<int>> lhs{{0, 1, 2, 3},
+                                      {4, 5, 6, 7},
+                                      {8, 9, 10, 11},
+                                      {12, 13, 14, 15},
+                                      {16, 17, 18, 19},
+                                      {20, 21, 22, 23}};
     // 6 detectors in each on the rhs
     std::vector<std::vector<int>> rhs{{0, 1, 2, 3, 4, 5},
                                       {6, 7, 8, 9, 10, 11},
