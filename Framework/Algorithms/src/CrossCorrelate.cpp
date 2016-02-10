19892e5b9d303a6e0ffc13e84be714701fcc17a4
Steven Hahn <hahnse@ornl.gov>
2016-02-09 22:24:25 -0500

    clang-format

--- Framework/Algorithms/src/CrossCorrelate.cpp
+++ Framework/Algorithms/src/CrossCorrelate.cpp
@@ -99,7 +99,7 @@ void CrossCorrelate::exec() {
   int nspecs = 1 + specmax - specmin;
   // Indexes of all spectra in range
   std::vector<size_t> indexes(boost::make_counting_iterator(specmin),
-                              boost::make_counting_iterator(specmax+1));
+                              boost::make_counting_iterator(specmax + 1));
 
   std::ostringstream mess;
   if (nspecs == 0) // Throw if no spectra in range
