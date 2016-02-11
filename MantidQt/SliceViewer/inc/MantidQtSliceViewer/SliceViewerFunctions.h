eec5db4510156ae8a35a3cefb26a33c19cc21150
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-02-11 11:50:08 +0000

    Refs #15163 reformat SliceViewerFunctions

--- MantidQt/SliceViewer/inc/MantidQtSliceViewer/SliceViewerFunctions.h
+++ MantidQt/SliceViewer/inc/MantidQtSliceViewer/SliceViewerFunctions.h
@@ -2,23 +2,25 @@
 #define SLICEVIEWER_FUNCTIONS_H
 
 #include "DllOption.h"
-#include "MantidKernel/VMD.h"
-#include "MantidGeometry/MDGeometry/MDHistoDimension.h"
 #include "MantidAPI/IMDWorkspace.h"
+#include "MantidGeometry/MDGeometry/MDHistoDimension.h"
+#include "MantidKernel/VMD.h"
 
 namespace MantidQt {
 namespace SliceViewer {
 
 /// Checks if a slice lies within a workspace or not
-bool EXPORT_OPT_MANTIDQT_SLICEVIEWER doesSliceCutThroughWorkspace(const Mantid::Kernel::VMD& min, const  Mantid::Kernel::VMD& max,
-  const std::vector<Mantid::Geometry::MDHistoDimension_sptr>& dimensions);
+bool EXPORT_OPT_MANTIDQT_SLICEVIEWER doesSliceCutThroughWorkspace(
+    const Mantid::Kernel::VMD &min, const Mantid::Kernel::VMD &max,
+    const std::vector<Mantid::Geometry::MDHistoDimension_sptr> &dimensions);
 
 /// Checks if rebin mode is in consistent state
-bool EXPORT_OPT_MANTIDQT_SLICEVIEWER isRebinInConsistentState(Mantid::API::IMDWorkspace* rebinnedWS, bool useRebinMode);
+bool EXPORT_OPT_MANTIDQT_SLICEVIEWER isRebinInConsistentState(
+    Mantid::API::IMDWorkspace *rebinnedWS, bool useRebinMode);
 
 /// Should perform auto color scaling on load
-bool EXPORT_OPT_MANTIDQT_SLICEVIEWER shouldAutoScaleForNewlySetWorkspace(bool isFirstWorkspaceOpen, bool isAutoScalingOnLoad);
-
+bool EXPORT_OPT_MANTIDQT_SLICEVIEWER shouldAutoScaleForNewlySetWorkspace(
+    bool isFirstWorkspaceOpen, bool isAutoScalingOnLoad);
 }
 }
 #endif
