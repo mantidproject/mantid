17c0edcd41a6a8b2c0202411fc4c62cb79c7cf88
Dan Nixon <dan@dan-nixon.com>
2016-02-17 12:54:32 +0000

    Add proper testing Re #10852

--- Framework/CurveFitting/test/Functions/MultivariateGaussianComptonProfileTest.h
+++ Framework/CurveFitting/test/Functions/MultivariateGaussianComptonProfileTest.h
@@ -10,14 +10,14 @@
 using Mantid::CurveFitting::Functions::MultivariateGaussianComptonProfile;
 using Mantid::CurveFitting::Functions::ComptonProfile;
 
-class MutivariateGaussianComptonProfile : public CxxTest::TestSuite {
+class MultivariateGaussianComptonProfileTest : public CxxTest::TestSuite {
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
-  static MutivariateGaussianComptonProfile *createSuite() {
-    return new MutivariateGaussianComptonProfile();
+  static MultivariateGaussianComptonProfileTest *createSuite() {
+    return new MultivariateGaussianComptonProfileTest();
   }
-  static void destroySuite(MutivariateGaussianComptonProfile *suite) {
+  static void destroySuite(MultivariateGaussianComptonProfileTest *suite) {
     delete suite;
   }
 
@@ -27,13 +27,75 @@ public:
     TS_ASSERT_EQUALS("MultivariateGaussianComptonProfile", profile->name());
   }
 
-  // TODO
+  void test_Initialized_Function_Has_Expected_Parameters_In_Right_Order() {
+    Mantid::API::IFunction_sptr profile = createFunction();
+    static const size_t nparams(3);
+    const char *expectedParams[nparams] = {"SigmaX", "SigmaY", "SigmaZ"};
+
+    auto currentNames = profile->getParameterNames();
+    const size_t nnames = currentNames.size();
+    TS_ASSERT_EQUALS(nparams, nnames);
+    if (nnames == nparams) {
+      for (size_t i = 0; i < nnames; ++i) {
+        TS_ASSERT_EQUALS(expectedParams[i], currentNames[i]);
+      }
+    }
+  }
+
+  void test_Initialized_Function_Has_Expected_Attributes() {
+    Mantid::API::IFunction_sptr profile = createFunction();
+    static const size_t nattrs(2);
+    const char *expectedAttrs[nattrs] = {"Mass", "IntegrationSteps"};
+
+    TS_ASSERT_EQUALS(nattrs, profile->nAttributes());
+
+    // Test names as they are used in scripts
+    if (profile->nAttributes() > 0) {
+      std::set<std::string> expectedAttrSet(expectedAttrs,
+                                            expectedAttrs + nattrs);
+      std::vector<std::string> actualNames = profile->getAttributeNames();
+
+      for (size_t i = 0; i < nattrs; ++i) {
+        const std::string &name = actualNames[i];
+        size_t keyCount = expectedAttrSet.count(name);
+        TSM_ASSERT_EQUALS("Expected " + name +
+                              " to be found as attribute but it was not.",
+                          1, keyCount);
+      }
+    }
+  }
+
+  void test_Expected_Results_Returned_Given_Data() {
+    using namespace Mantid::API;
+
+    auto func = createFunctionWithParamsSet();
+    double x0(200.0), x1(220.0), dx(10.0);
+    auto testWS = ComptonProfileTestHelpers::createTestWorkspace(1, x0, x1, dx);
+    auto &dataX = testWS->dataX(0);
+    std::transform(
+        dataX.begin(), dataX.end(), dataX.begin(),
+        std::bind2nd(std::multiplies<double>(), 1e-06)); // to seconds
+    func->setMatrixWorkspace(testWS, 0, dataX.front(), dataX.back());
+    FunctionDomain1DView domain(dataX.data(), dataX.size());
+    FunctionValues values(domain);
+
+    TS_ASSERT_THROWS_NOTHING(func->function(domain, values));
+
+    const double tol(1e-10);
+    TS_ASSERT_DELTA(0.0335150539, values.getCalculated(0), tol);
+    TS_ASSERT_DELTA(0.0198096734, values.getCalculated(1), tol);
+    TS_ASSERT_DELTA(0.0114435282, values.getCalculated(2), tol);
+  }
 
 private:
   boost::shared_ptr<MultivariateGaussianComptonProfile>
   createFunctionWithParamsSet() {
     auto func = createFunction();
-    // TODO
+    func->setAttributeValue("Mass", 1.0);
+    func->setAttributeValue("IntegrationSteps", 35);
+    func->setParameter("SigmaX", 2.5);
+    func->setParameter("SigmaY", 2.5);
+    func->setParameter("SigmaZ", 6.0);
     func->setUpForFit();
     return func;
   }
@@ -45,4 +107,4 @@ private:
   }
 };
 
-#endif /* MANTID_CURVEFITTING_GAUSSIANCOMPTONPROFILETEST_H_ */
+#endif /* MANTID_CURVEFITTING_MULTIVARIATEGAUSSIANCOMPTONPROFILETEST_H_ */
