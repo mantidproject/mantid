13c692ee3804c4eb3ea4d39672f630bfeb458cc6
Simon Heybrock <simon.heybrock@esss.se>
2016-02-17 16:21:33 +0100

    Re #15294 Better error handling in GeometryInfoFactory constructor.

--- Framework/API/src/GeometryInfoFactory.cpp
+++ Framework/API/src/GeometryInfoFactory.cpp
@@ -9,9 +9,21 @@ namespace API {
 
 GeometryInfoFactory::GeometryInfoFactory(const MatrixWorkspace &workspace)
     : m_workspace(workspace), m_instrument(workspace.getInstrument()) {
+  if (!m_instrument)
+    throw std::runtime_error("Workspace " + workspace.getName() +
+                             " does not contain an instrument!");
+
   // TODO: Create these only when needed (thread-safe via atomics)
   m_source = m_instrument->getSource();
   m_sample = m_instrument->getSample();
+
+  if (!m_source)
+    throw std::runtime_error("Instrument in workspace " + workspace.getName() +
+                             " does not contain source!");
+  if (!m_sample)
+    throw std::runtime_error("Instrument in workspace " + workspace.getName() +
+                             "  does not contain sample!");
+
   m_sourcePos = m_source->getPos();
   m_samplePos = m_sample->getPos();
   m_L1 = getSource().getDistance(getSample());
