4b5012d26edcf95eb36484552af3f928a3941079
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-02-22 13:13:45 +0000

    Change ICat, ISISLiveData, LiveData to use unique_ptrs
    re #15289

--- Framework/ISISLiveData/src/FakeISISEventDAE.cpp
+++ Framework/ISISLiveData/src/FakeISISEventDAE.cpp
@@ -162,17 +162,22 @@ FakeISISEventDAE::~FakeISISEventDAE() {
 * Declare the algorithm properties
 */
 void FakeISISEventDAE::init() {
-  declareProperty(new PropertyWithValue<int>("NPeriods", 1, Direction::Input),
-                  "Number of periods.");
-  declareProperty(new PropertyWithValue<int>("NSpectra", 100, Direction::Input),
-                  "Number of spectra.");
-  declareProperty(new PropertyWithValue<int>("Rate", 20, Direction::Input),
-                  "Rate of sending the data: stream of NEvents events is sent "
-                  "every Rate milliseconds.");
-  declareProperty(new PropertyWithValue<int>("NEvents", 1000, Direction::Input),
-                  "Number of events in each packet.");
-  declareProperty(new PropertyWithValue<int>("Port", 59876, Direction::Input),
-                  "The port to broadcast on (default 59876, ISISDAE 10000).");
+  declareProperty(
+      make_unique<PropertyWithValue<int>>("NPeriods", 1, Direction::Input),
+      "Number of periods.");
+  declareProperty(
+      make_unique<PropertyWithValue<int>>("NSpectra", 100, Direction::Input),
+      "Number of spectra.");
+  declareProperty(
+      make_unique<PropertyWithValue<int>>("Rate", 20, Direction::Input),
+      "Rate of sending the data: stream of NEvents events is sent "
+      "every Rate milliseconds.");
+  declareProperty(
+      make_unique<PropertyWithValue<int>>("NEvents", 1000, Direction::Input),
+      "Number of events in each packet.");
+  declareProperty(
+      make_unique<PropertyWithValue<int>>("Port", 59876, Direction::Input),
+      "The port to broadcast on (default 59876, ISISDAE 10000).");
 }
 
 /**
