03402b0bdd40dbbd6c6971b2ce95c27298928995
Simon Heybrock <simon.heybrock@esss.se>
2016-02-24 09:42:29 +0100

    Re #15409 Added class SpectrumIndexSet, to be used in algorithms.

---
+++ Framework/Kernel/inc/MantidKernel/SpectrumIndexSet.h
@@ -0,0 +1,37 @@
+#ifndef MANTID_KERNEL_SPECTRUMINDEXSET_H_
+#define MANTID_KERNEL_SPECTRUMINDEXSET_H_
+
+#include <vector>
+
+#include "MantidKernel/DllConfig.h"
+
+namespace Mantid {
+namespace Kernel {
+
+class MANTID_KERNEL_DLL SpectrumIndexSet {
+public:
+  SpectrumIndexSet(size_t numberOfHistograms);
+  SpectrumIndexSet(int64_t min, int64_t max, size_t numberOfHistograms);
+  SpectrumIndexSet(const std::vector<size_t> indices,
+                   size_t numberOfHistograms);
+
+  size_t size() const { return m_size; }
+
+  size_t operator[](size_t index) const {
+    if (m_isRange)
+      return m_min + index;
+    return m_indices[index];
+  }
+
+private:
+  bool m_isRange;
+  // Default here to avoid uninitialized warning for m_isRange = true.
+  size_t m_min = 0;
+  size_t m_size;
+  std::vector<size_t> m_indices;
+};
+
+} // Namespace Kernel
+} // Namespace Mantid
+
+#endif /* MANTID_KERNEL_SPECTRUMINDEXSET_H_ */
