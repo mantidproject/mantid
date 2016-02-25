752b270bd05993bf3ef894ae7adc5e9dcc20561f
Simon Heybrock <simon.heybrock@esss.se>
2016-02-25 13:32:11 +0100

    Re #15409 Doxygen and comments for IndexSet.

--- Framework/Kernel/inc/MantidKernel/IndexSet.h
+++ Framework/Kernel/inc/MantidKernel/IndexSet.h
@@ -8,14 +8,52 @@
 namespace Mantid {
 namespace Kernel {
 
+/** IndexSet is a container that can be used to define and access a subset of
+  elements in a larger container such as std::vector.
+
+  In particular this is used when accessing the spectra in a workspace. Users
+  frequently need to specify a certain range or list of spectra to use for an
+  operation. This class provides a set of indices for this purpose.
+
+  @author Simon Heybrock, ESS
+
+  Copyright &copy; 2016 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
+  National Laboratory & European Spallation Source
+
+  This file is part of Mantid.
+
+  Mantid is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  Mantid is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  File change history is stored at: <https://github.com/mantidproject/mantid>
+  Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+
 class MANTID_KERNEL_DLL IndexSet {
 public:
+  /// Constructor for a set covering the full range from 0 to fullRange-1.
   IndexSet(size_t fullRange);
+  /// Constructor for a set covering the range from min to max. Range is
+  /// verified at construction time.
   IndexSet(int64_t min, int64_t max, size_t fullRange);
+  /// Constructor for a set containing all specified indices. Range is verified
+  /// at construction time and duplicates are removed.
   IndexSet(const std::vector<size_t> indices, size_t fullRange);
 
+  /// Returns the size of the set.
   size_t size() const { return m_size; }
 
+  // Returns the element at given index (range 0...size()-1).
   size_t operator[](size_t index) const {
     if (m_isRange)
       return m_min + index;
@@ -24,7 +62,7 @@ public:
 
 private:
   bool m_isRange = true;
-  // Default here to avoid uninitialized warning for m_isRange = true.
+  // Default here to avoid uninitialized warning for m_isRange = false.
   size_t m_min = 0;
   size_t m_size;
   std::vector<size_t> m_indices;
--- Framework/Kernel/src/IndexSet.cpp
+++ Framework/Kernel/src/IndexSet.cpp
@@ -6,12 +6,15 @@
 using namespace Mantid;
 using namespace Kernel;
 
+IndexSet::IndexSet(size_t fullRange) : m_size(fullRange) {}
+
 IndexSet::IndexSet(int64_t min, int64_t max, size_t fullRange) {
   if (min < 0 || min > max)
     throw Exception::IndexError(min, max, "IndexSet - min");
   if (max >= static_cast<int64_t>(fullRange))
     throw Exception::IndexError(max, fullRange - 1, "IndexSet - max");
 
+  // Bounds checked, cast should be fine in all cases.
   m_min = static_cast<size_t>(min);
   m_size = static_cast<size_t>(max - min + 1);
 }
@@ -24,10 +27,8 @@ IndexSet::IndexSet(const std::vector<size_t> indices, size_t fullRange)
     if (index >= fullRange)
       throw Exception::IndexError(index, fullRange - 1,
                                   "IndexSet - index vector entry");
-    index_set.insert(static_cast<size_t>(index));
+    index_set.insert(index);
   }
   m_indices = std::vector<size_t>(begin(index_set), end(index_set));
   m_size = m_indices.size();
 }
-
-IndexSet::IndexSet(size_t fullRange) : m_size(fullRange) {}
