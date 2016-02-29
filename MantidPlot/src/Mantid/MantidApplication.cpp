67e2ba91095877d44237b93991f7408ba634467e
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2016-02-29 17:35:49 +0000

    output exception details, re #15214

--- MantidPlot/src/Mantid/MantidApplication.cpp
+++ MantidPlot/src/Mantid/MantidApplication.cpp
@@ -25,8 +25,8 @@ MantidApplication::MantidApplication(int &argc, char ** argv ) : QApplication(ar
      Mantid::Kernel::UsageService::Instance().setApplication("mantidplot");
    } catch(std::runtime_error &rexc) {
      g_log.error() << "Failed to initialize the Mantid usage service. This "
-       "is probably a sign that this Mantid is not fully or correctly set up."
-       << std::endl;
+       "is probably a sign that this Mantid is not fully or correctly set up. "
+       "Error details: " + std::string(rexc.what()) << std::endl;
    }
 }
 
