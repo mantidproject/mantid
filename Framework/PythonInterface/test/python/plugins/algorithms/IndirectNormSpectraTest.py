8d0e444e0771b416a4a91e67a787c8cb34fc2492
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-02-29 17:12:30 +0000

    Add test for data with zeros
    Refs #15481

--- Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
@@ -5,20 +5,30 @@ from mantid.api import MatrixWorkspace, WorkspaceGroup
 
 class IndirectNormSpectraTest(unittest.TestCase):
 
+    _positive ='1,2,3,4,5'
+    _negative ='-5,-4,-3,-2,-1'
+    _zeros ='0,0,0,0,0'
+
+
 #----------------------------------Algorithm tests----------------------------------------
 
     def test_with_MatrixWorkspace_one_hist_positive(self):
-        in_ws = self._create_MatrixWorkspace(1, 'test', True)
+        in_ws = self._create_MatrixWorkspace(1, 'test', self._positive)
         out_ws = IndirectNormSpectra(InputWorkspace=in_ws)
         self._check_MatrixWorkspace_for_y_more_than_one(out_ws)
 
     def test_with_MatrixWorkspace_one_hist_negative(self):
-        in_ws = self._create_MatrixWorkspace(1, 'test', False)
+        in_ws = self._create_MatrixWorkspace(1, 'test', self._negative)
+        out_ws = IndirectNormSpectra(InputWorkspace=in_ws)
+        self._check_MatrixWorkspace_for_y_more_than_one(out_ws)
+
+    def test_with_MatrixWorkspace_one_hist_zeors(self):
+        in_ws = self._create_MatrixWorkspace(1, 'test', self._zeros)
         out_ws = IndirectNormSpectra(InputWorkspace=in_ws)
         self._check_MatrixWorkspace_for_y_more_than_one(out_ws)
 
     def test_with_MatrixWorkspace_multi_hist_(self):
-        in_ws = self._create_MatrixWorkspace(3, 'test', True)
+        in_ws = self._create_MatrixWorkspace(3, 'test', self._positive)
         out_ws = IndirectNormSpectra(InputWorkspace=in_ws)
         self._check_MatrixWorkspace_for_y_more_than_one(out_ws)
 
@@ -33,21 +43,19 @@ class IndirectNormSpectraTest(unittest.TestCase):
             self.assertLessEqual(y_data[i], 1)
 
 #--------------------------------Helper Functions-----------------------------------------
-    def _create_MatrixWorkspace(self, nhists, out_name, positive):
+    def _create_MatrixWorkspace(self, nhists, out_name, data_string):
         """
         Creates a basic Matrixworkspace
         """
         data = ""
         for i in range(nhists):
-            if positive:
-                data +='1,2,3,4,5'
-            else:
-                data +='-5,-4,-3,-2,-1'
+            data += data_string
             if i != (nhists - 1):
                 data += ','
         CreateWorkspace(OutputWorkspace=out_name, DataX=data,
                         DataY=data, DataE=data, Nspec=nhists)
         return mtd[out_name]
+
 
 if __name__=="__main__":
     unittest.main()
