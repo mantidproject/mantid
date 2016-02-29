84381f69d5edf01eb799ed8cd94bf1e89a492fdf
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-02-29 13:23:43 +0000

    Add validation for workspacegroup case
    Refs #15481

--- Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
@@ -26,10 +26,12 @@ class IndirectNormSpectraTest(unittest.TestCase):
     def test_with_TableWorkspace(self):
     
 #--------------------------------Validate results-----------------------------------------
+    def _check_all_items_in_workspace_group(self, workspace_group):
+        items = workspace_group.getNumberOfEntries
+        for i in range(items):
+            self._check_MatrixWorkspace_for_y_more_than_one(workspace_group.getItem(i))
+
     def _check_MatrixWorkspace_for_y_more_than_one(self, matrixWs):
-        """
-        Checks if any of the y values in the matrix workspace are more than 1
-        """
         for i in range(matrixWs.getNumberHistograms()):
             self._check_spectrum_for_y_more_than_one(matrixWs.readY(i))
 
@@ -38,19 +40,17 @@ class IndirectNormSpectraTest(unittest.TestCase):
             self.assertLessEqual(y_data[i], 1)
     
 #--------------------------------Helper Functions-----------------------------------------
-    def _create_MatrixWorkspace(self, nhists, name):
+    def _create_MatrixWorkspace(self, nhists, out_name):
         """
-        Creates a basic Matrixworkspace with nHists
-        @param nhists :: The numebr of histograms
-        @param name :: The name of the MatrixWorkspace
+        Creates a basic Matrixworkspace
         """
         data = []
         for i in range(nhists):
             data.append[1,2,3,4,5]
-        CreateWorkspace(OutputWorkspace=name, DataX=data, DataY=data, DataE=data, Nspec=nhists)
-        return mtd[name]
+        CreateWorkspace(OutputWorkspace=out_name, DataX=data, DataY=data, DataE=data, Nspec=nhists)
+        return mtd[out_name]
 
-    def _create_WorkspaceGroup(self, items, name):
+    def _create_WorkspaceGroup(self, items, out_name):
         """
         Creates a WorkspaceGroup with n items
         @parmas items :: The number of entries in the WorkspaceGroup
@@ -60,8 +60,8 @@ class IndirectNormSpectraTest(unittest.TestCase):
         for i in range(items):
             ws = self._create_MatrixWorkspace(items, (name + str(i)))
             workspaces.append(ws.getName())
-        GroupWorkspaces(InputWorkspaces=workspaces, OutputWorkspace=name+'_group')
-        return mtd[name + '_group']
+        GroupWorkspaces(InputWorkspaces=workspaces, OutputWorkspace=out_name+'_group')
+        return mtd[out_name + '_group']
 
 if __name__=="__main__":
     unittest.main()
