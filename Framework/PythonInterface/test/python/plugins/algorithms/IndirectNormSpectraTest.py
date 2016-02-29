f16db3907638d99e0314d8bdf799a6f9a9145acc
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-02-29 13:08:38 +0000

    Add Helper functions to unit test
    Refs #15481

--- Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
@@ -28,6 +28,31 @@ class IndirectNormSpectraTest(unittest.TestCase):
 #--------------------------------Validate results-----------------------------------------
     def _check_MatrixWorkspace_for_y_more_than_1(self):
     
+#--------------------------------Helper Functions-----------------------------------------
+    def create_MatrixWorkspace(self, nhists, name):
+        """
+        Creates a basic Matrixworkspace with nHists
+        @param nhists :: The numebr of histograms
+        @param name :: The name of the MatrixWorkspace
+        """
+        data = []
+        for i in range(nhists):
+            data.append[1,2,3,4,5]
+        CreateWorkspace(OutputWorkspace=name, DataX=data, DataY=data, DataE=data, Nspec=nhists)
+        return mtd[name]
+
+    def create_WorkspaceGroup(self, items, name):
+        """
+        Creates a WorkspaceGroup with n items
+        @parmas items :: The number of entries in the WorkspaceGroup
+        @params name  :: The base name of the Workspaces
+        """
+        workspaces = []
+        for i in range(items):
+            ws = self._create_MatrixWorkspace(items, (name + str(i)))
+            workspaces.append(ws.getName())
+        GroupWorkspaces(InputWorkspaces=workspaces, OutputWorkspace=name+'_group')
+        return mtd[name + '_group']
 
 if __name__=="__main__":
     unittest.main()
