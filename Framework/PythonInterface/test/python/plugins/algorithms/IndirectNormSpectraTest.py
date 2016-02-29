5c3bf732fd0ae46a9f10c2136a0f202897135bf9
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-02-29 13:17:09 +0000

    Implement functions to validate results
    Refs #15481

--- Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/IndirectNormSpectraTest.py
@@ -12,7 +12,7 @@ class IndirectNormSpectraTest(unittest.TestCase):
 #----------------------------------Algorithm tests----------------------------------------
 
     def test_with_MatrixWorkspace_one_hist(self):
-    
+
     def test_with_MatrixWorkspace_multi_hist(self):
     
     def test_with_WorkspaceGroup_one_workspace(self):
@@ -26,10 +26,19 @@ class IndirectNormSpectraTest(unittest.TestCase):
     def test_with_TableWorkspace(self):
     
 #--------------------------------Validate results-----------------------------------------
-    def _check_MatrixWorkspace_for_y_more_than_1(self):
+    def _check_MatrixWorkspace_for_y_more_than_one(self, matrixWs):
+        """
+        Checks if any of the y values in the matrix workspace are more than 1
+        """
+        for i in range(matrixWs.getNumberHistograms()):
+            self._check_spectrum_for_y_more_than_one(matrixWs.readY(i))
+
+    def _check_spectrum_for_y_more_than_one(self, y_data):
+        for i in range(len(y_data)):
+            self.assertLessEqual(y_data[i], 1)
     
 #--------------------------------Helper Functions-----------------------------------------
-    def create_MatrixWorkspace(self, nhists, name):
+    def _create_MatrixWorkspace(self, nhists, name):
         """
         Creates a basic Matrixworkspace with nHists
         @param nhists :: The numebr of histograms
@@ -41,7 +50,7 @@ class IndirectNormSpectraTest(unittest.TestCase):
         CreateWorkspace(OutputWorkspace=name, DataX=data, DataY=data, DataE=data, Nspec=nhists)
         return mtd[name]
 
-    def create_WorkspaceGroup(self, items, name):
+    def _create_WorkspaceGroup(self, items, name):
         """
         Creates a WorkspaceGroup with n items
         @parmas items :: The number of entries in the WorkspaceGroup
