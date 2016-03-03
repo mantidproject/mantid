b15f219d0b484e070bcc68593ce19998286ab732
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-03-03 09:47:37 +0000

    Add importing of all in __init__
    Refs #14993

--- scripts/Inelastic/vesuvio/__init__.py
+++ scripts/Inelastic/vesuvio/__init__.py
@@ -1,9 +1,13 @@
 """Supports the Vesuvio instrument at ISIS
-The module requires Mantid and importing it registers additional algorithms into
-the framework
+
+backgrounds -- Defines backgrounds for fitting
+base        -- Helper functions for Vesuvio algorithms
+commands    -- Used to start processing of Vesuvio data
+fitting     -- Support routines for fitting
+instrument  -- Instrument specific data for Vesuvio
+profiles    -- Defines mass profiles
 """
+
 from __future__ import absolute_import
 
-#import vesuvio.commands as commands
-
-import mantid
+__all__=['backgrounds','base','commands','fitting','instrument','profiles']
