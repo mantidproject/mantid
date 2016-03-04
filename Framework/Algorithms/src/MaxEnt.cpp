09a0424e8234d1c319edb72497928b5781b858ca
Raquel Alvarez Banos <raquel.alvarez.banos@gmail.com>
2016-03-04 17:10:14 +0000

    Re #15265 Accept X rounding errors

--- Framework/Algorithms/src/MaxEnt.cpp
+++ Framework/Algorithms/src/MaxEnt.cpp
@@ -133,16 +133,35 @@ std::map<std::string, std::string> MaxEnt::validateInputs() {
 
   std::map<std::string, std::string> result;
 
-  // X values in input workspace must be equally spaced
+  // 1. X values in input workspace must be (almost) equally spaced
+
   MatrixWorkspace_sptr inWS = getProperty("InputWorkspace");
+  const double warningLevel = 0.01;
+  const double errorLevel = 0.5;
+  bool printWarning = false;
+  // Average spacing
   const MantidVec &X = inWS->readX(0);
-  const double dx = X[1] - X[0];
-  for (size_t i = 1; i < X.size() - 2; i++) {
-    if (std::abs(dx - X[i + 1] + X[i]) / dx > 1e-7) {
-      result["InputWorkspace"] =
-          "X axis must be linear (all bins must have the same width)";
+  const double dx = (X[X.size() - 1] - X[0]) / (X.size() - 1);
+  for (size_t i = 1; i < X.size() - 1; i++) {
+    // 1% accuracy exceeded, but data still usable
+    if (std::abs(X[i] - X[0] - i * dx) / dx > warningLevel) {
+      printWarning = true;
+      if (std::abs(X[i] - X[0] - i * dx) / dx > errorLevel) {
+        // 50% accuracy exceeded, data not usable
+        printWarning = false;
+        result["InputWorkspace"] =
+            "X axis must be linear (all bins have same width)";
+        break;
+      }
     }
   }
+  if (printWarning) {
+    g_log.warning() << "Bin widths differ by more than " << warningLevel * 100
+                    << "% of average\n";
+  }
+
+  // 2. If the input signal is complex, we expect an even number of histograms
+  // in the input workspace
 
   size_t nhistograms = inWS->getNumberHistograms();
   bool complex = getProperty("ComplexData");
