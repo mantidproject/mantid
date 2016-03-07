0938c61491316c5893131f13162c4d5169ebcfa7
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-03-07 11:20:14 +0000

    Tidy up existing tests for ConvertToDistribution
    re #15575
    
    Remove dependence on ADS and make the tests independent, so the order they are run in doesn't matter

--- Framework/Algorithms/test/ConvertToDistributionTest.h
+++ Framework/Algorithms/test/ConvertToDistributionTest.h
@@ -17,35 +17,31 @@ public:
   }
   static void destroySuite(ConvertToDistributionTest *suite) { delete suite; }
 
-  ConvertToDistributionTest() : dist("notDist") {
-    Workspace_sptr WS =
-        WorkspaceCreationHelper::Create2DWorkspaceBinned(1, 10, 0, 0.5);
-    AnalysisDataService::Instance().add(dist, WS);
+  void testName() {
+    ConvertToDistribution conv;
+    TS_ASSERT_EQUALS(conv.name(), "ConvertToDistribution")
   }
 
-  void testName() { TS_ASSERT_EQUALS(conv.name(), "ConvertToDistribution") }
-
-  void testVersion() { TS_ASSERT_EQUALS(conv.version(), 1) }
+  void testVersion() {
+    ConvertToDistribution conv;
+    TS_ASSERT_EQUALS(conv.version(), 1)
+  }
 
   void testInit() {
+    ConvertToDistribution conv;
     TS_ASSERT_THROWS_NOTHING(conv.initialize())
     TS_ASSERT(conv.isInitialized())
   }
 
   void testExec() {
-    if (!conv.isInitialized())
-      conv.initialize();
-
-    TS_ASSERT_THROWS_NOTHING(conv.setPropertyValue("Workspace", dist))
-
-    TS_ASSERT_THROWS_NOTHING(conv.execute())
-    TS_ASSERT(conv.isExecuted())
-
-    MatrixWorkspace_const_sptr output;
-    TS_ASSERT_THROWS_NOTHING(
-        output =
-            AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(dist))
-
+    ConvertToDistribution conv;
+    conv.initialize();
+    conv.setChild(true);
+    auto dist = createTestWorkspace();
+    TS_ASSERT_THROWS_NOTHING(conv.setProperty("Workspace", dist));
+    TS_ASSERT_THROWS_NOTHING(conv.execute());
+    TS_ASSERT(conv.isExecuted());
+    auto output = boost::dynamic_pointer_cast<MatrixWorkspace>(dist);
     const Mantid::MantidVec &X = output->dataX(0);
     const Mantid::MantidVec &Y = output->dataY(0);
     const Mantid::MantidVec &E = output->dataE(0);
@@ -55,13 +51,12 @@ public:
       TS_ASSERT_EQUALS(E[i], sqrt(2.0) / 0.5)
     }
     TS_ASSERT(output->isDistribution())
-
-    AnalysisDataService::Instance().remove(dist);
   }
 
 private:
-  ConvertToDistribution conv;
-  std::string dist;
+  Workspace_sptr createTestWorkspace() {
+    return WorkspaceCreationHelper::Create2DWorkspaceBinned(1, 10, 0, 0.5);
+  }
 };
 
 #endif /*CONVERTTODISTRIBUTIONTEST_H_*/
