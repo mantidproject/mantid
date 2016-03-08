44bbd92353563cf1a1ed7d550487f59044b36a78
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2016-03-08 16:25:35 +0000

    sets up the presWorker for fitting thread
    refs #15550

--- MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/EnggDiffraction/EnggDiffractionPresWorker.h
+++ MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/EnggDiffraction/EnggDiffractionPresWorker.h
@@ -67,6 +67,13 @@ public:
       : m_pres(pres), m_runNo(runNo), m_bin(timeStep), m_nperiods(nperiods),
         m_outWSName(outWSName) {}
 
+  // for fitting (single peak fits)
+  EnggDiffWorker(EnggDiffractionPresenter *pres,
+                 const std::string &focusedRunNo,
+                 const std::string &ExpectedPeaks)
+      : m_pres(pres), m_runNo(focusedRunNo), m_expectedPeaks(ExpectedPeaks),
+        m_bin(.0), m_nperiods(0) {}
+
 private slots:
 
   /**
@@ -103,6 +110,11 @@ private slots:
     emit finished();
   }
 
+  void fitting() {
+    m_pres->doFitting(m_runNo, m_expectedPeaks);
+    emit finished();
+  }
+
 signals:
   void finished();
 
@@ -131,6 +143,7 @@ private:
   const double m_bin;
   const size_t m_nperiods;
   const std::string m_outWSName;
+  const std::string m_expectedPeaks;
 };
 
 } // namespace CustomInterfaces
