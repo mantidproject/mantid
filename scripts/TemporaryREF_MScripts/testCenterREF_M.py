84e3400eccd4806b87aba39265e6e1c5c2f011f0
Andrei Savici <saviciat@ornl.gov>
2016-03-11 16:40:07 -0500

    Refs #15577. Update test script to calculate reflectivity

--- scripts/TemporaryREF_MScripts/testCenterREF_M.py
+++ scripts/TemporaryREF_MScripts/testCenterREF_M.py
@@ -18,10 +18,22 @@ def calculateCenter(ws):
     rotation010=np.degrees(mantid.kernel.V3D(avex,0,avez).angle(mantid.kernel.V3D(0,0,1)))
     return (-avey,-rotation010)
     
-central=mantid.simpleapi.LoadEventNexus('REF_M_22715',NXentryName='entry-On_Off')
+central=mantid.simpleapi.LoadEventNexus('REF_M_22715',NXentryName='entry-Off_Off')
 original=mantid.simpleapi.CloneWorkspace(central)
 translation,rotation=calculateCenter(original)
-mantid.simpleapi.MoveInstrumentComponent(Workspace=central,ComponentName="DetectorArm",X=0,Y=translation,Z=1)
-mantid.simpleapi.RotateInstrumentComponent(Workspace=central,ComponentName="DetectorArm",X=0,Y=1,Z=0,Angle=rotation)
+mantid.simpleapi.MoveInstrumentComponent(Workspace=central,ComponentName="DetectorArm",X=0,Y=translation,Z=1,RelativePosition=1)
+mantid.simpleapi.RotateInstrumentComponent(Workspace=central,ComponentName="DetectorArm",X=0,Y=1,Z=0,Angle=rotation,RelativeRotation=1)
 
-print calculateCenter(central)
+atangle=mantid.simpleapi.LoadEventNexus('REF_M_22710',NXentryName='entry-Off_Off')
+mantid.simpleapi.MoveInstrumentComponent(Workspace=atangle,ComponentName="DetectorArm",X=0,Y=translation,Z=1,RelativePosition=1)
+mantid.simpleapi.RotateInstrumentComponent(Workspace=atangle,ComponentName="DetectorArm",X=0,Y=1,Z=0,Angle=rotation,RelativeRotation=1)
+
+central=ConvertUnits(central,Target="Wavelength",EMode="Elastic")
+central=Rebin(central,"3.5,0.1,7")
+
+atangle=ConvertUnits(atangle,Target="Wavelength",EMode="Elastic")
+atangle=Rebin(atangle,"3.5,0.1,7")
+
+csum=SumSpectra(central)
+asum=SumSpectra(atangle)
+normalized=asum/csum
