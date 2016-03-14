c0dee4012d74467601169768c36f7fb4b845f0bb
Steven Hahn <hahnse@ornl.gov>
2016-03-14 18:12:32 -0400

    Refs #15239. Begin adding unit tests of ellipsoid shape.

--- Vates/VatesAPI/test/vtkPeakMarkerFactoryTest.h
+++ Vates/VatesAPI/test/vtkPeakMarkerFactoryTest.h
@@ -22,6 +22,14 @@ using namespace ::testing;
 using namespace Mantid::VATES;
 using Mantid::VATES::vtkPeakMarkerFactory;
 
+class MockPeak1 : public Peak {
+public:
+  MOCK_CONST_METHOD0(getHKL, Mantid::Kernel::V3D(void));
+  MOCK_CONST_METHOD0(getQLabFrame, Mantid::Kernel::V3D(void));
+  MOCK_CONST_METHOD0(getQSampleFrame, Mantid::Kernel::V3D(void));
+  MOCK_CONST_METHOD0(getPeakShape, const Mantid::Geometry::PeakShape &(void));
+};
+
 class MockPeak : public Peak
 {
 public:
@@ -53,8 +61,7 @@ public:
   void do_test(MockPeak & peak1, vtkPeakMarkerFactory::ePeakDimensions dims)
   {
     FakeProgressAction updateProgress;
-    boost::shared_ptr<MockPeaksWorkspace> pw_ptr =
-        boost::make_shared<MockPeaksWorkspace>();
+    auto pw_ptr = boost::make_shared<MockPeaksWorkspace>();
     MockPeaksWorkspace & pw = *pw_ptr;
 
     //Peaks workspace will return 5 identical peaks
@@ -67,7 +74,7 @@ public:
         vtkSmartPointer<vtkPolyData>::Take(factory.create(updateProgress));
 
     // As the marker type are three axes(2 points), we expect 5*2*3 points
-    // The angle is 45degrees and the size is 0.3
+    // The angle is 0 degrees and the size is 0.3
 
     TS_ASSERT(set);
     TS_ASSERT_EQUALS(set->GetNumberOfPoints(), 30);
@@ -172,47 +179,151 @@ public:
 
   void testGetPeakRadiusWhenNotIntegrated()
   {
-    IPeaksWorkspace* mockWorkspace = new MockPeaksWorkspace;
+    auto mockWorkspace = Mantid::Kernel::make_unique<MockPeaksWorkspace>();
     const double expectedRadius =-1; // The default
     // Note that no PeaksRadius property has been set.
 
     vtkPeakMarkerFactory factory("signal");
-    factory.initialize(Mantid::API::IPeaksWorkspace_sptr(mockWorkspace));
+    factory.initialize(
+        Mantid::API::IPeaksWorkspace_sptr(std::move(mockWorkspace)));
     TS_ASSERT_EQUALS(expectedRadius, factory.getIntegrationRadius());
   }
 
   void testIsPeaksWorkspaceIntegratedWhenNotIntegrated()
   {
-    IPeaksWorkspace* mockWorkspace = new MockPeaksWorkspace;
+    auto mockWorkspace = Mantid::Kernel::make_unique<MockPeaksWorkspace>();
     // Note that no PeaksRadius property has been set.
 
     vtkPeakMarkerFactory factory("signal");
-    factory.initialize(Mantid::API::IPeaksWorkspace_sptr(mockWorkspace));
+    factory.initialize(
+        Mantid::API::IPeaksWorkspace_sptr(std::move(mockWorkspace)));
     TS_ASSERT_EQUALS(false, factory.isPeaksWorkspaceIntegrated()); // false is the default
   }
 
   void testGetPeakRadiusWhenIntegrated()
   {
-    IPeaksWorkspace* mockWorkspace = new MockPeaksWorkspace;
+    auto mockWorkspace = Mantid::Kernel::make_unique<MockPeaksWorkspace>();
     const double expectedRadius = 4;
     mockWorkspace->mutableRun().addProperty("PeakRadius", expectedRadius, true); // Has a PeaksRadius so must have been processed via IntegratePeaksMD
 
     vtkPeakMarkerFactory factory("signal");
-    factory.initialize(Mantid::API::IPeaksWorkspace_sptr(mockWorkspace));
+    factory.initialize(
+        Mantid::API::IPeaksWorkspace_sptr(std::move(mockWorkspace)));
     TS_ASSERT_EQUALS(expectedRadius, factory.getIntegrationRadius());
   }
 
   void testIsPeaksWorkspaceIntegratedWhenIntegrated()
   {
-    IPeaksWorkspace* mockWorkspace = new MockPeaksWorkspace;
+    auto mockWorkspace = Mantid::Kernel::make_unique<MockPeaksWorkspace>();
     const double expectedRadius = 4;
     mockWorkspace->mutableRun().addProperty("PeakRadius", expectedRadius, true); // Has a PeaksRadius so must have been processed via IntegratePeaksMD
 
     vtkPeakMarkerFactory factory("signal");
-    factory.initialize(Mantid::API::IPeaksWorkspace_sptr(mockWorkspace));
+    factory.initialize(
+        Mantid::API::IPeaksWorkspace_sptr(std::move(mockWorkspace)));
     TS_ASSERT_EQUALS(true, factory.isPeaksWorkspaceIntegrated()); 
   }
 
+  void testShapeOfEllipsoid() {
+    FakeProgressAction updateProgress;
+    auto pw_ptr = boost::make_shared<MockPeaksWorkspace>();
+    const double expectedRadius = 4;
+    pw_ptr->mutableRun().addProperty("PeakRadius", expectedRadius,
+                                     true); // Has a PeaksRadius so must have
+                                            // been processed via
+                                            // IntegratePeaksMD
+    MockPeaksWorkspace &pw = *pw_ptr;
+
+    std::vector<Mantid::Kernel::V3D> directions{
+        {1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}};
+    std::vector<double> abcRadii{1., 2., 3.};
+    std::vector<double> abcRadiiBackgroundInner{1., 2., 3.};
+    std::vector<double> abcRadiiBackgroundOuter{1., 2., 3.};
+
+    PeakShapeEllipsoid ellipsoid(directions, abcRadii, abcRadiiBackgroundInner,
+                                 abcRadiiBackgroundOuter,
+                                 Kernel::SpecialCoordinateSystem::QLab);
+
+    MockPeak1 peak1;
+    // Peaks workspace will return 5 identical peaks
+    EXPECT_CALL(pw, getNumberPeaks()).WillOnce(Return(1));
+    EXPECT_CALL(pw, getPeak(_)).WillRepeatedly(ReturnRef(peak1));
+    EXPECT_CALL(peak1, getQLabFrame()).WillRepeatedly(Return(V3D(0., 0., 0.)));
+    EXPECT_CALL(peak1, getPeakShape()).WillRepeatedly(ReturnRef(ellipsoid));
+
+    vtkPeakMarkerFactory factory("signal");
+    factory.initialize(pw_ptr);
+    auto set =
+        vtkSmartPointer<vtkPolyData>::Take(factory.create(updateProgress));
+
+    TS_ASSERT(set);
+    TS_ASSERT_EQUALS(set->GetNumberOfPoints(), 300);
+
+    for (vtkIdType i = 0; i < set->GetNumberOfPoints(); ++i) {
+      double pt[3];
+      set->GetPoint(i, pt);
+      std::cout << pt[0] << " " << pt[1] << " " << pt[2] << " "
+                << std::sqrt(pt[0] * pt[0] / (abcRadii[0] * abcRadii[0]) +
+                             pt[1] * pt[1] / (abcRadii[1] * abcRadii[1]) +
+                             pt[2] * pt[2] / (abcRadii[2] * abcRadii[2]))
+                << std::endl;
+    }
+
+    TS_ASSERT(testing::Mock::VerifyAndClearExpectations(&pw));
+    TS_ASSERT(testing::Mock::VerifyAndClearExpectations(&peak1));
+  }
+  void testShapeOfRotatedEllipsoid() {
+    FakeProgressAction updateProgress;
+    auto pw_ptr = boost::make_shared<MockPeaksWorkspace>();
+    const double expectedRadius = 4;
+    pw_ptr->mutableRun().addProperty("PeakRadius", expectedRadius,
+                                     true); // Has a PeaksRadius so must have
+                                            // been processed via
+                                            // IntegratePeaksMD
+    MockPeaksWorkspace &pw = *pw_ptr;
+
+    std::vector<Mantid::Kernel::V3D> directions{
+        {1. / sqrt(2.), 1. / sqrt(2.), 0.},
+        {1. / sqrt(2.), -1. / sqrt(2.0), 0.},
+        {0., 0., 1.}};
+    std::vector<double> abcRadii{1., 1., 1.};
+    std::vector<double> abcRadiiBackgroundInner{1., 2., 3.};
+    std::vector<double> abcRadiiBackgroundOuter{1., 2., 3.};
+
+    PeakShapeEllipsoid ellipsoid(directions, abcRadii, abcRadiiBackgroundInner,
+                                 abcRadiiBackgroundOuter,
+                                 Kernel::SpecialCoordinateSystem::QLab);
+
+    MockPeak1 peak1;
+    // Peaks workspace will return 5 identical peaks
+    EXPECT_CALL(pw, getNumberPeaks()).WillOnce(Return(1));
+    EXPECT_CALL(pw, getPeak(_)).WillRepeatedly(ReturnRef(peak1));
+    EXPECT_CALL(peak1, getQLabFrame()).WillRepeatedly(Return(V3D(0., 0., 0.)));
+    EXPECT_CALL(peak1, getPeakShape()).WillRepeatedly(ReturnRef(ellipsoid));
+
+    vtkPeakMarkerFactory factory("signal");
+    factory.initialize(pw_ptr);
+    auto set =
+        vtkSmartPointer<vtkPolyData>::Take(factory.create(updateProgress));
+
+    TS_ASSERT(set);
+    TS_ASSERT_EQUALS(set->GetNumberOfPoints(), 300);
+
+    for (vtkIdType i = 0; i < set->GetNumberOfPoints(); ++i) {
+      double pt[3];
+      set->GetPoint(i, pt);
+      double rot_x = pt[0] * cos(M_PI_4) - pt[1] * sin(M_PI_4);
+      double rot_y = pt[0] * sin(M_PI_4) + pt[1] * cos(M_PI_4);
+      std::cout << pt[0] << " " << pt[1] << " " << pt[2] << " "
+                << std::sqrt(rot_x * rot_x / (abcRadii[0] * abcRadii[0]) +
+                             rot_y * rot_y / (abcRadii[1] * abcRadii[1]) +
+                             pt[2] * pt[2] / (abcRadii[2] * abcRadii[2]))
+                << std::endl;
+    }
+
+    TS_ASSERT(testing::Mock::VerifyAndClearExpectations(&pw));
+    TS_ASSERT(testing::Mock::VerifyAndClearExpectations(&peak1));
+  }
 };
 
 
