9f4df6e42b7a61603aea52a4a7850d2e2f30dba4
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-03-18 10:27:29 +0000

    Reduce code duplication in VesuvioCorrectionsTest
    Refs #15661

--- Framework/PythonInterface/test/python/plugins/algorithms/VesuvioCorrectionsTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/VesuvioCorrectionsTest.py
@@ -51,21 +51,16 @@ class VesuvioCorrectionsTest(unittest.TestCase):
         self.assertTrue(alg.isExecuted())
 
         corrections_wsg = alg.getProperty("CorrectionWorkspaces").value
-        self.assertTrue(isinstance(corrections_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrections_wsg), 3)
+        self._validate_group_structure(corrections_wsg, 3)
 
         corrected_wsg = alg.getProperty("CorrectedWorkspaces").value
-        self.assertTrue(isinstance(corrected_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrected_wsg), 3)
+        self._validate_group_structure(corrected_wsg, 3)
 
         output_ws = alg.getProperty("OutputWorkspace").value
-        self.assertTrue(isinstance(output_ws, MatrixWorkspace))
-        self.assertEqual(output_ws.getNumberHistograms(), 1)
+        self._validate_matrix_structure(output_ws, 1, 257)
 
         linear_params = alg.getProperty("LinearFitResult").value
-        self.assertTrue(isinstance(linear_params, ITableWorkspace))
-        self.assertEqual(linear_params.columnCount(), 3)
-        self.assertEqual(linear_params.rowCount(), 7)
+        self._validate_table_workspace(linear_params, 7, 3)
 
 
     def test_ms_correct_with_container(self):
@@ -84,21 +79,16 @@ class VesuvioCorrectionsTest(unittest.TestCase):
         self.assertTrue(alg.isExecuted())
 
         corrections_wsg = alg.getProperty("CorrectionWorkspaces").value
-        self.assertTrue(isinstance(corrections_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrections_wsg), 3)
+        self._validate_group_structure(corrections_wsg, 3)
 
         corrected_wsg = alg.getProperty("CorrectedWorkspaces").value
-        self.assertTrue(isinstance(corrected_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrected_wsg), 3)
+        self._validate_group_structure(corrected_wsg, 3)
 
         output_ws = alg.getProperty("OutputWorkspace").value
-        self.assertTrue(isinstance(output_ws, MatrixWorkspace))
-        self.assertEqual(output_ws.getNumberHistograms(), 1)
+        self._validate_matrix_structure(output_ws, 1, 257)
 
         linear_params = alg.getProperty("LinearFitResult").value
-        self.assertTrue(isinstance(linear_params, ITableWorkspace))
-        self.assertEqual(linear_params.columnCount(), 3)
-        self.assertEqual(linear_params.rowCount(), 7)
+        self._validate_table_workspace(linear_params, 7, 3)
 
 
     def test_gamma_and_ms_correct_with_container(self):
@@ -116,22 +106,16 @@ class VesuvioCorrectionsTest(unittest.TestCase):
         self.assertTrue(alg.isExecuted())
 
         corrections_wsg = alg.getProperty("CorrectionWorkspaces").value
-        print corrections_wsg
-        self.assertTrue(isinstance(corrections_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrections_wsg), 4)
+        self._validate_group_structure(corrections_wsg, 4)
 
         corrected_wsg = alg.getProperty("CorrectedWorkspaces").value
-        self.assertTrue(isinstance(corrected_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrected_wsg), 4)
+        self._validate_group_structure(corrected_wsg, 4)
 
         output_ws = alg.getProperty("OutputWorkspace").value
-        self.assertTrue(isinstance(output_ws, MatrixWorkspace))
-        self.assertEqual(output_ws.getNumberHistograms(), 1)
+        self._validate_matrix_structure(output_ws, 1, 257)
 
         linear_params = alg.getProperty("LinearFitResult").value
-        self.assertTrue(isinstance(linear_params, ITableWorkspace))
-        self.assertEqual(linear_params.columnCount(), 3)
-        self.assertEqual(linear_params.rowCount(), 10)
+        self._validate_table_workspace(linear_params, 10, 3)
 
 
     def test_gamma_and_ms_correct_with_container_fixed_scaling(self):
@@ -152,21 +136,16 @@ class VesuvioCorrectionsTest(unittest.TestCase):
         self.assertTrue(alg.isExecuted())
 
         corrections_wsg = alg.getProperty("CorrectionWorkspaces").value
-        self.assertTrue(isinstance(corrections_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrections_wsg), 4)
+        self._validate_group_structure(corrections_wsg, 4)        
 
         corrected_wsg = alg.getProperty("CorrectedWorkspaces").value
-        self.assertTrue(isinstance(corrected_wsg, WorkspaceGroup))
-        self.assertEqual(len(corrected_wsg), 4)
+        self._validate_group_structure(corrected_wsg, 4)
 
         output_ws = alg.getProperty("OutputWorkspace").value
-        self.assertTrue(isinstance(output_ws, MatrixWorkspace))
-        self.assertEqual(output_ws.getNumberHistograms(), 1)
+        self._validate_matrix_structure(output_ws, 1, 257)
 
         linear_params = alg.getProperty("LinearFitResult").value
-        self.assertTrue(isinstance(linear_params, ITableWorkspace))
-        self.assertEqual(linear_params.columnCount(), 3)
-        self.assertEqual(linear_params.rowCount(), 10)
+        self._validate_table_workspace(linear_params, 10, 3)
 
         self.assertAlmostEqual(linear_params.cell(0, 1), 0.1)
         self.assertAlmostEqual(linear_params.cell(1, 1), 0.0)
@@ -244,6 +223,47 @@ class VesuvioCorrectionsTest(unittest.TestCase):
 
     def _create_dummy_profiles(self):
         return "function=GramCharlier,hermite_coeffs=[1, 0, 0],k_free=0,sears_flag=1,width=[2, 5, 7];function=Gaussian,width=10;function=Gaussian,width=13;function=Gaussian,width=30"
+
+    # --------------------------------------------------- Validation ---------------------------------------------------------------
+    # -----------------------Structure------------------------------
+
+    def _validate_group_structure(self, ws_group, expected_entries):
+        """
+        Checks that a workspace is a group and has the correct number of entries
+        ws_group            :: Workspace to be validated
+        expected_entries    :: Expected number of entries
+        """
+        self.assertTrue(isinstance(ws_group, WorkspaceGroup))
+        num_entries = ws_group.getNumberOfEntries()
+        self.assertEqual(num_entries, expected_entries)
+
+    def _validate_matrix_structure(self, matrix_ws, expected_hist, expected_bins):
+        """
+        Checks that a workspace is a matrix workspace and has the correct number of histograms and bins
+        matrix_ws           :: Workspace to be validated
+        expected_hist       :: Expected number of histograms
+        expected_bins       :: Expected number of bins
+        """
+        self.assertTrue(isinstance(matrix_ws, MatrixWorkspace))
+        num_hists = matrix_ws.getNumberHistograms()
+        num_bins = matrix_ws.blocksize()
+        self.assertEqual(num_hists, expected_hist)
+        self.assertEqual(num_bins, expected_bins)
+
+    def _validate_table_workspace(self, table_ws, expected_rows, expected_columns):
+        """
+        Checks that a workspace is a table workspace and has the correct number of rows and columns
+        table_ws            :: Workspace to be validated
+        expected_rows       :: Expected number of rows
+        ecpected_columns    :: Expected number of columns
+        """
+        self.assertTrue(isinstance(table_ws, ITableWorkspace))
+        num_rows = table_ws.rowCount()
+        num_columns = table_ws.columnCount()
+        self.assertEqual(num_rows, expected_rows)
+        self.assertEqual(num_columns, expected_columns)
+        
+    # -----------------------Values-----------------------------------
 
 
 if __name__ == "__main__":
