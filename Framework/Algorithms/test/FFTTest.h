3e2b6b701b63c2486746de3f252967c5e381ae28
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-03-22 17:34:34 +0000

    FFT: add new tests for phase shifts
    - Offset workspaces should not have the same FFT if the phase shift is handled properly.
    
    - complex data and phase shifts
    
    - Gaussian bursts as suggested by James Lord
    
    - Different X spacing/number of points
    
    re #15279

--- Framework/Algorithms/test/FFTTest.h
+++ Framework/Algorithms/test/FFTTest.h
@@ -33,7 +33,8 @@ public:
   static FFTTest *createSuite() { return new FFTTest(); }
   static void destroySuite(FFTTest *suite) { delete suite; }
 
-  FFTTest() : dX(0.2), h(sqrt(M_PI / 3)), a(M_PI * M_PI / 3) {}
+  FFTTest()
+      : dX(0.2), h(sqrt(M_PI / 3)), a(M_PI * M_PI / 3), tolerance(0.001) {}
   ~FFTTest() override {}
 
   void testForward() {
@@ -67,7 +68,6 @@ public:
     for (int i = 0; i < N / 4; i++) {
       int j = i0 + i;
       double x = X[j];
-      // std::cerr<<j<<' '<<x<<' '<<Yr[j]<<' '<<h*exp(-a*x*x)<<'\n';
       TS_ASSERT_DELTA(x, dx * i, 0.00001);
       TS_ASSERT_DELTA(Yr[i0 + i] / (h * exp(-a * x * x)), 1., 0.001);
       TS_ASSERT_DELTA(Yi[i0 + i], 0.0, 0.00001);
@@ -557,7 +557,142 @@ public:
     AnalysisDataService::Instance().clear();
   }
 
+  void test_AutoShift() {
+    // create two workspaces offset from each other
+    const auto inputWS = createWS(100, 0);
+    auto offsetWS = offsetWorkspace(createWS(100, 0));
+
+    // perform transforms for no shift and auto shift
+    auto fftNoShiftNoOffset = doFFT(inputWS, false, false);
+    auto fftNoShiftWithOffset = doFFT(offsetWS, false, false);
+    auto fftAutoShiftNoOffset = doFFT(inputWS, false, true);
+    auto fftAutoShiftWithOffset = doFFT(offsetWS, false, true);
+
+    // perform transforms for manual shift
+    auto fft = FrameworkManager::Instance().createAlgorithm("FFT");
+    fft->initialize();
+    fft->setChild(true);
+    fft->setPropertyValue("OutputWorkspace", "__NotUsed");
+    fft->setPropertyValue("Real", "0");
+    const auto &noOffsetX = inputWS->readX(0);
+    const auto &offsetX = offsetWS->readX(0);
+    const double offsetShift = -offsetX[offsetX.size() / 2];
+    const double noOffsetShift = -noOffsetX[noOffsetX.size() / 2];
+    fft->setProperty("InputWorkspace", inputWS);
+    fft->setProperty("AutoShift", false);
+    fft->setProperty("Shift", noOffsetShift);
+    fft->execute();
+    MatrixWorkspace_sptr fftManualShiftNoOffset =
+        fft->getProperty("OutputWorkspace");
+    fft->setProperty("InputWorkspace", offsetWS);
+    fft->setProperty("Shift", offsetShift);
+    fft->execute();
+    MatrixWorkspace_sptr fftManualShiftWithOffset =
+        fft->getProperty("OutputWorkspace");
+
+    // No shift - should match
+    TS_ASSERT(Mantid::API::equals(fftNoShiftNoOffset, fftNoShiftWithOffset,
+                                  tolerance));
+    // Shift - should have a phase difference (correct)
+    TS_ASSERT(!Mantid::API::equals(fftAutoShiftNoOffset, fftAutoShiftWithOffset,
+                                   tolerance));
+    TS_ASSERT(!Mantid::API::equals(fftManualShiftNoOffset,
+                                   fftManualShiftWithOffset, tolerance));
+    // Manual shift by -X[N/2] should do the same as auto shift
+    TS_ASSERT(Mantid::API::equals(fftAutoShiftNoOffset, fftManualShiftNoOffset,
+                                  tolerance));
+    TS_ASSERT(Mantid::API::equals(fftAutoShiftWithOffset,
+                                  fftManualShiftWithOffset, tolerance));
+  }
+
+  void test_complexWorkspace_phase() {
+    const auto inputWS = createComplexWorkspace(100, 10);
+    doPhaseTest(inputWS, true);
+  }
+
+  /**
+   * This test suggested by instrument scientists
+   * The transform of a symmetric pulse centered at t=0 should have phase=0 (or
+   * pi) for all frequency components
+   */
+  void test_GaussianBurst_phase() {
+    // These Gaussian bursts are not the same
+    const auto inputWSOne = createPulseWS(1000, 41.76, 0.0, -2.32, 2.51);
+    const auto inputWSTwo = createPulseWS(1000, 41.76, 0.0, -3.123, 2.51);
+    TS_ASSERT(!Mantid::API::equals(inputWSOne, inputWSTwo, tolerance));
+
+    // but their transforms should be the same
+    const auto fftOne = doFFT(inputWSOne, true, true);
+    const auto fftTwo = doFFT(inputWSTwo, true, true);
+    TS_ASSERT(Mantid::API::equals(fftOne, fftTwo, tolerance));
+  }
+
+  /**
+   * Test suggested by instrument scientists
+   * Generate the same function with different X point spacing
+   * Transforms should match (although with different point spacing)
+   */
+  void test_GaussianBurst_xSpacing() {
+    // Same function, different number of points
+    const auto inputWSOne = createPulseWS(1000, 41.76, 0.0, -2.32, 2.51);
+    const auto inputWSTwo = createPulseWS(500, 41.76, 0.0, -2.32, 2.51);
+    const auto fftOne = doFFT(inputWSOne, true, true);
+    const auto fftTwo = doFFT(inputWSOne, true, true);
+    // Rebin due to different point spacing
+    const auto rebinOne = doRebin(fftOne, "-20, 0.1, 20");
+    const auto rebinTwo = doRebin(fftTwo, "-20, 0.1, 20");
+    TS_ASSERT(Mantid::API::equals(rebinOne, rebinTwo, tolerance));
+  }
+
 private:
+  MatrixWorkspace_sptr doRebin(MatrixWorkspace_sptr inputWS,
+                               const std::string &params) {
+    auto rebin = FrameworkManager::Instance().createAlgorithm("Rebin");
+    rebin->initialize();
+    rebin->setChild(true);
+    rebin->setProperty("InputWorkspace", inputWS);
+    rebin->setPropertyValue("OutputWorkspace", "__NotUsed");
+    rebin->setPropertyValue("Params", params);
+    rebin->execute();
+    return rebin->getProperty("OutputWorkspace");
+  }
+
+  MatrixWorkspace_sptr doFFT(MatrixWorkspace_sptr inputWS, const bool complex,
+                             const bool phaseShift) {
+    auto fft = FrameworkManager::Instance().createAlgorithm("FFT");
+    fft->initialize();
+    fft->setChild(true);
+    fft->setProperty("InputWorkspace", inputWS);
+    fft->setPropertyValue("OutputWorkspace", "__NotUsed");
+    fft->setPropertyValue("Real", "0");
+    if (complex) {
+      fft->setPropertyValue("Imaginary", "1");
+    }
+    if (phaseShift) {
+      fft->setProperty("AutoShift", true);
+    }
+    fft->execute();
+    return fft->getProperty("OutputWorkspace");
+  }
+
+  void doPhaseTest(MatrixWorkspace_sptr inputWS, bool complex) {
+    // Offset the input workspace
+    const auto offsetWS = offsetWorkspace(inputWS);
+
+    // perform transforms
+    auto fftNoShiftNoOffset = doFFT(inputWS, complex, false);
+    auto fftNoShiftWithOffset = doFFT(offsetWS, complex, false);
+    auto fftAutoShiftNoOffset = doFFT(inputWS, complex, true);
+    auto fftAutoShiftWithOffset = doFFT(offsetWS, complex, true);
+
+    // No shift - should match
+    TS_ASSERT(Mantid::API::equals(fftNoShiftNoOffset, fftNoShiftWithOffset,
+                                  tolerance));
+    // Shift - should have a phase difference (correct)
+    TS_ASSERT(!Mantid::API::equals(fftAutoShiftNoOffset, fftAutoShiftWithOffset,
+                                   tolerance));
+  }
+
   MatrixWorkspace_sptr createWS(int n, int dn) {
     Mantid::DataObjects::Workspace2D_sptr ws =
         boost::dynamic_pointer_cast<Mantid::DataObjects::Workspace2D>(
@@ -596,9 +731,95 @@ private:
     return ws;
   }
 
+  MatrixWorkspace_sptr offsetWorkspace(MatrixWorkspace_sptr workspace) {
+    auto scaleX = FrameworkManager::Instance().createAlgorithm("ScaleX");
+    scaleX->initialize();
+    scaleX->setChild(true);
+    scaleX->setProperty("InputWorkspace", workspace);
+    scaleX->setPropertyValue("OutputWorkspace", "__NotUsed");
+    scaleX->setProperty("Factor", "1"); // 1 us offset is 62.5 bins
+    scaleX->setProperty("Operation", "Add");
+    scaleX->execute();
+    return scaleX->getProperty("OutputWorkspace");
+  }
+
+  MatrixWorkspace_sptr createComplexWorkspace(const size_t n,
+                                              const double omega) {
+    Mantid::MantidVec X, Y, E;
+    X.reserve(2 * n);
+    Y.reserve(2 * n);
+    E.reserve(2 * n);
+    // real spectrum
+    for (size_t i = 0; i < n; i++) {
+      double x = 2.0 * M_PI * double(i) / double(n);
+      X.push_back(x);
+      Y.push_back(cos(omega * x));
+      E.push_back(0.1);
+    }
+    // imaginary spectrum
+    for (size_t i = n; i < 2 * n; i++) {
+      double j = (double)(i - n);
+      double x = 2.0 * M_PI * j / double(n);
+      X.push_back(x);
+      Y.push_back(sin(omega * x));
+      E.push_back(0.1);
+    }
+    auto create =
+        FrameworkManager::Instance().createAlgorithm("CreateWorkspace");
+    create->initialize();
+    create->setChild(true);
+    create->setProperty("DataX", X);
+    create->setProperty("DataY", Y);
+    create->setProperty("DataE", E);
+    create->setProperty("NSpec", 2);
+    create->setPropertyValue("OutputWorkspace", "__NotUsed");
+    create->execute();
+    return create->getProperty("OutputWorkspace");
+  }
+
+  MatrixWorkspace_sptr createPulseWS(const size_t n, const double omega,
+                                     const double x0, const double factor,
+                                     const double sigma) {
+    Mantid::MantidVec X, Y, E;
+    X.reserve(2 * n);
+    Y.reserve(2 * n);
+    E.reserve(2 * n);
+    // real spectrum
+    for (size_t i = 0; i < n; i++) {
+      double x = (2.0 * M_PI * double(i) / double(n)) + factor;
+      X.push_back(x);
+      double y = cos(omega * x);
+      y *= exp(-1.0 * (pow((x - x0) * sigma, 2.0)));
+      Y.push_back(y);
+      E.push_back(0.1);
+    }
+    // imaginary spectrum
+    for (size_t i = n; i < 2 * n; i++) {
+      double j = (double)(i - n);
+      double x = (2.0 * M_PI * j / double(n)) + factor;
+      X.push_back(x);
+      double y = sin(omega * x);
+      y *= exp(-1.0 * (pow((x - x0) * sigma, 2.0)));
+      Y.push_back(y);
+      E.push_back(0.1);
+    }
+    auto create =
+        FrameworkManager::Instance().createAlgorithm("CreateWorkspace");
+    create->initialize();
+    create->setChild(true);
+    create->setProperty("DataX", X);
+    create->setProperty("DataY", Y);
+    create->setProperty("DataE", E);
+    create->setProperty("NSpec", 2);
+    create->setPropertyValue("OutputWorkspace", "__NotUsed");
+    create->execute();
+    return create->getProperty("OutputWorkspace");
+  }
+
   const double dX;
   const double h;
   const double a;
+  const double tolerance;
 };
 
 #endif /*FFT_TEST_H_*/
