4d20914f0bd089e916ee2044e46a995029505df7
Pete Peterson <petersonpf@ornl.gov>
2016-04-04 15:52:55 -0400

    Adding new mode to SetUncertainties

--- Framework/Algorithms/src/SetUncertainties.cpp
+++ Framework/Algorithms/src/SetUncertainties.cpp
@@ -6,6 +6,7 @@
 #include "MantidAPI/WorkspaceFactory.h"
 #include "MantidKernel/ListValidator.h"
 
+#include <algorithm>
 #include <vector>
 
 namespace Mantid {
@@ -20,6 +21,27 @@ using namespace API;
 namespace {
 /// Used to compare signal to zero
 const double TOLERANCE = 1.e-10;
+
+struct oneifzeroerror {
+  double operator()(const double error) {
+    if (error < TOLERANCE) {
+      return 1.;
+    } else {
+      return error;
+    }
+  }
+};
+
+struct sqrterror {
+  double operator()(const double intensity) {
+    const double localIntensity = fabs(intensity);
+    if (localIntensity > TOLERANCE) {
+      return sqrt(localIntensity);
+    } else {
+      return 0.;
+    }
+  }
+};
 }
 
 /// (Empty) Constructor
@@ -36,15 +58,14 @@ int SetUncertainties::version() const { return (1); }
 
 const std::string ZERO("zero");
 const std::string SQRT("sqrt");
+const std::string ONEIFZERO("oneifzero");
 
 void SetUncertainties::init() {
   declareProperty(make_unique<WorkspaceProperty<API::MatrixWorkspace>>(
       "InputWorkspace", "", Direction::Input));
   declareProperty(make_unique<WorkspaceProperty<API::MatrixWorkspace>>(
       "OutputWorkspace", "", Direction::Output));
-  std::vector<std::string> errorTypes;
-  errorTypes.push_back(ZERO);
-  errorTypes.push_back(SQRT);
+  std::vector<std::string> errorTypes = {ZERO, SQRT, ONEIFZERO};
   declareProperty("SetError", ZERO,
                   boost::make_shared<StringListValidator>(errorTypes),
                   "How to reset the uncertainties");
@@ -54,6 +75,7 @@ void SetUncertainties::exec() {
   MatrixWorkspace_const_sptr inputWorkspace = getProperty("InputWorkspace");
   std::string errorType = getProperty("SetError");
   bool zeroError = (errorType.compare(ZERO) == 0);
+  bool oneIfZeroError = (errorType.compare(ONEIFZERO) == 0);
 
   // Create the output workspace. This will copy many aspects from the input
   // one.
@@ -68,19 +90,24 @@ void SetUncertainties::exec() {
   for (int64_t i = 0; i < int64_t(numHists); ++i) {
     PARALLEL_START_INTERUPT_REGION
 
+    // copy the X/Y
     outputWorkspace->setX(i, inputWorkspace->refX(i));
     outputWorkspace->dataY(i) = inputWorkspace->readY(i);
-    outputWorkspace->dataE(i) =
-        std::vector<double>(inputWorkspace->readE(i).size(), 0.);
+    // copy the E or set to zero depending on the mode
+    if (oneIfZeroError) {
+      outputWorkspace->dataE(i) = inputWorkspace->readE(i);
+    } else {
+      outputWorkspace->dataE(i) =
+          std::vector<double>(inputWorkspace->readE(i).size(), 0.);
+    }
 
     if (!zeroError) {
-      MantidVec &Y = outputWorkspace->dataY(i);
       MantidVec &E = outputWorkspace->dataE(i);
-      std::size_t numE = E.size();
-      for (std::size_t j = 0; j < numE; j++) {
-        const double y_val = fabs(Y[j]);
-        if (y_val > TOLERANCE)
-          E[j] = sqrt(y_val);
+      if (oneIfZeroError) {
+        std::for_each(E.begin(), E.end(), oneifzeroerror());
+      } else {
+        const MantidVec &Y = outputWorkspace->readY(i);
+        std::transform(Y.begin(), Y.end(), E.begin(), sqrterror());
       }
     }
 
--- docs/source/algorithms/SetUncertainties-v1.rst
+++ docs/source/algorithms/SetUncertainties-v1.rst
@@ -12,7 +12,8 @@ Description
 The uncertainties for the entire workspace will be recalculated according to the ``SetError`` property.
 
 - ``SetError="zero"`` will change all of the uncertainties to zero
-- ``SetError"sqrt"`` will recalculate all of the uncertainties to be the square root of the y value
+- ``SetError="sqrt"`` will recalculate all of the uncertainties to be the square root of the y value
+- ``SetError="oneifzero"`` will change the uncertainties to one if they are currently zero
 
 .. categories::
 
