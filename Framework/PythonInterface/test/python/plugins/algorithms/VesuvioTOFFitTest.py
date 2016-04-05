4a7bdabf8f9888e8996ffff4abfc040dad1e2bdd
Steven Hahn <hahnse@ornl.gov>
2016-04-05 16:31:18 -0400

    Increase tolerances.

--- Framework/PythonInterface/test/python/plugins/algorithms/VesuvioTOFFitTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/VesuvioTOFFitTest.py
@@ -60,12 +60,7 @@ class VesuvioTOFFitTest(unittest.TestCase):
         self.assertAlmostEqual(0.0155041, output_ws.readY(0)[0])
         self.assertAlmostEqual(-0.0070975, output_ws.readY(0)[-1])
         self.assertAlmostEqual(0.8693019e-05, output_ws.readY(1)[0])
-        if platform.system() == "Darwin":
-            self.assertAlmostEqual(0.7463842e-04, output_ws.readY(1)[-1])
-        else:
-            #self.assertAlmostEqual(0.7458504e-04, output_ws.readY(1)[-1])
-            self.assertAlmostEqual(0.74533284876806836e-04,output_ws.readY(1)[-1])
-
+        self.assertAlmostEqual(0.746e-04,output_ws.readY(1)[-1],delta=0.2e-06)
 
     def test_single_run_produces_correct_output_workspace_index0_kfixed_including_background(self):
         profiles = "function=GramCharlier,width=[2, 5, 7],hermite_coeffs=[1, 0, 0],k_free=0,sears_flag=1;"\
@@ -88,12 +83,8 @@ class VesuvioTOFFitTest(unittest.TestCase):
         self.assertAlmostEqual(0.0279822, output_ws.readY(0)[0])
         self.assertAlmostEqual(0.0063585, output_ws.readY(0)[-1])
         # Small difference in fitting between windows and other platforms
-        if platform.system() == "Windows":
-            #self.assertAlmostEqual(-0.010972, output_ws.readY(1)[0])
-            self.assertAlmostEqual(-0.012730248871069308, output_ws.readY(1)[0])
-        else:
-            self.assertAlmostEqual(-0.0119362, output_ws.readY(1)[0])
-            self.assertAlmostEqual(0.00554965, output_ws.readY(1)[-1])
+        self.assertAlmostEqual(-0.012, output_ws.readY(1)[0],delta=0.002)
+        self.assertAlmostEqual(0.00554965, output_ws.readY(1)[-1])
 
     # -------------- Failure cases ------------------
 
