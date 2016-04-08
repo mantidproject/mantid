53bd85d17b646cf299cbd224b18c8df135c63bd4
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-04-08 08:40:37 +0100

    Write Python wrapper function for quickConversion
    Refs #15751

--- Framework/PythonInterface/mantid/kernel/src/Exports/Unit.cpp
+++ Framework/PythonInterface/mantid/kernel/src/Exports/Unit.cpp
@@ -1,6 +1,7 @@
 #include "MantidKernel/Unit.h"
 
 #include <boost/python/class.hpp>
+#include <boost/python/tuple.hpp>
 #include <boost/python/register_ptr_to_python.hpp>
 
 using Mantid::Kernel::Unit;
@@ -27,16 +28,23 @@ const std::string deprecatedLabel(Unit &self) {
              "'unit.label()' is deprecated, use 'str(unit.symbol())' instead.");
   return self.label().ascii();
 }
+
+template <class T>
+tuple quickConversionWrapper(Unit &self, const T &destUnitName) {
+  double wavelengthFactor = 0;
+  double wavelengthPower = 0;
+  bool converted =
+      self.quickConversion(destUnitName, wavelengthFactor, wavelengthPower);
+  if (!converted) {
+    throw std::runtime_error("Quick conversion is not possible for " +
+                             std::string(self.unitID()) +
+                             " to the desired unit.");
+  }
+  return boost::python::make_tuple<double>(wavelengthFactor, wavelengthPower);
+}
 }
 
 void export_Unit() {
-  // Function pointer typedef
-  // Unit version
-  typedef bool (Unit::*UnitVersion)(const Unit &destination, double &factor,
-                                    double &power) const;
-  // String version
-  typedef bool (Unit::*StringVersion)(std::string destUnitName, double &factor,
-                                      double &power) const;
 
   register_ptr_to_python<boost::shared_ptr<Unit>>();
 
@@ -55,12 +63,12 @@ void export_Unit() {
       .def("unitID", &Unit::unitID, arg("self"),
            "Returns the string ID of the unit. This may/may not match "
            "its name")
-      .def("quickConversion", (StringVersion)&Unit::quickConversion,
-           (arg("destUnitName"), arg("factor"), arg("power")),
+      .def("quickConversion", &quickConversionWrapper<Unit>,
+           (arg("self"), arg("destUnitName")),
            "Check whether the unit can be converted to another via a "
            "simple factor")
-      .def("quickConversion", (UnitVersion)&Unit::quickConversion,
-           (arg("destination"), arg("factor"), arg("power")),
+      .def("quickConversion", &quickConversionWrapper<std::string>,
+           (arg("self"), arg("destination")),
            "Check whether the unit can be converted to another via a "
            "simple factor");
 }
