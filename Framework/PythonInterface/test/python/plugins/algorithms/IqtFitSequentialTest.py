8332296fc3c726e7edb9a269ad43a7140ff92e99
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-04-08 11:02:31 +0100

    Add Workspace shape/structure tests
    Refs #15440

--- Framework/PythonInterface/test/python/plugins/algorithms/IqtFitSequentialTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/IqtFitSequentialTest.py
@@ -5,23 +5,78 @@ from mantid.api import MatrixWorkspace, WorkspaceGroup, ITableWorkspace
 
 class IqtFitSequentialTest(unittest.TestCase):
 
+    _iqt_ws = None
 
     def setUp(self):
+        self._iqt_ws = Load(Filename='iris26176_graphite002_iqt.nxs',
+                            OutputWorkspace='iris26176_graphite002_iqt')
 
 
 #-----------------------------------Validation of result-------------------------------------
 
     def _validate_output(self, params, result, fit_group):
+        self.assertTrue(isinstance(params, ITableWorkspace))
+        self.assertTrue(isinstance(result, MatrixWorkspace))
+        self.assertTrue(isinstance(fit_group, WorkspaceGroup))
 
+        self._validate_table_shape(params)
+        self._validate_matrix_shape(result)
+        self._validate_group_shape(fit_group)
+
+        self._validate_table_values(params)
+        self._validate_matrix_values(result)
+        self._validate_group_values(fit_group)
 
 
     def _validate_table_shape(self, tableWS):
+        # Check length of rows and columns
+        rows = tableWS.rowCount()
+        columns = tableWS.columnCount()
+        self.assertEquals(rows, 17)
+        self.assertEquals(columns, 10)
 
+        # Check some column names
+        column_names = tableWS.getColumnNames()
+        self.assertEquals('axis-1', column_names[0])
+        self.assertEquals('f0.A0', column_names[1])
+        self.assertEquals('f0.A0_Err', column_names[2])
 
     def _validate_matrix_shape(self, matrixWS):
+        # Check no. bins and no. hists
+        nbins = matrixWS.blocksize()
+        nhists = matrixWS.getNumberHistograms()
+        self.assertEquals(nbins, 17)
+        self.assertEquals(nhists, 3)
 
+        # Check histogram names
+        text_axis = matrixWS.getAxis(1)
+        self.assertTrue(text_axis.isText())
+        self.assertEquals('f0.A0',text_axis.label(0))
+        self.assertEquals('f1.Intensity',text_axis.label(1))
+        self.assertEquals('f1.Tau',text_axis.label(2))
+        
+        # Check bin units
+        self.assertEquals('MomentumTransfer', matrixWS.getAxis(0).getUnit().unitID())
 
     def _validate_group_shape(self, groupWS):
+        # Check number of workspaces and size
+        nitems = groupWS.getNumberOfEntries()
+        self.assertEquals(nitems, 17)
+        sub_ws = groupWS.getItem(0)
+        nbins = sub_ws.blocksize()
+        nhists = sub_ws.getNumberHistograms()
+        self.assertEquals(nbins, 58)
+        self.assertEquals(nhists, 3)
+        
+        # Check histogram names
+        text_axis = sub_ws.getAxis(1)
+        self.assertTrue(text_axis.isText())
+        self.assertEquals('Data',text_axis.label(0))
+        self.assertEquals('Calc',text_axis.label(1))
+        self.assertEquals('Diff',text_axis.label(2))
+        
+        # Check bin units
+        self.assertEquals('ns', str(sub_ws.getAxis(0).getUnit().symbol()))
 
 
     def _validate_table_values(self, tableWS):
