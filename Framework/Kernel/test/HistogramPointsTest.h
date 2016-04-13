29ab90121873d5ca4156d1b93d78bf53c932593c
Simon Heybrock <simon.heybrock@esss.se>
2016-04-13 10:05:32 +0200

    Re #16086. Added more tests for BinEdges and Points.

--- Framework/Kernel/test/HistogramPointsTest.h
+++ Framework/Kernel/test/HistogramPointsTest.h
@@ -19,13 +19,25 @@ public:
   static void destroySuite(HistogramPointsTest *suite) { delete suite; }
 
   void test_construct_default() {
-    Points points;
+    const Points points{};
     TS_ASSERT(!points);
   }
 
+  void test_construct_from_null_BinEdges() {
+    const BinEdges edges{};
+    const Points points(edges);
+    TS_ASSERT(!points);
+  }
+
+  void test_construct_from_empty_BinEdges() {
+    const BinEdges edges(0);
+    const Points points(edges);
+    TS_ASSERT_EQUALS(points.size(), 0);
+  }
+
   void test_construct_from_BinEdges() {
-    BinEdges edges = {1.0, 3.0, 7.0, 15.0};
-    Points points(edges);
+    const BinEdges edges{1.0, 3.0, 7.0, 15.0};
+    const Points points(edges);
     TS_ASSERT_EQUALS(points.size(), 3);
     TS_ASSERT_DELTA(points[0], 2.0, 1e-14);
     TS_ASSERT_DELTA(points[1], 5.0, 1e-14);
