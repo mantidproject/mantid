4dfe8dccf700d32a83c75ee52c38ecb8f7c8897c
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-04-13 09:38:29 +0100

    Apply clang format patch
    Refs #15888

--- Framework/DataHandling/src/SaveAscii2.cpp
+++ Framework/DataHandling/src/SaveAscii2.cpp
@@ -71,9 +71,12 @@ void SaveAscii2::init() {
                   "Character(s) to put in front of comment lines.");
 
   // For the ListValidator
-  std::string spacers[6][2] = {
-      {"CSV", ","},   {"Tab", "\t"},      {"Space", " "},
-      {"Colon", ":"}, {"SemiColon", ";"}, {"UserDefined", "UserDefined"}};
+  std::string spacers[6][2] = {{"CSV", ","},
+                               {"Tab", "\t"},
+                               {"Space", " "},
+                               {"Colon", ":"},
+                               {"SemiColon", ";"},
+                               {"UserDefined", "UserDefined"}};
   std::vector<std::string> sepOptions;
   for (auto &spacer : spacers) {
     std::string option = spacer[0];
