577615be413a35abfe08b6d71cc5899f2612ed06
Simon Heybrock <simon.heybrock@esss.se>
2016-04-13 09:31:02 +0200

    Re #16086. Added more tests for Histogram/VectorOf.

--- Framework/Kernel/test/HistogramVectorOfTest.h
+++ Framework/Kernel/test/HistogramVectorOfTest.h
@@ -3,6 +3,7 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidKernel/make_cow.h"
 #include "MantidKernel/Histogram/VectorOf.h"
 #include "MantidKernel/Histogram/ConstIterable.h"
 
@@ -43,6 +44,11 @@ public:
     TS_ASSERT_EQUALS(values[1], 0.1);
   }
 
+  void test_length_zero_value_constructor() {
+    const VectorOfTester values(0, 0.1);
+    TS_ASSERT_EQUALS(values.size(), 0);
+  }
+
   void test_count_constructor() {
     const VectorOfTester values(2);
     TS_ASSERT_EQUALS(values.size(), 2);
@@ -58,11 +64,25 @@ public:
     TS_ASSERT_EQUALS(values[2], 0.3);
   }
 
+  void test_empty_initializer_list_constructor() {
+    std::initializer_list<double> empty_list;
+    const VectorOfTester values(empty_list);
+    // Note difference to default constructor: No elements, but allocated.
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(values.size(), 0);
+  }
+
   void test_copy_constructor() {
     const VectorOfTester src(2, 0.1);
     const VectorOfTester dest(src);
     TS_ASSERT_EQUALS(dest[0], 0.1);
     TS_ASSERT_EQUALS(dest[1], 0.1);
+  }
+
+  void test_copy_from_null_constructor() {
+    const VectorOfTester src{};
+    const VectorOfTester dest(src);
+    TS_ASSERT(!dest);
   }
 
   void test_move_constructor() {
@@ -75,6 +95,13 @@ public:
     TS_ASSERT_EQUALS(dest[1], 0.1);
   }
 
+  void test_move_from_null_constructor() {
+    VectorOfTester src{};
+    const VectorOfTester dest(std::move(src));
+    TS_ASSERT(!src);
+    TS_ASSERT(!dest);
+  }
+
   void test_copy_assignment() {
     const VectorOfTester src(2, 0.1);
     VectorOfTester dest(1);
@@ -84,6 +111,13 @@ public:
     TS_ASSERT_EQUALS(dest.size(), 2);
     TS_ASSERT_EQUALS(dest[0], 0.1);
     TS_ASSERT_EQUALS(dest[1], 0.1);
+  }
+
+  void test_copy_assignment_from_null() {
+    const VectorOfTester src{};
+    VectorOfTester dest(1);
+    dest = src;
+    TS_ASSERT(!dest);
   }
 
   void test_move_assignment() {
@@ -98,6 +132,14 @@ public:
     TS_ASSERT_EQUALS(dest[1], 0.1);
   }
 
+  void test_move_assignment_from_null() {
+    VectorOfTester src{};
+    VectorOfTester dest(1);
+    dest = std::move(src);
+    TS_ASSERT(!src);
+    TS_ASSERT(!dest);
+  }
+
   void test_initializer_list_assignment() {
     VectorOfTester values(2, 0.1);
     TS_ASSERT_EQUALS(values.size(), 2);
@@ -110,17 +152,142 @@ public:
     TS_ASSERT_EQUALS(values[2], 0.3);
   }
 
-  void test_vector_assignment() {
-    std::vector<double> raw{0.1, 0.2, 0.3};
-    VectorOfTester values;
+  void test_empty_initializer_list_assignment() {
+    std::initializer_list<double> empty_list;
+    VectorOfTester values(1);
+    values = empty_list;
+    TS_ASSERT_EQUALS(values.size(), 0);
+  }
+
+  void test_cow_ptr_constructor() {
+    auto cow = make_cow<std::vector<double>>(2, 0.1);
+    VectorOfTester values(cow);
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+    TS_ASSERT_EQUALS(&values.constData(), cow.get());
+  }
+
+  void test_null_cow_ptr_constructor() {
+    cow_ptr<std::vector<double>> cow(nullptr);
+    VectorOfTester values(cow);
     TS_ASSERT(!values);
+  }
+
+  void test_shared_ptr_constructor() {
+    auto shared = boost::make_shared<std::vector<double>>(2, 0.1);
+    VectorOfTester values(shared);
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+    TS_ASSERT_EQUALS(&values.constData(), shared.get());
+  }
+
+  void test_null_shared_ptr_constructor() {
+    boost::shared_ptr<std::vector<double>> shared;
+    VectorOfTester values(shared);
+    TS_ASSERT(!values);
+  }
+
+  void test_vector_constructor() {
+    std::vector<double> vector(2, 0.1);
+    VectorOfTester values(vector);
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+  }
+
+  void test_cow_ptr_assignment() {
+    auto cow = make_cow<std::vector<double>>(2, 0.1);
+    VectorOfTester values(1);
+    values = cow;
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+    TS_ASSERT_EQUALS(&values.constData(), cow.get());
+  }
+
+  void test_null_cow_ptr_assignment() {
+    cow_ptr<std::vector<double>> cow(nullptr);
+    VectorOfTester values(1);
+    values = cow;
+    TS_ASSERT(!values);
+  }
+
+  void test_cow_ptr_self_assignment() {
+    VectorOfTester values(2, 0.1);
+    const auto *raw_data = &values.constData();
+    auto cow = values.cowData();
+    values = cow;
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(cow.use_count(), 2);
+    TS_ASSERT_EQUALS(&values.constData(), raw_data);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+    TS_ASSERT_EQUALS(&values.constData(), cow.get());
+  }
+
+  void test_shared_ptr_assignment() {
+    auto shared = boost::make_shared<std::vector<double>>(2, 0.1);
+    VectorOfTester values(1);
+    values = shared;
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+    TS_ASSERT_EQUALS(&values.constData(), shared.get());
+  }
+
+  void test_null_shared_ptr_assignment() {
+    boost::shared_ptr<std::vector<double>> shared;
+    VectorOfTester values(1);
+    values = shared;
+    TS_ASSERT(!values);
+  }
+
+  void test_shared_ptr_self_assignment() {
+    auto shared = boost::make_shared<std::vector<double>>(2, 0.1);
+    VectorOfTester values(1);
+    values = shared;
+    TS_ASSERT_THROWS_NOTHING(values = shared);
+    TS_ASSERT(values);
+    TS_ASSERT_EQUALS(shared.use_count(), 2);
+    TS_ASSERT_EQUALS(values.size(), 2);
+    TS_ASSERT_EQUALS(values[0], 0.1);
+    TS_ASSERT_EQUALS(values[1], 0.1);
+    TS_ASSERT_EQUALS(&values.constData(), shared.get());
+  }
+
+  void test_vector_assignment() {
+    const std::vector<double> raw{0.1, 0.2, 0.3};
+    VectorOfTester values;
     TS_ASSERT_THROWS_NOTHING(values = raw);
     TS_ASSERT(values);
     TS_ASSERT_DIFFERS(&(values.constData()), &raw);
-    TS_ASSERT_EQUALS(raw.size(), 3);
-    TS_ASSERT_EQUALS(raw[0], 0.1);
     TS_ASSERT_EQUALS(values.size(), 3);
     TS_ASSERT_EQUALS(values[0], 0.1);
+  }
+
+  void test_vector_self_assignment() {
+    VectorOfTester values(2, 0.1);
+    // Reference to internal data
+    auto &vector = values.constData();
+    values = vector;
+    TS_ASSERT(values);
+    // Reference still valid after self assignment
+    TS_ASSERT_EQUALS(&(values.constData()), &vector);
+  }
+
+  void test_operator_bool() {
+    VectorOfTester null;
+    TS_ASSERT(!null);
+    VectorOfTester not_null(0);
+    TS_ASSERT(not_null);
   }
 
   void test_size() {
@@ -128,12 +295,42 @@ public:
     TS_ASSERT_EQUALS(values.size(), 42);
   }
 
-  void test_data() {
+  void test_data_const() {
     const VectorOfTester values(2, 0.1);
+    auto copy(values);
+    TS_ASSERT_EQUALS(&copy.constData(), &values.constData());
     const auto &data = values.data();
+    TS_ASSERT_EQUALS(&copy.constData(), &values.constData());
     TS_ASSERT_EQUALS(data.size(), 2);
-    TS_ASSERT_EQUALS(data[0], 0.1);
-    TS_ASSERT_EQUALS(data[1], 0.1);
+  }
+
+  void test_const_data() {
+    VectorOfTester values(2, 0.1);
+    auto copy(values);
+    TS_ASSERT_EQUALS(&copy.constData(), &values.constData());
+    const auto &data = values.constData();
+    TS_ASSERT_EQUALS(&copy.constData(), &values.constData());
+    TS_ASSERT_EQUALS(data.size(), 2);
+  }
+
+  void test_data() {
+    VectorOfTester values(2, 0.1);
+    auto copy(values);
+    TS_ASSERT_EQUALS(&copy.constData(), &values.constData());
+    auto &data = values.data();
+    TS_ASSERT_DIFFERS(&copy.constData(), &values.constData());
+    TS_ASSERT_EQUALS(data.size(), 2);
+  }
+
+  void test_cow_data() {
+    VectorOfTester values(2, 0.1);
+    auto cow = values.cowData();
+    TS_ASSERT_EQUALS(cow.get(), &values.constData());
+    TS_ASSERT_EQUALS(cow.use_count(), 2);
+    auto &data = values.data();
+    TS_ASSERT_DIFFERS(cow.get(), &values.constData());
+    TS_ASSERT_EQUALS(cow.use_count(), 1);
+    TS_ASSERT_EQUALS(data.size(), 2);
   }
 };
 
