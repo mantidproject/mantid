6b377a8080063d8feda3abbb18acfbe604ddf422
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2016-04-13 13:42:03 +0100

    new optional path property, more imports, refactoring, re #15771

--- Framework/PythonInterface/plugins/algorithms/GSASIIFitPeaks.py
+++ Framework/PythonInterface/plugins/algorithms/GSASIIFitPeaks.py
@@ -33,6 +33,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
         PROP_WORKSPACE_INDEX = 'WorkspaceIndex'
         PROP_INSTR_FILE = 'InstrumentFile'
         PROP_PHASE_INFO_FILE = 'PhaseInfoFile'
+        PROP_PATH_TO_GSASII = 'PathToGSASII'
         PROP_BACKGROUND_TYPE = 'BackgroundType'
         PROP_EXPECTED_PEAKS = "ExpectedPeaks"
         PROP_EXPECTED_PEAKS_FROM_FILE = "ExpectedPeaksFromFile"
@@ -47,7 +48,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
         PROP_REFINE_SIGMA = 'RefineSigma'
         PROP_REFINE_GAMMA = 'RefineGamma'
 
-        self.declareProperty(MatrixWorkspaceProperty(PROP_INPUT_WORKSPACE, "",
+        self.declareProperty(MatrixWorkspaceProperty(PROP_INPUT_WORKSPACE, '',
                                                      optional = PropertyMode.Mandatory,
                                                      direction = Direction.Input),
                              doc = 'Workspace with spectra to fit peaks. ToF is expected X unit.')
@@ -57,7 +58,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
                              'the first spectrum will be processed (that is, the only spectrum '
                              'for focussed data workspaces.', direction = Direction.Input)
 
-        self.declareProperty(FileProperty(name = PROP_INSTR_FILE, defaultValue = "",
+        self.declareProperty(FileProperty(name = PROP_INSTR_FILE, defaultValue = '',
                                           action = FileAction.Load,
                                           extensions = [".par", ".prm", ".ipar", ".iparm"]),
                              doc = 'File with instrument parameters (in GSAS format).')
@@ -68,9 +69,13 @@ class GSASIIFitPeaks(PythonAlgorithm):
                              doc = 'Type of background for the peak fitting.')
 
         # Phase information: TODO
-        self.declareProperty(FileProperty(name = PROP_PHASE_INFO_FILE, defaultValue = "",
+        self.declareProperty(FileProperty(name = PROP_PHASE_INFO_FILE, defaultValue = '',
                                           action = FileAction.OptionalLoad, extensions = [".cif"]),
                              doc = 'File with phase information for the material.')
+
+        self.declareProperty(FileProperty(name = PROP_PATH_TO_GSASII, defaultValue = '',
+                                          action = FileAction.OptionalDirectory),
+                             doc = 'Optional path to GSAS-II software installation.')
 
         GRP_PEAKS = "Expected peaks (leave empty to auto-find)"
 
@@ -102,7 +107,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
         self.declareProperty(ITableWorkspaceProperty(PROP_OUT_FITTED_PARAMS, "", Direction.Output),
                              doc = "Fitted parameters. One row per peak found.")
 
-        self.declareProperty(FileProperty(name = PROP_OUT_PROJECT_FILE, defaultValue = "",
+        self.declareProperty(FileProperty(name = PROP_OUT_PROJECT_FILE, defaultValue = '',
                                           direction = Direction.Input,
                                           action = FileAction.OptionalSave, extensions = [".gpx"]),
                              doc = 'GSAS-II project file (that can be openened in the GSAS-II GUI).')
@@ -148,83 +153,67 @@ class GSASIIFitPeaks(PythonAlgorithm):
         pass
 
     def PyExec(self):
-        path_gsas2 = self.getPropertyValue("PathToGSASII")
+        prog = Progress(self, start=0, end=1, nreports=5)
 
-        try:
-            import GSASIIpwd
-            import GSASII
-        except ImportError as ierr:
-            raise ImportError("Failed to import the GSASII and GSASIIpwd modules "
-                              "from GSAS-II. Please check that it is available in the "
-                              "Python path and/or the path to GSAS-II given in the "
-                              "input properties: " + path_gsas2 + ". More error "
-                              "details: " +ierr)
-
-        # Split this into 3/4 calls. TODO
-
-        # Should do much better - TODO
-        prog = Progress(self, start=0, end=1, nreports=3)
+        prog.report('Importing GSAS-II ')
+        self._import_gsas2(PROP_PATH_TO_GSASII)
 
         prog.report('Initializing GSAS-II ')
         gs2 = self._init_gs2()
 
-        in_wks = self.getProperty(PROP_INPUT_WORKSPACE).value
-        in_idx = self.getProperty(PROP_WORKSPACE_INDEX)
 
         prog.report('Loading and preparing input data')
-        focused_wks = sapi.ExtractSpectra(InputWorkspace = in_wks, StartWorkspaceIndex = in_idx,
-                                          EndworkspaceIndex = in_idx)
-
-        # produce histo_file
-        histo_data_file = 'tmp.xye'
-        sapi.SaveFocusedXYE(InputWorkspace=focused_wks, Filename=histo_data_file)
-
-        gs2_rd = self.get_histo_data_reader(gs2, histo_file)
-
-        gs2_rd.powderdata = _transform_to_centers_bins(gs2_rd.powderdata)
-
-        if not isinstance(gs2_rd.powderdata, list):
-            raise ValueError('rd.powderdata is not a list of array as expected')
-
+        in_wks = self.getProperty(PROP_INPUT_WORKSPACE).value
+        in_idx = self.getProperty(PROP_WORKSPACE_INDEX)
         inst_file = self.getProperty(PROP_INSTR_FILE)
-        if 'Instrument Parameters' not in gs2_rd.pwdparms:
-            gs2.zipfile = None # init required before GetPowderIparm
-            # Trick: if you pass lastIparmfile (being sure that it exits) it will be
-            # picked up without having to ask via a pop-up dialog
-            # An alternative is to set 'gs2_rd.instparm = inst_file' but that assumes both
-            # the data and instrument files are in the same directory
-            inst_parm1, inst_parm2 = gs2.GetPowderIparm(rd=gs2_rd, prevIparm=None,
-                                                        lastIparmfile=inst_file, lastdatafile='')
-            if not inst_parm1: # or not inst_parm2:  # (note inst_parm2 is commonly an empty dict)
-                raise RuntimeError('Failed to import the instrument parameter structure')
 
-        gs2_rd.pwdparms = (inst_parm1, inst_parm2)
+        if in_wks.getNumberHistograms() > 1 or 0 != in_idx:
+            gs2_focused_wks = sapi.ExtractSpectra(InputWorkspace = in_wks, StartWorkspaceIndex = in_idx,
+                                                  EndworkspaceIndex = in_idx)
+        else:
+            gs2_focused_wks = in_wks
 
-        self.log().information("Parameters from instrument file: {0}".format(gs2_rd.pwdparms))
+        (peaks_init, background_def, gs2_rd) = self._load_prepare_data_for_fit(gs2_focused_wks, inst_file)
 
-        # Note: blatantly ignores self.getProperty(PROP_PHASE_INFO_FILE)
-        background_def = [['chebyschev', True, 3, 1.0, 0.0, 0.0],
-                          {'peaksList': [], 'debyeTerms': [], 'nPeaks': 0, 'nDebye': 0}]
-
-        self.log().information("Using background function: {0}".format(background_def))
-
-        peaks_init = self.init_peaks_list()
-        self.log().information("Peaks parameters initialized as: {0}".format(peaks_init))
-
-        prog.report('Running refinement')
+        # No obvious way to provide proper progress report from inside the fitting routines
+        prog.report('Running refinement. This may take some times')
         (gof_estimates, parm_dict) = self._run_peak_fit(peaks_init, background_def, gs2_rd.powderdata)
 
-        proj_file = self.getProperty(PROP_OUT_PROJECT_FILE)
+        prog.report('Producing outputs')
+        out_proj_file = self.getProperty(PROP_OUT_PROJECT_FILE)
         if proj_file:
-            # Save in gpx project format- TODO
-            pass
+            self._save_gsas2_project(gs2, out_proj_file)
+        self._produce_outputs(gof_estimates, parm_dict)
 
-        (result_rwp, result_gof) = gof_estimates
-        self.setProperty(PROP_OUT_RWP, result_rwp)
-        self.setProperty(PROP_OUT_GOF, result_gof)
+    def _load_prepare_data_for_fit(self, gs2_focused_wks, inst_file):
+        """
+        Loads the data into an "rd" object as used in the GSAS-II python modules, and
+        prepares an initia peaks list and a background function definition.
+        Importantly, this reads the rd.powderdata list with the histogram data,
+        and the rd.pwdparms tuple with instrument parameter lists
 
-        _build_output_table(parm_dict)
+        @param gs2_focused_wks :: a focused (single spectrum) workspace
+        @param inst_file :: GSAS instrument parameters file (.par / .prm / .iparm, etc.)
 
+        @returns a tuple with: 1) list of peaks, 2) background definition, 3) GSAS-II "rd"
+        object. These are ready to be passed to the peak fitting functions
+        """
+        gs2_rd = self._build_gsas2_reader_with_data(gs2_focused_wks)
+        self.log().information("Loaded histogram data in GSAS-II data object: {0}".
+                               format(gs2_rd.powderdata))
+
+        gs2_rd = self._add_instrument_info(gs2_rd, inst_file)
+        self.log().information("Parameters from instrument file: {0}".format(gs2_rd.pwdparms))
+
+        background_def = self._build_background_definition()
+        self.log().information("Using background function: {0}".format(background_def))
+
+        # Assumes peaks of type back-to-back exponential convoluted with pseudo-Voigt
+        # That is true in ENGIN-X instrument parameters file
+        peaks_init = self._init_peaks_list()
+        self.log().information("Peaks parameters initialized as: {0}".format(peaks_init))
+
+        return (peaks_init, background_def, gs2_rd)
 
     def _run_peak_fit(self, peaks_list, background_def, powderdata):
         """
@@ -234,7 +223,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
         R-factor, goodness of fit), 2) the parameters dictionary
         """
         limits = [powderdata[0].min(), powderdata[0].max()]
-        self.log().notice("Loaded histogram data with limits: {0}".format(limits))
+        self.log().notice("Fitting loaded histogram data, with limits: {0}".format(limits))
 
         # peaks: ['pos','int','alp','bet','sig','gam'] / with the refine flag each
         sig_dict, result, sig, Rvals, vary_list, parm_dict, full_vary_list, bad_vary = \
@@ -249,7 +238,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
         self.log().information("Result: : {0}".format(result))
         self.log().information("Rwp: : {0}".format(Rvals[0]))
         self.log().information("GoF: : {0}".format(Rvals[1]))
-        self.log().information("'Sig': : {0}".format(sig))
+        self.log().information("'Sig': {0}".format(sig))
         self.log().information("'Sig', values: : {0}".format(sig_dict))
         self.log().information("List of parameters fitted: : {0}".format(vary_list))
         self.log().information("Parameters fitted, values: {0}".format(parm_dict))
@@ -259,12 +248,72 @@ class GSASIIFitPeaks(PythonAlgorithm):
 
         return (Rvals, parm_dict)
 
+    def _import_gsas2(self, additional_path_prop):
+        try:
+            import GSASIIIO
+            import GSASIIpwd
+            import GSASII
+        except ImportError as ierr:
+            raise ImportError("Failed to import the GSASII and GSASIIpwd modules "
+                              "from GSAS-II. Please make sure that it is available in the "
+                              "Mantid Python path and/or the path to GSAS-II given in the "
+                              "input property " + getProperty(additional_path_prop) +
+                              ". More error details: " + ierr)
+
     def _init_gs2(self):
         _gsas2_app = GSASII.GSASIImain(0)
         gs2 = GSASII.GSASII(None)
         return gs2
 
-    def _get_histo_data_reader(self, gs2):
+    def _build_gsas2_reader_with_data(self, gs2_focused_wks):
+        """
+        Build an "rd" GSAS-II data structure with reader, and importantly the rd.powderdata
+        list with the histogram data.
+
+        @param gs2_focused_wks :: a workspace with a histogram from focused data
+
+        @return an "rd" object as used in GSAS-II, with histogram data in the 'powderdata'
+        data member
+        """
+
+        # produce histo_file, to use the reader from GSAS-II which will initialize the
+        # "rd" object
+        histo_data_filename = 'tmp_gsasii_histo_data.xye'
+        sapi.SaveFocusedXYE(InputWorkspace=gs2_focused_wks, Filename=histo_data_filename)
+
+        gs2_rd = self._get_histo_data_reader(gs2, histo_file)
+
+        gs2_rd.powderdata = self._transform_to_centers_bins(gs2_rd.powderdata)
+
+        if not isinstance(gs2_rd.powderdata, list):
+            raise ValueError('rd.powderdata is not a list of array as expected')
+
+        return gs2_rd
+
+    def _add_instrument_info(self, gs2_rd, inst_file):
+        if 'Instrument Parameters' not in gs2_rd.pwdparms:
+            gs2.zipfile = None # init required before GetPowderIparm
+            # Trick: if you pass lastIparmfile (being sure that it exits) it will be
+            # picked up without having to ask via a pop-up dialog
+            # An alternative is to set 'gs2_rd.instparm = inst_file' but that assumes both
+            # the data and instrument files are in the same directory
+            inst_parm1, inst_parm2 = gs2.GetPowderIparm(rd=gs2_rd, prevIparm=None,
+                                                        lastIparmfile=inst_file, lastdatafile='')
+            if not inst_parm1: # or not inst_parm2:  # (note inst_parm2 is commonly an empty dict)
+                raise RuntimeError('Failed to import the instrument parameter structure')
+
+        gs2_rd.pwdparms = (inst_parm1, inst_parm2)
+
+        return gs2_rd
+
+    def _build_background_definition(self):
+        # Note: blatantly ignores self.getProperty(PROP_PHASE_INFO_FILE)
+        backg_def = [['chebyschev', True, 3, 1.0, 0.0, 0.0],
+                     {'peaksList': [], 'debyeTerms': [], 'nPeaks': 0, 'nDebye': 0}]
+
+        return backg_def
+
+    def _get_histo_data_reader(self, gs2, histo_data_file):
         readers_list = self._init_histo_data_readers(gs2)
 
         # 5 is a: 'G2pwd_xye.xye_ReaderClass object'
@@ -272,7 +321,7 @@ class GSASIIFitPeaks(PythonAlgorithm):
         if not isinstance(reader, gs2.G2pwd_xye.xye_ReaderClass):
             raise RuntimeError("Could not find the reader of type G2pwd_xye.xye_ReaderClass")
 
-        success, gs2_rd_list, err_msg = gs2.ImportDataGeneric(datafile, reader_xye, [])
+        success, gs2_rd_list, err_msg = gs2.ImportDataGeneric(histo_data_file, reader_xye, [])
         if not success or 0 == len(rd_list):
             raise RuntimeError('Empty list of readers. Cannot continue. The error message from '
                                'GSAS-II is: ' + err_msg)
@@ -328,10 +377,22 @@ class GSASIIFitPeaks(PythonAlgorithm):
 
         return peaks_init
 
+    def _produce_outputs(self, gof_estimates, parm_dict):
+        (result_rwp, result_gof) = gof_estimates
+        self.setProperty(PROP_OUT_RWP, result_rwp)
+        self.setProperty(PROP_OUT_GOF, result_gof)
+
+        _build_output_table(parm_dict)
+
     def _build_output_table(self, parm_dict):
         # build the table property - TODO
         for parm in parm_dict:
-            self.log().debug("Parameter: {0}".format(parm))
+            self.log().debug("Parameters for output table: {0}".format(parm))
+
+    def _save_gsas2_project(self, gsas2, filename):
+        self.notice("Saving GSAS-II project file into: {0}".format(filename))
+        self.debug("Saving GSAS-II project: {0}".format(gsas2))
+        # implement save as in GSASII.OnFileSave - TODO
 
 # Need GSAS-II _init_Imports()
 #pylint: disable=protected-access
