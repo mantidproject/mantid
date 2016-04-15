ca11654fd546a3effe3e68a55d839cf225bc0395
Simon Heybrock <simon.heybrock@esss.se>
2016-04-15 09:48:25 +0200

    Re #16086. Moved HistogramX code from header to source.

--- Framework/Kernel/src/Histogram/HistogramX.cpp
+++ Framework/Kernel/src/Histogram/HistogramX.cpp
@@ -12,5 +12,39 @@ HistogramX::HistogramX(const BinEdges &binEdges)
     throw std::logic_error("HistogramX: BinEdges size cannot be 1");
 }
 
+Points HistogramX::points() const {
+  if (xMode() == XMode::BinEdges)
+    return Points(BinEdges(m_data));
+  else
+    return Points(m_data);
+}
+
+BinEdges HistogramX::binEdges() const {
+  if (xMode() == XMode::Points)
+    return BinEdges(Points(m_data));
+  else
+    return BinEdges(m_data);
+}
+
+HistogramX::XMode HistogramX::xMode() const noexcept { return m_xMode; }
+
+void HistogramX::checkSize(const Points &points) const {
+  size_t target = size();
+  // 0 edges -> 0 points, otherwise points are 1 less than edges.
+  if (xMode() == XMode::BinEdges && target > 0)
+    target--;
+  if (target != points.size())
+    throw std::logic_error("HistogramX: size mismatch of Points\n");
+}
+
+void HistogramX::checkSize(const BinEdges &binEdges) const {
+  size_t target = size();
+  // 0 points -> 0 edges, otherwise edges are 1 more than points.
+  if (xMode() == XMode::Points && target > 0)
+    target++;
+  if (target != binEdges.size())
+    throw std::logic_error("HistogramX: size mismatch of BinEdges\n");
+}
+
 } // namespace Kernel
 } // namespace Mantid
