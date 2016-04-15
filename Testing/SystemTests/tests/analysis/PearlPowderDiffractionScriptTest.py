3ac80915211d3f6ed60935335b886dfd63ac741a
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2016-04-15 16:10:47 +0100

    code to run the system test script
    refs #15913

--- Testing/SystemTests/tests/analysis/PearlPowderDiffractionScriptTest.py
+++ Testing/SystemTests/tests/analysis/PearlPowderDiffractionScriptTest.py
@@ -1,9 +1,9 @@
-from mantid.simpleapi import *
+?from mantid.simpleapi import *
 from mantid import config
 import os.pathimport
-
-shutil
 import stresstesting
+
+import pearl_routines
 
 DIRS = config['datasearch.directories'].split(';')
 
@@ -25,7 +25,46 @@ class PearlPowderDiffractionScriptTest(stresstesting.MantidStressTest):
     def _clean_up_files(self, filenames, directories):
         try:
             for files in filenames:
-                path = os.path.join(directories[0], filenames)
+                path = os.path.join(directories[0], files)
                 os.remove(path)
         except OSError, ose:
             print 'could not delete the generated file: ', ose.filename
+
+    def runTest(self):
+        self._success = False
+
+        # code to run the script starts here
+        pearl_routines.PEARL_startup(usern="Mantid_Tester", thiscycle="15_4")
+
+        # setting files directory here
+        # DIRS[0] is the system test directory
+
+        # setting raw files directory
+        current_directory = pearl_routines.pearl_set_currentdatadir(DIRS[0] + "PEARL\\RawFiles\\")
+        pearl_routines.PEARL_setdatadir(current_directory)
+
+        # setting calibration files directory
+        pearl_routines.pearl_initial_dir(DIRS[0] + "\\PEARL\\")
+        pearl_routines.PEARL_setattenfile(DIRS[0] + "\\PEARL\\Attentuation\PRL112_DC25_10MM_FF.OUT")
+
+        # setting data output folder
+        pearl_routines.pearl_set_userdataoutput_dir(DIRS[0] + '\\PEARL\\DataOut\\')
+
+        # run the script by calling PEARL_focus function
+        pearl_routines.PEARL_focus('92476_92479', 'raw', fmode='trans', ttmode='TT70',
+                                   atten=True, van_norm=True, debug=True)
+
+        # Custom code to create and run this single test suite
+        # and then mark as success or a failure
+        suite = unittest.TestSuite()
+        suite.adTest(unittest.makeSuite(LoadTests, "test"))
+        runner = unittest.TextTestRunner()
+        # Run using either runner
+        res = runner.run(suite)
+        if res.wasSuccessful():
+            self._success = True
+        else:
+            self._success = False
+
+    def validate(self):
+        return self._success
