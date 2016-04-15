0cbbef2d9ec647105d5c775a444469f80ff39aaf
Simon Heybrock <simon.heybrock@esss.se>
2016-04-15 09:48:25 +0200

    Re #0. Moved HistogramX code from header to source.

--- Framework/Kernel/inc/MantidKernel/Histogram/HistogramX.h
+++ Framework/Kernel/inc/MantidKernel/Histogram/HistogramX.h
@@ -34,63 +34,38 @@ namespace Kernel {
   Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
 class MANTID_KERNEL_DLL HistogramX : public HistogramData<HistogramX> {
-private:
-  enum class XMode { BinEdges, Points };
-  XMode m_xMode;
-
 public:
   explicit HistogramX(const Points &points);
   explicit HistogramX(const BinEdges &binEdges);
 
-  Points points() const {
-    if (xMode() == XMode::BinEdges)
-      return Points(BinEdges(m_data));
-    else
-      return Points(m_data);
-  }
-
-  template <typename T> void setPoints(T &&data) {
-    Points &&points = Points(std::forward<T>(data));
-    checkSize(points);
-    m_xMode = XMode::Points;
-    m_data = points.cowData();
-  }
-
-  BinEdges binEdges() const {
-    if (xMode() == XMode::Points)
-      return BinEdges(Points(m_data));
-    else
-      return BinEdges(m_data);
-  }
-
-  template <typename T> void setBinEdges(T &&data) {
-    BinEdges &&edges = BinEdges(std::forward<T>(data));
-    checkSize(edges);
-    m_xMode = XMode::BinEdges;
-    m_data = edges.cowData();
-  }
+  Points points() const;
+  template <typename T> void setPoints(T &&data);
+  BinEdges binEdges() const;
+  template <typename T> void setBinEdges(T &&data);
 
 private:
-  XMode xMode() const { return m_xMode; }
+  enum class XMode { BinEdges, Points };
 
-  void checkSize(const Points &points) const {
-    size_t target = size();
-    // 0 edges -> 0 points, otherwise points are 1 less than edges.
-    if (xMode() == XMode::BinEdges && target > 0)
-      target--;
-    if (target != points.size())
-      throw std::logic_error("HistogramX: size mismatch of Points\n");
-  }
+  XMode xMode() const noexcept;
+  void checkSize(const Points &points) const;
+  void checkSize(const BinEdges &binEdges) const;
 
-  void checkSize(const BinEdges &binEdges) const {
-    size_t target = size();
-    // 0 points -> 0 edges, otherwise edges are 1 more than points.
-    if (xMode() == XMode::Points && target > 0)
-      target++;
-    if (target != binEdges.size())
-      throw std::logic_error("HistogramX: size mismatch of BinEdges\n");
-  }
+  XMode m_xMode;
 };
+
+template <typename T> void HistogramX::setPoints(T &&data) {
+  Points &&points = Points(std::forward<T>(data));
+  checkSize(points);
+  m_xMode = XMode::Points;
+  m_data = points.cowData();
+}
+
+template <typename T> void HistogramX::setBinEdges(T &&data) {
+  BinEdges &&edges = BinEdges(std::forward<T>(data));
+  checkSize(edges);
+  m_xMode = XMode::BinEdges;
+  m_data = edges.cowData();
+}
 
 } // namespace Kernel
 } // namespace Mantid
