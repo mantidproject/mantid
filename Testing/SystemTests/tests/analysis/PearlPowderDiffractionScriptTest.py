97185471e254d007562831f4e990162ca03c996f
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2016-04-19 15:30:48 +0100

    tests for the vanadium workspaces
    refs #15913

--- Testing/SystemTests/tests/analysis/PearlPowderDiffractionScriptTest.py
+++ Testing/SystemTests/tests/analysis/PearlPowderDiffractionScriptTest.py
@@ -1,12 +1,14 @@
-﻿import unittest
+import unittest
 import os.path
 import shutil
 
+from mantid.api import AnalysisDataService, MatrixWorkspace
 from mantid.simpleapi import *
 from mantid import config
 import stresstesting
 import pearl_routines
 
+DIFF_PLACES = 8
 # initial directory to the system test data files
 DIRS = config['datasearch.directories'].split(';')
 
@@ -76,7 +78,7 @@ class PearlPowderDiffractionScriptTest(stresstesting.MantidStressTest):
 
     def cleanup(self):
         filenames = []
-        filenames.extend(("PEARL/DataOut/PRL92476_92479.nxs", "PEARL/DataOut/PRL92476_92479_d_xye-0.dat", 
+        filenames.extend(("PEARL/DataOut/PRL92476_92479.nxs", "PEARL/DataOut/PRL92476_92479_d_xye-0.dat",
                           "PEARL/DataOut/PRL92476_92479_tof_xye-0.dat",
                           "PEARL/DataOut/PRL92476_92479-0.gss"))
         self._clean_up_files(filenames, DIRS)
@@ -98,3 +100,22 @@ class LoadTests(unittest.TestCase):
         self.cleanup_names = []
 
         # ============================ Success =============================
+
+    def test_vanadium_workspace(self):
+        for i in range(1, 15):
+            van_file = mtd["van" + str(i)]
+            self.assertTrue(isinstance(van_file, MatrixWorkspace))
+            self.assertEquals(1, van_file.getNumberHistograms())
+            self.assertEquals(4310, van_file.blocksize())
+
+        # check random values, places variables doesnt seem to work when
+        van1 = mtd["van1"]
+        self.assertAlmostEqual(1690.17031579, van1.readX(0)[199], places=DIFF_PLACES)
+        van4 = mtd["van4"]
+        self.assertAlmostEqual(6.5605707095e-06, van4.readY(0)[4000], places=DIFF_PLACES)
+        van7 = mtd["van7"]
+        self.assertAlmostEquals(1.03406081396e-08, van7.readE(0)[50], places=DIFF_PLACES)
+        van10 = mtd["van10"]
+        self.assertAlmostEquals(11667.996525642744, van10.readX(0)[3420], places=DIFF_PLACES)
+        van14 = mtd["van14"]
+        self.assertAlmostEqual(3.39159292684e-05, van14.readY(0)[187], places=DIFF_PLACES)
