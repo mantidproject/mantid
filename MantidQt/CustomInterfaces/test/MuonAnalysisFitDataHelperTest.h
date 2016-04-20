3fafbbd6423582e04143352c16a504009462ea43
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-04-20 14:50:54 +0100

    Update tests for fit data helper
    re #15518

--- MantidQt/CustomInterfaces/test/MuonAnalysisFitDataHelperTest.h
+++ MantidQt/CustomInterfaces/test/MuonAnalysisFitDataHelperTest.h
@@ -71,23 +71,25 @@ public:
     delete m_helper;
   }
 
-  void test_handleWorkspacePropertiesChanged() {
-    QStringList runs;
-    runs << "MUSR00015189.nxs"
-         << "MUSR00015190.nxs";
-    ON_CALL(*m_dataSelector, getRuns()).WillByDefault(Return(runs));
+  void test_handleDataPropertiesChanged() {
     ON_CALL(*m_dataSelector, getWorkspaceIndex()).WillByDefault(Return(0));
     ON_CALL(*m_dataSelector, getStartTime()).WillByDefault(Return(0.3));
     ON_CALL(*m_dataSelector, getEndTime()).WillByDefault(Return(9.9));
     EXPECT_CALL(*m_fitBrowser, setWorkspaceIndex(0)).Times(1);
     EXPECT_CALL(*m_fitBrowser, setStartX(0.3)).Times(1);
     EXPECT_CALL(*m_fitBrowser, setEndX(9.9)).Times(1);
-    m_helper->handleWorkspacePropertiesChanged();
+    m_helper->handleDataPropertiesChanged();
   }
 
-  void test_handleSelectedGroupsChanged() { TS_FAIL("Test not implemented!"); }
+  void test_handleSelectedGroupsChanged() {
+    TS_FAIL("Test not implemented!");
+    m_helper->handleSelectedGroupsChanged();
+  }
 
-  void test_handleSelectedPeriodsChanged() { TS_FAIL("Test not implemented!"); }
+  void test_handleSelectedPeriodsChanged() {
+    TS_FAIL("Test not implemented!");
+    m_helper->handleSelectedPeriodsChanged();
+  }
 
   void test_handleXRangeChangedGraphically() {
     EXPECT_CALL(*m_dataSelector, setStartTimeQuietly(0.4)).Times(1);
@@ -95,12 +97,40 @@ public:
     m_helper->handleXRangeChangedGraphically(0.4, 9.4);
   }
 
-  void test_peakPickerReassigned() {
-    //const QString wsName("MUSR")
-    TS_FAIL("Test not implemented!");
-    // EXPECT_CALL(*m_dataSelector, setWorkspaceDetails()).Times(1);
+  void test_peakPickerReassigned_singleWorkspace() {
+    const QString wsName("MUSR00015189; Pair; long; Asym; 1; #1");
+    EXPECT_CALL(*m_dataSelector,
+                setWorkspaceDetails(QString("00015189"), QString("MUSR")))
+        .Times(1);
     EXPECT_CALL(*m_dataSelector, setWorkspaceIndex(0u)).Times(1);
-    // m_helper->peakPickerReassigned()
+    EXPECT_CALL(*m_fitBrowser, allowSequentialFits(true)).Times(1);
+    m_helper->peakPickerReassigned(wsName);
+  }
+
+  void test_peakPickerReassigned_contiguousRange() {
+    const QString wsName("MUSR00015189-91; Pair; long; Asym; 1; #1");
+    EXPECT_CALL(*m_dataSelector,
+                setWorkspaceDetails(QString("00015189-91"), QString("MUSR")))
+        .Times(1);
+    EXPECT_CALL(*m_dataSelector, setWorkspaceIndex(0u)).Times(1);
+    EXPECT_CALL(*m_fitBrowser, allowSequentialFits(false)).Times(1);
+    m_helper->peakPickerReassigned(wsName);
+  }
+
+  void test_peakPickerReassigned_nonContiguousRange() {
+    const QString wsName("MUSR00015189-91, 15193; Pair; long; Asym; 1; #1");
+    EXPECT_CALL(
+        *m_dataSelector,
+        setWorkspaceDetails(QString("00015189-91, 15193"), QString("MUSR")))
+        .Times(1);
+    EXPECT_CALL(*m_dataSelector, setWorkspaceIndex(0u)).Times(1);
+    EXPECT_CALL(*m_fitBrowser, allowSequentialFits(false)).Times(1);
+    m_helper->peakPickerReassigned(wsName);
+  }
+
+  void test_handleDataWorkspaceChanged() {
+    TS_FAIL("Test not implemented!");
+    m_helper->handleDataWorkspaceChanged();
   }
 
 private:
