e5e1e1faa9e785dc73a370534c3f8e1971b64dab
Pete Peterson <petersonpf@ornl.gov>
2016-04-29 09:56:50 -0400

    Fix error initializing python with broken files
    If the python initialization came across a file which could not be
    read (e.g. emacs intermediate save file), all of python would fail
    to initialize. This will only affect people that develop code in a
    directory that mantid checks for algorithms.

--- Framework/PythonInterface/mantid/kernel/plugins.py
+++ Framework/PythonInterface/mantid/kernel/plugins.py
@@ -220,14 +220,16 @@ def contains_newapi_algorithm(filename):
         @returns True if a python algorithm written with the new API
         has been found.
     """
-    file = open(filename,'r')
-    alg_found = True
-    for line in reversed(file.readlines()):
-        if 'registerPyAlgorithm' in line:
-            alg_found = False
-            break
-        if 'AlgorithmFactory.subscribe' in line:
-            alg_found = True
-            break
-    file.close()
+    alg_found = False
+    try:
+        with open(filename,'r') as file:
+            for line in reversed(file.readlines()):
+                if 'registerPyAlgorithm' in line:
+                    break  # not found
+                if 'AlgorithmFactory.subscribe' in line:
+                    alg_found = True
+                    break
+    except IOError:
+        pass  # something wrong with reading the file
+
     return alg_found
