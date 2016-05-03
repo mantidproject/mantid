6c3113fa551d2a1fde8407e6cee004f0dbdeacb6
Elliot Oram <elliot.oram@stfc.ac.uk>
2016-05-03 12:57:52 +0100

    Use FloatArrayProperty for Mass and Amplitude inputs
    Refs #15816

--- Framework/PythonInterface/plugins/algorithms/VesuvioThickness.py
+++ Framework/PythonInterface/plugins/algorithms/VesuvioThickness.py
@@ -1,4 +1,4 @@
-from mantid.kernel import Direction
+from mantid.kernel import Direction, FloatArrayProperty
 from mantid.api import (PythonAlgorithm, AlgorithmFactory, ITableWorkspaceProperty)
 
 import numpy as np
@@ -20,17 +20,17 @@ class VesuvioThickness(PythonAlgorithm):
         return "Inelastic\\Indirect\\Vesuvio"
 
     def PyInit(self):
-        self.declareProperty("Masses", "",
-                             doc="A comma seperated list of the masses that make up the sample")
+        self.declareProperty(FloatArrayProperty("Masses", direction=Direction.Input),
+                             doc="The masses that make up the sample")
 
-        self.declareProperty("Amplitudes", "",
-                             doc="A comma separated list of the amplitudes of the peaks")
+        self.declareProperty(FloatArrayProperty("Amplitudes", direction=Direction.Input),
+                             doc="The amplitudes of the peaks")
 
         self.declareProperty("TransmissionGuess", 1.0,
                              doc="Initial guess for the transmission")
 
         self.declareProperty("Thickness", 5.0,
-                             doc="The thickness of the sample")
+                             doc="The thickness of the sample in centimetres (cm)")
 
         self.declareProperty("NumberDensity", 1.0,
                              doc="The Number Density of the sample material")
@@ -48,8 +48,8 @@ class VesuvioThickness(PythonAlgorithm):
 
 
     def _get_properties(self):
-        self._masses = self.getPropertyValue("Masses").split(',')
-        self._amplitudes = self.getPropertyValue("Amplitudes").split(',')
+        self._masses = self.getProperty("Masses").value
+        self._amplitudes = self.getProperty("Amplitudes").value
         self._transmission_guess = self.getProperty("TransmissionGuess").value
         self._thickness = self.getProperty("Thickness").value
         self._number_density = self.getProperty("NumberDensity").value
@@ -59,21 +59,13 @@ class VesuvioThickness(PythonAlgorithm):
         self._get_properties()
         issues = dict()
 
-        try:
-            self._masses = np.asarray([float(mass) for mass in self._masses])
-        except ValueError:
-            issues['Masses'] = ('Could not interpret \"%s\" ' % self._masses \
-                                    + 'as a list of numbers . Please ensure that ' \
-                                    + 'all inputs are valid')
-        try:
-            self._amplitudes = np.asarray([float(amp) for amp in self._amplitudes])
-        except ValueError:
-            issues['Amplitudes'] = ('Could not interpret \"%s\" ' % self._amplitudes \
-                                    + 'as a list of numbers . Please ensure that ' \
-                                    + 'all inputs are valid')
-
-        num_masses = len(self._masses)
+	num_masses = len(self._masses)
         num_amplitudes = len(self._amplitudes)
+	if num_masses == 0:
+	    isuues['Masses'] = ('Must have 1 or more Masses defined')
+	if num_amplitudes == 0:
+	    isuues['Amplitudes'] = ('Must have 1 or more Amplitudes defined')
+
         if num_masses != num_amplitudes:
             issues['Masses'] = ('The number of masses: %d, ' % num_masses \
                                 + 'is not equal to the number of amplitudes: %d' % num_amplitudes)
@@ -82,6 +74,10 @@ class VesuvioThickness(PythonAlgorithm):
 
 
     def PyExec(self):
+	# Create numpy arrays
+	self._masses = np.asarray(self._masses)
+	self._amplitudes = np.asarray(self._amplitudes)
+
         # Initialise output table workspaces
         create_tbl_alg = self.createChildAlgorithm("CreateEmptyTableWorkspace")
 
@@ -99,9 +95,11 @@ class VesuvioThickness(PythonAlgorithm):
         trans_guesses_tbl_ws.addColumn("double","Transmission")
         trans_guesses_tbl_ws.setPlotType(0, 1)
 
-        # Unit conversions and scatter length calculation
+	### Unit conversions and scatter length calculation ###
+	# Thickness converted to m and halved
         self._thickness /= 200.0
         scatter_length = np.sqrt(np.divide(self._amplitudes, self.FOUR_PI))
+	# Mass * atomic mass unit(g)
         total_mass = self._masses*1.66054e-24
         total_mass = sum(total_mass)
 
