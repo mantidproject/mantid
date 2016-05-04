3ba55befaee3c144d7b0fad67a282d9d66525d19
Pete Peterson <petersonpf@ornl.gov>
2016-05-04 14:54:54 -0400

    Add more testing of SaveNexusPD
    Adding a test with instrument and re-organized code to promote
    reuse.

--- Framework/PythonInterface/test/python/plugins/algorithms/SaveNexusPDTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/SaveNexusPDTest.py
@@ -1,17 +1,17 @@
 #pylint: disable=invalid-name,too-many-public-methods,too-many-arguments
-import unittest
-import numpy as np
+import h5py
 import mantid
-from mantid.simpleapi import CreateWorkspace, SaveNexusPD
-from mantid.api import *
-from testhelpers import run_algorithm
 from mantid.api import AnalysisDataService
+from mantid.simpleapi import CreateWorkspace, SaveNexusPD
+import numpy as np
+import os
+from testhelpers import WorkspaceCreationHelper as WCH
+import unittest
 
-import os, json
 
 class SaveNexusPDTest(unittest.TestCase):
     def saveFilePath(self, wkspname):
-        dataDir=mantid.config.getString('defaultsave.directory')
+        dataDir = mantid.config.getString('defaultsave.directory')
         return os.path.join(dataDir, wkspname+'.h5')
 
     def cleanup(self, wkspname, filename):
@@ -20,32 +20,87 @@ class SaveNexusPDTest(unittest.TestCase):
         if mantid.mtd.doesExist(wkspname):
             mantid.api.AnalysisDataService.remove(wkspname)
 
-    def test_saveOneSpectrumNoInstrument(self):
+    def testSaveOneSpectrumNoInstrument(self):
         """ Test to Save one spectrum
         """
         wkspname = "SaveNexusPDTest_onespectrumnoinstr"
         filename = self.saveFilePath(wkspname)
-        self._createOneSpectrum(wkspname, False)
+        self._createOneSpectrum(wkspname)
 
         try:
             SaveNexusPD(InputWorkspace=wkspname, OutputFilename=filename)
 
-            # verify
+            self.check(filename, False)
         finally:
-            # cleanup
             self.cleanup(filename, wkspname)
 
-    def _createOneSpectrum(self, wkspname, withInstrument=True):
-        """ Create data workspace
+    def testSaveMultiSpectra(self):
+        """ Test to Save two curves
         """
+        wkspname = "SaveNexusPDTest_multispectra"
+        filename = self.saveFilePath(wkspname)
+        self._createMultiSpectra(wkspname)
+
+        try:
+            SaveNexusPD(InputWorkspace=wkspname, OutputFilename=filename)
+
+            self.check(filename, True)
+        finally:
+            self.cleanup(filename, wkspname)
+
+    def checkDataFields(self, nxitem, withInstrument):
+        keys = nxitem.keys()
+        for fieldname in ['data', 'errors', 'tof']:
+            self.assertTrue(fieldname in keys)
+        if withInstrument:
+            for fieldname in ['Q', 'dspacing']:
+                self.assertTrue(fieldname in keys)
+
+    def checkDetectorFields(self, nxitem, withInstrument):
+        if not withInstrument:
+            return
+        keys = nxitem.keys()
+        for fieldname in ['distance',
+                          'azimuthal_angle', 'polar_angle']:
+            self.assertTrue(fieldname in keys)
+
+    def check(self, filename, withInstrument):
+        with h5py.File(filename, 'r') as handle:
+            nxentry = handle[handle.keys()[0]]
+            nxinstrument = nxentry['instrument']
+
+            nxmoderator = nxinstrument['moderator']
+            if withInstrument:
+                self.assertLess(nxmoderator['distance'][0], 0.)
+
+            for name in nxinstrument.keys():
+                if name == 'moderator':
+                    continue
+                nxdetector = nxinstrument[name]
+                self.checkDataFields(nxdetector, withInstrument)
+                self.checkDetectorFields(nxdetector, withInstrument)
+
+            for name in nxentry.keys():
+                if name == 'instrument':
+                    continue
+                if name == 'proton_charge':
+                    continue
+                nxdata = nxentry[name]
+                self.checkDataFields(nxdata, withInstrument)
+
+    def _createOneSpectrum(self, wkspname):
         x = np.arange(300, 16667, 15.)
-        y = np.random.random(len(x))
+        y = np.random.random(len(x)-1)  # histogram
         e = np.sqrt(y)
 
         CreateWorkspace(OutputWorkspace=wkspname,
                         DataX=x, DataY=y, DataE=e, NSpec=1,
-                        UnitX='TOF',#Distribution='1',
-                        YUnitlabel="S(q)")
+                        UnitX='TOF',
+                        YUnitlabel="stuff")
+
+    def _createMultiSpectra(self, wkspname):
+        wksp = WCH.create2DWorkspaceWithFullInstrument(30, 5, False, False)
+        AnalysisDataService.add(wkspname, wksp)
 
 if __name__ == '__main__':
     unittest.main()
