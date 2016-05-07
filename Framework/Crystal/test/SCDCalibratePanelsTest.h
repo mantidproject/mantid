9a9c15933e9c7ba94c64455dcb524ba9a6ef68d4
Vickie Lynch <lynchve@ornl.gov>
2016-05-07 10:23:20 -0400

    Refs #15644 fix unit test

--- Framework/Crystal/test/SCDCalibratePanelsTest.h
+++ Framework/Crystal/test/SCDCalibratePanelsTest.h
@@ -55,7 +55,7 @@ public:
     ITableWorkspace_sptr results =
         AnalysisDataService::Instance().retrieveWS<ITableWorkspace>("Result");
     TS_ASSERT_DELTA(-0.000561209, results->cell<double>(3, 1), .01);
-    TS_ASSERT_DELTA(6.6194e-06, results->cell<double>(2, 1), .01);
+    TS_ASSERT_DELTA(-0.0099, results->cell<double>(2, 1), .01);
     TS_ASSERT_DELTA(0, results->cell<double>(9, 1), .01);
     TS_ASSERT_DELTA(17.7186, results->cell<double>(8, 1), .01);
   }
