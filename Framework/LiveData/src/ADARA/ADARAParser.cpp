cd9aae2e24187e5c5bc818640e9793ba6d193f4c
Steven Hahn <hahnse@ornl.gov>
2016-05-08 00:34:59 -0400

    Apply changes suggsted by clang-tidy modernize-redundant-void-arg.

--- Framework/LiveData/src/ADARA/ADARAParser.cpp
+++ Framework/LiveData/src/ADARA/ADARAParser.cpp
@@ -41,7 +41,7 @@ Parser::Parser(unsigned int initial_buffer_size, unsigned int max_pkt_size)
 
 Parser::~Parser() { delete[] m_buffer; }
 
-void Parser::reset(void) {
+void Parser::reset() {
   m_len = 0;
   m_oversize_len = 0;
   m_restart_offset = 0;
@@ -334,7 +334,7 @@ void Parser::getDiscardedPacketsLogString(std::string &log_info) {
   log_info.append(ss.str());
 }
 
-void Parser::resetDiscardedPacketsStats(void) {
+void Parser::resetDiscardedPacketsStats() {
   // Reset Associative Map, Start Clean Stats...
   m_discarded_packets.clear();
 }
