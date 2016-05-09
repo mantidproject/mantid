5c6b63c80fecee8c8cd38f00cb3f878de7743216
Marina Ganeva <m.ganeva@fz-juelich.de>
2016-05-09 09:48:53 +0200

    Fixed compatibility with Python 2.6

--- Framework/PythonInterface/test/python/plugins/algorithms/FitGaussianTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/FitGaussianTest.py
@@ -38,11 +38,13 @@ class FitGaussianTest(unittest.TestCase):
         """
         self._linearWorkspace(0)
         fitResult = FitGaussian(self.ws,0)
-        self.assertTupleEqual((0.0,0.0), fitResult)
+        self.assertEqual(0.0, fitResult[0])
+        self.assertEqual(0.0, fitResult[1])
 
         self._veryNarrowPeakWorkspace()
         fitResult = FitGaussian(self.ws,0)
-        self.assertTupleEqual((0.0,0.0), fitResult)
+        self.assertEqual(0.0, fitResult[0])
+        self.assertEqual(0.0, fitResult[1])
 
     def _guessPeak(self,peakCentre,height,sigma):
         """Test-fitting one generated Gaussian peak.
@@ -54,8 +56,8 @@ class FitGaussianTest(unittest.TestCase):
         diffPeakCentre = abs((fitPeakCentre - peakCentre) / peakCentre)
         diffSigma      = abs((fitSigma      - sigma)      / sigma)
 
-        self.assertLess(diffPeakCentre,.03)
-        self.assertLess(diffSigma,     1e-6)
+        self.assertTrue(diffPeakCentre < 0.03)
+        self.assertTrue(diffSigma < 1e-6)
 
     def test_guessedPeaks(self):
         """Test that generated Gaussian peaks are reasonably well guessed.
