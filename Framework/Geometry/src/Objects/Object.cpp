e7c5b3125336d3261c15dd1c999dc57034db556a
Moore <lamar.moore@stfc.ac.uk>
2016-05-12 13:31:22 +0100

    Re #16057 clang-format fix

--- Framework/Geometry/src/Objects/Object.cpp
+++ Framework/Geometry/src/Objects/Object.cpp
@@ -1120,8 +1120,8 @@ double Object::triangleSolidAngle(const V3D &observer,
     case GluGeometryHandler::GeometryType::SPHERE:
       return SphereSolidAngle(observer, vectors, radius);
       break;
-	default:
-	  break;
+    default:
+      break;
     }
 
     //
