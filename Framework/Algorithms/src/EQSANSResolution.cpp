33b11bd4dd8379e44b25acaf2c16f6fb37450717
Mathieu Doucet <matd10@yahoo.com>
2016-05-13 10:01:24 -0400

    Add missing term

--- Framework/Algorithms/src/EQSANSResolution.cpp
+++ Framework/Algorithms/src/EQSANSResolution.cpp
@@ -17,7 +17,7 @@ double EQSANSResolution::getTOFResolution(double wl) {
     return 0.0148 * wl * wl * wl - 0.5233 * wl * wl + 6.4797 * wl + 231.99;
   } else {
     return 392.31 * pow(wl, 6) - 3169.3 * pow(wl, 5) + 10445 * pow(wl, 4) -
-           17872 * wl * wl * wl + 16509 * wl * wl + 1280.5;
+           17872 * wl * wl * wl + 16509 * wl * wl - 7448.4 * wl + 1280.5;
   }
 }
 
