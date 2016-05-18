e7f2b2d79cf5e8969781cf123d3370eaeb3a924d
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-05-18 17:20:19 +0100

    Add unit tests for EqualBinsChecker
    Tests the error level. (Not sure how to test the warning writing to the log).
    
    re #15659

--- Framework/Kernel/test/EqualBinsCheckerTest.h
+++ Framework/Kernel/test/EqualBinsCheckerTest.h
@@ -11,17 +11,38 @@ class EqualBinsCheckerTest : public CxxTest::TestSuite {
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
-  static EqualBinsCheckerTest *createSuite() { return new EqualBinsCheckerTest(); }
-  static void destroySuite( EqualBinsCheckerTest *suite ) { delete suite; }
+  static EqualBinsCheckerTest *createSuite() {
+    return new EqualBinsCheckerTest();
+  }
+  static void destroySuite(EqualBinsCheckerTest *suite) { delete suite; }
 
-
-  void test_Something()
-  {
-    TS_FAIL( "You forgot to write a test!");
+  void test_validate_pass() {
+    const auto fivePercentData = generateData(10, 0.05);
+    EqualBinsChecker checker(fivePercentData, 0.1, -1);
+    const std::string message = checker.validate();
+    TS_ASSERT(message.empty());
   }
 
+  void test_validate_fail() {
+    const auto tenPercentData = generateData(10, 0.1);
+    EqualBinsChecker checker(tenPercentData, 0.05, -1);
+    const std::string message = checker.validate();
+    TS_ASSERT(!message.empty());
+  }
 
+private:
+  /// Generate a vector of X data with "error" percentage error
+  Mantid::MantidVec generateData(const size_t length, const double error) {
+    Mantid::MantidVec data;
+    for (size_t i = 0; i < length; i++) {
+      double val = static_cast<double>(i);
+      if (i == length - 1) {
+        val += error;
+      }
+      data.push_back(val);
+    }
+    return data;
+  }
 };
-
 
 #endif /* MANTID_KERNEL_EQUALBINSCHECKERTEST_H_ */
