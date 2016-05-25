aef3dbc448aab4ac7b38c45ae1befdf786cfba55
Matt King <matthew.king@stfc.ac.uk>
2016-05-25 10:35:37 +0100

    Changed default of WavelengthStep to 0.02 and adapted testing
    Refs #16347

--- Framework/Algorithms/src/ReflectometryWorkflowBase.cpp
+++ Framework/Algorithms/src/ReflectometryWorkflowBase.cpp
@@ -74,9 +74,9 @@ void ReflectometryWorkflowBase::initWavelengthInputs() {
 
   declareProperty(
       make_unique<PropertyWithValue<double>>(
-          "WavelengthStep", 0.05,
+          "WavelengthStep", 0.02,
           boost::make_shared<MandatoryValidator<double>>(), Direction::Input),
-      "Wavelength rebinning step in angstroms. Defaults to 0.05. Used for "
+      "Wavelength rebinning step in angstroms. Defaults to 0.02. Used for "
       "rebinning intermediate workspaces converted into wavelength.");
 
   declareProperty(make_unique<PropertyWithValue<double>>(
