d77f7eedf07ce07fd3c1dc8690ac933ae80fec9b
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2016-06-07 16:23:15 +0100

    testing function values within CyrstalFieldPeaks instead
    refs #16483

--- Framework/CurveFitting/test/Functions/CrystalFieldPeaksTest.h
+++ Framework/CurveFitting/test/Functions/CrystalFieldPeaksTest.h
@@ -50,6 +50,29 @@ public:
     TS_ASSERT_DELTA(values[5], 0.43, 0.01);
   }
 
+  void test_further_calculation() {
+
+    CrystalFieldPeaks fun;
+    FunctionDomainGeneral domain;
+    FunctionValues values;
+    fun.setParameter("B20", 0.366336);
+    fun.setParameter("B22", 3.98132);
+    fun.setParameter("B40", -0.0304001);
+    fun.setParameter("B42", -0.119605);
+    fun.setParameter("B44", -0.130124);
+    fun.setAttributeValue("Ion", "Ce");
+    fun.setAttributeValue("Temperature", 44.0);
+    fun.setAttributeValue("ToleranceIntensity", 0.001);
+    fun.function(domain, values);
+
+    TS_ASSERT_DELTA(values[0], 0.0, 0.0001);
+    TS_ASSERT_DELTA(values[1], 29.3261, 0.00005);
+    TS_ASSERT_DELTA(values[2], 44.3412, 0.00005);
+    TS_ASSERT_DELTA(values[3], 2.74937, 0.000005);
+    TS_ASSERT_DELTA(values[4], 0.7204, 0.00005);
+    TS_ASSERT_DELTA(values[5], 0.429809, 0.000005);
+  }
+
   void test_factory() {
     std::string ini =
         "name=CrystalFieldPeaks,Ion=Ce,Temperature=25.0,B20=1,B22="
