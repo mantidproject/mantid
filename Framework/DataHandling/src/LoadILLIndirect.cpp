9072176a5c836d477afa440d32351f66240c8628
Gagik Vardanyan <vardanyan@ill.fr>
2016-06-15 11:16:32 +0200

    Re #16588 fixed clang formatting

--- Framework/DataHandling/src/LoadILLIndirect.cpp
+++ Framework/DataHandling/src/LoadILLIndirect.cpp
@@ -62,8 +62,10 @@ int LoadILLIndirect::confidence(Kernel::NexusDescriptor &descriptor) const {
   if (descriptor.pathExists("/entry0/wavelength")               // ILL
       && descriptor.pathExists("/entry0/experiment_identifier") // ILL
       && descriptor.pathExists("/entry0/mode")                  // ILL
-      && (descriptor.pathExists("/entry0/dataSD/dataSD") ||     // IN16B
-          descriptor.pathExists("/entry0/dataSD/SingleD_data")) // IN16B New format
+      &&
+      (descriptor.pathExists("/entry0/dataSD/dataSD") // IN16B
+       ||
+       descriptor.pathExists("/entry0/dataSD/SingleD_data")) // IN16B New format
       ) {
     return 80;
   } else {
