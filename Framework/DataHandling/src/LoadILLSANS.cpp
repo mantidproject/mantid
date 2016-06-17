29f0c015b5a353961c43e7e1ee2789ad8c5a3194
Steven Hahn <hahnse@ornl.gov>
2016-06-17 18:18:59 -0400

    Fix clang warning.

--- Framework/DataHandling/src/LoadILLSANS.cpp
+++ Framework/DataHandling/src/LoadILLSANS.cpp
@@ -25,7 +25,7 @@ DECLARE_NEXUS_FILELOADER_ALGORITHM(LoadILLSANS)
 /** Constructor
  */
 LoadILLSANS::LoadILLSANS()
-    : m_defaultBinning(2), m_supportedInstruments{"D33"} {}
+    : m_supportedInstruments{"D33"}, m_defaultBinning{2} {}
 
 //----------------------------------------------------------------------------------------------
 /// Algorithm's name for identification. @see Algorithm::name
