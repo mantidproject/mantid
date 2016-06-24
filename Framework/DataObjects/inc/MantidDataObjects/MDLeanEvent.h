388af97607d231083f8392fe8f4b67522fb2ce0f
Steven Hahn <hahnse@ornl.gov>
2016-06-24 14:06:03 -0400

    Remove pack pragmas.

--- Framework/DataObjects/inc/MantidDataObjects/MDLeanEvent.h
+++ Framework/DataObjects/inc/MantidDataObjects/MDLeanEvent.h
@@ -13,9 +13,6 @@
 namespace Mantid {
 namespace DataObjects {
 
-// To ensure MDEvent is as small as possible
-#pragma pack(push, 2)
-
 /** Templated class holding data about a neutron detection event
  * in N-dimensions (for example, Qx, Qy, Qz, E).
  *
@@ -305,8 +302,6 @@ public:
     }
   }
 };
-// Return to normal packing
-#pragma pack(pop)
 } // namespace DataObjects
 } // namespace Mantid
 
