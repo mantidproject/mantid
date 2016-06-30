5f8d20fcf2634228cb0b4e3ce2b0ecda8920df11
Krzysztof Dymkowski <kdymkowski84@gmail.com>
2016-06-30 16:33:35 +0100

    Add critical temperature to constants.

--- scripts/AbinsModules/Constants.py
+++ scripts/AbinsModules/Constants.py
@@ -3,3 +3,6 @@ Constants for instruments
 """
 
 TOSCA_constant = 1 / 16.0 # magic number for TOSCA...
+DW_critical_temperature = 20 # temperature in  K; if the temperature set in calculation is below or equal  this
+                             # critical temperature then we have low temperature scenario
+                             # and otherwise we have high temperature scenario.
