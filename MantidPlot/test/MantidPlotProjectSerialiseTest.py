e49d6408eeb92dfaf7eaa548a01f149d6ffb8afc
Samuel Jackson <samueljackson@outlook.com>
2016-06-30 15:36:40 +0100

    Refs #16719 Fix PEP8 warnings

--- MantidPlot/test/MantidPlotProjectSerialiseTest.py
+++ MantidPlot/test/MantidPlotProjectSerialiseTest.py
@@ -276,8 +276,7 @@ def clear_mantid():
 
     # Clear workspaces
     mtd.clear()
-    # Start a blank project to remove
-    # anything else
+    # Start a blank project to remove anything else
     newProject()
 
 
@@ -288,11 +287,11 @@ def find_elements(tokens, name):
 
 def create_dummy_workspace(ws_name):
     """ Create a dummy mantid workspace with some data """
-    X1 = np.linspace(0,10, 100)
+    X1 = np.linspace(0, 10, 100)
     Y1 = 1000*(np.sin(X1)**2) + X1*10
     X1 = np.append(X1, 10.1)
 
-    X2 = np.linspace(2,12, 100)
+    X2 = np.linspace(2, 12, 100)
     Y2 = 500*(np.cos(X2/2.)**2) + 20
     X2 = np.append(X2, 12.10)
 
@@ -317,7 +316,6 @@ def remove_folder(folder_name):
         raise IOError('Could not clean up folder after test')
 
 
-
 def read_project_file(folder_name):
     """ Read lines from a .mantid project file """
 
@@ -335,10 +333,10 @@ def read_project_file(folder_name):
 
     return tokenise_project_file(lines)
 
+
 def tokenise_project_file(lines):
-    format_func = lambda s: s.strip().split('\t')
-    tokens = map(format_func, lines)
-    return tokens
+    """ Convert lines from a project file to a list of lists """
+    return map(lambda s: s.strip().split('\t'), lines)
 
 
 # Run the unit tests
