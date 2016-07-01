da0b1fec5a379784d1139a2f4c5cd8bcea4681af
Gagik Vardanyan <vardanyan@ill.fr>
2016-07-01 14:19:21 +0200

    Re #16676 added functionality to set instrument and
    data search directories from test, that are restored to initial settings after test

--- Framework/Kernel/src/ConfigService.cpp
+++ Framework/Kernel/src/ConfigService.cpp
@@ -1582,6 +1582,39 @@ void ConfigServiceImpl::setDataSearchDirs(const std::string &searchDirs) {
 }
 
 /**
+ *  Appends the passed subdirectory path to the end of each of data
+ *  search dirs and adds these new dirs to data search directories
+ *  @param subdir :: the subdirectory path to add (relative)
+ */
+void ConfigServiceImpl::appendDataSearchSubDir(const std::string &subdir) {
+  if (subdir.empty())
+    return;
+
+  Poco::Path subDirPath;
+  try {
+    subDirPath = Poco::Path(subdir);
+  } catch (Poco::PathSyntaxException &) {
+    return;
+  }
+  std::vector<std::string> newDataDirs = m_DataSearchDirs;
+  std::vector<std::string>::const_iterator it = newDataDirs.begin();
+  std::vector<std::string>::const_iterator end = newDataDirs.end();
+
+  for (; it != end; ++it) {
+    Poco::Path newDirPath;
+    try {
+      newDirPath = Poco::Path(*it);
+      newDirPath.append(subDirPath);
+      newDataDirs.push_back(newDirPath.toString());
+    } catch (Poco::PathSyntaxException &) {
+      continue;
+    }
+  }
+
+  setDataSearchDirs(newDataDirs);
+}
+
+/**
  *  Adds the passed path to the end of the list of data search paths
  *  the path name must be absolute
  *  @param path :: the absolute path to add
