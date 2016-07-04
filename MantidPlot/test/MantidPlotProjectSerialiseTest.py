e2a26eb869264817ce6913e35e6ea0caa41bb2d7
Samuel Jackson <samueljackson@outlook.com>
2016-07-04 17:11:13 +0100

    Refs #16719 Revert to using userexpand but remove slash
    This should work fine, the slash could of been complicating matters.

--- MantidPlot/test/MantidPlotProjectSerialiseTest.py
+++ MantidPlot/test/MantidPlotProjectSerialiseTest.py
@@ -12,12 +12,7 @@ class MantidPlotProjectSerialiseTest(unittest.TestCase):
 
     def setUp(self):
         self._project_name = "MantidPlotTestProject"
-
-        # set the default directory to something if it is not set already
-        if config['defaultsave.directory'] == '':
-            config['defaultsave.directory'] = os.path.expanduser("~")
-
-        self._project_folder = os.path.join(config['defaultsave.directory'],
+        self._project_folder = os.path.join(os.path.expanduser("~"),
                                             self._project_name)
         file_name = "%s.mantid" % self._project_name
         self._project_file = os.path.join(self._project_folder, file_name)
