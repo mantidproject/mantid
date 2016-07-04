207130e5b63bb84cc774c45a41ab6226a1609b14
Samuel Jackson <samueljackson@outlook.com>
2016-07-04 15:01:40 +0100

    Refs #16719 Use default save directory
    Instead of the users home path. This should be more consistent with how
    other mantid tests work.

--- MantidPlot/test/MantidPlotProjectSerialiseTest.py
+++ MantidPlot/test/MantidPlotProjectSerialiseTest.py
@@ -12,7 +12,8 @@ class MantidPlotProjectSerialiseTest(unittest.TestCase):
 
     def setUp(self):
         self._project_name = "MantidPlotTestProject"
-        self._project_folder = os.path.expanduser("~/%s" % self._project_name)
+        self._project_folder = os.path.join(config['defaultsave.directory'],
+                                            self._project_name)
         file_name = "%s.mantid" % self._project_name
         self._project_file = os.path.join(self._project_folder, file_name)
 
