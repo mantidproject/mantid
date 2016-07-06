1a1b3876fcbfc41e8048cc61d408c2c41ba7e37a
Gagik Vardanyan <vardanyan@ill.fr>
2016-07-06 16:51:13 +0200

    Re #16753 normalise to monitor,clean-up

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/IndirectILLReduction.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/IndirectILLReduction.py
@@ -12,20 +12,20 @@ import os.path
 #pylint: disable=too-many-instance-attributes
 class IndirectILLReduction(DataProcessorAlgorithm):
 
-    #workspaces
-    _raw_ws = None
-    _grouped_ws = None
-    _calib_ws = None
-    _mnorm_ws = None
-    _vnorm_ws = None
-    _unmirror_ws = None
-    _red_ws= None
-    _monitor_ws = None
+    #workspace names
+    _raw_ws_name = None
+    _grouped_ws_name = None
+    _calib_ws_name = None
+    _mnorm_ws_name = None
+    _vnorm_ws_name = None
+    _unmirror_ws_name = None
+    _red_ws_name = None
+    _monitor_ws_name = None
 
-    #files
-    _run_file = None
-    _map_file = None
-    _parameter_file = None
+    #files names
+    _run_file_name = None
+    _map_file_name = None
+    _parameter_file_name = None
 
     #bool flags
     _mirror_sense = None
@@ -35,10 +35,10 @@ class IndirectILLReduction(DataProcessorAlgorithm):
 
     #other
     _analyser = None
+    _instrument = None
     _instrument_name = None
     _reflection = None
     _unmirror_option = None
-    _instrument = None
 
     def category(self):
         return "Workflow\\MIDAS;Inelastic\\Reduction"
@@ -130,52 +130,53 @@ class IndirectILLReduction(DataProcessorAlgorithm):
 
     def setUp(self):
 
-        self._run_file = self.getPropertyValue('Run')
-        self._calib_ws = self.getPropertyValue('CalibrationWorkspace')
-        self._raw_ws = self.getPropertyValue('RawWorkspace')
-        self._red_ws= self.getPropertyValue('ReducedWorkspace')
-        self._vnorm_ws = self.getPropertyValue('VNormalisedWorkspace')
-        self._mnorm_ws = self.getPropertyValue('MNormalisedWorkspace')
-        self._unmirror_ws = self.getPropertyValue('UnmirroredWorkspace')
-        self._grouped_ws = self.getPropertyValue('DetGroupedWorkspace')
+        self._run_file_name = self.getPropertyValue('Run')
+        self._calib_ws_name = self.getPropertyValue('CalibrationWorkspace')
+        self._raw_ws_name = self.getPropertyValue('RawWorkspace')
+        self._red_ws_name= self.getPropertyValue('ReducedWorkspace')
+        self._vnorm_ws_name = self.getPropertyValue('VNormalisedWorkspace')
+        self._mnorm_ws_name = self.getPropertyValue('MNormalisedWorkspace')
+        self._unmirror_ws_name = self.getPropertyValue('UnmirroredWorkspace')
+        self._grouped_ws_name = self.getPropertyValue('DetGroupedWorkspace')
         self._monitor_ws = self.getPropertyValue('MonitorWorkspace')
-        self._map_file = self.getPropertyValue('MapFile')
-        self._mirror_sense = self.getPropertyValue('MirrorSense')
-        self._control_mode = self.getPropertyValue('ControlMode')
-        self._unmirror_option = self.getPropertyValue('UnmirrorOption')
-        self._save = self.getPropertyValue('Save')
-        self._plot = self.getPropertyValue('Plot')
+        self._map_file_name = self.getPropertyValue('MapFile')
         self._analyser = self.getPropertyValue('Analyser')
         self._reflection = self.getPropertyValue('Reflection')
+
+        self._mirror_sense = self.getProperty('MirrorSense').value
+        self._control_mode = self.getProperty('ControlMode').value
+        self._unmirror_option = self.getProperty('UnmirrorOption').value
+        self._save = self.getProperty('Save').value
+        self._plot = self.getProperty('Plot').value
 
     def PyExec(self):
 
         self.setUp()
 
-        Load(Filename=self._run_file,OutputWorkspace=self._raw_ws)
+        Load(Filename=self._run_file_name,OutputWorkspace=self._raw_ws_name)
 
-        self.log().information('Loaded .nxs file(s) : %s' % self._run_file)
+        self.log().information('Loaded .nxs file(s) : %s' % self._run_file_name)
 
-        self.setPropertyValue('RawWorkspace', self._raw_ws)
+        self.setPropertyValue('RawWorkspace', self._raw_ws_name)
 
-        if isinstance(mtd[self._raw_ws],WorkspaceGroup):
-            self._instrument = mtd[self._raw_ws].getItem(0).getInstrument()
+        if isinstance(mtd[self._raw_ws_name],WorkspaceGroup):
+            self._instrument = mtd[self._raw_ws_name].getItem(0).getInstrument()
             self._loadParamFiles()
-            out_ws = self._multifile_reduction(self._raw_ws)
+            out_ws = self._multifile_reduction(mtd[self._raw_ws_name])
         else:
-            self._instrument = mtd[self._raw_ws].getInstrument()
+            self._instrument = mtd[self._raw_ws_name].getInstrument()
             self._loadParamFiles()
-            out_ws = self._reduction(self._raw_ws)
+            out_ws = self._reduction(mtd[self._raw_ws_name], False)
 
-        if self._control_mode == False:
-            self.setPropertyValue('ReducedWorkspace', out_ws)
+        if not self._control_mode:
+            self.setProperty('ReducedWorkspace', out_ws)
         else:
-            self.setPropertyValue('MonitorWorkspace', out_ws[0])
-            self.setPropertyValue('DetGroupedWorkspace', out_ws[1])
-            self.setPropertyValue('MNormalisedWorkspace', out_ws[2])
-            self.setPropertyValue('VNormalisedWorkspace', out_ws[3])
-            self.setPropertyValue('UnmirroredWorkspace', out_ws[4])
-            self.setPropertyValue('ReducedWorkspace', out_ws[5])
+            self.setProperty('MonitorWorkspace', out_ws[0])
+            self.setProperty('DetGroupedWorkspace', out_ws[1])
+            self.setProperty('MNormalisedWorkspace', out_ws[2])
+            self.setProperty('VNormalisedWorkspace', out_ws[3])
+            self.setProperty('UnmirroredWorkspace', out_ws[4])
+            self.setProperty('ReducedWorkspace', out_ws[5])
 
     def _loadParamFiles(self):
         """
@@ -185,19 +186,19 @@ class IndirectILLReduction(DataProcessorAlgorithm):
         self._instrument_name = self._instrument.getName()
         idf_directory = config['instrumentDefinition.directory']
         ipf_name = self._instrument_name + '_' + self._analyser + '_' + self._reflection + '_Parameters.xml'
-        self._parameter_file = os.path.join(idf_directory, ipf_name)
+        self._parameter_file_name = os.path.join(idf_directory, ipf_name)
 
-        logger.information('Parameter file : %s' % self._parameter_file)
+        logger.information('Parameter file : %s' % self._parameter_file_name)
 
-        if self._map_file == '':
+        if self._map_file_name == '':
             # path name for default map file
             if self._instrument.hasParameter('Workflow.GroupingFile'):
                grouping_filename = self._instrument.getStringParameter('Workflow.GroupingFile')[0]
-               self._map_file = os.path.join(config['groupingFiles.directory'], grouping_filename)
+               self._map_file_name = os.path.join(config['groupingFiles.directory'], grouping_filename)
             else:
               raise ValueError("Failed to find default detector grouping file. Please specify.")
 
-        logger.information('Map file : %s' % self._map_file)
+        logger.information('Map file : %s' % self._map_file_name)
 
     def _save(self):
         """
@@ -205,8 +206,8 @@ class IndirectILLReduction(DataProcessorAlgorithm):
         """
 
         workdir = config['defaultsave.directory']
-        file_path = os.path.join(workdir, self._red_ws+ '.nxs')
-        SaveNexusProcessed(InputWorkspace=self._red_ws, Filename=file_path)
+        file_path = os.path.join(workdir, self._red_ws_name+ '.nxs')
+        SaveNexusProcessed(InputWorkspace=self._red_ws_name, Filename=file_path)
         logger.information('Output file : ' + file_path)
 
     def _plot(self):
@@ -217,7 +218,7 @@ class IndirectILLReduction(DataProcessorAlgorithm):
         from IndirectImport import import_mantidplot
         mtd_plot = import_mantidplot()
         graph = mtd_plot.newGraph()
-        mtd_plot.plotSpectrum(self._red_ws, 0, window=graph)
+        mtd_plot.plotSpectrum(self._red_ws_name, 0, window=graph)
         layer = graph.activeLayer()
         layer.setAxisTitle(mtd_plot.Layer.Bottom, 'Energy Transfer (micro eV)')
         layer.setAxisTitle(mtd_plot.Layer.Left, '')
@@ -225,11 +226,11 @@ class IndirectILLReduction(DataProcessorAlgorithm):
 
     def _multifile_reduction(self, gws):
         """
-        Call _reduction for GroupWorkspace
+        Calls _reduction for GroupWorkspace
 
-        @param gws :: name of the input workspace group to reduce
-        @return    :: the reduced group workspace or a list of group workspaces containing
-                      control and reduced ones depending on the _control_mode
+        @param gws :: input workspace group to reduce
+        @return    :: the reduced group workspace or a list of group
+                      workspaces depending on the _control_mode
         """
 
         list_red = []
@@ -239,11 +240,11 @@ class IndirectILLReduction(DataProcessorAlgorithm):
         list_vnorm = []
         list_unmirrored = []
 
-        size = mtd[gws].size()
+        size = gws.size()
 
         for i in range(0,size):
-            out = self._reduction(mtd[gws].getItem(i))
-            if self._control_mode == False:
+            out = self._reduction(gws.getItem(i),True)
+            if not self._control_mode:
                 list_red.append(out)
             else:
                 list_monitor.append(out[0])
@@ -255,31 +256,42 @@ class IndirectILLReduction(DataProcessorAlgorithm):
 
         group_red = GroupWorkspaces(list_red)
 
-        if self._control_mode == True:
+        if not self._control_mode:
+            return group_red
+        else:
             group_monitor = GroupWorkspaces(list_monitor)
             group_grouped = GroupWorkspaces(list_grouped)
             group_mnorm = GroupWorkspaces(list_mnorm)
             group_vnorm = GroupWorkspaces(list_vnorm)
             group_unmirrored = GroupWorkspaces(list_unmirrored)
-            group = [group_monitor,group_grouped,group_mnorm,
+            return [group_monitor,group_grouped,group_mnorm,
                     group_vnorm,group_unmirrored,group_red]
-        else:
-            group = group_red
 
-        return group
-
-    def _reduction(self, ws):
+    def _reduction(self, ws, multiple = False):
         """
         Run indirect reduction for IN16B
 
-        @param ws :: name of the input raw workspace (single) to reduce
-        @return   :: the reduced workspace or a list of workspaces containing
-                     control and reduced ones depending on the _control_mode
+        @param ws       :: name of the input raw workspace (single) to reduce
+        @param multiple :: bool flag indicating if it is a part of multifile reduction
+        @return         :: the reduced workspace or a list of workspaces
+                           depending on the control mode
         """
 
-        logger.information('Reducting input workspace : %s' % ws)
+        logger.information('Reducting input workspace : %s' % ws.name)
 
-        LoadParameterFile(Workspace=ws, Filename=self._parameter_file)
+        if multiple:
+           run_number = str(ws.getRunNumber()) + '_'
+        else:
+           run_number = ""
+
+        monitor =  run_number + self._monitor_ws
+        grouped =  run_number + self._grouped_ws_name
+        mnorm = run_number + self._mnorm_ws_name
+        vnorm =  run_number + self._vnorm_ws_name
+        unmirror =  run_number + self._unmirror_ws_name
+        red = run_number + self._red_ws_name
+
+        LoadParameterFile(Workspace=ws, Filename=self._parameter_file_name)
 
         AddSampleLog(Workspace=ws,
                      LogName="Facility",
@@ -287,43 +299,38 @@ class IndirectILLReduction(DataProcessorAlgorithm):
                      LogText="ILL")
 
         ExtractSingleSpectrum(InputWorkspace=ws,
-                              OutputWorkspace=self._monitor_ws,
+                              OutputWorkspace=monitor,
                               WorkspaceIndex=0)
 
         GroupDetectors(InputWorkspace=ws,
-                       OutputWorkspace=self._grouped_ws,
-                       MapFile=self._map_file,
+                       OutputWorkspace=grouped,
+                       MapFile=self._map_file_name,
                        Behaviour='Average')
 
-        self._mnorm_ws = self._normalise_to_monitor(self._monitor_ws, self._grouped_ws)
+        NormaliseToMonitor(InputWorkspace = grouped,
+                           MonitorWorkspace = monitor,
+                           OutputWorkspace = mnorm)
 
-        if self._calib_ws != '':
-            self._vnorm_ws = self._normalise_to_vanadium(self._mnorm_ws, self._calib_ws)
+        if self._calib_ws_name != '':
+            vnorm = self._normalise_to_vanadium(mnorm, self._calib_ws_name)
         else:
-            self._vnorm_ws = self._mnorm_ws
+            vnorm = mnorm
 
         if self._mirror_sense:
-            self._unmirror_ws = self._unmirror(self._vnorm_ws)
+            unmirror = self._unmirror(vnorm)
         else:
-            self._unmirror_ws = self._vnorm_ws
+            unmirror = vnorm
 
-        self._red_ws = self._calculate_energy(self._unmirror_ws)
+        red = self._calculate_energy(mnorm, red) #should be unmirror
 
-        if self._control_mode == False:
-            return self._red_ws
+        if not self._control_mode:
+            DeleteWorkspace(monitor)
+            DeleteWorkspace(mnorm)
+            DeleteWorkspace(grouped)
+            return red
         else:
-            return [self._monitor_ws,self._grouped_ws,self._mnorm_ws,
-                    self._vnorm_ws,self._unmirror_ws,self._red_ws]
-
-    def _normalise_to_monitor(self, mon, ws):
-        """
-        Normalises to monitor.
-
-        @param mon :: name of the monitor workspace
-        @param ws  :: name of the spectra to normalise
-        @return    :: the spectra normalised to monitor
-        """
-        return ws
+            return [monitor,grouped,mnorm,
+                    vnorm,unmirror,red]
 
     def _normalise_to_vanadium(self, ws, van):
         """
@@ -347,33 +354,33 @@ class IndirectILLReduction(DataProcessorAlgorithm):
 
         return ws
 
-    def _calculate_energy(self, ws):
+    def _calculate_energy(self, ws, red):
         """
         Convert the input run to energy transfer
         @param ws :: name of the input workspace
+        @param ws :: name of the output workspace
         @return   :: workspace with x-axis converted to energy transfer
         """
 
         formula = self._energy_range(ws)
 
         ConvertAxisByFormula(InputWorkspace=ws,
-                             OutputWorkspace=self._red_ws,
+                             OutputWorkspace=red,
                              Axis='X',
                              Formula=formula)
 
-        red_ws_p = mtd[self._red_ws]
-        red_ws_p.getAxis(0).setUnit('DeltaE')
+        mtd[red].getAxis(0).setUnit('DeltaE')
 
-        xnew = red_ws_p.readX(0)  # energy array
+        xnew = mtd[red].readX(0)  # energy array
         logger.information('Energy range : %f to %f' % (xnew[0], xnew[-1]))
 
-        return self._red_ws
+        return mtd[red]
 
     def _energy_range(self, ws):
         """
         Calculate the energy range for the workspace
 
-        @param ws :: name of the workspace
+        @param ws :: name of the input workspace
         @return   :: formula to transform from time channel to energy transfer
         """
         x = mtd[ws].readX(0)
