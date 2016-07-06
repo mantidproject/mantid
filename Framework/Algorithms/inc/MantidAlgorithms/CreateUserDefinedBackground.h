ee7b430a6c434a349b4cf1c5179a7bd39f7e38fb
Tom Perkins <thomas.perkins@stfc.ac.uk>
2016-07-06 13:51:27 +0100

    First attempt at implementing algorithm
    CreateUserDefinedBackground takes a set of points that the user has chosen
    and creates a background workspace out of them. It interpolates the points
    so the resulting background can be subtracted from the original data.
    
    re #16798

--- Framework/Algorithms/inc/MantidAlgorithms/CreateUserDefinedBackground.h
+++ Framework/Algorithms/inc/MantidAlgorithms/CreateUserDefinedBackground.h
@@ -3,10 +3,17 @@
 
 #include "MantidAlgorithms/DllConfig.h"
 #include "MantidAPI/Algorithm.h"
+#include "MantidAPI/ITableWorkspace_fwd.h"
 namespace Mantid {
+  namespace Kernel {
+    class Interpolation;
+  }
 namespace Algorithms {
 
-/** CreateUserDefinedBackground : TODO: DESCRIPTION
+/** CreateUserDefinedBackground : Given an input workspace containing data with
+  a background and a table of user-selected points defining the background,
+  creates a new workspace containing background data that can be subtracted
+  from the original data.
 
   Copyright &copy; 2016 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
   National Laboratory & European Spallation Source
@@ -29,16 +36,39 @@ namespace Algorithms {
   File change history is stored at: <https://github.com/mantidproject/mantid>
   Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
-class MANTID_ALGORITHMS_DLL CreateUserDefinedBackground final : public API::Algorithm {
+class MANTID_ALGORITHMS_DLL CreateUserDefinedBackground final
+    : public API::Algorithm {
 public:
+  /// Return name of algorithm
   const std::string name() const override final;
+  /// Version number
   int version() const override final;
+  /// Category algorithm belongs to
   const std::string category() const override final;
+  /// Description of algorithm
   const std::string summary() const override final;
 
 private:
+  /// Declare properties and initialise algorithm
   void init() override final;
+  /// Execute algorithm
   void exec() override final;
+  /// Verify input properties and return errors
+  std::map<std::string, std::string> validateInputs() override final;
+  /// Sort table and remove blank rows
+  void cleanUpTable(API::ITableWorkspace_sptr &table) const;
+  /// Extend background to cover range of data
+  void
+  extendBackgroundToData(API::ITableWorkspace_sptr &background,
+                         const API::MatrixWorkspace_const_sptr &data) const;
+  /// Create new background workspace with data interpolated from table
+  API::MatrixWorkspace_sptr
+  createBackgroundWorkspace(const API::ITableWorkspace_const_sptr &background,
+                            const API::MatrixWorkspace_const_sptr &data) const;
+  /// Set up and return an interpolator object ready for use
+  Kernel::Interpolation &
+  getInterpolator(const API::ITableWorkspace_const_sptr &background,
+                  const API::MatrixWorkspace_const_sptr &workspace) const;
 };
 
 } // namespace Algorithms
--- Framework/Algorithms/src/CreateUserDefinedBackground.cpp
+++ Framework/Algorithms/src/CreateUserDefinedBackground.cpp
@@ -1,4 +1,11 @@
 #include "MantidAlgorithms/CreateUserDefinedBackground.h"
+#include "MantidAPI/Axis.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidKernel/Interpolation.h"
+
+#include <algorithm>
 
 namespace Mantid {
 namespace Algorithms {
@@ -11,41 +18,218 @@ DECLARE_ALGORITHM(CreateUserDefinedBackground)
 
 //----------------------------------------------------------------------------------------------
 
-/// Algorithms name for identification. @see Algorithm::name
-const std::string CreateUserDefinedBackground::name() const { return "CreateUserDefinedBackground"; }
+/// Algorithm's name for identification. @see Algorithm::name
+const std::string CreateUserDefinedBackground::name() const {
+  return "CreateUserDefinedBackground";
+}
 
 /// Algorithm's version for identification. @see Algorithm::version
 int CreateUserDefinedBackground::version() const { return 1; }
 
 /// Algorithm's category for identification. @see Algorithm::category
 const std::string CreateUserDefinedBackground::category() const {
-  return "TODO: FILL IN A CATEGORY";
+  return "CorrectionFunctions\\BackgroundCorrections";
 }
 
 /// Algorithm's summary for use in the GUI and help. @see Algorithm::summary
 const std::string CreateUserDefinedBackground::summary() const {
-  return "TODO: FILL IN A SUMMARY";
+  return "Creates a workspace of background data from a user-supplied set of "
+         "points. This workspace can then be subtracted from the original "
+         "data.";
 }
 
 //----------------------------------------------------------------------------------------------
-/** Initialize the algorithm's properties.
+/**
+ * Initialize the algorithm's properties.
  */
 void CreateUserDefinedBackground::init() {
-  declareProperty(
-      Kernel::make_unique<WorkspaceProperty<API::Workspace>>("InputWorkspace", "",
-                                                             Direction::Input),
-      "An input workspace.");
-  declareProperty(
-      Kernel::make_unique<WorkspaceProperty<API::Workspace>>("OutputWorkspace", "",
-                                                             Direction::Output),
-      "An output workspace.");
+  declareProperty(Kernel::make_unique<WorkspaceProperty<API::MatrixWorkspace>>(
+                      "InputWorkspace", "", Direction::Input),
+                  "Input workspace containing data and background");
+  declareProperty(Kernel::make_unique<WorkspaceProperty<API::ITableWorkspace>>(
+                      "BackgroundPoints", "", Direction::Input),
+                  "Table containing user-defined background points");
+  declareProperty(Kernel::make_unique<WorkspaceProperty<API::MatrixWorkspace>>(
+                      "OutputBackgroundWorkspace", "", Direction::Output),
+                  "Workspace containing background to be subtracted");
 }
 
 //----------------------------------------------------------------------------------------------
-/** Execute the algorithm.
+/**
+ * Execute the algorithm.
  */
 void CreateUserDefinedBackground::exec() {
-  // TODO Auto-generated execute stub
+  const API::MatrixWorkspace_const_sptr inputWS = getProperty("InputWorkspace");
+  API::ITableWorkspace_sptr pointsTable = getProperty("BackgroundPoints");
+
+  // Clean up input points table and extend to range of data
+  cleanUpTable(pointsTable);
+  extendBackgroundToData(pointsTable, inputWS);
+
+  // Generate output workspace with background data
+  const auto outputWS = createBackgroundWorkspace(pointsTable, inputWS);
+
+  setProperty("OutputWorkspace",
+              API::MatrixWorkspace_sptr(std::move(outputWS)));
+}
+
+/**
+ * Cleans up input points table by sorting points and removing any (0, 0) blank
+ * rows
+ * @param table :: [input, output] Table of points to work on
+ */
+void CreateUserDefinedBackground::cleanUpTable(
+    API::ITableWorkspace_sptr &table) const {
+  // Sort the table
+  std::vector<std::pair<std::string, bool>> sortArgs;
+  sortArgs.emplace_back(table->getColumn(0)->name(), true);
+  table->sort(sortArgs);
+
+  // Delete blank (zero) rows
+  std::vector<size_t> blankRows;
+  const auto isZero = [](const double n) {
+    return !(fabs(n) > std::numeric_limits<double>::epsilon());
+  };
+  for (size_t i = 0; i < table->rowCount(); i++) {
+    double x, y;
+    API::TableRow row = table->getRow(i);
+    row >> x >> y;
+    if (!isZero(x)) {
+      break;
+    } else if (isZero(y)) {
+      blankRows.push_back(i);
+    }
+  }
+  for (const auto &row : blankRows) {
+    table->removeRow(row);
+  }
+}
+
+/**
+ * Extend background to limits of data: if it doesn't extend this far already,
+ * insert first and last points of data into it.
+ * @param background :: [input, output] Table of background points to work on
+ * @param data :: [input] Input workspace with data
+ */
+void CreateUserDefinedBackground::extendBackgroundToData(
+    API::ITableWorkspace_sptr &background,
+    const API::MatrixWorkspace_const_sptr &data) const {
+  const auto &xData = data->readX(0);
+  const auto &yData = data->readY(0);
+  const auto xMinMax = std::minmax_element(xData.begin(), xData.end());
+  // If first point > data minimum, insert a new first point
+  if (background->Double(0, 0) > *xMinMax.first) {
+    background->insertRow(0);
+    API::TableRow firstRow = background->getFirstRow();
+    const auto dataPosition = xMinMax.first - xData.begin();
+    firstRow << xData[dataPosition] << yData[dataPosition];
+  }
+  // If last point < data maximum, append a new last point
+  if (background->Double(background->rowCount(), 0) < *xMinMax.second) {
+    API::TableRow lastRow = background->appendRow();
+    const auto dataPosition = xMinMax.second - xData.begin();
+    lastRow << xData[dataPosition] << yData[dataPosition];
+  }
+}
+
+/**
+ * Given a table of background points and the original workspace,
+ * return a new workspace containing interpolated background data.
+ * The same background is assumed for all spectra.
+ * @param background :: [input] Table of background points
+ * @param data :: [input] Original data workspace
+ * @returns :: Workspace containing background to be subtracted
+ */
+API::MatrixWorkspace_sptr
+CreateUserDefinedBackground::createBackgroundWorkspace(
+    const API::ITableWorkspace_const_sptr &background,
+    const API::MatrixWorkspace_const_sptr &data) const {
+  auto outputWS = data->clone();
+  const auto &xData = outputWS->readX(0);
+  MantidVec yBackground;
+  MantidVec eBackground(outputWS->blocksize(), 0);
+
+  // Interpolate Y data in the table
+  const auto &lerp = getInterpolator(background, data);
+  for (const double &x : xData) {
+    yBackground.push_back(lerp.value(x));
+  }
+
+  // Apply Y and E data to all spectra in the workspace
+  for (size_t spec = 0; spec < outputWS->getNumberHistograms(); spec++) {
+    outputWS->dataY(spec) = yBackground;
+    outputWS->dataE(spec) = eBackground;
+  }
+
+  return API::MatrixWorkspace_sptr(std::move(outputWS));
+}
+
+/**
+ * Set up and return an interpolation object using the given data
+ * @param background :: [input] Background data to interpolate
+ * @param workspace :: [input] Workspace to use for units
+ * @returns :: Interpolation object ready for use
+ */
+Kernel::Interpolation &CreateUserDefinedBackground::getInterpolator(
+    const API::ITableWorkspace_const_sptr &background,
+    const API::MatrixWorkspace_const_sptr &workspace) const {
+  Kernel::Interpolation lerp;
+  lerp.setMethod("linear");
+  lerp.setXUnit(workspace->getAxis(0)->unit()->unitID());
+  lerp.setYUnit(workspace->YUnit());
+
+  // Set up data from table
+  const auto xColumn = background->getColumn(0);
+  const auto yColumn = background->getColumn(1);
+  for (size_t i = 0; i < background->rowCount(); i++) {
+    double x = xColumn->cell<double>(i);
+    double y = yColumn->cell<double>(i);
+    lerp.addPoint(x, y);
+  }
+  return lerp;
+}
+
+/**
+ * Validate input properties:
+ * - Table of points must have two numeric columns (X, Y)
+ * - Table of points must contain at least two points
+ * - Input workspace must contain at least one spectrum and two points
+ * - Input workspace must have common bins in all spectra
+ * @returns :: map of property names to errors (empty if no errors)
+ */
+std::map<std::string, std::string>
+CreateUserDefinedBackground::validateInputs() {
+  std::map<std::string, std::string> errors;
+
+  const static std::string pointsProp = "BackgroundPoints",
+                           inputProp = "InputWorkspace";
+  const API::ITableWorkspace_const_sptr pointsTable = getProperty(pointsProp);
+  if (pointsTable) {
+    if (pointsTable->columnCount() != 2) {
+      errors[pointsProp] = "Table of points must have two columns (X, Y)";
+    }
+    for (size_t col = 0; col < pointsTable->columnCount(); col++) {
+      const std::string colType = pointsTable->getColumn(col)->type();
+      if (colType != "double" && colType != "int") {
+        errors[pointsProp] = "Table of points must have numeric columns";
+      }
+    }
+    if (pointsTable->rowCount() < 2) {
+      errors[pointsProp] = "Table of points must contain at least two points";
+    }
+  }
+
+  const API::MatrixWorkspace_const_sptr inputWS = getProperty(inputProp);
+  if (inputWS) {
+    if (inputWS->getNumberHistograms() == 0 || inputWS->blocksize() < 2) {
+      errors[inputProp] = "Input workspace must contain some data";
+    }
+    if (!inputWS->isCommonBins()) {
+      errors[inputProp] = "Input workspace must have common bins";
+    }
+  }
+
+  return errors;
 }
 
 } // namespace Algorithms
