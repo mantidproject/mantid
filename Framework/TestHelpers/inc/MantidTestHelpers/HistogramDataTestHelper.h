fc8bc3ca23c587a52435a51045ed505946a92787
Moore <lamar.moore@stfc.ac.uk>
2016-07-08 10:24:38 +0100

    Added HistogramDataTestHelper re #16612

---
+++ Framework/TestHelpers/inc/MantidTestHelpers/HistogramDataTestHelper.h
@@ -0,0 +1,21 @@
+/*********************************************************************************
+*  PLEASE READ THIS!!!!!!!
+*
+*  This header MAY ONLY be included from a test using Histogram types.
+*********************************************************************************/
+
+#ifndef HISTOGRAMDATATESTHELPER_H
+#define HISTOGRAMDATATESTHELPER_H
+
+#include <MantidHistogramData/FixedLengthVector.h>
+
+namespace HistogramDataTestHelper {
+template <class T>
+bool operator==(
+    const Mantid::HistogramData::detail::FixedLengthVector<T> &lhs,
+    const Mantid::HistogramData::detail::FixedLengthVector<T> &rhs) {
+  return lhs.rawData() == rhs.rawData();
+}
+
+} // namespace HistogramDataTestHelper
+#endif // HISTGRAMDATATESTHELPER_H
