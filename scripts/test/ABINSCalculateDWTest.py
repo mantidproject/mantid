dd7d66951475f27c181c9c05eb828572b9cad652
Krzysztof Dymkowski <kdymkowski84@gmail.com>
2016-07-14 11:26:13 +0100

    Add benchmark with benzene for CalculateDWTest.

--- scripts/test/ABINSCalculateDWTest.py
+++ scripts/test/ABINSCalculateDWTest.py
@@ -10,55 +10,79 @@ from AbinsModules import LoadCASTEP
 
 
 class ABINSCalculateDWTest(unittest.TestCase):
-    _core = "../ExternalData/Testing/Data/UnitTest/"
-    _dft_file = _core + "Si2-sc.phonon"
-    _temperature = 10 # 10 K
-    _good_DW = np.array([[[  4.69472745e-02,  -3.41878473e-03,    -3.41878473e-03], # comment: This values are calculated only from 4 k-points
-                          [ -3.41878473e-03,   7.99600176e-02,  -9.65204800e-03],
-                          [ -3.41878473e-03,  -9.65204800e-03,   4.96517869e+04]],
-                         [[  4.69472745e-02,  -3.41878473e-03,  -3.41878473e-03],
-                          [ -3.41878473e-03,   7.99600176e-02,  -9.65204800e-03],
-                          [ -3.41878473e-03,  -9.65204800e-03,   4.96517869e+04]]])
 
-    # simple tests
+    _core = "../ExternalData/Testing/Data/UnitTest/" # path to files
+    _temperature = 10 # 10 K,  temperature for the benchmark
+
+    # data
+    # Use case: one k-point
+    C6H6 = _core + "benzene"
+
+    #  Use case: many k-points
+    Si2 = _core + "Si2-sc"
+
+
+    #      simple tests
     def test_wrong_input(self):
+        filename = self.Si2 + ".phonon"
 
-        _good_data = self._get_good_data()
+        _castep_reader =  LoadCASTEP(input_DFT_filename=filename)
 
+        _good_data = _castep_reader.readPhononFile()
         # wrong filename
         with self.assertRaises(ValueError):
             _poor_tester = CalculateDW(filename=1, temperature=self._temperature, abins_data=_good_data)
 
         # wrong temperature
         with self.assertRaises(ValueError):
-            _poor_tester = CalculateDW(filename=self._dft_file, temperature=-10, abins_data=_good_data)
+            _poor_tester = CalculateDW(filename=filename, temperature=-10, abins_data=_good_data)
 
         # data from object of type AtomsData instead of object of type AbinsData
         bad_data = _good_data.extract()["atoms_data"]
         with self.assertRaises(ValueError):
-            _poor_tester = CalculateDW(filename=self._dft_file, temperature=self._temperature, abins_data=bad_data)
+            _poor_tester = CalculateDW(filename=filename, temperature=self._temperature, abins_data=bad_data)
 
 
+    #       main test
     def test_good_case(self):
+        self._good_case(name=self.C6H6)
+        self._good_case(name=self.Si2)
+
+
+
+    #       helper functions
+    def _good_case(self, name=None):
+
 
         # calculation of DW
-        _good_data = self._get_good_data()
-        _good_tester = CalculateDW(filename=self._dft_file, temperature=self._temperature, abins_data=_good_data)
+        _good_data = self._get_good_data(filename=name)
+
+        _good_tester = CalculateDW(filename=name + ".phonon", temperature=self._temperature, abins_data=_good_data["DFT"])
         calculated_data = _good_tester.getDW()
 
         # check if evaluated DW are correct
-        self.assertEqual(True, np.allclose(self._good_DW, calculated_data.extract()))
+        self.assertEqual(True, np.allclose(_good_data["DW"], calculated_data.extract()))
 
         # check loading DW
-        new_tester =  CalculateDW(filename=self._dft_file, temperature=self._temperature, abins_data=_good_data)
+        new_tester =  CalculateDW(filename=name + ".phonon", temperature=self._temperature, abins_data=_good_data["DFT"])
         loaded_data = new_tester.loadData()
         self.assertEqual(True, np.allclose(calculated_data.extract(), loaded_data.extract()))
 
-    # helper functions
-    def _get_good_data(self):
-        _CASTEP_reader = LoadCASTEP(input_DFT_filename=self._dft_file)
-        return _CASTEP_reader.readPhononFile()
 
+    def _get_good_data(self, filename=None):
+
+        _CASTEP_reader = LoadCASTEP(input_DFT_filename=filename + ".phonon")
+        _dw = self._prepare_data(filename=filename + "_DW.txt")
+
+        return {"DFT":_CASTEP_reader.readPhononFile(), "DW": _dw}
+
+
+    def _prepare_data(self, filename=None):
+        """Reads a correct values from ASCII file."""
+        correct_data = None
+        with open(filename) as data_file:
+            correct_data = json.loads(data_file.read().replace("\n"," "))
+        return correct_data
 
 
 if __name__ == '__main__':
