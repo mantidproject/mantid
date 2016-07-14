1bdc2f6712557ccdb3b18df03e53577d8c8104d8
Alex Buts <Alex.Buts@stfc.ac.uk>
2016-07-14 17:24:45 +0100

    Re #16848 Starting unit tests for changes

--- Framework/DataHandling/test/LoadMaskTest.h
+++ Framework/DataHandling/test/LoadMaskTest.h
@@ -9,6 +9,7 @@
 #include "MantidDataHandling/LoadMask.h"
 #include "MantidDataObjects/MaskWorkspace.h"
 #include "MantidTestHelpers/ScopedFileHelper.h"
+#include "MantidAPI/AlgorithmManager.h"
 
 using namespace Mantid;
 using namespace Mantid::DataHandling;
@@ -159,6 +160,76 @@ public:
     }
     std::cout << "Total " << errorcounts << " errors \n";
   }
+
+  void test_ISISWithRefWS() {
+      auto ws_creator =
+          AlgorithmManager::Instance().create("CreateSimulationWorkspace");
+      ws_creator->initialize();
+      ws_creator->setPropertyValue("Instrument", "MARI");
+      ws_creator->setPropertyValue("BinParams","100,100,300");
+      ws_creator->setPropertyValue("OutputWorkspace","testWS");
+      ws_creator->setPropertyValue("UnitX", "TOF");
+
+      ws_creator->execute();
+      Workspace_sptr source = ws_creator->getProperty("OutputWorkspace");
+
+      auto masker =
+          AlgorithmManager::Instance().create("MaskDetectors");
+      masker->initialize();
+      masker->setProperty("Workspace", source);
+      std::vector<int> masked_spectra(10);
+      masked_spectra[0] = 10;  masked_spectra[1] = 11;  masked_spectra[2] = 13;
+      masked_spectra[3] = 100; masked_spectra[4] = 110; masked_spectra[5] = 120;
+      masked_spectra[6] = 130; masked_spectra[7] = 140; masked_spectra[8] = 200;
+      masked_spectra[9] = 300;
+      masker->setProperty("SpectraList", masked_spectra);
+      masker->execute();
+
+      auto exporter =
+          AlgorithmManager::Instance().create("ExportSpectraMask");
+      exporter->initialize();
+
+
+            auto isisMaskFile =
+          genISISMaskingFile("isismask.msk", singlespectra, pairspectra);
+
+      Load
+
+      // 2. Run
+      LoadMask loadfile;
+      loadfile.initialize();
+
+      loadfile.setProperty("Instrument", "VULCAN");
+      loadfile.setProperty("InputFile", isisMaskFile.getFileName());
+      loadfile.setProperty("OutputWorkspace", "VULCAN_Mask_Detectors");
+
+      TS_ASSERT_EQUALS(loadfile.execute(), true);
+      DataObjects::MaskWorkspace_sptr maskws =
+          AnalysisDataService::Instance().retrieveWS<DataObjects::MaskWorkspace>(
+              "VULCAN_Mask_Detectors");
+
+      // 3. Check
+      size_t errorcounts = 0;
+      for (size_t iws = 0; iws < maskws->getNumberHistograms(); iws++) {
+          double y = maskws->dataY(iws)[0];
+          if (iws == 34 || iws == 1000 || iws == 2000 || (iws >= 36 && iws <= 39) ||
+              (iws >= 1001 && iws <= 1004)) {
+              // All these workspace index are masked
+              TS_ASSERT_DELTA(y, 1.0, 1.0E-5);
+          }
+          else {
+              // Unmasked
+              TS_ASSERT_DELTA(y, 0.0, 1.0E-5);
+              if (fabs(y) > 1.0E-5) {
+                  errorcounts++;
+                  std::cout << "Workspace Index " << iws
+                      << " has a wrong set on masks\n";
+              }
+          }
+      }
+      std::cout << "Total " << errorcounts << " errors \n";
+  }
+
 
   /*
    * Load "testingmasking.xml" and "regionofinterest.xml"
