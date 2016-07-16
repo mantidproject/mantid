ff4d1b478ad5239ec36dff0aef4307170571b349
Krzysztof Dymkowski <kdymkowski84@gmail.com>
2016-07-16 15:45:33 +0100

    Update documentation in CalculatePowder.py

--- scripts/AbinsModules/CalculatePowder.py
+++ scripts/AbinsModules/CalculatePowder.py
@@ -7,14 +7,14 @@ import Constants
 
 class CalculatePowder(IOmodule):
     """
-    Class for calculating means square displacements (MSD).
-    Means square displacements are in Hartree atomic units.
+    Class for calculating mean square displacements (MSD) for the powder use case.
+    Mean square displacements are in Hartree atomic units.
     Working equation is taken from http://atztogo.github.io/phonopy/thermal-displacement.html.
-    Additionally MSD are multiplied by coth(omega/2T)^2 for each frequency.  MSD can be directly used for calculation of
-    S(Q, omega) in case sample has form of powder.
+    Additionally Debye-Waller factors are calculated which are  MSD multiplied by coth(omega/2T)^2 for each frequency.
+    Obtained MSD and DW  can be directly used for calculation of S(Q, omega) in case sample has a form of powder.
     """
     def __init__(self, filename=None, abins_data=None, temperature=None):
-        super(CalculatePowder, self).__init__(input_filename=filename, group_name=Constants.MSD_data_group)
+        super(CalculatePowder, self).__init__(input_filename=filename, group_name=Constants.powder_data_group)
 
         if not isinstance(abins_data, AbinsData):
             raise ValueError("Object of AbinsData was expected.")
@@ -30,8 +30,8 @@ class CalculatePowder(IOmodule):
     def _calculate_msd(self):
         """
         MSD are calculated according to http://atztogo.github.io/phonopy/thermal-displacement.html.
-        MSD are expressed in Hartree atomic units. Additionally for every frequency they are multiplied by
-        coth(omega/2T)^2.
+        MSD are expressed in Hartree atomic units. Additionally DW are calculated. DW are MSD for every frequency
+        multiplied by coth(omega/2T)^2.
         """
 
         _data = self._abins_data.extract()
@@ -53,12 +53,12 @@ class CalculatePowder(IOmodule):
 
         for atom in range(num_atoms):
 
-            _msd_atom = {"msd": 0.0, "dw":0}
+            _powder_atom = {"msd": 0.0, "dw":0}
             _coth = 1.0 / np.tanh(_coth_factor * freq_hartree)
             _coth_square = np.multiply(_coth, _coth)
-            expm1 = np.expm1(freq_hartree * exp_factor)
+            expm1 = np.expm1(exp_factor * freq_hartree)
             one_over_freq = 1.0 / freq_hartree
-            two_over_freq = 2.0 / (np.multiply(freq_hartree, expm1))
+            two_over_freq_n = 2.0 / (np.multiply(freq_hartree, expm1))
 
             for k in range(num_k):
 
@@ -67,24 +67,31 @@ class CalculatePowder(IOmodule):
                 else: start = 0
 
                 for freq in range(start, num_freq):
-                    disp = displacements[k, atom, freq, :]
-                    _msd_atom["msd"] += np.vdot(disp, disp).real * (one_over_freq[k, freq] + two_over_freq[k, freq])
-                    _msd_atom["dw"] = _msd_atom["dw"] * _coth_square[k, freq]
-                _msd_atom["msd"] += _msd_atom["msd"] * weights[k]
-                _msd_atom["dw"] += _msd_atom["dw"] * weights[k]
 
-            _msd._append(num_atom=atom, msd_atom=_msd_atom * mass_hartree_factor[atom])
+                    disp = displacements[k, atom, freq, :]
+                    _powder_atom["msd"] += np.vdot(disp, disp).real * (one_over_freq[k, freq] + two_over_freq_n[k, freq])
+                    _powder_atom["dw"] = _powder_atom["msd"] * _coth_square[k, freq]
+                _powder_atom["msd"] += _powder_atom["msd"] * weights[k]
+                _powder_atom["dw"] += _powder_atom["dw"] * weights[k]
+
+            _powder_atom["msd"] += _powder_atom["msd"] * mass_hartree_factor[atom]
+            _powder_atom["dw"] += _powder_atom["dw"] * mass_hartree_factor[atom]
+            _msd._append(num_atom=atom, powder_atom=_powder_atom)
 
         return _msd
 
 
-    def getMSD(self):
+    def getPowder(self):
+        """
+        Calculates mean square displacements and Debye-Waller factors.  Saves both MSD and DW  to an hdf file.
+        @return: object of type PowderData with mean square displacements and Debye-Waller factors.
+        """
 
         data = self._calculate_msd()
 
         self.addAttribute("temperature", self._temperature)
         self.addAttribute("filename", self._input_filename)
-        self.addNumpyDataset("data", data.extract())
+        self.addStructuredDataset("data", data.extract())
 
         self.save()
 
@@ -92,11 +99,14 @@ class CalculatePowder(IOmodule):
 
 
     def loadData(self):
-
-        _data = self.load(list_of_numpy_datasets=["data"], list_of_attributes=["temperature"])
-        _num_atoms = _data["datasets"]["data"].shape[0]
+        """
+        Loads mean square displacements and Debye-Waller factors from an hdf file.
+        @return: object of type PowderData with mean square displacements and Debye-Waller factors.
+        """
+        _data = self.load(list_of_structured_datasets=["data"], list_of_attributes=["temperature"])
+        _num_atoms = _data["structured_datasets"]["data"].shape[0]
         _msd_data = PowderData(temperature=_data["attributes"]["temperature"], num_atoms=_num_atoms)
-        _msd_data.set(_data["datasets"]["data"])
+        _msd_data.set(_data["structured_datasets"]["data"])
 
         return _msd_data
 
