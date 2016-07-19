8184d46f6b3a332055a6386109cdaba85b95de86
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-07-19 10:16:05 +0100

    Refs #16894 Fix formatting

--- Framework/DataHandling/src/SaveCanSAS1D.cpp
+++ Framework/DataHandling/src/SaveCanSAS1D.cpp
@@ -18,17 +18,29 @@
 
 //-----------------------------------------------------------------------------
 namespace {
-void encode(std::string& data) {
+void encode(std::string &data) {
   std::string buffer;
   buffer.reserve(data.size());
   for (size_t pos = 0; pos != data.size(); ++pos) {
     switch (data[pos]) {
-    case '&':  buffer.append("&amp;");       break;
-    case '\"': buffer.append("&quot;");      break;
-    case '\'': buffer.append("&apos;");      break;
-    case '<':  buffer.append("&lt;");        break;
-    case '>':  buffer.append("&gt;");        break;
-    default:   buffer.append(&data[pos], 1); break;
+    case '&':
+      buffer.append("&amp;");
+      break;
+    case '\"':
+      buffer.append("&quot;");
+      break;
+    case '\'':
+      buffer.append("&apos;");
+      break;
+    case '<':
+      buffer.append("&lt;");
+      break;
+    case '>':
+      buffer.append("&gt;");
+      break;
+    default:
+      buffer.append(&data[pos], 1);
+      break;
     }
   }
   data.swap(buffer);
