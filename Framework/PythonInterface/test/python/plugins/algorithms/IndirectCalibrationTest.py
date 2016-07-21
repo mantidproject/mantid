142c53ebb84ed9c9a53ae9a202ffefafe18f83c6
Louise McCann <louise.mccann@stfc.ac.uk>
2016-07-21 11:38:09 +0100

    Fixed unit test for logs in Indirect Calibration
    Refs #15989

--- Framework/PythonInterface/test/python/plugins/algorithms/IndirectCalibrationTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/IndirectCalibrationTest.py
@@ -26,7 +26,7 @@ class IndirectCalibrationTest(unittest.TestCase):
 
         self.assertEqual(cal_ws.getNumberHistograms(), 51)
         self.assertEqual(cal_ws.blocksize(), 1)
-        self.assertEqual(cal_ws.run().getProperty('SampleLogs'))
+        self.assertEqual(cal_ws.run().getProperty('current_period'), 1)
 
 if __name__ == "__main__":
 	unittest.main()
