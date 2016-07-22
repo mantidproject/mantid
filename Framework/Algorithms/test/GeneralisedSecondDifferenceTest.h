6a5bf6282ce51dce73470061e9db1d3b09d5f087
Dimitar Tasev <dbt@aber.ac.uk>
2016-07-22 16:36:57 +0100

    Re #16959 performance test for GSD

--- Framework/Algorithms/test/GeneralisedSecondDifferenceTest.h
+++ Framework/Algorithms/test/GeneralisedSecondDifferenceTest.h
@@ -6,6 +6,9 @@
 #include "MantidAPI/AlgorithmManager.h"
 #include "MantidAPI/MatrixWorkspace.h"
 #include "MantidAPI/WorkspaceFactory.h"
+#include <MantidDataObjects/EventWorkspace.h>
+#include "../../DataObjects/test/EventWorkspaceTest.h"
+#include <MantidAlgorithms/GeneralisedSecondDifference.h>
 
 using namespace Mantid::API;
 
@@ -28,8 +31,8 @@ public:
     IAlgorithm_sptr gsd = Mantid::API::AlgorithmManager::Instance().create(
         "GeneralisedSecondDifference", 1);
 
-	Mantid::HistogramData::HistogramX x = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	Mantid::HistogramData::HistogramY y = {0.3,  0.3, 0.3,  0.47, 3.9,
+	std::vector<double> x = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	std::vector<double> y = {0.3,  0.3, 0.3,  0.47, 3.9,
                              10.3, 3.9, 0.47, 0.3,  0.3};
     MatrixWorkspace_sptr inputWs = WorkspaceFactory::Instance().create(
         "Workspace2D", 1, y.size(), y.size());
@@ -51,16 +54,58 @@ public:
     TS_ASSERT_EQUALS(outWs->getNumberHistograms(), 1);
     TS_ASSERT_EQUALS(outWs->blocksize(), 4);
 
-    x = outWs->x(0);
-    TS_ASSERT_EQUALS(x[0], 3);
-    TS_ASSERT_EQUALS(x[3], 6);
+    auto x1 = outWs->x(0);
+    TS_ASSERT_EQUALS(x1[0], 3);
+    TS_ASSERT_EQUALS(x1[3], 6);
 
-    y = outWs->y(0);
-    TS_ASSERT_DELTA(y[1], -7.0300, 0.0001);
-    TS_ASSERT_DELTA(y[2], -20.0000, 0.0001);
+    auto y1 = outWs->y(0);	
+    TS_ASSERT_DELTA(y1[1], -7.0300, 0.0001);
+    TS_ASSERT_DELTA(y1[2], -20.0000, 0.0001);
 
     Mantid::API::AnalysisDataService::Instance().remove("secondDiff");
   }
+};
+
+class GeneralisedSecondDifferenceTestPerformance : public CxxTest::TestSuite{
+public:
+	// This pair of boilerplate methods prevent the suite being created statically
+	// This means the constructor isn't called when running other tests
+	static GeneralisedSecondDifferenceTestPerformance *createSuite() {
+		return new GeneralisedSecondDifferenceTestPerformance();
+	}
+
+	static void destroySuite(GeneralisedSecondDifferenceTestPerformance *suite) { delete suite; }
+
+	void setUp() {
+		inputMatrix = WorkspaceCreationHelper::Create2DWorkspaceBinned(10000, 1000);
+		inputEvent =
+			WorkspaceCreationHelper::CreateEventWorkspace(10000, 1000, 5000);
+	}
+
+	void tearDown() {
+		Mantid::API::AnalysisDataService::Instance().remove("output");
+		Mantid::API::AnalysisDataService::Instance().remove("output2");
+	}
+
+	void testPerformanceMatrixWS() {
+		Mantid::Algorithms::GeneralisedSecondDifference genSecDiff;
+		genSecDiff.initialize();
+		genSecDiff.setProperty("InputWorkspace", inputMatrix);
+		genSecDiff.setPropertyValue("OutputWorkspace", "output");
+		genSecDiff.execute();
+	}
+
+	void testPerformanceEventWS() {
+		Mantid::Algorithms::GeneralisedSecondDifference genSecDiff;
+		genSecDiff.initialize();
+		genSecDiff.setProperty("InputWorkspace", inputEvent);
+		genSecDiff.setPropertyValue("OutputWorkspace", "output2");
+		genSecDiff.execute();
+	}
+
+private:
+	Mantid::API::MatrixWorkspace_sptr inputMatrix;
+	Mantid::DataObjects::EventWorkspace_sptr inputEvent;
 };
 
 #endif /* GENERALISEDSECONDDIFERENCETEST_H_ */
