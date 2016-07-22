3e3f10d263aca301b76a282668bea052abfc99c6
Samuel Jackson <samueljackson@outlook.com>
2016-07-22 15:44:09 +0100

    Refs #16977 Replace dynamic_casts

--- MantidQt/API/test/SignalRangeTest.h
+++ MantidQt/API/test/SignalRangeTest.h
@@ -44,6 +44,8 @@ private:
     MOCK_METHOD0(clearMDMasking, void());
     MOCK_CONST_METHOD0(getSpecialCoordinateSystem,
                        Mantid::Kernel::SpecialCoordinateSystem());
+    MOCK_CONST_METHOD0(isHistogramData,
+                       bool());
 
   private:
     MockMDWorkspace *doClone() const override {
