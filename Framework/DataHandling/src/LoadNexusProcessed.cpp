a8d1807ba685d01cb132658d8504a25e5c483502
David Fairbrother <DavidFair@users.noreply.github.com>
2016-07-25 12:04:24 +0100

    Re #17007 Changed exec and unit tests to use new hist interface

--- Framework/DataHandling/src/LoadNexusProcessed.cpp
+++ Framework/DataHandling/src/LoadNexusProcessed.cpp
@@ -252,7 +252,7 @@ Workspace_sptr LoadNexusProcessed::doAccelleratedMultiPeriodLoading(
 
   const size_t nHistograms = periodWorkspace->getNumberHistograms();
   for (size_t i = 0; i < nHistograms; ++i) {
-    periodWorkspace->setX(i, tempMatrixWorkspace->refX(i));
+    periodWorkspace->setSharedX(i, tempMatrixWorkspace->refX(i));
   }
 
   // We avoid using `openEntry` or similar here because they're just wrappers
@@ -315,11 +315,11 @@ Workspace_sptr LoadNexusProcessed::doAccelleratedMultiPeriodLoading(
 
     int64_t final(histIndex + blockSize);
     while (histIndex < final) {
-      MantidVec &Y = periodWorkspace->dataY(wsIndex);
+      auto &Y = periodWorkspace->mutableY(wsIndex);
       Y.assign(dataStart, dataEnd);
       dataStart += nChannels;
       dataEnd += nChannels;
-      MantidVec &E = periodWorkspace->dataE(wsIndex);
+      auto &E = periodWorkspace->mutableE(wsIndex);
       E.assign(errorStart, errorEnd);
       errorStart += nChannels;
       errorEnd += nChannels;
@@ -740,14 +740,14 @@ LoadNexusProcessed::loadEventEntry(NXData &wksp_cls, NXDouble &xbins,
 
       // Set the X axis
       if (this->m_shared_bins)
-        el.setX(this->m_xbins.cowData());
+        el.setSharedX(this->m_xbins.cowData());
       else {
         MantidVec x;
         x.resize(xbins.dim1());
         for (int i = 0; i < xbins.dim1(); i++)
           x[i] = xbins(static_cast<int>(wi), i);
         // Workspace and el was just created, so we can just set a new histogram
-        el.setX(make_cow<HistogramData::HistogramX>(x));
+        el.setSharedX(make_cow<HistogramData::HistogramX>(x));
       }
     }
 
@@ -1856,11 +1856,11 @@ void LoadNexusProcessed::loadBlock(NXDataSetTyped<double> &data,
 
   int64_t final(hist + blocksize);
   while (hist < final) {
-    MantidVec &Y = local_workspace->dataY(hist);
+    auto &Y = local_workspace->mutableY(hist);
     Y.assign(data_start, data_end);
     data_start += nchannels;
     data_end += nchannels;
-    MantidVec &E = local_workspace->dataE(hist);
+    auto &E = local_workspace->mutableE(hist);
     E.assign(err_start, err_end);
     err_start += nchannels;
     err_end += nchannels;
@@ -1878,7 +1878,7 @@ void LoadNexusProcessed::loadBlock(NXDataSetTyped<double> &data,
       xErrors_end += nxbins;
     }
 
-    local_workspace->setX(hist, m_xbins.cowData());
+    local_workspace->setSharedX(hist, m_xbins.cowData());
     ++hist;
   }
 }
@@ -1934,11 +1934,11 @@ void LoadNexusProcessed::loadBlock(NXDataSetTyped<double> &data,
 
   int64_t final(hist + blocksize);
   while (hist < final) {
-    MantidVec &Y = local_workspace->dataY(wsIndex);
+    auto &Y = local_workspace->mutableY(wsIndex);
     Y.assign(data_start, data_end);
     data_start += nchannels;
     data_end += nchannels;
-    MantidVec &E = local_workspace->dataE(wsIndex);
+    auto &E = local_workspace->mutableE(wsIndex);
     E.assign(err_start, err_end);
     err_start += nchannels;
     err_end += nchannels;
@@ -1955,7 +1955,7 @@ void LoadNexusProcessed::loadBlock(NXDataSetTyped<double> &data,
       xErrors_start += nxbins;
       xErrors_end += nxbins;
     }
-    local_workspace->setX(wsIndex, m_xbins.cowData());
+    local_workspace->setSharedX(wsIndex, m_xbins.cowData());
     ++hist;
     ++wsIndex;
   }
@@ -2017,11 +2017,11 @@ void LoadNexusProcessed::loadBlock(NXDataSetTyped<double> &data,
   }
 
   while (hist < final) {
-    MantidVec &Y = local_workspace->dataY(wsIndex);
+    auto &Y = local_workspace->mutableY(wsIndex);
     Y.assign(data_start, data_end);
     data_start += nchannels;
     data_end += nchannels;
-    MantidVec &E = local_workspace->dataE(wsIndex);
+    auto &E = local_workspace->mutableE(wsIndex);
     E.assign(err_start, err_end);
     err_start += nchannels;
     err_end += nchannels;
@@ -2038,7 +2038,7 @@ void LoadNexusProcessed::loadBlock(NXDataSetTyped<double> &data,
       xErrors_start += nxbins;
       xErrors_end += nxbins;
     }
-    MantidVec &X = local_workspace->dataX(wsIndex);
+    auto &X = local_workspace->mutableX(wsIndex);
     X.assign(xbin_start, xbin_end);
     xbin_start += nxbins;
     xbin_end += nxbins;
