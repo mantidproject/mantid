12d552ee60123debf4d6afab9cc28668a3fdb31e
Federico Montesino Pouzols <federico.montesino-pouzols@stfc.ac.uk>
2016-07-26 11:14:38 +0100

    Python 2/3 compatibility - EnggUtils.py, re #16739

--- scripts/Engineering/EnggUtils.py
+++ scripts/Engineering/EnggUtils.py
@@ -1,6 +1,7 @@
 #pylint: disable=invalid-name
 #pylint: disable=too-many-arguments
 # R0913: Too many arguments - strictly for write_ENGINX_GSAS_iparam_file()
+from __future__ import (absolute_import, division, print_function)
 from mantid.api import *
 import mantid.simpleapi as sapi
 
@@ -53,8 +54,8 @@ def read_in_expected_peaks(filename, expectedGiven):
             for a in readInArray:
                 for b in a:
                     exPeakArray.append(b)
-        except RuntimeError, ex:
-            raise RuntimeError("Error while reading file of expected peaks '%s': %s" % (filename, ex))
+        except RuntimeError as exc:
+            raise RuntimeError("Error while reading file of expected peaks '%s': %s" % (filename, exc))
 
         if not exPeakArray:
             # "File could not be read. Defaults in alternative option used."
