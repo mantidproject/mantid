3c1c79e2e314ee22420068af0aa1c561aa263d20
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2016-07-27 15:40:21 +0100

    set up SaveDiffFittingAscii.h initial setup
    refs #17048

---
+++ Framework/DataHandling/inc/MantidDataHandling/SaveDiffFittingAscii.h
@@ -0,0 +1,44 @@
+#ifndef MANTID_DATAHANDLING_SAVEDIFFFITTINGASCII_H_
+#define MANTID_DATAHANDLING_SAVEDIFFFITTINGASCII_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/ITableWorkspace_fwd.h"
+
+namespace Mantid {
+namespace DataHandling {
+
+class DLLExport SaveDiffFittingAscii : public Mantid::API::Algorithm {
+public:
+  /// (Empty) Constructor
+  SaveDiffFittingAscii();
+
+  /// Algorithm's name
+  const std::string name() const override { return "SaveDiffFittingAscii"; }
+
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Saves the results after carrying out single peak fitting process "
+           "or running "
+           "EnggFitPeaks v1 algorithm to ASCII file";
+  }
+
+  /// Algorithm's version
+  int version() const override { return (1); }
+
+  /// Algorithm's category for identification overriding a virtual method
+  const std::string category() const override { return "DataHandling\\Text"; }
+
+private:
+  /// Initialisation code
+  void init() override;
+
+  /// Execution code
+  void exec() override;
+};
+} // namespace DataHandling
+} // namespace Mantid
+
+#endif /*  MANTID_DATAHANDLING_SAVEDIFFFITTINGASCII_H_  */
