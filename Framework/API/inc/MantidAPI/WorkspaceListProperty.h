d27d4476db7a92331d407cdd651ca9ac4602995b
Moore <lamar.moore@stfc.ac.uk>
2016-08-01 14:08:36 +0100

    Removed all name and ADS references re #17035

--- Framework/API/inc/MantidAPI/WorkspaceListProperty.h
+++ Framework/API/inc/MantidAPI/WorkspaceListProperty.h
@@ -1,11 +1,10 @@
 #ifndef MANTID_API_WORKSPACELISTPROPERTY_H_
 #define MANTID_API_WORKSPACELISTPROPERTY_H_
 
-#include <vector>
+#include "MantidAPI/WorkspaceProperty.h"
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
-#include "MantidKernel/System.h"
-#include "MantidAPI/WorkspaceProperty.h"
+#include <vector>
 
 namespace Mantid {
 namespace API {
@@ -47,63 +46,6 @@ public:
   typedef std::vector<boost::shared_ptr<TYPE>> WorkspaceListPropertyType;
   typedef Kernel::PropertyWithValue<WorkspaceListPropertyType> SuperClass;
 
-  /** Constructor.
-  *  Sets the property and workspace names but initialises the workspace
-  * pointers to null.
-  *  @param name :: The name to assign to the property
-  *  @param wsNames :: The names of the workspaces
-  *  @param direction :: Whether this is a Direction::Input, Direction::Output
-  * or Direction::InOut (Input & Output) workspace
-  *  @param validator :: The (optional) validator to use for this property
-  *  @throw std::out_of_range if the direction argument is not a member of the
-  * Direction enum (i.e. 0-2)
-  */
-  explicit WorkspaceListProperty(
-      const std::string &name, const std::string &wsNames,
-      const unsigned int direction = Mantid::Kernel::Direction::Input,
-      Mantid::Kernel::IValidator_sptr validator =
-          Mantid::Kernel::IValidator_sptr(new Kernel::NullValidator))
-      : Mantid::Kernel::PropertyWithValue<WorkspaceListPropertyType>(
-            name, WorkspaceListPropertyType(0), validator, direction),
-        m_optional(PropertyMode::Mandatory), m_workspaceNames(0)
-  // ,m_workspaceName( wsName ), m_initialWSName( wsName ),
-  // m_optional(PropertyMode::Mandatory), m_locking(LockMode::Lock)
-  {
-    m_workspaceNames = namesToVector(wsNames);
-    const std::string errorMsg = syncWorkspaces();
-    if (!errorMsg.empty()) {
-      throw std::invalid_argument(errorMsg);
-    }
-  }
-
-  /** Constructor.
-  *  Sets the property and workspace names but initialises the workspace
-  * pointers to null.
-  *  @param name :: The name to assign to the property
-  *  @param wsNames :: The name of the workspace
-  *  @param direction :: Whether this is a Direction::Input, Direction::Output
-  * or Direction::InOut (Input & Output) workspace
-  *  @param optional :: Flag to determine whether this property is optional or
-  * not.
-  *  @param validator :: The (optional) validator to use for this property
-  *  @throw std::out_of_range if the direction argument is not a member of the
-  * Direction enum (i.e. 0-2)
-  */
-  explicit WorkspaceListProperty(
-      const std::string &name, const std::string &wsNames,
-      const unsigned int direction, const PropertyMode::Type optional,
-      Mantid::Kernel::IValidator_sptr validator =
-          Mantid::Kernel::IValidator_sptr(new Kernel::NullValidator))
-      : Mantid::Kernel::PropertyWithValue<WorkspaceListPropertyType>(
-            name, WorkspaceListPropertyType(0), validator, direction),
-        m_optional(optional), m_workspaceNames(0) {
-    m_workspaceNames = namesToVector(wsNames);
-    const std::string errorMsg = syncWorkspaces();
-    if (!errorMsg.empty()) {
-      throw std::invalid_argument(errorMsg);
-    }
-  }
-
   /**
   * WorkspaceListProperty
   * @param name: Name of the property
@@ -120,16 +62,17 @@ public:
           Mantid::Kernel::IValidator_sptr(new Kernel::NullValidator))
       : Mantid::Kernel::PropertyWithValue<WorkspaceListPropertyType>(
             name, WorkspaceListPropertyType(0), validator, direction),
-        m_optional(optional), m_workspaceNames(0) {
+        m_optional(optional) {
     Kernel::PropertyWithValue<WorkspaceListPropertyType>::m_value = workspaces;
-    syncNames();
+    auto errorMsg = isValid();
+    if (!errorMsg.empty())
+      throw std::invalid_argument(errorMsg);
   }
 
   /// Copy constructor, the default name stored in the new object is the same as
   /// the default name from the original object
   WorkspaceListProperty(const WorkspaceListProperty &right)
-      : SuperClass(right), m_optional(right.m_optional),
-        m_workspaceNames(right.m_workspaceNames) {}
+      : SuperClass(right), m_optional(right.m_optional) {}
 
   /**
   * Assignment overload
@@ -138,12 +81,12 @@ public:
   WorkspaceListProperty &operator=(const WorkspaceListProperty &right) {
     if (&right != this) {
       SuperClass::operator=(right);
-      m_workspaceNames = right.m_workspaceNames;
       m_optional = right.m_optional;
-      syncWorkspaces();
     }
     return *this;
   }
+
+  std::string value() const override { return ""; }
 
   /**
   * Assignment overload
@@ -160,16 +103,6 @@ public:
     return new WorkspaceListProperty<TYPE>(*this);
   }
 
-  /** Set the name of the workspace.
-  *  Also tries to retrieve it from the AnalysisDataService.
-  *  @param value :: The new name for the workspace
-  *  @return
-  */
-  virtual std::string setValue(const std::string &value) {
-    m_workspaceNames = namesToVector(value);
-    return syncWorkspaces();
-  }
-
   /** Checks whether the entered workspaces are valid.
   *  To be valid, in addition to satisfying the conditions of any validators.
   * Input ones must point to
@@ -177,69 +110,23 @@ public:
   *  @returns A user level description of the problem or "" if it is valid.
   */
   std::string isValid() const {
-    // start with the no error condition
-    std::string error = "";
-    // If an input (or inout) workspace, must point to something, although it
-    // doesn't have to have a name
-    // unless it's optional
-    if (this->direction() == Kernel::Direction::Input ||
-        this->direction() == Kernel::Direction::InOut) {
-      for (auto it = m_workspaceNames.begin(); it != m_workspaceNames.end();
-           ++it) {
-        const std::string wsName = *it;
-        if (!AnalysisDataService::Instance().doesExist(wsName)) {
-          /*
-          * If this property is being used with the optional flag, and the
-          * arguments look like the default ones. then continue.
-          */
-          if (this->isOptional() && m_workspaceNames.size() == 1 &&
-              wsName.empty()) {
-            continue;
-          } else {
-            return missingWorkspaceErrorMessage(*it);
-          }
-        }
-      }
-    }
-
+    std::string error;
     // Run the validator on each workspace held. This must be done after the
     // point that we have established that the workspaces exist.
     for (int i = 0; i < SuperClass::m_value.size(); ++i) {
       error = SuperClass::m_validator->isValid(SuperClass::m_value[i]);
       if (!error.empty()) {
-        return error;
+        break;
       }
     }
     return error;
   }
 
-  /**
-  * Overriden value getter. Export contents as a comma separated string of
-  * workspaces.
-  * @return Comma separated string of workspace names.
-  */
-  std::string value() const override {
-    this->syncNames();
-    std::stringstream result;
-    std::size_t vsize = m_workspaceNames.size();
-    for (std::size_t i = 0; i < vsize; ++i) {
-      result << m_workspaceNames[i];
-      if (i + 1 != vsize)
-        result << ",";
-    }
-    return result.str();
-  }
+  const WorkspaceListPropertyType &value() { return SuperClass::m_value; }
 
   /// Is the input workspace property optional?
   virtual bool isOptional() const {
     return m_optional == PropertyMode::Optional;
-  }
-
-  /**
-  * Getter for the workspace names.
-  */
-  std::vector<std::string> getWorkspaceNames() const {
-    return m_workspaceNames;
   }
 
   /**
@@ -248,72 +135,10 @@ public:
   virtual ~WorkspaceListProperty() {}
 
 private:
-  /**
-  * Sync workspaces. Using the collection of workspaces, record/overwrite the
-  * workspace names.
-  */
-  std::string syncWorkspaces() {
-    // Try and get the workspace from the ADS, but don't worry if we can't
-    WorkspaceListPropertyType temp;
-    for (auto it = m_workspaceNames.begin(); it != m_workspaceNames.end();
-         ++it) {
-      try {
-        temp.push_back(AnalysisDataService::Instance().retrieveWS<TYPE>(*it));
-      } catch (Kernel::Exception::NotFoundError &) {
-        // Set to null property if not found
-        this->clear();
-        // the workspace name is not reset here, however.
-      }
-    }
-    SuperClass::m_value = temp;
-    return isValid();
-  }
-
-  /**
-  * Format an error message for a missing workspace with the provided name
-  * @param wsName: Name of the workspace which is missing from the ADS.
-  */
-  std::string missingWorkspaceErrorMessage(const std::string &wsName) const {
-    std::stringstream stream;
-    stream << "Workspace called '" << wsName
-           << "' is not in the Workspace List and is unknown to Mantid";
-    return stream.str();
-  }
-
-  /**
-  * Using the collection of workspaces, sync the internal list of workspace
-  * names.
-  */
-  void syncNames() const {
-    std::vector<std::string> temp;
-    for (auto it = SuperClass::m_value.begin(); it != SuperClass::m_value.end();
-         ++it) {
-      const std::string wsName = (*it)->name();
-      temp.push_back((*it)->name());
-    }
-    m_workspaceNames = temp;
-  }
-
   void clear() { SuperClass::m_value = WorkspaceListPropertyType(0); }
-
-  /**
-  * Helper method to split a comma separated string into a vector of strings.
-  * @param Comma separated string to split up.
-  * @return vector of strings.
-  */
-  std::vector<std::string> namesToVector(std::string names) {
-    names.erase(
-        std::remove_if(names.begin(), names.end(), (int (*)(int))std::isspace),
-        names.end());
-    std::vector<std::string> splitNames;
-    boost::split(splitNames, names, boost::is_any_of(","));
-    return splitNames;
-  }
 
   /// Flag indicating whether the type is optional or not.
   PropertyMode::Type m_optional;
-  /// Keys to the workspaces in the ADS
-  mutable std::vector<std::string> m_workspaceNames;
 };
 
 } // namespace API
--- Framework/API/test/WorkspaceListPropertyTest.h
+++ Framework/API/test/WorkspaceListPropertyTest.h
@@ -3,19 +3,19 @@
 
 #include <cxxtest/TestSuite.h>
 
-#include "MantidAPI/WorkspaceListProperty.h"
-#include "MantidAPI/Workspace.h"
-#include "MantidAPI/MatrixWorkspace.h"
-#include "MantidAPI/IMDHistoWorkspace.h"
-#include "MantidAPI/IMDEventWorkspace.h"
-#include "MantidAPI/ITableWorkspace.h"
-#include "MantidAPI/IEventWorkspace.h"
 #include "MantidAPI/Algorithm.h"
-#include "MantidTestHelpers/FakeObjects.h"
-#include "MantidKernel/IValidator.h"
+#include "MantidAPI/IEventWorkspace.h"
+#include "MantidAPI/IMDEventWorkspace.h"
+#include "MantidAPI/IMDHistoWorkspace.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/Workspace.h"
+#include "MantidAPI/WorkspaceListProperty.h"
 #include "MantidKernel/DataItem.h"
-#include <vector>
+#include "MantidKernel/IValidator.h"
+#include "MantidTestHelpers/FakeObjects.h"
 #include <gmock/gmock.h>
+#include <vector>
 
 using namespace Mantid::API;
 using namespace Mantid::Kernel;
@@ -45,7 +45,7 @@ private:
 
     virtual const std::string name() const { return "MyAlgorithm"; }
 
-	const std::string summary() const override { return "MyAlgorithm helper."; }
+    const std::string summary() const override { return "MyAlgorithm helper."; }
 
     virtual void init() {
       declareProperty(std::make_unique<WorkspaceListProperty<Workspace>>(
@@ -71,98 +71,46 @@ public:
   // Functional Testing
   //------------------------------------------------------------------------------
 
-  void test_construction_minimalistic() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("MyWorkspace", boost::make_shared<WorkspaceTester>());
+  void test_constructor() {
+    auto list = createWorkspaceList();
 
-    TS_ASSERT_THROWS_NOTHING(WorkspaceListProperty<Workspace>(
-        "MyWorkspaceProperty", "MyWorkspace", Direction::Input));
-    TS_ASSERT_THROWS_NOTHING(WorkspaceListProperty<MatrixWorkspace>(
-        "MyWorkspaceProperty", "MyWorkspace", Direction::Input));
-    TS_ASSERT_THROWS_NOTHING(WorkspaceListProperty<IMDHistoWorkspace>(
-        "MyWorkspaceProperty", "MyWorkspace", Direction::Input));
-    TS_ASSERT_THROWS_NOTHING(WorkspaceListProperty<IMDEventWorkspace>(
-        "MyWorkspaceProperty", "MyWorkspace", Direction::Input));
-    TS_ASSERT_THROWS_NOTHING(WorkspaceListProperty<ITableWorkspace>(
-        "MyWorkspaceProperty", "MyWorkspace", Direction::Input));
-
-    ads.remove("MyWorkspace");
-  }
-
-  void test_construction() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("MyWorkspace1", boost::make_shared<WorkspaceTester>());
-    ads.add("MyWorkspace2", boost::make_shared<WorkspaceTester>());
-
-    WorkspaceListProperty<Workspace> workspaceListProperty(
-        "MyWorkspaceProperty", "MyWorkspace1, MyWorkspace2", Direction::Input,
-        PropertyMode::Optional);
-    auto workspaceNames = workspaceListProperty.getWorkspaceNames();
-    TS_ASSERT_EQUALS(2, workspaceNames.size());
-    TS_ASSERT_EQUALS(workspaceNames.front(), "MyWorkspace1");
-    TS_ASSERT_EQUALS(workspaceNames.back(), "MyWorkspace2");
-    TS_ASSERT_EQUALS(workspaceListProperty.isOptional(), true)
-
-    ads.remove("MyWorkspace1");
-    ads.remove("MyWorkspace2");
-  }
-
-  void test_construct_from_workspaces() {
-    auto a = boost::make_shared<WorkspaceTester>();
-    auto b = boost::make_shared<WorkspaceTester>();
-    AnalysisDataService::Instance().add("a", a);
-    AnalysisDataService::Instance().add("b", b);
-
-    WorkspaceListProperty<Workspace>::WorkspaceListPropertyType vec;
-    vec.push_back(a);
-    vec.push_back(b);
-
-    WorkspaceListProperty<Workspace> *property;
-    TS_ASSERT_THROWS_NOTHING(property = new WorkspaceListProperty<Workspace>(
-                                 "MyWorkspaceProperty", vec, Direction::Input,
-                                 PropertyMode::Mandatory));
-
-    TS_ASSERT_EQUALS(2, property->getWorkspaceNames().size());
-
-    delete property;
-    AnalysisDataService::Instance().remove("a");
-    AnalysisDataService::Instance().remove("b");
+    TS_ASSERT_THROWS_NOTHING(std::make_unique<WorkspaceListProperty<Workspace>>(
+        WorkspaceListProperty<Workspace>("MyWorkspaceProperty", list,
+                                         Direction::Input,
+                                         PropertyMode::Mandatory)));
   }
 
   void test_contruct_as_optional() {
-    const std::string defaultArg = "";
-    TS_ASSERT_THROWS(WorkspaceListProperty<Workspace>("MyProperty", defaultArg,
-                                                      Direction::Input,
-                                                      PropertyMode::Mandatory),
-                     std::invalid_argument);
-    TS_ASSERT_THROWS_NOTHING(WorkspaceListProperty<Workspace>(
-        "MyProperty", defaultArg, Direction::Input, PropertyMode::Optional))
+    auto list = std::vector<Workspace_sptr>();
+
+    TS_ASSERT_THROWS_NOTHING(std::make_unique<WorkspaceListProperty<Workspace>>(
+        WorkspaceListProperty<Workspace>("MyWorkspaceProperty", list,
+                                         Direction::Input,
+                                         PropertyMode::Optional)));
   }
 
   void test_copy_construction() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("a1", boost::make_shared<WorkspaceTester>());
-    ads.add("a2", boost::make_shared<WorkspaceTester>());
+    auto list = createWorkspaceList();
 
-    WorkspaceListProperty<Workspace> a("PropA", "a1, a2", Direction::Input,
+    WorkspaceListProperty<Workspace> a("PropA", list, Direction::Input,
                                        PropertyMode::Optional);
     WorkspaceListProperty<Workspace> b(a);
-    TS_ASSERT_EQUALS(a.getWorkspaceNames(), b.getWorkspaceNames());
-    TS_ASSERT_EQUALS(a.isOptional(), b.isOptional());
 
-    AnalysisDataService::Instance().remove("a1");
-    AnalysisDataService::Instance().remove("a2");
+    TS_ASSERT_EQUALS(list, a.value());
+    TS_ASSERT_EQUALS(a.value(), b.value());
+    TS_ASSERT_EQUALS(a.isOptional(), b.isOptional());
   }
 
   void test_assignment() {
-    auto a = boost::make_shared<WorkspaceTester>();
-    AnalysisDataService::Instance().add("a", a);
-    WorkspaceListProperty<Workspace> propA("PropA", "a", Direction::Input,
+    auto list = createWorkspaceList();
+    WorkspaceListProperty<Workspace> propA("PropA", list, Direction::Input,
                                            PropertyMode::Mandatory);
-    WorkspaceListProperty<Workspace> propB("PropB", "", Direction::Input,
-                                           PropertyMode::Optional);
+    WorkspaceListProperty<Workspace> propB(
+        "PropB", std::vector<Workspace_sptr>(), Direction::Input,
+        PropertyMode::Optional);
     propB = propA;
-    TS_ASSERT_EQUALS(propA.getWorkspaceNames(), propB.getWorkspaceNames());
+    TS_ASSERT_EQUALS(list, propA.value());
+    TS_ASSERT_EQUALS(propA.value(), propB.value());
     TS_ASSERT_EQUALS(propA.isOptional(), propB.isOptional());
     AnalysisDataService::Instance().remove("a");
   }
@@ -174,25 +122,23 @@ public:
     boost::shared_ptr<MockValidator> validator =
         boost::make_shared<MockValidator>();
     EXPECT_CALL(*validator.get(), check(_))
-        .Times(3)
+        .Times(2)
         .WillRepeatedly(Return(okString));
 
     auto ws = boost::make_shared<WorkspaceTester>();
-    AnalysisDataService::Instance().add("ws", ws);
-    WorkspaceListProperty<Workspace> prop("Prop", "ws, ws, ws",
-                                          Direction::Input,
+
+    auto list = createWorkspaceList();
+    WorkspaceListProperty<Workspace> prop("Prop", list, Direction::Input,
                                           PropertyMode::Mandatory, validator);
 
     TSM_ASSERT("Should repeatedly call the validator",
                Mock::VerifyAndClearExpectations(validator.get()));
-    AnalysisDataService::Instance().remove(ws->name());
   }
 
   void test_clone() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("a1", boost::make_shared<WorkspaceTester>());
+    auto list = createWorkspaceList();
     WorkspaceListProperty<Workspace> *propOne =
-        new WorkspaceListProperty<Workspace>("PropA", "a1", Direction::Input,
+        new WorkspaceListProperty<Workspace>("PropA", list, Direction::Input,
                                              PropertyMode::Optional);
     WorkspaceListProperty<Workspace> *propTwo = propOne->clone();
     TS_ASSERT_DIFFERS(propOne, propTwo);
@@ -204,101 +150,87 @@ public:
   // Integration type testing. Test that the Property works nicely via the
   // PropertyManager interfaces (such as Algorithm).
   //------------------------------------------------------------------------------
-  void test_setPropertyValue_throws_with_workspace_not_in_ADS() {
-    MyAlgorithm alg;
-    alg.initialize();
-    TSM_ASSERT_THROWS("Workspaces are not in the ADS, so should throw",
-                      alg.setPropertyValue("MyProperty", "a"),
-                      std::invalid_argument &);
-  }
-
-  void test_setPropertyValue_with_single_workspace() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("a", boost::make_shared<WorkspaceTester>());
-
-    MyAlgorithm alg;
-    alg.initialize();
-    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("MyProperty", "a"));
-
-    ads.remove("a");
-  }
-
-  void test_septPropertyValue_with_multiple_workspaces() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("a", boost::make_shared<WorkspaceTester>());
-    ads.add("b", boost::make_shared<WorkspaceTester>());
-
-    MyAlgorithm alg;
-    alg.initialize();
-    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("MyProperty", "a, b"));
-
-    ads.remove("a");
-    ads.remove("b");
-  }
-
-  void test_septPropertyValue_with_multiple_workspaces_last_doesnt_exist() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
-    ads.add("a", boost::make_shared<WorkspaceTester>());
-
-    MyAlgorithm alg;
-    alg.initialize();
-    TSM_ASSERT_THROWS("Not ALL Workspaces are in the ADS, so should throw",
-                      alg.setPropertyValue("MyProperty", "a, b"),
-                      std::invalid_argument &);
-
-    ads.remove("a");
-  }
-
-  void test_set_and_get_Property_as_vector() {
-    auto a = boost::make_shared<WorkspaceTester>();
-    auto b = boost::make_shared<WorkspaceTester>();
-
+  void test_set_and_get_Property() {
     MyAlgorithm alg;
     alg.initialize();
 
-    auto vecIn = std::vector<Workspace_sptr>();
-    vecIn.push_back(a);
-    vecIn.push_back(b);
-
-    alg.setProperty("MyProperty", vecIn);
+    auto ilist = createWorkspaceList();
+    alg.setProperty("MyProperty", ilist);
     // Now fetch the property.
-    std::vector<boost::shared_ptr<DataItem>> vecOut = alg.getProperty("MyProperty");
+    std::vector<boost::shared_ptr<DataItem>> olist =
+        alg.getProperty("MyProperty");
 
-    TS_ASSERT_EQUALS(vecIn.size(), vecOut.size());
-
-	auto testVec = std::vector<Workspace_sptr>();
-
-	for (auto &i : vecOut) {
-		testVec.push_back(boost::dynamic_pointer_cast<WorkspaceTester>(i));
-	}
-
-    TS_ASSERT_EQUALS(vecIn, testVec);
+    validateDataItemList(ilist, olist);
   }
 
-  void test_getPropertyValue() {
-    AnalysisDataServiceImpl &ads = AnalysisDataService::Instance();
+  void test_multiple_workspace_types() {
     auto a = boost::make_shared<WorkspaceTester>();
-    auto b = boost::make_shared<WorkspaceTester>();
-    ads.add("a", a);
-    ads.add("b", b);
+    auto b = boost::make_shared<TableWorkspaceTester>();
+
+    auto ilist = std::vector<Workspace_sptr>();
+
+    ilist.push_back(a);
+    ilist.push_back(b);
 
     MyAlgorithm alg;
     alg.initialize();
 
-    auto vecIn = std::vector<Workspace_sptr>();
-    vecIn.push_back(a);
-    vecIn.push_back(b);
+    alg.setProperty("MyProperty", ilist);
 
-    alg.setProperty("MyProperty", vecIn);
-    //Now fetch the property.
-    std::string outStr = alg.getPropertyValue("MyProperty");
+    std::vector<boost::shared_ptr<DataItem>> olist =
+        alg.getProperty("MyProperty");
 
-    TS_ASSERT_EQUALS("a,b", outStr);
+    TS_ASSERT_EQUALS(olist.size(), ilist.size());
 
-    ads.remove("a");
-    ads.remove("b");
+    auto oa = boost::dynamic_pointer_cast<WorkspaceTester>(olist[0]);
+    auto ob = boost::dynamic_pointer_cast<TableWorkspaceTester>(olist[1]);
+
+    std::vector<Workspace_sptr> tmpList{oa, ob};
+
+    TS_ASSERT_EQUALS(ilist, tmpList);
+  }
+
+  void test_return_property_as_string() {
+	  auto ilist = createWorkspaceList();
+	  MyAlgorithm alg;
+	  alg.initialize();
+
+	  alg.setProperty("MyProperty", ilist);
+
+	  std::string strlist = alg.getProperty("MyProperty");
+  }
+
+private:
+  std::vector<Workspace_sptr> createWorkspaceList() const {
+    auto a = boost::make_shared<WorkspaceTester>();
+    auto b = boost::make_shared<WorkspaceTester>();
+    auto list = std::vector<Workspace_sptr>();
+    list.push_back(a);
+    list.push_back(b);
+
+    return list;
+  }
+
+  std::vector<Workspace_sptr>
+  convertDataItemList(const std::vector<boost::shared_ptr<DataItem>> &list) {
+    auto newList = std::vector<Workspace_sptr>();
+
+    for (auto &i : list) {
+      newList.push_back(boost::dynamic_pointer_cast<WorkspaceTester>(i));
+    }
+
+    return newList;
+  }
+
+  void
+  validateDataItemList(const std::vector<Workspace_sptr> &wkspList,
+                       const std::vector<boost::shared_ptr<DataItem>> &diList) {
+    TS_ASSERT_EQUALS(wkspList.size(), diList.size());
+
+    auto newList = convertDataItemList(diList);
+
+    TS_ASSERT_EQUALS(wkspList, newList);
   }
 };
-
 
 #endif /* MANTID_API_WORKSPACELISTPROPERTYTEST_H_ */
