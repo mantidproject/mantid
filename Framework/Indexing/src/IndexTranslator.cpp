d04d0c6f4ce5e3eedb8ef1052e40e32741b480c8
Simon Heybrock <simon.heybrock@esss.se>
2016-08-03 09:18:26 +0200

    Re #17114. Added basic IndexTranslator.

---
+++ Framework/Indexing/src/IndexTranslator.cpp
@@ -0,0 +1,36 @@
+#include "MantidIndexing/IndexTranslator.h"
+
+#include <algorithm>
+#include <functional>
+
+namespace Mantid {
+namespace Indexing {
+
+IndexTranslator::IndexTranslator(
+    std::vector<int32_t> &&spectrumNumbers,
+    std::vector<std::vector<int32_t>> &&detectorIDs) {
+  if (spectrumNumbers.size() != detectorIDs.size())
+    throw std::runtime_error("IndexTranslator: Size mismatch between spectrum "
+                             "number vector and detector ID vector");
+  if (!std::is_sorted(spectrumNumbers.begin(), spectrumNumbers.end(),
+                      std::less_equal<int32_t>()))
+    throw std::runtime_error(
+        "IndexTranslator: Spectrum numbers are not strictly ascending");
+
+  m_spectrumNumbers = std::move(spectrumNumbers);
+  m_detectorIDs = std::move(detectorIDs);
+  for (auto &ids : m_detectorIDs) {
+    std::sort(ids.begin(), ids.end());
+    ids.erase(std::unique(ids.begin(), ids.end()), ids.end());
+  }
+}
+
+const std::vector<int32_t> &IndexTranslator::spectrumNumbers() {
+  return m_spectrumNumbers;
+}
+const std::vector<std::vector<int32_t>> &IndexTranslator::detectorIDs() {
+  return m_detectorIDs;
+}
+
+} // namespace Indexing
+} // namespace Mantid
---
+++ Framework/Indexing/test/IndexTranslatorTest.h
@@ -0,0 +1,60 @@
+#ifndef MANTID_INDEXING_INDEXTRANSLATORTEST_H_
+#define MANTID_INDEXING_INDEXTRANSLATORTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidIndexing/IndexTranslator.h"
+
+using Mantid::Indexing::IndexTranslator;
+
+class IndexTranslatorTest : public CxxTest::TestSuite {
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static IndexTranslatorTest *createSuite() {
+    return new IndexTranslatorTest();
+  }
+  static void destroySuite(IndexTranslatorTest *suite) { delete suite; }
+
+  void test_constructor() {
+    TS_ASSERT_THROWS_NOTHING(IndexTranslator({1, 2, 3}, {{0}, {1}, {2}}));
+  }
+
+  void test_constructor_size_mismatch() {
+    TS_ASSERT_THROWS(IndexTranslator({1, 2, 3}, {{0}, {1}}),
+                     std::runtime_error);
+  }
+
+  void test_constructor_not_strictly_ascending() {
+    TS_ASSERT_THROWS(IndexTranslator({1, 2, 1}, {{0}, {1}, {2}}),
+                     std::runtime_error);
+    TS_ASSERT_THROWS(IndexTranslator({1, 2, 2}, {{0}, {1}, {2}}),
+                     std::runtime_error);
+  }
+
+  void test_spectrumNumbers() {
+    std::vector<int32_t> spectrumNumbers{1, 2, 3};
+    auto ptr = spectrumNumbers.data();
+    IndexTranslator t(std::move(spectrumNumbers), {{0}, {1}, {2}});
+    auto &nums = t.spectrumNumbers();
+    TS_ASSERT_EQUALS(nums, (std::vector<int32_t>{1, 2, 3}));
+    // Was data moved?
+    TS_ASSERT_EQUALS(nums.data(), ptr);
+  }
+
+  void test_detectorIDs() {
+    std::vector<std::vector<int32_t>> detectorIDs{{1}, {2, 1, 2}, {4, 3}};
+    IndexTranslator t({1, 2, 3}, std::move(detectorIDs));
+    TS_ASSERT_EQUALS(t.detectorIDs(),
+                     (std::vector<std::vector<int32_t>>{{1}, {1, 2}, {3, 4}}));
+  }
+
+  void test_detectorIDs_moved_if_sorted_and_unique() {
+    std::vector<std::vector<int32_t>> detectorIDs{{1}, {1, 2}, {3, 4}};
+    auto ptr = detectorIDs.data();
+    IndexTranslator t({1, 2, 3}, std::move(detectorIDs));
+    TS_ASSERT_EQUALS(t.detectorIDs().data(), ptr);
+  }
+};
+
+#endif /* MANTID_INDEXING_INDEXTRANSLATORTEST_H_ */
