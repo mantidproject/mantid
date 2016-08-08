345beac3441a880e7b0de1e4eb0e24204cdc7ca1
Samuel Jackson <samueljackson@outlook.com>
2016-08-08 14:05:08 +0100

    Refs #16962 Serialise instrument widget pick tab

--- MantidQt/MantidWidgets/inc/MantidQtMantidWidgets/InstrumentView/InstrumentWidgetPickTab.h
+++ MantidQt/MantidWidgets/inc/MantidQtMantidWidgets/InstrumentView/InstrumentWidgetPickTab.h
@@ -84,9 +84,9 @@ public:
   void selectTool(const ToolType tool);
   boost::shared_ptr<ProjectionSurface> getSurface() const;
   /// Load settings for the widget tab from a project file
-  virtual void loadFromProject(const std::string &lines) override{};
+  virtual void loadFromProject(const std::string &lines) override;
   /// Save settings for the widget tab to a project file
-  virtual std::string saveToProject() const override { return ""; };
+  virtual std::string saveToProject() const override;
 
 public slots:
   void setTubeXUnits(int units);
