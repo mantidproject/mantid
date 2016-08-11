1a524091a70b1f3dbc0a43e164738800c85cc7d1
Antti Soininen <soininen@ill.fr>
2016-08-11 17:08:07 +0200

    Re #17009 Initial commit of GetEiMonDet version 2.

---
+++ Framework/Algorithms/inc/MantidAlgorithms/GetEiMonDet2.h
@@ -0,0 +1,73 @@
+#ifndef MANTID_ALGORITHMS_GETEIMONDET2_H_
+#define MANTID_ALGORITHMS_GETEIMONDET2_H_
+
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid {
+namespace Algorithms {
+/** TODO rewrite
+ * Requires an estimate for the initial neutron energy which it uses to
+  search for monitor peaks and from these calculate an accurate energy
+
+    Required Properties:
+    <UL>
+    <LI>InputWorkspace - The X units of this workspace must be time of flight
+  with times in micro-seconds</LI>
+    <LI>Monitor1ID - The detector ID of the first monitor</LI>
+    <LI>Monitor2ID - The detector ID of the second monitor</LI>
+    <LI>EnergyEstimate - An approximate value for the typical incident energy,
+  energy of neutrons leaving the source (meV)</LI>
+    <LI>IncidentEnergy - The calculated energy</LI>
+    </UL>
+
+    @author Antti Soininen Institut Laue-Langevin
+    @date XX/08/2016
+
+    Copyright &copy; 2016 ISIS Rutherford Appleton Laboratory, NScD Oak
+  Ridge National Laboratory & European Spallation Source
+
+    This file is part of Mantid.
+
+    Mantid is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Mantid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    File change history is stored at: <https://github.com/mantidproject/mantid>.
+    Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport GetEiMonDet2 : public Mantid::API::Algorithm {
+public:
+  /// Initialize the algorithm
+  void init() override;
+  /// Execute the algorithm
+  void exec() override;
+
+  /// Algorithm's name for identification overriding a virtual method
+  const std::string name() const override { return "GetEiMonDet"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    // TODO rewrite
+    return "Calculates the kinetic energy of neutrons leaving the source based "
+           "on the time it takes for them to travel between two monitors.";
+  }
+
+  /// Algorithm's version for identification overriding a virtual method
+  int version() const override { return 2; }
+  /// Algorithm's category for identification overriding a virtual method
+  const std::string category() const override { return "Inelastic\\Ei"; }
+
+};
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_GETEIMONDET2_H_*/
---
+++ Framework/Algorithms/src/GetEiMonDet2.cpp
@@ -0,0 +1,187 @@
+#include "MantidAlgorithms/GetEiMonDet2.h"
+
+#include "MantidAPI/InstrumentValidator.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/WorkspaceUnitValidator.h"
+#include "MantidGeometry/Instrument.h"
+#include "MantidKernel/BoundedValidator.h"
+#include "MantidKernel/CompositeValidator.h"
+#include "MantidKernel/EnabledWhenProperty.h"
+#include "MantidKernel/MandatoryValidator.h"
+#include "MantidKernel/PhysicalConstants.h"
+#include "MantidKernel/UserStringParser.h"
+#include "MantidKernel/VectorHelper.h"
+
+using namespace Mantid::API;
+using namespace Mantid::Kernel;
+using namespace Mantid::PhysicalConstants;
+
+namespace Mantid {
+namespace Algorithms {
+
+namespace PropertyNames {
+  const static std::string DETECTOR_SPECTRA("DetectorSpectra");
+  const static std::string DETECTOR_EPP_TABLE("DetectorEPPTable");
+  const static std::string DETECTOR_WORKSPACE("DetectorWorkspace");
+  const static std::string ENERGY_TOLERANCE("EnergyTolerance");
+  const static std::string INCIDENT_ENERGY("IncidentEnergy");
+  const static std::string MONITOR_EPP_TABLE("MonitorEPPTable");
+  const static std::string MONITOR_SPECTRUM_NUMBER("MonitorSpectrumNumber");
+  const static std::string MONITOR_WORKSPACE("MonitorWorkspace");
+  const static std::string PULSE_INTERVAL("PulseInterval");
+  const static std::string WAVELENGTH("Wavelength");
+}
+
+// Register the algorithm into the algorithm factory.
+DECLARE_ALGORITHM(GetEiMonDet2)
+
+void GetEiMonDet2::init() {
+  auto tofWorkspace = boost::make_shared<CompositeValidator>();
+  tofWorkspace->add<WorkspaceUnitValidator>("TOF");
+  tofWorkspace->add<InstrumentValidator>();
+
+  auto mandatoryStringProperty = boost::make_shared<MandatoryValidator<std::string>>();
+  auto mandatoryDetectorIdProperty = boost::make_shared<MandatoryValidator<detid_t>>();
+
+  declareProperty(
+      make_unique<WorkspaceProperty<>>(PropertyNames::DETECTOR_WORKSPACE, "", Direction::Input,
+                                       tofWorkspace),
+      "The X units of this workspace must be TOF in microseconds");
+  declareProperty(
+      make_unique<WorkspaceProperty<ITableWorkspace>>(PropertyNames::DETECTOR_EPP_TABLE, "", Direction::Input),
+      "EPP table corresponding to " + PropertyNames::DETECTOR_WORKSPACE);
+  declareProperty(
+      PropertyNames::DETECTOR_SPECTRA, "", "Formatting example: 1,3-7,10-15", mandatoryStringProperty);
+  declareProperty(
+      make_unique<WorkspaceProperty<>>(PropertyNames::MONITOR_WORKSPACE, "", Direction::Input, PropertyMode::Optional, tofWorkspace),
+      "If empty, " + PropertyNames::DETECTOR_WORKSPACE + " will be used");
+  declareProperty(
+      make_unique<WorkspaceProperty<ITableWorkspace>>(PropertyNames::MONITOR_EPP_TABLE,"", Direction::Input, PropertyMode::Optional),
+       "EPP table corresponding to " + PropertyNames::MONITOR_WORKSPACE);
+  setPropertySettings(PropertyNames::MONITOR_EPP_TABLE, make_unique<EnabledWhenProperty>(PropertyNames::MONITOR_WORKSPACE, IS_NOT_DEFAULT));
+  declareProperty(
+      PropertyNames::MONITOR_SPECTRUM_NUMBER, static_cast<detid_t>(EMPTY_INT()), mandatoryDetectorIdProperty);
+  declareProperty(
+      PropertyNames::PULSE_INTERVAL, EMPTY_DBL(), "In microseconds");
+  auto mustBePositive = boost::make_shared<BoundedValidator<double>>();
+  mustBePositive->setLower(0);
+  declareProperty(
+      PropertyNames::INCIDENT_ENERGY, EMPTY_DBL(), mustBePositive, "Taken from the sample logs, if not specified.", Direction::InOut);
+  declareProperty(
+      PropertyNames::ENERGY_TOLERANCE, 10.0, boost::make_shared<BoundedValidator<double>>(0, 100), "Tolerance between calculated energy and " + PropertyNames::INCIDENT_ENERGY + ", in percents.");
+  declareProperty(
+      PropertyNames::WAVELENGTH, -1.0, Direction::Output);
+}
+
+void GetEiMonDet2::exec() {
+  // TODO report progress
+  MatrixWorkspace_const_sptr detectorWs = getProperty(PropertyNames::DETECTOR_WORKSPACE);
+  ITableWorkspace_const_sptr detectorEPPTable = getProperty(PropertyNames::DETECTOR_EPP_TABLE);
+  UserStringParser spectraListParser;
+  // TODO Convert all indices to workspace indices.
+  auto detectorIndices = VectorHelper::flattenVector(spectraListParser.parse(getProperty(PropertyNames::DETECTOR_SPECTRA)));
+  // Remove duplicates from detectorIndices.
+  std::sort(detectorIndices.begin(), detectorIndices.end());
+  detectorIndices.erase(std::unique(detectorIndices.begin(), detectorIndices.end()), detectorIndices.end());
+  Property *monitorWsProperty = getProperty(PropertyNames::MONITOR_WORKSPACE);
+  MatrixWorkspace_const_sptr monitorWs;
+  ITableWorkspace_const_sptr monitorEPPTable;
+  const detid_t monitorIndex = getProperty(PropertyNames::MONITOR_SPECTRUM_NUMBER);
+  if (!monitorWsProperty->isDefault()) {
+    monitorWs = getProperty(PropertyNames::MONITOR_WORKSPACE);
+    monitorEPPTable = getProperty(PropertyNames::MONITOR_EPP_TABLE);
+    // TODO check if monitorEPPTable is valid
+  }
+  else {
+    monitorWs = detectorWs;
+    monitorEPPTable = detectorEPPTable;
+  }
+  if (monitorWs == detectorWs) {
+    if (std::find(detectorIndices.begin(), detectorIndices.end(), monitorIndex) != detectorIndices.end()) {
+      // TODO Throw something here.
+    }
+  }
+  double nominalIncidentEnergy = getProperty(PropertyNames::INCIDENT_ENERGY);
+  if (nominalIncidentEnergy == EMPTY_DBL()) {
+    if (!detectorWs->run().hasProperty("Ei")) {
+      // TODO throw
+    }
+    nominalIncidentEnergy = std::stod(detectorWs->run().getProperty("Ei")->value());
+  }
+
+  const std::string FIT_STATUS_COLUMN("FitStatus");
+  const std::string PEAK_CENTRE_COLUMN("PeakCentre");
+  const std::string FIT_STATUS_SUCCESS("success");
+  auto peakPositionColumn = detectorEPPTable->getColumn(PEAK_CENTRE_COLUMN);
+  auto fitStatusColumn = detectorEPPTable->getColumn(FIT_STATUS_COLUMN);
+  // TODO Check columns exist.
+  // Average detector EPPs.
+  double detectorEPP = 0.0;
+  size_t n = 0;
+  for (const auto index : detectorIndices) {
+    if (fitStatusColumn->cell<std::string>(index) == FIT_STATUS_SUCCESS) {
+      detectorEPP += (*peakPositionColumn)[index];
+      ++n;
+    }
+  }
+  if (n == 0) {
+    // TODO Throw.
+  }
+  detectorEPP /= static_cast<double>(n);
+  // Monitor peak position.
+  peakPositionColumn = monitorEPPTable->getColumn(PEAK_CENTRE_COLUMN);
+  fitStatusColumn = monitorEPPTable->getColumn(FIT_STATUS_COLUMN);
+  if (fitStatusColumn->cell<std::string>(monitorIndex) != FIT_STATUS_SUCCESS) {
+    // TODO throw
+  }
+  const double monitorEPP = (*peakPositionColumn)[monitorIndex];
+
+  // Average sample-to-detector distances.
+  const auto sample = detectorWs->getInstrument()->getSample();
+  double sampleToDetectorDistance = 0.0;
+  n = 0;
+  for (const auto index : detectorIndices) {
+    // Skip detectors without EPP.
+    if (fitStatusColumn->cell<std::string>(index) == FIT_STATUS_SUCCESS) {
+      const auto detector = detectorWs->getDetector(index);
+      sampleToDetectorDistance += detector->getDistance(*sample);
+      ++n;
+    }
+  }
+  sampleToDetectorDistance /= static_cast<double>(n);
+  // Monitor-to-sample distance.
+  const auto monitor = monitorWs->getDetector(monitorIndex);
+  const double monitorToSampleDistance = monitor->getDistance(*sample);
+
+  const double flightLength = sampleToDetectorDistance + monitorToSampleDistance;
+
+  // Calculate actual time of flight from monitor to detectors.
+  double timeOfFlight = detectorEPP - monitorEPP;
+  const double energyTolerance = static_cast<double>(getProperty(PropertyNames::ENERGY_TOLERANCE)) / 100;
+  const double lowerTimeTolerance = 1 / std::sqrt(1 + energyTolerance);
+  const double upperTimeTolerance = 1 / std::sqrt(1 - energyTolerance);
+  unsigned delayFrameCount = 0;
+  const double nominalTimeOfFlight = flightLength / std::sqrt(2 * nominalIncidentEnergy * meV / NeutronMass) * 1e6; // In microseconds.
+  while (timeOfFlight <= lowerTimeTolerance * nominalTimeOfFlight) {
+    // Neutrons hit the detectors in a later frame.
+    const double pulseInterval = getProperty(PropertyNames::PULSE_INTERVAL);
+    if (pulseInterval == EMPTY_DBL()) {
+      // TODO throw
+    }
+    ++delayFrameCount;
+    timeOfFlight = delayFrameCount * pulseInterval - monitorEPP + detectorEPP;
+  }
+  if (timeOfFlight > upperTimeTolerance * nominalTimeOfFlight) {
+    // TODO throw or warn user.
+  }
+
+  const double velocity = flightLength / timeOfFlight * 1e6;
+  const double energy = 0.5 * NeutronMass * velocity * velocity / meV;
+  const double wavelength = h / (NeutronMass * velocity) * 1e10; // in Ångströms.
+  // Set output properties.
+  setProperty(PropertyNames::INCIDENT_ENERGY, energy);
+  setProperty(PropertyNames::WAVELENGTH, wavelength);
+}
+
+} // namespace Algorithms
+} // namespace Mantid
