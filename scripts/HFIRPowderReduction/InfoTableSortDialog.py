065a481d98dd32ca991f55d7c5c9c02514d5afc8
Wenduo Zhou <zhouw@ornl.gov>
2016-08-18 16:11:19 -0400

    Refs #17223. Added a (prototype) dialog class.

---
+++ scripts/HFIRPowderReduction/InfoTableSortDialog.py
@@ -0,0 +1,34 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class DateDialog(QDialog):
+    def __init__(self, parent = None):
+        super(DateDialog, self).__init__(parent)
+
+        layout = QVBoxLayout(self)
+
+        # nice widget for editing the date
+        self.datetime = QDateTimeEdit(self)
+        self.datetime.setCalendarPopup(True)
+        self.datetime.setDateTime(QDateTime.currentDateTime())
+        layout.addWidget(self.datetime)
+
+        # OK and Cancel buttons
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
+            Qt.Horizontal, self)
+        buttons.accepted.connect(self.accept)
+        buttons.rejected.connect(self.reject)
+        layout.addWidget(buttons)
+
+    # get current date and time from the dialog
+    def dateTime(self):
+        return self.datetime.dateTime()
+
+    # static method to create the dialog and return (date, time, accepted)
+    @staticmethod
+    def getDateTime(parent = None):
+        dialog = DateDialog(parent)
+        result = dialog.exec_()
+        date = dialog.dateTime()
+        return (date.date(), date.time(), result == QDialog.Accepted)
