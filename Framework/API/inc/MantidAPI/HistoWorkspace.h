987703ce3c8f198c7755c9aa3d5249a2bf5900c7
Simon Heybrock <simon.heybrock@esss.se>
2016-08-29 13:14:45 +0200

    Re #17114. Attempt to fix VS compiler error.

--- Framework/API/inc/MantidAPI/HistoWorkspace.h
+++ Framework/API/inc/MantidAPI/HistoWorkspace.h
@@ -33,6 +33,9 @@ namespace API {
 */
 class MANTID_API_DLL HistoWorkspace : public MatrixWorkspace {
 public:
+  // Explicitly defined to avoid VS2015 compiler error.
+  ~HistoWorkspace() override{};
+
   /// Returns a clone of the workspace
   std::unique_ptr<HistoWorkspace> clone() const {
     return std::unique_ptr<HistoWorkspace>(doClone());
