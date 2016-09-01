e6d12c2f46eaf4125f84b0d7489d7601a6ba9150
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-09-01 15:17:04 +0100

    Refs #15825 Add second pair of braces

--- Framework/Algorithms/test/UnwrapMonitorsInTOFTest.h
+++ Framework/Algorithms/test/UnwrapMonitorsInTOFTest.h
@@ -49,7 +49,7 @@ public:
     // Time : 0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18
     // 19  20  | * 1e4 (in microseconds)
     const double tolerance = 1e-8;
-    std::array<size_t, 2> indicesToCheck{3, 4};
+    std::array<size_t, 2> indicesToCheck{{3, 4}};
     Mantid::HistogramData::BinEdges expectedBinEdges{
         0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
         11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
