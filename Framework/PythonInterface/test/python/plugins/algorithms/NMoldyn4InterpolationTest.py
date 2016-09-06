c7099db556bcd767b2ade2b7d68bf1e9167d8f21
Louise McCann <louise.mccann@stfc.ac.uk>
2016-09-06 11:13:17 +0100

    Fixed test issues in RHEL6
    Refs #17289

--- Framework/PythonInterface/test/python/plugins/algorithms/NMoldyn4InterpolationTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/NMoldyn4InterpolationTest.py
@@ -1,3 +1,5 @@
+#Author: Alex Phimister 08/2016
+#pylint disable: invalid-name
 import unittest
 from mantid.simpleapi import *
 from mantid.api import *
@@ -12,9 +14,10 @@ class NMoldyn4InterpolationTest(unittest.TestCase):
         self.osiris = osiris
 
     def test_interpolation_change(self):
-        x_data = np.arange(-2., 2., 0.05)
-        q_data = np.arange(0.5, 1.3, 0.1)
-        y_data = np.asarray([val*(np.cos(5*x_data)+1) for val in q_data])
+        x_data = np.arange(-5.01, 5.01, 0.25)
+        q_data = np.arange(-5.01, 5.01, 0.25)
+        xx, qq = np.meshgrid(x_data, q_data)
+        y_data = np.sin(xx**2 + qq**2)
         y_data = y_data.flatten()
         x_data = np.tile(x_data, len(q_data))
         sim = CreateWorkspace(DataX=x_data, DataY=y_data, NSpec = len(q_data),
@@ -26,14 +29,75 @@ class NMoldyn4InterpolationTest(unittest.TestCase):
         inter = NMoldyn4Interpolation(sim, self.osiris)
         inter_X = inter.readX(0)
         inter_Q_length = inter.getAxis(1).length()
-        sample_Y_data = inter.readY(5)
+        sample_Y_data = inter.readY(2)
 
         self.assertTrue(np.all(inter_X == osiris_X))
         self.assertNotEqual(len(inter_X), sim.getAxis(0).length())
         self.assertEqual(inter_Q_length, osiris_Q_length)
         self.assertNotEqual(inter_Q_length, sim.getNumberHistograms())
-        self.assertAlmostEqual(sample_Y_data[0], 0.020095312677012457)
-        self.assertAlmostEqual(sample_Y_data[30], 1.7310334927635238)
+        self.assertEqual(sample_Y_data[0], 0.76170572126095282)
+        self.assertEqual(sample_Y_data[5], 0.69047588058461018)
+
+
+    def test_X_min_too_big(self):
+        x_data = np.arange(-0.5, 2., 0.05)
+        q_data = np.arange(0.5, 1.3, 0.1)
+        y_data = np.asarray([val*(np.cos(5*x_data)+1) for val in q_data])
+        y_data = y_data.flatten()
+        x_data = np.tile(x_data, len(q_data))
+        sim = CreateWorkspace(DataX=x_data, DataY=y_data, NSpec = len(q_data),
+                              VerticalAxisUnit='MomentumTransfer',
+                              VerticalAxisValues=q_data)
+        self.assertRaises(RuntimeError,
+                          NMoldyn4Interpolation,
+                          InputWorkspace=sim,
+                          ReferenceWorkspace=self.osiris,
+                          OutputWorkspace='__NMoldyn4Interpolation_test')
+
+    def test_X_max_too_small(self):
+        x_data = np.arange(-2, 0.5, 0.05)
+        q_data = np.arange(0.5, 1.3, 0.1)
+        y_data = np.asarray([val*(np.cos(5*x_data)+1) for val in q_data])
+        y_data = y_data.flatten()
+        x_data = np.tile(x_data, len(q_data))
+        sim = CreateWorkspace(DataX=x_data, DataY=y_data, NSpec = len(q_data),
+                              VerticalAxisUnit='MomentumTransfer',
+                              VerticalAxisValues=q_data)
+        self.assertRaises(RuntimeError,
+                          NMoldyn4Interpolation,
+                          InputWorkspace=sim,
+                          ReferenceWorkspace=self.osiris,
+                          OutputWorkspace='__NMoldyn4Interpolation_test')
+
+    def test_Q_max_too_small(self):
+        x_data = np.arange(-2, 2, 0.05)
+        q_data = np.arange(0.8, 1.3, 0.1)
+        y_data = np.asarray([val*(np.cos(5*x_data)+1) for val in q_data])
+        y_data = y_data.flatten()
+        x_data = np.tile(x_data, len(q_data))
+        sim = CreateWorkspace(DataX=x_data, DataY=y_data, NSpec = len(q_data),
+                              VerticalAxisUnit='MomentumTransfer',
+                              VerticalAxisValues=q_data)
+        self.assertRaises(RuntimeError,
+                          NMoldyn4Interpolation,
+                          InputWorkspace=sim,
+                          ReferenceWorkspace=self.osiris,
+                          OutputWorkspace='__NMoldyn4Interpolation_test')
+
+    def test_Q_min_too_large(self):
+        x_data = np.arange(-2, 2, 0.05)
+        q_data = np.arange(0.5, 1., 0.1)
+        y_data = np.asarray([val*(np.cos(5*x_data)+1) for val in q_data])
+        y_data = y_data.flatten()
+        x_data = np.tile(x_data, len(q_data))
+        sim = CreateWorkspace(DataX=x_data, DataY=y_data, NSpec = len(q_data),
+                              VerticalAxisUnit='MomentumTransfer',
+                              VerticalAxisValues=q_data)
+        self.assertRaises(RuntimeError,
+                          NMoldyn4Interpolation,
+                          InputWorkspace=sim,
+                          ReferenceWorkspace=self.osiris,
+                          OutputWorkspace='__NMoldyn4Interpolation_test')
 
 if __name__=="__main__":
     unittest.main()
