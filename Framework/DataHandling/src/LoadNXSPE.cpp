feddbd46cc4a0b57e86a70fca32e19f8bd4f8890
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-09-06 09:43:52 +0100

    change psi to 0 if found to be NaN in LoadNXSPE re #17409

--- Framework/DataHandling/src/LoadNXSPE.cpp
+++ Framework/DataHandling/src/LoadNXSPE.cpp
@@ -135,6 +135,10 @@ void LoadNXSPE::exec() {
     file.getData(temporary);
     psi = temporary.at(0);
     file.closeData();
+	if (!isfinite(psi)) {
+		psi = 0;
+		g_log.warning() << "Value of psi found in file was NaN, changing to 0" << "\n";
+	}
   }
 
   int kikfscaling = 0;
