42879785b99bc3d8d0a11d119e89c6714a544ac3
Samuel Jackson <samueljackson@outlook.com>
2016-09-12 16:38:50 +0100

    Refs #17465 Fix algorithm history

--- MantidPlot/src/Mantid/AlgorithmHistoryWindow.cpp
+++ MantidPlot/src/Mantid/AlgorithmHistoryWindow.cpp
@@ -194,6 +194,14 @@ AlgorithmHistoryWindow::AlgorithmHistoryWindow(
   setMinimumWidth(750);
   setGeometry(50, 150, 540, 380);
 
+#ifdef Q_OS_MAC
+  // Work around to ensure that floating windows remain on top of the main
+  // application window, but below other applications on Mac
+  Qt::WindowFlags flags = windowFlags();
+  flags |= Qt::Tool;
+  setWindowFlags(flags);
+#endif
+
   // Create a tree widget to display the algorithm names in the workspace
   // history
   m_Historytree = new AlgHistoryTreeWidget(this);
