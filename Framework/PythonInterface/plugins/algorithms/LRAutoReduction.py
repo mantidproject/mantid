3c9be329d05f2549f8850a4fe60c50f0c4bd878c
Mathieu Doucet <matd10@yahoo.com>
2016-09-12 12:58:27 -0400

    use syntax for mantid logger

--- Framework/PythonInterface/plugins/algorithms/LRAutoReduction.py
+++ Framework/PythonInterface/plugins/algorithms/LRAutoReduction.py
@@ -490,8 +490,8 @@ class LRAutoReduction(PythonAlgorithm):
         new_data_sets = []
         for i in range(int(run_number) - int(first_run_of_set) + 1):
             if i >= len(self.data_series_template.data_sets):
-                logger.notice("Sequence information is corrupted: run=%s, first run of set=%s",
-                              str(run_number), str(first_run_of_set))
+                logger.notice("Sequence is corrupted: run=%s, first run of set=%s" % (str(run_number),
+                                                                                      str(first_run_of_set))
                 break
             d = self.data_series_template.data_sets[i]
             d.data_files = [int(first_run_of_set) + i]
