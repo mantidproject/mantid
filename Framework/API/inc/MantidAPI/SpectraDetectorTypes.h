7ffd13439479382b65b2f8d6d6c20e766bc72121
Martyn Gigg <martyn.gigg@gmail.com>
2016-09-12 09:24:52 +0100

    Remove final occurence of HAS_UNORDERED_MAP_H

--- Framework/API/inc/MantidAPI/SpectraDetectorTypes.h
+++ Framework/API/inc/MantidAPI/SpectraDetectorTypes.h
@@ -4,29 +4,16 @@
 // Includes
 //------------------------------------------------------------------------------
 #include "MantidGeometry/IDTypes.h"
-
-#ifndef HAS_UNORDERED_MAP_H
-#include <map>
-#else
-#include <tr1/unordered_map>
-#endif
+#include <unordered_map>
 
 namespace Mantid {
 
-#ifndef HAS_UNORDERED_MAP_H
 /// Map with key = spectrum number, value = workspace index
-typedef std::map<specnum_t, size_t> spec2index_map;
+typedef std::unordered_map<specnum_t, size_t> spec2index_map;
 /// Map with key = detector ID, value = workspace index
-typedef std::map<detid_t, size_t> detid2index_map;
-#else
-/// Map with key = spectrum number, value = workspace index
-typedef std::tr1::unordered_map<specnum_t, size_t> spec2index_map;
-/// Map with key = detector ID, value = workspace index
-typedef std::tr1::unordered_map<detid_t, size_t> detid2index_map;
-#endif
-
+typedef std::unordered_map<detid_t, size_t> detid2index_map;
 /// Map single det ID of group to its members
-typedef std::map<detid_t, std::vector<detid_t>> det2group_map;
+typedef std::unordered_map<detid_t, std::vector<detid_t>> det2group_map;
 }
 
 #endif // MANTID_API_SPECTRADETECTORMAP_TYPES
