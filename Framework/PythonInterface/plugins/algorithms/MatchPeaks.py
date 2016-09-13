51f550b753b238864e3efe1c91c5a47c5f436f8a
Verena Reimund <reimund@ill.eu>
2016-09-13 16:19:48 +0200

    Re #17367 Complete implementation and tests, table input uncommented

---
+++ Framework/PythonInterface/plugins/algorithms/MatchPeaks.py
@@ -0,0 +1,290 @@
+from __future__ import (absolute_import, division, print_function)
+from mantid.api import *
+from mantid.simpleapi import *
+from mantid.kernel import Direction
+import numpy as np
+import os.path
+
+def mask_reduced_ws(ws_to_mask, xstart, xend):
+    """
+    Calls MaskBins twice, for masking the first and last bins of a workspace
+    Args:
+        red:      reduced workspace
+        xstart:   MaskBins between x[0] and x[xstart]
+        xend:     MaskBins between x[xend] and x[-1]
+
+    """
+    x_values = ws_to_mask.readX(0)
+
+    if xstart > 0:
+        logger.debug('Mask bins smaller than {}'.format(xstart))
+        MaskBins(InputWorkspace=ws_to_mask, OutputWorkspace=ws_to_mask, XMin=x_values[0], XMax=x_values[xstart])
+    else:
+        logger.debug('No masking due to x bin < 0!: {}'.format(xstart))
+    if xend < len(x_values) - 1:
+        logger.debug('Mask bins larger than {}'.format(xend - 1))
+        MaskBins(InputWorkspace=ws_to_mask, OutputWorkspace=ws_to_mask, XMin=x_values[xend], XMax=x_values[-1])
+    else:
+        logger.debug('No masking due to x bin >= len(x_values) - 1!: {}'.format(xend))
+
+    if xstart > 0 and xend < len(x_values) - 1:
+        logger.notice('Bins out of range {} {} [Unit of X-axis] are masked'.format(x_values[xstart], x_values[xend]))
+
+
+class MatchPeaks(PythonAlgorithm):
+    def __init__(self):
+
+        PythonAlgorithm.__init__(self)
+
+        # Mandatory workspaces
+        self._input_ws = None
+
+        # Optional workspace / tables
+        self._input_2_ws = None
+        self._input_table = None
+        self._output_bin_range = None
+
+        # Bool flags
+        self._masking = None
+        self._match_option = None
+
+    def category(self):
+        return "Workflow\\Transforms"
+
+    def summary(self):
+        return 'Shift bins of each spectrum of the input workspace in order to match its peak positions '\
+               '(i.e. witch center bin, peak positions of another workspace)'
+
+    def PyInit(self):
+        self.declareProperty(MatrixWorkspaceProperty('InputWorkspace',
+                                                     defaultValue='',
+                                                     direction=Direction.Input),
+                             doc='Input workspace')
+
+        self.declareProperty(MatrixWorkspaceProperty('InputWorkspace2',
+                                                     defaultValue='',
+                                                     direction=Direction.Input,
+                                                     optional=PropertyMode.Optional),
+                             doc='Input workspace according to which bins of the input workspace will be shifted')
+
+        # Maybe for future use
+        #self.declareProperty(ITableWorkspaceProperty('InputTable',
+        #                                             defaultValue='',
+        #                                             direction=Direction.Input,
+        #                                             optional=PropertyMode.Optional),
+        #                     doc='Input table according to which bins of the input workspace will be shifted. '
+        #                         'The table must contain columns called PeakCentre, FitStatus and PeakCentreError')
+
+        self.declareProperty(MatrixWorkspaceProperty('OutputWorkspace',
+                                                     defaultValue='',
+                                                     direction=Direction.Output),
+                             doc='Shifted output workspace')
+
+        self.declareProperty('MaskBins',
+                             defaultValue=False,
+                             doc='Whether to mask shifted bins')
+
+        self.declareProperty('MatchInput2ToCenter',
+                             defaultValue=False,
+                             doc='Match peak bins such that InputWorkspace2 would require to be centered')
+
+        self.declareProperty(ITableWorkspaceProperty('BinRangeTable',
+                                                     defaultValue='',
+                                                     direction=Direction.Output,
+                                                     optional=PropertyMode.Optional),
+                             doc='Table workspace that contains a valid bin range: bins or x-values')
+
+    def setUp(self):
+        self._input_ws = self.getPropertyValue('InputWorkspace')
+        self._input_2_ws = self.getPropertyValue('InputWorkspace2')
+        self._output_ws = self.getPropertyValue('OutputWorkspace')
+        #self._input_table = self.getPropertyValue('InputTable')
+        self._masking = self.getProperty('MaskBins').value
+        self._match_option = self.getProperty('MatchInput2ToCenter').value
+        self._output_bin_range = self.getPropertyValue('BinRangeTable')
+
+    def validateInputs(self):
+        issues = dict()
+        input1 = self.getPropertyValue('InputWorkspace')
+        input2 = self.getPropertyValue('InputWorkspace2')
+        #input3 = self.getPropertyValue('InputTable')
+        if input2:
+            if mtd[input1].blocksize() != mtd[input2].blocksize():
+                issues['InputWorkspace2'] = 'Incompatible same number of bins'
+            if mtd[input1].getNumberHistograms() != mtd[input2].getNumberHistograms():
+                issues['InputWorkspace2'] = 'Incompatible number of spectra'
+            if np.all(mtd[input1].extractX() - mtd[input2].extractX()):
+                issues['InputWorkspace2'] = 'Incompatible x-values'
+
+            if np.any(np.isnan(mtd[input2].extractY())):
+                issues['InputWorkspace2'] = 'Y-values contain nans'
+            if np.any(np.isnan(mtd[input2].extractE())):
+                issues['InputWorkspace2'] = 'E-values contain nans'
+            if np.any(np.isinf(mtd[input2].extractY())):
+                issues['InputWorkspace2'] = 'Y-values contain infs'
+            if np.any(np.isinf(mtd[input2].extractE())):
+                issues['InputWorkspace2'] = 'E-values contain infs'
+        if input1:
+
+            if np.any(np.isnan(mtd[input1].extractY())):
+                issues['InputWorkspace'] = 'Y-values contain nans'
+            if np.any(np.isnan(mtd[input1].extractY())):
+                issues['InputWorkspace'] = 'E-values contain nans'
+            if np.any(np.isinf(mtd[input1].extractY())):
+                issues['InputWorkspace'] = 'Y-values contain infs'
+            if np.any(np.isinf(mtd[input1].extractY())):
+                issues['InputWorkspace'] = 'E-values contain infs'
+
+        #if input3:
+        #    if mtd[input1].getNumberHistograms() != mtd[input3].rowCount():
+        #        issues['InputTable'] = 'Incompatible number of spectra'
+        #    if not mtd[input3].row(0)["PeakCentre"]:
+        #        issues['InputTable'] = 'Column PeakCentre required'
+        #    if not mtd[input3].row(0)["PeakCentreError"]:
+        #        issues['InputTable'] = 'Column PeakCentreError required'
+        #    if not mtd[input3].row(0)["FitStatus"]:
+        #        issues['InputTable'] = 'Column FitStatus required'
+
+        #if input2 and input3:
+        #    issues['InputTable'] = 'Choose either InputWorkspace2 or InputTable'
+
+        return issues
+
+    def PyExec(self):
+        self.setUp()
+
+        output_ws = CloneWorkspace(InputWorkspace=mtd[self._input_ws], OutputWorkspace=self._output_ws)
+
+        size = mtd[self._input_ws].blocksize()
+
+        mid_bin = int(size / 2)
+
+        # Find peak positions in input workspace
+        peak_bins1 = self._get_peak_position(output_ws)
+        # Find peak positions in second input workspace
+        if self._input_2_ws:
+            peak_bins2 = self._get_peak_position(mtd[self._input_2_ws])
+        elif self._input_table:
+            peak_bins2 = self._get_peak_position(mtd[self._input_table])
+
+        self.log().notice('Peak bins {}: {}'.format(self._input_ws, peak_bins1))
+        if self._input_2_ws or self._input_table:
+            self.log().notice('Peak bins {} {}: {}'.format(self._input_2_ws, self._input_table, peak_bins2))
+
+        # All bins must be positive and larger than zero
+        if not self._input_2_ws and not self._input_table:
+            # Input workspace will match center
+            to_shift = peak_bins1 - mid_bin * np.ones(mtd[self._input_ws].getNumberHistograms())
+        elif self._match_option:
+            # Input workspace will be shifted according to centered peak of second input workspace
+            to_shift = peak_bins2 - mid_bin * np.ones(mtd[self._input_ws].getNumberHistograms())
+        else:
+            # Input workspace will be shifted according to peak position of second input workspace
+            to_shift = peak_bins1 - peak_bins2
+
+        for i in range(output_ws.getNumberHistograms()):
+            # Shift Y and E values of spectrum i
+            output_ws.setY(i, np.roll(output_ws.readY(i), int(-to_shift[i])))
+            output_ws.setE(i, np.roll(output_ws.readE(i), int(-to_shift[i])))
+        self.log().debug('Shift array: {}'.format(to_shift))
+
+        # Final treatment of bins (masking, produce output)
+        min_bin = 0
+        max_bin = size
+
+        if np.min(to_shift) < 0:
+            max_bin = size - abs(np.min(to_shift))
+
+        if np.max(to_shift) > 0:
+            min_bin = np.max(to_shift)
+
+        if self._masking:
+            mask_reduced_ws(output_ws, min_bin, max_bin)
+
+        if self._output_bin_range != '':
+            # Create table with its columns containing bin range
+            bin_range = CreateEmptyTableWorkspace(OutputWorkspace=self._output_bin_range)
+            bin_range.addColumn(type="double", name='MinBin')
+            bin_range.addColumn(type="double", name='MaxBin')
+
+            bin_range.addRow({'MinBin': min_bin, 'MaxBin': max_bin})
+            self.setProperty('BinRangeTable', bin_range)
+
+        # Set output properties
+        self.setProperty('OutputWorkspace', output_ws)
+
+        DeleteWorkspace(output_ws)
+
+        return
+
+    def _get_peak_position(self, input_ws):
+        """
+        Gives bin of the peak of each spectrum in the input_ws
+        @param input_ws  :: input workspace, can be a table workspace
+        @return          :: bin numbers of the peak positions
+        """
+
+        if isinstance(input_ws, MatrixWorkspace):
+            fit_table = FindEPP(InputWorkspace=input_ws)
+        elif isinstance(input_ws, ITableWorkspace):
+            fit_table = input_ws
+        else:
+            logger.error('Workspace not defined')
+
+        # Mid bin number
+        mid_bin = int(mtd[self._input_ws].blocksize() / 2)
+
+        # Initialisation
+        peak_bin = np.ones(mtd[self._input_ws].getNumberHistograms()) * mid_bin
+        peak_plus_error = np.zeros(mtd[self._input_ws].getNumberHistograms())
+
+        # Bin range: difference between mid bin and peak bin should be in this range
+        tolerance = int(mid_bin / 2)
+
+        x_values = mtd[self._input_ws].readX(0)
+
+        for i in range(mtd[self._input_ws].getNumberHistograms()):
+            fit = fit_table.row(i)
+
+            # Bin number, where Y has its maximum
+            y_values = mtd[self._input_ws].readY(i)
+
+            peak_pos_error = fit["PeakCentreError"] + fit["PeakCentre"]
+
+            if peak_pos_error >= x_values[0]:
+                if peak_pos_error <= x_values[-1]:
+                    if fit["FitStatus"] == 'success':
+                        peak_plus_error[i] = mtd[self._input_ws].binIndexOf(peak_pos_error)
+                        if abs(peak_plus_error[i] - mid_bin) < tolerance:
+                            peak_bin[i] = mtd[self._input_ws].binIndexOf(fit["PeakCentre"])
+                            logger.debug('Fit success, peak inside tolerance')
+                        else:
+                            logger.debug('Peak outside tolerance, do not shift spectrum')
+                    elif abs(np.argmax(y_values) - mid_bin) < tolerance:
+                        peak_bin[i] = np.argmax(y_values)
+                        logger.debug('Take maximum peak position {}'.format(peak_bin[i]))
+                    else:
+                        logger.debug('Fit failed and peak outside tolerance, do not shift spectrum')
+                else:
+                    logger.debug('Peak x-value {} > x-end {}, do not shift spectrum'.format(peak_pos_error,
+                                                                                            x_values[-1]))
+            else:
+                logger.debug('Peak x-value {} < x-begin {}, do not shift spectrum'.format(peak_pos_error, x_values[0]))
+
+        # Delete unused TableWorkspaces
+        try:
+            DeleteWorkspace('EPPfit_Parameters')
+        except ValueError:
+            logger.debug('No EPPfit_Parameters table available for deletion')
+
+        try:
+            DeleteWorkspace('EPPfit_NormalisedCovarianceMatrix')
+        except ValueError:
+            logger.debug('No EPPfit_NormalisedCovarianceMatrix table available for deletion')
+
+        DeleteWorkspace(fit_table)
+
+        return peak_bin
+
+
+AlgorithmFactory.subscribe(MatchPeaks)
---
+++ Framework/PythonInterface/test/python/plugins/algorithms/MatchPeaksTest.py
@@ -0,0 +1,286 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+from mantid.simpleapi import *
+from mantid.api import *
+import numpy as np
+from mantid.simpleapi import *
+from testhelpers import run_algorithm
+
+
+class MatchPeaksTest(unittest.TestCase):
+
+    _args = {}
+
+    def setUp(self):
+
+        func0 = "name=Gaussian, PeakCentre=3.2, Height=10, Sigma=0.3"
+        func1 = "name=Gaussian, PeakCentre=6, Height=10, Sigma=0.3"
+        func2 = "name=Gaussian, PeakCentre=4, Height=10000, Sigma=0.01"
+
+        _input_ws_0 = 'spectrum0'  # Gaussian
+        _input_ws_1 = 'spectrum1'  # Gaussian outside tolerance interval
+        _input_ws_2 = 'spectrum2'  # Gaussian, too narrow peak
+        self._input_ws_3 = 'spectrum3'  # Flat background
+
+        self._ws_shift = 'to_be_shifted'
+
+        spectrum_0 = CreateSampleWorkspace(Function='User Defined',
+                                               WorkspaceType='Histogram',
+                                               UserDefinedFunction=func0,
+                                               NumBanks=1, BankPixelWidth=1,
+                                               XUnit='DeltaE', XMin=0, XMax=7, BinWidth=0.099,
+                                               OutputWorkspace=_input_ws_0)
+        spectrum_1 = CreateSampleWorkspace(Function='User Defined',
+                                           WorkspaceType='Histogram',
+                                           UserDefinedFunction=func1,
+                                           NumBanks=1, BankPixelWidth=1,
+                                           XUnit='DeltaE', XMin=0, XMax=7, BinWidth=0.099,
+                                           OutputWorkspace=_input_ws_1)
+        spectrum_2 = CreateSampleWorkspace(Function='User Defined',
+                                           WorkspaceType='Histogram',
+                                           UserDefinedFunction=func2,
+                                           NumBanks=1, BankPixelWidth=1,
+                                           XUnit='DeltaE', XMin=0, XMax=7, BinWidth=0.099,
+                                           OutputWorkspace=_input_ws_2)
+        spectrum_3 = CreateSampleWorkspace(Function='Flat background',
+                                           WorkspaceType='Histogram',
+                                           NumBanks=1, BankPixelWidth=1,
+                                           XUnit='DeltaE', XMin=0, XMax=7, BinWidth=0.099,
+                                           OutputWorkspace=self._input_ws_3)
+
+        AppendSpectra(InputWorkspace1=spectrum_0, InputWorkspace2=spectrum_1, OutputWorkspace=self._ws_shift)
+        AppendSpectra(InputWorkspace1=self._ws_shift, InputWorkspace2=spectrum_2, OutputWorkspace=self._ws_shift)
+        AppendSpectra(InputWorkspace1=self._ws_shift, InputWorkspace2=spectrum_3, OutputWorkspace=self._ws_shift)
+
+        # Input workspace 2
+        self._ws_in_2 = 'in_2'
+        func3 = "name=LinearBackground, A0=0.3; name=Gaussian, PeakCentre=4.2, Height=10, Sigma=0.3"
+        CreateSampleWorkspace(Function='User Defined',
+                              WorkspaceType='Histogram',
+                              UserDefinedFunction=func3,
+                              NumBanks=4, BankPixelWidth=1,
+                              XUnit='DeltaE', XMin=0, XMax=7, BinWidth=0.099,
+                              OutputWorkspace=self._ws_in_2)
+
+        # Input workspaces that are incompatible
+        self._in1 = 'wrong_number_of_histograms'
+        CreateSampleWorkspace(Function='Flat background',
+                              WorkspaceType='Histogram',
+                              NumBanks=1, BankPixelWidth=1,
+                              XUnit='DeltaE', XMin=0, XMax=7, BinWidth=0.1,
+                              OutputWorkspace=self._in1)
+        self._in2 = 'wrong_number_of_bins'
+        CreateSampleWorkspace(Function='Flat background',
+                              WorkspaceType='Histogram',
+                              NumBanks=4, BankPixelWidth=1,
+                              XUnit='DeltaE', XMin=0, XMax=8, BinWidth=0.1,
+                              OutputWorkspace=self._in2)
+
+
+        # mtd[self._ws_shift].blocksize() = 70
+        # mid = 35
+
+        # Details:
+        # workspace has peak positions at : [32 35(mid) 40 35(mid)]
+        # the corresponding Y-values are (rounded)  : [10 0 3.4 1.0]
+        #
+        # -> test shifting to the right and to the left
+        # -> test options to use FindEPP, maximum peak position or no shifting
+
+    def tearDown(self):
+        if AnalysisDataService.doesExist('to_be_shifted'):
+            DeleteWorkspace(self._ws_shift)
+        if AnalysisDataService.doesExist('in_2'):
+            DeleteWorkspace(self._ws_in_2 )
+        if AnalysisDataService.doesExist('output'):
+            DeleteWorkspace(mtd['output'])
+        if AnalysisDataService.doesExist('wrong_number_of_histograms'):
+            DeleteWorkspace(self._in1)
+        if AnalysisDataService.doesExist('wrong_number_of_bins'):
+            DeleteWorkspace(self._in2)
+
+    def testValidatorInput(self):
+        self._args['OutputWorkspace'] = 'output'
+        # Test if incompatible workspaces will fail
+        with self.assertRaises(RuntimeError):
+            self._args['InputWorkspace'] = self._in1
+            run_algorithm('MatchPeaks', **self._args)
+
+            self._args['InputWorkspace'] = self._in2
+            run_algorithm('MatchPeaks', **self._args)
+
+        # Test if compatible workspaces will be accepted (size, X-values, E-values)
+        self._args['InputWorkspace'] = self._ws_shift
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        self._FindEPPtables_deleted
+
+    def testValidatorInput2(self):
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = 'output'
+        # Test if incompatible workspaces will fail
+        with self.assertRaises(RuntimeError):
+            self._args['InputWorkspace2'] = self._in1
+            run_algorithm('MatchPeaks', **self._args)
+
+            self._args['InputWorkspace2'] = self._in2
+            run_algorithm('MatchPeaks', **self._args)
+
+        # Test if compatible workspaces will be accepted (size, X-values, E-values)
+        self._args['InputWorkspace2'] = self._ws_in_2
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        self._FindEPPtables_deleted
+
+    #def testValidatorSpecialValues(self):
+
+    def testMatchCenter(self):
+        # Input workspace should match its center
+        # Bin ranges of each spectrum:
+        # spectrum 0 : [(32-35), 69] => [0, 67]      right shift
+        # spectrum 1 : [0, 70]                       no shift
+        # spectrum 2 : [0, 69 - (40-35)] => [5, 70]  left shift
+        # spectrum 3 : [0, 70]                       no shift
+        # Final bin range is [3, 65]
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = 'output'
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        shifted = AnalysisDataService.retrieve('output')
+        fit_table = FindEPP(shifted)
+        self.assertEqual(35, shifted.binIndexOf(fit_table.row(0)["PeakCentre"]))
+        self.assertEqual(35, np.argmax(shifted.readY(2)))
+        self._workspace_properties(shifted)
+        self._FindEPPtables_deleted
+        DeleteWorkspace(shifted)
+        DeleteWorkspace(fit_table)
+
+    def testBinRangeTable(self):
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = 'output'
+        self._args['BinRangeTable'] = 'bin_range'
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        bin_range_table = AnalysisDataService.retrieve('bin_range')
+        # Size of the table and its column names
+        self.assertEqual(1, bin_range_table.rowCount())
+        self.assertEqual(2, bin_range_table.columnCount())
+        columns = ['MinBin', 'MaxBin']
+        self.assertEqual(columns, bin_range_table.getColumnNames())
+        # Bin range
+        self.assertEqual(5, bin_range_table.row(0)["MinBin"])
+        self.assertEqual(67, bin_range_table.row(0)["MaxBin"])
+        DeleteWorkspace(bin_range_table)
+        self._FindEPPtables_deleted
+
+    def testMasking(self):
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = 'output'
+        self._args['MaskBins'] = True
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        masked = AnalysisDataService.retrieve('output')
+        for i in range(4):
+            for k in range(5):
+                self.assertEqual(0.0, masked.readY(i)[k], 'Mask spectrum {} bin {} failed'.format(i, k))
+            for k in range(67, 70):
+                self.assertEqual(0.0, masked.readY(i)[k], 'Mask spectrum {} bin {} failed'.format(i, k))
+        DeleteWorkspace(masked)
+        self._FindEPPtables_deleted
+
+    def testNoMasking(self):
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = 'output'
+        self._args['MaskBins'] = False # this is the default behaviour
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        not_masked = AnalysisDataService.retrieve('output')
+        self.assertTrue(1.0, not_masked.readY(2)[0])
+        DeleteWorkspace(not_masked)
+
+    def testMatchInput2(self):
+        # Input workspace should match the peak of input workspace 2:
+        # has its peaks at bin 42
+        # shifts: 32-42 = -10
+        #         35-42 = -7
+        #         40-42 = -2
+        #         35-42 = -7
+        # new bin range [0, 60]
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = 'output'
+        self._args['InputWorkspace2'] = self._ws_in_2
+        self._args['BinRangeTable'] = 'bin_range'
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        shifted = AnalysisDataService.retrieve('output')
+        bin_range_table = AnalysisDataService.retrieve('bin_range')
+        fit_table = FindEPP(shifted)
+        self.assertEqual(42, shifted.binIndexOf(fit_table.row(0)["PeakCentre"]))
+        self.assertEqual(42, np.argmax(shifted.readY(2)))
+        # Bin range
+        self.assertEqual(0, bin_range_table.row(0)["MinBin"])
+        self.assertEqual(60, bin_range_table.row(0)["MaxBin"])
+        self._workspace_properties(shifted)
+        self._FindEPPtables_deleted
+        DeleteWorkspace(shifted)
+        DeleteWorkspace(fit_table)
+        DeleteWorkspace(bin_range_table)
+
+    # An equivalent test can be performed for an input table created using
+    # FindEPP(InputWorkspace=self._ws_in_2, OutputWorkspace='input_table')
+    # self._args['InputTable'] = 'input_table'
+
+    def testMatchInput2MatchOption(self):
+        # match option true:
+        #               right shifts
+        # spectrum 0:   42 - 35 = 7
+        # spectrum 1:   42 - 35 = 7
+        # spectrum 2:   42 - 35 = 7
+        # spectrum 3:   42 - 35 = 7
+        # new bin range [7, 70]
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['InputWorkspace2'] = self._ws_in_2
+        self._args['MatchInput2ToCenter'] = True
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        shifted = AnalysisDataService.retrieve('output')
+        bin_range_table = AnalysisDataService.retrieve('bin_range')
+        fit_table = FindEPP(shifted)
+        self.assertEqual(32-7, shifted.binIndexOf(fit_table.row(0)["PeakCentre"]))
+        self.assertEqual(40-7, np.argmax(shifted.readY(2)))
+        # Bin range
+        self.assertEqual(7, bin_range_table.row(0)["MinBin"])
+        self.assertEqual(70, bin_range_table.row(0)["MaxBin"])
+        self._workspace_properties(shifted)
+        self._FindEPPtables_deleted
+        DeleteWorkspace(shifted)
+        DeleteWorkspace(fit_table)
+        DeleteWorkspace(bin_range_table)
+
+    def testOverride(self):
+        self._args['InputWorkspace'] = self._ws_shift
+        self._args['OutputWorkspace'] = self._ws_shift
+        alg_test = run_algorithm('MatchPeaks', **self._args)
+        self.assertTrue(alg_test.isExecuted())
+        shifted = AnalysisDataService.retrieve('to_be_shifted')
+        self.assertFalse(np.all(mtd['to_be_shifted'].extractY() - shifted.extractY()))
+        self._FindEPPtables_deleted
+        DeleteWorkspace(shifted)
+
+    def _FindEPPtables_deleted(self):
+        with self.assertRaises(ValueError):
+            run_algorithm('DeleteWorkspace', Workspace='EPPfit_Parameters')
+            DeleteWorkspace()
+        with self.assertRaises(ValueError):
+            run_algorithm('DeleteWorkspace', Workspace='EPPfit_NormalisedCovarianceMatrix')
+        with self.assertRaises(ValueError):
+            run_algorithm('DeleteWorkspace', Workspace='fit_table')
+
+    def _workspace_properties(self, test_ws):
+        self.assertTrue(isinstance(test_ws, MatrixWorkspace), "Should be a matrix workspace")
+        self.assertTrue(test_ws.getSampleDetails(), "Should have SampleLogs")
+        self.assertTrue(test_ws.getHistory().lastAlgorithm(), "Should have AlgorithmsHistory")
+
+if __name__=="__main__":
+    unittest.main()
