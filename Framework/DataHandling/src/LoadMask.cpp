914e99bbede6c5e4157ecce143ffb8481c000cb7
Alex Buts <Alex.Buts@stfc.ac.uk>
2016-09-14 12:18:55 +0100

    Re #16808 ffff-Clang

--- Framework/DataHandling/src/LoadMask.cpp
+++ Framework/DataHandling/src/LoadMask.cpp
@@ -206,7 +206,6 @@ void parseISISStringToVector(const std::string &ins,
     if (index >= splitstrings.size())
       tocontinue = false;
   } // END-WHILE
-
 }
 /*
 * Load and parse an ISIS masking file
