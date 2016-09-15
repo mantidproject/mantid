5a34bc99e4fbcc217894daae404862ba5140f1d6
Louise McCann <louise.mccann@stfc.ac.uk>
2016-09-15 17:10:21 +0100

    Refs #16516 Added mass density test to SampleTransmission

--- Framework/PythonInterface/test/python/plugins/algorithms/CalculateSampleTransmissionTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/CalculateSampleTransmissionTest.py
@@ -10,7 +10,7 @@ class CalculateSampleTransmissionTest(unittest.TestCase):
 
     def test_sample_transmission_calculation(self):
         """
-        Test a basic transmission calculation.
+        Test a basic transmission calculation using number density.
         """
 
         # Using water sample
@@ -19,7 +19,7 @@ class CalculateSampleTransmissionTest(unittest.TestCase):
         thickness = 0.1
 
         ws = CalculateSampleTransmission(WavelengthRange='5.0,0.2,7.0', ChemicalFormula=formula,
-                                         NumberDensity=density, Thickness=thickness)
+                                         Density=density, Thickness=thickness)
 
         self.assertEqual(ws.getNumberHistograms(), 2)
 
@@ -31,6 +31,26 @@ class CalculateSampleTransmissionTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(trans, expected_trans, decimal=4)
         np.testing.assert_array_almost_equal(scatt, expected_scatt, decimal=4)
 
+    def test_mass_density(self):
+        """
+        Tests a transmission calculation using mass density
+        """
+        formula = "H2-O"
+        density = 1
+        thickness = 0.1
+
+        ws = CalculateSampleTransmission(WavelengthRange='5.0,0.2,7.0', ChemicalFormula=formula,
+                                         Density=density, Thickness=thickness, UseMassDensity = True)
+
+        self.assertEqual(ws.getNumberHistograms(), 2)
+
+        expected_trans = [0.825901, 0.825778, 0.825656, 0.825533,  0.825411, 0.825288, 0.825166, 0.825044, 0.824921, 0.824799]
+        expected_scatt = [0.170971, 0.170971, 0.170971,  0.170971, 0.170971, 0.170971, 0.170971, 0.170971, 0.170971, 0.170971]
+
+        trans = ws.readY(0)
+        scatt = ws.readY(1)
+        np.testing.assert_array_almost_equal(trans, expected_trans, decimal=4)
+        np.testing.assert_array_almost_equal(scatt, expected_scatt, decimal=4)
 
     def test_validate_density(self):
         """
