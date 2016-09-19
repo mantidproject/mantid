8ed2837186b601612a5db159753842b215ace175
Dimitar Tasev <dbt@aber.ac.uk>
2016-09-19 09:56:46 +0100

    Re #17273 fixed convert units using detector table performance test

--- Framework/Algorithms/test/ConvertUnitsUsingDetectorTableTest.h
+++ Framework/Algorithms/test/ConvertUnitsUsingDetectorTableTest.h
@@ -21,7 +21,6 @@ using namespace Mantid::DataObjects;
 using namespace Mantid::Geometry;
 using namespace CxxTest;
 
-
 class ConvertUnitsUsingDetectorTableTest : public CxxTest::TestSuite {
 public:
   // This pair of boilerplate methods prevent the suite being created statically
@@ -97,56 +96,54 @@ public:
 
 class ConvertUnitsUsingDetectorTableTestPerformance : public TestSuite {
 public:
-	ConvertUnitsUsingDetectorTableTestPerformance() {}
-	~ConvertUnitsUsingDetectorTableTestPerformance() override {
-		AnalysisDataService::Instance().remove(workspaceName);
-	}
-	void testPerformance() {
-		ConvertUnitsUsingDetectorTable myAlg;
-		myAlg.initialize();
+  ConvertUnitsUsingDetectorTableTestPerformance() {}
+  ~ConvertUnitsUsingDetectorTableTestPerformance() override {
+    AnalysisDataService::Instance().remove(workspaceName);
+  }
+  void testPerformance() {
+    ConvertUnitsUsingDetectorTable myAlg;
+    myAlg.initialize();
 
-		int nBins = 10;
-		//     MatrixWorkspace_sptr WS =
-		//     WorkspaceCreationHelper::Create2DWorkspaceBinned(2, nBins, 500.0,
-		//     50.0);
-		WS =
-			WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(
-				2, nBins, false, false, true, "TESTY");
+    int nBins = 10;
+    //     MatrixWorkspace_sptr WS =
+    //     WorkspaceCreationHelper::Create2DWorkspaceBinned(2, nBins, 500.0,
+    //     50.0);
+    WS = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(
+        2, nBins, false, false, true, "TESTY");
 
-		WS->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
+    WS->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
 
-		AnalysisDataService::Instance().add(workspaceName, WS);
+    AnalysisDataService::Instance().add(workspaceName, WS);
 
-		// Create TableWorkspace with values in it
+    // Create TableWorkspace with values in it
 
-		pars =
-			WorkspaceFactory::Instance().createTable("TableWorkspace");
-		pars->addColumn("int", "spectra");
-		pars->addColumn("double", "l1");
-		pars->addColumn("double", "l2");
-		pars->addColumn("double", "twotheta");
-		pars->addColumn("double", "efixed");
-		pars->addColumn("int", "emode");
+    pars = WorkspaceFactory::Instance().createTable("TableWorkspace");
+    pars->addColumn("int", "spectra");
+    pars->addColumn("double", "l1");
+    pars->addColumn("double", "l2");
+    pars->addColumn("double", "twotheta");
+    pars->addColumn("double", "efixed");
+    pars->addColumn("int", "emode");
 
-		API::TableRow row0 = pars->appendRow();
-		row0 << 1 << 100.0 << 10.0 << 90.0 << 7.0 << 0;
+    API::TableRow row0 = pars->appendRow();
+    row0 << 1 << 100.0 << 10.0 << 90.0 << 7.0 << 0;
 
-		API::TableRow row1 = pars->appendRow();
-		row1 << 2 << 1.0 << 1.0 << 90.0 << 7.0 << 0;
+    API::TableRow row1 = pars->appendRow();
+    row1 << 2 << 1.0 << 1.0 << 90.0 << 7.0 << 0;
 
-		// Set the properties
-		myAlg.setRethrows(true);
-		myAlg.setPropertyValue("InputWorkspace", workspaceName);
-		myAlg.setPropertyValue("OutputWorkspace", workspaceName);
-		myAlg.setPropertyValue("Target", "Wavelength");
-		myAlg.setProperty("DetectorParameters", pars);
-		myAlg.execute();
-	}
+    // Set the properties
+    myAlg.setRethrows(true);
+    myAlg.setPropertyValue("InputWorkspace", workspaceName);
+    myAlg.setPropertyValue("OutputWorkspace", workspaceName);
+    myAlg.setPropertyValue("Target", "Wavelength");
+    myAlg.setProperty("DetectorParameters", pars);
+    myAlg.execute();
+  }
 
 private:
-	std::string workspaceName = "_ws_testConvertUsingDetectorTable";
-	MatrixWorkspace_sptr WS;
-	ITableWorkspace_sptr pars;
+  std::string workspaceName = "_ws_testConvertUsingDetectorTable";
+  MatrixWorkspace_sptr WS;
+  ITableWorkspace_sptr pars;
 };
 
 #endif /* MANTID_ALGORITHMS_CONVERTUNITSUSINGDETECTORTABLETEST_H_ */
