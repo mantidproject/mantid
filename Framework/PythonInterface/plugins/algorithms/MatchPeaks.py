Re #17367 fix mask range by propagating minus sign, improve texts

Framework/PythonInterface/plugins/algorithms/MatchPeaks.py
@@ -21,7 +21,7 @@ def mask_reduced_ws(ws_to_mask, xstart, xend):
         logger.debug('Mask bins smaller than {0}'.format(xstart))
         MaskBins(InputWorkspace=ws_to_mask, OutputWorkspace=ws_to_mask, XMin=x_values[0], XMax=x_values[xstart])
     else:
-        logger.debug('No masking due to x bin < 0!: {0}'.format(xstart))
+        logger.debug('No masking due to x bin <= 0!: {0}'.format(xstart))
     if xend < len(x_values) - 1:
         logger.debug('Mask bins larger than {0}'.format(xend))
         MaskBins(InputWorkspace=ws_to_mask, OutputWorkspace=ws_to_mask, XMin=x_values[xend + 1], XMax=x_values[-1])
@@ -41,9 +41,8 @@ class MatchPeaks(PythonAlgorithm):
         # Mandatory workspaces
         self._input_ws = None
 
-        # Optional workspace / tables
+        # Optional workspace
         self._input_2_ws = None
-        self._input_table = None
         self._output_bin_range = None
 
         # Bool flags
@@ -54,8 +53,8 @@ class MatchPeaks(PythonAlgorithm):
         return "Transforms"
 
     def summary(self):
-        return 'Shift bins of each spectrum of the input workspace such that peak positions match '\
-               '(i.e. the center bin, the corresponding peak positions of a second input workspace)'
+        return 'Shift the data of the input workspace to align each spectrum peaks with each other at the centre of ' \
+               'x-axis or, if requested, according to the positions, specified by the second input workspace.'
 
     def PyInit(self):
         self.declareProperty(MatrixWorkspaceProperty('InputWorkspace',
@@ -67,7 +66,7 @@ class MatchPeaks(PythonAlgorithm):
                                                      defaultValue='',
                                                      direction=Direction.Input,
                                                      optional=PropertyMode.Optional),
-                             doc='Input workspace for extracting its peak positions')
+                             doc='Input workspace as source of the peak positions to align with')
 
         self.declareProperty(MatrixWorkspaceProperty('OutputWorkspace',
                                                      defaultValue='',
@@ -86,7 +85,8 @@ class MatchPeaks(PythonAlgorithm):
                                                      defaultValue='',
                                                      direction=Direction.Output,
                                                      optional=PropertyMode.Optional),
-                             doc='Table workspace that contains a bin range for masking')
+                             doc='Table workspace that contains two values defining a range. Bins inside this range '
+                                 'were not circular shifted. Bins outside this range can be masked.')
 
     def setUp(self):
         self._input_ws = self.getPropertyValue('InputWorkspace')
@@ -139,18 +139,15 @@ class MatchPeaks(PythonAlgorithm):
 
         # Find peak positions in input workspace
         peak_bins1 = self._get_peak_position(output_ws)
+        self.log().notice('Peak bins {0}: {1}'.format(self._input_ws, peak_bins1))
+
         # Find peak positions in second input workspace
         if self._input_2_ws:
             peak_bins2 = self._get_peak_position(mtd[self._input_2_ws])
-        elif self._input_table:
-            peak_bins2 = self._get_peak_position(mtd[self._input_table])
-
-        self.log().notice('Peak bins {0}: {1}'.format(self._input_ws, peak_bins1))
-        if self._input_2_ws or self._input_table:
-            self.log().notice('Peak bins {0} {1}: {2}'.format(self._input_2_ws, self._input_table, peak_bins2))
+            self.log().notice('Peak bins {0} {1}: {2}'.format(self._input_2_ws, peak_bins2))
 
         # All bins must be positive and larger than zero
-        if not self._input_2_ws and not self._input_table:
+        if not self._input_2_ws:
             # Input workspace will match center
             to_shift = peak_bins1 - mid_bin * np.ones(mtd[self._input_ws].getNumberHistograms())
         elif self._match_option:
@@ -164,17 +161,19 @@ class MatchPeaks(PythonAlgorithm):
             # Shift Y and E values of spectrum i
             output_ws.setY(i, np.roll(output_ws.readY(i), int(-to_shift[i])))
             output_ws.setE(i, np.roll(output_ws.readE(i), int(-to_shift[i])))
-        self.log().debug('Shift array: {0}'.format(to_shift))
+        self.log().debug('Shift array: {0}'.format(-to_shift))
 
         # Final treatment of bins (masking, produce output)
         min_bin = 0
         max_bin = size
 
-        if np.min(to_shift) < 0:
-            max_bin = size - 1 - abs(np.min(to_shift))
+        # This is a left shift, bins at the end of the workspace may be masked
+        if np.min(-to_shift) < 0:
+            max_bin = size - 1 - abs(np.min(-to_shift))
 
-        if np.max(to_shift) > 0:
-            min_bin = np.max(to_shift)
+        # This is a right shift, bins at the beginning of the workspace may be masked
+        if np.max(-to_shift) > 0:
+            min_bin = np.max(-to_shift)
 
         if self._masking:
             mask_reduced_ws(output_ws, min_bin, max_bin)
@@ -198,7 +197,7 @@ class MatchPeaks(PythonAlgorithm):
     def _get_peak_position(self, input_ws):
         """
         Gives bin of the peak of each spectrum in the input_ws
-        @param input_ws  :: input workspace, can be a table workspace
+        @param input_ws  :: input workspace
         @return          :: bin numbers of the peak positions
         """
 

Framework/PythonInterface/test/python/plugins/algorithms/MatchPeaksTest.py
@@ -158,16 +158,14 @@ class MatchPeaksTest(unittest.TestCase):
         self.assertTrue(alg_test.isExecuted())
         self._FindEPPtables_deleted
 
-    #def testValidatorSpecialValues(self):
-
     def testMatchCenter(self):
         # Input workspace should match its center
         # Bin ranges of each spectrum:
-        # spectrum 0 : [(32-35), 69] => [0, 67]      right shift
+        # spectrum 0 : [(32-35), 70] => [3, 70]      right shift
         # spectrum 1 : [0, 70]                       no shift
-        # spectrum 2 : [0, 69 - (40-35)] => [5, 70]  left shift
+        # spectrum 2 : [0, 70 - (40-35)] => [0, 65]  left shift
         # spectrum 3 : [0, 70]                       no shift
-        # Final bin range is [3, 65]
+        # Final bin range is [3, 65-1]
         self._args['InputWorkspace'] = self._ws_shift
         self._args['OutputWorkspace'] = 'output'
         alg_test = run_algorithm('MatchPeaks', **self._args)
@@ -194,8 +192,8 @@ class MatchPeaksTest(unittest.TestCase):
         columns = ['MinBin', 'MaxBin']
         self.assertEqual(columns, bin_range_table.getColumnNames())
         # Bin range
-        self.assertEqual(5, bin_range_table.row(0)["MinBin"])
-        self.assertEqual(66, bin_range_table.row(0)["MaxBin"])
+        self.assertEqual(3, bin_range_table.row(0)["MinBin"])
+        self.assertEqual(64, bin_range_table.row(0)["MaxBin"])
         DeleteWorkspace(bin_range_table)
         self._FindEPPtables_deleted
 
@@ -207,9 +205,9 @@ class MatchPeaksTest(unittest.TestCase):
         self.assertTrue(alg_test.isExecuted())
         masked = AnalysisDataService.retrieve('output')
         for i in range(4):
-            for k in range(5):
+            for k in range(3):
                 self.assertEqual(0.0, masked.readY(i)[k], 'Mask spectrum {0} bin {1} failed'.format(i, k))
-            for k in range(67, 70):
+            for k in range(65, 70):
                 self.assertEqual(0.0, masked.readY(i)[k], 'Mask spectrum {0} bin {1} failed'.format(i, k))
         DeleteWorkspace(masked)
         self._FindEPPtables_deleted
@@ -227,11 +225,11 @@ class MatchPeaksTest(unittest.TestCase):
     def testMatchInput2(self):
         # Input workspace should match the peak of input workspace 2:
         # has its peaks at bin 42
-        # shifts: 32-42 = -10
-        #         35-42 = -7
-        #         40-42 = -2
-        #         35-42 = -7
-        # new bin range [0, 60]
+        # shifts: 32-42 = -10 (right shift)
+        #         35-42 = -7  (right shift)
+        #         40-42 = -2  (right shift)
+        #         35-42 = -7  (right shift)
+        # new bin range [10, 70]
         self._args['InputWorkspace'] = self._ws_shift
         self._args['OutputWorkspace'] = 'output'
         self._args['InputWorkspace2'] = self._ws_in_2
@@ -244,26 +242,22 @@ class MatchPeaksTest(unittest.TestCase):
         self.assertEqual(42, shifted.binIndexOf(fit_table.row(0)["PeakCentre"]))
         self.assertEqual(42, np.argmax(shifted.readY(2)))
         # Bin range
-        self.assertEqual(0, bin_range_table.row(0)["MinBin"])
-        self.assertEqual(59, bin_range_table.row(0)["MaxBin"])
+        self.assertEqual(10, bin_range_table.row(0)["MinBin"])
+        self.assertEqual(70, bin_range_table.row(0)["MaxBin"])
         self._workspace_properties(shifted)
         self._FindEPPtables_deleted
         DeleteWorkspace(shifted)
         DeleteWorkspace(fit_table)
         DeleteWorkspace(bin_range_table)
 
-    # An equivalent test can be performed for an input table created using
-    # FindEPP(InputWorkspace=self._ws_in_2, OutputWorkspace='input_table')
-    # self._args['InputTable'] = 'input_table'
-
     def testMatchInput2MatchOption(self):
         # match option true:
-        #               right shifts
-        # spectrum 0:   42 - 35 = 7
-        # spectrum 1:   42 - 35 = 7
-        # spectrum 2:   42 - 35 = 7
-        # spectrum 3:   42 - 35 = 7
-        # new bin range [7, 70]
+        #               left shifts
+        # spectrum 0:   35 - 35 = 0 (no shift)
+        # spectrum 1:   42 - 35 = 7 (left shift)
+        # spectrum 2:   42 - 35 = 7 (left shift)
+        # spectrum 3:   42 - 35 = 7 (left shift)
+        # new bin range [0, 70-7-1]
         self._args['InputWorkspace'] = self._ws_shift
         self._args['InputWorkspace2'] = self._ws_in_2
         self._args['MatchInput2ToCenter'] = True
@@ -275,8 +269,8 @@ class MatchPeaksTest(unittest.TestCase):
         self.assertEqual(32-7, shifted.binIndexOf(fit_table.row(0)["PeakCentre"]))
         self.assertEqual(40-7, np.argmax(shifted.readY(2)))
         # Bin range
-        self.assertEqual(7, bin_range_table.row(0)["MinBin"])
-        self.assertEqual(70, bin_range_table.row(0)["MaxBin"])
+        self.assertEqual(0, bin_range_table.row(0)["MinBin"])
+        self.assertEqual(62, bin_range_table.row(0)["MaxBin"])
         self._workspace_properties(shifted)
         self._FindEPPtables_deleted
         DeleteWorkspace(shifted)