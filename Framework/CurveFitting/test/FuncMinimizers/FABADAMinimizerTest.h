64afd64c027900a8ee0be01ff42951dad99ea8a4
Raquel Alvarez <raquel.alvarez-banos@stfc.ac.uk>
2016-09-27 12:36:25 +0100

    Re #17509 Added unit test with constraint

--- Framework/CurveFitting/test/FuncMinimizers/FABADAMinimizerTest.h
+++ Framework/CurveFitting/test/FuncMinimizers/FABADAMinimizerTest.h
@@ -30,7 +30,7 @@ public:
   static void destroySuite(FABADAMinimizerTest *suite) { delete suite; }
 
   void test_expDecay() {
-    auto ws2 = createTestWorkspace();
+    auto ws2 = createExpDecayWorkspace();
 
     API::IFunction_sptr fun(new ExpDecay);
     fun->setParameter("Height", 8.);
@@ -133,7 +133,7 @@ public:
   }
 
   void test_low_MaxIterations() {
-    auto ws2 = createTestWorkspace();
+    auto ws2 = createExpDecayWorkspace();
 
     API::IFunction_sptr fun(new ExpDecay);
     fun->setParameter("Height", 1.);
@@ -156,8 +156,59 @@ public:
     TS_ASSERT(!fit.isExecuted());
   }
 
+  void test_cosineWithConstraint() {
+
+    auto ws2 = createCosineWorkspace();
+
+    Algorithms::Fit fit;
+    fit.initialize();
+    fit.setChild(true);
+    fit.setPropertyValue("Function", "name=UserFunction, Formula=a*cos(b*x), "
+                                     "a=2, b=-1, constraints=(0<b<1)");
+    fit.setProperty("InputWorkspace", ws2);
+    fit.setProperty("WorkspaceIndex", 0);
+    fit.setProperty("CreateOutput", true);
+    fit.setProperty("MaxIterations", 100000);
+    fit.setProperty("Minimizer", "FABADA,ChainLength=5000,StepsBetweenValues="
+                                 "10,ConvergenceCriteria=0.1,CostFunctionTable="
+                                 "CostFunction,Chains=Chain,ConvergedChain"
+                                 "=ConvergedChain,Parameters=Parameters");
+    TS_ASSERT_THROWS_NOTHING(fit.execute());
+	TS_ASSERT_EQUALS(fit.getPropertyValue("OutputStatus"), "success");
+	IFunction_sptr fun = fit.getProperty("Function");
+    TS_ASSERT_DELTA(fun->getParameter("a"), 0.9, 0.1);
+    TS_ASSERT_DELTA(fun->getParameter("b"), 0.9, 0.1);
+
+    // Test PDF workspace
+    MatrixWorkspace_sptr pdf = fit.getProperty("PDF");
+    TS_ASSERT(pdf);
+    TS_ASSERT_DELTA(pdf->y(0)[11], 0.55, 0.01);
+    TS_ASSERT_DELTA(pdf->y(1)[19], 4.88, 0.01);
+    TS_ASSERT_DELTA(pdf->y(2)[0], 0.34, 0.01);
+
+    //  Test CostFunction table
+    ITableWorkspace_sptr costFunc = fit.getProperty("CostFunctionTable");
+    TS_ASSERT(costFunc);
+    TS_ASSERT_EQUALS(costFunc->columnCount(), 4);
+    TS_ASSERT_EQUALS(costFunc->rowCount(), 1);
+    TS_ASSERT_LESS_THAN_EQUALS(costFunc->Double(0, 0), costFunc->Double(0, 1));
+    TS_ASSERT_LESS_THAN_EQUALS(costFunc->Double(0, 2), costFunc->Double(0, 3));
+    TS_ASSERT_DELTA(costFunc->Double(0, 0), costFunc->Double(0, 1), 0.5);
+    TS_ASSERT_DELTA(costFunc->Double(0, 0), 0.0, 1.0);
+
+    // Parameters workspace
+    ITableWorkspace_sptr param = fit.getProperty("Parameters");
+    TS_ASSERT(param);
+    TS_ASSERT_EQUALS(param->columnCount(), 4);
+    TS_ASSERT_EQUALS(param->rowCount(), 2);
+    TS_ASSERT_DELTA(param->Double(0, 2), -0.50, 0.01);
+    TS_ASSERT_DELTA(param->Double(0, 3), 1.10, 0.01);
+    TS_ASSERT_DELTA(param->Double(1, 2), -0.07, 0.01);
+    TS_ASSERT_DELTA(param->Double(1, 3), 0.01, 0.1);
+  }
+
 private:
-  API::MatrixWorkspace_sptr createTestWorkspace() {
+  API::MatrixWorkspace_sptr createExpDecayWorkspace() {
     MatrixWorkspace_sptr ws2(new WorkspaceTester);
     ws2->initialize(1, 20, 20);
 
@@ -166,6 +217,21 @@ private:
     for (size_t i = 0; i < ws2->blocksize(); ++i) {
       x[i] = 0.1 * double(i);
       y[i] = 10.0 * exp(-(x[i]) / 0.5);
+    }
+
+    return ws2;
+  }
+
+  API::MatrixWorkspace_sptr createCosineWorkspace() {
+    MatrixWorkspace_sptr ws2(new WorkspaceTester);
+    ws2->initialize(1, 20, 20);
+
+    Mantid::MantidVec &x = ws2->dataX(0);
+    Mantid::MantidVec &y = ws2->dataY(0);
+    for (size_t i = 0; i < ws2->blocksize(); ++i) {
+      double xx = 2 * M_PI * i / 20;
+      x[i] = xx;
+      y[i] = cos(xx);
     }
 
     return ws2;
