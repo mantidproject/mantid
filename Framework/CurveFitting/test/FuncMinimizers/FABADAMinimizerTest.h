f72ff8aa9d1eb8e9cd95b936533bc6fb4271021b
Raquel Alvarez <raquel.alvarez-banos@stfc.ac.uk>
2016-09-27 11:21:53 +0100

    Re #17509 Updated unit test

--- Framework/CurveFitting/test/FuncMinimizers/FABADAMinimizerTest.h
+++ Framework/CurveFitting/test/FuncMinimizers/FABADAMinimizerTest.h
@@ -157,7 +157,105 @@ public:
 
   void test_expDecay() {
     auto ws2 = createTestWorkspace();
-    doTestExpDecay(ws2);
+
+    API::IFunction_sptr fun(new ExpDecay);
+    fun->setParameter("Height", 8.);
+    fun->setParameter("Lifetime", 1.0);
+
+    Algorithms::Fit fit;
+    fit.initialize();
+    fit.setChild(true);
+    fit.setProperty("Function", fun);
+    fit.setProperty("InputWorkspace", ws2);
+    fit.setProperty("WorkspaceIndex", 0);
+    fit.setProperty("CreateOutput", true);
+    fit.setProperty("MaxIterations", 100000);
+    fit.setProperty("Minimizer", "FABADA,ChainLength=5000,StepsBetweenValues="
+                                 "10,ConvergenceCriteria=0.1,CostFunctionTable="
+                                 "CostFunction,Chains=Chain,ConvergedChain"
+                                 "=ConvergedChain,Parameters=Parameters");
+
+    TS_ASSERT_THROWS_NOTHING(fit.execute());
+    TS_ASSERT(fit.isExecuted());
+
+    TS_ASSERT_DELTA(fun->getParameter("Height"), 10.0, 0.7);
+    TS_ASSERT_DELTA(fun->getParameter("Lifetime"), 0.5, 0.1);
+    TS_ASSERT_DELTA(fun->getError(0), 0.7, 1e-1);
+    TS_ASSERT_DELTA(fun->getError(1), 0.06, 1e-2);
+
+    TS_ASSERT_EQUALS(fit.getPropertyValue("OutputStatus"), "success");
+
+    size_t nParams = fun->nParams();
+
+    // Test PDF workspace
+    MatrixWorkspace_sptr pdf = fit.getProperty("PDF");
+    TS_ASSERT(pdf);
+    TS_ASSERT_EQUALS(pdf->getNumberHistograms(), nParams + 1);
+    TS_ASSERT_EQUALS(pdf->x(0).size(), 21);
+    TS_ASSERT_EQUALS(pdf->y(0).size(), 20);
+    TS_ASSERT_DELTA(pdf->y(0)[7], 0.41, 0.01);
+    TS_ASSERT_DELTA(pdf->y(1)[8], 5.24, 0.01);
+    TS_ASSERT_DELTA(pdf->y(2)[0], 0.44, 0.01);
+
+    //  Test CostFunction table
+    ITableWorkspace_sptr costFunctTable = fit.getProperty("CostFunctionTable");
+    TS_ASSERT(costFunctTable);
+    TS_ASSERT_EQUALS(costFunctTable->columnCount(), 4);
+    TS_ASSERT_EQUALS(costFunctTable->rowCount(), 1);
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(0)->type(), "double");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(0)->name(), "Chi2min");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(1)->type(), "double");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(1)->name(), "Chi2MP");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(2)->type(), "double");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(2)->name(), "Chi2min_red");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(3)->type(), "double");
+    TS_ASSERT_EQUALS(costFunctTable->getColumn(3)->name(), "Chi2MP_red");
+    TS_ASSERT_LESS_THAN_EQUALS(costFunctTable->Double(0, 0),
+                               costFunctTable->Double(0, 1));
+    TS_ASSERT_LESS_THAN_EQUALS(costFunctTable->Double(0, 2),
+                               costFunctTable->Double(0, 3));
+    TS_ASSERT_DELTA(costFunctTable->Double(0, 0), costFunctTable->Double(0, 1),
+                    1.5);
+    TS_ASSERT_DELTA(costFunctTable->Double(0, 0), 0.0, 1.0);
+
+    // Test ConvergedChain workspace
+    MatrixWorkspace_sptr convChain = fit.getProperty("ConvergedChain");
+    TS_ASSERT(convChain);
+    TS_ASSERT_EQUALS(convChain->getNumberHistograms(), nParams + 1);
+    TS_ASSERT_EQUALS(convChain->x(0).size(), 500);
+    TS_ASSERT_EQUALS(convChain->x(0)[437], 437);
+    TS_ASSERT_DELTA(convChain->y(0)[0], 9.4, 0.1);
+    TS_ASSERT_DELTA(convChain->y(0)[249], 9.4, 0.1);
+    TS_ASSERT_DELTA(convChain->y(0)[499], 10.4, 0.1);
+    TS_ASSERT_DELTA(convChain->y(1)[0], 0.5, 0.1);
+    TS_ASSERT_DELTA(convChain->y(1)[249], 0.5, 0.1);
+    TS_ASSERT_DELTA(convChain->y(1)[499], 0.5, 0.1);
+    TS_ASSERT_DELTA(convChain->y(2)[0], 0.3, 0.1);
+    TS_ASSERT_DELTA(convChain->y(2)[249], 0.2, 0.1);
+    TS_ASSERT_DELTA(convChain->y(2)[499], 0.6, 0.1);
+
+    // Test Chain workspace
+    MatrixWorkspace_sptr chain = fit.getProperty("Chains");
+    TS_ASSERT(chain);
+    TS_ASSERT_EQUALS(chain->getNumberHistograms(), nParams + 1);
+    TS_ASSERT_EQUALS(chain->x(0)[5000], 5000);
+    TS_ASSERT(convChain->x(0).size() < chain->x(0).size());
+
+    // Parameters workspace
+    ITableWorkspace_sptr param = fit.getProperty("Parameters");
+    TS_ASSERT(param);
+    TS_ASSERT_EQUALS(param->columnCount(), 4);
+    TS_ASSERT_EQUALS(param->rowCount(), nParams);
+    TS_ASSERT_EQUALS(param->getColumn(0)->type(), "str");
+    TS_ASSERT_EQUALS(param->getColumn(0)->name(), "Name");
+    TS_ASSERT_EQUALS(param->getColumn(1)->type(), "double");
+    TS_ASSERT_EQUALS(param->getColumn(1)->name(), "Value");
+    TS_ASSERT_EQUALS(param->getColumn(2)->type(), "double");
+    TS_ASSERT_EQUALS(param->getColumn(2)->name(), "Left's error");
+    TS_ASSERT_EQUALS(param->getColumn(3)->type(), "double");
+    TS_ASSERT_EQUALS(param->getColumn(3)->name(), "Rigth's error");
+    TS_ASSERT(param->Double(0, 1) == fun->getParameter("Height"));
+    TS_ASSERT(param->Double(1, 1) == fun->getParameter("Lifetime"));
   }
 
   void test_low_MaxIterations() {
@@ -196,6 +294,7 @@ private:
       x[i] = 0.1 * double(i);
       y[i] = 10.0 * exp(-(x[i]) / 0.5);
     }
+
     return ws2;
   }
 
