22dc76061876c251030a06d141ac00529b2019e7
Steven Hahn <hahnse@ornl.gov>
2016-09-28 15:57:01 -0400

    Rename mantidpython to mantidpython.bat

--- buildconfig/CMake/FindPyUnitTest.cmake
+++ buildconfig/CMake/FindPyUnitTest.cmake
@@ -27,18 +27,16 @@ macro ( PYUNITTEST_ADD_TEST _test_src_dir _testname_prefix )
     if ( MSVC )
       # Debug
       add_test ( NAME ${_pyunit_separate_name}_Debug CONFIGURATIONS Debug
-                 COMMAND mantidpython --classic -B ${_test_src_dir}/${_filename} )
+                 COMMAND mantidpython.bat --classic -B ${_test_src_dir}/${_filename} )
       # Set the PYTHONPATH so that the built modules can be found
       set_tests_properties ( ${_pyunit_separate_name}_Debug PROPERTIES
-                             ENVIRONMENT "PYTHONPATH=${_module_dir_debug}"
                              WORKING_DIRECTORY ${_working_dir_debug}
                              TIMEOUT ${TESTING_TIMEOUT} )
       # Release
       add_test ( NAME ${_pyunit_separate_name} CONFIGURATIONS Release
-                 COMMAND mantidpython --classic -B ${_test_src_dir}/${_filename} )
+                 COMMAND mantidpython.bat --classic -B ${_test_src_dir}/${_filename} )
       # Set the PYTHONPATH so that the built modules can be found
       set_tests_properties ( ${_pyunit_separate_name} PROPERTIES
-                             ENVIRONMENT "PYTHONPATH=${_module_dir}"
                              WORKING_DIRECTORY ${_working_dir}
                              TIMEOUT ${TESTING_TIMEOUT} )
     else()
@@ -46,7 +44,6 @@ macro ( PYUNITTEST_ADD_TEST _test_src_dir _testname_prefix )
                  COMMAND mantidpython --classic -B ${_test_src_dir}/${_filename} )
       # Set the PYTHONPATH so that the built modules can be found
       set_tests_properties ( ${_pyunit_separate_name} PROPERTIES
-                             ENVIRONMENT "PYTHONPATH=${_module_dir}"
                              WORKING_DIRECTORY ${_working_dir}
                              TIMEOUT ${TESTING_TIMEOUT} )
     endif()
