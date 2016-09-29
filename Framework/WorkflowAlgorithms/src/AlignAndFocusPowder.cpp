5f2d88a5fd7b5eabbe1115421d78f54ad8c747b0
Dimitar Tasev <dbt@aber.ac.uk>
2016-09-29 11:36:17 +0100

    Re #17590 fixed LoadEventNexus and AlignAndFocusPowder uses unique_ptr

--- Framework/WorkflowAlgorithms/src/AlignAndFocusPowder.cpp
+++ Framework/WorkflowAlgorithms/src/AlignAndFocusPowder.cpp
@@ -42,11 +42,6 @@ AlignAndFocusPowder::AlignAndFocusPowder()
       tmin(0.0), tmax(0.0), m_preserveEvents(false), m_processLowResTOF(false),
       m_lowResSpecOffset(0), m_progress(nullptr) {}
 
-AlignAndFocusPowder::~AlignAndFocusPowder() {
-  if (m_progress)
-    delete m_progress;
-}
-
 const std::string AlignAndFocusPowder::name() const {
   return "AlignAndFocusPowder";
 }
@@ -354,7 +349,7 @@ void AlignAndFocusPowder::exec() {
   }
 
   // set up a progress bar with the "correct" number of steps
-  m_progress = new Progress(this, 0., 1., 22);
+  m_progress = Kernel::make_unique<Progress>(this, 0., 1., 22);
 
   if (m_inputEW) {
     double tolerance = getProperty("CompressTolerance");
