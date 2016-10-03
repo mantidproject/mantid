bd574f77daa8fe1b2c16bad61cea07a87d49e141
Simon Heybrock <simon.heybrock@esss.se>
2016-10-03 16:36:13 +0200

    Revert "Re #17670. Moved SpectrumInfo getter call outside loop."
    This reverts commit 6d3569eceeda8574b657aa89a44917fda21f6475.

--- Framework/Algorithms/src/Qxy.cpp
+++ Framework/Algorithms/src/Qxy.cpp
@@ -122,8 +122,6 @@ void Qxy::exec() {
   // Set the progress bar (1 update for every one percent increase in progress)
   Progress prog(this, 0.05, 1.0, numSpec);
 
-  const auto &spectrumInfo = inputWorkspace->spectrumInfo()
-
   //  PARALLEL_FOR2(inputWorkspace,outputWorkspace)
   for (int64_t i = 0; i < int64_t(numSpec); ++i) {
     //    PARALLEL_START_INTERUPT_REGION
@@ -194,7 +192,8 @@ void Qxy::exec() {
     // constructed once per spectrum
     GravitySANSHelper grav;
     if (doGravity) {
-      grav = GravitySANSHelper(spectrumInfo, i, getProperty("ExtraLength"));
+      grav = GravitySANSHelper(inputWorkspace->spectrumInfo(), i,
+                               getProperty("ExtraLength"));
     }
 
     for (int j = static_cast<int>(numBins) - 1; j >= static_cast<int>(wavStart);
