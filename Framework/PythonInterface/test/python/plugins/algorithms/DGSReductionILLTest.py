b469463d632e8f0c00a68062134d69c6523634a3
Ian Bush <bush@ill.fr>
2016-10-04 09:30:37 +0200

    Refs #17382 Run tests on IN4 and IN6 data

--- Framework/PythonInterface/test/python/plugins/algorithms/DGSReductionILLTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/DGSReductionILLTest.py
@@ -8,23 +8,24 @@ from mantid.simpleapi import *
 class DGSReductionILLTest(unittest.TestCase):
 
     def setUp(self):
-        self._input_file = 'ILL/IN6/164192.nxs'
-
-        Load(Filename=self._input_file, OutputWorkspace='inputWS')
-        ExtractSpectra(InputWorkspace='inputWS', OutputWorkspace ='smallWS', DetectorList = '1,2,3,1001')
+        pass
 
     def tearDown(self):
         for ws in mtd.getObjectNames():
             if mtd.doesExist(ws):
                 DeleteWorkspace(Workspace = ws)
 
-    def test_minimal(self):
+    def load(self, filename, detectorList):
+        Load(Filename=filename, OutputWorkspace='inputWS')
+        ExtractSpectra(InputWorkspace='inputWS', OutputWorkspace ='smallWS', DetectorList = detectorList)
+
+    def do_test_minimal(self):
         alg_test = run_algorithm("DGSReductionILL", InputWorkspace='smallWS', OutputWorkspace='outWS')
 
         self.assertTrue(alg_test.isExecuted())
         self.assertTrue(mtd.doesExist('outWS'))
 
-    def test_output_workspace_names(self):
+    def do_test_output_workspace_names(self):
         alg_test = run_algorithm("DGSReductionILL", InputWorkspace='smallWS', OutputWorkspace='outWS', OutputPrefix = 'test', ControlMode = True)
 
         self.assertTrue(alg_test.isExecuted())
@@ -44,6 +45,22 @@ class DGSReductionILLTest(unittest.TestCase):
         self.assertTrue(mtd.doesExist('monitorWorkspace'))
         self.assertTrue(mtd.doesExist('normFactor'))
         self.assertTrue(mtd.doesExist('workspace'))
+
+    def test_in6_minimal(self):
+        self.load('ILL/IN6/164192.nxs', '1,2,3,1001')
+        self.do_test_minimal()
+
+    def test_in4_minimal(self):
+        self.load('ILL/IN4/084446.nxs', '0,1,2,3')
+        self.do_test_minimal()
+
+    def test_in6_output_workspace_names(self):
+        self.load('ILL/IN6/164192.nxs', '1,2,3,1001')
+        self.do_test_output_workspace_names()
+
+    def test_in4_output_workspace_names(self):
+        self.load('ILL/IN4/084446.nxs', '0,1,2,3')
+        self.do_test_output_workspace_names()
 
 if __name__ == '__main__':
     unittest.main()
