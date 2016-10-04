32d306c0db3f7fb7978f0f760fc47a90329b5a50
Wenduo Zhou <zhouw@ornl.gov>
2016-10-03 23:45:31 -0400

    Refs #16930. Removed pylint warnings.

--- scripts/HFIR_4Circle_Reduction/absorption.py
+++ scripts/HFIR_4Circle_Reduction/absorption.py
@@ -1,3 +1,4 @@
+#pylint: disable=R0913,R0913
 import numpy
 import numpy.linalg
 import math
@@ -28,9 +29,9 @@ class Lattice(object):
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
-        
+
         return
-        
+
     def __str__(self):
         """
         Customized output
@@ -56,25 +57,25 @@ def m_cos(degree):
     """
     return numpy.cos(degree / 180. * numpy.pi)
 
-        
-def multiply_matrices(m1, m2):
+
+def multiply_matrices(matrix1, matrix2):
     """
     multiply 2 matrices: same as numpy.dot()
-    :param m1:
-    :param m2:
+    :param matrix1:
+    :param matrix2:
     :return:
     """
-    m3 = numpy.ndarray(shape=(3, 3), dtype='float')
-    
+    matrix3 = numpy.ndarray(shape=(3, 3), dtype='float')
+
     for i in range(3):
         for j in range(3):
-            m3[i, j] = 0.
+            matrix3[i, j] = 0.
             for k in range(3):
-                m3[i, j] += m1[i][k] * m2[k][j]
-                
-    return m3
+                matrix3[i, j] += matrix1[i][k] * matrix2[k][j]
 
-    
+    return matrix3
+
+
 def multiply_matrix_vector(matrix, vector):
     """
     multiply matrix and vector: same as numpy.dot()
@@ -87,7 +88,7 @@ def multiply_matrix_vector(matrix, vector):
         out[i] = 0
         for j in range(3):
             out[i] += matrix[i, j] * vector[j]
-            
+
     return out
 
 
@@ -182,7 +183,7 @@ def calculate_upphi(omg0, theta2ave, chiave, phiave):
     up_phi[0] = m_sin(theta2ave*0.5+omg0)*m_cos(chiave)*m_cos(phiave)+m_cos(theta2ave*0.5+omg0)*m_sin(phiave)
     up_phi[1] = m_sin(theta2ave*0.5+omg0)*m_cos(chiave)*m_sin(phiave)-m_cos(theta2ave*0.5+omg0)*m_cos(phiave)
     up_phi[2] = m_sin(theta2ave*0.5+omg0)*m_sin(chiave)
-    
+
     print '[DB...TEST] UP PHI = ', up_phi, '|UP PHI| = ', numpy.dot(up_phi, up_phi)
 
     return up_phi
@@ -195,7 +196,7 @@ def calculate_usphi(omg0, theta2ave, chiave, phiave):
     us_phi[0] = m_sin(theta2ave*0.5-omg0)*m_cos(chiave)*m_cos(phiave)-m_cos(theta2ave*0.5-omg0)*m_sin(phiave)
     us_phi[1] = m_sin(theta2ave*0.5-omg0)*m_cos(chiave)*m_sin(phiave)+m_cos(theta2ave*0.5-omg0)*m_cos(phiave)
     us_phi[2] = m_sin(theta2ave*0.5-omg0)*m_sin(chiave)
-    
+
     print '[DB...TEST] US PHI = ', us_phi, '|US PHI| = ', numpy.dot(us_phi, us_phi)
 
     return us_phi
@@ -203,22 +204,22 @@ def calculate_usphi(omg0, theta2ave, chiave, phiave):
 
 def calculate_absorption_correction_spice(exp_number, scan_number, lattice, ub_matrix):
     """
-    SPICE ub matrix -0.1482003, -0.0376897, 0.0665967, 
-                             -0.0494848, 0.2256107, 0.0025953, 
-                             -0.1702423, -0.0327691, -0.0587285, 
+    SPICE ub matrix -0.1482003, -0.0376897, 0.0665967,
+                             -0.0494848, 0.2256107, 0.0025953,
+                             -0.1702423, -0.0327691, -0.0587285,
     """
-
     # process angles from SPICE table
     theta2ave = get_average_spice_table(exp_number, scan_number, '2theta')  # sum(theta2(:))/length(theta2);
     chiave = get_average_spice_table(exp_number, scan_number, 'chi') # sum(chi(:))/length(chi);
     phiave = get_average_spice_table(exp_number, scan_number, 'phi') # sum(phi(:))/length(phi);
     omg0 = theta2ave * 0.5
-    
-    print '[DB...TEST] Exp = %d Scan = %d:\n2theta = %f, chi = %f, phi = %f, omega = %f' % (exp_number, scan_number, theta2ave, chiave, phiave, omg0)
+
+    print '[DB...TEST] Exp = %d Scan = %d:\n2theta = %f, chi = %f, phi = %f, omega = %f' \
+          '' % (exp_number, scan_number, theta2ave, chiave, phiave, omg0)
 
     upphi = calculate_upphi(omg0, theta2ave, chiave, phiave)
     usphi = calculate_usphi(omg0, theta2ave, chiave, phiave)
-    
+
     matrix_B = calculate_b_matrix(lattice)
 
     invUB = invert_matrix(ub_matrix)
@@ -227,11 +228,9 @@ def calculate_absorption_correction_spice(exp_number, scan_number, lattice, ub_m
     med_matrix = multiply_matrices(matrix_B, invUB)
     upcart = multiply_matrix_vector(med_matrix, upphi)
     multiply_matrix_vector(med_matrix, upphi)
-    
+
     uscart = numpy.dot(numpy.dot(matrix_B, invUB), usphi)
 
-    print '[DB...BAT] ', upcart, uscart
-    
     return upcart, uscart
 
 
@@ -242,15 +241,15 @@ def get_average_spice_table(exp_number, scan_number, col_name):
     # spice_table_name = 'HB3A_Exp%d_%04d_SpiceTable' % (exp_number, scan_number)
     # spice_table = mtd[spice_table_name]
     spice_table = AnalysisDataService.retrieve(spice_table_name)
-    
-    col_index = spice_table.getColumnNames().index(col_name) 
-    
+
+    col_index = spice_table.getColumnNames().index(col_name)
+
     row_number = spice_table.rowCount()
     sum_item = 0.
     for i_row in range(row_number):
         sum_item += spice_table.cell(i_row, col_index)
     avg_value = sum_item / float(row_number)
-    
+
     return avg_value
 
 
@@ -304,8 +303,8 @@ def convert_mantid_ub_to_spice(mantid_ub):
         spice_ub[1, i] = -1.*mantid_ub[2, i]
 
     return spice_ub
-    
-    
+
+
 def calculate_absorption_correction_2(exp_number, scan_number, spice_ub_matrix):
     """
     Second approach to calculate absorption correction factor without calculating B matrix
@@ -319,13 +318,13 @@ def calculate_absorption_correction_2(exp_number, scan_number, spice_ub_matrix):
     chiave = get_average_spice_table(exp_number, scan_number, 'chi')  # sum(chi(:))/length(chi);
     phiave = get_average_spice_table(exp_number, scan_number, 'phi')  # sum(phi(:))/length(phi);
     avg_omega = get_average_omega(exp_number, scan_number)
-    
-    print '[DB...TEST] Exp = %d Scan = %d:\n2theta = %f, chi = %f, phi = %f, omega = %f' % (
-        exp_number, scan_number, theta2ave, chiave, phiave, avg_omega)
+
+    print '[DB...TEST] Exp = %d Scan = %d:\n2theta = %f, chi = %f, phi = %f, omega = %s' % (
+        exp_number, scan_number, theta2ave, chiave, phiave, str(avg_omega))
 
     up_phi = calculate_upphi(avg_omega, theta2ave, chiave, phiave)
     us_phi = calculate_usphi(avg_omega, theta2ave, chiave, phiave)
-    
+
     vec_q = numpy.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     matrix_q = numpy.dot(spice_ub_matrix, vec_q)
     h1c = matrix_q[:, 0]
@@ -340,14 +339,14 @@ def calculate_absorption_correction_2(exp_number, scan_number, spice_ub_matrix):
     up_cart[0] = numpy.dot(up_phi, t1c)
     up_cart[1] = numpy.dot(up_phi, t2c)
     up_cart[2] = numpy.dot(up_phi, t3c)
-    
+
     us_cart = numpy.ndarray(shape=(3,))
     us_cart[0] = numpy.dot(us_phi, t1c)
     us_cart[1] = numpy.dot(us_phi, t2c)
     us_cart[2] = numpy.dot(us_phi, t3c)
     
     return up_cart, us_cart
-    
+
 # Test ... ...
 # test_lattice = Lattice(4.32765, 4.32765, 11.25736, 90., 90., 90.)
 # ub_matrix = numpy.array([[-0.1482003, -0.0376897, 0.0665967], [-0.0494848, 0.2256107, 0.0025953],[-0.1702423, -0.0327691, -0.0587285]])
