6fd3df531e361fbdd44b70e120827f761a187ee7
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-10-13 09:31:39 +0100

    Refs #17787 Make LoadLotsOfFiles pass

--- Testing/SystemTests/tests/analysis/LoadLotsOfFiles.py
+++ Testing/SystemTests/tests/analysis/LoadLotsOfFiles.py
@@ -7,6 +7,8 @@ import re
 import stresstesting
 
 BANNED_FILES = ['80_tubes_Top_and_Bottom_April_2015.xml',
+                '80_tubes_Top_and_Bottom_May_2016.xml',
+                '80tubeCalibration_18-04-2016_r9330-9335.nxs',
                 '80tube_DIRECT_3146_M1_30April15_r3146.dat',
                 '992 Descriptions.txt',
                 'directBeamDatabaseFall2014_IPTS_11601_2.cfg',
@@ -21,6 +23,7 @@ BANNED_FILES = ['80_tubes_Top_and_Bottom_April_2015.xml',
                 'DISF_NaF.cdl',
                 'det_corrected7.dat',
                 'det_LET_cycle12-3.dat',
+                'DIRECT_M1_21Nov15_6x8mm_0.9_20.0_r6279_extrapolated.dat',
                 'eqsans_configuration.1463',
                 'FLAT_CELL.061',
                 'HYSA_mask.xml',
@@ -43,6 +46,8 @@ BANNED_FILES = ['80_tubes_Top_and_Bottom_April_2015.xml',
                 'MASK_SANS2D_REAR_Bottom_3_tubes_16May2014.xml',
                 'MASK_SANS2D_REAR_Edges_16Mar2015.xml',
                 'MASK_SANS2D_BOTH_Extras_24Mar2015.xml',
+                'MASK_Tube6.xml',
+                'MASK_squareBeamstop_6x8Beam_11-October-2016.xml',
                 'MAP17269.raw', # Don't need to check multiple MAPS files
                 'MAP17589.raw',
                 'MER06399.raw', # Don't need to check multiple MERLIN files
@@ -83,6 +88,7 @@ BANNED_FILES = ['80_tubes_Top_and_Bottom_April_2015.xml',
                 'poldi2015n000977.hdf',
                 'USER_SANS2D_143ZC_2p4_4m_M4_Knowles_12mm.txt',
                 'USER_LARMOR_151B_LarmorTeam_80tubes_BenchRot1p4_M4_r3699.txt',
+                'USER_Larmor_163F_HePATest_r13038.txt',
                 'Vesuvio_IP_file_test.par',
                 'IP0004_10.par'
                ]
