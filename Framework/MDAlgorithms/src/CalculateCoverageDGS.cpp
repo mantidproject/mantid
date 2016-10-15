a8a9c06cea1732c773feb5d6cd0736cb8ea8c09c
Steven Hahn <hahnse@ornl.gov>
2016-10-15 23:28:31 +0200

    Fix build error.

--- Framework/MDAlgorithms/src/CalculateCoverageDGS.cpp
+++ Framework/MDAlgorithms/src/CalculateCoverageDGS.cpp
@@ -403,7 +403,7 @@ void CalculateCoverageDGS::exec() {
 
   const int64_t ndets = static_cast<int64_t>(tt.size());
 
-  PARALLEL_FOR_IF(Kernel::threadSafe(inputWS))
+  PARALLEL_FOR_IF(Kernel::threadSafe(*inputWS))
   for (int64_t i = 0; i < ndets; i++) {
     PARALLEL_START_INTERUPT_REGION
     auto intersections = calculateIntersections(tt[i], phi[i]);
