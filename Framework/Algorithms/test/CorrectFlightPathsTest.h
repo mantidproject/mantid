f0e33e728156dc807efeb26ed66e1a1ceac90e4f
Ian Bush <bush@ill.fr>
2016-10-24 17:01:36 +0200

    Refs #17873 Better tests for CorrectFlightPaths catching failure

--- Framework/Algorithms/test/CorrectFlightPathsTest.h
+++ Framework/Algorithms/test/CorrectFlightPathsTest.h
@@ -30,8 +30,7 @@ public:
   static void destroySuite(CorrectFlightPathsTest *suite) { delete suite; }
 
   CorrectFlightPathsTest()
-      : // alter here if needed
-        m_l2(4) {}
+      : m_l2(4) {}
 
   void testTheBasics() {
     CorrectFlightPaths c;
@@ -40,41 +39,20 @@ public:
   }
 
   void testExec() {
-
     std::string inputWSName("test_input_ws");
     std::string outputWSName("test_output_ws");
 
-    std::vector<double> L2(5, 5);
-    std::vector<double> polar(5, (30. / 180.) * M_PI);
-    polar[0] = 0;
-    std::vector<double> azimutal(5, 0);
-    azimutal[1] = (45. / 180.) * M_PI;
-    azimutal[2] = (90. / 180.) * M_PI;
-    azimutal[3] = (135. / 180.) * M_PI;
-    azimutal[4] = (180. / 180.) * M_PI;
+    int numberOfAngles = 5;
+    int numberOfBins = 10;
 
-    int numBins = 10;
-    Mantid::API::MatrixWorkspace_sptr dataws =
-        WorkspaceCreationHelper::createProcessedInelasticWS(L2, polar, azimutal,
-                                                            numBins, -1, 3, 3);
+    auto inputWS = createTestWorkspace(numberOfAngles, numberOfBins, inputWSName);
 
-    dataws->getAxis(0)->setUnit("TOF");
-    dataws->mutableRun().addProperty("wavelength",
-                                     boost::lexical_cast<std::string>(5));
-
-    dataws->instrumentParameters().addString(
-        dataws->getInstrument()->getComponentID(), "l2",
-        boost::lexical_cast<std::string>(m_l2));
-
-    API::AnalysisDataService::Instance().addOrReplace(inputWSName, dataws);
-
-    // BEFORE
-    for (int i = 0; i < 5; i++) {
-      Mantid::Geometry::IDetector_const_sptr det = dataws->getDetector(i);
+    // BEFORE - check the L2 values differ
+    for (int i = 0; i < numberOfAngles; i++) {
+      Mantid::Geometry::IDetector_const_sptr det = inputWS->getDetector(i);
       double r, theta, phi;
       Mantid::Kernel::V3D pos = det->getPos();
       pos.getSpherical(r, theta, phi);
-      // Corrected distance to 4!
       TS_ASSERT_DIFFERS(r, m_l2)
     }
 
@@ -87,14 +65,11 @@ public:
     c.execute();
     TS_ASSERT(c.isExecuted());
 
-    Mantid::API::MatrixWorkspace_const_sptr output;
-    output = Mantid::API::AnalysisDataService::Instance()
-                 .retrieveWS<Mantid::API::MatrixWorkspace>(outputWSName);
+    Mantid::API::MatrixWorkspace_const_sptr outputWS = Mantid::API::AnalysisDataService::Instance().retrieveWS<Mantid::API::MatrixWorkspace>(outputWSName);
 
-    // AFTER
-    // test the first tube to see if distance was well corrected to l2
-    for (int i = 0; i < 5; i++) {
-      Mantid::Geometry::IDetector_const_sptr det = output->getDetector(i);
+    // AFTER - test the first tube to see if distance was well corrected to l2
+    for (int i = 0; i < numberOfAngles; i++) {
+      Mantid::Geometry::IDetector_const_sptr det = outputWS->getDetector(i);
       double r, theta, phi;
       Mantid::Kernel::V3D pos = det->getPos();
       pos.getSpherical(r, theta, phi);
@@ -103,12 +78,43 @@ public:
       TS_ASSERT_DELTA(r, m_l2, 0.001)
     }
 
+    // Check the contents of the y and e parts of the histogram are the same, and x differs
+    for (int i = 0; i < numberOfAngles; i++) {
+      TS_ASSERT_DIFFERS(outputWS->x(i).rawData(), inputWS->x(i).rawData());
+      TS_ASSERT_EQUALS(outputWS->y(i).rawData(), inputWS->y(i).rawData());
+      TS_ASSERT_EQUALS(outputWS->e(i).rawData(), inputWS->e(i).rawData());
+    }
+
     AnalysisDataService::Instance().remove(outputWSName);
     AnalysisDataService::Instance().remove(inputWSName);
   }
 
 private:
   int m_l2;
+
+  MatrixWorkspace_sptr createTestWorkspace(int numberOfAngles, int numberOfBins, std::string inputWSName) {
+    std::vector<double> L2(numberOfAngles, 5);
+    std::vector<double> polar(numberOfAngles, (30. / 180.) * M_PI);
+    polar[0] = 0;
+    std::vector<double> azimutal(numberOfAngles, 0);
+    azimutal[1] = (45. / 180.) * M_PI;
+    azimutal[2] = (90. / 180.) * M_PI;
+    azimutal[3] = (135. / 180.) * M_PI;
+    azimutal[4] = (180. / 180.) * M_PI;
+
+    Mantid::API::MatrixWorkspace_sptr inputWS =
+        WorkspaceCreationHelper::createProcessedInelasticWS(L2, polar, azimutal, numberOfBins, -1, 3, 3);
+
+    inputWS->getAxis(0)->setUnit("TOF");
+    inputWS->mutableRun().addProperty("wavelength", boost::lexical_cast<std::string>(numberOfAngles));
+
+    inputWS->instrumentParameters().addString(inputWS->getInstrument()->getComponentID(), "l2", boost::lexical_cast<std::string>(m_l2));
+
+    API::AnalysisDataService::Instance().addOrReplace(inputWSName, inputWS);
+
+    return inputWS;
+  }
+
 };
 
 #endif /* MANTID_ALGORITHMS_CORRECTFLIGHTPATHSTEST_H_ */
