b3cfa0ff9bb48fa2214f3c1709ed94a19324a6b8
Louise McCann <louise.mccann@stfc.ac.uk>
2016-10-25 12:18:38 +0100

    Refs #14616 refactor unit tests

--- Framework/PythonInterface/test/python/plugins/algorithms/EnergyWindowScanTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/EnergyWindowScanTest.py
@@ -7,7 +7,7 @@ from mantid.api import mtd
 
 class IndirectEnergyWindowScanTest(unittest.TestCase):
     def test_IRIS(self):
-        EnergyWindowScan(InputFiles="26176, 26173", Instrument='IRIS', Analyser='graphite',
+        EnergyWindowScan(InputFiles="IRS26176.RAW, IRS26173.RAW", Instrument='IRIS', Analyser='graphite',
                          Reflection='002', SpectraRange='3, 50', ElasticRange='-0.5, 0',
                          InelasticRange='0, 0.5', GroupingMethod='All')
         scan_ws = mtd['Scan_eisf']
@@ -15,7 +15,7 @@ class IndirectEnergyWindowScanTest(unittest.TestCase):
         self.assertEqual(round(scan_ws.readY(1)[0], 7), 1.0528091)
 
     def test_OSIRIS(self):
-        EnergyWindowScan(InputFiles="OSI100319, OSI100326", Instrument='OSIRIS', Analyser='graphite',
+        EnergyWindowScan(InputFiles="OSI100319.RAW, OSI100326.RAW", Instrument='OSIRIS', Analyser='graphite',
                          Reflection='002', SpectraRange='963, 1004', ElasticRange='-1.0, 0.',
                          InelasticRange='0, 1.5', GroupingMethod='All')
         scan_ws = mtd['Scan_eisf']
@@ -23,7 +23,7 @@ class IndirectEnergyWindowScanTest(unittest.TestCase):
         self.assertEqual(round(scan_ws.readY(1)[0], 7), 0.8224473)
 
     def test_MSDfit(self):
-        EnergyWindowScan(InputFiles="26176", Instrument='IRIS', Analyser='graphite',
+        EnergyWindowScan(InputFiles="IRS26176.RAW", Instrument='IRIS', Analyser='graphite',
                          Reflection='002', SpectraRange='3, 50', ElasticRange='-0.5, 0',
                          InelasticRange='0, 0.5', GroupingMethod='Individual', MSDFit=True)
         scan_ws = mtd['Scan_el_eq2_0_Workspace']
