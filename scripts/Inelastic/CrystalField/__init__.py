7d19aeb7c21d9422ba16e485761310e2eb59a459
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2016-10-26 11:34:19 +0100

    Re #17517. Trying to fix a python warning.

--- scripts/Inelastic/CrystalField/__init__.py
+++ scripts/Inelastic/CrystalField/__init__.py
@@ -1,2 +1,4 @@
 from .fitting import CrystalField, CrystalFieldFit, CrystalFieldMulti
 from .function import PeaksFunction, Background, Function, ResolutionModel
+__all__ = ['CrystalField', 'CrystalFieldFit', 'CrystalFieldMulti', 'PeaksFunction',
+           'Background', 'Function', 'ResolutionModel']
