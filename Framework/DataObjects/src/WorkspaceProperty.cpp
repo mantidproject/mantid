9fccad71653310e6ff6beed63b51d1af5c2865bc
Simon Heybrock <simon.heybrock@esss.se>
2016-10-26 12:11:19 +0200

    Re #17859. Clarified code comment.

--- Framework/DataObjects/src/WorkspaceProperty.cpp
+++ Framework/DataObjects/src/WorkspaceProperty.cpp
@@ -6,7 +6,8 @@
 #include "MantidAPI/WorkspaceProperty.tcc"
 
 namespace Mantid {
-// Note that this file is part of DataObjects, but we are extending API
+// Note that this file is part of DataObjects, but we are injecting explicit
+// instantiations into API. This does not extend or modify API.
 namespace API {
 ///@cond TEMPLATE
 template class MANTID_API_DLL
