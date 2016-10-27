8b9ed683020dce989ec9cf677cabe295c021b342
Simon Heybrock <simon.heybrock@esss.se>
2016-10-27 09:28:33 +0200

    Re #17859. Trying to fix unrelated(?) cppcheck warning.

--- Framework/Algorithms/src/NormaliseByCurrent.cpp
+++ Framework/Algorithms/src/NormaliseByCurrent.cpp
@@ -52,7 +52,6 @@ double NormaliseByCurrent::extractCharge(
   }
   // Handle multiperiod data.
   // The number of periods is set above by reference
-  // cppcheck-suppress knownConditionTrueFalse
   if (nPeriods > 1) {
     // Fetch the period property
     Property *currentPeriodNumberProperty = run.getLogData("current_period");
