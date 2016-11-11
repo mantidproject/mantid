f29b7ed788e27674346cee626c68fe2ac301a76d
Dimitar Tasev <dbt@aber.ac.uk>
2016-11-11 09:41:42 +0000

    Re #17244 clang format

--- MantidQt/CustomInterfaces/src/Tomography/TomographyIfaceModel.cpp
+++ MantidQt/CustomInterfaces/src/Tomography/TomographyIfaceModel.cpp
@@ -244,8 +244,7 @@ bool TomographyIfaceModel::doPing(const std::string &compRes) {
     tid = alg->getPropertyValue("TransactionID");
     g_log.information() << "Pinged '" << compRes
                         << "'succesfully. Checked that a transaction could "
-                           "be created, with ID: "
-                        << tid << '\n';
+                           "be created, with ID: " << tid << '\n';
   } catch (std::runtime_error &e) {
     throw std::runtime_error("Error. Failed to ping and start a transaction on "
                              "the remote resource." +
