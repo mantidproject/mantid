14d9aa48d3c578765481eba6195a66602ebde976
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2016-11-25 15:13:52 +0000

    removed some flake8 warnings. re #17529

--- scripts/AbinsModules/AbinsData.py
+++ scripts/AbinsModules/AbinsData.py
@@ -4,6 +4,7 @@ from GeneralData import  GeneralData
 from KpointsData import  KpointsData
 from AtomsData import  AtomsDaTa
 
+
 class AbinsData(GeneralData):
     """
     Class for storing input DFT data.
@@ -13,7 +14,6 @@ class AbinsData(GeneralData):
         super(AbinsData, self).__init__()
         self._atoms_data = None
         self._kpoints_data = None
-
 
     def set(self, k_points_data=None, atoms_data=None):
         """
@@ -34,21 +34,17 @@ class AbinsData(GeneralData):
 
         self._data = {"k_points_data":k_points_data.extract(), "atoms_data":atoms_data.extract()}
 
-
     def getKpointsData(self):
         return self._kpoints_data
 
-
     def getAtomsData(self):
         return self._atoms_data
-
 
     def extract(self):
         if self._data["k_points_data"]["atomic_displacements"].shape[1] != len(self._data["atoms_data"]):
             raise ValueError("Abins data is inconsistent.")
 
         return self._data
-
 
     def __str__(self):
         return "DFT data"
