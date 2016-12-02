414793057dd34f046f587207502dc9e8ffe2cc80
Antti Soininen <soininen@ill.fr>
2016-12-02 16:49:51 +0100

    Cleanup and improve unit tests.
    Re #18091

--- Framework/DataHandling/test/LoadILLTOF2Test.h
+++ Framework/DataHandling/test/LoadILLTOF2Test.h
@@ -2,9 +2,9 @@
 #define LOADILLTOF2TEST_H_
 
 #include <cxxtest/TestSuite.h>
-#include "MantidDataHandling/LoadILLTOF2.h"
 #include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/MatrixWorkspace.h"
+#include "MantidDataHandling/LoadILLTOF2.h"
 
 using namespace Mantid::API;
 using Mantid::DataHandling::LoadILLTOF2;
@@ -20,18 +20,19 @@ public:
 
   void testName() {
     LoadILLTOF2 loader;
-    TS_ASSERT_EQUALS(loader.name(), "LoadILLTOF");
+    TS_ASSERT_EQUALS( loader.name(), "LoadILLTOF" )
   }
 
   void testVersion() {
     LoadILLTOF2 loader;
-    TS_ASSERT_EQUALS(loader.version(), 2);
+    TS_ASSERT_EQUALS( loader.version(), 2 )
   }
 
   void testInit() {
     LoadILLTOF2 loader;
-    TS_ASSERT_THROWS_NOTHING(loader.initialize());
-    TS_ASSERT(loader.isInitialized());
+    loader.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING( loader.initialize() )
+    TS_ASSERT( loader.isInitialized() )
   }
 
   /*
@@ -39,53 +40,84 @@ public:
    * The elastic peak is obtained on the fly from the sample data.
    */
   MatrixWorkspace_sptr loadDataFile(const std::string dataFile,
-                                    const int numberOfHistograms) {
+                                    const size_t numberOfHistograms, const size_t numberOfChannels, const double tofDelay, const double tofChannelWidth) {
     LoadILLTOF2 loader;
     loader.setRethrows(true);
-    loader.initialize();
-    loader.setPropertyValue("Filename", dataFile);
+    TS_ASSERT_THROWS_NOTHING( loader.initialize() )
+    TS_ASSERT_THROWS_NOTHING( loader.setPropertyValue("Filename", dataFile) )
 
     std::string outputSpace = "LoadILLTOFTest_out";
-    loader.setPropertyValue("OutputWorkspace", outputSpace);
-    TS_ASSERT_THROWS_NOTHING(loader.execute());
+    TS_ASSERT_THROWS_NOTHING( loader.setPropertyValue("OutputWorkspace", outputSpace) )
+    TS_ASSERT_THROWS_NOTHING( loader.execute() )
 
     MatrixWorkspace_sptr output =
         AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(
             outputSpace);
-    MatrixWorkspace_sptr output2D =
-        boost::dynamic_pointer_cast<MatrixWorkspace>(output);
 
-    TS_ASSERT_EQUALS(output2D->getNumberHistograms(), numberOfHistograms);
+    TS_ASSERT_EQUALS( output->getNumberHistograms(), numberOfHistograms )
+    for (size_t wsIndex = 0; wsIndex != output->getNumberHistograms(); ++wsIndex) {
+      const auto histogram = output->histogram(wsIndex);
+      TS_ASSERT_EQUALS( histogram.xMode(), Mantid::HistogramData::Histogram::XMode::BinEdges )
+      TS_ASSERT_EQUALS( histogram.yMode(), Mantid::HistogramData::Histogram::YMode::Counts )
+      TS_ASSERT_EQUALS( histogram.size(), numberOfChannels )
+      const auto &xs = histogram.x();
+      for (size_t channelIndex = 0; channelIndex != xs.size(); ++channelIndex) {
+        const double binEdge = tofDelay + static_cast<double>(channelIndex) * tofChannelWidth - tofChannelWidth / 2;
+        TS_ASSERT_DELTA( xs[channelIndex], binEdge, 1e-3 )
+      }
+      const auto &ys = histogram.y();
+      const auto &es = histogram.e();
+      for (size_t channelIndex = 0; channelIndex != es.size(); ++channelIndex) {
+        TS_ASSERT_EQUALS( es[channelIndex], std::sqrt(ys[channelIndex]) )
+      }
+    }
 
     // Check all detectors have a defined detector ID >= 0
     Mantid::detid2index_map detectorMap;
     TS_ASSERT_THROWS_NOTHING(
-        detectorMap = output->getDetectorIDToWorkspaceIndexMap(true));
+        detectorMap = output->getDetectorIDToWorkspaceIndexMap(true) )
 
     // Check all detectors have a unique detector ID
-    TS_ASSERT_EQUALS(detectorMap.size(), output->getNumberHistograms());
+    TS_ASSERT_EQUALS( detectorMap.size(), output->getNumberHistograms() )
 
-    for (auto value : detectorMap) {
-      TS_ASSERT(value.first >= 0);
+    for (const auto value : detectorMap) {
+      TS_ASSERT( value.first >= 0 )
     }
 
-    return output2D;
+    return output;
   }
 
   void test_IN4_load() {
-    MatrixWorkspace_sptr ws = loadDataFile("ILL/IN4/084446.nxs", 397);
+    // From the input test file.
+    const double tofDelay = 238.34;
+    const double tofChannelWidth = 5.85;
+    const size_t channelCount = 512;
+    const size_t histogramCount = 397;
+    MatrixWorkspace_sptr ws = loadDataFile("ILL/IN4/084446.nxs", histogramCount, channelCount, tofDelay, tofChannelWidth);
 
-    double pulseInterval = ws->run().getLogAsSingleValue("pulse_interval");
-    TS_ASSERT_DELTA(0.003, pulseInterval, 1e-10);
+    const double pulseInterval = ws->run().getLogAsSingleValue("pulse_interval");
+    TS_ASSERT_DELTA( 0.003, pulseInterval, 1e-10 )
   }
 
-  void test_IN5_load() { loadDataFile("ILL/IN5/104007.nxs", 98305); }
+  void test_IN5_load() {
+    // From the input test file.
+    const double tofDelay = 5982.856;
+    const double tofChannelWidth = 14.6349;
+    const size_t channelCount = 512;
+    const size_t histogramCount = 98305;
+    loadDataFile("ILL/IN5/104007.nxs", histogramCount, channelCount, tofDelay, tofChannelWidth);
+  }
 
   void test_IN6_load() {
-    MatrixWorkspace_sptr ws = loadDataFile("ILL/IN6/164192.nxs", 340);
+    // From the input test file.
+    const double tofDelay = 430;
+    const double tofChannelWidth = 5.8;
+    const size_t channelCount = 1024;
+    const size_t histogramCount = 340;
+    MatrixWorkspace_sptr ws = loadDataFile("ILL/IN6/164192.nxs", histogramCount, channelCount, tofDelay, tofChannelWidth);
 
-    double pulseInterval = ws->run().getLogAsSingleValue("pulse_interval");
-    TS_ASSERT_DELTA(0.0060337892, pulseInterval, 1e-10);
+    const double pulseInterval = ws->run().getLogAsSingleValue("pulse_interval");
+    TS_ASSERT_DELTA( 0.0060337892, pulseInterval, 1e-10 )
   }
 };
 
@@ -99,10 +131,12 @@ public:
 
   void testDefaultLoad() {
     Mantid::DataHandling::LoadILLTOF2 loader;
-    loader.initialize();
-    loader.setPropertyValue("Filename", m_dataFile);
-    loader.setPropertyValue("OutputWorkspace", "ws");
-    TS_ASSERT(loader.execute());
+    loader.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING( loader.initialize() )
+    TS_ASSERT_THROWS_NOTHING( loader.setPropertyValue("Filename", m_dataFile) )
+    TS_ASSERT_THROWS_NOTHING( loader.setPropertyValue("OutputWorkspace", "ws") )
+    TS_ASSERT_THROWS_NOTHING( loader.execute() )
+    TS_ASSERT( loader.isExecuted() )
   }
 
 private:
