71532a2db4398e1e4902137a3c62645b9f0b7b89
Ian Bush <bush@ill.fr>
2016-12-06 18:10:24 +0100

    Refs #18031 Removed unimplemented method from SpectrumInfo.h

--- Framework/API/inc/MantidAPI/SpectrumInfo.h
+++ Framework/API/inc/MantidAPI/SpectrumInfo.h
@@ -86,7 +86,6 @@ public:
 
 private:
   const Geometry::IDetector &getDetector(const size_t index) const;
-  const std::set<detid_t> getValidDetectorIDs(const size_t index) const;
   std::vector<boost::shared_ptr<const Geometry::IDetector>>
   getDetectorVector(const size_t index) const;
 
