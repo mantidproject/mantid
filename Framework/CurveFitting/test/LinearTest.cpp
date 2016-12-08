112ce6a417a2b714c2543b21cfb8ed922118a34b
Verena Reimund <reimund@ill.eu>
2016-12-08 10:43:09 +0100

    Re #18097 apply clang patch

--- Framework/CurveFitting/test/LinearTest.cpp
+++ Framework/CurveFitting/test/LinearTest.cpp
@@ -18,18 +18,35 @@ bool suite_LinearTest_init = false;
 
 static LinearTest *suite_LinearTest = 0;
 
-static CxxTest::List Tests_LinearTest = { 0, 0 };
-CxxTest::DynamicSuiteDescription<LinearTest> suiteDescription_LinearTest( "/home/cs/reimund/mantid/Framework/CurveFitting/test/Functions/LinearTest.h", 12, "LinearTest", Tests_LinearTest, suite_LinearTest, 16, 19 );
+static CxxTest::List Tests_LinearTest = {0, 0};
+CxxTest::DynamicSuiteDescription<LinearTest>
+    suiteDescription_LinearTest("/home/cs/reimund/mantid/Framework/"
+                                "CurveFitting/test/Functions/LinearTest.h",
+                                12, "LinearTest", Tests_LinearTest,
+                                suite_LinearTest, 16, 19);
 
-static class TestDescription_suite_LinearTest_test_category final : public CxxTest::RealTestDescription {
+static class TestDescription_suite_LinearTest_test_category final
+    : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_LinearTest_test_category() : CxxTest::RealTestDescription( Tests_LinearTest, suiteDescription_LinearTest, 21, "test_category" ) {}
- void runTest() override final { if ( suite_LinearTest ) suite_LinearTest->test_category(); }
+  TestDescription_suite_LinearTest_test_category()
+      : CxxTest::RealTestDescription(Tests_LinearTest,
+                                     suiteDescription_LinearTest, 21,
+                                     "test_category") {}
+  void runTest() override final {
+    if (suite_LinearTest)
+      suite_LinearTest->test_category();
+  }
 } testDescription_suite_LinearTest_test_category;
 
-static class TestDescription_suite_LinearTest_test_calculate final : public CxxTest::RealTestDescription {
+static class TestDescription_suite_LinearTest_test_calculate final
+    : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_LinearTest_test_calculate() : CxxTest::RealTestDescription( Tests_LinearTest, suiteDescription_LinearTest, 33, "test_calculate" ) {}
- void runTest() override final { if ( suite_LinearTest ) suite_LinearTest->test_calculate(); }
+  TestDescription_suite_LinearTest_test_calculate()
+      : CxxTest::RealTestDescription(Tests_LinearTest,
+                                     suiteDescription_LinearTest, 33,
+                                     "test_calculate") {}
+  void runTest() override final {
+    if (suite_LinearTest)
+      suite_LinearTest->test_calculate();
+  }
 } testDescription_suite_LinearTest_test_calculate;
-
