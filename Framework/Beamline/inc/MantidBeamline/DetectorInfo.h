0dae29655c5497d0430030cc07119eb2e1cd7bac
Simon Heybrock <simon.heybrock@esss.se>
2016-12-13 12:46:40 +0100

    Re #18268. Doxygen for Beamline::DetectorInfo.

--- Framework/Beamline/inc/MantidBeamline/DetectorInfo.h
+++ Framework/Beamline/inc/MantidBeamline/DetectorInfo.h
@@ -6,7 +6,31 @@
 namespace Mantid {
 namespace Beamline {
 
-/** DetectorInfo : TODO: DESCRIPTION
+/** Beamline::DetectorInfo provides easy access to commonly used parameters of
+  individual detectors (pixels) in a beamline, such as mask and monitor flags,
+  positions, L2, and 2-theta.
+
+  Currently only a limited subset of functionality is implemented in
+  Beamline::DetectorInfo. The remainder is available in API::DetectorInfo which
+  acts as a wrapper around the old instrument implementation. API::DetectorInfo
+  will be removed once all functionality has been moved to
+  Beamline::DetectorInfo. For the time being, API::DetectorInfo will forward
+  calls to Beamline::DetectorInfo when applicable.
+
+  The reason for having both DetectorInfo classes in parallel is:
+  - We need to be able to move around the DetectorInfo object including data it
+    contains such as a vector of mask flags. This is relevant for the interface
+    of ExperimentInfo, when replacing the ParameterMap or when setting a new
+    instrument.
+  - API::DetectorInfo contains a caching mechanism and is frequently flushed
+    upon modification of the instrument and is thus hard to handle outside the
+    context of its owning workspace.
+  Splitting DetectorInfo into two classes seemed to be the safest and easiest
+  solution to this.
+
+
+  @author Simon Heybrock
+  @date 2016
 
   Copyright &copy; 2016 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
   National Laboratory & European Spallation Source
--- Framework/Beamline/src/DetectorInfo.cpp
+++ Framework/Beamline/src/DetectorInfo.cpp
@@ -6,6 +6,8 @@ namespace Beamline {
 DetectorInfo::DetectorInfo(const size_t numberOfDetectors)
     : m_size(numberOfDetectors) {}
 
+/// Returns the size of the DetectorInfo, i.e., the number of detectors in the
+/// instrument.
 size_t DetectorInfo::size() const { return m_size; }
 
 } // namespace Beamline
