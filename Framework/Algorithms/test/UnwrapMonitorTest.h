7acd354e27a32ef1b080b8b4d5a2c698e5d9972e
Gemma Guest <gemma.guest@stfc.ac.uk>
2016-12-20 15:01:45 +0000

    Re #18215 Add more tests, and tidy code

--- Framework/Algorithms/test/UnwrapMonitorTest.h
+++ Framework/Algorithms/test/UnwrapMonitorTest.h
@@ -24,7 +24,7 @@ private:
   void makeFakeWorkspace(std::string wsName) {
     Workspace2D_sptr test_in =
         WorkspaceCreationHelper::create2DWorkspaceWithRectangularInstrument(
-            2, 10, 50);
+            2, 3, 50);
     test_in->getAxis(0)->setUnit("TOF");
 
     // Add it to the workspace
@@ -63,20 +63,20 @@ public:
     // Check some x values
     const Mantid::MantidVec outX = ws->readX(0);
     TS_ASSERT_EQUALS(outX.size(), 23);
-    TS_ASSERT_EQUALS(outX.front(), 0.0);
-    TS_ASSERT_DELTA(outX.back(), 0.017982, 0.000001);
+    TS_ASSERT_DELTA(outX.front(), 0.0, 1e-6);
+    TS_ASSERT_DELTA(outX.back(), 0.017982, 1e-6);
 
     // Check some y values
     const Mantid::MantidVec outY = ws->readY(0);
     TS_ASSERT_EQUALS(outY.size(), 22);
-    TS_ASSERT_EQUALS(outY[3], 0.0);
-    TS_ASSERT_DELTA(outY[4], 2.991736, 0.000001);
-    TS_ASSERT_DELTA(outY[8], 6.198347, 0.000001);
-    TS_ASSERT_DELTA(outY[11], 3.818182, 0.000001);
-    TS_ASSERT_EQUALS(outY[12], 0.0);
+    TS_ASSERT_DELTA(outY[3], 0.0, 1e-6);
+    TS_ASSERT_DELTA(outY[4], 2.991736, 1e-6);
+    TS_ASSERT_DELTA(outY[8], 6.198347, 1e-6);
+    TS_ASSERT_DELTA(outY[11], 3.818182, 1e-6);
+    TS_ASSERT_DELTA(outY[12], 0.0, 1e-6);
 
     double joinWavelength = algo.getProperty("JoinWavelength");
-    TS_ASSERT_DELTA(joinWavelength, 0.003692, 0.000001);
+    TS_ASSERT_DELTA(joinWavelength, 0.003692, 1e-6);
 
     AnalysisDataService::Instance().remove(name);
   }
@@ -104,8 +104,11 @@ public:
     TS_ASSERT_EQUALS(nHist,
                      ws->getNumberHistograms()); // shouldn't drop histograms
 
+    const Mantid::MantidVec outY = ws->readY(0);
+    TS_ASSERT_EQUALS(outY.size(), 44);
+
     double joinWavelength = algo.getProperty("JoinWavelength");
-    TS_ASSERT_DELTA(joinWavelength, 0.001582, 0.000001);
+    TS_ASSERT_DELTA(joinWavelength, 0.001582, 1e-6);
 
     AnalysisDataService::Instance().remove(name);
   }
@@ -133,8 +136,77 @@ public:
     TS_ASSERT_EQUALS(nHist,
                      ws->getNumberHistograms()); // shouldn't drop histograms
 
+    const Mantid::MantidVec outY = ws->readY(0);
+    TS_ASSERT_EQUALS(outY.size(), 49);
+
     double joinWavelength = algo.getProperty("JoinWavelength");
-    TS_ASSERT_DELTA(joinWavelength, 0.000264, 0.000001);
+    TS_ASSERT_DELTA(joinWavelength, 0.000264, 1e-6);
+
+    AnalysisDataService::Instance().remove(name);
+  }
+
+  void testMinPossibleLref() {
+    // setup
+    std::string name("UnwrapMonitor");
+    this->makeFakeWorkspace(name);
+    Workspace2D_sptr ws =
+        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
+    size_t nHist = ws->getNumberHistograms();
+
+    // run the algorithm
+    UnwrapMonitor algo;
+    if (!algo.isInitialized())
+      algo.initialize();
+    algo.setPropertyValue("InputWorkspace", name);
+    algo.setPropertyValue("OutputWorkspace", name);
+    algo.setProperty("LRef", 0.01);
+    TS_ASSERT(algo.execute());
+    TS_ASSERT(algo.isExecuted());
+
+    // verify the output workspace
+    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
+    TS_ASSERT_EQUALS(nHist,
+                     ws->getNumberHistograms()); // shouldn't drop histograms
+
+    const Mantid::MantidVec outY = ws->readY(0);
+    TS_ASSERT_EQUALS(outY.size(), 50);
+    TS_ASSERT_DELTA(outY[0], 100.0, 1e-6);
+    TS_ASSERT_DELTA(outY[1], 0.0, 1e-6);
+
+    double joinWavelength = algo.getProperty("JoinWavelength");
+    TS_ASSERT_DELTA(joinWavelength, 0.0, 1e-6);
+
+    AnalysisDataService::Instance().remove(name);
+  }
+
+  void testLargeLref() {
+    // setup
+    std::string name("UnwrapMonitor");
+    this->makeFakeWorkspace(name);
+    Workspace2D_sptr ws =
+        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
+    size_t nHist = ws->getNumberHistograms();
+
+    // run the algorithm
+    UnwrapMonitor algo;
+    if (!algo.isInitialized())
+      algo.initialize();
+    algo.setPropertyValue("InputWorkspace", name);
+    algo.setPropertyValue("OutputWorkspace", name);
+    algo.setProperty("LRef", 100.0);
+    TS_ASSERT(algo.execute());
+    TS_ASSERT(algo.isExecuted());
+
+    // verify the output workspace
+    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
+    TS_ASSERT_EQUALS(nHist,
+                     ws->getNumberHistograms()); // shouldn't drop histograms
+
+    const Mantid::MantidVec outY = ws->readY(0);
+    TS_ASSERT_EQUALS(outY.size(), 10);
+
+    double joinWavelength = algo.getProperty("JoinWavelength");
+    TS_ASSERT_DELTA(joinWavelength, 0.0, 1e-6);
 
     AnalysisDataService::Instance().remove(name);
   }
