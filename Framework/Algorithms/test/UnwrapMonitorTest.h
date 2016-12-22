6c9e81262e2613ca4f00098ea80863de30b1046f
Gemma Guest <gemma.guest@stfc.ac.uk>
2016-12-22 12:18:16 +0000

    Re #18215 Refactor to simplify code and reduce duplication

--- Framework/Algorithms/test/UnwrapMonitorTest.h
+++ Framework/Algorithms/test/UnwrapMonitorTest.h
@@ -2,7 +2,6 @@
 #define MANTID_ALGORITHMS_UNWRAPMONITORTEST_H_
 
 #include <cxxtest/TestSuite.h>
-#include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/Axis.h"
 #include "MantidDataObjects/Workspace2D.h"
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
@@ -18,14 +17,22 @@ using namespace Mantid::Kernel;
 
 class UnwrapMonitorTest : public CxxTest::TestSuite {
 private:
-  void makeFakeWorkspace(std::string wsName) {
-    Workspace2D_sptr test_in =
+  const MatrixWorkspace_sptr makeFakeWorkspace() {
+    Workspace2D_sptr testWS =
         WorkspaceCreationHelper::create2DWorkspaceWithRectangularInstrument(
             2, 3, 50);
-    test_in->getAxis(0)->setUnit("TOF");
+    testWS->getAxis(0)->setUnit("TOF");
+    return testWS;
+  }
 
-    // Add it to the workspace
-    AnalysisDataService::Instance().add(wsName, test_in);
+  void setupAlgorithm(UnwrapMonitor &algo, const MatrixWorkspace_sptr inWS,
+                      const double lref) {
+    if (!algo.isInitialized())
+      algo.initialize();
+    algo.setChild(true);
+    algo.setProperty("InputWorkspace", inWS);
+    algo.setPropertyValue("OutputWorkspace", "outWS");
+    algo.setProperty("LRef", lref);
   }
 
 public:
@@ -36,35 +43,28 @@ public:
 
   void testLrefLessThanLd() {
     // setup
-    std::string name("UnwrapMonitor");
-    this->makeFakeWorkspace(name);
-    Workspace2D_sptr ws =
-        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    size_t nHist = ws->getNumberHistograms();
+    MatrixWorkspace_sptr inWS = this->makeFakeWorkspace();
+    UnwrapMonitor algo;
+    setupAlgorithm(algo, inWS, 11.0);
 
     // run the algorithm
-    UnwrapMonitor algo;
-    if (!algo.isInitialized())
-      algo.initialize();
-    algo.setPropertyValue("InputWorkspace", name);
-    algo.setPropertyValue("OutputWorkspace", name);
-    algo.setProperty("LRef", 11.0);
     TS_ASSERT(algo.execute());
     TS_ASSERT(algo.isExecuted());
 
     // verify the output workspace
-    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    TS_ASSERT_EQUALS(nHist,
-                     ws->getNumberHistograms()); // shouldn't drop histograms
+    const MatrixWorkspace_const_sptr outWS =
+        algo.getProperty("OutputWorkspace");
+    TS_ASSERT_EQUALS(inWS->getNumberHistograms(),
+                     outWS->getNumberHistograms()); // shouldn't drop histograms
 
     // Check some x values
-    const Mantid::MantidVec outX = ws->readX(0);
+    const Mantid::MantidVec outX = outWS->readX(0);
     TS_ASSERT_EQUALS(outX.size(), 23);
     TS_ASSERT_DELTA(outX.front(), 0.0, 1e-6);
     TS_ASSERT_DELTA(outX.back(), 0.017982, 1e-6);
 
     // Check some y values
-    const Mantid::MantidVec outY = ws->readY(0);
+    const Mantid::MantidVec outY = outWS->readY(0);
     TS_ASSERT_EQUALS(outY.size(), 22);
     TS_ASSERT_DELTA(outY[3], 0.0, 1e-6);
     TS_ASSERT_DELTA(outY[4], 2.991736, 1e-6);
@@ -72,140 +72,102 @@ public:
     TS_ASSERT_DELTA(outY[11], 3.818182, 1e-6);
     TS_ASSERT_DELTA(outY[12], 0.0, 1e-6);
 
-    double joinWavelength = algo.getProperty("JoinWavelength");
+    const double joinWavelength = algo.getProperty("JoinWavelength");
     TS_ASSERT_DELTA(joinWavelength, 0.003692, 1e-6);
-
-    AnalysisDataService::Instance().remove(name);
   }
 
   void testLrefGreaterThanLd() {
     // setup
-    std::string name("UnwrapMonitor");
-    this->makeFakeWorkspace(name);
-    Workspace2D_sptr ws =
-        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    size_t nHist = ws->getNumberHistograms();
+    const MatrixWorkspace_sptr inWS = this->makeFakeWorkspace();
+    UnwrapMonitor algo;
+    setupAlgorithm(algo, inWS, 17.0);
 
     // run the algorithm
-    UnwrapMonitor algo;
-    if (!algo.isInitialized())
-      algo.initialize();
-    algo.setPropertyValue("InputWorkspace", name);
-    algo.setPropertyValue("OutputWorkspace", name);
-    algo.setProperty("LRef", 17.0);
     TS_ASSERT(algo.execute());
     TS_ASSERT(algo.isExecuted());
 
     // verify the output workspace
-    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    TS_ASSERT_EQUALS(nHist,
-                     ws->getNumberHistograms()); // shouldn't drop histograms
+    const MatrixWorkspace_const_sptr outWS =
+        algo.getProperty("OutputWorkspace");
+    TS_ASSERT_EQUALS(inWS->getNumberHistograms(),
+                     outWS->getNumberHistograms()); // shouldn't drop histograms
 
-    const Mantid::MantidVec outY = ws->readY(0);
+    const Mantid::MantidVec outY = outWS->readY(0);
     TS_ASSERT_EQUALS(outY.size(), 44);
 
-    double joinWavelength = algo.getProperty("JoinWavelength");
+    const double joinWavelength = algo.getProperty("JoinWavelength");
     TS_ASSERT_DELTA(joinWavelength, 0.001582, 1e-6);
-
-    AnalysisDataService::Instance().remove(name);
   }
 
   void testLrefEqualsLd() {
     // setup
-    std::string name("UnwrapMonitor");
-    this->makeFakeWorkspace(name);
-    Workspace2D_sptr ws =
-        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    size_t nHist = ws->getNumberHistograms();
+    const MatrixWorkspace_sptr inWS = this->makeFakeWorkspace();
+    UnwrapMonitor algo;
+    setupAlgorithm(algo, inWS, 15.0);
 
     // run the algorithm
-    UnwrapMonitor algo;
-    if (!algo.isInitialized())
-      algo.initialize();
-    algo.setPropertyValue("InputWorkspace", name);
-    algo.setPropertyValue("OutputWorkspace", name);
-    algo.setProperty("LRef", 15.0);
     TS_ASSERT(algo.execute());
     TS_ASSERT(algo.isExecuted());
 
     // verify the output workspace
-    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    TS_ASSERT_EQUALS(nHist,
-                     ws->getNumberHistograms()); // shouldn't drop histograms
+    const MatrixWorkspace_const_sptr outWS =
+        algo.getProperty("OutputWorkspace");
+    TS_ASSERT_EQUALS(inWS->getNumberHistograms(),
+                     outWS->getNumberHistograms()); // shouldn't drop histograms
 
-    const Mantid::MantidVec outY = ws->readY(0);
+    const Mantid::MantidVec outY = outWS->readY(0);
     TS_ASSERT_EQUALS(outY.size(), 49);
 
-    double joinWavelength = algo.getProperty("JoinWavelength");
+    const double joinWavelength = algo.getProperty("JoinWavelength");
     TS_ASSERT_DELTA(joinWavelength, 0.000264, 1e-6);
-
-    AnalysisDataService::Instance().remove(name);
   }
 
   void testMinPossibleLref() {
     // setup
-    std::string name("UnwrapMonitor");
-    this->makeFakeWorkspace(name);
-    Workspace2D_sptr ws =
-        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    size_t nHist = ws->getNumberHistograms();
+    const MatrixWorkspace_sptr inWS = this->makeFakeWorkspace();
+    UnwrapMonitor algo;
+    setupAlgorithm(algo, inWS, 0.01);
 
     // run the algorithm
-    UnwrapMonitor algo;
-    if (!algo.isInitialized())
-      algo.initialize();
-    algo.setPropertyValue("InputWorkspace", name);
-    algo.setPropertyValue("OutputWorkspace", name);
-    algo.setProperty("LRef", 0.01);
     TS_ASSERT(algo.execute());
     TS_ASSERT(algo.isExecuted());
 
     // verify the output workspace
-    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    TS_ASSERT_EQUALS(nHist,
-                     ws->getNumberHistograms()); // shouldn't drop histograms
+    const MatrixWorkspace_const_sptr outWS =
+        algo.getProperty("OutputWorkspace");
+    TS_ASSERT_EQUALS(inWS->getNumberHistograms(),
+                     outWS->getNumberHistograms()); // shouldn't drop histograms
 
-    const Mantid::MantidVec outY = ws->readY(0);
+    const Mantid::MantidVec outY = outWS->readY(0);
     TS_ASSERT_EQUALS(outY.size(), 50);
     TS_ASSERT_DELTA(outY[0], 100.0, 1e-6);
     TS_ASSERT_DELTA(outY[1], 0.0, 1e-6);
 
-    double joinWavelength = algo.getProperty("JoinWavelength");
+    const double joinWavelength = algo.getProperty("JoinWavelength");
     TS_ASSERT_DELTA(joinWavelength, 0.0, 1e-6);
-
-    AnalysisDataService::Instance().remove(name);
   }
 
   void testLargeLref() {
     // setup
-    std::string name("UnwrapMonitor");
-    this->makeFakeWorkspace(name);
-    Workspace2D_sptr ws =
-        AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    size_t nHist = ws->getNumberHistograms();
+    const MatrixWorkspace_sptr inWS = this->makeFakeWorkspace();
+    UnwrapMonitor algo;
+    setupAlgorithm(algo, inWS, 100.0);
 
     // run the algorithm
-    UnwrapMonitor algo;
-    if (!algo.isInitialized())
-      algo.initialize();
-    algo.setPropertyValue("InputWorkspace", name);
-    algo.setPropertyValue("OutputWorkspace", name);
-    algo.setProperty("LRef", 100.0);
     TS_ASSERT(algo.execute());
     TS_ASSERT(algo.isExecuted());
 
     // verify the output workspace
-    ws = AnalysisDataService::Instance().retrieveWS<Workspace2D>(name);
-    TS_ASSERT_EQUALS(nHist,
-                     ws->getNumberHistograms()); // shouldn't drop histograms
+    const MatrixWorkspace_const_sptr outWS =
+        algo.getProperty("OutputWorkspace");
+    TS_ASSERT_EQUALS(inWS->getNumberHistograms(),
+                     outWS->getNumberHistograms()); // shouldn't drop histograms
 
-    const Mantid::MantidVec outY = ws->readY(0);
+    const Mantid::MantidVec outY = outWS->readY(0);
     TS_ASSERT_EQUALS(outY.size(), 10);
 
-    double joinWavelength = algo.getProperty("JoinWavelength");
+    const double joinWavelength = algo.getProperty("JoinWavelength");
     TS_ASSERT_DELTA(joinWavelength, 0.0, 1e-6);
-
-    AnalysisDataService::Instance().remove(name);
   }
 };
 
