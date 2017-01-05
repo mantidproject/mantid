17d03b5470d637521bc6d00263f1da1078870203
Antti Soininen <soininen@ill.fr>
2017-01-05 13:43:16 +0100

    Improve doxygen documentation.
    Re #18366

--- Framework/DataObjects/inc/MantidDataObjects/PeakColumn.h
+++ Framework/DataObjects/inc/MantidDataObjects/PeakColumn.h
@@ -80,6 +80,7 @@ private:
   typedef boost::variant<double, int, std::string, Kernel::V3D> CacheValueType;
   ///
   mutable std::list<CacheValueType> m_oldRows;
+  /// Sets the correct value in the referenced peak.
   void setPeakHKLOrRunNumber(const size_t index, const double val);
 };
 
