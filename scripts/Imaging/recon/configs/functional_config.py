Re #18386 fixed rotation of flat and dark

scripts/Imaging/recon/configs/functional_config.py
@@ -40,7 +40,7 @@ class FunctionalConfig(object):
         # uint16, float16, float32, float64!
         # Was getting bad data with float16 after median filter and scaling up
         # to save as PNG
-        self.data_dtype = np.uint16
+        self.data_dtype = np.float32
 
         self.cor = None
         self.find_cor = None

scripts/Imaging/recon/filters/normalise_by_air_region.py
@@ -36,7 +36,7 @@ def execute(data, config):
                 "(air region). Got these coordinates: {0}".format(
                     normalize_air_region))
 
-        right = normalize_air_region[2]  # why do we add 1?
+        right = normalize_air_region[2]
         top = normalize_air_region[1]
         left = normalize_air_region[0]
         bottom = normalize_air_region[3]
@@ -55,7 +55,7 @@ def execute(data, config):
 
         air_sums = np.true_divide(air_sums, np.amax(air_sums))
 
-        h.tomo_print(" Air region sums (relative to maximum): " + air_sums)
+        h.tomo_print(" Air region sums (relative to maximum): {0}".format(air_sums))
 
         for idx in range(0, data.shape[0]):
             data[idx, :, :] = np.true_divide(data[idx, :, :],

scripts/Imaging/recon/filters/normalise_by_flat_dark.py
@@ -35,10 +35,16 @@ def execute(data, config, norm_flat_img, norm_dark_img=0):
         norm_divide = norm_flat_img - norm_dark_img
 
         # prevent divide-by-zero issues
-        norm_divide[norm_divide == 0] = 1e-6
+        # norm_divide[norm_divide == 0] = 1e-6
+        clip_min = config.pre.clip_min
+        clip_max = config.pre.clip_max
 
+        # this divide gives bad results
         for idx in range(0, data.shape[0]):
-            data[idx, :, :] = np.true_divide(data[idx, :, :] - norm_dark_img, norm_divide)
+            data[idx, :, :] = np.clip(np.divide(data[idx, :, :] - norm_dark_img, norm_divide), clip_min, clip_max)
+            # d = np.true_divide(d - norm_dark_img, norm_divide)
+            # d[d < 0] = 1
+            # d[d > 1.5] = 1
 
         h.pstop(
             " * Finished normalization by flat/dark images, pixel data type: {0}.".format(data.dtype))