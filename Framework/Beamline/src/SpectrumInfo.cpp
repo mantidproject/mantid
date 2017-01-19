e89a9d3b12dc80bc2103f5997995b36b84250c21
Simon Heybrock <simon.heybrock@esss.se>
2017-01-19 10:55:10 +0100

    Re #18421. Removed now unused SpectrumInfo::mutableSpectrumDefinition.

--- Framework/Beamline/src/SpectrumInfo.cpp
+++ Framework/Beamline/src/SpectrumInfo.cpp
@@ -22,16 +22,6 @@ SpectrumInfo::spectrumDefinition(const size_t index) const {
   return (*m_spectrumDefinition)[index];
 }
 
-/** Returns a non-const reference to the SpectrumDefinition of the spectrum.
-*
-* This method might be removed in the future since it breaks encapsulation and
-* should thus be used sparingly, in cases where direct modification is necessary
-* for adequate performance. */
-SpectrumDefinition &
-SpectrumInfo::mutableSpectrumDefinition(const size_t index) {
-  return m_spectrumDefinition.access()[index];
-}
-
 /// Sets the SpectrumDefinition of the spectrum.
 void SpectrumInfo::setSpectrumDefinition(const size_t index,
                                          SpectrumDefinition def) {
