c1ee36de90ae645351e673c1484111f840fa8368
David Fairbrother <DavidFair@users.noreply.github.com>
2017-01-31 10:25:36 +0000

    Re #18625 Tidy header in SaveMask

--- Framework/DataHandling/src/SaveMask.cpp
+++ Framework/DataHandling/src/SaveMask.cpp
@@ -1,34 +1,21 @@
-#include <fstream>
-#include <sstream>
-#include <algorithm>
+#include "MantidDataHandling/SaveMask.h"
 
 #include "MantidAPI/FileProperty.h"
 #include "MantidAPI/ISpectrum.h"
-#include "MantidDataHandling/SaveMask.h"
 #include "MantidDataObjects/SpecialWorkspace2D.h"
 #include "MantidKernel/System.h"
 
-#include <boost/shared_ptr.hpp>
-
+#include <Poco/DOM/AutoPtr.h>
 #include <Poco/DOM/Document.h>
 #include <Poco/DOM/Element.h>
 #include <Poco/DOM/Text.h>
-#include <Poco/DOM/AutoPtr.h>
 #include <Poco/DOM/DOMWriter.h>
-#ifdef _MSC_VER
-// Disable a flood of warnings from Poco about inheriting from
-// std::basic_istream
-// See
-// http://connect.microsoft.com/VisualStudio/feedback/details/733720/inheriting-from-std-fstream-produces-c4250-warning
-#pragma warning(push)
-#pragma warning(disable : 4250)
-#endif
-
 #include <Poco/XML/XMLWriter.h>
 
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+#include <algorithm>
+#include <boost/shared_ptr.hpp>
+#include <fstream>
+#include <sstream>
 
 using namespace Mantid::Kernel;
 using namespace Mantid::API;
