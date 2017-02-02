2b108d9b36b198862edd5bcde530b324524751cb
David Fairbrother <DavidFair@users.noreply.github.com>
2017-02-02 17:43:01 +0000

    Re #18666 Fixed YAML parser not handlding string run number input

--- scripts/Diffraction/isis_powder/routines/yaml_parser.py
+++ scripts/Diffraction/isis_powder/routines/yaml_parser.py
@@ -7,6 +7,10 @@ from isis_powder.routines import yaml_sanity
 
 
 def get_run_dictionary(run_number, file_path):
+    if isinstance(run_number, str):
+        run_number_list = common.generate_run_numbers(run_number_string=run_number)
+        run_number = run_number_list[0]
+
     config_file = open_yaml_file_as_dictionary(file_path)
     yaml_sanity.calibration_file_sanity_check(config_file)
     run_key = _find_dictionary_key(dict_to_search=config_file, run_number=run_number)
