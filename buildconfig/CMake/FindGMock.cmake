31c3d5458b274cc4ca70a845ba8e4152f10abf21
Pete Peterson <petersonpf@ornl.gov>
2017-02-07 13:47:57 -0500

    gmock/gtest 1.8.0 works for most things

--- buildconfig/CMake/FindGMock.cmake
+++ buildconfig/CMake/FindGMock.cmake
@@ -21,11 +21,11 @@ else()
 
   # Download and unpack googletest at configure time
   configure_file(${CMAKE_SOURCE_DIR}/buildconfig/CMake/CMakeLists.txt.gtest.in
-    ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
+                 ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
   execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
   execute_process(COMMAND ${CMAKE_COMMAND} --build .
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
+                  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
 
   # Download and unpack googletest at configure time
 #  configure_file(${CMAKE_SOURCE_DIR}/buildconfig/CMake/CMakeLists.txt.gmock.in
@@ -39,13 +39,13 @@ else()
   # Add googletest directly to our build. This adds the following
   # targets: gtest, gtest_main, gmock and gmock_main
   add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src
-    ${CMAKE_BINARY_DIR}/googletest-build)
+                   ${CMAKE_BINARY_DIR}/googletest-build)
 
   # The gtest/gmock targets carry header search path dependencies
   # automatically when using CMake 2.8.11 or later. Otherwise we have
   # to add them here ourselves.
   include_directories("${gtest_SOURCE_DIR}/include"
-    "${gmock_SOURCE_DIR}/include")
+                      "${gmock_SOURCE_DIR}/include")
 endif()
 
 mark_as_advanced ( GMOCK_INCLUDE_DIR GMOCK_LIB GMOCK_LIB_DEBUG )
