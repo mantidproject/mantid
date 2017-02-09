9a21dc9b7679929995d633872a4860e7cf971e18
Simon Heybrock <simon.heybrock@esss.se>
2017-02-09 07:57:48 +0100

    Re #18659. More unit tests for changing positions and rotations.

--- Framework/API/test/DetectorInfoTest.h
+++ Framework/API/test/DetectorInfoTest.h
@@ -282,6 +282,68 @@ public:
     detInfo.setRotation(*root, oldRot);
   }
 
+  void test_setRotation_component_moved_root() {
+    auto &detInfo = m_workspace.mutableDetectorInfo();
+    const auto &instrument = m_workspace.getInstrument();
+    const auto &root = instrument->getComponentByName("SimpleFakeInstrument");
+    const auto oldPos = root->getPos();
+    const auto oldRot = root->getRotation();
+    V3D e2{0, 1, 0};
+    Quat rot(180.0, e2);
+
+    detInfo.setPosition(*root, V3D{0.0, 0.0, 1.0});
+    detInfo.setRotation(*root, rot);
+
+    // Rotations *and* positions have changed since *parent* was rotated
+    TS_ASSERT_EQUALS(detInfo.rotation(0), rot);
+    TS_ASSERT_EQUALS(detInfo.rotation(1), rot);
+    TS_ASSERT_EQUALS(detInfo.rotation(2), rot);
+    TS_ASSERT_EQUALS(detInfo.rotation(3), rot);
+    TS_ASSERT_EQUALS(detInfo.rotation(4), rot);
+    TS_ASSERT_EQUALS(detInfo.sourcePosition(), V3D(0.0, 0.0, 21.0));
+    TS_ASSERT_EQUALS(detInfo.samplePosition(), V3D(0.0, 0.0, 1.0));
+    TS_ASSERT_EQUALS(detInfo.position(0), V3D(0.0, -0.1, -4.0));
+
+    // For additional verification we do *not* use detInfo, but make sure that
+    // the changes actually affected the workspace.
+    const auto &clone = m_workspace.clone();
+    const auto &info = clone->detectorInfo();
+    TS_ASSERT_EQUALS(info.sourcePosition(), V3D(0.0, 0.0, 21.0));
+    TS_ASSERT_EQUALS(info.samplePosition(), V3D(0.0, 0.0, 1.0));
+    TS_ASSERT_EQUALS(info.position(0), V3D(0.0, -0.1, -4.0));
+
+    detInfo.setRotation(*root, oldRot);
+    detInfo.setPosition(*root, oldPos);
+  }
+
+  void test_setRotation_setPosition_commute() {
+    auto &detInfo = m_workspace.mutableDetectorInfo();
+    const auto &instrument = m_workspace.getInstrument();
+    const auto &root = instrument->getComponentByName("SimpleFakeInstrument");
+    const auto oldRot = root->getRotation();
+    const auto oldPos = root->getPos();
+    V3D axis{0.1, 0.2, 0.7};
+    Quat rot(42.0, axis);
+    V3D pos{-11.0, 7.0, 42.0};
+
+    // Note the order: We are going in a (figurative) square...
+    detInfo.setRotation(*root, rot);
+    detInfo.setPosition(*root, pos);
+    detInfo.setRotation(*root, oldRot);
+    detInfo.setPosition(*root, oldPos);
+    // ... and check that we come back to where we started.
+    TS_ASSERT_EQUALS(detInfo.position(0), V3D(0.0, -0.1, 5.0));
+    TS_ASSERT_EQUALS(detInfo.position(1), V3D(0.0, 0.0, 5.0));
+    TS_ASSERT_EQUALS(detInfo.position(2), V3D(0.0, 0.1, 5.0));
+    TS_ASSERT_EQUALS(detInfo.position(3), V3D(0.0, 0.0, -9.0));
+    TS_ASSERT_EQUALS(detInfo.position(4), V3D(0.0, 0.0, -2.0));
+    TS_ASSERT_EQUALS(detInfo.rotation(0), Quat(1.0, 0.0, 0.0, 0.0));
+    TS_ASSERT_EQUALS(detInfo.rotation(1), Quat(1.0, 0.0, 0.0, 0.0));
+    TS_ASSERT_EQUALS(detInfo.rotation(2), Quat(1.0, 0.0, 0.0, 0.0));
+    TS_ASSERT_EQUALS(detInfo.rotation(3), Quat(1.0, 0.0, 0.0, 0.0));
+    TS_ASSERT_EQUALS(detInfo.rotation(4), Quat(1.0, 0.0, 0.0, 0.0));
+  }
+
   void test_detectorIDs() {
     WorkspaceTester workspace;
     int32_t numberOfHistograms = 5;
