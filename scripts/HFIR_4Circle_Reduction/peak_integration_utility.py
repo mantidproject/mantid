0edaf9062baea3ab4be5aa4971ba4fdc350269f8
Wenduo Zhou <zhouw@ornl.gov>
2017-02-08 21:27:07 -0500

    Refs #18547. Checkpointing progress to start implement new methods.

--- scripts/HFIR_4Circle_Reduction/peak_integration_utility.py
+++ scripts/HFIR_4Circle_Reduction/peak_integration_utility.py
@@ -1,7 +1,8 @@
 # Utility methods to do peak integration
 import numpy
-import scipy
 from scipy.optimize import curve_fit
+import mantid.simpleapi as mantidsimple
+from mantid.api import AnalysisDataService
 
 
 def calculate_peak_intensity_gauss(gauss_a, gauss_sigma, error_a_sq=None, error_sigma_sq=None,
@@ -34,6 +35,62 @@ def calculate_peak_intensity_gauss(gauss_a, gauss_sigma, error_a_sq=None, error_
         error = numpy.sqrt(integral)
 
     return integral, error
+
+
+def estimate_background(pt_intensity_dict, bg_pt_list):
+    """
+    Estimate background value by average the integrated counts of some Pt.
+    :param pt_intensity_dict:
+    :param bg_pt_list: list of Pt. that are used to calculate background
+    :return:
+    """
+    # Check
+    assert isinstance(pt_intensity_dict, dict)
+    assert isinstance(bg_pt_list, list) and len(bg_pt_list) > 0
+
+    # Sum over all Pt.
+    bg_sum = 0.
+    for bg_pt in bg_pt_list:
+        assert bg_pt in pt_intensity_dict, 'Pt. %d is not calculated.' % bg_pt
+        bg_sum += pt_intensity_dict[bg_pt]
+
+    avg_bg = float(bg_sum) / len(bg_pt_list)
+
+    return avg_bg
+
+
+def get_moving_motor_information(spice_table_name):
+    """
+
+    :param spice_table_name:
+    :return:
+    """
+    table = AnalysisDataService.retrieve(spice_table_name)
+
+    col_names = table.getColumnNames()
+    pt_index = col_names.index('Pt.')
+    omega_index = col_names.index('omega')
+    chi_index = col_names.index('chi')
+    phi_index = col_names.index('phi')
+
+    col_tup_dict = {'omega': omega_index, 'phi': phi_index, 'chi': chi_index}
+
+    std_list = list()
+    motor_vector_dict = dict()
+    for motor in col_tup_dict:
+        motor_index = col_tup_dict[motor]
+        motor_vector = numpy.array(table.column(motor_index))
+        motor_vector_dict[motor] = motor_vector
+        std_list.append((motor_vector.std(), motor))
+    std_list.sort()
+    moving_motor = std_list[-1][1]
+    pt_list = table.column(pt_index)
+
+    motor_pos_dict = dict()
+    for i_m in range(len(pt_list)):
+        motor_pos_dict[pt_list[i_m]] = motor_vector_dict[moving_motor][i_m]
+
+    return moving_motor, motor_pos_dict
 
 
 def gaussian_linear_background(x, x0, sigma, a, b):
@@ -155,6 +212,26 @@ def fit_gaussian_linear_background(vec_x, vec_y, vec_e, start_value_list=None, f
     return error, fit2_coeff, fit2_cov_matrix
 
 
+def fit_motor_intensity_model(motor_pos_dict, integrated_pt_dict):
+    """
+
+    :param motor_pos_dict:
+    :param integrated_pt_dict:
+    :return:
+    """
+    # TODO/ISSUE/NOW - Implement!
+
+    return
+
+def gaussian_peak_intensity(parameter_dict, error_dict):
+    """
+
+    :return:
+    """
+    # TODO/ISSUE/NOW
+
+    return 1E20
+
 def get_finer_grid(vec_x, factor):
     """
     insert values to a vector (grid) to make it finer
@@ -180,3 +257,212 @@ def get_finer_grid(vec_x, factor):
     new_vector = numpy.array(new_list)
 
     return new_vector
+
+
+def integrate_scan_peaks(#exp, scan,
+                         merged_scan_workspace_name, integrated_peak_ws_name,
+                         peak_radius, peak_centre,
+                         merge_peaks=True, use_mask=False,
+                         normalization='', mask_ws_name=None,
+                         scale_factor=1):
+    """
+    # :param exp:
+    # :param scan:
+    :param peak_radius:
+    :param peak_centre:  a float radius or None for not using
+    :param merge_peaks: If selected, merged all the Pts can return 1 integrated peak's value;
+                        otherwise, integrate peak for each Pt.
+    :param use_mask:
+    :param normalization: normalization set up (by time or ...)
+    :param mask_ws_name: mask workspace name or None
+    :param scale_factor: integrated peaks' scaling factor
+    :return: dictionary of Pts.
+    """
+    # check
+    # assert isinstance(exp, int)
+    # assert isinstance(scan, int)
+    assert isinstance(peak_radius, float) or peak_radius is None
+    assert len(peak_centre) == 3
+    assert isinstance(merge_peaks, bool)
+
+    # get spice file
+    # spice_table_name = get_spice_table_name(exp, scan)
+    # if AnalysisDataService.doesExist(spice_table_name) is False:
+    #     self.download_spice_file(exp, scan, False)
+    #     self.load_spice_scan_file(exp, scan)
+
+    # get MD workspace name
+    # status, pt_list = self.get_pt_numbers(exp, scan)
+    # assert status, str(pt_list)
+    # md_ws_name = get_merged_md_name(self._instrumentName, exp, scan, pt_list)
+
+    peak_centre_str = '%f, %f, %f' % (peak_centre[0], peak_centre[1],
+                                      peak_centre[2])
+
+    # mask workspace
+    # if use_mask:
+    #     if mask_ws_name is None:
+    #         # get default mask workspace name
+    #         mask_ws_name = get_mask_ws_name(exp, scan)
+    #     elif not AnalysisDataService.doesExist(mask_ws_name):
+    #         # the appointed mask workspace has not been loaded
+    #         # then load it from saved mask
+    #         self.check_generate_mask_workspace(exp, scan, mask_ws_name)
+    #
+    #     assert AnalysisDataService.doesExist(mask_ws_name), 'MaskWorkspace %s does not exist.' \
+    #                                                         '' % mask_ws_name
+    #
+    #     integrated_peak_ws_name = get_integrated_peak_ws_name(exp, scan, pt_list, use_mask)
+    # else:
+    #     mask_ws_name = ''
+    #     integrated_peak_ws_name = get_integrated_peak_ws_name(exp, scan, pt_list)
+
+    # normalization
+    norm_by_mon = False
+    norm_by_time = False
+    if normalization == 'time':
+        norm_by_time = True
+    elif normalization == 'monitor':
+        norm_by_mon = True
+
+    # integrate peak of a scan
+    mantidsimple.IntegratePeaksCWSD(InputWorkspace=merged_scan_workspace_name,
+                                    OutputWorkspace=integrated_peak_ws_name,
+                                    PeakRadius=peak_radius,
+                                    PeakCentre=peak_centre_str,
+                                    MergePeaks=merge_peaks,
+                                    NormalizeByMonitor=norm_by_mon,
+                                    NormalizeByTime=norm_by_time,
+                                    MaskWorkspace=mask_ws_name,
+                                    ScaleFactor=scale_factor)
+
+    # process the output workspace
+    pt_dict = dict()
+    out_peak_ws = AnalysisDataService.retrieve(integrated_peak_ws_name)
+    num_peaks = out_peak_ws.rowCount()
+
+    for i_peak in xrange(num_peaks):
+        peak_i = out_peak_ws.getPeak(i_peak)
+        run_number_i = peak_i.getRunNumber() % 1000
+        intensity_i = peak_i.getIntensity()
+        pt_dict[run_number_i] = intensity_i
+    # END-FOR
+
+    # # store the data into peak info
+    # if (exp, scan) not in self._myPeakInfoDict:
+    #     raise RuntimeError('Exp %d Scan %d is not recorded in PeakInfo-Dict' % (exp, scan))
+    # self._myPeakInfoDict[(exp, scan)].set_pt_intensity(pt_dict)
+
+    return True, pt_dict
+
+
+def integrate_peak_full_version(scan_md_ws_name, spice_table_name, output_peak_ws_name,
+                                peak_center, mask_workspace_name, norm_type,
+                                intensity_scale_factor, background_pt_list):
+    """
+    Integrate peak with the full version including
+    1. simple summation
+    2. simple summation with gaussian fit
+    3. integrate with fitted gaussian
+    :return:
+    """
+    # integrate the peak in MD workspace
+    # spice_table_name, integrated_peak_ws_name, md_ws_name,
+    status, ret_obj = integrate_scan_peaks(merged_scan_workspace_name=scan_md_ws_name,
+                                           integrated_peak_ws_name=output_peak_ws_name,
+                                           peak_radius=1.0,
+                                           peak_centre=peak_center,
+                                           merge_peaks=False,
+                                           use_mask=True,
+                                           mask_ws_name=mask_workspace_name,
+                                           normalization=norm_type,
+                                           scale_factor=intensity_scale_factor)
+
+    # result due to error
+    if status is False:
+        error_message = ret_obj
+        return False, error_message
+    else:
+        # process result
+        integrated_pt_dict = ret_obj
+        assert isinstance(integrated_pt_dict, dict), 'Returned masked Pt dict must be a dictionary'
+
+    # get moving motor information. candidates are 2theta, phi and chi
+    motor, motor_pos_dict = get_moving_motor_information(spice_table_name)
+
+    # calculate the intensity with background removed and correct intensity by background value
+    averaged_background = estimate_background(integrated_pt_dict, background_pt_list)
+    simple_intensity, simple_intensity_error = simple_integrate_peak(integrated_pt_dict, averaged_background,
+                                                                     motor_pos_dict)
+
+    # fit gaussian + flat background
+    parameters, errors = fit_motor_intensity_model(motor_pos_dict, integrated_pt_dict)
+
+    # calculate intensity with method 2
+    intensity_m2, error_m2 = simple_integrate_peak(integrated_pt_dict, parameters['B'])
+
+    # calculate gaussian
+    intensity_gauss, intensity_guass_error = gaussian_peak_intensity(parameters, errors)
+
+    return
+
+    #
+    #
+    #
+    # simple_peak_intensity = self.ui.tableWidget_peakIntegration.sum_masked_intensity()
+    #     info_text += 'Simple summation of counts masked by {0}'.format(mask_name)
+    # self.ui.lineEdit_rawSinglePeakIntensity.setText(str(simple_peak_intensity))
+    #
+    # info_text += '; Normalized by {0}'.format(norm_type)
+    # self.ui.label_ingreateInformation.setText(info_text)
+    #
+    # # Clear previous line and plot the Pt.
+    # self.ui.graphicsView_integratedPeakView.reset()
+    # x_array = numpy.array(pt_list)
+    # y_array = numpy.array(intensity_list)
+    # self.ui.graphicsView_integratedPeakView.plot_raw_data(x_array, y_array)
+
+    return
+
+
+def calculate_motor_step(motor_pos_array, motor_step_tolerance=0.5):
+    """
+
+    :param pt_motor_dict:
+    :return:
+    """
+    assert isinstance(motor_pos_array, numpy.ndarray), 'blabla 2'
+    # need to check somewhere: assert len(pt_motor_dict) == len(pt_intensity_dict), 'blabla 3'
+
+    motor_step_vector = motor_pos_array[1:] - motor_pos_array[:-1]
+
+    motor_step = numpy.average(motor_step_vector)
+    motor_step_std = motor_step_vector.std()
+
+    if motor_step_std > motor_step_tolerance:
+        raise RuntimeError('Step deviation too large. Cannot use average!')
+
+    return motor_step
+
+
+def simple_integrate_peak(pt_intensity_dict, bg_value, motor_step):
+    """
+    A simple approach to integrate peak in a cuboid with background removed.
+    :param pt_intensity_dict:
+    :param bg_value:
+    :return:
+    """
+    # check
+    assert isinstance(pt_intensity_dict, dict), 'blabla'
+    assert isinstance(bg_value, float) and bg_value >= 0.
+
+    # calculate motor
+
+    # loop over Pt. to sum for peak's intensity
+    sum_intensity = 0.
+    error_2 = 0.
+    for intensity in pt_intensity_dict.values():
+        sum_intensity += (intensity - bg_value) * motor_step
+        error_2 += numpy.sqrt(intensity) * motor_step
+
+    return sum_intensity, error_2
