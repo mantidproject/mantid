6933946654956e620add6c922fb4100236e13523
David Fairbrother <DavidFair@users.noreply.github.com>
2017-02-10 19:10:11 +0000

    Re #18796 Mayers Correction - Skip 0 signal bins

--- Framework/Algorithms/test/MayersSampleCorrectionStrategyTest.h
+++ Framework/Algorithms/test/MayersSampleCorrectionStrategyTest.h
@@ -33,6 +33,22 @@ public:
     TS_ASSERT_DELTA(0.00030887, absFactor, delta);
   }
 
+  void test_Correction_Skips_Zero_Counts() {
+	  Histogram histo(Points{ 2, LinearGenerator(0,1) } , Counts{2, LinearGenerator(0, 1)});
+	  MayersSampleCorrectionStrategy mscat(createTestParameters(), histo);
+	  const auto outHisto = mscat.getCorrectedHisto();
+
+	  const auto &yVals = outHisto.y();
+	  const auto &eVals = outHisto.e();
+
+	  TSM_ASSERT_EQUALS("Bin with 0 count was modified", yVals[0], 0);
+	  TSM_ASSERT_EQUALS("Err val for 0 count was modified", eVals[0], 0);
+	  
+	  const double delta = 1e-8;
+	  TS_ASSERT_DELTA(0.18741573, yVals[1], delta);
+	  TS_ASSERT_DELTA(0.18741573, eVals[1], delta);
+  }
+
   void
   test_Multiple_Scattering_With_Fixed_Mur_And_Absorption_Correction_Factor() {
     Histogram histo(Points{0, 1}, Counts{0, 1});
