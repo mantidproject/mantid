48f85f3ecd21aa1b38c01cbd39c0d038c08a5fb2
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-02-13 09:20:43 +0000

    refs #18758 Updated ALCDataLoadingPresenter

--- MantidQt/CustomInterfaces/src/Muon/ALCDataLoadingPresenter.cpp
+++ MantidQt/CustomInterfaces/src/Muon/ALCDataLoadingPresenter.cpp
@@ -270,7 +270,7 @@ void ALCDataLoadingPresenter::updateAvailableInfo() {
   if (auto timeLimits = m_view->timeRange()) {
     if (std::abs(timeLimits->first) < 0.0001 &&
         std::abs(timeLimits->second) < 0.0001) {
-      m_view->setTimeLimits(firstGoodData - timeZero, ws->readX(0).back());
+      m_view->setTimeLimits(firstGoodData - timeZero, ws->x(0).back());
     }
   }
 
