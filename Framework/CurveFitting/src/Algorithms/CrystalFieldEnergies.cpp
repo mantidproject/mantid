0903eed7eacf43f5eabd24f79de62b4f670a1163
Duc Le <duc.le@stfc.ac.uk>
2017-02-14 14:20:51 +0000

    Re #17984 - updated python interface.

--- Framework/CurveFitting/src/Algorithms/CrystalFieldEnergies.cpp
+++ Framework/CurveFitting/src/Algorithms/CrystalFieldEnergies.cpp
@@ -46,7 +46,7 @@ const std::string CrystalFieldEnergies::summary() const {
 void CrystalFieldEnergies::init() {
 
   // Input
-  auto bounds = boost::make_shared<Kernel::BoundedValidator<int>>(1, 13);
+  auto bounds = boost::make_shared<Kernel::BoundedValidator<int>>(-99, 13);
   declareProperty("Nre", 1, bounds, "A rare earth ion. Possible values are: "
                                     "1=Ce 2=Pr 3=Nd 4=Pm 5=Sm 6=Eu 7=Gd 8=Tb "
                                     "9=Dy 10=Ho 11=Er 12=Tm 13=Yb");
