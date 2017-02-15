712f9b6a804949f8c28d2c3314d39e99c3dad033
Wenduo Zhou <zhouw@ornl.gov>
2017-02-15 10:57:33 -0500

    Refs #18874. Added the ':' to the validator of run numbers.

--- scripts/Interface/reduction_gui/widgets/diffraction/diffraction_run_setup.py
+++ scripts/Interface/reduction_gui/widgets/diffraction/diffraction_run_setup.py
@@ -173,7 +173,7 @@ class RunSetupWidget(BaseWidget):
             @param state: RunSetupScript object
         """
         self._content.runnumbers_edit.setText(state.runnumbers)
-        self._content.runnumbers_edit.setValidator(generateRegExpValidator(self._content.runnumbers_edit, r'[\d,-]*'))
+        self._content.runnumbers_edit.setValidator(generateRegExpValidator(self._content.runnumbers_edit, r'[\d,-:]*'))
 
         self._content.calfile_edit.setText(state.calibfilename)
         self._content.lineEdit_expIniFile.setText(state.exp_ini_file_name)
@@ -246,11 +246,13 @@ class RunSetupWidget(BaseWidget):
         """
         s = RunSetupScript(self._instrument_name)
 
-        s.runnumbers = self._content.runnumbers_edit.text()
+        s.runnumbers = str(self._content.runnumbers_edit.text())
         rtup = self.validateIntegerList(s.runnumbers)
         isvalid = rtup[0]
         if isvalid is False:
             raise NotImplementedError("Run number error @ %s" % (rtup[1]))
+        else:
+            s.runnumbers = rtup[2]
 
         s.calibfilename = self._content.calfile_edit.text()
         s.exp_ini_file_name = str(self._content.lineEdit_expIniFile.text())
@@ -374,11 +376,16 @@ class RunSetupWidget(BaseWidget):
 
     def validateIntegerList(self, intliststring):
         """ Validate whether the string can be divided into integer strings.
-        Allowed: a, b, c-d, e, f
+        Allowed: a, b, c-d, e, f, g:h
+        and replace ':' by ':'
+        :return: 3-tuple: state/error message/new integer list string
         """
         intliststring = str(intliststring)
         if intliststring == "":
             return (True, "")
+
+        # replace ':' by '-'
+        intliststring.replace(':', '-')
 
         # 1. Split by ","
         termlevel0s = intliststring.split(",")
@@ -409,10 +416,10 @@ class RunSetupWidget(BaseWidget):
                 # ENDFOR
 
             else:
-                return (False, level0term)
+                return False, level0term, intliststring
         # ENDFOR
 
-        return (True, "")
+        return True, '', intliststring
 
     def _overrideemptyrun_clicked(self):
         """ Handling event if overriding emptry run
