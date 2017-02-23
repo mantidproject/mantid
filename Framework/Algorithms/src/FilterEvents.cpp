f170d4c306313f86f181250b2b5f90858225a5f6
Wenduo Zhou <zhouw@ornl.gov>
2017-02-23 08:21:02 -0500

    Refs #12535. Checkpointing progress on refacotring.

--- Framework/Algorithms/src/FilterEvents.cpp
+++ Framework/Algorithms/src/FilterEvents.cpp
@@ -485,15 +485,69 @@ void FilterEvents::convertSplittersWorkspaceToVectors()
   for (size_t irow = 0; irow < num_rows; ++irow)
   {
     Kernel::SplittingInterval splitter = m_splittersWorkspace->getSplitter(irow);
-    if (m_vecSplitterTime.size() == 0 || splitter.start() > m_vecSplitterTime.back())
+    if (m_vecSplitterTime.size() == 0 || splitter.start() > m_vecSplitterTime.back() + TOLERANCE)
     {
       m_vecSplitterTime.push_back(splitter.start().totalNanoseconds());
       m_vecSplitterTime.push_back(splitter.stop().totalNanoseconds());
       // 0 stands for not defined
       m_vecSplitterGroup.push_back(0);
+      m_vecSplitterGroup.push_back(splitter.index());
     }
-  }
+    else if (splitter.start() < m_vecSplitterTime.back() - TOLERANCE)
+    {
+      // almost same: then add the spliters.stop() only
+      m_vecSplitterTime.push_back(splitter.stop());
+      m_vecSplitterGroup.push_back(splitter.index());
+    }
+    else
+    {
+      // have to insert the somewhere
+      std::vector<int64_t>::iterator finditer = std::lower_bound(m_vecSplitterTime.begin(), m_vecSplitterTime.end(), splitter.start());
+      // get the index
+      size_t split_index = static_cast<size_t>(finditer - m_vecSplitterTime.begin());
+      if (*finditer - splitter.start() > TOLERANCE)
+      {
+        // the start time is before one splitter indicated by *finditer: insert both
+        // check
+        if (m_vecSplitterGroup[split_index] != UNDEFINED_SPLITTING_TARGET)
+        {
+          std::stringstream errss;
+          errss << "Tried to insert splitter [" << splitter.start() << ", " << splitter.stop() << "] but there is "
+                << "already a time entry with target " << m_vecSplitterGroup[split_index] << " inside it.";
+          throw std::runtime_error(errss.str());
 
+        }
+        // inset the full set
+        m_vecSplitterTime.insert(finditer, splitter.stop());
+        m_vecSplitterTime.insert(finditer, splitter.start());
+        // insert the target
+        m_vecSplitterGroup.insert(splitter.index(), UNDEFINED_SPLITTING_TARGET);
+        m_vecSplitterGroup.insert(splitter.index(), splitter.index());
+      }
+      else if (*finditer - splitter.start() > -TOLERANCE)
+      {
+        // the start time is an existing entry
+        // check
+        if (m_vecSplitterGroup[split_index] != UNDEFINED_SPLITTING_TARGET)
+        {
+          std::stringstream errss;
+          errss << "Tried to insert splitter [" << splitter.start() << ", " << splitter.stop() << "] but there is "
+                << "already a time entry with target " << m_vecSplitterGroup[split_index] << " inside it.";
+          throw std::runtime_error(errss.str());
+        }
+        // inset the stop time
+        m_vecSplitterTime.insert(finditer+1, splitter.stop());
+        // insert the target
+        m_vecSplitterTime.insert(split_index+1, splitter.index());
+      }
+      else
+      {
+        throw std::runtime_error("This is not a possible situation!");
+      }
+    } // IF-ELSE to add a new entry
+  } // END-FOR (add all splitters)
+
+  return;
 }
 
 //----------------------------------------------------------------------------------------------
