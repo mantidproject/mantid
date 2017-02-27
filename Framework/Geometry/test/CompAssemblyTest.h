fe0ae5157aa0f365ce63c9698823c3532ad60347
Verena Reimund <reimund@ill.eu>
2017-02-27 10:39:06 +0100

    Re #19002 modified unit test

--- Framework/Geometry/test/CompAssemblyTest.h
+++ Framework/Geometry/test/CompAssemblyTest.h
@@ -166,9 +166,8 @@ public:
     TS_ASSERT(bool(temp));
     TS_ASSERT_EQUALS(temp->getName(), bankname);
 
-    // look for bank13 again - recursing just one level (should fail)
-    temp = instr->getComponentByName(bankname, 1);
-    TS_ASSERT(!bool(temp));
+    // look for bank13 again - recursing just one level (should thwow)
+    TS_ASSERT_THROWS(instr->getComponentByName(bankname, 1), std::runtime_error);
   }
 
   //-----------------------------------------------------------------------------
