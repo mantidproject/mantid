4d416ad6143be1b13ed3ce5d6c8012ace2e9a4a6
Louise McCann <louise.mccann@stfc.ac.uk>
2017-03-06 13:36:05 +0000

    Refs #17262 modify plotMultipleSpectra

--- MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Indirect/IndirectTab.h
+++ MantidQt/CustomInterfaces/inc/MantidQtCustomInterfaces/Indirect/IndirectTab.h
@@ -100,7 +100,9 @@ protected:
   /// Gets the base name of a workspace
   QString getWorkspaceBasename(const QString &wsName);
   /// Plot multiple spectra from multiple workspaces
-  void IndirectTab::plotMultipleSpectra(const QStringList &workspaceNames, const std::vector<int> &workspaceIndices);
+  void
+  IndirectTab::plotMultipleSpectra(const QStringList &workspaceNames,
+                                   const std::vector<int> &workspaceIndices);
   /// Plot a spectrum plot with a given ws index
   void plotSpectrum(const QStringList &workspaceNames, int wsIndex = 0);
   /// Plot a spectrum plot of a given workspace
