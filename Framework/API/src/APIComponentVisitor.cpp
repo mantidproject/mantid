6925fd0ce34f59c9e2f96f8a3467ce9b71d1bdad
Owen Arnold <owen.arnold@stfc.ac.uk>
2017-03-15 12:26:43 +0000

    Some additional comments

--- Framework/API/src/APIComponentVisitor.cpp
+++ Framework/API/src/APIComponentVisitor.cpp
@@ -16,14 +16,21 @@ APIComponentVisitor::APIComponentVisitor(
 void APIComponentVisitor::registerComponentAssembly(
     const ICompAssembly &bank, std::vector<size_t> &parentDetectorIndexes) {
 
+  // Local cache of immediate child detector indexes
   std::vector<size_t> localDetectorIndexes;
-  std::vector<IComponent_const_sptr> bankChildren;
-  bank.getChildren(bankChildren, false /*is recursive*/);
+  std::vector<IComponent_const_sptr> assemblyChildren;
+  bank.getChildren(assemblyChildren, false /*is recursive*/);
 
-  for (const auto &child : bankChildren) {
+  for (const auto &child : assemblyChildren) {
+    // register everything under this assembly
     child->registerContents(*this, localDetectorIndexes);
   }
 
+  /*
+   * The following allows us to refer to ALL nested
+   * detectors from any component in the tree. Otherwise these could
+   * be hidden by sub-tree assemblies.
+   */
   parentDetectorIndexes.reserve(parentDetectorIndexes.size() +
                                 localDetectorIndexes.size());
   parentDetectorIndexes.insert(parentDetectorIndexes.end(),
@@ -35,17 +42,26 @@ void APIComponentVisitor::registerComponentAssembly(
 
 void APIComponentVisitor::registerGenericComponent(const IComponent &component,
                                                    std::vector<size_t> &) {
+  /*
+   * For a generic leaf component we extend the component ids list, but
+   * the detector indexes entries will of course be empty
+   */
   m_componentDetectorIndexes.emplace_back(std::vector<size_t>());
   m_componentIds.emplace_back(component.getComponentID());
 }
 void APIComponentVisitor::registerDetector(
     const IDetector &detector, std::vector<size_t> &parentDetectorIndexes) {
 
+  /*
+   * Add the detector index to the parent collection, but as each detector
+   * is a leaf this components detector indexes are empty.
+   */
   parentDetectorIndexes.push_back(m_detectorInfo.indexOf(detector.getID()));
   m_componentDetectorIndexes.emplace_back(std::vector<size_t>());
   m_componentIds.emplace_back(detector.getComponentID());
 }
 APIComponentVisitor::~APIComponentVisitor() {}
+
 std::vector<Mantid::Geometry::ComponentID>
 APIComponentVisitor::componentIds() const {
   return m_componentIds;
