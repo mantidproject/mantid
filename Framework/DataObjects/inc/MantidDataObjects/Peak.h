57acc1c584846f4c9976e8ef86130996abab6dd5
David Fairbrother <DavidFair@users.noreply.github.com>
2017-03-22 10:14:55 +0000

    Re #0 Remove old MSVC workaround and update comment

--- Framework/DataObjects/inc/MantidDataObjects/Peak.h
+++ Framework/DataObjects/inc/MantidDataObjects/Peak.h
@@ -45,18 +45,19 @@ public:
 
   /// Copy constructor
   Peak(const Peak &other);
-// MSVC 2015 won't build with noexcept.
-// error C2610: 'Mantid::DataObjects::Peak::Peak(Mantid::DataObjects::Peak &&)
-// noexcept': is not a special member function which can be defaulted
+
+// MSVC 2015/17 can build with noexcept = default however
+// intellisense still incorrectly reports this as an error despite compiling. 
+// https://connect.microsoft.com/VisualStudio/feedback/details/1795240/visual-c-2015-default-move-constructor-and-noexcept-keyword-bug
+// For that reason we still use the supplied default which should be noexcept
+// once the above is fixed we can remove this workaround
+
 #if defined(_MSC_VER) && _MSC_VER <= 1910
-  Peak(Peak &&);
-  Peak &operator=(Peak &&);
-#elif defined(__GNUC__) && (__GNUC__ == 5)
+  Peak(Peak &&) = default;
+  Peak &operator=(Peak &&) = default;
+#else
   Peak(Peak &&) noexcept = default;
   Peak &operator=(Peak &&) noexcept = default;
-#else
-  Peak(Peak &&) noexcept;
-  Peak &operator=(Peak &&) noexcept;
 #endif
 
   // Construct a peak from a reference to the interface
