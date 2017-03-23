ccb2b0d372c03dc60e80268b3ba9b8ff1a83b0ff
Samuel Jackson <samueljackson@outlook.com>
2017-03-23 14:39:02 +0000

    Refs #19131 Revert NNDataPoints class

--- Framework/Kernel/inc/MantidKernel/NearestNeighbours.h
+++ Framework/Kernel/inc/MantidKernel/NearestNeighbours.h
@@ -56,14 +56,38 @@ namespace Kernel {
  */
 class NNDataPoints {
 public:
-  /// Construct a new set of data points
-  NNDataPoints(const size_t nPts, const size_t nElems);
-  ~NNDataPoints();
-  /// Return a handle to the raw ANNpointArray wrapped by this class
-  ANNpointArray rawData();
-  /// Access a raw point in the collection of points
-  ANNcoord *mutablePoint(const size_t i);
-  NNDataPoints(const NNDataPoints &) = delete;
+  /** Construct a new set of data points
+   *
+   * @param nPts :: the number of data points
+   * @param nElems :: the number of elements for each point
+   */
+  NNDataPoints(const size_t nPts, const size_t nElems)
+          : m_nPts(nPts) {
+    m_data = annAllocPts(static_cast<int>(m_nPts), static_cast<int>(nElems));
+  }
+
+  ~NNDataPoints() { annDeallocPts(m_data); }
+
+  /** Return a handle to the raw ANNpointArray wrapped by this class
+   *
+   * @return handle to the raw ANNpointArray
+   */
+  ANNpointArray rawData() { return m_data; }
+
+  /** Access a raw point in the collection of points
+   *
+   * This will check the index used is within bounds and return nullptr if
+   * outside of those bounds
+   *
+   * @param i :: the index of the point to return a handle to
+   * @return handle to a single point in the collection of points
+   */
+  ANNcoord *mutablePoint(const size_t i) {
+    if (i < m_nPts)
+      return m_data[i];
+    else
+      return nullptr;
+  }
 
 private:
   /// Number of points stored
