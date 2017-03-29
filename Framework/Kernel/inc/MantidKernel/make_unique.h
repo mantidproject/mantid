36d4837265ec7f29e40cebc3c6d6925904b11a07
Steven Hahn <hahnse@ornl.gov>
2017-03-29 08:08:11 -0400

    Update make_unique for MSVC.

--- Framework/Kernel/inc/MantidKernel/make_unique.h
+++ Framework/Kernel/inc/MantidKernel/make_unique.h
@@ -11,41 +11,42 @@
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3656.htm
 
 namespace Mantid {
-
 namespace Kernel {
 
-#if __cplusplus > 201103L // C++14
+#if __cplusplus >= 201402L ||                                                  \
+    (defined(_MSC_VER) && (_MSC_VER > 1700)) // C++14 or MSVC 2013+
 
 using std::make_unique;
 
-#else  // C++11
+#else // C++11
 
 template <class T> struct _Unique_if {
-  typedef std::unique_ptr<T> _Single_object;
+  using _Single_object = std::unique_ptr<T>;
 };
 
 template <class T> struct _Unique_if<T[]> {
-  typedef std::unique_ptr<T[]> _Unknown_bound;
+  using _Unknown_bound = std::unique_ptr<T[]>;
 };
 
 template <class T, size_t N> struct _Unique_if<T[N]> {
-  struct __invalid_type {};
+  using _Known_bound = void;
 };
+
+template <class T, class... Args>
+typename _Unique_if<T>::_Single_object make_unique(Args &&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
 
 template <class T>
 typename _Unique_if<T>::_Unknown_bound make_unique(size_t n) {
-  typedef typename std::remove_extent<T>::type U;
+  using U = typename std::remove_extent<T>::type;
   return std::unique_ptr<T>(new U[n]());
 }
 
 template <class T, class... Args>
-inline typename _Unique_if<T>::_Single_object make_unique(Args &&... args) {
-  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
+typename _Unique_if<T>::_Known_bound make_unique(Args &&...) = delete;
 
-template <class T, class... Args>
-inline typename _Unique_if<T>::_Known_bound make_unique(Args &&...) = delete;
 #endif // __cplusplus == 201402L
-} // namespace Kernel
-} // namespace Mantid
+}
+}
 #endif // Mantid_make_unique_h
