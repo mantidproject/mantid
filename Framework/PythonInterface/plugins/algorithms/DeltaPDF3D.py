3cfb290a9437e454636d5867cb68e4a83ab64b9c
Ross Whitfield <whitfieldre@ornl.gov>
2017-03-29 11:26:07 -0400

    Add inprint option

--- Framework/PythonInterface/plugins/algorithms/DeltaPDF3D.py
+++ Framework/PythonInterface/plugins/algorithms/DeltaPDF3D.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
 from mantid.api import PythonAlgorithm, AlgorithmFactory, IMDHistoWorkspaceProperty, PropertyMode, WorkspaceProperty, Progress
-from mantid.kernel import Direction, EnabledWhenProperty, PropertyCriterion, Property
+from mantid.kernel import Direction, EnabledWhenProperty, PropertyCriterion, Property, StringListValidator
 from mantid.geometry import SpaceGroupFactory
 from mantid import logger
 import numpy as np
@@ -28,8 +28,11 @@ class DeltaPDF3D(PythonAlgorithm):
                              "Output Workspace")
         self.declareProperty("RemoveReflections", True, "Remove reflections")
         condition = EnabledWhenProperty("RemoveReflections", PropertyCriterion.IsDefault)
-        self.declareProperty("BoxWidth", 0.1, "Width of box to remove reflections, in (HKL)")
-        self.setPropertySettings("BoxWidth", condition)
+        self.declareProperty("Shape", "sphere", doc="Shape to cut out reflections",
+                             validator=StringListValidator(['sphere', 'cube']))
+        self.setPropertySettings("Shape", condition)
+        self.declareProperty("Width", 0.1, "Width of sphere/cube to remove reflections, in (HKL)")
+        self.setPropertySettings("Width", condition)
         self.declareProperty("SpaceGroup", "", doc="Space groups, for reflection removal")
         self.setPropertySettings("SpaceGroup", condition)
         self.declareProperty("CutSphere", False, "Limit min/max q values. Can help with edge effects.")
@@ -42,6 +45,7 @@ class DeltaPDF3D(PythonAlgorithm):
         condition = EnabledWhenProperty("Convolution", PropertyCriterion.IsDefault)
         self.declareProperty("ConvolutionWidth", 2.0, "Width of gaussian convolution in pixels")
         self.setPropertySettings("ConvolutionWidth", condition)
+        self.declareProperty("Inpaint", False, "Use inpainting by biharmonic functions to fill remove reflactions")
         self.declareProperty("FFT", True, "Calculate 3D-deltaPDF")
 
     def validateInputs(self):
@@ -56,6 +60,16 @@ class DeltaPDF3D(PythonAlgorithm):
                 dimY.getMaximum() != -dimY.getMinimum() or
                 dimZ.getMaximum() != -dimZ.getMinimum()):
             issues['InputWorkspace'] = 'dimensions must be centered on zero'
+        if self.getProperty("Convolution").value:
+            try:
+                import astropy
+            except ImportError:
+                issues["Convolution"] = 'python-astropy required to do convolution'
+        if self.getProperty("Inpaint").value:
+            try:
+                import skimage
+            except ImportError:
+                issues["Inpaint"] = 'scikit-image required to do inprinting'
         return issues
 
     def PyExec(self):
@@ -69,10 +83,13 @@ class DeltaPDF3D(PythonAlgorithm):
         X=np.linspace(dimX.getMinimum(),dimX.getMaximum(),dimX.getNBins()+1)
         Y=np.linspace(dimY.getMinimum(),dimY.getMaximum(),dimY.getNBins()+1)
         Z=np.linspace(dimZ.getMinimum(),dimZ.getMaximum(),dimZ.getNBins()+1)
+        Xs, Ys, Zs = np.mgrid[(X[0]+X[1])/2:(X[-1]+X[-2])/2:dimX.getNBins()*1j,
+                              (Y[0]+Y[1])/2:(Y[-1]+Z[-2])/2:dimY.getNBins()*1j,
+                              (Z[0]+Z[1])/2:(Y[-1]+Z[-2])/2:dimZ.getNBins()*1j]
 
         if self.getProperty("RemoveReflections").value:
             progress.report("Removing Reflections")
-            box_width = self.getProperty("BoxWidth").value
+            width = self.getProperty("Width").value
             space_group = self.getProperty("SpaceGroup").value
             if space_group is '':
                 check_space_group = False
@@ -83,22 +100,18 @@ class DeltaPDF3D(PythonAlgorithm):
                 for k in range(int(np.ceil(dimY.getMinimum())), int(np.floor(dimY.getMaximum()))+1):
                     for l in range(int(np.ceil(dimZ.getMinimum())), int(np.floor(dimZ.getMaximum()))+1):
                         if not check_space_group or sg.isAllowedReflection([h,k,l]):
-                            x_min=np.searchsorted(X,h-box_width)
-                            x_max=np.searchsorted(X,h+box_width)
-                            y_min=np.searchsorted(Y,k-box_width)
-                            y_max=np.searchsorted(Y,k+box_width)
-                            z_min=np.searchsorted(Z,l-box_width)
-                            z_max=np.searchsorted(Z,l+box_width)
+                            x_min=np.searchsorted(X,h-width)
+                            x_max=np.searchsorted(X,h+width)
+                            y_min=np.searchsorted(Y,k-width)
+                            y_max=np.searchsorted(Y,k+width)
+                            z_min=np.searchsorted(Z,l-width)
+                            z_max=np.searchsorted(Z,l+width)
                             signal[x_min:x_max,y_min:y_max,z_min:z_max]=np.nan
 
         if self.getProperty("CutSphere").value:
             progress.report("Cutting to sphere")
             sphereMin = self.getProperty("SphereMin").value
             sphereMax = self.getProperty("SphereMax").value
-            shape=signal.shape
-            Xs=((X[1:] + X[:-1])/2).reshape((shape[0], 1, 1))*np.ones(shape)
-            Ys=((Y[1:] + Y[:-1])/2).reshape((1, shape[1], 1))*np.ones(shape)
-            Zs=((Z[1:] + Z[:-1])/2).reshape((1, 1, shape[2]))*np.ones(shape)
             q=Xs**2 + Ys**2 + Zs**2
             if sphereMax != Property.EMPTY_DBL:
                 signal[q > sphereMax**2]=np.nan
@@ -107,14 +120,11 @@ class DeltaPDF3D(PythonAlgorithm):
 
         if self.getProperty("Convolution").value:
             progress.report("Convoluting signal")
-            G1D = Gaussian1DKernel(2).array
-            G3D = G1D * G1D.reshape((-1,1)) * G1D.reshape((-1,1,1))
-            try:
-                logger.debug('Trying astropy.convolution.convolve_fft for convolution')
-                signal = convolve_fft(signal, G3D)  # Faster but will fail with large signal and kernel arrays
-            except ValueError:
-                logger.debug('Using astropy.convolution.convolve for convolution')
-                signal = convolve(signal, G3D)
+            signal = self._convolution(signal)
+
+        if self.getProperty("Inpaint").value:
+            progress.report("Inpainting signal")
+            signal = self._inpaint(signal)
 
         if self.getProperty("FFT").value:
             progress.report("Running FFT")
@@ -154,9 +164,20 @@ class DeltaPDF3D(PythonAlgorithm):
 
         self.setProperty("OutputWorkspace", outWS)
 
+    def _convolution(self, signal):
+        from astropy.convolution import convolve, convolve_fft, Gaussian1DKernel
+        G1D = Gaussian1DKernel(2).array
+        G3D = G1D * G1D.reshape((-1,1)) * G1D.reshape((-1,1,1))
+        try:
+            logger.debug('Trying astropy.convolution.convolve_fft for convolution')
+            return convolve_fft(signal, G3D)  # Faster but will fail with large signal and kernel arrays
+        except ValueError:
+            logger.debug('Using astropy.convolution.convolve for convolution')
+            return convolve(signal, G3D)
 
-try:
-    from astropy.convolution import convolve, convolve_fft, Gaussian1DKernel
-    AlgorithmFactory.subscribe(DeltaPDF3D)
-except ImportError:
-    logger.debug('Failed to subscribe algorithm DeltaPDF3D; cannot import astropy')
+    def _inpaint(self, signal):
+        from skimage.restoration import inpaint
+        return inpaint.inpaint_biharmonic(signal, np.isnan(signal))
+
+
+AlgorithmFactory.subscribe(DeltaPDF3D)
