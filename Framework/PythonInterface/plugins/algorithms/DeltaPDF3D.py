1c8fb4b28bcbfe89b48060a506f26ff5764e394b
Ross Whitfield <whitfieldre@ornl.gov>
2017-03-29 16:57:21 -0400

    Allow different widths for dimensions

--- Framework/PythonInterface/plugins/algorithms/DeltaPDF3D.py
+++ Framework/PythonInterface/plugins/algorithms/DeltaPDF3D.py
@@ -1,6 +1,7 @@
 from __future__ import (absolute_import, division, print_function)
 from mantid.api import PythonAlgorithm, AlgorithmFactory, IMDHistoWorkspaceProperty, PropertyMode, WorkspaceProperty, Progress
-from mantid.kernel import Direction, EnabledWhenProperty, PropertyCriterion, Property, StringListValidator
+from mantid.kernel import (Direction, EnabledWhenProperty, PropertyCriterion, Property, StringListValidator, FloatArrayBoundedValidator,
+                           FloatArrayProperty, FloatBoundedValidator)
 from mantid.geometry import SpaceGroupFactory
 from mantid import logger
 import numpy as np
@@ -35,44 +36,66 @@ class DeltaPDF3D(PythonAlgorithm):
         self.declareProperty("Shape", "sphere", doc="Shape to cut out reflections",
                              validator=StringListValidator(['sphere', 'cube']))
         self.setPropertySettings("Shape", condition)
-        self.declareProperty("Width", 0.1, "Width of sphere/cube to remove reflections, in (HKL)")
+        val_min_zero = FloatArrayBoundedValidator()
+        val_min_zero.setLower(0.)
+        self.declareProperty(FloatArrayProperty("Width", [0.1], validator=val_min_zero),
+                             "Width of sphere/cube to remove reflections, in (HKL)")
         self.setPropertySettings("Width", condition)
         self.declareProperty("SpaceGroup", "", doc="Space groups, for reflection removal")
         self.setPropertySettings("SpaceGroup", condition)
         self.declareProperty("CutSphere", False, "Limit min/max q values. Can help with edge effects.")
         condition = EnabledWhenProperty("CutSphere", PropertyCriterion.IsNotDefault)
-        self.declareProperty("SphereMin", Property.EMPTY_DBL, "Min Sphere")
+        self.declareProperty(FloatArrayProperty("SphereMin", [Property.EMPTY_DBL], validator=val_min_zero), "Min Sphere")
         self.setPropertySettings("SphereMin", condition)
-        self.declareProperty("SphereMax", Property.EMPTY_DBL, "Min Sphere")
+        self.declareProperty(FloatArrayProperty("SphereMax", [Property.EMPTY_DBL], validator=val_min_zero), "Max Sphere")
         self.setPropertySettings("SphereMax", condition)
         self.declareProperty("Convolution", True, "Apply convolution to fill in removed reflections")
         condition = EnabledWhenProperty("Convolution", PropertyCriterion.IsDefault)
-        self.declareProperty("ConvolutionWidth", 2.0, "Width of gaussian convolution in pixels")
+        self.declareProperty("ConvolutionWidth", 2.0, validator=FloatBoundedValidator(0.),
+                             doc="Width of gaussian convolution in pixels")
         self.setPropertySettings("ConvolutionWidth", condition)
         self.declareProperty("Inpaint", False, "Use inpainting by biharmonic functions to fill remove reflactions")
 
     def validateInputs(self):
         issues = dict()
+
         inWS = self.getProperty("InputWorkspace").value
         dimX=inWS.getXDimension()
         dimY=inWS.getYDimension()
         dimZ=inWS.getZDimension()
+
         if dimX.name != '[H,0,0]' or dimY.name != '[0,K,0]' or dimZ.name != '[0,0,L]':
             issues['InputWorkspace'] = 'dimensions must be [H,0,0], [0,K,0] and [0,0,L]'
+
         if (dimX.getMaximum() != -dimX.getMinimum() or
                 dimY.getMaximum() != -dimY.getMinimum() or
                 dimZ.getMaximum() != -dimZ.getMinimum()):
             issues['InputWorkspace'] = 'dimensions must be centered on zero'
+
         if self.getProperty("Convolution").value:
             try:
                 import astropy # noqa
             except ImportError:
                 issues["Convolution"] = 'python-astropy required to do convolution'
+
         if self.getProperty("Inpaint").value:
             try:
                 import skimage # noqa
             except ImportError:
                 issues["Inpaint"] = 'scikit-image required to do inprinting'
+
+        width = self.getProperty("Width").value
+        if len(width) != 1 and len(width) != 3:
+            issues["Width"] = 'Must provide 1 or 3 widths'
+
+        sphereMin = self.getProperty("SphereMin").value
+        if len(sphereMin) != 1 and len(sphereMin) != 3:
+            issues["SphereMin"] = 'Must provide 1 or 3 widths'
+
+        sphereMax = self.getProperty("SphereMax").value
+        if len(sphereMax) != 1 and len(sphereMax) != 3:
+            issues["SphereMax"] = 'Must provide 1 or 3 widths'
+
         return issues
 
     def PyExec(self):
@@ -93,6 +116,8 @@ class DeltaPDF3D(PythonAlgorithm):
         if self.getProperty("RemoveReflections").value:
             progress.report("Removing Reflections")
             width = self.getProperty("Width").value
+            if len(width)==1:
+                width = np.repeat(width, 3)
             cut_shape = self.getProperty("Shape").value
             space_group = self.getProperty("SpaceGroup").value
             if space_group:
@@ -105,25 +130,28 @@ class DeltaPDF3D(PythonAlgorithm):
                     for l in range(int(np.ceil(dimZ.getMinimum())), int(np.floor(dimZ.getMaximum()))+1):
                         if not check_space_group or sg.isAllowedReflection([h,k,l]):
                             if cut_shape == 'cube':
-                                x_min=np.searchsorted(X,h-width)
-                                x_max=np.searchsorted(X,h+width)
-                                y_min=np.searchsorted(Y,k-width)
-                                y_max=np.searchsorted(Y,k+width)
-                                z_min=np.searchsorted(Z,l-width)
-                                z_max=np.searchsorted(Z,l+width)
+                                x_min=np.searchsorted(X,h-width[0])
+                                x_max=np.searchsorted(X,h+width[0])
+                                y_min=np.searchsorted(Y,k-width[1])
+                                y_max=np.searchsorted(Y,k+width[1])
+                                z_min=np.searchsorted(Z,l-width[2])
+                                z_max=np.searchsorted(Z,l+width[2])
                                 signal[x_min:x_max,y_min:y_max,z_min:z_max]=np.nan
                             else:
-                                signal[(Xs-h)**2 + (Ys-k)**2 + (Zs-l)**2 < width**2]=np.nan
+                                signal[(Xs-h)**2/width[0]**2 + (Ys-k)**2/width[1]**2 + (Zs-l)**2/width[2]**2 < 1]=np.nan
 
         if self.getProperty("CutSphere").value:
             progress.report("Cutting to sphere")
             sphereMin = self.getProperty("SphereMin").value
+            if sphereMin[0] < Property.EMPTY_DBL:
+                if len(sphereMin)==1:
+                    sphereMin = np.repeat(sphereMin, 3)
+                signal[Xs**2/sphereMin[0]**2 + Ys**2/sphereMin[1]**2 + Zs**2/sphereMin[2]**2 < 1]=np.nan
             sphereMax = self.getProperty("SphereMax").value
-            q=Xs**2 + Ys**2 + Zs**2
-            if sphereMax != Property.EMPTY_DBL:
-                signal[q > sphereMax**2]=np.nan
-            if sphereMin != Property.EMPTY_DBL:
-                signal[q < sphereMin**2]=np.nan
+            if sphereMax[0] < Property.EMPTY_DBL:
+                if len(sphereMax)==1:
+                    sphereMax = np.repeat(sphereMax, 3)
+                signal[Xs**2/sphereMax[0]**2 + Ys**2/sphereMax[1]**2 + Zs**2/sphereMax[2]**2 > 1]=np.nan
 
         if self.getProperty("Convolution").value:
             progress.report("Convoluting signal")
