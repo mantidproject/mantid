c2c8d0519937196cc18e6bff3db5ed2993307f3b
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-04-07 13:33:00 +0100

    refs #19294 Fixed the stray tab in muonAnalysis

--- MantidQt/CustomInterfaces/src/Muon/MuonAnalysis.cpp
+++ MantidQt/CustomInterfaces/src/Muon/MuonAnalysis.cpp
@@ -1662,16 +1662,20 @@ void MuonAnalysis::plotSpectrum(const QString &wsName, bool logScale) {
   // Plot data in the given window with given options
   s << "def plot_data(ws_name,errors, connect, window_to_use):";
   if (parsePlotType(m_uiForm.frontPlotFuncs) == PlotType::Asymmetry) {
-    s << "  w = plotSpectrum(source=ws_name, indices=0,  "
-         "distribution=mantidqtpython.MantidQt.DistributionFalse,error_bars = "
-         "errors, type = connect, "
+    // clang-format off
+    s << "w = plotSpectrum(source = ws_name,"
+         "indices = 0,"
+         "distribution = mantidqtpython.MantidQt.DistributionFalse,"
+         "error_bars = errors," 
+         "type = connect,"
          "window = window_to_use)";
+    // clang-format on
   } else {
     // clang-format off
     s << "w = plotSpectrum(source = ws_name,"
          "indices = 0,"
          "distribution = mantidqtpython.MantidQt.DistributionDefault,"
-	     "error_bars = errors,"
+	 "error_bars = errors,"
          "type = connect,"
          "window = window_to_use)";
     // clang-format on
