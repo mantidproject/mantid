74eeb08ec89d2b46a9d477d466dfd2e89c7c4aae
Garrett Granroth <granrothge@ornl.gov>
2017-04-20 12:04:34 -0400

    Re #19393 updated label in Instrument Setupwidget.py to S2

--- scripts/DGSPlanner/InstrumentSetupWidget.py
+++ scripts/DGSPlanner/InstrumentSetupWidget.py
@@ -170,7 +170,7 @@ class InstrumentSetupWidget(QtGui.QWidget):
         self.signaldict['Ei']=self.Ei
         self.validatorS2=QtGui.QDoubleValidator(-90.,90.,5,self)
         self.validatorEi=QtGui.QDoubleValidator(1.,10000.,5,self)
-        self.labelS2=QtGui.QLabel('HYSPEC S2')
+        self.labelS2=QtGui.QLabel('S2')
         self.labelEi=QtGui.QLabel('Incident Energy')
         self.editS2=QtGui.QLineEdit()
         self.editS2.setValidator(self.validatorS2)
