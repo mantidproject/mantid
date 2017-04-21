cbe8d60628470041254bc87cc0481c310bbdfc01
Garrett Granroth <granrothge@ornl.gov>
2017-04-21 19:02:40 -0400

    Re #19416 generates data and monitor ws, does not load idf file

--- Framework/PythonInterface/plugins/algorithms/LoadExedPython.py
+++ Framework/PythonInterface/plugins/algorithms/LoadExedPython.py
@@ -7,7 +7,7 @@ import numpy as np
 import copy
 import types
 
-def read_header(fn):
+def read_file(fn):
     """
     function to read header and return a dictionary of the parameters
     """
@@ -19,8 +19,6 @@ def read_header(fn):
     while (header):
         b_line = fin.readline()
         line=b_line.decode('ascii')
-        if line.find("Following are binary data")>0:
-            header=False
         if line.find("=")>0:
             line_lst=line.split('=')
             if len(line_lst)==2:
@@ -30,30 +28,15 @@ def read_header(fn):
               parms_dict[line_lst[0].strip()]=parm_val
             if line.find("Comment")>-1:
                parms_dict['Comment']=line.strip('Comment =')
-    f_pos=fin.tell()
-    fin.close()
-    parms_dict['phi']=copy.deepcopy(parms_dict['CAR_OMEGA_MAG'])
-    return parms_dict, f_pos
-    
-def struct_data_read(fin,nrows,data_type='i',byte_size=4):
-    """
-    helper function to read binary data_type
-    requires the file handle, number of rows and data_type
-    """
-    tmp_lst=[]
-    for i in range(nrows):
-        data = struct.unpack(data_type,fin.read(byte_size))[0]
-    tmp_lst.append(data)
-    return tmp_lst
-
-def read_binary_data(fn, f_pos, nrows, nbins):
-    """
-    function to read binary data from file
-    requires the filename, the byte position in the file, number of rows and number of bins
-    """
-    fin = open(fn,'rb')
-    fin.seek(f_pos)
-
+        if line.find("Following are binary data")>0:
+            header=False
+            while line.find("DATA=")<0:
+                b_line = fin.readline()
+                line=b_line.decode('ascii')
+                print line
+    #
+    nrows=int(parms_dict['NDET'])
+    nbins=int(parms_dict['NTC'])
     print "read UDET"
     det_udet = struct_data_read(fin,nrows)
 
@@ -65,9 +48,21 @@ def read_binary_data(fn, f_pos, nrows, nbins):
 
     print "read Data"
     data = np.fromfile(fin, np.uint32, nrows*nbins, '')
-    fin.close()
-    return det_udet, det_count, det_tbc, data
 
+    fin.close()
+    parms_dict['phi']=copy.deepcopy(parms_dict['CAR_OMEGA_MAG'])
+    return parms_dict, det_udet, det_count, det_tbc, data
+
+def struct_data_read(fin,nrows,data_type='i',byte_size=4):
+    """
+    helper function to read binary data_type
+    requires the file handle, number of rows and data_type
+    """
+    tmp_lst=[]
+    for i in range(nrows):
+        data = struct.unpack(data_type,fin.read(byte_size))[0]
+        tmp_lst.append(data)
+    return tmp_lst
 
 class LoadEXED(PythonAlgorithm):
     __doc__ = """This is the EXED data loader written in Python.
@@ -112,11 +107,13 @@ class LoadEXED(PythonAlgorithm):
     def PyExec(self):
         from mantid.simpleapi import LoadInstrument
         #from mantid.simpleapi import LoadExedHelper
+        from mantid.simpleapi import CreateWorkspace
         from mantid.simpleapi import AddSampleLog
         from mantid.simpleapi import SetGoniometer
         from mantid.simpleapi import ExtractSpectra
         from mantid.simpleapi import MaskDetectors
         from mantid.simpleapi import RemoveMaskedSpectra
+        from mantid.simpleapi import mtd
 
         #self.LoadRaw()
 
@@ -130,40 +127,46 @@ class LoadEXED(PythonAlgorithm):
 
         #load header
 
-        parms_dict, f_pos = read_header(fn)
+        parms_dict, det_udet, det_count, det_tbc, data = read_file(fn)
         nrows=int(parms_dict['NDET'])
         nbins=int(parms_dict['NTC'])
-        det_udet, det_count, det_tbc, data = read_binary_data(fn,f_pos, nrows,nbins)
+        ydata=data.astype(np.float)
+        edata=np.sqrt(ydata)
+        CreateWorkspace(OutputWorkspace=wsn,DataX=det_tbc,DataY=ydata,DataE=edata,NSpec=nrows,UnitX='TOF')
 
-        ws = WorkspaceFactory.create("Workspace2D", NVectors=nrows,
-                                            XLength = nbins+1,
-                                            YLength = nbins)
-        print "ws:", ws
 
-        ws.setYUnit("Counts")
-        ws.setTitle("Data")
-        unit = ws.getAxis(0).setUnit("TOF")
-        data.shape = (nrows,nbins)
-        xdata = np.array(det_tbc)
-        xdata_mon = np.linspace(xdata[0],xdata[-1], len(xdata))
-        for i in range(nrows-2):
-                #print i
-            tmp = data[i]
-            ydata = tmp.astype(np.float)
-            edata = np.sqrt(ydata)
-            ws.setX(i,xdata)
-            ws.setY(i,ydata)
-            ws.setE(i,edata)
-        for i in range(nrows-2,nrows):
-                #print i
-            tmp = data[i]
-            ydata = tmp.astype(np.float)
-            edata = np.sqrt(ydata)
-            ws.setX(i,xdata_mon)
-            ws.setY(i,ydata)
-            ws.setE(i,edata)
-
-        print nrows, nbins
+        #ws = WorkspaceFactory.create("Workspace2D", NVectors=nrows,
+        #                                    XLength = nbins+1,
+        #                                    YLength = nbins)
+        self.setProperty("OutputWorkspace", wsn)
+        print "ws:", wsn
+        ws=mtd[wsn]
+        #ws.setYUnit("Counts")
+        #ws.setTitle("Data")
+        #unit = ws.getAxis(0).setUnit("TOF")
+        #data.shape = (nrows,nbins)
+        # xdata = np.array(det_tbc)
+        # xdata_mon = np.linspace(xdata[0],xdata[-1], len(xdata))
+        # ydata=data.astype(np.float)
+        # edata=np.sqrt(ydata)
+        # for i in range(nrows-2):
+        #         #print i
+        #     tmp = data[i]
+        #     ydata = tmp.astype(np.float)
+        #     edata = np.sqrt(ydata)
+        #     ws.setX(i,xdata)
+        #     ws.setY(i,ydata)
+        #     ws.setE(i,edata)
+        # for i in range(nrows-2,nrows):
+        #         #print i
+        #     tmp = data[i]
+        #     ydata = tmp.astype(np.float)
+        #     edata = np.sqrt(ydata)
+        #     ws.setX(i,xdata_mon)
+        #     ws.setY(i,ydata)
+        #     ws.setE(i,edata)
+        #
+        # print nrows, nbins
 
         print "set detector IDs"
     	#set detetector IDs
@@ -176,10 +179,10 @@ class LoadEXED(PythonAlgorithm):
         if (self.fxml == ""):
             self.fxml = fn[:fn.find('.')] + ".xml"
 
-        print ws, wsn
+        #print ws, wsn
 
 
-        LoadInstrument(Workspace=ws, Filename = self.fxml, RewriteSpectraMap= True)
+        #LoadInstrument(Workspace=str(ws), Filename = self.fxml, RewriteSpectraMap= True)
 
         #Sample_logs load the header values into the sample logs
 
@@ -187,17 +190,17 @@ class LoadEXED(PythonAlgorithm):
             #print (sl.encode('ascii','ignore'), parms_dict[sl].encode('ascii','ignore'))
             key_in=sl.encode('ascii','ignore')
             if isinstance(parms_dict[sl],types.UnicodeType):
-                AddSampleLog(Workspace=ws, LogName=key_in, LogText=parms_dict[sl].encode('ascii','ignore'))#, LogType='Number')
+                AddSampleLog(Workspace=wsn, LogName=key_in, LogText=parms_dict[sl].encode('ascii','ignore'))#, LogType='Number')
             else:
-                AddSampleLog(Workspace=ws, LogName=key_in, LogText=str(parms_dict[sl])), #LogType='Number')
+                AddSampleLog(Workspace=wsn, LogName=key_in, LogText=str(parms_dict[sl])), #LogType='Number')
 
-        SetGoniometer(Workspace=ws, Goniometers='Universal', Axis0='phi,0,1,0,1')
+        SetGoniometer(Workspace=wsn, Goniometers='Universal', Axis0='phi,0,1,0,1')
         # ExtractSingleSpectrum(InputWorkspace = wsn, WorkspaceIndex = nrows-2, OutputWorkspace = wsn + '_Monitors')
         ExtractSpectra(InputWorkspace = wsn, WorkspaceIndexList = ','.join([str(s) for s in range(nrows-2, nrows)]), OutputWorkspace = wsn + '_Monitors')
         MaskDetectors(Workspace = wsn, WorkspaceIndexList = ','.join([str(s) for s in range(nrows-2, nrows)]))
         ws = RemoveMaskedSpectra(InputWorkspace = wsn, OutputWorkspace = wsn)
 
-        self.setProperty("OutputWorkspace", ws)
+        self.setProperty("OutputWorkspace", wsn)
 
 
 # Register algorthm with Mantid.
