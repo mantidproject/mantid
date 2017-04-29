4c1f90179925c885163de13041f2e5d31a11a0cd
Steven Hahn <hahnse@ornl.gov>
2017-04-29 08:51:08 -0400

    Reduce memory allocation in MDNormSCD.

--- Framework/MDAlgorithms/src/MDNormSCD.cpp
+++ Framework/MDAlgorithms/src/MDNormSCD.cpp
@@ -414,8 +414,10 @@ void MDNormSCD::calculateNormalization(
 
   std::vector<std::atomic<signal_t>> signalArray(m_normWS->getNPoints());
 
+  std::vector<std::vector<double>> intersections;
   auto prog = make_unique<API::Progress>(this, 0.3, 1.0, ndets);
-  PARALLEL_FOR_IF(Kernel::threadSafe(*integrFlux))
+#pragma omp parallel for private(intersections) if Kernel::threadSafe(         \
+    *integrFlux)
   for (int64_t i = 0; i < ndets; i++) {
     PARALLEL_START_INTERUPT_REGION
 
@@ -430,7 +432,7 @@ void MDNormSCD::calculateNormalization(
     const auto detID = detector.getID();
 
     // Intersections
-    auto intersections = calculateIntersections(theta, phi);
+    this->calculateIntersections(intersections, theta, phi);
     if (intersections.empty())
       continue;
 
@@ -474,9 +476,8 @@ void MDNormSCD::calculateNormalization(
         continue; // Assume zero contribution if difference is small
 
       // Average between two intersections for final position
-      std::transform(curIntSec.getBareArray(),
-                     curIntSec.getBareArray() + vmdDims - 1,
-                     prevIntSec.getBareArray(), pos.begin(),
+      std::transform(curIntSec.begin(), curIntSec.begin() + vmdDims - 1,
+                     prevIntSec.begin(), pos.begin(),
                      VectorHelper::SimpleAverage<coord_t>());
       std::vector<coord_t> posNew = affineTrans * pos;
       size_t linIndex = m_normWS->getLinearIndexAtCoord(posNew.data());
@@ -582,8 +583,9 @@ void MDNormSCD::calcIntegralsForIntersections(
  * @param phi Azimuthal angle with detector
  * @return A list of intersections in HKL space
  */
-std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
-                                                           const double phi) {
+void MDNormSCD::calculateIntersections(
+    std::vector<std::vector<double>> &intersections, const double theta,
+    const double phi) {
   V3D q(-sin(theta) * cos(phi), -sin(theta) * sin(phi), 1. - cos(theta));
   q = m_rubw * q;
   if (convention == "Crystallography") {
@@ -599,7 +601,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
   auto hNBins = m_hX.size();
   auto kNBins = m_kX.size();
   auto lNBins = m_lX.size();
-  std::vector<Kernel::VMD> intersections;
+  intersections.clear();
   intersections.reserve(hNBins + kNBins + lNBins + 8);
 
   // calculate intersections with planes perpendicular to h
@@ -620,8 +622,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
           if ((ki >= m_kmin) && (ki <= m_kmax) && (li >= m_lmin) &&
               (li <= m_lmax)) {
             double momi = fmom * (hi - hStart) + m_kiMin;
-            Mantid::Kernel::VMD v(hi, ki, li, momi);
-            intersections.push_back(v);
+            intersections.push_back({hi, ki, li, momi});
           }
         }
       }
@@ -634,8 +635,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
       double lhmin = fl * (m_hmin - hStart) + lStart;
       if ((khmin >= m_kmin) && (khmin <= m_kmax) && (lhmin >= m_lmin) &&
           (lhmin <= m_lmax)) {
-        Mantid::Kernel::VMD v(m_hmin, khmin, lhmin, momhMin);
-        intersections.push_back(v);
+        intersections.push_back({m_hmin, khmin, lhmin, momhMin});
       }
     }
     double momhMax = fmom * (m_hmax - hStart) + m_kiMin;
@@ -645,8 +645,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
       double lhmax = fl * (m_hmax - hStart) + lStart;
       if ((khmax >= m_kmin) && (khmax <= m_kmax) && (lhmax >= m_lmin) &&
           (lhmax <= m_lmax)) {
-        Mantid::Kernel::VMD v(m_hmax, khmax, lhmax, momhMax);
-        intersections.push_back(v);
+        intersections.push_back({m_hmax, khmax, lhmax, momhMax});
       }
     }
   }
@@ -668,8 +667,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
           if ((hi >= m_hmin) && (hi <= m_hmax) && (li >= m_lmin) &&
               (li <= m_lmax)) {
             double momi = fmom * (ki - kStart) + m_kiMin;
-            Mantid::Kernel::VMD v(hi, ki, li, momi);
-            intersections.push_back(v);
+            intersections.push_back({hi, ki, li, momi});
           }
         }
       }
@@ -682,8 +680,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
       double lkmin = fl * (m_kmin - kStart) + lStart;
       if ((hkmin >= m_hmin) && (hkmin <= m_hmax) && (lkmin >= m_lmin) &&
           (lkmin <= m_lmax)) {
-        Mantid::Kernel::VMD v(hkmin, m_kmin, lkmin, momkMin);
-        intersections.push_back(v);
+        intersections.push_back({hkmin, m_kmin, lkmin, momkMin});
       }
     }
     double momkMax = fmom * (m_kmax - kStart) + m_kiMin;
@@ -693,8 +690,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
       double lkmax = fl * (m_kmax - kStart) + lStart;
       if ((hkmax >= m_hmin) && (hkmax <= m_hmax) && (lkmax >= m_lmin) &&
           (lkmax <= m_lmax)) {
-        Mantid::Kernel::VMD v(hkmax, m_kmax, lkmax, momkMax);
-        intersections.push_back(v);
+        intersections.push_back({hkmax, m_kmax, lkmax, momkMax});
       }
     }
   }
@@ -716,8 +712,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
           if ((hi >= m_hmin) && (hi <= m_hmax) && (ki >= m_kmin) &&
               (ki <= m_kmax)) {
             double momi = fmom * (li - lStart) + m_kiMin;
-            Mantid::Kernel::VMD v(hi, ki, li, momi);
-            intersections.push_back(v);
+            intersections.push_back({hi, ki, li, momi});
           }
         }
       }
@@ -730,8 +725,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
       double klmin = fk * (m_lmin - lStart) + kStart;
       if ((hlmin >= m_hmin) && (hlmin <= m_hmax) && (klmin >= m_kmin) &&
           (klmin <= m_kmax)) {
-        Mantid::Kernel::VMD v(hlmin, klmin, m_lmin, momlMin);
-        intersections.push_back(v);
+        intersections.push_back({hlmin, klmin, m_lmin, momlMin});
       }
     }
     double momlMax = fmom * (m_lmax - lStart) + m_kiMin;
@@ -741,8 +735,7 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
       double klmax = fk * (m_lmax - lStart) + kStart;
       if ((hlmax >= m_hmin) && (hlmax <= m_hmax) && (klmax >= m_kmin) &&
           (klmax <= m_kmax)) {
-        Mantid::Kernel::VMD v(hlmax, klmax, m_lmax, momlMax);
-        intersections.push_back(v);
+        intersections.push_back({hlmax, klmax, m_lmax, momlMax});
       }
     }
   }
@@ -750,22 +743,16 @@ std::vector<Kernel::VMD> MDNormSCD::calculateIntersections(const double theta,
   // add endpoints
   if ((hStart >= m_hmin) && (hStart <= m_hmax) && (kStart >= m_kmin) &&
       (kStart <= m_kmax) && (lStart >= m_lmin) && (lStart <= m_lmax)) {
-    Mantid::Kernel::VMD v(hStart, kStart, lStart, m_kiMin);
-    intersections.push_back(v);
+    intersections.push_back({hStart, kStart, lStart, m_kiMin});
   }
   if ((hEnd >= m_hmin) && (hEnd <= m_hmax) && (kEnd >= m_kmin) &&
       (kEnd <= m_kmax) && (lEnd >= m_lmin) && (lEnd <= m_lmax)) {
-    Mantid::Kernel::VMD v(hEnd, kEnd, lEnd, m_kiMax);
-    intersections.push_back(v);
+    intersections.push_back({hEnd, kEnd, lEnd, m_kiMax});
   }
 
   // sort intersections by momentum
   typedef std::vector<Mantid::Kernel::VMD>::iterator IterType;
-  std::stable_sort<IterType, bool (*)(const Mantid::Kernel::VMD &,
-                                      const Mantid::Kernel::VMD &)>(
-      intersections.begin(), intersections.end(), compareMomentum);
-
-  return intersections;
+  std::stable_sort(intersections.begin(), intersections.end(), compareMomentum);
 }
 
 } // namespace MDAlgorithms
