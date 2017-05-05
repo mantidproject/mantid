767e385c3aa849d700c98e43344709de9dc566be
Antti Soininen <soininen@ill.fr>
2017-05-05 13:01:58 +0200

    Add more unit tests.
    Re #19286

--- Framework/Algorithms/test/LineProfileTest.h
+++ Framework/Algorithms/test/LineProfileTest.h
@@ -31,7 +31,7 @@ public:
     TS_ASSERT(alg.isInitialized())
   }
 
-  void test_profile_of_single_horizontal_spectrum() {
+  void test_averaging_profile_of_single_horizontal_spectrum() {
     const size_t nHist = 13;
     const size_t nBins = 23;
     MatrixWorkspace_sptr inputWS = create2DWorkspace154(nHist, nBins);
@@ -77,6 +77,63 @@ public:
                      static_cast<double>(nHist) / 2 - 0.5)
     TS_ASSERT_EQUALS(vertAxis->getValue(1),
                      static_cast<double>(nHist) / 2 + 0.5)
+  }
+
+  void test_summing_profile() {
+    const size_t nHist = 13;
+    const size_t nBins = 23;
+    MatrixWorkspace_sptr inputWS = create2DWorkspace154(nHist, nBins);
+
+    const int start = 2;
+    const int end = nBins - 2;
+    Workspace2D_sptr outputWS = profileOverTwoSpectra(inputWS, start, end, "Sum");
+    TS_ASSERT(outputWS);
+    TS_ASSERT_EQUALS(outputWS->getNumberHistograms(), 1)
+    const auto hist = outputWS->histogram(0);
+    for (size_t i = 0; i < hist.x().size(); ++i) {
+      TS_ASSERT_EQUALS(hist.x()[i], i + start)
+    }
+    for (const auto y : hist.y()) {
+      TS_ASSERT_EQUALS(y, 2 * inputWS->y(0)[0])
+    }
+    const double realE = std::sqrt(2 * inputWS->e(0)[0] * inputWS->e(0)[0]);
+    for (const auto e : hist.e()) {
+      TS_ASSERT_EQUALS(e, realE )
+    }
+    const auto vertAxis = outputWS->getAxis(1);
+    TS_ASSERT_EQUALS(vertAxis->getValue(0),
+                     static_cast<double>(nHist) / 2 - 0.5)
+    TS_ASSERT_EQUALS(vertAxis->getValue(1),
+                     static_cast<double>(nHist) / 2 + 1.5)
+  }
+
+  void test_weighed_summing_profile() {
+    const size_t nHist = 13;
+    const size_t nBins = 23;
+    MatrixWorkspace_sptr inputWS = create2DWorkspace154(nHist, nBins);
+    for (size_t i = 0; i < nBins; ++i) {
+      inputWS->mutableY(nHist / 2)[i] = std::nan("");
+    }
+    const int start = 2;
+    const int end = nBins - 2;
+    Workspace2D_sptr outputWS = profileOverTwoSpectra(inputWS, start, end, "Weighed Sum");
+    TS_ASSERT(outputWS);
+    TS_ASSERT_EQUALS(outputWS->getNumberHistograms(), 1)
+    const auto hist = outputWS->histogram(0);
+    for (size_t i = 0; i < hist.x().size(); ++i) {
+      TS_ASSERT_EQUALS(hist.x()[i], i + start)
+    }
+    for (const auto y : hist.y()) {
+      TS_ASSERT_EQUALS(y, 2 * inputWS->y(0)[0])
+    }
+    for (const auto e : hist.e()) {
+      TS_ASSERT_EQUALS(e, 2 * inputWS->e(0)[0])
+    }
+    const auto vertAxis = outputWS->getAxis(1);
+    TS_ASSERT_EQUALS(vertAxis->getValue(0),
+                     static_cast<double>(nHist) / 2 - 0.5)
+    TS_ASSERT_EQUALS(vertAxis->getValue(1),
+                     static_cast<double>(nHist) / 2 + 1.5)
   }
 
   void test_horizontal_profile_linewidth_outside_workspace() {
@@ -289,6 +346,32 @@ public:
     TS_ASSERT_EQUALS(vertAxis->getValue(0), 3)
     TS_ASSERT_EQUALS(vertAxis->getValue(1), 5)
   }
+
+private:
+  Workspace2D_sptr profileOverTwoSpectra(MatrixWorkspace_sptr inputWS, const int start, const int end, const std::string& mode) {
+    LineProfile alg;
+    // Don't put output in ADS by default
+    alg.setChild(true);
+    alg.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("InputWorkspace", inputWS))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setPropertyValue("OutputWorkspace", "_unused_for_child"))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setProperty("Centre", static_cast<double>(inputWS->getNumberHistograms()) / 2))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("HalfWidth", 0.5))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setProperty("Start", static_cast<double>(start)))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("End", static_cast<double>(end)))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("Mode", mode))
+    TS_ASSERT_THROWS_NOTHING(alg.execute())
+    TS_ASSERT(alg.isExecuted())
+
+    Workspace2D_sptr outputWS = alg.getProperty("OutputWorkspace");
+    return outputWS;
+  }
+
 };
 
 #endif /* MANTID_ALGORITHMS_LINEPROFILETEST_H_ */
