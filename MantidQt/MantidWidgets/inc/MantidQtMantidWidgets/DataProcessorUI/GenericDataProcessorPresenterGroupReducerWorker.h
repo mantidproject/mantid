269af8ca2e5bb0d720af4ad0adb347e6312d1d4d
Pranav Bahuguna <pranav.bahuguna@stfc.ac.uk>
2017-05-08 09:56:23 +0100

    Re #19840 Split worker class into two separate worker classes

--- MantidQt/MantidWidgets/inc/MantidQtMantidWidgets/DataProcessorUI/GenericDataProcessorPresenterGroupReducerWorker.h
+++ MantidQt/MantidWidgets/inc/MantidQtMantidWidgets/DataProcessorUI/GenericDataProcessorPresenterGroupReducerWorker.h
@@ -1,5 +1,5 @@
-#ifndef MANTIDQTMANTIDWIDGETS_GENERICDATAPROCESSORPRESENTER_GENERICDATAPROCESSORPRESENTERREDUCERWORKER_H
-#define MANTIDQTMANTIDWIDGETS_GENERICDATAPROCESSORPRESENTER_GENERICDATAPROCESSORPRESENTERREDUCERWORKER_H
+#ifndef MANTIDQTMANTIDWIDGETS_GENERICDATAPROCESSORPRESENTER_GENERICDATAPROCESSORPRESENTERGROUPREDUCERWORKER_H
+#define MANTIDQTMANTIDWIDGETS_GENERICDATAPROCESSORPRESENTER_GENERICDATAPROCESSORPRESENTERGROUPREDUCERWORKER_H
 
 #include "MantidQtMantidWidgets/DataProcessorUI/GenericDataProcessorPresenter.h"
 
@@ -9,10 +9,10 @@ namespace MantidQt {
 namespace MantidWidgets {
 
 /**
-Worker to run the reduction process for each row for the
+Worker to run the reduction process for each group of the
 GenericDataProcessorPresenter for the GUI it is attached to. It has
-a finishedRow() and finishedGroup() signal, and it is expected to
-emit one of these when the hard/long-work methods finish.
+a finished() signal which is expected to be emittted when one of
+these when the hard/long-work methods finish.
 
 Copyright &copy; 2016 ISIS Rutherford Appleton Laboratory, NScD
 Oak Ridge National Laboratory & European Spallation Source
@@ -35,40 +35,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 File change history is stored at: <https://github.com/mantidproject/mantid>
 Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
-class GenericDataProcessorPresenterReducerWorker : public QObject {
+class GenericDataProcessorPresenterGroupReducerWorker : public QObject {
   Q_OBJECT
 
 public:
-  // Constructor for processing rows
-  GenericDataProcessorPresenterReducerWorker(
-      GenericDataProcessorPresenter *presenter, const RowData &rowData)
-      : m_presenter(presenter), m_rowData(rowData) {}
-
-  // Constructor for processing groups
-  GenericDataProcessorPresenterReducerWorker(
+  GenericDataProcessorPresenterGroupReducerWorker(
       GenericDataProcessorPresenter *presenter, const GroupData &groupData)
       : m_presenter(presenter), m_groupData(groupData) {}
 
 private slots:
-  void processRow() {
-    emit finishedRow();
-  }
-
-  void processGroup() {
-    emit finishedGroup();
-  }
+  void processGroup() { emit finished(); }
 
 signals:
-  void finishedRow();
-  void finishedGroup();
+  void finished();
 
 private:
   GenericDataProcessorPresenter *m_presenter;
-  const RowData m_rowData;
   const GroupData m_groupData;
 };
 
 } // namespace MantidWidgets
 } // namespace MantidQt
 
-#endif // MANTIDQTMANTIDWIDGETS_GENERICDATAPROCESSORPRESENTER_GENERICDATAPROCESSORPRESENTERREDUCERWORKER_H
+#endif // MANTIDQTMANTIDWIDGETS_GENERICDATAPROCESSORPRESENTER_GENERICDATAPROCESSORPRESENTERGROUPREDUCERWORKER_H
