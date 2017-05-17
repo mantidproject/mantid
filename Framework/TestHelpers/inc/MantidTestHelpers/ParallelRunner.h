a1f33fd23f1468555230378e08a2ad29b78ce980
Simon Heybrock <simon.heybrock@esss.se>
2017-05-17 15:06:43 +0200

    Re #19246. Missing boost include.

--- Framework/TestHelpers/inc/MantidTestHelpers/ParallelRunner.h
+++ Framework/TestHelpers/inc/MantidTestHelpers/ParallelRunner.h
@@ -4,7 +4,7 @@
 #include "MantidParallel/Communicator.h"
 #include "MantidParallel/DllConfig.h"
 
-#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 
 #include <functional>
 #include <thread>
