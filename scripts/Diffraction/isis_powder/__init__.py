412961c27d892d336104c62a1a268c0d1c086018
David Fairbrother <DavidFair@users.noreply.github.com>
2017-05-22 13:18:03 +0100

    Re #19509 Move noqa to line where it applies

--- scripts/Diffraction/isis_powder/__init__.py
+++ scripts/Diffraction/isis_powder/__init__.py
@@ -1,15 +1,13 @@
 from __future__ import (absolute_import, division, print_function)
 
 # Disable unused import warnings. The import is for user convenience
-# noqa: F401
-
 # Bring instruments into package namespace
-from .gem import Gem
-from .pearl import Pearl
-from .polaris import Polaris
+from .gem import Gem  # noqa: F401
+from .pearl import Pearl  # noqa: F401
+from .polaris import Polaris  # noqa: F401
 
 # Other useful classes
-from .routines.sample_details import SampleDetails
+from .routines.sample_details import SampleDetails  # noqa: F401
 
 # Prevent users using from import *
 __all__ = []
