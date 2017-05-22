4662647e941164cdbc7f6b21826f4a2b26844b1a
Wenduo Zhou <zhouw@ornl.gov>
2017-05-22 16:57:51 -0400

    Refs #19140. Fixed the part of code to write out splitter log.

--- Framework/Algorithms/src/FilterEvents.cpp
+++ Framework/Algorithms/src/FilterEvents.cpp
@@ -336,7 +336,7 @@ void FilterEvents::processAlgorithmProperties() {
   else
     m_useDBSpectrum = true;
 
-  // Get run start time
+  // Get run start time from property 'run_start'
   if (m_eventWS->run().hasProperty("run_start")) {
     Kernel::DateAndTime run_start_time(
         m_eventWS->run().getProperty("run_start")->value());
@@ -1593,6 +1593,7 @@ void FilterEvents::splitLog(EventWorkspace_sptr eventws, std::string logname,
   }
 }
 
+//----------------------------------------------------------------------------------------------
 /** Generate a vector of integer time series property for each splitter
  * corresponding to each target (in integer)
  * in each splitter-time-series-property, 1 stands for include and 0 stands for
@@ -1607,39 +1608,62 @@ void FilterEvents::generateSplitterTSP(
   split_tsp_vec.clear();
 
   // initialize m_maxTargetIndex + 1 time series properties in integer
-  DateAndTime split_time0 = m_vecSplitterTime[0];
   for (int itarget = 0; itarget <= m_maxTargetIndex; ++itarget) {
     Kernel::TimeSeriesProperty<int> *split_tsp =
         new Kernel::TimeSeriesProperty<int>("splitter");
     split_tsp_vec.push_back(split_tsp);
     // add initial value if the first splitter time is after the run start time
-    if (split_time0.totalNanoseconds() > m_runStartTime.totalNanoseconds())
-      split_tsp->addValue(Kernel::DateAndTime(m_runStartTime), 0);
+    split_tsp->addValue(Kernel::DateAndTime(m_runStartTime), 0);
   }
 
   // start to go through  m_vecSplitterTime (int64) and m_vecSplitterGroup add
   // each entry to corresponding splitter TSP
   for (size_t igrp = 0; igrp < m_vecSplitterGroup.size(); ++igrp) {
+    // get the target workspace's index and the starting
     int itarget = m_vecSplitterGroup[igrp];
+    // start time of the entry with value 1
     DateAndTime start_time(m_vecSplitterTime[igrp]);
-    // Note: Keep the full splitter information. No need to within range
-    // if (start_time <= m_runStartTime) {
-    //   // clear the initial value with check first
-    //   if (split_tsp_vec[itarget]->size() != 1) {
-    //     g_log.error() << "Splitter group " << igrp << ": "
-    //                   << "With start time " << start_time << "(i_target = "
-    //                   << itarget << ")"
-    //                   << " same as run start time " << m_runStartTime
-    //                   << ", the TSP must have only 1 entry from "
-    //                      "initialization.  But not it has "
-    //                   << split_tsp_vec[itarget]->size() << "entries\n";
-    //     throw std::runtime_error("Coding logic error");
-    //   }
-    //   split_tsp_vec[itarget]->clear();
-    // }
+    // identify by case
+    if (start_time < m_runStartTime) {
+      // too early, ignore
+      continue;
+    }
+
+    // get the current TSP
+    Kernel::TimeSeriesProperty<int> *curr_tsp = split_tsp_vec[itarget];
+
+    if (start_time == m_runStartTime) {
+      // just same as the run start time: there must be one and only 1 entry
+      // with value 0
+      if (curr_tsp->size() != 1) {
+        std::stringstream error;
+        error << "Splitter TSP for target workspace " << itarget
+              << " must have 1 and only 1 entry "
+              << "if there is a splitter right at its run start time.";
+        throw std::runtime_error(error.str());
+      }
+      // the first entry should have an entry with value 0
+      if (curr_tsp->firstValue() != 0) {
+        std::stringstream error;
+        error << "Splitter TSP for target workspace " << itarget
+              << " must have 1 and only 1 entry "
+              << "with value as 0 but not " << curr_tsp->firstValue()
+              << " if there is a splitter "
+              << "right at its run start time.";
+        throw std::runtime_error(error.str());
+      }
+      // replace the value with '1' by clearing the original one first
+      curr_tsp->clear();
+    }
+
+    // later than the run start time, then add a new entry
+    curr_tsp->addValue(start_time, 1);
+
+
+    // add run stop time as a new entry
     DateAndTime stop_time(m_vecSplitterTime[igrp + 1]);
-    split_tsp_vec[itarget]->addValue(start_time, 1);
-    split_tsp_vec[itarget]->addValue(stop_time, 0);
+    curr_tsp->addValue(stop_time, 0);
+
     // g_log.warning() << "Add " << "i_group " << igrp << " to i_target " <<
     // itarget << "\n";
   }
