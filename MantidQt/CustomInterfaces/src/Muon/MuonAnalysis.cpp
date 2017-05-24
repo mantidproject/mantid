43cddd0e4459e10670d04b3eac78a54291fb94d3
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-05-24 10:39:41 +0100

    Removed messages from dialog box

--- MantidQt/CustomInterfaces/src/Muon/MuonAnalysis.cpp
+++ MantidQt/CustomInterfaces/src/Muon/MuonAnalysis.cpp
@@ -318,18 +318,24 @@ void MuonAnalysis::initLayout() {
   // Manage User Directories
   connect(m_uiForm.manageDirectoriesBtn, SIGNAL(clicked()), this,
           SLOT(openDirectoryDialog()));
-  connect(this, SIGNAL(setChosenGroupSignal(QString &)), this,
-          SLOT(setChosenGroupSlot(QString &)));
-  connect(this, SIGNAL(setChosenPeriodSignal(QString &)), this,
-          SLOT(setChosenPeriodSlot(QString &)));
 }
 
-void MuonAnalysis::setChosenGroupSlot(QString &group) {
-  m_uiForm.fitBrowser->setChosenGroup(group);
+void MuonAnalysis::setChosenGroupAndPeriods(const QString &wsName) {
+	const auto wsParams =
+		MuonAnalysisHelper::parseWorkspaceName(wsName.toStdString());
+
+	const QString &groupToSet = QString::fromStdString(wsParams.itemName);
+	const QString &periodToSet = QString::fromStdString(wsParams.periods);
+	const auto &groups = m_dataSelector->getChosenGroups();
+	const auto &periods = m_dataSelector->getPeriodSelections();
+	if (!groups.contains(groupToSet)) {
+		m_uiForm.fitBrowser->setChosenGroup(groupToSet);
+	}
+	if (!periodToSet.isEmpty() && !periods.contains(periodToSet)) {
+		m_uiForm.fitBrowser->setChosenPeriods(periodToSet);
+	}
 }
-void MuonAnalysis::setChosenPeriodSlot(QString &period) {
-  m_uiForm.fitBrowser->setChosenPeriods(period);
-}
+
 
 /**
 * Muon Analysis help (slot)
@@ -1480,6 +1486,7 @@ void MuonAnalysis::updateFront() {
 
 /**
  * Update front including first re-populate pair list combo box
+ * Also update multiple fitting
  */
 void MuonAnalysis::updateFrontAndCombo() {
   // for now brute force clearing and adding new context
@@ -1493,13 +1500,19 @@ void MuonAnalysis::updateFrontAndCombo() {
 
   int numG = numGroups();
   int numP = numPairs();
-  for (int i = 0; i < numG; i++)
-    m_uiForm.frontGroupGroupPairComboBox->addItem(
-        m_uiForm.groupTable->item(m_groupToRow[i], 0)->text());
-  for (int i = 0; i < numP; i++)
-    m_uiForm.frontGroupGroupPairComboBox->addItem(
-        m_uiForm.pairTable->item(m_pairToRow[i], 0)->text());
+  QStringList groupsAndPairs; 
+  for (int i = 0; i < numG; i++) {
+	  m_uiForm.frontGroupGroupPairComboBox->addItem(
+		  m_uiForm.groupTable->item(m_groupToRow[i], 0)->text());
+	  groupsAndPairs << m_uiForm.groupTable->item(m_groupToRow[i], 0)->text();
+  }
+  for (int i = 0; i < numP; i++) {
+	  m_uiForm.frontGroupGroupPairComboBox->addItem(
+		  m_uiForm.pairTable->item(m_pairToRow[i], 0)->text());
+	  groupsAndPairs << m_uiForm.groupTable->item(m_pairToRow[i], 0)->text();
 
+  }
+  m_uiForm.fitBrowser->setAvailableGroups(groupsAndPairs);
   // If it doesn't match then reset
   if (currentI >= m_uiForm.frontGroupGroupPairComboBox->count()) {
     currentI = 0;
@@ -1862,6 +1875,7 @@ void MuonAnalysis::selectMultiPeak(const QString &wsName,
 
     // Set the selected run, group/pair and period
     m_fitDataPresenter->setAssignedFirstRun(wsName, filePath);
+	setChosenGroupAndPeriods(wsName);
   }
 
   QString code;
@@ -2131,10 +2145,6 @@ void MuonAnalysis::loadFittings() {
       Mantid::Kernel::make_unique<MuonAnalysisFitFunctionPresenter>(
           nullptr, m_uiForm.fitBrowser, m_functionBrowser);
   // Connect signals
-  connect(m_dataSelector, SIGNAL(selectedGroupsChanged()), this,
-          SLOT(dataToFitChanged()));
-  connect(m_dataSelector, SIGNAL(selectedPeriodsChanged()), this,
-          SLOT(dataToFitChanged()));
   connect(m_dataSelector, SIGNAL(workspaceChanged()), this,
           SLOT(dataToFitChanged()));
   connect(m_uiForm.plotCreation, SIGNAL(currentIndexChanged(int)), this,
@@ -2502,6 +2512,7 @@ void MuonAnalysis::changeTab(int newTabIndex) {
       const boost::optional<QString> filePath =
           m_uiForm.mwRunFiles->getUserInput().toString();
       m_fitDataPresenter->setSelectedWorkspace(m_currentDataName, filePath);
+	  setChosenGroupAndPeriods(m_currentDataName);
       selectMultiPeak(m_currentDataName, filePath);
     }
 
