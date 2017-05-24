882b3946bf64cc3248820604cfa32bc0dc3be42b
Mayer Alexandra <mayer.ali@t-online.de>
2017-05-24 09:07:08 +0200

    Refs #19517 add future improt

--- Framework/PythonInterface/plugins/algorithms/SaveYDA.py
+++ Framework/PythonInterface/plugins/algorithms/SaveYDA.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
 from mantid.api import PythonAlgorithm, AlgorithmFactory, MatrixWorkspaceProperty, WorkspaceUnitValidator, \
                        InstrumentValidator, FileProperty, FileAction
 from mantid.kernel import Direction, CompositeValidator
