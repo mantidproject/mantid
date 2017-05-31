06e73bc2ec883669347e0a21b256ba0d4c1de052
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-05-31 11:33:22 +0100

    Added md5 file for doc test

--- Framework/Algorithms/test/PlotAsymmetryByLogValueTest.h
+++ Framework/Algorithms/test/PlotAsymmetryByLogValueTest.h
@@ -538,7 +538,7 @@ public:
     //TemporaryRenamer renamedFile(middleFile);
 
     alg.setPropertyValue("FirstRun", "MUSR00015193.nxs");
-    alg.setPropertyValue("LastRun", "MUSR00015195 .nxs");
+    alg.setPropertyValue("LastRun", "MUSR00015195.nxs");
     alg.setPropertyValue("OutputWorkspace", "PlotAsymmetryByLogValueTest_WS");
     alg.setPropertyValue("LogValue", "run_number");
     alg.setPropertyValue("Red", "2");
