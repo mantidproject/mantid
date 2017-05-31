5bd083746894a08f7e6e948b91f8135d20d6d92c
Garrett Granroth <granrothge@ornl.gov>
2017-05-31 16:27:51 -0400

    Re #19747 replace file with open

--- Testing/SystemTests/tests/analysis/DirectInelasticDiagnostic.py
+++ Testing/SystemTests/tests/analysis/DirectInelasticDiagnostic.py
@@ -48,7 +48,7 @@ class DirectInelasticDiagnostic(MantidStressTest):
 
         # Save the masked spectra nmubers to a simple ASCII file for comparison
         self.saved_diag_file = os.path.join(ms.config['defaultsave.directory'], 'CurrentDirectInelasticDiag.txt')
-        handle = file(self.saved_diag_file, 'w')
+        handle = open(self.saved_diag_file, 'w')
         spectrumInfo = sample_ws.spectrumInfo()
         for index in range(sample_ws.getNumberHistograms()):
             if spectrumInfo.isMasked(index):
