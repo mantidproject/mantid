07c9464b6122ef8be81249d0bd0e4605d164b932
Karl Palmen <karl.palmen@stfc.ac.uk>
2017-06-05 11:37:08 +0100

    Add new wrapper class for functions re #19010
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

---
+++ Framework/PythonInterface/mantid/FunctionWrapper.py
@@ -0,0 +1,23 @@
+class FunctionWrapper:
+# Wrapper class for Fitting Function 
+  def __init__ (self, name, **kwargs):
+    self.fun = FunctionFactory.createFunction(name)
+    
+    # Deal with attributes first
+#    for key in kwargs:
+#       if(self.fun.hasAttribute(key)):
+#           self.fun.storeAttributeValue(key, kwargs[key])
+            
+    # Then deal with parameters        
+    for key in kwargs:
+       #print key, "corresponds to", kwargs[key]
+       self.fun.setParameter(key, kwargs[key])
+       
+  def __getitem__ (self, name):
+      return self.fun.getParameterValue(name)
+      
+  def __setitem__ (self, name, value):
+      self.fun.setParameter(name, value)
+        
+  def getFunction(self):
+      return self.fun
