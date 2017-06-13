8ed6ac55565172ba0d982719bab6fb56625e6f21
Antti Soininen <soininen@ill.fr>
2017-06-13 12:36:13 +0200

    Code beautification.
    Re #19757

--- Framework/Algorithms/src/SampleCorrections/DetectorGridDefinition.cpp
+++ Framework/Algorithms/src/SampleCorrections/DetectorGridDefinition.cpp
@@ -34,10 +34,10 @@ DetectorGridDefinition::DetectorGridDefinition(const double minLatitude,
     m_minLongitude -= smallShift;
     m_maxLongitude += smallShift;
   }
-  m_latitudeStep =
-      (m_maxLatitude - m_minLatitude) / static_cast<double>(m_latitudePoints - 1);
-  m_longitudeStep =
-      (m_maxLongitude - m_minLongitude) / static_cast<double>(m_longitudePoints - 1);
+  m_latitudeStep = (m_maxLatitude - m_minLatitude) /
+                   static_cast<double>(m_latitudePoints - 1);
+  m_longitudeStep = (m_maxLongitude - m_minLongitude) /
+                    static_cast<double>(m_longitudePoints - 1);
 }
 
 /** Return the latitude of the given row.
