19aaf351a73d8a668fb593b8e65b2f8a44a0ccc5
Samuel Jackson <samueljackson@outlook.com>
2017-06-15 10:38:45 +0100

    Add unit cell documentation

--- Framework/PythonInterface/mantid/geometry/src/Exports/UnitCell.cpp
+++ Framework/PythonInterface/mantid/geometry/src/Exports/UnitCell.cpp
@@ -78,131 +78,239 @@ void export_UnitCell() {
           (arg("self"), arg("_a"), arg("_b"), arg("_c"), arg("_alpha"),
            arg("_beta"), arg("_gamma"),
            arg("Unit") = static_cast<int>(angDegrees))))
-      .def("a", (double (UnitCell::*)() const) & UnitCell::a, arg("self"))
-      .def("a1", (double (UnitCell::*)() const) & UnitCell::a1, arg("self"))
-      .def("a2", (double (UnitCell::*)() const) & UnitCell::a2, arg("self"))
-      .def("a3", (double (UnitCell::*)() const) & UnitCell::a3, arg("self"))
+      .def("a", (double (UnitCell::*)() const) & UnitCell::a, arg("self"),
+           "Returns the length of the ``a`` direction of the unit cell in "
+           "Angstroms.")
+      .def("a1", (double (UnitCell::*)() const) & UnitCell::a1, arg("self"),
+           "Returns the length of the ``a`` direction of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.a`")
+      .def("a2", (double (UnitCell::*)() const) & UnitCell::a2, arg("self"),
+           "Returns the length of the ``b`` direction of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.b`")
+      .def("a3", (double (UnitCell::*)() const) & UnitCell::a3, arg("self"),
+           "Returns the length of the ``c`` direction of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.c`")
       .def("alpha", (double (UnitCell::*)() const) & UnitCell::alpha,
-           arg("self"))
+           arg("self"),
+           "Returns the ``alpha`` angle for this unit cell. The angle will "
+           "either be in degrees or radians depending on the ``Unit`` "
+           "parameter passed to the constructor.")
       .def("alpha1", (double (UnitCell::*)() const) & UnitCell::alpha1,
-           arg("self"))
+           arg("self"),
+           "Returns the ``alpha`` angle of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.alpha`")
       .def("alpha2", (double (UnitCell::*)() const) & UnitCell::alpha2,
-           arg("self"))
+           arg("self"),
+           "Returns the ``beta`` angle of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.beta`")
       .def("alpha3", (double (UnitCell::*)() const) & UnitCell::alpha3,
-           arg("self"))
+           arg("self"),
+           "Returns the ``gamma`` angle of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.gamma`")
       .def("alphastar", (double (UnitCell::*)() const) & UnitCell::alphastar,
-           arg("self"))
+           arg("self"),
+           "Returns the reciprocal ``alpha`` angle for this unit cell. The "
+           "angle "
+           "will either be in degrees or radians depending on the ``Unit`` "
+           "parameter passed to the constructor.")
       .def("astar", (double (UnitCell::*)() const) & UnitCell::astar,
-           arg("self"))
-      .def("b", (double (UnitCell::*)() const) & UnitCell::b, arg("self"))
-      .def("b1", (double (UnitCell::*)() const) & UnitCell::b1, arg("self"))
-      .def("b2", (double (UnitCell::*)() const) & UnitCell::b2, arg("self"))
-      .def("b3", (double (UnitCell::*)() const) & UnitCell::b3, arg("self"))
-      .def("beta", (double (UnitCell::*)() const) & UnitCell::beta, arg("self"))
+           arg("self"),
+           "Returns the length of the reciprocal ``a`` direction for this "
+           "unit cell in reciprocal Angstroms.")
+      .def("b", (double (UnitCell::*)() const) & UnitCell::b, arg("self"),
+           "Returns the length of the ``b`` direction of the unit cell in "
+           "Angstroms.")
+      .def("b1", (double (UnitCell::*)() const) & UnitCell::b1, arg("self"),
+           "Returns the length of the reciprocal ``a`` direction of the unit "
+           "cell. This is an alias for :func:`~mantid.geometry.UnitCell.astar`")
+      .def("b2", (double (UnitCell::*)() const) & UnitCell::b2, arg("self"),
+           "Returns the length of the reciprocal ``b`` direction of the unit "
+           "cell. This is an alias for :func:`~mantid.geometry.UnitCell.bstar`")
+      .def("b3", (double (UnitCell::*)() const) & UnitCell::b3, arg("self"),
+           "Returns the length of the reciprocal ``c`` direction of the unit "
+           "cell. This is an alias for :func:`~mantid.geometry.UnitCell.cstar`")
+      .def("beta", (double (UnitCell::*)() const) & UnitCell::beta, arg("self"),
+           "Returns the ``beta`` angle for this unit cell. The angle will "
+           "either be in degrees or radians depending on the ``Unit`` "
+           "parameter passed to the constructor.")
       .def("beta1", (double (UnitCell::*)() const) & UnitCell::beta1,
-           arg("self"))
+           arg("self"),
+           "Returns the reciprocal ``alpha`` angle of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.alphastar`")
       .def("beta2", (double (UnitCell::*)() const) & UnitCell::beta2,
-           arg("self"))
+           arg("self"),
+           "Returns the reciprocal ``beta`` angle of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.betastar`")
       .def("beta3", (double (UnitCell::*)() const) & UnitCell::beta3,
-           arg("self"))
-      .def("betastar", (double (UnitCell::*)() const) & UnitCell::betastar,
-           arg("self"))
+           arg("self"),
+           "Returns the reciprocal ``gamma`` angle of the unit cell. "
+           "This is an alias for :func:`~mantid.geometry.UnitCell.gammastar`")
+      .def(
+          "betastar", (double (UnitCell::*)() const) & UnitCell::betastar,
+          arg("self"),
+          "Returns the reciprocal ``beta`` angle for this unit cell. The angle "
+          "will either be in degrees or radians depending on the ``Unit`` "
+          "parameter passed to the constructor.")
       .def("bstar", (double (UnitCell::*)() const) & UnitCell::bstar,
-           arg("self"))
-      .def("c", (double (UnitCell::*)() const) & UnitCell::c, arg("self"))
+           arg("self"),
+           "Returns the length of the reciprocal ``b`` direction for this "
+           "unit cell in reciprocal Angstroms.")
+      .def("c", (double (UnitCell::*)() const) & UnitCell::c, arg("self"),
+           "Returns the length of the ``c`` direction of the unit cell in "
+           "Angstroms.")
       .def("cstar", (double (UnitCell::*)() const) & UnitCell::cstar,
-           arg("self"))
+           arg("self"),
+           "Returns the length of the reciprocal ``c`` direction for this "
+           "unit cell in reciprocal Angstroms.")
       .def("d",
            (double (UnitCell::*)(double, double, double) const) & UnitCell::d,
-           (arg("self"), arg("h"), arg("k"), arg("l")))
+           (arg("self"), arg("h"), arg("k"), arg("l")),
+           "Returns d-spacing for a given H, K, L coordinate in Angstroms.")
       .def("d", (double (UnitCell::*)(const V3D &) const) & UnitCell::d,
-           (arg("self"), arg("hkl")))
-      .def("dstar", (double (UnitCell::*)(double, double, double) const) &
-                        UnitCell::dstar,
-           (arg("self"), arg("h"), arg("k"), arg("l")))
+           (arg("self"), arg("hkl")),
+           "Returns d-spacing for a given H, K, L coordinate in Angstroms.")
+      .def("dstar",
+           (double (UnitCell::*)(double, double, double) const) &
+               UnitCell::dstar,
+           (arg("self"), arg("h"), arg("k"), arg("l")),
+           "Returns reciprocal d-spacing for a given H, K, L coordinate in "
+           "reciprocal Angstroms.")
       .def("errora", (double (UnitCell::*)() const) & UnitCell::errora,
-           arg("self"))
+           arg("self"), "Returns the error in the ``a`` unit cell length.")
       .def("errorb", (double (UnitCell::*)() const) & UnitCell::errorb,
-           arg("self"))
+           arg("self"), "Returns the error in the ``b`` unit cell length.")
       .def("errorc", (double (UnitCell::*)() const) & UnitCell::errorc,
-           arg("self"))
+           arg("self"), "Returns the error in the ``c`` unit cell length.")
       .def("erroralpha",
            (double (UnitCell::*)(int const) const) & UnitCell::erroralpha,
-           (arg("self"), arg("Unit") = static_cast<int>(angDegrees)))
+           (arg("self"), arg("Unit") = static_cast<int>(angDegrees)),
+           "Returns the error in the ``alpha`` angle of the unit cell.")
       .def("errorbeta",
            (double (UnitCell::*)(int const) const) & UnitCell::errorbeta,
-           (arg("self"), arg("Unit") = static_cast<int>(angDegrees)))
+           (arg("self"), arg("Unit") = static_cast<int>(angDegrees)),
+           "Returns the error in ``beta`` angle of the unit cell.")
       .def("errorgamma",
            (double (UnitCell::*)(int const) const) & UnitCell::errorgamma,
-           (arg("self"), arg("Unit") = static_cast<int>(angDegrees)))
+           (arg("self"), arg("Unit") = static_cast<int>(angDegrees)),
+           "Returns the error in ``gamma`` angle of the unit cell.")
       .def("gamma", (double (UnitCell::*)() const) & UnitCell::gamma,
-           arg("self"))
+           arg("self"),
+           "Returns the ``gamma`` angle for this unit cell. The angle will "
+           "either be in degrees or radians depending on the ``Unit`` "
+           "parameter passed to the constructor.")
       .def("gammastar", (double (UnitCell::*)() const) & UnitCell::gammastar,
-           arg("self"))
-      .def("recAngle", (double (UnitCell::*)(double, double, double, double,
-                                             double, double, int const) const) &
-                           UnitCell::recAngle,
+           arg("self"),
+           "Returns the reciprocal ``gamma`` angle for this unit cell. The "
+           "angle "
+           "will either be in degrees or radians depending on the ``Unit`` "
+           "parameter passed to the constructor.")
+      .def("recAngle",
+           (double (UnitCell::*)(double, double, double, double, double, double,
+                                 int const) const) &
+               UnitCell::recAngle,
            (arg("self"), arg("h1"), arg("k1"), arg("l1"), arg("h2"), arg("k2"),
-            arg("l2"), arg("Unit") = static_cast<int>(angDegrees)))
+            arg("l2"), arg("Unit") = static_cast<int>(angDegrees)),
+           "Returns the angle between two HKL reflections in reciprocal space.")
       .def("recVolume", (double (UnitCell::*)() const) & UnitCell::recVolume,
-           arg("self"))
-      .def("set", (void (UnitCell::*)(double, double, double, double, double,
-                                      double, int const)) &
-                      UnitCell::set,
+           arg("self"),
+           "Returns the volume of the reciprocal cell defined by this unit "
+           "cell.")
+      .def("set",
+           (void (UnitCell::*)(double, double, double, double, double, double,
+                               int const)) &
+               UnitCell::set,
            (arg("self"), arg("_a"), arg("_b"), arg("_c"), arg("_alpha"),
             arg("_beta"), arg("_gamma"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the parameters of the unit cell. Angles can be set in either"
+           "degrees or radians using the ``Unit`` parameter (0 = degrees, "
+           "1 = radians)")
       .def("seta", (void (UnitCell::*)(double))(&UnitCell::seta),
-           (arg("self"), arg("_a")))
+           (arg("self"), arg("_a")),
+           "Set the length of the ``a`` direction of the unit cell.")
       .def("setalpha",
            (void (UnitCell::*)(double, int const))(&UnitCell::setalpha),
            (arg("self"), arg("_alpha"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the ``alpha`` angle of the unit cell. The angle can be set "
+           "either in degrees or radians using the ``Unit`` parameter.")
       .def("setb", (void (UnitCell::*)(double))(&UnitCell::setb),
-           (arg("self"), arg("_b")))
+           (arg("self"), arg("_b")),
+           "Set the length of the ``b`` direction of the unit cell.")
       .def("setbeta",
            (void (UnitCell::*)(double, int const))(&UnitCell::setbeta),
            (arg("self"), arg("_beta"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the ``beta`` angle of the unit cell. The angle can be set "
+           "either in degrees or radians using the ``Unit`` parameter.")
       .def("setc", (void (UnitCell::*)(double))(&UnitCell::setc),
-           (arg("self"), arg("_c")))
+           (arg("self"), arg("_c")),
+           "Set the length of the ``c`` direction of the unit cell.")
       .def("setgamma",
            (void (UnitCell::*)(double, int const))(&UnitCell::setgamma),
            (arg("self"), arg("_gamma"),
-            arg("Unit") = static_cast<int>(angDegrees)))
-      .def("setError", (void (UnitCell::*)(double, double, double, double,
-                                           double, double, int const)) &
-                           UnitCell::setError,
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the ``gamma`` angle of the unit cell. The angle can be set "
+           "either in degrees or radians using the ``Unit`` parameter.")
+      .def("setError",
+           (void (UnitCell::*)(double, double, double, double, double, double,
+                               int const)) &
+               UnitCell::setError,
            (arg("self"), arg("_aerr"), arg("_berr"), arg("_cerr"),
             arg("_alphaerr"), arg("_betaerr"), arg("_gammaerr"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the errors in the unit cell parameters.")
       .def("setErrora", (void (UnitCell::*)(double))(&UnitCell::setErrora),
-           (arg("self"), arg("_aerr")))
+           (arg("self"), arg("_aerr")),
+           "Set the error in the length of the ``a`` direction of the unit "
+           "cell.")
       .def("setErroralpha",
            (void (UnitCell::*)(double, int const))(&UnitCell::setErroralpha),
            (arg("self"), arg("_alphaerr"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the error in the ``alpha`` angle of the unit cell.")
       .def("setErrorb", (void (UnitCell::*)(double))(&UnitCell::setErrorb),
-           (arg("self"), arg("_berr")))
+           (arg("self"), arg("_berr")),
+           "Set the error in the length of the ``b`` direction of the unit "
+           "cell.")
       .def("setErrorbeta",
            (void (UnitCell::*)(double, int const))(&UnitCell::setErrorbeta),
            (arg("self"), arg("_betaerr"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the error in the ``beta`` angle of the unit cell using the "
+           "``Unit`` parameter.")
       .def("setErrorc", (void (UnitCell::*)(double))(&UnitCell::setErrorc),
-           (arg("self"), arg("_cerr")))
+           (arg("self"), arg("_cerr")),
+           "Set the error in the length of the ``c`` direction of the unit "
+           "cell.")
       .def("setErrorgamma",
            (void (UnitCell::*)(double, int const))(&UnitCell::setErrorgamma),
            (arg("self"), arg("_gammaerr"),
-            arg("Unit") = static_cast<int>(angDegrees)))
+            arg("Unit") = static_cast<int>(angDegrees)),
+           "Set the error in the ``gamma`` angle of the unit cell using the "
+           "``Unit`` parameter.")
       .def("volume", (double (UnitCell::*)() const) & UnitCell::volume,
-           arg("self"))
-      .def("getG", &UnitCell::getG, arg("self"), return_readonly_numpy())
+           arg("self"), "Returns the volume of the unit cell.")
+      .def("getG", &UnitCell::getG, arg("self"), return_readonly_numpy(),
+           "Returns the metric tensor for the unit cell. This will return a "
+           ":class:`numpy.ndarray` with shape ``(3,3)``.")
       .def("getGstar", &UnitCell::getGstar, arg("self"),
-           return_readonly_numpy())
-      .def("getB", &UnitCell::getB, arg("self"), return_readonly_numpy())
-      .def("getBinv", &UnitCell::getBinv, arg("self"), return_readonly_numpy())
+           return_readonly_numpy(),
+           "Returns the metric tensor for the reciprocal unit cell. This will "
+           "return a :class:`numpy.ndarray` with shape ``(3,3)``.")
+      .def("getB", &UnitCell::getB, arg("self"), return_readonly_numpy(),
+           "Returns the B matrix for this unit cell. This will be in a "
+           "right-handed coordinate system and using the Busing-Levy "
+           "convention. This will return a :class:`numpy.ndarray` with shape "
+           "``(3,3)``.")
+      .def("getBinv", &UnitCell::getBinv, arg("self"), return_readonly_numpy(),
+           "Returns the inverse of the B matrix for this unit cell."
+           "This will return a :class:`numpy.ndarray` with shape ``(3,3)``. "
+           "See also :func:`~mantid.geometry.UnitCell.getB`.")
       .def("recalculateFromGstar", &recalculateFromGstar,
-           (arg("self"), arg("NewGstar")))
+           (arg("self"), arg("NewGstar")),
+           "Recalculate the unit cell parameters from a metric tensor. This "
+           "method accepts a :class:`numpy.ndarray` with shape ``(3,3)``.")
       .def("__str__", &__str__implementation)
       .def("__repr__", &__repr__implementation);
 
