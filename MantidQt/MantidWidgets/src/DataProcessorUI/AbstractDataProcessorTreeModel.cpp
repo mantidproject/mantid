e9411b53ae68d0a3b252cd8c09a69c2c65ceec3f
Pranav Bahuguna <pranav.bahuguna@stfc.ac.uk>
2017-06-27 16:47:07 +0100

    Re #19930 Created abstract class for tree models to inherit from

---
+++ MantidQt/MantidWidgets/src/DataProcessorUI/AbstractDataProcessorTreeModel.cpp
@@ -0,0 +1,46 @@
+#include "MantidQtMantidWidgets/DataProcessorUI/AbstractDataProcessorTreeModel.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/TableRow.h"
+
+namespace MantidQt {
+namespace MantidWidgets {
+using namespace Mantid::API;
+
+//----------------------------------------------------------------------------------------------
+/** Constructor
+* @param tableWorkspace : The table workspace to wrap
+* @param whitelist : A DataProcessorWhiteList containing the columns
+*/
+AbstractDataProcessorTreeModel::AbstractDataProcessorTreeModel(
+    ITableWorkspace_sptr tableWorkspace,
+    const DataProcessorWhiteList &whitelist)
+    : m_tWS(tableWorkspace), m_whitelist(whitelist){};
+
+AbstractDataProcessorTreeModel::~AbstractDataProcessorTreeModel() {}
+
+/** Returns the number of columns, i.e. elements in the whitelist
+* @return : The number of columns
+*/
+int AbstractDataProcessorTreeModel::columnCount(
+    const QModelIndex & /* parent */) const {
+  return static_cast<int>(m_whitelist.size());
+}
+
+Qt::ItemFlags
+AbstractDataProcessorTreeModel::flags(const QModelIndex &index) const {
+  if (!index.isValid())
+    return 0;
+
+  return Qt::ItemIsEditable | QAbstractItemModel::flags(index);
+}
+
+/** Returns the currently highlighted row / group
+* @return : The highlighted row / group as a pair of group and row indexes
+*/
+std::pair<int, int> AbstractDataProcessorTreeModel::getHighlighted() const {
+
+  return m_highlighted;
+}
+
+} // namespace MantidWidgets
+} // namespace Mantid 
