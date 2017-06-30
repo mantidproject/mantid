cb84b7ddc719fae6e7ecdee87eb823aa712a9dc9
Owen Arnold <owen.arnold@stfc.ac.uk>
2017-06-30 12:21:32 +0100

    refs #19278. Fix source error

--- Framework/Geometry/src/Instrument/ComponentInfo.cpp
+++ Framework/Geometry/src/Instrument/ComponentInfo.cpp
@@ -97,7 +97,7 @@ size_t ComponentInfo::sample() const { return m_componentInfo.sample(); }
 
 double ComponentInfo::l1() const { return m_componentInfo.l1(); }
 
-size_t ComponentInfo::root() { returm m_componentInfo.root(); }
+size_t ComponentInfo::root() { return m_componentInfo.root(); }
 
 void ComponentInfo::setPosition(const size_t componentIndex,
                                 const Kernel::V3D &newPosition) {
