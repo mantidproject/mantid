865d7222bb5c126abce43e5514eb116453388d3c
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-07-11 16:22:25 +0100

    clang format

--- Framework/CurveFitting/test/Algorithms/FitTest.h
+++ Framework/CurveFitting/test/Algorithms/FitTest.h
@@ -320,9 +320,9 @@ public:
       e[i] = 1.0;
     }
     const double sqrh = 0.70710678; // cos( 45 degrees )
-    y = {1.e-4 * sqrh,  0.00,  -1.2e-2 * sqrh,  -5.6e-2, -18.2e-2 * sqrh,  0.0,
-         0.8008 * sqrh, 1.144, 1.287 * sqrh, 0.0,  -0.8008 * sqrh, -0.4368,
-         -0.182 * sqrh, 0.0,   1.2e-2 * sqrh,   0.16e-2, 1.e-4 * sqrh,   0.00};
+    y = {1.e-4 * sqrh, 0.00, -1.2e-2 * sqrh, -5.6e-2, -18.2e-2 * sqrh, 0.0,
+         0.8008 * sqrh, 1.144, 1.287 * sqrh, 0.0, -0.8008 * sqrh, -0.4368,
+         -0.182 * sqrh, 0.0, 1.2e-2 * sqrh, 0.16e-2, 1.e-4 * sqrh, 0.00};
 
     Fit fit;
     fit.initialize();
