6d4a9baa89723c5f80e2ac5a97d9a95ad992e19e
Louise McCann <louise.mccann@stfc.ac.uk>
2017-07-13 12:19:12 +0100

    add basic imports, categories etc
    Refs #19907

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/NewMCAbsorption.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/NewMCAbsorption.py
@@ -1,2 +1,17 @@
 from __future__ import (absolute_import, division, print_function)
 
+from mantid.simpleapi import *
+from mantid.api import *
+from mantid.kernel import *
+
+class NewMCAbsorption(DataProcessorAlgorithm):
+
+    def category(self):
+        return "Workflow\\Inelastic;CorrectionFunctions\\AbsorptionCorrections;Workflow\\MIDAS"
+
+    def summary(self):
+        return "Calculates absorption corrections for a given sample shape."
+
+
+# Register algorithm with Mantid
+AlgorithmFactory.subscribe(NewMCAbsorption)
