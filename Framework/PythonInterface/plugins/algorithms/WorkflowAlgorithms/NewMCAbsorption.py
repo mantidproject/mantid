2f12142ad9ebcebc749ccb6ef178672b4f68b9ad
Louise McCann <louise.mccann@stfc.ac.uk>
2017-07-13 12:29:58 +0100

    initialize variables
    Refs #19907

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/NewMCAbsorption.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/NewMCAbsorption.py
@@ -6,6 +6,35 @@ from mantid.kernel import *
 
 class NewMCAbsorption(DataProcessorAlgorithm):
 
+    # basic sample variables
+    _input_ws_name = None
+    _sample_chemical_formula = None
+    _sample_density_type = None
+    _sample_density = None
+    _sample_height = None
+
+    # general variables
+    _events = None
+    _interpolation = None
+    _output_ws = None
+
+    # beam variables
+    _beam_height = None
+    _beam_width = None
+
+    # annulus variables
+    _sample_inner_radius = None
+    _sample_outer_radius = None
+
+    # cylinder variables
+    _sample_radius = None
+
+    # flat plate variables
+    _sample_width = None
+    _sample_thickness = None
+    _sample_angle = None
+
+
     def category(self):
         return "Workflow\\Inelastic;CorrectionFunctions\\AbsorptionCorrections;Workflow\\MIDAS"
 
