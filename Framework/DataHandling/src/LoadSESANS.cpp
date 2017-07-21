a11e31e3b909b8da31f48271de304c08d69dcb10
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-07-21 09:57:13 +0100

    Re #19943 Added comments to LoadSESANS

--- Framework/DataHandling/src/LoadSESANS.cpp
+++ Framework/DataHandling/src/LoadSESANS.cpp
@@ -11,28 +11,40 @@
 namespace Mantid {
 namespace DataHandling {
 
+// Register with the AlgorithmFactory
 DECLARE_FILELOADER_ALGORITHM(LoadSESANS)
 
+/// Get algorithm's name
 const std::string LoadSESANS::name() const {
 	return "LoadSESANS";
 }
 
+/// Get summary of algorithm
 const std::string LoadSESANS::summary() const {
 	return "Load a file using the SESANS format";
 }
 
+/// Get algorithm's version number
 int LoadSESANS::version() const {
 	return 1;
 }
 
+/// Get algorithm's category
 const std::string LoadSESANS::category() const {
 	return "DataHandling\\Text";
 }
 
+/** Get the confidence that this algorithm can load a file
+ * @param descriptor A descriptor for the file
+ * @return The confidence level (0 to 100) where 0 indicates this file will not be used
+ */
 int LoadSESANS::confidence(Kernel::FileDescriptor &descriptor) const {
 	return 50; //ARBITRARY - CHANGE THIS
 }
 
+/**
+ * Initialise the algorithm
+ */
 void LoadSESANS::init() {
 	declareProperty(Kernel::make_unique<API::FileProperty>(
 		"Filename", "", API::FileProperty::Load, fileExtensions),
@@ -42,6 +54,9 @@ void LoadSESANS::init() {
 		"The name to use for the output workspace");
 }
 
+/**
+ * Execute the algorithm
+ */
 void LoadSESANS::exec() {
 	std::string filename = getPropertyValue("Filename");
 	std::ifstream infile(filename);
@@ -74,6 +89,13 @@ void LoadSESANS::exec() {
 	infile.close();
 }
 
+/**
+ * Split a header into a key-value pair delimited by whitespace, where the first token is the key and the remainder the value
+ * @param line A string containing the line to split
+ * @param lineNum The number of the line in the file, used for error messaging
+ * @return A key-value pair
+ * @throw runtime_error If the line contains less than two tokens
+*/
 std::pair<std::string, std::string> LoadSESANS::splitHeader(const std::string &line, const int &lineNum) {
 	std::pair<std::string, std::string> attribute;
 	auto i = line.begin();
@@ -99,15 +121,28 @@ std::pair<std::string, std::string> LoadSESANS::splitHeader(const std::string &l
 	return attribute;
 }
 
+/** Is a character whitespace (here considered space or tab)?
+ * @param c The character
+ * @return Whether it is whitespace
+ */
 bool LoadSESANS::space(const char &c) {
 	return c == ' ' || c == '\t';
 }
 
+/** Is a character not whitespace (here considered space or tab)?
+* @param c The character
+* @return Whether it is not whitespace
+*/
 bool LoadSESANS::notSpace(const char &c) {
 	return !space(c);
 }
 
-
+/** Helper function to throw an error relating to the format of the file
+ * @param line The line where a format error was found
+ * @param message A message detailing the reason for the error
+ * @param lineNum The number of the line
+ * @throw runtime_error Under all circumstances
+*/
 void LoadSESANS::throwFormatError(const std::string &line, const std::string &message, const int &lineNum) {
 	std::string output = "Badly formed line at line " + std::to_string(lineNum) + "\n\n" +
 		"\t" + line + "\n\n" + message;
@@ -115,6 +150,12 @@ void LoadSESANS::throwFormatError(const std::string &line, const std::string &me
 	throw std::runtime_error(output);
 }
 
+/** Read headers from the input file into the attribute map, until BEGIN_DATA is found
+ * @param infile Reference to the input file
+ * @param line Reference to the line currently being processed
+ * @param lineNum Number of the line currently being processed
+ * @throw runtime_error If incorrectly formatted headers are found (empty lines are permitted)
+*/
 void LoadSESANS::consumeHeaders(std::ifstream &infile, std::string &line, int &lineNum) {
 	std::pair<std::string, std::string> attr;
 
@@ -128,6 +169,9 @@ void LoadSESANS::consumeHeaders(std::ifstream &infile, std::string &line, int &l
 	} while (std::getline(infile, line) && line != "BEGIN_DATA");
 }
 
+/** Make sure that all mandatory headers are supplied in the file
+ * @throw runtime_error If any other the mandatory headers are missing
+ */
 void LoadSESANS::checkMandatoryHeaders(){
 	for (std::string attr : mandatoryAttributes) {
 		if (attributes.find(attr) == attributes.end()) {
