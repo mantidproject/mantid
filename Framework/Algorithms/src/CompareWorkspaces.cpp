b6354b7982036aeef34ae7ee63e7fd0e2cc2ff5a
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-07-26 11:30:22 +0100

    Re #19684 Add flag in CompareWorkspaces to ignore ParameterMap

--- Framework/Algorithms/src/CompareWorkspaces.cpp
+++ Framework/Algorithms/src/CompareWorkspaces.cpp
@@ -50,6 +50,8 @@ void CompareWorkspaces::init() {
                   "Whether to check that the instruments match. ");
   declareProperty("CheckMasking", true,
                   "Whether to check that the bin masking matches. ");
+  declareProperty("CheckParameterMap", true,
+		  "Whether to check that the parameter maps match. ");
 
   // Have this one false by default - the logs are brittle
   declareProperty("CheckSample", false,
@@ -746,9 +748,11 @@ bool CompareWorkspaces::checkInstrument(API::MatrixWorkspace_const_sptr ws1,
     return false;
   }
 
-  const Geometry::ParameterMap &ws1_parmap = ws1->constInstrumentParameters();
-  const Geometry::ParameterMap &ws2_parmap = ws2->constInstrumentParameters();
-
+  if (checkParameterMap){
+    const Geometry::ParameterMap &ws1_parmap = ws1->constInstrumentParameters();
+    const Geometry::ParameterMap &ws2_parmap = ws2->constInstrumentParameters();
+  }
+  
   if (ws1_parmap != ws2_parmap) {
     g_log.debug()
         << "Here information to help understand parameter map differences:\n";
