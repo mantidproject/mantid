b85f3d0f19709320b0f21996603dd4f48dddd313
Steven Hahn <hahnse@ornl.gov>
2017-07-30 10:30:28 -0400

    add missing suppression.

--- Framework/ICat/src/GSoap/stdsoap2.cpp
+++ Framework/ICat/src/GSoap/stdsoap2.cpp
@@ -10,7 +10,7 @@
 // clang-format off
 GCC_DIAG_OFF(deprecated-declarations)
 CGG_DIAG_OFF(format-overflow=)
-CGG_DIAG_OFF(format-overflow=)
+CGG_DIAG_OFF(format-truncation=)
 GCC_DIAG_OFF(implicit-fallthrough=)
 // clang-format on
 
@@ -17022,7 +17022,7 @@ soap::~soap() {
 // clang-format off
 GCC_DIAG_ON(deprecated-declarations)
 CGG_DIAG_ON(format-overflow=)
-CGG_DIAG_ON(format-overflow=)
+CGG_DIAG_ON(format-truncation=)
 GCC_DIAG_ON(implicit-fallthrough=)
 // clang-format on
 
