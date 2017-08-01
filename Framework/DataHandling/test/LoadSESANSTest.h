87e9a5432a127834b6e1bf715cbb524aa3772a75
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-08-01 16:52:26 +0100

    Re #19943 Refactored LoadSESANSTest to remove repetition

--- Framework/DataHandling/test/LoadSESANSTest.h
+++ Framework/DataHandling/test/LoadSESANSTest.h
@@ -21,7 +21,8 @@ public:
   static void destroySuite(LoadSESANSTest *suite) { delete suite; }
 
   void test_init() {
-    writeGoodFile();
+    writeFile(goodFile);
+
     LoadSESANS testAlg;
     TS_ASSERT_THROWS_NOTHING(testAlg.initialize());
     TS_ASSERT(testAlg.isInitialized());
@@ -92,145 +93,113 @@ public:
     TS_ASSERT_EQUALS(testAlg.confidence(descriptor), 70);
   }
 
-  void test_requireFFV() {
-    LoadSESANS testAlg;
-    TS_ASSERT_THROWS_NOTHING(testAlg.initialize());
-    TS_ASSERT(testAlg.isInitialized());
-    testAlg.setChild(true);
-    testAlg.setRethrows(true);
-    writeFileMissingFFV();
-    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("Filename", infileName));
-    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("OutputWorkspace", "ws"));
+  void test_requireFFV() { attemptToLoadBadFile(fileMissingFFV); }
 
-    TS_ASSERT_THROWS(testAlg.execute(), std::runtime_error);
-    deleteFile(testAlg);
-  }
+  void test_mandatoryHeaders() { attemptToLoadBadFile(fileMissingHeaders); }
 
-  void test_mandatoryHeaders() {
-    LoadSESANS testAlg;
-    TS_ASSERT_THROWS_NOTHING(testAlg.initialize());
-    TS_ASSERT(testAlg.isInitialized());
-    testAlg.setChild(true);
-    testAlg.setRethrows(true);
-    writeFileMissingHeaders();
-    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("Filename", infileName));
-    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("OutputWorkspace", "ws"));
-
-    TS_ASSERT_THROWS(testAlg.execute(), std::runtime_error);
-    deleteFile(testAlg);
-  }
-
-  void test_mandatoryColumns() {
-    LoadSESANS testAlg;
-    TS_ASSERT_THROWS_NOTHING(testAlg.initialize());
-    TS_ASSERT(testAlg.isInitialized());
-    testAlg.setChild(true);
-    testAlg.setRethrows(true);
-    writeFileMissingColumns();
-    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("Filename", infileName));
-    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("OutputWorkspace", "ws"));
-
-    TS_ASSERT_THROWS(testAlg.execute(), std::runtime_error);
-    deleteFile(testAlg);
-  }
+  void test_mandatoryColumns() { attemptToLoadBadFile(fileMissingColumns); }
 
 private:
   std::string infileName = "temp.ses";
 
-  void writeGoodFile() {
-    std::string contents =
-        "FileFormatVersion       1.0\n"
-        "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
-        "Sample                  Ostensibly 40$ 100nm radius PMMA hard spheres "
-        "in mixed deuterarted decalin.\n"
-        "Thickness               2\n"
-        "Thickness_unit          mm\n"
-        "Theta_zmax              0.09\n"
-        "Theta_zmax_unit         radians\n"
-        "Theta_ymax              0.09\n"
-        "Theta_ymax_unit         radians\n"
-        "Orientation             Z\n"
-        "SpinEchoLength_unit     A\n"
-        "Depolarisation_unit     A - 2 cm - 1\n"
-        "Wavelength_unit         A\n"
-        "\n"
-        "BEGIN_DATA\n"
-        "SpinEchoLength Depolarisation Depolarisation_error Wavelength\n"
-        "260.0 -1.42E-3 2.04E-3 1.612452\n"
-        "280.8 -1.45E-3 1.87E-3 1.675709\n"
-        "303.264 -1.64E-3 1.23E-3 1.741448\n"
-        "327.525 -1.69E-3 1.809765\n"
-        "353.727 -2.23E-3 9.09E-4 1.880763\n"
-        "382.025 -2.26E-3 8.58E-4 1.954546\n";
+  /// Try and fail to load a file which violates the allowed format
+  void attemptToLoadBadFile(std::string fileContents) {
+    writeFile(fileContents);
+
+    LoadSESANS testAlg;
+    TS_ASSERT_THROWS_NOTHING(testAlg.initialize());
+    TS_ASSERT(testAlg.isInitialized());
+    testAlg.setChild(true);
+    testAlg.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("Filename", infileName));
+    TS_ASSERT_THROWS_NOTHING(testAlg.setProperty("OutputWorkspace", "ws"));
+
+    TS_ASSERT_THROWS(testAlg.execute(), std::runtime_error);
+
+    deleteFile(testAlg);
+  }
+
+  void writeFile(std::string fileContents) {
+    // Write a file to our temporary file
     std::ofstream file(infileName);
-    file << contents;
+    file << fileContents;
     file.close();
   }
 
-  void writeFileMissingFFV() {
-    std::string contents =
-        "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
-        "Sample                  Ostensibly 40$ 100nm radius PMMA hard spheres "
-        "in mixed deuterarted decalin.\n"
-        "Thickness               2\n"
-        "Thickness_unit          mm\n"
-        "Theta_zmax              0.09\n"
-        "Theta_zmax_unit         radians\n"
-        "Theta_ymax              0.09\n"
-        "Theta_ymax_unit         radians\n"
-        "Orientation             Z\n"
-        "SpinEchoLength_unit     A\n"
-        "Depolarisation_unit     A - 2 cm - 1\n"
-        "Wavelength_unit         A\n"
-        "\n"
-        "BEGIN_DATA\n"
-        "SpinEchoLength Depolarisation Depolarisation_error Wavelength\n"
-        "260.0 -1.42E-3 2.04E-3 1.612452\n"
-        "382.025 -2.26E-3 8.58E-4 1.954546\n";
-    std::ofstream file(infileName);
-    file << contents;
-    file.close();
-  }
+  std::string goodFile =
+      "FileFormatVersion       1.0\n"
+      "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
+      "Sample                  Ostensibly 40$ 100nm radius PMMA hard spheres "
+      "in mixed deuterarted decalin.\n"
+      "Thickness               2\n"
+      "Thickness_unit          mm\n"
+      "Theta_zmax              0.09\n"
+      "Theta_zmax_unit         radians\n"
+      "Theta_ymax              0.09\n"
+      "Theta_ymax_unit         radians\n"
+      "Orientation             Z\n"
+      "SpinEchoLength_unit     A\n"
+      "Depolarisation_unit     A - 2 cm - 1\n"
+      "Wavelength_unit         A\n"
+      "\n"
+      "BEGIN_DATA\n"
+      "SpinEchoLength Depolarisation Depolarisation_error Wavelength\n"
+      "260.0 -1.42E-3 2.04E-3 1.612452\n"
+      "280.8 -1.45E-3 1.87E-3 1.675709\n"
+      "303.264 -1.64E-3 1.23E-3 1.741448\n"
+      "327.525 -1.69E-3 1.809765\n"
+      "353.727 -2.23E-3 9.09E-4 1.880763\n"
+      "382.025 -2.26E-3 8.58E-4 1.954546\n";
 
-  void writeFileMissingHeaders() {
-    std::string contents =
-        "FileFormatVersion       1.0\n"
-        "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
-        "\n"
-        "BEGIN_DATA\n"
-        "SpinEchoLength Depolarisation Depolarisation_error Wavelength\n"
-        "260.0 -1.42E-3 2.04E-3 1.612452\n"
-        "382.025 -2.26E-3 8.58E-4 1.954546\n";
-    std::ofstream file(infileName);
-    file << contents;
-    file.close();
-  }
+  std::string fileMissingFFV =
+      "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
+      "Sample                  Ostensibly 40$ 100nm radius PMMA hard spheres "
+      "in mixed deuterarted decalin.\n"
+      "Thickness               2\n"
+      "Thickness_unit          mm\n"
+      "Theta_zmax              0.09\n"
+      "Theta_zmax_unit         radians\n"
+      "Theta_ymax              0.09\n"
+      "Theta_ymax_unit         radians\n"
+      "Orientation             Z\n"
+      "SpinEchoLength_unit     A\n"
+      "Depolarisation_unit     A - 2 cm - 1\n"
+      "Wavelength_unit         A\n"
+      "\n"
+      "BEGIN_DATA\n"
+      "SpinEchoLength Depolarisation Depolarisation_error Wavelength\n"
+      "260.0 -1.42E-3 2.04E-3 1.612452\n"
+      "382.025 -2.26E-3 8.58E-4 1.954546\n";
 
-  void writeFileMissingColumns() {
-    std::string contents =
-        "FileFormatVersion       1.0\n"
-        "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
-        "Sample                  Ostensibly 40$ 100nm radius PMMA hard spheres "
-        "in mixed deuterarted decalin.\n"
-        "Thickness               2\n"
-        "Thickness_unit          mm\n"
-        "Theta_zmax              0.09\n"
-        "Theta_zmax_unit         radians\n"
-        "Theta_ymax              0.09\n"
-        "Theta_ymax_unit         radians\n"
-        "Orientation             Z\n"
-        "SpinEchoLength_unit     A\n"
-        "Depolarisation_unit     A - 2 cm - 1\n"
-        "Wavelength_unit         A\n"
-        "\n"
-        "BEGIN_DATA\n"
-        "SpinEchoLength Depolarisation Depolarisation_error SomethingElse\n"
-        "260.0 -1.42E-3 2.04E-3 1.612452\n"
-        "280.8 -1.45E-3 1.87E-3 1.675709\n";
-    std::ofstream file(infileName);
-    file << contents;
-    file.close();
-  }
+  std::string fileMissingHeaders =
+      "FileFormatVersion       1.0\n"
+      "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
+      "\n"
+      "BEGIN_DATA\n"
+      "SpinEchoLength Depolarisation Depolarisation_error Wavelength\n"
+      "260.0 -1.42E-3 2.04E-3 1.612452\n"
+      "382.025 -2.26E-3 8.58E-4 1.954546\n";
+
+  std::string fileMissingColumns =
+      "FileFormatVersion       1.0\n"
+      "DataFileTitle           PMMA in Mixed Deuterated decalin\n"
+      "Sample                  Ostensibly 40$ 100nm radius PMMA hard spheres "
+      "in mixed deuterarted decalin.\n"
+      "Thickness               2\n"
+      "Thickness_unit          mm\n"
+      "Theta_zmax              0.09\n"
+      "Theta_zmax_unit         radians\n"
+      "Theta_ymax              0.09\n"
+      "Theta_ymax_unit         radians\n"
+      "Orientation             Z\n"
+      "SpinEchoLength_unit     A\n"
+      "Depolarisation_unit     A - 2 cm - 1\n"
+      "Wavelength_unit         A\n"
+      "\n"
+      "BEGIN_DATA\n"
+      "SpinEchoLength Depolarisation Depolarisation_error SomethingElse\n"
+      "260.0 -1.42E-3 2.04E-3 1.612452\n"
+      "280.8 -1.45E-3 1.87E-3 1.675709\n";
 
   void deleteFile(const LoadSESANS &testAlg) {
     std::string outputPath = testAlg.getProperty("Filename");
