107e3697aa03a4e96e2d4049f85b96ec16682b1d
LamarMoore <lamar.moore@stfc.ac.uk>
2017-08-10 11:51:08 +0100

    Alternative solution to C4661 compiler warning

--- Framework/Kernel/src/ArrayProperty.cpp
+++ Framework/Kernel/src/ArrayProperty.cpp
@@ -4,8 +4,99 @@
 #include "MantidKernel/PropertyWithValue.tcc"
 namespace Mantid {
 namespace Kernel {
-/// @cond
+/** Constructor
+ *  @param name ::      The name to assign to the property
+ *  @param vec ::       The initial vector of values to assign to the
+ * property.
+ *  @param validator :: The validator to use for this property, if required.
+ *  @param direction :: The direction (Input/Output/InOut) of this property
+ */
+template <typename T>
+ArrayProperty<T>::ArrayProperty(const std::string &name,
+                                const std::vector<T> &vec,
+                                IValidator_sptr validator,
+                                const unsigned int direction)
+    : PropertyWithValue<std::vector<T>>(name, vec, validator, direction) {}
 
+/** Constructor
+ *  Will lead to the property having a default-constructed (i.e. empty) vector
+ *  as its initial (default) value
+ *  @param name ::      The name to assign to the property
+ *  @param validator :: The validator to use for this property, if required
+ *  @param direction :: The direction (Input/Output/InOut) of this property
+ */
+
+template <typename T>
+ArrayProperty<T>::ArrayProperty(const std::string &name,
+                                IValidator_sptr validator,
+                                const unsigned int direction)
+    : PropertyWithValue<std::vector<T>>(name, std::vector<T>(), validator,
+                                        direction) {}
+
+/** Constructor that's useful for output properties or inputs with an empty
+ * default and no validator.
+ *  Will lead to the property having a default-constructed (i.e. empty) vector
+ *  as its initial (default) value and no validator
+ *  @param name ::      The name to assign to the property
+ *  @param direction :: The direction (Input/Output/InOut) of this property
+ */
+template <typename T>
+ArrayProperty<T>::ArrayProperty(const std::string &name,
+                                const unsigned int direction)
+    : PropertyWithValue<std::vector<T>>(name, std::vector<T>(),
+                                        IValidator_sptr(new NullValidator),
+                                        direction) {}
+
+/** Constructor from which you can set the property's values through a string:
+ *
+ * Inherits from the constructor of PropertyWithValue specifically made to
+ * handle a list
+ * of numeric values in a string format so that initial value is set
+ * correctly.
+ *
+ *  @param name ::      The name to assign to the property
+ *  @param values ::    A comma-separated string containing the values to
+ * store in the property
+ *  @param validator :: The validator to use for this property, if required
+ *  @param direction :: The direction (Input/Output/InOut) of this property
+ *  @throw std::invalid_argument if the string passed is not compatible with
+ * the array type
+ */
+template <typename T>
+ArrayProperty<T>::ArrayProperty(const std::string &name,
+                                const std::string &values,
+                                IValidator_sptr validator,
+                                const unsigned int direction)
+    : PropertyWithValue<std::vector<T>>(name, std::vector<T>(), values,
+                                        validator, direction) {}
+
+/// 'Virtual copy constructor'
+template <typename T> ArrayProperty<T> *ArrayProperty<T>::clone() const {
+  return new ArrayProperty<T>(*this);
+}
+
+/** Returns the values stored in the ArrayProperty
+ *  @return The stored values as a comma-separated list
+ */
+template <typename T> std::string ArrayProperty<T>::value() const {
+  // Implemented this method for documentation reasons. Just calls base class
+  // method.
+  return PropertyWithValue<std::vector<T>>::value();
+}
+
+/** Sets the values stored in the ArrayProperty from a string representation
+ *  @param value :: The values to assign to the property, given as a
+ * comma-separated list
+ *  @return True if the assignment was successful
+ */
+template <typename T>
+std::string ArrayProperty<T>::setValue(const std::string &value) {
+  // Implemented this method for documentation reasons. Just calls base class
+  // method.
+  return PropertyWithValue<std::vector<T>>::setValue(value);
+}
+
+/// @cond
 template class DLLExport ArrayProperty<int32_t>;
 template class DLLExport ArrayProperty<uint32_t>;
 template class DLLExport ArrayProperty<int64_t>;
@@ -30,5 +121,11 @@ template class DLLExport ArrayProperty<std::vector<unsigned long>>;
 #endif
 
 /// @endcond
+
+template <>
+MANTID_KERNEL_DLL ArrayProperty<int> *ArrayProperty<int>::clone() const {
+  return new ArrayProperty<int>(*this);
+}
+
 } // namespace Kernel
 } // namespace Mantid
