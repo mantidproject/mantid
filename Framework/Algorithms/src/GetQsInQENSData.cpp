901107addbdbabd6088eaa7313120c390b574b98
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2017-08-14 11:14:48 +0100

    Apply ClangFormat changes
    Refs #20168

--- Framework/Algorithms/src/GetQsInQENSData.cpp
+++ Framework/Algorithms/src/GetQsInQENSData.cpp
@@ -116,7 +116,8 @@ MantidVec GetQsInQENSData::extractQValues(
       const DetectorInfo detInf = workspace->detectorInfo();
 
       for (size_t i = 0; i < numSpectra; i++) {
-        IDetector_const_sptr detector = IDetector_const_sptr(&detInf.detector(i));
+        IDetector_const_sptr detector =
+            IDetector_const_sptr(&detInf.detector(i));
         double efixed = workspace->getEFixed(detector);
         double theta = 0.5 * workspace->detectorTwoTheta(*detector);
         qValues[i] = UnitConversion::convertToElasticQ(theta, efixed);
