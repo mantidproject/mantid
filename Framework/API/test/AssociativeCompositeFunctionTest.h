e378026b952632c7049cc95ffb356f6ec580e4af
Jose Borreguero <borreguero@gmail.com>
2017-08-17 18:53:19 -0400

    Refs #20250 Beginning with test of associative base class

--- Framework/API/test/AssociativeCompositeFunctionTest.h
+++ Framework/API/test/AssociativeCompositeFunctionTest.h
@@ -1,12 +1,15 @@
 #ifndef ASSOCIATIVECOMPOSITEFUNCTIONTEST_H_
 #define ASSOCIATIVECOMPOSITEFUNCTIONTEST_H_
 
+#include <string>
 #include <cxxtest/TestSuite.h>
+#include <boost/shared_array.hpp>
 
+#include "MantidAPI/IFunction.h"
 #include "MantidAPI/AssociativeCompositeFunction.h"
 #include "MantidAPI/IFunction1D.h"
 #include "MantidAPI/FunctionFactory.h"
-
+#include "MantidTestHelpers/FunctionTestHelper.h"
 
 using namespace Mantid;
 using namespace Mantid::API;
@@ -14,15 +17,20 @@ using namespace Mantid::API;
 
 /// Moc implementation of AssociativeCompositeFunction
 class AssociativeMoc : public AssociativeCompositeFunction {
+
 public:
+
     std::string name() const override { return "AssociativeMoc"; }
+
     bool isAssociative(API::IFunction_sptr f) const override {
-        if (boost::dynamic_pointer_cast<ProductFunction>(f)) {
+        if (boost::dynamic_pointer_cast<AssociativeMoc>(f)) {
             return true;
         }
         return false;
     }
+
 protected:
+
     /// overwrite IFunction base class method, which declare function parameters
     void init() override {};
 };
@@ -32,43 +40,144 @@ class AssociativeCompositeFunctionTest : public CxxTest::TestSuite {
 
 public:
 
+    /*
+     * Boilerplate
+     */
     static AssociativeCompositeFunctionTest *createSuite() {
         return new AssociativeCompositeFunctionTest();
     }
 
     static void destroySuite(AssociativeCompositeFunctionTest *suite) { delete suite; }
 
-    AssociativeCompositeFunctionTest(){
-        initializeFunctions();
-    };
+    /// Set up
+    AssociativeCompositeFunctionTest() : m_f() {
+        FunctionFactory::Instance().subscribe<Gauss>("Gauss");
+        FunctionFactory::Instance().subscribe<Linear>("Linear");
+        FunctionFactory::Instance().subscribe<Cubic>("Cubic");
+        FunctionFactory::Instance().subscribe<AssociativeMoc>("AssociativeMoc");
+        initializeMocFunctions();
+    }
 
-    testInitialization() {
-        // prod3 is a product within a product. It should become a product with three components
-        TS_ASSERT_EQUALS(m_f["prod3"]->asString(), "");
+    /// Tear down
+    ~AssociativeCompositeFunctionTest() override {
+        FunctionFactory::Instance().unsubscribe("Gauss");
+        FunctionFactory::Instance().unsubscribe("Linear");
+        FunctionFactory::Instance().unsubscribe("Cubic");
+        FunctionFactory::Instance().unsubscribe("AssociativeMoc");
+    }
+
+    /**
+     * Tests begin here
+     */
+
+    void testInitialization() {
+        std::string s("composite=AssociativeMoc,NumDeriv=false;" \
+                      "name=Linear,a=0,b=0;" \
+                      "name=Gauss,c=0,h=1,s=1;" \
+                      "name=Cubic,c0=0,c1=0,c2=0,c3=0");
+        TS_ASSERT_EQUALS(m_f["moc4"]->asString(), s);
+        s.assign("composite=AssociativeMoc,NumDeriv=false;" \
+                 "name=Linear,a=0,b=0;" \
+                 "name=Gauss,c=0,h=1,s=1;" \
+                 "name=Cubic,c0=0,c1=0,c2=0,c3=0;" \
+                 "name=Linear,a=0,b=0");
+        TS_ASSERT_EQUALS(m_f["moc5"]->asString(), s);
+    }
+
+    void testAddFunction() {
+        auto f = functionAssociativeInitialized("(name=AssociativeMoc)");
+        f->addFunction(m_f["line"]);
+        TS_ASSERT_EQUALS(f->asString(), "composite=AssociativeMoc,NumDeriv=false;name=Linear,a=0,b=0");
+        f->addFunction(m_f["comp"]);
+        // "comp" does not unroll, it's not associative
+        std::string s("composite=AssociativeMoc,NumDeriv=false;" \
+                      "name=Linear,a=0,b=0;" \
+                      "(name=Gauss,c=0,h=1,s=1;name=Cubic,c0=0,c1=0,c2=0,c3=0)");
+        TS_ASSERT_EQUALS(f->asString(), s);
+        f->addFunction(m_f["moc2"]);
+        s.append(";name=Linear,a=0,b=0");
+        TS_ASSERT_EQUALS(f->asString(), s);
+        f->addFunction(m_f["moc3"]);
+        s.append(";name=Gauss,c=0,h=1,s=1;" \
+                 "name=Cubic,c0=0,c1=0,c2=0,c3=0");  // moc3 is unrolled
+        TS_ASSERT_EQUALS(f->asString(), s);
+    }
+
+    void testInsertFunction() {
+        std::string s("(composite=AssociativeMoc;name=Gauss;name=Cubic)");
+        auto f = functionAssociativeInitialized(s);
+        f->insertFunction(1, m_f["line"]);  // insert Linear before Cubic
+        s.assign("composite=AssociativeMoc,NumDeriv=false;" \
+                 "name=Gauss,c=0,h=1,s=1;" \
+                 "name=Linear,a=0,b=0;" \
+                 "name=Cubic,c0=0,c1=0,c2=0,c3=0");
+        TS_ASSERT_EQUALS(f->asString(), s);
+        f->insertFunction(2, m_f["moc2"]);  // insert moc2 (Linear) before Cubic
+        s.assign("composite=AssociativeMoc,NumDeriv=false;" \
+                 "name=Gauss,c=0,h=1,s=1;" \
+                 "name=Linear,a=0,b=0;" \
+                 "name=Linear,a=0,b=0;" \
+                 "name=Cubic,c0=0,c1=0,c2=0,c3=0");
+        //TS_ASSERT_EQUALS(f->asString(), s);
+        f->insertFunction(3, m_f["moc3"]);  // insert moc3 (Gauss * Cubic) before Cubic
+        s.assign("composite=AssociativeMoc,NumDeriv=false;" \
+                 "name=Gauss,c=0,h=1,s=1;" \
+                 "name=Linear,a=0,b=0;" \
+                 "name=Linear,a=0,b=0;" \
+                 "name=Gauss,c=0,h=1,s=1;" \
+                 "name=Cubic,c0=0,c1=0,c2=0,c3=0;" \
+                 "name=Cubic,c0=0,c1=0,c2=0,c3=0");
+        //TS_ASSERT_EQUALS(f->asString(), s);
+    }
+
+    void testReplaceFunction() {
+        std::string s("(composite=AssociativeMoc;name=Gauss;name=Cubic)");
+        auto f = functionAssociativeInitialized(s);
+        f->replaceFunction(1, m_f["line"]);
+        //TS_ASSERT_EQUALS(f->asString(), s);
+        f->addFunction(m_f["comp"]);
+        //TS_ASSERT_EQUALS(f->asString(), s);
+        f->replaceFunction(0, m_f["moc2"]);
+        //TS_ASSERT_EQUALS(f->asString(), s);
+        f->insertFunction(1, m_f["moc3"]);
+        //TS_ASSERT_EQUALS(f->asString(), s);
     }
 
 private:
     // Initialize some functions that we'll use in the tests
-    void initializeFunctions() {
-        // aliases to certain function
+    void initializeMocFunctions() {
+        // aliases to certain functions
         std::map<std::string, std::string> aliases = {
-            {"exp", "name=ExpDecay"},
-            {"lin", "name=LinearBackground"},
-            {"comp", "name=Lorentzian;name=InelasticDiffSphere"},
-            {"conv", "(composite=Convolution;name=Resolution,X=(3.0,3.1),Y=(3.2,3.3);"
-                             "name=DeltaFunction)"},
-            {"prod1", "(name=AssociativeMoc)"},
-            {"prod2", "(composite=AssociativeMoc;name=ExpDecay;name=FlatBackground)"},
-            {"prod3", "(composite=AssociativeMoc;name=ExpDecay;"
-                              "(composite=ProductFunction;name=Gaussian;name=Quadratic))"}
+            {"line", "name=Linear"},
+            {"comp", "name=Gauss;name=Cubic"},
+            {"moc1", "(name=AssociativeMoc)"},
+            {"moc2", "(composite=AssociativeMoc;name=Linear)"},
+            // moc3 = Gauss * Linear
+            {"moc3", "(composite=AssociativeMoc;name=Gauss;name=Cubic)"},
+            // moc4 = Gauss * (Linear * Cubic)
+            {"moc4", "(composite=AssociativeMoc;name=Linear;"
+                        "(composite=AssociativeMoc;name=Gauss;name=Cubic))"},
+            // moc5 = (Gauss * Linear) * (Cubic * Gauss)
+            {"moc5", "(composite=AssociativeMoc;" \
+                     "(composite=AssociativeMoc;name=Linear;name=Gauss);" \
+                     "(composite=AssociativeMoc;name=Cubic;name=Linear))"},
         };
-        // Create the function objects
         for (auto& kv : aliases) {
-            m_f[kv.first] = FunctionFactory::Instance().createInitialized(kv.second);
+            //m_f[kv.first] = FunctionFactory::Instance().createInitialized(kv.second);
+            m_f.insert(std::pair<std::string, IFunction_sptr>(kv.first,
+               FunctionFactory::Instance().createInitialized(kv.second)));
         }
     }
+
+    boost::shared_ptr<AssociativeMoc> functionAssociativeInitialized(const std::string &s) {
+        auto f = FunctionFactory::Instance().createInitialized(s);
+        return boost::dynamic_pointer_cast<AssociativeMoc>(f);
+    }
+
     // a map containing aliases to function pointers
-    std::map<std::string, std::string> m_f;
-};
+    std::map<std::string, IFunction_sptr> m_f;
+
+};  /* class AssociativeCompositeFunctionTest */
+
 #endif /*ASSOCIATIVECOMPOSITEFUNCTIONTEST_H_*/
 
--- Framework/API/test/CompositeFunctionTest.h
+++ Framework/API/test/CompositeFunctionTest.h
@@ -3,15 +3,14 @@
 
 #include <cxxtest/TestSuite.h>
 
-#include "MantidAPI/IPeakFunction.h"
 #include "MantidAPI/CompositeFunction.h"
 #include "MantidAPI/MatrixWorkspace.h"
 #include "MantidAPI/FrameworkManager.h"
 #include "MantidAPI/WorkspaceFactory.h"
-#include "MantidAPI/ParamFunction.h"
 #include "MantidAPI/IFunction1D.h"
 #include "MantidAPI/FunctionFactory.h"
 #include "MantidTestHelpers/FakeObjects.h"
+#include "MantidTestHelpers/FunctionTestHelper.h"
 
 using namespace Mantid;
 using namespace Mantid::API;
@@ -84,113 +83,6 @@ private:
   size_t m_blocksize;
 };
 
-class Gauss : public IPeakFunction {
-public:
-  Gauss() {
-    declareParameter("c");
-    declareParameter("h", 1.);
-    declareParameter("s", 1.);
-  }
-
-  std::string name() const override { return "Gauss"; }
-
-  void functionLocal(double *out, const double *xValues,
-                     const size_t nData) const override {
-    double c = getParameter("c");
-    double h = getParameter("h");
-    double w = getParameter("s");
-    for (size_t i = 0; i < nData; i++) {
-      double x = xValues[i] - c;
-      out[i] = h * exp(-0.5 * x * x * w);
-    }
-  }
-  void functionDerivLocal(Jacobian *out, const double *xValues,
-                          const size_t nData) override {
-    // throw Mantid::Kernel::Exception::NotImplementedError("");
-    double c = getParameter("c");
-    double h = getParameter("h");
-    double w = getParameter("s");
-    for (size_t i = 0; i < nData; i++) {
-      double x = xValues[i] - c;
-      double e = h * exp(-0.5 * x * x * w);
-      out->set(static_cast<int>(i), 0, x * h * e * w);
-      out->set(static_cast<int>(i), 1, e);
-      out->set(static_cast<int>(i), 2, -0.5 * x * x * h * e);
-    }
-  }
-
-  double centre() const override { return getParameter(0); }
-
-  double height() const override { return getParameter(1); }
-
-  double fwhm() const override { return getParameter(2); }
-
-  void setCentre(const double c) override { setParameter(0, c); }
-  void setHeight(const double h) override { setParameter(1, h); }
-
-  void setFwhm(const double w) override { setParameter(2, w); }
-};
-
-class Linear : public ParamFunction, public IFunction1D {
-public:
-  Linear() {
-    declareParameter("a");
-    declareParameter("b");
-  }
-
-  std::string name() const override { return "Linear"; }
-
-  void function1D(double *out, const double *xValues,
-                  const size_t nData) const override {
-    double a = getParameter("a");
-    double b = getParameter("b");
-    for (size_t i = 0; i < nData; i++) {
-      out[i] = a + b * xValues[i];
-    }
-  }
-  void functionDeriv1D(Jacobian *out, const double *xValues,
-                       const size_t nData) override {
-    // throw Mantid::Kernel::Exception::NotImplementedError("");
-    for (size_t i = 0; i < nData; i++) {
-      out->set(static_cast<int>(i), 0, 1.);
-      out->set(static_cast<int>(i), 1, xValues[i]);
-    }
-  }
-};
-
-class Cubic : public ParamFunction, public IFunction1D {
-public:
-  Cubic() {
-    declareParameter("c0");
-    declareParameter("c1");
-    declareParameter("c2");
-    declareParameter("c3");
-  }
-
-  std::string name() const override { return "Cubic"; }
-
-  void function1D(double *out, const double *xValues,
-                  const size_t nData) const override {
-    double c0 = getParameter("c0");
-    double c1 = getParameter("c1");
-    double c2 = getParameter("c2");
-    double c3 = getParameter("c3");
-    for (size_t i = 0; i < nData; i++) {
-      double x = xValues[i];
-      out[i] = c0 + x * (c1 + x * (c2 + x * c3));
-    }
-  }
-  void functionDeriv1D(Jacobian *out, const double *xValues,
-                       const size_t nData) override {
-    for (size_t i = 0; i < nData; i++) {
-      double x = xValues[i];
-      out->set(static_cast<int>(i), 0, 1.);
-      out->set(static_cast<int>(i), 1, x);
-      out->set(static_cast<int>(i), 2, x * x);
-      out->set(static_cast<int>(i), 3, x * x * x);
-    }
-  }
-};
 
 class CompositeFunctionTest : public CxxTest::TestSuite {
 public:
