f63a5253f24af05bd9c72077489588e3968abf80
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2017-08-18 12:09:23 +0100

    Fix PEP warnings in OSIRISDiffractionReductionTest
    Refs #18858

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/OSIRISDiffractionReductionTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/OSIRISDiffractionReductionTest.py
@@ -1,4 +1,4 @@
-#pylint: disable=too-many-public-methods,invalid-name
+# pylint: disable=too-many-public-methods,invalid-name
 from __future__ import (absolute_import, division, print_function)
 
 import unittest
@@ -7,7 +7,6 @@ from mantid.api import *
 
 
 class OSIRISDiffractionReductionTest(unittest.TestCase):
-
     def test_basic_reduction_completes(self):
         """
         Sanity test to ensure the most basic reduction actually completes.
@@ -22,7 +21,6 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
         self.assertTrue(isinstance(wks, MatrixWorkspace), 'Result workspace should be a matrix workspace.')
         self.assertEqual(wks.getAxis(0).getUnit().unitID(), 'dSpacing')
         self.assertEqual(wks.getNumberHistograms(), 1)
-
 
     def test_reduction_with_manual_drange_completes(self):
         """
@@ -42,7 +40,6 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
         self.assertEqual(wks.getAxis(0).getUnit().unitID(), 'dSpacing')
         self.assertEqual(wks.getNumberHistograms(), 1)
 
-
     def test_reduction_with_can_subtraction(self):
         """
         Tests reduction after subtraction of an empty can.
@@ -60,7 +57,6 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
         self.assertTrue(isinstance(wks, MatrixWorkspace), 'Result workspace should be a matrix workspace.')
         self.assertEqual(wks.getAxis(0).getUnit().unitID(), 'dSpacing')
         self.assertEqual(wks.getNumberHistograms(), 1)
-
 
     def test_reduction_with_can_subtraction_and_scale(self):
         """
@@ -81,14 +77,13 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
         self.assertEqual(wks.getAxis(0).getUnit().unitID(), 'dSpacing')
         self.assertEqual(wks.getNumberHistograms(), 1)
 
-
     def test_reduction_with_multiple_runs(self):
         """
         Test reduction with multiple sample and vanadium runs
         """
-        wks = OSIRISDiffractionReduction(Sample=['OSI10203.raw','OSI10204.RAW'],
+        wks = OSIRISDiffractionReduction(Sample=['OSI10203.raw', 'OSI10204.RAW'],
                                          CalFile='osiris_041_RES10.cal',
-                                         Vanadium=['OSI10156.raw','OSI10157.RAW'],
+                                         Vanadium=['OSI10156.raw', 'OSI10157.RAW'],
                                          ContainerScaleFactor=0.5,
                                          SpectraMin=3,
                                          SpectraMax=361,
@@ -97,15 +92,14 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
         self.assertEqual(wks.getAxis(0).getUnit().unitID(), 'dSpacing')
         self.assertEqual(wks.getNumberHistograms(), 1)
 
-
     def test_reduction_with_multiple_sample_and_multiple_can(self):
         """
         Test reduction with multiple sample, vanadium and container runs
         """
-        wks = OSIRISDiffractionReduction(Sample=['OSI10203.raw','OSI10204.RAW'],
+        wks = OSIRISDiffractionReduction(Sample=['OSI10203.raw', 'OSI10204.RAW'],
                                          CalFile='osiris_041_RES10.cal',
-                                         Vanadium=['OSI10156.raw','OSI10157.RAW'],
-                                         Container=['OSI10241.raw','OSI10242.RAW'],
+                                         Vanadium=['OSI10156.raw', 'OSI10157.RAW'],
+                                         Container=['OSI10241.raw', 'OSI10242.RAW'],
                                          ContainerScaleFactor=0.5,
                                          SpectraMin=3,
                                          SpectraMax=361,
@@ -142,7 +136,6 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
                           SpectraMin=16,
                           SpectraMax=17)
 
-
     def test_failure_no_vanadium(self):
         """
         Tests error handling when failed to obtain a vanadium run.
@@ -155,7 +148,6 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
                           SpectraMin=3,
                           SpectraMax=361)
 
-
     def test_mismatch_sample_vanadium_numbers(self):
         """
         Test error handling when number of samples is not equal to number of vanadium
@@ -167,7 +159,6 @@ class OSIRISDiffractionReductionTest(unittest.TestCase):
                           Vanadium=['OSI10156.raw'],
                           SpectraMin=3,
                           SpectraMax=361)
-
 
     def test_mismatch_sample_container_numbers(self):
         """
