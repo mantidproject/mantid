cf026c97a279d4781ec8ab7d9eabbb38031b0b34
Steven Hahn <hahnse@ornl.gov>
2017-08-21 14:58:14 -0400

    Make AlignedCutter inherit from vtkCutter.

--- qt/paraview_ext/PVPlugins/Representations/AlignedCutter.h
+++ qt/paraview_ext/PVPlugins/Representations/AlignedCutter.h
@@ -45,235 +45,29 @@
 #ifndef AlignedCutter_h
 #define AlignedCutter_h
 
-#include "vtkFiltersCoreModule.h" // For export macro
-#include "vtkPolyDataAlgorithm.h"
+#include "vtkCutter.h"
 
-#include "vtkContourValues.h" // Needed for inline methods
-
-#define VTK_SORT_BY_VALUE 0
-#define VTK_SORT_BY_CELL 1
-
-class vtkImplicitFunction;
-class vtkIncrementalPointLocator;
-class vtkSynchronizedTemplates3D;
-class vtkSynchronizedTemplatesCutter3D;
-class vtkGridSynchronizedTemplates3D;
-class vtkRectilinearSynchronizedTemplates;
-
-class VTKFILTERSCORE_EXPORT AlignedCutter : public vtkPolyDataAlgorithm
-{
+class VTKFILTERSCORE_EXPORT AlignedCutter : public vtkCutter {
 public:
-  vtkTypeMacro(AlignedCutter,vtkPolyDataAlgorithm);
+  vtkTypeMacro(AlignedCutter, vtkCutter);
   void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
-
+  vtkSetMacro(AxisNumber, int);
+  vtkGetMacro(AxisNumber, int);
   /**
    * Construct with user-specified implicit function; initial value of 0.0; and
    * generating cut scalars turned off.
    */
   static AlignedCutter *New();
-
-  /**
-   * Set a particular contour value at contour number i. The index i ranges
-   * between 0<=i<NumberOfContours.
-   */
-  void SetValue(int i, double value)
-    {this->ContourValues->SetValue(i,value);}
-
-  /**
-   * Get the ith contour value.
-   */
-  double GetValue(int i)
-    {return this->ContourValues->GetValue(i);}
-
-  /**
-   * Get a pointer to an array of contour values. There will be
-   * GetNumberOfContours() values in the list.
-   */
-  double *GetValues()
-    {return this->ContourValues->GetValues();}
-
-  /**
-   * Fill a supplied list with contour values. There will be
-   * GetNumberOfContours() values in the list. Make sure you allocate
-   * enough memory to hold the list.
-   */
-  void GetValues(double *contourValues)
-    {this->ContourValues->GetValues(contourValues);}
-
-  /**
-   * Set the number of contours to place into the list. You only really
-   * need to use this method to reduce list size. The method SetValue()
-   * will automatically increase list size as needed.
-   */
-  void SetNumberOfContours(int number)
-    {this->ContourValues->SetNumberOfContours(number);}
-
-  /**
-   * Get the number of contours in the list of contour values.
-   */
-  int GetNumberOfContours()
-    {return this->ContourValues->GetNumberOfContours();}
-
-  /**
-   * Generate numContours equally spaced contour values between specified
-   * range. Contour values will include min/max range values.
-   */
-  void GenerateValues(int numContours, double range[2])
-    {this->ContourValues->GenerateValues(numContours, range);}
-
-  /**
-   * Generate numContours equally spaced contour values between specified
-   * range. Contour values will include min/max range values.
-   */
-  void GenerateValues(int numContours, double rangeStart, double rangeEnd)
-    {this->ContourValues->GenerateValues(numContours, rangeStart, rangeEnd);}
-
-  /**
-   * Override GetMTime because we delegate to vtkContourValues and refer to
-   * vtkImplicitFunction.
-   */
-  vtkMTimeType GetMTime() VTK_OVERRIDE;
-
-  //@{
-  /**
-   * Specify the implicit function to perform the cutting.
-   */
-  virtual void SetCutFunction(vtkImplicitFunction*);
-  vtkGetObjectMacro(CutFunction,vtkImplicitFunction);
-  //@}
-
-  //@{
-  /**
-   * If this flag is enabled, then the output scalar values will be
-   * interpolated from the implicit function values, and not the input scalar
-   * data.
-   */
-  vtkSetMacro(GenerateCutScalars,int);
-  vtkGetMacro(GenerateCutScalars,int);
-  vtkBooleanMacro(GenerateCutScalars,int);
-  //@}
-    
-  vtkSetMacro(AxisNumber,int);
-  vtkGetMacro(AxisNumber,int);
-
- //@{
- /**
-  * If this is enabled (by default), the output will be triangles
-  * otherwise, the output will be the intersection polygons
-  * WARNING: if the cutting function is not a plane, the output
-  * will be 3D poygons, which might be nice to look at but hard
-  * to compute with downstream.
-  */
-  vtkSetMacro(GenerateTriangles,int);
-  vtkGetMacro(GenerateTriangles,int);
-  vtkBooleanMacro(GenerateTriangles,int);
- //@}
-
-  //@{
-  /**
-   * Specify a spatial locator for merging points. By default,
-   * an instance of vtkMergePoints is used.
-   */
-  void SetLocator(vtkIncrementalPointLocator *locator);
-  vtkGetObjectMacro(Locator,vtkIncrementalPointLocator);
-  //@}
-
-  //@{
-  /**
-   * Set the sorting order for the generated polydata. There are two
-   * possibilities:
-   * Sort by value = 0 - This is the most efficient sort. For each cell,
-   * all contour values are processed. This is the default.
-   * Sort by cell = 1 - For each contour value, all cells are processed.
-   * This order should be used if the extracted polygons must be rendered
-   * in a back-to-front or front-to-back order. This is very problem
-   * dependent.
-   * For most applications, the default order is fine (and faster).
-
-   * Sort by cell is going to have a problem if the input has 2D and 3D cells.
-   * Cell data will be scrambled becauses with
-   * vtkPolyData output, verts and lines have lower cell ids than triangles.
-   */
-  vtkSetClampMacro(SortBy,int,VTK_SORT_BY_VALUE,VTK_SORT_BY_CELL);
-  vtkGetMacro(SortBy,int);
-  void SetSortByToSortByValue()
-    {this->SetSortBy(VTK_SORT_BY_VALUE);}
-  void SetSortByToSortByCell()
-    {this->SetSortBy(VTK_SORT_BY_CELL);}
-  const char *GetSortByAsString();
-  //@}
-
-  /**
-   * Create default locator. Used to create one when none is specified. The
-   * locator is used to merge coincident points.
-   */
-  void CreateDefaultLocator();
-
-  /**
-   * Normally I would put this in a different class, but since
-   * This is a temporary fix until we convert this class and contour filter
-   * to generate unstructured grid output instead of poly data, I am leaving it here.
-   */
-  static void GetCellTypeDimensions(unsigned char* cellTypeDimensions);
-
-  //@{
-  /**
-   * Set/get the desired precision for the output types. See the documentation
-   * for the vtkAlgorithm::DesiredOutputPrecision enum for an explanation of
-   * the available precision settings.
-   */
-  vtkSetClampMacro(OutputPointsPrecision, int, SINGLE_PRECISION, DEFAULT_PRECISION);
-  vtkGetMacro(OutputPointsPrecision, int);
-  //@}
-
 protected:
   AlignedCutter(vtkImplicitFunction *cf=NULL);
   ~AlignedCutter() VTK_OVERRIDE;
-
   int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *) VTK_OVERRIDE;
-  int RequestUpdateExtent(vtkInformation *, vtkInformationVector **, vtkInformationVector *) VTK_OVERRIDE;
-  int FillInputPortInformation(int port, vtkInformation *info) VTK_OVERRIDE;
-  void UnstructuredGridCutter(vtkDataSet *input, vtkPolyData *output);
-  void DataSetCutter(vtkDataSet *input, vtkPolyData *output);
-  void StructuredPointsCutter(vtkDataSet *, vtkPolyData *,
-                              vtkInformation *, vtkInformationVector **,
-                              vtkInformationVector *);
-  void StructuredGridCutter(vtkDataSet *, vtkPolyData *);
-  void RectilinearGridCutter(vtkDataSet *, vtkPolyData *);
-  vtkImplicitFunction *CutFunction;
-  int GenerateTriangles;
-
-  vtkSynchronizedTemplates3D *SynchronizedTemplates3D;
-  vtkSynchronizedTemplatesCutter3D *SynchronizedTemplatesCutter3D;
-  vtkGridSynchronizedTemplates3D *GridSynchronizedTemplates;
-  vtkRectilinearSynchronizedTemplates *RectilinearSynchronizedTemplates;
-
-  vtkIncrementalPointLocator *Locator;
-  int SortBy;
-  vtkContourValues *ContourValues;
-  int GenerateCutScalars;
+  void AlignedStructuredGridCutter(vtkDataSet *, vtkPolyData *);
   int AxisNumber;
-  int OutputPointsPrecision;
 private:
   AlignedCutter(const AlignedCutter&) VTK_DELETE_FUNCTION;
   void operator=(const AlignedCutter&) VTK_DELETE_FUNCTION;
 };
-
-//@{
-/**
- * Return the sorting procedure as a descriptive character string.
- */
-inline const char *AlignedCutter::GetSortByAsString(void)
-{
-  if ( this->SortBy == VTK_SORT_BY_VALUE )
-  {
-    return "SortByValue";
-  }
-  else
-  {
-    return "SortByCell";
-  }
-}
 //@}
 
 #endif
