ccbe6e0f5c8f3dae29fb0125338aeaa5d0c25d91
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-08-22 16:20:51 +0100

    refs #20233 fixed flake 8 warnings

--- scripts/Muon/model_constructor.py
+++ scripts/Muon/model_constructor.py
@@ -2,9 +2,10 @@ from __future__ import (absolute_import, division, print_function)
 from Muon import MaxEnt_model
 
 # simple class to create a single object
-# containing all of the models. 
+# containing all of the models.
 # Only need to pass a single object to all
-# presenters 
+# presenters
+
 
 class modelConstructor(object):
 
@@ -14,7 +15,6 @@ class modelConstructor(object):
             self.transformModels={}
             alg =MaxEnt_model.MaxEntModel()
             self.transformModels["MaxEnt"]=MaxEnt_model.MaxEntThread(alg)
-            
+
     def getModel(self,name):
         return self.transformModels[name]
- 
