d2d8fd8e9a6172d2bf15dfe39c4ba620834cdb46
Karl Palmen <karl.palmen@stfc.ac.uk>
2017-08-23 09:09:51 +0100

    Improve introductory comments of __init__.py file re #20279
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Framework/PythonInterface/mantid/_plugins/__init__.py
+++ Framework/PythonInterface/mantid/_plugins/__init__.py
@@ -2,12 +2,13 @@
 Plugins
 =======
 
-Defines Python objects that wrap the C++ DataObjects namespace. These are exported
-so that boost::python can automagically downcast objects to the correct leaf type.
+Defines Python objects that wrap classes from the framework defined as plugins. 
+The classes defined here simply export the class types to Python, but not any additional functionality. 
+This allows Boost Python to automatically downcast a type to the actual leaf type, 
+making it easier to work with these objects in Python
 
-For example, sunclasses of composite function such as ProductFunction is made 
-available as ProductFunction rather than IFunction and so methods, such as
-__len__() & add() inherited from CompositeFunction are made available.
+For example, ProductFunction is made available as ProductFunction rather than IFunction 
+and so methods, such as __len__() & add() inherited from CompositeFunction are made available.
 
 The names from the library are not imported by default as it is best if the interface classes
 are used for checks such as isinstance()
