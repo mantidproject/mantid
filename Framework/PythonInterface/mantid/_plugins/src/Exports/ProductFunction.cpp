a5b918a73b6914313f7ae9126496245d6503d393
Karl Palmen <karl.palmen@stfc.ac.uk>
2017-08-23 10:18:37 +0100

    Remove unnecessary stuff re #20279
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Framework/PythonInterface/mantid/_plugins/src/Exports/ProductFunction.cpp
+++ Framework/PythonInterface/mantid/_plugins/src/Exports/ProductFunction.cpp
@@ -6,17 +6,6 @@ using Mantid::CurveFitting::Functions::ProductFunction;
 using Mantid::API::CompositeFunction;
 using namespace boost::python;
 
-namespace {
-
-typedef double (ProductFunction::*getParameterType1)(size_t) const;
-typedef double (ProductFunction::*getParameterType2)(const std::string &) const;
-
-typedef void (ProductFunction::*setParameterType2)(const std::string &,
-                                                   const double &, bool);
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(setParameterType2_Overloads,
-                                       setParameter, 2, 3)
-}
-
 void export_ProductFunction() {
 
   class_<ProductFunction, bases<CompositeFunction>, boost::noncopyable>(
