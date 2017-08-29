c19b649016da1365c74d7f6732e5a5719536f2fe
Gagik Vardanyan <vardanyan@ill.fr>
2017-08-29 09:37:50 +0200

    Re #18097 reverting derivOrder change

--- Framework/CurveFitting/src/Algorithms/SplineInterpolation.cpp
+++ Framework/CurveFitting/src/Algorithms/SplineInterpolation.cpp
@@ -119,7 +119,8 @@ std::map<std::string, std::string> SplineInterpolation::validateInputs() {
  */
 void SplineInterpolation::exec() {
   // read in algorithm parameters
-  const size_t order = getProperty("DerivOrder");
+  const int derivOrder = getProperty("DerivOrder");
+  const size_t order = static_cast<size_t>(derivOrder);
 
   // set input workspaces
   MatrixWorkspace_sptr mws = getProperty("WorkspaceToMatch");
