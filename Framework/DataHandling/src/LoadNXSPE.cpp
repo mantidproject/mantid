84ad584e7e8d3d2d2b018cd5e5f1271323fa535f
Simon Heybrock <simon.heybrock@esss.se>
2017-08-31 08:09:50 +0200

    Re #0. Fixes for failing doctests.
    Failures were:
        docs.algorithms/SaveNXSPE-v1.ExSimpleSavingRoundtrip
        docs.algorithms/NormaliseVanadium-v1.NormaliseVanadium
        docs.algorithms/SaveYDA-v1.SaveYDAExample
        docs.algorithms/SaveYDA-v1.SaveYDAExample
        docs.algorithms/ViewBOA-v1.ViewBOAEx

--- Framework/DataHandling/src/LoadNXSPE.cpp
+++ Framework/DataHandling/src/LoadNXSPE.cpp
@@ -304,6 +304,7 @@ void LoadNXSPE::exec() {
   for (std::size_t i = 0; i < numSpectra; ++i) {
     itdataend = itdata + numBins;
     iterrorend = iterror + numBins;
+    outputWS->getSpectrum(i).setDetectorID(static_cast<detid_t>(i + 1));
     outputWS->setBinEdges(i, edges);
     if ((!std::isfinite(*itdata)) || (*itdata <= -1e10)) // masked bin
     {
