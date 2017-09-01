9e2f4cf61cdbb34fa7f9937864f6dba1dc2a626a
mantid-builder <mantid-buildserver@mantidproject.org>
2017-09-01 06:12:27 -0400

    clang-format PR20368 7ef1e8d

--- Framework/PythonInterface/mantid/kernel/src/Exports/StringContainsValidator.cpp
+++ Framework/PythonInterface/mantid/kernel/src/Exports/StringContainsValidator.cpp
@@ -11,16 +11,15 @@ using Mantid::Kernel::IValidator;
 using namespace boost::python;
 
 namespace {
-  StringContainsValidator *createStringContainsValidator() {
-    return new StringContainsValidator();
-  }
+StringContainsValidator *createStringContainsValidator() {
+  return new StringContainsValidator();
+}
 
 /// Set required strings from a python list
 void setRequiredStrings(StringContainsValidator &self,
-          const boost::python::list &strs) {
-    using namespace Mantid::PythonInterface;
-    self.setRequiredStrings(Converters::PySequenceToVector<std::string>(strs)());
-
+                        const boost::python::list &strs) {
+  using namespace Mantid::PythonInterface;
+  self.setRequiredStrings(Converters::PySequenceToVector<std::string>(strs)());
 }
 }
 
@@ -29,8 +28,6 @@ void export_StringContainsValidator() {
       "StringContainsValidator")
       .def("__init__", make_constructor(&createStringContainsValidator,
                                         default_call_policies()))
-      .def("setRequiredStrings", &setRequiredStrings,
-           arg("Strings"),
+      .def("setRequiredStrings", &setRequiredStrings, arg("Strings"),
            "Set the list of sub strings that the input must contain");
 }
-
