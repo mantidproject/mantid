64fb31f4429a96f31e97eb5cb0cb0ed5cda5616b
Edward Brown <edward.brown@stfc.ac.uk>
2017-09-06 14:36:41 +0100

    Re #19951: Added missing DllOption to GenericPresenterFactory.

--- qt/widgets/common/inc/MantidQtWidgets/Common/DataProcessorUI/GenericDataProcessorTreeManagerFactory.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/DataProcessorUI/GenericDataProcessorTreeManagerFactory.h
@@ -1,9 +1,11 @@
 #include "MantidQtWidgets/Common/DataProcessorUI/DataProcessorTreeManagerFactory.h"
+#include "MantidQtWidgets/Common/DllOption.h"
 namespace MantidQt {
 namespace MantidWidgets {
-class GenericDataProcessorTreeManagerFactory
+class EXPORT_OPT_MANTIDQT_COMMON GenericDataProcessorTreeManagerFactory
     : public DataProcessorTreeManagerFactory {
-  std::pair<std::unique_ptr<DataProcessorTreeManager>, DataProcessorTreeManagerFactory::PostProcessing>
+  std::pair<std::unique_ptr<DataProcessorTreeManager>,
+            DataProcessorTreeManagerFactory::PostProcessing>
   fromPostProcessorName(GenericDataProcessorPresenter &presenter,
                         const QString &postprocessorName,
                         DataProcessorWhiteList whitelist) override;
