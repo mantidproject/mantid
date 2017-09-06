e859be82134f74bcdaf6e647e6b20fd1b48f6802
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2017-09-06 11:56:14 +0100

    adjust init and attribute set/get Re #20358

--- scripts/Inelastic/CrystalField/CrystalFieldMultiSite.py
+++ scripts/Inelastic/CrystalField/CrystalFieldMultiSite.py
@@ -8,7 +8,7 @@ class CrystalFieldMultiSite(object):
                              'IB21', 'IB22', 'IB41', 'IB42', 'IB43', 'IB44', 'IB61', 'IB62', 'IB63', 'IB64', 'IB65',
                              'IB66']
 
-    def __init__(self, Ions, Symmetries, kwargs):
+    def __init__(self, Ions, Symmetries, **kwargs):
 
         self._makeFunction(Ions, Symmetries)
         self.Ions = Ions
@@ -16,16 +16,14 @@ class CrystalFieldMultiSite(object):
 
         free_parameters = []
         for key in kwargs:
-            if key == 'Temperature':
-                self.Temperature = kwargs[key]
+            if key == 'Temperatures':
+                self.Temperatures = kwargs[key]
             elif key == 'ToleranceEnergy':
                 self.ToleranceEnergy = kwargs[key]
             elif key == 'ToleranceIntensity':
                 self.ToleranceIntensity = kwargs[key]
-            elif key == 'IntensityScaling':
-                self.IntensityScaling = kwargs[key]
-            elif key == 'FWHM':
-                self.FWHM = kwargs[key]
+            elif key == 'FWHMs':
+                self.FWHMs = kwargs[key]
             elif key == 'ResolutionModel':
                 self.ResolutionModel = kwargs[key]
             elif key == 'NPeaks':
@@ -34,36 +32,68 @@ class CrystalFieldMultiSite(object):
                 self.FWHMVariation = kwargs[key]
             elif key == 'FixAllPeaks':
                 self.FixAllPeaks = kwargs[key]
+            elif key == 'PeakShape':
+                self.PeakShape = kwargs[key]
             elif key == 'PhysicalProperty':
                 self.PhysicalProperty = kwargs[key]
+            elif key == 'parameters':
+                for name, value in kwargs[key].items():
+                    self.function.setParameter(name, value)
+            elif key == 'attributes':
+                for name, value in kwargs[key].items():
+                    self.function.setAttributeValue(name, value)
             else:
                 # Crystal field parameters
                 self.function.setParameter(key, kwargs[key])
                 free_parameters.append(key)
+        if not self.isMultiSite():
+            for param in CrystalFieldMultiSite.field_parameter_names:
+                if param not in free_parameters:
+                    self.function.fixParameter(param)
+        #  else:  return to this?
 
-        for param in CrystalFieldMultiSite.field_parameter_names:
-            if param not in free_parameters:
-                self.function.fixParameter(param)
+
+    def isMultiSite(self):
+        return len(self.Ions) > 1
 
     def _makeFunction(self, ion, symmetry):
         from mantid.simpleapi import FunctionFactory
         self.function = FunctionFactory.createFunction('CrystalFieldFunction')
 
+    @staticmethod
+    def iterable_to_string(iterable):
+        values_as_string = ""
+        for element in iterable:
+            values_as_string += ","
+            values_as_string += element
+        values_as_string = values_as_string[1:]
+        return values_as_string
+
     @property
     def Ions(self):
-        return self.function.getAttributeValue('Ion')
+        string_ions = self.function.getAttributeValue('Ions')
+        string_ions = string_ions[1:-1]
+        return string_ions.split(",")
 
     @Ions.setter
     def Ions(self, value):
-        self.function.setAttributeValue('Ion', value)
+        if isinstance(value, basestring):
+            self.function.setAttributeValue('Ions', value)
+        else:
+            self.function.setAttributeValue('Ions', self.iterable_to_string(value))
 
     @property
     def Symmetries(self):
-        return self.function.getAttributeValue('Symmetry')
+        string_symmetries = self.function.getAttributeValue('Symmetries')
+        string_symmetries = string_symmetries[1:-1]
+        return string_symmetries.split(",")
 
     @Symmetries.setter
     def Symmetries(self, value):
-        self.function.setAttributeValue('Symmetry', value)
+        if isinstance(value, basestring):
+            self.function.setAttributeValue('Symmetries', value)
+        else:
+            self.function.setAttributeValue('Symmetries', self.iterable_to_string(value))
 
     @property
     def ToleranceEnergy(self):
@@ -86,41 +116,23 @@ class CrystalFieldMultiSite(object):
         self.function.setAttributeValue('ToleranceIntensity', float(value))
 
     @property
-    def IntensityScaling(self):
-        iscaling = []
-        for i in range(self.NumberOfSpectra):
-            paramName = 'IntensityScaling%s' % i
-            iscaling.append(self.function.getParameterValue(paramName))
-        return iscaling
+    def Temperatures(self):
+        return list(self.function.getAttributeValue("Temperatures"))
 
-    @IntensityScaling.setter
-    def IntensityScaling(self, value):
-        n = self.NumberOfSpectra
-        if len(value) != n:
-            raise ValueError('IntensityScaling is expected to be a list of %s values' % n)
-        for i in range(n):
-            paramName = 'IntensityScaling%s' % i
-            self.function.setParameter(paramName, value[i])
-
-    @property
-    def Temperature(self):
-        return self.function.getAttributeValue("Temperatures")
-
-    @Temperature.setter
-    def Temperature(self, value):
+    @Temperatures.setter
+    def Temperatures(self, value):
         self.function.setAttributeValue('Temperatures', value)
 
     @property
-    def FWHM(self):
-        attrName = 'FWHMs'
-        fwhm = self.function.getAttributeValue(attrName)
-        nDatasets = len(self.Temperature)
+    def FWHMs(self):
+        fwhm = self.function.getAttributeValue('FWHMs')
+        nDatasets = len(self.Temperatures)
         if len(fwhm) != nDatasets:
             return list(fwhm) * nDatasets
-        return fwhm
+        return list(fwhm)
 
-    @FWHM.setter
-    def FWHM(self, value):
+    @FWHMs.setter
+    def FWHMs(self, value):
         if len(value) == 1:
             value = value[0]
             value = [value] * self.NumberOfSpectra
@@ -150,8 +162,46 @@ class CrystalFieldMultiSite(object):
                                 (self.NumberOfSpectra, self._resolutionModel.NumberOfSpectra))
         for i in range(self.NumberOfSpectra):
             model = self._resolutionModel.model[i]
-            self.function.setAttributeValue('FWHMX%s' % i, model[0])
-            self.function.setAttributeValue('FWHMY%s' % i, model[1])
+            for i in range(self.NumberOfSpectra):
+                model = self._resolutionModel.model[i]
+                # self.function.setAttributeValue('FWHMX%s' % i, model[0])
+                # self.function.setAttributeValue('FWHMY%s' % i, model[1])
+
+    @property
+    def PhysicalProperty(self):
+        return self._physprop
+
+    @PhysicalProperty.setter
+    def PhysicalProperty(self, value):
+        from .function import PhysicalProperties
+        vlist = value if islistlike(value) else [value]
+        if all([isinstance(pp, PhysicalProperties) for pp in vlist]):
+            nOldPP = len(self._physprop) if islistlike(self._physprop) else (0 if self._physprop is None else 1)
+            self._physprop = value
+        else:
+            errmsg = 'PhysicalProperty input must be a PhysicalProperties'
+            errmsg += ' instance or a list of such instances'
+            raise ValueError(errmsg)
+        # If a spectrum (temperature) is already defined, or multiple physical properties
+        # given, redefine the CrystalFieldMultiSpectrum function.
+        if not self.isPhysicalPropertyOnly or islistlike(self.PhysicalProperty):
+            tt = self.Temperature if islistlike(self.Temperature) else [self.Temperature]
+            ww = list(self.FWHM) if islistlike(self.FWHM) else [self.FWHM]
+            # Last n-set of temperatures correspond to PhysicalProperties
+            if nOldPP > 0:
+                tt = tt[:-nOldPP]
+            # Removes 'negative' temperature, which is a flag for no INS dataset
+            tt = [val for val in tt if val > 0]
+            pptt = [0 if val.Temperature is None else val.Temperature for val in vlist]
+            self._remakeFunction(list(tt) + pptt)
+            if len(tt) > 0 and len(pptt) > 0:
+                ww += [0] * len(pptt)
+            self.FWHM = ww
+            ppids = [pp.TypeID for pp in vlist]
+            self.function.setAttributeValue('PhysicalProperties', [0] * len(tt) + ppids)
+            for attribs in [pp.getAttributes(i + len(tt)) for i, pp in enumerate(vlist)]:
+                for item in attribs.items():
+                    self.function.setAttributeValue(item[0], item[1])
 
     @property
     def FixAllPeaks(self):
@@ -171,7 +221,7 @@ class CrystalFieldMultiSite(object):
 
     @property
     def NumberOfSpectra(self):
-        return self.function.getNumberDomains()
+        return len(self.Temperatures)
 
     @property
     def NPeaks(self):
--- scripts/test/CrystalFieldMultiSiteTest.py
+++ scripts/test/CrystalFieldMultiSiteTest.py
@@ -1,15 +1,22 @@
 import unittest
-from CrystalField import CrystalFieldMultiSite
+from CrystalField.CrystalFieldMultiSite import CrystalFieldMultiSite
+
 
 class CrystalFieldMultiSiteTests(unittest.TestCase):
 
-    def test_ion_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'))
-        self.assertEqual(cfms.Ions, ('Pm', 'Eu'))
+    def test_init_single_ion(self):
+        cfms = CrystalFieldMultiSite(Ions='Pm', Symmetries='D2', Temperatures=[20,52], FWHMs=[1.0, 1.0])
+        self.assertEqual(cfms.Ions, ['Pm'])
+        self.assertEqual(cfms.Symmetries, ['D2'])
+        self.assertEqual(cfms.Temperatures, [20,52])
+        self.assertEqual(cfms.FWHMs, [1.0, 1.0])
 
-    def test_symmetries_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'))
-        self.assertEqual(cfms.Symmetries, ('D2', 'C3v'))
+    def test_init_multiple_ions(self):
+        cfms = CrystalFieldMultiSite(Ions=('Pm', 'Eu'), Symmetries=('D2', 'C3v'), Temperatures=[20,52], FWHMs=[1.0, 1.0])
+        self.assertEqual(cfms.Ions, ['Pm', 'Eu'])
+        self.assertEqual(cfms.Symmetries, ['D2', 'C3v'])
+        self.assertEqual(cfms.Temperatures, [20,52])
+        self.assertEqual(cfms.FWHMs, [1.0, 1.0])
 
     def test_toleranceEnergy_property(self):
         cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), ToleranceEnergy=1.0)
@@ -19,30 +26,31 @@ class CrystalFieldMultiSiteTests(unittest.TestCase):
         cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), ToleranceIntensity=6.0)
         self.assertEqual(cfms.ToleranceIntensity, 6.0)
 
-    def test_intensityScaling_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), IntensityScaling0=1.0, IntensityScaling1=1.0)
-        self.assertEqual(cfms.ToleranceIntensity, [1.0, 1.0])
-
     def test_NPeaks_property(self):
         cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), NPeaks=11)
         self.assertEqual(cfms.NPeaks, 11)
 
-    def test_temperature_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), Temperature=[44.0, 50.0])
-        self.assertEqual(cfms.Temperature, [44.0, 50.0])
-
-    def test_FWHM_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), FWHM=[1.0, 1.0])
-        self.assertEqual(cfms.FWHM, [1.0, 1.0])
-
     def test_ResolutionModel_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), ResolutionModel=([0, 50], [1, 2]))
-        self.assertEqual(cfms.ResolutionModel, ([0, 50], [1, 2]))
+        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C2'), ResolutionModel=[([0, 50], [1, 2]), ([0, 51], [3, 4])])
+        self.assertEqual(cfms.ResolutionModel.NumberOfSpectra, 2)
 
     def test_fixAllPeaks_property(self):
         cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), FixAllPeaks=True)
         self.assertTrue(cfms.FixAllPeaks)
 
     def test_peakShape_property(self):
-        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), peakShape='Lorentzian')
-        self.assertEqual(cfms.PeakShape, 'Lorentzian')
+        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), PeakShape='Gaussian')
+        self.assertEqual(cfms.PeakShape, 'Gaussian')
+
+    def test_FWHM_variation_property(self):
+        cfms = CrystalFieldMultiSite(('Pm', 'Eu'), ('D2', 'C3v'), FWHMVariation=0.3)
+        self.assertEqual(cfms.FWHMVariation, 0.3)
+
+    # def test_init_parameters(self):
+    #     cfms = CrystalFieldMultiSite(Ions=('Pm', 'Eu'), Symmetries=('D2', 'C3v'), Temperatures=[20, 52],
+    #                                  FWHMs=[1.0, 1.0], parameters={'B40': -0.02, 'B42': -0.11, 'f0.B44': -0.12})
+
+    # def test_physical_properties(self):
+    #     from CrystalField import PhysicalProperties
+    #     cfms = CrystalFieldMultiSite(Ions=('Pm', 'Eu'), Symmetries=('D2', 'C3v'), Temperatures=[20, 52],
+    #                                       FWHMs=[1.0, 1.0], PhysicalProperty=[PhysicalProperties('susc', 'powder'), PhysicalProperties('M(H)', Hdir=[0,1,0])])
