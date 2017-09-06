cb18c03c86aca911782e718a9b18d37187f1f620
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-09-06 12:05:06 +0100

    Re #19684 Fixed merge conflict in scripts

--- scripts/reduction/__init__.py
+++ scripts/reduction/__init__.py
@@ -1,1 +1,4 @@
+from __future__ import (absolute_import, division, print_function)
+from reduction.reducer import *
+from reduction.instrument import *
 from reduction.find_data import find_data, find_file
