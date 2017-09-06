e5ad61cd121e34e541cd576c8828766bdcd8268e
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2017-09-06 17:02:06 +0100

    Apply clang format patch. Re #19341

--- Framework/CurveFitting/test/Functions/CrystalFieldMultiSpectrumTest.h
+++ Framework/CurveFitting/test/Functions/CrystalFieldMultiSpectrumTest.h
@@ -118,12 +118,11 @@ public:
   }
 
   void test_evaluate() {
-    auto funStr =
-        "name=CrystalFieldMultiSpectrum,Ion=Ce,Temperatures=(44, "
-        "50),ToleranceIntensity=0.001,B20=0.37737,B22=3.9770,"
-        "B40=-0.031787,B42=-0.11611,B44=-0.12544,"
-        "f0.f1.FWHM=1.6,f0.f2.FWHM=2.0,f0.f3.FWHM=2.3,f1.f1.FWHM=1.6,"
-        "f1.f2.FWHM=2.0,f1.f3.FWHM=2.3";
+    auto funStr = "name=CrystalFieldMultiSpectrum,Ion=Ce,Temperatures=(44, "
+                  "50),ToleranceIntensity=0.001,B20=0.37737,B22=3.9770,"
+                  "B40=-0.031787,B42=-0.11611,B44=-0.12544,"
+                  "f0.f1.FWHM=1.6,f0.f2.FWHM=2.0,f0.f3.FWHM=2.3,f1.f1.FWHM=1.6,"
+                  "f1.f2.FWHM=2.0,f1.f3.FWHM=2.3";
     auto ws = createWorkspace();
     auto alg = AlgorithmFactory::Instance().create("EvaluateFunction", -1);
     alg->initialize();
