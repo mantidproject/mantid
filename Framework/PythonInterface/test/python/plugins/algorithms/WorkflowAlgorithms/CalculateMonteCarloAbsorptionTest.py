22bba5ea782c586e0a6f9a608e7e961ba09e9c33
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2017-09-07 11:14:52 +0100

    Ensure tests initialize properties correctly
    Refs #20184

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CalculateMonteCarloAbsorptionTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CalculateMonteCarloAbsorptionTest.py
@@ -6,28 +6,33 @@ import unittest
 
 class CalculateMonteCarloAbsorptionTest(unittest.TestCase):
 
+    _red_ws = None
+    _container_ws = None
+    _indirect_elastic_ws = None
+
     def setUp(self):
         red_ws = Load('irs26176_graphite002_red.nxs')
         self._red_ws = red_ws
+        container_ws = Load('irs26173_graphite002_red.nxs')
+        self._container_ws = container_ws
 
-        self._arguments = {'SampleChemicalFormula': 'H2-O',
+        self._arguments = {'SampleWorkspace': self._red_ws,
+                           'SampleChemicalFormula': 'H2-O',
                            'SampleDensityType': 'Mass Density',
                            'SampleDensity': 1.0,
                            'EventsPerPoint': 200,
                            'BeamHeight': 3.5,
                            'BeamWidth': 4.0,
                            'Height': 2.0 }
+        self._test_arguments = dict()
 
     def tearDown(self):
         DeleteWorkspace(self._red_ws)
 
     def _setup_container(self):
-        container_ws = Load('irs26173_graphite002_red.nxs')
-        self._container_ws = container_ws
-
         container_args = {'ContainerWorkspace':self._container_ws,
                           'ContainerChemicalFormula':'Al',
-                          'ContainerDensityType':'MassDensity',
+                          'ContainerDensityType':'Mass Density',
                           'ContainerDensity':1.0,
                           'ContainerFrontThickness':1.5,
                           'ContainerBackThickness':1.5 }
@@ -38,7 +43,7 @@ class CalculateMonteCarloAbsorptionTest(unittest.TestCase):
 
     def _test_corrections_workspace(self, corr_ws):
         x_unit = corr_ws.getAxis(0).getUnit().unitID()
-        self.assertEquals(x_unit, 'Wavelength')
+        self.assertEquals(x_unit, self._sample_unit)
 
         y_unit = corr_ws.YUnitLabel()
         self.assertEquals(y_unit, 'Attenuation factor')
@@ -49,11 +54,19 @@ class CalculateMonteCarloAbsorptionTest(unittest.TestCase):
         blocksize = corr_ws.blocksize()
         self.assertEquals(blocksize, 1905)
 
+    def _test_corrections_workspaces(self, workspaces):
+        self.assertNotEquals(workspaces, None)
+
+        for workspace in workspaces:
+            self._test_corrections_workspace(workspace)
+
     def _run_correction_and_test(self, shape):
-        corrected = CalculateMonteCarloAbsorption(SampleWorkspace=self._red_ws,
-                                                  Shape=shape,
-                                                  **self._arguments)
-        self._test_corrections_workspace(corrected)
+        arguments = self._arguments.copy()
+        arguments.update(self._test_arguments)
+        self._sample_unit = arguments['SampleWorkspace'].getAxis(0).getUnit().unitID()
+        corrected = CalculateMonteCarloAbsorption(Shape=shape,
+                                                  **arguments)
+        self._test_corrections_workspaces(corrected)
 
     def _run_correction_with_container_test(self, shape):
         self._setup_container()
@@ -64,13 +77,17 @@ class CalculateMonteCarloAbsorptionTest(unittest.TestCase):
         self._run_correction_and_test(shape)
 
     def _flat_plate_test(self, test_func):
-        self._arguments['SampleWidth'] = 2.0
-        self._arguments['SampleThickness'] = 2.0
-        test_func('Flat Plate')
+        self._test_arguments['SampleWidth'] = 2.0
+        self._test_arguments['SampleThickness'] = 2.0
+        test_func('FlatPlate')
 
     def _annulus_test(self, test_func, shape='Annulus'):
-        self._arguments['InnerRadius'] = 2.0
-        self._arguments['OuterRadius'] = 2.0
+        self._test_arguments.clear()
+        self._test_arguments['SampleRadius'] = 0.2
+        self._test_arguments['SampleInnerRadius'] = 1.0
+        self._test_arguments['SampleOuterRadius'] = 1.5
+        self._test_arguments['ContainerInnerRadius'] = 0.5
+        self._test_arguments['ContainerOuterRadius'] = 2.0
         test_func(shape)
 
     def _cylinder_test(self, test_func):
