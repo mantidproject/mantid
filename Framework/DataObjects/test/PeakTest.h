e3ecc260d7852b07242d7452eb278d2e6916a403
Steven Hahn <hahnse@ornl.gov>
2017-09-08 17:20:53 -0400

    Fix warnings from clang 5.0.

--- Framework/DataObjects/test/PeakTest.h
+++ Framework/DataObjects/test/PeakTest.h
@@ -467,7 +467,7 @@ public:
     // test with & without extended detector space
     // extended space is a sphere, so all points should fall radius*detector
     // direction away from the detector direction with extended space
-    auto testTheta = [this, &sphereInst, &radius](const double theta) {
+    auto testTheta = [this, &sphereInst](const double theta) {
       const auto expectedDir = V3D(sin(theta), 0., cos(theta));
 
       // test without extended detector space
