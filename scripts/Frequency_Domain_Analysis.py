ed49e64bee411a41020f754f45749867692a38d9
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-09-11 13:35:44 +0100

    refs #20233 made main gui file merge friendly 1

--- scripts/Frequency_Domain_Analysis.py
+++ scripts/Frequency_Domain_Analysis.py
@@ -3,6 +3,8 @@ from __future__ import (absolute_import, division, print_function)
 import PyQt4.QtGui as QtGui
 import sys
 
+#from muon import FFT_view
+#from muon import FFT_presenter
 from Muon import view_constructor
 from Muon import model_constructor
 from Muon import transform_view
@@ -11,6 +13,11 @@ from Muon import transform_presenter
 class frequencyDomainAnalysisGui(QtGui.QMainWindow):
     def __init__(self,parent=None):
         super(frequencyDomainAnalysisGui,self).__init__(parent)
+
+        #      view =FFT_view.FFTView(self)
+        #        self.presenter =FFT_presenter.FFTPresenter(view) #the main ui class in this file is called MainWindow
+        #        self.setCentralWidget(view)
+        #        self.setWindowTitle("Frequency Domain Analysis")
         groupedViews = view_constructor.viewConstructor(True,self)
         groupedModels = model_constructor.modelConstructor(True)
         view =transform_view.transformView(groupedViews,self)
@@ -27,7 +34,7 @@ def qapp():
     return _app
 
 app = qapp()
-ex= frequencyDomainAanalysisGui()
+ex= frequencyDomainAanlysisGui()
 ex.resize(700,700)
 ex.show()
 app.exec_()
