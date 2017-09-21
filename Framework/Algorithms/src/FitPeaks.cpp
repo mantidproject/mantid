6df120fa40abbc35905d0387727e19434ba1d56c
Wenduo Zhou <zhouw@ornl.gov>
2017-09-21 13:46:59 -0400

    Refs #20565. Fixed clang-format.

--- Framework/Algorithms/src/FitPeaks.cpp
+++ Framework/Algorithms/src/FitPeaks.cpp
@@ -189,7 +189,7 @@ void FitPeaks::processInputs() {
 
 void FitPeaks::fitPeaks() {
 
-    // cppcheck-suppress syntaxError
+  // cppcheck-suppress syntaxError
     PRAGMA_OMP(parallel for schedule(dynamic, 1) )
     for (size_t wi = m_startWorkspaceIndex; wi < m_stopWorkspaceIndex; ++wi) {
 
