426bd6c2b76da72727959ed7c7e3ca610005572a
Anthony Lim <anthony.lim@stfc.ac.uk>
2017-09-21 13:39:51 +0100

    refs #20550 removed duplication of functions in muon analysis

--- qt/scientific_interfaces/Muon/MuonAnalysisFitFunctionPresenter.cpp
+++ qt/scientific_interfaces/Muon/MuonAnalysisFitFunctionPresenter.cpp
@@ -43,8 +43,6 @@ void MuonAnalysisFitFunctionPresenter::doConnect() {
     connect(fitBrowser, SIGNAL(errorsEnabled(bool)), this,
             SLOT(handleErrorsEnabled(bool)));
     connect(fitBrowser, SIGNAL(fitUndone()), this, SLOT(handleFitFinished()));
-    connect(fitBrowser, SIGNAL(functionLoaded(const QString &)), this,
-            SLOT(handleFunctionLoaded(const QString &)));
     connect(fitBrowser, SIGNAL(workspacesToFitChanged(int)), this,
             SLOT(updateNumberOfDatasets(int)));
     connect(fitBrowser, SIGNAL(userChangedDatasetIndex(int)), this,
@@ -174,16 +172,6 @@ void MuonAnalysisFitFunctionPresenter::handleErrorsEnabled(bool enabled) {
   m_funcBrowser->setErrorsEnabled(enabled);
 }
 
-/**
- * Called when a saved setup is loaded into the fit property browser.
- * Update the function browser with this loaded function.
- * @param funcString :: [input] Loaded function as a string
- */
-void MuonAnalysisFitFunctionPresenter::handleFunctionLoaded(
-    const QString &funcString) {
-  m_funcBrowser->clear();
-  m_funcBrowser->setFunction(funcString);
-}
 
 /**
  * Called when the number of datasets to fit is changed in the model.
