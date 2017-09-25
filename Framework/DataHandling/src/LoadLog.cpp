efa448fe5a435790c373de644a6c25727d2bc509
Samuel Jackson <samueljackson@outlook.com>
2017-09-25 19:21:48 +0100

    Refs #20515 Suppress cppcheck warning

--- Framework/DataHandling/src/LoadLog.cpp
+++ Framework/DataHandling/src/LoadLog.cpp
@@ -416,6 +416,7 @@ LoadLog::kind LoadLog::classify(const std::string &s) const {
     // try and get stold to parse a number out of the string
     // if this throws then we don't have a number
     try {
+      // cppcheck-suppress ignoredReturnValue
       std::stold(str);
       return true;
     } catch (const std::invalid_argument &e) {
