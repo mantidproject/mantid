945d012dbfb0aa4402a2e1c57d67815e65d289ce
Martyn Gigg <martyn.gigg@gmail.com>
2017-09-27 10:04:40 +0100

    Do not warn about missing workspace on saving too.
    Refs #20610

--- qt/scientific_interfaces/Indirect/Elwin.cpp
+++ qt/scientific_interfaces/Indirect/Elwin.cpp
@@ -518,7 +518,7 @@ void Elwin::saveClicked() {
     addSaveWorkspaceToQueue(workspaceBaseName + "_elf");
 
   if (checkADSForPlotSaveWorkspace((workspaceBaseName + "_elt").toStdString(),
-                                   false))
+                                   false, false))
     addSaveWorkspaceToQueue(workspaceBaseName + "_elt");
 
   m_batchAlgoRunner->executeBatchAsync();
