450f97f3e8d0e3842d489e5dbeebedf80a8d4e1e
Pete Peterson <petersonpf@ornl.gov>
2017-09-28 09:20:20 -0400

    Undid formatting changes

--- Testing/Tools/cxxtest/cxxtest/XmlFormatter.h
+++ Testing/Tools/cxxtest/cxxtest/XmlFormatter.h
@@ -23,18 +23,18 @@
 #define CXXTEST_STACK_TRACE_FILELINE_PREFIX "\" location=\""
 #define CXXTEST_STACK_TRACE_FILELINE_SUFFIX ""
 
-#include <chrono>
-#include <cstring>
-#include <ctime>
-#include <cxxtest/ErrorFormatter.h>
-#include <cxxtest/StdHeaders.h>
-#include <cxxtest/TestListener.h>
 #include <cxxtest/TestRunner.h>
+#include <cxxtest/TestListener.h>
 #include <cxxtest/TestTracker.h>
 #include <cxxtest/ValueTraits.h>
+#include <cxxtest/ErrorFormatter.h>
+#include <cxxtest/StdHeaders.h>
+#include <chrono>
 #include <iostream>
 #include <sstream>
+#include <cstring>
 #include <stdexcept>
+
 
 namespace CxxTest
 {
@@ -137,16 +137,16 @@ namespace CxxTest
             std::map<std::string,std::string>::iterator curr=attribute.begin();
             std::map<std::string,std::string>::iterator end =attribute.end();
             while (curr != end) {
-              os << curr->first.c_str() << "=\"" << curr->second.c_str()
-                 << "\" ";
+              os << curr->first.c_str()
+                 << "=\"" << curr->second.c_str() << "\" ";
               curr++;
               }
             if (value.str().empty()) {
                 os << "/>";
             }
             else {
-              os << ">" << escape(value.str()).c_str() << "</" << name.c_str()
-                 << ">";
+                os << ">" << escape(value.str()).c_str()
+                   << "</" << name.c_str() << ">";
             }
             os.endl(os);
             }
@@ -256,28 +256,31 @@ namespace CxxTest
     class XmlFormatter : public TestListener
     {
         public:
-          XmlFormatter(OutputStream *o, OutputStream *ostr,
-                       std::ostringstream *os)
-              : cpuStartTime(0), cpuStopTime(0), _o(o), _ostr(ostr), _os(os),
-                stream_redirect(NULL) {}
+        XmlFormatter( OutputStream *o, OutputStream *ostr, std::ostringstream *os)
+           : cpuStartTime(0), cpuStopTime(0), _o(o), _ostr(ostr), _os(os), stream_redirect(NULL)
+        {}
 
-          virtual ~XmlFormatter() { delete _ostr; }
+        virtual ~XmlFormatter()
+        {
+          delete _ostr;
+        }
 
-          std::list<TestCaseInfo> info;
-          std::list<TestCaseInfo>::iterator testcase;
-          typedef std::list<ElementInfo>::iterator element_t;
-          std::string classname;
-          int ntests;
-          int nfail;
-          int nerror;
-          double totaltime;
-          std::chrono::time_point<std::chrono::high_resolution_clock>
-              testStartTime, testStopTime, testRunStartTime, testRunStopTime;
+        std::list<TestCaseInfo> info;
+        std::list<TestCaseInfo>::iterator testcase;
+        typedef std::list<ElementInfo>::iterator element_t;
+        std::string classname;
+        int ntests;
+        int nfail;
+        int nerror;
+        double totaltime;
+        std::chrono::time_point<std::chrono::high_resolution_clock>
+        testStartTime, testStopTime, testRunStartTime, testRunStopTime;
 
-          /// CPU time (for all processors)
-          clock_t cpuStartTime, cpuStopTime;
+        /// CPU time (for all processors)
+        clock_t cpuStartTime, cpuStopTime;
 
-          int run() {
+        int run()
+        {
             TestRunner::runAllTests( *this );
             return tracker().failedTests();
         }
@@ -294,7 +297,7 @@ namespace CxxTest
         {
             char s[WorldDescription::MAX_STRLEN_TOTAL_TESTS];
             const WorldDescription &wd = tracker().world();
-            o << wd.strTotalTests(s)
+            o << wd.strTotalTests( s )
               << (wd.numTotalTests() == 1 ? " test" : " tests");
         }
 
@@ -309,7 +312,7 @@ namespace CxxTest
                 while ( ! classname.empty() && classname[0] == '.' )
                    classname.erase(0,1);
 
-                // CXXTEST_STD(cout) << "HERE " << desc.file() << " "
+                //CXXTEST_STD(cout) << "HERE " << desc.file() << " "
                 //                  << classname << CXXTEST_STD(endl);
 
                 //classname=desc.suiteName();
@@ -340,19 +343,19 @@ namespace CxxTest
 
         void enterTest( const TestDescription & desc )
         {
-          testStartTime = std::chrono::high_resolution_clock::now();
-          testcase = info.insert(info.end(), TestCaseInfo());
-          testcase->testName = desc.testName();
-          testcase->className = classname;
-          std::ostringstream os;
-          os << desc.line();
-          testcase->line = os.str();
+           testStartTime = std::chrono::high_resolution_clock::now();
+           testcase = info.insert(info.end(), TestCaseInfo());
+           testcase->testName = desc.testName();
+           testcase->className = classname;
+           std::ostringstream os;
+           os << desc.line();
+           testcase->line = os.str();
 
-          if (stream_redirect)
-            CXXTEST_STD(cerr)
-                << "ERROR: The stream_redirect != NULL" << CXXTEST_STD(endl);
+           if ( stream_redirect )
+              CXXTEST_STD(cerr) << "ERROR: The stream_redirect != NULL"
+                                << CXXTEST_STD(endl);
 
-          stream_redirect =
+           stream_redirect =
               new TeeOutputStreams(CXXTEST_STD(cout), CXXTEST_STD(cerr));
         }
 
@@ -427,10 +430,11 @@ namespace CxxTest
           timeStream << totaltime;
                 (*_o) << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" << endl;
                 (*_o) << "<testsuite name=\"" << desc.worldName() << "\" ";
-                (*_o) << " tests=\"" << ntests << "\" errors=\"" << nerror
-                      << "\" failures=\"" << nfail << "\" package=\""
-                      << desc.worldName() << "\" time=\""
-                      << timeStream.str().c_str() << "\" >";
+                (*_o) << " tests=\"" << ntests
+                      << "\" errors=\"" << nerror
+                      << "\" failures=\"" << nfail
+                      << "\" package=\"" << desc.worldName()
+                      << "\" time=\"" << timeStream.str().c_str() << "\" >";
                 _o->endl(*_o);
                 (*_o) << _os->str().c_str();
                 _os->clear();
@@ -462,8 +466,8 @@ namespace CxxTest
 
         void failedAssert( const char *file, unsigned line, const char *expression )
         {
-          testFailure(file, line, "failedAssert")
-              << "Assertion failed: " << expression;
+            testFailure( file, line, "failedAssert" )
+               << "Assertion failed: " << expression;
         }
 
         void failedAssertEquals( const char *file, unsigned line,
@@ -480,10 +484,10 @@ namespace CxxTest
                                    const char *xStr, const char *yStr, const char *sizeStr,
                                    const void* /*x*/, const void* /*y*/, unsigned size )
         {
-          testFailure(file, line, "failedAssertSameData")
-              << "Error: Expected " << sizeStr << " (" << size
-              << ")  bytes to be equal at (" << xStr << ") and (" << yStr
-              << "), found";
+            testFailure( file, line, "failedAssertSameData")
+               << "Error: Expected " << sizeStr
+               << " (" << size << ")  bytes to be equal at ("
+               << xStr << ") and (" << yStr << "), found";
         }
 
         void failedAssertSameFiles( const char *file, unsigned line,
@@ -499,18 +503,21 @@ namespace CxxTest
                                 const char *xStr, const char *yStr, const char *dStr,
                                 const char *x, const char *y, const char *d )
         {
-          testFailure(file, line, "failedAssertDelta")
-              << "Error: Expected (" << xStr << " == " << yStr << ") up to "
-              << dStr << " (" << d << "), found (" << x << " != " << y << ")";
+            testFailure( file, line, "failedAssertDelta" )
+               << "Error: Expected ("
+               << xStr << " == " << yStr << ") up to " << dStr
+               << " (" << d << "), found ("
+               << x << " != " << y << ")";
         }
 
         void failedAssertDiffers( const char *file, unsigned line,
                                   const char *xStr, const char *yStr,
                                   const char *value )
         {
-          testFailure(file, line, "failedAssertDiffers")
-              << "Error: Expected (" << xStr << " != " << yStr << "), found ("
-              << value << ")";
+            testFailure( file, line, "failedAssertDiffers" )
+               << "Error: Expected ("
+               << xStr << " != " << yStr << "), found ("
+               << value << ")";
         }
 
         void failedAssertLessThan( const char *file, unsigned line,
@@ -537,9 +544,10 @@ namespace CxxTest
                                    const char *relation, const char *xStr, const char *yStr,
                                    const char *x, const char *y )
         {
-          testFailure(file, line, "failedAssertRelation")
-              << "Error: Expected " << relation << "( " << xStr << ", " << yStr
-              << " ), found !" << relation << "( " << x << ", " << y << " )";
+            testFailure( file, line, "failedAssertRelation" )
+               << "Error: Expected " << relation << "( " <<
+               xStr << ", " << yStr << " ), found !" << relation
+               << "( " << x << ", " << y << " )";
         }
 
         void failedAssertPredicate( const char *file, unsigned line,
@@ -554,17 +562,17 @@ namespace CxxTest
                                  const char *expression, const char *type,
                                  bool otherThrown )
         {
-          testFailure(file, line, "failedAssertThrows")
-              << "Error: Expected (" << expression << ") to throw (" << type
-              << ") but it "
-              << (otherThrown ? "threw something else" : "didn't throw");
+            testFailure( file, line, "failedAssertThrows" )
+               << "Error: Expected (" << expression << ") to throw ("  <<
+               type << ") but it "
+               << (otherThrown ? "threw something else" : "didn't throw");
         }
 
         void failedAssertThrowsNot( const char *file, unsigned line, const char *expression )
         {
-          testFailure(file, line, "failedAssertThrowsNot")
-              << "Error: Expected (" << expression
-              << ") not to throw, but it did";
+            testFailure( file, line, "failedAssertThrowsNot" )
+               << "Error: Expected (" << expression
+               << ") not to throw, but it did";
         }
 
     protected:
