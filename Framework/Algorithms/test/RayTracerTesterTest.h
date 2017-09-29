56967c6b251436fb0d9d6ea4a42778f3a44b31a6
Owen Arnold <owen.arnold@stfc.ac.uk>
2017-09-29 13:16:34 +0100

    refs #20720. Make and use local cache
    InstrumentRayTracer now has local cache from which to fetch bounding boxes.

--- Framework/Algorithms/test/RayTracerTesterTest.h
+++ Framework/Algorithms/test/RayTracerTesterTest.h
@@ -4,38 +4,14 @@
 #include <cxxtest/TestSuite.h>
 #include "MantidKernel/Timer.h"
 #include "MantidKernel/System.h"
-
 #include "MantidAlgorithms/RayTracerTester.h"
 
 using namespace Mantid;
 using namespace Mantid::Algorithms;
 using namespace Mantid::API;
 
-class RayTracerTesterTest : public CxxTest::TestSuite {
-public:
-  // This pair of boilerplate methods prevent the suite being created statically
-  // This means the constructor isn't called when running other tests
-  static RayTracerTesterTest *createSuite() {
-    return new RayTracerTesterTest();
-  }
-  static void destroySuite(RayTracerTesterTest *suite) { delete suite; }
-
-  void test_Init() {
-    RayTracerTester alg;
-    TS_ASSERT_THROWS_NOTHING(alg.initialize())
-    TS_ASSERT(alg.isInitialized())
-  }
-
-  /** Disabled because this isn't a real algorithm, just a testing one */
-  void xtest_exec() {
-    RayTracerTester alg;
-    TS_ASSERT_THROWS_NOTHING(alg.initialize())
-    TS_ASSERT(alg.isInitialized())
-    alg.setPropertyValue("Filename", "CNCS_Definition.xml");
-    alg.setPropertyValue("OutputWorkspace", "cncs");
-    alg.exec();
-  }
-};
+// There are only performance tests here as this is not a real algorithm.
+// Functional tests disabled ec34e64616f34f1cf476b65f934272fdfda1212f
 
 class RayTracerTesterTestPerformance : public CxxTest::TestSuite {
 public:
