6f5d55d2c8aa484ed01a4d65d4a5cc6eda91056e
Jose Borreguero <borreguero@gmail.com>
2017-10-12 14:38:59 -0400

    Refs #20638 setup, teardown, and requiredfiles

--- Testing/SystemTests/tests/analysis/BASISDiffractionSystemTest.py
+++ Testing/SystemTests/tests/analysis/BASISDiffractionSystemTest.py
@@ -1,6 +1,7 @@
 from __future__ import (absolute_import, division, print_function)
 
 import stresstesting
+from mantid import config
 from mantid.simpleapi import BASISDiffraction
 
 
@@ -10,26 +11,56 @@ class OrientedSampleTest(stresstesting.MantidStressTest):
     of a crystal.
     """
 
+    def __init__(self):
+        super(OrientedSampleTest, self).__init__()
+        self.config = None
+        self.setUp()
+
+    def setUp(self):
+        self.config = {p: config[p] for p in ('default.facility',
+                                              'default.instrument',
+                                              'datasearch.directories')}
+        config['default.facility'] = 'SNS'
+        config['default.instrument'] = 'BASIS'
+        config.appendDataSearchSubDir('BASIS/BASISDiffraction')
+
+    def tearDown(self):
+        config.update(self.config)
+
+    def requiredFiles(self):
+        return ['BASIS_Mask_default_diff.xml',
+                'BSS_74799_event.nxs',
+                'BSS_74801_event.nxs',
+                'BSS_74802_event.nxs',
+                'BSS_74803_event.nxs',
+                'BSS_74804_event.nxs',
+                'BSS_64642_event.nxs',
+                'BSS_75527_event.nxs',
+                'BASISOrientedSample.nxs']
+
     def runTest(self):
         """
         Override parent method, does the work of running the test
         """
-        BASISDiffraction(SampleOrientation=True,
-                         RunNumbers='74799-74804',
-                         MaskFile='BASIS_Mask_default_diff.xml',
-                         VanadiumRuns='64642',
-                         BackgroundRun='75527',
-                         PsiAngleLog='SE50Rot',
-                         PsiOffset=-27.0,
-                         LatticeSizes=[10.71, 10.71, 10.71],
-                         LatticeAngles=[90.0, 90.0, 90.0],
-                         VectorU=[1, 1, 0],
-                         VectorV=[0, 0, 1],
-                         Uproj=[1, 1, 0],
-                         Vproj=[0, 0, 1],
-                         Wproj=[1, -1, 0],
-                         Nbins=300,
-                         OutputWorkspace='peaky')
+        try:
+            BASISDiffraction(SampleOrientation=True,
+                             RunNumbers='74799-74804',
+                             MaskFile='BASIS_Mask_default_diff.xml',
+                             VanadiumRuns='64642',
+                             BackgroundRun='75527',
+                             PsiAngleLog='SE50Rot',
+                             PsiOffset=-27.0,
+                             LatticeSizes=[10.71, 10.71, 10.71],
+                             LatticeAngles=[90.0, 90.0, 90.0],
+                             VectorU=[1, 1, 0],
+                             VectorV=[0, 0, 1],
+                             Uproj=[1, 1, 0],
+                             Vproj=[0, 0, 1],
+                             Wproj=[1, -1, 0],
+                             Nbins=300,
+                             OutputWorkspace='peaky')
+        finally:
+            self.tearDown()
 
     def validate(self):
         """
@@ -37,6 +68,5 @@ class OrientedSampleTest(stresstesting.MantidStressTest):
         compare to.
         :return: strings for workspace and file name
         """
-
         self.tolerance = 0.1
         return 'peaky', 'BASISOrientedSample.nxs'
