503a2b1260c083a7c1e1581722600a2e455b76af
Samuel Jackson <samueljackson@outlook.com>
2017-10-16 17:02:01 +0100

    Refs #20920 Update docstrings and light refactoring

--- Framework/PythonInterface/plugins/algorithms/SaveReflections.py
+++ Framework/PythonInterface/plugins/algorithms/SaveReflections.py
@@ -63,19 +63,40 @@ class SaveReflections(PythonAlgorithm):
 
 
 class FullprofFormat(object):
+    """Writes a PeaksWorkspace to an ASCII file in the format required
+    by the Fullprof crystallographic refinement program.
+
+    This is a 7 columns file format consisting of H, K, L, instensity,
+    sigma, crystal domain, and wavelength.
+    """
 
     def __call__(self, file_name, workspace):
+        """Write a PeaksWorkspace to an ASCII file using this formatter.
+
+        :param file_name: the file name to output data to.
+        :param workspace: the PeaksWorkspace to write to file.
+        """
         with open(file_name, 'w') as f_handle:
             self.write_header(f_handle, workspace)
             self.write_peaks(f_handle, workspace)
 
     def write_header(self, f_handle, workspace):
+        """Write the header of the Fullprof file format
+
+        :param f_handle: handle to the file to write to.
+        :param workspace: the PeaksWorkspace to save to file.
+        """
         f_handle.write(workspace.getTitle())
         f_handle.write("(3i4,2f12.2,i5,4f10.4)\n")
         f_handle.write("  0 0 0\n")
         f_handle.write("#  h   k   l      Fsqr       s(Fsqr)   Cod   Lambda\n")
 
     def write_peaks(self, f_handle, workspace):
+        """Write all the peaks in the workspace to file.
+
+        :param f_handle: handle to the file to write to.
+        :param workspace: the PeaksWorkspace to save to file.
+        """
         for i, peak in enumerate(workspace):
             data = (peak['h'],peak['k'],peak['l'],peak['Intens'],peak['SigInt'],i+1,peak['Wavelength'])
             line = "%4i%4i%4i%12.2f%12.2f%5i%10.4f\n" % data
@@ -85,51 +106,104 @@ class FullprofFormat(object):
 
 
 class JanaFormat(object):
+    """Writes a PeaksWorkspace to an ASCII file in the format required
+    by the Jana2006 crystallographic refinement program.
+
+    This is an 11 column file format consisting of H, K, L, intensity, sigma, 
+    crystal domain, wavelength, 2*theta, transmission, absorption weighted path length (Tbar),
+    and thermal diffuse scattering correction (TDS).
+
+    Currently the last three columns are shard coded to 1.0, 0.0, and 0.0 respectively.
+    """
 
     def __call__(self, file_name, workspace):
+        """Write a PeaksWorkspace to an ASCII file using this formatter.
+
+        :param file_name: the file name to output data to.
+        :param workspace: the PeaksWorkspace to write to file.
+        """
+        self._cache_instrument_params(workspace)
         with open(file_name, 'w') as f_handle:
             self.write_header(f_handle, workspace)
             self.write_peaks(f_handle, workspace)
 
-    def write_header(self, f_handle, ws):
-        sample = ws.sample()
+    def write_header(self, f_handle, workspace):
+        """Write the header of the Fullprof file format
+
+        :param f_handle: handle to the file to write to.
+        :param workspace: the PeaksWorkspace to save to file.
+        """
+        sample = workspace.sample()
         lattice = sample.getOrientedLattice()
         lattice_params = [lattice.a(), lattice.b(), lattice.c(), lattice.alpha(), lattice.beta(), lattice.gamma()]
         lattice_params = "".join(["{: >10.4f}".format(value) for value in lattice_params])
         f_handle.write("# Lattice parameters   {}\n".format(lattice_params))
         f_handle.write("(3i5,2f12.2,i5,4f10.4)\n")
 
-    def write_peaks(self, f_handle, ws):
+    def write_peaks(self, f_handle, workspace):
+        """Write all the peaks in the workspace to file.
+
+        :param f_handle: handle to the file to write to.
+        :param workspace: the PeaksWorkspace to save to file.
+        """
         column_names = ["h", "k", "l", "Fsqr", "s(Fsqr)", "Cod", "Lambda", "Twotheta", "Transm.", "Tbar", "TDS"]
         column_format = "#{:>4}{:>4}{:>4}{:>12}{:>12}{:>5}{:>10}{:>10}{:>10}{:>10}{:>10}\n"
         f_handle.write(column_format.format(*column_names))
-        for row in ws:
-            self.write_peak(f_handle, ws, row)
+        for row in workspace:
+            self.write_peak(f_handle, row)
 
-    def write_peak(self, f_handle, ws, peak):
+    def write_peak(self, f_handle, peak):
+        """Write a single Peak from the peaks workspace to file.
+
+        :param f_handle: handle to the file to write to.
+        :param peak: the peak object to write to the file.
+        """
         f_handle.write("{h: >5.0f}{k: >5.0f}{l: >5.0f}".format(**peak))
         f_handle.write("{Intens: >12.2f}".format(**peak))
         f_handle.write("{SigInt: >12.2f}".format(**peak))
         f_handle.write("{: >5.0f}".format(1))
         f_handle.write("{Wavelength: >10.4f}".format(**peak))
-        f_handle.write("{: >10.4f}".format(self._get_two_theta(ws, peak['DetID'])))
+        f_handle.write("{: >10.4f}".format(self._get_two_theta(peak['DetID'])))
         f_handle.write("{: >10.4f}{: >10.4f}{: >10.4f}".format(1.0, 0.0, 0.0))
         f_handle.write("\n")
 
-    def _get_two_theta(self, ws, det_id):
-        instrument = ws.getInstrument()
-        det = instrument.getDetector(det_id)
-        frame = instrument.getReferenceFrame()
-        z_axis = frame.vecPointingAlongBeam()
-        return np.degrees(det.getTwoTheta(instrument.getPos(), z_axis))
+    def _get_two_theta(self, det_id):
+        """Get the two theta value for this peak
+        :param det_id: the detector ID of this Peak
+        """
+        det = self._instrument.getDetector(det_id)
+        return np.degrees(det.getTwoTheta(self._instrument.getPos(), self._z_axis))
+
+    def _cache_instrument_params(self, workspace):
+        """Cache some parameters about the instrument
+
+        This stores some of the instrument parameters at the start
+        of execution so we can quickly access them later.
+
+        :param workspace: the PeaksWorkspace to cache instrument data from.
+        """
+        self._instrument = workspace.getInstrument()
+        frame = self._instrument.getReferenceFrame()
+        self._z_axis = frame.vecPointingAlongBeam()
 
 
 # ------------------------------------------------------------------------------------------------------
 
 
 class SaveHKLFormat(object):
+    """Writes a PeaksWorkspace to an ASCII file in the format output from
+    the SaveHKL algorithm.
+
+    The SaveHKL algorithm currently supports both the GSAS and SHELX formats. For
+    more information see the SaveHKL algorithm documentation.
+    """
 
     def __call__(self, file_name, workspace):
+        """Write a PeaksWorkspace to an ASCII file using this formatter.
+
+        :param file_name: the file name to output data to.
+        :param workspace: the PeaksWorkspace to write to file.
+        """
         SaveHKL(Filename=file_name, InputWorkspace=workspace, OutputWorkspace=workspace.name())
 
 
