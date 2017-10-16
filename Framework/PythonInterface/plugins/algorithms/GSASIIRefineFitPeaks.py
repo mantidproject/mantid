591f871be30a43b7e9884ab20b3fc704397f4fc2
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-10-16 12:05:13 +0100

    Re #19578 Enabled Pawley refinement in GSASIIRefineFitPeaks

--- Framework/PythonInterface/plugins/algorithms/GSASIIRefineFitPeaks.py
+++ Framework/PythonInterface/plugins/algorithms/GSASIIRefineFitPeaks.py
@@ -15,11 +15,15 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
     (https://subversion.xray.aps.anl.gov/trac/pyGSAS)
     """
 
-    PROP_GSAS_PROJ_PATH = "GSASProjectPath"
+    PROP_GROUP_PAWLEY_PARAMS = "Pawley Parameters"
+    PROP_GSAS_PROJ_PATH = "SaveGSASIIProjectFile"
     PROP_INPUT_WORKSPACE = "InputWorkspace"
-    PROP_PATH_TO_GSASII = "GSASIIPath"
-    PROP_PATH_TO_INST_PARAMS = "InstrumentParameterPath"
-    PROP_PATH_TO_PHASE = "PhasePath"
+    PROP_PATH_TO_GSASII = "PathToGSASII"
+    PROP_PATH_TO_INST_PARAMS = "InstrumentFile"
+    PROP_PATH_TO_PHASE = "PhaseInfoFile"
+    PROP_PAWLEY_DMIN = "PawleyDMin"
+    PROP_PAWLEY_NEGATIVE_WEIGHT = "PawleyNegativeWeight"
+    PROP_REFINEMENT_METHOD = "RefinementMethod"
     PROP_OUT_GOF = "GOF"
     PROP_OUT_GROUP_RESULTS = "Results"
     PROP_OUT_LATTICE_PARAMS = "LatticeParameters"
@@ -31,6 +35,7 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
                                                  "refine": True},
                                   "Sample Parameters": ["Scale"]}}
     LATTICE_TABLE_PARAMS = ["length_a", "length_b", "length_c", "angle_alpha", "angle_beta", "angle_gamma", "volume"]
+    REFINEMENT_METHODS = ["Pawley refinement", "Rietveld refinement", "Peak fitting"]
 
     def category(self):
         return "Diffraction\\Engineering;Diffraction\\Fitting"
@@ -43,66 +48,81 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
                 "using GSAS-II scriptable API")
 
     def PyInit(self):
-        self.declareProperty(FileProperty(name=self.PROP_PATH_TO_GSASII, defaultValue="", action=FileAction.Directory),
-                             doc="Path to the directory containing GSASII executable on the user's machine")
+        self.declareProperty(name=self.PROP_REFINEMENT_METHOD, defaultValue=self.REFINEMENT_METHODS[0],
+                             validator=StringListValidator(self.REFINEMENT_METHODS), direction=Direction.Input,
+                             doc="Refinement method (TODO)")
+
         self.declareProperty(WorkspaceProperty(name=self.PROP_INPUT_WORKSPACE, defaultValue="",
                                                direction=Direction.Input), doc="Workspace with spectra to fit peaks")
         self.declareProperty(name=self.PROP_WORKSPACE_INDEX, defaultValue=0, direction=Direction.Input,
                              doc="Index of the spectrum in InputWorkspace to fit. By default, the first spectrum "
                                  "(ie the only one for a focused workspace) is used")
-        self.declareProperty(FileProperty(name=self.PROP_PATH_TO_PHASE, defaultValue="", action=FileAction.Load,
-                                          extensions=[".cif"]), doc="Location of the phase file")
         self.declareProperty(FileProperty(name=self.PROP_PATH_TO_INST_PARAMS, defaultValue="", action=FileAction.Load,
                                           extensions=[".prm"]), doc="Location of the phase file")
+        self.declareProperty(FileProperty(name=self.PROP_PATH_TO_PHASE, defaultValue="", action=FileAction.Load,
+                                          extensions=[".cif"]), doc="Location of the phase file")
+        self.declareProperty(FileProperty(name=self.PROP_PATH_TO_GSASII, defaultValue="", action=FileAction.Directory),
+                             doc="Path to the directory containing GSASII executable on the user's machine")
 
+        self.declareProperty(ITableWorkspaceProperty(name=self.PROP_OUT_LATTICE_PARAMS, direction=Direction.Output,
+                                                     defaultValue=self.PROP_OUT_LATTICE_PARAMS),
+                             doc="Table to output the lattice parameters (refined)")
         self.declareProperty(FileProperty(name=self.PROP_GSAS_PROJ_PATH, defaultValue="", action=FileAction.Save,
                                           extensions=".gpx"), doc="GSASII Project to work on")
         self.declareProperty(name=self.PROP_OUT_GOF, defaultValue=0.0, direction=Direction.Output,
                              doc="Goodness of fit value (Chi squared)")
         self.declareProperty(name=self.PROP_OUT_RWP, defaultValue=0.0, direction=Direction.Output,
                              doc="Weight profile R-factor (Rwp) discrepancy index for the goodness of fit")
-        self.declareProperty(ITableWorkspaceProperty(name=self.PROP_OUT_LATTICE_PARAMS, direction=Direction.Output,
-                                                     defaultValue=self.PROP_OUT_LATTICE_PARAMS),
-                             doc="Table to output the lattice parameters (refined)")
 
+        self.setPropertyGroup(self.PROP_OUT_LATTICE_PARAMS, self.PROP_OUT_GROUP_RESULTS)
         self.setPropertyGroup(self.PROP_GSAS_PROJ_PATH, self.PROP_OUT_GROUP_RESULTS)
         self.setPropertyGroup(self.PROP_OUT_GOF, self.PROP_OUT_GROUP_RESULTS)
         self.setPropertyGroup(self.PROP_OUT_RWP, self.PROP_OUT_GROUP_RESULTS)
-        self.setPropertyGroup(self.PROP_OUT_LATTICE_PARAMS, self.PROP_OUT_GROUP_RESULTS)
+
+        self.declareProperty(name=self.PROP_PAWLEY_DMIN, defaultValue=1.0, direction=Direction.Input,
+                             doc="For Pawley refiment: as defined in GSAS-II, the minimum d-spacing to be used in a "
+                                 "Pawley refinement. Please refer to the GSAS-II documentation for full details.")
+        self.declareProperty(name=self.PROP_PAWLEY_NEGATIVE_WEIGHT, defaultValue=0.0, direction=Direction.Input,
+                             doc="For Pawley refinement: as defined in GSAS-II, the weight for a penalty function "
+                                 "applied during a Pawley refinement on resulting negative intensities. "
+                                 "Please refer to the GSAS-II documentation for full details.")
+
+        self.setPropertyGroup(self.PROP_PAWLEY_DMIN, self.PROP_GROUP_PAWLEY_PARAMS)
+        self.setPropertyGroup(self.PROP_PAWLEY_NEGATIVE_WEIGHT, self.PROP_GROUP_PAWLEY_PARAMS)
 
     def PyExec(self):
         gsas_proj = self._initialise_GSAS()
 
-        spectrum = self._extract_spectrum_from_workspace()
-        spectrum_path = self._save_temporary_fxye(spectrum=spectrum)
-        mantid.DeleteWorkspace(Workspace=spectrum)
+        refinement_method = self.getPropertyValue(self.PROP_REFINEMENT_METHOD)
+        if refinement_method == self.REFINEMENT_METHODS[0]:  # Rawley refinement
+            rwp, gof, lattice_params = self._run_rietveld_pawley_refinement(gsas_proj=gsas_proj, do_pawley=True,
+                                                                            histogram=gsas_proj.histograms()[0])
+        elif refinement_method == self.REFINEMENT_METHODS[1]:  # Rietveld refinement
+            rwp, gof, lattice_params = self._run_rietveld_pawley_refinement(gsas_proj=gsas_proj, do_pawley=False,
+                                                                            histogram=gsas_proj.histograms()[0])
+        else:  # Peak fitting
+            raise NotImplementedError("GSAS-II Peak fitting not yet implemented in Mantid")
 
-        inst_param_path = self.getPropertyValue(self.PROP_PATH_TO_INST_PARAMS)
-        histogram = gsas_proj.add_powder_histogram(datafile=spectrum_path, iparams=inst_param_path)
-
-        self._remove_temporary_fxye(spectrum_path=spectrum_path)
-
-        phase_path = self.getPropertyValue(self.PROP_PATH_TO_PHASE)
-        phase = gsas_proj.add_phase(phasefile=phase_path, histograms=[histogram])
-
-        gsas_proj.set_refinement(refinement=self.DEFAULT_REFINEMENT_PARAMS)
-        gsas_proj.do_refinements([{}])
-
-        covariance_rvals = gsas_proj.values()[2]["data"]["Rvals"]
-        self.setProperty(self.PROP_OUT_RWP, covariance_rvals["Rwp"])
-        self.setProperty(self.PROP_OUT_GOF, covariance_rvals["GOF"])
-
-        lattice_params = gsas_proj.phases()[0].get_cell()
-        lattice_param_table = self._build_output_lattice_table(lattice_params)
-        self.setProperty(self.PROP_OUT_LATTICE_PARAMS, lattice_param_table)
+        self._set_output_properties(rwp=rwp, gof=gof, lattice_params=lattice_params)
 
     def _initialise_GSAS(self):
         gsas_path = self.getPropertyValue(self.PROP_PATH_TO_GSASII)
         sys.path.append(gsas_path)
         import GSASIIscriptable as GSASII
 
-        gsas_proj = self.getPropertyValue(self.PROP_GSAS_PROJ_PATH)
-        return GSASII.G2Project(filename=gsas_proj)
+        gsas_proj_path = self.getPropertyValue(self.PROP_GSAS_PROJ_PATH)
+        gsas_proj = GSASII.G2Project(filename=gsas_proj_path)
+
+        spectrum = self._extract_spectrum_from_workspace()
+        spectrum_path = self._save_temporary_fxye(spectrum=spectrum)
+        mantid.DeleteWorkspace(Workspace=spectrum)
+
+        inst_param_path = self.getPropertyValue(self.PROP_PATH_TO_INST_PARAMS)
+        gsas_proj.add_powder_histogram(datafile=spectrum_path, iparams=inst_param_path)
+
+        self._remove_temporary_fxye(spectrum_path=spectrum_path)
+
+        return gsas_proj
 
     def _build_output_lattice_table(self, lattice_params):
         alg = self.createChildAlgorithm('CreateEmptyTableWorkspace')
@@ -136,6 +156,42 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
             os.remove(spectrum_path)
         except Exception as e:
             raise Warning("Couldn't remove temporary spectrum file at location \"{}\":\n{}".format(spectrum_path, e))
+
+    def _run_rietveld_pawley_refinement(self, gsas_proj, histogram, do_pawley):
+        phase_path = self.getPropertyValue(self.PROP_PATH_TO_PHASE)
+        phase = gsas_proj.add_phase(phasefile=phase_path, histograms=[histogram])
+
+        if do_pawley:
+            self._set_pawley_phase_parameters(phase)
+
+        gsas_proj.set_refinement(refinement=self.DEFAULT_REFINEMENT_PARAMS)
+        gsas_proj.do_refinements([{}])
+
+        residuals = gsas_proj.values()[2]["data"]["Rvals"]
+        lattice_params = gsas_proj.phases()[0].get_cell()
+        lattice_param_table = self._build_output_lattice_table(lattice_params)
+
+        return residuals["Rwp"], residuals["GOF"], lattice_param_table
+
+    def _set_output_properties(self, rwp, gof, lattice_params):
+        self.setProperty(self.PROP_OUT_RWP, rwp)
+        self.setProperty(self.PROP_OUT_GOF, gof)
+        self.setProperty(self.PROP_OUT_LATTICE_PARAMS, lattice_params)
+
+    def _set_pawley_phase_parameters(self, phase):
+        # Note from GSAS-II doc: "you probably should clear the Histogram scale factor refinement
+        # flag (found in Sample parameters for the powder data set) as it cannot be refined
+        # simultaneously with the Pawley reflection intensities"
+        phase.values()[2].values()[0]["Scale"] = [1.0, False]
+
+        phase_params = phase.values()[4]
+        phase_params["doPawley"] = True
+
+        pawley_dmin = self.getPropertyValue(self.PROP_PAWLEY_DMIN)
+        phase_params["Pawley dmin"] = pawley_dmin
+
+        pawley_neg_wt = self.getPropertyValue(self.PROP_PAWLEY_NEGATIVE_WEIGHT)
+        phase_params["Pawley neg wt"] = pawley_neg_wt
 
 
 AlgorithmFactory.subscribe(GSASIIRefineFitPeaks)
