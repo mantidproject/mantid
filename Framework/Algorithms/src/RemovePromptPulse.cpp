566f7b3ef9c39629b73648f6dd67e877875c7f12
Andrei Savici <saviciat@ornl.gov>
2017-10-18 13:04:27 -0400

    Re #20946. Fix logic error

--- Framework/Algorithms/src/RemovePromptPulse.cpp
+++ Framework/Algorithms/src/RemovePromptPulse.cpp
@@ -64,11 +64,11 @@ double getMedian(const API::Run &run, const std::string &name) {
 
 void getTofRange(MatrixWorkspace_const_sptr wksp, double &tmin, double &tmax) {
   DataObjects::EventWorkspace_const_sptr eventWksp =
-    boost::dynamic_pointer_cast<const DataObjects::EventWorkspace>(wksp);
+      boost::dynamic_pointer_cast<const DataObjects::EventWorkspace>(wksp);
   if (eventWksp == nullptr) {
-    eventWksp->getEventXMinMax(tmin, tmax);
-  } else {
     wksp->getXMinMax(tmin, tmax);
+  } else {
+    eventWksp->getEventXMinMax(tmin, tmax);
   }
 }
 } // anonymous namespace end
