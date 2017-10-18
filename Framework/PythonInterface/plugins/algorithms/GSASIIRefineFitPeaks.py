fd7b829c82877ce19780600c951232291f04b130
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-10-18 14:15:40 +0100

    re #19578 Removed PeakFitting functionality

--- Framework/PythonInterface/plugins/algorithms/GSASIIRefineFitPeaks.py
+++ Framework/PythonInterface/plugins/algorithms/GSASIIRefineFitPeaks.py
@@ -1,6 +1,5 @@
 from __future__ import (absolute_import, division, print_function)
 from contextlib import contextmanager
-import numpy
 import os
 import sys
 import tempfile
@@ -22,23 +21,16 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
     PROP_INPUT_WORKSPACE = "InputWorkspace"
     PROP_OUT_GOF = "GOF"
     PROP_OUT_GROUP_RESULTS = "Results"
-    PROP_OUT_REFINEMENT_PARAMS = "RefinementParameters"
+    PROP_OUT_LATTICE_PARAMS = "LatticeParameters"
     PROP_OUT_RWP = "Rwp"
     PROP_PATH_TO_GSASII = "PathToGSASII"
     PROP_PATH_TO_INST_PARAMS = "InstrumentFile"
     PROP_PATH_TO_PHASE = "PhaseInfoFile"
     PROP_PAWLEY_DMIN = "PawleyDMin"
     PROP_PAWLEY_NEGATIVE_WEIGHT = "PawleyNegativeWeight"
-    PROP_PEAKS_FILE = "PeaksFile"
-    PROP_REFINE_CENTRE = "RefineCentre"
-    PROP_REFINE_INTENSITY = "RefineIntensity"
-    PROP_REFINE_SIGMA = "RefineSigma"
-    PROP_REFINE_GAMMA = "RefineGamma"
     PROP_REFINEMENT_METHOD = "RefinementMethod"
     PROP_SUPPRESS_GSAS_OUTPUT = "MuteGSASII"
     PROP_WORKSPACE_INDEX = "WorkspaceIndex"
-    PROP_X_MIN = "XMin"
-    PROP_X_MAX = "XMax"
 
     DEFAULT_REFINEMENT_PARAMS = {"set":
                                  {"Background": {"no.coeffs": 3,
@@ -59,16 +51,17 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
 
     def PyInit(self):
         self.declareProperty(name=self.PROP_REFINEMENT_METHOD, defaultValue=self.REFINEMENT_METHODS[0],
-                             validator=StringListValidator(self.REFINEMENT_METHODS), doc="Refinement method (TODO)")
+                             validator=StringListValidator(self.REFINEMENT_METHODS), direction=Direction.Input,
+                             doc="Refinement method (TODO)")
 
         self.declareProperty(WorkspaceProperty(name=self.PROP_INPUT_WORKSPACE, defaultValue="",
                                                direction=Direction.Input), doc="Workspace with spectra to fit peaks")
-        self.declareProperty(name=self.PROP_WORKSPACE_INDEX, defaultValue=0,
+        self.declareProperty(name=self.PROP_WORKSPACE_INDEX, defaultValue=0, direction=Direction.Input,
                              doc="Index of the spectrum in InputWorkspace to fit. By default, the first spectrum "
                                  "(ie the only one for a focused workspace) is used")
         self.declareProperty(FileProperty(name=self.PROP_PATH_TO_INST_PARAMS, defaultValue="", action=FileAction.Load,
                                           extensions=[".prm"]), doc="Location of the phase file")
-        self.declareProperty(FileProperty(name=self.PROP_PATH_TO_PHASE, defaultValue="", action=FileAction.OptionalLoad,
+        self.declareProperty(FileProperty(name=self.PROP_PATH_TO_PHASE, defaultValue="", action=FileAction.Load,
                                           extensions=[".cif"]), doc="Location of the phase file")
         self.declareProperty(FileProperty(name=self.PROP_PATH_TO_GSASII, defaultValue="", action=FileAction.Directory),
                              doc="Path to the directory containing GSASII executable on the user's machine")
@@ -77,21 +70,21 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
                              doc="Goodness of fit value (Chi squared)")
         self.declareProperty(name=self.PROP_OUT_RWP, defaultValue=0.0, direction=Direction.Output,
                              doc="Weight profile R-factor (Rwp) discrepancy index for the goodness of fit")
-        self.declareProperty(ITableWorkspaceProperty(name=self.PROP_OUT_REFINEMENT_PARAMS, direction=Direction.Output,
-                                                     defaultValue=self.PROP_OUT_REFINEMENT_PARAMS),
+        self.declareProperty(ITableWorkspaceProperty(name=self.PROP_OUT_LATTICE_PARAMS, direction=Direction.Output,
+                                                     defaultValue=self.PROP_OUT_LATTICE_PARAMS),
                              doc="Table to output the lattice parameters (refined)")
         self.declareProperty(FileProperty(name=self.PROP_GSAS_PROJ_PATH, defaultValue="", action=FileAction.Save,
                                           extensions=".gpx"), doc="GSASII Project to work on")
 
         self.setPropertyGroup(self.PROP_OUT_GOF, self.PROP_OUT_GROUP_RESULTS)
         self.setPropertyGroup(self.PROP_OUT_RWP, self.PROP_OUT_GROUP_RESULTS)
-        self.setPropertyGroup(self.PROP_OUT_REFINEMENT_PARAMS, self.PROP_OUT_GROUP_RESULTS)
+        self.setPropertyGroup(self.PROP_OUT_LATTICE_PARAMS, self.PROP_OUT_GROUP_RESULTS)
         self.setPropertyGroup(self.PROP_GSAS_PROJ_PATH, self.PROP_OUT_GROUP_RESULTS)
 
-        self.declareProperty(name=self.PROP_PAWLEY_DMIN, defaultValue=1.0,
+        self.declareProperty(name=self.PROP_PAWLEY_DMIN, defaultValue=1.0, direction=Direction.Input,
                              doc="For Pawley refiment: as defined in GSAS-II, the minimum d-spacing to be used in a "
                                  "Pawley refinement. Please refer to the GSAS-II documentation for full details.")
-        self.declareProperty(name=self.PROP_PAWLEY_NEGATIVE_WEIGHT, defaultValue=0.0,
+        self.declareProperty(name=self.PROP_PAWLEY_NEGATIVE_WEIGHT, defaultValue=0.0, direction=Direction.Input,
                              doc="For Pawley refinement: as defined in GSAS-II, the weight for a penalty function "
                                  "applied during a Pawley refinement on resulting negative intensities. "
                                  "Please refer to the GSAS-II documentation for full details.")
@@ -99,46 +92,22 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
         self.setPropertyGroup(self.PROP_PAWLEY_DMIN, self.PROP_GROUP_PAWLEY_PARAMS)
         self.setPropertyGroup(self.PROP_PAWLEY_NEGATIVE_WEIGHT, self.PROP_GROUP_PAWLEY_PARAMS)
 
-        self.declareProperty(FileProperty(name=self.PROP_PEAKS_FILE, defaultValue="", action=FileAction.OptionalLoad),
-                             doc="File with detected peak parameters from GSAS-II. Column headings should be "
-                                 "pos dsp esd int sig gam FWHM")
-        self.declareProperty(name=self.PROP_REFINE_CENTRE, defaultValue=False, doc="Whether to refine peak centre")
-        self.declareProperty(name=self.PROP_REFINE_INTENSITY, defaultValue=False,
-                             doc="Whether to refine peak intensity")
-        self.declareProperty(name=self.PROP_REFINE_SIGMA, defaultValue=True, doc="Whether to refine sigma parameter")
-        self.declareProperty(name=self.PROP_REFINE_GAMMA, defaultValue=True, doc="Whether to refine gamma parameter")
-        self.declareProperty(name=self.PROP_X_MIN, defaultValue=0.0, doc="Lower limit")
-        self.declareProperty(name=self.PROP_X_MAX, defaultValue=0.0, doc="Upper limit")
-
-        self.declareProperty(name=self.PROP_SUPPRESS_GSAS_OUTPUT, defaultValue=False,
+        self.declareProperty(name=self.PROP_SUPPRESS_GSAS_OUTPUT, defaultValue=False, direction=Direction.Input,
                              doc="Set to True to prevent GSAS run info from being "
                                  "printed (not recommended, but can be useful for debugging)")
 
-    def validateInputs(self):
-        errors = {}
-
-        if self._is_peak_fitting_mode() and not self.getPropertyValue(self.PROP_PEAKS_FILE):
-            errors[self.PROP_PEAKS_FILE] = "Must be provided when running peak fitting"
-
-        if not self._is_peak_fitting_mode() and not self.getPropertyValue(self.PROP_PATH_TO_PHASE):
-            errors[self.PROP_PATH_TO_PHASE] = "Must be provided when doing Rietveld/Pawley refinement"
-
-        return errors
-
     def PyExec(self):
+        refinement_method = self.getPropertyValue(self.PROP_REFINEMENT_METHOD)
+        if refinement_method == self.REFINEMENT_METHODS[2]:  # Peak fitting
+            raise NotImplementedError("GSAS-II Peak fitting not yet implemented in Mantid")
+
         with self._suppress_stdout():
             gsas_proj = self._initialise_GSAS()
 
-            if self._is_peak_fitting_mode():
-                rwp, gof, refinement_params = \
-                    self._run_fit_peaks(gsas_proj=gsas_proj,
-                                        input_ws=mtd[self.getPropertyValue(self.PROP_INPUT_WORKSPACE)])
-            else:
-                refinement_method = self.getPropertyValue(self.PROP_REFINEMENT_METHOD)
-                rwp, gof, refinement_params = \
-                    self._run_rietveld_pawley_refinement(gsas_proj=gsas_proj,
-                                                         do_pawley=refinement_method == self.REFINEMENT_METHODS[0])
-            self._set_output_properties(rwp=rwp, gof=gof, lattice_params=refinement_params)
+            rwp, gof, lattice_params = \
+                self._run_rietveld_pawley_refinement(gsas_proj=gsas_proj,
+                                                     do_pawley=refinement_method == self.REFINEMENT_METHODS[0])
+            self._set_output_properties(rwp=rwp, gof=gof, lattice_params=lattice_params)
 
     def _build_output_lattice_table(self, lattice_params):
         alg = self.createChildAlgorithm('CreateEmptyTableWorkspace')
@@ -151,26 +120,6 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
         table.addRow([float(lattice_params[param]) for param in self.LATTICE_TABLE_PARAMS])
         return table
 
-    def _build_output_peak_param_table(self, param_dict):
-        table_name = self.getPropertyValue(self.PROP_OUT_REFINEMENT_PARAMS)
-
-        param_names = ["Centre", "Intensity", "Sigma", "Gamma"]
-        param_prefixes = ["pos", "int", "sig", "gam"]
-        table = mantid.CreateEmptyTableWorkspace(OutputWorkspace=table_name)
-
-        num_peaks = 0
-        while param_prefixes[0] + str(num_peaks) in param_dict:
-            num_peaks += 1
-
-        for name in param_names:
-            table.addColumn("double", name)
-
-        for peak_index in range(num_peaks):
-            param_values = [param_dict[param_prefix + str(peak_index)] for param_prefix in param_prefixes]
-            table.addRow(param_values)
-
-        return table
-
     def _extract_spectrum_from_workspace(self):
         """
         Extract a single spectrum from the input workspace. If the input workspace only has one spectrum then just
@@ -181,21 +130,10 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
         if mtd[ws].getNumberHistograms > 1:
             ws_index = self.getPropertyValue(self.PROP_WORKSPACE_INDEX)
             spectrum = mantid.ExtractSpectra(InputWorkspace=ws, StartWorkspaceIndex=ws_index, EndWorkspaceIndex=ws_index)
+            mantid.DeleteWorkspace(Workspace=ws)
             return spectrum
         else:
             return ws
-
-    def _get_fit_peaks_params(self, gsas_proj):
-        proj_vals = gsas_proj.values()[4]
-
-        fit_pgm = "LSQ"
-        peaks = self._read_peaks_from_data_file()
-        background = proj_vals["Background"]
-        limits = [float(self.getPropertyValue(self.PROP_X_MIN)), float(self.getPropertyValue(self.PROP_X_MAX))]
-        [inst_params1, inst_params2] = proj_vals["Instrument Parameters"]
-        data = proj_vals["data"][1]
-
-        return fit_pgm, peaks, background, limits, inst_params1, inst_params2, data
 
     def _initialise_GSAS(self):
         """
@@ -214,74 +152,17 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
         mantid.DeleteWorkspace(Workspace=spectrum)
 
         inst_param_path = self.getPropertyValue(self.PROP_PATH_TO_INST_PARAMS)
-        hist = gsas_proj.add_powder_histogram(datafile=spectrum_path, iparams=inst_param_path)
-        self.log().information(str(hist.values()))
+        gsas_proj.add_powder_histogram(datafile=spectrum_path, iparams=inst_param_path)
+
         self._remove_temporary_fxye(spectrum_path=spectrum_path)
 
         return gsas_proj
-
-    def _is_peak_fitting_mode(self):
-        return self.getPropertyValue(self.PROP_REFINEMENT_METHOD) == self.REFINEMENT_METHODS[2]
-
-    def _read_peaks_from_data_file(self):
-        refine_centre = self.getPropertyValue(self.PROP_REFINE_CENTRE) == "1"
-        refine_intensity = self.getPropertyValue(self.PROP_REFINE_INTENSITY) == "1"
-        refine_sigma = self.getPropertyValue(self.PROP_REFINE_SIGMA) == "1"
-        refine_gamma = self.getPropertyValue(self.PROP_REFINE_GAMMA) == "1"
-
-        peaks_data_file = self.getPropertyValue(self.PROP_PEAKS_FILE)
-        with open(peaks_data_file) as peaks_file:
-            file_lines = peaks_file.read().split("\n")
-
-        peaks = []
-        for line in file_lines:
-            if line and not line.startswith("#"):
-                all_peak_params = line.split()
-                # position, intensity, sigma, gamma
-                peak_params = [float(all_peak_params[i]) for i in [0, 3, 4, 5]]
-                peak_params_with_flags = [j for i in zip(peak_params,
-                                                         [refine_centre, refine_intensity, refine_sigma, refine_gamma])
-                                          for j in i]
-                peaks.append(peak_params_with_flags)
-
-        return peaks
 
     def _remove_temporary_fxye(self, spectrum_path):
         try:
             os.remove(spectrum_path)
         except Exception as e:
             raise Warning("Couldn't remove temporary spectrum file at location \"{}\":\n{}".format(spectrum_path, e))
-
-    def _run_fit_peaks(self, input_ws, gsas_proj):
-        #data = gsas_proj.histograms()[0].values()[7][1]
-        x_values = input_ws.dataX(0)
-        y_values = input_ws.dataY(0)
-        zeroes = numpy.zeros(len(x_values))
-        self.log().debug(str(x_values))
-        self.log().debug(str(y_values))
-
-        data = [x_values, y_values, zeroes, zeroes, zeroes, zeroes]
-
-        fix_back = data[-1]
-        fit_pgm, peaks, background, limits, inst_params1, inst_params2, data = self._get_fit_peaks_params(gsas_proj)
-
-        gsas_path = self.getPropertyValue(self.PROP_PATH_TO_GSASII)
-        if gsas_path not in sys.path:
-            sys.path.append(gsas_path)
-        gsas_bin = os.path.join(gsas_path, "bindist")
-        if gsas_bin not in sys.path:
-            sys.path.append(gsas_bin)
-
-        import GSASIIpwd
-        print(data)
-        fit_peak_results = GSASIIpwd.DoPeakFit(FitPgm=fit_pgm, Peaks=peaks, Background=background, Limits=limits,
-                                               Inst=inst_params1, Inst2=inst_params2, data=data, fixback=fix_back)
-
-        gof = fit_peak_results[3]["GOF"]
-        rwp = fit_peak_results[3]["Rwp"]
-        refined_peak_params = self._build_output_peak_param_table(fit_peak_results[5])
-
-        return rwp, gof, refined_peak_params
 
     def _run_rietveld_pawley_refinement(self, gsas_proj, do_pawley):
         """
@@ -323,7 +204,7 @@ class GSASIIRefineFitPeaks(PythonAlgorithm):
     def _set_output_properties(self, rwp, gof, lattice_params):
         self.setProperty(self.PROP_OUT_RWP, rwp)
         self.setProperty(self.PROP_OUT_GOF, gof)
-        self.setProperty(self.PROP_OUT_REFINEMENT_PARAMS, lattice_params)
+        self.setProperty(self.PROP_OUT_LATTICE_PARAMS, lattice_params)
 
     def _set_pawley_phase_parameters(self, phase):
         # Note from GSAS-II doc: "you probably should clear the Histogram scale factor refinement
