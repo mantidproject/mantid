bc31f21c855f61bf2ee5990df789f0a036a565cc
Edward Brown <edward.brown@stfc.ac.uk>
2017-10-19 14:19:58 +0100

    Re #20927: Fixed errors, updated typedef and replaced end with cend.

--- MantidPlot/src/Mantid/MantidMatrixTabExtension.h
+++ MantidPlot/src/Mantid/MantidMatrixTabExtension.h
@@ -23,7 +23,5 @@ struct MantidMatrixTabExtension {
   MantidMatrixModel::Type type;
 };
 
-typedef std::map<MantidMatrixModel::Type, MantidMatrixTabExtension>
-    MantidMatrixTabExtensionMap;
-
+using MantidMatrixTabExtensionMap = std::map<MantidMatrixModel::Type, MantidMatrixTabExtension>;
 #endif
