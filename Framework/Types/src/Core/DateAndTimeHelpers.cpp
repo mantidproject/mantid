4cfb6b0cb2c06f29af289ab97ddee580e7d54e2c
Antti Soininen <soininen@ill.fr>
2017-10-24 17:39:09 +0200

    Code beautification.
    Re #20969

--- Framework/Types/src/Core/DateAndTimeHelpers.cpp
+++ Framework/Types/src/Core/DateAndTimeHelpers.cpp
@@ -13,7 +13,8 @@ namespace DateAndTimeHelpers {
  */
 bool stringIsISO8601(const std::string &date) {
   // On Ubuntu 14.04, std::regex seems to be broken, thus boost.
-  const boost::regex r(R"(^\d{4}-[01]\d-[0-3]\d([T\s][0-2]\d:[0-5]\d(:\d{2})?(.\d+)?(Z|[+-]\d{2}(:\d{2})?)?)?$)");
+  const boost::regex r(
+      R"(^\d{4}-[01]\d-[0-3]\d([T\s][0-2]\d:[0-5]\d(:\d{2})?(.\d+)?(Z|[+-]\d{2}(:\d{2})?)?)?$)");
   return boost::regex_match(date, r);
 }
 } // namespace DateAndTimeHelpers
