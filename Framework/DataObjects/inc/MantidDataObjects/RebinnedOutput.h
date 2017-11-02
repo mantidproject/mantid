9247dd57ea130916e8c4751a08c7c59f31abe0ff
Duc Le <duc.le@stfc.ac.uk>
2017-11-02 02:02:01 +0000

    Re #20599 - clang format

--- Framework/DataObjects/inc/MantidDataObjects/RebinnedOutput.h
+++ Framework/DataObjects/inc/MantidDataObjects/RebinnedOutput.h
@@ -92,7 +92,9 @@ protected:
 
 private:
   RebinnedOutput *doClone() const override { return new RebinnedOutput(*this); }
-  RebinnedOutput *doCloneEmpty() const override { return new RebinnedOutput(m_finalized); }
+  RebinnedOutput *doCloneEmpty() const override { 
+    return new RebinnedOutput(m_finalized); 
+  }
 };
 
 /// shared pointer to the RebinnedOutput class
