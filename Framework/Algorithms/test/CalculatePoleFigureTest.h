485ed8b190d3eb8e58d2e52d55884c4d36441aa8
Wenduo Zhou <zhouw@ornl.gov>
2017-11-01 22:14:15 -0400

    Refs #20988. Improved unit test.

--- Framework/Algorithms/test/CalculatePoleFigureTest.h
+++ Framework/Algorithms/test/CalculatePoleFigureTest.h
@@ -5,20 +5,28 @@
 #include "MantidAPI/Axis.h"
 #include "MantidAPI/FrameworkManager.h"
 #include "MantidAPI/IAlgorithm.h"
+#include "MantidAPI/ExperimentInfo.h"
+#include "MantidAPI/Run.h"
+#include "MantidGeometry/Instrument.h"
+#include "MantidGeometry/Instrument/Detector.h"
+#include "MantidGeometry/Objects/Object.h"
 #include "MantidAPI/MatrixWorkspace.h"
 #include "MantidAPI/WorkspaceFactory.h"
 #include "MantidDataObjects/Workspace2D.h"
+#include "MantidTypes/Core/DateAndTime.h"
 #include "MantidKernel/UnitFactory.h"
 #include "MantidAPI/ITableWorkspace.h"
 #include "MantidAPI/TableRow.h"
 #include <cmath>
 #include <cxxtest/TestSuite.h>
 #include <numeric>
+#include "MantidKernel/TimeSeriesProperty.h"
 
 #include "MantidAlgorithms/CalculatePoleFigure.h"
 
 using namespace Mantid::Algorithms;
 using namespace Mantid::Kernel;
+using namespace Mantid::Geometry;
 using namespace Mantid;
 
 namespace {
@@ -41,7 +49,46 @@ Mantid::API::MatrixWorkspace_sptr createBraggWorkspace(const std::string &name) 
       boost::dynamic_pointer_cast<Mantid::DataObjects::Workspace2D>(
           Mantid::API::WorkspaceFactory::Instance().create("Workspace2D", 2, n,
                                                            n));
+  // create unit
+  std::string unitlabel("dSpacing");
+  ws->getAxis(0)->unit() =
+      Mantid::Kernel::UnitFactory::Instance().create(unitlabel);
 
+  // set instrument as reduced VULCAN east and west bank
+  Instrument_sptr testInst(new Instrument);
+
+  // Define a source component: [0,0,-43.754]
+  Mantid::Geometry::ObjComponent *source =
+      new Mantid::Geometry::ObjComponent("moderator", Object_sptr(), testInst.get());
+  source->setPos(V3D(0, 0.0, -43.754));
+  testInst->add(source);
+  testInst->markAsSource(source);
+
+  // Define a sample as a simple sphere
+  Mantid::Geometry::ObjComponent *sample =
+      new Mantid::Geometry::ObjComponent("samplePos", Object_sptr(), testInst.get());
+  testInst->setPos(0.0, 0.0, 0.0);
+  testInst->add(sample);
+  testInst->markAsSamplePos(sample);
+
+  // add two pixels
+  // pixel 1:  [-2,0,-3.67394e-16]
+  Mantid::Geometry::Detector *physicalPixel = new Detector("pixel", 1, testInst.get());
+  physicalPixel->setPos(-2, 0, 0);
+  testInst->add(physicalPixel);
+  testInst->markAsDetector(physicalPixel);
+
+  Mantid::Geometry::Detector *physicalPixel2 = new Detector("pixel", 2, testInst.get());
+  physicalPixel2->setPos(2, 0, 0);
+  testInst->add(physicalPixel2);
+  testInst->markAsDetector(physicalPixel2);
+
+  // set instrument
+  ws->setInstrument(testInst);
+  ws->getSpectrum(0).addDetectorID(physicalPixel->getID());
+  ws->getSpectrum(1).addDetectorID(physicalPixel2->getID());
+
+  // set data
   auto &X = ws->mutableX(0);
   auto &Y = ws->mutableY(0);
   auto &E = ws->mutableE(0);
@@ -54,28 +101,21 @@ Mantid::API::MatrixWorkspace_sptr createBraggWorkspace(const std::string &name) 
     E[i] = sqrt(fabs(Y[i]));
   }
 
-  // create unit
-  std::string unitlabel("dSpacing");
-  ws->getAxis(0)->unit() =
-      Mantid::Kernel::UnitFactory::Instance().create(unitlabel);
+  // add properties
+  // add HROT
+  Kernel::TimeSeriesProperty<double> *hrotprop = new Kernel::TimeSeriesProperty<double>("HROT");
+  Types::Core::DateAndTime time0(1000000);
+  hrotprop->addValue(time0, -0.003857142);
+  ws->mutableRun().addProperty(hrotprop);
 
+  // add Omega
+  Kernel::TimeSeriesProperty<double> *omegaprop = new Kernel::TimeSeriesProperty<double>("OMEGA");
+  omegaprop->addValue(time0, 89.998);
+  API::Run &run = ws->mutableRun();
+  run.addProperty(omegaprop);
+
+  // add to ADS
   Mantid::API::AnalysisDataService::Instance().add(name, ws);
-
-  // run 153144
-  double hrot = -0.003857142;
-  double omega = 89.998;
-
-
-  auto instrument = ws->setInstrument();
-
-  // detector position
-  // det0 = ws1.getDetector(0)
-
-  //  Out[9]: [-2,0,-3.67394e-16]
-
-  // Out[11]: [2,0,1.22465e-16]
-
-  // source: [0,0,-43.754]
 
 
   return ws;
@@ -127,6 +167,10 @@ public:
     int wsindex;
     double r_td, r_nd, intensity;
     row0 >> wsindex >> r_td >> r_nd >> intensity;
+
+    // row 1
+    API::TableRow row1 = outws->getRow(1);
+
 
 
   }
