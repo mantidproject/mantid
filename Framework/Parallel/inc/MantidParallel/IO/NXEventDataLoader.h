adf24da604e06f7d372ebda5ae84a241219ce56e
Simon Heybrock <simon.heybrock@esss.se>
2017-11-06 07:53:58 +0100

    Re #0. Make NXEventDataLoader work with libhdf5-1.8 (Ubuntu 14.04).

--- Framework/Parallel/inc/MantidParallel/IO/NXEventDataLoader.h
+++ Framework/Parallel/inc/MantidParallel/IO/NXEventDataLoader.h
@@ -122,8 +122,13 @@ std::unique_ptr<AbstractEventDataPartitioner<TimeOffsetType>>
 makeEventDataPartitioner(const H5::Group &group, const int numWorkers) {
   const auto timeZero = group.openDataSet("event_time_zero");
   int64_t time_zero_offset{0};
-  if (timeZero.attrExists("offset")) {
-    const auto &offset = readAttribute(timeZero, "offset");
+  // libhdf5 on Ubuntu 14.04 is too old to support timeZero.attrExists("offset")
+  // and Attribute::readAttribute, check and read manually.
+  hid_t attr_id = H5Aopen(timeZero.getId(), "offset", H5P_DEFAULT);
+  if (attr_id > 0) {
+    const H5::Attribute attr(attr_id);
+    std::string offset;
+    attr.read(attr.getDataType(), offset);
     time_zero_offset = Types::Core::DateAndTime(offset).totalNanoseconds();
   }
   return Kernel::make_unique<
