1454324427eab87571e5bdae2cf37f2dce967ed6
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-11-08 13:34:49 +0000

    Re #20877 added check for at least one run number

--- scripts/Diffraction/isis_powder/routines/common.py
+++ scripts/Diffraction/isis_powder/routines/common.py
@@ -179,6 +179,9 @@ def get_first_run_number(run_number_string):
     """
     run_numbers = generate_run_numbers(run_number_string=run_number_string)
     if isinstance(run_numbers, list):
+        if not run_numbers:
+            raise RuntimeError("Attempted to load empty set of workspaces. Please input at least one valid run number")
+        
         run_numbers = run_numbers[0]
 
     return run_numbers
