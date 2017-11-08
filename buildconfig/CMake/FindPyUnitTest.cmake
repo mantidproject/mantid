de4509594bb25437538b5581973969c52f91ce62
Martyn Gigg <martyn.gigg@gmail.com>
2017-11-08 17:26:46 +0000

    Add test for basic import of the package
    Refs #20381

--- buildconfig/CMake/FindPyUnitTest.cmake
+++ buildconfig/CMake/FindPyUnitTest.cmake
@@ -2,7 +2,8 @@
 # PYUNITTEST_ADD_TEST (public macro to add unit tests)
 #   Adds a set of python tests based upon the unittest module
 #   Parameters:
-#       _test_src_dir :: The directory where the src files reside
+#       _test_src_dir_base :: A base directory when added to the relative test paths gives
+#                             an absolute path to that test
 #       _testname_prefix :: A prefix for each test that is added to ctest, the name will be
 #                           ${_testname_prefix}_TestName
 #       ${ARGN} :: List of test files
@@ -10,7 +11,7 @@ function ( PYUNITTEST_ADD_TEST _test_src_dir _testname_prefix )
   # Property for the module directory
   set ( _working_dir ${CMAKE_BINARY_DIR}/bin/Testing )
   if ( CMAKE_GENERATOR MATCHES "Visual Studio" OR CMAKE_GENERATOR MATCHES "Xcode" )
-    set ( _module_dir ${CMAKE_BINARY_DIR}/bin/$<CONFIGURATION> )
+    set ( _module_dir ${CMAKE_BINARY_DIR}/bin/$<CONFIG> )
   else()
     set ( _module_dir ${CMAKE_BINARY_DIR}/bin )
   endif()
@@ -18,18 +19,27 @@ function ( PYUNITTEST_ADD_TEST _test_src_dir _testname_prefix )
   if ( WIN32 )
     set ( _test_runner ${_test_runner}.bat )
   endif ()
+  set ( _test_runner_module ${CMAKE_SOURCE_DIR}/Framework/PythonInterface/test/testhelpers/testrunner.py )
+  # Environment
+  if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    set ( _python_path ${PYTHON_XMLRUNNER_DIR};${PYTHON_MODULE_OUTPUT_DIRECTORY};$ENV{PYTHONPATH} )
+    # cmake list separator and Windows environment seprator are the same so escape the cmake one
+    string ( REPLACE ";" "\\;" _python_path "${_python_path}" )
+  else()
+    set ( _python_path ${PYTHON_XMLRUNNER_DIR}:${PYTHON_MODULE_OUTPUT_DIRECTORY}:$ENV{PYTHONPATH} )
+  endif()
 
   # Add all of the individual tests so that they can be run in parallel
   foreach ( part ${ARGN} )
-    get_filename_component( _filename ${part} NAME )
+    set ( _filename ${part} )
     get_filename_component( _suitename ${part} NAME_WE )
     # We duplicate the suitename so that it matches the junit output name
     set ( _pyunit_separate_name "${_testname_prefix}.${_suitename}.${_suitename}" )
     add_test ( NAME ${_pyunit_separate_name}
-               COMMAND ${_test_runner} --classic -m testhelpers.testrunner ${_test_src_dir}/${_filename} )
+               COMMAND ${_test_runner} --classic ${_test_runner_module} ${_test_src_dir}/${_filename} )
     # Set the PYTHONPATH so that the built modules can be found
     set_tests_properties ( ${_pyunit_separate_name} PROPERTIES
-                           ENVIRONMENT PYTHONPATH=${PYTHON_XMLRUNNER_DIR}
+                           ENVIRONMENT "PYTHONPATH=${_python_path}"
                            WORKING_DIRECTORY ${_working_dir}
                            TIMEOUT ${TESTING_TIMEOUT} )
   endforeach ( part ${ARGN} )
