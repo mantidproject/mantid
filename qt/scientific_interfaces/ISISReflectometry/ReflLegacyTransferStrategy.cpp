5258628cb06196cf772ac4b9d819db18b32ee9b0
Edward Brown <edward.brown@stfc.ac.uk>
2017-11-13 11:48:22 +0000

    Re #21134: Reverted to boost::regex due to lack of std::regex support.
    - RHEL7 lacks a complete version of std::regex in gcc 3.8.5.

--- qt/scientific_interfaces/ISISReflectometry/ReflLegacyTransferStrategy.cpp
+++ qt/scientific_interfaces/ISISReflectometry/ReflLegacyTransferStrategy.cpp
@@ -4,7 +4,7 @@
 #include "MantidKernel/ProgressBase.h"
 #include "ReflTableSchema.h"
 #include <algorithm>
-#include <regex>
+#include <boost/regex.hpp>
 
 namespace MantidQt {
 namespace CustomInterfaces {
@@ -27,48 +27,46 @@ TransferResults ReflLegacyTransferStrategy::transferRuns(
   std::map<std::string, std::string> descriptionToTheta;
 
   // Iterate over the input and build the maps
-  for (auto const &runDescriptionPair : searchResults) {
+  for (auto const &runDescriptionPair : searchResults)
     const auto run = runDescriptionPair.first;
-    const auto description = runDescriptionPair.second.description;
-    auto groupName = description;
-    auto cleanDescription = description;
+  const auto description = runDescriptionPair.second.description;
+  auto groupName = description;
+  auto cleanDescription = description;
 
-    static std::regex descriptionFormatRegex("(.*)(th[:=]([0-9.]+))(.*)",
-                                             std::regex_constants::basic);
-    std::smatch matches;
-    if (std::regex_search(description, matches, descriptionFormatRegex)) {
-      constexpr auto preThetaGroup = 1;
-      constexpr auto thetaValueGroup = 3;
-      constexpr auto postThetaGroup = 4;
-      // We have theta. Let's get a clean description
-      const auto theta = matches[thetaValueGroup].str();
-      const auto preTheta = matches[preThetaGroup].str();
-      const auto postTheta = matches[postThetaGroup].str();
-      groupName = preTheta;
-      cleanDescription = preTheta + "?" + postTheta;
-      descriptionToTheta[description] = theta;
-    }
-
-    // map the description to the run, making sure to join with a + if one
-    // already exists
-    const std::string prevRun = descriptionToRun[description];
-    if (prevRun.empty())
-      descriptionToRun[description] = run;
-    else
-      descriptionToRun[description] = prevRun + "+" + run;
-
-    // If there isn't a group for this description (ignoring differences in
-    // theta) yet, make one
-    if (descriptionToGroup[cleanDescription].empty()) {
-      trim(groupName);
-      descriptionToGroup[cleanDescription] = groupName;
-    }
-
-    // Assign this description to the group it belongs to
-    descriptionToGroup[description] = descriptionToGroup[cleanDescription];
-
-    progress.report();
+  static boost::regex descriptionFormatRegex("(.*)(th[:=]([0-9.]+))(.*)");
+  boost::smatch matches;
+  if (boost::regex_search(description, matches, descriptionFormatRegex)) {
+    constexpr auto preThetaGroup = 1;
+    constexpr auto thetaValueGroup = 3;
+    constexpr auto postThetaGroup = 4;
+    // We have theta. Let's get a clean description
+    const auto theta = matches[thetaValueGroup].str();
+    const auto preTheta = matches[preThetaGroup].str();
+    const auto postTheta = matches[postThetaGroup].str();
+    groupName = preTheta;
+    cleanDescription = preTheta + "?" + postTheta;
+    descriptionToTheta[description] = theta;
   }
+
+  // map the description to the run, making sure to join with a + if one
+  // already exists
+  const std::string prevRun = descriptionToRun[description];
+  if (prevRun.empty())
+    descriptionToRun[description] = run;
+  else
+    descriptionToRun[description] = prevRun + "+" + run;
+
+  // If there isn't a group for this description (ignoring differences in
+  // theta) yet, make one
+  if (descriptionToGroup[cleanDescription].empty()) {
+    trim(groupName);
+    descriptionToGroup[cleanDescription] = groupName;
+  }
+
+  // Assign this description to the group it belongs to
+  descriptionToGroup[description] = descriptionToGroup[cleanDescription];
+
+  progress.report();
 
   // All the data we need is now properly organised, so we can quickly throw out
   // the rows needed
@@ -100,9 +98,9 @@ ReflLegacyTransferStrategy::clone() const {
 
 bool MantidQt::CustomInterfaces::ReflLegacyTransferStrategy::knownFileType(
     const std::string &filename) const {
-  std::regex pattern("raw$", std::regex::icase);
-  std::smatch match; // Unused.
-  return std::regex_search(filename, match, pattern);
+  boost::regex pattern("raw$", boost::regex::icase);
+  boost::smatch match; // Unused.
+  return boost::regex_search(filename, match, pattern);
 }
 }
 }
