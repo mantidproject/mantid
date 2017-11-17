74a76ee8b26f2a0b140b03771f7d4bd53196d5b4
Verena Reimund <reimund@ill.eu>
2017-11-17 16:13:28 +0100

    clang format  Re #20965

--- Framework/TestHelpers/inc/MantidTestHelpers/WorkspaceCreationHelper.h
+++ Framework/TestHelpers/inc/MantidTestHelpers/WorkspaceCreationHelper.h
@@ -389,14 +389,14 @@ Mantid::API::MatrixWorkspace_sptr create2DWorkspaceWithReflectometryInstrument(
     Mantid::Kernel::V3D slit1Pos = Mantid::Kernel::V3D(0, 0, 0),
     Mantid::Kernel::V3D slit2Pos = Mantid::Kernel::V3D(0, 0, 1),
     double vg1 = 0.5, double vg2 = 1.0,
-        Mantid::Kernel::V3D sourcePos = Mantid::Kernel::V3D(0, 0, 0),
-        Mantid::Kernel::V3D monitorPos = Mantid::Kernel::V3D(14, 0, 0),
-        Mantid::Kernel::V3D samplePos = Mantid::Kernel::V3D(15, 0, 0),
-        Mantid::Kernel::V3D detectorPos = Mantid::Kernel::V3D(20, (20 - 15), 0),
-        Mantid::Geometry::PointingAlong up = Mantid::Geometry::PointingAlong::Y,
-        Mantid::Geometry::PointingAlong along = Mantid::Geometry::PointingAlong::X,
-        const int nSpectra = 2,
-    const int nBins = 100, const double deltaX = 2000.0);
+    Mantid::Kernel::V3D sourcePos = Mantid::Kernel::V3D(0, 0, 0),
+    Mantid::Kernel::V3D monitorPos = Mantid::Kernel::V3D(14, 0, 0),
+    Mantid::Kernel::V3D samplePos = Mantid::Kernel::V3D(15, 0, 0),
+    Mantid::Kernel::V3D detectorPos = Mantid::Kernel::V3D(20, (20 - 15), 0),
+    Mantid::Geometry::PointingAlong up = Mantid::Geometry::PointingAlong::Y,
+    Mantid::Geometry::PointingAlong along = Mantid::Geometry::PointingAlong::X,
+    const int nSpectra = 2, const int nBins = 100,
+    const double deltaX = 2000.0);
 
 /// Create a 2D workspace with one monitor and three detectors based around
 /// a virtual reflectometry instrument.
