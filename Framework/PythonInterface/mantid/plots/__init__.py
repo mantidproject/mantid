a19ec37d9aeedad696ce8e44229923e2fc274338
Pete Peterson <petersonpf@ornl.gov>
2017-11-21 10:33:15 -0500

    Move module

--- Framework/PythonInterface/mantid/plots/__init__.py
+++ Framework/PythonInterface/mantid/plots/__init__.py
@@ -14,9 +14,10 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""A collection of functions that unpack mantid objects for plotting
-with matplotlib.
+"""Functionality for unpacking mantid objects for plotting with matplotlib.
 """
 
 # This file should be left free of PyQt imports to allow quick importing
 # of the main package.
+
+from mantid.plots.functions import *
