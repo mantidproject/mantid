cd2ffa3e367f2adba40966fd5ff8879a9ceacc04
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2017-11-21 17:54:55 +0000

    Remove incorrect use of range in numpy histogram function
    Refs #21172

--- scripts/AbinsModules/SPowderSemiEmpiricalCalculator.py
+++ scripts/AbinsModules/SPowderSemiEmpiricalCalculator.py
@@ -641,8 +641,8 @@ class SPowderSemiEmpiricalCalculator(object):
         :returns: rebined frequencies, rebined S
         """
         maximum_index = min(len(array_x), len(array_y))
-        return np.histogram(array_x[:maximum_index], bins=self._bins, range=(0, self._freq_size),
-                            weights=array_y[:maximum_index], density=False)[0]
+        return np.histogram(array_x[:maximum_index], bins=self._bins, weights=array_y[:maximum_index],
+                            density=False)[0]
 
     def _rebin_data_opt(self, array_x=None, array_y=None):
         """
