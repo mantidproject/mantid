5f7699a493529c92baf452283ce0d25487569824
Pete Peterson <petersonpf@ornl.gov>
2017-11-21 11:36:52 -0500

    Fix version check

--- buildconfig/CMake/CommonSetup.cmake
+++ buildconfig/CMake/CommonSetup.cmake
@@ -259,7 +259,7 @@ endif ()
 # Configure clang-tidy if the tool is found
 ###########################################################################
 
-if ( CMAKE_VERSION GREATER "3.5" )
+if ( CMAKE_VERSION VERSION_GREATER "3.5" )
   set(ENABLE_CLANG_TIDY OFF CACHE BOOL "Add clang-tidy automatically to builds")
   if (ENABLE_CLANG_TIDY)
     find_program (CLANG_TIDY_EXE NAMES "clang-tidy" PATHS /usr/local/opt/llvm/bin )
