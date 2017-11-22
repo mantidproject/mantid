f9bd92174eefd207573ac2825d8604780bf52d53
Pete Peterson <petersonpf@ornl.gov>
2017-11-22 09:52:12 -0500

    Refactor data extraction

--- Framework/PythonInterface/mantid/plots/_functions.py
+++ Framework/PythonInterface/mantid/plots/_functions.py
@@ -43,6 +43,24 @@ def _getDistribution(workspace, kwargs):
 
     return (distribution, kwargs)
 
+def _getSpectrum(workspace, wkspIndex, distribution, withDy=False, withDx=False):
+    x = workspace.readX(wkspIndex)
+    y = workspace.readY(wkspIndex)
+    dy = None
+    dx = None
+
+    if withDy:
+        dy = workspace.readE(wkspIndex)
+    # TODO should extract dx but EventWorkspace does the wrong thing
+    if workspace.isHistogramData():
+        if not distribution:
+            y = y / (x[1:] - x[0:-1])
+            if dy is not None:
+                dy = dy / (x[1:] - x[0:-1])
+        x = .5*(x[0:-1]+x[1:])
+
+    return (x,y,dy,dx)
+
 def plot(axes, workspace, *args, **kwargs):
     '''Unpack mantid workspace and render it with matplotlib. ``args`` and
     ``kwargs`` are passed to :meth:matplotlib.axes.Axes.plot after special
@@ -61,15 +79,7 @@ def plot(axes, workspace, *args, **kwargs):
     both will generate a :class:`exceptions.RuntimeError`.
     '''
     (wkspIndex, distribution, kwargs) = _getWkspIndexDistAndLabel(workspace, kwargs)
-
-    x = workspace.readX(wkspIndex)
-    y = workspace.readY(wkspIndex)
-    if workspace.isHistogramData():
-        if not distribution:
-            y = y / (x[1:] - x[0:-1])
-
-        x = .5*(x[0:-1]+x[1:])
-
+    (x, y, _, _) = _getSpectrum(workspace, wkspIndex, distribution, withDy=False, withDx=False)
     axes.plot(x, y, *args, **kwargs)
 
 def errorbar(axes, workspace, *args, **kwargs):
@@ -90,33 +100,31 @@ def errorbar(axes, workspace, *args, **kwargs):
     both will generate a :class:`exceptions.RuntimeError`.
     '''
     (wkspIndex, distribution, kwargs) = _getWkspIndexDistAndLabel(workspace, kwargs)
+    (x, y, dy, dx) = _getSpectrum(workspace, wkspIndex, distribution, withDy=True, withDx=True)
+    axes.errorbar(x, y, dy, dx, *args, **kwargs)
 
-    x = workspace.readX(wkspIndex)
-    y = workspace.readY(wkspIndex)
-    # TODO should extract dx but EventWorkspace does the wrong thing
-    dy = workspace.readE(wkspIndex)
-    if workspace.isHistogramData():
-        if not distribution:
-            y = y / (x[1:] - x[0:-1])
-            dy = dy / (x[1:] - x[0:-1])
-        x = .5*(x[0:-1]+x[1:])
-
-    axes.errorbar(x, y, dy, *args, **kwargs)
-
-
-def contour(axes, workspace, *args, **kwargs):
-    '''Essentially the same as :meth:`matplotlib.axes.Axes.contour`
-    but calculates the countour levels. Currently this only works with
-    workspaces that have a constant number of bins between spectra.
+def scatter(axes, workspace, *args, **kwargs):
+    '''Unpack mantid workspace and render it with matplotlib. ``args`` and
+    ``kwargs`` are passed to :meth:matplotlib.axes.Axes.scatter after special
+    keyword arguments are removed. This will automatically label the
+    line according to the spectrum number unless specified otherwise.
 
     @param workspace :: :class:`mantid.api.MatrixWorkspace` to extract the data from
+    @param specNum :: spectrum number to plot
+    @param wkspIndex :: workspace index to plot
     @param axes :: :class:`matplotlib.axes.Axes` object that will do the plotting
     @param distribution :: None (default) asks the workspace. False
     means divide by bin width. True means do not divide by bin width.
     Applies only when the the workspace is a histogram.
-    '''
-    (distribution, kwargs) = _getDistribution(workspace, kwargs)
 
+    Either ``specNum`` or ``wkspIndex`` needs to be specified. Giving
+    both will generate a :class:`exceptions.RuntimeError`.
+    '''
+    (wkspIndex, distribution, kwargs) = _getWkspIndexDistAndLabel(workspace, kwargs)
+    (x, y, _, _) = _getSpectrum(workspace, wkspIndex, distribution)
+    axes.scatter(x, y, *args, **kwargs)
+
+def _getContour(workspace, distribution):
     x = workspace.extractX()
     z = workspace.extractY()
 
@@ -132,6 +140,22 @@ def contour(axes, workspace, *args, **kwargs):
         y[index] = float(specNum)
 
     y = numpy.tile(y, (x.shape[1], 1)).transpose()
+
+    return (x,y,z)
+
+def contour(axes, workspace, *args, **kwargs):
+    '''Essentially the same as :meth:`matplotlib.axes.Axes.contour`
+    but calculates the countour levels. Currently this only works with
+    workspaces that have a constant number of bins between spectra.
+
+    @param workspace :: :class:`mantid.api.MatrixWorkspace` to extract the data from
+    @param axes :: :class:`matplotlib.axes.Axes` object that will do the plotting
+    @param distribution :: None (default) asks the workspace. False
+    means divide by bin width. True means do not divide by bin width.
+    Applies only when the the workspace is a histogram.
+    '''
+    (distribution, kwargs) = _getDistribution(workspace, kwargs)
+    (x,y,z) = _getContour(workspace, distribution)
 
     axes.contour(x, y, z, *args, **kwargs)
 
@@ -147,21 +171,6 @@ def contourf(axes, workspace, *args, **kwargs):
     Applies only when the the workspace is a histogram.
     '''
     (distribution, kwargs) = _getDistribution(workspace, kwargs)
-
-    x = workspace.extractX()
-    z = workspace.extractY()
-
-    if workspace.isHistogramData():
-        if not distribution:
-            z = z / (x[:,1:] - x[:,0:-1])
-        x = .5*(x[:,0:-1]+x[:,1:])
-
-    # y axis is spectrum number
-    y = numpy.zeros(workspace.getNumberHistograms(), dtype=float)
-    for index in range(workspace.getNumberHistograms()):
-        specNum = workspace.getSpectrum(index).getSpectrumNo()
-        y[index] = float(specNum)
-
-    y = numpy.tile(y, (x.shape[1], 1)).transpose()
+    (x,y,z) = _getContour(workspace, distribution)
 
     axes.contourf(x, y, z, *args, **kwargs)
