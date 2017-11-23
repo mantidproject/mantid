fa54fb7c2ba8c5f2abca35ecc5c37c01e56c072d
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-11-23 10:31:37 +0000

    Re #21238 Apply clang-format

--- qt/scientific_interfaces/test/EnggDiffFittingPresenterTest.h
+++ qt/scientific_interfaces/test/EnggDiffFittingPresenterTest.h
@@ -558,7 +558,9 @@ public:
 
     EXPECT_CALL(mockView, getPeakCentre()).Times(1);
 
-    EXPECT_CALL(mockView, getExpectedPeaksInput()).Times(1).WillOnce(Return(""));
+    EXPECT_CALL(mockView, getExpectedPeaksInput())
+        .Times(1)
+        .WillOnce(Return(""));
     ;
 
     EXPECT_CALL(mockView, setPeakList(testing::_)).Times(1);
@@ -668,7 +670,9 @@ public:
 
     EXPECT_CALL(mockView, getPeakCentre()).Times(1).WillOnce(Return(0.0133));
 
-    EXPECT_CALL(mockView, getExpectedPeaksInput()).Times(1).WillOnce(Return(""));
+    EXPECT_CALL(mockView, getExpectedPeaksInput())
+        .Times(1)
+        .WillOnce(Return(""));
 
     // string should be "0.133," instead
     EXPECT_CALL(mockView, setPeakList("0.0133")).Times(0);
