175e27aa47e7a776914e958d7a2d76f6ad6fc144
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-11-23 10:12:12 +0000

    Re #21238 Removed bank combo box from fitting tab
    The 'bank' combo box was no longer required in the fitting tab, as the
    list widget now takes care of bank number as well as run number. The
    combo box itself, and all related code, was therefore removed from the
    gui

--- qt/scientific_interfaces/EnggDiffraction/EnggDiffFittingViewQtWidget.cpp
+++ qt/scientific_interfaces/EnggDiffraction/EnggDiffFittingViewQtWidget.cpp
@@ -100,9 +100,6 @@ void EnggDiffFittingViewQtWidget::doSetup() {
   connect(m_ui.listWidget_fitting_run_num, SIGNAL(itemSelectionChanged()), this,
           SLOT(listViewFittingRun()));
 
-  connect(m_ui.comboBox_bank, SIGNAL(currentIndexChanged(int)), this,
-          SLOT(setBankDir(int)));
-
   connect(m_ui.pushButton_fitting_browse_peaks, SIGNAL(released()), this,
           SLOT(browseClicked()));
 
@@ -162,7 +159,6 @@ void EnggDiffFittingViewQtWidget::readSettings() {
   // user params
   m_ui.lineEdit_pushButton_run_num->setText(
       qs.value("user-params-fitting-focused-file", "").toString());
-  m_ui.comboBox_bank->setCurrentIndex(0);
   m_ui.lineEdit_fitting_peaks->setText(
       qs.value("user-params-fitting-peaks-to-fit", "").toString());
 
@@ -190,7 +186,6 @@ void EnggDiffFittingViewQtWidget::enable(bool enable) {
   m_ui.pushButton_fit->setEnabled(enable);
   m_ui.pushButton_clear_peak_list->setEnabled(enable);
   m_ui.pushButton_save_peak_list->setEnabled(enable);
-  m_ui.comboBox_bank->setEnabled(enable);
   m_ui.groupBox_fititng_preview->setEnabled(enable);
 }
 
@@ -495,14 +490,6 @@ void EnggDiffFittingViewQtWidget::enableFitAllButton(bool enable) const {
   m_ui.pushButton_fit_all->setEnabled(enable);
 }
 
-void EnggDiffFittingViewQtWidget::clearFittingComboBox() const {
-  m_ui.comboBox_bank->clear();
-}
-
-void EnggDiffFittingViewQtWidget::enableFittingComboBox(bool enable) const {
-  m_ui.comboBox_bank->setEnabled(enable);
-}
-
 void EnggDiffFittingViewQtWidget::clearFittingListWidget() const {
   m_ui.listWidget_fitting_run_num->clear();
 }
@@ -527,10 +514,6 @@ bool EnggDiffFittingViewQtWidget::listWidgetHasSelectedRow() const {
 void EnggDiffFittingViewQtWidget::setFittingListWidgetCurrentRow(
     int idx) const {
   m_ui.listWidget_fitting_run_num->setCurrentRow(idx);
-}
-
-int EnggDiffFittingViewQtWidget::getFittingComboIdx(std::string bank) const {
-  return m_ui.comboBox_bank->findText(QString::fromStdString(bank));
 }
 
 void EnggDiffFittingViewQtWidget::plotSeparateWindow() {
@@ -579,17 +562,6 @@ std::string EnggDiffFittingViewQtWidget::getExpectedPeaksInput() const {
 void EnggDiffFittingViewQtWidget::setPeakList(
     const std::string &peakList) const {
   m_ui.lineEdit_fitting_peaks->setText(QString::fromStdString(peakList));
-}
-
-void EnggDiffFittingViewQtWidget::setBankEmit() { emit setBank(); }
-
-void EnggDiffFittingViewQtWidget::setBankIdComboBox(int idx) {
-  QComboBox *bankName = m_ui.comboBox_bank;
-  bankName->setCurrentIndex(idx);
-}
-
-void EnggDiffFittingViewQtWidget::addBankItem(std::string bankID) {
-  m_ui.comboBox_bank->addItem(QString::fromStdString(bankID));
 }
 
 void EnggDiffFittingViewQtWidget::addRunNoItem(std::string runNo) {
