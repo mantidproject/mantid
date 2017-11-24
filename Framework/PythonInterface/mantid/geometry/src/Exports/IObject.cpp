55bcfac4750f38d44ec75470011d7b719851b4fa
mantid-builder <mantid-buildserver@mantidproject.org>
2017-11-24 10:34:36 +0000

    clang-format PR21245 b3934f3

--- Framework/PythonInterface/mantid/geometry/src/Exports/IObject.cpp
+++ Framework/PythonInterface/mantid/geometry/src/Exports/IObject.cpp
@@ -4,18 +4,13 @@
 #include <boost/python/class.hpp>
 #include <boost/python/register_ptr_to_python.hpp>
 
-
 using Mantid::Geometry::IObject;
 using namespace boost::python;
 
 GET_POINTER_SPECIALIZATION(IObject)
 
-
-
 void export_IObject() {
-    register_ptr_to_python<boost::shared_ptr<IObject>>();
+  register_ptr_to_python<boost::shared_ptr<IObject>>();
 
-    class_<IObject, boost::noncopyable>("IObject", no_init)
-      ;
+  class_<IObject, boost::noncopyable>("IObject", no_init);
 }
-
