aca8286bca5c632c80707e4c5e6623cfe3d31061
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2017-11-27 09:05:02 +0000

    Remove unused import. Re #21180

--- qt/python/mantidqt/widgets/algorithmselector/widget.py
+++ qt/python/mantidqt/widgets/algorithmselector/widget.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function
 from PyQt4.QtGui import QWidget, QPushButton, QComboBox, QTreeWidget, QVBoxLayout, QHBoxLayout, QCompleter
 from PyQt4.QtGui import QTreeWidgetItem
-from PyQt4.QtCore import QModelIndex, pyqtSignal, Qt
+from PyQt4.QtCore import QModelIndex, pyqtSignal
 from .presenter import IAlgorithmSelectorView, SelectedAlgorithm
 from mantidqt.utility import block_signals
 import re
