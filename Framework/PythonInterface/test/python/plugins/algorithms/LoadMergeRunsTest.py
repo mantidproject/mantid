413d36e13b2c0002f67b25bbfaf036886e502f98
Gagik Vardanyan <vardanyan@ill.fr>
2017-11-28 16:56:16 +0100

    Re #21156 made tests more orthogonal

--- Framework/PythonInterface/test/python/plugins/algorithms/LoadMergeRunsTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/LoadMergeRunsTest.py
@@ -12,7 +12,7 @@ class LoadMergeRunsTest(unittest.TestCase):
         config.appendDataSearchSubDir('ILL/IN16B/')
         config.appendDataSearchSubDir('ILL/D20/')
 
-    def test_single_run(self):
+    def test_single_run_load(self):
         out1 = LoadMergeRuns(Filename='170257')
         self.assertTrue(out1)
         self.assertEquals(out1.getName(), 'out1')
@@ -20,14 +20,14 @@ class LoadMergeRunsTest(unittest.TestCase):
         mtd.clear()
 
     def test_many_runs_summed(self):
-        out2 = LoadMergeRuns(Filename='170257+170258')
+        out2 = LoadMergeRuns(Filename='170257+170258',LoaderName='LoadILLIndirect')
         self.assertTrue(out2)
         self.assertEquals(out2.getName(), 'out2')
         self.assertTrue(isinstance(out2, MatrixWorkspace))
         mtd.clear()
 
     def test_many_runs_listed(self):
-        out3 = LoadMergeRuns(Filename='170257,170258')
+        out3 = LoadMergeRuns(Filename='170257,170258',LoaderName='LoadILLIndirect')
         self.assertTrue(out3)
         self.assertEquals(out3.getName(), 'out3')
         self.assertTrue(isinstance(out3, WorkspaceGroup))
@@ -39,7 +39,7 @@ class LoadMergeRunsTest(unittest.TestCase):
         mtd.clear()
 
     def test_many_runs_mixed(self):
-        out4 = LoadMergeRuns(Filename='170257+170258,170300+170302')
+        out4 = LoadMergeRuns(Filename='170257+170258,170300+170302',LoaderName='LoadILLIndirect')
         self.assertTrue(out4)
         self.assertEquals(out4.getName(), 'out4')
         self.assertTrue(isinstance(out4, WorkspaceGroup))
@@ -51,27 +51,28 @@ class LoadMergeRunsTest(unittest.TestCase):
         mtd.clear()
 
     def test_merge_options(self):
-        self.assertRaises(RuntimeError,LoadMergeRuns,
-                          Filename='170300+170301',
-                          OutputWorkspace='out5',
+        self.assertRaises(RuntimeError,LoadMergeRuns,Filename='170300+170301',
+                          OutputWorkspace='out5',LoaderName='LoadILLIndirect',
                           MergeRunsOptions=dict({'FailBehaviour':'Stop'}))
 
     def test_specific_loader(self):
-        out5 = LoadMergeRuns(Filename='170257',LoaderName='LoadILLIndirect',LoaderVersion=2)
+        out5 = LoadMergeRuns(Filename='170257',LoaderName='LoadILLIndirect',)
         self.assertTrue(out5)
         self.assertEquals(out5.getName(), 'out5')
         self.assertTrue(isinstance(out5, MatrixWorkspace))
         mtd.clear()
 
     def test_loader_option(self):
-        out6 = LoadMergeRuns(Filename='967101',LoaderOptions=dict({'DataType':'Raw'}))
+        out6 = LoadMergeRuns(Filename='967101',LoaderName='LoadILLDiffraction',
+                             LoaderVersion=1,LoaderOptions=dict({'DataType':'Raw'}))
         self.assertTrue(out6)
         self.assertEquals(out6.getName(), 'out6')
         self.assertTrue(isinstance(out6, MatrixWorkspace))
         mtd.clear()
 
     def test_output_hidden(self):
-        LoadMergeRuns(Filename='170257+170258,170300+170302', OutputWorkspace='__out')
+        LoadMergeRuns(Filename='170257+170258,170300+170302',LoaderName='LoadILLIndirect',
+                      OutputWorkspace='__out')
         self.assertTrue(mtd['__out'])
         self.assertTrue(isinstance(mtd['__out'], WorkspaceGroup))
         self.assertEquals(mtd['__out'].getNumberOfEntries(), 2)
@@ -81,7 +82,7 @@ class LoadMergeRunsTest(unittest.TestCase):
         self.assertEquals(mtd['__out'].getItem(1).getName(),'__170300_170302')
         mtd.clear()
 
-    def test_non_ill(self):
+    def test_non_ill_load(self):
         out7 = LoadMergeRuns(Filename='IRS26173,26174.RAW')
         self.assertTrue(out7)
         self.assertTrue(isinstance(out7, WorkspaceGroup))
