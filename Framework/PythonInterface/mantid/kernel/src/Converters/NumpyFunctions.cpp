f7b49ccfcfa2bfbef9a0ff72bbe69837c69e6510
Pete Peterson <petersonpf@ornl.gov>
2017-12-06 13:48:03 -0500

    Add additional function to create np arrays

--- Framework/PythonInterface/mantid/kernel/src/Converters/NumpyFunctions.cpp
+++ Framework/PythonInterface/mantid/kernel/src/Converters/NumpyFunctions.cpp
@@ -16,6 +16,23 @@ PyArrayObject *func_PyArray_NewFromDescr(int datatype, const int ndims,
       dims, // Length in each dimension
       nullptr, nullptr, 0, nullptr));
 }
+
+PyArrayObject *func_PyArray_NewFromDescr(const std::string &datadescr,
+                                         const int ndims, Py_intptr_t *dims) {
+  // convert the string description to an actual description
+  PyObject *date_type = Py_BuildValue("s", datadescr.c_str());
+  PyArray_Descr *descr;
+  PyArray_DescrConverter(date_type, &descr);
+
+  // create the array
+  PyArrayObject *nparray = reinterpret_cast<PyArrayObject *>(
+      PyArray_NewFromDescr(&PyArray_Type, descr, ndims, // rank
+                           dims, // Length in each dimension
+                           nullptr, nullptr, 0, nullptr));
+  Py_XDECREF(date_type); // function above incremented reference count
+
+  return nparray;
+}
 }
 }
 }
