f31a7563088aaa73bfad71304794eef174aba8e8
Martyn Gigg <martyn.gigg@gmail.com>
2017-12-06 17:37:40 +0000

    Fix Scintilla finder for macOS and homebrew
    Refs #0

--- buildconfig/CMake/QScintillaFindImpl.cmake
+++ buildconfig/CMake/QScintillaFindImpl.cmake
@@ -17,14 +17,16 @@ function (find_qscintilla qt_version)
     set ( _qsci_lib_names_debug
       qscintilla2d
     )
-    set ( _qsci_lib_paths
-      ${QT_LIBRARY_DIR}
-    )
-    set ( _qsci_include_paths
-      ${QT_INCLUDE_DIR}
-    )
     if ( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
       list ( APPEND _qsci_include_paths /usr/local/opt/qscintilla2qt4/include )
+      list ( APPEND _qsci_lib_paths /usr/local/opt/qscintilla2qt4/lib )
+    else ()
+      set ( _qsci_lib_paths
+        ${QT_LIBRARY_DIR}
+      )
+      set ( _qsci_include_paths
+        ${QT_INCLUDE_DIR}
+      )
     endif ()
   else()
     if ( NOT Qt5_FOUND)
@@ -38,34 +40,39 @@ function (find_qscintilla qt_version)
       qt5scintilla2
       libqscintilla2_qt5.dylib
     )
+    set ( _qsci_include_paths
+      ${Qt5Core_INCLUDE_DIRS}
+    )
     if ( MSVC )
       set ( _qsci_lib_paths
         ${THIRD_PARTY_DIR}/lib/qt5/lib
       )
-    endif()
-    set ( _qsci_include_paths
-      ${Qt5Core_INCLUDE_DIRS}
-    )
-    if ( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
+    elseif ( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
       list ( APPEND _qsci_include_paths /usr/local/opt/qscintilla2/include )
+      list ( APPEND _qsci_lib_paths /usr/local/opt/qscintilla2/lib )
     endif ()
   endif()
 
-  set ( _include_var QSCINTILLA_QT${qt_version}_INCLUDE_DIR )
+  set ( _include_var QSCINTILLA_QT${qt_version}_INC_DIR )
   find_path ( ${_include_var}
       NAMES Qsci/qsciglobal.h
       PATHS ${_qsci_include_paths}
       NO_DEFAULT_PATH
   )
-  set ( _library_var QSCINTILLA_QT${qt_version}_LIBRARY )
+  set ( _library_var QSCINTILLA_QT${qt_version}_LIB )
+  if ( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
+    set ( _default_path_opt NO_DEFAULT_PATH )
+  endif()
   find_library ( ${_library_var}
     NAMES ${_qsci_lib_names}
     PATHS ${_qsci_lib_paths}
+    ${_default_path_opt}
   )
-  set ( _library_var_debug QSCINTILLA_QT${qt_version}_LIBRARY_DEBUG )
+  set ( _library_var_debug QSCINTILLA_QT${qt_version}_LIB_DEBUG )
   find_library ( ${_library_var_debug}
     NAMES ${_qsci_lib_names_debug}
     PATHS ${_qsci_lib_paths}
+    ${_default_path_opt}
   )
 
   if ( ${_include_var} AND ${_library_var} )
