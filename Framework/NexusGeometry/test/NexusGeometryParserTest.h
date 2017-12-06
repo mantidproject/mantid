d6abfac5bbae875ef2fe1aa32881e9f32b2af8b0
Owen Arnold <owen.arnold@stfc.ac.uk>
2017-12-06 16:32:30 +0000

    Update tests

--- Framework/NexusGeometry/test/NexusGeometryParserTest.h
+++ Framework/NexusGeometry/test/NexusGeometryParserTest.h
@@ -8,8 +8,8 @@
 #include "MantidKernel/ConfigService.h"
 #include "MantidNexusGeometry/NexusGeometryParser.h"
 
-#include "MantidGeometry/Instrument/DetectorInfo.h"
 #include "MantidGeometry/Instrument/ComponentInfo.h"
+#include "MantidGeometry/Instrument/DetectorInfo.h"
 #include "MantidKernel/EigenConversionHelpers.h"
 #include <string>
 
@@ -18,8 +18,7 @@ using namespace NexusGeometry;
 
 class NexusGeometryParserTest : public CxxTest::TestSuite {
 public:
-
-  Geometry::Instrument_sptr extractInstrument(){
+  Geometry::Instrument_sptr extractInstrument() {
     // Initialise abstract instrument builder
     iAbstractBuilder_sptr iAbsBuilder_sptr(
         new iAbstractBuilder(this->testInstName));
@@ -41,53 +40,72 @@ public:
     return this->iAbsBuilder_sptr->_unAbstractInstrument();
   }
 
-  std::unique_ptr<Geometry::DetectorInfo> extractDetectorInfo(){
+  std::unique_ptr<Geometry::DetectorInfo> extractDetectorInfo() {
     auto instrument = extractInstrument();
     Geometry::ParameterMap pmap;
     return std::move(std::get<1>(instrument->makeBeamline(pmap)));
   }
 
-  std::pair<std::unique_ptr<Geometry::ComponentInfo>, std::unique_ptr<Geometry::DetectorInfo>>  extractBeamline(){
+  std::pair<std::unique_ptr<Geometry::ComponentInfo>,
+            std::unique_ptr<Geometry::DetectorInfo>>
+  extractBeamline() {
     auto instrument = extractInstrument();
     Geometry::ParameterMap pmap;
-    return {std::move(std::get<0>(instrument->makeBeamline(pmap))),std::move(std::get<1>(instrument->makeBeamline(pmap)))};
+    return {std::move(std::get<0>(instrument->makeBeamline(pmap))),
+            std::move(std::get<1>(instrument->makeBeamline(pmap)))};
   }
 
-  void test_basic_instrument_information(){
-      auto beamline = extractBeamline();
-      auto componentInfo = std::move(beamline.first);
-      auto detectorInfo = std::move(beamline.second);
-      TSM_ASSERT_EQUALS("Detectors + 1 monitor", detectorInfo->size(), 128 * 2 + 1); // TODO, currently NOT counting monitor. Needs fixing.
-      TSM_ASSERT_EQUALS("Detectors + 3 non-detector components", componentInfo->size(), detectorInfo->size()  + 3); // TODO, currently NOT counting source + sample + root!
+  void test_basic_instrument_information() {
+    auto beamline = extractBeamline();
+    auto componentInfo = std::move(beamline.first);
+    auto detectorInfo = std::move(beamline.second);
+    TSM_ASSERT_EQUALS(
+        "Detectors + 1 monitor", detectorInfo->size(),
+        128 * 2 + 1); // TODO, currently NOT counting monitor. Needs fixing.
+    TSM_ASSERT_EQUALS(
+        "Detectors + 3 non-detector components", componentInfo->size(),
+        detectorInfo->size() +
+            3); // TODO, currently NOT counting source + sample + root!
   }
 
   void test_simple_translation() {
     auto detectorInfo = extractDetectorInfo();
     // First pixel in bank 2
-    auto det0Postion = Kernel::toVector3d(detectorInfo->position(detectorInfo->indexOf(1100000)));
-    Eigen::Vector3d unitVector(0, 0, 1); // Fixed in file location vector attribute
-    const double magnitude = 4.0; // Fixed in file location value
-    Eigen::Vector3d offset(-0.498, -0.200, 0.00); // All offsets for pixel x, and y specified separately, z defaults to 0
+    auto det0Postion = Kernel::toVector3d(
+        detectorInfo->position(detectorInfo->indexOf(1100000)));
+    Eigen::Vector3d unitVector(0, 0,
+                               1); // Fixed in file location vector attribute
+    const double magnitude = 4.0;  // Fixed in file location value
+    Eigen::Vector3d offset(-0.498, -0.200, 0.00); // All offsets for pixel x,
+                                                  // and y specified separately,
+                                                  // z defaults to 0
     auto expectedPosition = offset + (magnitude * unitVector);
     TS_ASSERT(det0Postion.isApprox(expectedPosition));
   }
 
-  void test_complex_translation(){
+  void test_complex_translation() {
 
     auto detectorInfo = extractDetectorInfo();
     // First pixel in bank 1
 
-    auto det0Postion = Kernel::toVector3d(detectorInfo->position(detectorInfo->indexOf(2100000)));
-    Eigen::Vector3d unitVectorTranslation(0.2651564830210424, 0.0, 0.9642053928037905);// Fixed in file location vector attribute
-    const double magnitude = 4.148; // Fixed in file location value
+    auto det0Postion = Kernel::toVector3d(
+        detectorInfo->position(detectorInfo->indexOf(2100000)));
+    Eigen::Vector3d unitVectorTranslation(
+        0.2651564830210424, 0.0,
+        0.9642053928037905);         // Fixed in file location vector attribute
+    const double magnitude = 4.148;  // Fixed in file location value
     const double rotation = -15.376; // Fixed in file orientation value
-    Eigen::Vector3d rotationAxis(0, -1, 0); // Fixed in file orientation vector attribute
-    Eigen::Vector3d offset(-0.498, -0.200, 0.00); // All offsets for pixel x, and y specified separately, z defaults to 0
+    Eigen::Vector3d rotationAxis(
+        0, -1, 0); // Fixed in file orientation vector attribute
+    Eigen::Vector3d offset(-0.498, -0.200, 0.00); // All offsets for pixel x,
+                                                  // and y specified separately,
+                                                  // z defaults to 0
     auto affine = Eigen::Transform<double, 3, Eigen::Affine>::Identity();
     // Tranlsation of bank
     affine *= Eigen::Translation3d(magnitude * unitVectorTranslation);
     // Rotation of bank
-    affine *= Eigen::Quaterniond(Eigen::AngleAxisd(rotation * M_PI/180, rotationAxis));
+    affine *= Eigen::Quaterniond(
+        Eigen::AngleAxisd(rotation * M_PI / 180, rotationAxis));
     auto expectedPosition = affine * offset;
     TS_ASSERT(det0Postion.isApprox(expectedPosition, 1e-3));
   }
