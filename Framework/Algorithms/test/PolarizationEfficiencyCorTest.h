ab55668cd38b1459464df7902486ef2c2ef4ecf1
Antti Soininen <soininen@ill.fr>
2017-12-08 11:24:12 +0100

    Extends and refactor unit tests. Re #20962

--- Framework/Algorithms/test/PolarizationEfficiencyCorTest.h
+++ Framework/Algorithms/test/PolarizationEfficiencyCorTest.h
@@ -95,87 +95,14 @@ public:
   }
 
   void test_IdealCaseThreeInputs10Missing() {
-    using namespace Mantid::API;
-    using namespace Mantid::DataObjects;
-    using namespace Mantid::HistogramData;
-    using namespace Mantid::Kernel;
-    constexpr size_t nBins{3};
-    constexpr size_t nHist{2};
-    BinEdges edges{0.3, 0.6, 0.9, 1.2};
-    const double yVal = 2.3;
-    Counts counts{yVal, yVal, yVal};
-    MatrixWorkspace_sptr ws00 = create<Workspace2D>(nHist, Histogram(edges, counts));
-    MatrixWorkspace_sptr ws01 = ws00->clone();
-    MatrixWorkspace_sptr ws11 = ws00->clone();
-    WorkspaceGroup_sptr inputWS = boost::make_shared<WorkspaceGroup>();
-    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws00));
-    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws01));
-    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws11));
-    for (size_t i = 0; i != 3; ++i) {
-      MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>(inputWS->getItem(i));
-      for (size_t j = 0; j != nHist; ++j) {
-        ws->mutableY(j) *= static_cast<double>(i + 1);
-        ws->mutableE(j) *= static_cast<double>(i + 1);
-      }
-    }
-    auto effWS = efficiencies(edges);
-    constexpr char *OUTWS_NAME{"output"};
-    PolarizationEfficiencyCor alg;
-    alg.setChild(true);
-    alg.setRethrows(true);
-    TS_ASSERT_THROWS_NOTHING(alg.initialize())
-    TS_ASSERT(alg.isInitialized())
-    TS_ASSERT_THROWS_NOTHING(alg.setProperty("InputWorkspace", inputWS))
-    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("OutputWorkspace", OUTWS_NAME))
-    TS_ASSERT_THROWS_NOTHING(alg.setProperty("Efficiencies", effWS))
-    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("Flippers", "00, 01, 11"))
-    TS_ASSERT_THROWS_NOTHING(alg.execute())
-    TS_ASSERT(alg.isExecuted())
-    WorkspaceGroup_sptr outputWS = alg.getProperty("OutputWorkspace");
-    TS_ASSERT(outputWS)
-    TS_ASSERT_EQUALS(outputWS->getNumberOfEntries(), 4)
-    const std::array<std::string, 4> POL_DIRS{{"++", "+-", "-+", "--"}};
-    for (size_t i = 0; i != 4; ++i) {
-      const auto &dir = POL_DIRS[i];
-      const std::string wsName = OUTWS_NAME + std::string("_") + dir;
-      MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>(outputWS->getItem(wsName));
-      TS_ASSERT(ws)
-      const double expected = [yVal, &dir]() {
-        if (dir == "++") {
-          return yVal;
-        } else if (dir == "--") {
-          return 3. * yVal;
-        } else {
-          return 2. * yVal;
-        }
-      }();
-      const double expectedError = [yVal, &dir]() {
-        if (dir == "++") {
-          return std::sqrt(yVal);
-        } else if (dir == "--") {
-          return 3. * std::sqrt(yVal);
-        } else if (dir == "+-"){
-          return 2. * std::sqrt(yVal);
-        } else {
-          return 0.;
-        }
-      }();
-      TS_ASSERT_EQUALS(ws->getNumberHistograms(), nHist)
-      for (size_t j = 0; j != nHist; ++j) {
-        const auto &xs = ws->x(j);
-        const auto &ys = ws->y(j);
-        const auto &es = ws->e(j);
-        TS_ASSERT_EQUALS(ys.size(), nBins)
-        for (size_t k = 0; k != nBins; ++k) {
-          TS_ASSERT_EQUALS(xs[k], edges[k])
-          TS_ASSERT_EQUALS(ys[k], expected)
-          TS_ASSERT_EQUALS(es[k], expectedError)
-        }
-      }
-    }
+    threeInputsTest("10");
   }
 
   void test_IdealCaseThreeInputs01Missing() {
+    threeInputsTest("01");
+  }
+
+  void test_IdealCaseTwoInputsWithAnalyzer() {
     using namespace Mantid::API;
     using namespace Mantid::DataObjects;
     using namespace Mantid::HistogramData;
@@ -186,19 +113,15 @@ public:
     const double yVal = 2.3;
     Counts counts{yVal, yVal, yVal};
     MatrixWorkspace_sptr ws00 = create<Workspace2D>(nHist, Histogram(edges, counts));
-    MatrixWorkspace_sptr ws10 = ws00->clone();
     MatrixWorkspace_sptr ws11 = ws00->clone();
     WorkspaceGroup_sptr inputWS = boost::make_shared<WorkspaceGroup>();
     inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws00));
-    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws10));
     inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws11));
-    for (size_t i = 0; i != 3; ++i) {
-      MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>(inputWS->getItem(i));
-      for (size_t j = 0; j != nHist; ++j) {
-        ws->mutableY(j) *= static_cast<double>(i + 1);
-        ws->mutableE(j) *= static_cast<double>(i + 1);
-      }
+    for (size_t i = 0; i != nHist; ++i) {
+      ws11->mutableY(i) *= 2.;
+      ws11->mutableE(i) *= 2.;
     }
+
     auto effWS = efficiencies(edges);
     constexpr char *OUTWS_NAME{"output"};
     PolarizationEfficiencyCor alg;
@@ -209,7 +132,7 @@ public:
     TS_ASSERT_THROWS_NOTHING(alg.setProperty("InputWorkspace", inputWS))
     TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("OutputWorkspace", OUTWS_NAME))
     TS_ASSERT_THROWS_NOTHING(alg.setProperty("Efficiencies", effWS))
-    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("Flippers", "00, 10, 11"))
+    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("Flippers", "00, 11"))
     TS_ASSERT_THROWS_NOTHING(alg.execute())
     TS_ASSERT(alg.isExecuted())
     WorkspaceGroup_sptr outputWS = alg.getProperty("OutputWorkspace");
@@ -225,20 +148,18 @@ public:
         if (dir == "++") {
           return yVal;
         } else if (dir == "--") {
-          return 3. * yVal;
-        } else {
           return 2. * yVal;
+        } else {
+          return 0.;
         }
       }();
       const double expectedError = [yVal, &dir]() {
         if (dir == "++") {
           return std::sqrt(yVal);
         } else if (dir == "--") {
-          return 3. * std::sqrt(yVal);
-        } else if (dir == "-+"){
           return 2. * std::sqrt(yVal);
         } else {
-          return 0.;
+            return 0.;
         }
       }();
       TS_ASSERT_EQUALS(ws->getNumberHistograms(), nHist)
@@ -275,6 +196,92 @@ private:
     axis->setLabel(3, "P2");
     ws->replaceAxis(1, axis.release());
     return ws;
+  }
+
+  void threeInputsTest(const std::string &missingFlipperConf) {
+    using namespace Mantid::API;
+    using namespace Mantid::DataObjects;
+    using namespace Mantid::HistogramData;
+    using namespace Mantid::Kernel;
+    constexpr size_t nBins{3};
+    constexpr size_t nHist{2};
+    BinEdges edges{0.3, 0.6, 0.9, 1.2};
+    const double yVal = 2.3;
+    Counts counts{yVal, yVal, yVal};
+    MatrixWorkspace_sptr ws00 = create<Workspace2D>(nHist, Histogram(edges, counts));
+    MatrixWorkspace_sptr wsXX = ws00->clone();
+    MatrixWorkspace_sptr ws11 = ws00->clone();
+    WorkspaceGroup_sptr inputWS = boost::make_shared<WorkspaceGroup>();
+    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws00));
+    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(wsXX));
+    inputWS->addWorkspace(boost::dynamic_pointer_cast<Workspace>(ws11));
+    for (size_t i = 0; i != 3; ++i) {
+      MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>(inputWS->getItem(i));
+      for (size_t j = 0; j != nHist; ++j) {
+        ws->mutableY(j) *= static_cast<double>(i + 1);
+        ws->mutableE(j) *= static_cast<double>(i + 1);
+      }
+    }
+    auto effWS = efficiencies(edges);
+    constexpr char *OUTWS_NAME{"output"};
+    PolarizationEfficiencyCor alg;
+    alg.setChild(true);
+    alg.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("InputWorkspace", inputWS))
+    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("OutputWorkspace", OUTWS_NAME))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("Efficiencies", effWS))
+    const std::string presentFlipperConf = missingFlipperConf == "01" ? "10" : "01";
+    const std::string flipperConf = "00, " + presentFlipperConf + ", 11";
+    TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("Flippers", flipperConf))
+    TS_ASSERT_THROWS_NOTHING(alg.execute())
+    TS_ASSERT(alg.isExecuted())
+    WorkspaceGroup_sptr outputWS = alg.getProperty("OutputWorkspace");
+    TS_ASSERT(outputWS)
+    TS_ASSERT_EQUALS(outputWS->getNumberOfEntries(), 4)
+    const std::array<std::string, 4> POL_DIRS{{"++", "+-", "-+", "--"}};
+    for (size_t i = 0; i != 4; ++i) {
+      const auto &dir = POL_DIRS[i];
+      const std::string wsName = OUTWS_NAME + std::string("_") + dir;
+      MatrixWorkspace_sptr ws = boost::dynamic_pointer_cast<MatrixWorkspace>(outputWS->getItem(wsName));
+      TS_ASSERT(ws)
+      const double expected = [yVal, &dir]() {
+        if (dir == "++") {
+          return yVal;
+        } else if (dir == "--") {
+          return 3. * yVal;
+        } else {
+          return 2. * yVal;
+        }
+      }();
+      const double expectedError = [yVal, &dir, &missingFlipperConf]() {
+        if (dir == "++") {
+          return std::sqrt(yVal);
+        } else if (dir == "--") {
+          return 3. * std::sqrt(yVal);
+        } else {
+          std::string conf = std::string(dir.front() == '+' ? "0" : "1") + std::string(dir.back() == '+' ? "0" : "1");
+          if (conf != missingFlipperConf) {
+            return 2. * std::sqrt(yVal);
+          } else {
+            return 0.;
+          }
+        }
+      }();
+      TS_ASSERT_EQUALS(ws->getNumberHistograms(), nHist)
+      for (size_t j = 0; j != nHist; ++j) {
+        const auto &xs = ws->x(j);
+        const auto &ys = ws->y(j);
+        const auto &es = ws->e(j);
+        TS_ASSERT_EQUALS(ys.size(), nBins)
+        for (size_t k = 0; k != nBins; ++k) {
+          TS_ASSERT_EQUALS(xs[k], edges[k])
+          TS_ASSERT_EQUALS(ys[k], expected)
+          TS_ASSERT_EQUALS(es[k], expectedError)
+        }
+      }
+    }
   }
 };
 
