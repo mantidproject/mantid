76b73960f77239ad95f9ffa652a5e12d7dc7710e
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2017-12-12 10:33:00 +0000

    Re #21350 Make chopper mode case insensitive and better validated

--- scripts/Diffraction/isis_powder/polaris_routines/polaris_advanced_config.py
+++ scripts/Diffraction/isis_powder/polaris_routines/polaris_advanced_config.py
@@ -93,9 +93,12 @@ variables = {
 
 
 def get_mode_specific_dict(mode):
-    return {"focused_cropping_values":
-            pdf_focused_cropping_values if mode == "PDF"
-            else rietveld_focused_cropping_values}
+    mode = mode.lower()
+    if mode == "pdf":
+        return {"focused_cropping_values": pdf_focused_cropping_values}
+    if mode == "rietveld":
+        return {"focused_cropping_values": rietveld_focused_cropping_values}
+    raise ValueError("Invalid chopper mode {}".format(mode))
 
 
 def get_all_adv_variables(mode="PDF"):
