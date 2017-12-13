9a044e38b63401b4faaf66e60a14f1ef9083dcc7
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2017-12-13 16:51:19 +0000

    Export GenericDialog. Re #21281

--- qt/python/mantidqt/test/test_algorithm_dialog.py
+++ qt/python/mantidqt/test/test_algorithm_dialog.py
@@ -1,8 +1,12 @@
 import unittest
 
-from qtpy.QtWidgets import QWidget
+from qtpy.QtWidgets import QApplication, QWidget
 
-from _widgetscoreqt5 import AlgorithmDialog
+from mantidqt.utils.qt.plugins import setup_library_paths
+from mantid.simpleapi import AlgorithmManager
+from _widgetscoreqt5 import AlgorithmDialog, GenericDialog
+
+setup_library_paths()
 
 
 class TestAlgorithmDialog(unittest.TestCase):
@@ -11,3 +15,12 @@ class TestAlgorithmDialog(unittest.TestCase):
         self.assertTrue(issubclass(AlgorithmDialog, QWidget))
         self.assertTrue('setAlgorithm' in dir(AlgorithmDialog))
         self.assertTrue('addAlgorithmObserver' in dir(AlgorithmDialog))
+
+    def test_generic_dialog(self):
+        app = QApplication([''])
+        dialog = GenericDialog()
+        alg = AlgorithmManager.create('Load')
+        dialog.setAlgorithm(alg)
+        dialog.initializeLayout()
+        dialog.show()
+        app.exec_()
