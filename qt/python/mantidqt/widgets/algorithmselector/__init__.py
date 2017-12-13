14fd95b1efd29f217645f698e3babcd40fdb4e71
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2017-12-13 11:56:02 +0000

    Ignore flake8 warning. Re #21180

--- qt/python/mantidqt/widgets/algorithmselector/__init__.py
+++ qt/python/mantidqt/widgets/algorithmselector/__init__.py
@@ -14,4 +14,6 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# flake8: noqa
 from .widget import AlgorithmSelectorWidget
