bb5a3fe4783340ea9f9c4b284d59837a89b3c0ed
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2017-12-18 10:15:25 +0000

    Export InterfaceManager. Re #21281

--- qt/python/mantidqt/test/test_algorithm_dialog.py
+++ qt/python/mantidqt/test/test_algorithm_dialog.py
@@ -1,15 +1,41 @@
 import unittest
 
-from qtpy.QtWidgets import QApplication, QWidget
+from qtpy.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton
 
+from mantid.api import PythonAlgorithm
+from mantid.kernel import Direction, FloatArrayProperty
+from mantid.simpleapi import AlgorithmManager, AlgorithmFactory
 from mantidqt.utils.qt.plugins import setup_library_paths
-from mantid.simpleapi import AlgorithmManager
-from _widgetscoreqt5 import AlgorithmDialog, GenericDialog
+from mantidqt.utils.qt.testing import gui_test, gui_test_case
+
+from _widgetscoreqt5 import AlgorithmDialog, GenericDialog, InterfaceManager
 
 setup_library_paths()
 
 
+class AlgorithmDialogMockAlgorithm(PythonAlgorithm):
+
+    def category(self):
+        return 'Examples'
+
+    def PyInit(self):
+        self.declareProperty("InValue", 0)
+        self.declareProperty("DoubleValue", 1.0)
+        self.declareProperty(FloatArrayProperty("Floats", direction=Direction.Input))
+        self.declareProperty("OutValue", 0, direction=Direction.Output)
+
+    def PyExec(self):
+        pass
+
+
+@gui_test_case
 class TestAlgorithmDialog(unittest.TestCase):
+
+    def setUp(self):
+        AlgorithmFactory.subscribe(AlgorithmDialogMockAlgorithm)
+
+    def tearDown(self):
+        AlgorithmFactory.unsubscribe('AlgorithmDialogMockAlgorithm', 1)
 
     def test_it_exists(self):
         self.assertTrue(issubclass(AlgorithmDialog, QWidget))
@@ -17,10 +43,16 @@ class TestAlgorithmDialog(unittest.TestCase):
         self.assertTrue('addAlgorithmObserver' in dir(AlgorithmDialog))
 
     def test_generic_dialog(self):
-        app = QApplication([''])
         dialog = GenericDialog()
-        alg = AlgorithmManager.create('Load')
+        alg = AlgorithmManager.create('AlgorithmDialogMockAlgorithm')
         dialog.setAlgorithm(alg)
         dialog.initializeLayout()
-        dialog.show()
-        app.exec_()
+        input_widgets = dialog.findChildren(QLineEdit)
+        self.assertEqual(len(input_widgets), 3)
+
+    def test_interface_manager(self):
+        manager = InterfaceManager()
+        dialog = manager.createDialogFromName("AlgorithmDialogMockAlgorithm", -1)
+        self.assertTrue(dialog is not None)
+        input_widgets = dialog.findChildren(QLineEdit)
+        self.assertEqual(len(input_widgets), 3)
