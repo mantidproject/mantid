2fc70a66a499d38b8bbd8b6f8a8e723aba475868
Matthew Andrew <matthew.andrew@tessella.com>
2017-12-19 11:02:59 +0000

    Re #21438 Mocked out batch file reader

--- scripts/test/SANS/gui_logic/run_tab_presenter_test.py
+++ scripts/test/SANS/gui_logic/run_tab_presenter_test.py
@@ -12,7 +12,8 @@ from sans.common.enums import (SANSFacility, ReductionDimensionality, SaveType, 
                                RangeStepType, FitType)
 from sans.test_helper.user_file_test_helper import (create_user_file, sample_user_file, sample_user_file_gravity_OFF)
 from sans.test_helper.mock_objects import (create_mock_view)
-from sans.test_helper.common import (remove_file, save_to_csv)
+from sans.test_helper.common import (remove_file)
+from sans.common.enums import BatchReductionEntry
 
 
 if sys.version_info.major == 3:
@@ -21,22 +22,41 @@ else:
     import mock
 
 
-BATCH_FILE_TEST_CONTENT_1 = "# MANTID_BATCH_FILE add more text here\n" \
-                            "sample_sans,1,sample_trans,2,sample_direct_beam,3," \
-                            "output_as,test_file,user_file,user_test_file\n" \
-                            "sample_sans,1,can_sans,2,output_as,test_file2\n"
+# BATCH_FILE_TEST_CONTENT_1 = "# MANTID_BATCH_FILE add more text here\n" \
+#                             "sample_sans,1,sample_trans,2,sample_direct_beam,3," \
+#                             "output_as,test_file,user_file,user_test_file\n" \
+#                             "sample_sans,1,can_sans,2,output_as,test_file2\n"
+
+BATCH_FILE_TEST_CONTENT_1 = [{BatchReductionEntry.SampleScatter: 1, BatchReductionEntry.SampleTransmission: 2,
+                              BatchReductionEntry.SampleDirect: 3, BatchReductionEntry.Output: 'test_file',
+                              BatchReductionEntry.UserFile: 'user_test_file'},
+                             {BatchReductionEntry.SampleScatter: 1, BatchReductionEntry.CanScatter: 2,
+                              BatchReductionEntry.Output: 'test_file2'}]
+
+BATCH_FILE_TEST_CONTENT_2 = [{BatchReductionEntry.SampleScatter: 'SANS2D00022024',
+                              BatchReductionEntry.SampleTransmission: 'SANS2D00022048',
+                              BatchReductionEntry.SampleDirect: 'SANS2D00022048',
+                              BatchReductionEntry.Output: 'test_file', BatchReductionEntry.UserFile: 'user_test_file'},
+                             {BatchReductionEntry.SampleScatter: 'SANS2D00022024', BatchReductionEntry.Output: 'test_file2'}]
 
 
-BATCH_FILE_TEST_CONTENT_2 = "# MANTID_BATCH_FILE add more text here\n" \
-                            "sample_sans,SANS2D00022024,sample_trans,SANS2D00022048," \
-                            "sample_direct_beam,SANS2D00022048,output_as,test_file\n" \
-                            "sample_sans,SANS2D00022024,output_as,test_file2\n"
+# BATCH_FILE_TEST_CONTENT_2 = "# MANTID_BATCH_FILE add more text here\n" \
+#                             "sample_sans,SANS2D00022024,sample_trans,SANS2D00022048," \
+#                             "sample_direct_beam,SANS2D00022048,output_as,test_file\n" \
+#                             "sample_sans,SANS2D00022024,output_as,test_file2\n"
 
 
 class RunTabPresenterTest(unittest.TestCase):
     def setUp(self):
         config.setFacility("ISIS")
         config.setString("default.instrument", "SANS2D")
+        patcher = mock.patch('sans.gui_logic.presenter.run_tab_presenter.BatchCsvParser')
+        self.addCleanup(patcher.stop)
+        self.BatchCsvParserMock = patcher.start()
+
+        self.os_patcher = mock.patch('sans.gui_logic.presenter.run_tab_presenter.os')
+        self.addCleanup(self.os_patcher.stop)
+        self.osMock = self.os_patcher.start()
 
     def test_that_will_load_user_file(self):
         # Setup presenter and mock view
@@ -121,6 +141,7 @@ class RunTabPresenterTest(unittest.TestCase):
         remove_file(user_file_path)
 
     def test_fails_silently_when_user_file_does_not_exist(self):
+        self.os_patcher.stop()
         view, _, _ = create_mock_view("non_existent_user_file")
 
         presenter = RunTabPresenter(SANSFacility.ISIS)
@@ -132,8 +153,10 @@ class RunTabPresenterTest(unittest.TestCase):
         except:  # noqa
             has_raised = True
         self.assertFalse(has_raised)
+        self.os_patcher.start()
 
     def test_that_loads_batch_file_and_places_it_into_table(self):
+
         # Arrange
         batch_file_path, user_file_path, presenter, view = self._get_files_and_mock_presenter(BATCH_FILE_TEST_CONTENT_1)
 
@@ -141,7 +164,7 @@ class RunTabPresenterTest(unittest.TestCase):
         presenter.on_batch_file_load()
 
         # Assert
-        self.assertTrue(view.add_row.call_count == 2)
+        self.assertEqual(view.add_row.call_count, 2)
         expected_first_row = "SampleScatter:1,ssp:,SampleTrans:2,stp:,SampleDirect:3,sdp:," \
                              "CanScatter:,csp:,CanTrans:,ctp:,CanDirect:,cdp:,OutputName:test_file"
         expected_second_row = "SampleScatter:1,ssp:,SampleTrans:,stp:,SampleDirect:,sdp:," \
@@ -154,6 +177,7 @@ class RunTabPresenterTest(unittest.TestCase):
         self._remove_files(user_file_path=user_file_path, batch_file_path=batch_file_path)
 
     def test_fails_silently_when_batch_file_does_not_exist(self):
+        self.os_patcher.stop()
         presenter = RunTabPresenter(SANSFacility.ISIS)
         user_file_path = create_user_file(sample_user_file)
         view, settings_diagnostic_tab, masking_table = create_mock_view(user_file_path, "non_existent_batch_file")
@@ -168,6 +192,7 @@ class RunTabPresenterTest(unittest.TestCase):
 
         # Clean up
         self._remove_files(user_file_path=user_file_path)
+        self.os_patcher.start()
 
     def test_that_gets_states_from_view(self):
         # Arrange
@@ -255,10 +280,8 @@ class RunTabPresenterTest(unittest.TestCase):
 
     def test_that_returns_none_when_index_does_not_exist(self):
         # Arrange
-        batch_file_path = save_to_csv(BATCH_FILE_TEST_CONTENT_2)
-        user_file_path = create_user_file(sample_user_file)
+        batch_file_path, user_file_path, presenter, _ = self._get_files_and_mock_presenter(BATCH_FILE_TEST_CONTENT_2)
         view, _, _ = create_mock_view(user_file_path, batch_file_path)
-        presenter = RunTabPresenter(SANSFacility.ISIS)
         presenter.set_view(view)
         presenter.on_user_file_load()
         presenter.on_batch_file_load()
@@ -386,9 +409,11 @@ class RunTabPresenterTest(unittest.TestCase):
             if PropertyManagerDataService.doesExist(element):
                 PropertyManagerDataService.remove(element)
 
-    @staticmethod
-    def _get_files_and_mock_presenter(content, row_user_file_path = ""):
-        batch_file_path = save_to_csv(content)
+    def _get_files_and_mock_presenter(self, content, row_user_file_path = ""):
+        batch_parser = mock.MagicMock()
+        batch_parser.parse_batch_file = mock.MagicMock(return_value=content)
+        self.BatchCsvParserMock.return_value = batch_parser
+        batch_file_path = 'batch_file_path'
         user_file_path = create_user_file(sample_user_file)
         view, _, _ = create_mock_view(user_file_path, batch_file_path, row_user_file_path)
         # We just use the sample_user_file since it exists.
