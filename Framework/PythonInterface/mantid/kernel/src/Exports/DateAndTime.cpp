3140291b603d8b1d27e134db2aefa834e537669e
Pete Peterson <petersonpf@ornl.gov>
2017-12-29 13:52:11 -0500

    Expose converting to np.datetime64

--- Framework/PythonInterface/mantid/kernel/src/Exports/DateAndTime.cpp
+++ Framework/PythonInterface/mantid/kernel/src/Exports/DateAndTime.cpp
@@ -1,10 +1,20 @@
 #include "MantidKernel/DateAndTime.h"
+#include "MantidPythonInterface/kernel/Converters/NumpyFunctions.h"
 #include <boost/python/class.hpp>
 #include <boost/python/operators.hpp> // Also provides self
+#include <numpy/arrayobject.h>
+#include <numpy/arrayscalars.h>
 
 using namespace Mantid::Types::Core;
+using namespace Mantid::PythonInterface;
 using namespace boost::python;
 using boost::python::arg;
+
+namespace {
+// there is a different EPOCH for DateAndTime vs npy_datetime
+const npy_datetime UNIX_EPOCH_NS =
+    DateAndTime("1975-01-01T00:00").totalNanoseconds();
+}
 
 /** Circumvent a bug in IPython 1.1, which chokes on nanosecond precision
  * datetimes.
@@ -17,6 +27,20 @@ std::string ISO8601StringPlusSpace(DateAndTime &self) {
   return self.toISO8601String() + " ";
 }
 
+// for numpy's datetime64. This is panda's name for the function.
+PyObject *to_datetime64(DateAndTime &self) {
+  PyArray_Descr *descr = Converters::Impl::func_PyArray_Descr(
+      "M8[ns]"); // datetime64[ns] from 64bit integer
+
+  // create object
+  npy_datetime abstime =
+      static_cast<npy_datetime>(self.totalNanoseconds()) - UNIX_EPOCH_NS;
+  PyObject *ret =
+      PyArray_Scalar(reinterpret_cast<char *>(&abstime), descr, nullptr);
+
+  return ret;
+}
+
 void export_DateAndTime() {
   class_<DateAndTime>("DateAndTime", no_init)
       // Constructors
@@ -24,15 +48,22 @@ void export_DateAndTime() {
                                    "Construct from an ISO8601 string"))
       .def(init<double, double>(
           (arg("self"), arg("seconds"), arg("nanoseconds")),
-          "Construct using a number of seconds and nanoseconds (floats)"))
+          "Construct using a number of seconds and nanoseconds (floats)since "
+          "1990-01-01T00:00"))
       .def(init<int64_t, int64_t>(
           (arg("self"), arg("seconds"), arg("nanoseconds")),
-          "Construct using a number of seconds and nanoseconds (integers)"))
-      .def(init<int64_t>((arg("self"), arg("total nanoseconds")),
-                         "Construct a total number of nanoseconds"))
-      .def("total_nanoseconds", &DateAndTime::totalNanoseconds, arg("self"))
-      .def("totalNanoseconds", &DateAndTime::totalNanoseconds, arg("self"))
+          "Construct using a number of seconds and nanoseconds (integers) "
+          "since 1990-01-01T00:00"))
+      .def(init<int64_t>(
+          (arg("self"), arg("total_nanoseconds")),
+          "Construct a total number of nanoseconds since 1990-01-01T00:00"))
+      .def("total_nanoseconds", &DateAndTime::totalNanoseconds, arg("self"),
+           "Since 1990-01-01T00:00")
+      .def("totalNanoseconds", &DateAndTime::totalNanoseconds, arg("self"),
+           "Since 1990-01-01T00:00")
       .def("setToMinimum", &DateAndTime::setToMinimum, arg("self"))
+      .def("to_datetime64", &to_datetime64, arg("self"),
+           "Convert to numpy.datetime64")
       .def("__str__", &ISO8601StringPlusSpace, arg("self"))
       .def("__long__", &DateAndTime::totalNanoseconds, arg("self"))
       .def("__int__", &DateAndTime::totalNanoseconds, arg("self"))
