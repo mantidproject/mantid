144755c8fe4ab73e4a07b45afdedbec5baf4fc4b
Edward Brown <edward.brown@stfc.ac.uk>
2018-01-08 11:50:03 +0000

    Re #21427: Removed debugging instrumentation minus GIL lock.

--- Framework/PythonInterface/mantid/kernel/src/Exports/ConfigObserver.cpp
+++ Framework/PythonInterface/mantid/kernel/src/Exports/ConfigObserver.cpp
@@ -1,12 +1,8 @@
 #include "MantidKernel/ConfigObserver.h"
-#include "MantidAPI/FrameworkManager.h"
 #include "MantidPythonInterface/kernel/Environment/GlobalInterpreterLock.h"
 #include <boost/python/class.hpp>
 #include <boost/python/def.hpp>
 #include <boost/python/pure_virtual.hpp>
-#include <iostream>
-#include <csignal>
-#include <execinfo.h>
 
 using namespace boost::python;
 using Mantid::Kernel::ConfigObserver;
@@ -20,26 +16,9 @@ public:
 
   void onValueChanged(const std::string &name, const std::string &newValue,
                       const std::string &prevValue) override {
-    try {
-      GlobalInterpreterLock lock;
-      auto onValueChangedOverride = this->get_override("onValueChanged");
-      onValueChangedOverride(name, newValue, prevValue);
-    } catch (const std::exception &exc) {
-      std::cerr << "  what(): " << exc.what() << "\n\n";
-    } catch (boost::python::error_already_set &) {
-      PyErr_Print();
-    } catch (...) {
-      std::cerr << "  what(): Unknown exception type. No more information "
-                   "available\n\n";
-      std::cerr << "Backtrace:\n";
-      void *trace_elems[32];
-      int trace_elem_count(backtrace(trace_elems, 32));
-      char **stack_syms(backtrace_symbols(trace_elems, trace_elem_count));
-      for (int i = 0; i < trace_elem_count; ++i) {
-        std::cerr << ' ' << stack_syms[i] << '\n';
-      }
-      free(stack_syms);
-    }
+    GlobalInterpreterLock lock;
+    auto onValueChangedOverride = this->get_override("onValueChanged");
+    onValueChangedOverride(name, newValue, prevValue);
   }
 };
 
