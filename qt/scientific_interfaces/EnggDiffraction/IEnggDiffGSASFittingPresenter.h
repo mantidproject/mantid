5843b08574ae6f90cecd1c2e25087755fbaf2d2a
Joe Ramsay <joseph.ramsay@stfc.ac.uk>
2018-01-09 11:04:14 +0000

    Re #15552 Add skeleton interfaces for GSAS tab MVP
    Basic interfaces and a header for the presenter have been written for GSAS
    fitting tab of the engg gui, but no functionality is yet in place

---
+++ qt/scientific_interfaces/EnggDiffraction/IEnggDiffGSASFittingPresenter.h
@@ -0,0 +1,32 @@
+#ifndef MANTIDQTCUSTOMINTERFACES_ENGGDIFFRACTION_IENGGDIFFGSASFITTINGPRESENTER_H_
+#define MANTIDQTCUSTOMINTERFACES_ENGGDIFFRACTION_IENGGDIFFGSASFITTINGPRESENTER_H_
+
+namespace MantidQt {
+namespace CustomInterfaces {
+
+class IEnggDiffGSASFittingPresenter {
+
+public:
+  virtual ~IEnggDiffGSASFittingPresenter() = default;
+
+  // User actions, triggered by the (passive) view, 
+  // which need handling in implementation
+  enum Notification {
+    Start, ///< Start and setup the interface
+  };
+
+  /**
+   * Notifications sent through the presenter when something changes
+   * in the view. This plays the role of signals emitted by the view
+   * to this presenter.
+   *
+   * @param notif Type of notification to process.
+   */
+  virtual void notify(IEnggDiffGSASFittingPresenter::Notification notif) = 0;
+
+};
+
+} // namespace CustomInterfaces
+} // namespace MantidQt
+
+#endif // MANTIDQTCUSTOMINTERFACES_ENGGDIFFRACTION_IENGGDIFFGSASFITTINGPRESENTER_H_
