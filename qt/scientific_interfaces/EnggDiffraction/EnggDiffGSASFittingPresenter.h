8ad1ef993a3b4bcc9b3ddb5ce3f5a77ae9485e1d
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-01-09 11:29:52 +0000

    Re #15552 Add skeleton presenter implementation

--- qt/scientific_interfaces/EnggDiffraction/EnggDiffGSASFittingPresenter.h
+++ qt/scientific_interfaces/EnggDiffraction/EnggDiffGSASFittingPresenter.h
@@ -12,22 +12,26 @@
 
 namespace MantidQt {
 namespace CustomInterfaces {
- 
+
 // needs to be dll-exported for the tests
-  class MANTIDQT_ENGGDIFFRACTION_DLL EnggDiffGSASFittingPresenter
+class MANTIDQT_ENGGDIFFRACTION_DLL EnggDiffGSASFittingPresenter
     : public QObject,
       public IEnggDiffGSASFittingPresenter {
   // Q_OBJECT for 'connect' with thread/worker
   Q_OBJECT
 
 public:
-  EnggDiffGSASFittingPresenter(
-    std::unique_ptr<IEnggDiffGSASFittingModel> model,
-    std::unique_ptr<IEnggDiffGSASFittingView> view);
+  EnggDiffGSASFittingPresenter(std::unique_ptr<IEnggDiffGSASFittingModel> model,
+                               std::unique_ptr<IEnggDiffGSASFittingView> view);
 
   ~EnggDiffGSASFittingPresenter() override;
 
   void notify(IEnggDiffGSASFittingPresenter::Notification notif) override;
+
+private:
+  std::unique_ptr<IEnggDiffGSASFittingModel> m_model;
+
+  std::unique_ptr<IEnggDiffGSASFittingView> m_view;
 };
 
 } // namespace CustomInterfaces
