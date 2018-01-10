3fdde288b3add777f9f6163b70a226de7a5ec9d0
Andrei Savici <saviciat@ornl.gov>
2018-01-10 11:24:08 -0500

    Re #21385. Set v2 for QObjects.
    See http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html

--- Framework/PythonInterface/mantid/plots/__init__.py
+++ Framework/PythonInterface/mantid/plots/__init__.py
@@ -22,7 +22,12 @@ Functionality for unpacking mantid objects for plotting with matplotlib.
 # of the main package.
 from __future__ import (absolute_import, division, print_function)
 import sip
+sip.setapi('QDate', 2)
+sip.setapi('QDateTime', 2)
 sip.setapi('QString', 2)
+sip.setapi('QTextStream', 2)
+sip.setapi('QTime', 2)
+sip.setapi('QUrl', 2)
 sip.setapi('QVariant', 2)
 import mantid.plots.functions
 from mantid.dataobjects import EventWorkspace,Workspace2D,MDHistoWorkspace
--- Framework/PythonInterface/mantid/plots/functions.py
+++ Framework/PythonInterface/mantid/plots/functions.py
@@ -16,7 +16,12 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import (absolute_import, division, print_function)
 import sip
+sip.setapi('QDate', 2)
+sip.setapi('QDateTime', 2)
 sip.setapi('QString', 2)
+sip.setapi('QTextStream', 2)
+sip.setapi('QTime', 2)
+sip.setapi('QUrl', 2)
 sip.setapi('QVariant', 2)
 import numpy
 import mantid.kernel
