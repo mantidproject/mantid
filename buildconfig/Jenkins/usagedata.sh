a85dd6a3774ba22eaa5cf68c7a08b44cfcc8233e
Pete Peterson <petersonpf@ornl.gov>
2018-01-11 16:24:00 -0500

    Setting BUILD_DIR when on build server

--- buildconfig/Jenkins/usagedata.sh
+++ buildconfig/Jenkins/usagedata.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # make testing easier - BUILD_DIR should be root of build tree
-if [ -z "$BUILD_DIR" ]; then
+if [ -z "$WORKSPACEDIR" ]; then
     BUILD_DIR=`pwd`
     echo "setting BUILD_DIR to $BUILD_DIR"
+else
+    BUILD_DIR=$WORKSPACE/build
 fi
 
 # remove old copy
@@ -12,21 +14,7 @@ if [ -f UsageData.zip ]; then
     rm UsageData.zip
 fi
 
-# we use cmake3 on rhel because cmake is too old
-if [ $(command -v cmake3) ]; then
-    CMAKE_EXE=cmake3
-    CPACK_EXE=cpack3
-    CTEST_EXE=ctest3
-else
-    CMAKE_EXE=cmake
-    CPACK_EXE=cpack
-    CTEST_EXE=ctest
-fi
-$CMAKE_EXE --version
-${CMAKE_EXE} --build . --target StandardTestData
-
 # create the symbolic link so the zip decompresses into the familiar name
-pwd
 cd ExternalData/Testing/Data || exit -1
 if [ -d UsageData ]; then
     echo "symbolic link already exists"
