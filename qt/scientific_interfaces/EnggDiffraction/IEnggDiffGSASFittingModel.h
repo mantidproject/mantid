bb28cb015b33b29fee8154b6ccdbb66b4e19b09d
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-01-11 15:47:08 +0000

    Re #15552 Add updateplot method and separate plotting data and fit

--- qt/scientific_interfaces/EnggDiffraction/IEnggDiffGSASFittingModel.h
+++ qt/scientific_interfaces/EnggDiffraction/IEnggDiffGSASFittingModel.h
@@ -1,7 +1,10 @@
 #ifndef MANTIDQTCUSTOMINTERFACES_ENGGDIFFRACTION_IENGGDIFFGSASFITTINGMODEL_H_
 #define MANTIDQTCUSTOMINTERFACES_ENGGDIFFRACTION_IENGGDIFFGSASFITTINGMODEL_H_
 
+#include "MantidAPI/ITableWorkspace.h"
 #include "MantidAPI/MatrixWorkspace_fwd.h"
+
+#include <boost/optional.hpp>
 
 #include <string>
 #include <utility>
@@ -25,12 +28,13 @@ public:
    @param GSASIIProjectFile Location to save the .gpx project to
    @return Whether the refinement was successful
    */
-  bool doPawleyRefinement(const int runNumber, const size_t bank,
-                          const std::string &instParamFile,
-                          const std::vector<std::string> &phaseFiles,
-                          const std::string &pathToGSASII,
-                          const std::string &GSASIIProjectFile,
-                          const double dMin, const double negativeWeight);
+  virtual bool doPawleyRefinement(const int runNumber, const size_t bank,
+                                  const std::string &instParamFile,
+                                  const std::vector<std::string> &phaseFiles,
+                                  const std::string &pathToGSASII,
+                                  const std::string &GSASIIProjectFile,
+                                  const double dMin,
+                                  const double negativeWeight) = 0;
   /**
    Perform a Rietveld refinement on a run
    @param runNumber The run number of the run
@@ -41,37 +45,58 @@ public:
    @param GSASIIProjectFile Location to save the .gpx project to
    @return Whether the refinement was successful
    */
-  bool doRietveldRefinement(const int runNumber, const size_t bank,
-                            const std::string &instParamFile,
-                            const std::vector<std::string> &phaseFiles,
-                            const std::string &pathToGSASII,
-                            const std::string &GSASIIProjectFile);
+  virtual bool doRietveldRefinement(const int runNumber, const size_t bank,
+                                    const std::string &instParamFile,
+                                    const std::vector<std::string> &phaseFiles,
+                                    const std::string &pathToGSASII,
+                                    const std::string &GSASIIProjectFile) = 0;
 
   /**
    Get the fit results for a given run
    @param runNumber The run number of the run
    @param bank The bank ID of the run
-   @return MatrixWorkspace containing the fitted peaks
-   @throws If the model does not contain fit results for this run
+   @return MatrixWorkspace containing the fitted peaks, or empty if the model
+   does not contain fit results for this run
    */
-  virtual Mantid::API::MatrixWorkspace_sptr
+  virtual boost::optional<Mantid::API::MatrixWorkspace_sptr>
   getFittedPeaks(const int runNumber, const size_t bank) const = 0;
 
   /**
    Get a focused run (as a MatrixWorkspace) by run label
    @param runNumber The runNumber of the run
    @param bank The bank ID of the run
-   @return The corresponding workspace
-   @throws If the run is not found in the model
+   @return The corresponding workspace (empty if the model does not contain this
+   run)
    */
-  virtual Mantid::API::MatrixWorkspace_sptr
+  virtual boost::optional<Mantid::API::MatrixWorkspace_sptr>
   getFocusedWorkspace(const int runNumber, const size_t bank) const = 0;
+
+  /**
+   Get refined lattice parameters for a run
+   @param runNumber The run number of the run
+   @param bank The bank ID of the run
+   @return TableWorkspace of the corresponding lattice parameters (empty
+   optional if the model does not contain fit results for this run)
+   */
+  virtual boost::optional<Mantid::API::ITableWorkspace_sptr>
+  getLatticeParams(const int runNumber, const size_t bank) const = 0;
 
   /**
    Get run labels (ie run number and bank id) of all runs loaded into model
    @return Vector of all run labels (as pairs)
    */
   virtual std::vector<std::pair<int, size_t>> getRunLabels() const = 0;
+
+  /**
+   Get the weighted profile R-factor discrepancy index for goodness of fit on a
+   run
+   @param runNumber The run number of the run
+   @param bank The bank ID of the run
+   @return The corresponding Rwp value (empty optional if a refinement has not
+   been performed on this run)
+  */
+  virtual boost::optional<double> getRwp(const int runNumber,
+                                         const size_t bank) const = 0;
 
   /**
    Has a fit been performed on a given run?
