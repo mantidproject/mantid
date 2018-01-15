43866f8b1951e0be262e5011ab2dac48b20f42e4
Samuel Jackson <samueljackson@outlook.com>
2018-01-15 17:57:15 +0000

    Apply clang format patch

--- Framework/PythonInterface/mantid/api/src/Exports/ITableWorkspace.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/ITableWorkspace.cpp
@@ -559,8 +559,8 @@ void export_ITableWorkspace() {
            "number then it is interpreted as a row otherwise it "
            "is interpreted as a column name")
 
-      .def("setCell", &setCell, (arg("self"), arg("row_or_column"), arg("column_or_row"),
-                                 arg("value")),
+      .def("setCell", &setCell, (arg("self"), arg("row_or_column"),
+                                 arg("column_or_row"), arg("value")),
            "Sets the value of a given cell. If the first argument is a "
            "number then it is interpreted as a row otherwise it is interpreted "
            "as a column name")
