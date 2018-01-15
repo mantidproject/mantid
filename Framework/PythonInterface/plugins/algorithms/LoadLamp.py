b1ea671b5850a217e7e99d60e92b9411cfeadbfa
Gagik Vardanyan <vardanyan@ill.fr>
2018-01-15 11:12:00 +0100

    Re #0 loading logs made optional

--- Framework/PythonInterface/plugins/algorithms/LoadLamp.py
+++ Framework/PythonInterface/plugins/algorithms/LoadLamp.py
@@ -24,6 +24,7 @@ class LoadLamp(PythonAlgorithm):
     def PyExec(self):
         input_file = self.getProperty("InputFile").value
         output_ws = self.getPropertyValue("OutputWorkspace")
+        logs = ''
 
         with h5py.File(input_file, 'r') as hf:
             data = numpy.array(hf.get('entry1/data1/DATA'), dtype='float')
@@ -31,7 +32,8 @@ class LoadLamp(PythonAlgorithm):
                 raise RuntimeError('Data with more than 2 dimensions are not supported.')
             errors = numpy.array(hf.get('entry1/data1/errors'), dtype='float')
             x = numpy.array(hf.get('entry1/data1/X'), dtype='float')
-            logs = str(hf.get('entry1/data1/PARAMETERS')[0])
+            if "entry1/data1/PARAMETERS" in hf:
+                logs = str(hf.get('entry1/data1/PARAMETERS')[0])
             y = numpy.array([0])
             nspec = 1
             if data.ndim == 2:
@@ -43,21 +45,22 @@ class LoadLamp(PythonAlgorithm):
         CreateWorkspace(DataX=x, DataY=data, DataE=errors, NSpec=nspec, VerticalAxisUnit='Label',
                         VerticalAxisValues=y, OutputWorkspace=output_ws)
 
-        log_names = []
-        log_values = []
-        for log in logs.split('\n'):
-            split = log.strip().split('=')
-            if len(split) == 2:
-                name = split[0]
-                value = split[1]
-                if name and value:
-                    log_names.append(name)
-                    log_values.append(value)
-        if log_names:
-            try:
-                AddSampleLogMultiple(Workspace=output_ws, LogNames=log_names, LogValues=log_values)
-            except RuntimeError as e:
-                self.log().warning('Unable to set the sample logs, reason: '+str(e))
+        if logs:
+            log_names = []
+            log_values = []
+            for log in logs.split('\n'):
+                split = log.strip().split('=')
+                if len(split) == 2:
+                    name = split[0]
+                    value = split[1]
+                    if name and value:
+                        log_names.append(name)
+                        log_values.append(value)
+            if log_names:
+                try:
+                    AddSampleLogMultiple(Workspace=output_ws, LogNames=log_names, LogValues=log_values)
+                except RuntimeError as e:
+                    self.log().warning('Unable to set the sample logs, reason: '+str(e))
 
         self.setProperty('OutputWorkspace', output_ws)
 
