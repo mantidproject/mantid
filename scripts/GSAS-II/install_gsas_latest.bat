b04bd3e76e6276a968b04a86b5028d7322a24725
Joe Ramsay <joseph.ramsay@stfc.ac.uk>
2018-01-16 10:08:18 +0000

    Re #21482 Add non-interactive flag and revision number variable
    The scripts now don't pause if run with the /q flag

--- scripts/GSAS-II/install_gsas_latest.bat
+++ scripts/GSAS-II/install_gsas_latest.bat
@@ -1,5 +1,6 @@
 @echo off
-@echo Running install_gsas_proxy.bat
+@echo Running install_gsas_latest.bat
+@echo Run with /q flag to quit without prompt after installation complete
 
 @set DEV_PYTHON_EXE=%~dp0..\..\external\src\ThirdParty\lib\python2.7\python.exe
 @set RELEASE_PYTHON_EXE=%~dp0..\..\bin\python.exe
@@ -8,10 +9,10 @@ if exist %DEV_PYTHON_EXE% (
    @echo Using %DEV_PYTHON_EXE%
    %DEV_PYTHON_EXE% install_gsas_proxy.py
 ) else (if exist %RELEASE_PYTHON_EXE% (
-  @echo Using %RELEASE_PYTHON_EXE%
+   @echo Using %RELEASE_PYTHON_EXE%
    %RELEASE_PYTHON_EXE% install_gsas_proxy.py
 ) else (
    echo Could not find Mantid Python executable
 )
 )
-pause
+if not "%1"=="/q" pause
--- scripts/GSAS-II/install_gsas_vetted.bat
+++ scripts/GSAS-II/install_gsas_vetted.bat
@@ -1,17 +1,19 @@
 @echo off
-@echo Running install_gsas_proxy.bat
+@echo Running install_gsas_vetted.bat
+@echo Run with /q flag to quit without prompt after installation complete
 
 @set DEV_PYTHON_EXE=%~dp0..\..\external\src\ThirdParty\lib\python2.7\python.exe
 @set RELEASE_PYTHON_EXE=%~dp0..\..\bin\python.exe
+@set VETTED_REVISION_NUMBER=3216
 
 if exist %DEV_PYTHON_EXE% (
    @echo Using %DEV_PYTHON_EXE%
-   %DEV_PYTHON_EXE% install_gsas_proxy.py -v 3216
+   %DEV_PYTHON_EXE% install_gsas_proxy.py -v %VETTED_REVISION_NUMBER%
 ) else (if exist %RELEASE_PYTHON_EXE% (
-  @echo Using %RELEASE_PYTHON_EXE%
-   %RELEASE_PYTHON_EXE% install_gsas_proxy.py -v 3216
+   @echo Using %RELEASE_PYTHON_EXE%
+   %RELEASE_PYTHON_EXE% install_gsas_proxy.py -v %VETTED_REVISION_NUMBER%
 ) else (
    echo Could not find Mantid Python executable
 )
 )
-pause
+if not "%1"=="/q" pause
