6f0ab2150fef97a9afa46f36fee567989734ef6e
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-01-16 16:53:39 +0000

    Re #21565 Add necessary functions to RunMapTest

--- qt/scientific_interfaces/test/RunMapTest.h
+++ qt/scientific_interfaces/test/RunMapTest.h
@@ -7,10 +7,10 @@
 
 using namespace MantidQt::CustomInterfaces;
 
-class RunMapTest: public CxxTest::TestSuite{
+class RunMapTest : public CxxTest::TestSuite {
 
-public :
-  void test_addedItemsExistInMap(){
+public:
+  void test_addedItemsExistInMap() {
     RunMap<3, std::string> runMap;
     TS_ASSERT_THROWS_NOTHING(runMap.add(123, 1, "Polly"));
     TS_ASSERT_THROWS_NOTHING(runMap.add(456, 2, "Morphism"));
@@ -20,17 +20,40 @@ public :
     TS_ASSERT(runMap.contains(456, 2));
     TS_ASSERT(!runMap.contains(789, 4));
   }
-  
-  void test_addedItemsAreCorrect(){
+
+  void test_addedItemsAreCorrect() {
     RunMap<3, std::string> runMap;
-    TS_ASSERT_THROWS_NOTHING(runMap.add(123, 1, "Polly"));
-    TS_ASSERT_THROWS_NOTHING(runMap.add(456, 2, "Morphism"));
+    runMap.add(123, 1, "Polly");
+    runMap.add(456, 2, "Morphism");
 
     TS_ASSERT_EQUALS(runMap.get(123, 1), "Polly");
     TS_ASSERT_EQUALS(runMap.get(456, 2), "Morphism");
-    
   }
-  
+
+  void test_remove() {
+    RunMap<3, std::string> runMap;
+
+    runMap.add(123, 1, "Polly");
+    TS_ASSERT(runMap.contains(123, 1));
+
+    TS_ASSERT_THROWS_NOTHING(runMap.remove(123, 1));
+    TS_ASSERT(!runMap.contains(123, 1));
+  }
+
+  void test_getRunNumbersAndBankIDs() {
+    runMap.add(111, 1, "Polly");
+    runMap.add(222, 2, "Morphism");
+    runMap.add(333, 3, "Al");
+    runMap.add(444, 1, "Gorithm");
+
+    const auto runBankPairs = runMap.getRunNumbersAndBankIDs();
+
+    TS_ASSERT_EQUALS(runBankPairs.size(), 4);
+    for (int i = 0; i < 4; ++i) {
+      TS_ASSERT_EQUALS(runBankPairs[i],
+                       std::make_pair((i + 1) * 111, (i + 1) % 3));
+    }
+  }
 };
 
 #endif // MANTIDQTCUSTOMINTERFACES_ENGGDIFFRACTION_RUNMAPTEST_H_
