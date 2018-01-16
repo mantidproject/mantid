c510acd35b04f5c1ac643713b6d2f62adc2aa22b
Edward Brown <edward.brown@stfc.ac.uk>
2018-01-16 14:53:02 +0000

    Re #21216: Added fix to missing settingsChanged notifications in sans.

--- qt/widgets/common/src/DataProcessorUI/QDataProcessorWidget.cpp
+++ qt/widgets/common/src/DataProcessorUI/QDataProcessorWidget.cpp
@@ -168,6 +168,10 @@ void QDataProcessorWidget::processClicked() {
   m_presenter->notify(DataProcessorPresenter::ProcessFlag);
 }
 
+void QDataProcessorWidget::settingsChanged() {
+  m_presenter->settingsChanged();
+}
+
 /**
 This slot loads a table workspace model and changes to a LoadedMainView
 presenter
