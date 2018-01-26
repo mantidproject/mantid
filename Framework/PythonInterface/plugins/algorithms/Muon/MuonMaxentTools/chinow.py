f70c84c2edabba259c54400a2bf2d3e62e37a21b
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-01-26 11:37:58 +0000

    refs #21630 flake8 fixes for MuonMaxent up to maxent.py

--- Framework/PythonInterface/plugins/algorithms/Muon/MuonMaxentTools/chinow.py
+++ Framework/PythonInterface/plugins/algorithms/Muon/MuonMaxentTools/chinow.py
@@ -1,5 +1,7 @@
+
+from __future__ import (absolute_import, division, print_function)
 import numpy as np
-from chosol import CHOSOL
+from .chosol import CHOSOL
 
 # translation of chinow.for
 """
@@ -7,11 +9,13 @@ from chosol import CHOSOL
       END
 """
 # JSL: ASSUME all of arrays SPACE_c1 etc are used, i.e. m=3 and size=3
-def CHINOW(ax,SPACE_c1,SPACE_c2,SPACE_s1,SPACE_s2,mylog):
-	bx=1.-ax
-	a=bx*SPACE_c2-ax*SPACE_s2
-	b=-(bx*SPACE_c1-ax*SPACE_s1)
-	SPACE_beta=CHOSOL(a,b,mylog)
-	z=np.dot(SPACE_c2,SPACE_beta)
-	w=np.sum(SPACE_beta*(SPACE_c1+0.5*z))
-	return 1.+w, SPACE_beta
+
+
+def CHINOW(ax, SPACE_c1, SPACE_c2, SPACE_s1, SPACE_s2, mylog):
+    bx = 1. - ax
+    a = bx * SPACE_c2 - ax * SPACE_s2
+    b = -(bx * SPACE_c1 - ax * SPACE_s1)
+    SPACE_beta = CHOSOL(a, b, mylog)
+    z = np.dot(SPACE_c2, SPACE_beta)
+    w = np.sum(SPACE_beta * (SPACE_c1 + 0.5 * z))
+    return 1. + w, SPACE_beta
