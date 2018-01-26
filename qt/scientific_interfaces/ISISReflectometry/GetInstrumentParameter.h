0eb84787ac710f52161db99d4cc068155ced5935
Edward Brown <edward.brown@stfc.ac.uk>
2018-01-26 13:50:06 +0000

    Re #21543: Fixed error caused by handling of only two possible types.

--- qt/scientific_interfaces/ISISReflectometry/GetInstrumentParameter.h
+++ qt/scientific_interfaces/ISISReflectometry/GetInstrumentParameter.h
@@ -52,17 +52,37 @@ private:
   std::string m_originalMessage;
 };
 
-template <typename T1, typename T2, typename... Ts>
-class InstrumentParameter<boost::variant<T1, T2, Ts...>> {
+template <typename T1, typename T2>
+class InstrumentParameter<boost::variant<T1, T2>> {
 public:
-  static boost::variant<std::vector<T1>, std::vector<T2>, std::vector<Ts>...>
+  static boost::variant<std::vector<T1>, std::vector<T2>>
+  get(Mantid::Geometry::Instrument_const_sptr instrument, std::string const& parameterName) {
+    try {
+      return InstrumentParameter<T1>::get(instrument, parameterName);
+    } catch(InstrumentParameterTypeMissmatch const&t1ex) {
+      try {
+        return InstrumentParameter<T2>::get(instrument, parameterName);
+      } catch (InstrumentParameterTypeMissmatch const &t2ex) {
+        throw InstrumentParameterTypeMissmatch(
+            parameterName, t1ex.expectedType() + " or a " + t2ex.expectedType(),
+            t2ex);
+      }
+    }
+  }
+
+};
+
+template <typename T1, typename T2, typename T3, typename... Ts>
+class InstrumentParameter<boost::variant<T1, T2, T3, Ts...>> {
+public:
+  static boost::variant<std::vector<T1>, std::vector<T2>, std::vector<T3>, std::vector<Ts>...>
   get(Mantid::Geometry::Instrument_const_sptr instrument,
       std::string const &parameterName) {
     try {
       return InstrumentParameter<T1>::get(instrument, parameterName);
     } catch (InstrumentParameterTypeMissmatch const &t1ex) {
       try {
-        return InstrumentParameter<T2, Ts...>::get(instrument, parameterName);
+        return InstrumentParameter<boost::variant<T2, T3, Ts...>>::get(instrument, parameterName);
       } catch (InstrumentParameterTypeMissmatch const &t2ex) {
         throw InstrumentParameterTypeMissmatch(
             parameterName, t1ex.expectedType() + " or a " + t2ex.expectedType(),
