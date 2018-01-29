d354289f698696df9832ccf78a9f9212a4d0cea6
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-01-29 14:08:27 +0000

    Re #21640 Add test for add and get focused run

--- qt/scientific_interfaces/test/EnggDiffMultiRunFittingWidgetModelTest.h
+++ qt/scientific_interfaces/test/EnggDiffMultiRunFittingWidgetModelTest.h
@@ -26,6 +26,22 @@ public:
     TS_ASSERT_THROWS_NOTHING(retrievedWS = model.getFittedPeaks(456, 2));
     TS_ASSERT(!retrievedWS);
   }
+
+  void test_addAndGetFocusedRun() {
+    EnggDiffMultiRunFittingWidgetModel model;
+
+    Mantid::API::MatrixWorkspace_sptr ws =
+        WorkspaceCreationHelper::create2DWorkspaceBinned(4, 4, 0.5);
+
+    TS_ASSERT_THROWS_NOTHING(model.addFocusedRun(123, 1, ws));
+    boost::optional<Mantid::API::MatrixWorkspace_sptr> retrievedWS(boost::none);
+    TS_ASSERT_THROWS_NOTHING(retrievedWS = model.getFocusedRun(123, 1));
+    TS_ASSERT(retrievedWS);
+    TS_ASSERT_EQUALS(ws, *retrievedWS);
+
+    TS_ASSERT_THROWS_NOTHING(retrievedWS = model.getFocusedRun(456, 2));
+    TS_ASSERT(!retrievedWS);
+  }
 };
 
 #endif // MANTIDQT_CUSTOMINTERFACES_ENGGDIFFMULTIRUNFITTINGWIDGETMODELTEST_H_
