fbfe7455c2b252b48393cabe7924b992f2bdd138
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2018-01-31 11:35:00 +0000

    Use Mantid::Kernel::make_unique (C++ 11 compatibility)
    Refs #21648

--- Framework/WorkflowAlgorithms/test/ExtractQENSMembersTest.h
+++ Framework/WorkflowAlgorithms/test/ExtractQENSMembersTest.h
@@ -15,6 +15,8 @@
 
 #include "MantidDataHandling/Load.h"
 
+#include "MantidKernel/make_unique.h"
+
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
 
 #include "MantidWorkflowAlgorithms/ExtractQENSMembers.h"
@@ -180,7 +182,7 @@ private:
             static_cast<int>(members.size()), static_cast<int>(dataX.size()),
             dataX.data());
 
-    auto axis = std::make_unique<TextAxis>(members.size());
+    auto axis = Mantid::Kernel::make_unique<TextAxis>(members.size());
     for (auto i = 0u; i < members.size(); ++i) {
       auto memberWS = createGenericWorkspace(
           dataX, randomDataVector<double>(dataX.size() - 1, 0.0, 10.0));
