a080295e6d0fd0c8cbb4a438fa97b8808fa0b5aa
Steven Hahn <hahnse@ornl.gov>
2018-02-01 11:04:07 -0500

    clang-format.

--- Framework/Beamline/src/DetectorInfo.cpp
+++ Framework/Beamline/src/DetectorInfo.cpp
@@ -10,15 +10,13 @@ namespace Beamline {
 DetectorInfo::DetectorInfo(
     std::vector<Eigen::Vector3d> positions,
     std::vector<Eigen::Quaterniond,
-                Eigen::aligned_allocator<Eigen::Quaterniond>>
-        rotations)
+                Eigen::aligned_allocator<Eigen::Quaterniond>> rotations)
     : m_isMonitor(Kernel::make_cow<std::vector<bool>>(positions.size())),
       m_isMasked(Kernel::make_cow<std::vector<bool>>(positions.size())),
       m_positions(
           Kernel::make_cow<std::vector<Eigen::Vector3d>>(std::move(positions))),
-      m_rotations(Kernel::make_cow<
-                  std::vector<Eigen::Quaterniond,
-                              Eigen::aligned_allocator<Eigen::Quaterniond>>>(
+      m_rotations(Kernel::make_cow<std::vector<
+          Eigen::Quaterniond, Eigen::aligned_allocator<Eigen::Quaterniond>>>(
           std::move(rotations))) {
   if (m_positions->size() != m_rotations->size())
     throw std::runtime_error("DetectorInfo: Position and rotations vectors "
@@ -28,8 +26,7 @@ DetectorInfo::DetectorInfo(
 DetectorInfo::DetectorInfo(
     std::vector<Eigen::Vector3d> positions,
     std::vector<Eigen::Quaterniond,
-                Eigen::aligned_allocator<Eigen::Quaterniond>>
-        rotations,
+                Eigen::aligned_allocator<Eigen::Quaterniond>> rotations,
     const std::vector<size_t> &monitorIndices)
     : DetectorInfo(std::move(positions), std::move(rotations)) {
   for (const auto i : monitorIndices)
