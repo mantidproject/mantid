4d971bcaa4aa47079f9d5623e3eed1079fa1e523
Vickie Lynch <lynchve@ornl.gov>
2018-02-02 08:37:17 -0500

    Refs #21597 count number of peaks in file

--- Framework/Crystal/src/SaveLauenorm.cpp
+++ Framework/Crystal/src/SaveLauenorm.cpp
@@ -73,6 +73,7 @@ void SaveLauenorm::init() {
 /** Execute the algorithm.
  */
 void SaveLauenorm::exec() {
+  int count = 0;
 
   std::string filename = getProperty("Filename");
   Poco::Path path(filename);
@@ -125,25 +126,73 @@ void SaveLauenorm::exec() {
   auto inst = ws->getInstrument();
   OrientedLattice lattice;
   if (newFormat) {
-  type = "RunNumber";
-  if (!ws->sample().hasOrientedLattice()) {
-
-    const std::string fft("FindUBUsingIndexedPeaks");
-    API::IAlgorithm_sptr findUB = this->createChildAlgorithm(fft);
-    findUB->initialize();
-    findUB->setProperty<PeaksWorkspace_sptr>("PeaksWorkspace", ws);
-    findUB->executeAsChildAlg();
-
+    type = "RunNumber";
     if (!ws->sample().hasOrientedLattice()) {
-      g_log.notice(std::string("Could not find UB for ") +
-                   std::string(ws->getName()));
-      throw std::invalid_argument(std::string("Could not find UB for ") +
-                                  std::string(ws->getName()));
-    }
-  } 
-  lattice = ws->sample().getOrientedLattice();
-}
 
+      const std::string fft("FindUBUsingIndexedPeaks");
+      API::IAlgorithm_sptr findUB = this->createChildAlgorithm(fft);
+      findUB->initialize();
+      findUB->setProperty<PeaksWorkspace_sptr>("PeaksWorkspace", ws);
+      findUB->executeAsChildAlg();
+
+      if (!ws->sample().hasOrientedLattice()) {
+        g_log.notice(std::string("Could not find UB for ") +
+                     std::string(ws->getName()));
+        throw std::invalid_argument(std::string("Could not find UB for ") +
+                                    std::string(ws->getName()));
+      }
+    }
+    lattice = ws->sample().getOrientedLattice();
+  }
+  // Count peaks
+  for (int wi = 0; wi < ws->getNumberPeaks(); wi++) {
+
+    Peak &p = peaks[wi];
+    double intensity = p.getIntensity();
+    double sigI = p.getSigmaIntensity();
+    if (intensity == 0.0 || !(std::isfinite(sigI)))
+      continue;
+    if (minIsigI != EMPTY_DBL() && intensity < std::abs(minIsigI * sigI))
+      continue;
+    int sequence = p.getRunNumber();
+    std::string bankName = p.getBankName();
+    int nCols, nRows;
+    sizeBanks(bankName, nCols, nRows);
+    if (widthBorder != EMPTY_INT() &&
+        (p.getCol() < widthBorder || p.getRow() < widthBorder ||
+         p.getCol() > (nCols - widthBorder) ||
+         p.getRow() > (nRows - widthBorder)))
+      continue;
+    // Take out the "bank" part of the bank name and convert to an int
+    bankName.erase(remove_if(bankName.begin(), bankName.end(),
+                             not1(std::ptr_fun(::isdigit))),
+                   bankName.end());
+    if (type.compare(0, 2, "Ba") == 0) {
+      Strings::convert(bankName, sequence);
+    }
+    // Do not use peaks from these banks
+    std::vector<std::string> notBanks = getProperty("EliminateBankNumbers");
+    if (std::find(notBanks.begin(), notBanks.end(), bankName) != notBanks.end())
+      continue;
+    if (scaleDet) {
+      if (inst->hasParameter("detScale" + bankName)) {
+        double correc = static_cast<double>(
+            inst->getNumberParameter("detScale" + bankName)[0]);
+        intensity *= correc;
+        sigI *= correc;
+      }
+    }
+    if (minIntensity != EMPTY_DBL() && intensity < minIntensity)
+      continue;
+    double lambda = p.getWavelength();
+    double dsp = p.getDSpacing();
+    if (dsp < dMin || lambda < wlMin ||
+        (minIsigI != EMPTY_DBL() && lambda > wlMax))
+      continue;
+    if (p.getH() == 0 && p.getK() == 0 && p.getL() == 0)
+      continue;
+    count++;
+  }
   // Go through each peak at this run / bank
   for (int wi = 0; wi < ws->getNumberPeaks(); wi++) {
 
@@ -207,21 +256,28 @@ void SaveLauenorm::exec() {
       path.append(basename + ss.str());
       Poco::File fileobj(path);
       out.open(path.toString().c_str(), std::ios::out);
-  std::cout << basename<<" open\n";
-  if (newFormat) {
-  out << "TITL\n";
-  out << basename << "\n";
-  out << "CRYS " << basename.substr(0,6) << "\n";
-  out << "CELL "
-      << std::setw(11) << std::setprecision(4) << lattice.a() << std::setw(12)
-      << std::setprecision(4) << lattice.b() << std::setw(12) << std::setprecision(4)
-      << lattice.c() << std::setw(12) << std::setprecision(4) << lattice.alpha()
-      << std::setw(12) << std::setprecision(4) << lattice.beta() << std::setw(12)
-      << std::setprecision(4) << lattice.gamma() 
-      << "\n";
-  out << "SYST    1   1   0   0" << "\n";
-  out << "RAST      0.050" << "\n";
-  out << "IBOX   1  1   1   1   1" << "\n";
+      if (newFormat) {
+        out << "TITL\n";
+        out << basename << "\n";
+        out << "CRYS " << basename.substr(0, 6) << "\n";
+        out << "FIDX     1.00000     1.00000     1.00000     1.00000     "
+               "1.00000     1.00000\n";
+        out << "FIDY     1.00000     1.00000     1.00000     1.00000     "
+               "1.00000     1.00000\n";
+        out << "OMEG     1.00000     1.00000     1.00000     1.00000     "
+               "1.00000     1.00000\n";
+        out << "CELL " << std::setw(11) << std::setprecision(4) << lattice.a()
+            << std::setw(12) << std::setprecision(4) << lattice.b()
+            << std::setw(12) << std::setprecision(4) << lattice.c()
+            << std::setw(12) << std::setprecision(4) << lattice.alpha()
+            << std::setw(12) << std::setprecision(4) << lattice.beta()
+            << std::setw(12) << std::setprecision(4) << lattice.gamma() << "\n";
+        out << "SYST    1   1   0   0"
+            << "\n";
+        out << "RAST      0.050"
+            << "\n";
+        out << "IBOX   1  1   1   1   1"
+            << "\n";
         Goniometer gon(p.getGoniometerMatrix());
         std::vector<double> angles = gon.getEulerAngles("yzy");
 
@@ -229,60 +285,87 @@ void SaveLauenorm::exec() {
         double chi = angles[1];
         double omega = angles[0];
 
-  out << "PHIS " 
-      << std::setw(11) << std::setprecision(4) << phi << std::setw(12)
-      << std::setprecision(4) << chi << std::setw(12) << std::setprecision(4)
-      << omega << "\n";
-  out << "LAMS      ";
-  if (wlMax != EMPTY_DBL() ) {
-  out << (wlMin+wlMax)/2 << "  " << wlMin << "  " << wlMax << "\n";
-  }
-  else {
-  out << "0.0 0.0 0.0\n";
-  }
-  out << "DMIN      ";
-  if (dMin != EMPTY_DBL() ) {
-  out << dMin << "\n";
-  }
-  else {
-  out << "0.0\n";
-  }
-  out << "RADI     59.000" << "\n";
-  out << "SPIN      0.000" << "\n";
-  out << "XC_S     0.23000    0.23000    0.24000    0.26000    0.26000    0.22000" << "\n";
-  out << "YC_S     0.09000    0.11000    0.09000    0.11000    0.11000    0.09000" << "\n";
-  out << "WC_S     3.79368    3.80879    3.78424    3.80775    3.80466    3.82321" << "\n";
-  out << "DELT       0.4000" << "\n";
-  out << "TWIS    -2.33700   -4.30406   -5.62749   -0.10663   -0.99198    3.61740" << "\n";
-  out << "TILT    -4.79623    0.37310   -3.58647    0.53322   -1.06657   -3.66751" << "\n";
-  out << "BULG    45.40230   56.51379   34.71940   31.53628   36.29953   42.53154" << "\n";
-  out << "CTOF     61.067" << "\n";
-  out << "YSCA     0.99748    0.99738    0.99696    0.99726    0.99758    0.99716" << "\n";
-  out << "CRAT     0.99494    0.99720    1.00672    1.01395    1.01970    1.02644" << "\n";
-  out << "MINI          " ;
-  if (minIntensity != EMPTY_DBL() ) {
-  out << minIntensity << "\n";
-  }
-  else {
-  out << "0.0\n";
-  }
-  out << "MULT" << "\n";
-  out << "   1670    175     40     12      8      5      0      0      0      0" << "\n";
-  out << "      0      0      0      0      0      0      0      0      0      0" << "\n";
-  out << "      0" << "\n";
-  out << "LAMH" << "\n";
-  out << "     401     383     389     391     282     158      89      53" << "\n";
-  out << "      38      21      19      11      11       6       6       0" << "\n";
-  out << "VERS    2" << "\n";
-  out << "PACK        0" << "\n";
-  out << "NSPT     1910     240       0       0    1605" << "\n";
-  out << "NODH" << "\n";
-  out << "       6      23      63     124     243     356" << "\n";
-  out << "     571     775    1076    1312    1499    1605" << "\n";
-  out << "INTF        0" << "\n";
-  out << "REFLECTION DATA   " << ws->getNumberPeaks() << " REFLECTIONS" << "\n";
-  }
-}
+        out << "PHIS " << std::setw(11) << std::setprecision(4) << phi
+            << std::setw(12) << std::setprecision(4) << chi << std::setw(12)
+            << std::setprecision(4) << omega << "\n";
+        out << "LAMS      ";
+        if (wlMax != EMPTY_DBL()) {
+          out << (wlMin + wlMax) / 2 << "  " << wlMin << "  " << wlMax << "\n";
+        } else {
+          out << "0.0 0.0 0.0\n";
+        }
+        out << "DMIN      ";
+        if (dMin != EMPTY_DBL()) {
+          out << dMin << "\n";
+        } else {
+          out << "0.0\n";
+        }
+        out << "RADI     59.000"
+            << "\n";
+        out << "SPIN      0.000"
+            << "\n";
+        out << "XC_S     0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000\n";
+        out << "YC_S     0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000\n";
+        out << "WC_S     0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000\n";
+        out << "DELT       0.4000"
+            << "\n";
+        out << "TWIS    0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000 \n";
+        out << "TILT    0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000 \n";
+        out << "BULG    0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000 \n";
+        out << "CTOF     61.067"
+            << "\n";
+        out << "YSCA     0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000\n";
+        out << "CRAT     0.00000     0.00000     0.00000     0.00000     "
+               "0.00000     0.00000\n";
+        out << "MINI          ";
+        if (minIntensity != EMPTY_DBL()) {
+          out << minIntensity << "\n";
+        } else {
+          out << "0.0\n";
+        }
+        out << "MULT"
+            << "\n";
+        out << "   1670    175     40     12      8      5      0      0      "
+               "0      0"
+            << "\n";
+        out << "      0      0      0      0      0      0      0      0      "
+               "0      0"
+            << "\n";
+        out << "      0"
+            << "\n";
+        out << "LAMH"
+            << "\n";
+        out << "     401     383     389     391     282     158      89      "
+               "53"
+            << "\n";
+        out << "      38      21      19      11      11       6       6       "
+               "0"
+            << "\n";
+        out << "VERS    2"
+            << "\n";
+        out << "PACK        0"
+            << "\n";
+        out << "NSPT   " << count << "       0       0       0       0"
+            << "\n";
+        out << "NODH"
+            << "\n";
+        out << "       6      23      63     124     243     356"
+            << "\n";
+        out << "     571     775    1076    1312    1499    1605"
+            << "\n";
+        out << "INTF        0"
+            << "\n";
+        out << "REFLECTION DATA   " << count << " REFLECTIONS"
+            << "\n";
+      }
+    }
     // h k l lambda theta intensity and  sig(intensity)  in format
     // (3I5,2F10.5,2I10)
     // HKL is flipped by -1 due to different q convention in ISAW vs mantid.
@@ -297,7 +380,7 @@ void SaveLauenorm::exec() {
       out << std::setw(10) << std::fixed << std::setprecision(5)
           << (p.getCol() - 127.5) * 150.0 / 256.0;
       out << std::setw(10) << std::fixed << std::setprecision(5)
-          << (p.getRow() - 127.5) * 150.0 / 256.0;
+          << (p.getRow() - 127.5) * 150.0 / 256.0 << "\n";
     }
     out << std::setw(10) << std::fixed << std::setprecision(5) << lambda;
     if (newFormat) {
@@ -308,7 +391,7 @@ void SaveLauenorm::exec() {
     if (newFormat) {
       // Dmin threshold squared for next harmonic
       out << std::setw(10) << std::fixed << std::setprecision(5)
-          << dsp * dsp * 0.25;
+          << dsp * dsp * 0.25 << "\n";
     } else {
       // Assume that want theta not two-theta
       out << std::setw(10) << std::fixed << std::setprecision(5)
@@ -324,39 +407,39 @@ void SaveLauenorm::exec() {
       out << std::setw(10) << Utils::round(ckIntensity);
       if (newFormat) {
         // mult nodal ovlp close h2 k2 l2 nidx lambda2 ipoint
-        out << " 0 0 0 0 0 ";
+        out << " -9999 -9999 -9999 -9999 -9999 \n";
       }
 
       out << std::setw(10) << Utils::round(scaleFactor * sigI);
       if (newFormat) {
         // mult nodal ovlp close h2 k2 l2 nidx lambda2 ipoint
-        out << " 0 0 0 0 0 ";
+        out << " -9999 -9999 -9999 -9999 -9999 \n";
         out << std::setw(10) << Utils::round(ckIntensity);
-        out << " 0 0 0 0 0 ";
+        out << " -9999 -9999 -9999 -9999 -9999 \n";
         out << std::setw(10) << Utils::round(scaleFactor * sigI);
-        out << " 0 0 0 0 0 * ";
+        out << " -9999 -9999 -9999 -9999 -9999 * ";
       }
     } else {
       // This is data from LoadLauenorm which is already corrected
       out << std::setw(10) << Utils::round(intensity);
       if (newFormat) {
         // 5 more films (dummy)
-        out << " 0 0 0 0 0 ";
+        out << " -9999 -9999 -9999 -9999 -9999 \n";
       }
       out << std::setw(10) << Utils::round(sigI);
       if (newFormat) {
         // 5 more films (dummy)
-        out << " 0 0 0 0 0 ";
+        out << " -9999 -9999 -9999 -9999 -9999 \n";
         out << std::setw(10) << Utils::round(intensity);
-        out << " 0 0 0 0 0 ";
+        out << " -9999 -9999 -9999 -9999 -9999 \n";
         out << std::setw(10) << Utils::round(sigI);
-        out << " 0 0 0 0 0 * ";
+        out << " -9999 -9999 -9999 -9999 -9999 * ";
       }
     }
 
     out << '\n';
   }
-
+  out << "END-OF-FILE\n";
   out.flush();
   out.close();
 }
