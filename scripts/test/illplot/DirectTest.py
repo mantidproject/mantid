65097a1c648560ffc7479b5c97b9c0118ba0e8c5
Antti Soininen <soininen@ill.fr>
2018-02-05 02:56:51 -0500

    Add prototype illplot Python module in scripts/ Re #21675

---
+++ scripts/test/illplot/DirectTest.py
@@ -0,0 +1,9 @@
+from __future__ import (absolute_import, division, print_function)
+import unittest
+
+class DirectTest(unittest.TestCase):
+    def test_nothing(self):
+        self.assert(False)
+
+if __name__ == '__main__':
+    unittest.main()
