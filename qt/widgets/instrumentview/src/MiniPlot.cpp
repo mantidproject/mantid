2e792fee53d94060623afbf42d6b2d4cc1c7efc8
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-02-06 13:36:46 +0000

    Cut off zero and negative values from y axis. Re #20240

--- qt/widgets/instrumentview/src/MiniPlot.cpp
+++ qt/widgets/instrumentview/src/MiniPlot.cpp
@@ -11,6 +11,8 @@ namespace {
 
 /// Active curve format
 const char *ACTIVE_CURVE_FORMAT = "k-";
+/// Smallest positive cut-off value for plotting on a log axis.
+const double SMALLEST_POSITIVE_VALUE = 0.5;
 
 /**
  * Return a color for a stored line at the given index
@@ -88,6 +90,7 @@ void MiniPlot::update() {
  */
 void MiniPlot::setActiveCurve(std::vector<double> x, std::vector<double> y,
                               QString xunit, QString curveLabel) {
+  std::transform(y.begin(), y.end(), y.begin(), [](double v){return v > 0? v : SMALLEST_POSITIVE_VALUE;});
   removeActiveCurve();
   plotLine(std::move(x), std::move(y), ACTIVE_CURVE_FORMAT);
   setLabel(Axes::Label::X, xunit.toAscii().data());
