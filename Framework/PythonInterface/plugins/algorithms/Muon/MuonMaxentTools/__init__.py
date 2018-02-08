2b82c895a81d28eea51d7ebc66cc49f3f12ffacb
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-02-08 13:00:14 +0000

    refs #21630 fixed some more flake8 warnings

--- Framework/PythonInterface/plugins/algorithms/Muon/MuonMaxentTools/__init__.py
+++ Framework/PythonInterface/plugins/algorithms/Muon/MuonMaxentTools/__init__.py
@@ -1,2 +1,3 @@
 # dummy!
 #me too
+
--- Framework/PythonInterface/plugins/algorithms/Muon/MuonMaxentTools/dist.py
+++ Framework/PythonInterface/plugins/algorithms/Muon/MuonMaxentTools/dist.py
@@ -3,6 +3,5 @@ import numpy as np
 
 # inner loop Z=sum_L(S2(K,L)*beta_L)
 # outer loop W=sum(beta(k)*Z(k))
-def DIST(SPACE_beta,SPACE_s2):
-	return -np.dot(SPACE_beta,np.dot(SPACE_s2,SPACE_beta))
-	
+def DIST(SPACE_beta, SPACE_s2):
+    return -np.dot(SPACE_beta, np.dot(SPACE_s2, SPACE_beta))
