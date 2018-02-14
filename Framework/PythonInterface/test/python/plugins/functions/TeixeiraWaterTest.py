68ee5737eec3555766dce28870024e0e0870c60c
Jose Borreguero <borreguero@gmail.com>
2018-02-14 18:20:51 -0500

    Refs #21619 fix test, again :( :(

--- Framework/PythonInterface/test/python/plugins/functions/TeixeiraWaterTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/TeixeiraWaterTest.py
@@ -15,8 +15,8 @@ class TeixeiraWaterTest(unittest.TestCase):
 
     def test_function_output(self):
         input = [0.01, 0.1, 1.0, 10.0]
-        expected = [2.24949386e-04, 2.20048900e-02,
-                    6.92307692e-01, 9.95575221e-01]
+        expected = [2.46820217e-05, 2.45907320e-03,
+                    1.79512344e-01, 6.41115514e-01]
         tolerance = 1.0e-05
         status, output = check_output("TeixeiraWater", input, expected,
                                       tolerance, Tau=1.0, L=1.5)
