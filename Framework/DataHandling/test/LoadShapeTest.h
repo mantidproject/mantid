29b6fc3e2faf2135f21e2079755c2da94529f424
Karl Palmen <karl.palmen@stfc.ac.uk>
2018-02-26 17:16:43 +0000

    Add skeleton unit test re #12828
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

---
+++ Framework/DataHandling/test/LoadShapeTest.h
@@ -0,0 +1,43 @@
+#ifndef LOAD_SHAPETEST_H_
+#define LOAD_SHAPETEST_H_
+
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidDataHandling/LoadShape.h"
+#include "MantidDataHandling/LoadInstrument.h"
+#include "MantidKernel/OptionalBool.h"
+#include "MantidTestHelpers/ComponentCreationHelper.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+#include <cxxtest/TestSuite.h>
+
+using namespace Mantid;
+using namespace Mantid::API;
+using namespace Mantid::DataHandling;
+
+
+class LoadShapeTest : public CxxTest::TestSuite {
+public:
+  static LoadShapeTest *createSuite() {
+    return new LoadShapeTest();
+  }
+  static void destroySuite(LoadShapeTest *suite) { delete suite; }
+
+
+  void testName() { TS_ASSERT_EQUALS(loadShape.name(), "LoadShape"); }
+
+  void testVersion() { TS_ASSERT_EQUALS(loadShape.version(), 1); }
+
+  void testInit() {
+
+    TS_ASSERT_THROWS_NOTHING(loadShape.initialize());
+    TS_ASSERT(loadShape.isInitialized());
+
+    TSM_ASSERT_EQUALS("should be 5 properties here", 5,
+                      (size_t)(loadShape.getProperties().size()));
+  }
+
+
+private:
+  LoadShape loadShape; 
+};
+#endif /* LOAD_SHAPETEST_H_ */
