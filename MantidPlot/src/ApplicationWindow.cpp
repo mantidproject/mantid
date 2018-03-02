50890b840d6cd941b138c2355b749dfa0ebb3949
Edward Brown <edward.brown.96@live.co.uk>
2018-03-02 13:02:29 +0000

    Re #21921: Fixed typo + applied clang-format.

--- MantidPlot/src/ApplicationWindow.cpp
+++ MantidPlot/src/ApplicationWindow.cpp
@@ -2603,8 +2603,7 @@ void ApplicationWindow::exportMatrix() {
     QMessageBox::critical(
         this, tr("MantidPlot - Export error"), // Mantid
         tr("Could not write to file: <br><h4> %1 </h4><p>Please verify that "
-           "you have the right to write to this location!")
-            .arg(file_name));
+           "you have the right to write to this location!").arg(file_name));
     return;
   }
 
@@ -2971,8 +2970,8 @@ void ApplicationWindow::setAutoUpdateTableValues(bool on) {
     QList<MdiSubWindow *> folderWindows = f->windowsList();
     foreach (MdiSubWindow *w, folderWindows) {
       if (w->inherits("Table"))
-        dynamic_cast<Table *>(w)->setAutoUpdateValues(
-            d_auto_update_table_values);
+        dynamic_cast<Table *>(w)
+            ->setAutoUpdateValues(d_auto_update_table_values);
     }
     f = f->folderBelow();
   }
@@ -3575,12 +3574,11 @@ MantidTable *ApplicationWindow::convertTableToTableWorkspace(Table *t) {
   }
   std::string wsName = t->objectName().toStdString();
   if (Mantid::API::AnalysisDataService::Instance().doesExist(wsName)) {
-    if (QMessageBox::question(this, "MantidPlot",
-                              "Workspace with name " + t->objectName() +
-                                  " already exists\n"
-                                  "Do you want to overwrite it?",
-                              QMessageBox::Yes | QMessageBox::No) ==
-        QMessageBox::Yes) {
+    if (QMessageBox::question(
+            this, "MantidPlot", "Workspace with name " + t->objectName() +
+                                    " already exists\n"
+                                    "Do you want to overwrite it?",
+            QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
       Mantid::API::AnalysisDataService::Instance().addOrReplace(wsName, tws);
     } else {
       return nullptr;
@@ -3758,10 +3756,9 @@ void ApplicationWindow::addErrorBars() {
 
   ErrDialog *ed = new ErrDialog(this);
   ed->setAttribute(Qt::WA_DeleteOnClose);
-  connect(
-      ed, SIGNAL(options(const QString &, int, const QString &, int, bool)),
-      this,
-      SLOT(defineErrorBars(const QString &, int, const QString &, int, bool)));
+  connect(ed, SIGNAL(options(const QString &, int, const QString &, int, bool)),
+          this, SLOT(defineErrorBars(const QString &, int, const QString &, int,
+                                     bool)));
   connect(ed, SIGNAL(options(const QString &, const QString &, int)), this,
           SLOT(defineErrorBars(const QString &, const QString &, int)));
 
@@ -4502,8 +4499,7 @@ ApplicationWindow *ApplicationWindow::open(const QString &fn,
       int choice = QMessageBox::question(
           this, tr("MantidPlot - File opening error"), // Mantid
           tr("The file <b>%1</b> is corrupted, but there exists a backup "
-             "copy.<br>Do you want to open the backup instead?")
-              .arg(fn),
+             "copy.<br>Do you want to open the backup instead?").arg(fn),
           QMessageBox::Yes | QMessageBox::Default,
           QMessageBox::No | QMessageBox::Escape);
       if (choice == QMessageBox::Yes)
@@ -4544,8 +4540,7 @@ void ApplicationWindow::openRecentFile(QAction *action) {
       QMessageBox::critical(
           this, tr("MantidPlot - File Open Error"), // Mantid
           tr("The file: <b> %1 </b> <p>is not there anymore!"
-             "<p>It will be removed from the list of recent files.")
-              .arg(fn));
+             "<p>It will be removed from the list of recent files.").arg(fn));
 
       recentFiles.removeAll(fn);
       updateRecentFilesList();
@@ -4566,8 +4561,7 @@ void ApplicationWindow::openRecentProject(QAction *action) {
     QMessageBox::critical(
         this, tr("MantidPlot - File Open Error"), // Mantid
         tr("The file: <b> %1 </b> <p>does not exist anymore!"
-           "<p>It will be removed from the list of recent projects.")
-            .arg(fn));
+           "<p>It will be removed from the list of recent projects.").arg(fn));
 
     recentProjects.removeAll(fn);
     updateRecentProjectsList();
@@ -4796,8 +4790,8 @@ void ApplicationWindow::readSettings() {
       settings.value("/Language", QLocale::system().name().section('_', 0, 0))
           .toString();
   show_windows_policy =
-      (ShowWindowsPolicy)settings
-          .value("/ShowWindowsPolicy", ApplicationWindow::ActiveFolder)
+      (ShowWindowsPolicy)settings.value("/ShowWindowsPolicy",
+                                        ApplicationWindow::ActiveFolder)
           .toInt();
 
   recentProjects = settings.value("/RecentProjects").toStringList();
@@ -5726,8 +5720,7 @@ void ApplicationWindow::exportGraph() {
     QMessageBox::critical(
         this, tr("MantidPlot - Export error"), // Mantid
         tr("Could not write to file: <br><h4> %1 </h4><p>Please verify that "
-           "you have the right to write to this location!")
-            .arg(file_name));
+           "you have the right to write to this location!").arg(file_name));
     return;
   }
   file.close();
@@ -5790,8 +5783,7 @@ void ApplicationWindow::exportLayer() {
     QMessageBox::critical(
         this, tr("MantidPlot - Export error"), // Mantid
         tr("Could not write to file: <br><h4> %1 </h4><p>Please verify that "
-           "you have the right to write to this location!")
-            .arg(file_name));
+           "you have the right to write to this location!").arg(file_name));
     return;
   }
   file.close();
@@ -5853,8 +5845,7 @@ void ApplicationWindow::exportAllGraphs() {
         QMessageBox::warning(
             this, tr("MantidPlot - Warning"), // Mantid
             tr("There are no plot layers available in window <b>%1</b>.<br>"
-               "Graph window not exported!")
-                .arg(plot2D->objectName()));
+               "Graph window not exported!").arg(plot2D->objectName()));
         QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
         continue;
       }
@@ -5872,8 +5863,7 @@ void ApplicationWindow::exportAllGraphs() {
       QApplication::restoreOverrideCursor();
 
       QString msg = tr("A file called: <p><b>%1</b><p>already exists. "
-                       "Do you want to overwrite it?")
-                        .arg(file_name);
+                       "Do you want to overwrite it?").arg(file_name);
       QMessageBox msgBox(QMessageBox::Question,
                          tr("MantidPlot - Overwrite file?"), msg, // Mantid
                          QMessageBox::Yes | QMessageBox::YesToAll |
@@ -6516,8 +6506,7 @@ void ApplicationWindow::exportAllTables(const QString &sep, bool colNames,
           auto result = QMessageBox::question(
               this, tr("MantidPlot - Overwrite file?"),
               tr("A file called: <p><b>%1</b><p>already exists. "
-                 "Do you want to overwrite it?")
-                  .arg(fileName),
+                 "Do you want to overwrite it?").arg(fileName),
               tr("&Yes"), tr("&All"), tr("&Cancel"), 0, 1);
 
           if (result == 1)
@@ -7708,8 +7697,7 @@ void ApplicationWindow::exportPDF() {
           this, tr("MantidPlot - Export error"), // Mantid
           tr("Could not write to file: <h4>%1</h4><p>Please verify that you "
              "have the right to write to this location or that the file is not "
-             "being used by another application!")
-              .arg(fname));
+             "being used by another application!").arg(fname));
       return;
     }
 
@@ -9525,7 +9513,7 @@ void ApplicationWindow::windowsMenuAboutToShow() {
 }
 
 namespace // anonymous
-{
+    {
 /**
  * Helper function used with Qt's qSort to make sure interfaces are in
  * alphabetical order.
@@ -10519,8 +10507,7 @@ void ApplicationWindow::showHelp() {
     QMessageBox::critical(
         this, tr("MantidPlot - Help Profile Not Found!"), // Mantid
         tr("The assistant could not start because the file <b>%1</b> was not "
-           "found in the help file directory!")
-                .arg("qtiplot.adp") +
+           "found in the help file directory!").arg("qtiplot.adp") +
             "<br>" +
             tr("This file is provided with the MantidPlot manual which can be "
                "downloaded from the following internet address:") +
@@ -11270,7 +11257,8 @@ void ApplicationWindow::addLayer() {
       tr("MantidPlot - Guess best origin for the new layer?"), // Mantid
       tr("Do you want MantidPlot to guess the best position for the new "
          "layer?\n Warning: this will rearrange existing layers!"), // Mantid
-      tr("&Guess"), tr("&Top-left corner"), tr("&Cancel"), 0, 2)) {
+      tr("&Guess"),
+      tr("&Top-left corner"), tr("&Cancel"), 0, 2)) {
   case 0: {
     setPreferences(plot->addLayer());
     plot->arrangeLayers(true, false);
@@ -11590,7 +11578,7 @@ void ApplicationWindow::setUpdateCurvesFromTable(Table *table, bool on) {
   colour is also dark.
 
   At the moment the fix is to simply hardcode the values to a dark on
-  light colour scheme which works cosistently on Fedora 26-7 and Ubuntu
+  light colour scheme which works consistently on Fedora 26-7 and Ubuntu
   14.04 where ToolTipBase has no effect.
 */
 void ApplicationWindow::patchPaletteForLinux(QPalette &palette) const {
@@ -15346,11 +15334,11 @@ void ApplicationWindow::saveFitFunctions(const QStringList &lst) {
                        "defined fit models to a different location.");
   explain += " " + tr("If you want to save your already defined models, please "
                       "choose a destination folder.");
-  if (QMessageBox::Ok != QMessageBox::information(
-                             this,
-                             tr("MantidPlot") + " - " + tr("Import fit models"),
-                             explain, // Mantid
-                             QMessageBox::Ok, QMessageBox::Cancel))
+  if (QMessageBox::Ok !=
+      QMessageBox::information(this, tr("MantidPlot") + " - " +
+                                         tr("Import fit models"),
+                               explain, // Mantid
+                               QMessageBox::Ok, QMessageBox::Cancel))
     return;
 
   QString dir = QFileDialog::getExistingDirectory(
@@ -15709,9 +15697,8 @@ void ApplicationWindow::performCustomAction(QAction *action) {
       connect(user_interface,
               SIGNAL(setFitPropertyBrowser(
                   MantidQt::MantidWidgets::FitPropertyBrowser *)),
-              mantidUI,
-              SLOT(setFitFunctionBrowser(
-                  MantidQt::MantidWidgets::FitPropertyBrowser *)));
+              mantidUI, SLOT(setFitFunctionBrowser(
+                            MantidQt::MantidWidgets::FitPropertyBrowser *)));
       user_interface->initializeLocalPython();
     } else {
       delete usr_win;
