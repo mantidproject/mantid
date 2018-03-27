0b6218f19d2a4bfcc0bc019b90fd28d0c497be93
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-03-27 16:22:21 +0100

    Re #22099 cleaned up mock object

--- scripts/SANS/sans/test_helper/file_information_mock.py
+++ scripts/SANS/sans/test_helper/file_information_mock.py
@@ -5,7 +5,8 @@ from sans.common.enums import (SANSFacility, SANSInstrument, FileType, SampleSha
 
 class SANSFileInformationMock(SANSFileInformation):
     def __init__(self, instrument=SANSInstrument.LOQ, facility=SANSFacility.ISIS, run_number=00000, file_name='file_name',
-                 height=8.0, width=8.0, thickness=1.0, shape=SampleShape.Cuboid):
+                 height=8.0, width=8.0, thickness=1.0, shape=SampleShape.Cuboid, date='2012-10-22T22:41:27', periods=1,
+                 event_mode=True, added_data=False):
         super(SANSFileInformationMock, self).__init__(file_name)
         self._instrument = instrument
         self._facility = facility
@@ -14,9 +15,14 @@ class SANSFileInformationMock(SANSFileInformation):
         self._width = width
         self._thickness = thickness
         self._shape = shape
+        self._date = date
+        self._file_name = file_name
+        self._periods = periods
+        self._event_mode = event_mode
+        self._added_data = added_data
 
     def get_file_name(self):
-        return 'file_name'
+        return self._file_name
 
     def get_instrument(self):
         return self._instrument
@@ -25,10 +31,10 @@ class SANSFileInformationMock(SANSFileInformation):
         return self._facility
 
     def get_date(self):
-        return DateAndTime('2012-10-22T22:41:27')
+        return DateAndTime(self._date)
 
     def get_number_of_periods(self):
-        return 1
+        return self._periods
 
     def get_run_number(self):
         return self._run_number
@@ -37,10 +43,10 @@ class SANSFileInformationMock(SANSFileInformation):
         return FileType.ISISNexus
 
     def is_event_mode(self):
-        return True
+        return self._event_mode
 
     def is_added_data(self):
-        return False
+        return self._added_data
 
     def get_height(self):
         return self._height
