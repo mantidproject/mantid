a74c930a29a51c104b1557d8a11015f37655a7d8
Nick Draper <nick.draper@stfc.ac.uk>
2018-04-03 14:47:52 +0100

    clecng formatting re #22143

--- Framework/ICat/inc/MantidICat/CatalogDownloadDataFiles.h
+++ Framework/ICat/inc/MantidICat/CatalogDownloadDataFiles.h
@@ -61,8 +61,7 @@ public:
   /// Algorithm's version for identification overriding a virtual method
   int version() const override { return 1; }
   const std::vector<std::string> seeAlso() const override {
-    return {"DownloadFile", "CatalogGetDataFiles", 
-            "CatalogLogin"};
+    return {"DownloadFile", "CatalogGetDataFiles", "CatalogLogin"};
   }
   /// Algorithm's category for identification overriding a virtual method
   const std::string category() const override {
