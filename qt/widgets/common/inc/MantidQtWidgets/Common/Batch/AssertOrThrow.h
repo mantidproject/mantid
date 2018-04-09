4b63056bea9adeead97a547cc4cf1f977449a538
Edward Brown <edward.brown@stfc.ac.uk>
2018-04-09 14:26:17 +0100

    Added assertOrThrow method.
    Re #22263

---
+++ qt/widgets/common/inc/MantidQtWidgets/Common/Batch/AssertOrThrow.h
@@ -0,0 +1,17 @@
+#ifndef MANTIDQTMANTIDWIDGETS_ASSERTORTHROW_H_
+#define MANTIDQTMANTIDWIDGETS_ASSERTORTHROW_H_
+#include <string>
+#include <stdexcept>
+namespace MantidQt {
+namespace MantidWidgets {
+namespace Batch {
+
+inline void assertOrThrow(bool condition, std::string const &message) {
+  if (!condition)
+    throw std::runtime_error(message);
+}
+
+}
+}
+}
+#endif
