970ec88d6a78bbc9f0605190d6b16b3e9ee8406a
Edward Brown <edward.brown@stfc.ac.uk>
2018-04-11 14:12:15 +0100

    Exposed JobTreeView to python.
    - Added bindings in SIP definition file.
    - Added JobTreeViewSignalAdapter which converts method calls on the
    base class JobTreeViewSubscriber into signals.
    - Added SIP bindings for std::string, std::vector and specializations
    of std::vector.
    
    Re #22263

---
+++ qt/widgets/common/inc/MantidQtWidgets/Common/Batch/JobTreeViewSignalAdapter.h
@@ -0,0 +1,28 @@
+#ifndef MANTIDQTMANTIDWIDGETS_JOBTREEVIEWSIGNALADAPTER_H_
+#define MANTIDQTMANTIDWIDGETS_JOBTREEVIEWSIGNALADAPTER_H_
+#include "MantidQtWidgets/Common/DllOption.h"
+#include "MantidQtWidgets/Common/Batch/JobTreeView.h"
+namespace MantidQt {
+namespace MantidWidgets {
+namespace Batch {
+class EXPORT_OPT_MANTIDQT_COMMON JobTreeViewSignalAdapter : public QObject, public JobTreeViewSubscriber {
+  Q_OBJECT
+public:
+  JobTreeViewSignalAdapter(JobTreeView &view, QObject *parent = nullptr);
+
+  void notifyCellChanged(RowLocation const &itemIndex, int column,
+                         std::string const &newValue) override;
+  void notifyRowInserted(RowLocation const &itemIndex) override;
+  void notifyRemoveRowsRequested(
+      std::vector<RowLocation> const &locationsOfRowsToRemove) override;
+signals:
+  void cellChanged(RowLocation const &itemIndex, int column,
+                   std::string newValue);
+  void rowInserted(RowLocation const &itemIndex);
+  void removeRowsRequested(
+      std::vector<MantidQt::MantidWidgets::Batch::RowLocation> const &locationsOfRowsToRemove);
+};
+}
+}
+}
+#endif // MANTIDQTMANTIDWIDGETS_JOBTREEVIEWSIGNALADAPTER_H_
---
+++ qt/widgets/common/src/Batch/JobTreeViewSignalAdapter.cpp
@@ -0,0 +1,29 @@
+#include "MantidQtWidgets/Common/Batch/JobTreeViewSignalAdapter.h"
+
+namespace MantidQt {
+namespace MantidWidgets {
+namespace Batch {
+JobTreeViewSignalAdapter::JobTreeViewSignalAdapter(JobTreeView &view,
+                                                   QObject *parent)
+    : QObject(parent) {
+  qRegisterMetaType<std::vector<RowLocation>>("std::vector<MantidQt::MantidWidgets::Batch::RowLocation>");
+  view.subscribe(*this);
+}
+
+void JobTreeViewSignalAdapter::notifyCellChanged(RowLocation const& itemIndex,
+                                                 int column,
+                                                 std::string const& newValue) {
+  emit cellChanged(itemIndex, column, newValue);
+}
+
+void JobTreeViewSignalAdapter::notifyRowInserted(RowLocation const& itemIndex) {
+  emit rowInserted(itemIndex);
+}
+
+void JobTreeViewSignalAdapter::notifyRemoveRowsRequested(
+    std::vector<RowLocation> const &locationsOfRowsToRemove) {
+  emit removeRowsRequested(locationsOfRowsToRemove);
+}
+}
+}
+}
