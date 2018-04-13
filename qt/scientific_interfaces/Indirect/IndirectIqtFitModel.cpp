9ab7565594b9df00ebee8d1c1df34fc5adef94cf
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2018-04-13 18:48:45 +0100

    Add the IndirectIqtFitModel
    Refs #22064

---
+++ qt/scientific_interfaces/Indirect/IndirectIqtFitModel.cpp
@@ -0,0 +1,135 @@
+#include "IndirectIqtFitModel.h"
+
+#include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/CompositeFunction.h"
+
+using namespace Mantid::API;
+
+namespace {
+MatrixWorkspace_sptr replaceInfinityAndNaN(MatrixWorkspace_sptr inputWS) {
+  auto replaceAlg = AlgorithmManager::Instance().create("ReplaceSpecialValues");
+  replaceAlg->setChild(true);
+  replaceAlg->initialize();
+  replaceAlg->setProperty("InputWorkspace", inputWS);
+  replaceAlg->setProperty("NaNValue", 0.0);
+  replaceAlg->setProperty("InfinityError", 0.0);
+  replaceAlg->setProperty("OutputWorkspace", inputWS->getName() + "_nospecial");
+  replaceAlg->execute();
+  return replaceAlg->getProperty("OutputWorkspace");
+}
+
+IFunction_sptr getIndexOfFirstInCategory(CompositeFunction_sptr function,
+                                         const std::string &category) {
+  for (auto i = 0u; i < function->nFunctions(); ++i) {
+    if (function->getFunction(i)->category() == category)
+      return function->getFunction(i);
+  }
+  return nullptr;
+}
+
+IFunction_sptr getFirstInCategory(IFunction_sptr function,
+                                  const std::string &category) {
+  auto composite = boost::dynamic_pointer_cast<CompositeFunction>(function);
+  if (composite)
+    return getFirstInCategory(composite, category);
+  else if (function->category() == category)
+    return function;
+  return nullptr;
+}
+
+std::vector<std::string> getParameters(IFunction_sptr function,
+                                       const std::string &shortParameterName) {
+  std::vector<std::string> parameters;
+
+  for (const auto &longName : function->getParameterNames()) {
+    if (boost::algorithm::ends_with(longName, shortParameterName))
+      parameters.push_back(longName);
+  }
+  return parameters;
+}
+}; // namespace
+
+namespace MantidQt {
+namespace CustomInterfaces {
+namespace IDA {
+
+void IndirectIqtFitModel::addWorkspace(MatrixWorkspace_sptr workspace,
+                                       const Spectra &spectra) {
+  IndirectFittingModel::addWorkspace(replaceInfinityAndNaN(workspace), spectra);
+}
+
+IAlgorithm_sptr IndirectIqtFitModel::sequentialFitAlgorithm() const {
+  return AlgorithmManager::Instance().create("IqtFitSequential");
+}
+
+IAlgorithm_sptr IndirectIqtFitModel::simultaneousFitAlgorithm() const {
+  return AlgorithmManager::Instance().create("IqtFitSimultaneous");
+}
+
+std::string IndirectIqtFitModel::sequentialFitOutputName() const {
+  if (isMultiFit())
+    return "MultiIqtFit_" + m_fitType;
+  return inputDisplayName("%1%_IqtFit_" + m_fitType + "_s%2%", "_to_", 0);
+}
+
+std::string IndirectIqtFitModel::simultaneousFitOutputName() const {
+  if (isMultiFit())
+    return "MultiSimultaneousIqtFit_" + m_fitType;
+  return inputDisplayName("%1%_IqtFit_mult" + m_fitType + "_s%2%", "_to_", 0);
+}
+
+void IndirectIqtFitModel::setFitTypeString(const std::string &fitType) {
+  m_fitType = fitType;
+}
+
+std::string IndirectIqtFitModel::createIntensityTie() const {
+  const auto intensityParameters =
+      getParameters(getFittingFunction(), "Height");
+  const auto backgroundParameters = getParameters(getFittingFunction(), "A0");
+
+  if (intensityParameters.size() + backgroundParameters.size() < 2 ||
+      intensityParameters.empty())
+    return "";
+
+  std::stringstream tieString;
+
+  for (const auto &parameter : backgroundParameters)
+    tieString << "-" << parameter;
+
+  for (auto i = 1u; i < intensityParameters.size(); ++i)
+    tieString << "-" << intensityParameters[i];
+
+  return intensityParameters[0] + "=" + tieString.str();
+}
+
+std::unordered_map<std::string, ParameterValue>
+IndirectIqtFitModel::getDefaultParameters(std::size_t index) const {
+  std::unordered_map<std::string, ParameterValue> parameters;
+  auto background = getFirstInCategory(getFittingFunction(), "Background");
+
+  double height = 1.0;
+  if (background && background->hasParameter("A0"))
+    height -= background->getParameter("A0");
+  parameters["Height"] = height;
+
+  auto inputWs = getWorkspace(index);
+  double tau = 0;
+
+  if (inputWs) {
+    auto x = inputWs->x(0);
+    auto y = inputWs->y(0);
+
+    if (x.size() > 4) {
+      tau = -x[4] / log(y[4]);
+    }
+  }
+
+  parameters["Lifetime"] = tau;
+  parameters["Stretching"] = 1.0;
+  parameters["A0"] = 0.0;
+  return parameters;
+}
+
+} // namespace IDA
+} // namespace CustomInterfaces
+} // namespace MantidQt
---
+++ qt/scientific_interfaces/Indirect/IndirectIqtFitModel.h
@@ -0,0 +1,34 @@
+#ifndef MANTIDQTCUSTOMINTERFACESIDA_INDIRECTIQTFITMODEL_H_
+#define MANTIDQTCUSTOMINTERFACESIDA_INDIRECTIQTFITMODEL_H_
+
+#include "IndirectFittingModel.h"
+
+namespace MantidQt {
+namespace CustomInterfaces {
+namespace IDA {
+
+class DLLExport IndirectIqtFitModel : public IndirectFittingModel {
+public:
+  void setFitTypeString(const std::string &fitType);
+  void addWorkspace(Mantid::API::MatrixWorkspace_sptr workspace,
+                    const Spectra &spectra) override;
+
+  std::unordered_map<std::string, ParameterValue>
+  getDefaultParameters(std::size_t index) const override;
+
+  std::string createIntensityTie() const;
+
+private:
+  Mantid::API::IAlgorithm_sptr sequentialFitAlgorithm() const override;
+  Mantid::API::IAlgorithm_sptr simultaneousFitAlgorithm() const override;
+  std::string sequentialFitOutputName() const override;
+  std::string simultaneousFitOutputName() const override;
+
+  std::string m_fitType;
+};
+
+} // namespace IDA
+} // namespace CustomInterfaces
+} // namespace MantidQt
+
+#endif
