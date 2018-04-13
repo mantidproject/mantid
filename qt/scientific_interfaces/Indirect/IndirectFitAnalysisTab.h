6873c06e1a99806df3c42ddeb2a352b1613ddf04
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2018-04-13 18:46:24 +0100

    Update IndirectFitAnalysisTab to use IndirectFittingModel
    Refs #22064

--- qt/scientific_interfaces/Indirect/IndirectFitAnalysisTab.h
+++ qt/scientific_interfaces/Indirect/IndirectFitAnalysisTab.h
@@ -2,6 +2,9 @@
 #define MANTIDQTCUSTOMINTERFACESIDA_IFATAB_H_
 
 #include "IndirectDataAnalysisTab.h"
+#include "IndirectFittingModel.h"
+#include "IndirectSpectrumSelectionPresenter.h"
+#include "IndirectSpectrumSelectionView.h"
 
 #include "MantidQtWidgets/Common/IndirectFitPropertyBrowser.h"
 
@@ -76,7 +79,10 @@ class DLLExport IndirectFitAnalysisTab : public IndirectDataAnalysisTab {
 
 public:
   /// Constructor
-  IndirectFitAnalysisTab(QWidget *parent = nullptr);
+  IndirectFitAnalysisTab(IndirectFittingModel *model,
+                         QWidget *parent = nullptr);
+
+  void setSpectrumSelectionView(IndirectSpectrumSelectionView *view);
 
   Mantid::API::IFunction_sptr background() const;
 
@@ -105,10 +111,6 @@ public:
   virtual bool canPlotGuess() const;
 
   virtual bool doPlotGuess() const = 0;
-
-  std::string outputWorkspaceName() const;
-
-  std::string outputWorkspaceName(const size_t &spectrum) const;
 
   void setConvolveMembers(bool convolveMembers);
 
@@ -175,21 +177,6 @@ public:
   void setCustomSettingChangesFunction(const QString &settingKey,
                                        bool changesFunction);
 
-  QHash<QString, double> fitParameterValues() const;
-
-  virtual QHash<QString, double> createDefaultValues() const;
-
-  QHash<QString, double> defaultParameterValues() const;
-
-  QHash<QString, double> parameterValues() const;
-
-  virtual Mantid::API::IFunction_sptr fitFunction() const;
-
-  virtual QHash<QString, QString>
-  functionNameChanges(Mantid::API::IFunction_sptr function) const;
-
-  virtual Mantid::API::MatrixWorkspace_sptr fitWorkspace() const;
-
 protected:
   /**
    * Adds a fit property browser to the specified Indirect Fit Analysis Tab.
@@ -201,22 +188,13 @@ protected:
     tab->properties->addWidget(m_fitPropertyBrowser);
   }
 
-  virtual int minimumSpectrum() const = 0;
+  IndirectFittingModel *fittingModel() const;
 
-  virtual int maximumSpectrum() const = 0;
+  void fitAlgorithmComplete();
 
-  void setDefaultPropertyValue(const QString &propertyName,
-                               const double &propertyValue);
+  void saveResult();
 
-  void removeDefaultPropertyValue(const QString &propertyName);
-
-  bool hasDefaultPropertyValue(const QString &propertyName);
-
-  void fitAlgorithmComplete(const std::string &paramWSName);
-
-  void saveResult(const std::string &resultName);
-
-  void plotResult(const std::string &resultName, const QString &plotType);
+  void plotResult(const QString &plotType);
 
   void fillPlotTypeComboBox(QComboBox *comboBox);
 
@@ -242,10 +220,6 @@ protected:
       Mantid::API::MatrixWorkspace_sptr inputWS,
       Mantid::API::MatrixWorkspace_sptr guessWorkspace) const;
 
-  virtual Mantid::API::IAlgorithm_sptr singleFitAlgorithm() const;
-
-  virtual Mantid::API::IAlgorithm_sptr sequentialFitAlgorithm() const;
-
   Mantid::API::MatrixWorkspace_sptr
   createGuessWorkspace(Mantid::API::IFunction_const_sptr func,
                        int wsIndex) const;
@@ -263,10 +237,6 @@ protected:
 
   virtual void setMaxIterations(Mantid::API::IAlgorithm_sptr fitAlgorithm,
                                 int maxIterations) const;
-
-  virtual std::string createSequentialFitOutputName() const;
-
-  virtual std::string createSingleFitOutputName() const = 0;
 
   virtual void addGuessPlot(Mantid::API::MatrixWorkspace_sptr workspace) = 0;
   virtual void removeGuessPlot() = 0;
@@ -286,6 +256,8 @@ signals:
   void customBoolChanged(const QString &key, bool value);
 
 protected slots:
+  virtual void setModelFitFunction();
+
   void clearGuessWindowPlot();
 
   void setSelectedSpectrum(int spectrum) override;
@@ -298,19 +270,20 @@ protected slots:
 
   void xMaxSelected(double xMax);
 
-  void updatePreviousModelSelected();
-
   virtual void updatePlotRange() = 0;
 
   void executeSingleFit();
 
-  void executeSequentialFit();
+  void executeFit();
 
   virtual void algorithmComplete(bool error) = 0;
 
   void newInputDataLoaded(const QString &wsName);
 
   void updateParameterValues();
+
+  void updateParameterValues(
+      const std::unordered_map<std::string, ParameterValue> &parameters);
 
   virtual void updatePreviewPlots() = 0;
 
@@ -339,11 +312,6 @@ private:
   void loadSettings(const QSettings &settings) override = 0;
   virtual void disablePlotGuess() = 0;
   virtual void enablePlotGuess() = 0;
-  QSet<QString> parameterNames();
-  void updateParametersFromTable(const std::string &paramWSName);
-  Mantid::API::IFunction_sptr
-  updateFunctionTies(Mantid::API::IFunction_sptr function,
-                     const QHash<QString, QString> &functionNameChanges) const;
 
   void
   ensureAppendCompatibility(Mantid::API::MatrixWorkspace_sptr inputWS,
@@ -364,18 +332,14 @@ private:
                 double endX, int startIndex, int endIndex) const;
   void deleteWorkspace(Mantid::API::MatrixWorkspace_sptr workspace) const;
 
-  Mantid::API::IFunction_const_sptr m_fitFunction;
-  QHash<size_t, QHash<QString, double>> m_parameterValues;
-  QHash<QString, double> m_defaultPropertyValues;
-  QHash<QString, QString> m_functionNameChanges;
-  MantidWidgets::IndirectFitPropertyBrowser *m_fitPropertyBrowser;
-
-  QHash<size_t, std::pair<size_t, std::string>> m_outputFitPosition;
-  bool m_appendResults;
-  bool m_previousModelSelected;
   Mantid::API::MatrixWorkspace_sptr m_inputAndGuessWorkspace;
-
   QtLazyAsyncRunner<std::function<void()>> m_plotWindowGuessRunner;
+
+  std::unique_ptr<IndirectFittingModel> m_fittingModel;
+  MantidWidgets::IndirectFitPropertyBrowser *m_fitPropertyBrowser;
+  std::unique_ptr<IndirectSpectrumSelectionPresenter> m_spectrumPresenter;
+
+  std::string m_outputBaseName;
 };
 
 } // namespace IDA
