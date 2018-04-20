048c83e39206a79c284f8a089b99d069b3c113be
Gagik Vardanyan <vardanyan@ill.fr>
2018-04-20 14:46:58 +0200

    Re #22274 fix flake8 warning

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/PowderDiffILLDetScanReduction.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/PowderDiffILLDetScanReduction.py
@@ -8,6 +8,12 @@ from mantid.simpleapi import *
 
 class PowderDiffILLDetScanReduction(DataProcessorAlgorithm):
     _progress = None
+    _height_range = ''
+    _tolerance = 1000. # set a large number to avoid splitting of counts in SoT
+    _mirror = None
+    _crop_negative = None
+    _out_ws_name = None
+    _final_mask = None
 
     def category(self):
         return "ILL\\Diffraction;Diffraction\\Reduction"
@@ -106,13 +112,54 @@ class PowderDiffILLDetScanReduction(DataProcessorAlgorithm):
         self.log().debug('Preparing to mask with DetectorList='+mask[:-1])
         return mask[:-1]
 
+    def _reduce_1D(self, input_group):
+        output1D = SumOverlappingTubes(InputWorkspaces=input_group,
+                                       OutputType='1D',
+                                       HeightAxis=self._height_range,
+                                       MirrorScatteringAngles=self._mirror,
+                                       CropNegativeScatteringAngles=self._crop_negative,
+                                       OutputWorkspace=self._out_ws_name + '_1D',
+                                       ScatteringAngleTolerance=self._tolerance)
+        return output1D
+
+    def _reduce_2DTubes(self, input_group):
+        output2DtubesName = self._out_ws_name + '_2DTubes'
+        output2DTubes = SumOverlappingTubes(InputWorkspaces=input_group,
+                                            OutputType='2DTubes',
+                                            HeightAxis=self._height_range,
+                                            MirrorScatteringAngles=self._mirror,
+                                            CropNegativeScatteringAngles=self._crop_negative,
+                                            ScatteringAngleTolerance=self._tolerance,
+                                            OutputWorkspace=output2DtubesName)
+        if self._final_mask != 0:
+            nSpec = mtd[output2DtubesName].getNumberHistograms()
+            mask_list = '0-{0},{1}-{2}'.format(self._final_mask,nSpec-self._final_mask,nSpec-1)
+            MaskDetectors(Workspace=output2DtubesName,WorkspaceIndexList=mask_list)
+
+        return output2DTubes
+
+    def _reduce_2D(self, input_group):
+        output2DName = self._out_ws_name + '_2D'
+        output2D = SumOverlappingTubes(InputWorkspaces=input_group,
+                                       OutputType='2D',
+                                       HeightAxis=self._height_range,
+                                       MirrorScatteringAngles=self._mirror,
+                                       CropNegativeScatteringAngles=self._crop_negative,
+                                       ScatteringAngleTolerance=self._tolerance,
+                                       OutputWorkspace = output2DName)
+        if self._final_mask != 0:
+            nSpec = mtd[output2DName].getNumberHistograms()
+            mask_list = '0-{0},{1}-{2}'.format(self._final_mask,nSpec-self._final_mask,nSpec-1)
+            MaskDetectors(Workspace=output2DName,WorkspaceIndexList=mask_list)
+
+        return output2D
+
     def PyExec(self):
         data_type = 'Raw'
         if self.getProperty('UseCalibratedData').value:
             data_type = 'Calibrated'
 
         self._progress = Progress(self, start=0.0, end=1.0, nreports=6)
-
         self._progress.report('Loading data')
         input_workspace = LoadAndMerge(Filename=self.getPropertyValue('Run'),
                                        LoaderName='LoadILLDiffraction',
@@ -154,78 +201,42 @@ class PowderDiffILLDetScanReduction(DataProcessorAlgorithm):
             for ws in input_group:
                 MaskDetectors(Workspace=ws, ComponentList=components_to_mask)
 
-        height_range = ''
         height_range_prop = self.getProperty('HeightRange').value
         if len(height_range_prop) == 0:
             run = mtd["input_group"].getItem(0).getRun()
             if run.hasProperty("PixelHeight") and run.hasProperty("MaxHeight"):
                 pixelHeight = run.getLogData("PixelHeight").value
                 maxHeight = run.getLogData("MaxHeight").value
-                height_range = str(-maxHeight) + ',' + str(pixelHeight) + ',' + str(maxHeight)
+                self._height_range = str(-maxHeight) + ',' + str(pixelHeight) + ',' + str(maxHeight)
         elif len(height_range_prop) == 2:
-            height_range = str(height_range_prop[0]) + ', ' + str(height_range_prop[1])
-
+            self._height_range = str(height_range_prop[0]) + ', ' + str(height_range_prop[1])
         output_workspaces = []
-        output_workspace_name = self.getPropertyValue('OutputWorkspace')
-
-        mirror_angles = False
-        crop_negative = self.getProperty('CropNegativeScatteringAngles').value
+        self._out_ws_name = self.getPropertyValue('OutputWorkspace')
+        self._mirror = False
+        self._crop_negative = self.getProperty('CropNegativeScatteringAngles').value
         if instrument.hasParameter("mirror_scattering_angles"):
-            mirror_angles = instrument.getBoolParameter("mirror_scattering_angles")[0]
-
-        scatterting_angle_tol = 1000.
-        final_mask = self.getProperty('FinalMask').value
+            self._mirror = instrument.getBoolParameter("mirror_scattering_angles")[0]
+        self._final_mask = self.getProperty('FinalMask').value
 
         self._progress.report('Doing Output2DTubes Option')
         if self.getProperty('Output2DTubes').value:
-            output2DtubesName = output_workspace_name + '_2DTubes'
-            output2DTubes = SumOverlappingTubes(InputWorkspaces=input_group,
-                                                OutputType='2DTubes',
-                                                HeightAxis=height_range,
-                                                MirrorScatteringAngles=mirror_angles,
-                                                CropNegativeScatteringAngles=crop_negative,
-                                                ScatteringAngleTolerance=scatterting_angle_tol,
-                                                OutputWorkspace=output2DtubesName)
-            if final_mask != 0:
-                nSpec = mtd[output2DtubesName].getNumberHistograms()
-                mask_list = '0-{0},{1}-{2}'.format(final_mask,nSpec-final_mask,nSpec-1)
-                MaskDetectors(Workspace=output2DtubesName,WorkspaceIndexList=mask_list)
-
+            output2DTubes = self._reduce_2DTubes(input_group)
             output_workspaces.append(output2DTubes)
 
         self._progress.report('Doing Output2D Option')
         if self.getProperty('Output2D').value:
-            output2DName = output_workspace_name + '_2D'
-            output2D = SumOverlappingTubes(InputWorkspaces=input_group,
-                                           OutputType='2D',
-                                           HeightAxis=height_range,
-                                           MirrorScatteringAngles=mirror_angles,
-                                           CropNegativeScatteringAngles=crop_negative,
-                                           ScatteringAngleTolerance=scatterting_angle_tol,
-                                           OutputWorkspace = output2DName)
-            if final_mask != 0:
-                nSpec = mtd[output2DName].getNumberHistograms()
-                mask_list = '0-{0},{1}-{2}'.format(final_mask,nSpec-final_mask,nSpec-1)
-                MaskDetectors(Workspace=output2DName,WorkspaceIndexList=mask_list)
-
+            output2D = self._reduce_2D(input_group)
             output_workspaces.append(output2D)
 
         self._progress.report('Doing Output1D Option')
         if self.getProperty('Output1D').value:
-            output1D = SumOverlappingTubes(InputWorkspaces=input_group,
-                                           OutputType='1D',
-                                           HeightAxis=height_range,
-                                           MirrorScatteringAngles=mirror_angles,
-                                           CropNegativeScatteringAngles=crop_negative,
-                                           OutputWorkspace=output_workspace_name + '_1D',
-                                           ScatteringAngleTolerance=scatterting_angle_tol)
+            output1D = self._reduce_1D(input_group)
             output_workspaces.append(output1D)
-        DeleteWorkspace('input_group')
 
         self._progress.report('Finishing up...')
-
-        GroupWorkspaces(InputWorkspaces=output_workspaces, OutputWorkspace=output_workspace_name)
-        self.setProperty('OutputWorkspace', output_workspace_name)
+        DeleteWorkspace('input_group')
+        GroupWorkspaces(InputWorkspaces=output_workspaces, OutputWorkspace=self._out_ws_name)
+        self.setProperty('OutputWorkspace', self._out_ws_name)
 
 # Register the algorithm with Mantid
 AlgorithmFactory.subscribe(PowderDiffILLDetScanReduction)
