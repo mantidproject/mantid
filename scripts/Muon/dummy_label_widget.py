75b09b691b248a59f489e012308fa692b6ba1b3f
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-04-23 13:29:21 +0100

    refs #20941 fixed flake8 in muon analysis 22

--- scripts/Muon/dummy_label_widget.py
+++ scripts/Muon/dummy_label_widget.py
@@ -1,12 +1,10 @@
 from __future__ import (absolute_import, division, print_function)
 
-
 from Muon import dummy_label_view
 from Muon import dummy_label_presenter
 
 
 class DummyLabelWidget(object):
-
 
     def __init__(self, name, parent=None):
         view = dummy_label_view.DummyLabelView(name, parent)
--- scripts/Muon/dummy_presenter.py
+++ scripts/Muon/dummy_presenter.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division, print_function)
 
 
 class DummyPresenter(object):
-    
+
     def __init__(self,view,model):
         self.view=view
         self.model=model
