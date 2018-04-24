0716bfa4d0b7744cb851045e1b37907d5657b22b
Vickie Lynch <lynchve@ornl.gov>
2018-04-24 11:28:53 -0400

    Refs #22320 clang-format

--- Framework/Crystal/inc/MantidCrystal/SaveLauenorm.h
+++ Framework/Crystal/inc/MantidCrystal/SaveLauenorm.h
@@ -19,18 +19,14 @@ namespace Crystal {
 class DLLExport SaveLauenorm : public API::Algorithm {
 public:
   /// Algorithm's name for identification
-  const std::string name() const override {
-    return "SaveLauenorm";
-  };
+  const std::string name() const override { return "SaveLauenorm"; };
   /// Summary of algorithms purpose
   const std::string summary() const override {
     return "Save a PeaksWorkspace to a ASCII file for each detector.";
   }
 
   /// Algorithm's version for identification
-  int version() const override {
-    return 1;
-  };
+  int version() const override { return 1; };
   /// Algorithm's category for identification
   const std::string category() const override {
     return "Crystal\\DataHandling;DataHandling\\Text";
@@ -45,13 +41,12 @@ private:
   DataObjects::PeaksWorkspace_sptr ws;
   void sizeBanks(std::string bankName, int &nCols, int &nRows);
 
-  const std::vector<std::string> m_typeList{ "TRICLINIC",    "MONOCLINIC",
-                                             "ORTHORHOMBIC", "TETRAGONAL",
-                                             "HEXAGONAL",    "RHOMBOHEDRAL",
-                                             "CUBIC" };
+  const std::vector<std::string> m_typeList{
+      "TRICLINIC", "MONOCLINIC",   "ORTHORHOMBIC", "TETRAGONAL",
+      "HEXAGONAL", "RHOMBOHEDRAL", "CUBIC"};
 
-  const std::vector<std::string> m_centeringList{ "P", "A", "B", "C",
-                                                  "I", "F", "R" };
+  const std::vector<std::string> m_centeringList{"P", "A", "B", "C",
+                                                 "I", "F", "R"};
 };
 
 } // namespace Mantid
