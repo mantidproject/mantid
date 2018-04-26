dd294a4b4ac377c224773c0a170ccee5b4cc491f
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2018-04-26 14:34:06 +0100

    Ensure the correct SDK is set for msbuild

--- buildconfig/windows/command-prompt.bat
+++ buildconfig/windows/command-prompt.bat
@@ -7,4 +7,4 @@
 call %~dp0buildenv.bat
 set VCVARS=@MSVC_VAR_LOCATION@
 :: Start command line
-%COMSPEC% /k ""%VCVARS%\vcvarsall.bat"" amd64
+%COMSPEC% /k ""%VCVARS%\vcvarsall.bat"" amd64 @CMAKE_SYSTEM_VERSION@
