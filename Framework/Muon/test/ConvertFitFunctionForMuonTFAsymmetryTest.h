cb389e46ee624cfbec61fc931d391dd90818ff69
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-05-22 10:42:07 +0100

    refs #22423 added a unit test for convertFitFunc

--- Framework/Muon/test/ConvertFitFunctionForMuonTFAsymmetryTest.h
+++ Framework/Muon/test/ConvertFitFunctionForMuonTFAsymmetryTest.h
@@ -23,6 +23,12 @@ const std::string OFFSET_PARAM{"f0.f0.f1.f0.A0"};
 const std::string USER_FUNC   {"f0.f0.f1.f1."};
 const std::string EXP_PARAM   {"f0.f1.A"};
 
+// for domain 2
+const std::string NORM_PARAM2  {"f1.f0.f0.f0.A0"};
+const std::string OFFSET_PARAM2{"f1.f0.f1.f0.A0"};
+const std::string USER_FUNC2   {"f1.f0.f1.f1."};
+const std::string EXP_PARAM2   {"f1.f1.A"};
+
 struct yData {
   double operator()(const double x, size_t) {
     // Create a fake muon dataset
@@ -56,7 +62,7 @@ row<<double(j+1)<<names[j]<<"test";
 return table;
 }
 
-IAlgorithm_sptr setUpAlg(std::vector<std::string> wsNames, std::string funcString) {
+IAlgorithm_sptr setUpAlg(std::vector<std::string> wsNames, const IFunction_sptr &func) {
   IAlgorithm_sptr asymmAlg =
       AlgorithmManager::Instance().create("ConvertFitFunctionForMuonTFAsymmetry");
   asymmAlg->initialize();
@@ -64,7 +70,6 @@ IAlgorithm_sptr setUpAlg(std::vector<std::string> wsNames, std::string funcStrin
   asymmAlg->setProperty("WorkspaceList", wsNames);
   ITableWorkspace_sptr table = genTable();
   asymmAlg->setProperty("NormalisationTable", table);
-  auto func = FunctionFactory::Instance().createInitialized(funcString);
   asymmAlg->setProperty("InputFunction", func);
   return asymmAlg;
 }
@@ -77,13 +82,16 @@ void genData(){
     AnalysisDataService::Instance().addOrReplace("ws3",ws1);
 }
 
-IFunction_sptr doFit(const std::string funcIn, int iterations,std::vector<std::string> wsNames){
+IFunction_sptr doFit(const IFunction_sptr &func, int iterations,std::vector<std::string> wsNames){
  
-  auto func = FunctionFactory::Instance().createInitialized(funcIn);
   IAlgorithm_sptr  fit = AlgorithmManager::Instance().create("Fit");
   fit->initialize();
   fit->setProperty("Function",func);
   fit->setProperty("InputWorkspace", wsNames[0]);
+  if(wsNames.size()>1){
+  
+     fit->setProperty("InputWorkspace_1", wsNames[1]);
+  }
   fit->setProperty("Output","fit");
   fit->setProperty("MaxIterations",iterations);
   fit->execute();
@@ -109,7 +117,7 @@ public:
   void testInit() {
     genData();
     std::vector<std::string> wsNames = {"ws1"};
-    std::string func = "name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)";
+    IFunction_sptr func = FunctionFactory::Instance().createInitialized("name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)");
     IAlgorithm_sptr alg = setUpAlg(wsNames,func);
     TS_ASSERT(alg->isInitialized())
   }
@@ -117,7 +125,7 @@ public:
   void test_Execute() {
     genData();
     std::vector<std::string> wsNames = {"ws1"};
-    std::string func = "name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)";
+     IFunction_sptr func = FunctionFactory::Instance().createInitialized("name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)");
     IAlgorithm_sptr alg = setUpAlg(wsNames,func);
     TS_ASSERT(alg->isInitialized())
     TS_ASSERT_THROWS_NOTHING(alg->execute());
@@ -127,48 +135,150 @@ public:
   void test_1D() {
     genData();
     std::vector<std::string> wsNames = {"ws1"};
-    std::string func = "name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)";
+    IFunction_sptr func = FunctionFactory::Instance().createInitialized("name=LinearBackground,A0=0,A1=2;");
     IAlgorithm_sptr alg = setUpAlg(wsNames,func);
     TS_ASSERT(alg->isInitialized())
     TS_ASSERT_THROWS_NOTHING(alg->execute());
     TS_ASSERT(alg->isExecuted());
-    auto normFuncString = alg->getPropertyValue("OutputFunction");
-    auto normFunc = FunctionFactory::Instance().createInitialized(normFuncString);
+    IFunction_sptr normFunc = alg->getProperty("OutputFunction");
+
+    auto outFunc = doFit(normFunc,0,wsNames);
+    TS_ASSERT_DELTA(outFunc->getParameter(OFFSET_PARAM),1.,0.0001);
+    TS_ASSERT_DELTA(outFunc->getParameter(EXP_PARAM),0.0,0.0001);
+    TS_ASSERT_DELTA(outFunc->getParameter(USER_FUNC+"A1"),2.0,0.0001);
+ } 
+  
+
+  void test_1DFix() {
+    genData();
+    std::vector<std::string> wsNames = {"ws1"};
+     IFunction_sptr func = FunctionFactory::Instance().createInitialized("name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)");
+    IAlgorithm_sptr alg = setUpAlg(wsNames,func);
+    TS_ASSERT(alg->isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg->execute());
+    TS_ASSERT(alg->isExecuted());
+    IFunction_sptr normFunc = alg->getProperty("OutputFunction");
+
+    auto outFunc = doFit(normFunc,200,wsNames);
+    TS_ASSERT_DELTA(outFunc->getParameter(OFFSET_PARAM),1.,0.0001);
+    TS_ASSERT_DELTA(outFunc->getParameter(EXP_PARAM),0.0,0.0001);
+    TS_ASSERT_DELTA(outFunc->getParameter(USER_FUNC+"A1"),2.0,0.0001);
+    TS_ASSERT_DIFFERS(outFunc->getParameter(USER_FUNC+"A0"),0.0);
+    
+  }
+  void test_1DTie() {
+    genData();
+    std::vector<std::string> wsNames = {"ws1"};
+     IFunction_sptr func = FunctionFactory::Instance().createInitialized("name=LinearBackground,A0=0,A1=2;name=LinearBackground,A0=0,A1=4;ties =(f0.A1=f1.A1)");
+    IAlgorithm_sptr alg = setUpAlg(wsNames,func);
+    TS_ASSERT(alg->isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg->execute());
+    TS_ASSERT(alg->isExecuted());
+    IFunction_sptr normFunc = alg->getProperty("OutputFunction");
+
+    auto outFunc = doFit(normFunc,200,wsNames);
+    TS_ASSERT_DELTA(outFunc->getParameter(OFFSET_PARAM),1.,0.0001);
+    TS_ASSERT_DELTA(outFunc->getParameter(EXP_PARAM),0.0,0.0001);
+    TS_ASSERT_EQUALS(outFunc->getParameter(USER_FUNC+"f0.A1"),outFunc->getParameter(USER_FUNC+"f1.A1"));
+    TS_ASSERT_DIFFERS(outFunc->getParameter(USER_FUNC+"f0.A0"),func->getParameter("f0.A0"));
+    TS_ASSERT_DIFFERS(outFunc->getParameter(USER_FUNC+"f1.A0"),func->getParameter("f1.A0"));
+    
+  }
+  // tests for multi domain
+  void test_2D() {
+    genData();
+    std::vector<std::string> wsNames = {"ws1","ws2"};
+    std::string multiFuncString = "composite=MultiDomainFunction,NumDeriv=1;";
+    multiFuncString += "name=LinearBackground, $domains=i,A0=0,A1=1;";
+    multiFuncString += "name=LinearBackground, $domains=i,A0=2,A1=3;";
+
+    IFunction_sptr multiFunc = FunctionFactory::Instance().createInitialized(multiFuncString);
+
+    IAlgorithm_sptr alg = setUpAlg(wsNames,multiFunc);
+    TS_ASSERT(alg->isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg->execute());
+    TS_ASSERT(alg->isExecuted());
+    IFunction_sptr normFunc = doFit(alg->getProperty("OutputFunction"),0,wsNames);
 
     TS_ASSERT_DELTA(normFunc->getParameter(NORM_PARAM),1.,0.0001);
     TS_ASSERT_DELTA(normFunc->getParameter(OFFSET_PARAM),1.,0.0001);
     TS_ASSERT_DELTA(normFunc->getParameter(EXP_PARAM),0.0,0.0001);
 
     TS_ASSERT_DELTA(normFunc->getParameter(USER_FUNC+"A0"),0.0,0.0001);
-    TS_ASSERT_DELTA(normFunc->getParameter(USER_FUNC+"A1"),2.0,0.0001);
-    //auto paramTable = doFit(normFunc, 0,wsNames);
+    TS_ASSERT_DELTA(normFunc->getParameter(USER_FUNC+"A1"),1.0,0.0001);
+    
+
+    TS_ASSERT_DELTA(normFunc->getParameter(NORM_PARAM2),2.,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(OFFSET_PARAM2),1.,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(EXP_PARAM2),0.0,0.0001);
+
+   TS_ASSERT_DELTA(normFunc->getParameter(USER_FUNC2+"A0"),2.0,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(USER_FUNC2+"A1"),3.0,0.0001);
     
   }
-
-  void test_1DFix() {
+  void test_2DFix() {
     genData();
-    std::vector<std::string> wsNames = {"ws1"};
-    std::string func = "name=LinearBackground,A0=0,A1=2;ties =(f0.A1=2)";
-    IAlgorithm_sptr alg = setUpAlg(wsNames,func);
+    std::vector<std::string> wsNames = {"ws1","ws2"};
+    std::string multiFuncString = "composite=MultiDomainFunction,NumDeriv=1;";
+    multiFuncString += "name=LinearBackground, $domains=i,A0=0,A1=1.5;";
+    multiFuncString += "name=LinearBackground, $domains=i,A0=2,A1=3;";
+
+    IFunction_sptr multiFunc = FunctionFactory::Instance().createInitialized(multiFuncString);
+    multiFunc->addTies("f0.A1=1.5");
+    IAlgorithm_sptr alg = setUpAlg(wsNames,multiFunc);
     TS_ASSERT(alg->isInitialized())
     TS_ASSERT_THROWS_NOTHING(alg->execute());
     TS_ASSERT(alg->isExecuted());
-    auto normFuncString = alg->getPropertyValue("OutputFunction");
+    IFunction_sptr normFunc = doFit(alg->getProperty("OutputFunction"),200,wsNames);
 
-    auto outFunc = doFit(normFuncString,200,wsNames);
-    TS_ASSERT_DELTA(outFunc->getParameter(OFFSET_PARAM),1.,0.0001);
-    TS_ASSERT_DELTA(outFunc->getParameter(EXP_PARAM),0.0,0.0001);
-    TS_ASSERT_DELTA(outFunc->getParameter(USER_FUNC+"A1"),2.0,0.0001);
-    TS_ASSERT_DELTA(outFunc->getParameter(USER_FUNC+"A0"),-1.02,0.0001);
+    TS_ASSERT_DIFFERS(normFunc->getParameter(NORM_PARAM),1.);
+    TS_ASSERT_DELTA(normFunc->getParameter(OFFSET_PARAM),1.,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(EXP_PARAM),0.0,0.0001);
+
+    TS_ASSERT_DIFFERS(normFunc->getParameter(USER_FUNC+"A0"),0.0);
+    TS_ASSERT_DELTA(normFunc->getParameter(USER_FUNC+"A1"),1.5,0.0001);
+    
+
+    TS_ASSERT_DIFFERS(normFunc->getParameter(NORM_PARAM2),2.);
+    TS_ASSERT_DELTA(normFunc->getParameter(OFFSET_PARAM2),1.,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(EXP_PARAM2),0.0,0.0001);
+
+    TS_ASSERT_DIFFERS(normFunc->getParameter(USER_FUNC2+"A0"),2.0);
+    TS_ASSERT_DIFFERS(normFunc->getParameter(USER_FUNC2+"A1"),3.0);
     
   }
-  //test for 1D tie
 
-  // test for multi domain
+  void test_2DTie() {
+    genData();
+    std::vector<std::string> wsNames = {"ws1","ws2"};
+    std::string multiFuncString = "composite=MultiDomainFunction,NumDeriv=1;";
+    multiFuncString += "name=LinearBackground, $domains=i,A0=0,A1=1.5;";
+    multiFuncString += "name=LinearBackground, $domains=i,A0=2,A1=3;";
 
- // test for multi domain fix
+    IFunction_sptr multiFunc = FunctionFactory::Instance().createInitialized(multiFuncString);
+    multiFunc->addTies("f0.A1=f1.A1");
+    IAlgorithm_sptr alg = setUpAlg(wsNames,multiFunc);
+    TS_ASSERT(alg->isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg->execute());
+    TS_ASSERT(alg->isExecuted());
+    IFunction_sptr normFunc = doFit(alg->getProperty("OutputFunction"),200,wsNames);
 
- // test for multi domain tie
+    TS_ASSERT_DIFFERS(normFunc->getParameter(NORM_PARAM),1.);
+    TS_ASSERT_DELTA(normFunc->getParameter(OFFSET_PARAM),1.,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(EXP_PARAM),0.0,0.0001);
+
+    TS_ASSERT_DIFFERS(normFunc->getParameter(USER_FUNC+"A0"),0.0);
+    TS_ASSERT_EQUALS(normFunc->getParameter(USER_FUNC+"A1"),normFunc->getParameter(USER_FUNC2+"A1"));
+    
+
+    TS_ASSERT_DIFFERS(normFunc->getParameter(NORM_PARAM2),2.);
+    TS_ASSERT_DELTA(normFunc->getParameter(OFFSET_PARAM2),1.,0.0001);
+    TS_ASSERT_DELTA(normFunc->getParameter(EXP_PARAM2),0.0,0.0001);
+
+    TS_ASSERT_DIFFERS(normFunc->getParameter(USER_FUNC2+"A0"),2.0);
+    
+  }
+
 
 
 private:
