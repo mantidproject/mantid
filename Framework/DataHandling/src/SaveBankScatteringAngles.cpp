1d6283dea99f876ef9ee74636e840bc3dd8feb9b
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-05-24 12:08:32 +0100

    Re #22468 Implement algorithm to save grouping.new file

---
+++ Framework/DataHandling/src/SaveBankScatteringAngles.cpp
@@ -0,0 +1,126 @@
+#include "MantidDataHandling/SaveBankScatteringAngles.h"
+
+#include "MantidAPI/FileProperty.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/WorkspaceGroup.h"
+#include "MantidAPI/WorkspaceProperty.h"
+#include "MantidGeometry/Instrument.h"
+#include "MantidKernel/make_unique.h"
+
+#include <cmath>
+#include <fstream>
+
+namespace {
+
+double radToDeg(const double rad) {
+  const static double PI = 4 * atan(1.0);
+  const static double radToDegFactor = 180 / PI;
+  return rad * radToDegFactor;
+}
+} // anonymous namespace
+
+namespace Mantid {
+namespace DataHandling {
+
+using namespace API;
+
+DECLARE_ALGORITHM(SaveBankScatteringAngles)
+
+const std::string SaveBankScatteringAngles::name() const {
+  return "SaveBankScatteringAngles";
+}
+
+const std::string SaveBankScatteringAngles::summary() const {
+  return "Save scattering angles two-theta and phi for each workspace in a "
+         "GroupWorkspace of focused banks";
+}
+
+int SaveBankScatteringAngles::version() const { return 1; }
+
+const std::vector<std::string> SaveBankScatteringAngles::seeAlso() const {
+  return {"SaveFocusedXYE"};
+}
+
+const std::string SaveBankScatteringAngles::category() const {
+  return "DataHandling\\Text;Diffraction\\DataHandling";
+}
+
+const std::string SaveBankScatteringAngles::PROP_FILENAME = "Filename";
+
+const std::string SaveBankScatteringAngles::PROP_INPUT_WS = "InputWorkspace";
+
+void SaveBankScatteringAngles::init() {
+  declareProperty(Kernel::make_unique<WorkspaceProperty<WorkspaceGroup>>(
+                      PROP_INPUT_WS, "", Kernel::Direction::Input),
+                  "A GroupWorkspace where every sub-workspace is a "
+                  "single-spectra focused run corresponding to a particular "
+                  "bank");
+
+  const static std::vector<std::string> exts{".txt", ".new"};
+  declareProperty(Kernel::make_unique<FileProperty>(PROP_FILENAME, "",
+                                                    FileProperty::Save, exts),
+                  "The name of the file to save to");
+}
+
+void SaveBankScatteringAngles::exec() {
+  const std::string filename = getProperty(PROP_FILENAME);
+  std::ofstream outFile(filename.c_str());
+
+  if (!outFile) {
+    throw Kernel::Exception::FileError("Unable to create file: ", filename);
+  }
+
+  outFile << std::fixed << std::setprecision(10);
+
+  const API::WorkspaceGroup_sptr inputWS = getProperty(PROP_INPUT_WS);
+  //  for (const auto &ws : *inputWS) {
+  for (int i = 0; i < inputWS->getNumberOfEntries(); ++i) {
+    const auto ws = inputWS->getItem(i);
+    const auto matrixWS = boost::dynamic_pointer_cast<MatrixWorkspace>(ws);
+    const auto &instrument = matrixWS->getInstrument();
+    const auto &samplePosition = instrument->getSample()->getPos();
+    const auto &sourcePosition = instrument->getSource()->getPos();
+    const auto &detector = matrixWS->getDetector(0);
+    const auto &detectorPosition = detector->getPos();
+
+    const auto beamVector = samplePosition - sourcePosition;
+    const auto detectorVector = detectorPosition - samplePosition;
+
+    const auto twoTheta = radToDeg(beamVector.angle(detectorVector));
+    const auto phi = radToDeg(detector->getPhi());
+    const auto group = matrixWS->getSpectrum(0).getSpectrumNo();
+
+    outFile << "bank :    " << i << "  "
+            << "group:     " << group << "    " << twoTheta << "    " << phi
+            << '\n';
+  }
+
+  outFile.close();
+}
+
+std::map<std::string, std::string> SaveBankScatteringAngles::validateInputs() {
+  std::map<std::string, std::string> issues;
+
+  const API::WorkspaceGroup_sptr inputWS = getProperty(PROP_INPUT_WS);
+  for (const auto &ws : *inputWS) {
+    const auto matrixWS = boost::dynamic_pointer_cast<MatrixWorkspace>(ws);
+    if (matrixWS) {
+      if (matrixWS->getNumberHistograms() != 1) {
+        issues[PROP_INPUT_WS] = "The workspace " + matrixWS->getName() +
+                                " has the wrong number of histograms. It "
+                                "should contain data for a single focused "
+                                "spectra";
+        return issues;
+      }
+    } else {
+      issues[PROP_INPUT_WS] =
+          "The workspace " + ws->getName() +
+          " is of the wrong type. It should be a MatrixWorkspace";
+      return issues;
+    }
+  }
+  return issues;
+}
+
+} // namespace DataHandling
+} // namespace Mantid
---
+++ docs/source/algorithms/SaveBankScatteringAngles-v1.rst
@@ -0,0 +1,58 @@
+.. algorithm::
+
+.. summary::
+
+.. relatedalgorithm::
+
+.. properties::
+
+Description
+-----------
+
+Takes a WorkspaceGroup whose children are single-spectra workspaces
+corresponding to focused banks, and saves their scattering angles
+**two-theta** and **phi** (sometimes called **eta**) to a 4-column
+format readable by MAUD.
+
+The columns are, in order, **Bank number**, **Group number**,
+**two-theta**, and **phi**. An example file looks something like this::
+
+  bank :    1  group:     4    9.12160000026    0.0
+  bank :    2  group:     5    8.15584000023    30.0000000009
+  bank :    3  group:     6    8.03516799229    150.000000004
+  bank :    4  group:     7    9.0611418429    180.000000005
+  ... (and so on)
+
+
+Usage
+-----
+
+.. testcode:: SaveBankScatteringAngles
+
+   import os
+  
+   input_ws = Load("ENGINX_277208_focused_bank_2.nxs")
+   group = GroupWorkspaces(InputWorkspaces=[input_ws])
+
+   output_file = os.path.join(config["defaultsave.directory"], "grouping.new")
+
+   SaveBankScatteringAngles(InputWorkspace=group, Filename=output_file)
+
+   with open(output_file) as f:
+       contents = f.read().split("\n")
+
+   print("File contents (there is only one line as we only bothered"
+	 " saving one spectra):")
+   print(contents[0])
+
+.. testcleanup:: SaveBankScatteringAngles
+
+   os.remove(output_file)
+			
+Output:
+       
+.. testoutput:: SaveBankScatteringAngles
+
+   File contents (there is only one line as we only bothered saving one spectra):
+   bank :    0  group:     1201    89.9396035211    180.0000000000
+		
