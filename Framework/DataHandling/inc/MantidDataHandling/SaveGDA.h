ad10698344d0425a752ba785245a80e609738e0c
Joseph Ramsay <joseph.ramsay@stfc.ac.uk>
2018-05-25 16:11:47 +0100

    Re #22468 Implement SaveGDA

---
+++ Framework/DataHandling/inc/MantidDataHandling/SaveGDA.h
@@ -0,0 +1,38 @@
+#ifndef MANTID_DATAHANDLING_SAVEGDA_H_
+#define MANTID_DATAHANDLING_SAVEGDA_H_
+
+#include "MantidAPI/Algorithm.h"
+#include "MantidDataHandling/DllConfig.h"
+
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+
+namespace Mantid {
+namespace DataHandling {
+
+class MANTID_DATAHANDLING_DLL SaveGDA : public API::Algorithm {
+public:
+  const std::string name() const override;
+
+  const std::string summary() const override;
+
+  int version() const override;
+
+  const std::vector<std::string> seeAlso() const override;
+
+  const std::string category() const override;
+
+private:
+  const static std::string PROP_FILENAME;
+  const static std::string PROP_INPUT_WS;
+
+  void init() override;
+
+  void exec() override;
+
+  std::map<std::string, std::string> validateInputs() override;
+};
+
+} // DataHandling
+} // Mantid
+
+#endif // MANTID_DATAHANDLING_SAVEGDA_H_
