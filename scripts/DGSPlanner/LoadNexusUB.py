a9b465f39a299882ddb666f636b1472bcd22b8c9
Andrei Savici <saviciat@ornl.gov>
2018-06-05 16:26:19 -0400

    Update LoadNexusUB.py

--- scripts/DGSPlanner/LoadNexusUB.py
+++ scripts/DGSPlanner/LoadNexusUB.py
@@ -9,6 +9,6 @@ def LoadNexusUB(fname):
     run = __raw.getRun()
     run_keys = run.keys()
     UBMatrix_key = [test_key for test_key in run_keys if 'UBMatrix' in test_key][0]
-    UB=np.array( eval(raw.run()[UBMatrix_key].value[0].strip(')').split('(')[1]))
+    UB=np.array( eval(run[UBMatrix_key].value[0].strip(')').split('(')[1]))
     DeleteWorkspace(__raw)
     return UB
