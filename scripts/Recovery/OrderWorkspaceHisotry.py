dae3748f73dec3223c5c70ee0f687c64b3cad516
Keith Butler <keith.butler@stfc.ac.uk>
2018-06-06 09:35:31 +0100

    Re #0 No longer need to convert dates

--- scripts/Recovery/OrderWorkspaceHisotry.py
+++ scripts/Recovery/OrderWorkspaceHisotry.py
@@ -4,22 +4,6 @@ from os import listdir
 from os.path import isfile, join
 import datetime
 
-# Because Mantid has an awkward date format
-# we need to convert string to number for sorting later
-months = {
-    'Jan': '01',
-    'Feb': '02',
-    'Mar': '03',
-    'Apr': '04',
-    'May': '05',
-    'Jun': '06',
-    'Jul': '07',
-    'Aug': '08',
-    'Sep': '09',
-    'Oct': '10',
-    'Nov': '11',
-    'Dec': '12'
-    }
 
 def _has_duplicates(values):
     '''
