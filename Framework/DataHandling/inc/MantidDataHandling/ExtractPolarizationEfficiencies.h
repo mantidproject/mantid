9b89514fa626b8665aa9a39e3296068fd0a366f6
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-06-07 11:30:31 +0100

    Update the documentation. Re #2250.

--- Framework/DataHandling/inc/MantidDataHandling/ExtractPolarizationEfficiencies.h
+++ Framework/DataHandling/inc/MantidDataHandling/ExtractPolarizationEfficiencies.h
@@ -36,7 +36,7 @@ public:
   const std::string name() const override;
   int version() const override;
   const std::vector<std::string> seeAlso() const override {
-    return {"PolarizationEfficienciCor"};
+    return {"PolarizationEfficiencyCor"};
   }
   const std::string category() const override;
   const std::string summary() const override;
