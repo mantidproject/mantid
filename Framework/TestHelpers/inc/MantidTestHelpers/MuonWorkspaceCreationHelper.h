99f69705c495afc7f692cd6dc5f449110f6b12d0
T Jubb <t.w.jubb@gmail.com>
2018-06-13 09:45:15 +0100

    Refs #22506 add test helper for muon workspaces.

---
+++ Framework/TestHelpers/inc/MantidTestHelpers/MuonWorkspaceCreationHelper.h
@@ -0,0 +1,106 @@
+#ifndef MUONWORKSPACECREATIONHELPER_H_
+#define MUONWORKSPACECREATIONHELPER_H_
+
+#include "MantidKernel/PhysicalConstants.h"
+
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidAPI/GroupingLoader.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/Workspace.h"
+#include "MantidAPI/WorkspaceGroup.h"
+#include "MantidDataObjects/TableWorkspace.h"
+#include "MantidKernel/PhysicalConstants.h"
+#include "MantidTestHelpers/ComponentCreationHelper.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+
+namespace MuonWorkspaceCreationHelper {
+
+/**
+ * Create a fake muon dataset, each spectrum is offset by 4 degrees in phase
+ * and has a different normalization.
+ */
+struct yDataAsymmetry {
+  yDataAsymmetry();
+  yDataAsymmetry(const double amp, const double phi);
+  double operator()(const double t, size_t spec);
+
+private:
+  double m_amp;         // Amplitude of the oscillations
+  double m_phi;         // Phase of the sinusoid
+  double m_omega = 5.0; // Frequency of the oscillations
+  double tau = Mantid::PhysicalConstants::MuonLifetime *
+               1e6; // Muon life time in microseconds
+};
+
+// Generate y-values which increment by 1 each time the function is called
+struct yDataCounts2 {
+  yDataCounts2();
+  double operator()(const double, size_t);
+
+private:
+  int m_count;
+};
+
+// Errors
+struct eData {
+  double operator()(const double, size_t);
+};
+
+/**
+ * Create a matrix workspace appropriate for Group Asymmetry. One detector per
+ * spectra, numbers starting from 1. The detector ID and spectrum number are
+ * equal.
+ * @param nspec :: The number of spectra
+ * @param maxt ::  The number of histogram bin edges (between 0.0 and 1.0).
+ * Number of bins = maxt - 1 .
+ * @return Pointer to the workspace.
+ */
+template <typename Function>
+Mantid::API::MatrixWorkspace_sptr
+createAsymmetryWorkspace(size_t nspec, size_t maxt, Function dataGenerator);
+
+Mantid::API::MatrixWorkspace_sptr createAsymmetryWorkspace(size_t nspec,
+                                                           size_t maxt);
+
+/**
+ * Create a matrix workspace appropriate for Group Counts. One detector per
+ * spectra, numbers starting from 1. The detector ID and spectrum number are
+ * equal. Y values increase from 0 in integer steps.
+ * @param nspec :: The number of spectra
+ * @param maxt ::  The number of histogram bin edges (between 0.0 and 1.0).
+ * Number of bins = maxt - 1 .
+ * @param seed :: Number added to all y-values.
+ * @return Pointer to the workspace.
+ */
+Mantid::API::MatrixWorkspace_sptr
+createCountsWorkspace(size_t nspec, size_t maxt, double seed,
+                      size_t detectorIDseed = 1);
+
+/**
+ * Create a WorkspaceGroup and add to the ADS, populate with MatrixWorkspaces
+ * simulating periods as used in muon analysis. Workspace for period i has a
+ * name ending _i.
+ * @param nPeriods :: The number of periods (independent workspaces)
+ * @param maxt ::  The number of histogram bin edges (between 0.0 and 1.0).
+ * Number of bins = maxt - 1 .
+ * @param wsGroupName :: Name of the workspace group containing the period
+ * workspaces.
+ * @return Pointer to the workspace group.
+ */
+Mantid::API::WorkspaceGroup_sptr
+createMultiPeriodWorkspaceGroup(const int &nPeriods, size_t nspec, size_t maxt,
+                                const std::string &wsGroupName);
+
+/**
+* Create a simple dead time TableWorkspace with two columns (spectrum number
+* and dead time).
+* @param nspec :: The number of spectra (rows in table).
+* @param deadTimes ::  The dead times for each spectra.
+* @return TableWorkspace with dead times appropriate for pairing algorithm.
+*/
+Mantid::API::ITableWorkspace_sptr createDeadTimeTable(const size_t &nspec,
+	std::vector<double> &deadTimes);
+
+} // namespace MuonWorkspaceCreationHelper
+
+#endif /*MUONWORKSPACECREATIONHELPER_H_*/
---
+++ Framework/TestHelpers/src/MuonWorkspaceCreationHelper.cpp
@@ -0,0 +1,206 @@
+#include "MantidTestHelpers/MuonWorkspaceCreationHelper.h"
+#include "MantidTestHelpers/ComponentCreationHelper.h"
+#include "MantidTestHelpers/InstrumentCreationHelper.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidAPI/GroupingLoader.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/ScopedWorkspace.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidAPI/Workspace.h"
+#include "MantidAPI/WorkspaceGroup.h"
+#include "MantidAlgorithms/CompareWorkspaces.h"
+#include "MantidDataHandling/LoadMuonNexus2.h"
+#include "MantidDataObjects/TableWorkspace.h"
+#include "MantidKernel/PhysicalConstants.h"
+#include "MantidTestHelpers/ScopedFileHelper.h"
+
+using namespace Mantid;
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
+namespace MuonWorkspaceCreationHelper {
+
+// Generate y-values which increment by 1 each time the function is called
+yDataCounts2::yDataCounts2() : m_count(-1) {}
+double yDataCounts2::operator()(const double, size_t) {
+  m_count++;
+  return static_cast<double>(m_count);
+};
+
+/**
+ * Create y-values representing muon data, each spectrum is offset by 4 degrees
+ * in phase and has a different normalization.
+ */
+yDataAsymmetry::yDataAsymmetry(const double amp, const double phi)
+    : m_amp(amp), m_phi(phi){};
+yDataAsymmetry::yDataAsymmetry() {
+  m_amp = 1.5;
+  m_phi = 0.1;
+};
+double yDataAsymmetry::operator()(const double t, size_t spec) {
+  double e = exp(-t / tau);
+  double factor = (static_cast<double>(spec) + 1.0) * 0.5;
+  double phase_offset = 4 * M_PI / 180;
+  return (10. * factor *
+          (1.0 + m_amp * cos(m_omega * t + m_phi + spec * phase_offset)) * e);
+}
+
+// Errors are fixed to 0.005
+double eData::operator()(const double, size_t) { return 0.005; }
+
+/**
+ * Create a matrix workspace appropriate for Group Asymmetry. One detector per
+ * spectra, numbers starting from 1. The detector ID and spectrum number are
+ * equal.
+ * @param nspec :: The number of spectra
+ * @param maxt ::  The number of histogram bin edges (between 0.0 and 1.0).
+ * Number of bins = maxt - 1 .
+ * @param dataGenerator :: A function taking double, size_t arguments and
+ * returning a double
+ * @return Pointer to the workspace.
+ */
+template <typename Function>
+MatrixWorkspace_sptr createAsymmetryWorkspace(std::size_t nspec,
+                                              std::size_t maxt,
+                                              Function dataGenerator) {
+
+  MatrixWorkspace_sptr ws =
+      WorkspaceCreationHelper::create2DWorkspaceFromFunction(
+          dataGenerator, static_cast<int>(nspec), 0.0, 1.0,
+          (1.0 / static_cast<double>(maxt)), true, eData());
+
+  ws->setInstrument(ComponentCreationHelper::createTestInstrumentCylindrical(
+      static_cast<int>(nspec)));
+
+  for (auto g = 0u; g < nspec; ++g) {
+    auto &spec = ws->getSpectrum(g);
+    spec.addDetectorID(g + 1);
+    spec.setSpectrumNo(g + 1);
+  }
+
+  // Add number of good frames (required for Asymmetry calculation)
+  ws->mutableRun().addProperty("goodfrm", 10);
+
+  boost::shared_ptr<Geometry::Instrument> inst1 =
+      boost::make_shared<Geometry::Instrument>();
+  inst1->setName("EMU");
+  ws->setInstrument(inst1);
+  ws->mutableRun().addProperty("run_number", 12345);
+
+  return ws;
+}
+
+Mantid::API::MatrixWorkspace_sptr createAsymmetryWorkspace(size_t nspec,
+                                                           size_t maxt) {
+  return createAsymmetryWorkspace(nspec, maxt, yDataAsymmetry());
+}
+
+/**
+* Create a matrix workspace appropriate for Group Counts. One detector per
+* spectra, numbers starting from 1. The detector ID and spectrum number are
+* equal. Y values increase from 0 in integer steps.
+* @param nspec :: The number of spectra
+* @param maxt ::  The number of histogram bin edges (between 0.0 and 1.0).
+* Number of bins = maxt - 1 .
+* @param seed :: Number added to all y-values.
+* @return Pointer to the workspace.
+*/
+MatrixWorkspace_sptr createCountsWorkspace(size_t nspec, size_t maxt,
+	double seed,
+	size_t detectorIDseed) {
+
+	MatrixWorkspace_sptr ws =
+		WorkspaceCreationHelper::create2DWorkspaceFromFunction(
+			yDataCounts2(), static_cast<int>(nspec), 0.0, 1.0,
+			(1.0 / static_cast<double>(maxt)), true, eData());
+
+	ws->setInstrument(ComponentCreationHelper::createTestInstrumentCylindrical(
+		static_cast<int>(nspec)));
+
+	for (int g = 0; g < static_cast<int>(nspec); g++) {
+		auto &spec = ws->getSpectrum(g);
+		spec.addDetectorID(g + 1);
+		spec.setSpectrumNo(g + 1);
+		ws->mutableY(g) += seed;
+	}
+
+	boost::shared_ptr<Geometry::Instrument> inst1 =
+		boost::make_shared<Geometry::Instrument>();
+	inst1->setName("EMU");
+	ws->setInstrument(inst1);
+	ws->mutableRun().addProperty("run_number", 12345);
+	ws->mutableRun().addProperty("goodfrm", 10);
+
+	return ws;
+}
+
+/**
+* Create a WorkspaceGroup and add to the ADS, populate with MatrixWorkspaces
+* simulating periods as used in muon analysis. Workspace for period i has a
+* name ending _i.
+* @param nPeriods :: The number of periods (independent workspaces)
+* @param maxt ::  The number of histogram bin edges (between 0.0 and 1.0).
+* Number of bins = maxt - 1 .
+* @param wsGroupName :: Name of the workspace group containing the period
+* workspaces.
+* @return Pointer to the workspace group.
+*/
+WorkspaceGroup_sptr
+createMultiPeriodWorkspaceGroup(const int &nPeriods, size_t nspec, size_t maxt,
+	const std::string &wsGroupName) {
+
+	WorkspaceGroup_sptr wsGroup = boost::make_shared<WorkspaceGroup>();
+	AnalysisDataService::Instance().addOrReplace(wsGroupName, wsGroup);
+
+	std::string wsNameStem = "MuonDataPeriod_";
+	std::string wsName;
+
+	boost::shared_ptr<Geometry::Instrument> inst1 =
+		boost::make_shared<Geometry::Instrument>();
+	inst1->setName("EMU");
+
+	for (int period = 1; period < nPeriods + 1; period++) {
+		// Period 1 yvalues : 1,2,3,4,5,6,7,8,9,10
+		// Period 2 yvalues : 2,3,4,5,6,7,8,9,10,11 etc..
+		MatrixWorkspace_sptr ws = createCountsWorkspace(nspec, maxt, period);
+
+		wsGroup->addWorkspace(ws);
+		wsName = wsNameStem + std::to_string(period);
+		AnalysisDataService::Instance().addOrReplace(wsName, ws);
+	}
+
+	return wsGroup;
+}
+
+/**
+* Create a simple dead time TableWorkspace with two columns (spectrum number
+* and dead time).
+* @param nspec :: The number of spectra (rows in table).
+* @param deadTimes ::  The dead times for each spectra.
+* @return TableWorkspace with dead times appropriate for pairing algorithm.
+*/
+ITableWorkspace_sptr createDeadTimeTable(const size_t &nspec,
+	std::vector<double> &deadTimes) {
+
+	auto deadTimeTable = boost::dynamic_pointer_cast<ITableWorkspace>(
+		WorkspaceFactory::Instance().createTable("TableWorkspace"));
+
+	deadTimeTable->addColumn("int", "Spectrum Number");
+	deadTimeTable->addColumn("double", "Dead Time");
+
+	if (deadTimes.size() != nspec) {
+		return deadTimeTable;
+	}
+
+	for (size_t spec = 0; spec < deadTimes.size(); spec++) {
+		TableRow newRow = deadTimeTable->appendRow();
+		newRow << static_cast<int>(spec) + 1;
+		newRow << deadTimes[spec];
+	}
+
+	return deadTimeTable;
+}
+
+} // namespace MuonWorkspaceCreationHelper
