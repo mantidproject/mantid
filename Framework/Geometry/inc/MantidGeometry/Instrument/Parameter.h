6885ddeca2c664eb29b05adb1c617b78b8dcdac7
Gemma Guest <gemma.guest@stfc.ac.uk>
2018-06-13 12:24:47 +0100

    Revert "Re #21749: Use maximum precision when converting to string."
    This reverts commit bd6530b5d5e029a91df1273bfcc36aa129e9d3f8.
    
    I'm reverting the change to precision in Parameter.h for now because this is causing test failures which are out of scope for this PR.
    
    Re #21749

--- Framework/Geometry/inc/MantidGeometry/Instrument/Parameter.h
+++ Framework/Geometry/inc/MantidGeometry/Instrument/Parameter.h
@@ -10,7 +10,6 @@
 #include <typeinfo>
 #include <vector>
 #include <stdexcept>
-#include <iomanip>
 
 namespace Mantid {
 
@@ -186,7 +185,6 @@ template <class T> void Parameter::set(const T &t) {
  */
 template <class Type> std::string ParameterType<Type>::asString() const {
   std::ostringstream str;
-  str << std::setprecision(std::numeric_limits<Type>::digits10);
   str << m_value;
   return str.str();
 }
