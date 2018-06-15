8f5fae5db258e136ff21037fc33263d1688c6a6d
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-06-15 11:42:46 +0100

    tidy up I(Q,t) calculation re #22582

--- Framework/Algorithms/src/CalculateIqt.cpp
+++ Framework/Algorithms/src/CalculateIqt.cpp
@@ -1,5 +1,6 @@
 #include "MantidAlgorithms/CalculateIqt.h"
 #include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/MatrixWorkspace.h"
 #include "MantidKernel/BoundedValidator.h"
 
 using namespace Mantid::API;
@@ -9,7 +10,7 @@ namespace {
   constexpr int DEFAULT_ITERATIONS = 10;
   constexpr int DEFAULT_SEED = 23021997;
 
-  MatrixWorkspace_sptr rebin(MatrixWorkspace_sptr workspace, std::string params) {
+  MatrixWorkspace_sptr rebin(MatrixWorkspace_sptr workspace, const std::string &params) {
     IAlgorithm_sptr rebinAlgorithm = AlgorithmManager::Instance().create("Rebin");
     rebinAlgorithm->setChild(true);
     rebinAlgorithm->initialize();
@@ -20,12 +21,12 @@ namespace {
   }
 
   MatrixWorkspace_sptr integration(MatrixWorkspace_sptr workspace) {
-    IAlgorithm_sptr rebinAlgorithm = AlgorithmManager::Instance().create("Integration");
-    rebinAlgorithm->setChild(true);
-    rebinAlgorithm->initialize();
-    rebinAlgorithm->setProperty("InputWorkspace", workspace);
-    rebinAlgorithm->execute();
-    return rebinAlgorithm->getProperty("OutputWorkspace");
+    IAlgorithm_sptr integrationAlgorithm = AlgorithmManager::Instance().create("Integration");
+    integrationAlgorithm->setChild(true);
+    integrationAlgorithm->initialize();
+    integrationAlgorithm->setProperty("InputWorkspace", workspace);
+    integrationAlgorithm->execute();
+    return integrationAlgorithm->getProperty("OutputWorkspace");
   }
 
   MatrixWorkspace_sptr convertToPointData(MatrixWorkspace_sptr workspace) {
@@ -55,6 +56,34 @@ namespace {
     rebinAlgorithm->setProperty("RHSWorkspace", rhsWorkspace);
     rebinAlgorithm->execute();
     return rebinAlgorithm->getProperty("OutputWorkspace");
+  }
+
+  std::string createRebinString(double minimum, double maximum, double width) {
+    std::stringstream rebinStream;
+    rebinStream.precision(14);
+    rebinStream << minimum << ", " << width << ", " << maximum;
+    return rebinStream.str();
+  }
+
+  struct NormalisedFourierTransform {
+  public:
+    explicit NormalisedFourierTransform(const std::string &rebinParams) : m_rebinParams(rebinParams) {}
+
+    MatrixWorkspace_sptr operator()(MatrixWorkspace_sptr workspace) const {
+      workspace = rebin(workspace, m_rebinParams);
+      auto workspace_int = integration(workspace);
+      workspace = convertToPointData(workspace);
+      workspace = extractFFTSpectrum(workspace);
+      return divide(workspace, workspace_int);
+    }
+
+  private:
+    const std::string m_rebinParams;
+  };
+
+  MatrixWorkspace_sptr calculateIqt(MatrixWorkspace_sptr sample, MatrixWorkspace_sptr resolution,
+    const NormalisedFourierTransform &fourierTransform) {
+    return divide(fourierTransform(sample), resolution);
   }
 }
 
@@ -97,37 +126,18 @@ void CalculateIqt::init() {
 }
 
 void CalculateIqt::exec() {
-  auto rebinString = rebinParamsAsString();
-  const MatrixWorkspace_sptr sampleWorkspace = getProperty("InputWorkspace");
-  const MatrixWorkspace_sptr inputResolutionWorkspace = getProperty("ResolutionWorkspace");
-  const auto resolutionWorkspace = calculateIntermediateWorkspace(sampleWorkspace, rebinString);
-  auto outputWorkspace = calculateIqt(sampleWorkspace, resolutionWorkspace, rebinString);
+  NormalisedFourierTransform fourierTransform(rebinParamsAsString());
+  MatrixWorkspace_sptr sample = getProperty("InputWorkspace");
+  MatrixWorkspace_sptr resolution = getProperty("ResolutionWorkspace");
+  resolution = fourierTransform(resolution);
+  calculateIqt(sample, resolution, fourierTransform);
 }
 
 std::string CalculateIqt::rebinParamsAsString() {
   const double e_min = getProperty("EnergyMin");
   const double e_max = getProperty("EnergyMax");
   const double e_width = getProperty("EnergyWidth");
-
-  std::stringstream rebinStream;
-  rebinStream.precision(14);
-  rebinStream << e_min << ", " << e_width << ", " << e_max;
-  std::string rebinString = rebinStream.str();
-}
-
-MatrixWorkspace_sptr CalculateIqt::calculateIqt(MatrixWorkspace_sptr workspace,
-  MatrixWorkspace_sptr resolutionWorkspace, std::string rebinParams) {
-  workspace = calculateIntermediateWorkspace(workspace, rebinParams);
-  return divide(workspace, resolutionWorkspace);
-}
-
-MatrixWorkspace_sptr CalculateIqt::calculateIntermediateWorkspace(MatrixWorkspace_sptr workspace, std::string rebinParams) {
-  workspace = rebin(workspace, rebinParams);
-  auto workspace_int = integration(workspace);
-  workspace = convertToPointData(workspace);
-  workspace = extractFFTSpectrum(workspace);
-  workspace = divide(workspace, workspace_int);
-  return workspace;
+  return createRebinString(e_min, e_max, e_width);
 }
 
 std::map<std::string, std::string> CalculateIqt::validateInputs() {
