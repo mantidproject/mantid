d566e8aadf2829d83119dc91cc5137da5b326a44
Karl Palmen <karl.palmen@stfc.ac.uk>
2018-06-20 16:12:09 +0100

    Add tests to MaxentTransformMultiFourierTest re #21577
    Last of these tests shows an error in the application of
    the linear adjustments, which will be fixed.
    
    Signed-off-by: Karl Palmen <karl.palmen@stfc.ac.uk>

--- Framework/Algorithms/test/MaxEnt/MaxentTransformMultiFourierTest.h
+++ Framework/Algorithms/test/MaxEnt/MaxentTransformMultiFourierTest.h
@@ -144,6 +144,81 @@ public:
 
   }
 
+  void test_image_to_data_repeats_fourier()
+  {
+    MaxentSpace_sptr dataSpaceMF = std::make_shared<MaxentSpaceComplex>();
+    MaxentSpace_sptr imageSpaceMF = std::make_shared<MaxentSpaceComplex>();
+    MaxentTransformMultiFourier transformMF(dataSpaceMF, imageSpaceMF, 3);
+    MaxentSpace_sptr dataSpaceF = std::make_shared<MaxentSpaceComplex>();
+    MaxentSpace_sptr imageSpaceF = std::make_shared<MaxentSpaceComplex>();
+    MaxentTransformFourier transformF(dataSpaceF, imageSpaceF);
+
+    std::vector<double> complexImage = {
+      4.0, 3.0, 2.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
+
+    // Perform the transformation
+    auto result = transformMF.imageToData(complexImage);
+    // Perform Fourier transformation for comparison
+    auto resultF = transformF.imageToData(complexImage);
+
+    // Check that result is a triple repetition of fourier
+    TS_ASSERT_EQUALS(result.size(), 3*resultF.size());
+    if (result.size() == 3*resultF.size()) {
+      size_t n = resultF.size();
+      for (size_t i = 0; i < n; i++) {
+        TS_ASSERT_DELTA(result[i], resultF[i], 1e-4);
+        TS_ASSERT_DELTA(result[i + n], resultF[i], 1e-4);
+        TS_ASSERT_DELTA(result[i + 2*n], resultF[i], 1e-4);
+      }
+    }
+
+  }
+
+  void test_image_to_data_with_real_adjustments()
+  {
+    MaxentSpace_sptr dataSpaceMF = std::make_shared<MaxentSpaceComplex>();
+    MaxentSpace_sptr imageSpaceMF = std::make_shared<MaxentSpaceComplex>();
+    MaxentTransformMultiFourier transformMF(dataSpaceMF, imageSpaceMF, 3);
+    MaxentSpace_sptr dataSpaceF = std::make_shared<MaxentSpaceComplex>();
+    MaxentSpace_sptr imageSpaceF = std::make_shared<MaxentSpaceComplex>();
+    MaxentTransformFourier transformF(dataSpaceF, imageSpaceF);
+
+    std::vector<double> complexImage = {
+      4.0, 3.0, 2.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
+    std::vector<double> linearAdjustments = {
+      2.1, 0.0, 3.1, 0.0, 4.1, 0.0, 5.1, 0.0,
+      5.1, 0.0, 4.1, 0.0, 3.1, 0.0, 2.1, 0.0,
+      -3.1, 0.0, -3.1, 0.0, -3.1, 0.0, -3.1, 0.0};
+    std::vector<double> constAdjustments = {
+      1.0, 0.0, 1.0, 0.0, -1.0, 0.0, -1.0, 0.0,
+      -1.0, 0.0, -1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+      -2.0, 0.0, -1.0, 0.0, 1.0, 0.0, 2.0, 0.0 };
+
+    // Set Adjustments
+    transformMF.setAdjustments(linearAdjustments, constAdjustments);
+
+    // Perform the transformation
+    auto result = transformMF.imageToData(complexImage);
+    // Perform Fourier transformation for comparison
+    auto resultF = transformF.imageToData(complexImage);
+
+    // Check that result is a triple repetition of fourier
+    // but with the adjustments applied
+    TS_ASSERT_EQUALS(result.size(), 3 * resultF.size());
+    TS_ASSERT_EQUALS(result.size(), linearAdjustments.size());
+    TS_ASSERT_EQUALS(result.size(), constAdjustments.size());
+
+    if (result.size() == 3 * resultF.size()) {
+      size_t n = resultF.size();
+      for (size_t i = 0; i < n; i++) {
+        TS_ASSERT_DELTA(result[i], linearAdjustments[i-i%2]*resultF[i] + constAdjustments[i], 1e-4);
+        TS_ASSERT_DELTA(result[i + n], linearAdjustments[i + n - i % 2]*resultF[i] + constAdjustments[i + n], 1e-4);
+        TS_ASSERT_DELTA(result[i + 2 * n], linearAdjustments[i + 2*n - i % 2]*resultF[i] + constAdjustments[i + 2*n], 1e-4);
+      }
+    }
+
+  }
+
 };
 
 #endif /* MANTID_ALGORITHMS_MAXENTTRANSFORMMULTIFOURIERTEST_H_ */
