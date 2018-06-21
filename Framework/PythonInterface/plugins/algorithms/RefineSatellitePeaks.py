9ec98457db1223c3da7a29fc84fc2a91326aaf21
Samuel Jackson <samueljackson@outlook.com>
2018-06-21 16:52:38 +0100

    Refs #20883 Fix flake8 warnings

--- Framework/PythonInterface/plugins/algorithms/RefineSatellitePeaks.py
+++ Framework/PythonInterface/plugins/algorithms/RefineSatellitePeaks.py
@@ -16,13 +16,13 @@ class RefineSatellitePeaks(DataProcessorAlgorithm):
 
     def PyInit(self):
         self.declareProperty(PeaksWorkspaceProperty(name="NuclearPeaks",
-                                                     defaultValue="",
-                                                     direction=Direction.Input),
+                                                    defaultValue="",
+                                                    direction=Direction.Input),
                              doc="Main integer HKL peaks. Q vectors will be calculated relative to these peaks.")
 
         self.declareProperty(PeaksWorkspaceProperty(name="SatellitePeaks",
-                                                     defaultValue="",
-                                                     direction=Direction.Input),
+                                                    defaultValue="",
+                                                    direction=Direction.Input),
                              doc="Positions of seed satellite peaks. These will be used to define the q vectors for each satellite.")
 
         self.declareProperty(WorkspaceProperty(name="MDWorkspace",
@@ -32,8 +32,8 @@ class RefineSatellitePeaks(DataProcessorAlgorithm):
                              doc="MD workspace to search for satellites peak in. This data must be in the HKL frame.")
 
         self.declareProperty(PeaksWorkspaceProperty(name="OutputWorkspace",
-                                                     defaultValue="",
-                                                     direction=Direction.Output),
+                                                    defaultValue="",
+                                                    direction=Direction.Output),
                              doc="All found satellite peaks. These will be given with satellite coordinates.")
 
         self.declareProperty('NumOfQs', -1, direction=Direction.Input,
@@ -77,12 +77,13 @@ class RefineSatellitePeaks(DataProcessorAlgorithm):
         qs = self.average_clusters(qs, clusters)
         predicted_satellites = self.create_fractional_peaks_workspace(qs, nuclear)
 
-        centroid_satellites = CentroidPeaksMD(InputWorkspace=md, PeaksWorkspace=predicted_satellites, PeakRadius=peak_radius, StoreInADS=False)
-        print type(centroid_satellites)
+        centroid_satellites = CentroidPeaksMD(InputWorkspace=md, PeaksWorkspace=predicted_satellites,
+                                              PeakRadius=peak_radius, StoreInADS=False)
         satellites_int_spherical = IntegratePeaksMD(InputWorkspace=md, PeaksWorkspace=centroid_satellites, PeakRadius=peak_radius,
                                                     BackgroundInnerRadius=background_radii[0], BackgroundOuterRadius=background_radii[1],
                                                     IntegrateIfOnEdge=True, StoreInADS=False)
-        satellites_int_spherical = FilterPeaks(satellites_int_spherical, FilterVariable="Intensity", FilterValue=0, Operator=">", StoreInADS=False)
+        satellites_int_spherical = FilterPeaks(satellites_int_spherical, FilterVariable="Intensity",
+                                               FilterValue=0, Operator=">", StoreInADS=False)
         satellites_int_spherical = FilterPeaks(satellites_int_spherical, FilterVariable="Signal/Noise",
                                                FilterValue=I_over_sigma, Operator=">", StoreInADS=False)
 
@@ -168,8 +169,10 @@ class RefineSatellitePeaks(DataProcessorAlgorithm):
             predicted_satellites.removePeak(0)
 
         for q in qs:
-            predicted_q = PredictFractionalPeaks(nuclear, HOffset=q[0], KOffset=q[1], LOffset=q[2], StoreInADS=False, FracPeaks='predicted_q')
-            predicted_satellites = CombinePeaksWorkspaces(predicted_satellites, predicted_q, StoreInADS=False, OutputWorkspace='predicted_satellites')
+            predicted_q = PredictFractionalPeaks(nuclear, HOffset=q[0], KOffset=q[1],
+                                                 LOffset=q[2], StoreInADS=False, FracPeaks='predicted_q')
+            predicted_satellites = CombinePeaksWorkspaces(predicted_satellites, predicted_q,
+                                                          StoreInADS=False, OutputWorkspace='predicted_satellites')
 
         return predicted_satellites
 
