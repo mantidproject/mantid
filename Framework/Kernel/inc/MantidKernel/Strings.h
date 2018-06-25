0e3d89e9f0ad9f40e5944968b1a3c83b2069a993
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2018-06-25 13:26:17 +0100

    Workaround Qt4 moc bug with boost

--- Framework/Kernel/inc/MantidKernel/Strings.h
+++ Framework/Kernel/inc/MantidKernel/Strings.h
@@ -8,7 +8,9 @@
 #include "MantidKernel/StringTokenizer.h"
 #include "MantidKernel/System.h"
 
+#ifndef Q_MOC_RUN
 #include <boost/lexical_cast.hpp>
+#endif
 #include <iosfwd>
 #include <map>
 #include <set>
