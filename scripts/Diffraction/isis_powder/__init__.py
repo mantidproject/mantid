d8f15f18a78c03a1095f1b727144d323e45cbbb8
David Fairbrother <DavidFair@users.noreply.github.com>
2018-06-25 14:25:10 +0100

    Re #22638 Remove chaining for ISIS run details on GEM
    Removes chaining callbacks together to access a dictionary. This is one
    of the things originally added to the scripts which should have been
    removed.
    
    This overly complex piece of code attempted to take the DRY pricnciple
    to an extreme. Instead each instrument should just specify the layout of
    their file

--- scripts/Diffraction/isis_powder/__init__.py
+++ scripts/Diffraction/isis_powder/__init__.py
@@ -3,9 +3,9 @@ from __future__ import (absolute_import, division, print_function)
 # Disable unused import warnings. The import is for user convenience
 # Bring instruments into package namespace
 from .gem import Gem  # noqa: F401
-from .hrpd import HRPD # noqa: F401
-from .pearl import Pearl  # noqa: F401
-from .polaris import Polaris  # noqa: F401
+#from .hrpd import HRPD # noqa: F401
+#from .pearl import Pearl  # noqa: F401
+#from .polaris import Polaris  # noqa: F401
 
 # Other useful classes
 from .routines.sample_details import SampleDetails  # noqa: F401
