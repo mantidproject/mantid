e53714fe6253702d099ecd42daf29882a71e01f0
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-06-26 13:08:57 +0100

    refs #22399 fixed flake 8 for mock_widget in muon

--- scripts/Muon/GUI/Common/mock_widget.py
+++ scripts/Muon/GUI/Common/mock_widget.py
@@ -5,7 +5,6 @@ import PyQt4.QtGui as QtGui
 def mockQapp():
     qapp = QtGui.QApplication.instance()
     if qapp is None:
-       return  QtGui.QApplication([''])
+        return QtGui.QApplication([''])
     else:
-       return qapp
- 
+        return qapp
