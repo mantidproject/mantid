b5a6472bd6a21402caf7a0acdd156f747eec95ee
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-06-28 16:24:17 +0100

    increase iterations for system tests re #22582

--- Testing/SystemTests/tests/analysis/ISISIndirectInelastic.py
+++ Testing/SystemTests/tests/analysis/ISISIndirectInelastic.py
@@ -757,8 +757,7 @@ class ISISIndirectInelasticIqtAndIqtFit(with_metaclass(ABCMeta, ISISIndirectInel
                                    EnergyMin=self.e_min,
                                    EnergyMax=self.e_max,
                                    BinReductionFactor=self.num_bins,
-                                   DryRun=False,
-                                   NumberOfIterations=50)
+                                   DryRun=False)
 
         # Test IqtFit Sequential
         iqtfitSeq_ws, params, fit_group = IqtFitSequential(InputWorkspace=iqt_ws, Function=self.func,
@@ -876,8 +875,7 @@ class ISISIndirectInelasticIqtAndIqtFitMulti(with_metaclass(ABCMeta, ISISIndirec
                                    EnergyMin=self.e_min,
                                    EnergyMax=self.e_max,
                                    BinReductionFactor=self.num_bins,
-                                   DryRun=False,
-                                   NumberOfIterations=50)
+                                   DryRun=False)
 
         # Test IqtFitMultiple
         iqtfitSeq_ws, params, fit_group = IqtFitMultiple(iqt_ws.name(),
