c0998957b306e6d5b460fcd8184a8c1e098dbcfd
Keith Butler <keith.butler@stfc.ac.uk>
2018-06-28 11:27:27 +0100

    Re #0 add handling for when qtdir is not set

--- scripts/ErrorReporter/error_dialog_app.py
+++ scripts/ErrorReporter/error_dialog_app.py
@@ -18,10 +18,11 @@ from mantid.kernel import UsageService # noqa
 from ErrorReporter.error_report_presenter import ErrorReporterPresenter # noqa
 from ErrorReporter.errorreport import CrashReportPage # noqa
 # Set path to look for package qt libraries
-from PyQt4.QtCore import QCoreApplication
-QCoreApplication.addLibraryPath(
-    command_line_args.qtdir
-)
+if command_line_args.qtdir != None:
+    from PyQt4.QtCore import QCoreApplication
+    QCoreApplication.addLibraryPath(
+        command_line_args.qtdir
+    )
 
 def main():
     if not UsageService.isEnabled():
