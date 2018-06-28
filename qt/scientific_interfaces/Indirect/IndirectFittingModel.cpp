704ae376df07c7e1b48c437db2b6d471243c0afa
Brandon Hewer <brandon.hewer@stfc.ac.uk>
2018-06-28 18:30:55 +0100

    Apply ClangFormat Changes
    Refs #22672

--- qt/scientific_interfaces/Indirect/IndirectFittingModel.cpp
+++ qt/scientific_interfaces/Indirect/IndirectFittingModel.cpp
@@ -109,9 +109,8 @@ bool equivalentFunctions(IFunction_const_sptr func1,
 std::ostringstream &addInputString(IndirectFitData *fitData,
                                    std::ostringstream &stream) {
   const auto &name = fitData->workspace()->getName();
-  auto addToStream = [&](std::size_t spectrum) {
-    stream << name << ",i" << spectrum << ";";
-  };
+  auto addToStream =
+      [&](std::size_t spectrum) { stream << name << ",i" << spectrum << ";"; };
   fitData->applySpectra(addToStream);
   return stream;
 }
@@ -631,7 +630,7 @@ IndirectFittingModel::mapDefaultParameterNames() const {
 }
 
 std::unordered_map<std::string, ParameterValue>
-IndirectFittingModel::createDefaultParameters(std::size_t) const {
+    IndirectFittingModel::createDefaultParameters(std::size_t) const {
   return std::unordered_map<std::string, ParameterValue>();
 }
 
