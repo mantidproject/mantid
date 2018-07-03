c75ebe2ff8715d8b4aef872a7e1206ce7a26caf2
Lamar Moore <lamar.moore@stfc.ac.uk>
2018-07-03 14:40:30 +0100

    Fix cmake error

--- buildconfig/Jenkins/buildscript.bat
+++ buildconfig/Jenkins/buildscript.bat
@@ -30,7 +30,6 @@ set SDK_VERSION=8.1
 call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64 %SDK_VERSION%
 set UseEnv=true
 set CM_GENERATOR=Visual Studio 14 2015 Win64
-:: ParaView version
 set PARAVIEW_DIR=%PARAVIEW_DIR%
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
