df873e2455121ade5465f9701608f59cce2647e8
Bhuvan Bezawada <bhuvan_777@outlook.com>
2018-07-05 13:09:19 +0100

    Cleaned up test file and PropertyFactory.cpp
    re #22554
    Correct TimeSeriesProperty object should now be created

--- Framework/PythonInterface/mantid/kernel/src/Exports/PropertyFactory.cpp
+++ Framework/PythonInterface/mantid/kernel/src/Exports/PropertyFactory.cpp
@@ -1,61 +1,41 @@
-// Import the header for the PropertyWithValueFactory so that we can access 
-// the create() functions 
 #include "MantidPythonInterface/kernel/Registry/PropertyWithValueFactory.h"
-
-//#include "MantidPythonInterface/kernel/Registry/MappingTypeHandler.h"
-#include "MantidPythonInterface/kernel/Registry/TypedPropertyValueHandler.h"
 #include "MantidPythonInterface/kernel/Registry/SequenceTypeHandler.h"
-//#include "MantidPythonInterface/kernel/GetPointer.h"
+#include "MantidPythonInterface/kernel/Registry/TypedPropertyValueHandler.h"
 
-// For exporting to the Python side
 #include <boost/python/class.hpp>
-
+#include <boost/python/manage_new_object.hpp>
 #include <boost/python/register_ptr_to_python.hpp>
 #include <boost/python/return_value_policy.hpp>
-#include <boost/python/manage_new_object.hpp>
 
-// Get the Classes to use
-using Mantid::PythonInterface::Registry::PropertyWithValueFactory;
 using Mantid::Kernel::Property;
-
-// Boost
+using Mantid::PythonInterface::Registry::PropertyWithValueFactory;
 using namespace boost::python;
 
-// Dummy Class
-class DLLExport PropertyFactory {};
+// Empty class definition
+class PropertyFactory {};
 
-//GET_POINTER_SPECIALIZATION(PropertyFactory)
+// Helper namespace
+namespace propertyFactoryHelper {
 
-// Dummy helper namespace
-namespace helper {
-  std::string testFunction() {
-    return "Hello!";
-  }
+// Helper function to remove unique pointer and return a raw pointer
+Property *removeUniquePointer(const std::string &name,
+                              const boost::python::object &defaultValue) {
 
-  Property* removeUniquePointer(const std::string &name, 
-                                const boost::python::object &defaultValue) {
+  // Get the unique pointer from the factory and convert it into a raw pointer
+  std::unique_ptr<Property> ptr =
+      PropertyWithValueFactory::createTimeSeries(name, defaultValue);
 
-    std::unique_ptr<Property> ptr = PropertyWithValueFactory::createTimeSeries(name, defaultValue);
+  Property *raw_ptr = ptr.release();
 
-    Property* raw_ptr = ptr.release();
-
-    return raw_ptr;
-  }
+  return raw_ptr;
 }
+} // namespace propertyFactoryHelper
 
 // Export the PropertyFactory
 void export_PropertyFactory() {
-  
-  /*
-  // Try a dummy export
-  class_<PropertyFactory>("PropertyFactory")
-    .def("testFunction", &helper::testFunction)
-    .staticmethod("testFunction");
-  */
-  //register_ptr_to_python<PropertyFactory *>();
-
   class_<PropertyFactory, boost::noncopyable>("PropertyFactory", no_init)
-    .def("createTimeSeries", &helper::removeUniquePointer,
-      arg("log_name"), arg("log_values"), return_value_policy<manage_new_object>())
-    .staticmethod("createTimeSeries");
+      .def("createTimeSeries", &propertyFactoryHelper::removeUniquePointer,
+           arg("log_name"), arg("log_values"),
+           return_value_policy<manage_new_object>())
+      .staticmethod("createTimeSeries");
 }
