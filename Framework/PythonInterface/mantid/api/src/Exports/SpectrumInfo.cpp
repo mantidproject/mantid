8ff4d1e05bc720155ec211017c2a22d74bfca4c0
Bhuvan Bezawada <bhuvan_777@outlook.com>
2018-07-10 14:38:26 +0100

    Minor fix for exports file
    re #22849

--- Framework/PythonInterface/mantid/api/src/Exports/SpectrumInfo.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/SpectrumInfo.cpp
@@ -10,51 +10,51 @@ void export_SpectrumInfo() {
   // NOT ADD EXPORTS TO OTHER METHODS without contacting the team working on
   // Instrument-2.0.
   class_<SpectrumInfo, boost::noncopyable>("SpectrumInfo", no_init)
-      .def("isMonitor", &SpectrumInfo::isMonitor, (arg("self"), arg("index")),
-           "Returns true if the detector(s) associated with the spectrum are "
-           "monitors.")
+    .def("isMonitor", &SpectrumInfo::isMonitor, (arg("self"), arg("index")),
+      "Returns true if the detector(s) associated with the spectrum are "
+      "monitors.")
 
-      .def("isMasked", &SpectrumInfo::isMasked, (arg("self"), arg("index")),
-           "Returns true if the detector(s) associated with the spectrum are "
-           "masked.")
+    .def("isMasked", &SpectrumInfo::isMasked, (arg("self"), arg("index")),
+      "Returns true if the detector(s) associated with the spectrum are "
+      "masked.")
 
-      .def("setMasked", &SpectrumInfo::setMasked,
-           (arg("self"), arg("index"), arg("masked")),
-           "Set the mask flag of the spectrum with the given index.")
+    .def("setMasked", &SpectrumInfo::setMasked,
+    (arg("self"), arg("index"), arg("masked")),
+      "Set the mask flag of the spectrum with the given index.")
 
-      .def("hasDetectors", &SpectrumInfo::hasDetectors, (arg("self")),
-           "Returns true if the spectrum is associated with detectors in the "
-           "instrument.")
+    .def("hasDetectors", &SpectrumInfo::hasDetectors, (arg("self")),
+      "Returns true if the spectrum is associated with detectors in the "
+      "instrument.")
 
-      .def("twoTheta", &SpectrumInfo::twoTheta, (arg("self"), arg("index")),
-           "Returns the scattering angle 2 theta in radians w.r.t. beam "
-           "direction.")
+    .def("twoTheta", &SpectrumInfo::twoTheta, (arg("self"), arg("index")),
+      "Returns the scattering angle 2 theta in radians w.r.t. beam "
+      "direction.")
 
-      .def("signedTwoTheta", &SpectrumInfo::signedTwoTheta,
-           (arg("self"), arg("index")),
-           "Returns the signed scattering angle 2 theta in radians w.r.t. beam "
-           "direction.")
+    .def("signedTwoTheta", &SpectrumInfo::signedTwoTheta,
+    (arg("self"), arg("index")),
+      "Returns the signed scattering angle 2 theta in radians w.r.t. beam "
+      "direction.")
 
-      .def("size", &SpectrumInfo::size, arg("self"),
-           "Returns the number of spectra.")
+    .def("size", &SpectrumInfo::size, arg("self"),
+      "Returns the number of spectra.")
 
-      .def("hasUniqueDetector", &SpectrumInfo::hasUniqueDetector,
-           (arg("self"), arg("index")),
-           "Returns true if the spectrum is associated with exactly one "
-           "detector.")
+    .def("hasUniqueDetector", &SpectrumInfo::hasUniqueDetector,
+    (arg("self"), arg("index")),
+      "Returns true if the spectrum is associated with exactly one "
+      "detector.")
 
-      .def("l1", &SpectrumInfo::l1, arg("self"),
-           "Returns the distance from the source to the sample.")
+    .def("l1", &SpectrumInfo::l1, arg("self"),
+      "Returns the distance from the source to the sample.")
 
-      .def("l2", &SpectrumInfo::l2, (arg("self"), arg("index")),
-           "Returns the distance from the sample to the spectrum.")
+    .def("l2", &SpectrumInfo::l2, (arg("self"), arg("index")),
+      "Returns the distance from the sample to the spectrum.")
 
-      .def("position", &SpectrumInfo::position, (arg("self"), arg("index")),
-           "Returns the position of the spectrum with the given index.")
+    .def("position", &SpectrumInfo::position, (arg("self"), arg("index")),
+      "Returns the position of the spectrum with the given index.")
 
-      .def("sourcePosition", &SpectrumInfo::sourcePosition, arg("self"),
-           "Returns the source position.")
+    .def("sourcePosition", &SpectrumInfo::sourcePosition, arg("self"),
+      "Returns the source position.")
 
-      .def("samplePosition", &SpectrumInfo::samplePosition, arg("self"),
-           "Returns the sample position.")
+    .def("samplePosition", &SpectrumInfo::samplePosition, arg("self"),
+      "Returns the sample position.");
 }
