b1f56d691cc1afe68655c024dfb3798716f387a8
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-07-12 16:03:23 +0100

    cppcheck error re #22582

--- Framework/Algorithms/src/CalculateIqt.cpp
+++ Framework/Algorithms/src/CalculateIqt.cpp
@@ -132,9 +132,10 @@ randomizeWorkspaceWithinError(MatrixWorkspace_sptr workspace, const int seed) {
 
 double standardDeviation(const std::vector<double> &inputValues) {
   const auto inputSize =
-      boost::numeric_cast<double>(inputValues.size()) const auto mean =
-          std::accumulate(inputValues.begin(), inputValues.end(), 0.0) /
-          inputSize;
+      boost::numeric_cast<double>(inputValues.size());
+  const auto mean =
+      std::accumulate(inputValues.begin(), inputValues.end(), 0.0) /
+      inputSize;
   double sumOfXMinusMeanSquared = 0;
   for (auto &&x : inputValues)
     sumOfXMinusMeanSquared += (x - mean) * (x - mean);
