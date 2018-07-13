deffb7858446ab974b8847fa53e44a4785a50dc4
Sam <samjones714@gmail.com>
2018-07-13 09:35:26 +0100

    Re #22705 Sample Log Writing and some data handling

--- Framework/DataHandling/inc/MantidDataHandling/LoadPSIMuonBin.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadPSIMuonBin.h
@@ -9,9 +9,46 @@
 namespace Mantid {
 namespace DataHandling {
 
+struct headerData{
+  int16_t tdcResolution;
+  int16_t tdcOverflow;
+  int16_t numberOfRuns;
+  int16_t lengthOfHistograms;
+  int16_t numberOfHistograms;
+  int32_t totalEvents;
+  float histogramBinWidth;
+  int32_t monNumberOfevents;
+  int16_t numberOfDataRecordsFile;
+  int16_t lengthOfDataRecordsBin;
+  int16_t numberOfDataRecordsHistogram;
+  int16_t numberOfHistogramsPerRecord;
+  int32_t periodOfSave;
+  int32_t periodOfMon;
+  std::string sample;
+  std::string temp;
+  std::string field;
+  std::string orientation;
+  std::string comment;
+  std::string dateStart;
+  std::string dateEnd;
+  std::string timeStart;
+  std::string timeEnd;
+  std::string monDeviation;
+  int32_t scalars[18];
+  std::string labels_scalars[18]; // 18 Strings with 4 max length
+  int32_t labelsOfHistograms[16];
+  int16_t integerT0[16];
+  int16_t firstGood[16];
+  int16_t lastGood[16];
+  float realT0[16];
+  float temperatures[4];
+  float temperatureDeviation[4];
+  float monLow[4];
+  float monHigh[4];
+};
+
 class DLLExport LoadPSIMuonBin
     : public API::IFileLoader<Kernel::FileDescriptor> {
-      
 public:
   const std::string name() const override;
   const std::string summary() const override;
@@ -23,8 +60,12 @@ public:
 private:
   void init() override;
   void exec() override;
-  std::string getFormattedDate(std::string date, std::string time);
+  std::string getFormattedDateTime(std::string date, std::string time);
+  void assignOutputWorkspaceParticulars(DataObjects::Workspace2D_sptr &outputWorkspace);
+
+  struct headerData m_header;
 };
+
 } // namespace DataHandling
 } // namespace Mantid
 
--- Framework/DataHandling/src/LoadPSIMuonBin.cpp
+++ Framework/DataHandling/src/LoadPSIMuonBin.cpp
@@ -8,9 +8,14 @@
 #include "MantidHistogramData/Histogram.h"
 #include "MantidDataObjects/Workspace2D.h"
 #include "MantidKernel/DateAndTime.h"
+#include "MantidAPI/Axis.h"
+#include "MantidKernel/Unit.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidKernel/UnitLabelTypes.h"
 
 #include <fstream>
 #include <map>
+#include <boost/shared_ptr.hpp>
 
 namespace Mantid {
 namespace DataHandling {
@@ -50,7 +55,7 @@ int LoadPSIMuonBin::confidence(Kernel::FileDescriptor &descriptor) const {
   return confidence;
 }
 
-// version 1 does not however there is an issue open to create a version which
+// version 1 however there is an issue open to create a version which
 // handles temperatures aswell
 bool LoadPSIMuonBin::loadMutipleAsOne() { return false; }
 
@@ -88,176 +93,141 @@ void LoadPSIMuonBin::exec() {
 
   //The single variables in the header of the binary file:
   //Should be at 3rd byte
-  int16_t tdcResolution;
-  streamReader >> tdcResolution;
+  streamReader >> m_header.tdcResolution;
 
   //Should be at 5th byte
-  int16_t tdcOverflow;
-  streamReader >> tdcOverflow;
+  streamReader >> m_header.tdcOverflow;
 
   //Should be at 7th byte
-  int16_t numberOfRuns;
-  streamReader >> numberOfRuns;
+  streamReader >> m_header.numberOfRuns;
 
   //This may be 29 but set to 28
   streamReader.moveStreamToPosition(28);
-  int16_t lengthOfHistograms;
-  streamReader >> lengthOfHistograms;
+  streamReader >> m_header.lengthOfHistograms;
 
   //Should be at 31st byte
-  int16_t numberOfHistograms;
-  streamReader >> numberOfHistograms;
+  streamReader >> m_header.numberOfHistograms;
 
   streamReader.moveStreamToPosition(424);
-  int32_t totalEvents;
-  streamReader >> totalEvents;
+  streamReader >> m_header.totalEvents;
 
   streamReader.moveStreamToPosition(1012);
-  float histogramBinWidth;
-  streamReader >> histogramBinWidth;
+  streamReader >> m_header.histogramBinWidth;
 
-  if(histogramBinWidth == 0){
-    histogramBinWidth = static_cast<float>((625.E-6)/8.*pow(static_cast<float>(2.), static_cast<float>(tdcResolution)));
+  if(m_header.histogramBinWidth == 0){
+    m_header.histogramBinWidth = static_cast<float>((625.E-6)/8.*pow(static_cast<float>(2.), static_cast<float>(m_header.tdcResolution)));
   }
 
   streamReader.moveStreamToPosition(712);
-  int32_t monNumberOfevents;
-  streamReader >> monNumberOfevents;
+  streamReader >> m_header.monNumberOfevents;
 
-  streamReader.moveStreamToPosition(128);
-  int16_t numberOfDataRecordsFile;      // numdef
-  streamReader >> numberOfDataRecordsFile;
+  streamReader.moveStreamToPosition(128);   // numdef
+  streamReader >> m_header.numberOfDataRecordsFile;
 
   //Should be at 130th byte
-  streamReader.moveStreamToPosition(130);
-  int16_t lengthOfDataRecordsBin;       // lendef
-  streamReader >> lengthOfDataRecordsBin;
+  streamReader.moveStreamToPosition(130);      // lendef
+  streamReader >> m_header.lengthOfDataRecordsBin;
 
   //Should be at 132nd byte
-  streamReader.moveStreamToPosition(132);
-  int16_t numberOfDataRecordsHistogram; // kdafhi
-  streamReader >> numberOfDataRecordsHistogram;
+  streamReader.moveStreamToPosition(132); // kdafhi
+  streamReader >> m_header.numberOfDataRecordsHistogram;
 
   //Should be at 134th Byte
-  streamReader.moveStreamToPosition(134);
-  int16_t numberOfHistogramsPerRecord;  // khidaf
-  streamReader >> numberOfHistogramsPerRecord;
+  streamReader.moveStreamToPosition(134);  // khidaf
+  streamReader >> m_header.numberOfHistogramsPerRecord;
 
   streamReader.moveStreamToPosition(654);
-  int32_t periodOfSave;
-  streamReader >> periodOfSave;
+  streamReader >> m_header.periodOfSave;
 
   //Should be at 658th byte
-  int32_t periodOfMon;
-  streamReader >> periodOfMon;
+  streamReader >> m_header.periodOfMon;
 
   //The strings in the header of the binary file:
-  streamReader.moveStreamToPosition(138);
-  std::string sample; // Only pass 10 bytes into the string from stream
-  streamReader.read(sample, 10);
+  streamReader.moveStreamToPosition(138); // Only pass 10 bytes into the string from stream
+  streamReader.read(m_header.sample, 10);
 
-  streamReader.moveStreamToPosition(148);
-  std::string temp; // Only pass 10 bytes into the string from stream
-  streamReader.read(temp, 10);
+  streamReader.moveStreamToPosition(148); // Only pass 10 bytes into the string from stream
+  streamReader.read(m_header.temp, 10);
 
-  streamReader.moveStreamToPosition(158);
-  std::string field; // Only pass 10 bytes into the string from stream
-  streamReader.read(field, 10);
+  streamReader.moveStreamToPosition(158); // Only pass 10 bytes into the string from stream
+  streamReader.read(m_header.field, 10);
 
-  streamReader.moveStreamToPosition(168);
-  std::string orientation; // Only pass 10 bytes into the string from stream
-  streamReader.read(orientation, 10);
+  streamReader.moveStreamToPosition(168); // Only pass 10 bytes into the string from stream
+  streamReader.read(m_header.orientation, 10);
 
-  streamReader.moveStreamToPosition(860);
-  std::string comment; // Only pass 62 bytes into the string from stream
-  streamReader.read(comment, 62);
+  streamReader.moveStreamToPosition(860); // Only pass 62 bytes into the string from stream
+  streamReader.read(m_header.comment, 62);
 
-  streamReader.moveStreamToPosition(218);
-  std::string dateStart; // Only pass 9 bytes into the string from stream
-  streamReader.read(dateStart, 9);
+  streamReader.moveStreamToPosition(218); // Only pass 9 bytes into the string from stream
+  streamReader.read(m_header.dateStart, 9);
 
-  streamReader.moveStreamToPosition(227);
-  std::string dateEnd; // Only pass 9 bytes into the string from stream
-  streamReader.read(dateEnd, 9);
+  streamReader.moveStreamToPosition(227); // Only pass 9 bytes into the string from stream
+  streamReader.read(m_header.dateEnd, 9);
 
-  streamReader.moveStreamToPosition(236);
-  std::string timeStart; // Only pass 8 bytes into the string from stream
-  streamReader.read(timeStart, 8);
+  streamReader.moveStreamToPosition(236); // Only pass 8 bytes into the string from stream
+  streamReader.read(m_header.timeStart, 8);
 
-  streamReader.moveStreamToPosition(244);
-  std::string timeEnd; // Only pass 8 bytes into the string from stream
-  streamReader.read(timeEnd, 8);
+  streamReader.moveStreamToPosition(244); // Only pass 8 bytes into the string from stream
+  streamReader.read(m_header.timeEnd, 8);
 
-  streamReader.moveStreamToPosition(60);
-  std::string monDeviation; // Only pass 11 bytes into the string from stream
-  streamReader.read(monDeviation, 11);
+  streamReader.moveStreamToPosition(60); // Only pass 11 bytes into the string from stream
+  streamReader.read(m_header.monDeviation, 11);
 
   //The arrays in the header of the binary file:
-  int32_t scalars[18];
-  std::string labels_scalars[18]; // 18 Strings with 4 max length
   for(auto i = 0u; i <= 5; ++i){
     streamReader.moveStreamToPosition(924 + (i*4));
-    streamReader.read(labels_scalars[i], 4); 
+    streamReader.read(m_header.labels_scalars[i], 4); 
 
     streamReader.moveStreamToPosition(670 + (i*4));
-    streamReader >> scalars[i];
+    streamReader >> m_header.scalars[i];
   }
 
   for(auto i = 6u; i < 18; ++i){
     streamReader.moveStreamToPosition(554 + ((i-6)*4));
-    streamReader.read(labels_scalars[i], 4);
+    streamReader.read(m_header.labels_scalars[i], 4);
 
     streamReader.moveStreamToPosition(360 + ((i-6)*4));
-    streamReader >> scalars[i];
+    streamReader >> m_header.scalars[i];
   }
 
-  int32_t labelsOfHistograms[16];
-  int16_t integerT0[16];
-  int16_t firstGood[16];
-  int16_t lastGood[16];
-  float realT0[16];
   for(auto i = 0u; i <=15; ++i){
     streamReader.moveStreamToPosition(948 + (i*4));
-    streamReader >> labelsOfHistograms[i];
+    streamReader >> m_header.labelsOfHistograms[i];
 
     streamReader.moveStreamToPosition(458 + (i*2));
-    streamReader >> integerT0[i];
+    streamReader >> m_header.integerT0[i];
 
     streamReader.moveStreamToPosition(490 + (i*2));
-    streamReader >> firstGood[i];
+    streamReader >> m_header.firstGood[i];
 
     streamReader.moveStreamToPosition(522 + (i*2));
-    streamReader >> lastGood[i];
+    streamReader >> m_header.lastGood[i];
 
     streamReader.moveStreamToPosition(792 + (i*4));
-    streamReader >> realT0[i];
+    streamReader >> m_header.realT0[i];
   }
 
-  float temperatures[4];
-  float temperatureDeviation[4];
-  float monLow[4];
-  float monHigh[4];
   for(auto i = 0u; i < 4; ++i){
     streamReader.moveStreamToPosition(716 + (i*4));
-    streamReader >> temperatures[i];
+    streamReader >> m_header.temperatures[i];
 
     streamReader.moveStreamToPosition(738 + (i*4));
-    streamReader >> temperatureDeviation[i];
+    streamReader >> m_header.temperatureDeviation[i];
 
     streamReader.moveStreamToPosition(72 + (i*4));
-    streamReader >> monLow[i];
+    streamReader >> m_header.monLow[i];
 
     streamReader.moveStreamToPosition(88 + (i*4));
-    streamReader >> monHigh[i];
+    streamReader >> m_header.monHigh[i];
   }
 
   //Read in the histograms
   std::vector<std::vector<double>> histograms;
-  for(auto histogramIndex = 0; histogramIndex < numberOfHistograms; ++histogramIndex){
+  for(auto histogramIndex = 0; histogramIndex < m_header.numberOfHistograms; ++histogramIndex){
     std::vector<double> nextHistogram;
-    for(auto rowIndex = 0; rowIndex < lengthOfHistograms; ++rowIndex){
+    for(auto rowIndex = 0; rowIndex < m_header.lengthOfHistograms; ++rowIndex){
       //Each histogram bit is 1024 bytes below the file start, and 4 bytes apart, and the HistogramNumber * NumberOfRecordsInEach * LengthOfTheDataRecordBins + PositionInHistogram
-      unsigned long histogramStreamPosition = 1024+(histogramIndex * numberOfDataRecordsFile * lengthOfDataRecordsBin);
+      unsigned long histogramStreamPosition = 1024+(histogramIndex * m_header.numberOfDataRecordsFile * m_header.lengthOfDataRecordsBin);
       unsigned long streamPosition = histogramStreamPosition + rowIndex*sizeof(int32_t);
       streamReader.moveStreamToPosition(streamPosition);
       int32_t nextReadValue;
@@ -273,41 +243,92 @@ void LoadPSIMuonBin::exec() {
   // Create a x axis, assumption that histograms will all be the same size, and that x will be 1 more in size than y
   std::vector<double> xAxis;
   for(auto xIndex = 0; static_cast<unsigned int>(xIndex) < histograms[0].size()+1; ++xIndex){
-    xAxis.push_back(static_cast<double>(xIndex) * histogramBinWidth);
+    xAxis.push_back(static_cast<double>(xIndex) * m_header.histogramBinWidth);
+  }
+
+  // Create Errors
+  std::vector<std::vector<double >> eAxis;
+  for(auto histogramIndex = 0u; histogramIndex < histograms.size(); ++histogramIndex){
+    std::vector<double> newEAxis;
+    for(auto eIndex = 0u; eIndex < histograms[0].size(); ++eIndex){
+      newEAxis.push_back(sqrt(histograms[histogramIndex][eIndex]));
+    }
+    eAxis.push_back(newEAxis);
   }
 
   auto sizeOfXForHistograms = histograms[0].size()+1;
-  DataObjects::Workspace2D_sptr outputWorkspace = DataObjects::create<DataObjects::Workspace2D>(numberOfHistograms, Mantid::HistogramData::Histogram(Mantid::HistogramData::BinEdges(sizeOfXForHistograms)));
+  DataObjects::Workspace2D_sptr outputWorkspace = DataObjects::create<DataObjects::Workspace2D>(m_header.numberOfHistograms, Mantid::HistogramData::Histogram(Mantid::HistogramData::BinEdges(sizeOfXForHistograms)));
 
   for (auto specNum = 0u; specNum < histograms.size(); ++specNum){
     outputWorkspace->mutableX(specNum) = xAxis;
     outputWorkspace->mutableY(specNum) = histograms[specNum];
-    //Add Errors and Dx if nessercary
+    outputWorkspace->mutableE(specNum) = eAxis[specNum];
   }
 
-  //Set axis variables
-  //outputWorkspace.setYUnit("Counts");
-  //outputWorkspace.setXUnit("μs")
-  //outputWorkspace.setLabel("Time");
+  //Sort some workspace particulars
+  outputWorkspace->setTitle(m_header.sample + " - Run:" + std::to_string(m_header.numberOfRuns));
 
-  //Set log numbers 
-  Mantid::Types::Core::DateAndTime start(getFormattedDate(dateStart));
-  Mantid::Types::Core::DateAndTime end(getFormattedDate(dateEnd));
+  //Set axis variables
+  outputWorkspace->setYUnit("Counts");
+  boost::shared_ptr<Kernel::Units::Label> lblUnit =
+      boost::dynamic_pointer_cast<Kernel::Units::Label>(
+          Kernel::UnitFactory::Instance().create("Label"));
+  lblUnit->setLabel("Time", Kernel::Units::Symbol::Microsecond);
+  outputWorkspace->getAxis(0)->unit() = lblUnit;
+
+  //Set Start date and time and end date and time 
+  Mantid::Types::Core::DateAndTime start(getFormattedDateTime(m_header.dateStart, m_header.timeStart));
+  Mantid::Types::Core::DateAndTime end(getFormattedDateTime(m_header.dateEnd, m_header.timeEnd));
   outputWorkspace->mutableRun().setStartAndEndTime(start, end);
+
+  //Start the log building
+  Mantid::API::Algorithm_sptr logAlg = createChildAlgorithm("AddSampleLog");
+  logAlg->setProperty("Workspace", outputWorkspace);
+  
+  //Log the set temperature
+  logAlg->setProperty("LogType", "String");
+  logAlg->setProperty("LogName", "Set Temperature");
+  logAlg->setProperty("LogText", m_header.temp);
+  logAlg->executeAsChildAlg();
+
+  //Add The other temperatures as log
+  auto sizeOfTemps = sizeof(m_header.temperatures)/sizeof(*m_header.temperatures);
+  for(auto tempNum = 1u; tempNum < sizeOfTemps + 1; ++tempNum){
+    if(m_header.temperatures[tempNum-1] != 0){
+      logAlg->setProperty("LogType", "Number");
+      logAlg->setProperty("LogName", "Actual Temperature" + std::to_string(tempNum));
+      logAlg->setProperty("LogText", std::to_string(m_header.temperatures[tempNum-1]));
+      logAlg->executeAsChildAlg();
+    }
+  }
+
+  //Set the comment
+  outputWorkspace->setComment(m_header.comment);
+  logAlg->setProperty("LogType", "String");
+  logAlg->setProperty("LogName", "Comment");
+  logAlg->setProperty("LogText", m_header.comment);
+  logAlg->executeAsChildAlg();
+
+  //Length of run
+  logAlg->setProperty("LogType", "String");
+  logAlg->setProperty("LogName", "Length of Run");
+  logAlg->setProperty("LogText", std::to_string((static_cast<double>(histograms[0].size())) * m_header.histogramBinWidth) + "μs");
+  logAlg->executeAsChildAlg();
+
+  //Log field
+  logAlg->setProperty("LogType", "String");
+  logAlg->setProperty("LogName", "Field");
+  logAlg->setProperty("LogText", m_header.field);
+  logAlg->executeAsChildAlg();
 
   setProperty("OutputWorkspace", outputWorkspace);
 }
 
 std::string LoadPSIMuonBin::getFormattedDateTime(std::string date, std::string time){
     std::map<std::string, std::string> months{{"JAN", "01"},{"FEB", "02"},{"MAR", "03"},{"APR", "04"},{"MAY", "05"},{"JUN", "06"},{"JUL", "07"},{"AUG", "08"},{"SEP", "09"},{"OCT", "10"},{"NOV", "11"},{"DEC", "12"}};
-    /*
-    std::string day = date.substr(0,2);
-    std::string year = "20" + date.substr(7,2);
-    std::string originalMonth = date.substr(3, 3);
-    std::string month = months.find(originalMonth)->second;
-    std::string validDate = year + "-" + month + "-" + day;
-    */
     return "20" + date.substr(7,2) + "-" + months.find(date.substr(3, 3))->second + "-" + date.substr(0,2) + "T" + time;
 }
+
+void LoadPSIMuonBin::assignOutputWorkspaceParticulars(DataObjects::Workspace2D_sptr &outputWorkspace){}
 } // namespace DataHandling
 } // namespace Mantid
