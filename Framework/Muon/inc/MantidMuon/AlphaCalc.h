52f33e1571790ab7ade14214befee622e99cf4d1
T Jubb <t.w.jubb@gmail.com>
2018-07-16 13:40:35 +0100

    Refs #22930 Move AlphaCalc.

---
+++ Framework/Muon/inc/MantidMuon/AlphaCalc.h
@@ -0,0 +1,73 @@
+#ifndef MANTID_ALGORITHM_MUONALPHACALC_H_
+#define MANTID_ALGORITHM_MUONALPHACALC_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid {
+namespace Algorithms {
+/**Muon algorithm for calculating the detector efficiency between two groups of
+detectors.
+
+Required Properties:
+<UL>
+<LI> InputWorkspace - The name of the Workspace2D to take as input </LI>
+<LI> ForwardSpectra - The spectra numbers of the forward group </LI>
+<LI> BackwardSpectra - The spectra numbers of the backward group </LI>
+<LI> FirstGoodValue - First good value </LI>
+<LI> LastGoodValue - Last good value </LI>
+<LI> Alpha (output) </LI>
+</UL>
+
+
+@author Anders Markvardsen, ISIS, RAL
+@date 21/09/2010
+
+Copyright &copy; 2008-10 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
+National Laboratory & European Spallation Source
+
+This file is part of Mantid.
+
+Mantid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Mantid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+File change history is stored at: <https://github.com/mantidproject/mantid>
+Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+class DLLExport AlphaCalc : public API::Algorithm {
+public:
+  /// Algorithm's name for identification overriding a virtual method
+  const std::string name() const override { return "AlphaCalc"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Muon algorithm for calculating the detector efficiency between two "
+           "groups of detectors.";
+  }
+
+  /// Algorithm's version for identification overriding a virtual method
+  int version() const override { return 1; }
+  /// Algorithm's category for identification overriding a virtual method
+  const std::string category() const override { return "Muon"; }
+
+private:
+  // Overridden Algorithm methods
+  void init() override;
+  void exec() override;
+};
+
+} // namespace Algorithm
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHM_MUONALPHACALC_H_*/
