15ed9dce2258c2ceb24432c5263fade5bed7ba03
mantid-builder <mantid-buildserver@mantidproject.org>
2018-07-16 15:11:51 +0100

    clang-format PR22919 8249b91

--- Framework/PythonInterface/mantid/api/src/Exports/ExperimentInfo.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/ExperimentInfo.cpp
@@ -81,7 +81,8 @@ void export_ExperimentInfo() {
 
       .def("detectorInfo", &ExperimentInfo::detectorInfo,
            return_value_policy<reference_existing_object>(), args("self"),
-           "Return a const reference to the :class:`~mantid.geometry.DetectorInfo` "
+           "Return a const reference to the "
+           ":class:`~mantid.geometry.DetectorInfo` "
            "object.")
       .def("spectrumInfo", &ExperimentInfo::spectrumInfo,
            return_value_policy<reference_existing_object>(), args("self"),
