483730c2b6212f58fca22e08b12d48006b1c9e3d
Gagik Vardanyan <vardanyan@ill.fr>
2018-07-17 11:30:50 +0200

    Re #22212 added unit tests

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ILLSANSReductionTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ILLSANSReductionTest.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division, print_function)
 
 import unittest
 from mantid.api import MatrixWorkspace
-from mantid.simpleapi import ILLSANSReduction
+from mantid.simpleapi import ILLSANSReduction, config, mtd
 
 
 class ILLSANSReductionTest(unittest.TestCase):
@@ -18,12 +18,37 @@ class ILLSANSReductionTest(unittest.TestCase):
         mtd.clear()
 
     def test_absorber(self):
-
         ILLSANSReduction(Run='010462', ProcessAs='Absorber', OutputWorkspace='Cd')
         self._check_output(mtd['Cd'])
 
-    def _check_output(self, ws):
+    def test_beam(self):
+        beam = ILLSANSReduction(Run='010414', ProcessAs='Beam', OutputWorkspace='Db', ReturnAll=True)
+        self._check_output(mtd['Db'])
+        self.assertAlmostEqual(beam.BeamCenterX, -0.0048, delta=1e-4)
+        self.assertAlmostEqual(beam.BeamCenterY, -0.0027, delta=1e-4)
+        self.assertAlmostEqual(beam.BeamFluxValue, 6618861, delta=1)
+        self.assertAlmostEqual(beam.BeamFluxError, 8554, delta=1)
 
+    def test_transmission(self):
+        beam = ILLSANSReduction(Run='010414', ProcessAs='Beam', OutputWorkspace='Db', ReturnAll=True)
+        transmission = ILLSANSReduction(Run='010585', ProcessAs='Transmission', BeamInputWorkspace='Db', ReturnAll=True)
+        self.assertAlmostEqual(transmission.TransmissionValue, 0.640, delta=1e-3)
+        self.assertAlmostEqual(transmission.TransmissionError, 0.0019, delta=1e-4)
+
+    def test_container(self):
+        container = ILLSANSReduction(Run='010460', ProcessAs='Container', OutputWorkspace='can')
+        self._check_output(mtd['can'])
+
+    def test_reference(self):
+        ILLSANSReduction(Run='010453', ProcessAs='Reference', SensitivityOutputWorkspace='sens', OutputWorkspace='water')
+        self._check_output(mtd['water'])
+        self._check_output(mtd['sens'], logs=False)
+
+    def test_sample(self):
+        sample = ILLSANSReduction(Run='010569', ProcessAs='Sample')
+        self._check_output(mtd['sample'])
+
+    def _check_output(self, ws, logs=True):
         self.assertTrue(ws)
         self.assertTrue(isinstance(ws, MatrixWorkspace))
         self.assertTrue(ws.isHistogramData())
@@ -34,11 +59,12 @@ class ILLSANSReductionTest(unittest.TestCase):
         self.assertTrue(ws.getRun())
         self.assertTrue(ws.getSampleDetails())
         self.assertTrue(ws.getHistory())
-        self.assertTrue(ws.getRun().hasProperty('qmin'))
-        self.assertTrue(ws.getRun().hasProperty('qmax'))
-        self.assertTrue(ws.getRun().hasProperty('l2'))
-        self.assertTrue(ws.getRun().hasProperty('pixel_height'))
-        self.assertTrue(ws.getRun().hasProperty('pixel_width'))
+        if logs:
+            self.assertTrue(ws.getRun().hasProperty('qmin'))
+            self.assertTrue(ws.getRun().hasProperty('qmax'))
+            self.assertTrue(ws.getRun().hasProperty('l2'))
+            self.assertTrue(ws.getRun().hasProperty('pixel_height'))
+            self.assertTrue(ws.getRun().hasProperty('pixel_width'))
 
 if __name__ == '__main__':
     unittest.main()
