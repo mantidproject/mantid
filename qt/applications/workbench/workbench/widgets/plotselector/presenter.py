1f4db4ba40cb0505341982403bf8ca94365f07d3
Ian Bush <bush@ill.fr>
2018-07-18 13:46:33 +0200

    Refs #22518 Fix flake8 warning

--- qt/applications/workbench/workbench/widgets/plotselector/presenter.py
+++ qt/applications/workbench/workbench/widgets/plotselector/presenter.py
@@ -16,7 +16,8 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import, print_function
 
-import os, re
+import os
+import re
 
 from .model import PlotSelectorModel
 from .view import PlotSelectorView, Column
