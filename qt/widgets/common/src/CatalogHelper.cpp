a7923864bb52da08e9614b7b78607eb56269d418
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-07-19 10:25:10 +0100

    Removed working comment
    Re #18529

--- qt/widgets/common/src/CatalogHelper.cpp
+++ qt/widgets/common/src/CatalogHelper.cpp
@@ -252,7 +252,6 @@ time_t CatalogHelper::getTimevalue(const std::string &inputDate) {
   std::string isoDate = dateSegments.at(2) + "-" + dateSegments.at(1) + "-" +
                         dateSegments.at(0) + " 00:00:00.000";
   // Return the date as time_t value.
-  //temp change once asked how to handle
 
   return Mantid::Types::Core::DateAndTime(isoDate).to_time_t();
 
