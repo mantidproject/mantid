62c81ef48156d1baf87d32df401942b608bcd46f
Ewan Cook <ewan.cook@stfc.ac.uk>
2018-07-20 11:31:57 +0100

    refs #23023 create functionality for identifying a spinbox submission

--- scripts/Muon/GUI/ElementalAnalysis/LoadWidget/load_presenter.py
+++ scripts/Muon/GUI/ElementalAnalysis/LoadWidget/load_presenter.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 
-class LoadWidget(object):
+class LoadPresenter(object):
     def __init__(self, view, model):
         self.view = view
         self.model = model
@@ -17,3 +17,9 @@ class LoadWidget(object):
 
     def unregister_spinbox_val_changed(self, slot):
         self.view.unreg_on_spinbox_val_changed(slot)
+
+    def register_spinbox_submit(self, slot):
+        self.view.on_spinbox_submit(slot)
+
+    def unregister_spinbox_submit(self, slot):
+        self.view.unreg_on_spinbox_submit(slot)
