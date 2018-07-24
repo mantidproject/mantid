00d42bd1d2f4dd49751c2c834b1bcafe8c313626
Ewan Cook <ewan.cook@stfc.ac.uk>
2018-07-24 10:44:01 +0100

    refs #22915 checkout new files from load_widget branch

--- scripts/test/Muon/PeriodicTablePresenter_test.py
+++ scripts/test/Muon/PeriodicTablePresenter_test.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function
 
 import unittest
 
-from Muon.GUI.ElementalAnalysis.PeriodicTable.periodic_table_presenter import PeriodicTable
+from Muon.GUI.ElementalAnalysis.PeriodicTable.periodic_table_presenter import PeriodicTablePresenter
 from Muon.GUI.ElementalAnalysis.PeriodicTable.periodic_table_view import PeriodicTableView
 from Muon.GUI.ElementalAnalysis.PeriodicTable.periodic_table_model import PeriodicTableModel
 from Muon.GUI.ElementalAnalysis.PeriodicTable.periodic_table import PeriodicTable as silxPT
@@ -16,32 +16,33 @@ except ImportError:
     import mock
 
 
-class PeriodicTableWidgetTest(unittest.TestCase):
+class PeriodicTablePresenterTest(unittest.TestCase):
     def setUp(self):
         self._qapp = mock_widget.mockQapp()
         self._model = mock.create_autospec(PeriodicTableModel)
-        self.widget = PeriodicTable(PeriodicTableView(), self._model)
+        self.presenter = PeriodicTablePresenter(
+            PeriodicTableView(), self._model)
         self.mock_elem = mock.Mock()
 
-        self.widget.view.ptable = mock.create_autospec(silxPT)
-        self.widget.view.ptable.getSelection = mock.Mock(
+        self.presenter.view.ptable = mock.create_autospec(silxPT)
+        self.presenter.view.ptable.getSelection = mock.Mock(
             return_value=self.mock_elem)
-        self.widget.view.ptable.isElementSelected = mock.Mock(
+        self.presenter.view.ptable.isElementSelected = mock.Mock(
             return_value=True)
-        self.widget.view.ptable.setSelection = mock.Mock()
-        self.widget.view.ptable.setElementSelected = mock.Mock()
+        self.presenter.view.ptable.setSelection = mock.Mock()
+        self.presenter.view.ptable.setElementSelected = mock.Mock()
 
-        self.widget.is_selected = mock.Mock()
+        self.presenter.is_selected = mock.Mock()
 
-        self.widget.view.on_table_lclicked = mock.Mock()
-        self.widget.view.on_table_rclicked = mock.Mock()
-        self.widget.view.on_table_changed = mock.Mock()
+        self.presenter.view.on_table_lclicked = mock.Mock()
+        self.presenter.view.on_table_rclicked = mock.Mock()
+        self.presenter.view.on_table_changed = mock.Mock()
 
-        self.widget.view.unreg_on_table_lclicked = mock.Mock()
-        self.widget.view.unreg_on_table_rclicked = mock.Mock()
-        self.widget.view.unreg_on_table_changed = mock.Mock()
+        self.presenter.view.unreg_on_table_lclicked = mock.Mock()
+        self.presenter.view.unreg_on_table_rclicked = mock.Mock()
+        self.presenter.view.unreg_on_table_changed = mock.Mock()
 
-        self.view = self.widget.view
+        self.view = self.presenter.view
 
     # checks if subsequent function is called on func()
     def check_second_func_called(self, func, sub_func):
@@ -50,48 +51,48 @@ class PeriodicTableWidgetTest(unittest.TestCase):
 
     def test_register_table_lclicked(self):
         self.check_second_func_called(
-            self.widget.register_table_lclicked,
+            self.presenter.register_table_lclicked,
             self.view.on_table_lclicked)
 
     def test_unregister_table_lclicked(self):
         self.check_second_func_called(
-            self.widget.unregister_table_lclicked,
+            self.presenter.unregister_table_lclicked,
             self.view.unreg_on_table_lclicked)
 
     def test_register_table_rclicked(self):
         self.check_second_func_called(
-            self.widget.register_table_rclicked,
+            self.presenter.register_table_rclicked,
             self.view.on_table_rclicked)
 
     def test_unregister_table_rclicked(self):
         self.check_second_func_called(
-            self.widget.unregister_table_rclicked,
+            self.presenter.unregister_table_rclicked,
             self.view.unreg_on_table_rclicked)
 
     def test_register_table_changed(self):
         self.check_second_func_called(
-            self.widget.register_table_changed,
+            self.presenter.register_table_changed,
             self.view.on_table_changed)
 
     def test_unregister_table_changed(self):
         self.check_second_func_called(
-            self.widget.unregister_table_changed,
+            self.presenter.unregister_table_changed,
             self.view.unreg_on_table_changed)
 
     def test_selection(self):
-        assert self.widget.selection == self.mock_elem
+        assert self.presenter.selection == self.mock_elem
 
     def test_is_selected(self):
-        assert self.widget.is_selected(mock.Mock())
+        assert self.presenter.is_selected(mock.Mock())
 
     def test_select_element(self):
         self.check_second_func_called(
-            self.widget.select_element,
+            self.presenter.select_element,
             self.view.ptable.setElementSelected)
 
     def test_add_elements(self):
         self.check_second_func_called(
-            self.widget.add_elements,
+            self.presenter.add_elements,
             self.view.ptable.setSelection)
 
 
