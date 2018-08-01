0f646b003b4493753fde0c5b7070d6b2f5876ce4
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-08-01 18:00:19 -0400

    Re #000 SANSMaskDTP step 1

--- Framework/PythonInterface/plugins/algorithms/SANSMaskBTP.py
+++ Framework/PythonInterface/plugins/algorithms/SANSMaskBTP.py
@@ -1,17 +1,17 @@
 # pylint: disable=no-init,invalid-name
 from __future__ import (absolute_import, division, print_function)
 import mantid.simpleapi
-import mantid.api
-import mantid.kernel
-import numpy
-from collections import defaultdict
+from mantid.api import MatrixWorkspaceProperty, AlgorithmFactory
+from mantid.kernel import Direction, logger
+import re
 
 
-class SANSMaskBTP(mantid.api.PythonAlgorithm):
+
+class SANSMaskDTP(mantid.api.PythonAlgorithm):
     """ Class to generate grouping file
     """
 
-    {
+    INSTRUMENTS = {
         'BIOSANS': [
             {
                 "id": 1,
@@ -44,6 +44,10 @@ class SANSMaskBTP(mantid.api.PythonAlgorithm):
         ],
     }
 
+    # This is the pattern for tubes, pixels
+    PATTERN_HYPHEN_RANGE = r"^\d+([\-,]\d+)*$"
+    PATTERN_COLON_RANGE = r"^\d+\:\d*\:\d+$"
+
     def category(self):
         """ Mantid required
         """
@@ -55,12 +59,37 @@ class SANSMaskBTP(mantid.api.PythonAlgorithm):
     def name(self):
         """ Mantid required
         """
-        return "MaskBTP"
+        return "SANSMaskDTP"
 
     def summary(self):
         """ Mantid required
         """
-        return "Algorithm to mask detectors in particular banks, tube, or pixels."
+        return "Algorithm to mask detectors in particular detectors, tube, or pixels."
+
+    def PyInit(self):
+        self.declareProperty(
+            mantid.api.MatrixWorkspaceProperty(
+                "InputWorkspace", "", direction = Direction.Input),
+            doc="Input Workspace")
+
+        self.declareProperty(
+            "Detector", "",
+            doc="Detector(s) number(s) to be masked. If empty, will apply to all Detectors")
+        
+        self.declareProperty(
+            "Tube", "",
+            doc="Tube(s) to be masked. If empty, will apply to all tubes")
+        
+        self.declareProperty(
+            "Pixel", "",
+            doc="Pixel(s) to be masked. If empty, will apply to all pixels")
+        
+    def _validate_instrument_name(self):
+        valid_instruments = self.INSTRUMENTS.keys()
+        if self.instrument_name not in valid_instruments:
+            raise RuntimeError("Instrument {} not valid. Valid instruments are {}.".format(
+                self.instrument_name, valid_instruments) +
+                " Make sure the instrument name is defined in the workspace.")
 
     def _hyphen_range(self, s):
         """ yield each integer from a complex range string like "1-9,12, 15-20,23"
@@ -85,52 +114,49 @@ class SANSMaskBTP(mantid.api.PythonAlgorithm):
                 raise ValueError('format error in %s' % x)
         
     def _double_colon_range(self, s, max_value):
-        """ yield each integer from a complex range string like "1::2"
-
+        """ 
+        yield each integer from a complex range string like:
+        1::2 or 1:10:2
+        Note that no validation is done here!!
         """
-        frm, to, step = s.split(':')
-        if not to:
-            to = max_value        
-        return range(frm, to, step+1)
+        try:
+            frm, to, step = map(int, s.split(':'))
+        except ValueError:
+            to = max_value
+            frm, step = map(int, s.split('::'))
+        return range(frm, to+1, step)
+    
+    def _string_to_int(self, sequence, min_value, max_value):
+        sequence = sequence.replace(" ", "") # remove spaces
+        if not sequence: # empty
+            return range(min_value, max_value)
+        elif re.match(self.PATTERN_COLON_RANGE, sequence):
+            return self._double_colon_range(sequence, max_value)
+        elif re.match(self.PATTERN_HYPHEN_RANGE, sequence):
+            return self._hyphen_range(sequence)
+        else:
+            raise ValueError('See the format of the fields in the documentation')
 
-
-
-    def PyInit(self):
-        self.declareProperty(mantid.api.WorkspaceProperty(
-            "Workspace", "",
-            direction=mantid.kernel.Direction.InOut,
-            "Input workspace (optional)")
-        
-        self.declareProperty(
-            "Bank", "", doc="Bank(s) to be masked. If empty, will apply to all banks")
-        
-        self.declareProperty(
-            "Tube", "", doc="Tube(s) to be masked. If empty, will apply to all tubes")
-        
-        self.declareProperty(
-            "Pixel", "", doc="Pixel(s) to be masked. If empty, will apply to all pixels")
-        
-        self.declareProperty(
-            mantid.kernel.IntArrayProperty(
-                name="MaskedDetectors", 
-                direction=mantid.kernel.Direction.Output),
-                doc="List of  masked detectors")
-
-
-    # pylint: disable=too-many-branches
     def PyExec(self):
-        ws = self.getProperty("Workspace").value
-        self.instrument = None
-        self.instname = self.getProperty("Instrument").value
-        
-        bankString = self.getProperty("Bank").value
-        tubeString = self.getProperty("Tube").value
-        pixelString = self.getProperty("Pixel").value
+        ws = self.getProperty("InputWorkspace").value
+        self.instrument = ws.getInstrument()
+        self.instrument_name = self.instrument.getName()
+        self._validate_instrument_name()
 
-        if ws is not None:
-            self.instrument = ws.getInstrument()
-            self.instname = self.instrument.getName()
+        detector_str = self.getProperty("Detector").value
+        tube_str = self.getProperty("Tube").value
+        pixel_str = self.getProperty("Pixel").value
+
+        if detector_str:
+            detectors = self._string_to_int(detector_str, 1, len(self.INSTRUMENTS[self.instrument_name]))
+            logger.debug("Detectors: {}".format(detectors))
+        if tube_str:
+            tubes = self._string_to_int(tube_str, 1, self.INSTRUMENTS[self.instrument_name]["tubes"])
+            logger.debug("Tubes: {}".format(tubes))
+        if pixel_str:
+            pixels = self._string_to_int(pixel_str, 1, self.INSTRUMENTS[self.instrument_name]["pixels"])
+            logger.debug("Pixels: {}".format(pixels))
 
 
 
-mantid.api.AlgorithmFactory.subscribe(MaskBTP)
+AlgorithmFactory.subscribe(SANSMaskDTP)
