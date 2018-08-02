3f95f3e373b89213c669e1d32595d0efd75c23ff
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-08-02 16:18:06 -0400

    Re #000 SANSMaskDTP Working

--- Framework/PythonInterface/plugins/algorithms/SANSMaskDTP.py
+++ Framework/PythonInterface/plugins/algorithms/SANSMaskDTP.py
@@ -1,26 +1,27 @@
 # pylint: disable=no-init,invalid-name
-from __future__ import (absolute_import, division, print_function)
-import mantid.simpleapi
-from mantid.api import MatrixWorkspaceProperty, AlgorithmFactory
-from mantid.kernel import Direction, logger
+from __future__ import absolute_import, division, print_function
+
 import re
 
+from mantid.api import (AlgorithmFactory, MatrixWorkspaceProperty,
+                        PythonAlgorithm)
+from mantid.kernel import Direction, IntArrayProperty, logger
+from mantid.simpleapi import MaskDetectors
 
 
-class SANSMaskDTP(mantid.api.PythonAlgorithm):
-    """ Class to generate grouping file
+class SANSMaskDTP(PythonAlgorithm):
+    """ 
+    Class to generate grouping file
     """
 
     INSTRUMENTS = {
         'BIOSANS': [
             {
-                "id": 1,
                 "name": "detector1",
                 "tubes": 192,
                 "pixels": 256
             },
             {
-                "id": 2,
                 "name": "wing_detector",
                 "tubes": 160,
                 "pixels": 256
@@ -28,7 +29,6 @@ class SANSMaskDTP(mantid.api.PythonAlgorithm):
         ],
         'GPSANS': [
             {
-                "id": 1,
                 "name": "detector1",
                 "tubes": 192,
                 "pixels": 256
@@ -36,7 +36,6 @@ class SANSMaskDTP(mantid.api.PythonAlgorithm):
         ],
         'EQ-SANS': [
             {
-                "id": 1,
                 "name": "detector1",
                 "tubes": 192,
                 "pixels": 256
@@ -68,28 +67,61 @@ class SANSMaskDTP(mantid.api.PythonAlgorithm):
 
     def PyInit(self):
         self.declareProperty(
-            mantid.api.MatrixWorkspaceProperty(
+            MatrixWorkspaceProperty(
                 "InputWorkspace", "", direction = Direction.Input),
             doc="Input Workspace")
 
         self.declareProperty(
-            "Detector", "",
-            doc="Detector(s) number(s) to be masked. If empty, will apply to all Detectors")
-        
+            "Detector", "1", # Default 1
+            doc="Detector number to be masked.")
+
         self.declareProperty(
             "Tube", "",
             doc="Tube(s) to be masked. If empty, will apply to all tubes")
-        
+
         self.declareProperty(
             "Pixel", "",
             doc="Pixel(s) to be masked. If empty, will apply to all pixels")
-        
-    def _validate_instrument_name(self):
+
+        self.declareProperty(IntArrayProperty(
+                name="MaskedDetectors", direction=Direction.Output),
+            doc="List of masked detectors")
+
+    def validateInputs(self):
+        '''
+        Called before everything else to make sure the inputs are valid
+        '''
+
+        issues = dict()
+
+        self.ws = self.getProperty("InputWorkspace").value
+        self.instrument = self.ws.getInstrument()
+        self.instrument_name = self.instrument.getName()
+
+        # Validate instrument
         valid_instruments = self.INSTRUMENTS.keys()
         if self.instrument_name not in valid_instruments:
-            raise RuntimeError("Instrument {} not valid. Valid instruments are {}.".format(
-                self.instrument_name, valid_instruments) +
-                " Make sure the instrument name is defined in the workspace.")
+            message = "Instrument {} not valid. Valid instruments are {}.  Make" \
+                      "sure the instrument name is defined in the workspace.".format(
+                         self.instrument_name, valid_instruments)
+            issues['InputWorkspace'] = message
+            return issues
+
+        # Validate detector: must be an int
+        detector_str = self.getProperty("Detector").value
+
+        try:
+            detector_int = int(detector_str)
+            number_of_detectors = len(self.INSTRUMENTS[self.instrument_name])
+            if detector_int > number_of_detectors:
+                message = "Detector must be less than the number of detectors: " \
+                          "{}".format(number_of_detectors)
+                issues['Detector'] = message
+        except ValueError:
+            message = "Detector must be an integer value"
+            issues['Detector'] = message
+
+        return issues
 
     def _hyphen_range(self, s):
         """ yield each integer from a complex range string like "1-9,12, 15-20,23"
@@ -112,9 +144,9 @@ class SANSMaskDTP(mantid.api.PythonAlgorithm):
                     yield i
             else: # more than one hyphen
                 raise ValueError('format error in %s' % x)
-        
+
     def _double_colon_range(self, s, max_value):
-        """ 
+        """
         yield each integer from a complex range string like:
         1::2 or 1:10:2
         Note that no validation is done here!!
@@ -125,38 +157,63 @@ class SANSMaskDTP(mantid.api.PythonAlgorithm):
             to = max_value
             frm, step = map(int, s.split('::'))
         return range(frm, to+1, step)
-    
+
     def _string_to_int(self, sequence, min_value, max_value):
         sequence = sequence.replace(" ", "") # remove spaces
         if not sequence: # empty
-            return range(min_value, max_value)
+            return range(min_value, max_value+1)
         elif re.match(self.PATTERN_COLON_RANGE, sequence):
             return self._double_colon_range(sequence, max_value)
         elif re.match(self.PATTERN_HYPHEN_RANGE, sequence):
-            return self._hyphen_range(sequence)
+            return list(self._hyphen_range(sequence))
         else:
             raise ValueError('See the format of the fields in the documentation')
 
     def PyExec(self):
-        ws = self.getProperty("InputWorkspace").value
-        self.instrument = ws.getInstrument()
-        self.instrument_name = self.instrument.getName()
-        self._validate_instrument_name()
 
         detector_str = self.getProperty("Detector").value
         tube_str = self.getProperty("Tube").value
         pixel_str = self.getProperty("Pixel").value
 
-        if detector_str:
-            detectors = self._string_to_int(detector_str, 1, len(self.INSTRUMENTS[self.instrument_name]))
-            logger.debug("Detectors: {}".format(detectors))
-        if tube_str:
-            tubes = self._string_to_int(tube_str, 1, self.INSTRUMENTS[self.instrument_name]["tubes"])
-            logger.debug("Tubes: {}".format(tubes))
-        if pixel_str:
-            pixels = self._string_to_int(pixel_str, 1, self.INSTRUMENTS[self.instrument_name]["pixels"])
-            logger.debug("Pixels: {}".format(pixels))
+        # This was validated above, so we can safely convert it to int
+        detectors = int(detector_str)
+        logger.debug("Detectors: {}".format(detectors))
 
+        tubes = self._string_to_int(
+            tube_str, 1,
+            self.INSTRUMENTS[self.instrument_name][detectors-1]["tubes"])
+        logger.debug("Tubes: {}".format(tubes))
+
+        pixels = self._string_to_int(
+            pixel_str, 1,
+            self.INSTRUMENTS[self.instrument_name][detectors-1]["pixels"])
+        logger.debug("Pixels: {}".format(pixels))
+
+        # Let's mast it
+        detectors_to_mask = []
+        # get detector name
+        detector = self.instrument.getComponentByName(
+            self.INSTRUMENTS[self.instrument_name][detectors-1]["name"])
+            # iterate tubes
+        for t in tubes:
+            tube_component = detector[t-1]
+            for p in pixels:
+                # EQSANS needs an extra array !!! pixel128 = tube0[0][128]
+                if tube_component.nelements() <= 1:
+                    tube_component = tube_component[0]
+                pixel_component = tube_component[p-1]
+                detectors_to_mask.append(pixel_component.getID())
+
+        logger.debug("IDs: {}".format(detectors_to_mask))
+
+        if len(detectors_to_mask) > 0:
+            MaskDetectors(Workspace=self.ws, DetectorList=detectors_to_mask,
+                          EnableLogging=False)
+            logger.information("{} detectors masked.".format(len(detectors_to_mask)))
+        else:
+            logger.information("No detectors within this range!")
+
+        self.setProperty("MaskedDetectors", detectors_to_mask)
 
 
 AlgorithmFactory.subscribe(SANSMaskDTP)
