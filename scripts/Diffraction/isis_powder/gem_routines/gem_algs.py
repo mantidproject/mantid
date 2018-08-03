06013ad308e5907082ca1fb14a523eaa7849a2ad
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-08-03 13:22:21 +0100

    (Re #23153 Formatted for flake

--- scripts/Diffraction/isis_powder/gem_routines/gem_algs.py
+++ scripts/Diffraction/isis_powder/gem_routines/gem_algs.py
@@ -46,5 +46,3 @@ def get_run_details(run_number_string, inst_settings, is_vanadium_run):
     return create_run_details_object(run_number_string=run_number_string, inst_settings=inst_settings,
                                      is_vanadium_run=is_vanadium_run, empty_run_call=empty_run_callable,
                                      vanadium_run_call=vanadium_run_callable)
-
-
