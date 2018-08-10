cfc19b6f18520cf5ef1bd7468baead2f42100388
Samuel Jones <samjones714@gmail.com>
2018-08-10 08:59:06 +0100

    Re #22515 Added Copyright to the test

--- Framework/Algorithms/test/SortXAxisTest.h
+++ Framework/Algorithms/test/SortXAxisTest.h
@@ -9,6 +9,24 @@
 #include "MantidHistogramData/Histogram.h"
 #include <cxxtest/TestSuite.h>
 
+/**
+  Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
+  National Laboratory & European Spallation Source
+  This file is part of Mantid.
+  Mantid is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  Mantid is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  File change history is stored at: <https://github.com/mantidproject/mantid>
+  Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
+
 using namespace Mantid;
 using namespace Mantid::API;
 using namespace Mantid::Kernel;
