cdae8f07a631879ec709d2e6a14cf31ec6e0189d
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-08-10 14:21:31 +0100

    Add system tests. Re #23212.

---
+++ Testing/SystemTests/tests/analysis/ReflectometryFloodCorrection.py
@@ -0,0 +1,26 @@
+#pylint: disable=no-init
+import stresstesting
+from mantid.simpleapi import *
+
+
+class ReflectometryCreateFloodWorkspaceNoExclude(stresstesting.MantidStressTest):
+
+    flood_ws_name = 'flood'
+
+    def runTest(self):
+        flood = CreateFloodWorkspace('OFFSPEC00035946.nxs', StartSpectrumIndex=265, EndSpectrumIndex=500, OutputWorkspace=self.flood_ws_name)
+
+    def validate(self):
+        return self.flood_ws_name,'ReflFloodFileNoExclude.nxs'
+
+
+class ReflectometryCreateFloodWorkspaceExclude(stresstesting.MantidStressTest):
+
+    flood_ws_name = 'flood'
+
+    def runTest(self):
+        flood = CreateFloodWorkspace('OFFSPEC00035946.nxs', StartSpectrumIndex=250, EndSpectrumIndex=600,
+            Exclude=[260, 261, 262, 516, 517, 518], OutputWorkspace=self.flood_ws_name)
+
+    def validate(self):
+        return self.flood_ws_name,'ReflFloodFileExclude.nxs'
