2a928676fb2791d38d451ab31efe22fdc54d3eaa
Igor Gudich <igor.gudich@esss.se>
2018-08-13 11:34:04 +0200

    Moving the runLoadInstrument to connect function.
    The buffer workspace is added to store workspace instrument data and
    axis names. Loading the instrument data is moved to "connect" function
    to avoid multiple loading during streaming.
    
    re #20525

--- Framework/LiveData/src/ISIS/ISISHistoDataListener.cpp
+++ Framework/LiveData/src/ISIS/ISISHistoDataListener.cpp
@@ -124,6 +124,17 @@ bool ISISHistoDataListener::connect(const Poco::Net::SocketAddress &address) {
 
   loadTimeRegimes();
 
+  // Create dummy workspace to store instrument data
+  bufferWorkspace = WorkspaceFactory::Instance().create(
+      "Workspace2D", 1,1,1);
+
+  bufferWorkspace->getAxis(0)->unit() =
+      Kernel::UnitFactory::Instance().create("TOF");
+  bufferWorkspace->setYUnit("Counts");
+
+  // Only run the loading of instrument once
+  runLoadInstrument(bufferWorkspace, getString("NAME"));
+
   return true;
 }
 
@@ -196,13 +207,9 @@ boost::shared_ptr<Workspace> ISISHistoDataListener::extractData() {
 
   // Create the 2D workspace for the output
   auto localWorkspace = WorkspaceFactory::Instance().create(
-      "Workspace2D", numberOfHistograms, m_numberOfBins[m_timeRegime] + 1,
+      bufferWorkspace, numberOfHistograms, m_numberOfBins[m_timeRegime] + 1,
       m_numberOfBins[m_timeRegime]);
 
-  // Set the unit on the workspace to TOF
-  localWorkspace->getAxis(0)->unit() =
-      Kernel::UnitFactory::Instance().create("TOF");
-  localWorkspace->setYUnit("Counts");
   localWorkspace->updateSpectraUsing(
       SpectrumDetectorMapping(m_specIDs, m_detIDs));
 
@@ -232,8 +239,6 @@ boost::shared_ptr<Workspace> ISISHistoDataListener::extractData() {
     }
 
     if (period == firstPeriod) {
-      // Only run the Child Algorithms once
-      runLoadInstrument(localWorkspace, getString("NAME"));
       if (m_numberOfPeriods > 1) {
         // adding first ws to the group after loading instrument
         // otherwise ws can be lost.
