d50e83af30452d73add9a4c9a59570781e0e110e
Ewan Cook <5237234+ewancook@users.noreply.github.com>
2018-08-16 11:42:35 +0100

    refs #23260 fix matplotlib issue with set_position and add_subplot

--- scripts/Muon/GUI/ElementalAnalysis/Plotting/plotting_presenter.py
+++ scripts/Muon/GUI/ElementalAnalysis/Plotting/plotting_presenter.py
@@ -1,3 +1,18 @@
 class PlotPresenter(object):
     def __init__(self, view):
         self.view = view
+
+    def add_vline(self, plot_name, x_value, y_min, y_max, **kwargs):
+        pass
+
+    def add_hline(self, plot_name, y_value, x_min, x_max, **kwargs):
+        pass
+
+    def add_moveable_vline(self, plot_name, x_value, y_minx, y_max, **kwargs):
+        pass
+
+    def add_moveable_hline(self, plot_name, y_value, x_min, x_max, **kwargs):
+        pass
+
+    def add_errors(self, *plots):
+        pass
--- scripts/Muon/GUI/ElementalAnalysis/Plotting/plotting_view.py
+++ scripts/Muon/GUI/ElementalAnalysis/Plotting/plotting_view.py
@@ -1,7 +1,8 @@
 from Muon.GUI.ElementalAnalysis.Plotting import plotting_utils as putils
-from Muon.GUI.Common.message_box import warning
 
 #from mantid import plots
+
+from collections import OrderedDict
 
 from matplotlib.figure import Figure
 from matplotlib import gridspec
@@ -11,11 +12,15 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
 from PyQt4 import QtGui
 
+from random import choice, randint
+from time import time
+
 
 class PlotView(QtGui.QWidget):
     def __init__(self):
         super(PlotView, self).__init__()
-        self.plots = {}
+        self.plots = OrderedDict({})
+        self.current_grid = None
         self.gridspecs = {
             1: gridspec.GridSpec(1, 1),
             2: gridspec.GridSpec(1, 2),
@@ -25,19 +30,28 @@ class PlotView(QtGui.QWidget):
         self.figure = Figure()
         self.figure.set_facecolor("none")
         self.canvas = FigureCanvas(self.figure)
+        self.last_positions = []
 
         self.plot_selector = QtGui.QComboBox()
-        add_button = QtGui.QPushButton("add")
-        add_button.clicked.connect(self.plot)
-
-        del_button = QtGui.QPushButton("del")
-        del_button.clicked.connect(self.del_plot)
 
         button_layout = QtGui.QHBoxLayout()
         self.x_axis_changer = putils.AxisChanger("X")
         self.y_axis_changer = putils.AxisChanger("Y")
-        button_layout.addWidget(add_button)
-        button_layout.addWidget(del_button)
+
+        ### to test functionality: ###
+        self.add_button = QtGui.QPushButton("Add")
+        self.del_button = QtGui.QPushButton("Del")
+
+        def _add(): return self.add_subplot("{:.2f}".format(time()))
+
+        def _del(): return self.remove_subplot(choice(self.plots.keys()))
+        self.add_button.clicked.connect(_add)
+        self.del_button.clicked.connect(_del)
+
+        button_layout.addWidget(self.add_button)
+        button_layout.addWidget(self.del_button)
+        ###  -------------------  ###
+
         button_layout.addWidget(self.plot_selector)
         button_layout.addWidget(self.x_axis_changer)
         button_layout.addWidget(self.y_axis_changer)
@@ -47,42 +61,42 @@ class PlotView(QtGui.QWidget):
         grid.addLayout(button_layout, 1, 0)
         self.setLayout(grid)
 
-    def _set_positions(self, grid, positions):
+    def _get_positions(self):
+        return [p.get_position() for p in self.plots.values()]
+
+    def _set_positions(self, positions):
         for plot, pos in zip(self.plots.values(), positions):
             plot.set_position(
-                grid[pos[0], pos[1]].get_position(self.figure))  # magic
+                self.current_grid[pos[0], pos[1]].get_position(self.figure))
+
+    def _update_gridspec(self, new_plots, last=None):
+        if new_plots:
+            self.current_grid = self.gridspecs[new_plots]
+            positions = putils.get_layout(new_plots)
+            self._set_positions(positions)
+            if last is not None:
+                # label is necessary to fix
+                # https://github.com/matplotlib/matplotlib/issues/4786
+                self.plots[last] = self.figure.add_subplot(
+                    self.current_grid[positions[-1][0], positions[-1][1]], label=last)
+                # testing purposes
+                self.plots[last].plot([randint(0, 100) for i in range(5)])
+                self.plots[last].set_title(last)
+        self.canvas.draw()
+        self.update_plot_selector()
+
+    def update_plot_selector(self):
+        self.plot_selector.clear()
+        self.plot_selector.addItems(self.plots.keys())
 
     def add_subplot(self, name):
-        """ will raise KeyError if: 'name' isn't a plot; plots exceed 4 """
-        new_plots = len(self.plots) + 1
-        g = self.gridspecs[new_plots]
-        positions = putils.get_layout(new_plots)
-        self._set_positions(g, positions)
-        self.plots[name] = self.figure.add_subplot(
-            g[positions[-1][0], positions[-1][1]])
-        self.canvas.draw()
+        """ will raise KeyError if: plots exceed 4 """
+        self._update_gridspec(len(self.plots) + 1, last=name)
         return self.plots[name]
 
     def remove_subplot(self, name):
         """ will raise KeyError if: 'name' isn't a plot; there are no plots """
-        try:
-            self.figure.delaxes(self.plots[name])
-            del self.plots[name]
-            new_plots = len(self.plots)
-            if new_plots:
-                g = self.gridspecs[new_plots]
-                positions = putils.get_layout(new_plots)
-                self._set_positions(g, positions)
-            self.canvas.draw()
-        except KeyError as e:
-            warning(e)
-
-    def plot(self):
-        self.add_subplot(str(len(self.plots) + 1))
-
-    def del_plot(self):
-        try:
-            # self.remove_subplot(random.choice(self.plots.keys())) breaks?
-            self.remove_subplot(str(len(self.plots)))
-        except BaseException:
-            return
+        self.figure.delaxes(self.plots[name])
+        self.canvas.draw()
+        del self.plots[name]
+        self._update_gridspec(len(self.plots))
