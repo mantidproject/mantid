f9abbf12a5a5072c0466eedc2fb3ac7e82c51201
Elliot Oram <elliot.oram@stfc.ac.uk>
2018-08-21 14:24:04 +0100

    Improve tests and add data files
    Refs #23298

--- Framework/PythonInterface/test/python/plugins/algorithms/LoadGudrunOutputTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/LoadGudrunOutputTest.py
@@ -1,53 +1,70 @@
 from __future__ import (absolute_import, division, print_function)
 from mantid.simpleapi import LoadGudrunOutput, Workspace
+
 import unittest
 import tempfile
 
 
 class LoadGudrunOutputTest(unittest.TestCase):
 
+    def setUp(self):
+        self.file_name = 'POLARIS00097947-min{}'
+
     def test_valid_extensions(self):
-        self.assertIsNotNone(LoadGudrunOutput('file.dcs01'))
-        self.assertIsNotNone(LoadGudrunOutput('file.mdcs01'))
-        self.assertIsNotNone(LoadGudrunOutput('file.mint01'))
-        self.assertIsNotNone(LoadGudrunOutput('file.mdor01'))
-        self.assertIsNotNone(LoadGudrunOutput('file.mgor01'))
+        self.assertIsNotNone(LoadGudrunOutput(InputFile=self.file_name.format('.dcs01'), OutputWorkspace='out_ws'))
+        self.assertIsNotNone(LoadGudrunOutput(InputFile=self.file_name.format('.mdcs01'), OutputWorkspace='out_ws'))
+        self.assertIsNotNone(LoadGudrunOutput(InputFile=self.file_name.format('.mint01'), OutputWorkspace='out_ws'))
+        self.assertIsNotNone(LoadGudrunOutput(InputFile=self.file_name.format('.mdor01'), OutputWorkspace='out_ws'))
+        self.assertIsNotNone(LoadGudrunOutput(InputFile=self.file_name.format('.mgor01'), OutputWorkspace='out_ws'))
 
     def test_invalid_extension(self):
-        self.assertRaisesRegexp(ValueError, "Unexpected file extensions \'.nxs\'. Valid extensions are: "
-                                            "[\'.dcs\', \'.mdsc\', \'.mint01\', \'.mdor\', \'mgor\'",
-                                LoadGudrunOutput, 'file.nxs')
+        self.assertRaises(ValueError, LoadGudrunOutput, 'file.nxs', 'out_ws')
 
     def test_file_does_not_exist(self):
-        tempfile.mktemp()
-        self.assertRaisesRegexp(ValueError, "Unexpected file extensions \'.nxs\'. Valid extensions are: "
-                                            "[\'.dcs\', \'.mdsc\', \'.mint01\', \'.mdor\', \'mgor\'",
-                                LoadGudrunOutput, 'file.nxs')
+        self.assertRaises(ValueError, LoadGudrunOutput, 'file.dcs01', 'out_ws')
 
     def test_load_dcs(self):
-        actual = LoadGudrunOutput('file.dcs01')
+        actual = LoadGudrunOutput(self.file_name.format('.dcs01'))
         self.assertIsInstance(actual, Workspace)
+        self.assertEqual(actual.getNumberBins(), 100)
+        self.assertEqual(actual.getNumberHistograms(), 5)
 
     def test_load_mdsc(self):
-        actual = LoadGudrunOutput('file.mdcs01')
+        actual = LoadGudrunOutput(self.file_name.format('.mdcs01'))
         self.assertIsInstance(actual, Workspace)
+        self.assertEqual(actual.getNumberBins(), 100)
+        self.assertEqual(actual.getNumberHistograms(), 1)
 
     def test_load_mint(self):
-        actual = LoadGudrunOutput('file.mint01')
+        actual = LoadGudrunOutput(self.file_name.format('.mint01'))
         self.assertIsInstance(actual, Workspace)
+        self.assertEqual(actual.getNumberBins(), 100)
+        self.assertEqual(actual.getNumberHistograms(), 1)
 
     def test_load_mdor(self):
-        actual = LoadGudrunOutput('file.mdor01')
+        actual = LoadGudrunOutput(self.file_name.format('.mdor01'))
         self.assertIsInstance(actual, Workspace)
+        self.assertEqual(actual.getNumberBins(), 100)
+        self.assertEqual(actual.getNumberHistograms(), 1)
 
     def test_load_mgor(self):
-        actual = LoadGudrunOutput('file.mgor01')
+        actual = LoadGudrunOutput(self.file_name.format('.mgor01'))
         self.assertIsInstance(actual, Workspace)
+        self.assertEqual(actual.getNumberBins(), 100)
+        self.assertEqual(actual.getNumberHistograms(), 1)
 
     def test_one_column_data_file(self):
-        # create fake data file (1 column)
-        self.fail("Not implemented")
+        with tempfile.NamedTemporaryFile(suffix='.dcs01') as tmp:
+            tmp.write('1234\n2345\n3456\n4567')
+            tmp.close()
+            self.assertRaises(ValueError, LoadGudrunOutput, tmp.name, 'out_ws')
 
     def test_even_column_data_file(self):
-        # create fake data file (4 columns)
-        self.fail("Not implemented")
+        with tempfile.NamedTemporaryFile(suffix='.dcs01') as tmp:
+            tmp.write('1234 2345 3456 4567\n1234 2345 3456 4567')
+            tmp.close()
+            self.assertRaises(ValueError, LoadGudrunOutput, tmp.name, 'out_ws')
+
+
+if __name__ == '__main__':
+    unittest.main()
