34465af7c0dffc77d62603d51f4bf3181a2e1efd
Ewan Cook <5237234+ewancook@users.noreply.github.com>
2018-08-21 16:41:27 +0100

    refs #23260 add axis_changer_view tests

--- scripts/test/Muon/AxisChangerView_test.py
+++ scripts/test/Muon/AxisChangerView_test.py
@@ -74,10 +74,71 @@ if __name__ == "__main__":
 =======
 import unittest
 
+from Muon.GUI.ElementalAnalysis.Plotting.AxisChanger.axis_changer_view import AxisChangerView
+
+
+from Muon.GUI.Common import mock_widget
+
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 
 class AxisChangerViewTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self._qapp = mock_widget.mockQapp()
+
+        label = "test"
+        self.view = AxisChangerView(label)
+        self.test_bounds = ["10", "20"]
+
+        self.view.lower_bound.text = mock.Mock(
+            return_value=self.test_bounds[0])
+        self.view.upper_bound.text = mock.Mock(
+            return_value=self.test_bounds[1])
+
+        self.view.lower_bound.setText = mock.Mock()
+        self.view.upper_bound.setText = mock.Mock()
+
+        self.view.lower_bound.clear = mock.Mock()
+        self.view.upper_bound.clear = mock.Mock()
+
+        self.view.sig_bounds_changed = mock.Mock()
+        self.view.sig_bounds_changed.emit = mock.Mock()
+        self.view.sig_bounds_changed.connect = mock.Mock()
+        self.view.sig_bounds_changed.disconnect = mock.Mock()
+
+        self.slot = mock.Mock()
+
+    def test_get_bounds(self):
+        self.assertEqual(
+            self.view.get_bounds(), [
+                int(i) for i in self.test_bounds])
+
+    def test_set_bounds(self):
+        self.view.set_bounds(self.test_bounds)
+        self.view.lower_bound.setText.assert_called_with(self.test_bounds[0])
+        self.view.upper_bound.setText.assert_called_with(self.test_bounds[1])
+
+    def test_clear_bounds(self):
+        self.view.clear_bounds()
+        self.assertEquals(self.view.lower_bound.clear.call_count, 1)
+        self.assertEquals(self.view.upper_bound.clear.call_count, 1)
+
+    def test__bounds_changed(self):
+        self.view._bounds_changed()
+        self.view.sig_bounds_changed.emit.assert_called_with(
+            self.view.get_bounds())
+
+    def test_on_bounds_changed(self):
+        self.view.on_bounds_changed(self.slot)
+        self.view.sig_bounds_changed.connect.assert_called_with(self.slot)
+
+    def test_unreg_on_bounds_changed(self):
+        self.view.unreg_on_bounds_changed(self.slot)
+        self.view.sig_bounds_changed.disconnect.assert_called_with(self.slot)
 
 
 if __name__ == "__main__":
