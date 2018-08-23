75e30bfbd54eeb3d4093e991877cdc27c40ffb4c
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-08-23 14:33:01 +0100

    Added a system test. Re #23318

--- Testing/SystemTests/tests/analysis/ReflectometryFloodCorrection.py
+++ Testing/SystemTests/tests/analysis/ReflectometryFloodCorrection.py
@@ -39,3 +39,19 @@ class ReflectometryCreateFloodWorkspaceQuadratic(stresstesting.MantidStressTest)
     def validate(self):
         self.disableChecking.append('Instrument')
         return self.flood_ws_name,'ReflFloodFileQuadratic.nxs'
+
+
+class ReflectometryApplyFloodWorkspace(stresstesting.MantidStressTest):
+
+    out_ws_name = 'out'
+
+    def runTest(self):
+        flood = CreateFloodWorkspace('OFFSPEC00035946.nxs', StartSpectrumIndex=250, EndSpectrumIndex=600,
+                                     Exclude=[260, 261, 262, 516, 517, 518], OutputWorkspace='flood')
+        data = Load('OFFSPEC00044998.nxs')
+        data = Rebin(data, [0,1000,100000], PreserveEvents=False)
+        ApplyFloodWorkspace(InputWorkspace=data, FloodWorkspace=flood, OutputWorkspace=self.out_ws_name)
+
+    def validate(self):
+        self.disableChecking.append('Instrument')
+        return self.out_ws_name,'ReflectometryApplyFloodWorkspace.nxs'
