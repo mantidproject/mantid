e61b4fb9837b21d6773d7ce9b54e4386bed3015a
Tom Jubb <t.w.jubb@gmail.com>
2018-08-23 18:44:15 +0100

    Refs #23323 Initial commit.

---
+++ scripts/Muon/GUI/MuonAnalysis/loadrun/load_run_model_multithreading.py
@@ -0,0 +1,114 @@
+from __future__ import (absolute_import, division, print_function)
+
+import mantid.simpleapi as mantid
+
+from Muon.GUI.Common.muon_load_data import MuonLoadData
+import Muon.GUI.Common.threading_manager as thread_manager
+
+
+class LoadRunWidgetModel(object):
+    """Stores info on all currently loaded workspaces"""
+
+    def __init__(self, loaded_data_store=MuonLoadData()):
+        # Used with load thread
+        self._filenames = []
+
+        self._loaded_data_store = loaded_data_store
+        self._current_run = None
+
+        self.thread_manager = None
+
+    # TODO : remove the need for this function
+    def remove_previous_data(self):
+        self._loaded_data_store.remove_last_added_data()
+
+    # Used with load thread
+    def loadData(self, filenames):
+        self._filenames = filenames
+
+    # Used with load thread
+    def execute(self):
+        failed_files = []
+        for filename in self._filenames:
+            try:
+                ws, filename, run = self.load_workspace_from_filename(filename)
+            except Exception as e:
+                failed_files += [filename]
+                continue
+            self._loaded_data_store.add_data(run=run, workspace=ws, filename=filename)
+        if failed_files:
+            message = self.exception_message_for_failed_files(failed_files)
+            raise ValueError(message)
+
+    def add_thread_data(self):
+        for res in self.thread_manager.results["results"]:
+            self._loaded_data_store.add_data(run=res[2], workspace=res[0], filename=res[1])
+
+    def load_with_multithreading(self, filenames, callback_finished, callback_progress, callback_exception):
+        self.load_func = thread_manager.threading_decorator(self.load_workspace_from_filename)
+        n_threads = min(2, len(filenames))
+        self.thread_manager = thread_manager.WorkerManager(fn=self.load_workspace_from_filename, num_threads=n_threads,
+                                                           callback_on_progress_update=callback_progress,
+                                                           callback_on_thread_exception=callback_exception,
+                                                           callback_on_threads_complete=callback_finished,
+                                                           filename=filenames)
+        self.thread_manager.start()
+
+    def exception_message_for_failed_files(self, failed_file_list):
+        return "Could not load the following files : \n - " + "\n - ".join(failed_file_list)
+
+    # Used with load thread
+    def output(self):
+        pass
+
+    # Used with load thread
+    def cancel(self):
+        pass
+
+    def load_workspace_from_filename(self, filename):
+
+        alg = mantid.AlgorithmManager.create("LoadMuonNexus")
+        alg.initialize()
+        alg.setAlwaysStoreInADS(False)
+        alg.setProperty("OutputWorkspace", "__notUsed")
+
+        try:
+            alg.setProperty("Filename", filename)
+            alg.execute()
+            workspace = alg.getProperty("OutputWorkspace").value
+        except Exception as e:
+            # let Load search for the file
+            alg.setProperty("Filename", filename.split("\\")[-1])
+            alg.execute()
+            workspace = alg.getProperty("OutputWorkspace").value
+            filename = alg.getProperty("Filename").value
+
+        run = int(workspace.getRunNumber())
+
+        return workspace, filename, run
+
+    def get_run_list(self):
+        return self.loaded_runs
+
+    def clear_loaded_data(self):
+        self._loaded_data_store.clear()
+
+    @property
+    def current_run(self):
+        return self._current_run
+
+    @current_run.setter
+    def current_run(self, run):
+        self._current_run = run
+
+    @property
+    def loaded_filenames(self):
+        return self._loaded_data_store.get_parameter("filename")
+
+    @property
+    def loaded_workspaces(self):
+        return self._loaded_data_store.get_parameter("workspace")
+
+    @property
+    def loaded_runs(self):
+        return self._loaded_data_store.get_parameter("run")
---
+++ scripts/Muon/GUI/MuonAnalysis/loadrun/load_run_view.py
@@ -0,0 +1,140 @@
+from __future__ import (absolute_import, division, print_function)
+
+from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtCore import Signal
+
+import Muon.GUI.Common.run_string_utils as run_utils
+
+
+class LoadRunWidgetView(QtWidgets.QWidget):
+    # signals for parent widgets
+    loadingStarted = Signal()
+    loadingFinished = Signal()
+    dataChanged = Signal()
+
+    def __init__(self, parent=None):
+        super(LoadRunWidgetView, self).__init__(parent)
+        self.setupUi(self)
+        self.set_run_edit_regex()
+
+        self._warning_window = None
+        self._cached_text = ""
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(468, 45)
+
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        self.loadCurrentRunButton = QtWidgets.QPushButton(Form)
+        self.loadCurrentRunButton.setText("Load Current Run")
+        self.loadCurrentRunButton.setMinimumSize(QtCore.QSize(100, 25))
+        self.loadCurrentRunButton.setToolTip("Load the current run for the current instrument")
+        self.loadCurrentRunButton.setObjectName("loadCurrentRunButton")
+
+        self.incrementRunButton = QtWidgets.QToolButton(Form)
+        self.incrementRunButton.setText(">")
+        self.incrementRunButton.setMinimumSize(QtCore.QSize(25, 25))
+        self.incrementRunButton.setToolTip("Increment the run")
+        self.incrementRunButton.setObjectName("incrementRunButton")
+
+        self.decrementRunButton = QtWidgets.QToolButton(Form)
+        self.decrementRunButton.setText("<")
+        self.decrementRunButton.setMinimumSize(QtCore.QSize(25, 25))
+        self.decrementRunButton.setToolTip("Decrement the run")
+        self.decrementRunButton.setObjectName("decrementRunButton")
+
+        self.instrumentLabel = QtWidgets.QLabel(Form)
+        self.instrumentLabel.setText("Instrument")
+        self.instrumentLabel.setToolTip("")
+        self.instrumentLabel.setObjectName("instrumentLabel")
+
+        self.runEdit = QtWidgets.QLineEdit(Form)
+        self.runEdit.setMinimumSize(QtCore.QSize(0, 25))
+        self.runEdit.setToolTip(
+            "Enter run number using " + run_utils.delimiter
+            + " and " + run_utils.range_separator + " as delimiter and range-separator respectively")
+        self.runEdit.setObjectName("runEdit")
+
+        spacerItem = QtWidgets.QSpacerItem(25, 25, QtWidgets.QSizePolicy.Minimum,
+                                           QtWidgets.QSizePolicy.Minimum)
+
+        self.horizontalLayout.addWidget(self.loadCurrentRunButton)
+        self.horizontalLayout.addWidget(self.decrementRunButton)
+        self.horizontalLayout.addWidget(self.instrumentLabel)
+        self.horizontalLayout.addWidget(self.runEdit)
+        self.horizontalLayout.addWidget(self.incrementRunButton)
+        self.horizontalLayout.addItem(spacerItem)
+
+    def set_run_edit_regex(self):
+        regex = QtCore.QRegExp(run_utils.run_string_regex)  # "^[0-9]*([0-9]+[,-]{0,1})*[0-9]+$"
+        validator = QtGui.QRegExpValidator(regex)
+        self.runEdit.setValidator(validator)
+
+    def disable_loading(self):
+        self.disable_load_buttons()
+        self.loadingStarted.emit()
+
+    def enable_loading(self):
+        self.enable_load_buttons()
+        self.loadingFinished.emit()
+        self.dataChanged.emit()
+
+    def notify_loading_started(self):
+        self.loadingStarted.emit()
+
+    def notify_loading_finished(self):
+        self.loadingFinished.emit()
+        self.dataChanged.emit()
+
+    def disable_load_buttons(self):
+        self.loadCurrentRunButton.setEnabled(False)
+        self.runEdit.setEnabled(False)
+        self.incrementRunButton.setEnabled(False)
+        self.decrementRunButton.setEnabled(False)
+
+    def enable_load_buttons(self):
+        self.loadCurrentRunButton.setEnabled(True)
+        self.runEdit.setEnabled(True)
+        self.incrementRunButton.setEnabled(True)
+        self.decrementRunButton.setEnabled(True)
+
+    def set_instrument_label(self, text):
+        self.instrumentLabel.setText(text)
+
+    def set_run_edit_text(self, text):
+        self.runEdit.setText(text)
+        self._cached_text = self.get_run_edit_text()
+
+    def reset_run_edit_from_cache(self):
+        tmp = self._cached_text
+        self.set_run_edit_text(tmp)
+        self._cached_text = tmp
+
+    def set_current_instrument(self, instrument):
+        self.instrumentLabel.setText(instrument)
+
+    def get_run_edit_text(self):
+        return self.runEdit.text()
+
+    # Signal/slot connections called by presenter
+
+    def on_decrement_run_clicked(self, slot):
+        self.decrementRunButton.clicked.connect(slot)
+
+    def on_increment_run_clicked(self, slot):
+        self.incrementRunButton.clicked.connect(slot)
+
+    def on_load_current_run_clicked(self, slot):
+        self.loadCurrentRunButton.clicked.connect(slot)
+
+    def on_run_edit_changed(self, slot):
+        self.runEdit.returnPressed.connect(slot)
+
+    def warning_popup(self, message):
+        self._warning_window = None
+        self._warning_window = QtWidgets.QMessageBox.warning(self, "Error", str(message))
+
+    def clear(self):
+        self.set_run_edit_text("")
