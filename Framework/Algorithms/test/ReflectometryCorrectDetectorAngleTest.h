f3420808aa03bc034f863faef5767243f6138ad5
Antti Soininen <soininen@ill.fr>
2018-08-24 09:53:57 +0200

    ReflectometryCorrectDetectorAngle: implement first unit tests.
    Re #22734

--- Framework/Algorithms/test/ReflectometryCorrectDetectorAngleTest.h
+++ Framework/Algorithms/test/ReflectometryCorrectDetectorAngleTest.h
@@ -5,53 +5,114 @@
 
 #include "MantidAlgorithms/ReflectometryCorrectDetectorAngle.h"
 
-using Mantid::Algorithms::ReflectometryCorrectDetectorAngle;
+#include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/SpectrumInfo.h"
+#include "MantidGeometry/Crystal/AngleUnits.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+
+using namespace Mantid;
+using Mantid::Geometry::deg2rad;
 
 class ReflectometryCorrectDetectorAngleTest : public CxxTest::TestSuite {
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
-  static ReflectometryCorrectDetectorAngleTest *createSuite() { return new ReflectometryCorrectDetectorAngleTest(); }
-  static void destroySuite( ReflectometryCorrectDetectorAngleTest *suite ) { delete suite; }
-
-
-  void test_Init()
-  {
-    ReflectometryCorrectDetectorAngle alg;
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
+  static ReflectometryCorrectDetectorAngleTest *createSuite() {
+    return new ReflectometryCorrectDetectorAngleTest();
+  }
+  static void destroySuite(ReflectometryCorrectDetectorAngleTest *suite) {
+    delete suite;
   }
 
-  void test_exec()
-  {
-    /*// Create test input if necessary
-    MatrixWorkspace_sptr inputWS = //-- Fill in appropriate code. Consider using TestHelpers/WorkspaceCreationHelpers.h --
+  void test_Init() {
+    Algorithms::ReflectometryCorrectDetectorAngle alg;
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
+  }
 
-    ReflectometryCorrectDetectorAngle alg;
-    // Don't put output in ADS by default
+  void test_SetTwoTheta() {
+    using namespace WorkspaceCreationHelper;
+    constexpr double pixelSize{0.003};
+    constexpr double angle{1.23};
+    constexpr double startX{0.};
+    Kernel::V3D const slit1Pos(-2, 0, 0);
+    Kernel::V3D const slit2Pos(-1, 0, 0);
+    double const slitOpening{0.001};
+    Kernel::V3D const sourcePos(-15, 0, 0);
+    Kernel::V3D const monitorPos(-3, 0, 0);
+    Kernel::V3D const samplePos(0, 0, 0);
+    Kernel::V3D const detectorPos(5.42, 0, 0);
+    auto inputWS = create2DWorkspaceWithReflectometryInstrument(
+        startX, slit1Pos, slit2Pos, slitOpening, slitOpening, sourcePos,
+        monitorPos, samplePos, detectorPos);
+    Algorithms::ReflectometryCorrectDetectorAngle alg;
     alg.setChild(true);
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", inputWS) );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", "_unused_for_child") );
-    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
-    TS_ASSERT( alg.isExecuted() );
-
-    // Retrieve the workspace from the algorithm. The type here will probably need to change. It should
-    // be the type using in declareProperty for the "OutputWorkspace" type.
-    // We can't use auto as it's an implicit conversion.
-    Workspace_sptr outputWS = alg.getProperty("OutputWorkspace");
-    TS_ASSERT(outputWS);*/
-    TS_FAIL("TODO: Check the results and remove this line");
+    alg.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("InputWorkspace", inputWS))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setPropertyValue("OutputWorkspace", "_unused_for_child"))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("TwoTheta", angle))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setProperty("DetectorComponent", "point-detector"))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("PixelSize", pixelSize))
+    TS_ASSERT_THROWS_NOTHING(alg.execute())
+    TS_ASSERT(alg.isExecuted())
+    API::MatrixWorkspace_sptr outputWS = alg.getProperty("OutputWorkspace");
+    TS_ASSERT(outputWS);
+    TS_ASSERT(onlyInstrumentsDiffer(inputWS, outputWS));
+    auto const &spectrumInfo = outputWS->spectrumInfo();
+    TS_ASSERT(!spectrumInfo.isMonitor(0));
+    TS_ASSERT_DELTA(spectrumInfo.twoTheta(0), angle * deg2rad, 1e-10)
   }
-  
-  void test_Something()
-  {
-    TS_FAIL( "You forgot to write a test!");
+
+  void test_SetTwoThetaWhenSampleNotInOrigin() {
+    using namespace WorkspaceCreationHelper;
+    constexpr double pixelSize{0.003};
+    constexpr double angle{1.23};
+    auto inputWS = create2DWorkspaceWithReflectometryInstrument();
+    Algorithms::ReflectometryCorrectDetectorAngle alg;
+    alg.setChild(true);
+    alg.setRethrows(true);
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("InputWorkspace", inputWS))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setPropertyValue("OutputWorkspace", "_unused_for_child"))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("TwoTheta", angle))
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setProperty("DetectorComponent", "point-detector"))
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("PixelSize", pixelSize))
+    TS_ASSERT_THROWS_NOTHING(alg.execute())
+    TS_ASSERT(alg.isExecuted())
+    API::MatrixWorkspace_sptr outputWS = alg.getProperty("OutputWorkspace");
+    TS_ASSERT(outputWS);
+    TS_ASSERT(onlyInstrumentsDiffer(inputWS, outputWS));
+    auto const &spectrumInfo = outputWS->spectrumInfo();
+    TS_ASSERT(!spectrumInfo.isMonitor(0));
+    TS_ASSERT_DELTA(spectrumInfo.twoTheta(0), angle * deg2rad, 1e-10)
   }
 
-
+private:
+  bool onlyInstrumentsDiffer(API::MatrixWorkspace_sptr &ws1,
+                             API::MatrixWorkspace_sptr &ws2) {
+    auto alg =
+        API::AlgorithmManager::Instance().createUnmanaged("CompareWorkspaces");
+    alg->initialize();
+    alg->setChild(true);
+    alg->setProperty("Workspace1", ws1);
+    alg->setProperty("Workspace2", ws2);
+    alg->setProperty("CheckType", true);
+    alg->setProperty("CheckAxes", true);
+    alg->setProperty("CheckSpectraMap", true);
+    alg->setProperty("CheckInstrument", false);
+    alg->setProperty("CheckMasking", true);
+    alg->setProperty("CheckSample", true);
+    alg->execute();
+    bool const result = alg->getProperty("Result");
+    return result;
+  }
 };
-
 
 #endif /* MANTID_ALGORITHMS_REFLECTOMETRYCORRECTDETECTORANGLETEST_H_ */
