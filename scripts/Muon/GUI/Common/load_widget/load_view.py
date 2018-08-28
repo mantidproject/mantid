e8f7832ce93dc87ad45ae14208d6530db281e79e
Tom Jubb <t.w.jubb@gmail.com>
2018-08-28 13:06:35 +0100

    Refs #23335 Comment out MantidQt.

--- scripts/Muon/GUI/Common/load_widget/load_view.py
+++ scripts/Muon/GUI/Common/load_widget/load_view.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 
 from qtpy import QtWidgets
 
-from mantidqtpython import MantidQt
+
+# from mantidqtpython import MantidQt
 
 
 class LoadView(QtWidgets.QWidget):
