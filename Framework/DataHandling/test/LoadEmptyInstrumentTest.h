ed900e3a1ff7233d218c14885bcf948ad93c9936
Owen Arnold <owen.arnold@stfc.ac.uk>
2018-08-29 12:56:27 +0100

    Compare base instruments

--- Framework/DataHandling/test/LoadEmptyInstrumentTest.h
+++ Framework/DataHandling/test/LoadEmptyInstrumentTest.h
@@ -1063,8 +1063,8 @@ public:
     // Sanity check that we are not comparing the same instrument (i.e. via some
     // smart caching)
     TSM_ASSERT("Premise of comparision test broken!",
-               wish_xml->getInstrument().get() !=
-                   wish_nexus->getInstrument().get());
+               wish_xml->getInstrument()->baseInstrument().get() !=
+                   wish_nexus->getInstrument()->baseInstrument().get());
 
     const auto &wish_nexus_detinfo = wish_nexus->detectorInfo();
     const auto &wish_xml_detinfo = wish_xml->detectorInfo();
