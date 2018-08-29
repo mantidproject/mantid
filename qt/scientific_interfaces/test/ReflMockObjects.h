818c9abc88e8d65a5f7c68a21976114aefe55f5c
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-08-29 10:14:34 +0100

    Updated mock objects Re #22263

--- qt/scientific_interfaces/test/ReflMockObjects.h
+++ qt/scientific_interfaces/test/ReflMockObjects.h
@@ -26,6 +26,7 @@
 #include "MantidQtWidgets/Common/DataProcessorUI/Command.h"
 #include "MantidQtWidgets/Common/DataProcessorUI/OptionsMap.h"
 #include "MantidQtWidgets/Common/DataProcessorUI/TreeData.h"
+#include "MantidQtWidgets/Common/Hint.h"
 #include <gmock/gmock.h>
 
 using namespace MantidQt::CustomInterfaces;
@@ -146,7 +147,7 @@ public:
   MOCK_CONST_METHOD0(detectorCorrectionEnabled, bool());
   // Calls we don't care about
   void
-  createStitchHints(const std::map<std::string, std::string> &hints) override {
+  createStitchHints(const std::vector<MantidQt::MantidWidgets::Hint> &hints) override {
     UNUSED_ARG(hints);
   };
   IReflSettingsPresenter *getPresenter() const override { return nullptr; }
