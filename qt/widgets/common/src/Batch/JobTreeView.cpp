d3bc545a6efd070af411af2a8cc71e9b0d0c698a
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-08-31 17:10:40 +0100

    Added missing else statement Re #22263

--- qt/widgets/common/src/Batch/JobTreeView.cpp
+++ qt/widgets/common/src/Batch/JobTreeView.cpp
@@ -487,6 +487,7 @@ void JobTreeView::keyPressEvent(QKeyEvent *event) {
     if (event->modifiers() & Qt::ControlModifier) {
       cutSelectedRequested();
     }
+  } else {
     QTreeView::keyPressEvent(event);
   }
 }
