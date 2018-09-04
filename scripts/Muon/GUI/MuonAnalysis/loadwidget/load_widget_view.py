0570f85ee4b4d1cd02f0ada6f33849bc0bf1b6a9
T Jubb <t.w.jubb@gmail.com>
2018-09-04 12:29:04 +0100

    Refs #23324 Neaten the view code.

--- scripts/Muon/GUI/MuonAnalysis/loadwidget/load_widget_view.py
+++ scripts/Muon/GUI/MuonAnalysis/loadwidget/load_widget_view.py
@@ -13,8 +13,60 @@ class LoadWidgetView(QtGui.QWidget):
         self.load_run_widget = load_run_view
         self.load_file_widget = load_file_view
 
-        self.setupUi(self)
+        self.setup_interface_layout()
         self.set_connections()
+
+    def setup_interface_layout(self):
+
+        self.clear_button = QtGui.QPushButton(self)
+        self.clear_button.setMinimumSize(QtCore.QSize(100, 25))
+        self.clear_button.setObjectName("clearButton")
+        self.clear_button.setToolTip("Clear the currently loaded data")
+        self.clear_button.setText("Clear")
+
+        self.multiple_loading_label = QtGui.QLabel(self)
+        self.multiple_loading_label.setObjectName("multiple_loading_label")
+        self.multiple_loading_label.setText("Multiple loading : ")
+
+        self.multiple_loading_check = QtGui.QCheckBox(self)
+        self.multiple_loading_check.setToolTip("Enable/disable loading multiple runs at once")
+        self.multiple_loading_check.setChecked(False)
+
+        # self.load_behaviour_label = QtWidgets.QLabel(self)
+        # self.load_behaviour_label.setObjectName("load_behaviour_label")
+        # self.load_behaviour_label.setText("Load Behaviour : ")
+
+        self.load_behaviour_combo = QtGui.QComboBox(self)
+        self.load_behaviour_combo.setObjectName("load_behaviour_combo")
+        self.load_behaviour_combo.addItem("Co-Add")
+        self.load_behaviour_combo.addItem("Simultaneous")
+        self.load_behaviour_combo.setToolTip("The behaviour of the loaded data in multiple file mode")
+        self.load_behaviour_combo.setEnabled(False)
+
+        self.manage_directories_button = QtGui.QPushButton(self)
+        self.manage_directories_button.setMinimumSize(QtCore.QSize(100, 25))
+        self.manage_directories_button.setObjectName("manageDirectoriesButton")
+        self.manage_directories_button.setText("Manage User Directories")
+
+        self.spacer = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+
+        # Set the layout of the tools at the bottom of the widget
+        self.horizontal_layout = QtGui.QHBoxLayout()
+        self.horizontal_layout.setObjectName("horizontalLayout")
+        self.horizontal_layout.addWidget(self.clear_button)
+        self.horizontal_layout.addWidget(self.multiple_loading_label)
+        self.horizontal_layout.addWidget(self.multiple_loading_check)
+        # self.horizontalLayout.addWidget(self.load_behaviour_label)
+        self.horizontal_layout.addWidget(self.load_behaviour_combo)
+        self.horizontal_layout.addWidget(self.manage_directories_button)
+        self.horizontal_layout.addItem(self.spacer)
+
+        # Set the layout vertically
+        self.vertical_layout = QtGui.QVBoxLayout(self)
+        self.vertical_layout.addWidget(self.load_file_widget)
+        self.vertical_layout.addWidget(self.load_run_widget)
+        self.vertical_layout.addLayout(self.horizontal_layout)
+        self.vertical_layout.addStretch(1)
 
     def set_connections(self):
         self.on_multiple_loading_check_changed(self.change_multiple_loading_state)
@@ -46,72 +98,14 @@ class LoadWidgetView(QtGui.QWidget):
         self.load_run_widget.dataChanged.connect(slot)
 
     def on_clear_button_clicked(self, slot):
-        self.clearButton.clicked.connect(slot)
-
-    def setupUi(self, Form):
-
-        self.clearButton = QtGui.QPushButton(Form)
-        self.clearButton.setMinimumSize(QtCore.QSize(100, 25))
-        self.clearButton.setObjectName("clearButton")
-        self.clearButton.setToolTip("Clear the currently loaded data")
-        self.clearButton.setText("Clear")
-
-        self.multiple_loading_label = QtGui.QLabel(Form)
-        self.multiple_loading_label.setObjectName("multiple_loading_label")
-        self.multiple_loading_label.setText("Multiple loading : ")
-
-        self.multiple_loading_check = QtGui.QCheckBox(Form)
-        self.multiple_loading_check.setToolTip("Enable/disable loading multiple runs at once")
-        self.multiple_loading_check.setChecked(False)
-
-        # self.load_behaviour_label = QtWidgets.QLabel(self)
-        # self.load_behaviour_label.setObjectName("load_behaviour_label")
-        # self.load_behaviour_label.setText("Load Behaviour : ")
-
-        self.load_behaviour_combo = QtGui.QComboBox(Form)
-        self.load_behaviour_combo.setObjectName("load_behaviour_combo")
-        self.load_behaviour_combo.addItem("Co-Add")
-        self.load_behaviour_combo.addItem("Simultaneous")
-        self.load_behaviour_combo.setToolTip("The behaviour of the loaded data in multiple file mode")
-        self.load_behaviour_combo.setEnabled(False)
-
-        self.manageDirectoriesButton = QtGui.QPushButton(Form)
-        self.manageDirectoriesButton.setMinimumSize(QtCore.QSize(100, 25))
-        self.manageDirectoriesButton.setObjectName("manageDirectoriesButton")
-        self.manageDirectoriesButton.setText("Manage User Directories")
-
-        self.spacer = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-
-        # Set the layout vertically
-        self.verticalLayout = QtGui.QVBoxLayout(Form)
-
-        self.control_frame = QtGui.QFrame(self)
-
-        # Set the layout of the tools at the bottom of the widget
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.horizontalLayout.addWidget(self.clearButton)
-        self.horizontalLayout.addWidget(self.multiple_loading_label)
-        self.horizontalLayout.addWidget(self.multiple_loading_check)
-        # # self.horizontalLayout.addWidget(self.load_behaviour_label)
-        self.horizontalLayout.addWidget(self.load_behaviour_combo)
-        # self.horizontalLayout.addItem(self.spacer)
-        self.horizontalLayout.addWidget(self.manageDirectoriesButton)
-        self.horizontalLayout.addItem(self.spacer)
-
-        self.verticalLayout.addWidget(self.load_file_widget)
-        self.verticalLayout.addWidget(self.load_run_widget)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.verticalLayout.addStretch(1)
-
-        # self.setLayout(self.verticalLayout)
+        self.clear_button.clicked.connect(slot)
 
     def disable_loading(self):
-        self.clearButton.setEnabled(False)
+        self.clear_button.setEnabled(False)
         self.multiple_loading_check.setEnabled(False)
 
     def enable_loading(self):
-        self.clearButton.setEnabled(True)
+        self.clear_button.setEnabled(True)
         self.multiple_loading_check.setEnabled(True)
 
     # def show_directory_manager(self):
