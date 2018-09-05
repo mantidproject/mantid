bb54557d15884b0595e250ebd1ee9f3db4bda20a
T Jubb <t.w.jubb@gmail.com>
2018-09-05 10:33:08 +0100

    Refs #23323 Tidy up view code.

--- scripts/Muon/GUI/MuonAnalysis/loadrun/load_run_view.py
+++ scripts/Muon/GUI/MuonAnalysis/loadrun/load_run_view.py
@@ -3,7 +3,7 @@ from __future__ import (absolute_import, division, print_function)
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtCore import pyqtSignal as Signal
 
-import Muon.GUI.Common.run_string_utils as runUtils
+import Muon.GUI.Common.run_string_utils as run_utils
 from Muon.GUI.Common.message_box import warning
 
 
@@ -15,63 +15,70 @@ class LoadRunWidgetView(QtGui.QWidget):
 
     def __init__(self, parent=None):
         super(LoadRunWidgetView, self).__init__(parent)
-        self.setupUi(self)
+        self.setup_interface_layout()
         self.set_run_edit_regex()
 
-        self._warning_window = None
         self._cached_text = ""
 
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(468, 45)
+        self.load_current_run_button = None
+        self.increment_run_button = None
+        self.decrement_run_button = None
+        self.horizontal_layout = None
+        self.instrument_label = None
+        self.run_edit = None
+        self.spacer_item = None
 
-        self.horizontalLayout = QtGui.QHBoxLayout(self)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+    def setup_interface_layout(self):
+        self.setObjectName("LoadRunWidget")
+        self.resize(468, 45)
 
-        self.loadCurrentRunButton = QtGui.QPushButton(Form)
-        self.loadCurrentRunButton.setText("Load Current Run")
-        self.loadCurrentRunButton.setMinimumSize(QtCore.QSize(100, 25))
-        self.loadCurrentRunButton.setToolTip("Load the current run for the current instrument")
-        self.loadCurrentRunButton.setObjectName("loadCurrentRunButton")
+        self.load_current_run_button = QtGui.QPushButton(self)
+        self.load_current_run_button.setText("Load Current Run")
+        self.load_current_run_button.setMinimumSize(QtCore.QSize(100, 25))
+        self.load_current_run_button.setToolTip("Load the current run for the current instrument")
+        self.load_current_run_button.setObjectName("loadCurrentRunButton")
 
-        self.incrementRunButton = QtGui.QToolButton(Form)
-        self.incrementRunButton.setText(">")
-        self.incrementRunButton.setMinimumSize(QtCore.QSize(25, 25))
-        self.incrementRunButton.setToolTip("Increment the run")
-        self.incrementRunButton.setObjectName("incrementRunButton")
+        self.increment_run_button = QtGui.QToolButton(self)
+        self.increment_run_button.setText(">")
+        self.increment_run_button.setMinimumSize(QtCore.QSize(25, 25))
+        self.increment_run_button.setToolTip("Increment the run")
+        self.increment_run_button.setObjectName("incrementRunButton")
 
-        self.decrementRunButton = QtGui.QToolButton(Form)
-        self.decrementRunButton.setText("<")
-        self.decrementRunButton.setMinimumSize(QtCore.QSize(25, 25))
-        self.decrementRunButton.setToolTip("Decrement the run")
-        self.decrementRunButton.setObjectName("decrementRunButton")
+        self.decrement_run_button = QtGui.QToolButton(self)
+        self.decrement_run_button.setText("<")
+        self.decrement_run_button.setMinimumSize(QtCore.QSize(25, 25))
+        self.decrement_run_button.setToolTip("Decrement the run")
+        self.decrement_run_button.setObjectName("decrementRunButton")
 
-        self.instrumentLabel = QtGui.QLabel(Form)
-        self.instrumentLabel.setText("Instrument")
-        self.instrumentLabel.setToolTip("")
-        self.instrumentLabel.setObjectName("instrumentLabel")
+        self.instrument_label = QtGui.QLabel(self)
+        self.instrument_label.setText("Instrument")
+        self.instrument_label.setToolTip("")
+        self.instrument_label.setObjectName("instrumentLabel")
 
-        self.runEdit = QtGui.QLineEdit(Form)
-        self.runEdit.setMinimumSize(QtCore.QSize(0, 25))
-        self.runEdit.setToolTip(
-            "Enter run number using " + runUtils.delimiter
-            + " and " + runUtils.range_separator + " as delimiter and range-separator respectively")
-        self.runEdit.setObjectName("runEdit")
+        self.run_edit = QtGui.QLineEdit(self)
+        self.run_edit.setMinimumSize(QtCore.QSize(0, 25))
+        self.run_edit.setToolTip(
+            "Enter run number using " + run_utils.delimiter
+            + " and " + run_utils.range_separator + " as delimiter and range-separator respectively")
+        self.run_edit.setObjectName("runEdit")
 
-        spacerItem = QtGui.QSpacerItem(25, 25, QtGui.QSizePolicy.Minimum,
-                                       QtGui.QSizePolicy.Minimum)
+        self.spacer_item = QtGui.QSpacerItem(25, 25, QtGui.QSizePolicy.Minimum,
+                                             QtGui.QSizePolicy.Minimum)
 
-        self.horizontalLayout.addWidget(self.loadCurrentRunButton)
-        self.horizontalLayout.addWidget(self.decrementRunButton)
-        self.horizontalLayout.addWidget(self.instrumentLabel)
-        self.horizontalLayout.addWidget(self.runEdit)
-        self.horizontalLayout.addWidget(self.incrementRunButton)
-        self.horizontalLayout.addItem(spacerItem)
+        self.horizontal_layout = QtGui.QHBoxLayout(self)
+        self.horizontal_layout.setObjectName("horizontalLayout")
+        self.horizontal_layout.addWidget(self.load_current_run_button)
+        self.horizontal_layout.addWidget(self.decrement_run_button)
+        self.horizontal_layout.addWidget(self.instrument_label)
+        self.horizontal_layout.addWidget(self.run_edit)
+        self.horizontal_layout.addWidget(self.increment_run_button)
+        self.horizontal_layout.addItem(self.spacer_item)
 
     def set_run_edit_regex(self):
-        regex = QtCore.QRegExp(runUtils.run_string_regex)  # "^[0-9]*([0-9]+[,-]{0,1})*[0-9]+$"
+        # "^[0-9]*([0-9]+[,-]{0,1})*[0-9]+$"
+        regex = QtCore.QRegExp(run_utils.run_string_regex)
         validator = QtGui.QRegExpValidator(regex)
-        self.runEdit.setValidator(validator)
+        self.run_edit.setValidator(validator)
 
     def disable_loading(self):
         self.disable_load_buttons()
@@ -90,22 +97,22 @@ class LoadRunWidgetView(QtGui.QWidget):
         self.dataChanged.emit()
 
     def disable_load_buttons(self):
-        self.loadCurrentRunButton.setEnabled(False)
-        self.runEdit.setEnabled(False)
-        self.incrementRunButton.setEnabled(False)
-        self.decrementRunButton.setEnabled(False)
+        self.load_current_run_button.setEnabled(False)
+        self.run_edit.setEnabled(False)
+        self.increment_run_button.setEnabled(False)
+        self.decrement_run_button.setEnabled(False)
 
     def enable_load_buttons(self):
-        self.loadCurrentRunButton.setEnabled(True)
-        self.runEdit.setEnabled(True)
-        self.incrementRunButton.setEnabled(True)
-        self.decrementRunButton.setEnabled(True)
+        self.load_current_run_button.setEnabled(True)
+        self.run_edit.setEnabled(True)
+        self.increment_run_button.setEnabled(True)
+        self.decrement_run_button.setEnabled(True)
 
     def set_instrument_label(self, text):
-        self.instrumentLabel.setText(text)
+        self.instrument_label.setText(text)
 
     def set_run_edit_text(self, text):
-        self.runEdit.setText(text)
+        self.run_edit.setText(text)
         self._cached_text = self.get_run_edit_text()
 
     def reset_run_edit_from_cache(self):
@@ -114,27 +121,27 @@ class LoadRunWidgetView(QtGui.QWidget):
         self._cached_text = tmp
 
     def set_current_instrument(self, instrument):
-        self.instrumentLabel.setText(instrument)
+        self.instrument_label.setText(instrument)
 
     def get_run_edit_text(self):
-        return str(self.runEdit.text())
-
-    # Signal/slot connections called by presenter
-
-    def on_decrement_run_clicked(self, slot):
-        self.decrementRunButton.clicked.connect(slot)
-
-    def on_increment_run_clicked(self, slot):
-        self.incrementRunButton.clicked.connect(slot)
-
-    def on_load_current_run_clicked(self, slot):
-        self.loadCurrentRunButton.clicked.connect(slot)
-
-    def on_run_edit_changed(self, slot):
-        self.runEdit.returnPressed.connect(slot)
+        return str(self.run_edit.text())
 
     def warning_popup(self, message):
         warning(message, parent=self)
 
     def clear(self):
         self.set_run_edit_text("")
+
+    # Signal/slot connections called by presenter
+
+    def on_decrement_run_clicked(self, slot):
+        self.decrement_run_button.clicked.connect(slot)
+
+    def on_increment_run_clicked(self, slot):
+        self.increment_run_button.clicked.connect(slot)
+
+    def on_load_current_run_clicked(self, slot):
+        self.load_current_run_button.clicked.connect(slot)
+
+    def on_run_edit_changed(self, slot):
+        self.run_edit.returnPressed.connect(slot)
