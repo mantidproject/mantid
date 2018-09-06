ab2f50b065de5d1229c7f6f54dc4a28a8e206977
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-09-06 14:19:53 +0100

    Re #23449 Made multiple scattering an optional flag
    The multiple_scattering flag was required if absorption_corrections was true, despite multiple_scattering not being available.
    This changes multiple scattering to be optional.

--- scripts/Diffraction/isis_powder/polaris_routines/polaris_param_mapping.py
+++ scripts/Diffraction/isis_powder/polaris_routines/polaris_param_mapping.py
@@ -20,7 +20,7 @@ attr_mapping = \
      ParamMapEntry(ext_name="input_mode",                  int_name="input_mode", enum_class=INPUT_BATCHING),
      ParamMapEntry(ext_name="merge_banks",                 int_name="merge_banks"),
      ParamMapEntry(ext_name="mode",                        int_name="mode", enum_class=POLARIS_CHOPPER_MODES),
-     ParamMapEntry(ext_name="multiple_scattering",         int_name="multiple_scattering"),
+     ParamMapEntry(ext_name="multiple_scattering",         int_name="multiple_scattering", optional=True),
      ParamMapEntry(ext_name="raw_data_cropping_values",    int_name="raw_data_crop_values"),
      ParamMapEntry(ext_name="run_number",                  int_name="run_number"),
      ParamMapEntry(ext_name="sample_empty",                int_name="sample_empty",   optional=True),
