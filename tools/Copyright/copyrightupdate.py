7e743efa5982da1395c0fa0c29ed6a0e2e1ced7f
Nick Draper <nick.draper@stfc.ac.uk>
2018-09-19 16:49:42 +0100

    Add an excluded directory
    re #23488

--- tools/Copyright/copyrightupdate.py
+++ tools/Copyright/copyrightupdate.py
@@ -31,7 +31,7 @@ regex_other_style = re.compile("^.*?Copyright\s.*?(\d{4}).*?$",
 regex_lines_to_skip = [re.compile("^#!.*?$[\s]*",re.MULTILINE)]
 
 #Directories to ignore - any pathss including these strings will be ignored, so it will cascade
-directories_to_ignore = ["external"]
+directories_to_ignore = ["external","CMake"]
 #Accepted file extensions
 accepted_file_extensions = [".py",".cpp",".h",".tcc",".in",".hh"]
 #python file exxtensions
