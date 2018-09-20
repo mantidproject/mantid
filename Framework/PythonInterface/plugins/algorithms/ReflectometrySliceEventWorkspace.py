8b2f32821072466efa3d2625f57755552e586ef6
Gemma Guest <gemma.guest@stfc.ac.uk>
2018-09-20 11:18:40 +0100

    Call all algorithms as children

--- Framework/PythonInterface/plugins/algorithms/ReflectometrySliceEventWorkspace.py
+++ Framework/PythonInterface/plugins/algorithms/ReflectometrySliceEventWorkspace.py
@@ -3,6 +3,7 @@ from mantid.api import *
 from mantid.kernel import *
 from mantid.simpleapi import *
 
+
 class ReflectometrySliceEventWorkspace(DataProcessorAlgorithm):
     def category(self):
         return "Reflectometry"
@@ -27,92 +28,124 @@ class ReflectometrySliceEventWorkspace(DataProcessorAlgorithm):
                                                     direction=Direction.Output),
                              doc='Group name for the output workspace(s).')
 
-
     def PyExec(self):
-        self._input_ws_name = self.getPropertyValue("InputWorkspace")
-        self._input_monitor_ws_name = self.getPropertyValue("MonitorWorkspace")
+        self._input_ws = self.getProperty("InputWorkspace").value
+        self._output_ws_group_name = self.getPropertyValue("OutputWorkspace")
 
-        self._sliced_output_ws_group_name = self.getPropertyValue("OutputWorkspace")
         self._create_filter()
-        self._slice_input_workspace()
-        self._scale_monitors_for_each_slice()
-        self._rebin_to_monitors()
-        self._add_monitors_to_sliced_output()
+        output_ws_group = self._slice_input_workspace()
+        self._scale_monitors_for_each_slice(output_ws_group)
+        output_ws_group = self._rebin_to_monitors()
+        output_ws_group = self._add_monitors_to_sliced_output()
 
-        self._set_outputs()
+        self.setProperty("OutputWorkspace", self._output_ws_group_name)
         self._clean_up()
 
     def _create_filter(self):
         """Generate the splitter workspace for performing the filtering for each required slice"""
-        alg = AlgorithmManager.create("GenerateEventsFilter")
-        alg.initialize()
-        alg.setChild(True)
-        self._copy_filter_property_values_to(alg)
+        alg = self.createChildAlgorithm("GenerateEventsFilter")
+        for property in self._filter_properties:
+            alg.setProperty(property, self.getPropertyValue(property))
         alg.setProperty("OutputWorkspace", '__split')
         alg.setProperty("InformationWorkspace", '__info')
         alg.execute()
-        self._split_ws = alg.getProperty("OutputWorkspace")
-        self._info_ws = alg.getProperty("InformationWorkspace")
-
-    def _copy_filter_property_values_to(self, filter_alg):
-        """Copy input properties from this algorithm to the child filter algorithm"""
-        for prop in self._filter_properties:
-            value = self.getPropertyValue(prop)
-            filter_alg.setPropertyValue(prop, value)
+        self._split_ws = alg.getProperty("OutputWorkspace").value
+        self._info_ws = alg.getProperty("InformationWorkspace").value
 
     def _slice_input_workspace(self):
         """Perform the slicing of the input workspace"""
-        self._mock_ws_name = '__mock'
-        FilterEvents(InputWorkspace=self._input_ws_name,
-                     SplitterWorkspace=self._split_ws.value,
-                     InformationWorkspace=self._info_ws.value,
-                     OutputWorkspaceBaseName=self._sliced_output_ws_group_name,
-                     GroupWorkspaces=True,FilterByPulseTime = False,
-                     OutputWorkspaceIndexedFrom1 = True,CorrectionToSample = "None",
-                     SpectrumWithoutDetector = "Skip",SplitSampleLogs = False,
-                     OutputTOFCorrectionWorkspace=self._mock_ws_name,ExcludeSpecifiedLogs=False,
-                     TimeSeriesPropertyLogs='proton_charge')
+        alg = self.createChildAlgorithm("FilterEvents")
+        alg.setProperty("InputWorkspace", self._input_ws)
+        alg.setProperty("SplitterWorkspace", self._split_ws)
+        alg.setProperty("InformationWorkspace", self._info_ws)
+        alg.setProperty("OutputWorkspaceBaseName", self._output_ws_group_name)
+        alg.setProperty("GroupWorkspaces", True)
+        alg.setProperty("FilterByPulseTime", False)
+        alg.setProperty("OutputWorkspaceIndexedFrom1", True)
+        alg.setProperty("CorrectionToSample", "None")
+        alg.setProperty("SpectrumWithoutDetector", "Skip")
+        alg.setProperty("SplitSampleLogs", False)
+        alg.setProperty("OutputTOFCorrectionWorkspace", "__mock")
+        alg.setProperty("ExcludeSpecifiedLogs", False)
+        alg.setProperty("TimeSeriesPropertyLogs", 'proton_charge')
+        alg.execute()
+        return mtd[self._output_ws_group_name]
 
-    def _scale_monitors_for_each_slice(self):
+    def _scale_monitors_for_each_slice(self, sliced_ws_group):
         """Create a group workspace which contains a copy of the monitors workspace for
         each slice, scaled by the relative proton charge for that slice"""
+        input_monitor_ws = self.getProperty("MonitorWorkspace").value
         total_proton_charge = self._total_proton_charge()
-        monitors_list = list()
+        monitors_ws_list = []
         i=1
-        for slice in mtd[self._sliced_output_ws_group_name]:
-            slice_monitor_ws_name = self._input_monitor_ws_name + '_'+str(i)
-            CloneWorkspace(InputWorkspace=self._input_monitor_ws_name,
-                           OutputWorkspace=slice_monitor_ws_name)
+        for slice in sliced_ws_group:
+            slice_monitor_ws_name = input_monitor_ws.getName() + '_'+str(i)
+            slice_monitor_ws = self._clone_workspace(input_monitor_ws, slice_monitor_ws_name)
             scale_factor = slice.run().getProtonCharge() / total_proton_charge
-            Scale(InputWorkspace=slice_monitor_ws_name,OutputWorkspace=slice_monitor_ws_name,
-                  Factor=scale_factor)
-            monitors_list.append(slice_monitor_ws_name)
+            slice_monitor_ws = self._scale_workspace(slice_monitor_ws, slice_monitor_ws_name,
+                                                     scale_factor)
+            # The workspace must be in the ADS for grouping
+            mtd.addOrReplace(slice_monitor_ws_name, slice_monitor_ws)
+            monitors_ws_list.append(slice_monitor_ws_name)
             i+=1
-        self._sliced_monitor_ws_group_name = self._input_monitor_ws_name + '_sliced'
-        GroupWorkspaces(InputWorkspaces=monitors_list,
-                        OutputWorkspace=self._sliced_monitor_ws_group_name)
+
+        self._monitor_ws_group_name = input_monitor_ws.getName() + '_sliced'
+        self._monitor_ws_group = self._group_workspaces(monitors_ws_list, self._monitor_ws_group_name)
+        mtd.addOrReplace(self._monitor_ws_group_name, self._monitor_ws_group)
+
+    def _clone_workspace(self, ws_to_clone, output_ws_name):
+        alg = self.createChildAlgorithm("CloneWorkspace")
+        alg.setProperty("InputWorkspace", ws_to_clone)
+        alg.setProperty("OutputWorkspace", output_ws_name)
+        alg.execute()
+        return alg.getProperty("OutputWorkspace").value
+
+    def _scale_workspace(self, ws_to_scale, output_ws_name, scale_factor):
+        alg = self.createChildAlgorithm("Scale")
+        alg.setProperty("InputWorkspace", ws_to_scale)
+        alg.setProperty("OutputWorkspace", output_ws_name)
+        alg.setProperty("Factor", scale_factor)
+        alg.execute()
+        return alg.getProperty("OutputWorkspace").value
+
+    def _group_workspaces(self, ws_list, output_ws_name):
+        alg = self.createChildAlgorithm("GroupWorkspaces")
+        alg.setProperty("InputWorkspaces", ws_list)
+        alg.setProperty("OutputWorkspace", output_ws_name)
+        alg.execute()
+        return alg.getProperty("OutputWorkspace").value
 
     def _total_proton_charge(self):
         """Get the proton charge for the input workspace"""
-        return mtd[self._input_ws_name].run().getProtonCharge()
+        return self._input_ws.run().getProtonCharge()
 
     def _rebin_to_monitors(self):
         """Rebin the output workspace group to the monitors workspace group"""
-        RebinToWorkspace(WorkspaceToRebin=self._sliced_output_ws_group_name,
-                         WorkspaceToMatch=self._sliced_monitor_ws_group_name,
-                         OutputWorkspace=self._sliced_output_ws_group_name,PreserveEvents=False)
+        alg = self.createChildAlgorithm("RebinToWorkspace")
+        alg.setProperty("WorkspaceToRebin", self._output_ws_group_name)
+        alg.setProperty("WorkspaceToMatch", self._monitor_ws_group_name)
+        alg.setProperty("OutputWorkspace", self._output_ws_group_name)
+        alg.setProperty("PreserveEvents", False)
+        alg.execute()
+        return alg.getProperty("OutputWorkspace").value
 
     def _add_monitors_to_sliced_output(self):
         """Add the monitors for each slice to the output workspace for each slice"""
-        AppendSpectra(InputWorkspace1=self._sliced_monitor_ws_group_name,
-                      InputWorkspace2=self._sliced_output_ws_group_name,
-                      MergeLogs=True, OutputWorkspace=self._sliced_output_ws_group_name)
-
-    def _set_outputs(self):
-        """Set the output property values for this algorithm"""
-        self.setPropertyValue("OutputWorkspace", self._sliced_output_ws_group_name)
+        alg = self.createChildAlgorithm("AppendSpectra")
+        alg.setProperty("InputWorkspace1", self._monitor_ws_group_name)
+        alg.setProperty("InputWorkspace2", self._output_ws_group_name)
+        alg.setProperty("MergeLogs", True)
+        alg.setProperty("OutputWorkspace", self._output_ws_group_name)
+        alg.execute()
+        return alg.getProperty("OutputWorkspace").value
 
     def _clean_up(self):
-        AnalysisDataService.remove(self._mock_ws_name)
-        
+        """Remove worspaces added to the ADS"""
+        monitor_ws_names = [ws.getName() for ws in self._monitor_ws_group]
+        alg = self.createChildAlgorithm("UnGroupWorkspace")
+        alg.setProperty("InputWorkspace", self._monitor_ws_group_name)
+        alg.execute()
+        for ws_name in monitor_ws_names:
+            mtd.remove(ws_name)
+
 AlgorithmFactory.subscribe(ReflectometrySliceEventWorkspace())
