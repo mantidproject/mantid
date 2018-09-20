cc3d0022f07309e1c81921d187baf790fc32a8e0
Samuel Jones <samjones714@gmail.com>
2018-09-20 15:41:09 +0100

    Re #23564 Make it clean up after itself

--- MantidPlot/test/MantidPlotProjectRecovery.py
+++ MantidPlot/test/MantidPlotProjectRecovery.py
@@ -12,27 +12,43 @@ from PyQt4 import QtGui
 import shutil
 import _qti
 
+path = os.path.join(ConfigService.getAppDataDirectory(), 'recovery', platform.node())
+
+def cleanUp():
+    # Clean Up
+    for cc in glob(os.path.join(path, "*", "")):
+        shutil.rmtree(cc)
+    AnalysisDataService.clear()
 
 class MantidPlotProjectRecovery(unittest.TestCase):
+
+
 
     def test_exec(self):
         CreateWorkspace(OutputWorkspace="ws", DataX=[
                         1, 2, 3], DataY=[1, 2, 3], NSpec=1)
         threadsafe_call(_qti.app.saveRecoveryCheckpoint)
-        AnalysisDataService.clear()
+        
+        cleanUp()
+        #Test that it cleaned up after itself
+        self.assertEqual(len(glob(os.path.join(path, "*", ""))), 0)
 
     def test_checkpoint_creation(self):
         CreateWorkspace(OutputWorkspace="ws", DataX=[
                         1, 2, 3], DataY=[1, 2, 3], NSpec=1)
-        path = os.path.join(
-            ConfigService.getAppDataDirectory(), 'recovery', platform.node())
+
         listOfCheckpointsBefore = glob(os.path.join(path, "*", ""))
         for ii in listOfCheckpointsBefore:
             shutil.rmtree(ii)
+
         threadsafe_call(_qti.app.saveRecoveryCheckpoint)
-        AnalysisDataService.clear()
-        listOfCheckpointsAfter = glob(os.path.join(path, "*", ""))
-        self.assertTrue(len(listOfCheckpointsAfter) == 1)
+        listOfCheckpointsAfter = glob(os.path.join(path, "*", "")) 
+
+        self.assertEqual(len(listOfCheckpointsAfter), 1)
+
+        cleanUp()
+        #Test that it cleaned up after itself
+        self.assertEqual(len(glob(os.path.join(path, "*", ""))), 0)
 
 
 # Run the unit tests
