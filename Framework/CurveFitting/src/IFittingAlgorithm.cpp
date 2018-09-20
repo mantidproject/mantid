4f74b5beb6bbea6e3bb5c82e06cb41bb26b965d4
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-09-20 09:51:28 +0100

    Use ordered ties in Fit. Re #22925

--- Framework/CurveFitting/src/IFittingAlgorithm.cpp
+++ Framework/CurveFitting/src/IFittingAlgorithm.cpp
@@ -303,6 +303,7 @@ void IFittingAlgorithm::declareCostFunctionProperty() {
 boost::shared_ptr<CostFunctions::CostFuncFitting>
 IFittingAlgorithm::getCostFunctionInitialized() const {
   // Function may need some preparation.
+  m_function->makeOrderedTies();
   m_function->setUpForFit();
 
   API::FunctionDomain_sptr domain;
