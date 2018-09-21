163a3fa80ca69b701c928f95bf88893cf00de428
Igor Gudich <igor.gudich@esss.se>
2018-09-21 15:50:08 +0200

    Minor corrections against warnings
    re #23268

--- Framework/Parallel/src/IO/EventLoaderChild.cpp
+++ Framework/Parallel/src/IO/EventLoaderChild.cpp
@@ -10,9 +10,9 @@ using namespace Mantid::Types;
 int main(int argc, char **argv) {
   const std::string segmentName(argv[1]);
   const std::string storageName(argv[2]);
-  unsigned procId = std::atoi(argv[3]);
-  std::size_t firstEvent = std::atoll(argv[4]);
-  std::size_t upperEvent = std::atoll(argv[5]);
+//  unsigned procId = std::atoi(argv[3]);
+  unsigned firstEvent = std::atoi(argv[4]);
+  unsigned upperEvent = std::atoi(argv[5]);
   unsigned numPixels = std::atoi(argv[6]);
   std::size_t size = std::atoll(argv[7]);
   const std::string fileName(argv[8]);
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
 
   std::vector<std::string> bankNames;
   std::vector<int32_t> bankOffsets;
-  for (unsigned i = 11; i < argc; i += 2) {
+  for (int i = 11; i < argc; i += 2) {
     bankNames.emplace_back(argv[i]);
     bankOffsets.emplace_back(std::atoi(argv[i + 1]));
   }
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
                                   false);
 
   MultiProcessEventLoader::fillFromFile(storage, fileName, groupName, bankNames,
-                                        bankOffsets, firstEvent, upperEvent, true);
+                                        bankOffsets, firstEvent, upperEvent, precalcEvents);
 
   return 0;
 }
