a1bd9c8d9840cdf656e1f8ec93ec30ddb9e80164
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-09-21 15:32:22 +0100

    Re #23574 Stl reader working

--- Framework/DataHandling/inc/MantidDataHandling/LoadBinStl.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadBinStl.h
@@ -1,18 +1,35 @@
 #ifndef MANTID_DATAHANDLING_LOADBINSTL_H_
 #define MANTID_DATAHANDLING_LOADBINSTL_H_
-#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/IFileLoader.h"
 #include "MantidKernel/BinaryStreamReader.h"
 #include <MantidKernel/V3D.h>
 namespace Mantid {
 namespace DataHandling {
 
-class DLLExport LoadBinStl : public Mantid::API::Algorithm {
+class DLLExport LoadBinStl : public API::IFileLoader<Kernel::FileDescriptor> {
 public:
+  /// Algorithm's name
+  const std::string name() const override;
+  /// Algorithm's version
+  int version() const override;
+  /// Algorithm's category for identification
+  const std::string category() const override;
+
+  const std::string summary() const override;
+
+  /// Returns a confidence value that this algorithm can load a file
+  int confidence(Kernel::FileDescriptor &descriptor) const {return 0;};
+  /// Returns a value indicating whether or not loader wants to load multiple
+  /// files into a single workspace
+  bool loadMutipleAsOne() { return false; }
+
 private:
+  void init() override;
+  void exec() override;
   void readStl(std::string filename);
   void readTriangle(Kernel::BinaryStreamReader);
-  std::vector<uint16_t> triangle;
-  std::vector<Kernel::V3D> verticies;
+  std::vector<uint16_t> m_triangle;
+  std::vector<Kernel::V3D> m_verticies;
 };
 } // namespace DataHandling
 } // namespace Mantid
