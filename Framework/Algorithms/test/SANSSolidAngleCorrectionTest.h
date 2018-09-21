68aceebe152cdeb0a57214b19c3091629504269d
Jose Borreguero <borreguero@gmail.com>
2018-09-21 14:13:24 -0400

    Refs #23590 apply clang correction to SANSSolidAngleCorrectionTest

--- Framework/Algorithms/test/SANSSolidAngleCorrectionTest.h
+++ Framework/Algorithms/test/SANSSolidAngleCorrectionTest.h
@@ -7,13 +7,13 @@
 #ifndef SANSSOLIDANGLECORRECTIONTEST_H_
 #define SANSSOLIDANGLECORRECTIONTEST_H_
 
-#include <cxxtest/TestSuite.h>
-#include "MantidAlgorithms/SANSSolidAngleCorrection.h"
 #include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/Axis.h"
-#include "MantidKernel/Unit.h"
+#include "MantidAlgorithms/SANSSolidAngleCorrection.h"
 #include "MantidDataHandling/LoadSpice2D.h"
 #include "MantidDataHandling/MoveInstrumentComponent.h"
+#include "MantidKernel/Unit.h"
+#include <cxxtest/TestSuite.h>
 
 using namespace Mantid::API;
 using namespace Mantid::Kernel;
@@ -90,9 +90,9 @@ public:
     // Get the coordinate of the detector pixel
     double iy = (i - nmon) % 192;
     double ix = (int)((i - nmon) / 192);
-    double r = sqrt(1.0 +
-                    5.15 * 5.15 / 6000 / 6000 * ((ix - 16.0) * (ix - 16.0) +
-                                                 (iy - 95.0) * (iy - 95.0)));
+    double r =
+        sqrt(1.0 + 5.15 * 5.15 / 6000 / 6000 *
+                       ((ix - 16.0) * (ix - 16.0) + (iy - 95.0) * (iy - 95.0)));
     double corr = r * r * r;
     double ratio =
         ws2d_out->dataY(130 + nmon)[0] / ws2d_in->dataY(130 + nmon)[0];
