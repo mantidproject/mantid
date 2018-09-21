79876d9cb36ba1b9da7cda30f40a254f982d3125
Igor Gudich <igor.gudich@esss.se>
2018-09-21 14:25:01 +0200

    Add some comments to functions
    re #23268

--- Framework/Parallel/src/IO/MultiProcessEventLoader.cpp
+++ Framework/Parallel/src/IO/MultiProcessEventLoader.cpp
@@ -15,17 +15,19 @@ namespace Mantid {
 namespace Parallel {
 namespace IO {
 
+/// Constructor
 MultiProcessEventLoader::MultiProcessEventLoader(unsigned int numPixels,
                                                  unsigned int numProcesses,
                                                  unsigned int numThreads,
                                                  const std::string &binary,
                                                  bool precalc)
-    : m_precalculateEvents(precalc),
-      m_numPixels(numPixels), m_numProcesses(numProcesses),
-      m_numThreads(numThreads), m_binaryToLaunch(binary),
+    : m_precalculateEvents(precalc), m_numPixels(numPixels),
+      m_numProcesses(numProcesses), m_numThreads(numThreads),
+      m_binaryToLaunch(binary),
       m_segmentNames(GenerateSegmentsName(numProcesses)),
       m_storageName(GenerateStoragename()) {}
 
+/// Generates "unique" shared memory segment name
 std::vector<std::string>
 MultiProcessEventLoader::GenerateSegmentsName(unsigned procNum) {
   std::vector<std::string> res(procNum,
@@ -37,10 +39,12 @@ MultiProcessEventLoader::GenerateSegmentsName(unsigned procNum) {
   return res;
 }
 
+/// Generates "unique" shared memory storage structure name
 std::string MultiProcessEventLoader::GenerateStoragename() {
   return GenerateTimeBasedPrefix() + "_mantid_multiprocess_NXloader_storage";
 }
 
+/// Generates "unique" prefix for shared memory stuff
 std::string MultiProcessEventLoader::GenerateTimeBasedPrefix() {
   auto now = std::chrono::system_clock::now();
   auto in_time_t = std::chrono::system_clock::to_time_t(now);
@@ -59,8 +63,6 @@ void MultiProcessEventLoader::load(
     std::vector<std::vector<Types::Event::TofEvent> *> eventLists) const {
 
   try {
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
     H5::H5File file(filename.c_str(), H5F_ACC_RDONLY);
     auto instrument = file.openGroup(groupname);
 
@@ -73,7 +75,7 @@ void MultiProcessEventLoader::load(
 
     std::vector<bp::child> vChilds;
 
-    //prepare command for launching of parallel processes
+    // prepare command for launching of parallel processes
     for (unsigned i = 1; i < m_numProcesses; ++i) {
       std::size_t upperBound =
           i < m_numProcesses - 1 ? evPerPr * (i + 1) : numEvents;
@@ -89,7 +91,9 @@ void MultiProcessEventLoader::load(
       command += std::to_string(storageSize) + " "; // memory size
       command += filename + " ";                    // nexus file name
       command += groupname + " ";                   // instrument group name
-      command += m_precalculateEvents ? "1 " : "0 ";// variant of algorithm used for loading
+      command += m_precalculateEvents
+                 ? "1 "
+                 : "0 "; // variant of algorithm used for loading
       for (unsigned j = 0; j < bankNames.size(); ++j) {
         command += bankNames[j] + " ";                   // bank name
         command += std::to_string(bankOffsets[j]) + " "; // bank size
@@ -103,28 +107,20 @@ void MultiProcessEventLoader::load(
       }
     }
 
+    // Do self piece of multiprocess work
     EventsListsShmemStorage storage(m_segmentNames[0], m_storageName,
                                     storageSize, 1, m_numPixels, false);
     fillFromFile(storage, filename, groupname, bankNames, bankOffsets, 0,
                  numEvents / m_numProcesses, m_precalculateEvents);
 
-    //waiting for child processes
+    // waiting for child processes
     for (auto &c : vChilds)
       c.wait();
 
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Multiprocess: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()
-              << std::endl;
-
-    begin = std::chrono::steady_clock::now();
-
+    // Assemble multiprocess data from shared memory
     assembleFromShared(eventLists);
 
-    end = std::chrono::steady_clock::now();
-    std::cout << "Multithreading: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()
-              << std::endl;
-
-    //clean up shared memory
+    // clean up shared memory
     for (const auto &name : m_segmentNames)
       ip::shared_memory_object::remove(name.c_str());
   } catch (std::exception const &ex) {
@@ -152,11 +148,13 @@ void MultiProcessEventLoader::assembleFromShared(
                 .first);
       }
 
-      const unsigned portion{std::max<unsigned>(m_numPixels / m_numThreads / 3, 1)};
-      for (unsigned startPixel = cnt.fetch_add(portion); startPixel < m_numPixels;
-           startPixel = cnt.fetch_add(portion)) {
+      const unsigned portion{
+          std::max<unsigned>(m_numPixels / m_numThreads / 3, 1)};
+      for (unsigned startPixel = cnt.fetch_add(portion);
+           startPixel < m_numPixels; startPixel = cnt.fetch_add(portion)) {
 
-        for (auto pixel = startPixel; pixel < std::min(startPixel + portion, m_numPixels); ++pixel) {
+        for (auto pixel = startPixel;
+             pixel < std::min(startPixel + portion, m_numPixels); ++pixel) {
           auto &res = result[pixel];
           for (unsigned i = 0; i < m_numProcesses; ++i) {
             for (auto &chunk : *chunksPtrs[i]) {
@@ -177,7 +175,8 @@ void MultiProcessEventLoader::assembleFromShared(
 void MultiProcessEventLoader::fillFromFile(
     EventsListsShmemStorage &storage, const std::string &filename,
     const std::string &groupname, const std::vector<std::string> &bankNames,
-    const std::vector<int32_t> &bankOffsets, unsigned from, unsigned to, bool precalc) {
+    const std::vector<int32_t> &bankOffsets, unsigned from, unsigned to,
+    bool precalc) {
   H5::H5File file(filename.c_str(), H5F_ACC_RDONLY);
   auto instrument = file.openGroup(groupname);
 
@@ -186,58 +185,58 @@ void MultiProcessEventLoader::fillFromFile(
 
   if (precalc) {
     if (type == H5::PredType::NATIVE_INT32)
-      return GroupLoader<LoadType::preCalcEvents>::
-      loadFromGroup<int32_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::preCalcEvents>::loadFromGroup<int32_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_INT64)
-      return GroupLoader<LoadType::preCalcEvents>::
-      loadFromGroup<int64_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::preCalcEvents>::loadFromGroup<int64_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_UINT32)
-      return GroupLoader<LoadType::preCalcEvents>::
-      loadFromGroup<uint32_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::preCalcEvents>::loadFromGroup<uint32_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_UINT64)
-      return GroupLoader<LoadType::preCalcEvents>::
-      loadFromGroup<uint64_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::preCalcEvents>::loadFromGroup<uint64_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_FLOAT)
-      return GroupLoader<LoadType::preCalcEvents>::
-      loadFromGroup<float>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::preCalcEvents>::loadFromGroup<float>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_DOUBLE)
-      return GroupLoader<LoadType::preCalcEvents>::
-      loadFromGroup<double>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::preCalcEvents>::loadFromGroup<double>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     throw std::runtime_error(
         "Unsupported H5::DataType for event_time_offset in NXevent_data");
   } else {
     if (type == H5::PredType::NATIVE_INT32)
-      return GroupLoader<LoadType::producerConsumer>::
-      loadFromGroup<int32_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::producerConsumer>::loadFromGroup<int32_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_INT64)
-      return GroupLoader<LoadType::producerConsumer>::
-      loadFromGroup<int64_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::producerConsumer>::loadFromGroup<int64_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_UINT32)
-      return GroupLoader<LoadType::producerConsumer>::
-      loadFromGroup<uint32_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::producerConsumer>::loadFromGroup<uint32_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_UINT64)
-      return GroupLoader<LoadType::producerConsumer>::
-      loadFromGroup<uint64_t>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::producerConsumer>::loadFromGroup<uint64_t>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_FLOAT)
-      return GroupLoader<LoadType::producerConsumer>::
-      loadFromGroup<float>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::producerConsumer>::loadFromGroup<float>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     if (type == H5::PredType::NATIVE_DOUBLE)
-      return GroupLoader<LoadType::producerConsumer>::
-      loadFromGroup<double>(storage, instrument, bankNames, bankOffsets, from, to);
+      return GroupLoader<LoadType::producerConsumer>::loadFromGroup<double>(
+          storage, instrument, bankNames, bankOffsets, from, to);
     throw std::runtime_error(
         "Unsupported H5::DataType for event_time_offset in NXevent_data");
   }
 }
 
 // Estimates the memory amount for shared memory segments
-// vector representing each pixel allocated only once, so we have allocationFee bytes extra overhead
+// vector representing each pixel allocated only once, so we have allocationFee
+// bytes extra overhead
 size_t MultiProcessEventLoader::estimateShmemAmount(size_t eventCount) const {
   unsigned allocationFee = 8 + 8 + GenerateStoragename().length();
-  std::size_t len{(eventCount / m_numProcesses + eventCount % m_numProcesses)
-                      * sizeof(TofEvent)
-                      + m_numPixels *
-                          (sizeof(EventLists) + allocationFee)
-                      + sizeof(Chunks) + allocationFee};
+  std::size_t len{(eventCount / m_numProcesses + eventCount % m_numProcesses) *
+      sizeof(TofEvent) +
+      m_numPixels * (sizeof(EventLists) + allocationFee) +
+      sizeof(Chunks) + allocationFee};
   return len;
 }
 
