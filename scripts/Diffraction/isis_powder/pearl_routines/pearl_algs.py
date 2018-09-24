28843ffdaf571ccb181bdea171db9e35dd0028bd
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-09-24 09:58:58 +0100

    Re #23579 made clear that rebin to first spectrum is deliberate

--- scripts/Diffraction/isis_powder/pearl_routines/pearl_algs.py
+++ scripts/Diffraction/isis_powder/pearl_routines/pearl_algs.py
@@ -29,7 +29,7 @@ def apply_vanadium_absorb_corrections(van_ws, run_details, absorb_ws=None):
     absorb_units = absorb_ws.getAxis(0).getUnit().unitID()
     if van_original_units != absorb_units:
         van_ws = mantid.ConvertUnits(InputWorkspace=van_ws, Target=absorb_units, OutputWorkspace=van_ws)
-
+    # This deliberately matches only to the first spectrum of absorb_ws
     # ensure uniform binning across spectra
     # extract the binning parameters from the first spectrum.
     # there is probably a better way to calculate the binning parameters, but this
@@ -42,7 +42,6 @@ def apply_vanadium_absorb_corrections(van_ws, run_details, absorb_ws=None):
             params.append(xaxis[i + 1] - x)  # delta
     van_ws = mantid.Rebin(InputWorkspace=van_ws, Params=params)
 
-    #van_ws = mantid.RebinToWorkspace(WorkspaceToRebin=van_ws, WorkspaceToMatch=absorb_ws, OutputWorkspace=van_ws)
     van_ws = mantid.Divide(LHSWorkspace=van_ws, RHSWorkspace=absorb_ws, OutputWorkspace=van_ws)
 
     if van_original_units != absorb_units:
