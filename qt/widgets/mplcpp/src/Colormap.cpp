1e622e28a8f3c35125a1ac4fcac0eacc5987e482
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2018-09-24 14:01:55 +0100

    Add C++ Colormap wrapper for cm.Colormap instance

---
+++ qt/widgets/mplcpp/src/Colormap.cpp
@@ -0,0 +1,34 @@
+#include "MantidQtWidgets/MplCpp/Colormap.h"
+#include "MantidQtWidgets/MplCpp/Colors.h"
+
+#include "MantidPythonInterface/core/ErrorHandling.h"
+
+using Mantid::PythonInterface::PythonRuntimeError;
+
+namespace MantidQt {
+namespace Widgets {
+namespace MplCpp {
+
+/**
+ * @brief Construct a Colormap object given a name
+ */
+Colormap::Colormap(Python::Object obj)
+    : Python::InstanceHolder(obj, "is_gray") {}
+
+/**
+ * @param name The name of an existing colormap.
+ * @return A new Colormap instance for the named map
+ * @throws std::invalid_argument if the name is unknown
+ */
+Colormap getCMap(const QString &name) {
+  static auto cmModule{Python::NewRef(PyImport_ImportModule("matplotlib.cm"))};
+  try {
+    return cmModule.attr("get_cmap")(name.toLatin1().constData());
+  } catch (Python::ErrorAlreadySet &) {
+    throw PythonRuntimeError();
+  }
+}
+
+} // namespace MplCpp
+} // namespace Widgets
+} // namespace MantidQt
