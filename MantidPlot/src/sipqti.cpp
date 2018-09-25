mantidproject/mantid

commit deb07b56a3369516aca6e7b719877bc25cb49e76

Fix dependency rules for MantidPlot sip files
MSVC was not copying sipqti.cpp.in when it was updated. This
simplifies the rule set to avoid needing to copy it.

MantidPlot/CMakeLists.txt
@@ -410,8 +410,7 @@ include ( UseQt4 )
 include_directories ( ${PYTHON_INCLUDE_PATH} )
 
 set ( SIP_SPEC ${CMAKE_CURRENT_SOURCE_DIR}/src/qti.sip )
-set ( SIP_SRC_IN ${CMAKE_CURRENT_SOURCE_DIR}/src/sipqti.cpp.in )
-set ( SIP_SRC ${CMAKE_CURRENT_BINARY_DIR}/sipqti.cpp )
+set ( SIP_SRC ${CMAKE_CURRENT_SOURCE_DIR}/src/sipqti.cpp )
 set ( SIP_SRC_AUTO sip_qtipart0.cpp )
 
 
@@ -458,12 +457,6 @@ add_custom_command ( OUTPUT ${SIP_SRC_AUTO}
                           ${SIP_SPEC}
                      DEPENDS ${SIP_INCLUDE_DIR}/sip.h src/qti.sip ${QTI_SIP_HDRS} ../qt/python/mantidqtpython/mantidqtpython_def.sip
                      COMMENT "Generating python bindings using sip"
-)
-
-add_custom_command ( OUTPUT ${SIP_SRC}
-                     COMMAND ${CMAKE_COMMAND} ARGS -E copy_if_different ${SIP_SRC_IN} ${SIP_SRC}
-                     DEPENDS ${SIP_SRC_IN}
-                     COMMENT ""
 )
 
 # Needed for sip.h header that can end up in a different place to to the main Python include directory

MantidPlot/src/qti.sip
@@ -37,10 +37,6 @@
   #endif
 %End
 
-%ModuleHeaderCode
-#include "MantidPythonInterface/core/VersionCompat.h"
-%End
-
 %Import QtCore/QtCoremod.sip
 %Import QtGui/QtGuimod.sip
 %Import mantidqtpython.sip

MantidPlot/src/sipqti.cpp
@@ -6,4 +6,6 @@
 //------------------------------------------------------------------------------
 #include "MantidPythonInterface/core/VersionCompat.h"
 
+// this file will be autogenerated by the sip generation process
+// and appear in the binary directory
 #include "sip_qtipart0.cpp"

commit 5a2a8abeb484aa2e1d52a5f7caad618a44a26c0b

Fix sip generation rules for MantidPlot
They were broken in deb07b56a3369516aca6e7b719877bc25cb49e76
when fixing another issue with the rules. This simplifies
the rules so that the compilation is directly performed
on the auto-generated sip file with any warnings being ignored
in the sip definition

MantidPlot/CMakeLists.txt
@@ -409,11 +409,6 @@ include ( UseQt4 )
 
 include_directories ( ${PYTHON_INCLUDE_PATH} )
 
-set ( SIP_SPEC ${CMAKE_CURRENT_SOURCE_DIR}/src/qti.sip )
-set ( SIP_SRC ${CMAKE_CURRENT_SOURCE_DIR}/src/sipqti.cpp )
-set ( SIP_SRC_AUTO sip_qtipart0.cpp )
-
-
 # We need to manually add all the headers that are in qti.sip
 # so that the dependencies are known to CMake
 set ( QTI_SIP_HDRS src/MdiSubWindow.h
@@ -443,14 +438,11 @@ set( SRC_UNITY_IGNORE_FILES )
 ###########################################################################
 # Sip generated files
 ###########################################################################
+set ( SIP_SPEC ${CMAKE_CURRENT_SOURCE_DIR}/src/qti.sip )
+set ( SIP_SRC_AUTO sip_qtipart0.cpp )
 
-# The code generated by sip causes compiler warnings therefore the
-# generated file is wrapped by ${SIP_SRC} and these warnings are
-# disabled. In order for VS2010 to to this correctly the second
-# custom command below is required along with the committed
-# src/sipqti.cpp.rule file.
 set ( MANTIDQTPYTHON_SIP_INCLUDES -I ${CMAKE_CURRENT_BINARY_DIR}/../qt/python/mantidqtpython -I ${CMAKE_CURRENT_SOURCE_DIR}/../qt/python/mantidqtpython )
-add_custom_command ( OUTPUT ${SIP_SRC_AUTO}
+add_custom_command ( OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${SIP_SRC_AUTO}
                      COMMAND ${SIP_EXECUTABLE}
                           -I ${PYQT4_SIP_DIR} ${MANTIDQTPYTHON_SIP_INCLUDES}  ${PYQT4_SIP_FLAGS}
                           -c ${CMAKE_CURRENT_BINARY_DIR} -j1 -w -o
@@ -639,7 +631,7 @@ set_source_files_properties ( ${CMAKE_CURRENT_BINARY_DIR}/qtcolorpicker.moc
                              PROPERTIES HEADER_FILE_ONLY true )
 set ( MOCCED_FILES ${MOCCED_FILES} ${CMAKE_CURRENT_BINARY_DIR}/qtcolorpicker.moc )
 
-set ( SRC_FILES ${QTIPLOT_SRCS} ${MANTID_SRCS} ${SIP_SRC} )
+set ( SRC_FILES ${QTIPLOT_SRCS} ${MANTID_SRCS} ${SIP_SRC_AUTO} )
 set ( INC_FILES ${QTIPLOT_HDRS} ${MANTID_HDRS} )
 if ( WIN32 )
   set ( MANIFEST_FILES MantidPlot.manifest )

MantidPlot/src/qti.sip
@@ -32,9 +32,10 @@
 %Module _qti
 
 %UnitCode
-  #if defined(__GNUC__) && !(defined(__INTEL_COMPILER))
-    #pragma GCC system_header
-  #endif
+#include "MantidKernel/WarningSuppressions.h"
+#include "MantidPythonInterface/core/VersionCompat.h"
+GNU_DIAG_OFF("conversion")
+GNU_DIAG_OFF("suggest-override")
 %End
 
 %Import QtCore/QtCoremod.sip

MantidPlot/src/sipqti.cpp
@@ -1,11 +1,0 @@
-//------------------------------------------------------------------------------
-// A wrapper for the auto-generated sipqtipart?.cpp files to disable warnings
-// that we can't control. The warnings are actually suppressed in qti.sip
-// with '#pragma GCC system_header' but that pragma only works if it occurs
-// in a file that has been included with '#include'
-//------------------------------------------------------------------------------
-#include "MantidPythonInterface/core/VersionCompat.h"
-
-// this file will be autogenerated by the sip generation process
-// and appear in the binary directory
-#include "sip_qtipart0.cpp"