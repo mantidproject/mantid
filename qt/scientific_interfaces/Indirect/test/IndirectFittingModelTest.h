69cc4d56460fc2616dac1fd2dfb175d42fd3eb78
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2018-09-27 16:33:01 +0100

    Start unit tests Refs #23463

--- qt/scientific_interfaces/Indirect/test/IndirectFittingModelTest.h
+++ qt/scientific_interfaces/Indirect/test/IndirectFittingModelTest.h
@@ -3,7 +3,30 @@
 
 #include <cxxtest/TestSuite.h>
 
-#include <iostream>
+#include "IndirectFitData.h"
+#include "IndirectFittingModel.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidDataObjects/Workspace2D.h"
+#include "MantidTestHelpers/WorkspaceCreationHelper.h"
+
+using namespace Mantid::API;
+using namespace Mantid::DataObjects;
+using namespace MantidQt::CustomInterfaces::IDA;
+
+namespace {
+
+/// Simple class to set up the ADS with the configuration required
+struct SetUpADSWithWorkspace {
+
+  SetUpADSWithWorkspace(std::string const &inputWSName,
+                        Workspace2D_sptr const &workspace) {
+    AnalysisDataService::Instance().addOrReplace(inputWSName, workspace);
+  };
+
+  ~SetUpADSWithWorkspace() { AnalysisDataService::Instance().clear(); };
+};
+
+} // namespace
 
 class IndirectFittingModelTest : public CxxTest::TestSuite {
 public:
@@ -13,7 +36,40 @@ public:
 
   static void destroySuite(IndirectFittingModelTest *suite) { delete suite; }
 
-  void test_test() { std::cout << "here"; }
+  void test_data_is_instantiated_correctly() {}
+
+  void test_workspace_is_stored_correctly_in_the_ADS() {
+    auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
+    SetUpADSWithWorkspace ads("WorkspaceName", workspace);
+
+    TS_ASSERT(AnalysisDataService::Instance().doesExist("WorkspaceName"));
+    MatrixWorkspace_sptr storedWorkspace =
+        boost::dynamic_pointer_cast<MatrixWorkspace>(
+            AnalysisDataService::Instance().retrieve("WorkspaceName"));
+    TS_ASSERT_EQUALS(storedWorkspace->getNumberHistograms(), 3);
+  }
+
+  void test_addWorkspace_will_add_a_workspace_to_the_fittingData_correctly() {
+    auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
+    SetUpADSWithWorkspace ads("WorkspaceName", workspace);
+
+    m_model->addWorkspace("WorkspaceName");
+
+    TS_ASSERT_EQUALS(m_model->getWorkspace(0), workspace);
+  }
+
+  void
+  test_nullptr_is_returned_when_getWorkspace_provided_out_of_range_index() {
+    auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
+    SetUpADSWithWorkspace ads("WorkspaceName", workspace);
+
+    m_model->addWorkspace("WorkspaceName");
+
+    TS_ASSERT_EQUALS(m_model->getWorkspace(1), nullptr);
+  }
+
+private:
+  IndirectFittingModel *m_model;
 };
 
 #endif
