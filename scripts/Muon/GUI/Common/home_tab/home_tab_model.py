bf94a1bfc2df347eb68d4fead72652873ae1ee4e
T Jubb <t.w.jubb@gmail.com>
2018-09-27 15:18:24 +0100

    Refs #23642 Add files.

---
+++ scripts/Muon/GUI/Common/home_tab/home_tab_model.py
@@ -0,0 +1,24 @@
+from __future__ import (absolute_import, division, print_function)
+
+# import mantid.simpleapi as mantid
+from Muon.GUI.Common.muon_context import MuonContext
+
+
+class HomeTabModel(object):
+
+    def __init__(self, muon_data=MuonContext()):
+        self._data = muon_data
+
+    def is_data_loaded(self):
+        return self._data.is_data_loaded()
+
+    def loaded_instrument(self):
+        return self._data.instrument
+
+    def show_all_data(self):
+        self._data.show_raw_data()
+        self._data.show_all_groups()
+        self._data.show_all_pairs()
+
+    def update_current_data(self):
+        self._data.update_current_data()
---
+++ scripts/Muon/GUI/Common/home_tab/home_tab_view.py
@@ -0,0 +1,50 @@
+from __future__ import (absolute_import, division, print_function)
+
+from PyQt4 import QtGui, QtCore
+
+
+class HomeTabView(QtGui.QWidget):
+
+    def __init__(self, parent=None,
+                 instrument_widget=None,
+                 grouping_widget=None,
+                 plot_widget=None,
+                 run_info_widget=None):
+        super(HomeTabView, self).__init__(parent)
+
+        self._instrument_widget = instrument_widget
+        self._grouping_widget = grouping_widget
+        self._plot_widget = plot_widget
+        self._run_info_widget = run_info_widget
+
+        self.splitter = None
+        self.vertical_layout = None
+
+        self.setup_interface()
+
+    def setup_interface(self):
+        self.setObjectName("HomeTab")
+        self.setWindowTitle("Home Tab")
+        self.resize(500, 100)
+
+        self.splitter = QtGui.QSplitter(QtCore.Qt.Vertical)
+
+        self.vertical_layout = QtGui.QVBoxLayout(self)
+
+        self.splitter.addWidget(self._instrument_widget)
+        self.splitter.addWidget(self._grouping_widget)
+        self.splitter.addWidget(self._plot_widget)
+        self.splitter.addWidget(self._run_info_widget)
+        self.splitter.setCollapsible(0, False)
+        self.splitter.setCollapsible(1, False)
+        self.splitter.setCollapsible(2, False)
+        self.splitter.setCollapsible(3, False)
+        self.splitter.setHandleWidth(2)
+        self.setStyleSheet("QSplitter::handle {background-color: #3498DB}")
+
+        self.vertical_layout.addWidget(self.splitter)
+        self.setLayout(self.vertical_layout)
+
+    # for docking
+    def getLayout(self):
+        return self.vertical_layout
