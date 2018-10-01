7513d5bbaf8249c5515b429e73c9ef1dcd7e1a42
Nick Draper <nick.draper@stfc.ac.uk>
2018-10-01 13:58:50 +0100

    remove double copyrifght entry
    re #23488

--- qt/widgets/common/inc/MantidQtWidgets/Common/InterfaceFactory.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/InterfaceFactory.h
@@ -79,26 +79,6 @@ using AlgorithmDialogFactory =
 
     @author Martyn Gigg, Tessella plc
     @date 06/07/2010
-
-    Copyright &copy; 2009 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
-   National Laboratory & European Spallation Source
-
-    This file is part of Mantid.
-
-    Mantid is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    Mantid is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    File change history is stored at: <https://github.com/mantidproject/mantid>
 */
 class EXPORT_OPT_MANTIDQT_COMMON UserSubWindowFactoryImpl
     : public Mantid::Kernel::DynamicFactory<UserSubWindow> {
