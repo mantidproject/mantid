758a22742bb0570be86a8e42dd22e11e23dbf5ec
Tom Jubb <t.w.jubb@gmail.com>
2018-10-01 15:06:49 +0100

    Refs #23642 Dark blue for splitter handles.

--- scripts/Muon/GUI/Common/home_tab/home_tab_view.py
+++ scripts/Muon/GUI/Common/home_tab/home_tab_view.py
@@ -5,21 +5,10 @@ from PyQt4 import QtGui, QtCore
 
 class HomeTabView(QtGui.QWidget):
 
-    def __init__(self, parent=None,
-                 widget_list=None
-                 # instrument_widget=None,
-                 # grouping_widget=None,
-                 # plot_widget=None,
-                 # run_info_widget=None
-                 ):
+    def __init__(self, parent=None,widget_list=None):
         super(HomeTabView, self).__init__(parent)
 
         self._widget_list = widget_list
-
-        # self._instrument_widget = instrument_widget
-        # self._grouping_widget = grouping_widget
-        # self._plot_widget = plot_widget
-        # self._run_info_widget = run_info_widget
 
         self.splitter = None
         self.vertical_layout = None
@@ -40,16 +29,8 @@ class HomeTabView(QtGui.QWidget):
                 self.splitter.addWidget(widget)
                 self.splitter.setCollapsible(i, False)
 
-        # self.splitter.addWidget(self._instrument_widget)
-        # self.splitter.addWidget(self._grouping_widget)
-        # self.splitter.addWidget(self._plot_widget)
-        # self.splitter.addWidget(self._run_info_widget)
-        # self.splitter.setCollapsible(0, False)
-        # self.splitter.setCollapsible(1, False)
-        # self.splitter.setCollapsible(2, False)
-        # self.splitter.setCollapsible(3, False)
         self.splitter.setHandleWidth(2)
-        self.setStyleSheet("QSplitter::handle {background-color: #3498DB}")
+        self.setStyleSheet("QSplitter::handle {background-color: darkBlue}")
 
         self.vertical_layout.addWidget(self.splitter)
         self.setLayout(self.vertical_layout)
