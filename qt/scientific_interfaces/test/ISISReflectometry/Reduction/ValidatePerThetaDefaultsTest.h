616c5e90c2d33e593cedd07344de4ec3787ed1fd
Gemma Guest <gemma.guest@stfc.ac.uk>
2018-10-03 17:48:17 +0100

    Add unit tests for per-theta defaults validation
    Re #22263

--- qt/scientific_interfaces/test/ISISReflectometry/Reduction/ValidatePerThetaDefaultsTest.h
+++ qt/scientific_interfaces/test/ISISReflectometry/Reduction/ValidatePerThetaDefaultsTest.h
@@ -16,9 +16,91 @@ public:
     delete suite;
   }
 
-  static auto constexpr TOLERANCE = 0.000001;
+  void testParseTheta() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"1.3"});
+    TS_ASSERT(result.isValid());
+    TS_ASSERT(result.assertValid().thetaOrWildcard().is_initialized());
+    TS_ASSERT_EQUALS(result.assertValid().thetaOrWildcard().get(), 1.3);
+  }
 
-  // TODO
-  void testThing() {}
+  void testParseThetaWildcard() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({""});
+    TS_ASSERT(result.isValid());
+    TS_ASSERT(!result.assertValid().thetaOrWildcard().is_initialized());
+  }
+
+  void testParseThetaError() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"bad"});
+    std::vector<int> errorCells = {0};
+    TS_ASSERT(result.isError());
+    TS_ASSERT_EQUALS(result.assertError(), errorCells);
+  }
+
+  void testParseTransmissionRuns() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "13463", "13464"});
+    auto expected = TransmissionRunPair("13463", "13464");
+    TS_ASSERT(result.isValid());
+    TS_ASSERT_EQUALS(result.assertValid().transmissionWorkspaceNames(),
+                     expected);
+  }
+
+  void testParseTransmissionRunsError() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "bad", "bad"});
+    std::vector<int> errorCells = {1, 2};
+    TS_ASSERT(result.isError());
+    TS_ASSERT_EQUALS(result.assertError(), errorCells);
+  }
+
+  void testParseQRange() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "", "", "0.05", "1.3", "0.02"});
+    TS_ASSERT(result.isValid());
+    TS_ASSERT_EQUALS(result.assertValid().qRange(), RangeInQ(0.05, 0.02, 1.3));
+  }
+
+  void testParseQRangeError() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "", "", "bad", "bad", "bad"});
+    std::vector<int> errorCells = {3, 4, 5};
+    TS_ASSERT(result.isError());
+    TS_ASSERT_EQUALS(result.assertError(), errorCells);
+  }
+
+  void testParseScaleFactor() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "", "", "", "", "", "1.4"});
+    TS_ASSERT(result.isValid());
+    TS_ASSERT_EQUALS(result.assertValid().scaleFactor(), 1.4);
+  }
+
+  void testParseScaleFactorError() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "", "", "", "", "", "bad"});
+    std::vector<int> errorCells = {6};
+    TS_ASSERT(result.isError());
+    TS_ASSERT_EQUALS(result.assertError(), errorCells);
+  }
+
+  void testParseProcessingInstructions() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "", "", "", "", "", "", "1-3"});
+    TS_ASSERT(result.isValid());
+    TS_ASSERT(result.assertValid().processingInstructions().is_initialized());
+    TS_ASSERT_EQUALS(result.assertValid().processingInstructions().get(),
+                     "1-3");
+  }
+
+  void testParseProcessingInstructionsError() {
+    PerThetaDefaultsValidator validator;
+    auto result = validator({"", "", "", "", "", "", "", "bad"});
+    std::vector<int> errorCells = {7};
+    TS_ASSERT(result.isError());
+    TS_ASSERT_EQUALS(result.assertError(), errorCells);
+  }
 };
 #endif // MANTID_CUSTOMINTERFACES_VALIDATEPERTHETADEFAULTSTEST_H_
