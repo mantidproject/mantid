523642cf08272f490b3db198cda30eb2a93755e1
mantid-builder <mantid-buildserver@mantidproject.org>
2018-10-04 00:56:18 +0100

    clang-format PR23713 80a663a26

--- Framework/DataHandling/inc/MantidDataHandling/LoadEMU.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadEMU.h
@@ -5,97 +5,97 @@
 // Includes
 //---------------------------------------------------
 
-#include "MantidAPI/IFileLoader.h"
-#include "MantidAPI/LogManager.h"
-#include "MantidGeometry/Instrument.h"
-#include "MantidDataObjects/EventWorkspace.h"
-#include "MantidNexus/NexusClasses.h"
 #include "LoadANSTOEventFile.h"
 #include "LoadANSTOHelper.h"
+#include "MantidAPI/IFileLoader.h"
+#include "MantidAPI/LogManager.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidGeometry/Instrument.h"
+#include "MantidNexus/NexusClasses.h"
 
 namespace Mantid {
 namespace DataHandling {
-	/*
-	Loads an Emu data file. Implements API::IFileLoader and its file check methods
-	to recognise a file as the one containing Emu data.
+/*
+Loads an Emu data file. Implements API::IFileLoader and its file check methods
+to recognise a file as the one containing Emu data.
 
-	Based on the LoadBBY implementation for which the original authors were
-	David Mannicke (ANSTO), Anders Markvardsen (ISIS), Roman Tolchenov
-	(Tessella plc)
+Based on the LoadBBY implementation for which the original authors were
+David Mannicke (ANSTO), Anders Markvardsen (ISIS), Roman Tolchenov
+(Tessella plc)
 
-	@author Geish Miladinovic (ANSTO)
-	@date 26/07/2018
+@author Geish Miladinovic (ANSTO)
+@date 26/07/2018
 
-	Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
-	National Laboratory & European Spallation Source
+Copyright &copy; 2010 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
+National Laboratory & European Spallation Source
 
-	This file is part of Mantid.
+This file is part of Mantid.
 
-	Mantid is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 3 of the License, or
-	(at your option) any later version.
+Mantid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
-	Mantid is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
+Mantid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	File change history is stored at: <https://github.com/mantidproject/mantid>
-	Code Documentation is available at: <http://doxygen.mantidproject.org>
-	*/
+File change history is stored at: <https://github.com/mantidproject/mantid>
+Code Documentation is available at: <http://doxygen.mantidproject.org>
+*/
 
-	class DLLExport LoadEMU : public API::IFileLoader<Kernel::FileDescriptor> {
+class DLLExport LoadEMU : public API::IFileLoader<Kernel::FileDescriptor> {
 
-		DataObjects::EventWorkspace_sptr m_localWorkspace;
-		std::vector<double>	m_detectorL2;
+  DataObjects::EventWorkspace_sptr m_localWorkspace;
+  std::vector<double> m_detectorL2;
 
-	public:
-		// description
-		int version() const override { return 1; }
-		const std::vector<std::string> seeAlso() const override {
-			return{ "Load", "LoadQKK" };
-		}
-		const std::string name() const override { return "LoadEMU"; }
-		const std::string category() const override { return "DataHandling\\ANSTO"; }
-		const std::string summary() const override {
-			return "Loads an EMU data file into a workspace.";
-		}
+public:
+  // description
+  int version() const override { return 1; }
+  const std::vector<std::string> seeAlso() const override {
+    return {"Load", "LoadQKK"};
+  }
+  const std::string name() const override { return "LoadEMU"; }
+  const std::string category() const override { return "DataHandling\\ANSTO"; }
+  const std::string summary() const override {
+    return "Loads an EMU data file into a workspace.";
+  }
 
-		// returns a confidence value that this algorithm can load a specified file
-		int confidence(Kernel::FileDescriptor &descriptor) const override;
+  // returns a confidence value that this algorithm can load a specified file
+  int confidence(Kernel::FileDescriptor &descriptor) const override;
 
-	protected:
-		// initialisation
-		void init() override;
-		// execution
-		void exec() override;
+protected:
+  // initialisation
+  void init() override;
+  // execution
+  void exec() override;
 
-	private:
-		// region of intreset
-		static std::vector<bool> createRoiVector(const std::string& seltubes, const std::string& maskfile);
+private:
+  // region of intreset
+  static std::vector<bool> createRoiVector(const std::string &seltubes,
+                                           const std::string &maskfile);
 
-		// load parameters
-		void loadParameters(ANSTO::Tar::File &tarFile, API::LogManager &logm);
+  // load parameters
+  void loadParameters(ANSTO::Tar::File &tarFile, API::LogManager &logm);
 
-		// create workspace
-		void createWorkspace(ANSTO::Tar::File& tarFile);
+  // create workspace
+  void createWorkspace(ANSTO::Tar::File &tarFile);
 
-		void loadDetectorL2Values();
-		void updateNeutronicPostions(int detID, double sampleAnalyser);
+  void loadDetectorL2Values();
+  void updateNeutronicPostions(int detID, double sampleAnalyser);
 
-		// binary file access
-		template <class EventProcessor>
-		static void loadEvents(API::Progress &prog, const char *progMsg,
-			ANSTO::Tar::File &tarFile, EventProcessor &eventProcessor);
-	};
+  // binary file access
+  template <class EventProcessor>
+  static void loadEvents(API::Progress &prog, const char *progMsg,
+                         ANSTO::Tar::File &tarFile,
+                         EventProcessor &eventProcessor);
+};
 
-} // DataHandling
-} // Mantid
+} // namespace DataHandling
+} // namespace Mantid
 
 #endif // DATAHANDING_LOADEMU_H_
-
-
--- Framework/DataHandling/test/LoadEMUauTest.h
+++ Framework/DataHandling/test/LoadEMUauTest.h
@@ -30,57 +30,60 @@ public:
   }
 
   void test_load_emu_algorithm() {
-	  LoadEMU algToBeTested;
+    LoadEMU algToBeTested;
 
-	  if (!algToBeTested.isInitialized())
-		  algToBeTested.initialize();
+    if (!algToBeTested.isInitialized())
+      algToBeTested.initialize();
 
-	  std::string outputSpace = "LoadEMUauTest";
-	  algToBeTested.setPropertyValue("OutputWorkspace", outputSpace);
+    std::string outputSpace = "LoadEMUauTest";
+    algToBeTested.setPropertyValue("OutputWorkspace", outputSpace);
 
-	  // should fail because mandatory parameter has not been set
-	  TS_ASSERT_THROWS(algToBeTested.execute(), std::runtime_error);
+    // should fail because mandatory parameter has not been set
+    TS_ASSERT_THROWS(algToBeTested.execute(), std::runtime_error);
 
-	  // should succeed now
-	  std::string inputFile = "EMU0006330.tar";
-	  algToBeTested.setPropertyValue("Filename", inputFile);
-	  algToBeTested.setPropertyValue("SelectDetectorTubes", "16-50");
-	  TS_ASSERT_THROWS_NOTHING(algToBeTested.execute());
-	  TS_ASSERT(algToBeTested.isExecuted());
+    // should succeed now
+    std::string inputFile = "EMU0006330.tar";
+    algToBeTested.setPropertyValue("Filename", inputFile);
+    algToBeTested.setPropertyValue("SelectDetectorTubes", "16-50");
+    TS_ASSERT_THROWS_NOTHING(algToBeTested.execute());
+    TS_ASSERT(algToBeTested.isExecuted());
 
-	  // get workspace generated
-	  MatrixWorkspace_sptr output =
-		  AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(
-			  outputSpace);
+    // get workspace generated
+    MatrixWorkspace_sptr output =
+        AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(
+            outputSpace);
 
-	  // check number of histograms
-	  TS_ASSERT_EQUALS(output->getNumberHistograms(), 6528);
-	  double sum = 0.0;
-	  for (size_t i = 0; i < output->getNumberHistograms(); i++)
-		  sum += output->readY(i)[0];
-	  TS_ASSERT_EQUALS(sum, 55126);
+    // check number of histograms
+    TS_ASSERT_EQUALS(output->getNumberHistograms(), 6528);
+    double sum = 0.0;
+    for (size_t i = 0; i < output->getNumberHistograms(); i++)
+      sum += output->readY(i)[0];
+    TS_ASSERT_EQUALS(sum, 55126);
 
-	  // check that all required log values are there
-	  auto run = output->run();
+    // check that all required log values are there
+    auto run = output->run();
 
-	  // test start and end time
-	  TS_ASSERT(
-		  run.getProperty("start_time")->value().compare("2018-07-26T10:13:12") ==
-		  0)
-		  TS_ASSERT(
-			  run.getProperty("end_time")->value().find("2018-07-26T10:17:12.6") ==
-			  0)
+    // test start and end time
+    TS_ASSERT(
+        run.getProperty("start_time")->value().compare("2018-07-26T10:13:12") ==
+        0)
+    TS_ASSERT(
+        run.getProperty("end_time")->value().find("2018-07-26T10:17:12.6") == 0)
 
-		  // test some data properties
-		  auto logpm = [&run](std::string tag) { return run.getPropertyValueAsType<double>(tag); };
-	  TS_ASSERT_DELTA(logpm("DopplerFrequency"), 9.974, 1.0e-3);
-	  TS_ASSERT_DELTA(logpm("DopplerAmplitude"), 0.075, 1.0e-4);
+    // test some data properties
+    auto logpm = [&run](std::string tag) {
+      return run.getPropertyValueAsType<double>(tag);
+    };
+    TS_ASSERT_DELTA(logpm("DopplerFrequency"), 9.974, 1.0e-3);
+    TS_ASSERT_DELTA(logpm("DopplerAmplitude"), 0.075, 1.0e-4);
 
-	  // test some instrument parameters
-	  auto instr = output->getInstrument();
-	  auto iparam = [&instr](std::string tag) { return instr->getNumberParameter(tag)[0]; };
-	  TS_ASSERT_DELTA(iparam("AnalysedV2"), 630.866, 1.0e-3);
-	  TS_ASSERT_DELTA(iparam("SampleAnalyser"), 1.8, 1.0e-3);
+    // test some instrument parameters
+    auto instr = output->getInstrument();
+    auto iparam = [&instr](std::string tag) {
+      return instr->getNumberParameter(tag)[0];
+    };
+    TS_ASSERT_DELTA(iparam("AnalysedV2"), 630.866, 1.0e-3);
+    TS_ASSERT_DELTA(iparam("SampleAnalyser"), 1.8, 1.0e-3);
   }
 };
 
