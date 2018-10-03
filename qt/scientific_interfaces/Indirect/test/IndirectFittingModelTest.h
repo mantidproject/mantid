a46b8e6f110e0625372273d7a296c50e4fa729e5
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2018-10-03 11:36:38 +0100

    Refs #23463. Unit Tests

--- qt/scientific_interfaces/Indirect/test/IndirectFittingModelTest.h
+++ qt/scientific_interfaces/Indirect/test/IndirectFittingModelTest.h
@@ -9,11 +9,22 @@
 #include "MantidDataObjects/Workspace2D.h"
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
 
+#include <iostream>
+
 using namespace Mantid::API;
 using namespace Mantid::DataObjects;
 using namespace MantidQt::CustomInterfaces::IDA;
 
 namespace {
+
+IndirectFitData getIndirectFitData(std::size_t const &numberOfSpectra,
+                                   std::size_t const &numberOfBins) {
+  auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(
+      numberOfSpectra, numberOfBins);
+  Spectra const spec = std::make_pair(0u, workspace->getNumberHistograms() - 1);
+  IndirectFitData data(workspace, spec);
+  return data;
+}
 
 /// Simple class to set up the ADS with the configuration required
 struct SetUpADSWithWorkspace {
@@ -26,6 +37,23 @@ struct SetUpADSWithWorkspace {
   ~SetUpADSWithWorkspace() { AnalysisDataService::Instance().clear(); };
 };
 
+class DummyModel : public IndirectFittingModel {
+public:
+  ~DummyModel(){};
+
+private:
+  std::string sequentialFitOutputName() const override { return ""; };
+  std::string simultaneousFitOutputName() const override { return ""; };
+  std::string singleFitOutputName(std::size_t index,
+                                  std::size_t spectrum) const override {
+    return "";
+  };
+};
+
+std::unique_ptr<DummyModel> getModel() {
+  return std::make_unique<DummyModel>();
+}
+
 } // namespace
 
 class IndirectFittingModelTest : public CxxTest::TestSuite {
@@ -36,7 +64,9 @@ public:
 
   static void destroySuite(IndirectFittingModelTest *suite) { delete suite; }
 
-  void test_data_is_instantiated_correctly() {}
+  void test_model_is_instantiated_correctly() {
+    // NOT FINISHED
+  }
 
   void test_workspace_is_stored_correctly_in_the_ADS() {
     auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
@@ -50,26 +80,59 @@ public:
   }
 
   void test_addWorkspace_will_add_a_workspace_to_the_fittingData_correctly() {
+    auto model = getModel();
     auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
     SetUpADSWithWorkspace ads("WorkspaceName", workspace);
 
-    m_model->addWorkspace("WorkspaceName");
+    model->addWorkspace("WorkspaceName");
 
-    TS_ASSERT_EQUALS(m_model->getWorkspace(0), workspace);
+    TS_ASSERT_EQUALS(model->getWorkspace(0), workspace);
   }
 
   void
-  test_nullptr_is_returned_when_getWorkspace_provided_out_of_range_index() {
+  test_nullptr_is_returned_when_getWorkspace_is_provided_an_out_of_range_index() {
+    auto model = getModel();
     auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
     SetUpADSWithWorkspace ads("WorkspaceName", workspace);
 
-    m_model->addWorkspace("WorkspaceName");
+    model->addWorkspace("WorkspaceName");
 
-    TS_ASSERT_EQUALS(m_model->getWorkspace(1), nullptr);
+    TS_ASSERT_EQUALS(model->getWorkspace(1), nullptr);
   }
 
-private:
-  IndirectFittingModel *m_model;
+  void
+  test_getSpectra_returns_a_correct_spectra_when_the_index_provided_is_valid() {
+    auto model = getModel();
+    auto const workspace = WorkspaceCreationHelper::create2DWorkspace123(3, 3);
+    SetUpADSWithWorkspace ads("WorkspaceName", workspace);
+
+    model->setSpectra("0-1", 0);
+    Spectra spec = std::make_pair(0u, 1u);
+
+    TS_ASSERT_EQUALS(model->getSpectra(0), spec);
+  }
+
+  void
+  test_getSpectra_returns_an_empty_DiscontinuousSpectra_when_provided_an_out_of_range_index() {
+  }
+
+  void
+  test_getFittingRange_returns_correct_range_when_provided_a_valid_index_and_spectrum() {
+  }
+
+  void
+  test_getFittingRange_returns_empty_range_when_provided_an_out_of_range_dataIndex() {
+  }
+
+  void test_getFittingRange_returns_empty_range_when_there_are_zero_spectra() {}
+
+  void
+  test_getFittingRange_returns_correct_range_when_the_fittingMode_is_sequential() {
+  }
+
+  void
+  test_getFittingRange_returns_correct_range_when_the_fittingMode_is_not_sequential() {
+  }
 };
 
 #endif
