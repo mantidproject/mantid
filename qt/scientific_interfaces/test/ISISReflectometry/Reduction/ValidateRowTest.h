1f23f1cd7035f0f08fe24ea8b68f07a847bab1ef
Gemma Guest <gemma.guest@stfc.ac.uk>
2018-10-05 10:58:19 +0100

    Update copyright notice for new files
    Re #22263

--- qt/scientific_interfaces/test/ISISReflectometry/Reduction/ValidateRowTest.h
+++ qt/scientific_interfaces/test/ISISReflectometry/Reduction/ValidateRowTest.h
@@ -1,3 +1,9 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
 #ifndef MANTID_CUSTOMINTERFACES_VALIDATEROWTEST_H_
 #define MANTID_CUSTOMINTERFACES_VALIDATEROWTEST_H_
 #include "../../../ISISReflectometry/Parse.h"
