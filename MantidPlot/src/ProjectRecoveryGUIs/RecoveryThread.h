ea73c87b94383446fc9e64ce8321184b5e1d8f9f
Samuel Jones <samjones714@gmail.com>
2018-10-08 13:03:59 +0100

    Re #23570 clang-formatting

--- MantidPlot/src/ProjectRecoveryGUIs/RecoveryThread.h
+++ MantidPlot/src/ProjectRecoveryGUIs/RecoveryThread.h
@@ -18,7 +18,7 @@ class RecoveryThread : public QThread {
   Q_OBJECT
 
 public:
-  RecoveryThread() {m_projRec = nullptr}
+  RecoveryThread() { m_projRec = nullptr }
   bool getFailedRun();
   void setCheckpoint(Poco::Path checkpoint);
   void setProjRecPtr(MantidQt::ProjectRecovery *projectRec);
