43101e7d94552af919292c78aa72d41069fbe7ed
igudich <igor.gudich@esss.se>
2018-10-09 11:28:49 +0200

    Update newly added files with new copyright message.
    re #23268

--- Framework/Parallel/inc/MantidParallel/IO/EventsListsShmemManager.h
+++ Framework/Parallel/inc/MantidParallel/IO/EventsListsShmemManager.h
@@ -1,3 +1,9 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
 #ifndef MANTID_PARALLEL_EVENTSLISTSSHMEMMANAGER_H_
 #define MANTID_PARALLEL_EVENTSLISTSSHMEMMANAGER_H_
 
@@ -41,27 +47,6 @@ namespace IO {
 
   @author Igor Gudich
   @date 2018
-
-  Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
-  National Laboratory & European Spallation Source
-
-  This file is part of Mantid.
-
-  Mantid is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
-
-  Mantid is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-  File change history is stored at: <https://github.com/mantidproject/mantid>
-  Code Documentation is available at: <http://doxygen.mantidproject.org>
 */
 
 using SegmentManager = ip::managed_shared_memory::segment_manager;
