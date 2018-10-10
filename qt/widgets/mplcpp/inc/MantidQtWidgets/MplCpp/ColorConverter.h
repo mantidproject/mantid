8ef06e7f6c2a541153644ca41ec68aab2a7ecbc4
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2018-10-10 17:32:35 +0100

    Add/update copyright headers for new files

--- qt/widgets/mplcpp/inc/MantidQtWidgets/MplCpp/ColorConverter.h
+++ qt/widgets/mplcpp/inc/MantidQtWidgets/MplCpp/ColorConverter.h
@@ -1,21 +1,12 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
 #ifndef MPLCPP_COLORCONVERTER_H
 #define MPLCPP_COLORCONVERTER_H
-/*
- Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory, NScD Oak Ridge
- National Laboratory & European Spallation Source
 
- This file is part of Mantid.
-
- Mantid is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
-
- Mantid is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-*/
 #include "MantidQtWidgets/MplCpp/DllConfig.h"
 #include "MantidQtWidgets/MplCpp/Python/Object.h"
 
