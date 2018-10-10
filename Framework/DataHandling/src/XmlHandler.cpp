15d7955fadae5474161bab4202640295ca575855
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-10-10 13:43:12 -0400

    Re #000 trimmed values in metadata

--- Framework/DataHandling/src/XmlHandler.cpp
+++ Framework/DataHandling/src/XmlHandler.cpp
@@ -5,6 +5,7 @@
 //     & Institut Laue - Langevin
 // SPDX - License - Identifier: GPL - 3.0 +
 #include <algorithm>
+#include <boost/algorithm/string/trim.hpp>
 #include <iostream>
 #include <iterator>
 #include <string>
@@ -44,8 +45,6 @@ XmlHandler::get_metadata(const std::vector<std::string> &tags_to_ignore) {
   Poco::XML::NodeIterator it(pDoc, Poco::XML::NodeFilter::SHOW_ELEMENT);
   Poco::XML::Node *pNode = it.nextNode();
 
-  std::begin(tags_to_ignore);
-
   while (pNode) {
     if (pNode->childNodes()->length() == 1 &&
         std::find(std::begin(tags_to_ignore), std::end(tags_to_ignore),
@@ -53,6 +52,7 @@ XmlHandler::get_metadata(const std::vector<std::string> &tags_to_ignore) {
       std::string key =
           pNode->parentNode()->nodeName() + "/" + pNode->nodeName();
       std::string value = pNode->innerText();
+      boost::algorithm::trim(value);
       metadata.emplace(key, value);
     }
     pNode = it.nextNode();
