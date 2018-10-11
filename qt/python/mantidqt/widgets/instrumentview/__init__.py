c18adc004071067e9de69a29d4a335fa383673f9
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2018-10-11 22:23:57 +0100

    Add instrument view plugin wrapper to workbench
    Launch instrument view from context menu

--- qt/python/mantidqt/widgets/instrumentview/__init__.py
+++ qt/python/mantidqt/widgets/instrumentview/__init__.py
@@ -7,16 +7,24 @@
 #  This file is part of the mantidqt package
 #
 #
+"""
+You can run this widget independently by for example:
+
+    from mantidqt.widgets.instrumentview.presenter import InstrumentView
+    from mantid.simpleapi import Load
+    from qtpy.QtWidgets import QApplication
+
+    ws=Load('CNCS_7860')
+
+    app = QApplication([])
+    window = InstrumentView(ws)
+    app.exec_()
+"""
 from __future__ import (absolute_import, unicode_literals)
 
 # 3rdparty imports
 from qtpy import PYQT4
-# local imports
-from mantidqt.utils.qt import import_qt
 
 
 if PYQT4:
     raise ImportError("Instrument view requires Qt >= v5")
-# import widget class from C++ wrappers
-InstrumentWidget = import_qt('._instrumentview', 'mantidqt.widgets.instrumentview',
-                             'InstrumentWidget')
