2a7a04eb2cdff1c8938034e83af77f1f407657c0
Tom Jubb <t.w.jubb@gmail.com>
2018-10-12 10:05:02 +0100

    Refs #23767 Add message box.

--- scripts/Muon/GUI/Common/message_box.py
+++ scripts/Muon/GUI/Common/message_box.py
@@ -1,13 +1,7 @@
-# Mantid Repository : https://github.com/mantidproject/mantid
-#
-# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
-#     NScD Oak Ridge National Laboratory, European Spallation Source
-#     & Institut Laue - Langevin
-# SPDX - License - Identifier: GPL - 3.0 +
 import PyQt4.QtGui as QtGui
 
 
-def warning(error):
-
-    ex = QtGui.QWidget()
-    QtGui.QMessageBox.warning(ex, "Error", str(error))
+def warning(error, parent=None):
+    if not parent:
+        parent = QtGui.QWidget()
+    QtGui.QMessageBox.warning(parent, "Error", str(error))
