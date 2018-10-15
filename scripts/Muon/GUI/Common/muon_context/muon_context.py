bc597992de189279fc748ec327f203009618dbd5
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-10-15 10:42:44 +0100

    refs #23413 copyright added to muon context

--- scripts/Muon/GUI/Common/muon_context/muon_context.py
+++ scripts/Muon/GUI/Common/muon_context/muon_context.py
@@ -1,3 +1,10 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
 # Muon context - contains all of the values from the GUI
 from __future__ import (absolute_import, division, print_function)
 from Muon.GUI.Common import group_object
