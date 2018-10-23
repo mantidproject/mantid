c93611b205b323b56346c45aad6d76974cebf603
Andrei Savici <saviciat@ornl.gov>
2018-10-23 18:42:43 -0400

    Re #23320. Read initial values for limits

--- Framework/MDAlgorithms/src/RecalculateTrajectoriesExtents.cpp
+++ Framework/MDAlgorithms/src/RecalculateTrajectoriesExtents.cpp
@@ -7,14 +7,16 @@
 
 #include "MantidMDAlgorithms/RecalculateTrajectoriesExtents.h"
 #include "MantidAPI/IMDEventWorkspace.h"
+#include "MantidGeometry/MDGeometry/QSample.h"
+#include "MantidKernel/ArrayProperty.h"
+#include "MantidAPI/Run.h"
+#include "MantidAPI/SpectrumInfo.h"
 
 namespace Mantid {
 namespace MDAlgorithms {
-using Mantid::Kernel::Direction;
-using Mantid::Kernel::make_unique;
-using Mantid::API::WorkspaceProperty;
-using Mantid::API::IMDEventWorkspace;
-using Mantid::API::IMDEventWorkspace_sptr;
+using namespace Mantid::Kernel;
+using namespace Mantid::API;
+
 // Register the algorithm into the AlgorithmFactory
 DECLARE_ALGORITHM(RecalculateTrajectoriesExtents)
 
@@ -42,7 +44,7 @@ const std::string RecalculateTrajectoriesExtents::summary() const {
 void RecalculateTrajectoriesExtents::init() {
   declareProperty(make_unique<WorkspaceProperty<IMDEventWorkspace>>(
                         "InputWorkspace", "", Direction::Input),
-                    "An input MDEventWorkspace.");
+                    "An input MDEventWorkspace. Must be in Q_sample frame.");
   declareProperty(make_unique<WorkspaceProperty<IMDEventWorkspace>>(
                         "OutputWorkspace", "", Direction::Output),
                     "Copy of the input MDEventWorkspace with the corrected trajectory extents.");
@@ -51,22 +53,20 @@ void RecalculateTrajectoriesExtents::init() {
 /// Validate the input workspace @see Algorithm::validateInputs
 std::map<std::string, std::string> RecalculateTrajectoriesExtents::validateInputs() {
   std::map<std::string, std::string> errorMessage;
-/*
-  // Check for input workspace type
-  Mantid::API::IMDWorkspace_sptr inputWS = this->getProperty("InputWorkspace");
-  if (!boost::dynamic_pointer_cast<Mantid::API::IMDHistoWorkspace>(inputWS) &&
-      !boost::dynamic_pointer_cast<Mantid::API::IMDEventWorkspace>(inputWS)) {
+
+  // Check for input workspace frame
+  Mantid::API::IMDEventWorkspace_sptr inputWS = this->getProperty("InputWorkspace");
+  if (inputWS->getNumDims() < 3) {
     errorMessage.emplace("InputWorkspace",
-                         "Only MDHisto or MDEvent workspaces can be saved.");
+                         "The input workspace must be at least 3D");
+  } else {
+    for (size_t i=0; i<3;i++){
+      if (inputWS->getDimension(i)->getMDFrame().name() != Mantid::Geometry::QSample::QSampleName){
+        errorMessage.emplace("InputWorkspace",
+                             "The input workspace must be in Q_sample");
+      }
+    }
   }
-
-  // Check for the dimensionality
-  if (!saver->is3DWorkspace(*inputWS)) {
-    errorMessage.emplace("InputWorkspace", "The MD workspace must be 3D.");
-  }
-
-  // Check for file location
-  */
   return errorMessage;
 }
 
@@ -77,11 +77,66 @@ std::map<std::string, std::string> RecalculateTrajectoriesExtents::validateInput
 void RecalculateTrajectoriesExtents::exec() {
   IMDEventWorkspace_sptr inWS = getProperty("InputWorkspace");
   IMDEventWorkspace_sptr outWS = getProperty("OutputWorkspace");
+
   // If input and output workspaces are not the same, create a new workspace for
   // the output
   if (outWS != inWS) {
     outWS = inWS->clone();
   }
+
+  //check if using diffraction or direct inelastic
+  bool diffraction(true);
+  double Ei(0.0);
+  if (outWS->getNumDims() > 3){
+      if (outWS->getDimension(3)->getMDFrame().name() == "DeltaE") {
+        diffraction = false;
+        if (outWS->getExperimentInfo(0)->run().hasProperty("Ei")) {
+          Property *eiprop = outWS->getExperimentInfo(0)->run().getProperty("Ei");
+          Ei = boost::lexical_cast<double>(eiprop->value());
+        } else {
+          throw std::runtime_error("Workspace contains energy transfer axis, but no Ei."
+                                   "The MD normalization workflow is not implemented for "
+                                   "indirect geometry");
+        }
+      }
+  }
+
+  size_t nExperimentInfos = outWS->getNumExperimentInfo();
+  if (nExperimentInfos >1) {
+    g_log.warning("More than one experiment info. On merged workspaces, the "
+                  "limits recalculations might be wrong");
+  }
+  for (size_t i=0; i<nExperimentInfos; i++){
+    const auto &currentExptInfo = *(outWS->getExperimentInfo(static_cast<uint16_t>(i)));
+    // get instrument and goniometer
+    const auto &spectrumInfo = currentExptInfo.spectrumInfo();
+    const int64_t nspectra = static_cast<int64_t>(spectrumInfo.size());
+    std::vector<double> lowValues, highValues;
+
+    if (currentExptInfo.run().hasProperty("MDNorm_low")){
+      using VectorDoubleProperty = Kernel::PropertyWithValue<std::vector<double>>;
+      auto *lowValuesLog = dynamic_cast<VectorDoubleProperty *>(
+          currentExptInfo.getLog("MDNorm_low"));
+          lowValues = (*lowValuesLog)();
+    } else {
+      double minValue=currentExptInfo.run().getBinBoundaries().front();
+      lowValues = std::vector<double>(nspectra, minValue);
+    }
+    if (currentExptInfo.run().hasProperty("MDNorm_high")){
+      using VectorDoubleProperty = Kernel::PropertyWithValue<std::vector<double>>;
+      auto *highValuesLog = dynamic_cast<VectorDoubleProperty *>(
+          currentExptInfo.getLog("MDNorm_high"));
+          highValues = (*highValuesLog)();
+    } else {
+      double maxValue=currentExptInfo.run().getBinBoundaries().back();
+      highValues = std::vector<double>(nspectra, maxValue);
+    }
+    g_log.warning()<<lowValues[0]<<"\n";
+    g_log.warning()<<highValues[0]<<"\n";
+
+    // loop over detectors
+  }
+
   setProperty("OutputWorkspace", outWS);
 }
 
