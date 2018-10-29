dc8ec50a3f7caece8cf12e10df41d66d4e0a5858
Dimitar Tasev <dimtasev@gmail.com>
2018-10-29 13:26:40 +0000

    Added copyright text, re #21593

--- qt/python/mantidqt/widgets/matrixworkspacedisplay/run.py
+++ qt/python/mantidqt/widgets/matrixworkspacedisplay/run.py
@@ -1,3 +1,12 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantid workbench.
+#
+#
 import os
 
 from qtpy.QtWidgets import QApplication
