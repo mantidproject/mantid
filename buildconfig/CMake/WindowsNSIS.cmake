23113049c28a38810502722f58bf7113f31d3d63
Dimitar Tasev <dimtasev@gmail.com>
2018-10-31 11:12:33 +0000

    Split WindowsNSIS Qt relevant configurations, re #23263

--- buildconfig/CMake/WindowsNSIS.cmake
+++ buildconfig/CMake/WindowsNSIS.cmake
@@ -121,28 +121,6 @@ set ( MISC_CORE_DIST_DLLS
     tbb_preview.dll
     zlib.dll
 )
-set ( QT4_DIST_DLLS
-    phonon4.dll
-    Qt3Support4.dll
-    QtCLucene4.dll
-    QtCore4.dll
-    QtDeclarative4.dll
-    QtDesigner4.dll
-    QtDesignerComponents4.dll
-    QtGui4.dll
-    QtHelp4.dll
-    QtMultimedia4.dll
-    QtNetwork4.dll
-    QtOpenGL4.dll
-    QtScript4.dll
-    QtScriptTools4.dll
-    QtSql4.dll
-    QtSvg4.dll
-    QtTest4.dll
-    QtWebKit4.dll
-    QtXml4.dll
-    QtXmlPatterns4.dll
-    qscintilla2.dll )
 
 set ( MISC_GUI_DIST_DLLS
     qwt5.dll
@@ -154,40 +132,8 @@ foreach( DLL ${BIN_DLLS} )
   install ( FILES ${THIRD_PARTY_DIR}/bin/${DLL} DESTINATION bin )
 endforeach()
 
-set ( QT4_INSTALL_PREFIX ${THIRD_PARTY_DIR}/lib/qt4 )
-foreach( DLL ${QT4_DIST_DLLS} )
-  install ( FILES ${QT4_INSTALL_PREFIX}/lib/${DLL} DESTINATION bin )
-endforeach()
-
-set ( QT5_DIST_DLLS
-    Qt5Core.dll
-    Qt5Gui.dll
-    Qt5Widgets.dll
-    Qt5PrintSupport.dll
-    Qt5Svg.dll
-    Qt5OpenGL.dll
-)
-
-set ( QT5_INSTALL_PREFIX ${THIRD_PARTY_DIR}/lib/qt5 )
-foreach( DLL ${QT5_DIST_DLLS} )
-  # Note: QT5 has its DLLs only in /bin, unlike QT4 where they are in both /lib and /bin
-  install ( FILES ${QT5_INSTALL_PREFIX}/bin/${DLL} DESTINATION bin )
-endforeach()
-
-install ( FILES ${QT5_INSTALL_PREFIX}/lib/qscintilla2_qt5.dll  DESTINATION bin )
-
-###########################################################################
-# Qt Plugins + qt.conf file
-###########################################################################
-install ( FILES ${CMAKE_CURRENT_SOURCE_DIR}/installers/WinInstaller/qt.conf DESTINATION bin )
-# imageformats
-set ( QT_PLUGINS_IMAGEFORMAT qgif4.dll qico4.dll qjpeg4.dll qmng4.dll qsvg4.dll qtga4.dll qtiff4.dll )
-set ( QT_PLUGIN_DIR ${QT4_INSTALL_PREFIX}/plugins )
-foreach( DLL ${QT_PLUGINS_IMAGEFORMAT} )
-  install ( FILES ${QT_PLUGIN_DIR}/imageformats/${DLL} DESTINATION plugins/qt4/imageformats )
-endforeach()
-# sqlite
-install ( FILES ${QT_PLUGIN_DIR}/sqldrivers/qsqlite4.dll DESTINATION plugins/qt4/sqldrivers )
+include( WindowsNSISQt4 )
+include( WindowsNSISQt5 )
 
 ###########################################################################
 # Startup files
