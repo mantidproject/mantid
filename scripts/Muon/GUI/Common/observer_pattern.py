f38f0bb00b053f448617dc0ac5ac4df67793e949
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-11-02 16:56:45 +0000

    Updated observer pattern for clarity Re #23642

--- scripts/Muon/GUI/Common/observer_pattern.py
+++ scripts/Muon/GUI/Common/observer_pattern.py
@@ -3,8 +3,8 @@ from __future__ import (absolute_import, division, print_function)
 
 class Observer(object):
     """
-    The Observer observes (or subscribes) Observables, when the Observable notifies its subscribers (the observers)
-    they will cal their update() method
+    The Observer subscribes to Observables, when the Observable notifies its subscribers (the observers)
+    they will call their update() method
     """
 
     def __init__(self):
@@ -20,18 +20,18 @@ class Observer(object):
 
 class Observable(object):
     """
-    The Observable is an object which may be observed by Observers. It maintains a list of observers (aka subscribers)
-    to it, and when needed, it will notify those subscribers.
+    The Observable is an object which may be subscribed to by Observers. It maintains a list of subscribers to it,
+    and when needed, it will notify those subscribers.
     """
 
     def __init__(self):
         self._subscribers = []
 
-    def add_subscriber(self, observer):
-        if not isinstance(observer, Observer):
+    def add_subscriber(self, observer_instance):
+        if not isinstance(observer_instance, Observer):
             raise AttributeError("Trying to add subscriber which is not Observer type.")
-        if observer not in self._subscribers:
-            self._subscribers.append(observer)
+        if observer_instance not in self._subscribers:
+            self._subscribers.append(observer_instance)
 
     def delete_subscriber(self, observer):
         self._subscribers.remove(observer)
