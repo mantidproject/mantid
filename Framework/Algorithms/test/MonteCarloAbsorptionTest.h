5a25a142af67e0faee496212cb11745483f4fd61
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-11-05 12:56:09 +0000

    Re #23807 applied clang formatting

--- Framework/Algorithms/test/MonteCarloAbsorptionTest.h
+++ Framework/Algorithms/test/MonteCarloAbsorptionTest.h
@@ -65,7 +65,8 @@ void addSample(Mantid::API::MatrixWorkspace_sptr ws,
     canShape->setMaterial(Material(
         "CanMaterial", PhysicalConstants::getNeutronAtom(26, 0), 0.01));
     auto can = boost::make_shared<Container>(canShape);
-    ws->mutableSample().setEnvironment(std::make_unique<SampleEnvironment>("can", can));
+    ws->mutableSample().setEnvironment(
+        std::make_unique<SampleEnvironment>("can", can));
   } else if (environment == Environment::UserBeamSize) {
     auto inst = ws->getInstrument();
     auto &pmap = ws->instrumentParameters();
