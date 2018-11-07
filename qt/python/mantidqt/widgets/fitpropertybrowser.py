0f41c98a891fa4b84d4c7dace7637e66ab5ce52d
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-11-07 16:05:19 +0000

    Pure python wrapper for FitPropertyBrowser. Re #23920

---
+++ qt/python/mantidqt/widgets/fitpropertybrowser.py
@@ -0,0 +1,22 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2017 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantidqt package
+#
+#
+from __future__ import (absolute_import, unicode_literals)
+
+from mantidqt.utils.qt import import_qt
+
+
+BaseBrowser = import_qt('.._common', 'mantidqt.widgets', 'FitPropertyBrowser')
+
+
+class FitPropertyBrowser(BaseBrowser):
+
+    def __init__(self, parent=None):
+        super(FitPropertyBrowser, self).__init__(parent)
+        self.init()
