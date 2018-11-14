6c20a38f7798ca2a7d7ae289eb0b6a62c746b78d
Dimitar Tasev <dimtasev@gmail.com>
2018-11-14 14:08:17 +0000

    Remove fullpath from samplelogs __main__, re #21593

--- qt/python/mantidqt/widgets/samplelogs/__main__.py
+++ qt/python/mantidqt/widgets/samplelogs/__main__.py
@@ -14,7 +14,7 @@ from mantidqt.widgets.samplelogs.presenter import SampleLogs
 from mantid.simpleapi import Load
 from qtpy.QtWidgets import QApplication
 
-ws = Load(r'C:\Users\qbr77747\Downloads\SampleData-ISIS\HRP39187.RAW')
+ws = Load('HRP39187.RAW')
 
 app = QApplication([])
 window = SampleLogs(ws)
