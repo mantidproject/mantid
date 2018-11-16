1aa6ba5108bf66290df59eb5665ed7c2c29c9fa9
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-11-16 16:48:30 +0000

    Refactored muon_context Re #23642
    The workspace nameing functions were moved into their own module
    The pair and group calculation methods were moved into own module
    The default group and pair methods are moved out of class into free functions

--- scripts/Muon/GUI/Common/muon_context.py
+++ scripts/Muon/GUI/Common/muon_context.py
@@ -1,3 +1,10 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
 from __future__ import (absolute_import, division, print_function)
 
 import Muon.GUI.Common.load_utils as load_utils
@@ -7,10 +14,64 @@ from Muon.GUI.Common.muon_pair import MuonPair
 from Muon.GUI.Common.muon_load_data import MuonLoadData
 from Muon.GUI.Common.muon_file_utils import format_run_for_file
 from Muon.GUI.Common.run_string_utils import run_list_to_string
+from Muon.Gui.Common.ADSHandler.workspace_naming import (get_raw_data_workspace_name, get_group_data_workspace_name,
+                                                          get_pair_data_workspace_name, get_base_data_directory,
+                                                          get_raw_data_directory, get_cached_data_directory,
+                                                          get_group_data_directory, get_pair_data_directory)
+
+from Muon.Gui.Common.calculate_pair_and_group import calculate_group_data, calculate_pair_data
 
 from collections import OrderedDict
 
-from mantid.kernel import ConfigServiceImpl
+from mantid.kernel import ConfigServiceImpl, ConfigService
+
+
+def get_default_grouping(instrument, main_field_direction):
+    parameter_name = "Default grouping file"
+    if instrument == "MUSR":
+        parameter_name += " - " + main_field_direction
+    try:
+        grouping_file = ConfigService.getInstrument(instrument).getStringParameter(parameter_name)[0]
+    except IndexError:
+        return [], []
+    instrument_directory = ConfigServiceImpl.Instance().getInstrumentDirectory()
+    filename = instrument_directory + grouping_file
+    new_groups, new_pairs = load_utils.load_grouping_from_XML(filename)
+    return new_groups, new_pairs
+
+
+def construct_empty_group(group_names, group_index=0):
+    """
+    Create an empty MuonGroup appropriate for adding to the current grouping table.
+    """
+    new_group_name = "group_" + str(group_index)
+    while new_group_name in group_names:
+        # modify the name until it is unique
+        group_index += 1
+        new_group_name = "group_" + str(group_index)
+    return MuonGroup(group_name=new_group_name, detector_ids=[1])
+
+
+def construct_empty_pair(group_names, pair_names, pair_index=0):
+    """
+    Create an empty MuonPair appropriate for adding to the current pairing table.
+    """
+    new_pair_name = "pair_" + str(pair_index)
+    while new_pair_name in pair_names:
+        # modify the name until it is unique
+        pair_index += 1
+        new_pair_name = "pair_" + str(pair_index)
+    if len(group_names) == 1:
+        group1 = group_names[0]
+        group2 = group_names[0]
+    elif len(group_names) >= 2:
+        group1 = group_names[0]
+        group2 = group_names[1]
+    else:
+        group1 = None
+        group2 = None
+    return MuonPair(pair_name=new_pair_name,
+                    forward_group_name=group1, backward_group_name=group2, alpha=1.0)
 
 
 class MuonContext(object):
@@ -50,7 +111,7 @@ class MuonContext(object):
 
     @property
     def instrument(self):
-        inst = self.loaded_workspace.getInstrument().getName()
+        inst = ConfigService.getInstrument()
         return inst
 
     @property
@@ -168,10 +229,6 @@ class MuonContext(object):
         self.clear_pairs()
         self._current_data = {"workspace": load_utils.empty_loaded_data()}
 
-    # ------------------------------------------------------------------------------------------------------------------
-    # Workspace naming
-    # ------------------------------------------------------------------------------------------------------------------
-
     def _base_run_name(self):
         """ e.g. EMU0001234 """
         if isinstance(self.run, int):
@@ -179,85 +236,32 @@ class MuonContext(object):
         else:
             return str(self.instrument) + self.run
 
-    def get_raw_data_workspace_name(self):
-        return self._base_run_name() + "_raw_data"
-
-    def get_group_data_workspace_name(self, group_name):
-        if self.is_multi_period():
-            return self._base_run_name() + "; Group; " + group_name + \
-                   "; Counts; Periods; " + self.period_string + "; #1"
-        else:
-            return self._base_run_name() + "; Group; " + group_name + "; Counts; #1"
-
-    def get_pair_data_workspace_name(self, pair_name):
-        if self.is_multi_period():
-            return self._base_run_name() + "; Pair Asym; " + pair_name + "; Periods; " + self.period_string + "; #1"
-        else:
-            return self._base_run_name() + "; Pair Asym; " + pair_name + "; #1"
-
-    def get_base_data_directory(self):
-        if self.is_multi_period():
-            return self.base_directory + "/" + self._base_run_name() + " Period " + self.period_string + "/"
-        else:
-            return self.base_directory + "/" + self._base_run_name() + "/"
-
-    def get_raw_data_directory(self):
-        if self.is_multi_period():
-            return self._base_run_name() + " Period " + self.period_string + "; Raw Data/"
-        else:
-            return self._base_run_name() + " Raw Data/"
-
-    def get_cached_data_directory(self):
-        if self.is_multi_period():
-            return self._base_run_name() + " Period " + self.period_string + "; Cached/"
-        else:
-            return self._base_run_name() + " Cached/"
-
-    def get_group_data_directory(self):
-        if self.is_multi_period():
-            return self._base_run_name() + " Period " + self.period_string + "; Groups/"
-        else:
-            return self._base_run_name() + " Groups/"
-
-    def get_pair_data_directory(self):
-        if self.is_multi_period():
-            return self._base_run_name() + " Period " + self.period_string + "; Pairs/"
-        else:
-            return self._base_run_name() + " Pairs/"
-
     # ------------------------------------------------------------------------------------------------------------------
     # Showing workspaces in the ADS
     # ------------------------------------------------------------------------------------------------------------------
 
-    def _show_single_workspace(self, workspace, name):
-        """
-        workspace must be MuonWorkspace type.
-        """
-        workspace.hide()
-        workspace.show(name=name)
-
     def show_raw_data(self):
         workspace = self.current_data["OutputWorkspace"]
-        directory = self.get_base_data_directory() + self.get_raw_data_directory()
+        directory = get_base_data_directory(self) + get_raw_data_directory(self)
 
         if isinstance(workspace, list):
             # Multi-period data
             for i, single_ws in enumerate(workspace):
-                name = directory + self.get_raw_data_workspace_name() + "_period_" + str(i)
-                self._show_single_workspace(single_ws, name)
+                name = directory + get_raw_data_workspace_name(self) + "_period_" + str(i)
+                single_ws.show(name)
         else:
             # Single period data
-            name = directory + self.get_raw_data_workspace_name()
-            self._show_single_workspace(workspace, name)
+            name = directory + get_raw_data_workspace_name(self)
+            workspace.show(name)
 
     def show_all_groups(self):
         for group_name in self._groups.keys():
             self.show_group_data(group_name)
 
     def show_group_data(self, group_name, show=True):
-        name = self.get_group_data_workspace_name(group_name)
-        directory = self.get_base_data_directory() + self.get_group_data_directory()
-        workspace = self.calculate_group_data(group_name)
+        name = get_group_data_workspace_name(self, group_name)
+        directory = get_base_data_directory(self) + get_group_data_directory(self)
+        workspace = calculate_group_data(self, group_name)
 
         self._groups[group_name].workspace = load_utils.MuonWorkspace(workspace)
         if show:
@@ -268,9 +272,9 @@ class MuonContext(object):
             self.show_pair_data(pair_name)
 
     def show_pair_data(self, pair_name, show=True):
-        name = self.get_pair_data_workspace_name(pair_name)
-        directory = self.get_base_data_directory() + self.get_pair_data_directory()
-        workspace = self.calculate_pair_data(pair_name)
+        name = get_pair_data_workspace_name(self, pair_name)
+        directory = get_base_data_directory(self) + get_pair_data_directory(self)
+        workspace = calculate_pair_data(self, pair_name)
 
         self._pairs[pair_name].workspace = load_utils.MuonWorkspace(workspace)
         if show:
@@ -278,111 +282,10 @@ class MuonContext(object):
 
     def calculate_all_groups(self):
         for group_name in self._groups.keys():
-            self.calculate_group_data(group_name)
-
-    def _run_pre_processing(self):
-        params = self._get_pre_processing_params()
-        params["InputWorkspace"] = self.loaded_workspace
-        processed_data = load_utils.run_MuonPreProcess(params)
-        return processed_data
-
-    def _get_pre_processing_params(self):
-        pre_process_params = {}
-        try:
-            time_min = self.loaded_data["FirstGoodData"]
-            pre_process_params["TimeMin"] = time_min
-        except KeyError:
-            pass
-
-        try:
-            rebin_args = self.loaded_data["Rebin"]
-            pre_process_params["RebinArgs"] = rebin_args
-        except KeyError:
-            pass
-
-        try:
-            time_offset = self.loaded_data["TimeZero"]
-            pre_process_params["TimeOffset"] = time_offset
-        except KeyError:
-            pass
-
-        try:
-            dead_time_table = self.dead_time_table
-            if dead_time_table is not None:
-                pre_process_params["DeadTimeTable"] = dead_time_table
-        except KeyError:
-            pass
-
-        return pre_process_params
-
-    def _get_MuonGroupingCounts_parameters(self, group_name):
-        params = {}
-        try:
-            summed_periods = self.loaded_data["SummedPeriods"]
-            params["SummedPeriods"] = str(summed_periods)
-        except KeyError:
-            params["SummedPeriods"] = "1"
-
-        try:
-            subtracted_periods = self.loaded_data["SubtractedPeriods"]
-            params["SubtractedPeriods"] = str(subtracted_periods)
-        except KeyError:
-            params["SubtractedPeriods"] = ""
-
-        group = self._groups.get(group_name, None)
-        if group:
-            params["GroupName"] = group_name
-            params["Grouping"] = ",".join([str(i) for i in group.detectors])
-
-        return params
-
-    def _get_MuonPairingAsymmetry_parameters(self, pair_name):
-        params = {}
-        try:
-            summed_periods = self.loaded_data["SummedPeriods"]
-            params["SummedPeriods"] = str(summed_periods)
-        except KeyError:
-            params["SummedPeriods"] = "1"
-
-        try:
-            subtracted_periods = self.loaded_data["SubtractedPeriods"]
-            params["SubtractedPeriods"] = str(subtracted_periods)
-        except KeyError:
-            params["SubtractedPeriods"] = ""
-
-        pair = self._pairs.get(pair_name, None)
-
-        if pair:
-            params["SpecifyGroupsManually"] = True
-            params["PairName"] = str(pair_name)
-            detectors1 = ",".join([str(i) for i in self._groups[pair.forward_group].detectors])
-            detectors2 = ",".join([str(i) for i in self._groups[pair.backward_group].detectors])
-            params["Group1"] = detectors1
-            params["Group2"] = detectors2
-            params["Alpha"] = str(pair.alpha)
-
-        return params
-
-    def calculate_group_data(self, group_name):
-        processed_data = self._run_pre_processing()
-
-        params = self._get_MuonGroupingCounts_parameters(group_name)
-        params["InputWorkspace"] = processed_data
-        group_data = load_utils.run_MuonGroupingCounts(params)
-
-        return group_data
-
-    def calculate_pair_data(self, pair_name):
-        processed_data = self._run_pre_processing()
-
-        params = self._get_MuonPairingAsymmetry_parameters(pair_name)
-        params["InputWorkspace"] = processed_data
-        pair_data = load_utils.run_MuonPairingAsymmetry(params)
-
-        return pair_data
+            calculate_group_data(group_name)
 
     def set_groups_and_pairs_to_default(self):
-        groups, pairs = self.get_default_grouping("_dummy_args")
+        groups, pairs = get_default_grouping(self.instrument, self.main_field_direction)
 
         self.clear_groups()
         for group in groups:
@@ -391,51 +294,3 @@ class MuonContext(object):
         self.clear_pairs()
         for pair in pairs:
             self.add_pair(pair)
-
-    def get_default_grouping(self, _instrument):
-        parameter_name = "Default grouping file"
-        if self.instrument == "MUSR":
-            parameter_name += " - " + self.main_field_direction
-        workspace = self.loaded_workspace
-        try:
-            grouping_file = workspace.getInstrument().getStringParameter(parameter_name)[0]
-        except IndexError:
-            return [], []
-        instrument_directory = ConfigServiceImpl.Instance().getInstrumentDirectory()
-        filename = instrument_directory + grouping_file
-        new_groups, new_pairs = load_utils.load_grouping_from_XML(filename)
-        return new_groups, new_pairs
-
-    def construct_empty_group(self, _group_index):
-        """
-        Create an empty MuonGroup appropriate for adding to the current grouping table.
-        """
-        group_index = 0
-        new_group_name = "group_" + str(group_index)
-        while new_group_name in self.group_names:
-            # modify the name until it is unique
-            group_index += 1
-            new_group_name = "group_" + str(group_index)
-        return MuonGroup(group_name=new_group_name, detector_ids=[1])
-
-    def construct_empty_pair(self, _pair_index):
-        """
-        Create an empty MuonPair appropriate for adding to the current pairing table.
-        """
-        pair_index = 0
-        new_pair_name = "pair_" + str(pair_index)
-        while new_pair_name in self.pair_names:
-            # modify the name until it is unique
-            pair_index += 1
-            new_pair_name = "pair_" + str(pair_index)
-        if len(self.group_names) == 1:
-            group1 = self.group_names[0]
-            group2 = self.group_names[0]
-        elif len(self.group_names) >= 2:
-            group1 = self.group_names[0]
-            group2 = self.group_names[1]
-        else:
-            group1 = None
-            group2 = None
-        return MuonPair(pair_name=new_pair_name,
-                        forward_group_name=group1, backward_group_name=group2, alpha=1.0)
