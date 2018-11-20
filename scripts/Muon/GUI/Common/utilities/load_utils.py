4329628e0b206f1841cbb51ea7bb05d8a600f964
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-11-20 14:39:25 +0000

    Removed unused class Re #23642

--- scripts/Muon/GUI/Common/utilities/load_utils.py
+++ scripts/Muon/GUI/Common/utilities/load_utils.py
@@ -154,24 +154,6 @@ DEFAULT_OUTPUT_VALUES = [__default_workspace(),
                          0.0, "Unknown direction"]
 
 
-class floatPropertyWithValue(object):
-    """
-    Simple class to replicate the functionality of the mantid "PropertyWithValue", allowing this class
-    to take its place when, for example, using a dummy load result.
-    """
-
-    def __init__(self, value):
-        self._value = value
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, new_value):
-        self._value = new_value
-
-
 def is_workspace_group(workspace):
     return isinstance(workspace, WorkspaceGroup)
 
