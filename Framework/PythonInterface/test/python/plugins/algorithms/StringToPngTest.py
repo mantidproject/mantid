b599ef7ad2b4f03e9676d3f7a82df06f4f8fb014
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2018-11-21 15:56:28 +0000

    Fix further tests expecting framework to be started

--- Framework/PythonInterface/test/python/plugins/algorithms/StringToPngTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/StringToPngTest.py
@@ -6,34 +6,35 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from __future__ import (absolute_import, division, print_function)
 
-import unittest,os
-import mantid
+import unittest, os
+from mantid import AnalysisDataServiceImpl, config, simpleapi
+
 
 class StringToPngTest(unittest.TestCase):
-
-    plotfile=os.path.join(mantid.config.getString('defaultsave.directory'),"StringToPngTest.png")
+    plotfile = os.path.join(config.getString('defaultsave.directory'), "StringToPngTest.png")
 
     def cleanup(self):
         if os.path.exists(self.plotfile):
             os.remove(self.plotfile)
 
     def testPlot(self):
-        to_plot='This is a string\nAnd this is a second line'
-        ok2run=''
+        to_plot = 'This is a string\nAnd this is a second line'
+        ok2run = ''
         try:
             import matplotlib
             from distutils.version import LooseVersion
-            if LooseVersion(matplotlib.__version__)<LooseVersion("1.2.0"):
-                ok2run='Wrong version of matplotlib. Required >= 1.2.0'
+            if LooseVersion(matplotlib.__version__) < LooseVersion("1.2.0"):
+                ok2run = 'Wrong version of matplotlib. Required >= 1.2.0'
             else:
                 matplotlib.use("agg")
                 import matplotlib.pyplot as plt
         except:
-            ok2run='Problem importing matplotlib'
-        if ok2run=='':
-            mantid.simpleapi.StringToPng(String=to_plot,OutputFilename=self.plotfile)
-            self.assertTrue(os.path.getsize(self.plotfile)>1e3)
+            ok2run = 'Problem importing matplotlib'
+        if ok2run == '':
+            simpleapi.StringToPng(String=to_plot, OutputFilename=self.plotfile)
+            self.assertTrue(os.path.getsize(self.plotfile) > 1e3)
         self.cleanup()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
