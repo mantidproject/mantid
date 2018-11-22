9b541affabce9300ad96b170726c0aa05c161a83
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2018-11-22 16:55:35 +0000

    Refs #23828. Remove unwanted import

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/ResNorm2.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/ResNorm2.py
@@ -9,7 +9,7 @@ from __future__ import (absolute_import, division, print_function)
 
 from mantid.api import (PythonAlgorithm, AlgorithmFactory, MatrixWorkspaceProperty,
                         WorkspaceGroup, WorkspaceGroupProperty, ITableWorkspaceProperty,
-                        Progress, PropertyMode, AnalysisDataService)
+                        Progress, PropertyMode)
 from mantid.kernel import Direction
 from mantid.simpleapi import *
 
