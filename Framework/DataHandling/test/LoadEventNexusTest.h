b4498c12f2e6331b89c9fd7d71ffaa041a241e15
Owen Arnold <owen.arnold@stfc.ac.uk>
2018-11-22 15:06:03 +0000

    refs #24078. Compare to double not int

--- Framework/DataHandling/test/LoadEventNexusTest.h
+++ Framework/DataHandling/test/LoadEventNexusTest.h
@@ -161,14 +161,14 @@ public:
     TS_ASSERT(eventWS);
 
     TS_ASSERT_EQUALS(eventWS->getNumberEvents(), 14258850);
-    TS_ASSERT_EQUALS(eventWS->counts(0)[0], 0);
-    TS_ASSERT_EQUALS(eventWS->counts(1)[0], 2);
-    TS_ASSERT_EQUALS(eventWS->counts(2)[0], 1);
+    TS_ASSERT_EQUALS(eventWS->counts(0)[0], 0.0);
+    TS_ASSERT_EQUALS(eventWS->counts(1)[0], 2.0);
+    TS_ASSERT_EQUALS(eventWS->counts(2)[0], 1.0);
     TS_ASSERT_EQUALS(eventWS->counts(122879)[0],
-                     4); // Regession test for miss
-                         // setting max detector and
-                         // subsequent incorrect event
-                         // count
+                     4.0); // Regession test for miss
+                           // setting max detector and
+                           // subsequent incorrect event
+                           // count
     TS_ASSERT_EQUALS(eventWS->indexInfo().spectrumNumber(0), 1);
     TS_ASSERT_EQUALS(eventWS->indexInfo().spectrumNumber(1), 2);
     TS_ASSERT_EQUALS(eventWS->indexInfo().spectrumNumber(2), 3);
