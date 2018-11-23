e0e8cf9b22e85994ccfa28f3c3d41991c8df2993
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2018-11-23 10:38:31 +0000

    Refs #24065. Fix linker error

--- qt/scientific_interfaces/Indirect/test/IndirectFitPlotPresenterTest.h
+++ qt/scientific_interfaces/Indirect/test/IndirectFitPlotPresenterTest.h
@@ -15,41 +15,20 @@
 #include "IndirectFitPlotPresenter.h"
 #include "IndirectFittingModel.h"
 #include "MantidAPI/FrameworkManager.h"
-//#include "MantidKernel/WarningSuppressions.h"
-////#include "MantidAPI/IAlgorithm.h"
-////#include "MantidAPI/IFunction.h"
-////#include "MantidAPI/MatrixWorkspace.h"
-////#include "MantidTestHelpers/IndirectFitDataCreationHelper.h"
-//
+#include "MantidKernel/WarningSuppressions.h"
+//#include "MantidAPI/IAlgorithm.h"
+//#include "MantidAPI/IFunction.h"
+//#include "MantidAPI/MatrixWorkspace.h"
+//#include "MantidTestHelpers/IndirectFitDataCreationHelper.h"
+
 using namespace Mantid::API;
-////using namespace Mantid::IndirectFitDataCreationHelper;
+//using namespace Mantid::IndirectFitDataCreationHelper;
 using namespace MantidQt::CustomInterfaces;
 using namespace MantidQt::CustomInterfaces::IDA;
 using namespace testing;
-//
-///// This QApplication object is required to construct the view
-////class QApplicationHolder : CxxTest::GlobalFixture {
-////public:
-////	bool setUpWorld() override {
-////		int argc(0);
-////		char **argv = {};
-////		m_app = new QApplication(argc, argv);
-////		return true;
-////	}
-////
-////	bool tearDownWorld() override {
-////		delete m_app;
-////		return true;
-////	}
-////
-////private:
-////	QApplication *m_app;
-////};
-////
-////static QApplicationHolder MAIN_QAPPLICATION;
-//
-//GNU_DIAG_OFF_SUGGEST_OVERRIDE
-//
+
+GNU_DIAG_OFF_SUGGEST_OVERRIDE
+
 /// Mock object to mock the view
 class MockIndirectFitPlotView : public IndirectFitPlotView {
 public:
@@ -102,25 +81,25 @@ public:
 	//MOCK_CONST_METHOD0(getSelectedSpectrum, std::size_t());
 };
 
-//class MockIndirectFittingModel : public IndirectFittingModel {
-////public:
-//private:
-//	std::string sequentialFitOutputName() const override { return ""; };
-//	std::string simultaneousFitOutputName() const override { return ""; };
-//	std::string singleFitOutputName(std::size_t index,
-//		std::size_t spectrum) const override {
-//		UNUSED_ARG(index);
-//		UNUSED_ARG(spectrum);
-//		return "";
-//	};
-//
-//	std::vector<std::string> getSpectrumDependentAttributes() const override {
-//		return{};
-//	};
-//};
-//
-//GNU_DIAG_ON_SUGGEST_OVERRIDE
-//
+class MockIndirectFittingModel : public IndirectFittingModel {
+public:
+private:
+	std::string sequentialFitOutputName() const override { return ""; };
+	std::string simultaneousFitOutputName() const override { return ""; };
+	std::string singleFitOutputName(std::size_t index,
+		std::size_t spectrum) const override {
+		UNUSED_ARG(index);
+		UNUSED_ARG(spectrum);
+		return "";
+	};
+
+	std::vector<std::string> getSpectrumDependentAttributes() const override {
+		return{};
+	};
+};
+
+GNU_DIAG_ON_SUGGEST_OVERRIDE
+
 class IndirectFitPlotPresenterTest : public CxxTest::TestSuite {
 public:
 	/// Needed to make sure everything is initialized
@@ -136,30 +115,30 @@ public:
 
 	void setUp() override {
 		m_view = new NiceMock<MockIndirectFitPlotView>();
-		//m_model = new NiceMock<MockIndirectFittingModel>();
-		//m_presenter = new IndirectFitPlotPresenter(m_model, m_view);
+		m_model = new NiceMock<MockIndirectFittingModel>();
+		m_presenter = new IndirectFitPlotPresenter(m_model, m_view);
 
 		//SetUpADSWithWorkspace ads("WorkspaceName", createWorkspace(10));
 		//m_model->addWorkspace("WorkspaceName");
 	}
 
-	//void tearDown() override {
-	//	AnalysisDataService::Instance().clear();
+	void tearDown() override {
+		AnalysisDataService::Instance().clear();
 
-	//	TS_ASSERT(Mock::VerifyAndClearExpectations(m_view));
-	//	TS_ASSERT(Mock::VerifyAndClearExpectations(m_model));
+		TS_ASSERT(Mock::VerifyAndClearExpectations(m_view));
+		TS_ASSERT(Mock::VerifyAndClearExpectations(m_model));
 
-	//	delete m_presenter;
-	//	delete m_model;
-	//	// delete m_view; - causes an error
-	//}
+		delete m_presenter;
+		delete m_model;
+		// delete m_view; - causes an error
+	}
 
 	void test_test() {}
 
 private:
 	MockIndirectFitPlotView *m_view;
-	//MockIndirectFittingModel *m_model;
-	//IndirectFitPlotPresenter *m_presenter;
+	MockIndirectFittingModel *m_model;
+	IndirectFitPlotPresenter *m_presenter;
 };
 
 #endif 
