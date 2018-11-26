d27708d2876dbf9d7e9027ccecc5c2753fa8b3b5
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-11-26 15:11:47 +0000

    Re #0 changed to numeric limit and noexcept

--- Framework/Kernel/inc/MantidKernel/EmptyValues.h
+++ Framework/Kernel/inc/MantidKernel/EmptyValues.h
@@ -15,8 +15,7 @@
 #include "MantidKernel/DllConfig.h"
 #include "MantidKernel/System.h"
 
-#include <cfloat>
-#include <climits>
+#include <numeric_limits>
 
 namespace Mantid {
 
@@ -24,25 +23,25 @@ namespace Mantid {
  * Returns what we consider an "empty" integer within a property
  * @returns An flag value
  */
-constexpr int EMPTY_INT() { return INT_MAX; }
+constexpr int EMPTY_INT() noexcept { return  std::numeric_limits<int>::max();; }
 
 /**
  * Returns what we consider an "empty" long within a property
  * @returns An flag value
  */
-constexpr long EMPTY_LONG() { return LONG_MAX; }
+constexpr long EMPTY_LONG() noexcept { return  std::numeric_limits<long>::max();; }
 
 /**
  * Returns what we consider an "empty" int64_t within a property
  * @returns An flag value
  */
-constexpr int64_t EMPTY_INT64() { return INT64_MAX; }
+constexpr int64_t EMPTY_INT64() noexcept { return  std::numeric_limits<int64_t>::max();; }
 
 /**
  * Returns what we consider an "empty" double within a property
  * @returns An flag value
  */
-constexpr double EMPTY_DBL() { return DBL_MAX / 2; }
+constexpr double EMPTY_DBL() noexcept { return  std::numeric_limits<double>::max(); / 2; }
 
 } // namespace Mantid
 
