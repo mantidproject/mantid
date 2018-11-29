e034a52ef95be6cc050c46ea762155f52bdb2623
Gagik Vardanyan <vardanyan@ill.fr>
2018-11-29 13:56:10 +0100

    Re #24124 include order in the header

--- Framework/Algorithms/inc/MantidAlgorithms/ParallaxCorrection.h
+++ Framework/Algorithms/inc/MantidAlgorithms/ParallaxCorrection.h
@@ -7,8 +7,8 @@
 #ifndef MANTID_ALGORITHMS_PARALLAXCORRECTION_H_
 #define MANTID_ALGORITHMS_PARALLAXCORRECTION_H_
 
-#include "MantidAlgorithms/DllConfig.h"
 #include "MantidAPI/Algorithm.h"
+#include "MantidAlgorithms/DllConfig.h"
 
 namespace Mantid {
 namespace Algorithms {
