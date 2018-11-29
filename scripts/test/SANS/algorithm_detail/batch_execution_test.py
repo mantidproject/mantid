d76a798885ce9906ac90ed61c02da55b6bdeb022
Tom Titcombe <t.j.titcombe@gmail.com>
2018-11-29 16:46:17 +0000

    Update failing unit tests Refs #23773

--- scripts/test/SANS/algorithm_detail/batch_execution_test.py
+++ scripts/test/SANS/algorithm_detail/batch_execution_test.py
@@ -32,26 +32,35 @@ class GetAllNamesToSaveTest(unittest.TestCase):
                                                 XMin=1, XMax=14, BinWidth=2)
         reduced_hab_can = CreateSampleWorkspace(Function='Flat background', NumBanks=1, BankPixelWidth=1, NumEvents=1,
                                                 XMin=1, XMax=14, BinWidth=2)
+        reduced_lab_sample = CreateSampleWorkspace(Function='Flat background', NumBanks=1, BankPixelWidth=1, NumEvents=1,
+                                                XMin=1, XMax=14, BinWidth=2)
+        reduced_hab_sample = CreateSampleWorkspace(Function='Flat background', NumBanks=1, BankPixelWidth=1, NumEvents=1,
+                                                XMin=1, XMax=14, BinWidth=2)
         self.reduction_package_merged.reduced_merged = merged_workspace
         self.reduction_package_merged.reduced_lab = lab_workspace
         self.reduction_package_merged.reduced_hab = hab_workspace
         self.reduction_package_merged.reduced_lab_can = reduced_lab_can
         self.reduction_package_merged.reduced_hab_can = reduced_hab_can
+        self.reduction_package_merged.reduced_lab_sample = reduced_lab_sample
+        self.reduction_package_merged.reduced_hab_sample = reduced_hab_sample
 
         self.reduction_package.reduced_lab = lab_workspace
         self.reduction_package.reduced_hab = hab_workspace
         self.reduction_package.reduced_lab_can = reduced_lab_can
         self.reduction_package.reduced_hab_can = reduced_hab_can
+        self.reduction_package.reduced_lab_sample = reduced_lab_sample
+        self.reduction_package.reduced_hab_sample = reduced_hab_sample
+
 
     def test_returns_merged_name_if_present(self):
         reduction_packages = [self.reduction_package_merged]
-        names_to_save = get_all_names_to_save(reduction_packages)
+        names_to_save = get_all_names_to_save(reduction_packages, save_can=False)
 
         self.assertEqual(names_to_save, {'merged_workspace'})
 
     def test_hab_and_lab_workspaces_returned_if_merged_workspace_not_present(self):
         reduction_packages = [self.reduction_package]
-        names_to_save = get_all_names_to_save(reduction_packages)
+        names_to_save = get_all_names_to_save(reduction_packages, save_can=False)
 
         self.assertEqual(names_to_save, {'lab_workspace', 'hab_workspace'})
 
@@ -60,14 +69,16 @@ class GetAllNamesToSaveTest(unittest.TestCase):
         names_to_save = get_all_names_to_save(reduction_packages, True)
 
         self.assertEqual(names_to_save, {'merged_workspace', 'lab_workspace', 'hab_workspace',
-                                         'reduced_lab_can', 'reduced_hab_can'})
+                                         'reduced_lab_can', 'reduced_hab_can',
+                                         'reduced_lab_sample', 'reduced_hab_sample'})
 
     def test_can_workspaces_returned_if_save_can_selected_no_merged(self):
         reduction_packages = [self.reduction_package]
         names_to_save = get_all_names_to_save(reduction_packages, True)
 
         self.assertEqual(names_to_save, {'lab_workspace', 'hab_workspace',
-                                         'reduced_lab_can', 'reduced_hab_can'})
+                                         'reduced_lab_can', 'reduced_hab_can',
+                                         'reduced_lab_sample', 'reduced_hab_sample'})
 
 
 if __name__ == '__main__':
