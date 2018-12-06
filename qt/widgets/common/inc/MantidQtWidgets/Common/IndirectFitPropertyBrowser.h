543d69247e0e65fa657bfb4d78f88069f08e8d5d
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2018-12-06 09:58:52 +0000

    Refs #24202. Fix doxygen warning

--- qt/widgets/common/inc/MantidQtWidgets/Common/IndirectFitPropertyBrowser.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/IndirectFitPropertyBrowser.h
@@ -133,7 +133,7 @@ public:
 
   void setWorkspaceIndex(int i) override;
 
-  void updatePlotGuess(Mantid::API::MatrixWorkspace_sptr workspace);
+  void updatePlotGuess(Mantid::API::MatrixWorkspace_sptr sampleWorkspace);
 
 public slots:
   void fit() override;
