c46f1a0810aa048bbaa45ef46f7b2861d9b698d9
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-12-07 15:08:16 +0000

    Re #23103 replaced owning ptr in Foreground model with unique_ptr

--- Framework/MDAlgorithms/src/Quantification/ForegroundModel.cpp
+++ Framework/MDAlgorithms/src/Quantification/ForegroundModel.cpp
@@ -44,7 +44,7 @@ ForegroundModel::ForegroundModel(const API::IFunction &fittingFunction)
 
 /**
  */
-ForegroundModel::~ForegroundModel() { delete m_formFactorTable; }
+ForegroundModel::~ForegroundModel() {}
 
 /**
  * Set a reference to the convolved fitting function. Required as we need a
@@ -135,15 +135,13 @@ const API::IFunction &ForegroundModel::functionUnderMinimization() const {
 void ForegroundModel::setFormFactorIon(const std::string &ionType) {
   // "0" indicates off
   if (ionType == "0") {
-    delete m_formFactorTable;
-    m_formFactorTable = nullptr;
+    m_formFactorTable.reset(nullptr);
   } else {
     using namespace PhysicalConstants;
     if (m_MagIonName != ionType) {
       if (m_formFactorTable) {
-        delete m_formFactorTable;
       }
-      m_formFactorTable = new MagneticFormFactorTable(FORM_FACTOR_TABLE_LENGTH,
+      m_formFactorTable = std::make_unique<MagneticFormFactorTable>(FORM_FACTOR_TABLE_LENGTH,
                                                       getMagneticIon(ionType));
       m_MagIonName = ionType;
     }
