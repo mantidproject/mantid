fe85c303273349b7159f031e559a4c6322295144
Martyn Gigg <martyn.gigg@gmail.com>
2018-12-10 16:28:49 +0000

    Remove use of msbuild directly in favour of cmake build

---
+++ buildconfig/Jenkins/cmakegenerator.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: SDK 8.1 is backwards compatible with Windows 7. It allows us to target Windows 7
+:: when building on newer versions of Windows. This value must be supplied
+:: externally and cannot be supplied in the cmake configuration
+set SDK_VERS=8.1
+set CM_GENERATOR=Visual Studio 15 2017 Win64
