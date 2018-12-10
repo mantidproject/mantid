c6a4c80c91717b2d743e0b331c0ec0b205e30f95
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-12-10 13:21:48 +0000

    refs #24285 added numbers to fit output labels

--- Framework/CurveFitting/src/IMWDomainCreator.cpp
+++ Framework/CurveFitting/src/IMWDomainCreator.cpp
@@ -303,13 +303,15 @@ boost::shared_ptr<API::Workspace> IMWDomainCreator::createOutputWorkspace(
   textAxis->setLabel(0, "Data");
   textAxis->setLabel(1, "Calc");
   textAxis->setLabel(2, "Diff");
-
+  std::unordered_map<std::string, int> functionCount;
   // Add each calculated function
   auto iend = functionsToDisplay.end();
   size_t wsIndex(1); // Zero reserved for data
   for (auto it = functionsToDisplay.begin(); it != iend; ++it) {
-    if (wsIndex > 2)
-      textAxis->setLabel(wsIndex, (*it)->name());
+    if (wsIndex > 2) {
+      std::string name = (*it)->name();
+       ++functionCount[name];
+      textAxis->setLabel(wsIndex, name + " "+std::to_string(functionCount[name]));}
     addFunctionValuesToWS(*it, ws, wsIndex, domain, values);
     if (it == functionsToDisplay.begin())
       wsIndex += 2; // Skip difference histogram for now
