eae3661652e6a81c63de9fe16907a28028e19c84
Samuel Jones <samjones714@gmail.com>
2018-12-10 15:19:30 +0000

    Re #24049 make tests more explicit

--- qt/python/mantidqt/project/test/test_project.py
+++ qt/python/mantidqt/project/test/test_project.py
@@ -26,28 +26,28 @@ else:
 
 
 class ProjectTest(unittest.TestCase):
+    def setUp(self):
+        self.project = Project()
+
     def tearDown(self):
         ADS.clear()
 
     def test_save_calls_save_as_when_last_location_is_not_none(self):
-        project = Project()
-        project.save_as = mock.MagicMock()
-        project.save()
-        self.assertEqual(project.save_as.call_count, 1)
+        self.project.save_as = mock.MagicMock()
+        self.project.save()
+        self.assertEqual(self.project.save_as.call_count, 1)
 
     def test_save_does_not_call_save_as_when_last_location_is_not_none(self):
-        project = Project()
-        project.save_as = mock.MagicMock()
-        project.last_project_location = "1"
-        self.assertEqual(project.save_as.call_count, 0)
+        self.project.save_as = mock.MagicMock()
+        self.project.last_project_location = "1"
+        self.assertEqual(self.project.save_as.call_count, 0)
 
     def test_save_saves_project_successfully(self):
-        project = Project()
         working_directory = tempfile.mkdtemp()
-        project.last_project_location = working_directory
+        self.project.last_project_location = working_directory
         CreateSampleWorkspace(OutputWorkspace="ws1")
 
-        project.save()
+        self.project.save()
 
         self.assertTrue(os.path.isdir(working_directory))
         file_list = os.listdir(working_directory)
@@ -55,109 +55,108 @@ class ProjectTest(unittest.TestCase):
         self.assertTrue("ws1.nxs" in file_list)
 
     def test_save_as_saves_project_successfully(self):
-        project = Project()
         working_directory = tempfile.mkdtemp()
-        project._get_directory_finder = mock.MagicMock(return_value=working_directory)
+        self.project._get_directory_finder = mock.MagicMock(return_value=working_directory)
         CreateSampleWorkspace(OutputWorkspace="ws1")
 
-        project.save_as()
+        self.project.save_as()
 
-        self.assertEqual(project._get_directory_finder.call_count, 1)
+        self.assertEqual(self.project._get_directory_finder.call_count, 1)
         self.assertTrue(os.path.isdir(working_directory))
         file_list = os.listdir(working_directory)
         self.assertTrue(os.path.basename(working_directory) + ".mtdproj" in file_list)
         self.assertTrue("ws1.nxs" in file_list)
 
     def test_load_calls_loads_successfully(self):
-        project = Project()
         working_directory = tempfile.mkdtemp()
-        project._get_directory_finder = mock.MagicMock(return_value=working_directory)
+        self.project._get_directory_finder = mock.MagicMock(return_value=working_directory)
         CreateSampleWorkspace(OutputWorkspace="ws1")
-        project.save_as()
+        self.project.save_as()
 
         ADS.clear()
 
-        project.load()
-        self.assertEqual(project._get_directory_finder.call_count, 2)
+        self.project.load()
+        self.assertEqual(self.project._get_directory_finder.call_count, 2)
         self.assertEqual(["ws1"], ADS.getObjectNames())
 
     def test_offer_save_does_nothing_if_saved_is_true(self):
-        project = Project()
-        self.assertEqual(project.offer_save(None), None)
+        self.assertEqual(self.project.offer_save(None), None)
 
     def test_offer_save_does_something_if_saved_is_false(self):
-        project = Project()
-        project._offer_save_message_box = mock.MagicMock(return_value=QMessageBox.Yes)
-        project.save = mock.MagicMock()
+        self.project._offer_save_message_box = mock.MagicMock(return_value=QMessageBox.Yes)
+        self.project.save = mock.MagicMock()
 
         # Add something to the ads so __saved is set to false
         CreateSampleWorkspace(OutputWorkspace="ws1")
 
-        self.assertEqual(project.offer_save(None), False)
-        self.assertEqual(project.save.call_count, 1)
-        self.assertEqual(project._offer_save_message_box.call_count, 1)
+        self.assertEqual(self.project.offer_save(None), False)
+        self.assertEqual(self.project.save.call_count, 1)
+        self.assertEqual(self.project._offer_save_message_box.call_count, 1)
 
-    def test_adding_to_ads_sets_saved_to_false(self):
-        project = Project()
+    def test_adding_to_ads_calls_any_change_handle(self):
+        self.project.anyChangeHandle = mock.MagicMock()
         CreateSampleWorkspace(OutputWorkspace="ws1")
 
-        self.assertTrue(not project.saved)
+        self.assertEqual(1, self.project.anyChangeHandle.call_count)
 
-    def test_removing_from_ads_sets_saved_to_false(self):
+    def test_removing_from_ads_calls_any_change_handle(self):
         CreateSampleWorkspace(OutputWorkspace="ws1")
-        project = Project()
+
+        self.project.anyChangeHandle = mock.MagicMock()
         ADS.remove("ws1")
 
-        self.assertTrue(not project.saved)
+        self.assertEqual(1, self.project.anyChangeHandle.call_count)
 
-    def test_grouping_in_ads_sets_saved_to_false(self):
+    def test_grouping_in_ads_calls_any_change_handle(self):
         CreateSampleWorkspace(OutputWorkspace="ws1")
         CreateSampleWorkspace(OutputWorkspace="ws2")
 
-        project = Project()
+        self.project.anyChangeHandle = mock.MagicMock()
         GroupWorkspaces(InputWorkspaces="ws1,ws2", OutputWorkspace="NewGroup")
 
-        self.assertTrue(not project.saved)
+        # Called twice because group is made and then added to the ADS
+        self.assertEqual(2, self.project.anyChangeHandle.call_count)
 
-    def test_renaming_in_ads_sets_saved_to_false(self):
+    def test_renaming_in_ads_calls_any_change_handle(self):
         CreateSampleWorkspace(OutputWorkspace="ws1")
 
-        project = Project()
+        self.project.anyChangeHandle = mock.MagicMock()
         RenameWorkspace(InputWorkspace="ws1", OutputWorkspace="ws2")
 
-        self.assertTrue(not project.saved)
+        # Called twice because first workspace is removed and second is added
+        self.assertEqual(2, self.project.anyChangeHandle.call_count)
 
-    def test_ungrouping_in_ads_sets_saved_to_false(self):
+    def test_ungrouping_in_ads_calls_any_change_handle(self):
         CreateSampleWorkspace(OutputWorkspace="ws1")
         CreateSampleWorkspace(OutputWorkspace="ws2")
         GroupWorkspaces(InputWorkspaces="ws1,ws2", OutputWorkspace="NewGroup")
 
-        project = Project()
+        self.project.anyChangeHandle = mock.MagicMock()
         UnGroupWorkspace(InputWorkspace="NewGroup")
 
-        self.assertTrue(not project.saved)
+        # 1 for removing old group and 1 for something else but 2 seems right
+        self.assertEqual(2, self.project.anyChangeHandle.call_count)
 
-    def test_group_updated_in_ads_sets_saved_to_false(self):
+    def test_group_updated_in_ads_calls_any_change_handle(self):
         CreateSampleWorkspace(OutputWorkspace="ws1")
         CreateSampleWorkspace(OutputWorkspace="ws2")
         GroupWorkspaces(InputWorkspaces="ws1,ws2", OutputWorkspace="NewGroup")
         CreateSampleWorkspace(OutputWorkspace="ws3")
 
-        project = Project()
+        self.project.anyChangeHandle = mock.MagicMock()
         ADS.addToGroup("NewGroup", "ws3")
 
-        self.assertTrue(not project.saved)
+        self.assertEqual(1, self.project.anyChangeHandle.call_count)
 
     def test_removing_unused_workspaces_operates_as_expected_from_save(self):
-        project = Project()
         working_directory = tempfile.mkdtemp()
-        project.last_project_location = working_directory
+        self.project.last_project_location = working_directory
         CreateSampleWorkspace(OutputWorkspace="ws1")
 
-        project.save()
+        self.project.save()
         ADS.clear()
         CreateSampleWorkspace(OutputWorkspace="ws2")
-        project.save()
+        self.project.save()
 
         self.assertTrue(os.path.isdir(working_directory))
         file_list = os.listdir(working_directory)
