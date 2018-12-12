3cce002fb9a39bd0cfc492e632eb3805db130462
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-12-12 12:53:51 +0000

    Re #23103 updated Icat3Helper based on review

--- Framework/ICat/src/ICat3/ICat3Helper.cpp
+++ Framework/ICat/src/ICat3/ICat3Helper.cpp
@@ -190,16 +190,16 @@ void CICatHelper::saveInvestigationIncludesResponse(
         savetoTableWorkspace((*datafile_citr)->location, t);
 
         // File creation Time.
-        std::unique_ptr<std::string> creationtime = nullptr;
-        if ((*datafile_citr)->datafileCreateTime != nullptr) {
-          time_t crtime = *(*datafile_citr)->datafileCreateTime;
-          char temp[25];
-          strftime(temp, 25, "%Y-%b-%d %H:%M:%S", localtime(&crtime));
-          std::string ftime(temp);
-          creationtime = std::make_unique<std::string>();
-          creationtime->assign(ftime);
+        if ((*datafile_citr)->datafileCreateTime) {
+          const static std::string format("%Y-%b-%d %H:%M:%S");
+          std::string creationTime;
+          creationTime.resize(format.size());
+          const time_t crtime = *(*datafile_citr)->datafileCreateTime;
+          strftime(const_cast<char *>(creationTime.data()), creationTime.size(),format.data(),
+                   localtime(&crtime));
+          savetoTableWorkspace(creationTime.data(), t);
         }
-        savetoTableWorkspace(creationtime.get(), t);
+        
 
         //
         savetoTableWorkspace((*datafile_citr)->id, t);
