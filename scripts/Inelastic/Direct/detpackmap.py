ce6ab75be2f22829cbcfb1943f1a3b206b557ffa
Jiao Lin <linjiao@ornl.gov>
2018-12-13 15:03:59 -0500

    Refs #24330. minor tweaks requested by Andrei

--- scripts/Inelastic/Direct/detpackmap.py
+++ scripts/Inelastic/Direct/detpackmap.py
@@ -1,10 +1,9 @@
-sequoia_packs = ['B%s' % i for i in range(1, 37+1)] \
+def sequoia(name):
+    name = name.strip().upper()
+    packs = ['B%s' % i for i in range(1, 37+1)] \
                 + ['C%s' % i for i in range(1, 24+1)] \
                 + ['C25T', 'C26T', 'C25B', 'C26B'] \
                 + ['C%s' % i for i in range(27, 37+1)] \
                 + ['D%s' % i for i in range(1, 37+1)]
-sequoia_start_index = 38
-
-
-def sequoia(name):
-    return sequoia_start_index + sequoia_packs.index(name)
+    start_index = 38
+    return start_index + packs.index(name)
