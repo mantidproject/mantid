fa91ae2f5fc895dc1e4786d8efbaa512531f7260
Gemma Guest <gemma.guest@stfc.ac.uk>
2018-12-13 11:29:52 +0000

    Fix linker error on Windows due to incorrect DLL export
    Also add header file to CMakeLists so it is visible in visual studio
    
    Re #22263

--- qt/widgets/common/inc/MantidQtWidgets/Common/Batch/MockJobTreeView.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/Batch/MockJobTreeView.h
@@ -18,7 +18,7 @@ namespace Batch {
 
 GNU_DIAG_OFF_SUGGEST_OVERRIDE
 
-class EXPORT_OPT_MANTIDQT_COMMON MockJobTreeView : public IJobTreeView {
+class DLLExport MockJobTreeView : public IJobTreeView {
 public:
   MockJobTreeView() : m_deadCell("", "white", 0, "transparent", 0, false) {
     ON_CALL(*this, deadCell()).WillByDefault(Return(m_deadCell));
