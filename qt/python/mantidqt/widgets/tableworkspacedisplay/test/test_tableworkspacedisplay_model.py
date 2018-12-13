4e6b64fc0d1a8c14d89583359ac2d1a6633325bc
Dimitar Tasev <dimtasev@gmail.com>
2018-12-13 13:37:49 +0000

    Model test, deleted old presenter tests, updated CMakeLists, re #24007

--- qt/python/mantidqt/widgets/tableworkspacedisplay/test/test_tableworkspacedisplay_model.py
+++ qt/python/mantidqt/widgets/tableworkspacedisplay/test/test_tableworkspacedisplay_model.py
@@ -9,13 +9,26 @@
 #
 from __future__ import (absolute_import, division, print_function)
 
+import functools
 import unittest
 
 from mock import Mock
 
+from mantid.kernel import V3D
 from mantidqt.widgets.matrixworkspacedisplay.test_helpers.matrixworkspacedisplay_common import \
     MockWorkspace
 from mantidqt.widgets.tableworkspacedisplay.model import TableWorkspaceDisplayModel
+
+
+def with_mock_model(func):
+    # type: (callable) -> callable
+    @functools.wraps(func)
+    def wrapper(self):
+        ws = MockWorkspace()
+        model = TableWorkspaceDisplayModel(ws)
+        return func(self, model)
+
+    return wrapper
 
 
 class TableWorkspaceDisplayModelTest(unittest.TestCase):
@@ -37,26 +50,93 @@ class TableWorkspaceDisplayModelTest(unittest.TestCase):
         self.assertRaises(ValueError, lambda: TableWorkspaceDisplayModel(1))
         self.assertRaises(ValueError, lambda: TableWorkspaceDisplayModel("test_string"))
 
+    @with_mock_model
+    def test_get_bool_from_str(self, model):
+        """
+        :type model: TableWorkspaceDisplayModel
+        """
+        self.assertEqual(model._get_bool_from_str(True), True)
+        self.assertEqual(model._get_bool_from_str(False), False)
 
-class TableWorkspaceDisplayModelTestWithFramework(unittest.TestCase):
+        self.assertEqual(model._get_bool_from_str("True"), True)
+        self.assertEqual(model._get_bool_from_str("true"), True)
 
-    @classmethod
-    def setUpClass(cls):
-        # Allow the MockWorkspace to work within the model
-        TableWorkspaceDisplayModel.ALLOWED_WORKSPACE_TYPES.append(MockWorkspace)
+        self.assertEqual(model._get_bool_from_str("False"), False)
+        self.assertEqual(model._get_bool_from_str("false"), False)
 
-    def test_no_raise_with_supported_workspace(self):
-        from mantid.simpleapi import CreateSampleWorkspace  # noqa
-        ws = MockWorkspace()
-        expected_name = "TEST_WORKSPACE"
-        ws.name = Mock(return_value=expected_name)
+    @with_mock_model
+    def test_get_bool_from_str_raises_invalid_str(self, model):
+        """
+        :type model: TableWorkspaceDisplayModel
+        """
+        self.assertRaisesRegexp(ValueError, "not a valid bool string", model._get_bool_from_str, "fadawd")
+        self.assertRaisesRegexp(ValueError, "cannot be converted to bool", model._get_bool_from_str, [12])
 
-        # no need to assert anything - if the constructor raises the test will fail
-        TableWorkspaceDisplayModel(ws)
+    @with_mock_model
+    def test_get_v3d_from_str(self, model):
+        """
+        :type model: TableWorkspaceDisplayModel
+        """
+        self.assertEqual(V3D(1, 2, 3), model._get_v3d_from_str("1,2,3"))
+        self.assertEqual(V3D(4, 5, 6), model._get_v3d_from_str("[4,5,6]"))
 
-        ws = CreateSampleWorkspace(NumBanks=1, BankPixelWidth=4, NumEvents=10)
-        TableWorkspaceDisplayModel(ws)
+    @with_mock_model
+    def test_map_from_type_name(self, model):
+        """
+        :type model: TableWorkspaceDisplayModel
+        """
+        conversion_funcs = model.map_from_type_names(
+            ['class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >', 'double',
+             'double', 'int', '__int64', 'float', 'unsigned __int64', 'struct Mantid::API::Boolean',
+             'class Mantid::Kernel::V3D', 'unsigned int'])
 
+        self.assertEqual(
+            [str, float, float, int, int, float, int, model._get_bool_from_str, model._get_v3d_from_str, int],
+            conversion_funcs)
 
-if __name__ == '__main__':
-    unittest.main()
+    @with_mock_model
+    def test_map_from_type_name_raises_for_unknown_type(self, model):
+        """
+        :type model: TableWorkspaceDisplayModel
+        """
+        self.assertRaisesRegexp(ValueError, "Trying to set data for unknown column type", model.map_from_type_names,
+                                ["grizzly", "bears"])
+
+    @with_mock_model
+    def test_set_cell_data(self, model):
+        """
+        :type model: TableWorkspaceDisplayModel
+        """
+        test_data = 4444
+
+        expected_col = 1111
+        model.conversion_functions = Mock()
+        model.conversion_functions.__getitem__ = Mock(return_value=lambda x: int(x))
+
+        model.set_cell_data(1, expected_col, test_data)
+
+        # check that the correct conversion function was retrieved
+        # -> the one for the column for which the data is being set
+        model.conversion_functions.__getitem__.assert_called_once_with(expected_col)
+        model.ws.setCell.assert_called_once_with(1, expected_col, test_data)
+
+    class TableWorkspaceDisplayModelFrameworkTest(unittest.TestCase):
+        @classmethod
+        def setUpClass(cls):
+            # Allow the MockWorkspace to work within the model
+            TableWorkspaceDisplayModel.ALLOWED_WORKSPACE_TYPES.append(MockWorkspace)
+
+        def test_no_raise_with_supported_workspace(self):
+            from mantid.simpleapi import CreateEmptyTableWorkspace
+            ws = MockWorkspace()
+            expected_name = "TEST_WORKSPACE"
+            ws.name = Mock(return_value=expected_name)
+
+            # no need to assert anything - if the constructor raises the test will fail
+            TableWorkspaceDisplayModel(ws)
+
+            ws = CreateEmptyTableWorkspace()
+            TableWorkspaceDisplayModel(ws)
+
+    if __name__ == '__main__':
+        unittest.main()
