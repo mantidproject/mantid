31f83eb2fc3691fb02f7f3f38c356f81166f2109
Jiao Lin <linjiao@ornl.gov>
2018-12-13 15:17:46 -0500

    Refs #24330. make flake8 happy

--- scripts/Inelastic/Direct/detpackmap.py
+++ scripts/Inelastic/Direct/detpackmap.py
@@ -1,9 +1,10 @@
 def sequoia(name):
     name = name.strip().upper()
-    packs = ['B%s' % i for i in range(1, 37+1)] \
-                + ['C%s' % i for i in range(1, 24+1)] \
-                + ['C25T', 'C26T', 'C25B', 'C26B'] \
-                + ['C%s' % i for i in range(27, 37+1)] \
-                + ['D%s' % i for i in range(1, 37+1)]
+    packs = (['B%s' % i for i in range(1, 37+1)]
+             + ['C%s' % i for i in range(1, 24+1)]
+             + ['C25T', 'C26T', 'C25B', 'C26B']
+             + ['C%s' % i for i in range(27, 37+1)]
+             + ['D%s' % i for i in range(1, 37+1)]
+             )
     start_index = 38
     return start_index + packs.index(name)
