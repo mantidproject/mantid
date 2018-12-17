d6a19495ce2ea1559bc845cfb79d6a184e0a2a4e
Andrei Savici <saviciat@ornl.gov>
2018-12-17 10:30:02 -0500

    Re #24330. Improve code readability

--- scripts/test/InelasticDirect_detpackmap_Test.py
+++ scripts/test/InelasticDirect_detpackmap_Test.py
@@ -5,18 +5,16 @@ import unittest
 class TestCase(unittest.TestCase):
 
     def test_sequoia(self):
-        asserteq = self.assertEqual
-        asserteq(sequoia('B1'),  38)
-        asserteq(sequoia('B37'),  74)
-        asserteq(sequoia('C1'),  75)
-        asserteq(sequoia('C25T'),  99)
-        asserteq(sequoia('C26T'),  100)
-        asserteq(sequoia('C25B'),  101)
-        asserteq(sequoia('C26B'),  102)
-        asserteq(sequoia('C37'),  113)
-        asserteq(sequoia('D1'),  114)
-        asserteq(sequoia('D37'),  150)
-        return
+        self.assertEqual(sequoia('B1'),  38)
+        self.assertEqual(sequoia('B37'),  74)
+        self.assertEqual(sequoia('C1'),  75)
+        self.assertEqual(sequoia('C25T'),  99)
+        self.assertEqual(sequoia('C26T'),  100)
+        self.assertEqual(sequoia('C25B'),  101)
+        self.assertEqual(sequoia('C26B'),  102)
+        self.assertEqual(sequoia('C37'),  113)
+        self.assertEqual(sequoia('D1'),  114)
+        self.assertEqual(sequoia('D37'),  150)
 
 
 if __name__ == '__main__':
