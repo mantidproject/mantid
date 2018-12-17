dc3537911d982ef5c8cbc0fa74bfdf8a2d5b4f51
Anthony Lim <anthony.lim@stfc.ac.uk>
2018-12-17 11:05:53 +0000

    rrefs #24285 fix for extractQENSMembers

--- Framework/WorkflowAlgorithms/src/ExtractQENSMembers.cpp
+++ Framework/WorkflowAlgorithms/src/ExtractQENSMembers.cpp
@@ -187,7 +187,7 @@ std::vector<std::string> ExtractQENSMembers::renameConvolvedMembers(
   auto index = 0u;
 
   for (const auto &member : members) {
-    if (member == "Convolution 1" && index < newNames.size())
+    if (member.substr(0,11) == "Convolution" && index < newNames.size())
       newMembers.emplace_back(newNames[index++]);
     else
       newMembers.emplace_back(member);
