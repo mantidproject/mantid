b4cfac81e6cfce1ae7082d89749945a479b3c217
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2018-12-17 14:28:36 +0000

    Test moving start x beyond end x. Re #24027

--- qt/applications/workbench/workbench/test/test_fit_interactive_tool.py
+++ qt/applications/workbench/workbench/test/test_fit_interactive_tool.py
@@ -12,30 +12,56 @@ from workbench.test.workbenchtests import runTests
 
 class TestFitPropertyBrowser(WorkbenchGuiTest):
 
-    def test_fit_range(self):
+    def start(self):
         ws = Load(r'irs26176_graphite002_conv_1LFixF_s0_to_9_Result.nxs', OutputWorkspace='ws')
         plot([ws], [1])
         manager = GlobalFigureManager.get_active()
-        w = manager.window
-        trigger_action(find_action_with_text(w, 'Fit'))
+        self.w = manager.window
+        trigger_action(find_action_with_text(self.w, 'Fit'))
         yield 0.1
-        fit_browser = manager.fit_browser
-        start_x = fit_browser.startX()
-        start_x_pxl = fit_browser.tool.fit_start_x.get_x_in_pixels()
-        end_x = fit_browser.endX()
-        end_x_pxl = fit_browser.tool.fit_end_x.get_x_in_pixels()
+        self.fit_browser = manager.fit_browser
+
+    def test_fit_range(self):
+        yield self.start()
+        start_x = self.fit_browser.startX()
+        start_x_pxl = self.fit_browser.tool.fit_start_x.get_x_in_pixels()
+        end_x = self.fit_browser.endX()
+        end_x_pxl = self.fit_browser.tool.fit_end_x.get_x_in_pixels()
         self.assertAlmostEqual(start_x, 0.5318, 4)
         self.assertAlmostEqual(end_x, 1.8186, 4)
-        pos = w._canvas.geometry().center()
-        canvas = w.childAt(pos)
-        pos.setX(fit_browser.tool.fit_start_x.get_x_in_pixels())
+        pos = self.w._canvas.geometry().center()
+        canvas = self.w.childAt(pos)
+        pos.setX(self.fit_browser.tool.fit_start_x.get_x_in_pixels())
         new_pos = pos + QPoint(100, 0)
         yield drag_mouse(canvas, pos, new_pos)
-        self.assertAlmostEqual(fit_browser.startX(), start_x + 100.0 / (end_x_pxl - start_x_pxl) * (end_x - start_x), 2)
-        pos.setX(fit_browser.tool.fit_end_x.get_x_in_pixels())
+        self.assertAlmostEqual(self.fit_browser.startX(), start_x + 100.0 / (end_x_pxl - start_x_pxl) * (end_x - start_x), 2)
+        pos.setX(self.fit_browser.tool.fit_end_x.get_x_in_pixels())
         new_pos = pos - QPoint(30, 0)
         yield drag_mouse(canvas, pos, new_pos)
-        self.assertAlmostEqual(fit_browser.endX(), end_x - 30.0 / (end_x_pxl - start_x_pxl) * (end_x - start_x), 2)
+        self.assertAlmostEqual(self.fit_browser.endX(), end_x - 30.0 / (end_x_pxl - start_x_pxl) * (end_x - start_x), 2)
+
+    def test_fit_range_start_moved_too_far(self):
+        yield self.start()
+        start_x = self.fit_browser.startX()
+        end_x = self.fit_browser.endX()
+        start_x_pxl = self.fit_browser.tool.fit_start_x.get_x_in_pixels()
+        end_x_pxl = self.fit_browser.tool.fit_end_x.get_x_in_pixels()
+        self.assertAlmostEqual(start_x, 0.5318, 4)
+        self.assertAlmostEqual(end_x, 1.8186, 4)
+
+        pos = self.w._canvas.geometry().center()
+        canvas = self.w.childAt(pos)
+        pos.setX(self.fit_browser.tool.fit_end_x.get_x_in_pixels())
+        new_pos = pos - QPoint(100, 0)
+        yield drag_mouse(canvas, pos, new_pos)
+        new_end_x = self.fit_browser.endX()
+        self.assertAlmostEqual(new_end_x, end_x - 100.0 / (end_x_pxl - start_x_pxl) * (end_x - start_x), 2)
+
+        pos.setX(self.fit_browser.tool.fit_start_x.get_x_in_pixels())
+        new_pos = pos + QPoint()
+        new_pos.setX(end_x_pxl)
+        yield drag_mouse(canvas, pos, new_pos)
+        self.assertAlmostEqual(self.fit_browser.startX(), new_end_x)
 
 
 runTests(TestFitPropertyBrowser)
