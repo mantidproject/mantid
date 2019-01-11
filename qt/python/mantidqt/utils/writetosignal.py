83bf7fd208091bb6bfbbbbd9aeed1c66d0c19b16
Dimitar Tasev <dimtasev@gmail.com>
2019-01-11 18:08:26 +0000

    Add a check for an external console to write to, re #24439

--- qt/python/mantidqt/utils/writetosignal.py
+++ qt/python/mantidqt/utils/writetosignal.py
@@ -9,10 +9,12 @@
 #
 from __future__ import (absolute_import)
 
-# std imports
-
 # 3rdparty imports
+import sys
 from qtpy.QtCore import QObject, Signal
+
+
+# std imports
 
 
 class WriteToSignal(QObject):
@@ -20,9 +22,14 @@ class WriteToSignal(QObject):
     used to transform write requests to
     Qt-signals. Mainly used to communicate
     stdout/stderr across threads"""
+
     def __init__(self, original_out):
         QObject.__init__(self)
-        self.__original_out = original_out
+        # If the file descriptor of the stream is -2 then we are running in a no-external-console mode
+        if self.__original_out.fileno() == -2:
+            self.__original_out = None
+        else:
+            self.__original_out = original_out
 
     sig_write_received = Signal(str)
 
@@ -36,7 +43,13 @@ class WriteToSignal(QObject):
         return False
 
     def write(self, txt):
-        # write to the console
-        self.__original_out.write(txt)
-        # emit the signal which will write to logging
+        if self.__original_out:
+            # write to the console
+            try:
+                self.__original_out.write(txt)
+            except IOError:
+                self.sig_write_received.emit("Error: Unable to write to the console of the process.\n"
+                                             "This error is not related to your script's execution.\n\n")
+
+        # always write to the message log
         self.sig_write_received.emit(txt)
