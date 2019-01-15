865ebd76e640075f40183fe7af8423838f15f927
Sam Jenkins <s.jenkins@stfc.ac.uk>
2019-01-15 16:36:50 +0000

    Re #24512 updated calibration to work with enginX

--- scripts/Engineering/EngineeringCalibration.py
+++ scripts/Engineering/EngineeringCalibration.py
@@ -6,29 +6,31 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from __future__ import (absolute_import, division, print_function)
 import mantid.simpleapi as simple
-import os
-
-spec_nos = None
-crop_name = "Cropped"
-spec_num_used = False
-van_run = "236516"
-ceria_run = "241391"
-van_name = "ENGINX" + ("0" * (8 - len(van_run))) + van_run
-van_file_name = r"~/Work/Build-1/ExternalData/Testing/Data/UnitTest/" + van_name
-curve_van = van_file_name + "_precalculated_vanadium_run_bank_curves.nxs"
-int_van = van_file_name + "_precalculated_vanadium_run_integration.nxs"
-van_curves_ws = simple.Load(curve_van, OutputWorkspace="curves_van")
-van_integrated_ws = simple.Load(int_van, OutputWorkspace="int_van")
-ceria_ws = simple.Load(Filename="ENGINX241391", OutputWorkspace="eng_calib")
 
 
-def create_calibration_cropped_file(use_spectrum_number):
-    if spec_num_used:
+def create_vanadium_workspaces(vanadium_run, curve_van, int_van):
+    van_name = "eng_vanadium_ws"
+    simple.Load(vanadium_run, OutputWorkspace=van_name)
+    simple.EnggVanadiumCorrections(VanadiumWorkspace=van_name,
+                                   OutIntegrationWorkspace="eng_vanadium_integration",
+                                   OutCurvesWorkspace="eng_vanadium_curves")
+    simple.SaveNexus("eng_vanadium_integration", int_van)
+    simple.SaveNexus("eng_vanadium_curves", curve_van)
+    simple.DeleteWorkspace(van_name)
+
+
+def create_calibration_cropped_file(use_spectrum_number, spec_nos, crop_name, curve_van, int_van, ceria_run):
+    van_curves_ws, van_integrated_ws = load_van_files(curve_van, int_van)
+    ceria_ws = simple.Load(Filename="ENGINX"+ceria_run, OutputWorkspace="eng_calib")
+    if use_spectrum_number:
+        if crop_name is None:
+            param_tbl_name = "cropped"
+        else:
             param_tbl_name = crop_name
-            simple.EnggCalibrate(InputWorkspace=ceria_ws, VanIntegrationWorkspace=van_integrated_ws,
-                                 VanCurvesWorkspace=van_curves_ws, SpectrumNumbers=str(spec_nos),
-                                 FittedPeaks=param_tbl_name,
-                                 OutputParametersTableName=param_tbl_name)
+        simple.EnggCalibrate(InputWorkspace=ceria_ws, VanIntegrationWorkspace=van_integrated_ws,
+                             VanCurvesWorkspace=van_curves_ws, SpectrumNumbers=str(spec_nos),
+                             FittedPeaks=param_tbl_name,
+                             OutputParametersTableName=param_tbl_name)
     else:
         if spec_nos == "North":
             param_tbl_name = "engg_calibration_bank_1"
@@ -41,14 +43,20 @@ def create_calibration_cropped_file(use_spectrum_number):
                              OutputParametersTableName=param_tbl_name)
 
 
-def create_calibration_files():
+def create_calibration_files(curve_van, int_van, ceria_run):
+    van_curves_ws, van_integrated_ws = load_van_files(curve_van, int_van)
+    ceria_ws = simple.Load(Filename="ENGINX"+ceria_run, OutputWorkspace="eng_calib")
     banks = 3
     for i in range(1, banks):
-        outFitParamsTblName = "engg_calibration_bank_" + str(i)
-        print(outFitParamsTblName)
+        param_tbl_name = "engg_calibration_bank_{}".format(i)
+        print(param_tbl_name)
         simple.EnggCalibrate(InputWorkspace=ceria_ws, VanIntegrationWorkspace=van_integrated_ws,
-                             VanCurvesWorkspace=van_curves_ws, Bank=str(i), FittedPeaks=outFitParamsTblName,
-                             OutputParametersTableName=outFitParamsTblName)
+                             VanCurvesWorkspace=van_curves_ws, Bank=str(i), FittedPeaks=param_tbl_name,
+                             OutputParametersTableName=param_tbl_name)
 
 
-print("Calibration done")
+def load_van_files(curves_van, ints_van):
+    van_curves_ws = simple.Load(curves_van, OutputWorkspace="curves_van")
+    van_integrated_ws = simple.Load(ints_van, OutputWorkspace="int_van")
+    return van_curves_ws, van_integrated_ws
+
