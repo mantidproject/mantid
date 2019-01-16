d5dc8d96392f9bc70efc7e343aa08e45628f1449
Andrei Savici <saviciat@ornl.gov>
2019-01-15 22:39:58 -0500

    Re #23320. Fix Flake8 warnings

--- Testing/SystemTests/tests/analysis/MDNormHYSPECTest.py
+++ Testing/SystemTests/tests/analysis/MDNormHYSPECTest.py
@@ -11,10 +11,11 @@ System test for MDNorm
 """
 
 from mantid.simpleapi import *
-import os
 import systemtesting
 
-class MDNormHYSPECTest(systemtesting.MantidSystemTest):    
+
+class MDNormHYSPECTest(systemtesting.MantidSystemTest):
+
     tolerance=1e-8
 
     def requiredMemoryMB(self):
@@ -27,46 +28,46 @@ class MDNormHYSPECTest(systemtesting.MantidSystemTest):
         config.setFacility('SNS')
         Load(Filename='HYS_13656-13658',OutputWorkspace='sum')
         SetGoniometer(Workspace='sum', Axis0='s1,0,1,0,1')
-        GenerateEventsFilter(InputWorkspace='sum', 
-                             OutputWorkspace='splboth', 
-                             InformationWorkspace='info', 
-                             UnitOfTime='Nanoseconds', 
-                             LogName='s1', 
-                             MaximumLogValue=90, 
+        GenerateEventsFilter(InputWorkspace='sum',
+                             OutputWorkspace='splboth',
+                             InformationWorkspace='info',
+                             UnitOfTime='Nanoseconds',
+                             LogName='s1',
+                             MaximumLogValue=90,
                              LogValueInterval=1)
-        FilterEvents(InputWorkspace='sum', 
-                     SplitterWorkspace='splboth',  
-                     InformationWorkspace='info', 
-                     FilterByPulseTime=True, 
+        FilterEvents(InputWorkspace='sum',
+                     SplitterWorkspace='splboth',
+                     InformationWorkspace='info',
+                     FilterByPulseTime=True,
                      GroupWorkspaces=True,
                      OutputWorkspaceIndexedFrom1=True,
                      OutputWorkspaceBaseName='split')
         DeleteWorkspace('sum')
         DeleteWorkspace('splboth')
-        DeleteWorkspace('info')             
+        DeleteWorkspace('info')
         DgsReduction(SampleInputWorkspace='split',
-                     SampleInputMonitorWorkspace='split_1', 
-                     IncidentEnergyGuess=50, 
-                     SofPhiEIsDistribution=False, 
-                     TimeIndepBackgroundSub=True, 
-                     TibTofRangeStart=10400, 
-                     TibTofRangeEnd=12400, 
+                     SampleInputMonitorWorkspace='split_1',
+                     IncidentEnergyGuess=50,
+                     SofPhiEIsDistribution=False,
+                     TimeIndepBackgroundSub=True,
+                     TibTofRangeStart=10400,
+                     TibTofRangeEnd=12400,
                      OutputWorkspace='reduced')
-        SetUB(Workspace='reduced', 
-              a=5.823, 
-              b=6.475, 
-              c=3.186, 
-              u='0,1,0', 
+        SetUB(Workspace='reduced',
+              a=5.823,
+              b=6.475,
+              c=3.186,
+              u='0,1,0',
               v='0,0,1')
-        CropWorkspaceForMDNorm(InputWorkspace='reduced', 
-                               XMin=-25, 
-                               XMax=49, 
+        CropWorkspaceForMDNorm(InputWorkspace='reduced',
+                               XMin=-25,
+                               XMax=49,
                                OutputWorkspace='reduced')
-        ConvertToMD(InputWorkspace='reduced', 
-                    QDimensions='Q3D', 
-                    Q3DFrames='Q_sample', 
-                    OutputWorkspace='md', 
-                    MinValues='-11,-11,-11,-25', 
+        ConvertToMD(InputWorkspace='reduced',
+                    QDimensions='Q3D',
+                    Q3DFrames='Q_sample',
+                    OutputWorkspace='md',
+                    MinValues='-11,-11,-11,-25',
                     MaxValues='11,11,11,49')
         DeleteWorkspace("split")
         DeleteWorkspace("reduced")
@@ -74,17 +75,17 @@ class MDNormHYSPECTest(systemtesting.MantidSystemTest):
         DeleteWorkspace("TOFCorrectWS")
         MergeMD(InputWorkspaces='md', OutputWorkspace='merged')
         DeleteWorkspace("md")
-        MDNorm(InputWorkspace='merged', 
-               Dimension0Name='QDimension2', 
-               Dimension0Binning='-5,0.05,5', 
-               Dimension1Name='QDimension3', 
-               Dimension1Binning='-5,0.05,5', 
-               Dimension2Name='DeltaE', 
-               Dimension2Binning='-2,2', 
-               Dimension3Name='QDimension1', 
-               Dimension3Binning='-0.5,0.5', 
+        MDNorm(InputWorkspace='merged',
+               Dimension0Name='QDimension2',
+               Dimension0Binning='-5,0.05,5',
+               Dimension1Name='QDimension3',
+               Dimension1Binning='-5,0.05,5',
+               Dimension2Name='DeltaE',
+               Dimension2Binning='-2,2',
+               Dimension3Name='QDimension1',
+               Dimension3Binning='-0.5,0.5',
                SymmetryOperations='x,y,z;x,-y,z;x,y,-z;x,-y,-z',
-               OutputWorkspace='result', 
+               OutputWorkspace='result',
                OutputDataWorkspace='dataMD',
                OutputNormalizationWorkspace='normMD')
         DeleteWorkspace("dataMD")
