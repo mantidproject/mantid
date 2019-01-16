515d391a59920fd9d30d2f4af8f81e7052b9c2db
Dimitar Borislavov Tasev <dimtasev@gmail.com>
2019-01-16 10:29:03 +0000

    Add line before future import, re #24427

--- Framework/PythonInterface/test/python/mantid/plots/helperfunctionsTest.py
+++ Framework/PythonInterface/test/python/mantid/plots/helperfunctionsTest.py
@@ -4,6 +4,7 @@
 #     NScD Oak Ridge National Laboratory, European Spallation Source
 #     & Institut Laue - Langevin
 # SPDX - License - Identifier: GPL - 3.0 +
+#
 from __future__ import (absolute_import, division, print_function)
 
 from mantid.simpleapi import CreateWorkspace, DeleteWorkspace, CreateMDHistoWorkspace, \
