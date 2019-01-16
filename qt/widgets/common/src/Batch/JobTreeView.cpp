927f12bcfbe114a3475a227585df307d5e2e4805
Tom Titcombe <t.j.titcombe@gmail.com>
2019-01-16 15:10:11 +0000

    Fix clang issues for real this time Refs #0

--- qt/widgets/common/src/Batch/JobTreeView.cpp
+++ qt/widgets/common/src/Batch/JobTreeView.cpp
@@ -461,13 +461,14 @@ void JobTreeView::appendAndEditAtChildRow() {
 
 void JobTreeView::appendAndEditAtRowBelow() {
   auto current = currentIndex();
-  if (current != m_mainModel.index(-1,-1)) {
+  if (current != m_mainModel.index(-1, -1)) {
     auto const below = findOrMakeCellBelow(fromFilteredModel(current));
     auto index = below.first;
     auto isNew = below.second;
 
     if (isNew)
-      m_notifyee->notifyRowInserted(rowLocation().atIndex(mapToMainModel(index)));
+      m_notifyee->notifyRowInserted(
+          rowLocation().atIndex(mapToMainModel(index)));
     editAt(index);
   }
 }
@@ -485,7 +486,8 @@ void JobTreeView::enableFiltering() {
 
 void JobTreeView::keyPressEvent(QKeyEvent *event) {
   switch (event->key()) {
-  case Qt::Key_Return: case Qt::Key_Enter: {
+  case Qt::Key_Return:
+  case Qt::Key_Enter: {
     if (event->modifiers() & Qt::ControlModifier) {
       appendAndEditAtChildRow();
     } else if (event->modifiers() & Qt::ShiftModifier) {
