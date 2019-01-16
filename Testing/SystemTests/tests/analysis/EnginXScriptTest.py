9c6f6f1546732344eb18935000c0f2f515d580f7
Sam Jenkins <s.jenkins@stfc.ac.uk>
2019-01-16 08:59:55 +0000

    Re #24512 added vanadium systemtest

--- Testing/SystemTests/tests/analysis/EnginXScriptTest.py
+++ Testing/SystemTests/tests/analysis/EnginXScriptTest.py
@@ -7,4 +7,29 @@
 
 from __future__ import (absolute_import, division, print_function)
 import systemtesting
+import mantid.simpleapi as simple
 from Engineering.EnginX import EnginX
+
+
+class CreateVanadiumTest(systemtesting.MantidSystemTest):
+
+    def runTest(self):
+        test = EnginX(user="test", vanadium_run="236516",
+                      directory="/home/sjenkins/Work/Build-1/ExternalData/Testing/Data/SystemTest")
+        test.create_vanadium()
+
+    def validate(self):
+        return "eng_vanadium_integration", "engggui_vanadium_integration.nxs"
+
+    def cleanup(self):
+        simple.mtd.clear()
+
+
+class createCalibrationWholeTest(systemtesting.MantidSystemTest):
+
+    def runTest(self):
+        test = EnginX(user="test", vanadium_run="236516",
+                      directory="/home/sjenkins/Work/Build-1/ExternalData/Testing/Data/SystemTest")
+        test.create_calibration()
+
+
