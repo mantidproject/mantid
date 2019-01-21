05ea2eb2f6a481d0305f59acae0157bf857b070e
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-01-21 13:44:38 +0000

    refs #24322. Comments no longer apply

--- Framework/PythonInterface/mantid/geometry/src/Exports/DetectorInfoPythonIterator.cpp
+++ Framework/PythonInterface/mantid/geometry/src/Exports/DetectorInfoPythonIterator.cpp
@@ -25,13 +25,4 @@ void export_DetectorInfoPythonIterator() {
       .def("next", &DetectorInfoPythonIterator::next)
 #endif
       ;
-  /*
-   Return value policy for next is to copy the const reference. Copy by value is
-   essential for python 2.0 compatibility because items (DetectorInfoItem) will
-   outlive their iterators if declared as part of for loops. There is no good
-   way to deal with this other than to force a copy so that internals of the
-   item are not also corrupted. Future developers may wish to choose a separte
-   policy for python 3.0 where this is not a concern, and const ref returns
-   would be faster.
-  */
 }
