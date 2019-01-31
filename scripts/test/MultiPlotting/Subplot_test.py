e0fec2db810bce9ae5d0f8f1b0766861314c6d1a
Anthony Lim <anthony.lim@stfc.ac.uk>
2019-01-31 12:51:47 +0000

    refs #24437 made multiplot test flake8

--- scripts/test/MultiPlotting/Subplot_test.py
+++ scripts/test/MultiPlotting/Subplot_test.py
@@ -9,7 +9,7 @@ import unittest
 from MultiPlotting.subplot.subPlot import subPlot
 from MultiPlotting.multiPlotting_context import PlottingContext
 
-from  Muon.GUI.Common import mock_widget
+from Muon.GUI.Common import mock_widget
 try:
     from unittest import mock
 except ImportError:
@@ -18,23 +18,23 @@ except ImportError:
 
 def rm_logic(name):
     if name == "two":
-       return False
+        return False
     return True
 
+
 class SubplotTest(unittest.TestCase):
+
     def setUp(self):
         self._qapp = mock_widget.mockQapp()
         context = PlottingContext()
         self.subplot = subPlot(context)
         self.subplot.canvas.draw = mock.MagicMock()
-    
 
     def setup_rm(self):
         self.subplot._raise_rm_window = mock.Mock()
         self.subplot._raise_selector_window = mock.Mock()
         self.subplot._get_rm_window = mock.Mock()
         self.subplot._createSelectWindow = mock.MagicMock()
- 
 
     def test_rmOnePlotNewWindow(self):
         self.subplot._rm_window = None
@@ -44,10 +44,10 @@ class SubplotTest(unittest.TestCase):
         self.setup_rm()
         self.subplot._rm()
 
-        self.assertEquals(self.subplot._raise_rm_window.call_count,0)
-        self.assertEquals(self.subplot._raise_selector_window.call_count,0)
-        self.assertEquals(self.subplot._get_rm_window.call_count,1)
-        self.assertEquals(self.subplot._createSelectWindow.call_count,0)
+        self.assertEquals(self.subplot._raise_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._raise_selector_window.call_count, 0)
+        self.assertEquals(self.subplot._get_rm_window.call_count, 1)
+        self.assertEquals(self.subplot._createSelectWindow.call_count, 0)
 
     def test_rmOnePlotOldWindow(self):
         self.subplot._rm_window = mock.Mock()
@@ -57,11 +57,10 @@ class SubplotTest(unittest.TestCase):
         self.setup_rm()
         self.subplot._rm()
 
-        self.assertEquals(self.subplot._raise_rm_window.call_count,1)
-        self.assertEquals(self.subplot._raise_selector_window.call_count,0)
-        self.assertEquals(self.subplot._get_rm_window.call_count,0)
-        self.assertEquals(self.subplot._createSelectWindow.call_count,0)
-
+        self.assertEquals(self.subplot._raise_rm_window.call_count, 1)
+        self.assertEquals(self.subplot._raise_selector_window.call_count, 0)
+        self.assertEquals(self.subplot._get_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._createSelectWindow.call_count, 0)
 
     def test_rmTwoPlotsNewWindow(self):
         self.subplot._rm_window = None
@@ -72,11 +71,10 @@ class SubplotTest(unittest.TestCase):
         self.setup_rm()
         self.subplot._rm()
 
-        self.assertEquals(self.subplot._raise_rm_window.call_count,0)
-        self.assertEquals(self.subplot._raise_selector_window.call_count,0)
-        self.assertEquals(self.subplot._get_rm_window.call_count,0)
-        self.assertEquals(self.subplot._createSelectWindow.call_count,1)
-
+        self.assertEquals(self.subplot._raise_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._raise_selector_window.call_count, 0)
+        self.assertEquals(self.subplot._get_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._createSelectWindow.call_count, 1)
 
     def test_rmTwoPlotsOldSelectWindow(self):
         self.subplot._rm_window = None
@@ -87,11 +85,10 @@ class SubplotTest(unittest.TestCase):
         self.setup_rm()
         self.subplot._rm()
 
-        self.assertEquals(self.subplot._raise_rm_window.call_count,0)
-        self.assertEquals(self.subplot._raise_selector_window.call_count,1)
-        self.assertEquals(self.subplot._get_rm_window.call_count,0)
-        self.assertEquals(self.subplot._createSelectWindow.call_count,0)
-
+        self.assertEquals(self.subplot._raise_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._raise_selector_window.call_count, 1)
+        self.assertEquals(self.subplot._get_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._createSelectWindow.call_count, 0)
 
     def test_rmTwoPlotsoldRmWindow(self):
         self.subplot._rm_window = mock.Mock()
@@ -102,49 +99,56 @@ class SubplotTest(unittest.TestCase):
         self.setup_rm()
         self.subplot._rm()
 
-        self.assertEquals(self.subplot._raise_rm_window.call_count,1)
-        self.assertEquals(self.subplot._raise_selector_window.call_count,0)
-        self.assertEquals(self.subplot._get_rm_window.call_count,0)
-        self.assertEquals(self.subplot._createSelectWindow.call_count,0)
+        self.assertEquals(self.subplot._raise_rm_window.call_count, 1)
+        self.assertEquals(self.subplot._raise_selector_window.call_count, 0)
+        self.assertEquals(self.subplot._get_rm_window.call_count, 0)
+        self.assertEquals(self.subplot._createSelectWindow.call_count, 0)
 
     def setup_applyRm(self):
-       self.subplot._rm_window = mock.Mock()
-       self.subplot._rm_window.subplot = "test"
-       self.subplot._context.subplots["test"] = mock.MagicMock()
-       self.subplot._remove_subplot = mock.Mock()
-       self.subplot._close_rm_window = mock.Mock()
+        self.subplot._rm_window = mock.Mock()
+        self.subplot._rm_window.subplot = "test"
+        self.subplot._context.subplots["test"] = mock.MagicMock()
+        self.subplot._remove_subplot = mock.Mock()
+        self.subplot._close_rm_window = mock.Mock()
 
     def test_applyRmAll(self):
-       names =["one","two","three"]
-       self.setup_applyRm()
-       self.subplot._rm_window.getState = mock.Mock(return_value = True)
+        names = ["one", "two", "three"]
+        self.setup_applyRm()
+        self.subplot._rm_window.getState = mock.Mock(return_value=True)
 
-       self.subplot._applyRm(names)
+        self.subplot._applyRm(names)
 
-       self.assertEquals(self.subplot._context.subplots["test"].removeLine.call_count,3)
-       self.assertEquals(self.subplot._close_rm_window.call_count,1)
-
+        self.assertEquals(
+            self.subplot._context.subplots[
+                "test"].removeLine.call_count,
+            3)
+        self.assertEquals(self.subplot._close_rm_window.call_count, 1)
 
     def test_applyRmNone(self):
-       names =["one","two","three"]
-       self.setup_applyRm()
-       self.subplot._rm_window.getState = mock.Mock(return_value = False)
+        names = ["one", "two", "three"]
+        self.setup_applyRm()
+        self.subplot._rm_window.getState = mock.Mock(return_value=False)
 
-       self.subplot._applyRm(names)
+        self.subplot._applyRm(names)
 
-       self.assertEquals(self.subplot._context.subplots["test"].removeLine.call_count,0)
-       self.assertEquals(self.subplot._close_rm_window.call_count,1)
-
+        self.assertEquals(
+            self.subplot._context.subplots[
+                "test"].removeLine.call_count,
+            0)
+        self.assertEquals(self.subplot._close_rm_window.call_count, 1)
 
     def test_applyRmSome(self):
-       names =["one","two","three"]
-       self.setup_applyRm()
-       self.subplot._rm_window.getState = mock.Mock(side_effect = rm_logic)
+        names = ["one", "two", "three"]
+        self.setup_applyRm()
+        self.subplot._rm_window.getState = mock.Mock(side_effect=rm_logic)
 
-       self.subplot._applyRm(names)
+        self.subplot._applyRm(names)
 
-       self.assertEquals(self.subplot._context.subplots["test"].removeLine.call_count,2)
-       self.assertEquals(self.subplot._close_rm_window.call_count,1)
+        self.assertEquals(
+            self.subplot._context.subplots[
+                "test"].removeLine.call_count,
+            2)
+        self.assertEquals(self.subplot._close_rm_window.call_count, 1)
 
 if __name__ == "__main__":
     unittest.main()
