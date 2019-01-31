9ed280ee8d19161e7b6ed2d1ad9312a9635d7411
Igor Gudich <igor.gudich@esss.se>
2019-01-31 16:50:02 +0100

    Correct formatting in python file
    re #0

--- Testing/SystemTests/tests/analysis/ConvToMDCompareDefaultVsIndexing.py
+++ Testing/SystemTests/tests/analysis/ConvToMDCompareDefaultVsIndexing.py
@@ -9,10 +9,12 @@
 import systemtesting
 from mantid.simpleapi import *
 
+
 class ConvToMDCompareDefaultVsIndexung(systemtesting.MantidSystemTest):
     @staticmethod
     def indexed_name(func):
         return '{}_ws_indexed'.format(func)
+
     @staticmethod
     def default_name(func):
         return '{}_ws_default'.format(func)
@@ -26,35 +28,16 @@ class ConvToMDCompareDefaultVsIndexung(systemtesting.MantidSystemTest):
     def runTest(self):
         for func in self.test_functions:
             data_name = '{}_data'.format(func)
-            CreateSampleWorkspace(WorkspaceType='Event',
-                                 OutputWorkspace=data_name,
-                                 Function='Multiple Peaks',
-                                 XMin='10000',
-                                 XMax='100000',
-                                 NumEvents='1000',
-                                 Random=False)
+            CreateSampleWorkspace(WorkspaceType='Event', OutputWorkspace=data_name, Function='Multiple Peaks',
+                                  XMin='10000', XMax='100000', NumEvents='1000', Random=False)
 
-            ConvertToMD(
-                OutputWorkspace=self.indexed_name(func),
-                SplitInto='2',
-                SplitThreshold='10',
-                ConverterType='Indexed',
-                InputWorkspace=data_name,
-                QDimensions='Q3D',
-                dEAnalysisMode='Elastic',
-                Q3DFrames='Q_lab'
-            )
+            ConvertToMD(OutputWorkspace=self.indexed_name(func), SplitInto='2', SplitThreshold='10',
+                        ConverterType='Indexed', InputWorkspace=data_name, QDimensions='Q3D', dEAnalysisMode='Elastic',
+                        Q3DFrames='Q_lab')
 
-            ConvertToMD(
-                OutputWorkspace=self.default_name(func),
-                SplitInto='2',
-                SplitThreshold='10',
-                ConverterType='Default',
-                InputWorkspace=data_name,
-                QDimensions='Q3D',
-                dEAnalysisMode='Elastic',
-                Q3DFrames='Q_lab'
-            )
+            ConvertToMD(OutputWorkspace=self.default_name(func), SplitInto='2', SplitThreshold='10',
+                        ConverterType='Default', InputWorkspace=data_name, QDimensions='Q3D', dEAnalysisMode='Elastic',
+                        Q3DFrames='Q_lab')
 
     def validate(self):
         out = True
@@ -67,15 +50,11 @@ class ConvToMDCompareDefaultVsIndexung(systemtesting.MantidSystemTest):
                 min.append(md1.getDimension(0).getMinimum())
                 max.append(md1.getDimension(0).getMaximum())
 
-            hmd1 = BinMD(InputWorkspace=md1,
-                         AxisAligned=True,
-                         AlignedDim0='Q_lab_x,{},{},10000'.format(min[0], max[0]),
+            hmd1 = BinMD(InputWorkspace=md1, AxisAligned=True, AlignedDim0='Q_lab_x,{},{},10000'.format(min[0], max[0]),
                          AlignedDim1='Q_lab_y,{},{},100'.format(min[1], max[1]),
                          AlignedDim2='Q_lab_z,{},{},10'.format(min[2], max[2]))
 
-            hmd2 = BinMD(InputWorkspace=md2,
-                         AxisAligned=True,
-                         AlignedDim0='Q_lab_x,{},{},10000'.format(min[0], max[0]),
+            hmd2 = BinMD(InputWorkspace=md2, AxisAligned=True, AlignedDim0='Q_lab_x,{},{},10000'.format(min[0], max[0]),
                          AlignedDim1='Q_lab_y,{},{},100'.format(min[1], max[1]),
                          AlignedDim2='Q_lab_z,{},{},10'.format(min[2], max[2]))
 
