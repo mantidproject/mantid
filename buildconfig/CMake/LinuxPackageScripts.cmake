707dfcc7a17d8cfbf4c2e87dd6190dcd838af5a7
Pete Peterson <petersonpf@ornl.gov>
2019-02-13 15:20:27 -0500

    Inject the correct PYTHONPATH into error reporter
    This way uses the exact same mechanism that is used for launching mantid
    so it should be updated as necessary already. Only the linux launching
    scripts were modified.
    
    Fixes #24762

--- buildconfig/CMake/LinuxPackageScripts.cmake
+++ buildconfig/CMake/LinuxPackageScripts.cmake
@@ -205,7 +205,7 @@ if [ -n \"\$1\" ] && [ \"\$1\" = \"--debug\" ]; then
     GDB=\"gdb --args\"
 fi" )
 
-set ( ERROR_CMD "ErrorReporter/error_dialog_app.py --exitcode=\$? --directory=\$INSTALLDIR/bin" )
+set ( ERROR_CMD "ErrorReporter/error_dialog_app.py --exitcode=\$?" )
 
 # Local dev version
 if ( MAKE_VATES )
