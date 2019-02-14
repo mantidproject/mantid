310b02918fe640de492a4625524ea9fabd4e46d1
Dimitar Tasev <dimtasev@gmail.com>
2019-02-14 11:22:18 +0000

    Add delete test, add to cmakelists

--- qt/python/mantidqt/widgets/samplelogs/test/test_samplelogs_view.py
+++ qt/python/mantidqt/widgets/samplelogs/test/test_samplelogs_view.py
@@ -5,12 +5,21 @@
 #     & Institut Laue - Langevin
 # SPDX - License - Identifier: GPL - 3.0 +
 #  This file is part of the mantid workbench.
+from qtpy.QtWidgets import QApplication
+
 from mantid.simpleapi import CreateSampleWorkspace
 from mantidqt.utils.qt.test import GuiTest
+from mantidqt.utils.qt.test.qt_widget_finder import QtWidgetFinder
 from mantidqt.widgets.samplelogs.presenter import SampleLogs
 
 
-class SampleLogsViesTest(GuiTest):
+class SampleLogsViewTest(GuiTest, QtWidgetFinder):
     def test_deleted_on_close(self):
         ws = CreateSampleWorkspace()
         pres = SampleLogs(ws)
+        self.assert_window_created()
+        pres.view.close()
+
+        QApplication.processEvents()
+
+        self.assert_no_widgets()
