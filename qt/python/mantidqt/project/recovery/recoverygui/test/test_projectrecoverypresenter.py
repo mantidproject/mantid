825e5a6611186db64fd3b21f00de1fd771d53c78
Samuel Jones <samjones714@gmail.com>
2019-02-15 10:01:51 +0000

    Re #24656 Project recovery GUI presenter tests added

--- qt/python/mantidqt/project/recovery/recoverygui/test/test_projectrecoverypresenter.py
+++ qt/python/mantidqt/project/recovery/recoverygui/test/test_projectrecoverypresenter.py
@@ -8,3 +8,207 @@
 #
 
 import unittest
+import mock
+
+from mantidqt.project.recovery.recoverygui.projectrecoverypresenter import ProjectRecoveryPresenter
+
+
+def raise_exception():
+    raise Exception
+
+
+class ProjectRecoveryPresenterTest(unittest.TestCase):
+    def setUp(self):
+        self.prp = ProjectRecoveryPresenter(mock.MagicMock())
+        self.prp.current_view = mock.MagicMock()
+        self.prp.model = mock.MagicMock()
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.ProjectRecoveryWidgetView')
+    def test_start_recovery_view_exception_raised(self, view):
+        self.prp.current_view = None
+        view.side_effect = raise_exception
+
+        self.assertTrue(self.prp.start_recovery_view())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.ProjectRecoveryWidgetView')
+    def test_start_recovery_view_start_mantid_normally_called(self, _):
+        self.prp.current_view = None
+        self.prp.start_mantid_normally_called = True
+
+        self.assertTrue(not self.prp.start_recovery_view())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.ProjectRecoveryWidgetView')
+    def test_start_recovery_view_model_has_a_failed_run(self, _):
+        self.prp.current_view = None
+        self.prp.model.get_failed_run.return_value = True
+
+        self.assertTrue(self.prp.start_recovery_view())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.ProjectRecoveryWidgetView')
+    def test_start_recovery_view_successful_run(self, _):
+        self.prp.current_view = None
+        self.prp.model.get_failed_run.return_value = False
+
+        self.assertTrue(not self.prp.start_recovery_view())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.RecoveryFailureView')
+    def test_start_recovery_failure_start_mantid_normally_called(self, _):
+        self.prp.current_view = None
+        self.prp.start_mantid_normally_called = True
+
+        self.assertTrue(not self.prp.start_recovery_failure())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.RecoveryFailureView')
+    def test_start_recovery_failure_exception_raised(self, view):
+        self.prp.current_view = None
+        view.side_effect = raise_exception
+
+        self.assertTrue(self.prp.start_recovery_failure())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.RecoveryFailureView')
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.ProjectRecoveryModel')
+    def test_start_recovery_failure_model_has_a_failed_run(self, model, _):
+        self.prp.current_view = None
+        model.return_value.get_failed_run.return_value = True
+
+        self.assertTrue(self.prp.start_recovery_failure())
+
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.RecoveryFailureView')
+    @mock.patch('mantidqt.project.recovery.recoverygui.projectrecoverypresenter.ProjectRecoveryModel')
+    def test_start_recovery_failure_successful_run(self, model, _):
+        self.prp.current_view = None
+        model.return_value.get_failed_run.return_value = False
+
+        self.assertTrue(not self.prp.start_recovery_failure())
+
+    def test_get_row_empty_list(self):
+        self.prp.model.get_row.return_value = []
+
+        self.assertEqual(self.prp.get_row(0), ["", "", ""])
+
+    def test_get_row_actual_list(self):
+        self.prp.model.get_row.return_value = ["Checkpoint1", "20", "No"]
+
+        self.assertEqual(self.prp.get_row(0), ["Checkpoint1", "20", "No"])
+
+    def test_recover_last_recovery_started(self):
+        self.prp.model.has_recovery_started.return_value = True
+
+        self.assertIsNone(self.prp.recover_last())
+
+        self.prp.model.decide_last_checkpoint.assert_not_called()
+        self.prp.model.recover_selected_checkpoint.assert_not_called()
+
+    def test_recover_last(self):
+        self.prp.model.has_recovery_started.return_value = False
+
+        self.prp.recover_last()
+
+        self.prp.model.decide_last_checkpoint.assert_called_once()
+        self.prp.model.recover_selected_checkpoint.assert_called_once()
+
+    def test_open_last_in_editor_recovery_started(self):
+        self.prp.model.has_recovery_started.return_value = True
+
+        self.assertIsNone(self.prp.open_last_in_editor())
+
+        self.prp.model.decide_last_checkpoint.assert_not_called()
+        self.prp.model.open_selected_in_editor.assert_not_called()
+
+    def test_open_last_in_editor(self):
+        self.prp.model.has_recovery_started.return_value = False
+
+        self.prp.open_last_in_editor()
+
+        self.prp.model.decide_last_checkpoint.assert_called_once()
+        self.prp.model.open_selected_in_editor.assert_called_once()
+
+    def test_start_mantid_normally_self_not_allow_start_mantid_normally_and_current_view_is_none(self):
+        self.prp.current_view = None
+        self.prp.allow_start_mantid_normally = False
+
+        self.assertIsNone(self.prp.start_mantid_normally())
+
+    def test_start_mantid_normally_self_not_allow_start_mantid_normally_and_current_view_is_not_none(self):
+        self.prp.current_view = mock.MagicMock()
+        self.prp.allow_start_mantid_normally = False
+
+        self.prp.start_mantid_normally()
+
+        self.prp.current_view.emit_abort_script.assert_called_once()
+        self.assertTrue(self.prp.start_mantid_normally_called)
+        self.prp.model.start_mantid_normally.assert_called_once()
+
+    def test_start_mantid_normally_self_allow_start_mantid_normally(self):
+        self.prp.current_view = None
+        self.prp.allow_start_mantid_normally = True
+
+        self.prp.start_mantid_normally()
+
+        self.assertTrue(self.prp.start_mantid_normally_called)
+        self.prp.model.start_mantid_normally.assert_called_once()
+
+    def test_recover_selected_checkpoint_recovery_has_started(self):
+        self.prp.model.has_recovery_started.return_value = True
+
+        self.prp.recover_selected_checkpoint("1")
+
+        self.prp.model.recover_selected_checkpoint.assert_not_called()
+
+    def test_recover_selected_checkpoint(self):
+        self.prp.model.has_recovery_started.return_value = False
+
+        self.prp.recover_selected_checkpoint("1")
+
+        self.prp.model.recover_selected_checkpoint.assert_called_with("1")
+
+    def test_open_selected_checkpoint_in_editor(self):
+        self.prp.model.has_recovery_started.return_value = False
+
+        self.prp.open_selected_checkpoint_in_editor("1")
+
+        self.assertTrue(self.prp.open_selected_in_editor_selected)
+        self.prp.model.open_selected_in_editor.assert_called_with("1")
+
+    def test_open_selected_checkpoint_in_editor_recovery_has_started(self):
+        self.prp.model.has_recovery_started.return_value = True
+
+        self.prp.open_selected_checkpoint_in_editor("1")
+
+        self.assertTrue(not self.prp.open_selected_in_editor_selected)
+        self.prp.model.open_selected_in_editor.assert_not_called()
+
+    def test_close_view_is_none(self):
+        self.prp.current_view = None
+
+        self.assertIsNone(self.prp.close_view())
+
+    def test_close_view_is_not_none(self):
+        view = mock.MagicMock()
+        self.prp.current_view = view
+
+        self.prp.close_view()
+
+        view.setVisible.assert_called_with(False)
+        self.assertIsNone(self.prp.current_view)
+
+    def test_connect_progress_bar_to_recovery_view(self):
+        self.prp.connect_progress_bar_to_recovery_view()
+
+        self.prp.current_view.connect_progress_bar.assert_called_once()
+
+    def test_change_start_mantid_to_cancel_label(self):
+        self.prp.change_start_mantid_to_cancel_label()
+
+        self.prp.allow_start_mantid_normally = False
+        self.prp.current_view.change_start_mantid_button.assert_called_with("Cancel Recovery")
+
+    def test_set_up_progress_bar(self):
+        self.prp.set_up_progress_bar(1)
+
+        self.prp.current_view.set_progress_bar_maximum.assert_called_with(1)
+
+    def test_get_number_of_checkpoints(self):
+        self.prp.model.get_number_of_checkpoints.return_value = 1
+
+        self.assertEqual(self.prp.get_number_of_checkpoints(), 1)
