8f85107134c12490d14d460688afddd34897d837
Igor Gudich <igor.gudich@esss.se>
2019-02-18 11:56:40 +0100

    Description for Multiprocess option in LoadEventNexus
    re #0

--- Framework/DataHandling/src/LoadEventNexus.cpp
+++ Framework/DataHandling/src/LoadEventNexus.cpp
@@ -279,7 +279,7 @@ void LoadEventNexus::init() {
   auto loadTypeValidator = boost::make_shared<StringListValidator>(loadType);
   declareProperty("LoadType", "Default", loadTypeValidator,
                   "Set type of loader. 2 options {Default, Multiproceess},"
-                  "'Multiprocess' should work faster for big files and is "
+                  "'Multiprocess' should work faster for big files and it is "
                   "experimental, available only in Linux");
 }
 
