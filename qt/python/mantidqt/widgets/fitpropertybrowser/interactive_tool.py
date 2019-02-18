f16093f867af93fb411fab418747a291b97c25f9
Roman Tolchenov <roman.tolchenov@stfc.ac.uk>
2019-02-18 08:47:40 +0000

    Add the licence agrrement to the new files. Re #24434

--- qt/python/mantidqt/widgets/fitpropertybrowser/interactive_tool.py
+++ qt/python/mantidqt/widgets/fitpropertybrowser/interactive_tool.py
@@ -1,3 +1,9 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
 from qtpy.QtCore import QObject, Signal, Slot
 from qtpy.QtGui import QCursor
 from qtpy.QtWidgets import QApplication, QMenu, QInputDialog
