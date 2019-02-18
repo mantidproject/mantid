a32466af286a2406cacdb1a4b22e5e38a5c9570b
Vickie Lynch <lynchve@ornl.gov>
2019-02-18 13:23:50 -0500

    Refs #24808 change sign

--- Framework/CurveFitting/src/Functions/IkedaCarpenterPV.cpp
+++ Framework/CurveFitting/src/Functions/IkedaCarpenterPV.cpp
@@ -286,7 +286,7 @@ void IkedaCarpenterPV::constFunction(double *out, const double *xValues,
     std::complex<double> zs =
         std::complex<double>(-alpha * diff, 0.5 * alpha * gamma);
     std::complex<double> zu = (1 - k) * zs;
-    std::complex<double> zv = (1 - k) * zs;
+    std::complex<double> zv = (1 + k) * zs;
     std::complex<double> zr =
         std::complex<double>(-beta * diff, 0.5 * beta * gamma);
 
@@ -370,7 +370,7 @@ void IkedaCarpenterPV::functionLocal(double *out, const double *xValues,
     std::complex<double> zs =
         std::complex<double>(-alpha * diff, 0.5 * alpha * gamma);
     std::complex<double> zu = (1 - k) * zs;
-    std::complex<double> zv = (1 - k) * zs;
+    std::complex<double> zv = (1 + k) * zs;
     std::complex<double> zr =
         std::complex<double>(-beta * diff, 0.5 * beta * gamma);
 
