34479388aafbc587c50043c953f261093acfc675
Mathieu Doucet <matd10@yahoo.com>
2019-02-22 16:53:15 -0500

    Leave points with big error

--- Framework/PythonInterface/plugins/algorithms/MagnetismReflectometryReduction.py
+++ Framework/PythonInterface/plugins/algorithms/MagnetismReflectometryReduction.py
@@ -505,10 +505,10 @@ class MagnetismReflectometryReduction(PythonAlgorithm):
         data_e = q_rebin.dataE(0)
 
         # Values < 1e-12 and values where the error is greater than the value are replaced by 0+-1
-        for i in range(len(data_y)):
-            if data_y[i] < 1e-12 or data_e[i]>data_y[i]:
-                data_y[i]=0.0
-                data_e[i]=1.0
+        #for i in range(len(data_y)):
+        #    if data_y[i] < 1e-12 or data_e[i]>data_y[i]:
+        #        data_y[i]=0.0
+        #        data_e[i]=1.0
 
         # Sanity check
         if sum(data_y) == 0:
