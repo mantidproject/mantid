a7c385d0a34bdd3ad27ad9bbc66c9bf6c450426a
Dimitar Tasev <dimtasev@gmail.com>
2019-02-25 17:26:36 +0000

    Extract dir before ShowInExplorer.open_directory, re #24812

--- qt/python/mantidqt/widgets/codeeditor/tab_widget/codeeditor_tab_presenter.py
+++ qt/python/mantidqt/widgets/codeeditor/tab_widget/codeeditor_tab_presenter.py
@@ -18,7 +18,7 @@ class ShowInExplorer(object):
     @staticmethod
     def open_directory(path):
         if sys.platform == "win32":
-            os.startfile(os.path.dirname(path))
+            os.startfile(path)
         elif sys.platform == 'darwin':
             subprocess.check_call(['open', '--', path])
         elif sys.platform == 'linux2':
@@ -40,8 +40,8 @@ class CodeEditorTabPresenter(object):
             # the widget has been deleted without updating the last tab clicked
             return
 
-        dirname = widget.editor.fileName()
-        if "" != dirname:
-            ShowInExplorer.open_directory(dirname)
+        filepath = widget.editor.fileName()
+        if "" != filepath:
+            ShowInExplorer.open_directory(os.path.dirname(filepath))
         else:
             logger.notice("Selected tab is not saved to a file, and cannot be shown in explorer.")
