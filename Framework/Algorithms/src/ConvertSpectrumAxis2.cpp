d508d59dd3ef01aa128cf6c3c4e356ee8f96c891
Sam Jenkins <s.jenkins@stfc.ac.uk>
2019-02-25 10:01:58 +0000

    Revert "Re #24795 fixed algorithm failing on event ws"
    This reverts commit 0a3727a26e841da8970c0c928b2441f315f6ec7d.

--- Framework/Algorithms/src/ConvertSpectrumAxis2.cpp
+++ Framework/Algorithms/src/ConvertSpectrumAxis2.cpp
@@ -10,7 +10,6 @@
 #include "MantidAPI/Run.h"
 #include "MantidAPI/SpectraAxisValidator.h"
 #include "MantidAPI/SpectrumInfo.h"
-#include "MantidDataObjects/Workspace2D.h"
 #include "MantidDataObjects/WorkspaceCreation.h"
 #include "MantidGeometry/Instrument.h"
 #include "MantidGeometry/Instrument/DetectorInfo.h"
@@ -235,7 +234,7 @@ MatrixWorkspace_sptr ConvertSpectrumAxis2::createOutputWorkspace(
     builder.setY(inputWS->y(0).size());
     builder.setDistribution(inputWS->isDistribution());
     outputWorkspace =
-        create<HistoWorkspace>(*inputWS, m_indexMap.size(), builder.build());
+        create<MatrixWorkspace>(*inputWS, m_indexMap.size(), builder.build());
     std::vector<double> axis;
     axis.reserve(m_indexMap.size());
     for (const auto &it : m_indexMap) {
