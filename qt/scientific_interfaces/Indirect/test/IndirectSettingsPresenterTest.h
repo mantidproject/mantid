478b2f035ff7eff879cbb37bba6976bf33cd61cc
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2019-02-25 11:04:08 +0000

    Refs #24735. Settings presenter tests

--- qt/scientific_interfaces/Indirect/test/IndirectSettingsPresenterTest.h
+++ qt/scientific_interfaces/Indirect/test/IndirectSettingsPresenterTest.h
@@ -102,9 +102,139 @@ public:
   /// Unit tests to check for successful presenter instantiation
   ///----------------------------------------------------------------------
 
-  void test_that_the_model_has_been_instantiated_correctly() {}
+  void
+  test_that_calling_a_presenter_method_will_invoke_the_relevant_view_and_model_methods() {
+    QString const settingGroup("Data Analysis");
+    checkForLoadingOfSettings(settingGroup);
+  }
+
+  ///----------------------------------------------------------------------
+  /// Unit Tests that test the views signals invoke the correct methods
+  ///----------------------------------------------------------------------
+
+  void
+  test_that_the_updateRestrictInputByName_signal_will_set_the_input_data_to_be_restricted_when_passed_ISIS() {
+    EXPECT_CALL(*m_view, setRestrictInputByNameChecked(true)).Times(1);
+    m_view->emitUpdateRestrictInputByName("ISIS");
+  }
+
+  void
+  test_that_the_updateRestrictInputByName_signal_will_set_the_input_data_to_be_unrestricted_when_not_passed_ISIS() {
+    EXPECT_CALL(*m_view, setRestrictInputByNameChecked(false)).Times(1);
+    m_view->emitUpdateRestrictInputByName("ILL");
+  }
+
+  void test_that_the_okClicked_signal_will_attempt_to_save_the_settings() {
+    QString const settingGroup("Data Analysis");
+
+    checkForSavingOfSettings(settingGroup);
+
+    m_view->emitOkClicked();
+  }
+
+  void test_that_the_applyClicked_signal_will_attempt_to_save_the_settings() {
+    QString const settingGroup("Data Analysis");
+
+    checkForSavingOfSettings(settingGroup);
+
+    m_view->emitApplyClicked();
+  }
+
+  void
+  test_that_the_applyClicked_signal_will_disable_the_settings_buttons_while_it_is_applying_the_changes() {
+    Expectation disableApply =
+        EXPECT_CALL(*m_view, setApplyEnabled(false)).Times(1);
+    Expectation disableOk = EXPECT_CALL(*m_view, setOkEnabled(false)).Times(1);
+    Expectation disableCancel =
+        EXPECT_CALL(*m_view, setCancelEnabled(false)).Times(1);
+
+    EXPECT_CALL(*m_view, setApplyEnabled(true)).Times(1).After(disableApply);
+    EXPECT_CALL(*m_view, setOkEnabled(true)).Times(1).After(disableOk);
+    EXPECT_CALL(*m_view, setCancelEnabled(true)).Times(1).After(disableCancel);
+
+    m_view->emitApplyClicked();
+  }
+
+  ///----------------------------------------------------------------------
+  /// Unit Tests that test the methods of the presenter
+  ///----------------------------------------------------------------------
+
+  void test_that_loadSettings_will_attempt_to_load_the_relevant_settings() {
+    QString const settingGroup("Data Analysis");
+    checkForLoadingOfSettings(settingGroup);
+  }
+
+  void test_that_getSetting_will_attempt_to_get_a_setting_from_the_view() {
+    QString const settingGroup("Data Analysis");
+    std::string const settingName("plot-error-bars");
+
+    ON_CALL(*m_model, getSettingsGroup())
+        .WillByDefault(Return(settingGroup.toStdString()));
+    ON_CALL(*m_view,
+            getSetting(settingGroup, QString::fromStdString(settingName)))
+        .WillByDefault(Return(true));
+
+    Expectation getSettingsGroup =
+        EXPECT_CALL(*m_model, getSettingsGroup())
+            .Times(1)
+            .WillOnce(Return(settingGroup.toStdString()));
+    EXPECT_CALL(*m_view,
+                getSetting(settingGroup, QString::fromStdString(settingName)))
+        .Times(1)
+        .After(getSettingsGroup);
+
+    m_presenter->getSetting(settingName);
+  }
 
 private:
+  void checkForLoadingOfSettings(QString const &settingGroup) {
+    ON_CALL(*m_model, isSettingAvailable("restrict-input-by-name"))
+        .WillByDefault(Return(true));
+    ON_CALL(*m_model, isSettingAvailable("plot-error-bars"))
+        .WillByDefault(Return(true));
+    ON_CALL(*m_model, getSettingsGroup())
+        .WillByDefault(Return(settingGroup.toStdString()));
+    ON_CALL(*m_view,
+            getSetting(settingGroup, QString("restrict-input-by-name")))
+        .WillByDefault(Return(true));
+    ON_CALL(*m_view, getSetting(settingGroup, QString("plot-error-bars")))
+        .WillByDefault(Return(true));
+
+    ExpectationSet expectations =
+        EXPECT_CALL(*m_model, isSettingAvailable("restrict-input-by-name"))
+            .Times(1)
+            .WillOnce(Return(true));
+    expectations +=
+        EXPECT_CALL(*m_view, setRestrictInputByNameChecked(true)).Times(1);
+    expectations += EXPECT_CALL(*m_model, isSettingAvailable("plot-error-bars"))
+                        .Times(1)
+                        .WillOnce(Return(true));
+    EXPECT_CALL(*m_view, setPlotErrorBarsChecked(true))
+        .Times(1)
+        .After(expectations);
+
+    m_presenter->loadSettings();
+  }
+
+  void checkForSavingOfSettings(QString const &settingGroup) {
+    ON_CALL(*m_model, getSettingsGroup())
+        .WillByDefault(Return(settingGroup.toStdString()));
+    ON_CALL(*m_model, isSettingAvailable("restrict-input-by-name"))
+        .WillByDefault(Return(false));
+    ON_CALL(*m_model, isSettingAvailable("plot-error-bars"))
+        .WillByDefault(Return(false));
+
+    EXPECT_CALL(*m_model, getSettingsGroup())
+        .Times(1)
+        .WillOnce(Return(settingGroup.toStdString()));
+    EXPECT_CALL(*m_model, isSettingAvailable("restrict-input-by-name"))
+        .Times(1)
+        .WillOnce(Return(false));
+    EXPECT_CALL(*m_model, isSettingAvailable("plot-error-bars"))
+        .Times(1)
+        .WillOnce(Return(false));
+  }
+
   MockIIndirectSettingsView *m_view;
   MockIndirectSettingsModel *m_model;
   std::unique_ptr<IndirectSettingsPresenter> m_presenter;
