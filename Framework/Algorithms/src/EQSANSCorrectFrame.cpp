1151afc5964a639f3e3f1e2e1e7cf0ca37c7d158
Jose Borreguero <borreguero@gmail.com>
2019-02-28 11:28:48 -0500

    Refs #24895 accept clangformat changes

--- Framework/Algorithms/src/EQSANSCorrectFrame.cpp
+++ Framework/Algorithms/src/EQSANSCorrectFrame.cpp
@@ -29,71 +29,74 @@ using namespace Mantid::Geometry;
 using Types::Event::TofEvent;
 
 void EQSANSCorrectFrame::init() {
-    declareProperty(make_unique<WorkspaceProperty<EventWorkspace>>(
-            "InputWorkspace", "", Direction::Input,
-            boost::make_shared<WorkspaceUnitValidator>("TOF")),
-                    "Workspace to apply the TOF correction to");
-    declareProperty("PulsePeriod", 1.0E6 / 60.0,
-                    "Period of the neutron pulse, in micro-seconds",
-                    Direction::Input);
-    declareProperty("MinTOF", EMPTY_DBL(),
-                    "Time of flight of fastest neutrons from the lead pulse, in micro-seconds",
-                    Direction::Input);
-    declareProperty("FrameWidth", 1.0E6 / 60.0,
-                    "Period of the chopper system, in micro-seconds",
-                    Direction::Input);
-    declareProperty("FrameSkipping", false,
-                    "If True, the data was taken in frame skipping mode",
-                    Direction::Input);
+  declareProperty(make_unique<WorkspaceProperty<EventWorkspace>>(
+                      "InputWorkspace", "", Direction::Input,
+                      boost::make_shared<WorkspaceUnitValidator>("TOF")),
+                  "Workspace to apply the TOF correction to");
+  declareProperty("PulsePeriod", 1.0E6 / 60.0,
+                  "Period of the neutron pulse, in micro-seconds",
+                  Direction::Input);
+  declareProperty("MinTOF", EMPTY_DBL(),
+                  "Time of flight of fastest neutrons from the lead pulse, in "
+                  "micro-seconds",
+                  Direction::Input);
+  declareProperty("FrameWidth", 1.0E6 / 60.0,
+                  "Period of the chopper system, in micro-seconds",
+                  Direction::Input);
+  declareProperty("FrameSkipping", false,
+                  "If True, the data was taken in frame skipping mode",
+                  Direction::Input);
 }
 
 void EQSANSCorrectFrame::exec() {
-    processAlgProperties();
-    EventWorkspace_sptr inputWS = getProperty("InputWorkspace");
-    const size_t numHists = inputWS->getNumberHistograms();
-    Progress progress(this, 0.0, 1.0, numHists);
+  processAlgProperties();
+  EventWorkspace_sptr inputWS = getProperty("InputWorkspace");
+  const size_t numHists = inputWS->getNumberHistograms();
+  Progress progress(this, 0.0, 1.0, numHists);
 
-    const double pulsePeriod = getProperty("PulsePeriod");
-    const double minTOF = getProperty("MinTOF");
-    const double minTOF_delayed = minTOF + pulsePeriod;
-    const double frameWidth = getProperty("FrameWidth");
-    const size_t nFrames = std::static_cast<size_t>(minTOF / frame_width);
-    const double frames_offset_time = frame_width * std::static_cast<double>(nFrames);
-    const bool isFrameSkipping = getProperty("FrameSkipping");
-    const auto &spectrumInfo = inputWS->spectrumInfo();
+  const double pulsePeriod = getProperty("PulsePeriod");
+  const double minTOF = getProperty("MinTOF");
+  const double minTOF_delayed = minTOF + pulsePeriod;
+  const double frameWidth = getProperty("FrameWidth");
+  const size_t nFrames = std::static_cast<size_t>(minTOF / frame_width);
+  const double frames_offset_time =
+      frame_width * std::static_cast<double>(nFrames);
+  const bool isFrameSkipping = getProperty("FrameSkipping");
+  const auto &spectrumInfo = inputWS->spectrumInfo();
 
-    // Loop through the spectra and apply correction
-    PARALLEL_FOR_IF(Kernel::threadSafe(*inputWS))
-    for (int64_t ispec = 0; ispec < int64_t(numHists); ++ispec) {
-        PARALLEL_START_INTERUPT_REGION
-        if (!spectrumInfo.hasDetectors(ispec)) {
-            g_log.warning() << "Workspace index " << ispec
-                            << " has no detector assigned to it - discarding\n";
-            continue;
-        }
-        std::vector<TofEvent> &events = inputWS->getSpectrum(ispec).getEvents();
-        if (events.empty())
-            continue;  // no events recorded in this spectrum
-        std::vector<TofEvent> clean_events;
-        double newTOF;
-        for (auto event : events) {
-            // shift times to the correct frame
-            newTOF = event.tof() + frames_offset_time;
-            // TOF values smaller than that of the fastest neutrons have been
-            // 'folded' by the data acquisition system. They must be shifted
-            if (newTOF < minTOF)
-                newTOF += frameWidth;
-            // Events from the skipped pulse are delayed by one pulse period
-            if (isFrameSkipping && newTOF > minTOF_delayed)
-                    newTOF += pulsePeriod;
-            clean_events.emplace_back(newtof, event.pulseTime());
-        }
-        events.clear();
-        events.reserve(clean_events.size());
-        std::copy(clean_events.begin(), clean_events.end(), std::back_inserter(events));
-        progress.report("Correct TOF frame");
-        PARALLEL_END_INTERUPT_REGION
+  // Loop through the spectra and apply correction
+  PARALLEL_FOR_IF(Kernel::threadSafe(*inputWS))
+  for (int64_t ispec = 0; ispec < int64_t(numHists); ++ispec) {
+    PARALLEL_START_INTERUPT_REGION
+    if (!spectrumInfo.hasDetectors(ispec)) {
+      g_log.warning() << "Workspace index " << ispec
+                      << " has no detector assigned to it - discarding\n";
+      continue;
     }
+    std::vector<TofEvent> &events = inputWS->getSpectrum(ispec).getEvents();
+    if (events.empty())
+      continue; // no events recorded in this spectrum
+    std::vector<TofEvent> clean_events;
+    double newTOF;
+    for (auto event : events) {
+      // shift times to the correct frame
+      newTOF = event.tof() + frames_offset_time;
+      // TOF values smaller than that of the fastest neutrons have been
+      // 'folded' by the data acquisition system. They must be shifted
+      if (newTOF < minTOF)
+        newTOF += frameWidth;
+      // Events from the skipped pulse are delayed by one pulse period
+      if (isFrameSkipping && newTOF > minTOF_delayed)
+        newTOF += pulsePeriod;
+      clean_events.emplace_back(newtof, event.pulseTime());
+    }
+    events.clear();
+    events.reserve(clean_events.size());
+    std::copy(clean_events.begin(), clean_events.end(),
+              std::back_inserter(events));
+    progress.report("Correct TOF frame");
+    PARALLEL_END_INTERUPT_REGION
+  }
   PARALLEL_CHECK_INTERUPT_REGION
 }
 
