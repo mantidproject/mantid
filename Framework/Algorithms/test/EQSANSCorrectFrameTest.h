1920283342584f1cb185f8d23445b03a74992413
Jose Borreguero <borreguero@gmail.com>
2019-03-05 15:51:42 -0500

    Refs #24895 unit test with various TOF cases

--- Framework/Algorithms/test/EQSANSCorrectFrameTest.h
+++ Framework/Algorithms/test/EQSANSCorrectFrameTest.h
@@ -1,11 +1,11 @@
-#ifndef EQSANSCORRECTFRAMETEST_H_
-#define EQSANSCORRECTFRAMETEST_H_
+#ifndef MANTID_ALGORITHMS_EQSANSCORRECTFRAMETEST_H_
+#define MANTID_ALGORITHMS_EQSANSCORRECTFRAMETEST_H_
 
 #include "MantidAlgorithms/EQSANSCorrectFrame.h"
 #include "MantidDataObjects/EventList.h"
 #include "MantidDataObjects/EventWorkspace.h"
 #include "MantidDataObjects/Events.h"
-#include "MantidKernel/Unit.h"
+#include "MantidAPI/Axis.h"
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
 
 using namespace Mantid;
@@ -14,7 +14,6 @@ using namespace Mantid::API;
 using namespace Mantid::DataObjects;
 using namespace Mantid::Geometry;
 using Types::Event::TofEvent;
-using serveEW = WorkspaceCreationHelper::createEventWorkspace;
 
 class EQSANSCorrectFrameTest : public CxxTest::TestSuite {
 public:
@@ -32,52 +31,50 @@ public:
   // constructor
   EQSANSCorrectFrameTest()
       : m_pulseWidth(1.E6 / 60), m_frameWidth(2.E6 / 60), m_minTOF(4.1E6 / 60),
-        m_frameSkipping(true) {
+        m_frameSkipping(true), m_bankSize(2){
 
-    // Create event workspace with four pixels
-    int numPixels = 4;
-    int numBins = 1;
-    int numEvents = 0;
-    double x0 = 0.0;
-    double binDelta = 1.0; // microseconds
-    int eventPattern = 1;
-    m_ews = serveEW(numPixels, numBins, numEvents, x0, binDelta, eventPattern);
+    // bank contains m_bankSize^2 pixels
+    const int numBanks = 1;
+    m_ews = WorkspaceCreationHelper::createEventWorkspaceWithFullInstrument(
+                numBanks, m_bankSize);
     m_ews->getAxis(0)->setUnit("TOF");
 
     // insert one event in each pixel
     std::vector<double> tofs = {0.05, 0.15, 1.05, 1.15};
+    const double pulseWidth(m_pulseWidth);
     std::transform(tofs.begin(), tofs.end(), tofs.begin(),
-                   [](double tof) { return tof * m_pulseWidth });
+                   [&pulseWidth](double tof) {return tof * pulseWidth;});
+    const int numPixels(m_bankSize * m_bankSize);
     for (int i = 0; i < numPixels; i++) {
-      EventList &evlist = m_ews->getSpectrum(ihist);
+      EventList &evlist = m_ews->getSpectrum(i);
       evlist.addEventQuickly(TofEvent(tofs[i]));
     }
   }
 
-  void testConstructor() { TS_ASSERT_THROWS_NOTHING(EQSANSCorrectFrameTest); }
-
   void testInit() {
-    EQSANSCorrectFrame alg;
+    Mantid::Algorithms::EQSANSCorrectFrame alg;
     TS_ASSERT_THROWS_NOTHING(alg.initialize());
     TS_ASSERT(alg.isInitialized());
   }
 
   void testExec() {
-    EQSANSCorrectFrame alg;
+    Mantid::Algorithms::EQSANSCorrectFrame alg;
     alg.initialize();
-    alg.setPropertyValue("InputWorkspace", m_ews->getName());
+    alg.setProperty("InputWorkspace", m_ews);
     alg.setProperty("MinTOF", m_minTOF);
     alg.setProperty("FrameWidth", m_frameWidth);
     alg.setProperty("FrameSkipping", m_frameSkipping);
     alg.execute();
 
-    std::vector<double> tofs = {6.05, 4.15, 5.05, 6.15};
+    std::vector<double> tofs = {7.05, 4.15, 5.05, 6.15};
+    const double pulseWidth(m_pulseWidth);
     std::transform(tofs.begin(), tofs.end(), tofs.begin(),
-                   [](double tof) { return tof * m_pulseWidth });
-    for (int i = 0; i < numPixels; i++) {
-      std::vector<TofEvent> &events = inputWS->getSpectrum(i).getEvents();
+                   [&pulseWidth](double tof) {return tof * pulseWidth;});
+    const int numPixels(m_bankSize * m_bankSize);
+    for (int i=0; i<numPixels; i++) {
+      std::vector<TofEvent> &events = m_ews->getSpectrum(i).getEvents();
       TS_ASSERT_EQUALS(events.size(), 1);
-      TS_ASSERT_DELTA(events[0], tofs[i], 1.0E-03 * m_pulseWidth);
+      TS_ASSERT_DELTA(events[0].tof(), tofs[i], 1.0E-03 * m_pulseWidth);
     }
   }
 
@@ -87,7 +84,8 @@ private:
   double m_frameWidth;
   double m_minTOF;
   bool m_frameSkipping;
+  int m_bankSize;
   void cleanup() { return; }
 };
 
-#endif EQSANSCORRECTFRAMETEST_H_
+#endif  // MANTID_ALGORITHMS_EQSANSCORRECTFRAMETEST_H_
