889e9f9a104d92760ec2072847e372b3894230fb
Verena Reimund <reimund@ill.eu>
2019-03-15 13:21:41 +0100

    Add offsets to detector edges
    when calling setBankNormal
    
    For avoiding to eventually throw an exception when calculating the bank normal.
    Thanks for the suggestion, Roman.
    
    Refs #25104

--- qt/widgets/instrumentview/src/BankRenderingHelpers.cpp
+++ qt/widgets/instrumentview/src/BankRenderingHelpers.cpp
@@ -216,11 +216,7 @@ void render2DTexture(const Corners &corners, size_t nX, size_t nY,
   glBegin(GL_QUADS);
 
   // Set the bank normal to facilitate lighting effects
-  try {
-    setBankNormal(corners.bottomRight(), corners.topLeft(), basePos);
-  } catch (std::runtime_error &) {
-    g_log.warning("Cannot set the bank normal to facilitate lighting effects.");
-  }
+  setBankNormal(corners.bottomRight() + bottomRightOffset, corners.topLeft() + topLeftOffset, basePos);
 
   glTexCoord2f(0.0, 0.0);
   addVertex(corners.bottomLeft() - basePos + bottomLeftOffset);
