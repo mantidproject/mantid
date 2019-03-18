f17f800b7fb39f6e0feaad7ce558d7f4a7af0070
Robert Applin <40830825+robertapplin@users.noreply.github.com>
2019-03-18 15:57:15 +0000

    Refs #25235. Re-enable Run after an error

--- qt/scientific_interfaces/Indirect/ApplyAbsorptionCorrections.cpp
+++ qt/scientific_interfaces/Indirect/ApplyAbsorptionCorrections.cpp
@@ -400,6 +400,7 @@ void ApplyAbsorptionCorrections::addInterpolationStep(
 void ApplyAbsorptionCorrections::absCorComplete(bool error) {
   disconnect(m_batchAlgoRunner, SIGNAL(batchComplete(bool)), this,
              SLOT(absCorComplete(bool)));
+  setRunIsRunning(false);
 
   if (!error) {
     if (m_uiForm.ckUseCan->isChecked()) {
@@ -421,7 +422,6 @@ void ApplyAbsorptionCorrections::absCorComplete(bool error) {
             SLOT(postProcessComplete(bool)));
     m_batchAlgoRunner->executeBatchAsync();
   } else {
-    setRunIsRunning(false);
     setPlotSpectrumEnabled(false);
     setPlotContourEnabled(false);
     setSaveResultEnabled(false);
