0e10b1adc8841950c6b3ea0ffb74928f91a72837
Vickie Lynch <lynchve@ornl.gov>
2019-03-19 09:22:50 -0400

    Refs #22420 new style copyright

--- Framework/Crystal/inc/MantidCrystal/PredictSatellitePeaks.h
+++ Framework/Crystal/inc/MantidCrystal/PredictSatellitePeaks.h
@@ -1,3 +1,10 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2011 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+
 #ifndef MANTID_CRYSTAL_PREDICTSATELLITEPEAKS_H_
 #define MANTID_CRYSTAL_PREDICTSATELLITEPEAKS_H_
 
@@ -14,30 +21,9 @@ namespace Crystal {
     to fractional h,k,and l values.
 
     @author Shiyun Liu/Vickie Lynch
-    @date   2018-07-13
+    @date   2019-03-2019
 
-    Copyright &copy; 2012 ISIS Rutherford Appleton Laboratory &
-                     NScD Oak Ridge National Laboratory
-
-    This file is part of Mantid.
-
-    Mantid is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    Mantid is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    File change history is stored at:
-    <https://github.com/mantidproject/mantid>
-    Code Documentation is available at: <http://doxygen.mantidproject.org>
-  */
+*/
 class DLLExport PredictSatellitePeaks : public API::Algorithm {
 public:
   PredictSatellitePeaks();
