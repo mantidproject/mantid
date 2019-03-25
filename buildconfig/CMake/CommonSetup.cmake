ed36086159da66bbce127eaf1c0c91b31bef8ea1
Tom Titcombe <t.j.titcombe@gmail.com>
2019-03-25 15:15:13 +0000

    Post correct cmake warnings for the correct if statement
    Add warning when clang tidy is not enabled
    
    Move cmake version warning to correct if statement

--- buildconfig/CMake/CommonSetup.cmake
+++ buildconfig/CMake/CommonSetup.cmake
@@ -283,8 +283,9 @@ if ( CMAKE_VERSION VERSION_GREATER "3.5" )
     endif()
   else()
     set(CMAKE_CXX_CLANG_TIDY "" CACHE STRING "" FORCE) # delete it
-    message(AUTHOR_WARNING "Using cmake version 3.5 or below. Clang-tidy is not supported!")
+    message(AUTHOR_WARNING "ENABLE_CLANG_TIDY is off. Clang-tidy will not run!")
   endif()
+  message(AUTHOR_WARNING "Using cmake version 3.5 or below. Clang-tidy is not supported!")
 endif()
 
 ###########################################################################
