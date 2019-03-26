12c4f61a75afe8a4956cf1792df9eb6d5db8ee30
Dimitar Tasev <dimtasev@gmail.com>
2019-03-26 11:46:22 +0000

    Remove old CMake code and generated files, re #25139

--- Framework/PythonInterface/mantid/kernel/mpisetup.py
+++
@@ -1,5 +0,0 @@
-# For MPI builds we need to import boost.mpi to initialize the MPI environment.
-# The environment is also instantiated in C++ but that does seem to lead to
-# issues (probably related to how Ubuntu builds their MPI and Python packages).
-if False:
-    import boost.mpi
