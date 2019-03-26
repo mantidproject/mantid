439d3f2afe8832bc2266d8bdfa4d481b87c4e7d0
Samuel Jones <samjones714@gmail.com>
2019-03-26 14:42:08 +0000

    Re #25383 start prototype of plotting

---
+++ qt/widgets/mplcpp/inc/MantidQtWidgets/MplCpp/Plot.h
@@ -0,0 +1,41 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#ifndef MPLCPP_PLOT_H
+#define MPLCPP_PLOT_H
+
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidQtWidgets/MplCpp/DllConfig.h"
+#include "MantidQtWidgets/MplCpp/Python/Object.h"
+
+#include <QMap>
+#include <QString>
+#include <QVariant>
+#include <boost/none_t.hpp>
+#include <boost/optional.hpp>
+#include <vector>
+
+using namespace Mantid::API;
+using namespace boost;
+
+namespace MantidQt {
+namespace Widgets {
+namespace MplCpp {
+
+MANTID_MPLCPP_DLL Python::Object
+plot(std::vector<MatrixWorkspace_sptr> workspaces,
+     boost::optional<std::vector<std::size_t>> spectrum_nums,
+     boost::optional<std::vector<std::size_t>> wksp_indices,
+     boost::optional<Python::Object>, boost::optional<QMap<QString, QVariant>>,
+     boost::optional<QMap<QString, QVariant>>,
+     boost::optional<std::string> window_title, bool errors = false,
+     bool overplot = false);
+
+} // namespace MplCpp
+} // namespace Widgets
+} // namespace MantidQt
+
+#endif // MPLCPP_PLOT_H
