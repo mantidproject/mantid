de9f8b18217cf7d2abdeeb3c5f19b0b7fb731e5d
Dimitar Tasev <dimtasev@gmail.com>
2019-03-28 10:52:14 +0000

    Manually expand egg-link for development builds, should fix MantidPlot
    Re #25139

--- MantidPlot/mantidplot.py
+++ MantidPlot/mantidplot.py
@@ -14,6 +14,27 @@ from __future__ import (absolute_import, division,
                         print_function)
 
 import os.path as osp
+import sys
+
+try:
+    import mantid
+except ImportError:
+    # Find the egg link (relative to THIS file), and import it's linked path to the PYTHONPATH
+    _mantid_egg_link = 'mantid.egg-link'
+    _mantid_egg_link_path = osp.join(osp.dirname(__file__), _mantid_egg_link) + "PALEPSALPD"
+    try:
+        with open(_mantid_egg_link_path) as f:
+            python_interface_dir = f.readline().strip()
+        # Append the linked path to the PYTHONPATH so that Python can find Mantid
+        sys.path.append(python_interface_dir)
+        import mantid
+    except IOError as exc:
+        # raise an IOError with a custom message
+        raise IOError(
+            "\nCould not find {} at {}.\nThis prevents mantid's PythonInterface from being loaded.\n".format(
+                _mantid_egg_link,
+                _mantid_egg_link_path))
+
 import pymantidplot
 from pymantidplot import *
 
