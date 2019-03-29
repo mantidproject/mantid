7ba80aa2fc40af77953a987b4263bb4c589643c6
Anthony Lim <anthony.lim@stfc.ac.uk>
2019-03-29 15:21:55 +0000

    refs #23335 missed file in pyqt update for muon GUI

--- scripts/Muon/GUI/FrequencyDomainAnalysis/FFT/fft_widget.py
+++ scripts/Muon/GUI/FrequencyDomainAnalysis/FFT/fft_widget.py
@@ -10,10 +10,10 @@ from Muon.GUI.FrequencyDomainAnalysis.FFT.fft_view import FFTView
 from Muon.GUI.FrequencyDomainAnalysis.FFT.fft_presenter import FFTPresenter
 from Muon.GUI.FrequencyDomainAnalysis.FFT.fft_model import FFTModel, FFTWrapper
 
-from PyQt4 import QtGui
+from qtpy import QtWidgets
 
 
-class FFTWidget(QtGui.QWidget):
+class FFTWidget(QtWidgets.QWidget):
 
     def __init__(self, load, parent=None):
         super(FFTWidget, self).__init__(parent)
