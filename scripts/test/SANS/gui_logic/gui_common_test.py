ca507f16bf90b0549095b331d83728bc6a69fb02
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-03 13:49:30 +0100

    Add unit test for SANSGuiPropertiesHandler

--- scripts/test/SANS/gui_logic/gui_common_test.py
+++ scripts/test/SANS/gui_logic/gui_common_test.py
@@ -8,16 +8,14 @@ from __future__ import (absolute_import, division, print_function)
 
 import unittest
 
-import mantid
-from mantid.kernel import config
-
+from mantid.py3compat import mock
 from sans.gui_logic.gui_common import (get_reduction_mode_strings_for_gui, get_reduction_selection,
                                        get_string_for_gui_from_reduction_mode,
                                        get_batch_file_dir_from_path,
                                        add_dir_to_datasearch,
-                                       remove_dir_from_datasearch)
+                                       remove_dir_from_datasearch,
+                                       SANSGuiPropertiesHandler)
 from sans.common.enums import (SANSInstrument, ISISReductionMode)
-import os
 
 
 class GuiCommonTest(unittest.TestCase):
@@ -123,6 +121,21 @@ class GuiCommonTest(unittest.TestCase):
 
         expected_result = "A/Path/;A/Final/Path/"
         self.assertEqual(expected_result, result)
+
+
+class SANSGuiPropertiesHandlerTest(unittest.TestCase):
+    def test_that_default_functions_are_called_on_initialisation(self):
+        with mock.patch.object(SANSGuiPropertiesHandler, "_load_property", lambda x, y, z: "default_value"):
+            default_property_setup_mock = mock.Mock()
+            default_values_input = {"a_default_property": (default_property_setup_mock, str)}
+            SANSGuiPropertiesHandler(default_values_input)
+
+            calls = default_property_setup_mock.call_args_list
+            self.assertEqual(len(calls), 1)
+
+            args = calls[0][0]
+            self.assertEqual(len(args), 1)
+            self.assertEqual(args[0], "default_value")
 
 
 if __name__ == '__main__':
