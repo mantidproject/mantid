6b00959b51197c6576cc3d57c6aa42ba5337d6b9
Sam Jenkins <s.jenkins@stfc.ac.uk>
2019-04-11 15:22:33 +0100

    Re #25502 updated based on review

--- scripts/Engineering/EnginX.py
+++ scripts/Engineering/EnginX.py
@@ -509,7 +509,7 @@ def focus_whole(run_number, van_curves, van_int, focus_directory, focus_general,
         simple.EnggFocus(InputWorkspace=ws_to_focus, OutputWorkspace=output_ws,
                          VanIntegrationWorkspace=van_integrated_ws, VanCurvesWorkspace=van_curves_ws,
                          Bank=str(i))
-        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_{}{}", str(i))
+        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_{}{{}}", str(i))
 
 
 def focus_cropped(run_number, van_curves, van_int, focus_directory, focus_general, do_pre_process, params, time_period,
@@ -542,14 +542,14 @@ def focus_cropped(run_number, van_curves, van_int, focus_directory, focus_genera
         simple.EnggFocus(InputWorkspace=ws_to_focus, OutputWorkspace=output_ws,
                          VanIntegrationWorkspace=van_integrated_ws, VanCurvesWorkspace=van_curves_ws,
                          Bank=bank.get(crop_on))
-        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_{}{}", crop_on)
+        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_{}{{}}", crop_on)
     else:
         # crop on the spectra passed in, focus and save it out
         output_ws = output_ws.format("", "")
         simple.EnggFocus(InputWorkspace=ws_to_focus, OutputWorkspace=output_ws,
                          VanIntegrationWorkspace=van_integrated_ws,
                          VanCurvesWorkspace=van_curves_ws, SpectrumNumbers=crop_on)
-        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_bank_{}{}", "cropped")
+        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_bank_{}{{}}", "cropped")
 
 
 def focus_texture_mode(run_number, van_curves, van_int, focus_directory, focus_general, do_pre_process, params,
@@ -586,7 +586,7 @@ def focus_texture_mode(run_number, van_curves, van_int, focus_directory, focus_g
         simple.EnggFocus(InputWorkspace=ws_to_focus, OutputWorkspace=output_ws,
                          VanIntegrationWorkspace=van_integrated_ws, VanCurvesWorkspace=van_curves_ws,
                          SpectrumNumbers=banks.get(bank))
-        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_texture_{}{}", bank)
+        _save_out(run_number, focus_directory, focus_general, output_ws, "ENGINX_{}_texture_{}{{}}", bank)
 
 
 def _prepare_focus(run_number, van_curves, van_int, do_pre_process, params, time_period):
@@ -647,7 +647,7 @@ def _save_out(run_number, focus_directory, focus_general, output, enginx_file_na
 
 def _find_focus_file_location(bank_id, focus_directory, enginx_file_name_format, run_number):
     # Leave final {} in string so that file extension can be set.
-    run_and_bank = enginx_file_name_format.format(run_number, bank_id, "{}")
+    run_and_bank = enginx_file_name_format.format(run_number, bank_id)
     filename = os.path.join(focus_directory, run_and_bank)
     genie_filename = os.path.join(focus_directory, run_and_bank.replace("_", "", 1).format(".his"))
 
