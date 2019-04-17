007e933a9a97e1c2547b520387c70b041b365a5a
Andrei Savici <saviciat@ornl.gov>
2019-04-17 15:27:01 -0400

    Finish writing unit test

--- Framework/MDAlgorithms/test/FlippingRatioCorrectionMDTest.h
+++ Framework/MDAlgorithms/test/FlippingRatioCorrectionMDTest.h
@@ -10,9 +10,11 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidMDAlgorithms/FlippingRatioCorrectionMD.h"
+#include "MantidMDAlgorithms/BinMD.h"
 #include "MantidMDAlgorithms/CreateMDWorkspace.h"
-#include "MantidMDAlgorithms/MergeMD.h"
 #include "MantidMDAlgorithms/FakeMDEventData.h"
+#include "MantidMDAlgorithms/MergeMD.h"
+#include "MantidDataObjects/MDHistoWorkspace.h"
 #include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/Run.h"
 
@@ -33,32 +35,73 @@ public:
     TS_ASSERT( alg.isInitialized() )
   }
 
-  void test_exec()
+  void test_exec_constFR()
   {
     createMergedMDWorkspace();
 
-/*
     FlippingRatioCorrectionMD alg;
-    // Don't put output in ADS by default
-    alg.setChild(true);
+    std::string outWS1Name("Output1"), outWS2Name("Output2");
     TS_ASSERT_THROWS_NOTHING( alg.initialize() )
     TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", inputWS) );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", "_unused_for_child") );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", "MergedWSForFR") );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("FlippingRatio", "10.") );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace1", outWS1Name) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace2", outWS2Name) );
     TS_ASSERT_THROWS_NOTHING( alg.execute(); );
     TS_ASSERT( alg.isExecuted() );
 
-    // Retrieve the workspace from the algorithm. The type here will probably need to change. It should
-    // be the type using in declareProperty for the "OutputWorkspace" type.
-    // We can't use auto as it's an implicit conversion.
-    Workspace_sptr outputWS = alg.getProperty("OutputWorkspace");
-    TS_ASSERT(outputWS);
-    TS_FAIL("TODO: Check the results and remove this line");
-    */
+    // first correction is F/(F-1)
+    checkFRCorrection(outWS1Name,10./9.,10./9.);
+    //second correction is 1/(F-1)
+    checkFRCorrection(outWS2Name,1./9.,1./9.);
   }
   
+  void test_exec_variableFR()
+  {
+    createMergedMDWorkspace();
 
+    FlippingRatioCorrectionMD alg;
+    std::string outWS1Name("Output1"), outWS2Name("Output2");
+    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
+    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", "MergedWSForFR") );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("FlippingRatio", "prop*pi") );
+    TS_ASSERT_THROWS_NOTHING( alg.setProperty("SampleLogs", "prop") );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace1", outWS1Name) );
+    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace2", outWS2Name) );
+    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
+    TS_ASSERT( alg.isExecuted() );
+
+    // first correction is F/(F-1)
+    double F1=10.*M_PI, F2=20.*M_PI;
+    checkFRCorrection(outWS1Name,F1/(F1-1.),F2/(F2-1.));
+    //second correction is 1/(F-1)
+    checkFRCorrection(outWS2Name,1./(F1-1.),1./(F2-1.));
+  }
 private:
+  void checkFRCorrection(std::string wsName,  double expectedValuePeak1, double expectedValuePeak2) {
+    Mantid::MDAlgorithms::BinMD algBin;
+    TS_ASSERT_THROWS_NOTHING(algBin.initialize())
+    TS_ASSERT(algBin.isInitialized())
+    TS_ASSERT_THROWS_NOTHING(algBin.setPropertyValue("InputWorkspace", wsName));
+    TS_ASSERT_THROWS_NOTHING(algBin.setProperty("AxisAligned", true));
+    TS_ASSERT_THROWS_NOTHING(algBin.setProperty("AlignedDim0", "A,-5,5,2"));
+    TS_ASSERT_THROWS_NOTHING(algBin.setProperty("AlignedDim1", "B,-5,5,2"));
+    TS_ASSERT_THROWS_NOTHING(algBin.setPropertyValue("OutputWorkspace", wsName+"bin"));
+    algBin.execute();
+    TS_ASSERT(algBin.isExecuted());
+    Mantid::DataObjects::MDHistoWorkspace_sptr out;
+    TS_ASSERT_THROWS_NOTHING(
+        out =boost::dynamic_pointer_cast<Mantid::DataObjects::MDHistoWorkspace>(
+               Mantid::API::AnalysisDataService::Instance().retrieve(wsName+"bin"));)
+    TS_ASSERT(out);
+    auto sig=out->getSignalDataVector();
+    TS_ASSERT_DELTA(out->getSignalAt(0,0), 100.*expectedValuePeak1, 1e-5);
+    TS_ASSERT_DELTA(out->getSignalAt(0,1), 0., 1e-5);
+    TS_ASSERT_DELTA(out->getSignalAt(1,0), 0., 1e-5);
+    TS_ASSERT_DELTA(out->getSignalAt(1,1), 100.*expectedValuePeak2, 1e-5);
+  }
+
   void createMergedMDWorkspace() {
     std::string md1Name = "__temp_InputMDWorkspaceName1";
     std::string md2Name = "__temp_InputMDWorkspaceName2";
@@ -99,8 +142,8 @@ private:
     TS_ASSERT_THROWS_NOTHING(alg.setPropertyValue("OutputWorkspace", wsName));
     alg.execute();
     // Add log
-    Mantid::API::IMDEventWorkspace_sptr out =
-        Mantid::API::AnalysisDataService::Instance().retrieveWS<Mantid::API::IMDEventWorkspace>(wsName);
+    Mantid::API::IMDEventWorkspace_sptr out = boost::dynamic_pointer_cast<Mantid::API::IMDEventWorkspace>(
+                Mantid::API::AnalysisDataService::Instance().retrieve(wsName));
     Mantid::API::ExperimentInfo_sptr ei(new Mantid::API::ExperimentInfo);
     out->addExperimentInfo(ei);
     out->getExperimentInfo(0)->mutableRun().addProperty("prop", sampleLog);
