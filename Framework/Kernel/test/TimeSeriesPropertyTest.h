1f7b3de67b738d4f3cfc70b1d941300101371fa0
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-18 15:31:03 +0100

    Add unit tests for cloneWithTimeShift
    Add note in cloneWithTimeShift and unit test explaining bug

--- Framework/Kernel/test/TimeSeriesPropertyTest.h
+++ Framework/Kernel/test/TimeSeriesPropertyTest.h
@@ -1345,6 +1345,38 @@ public:
     return;
   }
 
+    /*
+     * Test cloneWithTimeShift
+     */
+  void test_cloneWithTimeShift() {
+    TimeSeriesProperty<int> *time_series = new TimeSeriesProperty<int>("IntUnixTest");
+    time_series->addValue("2019-02-10T16:17:00", 1);
+
+    TimeSeriesProperty<int> *time_series_small_shift =
+            dynamic_cast<TimeSeriesProperty<int> *>(time_series->cloneWithTimeShift(100.));
+    std::vector<DateAndTime> small_shift_times = time_series_small_shift->timesAsVector();
+    TS_ASSERT_EQUALS(small_shift_times[0], DateAndTime("2019-02-10T16:18:40"));
+
+    TimeSeriesProperty<int> *time_series_large_shift =
+            dynamic_cast<TimeSeriesProperty<int> *>(time_series->cloneWithTimeShift(1234.));
+    std::vector<DateAndTime> large_shift_times = time_series_large_shift->timesAsVector();
+    TS_ASSERT_EQUALS(large_shift_times[0], DateAndTime("2019-02-10T16:37:34"));
+
+    TimeSeriesProperty<int> *time_series_negative_shift =
+            dynamic_cast<TimeSeriesProperty<int> *>(time_series->cloneWithTimeShift(-1234.));
+    std::vector<DateAndTime> negative_shift_times = time_series_negative_shift->timesAsVector();
+    TS_ASSERT_EQUALS(negative_shift_times[0], DateAndTime("2019-02-10T15:56:26"));
+
+    // There is a known issue which can cause cloneWithTimeShift to be called with a large (~9e+9 s) shift.
+    // Actual shifting is capped to be ~4.6e+19 seconds in DateAndTime::operator+=
+    // Typical usage of this method is with shifts in the range tested here.
+
+    delete time_series;
+    delete time_series_small_shift;
+    delete time_series_large_shift;
+    delete time_series_negative_shift;
+  }
+
   /*
    * Test countSize()
    */
