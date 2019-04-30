6756045cb53290bed37cbe71c818708c6dcea0d5
Dimitar Tasev <dimtasev@gmail.com>
2019-04-30 14:16:26 +0100

    Remove unused import, re #25547

--- qt/applications/workbench/workbench/plotting/test/test_figureerrorsmanager.py
+++ qt/applications/workbench/workbench/plotting/test/test_figureerrorsmanager.py
@@ -9,7 +9,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import matplotlib.figure
 import matplotlib.pyplot as plt
 from qtpy.QtWidgets import QMenu
 
