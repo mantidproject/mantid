5aa777e8239846c95887638997f14b137a4788e4
Martyn Gigg <martyn.gigg@gmail.com>
2019-05-08 20:38:18 +0100

    Supress unused argument warnings.

--- qt/widgets/plotting/src/Mpl/RangeSelector.cpp
+++ qt/widgets/plotting/src/Mpl/RangeSelector.cpp
@@ -12,21 +12,29 @@ namespace MantidWidgets {
 
 RangeSelector::RangeSelector(PreviewPlot *plot, SelectType, bool visible,
                              bool infoOnly)
-    : QObject() {}
+    : QObject() {
+  Q_UNUSED(plot);
+  Q_UNUSED(visible);
+  Q_UNUSED(infoOnly);
+}
 
 void RangeSelector::setRange(const std::pair<double, double> &range) {
   setRange(range.first, range.second);
 }
 
 void RangeSelector::setRange(const double min, const double max) {
+  Q_UNUSED(min);
+  Q_UNUSED(max);
   throw std::runtime_error("RangeSelector::setRange not implemented");
 }
 
 void RangeSelector::setMinimum(double value) {
+  Q_UNUSED(value);
   throw std::runtime_error("RangeSelector::setMinimum not implemented");
 }
 
 void RangeSelector::setMaximum(double value) {
+  Q_UNUSED(value);
   throw std::runtime_error("RangeSelector::setMaximum not implemented");
 }
 
