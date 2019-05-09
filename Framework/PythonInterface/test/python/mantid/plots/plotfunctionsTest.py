3591dc535db9662fe8ce1ba1a22dfa00552a6bdc
Steven Hahn <hahnse@ornl.gov>
2019-05-09 17:22:28 -0400

    imshow isn't affected.

--- Framework/PythonInterface/test/python/mantid/plots/plotfunctionsTest.py
+++ Framework/PythonInterface/test/python/mantid/plots/plotfunctionsTest.py
@@ -130,7 +130,7 @@ class PlotFunctionsTest(unittest.TestCase):
         self.assertAlmostEqual(10.0, ax.get_xlim()[0])
         from distutils.version import LooseVersion
         #different results with 1.5.3 and 2.1.1
-        if LooseVersion(matplotlib.__version__) < LooseVersion("2"):
+        if color_func.__name__ != 'imshow' and LooseVersion(matplotlib.__version__) < LooseVersion("2"):
             self.assertAlmostEqual(100.0, ax.get_xlim()[1])
         else:
             self.assertAlmostEqual(30.0, ax.get_xlim()[1])
