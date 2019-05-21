113c41b93abcf2e01ae61cda381ed0a11b0fe524
Samuel Jones <samjones714@gmail.com>
2019-05-21 10:28:30 +0100

    Re #25383 Clang-format

--- qt/scientific_interfaces/test/ISISReflectometry/RunsTable/RunsTablePresenterTest.h
+++ qt/scientific_interfaces/test/ISISReflectometry/RunsTable/RunsTablePresenterTest.h
@@ -18,10 +18,10 @@
 #include <gtest/gtest.h>
 
 using namespace MantidQt::CustomInterfaces;
-using testing::_;
 using testing::Mock;
 using testing::NiceMock;
 using testing::Return;
+using testing::_;
 
 class RunsTablePresenterTest {
 public:
