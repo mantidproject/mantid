b75d6c55c45f8a173b453dfdf22e54d9f1cb736c
Tom Titcombe <t.j.titcombe@gmail.com>
2019-05-24 16:41:10 +0100

    Create MaskBinsFromWorkspace algorithm

---
+++ Framework/Algorithms/inc/MantidAlgorithms/MaskBinsFromWorkspace.h
@@ -0,0 +1,48 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#ifndef MANTID_ALGORITHMS_MASKBINSFROMWORKSPACE_H_
+#define MANTID_ALGORITHMS_MASKBINSFROMWORKSPACE_H_
+
+#include "MantidAPI/DistributedAlgorithm.h"
+#include "MantidIndexing/SpectrumIndexSet.h"
+
+namespace Mantid {
+
+namespace Histogram {
+class BinEdges;
+}
+namespace Algorithms {
+class DLLExport MaskBinsFromWorkspace : public API::DistributedAlgorithm {
+public:
+  /// Algorithm's name
+  const std::string name() const override { return "MaskBinsFromWorkspace"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Copies over masked bins from a workspace to another workspace.";
+  }
+
+  /// Algorithm's version
+  int version() const override { return (1); }
+  const std::vector<std::string> seeAlso() const override {
+    return {"MaskBinsFromTable"};
+  }
+  /// Algorithm's category for identification
+  const std::string category() const override { return "Transforms\\Masking"; }
+
+private:
+  /// Initialisation code
+  void init() override;
+  /// Execution code
+  void exec() override;
+  Indexing::SpectrumIndexSet
+      indexSet; ///< the list of Spectra (workspace index) to load
+};
+
+} // namespace Algorithms
+} // namespace Mantid
+
+#endif /*MANTID_ALGORITHMS_MASKBINSFROMWORKSPACE_H_*/
