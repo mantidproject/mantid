6c0a532bbe31f684ff3d20c522ae7f0ef40503bb
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2019-05-30 17:03:52 +0100

    Results tab view updates now happen on the main thread

--- scripts/Muon/GUI/Common/results_tab_widget/results_tab_presenter.py
+++ scripts/Muon/GUI/Common/results_tab_widget/results_tab_presenter.py
@@ -6,13 +6,18 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from __future__ import (absolute_import, division)
 
+from qtpy.QtCore import QMetaObject, QObject, Slot
+
 from Muon.GUI.Common.observer_pattern import GenericObserver
 
 
-class ResultsTabPresenter(object):
+# Ordinarily this does not need to be a QObject but it is required to use some QMetaObject.invokeMethod
+# magic. See on_new_fit_performed() for more details
+class ResultsTabPresenter(QObject):
     """Controller for the results tab"""
 
     def __init__(self, view, model):
+        super(ResultsTabPresenter, self).__init__()
         self.view = view
         self.model = model
         self.new_fit_performed_observer = GenericObserver(
@@ -27,11 +32,13 @@ class ResultsTabPresenter(object):
 
     def on_new_fit_performed(self):
         """React to a new fit created in the fitting tab"""
-        self.view.set_fit_function_names(self.model.fit_functions())
-
-        self._update_fit_results_view()
-        self._update_logs_view()
-        self.view.set_output_results_button_enabled(True)
+        # It's possible that this call can come in on a thread that
+        # is different to the one that the view lives on.
+        # In order to update the GUI we use invokeMethod with the assumption
+        # that 'self' lives on the same thread as the view and Qt forces
+        # the call to the chose method to be done on the thread the
+        # view lives on. This avoids errors from painting on non-gui threads.
+        QMetaObject.invokeMethod(self, "_on_new_fit_performed_impl")
 
     def on_output_results_request(self):
         """React to the output results table request"""
@@ -57,6 +64,14 @@ class ResultsTabPresenter(object):
         self.view.function_selection_changed.connect(
             self.on_function_selection_changed)
         self.view.set_output_results_button_enabled(False)
+
+    @Slot()
+    def _on_new_fit_performed_impl(self):
+        """Use as part of an invokeMethod call to call across threads"""
+        self.view.set_fit_function_names(self.model.fit_functions())
+        self._update_fit_results_view()
+        self._update_logs_view()
+        self.view.set_output_results_button_enabled(True)
 
     def _update_fit_results_view(self):
         """Update the view of results workspaces based on the current model"""
