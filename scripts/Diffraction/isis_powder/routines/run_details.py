af9915c31d67e01704d6e7e52ccff7d6999239ae
Sam Jenkins <s.jenkins@stfc.ac.uk>
2019-05-31 11:41:46 +0100

    Re #25810 added update spline function to alter spline on cache

--- scripts/Diffraction/isis_powder/routines/run_details.py
+++ scripts/Diffraction/isis_powder/routines/run_details.py
@@ -118,3 +118,22 @@ class _RunDetails(object):
         self.vanadium_absorption_path = vanadium_abs_path
         self.output_suffix = output_suffix
         self.van_paths = van_paths
+
+    def update_spline(self, inst_settings, new_splined_name_list):
+        """Updates the spline path using a new splined name list, this is necessary on instruments where the spline path
+        may change e.g. Pearl due to long-mode
+        :param inst_settings The current Instrument settings
+        :param new_splined_name_list  List of unique properties to generate a splined vanadium name from
+        """
+
+        cal_map_dict = get_cal_mapping_dict(run_number_string=self.output_run_string,
+                                            cal_mapping_path=inst_settings.cal_mapping_path)
+        offset_file_name = common.cal_map_dictionary_key_helper(dictionary=cal_map_dict, key="offset_file_name")
+
+        # Prepend the properties used for creating a van spline so we can fingerprint the file
+        splined_list = new_splined_name_list if new_splined_name_list else []
+        splined_list.append(os.path.basename(offset_file_name))
+
+        splined_van_name = common.generate_splined_name(self.vanadium_run_numbers, splined_list)
+        self.splined_vanadium_file_path = os.path.join(self.van_paths, splined_van_name)
+
