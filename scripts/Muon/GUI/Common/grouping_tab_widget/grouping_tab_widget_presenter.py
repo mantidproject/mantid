e0772f0ca6e70e2ba6db5313f7dda8cdee5289d4
Matthew Andrew <matthew.andrew@tessella.com>
2019-05-31 15:47:10 +0100

    Fix raw data not being shown

--- scripts/Muon/GUI/Common/grouping_tab_widget/grouping_tab_widget_presenter.py
+++ scripts/Muon/GUI/Common/grouping_tab_widget/grouping_tab_widget_presenter.py
@@ -179,6 +179,7 @@ class GroupingTabPresenter(object):
 
     def handle_new_data_loaded(self):
         if self._model.is_data_loaded():
+            self._model._context.show_raw_data()
             self.grouping_table_widget.update_view_from_model()
             self.pairing_table_widget.update_view_from_model()
             self.update_description_text()
