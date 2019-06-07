261f1ab13574c32a575bf92dbac0f99a8c60301b
Joachim Coenen <29330338+JoachimCoenen@users.noreply.github.com>
2019-06-07 13:53:15 +0200

    corrected wrong indentation Re: #22871

--- Framework/DataHandling/inc/MantidDataHandling/BitStream.h
+++ Framework/DataHandling/inc/MantidDataHandling/BitStream.h
@@ -49,13 +49,13 @@ template <typename T> inline T convert_endianness(T value) {
 
 enum class endian {
 #ifdef _WIN32
-   little = 0,
-   big    = 1,
-   native = little
+  little = 0,
+  big    = 1,
+  native = little
 #else
-   little = __ORDER_LITTLE_ENDIAN__,
-   big    = __ORDER_BIG_ENDIAN__,
-   native = __BYTE_ORDER__
+  little = __ORDER_LITTLE_ENDIAN__,
+  big    = __ORDER_BIG_ENDIAN__,
+  native = __BYTE_ORDER__
 #endif
 };
 
