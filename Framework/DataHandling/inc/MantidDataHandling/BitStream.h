eb9c92e0bc5d8d7e608718d6bfb61663ac68eb56
Joachim Coenen <29330338+JoachimCoenen@users.noreply.github.com>
2019-06-07 14:01:46 +0200

    realigned assignment operators Re: #22871

--- Framework/DataHandling/inc/MantidDataHandling/BitStream.h
+++ Framework/DataHandling/inc/MantidDataHandling/BitStream.h
@@ -50,11 +50,11 @@ template <typename T> inline T convert_endianness(T value) {
 enum class endian {
 #ifdef _WIN32
   little = 0,
-  big    = 1,
+  big = 1,
   native = little
 #else
   little = __ORDER_LITTLE_ENDIAN__,
-  big    = __ORDER_BIG_ENDIAN__,
+  big = __ORDER_BIG_ENDIAN__,
   native = __BYTE_ORDER__
 #endif
 };
