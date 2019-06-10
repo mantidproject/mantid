16d606b935392bfcd852d864197363c075339dbd
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2019-06-10 11:30:28 +0100

    Register clean up function for tests using QApplication
    Tests were seeing random segfaults when Python cleans
    up its imported modules. This ensures the Qapplication
    reference is removed before module destruction occurs.

--- qt/python/mantidqt/utils/qt/testing/application.py
+++ qt/python/mantidqt/utils/qt/testing/application.py
@@ -5,16 +5,25 @@
 #     & Institut Laue - Langevin
 # SPDX - License - Identifier: GPL - 3.0 +
 #  This file is part of the mantid workbench.
-#
-#
-# flake8: noqa
 from __future__ import absolute_import
+
+import atexit
+
 from qtpy.QtWidgets import QApplication
 
 from mantidqt.utils.qt.plugins import setup_library_paths
 
 # Hold on to QAPP reference to avoid garbage collection
 _QAPP = QApplication.instance()
+
+
+@atexit.register
+def cleanup_qapp_ref():
+    """
+    Remove the global reference to the QApplication object here
+    """
+    global _QAPP
+    del _QAPP
 
 
 def get_application(name=''):
