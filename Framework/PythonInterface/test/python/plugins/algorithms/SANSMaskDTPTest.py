69b0e0e10a16ca29f5a1f6162fe24aad214320fc
Pete Peterson <petersonpf@ornl.gov>
2019-06-11 14:39:16 -0400

    Add EQSANS and BIOSANS tests

--- Framework/PythonInterface/test/python/plugins/algorithms/SANSMaskDTPTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/SANSMaskDTPTest.py
@@ -6,59 +6,112 @@ from mantid.simpleapi import LoadEmptyInstrument, SANSMaskDTP, mtd
 
 
 class SANSMaskDTPTest(unittest.TestCase):
+    def checkConsistentMask(self, wksp, masked):
+        compInfo = wksp.componentInfo()
+        detInfo = wksp.detectorInfo()
+        # detector ids are any number, detector index are 0->number of detectors
+        detIds = detInfo.detectorIDs()
+        for detIndex, detId in enumerate(detIds):
+            if not compInfo.isDetector(detIndex):
+                continue
+
+            if detInfo.isMonitor(detIndex):
+                self.assertFalse(detInfo.isMasked(detIndex),
+                                 'DetID={} is a monitor and shouldn\'t be masked'.format(detId))
+            else:
+                self.assertEqual(detInfo.isMasked(detIndex), detId in masked,
+                                 'DetID={} is has incorrect mask bit. "{}" should be "{}"'.format(detId,
+                                                                                                  detInfo.isMasked(int(detId)),
+                                                                                                  detId in masked))
+    def checkDetectorIndexes(self, wksp, detIndices):
+        '''This is use to spot check specific detector indices (not identifiers) as masked'''
+        detInfo = wksp.detectorInfo()
+        for detIndex in detIndices:
+            self.assertTrue(detInfo.isMasked(detIndex),
+                             'Detector index={} should be masked'.format(detIndex))
+
 
     def test_cg2(self):
         ws_name = 'cg2'
         LoadEmptyInstrument(InstrumentName='CG2', OutputWorkspace=ws_name)
 
         # Let's mask just the first tube
-        SANSMaskDTP(InputWorkspace=ws_name, tube="1")
-        w = mtd[ws_name]
-        detInfo = w.detectorInfo()
-        start_id = 2  # First 2 are monitors
-        for i in range(start_id, start_id+256):
-            self.assertTrue(detInfo.isMasked(i),
-                            "DetID={} is not masked".format(i))
+        masked = SANSMaskDTP(InputWorkspace=ws_name, tube="1")
+        wksp = mtd[ws_name]
+        self.assertEqual(int(256), len(masked))
+        self.checkConsistentMask(wksp, masked)
+
+        # check for specific mask values
+        start_index = 2  # First 2 are monitors
+        self.checkDetectorIndexes(wksp, list(range(start_index, start_index+256)))
 
     def test_cg2_top_bottom(self):
         ws_name = 'cg2_top_bottom'
         LoadEmptyInstrument(InstrumentName='CG2', OutputWorkspace=ws_name)
 
         # Let's mask the bottom and top of the detector
-        SANSMaskDTP(InputWorkspace=ws_name, Pixel="1-10,247-256")
-        w = mtd[ws_name]
-        detInfo = w.detectorInfo()
-        start_id = 2
+        masked = SANSMaskDTP(InputWorkspace=ws_name, Pixel="1-10,247-256")
+        wksp = mtd[ws_name]
+        self.assertEqual(int(192*20), len(masked))
+        self.checkConsistentMask(wksp, masked)
 
+        # check for specific mask values
+        start_id = 2
         for tube in range(192):
             # top
-            this_tube_first_id = start_id + 256*tube
-            for i in range(this_tube_first_id, this_tube_first_id+10):
-                self.assertTrue(detInfo.isMasked(i),
-                                "DetID={} is not masked".format(i))
+            this_tube_first_id = start_id + 256 * tube
+            self.checkDetectorIndexes(wksp, list(range(this_tube_first_id, this_tube_first_id + 10)))
 
             # bottom
-            this_tube_almost_last_id = start_id + 246 + 256*tube
-            for i in range(this_tube_almost_last_id, this_tube_almost_last_id+10):
-                self.assertTrue(detInfo.isMasked(i),
-                                "DetID={} is not masked".format(i))
+            this_tube_almost_last_id = start_id + 246 + 256 * tube
+            self.checkDetectorIndexes(wksp, list(range(this_tube_almost_last_id, this_tube_almost_last_id + 10)))
 
     def test_cg2_interleaved(self):
         ws_name = 'cg2_interleaved'
         LoadEmptyInstrument(InstrumentName='CG2', OutputWorkspace=ws_name)
 
         # Let's mask the bottom and top of the detector
-        SANSMaskDTP(InputWorkspace=ws_name, Tube="1:300:2")
-        w = mtd[ws_name]
-        detInfo = w.detectorInfo()
-        start_id = 2
+        masked = SANSMaskDTP(InputWorkspace=ws_name, Tube="1:300:2")
+        wksp = mtd[ws_name]
+        self.assertEqual(int(192*256/2), len(masked))
+        self.checkConsistentMask(wksp, masked)
 
+        # check for specific mask values
+        start_id = 2
         for tube in range(0, 192, 2):
             this_tube_first_id = start_id + 256*tube
-            for i in range(this_tube_first_id, this_tube_first_id+256):
-                self.assertTrue(detInfo.isMasked(i),
-                                "DetID={} is not masked".format(i))
+            self.checkDetectorIndexes(wksp, list(range(this_tube_first_id, this_tube_first_id+256)))
 
+    def test_eqsans_interleaved(self):
+        ws_name = 'eqsans'
+        LoadEmptyInstrument(InstrumentName='EQSANS', OutputWorkspace=ws_name)
+
+        masked = SANSMaskDTP(InputWorkspace=ws_name, Tube="5:200:8,6:200:8,7:200:8,8:200:8")
+        wksp = mtd[ws_name]
+        self.assertEqual(int(192*256/2), len(masked))
+        self.checkConsistentMask(wksp, masked)
+
+        # check for specific mask values
+        masked = [i + 1 for i in range(256*4, 256 * 8 * 24, 2048)]  # overwrite previous version
+        self.checkDetectorIndexes(wksp, masked)
+
+    def test_biosans_wing_plane(self):
+        ws_name = 'biosans_wing'
+        LoadEmptyInstrument(InstrumentName='BIOSANS', OutputWorkspace=ws_name)
+
+        masked = SANSMaskDTP(InputWorkspace=ws_name, Detector=2, Tube='1:300:2')
+        wksp = mtd[ws_name]
+        self.assertEqual(int(160 * 256 / 2), len(masked))
+        self.checkConsistentMask(wksp, masked)
+
+    def test_biosans_wing_ends(self):
+        ws_name = 'biosans_wing'
+        LoadEmptyInstrument(InstrumentName='BIOSANS', OutputWorkspace=ws_name)
+
+        masked = SANSMaskDTP(InputWorkspace=ws_name, Detector=2, Pixel='1-20,245-256')
+        wksp = mtd[ws_name]
+        self.assertEqual(int(32 * 160), len(masked))
+        self.checkConsistentMask(wksp, masked)
 
 if __name__ == '__main__':
     unittest.main()
