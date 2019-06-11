40566f4d2910962a887ea00ff5a6a3dde167eeae
Gagik Vardanyan <vardanyan@ill.fr>
2019-06-11 16:09:32 +0200

    Re #0 renamed to Drill

--- scripts/DRILL.py
+++ scripts/DRILL.py
@@ -6,7 +6,7 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from __future__ import (absolute_import, division, print_function)
 from mantidqt.gui_helper import get_qapplication
-from mantidqt.widgets import jobtreeview
+from mantidqt.widgets import (jobtreeview, manageuserdirectories)
 from qtpy.QtWidgets import QMainWindow, QWidget
 
 from qtpy import PYQT5
@@ -15,15 +15,33 @@ if not PYQT5:
 
 from Interface.ui.ill.main.ui_main import Ui_MainWindow
 
-class MyApp(QMainWindow, Ui_MainWindow):
+class Drill(QMainWindow, Ui_MainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-        table = jobtreeview.JobTreeView(
-            ["Sample", "Transmissioni", "Absorber", "Beam", "Container", "Reference"],
+        self.table = jobtreeview.JobTreeView(
+            ["Sample", "Transmission", "Absorber", "Beam", "Container", "Reference"],
              self.cell(""), self.centralwidget)
+
+        self.table.setRootIsDecorated(False)
+
+        self.table.setStyleSheet('background: None')
+
+        row_entry = [''] * 6
+        self.add_row(row_entry)
+
+        table_signals = jobtreeview.JobTreeViewSignalAdapter(self.table, self)
+
+        self.mainLayout.addWidget(self.table)
+
+    def add_row(self, value):
+        value = [self.cell(x) for x in value]
+        self.table.appendChildRowOf(self.row([]), value)
+
+    def row(self, path):
+        return jobtreeview.RowLocation(path)
 
     def cell(self, text):
         background_color = 'white'
@@ -35,8 +53,7 @@ class MyApp(QMainWindow, Ui_MainWindow):
                                 border_color, border_opacity, is_editable)
 
 app, within_mantid = get_qapplication()
-main_window = Ui_MainWindow()
-window = MyApp()
+window = Drill()
 window.show()
 if not within_mantid:
     sys.exit(app.exec_())
