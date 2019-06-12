20cbc40c7c40406795185da4ba098633caad5b0a
Tom Titcombe <t.j.titcombe@gmail.com>
2019-06-12 09:27:00 +0100

    Fix merge conflict in plotfunctionsTest

--- Framework/PythonInterface/test/python/mantid/plots/plotfunctionsTest.py
+++ Framework/PythonInterface/test/python/mantid/plots/plotfunctionsTest.py
@@ -146,22 +146,18 @@ class PlotFunctionsTest(unittest.TestCase):
         ax.set_ylim(-0.05, 0.05)
         funcs.update_colorplot_datalimits(ax, mesh)
         self.assertAlmostEqual(10.0, ax.get_xlim()[0])
+        self.assertAlmostEqual(30.0, ax.get_xlim()[1])
+        # self.assertAlmostEqual(4.0, ax.get_ylim()[0])
+        # self.assertAlmostEqual(8.0, ax.get_ylim()[1])
+        from distutils.version import LooseVersion
 
-<< << << < HEAD
-self.assertAlmostEqual(30.0, ax.get_xlim()[1])
-# self.assertAlmostEqual(4.0, ax.get_ylim()[0])
-# self.assertAlmostEqual(8.0, ax.get_ylim()[1])
-== == == =
-from distutils.version import LooseVersion
-
-# different results with 1.5.3 and 2.1.1
-if color_func.__name__ != 'imshow' and LooseVersion(matplotlib.__version__) < LooseVersion("2"):
-    self.assertAlmostEqual(100.0, ax.get_xlim()[1])
-else:
-    self.assertAlmostEqual(30.0, ax.get_xlim()[1])
-self.assertAlmostEqual(4.0, ax.get_ylim()[0])
-self.assertAlmostEqual(8.0, ax.get_ylim()[1])
->> >> >> > master
+        # different results with 1.5.3 and 2.1.1
+        if color_func.__name__ != 'imshow' and LooseVersion(matplotlib.__version__) < LooseVersion("2"):
+            self.assertAlmostEqual(100.0, ax.get_xlim()[1])
+        else:
+            self.assertAlmostEqual(30.0, ax.get_xlim()[1])
+        self.assertAlmostEqual(4.0, ax.get_ylim()[0])
+        self.assertAlmostEqual(8.0, ax.get_ylim()[1])
 
 
 def test_update_colorplot_datalimits_for_pcolormesh(self):
