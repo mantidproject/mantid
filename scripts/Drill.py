9b5fefd19044df8252afc6001330d55076d6d3fa
Gagik Vardanyan <vardanyan@ill.fr>
2019-06-12 15:19:21 +0200

    Re #0 exported instrument selector

--- scripts/Drill.py
+++ scripts/Drill.py
@@ -6,8 +6,9 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from __future__ import (absolute_import, division, print_function)
 from mantidqt.gui_helper import get_qapplication
-from mantidqt.widgets import (jobtreeview, manageuserdirectories)
+from mantidqt.widgets import (jobtreeview, manageuserdirectories, instrumentselector)
 from qtpy.QtWidgets import QMainWindow, QWidget
+from qtpy import QtCore
 
 from qtpy import PYQT5
 if not PYQT5:
@@ -27,14 +28,15 @@ class Drill(QMainWindow, Ui_MainWindow):
 
         self.table.setRootIsDecorated(False)
 
-        self.table.setStyleSheet('background: None')
-
         row_entry = [''] * 6
         self.add_row(row_entry)
 
         table_signals = jobtreeview.JobTreeViewSignalAdapter(self.table, self)
 
-        self.mainLayout.addWidget(self.table)
+        self.instrumentselector = instrumentselector.InstrumentSelector(self)
+
+        self.headerLeft.addWidget(self.instrumentselector, 0, QtCore.Qt.AlignLeft)
+        self.center.addWidget(self.table)
 
     def add_row(self, value):
         value = [self.cell(x) for x in value]
