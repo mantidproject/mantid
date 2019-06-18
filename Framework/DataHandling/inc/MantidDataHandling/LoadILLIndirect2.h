fa9b9b3d99f79674bff4ab81c7524a9dc463b898
Gagik Vardanyan <vardanyan@ill.fr>
2019-06-18 13:23:00 +0200

    Re #25575 remove const from member variables

--- Framework/DataHandling/inc/MantidDataHandling/LoadILLIndirect2.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadILLIndirect2.h
@@ -55,8 +55,8 @@ private:
   std::string m_instrumentName; ///< Name of the instrument
 
   // Variables describing the data in the detector
-  const size_t m_numberOfTubes{16};          // number of tubes - X
-  const size_t m_numberOfPixelsPerTube{128}; // number of pixels per tube - Y
+  size_t m_numberOfTubes{16};          // number of tubes - X
+  size_t m_numberOfPixelsPerTube{128}; // number of pixels per tube - Y
   size_t m_numberOfChannels{1024};           // time channels - Z
   size_t m_numberOfSimpleDetectors{8};       // number of simple detector
   size_t m_numberOfMonitors{1};              // number of monitors
