736b7eb28ba47b72a036e098023aa67a3dda2cfa
Jiao Lin <linjiao@ornl.gov>
2019-06-25 08:55:06 -0400

    Refs #25992. makes flake happy

--- Testing/SystemTests/tests/analysis/GetEiT0atSNSSystemTest.py
+++ Testing/SystemTests/tests/analysis/GetEiT0atSNSSystemTest.py
@@ -10,16 +10,16 @@ GetEiT0atSNS
 """
 from systemtesting import MantidSystemTest
 import mantid.simpleapi as sm
-import hashlib
 
 
 class GetEiT0atSNSSystemTest(MantidSystemTest):
 
     def runTest(self):
+        # old DAS. all data in first frame
         ws = sm.LoadNexusMonitors('SEQ_169004.nxs.h5')
         Ei, T0 = sm.GetEiT0atSNS(ws)
         assert abs(Ei-8.31)< 0.01
-        
+        # new DAS. unwrapped
         ws = sm.LoadNexusMonitors('SEQ_176472.nxs.h5')
         Ei, T0 = sm.GetEiT0atSNS(ws)
         print Ei, T0
