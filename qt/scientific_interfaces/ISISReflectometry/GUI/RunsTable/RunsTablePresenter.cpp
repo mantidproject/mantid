135a48b4fe454c0b030050ce4755fd6712822845
Gemma Guest <gemma.guest@stfc.ac.uk>
2019-07-01 11:27:49 +0100

    Fix clang warning Re #26025

--- qt/scientific_interfaces/ISISReflectometry/GUI/RunsTable/RunsTablePresenter.cpp
+++ qt/scientific_interfaces/ISISReflectometry/GUI/RunsTable/RunsTablePresenter.cpp
@@ -616,7 +616,7 @@ void RunsTablePresenter::pasteRowsOntoRows(
     auto replacementRows = m_clipboard.createRowsForAllRoots();
     auto replacementRow = replacementRows.begin();
     auto replacementPoint = replacementRoots.cbegin();
-    for (; replacementRow < replacementRows.end(),
+    for (; replacementRow < replacementRows.end() &&
            replacementPoint < replacementRoots.cend();
          ++replacementRow, ++replacementPoint) {
       auto &group = groups[groupOf(*replacementPoint)];
