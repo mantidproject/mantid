mantidproject/mantid

commit e34368944945536b16f658f8ae9389298f69129e

Avoid summing empty list of detectors
Before choosing the method for how to sum the selected detectors, make
sure that the list is not empty. Also a minor fix in the uniform branch
to protect against the case when the first detector id supplied does not
point at a workspace index.
Fixes #24761

qt/widgets/instrumentview/src/InstrumentActor.cpp
@@ -458,6 +458,13 @@ double InstrumentActor::getIntegratedCounts(size_t index) const {
 void InstrumentActor::sumDetectors(const std::vector<size_t> &dets,
                                    std::vector<double> &x,
                                    std::vector<double> &y, size_t size) const {
+  // don't bother if no detectors are supplied
+  if (dets.empty() || size == 0) {
+    x.clear();
+    y.clear();
+    return;
+  }
+
   Mantid::API::MatrixWorkspace_const_sptr ws = getWorkspace();
   const auto blocksize = ws->blocksize();
   if (size > blocksize || size == 0) {
@@ -486,32 +493,25 @@ void InstrumentActor::sumDetectors(const std::vector<size_t> &dets,
 void InstrumentActor::sumDetectorsUniform(const std::vector<size_t> &dets,
                                           std::vector<double> &x,
                                           std::vector<double> &y) const {
-
-  bool isDataEmpty = dets.empty();
-
-  auto wi = getWorkspaceIndex(dets[0]);
-
-  if (wi == INVALID_INDEX)
-    isDataEmpty = true;
-
-  if (isDataEmpty) {
-    x.clear();
-    y.clear();
-    return;
-  }
-
   // find the bins inside the integration range
   size_t imin, imax;
-
-  getBinMinMaxIndex(wi, imin, imax);
+  size_t wi;
+  for (const auto det : dets) {
+    wi = getWorkspaceIndex(det);
+    if (wi != INVALID_INDEX) {
+      // first valid index is the one to use
+      getBinMinMaxIndex(wi, imin, imax);
+      break;
+    }
+  }
 
   Mantid::API::MatrixWorkspace_const_sptr ws = getWorkspace();
   const auto &XPoints = ws->points(wi);
   x.assign(XPoints.begin() + imin, XPoints.begin() + imax);
   y.resize(x.size(), 0);
   // sum the spectra
-  for (auto det : dets) {
-    auto index = getWorkspaceIndex(det);
+  for (const auto det : dets) {
+    const auto index = getWorkspaceIndex(det);
     if (index == INVALID_INDEX)
       continue;
     const auto &Y = ws->y(index);
@@ -535,12 +535,6 @@ void InstrumentActor::sumDetectorsRagged(const std::vector<size_t> &dets,
                                          std::vector<double> &x,
                                          std::vector<double> &y,
                                          size_t size) const {
-  if (dets.empty() || size == 0) {
-    x.clear();
-    y.clear();
-    return;
-  }
-
   Mantid::API::MatrixWorkspace_const_sptr ws = getWorkspace();
   //  create a workspace to hold the data from the selected detectors
   Mantid::API::MatrixWorkspace_sptr dws =
@@ -552,8 +546,8 @@ void InstrumentActor::sumDetectorsRagged(const std::vector<size_t> &dets,
 
   size_t nSpec = 0; // number of actual spectra to add
   // fill in the temp workspace with the data from the detectors
-  for (auto det : dets) {
-    auto index = getWorkspaceIndex(det);
+  for (const auto det : dets) {
+    const auto index = getWorkspaceIndex(det);
     if (index == INVALID_INDEX)
       continue;
     dws->setHistogram(nSpec, ws->histogram(index));

commit ec21dc59eb5754251788a130b5ed6f902debadba

Add overload to distinguish default vs actual size=0
Fixes issue introduced by e34368944945536b16f658f8ae9389298f69129e
where the default size=0 can now not be used to distinguish
auto-size computation between actual size=0.
Refs #26136

qt/widgets/instrumentview/inc/MantidQtWidgets/InstrumentView/InstrumentActor.h
@@ -163,7 +163,10 @@ public:
   double getIntegratedCounts(size_t index) const;
   /// Sum the counts in detectors
   void sumDetectors(const std::vector<size_t> &dets, std::vector<double> &x,
-                    std::vector<double> &y, size_t size = 0) const;
+                    std::vector<double> &y, size_t size) const;
+  /// Sum the counts in detectors.
+  void sumDetectors(const std::vector<size_t> &dets, std::vector<double> &x,
+                    std::vector<double> &y) const;
   /// Calc indexes for min and max bin values
   void getBinMinMaxIndex(size_t wi, size_t &imin, size_t &imax) const;
 

qt/widgets/instrumentview/src/InstrumentActor.cpp
@@ -452,7 +452,7 @@ double InstrumentActor::getIntegratedCounts(size_t index) const {
  * @param x :: (output) Time of flight values (or whatever values the x axis
  * has) to plot against.
  * @param y :: (output) The sums of the counts for each bin.
- * @param size :: (optional input) Size of the output vectors. If not given it
+ * @param size :: Size of the output vectors. If not given it
  * will be determined automatically.
  */
 void InstrumentActor::sumDetectors(const std::vector<size_t> &dets,
@@ -478,6 +478,16 @@ void InstrumentActor::sumDetectors(const std::vector<size_t> &dets,
     // should be faster than ragged
     sumDetectorsUniform(dets, x, y);
   }
+}
+
+/**
+ * @overload InstrumentActor::sumDetectors(const std::vector<size_t> &dets,
+ * std::vector<double> &x, std::vector<double> &y, size_t size = 0)
+ */
+void InstrumentActor::sumDetectors(const std::vector<size_t> &dets,
+                                   std::vector<double> &x,
+                                   std::vector<double> &y) const {
+  sumDetectors(dets, x, y, getWorkspace()->blocksize());
 }
 
 /**