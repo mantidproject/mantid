58124487a59c8d47db114a3685d9bd6a63fd3096
Samuel Jones <samjones714@gmail.com>
2019-07-04 09:52:55 +0100

    Re #26025 Add a batch if there is no batch

--- qt/scientific_interfaces/ISISReflectometry/GUI/MainWindow/MainWindowView.cpp
+++ qt/scientific_interfaces/ISISReflectometry/GUI/MainWindow/MainWindowView.cpp
@@ -43,6 +43,9 @@ IBatchView *MainWindowView::newBatch() {
 void MainWindowView::removeBatch(int batchIndex) {
   m_batchViews.erase(m_batchViews.begin() + batchIndex);
   m_ui.mainTabs->removeTab(batchIndex);
+  if (m_ui.mainTabs->count() == 0) {
+    newBatch();
+  }
 }
 
 std::vector<IBatchView *> MainWindowView::batches() const {
