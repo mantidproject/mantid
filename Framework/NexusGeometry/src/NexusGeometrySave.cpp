1623a57aa29e00983ae59e13dca5b0cbd817624d
Takudzwa Makoni <takudzwamilli@gmail.com>
2019-07-11 17:14:26 +0100

    array attribute in location for NXdetector

--- Framework/NexusGeometry/src/NexusGeometrySave.cpp
+++ Framework/NexusGeometry/src/NexusGeometrySave.cpp
@@ -49,15 +49,16 @@ const std::string METRES = "m";
 const std::string NAME = "name";
 const std::string UNITS = "units";
 const std::string SHAPE = "shape";
+const std::string VECTOR = "vector";
 
 const H5::DataSpace H5SCALAR(H5S_SCALAR);
 
 namespace forwardCompatibility {
 
-	/*
-	* Function "getObjName" 
-	* Ported from newer versions of the HDF API for forward compatibility.
-	*/
+/*
+ * Function "getObjName"
+ * Ported from newer versions of the HDF API for forward compatibility.
+ */
 
 ssize_t getObjName(const H5::H5Object &obj, char *obj_name, size_t buf_size) {
   // H5Iget_name will get buf_size-1 chars of the name to null terminate it
@@ -105,31 +106,50 @@ std::string getObjName(const H5::H5Object &obj) {
   return (obj_name);
 }
 
-} // namespace forward
+} // namespace forwardCompatibility
 
 } // namespace
 
 namespace NexusGeometrySave {
 
-
+/*
+ * Function: strTypeOfSize
+ * Produces the HDF StrType of size equal to that of the
+ * input string.
+ *
+ * @param str : std::string
+ * @return string datatype of size = length of input string
+ */
 inline H5::StrType strTypeOfSize(const std::string &str) {
   H5::StrType stringType(1, (size_t)str.length());
   return stringType;
 }
 
-
-
-inline void writeStrAttributeToGroupHelper(H5::Group &grp,
-                                           const std::string &attrName,
-                                           const std::string &attrVal,
-                                           H5::DataSpace dataSpace = H5SCALAR) {
+/*
+ * Function: writeAttribute
+ * writes a StrType HDF attribute and attribute value to a HDF group.
+ *
+ * @param grp : HDF group object.
+ * @param attrname : attribute name.
+ * @param attrVal : string attribute value to be stored in attribute.
+ */
+inline void writeStrAttribute(H5::Group &grp, const std::string &attrName,
+                              const std::string &attrVal,
+                              H5::DataSpace dataSpace = H5SCALAR) {
   H5::StrType dataType = strTypeOfSize(attrVal);
   H5::Attribute attribute = grp.createAttribute(attrName, dataType, dataSpace);
   attribute.write(dataType, attrVal);
 }
 
-
-
+/*
+ * Function: writeStrAttribute
+ * Overload function which writes a StrType HDF attribute and attribute value to
+ * a HDF dataset.
+ *
+ * @param dSet : HDF dataset object.
+ * @param attrname : attribute name.
+ * @param attrVal : string attribute value to be stored in attribute.
+ */
 inline void writeStrAttribute(H5::DataSet &dSet, const std::string &attrName,
                               const std::string &attrVal,
                               H5::DataSpace dataSpace = H5SCALAR) {
@@ -138,11 +158,15 @@ inline void writeStrAttribute(H5::DataSet &dSet, const std::string &attrName,
   attribute.write(dataType, attrVal);
 }
 
-inline void writeStrValue(H5::DataSet &dSet, std::string dSetValue) {
-
-  dSet.write(dSetValue, dSet.getDataType(), H5SCALAR);
-}
-
+/*
+ * Function: writeDetectorNumber
+ * For use with NXdetector group. Finds all detector pixels by index in the
+ * component Info object and writes the detector numbers to a new dataset in the
+ * group.
+ *
+ * @param grp : NXdetector group (HDF group)
+ * @param compInfo : componentInfo object.
+ */
 inline void writeDetectorNumber(H5::Group &grp,
                                 const Geometry::ComponentInfo &compInfo) {
 
@@ -172,6 +196,23 @@ inline void writeDetectorNumber(H5::Group &grp,
   detectorNumber.write(&data, H5::PredType::NATIVE_INT, space);
 }
 
+/*
+ * Function: writeLocation
+ * For use with NXdetector group. Writes position of detector bank to dataset
+ * and metadata as attributes:
+ *
+ * =>	norm of position vector stored as value in NXtransformation dataset
+ *		'location'.
+ * =>	normalised position vector stored as array in attribute 'vector'.
+ * =>	units of measurement stored as string value in attribute 'units'.
+ * =>	type of transformation stored as string value in attribute
+ *		'transformation_type'.
+ * =>	dependency of transformation stored as string value in attribute
+ *		'depends_on'.
+ *
+ * @param grp : NXdetector group : (HDF group)
+ * @param compInfo : componentInfo object.
+ */
 inline void writeLocation(H5::Group &grp,
                           const Geometry::ComponentInfo &compInfo) {
 
@@ -179,7 +220,8 @@ inline void writeLocation(H5::Group &grp,
   double norm;
 
   H5::DataSet location;
-  H5::DataSpace space;
+  H5::DataSpace dspace;
+  H5::DataSpace aspace;
 
   H5::Attribute vector;
   H5::Attribute units;
@@ -187,17 +229,12 @@ inline void writeLocation(H5::Group &grp,
   H5::Attribute dependsOn;
   H5::Attribute nxClass;
 
-  int rank = 1;
-  hsize_t dims[(hsize_t)1];
-  dims[0] = 1;
+  int drank = 1;
+  hsize_t ddims[(hsize_t)1];
+  ddims[0] = 1;
 
-  space = H5Screate_simple(rank, dims, NULL);
-  location = grp.createDataSet(LOCATION, H5::PredType::NATIVE_DOUBLE, space);
-
-  // write attributes
-  H5::StrType strSize = strTypeOfSize(NX_TRANSFORMATION);
-  nxClass = location.createAttribute(NX_CLASS, strSize, H5SCALAR);
-  nxClass.write(strSize, NX_TRANSFORMATION);
+  dspace = H5Screate_simple(drank, ddims, NULL);
+  location = grp.createDataSet(LOCATION, H5::PredType::NATIVE_DOUBLE, dspace);
 
   // write norm value to dataset
   position = Kernel::toVector3d(compInfo.position(compInfo.root()));
@@ -205,10 +242,48 @@ inline void writeLocation(H5::Group &grp,
   norm = position.norm();
 
   std::vector<double> data = {norm};
-  location.write(&data, H5::PredType::NATIVE_DOUBLE, space);
+  location.write(&data, H5::PredType::NATIVE_DOUBLE, dspace);
+
+  // write attributes
+  H5::StrType strSize = strTypeOfSize(NX_TRANSFORMATION);
+  nxClass = location.createAttribute(NX_CLASS, strSize, H5SCALAR);
+  nxClass.write(strSize, NX_TRANSFORMATION);
+
+  std::vector<double> stdNormPos;
+  stdNormPos.resize(normalisedPosition.size());
+  Eigen::VectorXd::Map(&stdNormPos[0], normalisedPosition.size()) =
+      normalisedPosition;
+
+  int arank = 1;
+  hsize_t adims[(hsize_t)3];
+  adims[0] = 1;
+  adims[1] = 1;
+  adims[2] = 1;
+
+  aspace = H5Screate_simple(3, adims, NULL);
+  vector =
+      location.createAttribute(VECTOR, H5::PredType::NATIVE_DOUBLE, aspace);
+
+  vector.write(H5::PredType::NATIVE_DOUBLE, "y");
 }
 
-
+/*
+ * Function: writeOrientation
+ * For use with NXdetector group. Writes rotation of detector bank to dataset
+ * and metadata as attributes:
+ *
+ * =>	magnitude of angle of rotation stored as value in NXtransformation
+ *dataset 'location'.
+ * =>	axis of rotation as unit vector stored as array in attribute 'vector'.
+ * =>	units of measurement stored as string value in attribute 'units'.
+ * =>	type of transformation stored as string value in attribute
+ *		'transformation_type'.
+ * =>	dependency of transformation stored as string value in attribute
+ *		'depends_on'.
+ *
+ * @param grp : NXdetector group : (HDF group)
+ * @param compInfo : componentInfo object.
+ */
 inline void writeOrientation(H5::Group &grp,
                              const Geometry::ComponentInfo &compInfo) {
 
@@ -244,47 +319,72 @@ inline void writeOrientation(H5::Group &grp,
   orientation.write(&data, H5::PredType::NATIVE_DOUBLE, space);
 }
 
-
+/*
+ * Function: instrument
+ * for NXentry parent (root group). Produces an NXinstrument group in the parent
+ * group, and writes Nexus compliant datasets and metadata stored in attributes
+ * to the new group.
+ *
+ * @param parent : parent group in which to write the NXinstrument group.
+ * @param compInfo : componentInfo object.
+ */
 H5::Group instrument(const H5::Group &parent,
                      const Geometry::ComponentInfo &compInfo) {
 
   std::string instrumentNameStr = compInfo.name(compInfo.root());
   H5::Group group = parent.createGroup(instrumentNameStr);
-  writeStrAttributeToGroupHelper(group, NX_CLASS, NX_INSTRUMENT);
+  writeStrAttribute(group, NX_CLASS, NX_INSTRUMENT);
   H5::StrType nameStrSize = strTypeOfSize(instrumentNameStr);
   H5::DataSet name = group.createDataSet("name", nameStrSize, H5SCALAR);
   writeStrAttribute(name, SHORT_NAME,
                     instrumentNameStr); // placeholder
-  writeStrValue(name, instrumentNameStr);
+
+  name.write(instrumentNameStr, name.getDataType(), H5SCALAR);
 
   return group;
 }
 
+/*
+ * Function: sample
+ * For NXentry parent (root group). Produces an NXsample group in the parent
+ * group, and writes the Nexus compliant datasets and metadata stored in
+ * attributes to the new group.
+ *
+ *
+ * @param parent : parent group in which to write the NXinstrument group.
+ * @param compInfo : componentInfo object.
+ */
 H5::Group sample(const H5::Group &parent,
                  const Geometry::ComponentInfo &compInfo) {
 
   H5::Group m_group;
   std::string sampleName = compInfo.name(compInfo.sample());
   m_group = parent.createGroup(sampleName);
-  writeStrAttributeToGroupHelper(m_group, NX_CLASS, NX_SAMPLE);
+  writeStrAttribute(m_group, NX_CLASS, NX_SAMPLE);
 
   return m_group;
 }
 
-
-
-
-
+/*
+ * Function: detector
+ * For NXinstrument parent (root group). Produces an NXdetctor group in the
+ * parent group, and writes the Nexus compliant datasets and metadata stored in
+ * attributes to the new group.
+ *
+ *
+ * @param parent : parent group in which to write the NXinstrument group.
+ * @param compInfo : componentInfo object.
+ */
 H5::Group detector(const std::string &name, const H5::Group &parent,
                    const Geometry::ComponentInfo &compInfo) {
 
   H5::Group child;
   child = parent.createGroup(name);
-  writeStrAttributeToGroupHelper(child, NX_CLASS, NX_DETECTOR);
+  writeStrAttribute(child, NX_CLASS, NX_DETECTOR);
 
-  std::string dependencyStr =
-      forwardCompatibility::getObjName(child) + "/" + LOCATION;         // needs to be full path to group
-  std::string localNameStr = "INST"; // placeholder
+  std::string dependencyStr = forwardCompatibility::getObjName(child) + "/" +
+                              LOCATION; // needs to be full path to group
+  std::string localNameStr = "INST";    // placeholder
   H5::StrType dependencyStrSize = strTypeOfSize(dependencyStr);
   H5::StrType localNameStrSize = strTypeOfSize(localNameStr);
 
@@ -296,32 +396,51 @@ H5::Group detector(const std::string &name, const H5::Group &parent,
       child.createDataSet(LOCAL_NAME, localNameStrSize, H5SCALAR);
   H5::DataSet dependency =
       child.createDataSet(DEPENDS_ON, dependencyStrSize, H5SCALAR);
-  writeStrValue(localName, localNameStr); // placeholder
-  writeStrValue(dependency, dependencyStr);
+
+  localName.write(localNameStr, localName.getDataType(), H5SCALAR);
+  dependency.write(dependencyStr, dependency.getDataType(), H5SCALAR);
 
   return child;
 }
 
-
-
+/*
+ * Function: source
+ * For NXentry (root group). Produces an NXsource group in the parent group, and
+ * writes the Nexus compliant datasets and metadata stored in attributes to the
+ * new group.
+ *
+ *
+ * @param parent : parent group in which to write the NXinstrument group.
+ * @param compInfo : componentInfo object.
+ */
 H5::Group source(const H5::Group &parent,
                  const Geometry::ComponentInfo &compInfo) {
 
   H5::Group child;
   child = parent.createGroup(compInfo.name(compInfo.source()));
-  writeStrAttributeToGroupHelper(child, NX_CLASS, NX_SOURCE);
+  writeStrAttribute(child, NX_CLASS, NX_SOURCE);
   return child;
 }
 
 } // namespace NexusGeometrySave
 
-
-
-
-
+/*
+ * Function: Saveinstrument
+ * writes the instrument from memory to disk in Nexus format.
+ *
+ * @param compInfo : componentInfo object.
+ * @param fullPath : save destination as full path.
+ * @param reporter : report to progressBase.
+ */
 void saveInstrument(const Geometry::ComponentInfo &compInfo,
                     const std::string &fullPath,
                     Kernel::ProgressBase *reporter) {
+
+  /*
+  ==============================================================================================================
+  Exception handling.
+  ==============================================================================================================
+  */
 
   boost::filesystem::path tmp(fullPath);
   if (!boost::filesystem::is_directory(tmp.root_directory())) {
@@ -354,21 +473,31 @@ void saveInstrument(const Geometry::ComponentInfo &compInfo,
 
   /*
  ==============================================================================================================
-  write component to tree structure.
+ write instrument to file.
  ==============================================================================================================
  */
 
-  H5::H5File file(fullPath, H5F_ACC_TRUNC);
-  H5::Group root = file.createGroup("/raw_data_1");
-  NexusGeometrySave::writeStrAttributeToGroupHelper(root, NX_CLASS, NX_ENTRY);
-  H5::Group instr = NexusGeometrySave::instrument(root, compInfo);
-  H5::Group detector =
-      NexusGeometrySave::detector("detector_0", instr, compInfo);
-  H5::Group source = NexusGeometrySave::source(instr, compInfo);
-  H5::Group sample = NexusGeometrySave::sample(root, compInfo);
+  H5::H5File file(fullPath, H5F_ACC_TRUNC); // open file
+  H5::Group root, instrument, sample, detector, source;
 
+  // create NXentry root group
+  root = file.createGroup("/raw_data_1");
+  NexusGeometrySave::writeStrAttribute(root, NX_CLASS, NX_ENTRY);
 
-  file.close();
+  // NXinstrument
+  instrument = NexusGeometrySave::instrument(root, compInfo);
+
+  // NXdetector
+  detector = NexusGeometrySave::detector("detector_0", instrument, compInfo);
+
+  // NXsource
+  source = NexusGeometrySave::source(instrument, compInfo);
+
+  // NXsample
+  sample = NexusGeometrySave::sample(root, compInfo);
+
+  file.close(); // close file
+
 } // saveInstrument
 
 } // namespace NexusGeometry
