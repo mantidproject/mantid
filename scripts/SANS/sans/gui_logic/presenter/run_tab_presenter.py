eef85ef815dd40069365b8aaad3f2c8e2590761a
Matthew Andrew <matthew.andrew@tessella.com>
2019-07-19 13:48:08 +0100

    Only reset settings if empty string

--- scripts/SANS/sans/gui_logic/presenter/run_tab_presenter.py
+++ scripts/SANS/sans/gui_logic/presenter/run_tab_presenter.py
@@ -323,7 +323,8 @@ class RunTabPresenter(object):
                                                                    str)},
                                                     line_edits={"user_file":
                                                                 self._view.user_file_line_edit})
-            self._view.gui_properties_handler.set_setting("user_file", self._view.get_user_file_path())
+            if self._view.get_user_file_path() == '':
+                self._view.gui_properties_handler.set_setting("user_file", '')
 
     def on_user_file_load(self):
         """
