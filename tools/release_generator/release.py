68d1525071c040c217dabab4345ad495d4688f2a
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2019-07-23 12:12:57 +0100

    Fix GitHub milestone link

--- tools/release_generator/release.py
+++ tools/release_generator/release.py
@@ -96,7 +96,7 @@ For a full list of all issues addressed during this release please see the `GitH
 
 .. _forum: http://forum.mantidproject.org
 
-.. _GitHub milestone: http://github.com/mantidproject/mantid/pulls?utf8=%E2%9C%93&q=is%3Apr+milestone%3A{milestone}+is%3Amerged
+.. _GitHub milestone: http://github.com/mantidproject/mantid/pulls?utf8=%E2%9C%93&q=is%3Apr+milestone%3A"Release {milestone}"+is%3Amerged
 
 .. _GitHub release page: https://github.com/mantidproject/mantid/releases/tag/v{version}
 ''',
