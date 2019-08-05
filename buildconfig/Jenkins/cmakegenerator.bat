3d819418368f7f1d99e5d14b8a4832ec8f7d4a4f
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-08-05 15:21:34 +0100

    Update Windows' build scripts for VS 2019
    This will tell the build servers to generate targets for and build with
    Visual Studio 16 2019. CMake now requires the -A flag to set the build
    architecture. Keep using the Windows 7 compatible SDK for now.

--- buildconfig/Jenkins/cmakegenerator.bat
+++ buildconfig/Jenkins/cmakegenerator.bat
@@ -4,4 +4,5 @@
 :: when building on newer versions of Windows. This value must be supplied
 :: externally and cannot be supplied in the cmake configuration
 set SDK_VERS=8.1
-set CM_GENERATOR=Visual Studio 15 2017 Win64
+set CM_GENERATOR=Visual Studio 16 2019
+set CM_ARCH=Win64
