790bb0fe4a21fdc094c5eb8dd1059751864fff57
Moore <lamar.moore@stfc.ac.uk>
2019-08-12 07:10:37 +0100

    fix pragma #26213

--- Framework/NexusGeometry/inc/MantidNexusGeometry/JSONGeometryParser.h
+++ Framework/NexusGeometry/inc/MantidNexusGeometry/JSONGeometryParser.h
@@ -9,7 +9,9 @@
 
 // Disable warnings generated from jsoncpp library related to inheriting
 // std::exception
+#ifdef _WIN32
 #pragma warning(disable : 4275)
+#endif
 
 #include "MantidGeometry/IDTypes.h"
 #include "MantidNexusGeometry/DllConfig.h"
