7ce5ac0874c4927a519b6ba6d5fa44df596b4975
Giovanni Di Siena <giodisiena@gmail.com>
2019-08-13 15:56:37 +0100

    Re #26134 Removes about MantidPlot help button (default)

--- qt/widgets/common/src/MantidDialog.cpp
+++ qt/widgets/common/src/MantidDialog.cpp
@@ -19,11 +19,8 @@ using namespace MantidQt::API;
 /**
  * Default Constructor
  */
-MantidDialog::MantidDialog(QWidget *parent) : QDialog(parent), m_pyRunner() {
-  // remove redundant help button
-  Qt::WindowFlags flags =
-      Qt::WindowCloseButtonHint | Qt::WindowType::WindowTitleHint;
-  this->setWindowFlags(flags);
+MantidDialog::MantidDialog(QWidget *parent, Qt::WindowFlags flags)
+    : QDialog(parent, flags), m_pyRunner() {
   // re-emit the run Python code from m_pyRunner, to work this signal must reach
   // the slot in QtiPlot
   connect(&m_pyRunner, SIGNAL(runAsPythonScript(const QString &, bool)), this,
