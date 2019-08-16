9b8aa0df4b0e8a7888fc79af4fda8cbc5988b90e
Gemma Guest <gemma.guest@stfc.ac.uk>
2019-08-16 17:17:26 +0100

    Add an interface and mock for the slit calculator
    Re #26533

---
+++ qt/widgets/common/inc/MantidQtWidgets/Common/ISlitCalculator.h
@@ -0,0 +1,23 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#ifndef MANTID_MANTIDWIDGETS_ISLITCALCULATOR_H
+#define MANTID_MANTIDWIDGETS_ISLITCALCULATOR_H
+
+#include "DllOption.h"
+#include <string>
+
+namespace MantidQt {
+namespace MantidWidgets {
+class EXPORT_OPT_MANTIDQT_COMMON ISlitCalculator {
+public:
+  virtual void setCurrentInstrumentName(std::string instrumentName) = 0;
+  virtual void processInstrumentHasBeenChanged() = 0;
+};
+} // namespace MantidWidgets
+} // namespace MantidQt
+
+#endif /* MANTID_MANTIDWIDGETS_ISLITCALCULATOR_H */
