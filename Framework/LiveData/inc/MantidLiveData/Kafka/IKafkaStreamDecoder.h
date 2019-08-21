22531cb16d0fd651a88549f916ac0a1f5a01bd05
Moore <lamar.moore@stfc.ac.uk>
2019-08-21 07:39:33 +0100

    store chopper timestamps in workspace logs

--- Framework/LiveData/inc/MantidLiveData/Kafka/IKafkaStreamDecoder.h
+++ Framework/LiveData/inc/MantidLiveData/Kafka/IKafkaStreamDecoder.h
@@ -59,7 +59,8 @@ public:
                       const std::string &streamTopic,
                       const std::string &runInfoTopic,
                       const std::string &spDetTopic,
-                      const std::string &sampleEnvTopic);
+                      const std::string &sampleEnvTopic,
+                      const std::string &chopperTopic);
   virtual ~IKafkaStreamDecoder();
   IKafkaStreamDecoder(const IKafkaStreamDecoder &) = delete;
   IKafkaStreamDecoder &operator=(const IKafkaStreamDecoder &) = delete;
@@ -124,6 +125,8 @@ protected:
   /// Populate cache workspaces with data from messages
   virtual void sampleDataFromMessage(const std::string &buffer) = 0;
 
+  void writeChopperInfoToWorkspaceLogs(Mantid::API::Run &mutableRun);
+
   /// For LoadLiveData to extract the cached data
   virtual API::Workspace_sptr extractDataImpl() = 0;
 
@@ -134,6 +137,7 @@ protected:
   const std::string m_runInfoTopic;
   const std::string m_spDetTopic;
   const std::string m_sampleEnvTopic;
+  const std::string m_chopperTopic;
   /// Flag indicating if user interruption has been requested
   std::atomic<bool> m_interrupt;
   /// Subscriber for the data stream
@@ -147,6 +151,8 @@ protected:
   std::unique_ptr<IKafkaStreamSubscriber> m_runStream;
   /// Subscriber for the run info stream
   std::unique_ptr<IKafkaStreamSubscriber> m_spDetStream;
+  /// Subscriber for the chopper timestamp stream
+  std::unique_ptr<IKafkaStreamSubscriber> m_chopperStream;
   /// Run number
   std::string m_runId;
 
--- Framework/LiveData/src/Kafka/KafkaEventStreamDecoder.cpp
+++ Framework/LiveData/src/Kafka/KafkaEventStreamDecoder.cpp
@@ -24,6 +24,7 @@ GNU_DIAG_OFF("conversion")
 #include "private/Schema/ev42_events_generated.h"
 #include "private/Schema/f142_logdata_generated.h"
 #include "private/Schema/is84_isis_events_generated.h"
+#include "private/Schema/tdct_timestamps_generated.h"
 #include "private/Schema/y2gw_run_info_generated.h"
 GNU_DIAG_ON("conversion")
 
@@ -53,6 +54,7 @@ const std::string RUN_START_PROPERTY = "run_start";
 const std::string RUN_MESSAGE_ID = "y2gw";
 const std::string EVENT_MESSAGE_ID = "ev42";
 const std::string SAMPLE_MESSAGE_ID = "f142";
+const std::string CHOPPER_MESSAGE_ID = "tdct";
 
 /**
  * Append sample log data to existing log or create a new log if one with
@@ -117,9 +119,10 @@ using Types::Event::TofEvent;
 KafkaEventStreamDecoder::KafkaEventStreamDecoder(
     std::shared_ptr<IKafkaBroker> broker, const std::string &eventTopic,
     const std::string &runInfoTopic, const std::string &spDetTopic,
-    const std::string &sampleEnvTopic, const std::size_t bufferThreshold)
+    const std::string &sampleEnvTopic, const std::string &chopperTopic,
+    const std::size_t bufferThreshold)
     : IKafkaStreamDecoder(broker, eventTopic, runInfoTopic, spDetTopic,
-                          sampleEnvTopic),
+                          sampleEnvTopic, chopperTopic),
       m_intermediateBufferFlushThreshold(bufferThreshold) {
 #ifndef _OPENMP
   g_log.warning() << "Multithreading is not available on your system. This "
