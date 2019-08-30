de44d74fd163a011422c9289f7aaf763088871ff
Phil <philipc99@hotmail.co.uk>
2019-08-30 11:11:03 +0100

    Re #26611 remove unused import

--- qt/applications/workbench/workbench/plotting/propertiesdialog.py
+++ qt/applications/workbench/workbench/plotting/propertiesdialog.py
@@ -15,7 +15,6 @@ from __future__ import (absolute_import, unicode_literals)
 from mantidqt.plotting.figuretype import FigureType, figure_type
 from mantidqt.utils.qt import load_ui
 from matplotlib.collections import QuadMesh
-from matplotlib.image import AxesImage
 from qtpy.QtGui import QDoubleValidator, QIcon
 from qtpy.QtWidgets import QDialog, QWidget
 
