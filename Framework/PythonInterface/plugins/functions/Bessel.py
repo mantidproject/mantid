476eb3afb8903820943b4e78d55a5ed4e7b72985
Wu <Chien.wu@stfc.ac.uk>
2019-09-11 16:34:56 +0100

    Added Bessel Function files

--- Framework/PythonInterface/plugins/functions/Bessel.py
+++ Framework/PythonInterface/plugins/functions/Bessel.py
@@ -1,7 +1,21 @@
-from mantid.api import *
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+# pylint: disable=invalid-name, anomalous-backslash-in-string, attribute-defined-outside-init
+
+"""
+@author Hank Wu, RAL STFC
+@date September 11, 2019
+"""
+
+from mantid.api import IFunction1D, FunctionFactory
 import math
 import numpy as np
 from scipy import special as sp
+
 
 class Bessel(IFunction1D):
 
@@ -10,12 +24,13 @@ class Bessel(IFunction1D):
 
   def init(self):
     self.declareParameter("A0",1)
-    self.declareParameter("fi",0.1)
+    self.declareParameter("phi",0.1)
     self.declareParameter("nu",0.1)
-    
+
   def function1D(self, x):
     A0 = self.getParameterValue("A0")
-    fi = self.getParameterValue("fi")
+    phi = self.getParameterValue("phi")
     nu = self.getParameterValue("nu")
-    return A0*sp.j0(2*np.pi*nu*x+fi)
+    return A0*sp.j0(2*np.pi*nu*x+phi)
+    
 FunctionFactory.subscribe(Bessel)
---
+++ docs/source/fitting/fitfunctions/Bessel.rst
@@ -0,0 +1,26 @@
+.. _func-Bessel:
+
+==============================
+Bessel
+==============================
+
+.. index:: Bessel
+
+Description
+-----------
+
+Fitting function for use by Muon scientists defined by:
+
+.. math:: \begin{align} A(t)&=A_0J_0(2\pi\nu t+\phi),\\ \text{where} \qquad J_0(x)&=\sum_{l=0}^{\infty}\frac{(-1)^l}{2^{2l}(l!)^2}x^{2l} \end{align}
+
+.. attributes::
+
+.. properties::
+J_0(x) is the Bessel Function
+A_0 is the amplitude at $t=0$
+$\nu$ is wavenumber
+$\phi$ is the phase
+
+.. categories::
+
+.. sourcelink::
