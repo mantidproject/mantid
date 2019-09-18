c15fc28363454fe657cd0ff18a5b39351a5a0a7b
Ciara Nightingale <ciara.nightingale@stfc.ac.uk>
2019-09-18 11:53:05 +0100

    Re #25341 edit test

--- scripts/test/Muon/grouping_tab/pairing_table_presenter_test.py
+++ scripts/test/Muon/grouping_tab/pairing_table_presenter_test.py
@@ -259,7 +259,7 @@ class PairingTablePresenterTest(unittest.TestCase):
         six.assertCountEqual(self, self.model.pair_names, ["pair_1", "pair_2", "pair_3"])
 
     def test_that_adding_pair_with_context_menu_allows_for_name_specification(self):
-        self.add_pair_from_grouping_table(MuonPair(pair_name="new"))
+        self.GroupingTabPresenter(self.model, self.view).add_pair_from_grouping_table(MuonPair(pair_name="new"))
 
         self.assertEqual(self.view.get_table_item_text(0, 0), "new")
 
