cd31b8c72ef5b0f5fdfcfb194f5d8df6f4bc8473
Wu <Chien.wu@stfc.ac.uk>
2019-09-18 15:37:18 +0100

    Fixed indentation

--- Framework/PythonInterface/plugins/functions/CombGaussLorentzKT.py
+++ Framework/PythonInterface/plugins/functions/CombGaussLorentzKT.py
@@ -12,18 +12,18 @@ import numpy as np
 
 class CombGaussLorentzKT(IFunction1D):
 
-	def category(self):
-		return "Muon"
+    def category(self):
+        return "Muon"
 
-	def init(self):
-		self.declareParameter("A0", 0.5, 'Amplitude')
-		self.declareParameter("Lambda", 0.1, 'Exponential decay rate')
-		self.declareParameter("Sigma", 0.2, 'KuboToyabe decay rate')
+    def init(self):
+        self.declareParameter("A0", 0.5, 'Amplitude')
+        self.declareParameter("Lambda", 0.1, 'Exponential decay rate')
+        self.declareParameter("Sigma", 0.2, 'KuboToyabe decay rate')
 
-	def function1D(self, x):
-		A0 = self.getParameterValue("A0")
-		Lambda = self.getParameterValue("Lambda")
-		Sigma = self.getParameterValue("Sigma")
-		return A0 * ((1./3) + (2./3) * (1-(Sigma * x) ** 2 - Lambda * x) * np.exp(-(Sigma * x) ** 2/2-Lambda * x))
+    def function1D(self, x):
+        A0 = self.getParameterValue("A0")
+        Lambda = self.getParameterValue("Lambda")
+        Sigma = self.getParameterValue("Sigma")
+        return A0 * ((1./3) + (2./3) * (1-(Sigma * x) ** 2 - Lambda * x) * np.exp(-(Sigma * x) ** 2/2-Lambda * x))
 
 FunctionFactory.subscribe(CombGaussLorentzKT)
