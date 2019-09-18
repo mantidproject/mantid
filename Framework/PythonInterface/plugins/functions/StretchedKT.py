d8379e291144b81806fad49ce4ab8ba833cd492d
Wu <Chien.wu@stfc.ac.uk>
2019-09-18 15:44:21 +0100

    Indentation fixing

--- Framework/PythonInterface/plugins/functions/StretchedKT.py
+++ Framework/PythonInterface/plugins/functions/StretchedKT.py
@@ -12,21 +12,21 @@ import numpy as np
 
 class StretchedKT(IFunction1D):
 
-	def category(self):
-		return "Muon"
+    def category(self):
+        return "Muon"
 
-	def init(self):
-		self.declareParameter("A0", 1)
-		self.declareParameter("Beta", 1)
-		self.declareParameter("Sigma", 0.1)
+    def init(self):
+        self.declareParameter("A0", 1)
+        self.declareParameter("Beta", 1)
+        self.declareParameter("Sigma", 0.1)
 
-	def function1D(self, x):
-		A0 = self.getParameterValue("A0")
-		beta = self.getParameterValue("Beta")
-		Sigma = self.getParameterValue("Sigma")
-		product = Sigma * x
-		A=(2./3) * (1 - product ** beta)
-		B=np.exp(- product ** beta / beta)
-		return A0*(1./3 + A * B)
+    def function1D(self, x):
+        A0 = self.getParameterValue("A0")
+        beta = self.getParameterValue("Beta")
+        Sigma = self.getParameterValue("Sigma")
+        product = Sigma * x
+        A=(2./3) * (1 - product ** beta)
+        B=np.exp(- product ** beta / beta)
+        return A0*(1./3 + A * B)
 
 FunctionFactory.subscribe(StretchedKT)
