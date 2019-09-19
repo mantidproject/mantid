ebe814288a623097a25677764df4084c0a809c3d
Wu <Chien.wu@stfc.ac.uk>
2019-09-19 15:52:18 +0100

    added space for readability in Test.py

--- Framework/PythonInterface/test/python/plugins/functions/CombGaussLorentzKTTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/CombGaussLorentzKTTest.py
@@ -23,13 +23,13 @@ class CombGaussLorentzKTTest(unittest.TestCase):
 		input = [0.0, 4.0, 8.0, 12.0]
 		expected = [0.5, 0.16017663658720036, 0.06838823307044849, 0.13307716823662552]
 		tolerance = 1.0e-05
-		status, output = check_output("CombGaussLorentzKT", input, expected, tolerance, A0=0.5, Lambda=0.1, Sigma=0.2)
+		status, output = check_output("CombGaussLorentzKT", input, expected, tolerance, A0 = 0.5, Lambda = 0.1, Sigma = 0.2)
 		if not status:
 			msg = 'Computed output {} from input {} unequal to expected: {}'
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'CombGaussLorentzKT', guess=dict(A0=0.55, Lambda=0.15, Sigma=0.5), target=dict(A0=0.5, Lambda=0.1, Sigma=0.2), atol=0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'CombGaussLorentzKT', guess = dict(A0 = 0.55, Lambda = 0.15, Sigma = 0.5), target=dict(A0 = 0.5, Lambda = 0.1, Sigma = 0.2), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
