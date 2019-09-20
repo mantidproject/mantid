5230b507d9a0529f2b91bf69883fa7f05f7e15be
Wu <Chien.wu@stfc.ac.uk>
2019-09-20 14:42:09 +0100

    Added MuH function

---
+++ Framework/PythonInterface/plugins/functions/MuH.py
@@ -0,0 +1,34 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+# pylint: disable=invalid-name, anomalous-backslash-in-string, attribute-defined-outside-init
+
+from mantid.api import IFunction1D, FunctionFactory
+import numpy as np
+
+
+class MuH(IFunction1D):
+
+    def category(self):
+        return "Muon"
+
+    def init(self):
+        self.declareParameter("Amp", 0.5, 'Amplitude')
+        self.declareParameter("FreqD", 0.5, 'Frequency (MHz)' )
+        self.declareParameter("Lambda", 0.3, 'Exponential decay rate')
+        self.declareParameter("Sigma", 0.05, 'Gaussian decay rate')
+
+    def function1D(self, x):
+        Amp = self.getParameterValue("Amp")
+        FreqD = self.getParameterValue("FreqD")
+        Lambda = self.getParameterValue("Lambda")
+        sigma = self.getParameterValue("Sigma")
+        OmegaD = FreqD * 2 * np.pi
+        gau = np.exp(-0.5 * (x * sigma) ** 2)
+        Lor = np.exp( - Lambda * x)
+        return Amp * gau * Lor * (1 + np.cos(OmegaD * x) + 2 * np.cos(0.5 * OmegaD * x) + 2 * np.cos(1.5 * OmegaD * x)) / 6
+
+FunctionFactory.subscribe(MuH)
---
+++ docs/source/fitting/fitfunctions/MuH.rst
@@ -0,0 +1,35 @@
+.. _func-MuH:
+
+===
+MuH
+===
+
+.. index:: MuH
+
+Description
+-----------
+
+Fitting function for use by Muon scientists defined by:
+
+.. math:: A(t)=\frac{A_0}{6}e^{-\lambda t}e^{-\frac{(\sigma t)^2}{2}}\left(1+\cos(\omega_Dt)+2\cos(\omega_Dt/2)+2\cos(\frac32\omega_Dt)\right)
+
+:math:`A_0` is the amplitude,
+
+
+:math:`\lambda` is exponential rate,
+
+:math:`\sigma` is gaussian decay rate,
+
+:math:`\nu` (MHz) is the oscillating frequency,
+
+and :math:`\phi` (rad) is the phase at time :math:`t=0`.
+
+.. figure:: /images/MuH.png
+
+.. attributes::
+
+.. properties::
+
+.. categories::
+
+.. sourcelink::
