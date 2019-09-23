3735659ffb8f7204b3889257643cca92e7d4198f
Wu <Chien.wu@stfc.ac.uk>
2019-09-23 10:34:55 +0100

    Updated .py again

--- Framework/PythonInterface/plugins/functions/Meier.py
+++ Framework/PythonInterface/plugins/functions/Meier.py
@@ -21,7 +21,7 @@ class Meier(IFunction1D):
         self.declareParameter("FreqQ", 0.05, 'Frequency due to quadrupole interaction of the nuclear spin (MHz)')
         self.declareParameter("Spin", 3.5, 'J, Total orbital quanutm number')
         self.declareParameter("Sigma", 0.2, 'Gaussian decay rate')
-        self.declareParameter("Lambda", 0.1, 'Exponential decay rate')       
+        self.declareParameter("Lambda", 0.1, 'Exponential decay rate')
 
     def function1D(self, x):
         A0 = self.getParameterValue("A0")
@@ -55,7 +55,7 @@ class Meier(IFunction1D):
             return lammValue
 
         def alpha(m):
-            alphaValue= 0.5 * np.arctan(OmegaD * np.sqrt(J * (J + 1) - m * (m - 1)) / ((1 - 2 * m) * (OmegaD + OmegaQ)))	
+            alphaValue= 0.5 * np.arctan(OmegaD * np.sqrt(J * (J + 1) - m * (m - 1)) / ((1 - 2 * m) * (OmegaD + OmegaQ)))
             return alphaValue
 
         tz = 0
@@ -68,7 +68,7 @@ class Meier(IFunction1D):
             b = np.cos(alpha(mm + 1)) ** 2 * np.cos(alpha(mm)) ** 2 * np.cos((lamp(mm + 1) - lamm(mm)) * x)
             c = np.sin(alpha(mm + 1)) ** 2 * np.sin(alpha(mm)) ** 2 * np.cos((lamm(mm + 1) - lamp(mm)) * x)
             d = np.sin(alpha(mm + 1)) ** 2 * np.cos(alpha(mm)) ** 2 * np.cos((lamm(mm + 1) - lamm(mm)) * x)
-            tx = tx + a + b + c + d 
+            tx = tx + a + b + c + d
         Px = tx / (2 * J + 1)
         return A0 * gau * Lor * (1./3.) * (2 * Px + Pz)
 
