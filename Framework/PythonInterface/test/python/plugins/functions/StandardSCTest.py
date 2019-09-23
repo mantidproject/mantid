2fd991e64f5241380433d8f8d7757d9ed02adba8
Wu <Chien.wu@stfc.ac.uk>
2019-09-23 13:53:31 +0100

    Corrected typo in test file

--- Framework/PythonInterface/test/python/plugins/functions/StandardSCTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/StandardSCTest.py
@@ -23,13 +23,13 @@ class StandardSCTest(unittest.TestCase):
 		input = [0.0, 4.0, 8.0, 12.0]
 		expected = [0.6, -0.22100145265607946, 0.16330356267457757, 0.07366277261260491]
 		tolerance = 1.0e-05
-		status, output = check_output("StandardSC", input, expected, tolerance, A0 = 0.5, FieldSC = 10, FieldBG = 11, Phi = 0.3, Sigma = 0.2, Abg = 0.1)
+		status, output = check_output("StandardSC", input, expected, tolerance, A0 = 0.5, FieldSC = 10, FieldBG = 11, Phi = 0.0, Sigma = 0.2, Abg = 0.1)
 		if not status:
 			msg = 'Computed output {} from input {} unequal to expected: {}'
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'StandardSC', guess = dict(A0 = 0.5, FieldSC = 10, FieldBG = 11, Phi = 0.3, Sigma = 0.2, Abg = 0.1), target = dict(A0 = 0.5, FieldSC = 10, FieldBG = 11, Phi = 0.3, Sigma = 0.2, Abg = 0.1), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'StandardSC', guess = dict(A0 = 0.5, FieldSC = 10, FieldBG = 11, Phi = 0.05, Sigma = 0.2, Abg = 0.1), target = dict(A0 = 0.5, FieldSC = 10, FieldBG = 11, Phi = 0.0, Sigma = 0.2, Abg = 0.1), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
