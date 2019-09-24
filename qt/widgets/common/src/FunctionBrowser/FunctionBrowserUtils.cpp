18bb318334961dbf8ba42cbaab2c6030b99f681a
Matthew Andrew <matthew.andrew@tessella.com>
2019-09-24 13:47:39 +0100

    Updated Function Browser and related files

--- qt/widgets/common/src/FunctionBrowser/FunctionBrowserUtils.cpp
+++ qt/widgets/common/src/FunctionBrowser/FunctionBrowserUtils.cpp
@@ -6,9 +6,13 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #include "MantidQtWidgets/Common/FunctionBrowser/FunctionBrowserUtils.h"
 #include "MantidAPI/CompositeFunction.h"
+#include "MantidAPI/Expression.h"
+#include <boost/lexical_cast.hpp>
 
 namespace MantidQt {
 namespace MantidWidgets {
+
+using namespace Mantid::API;
 
 std::pair<QString, QString> splitParameterName(const QString &paramName) {
   QString functionIndex;
@@ -54,6 +58,67 @@ std::pair<QString, int> splitFunctionPrefix(const QString &prefix) {
   auto parentPrefix = prefix.left(j > 0 ? j + 1 : 0);
   auto funIndex = prefix.mid(j + 2, prefix.size() - j - 3).toInt();
   return std::make_pair(parentPrefix, funIndex);
+}
+
+std::pair<QString, std::pair<QString, QString>>
+splitConstraintString(const QString &constraint) {
+  std::pair<QString, std::pair<QString, QString>> error;
+  if (constraint.isEmpty())
+    return error;
+  QString lowerBoundStr;
+  QString upperBoundStr;
+  QString paramName;
+  Mantid::API::Expression expr;
+  expr.parse(constraint.toStdString());
+  if (expr.name() != "==") {
+    return error;
+  }
+  if (expr.size() == 3) { // lower < param < upper
+    try {
+      // check that the first and third terms are numbers
+      double d1 = boost::lexical_cast<double>(expr[0].str());
+      (void)d1;
+      double d2 = boost::lexical_cast<double>(expr[2].str());
+      (void)d2;
+      if (expr[1].operator_name() == "<" && expr[2].operator_name() == "<") {
+        lowerBoundStr = QString::fromStdString(expr[0].str());
+        upperBoundStr = QString::fromStdString(expr[2].str());
+      } else { // assume that the operators are ">"
+        lowerBoundStr = QString::fromStdString(expr[2].str());
+        upperBoundStr = QString::fromStdString(expr[0].str());
+      }
+      paramName = QString::fromStdString(expr[1].str());
+    } catch (...) { // error in constraint
+      return error;
+    }
+  } else if (expr.size() == 2) { // lower < param or param > lower etc
+    size_t paramPos = 0;
+    try // find position of the parameter name in expression
+    {
+      double d = boost::lexical_cast<double>(expr[1].name());
+      (void)d;
+    } catch (...) {
+      paramPos = 1;
+    }
+    std::string op = expr[1].operator_name();
+    if (paramPos == 0) { // parameter goes first
+      if (op == "<") {   // param < number
+        upperBoundStr = QString::fromStdString(expr[1].str());
+      } else { // param > number
+        lowerBoundStr = QString::fromStdString(expr[1].str());
+      }
+      paramName = QString::fromStdString(expr[0].str());
+    } else {           // parameter is second
+      if (op == "<") { // number < param
+        lowerBoundStr = QString::fromStdString(expr[0].str());
+      } else { // number > param
+        upperBoundStr = QString::fromStdString(expr[0].str());
+      }
+      paramName = QString::fromStdString(expr[1].str());
+    }
+  }
+  return std::make_pair(paramName,
+                        std::make_pair(lowerBoundStr, upperBoundStr));
 }
 
 } // namespace MantidWidgets
