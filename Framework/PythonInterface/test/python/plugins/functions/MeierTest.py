2db86e652b1b15bd0fe64a472d037ce2051f571d
Wu <Chien.wu@stfc.ac.uk>
2019-09-24 14:06:32 +0100

    Fixed error in test.py

--- Framework/PythonInterface/test/python/plugins/functions/MeierTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/MeierTest.py
@@ -23,13 +23,13 @@ class MeierTest(unittest.TestCase):
 		input = [0.0, 4.0, 8.0, 12.0]
 		expected = [0.5, 0.0920992725837422, 0.0023798684614228663, 0.0007490849206591537]
 		tolerance = 1.0e-05
-		status, output = check_output("Meier", input, expected, tolerance, A0 = 0.5, FreqD = 0.01, FreqQ = 0.2, Spin = 3.5, Lambda = 0.1, Sigma = 0.2)
+		status, output = check_output("Meier", input, expected, tolerance, A0 = 0.5, FreqD = 0.01, FreqQ = 0.05, Spin = 3.5, Lambda = 0.1, Sigma = 0.2)
 		if not status:
 			msg = 'Computed output {} from input {} unequal to expected: {}'
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'Meier', guess = dict(A0 = 0.55, FreqD = 0.02, FreqQ = 0.10, Spin = 3.55, Lambda = 0.15, Sigma = 0.25), target = dict(A0 = 0.5, FreqD = 0.01, FreqQ = 0.05, Spin = 3.5, Lambda = 0.1, Sigma = 0.2), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'Meier', guess = dict(A0 = 0.55, FreqD = 0.015, FreqQ = 0.055, Spin = 3.55, Lambda = 0.15, Sigma = 0.25), target = dict(A0 = 0.5, FreqD = 0.01, FreqQ = 0.05, Spin = 3.5, Lambda = 0.1, Sigma = 0.2), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
