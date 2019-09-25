9b4dce037e7392eea10c96698c0879280b5405bf
Harriet Brown <harriet.brown@stfc.ac.uk>
2019-09-25 09:39:01 +0100

    fix error on iterating over formula unit

--- Framework/Algorithms/src/CalculatePlaczekSelfScattering.cpp
+++ Framework/Algorithms/src/CalculatePlaczekSelfScattering.cpp
@@ -29,7 +29,7 @@ getSampleSpeciesInfo(const API::MatrixWorkspace_const_sptr ws) {
   const double xSection = ws->sample().getMaterial().totalScatterXSection();
   const double bSqrdBar = xSection / (4.0 * M_PI);
 
-  for (auto element : formula) {
+  for (const Kernel::Material::Material::FormulaUnit element : formula) {
     const std::map<std::string, double> atomMap{
         {"mass", element.atom->mass},
         {"stoich", element.multiplicity},
