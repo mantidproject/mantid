c3fac5f353dcea723ce184b39c43a674f4dce0c4
hankwustfc <55147936+hankwustfc@users.noreply.github.com>
2019-09-27 14:31:29 +0100

    Update MuHTest.py

--- Framework/PythonInterface/test/python/plugins/functions/MuHTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/MuHTest.py
@@ -23,13 +23,13 @@ class MuHTest(unittest.TestCase):
 		input = [0.0, 4.0, 8.0, 12.0]
 		expected = [0.5, 0.1476150834620071, 0.04187161279609799, 0.011411345712546492]
 		tolerance = 1.0e-05
-		status, output = check_output("MuH", input, expected, tolerance, A0 = 0.5, FreqD = 0.5, Lambda = 0.3, Sigma = 0.05, Phi = 0.0)
+		status, output = check_output("MuH", input, expected, tolerance, A0 = 0.5, NuD = 0.5, Lambda = 0.3, Sigma = 0.05, Phi = 0.0)
 		if not status:
 			msg = 'Computed output {} from input {} unequal to expected: {}'
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'MuH', guess = dict(A0 = 0.55, FreqD = 0.5, Lambda = 0.3, Sigma = 0.05, Phi = 0.0), target = dict(A0 = 0.5, FreqD = 0.5, Lambda = 0.3, Sigma = 0.05, Phi = 0.0), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'MuH', guess = dict(A0 = 0.55, NuD = 0.5, Lambda = 0.3, Sigma = 0.05, Phi = 0.0), target = dict(A0 = 0.5, NuD = 0.5, Lambda = 0.3, Sigma = 0.05, Phi = 0.0), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
