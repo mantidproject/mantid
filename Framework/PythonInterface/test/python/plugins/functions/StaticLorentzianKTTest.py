6c67aff85339ec303ae04ed6f6aec1b75939e86f
Wu <Chien.wu@stfc.ac.uk>
2019-10-01 16:47:25 +0100

    Update test file

--- Framework/PythonInterface/test/python/plugins/functions/StaticLorentzianKTTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/StaticLorentzianKTTest.py
@@ -23,13 +23,13 @@ class StaticLorentzianKTTest(unittest.TestCase):
 		input = [0.0, 4.0, 8.0, 12.0]
 		expected = [0.2, 0.0875285, 0.0997056, 0.100168]
 		tolerance = 1.0e-05
-		status, output = check_output("StaticLorentzianKT", input, expected, tolerance, A0 = 0.2, A = 0.1, B = 0.1)
+		status, output = check_output("StaticLorentzianKT", input, expected, tolerance, A0 = 0.2, A = 0.1, B_L = 0.1)
 		if not status:
 			msg = 'Computed output {} from input {} unequal to expected: {}'
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'StaticLorentzianKT', guess = dict(A0 = 0.2, A = 0.1, B = 0.1), target = dict(A0 = 0.2, A = 0.1, B = 0.1), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'StaticLorentzianKT', guess = dict(A0 = 0.2, A = 0.1, B_L = 0.1), target = dict(A0 = 0.2, A = 0.1, B = 0.1), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
