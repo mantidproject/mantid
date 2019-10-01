64dc5fb1a164821fcafee4e5042a60b7aa7620df
hankwustfc <55147936+hankwustfc@users.noreply.github.com>
2019-10-01 22:51:22 +0100

    Update StaticLorentzianKTTest.py

--- Framework/PythonInterface/test/python/plugins/functions/StaticLorentzianKTTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/StaticLorentzianKTTest.py
@@ -29,7 +29,7 @@ class StaticLorentzianKTTest(unittest.TestCase):
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'StaticLorentzianKT', guess = dict(A0 = 0.2, A = 0.1, B_L = 0.1), target = dict(A0 = 0.2, A = 0.1, B = 0.1), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'StaticLorentzianKT', guess = dict(A0 = 0.2, A = 0.1, B_L = 0.1), target = dict(A0 = 0.2, A = 0.1, B_L = 0.1), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
