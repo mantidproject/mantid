ac174383d22bd1f9163df7c66a8746a15adb21ce
Wu <Chien.wu@stfc.ac.uk>
2019-10-02 14:07:16 +0100

    Fixed errors in .py file

--- Framework/PythonInterface/plugins/functions/SpinGlass.py
+++ Framework/PythonInterface/plugins/functions/SpinGlass.py
@@ -17,7 +17,7 @@ class SpinGlass(IFunction1D):
 
     def init(self):
         self.declareParameter("A0", 0.2, 'Asymmetry')
-        self.declareParameter("Width", 0.1, 'Half-width half maximum of the local field Lorentzian Distribution')
+        self.declareParameter("Width_a", 0.1, 'Half-width half maximum of the local field Lorentzian Distribution')
         self.declareParameter("Nu", 1, 'Rate of Markovian modulation')
         self.declareParameter("Q", 0.1, 'Order Parameter')
         self.addConstraints("Nu > 0")
@@ -25,15 +25,15 @@ class SpinGlass(IFunction1D):
 
     def function1D(self, x):
         A0 = self.getParameterValue("A0")
-        a = self.getParameterValue("Width")
+        a = self.getParameterValue("Width_a")
         nu = self.getParameterValue("Nu")
         q = self.getParameterValue("Q")
         x = x[1 : np.size(x)]
         term1 = 4 * (a ** 2) * (1 - q) * x / nu
         term2 = q * (a ** 2) * (x ** 2)
         term13 = np.exp(- np.sqrt(term1))
-        term23 = (1 - (term2 / np.sqrt(term1 + term2))) * np.exp(- np.sqrt(term1 + term2)
-        SpinGlass = A0 * ((1. / 3.) * term13 +(2. / 3.) * term23)
+        term23 = (1 - (term2 / np.sqrt(term1 + term2))) * np.exp(- np.sqrt(term1 + term2))
+        SpinGlass = A0 * ((1./3.) * term13 + (2./3.) * term23)
         SpinGlass = np.insert(SpinGlass, 0, A0)
         return SpinGlass
 
