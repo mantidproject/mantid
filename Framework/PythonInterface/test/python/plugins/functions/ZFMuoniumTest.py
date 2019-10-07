835279eb05f74cae5069989d7a7d371c03a243d7
Wu <Chien.wu@stfc.ac.uk>
2019-10-07 11:26:16 +0100

    Updated Test file

--- Framework/PythonInterface/test/python/plugins/functions/ZFMuoniumTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/ZFMuoniumTest.py
@@ -23,13 +23,13 @@ class ZFMuoniumTest(unittest.TestCase):
 		input = [0.0, 4.0, 8.0, 12.0]
 		expected = [0.15163664420855247, -0.05429533841911648, -0.021522983685159713, -0.02152298368515976]
 		tolerance = 1.0e-05
-		status, output = check_output("ZFMuonium", input, expected, tolerance, A0 = 0.2, FreqA = 0.3, FreqD = 0.2, Fcut = 1.0,Phi = 0.0)
+		status, output = check_output("ZFMuonium", input, expected, tolerance, A0 = 0.2, FreqA = 0.3, FreqD = 0.2, FCut = 1.0,Phi = 0.0)
 		if not status:
 			msg = 'Computed output {} from input {} unequal to expected: {}'
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'ZFMuonium', guess = dict(A0 = 0.25, FreqA = 0.35, FreqD = 0.25, Fcut = 1.05,Phi = 0.0), target = dict(A0 = 0.2, FreqA = 0.3, FreqD = 0.2, Fcut = 1.0,Phi = 0.0), atol = 0.01)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'ZFMuonium', guess = dict(A0 = 0.25, FreqA = 0.35, FreqD = 0.25, FCut = 1.05,Phi = 0.0), target = dict(A0 = 0.2, FreqA = 0.3, FreqD = 0.2, FCut = 1.0,Phi = 0.0), atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
