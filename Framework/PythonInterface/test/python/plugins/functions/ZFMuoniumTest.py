06782a1af749524684ace4b098205a7e02d7bc62
hankwustfc <55147936+hankwustfc@users.noreply.github.com>
2019-10-08 16:30:25 +0100

    Update ZFMuoniumTest.py

--- Framework/PythonInterface/test/python/plugins/functions/ZFMuoniumTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/ZFMuoniumTest.py
@@ -29,7 +29,9 @@ class ZFMuoniumTest(unittest.TestCase):
 			self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
 	def test_do_fit(self):
-		do_a_fit(np.arange(0.1, 16, 0.2), 'ZFMuonium', guess = dict(A0 = 0.25, FreqA = 0.35, FreqD = 0.25, FCut = 1.05,Phi = 0.0), target = dict(A0 = 0.2, FreqA = 0.3, FreqD = 0.2, FCut = 1.0,Phi = 0.0), atol = 0.01)
+                guess = dict(A0 = 0.25, FreqA = 0.35, FreqD = 0.25, FCut = 1.05,Phi = 0.0)
+		target = dict(A0 = 0.2, FreqA = 0.3, FreqD = 0.2, FCut = 1.0,Phi = 0.0)
+		do_a_fit(np.arange(0.1, 16, 0.2), 'ZFMuonium', guess , target, atol = 0.01)
 
 if __name__ == '__main__':
 	unittest.main()
