13598eec0605d7bc38acea891cb538711125d241
Harriet Brown <harriet.brown@stfc.ac.uk>
2019-10-09 15:55:17 +0100

    Removed placzek correction, this will be covered in a different issue
    Placzek correction will be covered in issue #26986

--- scripts/Diffraction/isis_powder/polaris_routines/polaris_algs.py
+++ scripts/Diffraction/isis_powder/polaris_routines/polaris_algs.py
@@ -82,11 +82,11 @@ def save_unsplined_vanadium(vanadium_ws, output_path):
         mantid.SaveNexus(InputWorkspace=vanadium_ws, Filename=output_path, Append=False)
 
 
-def generate_ts_pdf(run_number, focus_file_path, merge_banks=False, q_lims=None, cal_file_name=None, sample_details=None):
+def generate_ts_pdf(run_number, focus_file_path, merge_banks=False, q_lims=None):
     focused_ws = _obtain_focused_run(run_number, focus_file_path)
 
     if merge_banks:
-        pdf_output = _generate_grouped_ts_pdf(run_number, focused_ws, q_lims, cal_file_name, sample_details)
+        pdf_output = _generate_grouped_ts_pdf(focused_ws, q_lims)
     else:
         focused_ws = mantid.ConvertUnits(InputWorkspace=focused_ws.name(), Target="MomentumTransfer")
         pdf_output = mantid.PDFFourierTransform(Inputworkspace=focused_ws, InputSofQType="S(Q)", PDFType="G(r)",
@@ -123,7 +123,7 @@ def _obtain_focused_run(run_number, focus_file_path):
     return focused_ws
 
 
-def _generate_grouped_ts_pdf(run_number, focused_ws, q_lims, cal_file_name, sample_details):
+def _generate_grouped_ts_pdf(focused_ws, q_lims):
     focused_ws = mantid.ConvertUnits(InputWorkspace=focused_ws, Target="MomentumTransfer", EMode='Elastic')
     min_x = np.inf
     max_x = -np.inf
@@ -136,27 +136,6 @@ def _generate_grouped_ts_pdf(run_number, focused_ws, q_lims, cal_file_name, samp
     binning = [min_x, (max_x-min_x)/num_x, max_x]
     focused_ws = mantid.Rebin(InputWorkspace=focused_ws, Params=binning)
     focused_data_combined = mantid.ConjoinSpectra(InputWorkspaces=focused_ws)
-    mantid.ConvertFromDistribution(Workspace=focused_data_combined)
-    raw_ws = mantid.LoadRaw(Filename='POL'+str(run_number))
-    mantid.SetSample(InputWorkspace=raw_ws,
-                     Geometry=common.generate_sample_geometry(sample_details),
-                     Material=common.generate_sample_material(sample_details))
-
-    monitor = mantid.ExtractSpectra(InputWorkspace=raw_ws, WorkspaceIndexList=[11])
-
-    fit_spectra = mantid.FitIncidentSpectrum(InputWorkspace=monitor, FitSpectrumWith="GaussConvCubicSpline")
-    placzek = mantid.CalculatePlaczekSelfScattering(InputWorkspace=raw_ws, IncidentSpecta=fit_spectra)
-    mantid.ConvertFromDistribution(Workspace=placzek)
-    placzek = mantid.AlignDetectors(InputWorkspace=placzek, CalibrationFile=cal_file_name)
-    placzek = mantid.DiffractionFocussing(InputWorkspace=placzek, GroupingFileName=cal_file_name)
-    placzek = mantid.ConvertUnits(InputWorkspace=placzek, Target="MomentumTransfer", EMode='Elastic')
-    mantid.RebinToWorkspace(WorkspaceToRebin=placzek,
-                            WorkspaceToMatch=focused_data_combined,
-                            OutputWorkspace=placzek)
-    mantid.Subtract(LHSWorkspace=focused_data_combined,
-                    RHSWorkspace=placzek,
-                    OutputWorkspace=focused_data_combined)
-
     mantid.MatchSpectra(InputWorkspace=focused_data_combined,
                         OutputWorkspace=focused_data_combined,
                         ReferenceSpectrum=5)
@@ -193,9 +172,6 @@ def _generate_grouped_ts_pdf(run_number, focused_ws, q_lims, cal_file_name, samp
                                             InputSofQType="S(Q)",
                                             PDFType="G(r)",
                                             Filter=True)
-
-    common.remove_intermediate_workspace(fit_spectra)
-    common.remove_intermediate_workspace(monitor)
     return pdf_output
 
 
