bd0047f05bae9f42841e4b2db84f81fc977fdc76
Owen Arnold <owen.arnold@stfc.ac.uk>
2019-10-10 15:58:22 +0100

    Add documentation for new functions

--- Framework/PythonInterface/core/inc/MantidPythonInterface/core/Copyable.h
+++ Framework/PythonInterface/core/inc/MantidPythonInterface/core/Copyable.h
@@ -18,10 +18,18 @@ namespace PythonInterface {
 // Implementation based on
 // https://mail.python.org/pipermail/cplusplus-sig/2009-May/014505.html
 namespace bp = boost::python;
+
+/**
+ * Create a new object handle around arg. Caller is owner.
+ */
 template <typename T> inline PyObject *managingPyObject(T *p) {
   return typename bp::manage_new_object::apply<T *>::type()(p);
 }
 
+/**
+ * Create a shallow copy of type Copyable providing Copyable is newable and has
+ * a public copy constructor
+ */
 template <typename Copyable>
 boost::python::object generic__copy__(const bp::object &copyable) {
   Copyable *newCopyable(new Copyable(bp::extract<const Copyable &>(copyable)));
@@ -33,6 +41,10 @@ boost::python::object generic__copy__(const bp::object &copyable) {
   return result;
 }
 
+/**
+ * Create a deep copy of type Copyable providing Copyable is newable and has a
+ * public copy constructor
+ */
 template <typename Copyable>
 bp::object generic__deepcopy__(const bp::object &copyable, bp::dict &memo) {
   bp::object copyMod = bp::import("copy");
