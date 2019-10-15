81a91faea2b8ff965dd87a80ac0e320f9b2375ea
Conor Finn <conor.finn@stfc.ac.uk>
2019-10-15 16:45:17 +0100

    RE #27012 Add focus tab framework

---
+++ scripts/Engineering/gui/engineering_diffraction/tabs/focus/model.py
@@ -0,0 +1,13 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+from __future__ import (absolute_import, division, print_function)
+
+
+class FocusModel(object):
+    def focus_run(self, run_number, banks, plot_output, instrument, rb_number):
+        pass
