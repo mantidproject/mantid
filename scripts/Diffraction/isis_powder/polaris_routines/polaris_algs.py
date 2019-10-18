3b111435e9413caafa0b86e7c8d9770202d14002
Harriet Brown <harriet.brown@stfc.ac.uk>
2019-10-18 09:15:13 +0100

    Change PDFFourierTransform in _generate_grouped_ts_pdf to take S-1

--- scripts/Diffraction/isis_powder/polaris_routines/polaris_algs.py
+++ scripts/Diffraction/isis_powder/polaris_routines/polaris_algs.py
@@ -198,9 +198,9 @@ def _generate_grouped_ts_pdf(focused_ws, q_lims):
         q_min[i] = pdf_x_array[tmp2]
         q_max[i] = pdf_x_array[np.amax(np.where(pdf_x_array <= q_max[i]))]
         bin_width = min(pdf_x_array[1] - pdf_x_array[0], bin_width)
-    #mantid.MatchSpectra(InputWorkspace=focused_data_combined,
-    #                    OutputWorkspace=focused_data_combined,
-    #                    ReferenceSpectrum=1)
+    mantid.MatchSpectra(InputWorkspace=focused_data_combined,
+                        OutputWorkspace=focused_data_combined,
+                        ReferenceSpectrum=5)
     focused_data_combined = mantid.CropWorkspaceRagged(InputWorkspace=focused_data_combined, XMin=q_min, XMax=q_max)
     focused_data_combined = mantid.Rebin(InputWorkspace=focused_data_combined,
                                          Params=[min(q_min), bin_width, max(q_max)])
@@ -208,7 +208,7 @@ def _generate_grouped_ts_pdf(focused_ws, q_lims):
                                               WeightedSum=True,
                                               MultiplyBySpectra=False)
     pdf_output = mantid.PDFFourierTransform(Inputworkspace=focused_data_combined,
-                                            InputSofQType="S(Q)",
+                                            InputSofQType="S(Q)-1",
                                             PDFType="G(r)",
                                             Filter=True)
     common.remove_intermediate_workspace('cal_workspace_group')
