e22baa1200380ddb274210dfe89008425c55d9ef
Giovanni Di Siena <giodisiena@gmail.com>
2019-10-24 15:38:43 +0100

    Create mock dialog
    Re #26533

---
+++ qt/widgets/common/inc/MantidQtWidgets/Common/MockOptionsDialog.h
@@ -0,0 +1,25 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#ifndef MANTID_MANTIDWIDGETS_MOCKOPTIONSDIALOG_H
+#define MANTID_MANTIDWIDGETS_MOCKOPTIONSDIALOG_H
+
+#include "IOptionsDialog.h"
+#include <gmock/gmock.h>
+#include <map>
+#include <QVariant>
+
+class MockOptionsDialog : public MantidQt::MantidWidgets::IOptionsDialog {
+public:
+  GNU_DIAG_OFF_SUGGEST_OVERRIDE
+  MOCK_METHOD1(getOptions, void(std::map<QString, QVariant> &options));
+  MOCK_METHOD1(setOptions, void(std::map<QString, QVariant> &options));
+  MOCK_METHOD1(subscribe, void(MantidQt::MantidWidgets::OptionsDialogSubscriber *notifyee));
+  MOCK_METHOD0(show, void());
+  GNU_DIAG_ON_SUGGEST_OVERRIDE
+};
+
+#endif /* MANTID_MANTIDWIDGETS_MOCKOPTIONSDIALOG_H */
