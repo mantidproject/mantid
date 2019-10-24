4bbbed735f9d63e87b5594c60a96ff679df0d0d2
Anthony Lim <anthony.lim@stfc.ac.uk>
2019-10-24 16:49:23 +0100

    refs #26774 clang for fixed typos in Muon GUI's

--- qt/scientific_interfaces/Muon/MuonAnalysis.cpp
+++ qt/scientific_interfaces/Muon/MuonAnalysis.cpp
@@ -147,7 +147,7 @@ MuonAnalysis::MuonAnalysis(QWidget *parent)
                                               // instruments later
                    {"MUSR", "HIFI", "EMU", "ARGUS", "CHRONUS"}),
       m_deadTimeIndex(-1), m_useDeadTime(true) {
-      g_log.warning(
+  g_log.warning(
       "This interface is deprecated, please use Muon Analysis instead");
 }
 
