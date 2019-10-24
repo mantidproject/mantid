92b89f424c510b009083b0c31cef34c5108814ac
Anthony Lim <anthony.lim@stfc.ac.uk>
2019-10-24 09:50:18 +0100

    refs #26774 updated Muon GUI's to usage report and deprecation of old

--- scripts/Muon_Analysis.py
+++ scripts/Muon_Analysis.py
@@ -8,9 +8,10 @@
 from __future__ import (absolute_import, division, print_function)
 from Muon.GUI.MuonAnalysis.muon_analysis_2 import MuonAnalysisGui
 from qtpy import QtCore
+from Muon.GUI.Common.usage_report import register_GUI
 
 
-Name = "Muon_Analysis_2"
+Name = "Muon_Analysis"
 
 if 'muon_analysis' in globals():
     muon_analysis = globals()['muon_analysis']
@@ -21,9 +22,11 @@ if 'muon_analysis' in globals():
         muon_analysis.activateWindow()
     else:
         muon_analysis = MuonAnalysisGui()
+        register_GUI(Name)
         muon_analysis.resize(700, 700)
         muon_analysis.show()
 else:
     muon_analysis = MuonAnalysisGui()
+    register_GUI(Name)
     muon_analysis.resize(700, 700)
     muon_analysis.show()
