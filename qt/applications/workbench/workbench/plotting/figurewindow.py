b0aa63fb28a4484084e98434a432498a25310395
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-10-25 11:47:08 +0100

    Add some more detailed desgin choice comments

--- qt/applications/workbench/workbench/plotting/figurewindow.py
+++ qt/applications/workbench/workbench/plotting/figurewindow.py
@@ -47,6 +47,16 @@ class FigureWindow(QMainWindow, ObservingView):
         # the main window gets focus. This does cause a slight flicker effect
         # as the window is hidden and quickly brought back to the front. Using
         # the parent-child method at least avoids this flicker on Windows.
+
+        # Using the Qt.WindowStaysOnTopFlag was tried, however this caused the
+        # window to stay on top of all other windows, including external
+        # applications. This flag could be toggled off when the application
+        # was inactive, however a QWindow needs to be re-drawn when it is given
+        # new flags, which again, causes a flicker.
+
+        # Using the Qt.Tool flag, and setting the main window as this window's
+        # parent, keeps this window on top. However it does not allow the
+        # window to be minimized.
         if platform.system() == "Windows":
             from workbench.utils.windowfinder import get_main_window_widget
             self.setParent(get_main_window_widget(), Qt.Window)
@@ -138,5 +148,18 @@ class FigureWindow(QMainWindow, ObservingView):
         The figure window should always remain on top of the main
         Workbench window.
         """
+        # We hook into focusWindowChanged instead of focusChanged here.
+        # focusChanged returns the widget that now has focus. We can determine
+        # if this widget is a child of the main window but, we do not want to
+        # raise this window above all children of the main window,e.g. matrix
+        # workspace display. We only want to raise the window above the main
+        # window's window.
+
+        # The window object returned here is a QtGui.QWindow https://doc.qt.io/qt-5/qwindow.html
+        # which only has access to the widget whose focus is being changed to,
+        # e.g. MessageDisplay, but for reasons given above we cannot use this.
+        # The object name of the window appears to be the object name of the
+        # window's central widget with "Window" appended to it; so we check for
+        # this.
         if window and MAIN_WINDOW_OBJECT_NAME + 'Window' == window.objectName():
             self.raise_()
