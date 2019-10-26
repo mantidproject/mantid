00a8b51c6fc3b00ea0d410e2e0cab7babe3416b9
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2019-10-26 08:49:32 +0100

    Use Enum to define save format
    Refs #26839

--- Framework/PythonInterface/plugins/algorithms/SaveReflections.py
+++ Framework/PythonInterface/plugins/algorithms/SaveReflections.py
@@ -8,32 +8,30 @@ import re
 import numpy as np
 from mantid.api import AlgorithmFactory, FileProperty, FileAction, PythonAlgorithm, ITableWorkspaceProperty, IPeaksWorkspace
 from mantid.kernel import StringListValidator, Direction
+from mantid.py3compat.enum import Enum
 from mantid.simpleapi import SaveHKL
-
-# List of file format names supported by this algorithm
-SUPPORTED_FORMATS = ["Fullprof", "GSAS", "Jana", "SHELX"]
 
 
 def has_modulated_indexing(workspace):
-    """Check if this workspace has more than 3 indicies
+    """Check if this workspace has more than 3 indices
 
     :params: workspace :: the workspace to check
-    :returns: True if the workspace > 3 indicies else False
+    :returns: True if the workspace > 3 indices else False
     """
-    return num_additional_indicies(workspace) > 0
+    return num_additional_indices(workspace) > 0
 
 
-def num_additional_indicies(workspace):
-    """Check if this workspace has more than 3 indicies
+def num_additional_indices(workspace):
+    """Check if this workspace has more than 3 indices
 
-    :params: workspace :: the workspace count indicies in
-    :returns: the number of additional indicies present in the workspace
+    :params: workspace :: the workspace count indices in
+    :returns: the number of additional indices present in the workspace
     """
     return len(get_additional_index_names(workspace))
 
 
 def get_additional_index_names(workspace):
-    """Get the names of the additional indicies to export
+    """Get the names of the additional indices to export
 
     :params: workspace :: the workspace to get column names from
     :returns: the names of any additional columns in the workspace
@@ -65,39 +63,18 @@ class SaveReflections(PythonAlgorithm):
         self.declareProperty(name="Format",
                              direction=Direction.Input,
                              defaultValue="Fullprof",
-                             validator=StringListValidator(SUPPORTED_FORMATS),
+                             validator=StringListValidator(dir(ReflectionFormat)),
                              doc="The output format to export reflections to")
 
     def PyExec(self):
         """Execute the algorithm"""
         workspace = self.getProperty("InputWorkspace").value
-        output_format = self.getPropertyValue("Format")
+        output_format = ReflectionFormat(self.getPropertyValue("Format"))
         file_name = self.getPropertyValue("Filename")
 
-        file_writer  = self.choose_format(output_format)
-        file_writer(file_name, workspace)
-
-    def choose_format(self, output_format):
-        """Choose the function to use to write out data for this format
-
-        :param output_format: the format to use to output refelctions as. Options are
-            "Fullprof", "GSAS", "Jana", and "SHELX".
-
-        :returns: file format to use for saving reflections to an ASCII file.
-        """
-
-        if output_format == "Fullprof":
-            return FullprofFormat()
-        elif output_format == "Jana":
-            return JanaFormat()
-        elif output_format == "GSAS" or output_format == "SHELX":
-            return SaveHKLFormat()
-        else:
-            raise ValueError("Unexpected file format {}. Format should be one of {}."
-                             .format(output_format, ", ".join(SUPPORTED_FORMATS)))
+        FORMAT_MAP[output_format]()(file_name, workspace)
 
 # ------------------------------------------------------------------------------------------------------
-
 
 class FullprofFormat(object):
     """Writes a PeaksWorkspace to an ASCII file in the format required
@@ -123,7 +100,7 @@ class FullprofFormat(object):
         :param f_handle: handle to the file to write to.
         :param workspace: the PeaksWorkspace to save to file.
         """
-        num_hkl = 3+num_additional_indicies(workspace)
+        num_hkl = 3+num_additional_indices(workspace)
         f_handle.write(workspace.getTitle())
         f_handle.write("({}i4,2f12.2,i5,4f10.4)\n".format(num_hkl))
         f_handle.write("  0 0 0\n")
@@ -198,7 +175,7 @@ class JanaFormat(object):
         column_names.extend(["Fsqr", "s(Fsqr)", "Cod", "Lambda", "Twotheta", "Transm.", "Tbar", "TDS"])
 
         column_format = "#{:>4}{:>4}{:>4}"
-        column_format += "".join(["{:>4}" for _ in range(num_additional_indicies(workspace))])
+        column_format += "".join(["{:>4}" for _ in range(num_additional_indices(workspace))])
         column_format += "{:>12}{:>12}{:>5}{:>10}{:>10}{:>10}{:>10}{:>10}\n"
 
         f_handle.write(column_format.format(*column_names))
@@ -254,6 +231,21 @@ class SaveHKLFormat(object):
         if has_modulated_indexing(workspace):
             raise NotImplementedError("Cannot currently save modulated structures to GSAS or SHELX formats")
         SaveHKL(Filename=file_name, InputWorkspace=workspace, OutputWorkspace=workspace.name())
+
+
+class ReflectionFormat(Enum):
+    Fullprof = 1
+    GSAS = 2
+    Jana = 3
+    SHELX = 4
+
+
+FORMAT_MAP = {
+    ReflectionFormat.Fullprof: FullprofFormat,
+    ReflectionFormat.GSAS: SaveHKLFormat,
+    ReflectionFormat.Jana: JanaFormat,
+    ReflectionFormat.SHELX: SaveHKLFormat
+}
 
 
 AlgorithmFactory.subscribe(SaveReflections)
