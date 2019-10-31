d16b0b0919e03f916c634d0ce962809a16e7f535
Adam J. Jackson <a.j.jackson@physics.org>
2019-10-31 17:01:25 +0000

    Abins: Some more broadening implementations
    A considerable speedup is obtained by limiting the frequency range of the
    Gaussian functions. Performance is comparable to the legacy
    implementation - if a similar number of samples is used! 3*max(sigma)
    corresponds to about 150 points, while the default number of samples
    in the legacy implementation is just 50 points spread over the same
    distance. If these numbers are brought into line, the performance is
    quite similar. Interestingly, the for-loop summation and np.histogram
    summation change their ranking for speed depending on the number of
    samples. We should keep both approaches while developing this method,
    and see which is fastest for production calculations.
    
    The names aren't great and there's a lot of redundancy - this should
    be cleaned up once the best approaches are established. There isn't
    much reason to keep the legacy mode around either, as it gives
    objectively wrong results.

--- scripts/AbinsModules/Instruments/Instrument.py
+++ scripts/AbinsModules/Instruments/Instrument.py
@@ -35,8 +35,8 @@ class Instrument(object):
        """
         return None
 
-    @staticmethod
-    def _broaden_spectrum(frequencies=None, bins=None, s_dft=None, sigma=None, scheme='legacy_plus', prebin='auto'):
+    @classmethod
+    def _broaden_spectrum(cls, frequencies=None, bins=None, s_dft=None, sigma=None, scheme='legacy_plus', prebin='auto'):
         """Convert frequency/S data to broadened spectrum on a regular grid
 
         Several algorithms are implemented, for purposes of
@@ -68,10 +68,11 @@ class Instrument(object):
         if (bins is None) or (s_dft is None) or (sigma is None):
             raise ValueError("Frequency bins, S data and broadening width must be provided.")
 
-        if scheme not in ('legacy_plus'):
+        if scheme not in ('legacy_plus', 'gaussian', 'gaussian_trunc', 'gaussian_trunc_forloop'):
             raise ValueError('Broadening scheme "{}" not supported for this instrument, please correct '
                              'AbinsParameters.sampling["broadening_scheme"]'.format(scheme))
 
+        bins = np.asarray(bins)
         freq_points = (bins[1:] + bins[:-1]) / 2
 
         #  Don't bother broadening if there is nothing here to broaden: return empty spectrum
@@ -112,10 +113,98 @@ class Instrument(object):
             hist, bin_edges = np.histogram(flattened_freq, bins, weights=flattened_spectrum, density=False)
             return freq_points, hist
 
+        elif scheme == 'gaussian':
+            #  Gaussian broadening on the full grid (no range cutoff, sum over all peaks)
+            kernels = cls._gaussian(sigma=sigma,
+                                points=freq_points.reshape(len(freq_points), 1),
+                                center=frequencies)
+            spectrum = np.dot(kernels, s_dft)
+            return freq_points, spectrum
+        elif scheme == 'gaussian_trunc_slow':
+            # Gaussian broadening on the full grid (sum over all peaks, Gaussian range limited to 3 sigma)
+            kernels = cls._gaussian_trunc(sigma=sigma,
+                                          points=freq_points,
+                                          center=frequencies)
+            spectrum = np.dot(kernels.transpose(), s_dft)
+            return freq_points, spectrum
+        elif scheme == 'gaussian_trunc_forloop':
+            # Gaussian broadening on the full grid (sum over all peaks, Gaussian range limited to 3 sigma)
+            # All peaks use the same number of points; if the center is located close to the low- or high-freq limit,
+            # the frequency point set is "justified" to lie inside the range
+
+            bin_width = bins[1] - bins[0]
+            freq_range = 3 * max(sigma)
+            nrows = len(frequencies)
+            ncols = int(np.ceil((freq_range * 2) / bin_width))
+
+            start_indices = np.asarray((frequencies - freq_points[0] - freq_range + (bin_width / 2)) // bin_width,
+                                        int)
+
+            start_freqs = freq_points[start_indices]
+
+            left_justified = frequencies < freq_range
+            start_freqs[left_justified] = freq_points[0]
+            start_indices[left_justified] = 0
+
+            right_justified = frequencies > (freq_points[-1] - freq_range)
+            start_freqs[right_justified] = freq_points[-1] - (ncols * bin_width)
+            start_indices[right_justified] = len(freq_points) - ncols
+
+            freq_matrix = start_freqs.reshape(nrows, 1) + np.arange(0, 2 * freq_range, bin_width)
+
+            kernels = cls._gaussian(sigma=sigma.reshape(nrows, 1),
+                                    points=freq_matrix,
+                                    center=frequencies.reshape(nrows, 1))
+
+            spectrum = np.zeros_like(freq_points)
+            for start, kernel, s in zip(start_indices, kernels, s_dft):
+                scaled_kernel = kernel * s
+                spectrum[start:start+ncols] += scaled_kernel
+
+            return freq_points, spectrum
+
+        elif scheme == 'gaussian_trunc':
+            # Gaussian broadening on the full grid (sum over all peaks, Gaussian range limited to 3 sigma)
+            # All peaks use the same number of points; if the center is located close to the low- or high-freq limit,
+            # the frequency point set is "justified" to lie inside the range
+
+            bin_width = bins[1] - bins[0]
+            freq_range = 3 * max(sigma)
+            nrows = len(frequencies)
+            ncols = int(np.ceil((freq_range * 2) / bin_width))
+
+            start_indices = np.asarray((frequencies - freq_points[0] - freq_range + (bin_width / 2)) // bin_width,
+                                        int)
+
+            start_freqs = freq_points[start_indices]
+
+            left_justified = frequencies < freq_range
+            start_freqs[left_justified] = freq_points[0]
+            start_indices[left_justified] = 0
+
+            right_justified = frequencies > (freq_points[-1] - freq_range)
+            start_freqs[right_justified] = freq_points[-1] - (ncols * bin_width)
+            start_indices[right_justified] = len(freq_points) - ncols
+
+            freq_matrix = start_freqs.reshape(nrows, 1) + np.arange(0, 2 * freq_range, bin_width)
+
+            kernels = cls._gaussian(sigma=sigma.reshape(nrows, 1),
+                                    points=freq_matrix,
+                                    center=frequencies.reshape(nrows, 1))
+
+            # flattened_spectrum = np.ravel(s_dft.reshape(nrows, 1) * kernels)
+            # flattened_freq = np.ravel(freq_matrix)
+
+            hist, bin_edges = np.histogram(np.ravel(freq_matrix),
+                                           bins,
+                                           weights=np.ravel(s_dft.reshape(nrows, 1) * kernels),
+                                           density=False)
+            return freq_points, hist
+
     @staticmethod
     def _convolve_with_resolution_function_legacy(frequencies=None, s_dft=None, sigma=None, pkt_per_peak=None,
                                                   gaussian=None):
-        """Legacy implementation of gaussian convolution. 
+        """Legacy implementation of gaussian convolution.
 
         This method uses a kernel that is not commensurate with the
         sampling bins and is not recommended for production
@@ -129,8 +218,8 @@ class Instrument(object):
         :type sigma: float
         :param pkt_per_peak: number of points per peak
         :type pkt_per_peak: int
-        :param gaussian: 
-            Gaussian-like function used to broaden peaks; this should take keyword arguments *sigma*, *points*, *center* and return an array of 
+        :param gaussian:
+            Gaussian-like function used to broaden peaks; this should take keyword arguments *sigma*, *points*, *center* and return an array of
             broadening kernels with widths *sigma* evaluated at *points*
         :type gaussian: function
         :returns: frequencies for which peaks have been broadened, corresponding S
@@ -165,7 +254,7 @@ class Instrument(object):
 
         # gaussian_val[freq_num, pkt_per_peak]
         dirac_val = gaussian(sigma=sigma_matrix, points=broad_freq, center=frequencies_matrix)
-        
+
         # s_dft_matrix[freq_num, pkt_per_peak]
         s_dft_matrix = np.array([s_dft, ] * pkt_per_peak).transpose()
 
@@ -190,6 +279,34 @@ class Instrument(object):
         norm = (AbinsModules.AbinsParameters.sampling['pkt_per_peak']
                 / (AbinsModules.AbinsParameters.instruments['fwhm'] * two_sigma))
         return np.exp(-(points - center) ** 2 / sigma_factor) / (np.sqrt(sigma_factor * np.pi) * norm)
+
+    @staticmethod
+    def _gaussian_trunc(sigma=None, points=None, center=None, limit=3):
+        """
+        :param sigma: sigma defines width of Gaussian
+        :param points: points for which Gaussian should be evaluated
+        :param center: center of Gaussian
+        :returns: numpy array with calculated Gaussian values
+        """
+        points_matrix = points * np.ones((len(center), 1))
+        center_matrix = center.reshape(len(center), 1) * np.ones(len(points))
+        sigma_matrix = sigma.reshape(len(sigma), 1) * np.ones(len(points))
+
+        distances = abs(points_matrix - center_matrix)
+        samples_close_to_peaks = distances < (limit * sigma_matrix)
+
+        results = np.zeros((len(center), len(points)))
+
+        two_sigma = 2.0 * sigma_matrix[samples_close_to_peaks]
+        sigma_factor = two_sigma * sigma_matrix[samples_close_to_peaks]
+        # norm = (AbinsModules.AbinsParameters.sampling['pkt_per_peak']
+        #         / (AbinsModules.AbinsParameters.instruments['fwhm'] * two_sigma))
+        # np.exp(-(points - center) ** 2 / sigma_factor) / (np.sqrt(sigma_factor * np.pi) * norm)
+
+        results[samples_close_to_peaks] = np.exp(-(points_matrix[samples_close_to_peaks] - center_matrix[samples_close_to_peaks]) ** 2 / sigma_factor) / (np.sqrt(sigma_factor * np.pi))
+
+        return results
+
 
     def __str__(self):
         return self._name
