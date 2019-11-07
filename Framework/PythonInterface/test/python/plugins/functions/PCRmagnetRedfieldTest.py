81a4ae2fa634a0adf66749aaa45ce49d59820324
Stephen <stephen.smith@stfc.ac.uk>
2019-11-07 08:47:42 +0000

    Correcting failing tests

--- Framework/PythonInterface/test/python/plugins/functions/PCRmagnetRedfieldTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/PCRmagnetRedfieldTest.py
@@ -15,7 +15,7 @@ from MsdTestHelper import (is_registered, check_output, do_a_fit)
 class PCRmagnetRedfieldTest(unittest.TestCase):
 
     def test_function_has_been_registered(self):
-        status, msg = is_registered("PCRmagnetRedfield")
+        status, msg = is_registered("PCRmagRedfield")
         if not status:
             self.fail(msg)
 
@@ -23,16 +23,17 @@ class PCRmagnetRedfieldTest(unittest.TestCase):
         input = [0.0, 4.0, 8.0, 12.0]
         expected = [0.2, 0.07812243345468936, 0.05832788683141682, 0.06434817483220028]
         tolerance = 1.0e-05
-        status, output = check_output("PCRmagnetRedfield", input, expected, tolerance, A0 = 0.2, Delta = 0.2, Nu = 0.1)
+        status, output = check_output("PCRmagRedfield", input, expected, tolerance, A0=0.2, Delta=0.2, Nu=0.1)
 
         if not status:
             msg = 'Computed output {} from input {} unequal to expected: {}'
             self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
     def test_do_fit(self):
-        guess = dict(A0 = 0.25, Delta = 0.25, Nu = 0.15)
-        target = dict(A0 = 0.2, Delta = 0.2, Nu = 0.1)
-        do_a_fit(np.arange(0.1, 16, 0.2), 'PCRmagnetRedfield', guess, target, atol = 0.01)
+        guess = dict(A0=0.25, Delta=0.25, Nu=0.15)
+        target = dict(A0=0.2, Delta=0.2, Nu=0.1)
+        do_a_fit(np.arange(0.1, 16, 0.2), 'PCRmagRedfield', guess, target, atol=0.01)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- Framework/PythonInterface/test/python/plugins/functions/ZFprotonDipoleTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/ZFprotonDipoleTest.py
@@ -23,16 +23,17 @@ class ZFprotonDipoleTest(unittest.TestCase):
         input = [0.0, 4.0, 8.0, 12.0]
         expected = [0.2, 0.05659330906895196, 0.021173137737592145, 0.033106713658847285]
         tolerance = 1.0e-05
-        status, output = check_output("ZFprotonDipole", input, expected, tolerance, A0 = 0.2, Radius = 2, LambdaTrans = 0.2)
+        status, output = check_output("ZFprotonDipole", input, expected, tolerance, A0=0.2, Radius=2, LambdaTrans=0.2)
 
         if not status:
             msg = 'Computed output {} from input {} unequal to expected: {}'
             self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
     def test_do_fit(self):
-        guess = dict(A0 = 0.25, Radius = 2.5, LambdaTrans = 0.25)
-        target = dict(A0 = 0.2, Radius = 2, LambdaTrans = 0.2)
-        do_a_fit(np.arange(0.1, 16, 0.2), 'ZFprotonDipole', guess, target, atol = 0.01)
+        guess = dict(A0=0.25, Radius=2.5, LambdaTrans=0.25)
+        target = dict(A0=0.2, Radius=2, LambdaTrans=0.2)
+        do_a_fit(np.arange(0.1, 16, 0.2), 'ZFprotonDipole', guess, target, atol=0.01)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
