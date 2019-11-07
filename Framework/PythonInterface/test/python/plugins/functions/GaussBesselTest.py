c066eb01aa2f8ddd0315fd9f9acf04787f7e26f2
Stephen <stephen.smith@stfc.ac.uk>
2019-11-07 17:14:11 +0000

    FIxing formatting of tests and ZFProtonDIpole
    The formula used in ZFProtonDipole was incorrect, which explained the failing tests.

--- Framework/PythonInterface/test/python/plugins/functions/GaussBesselTest.py
+++ Framework/PythonInterface/test/python/plugins/functions/GaussBesselTest.py
@@ -23,16 +23,17 @@ class GaussBesselTest(unittest.TestCase):
         input = [0.0, 4.0, 8.0, 12.0]
         expected = [0.2, 0.08191651218438567, 0.07081749792685224, 0.06735210249013478]
         tolerance = 1.0e-05
-        status, output = check_output("GaussBessel", input, expected, tolerance, A0 = 0.2, Freq = 0.5, Sigma = 0.2, Phi = 0.0)
+        status, output = check_output("GaussBessel", input, expected, tolerance, A0=0.2, Freq=0.5, Sigma=0.2, Phi=0.0)
 
         if not status:
             msg = 'Computed output {} from input {} unequal to expected: {}'
             self.fail(msg.format(*[str(i) for i in (output, input, expected)]))
 
     def test_do_fit(self):
-        guess = dict(A0 = 0.25, Freq = 0.55, Sigma = 0.25, Phi = 0.0)
-        target = dict(A0 = 0.2, Freq = 0.5, Sigma = 0.2, Phi = 0.0)
-        do_a_fit(np.arange(0.1, 16, 0.2), 'GaussBessel', guess, target, atol = 0.01)
+        guess = dict(A0=0.25, Freq=0.55, Sigma=0.25, Phi=0.0)
+        target = dict(A0=0.2, Freq=0.5, Sigma=0.2, Phi=0.0)
+        do_a_fit(np.arange(0.1, 16, 0.2), 'GaussBessel', guess, target, atol=0.01)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
