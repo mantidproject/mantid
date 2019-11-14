d6fdf22d8a7b5d3ba7d63917375f2349dffeafa8
Harriet Brown <harriet.brown@stfc.ac.uk>
2019-11-14 11:56:35 +0000

    Move context menu for errors bars to figureinteraction
    This commit moves the function that generates the context menu from the
    FigureErrorsManager class to the FigureInteraction class. This is because the
    FigureInteraction class deals with all the other context menus for the figure plot
    window, while the FigureErrorsManager handles the plotting of error bars.
    
    Re #26330

--- qt/applications/workbench/workbench/plotting/figureerrorsmanager.py
+++ qt/applications/workbench/workbench/plotting/figureerrorsmanager.py
@@ -29,62 +29,7 @@ class FigureErrorsManager(object):
         self.canvas = canvas
         self.active_lines = []
 
-    def add_error_bars_menu(self, parent_menu, ax):
-        """
-        Add menu actions to toggle the errors for all lines in the plot.
-
-        Lines without errors are added in the context menu first,
-        then lines containing errors are appended afterwards.
-
-        This is done so that the context menu always has
-        the same order of curves as the legend is currently showing - and the
-        legend always appends curves with errors after the lines without errors.
-        Relevant source, as of 10 July 2019:
-        https://github.com/matplotlib/matplotlib/blob/154922992722db37a9d9c8680682ccc4acf37f8c/lib/matplotlib/legend.py#L1201
-
-        :param parent_menu: The menu to which the actions will be added
-        :type parent_menu: QMenu
-        :param ax: The Axes containing lines to toggle errors on
-        """
-        # if the ax is not a MantidAxes, and there are no errors plotted,
-        # then do not add any options for the menu
-        if not isinstance(ax, MantidAxes) and len(ax.containers) == 0:
-            return
-
-        error_bars_menu = QMenu(self.ERROR_BARS_MENU_TEXT, parent_menu)
-        error_bars_menu.addAction(self.SHOW_ERROR_BARS_BUTTON_TEXT,
-                                  partial(self._update_plot_after, self._toggle_all_errors, ax, make_visible=True))
-        error_bars_menu.addAction(self.HIDE_ERROR_BARS_BUTTON_TEXT,
-                                  partial(self._update_plot_after, self._toggle_all_errors, ax, make_visible=False))
-        parent_menu.addMenu(error_bars_menu)
-
-        self.active_lines = CurvesTabWidgetPresenter.get_curves_from_ax(ax)
-
-        # if there's more than one line plotted, then
-        # add a sub menu, containing an action to hide the
-        # error bar for each line
-        error_bars_menu.addSeparator()
-        add_later = []
-        for index, line in enumerate(self.active_lines):
-            if curve_has_errors(line):
-                curve_props = CurveProperties.from_curve(line)
-                # Add lines without errors first, lines with errors are appended later. Read docstring for more info
-                if not isinstance(line, ErrorbarContainer):
-                    action = error_bars_menu.addAction(line.get_label(), partial(
-                        self._update_plot_after, self.toggle_error_bars_for, ax, line))
-                    action.setCheckable(True)
-                    action.setChecked(not curve_props.hide_errors)
-                else:
-                    add_later.append((line.get_label(), partial(
-                        self._update_plot_after, self.toggle_error_bars_for, ax, line),
-                                      not curve_props.hide_errors))
-
-        for label, function, visible in add_later:
-            action = error_bars_menu.addAction(label, function)
-            action.setCheckable(True)
-            action.setChecked(visible)
-
-    def _toggle_all_errors(self, ax, make_visible):
+    def toggle_all_errors(self, ax, make_visible):
         for line in self.active_lines:
             if curve_has_errors(line):
                 self.toggle_error_bars_for(ax, line, make_visible)
@@ -112,7 +57,7 @@ class FigureErrorsManager(object):
         CurvesTabWidgetPresenter.toggle_errors(new_curve, curve_props)
         CurvesTabWidgetPresenter.update_limits_and_legend(ax, legend_props)
 
-    def _update_plot_after(self, func, *args, **kwargs):
+    def update_plot_after(self, func, *args, **kwargs):
         """
         Updates the legend and the plot after the function has been executed.
         Used to funnel through the updates through a common place
@@ -127,3 +72,11 @@ class FigureErrorsManager(object):
     @staticmethod
     def _supported_ax(ax):
         return hasattr(ax, 'creation_args')
+
+    @staticmethod
+    def get_errorbars_from_ax(ax):
+        return [cont for cont in ax.containers if isinstance(cont, ErrorbarContainer)]
+
+    @staticmethod
+    def get_curves_from_ax(ax):
+        return ax.get_lines() + CurvesTabWidgetPresenter.get_errorbars_from_ax(ax)
