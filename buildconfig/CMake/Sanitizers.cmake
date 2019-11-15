4122ea159a78b0c7e08bbae71a0f07a793ad070a
David Fairbrother <DavidFair@users.noreply.github.com>
2019-11-15 13:45:22 +0000

    Update build script to include sanitizers and leak suppressions
    Adds a step to the build scripts for jenkins to run sanitized builds
    with the correct environment variables. Also adds leak suppressions for
    upstream libraries too

--- buildconfig/CMake/Sanitizers.cmake
+++ buildconfig/CMake/Sanitizers.cmake
@@ -4,9 +4,11 @@
 
 set(USE_SANITIZER "Off" CACHE STRING "Sanitizer mode to enable")
 set_property(CACHE USE_SANITIZER PROPERTY STRINGS
-             Off Address Address+Leak Leak Memory Thread Undefined)
+             Off Address Memory Thread Undefined)
 
-if(NOT ${USE_SANITIZER} MATCHES "Off")
+string(TOLOWER "${USE_SANITIZER}" USE_SANITIZERS_LOWER)
+
+if(NOT ${USE_SANITIZERS_LOWER} MATCHES "off")
     if(${CMAKE_VERSION} VERSION_LESS "3.13.0")
         # Version 13 is needed for add_link_options, but not all platforms
         # currently have it
@@ -19,7 +21,7 @@ if(NOT ${USE_SANITIZER} MATCHES "Off")
 
     # Check and warn if we are not in a mode without debug symbols
     string(TOLOWER "${CMAKE_BUILD_TYPE}" build_type_lower)
-    if(${build_type_lower} MATCHES "release" OR ${build_type_lower} MATCHES "minsizerel" )
+    if("${build_type_lower}" MATCHES "release" OR "${build_type_lower}" MATCHES "minsizerel" )
         message(WARNING "You are running address sanitizers without debug information, try RelWithDebInfo")
 
     elseif(${build_type_lower} MATCHES "relwithdebinfo")
@@ -39,37 +41,25 @@ if(NOT ${USE_SANITIZER} MATCHES "Off")
 
     # N.b. we can switch this to add_link_options in CMake 3.13
     # rather than have to check the linker options etc
-    if (USE_SANITIZER STREQUAL "Address")
+    if (USE_SANITIZERS_LOWER STREQUAL "address")
         message(STATUS "Enabling address sanitizer")
 
         add_compile_options(-fsanitize=address)
         add_link_options(-fsanitize=address)
 
-    elseif (USE_SANITIZER STREQUAL "Address+Leak")
-        message(STATUS "Enabling address and leak sanitizer")
-
-        add_compile_options(-fsanitize=address,leak)
-        add_link_options(-fsanitize=address,leak)
-
-    elseif (USE_SANITIZER STREQUAL "Leak")
-        message(STATUS "Enabling leak sanitizer")
-
-        add_compile_options(-fsanitize=leak)
-        add_link_options(-fsanitize=leak)
-
-    elseif (USE_SANITIZER STREQUAL "Memory")
+    elseif (USE_SANITIZERS_LOWER STREQUAL "memory")
         # Requires Clang > 10 and libc++ (rather than libstdc++)
         # so we will wire up later
         message(FATAL_ERROR "Not Enabled Yet")
         message(STATUS "Enabling Memory sanitizer")
 
-    elseif (USE_SANITIZER STREQUAL "Thread")
+    elseif (USE_SANITIZERS_LOWER STREQUAL "thread")
         message(STATUS "Enabling Thread sanitizer")
 
         add_compile_options(-fsanitize=thread)
         add_link_options(-fsanitize=thread)
 
-    elseif (USE_SANITIZER STREQUAL "Undefined")
+    elseif (USE_SANITIZERS_LOWER STREQUAL "undefined")
         message(STATUS "Enabling undefined behaviour sanitizer")
 
         add_compile_options(
