f17e4f1459cfed5aae3a862320d22ecb27b5f970
Ross Whitfield <whitfieldre@ornl.gov>
2019-12-13 11:11:07 -0500

    Add working unittest

--- Framework/MDAlgorithms/test/ConvertSCDtoMDETest.h
+++ Framework/MDAlgorithms/test/ConvertSCDtoMDETest.h
@@ -10,69 +10,90 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/FileFinder.h"
+#include "MantidAPI/IMDEventWorkspace.h"
+#include "MantidAPI/Workspace_fwd.h"
+#include "MantidDataObjects/MDEventWorkspace.h"
 #include "MantidMDAlgorithms/ConvertSCDtoMDE.h"
 #include "MantidMDAlgorithms/LoadMD.h"
-//#include "TestHelpers/WorkspaceCreationHelpers.h"
-#include "MantidAPI/Workspace_fwd.h"
-#include "MantidAPI/AnalysisDataService.h"
-#include "MantidAPI/FileFinder.h"
-using Mantid::MDAlgorithms::ConvertSCDtoMDE;
-using Mantid::MDAlgorithms::LoadMD;
+
+using namespace Mantid::API;
+using namespace Mantid::MDAlgorithms;
 
 class ConvertSCDtoMDETest : public CxxTest::TestSuite {
 public:
   // This pair of boilerplate methods prevent the suite being created statically
   // This means the constructor isn't called when running other tests
-  static ConvertSCDtoMDETest *createSuite() { return new ConvertSCDtoMDETest(); }
-  static void destroySuite( ConvertSCDtoMDETest *suite ) { delete suite; }
+  static ConvertSCDtoMDETest *createSuite() {
+    return new ConvertSCDtoMDETest();
+  }
+  static void destroySuite(ConvertSCDtoMDETest *suite) { delete suite; }
 
-
-  void test_Init()
-  {
+  void test_Init() {
     ConvertSCDtoMDE alg;
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
   }
 
-  void test_exec()
-  {
+  void test_exec() {
     // Create test input if necessary
     LoadMD loader;
-    //loader.setChild(true);
     loader.initialize();
     loader.setPropertyValue(
         "Filename",
-        Mantid::API::FileFinder::Instance().getFullPath("MAPS_MDEW.nxs"));
-    loader.setPropertyValue("OutputWorkspace", "MD_EVENT_WS_ID");
+        Mantid::API::FileFinder::Instance().getFullPath("HB3A_data.nxs"));
+    loader.setPropertyValue("OutputWorkspace", "ConvertSCDtoMDETest_data");
     loader.setProperty("FileBackEnd", false);
     loader.execute();
-    auto inputWS = Mantid::API::AnalysisDataService::Instance().retrieveWS<Mantid::API::IMDHistoWorkspace>("MD_EVENT_WS_ID");
+    auto inputWS = Mantid::API::AnalysisDataService::Instance()
+                       .retrieveWS<Mantid::API::IMDHistoWorkspace>(
+                           "ConvertSCDtoMDETest_data");
 
     ConvertSCDtoMDE alg;
     // Don't put output in ADS by default
     alg.setChild(true);
-    TS_ASSERT_THROWS_NOTHING( alg.initialize() )
-    TS_ASSERT( alg.isInitialized() )
-    TS_ASSERT_THROWS_NOTHING( alg.setProperty("InputWorkspace", "MD_EVENT_WS_ID") );
-    TS_ASSERT_THROWS_NOTHING( alg.setPropertyValue("OutputWorkspace", "_unused_for_child") );
-    TS_ASSERT_THROWS_NOTHING( alg.execute(); );
-    TS_ASSERT( alg.isExecuted() );
+    TS_ASSERT_THROWS_NOTHING(alg.initialize())
+    TS_ASSERT(alg.isInitialized())
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setProperty("InputWorkspace", "ConvertSCDtoMDETest_data"));
+    TS_ASSERT_THROWS_NOTHING(alg.setProperty("Wavelength", "1.008"));
+    TS_ASSERT_THROWS_NOTHING(
+        alg.setPropertyValue("OutputWorkspace", "_unused_for_child"));
+    TS_ASSERT_THROWS_NOTHING(alg.execute(););
+    TS_ASSERT(alg.isExecuted());
 
-    // Retrieve the workspace from the algorithm. The type here will probably need to change. It should
-    // be the type using in declareProperty for the "OutputWorkspace" type.
-    // We can't use auto as it's an implicit conversion.
-    //Mantid::API::MatrixWorkspace_sptr outputWS = alg.getProperty("OutputWorkspace");
-    //TS_ASSERT(outputWS);
-    //TS_FAIL("TODO: Check the results and remove this line");
+    // Retrieve the workspace from the algorithm. The type here will probably
+    // need to change. It should be the type using in declareProperty for the
+    // "OutputWorkspace" type. We can't use auto as it's an implicit conversion.
+    IMDEventWorkspace_sptr outWS = alg.getProperty("OutputWorkspace");
+    TS_ASSERT(outWS);
+
+    // check dimensions
+    TS_ASSERT_EQUALS(3, outWS->getNumDims());
+    TS_ASSERT_EQUALS(Mantid::Kernel::QSample,
+                     outWS->getSpecialCoordinateSystem());
+    TS_ASSERT_EQUALS("QSample", outWS->getDimension(0)->getMDFrame().name());
+    TS_ASSERT_EQUALS(true, outWS->getDimension(0)->getMDUnits().isQUnit());
+    TS_ASSERT_EQUALS(-10, outWS->getDimension(0)->getMinimum());
+    TS_ASSERT_EQUALS(10, outWS->getDimension(0)->getMaximum());
+    TS_ASSERT_EQUALS("QSample", outWS->getDimension(1)->getMDFrame().name());
+    TS_ASSERT_EQUALS(true, outWS->getDimension(1)->getMDUnits().isQUnit());
+    TS_ASSERT_EQUALS(-10, outWS->getDimension(1)->getMinimum());
+    TS_ASSERT_EQUALS(10, outWS->getDimension(1)->getMaximum());
+    TS_ASSERT_EQUALS("QSample", outWS->getDimension(2)->getMDFrame().name());
+    TS_ASSERT_EQUALS(true, outWS->getDimension(2)->getMDUnits().isQUnit());
+    TS_ASSERT_EQUALS(-10, outWS->getDimension(2)->getMinimum());
+    TS_ASSERT_EQUALS(10, outWS->getDimension(2)->getMaximum());
+
+    // check other things
+    TS_ASSERT_EQUALS(1, outWS->getNumExperimentInfo());
+    TS_ASSERT_EQUALS(9038, outWS->getNEvents());
+    const Mantid::coord_t coords[3] = {
+        -0.42f, 1.71f, 2.3f}; // roughly the location of maximum instenity
+    TS_ASSERT_DELTA(
+        outWS->getSignalAtCoord(coords, Mantid::API::NoNormalization), 568,
+        1e-5);
   }
-  
-  void test_Something()
-  {
-    //TS_FAIL( "You forgot to write a test!");
-  }
-
-
 };
-
 
 #endif /* MANTID_MDALGORITHMS_CONVERTWANDSCDTOMDETEST_H_ */
