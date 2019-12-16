9bf798d61fc199d355a5de9660da50e6b37c43b4
Conor Finn <conor.finn@stfc.ac.uk>
2019-12-16 10:20:14 +0000

    RE #27187 Add settings model and tests

---
+++ scripts/Engineering/gui/engineering_diffraction/settings/settings_model.py
@@ -0,0 +1,31 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+from __future__ import (absolute_import, division, print_function)
+
+from Engineering.gui.engineering_diffraction.settings.settings_helper import get_setting, set_setting
+
+
+class SettingsModel(object):
+    INTERFACES_SETTINGS_GROUP = "CustomInterfaces"
+    ENGINEERING_PREFIX = "EngineeringDiffraction2/"
+
+    def get_settings_dict(self, keys):
+        settings = {}
+        for setting_name in keys:
+            settings[setting_name] = self.get_setting(setting_name)
+        return settings
+
+    def set_settings_dict(self, settings):
+        for key in settings:
+            self.set_setting(key, settings[key])
+
+    def get_setting(self, name):
+        return get_setting(self.INTERFACES_SETTINGS_GROUP, self.ENGINEERING_PREFIX, name)
+
+    def set_setting(self, name, value):
+        set_setting(self.INTERFACES_SETTINGS_GROUP, self.ENGINEERING_PREFIX, name, value)
