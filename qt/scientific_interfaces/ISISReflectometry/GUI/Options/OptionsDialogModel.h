af64102154f089e7c9f97c73a2897931a1b2c4d2
Giovanni Di Siena <giodisiena@gmail.com>
2020-01-06 10:23:09 +0000

    Add OptionsDialogModel
    Re #26533

---
+++ qt/scientific_interfaces/ISISReflectometry/GUI/Options/OptionsDialogModel.h
@@ -0,0 +1,38 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#ifndef MANTID_ISISREFLECTOMETRY_OPTIONSDIALOGMODEL_H
+#define MANTID_ISISREFLECTOMETRY_OPTIONSDIALOGMODEL_H
+
+#include "IOptionsDialogModel.h"
+#include <map>
+#include <string>
+
+namespace MantidQt {
+namespace CustomInterfaces {
+namespace ISISReflectometry {
+
+class MANTIDQT_ISISREFLECTOMETRY_DLL OptionsDialogModel
+    : public IOptionsDialogModel {
+public:
+  OptionsDialogModel();
+  ~OptionsDialogModel() = default;
+  void applyDefaultOptions(std::map<std::string, bool> &boolOptions,
+                           std::map<std::string, int> &intOptions) override;
+  void loadSettings(std::map<std::string, bool> &boolOptions,
+                    std::map<std::string, int> &intOptions) override;
+  void saveSettings(const std::map<std::string, bool> &boolOptions,
+                    const std::map<std::string, int> &intOptions) override;
+
+private:
+  const std::string REFLECTOMETRY_SETTINGS_GROUP = "ISISReflectometryUI";
+};
+
+} // namespace ISISReflectometry
+} // namespace CustomInterfaces
+} // namespace MantidQt
+
+#endif /* MANTID_ISISREFLECTOMETRY_OPTIONSDIALOGMODEL_H */
