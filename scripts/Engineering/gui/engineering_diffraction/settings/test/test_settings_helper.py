c55c9378d0c6b07e1488c95148fe72b71e511c69
Conor Finn <conor.finn@stfc.ac.uk>
2020-01-06 10:08:48 +0000

    RE #27187 Fix tests on windows
    On windows, the organisation and application must be specified
    explicitly to work in the tests.

--- scripts/Engineering/gui/engineering_diffraction/settings/test/test_settings_helper.py
+++ scripts/Engineering/gui/engineering_diffraction/settings/test/test_settings_helper.py
@@ -9,7 +9,10 @@ from __future__ import (absolute_import, division, print_function)
 
 from Engineering.gui.engineering_diffraction.settings.settings_helper import set_setting, get_setting
 
-from qtpy.QtCore import QSettings
+from qtpy.QtCore import QSettings, QCoreApplication
+
+from shutil import rmtree
+from os.path import dirname
 
 import unittest
 
@@ -21,10 +24,17 @@ class SettingsHelperTest(unittest.TestCase):
     def tearDown(self):
         settings = QSettings()
         settings.clear()
+        rmtree(dirname(settings.fileName()))
 
     def setUp(self):
         settings = QSettings()
         settings.clear()
+
+    @classmethod
+    def setUpClass(cls):
+        QCoreApplication.setApplicationName("test1")
+        QCoreApplication.setOrganizationName("org1")
+        QSettings.setDefaultFormat(QSettings.IniFormat)
 
     def test_set_setting_with_string(self):
         set_setting(GROUP, PREFIX, "something", "value")
@@ -35,7 +45,7 @@ class SettingsHelperTest(unittest.TestCase):
         settings.endGroup()
         self.assertEqual(returned, "value")
 
-    def test_set_setting_with_bool(self):
+    def test_set_setting_with_bool_false(self):
         set_setting(GROUP, PREFIX, "something", False)
 
         settings = QSettings()
@@ -43,6 +53,15 @@ class SettingsHelperTest(unittest.TestCase):
         returned = settings.value("EngineeringDiffraction2/" + "something", type=bool)
         settings.endGroup()
         self.assertEqual(returned, False)
+
+    def test_set_setting_with_bool_true(self):
+        set_setting(GROUP, PREFIX, "something", True)
+
+        settings = QSettings()
+        settings.beginGroup("CustomInterfaces")
+        returned = settings.value("EngineeringDiffraction2/" + "something", type=bool)
+        settings.endGroup()
+        self.assertEqual(returned, True)
 
     def test_set_setting_with_int(self):
         set_setting(GROUP, PREFIX, "something", 10)
@@ -61,13 +80,21 @@ class SettingsHelperTest(unittest.TestCase):
 
         self.assertEqual(get_setting(GROUP, PREFIX, "something"), "value")
 
-    def test_get_setting_with_bool(self):
+    def test_get_setting_with_bool_false(self):
         settings = QSettings()
         settings.beginGroup(GROUP)
         settings.setValue(PREFIX + "something", False)
         settings.endGroup()
 
         self.assertEqual(get_setting(GROUP, PREFIX, "something", return_type=bool), False)
+
+    def test_get_setting_with_bool_true(self):
+        settings = QSettings()
+        settings.beginGroup(GROUP)
+        settings.setValue(PREFIX + "something", True)
+        settings.endGroup()
+
+        self.assertEqual(get_setting(GROUP, PREFIX, "something", return_type=bool), True)
 
     def test_get_setting_with_int(self):
         settings = QSettings()
