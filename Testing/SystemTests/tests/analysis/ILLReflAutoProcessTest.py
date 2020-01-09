10dd1c4b8b3e0a4f7f5269a7089f7fb03d821d96
Gagik Vardanyan <vardanyan@ill.fr>
2020-01-09 11:19:32 +0100

    system test for 192 cycle data

---
+++ Testing/SystemTests/tests/analysis/ILLReflAutoProcessTest.py
@@ -0,0 +1,124 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+from __future__ import (absolute_import, division, print_function)
+
+import systemtesting
+from mantid.simpleapi import ReflectometryILLAutoProcess, config, mtd
+
+
+"""
+    @brief Tests with VoS11 sample at 2 angles with the data from cycle #192
+    Uses incoherent summation with sample angle option.
+"""
+class D17Cycle192IncoherentSanTest(systemtesting.MantidSystemTest):
+
+    def __init__(self):
+        super(D17Cycle192IncoherentSanTest, self).__init__()
+        self.setUp()
+
+    def setUp(self):
+        config['default.facility'] = 'ILL'
+        config['default.instrument'] = 'D17'
+        config['logging.loggers.root.level'] = 'Warning'
+        config.appendDataSearchSubDir('ILL/D17/')
+
+    def cleanup(self):
+        mtd.clear()
+
+    def validate(self):
+        self.tolerance = 1e-6
+        self.tolerance_is_rel_err = True
+        self.disableChecking = ['Instrument', 'Sample']
+        return ['VoS11', 'D17_VoS11.nxs']
+
+    def runTest(self):
+        name = 'VoS11'
+        directBeams = '541838,541839'
+        reflectedBeams = '541882,541883'
+        foregroundWidth = [3, 15]
+        angleOffset = [2, 5]
+        angleWidth = 5
+        ReflectometryILLAutoProcess(
+            Run=reflectedBeams,
+            DirectRun=directBeams,
+            OutputWorkspace=name,
+            SummationType='Incoherent',
+            AngleOption='SampleAngle',
+            DeltaQFractionBinning=0.5,
+            DirectLowAngleFrgHalfWidth=foregroundWidth,
+            DirectHighAngleFrgHalfWidth=foregroundWidth,
+            DirectLowAngleBkgOffset=angleOffset,
+            DirectLowAngleBkgWidth=angleWidth,
+            DirectHighAngleBkgOffset=angleOffset,
+            DirectHighAngleBkgWidth=angleWidth,
+            ReflLowAngleFrgHalfWidth=foregroundWidth,
+            ReflHighAngleFrgHalfWidth=foregroundWidth,
+            ReflLowAngleBkgOffset=angleOffset,
+            ReflLowAngleBkgWidth=angleWidth,
+            ReflHighAngleBkgOffset=angleOffset,
+            ReflHighAngleBkgWidth=angleWidth,
+            WavelengthLowerBound=[3., 3.],
+            WavelengthUpperBound=[27., 25.],
+            GlobalScaleFactor=0.13
+        )
+
+
+"""
+    @brief Tests with SiO2 sample at 2 angles with the data from cycle #192
+    Uses coherent summation with detector angle option.
+"""
+class D17Cycle192CoherentDanTest(systemtesting.MantidSystemTest):
+
+    def __init__(self):
+        super(D17Cycle192CoherentDanTest, self).__init__()
+        self.setUp()
+
+    def setUp(self):
+        config['default.facility'] = 'ILL'
+        config['default.instrument'] = 'D17'
+        config['logging.loggers.root.level'] = 'Warning'
+        config.appendDataSearchSubDir('ILL/D17/')
+
+    def cleanup(self):
+        mtd.clear()
+
+    def validate(self):
+        self.tolerance = 1e-6
+        self.tolerance_is_rel_err = True
+        self.disableChecking = ['Instrument', 'Sample']
+        return ['SiO2', 'D17_SiO2.nxs']
+
+    def runTest(self):
+        name = 'SiO2'
+        directBeams = '541838,541839'
+        reflectedBeams = '541853,541854'
+        foregroundWidth = [3, 15]
+        angleOffset = [2, 5]
+        angleWidth = 5
+        ReflectometryILLAutoProcess(
+            Run=reflectedBeams,
+            DirectRun=directBeams,
+            OutputWorkspace=name,
+            SummationType='Coherent',
+            AngleOption='DetectorAngle',
+            DeltaQFractionBinning=0.5,
+            DirectLowAngleFrgHalfWidth=foregroundWidth,
+            DirectHighAngleFrgHalfWidth=foregroundWidth,
+            DirectLowAngleBkgOffset=angleOffset,
+            DirectLowAngleBkgWidth=angleWidth,
+            DirectHighAngleBkgOffset=angleOffset,
+            DirectHighAngleBkgWidth=angleWidth,
+            ReflLowAngleFrgHalfWidth=foregroundWidth,
+            ReflHighAngleFrgHalfWidth=foregroundWidth,
+            ReflLowAngleBkgOffset=angleOffset,
+            ReflLowAngleBkgWidth=angleWidth,
+            ReflHighAngleBkgOffset=angleOffset,
+            ReflHighAngleBkgWidth=angleWidth,
+            WavelengthLowerBound=[3., 3.],
+            WavelengthUpperBound=[27., 25.],
+            GlobalScaleFactor=0.13
+        )
