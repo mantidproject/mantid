6b61614357741a5ab059d5c3ab6e9685cea4fba3
Martyn Gigg <martyn.gigg@gmail.com>
2020-01-14 12:09:54 +0000

    Only install minimal Python API DLL if it exists.
    Python 2 does not have this DLL.

--- Framework/PythonInterface/mantid/BundlePython.cmake
+++ Framework/PythonInterface/mantid/BundlePython.cmake
@@ -15,6 +15,11 @@ if( MSVC )
   install ( DIRECTORY ${PYTHON_DIR}/Scripts DESTINATION bin PATTERN ".svn" EXCLUDE PATTERN ".git" EXCLUDE PATTERN "*_d.py" EXCLUDE )
   install ( DIRECTORY ${PYTHON_DIR}/tcl DESTINATION bin PATTERN ".svn" EXCLUDE PATTERN ".git" EXCLUDE )
   install ( FILES ${PYTHON_DIR}/python${PYTHON_MAJOR_VERSION}${PYTHON_MINOR_VERSION}.dll
-            ${PYTHON_DIR}/python${PYTHON_MAJOR_VERSION}.dll ${PYTHON_EXECUTABLE} ${PYTHONW_EXECUTABLE}
+            ${PYTHON_EXECUTABLE} ${PYTHONW_EXECUTABLE}
             DESTINATION bin )
+  # Python >= 3 has an minimal API DLL too
+  if ( EXISTS ${PYTHON_DIR}/python${PYTHON_MAJOR_VERSION}.dll )
+    install ( FILES ${PYTHON_DIR}/python${PYTHON_MAJOR_VERSION}.dll
+              DESTINATION bin )
+  endif()
 endif()
