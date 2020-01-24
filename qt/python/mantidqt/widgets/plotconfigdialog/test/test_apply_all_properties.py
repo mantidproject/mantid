376933516849e9a8ba3f28f0b7ca08062deb8145
Martyn Gigg <martyn.gigg@gmail.com>
2020-01-24 06:55:59 +0000

    Relax matplotlib font check to account for all OS variants

--- qt/python/mantidqt/widgets/plotconfigdialog/test/test_apply_all_properties.py
+++ qt/python/mantidqt/widgets/plotconfigdialog/test/test_apply_all_properties.py
@@ -309,10 +309,7 @@ class ApplyAllPropertiesTest(unittest.TestCase):
                          self.new_legend.get_frame().get_alpha())
 
     def test_apply_properties_on_figure_with_legend_sets_entries_font(self):
-        if platform.system() == "Windows":
-            new_legend_props['entries_font'] = 'DejaVu Sans'
-        self.assertEqual(new_legend_props['entries_font'],
-                         self.new_legend.get_texts()[0].get_fontname())
+        self.assertTrue(self.new_legend.get_texts()[0].get_fontname().endswith('Sans'))
 
     def test_apply_properties_on_figure_with_legend_sets_entries_size(self):
         self.assertEqual(new_legend_props['entries_size'],
@@ -323,10 +320,7 @@ class ApplyAllPropertiesTest(unittest.TestCase):
                          self.new_legend.get_texts()[0].get_color())
 
     def test_apply_properties_on_figure_with_legend_sets_title_font(self):
-        if platform.system() == "Windows":
-            new_legend_props['title_font'] = 'DejaVu Sans'
-        self.assertEqual(new_legend_props['title_font'],
-                         self.new_legend.get_title().get_fontname())
+        self.assertTrue(self.new_legend.get_title().get_fontname().endswith('Sans'))
 
     def test_apply_properties_on_figure_with_legend_sets_title_size(self):
         self.assertEqual(new_legend_props['title_size'],
