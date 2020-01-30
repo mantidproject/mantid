34d6fe0a10892a8ba4f481679bf97703863e5cfb
Gagik Vardanyan <vardanyan@ill.fr>
2020-01-30 13:57:54 +0100

    add a separate function for uitopy for puqt5

--- scripts/Drill.py
+++ scripts/Drill.py
@@ -5,14 +5,14 @@
 #     & Institut Laue - Langevin
 # SPDX - License - Identifier: GPL - 3.0 +
 from __future__ import (absolute_import, division, print_function)
-from mantidqt.gui_helper import get_qapplication
-from Interface.ui.drill.main import (model, view, presenter)
 from qtpy import PYQT5
 if not PYQT5:
     raise RuntimeError('Drill is operational only in workbench.')
 from mantid.simpleapi import config
 if config['default.facility'] != 'ILL':
     raise RuntimeError('Set the facility to ILL to launch this interface.')
+from mantidqt.gui_helper import get_qapplication
+from Interface.ui.drill.main import (model, view, presenter)
 app, within_mantid = get_qapplication()
 window = view.DrillView()
 model = model.DrillModel()
