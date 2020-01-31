d960dbb4120b108ffce7d6a975dbe2188985e485
Martyn Gigg <martyn.gigg@gmail.com>
2020-01-31 17:23:55 +0000

    Create minimal standalone display of PeaksWorkspace
    Refs #27565

---
+++ qt/python/mantidqt/widgets/sliceviewer/peaksviewer/presenter.py
@@ -0,0 +1,85 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantid workbench.
+from __future__ import (absolute_import, division, unicode_literals)
+
+# 3rd party imports
+from mantid.py3compat.enum import Enum
+
+# local imports
+from mantidqt.widgets.workspacedisplay.table.model \
+    import TableWorkspaceDisplayModel
+from mantidqt.widgets.workspacedisplay.table.presenter \
+    import TableWorkspaceDataPresenter
+
+
+class PeaksViewerPresenter(object):
+    """Controls a PeaksViewerView with a given model to display
+    the peaks table and interaction controls for single workspace.
+    """
+    class Event(Enum):
+        PeaksListChanged = 1
+
+    def __init__(self, peaks_ws, view):
+        """
+        Constructs the view for the given PeaksWorkspace
+        :param peaks_ws: A handle to the PeaksWorkspace to display
+        :param view: A view object with a subscribe method to register this presenter
+                     as a listener for view events
+        """
+        super(PeaksViewerPresenter, self).__init__()
+        self._raise_error_if_workspace_incompatible(peaks_ws)
+        self._peaks_table_presenter = \
+            TableWorkspaceDataPresenter(TableWorkspaceDisplayModel(peaks_ws), view.table_view)
+
+        view.subscribe(self)
+        view.set_title(peaks_ws.name())
+        self.notify(PeaksViewerPresenter.Event.PeaksListChanged)
+
+    def notify(self, event):
+        """
+        Notification of an event that the presenter should react to
+        :param event:
+        """
+        if event == PeaksViewerPresenter.Event.PeaksListChanged:
+            self._peaks_table_presenter.refresh()
+
+    # private api
+    @staticmethod
+    def _raise_error_if_workspace_incompatible(ws):
+        """
+        :param ws: A reference to an object to check for compatability
+        """
+        if not hasattr(ws, "getNumberPeaks"):
+            raise ValueError("Expected a PeaksWorkspace. Found {}.".format(type(ws)))
+
+
+class PeaksViewerCollectionPresenter(object):
+    """Controls a widget comprising of multiple PeasViewerViews to display and
+    interact with multiple PeaksWorkspaces"""
+
+    def __init__(self, peaks_workspaces, view):
+        """
+        :param peaks_workspaces: An iterable of PeakWorkspace objects.
+        :param view: View displaying the model information
+        """
+        self._view = view
+        self._child_presenters = []
+        for peaks_ws in peaks_workspaces:
+            self.append_peaksworkspace(peaks_ws)
+
+    @property
+    def view(self):
+        return self._view
+
+    def append_peaksworkspace(self, peaks_ws):
+        """
+        Create and append a view for the given workspace
+        :param peaks_workspaces: A PeakWorkspace object.
+        """
+        self._child_presenters.append(PeaksViewerPresenter(peaks_ws,
+                                                           self._view.append_peaksviewer()))
---
+++ qt/python/mantidqt/widgets/sliceviewer/peaksviewer/view.py
@@ -0,0 +1,95 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantid workbench.
+from __future__ import (absolute_import, division, unicode_literals)
+
+# 3rd party imports
+from qtpy.QtWidgets import QGroupBox, QVBoxLayout, QWidget
+
+# local imports
+from mantidqt.widgets.workspacedisplay.table.view import TableWorkspaceDisplayView
+
+
+class PeaksViewerView(QWidget):
+    """Displays a table view of the PeaksWorkspace along with controls
+    to interact with the peaks.
+    """
+    TITLE_PREFIX = "Workspace: "
+
+    def __init__(self, parent=None):
+        """
+        :param parent: An optional parent widget
+        """
+        super(PeaksViewerView, self).__init__(parent)
+        self._group_box = None
+        self._presenter = None
+        self._table_view = None
+        self._setup_ui()
+
+    @property
+    def table_view(self):
+        return self._table_view
+
+    def subscribe(self, presenter):
+        """
+        :param presenter: An object to handle GUI events.
+        """
+        self._presenter = presenter
+        self._table_view.subscribe(presenter)
+
+    def set_title(self, name):
+        """
+        :param name: Set the name label for the workspace
+        """
+        self._group_box.setTitle(self.TITLE_PREFIX + name)
+
+    # private api
+    def _setup_ui(self):
+        """
+        Arrange the widgets on the window
+        """
+        self._group_box = QGroupBox(self)
+        self._group_box.setContentsMargins(0, 0, 0, 0)
+        self._table_view = TableWorkspaceDisplayView(parent=self)
+
+        group_box_layout = QVBoxLayout()
+        group_box_layout.addWidget(self._table_view)
+        self._group_box.setLayout(group_box_layout)
+        widget_layout = QVBoxLayout()
+        widget_layout.addWidget(self._group_box)
+        self.setLayout(widget_layout)
+
+
+class PeaksViewerCollectionView(QWidget):
+    """Display a collection of PeaksViewerView objects in a scrolling view.
+    """
+
+    def __init__(self, parent=None):
+        """
+        :param parent: An optional parent widget
+        """
+        super(PeaksViewerCollectionView, self).__init__(parent)
+        self._setup_ui()
+
+    def append_peaksviewer(self):
+        """
+        Append a view widget to end of the current list of views
+        :param peaks_view: A reference to a single view widget for a PeaksWorkspace
+        """
+        child_view = PeaksViewerView(self)
+        self._peaks_layout.addWidget(child_view)
+        return child_view
+
+    # private api
+    def _setup_ui(self):
+        """
+        Arrange widgets on the window.
+        """
+        # create vertical layouts for outer widget
+        outer_layout = QVBoxLayout()
+        self.setLayout(outer_layout)
+        self._peaks_layout = outer_layout
