51ef07170beca2769b64bfc69f9cb7f1a0333aba
Martyn Gigg <martyn.gigg@gmail.com>
2020-02-04 16:31:33 +0000

    Use ITableWorkspace interface in table display
    Avoids having to rely on implementation specific
    details.
    Refs #27465

--- qt/python/mantidqt/widgets/workspacedisplay/table/model.py
+++ qt/python/mantidqt/widgets/workspacedisplay/table/model.py
@@ -6,7 +6,7 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 # coding=utf-8
 #  This file is part of the mantidqt package.
-from mantid.dataobjects import PeaksWorkspace, TableWorkspace
+from mantid.dataobjects import PeaksWorkspace, ITableWorkspace
 from mantid.kernel import V3D
 from mantid.simpleapi import DeleteTableRows, SortPeaksWorkspace, SortTableWorkspace, StatisticsOfTableWorkspace
 from mantidqt.widgets.workspacedisplay.table.error_column import ErrorColumn
@@ -33,7 +33,7 @@ class TableWorkspaceDisplayModel:
     SPECTRUM_PLOT_LEGEND_STRING = '{}-{}'
     BIN_PLOT_LEGEND_STRING = '{}-bin-{}'
 
-    ALLOWED_WORKSPACE_TYPES = [PeaksWorkspace, TableWorkspace]
+    ALLOWED_WORKSPACE_TYPES = [ITableWorkspace]
 
     @classmethod
     def supports(cls, ws):
@@ -104,7 +104,7 @@ class TableWorkspaceDisplayModel:
         return self.get_column_headers()[index]
 
     def is_peaks_workspace(self):
-        return isinstance(self.ws, PeaksWorkspace)
+        return isinstance(self.ws, IPeaksWorkspace)
 
     def set_cell_data(self, row, col, data, is_v3d):
         # if the cell contains V3D data, construct a V3D object
@@ -132,11 +132,17 @@ class TableWorkspaceDisplayModel:
     def sort(self, column_index, sort_ascending):
         column_name = self.ws.getColumnNames()[column_index]
         if self.is_peaks_workspace():
-            SortPeaksWorkspace(InputWorkspace=self.ws, OutputWorkspace=self.ws, ColumnNameToSortBy=column_name,
-                               SortAscending=sort_ascending)
+            SortPeaksWorkspace(
+                InputWorkspace=self.ws,
+                OutputWorkspace=self.ws,
+                ColumnNameToSortBy=column_name,
+                SortAscending=sort_ascending)
         else:
-            SortTableWorkspace(InputWorkspace=self.ws, OutputWorkspace=self.ws, Columns=column_name,
-                               Ascending=sort_ascending)
+            SortTableWorkspace(
+                InputWorkspace=self.ws,
+                OutputWorkspace=self.ws,
+                Columns=column_name,
+                Ascending=sort_ascending)
 
     def set_column_type(self, col, type, linked_col_index=-1):
         self.ws.setPlotType(col, type, linked_col_index)
