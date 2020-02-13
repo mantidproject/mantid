9dfeb9b631972894b8854b5dd6b4706735c01278
Steven Hahn <hahnse@ornl.gov>
2020-02-13 13:51:36 -0500

    Add LogTypes parameter to AddSampleLogsMultiple
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/AddSampleLogMultiple.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/AddSampleLogMultiple.py
@@ -39,12 +39,15 @@ class AddSampleLogMultiple(PythonAlgorithm):
         self.declareProperty('ParseType', True,
                              doc='Determine the value type by parsing the string')
 
+        self.declareProperty(StringArrayProperty('LogTypes', ''),
+                             doc='Comma separated list of types the log values will be.')
     def PyExec(self):
         workspace = self.getProperty('Workspace').value
         log_names = self.getProperty('LogNames').value
         log_values = self.getProperty('LogValues').value
         log_units = self.getProperty('LogUnits').value
         parse_type = self.getProperty('ParseType').value
+        log_types = self.getProperty('LogTypes').value
 
         if len(log_units) == 0:
             log_units = [""]*len(log_names)
@@ -63,6 +66,8 @@ class AddSampleLogMultiple(PythonAlgorithm):
                     value_type = 'Number'
                 except ValueError:
                     pass
+            elif len(log_types):
+                value_type = log_types[idx]
 
             # Add the log
             alg = AlgorithmManager.create('AddSampleLog')
@@ -82,16 +87,18 @@ class AddSampleLogMultiple(PythonAlgorithm):
         log_names = self.getProperty('LogNames').value
         log_values = self.getProperty('LogValues').value
         log_units = self.getProperty('LogUnits').value
+        log_types = self.getProperty('LogTypes').value
 
         num_names = len(log_names)
         num_values = len(log_values)
         num_units = len(log_units)
+        num_types = len(num_types)
 
-        # Ensure there is at leats 1 log name
+        # Ensure there is at least 1 log name
         if num_names == 0:
             issues['LogNames'] = 'Must have at least one log name'
 
-        # Ensure there is at leats 1 log value
+        # Ensure there is at least 1 log value
         if num_values == 0:
             issues['LogValues'] = 'Must have at least one log value'
 
@@ -100,6 +107,17 @@ class AddSampleLogMultiple(PythonAlgorithm):
 
         if num_names > 0 and num_units != 0 and num_units != num_names:
             issues['LogUnits'] = 'Number of log units must be 0 or match the number of log names'
+
+        if self.getProperty('ParseType').value and num_types != 0:
+            issue['LogTypes'] = 'LogTypes array not used when ParseType=True'
+
+        if num_names > 0 and num_types != 0 and num_types != num_names:
+            issues['LogTypes'] = 'Number of log types must be 0 or match the number of log names'
+
+        allowed_types = ['String','Number', 'Number Series']
+        for value in log_types:
+            if value not in allowed_types:
+                issues['LogTypes'] = '{} is not an allowed log type'.format(value)
 
         return issues
 
