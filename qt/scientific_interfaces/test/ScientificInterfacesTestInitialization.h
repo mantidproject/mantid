8b04255e0958b4025620555455a57ab9f02d3cb3
Giovanni Di Siena <giodisiena@gmail.com>
2020-02-13 14:21:26 +0000

    Re #27889 Add Python interpreter initialisation file

---
+++ qt/scientific_interfaces/test/ScientificInterfacesTestInitialization.h
@@ -0,0 +1,20 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#ifndef REFLMPLCPPTESTGLOBALINITIALIZATION_H
+#define REFLMPLCPPTESTGLOBALINITIALIZATION_H
+
+#include "MantidPythonInterface/core/Testing/PythonInterpreterGlobalFixture.h"
+
+//------------------------------------------------------------------------------
+// Static definitions
+//
+// We rely on cxxtest only including this file once so that the following
+// statements do not cause multiple-definition errors.
+//------------------------------------------------------------------------------
+static PythonInterpreterGlobalFixture PYTHON_INTERPRETER;
+
+#endif // REFLMPLCPPTESTGLOBALINITIALIZATION_H
