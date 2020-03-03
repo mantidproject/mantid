b8a729b9ea5912c022f574731fdca2c513584f62
Anthony Lim <anthony.lim@stfc.ac.uk>
2020-03-03 15:09:18 +0000

    ALC code clean up - clang format

--- qt/scientific_interfaces/Muon/ALCDataLoadingPresenter.cpp
+++ qt/scientific_interfaces/Muon/ALCDataLoadingPresenter.cpp
@@ -159,26 +159,26 @@ void ALCDataLoadingPresenter::load(const std::string &lastFile) {
   }
 
   if (lastRunPath.toString() == "") {
-      m_view->displayError(
-          "The last run is not a valid run number. \n"
-          "This could be because the file is not in the search path or the "
-          "file does not exist yet. ");
+    m_view->displayError(
+        "The last run is not a valid run number. \n"
+        "This could be because the file is not in the search path or the "
+        "file does not exist yet. ");
     m_view->enableAll();
-      m_loadingData = false;
+    m_loadingData = false;
     return;
-    }
-    if (firstRunPath.toString() == "") {
-      m_view->displayError(
-          "The first run is not a valid run number. \n"
-          "This could be because the file is not in the search path or the "
-          "file does not exist yet. ");
-      m_view->enableAll();
-      m_loadingData = false;
-      return;
-    }
+  }
+  if (firstRunPath.toString() == "") {
+    m_view->displayError(
+        "The first run is not a valid run number. \n"
+        "This could be because the file is not in the search path or the "
+        "file does not exist yet. ");
+    m_view->enableAll();
+    m_loadingData = false;
+    return;
+  }
 
   try {
- 
+
     IAlgorithm_sptr alg =
         AlgorithmManager::Instance().create("PlotAsymmetryByLogValue");
     alg->setChild(true); // Don't want workspaces in the ADS
