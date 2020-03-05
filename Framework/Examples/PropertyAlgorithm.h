3ec5afa14630d27fffefbad90e96baaf3cb3e712
Harriet Brown <harriet.brown@stfc.ac.uk>
2020-03-05 16:08:30 +0000

    Replace header guards in Examples with pragma once
    This commit replaces all header guards in Examples with #pragma once

--- Framework/Examples/PropertyAlgorithm.h
+++ Framework/Examples/PropertyAlgorithm.h
@@ -4,8 +4,7 @@
 //     NScD Oak Ridge National Laboratory, European Spallation Source
 //     & Institut Laue - Langevin
 // SPDX - License - Identifier: GPL - 3.0 +
-#ifndef USER_ALGORITHMS_PROPERTYALGORITHM_H_
-#define USER_ALGORITHMS_PROPERTYALGORITHM_H_
+#pragma once
 
 #include "MantidAPI/Algorithm.h"
 
@@ -40,5 +39,3 @@ private:
 
 } // namespace Algorithms
 } // namespace Mantid
-
-#endif /*USER_ALGORITHMS_PROPERTYALGORITHM_H_*/
