d1ad02065670722bbafcead6ea6bd606d358b001
Nick Draper <nick.draper@stfc.ac.uk>
2020-03-16 09:25:52 +0000

    reduce a error log to warning when changing to log axes

--- Framework/PythonInterface/mantid/plots/datafunctions.py
+++ Framework/PythonInterface/mantid/plots/datafunctions.py
@@ -1015,11 +1015,11 @@ def update_colorbar_scale(figure, image, scale, vmin, vmax):
     """
     if vmin <= 0 and scale == LogNorm:
         vmin = 0.0001  # Avoid 0 log scale error
-        mantid.kernel.logger.error("Scale is set to logarithmic so non-positive min value has been changed to 0.0001.")
+        mantid.kernel.logger.warning("Scale is set to logarithmic so non-positive min value has been changed to 0.0001.")
 
     if vmax <= 0 and scale == LogNorm:
         vmax = 1  # Avoid 0 log scale error
-        mantid.kernel.logger.error("Scale is set to logarithmic so non-positive max value has been changed to 1.")
+        mantid.kernel.logger.warning("Scale is set to logarithmic so non-positive max value has been changed to 1.")
 
     image.set_norm(scale(vmin=vmin, vmax=vmax))
     if image.colorbar:
