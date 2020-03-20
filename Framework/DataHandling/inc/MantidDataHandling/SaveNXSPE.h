8d968f2ca6bceafa26753ffd3a0344b22ede5e10
Nick Draper <nick.draper@stfc.ac.uk>
2020-03-20 16:51:01 +0000

    restore header content that was mistakenly wiped

--- Framework/DataHandling/inc/MantidDataHandling/SaveNXSPE.h
+++ Framework/DataHandling/inc/MantidDataHandling/SaveNXSPE.h
@@ -5,6 +5,39 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 // Mantid Repository : https://github.com/mantidproject/mantid
+#pragma once
+
+//---------------------------------------------------
+// Includes
+//---------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+
+namespace Mantid {
+namespace DataHandling {
+
+/**
+ * Saves a workspace into a NeXus/HDF5 NXSPE file.
+ *
+ *
+ * This file is part of Mantid.
+ *
+ *   Mantid is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Mantid is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   File change history is stored at: <https://github.com/mantidproject/mantid>
+ *   Code Documentation is available at: <http://doxygen.mantidproject.org>
+ *
+ */
 
 class DLLExport SaveNXSPE : public API::Algorithm {
 public:
