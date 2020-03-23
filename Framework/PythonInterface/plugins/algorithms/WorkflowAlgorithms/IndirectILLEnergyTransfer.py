230264bb4fc8856527c37456dbeb308cf68c2667
Gagik Vardanyan <vardanyan@ill.fr>
2020-03-23 15:44:55 +0100

    included 2nd order correction to deltaE axis

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/IndirectILLEnergyTransfer.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/IndirectILLEnergyTransfer.py
@@ -262,30 +262,40 @@ class IndirectILLEnergyTransfer(PythonAlgorithm):
             logger.debug('Mask bins larger than {0}'.format(xend))
             MaskBins(InputWorkspace=ws, OutputWorkspace=ws, XMin=x_values[xend + 1], XMax=x_values[-1])
 
+    @staticmethod
+    def _velocity(e):
+        """
+        Returns neutrons velocity in m/s from energy in mev
+        """
+        return 437.695 * math.sqrt(e)
+
     def _convert_to_energy(self, ws):
         """
-        Converts the x-axis from raw channel number to energy transfer
+        Converts the x-axis from raw channel number to energy transfer for Doppler mode
         @param ws :: input workspace name
         """
-
-        x = mtd[ws].readX(0)
-        size = mtd[ws].blocksize()
-        mid = (x[-1] + x[0])/ 2.
-        scale = 0.001  # from micro ev to mili ev
-
-        factor = size / (size - 1)
-
-        # minus sign is needed
+        bsize = mtd[ws].blocksize()
         if self._doppler_energy != 0:
-            formula = '-(x/{0} - 1)*{1}'.format(mid, self._doppler_energy * scale * factor)
+            # the doppler channels are linear in velocity (not time, neither deltaE)
+            # so we perform 2-step conversion, first linear to v, then quadratic to deltaE
+            efixed = mtd[ws].getInstrument().getNumberParameter('Efixed')[0]
+            vfixed = self._velocity(efixed)
+            vmax = self._velocity(efixed + self._doppler_energy * 1E-3)
+            vformula = '2/({0}-1)*({1}-{2})*(x-{0}/2)+{2}'.format(bsize, vmax, vfixed)
+            ConvertAxisByFormula(InputWorkspace=ws, OutputWorkspace=ws, Axis='X', Formula=vformula)
+
+            # minus sign is needed, this had been discovered from a measurement at low T, where
+            # the QENS peak was asymmetric the 'wrong' way in terms of deltaE, if there is no -
+            # this means that the acquisition channel 0 is not vmin, but vmax
+            nmass = 1.04397 * 1E-5  # mev / (m/s)**2
+            eformula = '-{0}*x*x/2 + {1}'.format(nmass, efixed)
+            ConvertAxisByFormula(InputWorkspace=ws, OutputWorkspace=ws, Axis='X', Formula=eformula, AxisUnits='DeltaE')
         else:
             # Center the data for elastic fixed window scan, for integration over the elastic peak
-            formula = '-(x-{0})*{1}'.format(mid-0.5, 1. / scale)
-            self.log().notice('The only energy value is 0 meV. Ignore the x-axis.')
-
-        self.log().information('Energy conversion formula is: {0}'.format(formula))
-
-        ConvertAxisByFormula(InputWorkspace=ws, OutputWorkspace=ws, Axis='X', Formula=formula, AxisUnits = 'DeltaE')
+            # Here all that matters it that the center is at 0 deltaE, but there is no real meaning of the axis extent
+            formula = '-(x-{0})*{1}'.format(bsize / 2 - 0.5, 1E+3)
+            self.log().debug('The only energy value is 0 meV. Ignore the x-axis.')
+            ConvertAxisByFormula(InputWorkspace=ws, OutputWorkspace=ws, Axis='X', Formula=formula, AxisUnits='DeltaE')
 
     @staticmethod
     def _monitor_max_range(ws):
