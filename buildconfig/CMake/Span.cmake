9328462df9008926cfa03166291f0ef45d2e67bd
David Fairbrother <DavidFair@users.noreply.github.com>
2020-03-24 09:37:13 +0000

    Add quotes to patching step, in case there are spaces
    Adds quotes to the patching step for span in case there are spaces in
    the path causing it to fail

--- buildconfig/CMake/Span.cmake
+++ buildconfig/CMake/Span.cmake
@@ -9,8 +9,8 @@ FetchContent_Declare(
   span
   GIT_REPOSITORY https://github.com/tcbrindle/span.git
   GIT_TAG        08cb4bf0e06c0e36f7e2b64e488ede711a8bb5ad
-  PATCH_COMMAND     ${GIT_EXECUTABLE} reset --hard ${_tag}
-    COMMAND ${GIT_EXECUTABLE} apply ${_apply_flags} ${CMAKE_SOURCE_DIR}/buildconfig/CMake/span_disable_testing.patch
+  PATCH_COMMAND     "${GIT_EXECUTABLE}" reset --hard ${_tag}
+    COMMAND "${GIT_EXECUTABLE}" apply ${_apply_flags} "${CMAKE_SOURCE_DIR}/buildconfig/CMake/span_disable_testing.patch"
 )
 
 FetchContent_GetProperties(span)
