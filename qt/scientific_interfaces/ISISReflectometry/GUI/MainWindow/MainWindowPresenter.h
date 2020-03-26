4673ae906c8ccb1b05ee7ee89dd495583a8e48ed
Gemma Guest <gemma.guest@stfc.ac.uk>
2020-03-26 16:52:36 +0000

    Remove unused function in public class interface

--- qt/scientific_interfaces/ISISReflectometry/GUI/MainWindow/MainWindowPresenter.h
+++ qt/scientific_interfaces/ISISReflectometry/GUI/MainWindow/MainWindowPresenter.h
@@ -88,7 +88,6 @@ private:
   void addNewBatch(IBatchView *batchView);
   void initNewBatch(IBatchPresenter *batchPresenter,
                     std::string const &instrument);
-  void changeInstrument(std::string const &instrumentName);
   void updateInstrument(const std::string &instrumentName);
   void setDefaultInstrument(const std::string &newInstrument);
 
