d3d46b5a9d0f8408c76e9c9f931ae4d9d820b44f
Nick Draper <nick.draper@stfc.ac.uk>
2020-04-06 16:56:35 +0100

    clang format for this pr

--- qt/widgets/common/test/FitPropertyBrowserTest.h
+++ qt/widgets/common/test/FitPropertyBrowserTest.h
@@ -6,10 +6,10 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
-#include "MantidQtWidgets/Common/FitPropertyBrowser.h"
+#include "MantidAPI/FunctionFactory.h"
 #include "MantidAPI/IFunction1D.h"
 #include "MantidAPI/ParamFunction.h"
-#include "MantidAPI/FunctionFactory.h"
+#include "MantidQtWidgets/Common/FitPropertyBrowser.h"
 #include <cxxtest/TestSuite.h>
 
 using namespace Mantid::API;
@@ -41,17 +41,20 @@ DECLARE_FUNCTION(FitPropertyBrowserTest_Funct)
 
 class FitPropertyBrowserTest : public CxxTest::TestSuite {
 public:
-  //This is a very specific test for a bug that is now fixed to prevent regression
+  // This is a very specific test for a bug that is now fixed to prevent
+  // regression
   void test_FunctionFactory_notification_is_released() {
-    
-    //create a FunctionBrowser
-    auto fpBrowser = std::make_unique<MantidQt::MantidWidgets::FitPropertyBrowser>();
-    //initialise it - this adds an observer on the function factory update message
-    fpBrowser->init();
-    //delete the FunctionBrowser
-    fpBrowser.reset();
-    //Make sure the FunctionFactory does not have a dead link as an observer
-    TS_ASSERT_THROWS_NOTHING(FunctionFactory::Instance().unsubscribe("FitPropertyBrowserTest_Funct");)
-  }
 
+    // create a FunctionBrowser
+    auto fpBrowser =
+        std::make_unique<MantidQt::MantidWidgets::FitPropertyBrowser>();
+    // initialise it - this adds an observer on the function factory update
+    // message
+    fpBrowser->init();
+    // delete the FunctionBrowser
+    fpBrowser.reset();
+    // Make sure the FunctionFactory does not have a dead link as an observer
+    TS_ASSERT_THROWS_NOTHING(FunctionFactory::Instance().unsubscribe(
+        "FitPropertyBrowserTest_Funct");)
+  }
 };
