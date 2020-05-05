254fc322e05b8e4df570a5bde4114a85d571c605
Gagik Vardanyan <vardanyan@ill.fr>
2020-05-05 07:51:21 +0200

    check empty label on osiris elastic test

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CalculateMonteCarloAbsorptionTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CalculateMonteCarloAbsorptionTest.py
@@ -109,7 +109,7 @@ class CalculateMonteCarloAbsorptionTest(unittest.TestCase):
         self._expected_unit = "MomentumTransfer"
         self._expected_hist = 17
         self._expected_blocksize = 1
-        self._run_correction_and_test(shape, self._indirect_elastic_ws)
+        self._run_correction_and_test(shape, self._indirect_elastic_ws, 'Label')
 
     def _run_indirect_fws_test_red(self, shape):
         self._expected_unit = "Wavelength"
