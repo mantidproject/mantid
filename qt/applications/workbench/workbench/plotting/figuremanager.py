3077e9a23cf4773195539616685066b93d33eb5a
Conor Finn <conor.finn@stfc.ac.uk>
2020-05-07 11:30:16 +0100

    RE #28251 Fix colorbar axis type changing when scale changed

--- qt/applications/workbench/workbench/plotting/figuremanager.py
+++ qt/applications/workbench/workbench/plotting/figuremanager.py
@@ -97,14 +97,7 @@ class FigureManagerADSObserver(AnalysisDataServiceObserver):
                 to_redraw = ax.remove_workspace_artists(workspace)
             else:
                 to_redraw = False
-            # We check for images and lines below as a pseudo check for an axes being
-            # a colorbar. Creating a colorfill plot creates 2 axes: one linked
-            # to a workspace, the other a colorbar. Deleting the workspace
-            # deletes the colorfill, but the plot remains open due to the
-            # non-empty colorbar. This solution seems to work for the majority
-            # of cases but could lead to unmanaged figures only containing an
-            # Axes object being closed.
-            if len(ax.images) != 0 or len(ax.lines) != 0:
+            if type(ax) is not Axes:  # Solution for filtering out colorbar axes. Works most of the time.
                 empty_axes.append(MantidAxes.is_empty(ax))
             redraw = redraw | to_redraw
 
