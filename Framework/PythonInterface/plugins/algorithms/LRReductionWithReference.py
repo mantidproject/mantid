7f9c76d239e76b05efc4dcc692eae30f1ecfc5b9
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-05-11 13:29:34 -0400

    Re #28626 - Added normalization processing step to LReductionWithReference

--- Framework/PythonInterface/plugins/algorithms/LRReductionWithReference.py
+++ Framework/PythonInterface/plugins/algorithms/LRReductionWithReference.py
@@ -5,13 +5,10 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 #pylint: disable=no-init,invalid-name
-import time
-import math
-import os
+import copy
 from mantid.api import *
 from mantid.simpleapi import *
 from mantid.kernel import *
-from functools import reduce #pylint: disable=redefined-builtin
 
 # Unable to generate this props list in PyInit using AlgorithmManager
 # from LiquidsReflectometryReduction to copy properties here
@@ -68,7 +65,22 @@ class LRReductionWithReference(DataProcessorAlgorithm):
         self.copyProperties(LR_ALG_FOR_PROPS, PROPS_TO_COPY)
 
     def PyExec(self):
-        wksp = self.getProperty("InputWorkspace").value
+        kwargs = dict()
+        for prop in PROPS_TO_COPY:
+            kwargs[prop] = self.getProperty(prop).value
+
+        # Process the normalization run
+        norm_kwargs = copy.deepcopy(kwargs)
+        norm_kwargs['SignalPeakPixelRange'] = kwargs['NormPeakPixelRange']
+        norm_kwargs['SubtractSignalBackground'] = kwargs['SubtractNormBackground']
+        norm_kwargs['SignalBackgroundPixelRange'] = kwargs['NormBackgroundPixelRange']
+        norm_kwargs['NormFlag'] = False
+        norm_kwargs['LowResDataAxisPixelRangeFlag'] = kwargs['LowResNormAxisPixelRangeFlag']
+        norm_kwargs['LowResDataAxisPixelRange'] = kwargs['LowResNormAxisPixelRange']
+        norm_kwargs['ApplyScalingFactor'] = False
+        norm_wksp = LiquidsReflectometryReduction(**norm_kwargs)
+
+        wksp = norm_wksp
         self.setProperty('OutputWorkspace', wksp)
 
 AlgorithmFactory.subscribe(LRReductionWithReference)
