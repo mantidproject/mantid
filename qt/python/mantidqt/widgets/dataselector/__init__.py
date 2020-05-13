f0468ad8c551101a64c8e6feecb5b593bb565f77
Guillaume Communie <communieg@ill.eu>
2020-05-13 18:23:51 +0200

    Add the python export of DataSelector widget
    DataSelector widget is now accessible in python through
    mantidqt.widgets.dataselector. For now, only the constructor is
    exported.

---
+++ qt/python/mantidqt/widgets/dataselector/__init__.py
@@ -0,0 +1,15 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+from mantidqt.utils.qt import import_qt
+
+DataSelector = import_qt(
+        '..._common',
+        'mantidqt.widgets.dataselector',
+        'DataSelector'
+        )
+
