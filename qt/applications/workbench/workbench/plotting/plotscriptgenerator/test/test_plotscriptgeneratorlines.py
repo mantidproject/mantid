ca5ef5e1bb736ed8c964e1fd90a0d53ff47f1fc5
Nick Draper <nick.draper@stfc.ac.uk>
2020-05-22 16:01:33 +0100

    Update qt/applications/workbench/workbench/plotting/plotscriptgenerator/test/test_plotscriptgeneratorlines.py
    Co-authored-by: PhilColebrooke <52415735+PhilColebrooke@users.noreply.github.com>

--- qt/applications/workbench/workbench/plotting/plotscriptgenerator/test/test_plotscriptgeneratorlines.py
+++ qt/applications/workbench/workbench/plotting/plotscriptgenerator/test/test_plotscriptgeneratorlines.py
@@ -92,7 +92,7 @@ class PlotScriptGeneratorLinesTest(unittest.TestCase):
     def test_generate_plot_command_returns_correct_string_for_sample_log(self):
         kwargs = copy(LINE2D_KWARGS)
         kwargs["drawstyle"] = 'steps-post'
-        kwargs.update({"LogName":"my_log","ExperimentInfo":0,"Filtered":True})
+        kwargs.update({"LogName": "my_log", "ExperimentInfo": 0, "Filtered": True})
         # add a log
         AddTimeSeriesLog(self.test_ws, Name="my_log", Time="2010-01-01T00:00:00", Value=100)
         AddTimeSeriesLog(self.test_ws, Name="my_log", Time="2010-01-01T00:30:00", Value=15)
