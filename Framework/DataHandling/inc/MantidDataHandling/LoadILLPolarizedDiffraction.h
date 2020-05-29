6aab0655f956633832992c785f48734cc7c91edb
Dominik Arominski <arominski@ill.fr>
2020-05-29 17:56:55 +0200

    Removed unimplemented function, made interfaces consistent,
    removed getInstrumentFilePath function

--- Framework/DataHandling/inc/MantidDataHandling/LoadILLPolarizedDiffraction.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadILLPolarizedDiffraction.h
@@ -6,8 +6,6 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
-#include <utility>
-
 #include "MantidAPI/IFileLoader.h"
 #include "MantidAPI/WorkspaceGroup_fwd.h"
 #include "MantidDataHandling/DllConfig.h"
@@ -16,10 +14,13 @@
 #include "MantidKernel/NexusDescriptor.h"
 #include "MantidNexus/NexusClasses.h"
 
+#include <utility>
+
 namespace Mantid {
 namespace DataHandling {
 
-/** LoadILLPolarizedDiffraction : Loads ILL polarized diffraction nexus files.
+/** LoadILLPolarizedDiffraction : Loads ILL polarized diffraction nexus files
+  from instrument D7.
 
   @date 15/05/20
 */
@@ -33,33 +34,30 @@ public:
   }
   const std::string category() const override;
   const std::string summary() const override;
-  int confidence(Kernel::NexusDescriptor &descriptor) const override;
+  int confidence(Kernel::NexusDescriptor &) const override;
   LoadILLPolarizedDiffraction();
 
 private:
   void init() override;
+  std::map<std::string, std::string> validateInputs() override;
   void exec() override;
-
-  std::string getInstrumentFilePath(const std::string &) const;
-  bool calibrationFileExists(const std::string &filename) const;
 
   API::MatrixWorkspace_sptr initStaticWorkspace();
 
   void loadData();
   void loadMetaData();
-  void loadInstrument(API::MatrixWorkspace_sptr &);
-  std::vector<double> loadTwoThetaDetectors(const API::MatrixWorkspace_sptr &,
-                                            const NeXus::NXEntry &, int bankId);
-  void moveTwoTheta(const NeXus::NXEntry &, API::MatrixWorkspace_sptr &);
+  void loadInstrument(API::MatrixWorkspace_sptr);
+  std::vector<double> loadTwoThetaDetectors(const API::MatrixWorkspace_sptr,
+                                            const NeXus::NXEntry &, int);
+  void moveTwoTheta(const NeXus::NXEntry &, API::MatrixWorkspace_sptr);
 
   size_t m_numberOfChannels; // number of channels data
 
   std::string m_instName;            ///< instrument name to load the IDF
-  std::set<std::string> m_instNames; ///< supported instruments
-  std::string m_filename;            ///< file name to load
+  std::string m_fileName;            ///< file name to load
 
   LoadHelper m_loadHelper;                    ///< a helper for metadata
-  API::WorkspaceGroup_sptr m_outputWorkspace; ///< output workspace
+  API::WorkspaceGroup_sptr m_outputWorkspaceGroup; ///< output workspace
 };
 
 } // namespace DataHandling
