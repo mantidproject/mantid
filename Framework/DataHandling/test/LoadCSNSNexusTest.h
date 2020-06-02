e488a0c1fbcce48182765dc500c704bec2452d1a
Du Rong <durong@ihep.ac.cn>
2020-06-02 09:43:00 +0800

    Revert "addLoadCSNSNexusAlgorithm #12"
    This reverts commit ebd9c3a9720fc8ce0c10c5b0672ec50ac7283825.

--- Framework/DataHandling/test/LoadCSNSNexusTest.h
+++ Framework/DataHandling/test/LoadCSNSNexusTest.h
@@ -1,8 +1,9 @@
 // Mantid Repository : https://github.com/mantidproject/mantid
 //
-// Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
-//     NScD Oak Ridge National Laboratory, European Spallation Source,
-//     Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//      NScD Oak Ridge National Laboratory, European Spallation Source,
+//      Institut Laue - Langevin
+//      &  CSNS, Institute of High Energy Physics，Chinese Academy of Sciences.
 // SPDX - License - Identifier: GPL - 3.0 +
 #ifndef LOADCSNSNEXUSTEST_H_
 #define LOADCSNSNEXUSTEST_H_
