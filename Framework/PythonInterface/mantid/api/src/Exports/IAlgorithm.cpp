9644d6eff85a17b853df810e939a4b22419c76ad
Conor Finn <conor.finn@stfc.ac.uk>
2020-07-17 11:01:38 +0100

    Remove unneeded comment
    RE #29002

--- Framework/PythonInterface/mantid/api/src/Exports/IAlgorithm.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/IAlgorithm.cpp
@@ -362,8 +362,6 @@ boost::python::dict validateInputs(IAlgorithm &self) {
 }
 
 void initializeProxy(IAlgorithm &self) {
-  // Release the GIL to stop workbench freezing while initializing the
-  // algorithms.
   Mantid::PythonInterface::ReleaseGlobalInterpreterLock
       releaseGlobalInterpreterLock;
   self.initialize();
