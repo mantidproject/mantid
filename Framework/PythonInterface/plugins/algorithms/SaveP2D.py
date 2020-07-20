9b91fed071bd2aa3742dca39418913140c6b3aee
Yannick <ymeinerzhagen@web.de>
2020-07-20 10:32:24 +0200

    Changed category for SaveP2D to Diffraction/Data Handling

--- Framework/PythonInterface/plugins/algorithms/SaveP2D.py
+++ Framework/PythonInterface/plugins/algorithms/SaveP2D.py
@@ -15,7 +15,7 @@ from mantid.kernel import Direction
 
 class SaveP2D(PythonAlgorithm):
     def category(self):
-        return 'Tools//Algorithms'
+        return 'Diffraction//Data Handling'
 
     def summary(self):
         """
