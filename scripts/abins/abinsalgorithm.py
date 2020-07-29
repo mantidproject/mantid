14afee3b0753392411c2062479ec9a985a46d49b
Adam J. Jackson <a.j.jackson@physics.org>
2020-07-29 14:14:06 +0100

    Abins pedantry: represent set of options with a set data structure

--- scripts/abins/abinsalgorithm.py
+++ scripts/abins/abinsalgorithm.py
@@ -129,7 +129,7 @@ class AbinsAlgorithm:
 
         workspace_name = self.getPropertyValue("OutputWorkspace")
         # list of special keywords which cannot be used in the name of workspace
-        forbidden_keywords = ["total"]
+        forbidden_keywords = {"total",}
         if workspace_name in mtd:
             issues["OutputWorkspace"] = "Workspace with name " + workspace_name + " already in use; please give " \
                                                                                   "a different name for workspace."
