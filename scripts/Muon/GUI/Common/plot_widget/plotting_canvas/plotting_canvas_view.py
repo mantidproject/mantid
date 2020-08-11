69a834894f26b22df9d14d5ef77024e934dc31c6
Toluwalase Agoro <toluwalase.agoro@stfc.ac.uk>
2020-08-11 10:42:37 +0100

    Fixed plot diff checkbox

--- scripts/Muon/GUI/Common/plot_widget/plotting_canvas/plotting_canvas_view.py
+++ scripts/Muon/GUI/Common/plot_widget/plotting_canvas/plotting_canvas_view.py
@@ -124,7 +124,8 @@ class PlottingCanvasView(QtWidgets.QWidget, PlottingCanvasViewInterface):
             except RuntimeError:
                 continue
             for plotted_information in self._plot_information_list.copy():
-                if workspace_plot_info == plotted_information:
+                if workspace_plot_info.workspace_name == plotted_information.workspace_name and \
+                        workspace_plot_info.axis == plotted_information.axis:
                     axis = self.fig.axes[workspace_plot_info.axis]
                     axis.remove_workspace_artists(workspace)
                     self._plot_information_list.remove(plotted_information)
