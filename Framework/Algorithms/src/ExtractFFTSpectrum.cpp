78170e9b16b9fff5f3af3099a5c5c323f76072b6
Joseph Torsney <joseph.torsney@stfc.ac.uk>
2020-08-12 13:04:44 +0100

    Added additional FFT properties
    Adds properties of the FFT algorithm that were missing from ExtractFFTSpectrum.
    ref#26995

--- Framework/Algorithms/src/ExtractFFTSpectrum.cpp
+++ Framework/Algorithms/src/ExtractFFTSpectrum.cpp
@@ -41,11 +41,24 @@ void ExtractFFTSpectrum::init() {
   declareProperty(std::make_unique<WorkspaceProperty<>>("OutputWorkspace", "",
                                                         Direction::Output),
                   "The output workspace.");
+  declareProperty("Shift", 0.0,
+                  "Apply an extra phase equal to this quantity "
+                  "times 2*pi to the transform");
+  declareProperty("AutoShift", false,
+                  "Automatically calculate and apply phase shift. Zero on the "
+                  "X axis is assumed to be in the centre - if it is not, "
+                  "setting this property will automatically correct for this.");
+  declareProperty("AcceptXRoundingErrors", false,
+                  "Continue to process the data even if X values are not evenly spaced",
+                  Direction::Input);
 }
 
 void ExtractFFTSpectrum::exec() {
   MatrixWorkspace_sptr inputWS = getProperty("InputWorkspace");
   MatrixWorkspace_sptr inputImagWS = getProperty("InputImagWorkspace");
+  const double shift = getProperty("Shift");
+  const bool autoShift = getProperty("AutoShift");
+  const bool xRoundingErrs = getProperty("AcceptXRoundingErrors");
   const int fftPart = getProperty("FFTPart");
   const auto numHists = static_cast<int>(inputWS->getNumberHistograms());
   MatrixWorkspace_sptr outputWS = create<MatrixWorkspace>(*inputWS);
@@ -64,6 +77,9 @@ void ExtractFFTSpectrum::exec() {
                                                   inputImagWS);
       childFFT->setProperty<int>("Imaginary", i);
     }
+    childFFT->setProperty<double>("Shift", shift);
+    childFFT->setProperty<bool>("AutoShift", autoShift);
+    childFFT->setProperty<bool>("AcceptXRoundingErrors", xRoundingErrs);
     childFFT->execute();
     MatrixWorkspace_const_sptr fftTemp =
         childFFT->getProperty("OutputWorkspace");
