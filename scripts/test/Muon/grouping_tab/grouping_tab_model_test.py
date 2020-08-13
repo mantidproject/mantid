c96b03819bb432a0f0f3c17703d11be033243513
Matthew Andrew <matthew.andrew@tessella.com>
2020-08-13 09:32:52 +0100

    Update default groupings for multperiods data Re #29114

---
+++ scripts/test/Muon/grouping_tab/grouping_tab_model_test.py
@@ -0,0 +1,36 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+import unittest
+from unittest import mock
+from Muon.GUI.Common.grouping_tab_widget.grouping_tab_widget_model import GroupingTabModel
+
+
+class GroupingTabModelTest(unittest.TestCase):
+    def setUp(self):
+        self.context = mock.MagicMock()
+        self._num_periods_store = {62260: 4, 62261: 2, 62263: 1}
+        self.context.num_periods = lambda run : self._num_periods_store[run]
+
+    def test_reset_group_and_pairs_to_default_correctly_identifies_minimum_number_of_periods_for_single_run(self):
+        self.context.current_runs = [62260]
+        model = GroupingTabModel(self.context)
+
+        model.reset_groups_and_pairs_to_default()
+
+        self.context.group_pair_context.reset_group_and_pairs_to_default.assert_called_once_with(mock.ANY, mock.ANY, mock.ANY, 4)
+
+    def test_reset_group_and_pairs_to_default_correctly_identifies_minimum_number_of_periods_for_multiple_runs(self):
+        self.context.current_runs = [62260, 62261, 62263]
+        model = GroupingTabModel(self.context)
+
+        model.reset_groups_and_pairs_to_default()
+
+        self.context.group_pair_context.reset_group_and_pairs_to_default.assert_called_once_with(mock.ANY, mock.ANY, mock.ANY, 1)
+
+
+if __name__ == '__main__':
+    unittest.main(buffer=False, verbosity=2)
