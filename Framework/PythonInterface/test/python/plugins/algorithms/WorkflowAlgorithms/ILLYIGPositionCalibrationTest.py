5136696782efc27114db8aca805a898d58195947
Dominik Arominski <arominski@ill.fr>
2020-08-27 10:21:52 +0200

    Updated tests to include new arguments

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ILLYIGPositionCalibrationTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ILLYIGPositionCalibrationTest.py
@@ -5,13 +5,10 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
-from mantid.api import MatrixWorkspace, Run
 from mantid.simpleapi import ILLYIGPositionCalibration, Load, LoadILLPolarizedDiffraction, config, mtd
-from mantid.geometry import Instrument
 
-import numpy as np
-import os
 import xml.etree.ElementTree as ET
+
 
 class ILLYIGPositionCalibrationTest(unittest.TestCase):
 
@@ -30,13 +27,14 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
         Load('402652_403041.nxs', OutputWorkspace='shortWavelengthScan')
         with self.assertRaises(ValueError):
             ILLYIGPositionCalibration(ScanWorkspace='shortWavelengthScan', YIGPeaksFile='YIG_peaks.xml',
-                                      ApproximateWavelength="0.0")
-           
+                                      ApproximateWavelength="-1.0")
+
     def test_shortWavelength(self):
         Load('402652_403041.nxs', OutputWorkspace='shortWavelengthScan')
-        approximate_wavelength = '3.1' # Angstrom
+        approximate_wavelength = '3.14' # Angstrom
         ILLYIGPositionCalibration(ScanWorkspace='shortWavelengthScan', ApproximateWavelength=approximate_wavelength,
                                   YIGPeaksFile='YIG_peaks.xml', CalibrationFilename='test_shortWavelength.xml',
+                                  MinimalDistanceBetweenPeaks=1.75, BankOffsets="-3,-3,1",
                                   DetectorFitOutput='test_shortWavelength')
         self._check_fit_output('test_shortWavelength')
         self._check_load_data_with_calibration('test_shortWavelength.xml')
@@ -46,39 +44,44 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
         approximate_wavelength = '4.8' # Angstrom
         ILLYIGPositionCalibration(ScanWorkspace='intermediateWavelengthScan', ApproximateWavelength=approximate_wavelength,
                                   YIGPeaksFile='YIG_peaks.xml', CalibrationFilename='test_intermediateWavelength.xml',
+                                  MinimalDistanceBetweenPeaks=1.5, BankOffsets="-4,-4,0",
                                   DetectorFitOutput='test_intermediateWavelength')
         self._check_fit_output('test_intermediateWavelength')
         self._check_load_data_with_calibration('test_intermediateWavelength.xml')
-        
+
     def test_longWavelength(self):
         Load('394458_394882.nxs', OutputWorkspace='longWavelengthScan')
         approximate_wavelength = '5.7' # Angstrom
         ILLYIGPositionCalibration(ScanWorkspace='longWavelengthScan', ApproximateWavelength=approximate_wavelength,
                                   YIGPeaksFile='YIG_peaks.xml', CalibrationFilename='test_longWavelength.xml',
+                                  MinimalDistanceBetweenPeaks=1.5, BankOffsets="-3,-3,1",
                                   DetectorFitOutput='test_longWavelength')
         self._check_fit_output('test_longWavelength')
         self._check_load_data_with_calibration('test_longWavelength.xml')
 
     def _check_fit_output(self, fitTableName):
-        """ Checks the TableWorkspace if the output values are reasonable, 
+        """ Checks the TableWorkspace if the output values are reasonable,
         then check if the output IPF can be read by the Loader"""
-        D7_NUMBER_PIXELS = 132        
+        pixels_per_bank = 44
 
         self.assertNotEqual(mtd[fitTableName], None)
-        
-        wavelength = float(mtd[fitTableName].column(1)[1])
-        self.assertAlmostEqual(wavelength, 1.0,  delta=2e-2) # 2 %
-        bank2_slope = float(mtd[fitTableName].column(1)[0])
-        self.assertAlmostEqual(bank2_slope, 1.0, delta=1e-2) # 1 %
-        bank3_slope = float(mtd[fitTableName].column(1)[D7_NUMBER_PIXELS])
-        self.assertAlmostEqual(bank3_slope, 1.0, delta=1e-2) # 1 %
-        bank4_slope = float(mtd[fitTableName].column(1)[D7_NUMBER_PIXELS*2])
-        self.assertAlmostEqual(bank4_slope, 1.0, delta=1e-2) # 1 %
 
-        for pixel_no in range(D7_NUMBER_PIXELS):
-            offset = mtd[fitTableName].column(1)[2+3*pixel_no] * 180.0 / np.pi
-            self.assertAlmostEqual(offset, 0.0, delta=24.0) # +- 24 degrees
-            
+        wavelength = float(mtd[fitTableName].column(1)[1])
+        self.assertAlmostEqual(wavelength, 1.0,  delta=5e-2) # +/- 5 %
+        bank2_slope = 1.0 / float(mtd[fitTableName].column(1)[0])
+        self.assertAlmostEqual(bank2_slope, 1.0, delta=1e-2) # +/- 1 %
+        bank3_slope = 1.0 / float(mtd[fitTableName].column(1)[4*pixels_per_bank])
+        self.assertAlmostEqual(bank3_slope, 1.0, delta=1e-2) # +/- 1 %
+        bank4_slope = 1.0 / float(mtd[fitTableName].column(1)[8*pixels_per_bank])
+        self.assertAlmostEqual(bank4_slope, 1.0, delta=1e-2) # +/- 1 %
+
+        # for pixel_no in range(D7_NUMBER_PIXELS):
+        for row_no in range(mtd[fitTableName].rowCount()):
+            row_data = mtd[fitTableName].row(row_no)
+            if '.offset' in row_data['Name']:
+                offset = row_data['Value']
+                self.assertAlmostEqual(offset, 0.0, delta=24.0) # +- 24 degrees
+
     def _check_load_data_with_calibration(self, ipf_name):
         tree = ET.parse(ipf_name)
         self.assertNotEquals(tree, None)
@@ -86,7 +89,7 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
         positionCalibration = []
         for elem in root:
             for subelem in elem:
-                if 'wavelength' in subelem.attrib['name']:
+                if 'pixel' not in subelem.attrib['name']:
                     continue
                 for value in subelem:
                     positionCalibration.append(float(value.attrib['val']))
@@ -103,8 +106,8 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
         self.assertAlmostEqual(xAxisValues[44], bank_offsets[1]-positionCalibration[44], delta=1e-2)
         self.assertAlmostEqual(xAxisValues[87], bank_offsets[1]-positionCalibration[87], delta=1e-2)
         self.assertAlmostEqual(xAxisValues[88], bank_offsets[2]-positionCalibration[88], delta=1e-2)
-        self.assertAlmostEqual(xAxisValues[131], bank_offsets[2]-positionCalibration[131], delta=1e-2)       
-        
-        
+        self.assertAlmostEqual(xAxisValues[131], bank_offsets[2]-positionCalibration[131], delta=1e-2)
+
+
 if __name__ == '__main__':
     unittest.main()
