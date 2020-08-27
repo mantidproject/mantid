dfad4f3e85fe1153f9438ac69a26bbabb70501f3
Dominik Arominski <arominski@ill.fr>
2020-08-27 13:12:09 +0200

    Corrected test to include new elements of the calibration IPF

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ILLYIGPositionCalibrationTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ILLYIGPositionCalibrationTest.py
@@ -5,12 +5,14 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
-from mantid.simpleapi import ILLYIGPositionCalibration, Load, LoadILLPolarizedDiffraction, config, mtd
-
+from mantid.simpleapi import config, mtd, ILLYIGPositionCalibration, Load, LoadILLPolarizedDiffraction
 import xml.etree.ElementTree as ET
+import math
 
 
 class ILLYIGPositionCalibrationTest(unittest.TestCase):
+
+    _pixels_per_bank = 44
 
     @classmethod
     def setUpClass(cls):
@@ -62,7 +64,6 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
     def _check_fit_output(self, fitTableName):
         """ Checks the TableWorkspace if the output values are reasonable,
         then check if the output IPF can be read by the Loader"""
-        pixels_per_bank = 44
 
         self.assertNotEqual(mtd[fitTableName], None)
 
@@ -70,12 +71,11 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
         self.assertAlmostEqual(wavelength, 1.0,  delta=5e-2) # +/- 5 %
         bank2_slope = 1.0 / float(mtd[fitTableName].column(1)[0])
         self.assertAlmostEqual(bank2_slope, 1.0, delta=1e-2) # +/- 1 %
-        bank3_slope = 1.0 / float(mtd[fitTableName].column(1)[4*pixels_per_bank])
+        bank3_slope = 1.0 / float(mtd[fitTableName].column(1)[4*self._pixels_per_bank])
         self.assertAlmostEqual(bank3_slope, 1.0, delta=1e-2) # +/- 1 %
-        bank4_slope = 1.0 / float(mtd[fitTableName].column(1)[8*pixels_per_bank])
+        bank4_slope = 1.0 / float(mtd[fitTableName].column(1)[8*self._pixels_per_bank])
         self.assertAlmostEqual(bank4_slope, 1.0, delta=1e-2) # +/- 1 %
 
-        # for pixel_no in range(D7_NUMBER_PIXELS):
         for row_no in range(mtd[fitTableName].rowCount()):
             row_data = mtd[fitTableName].row(row_no)
             if '.offset' in row_data['Name']:
@@ -86,27 +86,35 @@ class ILLYIGPositionCalibrationTest(unittest.TestCase):
         tree = ET.parse(ipf_name)
         self.assertNotEquals(tree, None)
         root = tree.getroot()
-        positionCalibration = []
+        bank_offsets = []
+        bank_gradients = []
+        pixel_offsets = []
         for elem in root:
             for subelem in elem:
-                if 'pixel' not in subelem.attrib['name']:
-                    continue
                 for value in subelem:
-                    positionCalibration.append(float(value.attrib['val']))
+                    value = float(value.attrib['val'])
+                    if 'pixel' in subelem.attrib['name']:
+                        pixel_offsets.append(value)
+                    if 'gradient' in subelem.attrib['name']:
+                        bank_gradients.append(value)
+                    if 'offset' in subelem.attrib['name']:
+                        bank_offsets.append(value)
+        positionCalibration = [pixel_offset + bank_offsets[math.floor(pixel_no / self._pixels_per_bank)]
+                               for pixel_no, pixel_offset in enumerate(pixel_offsets)]
         self.assertEquals(len(positionCalibration), 132) # number of pixels
         LoadILLPolarizedDiffraction('401800', OutputWorkspace='output', PositionCalibration='YIGFile',
                                     YIGFilename=ipf_name, ConvertToScatteringAngle=True, TransposeMonochromatic=True)
         self.assertNotEquals('output', None)
-        bank_offsets = [ mtd['output'].getItem(0).getRun().getLogData('2theta.actual_bank{}'.format(bank_no+2)).value
-                         for bank_no in range(3) ]
+        nexus_bank_offsets = [ mtd['output'].getItem(0).getRun().getLogData('2theta.actual_bank{}'.format(bank_no+2)).value
+                               for bank_no in range(3) ]
 
         xAxisValues = mtd['output'].getItem(0).readX(0)
-        self.assertAlmostEqual(xAxisValues[0], bank_offsets[0]-positionCalibration[0], delta=1e-2)
-        self.assertAlmostEqual(xAxisValues[43], bank_offsets[0]-positionCalibration[43], delta=1e-2)
-        self.assertAlmostEqual(xAxisValues[44], bank_offsets[1]-positionCalibration[44], delta=1e-2)
-        self.assertAlmostEqual(xAxisValues[87], bank_offsets[1]-positionCalibration[87], delta=1e-2)
-        self.assertAlmostEqual(xAxisValues[88], bank_offsets[2]-positionCalibration[88], delta=1e-2)
-        self.assertAlmostEqual(xAxisValues[131], bank_offsets[2]-positionCalibration[131], delta=1e-2)
+        self.assertAlmostEqual(xAxisValues[0], bank_gradients[0]*nexus_bank_offsets[0]-positionCalibration[0], delta=1e-2)
+        self.assertAlmostEqual(xAxisValues[43], bank_gradients[0]*nexus_bank_offsets[0]-positionCalibration[43], delta=1e-2)
+        self.assertAlmostEqual(xAxisValues[44], bank_gradients[1]*nexus_bank_offsets[1]-positionCalibration[44], delta=1e-2)
+        self.assertAlmostEqual(xAxisValues[87], bank_gradients[1]*nexus_bank_offsets[1]-positionCalibration[87], delta=1e-2)
+        self.assertAlmostEqual(xAxisValues[88], bank_gradients[2]*nexus_bank_offsets[2]-positionCalibration[88], delta=1e-2)
+        self.assertAlmostEqual(xAxisValues[131], bank_gradients[2]*nexus_bank_offsets[2]-positionCalibration[131], delta=1e-2)
 
 
 if __name__ == '__main__':
