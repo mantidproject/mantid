b4495492aa87514c400b081a6494759d181df7df
Cole Kendrick <kendrickcj@ornl.gov>
2020-09-15 10:07:10 -0600

    Added docs to functions, renamed variables

--- Framework/PythonInterface/plugins/algorithms/ClipPeaks.py
+++ Framework/PythonInterface/plugins/algorithms/ClipPeaks.py
@@ -66,8 +66,15 @@ class ClipPeaks(PythonAlgorithm):
         """
         This smooths data based on linear weighted average around
         point i for example for an order of 7 the i point is
-        weighted 4, i=/- 1 weighted 3, i+/-2 weighted 2 and i+/-3
-        weighted 1 this input is only the y values
+        weighted 4, i=+/- 1 weighted 3, i+/-2 weighted 2 and i+/-3
+        weighted 1 this input is only the y values. See the equation below:
+
+        Y_{i}^{s} = \\frac{ \\sum_{r=-r_{max}}^{r_{max}} (r_{max} - \\vert r \\vert ) \\cdot Y_{i+r} }
+                         { \\sum_{r=-r_{max}}^{r_{max}} (r_{max} - \\vert r \\vert ) }
+        where r_{max} is the "order" parameter (smoothing factor).
+
+        :param data: Input numpy.ndarray to smooth (Y data of the input workspace).
+        :param order: Smoothing factor, assumed an odd integer (even factors will be rounded to the next odd).
         """
         sm = np.zeros(len(data))
         factor = int(order / 2) + 1
@@ -84,22 +91,35 @@ class ClipPeaks(PythonAlgorithm):
 
     def log_log_sqrt_transformation(self, input):
         """
-        this transforms data to be more sensitive to weak peaks. The
-        function is reversed by the Inv_LLS function below
+        This transforms data to be more sensitive to weak peaks. The
+        function is reversed by the inv_log_log_sqrt_transformation function below
         """
-        out = np.log(np.log((input + 1)**0.5 + 1) + 1)
+        out = np.log(np.log((input + 1) ** 0.5 + 1) + 1)
 
         return out
 
     def inv_log_log_sqrt_transformation(self, input):
         """
-        See Function LLS function above
+        Applies the inverse log-log-sqrt function on the input data. See Function LLS function above
         """
-        out = (np.exp(np.exp(input) - 1) - 1)**2 - 1
+        out = (np.exp(np.exp(input) - 1) - 1) ** 2 - 1
 
         return out
 
-    def peak_clip(self, data, win=30, decrease=True, LLS=True, smooth_window=0):
+    def peak_clip(self, data, win=30, decrease=True, lls=True, smooth_window=0):
+        """
+        Performs peak clipping on the data iteratively over the size of win. The input data can be optionally
+        smoothed if smooth_window is greater than 0. The LLS function can be performed on the data after smoothing
+        to help exaggerate smaller peaks present in the data before clipping. The peak clipping window used for
+        iteration is done with a decreasing window unless decrease is False.
+
+        :param data: Input (Y) data to clip.
+        :param win: Window size of the clipping (controls the iteration count of the clipping function).
+        :param decrease: Flag to determine if an increasing/decreasing window is applied to the data.
+        :param lls: Flag to enable/disable applying the LLS function to data before clipping.
+        :param smooth_window: Smoothing factor for the smoothing function. 0 to apply no smoothing.
+        :return: np.ndarray of peak clipped data
+        """
         start_data = np.copy(data)
 
         window = win
@@ -108,27 +128,27 @@ class ClipPeaks(PythonAlgorithm):
         if smooth_window > 0:
             data = self.smooth(data, smooth_window)
 
-        if LLS:
+        if lls:
             data = self.log_log_sqrt_transformation(data)
 
         temp = data.copy()
 
         if decrease:
-            scan = list(range(window + 1, 0, -1))
+            scan = range(window + 1, 0, -1)
         else:
-            scan = list(range(1, window + 1))
+            scan = range(1, window + 1)
 
         for w in scan:
             for i in range(len(temp)):
                 if i < w or i > (len(temp) - w - 1):
                     continue
                 else:
-                    win_array = temp[i - w:i + w + 1].copy()
+                    win_array = temp[i - w:i + w + 1]
                     win_array_reversed = win_array[::-1]
                     average = (win_array + win_array_reversed) / 2
                     temp[i] = np.min(average[:int(len(average) / 2)])
 
-        if LLS:
+        if lls:
             temp = self.inv_log_log_sqrt_transformation(temp)
 
         self.log().information('Minimum of starting data - peak clipped data: {0}'.format(str(min(start_data - temp))))
@@ -144,38 +164,41 @@ class ClipPeaks(PythonAlgorithm):
         return output
 
     def PyExec(self):
-        # Retrieve all relevant notice
+        """
+        Creates a clone of the input workspace to be used for the output, then performs the peak clipping algorithm
+        on each histogram using the specified options. The X and E data of the input remain unmodified in the
+        output workspace.
+        """
 
-        WS = self.getProperty("InputWorkspace").value
+        input_ws = self.getProperty("InputWorkspace").value
 
         window = self.getProperty("WindowSize").value
 
         smooth_range = self.getProperty("SmoothingRange").value
 
-        LLS_set = self.getProperty("LLSCorrection").value
+        lls_set = self.getProperty("LLSCorrection").value
 
         decreasing = self.getProperty("IncreasingWindow").value
 
-        n_histo = WS.getNumberHistograms()
+        n_histo = input_ws.getNumberHistograms()
 
-        peak_clip_WS = CloneWorkspace(WS, OutputWorkspace=self.getPropertyValue("OutputWorkspace"))
+        peak_clip_ws = CloneWorkspace(input_ws, OutputWorkspace=self.getPropertyValue("OutputWorkspace"))
 
-        x = WS.extractX()
-        y = WS.extractY()
-        e = WS.extractE()
+        x = input_ws.extractX()
+        y = input_ws.extractY()
+        e = input_ws.extractE()
 
         for h in range(n_histo):
-
-            peak_clip_WS.setX(h, x[h])
-            peak_clip_WS.setY(
+            peak_clip_ws.setX(h, x[h])
+            peak_clip_ws.setY(
                 h,
                 self.peak_clip(y[h],
                                win=window,
                                decrease=decreasing,
-                               LLS=LLS_set,
+                               lls=lls_set,
                                smooth_window=smooth_range))
-            peak_clip_WS.setE(h, e[h])
+            peak_clip_ws.setE(h, e[h])
 
-        self.setProperty("OutputWorkspace", peak_clip_WS)
+        self.setProperty("OutputWorkspace", peak_clip_ws)
 
 AlgorithmFactory.subscribe(ClipPeaks)
