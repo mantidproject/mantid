56ec1fea7409ec0eed840ad90d683e4fd5214fe1
Guillaume Communie <communieg@ill.eu>
2020-09-15 14:26:02 +0200

    [SANSILL] bug in validate inputs
    During the check of array like parameter, Validateinput() was reporting
    a wrong number of parameter (number of comma instead of number of
    values). There was also a typo in one of the parameter check.
    
    This commit rewrites the method to check the number of values instead
    of the number of commas.

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SANSILLAutoProcess.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SANSILLAutoProcess.py
@@ -116,60 +116,74 @@ class SANSILLAutoProcess(DataProcessorAlgorithm):
         return 'SANSILLAutoProcess'
 
     def validateInputs(self):
+        self.setUp()
+
         result = dict()
-        message = 'Wrong number of {0} runs: {1}. Provide one or as many as sample runs: {2}.'
+        message = 'Wrong number of {0} runs: {1}. Provide one or as many as '\
+                  'sample runs: {2}.'
         message_value = 'Wrong number of {0} values: {1}. Provide one or as ' \
                         'many as sample runs: {2}.'
-        tr_message = 'Wrong number of {0} runs: {1}. Provide one or multiple runs summed with +.'
-        sample_dim = self.getPropertyValue('SampleRuns').count(',')
-        abs_dim = self.getPropertyValue('AbsorberRuns').count(',')
-        beam_dim = self.getPropertyValue('BeamRuns').count(',')
-        flux_dim = self.getPropertyValue('FluxRuns').count(',')
-        can_dim = self.getPropertyValue('ContainerRuns').count(',')
-        str_dim = self.getPropertyValue('SampleTransmissionRuns').count(',')
-        ctr_dim = self.getPropertyValue('ContainerTransmissionRuns').count(',')
-        btr_dim = self.getPropertyValue('TransmissionBeamRuns').count(',')
-        atr_dim = self.getPropertyValue('TransmissionAbsorberRuns').count(',')
-        mask_dim = self.getPropertyValue('MaskFiles').count(',')
-        sens_dim = self.getPropertyValue('SensitivityMaps').count(',')
-        ref_dim = self.getPropertyValue('ReferenceFiles').count(',')
-        maxqxy_dim = self.getPropertyValue('MaxQxy').count(',')
-        deltaq_dim = self.getPropertyValue('DeltaQ').count(',')
-        output_type = self.getPropertyValue('OutputType')
-        n_wedges = self.getProperty('NumberOfWedges').value
+        tr_message = 'Wrong number of {0} runs: {1}. Provide one or multiple ' \
+                     'runs summed with +.'
+
+        # array parameters checks
+        sample_dim = len(self.sample)
+        abs_dim = len(self.absorber)
+        beam_dim = len(self.beam)
+        flux_dim = len(self.flux)
+        can_dim = len(self.container)
+        mask_dim = len(self.mask)
+        sens_dim = len(self.sensitivity)
+        ref_dim = len(self.reference)
+        maxqxy_dim = len(self.maxqxy)
+        deltaq_dim = len(self.deltaq)
         if self.getPropertyValue('SampleRuns') == '':
             result['SampleRuns'] = 'Please provide at least one sample run.'
-        if abs_dim != sample_dim and abs_dim != 0:
-            result['AbsorberRuns'] = message.format('Absorber', abs_dim, sample_dim)
-        if beam_dim != sample_dim and beam_dim != 0:
+        if abs_dim != sample_dim and abs_dim > 1:
+            result['AbsorberRuns'] = \
+                message.format('Absorber', abs_dim, sample_dim)
+        if beam_dim != sample_dim and beam_dim > 1:
             result['BeamRuns'] = message.format('Beam', beam_dim, sample_dim)
-        if can_dim != sample_dim and can_dim != 0:
-            result['ContainerRuns'] = message.format('Container', can_dim, sample_dim)
-        if str_dim != 0:
-            result['SampleTransmissionRuns'] = tr_message.format('SampleTransmission', str_dim)
-        if ctr_dim != 0:
-            result['ContainerTransmissionRuns'] = tr_message.format('ContainerTransmission', ctr_dim)
-        if btr_dim != 0:
-            result['TransmissionBeamRuns'] = tr_message.format('TransmissionBeam', btr_dim)
-        if atr_dim != 0:
-            result['TransmissionAbsorberRuns'] = tr_message.format('TransmissionAbsorber', atr_dim)
-        if mask_dim != sample_dim and mask_dim != 0:
+        if can_dim != sample_dim and can_dim > 1:
+            result['ContainerRuns'] = \
+                message.format('Container', can_dim, sample_dim)
+        if mask_dim != sample_dim and mask_dim > 1:
             result['MaskFiles'] = message.format('Mask', mask_dim, sample_dim)
-        if ref_dim != sample_dim and ref_dim != 0:
-            result['ReferenceFiles'] = message.format('Reference', ref_dim, sample_dim)
-        if sens_dim != sample_dim and sens_dim != 0:
-            result['SensitivityMaps'] = message.format('Sensitivity', sens_dim, sample_dim)
-        if flux_dim != flux_dim and flux_dim != 0:
+        if ref_dim != sample_dim and ref_dim > 1:
+            result['ReferenceFiles'] = \
+                    message.format('Reference', ref_dim, sample_dim)
+        if sens_dim != sample_dim and sens_dim > 1:
+            result['SensitivityMaps'] = \
+                    message.format('Sensitivity', sens_dim, sample_dim)
+        if flux_dim != sample_dim and flux_dim > 1:
             result['FluxRuns'] = message.format('Flux')
-        if maxqxy_dim != sample_dim and maxqxy_dim != 0:
-            result['MaxQxy'] = message_value.format('MaxQxy',
-                                                    maxqxy_dim,
-                                                    sample_dim)
-        if deltaq_dim != sample_dim and deltaq_dim != 0:
-            result['DeltaQ'] = message_value.format('DeltaQ',
-                                                    deltaq_dim,
-                                                    sample_dim)
-        if output_type == 'I(Phi,Q)' and n_wedges == 0:
+        if maxqxy_dim != sample_dim and maxqxy_dim > 1:
+            result['MaxQxy'] = \
+                message_value.format('MaxQxy', maxqxy_dim, sample_dim)
+        if deltaq_dim != sample_dim and deltaq_dim > 1:
+            result['DeltaQ'] = \
+                message_value.format('DeltaQ', deltaq_dim, sample_dim)
+
+        # transmission runs checks
+        str_dim = len(self.stransmission.split(','))
+        ctr_dim = len(self.ctransmission.split(','))
+        btr_dim = len(self.btransmission.split(','))
+        atr_dim = len(self.atransmission.split(','))
+        if str_dim > 1:
+            result['SampleTransmissionRuns'] = \
+                tr_message.format('SampleTransmission', str_dim)
+        if ctr_dim > 1:
+            result['ContainerTransmissionRuns'] = \
+                tr_message.format('ContainerTransmission', ctr_dim)
+        if btr_dim > 1:
+            result['TransmissionBeamRuns'] = \
+                tr_message.format('TransmissionBeam', btr_dim)
+        if atr_dim > 1:
+            result['TransmissionAbsorberRuns'] = \
+                tr_message.format('TransmissionAbsorber', atr_dim)
+
+        # other checks
+        if self.output_type == 'I(Phi,Q)' and self.n_wedges == 0:
             result['NumberOfWedges'] = "For I(Phi,Q) processing, the number " \
                                        "of wedges must be different from 0."
 
