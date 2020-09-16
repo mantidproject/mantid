02df04a4a627c264a0f1e61686a2b8d07a0b28b5
Stephen <stephen.smith@stfc.ac.uk>
2020-09-16 13:20:20 +0100

    Adjust color queue behavior
    This commit moves some things around in the color queue so that it doesn't depend on the matplotlib colorc ycle

--- scripts/Muon/GUI/Common/plot_widget/plotting_canvas/plot_color_queue.py
+++ scripts/Muon/GUI/Common/plot_widget/plotting_canvas/plot_color_queue.py
@@ -4,36 +4,49 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
-
 # See https://docs.python.org/3/library/heapq.html for details on heapq
 import heapq
 
 
 class ColorQueue(object):
-    def __init__(self):
-        self._queue = []
-        self._max_index = 0
-
-    def __call__(self):
-        if len(self._queue) != 0:
-            return 'C' + str(heapq.heappop(self._queue))
-        else:
-            return_value = 'C' + str(self._max_index)
-            self._max_index = self._max_index + 1
-            return return_value
-
-    def __add__(self, color):
-        # Could use rexp to do these checks but feels
-        # like overkill in this case.
-        if color[0] != 'C':
-            return self
-        try:
-            colour_index = int(color[1:])
-        except ValueError:
-            return self
-        heapq.heappush(self._queue, colour_index)
-        return self
+    def __init__(self, color_list):
+        """Create a ColorQueue based on an input color_list
+        The entry at index 0 has highest priority
+        color_list could be an input list of color codes
+        e.g ['C0', 'C1', 'C2']
+        or a list of hex colors
+        e.g [#000000,#FF0000....]
+        or rgb values
+        e.g [(0, 0, 0), (255, 0, 0)..]
+        All the above are supported by matplotlib
+        """
+        self._color_cache = {color_list[i]: i for i in range(len(color_list))}
+        self._initialise_queue()
 
     def reset(self):
-        self._max_index = 0
+        """Reset the queue based on the stored color_cache"""
+        self._initialise_queue()
+
+    def __call__(self):
+        """Pop the highest priority color off the queue"""
+        if len(self._queue) == 0:
+            self._initialise_queue()
+        return heapq.heappop(self._queue)[1]
+
+    def __add__(self, color):
+        """Add a color into the queue, with its priority found from the stored cache
+        If its a duplicate color, it will be ignored"""
+        if not any(self._queue[i][1] == color for i in range(len(self._queue))):
+            heapq.heappush(self._queue, (self._get_color_priority(color), color))
+        return self
+
+    def _initialise_queue(self):
+        """Initialize the heap queue with the color_cache"""
         self._queue = []
+        for color, priority in self._color_cache.items():
+            heapq.heappush(self._queue, (priority, color))
+
+    def _get_color_priority(self, color):
+        """Return the priority of a color, if it is not present in the cache
+        set the priority so that it is in the back of the queue"""
+        return self._color_cache.get(color, max(len(self._queue), len(self._color_cache)) + 1)
--- scripts/test/Muon/plotting_canvas/plot_color_queue_test.py
+++ scripts/test/Muon/plotting_canvas/plot_color_queue_test.py
@@ -7,29 +7,43 @@
 import unittest
 from Muon.GUI.Common.plot_widget.plotting_canvas.plot_color_queue import ColorQueue
 
+EXAMPLE_COLOR_QUEUE = ["C" + str(i) for i in range(10)]
 
-class PlottingCanvasPresenterTest(unittest.TestCase):
+
+class PlotColorQueueTest(unittest.TestCase):
 
     def setUp(self):
-        self.color_queue = ColorQueue()
+        self.color_queue = ColorQueue(EXAMPLE_COLOR_QUEUE)
 
     def test_color_queue_returns_expected_colors_when_none_added(self):
-        for i in range(27):
+        for i in range(10):
             self.assertEqual('C' + str(i), self.color_queue())
 
-    def test_color_queue_returns_top_queue_when_colors_added(self):
-        self.color_queue += 'C5'
-        self.color_queue += 'C7'
-
-        self.assertEqual('C5', self.color_queue())
-        self.assertEqual('C7', self.color_queue())
+    def test_color_queue_repeats_cycle_when_out_of_range(self):
+        for i in range(10):
+            self.assertEqual('C' + str(i), self.color_queue())
         self.assertEqual('C0', self.color_queue())
 
-    def test_that_color_queue_ignores_invalid_additions(self):
-        self.color_queue += 'fC5'
-        self.color_queue += 'hello'
+    def test_color_queue_returns_when_colors_popped_and_pushed(self):
+        # pop the first two colors
+        self.color_queue()
+        self.color_queue()
+        self.color_queue()
+        # Add first color back in, it should be top of queue
+        self.color_queue += EXAMPLE_COLOR_QUEUE[0]
 
         self.assertEqual('C0', self.color_queue())
+        # The next color in the cycle
+        self.assertEqual('C3', self.color_queue())
+
+    def test_add_unknown_color_to_queue_adds_at_back(self):
+        self.color_queue += "test"
+        self.assertEqual('test', self.color_queue._queue[-1][1])
+
+    def test_add_color_ignores_duplicates(self):
+        self.color_queue += EXAMPLE_COLOR_QUEUE[0]
+        self.assertEqual('C0', self.color_queue())
+        self.assertEqual('C1', self.color_queue())
 
     def test_that_reset_functions_correctly(self):
         self.color_queue()
