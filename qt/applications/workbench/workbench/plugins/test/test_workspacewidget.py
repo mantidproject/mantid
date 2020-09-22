09fd35bb1caf8f289e10629822e9158893913116
Wenduo Zhou <zhouw@ornl.gov>
2020-09-22 14:23:22 -0400

    Refs #29325. Added test.

--- qt/applications/workbench/workbench/plugins/test/test_workspacewidget.py
+++ qt/applications/workbench/workbench/plugins/test/test_workspacewidget.py
@@ -78,6 +78,12 @@ class WorkspaceWidgetTest(unittest.TestCase, QtWidgetFinder):
         self.ws_widget._do_plot_spectrum([self.ws_names[0]], False, False)
         mock_plot_from_names.assert_called_once_with([self.ws_names[0]], False, False, advanced=False)
 
+    @mock.patch('workbench.plugins.workspacewidget.plot_from_names', autospec=True)
+    def test_plot_with_1d_mdhistoworkspace(self, mock_plot_from_names):
+        self.ws_widget._do_plot_1d_md([self.ws_names[0]], False, False)
+        print(f'DEBUG: mock_plot_from_names: {mock_plot_from_names} of type {type(mock_plot_from_names)}')
+        mock_plot_from_names.assert_called_once_with([self.ws_names[0]], False, False, advanced=False)
+
     @mock.patch('workbench.plugins.workspacewidget.pcolormesh', autospec=True)
     def test_plot_with_plot_colorfill(self, mock_plot_colorfill):
         self.ws_widget._do_plot_colorfill([self.ws_names[0]])
