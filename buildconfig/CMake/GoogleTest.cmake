30b5cc164f2908dd4490e040e82520cf4e76f559
Danny Hindson <danny.hindson@stfc.ac.uk>
2020-09-24 10:52:27 +0100

    Fix build failure in gmock
    Build was failing with USE_SANITIZER=off because had assumed
    that compile option was always populated

--- buildconfig/CMake/GoogleTest.cmake
+++ buildconfig/CMake/GoogleTest.cmake
@@ -37,7 +37,7 @@ foreach( target_var gmock gtest gmock_main gtest_main )
 endforeach()
 
 # W4 logging doesn't work with MSVC address sanitizer, turn off sanitizer since not our code
-if(MSVC)
+if(MSVC AND (USE_SANITIZERS_LOWER STREQUAL "address"))
     get_target_property(opts gmock COMPILE_OPTIONS)
     string(REPLACE "/fsanitize=address" "" opts ${opts})
     set_property(TARGET gmock PROPERTY COMPILE_OPTIONS ${opts})
