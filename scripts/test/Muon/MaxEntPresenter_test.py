f74b4b2251b4d29e213314d14b9c82f63f729f27
Matt Cumber <matthew.cumber@stfc.ac.uk>
2020-09-25 16:09:05 +0100

    Re #29565 Reworking tests
    All tests now pass

--- scripts/test/Muon/MaxEntPresenter_test.py
+++ scripts/test/Muon/MaxEntPresenter_test.py
@@ -7,7 +7,6 @@
 import unittest
 
 from unittest import mock
-from Muon.GUI.Common.utilities import load_utils
 from Muon.GUI.Common import thread_model
 from Muon.GUI.Common.test_helpers.context_setup import setup_context
 from Muon.GUI.FrequencyDomainAnalysis.MaxEnt import maxent_presenter_new
@@ -15,16 +14,14 @@ from Muon.GUI.FrequencyDomainAnalysis.MaxEnt import maxent_view_new
 from Muon.GUI.FrequencyDomainAnalysis.MaxEnt import maxent_model
 
 
-def test(inputs):
-    inputs["OutputPhaseTable"] = "test"
-
 class MaxEntPresenterTest(unittest.TestCase):
     def setUp(self):
         self.context = setup_context(True)
         self.context.data_context.instrument = 'MUSR'
-
         self.context.gui_context.update({'RebinType': 'None'})
-        self.model=mock.create_autospec(maxent_model.MaxEntWrapper, spec_set=True)
+
+        # Model
+        self.model = mock.create_autospec(maxent_model.MaxEntWrapper, spec_set=True)
         self.model.cancel = mock.Mock()
 
         # View
@@ -37,12 +34,16 @@ class MaxEntPresenterTest(unittest.TestCase):
         self.view.addItems = mock.MagicMock()
         self.view.deactivateCalculateButton = mock.Mock()
         self.view.activateCalculateButton = mock.Mock()
-        self.view.input_workspace = mock.Mock(return_value="TEST0000001")
-
-        # Load
-        self.load=mock.create_autospec(load_utils.LoadUtils,spec_set=True)
-        self.load.getCurrentWS=mock.Mock(return_value=["TEST00000001",["fwd","bkwd"]])
-        self.load.hasDataChanged = mock.MagicMock(return_value=False)
+        self.view.input_workspace = "TEST00000001"
+        self.view.phase_table = "Construct"
+        self.view.num_points = 2048
+        self.view.inner_iterations = 10
+        self.view.outer_iterations = 10
+        self.view.double_pulse = False
+        self.view.lagrange_multiplier = 1
+        self.view.maximum_field = 1000
+        self.view.maximum_entropy_constant = 0.1
+        self.view.fit_dead_times = False
 
         # Presenter
         self.presenter = maxent_presenter_new.MaxEntPresenter(self.view, self.context)
@@ -72,18 +73,19 @@ class MaxEntPresenterTest(unittest.TestCase):
         self.presenter.deactivate()
         self.assertEqual(1,self.view.deactivateCalculateButton.call_count)
 
+    def test_clear(self):
+        self.presenter.clear()
+
+        self.assertEqual(2,self.view.addItems.call_count)
+        self.assertEqual(2,self.view.update_phase_table_combo.call_count)
+        self.assertRaises(KeyError,lambda: self.presenter.get_parameters_for_maxent_calculation()['InputPhaseTable'])
+
     def test_cancel(self):
         self.presenter.maxent_alg = self.model
-        self.presenter.get_parameters_for_maxent_calculation = mock.MagicMock(return_value={'InputWorkspace':'TEST000001'})
 
-        self.presenter.createThread = lambda *args: self.thread
-        self.presenter.calculation_started_notifier = mock.MagicMock()
-
-        self.presenter.handleMaxEntButton()
         self.presenter.cancel()
-        #self.presenter.handleFinished()
 
-        self.assertEqual(1,self.view.activateCalculateButton.call_count)
+        self.assertEqual(1,self.model.cancel.call_count)
 
     def test_maxent_button(self):
         self.presenter.createThread = lambda *args:self.thread
@@ -103,7 +105,7 @@ class MaxEntPresenterTest(unittest.TestCase):
 
         self.assertEqual(1, self.view.activateCalculateButton.call_count)
         self.assertEqual(1, self.presenter.calculation_finished_notifier.notify_subscribers.call_count)
-        self.presenter.calculation_finished_notifier.notify_subscribers.called_with("TEST000001")
+        self.presenter.calculation_finished_notifier.notify_subscribers.assert_called_with("TEST0000001")
 
     def test_handle_error(self):
         self.presenter.handle_error("Error message")
@@ -111,87 +113,6 @@ class MaxEntPresenterTest(unittest.TestCase):
         self.assertEqual(1, self.view.activateCalculateButton.call_count)
         self.assertEqual(1, self.view.warning_popup.call_count)
         self.view.warning_popup.assert_called_with("Error message")
-
-    # def test_dataHasChanged(self):
-    #     self.load.hasDataChanged = mock.MagicMock(return_value=True)
-    #     self.presenter.handleMaxEntButton()
-    #     assert(self.view.initMaxEntInput.call_count==0)
-    #
-    # def test_activateButton(self):
-    #     self.presenter.activate()
-    #     assert(self.view.activateCalculateButton.call_count==1)
-    #
-    # def test_deactivateButton(self):
-    #     self.presenter.deactivate()
-    #     assert(self.view.deactivateCalculateButton.call_count==1)
-    #
-    # def test_updatePhaseOptions(self):
-    #     self.view.addOutputPhases = mock.MagicMock(side_effect = test)
-    #     self.view.addPhaseTableToGUI = mock.MagicMock()
-    #     self.presenter.thread = mock.MagicMock()
-    #     self.presenter.phaseTableAdded = mock.Mock(return_value = True)
-    #     self.view.getPhaseTableIndex = mock.Mock(return_value = 2)
-    #     self.view.setPhaseTableIndex = mock.Mock()
-    #     self.view.getPhaseTableOptions = mock.Mock(return_value = [])
-    #
-    #     inputs = {}
-    #     self.view.addOutputPhases(inputs)
-    #     self.presenter.handleFinished()
-    #
-    #     self.assertEqual(inputs["OutputPhaseTable"],"test")
-    #     self.assertEqual(self.view.getPhaseTableOptions.call_count, 1)
-    #     self.assertEqual(self.view.getPhaseTableIndex.call_count, 1)
-    #     self.assertEqual(self.view.addPhaseTableToGUI.call_count, 1)
-    #     self.assertEqual(self.view.setPhaseTableIndex.call_count, 1)
-    #     self.view.setPhaseTableIndex.assert_called_with(2)
-    #
-    # def test_phaseOptionAlreadyExists(self):
-    #     self.view.addOutputPhases = mock.MagicMock(side_effect = test)
-    #     self.view.addPhaseTableToGUI = mock.MagicMock()
-    #     self.presenter.thread = mock.MagicMock()
-    #     self.presenter.phaseTableAdded = mock.Mock(return_value = True)
-    #     self.view.getPhaseTableIndex = mock.Mock(return_value = 2)
-    #     self.view.setPhaseTableIndex = mock.Mock()
-    #     self.view.getPhaseTableOptions = mock.Mock(return_value = ["test"])
-    #
-    #     inputs = {}
-    #     self.view.addOutputPhases(inputs)
-    #     self.presenter.handleFinished()
-    #
-    #     self.assertEqual(inputs["OutputPhaseTable"],"test")
-    #     self.assertEqual(self.view.getPhaseTableOptions.call_count, 1)
-    #     self.assertEqual(self.view.getPhaseTableIndex.call_count, 0)
-    #     self.assertEqual(self.view.addPhaseTableToGUI.call_count, 0)
-    #     self.assertEqual(self.view.setPhaseTableIndex.call_count, 0)
-    #
-    # def test_noUpdatePhaseOptions(self):
-    #     self.view.addOutputPhases = mock.MagicMock(side_effect = test)
-    #     self.view.addPhaseTableToGUI = mock.MagicMock()
-    #     self.presenter.thread = mock.MagicMock()
-    #     self.presenter.phaseTableAdded = mock.Mock(return_value = False)
-    #     self.view.getPhaseTableIndex = mock.Mock(return_value = 2)
-    #     self.view.setPhaseTableIndex = mock.Mock()
-    #     self.view.getPhaseTableOptions = mock.Mock(return_value = [])
-    #
-    #     inputs = {}
-    #     self.view.addOutputPhases(inputs)
-    #     self.presenter.handleFinished()
-    #
-    #     self.assertEqual(inputs["OutputPhaseTable"],"test")
-    #     self.assertEqual(self.view.getPhaseTableOptions.call_count, 1)
-    #     self.assertEqual(self.view.getPhaseTableIndex.call_count, 0)
-    #     self.assertEqual(self.view.addPhaseTableToGUI.call_count, 0)
-    #     self.assertEqual(self.view.setPhaseTableIndex.call_count, 0)
-    #
-    # def test_cancel(self):
-    #     return
-    #     self.presenter.createThread = lambda *args: self.thread
-    #
-    #     self.presenter.handleMaxEntButton()
-    #     self.presenter.cancel()
-    #     #self.assertEqual(self.presenter.handleFinished().call_count, 1)
-    #     #self.assertEqual(1,self.view.activateCalculateButton.call_count)
-
 
 if __name__ == '__main__':
     unittest.main()
