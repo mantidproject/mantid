d5de408362e4fd350fd10103d924d520ab87b330
Ross Whitfield <whitfieldre@ornl.gov>
2020-09-30 12:43:08 -0400

    Remove inelastic option

--- Framework/Algorithms/inc/MantidAlgorithms/AbsorptionCorrectionPaalmanPings.h
+++ Framework/Algorithms/inc/MantidAlgorithms/AbsorptionCorrectionPaalmanPings.h
@@ -85,20 +85,6 @@ public:
   /// Algorithm's version
   int version() const override { return (1); }
 
-protected:
-  /** A virtual function in which additional properties of an algorithm should
-   * be declared.
-   *  Called by init().
-   */
-  API::MatrixWorkspace_sptr m_inputWS;     ///< A pointer to the input workspace
-  const Geometry::IObject *m_sampleObject; ///< Local cache of sample object.
-  Kernel::V3D m_beamDirection;             ///< The direction of the beam.
-  std::vector<double> m_L1s,               ///< Cached L1 distances
-      m_elementVolumes;                    ///< Cached element volumes
-  std::vector<Kernel::V3D> m_elementPositions; ///< Cached element positions
-  size_t m_numVolumeElements; ///< The number of volume elements
-  double m_sampleVolume;      ///< The total volume of the sample
-
 private:
   /// Initialisation code
   void init() override;
@@ -120,6 +106,14 @@ private:
                               const size_t startIndex,
                               const size_t endIndex) const;
 
+  API::MatrixWorkspace_sptr m_inputWS;     ///< A pointer to the input workspace
+  const Geometry::IObject *m_sampleObject; ///< Local cache of sample object.
+  Kernel::V3D m_beamDirection;             ///< The direction of the beam.
+  std::vector<double> m_L1s,               ///< Cached L1 distances
+      m_elementVolumes;                    ///< Cached element volumes
+  std::vector<Kernel::V3D> m_elementPositions; ///< Cached element positions
+  size_t m_numVolumeElements; ///< The number of volume elements
+  double m_sampleVolume;      ///< The total volume of the sample
   Kernel::Material m_material;
   double m_linearCoefTotScatt; ///< The total scattering cross-section in 1/m
   int64_t m_num_lambda; ///< The number of points in wavelength, the rest is
