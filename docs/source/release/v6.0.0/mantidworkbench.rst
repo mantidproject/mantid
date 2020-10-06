b86c48afb6dceb432b5235c6adf01673b3184edc
Stephen <stephen.smith@stfc.ac.uk>
2020-10-06 12:43:21 +0100

    Create plot interaction helper class
    This class handles the plotting of individual functions for the fit property browser. In this commit the class is added along with unit tests

--- docs/source/release/v6.0.0/mantidworkbench.rst
+++ docs/source/release/v6.0.0/mantidworkbench.rst
@@ -9,6 +9,7 @@ Improvements
 ############
 
 - Enable workbench to plot 1D spectrum from an IMDHistoWorkspace whose non-integrated dimension is equal to 1 but not to launch slice viewer.
+- Enabled the plotting of individual functions in the Workbench fit browser. This replicates a feature that was in MantidPlot
 
 Bugfixes
 ########
---
+++ qt/python/mantidqt/widgets/fitpropertybrowser/fitpropertybrowserplotinteraction.py
@@ -0,0 +1,283 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2017 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantidqt package
+#
+#
+import typing
+
+from PyQt5 import sip
+from qtpy.QtCore import Qt, Signal, Slot, QObject
+
+import matplotlib.pyplot
+from mantid import logger
+from mantid.api import AlgorithmManager, AnalysisDataService, ITableWorkspace, MatrixWorkspace
+from mantidqt.plotting.functions import plot
+from mantidqt.utils.qt import import_qt
+
+PropertyHandler = import_qt('.._common', 'mantidqt.widgets', 'PropertyHandler')
+
+
+class PropertyHandlerType(PropertyHandler):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+class FitPropertyBrowserPlotInteraction(QObject):
+
+    def __init__(self, fit_browser, canvas, parent=None):
+        super().__init__(parent)
+        self.fit_browser = fit_browser
+        self.canvas = canvas
+
+        self._connect_signals()
+        # Initialise state storage
+        self.guess_line = None
+        self.guess_all_line = None
+        self.guess_line_handlers = None
+        self.guess_lines = {}
+
+    def _connect_signals(self):
+        self.fit_browser.plotGuess.connect(self.plot_guess_all_slot, Qt.QueuedConnection)
+        self.fit_browser.plotCurrentGuess.connect(self.plot_current_guess_slot, Qt.QueuedConnection)
+        self.fit_browser.removeCurrentGuess.connect(self.remove_current_guess_slot, Qt.QueuedConnection)
+        self.fit_browser.changedParameterOf.connect(self.parameters_changed_slot, Qt.QueuedConnection)
+        self.fit_browser.functionRemoved.connect(self.slot_for_function_removed)
+
+    @Slot()
+    def plot_guess_all_slot(self):
+        """
+        Toggle the guess plot.
+        """
+        if self.guess_all_line:
+            self.remove_guess_all()
+        else:
+            self.plot_guess_all()
+
+    @Slot()
+    def plot_current_guess_slot(self):
+        """
+        Toggle the guess plot.
+        """
+        self.fit_browser.currentHandler().setHasPlot(True)
+        self.plot_current_guess()
+
+    @Slot()
+    def remove_guess_all_slot(self):
+        """
+        Toggle the guess plot.
+        """
+        self.fit_browser.currentHandler().setHasPlot(False)
+        self.remove_guess_all()
+
+    @Slot()
+    def remove_current_guess_slot(self):
+        """
+        Toggle the guess plot.
+        """
+        self.fit_browser.currentHandler().setHasPlot(False)
+        self.remove_current_guess()
+
+    @Slot(str)
+    def parameters_changed_slot(self, prefix):
+        if not self.fit_browser.currentHandler():
+            return
+        name = self._get_current_prefixed_function_name()
+        if name in self.guess_lines:
+            self.update_current_handler_guess()
+        if self.guess_all_line:
+            self.update_guess_all()
+
+    @Slot()
+    def slot_for_function_removed(self):
+        if not self.fit_browser.currentHandler():
+            return
+        prefix = self.fit_browser.currentHandler().functionPrefix()
+        self.remove_current_guess()
+        self._shift_stored_prefixes(prefix)
+        if self.guess_all_line:
+            self.update_guess_all()
+
+    def plot_current_guess(self):
+        """
+        Plot the guess curve of the currently selected function.
+        """
+        fun = self.fit_browser.currentHandler().ifun()
+        ws_name = self.fit_browser.workspaceName()
+        if fun == '' or ws_name == '':
+            return
+        out_ws_name = self._get_current_prefixed_function_name()
+
+        line = self._plot_guess_workspace(ws_name, fun, out_ws_name)
+
+        if line:
+            self.guess_lines[self._get_current_prefixed_function_name()] = line
+
+    def evaluate_function(self, ws_name, fun, out_ws_name):
+        ws_index = self.fit_browser.workspaceIndex()
+        startX = self.fit_browser.startX()
+        endX = self.fit_browser.endX()
+        workspace = AnalysisDataService.retrieve(ws_name)
+        alg = AlgorithmManager.createUnmanaged('EvaluateFunction')
+        alg.setChild(True)
+        alg.initialize()
+        alg.setProperty('Function', fun)
+        alg.setProperty('InputWorkspace', ws_name)
+        if isinstance(workspace, ITableWorkspace):
+            alg.setProperty('XColumn', self.fit_browser.getXColumnName())
+            alg.setProperty('YColumn', self.fit_browser.getYColumnName())
+            if self.fit_browser.getErrColumnName():
+                alg.setProperty('ErrColumn', self.fit_browser.getErrColumnName())
+        else:
+            alg.setProperty('WorkspaceIndex', ws_index)
+        alg.setProperty('StartX', startX)
+        alg.setProperty('EndX', endX)
+        alg.setProperty('OutputWorkspace', out_ws_name)
+        alg.execute()
+
+        return alg.getProperty('OutputWorkspace').value
+
+    def plot_guess_all(self):
+        """
+        Plot the guess curve for the full function.
+        """
+        fun = self.fit_browser.getFittingFunction()
+        ws_name = self.fit_browser.workspaceName()
+        if fun == '' or ws_name == '':
+            return
+        out_ws_name = '{}_guess'.format(ws_name)
+
+        line = self._plot_guess_workspace(ws_name, fun, out_ws_name)
+
+        if line:
+            self.guess_all_line = line
+            self.fit_browser.setTextPlotGuess('Remove Guess')
+
+    def remove_guess_all(self):
+        if self.guess_all_line is None:
+            return
+        self.guess_all_line.remove()
+        self.guess_all_line = None
+        self.update_legend()
+        self.fit_browser.setTextPlotGuess('Plot Guess')
+        self.canvas.draw()
+
+    def remove_current_guess(self):
+        """
+        Remove the guess curve from the plot.
+        """
+        if not self.guess_lines:
+            return
+        try:
+            line = self.guess_lines.pop(self._get_current_prefixed_function_name())
+            line.remove()
+            self.update_legend()
+            self.canvas.draw()
+        except KeyError:
+            pass
+
+    def update_guess(self):
+        """
+        Update the guess curve.
+        """
+        if self.guess_all_line is None:
+            return
+        self.update_guess_all()
+
+    def update_current_handler_guess(self):
+        """
+        Plot the guess curve of the currently selected function.
+        """
+        fun = self.fit_browser.currentHandler().ifun()
+        ws_name = self.fit_browser.workspaceName()
+        if fun == '' or ws_name == '':
+            return
+        out_ws_name = self._get_current_prefixed_function_name()
+        line = self.guess_lines[self._get_current_prefixed_function_name()]
+        color = line.get_color()
+        line.remove()
+
+        line = self._plot_guess_workspace(ws_name, fun, out_ws_name, color=color)
+        if line:
+            self.guess_lines[self._get_current_prefixed_function_name()] = line
+
+    def update_guess_all(self):
+        """
+        Plot the guess curve of the currently selected function.
+        """
+        fun = self.fit_browser.getFittingFunction()
+        ws_name = self.fit_browser.workspaceName()
+        if fun == '' or ws_name == '':
+            return
+        out_ws_name = '{}_guess'.format(ws_name)
+        old_line = self.guess_all_line
+        color = old_line.get_color()
+        old_line.remove()
+
+        line = self._plot_guess_workspace(ws_name, fun, out_ws_name, color=color)
+        self.guess_all_line = line
+
+    def update_legend(self):
+        """
+        This needs to be called to update plot's legend after removing lines.
+        """
+        ax = self.canvas.figure.get_axes()[0]
+        # only create a legend if the plot already had one
+        if ax.get_legend():
+            ax.make_legend()
+
+    def get_axes(self):
+        """
+        Get the pyplot's Axes object.
+        :rtype: matplotlib.axes.Axes
+        """
+        return self.canvas.figure.get_axes()[0]
+
+    def _get_current_prefixed_function_name(self):
+        return self.fit_browser.currentHandler().functionPrefix() + '.' + \
+               self.fit_browser.currentHandler().ifun().name()
+
+    def _plot_guess_workspace(self, workspace_name, function, output_workspace_name, **plotkwargs):
+        try:
+            out_ws = self.evaluate_function(workspace_name, function, output_workspace_name)
+        except RuntimeError:
+            return
+
+        ax = self.get_axes()
+        legend = ax.get_legend()
+
+        # Setting distribution=True prevents the guess being normalised
+        line = ax.plot(out_ws, wkspIndex=1, label=output_workspace_name, distribution=True,
+                       update_axes_labels=False, autoscale_on_update=False, **plotkwargs)[0]
+
+        if legend:
+            ax.make_legend()
+
+        self.canvas.draw()
+
+        return line
+
+    def _shift_stored_prefixes(self, removed_prefix):
+        # Skip if no guess lines plotted, or removing last function in browser
+        if not self.guess_lines or removed_prefix[1] == len(self.guess_lines):
+            return
+        for prefixed_function in reversed(list(self.guess_lines)):
+            prefix_and_function = prefixed_function.split('.')
+            if int(prefix_and_function[0][1]) > int(removed_prefix[1]):
+                new_function_label = 'f' + str(int(prefix_and_function[0][1]) - 1) + '.' + prefix_and_function[1]
+                line = self.guess_lines.pop(prefixed_function)
+                self.guess_lines[new_function_label] = line
+                line.set_label(new_function_label)
+
+        self._refresh_legend()
+
+    def _refresh_legend(self):
+        ax = self.get_axes()
+        legend = ax.get_legend()
+        if legend:
+            ax.make_legend()
+        self.canvas.draw()
+
