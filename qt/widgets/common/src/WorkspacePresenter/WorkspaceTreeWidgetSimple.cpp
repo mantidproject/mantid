393eb0f4ef6c12216912e8830de8e326b3b88ba5
Steven Hahn <hahnse@ornl.gov>
2020-10-06 17:15:27 -0400

    clang-format
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- qt/widgets/common/src/WorkspacePresenter/WorkspaceTreeWidgetSimple.cpp
+++ qt/widgets/common/src/WorkspacePresenter/WorkspaceTreeWidgetSimple.cpp
@@ -189,7 +189,8 @@ void WorkspaceTreeWidgetSimple::popupContextMenu() {
       if (std::dynamic_pointer_cast<IPeaksWorkspace>(workspace)) {
         menu->addAction(m_showDetectors);
       }
-    } else if (auto md_ws = std::dynamic_pointer_cast<IMDWorkspace>(workspace)) {
+    } else if (auto md_ws =
+                   std::dynamic_pointer_cast<IMDWorkspace>(workspace)) {
       menu->addAction(m_showAlgorithmHistory);
       menu->addAction(m_sampleLogs);
 
