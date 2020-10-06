b22208fc000a179cd2415848e603bcbc9b85dce0
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-06 14:47:49 +0100

    Refs #28543. Move QECoverageGUI to new folder to stop weird error.

--- scripts/QECoverage/__init__.py
+++ scripts/QECoverage/__init__.py
@@ -4,13 +4,3 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
-# pylint: disable=invalid-name
-import sys
-from mantidqt.gui_helper import get_qapplication
-from QECoverage.QECoverageGUI import QECoverageGUI
-
-app, within_mantid = get_qapplication()
-mainForm = QECoverageGUI()
-mainForm.show()
-if not within_mantid:
-    sys.exit(app.exec_())
