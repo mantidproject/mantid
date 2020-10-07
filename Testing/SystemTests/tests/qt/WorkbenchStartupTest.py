03cb50965e648ef5928d2f22bdbf9e959bcb6ce2
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-07 12:08:53 +0100

    Refs #28544. Use f-strings for string formatting.

--- Testing/SystemTests/tests/qt/WorkbenchStartupTest.py
+++ Testing/SystemTests/tests/qt/WorkbenchStartupTest.py
@@ -10,6 +10,7 @@ import sys
 import systemtesting
 
 from mantid.kernel import ConfigService
+from tempfile import NamedTemporaryFile
 
 
 EXECUTABLE_SWITCHER = {"linux": ["launch_mantidworkbench.sh", "mantidworkbench"],
@@ -20,7 +21,7 @@ EXECUTABLE_SWITCHER = {"linux": ["launch_mantidworkbench.sh", "mantidworkbench"]
 def get_mantid_executables_for_platform(platform):
     workbench_executables = EXECUTABLE_SWITCHER.get(platform, None)
     if workbench_executables is None:
-        raise RuntimeError("Unknown platform {0}.".format(platform))
+        raise RuntimeError(f"Unknown platform {platform}.")
     return workbench_executables
 
 
@@ -30,13 +31,13 @@ def get_mantid_executable_path(platform):
         workbench_exe = os.path.join(directory, executable)
         if os.path.exists(workbench_exe):
             return workbench_exe
-    raise RuntimeError("Could not find path to {0}.".format(workbench_exe))
+    raise RuntimeError(f"Could not find path to {workbench_exe}.")
 
 
 def create_test_script(test_file_path):
-    test_script_path = ConfigService.getString("defaultsave.directory") + "/test_script.py"
+    test_script_path = NamedTemporaryFile(suffix=".py").name
     with open(test_script_path, "w") as file:
-        file.write("test_file_path = '" + test_file_path + "'")
+        file.write(f"test_file_path = '{test_file_path}'")
         file.write("\nwith open(test_file_path, 'w') as file:")
         file.write("\n    file.write('Hello Mantid')")
     return test_script_path
@@ -44,7 +45,10 @@ def create_test_script(test_file_path):
 
 def remove_file(file_path):
     if os.path.exists(file_path):
-        os.remove(file_path)
+        try:
+            os.remove(file_path)
+        except OSError as ex:
+            print(f"Error removing {file_path}: {ex}.")
 
 
 class WorkbenchStartupTest(systemtesting.MantidSystemTest):
@@ -54,6 +58,7 @@ class WorkbenchStartupTest(systemtesting.MantidSystemTest):
     def __init__(self):
         super(WorkbenchStartupTest, self).__init__()
 
+        #self._test_file = NamedTemporaryFile(suffix=".txt").name()
         self._test_file = ConfigService.getString("defaultsave.directory") + "/test_file.txt"
         self._test_script = create_test_script(self._test_file)
 
@@ -63,12 +68,8 @@ class WorkbenchStartupTest(systemtesting.MantidSystemTest):
         process = subprocess.Popen([self._executable, "--execute", self._test_script, "--quit"],
                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        _, error = process.communicate()
-
-        # Assert that an error message is not returned from stderr
-        self.assertTrue(not error)
-        # Assert the process was executed successfully
-        self.assertTrue(process.returncode == 0)
+        # Wait for the process to finish execution, and assert it was successfully
+        self.assertTrue(process.wait() == 0)
         # Assert that the test script runs successfully by creating a .txt file
         self.assertTrue(os.path.exists(self._test_file))
 
