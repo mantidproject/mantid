6530525a5acc6c0c3afe11755e081ee744f66762
William F Godoy <williamfgc@yahoo.com>
2020-10-13 14:19:01 -0400

    Corelli calibration functionality
    Combine banks for a specific calibration date
    Allow date and timestamp input in save function

--- scripts/corelli/calibration/database.py
+++ scripts/corelli/calibration/database.py
@@ -9,6 +9,7 @@
 
 import pathlib
 from datetime import datetime
+import re
 
 from mantid.dataobjects import TableWorkspace
 from mantid.api import WorkspaceGroup, Workspace
@@ -36,35 +37,76 @@ def has_valid_columns(table:TableWorkspace)->bool:
     return True
 
 
-def filename_bank_table( bankID:int, database_path:str, table_type:str = 'calibration' ) -> str:
+def filename_bank_table( bankID:int, database_path:str, date:str, table_type:str = 'calibration' ) -> str:
     """
     Function that returns the absolute path for a bank file using corelli format:
     database_path/bank0ID/type_corelli_bank0ID_YYYYMMDD.nxs
     :param bankID bank number that is calibrated
+    :param date format YYYYMMDD
     :param database_path location of the corelli database (absolute or relative)
            Example: database/corelli/ for
                     database/corelli/bank001/
                     database/corelli/bank002/
     :param type 'calibration', 'mask' or 'acceptance'
     """
+
+    verify_date_YYYYMMDD('filename_bank_table', date)
     subdirectory:str = database_path + '/' + 'bank' + str(bankID).zfill(3)
     pathlib.Path(subdirectory).mkdir(parents=True, exist_ok=True)
     message = f'{"Cannot process Corelli filename_bank_table, table_type must be calibration, mask or acceptance"}'
     assert table_type == 'calibration' or table_type == 'mask' or table_type == 'acceptance', message
 
-    time:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
-    filename = subdirectory + '/' + table_type + '_corelli_bank' + str(bankID).zfill(3) + '_' + time + '.nxs.h5'
+    filename = subdirectory + '/' + table_type + '_corelli_bank' + str(bankID).zfill(3) + '_' + date + '.nxs.h5'
     # make it portable
     filename = str(pathlib.Path(filename).resolve())
     return filename
 
 
-def save_bank_table( data:Workspace, bankID:int, database_path:str, table_type:str = 'calibration' ) -> None:
+def save_manifest_file( bankIDs:list, database_path:str, date:str, iso_timestamps:list)->None:
+    """
+    Function that saves or updates an existing manifest_corelli_date.csv file.
+    There is one file stored for each calibration day. If file exist it will append two columns:
+    bankID, timestamp (ISO format)
+    :param bankIDs input of bankIDs that have been calibrated
+    :param database_path location of the corelli database (absolute or relative)
+           Example: database/corelli/ for manifest file:
+                    database/corelli/manifest_corelli_YYYYMMDD.csv
+    :param date format YYYYMMDD
+    :param iso_timestamps list of timestamps for each calibrated bank in iso format YYYY-MM-DDTHH:MM:SS
+                          from datetime.datetime.now().replace(microsecond=0).isoformat()
+    """
+    # verify banks and timestamps are the same length
+    if len(bankIDs) != len(iso_timestamps):
+        raise ValueError('bankIDs and iso_timestamps list inputs must have the same lengths')
+
+    # verify timestamps are correct
+    for iso_timestamp in iso_timestamps:
+        if not re.match('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}', iso_timestamp):
+            raise ValueError('iso_timestamp entry: ' + str(iso_timestamp) + ' is not valid iso format YYYY-MM-DDTHH:MM:SS')
+
+    filename = database_path + '/manifest_corelli_' + date + '.csv'
+
+    if pathlib.Path(filename).is_file():
+        file = open(filename, 'a+')
+    else:
+        file = open(filename, 'w')
+        file.write('bankID, timestamp\n') # header
+
+    lines:str = ''
+    for t, bankID in enumerate(bankIDs):
+        lines += str(bankID) + ', ' + str(iso_timestamps[t]) + '\n'
+
+    file.write(lines)
+    file.close()
+
+
+def save_bank_table( data:Workspace, bankID:int, database_path:str, date:str,
+                     table_type:str = 'calibration' ) -> None:
     """
     Function that saves a bank calibrated TableWorkspace into a single HDF5 file
-    using corelli format:
-    database_path/bank0ID/type_corelli_bank0ID_YYYYMMDD.nxs
-    :param data input TableWorkspace data for calibrated pixels
+    using corelli format and using current date:
+    database_path/bank0ID/type_corelli_bank0ID_YYYYMMDD.nxs.h5
+    :param data input Workspace (TableWorkspace) data for calibrated pixels
     :param bankID bank number that is calibrated
     :param database_path location of the corelli database (absolute or relative)
            Example: database/corelli/ for
@@ -72,13 +114,14 @@ def save_bank_table( data:Workspace, bankID:int, database_path:str, table_type:s
                     database/corelli/bank002/
     :param table_type 'calibration', 'mask' or 'acceptance'
     """
-    filename:str = filename_bank_table(bankID, database_path, table_type)
+    verify_date_YYYYMMDD('save_bank_table', date)
+    filename:str = filename_bank_table(bankID, database_path, date, table_type)
     SaveNexusProcessed(data,filename)
 
 
-def load_bank_table( bankID:int, database_path:str, table_type:str = 'calibration' ) -> TableWorkspace:
+def load_bank_table( bankID:int, database_path:str, date:str, table_type:str = 'calibration' ) -> TableWorkspace:
     """
-    Function that loads a bank calibrated TableWorkspace from a single HDF5 file
+    Function that loads the latest bank calibrated TableWorkspace from a single HDF5 file
     using corelli format:
     database_path/bank0ID/type_corelli_bank0ID_YYYYMMDD.nxs
     :param bankID bank number that is calibrated
@@ -86,12 +129,71 @@ def load_bank_table( bankID:int, database_path:str, table_type:str = 'calibratio
            Example: database/corelli/ for
                     database/corelli/bank001/
                     database/corelli/bank002/
+    :param date current day in YYYYMMDD format
     :param table_type 'calibration', 'mask' or 'acceptance'
     :return TableWorkspace with corresponding data
     """
-    filename:str = filename_bank_table(bankID, database_path, table_type)
+    verify_date_YYYYMMDD('load_bank_table', date)
+    filename:str = filename_bank_table(bankID, database_path, date, table_type)
     outputWS = LoadNexusProcessed(filename)
     return outputWS
+
+
+def combine_temporal_banks( database_path:str, date:str, table_type:str = 'calibration' )->WorkspaceGroup:
+    """
+    Function that combines stored banks for a certain calibration date file,
+    if not available the latest calibration date is picked up.
+    Output WorkspaceGroup can be an input to combine_spatial_banks
+    Example:
+            database/corelli/
+                bank001/table_type_corelli_bank001_20201201.nxs.h5
+                bank002/table_type_corelli_bank002_20201201.nxs.h5
+                bank003/table_type_corelli_bank003_20201201.nxs.h5
+                bank004/table_type_corelli_bank004_20200601.nxs.h5
+
+    will return a WorkspaceGroup with TableWorkspace elements from the above
+    files if date = 20201201. Since bank004 wasn't calibrated on 20201201,
+    20200601 is selected as the most recent one before 20201201.
+    :param database_path location of the corelli database (absolute or relative)
+           Example: database/corelli/ for
+                    database/corelli/bank001/
+                    database/corelli/bank002/
+    :param date pivot date in YYYYMMDD
+    :param table_type 'calibration', 'mask' or 'acceptance'
+    :return WorkspaceGroup, each component is a calibrated TableWorkspace bank
+    """
+
+    tables:WorkspaceGroup = WorkspaceGroup()
+
+    bank_dirs = list(pathlib.Path(database_path).glob('bank'+ ('[0-9]' * 3) ))
+    # filter directories
+    bank_dirs = [dir for dir in bank_dirs if dir.is_dir()]
+
+    for bank_dir in bank_dirs:
+
+        # extract files in bank calibration directory
+        files = list(pathlib.Path(bank_dir).glob(table_type + '_corelli_bank' + ('[0-9]' * 3) + '*' ) )
+
+        available_dates = []
+
+        for file in files:
+            split = re.split( r'_|\.', file.name )
+            if len(split) < 4:
+                raise ValueError('Bank file ' +str(file) + ' does not meet the accepted format')
+            available_dates.append(int(split[3]))
+
+        available_dates.sort(reverse=True)
+
+        # check available dates choose the latest in the past if not found
+        for available_date in available_dates:
+
+            if available_date <= int(date):
+                bankID:int = int(str(bank_dir)[-3:])
+                table = load_bank_table(bankID, database_path, str(available_date), table_type)
+                tables.addWorkspace(table)
+                break
+
+    return tables
 
 
 def combine_spatial_banks(tables:WorkspaceGroup)->TableWorkspace:
@@ -123,3 +225,19 @@ def combine_spatial_banks(tables:WorkspaceGroup)->TableWorkspace:
             combined_table.addRow( [table_dict['Detector ID'][r], table_dict['Detector Position'][r] ])
 
     return combined_table
+
+
+def verify_date_YYYYMMDD(function_name:str, date:str)->None:
+    """
+    Function that verifies if date input string has a correct date format: YYYYMMDD.
+    Throws an exception otherwise.
+    :param function_name input that calls this funciton for better exception handling
+    :param date input date string to be verified
+    :raises ValueError if date is not in YYYYMMDD format
+    """
+    try:
+        if len(date) != 8:
+            raise ValueError('')
+        datetime.strptime(date, '%Y%m%d')
+    except ValueError:
+        raise ValueError('date in function ' + function_name + ' ' + date +  ' is not YYYYMMDD')
--- scripts/test/corelli/calibration/test_database.py
+++ scripts/test/corelli/calibration/test_database.py
@@ -17,7 +17,7 @@ from mantid.dataobjects import TableWorkspace
 from mantid.api import WorkspaceGroup
 from mantid.simpleapi import CreateEmptyTableWorkspace
 from corelli.calibration.database import combine_spatial_banks, init_corelli_table, save_bank_table, filename_bank_table,\
-    load_bank_table, has_valid_columns
+    load_bank_table, has_valid_columns, save_manifest_file, verify_date_YYYYMMDD, combine_temporal_banks
 
 
 class TestCorelliDatabase(unittest.TestCase):
@@ -26,30 +26,53 @@ class TestCorelliDatabase(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        # create a mock database and tests save_bank_table and load_bank_table
+        # create a mock database
+        # tests save_bank_table and load_bank_table, save_manifest
         self.database_path:str = TestCorelliDatabase.test_dir.name
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
 
-        calibratedWS10 = init_corelli_table()
-        calibratedWS10.addRow( [28672,[2.291367950578997, -1.2497636826045173, -0.7778867990739283]])
-        calibratedWS10.addRow( [28673,[2.291367950578997, -1.2462425728938251, -0.7778867990739283]])
-        calibratedWS10.addRow( [28674,[2.291367950578997, -1.2427213977528369, -0.7778867990739283]])
-        calibratedWS10.addRow( [28675,[2.291367950578997, -1.2392001571797284, -0.7778867990739283]])
-        save_bank_table( calibratedWS10, 10, self.database_path)
+        calibratedWS = init_corelli_table()
+        calibratedWS.addRow( [28672,[2.291367950578997, -1.2497636826045173, -0.7778867990739283]])
+        calibratedWS.addRow( [28673,[2.291367950578997, -1.2462425728938251, -0.7778867990739283]])
+        calibratedWS.addRow( [28674,[2.291367950578997, -1.2427213977528369, -0.7778867990739283]])
+        calibratedWS.addRow( [28675,[2.291367950578997, -1.2392001571797284, -0.7778867990739283]])
+        save_bank_table( calibratedWS, 10, self.database_path, date)
 
-        calibratedWS20 = init_corelli_table()
-        calibratedWS20.addRow( [28700,[2.291367950578997, -1.1511478720770645, -0.7778867990739283]])
-        calibratedWS20.addRow( [28701,[2.291367950578997, -1.1476249296284657, -0.7778867990739283]])
-        calibratedWS20.addRow( [28702,[2.291367950578997, -1.2427213977528369, -0.7778867990739283]])
-        calibratedWS20.addRow( [28703,[2.291367950578997, -1.2392001571797284, -0.7778867990739283]])
-        save_bank_table( calibratedWS20, 20, self.database_path)
+        calibratedWS = init_corelli_table()
+        calibratedWS.addRow( [28676,[2.291367950578997, -1.2597636826045173, -0.7778867990739283]])
+        calibratedWS.addRow( [28677,[2.291367950578997, -1.2562425728938251, -0.7778867990739283]])
+        calibratedWS.addRow( [28678,[2.291367950578997, -1.2527213977528369, -0.7778867990739283]])
+        calibratedWS.addRow( [28679,[2.291367950578997, -1.2492001571797284, -0.7778867990739283]])
+        save_bank_table( calibratedWS, 20, self.database_path, date)
+
+        calibratedWS = init_corelli_table()
+        calibratedWS.addRow( [28700,[2.291367950578997, -1.1511478720770645, -0.7778867990739283]])
+        calibratedWS.addRow( [28701,[2.291367950578997, -1.1476249296284657, -0.7778867990739283]])
+        calibratedWS.addRow( [28702,[2.291367950578997, -1.2427213977528369, -0.7778867990739283]])
+        calibratedWS.addRow( [28703,[2.291367950578997, -1.2392001571797284, -0.7778867990739283]])
+        save_bank_table( calibratedWS, 30, self.database_path, date)
+
+        calibratedWS = init_corelli_table()
+        calibratedWS.addRow( [28704,[2.291367950578997, -1.1611478720770645, -0.7778867990739283]])
+        calibratedWS.addRow( [28705,[2.291367950578997, -1.1776249296284657, -0.7778867990739283]])
+        calibratedWS.addRow( [28706,[2.291367950578997, -1.2827213977528369, -0.7778867990739283]])
+        calibratedWS.addRow( [28707,[2.291367950578997, -1.2992001571797284, -0.7778867990739283]])
+        save_bank_table( calibratedWS, 40, self.database_path, '20200601') # use different date
+
+        calibratedWS = init_corelli_table()
+        calibratedWS.addRow( [28704,[2.291367950578997, -1.1711478720770645, -0.7778867990739283]])
+        calibratedWS.addRow( [28705,[2.291367950578997, -1.1876249296284657, -0.7778867990739283]])
+        calibratedWS.addRow( [28706,[2.291367950578997, -1.2927213977528369, -0.7778867990739283]])
+        calibratedWS.addRow( [28707,[2.291367950578997, -1.3092001571797284, -0.7778867990739283]])
+        save_bank_table( calibratedWS, 40, self.database_path, '20200101') # use different date
 
         # placeholder to read from the database
         self.ws_group = WorkspaceGroup()
-        calibratedWS10 = load_bank_table( 10, self.database_path)
-        self.ws_group.addWorkspace(calibratedWS10)
-
-        calibratedWS20 = load_bank_table( 20, self.database_path)
-        self.ws_group.addWorkspace(calibratedWS20)
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
+        self.ws_group.addWorkspace(load_bank_table( 10, self.database_path, date))
+        self.ws_group.addWorkspace(load_bank_table( 20, self.database_path, date))
+        self.ws_group.addWorkspace(load_bank_table( 30, self.database_path, date))
+        self.ws_group.addWorkspace(load_bank_table( 40, self.database_path, '20200601'))
 
     def test_init_corelli_table(self):
 
@@ -68,26 +91,26 @@ class TestCorelliDatabase(unittest.TestCase):
     def test_filename_bank_table(self):
 
         abs_subdir:str = self.database_path + '/bank001/'
-        time:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
 
         # calibration
-        expected_filename = str(pathlib.Path(abs_subdir + '/calibration_corelli_bank001_' + time + '.nxs.h5').resolve())
-        filename = filename_bank_table(1, self.database_path, 'calibration')
+        expected_filename = str(pathlib.Path(abs_subdir + '/calibration_corelli_bank001_' + date + '.nxs.h5').resolve())
+        filename = filename_bank_table(1, self.database_path, date, 'calibration')
         self.assertEqual(filename, expected_filename)
 
         # mask
-        expected_filename = str(pathlib.Path(abs_subdir + '/mask_corelli_bank001_' + time + '.nxs.h5').resolve())
-        filename = filename_bank_table(1, self.database_path, 'mask')
+        expected_filename = str(pathlib.Path(abs_subdir + '/mask_corelli_bank001_' + date + '.nxs.h5').resolve())
+        filename = filename_bank_table(1, self.database_path, date, 'mask')
         self.assertEqual(filename, expected_filename)
 
         # acceptance
-        expected_filename = str(pathlib.Path(abs_subdir + '/acceptance_corelli_bank001_' + time + '.nxs.h5').resolve())
-        filename = filename_bank_table(1, self.database_path, 'acceptance')
+        expected_filename = str(pathlib.Path(abs_subdir + '/acceptance_corelli_bank001_' + date + '.nxs.h5').resolve())
+        filename = filename_bank_table(1, self.database_path, date, 'acceptance')
         self.assertEqual(filename, expected_filename)
 
         # verify assertion is raised for invalid name
         with self.assertRaises( AssertionError ) as ar:
-            filename_bank_table(1, self.database_path, 'wrong')
+            filename_bank_table(1, self.database_path, date, 'wrong')
 
         self.assertEqual( 'table_type must be calibration, mask or acceptance' in str(ar.exception), True)
 
@@ -96,16 +119,25 @@ class TestCorelliDatabase(unittest.TestCase):
         combined_table = combine_spatial_banks(self.ws_group)
         combined_dict = combined_table.toDict()
 
-        expected_dict = {'Detector ID': [28672, 28673, 28674, 28675, 28700, 28701, 28702, 28703],
-                         'Detector Position': [  np.array([2.29136795, -1.24976368, -0.7778868]),
-                                                 np.array([2.29136795, -1.24624257, -0.7778868]),
-                                                 np.array([2.29136795, -1.2427214,  -0.7778868]),
-                                                 np.array([2.29136795, -1.23920016, -0.7778868]),
-                                                 np.array([2.29136795, -1.15114787, -0.7778868]),
-                                                 np.array([2.29136795, -1.14762493, -0.7778868]),
-                                                 np.array([2.29136795, -1.2427214,  -0.7778868]),
-                                                 np.array([2.29136795, -1.23920016, -0.7778868])
-                                                 ]
+        expected_dict = {'Detector ID': [28672, 28673, 28674, 28675, 28676, 28677, 28678, 28679,
+                                         28700, 28701, 28702, 28703, 28704, 28705, 28706, 28707],
+                         'Detector Position': [np.array([ 2.29136795, -1.24976368, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.24624257, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.2427214 , -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.23920016, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.25976368, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.25624257, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.2527214 , -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.24920016, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.15114787, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.14762493, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.2427214 , -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.23920016, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.16114787, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.17762493, -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.2827214 , -0.7778868 ]),
+                                               np.array([ 2.29136795, -1.29920016, -0.7778868 ])
+                                               ]
                          }
 
         self.assertEqual(expected_dict['Detector ID'], combined_dict['Detector ID'] )
@@ -113,10 +145,119 @@ class TestCorelliDatabase(unittest.TestCase):
         for i,expected_array in enumerate(expected_dict['Detector Position']):
             self.assertAlmostEqual( expected_array.all(), combined_dict['Detector Position'][i].all() )
 
+    def test_save_manifest_file(self):
+
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
+        ts:str = datetime.now().replace(microsecond=0).isoformat()
+
+        filename = self.database_path + '/manifest_corelli_' + date + '.csv'
+
+        # writing
+        save_manifest_file( [10,11], self.database_path, date, [ts,ts] )
+        self.assertTrue(pathlib.Path(filename).is_file())
+
+        file_contents = pathlib.Path(filename).read_text() # safe one liner
+        expected_manifest = f'bankID, timestamp\n10, {ts}\n11, {ts}\n'
+        self.assertEqual(file_contents, expected_manifest)
+
+        # appending
+        ts = datetime.now().replace(microsecond=0).isoformat()
+        save_manifest_file( [12], self.database_path, date, [ts] )
+        self.assertTrue(pathlib.Path(filename).is_file())
+
+        file_contents = pathlib.Path(filename).read_text() # safe one liner
+        expected_manifest = f'bankID, timestamp\n10, {ts}\n11, {ts}\n12, {ts}\n'
+        self.assertEqual(file_contents, expected_manifest)
+
+        # verify exceptions
+        with self.assertRaises( ValueError ) as ar:
+            save_manifest_file( [12], self.database_path, date, [ts,ts] )
+
+        self.assertEqual( 'bankIDs and iso_timestamps list inputs must have the same lengths' in str(ar.exception), True)
+
+        # verify exceptions
+        with self.assertRaises( ValueError ) as ar:
+            save_manifest_file( [12], self.database_path, date, ['hello'] )
+
+        self.assertEqual( 'is not valid iso format YYYY-MM-DDTHH:MM:SS' in str(ar.exception), True)
+
+        remove(filename)
+
+    def test_verify_date_YYYYMMDD(self)-> None:
+
+        # success
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
+        verify_date_YYYYMMDD('test_verify_date_YYYYMMDD', date)
+
+        # failure
+        # verify assertion is raised for invalid date format
+        with self.assertRaises( ValueError ) as ar:
+            verify_date_YYYYMMDD('test_verify_date_YYYYMMDD', '120711')
+
+        self.assertEqual( 'date in function test_verify_date_YYYYMMDD' in str(ar.exception), True)
+
+        with self.assertRaises( ValueError ) as ar:
+            verify_date_YYYYMMDD('test_verify_date_YYYYMMDD', 'XX220101')
+
+        self.assertEqual( 'date in function test_verify_date_YYYYMMDD' in str(ar.exception), True)
+
+    def test_combine_temporal_banks(self) -> None:
+
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
+        groupWS:WorkspaceGroup = combine_temporal_banks( self.database_path, date )
+
+        # list of expected groupWS components as dictionaries
+        expected = []
+        expected.append( {'Detector ID': [28672, 28673, 28674, 28675],
+                          'Detector Position':
+                          [np.array([ 2.29136795, -1.24976368, -0.7778868 ]),
+                           np.array([ 2.29136795, -1.24624257, -0.7778868 ]),
+                           np.array([ 2.29136795, -1.2427214 , -0.7778868 ]),
+                           np.array([ 2.29136795, -1.23920016, -0.7778868 ])
+                           ]
+                          } )
+
+        expected.append( {'Detector ID': [28676, 28677, 28678, 28679],
+                          'Detector Position': [
+                              np.array([ 2.29136795, -1.25976368, -0.7778868 ]),
+                              np.array([ 2.29136795, -1.25624257, -0.7778868 ]),
+                              np.array([ 2.29136795, -1.2527214 , -0.7778868 ]),
+                              np.array([ 2.29136795, -1.24920016, -0.7778868 ]) ]
+                          }
+                         )
+
+        expected.append( {'Detector ID': [28700, 28701, 28702, 28703],
+                          'Detector Position': [
+                              np.array([ 2.29136795, -1.15114787, -0.7778868 ]),
+                              np.array([ 2.29136795, -1.14762493, -0.7778868 ]),
+                              np.array([ 2.29136795, -1.2427214 , -0.7778868 ]),
+                              np.array([ 2.29136795, -1.23920016, -0.7778868 ])]
+                          }
+                         )
+        # bank 40 from '20200601'
+        expected.append( {'Detector ID': [28704, 28705, 28706, 28707],
+                          'Detector Position': [
+                              np.array([ 2.29136795, -1.16114787, -0.7778868 ]),
+                              np.array([ 2.29136795, -1.17762493, -0.7778868 ]),
+                              np.array([ 2.29136795, -1.2827214 , -0.7778868 ]),
+                              np.array([ 2.29136795, -1.29920016, -0.7778868 ])]})
+
+        for b, ws in enumerate(groupWS):
+
+            table_dict = ws.toDict()
+            self.assertEqual(expected[b]['Detector ID'], table_dict['Detector ID'] )
+
+            for i,expected_array in enumerate(expected[b]['Detector Position']):
+                self.assertAlmostEqual( expected_array.all(), table_dict['Detector Position'][i].all() )
+
     def tearDown(self) -> None:
 
-        remove(filename_bank_table(10, self.database_path))
-        remove(filename_bank_table(20, self.database_path))
+        date:str = datetime.now().strftime('%Y%m%d') # format YYYYMMDD
+        remove(filename_bank_table(10, self.database_path, date))
+        remove(filename_bank_table(20, self.database_path, date))
+        remove(filename_bank_table(30, self.database_path, date))
+        remove(filename_bank_table(40, self.database_path, '20200601'))
+        remove(filename_bank_table(40, self.database_path, '20200101'))
         TestCorelliDatabase.test_dir.cleanup()
 
 
