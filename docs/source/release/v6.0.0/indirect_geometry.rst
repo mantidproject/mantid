704d4677cfe3aa17335087dfee1b1fdb5c7fa0ab
Harriet Brown <harriet.brown@stfc.ac.uk>
2020-10-16 16:15:51 +0100

    Remove resetting of integration rages on changing workspace.
    This commit removes the resetting of the integration parameters when changing the plotted workspace in elwin tab on indirect data analysis.

--- docs/source/release/v6.0.0/indirect_geometry.rst
+++ docs/source/release/v6.0.0/indirect_geometry.rst
@@ -13,4 +13,8 @@ Improvements
 ############
 - Added an option to load diffraction data on IN16B.
 
+Bug Fixes
+#########
+- A bug has been fixed in Indirect data analysis on the Elwin tab that causes the integration range to be reset when changing between plotted workspaces.
+
 :ref:`Release 6.0.0 <v6.0.0>`
--- qt/scientific_interfaces/Indirect/Elwin.cpp
+++ qt/scientific_interfaces/Indirect/Elwin.cpp
@@ -505,10 +505,8 @@ void Elwin::newPreviewFileSelected(int index) {
  */
 void Elwin::plotInput() {
   IndirectDataAnalysisTab::plotInput(m_uiForm.ppPlot);
-  IndirectDataAnalysisTab::updatePlotRange("ElwinIntegrationRange",
-                                           m_uiForm.ppPlot, "IntegrationStart",
-                                           "IntegrationEnd");
-
+  Elwin::updatePlotRange("ElwinIntegrationRange", m_uiForm.ppPlot,
+                         "IntegrationStart", "IntegrationEnd");
   setDefaultSampleLog(inputWorkspace());
 }
 
@@ -554,7 +552,6 @@ void Elwin::minChanged(double val) {
 
   disconnect(m_dblManager, SIGNAL(valueChanged(QtProperty *, double)), this,
              SLOT(updateRS(QtProperty *, double)));
-
   if (from == integrationRangeSelector) {
     m_dblManager->setValue(m_properties["IntegrationStart"], val);
   } else if (from == backgroundRangeSelector) {
@@ -661,6 +658,33 @@ void Elwin::setRunEnabled(const bool &enabled) {
 
 void Elwin::setSaveResultEnabled(const bool &enabled) {
   m_uiForm.pbSave->setEnabled(enabled);
+}
+
+/*
+ * Updates the plot range with the specified name, to match the range of
+ * the sample curve.
+ *
+ * @param rangeName           The name of the range to update.
+ * @param previewPlot         The preview plot widget, in which the range
+ *                            is specified.
+ * @param startRangePropName  The name of the property specifying the start
+ *                            value for the range.
+ * @param endRangePropName    The name of the property specifying the end
+ *                            value for the range.
+ */
+void Elwin::updatePlotRange(const QString &rangeName,
+                            MantidQt::MantidWidgets::PreviewPlot *previewPlot,
+                            const QString &startRangePropName,
+                            const QString &endRangePropName) {
+
+  if (auto const workspace = inputWorkspace()) {
+    try {
+      auto const xRange = getXRangeFromWorkspace(workspace);
+      auto rangeSelector = previewPlot->getRangeSelector(rangeName);
+    } catch (std::exception &exc) {
+      showMessageBox(exc.what());
+    }
+  }
 }
 
 } // namespace IDA
