12e4ba7757e4e3a5e6c4cf728169d9d64eac57a8
Dominik Arominski <arominski@ill.fr>
2020-10-21 14:54:34 +0200

    Corrected algorithm names in the CrossSectionSeparationTest

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CrossSectionSeparationTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CrossSectionSeparationTest.py
@@ -5,10 +5,10 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
-from mantid.api import WorkspaceGroup
-from mantid.simpleapi import config, mtd, ILLComponentSeparation
+from mantid.api import WorkspaceGroup, MatrixWorkspace
+from mantid.simpleapi import Load, config, mtd, CrossSectionSeparation
 
-class ILLComponentSeparationTest(unittest.TestCase):
+class CrossSectionSeparationTest(unittest.TestCase):
 
     _facility = None
     _instrument = None
@@ -16,6 +16,9 @@ class ILLComponentSeparationTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         config.appendDataSearchSubDir('ILL/D7/')
+        Load('vanadium_uniaxial.nxs', OutputWorkspace='vanadium_uniaxial')
+        Load('vanadium_xyz.nxs', OutputWorkspace='vanadium_xyz')
+        Load('vanadium_10p.nxs', OutputWorkspace='vanadium_10p')
 
     def setUp(self):
         self._facility = config['default.facility']
@@ -29,30 +32,35 @@ class ILLComponentSeparationTest(unittest.TestCase):
             config['default.facility'] = self._facility
         if self._instrument:
             config['default.instrument'] = self._instrument
+
+    @classmethod
+    def tearDownClass(cls):
         mtd.clear()
 
     def test_uniaxial_separation(self):
-        ILLComponentSeparation(Run='vanadium_uniaxial', OutputWorkspace='uniaxial')
+        CrossSectionSeparation(InputWorkspace='vanadium_uniaxial', OutputWorkspace='uniaxial')
+        self.assertTrue(mtd['uniaxial'].getNumberOfEntries() == 2)
         self._check_output('uniaxial', 1, 132)
 
     def test_xyz_separation(self):
-        ILLComponentSeparation(Run='vanadium_xyz', OutputWorkspace='xyz')
+        CrossSectionSeparation(InputWorkspace='vanadium_xyz', OutputWorkspace='xyz')
+        self.assertTrue(mtd['xyz'].getNumberOfEntries() == 3)
         self._check_output('xyz', 1, 132)
 
     def test_10p_separation(self):
-        ILLComponentSeparation(Run='vanadium_10p', OutputWorkspace='10p')
+        CrossSectionSeparation(InputWorkspace='vanadium_10p', OutputWorkspace='10p')
+        self.assertTrue(mtd['10p'].getNumberOfEntries() == 3)
         self._check_output('10p', 1, 132)
 
-
     def _check_output(self, ws, blocksize, spectra):
-        self.assertTrue(mtd['ws'])
-        self.assertTrue(isinstance(mtd['ws'], WorkspaceGroup))
-        for entry in mtd['ws']:
+        self.assertTrue(mtd[ws])
+        self.assertTrue(isinstance(mtd[ws], WorkspaceGroup))
+        for entry in mtd[ws]:
             self.assertTrue(isinstance(entry, MatrixWorkspace))
             self.assertTrue(entry.isHistogramData())
             self.assertTrue(not entry.isDistribution())
-            name = entry.name()[
-            name = name[name.rfind("_"):]            
+            name = entry.name()
+            name = name[name.rfind("_")+1:]
             self.assertTrue(name in ['Coherent', 'Incoherent', 'Magnetic'])
             self.assertEqual(entry.blocksize(), blocksize)
             self.assertEqual(entry.getNumberHistograms(), spectra)
