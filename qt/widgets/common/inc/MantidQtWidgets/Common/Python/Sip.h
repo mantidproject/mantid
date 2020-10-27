920117c1a73485f5707e4f96b6110ae473db0362
Gagik Vardanyan <vardanyan@ill.fr>
2020-10-27 12:15:59 +0100

    properly extract raw pointer from PyObject*

--- qt/widgets/common/inc/MantidQtWidgets/Common/Python/Sip.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/Python/Sip.h
@@ -42,6 +42,28 @@ template <typename T> T *extract(const Object &obj) {
 #endif
 }
 
+/**
+ * Extract a C++ object of type T from the Python object pointer
+ * @param obj A sip-wrapped PyObject pointer
+ */
+template <typename T> T *extract(PyObject* obj) {
+  const auto sipapi = Detail::sipAPI();
+  if (!PyObject_TypeCheck(obj, sipapi->api_wrapper_type)) {
+    throw std::runtime_error("extract() - Object is not a sip-wrapped type.");
+  }
+  // transfer ownership from python to C++
+  sipapi->api_transfer_to(obj, 0);
+  // reinterpret to sipWrapper
+  auto wrapper = reinterpret_cast<sipSimpleWrapper *>(obj);
+#if (SIP_API_MAJOR_NR == 8 && SIP_API_MINOR_NR >= 1) || SIP_API_MAJOR_NR > 8
+  return static_cast<T *>(sipapi->api_get_address(wrapper));
+#elif SIP_API_MAJOR_NR == 8
+  return static_cast<T *>(wrapper->data);
+#else
+  return static_cast<T *>(wrapper->u.cppPtr);
+#endif
+}
+
 } // namespace Python
 } // namespace Common
 } // namespace Widgets
