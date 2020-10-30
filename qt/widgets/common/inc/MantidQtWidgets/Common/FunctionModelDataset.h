74988840070506feb0a4ec340ea5a7b7a1e63909
Robert Applin <robert.applin@stfc.ac.uk>
2020-10-30 11:12:48 +0000

    Refs #29802. Create fundamental types for Dataset and Spectra.

---
+++ qt/widgets/common/inc/MantidQtWidgets/Common/FunctionModelDataset.h
@@ -0,0 +1,39 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "DllOption.h"
+
+#include <QList>
+#include <QString>
+#include <QStringList>
+
+namespace MantidQt {
+namespace MantidWidgets {
+
+struct Spectra {
+  Spectra(const QList<std::size_t> &spectra);
+
+  int numberOfSpectra() const;
+
+  QList<std::size_t> m_spectraList;
+};
+
+class Dataset {
+public:
+  Dataset(const QString &workspaceName, const QList<std::size_t> &spectra);
+
+  QString datasetName() const;
+  QStringList domainNames() const;
+
+private:
+  QString m_workspaceName;
+  Spectra m_spectra;
+};
+
+} // namespace MantidWidgets
+} // namespace MantidQt
---
+++ qt/widgets/common/src/FunctionModelDataset.cpp
@@ -0,0 +1,39 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidQtWidgets/Common/FunctionModelDataset.h"
+
+#include <stdexcept>
+
+namespace MantidQt {
+namespace MantidWidgets {
+
+Spectra::Spectra(const QList<std::size_t> &spectra) : m_spectraList(spectra) {}
+
+int Spectra::numberOfSpectra() const { return m_spectraList.size(); }
+
+Dataset::Dataset(const QString &workspaceName,
+                 const QList<std::size_t> &spectra)
+    : m_workspaceName(workspaceName), m_spectra(Spectra(spectra)) {}
+
+QString Dataset::datasetName() const { return m_workspaceName; }
+
+QStringList Dataset::domainNames() const {
+  const auto numberOfSpectra = m_spectra.numberOfSpectra();
+  if (numberOfSpectra == 0)
+    throw std::runtime_error("There are no spectra in this Dataset.");
+  if (numberOfSpectra == 1)
+    return QStringList(m_workspaceName);
+  else {
+    QStringList domainNames;
+    for (const auto &specNum : m_spectra.m_spectraList)
+      domainNames << m_workspaceName + " (" + QString::number(specNum) + ")";
+    return domainNames;
+  }
+}
+
+} // namespace MantidWidgets
+} // namespace MantidQt
