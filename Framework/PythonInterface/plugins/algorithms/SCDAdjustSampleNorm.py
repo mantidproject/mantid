355a6bb4ce5f4e155f8d39557ef9d332a8ee06c5
Cole Kendrick <kendrickcj@ornl.gov>
2020-11-04 22:38:43 -0700

    Added support to also output MDEvent

--- Framework/PythonInterface/plugins/algorithms/SCDAdjustSampleNorm.py
+++ Framework/PythonInterface/plugins/algorithms/SCDAdjustSampleNorm.py
@@ -6,7 +6,8 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from mantid.api import AlgorithmFactory, FileAction, FileProperty, PythonAlgorithm, PropertyMode, \
     MultipleFileProperty, WorkspaceProperty
-from mantid.kernel import Direction, V3D, FloatArrayProperty, FloatArrayLengthValidator
+from mantid.kernel import Direction, EnabledWhenProperty, PropertyCriterion, V3D, FloatArrayProperty, \
+    FloatArrayLengthValidator
 from mantid.simpleapi import DeleteWorkspace, Load, ConvertWANDSCDtoQ
 import os
 
@@ -28,6 +29,7 @@ class SCDAdjustSampleNorm(PythonAlgorithm):
                'and converts to Q-space. '
 
     def PyInit(self):
+        # Input params
         self.declareProperty(MultipleFileProperty(name="Filename",
                                                   extensions=["_event.nxs", ".nxs.h5", ".nxs"]),
                              "Input autoreduced detector scan data files to convert to Q-space.")
@@ -36,11 +38,28 @@ class SCDAdjustSampleNorm(PythonAlgorithm):
                          action=FileAction.Load),
             doc="File with Vanadium normalization scan data")
 
+        # Detector adjustment options
         self.declareProperty("DetectorHeightOffset", defaultValue=0.0, direction=Direction.Input,
                              doc="Optional distance to move detector height (relative to current position)")
         self.declareProperty("DetectorDistanceOffset", defaultValue=0.0, direction=Direction.Input,
                              doc="Optional distance to move detector distance (relative to current position)")
 
+        # Which conversion algorithm to use
+        self.declareProperty("OutputAsMDEventWorkspace", defaultValue=True, direction=Direction.Input,
+                             doc="Whether to use ConvertHFIRSCDtoQ for an MDEvent, or ConvertWANDSCDtoQ for an MDHisto")
+
+        # MDEvent WS Specific options for ConvertHFIRSCDtoQ
+        self.declareProperty(FloatArrayProperty("MinValues", [-10, -10, -10], FloatArrayLengthValidator(3),
+                                                direction=Direction.Input),
+                             doc="3 comma separated values, one for each q_sample dimension")
+        self.declareProperty(FloatArrayProperty("MaxValues", [10, 10, 10], FloatArrayLengthValidator(3),
+                                                direction=Direction.Input),
+                             doc="3 comma separated values, one for each q_sample dimension; must be larger than"
+                                 "those specified in MinValues")
+        self.declareProperty("MergeInputs", defaultValue=False, direction=Direction.Input,
+                             doc="If all inputs should be merged into one MDEvent output workspace")
+
+        # MDHisto WS Specific options for ConvertWANDSCDtoQ
         self.declareProperty(FloatArrayProperty("BinningDim0", [-8.02, 8.02, 401], FloatArrayLengthValidator(3),
                                                 direction=Direction.Input),
                              "Binning parameters for the 0th dimension. Enter it as a"
@@ -57,6 +76,22 @@ class SCDAdjustSampleNorm(PythonAlgorithm):
                              "comma-separated list of values with the"
                              "format: 'minimum,maximum,number_of_bins'.")
 
+        event_settings = EnabledWhenProperty('OutputAsMDEventWorkspace', PropertyCriterion.IsDefault)
+        self.setPropertyGroup("MinValues", "MDEvent Settings")
+        self.setPropertyGroup("MaxValues", "MDEvent Settings")
+        self.setPropertyGroup("MergeInputs", "MDEvent Settings")
+        self.setPropertySettings("MinValues", event_settings)
+        self.setPropertySettings("MaxValues", event_settings)
+        self.setPropertySettings("MergeInputs", event_settings)
+
+        histo_settings = EnabledWhenProperty('OutputAsMDEventWorkspace', PropertyCriterion.IsNotDefault)
+        self.setPropertyGroup("BinningDim0", "MDHisto Settings")
+        self.setPropertyGroup("BinningDim1", "MDHisto Settings")
+        self.setPropertyGroup("BinningDim2", "MDHisto Settings")
+        self.setPropertySettings("BinningDim0", histo_settings)
+        self.setPropertySettings("BinningDim1", histo_settings)
+        self.setPropertySettings("BinningDim2", histo_settings)
+
         self.declareProperty(
             WorkspaceProperty("OutputWorkspace", "", optional=PropertyMode.Mandatory, direction=Direction.Output),
             doc="Output MDWorkspace in Q-space, name is prefix if multiple input files were provided.")
@@ -66,9 +101,16 @@ class SCDAdjustSampleNorm(PythonAlgorithm):
         vanadiumfile = self.getProperty("VanadiumFile").value
         height = self.getProperty("DetectorHeightOffset").value
         distance = self.getProperty("DetectorDistanceOffset").value
-        bin0 = self.getProperty("BinningDim0").value
-        bin1 = self.getProperty("BinningDim1").value
-        bin2 = self.getProperty("BinningDim2").value
+        method = self.getProperty("OutputAsMDEventWorkspace").value
+
+        if method:
+            minvals = self.getProperty("MinValues").value
+            maxvals = self.getProperty("MaxValues").value
+        else:
+            bin0 = self.getProperty("BinningDim0").value
+            bin1 = self.getProperty("BinningDim1").value
+            bin2 = self.getProperty("BinningDim2").value
+
         out_ws = self.getPropertyValue("OutputWorkspace")
         out_ws_name = out_ws
 
@@ -92,7 +134,7 @@ class SCDAdjustSampleNorm(PythonAlgorithm):
 
             # Adjust detector height and distance with new offsets
             if height != 0.0 or distance != 0.0:
-                # Move all the instrument banks
+                # Move all the instrument banks (MoveInstrumentComponents does not work on MDHisto Workspaces)
                 component = exp_info.componentInfo()
                 for bank in range(1, 4):
                     index = component.indexOfAny("bank{}".format(bank))
@@ -104,15 +146,19 @@ class SCDAdjustSampleNorm(PythonAlgorithm):
 
                     component.setPosition(index, offset)
 
-            # Get the wavelength from the file if it exists
-            if (exp_info.run().hasProperty("wavelength")):
+            # Get the wavelength from the file sample logs if it exists
+            if exp_info.run().hasProperty("wavelength"):
                 wavelength = exp_info.run().getProperty("wavelength").value
 
-            # Convert to Q space and normalize with from the vanadium
-            ConvertWANDSCDtoQ(InputWorkspace=scan, NormalisationWorkspace=van_norm, Frame='Q_sample',
-                              Wavelength=wavelength, NormaliseBy='Monitor', BinningDim0=bin0, BinningDim1=bin1,
-                              BinningDim2=bin2,
-                              OutputWorkspace=out_ws_name)
+            # Use ConvertHFIRSCDtoQ (and normalize van), or use ConvertWANDSCtoQ which handles normalization itself
+            if method:
+
+            else:
+                # Convert to Q space and normalize with from the vanadium
+                ConvertWANDSCDtoQ(InputWorkspace=scan, NormalisationWorkspace=van_norm, Frame='Q_sample',
+                                  Wavelength=wavelength, NormaliseBy='Monitor', BinningDim0=bin0, BinningDim1=bin1,
+                                  BinningDim2=bin2,
+                                  OutputWorkspace=out_ws_name)
 
         DeleteWorkspace(van_norm)
         DeleteWorkspace(scan)
