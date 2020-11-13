59a565437464f1bf9b1d61ed25957d0f192818c5
Zhang, Chen <zhangc@ornl.gov>
2020-11-13 11:06:01 -0500

    add new unittest for saving reduced data feature

--- Framework/PythonInterface/test/python/plugins/algorithms/HB2AReduceTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/HB2AReduceTest.py
@@ -5,11 +5,15 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 from mantid.simpleapi import HB2AReduce
+import os
+import tempfile
 import unittest
 import numpy as np
 
 
 class HB2AReduceTest(unittest.TestCase):
+    def setUp(self):
+        self._default_save_directory = tempfile.gettempdir()
 
     def test_IndividualDetectors(self):
         HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat',
@@ -82,6 +86,19 @@ class HB2AReduceTest(unittest.TestCase):
         self.assertEqual(HB2AReduce_ws.blocksize(), 56)
         self.assertEqual(np.argmax(HB2AReduce_ws.readY(7)), 2)
         self.assertAlmostEquals(np.max(HB2AReduce_ws.readY(7)), 2.13258433)
+        HB2AReduce_ws.delete()
+
+    def test_saving_files(self):
+        HB2AReduce_ws = HB2AReduce(
+            'HB2A_exp0660_scan0146.dat',
+            Vanadium='HB2A_exp0644_scan0018.dat',
+            IndividualDetectors=True,
+            OutputWorkspace="HB2AReduce_ws",
+            OutputDirectory=self._default_save_directory,
+        )
+        self.assertTrue(HB2AReduce_ws)
+        self.assertTrue(
+            os.path.exists(os.path.join(self._default_save_directory, "HB2AReduce_ws.gss")))
         HB2AReduce_ws.delete()
 
 
