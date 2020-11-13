a6c2f1741796a0463900c5a1177e8de7aac3d646
Zhang, Chen <zhangc@ornl.gov>
2020-11-13 11:05:40 -0500

    turn off SaveData for previous unittest

--- Framework/PythonInterface/test/python/plugins/algorithms/HB2AReduceTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/HB2AReduceTest.py
@@ -13,7 +13,8 @@ class HB2AReduceTest(unittest.TestCase):
 
     def test_IndividualDetectors(self):
         HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat',
-                                   IndividualDetectors=True)
+                                   IndividualDetectors=True,
+                                   SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 44)
         self.assertEqual(HB2AReduce_ws.blocksize(), 121)
@@ -22,7 +23,7 @@ class HB2AReduceTest(unittest.TestCase):
         HB2AReduce_ws.delete()
 
     def test_NotBinned(self):
-        HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat', BinData=False)
+        HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat', BinData=False, SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 1)
         self.assertEqual(HB2AReduce_ws.blocksize(), 5324)
@@ -31,7 +32,7 @@ class HB2AReduceTest(unittest.TestCase):
         HB2AReduce_ws.delete()
 
     def test_Binned(self):
-        HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat')
+        HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat', SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 1)
         self.assertEqual(HB2AReduce_ws.blocksize(), 2439)
@@ -40,7 +41,8 @@ class HB2AReduceTest(unittest.TestCase):
         HB2AReduce_ws.delete()
 
     def test_TwoFiles(self):
-        HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat,HB2A_exp0666_scan0025.dat')
+        HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat,HB2A_exp0666_scan0025.dat',
+                                   SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 1)
         self.assertEqual(HB2AReduce_ws.blocksize(), 2439)
@@ -50,7 +52,8 @@ class HB2AReduceTest(unittest.TestCase):
 
     def test_Vanadium(self):
         HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat',
-                                   Vanadium='HB2A_exp0644_scan0018.dat')
+                                   Vanadium='HB2A_exp0644_scan0018.dat',
+                                   SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 1)
         self.assertEqual(HB2AReduce_ws.blocksize(), 2439)
@@ -60,7 +63,8 @@ class HB2AReduceTest(unittest.TestCase):
 
     def test_ExcludeDetectors(self):
         HB2AReduce_ws = HB2AReduce('HB2A_exp0666_scan0024.dat',
-                                   ExcludeDetectors='1-20,40-42')
+                                   ExcludeDetectors='1-20,40-42',
+                                   SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 1)
         self.assertEqual(HB2AReduce_ws.blocksize(), 1360)
@@ -71,7 +75,8 @@ class HB2AReduceTest(unittest.TestCase):
     def test_anode_vs_temp(self):
         HB2AReduce_ws = HB2AReduce('HB2A_exp0660_scan0146.dat',
                                    Vanadium='HB2A_exp0644_scan0018.dat',
-                                   IndividualDetectors=True)
+                                   IndividualDetectors=True,
+                                   SaveData=False)
         self.assertTrue(HB2AReduce_ws)
         self.assertEqual(HB2AReduce_ws.getNumberHistograms(), 44)
         self.assertEqual(HB2AReduce_ws.blocksize(), 56)
