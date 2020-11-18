f75bf8eb6ed4450c44d0bc3b0460558e9aeb4f00
Zhang, Chen <zhangc@ornl.gov>
2020-11-18 14:55:07 -0500

    skeleton unittest file

--- Framework/Algorithms/test/CorelliPowderCalibrationApplyTest.h
+++ Framework/Algorithms/test/CorelliPowderCalibrationApplyTest.h
@@ -5,3 +5,34 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/IAlgorithm.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/SpectrumInfo.h"
+#include "MantidAPI/TableRow.h"
+#include "MantidAPI/Workspace.h"
+#include "MantidAlgorithms/CorelliPowderCalibrationApply.h"
+#include "MantidAPI/ITableWorkspace.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidGeometry/Instrument/ComponentInfo.h"
+#include "MantidGeometry/Instrument/DetectorInfo.h"
+#include "MantidGeometry/Objects/IObject.h"
+#include "MantidKernel/Logger.h"
+
+#include <stdexcept>
+
+using namespace Mantid::Algorithms;
+using namespace Mantid::API;
+using namespace Mantid::Kernel;
+
+class CorelliPowderCalibrationAppyTest : public CxxTest::TestSuite {
+    public:
+    void testName() {
+        CorelliPowderCalibrationApply corelliPCA;
+        TS_ASSERT_EQUALS(corelliPCA.name(), "CorelliPowderCalibrationApply");
+    }
+};
