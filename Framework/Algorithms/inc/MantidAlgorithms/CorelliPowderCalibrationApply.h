36a3d11786c5ed19588093deb8d5850be386cd53
Zhang, Chen <zhangc@ornl.gov>
2020-11-18 11:56:48 -0500

    add place holders

---
+++ Framework/Algorithms/inc/MantidAlgorithms/CorelliPowderCalibrationApply.h
@@ -0,0 +1,21 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2014 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+
+#pragma once
+
+#include "MantidAPI/Algorithm.h"
+#include "MantidAlgorithms/DllConfig.h"
+#include "MantidDataObjects/EventWorkspace.h"
+
+namespace Mantid
+{
+    namespace Algorithms
+    {
+        
+    } // namespace Algorithms
+    
+} // namespace Mantid
