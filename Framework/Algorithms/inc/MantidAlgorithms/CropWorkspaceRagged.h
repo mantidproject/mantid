069adc4c002978aa9e5ab192695a2254ba1cd805
Anthony Lim <anthony.lim@stfc.ac.uk>
2020-11-20 17:03:02 +0000

    refs #29761 C++ version of crop workspace ragged

---
+++ Framework/Algorithms/inc/MantidAlgorithms/CropWorkspaceRagged.h
@@ -0,0 +1,81 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2008 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidAPI/DistributedAlgorithm.h"
+#include "MantidAlgorithms/DllConfig.h"
+
+namespace Mantid {
+namespace Algorithms {
+/** Extracts a 'block' from a workspace and places it in a new workspace.
+    (Or, to look at it another way, lops bins or spectra off a workspace).
+
+    Required Properties:
+    <UL>
+    <LI> InputWorkspace - The name of the Workspace2D to take as input. </LI>
+    <LI> OutputWorkspace - The name under which to store the output workspace.
+   </LI>
+    </UL>
+
+    Optional Properties:
+    <UL>
+    <LI> XMin - The X value to start the cropped workspace at (default 0)</LI>
+    <LI> XMax - The X value to end the cropped workspace at (default max)</LI>
+    <LI> StartSpectrum - The workspace index number to start the cropped
+   workspace from (default 0)</LI>
+    <LI> EndSpectrum - The workspace index number to end the cropped workspace
+   at (default max)</LI>
+    </UL>
+
+    If the input workspace must has common bin boundaries/X values then cropping
+   in X will
+    lead to an output workspace with fewer bins than the input one. If the
+   boundaries are
+    not common then the output workspace will have the same number of bins as
+   the input one,
+    but with data values outside the X range given set to zero.
+    If an X value given does not exactly match a bin boundary, then the closest
+   bin boundary
+    within the range will be used.
+    Note that if none of the optional properties are given, then the output
+   workspace will be
+    a copy of the input one.
+
+    @author Russell Taylor, Tessella Support Services plc
+    @date 15/10/2008
+*/
+class MANTID_ALGORITHMS_DLL CropWorkspaceRagged
+    : public API::DistributedAlgorithm {
+public:
+  /// Algorithm's name
+  const std::string name() const override { return "CropWorkspaceRagged"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Extracts a 'block' from a workspace and places it in a new "
+           "workspace.";
+  }
+
+  /// Algorithm's version
+  int version() const override { return (1); }
+  const std::vector<std::string> seeAlso() const override {
+    return {"CropWorkspace"};
+  }
+  /// Algorithm's category for identification
+  const std::string category() const override {
+    return "Transforms\\Splitting";
+  }
+
+private:
+  /// Initialisation code
+  void init() override;
+  /// Execution code
+  void exec() override;
+  std::map<std::string, std::string> validateInputs();
+};
+
+} // namespace Algorithms
+} // namespace Mantid
---
+++ Framework/Algorithms/src/CropWorkspaceRagged.cpp
@@ -0,0 +1,139 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidAlgorithms/CropWorkspaceRagged.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/WorkspaceFactory.h"
+#include "MantidKernel/ArrayProperty.h"
+#include "MantidKernel/BoundedValidator.h"
+#include "MantidKernel/MandatoryValidator.h"
+
+namespace Mantid {
+namespace Algorithms {
+
+using namespace Kernel;
+using namespace API;
+
+DECLARE_ALGORITHM(CropWorkspaceRagged)
+
+/// Init function
+void CropWorkspaceRagged::init() {
+  declareProperty(std::make_unique<WorkspaceProperty<MatrixWorkspace>>(
+                      "InputWorkspace", "",
+                                                        Direction::Input),
+                  "The input workspace");
+  declareProperty(std::make_unique<WorkspaceProperty<MatrixWorkspace>>(
+                      "OutputWorkspace", "",
+                                                        Direction::Output),
+                  "Name to be given to the cropped workspace.");
+
+  auto required = std::make_shared<MandatoryValidator<std::vector<double>>>();
+  declareProperty(std::make_unique<ArrayProperty<double>>("XMin", required),
+                  ".");
+  declareProperty(std::make_unique<ArrayProperty<double>>("XMax", required),
+                  "Y-axis data values for workspace (measures).");
+}
+
+/// Input validation
+std::map<std::string, std::string> CropWorkspaceRagged::validateInputs() {
+  std::map<std::string, std::string> issues;
+  MatrixWorkspace_sptr ws = getProperty("InputWorkspace");
+  auto numSpectra = ws->getNumberHistograms();
+  std::vector<double> xMin = getProperty("XMin");
+  std::vector<double> xMax = getProperty("XMax");
+  if (xMin.size() == 0 || (xMin.size() != numSpectra &&xMin.size() > 1) ) {
+    issues["XMin"] =
+        "Either one XMin value must be a single value or one value per spectrum.";
+  }
+  if (xMax.size() == 0 || (xMax.size() > 1 && xMax.size() != numSpectra)) {
+    issues["XMax"] =
+        "Either one XMax value must be a single value or one value per spectrum.";
+  }
+  return issues;
+}
+
+/// Exec function
+void CropWorkspaceRagged::exec() {
+  MatrixWorkspace_sptr ws = getProperty("InputWorkspace");
+  auto numSpectra = ws->getNumberHistograms();
+  // clone ws to copy logs etc.
+  MatrixWorkspace_sptr outputWS = ws->clone();
+
+  std::vector<double> xMin = getProperty("XMin");
+  std::vector<double> xMax = getProperty("XMax");
+  if (xMin.size() == 1) {
+    auto value = xMin[0];
+    xMin.assign(numSpectra, value);
+  }
+  if (xMax.size() == 1) {
+    auto value = xMax[0];
+    xMax.assign(numSpectra, value);
+  }
+
+  // Its easier to work with point data -> index is same for x, y, E
+  bool pointData = false;
+  if (outputWS->isHistogramData()) {
+    auto alg = createChildAlgorithm("ConvertToPointData");
+    alg->initialize();
+    alg->setRethrows(true);
+    alg->setProperty("InputWorkspace", outputWS);
+    alg->setProperty("OutputWorkspace", outputWS);
+    alg->execute();
+    outputWS = alg->getProperty("OutputWorkspace");
+  }
+
+  MatrixWorkspace_sptr tmp;
+  for (int64_t i = 0; i < int64_t(numSpectra); ++i) {
+
+    auto &dataX = outputWS->dataX(i);
+    auto &dataY = outputWS->dataY(i);
+    auto &dataE = outputWS->dataE(i);
+
+    // get iterators for cropped region
+    auto low =
+        std::lower_bound(dataX.begin(), dataX.end(), xMin[i]);
+    auto up = std::upper_bound(dataX.begin(), dataX.end(), xMax[i]);
+    // convert to index
+    auto lowerIndex = std::distance(dataX.begin(), low);
+    auto upperIndex = std::distance(dataX.begin(), up);
+
+    // get new vectors
+    std::vector<double> newX(low, up);
+
+    low = std::next(dataY.begin(), lowerIndex);
+    up = std::next(dataY.begin(), upperIndex);
+    std::vector<double> newY(low, up);
+
+    low = std::next(dataE.begin(), lowerIndex);
+    up = std::next(dataE.begin(), upperIndex);
+    std::vector<double> newE(low, up);
+
+    // resize the data
+    dataX.resize(newX.size());
+    dataY.resize(newY.size());
+    dataE.resize(newE.size());
+
+    // update the data
+    outputWS->setPoints(i, std::move(newX));
+    outputWS->setCounts(i, std::move(newY));
+    outputWS->mutableE(i) = std::move(newE);
+  }
+  // return the same ws type as we recieved
+  if (pointData) {
+    auto alg = createChildAlgorithm("ConvertToHistogram");
+    alg->initialize();
+    alg->setRethrows(true);
+    alg->setProperty("InputWorkspace", outputWS);
+    alg->setProperty("OutputWorkspace", outputWS);
+    alg->execute();
+    outputWS = alg->getProperty("OutputWorkspace");
+  }
+
+  setProperty("OutputWorkspace", outputWS);
+}
+
+} // namespace Algorithms
+} // namespace Mantid
