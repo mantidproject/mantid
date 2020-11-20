c982eea38c46c272aef0a5115bb7f3ba5d1f8be0
Zhang, Chen <zhangc@ornl.gov>
2020-11-20 15:45:45 -0500

    add pure unit test for this algorithm

--- Framework/Algorithms/test/CorelliPowderCalibrationApplyTest.h
+++ Framework/Algorithms/test/CorelliPowderCalibrationApplyTest.h
@@ -31,6 +31,9 @@ using namespace Mantid::API;
 using namespace Mantid::Kernel;
 using namespace Mantid::DataObjects;
 
+///
+/// The base class CxxTest doc is available at
+/// http://cxxtest.com/guide.html
 class CorelliPowderCalibrationAppyTest : public CxxTest::TestSuite {
     public:
     void testName() {
@@ -44,21 +47,121 @@ class CorelliPowderCalibrationAppyTest : public CxxTest::TestSuite {
         TS_ASSERT(crlCalApp.isInitialized());
     }
 
-    void testValidateWS(){
+    void testValidateWS() {
+        // get a mock workspace with wrong instrument name
+        IAlgorithm_sptr lei =
+            AlgorithmFactory::Instance().create("LoadEmptyInstrument", 1);
+        lei->initialize();
+        lei->setPropertyValue("Filename", "NOW4_Definition.xml");
+        lei->setPropertyValue("OutputWorkspace", "wrongTypeWs");
+        lei->setPropertyValue("MakeEventWorkspace", "1");
+        lei->execute();
+        auto ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>("wrongTypeWs");
 
+        // get a mock calTable
+        std::string calTableName = "correctCalTable";
+        auto calTable = createTestCalibrationTableWorkspace(calTableName);
+
+        // setup alg
+        CorelliPowderCalibrationApply alg;
+        alg.initialize();
+        alg.setPropertyValue("Workspace", "wrongTypeWs");
+        alg.setPropertyValue("CalibrationTable", calTableName);
+
+        // ensure that an exception is thrown here
+        TS_ASSERT_THROWS(alg.execute(), const std::runtime_error &);
     }
 
     void testValidateCalTable() {
+        // get a mock workspace with correct instrument name
+        auto ws = createTestEventWorkspace();
 
+        // get a mock calTable with wrong header
+        std::string calTableName = "wrongCalTable";
+        auto calTable = createTestCalibrationTableWorkspace(calTableName);
+        calTable->removeRow(1);
+
+        // setup alg
+        CorelliPowderCalibrationApply alg;
+        alg.initialize();
+        alg.setPropertyValue("Workspace", "CorelliPowderCalibrationApplyTest_matrixWS");
+        alg.setPropertyValue("CalibrationTable", calTableName);
+
+        // ensure that an exception is thrown here
+        TS_ASSERT_THROWS(alg.execute(), const std::runtime_error &);
     }
 
     void testExec() {
-        // setup input workspace ??
+        // setup input workspace
+        auto ws = createTestEventWorkspace();
 
-        // setup input calibration table
-        ITableWorkspace_sptr calTable = WorkspaceFactory::Instance().createTable();
+        // get a correct mock calibration table
+        std::string calTableName = "wrongCalTable";
+        auto calTable = createTestCalibrationTableWorkspace(calTableName);
 
-        // test for what??
+        // setup alg
+        CorelliPowderCalibrationApply alg;
+        alg.initialize();
+        alg.setPropertyValue("Workspace", "CorelliPowderCalibrationApplyTest_matrixWS");
+        alg.setPropertyValue("CalibrationTable", calTableName);
+
+        // make sure no exception is thrown here
+        TS_ASSERT_THROWS_NOTHING(alg.execute())
+
     }
+
+    private:
+    EventWorkspace_sptr createTestEventWorkspace() {
+        // Name of the output workspace.
+        std::string outWSName("CorelliPowderCalibrationApplyTest_matrixWS");
+
+        IAlgorithm_sptr lei =
+            AlgorithmFactory::Instance().create("LoadEmptyInstrument", 1);
+        lei->initialize();
+        lei->setPropertyValue("Filename", "CORELLI_Definition.xml");
+        lei->setPropertyValue("OutputWorkspace",
+                            "CorelliPowderCalibrationApplyTest_OutputWS");
+        lei->setPropertyValue("MakeEventWorkspace", "1");
+        lei->execute();
+
+        EventWorkspace_sptr ws;
+        ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>(
+            "CorelliPowderCalibrationApplyTest_OutputWS");
+        return ws;
+    }
+
+    TableWorkspace_sptr
+    createTestCalibrationTableWorkspace(const std::string &outWSName) {
+
+        ITableWorkspace_sptr itablews = WorkspaceFactory::Instance().createTable();
+        AnalysisDataService::Instance().addOrReplace(outWSName, itablews);
+
+        TableWorkspace_sptr tablews =
+            std::dynamic_pointer_cast<TableWorkspace>(itablews);
+        TS_ASSERT(tablews);
+
+        // Set up columns
+        for (size_t i = 0;
+            i < CorelliCalibration::calibrationTableColumnNames.size(); i++) {
+            std::string colname = CorelliCalibration::calibrationTableColumnNames[i];
+            std::string type = CorelliCalibration::calibrationTableColumnTypes[i];
+            tablews->addColumn(type, colname);
+        }
+
+        // append rows
+        Mantid::API::TableRow sourceRow = tablews->appendRow();
+        sourceRow << "source" << 0. << 0. << -15.560 << 0. << 0. << 0. << 0.;
+        Mantid::API::TableRow sampleRow = tablews->appendRow();
+        sampleRow << "sample" << 0.0001 << -0.0002 << 0.003 << 0. << 0. << 0. << 0.;
+        Mantid::API::TableRow bank1Row = tablews->appendRow();
+        bank1Row << "bank1" << 0.9678 << 0.0056 << 0.0003 << 0.4563 << -0.9999 << 0.3424 << 5.67;
+        // bank42 is at the x-axis (transverse of beam direction)
+        // rotating 180 should reverse its bottom pixel (1) and top pixel (256)
+        TableRow bank42Row = tablews->appendRow();
+        bank42Row << "bank42" << 0. << 0. << 0. << 1. << 0. << 0. << 180.;
+
+        return tablews;
+  }
+
 
 };
