e20ca542704c26c088df3a77aa96830e7615962c
Zhang, Chen <zhangc@ornl.gov>
2020-11-24 09:32:35 -0500

    add system test

--- Testing/SystemTests/tests/framework/CorelliPowderCalibrationTest.py
+++ Testing/SystemTests/tests/framework/CorelliPowderCalibrationTest.py
@@ -9,6 +9,8 @@ from mantid.simpleapi import CreateEmptyTableWorkspace
 from mantid.simpleapi import LoadEmptyInstrument
 from mantid.simpleapi import ConvertToEventWorkspace
 from mantid.simpleapi import CompareWorkspaces
+from mantid.simpleapi import MoveInstrumentComponent
+from mantid.simpleapi import RotateInstrumentComponent
 from mantid.simpleapi import CorelliPowderCalibrationApply
 
 
@@ -54,6 +56,64 @@ class CorelliPowderCalibrationApply(MantidSystemTest):
         for cp in _cpts:
             _calitab.addRow(cp)
 
+    def get_target_instrument(self):
+        self.ws_target = "ws_target"
+        LoadEmptyInstrument(Filename="CORELLI_Definition.xml", OutputWorkspace=self.ws_target)
+        ConvertToEventWorkspace(InputWorkspace=self.ws_target, OutputWorkspace=self.ws_target)
+        # explicitly translate component TO designated location
+        MoveInstrumentComponent(Workspace=self.ws_target,
+                                ComponentName="moderator",
+                                X=0,
+                                Y=0,
+                                Z=-19.9997,
+                                RelativePosition=False)
+        MoveInstrumentComponent(Workspace=self.ws_target,
+                                ComponentName="sample-position",
+                                X=0,
+                                Y=0,
+                                Z=0,
+                                RelativePosition=False)
+        MoveInstrumentComponent(Workspace=self.ws_target,
+                                ComponentName="bank7/sixteenpack",
+                                X=2.25637,
+                                Y=-0.814864,
+                                Z=-0.883485,
+                                RelativePosition=False)
+        MoveInstrumentComponent(Workspace=self.ws_target,
+                                ComponentName="bank42/sixteenpack",
+                                X=2.58643,
+                                Y=0.0725628,
+                                Z=0.0868798,
+                                RelativePosition=False)
+        MoveInstrumentComponent(Workspace=self.ws_target,
+                                ComponentName="bank57/sixteenpack",
+                                X=0.4545,
+                                Y=0.0788326,
+                                Z=2.53234,
+                                RelativePosition=False)
+        # explicitly rotate component TO designated orientation
+        RotateInstrumentComponent(Workspace=self.ws_target,
+                                  ComponentName="bank7/sixteenpack",
+                                  X=-0.0244456,
+                                  Y=-0.99953,
+                                  Z=-0.0184843,
+                                  Angle=69.4926,
+                                  RelativePosition=False)
+        RotateInstrumentComponent(Workspace=self.ws_target,
+                                  ComponentName="bank42/sixteenpack",
+                                  X=-0.011362,
+                                  Y=-0.999935,
+                                  Z=-0.000173303,
+                                  Angle=91.8796,
+                                  RelativePosition=False)
+        RotateInstrumentComponent(Workspace=self.ws_target,
+                                  ComponentName="bank42/sixteenpack",
+                                  X=-0.0158497,
+                                  Y=-0.999694,
+                                  Z=0.0189818,
+                                  Angle=169.519,
+                                  RelativePosition=False)
+
     def runTest(self):
         # apply the calibration to a reference CORELLI instrument
         self.ws_reference = "ws_ref"
@@ -66,13 +126,15 @@ class CorelliPowderCalibrationApply(MantidSystemTest):
         # ensure that the calibration application take place
         _ws_ref = mtd[self.ws_reference]
         _ws_cal = mtd[self.ws_calbrated]
-        rst, msg = CompareWorkspaces(_ws_ref, _ws_cal)
+        rst, msg = CompareWorkspaces(_ws_ref, _ws_tgt)
         if rst is True:
             raise ValueError("The Calibration is not applied to workspace")
 
-        # check if each component is in the correct location
-        # 372739 moderator
-        # 372740 sample-position
-        # 372870 bank7
-        # 373501 bank42
-        # 373771 bank57
+        # get the target ws
+        self.get_target_instrument()
+        _ws_tgt = mtd[self.ws_target]
+
+        # check if the calibrated workspace matches the target
+        rst, msg = CompareWorkspaces(_ws_tgt, _ws_tgt)
+        if rst is False:
+            raise ValueError("The calibration did not return correct results.")
