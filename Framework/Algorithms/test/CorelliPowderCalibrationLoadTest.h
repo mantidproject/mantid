738911f7d3a009a2d3a15a30949dd9af255be7c3
Zhang, Chen <zhangc@ornl.gov>
2020-12-02 13:09:44 -0500

    finish unit test

--- Framework/Algorithms/test/CorelliPowderCalibrationLoadTest.h
+++ Framework/Algorithms/test/CorelliPowderCalibrationLoadTest.h
@@ -9,7 +9,9 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/Run.h"
 #include "MantidAlgorithms/CorelliPowderCalibrationLoad.h"
+#include "MantidDataObjects/EventWorkspace.h"
 #include "MantidKernel/Logger.h"
 
 #include <stdexcept>
@@ -35,5 +37,74 @@ class CorelliPowderCalibrationLoadTest : public CxxTest::TestSuite {
             TS_ASSERT(corelliLoader.isInitialized());
         }
 
-        //TODO: more unit test to come
+        void testValidateWSType() {
+            // generate a mock workspace with wrong instrument name
+            IAlgorithm_sptr lei =
+            AlgorithmFactory::Instance().create("LoadEmptyInstrument", 1);
+            lei->initialize();
+            lei->setPropertyValue("Filename", "NOW4_Definition.xml");
+            lei->setPropertyValue("OutputWorkspace", "wrongTypeWs");
+            lei->setPropertyValue("MakeEventWorkspace", "1");
+            lei->execute();
+            EventWorkspace_sptr ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>("wrongTypeWs");
+            ws->mutableRun().addProperty<std::string>("start_time", "2020-11-17T12:57:17", "", true);
+
+            // setup alg
+            CorelliPowderCalibrationLoad alg;
+            alg.initialize();
+            alg.setPropertyValue("InputWorkspace", "wrongTypeWs");
+            alg.setPropertyValue("DatabaseDir", ".");
+            alg.setPropertyValue("OutputWorkspace", "outWS");
+
+            // ensure that an exception is thrown here
+            TS_ASSERT_THROWS(alg.execute(), const std::runtime_error &);
+        }
+
+        void testValidateWSTime() {
+            // generate a mock workspace with correct instrument name
+            IAlgorithm_sptr lei =
+            AlgorithmFactory::Instance().create("LoadEmptyInstrument", 1);
+            lei->initialize();
+            lei->setPropertyValue("Filename", "CORELLI_Definition.xml");
+            lei->setPropertyValue("OutputWorkspace", "correctTypeWs");
+            lei->setPropertyValue("MakeEventWorkspace", "1");
+            lei->execute();
+            EventWorkspace_sptr ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>("correctTypeWs");
+
+            // setup alg
+            CorelliPowderCalibrationLoad alg;
+            alg.initialize();
+            alg.setPropertyValue("InputWorkspace", "correctTypeWs");
+            alg.setPropertyValue("DatabaseDir", ".");
+            alg.setPropertyValue("OutputWorkspace", "outWS");
+
+            // ensure that an exception is thrown here
+            TS_ASSERT_THROWS(alg.execute(), const std::runtime_error &);
+        }
+
+        void testExec() {
+            // generate a mock workspace with correct instrument name
+            IAlgorithm_sptr lei =
+            AlgorithmFactory::Instance().create("LoadEmptyInstrument", 1);
+            lei->initialize();
+            lei->setPropertyValue("Filename", "CORELLI_Definition.xml");
+            lei->setPropertyValue("OutputWorkspace", "correctTypeWs");
+            lei->setPropertyValue("MakeEventWorkspace", "1");
+            lei->execute();
+            EventWorkspace_sptr ws = AnalysisDataService::Instance().retrieveWS<EventWorkspace>("correctTypeWs");
+
+            // add a starting time to log
+            ws->mutableRun().addProperty<std::string>("start_time", "2020-11-17T12:57:17", "", true);
+
+            // setup alg
+            CorelliPowderCalibrationLoad alg;
+            alg.initialize();
+            alg.setPropertyValue("InputWorkspace", "correctTypeWs");
+            alg.setPropertyValue("DatabaseDir", ".");
+            alg.setPropertyValue("OutputWorkspace", "outWS");
+
+            // make sure no exception is thrown here
+            TS_ASSERT_THROWS_NOTHING(alg.execute())
+        }
+
 };
