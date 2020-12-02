59f564cb587304a562e702165002972ef0a84a6b
Wenduo Zhou <zhouw@ornl.gov>
2020-12-02 10:16:09 -0500

    Refs #29987. Added skeleton PythonStdoutChannel.

---
+++ Framework/Kernel/src/PythonStdoutChannel.cpp
@@ -0,0 +1,17 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidKernel/PythonStdoutChannel.h"
+#include <Poco/Message.h>
+#include <iostream>
+
+#include <boost/iostreams/categories.hpp> // sink_tag
+#include <iosfwd>                         // streamsize
+
+namespace Poco {
+PythonStdoutChannel::PythonStdoutChannel() : ConsoleChannel(std::cout) {}
+
+} // namespace Poco
---
+++ Framework/Kernel/test/PythonStdoutChannelTest.h
@@ -0,0 +1,73 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidKernel/Logger.h"
+#include "MantidKernel/PythonStdoutChannel.h"
+#include <Poco/AutoPtr.h>
+#include <Poco/Logger.h>
+#include <Poco/NullChannel.h>
+
+using namespace Mantid::Kernel;
+
+class PythonStdoutChannelTest : public CxxTest::TestSuite {
+public:
+  void testContructor() { TS_ASSERT_THROWS_NOTHING(Poco::PythonStdoutChannel a;) }
+
+  void testLogMessage() {
+    // Save root channel
+    Poco::Channel *rootChannel = Poco::Logger::root().getChannel();
+
+    std::stringstream obuffer, lbuffer;
+    // Save cout's and clog's buffers here
+    std::streambuf *obuf = std::cout.rdbuf();
+    std::streambuf *lbuf = std::clog.rdbuf();
+    // Redirect cout to buffer or any other ostream
+    std::cout.rdbuf(obuffer.rdbuf());
+    std::clog.rdbuf(lbuffer.rdbuf());
+    // root logger has empty name
+    Logger log("");
+
+    // Test null channel first
+    Poco::AutoPtr<Poco::NullChannel> nullChannel(new Poco::NullChannel);
+    Poco::Logger::root().setChannel(nullChannel);
+
+    log.error() << "Error Message 1\n";
+    // cout and clog should be empty
+    TS_ASSERT_EQUALS(obuffer.str(), "");
+    TS_ASSERT_EQUALS(lbuffer.str(), "");
+    obuffer.str("");
+    lbuffer.str("");
+
+    // Test console channel
+    Poco::AutoPtr<Poco::ConsoleChannel> consoleChannel(
+        new Poco::ConsoleChannel);
+    Poco::Logger::root().setChannel(consoleChannel);
+    log.error() << "Error Message 2\n";
+    // the error should be in std::clog (or std:err)
+    TS_ASSERT_EQUALS(obuffer.str(), "");
+    TS_ASSERT_EQUALS(lbuffer.str(), "Error Message 2\n");
+    obuffer.str("");
+    lbuffer.str("");
+
+    // Test std channel
+    Poco::AutoPtr<Poco::PythonStdoutChannel> stdoutChannel(new Poco::PythonStdoutChannel);
+    Poco::Logger::root().setChannel(stdoutChannel);
+    log.error() << "Error Message 3\n";
+    // the error should be in std::cout
+    TS_ASSERT_EQUALS(obuffer.str(), "Error Message 3\n");
+    TS_ASSERT_EQUALS(lbuffer.str(), "");
+
+    // When done redirect cout to its old self
+    std::cout.rdbuf(obuf);
+    std::clog.rdbuf(lbuf);
+    // set back the channel on root
+    Poco::Logger::root().setChannel(rootChannel);
+  }
+};
