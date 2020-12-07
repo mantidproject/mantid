cab3252adde612e077ad71883b80ae49dca760e8
Jose Borreguero <borreguero@gmail.com>
2020-12-07 17:43:02 -0500

    Refs #29960 address review
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- Testing/SystemTests/tests/framework/CorelliPowderCalibrationTest.py
+++ Testing/SystemTests/tests/framework/CorelliPowderCalibrationTest.py
@@ -20,7 +20,7 @@ class CorelliPowderCalibrationCreateTest(MantidSystemTest):
         LoadNexus(Filename='CORELLI_124036_banks42_87.nxs', OutputWorkspace='LaB6')
         CorelliPowderCalibrationCreate(InputWorkspace='LaB6',
                                        OutputWorkspacesPrefix='LaB6_',
-                                       TubeDatabaseDir='/SNS/users/jbq/Downloads/calibration_database',
+                                       TubeDatabaseDir='/tmp',
                                        TofBinning=[3000, -0.001, 16660],
                                        PeakFunction='Gaussian',
                                        PeakPositions=[1.3143, 1.3854, 1.6967, 1.8587, 2.0781, 2.3995, 2.9388, 4.1561],
