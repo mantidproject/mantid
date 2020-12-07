821e1b97b6e61a7ee47858fa6ac5a4f2617ac705
Robert Applin <robert.applin@stfc.ac.uk>
2020-12-07 17:11:20 +0000

    Refs #0. Clang format.

--- Framework/API/inc/MantidAPI/MatrixWorkspace.h
+++ Framework/API/inc/MantidAPI/MatrixWorkspace.h
@@ -165,7 +165,7 @@ public:
     return getSpectrum(index).histogram();
   }
   template <typename... T>
-  void setHistogram(const size_t index, T &&...data) & {
+  void setHistogram(const size_t index, T &&... data) & {
     getSpectrum(index).setHistogram(std::forward<T>(data)...);
   }
   void convertToCounts(const size_t index) {
@@ -184,19 +184,20 @@ public:
   pointStandardDeviations(const size_t index) const {
     return getSpectrum(index).pointStandardDeviations();
   }
-  template <typename... T> void setBinEdges(const size_t index, T &&...data) & {
+  template <typename... T>
+  void setBinEdges(const size_t index, T &&... data) & {
     getSpectrum(index).setBinEdges(std::forward<T>(data)...);
   }
-  template <typename... T> void setPoints(const size_t index, T &&...data) & {
+  template <typename... T> void setPoints(const size_t index, T &&... data) & {
     getSpectrum(index).setPoints(std::forward<T>(data)...);
   }
   template <typename... T>
-  void setPointVariances(const size_t index, T &&...data) & {
+  void setPointVariances(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setPointVariances(
         std::forward<T>(data)...);
   }
   template <typename... T>
-  void setPointStandardDeviations(const size_t index, T &&...data) & {
+  void setPointStandardDeviations(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setPointStandardDeviations(
         std::forward<T>(data)...);
   }
@@ -221,31 +222,31 @@ public:
   frequencyStandardDeviations(const size_t index) const {
     return getSpectrum(index).frequencyStandardDeviations();
   }
-  template <typename... T> void setCounts(const size_t index, T &&...data) & {
+  template <typename... T> void setCounts(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setCounts(std::forward<T>(data)...);
   }
   template <typename... T>
-  void setCountVariances(const size_t index, T &&...data) & {
+  void setCountVariances(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setCountVariances(
         std::forward<T>(data)...);
   }
   template <typename... T>
-  void setCountStandardDeviations(const size_t index, T &&...data) & {
+  void setCountStandardDeviations(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setCountStandardDeviations(
         std::forward<T>(data)...);
   }
   template <typename... T>
-  void setFrequencies(const size_t index, T &&...data) & {
+  void setFrequencies(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setFrequencies(
         std::forward<T>(data)...);
   }
   template <typename... T>
-  void setFrequencyVariances(const size_t index, T &&...data) & {
+  void setFrequencyVariances(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setFrequencyVariances(
         std::forward<T>(data)...);
   }
   template <typename... T>
-  void setFrequencyStandardDeviations(const size_t index, T &&...data) & {
+  void setFrequencyStandardDeviations(const size_t index, T &&... data) & {
     getSpectrumWithoutInvalidation(index).setFrequencyStandardDeviations(
         std::forward<T>(data)...);
   }
