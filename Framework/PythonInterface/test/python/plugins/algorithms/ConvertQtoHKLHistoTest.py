8b4ba060041e1d0895f817b631230a9aa0eb0363
Cole Kendrick <kendrickcj@ornl.gov>
2020-12-09 11:08:11 -0700

    Unit test for corelli data

--- Framework/PythonInterface/test/python/plugins/algorithms/ConvertQtoHKLHistoTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/ConvertQtoHKLHistoTest.py
@@ -5,7 +5,9 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
-from mantid.simpleapi import HB3AAdjustSampleNorm, HB3AFindPeaks, ConvertQtoHKLHisto, mtd
+from mantid.simpleapi import BinMD, ConvertToMD, ConvertQtoHKLHisto, HB3AAdjustSampleNorm, HB3AFindPeaks, Load, \
+    LoadIsawUB, mtd, SetGoniometer
+import numpy as np
 
 
 class ConvertQtoHKLHistoTest(unittest.TestCase):
@@ -43,6 +45,43 @@ class ConvertQtoHKLHistoTest(unittest.TestCase):
         self.assertAlmostEqual(peak.getH(), -0.995644, delta=1.0e-5)
         self.assertAlmostEqual(peak.getK(), 0.977317, delta=1.0e-5)
         self.assertAlmostEqual(peak.getL(), 3.93929, delta=1.0e-5)
+
+    def test_qtohkl_corelli(self):
+        Load(Filename='CORELLI_29782.nxs', OutputWorkspace='data')
+        SetGoniometer(Workspace='data', Axis0='BL9:Mot:Sample:Axis1,0,1,0,1')
+        LoadIsawUB(InputWorkspace='data', Filename='SingleCrystalDiffuseReduction_UB.mat')
+        ConvertToMD(InputWorkspace='data',
+                    QDimensions='Q3D',
+                    dEAnalysisMode='Elastic',
+                    Q3DFrames='HKL',
+                    QConversionScales='HKL',
+                    OutputWorkspace='HKL',
+                    Uproj='1,1,0',
+                    Vproj='1,-1,0',
+                    Wproj='0,0,1',
+                    MinValues='-10,-10,-10',
+                    MaxValues='10,10,10')
+        HKL_binned = BinMD('HKL',
+                           AlignedDim0='[H,H,0],-10.05,10.05,201',
+                           AlignedDim1='[H,-H,0],-10.05,10.05,201',
+                           AlignedDim2='[0,0,L],-1.05,1.05,21')
+        ConvertToMD(InputWorkspace='data',
+                    QDimensions='Q3D',
+                    dEAnalysisMode='Elastic',
+                    Q3DFrames='Q_sample',
+                    OutputWorkspace='q_sample',
+                    MinValues='-10,-10,-10',
+                    MaxValues='10,10,10')
+
+        hkl = ConvertQtoHKLHisto(InputWorkspace=mtd["q_sample"],
+                                 FindUBFromPeaks=False,
+                                 Extents="-10.05,10.05,-10.05,10.05,-1.05,1.05",
+                                 Bins="201,201,21")
+
+        orig_sig = HKL_binned.getSignalArray()
+        new_sig = hkl.getSignalArray()
+
+        np.testing.assert_allclose(np.asarray(new_sig), np.asarray(orig_sig))
 
 
 if __name__ == '__main__':
