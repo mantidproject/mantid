95e190ca8a7aeafc90653bcacc5f57d70ef83828
Jose Borreguero <borreguero@gmail.com>
2020-12-17 10:13:02 -0500

    Refs #30172 correct seeAlso list of algorithms
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- Framework/Algorithms/inc/MantidAlgorithms/CorelliPowderCalibrationLoad.h
+++ Framework/Algorithms/inc/MantidAlgorithms/CorelliPowderCalibrationLoad.h
@@ -41,9 +41,9 @@ public:
 
   /// Extra help info
   const std::vector<std::string> seeAlso() const override {
-    return {"CorelliPowderCalibrationCreate "
-            "& CorelliPowderCalibrationDatabase "
-            "& CorelliPowderCalibrationApply"};
+    return {"CorelliPowderCalibrationCreate",
+            "CorelliPowderCalibrationDatabase",
+            "CorelliPowderCalibrationApply"};
   };
 
 private:
