f493afed6893fb352ddecf13c9a1aafbcf5ef4a5
Dominik Arominski <arominski@ill.fr>
2020-12-17 09:51:27 +0100

    Added a system test for the multiple transmission mode

--- Testing/SystemTests/tests/framework/SANSILLAutoProcessTest.py
+++ Testing/SystemTests/tests/framework/SANSILLAutoProcessTest.py
@@ -172,6 +172,65 @@ class D11_AutoProcess_IQxQy_Test(systemtesting.MantidSystemTest):
         )
 
 
+class D11_AutoProcess_Multiple_Transmissions_Test(systemtesting.MantidSystemTest):
+    """
+    Tests auto process for D11 with 3 samples at 3 different distances,
+    and with multiple transmissions per process.
+    """
+
+    def __init__(self):
+        super(D11_AutoProcess_Multiple_Transmissions_Test, self).__init__()
+        self.setUp()
+
+    def setUp(self):
+        config['default.facility'] = 'ILL'
+        config['default.instrument'] = 'D11'
+        config['logging.loggers.root.level'] = 'Warning'
+        config.appendDataSearchSubDir('ILL/D11/')
+
+    def cleanup(self):
+        mtd.clear()
+
+    def validate(self):
+        self.tolerance = 1e-3
+        self.tolerance_is_rel_err = True
+        return ['out', 'D11_AutoProcess_Reference.nxs']
+
+    def runTest(self):
+
+        beams = '2866,2867+2868,2878'
+        containers = '2888+2971,2884+2960,2880+2949'
+        container_tr = '2870+2954'
+        beam_tr = '2867+2868'
+        samples = ['2889,2885,2881',
+                   '2887,2883,2879',
+                   '3187,3177,3167']
+        sample_tr = ['2871,2871,2871', '2869', '3172,3172,3172']
+        thick = [0.1, 0.2, 0.2]
+
+        # reduce samples
+        # this also tests that already loaded workspace can be passed instead of a file
+        LoadNexusProcessed(Filename='sens-lamp.nxs', OutputWorkspace='sens-lamp')
+        for i in range(len(samples)):
+            SANSILLAutoProcess(
+                SampleRuns=samples[i],
+                BeamRuns=beams,
+                ContainerRuns=containers,
+                MaskFiles='mask1.nxs,mask2.nxs,mask3.nxs',
+                SensitivityMaps='sens-lamp',
+                SampleTransmissionRuns=sample_tr[i],
+                ContainerTransmissionRuns=container_tr,
+                TransmissionBeamRuns=beam_tr,
+                SampleThickness=thick[i],
+                CalculateResolution='MildnerCarpenter',
+                OutputWorkspace='iq_s' + str(i + 1),
+                BeamRadius='0.05,0.05,0.05',
+                TransmissionBeamRadius=0.05
+            )
+
+        GroupWorkspaces(InputWorkspaces=['iq_s1', 'iq_s2', 'iq_s3'], OutputWorkspace='out')
+
+
 class D33_AutoProcess_Test(systemtesting.MantidSystemTest):
     """
     Tests auto process with D33 monochromatic data
