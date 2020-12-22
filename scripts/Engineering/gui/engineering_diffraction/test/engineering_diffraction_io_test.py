f8c20b8215c507277f3962912cb5915d93f52d99
Tom Clayton <tom.clayton@stfc.ac.uk>
2020-12-22 17:12:31 +0000

    Add package and stub for encoder/decoder unit tests RE #29830

---
+++ scripts/Engineering/gui/engineering_diffraction/test/engineering_diffraction_io_test.py
@@ -0,0 +1,19 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+import unittest
+from Engineering.gui.engineering_diffraction.engineering_diffraction import EngineeringDiffractionGui
+from Engineering.gui.engineering_diffraction.engineering_diffraction_io import EngineeringDiffractionDecoder, \
+    EngineeringDiffractionEncoder
+
+
+class EngineeringDiffractionIOTest(unittest.TestCase):
+    def stub(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
