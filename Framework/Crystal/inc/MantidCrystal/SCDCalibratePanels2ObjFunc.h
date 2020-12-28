104e9a85c0c294cdd5ccdc0e801c145eddc0a9eb
Zhang, Chen <zhangc@ornl.gov>
2020-12-28 15:41:46 -0500

    setup for the def of objective func for SCDCalirbate

---
+++ Framework/Crystal/inc/MantidCrystal/SCDCalibratePanels2ObjFunc.h
@@ -0,0 +1,21 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidCrystal/DllConfig.h"
+
+
+namespace Mantid {
+namespace Crystal {
+
+/** SCDCalibratePanels2ObjFunc : TODO: DESCRIPTION
+*/
+class MANTID_CRYSTAL_DLL SCDCalibratePanels2ObjFunc {
+public:};
+
+} // namespace Crystal
+} // namespace Mantid
