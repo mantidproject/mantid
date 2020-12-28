1c3ac88d3239226b052a3353c5a072967e550e0c
Zhang, Chen <zhangc@ornl.gov>
2020-12-28 15:50:08 -0500

    fix error in the layout

--- Framework/Crystal/inc/MantidCrystal/SCDCalibratePanels2ObjFunc.h
+++ Framework/Crystal/inc/MantidCrystal/SCDCalibratePanels2ObjFunc.h
@@ -6,16 +6,27 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
+#include "MantidAPI/IFunction1D.h"
+#include "MantidAPI/ParamFunction.h"
 #include "MantidCrystal/DllConfig.h"
-
 
 namespace Mantid {
 namespace Crystal {
 
 /** SCDCalibratePanels2ObjFunc : TODO: DESCRIPTION
 */
-class MANTID_CRYSTAL_DLL SCDCalibratePanels2ObjFunc {
-public:};
+class MANTID_CRYSTAL_DLL SCDCalibratePanels2ObjFunc : public API::ParamFunction,
+                                                      public API::IFunction1D {
+public:
+    /// overwrite based class name
+    std::string name() const override {return "SCDCalibratePanels2ObjFunc";}
+
+    /// set category
+    const std::string category() const override {return "General";}
+
+private:
+    
+};
 
 } // namespace Crystal
 } // namespace Mantid
