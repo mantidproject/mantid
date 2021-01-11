1799a4467439f9be91decf9cad3f5d4c2af3d68d
Jose Borreguero <borreguero@gmail.com>
2021-01-10 19:09:33 -0500

    Refs #30255 adapt target translation tolerance
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- Testing/SystemTests/tests/framework/CorelliPowderCalibrationTest.py
+++ Testing/SystemTests/tests/framework/CorelliPowderCalibrationTest.py
@@ -34,7 +34,7 @@ class CorelliPowderCalibrationCreateTest(MantidSystemTest):
         assert_allclose(table.row(0)['Zposition'], -19.991, atol=0.002)
         # Check position of bank42
         assert_allclose([table.row(1)[x] for x in ('Xposition', 'Yposition', 'Zposition')],
-                        [2.577, 0.063, 0.082], atol=0.007)
+                        [2.577, 0.063, 0.082], atol=0.04)
         # Check rotation of bank87
         assert_allclose([table.row(2)[x] for x in ('XdirectionCosine', 'YdirectionCosine', 'ZdirectionCosine')],
                         [-0.01, -1.00, 0.03], atol=0.05)
