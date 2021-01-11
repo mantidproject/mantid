2ba73924eaa31071269bcb6420179740d20d4ad1
Jose Borreguero <borreguero@gmail.com>
2021-01-11 09:15:14 -0500

    Refs #30255 adapt target rotation angle tolerance
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CorelliPowderCalibrationCreateTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CorelliPowderCalibrationCreateTest.py
@@ -43,7 +43,7 @@ class CorelliPowderCalibrationCreateTest(unittest.TestCase):
         assert_allclose([row[name] for name in ('Xposition', 'Yposition', 'Zposition')], target_position, atol=0.05)
         assert_allclose([row[name] for name in ('XdirectionCosine', 'YdirectionCosine', 'ZdirectionCosine')],
                         target_orientation, atol=0.03)
-        assert_allclose(row['RotationAngle'], target_rotation, atol=0.3)
+        assert_allclose(row['RotationAngle'], target_rotation, atol=2.0)
 
 
 if __name__ == '__main__':
