37c859a5ae4c4456e0dccda8afada94182f259ed
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-01-12 11:23:03 +0000

    Re #30323 The changes, necessary for Direct inelastic results
    to be compatible with indirect analysis

--- scripts/Inelastic/Direct/DirectEnergyConversion.py
+++ scripts/Inelastic/Direct/DirectEnergyConversion.py
@@ -882,6 +882,7 @@ class DirectEnergyConversion(object):
             GetEi(InputWorkspace=monitor_ws, Monitor1Spec=ei_mon_spectra[0],
                   Monitor2Spec=ei_mon_spectra[1],
                   EnergyEstimate=ei_guess,FixEi=fix_ei)
+        SetInstrumentParameter(data_ws,ParameterName='EFixed',ParameterType='Number', Value='{0}'.format(ei))
 
         # Store found incident energy in the class itself
         if self.prop_man.normalise_method == 'monitor-2' and not separate_monitors:
@@ -1293,6 +1294,8 @@ class DirectEnergyConversion(object):
                     SaveSPE(InputWorkspace=workspace,Filename= filename)
                     break
                 if case('nxs'):
+                    if save_file[-4:] != '_red':
+                        save_file = save_file+'_red'
                     filename = save_file + '.nxs'
                     SaveNexus(InputWorkspace=workspace,Filename= filename)
                     break
