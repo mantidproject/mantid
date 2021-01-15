e8ffde3c9cbc8c03dd356095ca8140066e4f985a
Dominik Arominski <arominski@ill.fr>
2021-01-15 13:53:57 +0100

    Added search directory to the CalculateEfficiency test

--- Framework/Algorithms/test/CalculateEfficiency2Test.h
+++ Framework/Algorithms/test/CalculateEfficiency2Test.h
@@ -229,6 +229,10 @@ public:
     // Tests merging input and calculating efficiencies based on workspace
     // without beam mask shadow using ILL D22 data.
 
+    ConfigService::Instance().appendDataSearchSubDir("ILL/D22/");
+    ConfigService::Instance().setFacility("ILL");
+    ConfigService::Instance().setString("default.instrument", "D22");
+
     Mantid::DataHandling::Load loader;
     loader.initialize();
     TS_ASSERT_THROWS_NOTHING(loader.setPropertyValue(
