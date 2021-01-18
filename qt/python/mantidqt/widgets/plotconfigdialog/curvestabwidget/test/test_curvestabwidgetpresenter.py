63fa87f98b3ce1c5a1290c60eb649f49504ffe4c
Matt Cumber <matthew.cumber@stfc.ac.uk>
2021-01-18 12:26:31 +0000

    Re #30155 Fix Error Every Plot Settings
    Added a unit test for error every to check this does not happen again

--- qt/python/mantidqt/widgets/plotconfigdialog/curvestabwidget/test/test_curvestabwidgetpresenter.py
+++ qt/python/mantidqt/widgets/plotconfigdialog/curvestabwidget/test/test_curvestabwidgetpresenter.py
@@ -37,7 +37,6 @@ class CurvesTabWidgetPresenterTest(unittest.TestCase):
         cls.curve0 = cls.ax0.errorbar(cls.ws, specNum=1, fmt=':g', marker=1,
                                       label='Workspace')
         cls.ax0.plot([0, 1], [2, 3], '--r', marker='v', lw=1.1, label='noerrors')
-        cls.ax0.creation_args = [{'errorevery': 1}]
 
         ax1 = cls.fig.add_subplot(222)
         ax1.set_title("Image")
@@ -409,6 +408,33 @@ class CurvesTabWidgetPresenterTest(unittest.TestCase):
         presenter.on_curves_selection_changed()
 
         mock_view.enable_curve_config.assert_called_with(True)
+
+    def make_figure_with_error_bars(self):
+        fig = figure()
+        ax = fig.add_subplot(111, projection='mantid')
+        ax.errorbar([0, 1, 2, 4], [0, 1, 2, 4], yerr=[0.1, 0.2, 0.3, 0.4],
+                    label='errorbar_plot', errorevery=7)
+        ax.set_title('Axes 0')
+        ax.plot(self.ws, specNum=1, label='Workspace')
+        ax.plot(self.ws, specNum=1, label='Workspace 2')
+        ax.plot(self.ws, specNum=1, label='Workspace 3')
+        return fig
+
+    def test_errorevery_applied_correctly(self):
+        fig = self.make_figure_with_error_bars()
+        new_props = CurveProperties({'capsize': 1, 'errorevery': 7,
+                                     'hide': False, 'marker': None,
+                                     'label': "Workspace",
+                                     'hide_errors': False})
+        mock_view = Mock(get_selected_ax_name=lambda: "Axes 0: (0, 0)",
+                         get_current_curve_name=lambda: "Workspace",
+                         get_properties=lambda: new_props)
+        presenter = self._generate_presenter(fig=fig, mock_view=mock_view)
+        presenter.apply_properties()
+        presenter.update_view()
+        with patch.object(presenter.view, 'udpate_fields'):
+            args, kwargs = presenter.view.update_fields.call_args
+            self.assertEqual(args[0].errorevery, 7)
 
 
 if __name__ == '__main__':
