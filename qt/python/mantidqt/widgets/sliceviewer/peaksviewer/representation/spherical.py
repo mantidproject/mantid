6425db117c43cb3d4d24c07abf252491d7847445
Ross Whitfield <whitfieldre@ornl.gov>
2021-01-21 15:00:43 -0500

    Allow different relative thicknesses in ellipsoid

--- qt/python/mantidqt/widgets/sliceviewer/peaksviewer/representation/spherical.py
+++ qt/python/mantidqt/widgets/sliceviewer/peaksviewer/representation/spherical.py
@@ -120,8 +120,8 @@ def slice_sphere(peak_origin, radius, zp):
     :param zp: Slice point in slice dimension
     """
     # Sphere is just a special case of an ellipse
-    slice_origin, circle_radius, _, __ = slice_ellipsoid_matrix(peak_origin, zp,
-                                                                calculate_spherical_matrix(radius))
+    slice_origin, circle_radius, _, __, _ = slice_ellipsoid_matrix(peak_origin, zp,
+                                                                   calculate_spherical_matrix(radius))
     return slice_origin, circle_radius
 
 
