4bcae0822529d266714ed710a20eb814b1aaecb4
Pete Peterson <petersonpf@ornl.gov>
2021-01-25 16:57:52 -0500

    Copy from master

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CorelliPowderCalibrationCreateTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/CorelliPowderCalibrationCreateTest.py
@@ -60,6 +60,10 @@ class CorelliPowderCalibrationCreateTest(unittest.TestCase):
             ComponentList='bank1', ComponentMaxTranslation=0.2, ComponentMaxRotation=10)
         # Check source position
         row = mtd['cal_adjustments'].row(0)
+        assert_allclose([row[name] for name in ('Xposition', 'Yposition', 'Zposition')], [0., 0., -10.0], atol=0.001)
+        # Check position of first bank
+        row = mtd['cal_adjustments'].row(1)
+        target_position, target_orientation, target_rotation = [5.18, -0.32,  0.20], [0.001, 0.999, -0.027], 98.0
         # ToDO investigate the relatively large tolerance required for some operative systems, atol=0.05
         assert_allclose([row[name] for name in ('Xposition', 'Yposition', 'Zposition')], target_position, atol=0.05)
         assert_allclose([row[name] for name in ('XdirectionCosine', 'YdirectionCosine', 'ZdirectionCosine')],
