6e93aa261989d3a9ff79dd74d88be1a58e3bc445
Rob Applin <robert.applin@stfc.ac.uk>
2021-01-26 16:38:44 +0000

    Refs #29911. Add docs for FittingMode.

--- qt/widgets/common/inc/MantidQtWidgets/Common/FittingMode.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/FittingMode.h
@@ -11,10 +11,17 @@
 namespace MantidQt {
 namespace MantidWidgets {
 
+/*!
+ * FittingMode can be used to specify what mode you are using for a fit. It is
+ * also used by the FitPropertyBrowser to either allow the selection of
+ * sequential fitting (SEQUENTIAL), simultaneous fitting (SIMULTANEOUS) or both
+ * types (SIMULTANEOUS_SEQUENTIAL).
+ */
 enum EXPORT_OPT_MANTIDQT_COMMON FittingMode {
   SIMULTANEOUS = 0,
   SEQUENTIAL = 1,
   SIMULTANEOUS_SEQUENTIAL = 2
 };
-}
+
+} // namespace MantidWidgets
 } // namespace MantidQt
