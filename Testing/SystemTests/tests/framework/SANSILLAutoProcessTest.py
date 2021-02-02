05c0c5b851b7c6020734934395043c22c3a3124e
Guillaume Communie <communieg@ill.eu>
2021-02-02 15:18:49 +0100

    [SANSILL] adapt the system test to the new suffix

--- Testing/SystemTests/tests/framework/SANSILLAutoProcessTest.py
+++ Testing/SystemTests/tests/framework/SANSILLAutoProcessTest.py
@@ -124,9 +124,15 @@ class D11_AutoProcess_Wedges_Test(systemtesting.MantidSystemTest):
             )
 
         GroupWorkspaces(
-            InputWorkspaces=['iq_1', 'iq_2', 'iq_3',
-                             'iq_wedge_1_1', 'iq_wedge_1_2', 'iq_wedge_1_3',
-                             'iq_wedge_2_1', 'iq_wedge_2_2', 'iq_wedge_2_3'],
+            InputWorkspaces=['iq_d39.0m_c40.5m_w5.6A',
+                             'iq_d8.0m_c8.0m_w5.6A',
+                             'iq_d2.0m_c5.5m_w5.6A',
+                             'iq_wedge_1_d39.0m_c40.5m_w5.6A',
+                             'iq_wedge_1_d8.0m_c8.0m_w5.6A',
+                             'iq_wedge_1_d2.0m_c5.5m_w5.6A',
+                             'iq_wedge_2_d39.0m_c40.5m_w5.6A',
+                             'iq_wedge_2_d8.0m_c8.0m_w5.6A',
+                             'iq_wedge_2_d2.0m_c5.5m_w5.6A'],
             OutputWorkspace='out'
             )
 
