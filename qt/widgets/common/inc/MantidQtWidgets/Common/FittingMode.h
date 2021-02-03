d61680372b150484b01793f87c8d0d40a165370c
Rob Applin <robert.applin@stfc.ac.uk>
2021-02-03 13:15:15 +0000

    Refs #29911. Remove export macro.

--- qt/widgets/common/inc/MantidQtWidgets/Common/FittingMode.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/FittingMode.h
@@ -6,18 +6,10 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
-#include "DllOption.h"
-
 namespace MantidQt {
 namespace MantidWidgets {
 
-/*!
- * FittingMode can be used to specify what mode you are using for a fit. It is
- * also used by the FitPropertyBrowser to either allow the selection of
- * sequential fitting (SEQUENTIAL), simultaneous fitting (SIMULTANEOUS) or both
- * types (SIMULTANEOUS_SEQUENTIAL).
- */
-enum EXPORT_OPT_MANTIDQT_COMMON FittingMode {
+enum FittingMode {
   SIMULTANEOUS = 0,
   SEQUENTIAL = 1,
   SIMULTANEOUS_SEQUENTIAL = 2
