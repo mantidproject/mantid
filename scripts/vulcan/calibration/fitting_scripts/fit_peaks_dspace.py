be136690e498b5f0e465ea762c290c73b59188b3
Wenduo Zhou <zhouw@ornl.gov>
2021-02-03 11:03:43 -0500

    Added diagnostic scripts and results.

---
+++ scripts/vulcan/calibration/fitting_scripts/fit_peaks_dspace.py
@@ -0,0 +1,4 @@
+# Peak 1.07
+
+Fit(Function='name=BackToBackExponential,I=1.05074e+08,A=2517.65,B=1275.73,X0=1.07525,S=0.000566765;name=LinearBackground,A0=1.66376e+09,A1=-1.33005e+09', InputWorkspace='VULCAN_164960_pd0003_3banks', Output='VULCAN_164960_pd0003_3banks', OutputCompositeMembers=True, WorkspaceIndex=2, StartX=1.0430260736318451, EndX=1.1029278964344489, Normalise=True)
+
