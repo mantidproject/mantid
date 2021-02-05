75d33ca51424b817006bc58eda8c4de1a9205eb8
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-02-05 11:50:37 +0000

    Re #30500 Re-introduce CPack properly packaging on none windows
    Also add a comment that makes it useful and not tied to vates/paraview.

--- buildconfig/CMake/CPackCommon.cmake
+++ buildconfig/CMake/CPackCommon.cmake
@@ -12,6 +12,11 @@ set ( CPACK_PACKAGE_VERSION_MAJOR ${VERSION_MAJOR} )
 set ( CPACK_PACKAGE_VERSION_MINOR ${VERSION_MINOR} )
 set ( CPACK_PACKAGE_VERSION_PATCH ${VERSION_PATCH}${VERSION_TWEAK} )
 
+# Sets install for the /opt/mantid* directory
+if(NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  set ( CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX} )
+endif()
+
 # RPM information - only used if generating a rpm
 # the release number is an option set in LinuxPackageScripts.cmake
 set ( CPACK_RPM_PACKAGE_LICENSE GPLv3+ )
