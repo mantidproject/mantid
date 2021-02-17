9769d57fc0115d193eb758d9460e3deaf8b742b0
Robert Applin <robert.applin@stfc.ac.uk>
2021-02-17 08:53:29 +0000

    Refs #29892. Apply clang format.

--- scripts/Muon/GUI/Common/contexts/fitting_context.py
+++ scripts/Muon/GUI/Common/contexts/fitting_context.py
@@ -435,11 +435,9 @@ class FittingContext(object):
         self.remove_fits_from_stored_fit_list(fits_to_remove)
 
     def remove_all_fits(self):
-        removed_fits = []
-        for fit in self.fit_list:
-            self.fit_list.remove(fit)
-            removed_fits += [fit]
-            self._number_of_fits -= 1
+        removed_fits = self.fit_list
+        self.fit_list = []
+        self._number_of_fits = 0
         self.fit_removed_notifier.notify_subscribers(removed_fits)
 
     def remove_latest_fit(self):
