0d066ca194c47e8b6dd170e567468db6b5ec1a61
Rob Applin <40830825+robertapplin@users.noreply.github.com>
2021-02-17 11:43:09 +0000

    Fit Script Generator - Connect FunctionTreeView to data table (#30140)
    It connects the FunctionTreeView to the data table, and same with the check box above the FunctionTreeView. The following is now possible:
    
        Selecting individual or multiple datasets, and then add or remove functions to the FunctionTreeView for only these datasets by unticking the tick box.
        Add or remove functions to all datasets by ticking the checkbox.
        Selecting between datasets will display the functions related to that dataset.
        Switching between Sequential and Simultaneous fitting will hide/show the global parameter column and multi domain index
    
    It is also now possible to do the following:
    
        Editing parameter and attribute values
        Local and global ties
        constraints
        global parameters

--- qt/widgets/common/inc/MantidQtWidgets/Common/FitScriptGeneratorPresenter.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/FitScriptGeneratorPresenter.h
@@ -8,6 +8,7 @@
 
 #include "DllOption.h"
 #include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidQtWidgets/Common/FittingMode.h"
 #include "MantidQtWidgets/Common/IFitScriptGeneratorPresenter.h"
 #include "MantidQtWidgets/Common/IndexTypes.h"
 
@@ -21,6 +22,8 @@ namespace MantidWidgets {
 
 class IFitScriptGeneratorModel;
 class IFitScriptGeneratorView;
+struct GlobalParameter;
+struct GlobalTie;
 
 class EXPORT_OPT_MANTIDQT_COMMON FitScriptGeneratorPresenter
     : public IFitScriptGeneratorPresenter {
@@ -31,15 +34,38 @@ public:
                               double startX = 0.0, double endX = 0.0);
   ~FitScriptGeneratorPresenter() override;
 
-  void notifyPresenter(ViewEvent const &event) override;
+  void notifyPresenter(ViewEvent const &event, std::string const &arg1 = "",
+                       std::string const &arg2 = "") override;
+  void notifyPresenter(ViewEvent const &event,
+                       std::vector<std::string> const &vec) override;
+  void notifyPresenter(ViewEvent const &event,
+                       FittingMode fittingMode) override;
 
   void openFitScriptGenerator() override;
+
+  void setGlobalTies(std::vector<GlobalTie> const &globalTies) override;
+  void setGlobalParameters(
+      std::vector<GlobalParameter> const &globalParameters) override;
 
 private:
   void handleRemoveClicked();
   void handleAddWorkspaceClicked();
   void handleStartXChanged();
   void handleEndXChanged();
+  void handleSelectionChanged();
+  void handleFunctionRemoved(std::string const &function);
+  void handleFunctionAdded(std::string const &function);
+  void handleFunctionReplaced(std::string const &function);
+  void handleParameterChanged(std::string const &parameter);
+  void handleAttributeChanged(std::string const &attribute);
+  void handleParameterTieChanged(std::string const &parameter,
+                                 std::string const &tie);
+  void handleParameterConstraintRemoved(std::string const &parameter);
+  void handleParameterConstraintChanged(std::string const &functionIndex,
+                                        std::string const &constraint);
+  void handleGlobalParametersChanged(
+      std::vector<std::string> const &globalParameters);
+  void handleFittingModeChanged(FittingMode fittingMode);
 
   void setWorkspaces(QStringList const &workspaceNames, double startX,
                      double endX);
@@ -59,6 +85,20 @@ private:
                     WorkspaceIndex workspaceIndex, double startX);
   void updateEndX(std::string const &workspaceName,
                   WorkspaceIndex workspaceIndex, double endX);
+
+  void
+  removeFunctionForDomains(std::string const &function,
+                           std::vector<FitDomainIndex> const &domainIndices);
+  void addFunctionForDomains(std::string const &function,
+                             std::vector<FitDomainIndex> const &domainIndices);
+  void setFunctionForDomains(std::string const &function,
+                             std::vector<FitDomainIndex> const &domainIndices);
+
+  void updateParameterTie(std::string const &workspaceName,
+                          WorkspaceIndex workspaceIndex,
+                          std::string const &parameter, std::string const &tie);
+
+  [[nodiscard]] std::vector<FitDomainIndex> getRowIndices() const;
 
   void checkForWarningMessages();
 
