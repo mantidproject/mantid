5808a9eaf6fb860260a3769060e0155d42835d2a
Zhang, Chen <zhangc@ornl.gov>
2021-02-19 11:08:59 -0500

    first draft of unittest

--- scripts/test/vulcan/calibration/test_calibration.py
+++ scripts/test/vulcan/calibration/test_calibration.py
@@ -56,15 +56,24 @@ class TestUtils(unittest.TestCase):
     # ------------------ #
     def test_calibration(self):
         diamond_run = ["VULCAN_192226.nxs.h5"]
-        vulcan_x_idf = ""  # TODO: update IDF for vulcanx
+        vulcan_x_idf = "VULCAN_Definition.xml"  # Should be in Mantid instrument folder
         calibration_rst = os.path.join(tempfile.gettempdir(), "VULCAN_Calibration_Hybrid.h5")
 
         # perform calibration
+        # step_1:
         cc_calib_file, diamond_ws_name = cross_correlate_calibrate(diamond_run, vulcan_x_idf)
+        # step_2:
+        cc_focus_ws_name, cc_focus_nexus = align_vulcan_data(
+            dia_runs=diamond_run,
+            diff_cal_file_name=cc_calib_file,
+            )
+        # step_3:
+        peak_position_calibrate(cc_focus_ws_name, cc_calib_file, calibration_rst)
 
         # verify calibration file is created
         # NOTE: this is step 1, we will add more reliable asseration once
         #       we have a reference calibration results
+        assert os.path.exists(calibration_rst)
 
 
 if __name__ == "__main__":
