cf6ad72be83be71cfaee7748e2c5cf01aca120e6
Guillaume Communie <communieg@ill.eu>
2021-02-24 13:46:27 +0100

    [DRILL] load rundex that has no extension
    This commit changes the way the QFileDialog is listing all files. Now,
    when selecting "All files" option, the files that have no extension are
    also listed.

--- scripts/Interface/ui/drill/presenter/DrillPresenter.py
+++ scripts/Interface/ui/drill/presenter/DrillPresenter.py
@@ -323,7 +323,7 @@ class DrillPresenter:
         QDialog to get the file path from the user.
         """
         filename = QFileDialog.getOpenFileName(self.view, 'Load rundex', '.',
-                                               "Rundex (*.mrd);;All (*.*)")
+                                               "Rundex (*.mrd);;All (*)")
         if not filename[0]:
             return
         self.model.setIOFile(filename[0])
