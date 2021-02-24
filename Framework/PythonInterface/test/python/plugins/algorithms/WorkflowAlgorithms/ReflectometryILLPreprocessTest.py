bfcc64474a67406e7d6d0b0226e907344e039524
Srikanth Ravipati <rsri131@gmail.com>
2021-02-24 22:47:37 +0000

    Set facility and instrument

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ReflectometryILLPreprocessTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ReflectometryILLPreprocessTest.py
@@ -120,6 +120,8 @@ class ReflectometryILLPreprocessTest(unittest.TestCase):
         self.assertAlmostEqual(numpy.rad2deg(two_theta_fg), 0., delta=0.1)
 
     def testTwoInputFiles(self):
+        config.setString("default.facility", "ILL")
+        config.setString("default.instrument", "D17")
         outWSName = 'outWS'
         args = {
             'Run': 'ILL/D17/317369, ILL/D17/317370.nxs',
@@ -132,6 +134,8 @@ class ReflectometryILLPreprocessTest(unittest.TestCase):
         outWS = alg.getProperty('OutputWorkspace').value
         self.assertEqual(outWS.getAxis(0).getUnit().caption(), 'Wavelength')
         self.assertEqual(mtd.getObjectNames(), [])
+        config.setString("default.facility", "NONE")
+        config.setString("default.instrument", "NONE")
 
 
 if __name__ == "__main__":
