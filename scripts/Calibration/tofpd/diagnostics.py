14a8df9c347c9837ab11ebfaba6c6964e93a552f
Cole Kendrick <kendrickcj@ornl.gov>
2021-02-25 08:49:14 -0500

    Function to plot delta DIFC between calibrations

--- scripts/Calibration/tofpd/diagnostics.py
+++ scripts/Calibration/tofpd/diagnostics.py
@@ -1,7 +1,10 @@
 from mantid.plots.resampling_image.samplingimage import imshow_sampling
 from mantid.plots.datafunctions import get_axes_labels
-from mantid.simpleapi import mtd
+from mantid.simpleapi import CalculateDIFC, mtd
 import matplotlib.pyplot as plt
+import six
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
 import numpy as np
 
 
@@ -78,3 +81,71 @@ def plot2d(workspace, tolerance: float=0.001, peakpositions: np.ndarray=DIAMOND,
 
     # return the figure so others can customize it
     return fig, fig.axes
+
+
+def difc_plot2d(calws_a, calws_b):
+
+    # TODO: add WS name check for group and cal
+    difc_a = CalculateDIFC(str(calws_a) + "_group", CalibrationWorkspace=str(calws_a) + "_cal")
+    difc_b = CalculateDIFC(str(calws_b) + "_group", CalibrationWorkspace=str(calws_b) + "_cal")
+
+    delta = difc_b - difc_a
+
+    # Plotting below taken from addie/calibration/CalibrationDiagnostics.py
+    theta_array = [ ]
+    phi_array = [ ]
+    value_array = [ ]
+    masked_theta_array = [ ]
+    masked_phi_array = [ ]
+    info = delta.spectrumInfo()
+    for idx, x in enumerate(info):
+        pos = x.position
+        theta = np.arccos(pos[ 2 ] / pos.norm())
+        phi = np.arctan2(pos[ 1 ], pos[ 0 ])
+        #if mask.dataY(idx):
+        #    masked_theta_array.append(theta)
+        #    masked_phi_array.append(phi)
+        #else:
+        theta_array.append(theta)
+        phi_array.append(phi)
+        value_array.append(np.sum(delta.dataY(idx)))
+
+    # Use the largest solid angle for circle radius
+    sample_position = info.samplePosition()
+    maximum_solid_angle = 0.0
+    for idx in six.moves.xrange(info.size()):
+        maximum_solid_angle = max(maximum_solid_angle, delta.getDetector(idx).solidAngle(sample_position))
+
+    # Radius also includes a fudge factor to improve plotting.
+    # May need to add finer adjustments on a per-instrument basis.
+    # Small circles seem to alias less than rectangles.
+    radius = maximum_solid_angle * 8.0
+    patches = [ ]
+    for x1, y1 in six.moves.zip(theta_array, phi_array):
+        circle = Circle((x1, y1), radius)
+        patches.append(circle)
+
+    masked_patches = [ ]
+    for x1, y1 in six.moves.zip(masked_theta_array, masked_phi_array):
+        circle = Circle((x1, y1), radius)
+        masked_patches.append(circle)
+
+    # Matplotlib requires this to be a Numpy array.
+    colors = np.array(value_array)
+    p = PatchCollection(patches)
+    p.set_array(colors)
+    p.set_clim(-0.1, 0.1)
+    p.set_edgecolor('face')
+
+    fig, ax = plt.subplots()
+    ax.add_collection(p)
+    mp = PatchCollection(masked_patches)
+    mp.set_facecolor('gray')
+    mp.set_edgecolor('face')
+    ax.add_collection(mp)
+    fig.colorbar(p, ax=ax)
+    ax.set_xlabel(r'$\phi$')
+    ax.set_xlim(0.0, np.pi)
+    ax.set_ylabel(r'$\theta$')
+    ax.set_ylim(-np.pi, np.pi)
+    return fig, ax
