b23247c6e9e57ad4a8df2a444911499feca57eb2
Dominik Arominski <arominski@ill.fr>
2021-02-26 11:59:38 +0100

    Added discriminator for IN5/PANTHER scan data to LoadILLTOF

--- Framework/DataHandling/src/LoadILLTOF2.cpp
+++ Framework/DataHandling/src/LoadILLTOF2.cpp
@@ -52,6 +52,8 @@ int LoadILLTOF2::confidence(Kernel::NexusDescriptor &descriptor) const {
       && !descriptor.pathExists(
              "/entry0/instrument/VirtualChopper") // This one is for
                                                   // LoadILLReflectometry
+      && !descriptor.pathExists(
+                 "/entry0/data_scan") // This one is handled by LoadILLDiffraction
   ) {
     return 80;
   } else {
