2bd69870b1131d8ebcbce952be7b9e5356680fe5
Zhang, Chen <zhangc@ornl.gov>
2021-03-08 11:47:45 -0500

    total overhall of unit test

--- Framework/Crystal/test/SCDCalibratePanels2Test.h
+++ Framework/Crystal/test/SCDCalibratePanels2Test.h
@@ -29,6 +29,7 @@
 #include "MantidDataObjects/EventWorkspace.h"
 #include "MantidDataObjects/Workspace2D.h"
 #include "MantidGeometry/Crystal/CrystalStructure.h"
+#include "MantidGeometry/Crystal/OrientedLattice.h"
 #include "MantidKernel/Logger.h"
 #include "MantidKernel/Unit.h"
 
@@ -98,10 +99,29 @@ public:
     darkmagic->setLogging(false); // don't really care its output
     darkmagic->setPropertyValue("Filename", "Peaks5637.integrate");
     darkmagic->setPropertyValue("OutputWorkspace", "TOPAZ_5637");
-    TS_ASSERT(darkmagic->execute());
+    darkmagic->executeAsChildAlg();
 
     m_ws = generateSimulatedWorkspace();
     m_pws = generateSimulatedPeaksWorkspace(m_ws);
+
+    // NOTE:
+    //    Predict peaks will shift the q vector to point at the center
+    //    of each detector to mimic the real experiment, which leads to
+    //    a constant shift in the residual profile.
+    //    For unit test purpose, we are going to move the q vector back
+    //    to the true position.
+    V3D qv_ideal;
+    auto ubm = m_pws->sample().getOrientedLattice().getUB();
+    for (int i =0; i<m_pws->getNumberPeaks(); ++i){
+      qv_ideal = ubm * m_pws->getPeak(i).getIntHKL();
+      qv_ideal *= 2.0 *PI;
+      g_log.notice() << "peak_" << i << " = " << m_pws->getPeak(i).getIntHKL() << "\n";
+      g_log.notice() << "L2_" << i << " = " << m_pws->getPeak(i).getL2() << "\n";
+      g_log.notice() << "original qsample = " << m_pws->getPeak(i).getQSampleFrame() << "\n";
+      g_log.notice() << "ideal qsample = " << qv_ideal << "\n";
+      m_pws->getPeak(i).setQSampleFrame(qv_ideal);
+      g_log.notice() << "new qsample = " << m_pws->getPeak(i).getQSampleFrame() << "\n";
+    }
   }
 
   // ---------------------- //
@@ -131,333 +151,40 @@ public:
    * @brief Trivial case where all components are in ideal/starting position
    *        Therefore the calibration results should be close to a zero
    *        vector.
-   *
-   * NOTE: Change the name from run_Null_Case to test_Null_Case to run it
-   *       within the ctest framework.
    */
-  void run_Null_Case() {
+  void test_Null_Case() {
     g_log.notice() << "test: !Null case!\n";
 
     // Generate unique temp files
     auto filenamebase = boost::filesystem::temp_directory_path();
     filenamebase /= boost::filesystem::unique_path("nullcase_%%%%%%%%");
 
-    g_log.notice() << "-- generate simulated workspace\n";
-    MatrixWorkspace_sptr ws = m_ws->clone();
-    MatrixWorkspace_sptr wsraw = ws->clone();
+    // Make a clone of the standard peak workspace
+    PeaksWorkspace_sptr pws = m_pws->clone();
 
-    // Trivial case, no component undergoes any affine transformation
-    g_log.notice() << "-- trivial case, no components moved\n";
+    // No need to change anything
 
-    g_log.notice() << "-- generate peaks\n";
-    PeaksWorkspace_sptr pws = generateSimulatedPeaksWorkspace(ws);
-    PeaksWorkspace_sptr pwsref = pws->clone();
+    // Make a copy as the solution to check with
+    PeaksWorkspace_sptr pws_ref = pws->clone();
+    
+    // Pretend we forget the answer
+    pws->setInstrument(m_pws->getInstrument());
 
-    // Pretend we don't know the answer
-    g_log.notice() << "-- reset instrument positions&orientations\n";
-    pws->setInstrument(wsraw->getInstrument());
+    // Run the calibration
+    g_log.notice() << pws->getInstrument()->getSource()->getPos().Z() << "\n";
 
-    // Perform the calibration
-    g_log.notice() << "-- start calibration\n";
-    runCalibration(filenamebase.string(), pws, false, true, true);
-
-    // Check if the calibration returns the same instrument as we put in
-    g_log.notice() << "-- validate calibration output\n";
-    TS_ASSERT(validateCalibrationResults(pwsref, wsraw, filenamebase.string()));
-
-    // Cleanup
-    doCleanup();
-  }
-
-  /**
-   * @brief Only adjust T0
-   *
-   * NOTE: calibration of T0 shift always returns 0, therefore this
-   *       test should not be turned on until the issue is resolved.
-   */
-  void run_T0_Shift() {
-    g_log.notice() << "test: !T0 Shift!\n";
-
-    // prescribed shift
-    const double dT0 = 11; // micro seconds
-
-    // Generate unique temp files
-    auto filenamebase = boost::filesystem::temp_directory_path();
-    filenamebase /= boost::filesystem::unique_path("changeT0_%%%%%%%%");
-
-    g_log.notice() << "-- generate simulated workspace\n";
-    MatrixWorkspace_sptr ws = m_ws->clone();
-    MatrixWorkspace_sptr wsraw = ws->clone();
-
-    // Trivial case, no component undergoes any affine transformation
-    g_log.notice() << "-- generate peaks\n";
-    PeaksWorkspace_sptr pws = generateSimulatedPeaksWorkspace(ws);
-    // -- add T0 property
-    pws->mutableRun().addProperty<double>("T0", 0.0, true);
-    // -- make the twin
-    PeaksWorkspace_sptr pwsref = pws->clone();
-
-    // Adjust T0
-    adjustT0(dT0, pws);
-
-    // reset Workspace level of T0
-    g_log.notice() << "--Reset workspace T0 \n"
-                   << pws->mutableRun().getPropertyValueAsType<double>("T0")
-                   << "->"
-                   << pwsref->mutableRun().getPropertyValueAsType<double>("T0")
-                   << "\n";
-    pws->mutableRun().addProperty<double>(
-        "T0", pwsref->mutableRun().getPropertyValueAsType<double>("T0"), true);
-
-    // T0 got absorbed into peak wavelength, therefore there is no need to reset
-    // the instrument
-    // Perform the calibration
-    g_log.notice() << "-- start calibration\n";
-    runCalibration(filenamebase.string(), pws, true, false, false);
-  }
-
-  /**
-   * @brief Single variant case where only global var is adjusted.
-   *
-   * NOTE: technically we should also check T0, but the client, CORELLI
-   *       team does not seem to care about using T0, therefore we are
-   *       not implmenting T0 calibration here.
-   *
-   * NOTE: change the name from run_L1_Shift to test_L1_Shift to run
-   *       it within the ctest framework
-   */
-  void test_L1_Shift() {
-    g_log.notice() << "test: !Source Shift (L1 change)!\n";
-
-    g_log.notice() << "Tolerance of Distance (meter) :" << TOLERANCE_L << "\n";
-
-    // prescribed shift
-    // NOTE: the common range for dL1 is +-10cm
-    const double dL1 = boost::math::constants::e<double>() / 300;
-
-    // Generate unique temp files
-    // Generate unique temp files
-    auto filenamebase = boost::filesystem::temp_directory_path();
-    filenamebase /= boost::filesystem::unique_path("changeL1_%%%%%%%%");
-
-    g_log.notice() << "-- generate simulated workspace\n";
-    MatrixWorkspace_sptr ws = m_ws->clone();
-    MatrixWorkspace_sptr wsraw = ws->clone();
-
-    // move source
-    adjustComponent(0.0, 0.0, dL1, 1.0, 0.0, 0.0, 0.0,
-                    ws->getInstrument()->getSource()->getName(), ws);
-
-    g_log.notice() << "-- generate peaks\n";
-    PeaksWorkspace_sptr pws = generateSimulatedPeaksWorkspace(ws);
-    PeaksWorkspace_sptr pwsref = pws->clone();
-
-    // Pretend we don't know the answer
-    g_log.notice() << "-- reset instrument positions&orientations\n";
-    g_log.notice() << "    * before reset L1 = "
-                   << pws->getInstrument()->getSource()->getPos().Z() << "\n";
-    // reset instrument
-    // NOTE: this has to be done for each peak as updating the peak workspace
-    //       does not automatically update the peak inside
-    pws->setInstrument(wsraw->getInstrument());
-    double tof;
-    Units::Wavelength wl;
-    for (int i = 0; i < pws->getNumberPeaks(); ++i) {
-      tof = m_pws->getPeak(i).getTOF();
-      pws->getPeak(i).setInstrument(m_pws->getInstrument());
-      wl.initialize(m_pws->getPeak(i).getL1(), m_pws->getPeak(i).getL2(),
-                    pws->getPeak(i).getScattering(), 0,
-                    pws->getPeak(i).getInitialEnergy(), 0.0);
-      pws->getPeak(i).setDetectorID(pws->getPeak(i).getDetectorID());
-      pws->getPeak(i).setWavelength(wl.singleFromTOF(tof));
-    }
-
-    g_log.notice() << "    * after reset L1 = "
-                   << pws->getInstrument()->getSource()->getPos().Z() << "\n";
-
-    // Perform the calibration
     g_log.notice() << "-- start calibration\n";
     runCalibration(filenamebase.string(), pws, false, true, false);
 
-    // Check if the calibration returns the same instrument as we put in
-    g_log.notice() << "-- validate calibration output\n";
-    TS_ASSERT(validateCalibrationResults(pwsref, wsraw, filenamebase.string()));
+    g_log.notice() << pws->getInstrument()->getSource()->getPos().Z() << "\n";
 
-    // this is just for documentation purpose, the validation func above
-    // is better for robust testing
-    double L1_prescribed = ws->getInstrument()->getSource()->getPos().Z();
-    double L1_calibrated = pws->getInstrument()->getSource()->getPos().Z();
-    double dl = std::abs(L1_prescribed - L1_calibrated);
-    g_log.notice() << "-- |L1_prescribed-L1_calibrated| = " << dl << "\n";
-
-    // Cleanup
-    doCleanup();
-  }
-
-  /**
-   * @brief Moving (rotation and translation) single panel
-   *
-   */
-  void run_bank_moved() {
-    g_log.notice() << "test: !single bank moved!\n";
-
-    g_log.notice() << "Tolerance of Distance (meter) :" << TOLERANCE_L << "\n";
-    g_log.notice() << "Tolerance of Rotation (degree) :" << TOLERANCE_R << "\n";
-
-    // prescribed shift
-    // NOTE: the common range for dx, dy ,dz is +-5cm
-    double dx = 1.1e-2;
-    double dy = -0.9e-2;
-    double dz = 1.5e-2;
-
-    // prescribed rotation
-    double theta = PI / 3;
-    double phi = PI / 8;
-    double rvx = sin(theta) * cos(phi);
-    double rvy = sin(theta) * sin(phi);
-    double rvz = cos(theta);
-    double ang = 1.414; // degrees
-
-    // Generate unique temp files
-    auto filenamebase = boost::filesystem::temp_directory_path();
-    filenamebase /= boost::filesystem::unique_path("panelMove_%%%%%%%%");
-
-    g_log.notice() << "-- generate simulated workspace\n";
-    MatrixWorkspace_sptr ws = m_ws->clone();
-    MatrixWorkspace_sptr wsraw = ws->clone();
-
-    g_log.notice() << "-- for x(top) - bank73\n"
-                   << "   translated by (" << dx << "," << dy << "," << dz
-                   << ")\n"
-                   << "   rotated by " << ang << "@(" << rvx << "," << rvy
-                   << "," << rvz << ")\n";
-    adjustComponent(dx, dy, dz, rvx, rvy, rvz, ang, bank_xtop, ws);
-
-    g_log.notice() << "-- generate peaks\n";
-    PeaksWorkspace_sptr pws = generateSimulatedPeaksWorkspace(ws);
-    PeaksWorkspace_sptr pwsref = pws->clone();
-
-    // Pretend we don't know the answer
-    g_log.notice() << "-- reset instrument positions&orientations\n";
-    g_log.notice()
-        << "    * before reset x(top) - bank73:\n"
-        << "    pos(abs) = "
-        << pws->getInstrument()->getComponentByName(bank_xtop)->getPos() << "\n"
-        << "    quat(rel) = "
-        << pws->getInstrument()->getComponentByName(bank_xtop)->getRelativeRot()
-        << "\n";
-
-    pws->setInstrument(wsraw->getInstrument());
-    g_log.notice()
-        << "    * after reset x(top) - bank73:\n"
-        << "    pos(abs) = "
-        << pws->getInstrument()->getComponentByName(bank_xtop)->getPos() << "\n"
-        << "    quat(rel) = "
-        << pws->getInstrument()->getComponentByName(bank_xtop)->getRelativeRot()
-        << "\n";
-
-    // Perform the calibration
-    g_log.notice() << "-- start calibration\n";
-    runCalibration(filenamebase.string(), pws, false, false, true);
-
-    // Check if the calibration returns the same instrument as we put in
-    g_log.notice() << "-- validate calibration output\n";
-    TS_ASSERT(validateCalibrationResults(pwsref, wsraw, filenamebase.string()));
-
-    // Cleanup
-    doCleanup();
-  }
-
-  /**
-   * @brief Everything goes
-   *
-   * NOTE: not enough peaks on the y_panels, so we have to work with only the
-   *       x_panels
-   */
-  void run_Exec() {
-    g_log.notice() << "test: !calibrate L1 and two panels at the same time!\n";
-
-    g_log.notice() << "Tolerance of Distance (meter) :" << TOLERANCE_L << "\n";
-    g_log.notice() << "Tolerance of Rotation (degree) :" << TOLERANCE_R << "\n";
-
-    // ----------------------------- //
-    // ----- Precribe movement ----- //
-    // ----------------------------- //
-    //-- source
-    const double dL1 = boost::math::constants::e<double>() / 100;
-    //-- xtop
-    double dx1 = 1.1e-2;
-    double dy1 = -0.9e-2;
-    double dz1 = 1.5e-2;
-    double theta1 = PI / 3;
-    double phi1 = PI / 8;
-    double rvx1 = sin(theta1) * cos(phi1);
-    double rvy1 = sin(theta1) * sin(phi1);
-    double rvz1 = cos(theta1);
-    double ang1 = 1.414; // degrees
-    //-- xbottom
-    double dx2 = 0.5e-2;
-    double dy2 = 1.3e-2;
-    double dz2 = -1.9e-2;
-    double theta2 = PI / 4;
-    double phi2 = PI / 3;
-    double rvx2 = sin(theta2) * cos(phi2);
-    double rvy2 = sin(theta2) * sin(phi2);
-    double rvz2 = cos(theta2);
-    double ang2 = 2.13; // degrees
-
-    // ----------------------------------- //
-    // ----- Generate Synthetic Data ----- //
-    // ----------------------------------- //
-    // Generate unique temp files
-    auto filenamebase = boost::filesystem::temp_directory_path();
-    filenamebase /= boost::filesystem::unique_path("testExec_%%%%%%%%");
-    g_log.notice() << "Base name is: " << filenamebase << "\n";
-
-    g_log.notice() << "-- generate simulated workspace\n";
-    MatrixWorkspace_sptr ws = m_ws->clone();
-    MatrixWorkspace_sptr wsraw = ws->clone();
-
-    // Source
-    adjustComponent(0.0, 0.0, dL1, 1.0, 0.0, 0.0, 0.0,
-                    ws->getInstrument()->getSource()->getName(), ws);
-    g_log.notice() << "--Shift source by dL1 = " << dL1 << "\n";
-
-    // Bank73
-    adjustComponent(dx1, dy1, dz1, rvx1, rvy1, rvz1, ang1, bank_xtop, ws);
-    g_log.notice() << "-- for x(top) - bank73\n"
-                   << "   translated by (" << dx1 << "," << dy1 << "," << dz1
-                   << ")\n"
-                   << "   rotated by " << ang1 << "@(" << rvx1 << "," << rvy1
-                   << "," << rvz1 << ")\n";
-
-    // Bank11
-    adjustComponent(dx2, dy2, dz2, rvx2, rvy2, rvz2, ang2, bank_xbottom, ws);
-    g_log.notice() << "-- for x(bottom) - bank11\n"
-                   << "   translated by (" << dx2 << "," << dy2 << "," << dz2
-                   << ")\n"
-                   << "   rotated by " << ang2 << "@(" << rvx2 << "," << rvy2
-                   << "," << rvz2 << ")\n";
-
-    g_log.notice() << "-- generate peaks\n";
-    PeaksWorkspace_sptr pws = generateSimulatedPeaksWorkspace(ws);
-    PeaksWorkspace_sptr pwsref = pws->clone();
-
-    g_log.notice() << "-- reset instrument positions&orientations\n";
-    pws->setInstrument(wsraw->getInstrument());
-
-    // Perform the calibration
-    g_log.notice() << "-- start calibration\n";
-    runCalibration(filenamebase.string(), pws, false, true, true);
-
-    g_log.notice() << "-- calibration now completes!!!";
-
-    // Check if the calibration returns the same instrument as we put in
-    g_log.notice() << "-- validate calibration output\n";
-    TS_ASSERT(validateCalibrationResults(pwsref, wsraw, filenamebase.string()));
-
-    // Cleanup
-    doCleanup();
+    // Check if the calibration results
+    // -- get a blank workspace for loading cali results
+    MatrixWorkspace_sptr ws_raw = m_ws->clone();
+    // -- check
+    bool sameInstrument = validateCalibrationResults(pws_ref, ws_raw, filenamebase.string());
+    // -- assert
+    TS_ASSERT(sameInstrument);
   }
 
 private:
@@ -491,6 +218,8 @@ private:
     sub_alg->setProperty("Workspace", wsname);
     sub_alg->setProperty("u", "0.5,0.8660254037844387,0");
     sub_alg->setProperty("v", "-0.8660254037844387,0.5,0");
+    // sub_alg->setProperty("u", "1.0,0.0,0.0");
+    // sub_alg->setProperty("v", "0.0,1.0,0.0");
     sub_alg->setProperty("a", silicon_a);
     sub_alg->setProperty("b", silicon_b);
     sub_alg->setProperty("c", silicon_c);
@@ -503,8 +232,8 @@ private:
     MatrixWorkspace_sptr ws =
         AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(wsname);
 
-    auto &sample = ws->mutableSample();
-    sample.setCrystalStructure(silicon_cs);
+    // auto &sample = ws->mutableSample();
+    // sample.setCrystalStructure(silicon_cs);
 
     return ws;
   }
@@ -580,14 +309,14 @@ private:
    */
   void adjustComponent(double dx, double dy, double dz, double rvx, double rvy,
                        double rvz, double drotang, std::string cmptName,
-                       MatrixWorkspace_sptr ws) {
+                       PeaksWorkspace_sptr &pws) {
 
     // rotation
     IAlgorithm_sptr rot_alg = Mantid::API::AlgorithmFactory::Instance().create(
         "RotateInstrumentComponent", -1);
     rot_alg->initialize();
     rot_alg->setLogging(LOGCHILDALG);
-    rot_alg->setProperty("Workspace", ws);
+    rot_alg->setProperty("Workspace", pws);
     rot_alg->setProperty("ComponentName", cmptName);
     rot_alg->setProperty("X", rvx);
     rot_alg->setProperty("Y", rvy);
@@ -601,7 +330,7 @@ private:
         "MoveInstrumentComponent", -1);
     mv_alg->initialize();
     mv_alg->setLogging(LOGCHILDALG);
-    mv_alg->setProperty("Workspace", ws);
+    mv_alg->setProperty("Workspace", pws);
     mv_alg->setProperty("ComponentName", cmptName);
     mv_alg->setProperty("X", dx);
     mv_alg->setProperty("Y", dy);
@@ -610,37 +339,6 @@ private:
     mv_alg->executeAsChildAlg();
   }
 
-  /**
-   * @brief shift T0 for both peakworkspace and all peaks
-   *
-   * @param dT0
-   * @param pws
-   */
-  void adjustT0(double dT0, PeaksWorkspace_sptr pws) {
-    // update the T0 record in peakworkspace
-    pws->mutableRun().addProperty<double>(
-        "T0", pws->mutableRun().getPropertyValueAsType<double>("T0") + dT0,
-        true);
-
-    // update wavelength of each peak using new T0
-    for (int i = 0; i < pws->getNumberPeaks(); ++i) {
-      Peak &pk = pws->getPeak(i);
-      V3D hkl =
-          V3D(boost::math::iround(pk.getH()), boost::math::iround(pk.getK()),
-              boost::math::iround(pk.getL()));
-
-      // make a standalone peak to calculate q vectors
-      Peak tmppk(pws->getInstrument(), pk.getDetectorID(), pk.getWavelength(),
-                 hkl, pk.getGoniometerMatrix());
-      Units::Wavelength wl;
-      wl.initialize(tmppk.getL1(), tmppk.getL2(), tmppk.getScattering(), 0,
-                    tmppk.getInitialEnergy(), 0.0);
-      tmppk.setWavelength(wl.singleFromTOF(pk.getTOF() + dT0));
-
-      // only passing the q vector, not the energy info that relates T0
-      pk.setQSampleFrame(tmppk.getQSampleFrame());
-    }
-  }
 
   /**
    * @brief Run the calibration algorithm
@@ -668,6 +366,7 @@ private:
     alg.setProperty("alpha", silicon_alpha);
     alg.setProperty("beta", silicon_beta);
     alg.setProperty("gamma", silicon_gamma);
+    alg.setProperty("RecalculateUB", false);
     alg.setProperty("CalibrateT0", calibrateT0);
     alg.setProperty("CalibrateL1", calibrateL1);
     alg.setProperty("CalibrateBanks", calibrateBanks);
