944d451c419e19c3b5f859a624cd44b3d5b8b2b7
Robert Applin <robert.applin@stfc.ac.uk>
2021-03-10 13:28:30 +0000

    Refs #20093. Fix crash when entering a single dot.

--- Framework/API/src/FileFinder.cpp
+++ Framework/API/src/FileFinder.cpp
@@ -420,6 +420,9 @@ std::string FileFinderImpl::findRun(const std::string &hintstr, const std::vecto
     }
   }
 
+  if (filename.empty())
+    return "";
+
   // Look first at the original filename then for case variations. This is
   // important
   // on platforms where file names ARE case sensitive.
