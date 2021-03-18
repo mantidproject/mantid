9b70f7c12d99258205ae40023483a9ca6740372f
Srikanth Ravipati <rsri131@gmail.com>
2021-03-18 14:49:01 +0000

    Set bar color explicitly for the first time

--- qt/applications/workbench/workbench/plugins/memorywidget/memorypresenter.py
+++ qt/applications/workbench/workbench/plugins/memorywidget/memorypresenter.py
@@ -2,12 +2,22 @@ from qtpy.QtCore import QTimer
 
 from ..memorywidget.memoryinfo import get_memory_info
 
+
 class MemoryPresenter(object):
     def __init__(self, view):
-
         self.view = view
-        self.view.memory_bar.setValue(100)
         self.update_memory_usage()
+        
+        # Initial bar color has to be set explicitly
+        # as the initial value of the progress bar is
+        # undefined
+        current_value = self.view.memory_bar.value()
+        if (current_value >= 90):
+            self.view.set_bar_color(0, current_value)
+        elif (current_value < 90):
+            self.view.set_bar_color(100, current_value)
+        else:
+            pass
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_memory_usage)
