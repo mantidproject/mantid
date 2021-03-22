7f5bd6faded03d2d0f6a5182bed91b12ab657825
Mathieu Tillet <tillet@ill.eu>
2021-03-22 09:03:53 +0100

    Display files and show on click
    In the rdexp widget, add the file management and a very rudimentary
    display mecanism.

--- qt/python/mantidqt/widgets/rawdataexplorer/model.py
+++ qt/python/mantidqt/widgets/rawdataexplorer/model.py
@@ -6,17 +6,47 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 #  This file is part of the mantid workbench.
 #
-#
 
-class RawDataExplorerModel(object):
+from qtpy.QtWidgets import QFileSystemModel
+from mantid.simpleapi import Load
+from qtpy.QtCore import Signal, QObject
+
+
+class RawDataExplorerModel(QObject):
     """
     TODO
     """
+    show_ws = Signal(str)
 
     def __init__(self, presenter):
         """
-        Initialise the model, keeping references to the presenter and
-        GlobalFigureManager
+        Initialise the model
         :param presenter: The presenter controlling this model
         """
+        super().__init__()
         self.presenter = presenter
+        self.file_model = QFileSystemModel()
+        self.instrument = "D16"  # TODO set current facility instrument
+
+        self.current_workspaces = []
+
+    def on_instrument_changed(self, new_instrument):
+        """
+        Slot triggered by changing the instrument
+        @param new_instrument: the name of the instrument, from the instrument selector
+        """
+        self.instrument = new_instrument
+        # TODO emit a signal to modify both preview manager and target
+
+    def on_file_clicked(self, file_index):
+        """
+        Slot triggered by clicking on a file. If it is not loaded already, tries to do so.
+        @param file_index: a QModelIndex object, referencing the position of the file in the model
+        """
+        file_path = self.file_model.filePath(file_index)
+        ws_name = "__" + file_path
+        if ws_name not in self.current_workspaces:
+            Load(Filename=file_path, OutputWorkspace=ws_name)
+            self.current_workspaces.append(ws_name)
+        self.show_ws.emit(ws_name)
+
--- qt/python/mantidqt/widgets/rawdataexplorer/view.py
+++ qt/python/mantidqt/widgets/rawdataexplorer/view.py
@@ -8,19 +8,55 @@
 #
 #
 from mantidqt.utils.qt import load_ui
-from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtWidgets import QFileDialog, QFileSystemModel, QWidget, QTreeWidget
+# from mantidqt.widgets.previewSelectorWidget import PreviewSelectorWidget
+from qtpy.QtCore import *
 
+from mantidqt.widgets.instrumentview.api import get_instrumentview
 
-class RawDataExplorerView(QtWidgets.QWidget):
-    """The view for the workspace calculator widget."""
+class RawDataExplorerView(QWidget):
+    """The view for the raw data explorer widget."""
+
+    file_tree_path_changed = Signal(str)
+
+    instrument_changed = Signal(str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.ui = load_ui(__file__, 'rawdataexplorer.ui', baseinstance=self)
 
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
+        self.setAttribute(Qt.WA_DeleteOnClose, True)
+        self.setup()
 
     def closeEvent(self, event):
         self.deleteLater()
         super(RawDataExplorerView, self).closeEvent(event)
+
+    def setup(self):
+        self.browse.clicked.connect(self.show_directory_manager)
+
+        # self.instrumentSelector.instrumentSelectionChanged.connect(self.instrument_changed.emit)
+        # self.instrumentSelector.instrumentSelectionChanged.connect(self.instrumentChanged.emit)
+
+    def set_file_model(self, file_model):
+        """
+        Set the QFileSystemModel behind the tree view
+        @param file_model : the file model, a QFileSystemModel object
+        """
+        self.fileTree.setModel(file_model)
+
+    def show_directory_manager(self):
+        """
+        Open a new directory manager window so the user can select a directory.
+        """
+        file_tree_path = QFileDialog().getExistingDirectory(parent=self,
+                                                            caption="Select a directory",
+                                                            directory="/home",
+                                                            options=QFileDialog.DontUseNativeDialog |
+                                                                    QFileDialog.ShowDirsOnly)
+        self.file_tree_path_changed.emit(file_tree_path)
+
+    def show_ws(self, ws_to_show):
+        iview = get_instrumentview(ws_to_show)
+        iview.show_view()
