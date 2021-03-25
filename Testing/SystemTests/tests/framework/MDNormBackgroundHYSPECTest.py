a8d938ea93897bd0ca641790b4668dd5c15566a5
Wenduo Zhou <zhouw@ornl.gov>
2021-03-25 17:14:45 -0400

    Refs #30926. Updated system test for new MDNorm.

--- Testing/SystemTests/tests/framework/MDNormBackgroundHYSPECTest.py
+++ Testing/SystemTests/tests/framework/MDNormBackgroundHYSPECTest.py
@@ -14,10 +14,11 @@ from mantid.simpleapi import (config, Load, SetGoniometer, GenerateEventsFilter,
                               DgsReduction, SetUB, CropWorkspaceForMDNorm, ConvertToMD, MergeMD,
                               MDNorm, CloneWorkspace, AddSampleLogMultiple, mtd, SaveMD,
                               MinusMD)
-import systemtesting
+import pytest
+# FIXME import systemtesting
 
 
-class MDNormHYSPECBackgroundTest(systemtesting.MantidSystemTest):
+class MDNormHYSPECBackgroundTest:  # FIXME systemtesting.MantidSystemTest):
 
     tolerance = 1e-8
 
@@ -102,22 +103,7 @@ class MDNormHYSPECBackgroundTest(systemtesting.MantidSystemTest):
     def requiredFiles(self):
         return ['HYS_13656_event.nxs']
 
-    def runTest(self):
-        """ This is the old way to do MDNorm to sample, background and finally clean background from sample
-        Now it serves as a benchmark to generate expected result for enhanced MDNorm
-        """
-        # Set facility that load data
-        config.setFacility('SNS')
-        Load(Filename='HYS_13656', OutputWorkspace='sum')
-        SetGoniometer(Workspace='sum', Axis0='s1,0,1,0,1')
-
-        # prepare sample MD
-        self.prepare_md(input_ws_name='sum', merged_md_name='merged', min_log_value=10, max_log_value=12)
-        # Prepare background workspace
-        # old way - use reduced_1 as the background
-        self.prepare_background(input_md='reduced_1', reference_sample_mde='merged',
-                                background_md='background_MDE')
-
+    def old_solution(self):
         # do MDNorm to sample data
         MDNorm(InputWorkspace='merged',
                Dimension0Name='QDimension1',
@@ -132,6 +118,10 @@ class MDNormHYSPECBackgroundTest(systemtesting.MantidSystemTest):
                OutputWorkspace='result',
                OutputDataWorkspace='dataMD',
                OutputNormalizationWorkspace='normMD')
+
+        # old way - use reduced_1 as the background
+        self.prepare_background(input_md='reduced_1', reference_sample_mde='merged',
+                                background_md='background_MDE')
 
         # do MDNorm to background
         MDNorm(InputWorkspace='background_MDE',
@@ -148,13 +138,114 @@ class MDNormHYSPECBackgroundTest(systemtesting.MantidSystemTest):
                OutputDataWorkspace='background_dataMD',
                OutputNormalizationWorkspace='background_normMD')
 
+        # Clean data
         clean_data = MinusMD('result', 'backgroundMDH')
+
+        return clean_data
+
+    @staticmethod
+    def test_property_setup():
+        """Test set up properties
+
+        :return:
+        """
+        # Test raise exception for wrong background MDE type
+        with pytest.raises(RuntimeError):
+            # Create background workspace in Q lab
+            ConvertToMD(InputWorkspace='reduced_1',
+                        QDimensions='Q3D',
+                        Q3DFrames='Q_sample',
+                        OutputWorkspace='bkgd_md',
+                        MinValues='-11,-11,-11,-25',
+                        MaxValues='11,11,11,49')
+
+            MDNorm(InputWorkspace='merged',
+                   BackgroundWorkspace='bkgd_md',
+                   Dimension0Name='QDimension1',
+                   Dimension0Binning='-5,0.05,5',
+                   Dimension1Name='QDimension2',
+                   Dimension1Binning='-5,0.05,5',
+                   Dimension2Name='DeltaE',
+                   Dimension2Binning='-2,2',
+                   Dimension3Name='QDimension0',
+                   Dimension3Binning='-0.5,0.5',
+                   SymmetryOperations='x,y,z;x,-y,z;x,y,-z;x,-y,-z',
+                   OutputWorkspace='result',
+                   OutputDataWorkspace='dataMD',
+                   OutputNormalizationWorkspace='normMD')
+
+            DeleteWorkspace(Workspace='bkgd_md')
+
+        return True
+
+    def runTest(self):
+        """ This is the old way to do MDNorm to sample, background and finally clean background from sample
+        Now it serves as a benchmark to generate expected result for enhanced MDNorm
+        """
+        # Set facility that load data
+        config.setFacility('SNS')
+        Load(Filename='HYS_13656', OutputWorkspace='sum')
+        SetGoniometer(Workspace='sum', Axis0='s1,0,1,0,1')
+
+        # prepare sample MD
+        self.prepare_md(input_ws_name='sum', merged_md_name='merged', min_log_value=10, max_log_value=12)
+
+        # Test
+        self.test_property_setup()
+
+        # Prepare background workspace
+        self.prepare_single_exp_info_background(input_md_name='reduced_1',
+                                                output_md_name='bkgd_md',
+                                                target_qframe='Q_lab')
+
+        # Test new solutions
+        r = MDNorm(InputWorkspace='merged',
+                   BackgroundWorkspace='bkgd_md',  # TODO Enable/Disable
+                   Dimension0Name='QDimension1',
+                   Dimension0Binning='-5,0.05,5',
+                   Dimension1Name='QDimension2',
+                   Dimension1Binning='-5,0.05,5',
+                   Dimension2Name='DeltaE',
+                   Dimension2Binning='-2,2',
+                   Dimension3Name='QDimension0',
+                   Dimension3Binning='-0.5,0.5',
+                   SymmetryOperations='x,y,z;x,-y,z;x,y,-z;x,-y,-z',
+                   OutputWorkspace='clean_data',
+                   OutputDataWorkspace='dataMD',
+                   OutputNormalizationWorkspace='normMD',
+                   OutputBackgroundNormalizationWorkspace='normBkgdMD')
+        assert hasattr(r, 'OutputBackgroundNormalizationWorkspace')
+        clean_data = r.OutputWorkspace
+
         SaveMD(InputWorkspace=clean_data, Filename='/tmp/clean.nxs')
         SaveMD(InputWorkspace='normMD', Filename='/tmp/sample_norm.nxs')
         SaveMD(InputWorkspace='dataMD', Filename='/tmp/sample_data.nxs')
         SaveMD(InputWorkspace='background_normMD', Filename='/tmp/normed_background.nxs')
         SaveMD(InputWorkspace='background_MDE', Filename='/tmp/background_data.nxs')
 
+        assert mtd.doesExist('clean_data')
+
     def validate(self):
         self.tolerance = 1e-8
-        return 'result','MDNormHYSPEC.nxs'
+        return 'result', 'MDNormHYSPEC.nxs'
+
+    @staticmethod
+    def prepare_single_exp_info_background(input_md_name, output_md_name, target_qframe='Q_lab'):
+        """Prepare a background MDE containing only 1 ExperimentInfo
+
+        :param self:
+        :return:
+        """
+        if target_qframe not in ['Q_sample', 'Q_lab']:
+            raise ValueError(f'QFrame with name {target_qframe} is not recognized.')
+
+        # Create background workspace in Q lab
+        ConvertToMD(InputWorkspace=input_md_name,
+                    QDimensions='Q3D',
+                    Q3DFrames=target_qframe,
+                    OutputWorkspace=output_md_name,
+                    MinValues='-11,-11,-11,-25',
+                    MaxValues='11,11,11,49')
+
+
+MDNormHYSPECBackgroundTest().runTest()
