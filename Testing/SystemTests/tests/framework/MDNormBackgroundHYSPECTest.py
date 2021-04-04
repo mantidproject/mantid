98c5cd8311aa2e85a602aac774f7b94851b62611
Wenduo Zhou <zhouw@ornl.gov>
2021-04-04 16:42:52 -0400

    Refs #30926. Expanded sysmtem test with accumulation mode.

--- Testing/SystemTests/tests/framework/MDNormBackgroundHYSPECTest.py
+++ Testing/SystemTests/tests/framework/MDNormBackgroundHYSPECTest.py
@@ -15,6 +15,7 @@ from mantid.simpleapi import (config, Load, SetGoniometer, GenerateEventsFilter,
                               MinusMD, RenameWorkspace, CompareMDWorkspaces)
 from typing import Tuple
 import systemtesting
+from collections import namedtuple
 
 # Define symmetry operation
 SYMMETRY_OPERATION = 'x,y,z;x,-y,z;x,y,-z;x,-y,-z'
@@ -22,13 +23,16 @@ SYMMETRY_OPERATION = 'x,y,z;x,-y,z;x,y,-z;x,-y,-z'
 NUM_SO = SYMMETRY_OPERATION.count(';') + 1
 LOG_VALUE_STEP = 1
 
+# Returned workspaces
+MDNormResults = namedtuple('MDNormResults', 'cleaned sample_data, sample_norm, bkgd_data, bkgd_norm')
+
 
 class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
 
     tolerance = 1e-8
 
     @staticmethod
-    def prepare_md(input_ws_name, merged_md_name, min_log_value, max_log_value, log_step):
+    def prepare_md(input_ws_name, merged_md_name, min_log_value, max_log_value, log_step, prefix) -> str:
         """Load raw event Nexus file and reduce to MDEventWorkspace
         """
         # Filter
@@ -44,7 +48,7 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
                              MinimumLogValue=min_log_value,
                              MaximumLogValue=max_log_value,
                              **gef_kw_dict)
-        FilterEvents(InputWorkspace='sum',
+        FilterEvents(InputWorkspace=input_ws_name,
                      SplitterWorkspace='splboth',
                      InformationWorkspace='info',
                      FilterByPulseTime=True,
@@ -55,6 +59,7 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
         DeleteWorkspace('splboth')
         DeleteWorkspace('info')
 
+        reduced_ws_group = f'reduced_{prefix}'
         DgsReduction(SampleInputWorkspace='split',
                      SampleInputMonitorWorkspace='split_1',
                      IncidentEnergyGuess=50,
@@ -62,21 +67,21 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
                      TimeIndepBackgroundSub=True,
                      TibTofRangeStart=10400,
                      TibTofRangeEnd=12400,
-                     OutputWorkspace='reduced')
+                     OutputWorkspace=reduced_ws_group)
         # Clean memory
         DeleteWorkspace('split')
 
-        SetUB(Workspace='reduced',
+        SetUB(Workspace=reduced_ws_group,
               a=5.823,
               b=6.475,
               c=3.186,
               u='0,1,0',
               v='0,0,1')
-        CropWorkspaceForMDNorm(InputWorkspace='reduced',
+        CropWorkspaceForMDNorm(InputWorkspace=reduced_ws_group,
                                XMin=-25,
                                XMax=49,
-                               OutputWorkspace='reduced')
-        ConvertToMD(InputWorkspace='reduced',
+                               OutputWorkspace=reduced_ws_group)
+        ConvertToMD(InputWorkspace=reduced_ws_group,
                     QDimensions='Q3D',
                     Q3DFrames='Q_sample',
                     OutputWorkspace='md',
@@ -87,6 +92,8 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
             RenameWorkspace(mtd['md'][0], merged_md_name)
         else:
             MergeMD(InputWorkspaces='md', OutputWorkspace=merged_md_name)
+
+        return reduced_ws_group
 
     @staticmethod
     def prepare_background(input_md, reference_sample_mde, background_md: str):
@@ -102,7 +109,7 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
             phi, chi, omega = data_MDE.getExperimentInfo(i).run().getGoniometer().getEulerAngles('YZY')
             AddSampleLogMultiple(Workspace=dgs_data,
                                  LogNames='phi, chi, omega',
-                                 LogValues='{},{},{}'.format(phi,chi,omega))
+                                 LogValues='{},{},{}'.format(phi, chi, omega))
             SetGoniometer(Workspace=dgs_data, Goniometers='Universal')
             ConvertToMD(InputWorkspace=dgs_data,
                         QDimensions='Q3D',
@@ -120,14 +127,19 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
     def requiredFiles(self):
         return ['HYS_13656_event.nxs']
 
-    def run_old_solution(self, input_sample_md='merged', background_seed_md='reduced_1', prefix='old'):
+    def run_old_solution(self, input_sample_md, background_seed_md, prefix, temp_norm_result=None):
         """Do MDNorm to sample and background and clean sample with background in old way
         """
         sample_processed_mdh = f'{prefix}_reducedMD'
         sample_data_mdh = f'{prefix}_dataMD'
         sample_norm_mdh = f'{prefix}_normMD'
         bkgd_processed_mdh = f'{prefix}_backgroundMDH'
+
         # do MDNorm to sample data
+        data_temp_dict = dict()
+        if temp_norm_result:
+            data_temp_dict['TemporaryDataWorkspace'] = temp_norm_result.sample_data
+            data_temp_dict['TemporaryNormalizationWorkspace'] = temp_norm_result.sample_norm
         MDNorm(InputWorkspace=input_sample_md,
                Dimension0Name='QDimension1',
                Dimension0Binning='-5,0.05,5',
@@ -140,14 +152,19 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
                SymmetryOperations=SYMMETRY_OPERATION,
                OutputWorkspace=sample_processed_mdh,
                OutputDataWorkspace=sample_data_mdh,
-               OutputNormalizationWorkspace=sample_norm_mdh)
+               OutputNormalizationWorkspace=sample_norm_mdh,
+               **data_temp_dict)
 
         # use reduced_1 as the background
         self.prepare_background(input_md=background_seed_md,
-                                reference_sample_mde='merged',
+                                reference_sample_mde=input_sample_md,
                                 background_md='background_MDE')
 
         # do MDNorm to background
+        data_temp_dict = dict()
+        if temp_norm_result:
+            data_temp_dict['TemporaryDataWorkspace'] = temp_norm_result.bkgd_data
+            data_temp_dict['TemporaryNormalizationWorkspace'] = temp_norm_result.bkgd_norm
         bkgd_data_mdh = f'{prefix}_background_dataMD'
         bkgd_norm_mdh = f'{prefix}_background_normMD'
         MDNorm(InputWorkspace='background_MDE',
@@ -162,51 +179,20 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
                SymmetryOperations=SYMMETRY_OPERATION,
                OutputWorkspace=bkgd_processed_mdh,
                OutputDataWorkspace=bkgd_data_mdh,
-               OutputNormalizationWorkspace=bkgd_norm_mdh)
+               OutputNormalizationWorkspace=bkgd_norm_mdh,
+               **data_temp_dict)
 
         # clean data
         clean_data = MinusMD(sample_processed_mdh, bkgd_processed_mdh, OutputWorkspace=f'{prefix}_clean')
 
-        return clean_data, sample_data_mdh, sample_norm_mdh, bkgd_data_mdh, bkgd_norm_mdh
+        # Output
+        norm_step1 = {"cleaned": clean_data,
+                      "sample_data": sample_data_mdh,
+                      "sample_norm": sample_norm_mdh,
+                      "bkgd_data": bkgd_data_mdh,
+                      "bkgd_norm": bkgd_norm_mdh}
 
-    @staticmethod
-    def test_property_setup():
-        """Test set up properties
-
-        :return:
-        """
-        # Test raise exception for wrong background MDE type
-        try:
-            # Create background workspace in Q lab
-            ConvertToMD(InputWorkspace='reduced_1',
-                        QDimensions='Q3D',
-                        Q3DFrames='Q_sample',
-                        OutputWorkspace='bkgd_md',
-                        MinValues='-11,-11,-11,-25',
-                        MaxValues='11,11,11,49')
-
-            MDNorm(InputWorkspace='merged',
-                   BackgroundWorkspace='bkgd_md',
-                   Dimension0Name='QDimension1',
-                   Dimension0Binning='-5,0.05,5',
-                   Dimension1Name='QDimension2',
-                   Dimension1Binning='-5,0.05,5',
-                   Dimension2Name='DeltaE',
-                   Dimension2Binning='-2,2',
-                   Dimension3Name='QDimension0',
-                   Dimension3Binning='-0.5,0.5',
-                   SymmetryOperations='x,y,z;x,-y,z;x,y,-z;x,-y,-z',
-                   OutputWorkspace='result',
-                   OutputDataWorkspace='dataMD',
-                   OutputNormalizationWorkspace='normMD')
-
-            DeleteWorkspace(Workspace='bkgd_md')
-        except RuntimeError:
-            pass
-        else:
-            raise AssertionError(f'Expected failure due to Background MD in Q_sample.')
-
-        return True
+        return MDNormResults(**norm_step1)
 
     def runTest(self):
         """ This is the old way to do MDNorm to sample, background and finally clean background from sample
@@ -223,21 +209,22 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
         SetGoniometer(Workspace='sum', Axis0='s1,0,1,0,1')
 
         # prepare sample MD: in test must between 10 and 12 to match the gold data
-        self.prepare_md(input_ws_name='sum', merged_md_name='merged', min_log_value=10, max_log_value=12,
-                        log_step=LOG_VALUE_STEP)
+        dgs_red_group_name1 = self.prepare_md(input_ws_name='sum', merged_md_name='merged1',
+                                              min_log_value=10, max_log_value=12,
+                                              log_step=LOG_VALUE_STEP, prefix='step1')
 
         # 1-1 Existing method (but to keep)
-        clean_data, sample_data_mdh, sample_norm_mdh, background_data_mdh, background_norm_mdh = self.run_old_solution()
-        self._old_clean = str(clean_data)
+        benchmark = self.run_old_solution('merged1', f'{dgs_red_group_name1}_1', 'oldstep1')
+        self._old_clean = str(benchmark.cleaned)
 
         # Prepare background workspace
-        self.prepare_single_exp_info_background(input_md_name='reduced_1',
-                                                output_md_name='bkgd_md',
+        self.prepare_single_exp_info_background(input_md_name=f'{dgs_red_group_name1}_1',
+                                                output_md_name='bkgd_md1',
                                                 target_qframe='Q_lab')
 
         # Test new solutions
-        r = MDNorm(InputWorkspace='merged',
-                   BackgroundWorkspace='bkgd_md',
+        r = MDNorm(InputWorkspace='merged1',
+                   BackgroundWorkspace='bkgd_md1',
                    Dimension0Name='QDimension1',
                    Dimension0Binning='-5,0.05,5',
                    Dimension1Name='QDimension2',
@@ -255,15 +242,65 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
         # Test solution
         assert hasattr(r, 'OutputBackgroundNormalizationWorkspace'), 'MDNorm does not execute.'
         # Compare workspaces: binned background data workspace
-        rc = CompareMDWorkspaces(Workspace1=background_data_mdh, Workspace2='background_dataMD')
+        rc = CompareMDWorkspaces(Workspace1=benchmark.bkgd_data, Workspace2='background_dataMD')
         assert rc.Equals, f'Error message: {rc.Result}'
         # Compare workspaces: background normalization workspace
-        r89 = CompareMDWorkspaces(Workspace1=background_norm_mdh, Workspace2='background_normMD', Tolerance=1E-9)
+        r89 = CompareMDWorkspaces(Workspace1=benchmark.bkgd_norm, Workspace2='background_normMD', Tolerance=1E-9)
         assert r89.Equals, f'Incompatible background normalization workspaces: {r89.Result}'
+
+        print(f'[STATUS REPORT] Task #88 and #89 non-accumulation mode is done... ...')
+
+        # Round 2: Test accumulation mode
+        dgs_red_group_name1 = self.prepare_md(input_ws_name='sum', merged_md_name='merged2', min_log_value=13,
+                                              max_log_value=15, log_step=LOG_VALUE_STEP, prefix='step2')
+        # Old solution
+        benchmark2 = self.run_old_solution('merged2', f'{dgs_red_group_name1}_2', 'oldstep2', benchmark)
+        # 1-2 Test new solutions
+        self.prepare_single_exp_info_background(input_md_name=f'{dgs_red_group_name1}_2',
+                                                output_md_name='bkgd_md2',
+                                                target_qframe='Q_lab')
+        r = MDNorm(InputWorkspace='merged2',
+                   BackgroundWorkspace='bkgd_md2',
+                   TemporaryDataWorkspace='dataMD',
+                   TemporaryNormalizationWorkspace='normMD',
+                   TemporaryBackgroundDataWorkspace='background_dataMD',
+                   TemporaryBackgroundNormalizationWorkspace='background_normMD',
+                   Dimension0Name='QDimension1',
+                   Dimension0Binning='-5,0.05,5',
+                   Dimension1Name='QDimension2',
+                   Dimension1Binning='-5,0.05,5',
+                   Dimension2Name='DeltaE',
+                   Dimension2Binning='-2,2',
+                   Dimension3Name='QDimension0',
+                   Dimension3Binning='-0.5,0.5',
+                   SymmetryOperations=SYMMETRY_OPERATION,
+                   OutputWorkspace='clean_data_2',
+                   OutputDataWorkspace='dataMD2',
+                   OutputNormalizationWorkspace='normMD2',
+                   OutputBackgroundDataWorkspace='background_dataMD2',
+                   OutputBackgroundNormalizationWorkspace='background_normMD2')
+
+        # Compare data workspace
+        r_sd = CompareMDWorkspaces(Workspace1=benchmark2.sample_data, Workspace2='dataMD', Tolerance=1E-7)
+        assert r_sd.Equals, f'Sample binned data MD not equal: {r_sd.Result}'
+
+        # Compare gold (old solution) and new: passed test in #88
+        r = CompareMDWorkspaces(Workspace1=benchmark2.bkgd_data, Workspace2='background_dataMD2')
+        if r.Equals:
+            print(f'Accumulation mode: Background binned data MD: {r.Result}')
+        else:
+            raise ValueError(f'Accumulation mode: Background binned data MD: {r.Result}')
+
+        # Compare gold (old solution) and new: passed test in #88
+        r_bn = CompareMDWorkspaces(Workspace1=benchmark2.bkgd_norm, Workspace2='background_normMD2', Tolerance=1E-9)
+        if r_bn.Equals:
+            print(f'Accumulation mode: Background normlaization MD: {r_bn.Result}')
+        else:
+            raise ValueError(f'Accumulation mode: Background normalization MD: {r_bn.Result}')
 
         # Test: This can mess some experimental data set up for verifying
         # whether the reduction is correct.
-        self.test_property_setup()
+        self.test_property_setup('merged1', f'{dgs_red_group_name1}_1')
 
         # clean_data = r.OutputWorkspace
         # print(f'[VZ DEBUG] clean data: s1 = 10 .. 12: Number of Experiment Info = {clean_data.getNumExperimentInfo()}')
@@ -406,3 +443,40 @@ class MDNormBackgroundHYSPECTest(systemtesting.MantidSystemTest):
         # origin/SPEC59_MDNormBkgd_feature
 
         return clean_md
+
+    @staticmethod
+    def test_property_setup(input_mde: str, reduced_background: str):
+        """Test set up properties
+
+        :return:
+        """
+        # Test raise exception for wrong background MDE type
+        try:
+            # Create background workspace in Q lab
+            ConvertToMD(InputWorkspace=reduced_background,
+                        QDimensions='Q3D',
+                        Q3DFrames='Q_sample',
+                        OutputWorkspace='bkgd_md',
+                        MinValues='-11,-11,-11,-25',
+                        MaxValues='11,11,11,49')
+
+            MDNorm(InputWorkspace=input_mde,
+                   BackgroundWorkspace='bkgd_md',
+                   Dimension0Name='QDimension1',
+                   Dimension0Binning='-5,0.05,5',
+                   Dimension1Name='QDimension2',
+                   Dimension1Binning='-5,0.05,5',
+                   Dimension2Name='DeltaE',
+                   Dimension2Binning='-2,2',
+                   Dimension3Name='QDimension0',
+                   Dimension3Binning='-0.5,0.5',
+                   SymmetryOperations='x,y,z;x,-y,z;x,y,-z;x,-y,-z',
+                   OutputWorkspace='result',
+                   OutputDataWorkspace='dataMD',
+                   OutputNormalizationWorkspace='normMD')
+
+            DeleteWorkspace(Workspace='bkgd_md')
+        except RuntimeError:
+            pass
+        else:
+            raise AssertionError(f'Expected failure due to Background MD in Q_sample.')
