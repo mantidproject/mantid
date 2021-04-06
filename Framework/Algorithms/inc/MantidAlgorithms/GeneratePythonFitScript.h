ea0fb52b68c1e46d9a4bfb318d9724ac8b844678
Robert Applin <robert.applin@stfc.ac.uk>
2021-04-06 15:43:33 +0100

    Refs #29911. Generate a sequential fit python script.

--- Framework/Algorithms/inc/MantidAlgorithms/GeneratePythonFitScript.h
+++ Framework/Algorithms/inc/MantidAlgorithms/GeneratePythonFitScript.h
@@ -6,8 +6,12 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
+#include "MantidAPI/IFunction_fwd.h"
 #include "MantidAPI/SerialAlgorithm.h"
 #include "MantidAlgorithms/DllConfig.h"
+
+#include <string>
+#include <vector>
 
 namespace Mantid {
 namespace Algorithms {
@@ -27,6 +31,16 @@ private:
   void init() override;
   std::map<std::string, std::string> validateInputs() override;
   void exec() override;
+
+  std::string generateVariableSetupCode(std::vector<std::string> const &inputWorkspaces,
+                                        std::vector<std::size_t> const &workspaceIndices,
+                                        std::vector<double> const &startXs, std::vector<double> const &endXs,
+                                        Mantid::API::IFunction_const_sptr function) const;
+  std::string generateSequentialFittingCode() const;
+  std::string generateFitOptionsString() const;
+  std::string generateCodeForTidyingFitOutput() const;
+
+  void savePythonScript(std::string const &contents) const;
 };
 
 } // namespace Algorithms
