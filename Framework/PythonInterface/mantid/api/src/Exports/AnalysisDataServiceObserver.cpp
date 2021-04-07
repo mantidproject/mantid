0dcd63910f9a6fece1760a8962eeb5f55a377b7b
Joseph Torsney <joseph.torsney@stfc.ac.uk>
2021-04-07 08:54:10 +0100

    Apply formatting patch re30523

--- Framework/PythonInterface/mantid/api/src/Exports/AnalysisDataServiceObserver.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/AnalysisDataServiceObserver.cpp
@@ -17,65 +17,63 @@ using namespace Mantid::PythonInterface;
 using namespace boost::python;
 
 namespace {
-    void observeAll(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeAll(turnOn);
-    }
-
-    void observeRename(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeRename(turnOn);
-    }
-
-    void observeAdd(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeAdd(turnOn);
-    }
-
-    void observeReplace(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeReplace(turnOn);
-    }
-
-    void observeDelete(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeDelete(turnOn);
-    }
-
-    void observeClear(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeClear(turnOn);
-    }
-
-    void observeGroup(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeGroup(turnOn);
-    }
-
-    void observeUnGroup(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeUnGroup(turnOn);
-    }
-
-    void observeGroupUpdate(AnalysisDataServiceObserver &self, bool turnOn) {
-      ReleaseGlobalInterpreterLock releaseGil;
-      self.observeGroupUpdate(turnOn);
-    }
+void observeAll(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeAll(turnOn);
 }
+
+void observeRename(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeRename(turnOn);
+}
+
+void observeAdd(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeAdd(turnOn);
+}
+
+void observeReplace(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeReplace(turnOn);
+}
+
+void observeDelete(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeDelete(turnOn);
+}
+
+void observeClear(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeClear(turnOn);
+}
+
+void observeGroup(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeGroup(turnOn);
+}
+
+void observeUnGroup(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeUnGroup(turnOn);
+}
+
+void observeGroupUpdate(AnalysisDataServiceObserver &self, bool turnOn) {
+  ReleaseGlobalInterpreterLock releaseGil;
+  self.observeGroupUpdate(turnOn);
+}
+} // namespace
 
 void export_AnalysisDataServiceObserver() {
   boost::python::class_<AnalysisDataServiceObserver, bases<>, AnalysisDataServiceObserverAdapter, boost::noncopyable>(
       "AnalysisDataServiceObserver", "Observes AnalysisDataService notifications: all only")
-      .def("observeAll", &observeAll, (arg("self"), arg("on")),
-           "Observe AnalysisDataService for any changes")
+      .def("observeAll", &observeAll, (arg("self"), arg("on")), "Observe AnalysisDataService for any changes")
       .def("observeAdd", &observeAdd, (arg("self"), arg("on")),
            "Observe AnalysisDataService for a workspace being added")
       .def("observeReplace", &observeReplace, (arg("self"), arg("on")),
            "Observe AnalysisDataService for a workspace being replaced")
       .def("observeDelete", &observeDelete, (arg("self"), arg("on")),
            "Observe AnalysisDataService for a workspace being deleted")
-      .def("observeClear", &observeClear, (arg("self"), arg("on")),
-           "Observe AnalysisDataService for it being cleared")
+      .def("observeClear", &observeClear, (arg("self"), arg("on")), "Observe AnalysisDataService for it being cleared")
       .def("observeRename", &observeRename, (arg("self"), arg("on")),
            "Observe AnalysisDataService for a workspace being renamed")
       .def("observeGroup", &observeGroup, (arg("self"), arg("on")),
