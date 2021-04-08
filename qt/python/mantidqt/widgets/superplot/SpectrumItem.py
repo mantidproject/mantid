6051e66c500f206746643407964a197de2bd9aac
Guillaume Communie <communieg@ill.eu>
2021-04-08 14:06:23 +0200

    [SPLOT] link spectrum to workspace
    When the delete button associated to a spectrum is pressed, the signal
    contains the spectrum index but also the workpace name. This is
    necessary to delete the correct spectrum from the list.

--- qt/python/mantidqt/widgets/superplot/SpectrumItem.py
+++ qt/python/mantidqt/widgets/superplot/SpectrumItem.py
@@ -14,8 +14,11 @@ class SpectrumItemSignals(QObject):
 
     """
     Thrown when the delete button is pressed.
+    Args:
+        str: workspace name
+        int: spectrum index
     """
-    delClicked = Signal(int)
+    delClicked = Signal(str, int)
 
     def __init__(self):
         super().__init__()
@@ -27,6 +30,11 @@ class SpectrumItem(QTreeWidgetItem):
     Index of the spectrum represented by this item.
     """
     _spectrumIndex = None
+
+    """
+    Name of the corresponding workspace.
+    """
+    _workspaceName = None
 
     """
     Delete button.
@@ -41,6 +49,7 @@ class SpectrumItem(QTreeWidgetItem):
     def __init__(self, treeWidget, index):
         super().__init__(treeWidget)
         self._spectrumIndex = index
+        self._workspaceName = self.parent().getWorkspaceName()
         self._delButton = QToolButton()
         self._delButton.setMinimumSize(20, 20)
         self._delButton.setMaximumSize(20, 20)
@@ -48,7 +57,8 @@ class SpectrumItem(QTreeWidgetItem):
         self._delButton.setToolTip("Remove from the list")
         self.signals = SpectrumItemSignals()
         self._delButton.clicked.connect(
-                 lambda c : self.signals.delClicked.emit(self._spectrumIndex))
+                 lambda c : self.signals.delClicked.emit(self._workspaceName,
+                                                         self._spectrumIndex))
         self.treeWidget().setItemWidget(self, 1, self._delButton)
         self.treeWidget().resizeColumnToContents(1)
 
