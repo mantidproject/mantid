e5c78de53024c2ef3010c431b224ae9421e88b41
Tom Hampson <thomas.hampson@stfc.ac.uk>
2021-05-06 17:27:50 +0100

    Added failing test for zooming out too far.

--- qt/applications/workbench/workbench/plotting/test/test_utility.py
+++ qt/applications/workbench/workbench/plotting/test/test_utility.py
@@ -7,11 +7,14 @@
 #  This file is part of the mantid package
 import unittest
 
+import matplotlib
+matplotlib.use("Agg")  # noqa
 import matplotlib.pyplot as plt
 from mantid.plots.legend import LegendProperties
 from numpy import testing as np_testing
 
 from mantid.plots.utility import zoom, zoom_axis
+from testhelpers import assertRaisesNothing
 
 
 class TestUtility(unittest.TestCase):
@@ -38,6 +41,19 @@ class TestUtility(unittest.TestCase):
         legend_props = LegendProperties.from_legend(ax.legend_)
 
         self.assertEqual(legend_props['title'], '')
+
+    def test_zoom_out_really_far(self):
+        """
+        Attempt to zoom out far enough so that the new y-axis limits are beyond the maximum size of a float.
+        """
+        fig, ax = plt.subplots()
+        ax.plot([0, 1, 2], [1e10, -1e10, 1e10])
+
+        zoom_point = [0, 0]
+        zoom_factor = 1e-300
+
+        # zoom function will attempt to set the y-axis limits to +/-1e300.
+        assertRaisesNothing(self, zoom, ax, *zoom_point, zoom_factor)
 
 
 if __name__ == '__main__':
