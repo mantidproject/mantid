b8222eb8740e4a743507ea3b00170e7759b65804
abuts <Alex.Buts@stfc.ac.uk>
2021-05-10 14:03:37 +0100

    Update scripts/test/RunDescriptorTest.py
    Co-authored-by: Harrietbrown <49688535+Harrietbrown@users.noreply.github.com>

--- scripts/test/RunDescriptorTest.py
+++ scripts/test/RunDescriptorTest.py
@@ -716,7 +716,7 @@ class RunDescriptorTest(unittest.TestCase):
 
     def test_remove_background_matrix_minus_matrix(self):
         # Verify situation if sampe workspace is in event mode and
-        # and background loaded as hisograms with monitors included
+        # and background loaded as a histogram with monitors included
         bg_ws = CreateSampleWorkspace(Function='Multiple Peaks', WorkspaceType='Histogram',
                                       NumBanks=3, NumMonitors=3, BankPixelWidth=1, NumEvents=100, XUnit='TOF',
                                       XMin=2000, XMax=20000, BinWidth=1)
