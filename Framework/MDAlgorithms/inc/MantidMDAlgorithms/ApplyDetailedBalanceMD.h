8c9d601e1b16f24419af084d7ffbde6f3b1d5fcd
Wenduo Zhou <zhouw@ornl.gov>
2021-05-12 23:08:52 -0400

    Refs #31459.  Set up new alg including unit test.

---
+++ Framework/MDAlgorithms/inc/MantidMDAlgorithms/ApplyDetailedBalanceMD.h
@@ -0,0 +1,33 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2011 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidAPI/Algorithm.h"
+#include "MantidKernel/System.h"
+#include "MantidMDAlgorithms/BinaryOperationMD.h"
+#include "MantidMDAlgorithms/BooleanBinaryOperationMD.h"
+
+namespace Mantid {
+namespace MDAlgorithms {
+
+/** ApplyDetailedBalance : Perform the And boolean operation on two MDHistoWorkspaces
+
+  @date 2011-11-08
+*/
+class DLLExport ApplyDetailedBalanceMD : public BooleanBinaryOperationMD {
+public:
+  const std::string name() const override;
+  int version() const override;
+  const std::vector<std::string> seeAlso() const override { return {"XorMD", "OrMD", "NotMD"}; }
+
+private:
+  void execHistoHisto(Mantid::DataObjects::MDHistoWorkspace_sptr out,
+                      Mantid::DataObjects::MDHistoWorkspace_const_sptr operand) override;
+};
+
+} // namespace MDAlgorithms
+} // namespace Mantid
