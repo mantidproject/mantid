14650e07679ca5a48ebb37aef86a6d823cd76e06
Chen <zhangc@ornl.gov>
2021-05-19 21:24:27 -0400

    first draft of the diagnostic script

--- scripts/SCD_Reduction/SCDCalibratePanels2Diagnostics.py
+++ scripts/SCD_Reduction/SCDCalibratePanels2Diagnostics.py
@@ -6,22 +6,13 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 
+import os
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
-from mantid.dataobjects import PeaksWorkspace
 from typing import List, Dict, Union
-
-
-def collectAllBankNames(peaksWorkspace: PeaksWorkspace) -> list[str]:
-    """
-    Collect all bank names from given peaks workspace
-
-    @param peaksWorkspace: input peaks workspace
-
-    @return: list of unique bank names
-    """
-    pass
+from mantid.dataobjects import PeaksWorkspace
+from mantid.simpleapi import FilterPeaks
 
 
 def SCDCalibratePanels2DiagnosticsPlotBank(
@@ -43,16 +34,18 @@ def SCDCalibratePanels2DiagnosticsPlotBank(
     npeaks = pws.getNumberPeaks()
     ub = pws.sample().getOrientedLattice().getUB()
     # plot data container
-    rowsID_measured = np.zeros(npeaks)
-    colsID_measured = np.zeros(npeaks)
+    rowsID_observed = np.zeros(npeaks)
+    colsID_observed = np.zeros(npeaks)
     rowsID_calculated = np.zeros(npeaks)
     colsID_calculated = np.zeros(npeaks)
+    # use the first peak to infer the bank name
+    bankname = pws.row(0)["BankName"]
 
     # iterative through peaks to generate data
     for i in range(npeaks):
         entry = pws.row(i)
-        rowsID_measured[i] = entry["Row"]
-        colsID_measured[i] = entry["Col"]
+        rowsID_observed[i] = entry["Row"]
+        colsID_observed[i] = entry["Col"]
         # compute the calculated entry now
         pk = pws.getPeak(i)
         gm = pk.getGoniometerMatrix()
@@ -69,11 +62,37 @@ def SCDCalibratePanels2DiagnosticsPlotBank(
     # plotting
     fig, axes = plt.subplots(1, 2)  # side by side
     # plot col cmp
+    # -- plot the diagnal helper line
+    max_rown = max(rowsID_observed.max(), rowsID_calculated.max())
+    axes[0].plot([0, max_rown], [0, max_rown], "b:", alpha=0.5)
+    # -- plot the data
+    axes[0].plot(rowsID_calculated, rowsID_observed, 'r+')
+    # -- config grid
+    axes[0].grid(color='k', linestyle='--', alpha=0.25)
+    # -- config axes
+    axes[0].set_xlabel("calculated row number")
+    axes[0].set_ylable("observed row number")
+    axes[0].set_title(f"Detector Row Number Comparison, {bankname}")
+    axes[0].figtext(0.5, 0.2, f"Number of Peaks = {npeaks}")
     # plot row cmp
+    # -- plot the diagnal helper line
+    max_coln = max(colsID_observed.max(), colsID_calculated.max())
+    axes[1].plot([0, max_coln], [0, max_coln], "b:", alpha=0.5)
+    # -- plot the data
+    axes[1].plot(colsID_calculated, colsID_observed, 'r+')
+    # -- config grid
+    axes[1].grid(color='k', linestyle='--', alpha=0.25)
+    # -- config axes
+    axes[1].set_xlabel("calculated column number")
+    axes[1].set_ylable("observed column number")
+    axes[1].set_title(f"Detector Column Number Comparison, {bankname}")
+    axes[1].figtext(0.5, 0.2, f"Number of Peaks = {npeaks}")
     # display
     if showPlots:
         fig.show(block=False)
     # save
+    fig.savefig(os.path.join(savedir, figname))
+    # notify users
     logging.info(f"Figure {figname} is saved to {savedir}.")
 
 
@@ -97,9 +116,30 @@ def SCDCalibratePanels2DiagnosticsPlot(
 
     @returs: None
     """
-    logging.info(f"Start diagnostics with {peaksWorkspace.getName()}.")
-    pass
+    logging.info(f"Start diagnostics with {peaksWorkspace.name()}.")
+    pws = peaksWorkspace
+    # process all banks if banknames is None
+    if banknames is None:
+        banknames = set([pws.row(i)["BankName"] for i in range(pws.getNumberPeaks())])
+    # one bank at a time
+    for bn in banknames:
+        logging.info(f"--processing bank: {bn}")
+        pws_filtered = FilterPeaks(
+            InputWorkspace='pws',
+            FilterVariable='h^2+k^2+l^2',
+            FilterValue=0,
+            Operator='>',
+            BankName=bn,
+        )
+        # generate the plot
+        figname = f"{config['prefix']}_{bn}.{type}"
+        SCDCalibratePanels2DiagnosticsPlotBank(
+            filteredPeaksWS=pws_filtered,
+            figname=figname,
+            savedir=config["saveto"],
+            showPlots=showPlots,
+        )
 
 
 if __name__ == "__main__":
-    print("Running SCDCalibratePanels2Diagnostics as script")
+    logging.warning("Running SCDCalibratePanels2Diagnostics as script is not supported at the moment.")
