8e3e34bbe1faf705d1804a91a957f4d7aa19dd16
Richard Waite <richard.waite@stfc.ac.uk>
2021-05-24 14:29:36 +0100

    Add test to check failed fits are not used as input for next one

--- scripts/Engineering/gui/engineering_diffraction/tabs/fitting/plotting/test/test_plot_presenter.py
+++ scripts/Engineering/gui/engineering_diffraction/tabs/fitting/plotting/test/test_plot_presenter.py
@@ -57,7 +57,7 @@ class FittingPlotPresenterTest(unittest.TestCase):
         fun_str_list = ['name=Gaussian,Height=11,PeakCentre=30000,Sigma=40',  # initial
                         'name=Gaussian,Height=10,PeakCentre=35000,Sigma=50',  # fit result of ws1
                         'name=Gaussian,Height=9,PeakCentre=40000,Sigma=60']  # fit result of ws2
-        self.view.read_fitprop_from_browser.return_value = {'properties': {'Function': fun_str_list[0]}}
+        self.view.read_fitprop_from_browser.return_value = {'properties': {'Function': fun_str_list[0]}}  # initial
         mock_fit_output = [mock.MagicMock(), mock.MagicMock()]
         mock_fit_output[0].OutputStatus = "success"
         mock_fit_output[0].Function.fun = fun_str_list[1]
@@ -82,6 +82,25 @@ class FittingPlotPresenterTest(unittest.TestCase):
             fitprop_list.append({'properties': {'Function': fun_str_list[iws + 1], 'InputWorkspace': ws, 'Output': ws},
                                  'status':mock_fit_output[iws].OutputStatus})
         mock_notifier.notify_subscribers.assert_called_once_with(fitprop_list)
+
+    @mock.patch(dir_path + '.Fit')
+    def test_do_sequential_fit_does_not_use_failed_fit_as_input(self, mock_fit):
+        ws_list = ['ws1', 'ws2']
+        fun_str_list = ['name=Gaussian,Height=11,PeakCentre=30000,Sigma=40',  # initial
+                        'name=Gaussian,Height=10,PeakCentre=35000,Sigma=50']  # fit result of ws1 (and ws2)
+        self.view.read_fitprop_from_browser.return_value = {'properties': {'Function': fun_str_list[0]}}  # initial
+        mock_fit_output = mock.MagicMock()
+        mock_fit_output.OutputStatus = "fail"
+        mock_fit_output.Function.fun = fun_str_list[1]
+        mock_fit.return_value = mock_fit_output
+
+        self.presenter.do_sequential_fit(ws_list)
+
+        self.assertEqual(mock_fit.call_count, len(ws_list))
+        for iws, ws in enumerate(ws_list):
+            # check calls to fit use initial guess for both workspaces as first fit was unsuccessful
+            _, _, kwargs = mock_fit.mock_calls[iws]
+            self.assertEquals(kwargs, {'Function': fun_str_list[0], 'InputWorkspace': ws, 'Output': ws})
 
 
 if __name__ == '__main__':
