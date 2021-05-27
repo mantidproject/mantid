2143f943bfe61ab414849649d622d75fe8df9885
Guillaume Communie <communieg@ill.eu>
2021-05-27 13:35:52 +0200

    [DRILL] new parameter presenter class

---
+++ scripts/Interface/ui/drill/presenter/DrillParameterPresenter.py
@@ -0,0 +1,48 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+from qtpy.QtGui import QBrush, QColor
+from qtpy.QtCore import *
+
+
+class DrillParameterPresenter:
+
+    """
+    Reference to the QTableWidgetItem.
+    """
+    _item = None
+
+    """
+    Reference to the parameter model.
+    """
+    _parameter = None
+
+    ERROR_COLOR = "#3fff0000"
+
+    def __init__(self, item, parameter):
+        self._item = item
+        self._parameter = parameter
+        self._parameter.valid.connect(self.onValid)
+        self._parameter.invalid.connect(self.onInvalid)
+
+    def onValid(self):
+        """
+        Triggered when the parameter is valid.
+        """
+        self._item.setData(0, Qt.BackgroundRole, None)
+        self._item.setToolTip("")
+
+    def onInvalid(self, msg):
+        """
+        Triggered when the parameter is invalid.
+
+        Args:
+            msg (str): error message associated to the invalid state
+        """
+        brush = QBrush(QColor(self.ERROR_COLOR))
+        self._item.setBackground(brush)
+        self._item.setToolTip(msg)
