ff4ed3350a4a6b0d75d1c20548c3e84d6969e854
Guillaume Communie <communieg@ill.eu>
2021-05-28 10:52:11 +0200

    [SPLOT] keep homogeneity in context menu entries

--- qt/widgets/common/src/WorkspacePresenter/WorkspaceTreeWidgetSimple.cpp
+++ qt/widgets/common/src/WorkspacePresenter/WorkspaceTreeWidgetSimple.cpp
@@ -46,7 +46,7 @@ WorkspaceTreeWidgetSimple::WorkspaceTreeWidgetSimple(bool viewOnly, QWidget *par
       m_overplotMDHisto1D(new QAction("Overplot 1D MDHistogram...", this)),
       m_plotMDHisto1DWithErrs(new QAction("Plot 1D MDHistogram with errors...", this)),
       m_overplotMDHisto1DWithErrs(new QAction("Overplot 1D MDHistogram with errors...", this)),
-      m_sampleMaterial(new QAction("Show Sample Material", this)), m_superplot(new QAction("Superplot", this)) {
+      m_sampleMaterial(new QAction("Show Sample Material", this)), m_superplot(new QAction("Superplot...", this)) {
 
   // Replace the double click action on the MantidTreeWidget
   m_tree->m_doubleClickAction = [&](const QString &wsName) { emit workspaceDoubleClicked(wsName); };
