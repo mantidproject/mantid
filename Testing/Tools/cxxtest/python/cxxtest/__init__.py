6d542dcfdef920f59bf3e555112c31d96d7aff8a
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-06-09 15:33:52 +0100

    Fix Cxxtestgen scripts

--- Testing/Tools/cxxtest/python/cxxtest/__init__.py
+++ Testing/Tools/cxxtest/python/cxxtest/__init__.py
@@ -25,8 +25,12 @@ The cxxtest Python package includes capabilities for parsing C/C++ source files 
 CxxTest drivers.
 """
 
+from cxxtest.__release__ import __version__, __date__  # noqa: F401
+
 __maintainer__ = "TODO"
 __maintainer_email__ = "TODO"
 __copyright__ = "TODO"
 __license__ = "LGPL"
 __url__ = "http://cxxtest.tigris.org/"
+
+from cxxtest.cxxtestgen import *  # noqa: E402, F401
