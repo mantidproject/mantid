fec9d428d4ebc18876a0872620d0ba1edf9b6d7c
Guillaume Communie <communieg@ill.eu>
2021-06-14 14:06:56 +0200

    [RDEXP] preview for spec ILL instruments

--- Framework/API/src/PreviewDeclarations.cpp
+++ Framework/API/src/PreviewDeclarations.cpp
@@ -11,6 +11,57 @@
 namespace Mantid {
 namespace DataHandling {
 
+// Spectroscopy
+class SpecCountDiffractionCurve : public API::IPreview {
+public:
+  std::string name() const override { return "DiffractionCurve"; }
+  std::string facility() const override { return "ILL"; }
+  std::string technique() const override { return "Spectroscopy"; }
+  std::string acquisition() const override { return "Count"; }
+  std::string geometry() const override { return ""; }
+  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::PLOT1D; }
+};
+
+class SpecTOFCountDiffractionCurve : public API::IPreview {
+public:
+  std::string name() const override { return "DiffractionCurve"; }
+  std::string facility() const override { return "ILL"; }
+  std::string technique() const override { return "Spectroscopy"; }
+  std::string acquisition() const override { return "TOFCount"; }
+  std::string geometry() const override { return ""; }
+  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::PLOT1D; }
+};
+
+class SpecScanCountDiffractionCurveScan : public API::IPreview {
+public:
+  std::string name() const override { return "DiffractionCurveScan"; }
+  std::string facility() const override { return "ILL"; }
+  std::string technique() const override { return "Spectroscopy"; }
+  std::string acquisition() const override { return "ScanCount"; }
+  std::string geometry() const override { return ""; }
+  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::PLOT2D; }
+};
+
+class SpecTOFCountPowderGrouping : public API::IPreview {
+public:
+  std::string name() const override { return "PowderGrouping"; }
+  std::string facility() const override { return "ILL"; }
+  std::string technique() const override { return "Spectroscopy"; }
+  std::string acquisition() const override { return "TOFCount"; }
+  std::string geometry() const override { return ""; }
+  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::PLOT2D; }
+};
+
+class SpecAllRawDataInstrumentView : public API::IPreview {
+public:
+  std::string name() const override { return "InstrumentView"; }
+  std::string facility() const override { return "ILL"; }
+  std::string technique() const override { return "Spectroscopy"; }
+  std::string acquisition() const override { return ""; }
+  std::string geometry() const override { return ""; }
+  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::IVIEW; }
+};
+
 // SANS
 class SANSMonoRawDataInstrumentView : public API::IPreview {
 public:
@@ -152,61 +203,15 @@ public:
 // BACK
 // TODO
 
-// SPEC
-// TODO
-class SpecRawDataMonoIview : public API::IPreview {
-public:
-  std::string name() const override { return "IView | RawData"; }
-  std::string facility() const override { return "ILL"; }
-  std::string technique() const override { return "SPEC"; }
-  std::string acquisition() const override { return "Mono"; }
-  std::string geometry() const override { return "2D"; }
-  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::IVIEW; }
-};
-
-class SpecRawDataTOFPlot1D : public API::IPreview {
-public:
-  std::string name() const override { return "Plot1DSpectrum | RawData"; }
-  std::string facility() const override { return "ILL"; }
-  std::string technique() const override { return "SPEC"; }
-  std::string acquisition() const override { return "TOF"; }
-  std::string geometry() const override { return "2D"; }
-  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::PLOT1D; }
-};
-
-class SpecRawDataTOFPlot2D : public API::IPreview {
-public:
-  std::string name() const override { return "Plot2D | RawData"; }
-  std::string facility() const override { return "ILL"; }
-  std::string technique() const override { return "SPEC"; }
-  std::string acquisition() const override { return "TOF"; }
-  std::string geometry() const override { return "2D"; }
-  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::PLOT2D; }
-};
-
-class SpecRawDataTOFInstrumentView : public API::IPreview {
-public:
-  std::string name() const override { return "IView | RawData"; }
-  std::string facility() const override { return "ILL"; }
-  std::string technique() const override { return "SPEC"; }
-  std::string acquisition() const override { return "TOF"; }
-  std::string geometry() const override { return "2D"; }
-  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::IVIEW; }
-};
-
-class SpecRawDataTOFSliceViewer : public API::IPreview {
-public:
-  std::string name() const override { return "SliceViewer | RawData"; }
-  std::string facility() const override { return "ILL"; }
-  std::string technique() const override { return "SPEC"; }
-  std::string acquisition() const override { return "TOF"; }
-  std::string geometry() const override { return "2D"; }
-  API::IPreview::PreviewType type() const override { return API::IPreview::PreviewType::SVIEW; }
-};
-
 // TODO SCAN
 
 // Previews actual declaration
+DECLARE_PREVIEW(SpecCountDiffractionCurve)
+DECLARE_PREVIEW(SpecTOFCountDiffractionCurve)
+DECLARE_PREVIEW(SpecScanCountDiffractionCurveScan)
+DECLARE_PREVIEW(SpecTOFCountPowderGrouping)
+DECLARE_PREVIEW(SpecAllRawDataInstrumentView)
+
 DECLARE_PREVIEW(SANSMonoRawDataInstrumentView)
 DECLARE_PREVIEW(SANSTOFRawDataInstrumentView)
 DECLARE_PREVIEW(SANSTOFRawDataSliceViewer)
@@ -222,10 +227,5 @@ DECLARE_PREVIEW(REFLRawDataSliceViewer)
 DECLARE_PREVIEW(REFLRawDataPlot1D)
 DECLARE_PREVIEW(REFLRawDataPlot2D)
 
-DECLARE_PREVIEW(SpecRawDataMonoIview)
-DECLARE_PREVIEW(SpecRawDataTOFInstrumentView)
-DECLARE_PREVIEW(SpecRawDataTOFSliceViewer)
-DECLARE_PREVIEW(SpecRawDataTOFPlot1D)
-DECLARE_PREVIEW(SpecRawDataTOFPlot2D)
 } // namespace DataHandling
 } // namespace Mantid
