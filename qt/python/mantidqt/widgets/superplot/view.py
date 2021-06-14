756df205eb9c43998510f49cdbbe588c5c5eaa1d
Guillaume Communie <communieg@ill.eu>
2021-06-14 10:08:01 +0200

    [SPLOT] merge view related classes

--- qt/python/mantidqt/widgets/superplot/view.py
+++ qt/python/mantidqt/widgets/superplot/view.py
@@ -6,15 +6,128 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 
 
-from .WorkspaceItem import WorkspaceItem
-from .SpectrumItem import SpectrumItem
-
-from qtpy.QtWidgets import QDockWidget, QWidget, QHeaderView
+from qtpy.QtWidgets import QDockWidget, QWidget, QHeaderView, QTreeWidgetItem, \
+                           QToolButton
 from qtpy.QtGui import QColor
 from qtpy.QtCore import *
 from qtpy import uic
 
 import os
+
+
+class WorkspaceItemSignals(QObject):
+
+    """
+    Thrown when the delete button is pressed.
+    """
+    sig_del_clicked = Signal(str)
+
+    def __init__(self):
+        super().__init__()
+
+
+class WorkspaceItem(QTreeWidgetItem):
+
+    """
+    Name of the workspace represented by this item.
+    """
+    _workspace_name = None
+
+    """
+    Delete button.
+    """
+    _del_button = None
+
+    """
+    WorkspaceItem signals.
+    """
+    signals = None
+
+    def __init__(self, treeWidget, name):
+        super().__init__(treeWidget)
+        self._workspace_name = name
+        self._del_button = QToolButton()
+        self._del_button.setMinimumSize(20, 20)
+        self._del_button.setMaximumSize(20, 20)
+        self._del_button.setText("-")
+        self._del_button.setToolTip("Remove the workspace from the list")
+        self.signals = WorkspaceItemSignals()
+        self._del_button.clicked.connect(
+                lambda c : self.signals.sig_del_clicked.emit(
+                    self._workspace_name))
+        self.treeWidget().setItemWidget(self, 1, self._del_button)
+        self.treeWidget().resizeColumnToContents(1)
+
+    def get_workspace_name(self):
+        """
+        Get the name of the workspace.
+
+        Returns:
+            str: name of the workspace
+        """
+        return self._workspace_name
+
+
+class SpectrumItemSignals(QObject):
+
+    """
+    Thrown when the delete button is pressed.
+    Args:
+        str: workspace name
+        int: spectrum index
+    """
+    sig_del_clicked = Signal(str, int)
+
+    def __init__(self):
+        super().__init__()
+
+
+class SpectrumItem(QTreeWidgetItem):
+
+    """
+    Index of the spectrum represented by this item.
+    """
+    _spectrum_index = None
+
+    """
+    Name of the corresponding workspace.
+    """
+    _workspace_name = None
+
+    """
+    Delete button.
+    """
+    _del_button = None
+
+    """
+    Spectrum item signals.
+    """
+    signals = None
+
+    def __init__(self, treeWidget, index):
+        super().__init__(treeWidget)
+        self._spectrum_index = index
+        self._workspace_name = self.parent().get_workspace_name()
+        self._del_button = QToolButton()
+        self._del_button.setMinimumSize(20, 20)
+        self._del_button.setMaximumSize(20, 20)
+        self._del_button.setText("-")
+        self._del_button.setToolTip("Remove from the list")
+        self.signals = SpectrumItemSignals()
+        self._del_button.clicked.connect(
+                 lambda c : self.signals.sig_del_clicked.emit(
+                     self._workspace_name, self._spectrum_index))
+        self.treeWidget().setItemWidget(self, 1, self._del_button)
+        self.treeWidget().resizeColumnToContents(1)
+
+    def get_spectrum_index(self):
+        """
+        Get the index of the spectrum.
+
+        Returns:
+            int: index of the spectrum
+        """
+        return self._spectrum_index
 
 
 class SuperplotViewSide(QDockWidget):
