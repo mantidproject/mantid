233cecc37bd40a166a90475cd2d8d9cdb9dd4577
Harry Hughes <5n4@ornl.gov>
2021-06-15 10:44:37 -0400

    intermediate state

--- Framework/Algorithms/test/ConvertDiffCalTest.h
+++ Framework/Algorithms/test/ConvertDiffCalTest.h
@@ -33,6 +33,122 @@ public:
     TS_ASSERT(alg.isInitialized());
   }
 
+  /* specify fake entry in offset workspace or calibration table workspace */
+  class fake_entry
+  {
+    public:
+
+    enum
+    {
+      offset, /* fake entry specifies an entry in the fake input offsets workspace */
+      masked, /* fake entry specifies masked offset workspace entry */
+      unmasked,
+      calibration, /* fake entry specifies an entry in the input calibration table */
+    };
+
+    fake_entry( int detector_id,
+                int workspace_type,
+                int mask,
+                double diffc,
+                double diffa = 0,
+                double tzero = 0 )
+      :
+      detector_id(detector_id),
+      workspace_type(workspace_type),
+      mask(mask),
+      diffc(diffc),
+      diffa(diffa),
+      tzero(tzero)
+    {}
+
+    int detector_id;
+    int workspace_type;
+    int mask;
+    double diffc;
+    double diffa;
+    double tzero;
+  };
+
+  /* contains a fake offset workspace and accompanying fake previous calibration workspace */
+  class fake_workspaces
+  {
+    public:
+
+    fake_workspaces( OffsetsWorkspace_sptr offsets, ITableWorkspace_sptr calibration_table );
+
+    OffsetsWorkspace_sptr offsets;
+
+    ITableWorkspace_sptr calibration_table; 
+  };
+
+  fake_workspaces generate_test_data( std::vector< class fake_entry > const &entries )
+  {
+    OffsetsWorkspace_sptr offsets;
+
+    ITableWorkspace_sptr calibration_table; 
+
+    return fake_workspaces( offsets, calibration_table );
+  }
+
+  void test_partial_update()
+  {
+    /*
+     * Code implementation guide:
+     * 0. Add new property: previous calibration. Add it to the documentation.
+     * 1. Copy previous calibration workspace to output. How? is there a copy constructor?
+     * 2. Iterate through input offset workspace. Check if the detector id is masked.
+     *    check if calculated offset is zero. If non-masked an non-zero, recalculate as before.
+     * 3. Update documentation
+     * */
+
+    /*
+     * test implementation guide:
+     * 2 entries in the table that are not in the offset workspace - propagate exactly
+     * 2 entries in the offset workspace that are not in the table
+     * 3 entries in the offset table that are nonzero and non-masked to update.
+     * 1 entry that is zero but not masked
+     * 1 entry that is masked but not zero
+     * */
+
+    auto instr = ComponentCreationHelper::createMinimalInstrument(V3D(0., 0., -10.), // source
+                                                                  V3D(0., 0., 0.),   // sample
+                                                                  V3D(1., 0., 0.));  // detector
+
+    Mantid::Geometry::Detector *det = 
+    new Mantid::Geometry::Detector("point-detector", 420 /*detector id*/, nullptr);
+    instr->add(det);
+    instr->markAsDetector(det);
+
+    /* create an offset workspace based on the instrument */
+    OffsetsWorkspace_sptr offsets = std::make_shared<OffsetsWorkspace>(instr);
+
+    /* Extract the detector ids */
+    std::cout << "detector ids:" << std::endl;
+    Mantid::Geometry::DetectorInfo &d_info = offsets->mutableDetectorInfo();
+    std::vector< int > const &detector_ids = d_info.detectorIDs();
+    for( auto id : detector_ids )
+    {
+      /* set them to be masked */
+      size_t internal_index = d_info.indexOf( id );
+      std::cout << id << " at " << internal_index << std::endl;
+
+      /* check if masked */
+      std::cout << "before, d_info.isMasked(" << internal_index << "): "
+                << d_info.isMasked(internal_index) << std::endl;
+      /* mask it */
+      d_info.setMasked( internal_index, true );
+      std::cout << "after, d_info.isMasked(" << internal_index << "): "
+                << d_info.isMasked(internal_index) << std::endl;
+      /* set the value to something different */
+      double value = 27;  
+      offsets->setValue( id, value );
+      std::cout << "detector id " << id << "'s value: " << offsets->getValue( id )
+                << std::endl;
+    }
+
+    TS_ASSERT( false );
+  }
+
   void test_exec() {
     // Create a fake offsets workspace
     auto instr = ComponentCreationHelper::createMinimalInstrument(V3D(0., 0., -10.), // source
