3617663f302854a051780533112a450157db3f90
Michael Walsh <walshmm@ornl.gov>
2021-06-22 12:52:15 -0400

    need these changes back, found a solution

--- Framework/Algorithms/test/CrossCorrelateTestData.h
+++ Framework/Algorithms/test/CrossCorrelateTestData.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cxxtest/TestSuite.h>
 #include <initializer_list>
 
 #include "MantidAPI/CompositeFunction.h"
@@ -124,78 +125,69 @@ std::shared_ptr<IFunction> createPeakFunction(const PeakShapeEnum shape, const P
   std::cout << peak.str() << std::endl;
   return FunctionFactory::Instance().createInitialized(peak.str());
 }
-
-std::vector<double> evaluateFunction(std::shared_ptr<IFunction> function, std::vector<double> &xValues) {
-  FunctionDomain1DVector domain(xValues);
-  FunctionValues values(domain);
-  function->function(domain, values);
-  return values.toVector();
-}
-
 } // anonymous namespace
 
-CompositeFunction_sptr createCompositeB2BExp(const PeakShapeEnum shape, const int spectrumIndex) {
-  CompositeFunction_sptr function = std::make_shared<CompositeFunction>();
-
-  // values for reference spectrum that may be changed for the others
-  double shift_in_d{0.};
-  double scale_in_d{1.};
-  double horizontal_shift{0.};
-  double height_111{100.};
-  double height_220{200.};
-  double height_311{300.};
-
-  if (spectrumIndex == 0) { // expected value = 0.
-    // reference spectrum
-  } else if (spectrumIndex == 1) { // expected value = .1, or about 10 bins
-    // additive shift in d-spacing from the reference
-    // peak heights are unchanged
-    shift_in_d = .1;
-  } else if (spectrumIndex == 2) { // expected value = ???
-                                   // multiplicative shift in d-spacing from the reference
-                                   // peak heights are unchanged
-    scale_in_d = 1.1;
-  } else if (spectrumIndex == 3) { // expected value = 0.
-    // shifted vertically by  a constant
-    horizontal_shift = 40.;
-  } else if (spectrumIndex == 4) { // expected value = 0.
-    // scaled version of the reference - done via peak heights
-    height_111 *= 2;
-    height_220 *= 2;
-    height_311 *= 2;
-  } else {
-    throw std::runtime_error("Logic for this spectrum index has not been written");
-  }
-
-  function->addFunction(
-      createPeakFunction(shape, PeakIndex::POS_111, height_111, scale_in_d * B2BEXP_POSITION_111 + shift_in_d));
-  function->addFunction(
-      createPeakFunction(shape, PeakIndex::POS_220, height_220, scale_in_d * B2BEXP_POSITION_220 + shift_in_d));
-  function->addFunction(
-      createPeakFunction(shape, PeakIndex::POS_311, height_311, scale_in_d * B2BEXP_POSITION_311 + shift_in_d));
-  if (horizontal_shift > 0.) {
-    std::stringstream background;
-    background << "name=FlatBackground,A0=" << horizontal_shift;
-    function->addFunction(FunctionFactory::Instance().createInitialized(background.str()));
-  }
-
-  return function;
-}
-
-// THIS CODE IS COMPLETELY UNNECESSARY, BUT MAKE THE FUNCTION FACTORY WORK
-// Algorithm to force Gaussian1D to be run by simplex algorithm
-class SimplexGaussian : public Gaussian {
+class CrossCorrelateTestData : public CxxTest::TestSuite {
 public:
-  ~SimplexGaussian() override {}
-  std::string name() const override { return "Gaussian"; }
+  static CrossCorrelateTestData *createSuite() { return new CrossCorrelateTestData(); }
+  static void destroySuite(CrossCorrelateTestData *suite) { delete suite; }
 
-protected:
-  void functionDerivMW(Jacobian *out, const double *xValues, const size_t nData) {
-    UNUSED_ARG(out);
-    UNUSED_ARG(xValues);
-    UNUSED_ARG(nData);
-    throw Exception::NotImplementedError("No derivative function provided");
+  static CompositeFunction_sptr createCompositeB2BExp(const PeakShapeEnum shape, const int spectrumIndex) {
+    CompositeFunction_sptr function = std::make_shared<CompositeFunction>();
+
+    // values for reference spectrum that may be changed for the others
+    double shift_in_d{0.};
+    double scale_in_d{1.};
+    double horizontal_shift{0.};
+    double height_111{100.};
+    double height_220{200.};
+    double height_311{300.};
+
+    if (spectrumIndex == 0) { // expected value = 0.
+      // reference spectrum
+    } else if (spectrumIndex == 1) { // expected value = .1, or about 10 bins
+      // additive shift in d-spacing from the reference
+      // peak heights are unchanged
+      shift_in_d = .1;
+    } else if (spectrumIndex == 2) { // expected value = ???
+                                     // multiplicative shift in d-spacing from the reference
+                                     // peak heights are unchanged
+      scale_in_d = 1.1;
+    } else if (spectrumIndex == 3) { // expected value = 0.
+      // shifted vertically by  a constant
+      horizontal_shift = 40.;
+    } else if (spectrumIndex == 4) { // expected value = 0.
+      // scaled version of the reference - done via peak heights
+      height_111 *= 2;
+      height_220 *= 2;
+      height_311 *= 2;
+    } else {
+      throw std::runtime_error("Logic for this spectrum index has not been written");
+    }
+
+    function->addFunction(
+        createPeakFunction(shape, PeakIndex::POS_111, height_111, scale_in_d * B2BEXP_POSITION_111 + shift_in_d));
+    function->addFunction(
+        createPeakFunction(shape, PeakIndex::POS_220, height_220, scale_in_d * B2BEXP_POSITION_220 + shift_in_d));
+    function->addFunction(
+        createPeakFunction(shape, PeakIndex::POS_311, height_311, scale_in_d * B2BEXP_POSITION_311 + shift_in_d));
+    if (horizontal_shift > 0.) {
+      std::stringstream background;
+      background << "name=FlatBackground,A0=" << horizontal_shift;
+      function->addFunction(FunctionFactory::Instance().createInitialized(background.str()));
+    }
+
+    return function;
+  }
+  static std::vector<double> evaluateFunction(std::shared_ptr<IFunction> function, std::vector<double> &xValues) {
+    FunctionDomain1DVector domain(xValues);
+    FunctionValues values(domain);
+    function->function(domain, values);
+    return values.toVector();
+  }
+
+  void testDataGenerator() {
+    const auto result = createCompositeB2BExp(PeakShapeEnum::B2BEXP, 0);
+    TS_ASSERT(result != nullptr);
   }
 };
-
-DECLARE_FUNCTION(SimplexGaussian)
