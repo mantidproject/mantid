c3f445791b06c8c1e6723fd634405dd40732b0ae
Wenduo Zhou <zhouw@ornl.gov>
2021-06-23 15:33:27 -0400

    Refs #105.  Suppress cppcheck warning.

--- Framework/MDAlgorithms/src/CompareMDWorkspaces.cpp
+++ Framework/MDAlgorithms/src/CompareMDWorkspaces.cpp
@@ -293,10 +293,11 @@ void CompareMDWorkspaces::compareMDEventWorkspaces(typename MDEventWorkspace<MDE
   bool boxes_same(true);
   std::string errormessage("");
   int num_boxes = static_cast<int>(boxes1.size());
-
   bool condition_holds(false);
+  // !ws->isFileBacked()
+
   // cppcheck-suppress syntaxError
-  PRAGMA_OMP(parallel for schedule(dynamic, 1) if (condition_holds))
+  PRAGMA_OMP( parallel for if (condition_holds))
   for (int ibox = 0; ibox < num_boxes; ibox++) {
     PARALLEL_START_INTERUPT_REGION
     // No need to compare because the boxes are not same already
