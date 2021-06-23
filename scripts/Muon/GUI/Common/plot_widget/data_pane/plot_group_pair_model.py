f3f1f600af348caee34cec793d2a757ef1b8e4e4
Anthony Lim <anthony.lim@stfc.ac.uk>
2021-06-23 13:50:47 +0100

    refs #31495 added new test files for muon plots

--- scripts/Muon/GUI/Common/plot_widget/data_pane/plot_group_pair_model.py
+++ scripts/Muon/GUI/Common/plot_widget/data_pane/plot_group_pair_model.py
@@ -14,47 +14,6 @@ class PlotGroupPairModel(BasePaneModel):
     def __init__(self, context, name):
         super().__init__(context, name)
 
-    def get_workspace_and_indices_for_group_or_pair(self, group_or_pair, is_raw, plot_type):
-        """
-         :return: a list of workspace names and corresponding indices to plot
-         """
-        workspace_list = self.get_time_workspaces_to_plot(group_or_pair, is_raw, plot_type)
-        indices = self._generate_run_indices(workspace_list)
-
-        return workspace_list, indices
-
-    def get_workspaces_to_plot(self, is_raw, plot_type):
-        """
-        :param is_raw: Whether to use raw or rebinned data
-        :param plot_type: plotting type, e.g Counts, Frequency Re
-        :return: a list of workspace names
-        """
-        currently_selected = self.context.group_pair_context.selected_groups_and_pairs
-        workspace_list = []
-        for group_pair in currently_selected:
-            workspace_list += self.get_time_workspaces_to_plot(group_pair, is_raw, plot_type)
-        return workspace_list
-
-    def get_workspace_list_and_indices_to_plot(self, is_raw, plot_type):
-        """
-         :return: a list of workspace names to plot
-         """
-        workspace_list = self.get_workspaces_to_plot(is_raw, plot_type)
-        indices = self._generate_run_indices(workspace_list)
-
-        return workspace_list, indices
-
-    def get_workspaces_to_remove(self, group_pair_names, is_raw, plot_type):
-        """
-        :param is_raw: Whether to use raw or rebinned data
-        :param plot_type: plotting type, e.g Counts, Frequency Re
-        :return: a list of workspace names
-        """
-        workspace_list = []
-        for group_pair in group_pair_names:
-            workspace_list += self.get_time_workspaces_to_plot(group_pair, is_raw, plot_type)
-        return workspace_list
-
     def create_tiled_keys(self, tiled_by):
         if tiled_by == "Group/Pair":
             keys = self.context.group_pair_context.selected_groups_and_pairs
