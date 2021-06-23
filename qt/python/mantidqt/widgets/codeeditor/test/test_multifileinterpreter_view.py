2a7b2289724e83de5b418f2a4730f8aa710826a6
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-06-23 15:27:27 +0100

    Fix test_multifileinterpreter_view widget lifetime issues.

--- qt/python/mantidqt/widgets/codeeditor/test/test_multifileinterpreter_view.py
+++ qt/python/mantidqt/widgets/codeeditor/test/test_multifileinterpreter_view.py
@@ -10,6 +10,7 @@
 import unittest
 from unittest import mock
 
+from PyQt5 import sip  # not available in QtPy
 from qtpy.QtCore import Qt
 from qtpy.QtTest import QTest
 from qtpy.QtWidgets import QApplication, QTabBar
@@ -21,44 +22,49 @@ from mantidqt.widgets.codeeditor.multifileinterpreter import MultiPythonFileInte
 
 @start_qapplication
 class MultiPythonFileInterpreterViewTest(unittest.TestCase, QtWidgetFinder):
-    def test_editor_widget_not_leaked(self):
-        widget = MultiPythonFileInterpreter()
-        self.assertEqual(1, widget.editor_count)
-        widget.append_new_editor()
-        self.assertEqual(2, widget.editor_count)
-        widget.append_new_editor()
-        self.assertEqual(3, widget.editor_count)
+    def setUp(self):
+        self.widget = MultiPythonFileInterpreter()
 
-        widget.close_tab(0)
+    def tearDown(self):
+        if not sip.isdeleted(self.widget):
+            sip.delete(self.widget)
+
+    def test_editor_widget_not_leaked(self):
+        self.assertEqual(1, self.widget.editor_count)
+        self.widget.append_new_editor()
+        self.assertEqual(2, self.widget.editor_count)
+        self.widget.append_new_editor()
+        self.assertEqual(3, self.widget.editor_count)
+
+        self.widget.close_tab(0)
 
         QApplication.sendPostedEvents()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 2)
-        widget.close_tab(0)
+        self.widget.close_tab(0)
 
         QApplication.sendPostedEvents()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 1)
 
-        widget.close_all()
+        self.widget.close_all()
 
         QApplication.sendPostedEvents()
         # there should be zero interpreters
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 0)
 
         # close the whole widget, this should delete everything from the QApplication
-        widget.close()
+        self.widget.close()
         QApplication.sendPostedEvents()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 0)
         self.assert_no_toplevel_widgets()
 
     def test_editor_widget_deletes_find_replace_dialog(self):
-        widget = MultiPythonFileInterpreter()
-        self.assertEqual(1, widget.editor_count)
-        widget.append_new_editor()
+        self.assertEqual(1, self.widget.editor_count)
+        self.widget.append_new_editor()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 2)
-        widget.current_editor().show_find_replace_dialog()
+        self.widget.current_editor().show_find_replace_dialog()
         self.assert_number_of_widgets_matching("Embedded", 1)
 
-        widget.close_tab(1)
+        self.widget.close_tab(1)
 
         QApplication.sendPostedEvents()
         # there will always be 1, because we never allow an empty editor widget
@@ -66,19 +72,18 @@ class MultiPythonFileInterpreterViewTest(unittest.TestCase, QtWidgetFinder):
         self.assert_number_of_widgets_matching("Embedded", 0)
 
         # close the whole widget, this should delete everything from the QApplication
-        widget.close()
+        self.widget.close()
         QApplication.sendPostedEvents()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 0)
         self.assert_no_toplevel_widgets()
 
     def test_editor_widget_doesnt_create_find_replace_unless_requested(self):
-        widget = MultiPythonFileInterpreter()
-        self.assertEqual(1, widget.editor_count)
-        widget.append_new_editor()
+        self.assertEqual(1, self.widget.editor_count)
+        self.widget.append_new_editor()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 2)
         self.assert_number_of_widgets_matching("Embedded", 0)
 
-        widget.close_tab(1)
+        self.widget.close_tab(1)
 
         QApplication.sendPostedEvents()
         # there will always be 1, because we never allow an empty editor widget
@@ -86,33 +91,32 @@ class MultiPythonFileInterpreterViewTest(unittest.TestCase, QtWidgetFinder):
         self.assert_number_of_widgets_matching("Embedded", 0)
 
         # close the whole widget, this should delete everything from the QApplication
-        widget.close()
+        self.widget.close()
         QApplication.sendPostedEvents()
         self.assert_number_of_widgets_matching(".interpreter.PythonFileInterpreter", 0)
         self.assert_no_toplevel_widgets()
 
     def test_close_tab_button_closes_the_correct_tab(self):
-        widget = MultiPythonFileInterpreter()
-        widget.close_tab = mock.MagicMock()
+        self.widget.close_tab = mock.MagicMock()
 
-        self.assertEqual(1, widget.editor_count)
-        close_tab_button1 = widget._tabs.tabBar().tabButton(0, QTabBar.RightSide)
+        self.assertEqual(1, self.widget.editor_count)
+        close_tab_button1 = self.widget._tabs.tabBar().tabButton(0, QTabBar.RightSide)
 
-        widget.append_new_editor()
-        self.assertEqual(2, widget.editor_count)
-        close_tab_button2 = widget._tabs.tabBar().tabButton(1, QTabBar.RightSide)
+        self.widget.append_new_editor()
+        self.assertEqual(2, self.widget.editor_count)
+        close_tab_button2 = self.widget._tabs.tabBar().tabButton(1, QTabBar.RightSide)
 
         QTest.mouseClick(close_tab_button2, Qt.LeftButton)
         QApplication.sendPostedEvents()
 
-        widget.close_tab.assert_any_call(1)
+        self.widget.close_tab.assert_any_call(1)
         # Assert that the first tab has not been closed on call to tab index 1
-        self.assertFalse(mock.call(0) in widget.close_tab)
+        self.assertFalse(mock.call(0) in self.widget.close_tab)
 
         QTest.mouseClick(close_tab_button1, Qt.LeftButton)
         QApplication.sendPostedEvents()
 
-        widget.close_tab.assert_any_call(0)
+        self.widget.close_tab.assert_any_call(0)
 
 
 if __name__ == '__main__':
