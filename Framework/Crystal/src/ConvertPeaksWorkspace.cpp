ed7cfe05e405a16af2ede6174fecc46b0f4432ca
Chen <zhangc@ornl.gov>
2021-06-24 15:57:29 -0400

    alg structure 1st draft

--- Framework/Crystal/src/ConvertPeaksWorkspace.cpp
+++ Framework/Crystal/src/ConvertPeaksWorkspace.cpp
@@ -6,7 +6,74 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 
 #include "MantidCrystal/ConvertPeaksWorkspace.h"
+#include "MantidDataObjects/LeanElasticPeak.h"
+#include "MantidDataObjects/LeanElasticPeaksWorkspace.h"
+#include "MantidDataObjects/PeaksWorkspace.h"
+#include "MantidKernel/Logger.h"
 
 namespace Mantid {
-namespace Crystal {} // namespace Crystal
+namespace Crystal {
+
+using namespace Mantid::API;
+using namespace Mantid::DataObjects;
+using namespace Mantid::Geometry;
+using namespace Mantid::Kernel;
+
+/// Config logger
+namespace {
+Logger logger("ConvertPeaksWorkspace");
+}
+
+DECLARE_ALGORITHM(ConvertPeaksWorkspace)
+
+/**
+ * @brief initialization of alg
+ *
+ */
+void ConvertPeaksWorkspace::init() {
+  // Input peakworkspace
+  declareProperty(std::make_unique<WorkspaceProperty<IPeaksWorkspace>>("PeakWorkspace", "", Kernel::Direction::Input),
+                  "Workspace of Indexed Peaks");
+
+  // donor workspace if going from lean to regular
+  declareProperty(std::make_unique<WorkspaceProperty<>>("InstrumentWorkpace", "", Kernel::Direction::Input),
+                  "Workspace of Indexed Peaks");
+
+  // output
+  declareProperty(std::make_unique<WorkspaceProperty<IPeaksWorkspace>>("OutputWorkspace", "", Direction::Output),
+                  "The workspace containing the calibration table.");
+}
+
+/**
+ * @brief Inputs validation
+ *
+ * @return std::map<std::string, std::string>
+ */
+std::map<std::string, std::string> ConvertPeaksWorkspace::validateInputs() {}
+
+/**
+ * @brief Algorithm entrance func
+ *
+ */
+void ConvertPeaksWorkspace::exec() {}
+
+/**
+ * @brief make a LeanElasticPeaksWorkspace using peaks from a regular PeaksWorkspace
+ *
+ * @param pws
+ * @return IPeaksWorkspace_sptr
+ */
+IPeaksWorkspace_sptr ConvertPeaksWorkspace::makeLeanElasticPeaksWorkspace(IPeaksWorkspace_sptr pws) {}
+
+/**
+ * @brief Build a regular PeaksWorkspace using peaks from a LeanElasticPeaksWorkspace and the provided instrument
+ *
+ * @param lpws
+ * @param instrument
+ * @return IPeaksWorkspace_sptr
+ */
+IPeaksWorkspace_sptr ConvertPeaksWorkspace::makeLeanElasticPeaksWorkspace(IPeaksWorkspace_sptr lpws,
+                                                                          std::shared_ptr<Instrument> &instrument) {}
+
+} // namespace Crystal
 } // namespace Mantid
--- Framework/Crystal/test/ConvertPeaksWorkspaceTest.h
+++ Framework/Crystal/test/ConvertPeaksWorkspaceTest.h
@@ -6,11 +6,25 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
+#include "MantidAPI/Run.h"
+#include "MantidCrystal/ConvertPeaksWorkspace.h"
+#include "MantidDataObjects/LeanElasticPeak.h"
+#include "MantidDataObjects/LeanElasticPeaksWorkspace.h"
+#include "MantidDataObjects/PeaksWorkspace.h"
+#include "MantidGeometry/Instrument/Goniometer.h"
+#include "MantidTestHelpers/ComponentCreationHelper.h"
 #include <cxxtest/TestSuite.h>
 
-#include "MantidCrystal/ConvertPeaksWorkspace.h"
-
+using namespace Mantid::DataObjects;
+using namespace Mantid::API;
+using namespace Mantid::Geometry;
+using namespace Mantid::Kernel;
 using Mantid::Crystal::ConvertPeaksWorkspace;
+
+namespace {
+/// static logger
+Logger g_log("ConvertPeaksWorkspaceTest");
+} // namespace
 
 class ConvertPeaksWorkspaceTest : public CxxTest::TestSuite {
 public:
@@ -19,5 +33,55 @@ public:
   static ConvertPeaksWorkspaceTest *createSuite() { return new ConvertPeaksWorkspaceTest(); }
   static void destroySuite(ConvertPeaksWorkspaceTest *suite) { delete suite; }
 
-  void test_Something() { TS_FAIL("You forgot to write a test!"); }
+  void test_Name() {
+    ConvertPeaksWorkspace alg;
+    TS_ASSERT_EQUALS(alg.name(), "ConvertPeaksWorkspace");
+  }
+
+  void test_Init() {
+    ConvertPeaksWorkspace alg;
+    alg.initialize();
+    TS_ASSERT(alg.isInitialized());
+  }
+
+  void test_PeaksWorkspace_to_LeanElasticPeaksWorkspace() {
+    PeaksWorkspace_sptr pws = make_pws();
+    LeanElasticPeaksWorkspace_sptr lpws = make_lpws();
+    g_log.notice() << lpws->getPeak(0).getQSampleFrame() << "\n" << lpws->getPeak(0).getWavelength() << "\n";
+
+    // TODO
+    // add calling to the convertor
+  }
+
+  void test_LeanElasticPeaksWorkspace_to_PeaksWorkspace() {
+    PeaksWorkspace_sptr pws = make_pws();
+    LeanElasticPeaksWorkspace_sptr lpws = make_lpws();
+
+    // TODO
+    // add calling to the convertor
+  }
+
+private:
+  // ------------------- //
+  // ----- members ----- //
+  // ------------------- //
+  PeaksWorkspace_sptr make_pws() {
+    Instrument_sptr inst = ComponentCreationHelper::createTestInstrumentRectangular2(2, 10);
+    inst->setName("TestInstrument");
+
+    auto pw = std::make_shared<PeaksWorkspace>();
+    pw->setInstrument(inst);
+    std::string val = "test";
+    pw->mutableRun().addProperty("TestProp", val);
+    Peak pk(inst, 1, 1.0);
+    pw->addPeak(pk);
+    return pw;
+  }
+
+  LeanElasticPeaksWorkspace_sptr make_lpws() {
+    auto lpw = std::make_shared<LeanElasticPeaksWorkspace>();
+    LeanElasticPeak lpk(V3D(-6.27496, 0.200799, 6.03219), 1.0);
+    lpw->addPeak(lpk);
+    return lpw;
+  }
 };
