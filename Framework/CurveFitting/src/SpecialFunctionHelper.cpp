188bbb83c42f199d7b330b25c8e85b4d7e1967f5
Yuanpeng Zhang <zyroc1990@gmail.com>
2021-06-24 12:27:12 -0400

    solve the still existing issue with exponential integral and add in more tests

--- Framework/CurveFitting/src/SpecialFunctionHelper.cpp
+++ Framework/CurveFitting/src/SpecialFunctionHelper.cpp
@@ -14,45 +14,64 @@ namespace SpecialFunctionSupport {
 
 using std::complex;
 
-/** Implement Exponential Integral function, E1(z), based on formulaes in
- *Abramowitz and Stegun (A&S)
- *  In fact this implementation returns exp(z)*E1(z) where z is a complex number
+/** Implementation of complex integral E_1
  *
- *  @param z :: input
- *  @return exp(z)*E1(z)
+ *  The algorithm originates from the following reference,
+ *
+ *  Shanjie Zhang, Jianming Jin,
+ *  Computation of Special Functions,
+ *  Wiley, 1996,
+ *  ISBN: 0-471-11963-6,
+ *  LC: QA351.C45.
+ *
+ *  Also, refre to the following link for its implementation in Fortran,
+ *
+ *  https://people.sc.fsu.edu/~jburkardt/f_src/special_functions/special_functions.f90
+ *
  */
 complex<double> exponentialIntegral(const complex<double> &z) {
-  double z_abs = abs(z);
+  complex<double> e1;
 
-  if (z_abs == 0.0) {
-    // Basically function is infinite in along the real axis for this case
-    return complex<double>(std::numeric_limits<double>::max(), 0.0);
-  } else if (z_abs < 10.0) // 10.0 is a guess based on formula 5.1.55 (no idea
-                           // how good it really is)
-  {
-    // use formula 5.1.11 in A&S. rewrite last term in 5.1.11 as
-    // x*sum_n=0 (-1)^n x^n / (n+1)*(n+1)! and then calculate the
-    // terms in the sum recursively
-    complex<double> z1(1.0, 0.0);
-    complex<double> z2(1.0, 0.0);
-    for (int i = 1; i <= 100; i++) // is max of 100 here best?
-    {
-      z2 = -static_cast<double>(i) * (z2 * z) / ((i + 1.0) * (i + 1.0));
-      z1 += z2;
-      if (abs(z2) < 1.0E-10 * abs(z1))
-        break; // i.e. if break loop if little change to term added
-    }
-    return exp(z) * (-log(z) - M_EULER + z * z1);
+  double rz = real(z);
+  double az = abs(z);
+  double eL = 0.5772156649015328;
+
+  if (fabs(az) < 1.0E-8) {
+    // If z = 0, then the result is infinity... diverge!
+    complex<double> r(1.0E300, 0.0);
+    e1 = r;
+  } else if (az <= 10.0 || (rz < 0.0 && az < 20.0)) {
+    // Some interesting region, equal to integrate to infinity, converged
+    complex<double> r(1.0, 0.0);
+    e1 = r;
+    complex<double> cr = r;
+
+    for (size_t k = 1; k <= 150; ++k) {
+      auto dk = double(k);
+      cr = -cr * dk * z / ((dk + 1.0) * (dk + 1.0));
+      e1 += cr;
+      if (abs(cr) < abs(e1) * 1.0E-15) {
+        // cr is converged to zero
+        break;
+      }
+    } // ENDFOR k
+
+    e1 = -eL - log(z) + (z * e1);
   } else {
-    // use formula 5.1.22 in A&S. See discussion page 231
-    complex<double> z1(0.0, 0.0);
-    for (int i = 20; i >= 1; i--) // not sure if 20 is always sensible?
-      z1 = static_cast<double>(i) / (1.0 + static_cast<double>(i) / (z + z1));
-    complex<double> retVal = 1.0 / (z + z1);
-    if (z.real() <= 0.0 && z.imag() == 0.0)
-      retVal -= exp(z) * complex<double>(0.0, 1.0) * M_PI; // see formula 5.1.7
-    return retVal;
+    complex<double> ct0(0.0, 0.0);
+    for (int k = 120; k > 0; --k) {
+      complex<double> dk(double(k), 0.0);
+      ct0 = dk / (1.0 + dk / (z + ct0));
+    } // ENDFOR k
+
+    e1 = 1.0 / (z + ct0);
+    e1 = e1 * exp(-z);
+    if (rz < 0.0 && fabs(imag(z)) < 1.0E-10) {
+      complex<double> u(0.0, 1.0);
+      e1 = e1 - (M_PI * u);
+    }
   }
+  return e1;
 }
 
 } // End namespace SpecialFunctionSupport
