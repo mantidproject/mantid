1466690a6e04e9b1fedbe0d3721de313e8807ad9
Danny Hindson <danny.hindson@stfc.ac.uk>
2021-06-25 17:21:52 +0100

    Widen tolerance

--- Framework/Kernel/src/ANN/pr_queue_k.h
+++ Framework/Kernel/src/ANN/pr_queue_k.h
@@ -117,18 +117,18 @@ public:
       // This avoids any differences in the sort order across platforms due to different
       // floating point handling of double
       Mantid::Kernel::Logger g_log("pr_queue_k.h");
-      if ((inf == 19140) || (inf == 19142)) {
+      if ((inf == 19140) || (inf == 19142) || (inf == 19398) || (info == 19396)) {
         g_log.error() << "kv=" << setprecision(17) << kv << std::endl;
         g_log.error() << "inf=" << inf << std::endl;
         g_log.error() << setprecision(17) << std::abs(mk[i - 1].key - kv) << std::endl;
-        g_log.error() << setprecision(17) << 5 * std::numeric_limits<double>::epsilon() * (mk[i - 1].key + kv)
+        g_log.error() << setprecision(17) << 10 * std::numeric_limits<double>::epsilon() * (mk[i - 1].key + kv)
                       << std::endl;
         bool testvar =
-            (std::abs(mk[i - 1].key - kv) < (5 * std::numeric_limits<double>::epsilon() * (mk[i - 1].key + kv)));
+            (std::abs(mk[i - 1].key - kv) < (10 * std::numeric_limits<double>::epsilon() * (mk[i - 1].key + kv)));
         g_log.error() << "testvar=" << testvar << std::endl;
         g_log.error() << std::endl;
       }
-      if (std::abs(mk[i - 1].key - kv) < (10 * std::numeric_limits<double>::epsilon() * 0.5 * (mk[i - 1].key + kv))) {
+      if (std::abs(mk[i - 1].key - kv) < (10 * std::numeric_limits<double>::epsilon() * (mk[i - 1].key + kv))) {
         if (mk[i - 1].info > inf)
           mk[i] = mk[i - 1];
         else
