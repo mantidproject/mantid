d230af818d8bed5b0498413c4a62dda00e3777db
Gagik Vardanyan <vardanyan@ill.fr>
2021-07-01 10:54:31 +0200

    prepared unit tests

--- Framework/Algorithms/test/StitchTest.h
+++ Framework/Algorithms/test/StitchTest.h
@@ -8,9 +8,14 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/WorkspaceFactory.h"
 #include "MantidAlgorithms/Stitch.h"
+#include "MantidHistogramData/Histogram.h"
 
 using Mantid::Algorithms::Stitch;
+using namespace Mantid::API;
+using namespace Mantid::HistogramData;
 
 class StitchTest : public CxxTest::TestSuite {
 public:
@@ -23,5 +28,48 @@ public:
     Stitch alg;
     TS_ASSERT_THROWS_NOTHING(alg.initialize())
     TS_ASSERT(alg.isInitialized())
+  }
+
+  void test_NoOverlap() {}
+
+  void test_OneWorkspace() {}
+
+  void test_WorkspaceGroup() {}
+
+  void test_WorkspacesAndGroupsMixed() {}
+
+  void test_IncompatibleWorkspaces() {}
+
+  void test_NotEnoughOverlap() {}
+
+  void test_NoExplicitReference() {}
+
+  void test_ExplicitReference() {}
+
+  void test_PointData() {}
+
+  void test_HistogramData() {}
+
+  void test_MultipleSpectra() {}
+
+  void test_LeftToRight() {}
+
+  void test_RightToLeft() {}
+
+  void test_CustomOrder() {}
+
+private:
+  MatrixWorkspace_sptr sampleWorkspaceOneSpec(size_t nBins, double startX, double endX) {
+    MatrixWorkspace_sptr ws = WorkspaceFactory::Instance().create("Workspace2D", 1, nBins, nBins);
+    std::vector<double> x(nBins + 1), y(nBins), e(nBins);
+    const double step = (startX - endX) / nBins;
+    for (size_t ibin = 0; ibin < nBins; ++ibin) {
+      x[ibin] = startX + ibin * step;
+      y[ibin] = 7 * ibin + 3;
+      e[ibin] = std::sqrt(y[ibin]);
+    }
+    x[nBins] = endX;
+    ws->setHistogram(0, Histogram(x, y, e));
+    return ws;
   }
 };
