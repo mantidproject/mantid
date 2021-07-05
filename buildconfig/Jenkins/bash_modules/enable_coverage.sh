950d37edf7ed8c1d7bfd89427dfe89881b3e3638
David Fairbrother <DavidFair@users.noreply.github.com>
2021-07-05 12:47:04 +0100

    Add override for timeout when coverage is enabled
    Adds an override, bumping the max test time to 1200 seconds in Debug
    mode, to allow MD* tests to complete in time

--- buildconfig/Jenkins/bash_modules/enable_coverage.sh
+++ buildconfig/Jenkins/bash_modules/enable_coverage.sh
@@ -5,12 +5,15 @@ function enable_coverage(){
 
     if [[ ${JOB_NAME} == *coverage* ]]; then
         local COVERAGE=ON
+        # Since we currently don't support passing a timeout, hardcode it for coverage jobs
+        local TIMEOUT=1200  # Seconds, 4x normal 300 second job
     else
         local COVERAGE=OFF
+        local TIMEOUT=300
     fi
 
 
-    local cmake_string="$1 -DCOVERAGE=${COVERAGE}"
+    local cmake_string="$1 -DCOVERAGE=${COVERAGE} -DTESTING_TIMEOUT=${TIMEOUT}"
     echo "$cmake_string"
 }
 
