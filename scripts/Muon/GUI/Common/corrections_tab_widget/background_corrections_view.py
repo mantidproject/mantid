6c5f9e777dd906ce1ed389693ab3d0b54dd334e0
Robert Applin <robert.applin@stfc.ac.uk>
2021-07-06 09:16:32 +0100

    Refs #31816. Disable background corrections.

--- scripts/Muon/GUI/Common/corrections_tab_widget/background_corrections_view.py
+++ scripts/Muon/GUI/Common/corrections_tab_widget/background_corrections_view.py
@@ -52,7 +52,7 @@ class BackgroundCorrectionsView(widget, ui_form):
         self._handle_end_x_changed = None
 
         # Disable the background mode combo box while background corrections in still in development
-        # self.mode_combo_box.setEnabled(False)
+        self.mode_combo_box.setEnabled(False)
 
     def set_slot_for_mode_combo_box_changed(self, slot) -> None:
         """Connect the slot for the Background corrections mode combo box."""
