bfc33ff43ae956605bc5824f7fdbf0086ae36733
David Fairbrother <DavidFair@users.noreply.github.com>
2021-07-07 12:05:11 +0100

    Add virtual d'tor to MockAlgorithmProgress*
    These classes cannot be marked final as a Nice/Strict mock inherits from the original mock class, rather than holding it as a member.
    This means that we need to mark the mocks with a virtual d'tor as we (unintentionally) have them as part of an inheritance chain

--- qt/widgets/common/test/AlgorithmProgress/MockAlgorithmProgressDialogWidget.h
+++ qt/widgets/common/test/AlgorithmProgress/MockAlgorithmProgressDialogWidget.h
@@ -24,18 +24,6 @@ class QWidget;
 
 class MockAlgorithmProgressDialogWidget : public IAlgorithmProgressDialogWidget {
 public:
-  MockAlgorithmProgressDialogWidget() {
-    mainProgressBar = std::make_shared<MockAlgorithmProgressWidget>();
-    auto pres = mainProgressBar->m_presenter.get();
-    m_presenter =
-        std::make_shared<AlgorithmProgressDialogPresenter>(static_cast<QWidget *>(nullptr), this, pres->model());
-  }
-  virtual ~MockAlgorithmProgressDialogWidget() {}
-
-  MOCK_METHOD1(addAlgorithm, std::pair<QTreeWidgetItem *, QProgressBar *>(Mantid::API::IAlgorithm_sptr));
-
-  std::shared_ptr<AlgorithmProgressDialogPresenter> m_presenter;
-  // This is the mocked main progress bar,
-  // always shown on the Workbench GUI
-  std::shared_ptr<MockAlgorithmProgressWidget> mainProgressBar;
+  virtual ~MockAlgorithmProgressDialogWidget() = default;
+  MOCK_METHOD((std::pair<QTreeWidgetItem *, QProgressBar *>), addAlgorithm, (Mantid::API::IAlgorithm_sptr), (override));
 };
