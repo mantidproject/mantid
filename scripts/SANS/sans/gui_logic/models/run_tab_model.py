0d1c6c4c6c1923491b64ee12bab02433221aa860
David Fairbrother <DavidFair@users.noreply.github.com>
2021-07-09 13:08:07 +0100

    Add storage to model for Reduction dim
    Re #31929
    Store the current reduction dimension into the model. This does not wire
    up the model to change the default save yet

--- scripts/SANS/sans/gui_logic/models/run_tab_model.py
+++ scripts/SANS/sans/gui_logic/models/run_tab_model.py
@@ -6,14 +6,23 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from sans.gui_logic.models.POD.save_options import SaveOptions
 
+from scripts.SANS.sans.common.enums import ReductionDimensionality
+
 
 class RunTabModel:
     def __init__(self):
+        self._reduction_mode: ReductionDimensionality = ReductionDimensionality.ONE_DIM
         self._save_types = SaveOptions()
+
+    def get_save_types(self):
+        return self._save_types
 
     def update_save_types(self, selected_types: SaveOptions):
         self._save_types = selected_types
         self._save_types.user_modified = True
 
-    def get_save_types(self):
-        return self._save_types
+    def get_reduction_mode(self):
+        return self._reduction_mode
+
+    def update_reduction_mode(self, val: ReductionDimensionality):
+        self._reduction_mode = val
--- scripts/test/SANS/gui_logic/model_tests/test_run_tab_model.py
+++ scripts/test/SANS/gui_logic/model_tests/test_run_tab_model.py
@@ -6,6 +6,7 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
 
+from sans.common.enums import ReductionDimensionality
 from sans.gui_logic.models.POD.save_options import SaveOptions
 from sans.gui_logic.models.run_tab_model import RunTabModel
 
@@ -20,6 +21,11 @@ class TestRunTabModel(unittest.TestCase):
         self.model.update_save_types(default_options)
         save_types = self.model.get_save_types()
         self.assertTrue(save_types.user_modified)
+
+    def test_reduction_mode_stored(self):
+        for mode in [ReductionDimensionality.TWO_DIM, ReductionDimensionality.ONE_DIM]:
+            self.model.update_reduction_mode(mode)
+            self.assertEqual(mode, self.model.get_reduction_mode())
 
 
 if __name__ == '__main__':
