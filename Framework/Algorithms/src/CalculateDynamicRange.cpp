1de1c210cc722fb41d8b56077f79f76b315050b6
gvardany <gagikvar@gmail.com>
2021-07-16 09:46:51 +0200

    ammend calculate dynamic range to take wavelength from sample logs

--- Framework/Algorithms/src/CalculateDynamicRange.cpp
+++ Framework/Algorithms/src/CalculateDynamicRange.cpp
@@ -5,12 +5,14 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 #include "MantidAlgorithms/CalculateDynamicRange.h"
+#include "MantidAPI/Axis.h"
 #include "MantidAPI/Run.h"
 #include "MantidAPI/SpectrumInfo.h"
 #include "MantidAPI/WorkspaceUnitValidator.h"
 #include "MantidGeometry/Instrument.h"
 #include "MantidHistogramData/HistogramIterator.h"
 #include "MantidKernel/ArrayProperty.h"
+#include "MantidKernel/Unit.h"
 
 namespace {
 /**
@@ -49,14 +51,22 @@ const std::string CalculateDynamicRange::summary() const {
   return "Calculates and sets Qmin and Qmax of a SANS workspace";
 }
 
+/// Validate inputs
+std::map<std::string, std::string> CalculateDynamicRange::validateInputs() {
+  std::map<std::string, std::string> issues;
+  API::MatrixWorkspace_sptr workspace = getProperty("Workspace");
+  if (workspace->getAxis(0)->unit()->unitID() != "Wavelength" && !workspace->run().hasProperty("wavelength")) {
+    issues["InputWorkspace"] = "If the workspace is not in units of wavelength it must have a sample log wavelength.";
+  }
+  return issues;
+}
+
 //----------------------------------------------------------------------------------------------
 /** Initialize the algorithm's properties.
  */
 void CalculateDynamicRange::init() {
-  auto unitValidator = std::make_shared<WorkspaceUnitValidator>("Wavelength");
-  declareProperty(
-      std::make_unique<WorkspaceProperty<MatrixWorkspace>>("Workspace", "", Direction::InOut, unitValidator),
-      "An input workspace.");
+  declareProperty(std::make_unique<WorkspaceProperty<MatrixWorkspace>>("Workspace", "", Direction::InOut),
+                  "An input workspace.");
 
   declareProperty(std::make_unique<Mantid::Kernel::ArrayProperty<std::string>>("ComponentNames"),
                   "List of component names to calculate the q ranges for.");
@@ -71,6 +81,8 @@ void CalculateDynamicRange::init() {
 void CalculateDynamicRange::calculateQMinMax(const MatrixWorkspace_sptr &workspace, const std::vector<size_t> &indices,
                                              const std::string &compName = "") {
   const auto &spectrumInfo = workspace->spectrumInfo();
+  const auto unit = workspace->getAxis(0)->unit()->unitID();
+  const double lambda = workspace->run().getLogAsSingleValue("wavelength");
   double min = std::numeric_limits<double>::max(), max = std::numeric_limits<double>::lowest();
   // PARALLEL_FOR_NO_WSP_CHECK does not work with range-based for so NOLINT this
   // block
@@ -81,12 +93,20 @@ void CalculateDynamicRange::calculateQMinMax(const MatrixWorkspace_sptr &workspa
       const Kernel::V3D detPos = spectrumInfo.position(indices[index]);
       double r, theta, phi;
       detPos.getSpherical(r, theta, phi);
-      // Use the bin centers
-      const double v1 = calculateQ(spectrum.begin()->center(), theta);
-      const double v2 = calculateQ(std::prev(spectrum.end())->center(), theta);
-      PARALLEL_CRITICAL(CalculateDynamicRange) {
-        min = std::min(min, std::min(v1, v2));
-        max = std::max(max, std::max(v1, v2));
+      if (unit != "Wavelength") {
+        const double q = calculateQ(lambda, theta);
+        PARALLEL_CRITICAL(CalculateDynamicRange) {
+          min = std::min(min, q);
+          max = std::max(max, q);
+        }
+      } else {
+        // Use the bin centers
+        const double q1 = calculateQ(spectrum.begin()->center(), theta);
+        const double q2 = calculateQ(std::prev(spectrum.end())->center(), theta);
+        PARALLEL_CRITICAL(CalculateDynamicRange) {
+          min = std::min(min, std::min(q1, q2));
+          max = std::max(max, std::max(q1, q2));
+        }
       }
     }
   }
