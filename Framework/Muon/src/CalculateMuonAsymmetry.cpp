3aa5428cec1dc0fd50da93156f57602f375dd6bb
Robert Applin <robert.applin@stfc.ac.uk>
2021-07-16 08:13:53 +0100

    Refs #32018. Use array ordered validator.

--- Framework/Muon/src/CalculateMuonAsymmetry.cpp
+++ Framework/Muon/src/CalculateMuonAsymmetry.cpp
@@ -22,6 +22,7 @@
 #include "MantidAPI/MultiDomainFunction.h"
 #include "MantidAPI/Run.h"
 
+#include "MantidKernel/ArrayOrderedPairsValidator.h"
 #include "MantidKernel/ArrayProperty.h"
 #include "MantidKernel/BoundedValidator.h"
 #include "MantidKernel/ListValidator.h"
@@ -65,9 +66,10 @@ void CalculateMuonAsymmetry::init() {
 
   declareProperty("StartX", 0.1, "The lower limit for calculating the asymmetry (an X value).");
   declareProperty("EndX", 15.0, "The upper limit for calculating the asymmetry  (an X value).");
-  declareProperty(std::make_unique<ArrayProperty<double>>("Exclude", ""),
-                  "A list of pairs of real numbers, defining the regions to "
-                  "exclude from the fit for all spectra.");
+  declareProperty(
+      std::make_unique<ArrayProperty<double>>("Exclude", std::make_shared<ArrayOrderedPairsValidator<double>>()),
+      "A list of pairs of real numbers, defining the regions to "
+      "exclude from the fit for all spectra.");
 
   declareProperty(std::make_unique<API::FunctionProperty>("InputFunction"), "The fitting function to be converted.");
 
