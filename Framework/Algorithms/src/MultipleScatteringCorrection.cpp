1292806649e1c65edf7c2f897a2160255b47c11a
Michael Walsh <walshmm@ornl.gov>
2021-07-19 13:23:21 -0400

    copy in api stuff from paalmanpingsabsorbtion

--- Framework/Algorithms/src/MultipleScatteringCorrection.cpp
+++ Framework/Algorithms/src/MultipleScatteringCorrection.cpp
@@ -7,6 +7,60 @@
 
 #include "MantidAlgorithms/MultipleScatteringCorrection.h"
 
+#include "MantidAPI/InstrumentValidator.h"
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidAPI/Sample.h"
+#include "MantidAPI/WorkspaceGroup.h"
+#include "MantidAPI/WorkspaceUnitValidator.h"
+#include "MantidGeometry/Instrument/SampleEnvironment.h"
+#include "MantidKernel/BoundedValidator.h"
+#include "MantidKernel/CompositeValidator.h"
+
 namespace Mantid {
-namespace Algorithms {} // namespace Algorithms
+namespace Algorithms {
+
+using namespace API;
+using namespace Kernel;
+using namespace Mantid::DataObjects;
+
+void MultipleScatteringCorrection::init() {
+  // The input workspace must have an instrument and units of wavelength
+  auto wsValidator = std::make_shared<CompositeValidator>();
+  wsValidator->add<WorkspaceUnitValidator>("Wavelength");
+  wsValidator->add<InstrumentValidator>();
+
+  declareProperty(std::make_unique<WorkspaceProperty<>>("InputWorkspace", "", Direction::Input, wsValidator),
+                  "The X values for the input workspace must be in units of wavelength");
+  declareProperty(std::make_unique<WorkspaceProperty<WorkspaceGroup>>("OutputWorkspace", "", Direction::Output),
+                  "Output workspace name");
+
+  auto positiveInt = std::make_shared<BoundedValidator<int64_t>>();
+  positiveInt->setLower(1);
+  declareProperty("NumberOfWavelengthPoints", static_cast<int64_t>(EMPTY_INT()), positiveInt,
+                  "The number of wavelength points for which the numerical integral is\n"
+                  "calculated (default: all points)");
+
+  auto moreThanZero = std::make_shared<BoundedValidator<double>>();
+  moreThanZero->setLower(0.001);
+  declareProperty("ElementSize", 1.0, moreThanZero, "The size of one side of an integration element cube in mm");
+}
+
+std::map<std::string, std::string> MultipleScatteringCorrection::validateInputs() {
+  std::map<std::string, std::string> result;
+
+  // verify that the container information is there if requested
+  API::MatrixWorkspace_const_sptr wksp = getProperty("InputWorkspace");
+  const auto &sample = wksp->sample();
+  if (sample.hasEnvironment()) {
+    const auto numComponents = sample.getEnvironment().nelements();
+    // first element is assumed to be the container
+    if (numComponents == 0) {
+      result["InputWorkspace"] = "Sample does not have a container defined";
+    }
+  } else {
+    result["InputWorkspace"] = "Sample does not have a container defined";
+  }
+  return result;
+}
+} // namespace Algorithms
 } // namespace Mantid
