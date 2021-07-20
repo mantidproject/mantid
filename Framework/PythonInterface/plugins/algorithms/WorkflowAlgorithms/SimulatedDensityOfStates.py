095e319a3bc18645c5afa6cdbea2746d06dc3db0
Adam J. Jackson <a.j.jackson@physics.org>
2021-07-20 14:30:39 +0100

    SimulatedDensityOfStates: tweaks to euphonic import handling
    - Move force constants import to scripts/Indirect/dos for consistency
      with other formats
    - Add dedicated function for checking Euphonic library
      availability. (This should be useful for input safety checks; simply
      decline this type of input if the library isn't available.)
    - Remove special capture/warning logic if Scipy/Euphonic
      unavailable. Scipy is a full-blown Mantid dependency and shouldn't
      need special treatment. Euphonic availability is now being checked
      on a "soft" basis and should not block the algorithm from loading.

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SimulatedDensityOfStates.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SimulatedDensityOfStates.py
@@ -15,8 +15,10 @@ from mantid.kernel import *
 from mantid.api import *
 import mantid.simpleapi as s_api
 
-from dos.load_phonon import parse_phonon_file
 from dos.load_castep import parse_castep_file
+from dos.load_euphonic import euphonic_available, get_data_with_euphonic
+from dos.load_phonon import parse_phonon_file
+
 
 PEAK_WIDTH_ENERGY_FLAG = 'energy'
 
@@ -129,6 +131,9 @@ class SimulatedDensityOfStates(PythonAlgorithm):
 
         ions = self.getProperty('Ions').value
         calc_partial = len(ions) > 0
+
+        if euphonic_filename and not euphonic_available():
+            issues['ForceConstantsFile'] = 'Cannot import the Euphonic library for force constants import.'
 
         if spec_type == 'IonTable' and not pdos_available:
             issues['SpectrumType'] = 'Cannot produce ion table when only .castep file is provided'
@@ -258,9 +263,13 @@ class SimulatedDensityOfStates(PythonAlgorithm):
         elif castep_filename:
             return self._read_data_from_file(castep_filename)
         elif euphonic_filename:
-            return self._get_data_with_euphonic(
+            file_data, self._element_isotope = get_data_with_euphonic(
                 euphonic_filename,
                 cutoff=float(self.getPropertyValue('ForceConstantsSampling')))
+
+            self._num_ions = file_data['num_ions']
+            return file_data
+
         else:
             raise RuntimeError('No valid data file')
 
@@ -727,73 +736,6 @@ class SimulatedDensityOfStates(PythonAlgorithm):
 
         return self._compute_DOS(frequencies, x_sections, weights)
 
-    def _get_data_with_euphonic(self, file_name: str, cutoff: float = 20.):
-        """
-        Read force constants file with Euphonic and sample frequencies/modes
-
-        :param file_name: Input data
-        :param cutoff:
-            Sampling density of Brillouin-zone. Specified as real-space length
-            cutoff.
-
-        :returns: dict of structure and vibration data
-
-        """
-        from math import ceil
-        from euphonic.cli.utils import force_constants_from_file
-        from euphonic.util import mp_grid
-
-        fc = force_constants_from_file(file_name)
-        recip_lattice_lengths = np.linalg.norm(
-            fc.crystal.reciprocal_cell().to('1/angstrom').magnitude, axis=1)
-        mp_sampling = [ceil(x)
-                       for x in (cutoff * recip_lattice_lengths / (2 * np.pi))]
-        qpts = mp_grid(mp_sampling)
-        logger.notice('Calculating phonon modes on {} grid'.format(
-            'x'.join(map(str, mp_sampling))))
-        modes = fc.calculate_qpoint_phonon_modes(qpts, use_c=True, n_threads=4)
-
-        file_data = {'num_ions': len(fc.crystal.atom_type),
-                     'num_branches': modes.frequencies.magnitude.shape[1],
-                     'unit_cell': fc.crystal.cell_vectors.to('angstrom').magnitude}
-
-        file_data['ir_intensities'] = np.array([])
-        file_data['raman_intensities'] = np.array([])
-        file_data['frequencies'] = modes.frequencies.to('1/cm').magnitude.flatten()
-        eigenvectors = modes.eigenvectors.real
-        # Eigenvectors should be indexed (qpts, mode/ion combination, dim)
-        # (to match order in CASTEP .phonon files)
-        # so reshape data into this format.
-        file_data['eigenvectors'] = eigenvectors.reshape(
-                (len(qpts), file_data['num_branches'] * file_data['num_ions'], 3))
-        file_data['weights'] = (modes.weights * np.ones((file_data['num_branches'], 1))).flatten()
-
-        ions = [{'index': i,
-                 'species': element,
-                 'fract_coord': position,
-                 'isotope_number': mass}
-                for i, (element, position, mass)
-                in enumerate(zip(fc.crystal.atom_type,
-                                 fc.crystal.atom_r,
-                                 fc.crystal.atom_mass.magnitude))]
-
-        file_data['ions'] = ions
-
-        # Store some redundant information as a side-effect for compatibility
-        # with the existing code
-        self._element_isotope = {ion['species']: ion['isotope_number']
-                                 for ion in ions}
-        self._num_ions = file_data['num_ions']
-
-        # Generate index per element
-        ion_counts = {element: 0 for element in self._element_isotope}
-        for ion in ions:
-            species = ion['species']
-            ion_counts[species] += 1
-            ion['bond_number'] = ion_counts[species]
-
-        return file_data
-
     def _read_data_from_file(self, file_name):
         """
         Select the appropriate file parser and check data was successfully
@@ -826,9 +768,4 @@ class SimulatedDensityOfStates(PythonAlgorithm):
         return file_data
 
 
-try:
-    import scipy.constants
-    import euphonic  # noqa: F401
-    AlgorithmFactory.subscribe(SimulatedDensityOfStates)
-except ImportError:
-    logger.debug('Failed to subscribe algorithm SimulatedDensityOfStates; The python package scipy or euphonic may be missing.')
+AlgorithmFactory.subscribe(SimulatedDensityOfStates)
