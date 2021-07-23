b66cc6494a3f7e0eaac57e1444601326db31b922
Mathieu Tillet <tillet@ill.eu>
2021-07-23 09:14:38 +0200

    Discrete move for integration bar

--- qt/widgets/instrumentview/src/XIntegrationControl.cpp
+++ qt/widgets/instrumentview/src/XIntegrationControl.cpp
@@ -18,15 +18,17 @@
 #include <QMenu>
 #include <QMouseEvent>
 #include <QPushButton>
+#include <QSpinBox>
 
 #include <cfloat>
 #include <numeric>
 
 namespace MantidQt::MantidWidgets {
 
-XIntegrationScrollBar::XIntegrationScrollBar(QWidget *parent)
+XIntegrationScrollBar::XIntegrationScrollBar(QWidget *parent, bool isDiscrete, int stepsTotal)
     : QFrame(parent), m_resizeMargin(5), m_init(false), m_resizingLeft(false), m_resizingRight(false), m_moving(false),
-      m_changed(false), m_x(0), m_width(0), m_minimum(0.0), m_maximum(1.0) {
+      m_changed(false), m_x(0), m_width(0), m_minimum(0.0), m_maximum(1.0), m_stepsTotal(stepsTotal),
+      m_isDiscrete(isDiscrete), m_currentStepMin(0), m_currentStepMax(stepsTotal - 1) {
   setMouseTracking(true);
   setFrameShape(StyledPanel);
   m_slider = new QPushButton(this);
@@ -56,6 +58,9 @@ bool XIntegrationScrollBar::eventFilter(QObject *object, QEvent *e) {
   auto *slider = dynamic_cast<QPushButton *>(object);
   if (!slider)
     return false;
+  if (m_isDiscrete) {
+    return manageEventDiscrete(e);
+  }
   if (e->type() == QEvent::Leave) {
     if (QApplication::overrideCursor()) {
       QApplication::restoreOverrideCursor();
@@ -123,6 +128,7 @@ bool XIntegrationScrollBar::eventFilter(QObject *object, QEvent *e) {
     }
     return true;
   } else if (e->type() == QEvent::KeyPress) {
+
     QKeyEvent *keyEv = static_cast<QKeyEvent *>(e);
     int step = 1;
     int sliderx = m_slider->x();
@@ -188,9 +194,9 @@ bool XIntegrationScrollBar::eventFilter(QObject *object, QEvent *e) {
           m_slider->move(sliderx + (sliderWidth - m_resizeMargin) / 2, slidery);
           m_slider->resize(m_resizeMargin, sliderHeight);
         }
+        m_changed = true;
+        updateMinMax();
       }
-      m_changed = true;
-      updateMinMax();
       break;
 
     default:
@@ -207,6 +213,154 @@ bool XIntegrationScrollBar::eventFilter(QObject *object, QEvent *e) {
     return true;
   }
   return false;
+}
+
+bool XIntegrationScrollBar::manageEventDiscrete(QEvent *e) {
+
+  switch (e->type()) {
+  case QEvent::Leave:
+    if (QApplication::overrideCursor()) {
+      QApplication::restoreOverrideCursor();
+    }
+    return true;
+  case QEvent::KeyPress: {
+    QKeyEvent *keyEv = static_cast<QKeyEvent *>(e);
+    switch (keyEv->key()) {
+    case Qt::Key_Right:
+      if (m_currentStepMax < m_stepsTotal - 1) {
+        m_currentStepMax += 1;
+        m_currentStepMin += 1;
+      }
+      m_changed = true;
+      updateMinMax();
+      break;
+    case Qt::Key_Left:
+      if (m_currentStepMin > 0) {
+        m_currentStepMin -= 1;
+        m_currentStepMax -= 1;
+      }
+      m_changed = true;
+      updateMinMax();
+      break;
+    case Qt::Key_Up:
+      if ((m_currentStepMax + m_currentStepMin) % 2 == 0 && m_currentStepMax < m_stepsTotal - 1) {
+        m_currentStepMax += 1;
+        m_changed = true;
+      } else if (m_currentStepMin > 0) {
+        m_currentStepMin -= 1;
+        m_changed = true;
+      }
+      updateMinMax();
+      break;
+    case Qt::Key_Down:
+      if (m_currentStepMax - m_currentStepMin > 0) {
+        if ((m_currentStepMax + m_currentStepMin) % 2 == 1) {
+          m_currentStepMax -= 1;
+        } else {
+          m_currentStepMin += 1;
+        }
+        m_changed = true;
+      }
+      updateMinMax();
+      break;
+    default:
+      break;
+    }
+    setDiscreteValues();
+    return true;
+  }
+
+  case QEvent::KeyRelease: {
+    QKeyEvent *keyEv = static_cast<QKeyEvent *>(e);
+    if ((keyEv->key() == Qt::Key_Left || keyEv->key() == Qt::Key_Right || keyEv->key() == Qt::Key_Up ||
+         keyEv->key() == Qt::Key_Down) &&
+        !keyEv->isAutoRepeat()) {
+      emit changed(m_minimum, m_maximum);
+    }
+    m_changed = false;
+    return true;
+  }
+
+  case QEvent::MouseButtonPress: {
+    auto *me = static_cast<QMouseEvent *>(e);
+    m_x = me->x();
+    m_width = m_slider->width();
+    if (m_x < m_resizeMargin) {
+      m_resizingLeft = true;
+    } else if (m_x > m_width - m_resizeMargin) {
+      m_resizingRight = true;
+    } else {
+      m_moving = true;
+    }
+    break;
+  }
+
+  case QEvent::MouseButtonRelease:
+    m_resizingLeft = false;
+    m_resizingRight = false;
+    m_moving = false;
+    if (m_changed) {
+      emit changed(m_minimum, m_maximum);
+    }
+    m_changed = false;
+    break;
+
+  case QEvent::MouseMove: {
+    auto *me = static_cast<QMouseEvent *>(e);
+    int x = me->x();
+    int w = m_slider->width();
+    if (x < m_resizeMargin || x > w - m_resizeMargin) {
+      if (!QApplication::overrideCursor()) {
+        QApplication::setOverrideCursor(QCursor(Qt::SizeHorCursor));
+      }
+    } else {
+      QApplication::restoreOverrideCursor();
+    }
+
+    if (m_moving) {
+      int idx = x - m_x;
+      int new_x = m_slider->x() + idx;
+      int width = m_currentStepMax - m_currentStepMin;
+      m_currentStepMin = std::min(
+          static_cast<int>(std::round(static_cast<double>(std::max(new_x, 0) * (m_stepsTotal - 1)) / this->width())),
+          m_stepsTotal - width - 1);
+      m_currentStepMax = m_currentStepMin + width;
+      m_changed = true;
+
+      updateMinMax();
+
+    } else if (m_resizingLeft) {
+      int idx = x - m_x;
+      int new_x = m_slider->x() + idx;
+      m_currentStepMin =
+          static_cast<int>(std::round(static_cast<double>(std::max(new_x, 0) * (m_stepsTotal - 1)) / this->width()));
+      m_currentStepMin = std::min(m_currentStepMin, m_currentStepMax);
+      m_changed = true;
+      updateMinMax();
+
+    } else if (m_resizingRight) {
+      int idx = x - m_x;
+      int new_x = m_slider->x() + idx + m_width;
+      m_currentStepMax = static_cast<int>(
+          std::round(static_cast<double>(std::min(new_x, this->width()) * (m_stepsTotal - 1) / this->width())));
+      m_currentStepMax = std::max(m_currentStepMax, m_currentStepMin);
+      m_changed = true;
+      updateMinMax();
+    }
+    setDiscreteValues();
+    return true;
+  }
+  default:
+    break;
+  }
+  return false;
+}
+
+void XIntegrationScrollBar::setDiscreteValues() {
+  if (!m_isDiscrete)
+    return;
+  set(std::max((static_cast<double>(m_currentStepMin) - 0.1) / (m_stepsTotal - 1), 0.),
+      std::min((static_cast<double>(m_currentStepMax) + 0.1) / (m_stepsTotal - 1), 1.));
 }
 
 /**
@@ -236,26 +390,32 @@ void XIntegrationScrollBar::set(double minimum, double maximum) {
   }
   m_minimum = minimum;
   m_maximum = maximum;
+  m_currentStepMin = static_cast<int>(std::round(minimum * static_cast<double>(m_stepsTotal - 1)));
+  m_currentStepMax = static_cast<int>(std::round(maximum * static_cast<double>(m_stepsTotal - 1)));
   int x = static_cast<int>(m_minimum * this->width());
   int w = static_cast<int>((m_maximum - m_minimum) * this->width());
-  if (w <= 2 * m_resizeMargin) {
+
+  if (w <= 2 * m_resizeMargin)
     w = 2 * m_resizeMargin + 1;
-  }
+  if (x > this->width() - 2 * m_resizeMargin - 1)
+    x = this->width() - 2 * m_resizeMargin - 1;
+
   m_slider->move(x, 0);
   m_slider->resize(w, this->height());
 }
 
 void XIntegrationScrollBar::updateMinMax() {
-  m_minimum = double(m_slider->x()) / this->width();
-  m_maximum = m_minimum + double(m_slider->width()) / this->width();
+  if (!m_isDiscrete) {
+    m_minimum = double(m_slider->x()) / this->width();
+    m_maximum = m_minimum + double(m_slider->width()) / this->width();
+  }
   emit running(m_minimum, m_maximum);
 }
 
 //---------------------------------------------------------------------------------//
-
-XIntegrationControl::XIntegrationControl(InstrumentWidget *instrWindow)
-    : QFrame(instrWindow), m_instrWindow(instrWindow), m_totalMinimum(0), m_totalMaximum(1), m_minimum(0),
-      m_maximum(1) {
+XIntegrationControl::XIntegrationControl(InstrumentWidget *instrWindow, bool isDiscrete, int stepsTotal)
+    : QFrame(instrWindow), m_instrWindow(instrWindow), m_totalMinimum(0), m_totalMaximum(1), m_minimum(0), m_maximum(1),
+      m_isDiscrete(isDiscrete) {
   auto *layout = new QHBoxLayout();
   m_minText = new QLineEdit(this);
   m_minText->setMaximumWidth(100);
@@ -263,15 +423,18 @@ XIntegrationControl::XIntegrationControl(InstrumentWidget *instrWindow)
   m_maxText = new QLineEdit(this);
   m_maxText->setMaximumWidth(100);
   m_maxText->setToolTip("Maximum x value");
+
   m_units = new QLabel("TOF", this);
   m_setWholeRange = new QPushButton("Reset");
   m_setWholeRange->setToolTip("Reset integration range to maximum");
-  m_scrollBar = new XIntegrationScrollBar(this);
+  m_scrollBar = new XIntegrationScrollBar(this, isDiscrete, stepsTotal);
 
   layout->addWidget(m_units, 0);
+
   layout->addWidget(m_minText, 0);
   layout->addWidget(m_scrollBar, 1);
   layout->addWidget(m_maxText, 0);
+
   layout->addWidget(m_setWholeRange, 0);
   setLayout(layout);
 
@@ -287,7 +450,7 @@ void XIntegrationControl::sliderChanged(double minimum, double maximum) {
   double w = m_totalMaximum - m_totalMinimum;
   m_minimum = m_totalMinimum + minimum * w;
   m_maximum = m_totalMinimum + maximum * w;
-  if (w > 0 && (m_maximum - m_minimum) / w >= 0.98) {
+  if (!m_isDiscrete && w > 0 && (m_maximum - m_minimum) / w >= 0.98) {
     m_minimum = m_totalMinimum;
     m_maximum = m_totalMaximum;
   }
@@ -302,6 +465,13 @@ void XIntegrationControl::sliderRunning(double minimum, double maximum) {
   updateTextBoxes();
 }
 
+void XIntegrationControl::discretize() {
+  m_totalMinimum = std::round(m_totalMinimum);
+  m_totalMaximum = std::round(m_totalMaximum);
+  m_minimum = std::round(m_minimum);
+  m_maximum = std::round(m_maximum);
+}
+
 void XIntegrationControl::setTotalRange(double minimum, double maximum) {
   if (minimum > maximum) {
     std::swap(minimum, maximum);
@@ -310,6 +480,7 @@ void XIntegrationControl::setTotalRange(double minimum, double maximum) {
   m_totalMaximum = maximum;
   m_minimum = minimum;
   m_maximum = maximum;
+
   updateTextBoxes();
 }
 
@@ -342,6 +513,9 @@ double XIntegrationControl::getMaximum() const { return m_maximum; }
 double XIntegrationControl::getWidth() const { return m_maximum - m_minimum; }
 
 void XIntegrationControl::updateTextBoxes() {
+  if (m_isDiscrete) {
+    discretize();
+  }
   m_minText->setText(QString::number(m_minimum));
   m_maxText->setText(QString::number(m_maximum));
   m_setWholeRange->setEnabled(m_minimum != m_totalMinimum || m_maximum != m_totalMaximum);
@@ -360,6 +534,7 @@ void XIntegrationControl::setMinimum() {
 void XIntegrationControl::setMaximum() {
   bool ok;
   QString text = m_maxText->text();
+
   double maxValue = text.toDouble(&ok);
   if (!ok)
     return;
