83176862abc24e0271b96d8ebee38823f79a582c
Silke Schomann <silke.schomann@stfc.ac.uk>
2021-07-29 09:59:32 +0100

    Added FunctionDomain1DVector

---
+++ Framework/PythonInterface/mantid/api/src/Exports/FunctionDomain1D.cpp
@@ -0,0 +1,15 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidAPI/FunctionDomain1D.h"
+
+#include <boost/python/class.hpp>
+#include <boost/python/module.hpp>
+
+using Mantid::API::FunctionDomain1D;
+using namespace boost::python;
+
+void export_FunctionDomain1D() { class_<FunctionDomain1D, boost::noncopyable>("FunctionDomain1D", no_init); }
