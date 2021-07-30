7b84b5efccb97d4eae201ecadced2917cf1c418f
Silke Schomann <silke.schomann@stfc.ac.uk>
2021-07-30 15:34:46 +0100

    Added FunctionDomain1DHistogram

--- Framework/PythonInterface/mantid/api/src/Exports/FunctionDomain1DHistogram.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/FunctionDomain1DHistogram.cpp
@@ -15,23 +15,23 @@
 #include <boost/python/module.hpp>
 
 using Mantid::API::FunctionDomain1D;
-using Mantid::API::FunctionDomain1DVector;
+using Mantid::API::FunctionDomain1DHistogram;
 using namespace Mantid::PythonInterface;
 using namespace Mantid::PythonInterface::Converters;
 using namespace boost::python;
 
-GET_POINTER_SPECIALIZATION(FunctionDomain1DVector)
+GET_POINTER_SPECIALIZATION(FunctionDomain1Histogram)
 
-FunctionDomain1DVector *createFunctionDomain1DVector(const boost::python::object &xvalues) {
-  if (NDArray::check(xvalues)) {
-    return new FunctionDomain1DVector(NDArrayToVector<double>(NDArray(xvalues))());
+FunctionDomain1DHistogram *createFunctionDomain1DHistogram(const boost::python::object &bins) {
+  if (NDArray::check(bins)) {
+    return new FunctionDomain1DHistogram(NDArrayToVector<double>(NDArray(bins))());
   } else {
-    return new FunctionDomain1DVector(PySequenceToVector<double>(NDArray(xvalues))());
+    return new FunctionDomain1DHistogram(PySequenceToVector<double>(NDArray(bins))());
   }
 }
 
-void export_FunctionDomain1DVector() {
+void export_FunctionDomain1DHistogram() {
 
-  class_<FunctionDomain1DVector, bases<FunctionDomain1D>, boost::noncopyable>("FunctionDomain1DVector", no_init)
-      .def("__init__", make_constructor(&createFunctionDomain1DVector, default_call_policies(), (arg("xvalues"))));
+  class_<FunctionDomain1DHistogram, bases<FunctionDomain1D>, boost::noncopyable>("FunctionDomain1DHistogram", no_init)
+      .def("__init__", make_constructor(&createFunctionDomain1DHistogram, default_call_policies(), (arg("bins"))));
 }
