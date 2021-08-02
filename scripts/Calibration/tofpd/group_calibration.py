42c35a67a34a05fdd567fbf28bae5b7c04c8e098
Yuanpeng Zhang <zyroc1990@gmail.com>
2021-08-02 11:43:11 -0400

    fix a little bug

--- scripts/Calibration/tofpd/group_calibration.py
+++ scripts/Calibration/tofpd/group_calibration.py
@@ -289,7 +289,10 @@ def pdcalibration_groups(data_ws,
     for i in range(num_hist):
         difc_bak = mtd['calib_table_bak'].row(i)['DIFC']
         difc_calib = mtd[f'{output_basename}_cc_pd_diffcal_tmp'].row(i)['difc']
-        diff_difc = abs(difc_bak - difc_calib) / difc_calib * 100.0
+        if mtd[f'{output_basename}_pd_diffcal_mask'].readY(i)[0] == 0.0:
+            diff_difc = abs(difc_bak - difc_calib) / difc_calib * 100.0
+        else:
+            diff_difc = 0.0
         if diff_difc >= BadCalibThreshold:
             difc_calib = difc_bak
         new_row = { 'detid': mtd[f'{output_basename}_cc_pd_diffcal_tmp'].row(i)['detid'],
