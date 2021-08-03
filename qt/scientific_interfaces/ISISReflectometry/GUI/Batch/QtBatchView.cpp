fe16ec22ce6817153674b0fefc027d29c7efa007
David Fairbrother <DavidFair@users.noreply.github.com>
2021-08-03 20:12:58 +0100

    Remove tab instead of setting hidden in release builds
    SetVisible does not work as we originally thought, so instead completely
    hide the tab at build time to resolve the compilation error.
    Re #32096

--- qt/scientific_interfaces/ISISReflectometry/GUI/Batch/QtBatchView.cpp
+++ qt/scientific_interfaces/ISISReflectometry/GUI/Batch/QtBatchView.cpp
@@ -52,7 +52,7 @@ void QtBatchView::initLayout() {
   m_ui.batchTabs->addTab(m_preview.get(), "Reduction Preview");
 #ifdef NDEBUG
   // Preview disabled in Release mode to prevent users "finding" it in the nightly
-  m_ui.batchTabs->setVisible(m_ui.batchTabs->indexOf(m_preview.get()), false);
+  m_ui.batchTabs->removeTab(m_ui.batchTabs->indexOf(m_preview.get()));
 #endif // NDEBUG
 
   m_save = createSaveTab();
