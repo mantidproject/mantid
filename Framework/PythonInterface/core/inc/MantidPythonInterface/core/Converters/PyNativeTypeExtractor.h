032180876c3c942c4e36bf9c7e2ce24db523836c
Gemma Guest <gemma.guest@stfc.ac.uk>
2021-08-04 18:21:53 +0100

    Add support for property lists
    Uses recursive variant so supports lists of lists etc.

--- Framework/PythonInterface/core/inc/MantidPythonInterface/core/Converters/PyNativeTypeExtractor.h
+++ Framework/PythonInterface/core/inc/MantidPythonInterface/core/Converters/PyNativeTypeExtractor.h
@@ -10,27 +10,33 @@
 #include <vector>
 
 #include <boost/python/extract.hpp>
+#include <boost/python/list.hpp>
 #include <boost/python/object.hpp>
+#include <boost/variant.hpp>
+
 #include <exception>
 
 namespace Mantid::PythonInterface {
 
 struct PyNativeTypeExtractor {
-  using PythonOutputT = std::variant<bool, int, double, std::string>;
+  using PythonOutputT =
+      boost::make_recursive_variant<bool, int, double, std::string, std::vector<boost::recursive_variant_>>::type;
 
   static PythonOutputT convert(const boost::python::object &obj) {
     using namespace boost::python;
-    const PyObject *rawptr = obj.ptr();
+    PyObject *rawptr = obj.ptr();
     PythonOutputT out;
 
     // This currently  doesn't handle lists, but this could be retrofitted in future work
-    if (PyBool_Check(rawptr) == 1) {
+    if (PyList_Check(rawptr)) {
+      out = handleList(obj);
+    } else if (PyBool_Check(rawptr)) {
       bool val = extract<bool>(obj);
       out = val;
-    } else if (PyFloat_Check(rawptr) == 1) {
+    } else if (PyFloat_Check(rawptr)) {
       double val = extract<double>(obj);
       out = val;
-    } else if (PyLong_Check(rawptr) == 1) {
+    } else if (PyLong_Check(rawptr)) {
       int val = extract<int>(obj);
       out = val;
     } else if (PyUnicode_Check(rawptr)) {
@@ -40,6 +46,18 @@ struct PyNativeTypeExtractor {
       throw std::invalid_argument("Unrecognised Python type");
     }
     return out;
+  }
+
+private:
+  static PythonOutputT handleList(const boost::python::object &obj) {
+    auto rawptr = obj.ptr();
+    auto n = PyList_Size(rawptr);
+    auto vec = std::vector<PythonOutputT>();
+    vec.reserve(n);
+    for (Py_ssize_t i = 0; i < n; ++i) {
+      vec.emplace_back(convert(obj[i]));
+    }
+    return vec;
   }
 };
 
