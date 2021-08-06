dc5cc87427ccec79adb0645422e3243021edcb7f
Danny Hindson <danny.hindson@stfc.ac.uk>
2021-08-06 15:52:41 +0100

    Fix error in calibration
    Reinstate the alpha value as an instrument parameter so it can be retrieved
    at the end of the calibration and written out to the prm file
    Also rename alpha to alpha_0 to avoid name clash with new Bk2BkExpConvPV alpha
    parameter

--- scripts/Engineering/gui/engineering_diffraction/tabs/calibration/model.py
+++ scripts/Engineering/gui/engineering_diffraction/tabs/calibration/model.py
@@ -107,7 +107,7 @@ class CalibrationModel(object):
         SouthBank = ws_inst.getComponentByName("SouthBank")
         params_north = []
         params_south = []
-        for param_name in ["alpha", "beta_0", "beta_1", "sigma_0_sq", "sigma_1_sq", "sigma_2_sq"]:
+        for param_name in ["alpha_0", "beta_0", "beta_1", "sigma_0_sq", "sigma_1_sq", "sigma_2_sq"]:
             params_north += [NorthBank.getNumberParameter(param_name)[0]]
             params_south += [SouthBank.getNumberParameter(param_name)[0]]
 
