189b278f23a7571ed45260c4cb7bf27a62644770
Jose Borreguero <borreguero@gmail.com>
2021-08-12 09:13:02 -0400

    API algorithm
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- Framework/PythonInterface/plugins/algorithms/NOMADMedianDetectorTest.py
+++ Framework/PythonInterface/plugins/algorithms/NOMADMedianDetectorTest.py
@@ -5,24 +5,33 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 
-
 # package imports
-from mantid.api import FileAction, FileProperty, PropertyMode, PythonAlgorithm, WorkspaceProperty
+from mantid.api import (AlgorithmFactory, FileAction, FileProperty, PropertyMode, PythonAlgorithm, WorkspaceProperty)
 from mantid.kernel import Direction
-from mantid.utils.nomad._median_detector_test import _NOMADMedianDetectorTest
+from mantid.utils.nomad.diagnostics import _NOMADMedianDetectorTest
+
+# third-party imports
+import numpy as np
 
 
 class NOMADMedianDetectorTest(PythonAlgorithm, _NOMADMedianDetectorTest):
+
     def category(self):
+        """ Mantid required
+        """
         return "Diagnostics"
 
     def seeAlso(self):
         return ["MedianDetectorTest"]
 
     def name(self):
+        """ Mantid required
+        """
         return "NOMADMedianDetectorTest"
 
     def summary(self):
+        """ Mantid required
+        """
         return "Identifies detector pixels and whole tubes having total numbers of counts over a user defined maximum" \
                " or less than a user define minimum."
 
@@ -31,14 +40,26 @@ class NOMADMedianDetectorTest(PythonAlgorithm, _NOMADMedianDetectorTest):
             WorkspaceProperty('InputWorkspace', defaultValue='', direction=Direction.Input,
                               optional=PropertyMode.Mandatory),
             doc='Workspace containing reference pixel intensities')
+
         self.declareProperty(
             FileProperty(name='Configuration', defaultValue='', direction=Direction.Input, extensions=['.yml'],
-                         action=FileAction.Load, optional=PropertyMode.Mandatory),
+                         action=FileAction.Load),
             doc='YML file specifying collimation states and unused eight-packs')
+
         self.declareProperty(
             FileProperty(name='OutputMaskXML', defaultValue='', direction=Direction.Output, extensions=['.xml'],
-                         action=FileAction.Save, optional=PropertyMode.Mandatory)
-        )
+                         action=FileAction.Save),
+            doc='Output masked pixels in XML format')
 
     def PyExec(self):
+        ws = self.getProperty('InputWorkspace').value
+        ws.extractY()
+
+    def _load_intensities(self) -> np.ndarray:
+        r"""Pixel intensities
+        @details pixels in non-used eight-packs become masked
+        """
         pass
+
+
+AlgorithmFactory.subscribe(NOMADMedianDetectorTest)
