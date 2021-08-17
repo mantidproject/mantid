aeb7937f19c08a94cbe2d7f730df6df50d9489d3
Adam J. Jackson <a.j.jackson@physics.org>
2021-08-17 13:32:30 +0100

    euphonic DOS systemtest: skip install if already available
    - If Euphonic was already installed (e.g. Conda build) then no need to
      install Euphonic to temporary directory.

--- Testing/SystemTests/tests/framework/SimulatedDensityOfStatesEuphonicPackaging.py
+++ Testing/SystemTests/tests/framework/SimulatedDensityOfStatesEuphonicPackaging.py
@@ -9,12 +9,13 @@ import tempfile
 import scipy
 
 from abins.test_helpers import find_file
+from dos.load_euphonic import euphonic_available
 from mantid.simpleapi import SimulatedDensityOfStates
 from systemtesting import MantidSystemTest
 
 
 class SimulatedDensityOfStatesTest(MantidSystemTest):
-    """Make sure normal case will run without Euphonic installed"""
+    """Make sure normal case will run regardless of Euphonic status"""
     def runTest(self):
         SimulatedDensityOfStates(CASTEPFile=find_file('Na2SiF6_CASTEP.phonon'),
                                  Function='Gaussian',
@@ -51,7 +52,15 @@ class SimulatedDensityOfStatesEuphonicTest(MantidSystemTest):
                      "Here are the directory contents: ")
                     + "; ".join(directory_contents))
 
-    def runTest(self):
+    @classmethod
+    def _install_euphonic_to_tmp_prefix(cls, scipy_prefix, euphonic_prefix,
+                                        verbose=False):
+        """Install Euphonic library to temporary prefix
+
+        Up-to-date versions of Pip and Packaging are used to support this
+
+        If necessary, additional dependencies are installed to scipy_prefix
+        """
         # First check if we are using debian-style Pip, where --system
         # is needed to prevent default --user option from breaking --prefix.
 
@@ -61,53 +70,62 @@ class SimulatedDensityOfStatesEuphonicTest(MantidSystemTest):
         else:
             compatibility_args = []
 
-        with tempfile.TemporaryDirectory() as scipy_prefix, \
-                tempfile.TemporaryDirectory() as euphonic_prefix:
+        process = subprocess.run([sys.executable, "-m", "pip", "install",
+                                  "--prefix", scipy_prefix]
+                                 + compatibility_args
+                                 + ["pip", "packaging"],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
 
+        prefix_path = pathlib.Path(scipy_prefix)
+
+        # Add to path if anything was installed
+        env = os.environ.copy()
+        if list(prefix_path.iterdir()):
+            scipy_site_packages = cls._add_libs_from_prefix(prefix_path)
+
+            process_pythonpath =  (
+                ':'.join([str(dir) for dir in scipy_site_packages]
+                         + [os.environ['PYTHONPATH']]))
+            env['PYTHONPATH'] = process_pythonpath
+
+        # Install minimum Scipy version for Euphonic if necessary
+        from packaging import version
+        if version.parse(scipy.version.version) < version.parse('1.0.1'):
             process = subprocess.run([sys.executable, "-m", "pip", "install",
-                                      "--prefix", scipy_prefix]
-                                     + compatibility_args
-                                     + ["pip", "packaging"],
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.STDOUT)
-
-            prefix_path = pathlib.Path(scipy_prefix)
-
-            # Add to path if anything was installed
-            env = os.environ.copy()
-            if list(prefix_path.iterdir()):
-                scipy_site_packages = self._add_libs_from_prefix(prefix_path)
-
-                process_pythonpath =  ':'.join([str(dir) for dir in scipy_site_packages] + [os.environ['PYTHONPATH']])
-                env['PYTHONPATH'] = process_pythonpath
-
-            # Install minimum Scipy version for Euphonic if necessary
-            from packaging import version
-            if version.parse(scipy.version.version) < version.parse('1.0.1'):
-                process = subprocess.run([sys.executable, "-m", "pip", "install",
-                                          "--ignore-installed",
-                                          "--no-deps",
-                                          "--prefix", scipy_prefix,
-                                         "scipy==1.0", "pytest"],
-                                         stdout=subprocess.PIPE,
-                                         stderr=subprocess.STDOUT,
-                                         env=env)
-                print(process.stdout.decode('utf-8'))
-
-                # Add prefix again, in case nothing was installed before
-                scipy_site_packages = self._add_libs_from_prefix(prefix_path)
-
-                importlib.reload(scipy)
-
-            process = subprocess.run([sys.executable, "-m", "pip", "install",
-                                      "--prefix", euphonic_prefix,
-                                      "euphonic[phonopy_reader]"],
+                                      "--ignore-installed",
+                                      "--no-deps",
+                                      "--prefix", scipy_prefix,
+                                     "scipy==1.0", "pytest"],
                                      stdout=subprocess.PIPE,
                                      stderr=subprocess.STDOUT,
                                      env=env)
+            if verbose:
+                print(process.stdout.decode('utf-8'))
+
+            # Add prefix again, in case nothing was installed before
+            scipy_site_packages = cls._add_libs_from_prefix(prefix_path)
+
+            importlib.reload(scipy)
+
+        process = subprocess.run([sys.executable, "-m", "pip", "install",
+                                  "--prefix", euphonic_prefix,
+                                  "euphonic[phonopy_reader]"],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT,
+                                 env=env)
+        if verbose:
             print(process.stdout.decode('utf-8'))
 
-            self._add_libs_from_prefix(pathlib.Path(euphonic_prefix))
+        cls._add_libs_from_prefix(pathlib.Path(euphonic_prefix))
+
+    def runTest(self):
+        with tempfile.TemporaryDirectory() as scipy_prefix, \
+                tempfile.TemporaryDirectory() as euphonic_prefix:
+
+            if not euphonic_available():
+                self._install_euphonic_to_tmp_prefix(scipy_prefix,
+                                                     euphonic_prefix)
 
             import pint  # noqa: F401
             import euphonic  # noqa: F401
