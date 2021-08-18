ddf7cf63b3466506aba09dcf8a0922b3ac3c0f97
David Fairbrother <DavidFair@users.noreply.github.com>
2021-08-18 12:12:10 +0100

    Bump Sphinx to 1.8.5 on MSVC
    Bumps MSVC to use Sphinx 1.8.5 (matching Ubuntu 18.04). This allows us
    to build dev-docs with warnings enabled on both platforms.
    
    RHEL is the odd one out now, but as we have (comparitvely) few
    developers on RHEL and this only affects dev docs we can cross that
    bridge when Conda is complete

--- buildconfig/CMake/Bootstrap.cmake
+++ buildconfig/CMake/Bootstrap.cmake
@@ -15,7 +15,7 @@ if(MSVC AND NOT CONDA_BUILD)
   set(THIRD_PARTY_GIT_URL
       "https://github.com/mantidproject/thirdparty-msvc2015.git"
   )
-  set(THIRD_PARTY_GIT_SHA1 9cef05cc24a447b760ec2d3f289315514fc264ab)
+  set(THIRD_PARTY_GIT_SHA1 1f44d901442239ae12c11e33187b3d877bb9f2b2)
   set(THIRD_PARTY_DIR ${EXTERNAL_ROOT}/src/ThirdParty)
   # Generates a script to do the clone/update in tmp
   set(_project_name ThirdParty)
