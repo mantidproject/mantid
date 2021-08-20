c3e060b0bcea094cfc1cdf64b4117b25979ae414
Adam J. Jackson <a.j.jackson@physics.org>
2021-08-20 14:45:18 +0100

    Abins q-point sampling: set with advanced parameter, gamma-center

--- scripts/Inelastic/dos/load_euphonic.py
+++ scripts/Inelastic/dos/load_euphonic.py
@@ -4,6 +4,7 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
+from typing import Optional
 
 import numpy as np
 
@@ -24,7 +25,9 @@ def euphonic_available():
     return True
 
 
-def euphonic_calculate_modes(filename: str, cutoff: float = 20.):
+def euphonic_calculate_modes(filename: str, cutoff: float = 20.,
+                             gamma: bool = True,
+                             asr: Optional[str] = 'reciprocal'):
     """
     Read force constants file with Euphonic and sample frequencies/modes
 
@@ -32,6 +35,8 @@ def euphonic_calculate_modes(filename: str, cutoff: float = 20.):
     :param cutoff:
         Sampling density of Brillouin-zone. Specified as real-space length
         cutoff in Angstrom.
+    :param gamma:
+        Shift sampling grid to include the Gamma-point.
 
     :returns: euphonic.QpointPhononModes
 
@@ -47,9 +52,16 @@ def euphonic_calculate_modes(filename: str, cutoff: float = 20.):
     mp_sampling = [ceil(x)
                    for x in (cutoff * recip_lattice_lengths / (2 * np.pi))]
     qpts = mp_grid(mp_sampling)
+
+    if gamma:
+        mp_sampling = np.array(mp_sampling, dtype=int)
+        # Shift directions with even number of samples by half the grid spacing
+        offsets = ((mp_sampling + 1) % 2) * (0.5 / mp_sampling)
+        qpts += offsets
+
     logger.notice('Calculating phonon modes on {} grid'.format(
         'x'.join(map(str, mp_sampling))))
-    modes = fc.calculate_qpoint_phonon_modes(qpts)
+    modes = fc.calculate_qpoint_phonon_modes(qpts, asr=asr)
 
     return modes
 
@@ -69,9 +81,9 @@ def get_data_with_euphonic(file_name: str, cutoff: float = 20.):
     """
     modes = euphonic_calculate_modes(file_name=file_name, cutoff=cutoff)
 
-    file_data = {'num_ions': len(fc.crystal.atom_type),
+    file_data = {'num_ions': len(modes.crystal.atom_type),
                  'num_branches': modes.frequencies.magnitude.shape[1],
-                 'unit_cell': fc.crystal.cell_vectors.to('angstrom').magnitude}
+                 'unit_cell': modes.crystal.cell_vectors.to('angstrom').magnitude}
 
     file_data['ir_intensities'] = np.array([])
     file_data['raman_intensities'] = np.array([])
@@ -81,7 +93,7 @@ def get_data_with_euphonic(file_name: str, cutoff: float = 20.):
     # (to match order in CASTEP .phonon files)
     # so reshape data into this format.
     file_data['eigenvectors'] = eigenvectors.reshape(
-            (len(qpts), file_data['num_branches'] * file_data['num_ions'], 3))
+            (len(modes.qpts), file_data['num_branches'] * file_data['num_ions'], 3))
     file_data['weights'] = (modes.weights * np.ones((file_data['num_branches'], 1))).flatten()
 
     ions = [{'index': i,
@@ -89,9 +101,9 @@ def get_data_with_euphonic(file_name: str, cutoff: float = 20.):
              'fract_coord': position,
              'isotope_number': mass}
             for i, (element, position, mass)
-            in enumerate(zip(fc.crystal.atom_type,
-                             fc.crystal.atom_r,
-                             fc.crystal.atom_mass.magnitude))]
+            in enumerate(zip(modes.crystal.atom_type,
+                             modes.crystal.atom_r,
+                             modes.crystal.atom_mass.magnitude))]
 
     file_data['ions'] = ions
 
