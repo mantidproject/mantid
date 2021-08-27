22d137686aa1a86425aaa32d46d6c8b6a5539fdf
Joseph Torsney <joseph.torsney@stfc.ac.uk>
2021-08-27 10:48:29 +0100

    Remove unused imports causing pre-commit failure

--- scripts/Muon/GUI/Common/home_instrument_widget/home_instrument_widget_model.py
+++ scripts/Muon/GUI/Common/home_instrument_widget/home_instrument_widget_model.py
@@ -5,8 +5,6 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 from decimal import Decimal, InvalidOperation
-from mantid import api
-from mantid.api import ITableWorkspace
 
 
 class InstrumentWidgetModel(object):
