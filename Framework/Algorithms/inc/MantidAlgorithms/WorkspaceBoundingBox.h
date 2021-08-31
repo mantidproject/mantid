d870d12bde29b9e06b398cbb949c07a8652417e1
Michael Walsh <walshmm@ornl.gov>
2021-08-31 13:40:43 -0400

    refactor is passing tests

--- Framework/Algorithms/inc/MantidAlgorithms/WorkspaceBoundingBox.h
+++ Framework/Algorithms/inc/MantidAlgorithms/WorkspaceBoundingBox.h
@@ -6,68 +6,61 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidKernel/Logger.h"
+
 namespace Mantid {
 namespace Algorithms {
 
 /* This is a simple class originally intended for use solely with FindCenterOfMassPosition2.cpp
- * 
+ *
  */
 class WorkspaceBoundingBox {
 public:
-    WorkspaceBoundingBox(API::MatrixWorkspace_sptr workspace);
-    ~WorkspaceBoundingBox();
+  WorkspaceBoundingBox(API::MatrixWorkspace_sptr workspace, Kernel::Logger &g_log);
+  WorkspaceBoundingBox(Kernel::Logger &g_log);
+  ~WorkspaceBoundingBox();
 
-    API::MatrixWorkspace_sptr getWorkspace() { return workspace;}
-    double getX() { return x;}
-    double getY() { return y;}
-    double getCenterX() { return *centerX;}
-    double getCenterY() { return *centerY;}
-    double getXMin() { return xMin;}
-    double getXMax() { return xMax;}
-    double getYMin() { return yMin;}
-    double getYMax() { return yMax;}
+  API::MatrixWorkspace_sptr getWorkspace() { return workspace; }
+  double getX() { return x; }
+  double getY() { return y; }
+  double getCenterX() { return centerX; }
+  double getCenterY() { return centerY; }
+  double getXMin() { return xMin; }
+  double getXMax() { return xMax; }
+  double getYMin() { return yMin; }
+  double getYMax() { return yMax; }
 
-    void setPosition(double x, double y){
-        this.x = x;
-        this.y = y;
-    }
+  void setPosition(double x, double y);
+  void setCenter(double x, double y);
+  void setBounds(double xMin, double xMax, double yMin, double yMax);
 
-    void setCenter(double x, double y) {
-        this.centerX = x;
-        this.centerY = y;
-    }
-
-    void setBounds(double xMin, double xMax, double yMin, double yMax) {
-        this.xMin = xMin;
-        this.xMax = xMax;
-        this.yMin = yMin;
-        this.yMax = yMax;
-    }
-
-    double calculateDistance();
-    double calculateRadiusX();
-    double calculateRadiusY();
-    double updatePositionAndReturnCount(int index);
-    int findFirstValidWs(const int numSpec);
-    bool isValidWs(int index);
-    bool isOutOfBoundsOfNonDirectBeam(const double beam_radius, int index, const bool direct_beam);
-    bool containsPoint(double x, double y);
-    void normalizePosition(double x, double y);
-    void updateMinMax(int index);
+  double calculateDistance();
+  double calculateRadiusX();
+  double calculateRadiusY();
+  double updatePositionAndReturnCount(int index);
+  int findFirstValidWs(const int numSpec);
+  bool isValidWs(int index);
+  bool isOutOfBoundsOfNonDirectBeam(const double beam_radius, int index, const bool direct_beam);
+  bool containsPoint(double x, double y);
+  void normalizePosition(double x, double y);
+  void updateMinMax(int index);
 
 private:
-    API::MatrixWorkspace_sptr workspace;
-    double x = 0;
-    double y = 0;
-    double centerX;
-    double centerY;
-    double xMin = 0;
-    double xMax = 0;
-    double yMin = 0;
-    double yMax = 0;
-    const int m_specID = 0;
-}
+  API::MatrixWorkspace_sptr workspace;
+  double x = 0;
+  double y = 0;
+  double centerX;
+  double centerY;
+  double xMin = 0;
+  double xMax = 0;
+  double yMin = 0;
+  double yMax = 0;
+  const int m_specID = 0;
 
+  /// Logger for this helper
+  Kernel::Logger &g_log;
+};
 
-}
-}
+} // namespace Algorithms
+} // namespace Mantid
--- Framework/Algorithms/src/WorkspaceBoundingBox.cpp
+++ Framework/Algorithms/src/WorkspaceBoundingBox.cpp
@@ -1,14 +1,38 @@
 #include "MantidAlgorithms/WorkspaceBoundingBox.h"
+#include "MantidAPI/MatrixWorkspace.h"
+#include "MantidAPI/SpectrumInfo.h"
 
-WorkspaceBoundingBox(API::MatrixWorkspace_sptr workspace) 
-:workspace(workspace) {
-    //maybe calculate first run on init
+namespace Mantid {
+namespace Algorithms {
+
+WorkspaceBoundingBox::WorkspaceBoundingBox(API::MatrixWorkspace_sptr workspace, Kernel::Logger &g_log)
+    : workspace(workspace), g_log(g_log) {}
+
+WorkspaceBoundingBox::WorkspaceBoundingBox(Kernel::Logger &g_log) : g_log(g_log) {}
+
+WorkspaceBoundingBox::~WorkspaceBoundingBox() {}
+
+void WorkspaceBoundingBox::setPosition(double x, double y) {
+  this->x = x;
+  this->y = y;
+}
+
+void WorkspaceBoundingBox::setCenter(double x, double y) {
+  this->centerX = x;
+  this->centerY = y;
+}
+
+void WorkspaceBoundingBox::setBounds(double xMin, double xMax, double yMin, double yMax) {
+  this->xMin = xMin;
+  this->xMax = xMax;
+  this->yMin = yMin;
+  this->yMax = yMax;
 }
 
 bool WorkspaceBoundingBox::isValidWs(int index) {
-    const auto spectrumInfo = this.workspace->spectrumInfo();
+  const auto spectrumInfo = this->workspace->spectrumInfo();
   if (!spectrumInfo.hasDetectors(index)) {
-    g_log.warning() << "Workspace index " << i << " has no detector assigned to it - discarding\n";
+    g_log.warning() << "Workspace index " << index << " has no detector assigned to it - discarding\n";
     return false;
   }
   // Skip if we have a monitor or if the detector is masked.
@@ -16,7 +40,7 @@ bool WorkspaceBoundingBox::isValidWs(int index) {
     return false;
 
   // Get the current spectrum
-  auto &YIn = inputWS->y(iindex);
+  auto &YIn = this->workspace->y(index);
   // Skip if NaN of inf
   if (std::isnan(YIn[m_specID]) || std::isinf(YIn[m_specID]))
     return false;
@@ -24,41 +48,43 @@ bool WorkspaceBoundingBox::isValidWs(int index) {
 }
 
 int WorkspaceBoundingBox::findFirstValidWs(const int numSpec) {
-const auto spectrumInfo = this.workspace->spectrumInfo();
-  for(int i = 0; i < numSpec; ++i) {
-    if(isValidWs(spectrumInfo, i))
+  const auto spectrumInfo = this->workspace->spectrumInfo();
+  int i;
+  for (i = 0; i < numSpec; ++i) {
+    if (isValidWs(i))
       break;
   }
   return i;
 }
 
-double WorkspaceBoundingBox::updatePositionAndReturnCount(int index){
-  const auto spectrumInfo = this.workspace->spectrumInfo();
+double WorkspaceBoundingBox::updatePositionAndReturnCount(int index) {
+  const auto spectrumInfo = this->workspace->spectrumInfo();
+  auto &YIn = this->workspace->y(index);
   double x = spectrumInfo.position(index).X();
   double y = spectrumInfo.position(index).Y();
-  this.x += YIn[m_specID] * x;
-  this.y += YIn[m_specID] * y;
+  this->x += YIn[m_specID] * x;
+  this->y += YIn[m_specID] * y;
   return YIn[m_specID];
 }
 
-void WorkspaceBoundingBox::updateMinMax(int index){
-  const auto spectrumInfo = this.workspace->spectrumInfo();
+void WorkspaceBoundingBox::updateMinMax(int index) {
+  const auto spectrumInfo = this->workspace->spectrumInfo();
   double x = spectrumInfo.position(index).X();
   double y = spectrumInfo.position(index).Y();
-  this.xMin = std::min(x, this.xMin);
-  this.xMax = std::max(x, this.xMax);
-  this.yMin = std::min(y, this.yMin);
-  this.yMax = std::max(y, this.yMax);
+  this->xMin = std::min(x, this->xMin);
+  this->xMax = std::max(x, this->xMax);
+  this->yMin = std::min(y, this->yMin);
+  this->yMax = std::max(y, this->yMax);
 }
 
-bool WorkspaceBoundingBox::isOutOfBoundsOfNonDirectBeam(const double beam_radius, int index, const bool direct_beam){
-  const auto spectrumInfo = this.workspace->spectrumInfo();
+bool WorkspaceBoundingBox::isOutOfBoundsOfNonDirectBeam(const double beamRadius, int index, const bool directBeam) {
+  const auto spectrumInfo = this->workspace->spectrumInfo();
   double x = spectrumInfo.position(index).X();
   double y = spectrumInfo.position(index).Y();
-  if (!direct_beam) {
-    double dx = x - this.centerX;
-    double dy = y - this.centerY;
-    if (dx * dx + dy * dy < beam_radius * beam_radius)
+  if (!directBeam) {
+    double dx = x - this->centerX;
+    double dy = y - this->centerY;
+    if (dx * dx + dy * dy < beamRadius * beamRadius)
       return false;
   }
   return true;
@@ -67,20 +93,19 @@ bool WorkspaceBoundingBox::isOutOfBoundsOfNonDirectBeam(const double beam_radius
 double WorkspaceBoundingBox::calculateDistance() {
   return sqrt((centerX - x) * (centerX - x) + (centerY - y) * (centerY - y));
 }
-double WorkspaceBoundingBox::calculateRadiusX() {
-  return std::min((x - xMin), (xMax - x));
-}
-double WorkspaceBoundingBox::calculateRadiusY() {
-  return std::min((y - yMin), (yMax - y));
+double WorkspaceBoundingBox::calculateRadiusX() { return std::min((x - xMin), (xMax - x)); }
+double WorkspaceBoundingBox::calculateRadiusY() { return std::min((y - yMin), (yMax - y)); }
+
+void WorkspaceBoundingBox::normalizePosition(double x, double y) {
+  this->x /= x;
+  this->y /= y;
 }
 
-void WorkspaceBoundingBox::normalizePosition(double x, double y){
-    this.x /= x;
-    this.y /= y;
+bool WorkspaceBoundingBox::containsPoint(double x, double y) {
+  if (x > this->xMax || x < this->xMin || y > yMax || y < yMin)
+    return false;
+  return true;
 }
 
- bool WorkspaceBoundingBox::containsPoint(double x, double y) {
-     if(x > this.xMax || x < this.xMin || y > yMax, || y < yMin)
-        return false;
-    return true;
- }
+} // namespace Algorithms
+} // namespace Mantid
