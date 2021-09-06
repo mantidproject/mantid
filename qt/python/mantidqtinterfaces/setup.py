72fd3a0f018ca9ac0b7d21a476db9ec4c63f061e
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-09-06 11:02:39 +0100

    craete mantidqtinterfaces python project

--- qt/python/mantidqtinterfaces/setup.py
+++ qt/python/mantidqtinterfaces/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Mantid Repository : https://github.com/mantidproject/mantid
 #
-# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI,
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
@@ -9,8 +9,8 @@ import os
 from setuptools import find_packages, setup
 
 setup(
-    name='mantidqt',  # must match what is required by workbench setup.py
-    install_requires=['mantid'],
+    name='mantidqtinterfaces',  # must match what is required by workbench setup.py
+    install_requires=[''],
     version=os.environ['MANTID_VERSION_STR'],
     packages=find_packages(exclude=['*.test']),
     package_data={
