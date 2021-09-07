3446e4fc1568869799ec2123cd2cdea02e34a6fa
Michael Walsh <walshmm@ornl.gov>
2021-09-07 11:56:04 -0400

    added newline, moved docstrings to cpp

--- Framework/Algorithms/inc/MantidAlgorithms/WorkspaceBoundingBox.h
+++ Framework/Algorithms/inc/MantidAlgorithms/WorkspaceBoundingBox.h
@@ -42,52 +42,13 @@ public:
   double calculateDistance();
   double calculateRadiusX();
   double calculateRadiusY();
-  /** Sets member variables x/y to new x/y based on
-   *  spectrum info and historgram data at the given index
-   *
-   *  @param index :: index of spectrum data
-   *  @return number of points of histogram data at index
-   */
+
   double updatePositionAndReturnCount(int index);
-  /** Searches for the first valid spectrum info in member variable `workspace`
-   *
-   *  @param numSpec :: the number of spectrum in the workspace to search through
-   *  @return index of first valid spectrum
-   */
   int findFirstValidWs(const int numSpec);
-  /** Performs checks on the spectrum located at index to determine if
-   *  it is acceptable to be operated on
-   *
-   *  @param index :: index of spectrum data
-   *  @return true/false if its valid
-   */
   bool isValidWs(int index);
-  /** Checks to see if spectrum at index is within the diameter of the given beamRadius
-   *
-   *  @param beamRadius :: radius of beam in meters
-   *  @param index :: index of spectrum data
-   *  @param directBeam :: whether or not the spectrum is subject to the beam
-   *  @return number of points of histogram data at index
-   */
   bool isOutOfBoundsOfNonDirectBeam(const double beamRadius, int index, const bool directBeam);
-  /** Checks if a given x/y coord is within the bounding box
-   *
-   *  @param x :: x coordinate
-   *  @param y :: y coordinate
-   *  @return true/false if it is within the mins/maxs of the box
-   */
   bool containsPoint(double x, double y);
-  /** Perform normalization on x/y coords over given values
-   *
-   *  @param x :: value to normalize member x over
-   *  @param y :: value to normalize member y over
-   */
   void normalizePosition(double x, double y);
-  /** Compare current mins and maxs to the coordinates of the spectrum at index
-   *  expnd mins and maxs to include this spectrum
-   *
-   *  @param index :: index of spectrum data
-   */
   void updateMinMax(int index);
 
 private:
