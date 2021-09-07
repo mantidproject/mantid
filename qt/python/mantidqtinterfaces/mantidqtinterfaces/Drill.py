0f4e5f604de2edecbcbc5089279431ad445c05e4
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-09-07 11:38:18 +0100

    Move Diff, Drill and ISIS_sans launch scripts

---
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Drill.py
@@ -0,0 +1,29 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import sys
+
+from qtpy import PYQT5
+
+from mantid.kernel import config, logger
+
+
+if not PYQT5:
+    logger.error('Drill interface is supported only in workbench.')
+else:
+    if config['default.facility'] != 'ILL':
+        logger.error('Drill is enabled only if the facility is set to ILL.')
+    else:
+        from mantidqt.gui_helper import get_qapplication
+        from Interface.ui.drill.view.DrillView import DrillView
+
+        app, within_mantid = get_qapplication()
+        if 'drillInterface' not in globals():
+            drillInterface = DrillView()
+        drillInterface.show()
+        if not within_mantid:
+            sys.exit(app.exec_())
