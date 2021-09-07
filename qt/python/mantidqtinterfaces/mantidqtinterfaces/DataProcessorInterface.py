600c5864b9e3af7214a22812d037020ab2cb5460
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-09-07 14:57:52 +0100

    Move DataProcessorInterface

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/DataProcessorInterface.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/DataProcessorInterface.py
@@ -8,7 +8,7 @@
 """
     Script used to start the Test Interface from MantidPlot
 """
-from ui.dataprocessorinterface import data_processor_gui
+from mantidqtinterfaces.ui.dataprocessorinterface import data_processor_gui
 
 ui = data_processor_gui.DataProcessorGui()
 if ui.setup_layout():
