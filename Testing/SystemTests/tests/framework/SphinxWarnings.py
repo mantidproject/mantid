2b19f33288133f4da3536086c9d1896014538381
Wenduo Zhou <zhouw@ornl.gov>
2021-09-15 10:44:19 -0400

    Added Deprecated to allowed category.

--- Testing/SystemTests/tests/framework/SphinxWarnings.py
+++ Testing/SystemTests/tests/framework/SphinxWarnings.py
@@ -22,6 +22,7 @@ class SphinxWarnings(systemtesting.MantidSystemTest):
                                 'CorrectionFunctions',
                                 'Crystal',
                                 'DataHandling',
+                                'Deprecated',
                                 'Diagnostics',
                                 'Diffraction',
                                 'Events',
