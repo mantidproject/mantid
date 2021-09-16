a537c7f7889c082ef4079c4496b514d805fa0681
Pete Peterson <petersonpf@ornl.gov>
2021-09-16 07:48:48 -0400

    Make more options common between builds
    --keep-going isn't available until sphinx 2.0
    -W turns warnings into errors which should help jenkins figure out that
    the build finished with warnings

--- buildconfig/CMake/FindSphinx.cmake
+++ buildconfig/CMake/FindSphinx.cmake
@@ -45,10 +45,15 @@ find_package_handle_standard_args(Sphinx REQUIRED_VARS SPHINX_PACKAGE_DIR SPHINX
 
 # sphinx.__main__ was introduced in version 1.3.
 # Prior to this we need to pass sphinx.__init__ to -m
+set(SPHINX_MAIN "sphinx.__init__") # for very old sphinx
+set(SPHINX_NOCOLOR "")
+set (SPHINX_KEEPGOING "")
 if(SPHINX_VERSION VERSION_GREATER_EQUAL "1.3")
-  set(SPHINX_MAIN "sphinx")
+  set(SPHINX_MAIN "sphinx")  # sphinx can be called as a module
+  # arguments new in sphinx 1.3
   set(SPHINX_NOCOLOR "--no-color")
-else()
-  set(SPHINX_MAIN "sphinx.__init__")
-  set(SPHINX_NOCOLOR "")
+endif()
+if(SPHINX_VERSION VERSION_GREATER_EQUAL "2.0")
+  # arguments new in sphinx 2.0
+  set (SPHINX_KEEPGOING "--keep-going")
 endif()
