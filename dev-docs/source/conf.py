8d2a6ad97076f7dd34117eb366f685f3c352efc3
Pete Peterson <petersonpf@ornl.gov>
2021-09-16 08:57:24 -0400

    Be more flexible on how the stylesheet is added

--- dev-docs/source/conf.py
+++ dev-docs/source/conf.py
@@ -11,11 +11,15 @@ from distutils.version import LooseVersion
 
 
 # -- General configuration ------------------------------------------------
-def setup(app):
-    """Called automatically by Sphinx when starting the build process
-    """
-    app.add_css_file("custom.css")
+if LooseVersion(sphinx_version) > LooseVersion("1.6"):
 
+    def setup(app):
+        """Called automatically by Sphinx when starting the build process
+        """
+        if hasattr(app, 'add_css_file'):  # >=v1.8
+            app.add_css_file("custom.css")
+        else:
+            app.add_stylesheet("custom.css")  # v1.6-1.8
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
