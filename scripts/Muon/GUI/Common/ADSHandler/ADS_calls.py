d7d760e2a78fc6a39333b2cc2d30eca3ab0f96ef
Anthony Lim <anthony.lim@stfc.ac.uk>
2021-09-17 15:25:25 +0100

    refs #32519 fix rebin phasequads in muon GUI

--- scripts/Muon/GUI/Common/ADSHandler/ADS_calls.py
+++ scripts/Muon/GUI/Common/ADSHandler/ADS_calls.py
@@ -5,6 +5,7 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 from mantid.api import AnalysisDataService
+import mantid.simpleapi as mantid
 
 
 def remove_ws(name):
@@ -26,3 +27,11 @@ def check_if_workspace_exist(name):
 
 def add_ws_to_ads(name, workspace):
     AnalysisDataService.addOrReplace(name, workspace)
+
+
+# these are algs, but are related to the ADS
+def delete_ws(name):
+    alg = mantid.AlgorithmManager.create("DeleteWorkspace")
+    alg.initialize()
+    alg.setProperty("Workspace", name)
+    alg.execute()
