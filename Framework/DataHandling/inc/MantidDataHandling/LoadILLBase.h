db2d2163b832ad9cc2b7483eadafd2a5cefa8e91
gvardany <gagikvar@gmail.com>
2021-09-22 13:24:00 +0200

    doxygen comments in load base

--- Framework/DataHandling/inc/MantidDataHandling/LoadILLBase.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadILLBase.h
@@ -15,7 +15,7 @@
 
 namespace Mantid::DataHandling {
 
-/** LoadILLBase : A common base class for ILL raw data (numor) loaders.
+/** LoadILLBase : A common base class for ILL raw data nexus (numor) loaders.
  */
 
 class DLLExport LoadILLBase : public API::IFileLoader<Kernel::NexusDescriptor> {
@@ -33,16 +33,23 @@ protected:
   }
 
   // pure virtual methods
-  virtual API::Workspace_sptr buildWorkspace() = 0;
-  virtual void loadAndFillData() = 0;
+  virtual API::Workspace_sptr buildWorkspace() = 0; // initialize the workspace
+  virtual void loadAndFillData() = 0;               // load data from the file and fill into workspace
 
   // virtual methods
-  virtual void declareExtraProperties() {}
-  virtual void configureBeamline() {}
+  virtual void declareExtraProperties() {}            // declare additional properties if needed
+  virtual void configureBeamline() {}                 // configure the beamline components, place the detector, etc.
+  virtual bool isOutputGroup() { return false; }      // override if the output must be a workspace group
+  virtual std::string resolveVariant() { return ""; } // override to return the variant of the instrument, if any
+  virtual std::string resolveAcqMode() { return ""; } // resolve the acquisition mode as string for future queries
+
+  /**
+   * @brief mandatoryKeys override to return a vector of all the mandatory metadata keys according to the protocols
+   * Note, do not include the data entries here, as checking if data block exists is expensive. Furthermore, if it's
+   * data that is missing, there is nothing one could do.
+   * @return vector of mandatory keys
+   */
   virtual std::vector<std::string> mandatoryKeys() { return std::vector<std::string>(); }
-  virtual bool isOutputGroup() { return false; }
-  virtual std::string resolveVariant() { return ""; }
-  virtual std::string resolveAcqMode() { return ""; }
 
 private:
   // non-virtual methods
@@ -59,12 +66,12 @@ private:
   void setOutputWorkspace();
 
   // member variables
-  std::unique_ptr<NeXus::NXRoot> m_nxroot;
-  std::unique_ptr<NexusEntryProvider> m_nep;
-  std::shared_ptr<LoadHelper> m_helper;
-  std::shared_ptr<API::Workspace> m_workspace;
-  std::string m_mode;
-  std::string m_instrument;
-  std::string m_timestamp;
+  std::unique_ptr<NeXus::NXRoot> m_nxroot;     // pointer to the root of the nexus tree
+  std::unique_ptr<NexusEntryProvider> m_nep;   // pointer to nexus entry provider
+  std::shared_ptr<LoadHelper> m_helper;        // pointer to load helper
+  std::shared_ptr<API::Workspace> m_workspace; // pointer to output workspace
+  std::string m_mode;                          // acquisition mode
+  std::string m_instrument;                    // instrument name
+  std::string m_timestamp;                     // start time in ISO format
 };
 } // namespace Mantid::DataHandling
--- Framework/DataHandling/src/LoadILLBase.cpp
+++ Framework/DataHandling/src/LoadILLBase.cpp
@@ -24,6 +24,11 @@ using namespace Mantid::NeXus;
 
 namespace Mantid::DataHandling {
 
+/**
+ * @brief LoadILLBase::init initializes the common properties all loaders must have
+ * This method is final and cannot be overridden.
+ * If there are extra properties to declare, override declareExtraProperties
+ */
 void LoadILLBase::init() {
   declareProperty(std::make_unique<FileProperty>("Filename", "", FileProperty::Load, ".nxs"),
                   "The run number of the path of the data file to load.");
@@ -36,8 +41,16 @@ void LoadILLBase::init() {
   declareExtraProperties();
 }
 
+/**
+ * @brief LoadILLBase::setOutputWorkspace sets the output workspace
+ */
 void LoadILLBase::setOutputWorkspace() { setProperty<API::Workspace_sptr>("OutputWorkspace", m_workspace); }
 
+/**
+ * @brief LoadILLBase::getInstrumentDefinitionFilePath returns the fully resolved IDF file path.
+ * The instrument is loaded via file and not name, as the variants should not be declared in the facilities xml.
+ * @return the fully resolved path of the IDF file
+ */
 std::string LoadILLBase::getInstrumentDefinitionFilePath() {
   Poco::Path directory(ConfigService::Instance().getInstrumentDirectory());
   Poco::Path file(m_instrument + "_Definition.xml");
@@ -45,6 +58,15 @@ std::string LoadILLBase::getInstrumentDefinitionFilePath() {
   return fullPath.toString();
 }
 
+/**
+ * @brief LoadILLBase::bootstrap prepares and configures the loader
+ * The instrument, acquisition mode are resolved first.
+ * Then, the schema of mandatory metadata is validated.
+ * The reason for this is that depending on instrument and acquisition mode, there might be some extra entries that are
+ * mandatory. Only when this validation passes, it builds the workspace as provided by the overridden pure virtual
+ * method. There again, the type, size and the shape of the workspace depends on the mode and the instrument.
+ * Once the workspace is instantiated, it resolves the start time and loads the instrument.
+ */
 void LoadILLBase::bootstrap() {
   const std::string filename = getPropertyValue("Filename");
   PropertyManager_sptr pmp = getProperty("PatchNexusMetadataEntries");
@@ -59,6 +81,9 @@ void LoadILLBase::bootstrap() {
   loadInstrument();
 }
 
+/**
+ * @brief LoadILLBase::addSampleLogs adds all the metadata from nexus to the output workspace
+ */
 void LoadILLBase::addSampleLogs() {
   NXhandle nxHandle;
   NXstatus nxStat = NXopen(getPropertyValue("Filename").c_str(), NXACC_READ, &nxHandle);
@@ -78,6 +103,13 @@ void LoadILLBase::addSampleLogs() {
   }
 }
 
+/**
+ * @brief LoadILLBase::patchSampleLogs adds/overrides sample logs as provided in the dictionary
+ * This is useful if one needs to override those metadata, which are just sample logs.
+ * In other words, those, that are not used in the loader itself, but are put in the logs for future use later in data
+ * reduction. This way there is more flexibility. When overriding the sample logs, the keys are the names of the logs,
+ * not to be confused with nexus entries.
+ */
 void LoadILLBase::patchSampleLogs() {
   const PropertyManager_sptr logsToPatch = getProperty("PatchWorkspaceSampleLogs");
   const auto properties = logsToPatch->getProperties();
@@ -97,6 +129,9 @@ void LoadILLBase::patchSampleLogs() {
   }
 }
 
+/**
+ * @brief LoadILLBase::loadInstrument loads the instrument to the workspace
+ */
 void LoadILLBase::loadInstrument() {
   const std::string idf = getInstrumentDefinitionFilePath();
   auto loadInst = createChildAlgorithm("LoadInstrument");
@@ -106,6 +141,11 @@ void LoadILLBase::loadInstrument() {
   loadInst->execute();
 }
 
+/**
+ * @brief LoadILLBase::resolveStartTime resolves the start time in ISO format
+ * It is necessary to add it in the right format to the logs prior to loading the instrument.
+ * This way one can benefit from time-resolved values of instrument parameters.
+ */
 void LoadILLBase::resolveStartTime() {
   const std::string startTime = "start_time";
   m_timestamp = m_helper->dateTimeInIsoFormat(m_nxroot->openFirstEntry().getString(startTime));
@@ -121,6 +161,11 @@ void LoadILLBase::resolveStartTime() {
   }
 }
 
+/**
+ * @brief LoadILLBase::resolveInstrument resolves the name of the instrument
+ * Optionally, appends the variant, if overridden.
+ * @return the name of fully resolved instrument as mantid knows it
+ */
 std::string LoadILLBase::resolveInstrument() {
   NXEntry firstEntry = m_nxroot->openFirstEntry();
   const std::string instrumentPath = m_helper->findInstrumentNexusPath(firstEntry);
@@ -129,10 +174,15 @@ std::string LoadILLBase::resolveInstrument() {
   return instrumentName + resolveVariant();
 }
 
+/**
+ * @brief LoadILLBase::exec executes the core logic.
+ * This method is final and cannot be overridden.
+ * The sequence of what it does is not commutative.
+ */
 void LoadILLBase::exec() {
   bootstrap();
-  configureBeamline();
   loadAndFillData();
+  configureBeamline();
   addSampleLogs();
   patchSampleLogs();
   setOutputWorkspace();
