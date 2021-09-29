ddfc26d68016f5e9aa4eac91287642ba4c46a27d
David Fairbrother <DavidFair@users.noreply.github.com>
2021-09-29 12:11:30 +0100

    Set toolbar into zoom mode by default in constructor
    Sets the toolbar to zoom to match InstrumentView. This does not
    currently handle the empty workspace case, which we can add in the
    future.
    
    Re #32566

--- qt/scientific_interfaces/ISISReflectometry/GUI/Preview/PreviewPresenter.cpp
+++ qt/scientific_interfaces/ISISReflectometry/GUI/Preview/PreviewPresenter.cpp
@@ -19,6 +19,7 @@ PreviewPresenter::PreviewPresenter(Dependencies dependencies)
   m_view->subscribe(this);
   m_jobManager->subscribe(this);
   // TODO disable buttons when the workspace is not loaded initially
+  notifyInstViewZoomRequested();
 }
 
 /** Notification received when the user has requested to load a workspace. If it already exists in the ADS
