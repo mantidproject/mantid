b59538c5e5ec26a02a2037995fcab465565cdbe8
Pete Peterson <petersonpf@ornl.gov>
2021-10-05 11:41:07 -0400

    Finalize fork release

--- buildconfig/CMake/VersionNumber.cmake
+++ buildconfig/CMake/VersionNumber.cmake
@@ -13,7 +13,7 @@ set ( VERSION_PATCH 0 )
 # examples: First release cadidate for tweak 1 is "-1-rc.1"
 #           Second release cadidate for tweak 1 is "-1-rc.2"
 #           Actual tweak release is "-1"
-set ( VERSION_TWEAK "-3-rc.2" )
+set ( VERSION_TWEAK "-3" )
 
 # pep440 is incompatible with semantic versioning
 # https://www.python.org/dev/peps/pep-0440/
