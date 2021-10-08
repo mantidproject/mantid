0bb63206c5bf21d111e9154a68830f8c2acd7c62
Pete Peterson <petersonpf@ornl.gov>
2021-10-08 16:02:02 -0400

    Reformat cmake files according to configuration
    This is a necessary second step after adding the pre-commit hook

--- buildconfig/CMake/FindQScintillaQt5.cmake
+++ buildconfig/CMake/FindQScintillaQt5.cmake
@@ -1,5 +1,5 @@
 # Try to find QScintilla that is linked against Qt4
 #
 # Adds an imported target Qt5::Qscintilla
-include ( QScintillaFindImpl )
-find_qscintilla (5)
+include(QScintillaFindImpl)
+find_qscintilla(5)
