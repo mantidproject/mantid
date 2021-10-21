2ce04fd0695af076acf69706466820a2c4cf861b
Danny Hindson <danny.hindson@stfc.ac.uk>
2021-10-21 14:44:14 +0100

    Couple of improvements
    Add extra check if overlap between workspaces is zero
    When checking if overlap detector list is same as the sample list
    temporarily sort the lists first (ditto for similar check on V det list)

--- scripts/Diffraction/isis_powder/pearl_routines/pearl_algs.py
+++ scripts/Diffraction/isis_powder/pearl_routines/pearl_algs.py
@@ -55,12 +55,14 @@ def apply_vanadium_absorb_corrections(van_ws, run_details, absorb_ws=None):
     van_det_id_list = generate_det_id_list(van_ws)
 
     common_det_ids = [det_id for det_id in abs_det_id_list if det_id in van_det_id_list]
+    if not common_det_ids:
+        raise RuntimeError("No common detectors in Vanadium and sample workspaces")
 
     MSG_STEM = "Vanadium workspace and absorption workspaces have different spectra. "
-    if common_det_ids != van_det_id_list:
+    if sorted(common_det_ids) != sorted(van_det_id_list):
         logger.warning(MSG_STEM + "Removing unmatched spectra from the Vanadium workspace")
         van_ws = mantid.ExtractSpectra(InputWorkspace=van_ws, DetectorList=common_det_ids)
-    if common_det_ids != abs_det_id_list:
+    if sorted(common_det_ids) != sorted(abs_det_id_list):
         logger.warning(MSG_STEM + "Removing unmatched spectra from the absorption workspace")
         absorb_ws = mantid.ExtractSpectra(InputWorkspace=absorb_ws, DetectorList=common_det_ids)
 
