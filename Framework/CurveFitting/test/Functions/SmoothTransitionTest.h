18c0a90bb59a1f82e604dac8aabddd64e38dd843
Sarah Foxley <sarah.foxley@stfc.ac.uk>
2021-11-02 14:35:38 +0000

    Updated SmoothTransitionTest
    Updated formula for SmoothTransition function to match source file.
    Added a test for the Jacobians
    
    re #32580

--- Framework/CurveFitting/test/Functions/SmoothTransitionTest.h
+++ Framework/CurveFitting/test/Functions/SmoothTransitionTest.h
@@ -11,6 +11,7 @@
 #include "MantidAPI/AlgorithmManager.h"
 #include "MantidAPI/FunctionFactory.h"
 #include "MantidCurveFitting/Functions/SmoothTransition.h"
+#include "MantidCurveFitting/Jacobian.h"
 #include "MantidTestHelpers/WorkspaceCreationHelper.h"
 
 // using namespace Mantid;
@@ -33,30 +34,65 @@ public:
   }
 
   void test_function_gives_expected_value_for_given_input() {
-    SmoothTransition st;
-    st.initialize();
+    auto st = createTestSmoothTransition();
 
-    TS_ASSERT_THROWS(st.setParameter("mid", 1.0), const std::invalid_argument &);
-    TS_ASSERT_THROWS(st.setParameter("A9", 1.0), const std::invalid_argument &);
+    TS_ASSERT_THROWS(st->setParameter("mid", 1.0), const std::invalid_argument &);
+    TS_ASSERT_THROWS(st->setParameter("A9", 1.0), const std::invalid_argument &);
 
-    const double a1 = 2.3;
-    const double a2 = 4.0;
-    const double midpoint = 7.2;
-    const double gr = 1.0;
-
-    st.setParameter("A1", a1);
-    st.setParameter("A2", a2);
-    st.setParameter("Midpoint", midpoint);
-    st.setParameter("GrowthRate", gr);
+    const double a1 = st->getParameter("A1");
+    const double a2 = st->getParameter("A2");
+    const double midpoint = st->getParameter("Midpoint");
+    const double gr = st->getParameter("GrowthRate");
 
     const std::size_t numPoints = 100;
     std::array<double, numPoints> xValues;
     std::iota(xValues.begin(), xValues.end(), 0);
     std::array<double, numPoints> yValues;
-    st.function1D(yValues.data(), xValues.data(), numPoints);
+    st->function1D(yValues.data(), xValues.data(), numPoints);
 
     for (size_t i = 0; i < numPoints; i++) {
-      TS_ASSERT_DELTA(yValues[i], a2 + (a1 - a2) / (exp(-(xValues[i] - midpoint) / gr) + 1), 1e-12);
+      TS_ASSERT_DELTA(yValues[i], a2 + (a1 - a2) / (exp((xValues[i] - midpoint) / gr) + 1), 1e-12);
     }
+  }
+
+  void test_jacobian_gives_expected_values() {
+    auto st = createTestSmoothTransition();
+
+    const size_t nData(1);
+    std::vector<double> xValues(nData, 3.5);
+
+    Mantid::CurveFitting::Jacobian jacobian(nData, 4);
+    st->functionDeriv1D(&jacobian, xValues.data(), nData);
+
+    double dfda1 = jacobian.get(0, 0);
+    double dfda2 = jacobian.get(0, 1);
+    double dfdmp = jacobian.get(0, 2);
+    double dfdgr = jacobian.get(0, 3);
+
+    TS_ASSERT_DELTA(dfda1, 1.0, 1e-8);
+    TS_ASSERT_DELTA(dfda2, 0.0241270214, 1e-8);
+    TS_ASSERT_DELTA(dfdmp, -0.0400263440, 1e-8);
+    TS_ASSERT_DELTA(dfdgr, 0.1480974729, 1e-8);
+  }
+
+private:
+  class TestableSmoothTransition : public SmoothTransition {
+  public:
+    void function1D(double *out, const double *xValues, const size_t nData) const override {
+      SmoothTransition::function1D(out, xValues, nData);
+    }
+    void functionDeriv1D(Mantid::API::Jacobian *out, const double *xValues, const size_t nData) override {
+      SmoothTransition::functionDeriv1D(out, xValues, nData);
+    }
+  };
+
+  std::shared_ptr<TestableSmoothTransition> createTestSmoothTransition() {
+    auto func = std::make_shared<TestableSmoothTransition>();
+    func->initialize();
+    func->setParameter("A1", 2.3);
+    func->setParameter("A2", 4.0);
+    func->setParameter("Midpoint", 7.2);
+    func->setParameter("GrowthRate", 1.0);
+    return func;
   }
 };
