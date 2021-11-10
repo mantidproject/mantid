b68c492fd4d89cfb2ea71e866f5eb135f0bb4d2d
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-11-10 09:23:40 +0000

    Install missing plugins

--- buildconfig/CMake/BundleOSXConda.cmake
+++ buildconfig/CMake/BundleOSXConda.cmake
@@ -1,3 +1,9 @@
+macro(install_qt5_target_plugins _qt5_target_plugins _prefix)
+  foreach(plugin ${_qt5_target_plugins})
+    install_qt5_plugin_osx(${plugin} QT_PLUGINS ${_prefix})
+  endforeach()
+endmacro()
+
 macro(install_qt5_plugin_osx _qt_plugin_name _qt_plugins_var _prefix)
   get_target_property(_qt_plugin_path "${_qt_plugin_name}" LOCATION)
   if(EXISTS "${_qt_plugin_path}")
@@ -11,18 +17,36 @@ macro(install_qt5_plugin_osx _qt_plugin_name _qt_plugins_var _prefix)
       COMPONENT Runtime
     )
     set(${_qt_plugins_var}
-        "${${_qt_plugins_var}};\$ENV{DEST_DIR}\${CMAKE_INSTALL_PREFIX}/${_qt_plugin_dest}/${_qt_plugin_file}"
+        "${${_qt_plugins_var}};\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_qt_plugin_dest}/${_qt_plugin_file}"
     )
   else()
     message(FATAL_ERROR "QT plugin ${_qt_plugin_name} not found")
   endif()
 endmacro()
 
+# Need to import the following plugin targets Sql, PrintSupport and Svg
+find_package(
+  Qt5
+  COMPONENTS Sql PrintSupport Svg
+  REQUIRED
+)
+
+set(prefix "${WORKBENCH_APP}/Contents")
+# Qt5 GUI plugins - platforms, platform themes, icons ect.
+install_qt5_target_plugins("${Qt5Gui_PLUGINS}" ${prefix})
+# Widgets plugins - includes styling plugins
+install_qt5_target_plugins("${Qt5Widgets_PLUGINS}" ${prefix})
+# Rest of required plugins
+install_qt5_target_plugins("${Qt5Svg_PLUGINS}" ${prefix})
+install_qt5_target_plugins("${Qt5Sql_PLUGINS}" ${prefix})
+install_qt5_target_plugins("${Qt5PrintSupport_PLUGINS}" ${prefix})
+
+# Install dependencies for Mantid libraries and qt plugins
 install(CODE "set(WORKBENCH_APP \"${WORKBENCH_APP}\")" COMPONENT Runtime)
-# Manually sort out the dependencies using cmake
 install(
   CODE [[ file(GET_RUNTIME_DEPENDENCIES EXECUTABLES $<TARGET_FILE:MantidWorkbenchInstalled>
-LIBRARIES $<TARGET_FILE:Qt5::QCocoaIntegrationPlugin> $<TARGET_FILE:Qt5::QMacStylePlugin> $<TARGET_FILE:Kernel> $<TARGET_FILE:API> $<TARGET_FILE:Json> $<TARGET_FILE:mantidqt_commonqt5> $<TARGET_FILE:LiveData>
+LIBRARIES ${QT_PLUGINS} $<TARGET_FILE:Qt5::QCocoaIntegrationPlugin> $<TARGET_FILE:Qt5::QMacStylePlugin> $<TARGET_FILE:Qt5::QSvgPlugin> $<TARGET_FILE:Qt5::QSQLiteDriverPlugin> $<TARGET_FILE:Qt5::QCocoaPrinterSupportPlugin>
+$<TARGET_FILE:Kernel> $<TARGET_FILE:API> $<TARGET_FILE:Json> $<TARGET_FILE:mantidqt_commonqt5> $<TARGET_FILE:LiveData>
 RESOLVED_DEPENDENCIES_VAR _r_deps UNRESOLVED_DEPENDENCIES_VAR _u_deps
 DIRECTORIES ${MY_DEPENDENCY_PATHS} )
 foreach(_file ${_r_deps})
@@ -40,6 +64,3 @@ message(WARNING
 ]]
   COMPONENT Runtime
 )
-
-install_qt5_plugin_osx("Qt5::QCocoaIntegrationPlugin" QT_PLUGINS ${WORKBENCH_APP}/Contents/)
-install_qt5_plugin_osx("Qt5::QMacStylePlugin" QT_PLUGINS ${WORKBENCH_APP}/Contents/)
