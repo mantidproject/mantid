8d8d3997a3b8daaac32f8027f40b7ad8cb279952
Gemma Guest <gemma.guest@stfc.ac.uk>
2021-11-11 10:46:18 +0000

    Fix compiler error from merge conflicts
    Re #32772

--- qt/widgets/common/test/AlgorithmRuntimePropsTest.h
+++ qt/widgets/common/test/AlgorithmRuntimePropsTest.h
@@ -5,8 +5,8 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
+#include "MantidFrameworkTestHelpers/WorkspaceCreationHelper.h"
 #include "MantidQtWidgets/Common/AlgorithmRuntimeProps.h"
-#include "MantidTestHelpers/WorkspaceCreationHelper.h"
 
 #include <cxxtest/TestSuite.h>
 
