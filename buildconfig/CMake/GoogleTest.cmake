7bd95443d37c035433c033f37a339dcf955668df
David Fairbrother <DavidFair@users.noreply.github.com>
2021-11-19 16:54:27 +0000

    Bump gtest to 1.11
    Bumps gtest from 1.10 to 1.11
    This allows us to compile with newer compilers (such as clang-13) which
    reject implicitly declared copy ctor's found in the framework.
    
    This has been fixed upstream for a while, but the project hasn't picked
    it up because of breakage (see previous commit). Now I'm forced to
    because of wanting to use an OpenMP thread aware sanitizer included in
    newer versions of clang

--- buildconfig/CMake/GoogleTest.cmake
+++ buildconfig/CMake/GoogleTest.cmake
@@ -2,7 +2,7 @@
 
 # Make gtest_version available everywhere
 set(gtest_version
-    "1.10.0"
+    "1.11.0"
     CACHE INTERNAL ""
 )
 
@@ -17,7 +17,7 @@ set(gtest_force_shared_crt
 fetchcontent_declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG release-1.10.0
+  GIT_TAG release-1.11.0
   GIT_SHALLOW TRUE
 )
 
