0960ae810186d0decc3756cd5ca1bce52ad5258a
David Fairbrother <DavidFair@users.noreply.github.com>
2021-11-22 16:59:40 +0000

    Remove option to use system Eigen
    Remove the option to use System Eigen so we get nice reproducable
    builds, especially as we switch to Conda.
    
    This means we implicitly opt MSVC clients into building Eigen rather
    than using a pre-built version

--- buildconfig/CMake/Eigen.cmake
+++ buildconfig/CMake/Eigen.cmake
@@ -1,30 +1,19 @@
 include(ExternalProject)
 
-option(USE_SYSTEM_EIGEN "Use the system installed Eigen - v${eigen_version}?" OFF)
+message(STATUS "Using Eigen in ExternalProject")
 
-if(WIN32)
-  # Installed by 3rd party dependencies bundle
-  set(USE_SYSTEM_EIGEN ON)
-endif()
+# Download and unpack Eigen at configure time
+configure_file(${CMAKE_SOURCE_DIR}/buildconfig/CMake/Eigen.in ${CMAKE_BINARY_DIR}/extern-eigen/CMakeLists.txt)
 
-if(USE_SYSTEM_EIGEN)
-  message(STATUS "Using system Eigen")
-else()
-  message(STATUS "Using Eigen in ExternalProject")
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" -DCMAKE_SYSTEM_VERSION=${CMAKE_SYSTEM_VERSION} .
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/extern-eigen
+)
+execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/extern-eigen)
 
-  # Download and unpack Eigen at configure time
-  configure_file(${CMAKE_SOURCE_DIR}/buildconfig/CMake/Eigen.in ${CMAKE_BINARY_DIR}/extern-eigen/CMakeLists.txt)
+set(Eigen3_DIR
+    "${CMAKE_BINARY_DIR}/extern-eigen/install/share/eigen3/cmake"
+    CACHE PATH ""
+)
 
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" -DCMAKE_SYSTEM_VERSION=${CMAKE_SYSTEM_VERSION} .
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/extern-eigen
-  )
-  execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/extern-eigen)
-
-  set(Eigen3_DIR
-      "${CMAKE_BINARY_DIR}/extern-eigen/install/share/eigen3/cmake"
-      CACHE PATH ""
-  )
-endif()
-
-find_package(Eigen3 3.2 REQUIRED)
+find_package(Eigen3 3.4 REQUIRED)
