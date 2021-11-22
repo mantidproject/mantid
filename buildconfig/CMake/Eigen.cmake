d88e9b689b8b4eb449ab6bfbaa205d8b067a3654
David Fairbrother <DavidFair@users.noreply.github.com>
2021-11-22 17:39:31 +0000

    Modernise CMake for Eigen
    Switches from externalproject_add to fetch content.
    On MSVC this fails to identify a C++03/11 compiler during configure, but
    because it's a header-only lib we can still compile with it regardless

--- buildconfig/CMake/Eigen.cmake
+++ buildconfig/CMake/Eigen.cmake
@@ -1,19 +1,14 @@
-include(ExternalProject)
-
-message(STATUS "Using Eigen in ExternalProject")
-
-# Download and unpack Eigen at configure time
-configure_file(${CMAKE_SOURCE_DIR}/buildconfig/CMake/Eigen.in ${CMAKE_BINARY_DIR}/extern-eigen/CMakeLists.txt)
-
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" -DCMAKE_SYSTEM_VERSION=${CMAKE_SYSTEM_VERSION} .
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/extern-eigen
-)
-execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/extern-eigen)
-
-set(Eigen3_DIR
-    "${CMAKE_BINARY_DIR}/extern-eigen/install/share/eigen3/cmake"
-    CACHE PATH ""
+include(FetchContent)
+fetchcontent_declare(
+  Eigen
+  GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+  GIT_TAG 3.4.0
+  GIT_SHALLOW TRUE
+  GIT_PROGRESS TRUE
 )
 
-find_package(Eigen3 3.4 REQUIRED)
+option(EIGEN_BUILD_DOC OFF)
+option(BUILD_TESTING OFF)
+option(EIGEN_LEAVE_TEST_IN_ALL_TARGET OFF)
+option(EIGEN_BUILD_PKGCONFIG OFF)
+fetchcontent_makeavailable(Eigen)
