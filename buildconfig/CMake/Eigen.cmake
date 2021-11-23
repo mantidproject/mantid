224a9658b5bcfaff4d2b4fdf4b6a08564ff7d862
David Fairbrother <DavidFair@users.noreply.github.com>
2021-11-23 11:51:16 +0000

    Fix CMake defaulting to /usr/local for the install directory
    OSX was failing as it didn't know where to install the headers to. More fundamentally we
    shouldn't be polluting the system with Mantid headers, so manually set the location
    before calling FetchContent_MakeAvailable

--- buildconfig/CMake/Eigen.cmake
+++ buildconfig/CMake/Eigen.cmake
@@ -1,3 +1,4 @@
+cmake_minimum_required(VERSION 3.14) # For FetchContent_MakeAvailable
 include(FetchContent)
 fetchcontent_declare(
   Eigen
@@ -11,4 +12,23 @@ option(EIGEN_BUILD_DOC OFF)
 option(BUILD_TESTING OFF)
 option(EIGEN_LEAVE_TEST_IN_ALL_TARGET OFF)
 option(EIGEN_BUILD_PKGCONFIG OFF)
+
+# Preserve shared attrs
+set(CMAKE_INSTALL_PREFIX_OLD ${CMAKE_INSTALL_PREFIX})
+set(CMAKE_BUILD_TYPE_OLD ${CMAKE_BUILD_TYPE})
+
+# Install to build/_deps/eigen-install
+set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/_deps/eigen-install")
+set(CMAKE_BUILD_TYPE Release)
+
 fetchcontent_makeavailable(Eigen)
+
+# Restore shared attrs to not affect main mantid project
+set(CMAKE_INSTALL_PREFIX
+    ${CMAKE_INSTALL_PREFIX_OLD}
+    CACHE STRING "Install path" FORCE
+)
+set(CMAKE_BUILD_TYPE
+    ${CMAKE_BUILD_TYPE_OLD}
+    CACHE STRING "Choose the type of build" FORCE
+)
