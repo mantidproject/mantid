1de7c63f2a866e3e1c9dd47a83ce17f804a19a46
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-06 14:40:14 +0000

    Fix cppcheck from header includes
    For everything but data service, increase line position by 1 for new
    header.
    For DataService.h, fix the warnings since we've already done substantial
    work on this file anyway

--- Framework/Kernel/inc/MantidKernel/DataService.h
+++ Framework/Kernel/inc/MantidKernel/DataService.h
@@ -359,11 +359,8 @@ public:
 
   /// Checks all elements within the specified vector exist in the ADS
   bool doAllWsExist(const std::vector<std::string> &listOfNames) {
-    for (const auto &wsName : listOfNames) {
-      if (!doesExist(wsName))
-        return false;
-    }
-    return true;
+    return std::all_of(listOfNames.cbegin(), listOfNames.cend(),
+                       [this](const std::string &wsName) { return doesExist(wsName); });
   }
 
   /// Check to see if a data object exists in the store
@@ -381,12 +378,8 @@ public:
     if (showingHiddenObjects()) {
       return datamap.size();
     } else {
-      size_t count = 0;
-      for (auto &it : datamap) {
-        if (!isHiddenDataServiceObject(it.first))
-          ++count;
-      }
-      return count;
+      return std::count_if(datamap.cbegin(), datamap.cend(),
+                           [this](const auto &it) { return !isHiddenDataServiceObject(it.first); });
     }
   }
 
