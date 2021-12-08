5dfd0d523052cd810ee02afaf5d4e78ed6bd5ed6
Gemma Guest <gemma.guest@stfc.ac.uk>
2021-12-08 14:55:10 +0000

    Add outline widget for region selector

---
+++ qt/python/mantidqt/mantidqt/widgets/regionselector/view.py
@@ -0,0 +1,17 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantid workbench.
+from qtpy.QtWidgets import (QWidget)
+from qtpy.QtCore import Qt
+
+
+class RegionSelectorView(QWidget):
+    """The view for the data portion of the sliceviewer"""
+
+    def __init__(self, presenter, parent=None):
+        super().__init__(parent)
+        self.setWindowFlags(Qt.Window)
