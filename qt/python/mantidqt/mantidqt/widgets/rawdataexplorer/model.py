bca86491fbeefff89f0b2b3c01702f5a3ed78945
Mathieu Tillet <tillet@ill.eu>
2021-12-09 09:53:58 +0100

    [RDEXP] Add a memory manager
    Add a manager so the cached data do not eat all the available memory.
    It tries to smartly delete workspace if they are no needed anymore.

--- qt/python/mantidqt/mantidqt/widgets/rawdataexplorer/model.py
+++ qt/python/mantidqt/mantidqt/widgets/rawdataexplorer/model.py
@@ -9,9 +9,11 @@
 import os.path
 from qtpy.QtCore import *
 
-from mantid.simpleapi import Load, config, mtd, Plus, RenameWorkspace
+from mantid.simpleapi import Load, config, mtd, Plus, RenameWorkspace, DeleteWorkspace
 from mantid.kernel import logger
 
+from ...utils.asynchronous import set_interval
+from ..memorywidget.memoryinfo import get_memory_info
 from .PreviewFinder import PreviewFinder, AcquisitionType
 
 
@@ -60,6 +62,87 @@ class PreviewModel(QObject):
         return self._workspace_name
 
 
+class MemoryManager(QObject):
+    """
+    Gets system memory usage information and manages the caching of the workspaces loaded by the raw data explorer.
+    It keeps track of the loaded workspaces and delete the oldest ones if the memory goes above MEMORY_THRESHOLD percent
+    Update is made every TIME_INTERVAL_MEMORY_USAGE_UPDATE (s) using threading.Timer;
+    """
+
+    TIME_INTERVAL_MEMORY_USAGE_UPDATE = 5.000  # in s
+    MEMORY_THRESHOLD = 80  # in percentage of the total
+
+    sig_free_memory = Signal()
+
+    def __init__(self, rdexp_model):
+        super().__init__()
+
+        self.rdexp_model = rdexp_model
+        self._current_workspaces = []
+
+        self.update_allowed = True
+        self.update_memory_usage()
+        self.thread_stopper = self.update_memory_usage_threaded()
+        self.sig_free_memory.connect(self.on_free_requested)
+
+    def __del__(self):
+        self.cancel_memory_update()
+
+    @set_interval(TIME_INTERVAL_MEMORY_USAGE_UPDATE)
+    def update_memory_usage_threaded(self):
+        """
+        Calls update_memory_usage once every TIME_INTERVAL_MEMORY_USAGE_UPDATE
+        If the memory is too high, ask for workspace deletion.
+        """
+        mem_used_percent = self.update_memory_usage()
+        if mem_used_percent > self.MEMORY_THRESHOLD:
+            self.sig_free_memory.emit()
+
+    def update_memory_usage(self):
+        """
+        Gets memory usage information and passes it to the view
+        """
+        if self.update_allowed:
+            mem_used_percent, mem_used, mem_avail = get_memory_info()
+            return mem_used_percent
+
+    def cancel_memory_update(self):
+        """
+        Ensures that the thread will not restart after it finishes next, as well as attempting to cancel it.
+        """
+        self.update_allowed = False
+        self.thread_stopper.set()
+
+    def workspace_interacted_with(self, ws_name):
+        """
+        If a workspace is used for a preview, it is considered interacted with and
+        thus moved at the end of the queue for deletion
+        @param ws_name: the name of the workspace
+        """
+        try:
+            # if the workspace already exists, we remove it
+            self._current_workspaces.remove(ws_name)
+        except ValueError:
+            pass
+        self._current_workspaces.append(ws_name)
+
+    def on_free_requested(self):
+        """
+        Slot called when a workspace needs to be deleted to free memory.
+        It will delete the oldest workspace managed by the raw data explorer that is not currently shown in a preview.
+        """
+        index = 0
+        while index < len(self._current_workspaces):
+            ws_name = self._current_workspaces[index]
+            if self.rdexp_model.can_delete_workspace(ws_name):
+                self._current_workspaces.pop(index)
+                if mtd.doesExist(ws_name):
+                    logger.information("Deleting cached workspace ", ws_name)
+                    DeleteWorkspace(Workspace=ws_name)
+                    break
+            index += 1
+
+
 class RawDataExplorerModel(QObject):
     """
     Model for the RawDataExplorer widget
@@ -89,7 +172,7 @@ class RawDataExplorerModel(QObject):
 
         self.instrument = config["default.instrument"]
 
-        self.current_workspaces = []
+        self.memory_manager = MemoryManager(self)
 
     def on_instrument_changed(self, new_instrument):
         """
@@ -98,26 +181,6 @@ class RawDataExplorerModel(QObject):
         """
         self.instrument = new_instrument
         self.presenter.populate_acquisitions()
-        # TODO emit a signal to modify both preview manager and target
-
-    def new_preview(self, filename):
-        """
-        Add a preview to the model.
-        @param filename: the full path to the file to show
-        """
-
-        ws_name = os.path.basename(filename)[:-4]
-        if not mtd.doesExist(ws_name):
-            Load(Filename=filename, OutputWorkspace=ws_name)
-
-        preview = self.choose_preview(ws_name)
-
-        if preview is None:
-            return
-
-        preview_model = PreviewModel(preview, ws_name)
-        self._previews.append(preview_model)
-        self.sig_new_preview.emit(preview_model)
 
     def modify_preview(self, filename):
         """
@@ -126,8 +189,11 @@ class RawDataExplorerModel(QObject):
         """
 
         ws_name = os.path.basename(filename)[:-4]
+
         if not mtd.doesExist(ws_name):
             Load(Filename=filename, OutputWorkspace=ws_name)
+
+        self.memory_manager.workspace_interacted_with(ws_name)
 
         # determine the preview given the prepared data
         preview = self.choose_preview(ws_name)
@@ -141,6 +207,7 @@ class RawDataExplorerModel(QObject):
                     ws_name = self.accumulate(current_preview.get_workspace_name(), ws_name)
                     if ws_name is None:
                         return
+                    self.memory_manager.workspace_interacted_with(ws_name)
                 current_preview.set_workspace_name(ws_name)
                 return
 
@@ -154,6 +221,17 @@ class RawDataExplorerModel(QObject):
         @param previewModel(PreviewModel): reference to the model.
         """
         self._previews.remove(previewModel)
+
+    def can_delete_workspace(self, ws_name):
+        """
+        Checks if a workspace is not shown in any preview and thus can be deleted.
+        @param ws_name: the name of the workspace
+        @return whether the workspace can safely be deleted
+        """
+        for preview in self._previews:
+            if preview.get_workspace_name() == ws_name:
+                return False
+        return True
 
     def choose_preview(self, ws_name):
         """
