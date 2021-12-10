176bc6d95a7f02df4360ce07370ef834305e0804
Anthony Lim <anthony.lim@stfc.ac.uk>
2021-12-10 17:41:44 +0000

    refs #32732 can now select between field and freq in FDA

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/Muon/GUI/Common/utilities/algorithm_utils.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Muon/GUI/Common/utilities/algorithm_utils.py
@@ -248,7 +248,7 @@ def run_Plus(parameter_dict):
     return alg.getProperty("OutputWorkspace").value
 
 
-def convert_to_field(workspace_name):
+def convert_to_field(workspace_name, output_name):
     """
     Apply the ConvertAxisByFormula algorithm to convert from MHz to Field.
     """
@@ -256,10 +256,26 @@ def convert_to_field(workspace_name):
     alg.initialize()
     alg.setAlwaysStoreInADS(True)
     alg.setProperty("InputWorkspace", workspace_name)
-    alg.setProperty("OutputWorkspace", workspace_name)
+    alg.setProperty("OutputWorkspace", output_name)
     alg.setProperty("Formula", 'x * 1. / '+str(const.MuonGyromagneticRatio))
     alg.setProperty("AxisTitle", 'Field')
     alg.setProperty('AxisUnits', 'Gauss')
+    alg.execute()
+    return alg.getProperty("OutputWorkspace").valueAsStr
+
+
+def convert_to_freq(workspace_name, output_name):
+    """
+    Apply the ConvertAxisByFormula algorithm to convert from MHz to Field.
+    """
+    alg = mantid.AlgorithmManager.create("ConvertAxisByFormula")
+    alg.initialize()
+    alg.setAlwaysStoreInADS(True)
+    alg.setProperty("InputWorkspace", workspace_name)
+    alg.setProperty("OutputWorkspace", output_name)
+    alg.setProperty("Formula", 'x * '+str(const.MuonGyromagneticRatio))
+    alg.setProperty("AxisTitle", 'Frequency')
+    alg.setProperty('AxisUnits', 'MHz')
     alg.execute()
     return alg.getProperty("OutputWorkspace").valueAsStr
 
--- qt/python/mantidqtinterfaces/mantidqtinterfaces/Muon/GUI/FrequencyDomainAnalysis/plot_widget/plot_freq_fit_pane_presenter.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Muon/GUI/FrequencyDomainAnalysis/plot_widget/plot_freq_fit_pane_presenter.py
@@ -4,17 +4,30 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 from mantidqtinterfaces.Muon.GUI.Common.plot_widget.fit_pane.plot_fit_pane_presenter import PlotFitPanePresenter
+from mantidqt.utils.observer_pattern import GenericObservable
 
 
 class PlotFreqFitPanePresenter(PlotFitPanePresenter):
 
     def __init__(self, view, model, context, fitting_context, figure_presenter):
         super().__init__(view, model, context, fitting_context, figure_presenter)
-        self._data_type = ["Frequency"]
+        self._data_type = ["Frequency", "Field"]
+        self.context._frequency_context.x_label = "Frequency"
         self._sort_by = [""]
         self.update_view()
         self._view.hide_plot_raw()
         self._view.hide_tiled_by()
+        self.update_freq_units = GenericObservable()
+
+    def handle_data_type_changed(self):
+        """
+        Handles the data type being changed in the view by plotting the workspaces corresponding to the new data type
+        """
+        self.context._frequency_context.x_label = self._view.get_plot_type()
+        self.update_freq_units.notify_subscribers()
+        self._figure_presenter.set_plot_range(self.context._frequency_context.range())
+        # need to send signal out to update stuff => dont undate here
+        # the slot will update the plot when the fit list updates
 
     def handle_rebin_options_changed(self):
         # there is no way to rebin the data -> do nothing
