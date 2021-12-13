b04e8c1079bfa251cc34c09945a810a57ca561e2
Pete Peterson <petersonpf@ornl.gov>
2021-12-13 14:56:11 -0500

    Finalizing v6.2.0-3 release

--- buildconfig/CMake/VersionNumber.cmake
+++ buildconfig/CMake/VersionNumber.cmake
@@ -9,7 +9,7 @@ set(VERSION_PATCH 0)
 
 # The tweak is mean to keep in line with the pre-release numbering. https://semver.org/ examples: First release cadidate
 # for tweak 1 is "-1-rc.1" Second release cadidate for tweak 1 is "-1-rc.2" Actual tweak release is "-1"
-set(VERSION_TWEAK "-3-rc.2")
+set(VERSION_TWEAK "-3")
 
 # pep440 is incompatible with semantic versioning https://www.python.org/dev/peps/pep-0440/ example: First release
 # cadidate for tweak 1 is ".1rc.1"
