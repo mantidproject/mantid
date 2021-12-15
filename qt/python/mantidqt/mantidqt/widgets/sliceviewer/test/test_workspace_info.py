6cd803b9ac187686b361b1424253609190f728b9
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-15 12:00:21 +0000

    Add tests for Sliceviewer Model init
    Adds some missing tests, which were implicitly asserted by get_dim_info,
    that init rejects <2 bins / spectra ...etc.

---
+++ qt/python/mantidqt/mantidqt/widgets/sliceviewer/test/test_workspace_info.py
@@ -0,0 +1,14 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+import unittest
+
+
+class TestWorkspaceInfo(unittest.TestCase):
+    pass
+
+
+if __name__ == '__name':
+    unittest.main()
