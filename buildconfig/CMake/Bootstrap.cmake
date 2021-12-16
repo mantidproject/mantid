f6f0a4da5b4cacb972e110c6daed3aa44c92c8b2
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2021-12-16 13:54:09 +0000

    Replace internal Inelastic fortran modules
    Use external quasielasticbayes package instead.
    This uses the same sources but allows the package
    to be installed externally on other platforms where
    prebuilt binaries are not shipped with mantid
    
    Some bugs in the fortran code were fixed in the
    external package. This has given small changes in
    peak heights but not shapes.

--- buildconfig/CMake/Bootstrap.cmake
+++ buildconfig/CMake/Bootstrap.cmake
@@ -13,7 +13,7 @@ if(MSVC AND NOT CONDA_ENV)
       CACHE PATH "Location to clone third party dependencies to"
   )
   set(THIRD_PARTY_GIT_URL "https://github.com/mantidproject/thirdparty-msvc2015.git")
-  set(THIRD_PARTY_GIT_SHA1 1f44d901442239ae12c11e33187b3d877bb9f2b2)
+  set(THIRD_PARTY_GIT_SHA1 05882d108f10d0c1715ba1e861c7fc5d231a5f2f)
   set(THIRD_PARTY_DIR ${EXTERNAL_ROOT}/src/ThirdParty)
   # Generates a script to do the clone/update in tmp
   set(_project_name ThirdParty)
