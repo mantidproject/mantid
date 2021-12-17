84dd610a1179ada725ecd067e6850357e3c27b71
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-17 12:11:57 +0000

    Add test interface to open RegionSelector
    This is a hack so we can allow interested users / devs to test the
    interface without us having to embed it at this point.
    
    Simply add:
    mantidqt.python_interfaces = Utility/RegionSelectorInterface.py
    To your Mantid.user.properties to see the interface under the utility
    tab. You'll need at least one ws in the ADS for this to work.

---
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/RegionSelectorInterface.py
@@ -0,0 +1,22 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+"""
+Test interface for RegionSelector. This will be removed once the
+widget is embedded into ISISReflectometry - Preview tab.
+"""
+from typing import List
+from mantid.api import AnalysisDataService
+
+from mantidqt.widgets.regionselector.presenter import RegionSelector
+
+
+ads = AnalysisDataService.Instance()
+workspace_names: List[str] = ads.getObjectNames()
+if len(workspace_names) < 1:
+    raise RuntimeError("Ensure at least one workspace is loaded in Mantid")
+workspace = ads.retrieve(workspace_names[0])
+presenter = RegionSelector(ws=workspace)
+presenter.view.show()
