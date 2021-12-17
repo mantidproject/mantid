7301d1c4dbe6180f1c60568f9d39508aa252262e
David Fairbrother <DavidFair@users.noreply.github.com>
2021-12-17 10:48:53 +0000

    Add missing slots to dataviewsubscriber
    Adds various missing methods which are connected to slots in the view to
    the dataviewsubscriber interface

--- qt/python/mantidqt/mantidqt/widgets/sliceviewer/views/dataviewsubscriber.py
+++ qt/python/mantidqt/mantidqt/widgets/sliceviewer/views/dataviewsubscriber.py
@@ -10,7 +10,31 @@ from abc import ABC, abstractmethod
 
 class IDataViewSubscriber(ABC):
     @abstractmethod
+    def canvas_clicked(self, event) -> None:
+        pass
+
+    @abstractmethod
+    def data_limits_changed(self):
+        pass
+
+    @abstractmethod
     def dimensions_changed(self) -> None:
+        pass
+
+    @abstractmethod
+    def line_plots(self, state: bool) -> None:
+        pass
+
+    @abstractmethod
+    def nonorthogonal_axes(self, state: bool) -> None:
+        pass
+
+    @abstractmethod
+    def region_selection(self, state: bool) -> None:
+        pass
+
+    @abstractmethod
+    def show_all_data_clicked(self) -> None:
         pass
 
     @abstractmethod
@@ -18,13 +42,5 @@ class IDataViewSubscriber(ABC):
         pass
 
     @abstractmethod
-    def canvas_clicked(self, event) -> None:
-        pass
-
-    @abstractmethod
     def zoom_pan_clicked(self, active) -> None:
-        pass
-
-    @abstractmethod
-    def show_all_data_clicked(self) -> None:
         pass
