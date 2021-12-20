45291c05ba84b18d178a5dd3b649770c72609a5b
Robert Applin <robert.applin@stfc.ac.uk>
2021-12-20 18:00:12 +0000

    Refs #0. Pass by value into constructors.

--- Framework/Algorithms/inc/MantidAlgorithms/SampleCorrections/MayersSampleCorrectionStrategy.h
+++ Framework/Algorithms/inc/MantidAlgorithms/SampleCorrections/MayersSampleCorrectionStrategy.h
@@ -46,8 +46,8 @@ public:
   };
 
   /// Constructor
-  MayersSampleCorrectionStrategy(const MayersSampleCorrectionStrategy::Parameters &params,
-                                 HistogramData::Histogram inputHist);
+  MayersSampleCorrectionStrategy(const MayersSampleCorrectionStrategy::Parameters params,
+                                 const HistogramData::Histogram inputHist);
   /// Destructor - defined in cpp file to use forward declaration with
   /// unique_ptr
   ~MayersSampleCorrectionStrategy();
--- Framework/Kernel/inc/MantidKernel/PropertyWithValue.h
+++ Framework/Kernel/inc/MantidKernel/PropertyWithValue.h
@@ -36,10 +36,10 @@ namespace Kernel {
 */
 template <typename TYPE> class DLLExport PropertyWithValue : public Property {
 public:
-  PropertyWithValue(const std::string &name, TYPE defaultValue,
+  PropertyWithValue(const std::string name, TYPE defaultValue,
                     IValidator_sptr validator = IValidator_sptr(new NullValidator),
                     const unsigned int direction = Direction::Input);
-  PropertyWithValue(const std::string &name, TYPE defaultValue, const unsigned int direction);
+  PropertyWithValue(const std::string name, TYPE defaultValue, const unsigned int direction);
   PropertyWithValue(const std::string &name, TYPE defaultValue, const std::string &defaultValueStr,
                     IValidator_sptr validator, const unsigned int direction);
   PropertyWithValue(const PropertyWithValue<TYPE> &right);
