05725e07d3110e7fbe53a950711d8cdc4853b1de
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-07 12:21:20 +0100

    Add test_log_to_python

--- Framework/PythonInterface/test/python/mantid/utils/loggingTest.py
+++ Framework/PythonInterface/test/python/mantid/utils/loggingTest.py
@@ -7,12 +7,32 @@
 
 # package imports
 from mantid.kernel import ConfigService, logger
-from mantid.utils.logging import capture_logs
+from mantid.utils.logging import capture_logs, log_to_python
 
 # standard imports
-from pathlib import Path
-import os
+from contextlib import contextmanager
+import logging
 import unittest
+
+
+class CaptureHandler(logging.Handler):
+    def __init__(self):
+        super().__init__(level=logging.DEBUG)
+        self.records = []
+
+    def emit(self, record: logging.LogRecord):
+        self.records.append(record)
+
+
+@contextmanager
+def backup_config():
+    try:
+        config = ConfigService.Instance()
+        backup = {key: config[key] for key in config.keys() if 'logging' in key}
+        yield
+    finally:
+        for key, val in backup.items():
+            config[key] = val
 
 
 class loggingTest(unittest.TestCase):
@@ -32,6 +52,28 @@ class loggingTest(unittest.TestCase):
             self.assertFalse('Debug-message' in logs.getvalue())
 
         self.assertTrue(config['logging.loggers.root.level'] == 'information')
+
+    def test_log_to_python(self):
+        py_logger = logging.getLogger('Mantid')
+        py_logger.setLevel(logging.INFO)
+        handler = CaptureHandler()
+        for hdlr in py_logger.handlers:
+            py_logger.removeHandler(hdlr)
+        py_logger.addHandler(handler)
+
+        with backup_config():
+            log_to_python()
+            logger.information('[[info]]')
+            logger.warning('[[warning]]')
+            logger.error('[[error]]')
+            logger.fatal('[[fatal]]')
+
+        self.assertListEqual([record.msg for record in handler.records],
+                             ['[[info]]', '[[warning]]', '[[error]]', '[[fatal]]'])
+        self.assertListEqual([record.levelname for record in handler.records],
+                             ['INFO', 'WARNING', 'ERROR', 'CRITICAL'])
+
+        py_logger.removeHandler(handler)
 
 
 if __name__ == '__main__':
