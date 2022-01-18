c32334a7b68e9a2394ddb15cba2b7de69e74d91b
Pete Peterson <petersonpf@ornl.gov>
2022-01-18 12:39:01 -0500

    Comment out patch and tweak numbers

--- buildconfig/CMake/VersionNumber.cmake
+++ buildconfig/CMake/VersionNumber.cmake
@@ -6,12 +6,12 @@ set(VERSION_MINOR 2)
 # UNCOMMENT the next 'set' line to 'force' the patch version number to a value (instead of using the count coming out of
 # 'git describe') DO NOT COMMIT THIS TO MASTER UNCOMMENTED, ONLY TO A RELEASE BRANCH
 
-set(VERSION_PATCH 0)
+# set(VERSION_PATCH 0)
 
 # The tweak is mean to keep in line with the pre-release numbering. https://semver.org/ examples: First release cadidate
 # for tweak 1 is "-1-rc.1" Second release cadidate for tweak 1 is "-1-rc.2" Actual tweak release is "-1"
 
-set(VERSION_TWEAK "-4-rc.4")
+# set(VERSION_TWEAK "-4-rc.4")
 
 # pep440 is incompatible with semantic versioning https://www.python.org/dev/peps/pep-0440/ example: First release
 # cadidate for tweak 1 is ".1rc.1"
