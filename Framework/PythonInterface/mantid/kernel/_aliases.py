eb9406039e635ae2629b3e3998787ee012f1c211
Pete Peterson <petersonpf@ornl.gov>
2022-01-25 13:50:00 -0500

    Undo change to factories that are not exported as dict

--- Framework/PythonInterface/mantid/kernel/_aliases.py
+++ Framework/PythonInterface/mantid/kernel/_aliases.py
@@ -86,8 +86,8 @@ def lazy_instance_access(cls, key_as_str=False):
 
 UsageService = lazy_instance_access(UsageServiceImpl)
 ConfigService = lazy_instance_access(ConfigServiceImpl, key_as_str=True)
-PropertyManagerDataService = lazy_instance_access(PropertyManagerDataServiceImpl, key_as_str=True)
-UnitFactory = lazy_instance_access(UnitFactoryImpl, key_as_str=True)
+PropertyManagerDataService = lazy_instance_access(PropertyManagerDataServiceImpl)
+UnitFactory = lazy_instance_access(UnitFactoryImpl)
 
 config = ConfigService
 pmds = PropertyManagerDataService
