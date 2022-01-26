703e0521c3ce97e49320d9cda0c470bb03d6b1c8
MialLewis <95620982+MialLewis@users.noreply.github.com>
2022-01-26 17:42:59 +0000

    Corrected Return Type in IFunction Export

--- Framework/PythonInterface/mantid/api/src/Exports/IFunction.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/IFunction.cpp
@@ -80,13 +80,13 @@ using getErrorType2 = double (IFunction::*)(const std::string &) const;
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getErrorType2_Overloads, getError, 1, 1)
 
 // declareAttribute(name, defaultValue)
-using declareAttributeType1 = double (IFunction::*)(const std::string &name,
-                                                    const API::IFunction::Attribute &defaultValue) const;
+using declareAttributeType1 = void (IFunction::*)(const std::string &name,
+                                                  const API::IFunction::Attribute &defaultValue) const;
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(declareAttributeType1_Overloads, declareAttribute, 2, 2)
 // declareAttribute(name, defaultValue, validator)
-using declareAttributeType2 = double (IFunction::*)(const std::string &name,
-                                                    const API::IFunction::Attribute &defaultValue,
-                                                    const Kernel::IValidator &validator) const;
+using declareAttributeType2 = void (IFunction::*)(const std::string &name,
+                                                  const API::IFunction::Attribute &defaultValue,
+                                                  const Kernel::IValidator &validator) const;
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(declareAttributeType2_Overloads, declareAttribute, 3, 3)
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(tie_Overloads, tie, 2, 3)
