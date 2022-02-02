dea57b25986e53bcc32543a4088e3257b1fba34c
Dominik Arominski <arominski@ill.fr>
2022-02-02 17:24:22 +0100

    [DirectILL] AutoProcess system test with single crystal PANTHER exp

--- Testing/SystemTests/tests/framework/DirectILLAutoProcessTest.py
+++ Testing/SystemTests/tests/framework/DirectILLAutoProcessTest.py
@@ -114,3 +114,57 @@ class DirectILLAuto_PANTHER_Powder_Test(systemtesting.MantidSystemTest):
             SaveOutput=False,
             ClearCache=True
         )
+
+
+class DirectILLAuto_PANTHER_SingleCrystal_Test(systemtesting.MantidSystemTest):
+
+    @classmethod
+    def setUp(cls):
+        cls._original_facility = config['default.facility']
+        cls._original_instrument = config['default.instrument']
+        cls._data_search_dirs = config.getDataSearchDirs()
+        config['default.facility'] = 'ILL'
+        config['default.instrument'] = 'PANTHER'
+        config.appendDataSearchSubDir('ILL/PANTHER/')
+
+    @classmethod
+    def tearDown(cls):
+        config['default.facility'] = cls._original_facility
+        config['default.instrument'] = cls._original_instrument
+        config.setDataSearchDirs(cls._data_search_dirs)
+
+    def cleanup(self):
+        mtd.clear()
+
+    def validate(self):
+        self.tolerance = 1e-3
+        self.tolerance_is_rel_err = True
+        self.disableChecking = ['Instrument', 'Sample']
+        return ['panther_236_013806_reb_Ei_19', 'ILL_PANTHER_SingleCrystal_Auto.nxs']
+
+    def runTest(self):
+
+        ei = 19.03
+        elp = 104
+        Load(Filename='Vnorm012315.nxs', OutputWorkspace='Norm')
+        GroupWorkspaces(InputWorkspaces='Norm', OutputWorkspace='NormVana')
+        eBins = [-2., 0.19, 16]  # inelastic
+        group_by = 4
+
+        output_name = 'panther_236'
+        DirectILLAutoProcess(
+            Runs='13806',
+            OutputWorkspace=output_name,
+            ProcessAs='Sample',
+            ReductionType='SingleCrystal',
+            VanadiumWorkspace='NormVana',
+            MaskWorkspace='Mask212.xml',
+            IncidentEnergyCalibration="Energy Calibration ON",
+            IncidentEnergy=ei,
+            ElasticChannelIndex=elp,
+            EnergyRebinningParams=eBins,
+            FlatBkg='Flat Bkg ON',
+            GroupDetBy=group_by,
+            ClearCache=True,
+            SaveOutput=False
+        )
