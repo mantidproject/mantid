af31d9dacc94300e25b00f84ecc0c269c1db5435
thomashampson <thomas.hampson@stfc.ac.uk>
2022-02-07 16:17:03 +0000

    Fix whitespace in versionnumber.cmake

--- buildconfig/CMake/VersionNumber.cmake
+++ buildconfig/CMake/VersionNumber.cmake
@@ -22,7 +22,7 @@ endif()
 # UNCOMMENT the next 'set' line to 'force' the patch version number to a value (instead of using the count coming out of
 # 'git describe') DO NOT COMMIT THIS TO MASTER UNCOMMENTED, ONLY TO A RELEASE BRANCH
 
-set ( VERSION_PATCH 0 )
+set(VERSION_PATCH 0)
 
 # The tweak is mean to keep in line with the pre-release numbering. https://semver.org/ examples: First release cadidate
 # for tweak 1 is "-1-rc.1" Second release cadidate for tweak 1 is "-1-rc.2" Actual tweak release is "-1"
