13dc9494cd9c0925183b8b6e5bd0ba0a90e564de
Richard Waite <richard.waite@stfc.ac.uk>
2022-02-08 13:39:07 +0000

    Include bins to bottom left of axes limits in color data array
    pcolor puts bin center on bottom left corner of pixel, so will see a pixel with bin center outside the limits

--- Framework/PythonInterface/mantid/plots/quad_mesh_wrapper.py
+++ Framework/PythonInterface/mantid/plots/quad_mesh_wrapper.py
@@ -16,22 +16,25 @@ class QuadMeshWrapper:
     def __getattr__(self, item):
         return getattr(self._mesh, item)
 
-    def get_array_clipped_to_bounds(self):
+    def get_array_clipped_to_bounds(self) -> np.ndarray:
         """
         Get the color data for the subset of the array within the axes limits. This is necessary for autoscaling of the
         clim to work when plotting MDHisto workspaces in nonorthognonal view in sliceviewer
-        :return: masked array of color data clipped to axes limits
+        :return: masked array of color data clipped to axes limits (will be empty container if no data within limits)
         """
-        xlim, ylim = self.axes.get_xlim(), self.axes.get_ylim()
+        xlim, ylim = self.axes.get_xlim(), self.axes.get_ylim()  # limits for data transformed into orthogonal basis
 
-        # reshape array
-        nbins0, nbins1 = self._mesh._coordinates.shape[0:2]
-        arr = self._mesh.get_array().reshape((nbins0 - 1, nbins1 - 1))  # ny, nx
+        # reshape color data array
+        nedges_y, nedges_x = self._mesh._coordinates.shape[0:2]
+        arr = self._mesh.get_array().reshape((nedges_y - 1, nedges_x - 1))
 
         # y-axis is parallel to axes box so can determine ylimit as so
-        ydata = self._mesh._coordinates[:-1, 0, 1]
-        iy = np.logical_and(ydata >= ylim[0], ydata < ylim[1])
-        xdata = self._mesh._coordinates[:-1, :-1, 0][iy, :]  # unlike y need whole 2D array
-        ix = np.logical_and(xdata >= xlim[0], xdata < xlim[1])
+        ydata = self._mesh._coordinates[:-1, 0, 1]  # bin edges - excl. last edge as pcolor puts bin center bottom left
+        dy = ydata[1] - ydata[0]  # ybin width
+        iy_rows = np.logical_and(ydata >= ylim[0] - dy, ydata < ylim[1])  # include bins to bottom left of axes limits
+        # unlike ydata need whole 2D array for xdata as x-extent of data (in orthogonal basis) depends on y value
+        xdata = self._mesh._coordinates[:-1, :-1, 0][iy_rows, :]
+        dx = xdata[0][1] - xdata[0][0]  # xbin width
+        xmask = np.logical_and(xdata >= xlim[0] - dx, xdata < xlim[1])
 
-        return safe_masked_invalid(arr[iy, :][ix])
+        return safe_masked_invalid(arr[iy_rows, :][xmask])
