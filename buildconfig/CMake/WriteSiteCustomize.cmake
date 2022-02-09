3ee7724c3b28ad31c0f93964d01e4d147a2de825
Danny Hindson <danny.hindson@stfc.ac.uk>
2022-02-09 11:14:23 +0000

    Rename sitecustomize.py in bin/<config>
    To avoid it hiding the sitecustomize.py in the python site-packages
    directory on Windows
    Also add comments to explain what's going on

--- buildconfig/CMake/WriteSiteCustomize.cmake
+++ buildconfig/CMake/WriteSiteCustomize.cmake
@@ -1,9 +1,15 @@
 # CMake script to generate a Python sitecustomize.py file at build time It is intended to be used via cmake -P (script
 # mode)
-
 file(
-  WRITE ${SITECUSTOMIZE_DIR}/sitecustomize.py
+  WRITE ${SITECUSTOMIZE_DIR}/sitecustomize_mantid.py
   "
+\"\"\"
+Add bin/<config> as a site directory so that .pth files in it get processed
+Setuptools develop mode (=pip editable install) relies on this to find
+python packages in the source directories
+Can't rely on .egg-link files any more because not supported by importlib
+which is successor to the deprecated pkgresources
+\"\"\"
 import site
 site.addsitedir('${SITECUSTOMIZE_DIR}')
 "
