2ab8af13d43ef999a9a4b08d282acf94986f6ad4
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-02-15 10:22:41 +0000

    Change test_plotscriptgenerator tiled test to use mocks

--- qt/applications/workbench/workbench/plotting/plotscriptgenerator/test/test_plotscriptgenerator.py
+++ qt/applications/workbench/workbench/plotting/plotscriptgenerator/test/test_plotscriptgenerator.py
@@ -7,6 +7,8 @@
 #  This file is part of the mantid workbench.
 
 import unittest
+from distutils.version import LooseVersion
+
 import matplotlib
 
 matplotlib.use("Agg")  # noqa
@@ -76,34 +78,28 @@ SAMPLE_SCRIPT_WITH_FIT = ("from mantid.simpleapi import Fit\n"
                           "# https://docs.mantidproject.org/tutorials/python_in_mantid/plotting/02_scripting_plots"
                           ".html")
 
-SAMPLE_SCRIPT_TILED_PLOT = ("# import mantid algorithms, numpy and matplotlib\n"
-                            "from mantid.simpleapi import *\n"
-                            "import matplotlib.pyplot as plt\n"
-                            "import numpy as np\n\n\n"
-                            "import matplotlib.pyplot as plt\n"
+SAMPLE_SCRIPT_TILED_PLOT = ("import matplotlib.pyplot as plt\n"
                             "from mantid.plots.utility import MantidAxType\n"
-                            "from mantid.api import AnalysisDataService as ADS\n\n"
-                            "ws = ADS.retrieve('ws')\n\n"
-                            "fig, axes = plt.subplots(edgecolor='#ffffff', facecolor='#ffffff', ncols=2, num='ws-1', "
-                            "subplot_kw={'projection': 'mantid'})\n"
-                            "axes[0].plot(ws, color='#1f77b4', label='ws: spec 1', wkspIndex=0)\n"
-                            "axes[0].tick_params(axis='x', which='major', **{'gridOn': False, 'tick1On': True, "
-                            "'tick2On': False, 'label1On': True, 'label2On': False})\n"
-                            "axes[0].tick_params(axis='y', which='major', **{'gridOn': False, 'tick1On': True, "
-                            "'tick2On': False, 'label1On': True, 'label2On': False})\n"
-                            "axes[0].set_xlabel('Time-of-flight ($\\\\mu s$)')\n"
-                            "axes[0].set_ylabel('Counts ($\\\\mu s$)$^{-1}$')\n\n"
-                            "axes[1].plot(ws, color='#1f77b4', label='ws: spec 2', wkspIndex=1)\n"
-                            "axes[1].tick_params(axis='x', which='major', **{'gridOn': False, 'tick1On': True, "
-                            "'tick2On': False, 'label1On': True, 'label2On': False})\n"
-                            "axes[1].tick_params(axis='y', which='major', **{'gridOn': False, 'tick1On': True, "
-                            "'tick2On': False, 'label1On': True, 'label2On': False})\n"
-                            "axes[1].set_xlabel('Time-of-flight ($\\\\mu s$)')\n"
-                            "axes[1].set_ylabel('Counts ($\\\\mu s$)$^{-1}$')\n\n"
+                            "from mantid.api import AnalysisDataService\n"
+                            "\n"
+                            "ADS.retrieve(...)\n"
+                            "\n"
+                            "fig, axes = plt.subplots(...)\n"
+                            "axes[0].plot(...)\n"
+                            "axes[0].plot(...)\n"
+                            "axes[0].set_xlim(...)\n"
+                            "axes[0].set_ylim(...)\n"
+                            "axes[0].set_facecolor('#8a9aff')\n"
+                            "\n"
+                            "axes[1].plot(...)\n"
+                            "axes[1].set_xlim(...)\n"
+                            "axes[1].set_ylim(...)\n"
+                            "axes[1].set_facecolor('#8a9aff')\n"
+                            "\n"
                             "plt.show()\n"
                             "# Scripting Plots in Mantid:\n"
-                            "# https://docs.mantidproject.org/tutorials/python_in_mantid/plotting/"
-                            "02_scripting_plots.html")
+                            "# https://docs.mantidproject.org/tutorials/python_in_mantid/plotting/02_scripting_plots"
+                            ".html")
 
 
 class PlotScriptGeneratorTest(unittest.TestCase):
@@ -138,8 +134,6 @@ class PlotScriptGeneratorTest(unittest.TestCase):
             'containers': [],
             'get_xlabel': lambda: '',
             'get_ylabel': lambda: '',
-            'numRows': 1,
-            'numCols': 1,
             'get_title': lambda: '',
             'get_xscale': lambda: 'linear',
             'get_yscale': lambda: 'linear',
@@ -153,6 +147,12 @@ class PlotScriptGeneratorTest(unittest.TestCase):
         mock_ax.xaxis.minor.locator = Mock(spec=NullLocator)
         mock_ax.xaxis._major_tick_kw = {'gridOn': False}
         mock_ax.yaxis._major_tick_kw = {'gridOn': False}
+        if LooseVersion('3.1.3') < LooseVersion(matplotlib.__version__):
+            mock_ax.get_gridspec.return_value.nrows = 1
+            mock_ax.get_gridspec.return_value.ncols = 1
+        else:
+            mock_kwargs['numRows'] = 1
+            mock_kwargs['numCols'] = 1
         return mock_ax
 
     def test_generate_script_returns_None_if_no_MantidAxes_in_figure(self):
@@ -297,16 +297,32 @@ class PlotScriptGeneratorTest(unittest.TestCase):
         self.assertIn(mock_major_kw, commands)
         self.assertIn(mock_minor_kw, commands)
 
-    def test_generate_script_handles_tiled_plots_correctly(self):
-        from mantid.simpleapi import CreateSampleWorkspace
+    @patch(GET_AUTOSCALE_LIMITS)
+    @patch(GEN_AXIS_LIMIT_CMDS)
+    @patch(GEN_WS_RETRIEVAL_CMDS)
+    @patch(GEN_PLOT_CMDS)
+    @patch(GEN_SUBPLOTS_CMD)
+    def test_generate_script_compiles_script_correctly_for_tiled_plots(self, mock_subplots_cmd,
+                                                                       mock_plot_cmd, mock_retrieval_cmd,
+                                                                       mock_axis_lim_cmd,
+                                                                       mock_autoscale_lims):
+        mock_retrieval_cmd.return_value = self.retrieval_cmds
+        mock_subplots_cmd.return_value = self.subplots_cmd
+        mock_plot_cmd.return_value = self.plot_cmd
+        mock_axis_lim_cmd.return_value = self.ax_limit_cmds
+        mock_autoscale_lims.return_value = (-0.02, 1.02)
 
-        ws = CreateSampleWorkspace()
-        fig, axes = plt.subplots(ncols=2, num='ws-1', subplot_kw={'projection': 'mantid'})
-        axes[0].plot(ws, wkspIndex=0)
-        axes[1].plot(ws, wkspIndex=1)
+        mock_axes1 = self._gen_mock_axes(get_tracked_artists=lambda: [None, None],
+                                         get_lines=lambda: [None, None],
+                                         numRows=1, numCols=2, colNum=0)
+        mock_axes2 = self._gen_mock_axes(get_tracked_artists=lambda: [None],
+                                         get_lines=lambda: [None],
+                                         numRows=1, numCols=2, colNum=1)
+        mock_fig = Mock(get_axes=lambda: [mock_axes1, mock_axes2], axes=[mock_axes1, mock_axes2])
+        mock_fig.canvas.manager.fit_browser.fit_result_ws_name = ""
 
-        commands = generate_script(fig)
-        self.assertEqual(SAMPLE_SCRIPT_TILED_PLOT, commands)
+        output_script = generate_script(mock_fig, exclude_headers=True)
+        self.assertEqual(SAMPLE_SCRIPT_TILED_PLOT, output_script)
 
 
 if __name__ == '__main__':
