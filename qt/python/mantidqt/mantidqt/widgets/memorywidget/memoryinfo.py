92c063345079d5c99381b8bce806e2aaeb8654f3
sf1919 <sarah.foxley@stfc.ac.uk>
2022-03-10 15:00:59 +0000

    Add method for calculating process memory used

--- qt/python/mantidqt/mantidqt/widgets/memorywidget/memoryinfo.py
+++ qt/python/mantidqt/mantidqt/widgets/memorywidget/memoryinfo.py
@@ -7,6 +7,8 @@
 #  This file is part of the mantidqt package
 #
 #
+import os
+import psutil
 from psutil import virtual_memory
 from typing import Tuple
 
@@ -27,3 +29,19 @@ def get_memory_info() -> Tuple[int, float, float]:
     mem_used_GB = mem_used * CONVERSION_FACTOR_BYTES_TO_GB
     mem_total_GB = mem_total * CONVERSION_FACTOR_BYTES_TO_GB
     return mem_used_percent, mem_used_GB, mem_total_GB
+
+
+def get_mantid_memory_info() -> Tuple[int, float]:
+    """
+    Returns following process memory usage information obtained using psutil
+    mantid_mem_used_percent : Used  memory by the current process in percentage
+    mantid_mem_used_GB : Used memory by the current process in Gigabytes(GB)
+    """
+    current_process = psutil.Process(os.getpid())
+    mantid_mem_used = current_process.memory_info().rss
+    mem_total = virtual_memory().total
+    mantid_mem_used_percent = min(int(round(mantid_mem_used * 100 / mem_total)), 100)
+    if mantid_mem_used_percent < 0.1:
+        mantid_mem_used_percent = 0.1
+    mantid_mem_used_GB = mantid_mem_used * CONVERSION_FACTOR_BYTES_TO_GB
+    return mantid_mem_used_percent, mantid_mem_used_GB
