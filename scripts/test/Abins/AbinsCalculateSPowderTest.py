f02796ec80e63e53fdb3a95821519c6a58e905b4
Adam J. Jackson <a.j.jackson@physics.org>
2022-03-21 14:04:23 +0000

    Abins 2D: extend CalculateSPowderTest to test 2d, higher orders
    - Add test data for 2D instruments: one pychop-based case (Mari) and
      one special instrument (Panther). Both include NaN masking of
      kinematically-forbidden region.
    
    - Advanced parameters are used to evaluate data on a much coarser mesh
      than usual, reducing reference file size to manageable ~100 KB.
    
    - Testing of cache reload is disabled for 2D instruments, as this
      feature is currently disabled. (It costs time and is of little
      benefit for such calculations in its current form.)
    
    - 2D data breaks assumption of data handlers that the number of atoms
      will equal number of fields - 1. Make this more robust.
    
    - Existing tests only check fundamental spectrum, but other orders are
      still there in the test file? We really should be checking all the
      orders, it is quite possible an error will affect higher orders
      without changing fundamentals. Extended check to loop over orders.

--- scripts/test/Abins/AbinsCalculateSPowderTest.py
+++ scripts/test/Abins/AbinsCalculateSPowderTest.py
@@ -4,6 +4,7 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
+from copy import deepcopy
 import functools
 import json
 import unittest
@@ -27,6 +28,7 @@ class SCalculatorFactoryPowderTest(unittest.TestCase):
     _instrument = abins.instruments.get_instrument("TOSCA")
     _order_event = FUNDAMENTALS
     _si2 = "Si2-sc_CalculateSPowder"
+    _instruments_defaults = {}
 
     default_calculator_kwargs = dict(temperature=_temperature,
                                      instrument=_instrument,
@@ -37,10 +39,12 @@ class SCalculatorFactoryPowderTest(unittest.TestCase):
     def setUp(self):
         self.default_threads = abins.parameters.performance['threads']
         abins.parameters.performance['threads'] = 1
+        self._instruments_defaults = deepcopy(abins.parameters.instruments)
 
     def tearDown(self):
         abins.test_helpers.remove_output_files(list_of_names=["CalculateSPowder"])
         abins.parameters.performance['threads'] = self.default_threads
+        abins.parameters.instruments.update(self._instruments_defaults)
 
     #     test input
     def test_wrong_input(self):
@@ -94,8 +98,31 @@ class SCalculatorFactoryPowderTest(unittest.TestCase):
                         quantum_order_num=2,
                         autoconvolution=True)
 
+    def test_2d_order1_mari(self):
+        abins.parameters.instruments['MARI'].update({'q_size': 10,
+                                                     'n_energy_bins': 100})
+        mari = abins.instruments.get_instrument("MARI")
+        mari.set_incident_energy(1000.)
+
+        self._good_case(name=self._si2 + '_2d_o1_mari',
+                        castep_name=self._si2,
+                        instrument=mari)
+
+    def test_2d_order2_panther(self):
+        abins.parameters.instruments['PANTHER'].update({'q_size': 8,
+                                                        'n_energy_bins': 60})
+        panther = abins.instruments.get_instrument("PANTHER")
+        panther.set_incident_energy(1000.)
+
+        self._good_case(name=self._si2 + '_2d_o2_panther',
+                        quantum_order_num=2,
+                        castep_name=self._si2,
+                        instrument=panther)
+
     # helper functions
     def _good_case(self, name=_si2, castep_name=None, **calculator_kwargs):
+        from abins.constants import ONE_DIMENSIONAL_INSTRUMENTS
+
         if castep_name is None:
             castep_name = name
 
@@ -110,22 +137,23 @@ class SCalculatorFactoryPowderTest(unittest.TestCase):
         calculated_data = good_tester.get_formatted_data()
 
         ### Uncomment to generate new data ###
-        from pathlib import Path
-        data_path = Path(abins.test_helpers.find_file(filename=castep_name + ".phonon")).parent
-        self._write_data(data=calculated_data.extract(),
-                         filename=(data_path / f"{name}_S.txt"))
+        # from pathlib import Path
+        # data_path = Path(abins.test_helpers.find_file(filename=castep_name + ".phonon")).parent
+        # self._write_data(data=calculated_data.extract(),
+        #                  filename=(data_path / f"{name}_S.txt"))
 
         good_data = self._get_good_data(filename=name, castep_filename=castep_name)
         self._check_data(good_data=good_data["S"], data=calculated_data.extract())
 
         # check if loading powder data is correct
-        new_tester = abins.SCalculatorFactory.init(
-            filename=abins.test_helpers.find_file(filename=castep_name + ".phonon"),
-            abins_data=good_data["DFT"],
-            **calc_kwargs)
-        loaded_data = new_tester.load_formatted_data()
+        if calc_kwargs['instrument'].get_name() in ONE_DIMENSIONAL_INSTRUMENTS:
+            new_tester = abins.SCalculatorFactory.init(
+                filename=abins.test_helpers.find_file(filename=castep_name + ".phonon"),
+                abins_data=good_data["DFT"],
+                **calc_kwargs)
+            loaded_data = new_tester.load_formatted_data()
 
-        self._check_data(good_data=good_data["S"], data=loaded_data.extract())
+            self._check_data(good_data=good_data["S"], data=loaded_data.extract())
 
     @staticmethod
     @functools.lru_cache(maxsize=4)
@@ -160,30 +188,33 @@ class SCalculatorFactoryPowderTest(unittest.TestCase):
                                       replace("'",  '"').
                                       replace("0. ", "0.0"))
 
-        temp = np.asarray(correct_data["frequencies"])
-        correct_data["frequencies"] = temp
+        for array_key in ("frequencies", "q_bins"):
+            if correct_data.get(array_key) is not None:
+                correct_data[array_key] = np.asarray(correct_data[array_key])
 
-        # we need to - 1 because one entry is "frequencies"
-        for el in range(len(correct_data) - 1):
+        n_atoms = len([True for key in correct_data if 'atom' in key])
+        for i in range(n_atoms):
+            for order_key, order_data in correct_data[f"atom_{i}"]["s"].items():
+                correct_data[f"atom_{i}"]["s"][order_key] = np.asarray(order_data)
 
-            temp = np.asarray(correct_data["atom_%s" % el]["s"]["order_%s" % FUNDAMENTALS])
-            correct_data["atom_%s" % el]["s"]["order_%s" % FUNDAMENTALS] = temp
+            # correct_data[f"atom_{i}"]["s"]["order_1"] = np.asarray(
+            #     correct_data[f"atom_{i}"]["s"]["order_1"])
 
         return correct_data
 
     def _check_data(self, good_data=None, data=None):
 
-        good_temp = good_data["frequencies"]
-        data_temp = data["frequencies"]
-        assert_almost_equal(good_temp, data_temp)
+        for array_key in ("frequencies", "q_bins"):
+            if good_data.get(array_key) is not None:
+                assert_almost_equal(good_data[array_key],
+                                    data[array_key])
 
-        # we need to - 1 because one entry is "frequencies"
-        for el in range(len(good_data) - 1):
-
-            good_temp = good_data["atom_%s" % el]["s"]["order_%s" % FUNDAMENTALS]
-            data_temp = data["atom_%s" % el]["s"]["order_%s" % FUNDAMENTALS]
-
-            assert_almost_equal(good_temp, data_temp)
+        n_atoms = len([True for key in good_data if 'atom' in key])
+        for i in range(n_atoms):
+            for order_key in good_data[f"atom_{i}"]["s"]:
+                ref = good_data[f"atom_{i}"]["s"][order_key]
+                calculated = data[f"atom_{i}"]["s"].get(order_key)
+                assert_almost_equal(ref, calculated)
 
 
 if __name__ == '__main__':
