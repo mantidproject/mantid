399c267a813fd0059358f0ffdf62e50075970ca3
David Fairbrother <DavidFair@users.noreply.github.com>
2022-03-22 19:07:24 +0000

    Modernise SANS2D Tests
    Replaces the SANS2D tests from pre-2015 with more modern versions.
    This is because the workflow does not represent the fact that all data
    is now collected with monitor mode. This also means we can't map 1:1
    from legacy files to new files as the new command is not the same.
    
    Re #33659

---
+++ Testing/SystemTests/tests/framework/ISIS/SANS/SANS2D/SANS2D_GDW20_4m_cycle_22_02.py
@@ -0,0 +1,57 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import systemtesting
+from ISIS.SANS.isis_sans_system_test import ISISSansSystemTest
+
+from sans.command_interface.ISISCommandInterface import (SANS2D, MaskFile, UseCompatibilityMode,
+                                                         AssignSample, TransmissionSample, WavRangeReduction, Set2D)
+from sans.common.enums import SANSInstrument
+
+
+@ISISSansSystemTest(SANSInstrument.SANS2D)
+class SANS2D_GDW20_4m_22_02_2D_M3(systemtesting.MantidSystemTest):
+    def requiredMemoryMB(self):
+        return 2000
+
+    def runTest(self):
+        UseCompatibilityMode()
+        SANS2D()
+        Set2D()
+        MaskFile("USER_SANS2D_212A_2p4_4m_M3_Sans2d_Team_8mm_Changer_After_GuideRealignment.toml")
+        AssignSample("SANS2D00068982.nxs")
+        TransmissionSample("SANS2D00068976.nxs", "SANS2D00069299.nxs")
+        self.returned = WavRangeReduction()
+
+    def validate(self):
+        self.disableChecking.append('Axes')
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('SpectraMap')
+
+        return self.returned, "SANS2D_GDW20_4m_cycle_22_02_2D_M3_ref.nxs"
+
+
+@ISISSansSystemTest(SANSInstrument.SANS2D)
+class SANS2D_GDW20_4m_22_02_2D_M4(systemtesting.MantidSystemTest):
+    def requiredMemoryMB(self):
+        return 2000
+
+    def runTest(self):
+        UseCompatibilityMode()
+        SANS2D()
+        Set2D()
+        MaskFile("USER_SANS2D_212A_2p4_4m_M4_Sans2d_Team_8mm_Changer_After_GuideRealignment.toml")
+        AssignSample("SANS2D00068982.nxs")
+        TransmissionSample("SANS2D00068976.nxs", "SANS2D00069299.nxs")
+        self.returned = WavRangeReduction()
+
+    def validate(self):
+        self.disableChecking.append('Axes')
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('SpectraMap')
+
+        return self.returned, "SANS2D_GDW20_4m_cycle_22_02_2D_M4_ref.nxs"
---
+++ Testing/SystemTests/tests/framework/ISIS/SANS/SANS2D/SANS2D_glassy_carbon_cycle_22_02.py
@@ -0,0 +1,58 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import systemtesting
+from ISIS.SANS.isis_sans_system_test import ISISSansSystemTest
+
+from sans.command_interface.ISISCommandInterface import (SANS2D, Set1D,
+                                                         MaskFile, UseCompatibilityMode,
+                                                         AssignSample, TransmissionSample, WavRangeReduction, Set2D)
+from sans.common.enums import SANSInstrument
+
+
+@ISISSansSystemTest(SANSInstrument.SANS2D)
+class SANS2D_glassy_carbon_22_02_1D_M3(systemtesting.MantidSystemTest):
+    def requiredMemoryMB(self):
+        return 2000
+
+    def runTest(self):
+        UseCompatibilityMode()
+        SANS2D()
+        Set1D()
+        MaskFile("USER_SANS2D_212B_5_12m_M3_Sans2d_Team_8mm_Changer_After_GuideRealignment.toml")
+        AssignSample("SANS2D00068992.nxs")
+        TransmissionSample("SANS2D00068987.nxs", "SANS2D00069299.nxs")
+        self.returned = WavRangeReduction()
+
+    def validate(self):
+        self.disableChecking.append('Axes')
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('SpectraMap')
+
+        return self.returned, "SANS2D_glassy_carbon_cycle_22_02_1D_M3_ref.nxs"
+
+
+@ISISSansSystemTest(SANSInstrument.SANS2D)
+class SANS2D_glassy_carbon_22_02_2D_M3(systemtesting.MantidSystemTest):
+    def requiredMemoryMB(self):
+        return 2000
+
+    def runTest(self):
+        UseCompatibilityMode()
+        SANS2D()
+        Set2D()
+        MaskFile("USER_SANS2D_212B_5_12m_M3_Sans2d_Team_8mm_Changer_After_GuideRealignment.toml")
+        AssignSample("SANS2D00068992.nxs")
+        TransmissionSample("SANS2D00068987.nxs", "SANS2D00069299.nxs")
+        self.returned = WavRangeReduction()
+
+    def validate(self):
+        self.disableChecking.append('Axes')
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('SpectraMap')
+
+        return self.returned, "SANS2D_glassy_carbon_cycle_22_02_2D_M3_ref.nxs"
