6cfa903689befcd19b3aa6e4ebd6dde7daf40f53
Richard Waite <richard.waite@stfc.ac.uk>
2022-03-31 17:10:30 +0100

    Fix regression of bug #33104 reveresed in PR #33639
    Bug relates to support for dynamic rebinning when MDhisto workspace has been altered (e.g. scaled).
    It was initially fixed in #33362 which introduced a function to determine whether the orignal
    MDE workspace should be rebinned (it shouldn't if the MDHisto has been altered). In PR #33639
    the function that checks whether a ws can be dynamically rebinned was moved and it no longer
    checked whether the MDHisto had been altered (but the function to do so remained in the model).
    This commit moves the function to check if the MDHisto had been altered to the WorkspaceInfo model
    and fixes the regression.

--- qt/python/mantidqt/mantidqt/widgets/sliceviewer/models/model.py
+++ qt/python/mantidqt/mantidqt/widgets/sliceviewer/models/model.py
@@ -120,18 +120,6 @@ class SliceViewerModel(SliceViewerBaseModel):
 
         return False
 
-    def can_rebin_original_workspace(self):
-        ws = self._get_ws()
-        if self.get_ws_type() == WS_TYPE.MDH and self._get_ws().hasOriginalWorkspace(0):
-            has_same_dims = ws.getOriginalWorkspace(0).getNumDims() == ws.getNumDims()
-            # check if mdhisto altered since original ws BinMD - then not valid to rebin original
-            mdhisto_has_been_altered = False
-            if ws.getNumExperimentInfo() > 0 and ws.getExperimentInfo(0).run().hasProperty("mdhisto_was_modified"):
-                mdhisto_has_been_altered = bool(int(ws.getExperimentInfo(0).run().get("mdhisto_was_modified").value))
-            return has_same_dims and not mdhisto_has_been_altered
-        else:
-            return False
-
     def set_ws_name(self, new_name):
         self._ws_name = new_name
 
