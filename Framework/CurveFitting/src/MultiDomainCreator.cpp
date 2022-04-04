3aa452b7ab0f668c768c5a0cec0256466f608071
Dominik Arominski <arominski@ill.fr>
2022-04-04 17:06:07 +0200

    Removed deprecated code

--- Framework/CurveFitting/src/MultiDomainCreator.cpp
+++ Framework/CurveFitting/src/MultiDomainCreator.cpp
@@ -71,15 +71,6 @@ void MultiDomainCreator::initFunction(API::IFunction_sptr function) {
       // get domain indices for this function
       mdFunction->getDomainIndices(iFun, m_creators.size(), domainIndices);
       if (!domainIndices.empty()) {
-        /*
-        if ( domainIndices.size() != 1 )
-        {
-          std::stringstream msg;
-          msg << "Function #" << iFun << " applies to multiple domains.\n"
-              << "Only one of the domains is used to set workspace.";
-          g_log.warning(msg.str());
-        }
-        */
         size_t index = domainIndices[0];
         if (index >= m_creators.size()) {
           std::stringstream msg;
