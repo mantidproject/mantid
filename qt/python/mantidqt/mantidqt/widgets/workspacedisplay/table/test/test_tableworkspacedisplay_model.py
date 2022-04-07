ce15c92c5a9b6ee11ca3fffdf31fb48bebff32b2
Anthony <anthony.lim@stfc.ac.uk>
2022-04-07 11:43:08 +0100

    table WS now sends a replaced signal fixed 2 tests

--- qt/python/mantidqt/mantidqt/widgets/workspacedisplay/table/test/test_tableworkspacedisplay_model.py
+++ qt/python/mantidqt/mantidqt/widgets/workspacedisplay/table/test/test_tableworkspacedisplay_model.py
@@ -66,11 +66,11 @@ class TableWorkspaceDisplayModelTest(unittest.TestCase):
         expected_col = 1111
         expected_row = 1
 
-        model.set_cell_data(expected_row, expected_col, test_data, False)
+        model.set_cell_data(expected_row, expected_col, test_data)
 
         # check that the correct conversion function was retrieved
         # -> the one for the column for which the data is being set
-        model.ws.setCell.assert_called_once_with(expected_row, expected_col, test_data, notify_replace=False)
+        model.ws.setCell.assert_called_once_with(expected_row, expected_col, test_data)
 
     @with_mock_workspace
     def test_set_cell_data_v3d(self, model):
