6088177ac1b1ed95c26ad7451f10f62584847759
Danny Hindson <danny.hindson@stfc.ac.uk>
2022-04-11 14:14:19 +0100

    Add missing copyright header

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SimpleShapeDiscusInelastic.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SimpleShapeDiscusInelastic.py
@@ -1,4 +1,9 @@
-#pylint: disable=no-init
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2022 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
 from mantid.api import (PythonAlgorithm, AlgorithmFactory, MatrixWorkspaceProperty,
                         WorkspaceGroupProperty, PropertyMode)
 from mantid.kernel import (VisibleWhenProperty, PropertyCriterion,
