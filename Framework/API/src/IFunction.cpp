d542f8f56e2ce8a59938e711fe4c569bcbac9cbd
Robert Applin <robert.applin@stfc.ac.uk>
2022-04-13 16:07:57 +0100

    Refs #33616. Change the step size for fitting.

--- Framework/API/src/IFunction.cpp
+++ Framework/API/src/IFunction.cpp
@@ -1025,9 +1025,7 @@ void IFunction::calNumericalDeriv(const FunctionDomain &domain, Jacobian &jacobi
    * consider that method when updating this.
    */
 
-  constexpr double epsilon = std::numeric_limits<double>::epsilon() * 100;
-  constexpr double stepPercentage = 0.001;
-  constexpr double cutoff = 100.0 * std::numeric_limits<double>::min() / stepPercentage;
+  constexpr double epsilon = std::numeric_limits<double>::epsilon();
   const size_t nParam = nParams();
   size_t nData = getValuesSize(domain);
 
@@ -1045,10 +1043,10 @@ void IFunction::calNumericalDeriv(const FunctionDomain &domain, Jacobian &jacobi
   for (size_t iP = 0; iP < nParam; iP++) {
     if (isActive(iP)) {
       const double val = activeParameter(iP);
-      if (fabs(val) < cutoff) {
-        step = epsilon;
+      if (fabs(val) < 1 || val == 0) {
+        step = sqrt(epsilon);
       } else {
-        step = val * stepPercentage;
+        step = val * sqrt(epsilon);
       }
 
       const double paramPstep = val + step;
