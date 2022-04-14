3c615ed8ae2106b09ff0e7b513d234528c85643e
Pete Peterson <petersonpf@ornl.gov>
2022-04-14 08:52:04 -0400

    clang-format files that were wrong on main

--- qt/widgets/instrumentview/inc/MantidQtWidgets/InstrumentView/InstrumentTreeModel.h
+++ qt/widgets/instrumentview/inc/MantidQtWidgets/InstrumentView/InstrumentTreeModel.h
@@ -31,10 +31,8 @@ public:
 
   QVariant data(const QModelIndex &index, int role) const override;
   Qt::ItemFlags flags(const QModelIndex &index) const override;
-  QVariant headerData(int section, Qt::Orientation orientation,
-                      int role = Qt::DisplayRole) const override;
-  QModelIndex index(int row, int column,
-                    const QModelIndex &parent = QModelIndex()) const override;
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+  QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
   QModelIndex parent(const QModelIndex &index) const override;
   int rowCount(const QModelIndex &paren = QModelIndex()) const override;
   int columnCount(const QModelIndex &parent = QModelIndex()) const override;
