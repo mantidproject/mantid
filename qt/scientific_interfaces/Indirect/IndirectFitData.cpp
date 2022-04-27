0d83cfd22a98ec57539250529d538ad8eba63041
Robert Applin <robert.applin@stfc.ac.uk>
2022-04-27 15:40:27 +0100

    Refs #0. Fix the cppcheck warnings in indirect code.

--- qt/scientific_interfaces/Indirect/IndirectFitData.cpp
+++ qt/scientific_interfaces/Indirect/IndirectFitData.cpp
@@ -38,10 +38,9 @@ std::vector<double> extractQValues(const MatrixWorkspace_sptr &workspace, const 
   if (axis_ptr) {
     const std::shared_ptr<Mantid::Kernel::Unit> &unit_ptr = axis_ptr->unit();
     if (unit_ptr->unitID() == "MomentumTransfer") {
-      for (auto spectrum : spectra) {
-        qs.emplace_back(axis_ptr->operator()(spectrum.value));
-        // qs = axis_ptr->getValues();
-      }
+      qs.reserve(spectra.size().value);
+      std::transform(spectra.begin(), spectra.end(), std::back_inserter(qs),
+                     [&axis_ptr](const auto &spectrum) { return axis_ptr->operator()(spectrum.value); });
     }
   }
   // ...otherwise, compute the momentum transfer for each spectrum, if possible
@@ -123,7 +122,7 @@ std::string cutLastOf(const std::string &str, const std::string &delimiter) {
   return str;
 }
 
-boost::basic_format<char> &tryPassFormatArgument(boost::basic_format<char> &formatString, const std::string &arg) {
+boost::basic_format<char> tryPassFormatArgument(boost::basic_format<char> &formatString, const std::string &arg) {
   try {
     return formatString % arg;
   } catch (const boost::io::too_many_args &) {
@@ -157,8 +156,8 @@ std::string orderExcludeRegionString(std::vector<double> &bounds) {
 std::vector<double> getBoundsAsDoubleVector(std::vector<std::string> const &boundStrings) {
   std::vector<double> bounds;
   bounds.reserve(boundStrings.size());
-  for (auto bound : boundStrings)
-    bounds.emplace_back(convertBoundToDoubleAndFormat(bound));
+  std::transform(boundStrings.cbegin(), boundStrings.cend(), std::back_inserter(bounds),
+                 [](const auto &bound) { return convertBoundToDoubleAndFormat(bound); });
   return bounds;
 }
 
