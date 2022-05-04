2beaee97f7201f186374ba4e75001d2430fdc256
Oleksandr Koshchii <alex.koshchii@gmail.com>
2022-05-04 11:54:24 +0200

    Replace os.startfile by subprocess.call for windows; add style changes

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/dns_powder_tof/helpers/file_processing.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/dns_powder_tof/helpers/file_processing.py
@@ -4,6 +4,7 @@
 #     NScD Oak Ridge National Laboratory, European Spallation Source
 #     & Institut Laue - Langevin
 # SPDX - License - Identifier: GPL - 3.0 +
+
 """
 DNS file helpers
 """
@@ -16,12 +17,12 @@ import sys
 import zipfile
 
 
-def filter_filenames(alldatafiles, start, end):
+def filter_filenames(all_datafiles, start, end):
     """
-    Filter datafilenames to the range given
+    Filter data filenames to the range given.
     """
     filtered = []
-    for filename in alldatafiles:
+    for filename in all_datafiles:
         number = int(filename.split('_')[-2][:-2])
         if end >= number >= start:
             filtered.append(filename)
@@ -30,41 +31,41 @@ def filter_filenames(alldatafiles, start, end):
 
 def return_filelist(data_dir):
     """
-    Return list of names of dnsfiles in data_dir
+    Return list of names of dns files in data_dir.
     """
     filelist = []
     if not os.path.isdir(data_dir):
         return []
-    for fname in sorted(os.listdir(data_dir)):
-        if re.match(r".*?_[0-9]+.d_dat", fname):
-            filelist.append(fname)
+    for file_name in sorted(os.listdir(data_dir)):
+        if re.match(r".*?_[0-9]+.d_dat", file_name):
+            filelist.append(file_name)
     return filelist
 
 
 def return_standard_zip(data_dir):
     if not os.path.isdir(data_dir):
         return ''
-    zipfiles = sorted(glob.glob(data_dir + "/standard*.zip"))
-    if zipfiles:
-        latest_standard_file = max(zipfiles, key=os.path.getmtime)
+    zip_files = sorted(glob.glob(data_dir + "/standard*.zip"))
+    if zip_files:
+        latest_standard_file = max(zip_files, key=os.path.getmtime)
         return latest_standard_file
     return ''
 
 
-def unzip_latest_standard(data_dir, standarddir):
-    latest_zip = return_standard_zip(standarddir)
+def unzip_latest_standard(data_dir, standard_dir):
+    latest_zip = return_standard_zip(standard_dir)
     if not latest_zip:
         latest_zip = return_standard_zip(data_dir)
     if latest_zip:
         with zipfile.ZipFile(latest_zip, "r") as zip_ref:
-            zip_ref.extractall(standarddir)
+            zip_ref.extractall(standard_dir)
         return True
     return False
 
 
 def create_dir_from_filename(filename):
-    cddir = os.path.dirname(filename)
-    create_dir(cddir)
+    cur_dir = os.path.dirname(filename)
+    create_dir(cur_dir)
 
 
 def create_dir(current_dir):
@@ -77,8 +78,8 @@ def save_txt(txt, filename, current_dir=None):
         current_path = ''.join((current_dir, '/', filename))
     else:
         current_path = filename
-    with open(current_path, 'w', encoding="utf8") as myfile:
-        myfile.write(txt)
+    with open(current_path, 'w', encoding="utf8") as my_file:
+        my_file.write(txt)
     return [filename, current_path]
 
 
@@ -87,8 +88,8 @@ def load_txt(filename, current_dir=None):
         current_path = ''.join((current_dir, '/', filename))
     else:
         current_path = filename
-    with open(current_path, 'r', encoding="utf8") as myfile:
-        txt = myfile.readlines()
+    with open(current_path, 'r', encoding="utf8") as my_file:
+        txt = my_file.readlines()
     return txt
 
 
@@ -99,7 +100,7 @@ def open_editor(filename, current_dir=None):
         current_path = filename
     if os.path.exists(current_path):
         if sys.platform.startswith("win"):
-            os.startfile(current_path)
+            subprocess.call(["cmd.exe", "/c", current_path])
         elif sys.platform.startswith("linux"):
             subprocess.call(["xdg-open", current_path])
         elif sys.platform == "darwin":
