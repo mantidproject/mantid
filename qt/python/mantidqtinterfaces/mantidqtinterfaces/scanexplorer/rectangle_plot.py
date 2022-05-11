f1c6555eb2ba8c113fde0024c56ffbf020798ef6
Mathieu Tillet <tillet@ill.eu>
2022-05-11 16:48:51 +0200

    Recognize user interaction type

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/scanexplorer/rectangle_plot.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/scanexplorer/rectangle_plot.py
@@ -1,4 +1,5 @@
 from typing import Any
+from enum import Enum
 
 from matplotlib.widgets import RectangleSelector
 from matplotlib.patches import Rectangle
@@ -8,18 +9,28 @@ from mantidqt.widgets.sliceviewer.presenters.lineplots import LinePlots, KeyHand
 from mantid.simpleapi import CreateWorkspace
 
 
+class UserInteraction(Enum):
+    RECTANGLE_CREATED = 1
+    RECTANGLE_MOVED = 2
+    RECTANGLE_RESHAPED = 3
+    RECTANGLE_SELECTED = 4
+
+
 class MultipleRectangleSelectionLinePlot(KeyHandler):
 
     STATUS_MESSAGE = "Press key to send roi/cuts to workspaces: r=roi, c=both cuts, x=X, y=Y. Esc clears region"
     SELECTION_KEYS = ('c', 'x', 'y')
+    EPSILON = 1e-5
 
     def __init__(self, plotter: LinePlots, exporter: Any):
         super().__init__(plotter, exporter)
 
         ax = plotter.image_axes
-        self._selector = RectangleSelector(ax, self._on_rectangle_selected)
+        self._selector = RectangleSelector(ax, self._on_rectangle_selected, drawtype='box', interactive=True,
+                                           ignore_event_outside=False)
 
         self._rectangles = []
+        self._current_rectangle = None
 
     def _on_rectangle_selected(self, click_event, release_event):
         """
@@ -30,15 +41,57 @@ class MultipleRectangleSelectionLinePlot(KeyHandler):
         cinfo_click = cursor_info(self.plotter.image, click_event.xdata, click_event.ydata)
         if cinfo_click is None:
             return
+
         cinfo_release = cursor_info(self.plotter.image, release_event.xdata, release_event.ydata)
         if cinfo_release is None:
             return
 
-        self._draw_rectangle((click_event.xdata, click_event.ydata),
-                             release_event.xdata - click_event.xdata,
-                             release_event.ydata - click_event.ydata)
+        interaction = self._determine_behaviour(click_event, release_event)
+
+        if interaction == UserInteraction.RECTANGLE_CREATED:
+            self._draw_rectangle((click_event.xdata, click_event.ydata),
+                                 release_event.xdata - click_event.xdata,
+                                 release_event.ydata - click_event.ydata)
+
+        elif interaction == UserInteraction.RECTANGLE_SELECTED:
+            pass
+        elif interaction == UserInteraction.RECTANGLE_MOVED:
+            pass
+        elif interaction == UserInteraction.RECTANGLE_RESHAPED:
+            pass
 
         self._update_plot_values(cinfo_click.extent)
+
+    def _determine_behaviour(self, click_event, release_event) -> UserInteraction:
+        """
+        Determine if the user drew another rectangle, moved the currently selected one, changed it, or deselected.
+        A bit heuristical since RectangleSelector does not bother telling us what kind of event triggered
+         _on_rectangle_selected, only the end result.
+        TODO
+        """
+
+        # if the start and end points are the same, it's a click
+        if click_event.xdata == release_event.xdata and click_event.ydata == release_event.ydata:
+            print("click")
+            return UserInteraction.RECTANGLE_SELECTED
+
+        if not self._current_rectangle:
+            return UserInteraction.RECTANGLE_CREATED
+
+        x0, y0 = self._current_rectangle.get_xy()
+        x1 = x0 + self._current_rectangle.get_width()
+        y1 = y0 + self._current_rectangle.get_height()
+
+        # TODO float equality is a bad idea, change to epsilon
+        if x0 == click_event.xdata or x0 == release_event.xdata or x1 == click_event.xdata or x1 == release_event.xdata:
+            if y0 == click_event.ydata or y0 == release_event.ydata or y1 == click_event.ydata or y1 == release_event.ydata:
+                return UserInteraction.RECTANGLE_RESHAPED
+
+        if self._current_rectangle.get_width() == abs(click_event.xdata - release_event.xdata) and \
+           self._current_rectangle.get_height() == abs(click_event.ydata - release_event.ydata):
+            return UserInteraction.RECTANGLE_MOVED
+
+        return UserInteraction.RECTANGLE_CREATED
 
     def _draw_rectangle(self, point: tuple, width: float, height: float):
         """
@@ -50,6 +103,7 @@ class MultipleRectangleSelectionLinePlot(KeyHandler):
         rectangle_patch = Rectangle(point, width, height, edgecolor="black", facecolor='none', alpha=.7)
         self.plotter.image_axes.add_patch(rectangle_patch)
         self._rectangles.append(rectangle_patch)
+        self._current_rectangle = rectangle_patch
 
     def _update_plot_values(self, window_range: tuple):
         """
@@ -133,3 +187,8 @@ class MultipleRectangleSelectionLinePlot(KeyHandler):
             CreateWorkspace(DataX=x_line_x_axis, DataY=x_line_y_values, OutputWorkspace="x_cut")
         if key in ('c', 'y'):
             CreateWorkspace(DataX=y_line_x_axis, DataY=y_line_y_values, OutputWorkspace="y_cut")
+
+
+def is_the_same(point_a, point_b, epsilon):
+    return ((point_a[0] == 0 and point_b[0] == 0) or abs(point_a[0] - point_b[0]) / max(abs(point_a[0]), abs(point_b[0])) < epsilon) \
+            and ((point_a[1] == 0 and point_b[1] == 0) or abs(point_a[1] - point_b[1]) / max(abs(point_a[1]), abs(point_b[1])) < epsilon)
