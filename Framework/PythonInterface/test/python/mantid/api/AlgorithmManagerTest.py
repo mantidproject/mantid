22be8476161ccaab776b752a4dfd97868d3ba702
Gemma Guest <gemma.guest@stfc.ac.uk>
2022-05-13 18:59:49 +0100

    Add new unit tests to exercise setProperty from python
    Two tests set a function property using a function created a) from function factory and b) using the simpleapi.
    Two tests set a workspace property using a workspace created in a similar manner.
    
    The workspace tests pass because the python exports take into account proper handling of shared_ptrs. The function tests fail because they do not. The first function test segfaults during tearDown (due to the shared_ptr lifetime issues). The second one fails in a different place; I haven't investigated why.
    
    Re #33894

--- Framework/PythonInterface/test/python/mantid/api/AlgorithmManagerTest.py
+++ Framework/PythonInterface/test/python/mantid/api/AlgorithmManagerTest.py
@@ -7,7 +7,8 @@
 import unittest
 import testhelpers
 from mantid.api import (AlgorithmManager, Algorithm,
-                        FrameworkManagerImpl, IAlgorithm)
+                        FrameworkManagerImpl, IAlgorithm, FunctionFactory, WorkspaceFactory)
+from mantid.simpleapi import (CreateWorkspace, Gaussian)
 
 
 class AlgorithmManagerTest(unittest.TestCase):
@@ -16,6 +17,9 @@ class AlgorithmManagerTest(unittest.TestCase):
     def setUpClass(cls):
         # Load the plugins
         FrameworkManagerImpl.Instance()
+
+    def tearDown(self):
+        AlgorithmManager.clear()
 
     def test_create_default_version(self):
         alg = testhelpers.assertRaisesNothing(self, AlgorithmManager.create, "ConvertUnits")
@@ -40,9 +44,12 @@ class AlgorithmManagerTest(unittest.TestCase):
     def test_size_reports_number_of_managed_algorithms(self):
         # no longer deterministically possible to have a correct answer for size
         # if test are run multi threaded
-        # just check we got an integer back
+        # just check we got an integer back and that it is greater than zero if we
+        # have created at least one managed algorithm
+        AlgorithmManager.create("ConvertUnits")
         size = AlgorithmManager.size()
         self.assertTrue(isinstance(size, int))
+        self.assertGreater(size, 0)
 
     def test_getAlgorithm_returns_correct_instance(self):
         returned_instance = AlgorithmManager.create("ConvertUnits")
@@ -88,11 +95,50 @@ class AlgorithmManagerTest(unittest.TestCase):
             pause_thread.algorithm.cancel()
             pause_thread.join()
 
-
     def test_clear_removes_all_managed_algorithms(self):
+        AlgorithmManager.create("ConvertUnits")
         AlgorithmManager.clear()
-        new_size = AlgorithmManager.size()
-        self.assertEqual(0, new_size)
+        self.assertEqual(0, AlgorithmManager.size())
+
+    def test_clear_after_function_exit_with_set_function_property(self):
+        # This test checks that shared_ptrs are handled correctly when using setProperty with a function
+        # created using function factory.
+        # We deliberately:
+        # - Use AlgorithmManger.create
+        # - leave AlgorithmManager.clear function to be called by tearDown
+        func = FunctionFactory.createFunction('Gaussian')
+        alg = AlgorithmManager.create('Fit')
+        alg.setProperty('Function', func)
+
+    def test_clear_after_function_exit_with_set_function_property_using_simpleapi(self):
+        # This test checks that shared_ptrs are handled correctly when using setProperty with a function
+        # created using the simpleapi.
+        # We deliberately:
+        # - Use AlgorithmManger.create
+        # - leave AlgorithmManager.clear function to be called by tearDown
+        func = Gaussian()
+        alg = AlgorithmManager.create('Fit')
+        alg.setProperty('Function', func)
+
+    def test_clear_after_function_exit_with_set_workspace_property(self):
+        # This test checks that shared_ptrs are handled correctly when using setProperty with a workspace
+        # created using workspace factory.
+        # We deliberately:
+        # - Use AlgorithmManger.create
+        # - leave AlgorithmManager.clear function to be called by tearDown
+        ws = WorkspaceFactory.create('Workspace2D', 1, 1, 1)
+        alg = AlgorithmManager.create('Rebin')
+        alg.setProperty('InputWorkspace', ws)
+
+    def test_clear_after_function_exit_with_set_workspace_property_using_simpleapi(self):
+        # This test checks that shared_ptrs are handled correctly when using setProperty with a workspace
+        # created using the simpleapi.
+        # We deliberately:
+        # - Use AlgorithmManger.create
+        # - leave AlgorithmManager.clear function to be called by tearDown
+        ws = CreateWorkspace([0, 1, 2], [0, 1, 2])
+        alg = AlgorithmManager.create('Rebin')
+        alg.setProperty('InputWorkspace', ws)
 
 
 if __name__ == '__main__':
