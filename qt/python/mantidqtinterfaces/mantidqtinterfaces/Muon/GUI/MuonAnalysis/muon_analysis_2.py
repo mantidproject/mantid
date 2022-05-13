916011ff24a3e64fd43016dbcc4c347f38d55933
Anthony <anthony.lim@stfc.ac.uk>
2022-05-13 11:46:50 +0100

    fixed missing unsubscribe in muon GUI's

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/Muon/GUI/MuonAnalysis/muon_analysis_2.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Muon/GUI/MuonAnalysis/muon_analysis_2.py
@@ -483,4 +483,5 @@ class MuonAnalysisGui(QtWidgets.QMainWindow):
         self.tabs.closeEvent(event)
         self.context.ads_observer.unsubscribe()
         self.grouping_tab_widget.group_tab_presenter.closePeriodInfoWidget()
+        self.results_context.ADS_observer.unsubscribe()
         super(MuonAnalysisGui, self).closeEvent(event)
