Refs #33883. Fix log normalisation bug for masked data.

qt/python/mantidqt/mantidqt/widgets/colorbar/colorbar.py
@@ -351,7 +351,7 @@ class ColorbarWidget(QWidget):
         """Disable the Log option if no positive value can be found from given data (image)"""
         index = NORM_OPTS.index("Log")
         if mappable.get_array() is not None:
-            if np.all(mappable.get_array() <= 0):
+            if np.all(mappable.get_array() <= 0) or mappable.get_array().all() is np.ma.masked:
                 self.norm.model().item(index, 0).setEnabled(False)
                 self.norm.setItemData(index, "Log scale is disabled for non-positive data",
                                       Qt.ToolTipRole)

qt/python/mantidqt/mantidqt/widgets/colorbar/test/test_colorbar.py
@@ -7,10 +7,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from unittest import mock, TestCase
+from unittest import TestCase
 
 from mantidqt.utils.qt.testing import start_qapplication
 from mantidqt.widgets.colorbar.colorbar import ColorbarWidget
+from matplotlib.colors import LogNorm, Normalize, SymLogNorm
 
 
 @start_qapplication
@@ -19,37 +20,38 @@ class ColorbarWidgetTest(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.data = np.arange(100.0).reshape((10, 10))
-        cls.masked_data = np.ma.masked_array(cls.data, mask=[list(range(1, 11))]*10)
+        cls.masked_data = np.ma.masked_array(cls.data, mask=True)
 
     def setUp(self) -> None:
         self.fig = plt.figure(figsize=(1, 1))
-        self.image = plt.imshow(self.data, cmap="plasma")
-        self.masked_image = plt.imshow(self.masked_data, cmap="plasma")
 
         self.widget = ColorbarWidget()
         self.widget.cmin_value = 0
-        self.widget.set_mappable(self.image)
 
     def tearDown(self) -> None:
         plt.close('all')
-        self.fig, self.image, self.masked_image = None, None, None
-        self.widget = None
+        self.fig, self.widget = None, None
 
-    def test_that_masked_data_will_not_allow_negative_cmin_for_log_normalisation(self):
+    def test_that_masked_data_in_log_mode_will_cause_a_switch_back_to_linear_normalisation(self):
         normalisation_index = 1  # Log
+        image = plt.imshow(self.data, cmap="plasma", norm=LogNorm(vmin=0.01, vmax=1))
+        masked_image = plt.imshow(self.masked_data, cmap="plasma", norm=LogNorm(vmin=0.01, vmax=1))
 
+        self.widget.set_mappable(image)
         self.widget.norm.setCurrentIndex(normalisation_index)
-        self.widget.set_mappable(self.masked_image)
+        self.widget.set_mappable(masked_image)
 
-        self.assertGreaterEqual(self.widget.cmin_value, 0.0)
+        self.assertEqual(self.widget.norm.currentIndex(), 0)  # Linear
+        self.assertTrue(isinstance(masked_image.norm, Normalize))
 
-    def test_that_masked_data_will_not_cause_error_when_switching_to_symmetric_log_normalisation(self):
+    def test_that_masked_data_in_symmetric_log_mode_will_cause_a_switch_back_to_linear_normalisation(self):
         normalisation_index = 2  # SymmetricLog10
+        image = plt.imshow(self.data, cmap="plasma", norm=SymLogNorm(1e-8, vmin=0.01, vmax=1))
+        masked_image = plt.imshow(self.masked_data, cmap="plasma", norm=SymLogNorm(1e-8, vmin=0.01, vmax=1))
 
+        self.widget.set_mappable(image)
         self.widget.norm.setCurrentIndex(normalisation_index)
-        self.widget.set_mappable(self.masked_image)
+        self.widget.set_mappable(masked_image)
 
-        with mock.patch('qtpy.QtWidgets.QComboBox.currentIndex') as patch:
-            patch.return_value = normalisation_index
-
-            self.widget.norm_changed()
+        self.assertEqual(self.widget.norm.currentIndex(), 0)  # Linear
+        self.assertTrue(isinstance(masked_image.norm, Normalize))