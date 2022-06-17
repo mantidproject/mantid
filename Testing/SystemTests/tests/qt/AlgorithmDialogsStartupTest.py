fb42ec8417562e6642d3f5098f3c4478b9b17b3d
Robert Applin <robert.applin@stfc.ac.uk>
2022-06-17 10:06:54 +0100

    Run all tests on workspaces in separate system test classes

--- Testing/SystemTests/tests/qt/AlgorithmDialogsStartupTest.py
+++ Testing/SystemTests/tests/qt/AlgorithmDialogsStartupTest.py
@@ -4,8 +4,8 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
-import random
-import systemtesting
+from abc import ABCMeta, abstractmethod
+from systemtesting import MantidSystemTest
 
 from mantid.api import AlgorithmFactory, AlgorithmManager
 from mantid.simpleapi import CreateSampleWorkspace, CreateMDWorkspace, GroupWorkspaces
@@ -17,20 +17,6 @@ from qtpy.QtCore import Qt
 # located at PyQt5X.sip. Importing PyQt first sets a shim sip module to point
 # to the correct place
 import sip
-
-WORKSPACE_TYPES = ["MatrixWorkspace", "MDWorkspace", "WorkspaceGroup"]
-
-# These algorithms currently fail to open due to an error when the given workspace types are auto-selected from the ADS
-EXCLUDE_ALGORITHM = {"MatrixWorkspace": ["HB2AReduce", "MuonPairingAsymmetry"],
-                     "MDWorkspace": ["HB2AReduce", "MaskAngle", "MuonPairingAsymmetry", "TOFTOFCropWorkspace",
-                                     "VesuvioResolution"],
-                     "WorkspaceGroup": ["AnvredCorrection", "CalculateDynamicRange", "CopyDataRange", "CreateEPP",
-                                        "CropToComponent", "CylinderPaalmanPingsCorrection",
-                                        "EnggEstimateFocussedBackground", "FlatPlatePaalmanPingsCorrection",
-                                        "GetDetectorOffsets", "HB2AReduce", "HB3APredictPeaks", "LorentzCorrection",
-                                        "MaskBinsIf", "MaskNonOverlappingBins", "MSDFit", "MuonPairingAsymmetry",
-                                        "PDConvertRealSpace", "RebinRagged", "SetMDFrame", "Stitch1D", "Symmetrise",
-                                        "TOFTOFCropWorkspace", "VesuvioResolution", "XrayAbsorptionCorrection"]}
 
 
 def create_workspace_in_ads(workspace_type: str) -> None:
@@ -45,21 +31,17 @@ def create_workspace_in_ads(workspace_type: str) -> None:
         ws2 = CreateSampleWorkspace()
         GroupWorkspaces(InputWorkspaces=[ws1, ws2], OutputWorkspace="group")
     else:
-        raise RuntimeError("An unexpected workspace type was created.")
+        raise RuntimeError("An unexpected workspace type was provided.")
 
 
-class AlgorithmDialogsStartupTest(systemtesting.MantidSystemTest):
+class AlgorithmDialogsStartupTestBase(MantidSystemTest, metaclass=ABCMeta):
     """
-    A system test for testing that the Algorithm Dialogs open ok with different workspaces in the ADS.
-
-    In order to avoid an extremely long-running test:
-    - The test will only open the most recent version of an algorithm.
-    - The test will choose a random workspace type from WORKSPACE_TYPES each time it is run. This reduces the time it
-    takes to run the test, but we will still have some degree of coverage as each workspace type is run 1 in 3 times.
+    The base class for a system tests that test the Algorithm Dialogs open ok with different workspaces in the ADS.
+    The test will only open the most recent version of an algorithm.
     """
+
     def __init__(self):
-        super(AlgorithmDialogsStartupTest, self).__init__()
-
+        super(AlgorithmDialogsStartupTestBase, self).__init__()
         self._app = get_application()
         self._interface_manager = InterfaceManager()
 
@@ -67,19 +49,26 @@ class AlgorithmDialogsStartupTest(systemtesting.MantidSystemTest):
         # Remove duplicate algorithm names as we will only test the most recent versions
         self._unique_algorithm_names = list(dict.fromkeys(algorithm_names))
 
+        self._setup_test()
+
+    @abstractmethod
+    def _setup_test(self):
+        raise NotImplementedError("This method needs implementing in a subclass of this class.")
+
     def runTest(self) -> None:
         """Run the test for a random workspace type as a speed-up measure whilst still having a degree of coverage"""
         if len(self._unique_algorithm_names) == 0:
             self.fail("Failed to find any of the Algorithms.")
 
-        workspace_type = random.choice(WORKSPACE_TYPES)
-        create_workspace_in_ads(workspace_type)
+        create_workspace_in_ads(self._workspace_type)
 
-        print(f"Running the AlgorithmDialogsStartupTest with a '{workspace_type}' in the ADS")
+        print(f"Running the AlgorithmDialogsStartupTest with a '{self._workspace_type}' in the ADS")
+        print(f"Excluding the following algorithms from the test: '{str(self._exclude_algorithms)}'")
 
         for algorithm_name in self._unique_algorithm_names:
-            if algorithm_name not in EXCLUDE_ALGORITHM[workspace_type]:
-                self._attempt_to_open_algorithm_dialog(workspace_type, algorithm_name)
+            if algorithm_name not in self._exclude_algorithms:
+                # print(algorithm_name)  # Useful for debugging when an algorithm dialog crashes
+                self._attempt_to_open_algorithm_dialog(self._workspace_type, algorithm_name)
 
     def _attempt_to_open_algorithm_dialog(self, workspace_type: str, algorithm_name: str) -> None:
         """Attempt to open the most recent version of the algorithm provided."""
@@ -102,3 +91,41 @@ class AlgorithmDialogsStartupTest(systemtesting.MantidSystemTest):
         """We want to set the OutputWorkspace on algorithms when possible so that validateInputs gets triggered."""
         algorithm = AlgorithmManager.create(algorithm_name)
         return {"OutputWorkspace": "test_output"} if algorithm.existsProperty("OutputWorkspace") else {}
+
+
+class AlgorithmDialogsStartupMatrixWorkspaceTest(AlgorithmDialogsStartupTestBase):
+    """
+    A system test for testing that the Algorithm Dialogs open ok with a MatrixWorkspace in the ADS.
+    """
+
+    def _setup_test(self):
+        self._workspace_type = "MatrixWorkspace"
+        # These algorithms currently fail to open when the given workspace type is auto-selected from the ADS
+        self._exclude_algorithms = ["HB2AReduce", "MaskBinsIf", "MuonPairingAsymmetry"]
+
+
+class AlgorithmDialogsStartupMDWorkspaceTest(AlgorithmDialogsStartupTestBase):
+    """
+    A system test for testing that the Algorithm Dialogs open ok with a MDWorkspace in the ADS.
+    """
+    def _setup_test(self):
+        self._workspace_type = "MDWorkspace"
+        # These algorithms currently fail to open when the given workspace type is auto-selected from the ADS
+        self._exclude_algorithms = ["HB2AReduce", "MaskAngle", "MuonPairingAsymmetry", "TOFTOFCropWorkspace",
+                                    "VesuvioResolution"]
+
+
+class AlgorithmDialogsStartupWorkspaceGroupTest(AlgorithmDialogsStartupTestBase):
+    """
+    A system test for testing that the Algorithm Dialogs open ok with a WorkspaceGroup in the ADS.
+    """
+    def _setup_test(self):
+        self._workspace_type = "WorkspaceGroup"
+        # These algorithms currently fail to open when the given workspace type is auto-selected from the ADS
+        self._exclude_algorithms = ["AnvredCorrection", "CalculateDynamicRange", "CopyDataRange", "CreateEPP",
+                                    "CropToComponent", "CylinderPaalmanPingsCorrection",
+                                    "EnggEstimateFocussedBackground", "FlatPlatePaalmanPingsCorrection",
+                                    "GetDetectorOffsets", "HB2AReduce", "HB3APredictPeaks", "LorentzCorrection",
+                                    "MaskBinsIf", "MaskNonOverlappingBins", "MSDFit", "MuonPairingAsymmetry",
+                                    "PDConvertRealSpace", "RebinRagged", "SetMDFrame", "Stitch1D", "Symmetrise",
+                                    "TOFTOFCropWorkspace", "VesuvioResolution", "XrayAbsorptionCorrection"]
