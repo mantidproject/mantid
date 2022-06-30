92ff76ad47d2d226fedbda17aa331627c2cd6573
Robert Applin <robert.applin@stfc.ac.uk>
2022-06-30 11:41:02 +0100

    Add cpp interfaces to documentation system test

--- Testing/SystemTests/tests/qt/HelpDocsExistForInterfacesTest.py
+++ Testing/SystemTests/tests/qt/HelpDocsExistForInterfacesTest.py
@@ -8,20 +8,29 @@ from systemtesting import MantidSystemTest
 
 from mantid.kernel import ConfigService
 from mantidqt.interfacemanager import InterfaceManager
-#from mantidqt.usersubwindowfactory import UserSubWindowFactory
-from mantidqt.utils.qt.testing import get_application
+from mantidqt.usersubwindowfactory import UserSubWindowFactory
 
 # These interfaces are excluded for now as they do not have documentation
-EXCLUDE_INTERFACES = ["DGS_Reduction", "Elemental_Analysis", "ORNL_SANS", "Powder_Diffraction_Reduction"]
+EXCLUDE_INTERFACES = ["DGS_Reduction", "Elemental_Analysis", "ORNL_SANS", "Powder_Diffraction_Reduction",
+                      "Step Scan Analysis"]
 
 # A map of interface name to documentation category and page. We should remove this once the naming of interfaces and
 # their documentation page has been harmonized using a naming convention
-INTERFACE_DOCS = {"DGSPlanner": ("direct", "DGS Planner"),
+INTERFACE_DOCS = {"ALC": ("muon", "Muon ALC"),
+                  "Bayes": ("indirect", "Indirect Bayes"),
+                  "Corrections": ("indirect", "Indirect Corrections"),
+                  "Data Analysis": ("indirect", "Inelastic Data Analysis"),
+                  "Data Reduction": ("indirect", "Indirect Data Reduction"),
+                  "DGSPlanner": ("direct", "DGS Planner"),
+                  "Diffraction": ("indirect", "Indirect Diffraction"),
                   "Drill": ("ILL", "DrILL"),
                   "FilterEvents": ("utility", "Filter Events"),
                   "HFIR_4Circle_Reduction": ("diffraction", "HFIR Single Crystal Reduction"),
                   "ISIS_SANS": ("isis_sans", "ISIS SANS"),
+                  "Settings": ("indirect", "Indirect Settings"),
+                  "Simulation": ("indirect", "Indirect Simulation"),
                   "TofConverter": ("utility", "TOF Converter"),
+                  "Tools": ("indirect", "Indirect Tools"),
                   "QECoverage": ("utility", "QE Coverage")}
 
 
@@ -32,17 +41,15 @@ class HelpDocsExistForInterfacesTest(MantidSystemTest):
 
     def __init__(self):
         super(HelpDocsExistForInterfacesTest, self).__init__()
+        self._interface_manager = InterfaceManager()
+        self._sub_window_factory = UserSubWindowFactory.Instance()
+
         python_interfaces = [interface.split(".")[0].split("/")
                              for interface in ConfigService.getString('mantidqt.python_interfaces').split()]
-        cpp_interfaces = []
+        cpp_interfaces = [[self._sub_window_factory.categories(interface_name).pop(), interface_name]
+                          for interface_name in self._sub_window_factory.keys()]
 
-        self._interface_manager = InterfaceManager()
         self._interfaces = python_interfaces + cpp_interfaces
-        self._qapp = get_application()
-        #self._cpp_interfaces = UserSubWindowFactory.Instance().keys()
-
-    def cleanup(self):
-        self._qapp = None
 
     def runTest(self):
         if len(self._interfaces) == 0:
@@ -60,9 +67,3 @@ class HelpDocsExistForInterfacesTest(MantidSystemTest):
         print(url)  # Useful for debugging
         if not self._interface_manager.doesHelpPageExist(url):
             self.fail(f"Missing an interface help page for '{category}/{doc_name}'. Tried the following url:\n {url}")
-
-    def _attempt_to_open_cpp_help_window(self, interface_name):
-        """Attempt to open the help documentation for a cpp interface."""
-        print(interface_name)
-        interface = self._interface_manager.createSubWindow(interface_name)
-        self._interface_manager.showCustomInterfaceHelp(interface_name, interface.categoryInfo())
