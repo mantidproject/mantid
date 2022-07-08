368a0d557454b231d1560754fe9a1803a926b873
Mathieu Tillet <tillet@ill.eu>
2022-07-08 09:12:42 +0200

    Update copyrights

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/scanexplorer/rectangle_selection.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/scanexplorer/rectangle_selection.py
@@ -1,3 +1,11 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2022 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantid workbench.
+
 from matplotlib.widgets import RectangleSelector
 
 
