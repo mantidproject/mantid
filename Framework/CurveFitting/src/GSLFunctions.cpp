fa11d41d4e238191c0feea8cb32a315b5e4ddc22
MialLewis <95620982+MialLewis@users.noreply.github.com>
2022-08-10 14:04:31 +0100

    Change JacobianImpl1 to Eigen

--- Framework/CurveFitting/src/GSLFunctions.cpp
+++ Framework/CurveFitting/src/GSLFunctions.cpp
@@ -83,8 +83,11 @@ int gsl_f(const gsl_vector *x, void *params, gsl_vector *f) {
 int gsl_df(const gsl_vector *x, void *params, gsl_matrix *J) {
 
   auto *p = reinterpret_cast<struct GSL_FitData *>(params);
-
-  p->J.setJ(J);
+  gsl_matrix *J_tr = gsl_matrix_calloc(J->size2, J->size1);
+  gsl_matrix_transpose_memcpy(J_tr, J);
+  EigenMatrix m(J_tr->size1, J_tr->size2);
+  std::copy(&m.mutator().data()[0], &m.mutator().data()[J_tr->size1 * J_tr->size2], &J_tr->data[0]);
+  p->J.setJ(&m);
 
   // update function parameters
   if (x->data) {
