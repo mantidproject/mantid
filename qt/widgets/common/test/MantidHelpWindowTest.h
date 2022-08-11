c917dfcdefb497f68dda53ba9d55f4e0d8c586a6
Robert Applin <robert.applin@stfc.ac.uk>
2022-08-11 15:19:47 +0100

    Remove unneeded tests from MantidHelpWindowTest

--- qt/widgets/common/test/MantidHelpWindowTest.h
+++ qt/widgets/common/test/MantidHelpWindowTest.h
@@ -7,13 +7,9 @@
 #pragma once
 
 #include "MantidAPI/FrameworkManager.h"
-#include "MantidKernel/Instantiator.h"
-#include "MantidQtWidgets/Common/InterfaceManager.h"
-#include "MantidQtWidgets/Common/MantidHelpInterface.h"
 #include "MantidQtWidgets/Common/MantidHelpWindow.h"
 
 #include <QApplication>
-#include <QObject>
 #include <QTimer>
 #include <QWidget>
 
@@ -36,28 +32,16 @@ public:
 
   void tearDown() override { assertNoTopLevelWidgets(); }
 
-  void test_the_mantid_help_window_can_be_opened_and_closed_once_without_a_parent_widget() {
-    openHelpInterfaceWithoutParent();
-  }
+  void test_the_mantid_help_window_can_be_opened_and_closed_once() { openHelpInterface(); }
 
-  // void test_the_mantid_help_window_can_be_opened_and_closed_once_with_a_parent_widget() {
-  //  openHelpInterfaceWithParent();
-  //}
-
-  void test_the_mantid_help_window_can_be_opened_and_closed_multiple_times_without_a_parent_widget() {
+  void test_the_mantid_help_window_can_be_opened_and_closed_multiple_times() {
     for (auto i = 0u; i < m_openAttempts; ++i) {
-      openHelpInterfaceWithoutParent();
+      openHelpInterface();
     }
   }
 
-  // void test_the_mantid_help_window_can_be_opened_and_closed_multiple_times_with_a_parent_widget() {
-  //  for (auto i = 0u; i < m_openAttempts; ++i) {
-  //    openHelpInterfaceWithParent();
-  //  }
-  //}
-
 private:
-  void openHelpInterfaceWithoutParent() {
+  void openHelpInterface() {
     // Assert widget is created
     auto helpInterface = new MantidHelpWindow();
     helpInterface->setAttribute(Qt::WA_DeleteOnClose);
@@ -72,29 +56,10 @@ private:
     assertNoTopLevelWidgets();
   }
 
-  // void openHelpInterfaceWithParent() {
-  //  const auto factory = new Mantid::Kernel::Instantiator<MantidHelpWindow, MantidHelpInterface, QWidget *>();
-
-  //  QWidget *parent = new QWidget();
-  //  parent->setAttribute(Qt::WA_DeleteOnClose);
-
-  //  // Assert widget is created
-  //  auto helpInterface = factory->createUnwrappedInstance(parent);
-  //  helpInterface->showPage(m_url);
-  //  assertWidgetCreated();
-
-  //  // Assert widget is closed
-  //  QTimer::singleShot(0, parent, &QWidget::close);
-  //  QApplication::sendPostedEvents();
-  //  QApplication::sendPostedEvents();
-
-  //  assertNoTopLevelWidgets();
-  //}
-
   void assertWidgetCreated() { TS_ASSERT_LESS_THAN(0, QApplication::topLevelWidgets().size()); }
 
   void assertNoTopLevelWidgets() { TS_ASSERT_EQUALS(0, QApplication::topLevelWidgets().size()); }
 
-  std::size_t m_openAttempts{1000u};
+  std::size_t m_openAttempts{5u};
   std::string m_url{"qthelp://org.mantidproject/doc/interfaces/direct/MSlice.html"};
 };
