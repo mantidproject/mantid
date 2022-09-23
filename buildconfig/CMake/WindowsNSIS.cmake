847497bbde8a4e29f15c54265ce9553294a51e40
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2022-09-23 17:03:33 +0100

    Remove mantid_notebook

--- buildconfig/CMake/WindowsNSIS.cmake
+++ buildconfig/CMake/WindowsNSIS.cmake
@@ -113,31 +113,6 @@ set(CPACK_NSIS_ADDITIONAL_PLUGIN_UNICODE_DIR
     "!addplugindir /x86-unicode ${THIRD_PARTY_DIR}/share/nsis-plugins/Plugins/x86-unicode"
 )
 
-# Notebook
-set(WINDOWS_NSIS_MANTIDNOTEBOOK_ICON_NAME "mantid_notebook${CPACK_PACKAGE_SUFFIX}")
-set(MANTIDNOTEBOOK_LINK_NAME "Mantid Notebook ${CPACK_PACKAGE_SUFFIX_CAMELCASE}.lnk")
-install(FILES "${PACKAGE_IMAGES_DIR}\\\\${WINDOWS_NSIS_MANTIDNOTEBOOK_ICON_NAME}.ico" DESTINATION bin)
-set(CPACK_NSIS_CREATE_ICONS_EXTRA
-    "
-  CreateShortCut '$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\${MANTIDNOTEBOOK_LINK_NAME}' '$INSTDIR\\\\bin\\\\mantidpython.bat' 'notebook --notebook-dir=%userprofile%' '$INSTDIR\\\\bin\\\\${WINDOWS_NSIS_MANTIDNOTEBOOK_ICON_NAME}.ico'
-"
-)
-set(CPACK_NSIS_DELETE_ICONS_EXTRA
-    "
-  Delete \\\"$SMPROGRAMS\\\\$MUI_TEMP\\\\${MANTIDNOTEBOOK_LINK_NAME}\\\"
-"
-)
-set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
-    "
-  CreateShortCut '$DESKTOP\\\\${MANTIDNOTEBOOK_LINK_NAME}' '$INSTDIR\\\\bin\\\\mantidpython.bat' 'notebook --notebook-dir=%userprofile%' '$INSTDIR\\\\bin\\\\${WINDOWS_NSIS_MANTIDNOTEBOOK_ICON_NAME}.ico'
-"
-)
-set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS
-    "
-  Delete \\\"$DESKTOP\\\\${MANTIDNOTEBOOK_LINK_NAME}\\\"
-"
-)
-
 # Workbench
 if(ENABLE_WORKBENCH)
   include(WindowsNSISQt5)
