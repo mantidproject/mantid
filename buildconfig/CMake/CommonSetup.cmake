43534531cba5ccfd99ef2d74ab6e926e6d6fb262
Applin <robert.applin@stfc.ac.uk>
2022-10-10 13:27:17 +0100

    Only setup ccache for non-multi configuration generator

--- buildconfig/CMake/CommonSetup.cmake
+++ buildconfig/CMake/CommonSetup.cmake
@@ -301,7 +301,10 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 # ######################################################################################################################
 # Setup ccache
 # ######################################################################################################################
-include(CCacheSetup)
+get_property(_is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+if(NOT _is_multi_config)
+  include(CCacheSetup)
+endif()
 
 # ######################################################################################################################
 # Add compiler options if using gcc
