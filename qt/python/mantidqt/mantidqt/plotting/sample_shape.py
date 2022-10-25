2b780bbc750eea2a4382c0c2ef76dee8bb71adb3
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2022-10-25 16:37:53 +0100

    Set axes limits equal and by largest mesh limits

--- qt/python/mantidqt/mantidqt/plotting/sample_shape.py
+++ qt/python/mantidqt/mantidqt/plotting/sample_shape.py
@@ -7,7 +7,7 @@
 #  This file is part of the mantid workbench.
 #
 #
-
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
@@ -33,16 +33,23 @@ def get_valid_sample_shape_from_workspace(workspace_name):
 
 
 def plot_sample_shape(workspace_name):
-    sample_shape_figure = plot_sample_only(workspace_name)
     workspace = ADS.retrieve(workspace_name)
+    figure, axes = plt.subplots(subplot_kw={'projection': 'mantid3d'})
+    plot_sample_only(workspace, figure)
     if workspace.sample().hasEnvironment():
-        plot_container_and_components(workspace_name, sample_shape_figure)
-    return sample_shape_figure
+        plot_container_and_components(workspace, figure)
+
+    set_axes_to_largest_mesh(axes, workspace)
+    axes.view_init(elev=10, azim=-150)
+    axes.set_xlabel('X / m')
+    axes.set_ylabel('Y / m')
+    axes.set_zlabel('Z / m')
+    figure.show()
+    return figure
 
 
-def plot_sample_only(workspace_name):
-    workspace = ADS.retrieve(workspace_name)
-
+def plot_sample_only(workspace, figure):
+    axes = figure.gca()
     # get shape and mesh vertices
     sample = workspace.sample()
     shape = sample.getShape()
@@ -51,23 +58,14 @@ def plot_sample_only(workspace_name):
     # Create 3D Polygon and set facecolor
     mesh_polygon = Poly3DCollection(mesh, facecolors=['g'], edgecolors=['b'], alpha=0.5, linewidths=0.1)
     mesh_polygon.set_facecolor((0, 1, 0, 0.5))
-    fig, axes = plt.subplots(subplot_kw={'projection': 'mantid3d'})
     axes.add_collection3d(mesh_polygon)
+    axes.set_title(f'Sample: {workspace.name()}')
 
-    axes.set_mesh_axes_equal(mesh)
-    axes.view_init(elev=10, azim=-150)
-    axes.set_title(f'Sample Shape: {workspace_name}')
-    axes.set_xlabel('X / m')
-    axes.set_ylabel('Y / m')
-    axes.set_zlabel('Z / m')
-
-    fig.show()
-    return fig
+    return True
 
 
-def plot_container_and_components(workspace_name, figure):
+def plot_container_and_components(workspace, figure):
     axes = figure.gca()
-    workspace = ADS.retrieve(workspace_name)
     environment = workspace.sample().getEnvironment()
 
     number_of_components = environment.nelements()
@@ -81,9 +79,57 @@ def plot_container_and_components(workspace_name, figure):
         mesh_polygon_loop.set_facecolor((0, 1, 0, 0.5))
         axes.add_collection3d(mesh_polygon_loop)
 
-    axes.set_title('Sample and Components')
-    figure.show()
+    axes.set_title(f'Sample and Components: {workspace.name()}')
     return True
+
+
+def call_set_mesh_axes_equal(axes, mesh):
+    # not able to directly mock set_mesh_axes_equal() as part of MantidAxes3D, which inherits from matplotlib code
+    axes.set_mesh_axes_equal(mesh)
+
+
+def set_axes_to_largest_mesh(axes, workspace):
+    overall_min, overall_max = overall_limits_for_all_meshes(workspace)
+    call_set_mesh_axes_equal(axes, np.array(overall_min, overall_max))
+
+
+def overall_limits_for_all_meshes(workspace, include_components=True):
+    sample_mesh_limits = workspace.sample().getShape().getMesh().flatten()
+    overall_minimum, overall_maximum = overall_limits_for_every_axis(sample_mesh_limits)
+
+    if include_components and workspace.sample().hasEnvironment():
+        environment = workspace.sample().getEnvironment()
+        number_of_components = environment.nelements()
+        for component_index in range(number_of_components):
+            if component_index == 0:  # Container
+                loop_component_mesh = environment.getComponent(component_index).getShape().getMesh()
+            else:
+                loop_component_mesh = environment.getComponent(component_index).getMesh()
+            current_mesh_minimum, current_mesh_maximum = overall_limits_for_every_axis(loop_component_mesh)
+            overall_minimum, overall_maximum = greater_limits(current_mesh_minimum, current_mesh_maximum,
+                                                              overall_minimum, overall_maximum)
+    return overall_minimum, overall_maximum
+
+
+def overall_limits_for_every_axis(flattened_mesh):
+    minimum_x = flattened_mesh[0].min()
+    maximum_x = flattened_mesh[0].max()
+    minimum_y = flattened_mesh[1].min()
+    maximum_y = flattened_mesh[1].max()
+    minimum_z = flattened_mesh[2].min()
+    maximum_z = flattened_mesh[2].max()
+    return min(minimum_x, minimum_y, minimum_z), max(maximum_x, maximum_y, maximum_z)
+
+
+def greater_limits(new_minimum, new_maximum, old_minimum, old_maximum):
+    minimum, maximum = new_minimum, new_maximum
+    if old_minimum:
+        if old_minimum < minimum:
+            minimum = old_minimum
+    if old_maximum:
+        if old_maximum > maximum:
+            maximum = old_maximum
+    return minimum, maximum
 
 #
 # def arrow(ax, vector, origin = None, factor = None, color = 'black',linestyle = '-'):
--- qt/python/mantidqt/mantidqt/plotting/test/test_sample_shape.py
+++ qt/python/mantidqt/mantidqt/plotting/test/test_sample_shape.py
@@ -9,9 +9,10 @@
 #
 
 from unittest import TestCase, main
+from unittest.mock import patch
+import numpy as np
 import matplotlib
 matplotlib.use('AGG')  # noqa
-# import matplotlib.pyplot as plt
 
 from mantid.api import AnalysisDataService as ADS
 from mantid.simpleapi import (CreateWorkspace, CreateSampleWorkspace, SetSample, LoadSampleShape, DeleteWorkspace,
@@ -47,14 +48,15 @@ def setup_workspace_shape_from_mesh(workspace_name: str):
 
 def setup_workspace_sample_container_and_components_from_mesh(workspace_name: str):
     CreateWorkspace(OutputWorkspace=workspace_name, DataX=[1, 1], DataY=[2, 2])
-    LoadInstrument(Workspace=workspace_name,RewriteSpectraMap=True,InstrumentName="Pearl")
+    LoadInstrument(Workspace=workspace_name, RewriteSpectraMap=True, InstrumentName="Pearl")
     SetSample(workspace_name, Environment={'Name': 'Pearl'})
 
 
 class PlotSampleShapeTest(TestCase):
 
     def tearDown(self) -> None:
-        DeleteWorkspace("ws_shape")
+        if "ws_shape" in ADS:
+            DeleteWorkspace("ws_shape")
 
     def test_CSG_merged_shape_is_valid(self):
         setup_workspace_shape_from_CSG_merged("ws_shape")
@@ -80,29 +82,89 @@ class PlotSampleShapeTest(TestCase):
         self.assertTrue(ws_shape.sample().getShape())
         self.assertTrue(sample_shape.get_valid_sample_shape_from_workspace("ws_shape"))
 
-    def test_plot_created_for_CSG_sphere(self):
+    def test_plot_created_for_CSG_sphere_sample_only(self):
         CreateSampleWorkspace(OutputWorkspace="ws_shape")
         shape_plot = sample_shape.plot_sample_shape("ws_shape")
         self.assertTrue(shape_plot)
 
-    def test_plot_created_for_CSG_merged(self):
+    def test_plot_created_for_CSG_merged_sample_only(self):
         setup_workspace_shape_from_CSG_merged("ws_shape")
         shape_plot = sample_shape.plot_sample_shape("ws_shape")
         self.assertTrue(shape_plot)
 
-    def test_plot_created_for_mesh(self):
+    def test_plot_created_for_mesh_sample_only(self):
         setup_workspace_shape_from_mesh("ws_shape")
         shape_plot_axes = sample_shape.plot_sample_shape("ws_shape")
         self.assertTrue(shape_plot_axes)
 
     def test_plot_created_for_mesh_container_and_components(self):
         setup_workspace_sample_container_and_components_from_mesh("ws_shape")
+        ws_shape = ADS.retrieve("ws_shape")
         shape_plot_figure = sample_shape.plot_sample_shape("ws_shape")
         self.assertTrue(shape_plot_figure)
-        components_added_to_plot = sample_shape.plot_container_and_components("ws_shape", shape_plot_figure)
+        components_added_to_plot = sample_shape.plot_container_and_components(ws_shape, shape_plot_figure)
         self.assertTrue(components_added_to_plot)
 
-    # def test_do_not_add_components_if_no_environment(self):
+    @patch("mantidqt.plotting.sample_shape.plot_container_and_components")
+    def test_add_components_if_environment(self, mock_plot_container_and_components):
+        setup_workspace_sample_container_and_components_from_mesh("ws_shape")
+        sample_shape.plot_sample_shape("ws_shape")
+        self.assertEqual(1, mock_plot_container_and_components.call_count)
+
+    @patch("mantidqt.plotting.sample_shape.plot_container_and_components")
+    def test_do_not_add_components_if_no_environment(self, mock_plot_container_and_components):
+        setup_workspace_shape_from_mesh("ws_shape")
+        sample_shape.plot_sample_shape("ws_shape")
+        self.assertEqual(0, mock_plot_container_and_components.call_count)
+
+    @patch("mantidqt.plotting.sample_shape.call_set_mesh_axes_equal")
+    def test_call_axes_equal_once_for_sample_only(self, mock_call_set_mesh_axes_equal):
+        setup_workspace_shape_from_mesh("ws_shape")
+        sample_shape.plot_sample_shape("ws_shape")
+        self.assertEqual(1, mock_call_set_mesh_axes_equal.call_count)
+
+    @patch("mantidqt.plotting.sample_shape.call_set_mesh_axes_equal")
+    def test_call_axes_equal_once_for_sample_and_components(self, mock_call_set_mesh_axes_equal):
+        setup_workspace_sample_container_and_components_from_mesh("ws_shape")
+        ws_shape = ADS.retrieve("ws_shape")
+        shape_plot_figure = sample_shape.plot_sample_shape("ws_shape")
+        sample_shape.plot_container_and_components(ws_shape, shape_plot_figure)
+        self.assertEqual(1, mock_call_set_mesh_axes_equal.call_count)
+
+    def test_get_overall_limits_for_sample_only(self):
+        setup_workspace_shape_from_mesh("ws_shape")
+        ws_shape = ADS.retrieve("ws_shape")
+        [minimum, maximum] = sample_shape.overall_limits_for_all_meshes(ws_shape)
+        self.assertEqual([-0.15, 0.1], [minimum, maximum])
+
+    def test_get_overall_limits_for_sample_only_PEARL(self):
+        setup_workspace_sample_container_and_components_from_mesh("ws_shape")
+        ws_shape = ADS.retrieve("ws_shape")
+        [minimum, maximum] = sample_shape.overall_limits_for_all_meshes(ws_shape, include_components=False)
+        self.assertEqual([-3.289720742031932e-06, 0.0017999805212020874], [minimum, maximum])
+
+    def test_get_overall_limits_for_sample_and_components_PEARL(self):
+        setup_workspace_sample_container_and_components_from_mesh("ws_shape")
+        ws_shape = ADS.retrieve("ws_shape")
+        [minimum, maximum] = sample_shape.overall_limits_for_all_meshes(ws_shape)
+        self.assertEqual([-0.035900001525878904, 0.035900001525878904], [minimum, maximum])
+
+    def test_set_axes_to_largest_mesh(self):
+        setup_workspace_shape_from_mesh("ws_shape")
+        ws_shape = ADS.retrieve("ws_shape")
+        [minimum, maximum] = sample_shape.overall_limits_for_all_meshes(ws_shape)
+        self.assertEqual([-0.15, 0.1], [minimum, maximum])
+
+    def test_overall_limits_for_every_axis(self):
+        self.assertEqual((3, 9), sample_shape.overall_limits_for_every_axis([np.array([3, 5, 7]),
+                                                                             np.array([4, 6, 8]),
+                                                                             np.array([5, 7, 9])]))
+
+    def test_greater_limits(self):
+        self.assertEqual((1, 7), sample_shape.greater_limits(1, 4, 5, 7))
+        self.assertEqual((1, 7), sample_shape.greater_limits(4, 5, 1, 7))
+        self.assertEqual((1, 7), sample_shape.greater_limits(1, 7, 4, 5))
+        self.assertEqual((1, 7), sample_shape.greater_limits(4, 7, 1, 5))
 
 
 if __name__ == '__main__':
