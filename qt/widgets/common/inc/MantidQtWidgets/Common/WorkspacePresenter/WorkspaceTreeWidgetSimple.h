22b2b7083765f9fe689ad57707c1b2e69f6f9d8a
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2022-10-27 16:50:16 +0100

    Hook up Plotting.SampleShape to ADS right-click menu
    Add to workspacewidget tests

--- qt/widgets/common/inc/MantidQtWidgets/Common/WorkspacePresenter/WorkspaceTreeWidgetSimple.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/WorkspacePresenter/WorkspaceTreeWidgetSimple.h
@@ -56,6 +56,7 @@ signals:
   void plotWireframeClicked(const QStringList &workspaceNames);
   void plotContourClicked(const QStringList &workspaceNames);
   void sampleMaterialClicked(const QStringList &workspaceNames);
+  void sampleShapeClicked(const QStringList &workspaceNames);
   void superplotClicked(const QStringList &workspaceNames);
   void superplotWithErrsClicked(const QStringList &workspaceNames);
   void superplotBinsClicked(const QStringList &workspaceNames);
@@ -93,6 +94,7 @@ private slots:
   void onPlotMDHistoWorkspaceWithErrorsClicked();
   void onOverPlotMDHistoWorkspaceWithErrorsClicked();
   void onSampleMaterialClicked();
+  void onSampleShapeClicked();
   void onSuperplotClicked();
   void onSuperplotWithErrsClicked();
   void onSuperplotBinsClicked();
@@ -112,7 +114,7 @@ private:
   QAction *m_plotSpectrum, *m_plotBin, *m_overplotSpectrum, *m_plotSpectrumWithErrs, *m_overplotSpectrumWithErrs,
       *m_plotColorfill, *m_sampleLogs, *m_sliceViewer, *m_showInstrument, *m_showData, *m_showAlgorithmHistory,
       *m_showDetectors, *m_plotAdvanced, *m_plotSurface, *m_plotWireframe, *m_plotContour, *m_plotMDHisto1D,
-      *m_overplotMDHisto1D, *m_plotMDHisto1DWithErrs, *m_overplotMDHisto1DWithErrs, *m_sampleMaterial, *m_superplot,
-      *m_superplotWithErrs, *m_superplotBins, *m_superplotBinsWithErrs;
+      *m_overplotMDHisto1D, *m_plotMDHisto1DWithErrs, *m_overplotMDHisto1DWithErrs, *m_sampleMaterial, *m_sampleShape,
+      *m_superplot, *m_superplotWithErrs, *m_superplotBins, *m_superplotBinsWithErrs;
 };
 } // namespace MantidQt::MantidWidgets
