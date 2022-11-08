3e5db5cd1d47084c509788677b095dcbaf8c473a
Applin <robert.applin@stfc.ac.uk>
2022-11-08 16:42:11 +0000

    Move plot fit analysis tests and rename

---
+++ qt/scientific_interfaces/Direct/test/ALFAnalysisModelTest.h
@@ -0,0 +1,92 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include <cxxtest/TestSuite.h>
+
+#include "ALFAnalysisModel.h"
+
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidFrameworkTestHelpers/WorkspaceCreationHelper.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+using namespace Mantid::API;
+using namespace MantidQt::MantidWidgets;
+
+class ALFAnalysisModelTest : public CxxTest::TestSuite {
+public:
+  ALFAnalysisModelTest() { FrameworkManager::Instance(); }
+
+  static ALFAnalysisModelTest *createSuite() { return new ALFAnalysisModelTest(); }
+
+  static void destroySuite(ALFAnalysisModelTest *suite) { delete suite; }
+
+  // void setUp() override {
+  //  m_model = std::make_unique<PlotFitAnalysisPaneModel>();
+
+  //  m_workspace = WorkspaceCreationHelper::create2DWorkspace(1, 100);
+  //  m_workspaceName = "test";
+  //  m_range = std::make_pair<double, double>(0.0, 100.0);
+
+  //  AnalysisDataService::Instance().addOrReplace(m_workspaceName, m_workspace);
+  //}
+
+  // void tearDown() override {
+  //  AnalysisDataService::Instance().clear();
+  //  m_model.reset();
+  //}
+
+  // void test_that_the_model_is_instantiated_with_a_function_and_empty_fit_status() {
+  //  TS_ASSERT_THROWS_NOTHING(m_model->peakCentre());
+  //  TS_ASSERT_EQUALS("", m_model->fitStatus());
+  //}
+
+  // void test_that_doFit_sets_a_successful_fit_status_for_a_good_fit() {
+  //  m_model->doFit(m_workspaceName, m_range);
+
+  //  TS_ASSERT_EQUALS(0.0, m_model->peakCentre());
+  //  TS_ASSERT_EQUALS("success", m_model->fitStatus());
+  //}
+
+  // void test_that_calculateEstimate_returns_zero_peak_centre_if_the_workspace_does_not_exist_in_the_ADS() {
+  //  AnalysisDataService::Instance().clear();
+
+  //  m_model->calculateEstimate(m_workspaceName, m_range);
+
+  //  TS_ASSERT_EQUALS(0.0, m_model->peakCentre());
+  //  TS_ASSERT_EQUALS("", m_model->fitStatus());
+  //}
+
+  // void test_that_calculateEstimate_returns_an_estimate_if_the_workspace_does_exist_in_the_ADS() {
+  //  m_model->calculateEstimate(m_workspaceName, m_range);
+
+  //  TS_ASSERT_EQUALS(0.5, m_model->peakCentre());
+  //  TS_ASSERT_EQUALS("", m_model->fitStatus());
+  //}
+
+  // void test_that_calculateEstimate_returns_zero_peak_centre_if_the_crop_range_is_invalid() {
+  //  m_workspace = WorkspaceCreationHelper::create2DWorkspaceBinned(1, 100, 300.0);
+  //  AnalysisDataService::Instance().addOrReplace(m_workspaceName, m_workspace);
+
+  //  m_model->calculateEstimate(m_workspaceName, m_range);
+
+  //  TS_ASSERT_EQUALS(0.0, m_model->peakCentre());
+  //  TS_ASSERT_EQUALS("", m_model->fitStatus());
+  //}
+
+private:
+  std::unique_ptr<ALFAnalysisModel> m_model;
+
+  MatrixWorkspace_sptr m_workspace;
+  std::string m_workspaceName;
+  std::pair<double, double> m_range;
+};
---
+++ qt/scientific_interfaces/Direct/test/ALFAnalysisPresenterTest.h
@@ -0,0 +1,140 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2019 ISIS Rutherford Appleton Laboratory UKRI,
+//     NScD Oak Ridge National Laboratory, European Spallation Source
+//     & Institut Laue - Langevin
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include <cxxtest/TestSuite.h>
+#include <gmock/gmock.h>
+
+#include "ALFAnalysisMocks.h"
+#include "ALFAnalysisPresenter.h"
+
+#include "MantidAPI/AlgorithmManager.h"
+#include "MantidAPI/AnalysisDataService.h"
+#include "MantidAPI/FrameworkManager.h"
+#include "MantidAPI/FunctionFactory.h"
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidFrameworkTestHelpers/WorkspaceCreationHelper.h"
+#include "MantidGeometry/Instrument.h"
+
+#include <string>
+#include <utility>
+
+using namespace Mantid::API;
+using namespace testing;
+using Mantid::Geometry::Instrument;
+using namespace MantidQt::MantidWidgets;
+
+class ALFAnalysisPresenterTest : public CxxTest::TestSuite {
+public:
+  ALFAnalysisPresenterTest() { FrameworkManager::Instance(); }
+
+  static ALFAnalysisPresenterTest *createSuite() { return new ALFAnalysisPresenterTest(); }
+
+  static void destroySuite(ALFAnalysisPresenterTest *suite) { delete suite; }
+
+  // void setUp() override {
+  //  m_view = new NiceMock<MockALFAnalysisView>();
+  //  m_model = new MockALFAnalysisModel();
+  //  m_presenter = new ALFAnalysisPresenter(m_view, m_model);
+  //  m_workspaceName = "test";
+  //  m_range = std::make_pair(0.0, 1.0);
+  //  m_peakCentre = 0.5;
+  //  m_view->setPeakCentre(m_peakCentre);
+  //  m_model->setPeakCentre(m_peakCentre);
+  //}
+
+  // void tearDown() override {
+  //  AnalysisDataService::Instance().clear();
+  //  delete m_view;
+  //  delete m_presenter;
+  //  m_model = nullptr;
+  //}
+
+  // void test_peakCentreEditingFinished_sets_the_peak_centre_in_the_model_and_fit_status_in_the_view() {
+  //  EXPECT_CALL(*m_view, peakCentre()).Times(1).WillOnce(Return(m_peakCentre));
+  //  EXPECT_CALL(*m_model, setPeakCentre(m_peakCentre)).Times(1);
+
+  //  EXPECT_CALL(*m_model, fitStatus()).Times(1).WillOnce(Return(""));
+  //  EXPECT_CALL(*m_view, setPeakCentreStatus("")).Times(1);
+
+  //  m_presenter->peakCentreEditingFinished();
+  //}
+
+  // void test_fitClicked_will_display_a_warning_when_the_workspace_name_is_not_set() {
+  //  EXPECT_CALL(*m_view, displayWarning("Need to have extracted data to do a fit or estimate.")).Times(1);
+  //  m_presenter->fitClicked();
+  //}
+
+  // void test_fitClicked_will_display_a_warning_when_the_peak_centre_is_outside_the_fit_range() {
+  //  // set name via addSpectrum
+  //  EXPECT_CALL(*m_view, addSpectrum(m_workspaceName)).Times(1);
+  //  m_presenter->addSpectrum(m_workspaceName);
+
+  //  EXPECT_CALL(*m_view, peakCentre()).Times(1).WillOnce(Return(-1.0));
+  //  EXPECT_CALL(*m_view, getRange()).Times(1).WillOnce(Return(m_range));
+  //  EXPECT_CALL(*m_view, displayWarning("The Peak Centre provided is outside the fit range.")).Times(1);
+
+  //  m_presenter->fitClicked();
+  //}
+
+  // void test_fitClicked_will_perform_a_fit_when_the_workspace_name_and_peak_centre_is_valid() {
+  //  // set name via addSpectrum
+  //  EXPECT_CALL(*m_view, addSpectrum(m_workspaceName)).Times(1);
+  //  m_presenter->addSpectrum(m_workspaceName);
+
+  //  EXPECT_CALL(*m_view, peakCentre()).Times(1).WillOnce(Return(m_peakCentre));
+  //  EXPECT_CALL(*m_view, getRange()).Times(2).WillRepeatedly(Return(m_range));
+
+  //  EXPECT_CALL(*m_model, doFit(m_workspaceName, m_range)).Times(1);
+
+  //  m_presenter->fitClicked();
+  //}
+
+  // void test_addSpectrum_will_call_addSpectrum_in_the_view() {
+  //  EXPECT_CALL(*m_view, addSpectrum(m_workspaceName)).Times(1);
+  //  m_presenter->addSpectrum(m_workspaceName);
+  //}
+
+  // void test_that_calculateEstimate_is_not_called_when_the_current_workspace_name_is_blank() {
+  //  EXPECT_CALL(*m_view, displayWarning("Need to have extracted data to do a fit or estimate.")).Times(1);
+
+  //  m_presenter->updateEstimateClicked();
+  //}
+
+  // void test_that_calculateEstimate_is_not_called_when_the_peak_centre_is_invalid() {
+  //  // set name via addSpectrum
+  //  EXPECT_CALL(*m_view, addSpectrum(m_workspaceName)).Times(1);
+  //  m_presenter->addSpectrum(m_workspaceName);
+
+  //  EXPECT_CALL(*m_view, peakCentre()).Times(1).WillOnce(Return(-1.0));
+  //  EXPECT_CALL(*m_view, getRange()).Times(1).WillOnce(Return(m_range));
+  //  EXPECT_CALL(*m_view, displayWarning("The Peak Centre provided is outside the fit range.")).Times(1);
+
+  //  m_presenter->updateEstimateClicked();
+  //}
+
+  // void test_that_calculateEstimate_is_called_as_expected() {
+  //  EXPECT_CALL(*m_view, addSpectrum(m_workspaceName)).Times(1);
+  //  m_presenter->addSpectrum(m_workspaceName);
+
+  //  EXPECT_CALL(*m_view, peakCentre()).Times(1).WillOnce(Return(m_peakCentre));
+  //  EXPECT_CALL(*m_view, getRange()).Times(2).WillRepeatedly(Return(m_range));
+
+  //  EXPECT_CALL(*m_model, calculateEstimate(m_workspaceName, m_range)).Times(1);
+
+  //  m_presenter->updateEstimateClicked();
+  //}
+
+private:
+  NiceMock<MockALFAnalysisView> *m_view;
+  MockALFAnalysisModel *m_model;
+  ALFAnalysisPresenter *m_presenter;
+
+  std::string m_workspaceName;
+  std::pair<double, double> m_range;
+  double m_peakCentre;
+};
