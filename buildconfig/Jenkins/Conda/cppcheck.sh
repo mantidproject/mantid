601963cdbae80c96cadd72bcc1465efaf64bcbc0
Applin <robert.applin@stfc.ac.uk>
2022-11-09 12:46:06 +0000

    Remove unrequired if statement in cppcheck shell script

--- buildconfig/Jenkins/Conda/cppcheck.sh
+++ buildconfig/Jenkins/Conda/cppcheck.sh
@@ -6,17 +6,6 @@ SCRIPT_DIR=$(dirname "$0")
 # builds for all developers
 ALLOWED_ERRORS_COUNT=0
 
-if [[ ${JOB_NAME} == *pull_requests* ]]; then
-    # This relies on the fact pull requests use pull/$PR-NAME
-    # which squashes the branch into a single merge commit
-    cd $WORKSPACE
-
-    if ${SCRIPT_DIR}/check_for_changes cpp; then
-        echo "No C++ files have changed. Skipping check."
-        exit 0
-    fi
-fi
-
 ###############################################################################
 # Create the build directory if it doesn't exist
 ###############################################################################
