f7f7ca61dc2dd23cc45ce29a700371bad7a61095
Dominik Arominski <arominski@ill.fr>
2022-11-23 15:54:00 +0100

    [DirectILL] Removed redundant calculateError function

--- Framework/DataHandling/inc/MantidDataHandling/LoadILLTOF2.h
+++ Framework/DataHandling/inc/MantidDataHandling/LoadILLTOF2.h
@@ -55,9 +55,6 @@ private:
   void loadSpectra(size_t &spec, const size_t numberOfTubes, const std::vector<Mantid::detid_t> &detectorIDs,
                    const NeXus::NXInt &data, Mantid::API::Progress &progress);
 
-  /// Calculate error for y
-  static double calculateError(double in) { return sqrt(in); }
-
   API::MatrixWorkspace_sptr m_localWorkspace;
 
   std::string m_instrumentName; ///< Name of the instrument
