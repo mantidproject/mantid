e2b426bcf89b1b5f8c1e3a37528f68f3de622b1d
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2022-12-02 14:38:00 +0000

    restructure half focus

--- scripts/Diffraction/isis_powder/polaris.py
+++ scripts/Diffraction/isis_powder/polaris.py
@@ -37,7 +37,7 @@ class Polaris(AbstractInst):
                            do_absorb_corrections=self._inst_settings.do_absorb_corrections,
                            sample_details=self._sample_details,
                            empty_can_subtraction_method=self._inst_settings.empty_can_subtraction_method,
-                           paalman_pings_events_per_point=self._inst_settings.paalman_pings_events_per_point)
+                           paalman_pings_events_per_point=self._inst_settings.paalman_pings_events_per_point,)
 
     def create_vanadium(self, test=False, **kwargs):
         self._switch_mode_specific_inst_settings(kwargs.get("mode"))
