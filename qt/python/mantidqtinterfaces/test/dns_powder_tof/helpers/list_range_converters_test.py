3e3f29f3b39d4154f57ca7ed87e93d6102af7700
Oleksandr Koshchii <o.koshchii@fz-juelich.de>
2022-12-07 11:25:44 +0100

    Minor refactor list_range_converters_test.py

--- qt/python/mantidqtinterfaces/test/dns_powder_tof/helpers/list_range_converters_test.py
+++ qt/python/mantidqtinterfaces/test/dns_powder_tof/helpers/list_range_converters_test.py
@@ -32,7 +32,7 @@ class list_range_convertersTest(unittest.TestCase):
             testv, '[1, 11] + [*range(12, 17, 1)]'
             ' + [*range(20, 27, 1)]')
 
-    def test_get_normation(self):
+    def test_get_normalisation(self):
         testv = get_normalisation({'norm_monitor': True})
         self.assertEqual(testv, 'monitor')
         testv = get_normalisation({'norm_monitor': False})
