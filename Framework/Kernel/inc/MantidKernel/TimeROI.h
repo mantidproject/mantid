344929a9800a3c31c646aeb91e363b3bcbcad181
Pete Peterson <petersonpf@ornl.gov>
2022-12-19 10:37:21 -0500

    Generated code from class_maker

---
+++ Framework/Kernel/inc/MantidKernel/TimeROI.h
@@ -0,0 +1,21 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2022 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidKernel/DllConfig.h"
+
+namespace Mantid {
+namespace Kernel {
+
+/** TimeROI : TODO: DESCRIPTION
+ */
+class MANTID_KERNEL_DLL TimeROI {
+public:
+};
+
+} // namespace Kernel
+} // namespace Mantid
