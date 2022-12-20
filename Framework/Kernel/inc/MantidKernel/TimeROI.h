13361bfa1358fd10ec1b11bb0ab8537dc597c862
Pete Peterson <petersonpf@ornl.gov>
2022-12-20 10:18:31 -0500

    Add methods for adding ROI and calculating duration

--- Framework/Kernel/inc/MantidKernel/TimeROI.h
+++ Framework/Kernel/inc/MantidKernel/TimeROI.h
@@ -7,14 +7,32 @@
 #pragma once
 
 #include "MantidKernel/DllConfig.h"
+#include "MantidKernel/TimeSeriesProperty.h"
 
 namespace Mantid {
 namespace Kernel {
+
+namespace {
+// const std::string NAME{"roi"};
+}
 
 /** TimeROI : TODO: DESCRIPTION
  */
 class MANTID_KERNEL_DLL TimeROI {
 public:
+  TimeROI();
+  double durationInSeconds() const;
+  void addROI(const std::string &startTime, const std::string &stopTime);
+  void addROI(const Types::Core::DateAndTime &startTime, const Types::Core::DateAndTime &stopTime);
+  void addROI(const std::time_t &startTime, const std::time_t &stopTime);
+
+private:
+  /**
+   * @brief m_roi private member that holds most of the information
+   *
+   * This handles the details of the ROI and guarantees that views into the underlying structure is sorted by time.
+   */
+  TimeSeriesProperty<bool> m_roi;
 };
 
 } // namespace Kernel
--- Framework/Kernel/test/TimeROITest.h
+++ Framework/Kernel/test/TimeROITest.h
@@ -11,6 +11,16 @@
 #include "MantidKernel/TimeROI.h"
 
 using Mantid::Kernel::TimeROI;
+using Mantid::Types::Core::DateAndTime;
+
+constexpr double ONE_DAY_DURATION{24 * 3600};
+
+const std::string HANUKKAH_START("2022-12-19T00:01");
+const std::string HANUKKAH_STOP("2022-12-26T00:01");
+constexpr double HANUKKAH_DURATION{7. * ONE_DAY_DURATION};
+
+const std::string NEW_YEARS_START("2022-12-31T00:01");
+const std::string NEW_YEARS_STOP("2023-01-01T00:01");
 
 class TimeROITest : public CxxTest::TestSuite {
 public:
@@ -19,5 +29,29 @@ public:
   static TimeROITest *createSuite() { return new TimeROITest(); }
   static void destroySuite(TimeROITest *suite) { delete suite; }
 
-  void test_Something() { TS_FAIL("You forgot to write a test!"); }
+  void test_emptyROI() {
+    TimeROI value;
+    TS_ASSERT_EQUALS(value.durationInSeconds(), 0.);
+  }
+  void test_sortedROI() {
+    TimeROI value;
+    // add Hanukkah
+    value.addROI(HANUKKAH_START, HANUKKAH_STOP);
+    TS_ASSERT_EQUALS(value.durationInSeconds(), HANUKKAH_DURATION);
+
+    // add New Year's eve
+    value.addROI(NEW_YEARS_START, NEW_YEARS_STOP);
+    TS_ASSERT_EQUALS(value.durationInSeconds(), HANUKKAH_DURATION + ONE_DAY_DURATION);
+  }
+
+  void test_reversesortedROI() {
+    TimeROI value;
+    // add New Year's eve
+    value.addROI(DateAndTime(NEW_YEARS_START), DateAndTime(NEW_YEARS_STOP));
+    TS_ASSERT_EQUALS(value.durationInSeconds(), ONE_DAY_DURATION);
+
+    // add Hanukkah
+    value.addROI(HANUKKAH_START, HANUKKAH_STOP);
+    TS_ASSERT_EQUALS(value.durationInSeconds(), ONE_DAY_DURATION + HANUKKAH_DURATION);
+  }
 };
