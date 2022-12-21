25f9d92d2f94db2ee29731ed7a4cd45c26afd268
Pete Peterson <petersonpf@ornl.gov>
2022-12-21 13:17:25 -0500

    Undo formatting of generated scripts

--- scripts/templates/reference/generate_sequential_fit_script_reference_file.py
+++ scripts/templates/reference/generate_sequential_fit_script_reference_file.py
@@ -3,10 +3,14 @@ from mantid.simpleapi import *
 import matplotlib.pyplot as plt
 
 # Dictionary { workspace_name: (workspace_index, start_x, end_x) }
-input_data = {"Name1": (0, 0.500000, 1.500000), "Name2": (1, 0.600000, 1.600000)}
+input_data = {
+    "Name1": (0, 0.500000, 1.500000),
+    "Name2": (1, 0.600000, 1.600000)
+}
 
 # Fit function as a string
-function = "name=GausOsc,A=0.2,Sigma=0.2,Frequency=0.1,Phi=0"
+function = \
+    "name=GausOsc,A=0.2,Sigma=0.2,Frequency=0.1,Phi=0"
 
 # Fitting options
 max_iterations = 500
@@ -18,18 +22,10 @@ output_base_name = "Output_Fit"
 # Perform a sequential fit
 output_workspaces, parameter_tables, normalised_matrices = [], [], []
 for input_workspace, domain_data in input_data.items():
-    fit_output = Fit(
-        Function=function,
-        InputWorkspace=input_workspace,
-        WorkspaceIndex=domain_data[0],
-        StartX=domain_data[1],
-        EndX=domain_data[2],
-        MaxIterations=max_iterations,
-        Minimizer=minimizer,
-        CostFunction=cost_function,
-        EvaluationType=evaluation_type,
-        Output=output_base_name + input_workspace,
-    )
+    fit_output = Fit(Function=function, InputWorkspace=input_workspace, WorkspaceIndex=domain_data[0],
+                     StartX=domain_data[1], EndX=domain_data[2], MaxIterations=max_iterations,
+                     Minimizer=minimizer, CostFunction=cost_function, EvaluationType=evaluation_type,
+                     Output=output_base_name + input_workspace)
 
     output_workspaces.append(output_base_name + input_workspace + "_Workspace")
     parameter_tables.append(output_base_name + input_workspace + "_Parameters")
@@ -44,9 +40,11 @@ GroupWorkspaces(InputWorkspaces=parameter_tables, OutputWorkspace=output_base_na
 GroupWorkspaces(InputWorkspaces=normalised_matrices, OutputWorkspace=output_base_name + "NormalisedCovarianceMatrices")
 
 # Plot the results of the fit
-fig, axes = plt.subplots(
-    nrows=2, ncols=len(output_workspaces), sharex=True, gridspec_kw={"height_ratios": [2, 1]}, subplot_kw={"projection": "mantid"}
-)
+fig, axes = plt.subplots(nrows=2,
+                         ncols=len(output_workspaces),
+                         sharex=True,
+                         gridspec_kw={"height_ratios": [2, 1]},
+                         subplot_kw={"projection": "mantid"})
 
 for i, workspace_name in enumerate(output_workspaces):
     workspace = AnalysisDataService.retrieve(workspace_name)
