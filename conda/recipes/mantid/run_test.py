3e9542aa617f8c9f02a20b1208bc872f55dc5dd2
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2023-01-04 13:45:03 +0000

    Sync with latest version of mantidproject/conda-recipes

--- conda/recipes/mantid/run_test.py
+++ conda/recipes/mantid/run_test.py
@@ -1,9 +1,10 @@
 # Turn off network required things that the framework does
 from mantid.kernel import ConfigService
+
 ConfigService["UpdateInstrumentDefinitions.OnStartup"] = "0"
 ConfigService["usagereports.enabled"] = "0"
 ConfigService["CheckMantidVersion.OnStartup"] = "0"
 
-import mantid.simpleapi as sapi
+import mantid.simpleapi
 
-assert "Load" in dir(sapi)
+assert "Rebin" in dir(mantid.simpleapi), "Rebin not found in simpleapi!"
