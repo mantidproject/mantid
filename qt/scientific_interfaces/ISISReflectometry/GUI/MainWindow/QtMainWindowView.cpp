be58c72fc22fec594a5cf6521fa3bd9932b29042
Rachel Baust <rachel.baust@stfc.ac.uk>
2023-01-11 16:07:28 +0000

    Deallocate memory when batch tab is deleted

--- qt/scientific_interfaces/ISISReflectometry/GUI/MainWindow/QtMainWindowView.cpp
+++ qt/scientific_interfaces/ISISReflectometry/GUI/MainWindow/QtMainWindowView.cpp
@@ -49,7 +49,10 @@ IBatchView *QtMainWindowView::newBatch() {
 
 void QtMainWindowView::removeBatch(int batchIndex) {
   m_batchViews.erase(m_batchViews.begin() + batchIndex);
+  auto *batchToRemove = m_ui.mainTabs->widget(batchIndex);
   m_ui.mainTabs->removeTab(batchIndex);
+  // Calling removeTab doesn't deallocate the memory for the QtBatchView, so we must do that here
+  delete batchToRemove;
   if (m_ui.mainTabs->count() == 0) {
     m_notifyee->notifyNewBatchRequested();
   }
