0a4ccb5aaf57b85353d2f7c5bc560edd7fb2e939
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2023-01-25 11:40:59 +0000

    Create Separate function and add tests
    If there is an oldTie for the parameter, which was a tie to function
    (e.g. f0.Height=f1.Height) then the oldTieExpression is returned.
    This allows an edited Tie to function text box to revert upon an invalid entry see pr #34817
    Note this function does not get an oldTie if it was custom (e.g. f0.Height=2.0)
    This new function also allows fixing a parameter, when oldTie is nullptr.

--- qt/widgets/common/test/FitPropertyBrowserTest.h
+++ qt/widgets/common/test/FitPropertyBrowserTest.h
@@ -10,6 +10,7 @@
 #include "MantidAPI/IFunction1D.h"
 #include "MantidAPI/ParamFunction.h"
 #include "MantidQtWidgets/Common/FitPropertyBrowser.h"
+#include <QString>
 #include <cxxtest/TestSuite.h>
 
 using namespace Mantid::API;
@@ -39,8 +40,16 @@ DECLARE_FUNCTION(FitPropertyBrowserTest_Funct)
 
 class FitPropertyBrowserTest : public CxxTest::TestSuite {
 public:
-  // This is a very specific test for a bug that is now fixed to prevent
-  // regression
+  static FitPropertyBrowserTest *createSuite() { return new FitPropertyBrowserTest; }
+  static void destroySuite(FitPropertyBrowserTest *suite) { delete suite; }
+
+  void setUp() override { // create a FunctionBrowser
+    m_fitPropertyBrowser = std::make_unique<MantidQt::MantidWidgets::FitPropertyBrowser>();
+  }
+
+  void tearDown() override { m_fitPropertyBrowser.reset(); }
+
+  // This is a very specific test for a bug that is now fixed to prevent regression
   void test_FunctionFactory_notification_is_released() {
 
     // create a FunctionBrowser
@@ -53,4 +62,39 @@ public:
     // Make sure the FunctionFactory does not have a dead link as an observer
     TS_ASSERT_THROWS_NOTHING(FunctionFactory::Instance().unsubscribe("FitPropertyBrowserTest_Funct");)
   }
+
+  void test_getOldExpressionAsString_returns_empty_string_when_tie_is_null() {
+    // initialise it fitPropertyBrowser- this adds an observer on the function factory update message
+    m_fitPropertyBrowser->init();
+    m_fitPropertyBrowser->createCompositeFunction("name=Gaussian,Height=100,PeakCentre=1.45,Sigma=0.2;");
+    QString parameterName = "f0.Height";
+    auto oldExpString = m_fitPropertyBrowser->getOldExpressionAsString(parameterName);
+    TS_ASSERT_EQUALS(oldExpString.toStdString(), "");
+  }
+
+  void test_getOldExpressionAsString_returns_empty_string_when_parameter_is_null() {
+    // initialise it fitPropertyBrowser- this adds an observer on the function factory update message
+    m_fitPropertyBrowser->init();
+    m_fitPropertyBrowser->createCompositeFunction("name=Gaussian,Height=100,PeakCentre=1.45,Sigma=0.2;");
+    QString parameterName = "InvalidParameterName";
+    auto oldExpString = m_fitPropertyBrowser->getOldExpressionAsString(parameterName);
+    TS_ASSERT_EQUALS(oldExpString.toStdString(), "");
+  }
+
+  void test_getOldExpressionAsString_returns_function_expression() {
+    // Note this test only works for a tie to a function (e.g. f0.Height=f1.Height) but not a constant (e.g.
+    // f0.Height=5.0)
+
+    m_fitPropertyBrowser->init();
+    m_fitPropertyBrowser->createCompositeFunction(
+        "name=Gaussian,Height=10.0,PeakCentre=-0.145,Sigma=0.135;name=Gaussian,Height=12.0,PeakCentre=0.245,Sigma=0."
+        "135;ties=(f0.Height=f1.Height)");
+    QString parameterName = "f0.Height";
+    const std::string expectedTieExpression = "f1.Height";
+    auto oldExpString = m_fitPropertyBrowser->getOldExpressionAsString(parameterName);
+    TS_ASSERT_EQUALS(oldExpString.toStdString(), expectedTieExpression);
+  }
+
+private:
+  std::unique_ptr<MantidQt::MantidWidgets::FitPropertyBrowser> m_fitPropertyBrowser;
 };
