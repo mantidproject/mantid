ce20a853a1dfee4cdcfc0ea0f5cc49175263b1f3
Applin <robert.applin@stfc.ac.uk>
2023-02-06 10:17:14 +0000

    Divide two theta by two

--- qt/scientific_interfaces/Direct/ALFAnalysisModel.cpp
+++ qt/scientific_interfaces/Direct/ALFAnalysisModel.cpp
@@ -210,7 +210,7 @@ std::optional<double> ALFAnalysisModel::rotationAngle() const {
   if (!twoTheta) {
     return std::nullopt;
   }
-  return peakCentre() / (2 * sin((*twoTheta) * M_PI / 180.0));
+  return peakCentre() / (2 * sin((*twoTheta / 2.0) * M_PI / 180.0));
 }
 
 } // namespace MantidQt::CustomInterfaces
