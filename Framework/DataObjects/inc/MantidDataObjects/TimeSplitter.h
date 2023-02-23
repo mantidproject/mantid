fbe46c7ac5547cd1a517383c54923a2cd7ed1833
Robert Bolotovsky <bolotovskyr@ornl.gov>
2023-02-23 01:49:08 -0500

    Build TimeSplitter from TableWorkspace.

--- Framework/DataObjects/inc/MantidDataObjects/TimeSplitter.h
+++ Framework/DataObjects/inc/MantidDataObjects/TimeSplitter.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "MantidDataObjects/DllConfig.h"
+#include "MantidDataObjects/TableWorkspace.h"
 #include "MantidKernel/DateAndTime.h"
 
 namespace Mantid {
@@ -25,6 +26,9 @@ class MANTID_DATAOBJECTS_DLL TimeSplitter {
 public:
   TimeSplitter() = default;
   TimeSplitter(const DateAndTime &start, const DateAndTime &stop);
+  TimeSplitter(const TableWorkspace_sptr &tws, const DateAndTime &offset = DateAndTime(0, 0));
+
+public:
   int valueAtTime(const DateAndTime &time) const;
   void addROI(const DateAndTime &start, const DateAndTime &stop, const int value);
   std::vector<int> outputWorkspaceIndices() const;
--- Framework/DataObjects/test/TimeSplitterTest.h
+++ Framework/DataObjects/test/TimeSplitterTest.h
@@ -8,9 +8,12 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidAPI/TableRow.h"
+#include "MantidDataObjects/TableWorkspace.h"
 #include "MantidDataObjects/TimeSplitter.h"
 #include "MantidKernel/TimeROI.h"
 
+using Mantid::API::TableRow;
 using Mantid::DataObjects::TimeSplitter;
 using Mantid::Kernel::TimeROI;
 using Mantid::Types::Core::DateAndTime;
@@ -179,5 +182,71 @@ public:
     roi = splitter.getTimeROI(1);
     TS_ASSERT(!roi.empty());
     TS_ASSERT_EQUALS(roi.numBoundaries(), 4);
+  }
+
+  /** Test that a TimeSplitter object constructed from a TableWorkspace object
+   * is equivalent to a TimeSplitter object built by successively adding the corresponding time ROIs.
+   * @brief test_createFromTableWorkspace
+   */
+  void test_createFromTableWorkspace() {
+    auto tws = std::make_shared<Mantid::DataObjects::TableWorkspace>(3 /*rows*/);
+
+    // by design, a table workspace used for event filtering must have 3 columns
+    tws->addColumn("double", "start");
+    tws->addColumn("double", "stop");
+    tws->addColumn("str", "target"); // index of a target workspace where the filtered time events will be output
+
+    // by design, all times in the table must be in seconds
+    const double time1_s{1.0e-5};
+    const double time2_s{1.5e-5};
+    const double time3_s{2.0e-5};
+    const double time4_s{3.0e-5};
+    const double time5_s{4.0e-5};
+    const double time6_s{5.0e-5};
+
+    TableRow row = tws->getFirstRow();
+    row << time1_s << time2_s << "1";
+    row.next();
+    row << time3_s << time4_s << "3";
+    row.next();
+    row << time5_s << time6_s << "2";
+
+    // create a TimeSplitter object from the table. By design, the table creator must know whether
+    // the table holds absolute or relative times. In the latter case the creator must have
+    // a time offset to be used with the table.
+    DateAndTime offset(THREE);
+    TimeSplitter workspaceDerivedSplitter(tws, offset);
+
+    // build a reference time splitter
+
+    // create all time objects and offset them
+    DateAndTime time1_abs(time1_s, 0.0);
+    DateAndTime time2_abs(time2_s, 0.0);
+    DateAndTime time3_abs(time3_s, 0.0);
+    DateAndTime time4_abs(time4_s, 0.0);
+    DateAndTime time5_abs(time5_s, 0.0);
+    DateAndTime time6_abs(time6_s, 0.0);
+
+    int64_t offset_ns = offset.totalNanoseconds();
+    time1_abs += offset_ns;
+    time2_abs += offset_ns;
+    time3_abs += offset_ns;
+    time4_abs += offset_ns;
+    time5_abs += offset_ns;
+    time6_abs += offset_ns;
+
+    // build the reference splitter by adding ROIs
+    TimeSplitter referenceSplitter;
+    referenceSplitter.addROI(time1_abs, time2_abs, 1);
+    referenceSplitter.addROI(time3_abs, time4_abs, 3);
+    referenceSplitter.addROI(time5_abs, time6_abs, 2);
+
+    TS_ASSERT_EQUALS(referenceSplitter.numRawValues(), workspaceDerivedSplitter.numRawValues());
+    TS_ASSERT(referenceSplitter.valueAtTime(time1_abs) == workspaceDerivedSplitter.valueAtTime(time1_abs));
+    TS_ASSERT(referenceSplitter.valueAtTime(time2_abs) == workspaceDerivedSplitter.valueAtTime(time2_abs));
+    TS_ASSERT(referenceSplitter.valueAtTime(time3_abs) == workspaceDerivedSplitter.valueAtTime(time3_abs));
+    TS_ASSERT(referenceSplitter.valueAtTime(time4_abs) == workspaceDerivedSplitter.valueAtTime(time4_abs));
+    TS_ASSERT(referenceSplitter.valueAtTime(time5_abs) == workspaceDerivedSplitter.valueAtTime(time5_abs));
+    TS_ASSERT(referenceSplitter.valueAtTime(time6_abs) == workspaceDerivedSplitter.valueAtTime(time6_abs));
   }
 };
