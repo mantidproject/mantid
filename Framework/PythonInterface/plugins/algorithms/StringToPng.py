6ce9d727bc1fd25ab99707b5dadfe8ca41353938
sf1919 <sarah.foxley@stfc.ac.uk>
2023-02-24 16:53:25 +0000

    Remove unnecessary matplotlib versioning from PythonInterface files

--- Framework/PythonInterface/plugins/algorithms/StringToPng.py
+++ Framework/PythonInterface/plugins/algorithms/StringToPng.py
@@ -40,10 +40,7 @@ class StringToPng(mantid.api.PythonAlgorithm):
             import matplotlib
         except ImportError:
             ok2run = "Problem importing matplotlib"
-            from distutils.version import LooseVersion
 
-            if LooseVersion(matplotlib.__version__) < LooseVersion("1.2.0"):
-                ok2run = "Wrong version of matplotlib. Required >= 1.2.0"
         if ok2run != "":
             raise RuntimeError(ok2run)
         matplotlib.use("agg")
