e38bcf5ca4224402f67eb963c9f4b47432323667
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2023-02-24 12:31:47 +0000

    added raises regex check to SelectNexusFilesByMetadataTest

--- Framework/PythonInterface/test/python/plugins/algorithms/SelectNexusFilesByMetadataTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/SelectNexusFilesByMetadataTest.py
@@ -10,12 +10,10 @@ from mantid.simpleapi import *
 
 
 class SelectNexusFilesByMetadataTest(unittest.TestCase):
-
     _fileslist = "INTER00013460,13463,13464.nxs"
     _sumfileslist = "INTER00013460+13463+13464.nxs"
 
     def test_happy_case(self):
-
         criteria = "$raw_data_1/duration$ > 1000 or $raw_data_1/good_frames$ > 10000"
         res = SelectNexusFilesByMetadata(FileList=self._fileslist, NexusCriteria=criteria)
         outfiles = res.split(",")
@@ -32,30 +30,21 @@ class SelectNexusFilesByMetadataTest(unittest.TestCase):
         self.assertTrue(outfiles[1].endswith("INTER00013464.nxs"), "Should be second file name")
 
     def test_invalid_syntax(self):
-
         criteria = "$raw_data_1/duration$ += 1000"
-        throws = False
-        try:
+        with self.assertRaisesRegex(RuntimeError, "Invalid syntax, check NexusCriteria."):
             SelectNexusFilesByMetadata(FileList=self._fileslist, NexusCriteria=criteria)
-        except RuntimeError:
-            throws = True
-        self.assertTrue(throws, "Should raise a runtime error")
-        # asserRaises does not work with python 2.6 so fails on RHEL 6
 
     def test_wrong_nexus_entry(self):
-
         criteria = "$raw_data_1/duration$ > 1000 or $raw_data_1/good_Grames$ > 10000"
         res = SelectNexusFilesByMetadata(FileList=self._fileslist, NexusCriteria=criteria)
         self.assertFalse(res, "Output should be empty since 2nd nexus entry name does not exist")
 
     def test_unsatisfying_criteria(self):
-
         criteria = "$raw_data_1/duration$ > 1000 and $raw_data_1/good_frames$ < 10000"
         res = SelectNexusFilesByMetadata(FileList=self._fileslist, NexusCriteria=criteria)
         self.assertFalse(res, "Output should be empty since no file satisfies this criteria")
 
     def test_cross_criteria(self):
-
         criteria = "10 * $raw_data_1/duration$ < $raw_data_1/good_frames$"
         res = SelectNexusFilesByMetadata(FileList=self._fileslist, NexusCriteria=criteria)
         outfiles = res.split(",")
@@ -63,7 +52,6 @@ class SelectNexusFilesByMetadataTest(unittest.TestCase):
         self.assertTrue(outfiles[0].endswith("INTER00013460.nxs"), "Should be 1st first file name")
 
     def test_string_criteria(self):
-
         filelist = "ILL/D33/001030.nxs"
         criteria = '$entry0/D33/name$ == "D33"'
         res = SelectNexusFilesByMetadata(FileList=filelist, NexusCriteria=criteria)
