1cd4ae1c1dce60e45f42a0e7a9031fd876315ca7
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2023-03-01 15:48:09 +0000

    added raise regex checks to SANSILLAutoProcessTest

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SANSILLAutoProcess.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SANSILLAutoProcess.py
@@ -159,7 +159,7 @@ class SANSILLAutoProcess(DataProcessorAlgorithm):
         if sens_dim != sample_dim and sens_dim > 1:
             result["SensitivityMaps"] = message.format("Sensitivity", sens_dim, sample_dim)
         if flux_dim != sample_dim and flux_dim > 1:
-            result["FluxRuns"] = message.format("Flux")
+            result["FluxRuns"] = message.format("Flux", flux_dim, sample_dim)
         if maxqxy_dim != sample_dim and maxqxy_dim > 1:
             result["MaxQxy"] = message_value.format("MaxQxy", maxqxy_dim, sample_dim)
         if deltaq_dim != sample_dim and deltaq_dim > 1:
