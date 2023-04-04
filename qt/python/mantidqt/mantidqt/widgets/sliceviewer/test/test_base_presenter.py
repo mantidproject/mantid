5811d4265a788dd963974e3dec257fe539824342
zjmorgan <zjmorgan@mtu.edu>
2023-04-04 14:09:38 -0400

    Fixing tests

--- qt/python/mantidqt/mantidqt/widgets/sliceviewer/test/test_base_presenter.py
+++ qt/python/mantidqt/mantidqt/widgets/sliceviewer/test/test_base_presenter.py
@@ -56,6 +56,7 @@ class SliceViewerBasePresenterTest(unittest.TestCase):
         self.patched_deps["WorkspaceInfo"].can_support_dynamic_rebinning.return_value = True
         new_plot_mock = mock.MagicMock()
         presenter.new_plot = new_plot_mock
+        presenter._data_view.get_data_limits_to_fill_current_axes = mock.MagicMock(return_value=None)
 
         presenter.data_limits_changed()
 
@@ -66,6 +67,7 @@ class SliceViewerBasePresenterTest(unittest.TestCase):
         self.patched_deps["WorkspaceInfo"].can_support_dynamic_rebinning.return_value = False
         new_plot_mock = mock.MagicMock()
         presenter.new_plot = new_plot_mock
+        presenter._data_view.get_data_limits_to_fill_current_axes = mock.MagicMock(return_value=None)
 
         presenter.data_limits_changed()
 
