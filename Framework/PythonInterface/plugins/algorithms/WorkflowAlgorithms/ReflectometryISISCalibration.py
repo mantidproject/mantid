4743ee350a30f10bad4cafa118261b6c607ec0c6
Rachel Baust <rachel.baust@stfc.ac.uk>
2023-04-25 16:14:13 +0100

    Change ReflectometryISISCalibration to use new logic
    Also removed debug mode that is no longer useful with new approach

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/ReflectometryISISCalibration.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/ReflectometryISISCalibration.py
@@ -4,7 +4,7 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
-from mantid.simpleapi import CreateEmptyTableWorkspace, ApplyCalibration, CloneWorkspace
+from mantid.simpleapi import CloneWorkspace, SpecularReflectionPositionCorrect
 from mantid.api import (
     AlgorithmFactory,
     AnalysisDataService,
@@ -16,6 +16,7 @@ from mantid.api import (
 )
 from mantid.kernel import Direction
 import csv
+import collections
 import math
 
 
@@ -23,7 +24,10 @@ class ReflectometryISISCalibration(DataProcessorAlgorithm):
     _WORKSPACE = "InputWorkspace"
     _CALIBRATION_FILE = "CalibrationFile"
     _OUTPUT_WORKSPACE = "OutputWorkspace"
-    _DEBUG = "Debug"
+
+    _DET_ID_LABEL = "detectorid"
+    _THETA_LABEL = "theta_offset"
+    _RAD_TO_DEG = 180.0 / math.pi
 
     def category(self):
         """Return the categories of the algorithm."""
@@ -35,152 +39,118 @@ class ReflectometryISISCalibration(DataProcessorAlgorithm):
 
     def summary(self):
         """Return a summary of the algorithm."""
-        return "Apply calibration data to detector pixel Y locations"
+        return "Corrects the positions of detector pixels by applying TwoTheta offsets provided in a calibration file"
 
     def seeAlso(self):
         """Return a list of related algorithm names."""
-        return ["ReflectometryISISLoadAndProcess"]
+        return ["ReflectometryISISLoadAndProcess", "SpecularReflectionPositionCorrect"]
 
     def PyInit(self):
         self.declareProperty(
             WorkspaceProperty(self._WORKSPACE, "", direction=Direction.Input, optional=PropertyMode.Mandatory),
-            doc="An input workspace or workspace group",
+            doc="An input workspace or workspace group.",
         )
         self.declareProperty(
             FileProperty(self._CALIBRATION_FILE, "", action=FileAction.OptionalLoad, direction=Direction.Input, extensions=["dat"]),
-            doc="Calibration data file containing Y locations for detector pixels in mm.",
+            doc=f"Calibration data file containing a list of detector IDs and twoTheta offsets (in degrees)."
+            f"These should be provided as two spaced-delimited columns, labelled {self._DET_ID_LABEL} and {self._THETA_LABEL}.",
         )
         self.declareProperty(
             WorkspaceProperty(self._OUTPUT_WORKSPACE, "", direction=Direction.Output), doc="The calibrated output workspace."
         )
-        self.copyProperties("ReflectometryReductionOneAuto", [self._DEBUG])
 
     def PyExec(self):
+        try:
+            calibration_data = self._parse_calibration_file(self.calibration_filepath)
+        except FileNotFoundError:
+            raise FileNotFoundError("Calibration file path cannot be found")
+
         ws = self.getProperty(self._WORKSPACE).value
         det_info = ws.detectorInfo()
-        comp_info = ws.componentInfo()
+        calibrated_ws = CloneWorkspace(InputWorkspace=ws)
 
-        specular_pixel_idx = self._find_specular_pixel_index(ws, det_info)
-        calib_table = self._create_calibration_table_from_scan(ws, det_info, comp_info, specular_pixel_idx)
+        for det_id, theta_offset in calibration_data.items():
+            new_two_theta = self._calculate_calibrated_two_theta(det_info, det_id, theta_offset)
+            # Passing calibrated_ws as both input and output means all the detector moves are applied to this workspace
+            SpecularReflectionPositionCorrect(
+                InputWorkspace=calibrated_ws,
+                TwoTheta=new_two_theta,
+                DetectorID=det_id,
+                MoveFixedDetectors=True,
+                OutputWorkspace=calibrated_ws,
+            )
 
-        # Apply calibration to workspace
-        output_ws = CloneWorkspace(InputWorkspace=ws)
-        ApplyCalibration(Workspace=output_ws, CalibrationTable=calib_table)
-        self.setProperty(self._OUTPUT_WORKSPACE, output_ws)
-        AnalysisDataService.remove("output_ws")
-        if self.getProperty(self._DEBUG).isDefault:
-            # Only output the calibration table if we're in debug mode
-            AnalysisDataService.remove(calib_table.name())
+        self.setProperty(self._OUTPUT_WORKSPACE, calibrated_ws)
+        AnalysisDataService.remove(calibrated_ws.name())
 
     def validateInputs(self):
         """Return a dictionary containing issues found in properties."""
         issues = dict()
 
-        calibration_file = self.getPropertyValue(self._CALIBRATION_FILE)
-        if not calibration_file:
+        self.calibration_filepath = self.getPropertyValue(self._CALIBRATION_FILE)
+        if not self.calibration_filepath:
             issues[self._CALIBRATION_FILE] = "Calibration file path must be provided"
-        else:
-            calib_file_error = self._validate_calibration_filepath(calibration_file)
-            if calib_file_error:
-                issues[self._CALIBRATION_FILE] = calib_file_error
         return issues
 
-    def _validate_calibration_filepath(self, filepath):
-        try:
-            self._scanned_pixel_positions = self._get_pixel_positions_from_file(filepath)
-            if len(self._scanned_pixel_positions) == 0:
-                return "Calibration file provided contains no data"
-        except FileNotFoundError:
-            return "Calibration file path cannot be found"
-        except Exception:
-            return "Could not read contents of calibration file, please see documentation to check the required format"
-        return ""
-
-    def _find_specular_pixel_index(self, ws, det_info):
-        # Determine position of the specular pixel from the workspace:
-        ws_two_theta = ws.run().getTimeAveragedValue("theta") * 2
-
-        min_theta_diff = None
-        specular_pixel_idx = None
-
-        for i in range(0, ws.getNumberHistograms()):
-            try:
-                detector = ws.getDetector(i)
-            except RuntimeError:
-                # Exclude point detectors that don't have IDs
-                continue
-
-            detector_index = det_info.indexOf(detector.getID())
-
-            if det_info.isMonitor(detector_index):
-                continue
-
-            # Get two theta for the detector in radians
-            det_two_theta = det_info.twoTheta(detector_index) * (180 / math.pi)
-
-            # The specular pixel is the one that has a two theta value closest to the workspace two theta value
-            theta_diff = abs(ws_two_theta - det_two_theta)
-            if min_theta_diff is None or theta_diff < min_theta_diff:
-                min_theta_diff = theta_diff
-                specular_pixel_idx = i
-
-        if not specular_pixel_idx:
-            raise RuntimeError("Could not find specular pixel index from the workspace")
-
-        return specular_pixel_idx
-
-    def _get_pixel_positions_from_file(self, filepath):
-        # Create dictionary of pixel spectrum number and position from scan
-        scanned_pixel_positions = {}
+    def _parse_calibration_file(self, filepath):
+        # Create dictionary of detector ID and theta offset from scan
+        scanned_theta_offsets = {}
         with open(filepath, "r") as file:
-            pixels = csv.reader(file)
-            for pixel in pixels:
-                pixel_info = pixel[0].split()
-                scanned_pixel_positions[int(pixel_info[0])] = float(pixel_info[1])
-        return scanned_pixel_positions
+            labels_checked = False
+            det_id_idx = 0
+            theta_offset_idx = 1
 
-    def _create_calibration_table_from_scan(self, ws, det_info, comp_info, specular_pixel_idx):
-        # Get the current specular pixel Y position and the position of the specular pixel from the calibration scan
-        specpixel_y = ws.getDetector(specular_pixel_idx).getPos().Y()
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                entries = row[0].split()
 
-        scanned_specpixel_y = self._scanned_pixel_positions.get(specular_pixel_idx)
-        if not scanned_specpixel_y:
-            raise RuntimeError(f"Missing calibration data for specular pixel with workspace index {specular_pixel_idx}")
+                if entries[0][0] == "#":
+                    # Ignore any lines that begin with a #
+                    # This allows the user to add any metadata they would like
+                    continue
 
-        table = CreateEmptyTableWorkspace(OutputWorkspace=f"Calib_Table_{str(ws.getRunNumber())}")
-        table.addColumn(type="int", name="Detector ID")
-        table.addColumn(type="V3D", name="Detector Position")
-        table.addColumn(type="double", name="Detector Y Coordinate")
-        table.addColumn(type="double", name="Detector Height")
-        table.addColumn(type="double", name="Detector Width")
+                if len(entries) != 2:
+                    raise RuntimeError(
+                        f"Calibration file should contain two columns, labelled as {self._DET_ID_LABEL} and {self._THETA_LABEL}"
+                    )
 
-        # Populate the table with the calibrated data
-        for ws_id, scanned_pixel_pos in self._scanned_pixel_positions.items():
-            try:
-                det = ws.getDetector(ws_id)
-            except RuntimeError:
-                raise RuntimeError(f"Could not find detector to calibrate at workspace id {ws_id}")
+                if not labels_checked:
+                    # The labels should be the first row in the file that doesn't begin with a #
+                    valid_labels = collections.Counter([self._DET_ID_LABEL, self._THETA_LABEL])
+                    first_label = entries[det_id_idx].lower()
+                    second_label = entries[theta_offset_idx].lower()
+                    if collections.Counter([first_label, second_label]) == valid_labels:
+                        if first_label == self._THETA_LABEL:
+                            # Allow the columns to be specified in any order
+                            det_id_idx = 1
+                            theta_offset_idx = 0
+                        labels_checked = True
+                        continue
+                    else:
+                        raise ValueError(
+                            f"Incorrect column labels in calibration file - should be {self._DET_ID_LABEL} and {self._THETA_LABEL}"
+                        )
 
-            det_idx = det_info.indexOf(det.getID())  # detectorInfo and componentInfo index
-            box = comp_info.shape(det_idx).getBoundingBox().width()
-            scalings = comp_info.scaleFactor(det_idx)
+                try:
+                    scanned_theta_offsets[int(entries[det_id_idx])] = float(entries[theta_offset_idx])
+                except ValueError:
+                    raise ValueError(
+                        "Invalid data in calibration file - detector ids should be integers and theta offsets should be floats"
+                    )
 
-            next_row = {
-                "Detector ID": det.getID(),
-                "Detector Position": det.getPos(),
-                "Detector Y Coordinate": self._calculate_calibrated_y_pos(specpixel_y, scanned_specpixel_y, scanned_pixel_pos),
-                "Detector Width": box[0] * scalings[0],
-                "Detector Height": box[1] * scalings[1],
-            }
-            table.addRow(next_row)
-        return table
+            if len(scanned_theta_offsets) == 0:
+                raise RuntimeError("Calibration file provided contains no data")
+        return scanned_theta_offsets
 
-    def _calculate_calibrated_y_pos(self, specpixel_y, scanned_specpixel_y, scanned_pixel_pos):
-        # First find the difference between the specular pixel position and the pixel position in the scan data,
-        # and convert to metres.
-        # The calibrated Y co-ordinate is then found by adding this difference to the specular pixel position from
-        # the IDF.
-        return specpixel_y + (scanned_specpixel_y - scanned_pixel_pos) * 0.001
+    def _calculate_calibrated_two_theta(self, det_info, det_id, theta_offset):
+        """Calculates the new twoTheta value for a detector from a given offset, in degrees"""
+        try:
+            det_idx = det_info.indexOf(det_id)
+        except IndexError:
+            raise RuntimeError(f"Detector id {det_id} from calibration file cannot be found in input workspace")
+
+        current_two_theta = det_info.twoTheta(det_idx)
+        return (current_two_theta * self._RAD_TO_DEG) + theta_offset
 
 
 AlgorithmFactory.subscribe(ReflectometryISISCalibration)
--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ReflectometryISISPreprocessTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ReflectometryISISPreprocessTest.py
@@ -76,18 +76,6 @@ class ReflectometryISISPreprocessTest(unittest.TestCase):
         self.assertIsInstance(output_ws, WorkspaceGroup)
         self.assertEqual(output_ws.getNumberOfEntries(), 2)
 
-    def test_calibration_file_is_applied_when_provided(self):
-        args = {"InputRunList": "INTER45455", "Debug": True, "CalibrationFile": self._CALIBRATION_TEST_DATA, "OutputWorkspace": "ws"}
-        output_ws = self._run_test(args)
-        self.assertIsInstance(output_ws, MatrixWorkspace)
-        self._check_calibration(output_ws, is_calibrated=True)
-
-    def test_calibration_is_skipped_if_file_not_provided(self):
-        args = {"InputRunList": "INTER45455", "Debug": True, "OutputWorkspace": "ws"}
-        output_ws = self._run_test(args)
-        self.assertIsInstance(output_ws, MatrixWorkspace)
-        self._check_calibration(output_ws, is_calibrated=False)
-
     def test_workspace_group_with_calibration_throws(self):
         args = {"InputRunList": "POLREF14966", "CalibrationFile": self._CALIBRATION_TEST_DATA, "OutputWorkspace": "ws"}
         self._assert_run_algorithm_raises_exception(args, "Workspace Group")
@@ -113,9 +101,6 @@ class ReflectometryISISPreprocessTest(unittest.TestCase):
         """Run the algorithm with the given args and check it raises the expected exception"""
         alg = self._setup_algorithm(args)
         self.assertRaisesRegex(RuntimeError, error_msg_regex, alg.execute)
-
-    def _check_calibration(self, ws, is_calibrated):
-        self.assertEqual(is_calibrated, AnalysisDataService.doesExist(f"Calib_Table_{str(ws.getRunNumber())}"))
 
 
 if __name__ == "__main__":
