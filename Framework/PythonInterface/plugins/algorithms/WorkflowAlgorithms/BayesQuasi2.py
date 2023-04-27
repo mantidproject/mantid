0b83710950dd39b23c2378c4b3bfa363e3297fd1
Anthony Lim <anthony.lim@stfc.ac.uk>
2023-04-27 10:57:49 +0100

    refs 35442 adding quickbayes

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/BayesQuasi2.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/BayesQuasi2.py
@@ -10,12 +10,30 @@ from mantid.api import PythonAlgorithm, AlgorithmFactory, MatrixWorkspacePropert
 from mantid.kernel import StringListValidator, Direction
 import mantid.simpleapi as s_api
 from mantid import logger
-from IndirectCommon import *
-from IndirectCommon import GetThetaQ
-from quickBayesHelper import make_fit_ws, make_results, add_sample_logs
+from IndirectCommon import GetThetaQ, CheckHistZero, CheckHistSame
+#from quickBayesHelper import make_fit_ws, make_results, add_sample_logs
+from typing import Dict
+from numpy import ndarray
+try:
+    from quickBayes.fitting.fit_engine import FitEngine
+except (Exception, Warning):
+    import subprocess
+
+    print(
+        subprocess.Popen(
+            "python -m pip install -U --no-deps quickBayes==1.0.0b6",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+        ).communicate()
+    )
+    from quickBayes.fitting.fit_engine import FitEngine
 from quickBayes.functions.qldata_function import QlDataFunction
 from quickBayes.utils.general import get_background_function
 from quickBayes.workflow.QlData import QLData
+import numpy as np
+
 
 
 class BayesQuasi2(PythonAlgorithm):
@@ -56,18 +74,17 @@ class BayesQuasi2(PythonAlgorithm):
             validator=StringListValidator(["Linear", "Flat", "None"]),
             doc="Fit option for the type of background",
         )
-
+       
         self.declareProperty(
-            WorkspaceGroupProperty("OutputWorkspaceFit", "", direction=Direction.Output), doc="The name of the fit output workspaces"
+            WorkspaceGroupProperty('OutputWorkspaceFit', '', direction=Direction.Output), doc='The name of the fit output workspaces'
+        )
+        self.declareProperty(
+            MatrixWorkspaceProperty('OutputWorkspaceResult', '', direction=Direction.Output), doc='The name of the result output workspaces'
         )
 
         self.declareProperty(
-            MatrixWorkspaceProperty("OutputWorkspaceResult", "", direction=Direction.Output), doc="The name of the result output workspaces"
-        )
-
-        self.declareProperty(
-            MatrixWorkspaceProperty("OutputWorkspaceProb", "", optional=PropertyMode.Optional, direction=Direction.Output),
-            doc="The name of the probability output workspaces",
+            MatrixWorkspaceProperty('OutputWorkspaceProb', '', optional=PropertyMode.Optional, direction=Direction.Output),
+            doc='The name of the probability output workspaces',
         )
 
     def validateInputs(self):
@@ -81,16 +98,148 @@ class BayesQuasi2(PythonAlgorithm):
 
         return issues
 
-    # pylint: disable=too-many-locals,too-many-statements
+    def point_data(self, name):
+        workspace = self.getPropertyValue(name)
+        alg =self.createChildAlgorithm("ConvertToPointData", enableLogging=True)
+        alg.initialize()
+        alg.setProperty("InputWorkspace", workspace)
+        alg.setProperty("OutputWorkspace", workspace)
+        alg.execute()
+        return alg.getProperty("OutputWorkspace").value
+
+    def duplicate_res(self, res_ws, N):
+        res_list = []
+        for j in range(N):
+            res_list.append({"x": res_ws.readX(0), "y": res_ws.readY(0)})
+        return res_list
+
+    def unique_res(self, res_ws, N):
+        res_list = []
+        for j in range(N):
+            res_list.append({"x": res_ws.readX(j), "y": res_ws.readY(j)})
+        return res_list
+
+
+    def create_ws(self, OutputWorkspace, DataX, DataY, NSpec,
+                    UnitX, YUnitLabel, VerticalAxisUnit, VerticalAxisValues, DataE=None):
+        alg =self.createChildAlgorithm("CreateWorkspace")
+        #alg.initialize()
+        alg.setAlwaysStoreInADS(True)
+        alg.setProperty('OutputWorkspace', OutputWorkspace)
+        alg.setProperty('DataX', DataX)
+        alg.setProperty('DataY', DataY)
+        alg.setProperty('NSpec', NSpec)
+        #alg.setProperty('UnitX', UnitX)
+        #alg.setProperty('YUnitLabel', YUnitLabel)
+        #alg.setProperty('VerticalAxisUnit', VerticalAxisUnit)
+        #alg.setProperty('VerticalAxisValues', VerticalAxisValues)
+        #if DataE is not None:
+        #    alg.setProperty('DataE', DataE)
+
+        alg.execute()
+        return alg.getPropertyValue("OutputWorkspace")
+
+    def make_fit_ws(self, engine, max_features, ws_list, x_unit,  name):
+        """
+        Simple function for creating a fit ws
+        :param engine: the quickBayes fit engine used
+        :param max_features: the maximum number of features (e.g. lorentzians)
+        :param ws_list: list of fit workspaces (inout)
+        :param x_unit: the x axis unit
+        :param name: name of the output
+        :return the list of fitting workspaces
+        """
+        x = list(engine._x_data[:1901])
+        y = list(engine._y_data[:1901])
+        axis_names = ["data"]
+        for j in range(max_features):
+            print(j)
+            x_data, fit, e, df, de = engine.get_fit_values(j)
+
+            y += list(fit[:1901]) + list(df[:1901])
+            x += list(x_data[:1901]) + list(x_data[:1901])
+            axis_names.append(f'fit {j+1}')
+            axis_names.append(f'diff {j+1}')
+            ws = self.create_ws(
+                OutputWorkspace=f'{name}_workspace',
+                DataX=np.array(x),
+                DataY=np.array(y),
+                NSpec=len(axis_names),
+                UnitX=x_unit,
+                YUnitLabel="",
+                VerticalAxisUnit="Text",
+                VerticalAxisValues=axis_names)
+        ws_list.append(ws)
+        #print(ws_list)
+        return ws_list
+
+    def make_results(self, results: Dict['str', ndarray], results_errors: Dict['str', ndarray],
+                     x_data: ndarray, x_unit: str, max_features: int, name: str):
+        """
+        Takes the output of quickBayes and makes Mantid workspaces
+        :param results: dict of quickBayes parameter results
+        :param results_errors: dict of quickBayes parameter errors
+        :param x_data: the x data for plotting the results (e.g. Q)
+        :param x_unit: the x unit
+        :param max_features: the maximum number of features used
+        :param name: the name of the output worksapce
+        :return workspace of fit paramters and workspace of loglikelihoods (probs)
+        """
+        axis_names = []
+        y_data = []
+        e_data = []
+        prob = []
+
+        for key in results.keys():
+            if "loglikelihood" in key:
+                prob += list(results[key])
+            else:
+                y_data.append(results[key])
+                e_data.append(results_errors[key])
+                axis_names.append(key)
+
+        params = self.create_ws(
+            OutputWorkspace=f"{name}_results",
+            DataX=np.array(x_data),
+            DataY=np.array(y_data),
+            DataE=np.array(e_data),
+            NSpec=len(axis_names),
+            UnitX=x_unit,
+            YUnitLabel="",
+            VerticalAxisUnit="Text",
+            VerticalAxisValues=axis_names
+        )
+
+        prob_ws = self.create_ws(
+            OutputWorkspace=f"{name}_prob",
+            DataX=np.array(x_data),
+            DataY=np.array(prob),
+            NSpec=max_features,
+            UnitX=x_unit,
+            YUnitLabel="",
+            VerticalAxisUnit="Text",
+            VerticalAxisValues=[f"{k + 1} feature(s)" for k in range(max_features)]
+        )
+        return f"{name}_results", f"{name}_prob"
+
     def PyExec(self):
         self.log().information("BayesQuasi input")
         program = self.getPropertyValue("Program")
 
-        sample_ws = self.getPropertyValue("SampleWorkspace")
-        sample_ws = s_api.ConvertToPointData(sample_ws)
+        name = self.getPropertyValue("SampleWorkspace")
 
-        res_ws = self.getPropertyValue("ResolutionWorkspace")
-        res_ws = s_api.ConvertToPointData(res_ws)
+        alg =self.createChildAlgorithm("ConvertToPointData")
+        alg.setProperty("InputWorkspace", name)
+        alg.setProperty("OutputWorkspace", name)
+        alg.execute()
+        sample_ws = alg.getProperty("OutputWorkspace").value
+
+        res_name = self.getPropertyValue("ResolutionWorkspace")
+        #alg2 =self.createChildAlgorithm("ConvertToPointData")
+        alg.setProperty("InputWorkspace", res_name)
+        alg.setProperty("OutputWorkspace", res_name)
+        alg.execute()
+        res_ws = alg.getProperty("OutputWorkspace").value
 
         start_x = self.getProperty("MinRange").value
         end_x = self.getProperty("MaxRange").value
@@ -105,51 +254,45 @@ class BayesQuasi2(PythonAlgorithm):
 
         N = sample_ws.getNumberHistograms()
         report_progress = Progress(self, start=0.0, end=1.0, nreports=N)
-        nres = CheckHistZero(res_ws.name())[0]
-
-        # setup_prog.report("Checking Histograms")
+        self.log().notice(res_ws.name())
+        N_res_hist = res_ws.getNumberHistograms() 
+        prog = None
+        res_list = None
         if program == "QL":
-            if nres == 1:
+            if N_res_hist == 1:
                 prog = "QLr"  # res file
-            else:
+                res_list = self.duplicate_res(res_ws, N)
+            elif N_res_hist == N:
                 prog = "QLd"  # data file
-                CheckHistSame(sample_ws.name(), "Sample", res_ws.name(), "Resolution")
+                res_list = self.unique_res(res_ws, N)
+            else:
+                raise ValueError("RES file needs to have either 1 or the same number of histograms as sample.")
         elif program == "QSe":
-            if nres == 1:
+            if N_res_hist == 1:
                 prog = "QSe"  # res file
+                res_list = self.duplicate_res(res_ws, N)
             else:
                 raise ValueError("Stretched Exp ONLY works with RES file")
-
         logger.information("Version is {0}".format(prog))
         logger.information(" Number of spectra = {0} ".format(N))
         logger.information(" Erange : {0}  to {1} ".format(start_x, end_x))
-
-        # -4 to remove "_red" extension
-        name = sample_ws.name()[:-4] + "_" + prog
 
         results_errors = {}
         results = {}
         init_params = None
         ws_list = []
-        res_list = []
         max_num_peaks = 3
         BG = get_background_function(BG_str)
 
-        if res_ws.getNumberHistograms() == 1:
-            for j in range(N):
-                res_list.append({"x": res_ws.readX(0), "y": res_ws.readY(0)})
-        elif res_ws.getNumberHistograms() == N:
-            for j in range(N):
-                res_list.append({"x": res_ws.readX(j), "y": res_ws.readY(j)})
         Q = GetThetaQ(sample_ws)
 
         sample_logs = [
-            ("res_workspace", res_ws),
+            ("res_workspace", res_name),
             ("fit_program", prog),
             ("background", BG_str),
             ("elastic_peak", elastic),
             ("energy_min", start_x),
-            ("energy_max", end_x),
+            ("energy_max", end_x)
         ]
 
         for spec in range(N):
@@ -179,18 +322,25 @@ class BayesQuasi2(PythonAlgorithm):
 
             engine = workflow.fit_engine
 
-            ws_list = make_fit_ws(engine, max_num_peaks, f"{name}_{spec}_", ws_list, "DeltaE")
+            ws_list = self.make_fit_ws(engine, max_num_peaks, ws_list, "DeltaE", f"{name}_{spec}_")
 
-        fits = s_api.GroupWorkspaces(ws_list, OutputWorkspace=f"{name}_workspaces")
-        add_sample_logs(fits, sample_logs, sample_ws)
-
-        params, prob = make_results(results, results_errors, Q[1], "MomentumTransfer", max_num_peaks, name)
-        add_sample_logs(params, sample_logs, sample_ws)
-
+        
+        alg3 =self.createChildAlgorithm("GroupWorkspaces")
+        alg3.setAlwaysStoreInADS(True)
+        alg3.setProperty("InputWorkspaces", ws_list)
+        alg3.setProperty("OutputWorkspace", f"{name}_workspaces")
+        alg3.execute()
+        
+        fits = alg3.getPropertyValue("OutputWorkspace")
         self.setProperty("OutputWorkspaceFit", fits)
+        #add_sample_logs(fits, sample_logs, sample_ws)
+        
+        params, prob = self.make_results(results, results_errors, Q[1], "MomentumTransfer", max_num_peaks, name)
+        #add_sample_logs(params, sample_logs, sample_ws)
+
         self.setProperty("OutputWorkspaceResult", params)
         self.setProperty("OutputWorkspaceProb", prob)
-
+        
 
 # Register algorithm with Mantid
 AlgorithmFactory.subscribe(BayesQuasi2)
--- scripts/quickBayesHelper.py
+++ scripts/quickBayesHelper.py
@@ -6,7 +6,8 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 import mantid.simpleapi as s_api
 
-from typing import List
+from typing import List, Dict
+from numpy import ndarray
 import numpy as np
 
 try:
@@ -16,7 +17,7 @@ except (Exception, Warning):
 
     print(
         subprocess.Popen(
-            "python -m pip install -U --no-deps quickBayes==1.0.0b5",
+            "python -m pip install -U --no-deps quickBayes==1.0.0b6",
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -25,21 +26,49 @@ except (Exception, Warning):
     )
     from quickBayes.fitting.fit_engine import FitEngine
 
+def create_ws(OutputWorkspace, DataX, DataY, NSpec,
+              UnitX, YUnitLabel, VerticalAxisUnit, VerticalAxisValues, DataE=None):
 
-def make_fit_ws(engine: FitEngine, max_num_peaks: int, name: str, ws_list: List, x_unit: str):
+    alg = s_api.AlgorithmManager.create("CreateWorkspace")
+    alg.initialize()
+    alg.setAlwaysStoreInADS(True)
+    alg.setProperty('OutputWorkspace', OutputWorkspace)
+    alg.setProperty('DataX', DataX)
+    alg.setProperty('DataY', DataY)
+    alg.setProperty('NSpec', NSpec)
+    alg.setProperty('UnitX', UnitX)
+    alg.setProperty('YUnitLabel', YUnitLabel)
+    alg.setProperty('VerticalAxisUnit', VerticalAxisUnit)
+    alg.setProperty('VerticalAxisValues', VerticalAxisValues)
+    if DataE is not None:
+        alg.setProperty('DataE', DataE)
+
+    alg.execute()
+    return alg.getProperty("OutputWorkspace")
+
+
+
+def make_fit_ws(engine: FitEngine, max_features: int, ws_list: List, x_unit: str,  name: str) -> List:
+    """
+    Simple function for creating a fit ws
+    :param engine: the quickBayes fit engine used
+    :param max_features: the maximum number of features (e.g. lorentzians)
+    :param ws_list: list of fit workspaces (inout)
+    :param x_unit: the x axis unit
+    :param name: name of the output
+    :return the list of fitting workspaces
+    """
     x = list(engine._x_data)
     y = list(engine._y_data)
     axis_names = ["data"]
-    for j in range(max_num_peaks):
+    for j in range(max_features):
         x_data, fit, e, df, de = engine.get_fit_values(j)
 
         y += list(fit) + list(df)
         x += list(x_data) + list(x_data)
         axis_names.append(f"fit {j+1}")
         axis_names.append(f"diff {j+1}")
-
-    ws_list.append(
-        s_api.CreateWorkspace(
+        create_ws(
             OutputWorkspace=f"{name}_workspace",
             DataX=np.array(x),
             DataY=np.array(y),
@@ -47,13 +76,23 @@ def make_fit_ws(engine: FitEngine, max_num_peaks: int, name: str, ws_list: List,
             UnitX=x_unit,
             YUnitLabel="",
             VerticalAxisUnit="Text",
-            VerticalAxisValues=axis_names,
-        )
-    )
+            VerticalAxisValues=axis_names)
+        ws_list.append(f"{name}_workspace")
     return ws_list
 
 
-def make_results(results, results_errors, x_data, x_unit, max_features, name):
+def make_results(results: Dict['str', ndarray], results_errors: Dict['str', ndarray],
+                 x_data: ndarray, x_unit: str, max_features: int, name: str):
+    """
+    Takes the output of quickBayes and makes Mantid workspaces
+    :param results: dict of quickBayes parameter results
+    :param results_errors: dict of quickBayes parameter errors
+    :param x_data: the x data for plotting the results (e.g. Q)
+    :param x_unit: the x unit
+    :param max_features: the maximum number of features used
+    :param name: the name of the output worksapce
+    :return workspace of fit paramters and workspace of loglikelihoods (probs)
+    """
     axis_names = []
     y_data = []
     e_data = []
@@ -66,7 +105,8 @@ def make_results(results, results_errors, x_data, x_unit, max_features, name):
             y_data.append(results[key])
             e_data.append(results_errors[key])
             axis_names.append(key)
-    params = s_api.CreateWorkspace(
+
+    params = create_ws(
         OutputWorkspace=f"{name}_results",
         DataX=np.array(x_data),
         DataY=np.array(y_data),
@@ -75,10 +115,10 @@ def make_results(results, results_errors, x_data, x_unit, max_features, name):
         UnitX=x_unit,
         YUnitLabel="",
         VerticalAxisUnit="Text",
-        VerticalAxisValues=axis_names,
+        VerticalAxisValues=axis_names
     )
 
-    prob_ws = s_api.CreateWorkspace(
+    prob_ws = create_ws(
         OutputWorkspace=f"{name}_prob",
         DataX=np.array(x_data),
         DataY=np.array(prob),
@@ -86,11 +126,20 @@ def make_results(results, results_errors, x_data, x_unit, max_features, name):
         UnitX=x_unit,
         YUnitLabel="",
         VerticalAxisUnit="Text",
-        VerticalAxisValues=[f"{k + 1} feature(s)" for k in range(max_features)],
+        VerticalAxisValues=[f"{k + 1} feature(s)" for k in range(max_features)]
     )
-    return params, prob_ws
+    return f"{name}_results", f"{name}_prob",
 
 
-def add_sample_logs(workspace, sample_logs, data_ws):
-    s_api.CopyLogs(InputWorkspace=data_ws, OutputWorkspace=workspace)
-    s_api.AddSampleLogMultiple(Workspace=workspace, LogNames=[log[0] for log in sample_logs], LogValues=[log[1] for log in sample_logs])
+def add_sample_logs(workspace, sample_logs: List, data_ws):
+    """
+    Method for adding sample logs to results
+    :param workspace: the workspace to add sample logs too
+    :param sample_logs: new sample logs to add
+    :param data_ws: the workspace to copy sample logs from
+    """
+    for k in sample_logs:
+        print("mooo", k)
+    print()
+    #s_api.CopyLogs(InputWorkspace=data_ws, OutputWorkspace=workspace)
+    #s_api.AddSampleLogMultiple(Workspace=workspace, LogNames=[log[0] for log in sample_logs], LogValues=[log[1] for log in sample_logs])
