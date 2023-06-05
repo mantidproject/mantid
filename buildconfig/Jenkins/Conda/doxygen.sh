cac7179f6b43be62c2113fae177c141d2e7b6db1
Robert <robert.applin@stfc.ac.uk>
2023-06-05 08:41:38 +0100

    Remove job name argument for doxygen

--- buildconfig/Jenkins/Conda/doxygen.sh
+++ buildconfig/Jenkins/Conda/doxygen.sh
@@ -1,33 +1,27 @@
 # This script will set up a conda environment before building the doxygen target.
 #
 # Script usage:
-# doxygen.sh <job-name> <path-to-workspace>
+# doxygen.sh <path-to-workspace>
 #
 # Example command to run a PR build on ubuntu:
-# doxygen.sh $JOB_NAME $WORKSPACE
+# doxygen.sh $WORKSPACE
 #
 # Expected args:
-#   1. JOB_NAME: If the name of the job contains 'pull_requests', the script enters the workspace directory.
-#   2. WORKSPACE: path to the root of the source code. On Windows, only use / for
+#   1. WORKSPACE: path to the root of the source code. On Windows, only use / for
 #                 this argument do not use \\ or \ in the path.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/mamba-utils
 
 # Check 1 argument is passed and is not optional
-if [[ $# < 2 || $1 == "--"* || $2 == "--"* ]]; then
-    echo "Pass 2 arguments: doxygen.sh <job-name> <path-to-workspace>"
+if [[ $# < 1 || $1 == "--"* ]]; then
+    echo "Pass 1 argument: doxygen.sh <path-to-workspace>"
     exit 1
 fi
 
-JOB_NAME=$1
-WORKSPACE=$2
-shift 2
+WORKSPACE=$1
+shift 1
 
-if [[ $JOB_NAME == *pull_requests* ]]; then
-    # This relies on the fact pull requests use pull/$PR-NAME
-    # which squashes the branch into a single merge commit
-    cd $WORKSPACE
-fi
+cd $WORKSPACE
 
 # Setup Mamba. Create and activate environment
 setup_mamba $WORKSPACE/mambaforge "" true
