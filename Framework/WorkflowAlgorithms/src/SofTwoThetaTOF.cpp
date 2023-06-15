4d28d7dca35c7dcf3eacbb0938862b0b79d01e3e
rboston628 <bostonsr@ornl.gov>
2023-06-15 12:36:25 -0400

    suppress cppcheck complaining about more unchanged 9-year-old code

--- Framework/WorkflowAlgorithms/src/SofTwoThetaTOF.cpp
+++ Framework/WorkflowAlgorithms/src/SofTwoThetaTOF.cpp
@@ -188,6 +188,7 @@ API::MatrixWorkspace_sptr SofTwoThetaTOF::groupByTwoTheta(API::MatrixWorkspace_s
     tempPath /= boost::filesystem::unique_path("detector-grouping-%%%%-%%%%-%%%%-%%%%.xml");
     filename = tempPath.string();
     generateGrouping->setProperty("GenerateParFile", false);
+    // cppcheck-suppress unmatchedSuppression
     // Make sure the file gets deleted at scope exit.
     // cppcheck-suppress unreadVariable
     deleteThisLater.name = filename;
