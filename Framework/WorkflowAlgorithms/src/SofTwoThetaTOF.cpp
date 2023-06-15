cb5818a4d522f0c0966e3806e9d5081e16156aa8
rboston628 <bostonsr@ornl.gov>
2023-06-15 14:20:43 -0400

    make cppcheck stop

--- Framework/WorkflowAlgorithms/src/SofTwoThetaTOF.cpp
+++ Framework/WorkflowAlgorithms/src/SofTwoThetaTOF.cpp
@@ -189,7 +189,6 @@ API::MatrixWorkspace_sptr SofTwoThetaTOF::groupByTwoTheta(API::MatrixWorkspace_s
     filename = tempPath.string();
     generateGrouping->setProperty("GenerateParFile", false);
     // Make sure the file gets deleted at scope exit.
-    // cppcheck-suppress unreadVariable
     deleteThisLater.name = filename;
   } else {
     filename = static_cast<std::string>(getProperty(Prop::FILENAME));
