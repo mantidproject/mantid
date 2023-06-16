9643f7791cc3536aaa2762114ea31bf2adce4d1c
Pete Peterson <petersonpf@ornl.gov>
2023-06-16 12:25:58 -0400

    Consolodate duplicate code in the test

--- Framework/Algorithms/test/FindCenterOfMassPosition2Test.h
+++ Framework/Algorithms/test/FindCenterOfMassPosition2Test.h
@@ -32,34 +32,51 @@ public:
    * Generate fake data for which we know what the result should be
    */
   FindCenterOfMassPosition2Test() {
-    inputWS = "sampledata";
-    center_x = 25.5;
-    center_y = 10.5;
-    pixel_size = 0.005;
-
-    ws = SANSInstrumentCreationHelper::createSANSInstrumentWorkspace(inputWS);
+    DataObjects::Workspace2D_sptr ws = SANSInstrumentCreationHelper::createSANSInstrumentWorkspace(m_inputWSname);
 
     // Generate sample data as a 2D Gaussian around the defined center
-    for (int ix = 0; ix < SANSInstrumentCreationHelper::nBins; ix++) {
-      for (int iy = 0; iy < SANSInstrumentCreationHelper::nBins; iy++) {
-        int i = ix * SANSInstrumentCreationHelper::nBins + iy + SANSInstrumentCreationHelper::nMonitors;
+    const std::size_t NUM_BINS = static_cast<std::size_t>(SANSInstrumentCreationHelper::nBins);
+    for (std::size_t ix = 0; ix < NUM_BINS; ix++) {
+      for (std::size_t iy = 0; iy < NUM_BINS; iy++) {
+        std::size_t i = ix * NUM_BINS + iy + static_cast<std::size_t>(SANSInstrumentCreationHelper::nMonitors);
         auto &X = ws->mutableX(i);
         auto &Y = ws->mutableY(i);
         auto &E = ws->mutableE(i);
         X[0] = 1;
         X[1] = 2;
-        double dx = (center_x - (double)ix);
-        double dy = (center_y - (double)iy);
+        double dx = (m_centerX - (double)ix);
+        double dy = (m_centerY - (double)iy);
         Y[0] = exp(-(dx * dx + dy * dy));
         // Set tube extrema to special values
-        if (iy == 0 || iy == SANSInstrumentCreationHelper::nBins - 1)
+        if (iy == 0 || iy + 1 == NUM_BINS)
           Y[0] = (iy % 2) ? std::nan("") : std::numeric_limits<double>::infinity();
         E[0] = 1;
       }
     }
   }
 
-  ~FindCenterOfMassPosition2Test() override { AnalysisDataService::Instance().clear(); }
+  ~FindCenterOfMassPosition2Test() override { AnalysisDataService::Instance().remove(m_inputWSname); }
+
+  void validateCenterAndRemoveTableWS(const std::string &tableWSname, const double centerX, const double centerY,
+                                      const double tolerance) {
+    Mantid::DataObjects::TableWorkspace_sptr table =
+        AnalysisDataService::Instance().retrieveWS<Mantid::DataObjects::TableWorkspace>(tableWSname);
+    TS_ASSERT(table);
+
+    TS_ASSERT_EQUALS(table->rowCount(), 2);
+    TS_ASSERT_EQUALS(table->columnCount(), 2);
+
+    TableRow row = table->getFirstRow();
+    TS_ASSERT_EQUALS(row.String(0), "X (m)");
+    TS_ASSERT_DELTA(row.Double(1), centerX, tolerance);
+
+    row = table->getRow(1);
+    TS_ASSERT_EQUALS(row.String(0), "Y (m)");
+    TS_ASSERT_DELTA(row.Double(1), centerY, tolerance);
+
+    // remove the TableWorkspace from the ADS
+    AnalysisDataService::Instance().remove(tableWSname);
+  }
 
   void testParameters() {
     Mantid::Algorithms::FindCenterOfMassPosition2 center;
@@ -76,7 +93,7 @@ public:
       center.initialize();
 
     const std::string outputWS("center_of_mass");
-    center.setPropertyValue("InputWorkspace", inputWS);
+    center.setPropertyValue("InputWorkspace", m_inputWSname);
     center.setPropertyValue("Output", outputWS);
     center.setPropertyValue("CenterX", "0");
     center.setPropertyValue("CenterY", "0");
@@ -84,20 +101,7 @@ public:
     TS_ASSERT_THROWS_NOTHING(center.execute())
     TS_ASSERT(center.isExecuted())
 
-    // Get the resulting table workspace
-    Mantid::DataObjects::TableWorkspace_sptr table =
-        AnalysisDataService::Instance().retrieveWS<Mantid::DataObjects::TableWorkspace>(outputWS);
-
-    TS_ASSERT_EQUALS(table->rowCount(), 2);
-    TS_ASSERT_EQUALS(table->columnCount(), 2);
-
-    TableRow row = table->getFirstRow();
-    TS_ASSERT_EQUALS(row.String(0), "X (m)");
-    TS_ASSERT_DELTA(row.Double(1), center_x * pixel_size, 0.0001);
-
-    row = table->getRow(1);
-    TS_ASSERT_EQUALS(row.String(0), "Y (m)");
-    TS_ASSERT_DELTA(row.Double(1), center_y * pixel_size, 0.0001);
+    validateCenterAndRemoveTableWS(outputWS, m_centerX * m_pixel_size, m_centerY * m_pixel_size, 0.0001);
   }
 
   void testExecScatteredData() {
@@ -106,30 +110,17 @@ public:
       center.initialize();
 
     const std::string outputWS("center_of_mass");
-    center.setPropertyValue("InputWorkspace", inputWS);
+    center.setPropertyValue("InputWorkspace", m_inputWSname);
     center.setPropertyValue("Output", outputWS);
-    center.setPropertyValue("CenterX", "0");
-    center.setPropertyValue("CenterY", "0");
-    center.setPropertyValue("DirectBeam", "0");
-    center.setPropertyValue("BeamRadius", "0.0075"); // 1.5*0.005, now in meters, not in pixels
+    center.setProperty("CenterX", 0.);
+    center.setProperty("CenterY", 0.);
+    center.setProperty("DirectBeam", false);
+    center.setProperty("BeamRadius", 0.0075); // 1.5*0.005, now in meters, not in pixels
 
     TS_ASSERT_THROWS_NOTHING(center.execute())
     TS_ASSERT(center.isExecuted())
 
-    // Get the resulting table workspace
-    Mantid::DataObjects::TableWorkspace_sptr table =
-        AnalysisDataService::Instance().retrieveWS<Mantid::DataObjects::TableWorkspace>(outputWS);
-
-    TS_ASSERT_EQUALS(table->rowCount(), 2);
-    TS_ASSERT_EQUALS(table->columnCount(), 2);
-
-    TableRow row = table->getFirstRow();
-    TS_ASSERT_EQUALS(row.String(0), "X (m)");
-    TS_ASSERT_DELTA(row.Double(1), center_x * pixel_size, 0.0001);
-
-    row = table->getRow(1);
-    TS_ASSERT_EQUALS(row.String(0), "Y (m)");
-    TS_ASSERT_DELTA(row.Double(1), center_y * pixel_size, 0.0001);
+    validateCenterAndRemoveTableWS(outputWS, m_centerX * m_pixel_size, m_centerY * m_pixel_size, 0.0001);
   }
 
   void testExecWithArrayResult() {
@@ -137,17 +128,17 @@ public:
     if (!center.isInitialized())
       center.initialize();
 
-    center.setPropertyValue("InputWorkspace", inputWS);
-    center.setPropertyValue("CenterX", "0");
-    center.setPropertyValue("CenterY", "0");
+    center.setPropertyValue("InputWorkspace", m_inputWSname);
+    center.setProperty("CenterX", 0.);
+    center.setProperty("CenterY", 0.);
 
     TS_ASSERT_THROWS_NOTHING(center.execute())
     TS_ASSERT(center.isExecuted())
 
     std::vector<double> list = center.getProperty("CenterOfMass");
     TS_ASSERT_EQUALS(list.size(), 2);
-    TS_ASSERT_DELTA(list[0], center_x * pixel_size, 0.0001);
-    TS_ASSERT_DELTA(list[1], center_y * pixel_size, 0.0001);
+    TS_ASSERT_DELTA(list[0], m_centerX * m_pixel_size, 0.0001);
+    TS_ASSERT_DELTA(list[1], m_centerY * m_pixel_size, 0.0001);
   }
 
   void testCG3Data() {
@@ -155,6 +146,7 @@ public:
     // values estimated by eye
     const double X_EXP{-0.0078};
     const double Y_EXP{-0.0143};
+    const std::string IN_WKSP_NAME("testCG3DataInputWorkspace");
 
     Mantid::Algorithms::FindCenterOfMassPosition2 center;
     center.initialize();
@@ -163,7 +155,7 @@ public:
     auto loader = center.createChildAlgorithm("LoadNexusProcessed");
     loader->initialize();
     loader->setPropertyValue("Filename", "CG3_beamcenter_input.nxs");
-    loader->setPropertyValue("OutputWorkspace", "testCG3DataInputWorkspace");
+    loader->setPropertyValue("OutputWorkspace", IN_WKSP_NAME);
     loader->setAlwaysStoreInADS(true); // required to retrieve later the workspace by its name
     loader->execute();
 
@@ -178,17 +170,9 @@ public:
     TS_ASSERT_THROWS_NOTHING(center.execute())
     TS_ASSERT(center.isExecuted())
 
-    // Get the resulting table workspace
-    Mantid::DataObjects::TableWorkspace_sptr table =
-        AnalysisDataService::Instance().retrieveWS<Mantid::DataObjects::TableWorkspace>(outputWSname);
+    validateCenterAndRemoveTableWS(outputWSname, X_EXP, Y_EXP, 0.5 * CENTER_TOL);
 
-    TableRow row = table->getFirstRow();
-    TS_ASSERT_EQUALS(row.String(0), "X (m)");
-    TS_ASSERT_DELTA(row.Double(1), X_EXP, CENTER_TOL);
-
-    row = table->getRow(1);
-    TS_ASSERT_EQUALS(row.String(0), "Y (m)");
-    TS_ASSERT_DELTA(row.Double(1), Y_EXP, CENTER_TOL);
+    AnalysisDataService::Instance().remove(IN_WKSP_NAME);
   }
 
   /*
@@ -197,19 +181,20 @@ public:
    * it in the set of unit tests.
    */
   void test_biosans_empty_cell() {
+    const std::string IN_WKSP_NAME("wav");
+
     // load in the data
     Mantid::DataHandling::LoadSpice2D loader;
     loader.initialize();
     loader.setPropertyValue("Filename", "BioSANS_empty_cell.xml");
-    const std::string inputWS("wav");
-    loader.setPropertyValue("OutputWorkspace", inputWS);
+    loader.setPropertyValue("OutputWorkspace", IN_WKSP_NAME);
     loader.execute();
 
     // run the centering algorithm
     Mantid::Algorithms::FindCenterOfMassPosition2 center;
     center.initialize();
 
-    TS_ASSERT_THROWS_NOTHING(center.setPropertyValue("InputWorkspace", inputWS))
+    TS_ASSERT_THROWS_NOTHING(center.setPropertyValue("InputWorkspace", IN_WKSP_NAME))
     const std::string outputWS("result");
     TS_ASSERT_THROWS_NOTHING(center.setPropertyValue("Output", outputWS))
     center.setProperty("CenterX", 0.);
@@ -219,16 +204,9 @@ public:
     TS_ASSERT_THROWS_NOTHING(center.execute())
     TS_ASSERT(center.isExecuted())
 
-    // Get the resulting table workspace
-    Mantid::DataObjects::TableWorkspace_sptr table =
-        AnalysisDataService::Instance().retrieveWS<Mantid::DataObjects::TableWorkspace>(outputWS);
-
-    TS_ASSERT_EQUALS(table->rowCount(), 2);
-    TS_ASSERT_EQUALS(table->columnCount(), 2);
-
     // Check that the position is the same as obtained with the HFIR code
-    TableRow row = table->getFirstRow();
-    TS_ASSERT_EQUALS(row.String(0), "X (m)");
+    validateCenterAndRemoveTableWS(outputWS, -0.40658, 0.0090835, 0.0001);
+
     // NOTE: Version 1 (from original IGOR HFIR code) computes everything in
     // pixels, where
     // the counts in a pixel is effectively put at the center of the pixel. In
@@ -244,17 +222,13 @@ public:
     // comes out
     // of the algorithm if the one-pixel mask is applied. See python unit tests.
     // For this test we simply compare to the correct output _without_ masking.
-    TS_ASSERT_DELTA(row.Double(1), -0.40658, 0.0001);
 
-    row = table->getRow(1);
-    TS_ASSERT_EQUALS(row.String(0), "Y (m)");
-    TS_ASSERT_DELTA(row.Double(1), 0.0090835, 0.0001);
+    AnalysisDataService::Instance().remove(IN_WKSP_NAME);
   }
 
 private:
-  std::string inputWS;
-  double center_x;
-  double center_y;
-  double pixel_size;
-  DataObjects::Workspace2D_sptr ws;
+  const std::string m_inputWSname{"FindCenterOfMassPosition2Test_engineered_input"};
+  const double m_centerX{25.5};
+  const double m_centerY{10.5};
+  const double m_pixel_size{0.005};
 };
