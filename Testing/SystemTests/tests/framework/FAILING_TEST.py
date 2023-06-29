bd3b3e249235abb200f1bf7db54ed181a2bb649d
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2023-06-29 09:50:51 +0100

    deleted test test

--- Testing/SystemTests/tests/framework/FAILING_TEST.py
+++
@@ -1,7 +0,0 @@
-from systemtesting import MantidSystemTest
-
-
-# Test that fails to test that failing system tests will end a build
-class FailingSystemTest(MantidSystemTest):
-    def runTest(self):
-        raise RuntimeError("The fake test failed!")
