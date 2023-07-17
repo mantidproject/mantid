9f51b0431324efcf5a369047e2b0797c186022d0
Pete Peterson <petersonpf@ornl.gov>
2023-07-17 13:39:11 -0400

    Do not allow PushDataPositive and OffsetData to both be supplied

---
+++ Framework/PythonInterface/test/python/plugins/algorithms/SNSPowderReductionTest.py
@@ -0,0 +1,24 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2023 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+import unittest
+from mantid.simpleapi import SNSPowderReduction
+from numpy import concatenate, arange, sort, array_equal, where
+
+# tests run x10 slower with this on, but it may be useful to track down issues refactoring
+CHECK_CONSISTENCY = False
+
+
+class SNSPowderReductionTest(unittest.TestCase):
+    def testValidateInputs(self):
+        # PushDataPositive and OffsetData cannot be specified together
+        self.assertRaises(
+            RuntimeError, SNSPowderReduction, Filename="PG3_46577", OutputDirectory="/tmp/", PushDataPositive="AddMinimum", OffsetData=42.0
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
