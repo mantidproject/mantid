6f4d34249afc4bd3cf1155430926296a2cb7b841
Adam J. Jackson <a.j.jackson@physics.org>
2023-07-18 12:55:53 +0100

    SimulatedDensityOfStates systemtest: hack for CI insight
    The CI systemtest is failing, and it appears to be because Euphonic is
    unavailable. That seems unlikely, so add some extra nonsense to get a
    more fine-grained view on what is going wrong.

--- Testing/SystemTests/tests/framework/SimulatedDensityOfStatesEuphonic.py
+++ Testing/SystemTests/tests/framework/SimulatedDensityOfStatesEuphonic.py
@@ -19,6 +19,13 @@ class SimulatedDensityOfStatesEuphonicTest(MantidSystemTest):
     """ "Install Euphonic library to temporary prefix and check results"""
 
     def runTest(self):
+        from dos.load_euphonic import euphonic_available
+
+        from euphonic.cli.utils import force_constants_from_file  # noqa: F401
+        from euphonic.util import mp_grid  # noqa: F401
+
+        assert euphonic_available()
+
         SimulatedDensityOfStates(
             ForceConstantsFile=find_file("phonopy-Al.yaml"), Function="Gaussian", SpectrumType="DOS", OutputWorkspace="phonopy-Al_DOS"
         )
