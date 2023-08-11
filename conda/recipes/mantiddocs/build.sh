ba1113eca671bd98b503da2e548579f9072afad6
Robert <robert.applin@stfc.ac.uk>
2023-08-11 14:23:09 +0100

    Configure the datasearch directories before building docs-qthelp

--- conda/recipes/mantiddocs/build.sh
+++ conda/recipes/mantiddocs/build.sh
@@ -58,7 +58,14 @@ cmake \
   ../
 
 cmake --build .
+
+# Build the StandardTestData target. We need this test data to build docs-qthelp
 cmake --build . --target StandardTestData
+
+# Configure the 'datasearch.directories' in the Mantid.properties file so the test data is found
+export STANDARD_TEST_DATA_DIR=$SRC_DIR/build/ExternalData/Testing/Data
+echo 'datasearch.directories = '$STANDARD_TEST_DATA_DIR'/UnitTest/;'$STANDARD_TEST_DATA_DIR'/DocTest/' >> $PREFIX/bin/Mantid.properties
+
 run_with_xvfb cmake --build . --target docs-qthelp
 terminate_xvfb_sessions
 cmake --build . --target install
