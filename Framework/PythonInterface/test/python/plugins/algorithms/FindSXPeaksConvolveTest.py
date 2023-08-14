f2283a11d86bd0e15b294ee1d8fcd570d00f2d08
Richard Waite <richard.waite@stfc.ac.uk>
2023-08-14 13:34:45 +0100

    Fix bugs in new tests

--- Framework/PythonInterface/test/python/plugins/algorithms/FindSXPeaksConvolveTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/FindSXPeaksConvolveTest.py
@@ -23,10 +23,10 @@ class FindSXPeaksConvolveTest(unittest.TestCase):
         AnalysisDataService.addOrReplace("ws_rect", cls.ws)
         axis = cls.ws.getAxis(0)
         axis.setUnit("TOF")
-        # fake peak centred on ispec=24 (detid=79) and TOF=5 - near middle of bank
-        peak_1D = array([0, 0, 0, 1, 4, 6, 4, 1, 0, 0, 0, 0, 0])
-        cls.ws.setY(24, cls.ws.readY(12) + peak_1D)
-        for ispec in [17, 23, 24, 25, 31]:
+        # fake peak centred on ispec=30 (detid=79) and TOF=5 - near middle of bank
+        peak_1D = array([0, 0, 0, 0, 4, 6, 4, 0, 0, 0, 0, 0, 0])
+        cls.ws.setY(30, cls.ws.readY(30) + peak_1D)
+        for ispec in [23, 29, 30, 31, 37]:
             cls.ws.setY(ispec, cls.ws.readY(ispec) + peak_1D)
             cls.ws.setE(ispec, sqrt(cls.ws.readY(ispec)))
         # fake peak centred on ispec=12 (detid=61) and TOF=7 - near detector edge
@@ -46,7 +46,7 @@ class FindSXPeaksConvolveTest(unittest.TestCase):
         <formula eq="2" unit="TOF" result-unit="1/TOF" /> <fixed />
         </parameter>
         <parameter name="BackToBackExponential:S" type="fitting">
-        <formula eq="2" unit="TOF" result-unit="TOF" />
+        <formula eq="0.1" unit="TOF" result-unit="TOF" />
         </parameter>
         </component-link>
         </parameter-file>
@@ -64,18 +64,18 @@ class FindSXPeaksConvolveTest(unittest.TestCase):
         )
         pk = peak_ws.getPeak(0)
         self.assertEqual(pk.getDetectorID(), 79)
-        self.assertEqual(pk.getTOF(), 5.0, delta=1e-8)
+        self.assertAlmostEqual(pk.getTOF(), 5.0, delta=1e-8)
         self.assertAlmostEqual(pk.getIntensityOverSigma(), 6.0622, delta=1e-4)
         pk = peak_ws.getPeak(1)
         self.assertEqual(pk.getDetectorID(), 61)
-        self.assertEqual(pk.getTOF(), 7.0, delta=1e-8)
+        self.assertAlmostEqual(pk.getTOF(), 7.0, delta=1e-8)
         self.assertAlmostEqual(pk.getIntensityOverSigma(), 5.3981, delta=1e-4)
 
     def test_exec_specify_nbins(self):
         out = FindSXPeaksConvolve(
-            InputWorkspace=self.ws, PeaksWorkspace="peaks1", NRows=3, NCols=3, NBins=5, ThresholdIoverSigma=3.0, MinFracSize=0.02
+            InputWorkspace=self.ws, PeaksWorkspace="peaks1", NRows=3, NCols=3, NBins=3, ThresholdIoverSigma=3.0, MinFracSize=0.02
         )
-        self._assert_found_correct_peak(out)
+        self._assert_found_correct_peaks(out)
 
     def test_exec_get_nbins_from_back_to_back_params(self):
         out = FindSXPeaksConvolve(
@@ -87,10 +87,10 @@ class FindSXPeaksConvolveTest(unittest.TestCase):
             ThresholdIoverSigma=3.0,
             MinFracSize=0.02,
         )
-        self._assert_found_correct_peak(out)
+        self._assert_found_correct_peaks(out)
 
     def test_exec_IoverSigma_threshold(self):
-        out = FindSXPeaksConvolve(InputWorkspace=self.ws, PeaksWorkspace="peaks3", NRows=3, NCols=3, NBins=5, ThresholdIoverSigma=100.0)
+        out = FindSXPeaksConvolve(InputWorkspace=self.ws, PeaksWorkspace="peaks3", NRows=3, NCols=3, NBins=3, ThresholdIoverSigma=100.0)
         self.assertEqual(out.getNumberPeaks(), 0)
 
     def test_exec_remove_on_edge(self):
@@ -99,7 +99,7 @@ class FindSXPeaksConvolveTest(unittest.TestCase):
             PeaksWorkspace="peaks1",
             NRows=3,
             NCols=3,
-            NBins=5,
+            NBins=3,
             ThresholdIoverSigma=3.0,
             MinFracSize=0.02,
             RemoveOnEdge=True,
