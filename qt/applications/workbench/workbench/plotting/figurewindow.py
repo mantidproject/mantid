06fbde29c4cdf3c06116236d020027e2bfed6e9d
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2023-08-16 09:38:50 +0100

    add validator to check workspace has multiple bins

--- qt/applications/workbench/workbench/plotting/figurewindow.py
+++ qt/applications/workbench/workbench/plotting/figurewindow.py
@@ -9,6 +9,7 @@
 #
 """Provides the QMainWindow subclass for a plotting window"""
 # std imports
+from typing import List
 import weakref
 
 # 3rdparty imports
@@ -19,6 +20,34 @@ from qtpy.QtWidgets import QMainWindow
 # local imports
 from mantidqt.plotting.figuretype import FigureType, figure_type
 from mantidqt.widgets.observers.observing_view import ObservingView
+import mantid.kernel
+
+
+def _validate_workspaces(names: List[str]) -> List[bool]:
+    """
+    Checks that workspaces being over plot have multiple bins
+    :param names: A list of workspace names
+    :return: List of bools, True if all workspaces have multiple bins
+    """
+    from mantid.api import AnalysisDataServiceImpl
+
+    ads = AnalysisDataServiceImpl.Instance()
+    results = []
+    for name in names:
+        result = False
+        ws = ads.retrieve(name)
+        try:
+            result = ws.blocksize() > 1
+        except RuntimeError:
+            # blocksize() implementation in Workspace2D and EventWorkspace can through an error if histograms are not equal
+            for i in ws.getNumberHistograms():
+                if ws.y(i).size() > 1:
+                    result = True
+                    break
+        finally:
+            results.append(result)
+
+    return results
 
 
 class FigureWindow(QMainWindow, ObservingView):
@@ -123,6 +152,9 @@ class FigureWindow(QMainWindow, ObservingView):
         :param ax: The matplotlib axes object to overplot onto
         """
         if len(names) == 0:
+            return
+        if not all(_validate_workspaces(names)):
+            mantid.kernel.logger.warning("To overplot, workspace(s) must have multiple bins")
             return
         # local import to avoid circular import with FigureManager
         from mantidqt.plotting.functions import pcolormesh, plot_from_names, plot_surface, plot_wireframe, plot_contour
