ff2fc33c1884880989c9021bedecc14163cdb1e6
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2023-08-25 10:51:50 +0100

    review suggestions

--- qt/widgets/common/src/AlgorithmPropertiesWidget.cpp
+++ qt/widgets/common/src/AlgorithmPropertiesWidget.cpp
@@ -273,16 +273,10 @@ void AlgorithmPropertiesWidget::propertyChanged(const QString &changedPropName) 
   this->hideOrDisableProperties(changedPropName);
 }
 
-bool isCalledInputWorkspace(PropertyWidget *const candidate) {
+bool isCalledInputWorkspaceOrLHSWorkspace(PropertyWidget *const candidate) {
   Mantid::Kernel::Property const *const property = candidate->getProperty();
   const std::string &propertyName = property->name();
-  return propertyName == "InputWorkspace";
-}
-
-bool isCalledLHSWorkspace(PropertyWidget *const candidate) {
-  Mantid::Kernel::Property const *const property = candidate->getProperty();
-  const std::string &propertyName = property->name();
-  return propertyName == "LHSWorkspace";
+  return propertyName == "InputWorkspace" || propertyName == "LHSWorkspace";
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -318,9 +312,8 @@ void AlgorithmPropertiesWidget::replaceWSClicked(const QString &propName) {
       // Choose from candidates, only do this if there are candidates to select
       // from.
       if (candidateReplacementSources.size() > 0) {
-        CollectionOfPropertyWidget::iterator selectedIt =
-            std::find_if(candidateReplacementSources.begin(), candidateReplacementSources.end(),
-                         [](const auto &prop) { return isCalledInputWorkspace(prop) || isCalledLHSWorkspace(prop); });
+        const auto selectedIt = std::find_if(candidateReplacementSources.cbegin(), candidateReplacementSources.cend(),
+                                             isCalledInputWorkspaceOrLHSWorkspace);
         if (selectedIt != candidateReplacementSources.end()) {
           // Use the InputWorkspace property called "InputWorkspace" or "LHSWorkspace" as the
           // source for the OutputWorkspace.
