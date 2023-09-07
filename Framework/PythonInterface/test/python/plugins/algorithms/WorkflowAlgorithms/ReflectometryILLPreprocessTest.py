dbd560df940d76d79cc3446e2ea59b1efa2cd1c4
Tom Hampson <thomas.hampson@stfc.ac.uk>
2023-09-07 12:43:59 +0100

    add missing numpy import

--- Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ReflectometryILLPreprocessTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/WorkflowAlgorithms/ReflectometryILLPreprocessTest.py
@@ -4,14 +4,14 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
+import math
+import numpy
+import unittest
 
 from mantid.kernel import config
 from mantid.api import mtd
-from mantid.simpleapi import ReflectometryILLPreprocess
 from testhelpers import assertRaisesNothing, create_algorithm, illhelpers
-import unittest
 import ReflectometryILL_common as common
-import math
 
 
 class ReflectometryILLPreprocessTest(unittest.TestCase):
