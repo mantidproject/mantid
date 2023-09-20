a6f45371be0aa6851f442ed35cb16e0017edac1f
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2023-09-20 14:50:17 +0100

    add failing Interface documentation test

--- Testing/SystemTests/tests/framework/DocumentationTest.py
+++ Testing/SystemTests/tests/framework/DocumentationTest.py
@@ -5,19 +5,20 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 
-from itertools import chain
 import pathlib
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 from systemtesting import MantidSystemTest
 from mantid.api import AlgorithmFactory, FunctionFactory
 from mantid.kernel import ConfigService
 from workbench.utils.gather_interfaces import gather_interface_names
+from mantidqt.interfacemanager import InterfaceManager
 
 path_to_docs = (pathlib.Path(__file__).resolve() / "../../../../../docs/source").resolve()
 ALG_DOCS_PATH = path_to_docs / "algorithms"
 FIT_DOCS_PATH = path_to_docs / "fitting/fitfunctions"
+INTERFACES_DOCS_PATH = path_to_docs / "interfaces"
 
 # This should be empty
 ALGORITHMS_WITH_NO_DOCS = [
@@ -59,6 +60,8 @@ FIT_FUNCTIONS_WITH_NO_DOCS = [
     "SCgapSwave.rst",
     "VesuvioResolution.rst",
 ]
+# This should be empty
+INTERFACES_WITH_NO_DOCS = []
 
 
 def _file_exists_under_dir(dir_path: Path, file_name: str) -> bool:
@@ -72,11 +75,13 @@ def _file_exists_under_dir(dir_path: Path, file_name: str) -> bool:
     return len(found_files) > 0
 
 
-def _get_interface_names() -> List[str]:
+def _get_interface_names() -> Dict[str, List[str]]:
+    # need to initialise this so cpp interfaces are registered
+    InterfaceManager()
     interfaces = gather_interface_names()
     hidden_interfaces = ConfigService["interfaces.categories.hidden"].split(";")
-    interface_names = [interfaces[key] for key in interfaces.keys() if key not in hidden_interfaces]
-    return list(chain.from_iterable(interface_names))
+    interface_names = {key: names for key, names in interfaces.items() if key not in hidden_interfaces}
+    return interface_names
 
 
 class AlgorithmDocumentationTest(MantidSystemTest):
@@ -94,7 +99,7 @@ class AlgorithmDocumentationTest(MantidSystemTest):
                 file_name = f"{name}-v{version}.rst"
                 if not _file_exists_under_dir(ALG_DOCS_PATH, file_name):
                     if file_name not in ALGORITHMS_WITH_NO_DOCS:
-                        missing.append(str(file_name))
+                        missing.append(file_name)
                 elif file_name in ALGORITHMS_WITH_NO_DOCS:
                     raise FileExistsError(f"{file_name} exists but is still in the exceptions list. Please update the list.")
 
@@ -115,7 +120,7 @@ class FittingDocumentationTest(MantidSystemTest):
             file_name = f"{name}.rst"
             if not _file_exists_under_dir(FIT_DOCS_PATH, file_name):
                 if file_name not in FIT_FUNCTIONS_WITH_NO_DOCS:
-                    missing.append(str(file_name))
+                    missing.append(file_name)
             elif file_name in FIT_FUNCTIONS_WITH_NO_DOCS:
                 raise FileExistsError(f"{file_name} exists but is still in the exceptions list. Please update the list.")
 
@@ -128,5 +133,23 @@ class InterfaceDocumentationTest(MantidSystemTest):
     """
 
     def runTest(self):
-        interface_names = _get_interface_names()
-        print(interface_names)
+        interfaces = _get_interface_names()
+        print(interfaces)
+        self.assertGreaterThan(len(interfaces), 0, msg="No interfaces found")
+        missing = []
+
+        for category, interfaces in interfaces.items():
+            for interface_name in interfaces:
+                file_names = []
+                if interface_name.endswith(".py"):
+                    interface_name = interface_name.replace(".py", "").replace("_", " ")
+                else:
+                    file_names.append(f"{category} {interface_name}.rst")
+                file_names.append(f"{interface_name}.rst")
+                if not any(_file_exists_under_dir(INTERFACES_DOCS_PATH, file_name) for file_name in file_names):
+                    if all(file_name not in INTERFACES_WITH_NO_DOCS for file_name in file_names):
+                        missing.append(file_names)
+                elif any(file_name in INTERFACES_WITH_NO_DOCS for file_name in file_names):
+                    raise FileExistsError(f"{' or '.join(file_names)} exists but is still in the exceptions list. Please update the list.")
+
+        self.assertEqual(len(missing), 0, msg="Missing the following interface docs:\n" + "\n".join("/".join(names) for names in missing))
