cd292f798493f9943025ecce2810c8a320c2415f
James Clarke <james.clarke@stfc.ac.uk>
2023-10-09 09:57:59 +0100

    Add extra test case with more than three params.

--- Framework/Algorithms/test/RebinTest.h
+++ Framework/Algorithms/test/RebinTest.h
@@ -93,6 +93,7 @@ public:
     // this fails fast, due to the rebin param property validator
     Rebin rebin;
     rebin.initialize();
+    TS_ASSERT_THROWS_ANYTHING(rebin.setProperty("Params", std::vector<double>{0, 2, 4, 3, 5}));
     TS_ASSERT_THROWS_ANYTHING(rebin.setProperty("Params", std::vector<double>{0, 2, 1}));
     TS_ASSERT_THROWS_ANYTHING(rebin.setProperty("Params", std::vector<double>{-1.0, 2, 0}));
     TS_ASSERT_THROWS_ANYTHING(rebin.setProperty("Params", std::vector<double>{-1.0, -1.0, 10.0}));
