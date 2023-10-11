30fca04e078f0f163ec3e78bb502518bd3c158c7
MialLewis <95620982+MialLewis@users.noreply.github.com>
2023-10-11 17:32:36 +0100

    change test name and add test for alg finished signal

--- qt/widgets/common/test/FitPropertyBrowserTest.h
+++ qt/widgets/common/test/FitPropertyBrowserTest.h
@@ -205,15 +205,19 @@ public:
     TS_ASSERT_EQUALS(height, 12);
   }
 
-  void test_fit_error_handled_and_failed_signal_emmitted() {
-    QSignalSpy algFailedSpy(m_fitPropertyBrowser.get(), &MantidQt::MantidWidgets::FitPropertyBrowser::algorithmFailed);
+  void test_alg_failed_signal_emmitted_upon_exception() {
+    QSignalSpy algFailedSpy_failed(m_fitPropertyBrowser.get(),
+                                   &MantidQt::MantidWidgets::FitPropertyBrowser::algorithmFailed);
+    QSignalSpy algFailedSpy_finished(m_fitPropertyBrowser.get(),
+                                     &MantidQt::MantidWidgets::FitPropertyBrowser::algorithmFinished);
     auto ws = Mantid::API::WorkspaceFactory::Instance().create("Workspace2D", 5, 5, 5);
     Mantid::API::AnalysisDataService::Instance().addOrReplace("test_ws_name", ws);
     m_fitPropertyBrowser->init();
     m_fitPropertyBrowser->createCompositeFunction("name=UserFunction;");
     m_fitPropertyBrowser->setWorkspaceName("test_ws_name");
     m_fitPropertyBrowser->fit();
-    TS_ASSERT_EQUALS(algFailedSpy.count(), 1);
+    TS_ASSERT_EQUALS(algFailedSpy_failed.count(), 1);
+    TS_ASSERT_EQUALS(algFailedSpy_finished.count(), 0);
   }
 
 private:
