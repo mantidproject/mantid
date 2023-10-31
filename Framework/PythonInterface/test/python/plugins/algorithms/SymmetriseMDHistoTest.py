6edc32fb2692b94fae86f0b88d0bfeca6e389731
Richard Waite <richard.waite@stfc.ac.uk>
2023-10-31 09:58:03 +0000

    Add unit test for odd nbin (i.e. bins centers at 0)

--- Framework/PythonInterface/test/python/plugins/algorithms/SymmetriseMDHistoTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/SymmetriseMDHistoTest.py
@@ -15,8 +15,8 @@ class SymmeteriseMDHistoTest(unittest.TestCase):
         ADS.clear()
 
     def test_even_nbins_unweighted_avg_full_signal(self):
-        signal = np.ones(8)
-        ws = self._make_md_ws(np.ones(8), "ws1")
+        signal = np.ones(8)  # 2 x 2 x 2
+        ws = self._make_md_ws(signal, "ws1")
 
         ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=False, OutputWorkspace=ws.name() + "_sym")
 
@@ -25,7 +25,7 @@ class SymmeteriseMDHistoTest(unittest.TestCase):
 
     def test_even_nbins_weighted_avg_full_signal(self):
         signal = np.ones(8)
-        ws = self._make_md_ws(np.ones(8), "ws2")
+        ws = self._make_md_ws(signal, "ws2")
 
         ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=True, OutputWorkspace=ws.name() + "_sym")
 
@@ -47,7 +47,51 @@ class SymmeteriseMDHistoTest(unittest.TestCase):
     def test_even_nbins_weighted_avg_partial_signal(self):
         signal = np.zeros(8)
         signal[0] = 1.0
-        ws = self._make_md_ws(signal, "ws4")
+        ws = self._make_md_ws(signal, "ws5")
+
+        ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=True, OutputWorkspace=ws.name() + "_sym")
+
+        expected_signal = signal.copy()
+        expected_signal[-1] = 1.0
+        self.assertTrue(np.allclose(expected_signal, ws_sym.getSignalArray().flatten()))
+        self.assertTrue(np.allclose(expected_signal, ws_sym.getErrorSquaredArray().flatten()))  # 1 contributing bin
+
+    def test_odd_nbins_unweighted_avg_full_signal(self):
+        signal = np.ones(27)  # 3 x 3 x 3
+        ws = self._make_md_ws(signal, "ws6")
+
+        ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=False, OutputWorkspace=ws.name() + "_sym")
+
+        self.assertTrue(np.allclose(signal, ws_sym.getSignalArray().flatten()))
+        self.assertTrue(np.allclose(signal, ws_sym.getErrorSquaredArray().flatten()))
+
+    def test_odd_nbins_weighted_avg_full_signal(self):
+        signal = np.ones(27)
+        ws = self._make_md_ws(signal, "ws7")
+
+        ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=True, OutputWorkspace=ws.name() + "_sym")
+
+        self.assertTrue(np.allclose(signal, ws_sym.getSignalArray().flatten()))
+        expected_error_sq = 0.5 * signal
+        expected_error_sq[len(signal) // 2] = 1  # inversion center at origin not double counted
+        self.assertTrue(np.allclose(expected_error_sq, ws_sym.getErrorSquaredArray().flatten()))
+
+    def test_odd_nbins_unweighted_avg_partial_signal(self):
+        signal = np.zeros(27)
+        signal[0] = 1.0
+        ws = self._make_md_ws(signal, "ws8")
+
+        ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=False, OutputWorkspace=ws.name() + "_sym")
+
+        expected_signal = signal.copy()
+        expected_signal[-1] = 1.0
+        self.assertTrue(np.allclose(expected_signal, ws_sym.getSignalArray().flatten()))
+        self.assertTrue(np.allclose(expected_signal, ws_sym.getErrorSquaredArray().flatten()))  # 1 contributing bin
+
+    def test_odd_nbins_weighted_avg_partial_signal(self):
+        signal = np.zeros(27)
+        signal[0] = 1.0
+        ws = self._make_md_ws(signal, "ws9")
 
         ws_sym = SymmetriseMDHisto(ws, PointGroup="-1", WeightedAverage=True, OutputWorkspace=ws.name() + "_sym")
 
@@ -64,7 +108,7 @@ class SymmeteriseMDHistoTest(unittest.TestCase):
             ErrorInput=signal,
             Dimensionality=3,
             Extents="-1,1,-1,1,-1,1",
-            NumberOfBins="2,2,2",
+            NumberOfBins=3 * [nbins],
             Names="H,K,L",
             Units="rlu,rlu,rlu",
             OutputWorkspace=wsname,
