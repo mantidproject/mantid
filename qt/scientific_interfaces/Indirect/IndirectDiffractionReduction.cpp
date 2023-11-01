2ab1a97ee020df976b305e82c10b0a4dae558f0e
Applin <robert.applin@stfc.ac.uk>
2023-11-01 17:27:14 +0000

    Set ComponentName as 'bank' only if the component is found

--- qt/scientific_interfaces/Indirect/IndirectDiffractionReduction.cpp
+++ qt/scientific_interfaces/Indirect/IndirectDiffractionReduction.cpp
@@ -35,7 +35,7 @@ using MantidQt::API::BatchAlgorithmRunner;
 
 IndirectDiffractionReduction::IndirectDiffractionReduction(QWidget *parent)
     : IndirectInterface(parent), m_valDbl(nullptr), m_settingsGroup("CustomInterfaces/DEMON"),
-      m_batchAlgoRunner(new BatchAlgorithmRunner(parent)) {}
+      m_batchAlgoRunner(new BatchAlgorithmRunner(parent)), m_groupingComponent() {}
 
 IndirectDiffractionReduction::~IndirectDiffractionReduction() { saveSettings(); }
 
@@ -477,14 +477,12 @@ void IndirectDiffractionReduction::createGroupingWorkspace(const std::string &ou
   auto instrumentConfig = m_uiForm.iicInstrumentConfiguration;
   auto const numberOfGroups = m_uiForm.spNumberGroups->value();
   auto const instrument = instrumentConfig->getInstrumentName().toStdString();
-  auto const analyser = instrumentConfig->getAnalyserName().toStdString();
-  auto const componentName = analyser == "diffraction" ? "bank" : analyser;
 
   auto groupingAlg = AlgorithmManager::Instance().create("CreateGroupingWorkspace");
   groupingAlg->initialize();
   groupingAlg->setProperty("FixedGroupCount", numberOfGroups);
   groupingAlg->setProperty("InstrumentName", instrument);
-  groupingAlg->setProperty("ComponentName", componentName);
+  groupingAlg->setProperty("ComponentName", m_groupingComponent);
   groupingAlg->setProperty("OutputWorkspace", outputWsName);
 
   m_batchAlgoRunner->addAlgorithm(groupingAlg);
@@ -557,6 +555,9 @@ void IndirectDiffractionReduction::instrumentSelected(const QString &instrumentN
 
   MatrixWorkspace_sptr instWorkspace = loadInstrument(instrumentName.toStdString(), reflectionName.toStdString());
   Instrument_const_sptr instrument = instWorkspace->getInstrument();
+
+  auto const bankComponent = instrument->getComponentByName("bank");
+  m_groupingComponent = bankComponent ? "bank" : "diffraction";
 
   // Get default spectra range
   double specMin = instrument->getNumberParameter("spectra-min")[0];
