4feae2a8d39cd17c3e23e3933a188e7e8b0f6fb5
Robert Bolotovsky <bolotovskyr@ornl.gov>
2023-11-16 22:47:01 -0500

    Add a system test.

--- Framework/DataObjects/src/TimeSplitter.cpp
+++ Framework/DataObjects/src/TimeSplitter.cpp
@@ -204,8 +204,8 @@ std::string TimeSplitter::getWorkspaceIndexName(const int workspaceIndex, const 
 
   std::string target_name = m_index_name_map[workspaceIndex];
 
-  // If numericalShift > 0, the caller will get back a shifted index.
-  // This is needed for supporting FilterEvents property OutputWorkspaceIndexedFrom1.
+  // If numericalShift is not zero, the "_index" suffix of the name will be shifted.
+  // This is needed to support FilterEvents property OutputWorkspaceIndexedFrom1.
   if (numericalShift != 0) {
     // If this TimeSplitter was built from a TableWorkspace, targets could be non-numeric, in which case a numeric
     // shift wouldn't make sense.
