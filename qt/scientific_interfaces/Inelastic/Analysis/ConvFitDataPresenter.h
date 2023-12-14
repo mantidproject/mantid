eb2d6af446fe8eba6b372ac69ef7c2cd2eb56047
Applin <robert.applin@stfc.ac.uk>
2023-12-14 14:33:34 +0000

    ConvFitDataPresenter no longer a qobject

--- qt/scientific_interfaces/Inelastic/Analysis/ConvFitDataPresenter.h
+++ qt/scientific_interfaces/Inelastic/Analysis/ConvFitDataPresenter.h
@@ -16,14 +16,10 @@ namespace IDA {
 
 class ConvFitAddWorkspaceDialog;
 class MANTIDQT_INELASTIC_DLL ConvFitDataPresenter : public IndirectFitDataPresenter {
-  Q_OBJECT
 public:
   ConvFitDataPresenter(IIndirectFitDataModel *model, IIndirectFitDataView *view);
 
   bool addWorkspaceFromDialog(IAddWorkspaceDialog const *dialog) override;
-
-signals:
-  void modelResolutionAdded(std::string const &name, WorkspaceID const &workspaceID);
 
 protected:
   void addTableEntry(FitDomainIndex row) override;
