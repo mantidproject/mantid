85c7812798ed4f4cc31c7d818cd5ac5dbcd3e062
Mohammed Almakki <mohammed.almakki@stfc.ac.uk>
2024-01-15 12:29:56 +0000

    Expose multiple lattices to the python interface

--- Framework/PythonInterface/mantid/api/src/Exports/Sample.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/Sample.cpp
@@ -35,7 +35,10 @@ void export_Sample() {
       .def("getName", &Sample::getName, return_value_policy<copy_const_reference>(), arg("self"),
            "Returns the string name of the sample")
       .def("getOrientedLattice", (const OrientedLattice &(Sample::*)() const) & Sample::getOrientedLattice, arg("self"),
-           return_value_policy<reference_existing_object>(), "Get the oriented lattice for this sample")
+           return_value_policy<reference_existing_object>(), "Get the first oriented lattice for this sample")
+      .def("getOrientedLattices",
+           (const std::vector<OrientedLattice> &(Sample::*)() const) & Sample::getOrientedLattices,
+           return_value_policy<reference_existing_object>(), "Get the oriented lattices for this sample")
       .def("hasOrientedLattice", &Sample::hasOrientedLattice, arg("self"),
            "Returns True if this sample has an oriented lattice, false "
            "otherwise")
