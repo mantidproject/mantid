ce2bc678fbec708d01ce6008721af227f3ed1f68
zjmorgan <morganzj@ornl.gov>
2024-01-17 20:51:48 -0500

    Writing sgl/sgu test

--- Testing/SystemTests/tests/framework/LoadWANDSCDTest.py
+++ Testing/SystemTests/tests/framework/LoadWANDSCDTest.py
@@ -6,7 +6,7 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 import systemtesting
 import numpy as np
-from mantid.simpleapi import AddSampleLog, LoadWANDSCD
+from mantid.simpleapi import AddSampleLog, SetGoniometer, LoadWANDSCD
 
 
 class LoadWANDSCDTest(systemtesting.MantidSystemTest):
@@ -34,10 +34,25 @@ class LoadWANDSCDTest(systemtesting.MantidSystemTest):
         self.assertAlmostEqual(LoadWANDTest_ws.getSignalArray().max(), ref_val, 5)
         ref_err = 0.0157660
         self.assertAlmostEqual(LoadWANDTest_ws.getErrorSquaredArray().max(), ref_err, 5)
+        LoadWANDTest_ws = LoadWANDSCD(Filename="HB2C_7000.nxs.h5,HB2C_7001.nxs.h5", ApplyGoniometerTilt=True)
+        LoadWANDGonTest_ws = LoadWANDSCD(Filename="HB2C_7000.nxs.h5,HB2C_7001.nxs.h5", ApplyGoniometerTilt=False)
+        SetGoniometer(
+            LoadWANDGonTest_ws, Axis0="HB2C:Mot:s1,0,1,0,1", Axis1="HB2C:Mot:sgl,1,0,0,-1", Axis2="HB2C:Mot:sgu,0,0,1,-1", Average=False
+        )
+        UB = LoadWANDTest_ws.getExperimentInfo(0).sample().getOrientedLattice().getUB().copy()
+        UBGon = LoadWANDGonTest_ws.getExperimentInfo(0).sample().getOrientedLattice().getUB().copy()
+        # NOTE: the outer to innermost is s1, sgl, sgu
         np.testing.assert_allclose(
-            LoadWANDTest_ws.getExperimentInfo(0).run().getProperty("s1").value,
-            LoadWANDTest_ws.getExperimentInfo(0).run().getProperty("HB2C:Mot:s1").value,
+            LoadWANDTest_ws.getExperimentInfo(0).run().getGoniometer(0).getR() @ UB,
+            LoadWANDGonTest_ws.getExperimentInfo(0).run().getGoniometer(0).getR() @ UBGon,
+            3,
+        )
+        np.testing.assert_allclose(
+            LoadWANDTest_ws.getExperimentInfo(0).run().getGoniometer(1).getR() @ UB,
+            LoadWANDGonTest_ws.getExperimentInfo(0).run().getGoniometer(1).getR() @ UBGon,
+            3,
         )
 
         LoadWANDTest_ws.delete()
+        LoadWANDGonTest_ws.delete()
         van.delete()
