8d055ff310911e5fb3e57c1a5af1a32ab22f50cc
James Clarke <james.clarke@stfc.ac.uk>
2024-01-29 16:28:32 +0000

    Handle group workspaces when valdating parameters
    If the workspace selected in the dialog is a group workspace, then calling
    getProperty() with MatrixWorkspace as the return type will return null. In that case
    we can check if the specified workspace name exists in the ADS and skip the Rebin
    params validation (which requires a MatrixWorkspace). When Rebin is actually called,
    it will be called on each of the members of the group workspace individually, and
    the parameters will be validated for each one.

--- Framework/Algorithms/src/Rebin.cpp
+++ Framework/Algorithms/src/Rebin.cpp
@@ -8,6 +8,7 @@
 #include "MantidHistogramData/Exception.h"
 #include "MantidHistogramData/Rebin.h"
 
+#include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/HistoWorkspace.h"
 #include "MantidDataObjects/EventList.h"
 #include "MantidDataObjects/EventWorkspace.h"
@@ -135,15 +136,25 @@ std::map<std::string, std::string> Rebin::validateInputs() {
   // validate the rebin params, and outside default mode, reset them
   MatrixWorkspace_sptr inputWS = getProperty(PropertyNames::INPUT_WKSP);
   std::vector<double> rbParams = getProperty(PropertyNames::PARAMS);
-  try {
-    std::vector<double> validParams = rebinParamsFromInput(rbParams, *inputWS, g_log, binMode);
-    // if the binmode has been set, force the rebin params to be consistent
-    if (binMode != BinningMode::DEFAULT) {
-      setProperty(PropertyNames::PARAMS, validParams);
-      rbParams = validParams;
+  if (inputWS == nullptr) {
+    // The workspace could exist, but not be a MatrixWorkspace, e.g. it might be
+    // a group workspace. In that case we don't want a validation error for the
+    // Rebin parameters.
+    std::string inputWsName = getProperty(PropertyNames::INPUT_WKSP);
+    auto &ws = AnalysisDataService::Instance().retrieve(inputWsName);
+    if (ws == nullptr)
+      helpMessages[PropertyNames::INPUT_WKSP] = "Input workspace not in ADS.";
+  } else {
+    try {
+      std::vector<double> validParams = rebinParamsFromInput(rbParams, *inputWS, g_log, binMode);
+      // if the binmode has been set, force the rebin params to be consistent
+      if (binMode != BinningMode::DEFAULT) {
+        setProperty(PropertyNames::PARAMS, validParams);
+        rbParams = validParams;
+      }
+    } catch (std::exception &err) {
+      helpMessages[PropertyNames::PARAMS] = err.what();
     }
-  } catch (std::exception &err) {
-    helpMessages[PropertyNames::PARAMS] = err.what();
   }
 
   // if user specifies a binning mode, set this flag for them
