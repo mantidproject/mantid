1fbdb93d3bebf62e0acf4e0615932b92a86293d0
James Clarke <james.clarke@stfc.ac.uk>
2024-01-30 11:38:55 +0000

    Use doesExist() instead of retrieve()
    If you try and retrieve() a workspace that's not in the ADS, you'll get an
    exception, which is not what we want, we just want to check if it's there
    or not.

--- Framework/Algorithms/src/Rebin.cpp
+++ Framework/Algorithms/src/Rebin.cpp
@@ -141,8 +141,8 @@ std::map<std::string, std::string> Rebin::validateInputs() {
     // a group workspace. In that case we don't want a validation error for the
     // Rebin parameters.
     std::string inputWsName = getProperty(PropertyNames::INPUT_WKSP);
-    const auto &ws = AnalysisDataService::Instance().retrieve(inputWsName);
-    if (ws == nullptr)
+    const bool wsExists = AnalysisDataService::Instance().doesExist(inputWsName);
+    if (!wsExists)
       helpMessages[PropertyNames::INPUT_WKSP] = "Input workspace not in ADS.";
   } else {
     try {
