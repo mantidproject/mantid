a92c2b422d764e1e8437ec72ac59add86856f442
Adam J. Jackson <a.j.jackson@physics.org>
2024-02-14 13:39:05 +0000

    More Abins spectra->workspaces tidy-up

--- scripts/abins/abins2.py
+++ scripts/abins/abins2.py
@@ -5,6 +5,7 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 
+from operator import itemgetter
 from typing import Dict, Optional
 
 import numpy as np
@@ -132,6 +133,9 @@ class Abins(AbinsAlgorithm, PythonAlgorithm):
         s_calculator.progress_reporter = prog_reporter
         s_data = s_calculator.get_formatted_data()
 
+        atoms_data = ab_initio_data.get_atoms_data()
+        spectra = s_data.get_spectrum_collection(symbols=map(itemgetter("symbol"), atoms_data), masses=map(itemgetter("mass"), atoms_data))
+
         # Hold reporter at 80% for this message
         prog_reporter.resetNumSteps(1, 0.8, 0.80000001)
         prog_reporter.report("Dynamical structure factors have been determined.")
@@ -140,7 +144,6 @@ class Abins(AbinsAlgorithm, PythonAlgorithm):
         prog_reporter.resetNumSteps(n_messages, 0.8, 1)
 
         # 4) get atoms for which S should be plotted
-        atoms_data = ab_initio_data.get_atoms_data()
         atom_numbers, atom_symbols = self.get_atom_selection(atoms_data=atoms_data, selection=self._atoms)
         prog_reporter.report("Atoms, for which dynamical structure factors should be plotted, have been determined.")
 
@@ -149,11 +152,13 @@ class Abins(AbinsAlgorithm, PythonAlgorithm):
 
         workspaces.extend(
             self.create_workspaces(
-                atoms_symbols=atom_symbols, s_data=s_data, atoms_data=atoms_data, max_quantum_order=self._max_event_order
+                atoms_symbols=atom_symbols, spectra=spectra, max_quantum_order=self._max_event_order, atoms_data=atoms_data
             )
         )
         workspaces.extend(
-            self.create_workspaces(atom_numbers=atom_numbers, s_data=s_data, atoms_data=atoms_data, max_quantum_order=self._max_event_order)
+            self.create_workspaces(
+                atom_numbers=atom_numbers, spectra=spectra, max_quantum_order=self._max_event_order, atoms_data=atoms_data
+            )
         )
         prog_reporter.report("Workspaces with partial dynamical structure factors have been constructed.")
 
