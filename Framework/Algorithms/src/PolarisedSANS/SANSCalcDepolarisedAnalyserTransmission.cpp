737d067ece2c8f14e054002663e5d1d3a0ec3495
Conor Finn <conor.finn@stfc.ac.uk>
2024-02-15 14:36:08 +0000

    Add initial algorithm properties
    RE #36383

--- Framework/Algorithms/src/PolarisedSANS/SANSCalcDepolarisedAnalyserTransmission.cpp
+++ Framework/Algorithms/src/PolarisedSANS/SANSCalcDepolarisedAnalyserTransmission.cpp
@@ -5,16 +5,30 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 #include "MantidAlgorithms/PolarisedSANS/SANSCalcDepolarisedAnalyserTransmission.h"
-
-namespace Mantid::Algorithms {
+#include "MantidAPI/ADSValidator.h"
+#include "MantidAPI/WorkspaceGroup.h"
 
 namespace {
+/// Property Names
+namespace Prop {
+std::string_view constexpr DEP_WORKSPACE{"DepolarisedWorkspace"};
+std::string_view constexpr MT_WORKSPACE{"EmptyCellWorkspace"};
+std::string_view constexpr T_E_START{"T_EStartingValue"};
+std::string_view constexpr PXD_START{"PxDStartingValue"};
+} // namespace Prop
+
+/// Initial fitting function values.
 namespace FitStartValues {
 double constexpr T_E = 0.9;
 double constexpr PXD = 12.6;
 } // namespace FitStartValues
+
 double constexpr LAMBDA_CONVERSION_FACTOR = 0.0733;
 } // namespace
+
+namespace Mantid::Algorithms {
+
+using namespace API;
 
 // Register the algorithm in the AlgorithmFactory
 DECLARE_ALGORITHM(SANSCalcDepolarisedAnalyserTransmission)
@@ -23,7 +37,14 @@ std::string const SANSCalcDepolarisedAnalyserTransmission::summary() const {
   return "Calculate the transmission rate through a depolarised He3 cell.";
 }
 
-void SANSCalcDepolarisedAnalyserTransmission::init() {}
+void SANSCalcDepolarisedAnalyserTransmission::init() {
+  declareProperty(std::make_unique<WorkspaceProperty<WorkspaceGroup>>(std::string(Prop::DEP_WORKSPACE), "",
+                                                                      Kernel::Direction::Input),
+                  "The group of fully depolarised workspaces.");
+  declareProperty(std::make_unique<WorkspaceProperty<WorkspaceGroup>>(std::string(Prop::MT_WORKSPACE), "",
+                                                                      Kernel::Direction::Input),
+                  "The group of empty cell workspaces.");
+}
 
 void SANSCalcDepolarisedAnalyserTransmission::exec() {}
 
