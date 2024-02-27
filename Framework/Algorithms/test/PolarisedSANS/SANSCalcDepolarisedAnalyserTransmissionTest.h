201d31b689a1c4f8a70bfc9e07dfb2a80e549078
Conor Finn <conor.finn@stfc.ac.uk>
2024-02-27 16:05:38 +0000

    Add failing state test cases
    RE #36383

--- Framework/Algorithms/test/PolarisedSANS/SANSCalcDepolarisedAnalyserTransmissionTest.h
+++ Framework/Algorithms/test/PolarisedSANS/SANSCalcDepolarisedAnalyserTransmissionTest.h
@@ -12,12 +12,13 @@
 #include "MantidAlgorithms/PolarisedSANS/SANSCalcDepolarisedAnalyserTransmission.h"
 
 namespace {
-constexpr double DELTA = 1e-5;
 constexpr double T_E_VALUE = 82593.9;
-constexpr double PXD_VALUE = 26088049.0;
-constexpr double T_E_ERROR = 14.9860;
-constexpr double PXD_ERROR = 467.99;
-constexpr double COST_FUNC_MAX = 5e-20
+constexpr double PXD_VALUE = 14.9860;
+constexpr double T_E_ERROR = 26088049.0;
+constexpr double PXD_ERROR = 467.994241;
+constexpr double T_E_DELTA = 1e-1;
+constexpr double PXD_DELTA = 1e-5;
+constexpr double COST_FUNC_MAX = 5e-15;
 } // namespace
 
 using Mantid::Algorithms::CreateSampleWorkspace;
@@ -49,11 +50,45 @@ public:
     alg.execute();
     TS_ASSERT(alg.isExecuted());
     Mantid::API::ITableWorkspace_sptr const &outputWs = alg.getProperty("OutputWorkspace");
-    TS_ASSERT_DELTA(outputWs->getColumn("Value")->toDouble(0), T_E_VALUE, DELTA);
-    TS_ASSERT_DELTA(outputWs->getColumn("Value")->toDouble(1), PXD_VALUE, DELTA);
-    TS_ASSERT_DELTA(outputWs->getColumn("Error")->toDouble(0), T_E_ERROR, DELTA);
-    TS_ASSERT_DELTA(outputWs->getColumn("Error")->toDouble(1), PXD_ERROR, DELTA);
+    TS_ASSERT_DELTA(outputWs->getColumn("Value")->toDouble(0), T_E_VALUE, T_E_DELTA);
+    TS_ASSERT_DELTA(outputWs->getColumn("Value")->toDouble(1), PXD_VALUE, PXD_DELTA);
+    TS_ASSERT_DELTA(outputWs->getColumn("Error")->toDouble(0), T_E_ERROR, T_E_DELTA);
+    TS_ASSERT_DELTA(outputWs->getColumn("Error")->toDouble(1), PXD_ERROR, PXD_DELTA);
     TS_ASSERT_LESS_THAN(outputWs->getColumn("Value")->toDouble(2), COST_FUNC_MAX);
+  }
+
+  void test_failed_fit() {
+    MatrixWorkspace_sptr const &mtWs = createTestingWorkspace("__mt", "1.465e-07*exp(0.0733*4.76*x)");
+    auto const &depWs = createTestingWorkspace("__dep", "0.0121*exp(-0.0733*10.226*x)");
+    SANSCalcDepolarisedAnalyserTransmission alg;
+    alg.setChild(true);
+    alg.initialize();
+    TS_ASSERT(alg.isInitialized());
+    alg.setProperty("DepolarisedWorkspace", depWs);
+    alg.setProperty("EmptyCellWorkspace", mtWs);
+    alg.setProperty("T_EStartingValue", 1e50);
+    alg.setProperty("PxDStartingValue", 1e50);
+    alg.setPropertyValue("OutputWorkspace", "__unused_for_child");
+    TS_ASSERT_THROWS_EQUALS(alg.execute(), std::runtime_error const &e, std::string(e.what()),
+                            "Failed to fit to transmission workspace, : Changes in function value are too small");
+    TS_ASSERT(!alg.isExecuted());
+  }
+
+  void test_apparently_successful_fit() {
+    MatrixWorkspace_sptr const &mtWs = createTestingWorkspace("__mt", "0*x");
+    auto const &depWs = createTestingWorkspace("__dep", "0.0121*exp(-0.0733*10.226*x)");
+    SANSCalcDepolarisedAnalyserTransmission alg;
+    alg.setChild(true);
+    alg.initialize();
+    TS_ASSERT(alg.isInitialized());
+    alg.setProperty("DepolarisedWorkspace", depWs);
+    alg.setProperty("EmptyCellWorkspace", mtWs);
+    alg.setPropertyValue("OutputWorkspace", "__unused_for_child");
+    TS_ASSERT_THROWS_EQUALS(alg.execute(), std::runtime_error const &e, std::string(e.what()),
+                            "Failed to fit to transmission workspace, : Fit quality is too low (0.000000). You may "
+                            "want to check that the correct monitor spectrum was provided.");
+    alg.isExecuted();
+    Mantid::API::ITableWorkspace_sptr const &outputWs = alg.getProperty("OutputWorkspace");
   }
 
 private:
