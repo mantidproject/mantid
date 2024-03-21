447fab637591567fcc3469d5a982a2786c986b72
GuiMacielPereira <gui.maciel-pereira@stfc.ac.uk>
2024-03-21 11:29:33 +0000

    Added if statement to update model if introducing new changes
    - Only updates aspect ratio if incoming command is different from what is already set

--- qt/widgets/instrumentview/src/InstrumentWidget.cpp
+++ qt/widgets/instrumentview/src/InstrumentWidget.cpp
@@ -1093,10 +1093,12 @@ void InstrumentWidget::setWireframe(bool on) {
 }
 
 void InstrumentWidget::setMaintainAspectRatio(bool on) {
-  m_maintainAspectRatio = on;
-  setSurfaceType(m_surfaceType);
-  updateInstrumentView();
-  emit maintainAspectRatioChanged(on);
+  if (m_maintainAspectRatio != on) {
+    m_maintainAspectRatio = on;
+    setSurfaceType(m_surfaceType);
+    updateInstrumentView();
+    emit maintainAspectRatioChanged(on);
+  }
 }
 
 /**
