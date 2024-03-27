c9eee861f84ee788a758508ab1e23eecb2d41679
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2024-03-27 15:02:53 +0000

    Add derivative and refine parameters and attributes

--- Framework/PythonInterface/plugins/functions/TeixeiraWaterIqt.py
+++ Framework/PythonInterface/plugins/functions/TeixeiraWaterIqt.py
@@ -14,49 +14,63 @@
 import numpy as np
 
 from mantid.api import IFunction1D, FunctionFactory
-
-from scipy import constants
 from scipy.special import spherical_jn  # bessel
 
 
 class TeixeiraWaterIqt(IFunction1D):
-    planck_constant = constants.Planck / constants.e * 1e15  # meV*psec
-    hbar = planck_constant / (2 * np.pi)  # meV * ps  = ueV * ns
 
     def category(self):
         return "QuasiElastic"
 
     def init(self):
-        # Active fitting parameters
+        # Fitting parameters
         self.declareParameter("Amp", 1.00, "Amplitude")
-        self.declareParameter("Tau0", 1.25, "Residence time")
         self.declareParameter("Tau1", 0.05, "Relaxation time")
-        self.declareParameter("a", 1.3, "Radius rotation")
-        self.declareParameter("Diff", 2.3, "Diffusion coefficient")
+        self.declareParameter("Gamma", 1.2, "Line Width")
+        self.addConstraints("Tau1 > 0")
+        # Non-fitting parameters
         self.declareAttribute("Q", 0.4)
+        self.declareAttribute("a", 0.98)
 
     def setAttributeValue(self, name, value):
         if name == "Q":
             self.Q = value
+        if name == "a":
+            self.radius = value
 
     def function1D(self, xvals):
         amp = self.getParameterValue("Amp")
-        tau0 = self.getParameterValue("Tau0")
         tau1 = self.getParameterValue("Tau1")
-        radius = self.getParameterValue("a")
-        diff = self.getParameterValue("Diff")
+        gamma = self.getParameterValue("Gamma")
         xvals = np.array(xvals)
 
-        qr = np.array(self.Q * radius)
+        qr = np.array(self.Q * self.radius)
         j0 = spherical_jn(0, qr)
         j1 = spherical_jn(1, qr)
         j2 = spherical_jn(2, qr)
         with np.errstate(divide="ignore"):
             rotational = j0 * j0 + 3 * j1 * j1 * np.exp(-xvals / (3 * tau1)) + 5 * j2 * j2 * np.exp(-xvals / tau1)
-            gamma = diff * self.Q * self.Q / (1 + diff * self.Q * self.Q * tau0)
             translational = np.exp(-gamma * xvals)
             iqt = amp * rotational * translational
         return iqt
+
+    def functionDeriv1D(self, xvals, jacobian):
+        amp = self.getParameterValue("Amp")
+        tau1 = self.getParameterValue("Tau1")
+        gamma = self.getParameterValue("Gamma")
+
+        qr = np.array(self.Q * self.radius)
+        j0 = spherical_jn(0, qr)
+        j1 = spherical_jn(1, qr)
+        j2 = spherical_jn(2, qr)
+        with np.errstate(divide="ignore"):
+            for i, x in enumerate(xvals, start=0):
+                rotational = j0 * j0 + 3 * j1 * j1 * np.exp(-x / (3 * tau1)) + 5 * j2 * j2 * np.exp(-x / tau1)
+                translational = np.exp(-gamma * x)
+                partial_tau = (x / np.square(tau1)) * (j1 * j1 * np.exp(-x / (3 * tau1)) + 5 * j2 * j2 * np.exp(-x / tau1))
+                jacobian.set(i, 0, rotational * translational)
+                jacobian.set(i, 1, amp * rotational * partial_tau)
+                jacobian.set(i, 2, -x * amp * rotational * translational)
 
 
 # Required to have Mantid recognise the new function
