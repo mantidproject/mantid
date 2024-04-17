873751f9389395bcacd5c237d9b9d67dc4e47400
Applin <robert.applin@stfc.ac.uk>
2024-04-17 10:43:19 +0100

    Implement spin diffusion function for 2D and 3D cases

--- Framework/PythonInterface/plugins/functions/SpinDiffusion.py
+++ Framework/PythonInterface/plugins/functions/SpinDiffusion.py
@@ -10,64 +10,63 @@ from functools import cache
 from numpy import float64
 from numpy.typing import NDArray
 from mantid.api import IFunction1D, FunctionFactory
+from scipy.integrate import quad
 from scipy.special import i0e
 from typing import Tuple
 
 
 @cache
 def cached_i0e(d_i: float, t: Tuple[float]) -> NDArray[float64]:
+    """Calculate the zeroth order modified Bessel function, scaled by an exponential term."""
     return i0e(2 * d_i * np.array(t))
 
 
 def autocorrelation_st(d_1: float, d_2: float, d_3: float, t: Tuple[float]) -> float:
+    """Calculate S(t), the autocorrelation function represented by an anisotropic random walk."""
     return cached_i0e(d_1, t) * cached_i0e(d_2, t) * cached_i0e(d_3, t)
 
 
 def spectral_density_approximation_1d(d_1: float, d_2: float, w: NDArray[float64]) -> NDArray[float64]:
+    """Calculate J(w) approximation for a 1D system."""
     return (1 / np.sqrt(4 * d_1 * d_2)) * np.sqrt((1 + np.sqrt(1 + np.power(w / (2 * d_2), 2))) / (1 + np.power(w / (2 * d_2), 2)))
 
 
+def integrand(t: Tuple[float], d_1: float, d_2: float, d_3: float, w: float) -> float:
+    "Integrand used to calculate J(w)."
+    return autocorrelation_st(d_1, d_2, d_3, t) * np.cos(w * t)
+
+
 def spectral_density(d_1: float, d_2: float, d_3: float, w: float) -> float:
-    raise NotImplementedError("This is not implemented yet")
+    """Calculate J(w) for 2D and 3D systems."""
+    integral, _ = quad(
+        integrand,
+        0,
+        300,
+        args=(
+            d_1,
+            d_2,
+            d_3,
+            w,
+        ),
+    )  # Change integration range so it tends towards infinity
+    return 2.0 * integral
 
 
 class SpinDiffusion(IFunction1D):
-
-    # For integration over all directions of vector Q
-    n_theta = 128
-    d_theta = (np.pi / 2.0) / n_theta
-    theta = d_theta * np.arange(1, n_theta)
-    sin_theta = np.sin(theta)
-    cos_theta = np.cos(theta)
 
     def category(self):
         return "Muon\\MuonSpecific"
 
     def init(self):
-        # Active fitting parameters
+        # Fitting parameters
         self.declareParameter("A", 1.0, "Amplitude, or Scaling factor")
         self.declareParameter("D1", 1e3, "Dipolar term 1 (MHz)")
         self.declareParameter("D2", 1e-2, "Dipolar term 2 (MHz)")
         self.declareParameter("D3", 1e-2, "Dipolar term 3 (MHz)")
         # Non-fitting parameters
-        self.declareAttribute("NDimensions", 1)
+        self.declareAttribute("NDimensions", 1, "The number of dimensions (1, 2 or 3)")
 
     def function1D(self, xvals):
-        r"""Calculate the intensities
-
-        Parameters
-        ----------
-        xvals : sequence of floats
-          The domain where to evaluate the function
-        jacobian: 2D array
-          partial derivative of the function with respect to the fitting
-          parameters evaluated at the domain.
-
-        Returns
-        -------
-        numpy.ndarray
-            Function values
-        """
         A = self.getParameterValue("A")
         d_1 = self.getParameterValue("D1")
         d_2 = self.getParameterValue("D2")
@@ -75,9 +74,16 @@ class SpinDiffusion(IFunction1D):
 
         n_dimensions = int(self.getAttributeValue("NDimensions"))
         if n_dimensions == 1:
+            d_3 = d_2
             spectral_density_results = spectral_density_approximation_1d(d_1 / d_1, d_2 / d_1, np.array(xvals))
+        elif n_dimensions == 2:
+            d_2 = d_1
+            spectral_density_results = np.array([spectral_density(d_1 / d_1, d_2 / d_1, d_3 / d_1, w) for w in xvals])
+        elif n_dimensions == 3:
+            d_3 = d_2 = d_1
+            spectral_density_results = np.array([spectral_density(d_1 / d_1, d_2 / d_1, d_3 / d_1, w) for w in xvals])
         else:
-            spectral_density_results = spectral_density(d_1 / d_1, d_2 / d_1, d_3 / d_1, "Not implemented")
+            raise NotImplementedError("Not implemented for more than 3 dimensions")
 
         return np.square(A) / 4 * spectral_density_results
 
