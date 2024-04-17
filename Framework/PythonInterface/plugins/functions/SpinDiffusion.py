180c8425fec48a2ebb347da0fbf75780f6a75795
Applin <robert.applin@stfc.ac.uk>
2024-04-17 11:53:10 +0100

    Tidy up changes to spin diffusion fit function

--- Framework/PythonInterface/plugins/functions/SpinDiffusion.py
+++ Framework/PythonInterface/plugins/functions/SpinDiffusion.py
@@ -27,9 +27,11 @@ def autocorrelation_st(d_1: float, d_2: float, d_3: float, t: Tuple[float]) -> f
     return cached_i0e(d_1, t) * cached_i0e(d_2, t) * cached_i0e(d_3, t)
 
 
-def spectral_density_approximation_1d(d_1: float, d_2: float, w: NDArray[float64]) -> NDArray[float64]:
+def spectral_density_approximation_1d(d_parallel: float, d_perpendicular: float, w: NDArray[float64]) -> NDArray[float64]:
     """Calculate J(w) approximation for a 1D system."""
-    return (1 / np.sqrt(4 * d_1 * d_2)) * np.sqrt((1 + np.sqrt(1 + np.power(w / (2 * d_2), 2))) / (1 + np.power(w / (2 * d_2), 2)))
+    return (1 / np.sqrt(4 * d_parallel * d_perpendicular)) * np.sqrt(
+        (1 + np.sqrt(1 + np.power(w / (2 * d_perpendicular), 2))) / (1 + np.power(w / (2 * d_perpendicular), 2))
+    )
 
 
 def integrand(t: Tuple[float], d_1: float, d_2: float, d_3: float, w: float) -> float:
@@ -37,20 +39,33 @@ def integrand(t: Tuple[float], d_1: float, d_2: float, d_3: float, w: float) -> 
     return autocorrelation_st(d_1, d_2, d_3, t) * np.cos(w * t)
 
 
-def spectral_density(d_1: float, d_2: float, d_3: float, w: float, n_points: int) -> float:
+def spectral_density(d_1: float, d_2: float, d_3: float, w: float, integration_upper: float) -> float:
     """Calculate J(w) for 2D and 3D systems."""
     integral, _ = quad(
         integrand,
         0,
-        n_points / 4,
+        integration_upper,
         args=(
             d_1,
             d_2,
             d_3,
             w,
         ),
-    )  # Change integration range so it tends towards infinity
+    )  # Note the integration range in the paper tends towards infinity
     return 2.0 * integral
+
+
+def d_rates(n_dimensions: int, d_par: float, d_perp: float) -> Tuple[float]:
+    """Tie dipolar rates based on the dimensionality of the system."""
+    match n_dimensions:
+        case 1:  # D1 = D||           D2, D3 = D_|_
+            return d_par / d_par, d_perp / d_par
+        case 2:  # D1, D2 = D||       D3 = D_|_
+            return d_par / d_par, d_par / d_par, d_perp / d_par
+        case 3:  # D1, D2, D3 = D||
+            return d_par / d_par, d_par / d_par, d_par / d_par
+        case _:
+            raise RuntimeError("The number of dimensions has an upper bound of 3.")
 
 
 class SpinDiffusion(IFunction1D):
@@ -61,30 +76,23 @@ class SpinDiffusion(IFunction1D):
     def init(self):
         # Fitting parameters
         self.declareParameter("A", 1.0, "Amplitude, or Scaling factor")
-        self.declareParameter("D1", 1e3, "Dipolar term 1 (MHz)")
-        self.declareParameter("D2", 1e-2, "Dipolar term 2 (MHz)")
-        self.declareParameter("D3", 1e-2, "Dipolar term 3 (MHz)")
+        self.declareParameter("DParallel", 1e3, "Dipolar parallel, the fast rate dipolar term (MHz)")
+        self.declareParameter("DPerpendicular", 1e-2, "Dipolar perpendicular, the slow rate dipolar term (MHz)")
         # Non-fitting parameters
         self.declareAttribute("NDimensions", 1, IntBoundedValidator(lower=1, upper=3))
 
     def function1D(self, xvals):
         A = self.getParameterValue("A")
-        d_1 = self.getParameterValue("D1")
-        d_2 = self.getParameterValue("D2")
-        d_3 = self.getParameterValue("D3")
+        d_par = self.getParameterValue("DParallel")
+        d_perp = self.getParameterValue("DPerpendicular")
 
-        n_dimensions = int(self.getAttributeValue("NDimensions"))
-        n_points = len(xvals)
-        match n_dimensions:
-            case 1:
-                d_3 = d_2
-                spectral_density_results = spectral_density_approximation_1d(d_1 / d_1, d_2 / d_1, np.array(xvals))
-            case 2:
-                d_2 = d_1
-                spectral_density_results = np.array([spectral_density(d_1 / d_1, d_2 / d_1, d_3 / d_1, w, n_points) for w in xvals])
-            case 3:
-                d_3 = d_2 = d_1
-                spectral_density_results = np.array([spectral_density(d_1 / d_1, d_2 / d_1, d_3 / d_1, w, n_points) for w in xvals])
+        n_dimensions = self.getAttributeValue("NDimensions")
+
+        d_i_terms = d_rates(n_dimensions, d_par, d_perp)
+        if n_dimensions == 1:
+            spectral_density_results = spectral_density_approximation_1d(*d_i_terms, np.array(xvals))
+        else:
+            spectral_density_results = np.array([spectral_density(*d_i_terms, w, len(xvals) / 4) for w in xvals])
 
         return np.square(A) / 4 * spectral_density_results
 
