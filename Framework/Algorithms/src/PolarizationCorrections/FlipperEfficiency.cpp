7c62a374c28dfe8de5bb8dac5be0c3167831bf36
Conor Finn <conor.finn@stfc.ac.uk>
2024-04-17 16:52:36 +0100

    Add initial properties
    RE #36144

--- Framework/Algorithms/src/PolarizationCorrections/FlipperEfficiency.cpp
+++ Framework/Algorithms/src/PolarizationCorrections/FlipperEfficiency.cpp
@@ -8,7 +8,11 @@
 
 namespace {
 /// Property Names
-namespace PropNames {} // namespace PropNames
+namespace PropNames {
+std::string const &INPUT_WS{"InputWorkspace"};
+std::string const &OUTPUT_WS{"OutputWorkspace"};
+std::string const &OUTPUT_FILE{"OutputFilePath"};
+} // namespace PropNames
 } // namespace
 
 namespace Mantid::Algorithms {
@@ -21,7 +25,13 @@ DECLARE_ALGORITHM(FlipperEfficiency)
 
 std::string const FlipperEfficiency::summary() const { return "Calculate the efficiency of the polarization flipper."; }
 
-void FlipperEfficiency::init() {}
+void FlipperEfficiency::init() {
+  declareProperty(std::make_unique<WorkspaceProperty<MatrixWorkspace>>(PropNames::INPUT_WS, "", Direction::Input),
+                  "Group workspace containing the 4 polarisation periods.");
+  declareProperty(std::make_unique<WorkspaceProperty<MatrixWorkspace>>(PropNames::OUTPUT_WS, "", Direction::Output),
+                  "Workspace containing the wavelength-dependent efficiency for the flipper.");
+  declareProperty(PropNames::OUTPUT_FILE, "", "File name or path for the output to be saved to.");
+}
 
 void FlipperEfficiency::exec() {}
 
