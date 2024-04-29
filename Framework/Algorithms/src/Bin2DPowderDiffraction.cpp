ba735d2171e77e4a133b07d88ee1ee1c50791282
Jonathan Haigh <35813666+jhaigh0@users.noreply.github.com>
2024-04-29 15:07:11 +0100

    Update Framework/Algorithms/src/Bin2DPowderDiffraction.cpp

--- Framework/Algorithms/src/Bin2DPowderDiffraction.cpp
+++ Framework/Algorithms/src/Bin2DPowderDiffraction.cpp
@@ -325,8 +325,8 @@ size_t Bin2DPowderDiffraction::UnifyXBins(std::vector<std::vector<double>> &Xbin
     return 0;
   // get maximal vector size
   size_t maxSize = std::max_element(Xbins.cbegin(), Xbins.cend(), [](const auto &a, const auto &b) {
-                      return a.size() < b.size();
-                    })->size();
+                     return a.size() < b.size();
+                   })->size();
   // resize all vectors to maximum size, fill last vector element at the end
   for (auto &v : Xbins) {
     if (v.size() < max_size)
