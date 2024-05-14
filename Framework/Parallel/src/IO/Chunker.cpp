f4b29d468899674b8e03060f7259dad00f3ec11d
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2024-05-14 17:23:33 +0100

    fix test failures
    try removing const to fix linux test

--- Framework/Parallel/src/IO/Chunker.cpp
+++ Framework/Parallel/src/IO/Chunker.cpp
@@ -174,7 +174,7 @@ std::vector<std::pair<int, std::vector<size_t>>> Chunker::makeBalancedPartitioni
   // Indexed size vector such that we can sort it but still know original index.
   // Elements are <size, original index, done flag>
   std::vector<std::tuple<size_t, size_t, bool>> sortedSizes;
-  std::transform(sizes.cbegin(), sizes.cend(), sortedSizes.begin(),
+  std::transform(sizes.cbegin(), sizes.cend(), std::back_inserter(sortedSizes),
                  [&sortedSizes](const auto size) { return std::make_tuple(size, sortedSizes.size(), false); });
   std::sort(sortedSizes.begin(), sortedSizes.end(),
             [](const auto &a, const auto &b) { return std::get<0>(a) > std::get<0>(b); });
