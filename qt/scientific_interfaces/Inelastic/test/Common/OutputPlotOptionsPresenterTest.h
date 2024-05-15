3a6df51be123dbc82fb7016a00810b8e08fdcf9a
Applin <robert.applin@stfc.ac.uk>
2024-05-15 12:02:09 +0100

    Update OutputPlotOptionsPresenterTest

--- qt/scientific_interfaces/Inelastic/test/Common/OutputPlotOptionsPresenterTest.h
+++ qt/scientific_interfaces/Inelastic/test/Common/OutputPlotOptionsPresenterTest.h
@@ -23,10 +23,10 @@ std::string const WORKSPACE_NAME = "WorkspaceName";
 std::string const WORKSPACE_INDICES = "0-2,4";
 
 std::map<std::string, std::string>
-constructActions(boost::optional<std::map<std::string, std::string>> const &availableActions) {
+constructActions(std::optional<std::map<std::string, std::string>> const &availableActions) {
   std::map<std::string, std::string> actions;
   if (availableActions)
-    actions = availableActions.get();
+    actions = *availableActions;
   if (actions.find("Plot Spectra") == actions.end())
     actions["Plot Spectra"] = "Plot Spectra";
   if (actions.find("Plot Bins") == actions.end())
@@ -80,8 +80,10 @@ public:
     auto model = std::make_unique<NiceMock<MockOutputPlotOptionsModel>>();
     m_model = model.get();
 
+    auto actions = constructActions(std::nullopt);
     EXPECT_CALL(*m_view, setIndicesRegex(_)).Times(1);
-    EXPECT_CALL(*m_view, setPlotType(PlotWidget::Spectra, constructActions(boost::none))).Times(1);
+    ON_CALL(*m_model, availableActions()).WillByDefault(Return(actions));
+    EXPECT_CALL(*m_view, setPlotType(PlotWidget::Spectra, actions)).Times(1);
     EXPECT_CALL(*m_view, setIndices(QString(""))).Times(1);
     EXPECT_CALL(*m_model, setFixedIndices("")).Times(1);
 
@@ -209,7 +211,9 @@ public:
   ///----------------------------------------------------------------------
 
   void test_setPlotType_sets_the_view() {
-    EXPECT_CALL(*m_view, setPlotType(PlotWidget::Spectra, constructActions(boost::none))).Times(1);
+    auto actions = constructActions(std::nullopt);
+    ON_CALL(*m_model, availableActions()).WillByDefault(Return(actions));
+    EXPECT_CALL(*m_view, setPlotType(PlotWidget::Spectra, actions)).Times(1);
     m_presenter->setPlotType(PlotWidget::Spectra);
   }
 
