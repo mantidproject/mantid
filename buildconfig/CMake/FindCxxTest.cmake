179682631cdd08dc6a86f6b332837aceaa120de9
Waruna Wickramasingha <waruna.wickramasingha@stfc.ac.uk>
2024-06-12 10:47:45 +0100

    change dir before test

--- buildconfig/CMake/FindCxxTest.cmake
+++ buildconfig/CMake/FindCxxTest.cmake
@@ -230,8 +230,8 @@ PYTHONHOME=${_python_home}"
         NAME ${_cxxtest_separate_name}
         COMMAND
           valgrind --gen-suppressions=all --track-origins=yes --show-reachable=yes --error-limit=no --leak-check=full
-          --errors-for-leak-kinds=definite --show-leak-kinds=definite --log-file=$ENV{VALGRIND_LOG_FILE}
-          $<TARGET_FILE:${_cxxtest_testname}>
+          --errors-for-leak-kinds=definite --show-leak-kinds=definite --log-file=$ENV{VALGRIND_LOG_FILE} -E chdir
+          "${CMAKE_BINARY_DIR}/bin/Testing" $<TARGET_FILE:${_cxxtest_testname}>
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
       )
       set_tests_properties(${_cxxtest_separate_name} PROPERTIES TIMEOUT ${TESTING_TIMEOUT})
