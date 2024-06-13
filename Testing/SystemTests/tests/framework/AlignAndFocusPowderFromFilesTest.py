a6f86beca9aa6fb48429f5f9ff4635f402f23aa7
Ross Whitfield <whitfieldre1@ornl.gov>
2024-06-13 11:28:37 +1000

    Add system test for Logarithmic compression

--- Testing/SystemTests/tests/framework/AlignAndFocusPowderFromFilesTest.py
+++ Testing/SystemTests/tests/framework/AlignAndFocusPowderFromFilesTest.py
@@ -106,6 +106,86 @@ class SimplestCompare(systemtesting.MantidSystemTest):
         return (self.wksp_mem, self.wksp_file)
 
 
+class LogarithmicCompression(systemtesting.MantidSystemTest):
+    cal_file = "PG3_FERNS_d4832_2011_08_24.cal"
+    char_file = "PG3_characterization_2012_02_23-HR-ILL.txt"
+    data_file = "PG3_9829_event.nxs"
+
+    def cleanup(self):
+        return do_cleanup(self.cacheDir)
+
+    def requiredMemoryMB(self):
+        return 3 * 1024  # GiB
+
+    def requiredFiles(self):
+        return [self.cal_file, self.char_file, self.data_file]
+
+    def runTest(self):
+        self.cacheDir = getCacheDir()
+
+        PDLoadCharacterizations(
+            Filename=self.char_file, OutputWorkspace="characterizations", SpectrumIDs="1", L2="3.18", Polar="90", Azimuthal="0"
+        )
+
+        self.wksp_mem = os.path.basename(self.data_file).split(".")[0]
+        self.wksp_mem, self.wksp_file = self.wksp_mem + "_mem", self.wksp_mem + "_file"
+
+        # load then process
+        LoadEventNexus(Filename=self.data_file, OutputWorkspace=self.wksp_mem)
+        FilterBadPulses(InputWorkspace=self.wksp_mem, OutputWorkspace=self.wksp_mem, LowerCutoff=95)
+        LoadDiffCal(Filename=self.cal_file, InputWorkspace=self.wksp_mem, WorkspaceName="PG3")
+        PDDetermineCharacterizations(
+            InputWorkspace=self.wksp_mem, Characterizations="characterizations", ReductionProperties="__snspowderreduction_inner"
+        )
+        AlignAndFocusPowder(
+            InputWorkspace=self.wksp_mem,
+            OutputWorkspace=self.wksp_mem,
+            GroupingWorkspace="PG3_group",
+            CalibrationWorkspace="PG3_cal",
+            MaskWorkspace="PG3_mask",
+            Params=-0.0002,
+            CompressTolerance=-0.01,
+            PrimaryFlightPath=60,
+            SpectrumIDs="1",
+            L2="3.18",
+            Polar="90",
+            Azimuthal="0",
+            ReductionProperties="__snspowderreduction_inner",
+        )
+        NormaliseByCurrent(InputWorkspace=self.wksp_mem, OutputWorkspace=self.wksp_mem)
+        ConvertUnits(InputWorkspace=self.wksp_mem, OutputWorkspace=self.wksp_mem, Target="dSpacing")
+        SortEvents(self.wksp_mem)
+
+        # everything inside the algorithm
+        AlignAndFocusPowderFromFiles(
+            Filename=self.data_file,
+            OutputWorkspace=self.wksp_file,
+            GroupingWorkspace="PG3_group",
+            CalibrationWorkspace="PG3_cal",
+            MaskWorkspace="PG3_mask",
+            Params=-0.0002,
+            CompressTolerance=0.01,
+            CompressBinningMode="Logarithmic",
+            FilterBadPulses=95,
+            PrimaryFlightPath=60,
+            SpectrumIDs="1",
+            L2="3.18",
+            Polar="90",
+            Azimuthal="0",
+            ReductionProperties="__snspowderreduction_inner",
+        )
+        NormaliseByCurrent(InputWorkspace=self.wksp_file, OutputWorkspace=self.wksp_file)
+        ConvertUnits(InputWorkspace=self.wksp_file, OutputWorkspace=self.wksp_file, Target="dSpacing")
+        SortEvents(self.wksp_file)
+
+    def validateMethod(self):
+        self.tolerance = 1.0e-2
+        return "ValidateWorkspaceToWorkspace"
+
+    def validate(self):
+        return (self.wksp_mem, self.wksp_file)
+
+
 class ChunkingCompare(systemtesting.MantidSystemTest):
     # this test is very similar to SNAPRedux.Simple
 
