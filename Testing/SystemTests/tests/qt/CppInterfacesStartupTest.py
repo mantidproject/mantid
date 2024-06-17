f3a15aea8609f7e9247b2fcc9c357fa7d95aba08
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2024-06-17 15:13:02 +0100

    add assertions that interfaces are using the correct flags

--- Testing/SystemTests/tests/qt/CppInterfacesStartupTest.py
+++ Testing/SystemTests/tests/qt/CppInterfacesStartupTest.py
@@ -9,7 +9,7 @@ from itertools import chain
 
 from mantidqt.interfacemanager import InterfaceManager
 from mantidqt.utils.qt.testing import get_application
-from workbench.config import CONF
+from workbench.config import CONF, WINDOW_ONTOP_FLAGS, WINDOW_STANDARD_FLAGS
 from workbench.utils.gather_interfaces import gather_cpp_interface_names
 
 from qtpy.QtCore import Qt
@@ -42,13 +42,23 @@ class CppInterfacesStartupTest(systemtesting.MantidSystemTest):
     def _open_interfaces(self, window_behaviour):
         CONF.set("AdditionalWindows/behaviour", window_behaviour)
         for interface_name in self._cpp_interface_names:
-            self._attempt_to_open_cpp_interface(interface_name)
+            self._attempt_to_open_cpp_interface(interface_name, window_behaviour)
 
-    def _attempt_to_open_cpp_interface(self, interface_name):
+    def _attempt_to_open_cpp_interface(self, interface_name, window_behaviour):
         try:
             interface = self._interface_manager.createSubWindow(interface_name)
             interface.setAttribute(Qt.WA_DeleteOnClose, True)
             interface.show()
+            if window_behaviour == "On Top":
+                self.assertTrue(
+                    interface.windowFlags() & WINDOW_ONTOP_FLAGS,
+                    f"{interface_name} is not using correct window flags for 'On Top' behaviour",
+                )
+            else:
+                self.assertTrue(
+                    interface.windowFlags() & WINDOW_STANDARD_FLAGS,
+                    f"{interface_name} is not using correct window flags for 'Floating' behaviour",
+                )
             interface.close()
 
             # Delete the interface manually because the destructor is not being called as expected on close (even with
