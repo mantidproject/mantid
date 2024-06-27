82ca13bea14e4288a4b4b948db3961528def667e
Applin <robert.applin@stfc.ac.uk>
2024-06-27 16:30:10 +0100

    Move FunctionQParameters class to separate file

---
+++ qt/scientific_interfaces/Inelastic/QENSFitting/FunctionQParameters.h
@@ -0,0 +1,42 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "DllConfig.h"
+
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace MantidQt {
+namespace CustomInterfaces {
+namespace Inelastic {
+
+class MANTIDQT_INELASTIC_DLL FunctionQParameters {
+public:
+  FunctionQParameters();
+  FunctionQParameters(std::pair<std::vector<std::string>, std::vector<std::size_t>> widths,
+                      std::pair<std::vector<std::string>, std::vector<std::size_t>> eisfs);
+
+  std::vector<std::string> names(std::string const &parameterType) const;
+
+  std::vector<std::size_t> spectra(std::string const &parameterType) const;
+
+  std::vector<std::string> types() const;
+
+  operator bool() const { return !m_widthSpectra.empty() || !m_eisfSpectra.empty(); }
+
+private:
+  std::vector<std::string> m_widths;
+  std::vector<std::size_t> m_widthSpectra;
+  std::vector<std::string> m_eisf;
+  std::vector<std::size_t> m_eisfSpectra;
+};
+
+} // namespace Inelastic
+} // namespace CustomInterfaces
+} // namespace MantidQt
