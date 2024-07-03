c21c640bffdd628bc3861ecbaf504b5a8ac10447
Applin <robert.applin@stfc.ac.uk>
2024-07-03 11:00:09 +0100

    Add separate system test for IqtFitMultiple

---
+++ Testing/SystemTests/tests/framework/IqtFitMultipleTest.py
@@ -0,0 +1,38 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+# pylint: disable=no-init
+import systemtesting
+from mantid.simpleapi import Load, IqtFitMultiple
+
+
+class IqtFitMultipleTest(systemtesting.MantidSystemTest):
+
+    def runTest(self):
+        Load(Filename="irs26176_graphite002_iqt.nxs", OutputWorkspace="irs26176_graphite002_iqt")
+
+        func = (
+            "name=LinearBackground,A0=0.213439,A1=0,ties=(A1=0);name=StretchExp,"
+            "Height=0.786561,Lifetime=0.0247894,"
+            "Stretching=1;ties=(f1.Height=1-f0.A0)"
+        )
+        IqtFitMultiple(
+            InputWorkspace="irs26176_graphite002_iqt",
+            Function=func,
+            FitType="1E_s",
+            StartX=0.0,
+            EndX=0.12,
+            SpecMin=0,
+            SpecMax=9,
+            OutputResultWorkspace="Result",
+            OutputParameterWorkspace="Parameters",
+            OutputWorkspaceGroup="ResultGroup",
+        )
+
+    def validate(self):
+        # Fitting can give slightly different results across different operating systems
+        self.tolerance = 1e-2
+        return "Result", "irs26176_graphite002_iqt_fit_parameters.nxs"
