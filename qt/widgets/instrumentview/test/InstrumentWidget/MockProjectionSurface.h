ac0948a72d77e0d286a747ea08aebda88a6e1312
James Clarke <james.clarke@stfc.ac.uk>
2024-07-22 15:11:01 +0100

    Add test for overlaying a peak with no detector

--- qt/widgets/instrumentview/test/InstrumentWidget/MockProjectionSurface.h
+++ qt/widgets/instrumentview/test/InstrumentWidget/MockProjectionSurface.h
@@ -8,6 +8,7 @@
 
 #include "MantidQtWidgets/InstrumentView/GLDisplay.h"
 #include "MantidQtWidgets/InstrumentView/ProjectionSurface.h"
+#include "MantidQtWidgets/InstrumentView/UnwrappedSphere.h"
 
 #include <gmock/gmock.h>
 
@@ -16,6 +17,20 @@ class MockProjectionSurface : public ProjectionSurface {
 public:
   MockProjectionSurface() : ProjectionSurface(nullptr) {}
   virtual ~MockProjectionSurface() = default;
+  MOCK_METHOD(void, init, (), (override));
+  MOCK_METHOD(void, componentSelected, (size_t), (override));
+  MOCK_METHOD(void, getSelectedDetectors, (std::vector<size_t> &), (override));
+  MOCK_METHOD(void, getMaskedDetectors, (std::vector<size_t> &), (const, override));
+  MOCK_METHOD(void, drawSurface, (GLDisplay *, bool), (const, override));
+  MOCK_METHOD(void, changeColorMap, (), (override));
+  MOCK_METHOD(void, setInteractionMode, (int), (override));
+};
+
+class MockUnwrappedSphere : public UnwrappedSphere {
+public:
+  MockUnwrappedSphere(const InstrumentActor *rootActor)
+      : UnwrappedSphere(rootActor, Mantid::Kernel::V3D(), Mantid::Kernel::V3D(0, 0, 1), QSize(), true) {}
+  virtual ~MockUnwrappedSphere() = default;
   MOCK_METHOD(void, init, (), (override));
   MOCK_METHOD(void, componentSelected, (size_t), (override));
   MOCK_METHOD(void, getSelectedDetectors, (std::vector<size_t> &), (override));
