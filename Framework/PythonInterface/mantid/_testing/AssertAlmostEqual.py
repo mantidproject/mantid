7910b0f320e3739fd6c48582a0772458e370a3ad
Dmitry Ganyushin <ganyushin@gmail.com>
2024-07-26 12:50:33 -0400

    Added kwargs to pass parameters to Compare Workspaces function.

--- Framework/PythonInterface/mantid/_testing/AssertAlmostEqual.py
+++ Framework/PythonInterface/mantid/_testing/AssertAlmostEqual.py
@@ -8,7 +8,7 @@
 from mantid.simpleapi import CompareWorkspaces
 
 
-def assert_almost_equal(Workspace1, Workspace2, rtol=0.0, atol=0.0):
+def assert_almost_equal(Workspace1, Workspace2, **kwargs):
     """
     Raises an assertion error if two workspaces are not within specified tolerance.
 
@@ -29,19 +29,24 @@ def assert_almost_equal(Workspace1, Workspace2, rtol=0.0, atol=0.0):
         If atol and rtol are both provided
 
     """
-
-    if rtol != 0.0 and atol != 0.0:
-        raise ValueError("Specify rtol or atol, not both")
+    rtol = kwargs.pop("rtol", None)
+    atol = kwargs.pop("atol", None)
     _rel = False
     tolerance = 1e-10
-    if atol:
-        tolerance = atol
 
-    if rtol:
-        tolerance = rtol
-        _rel = True
+    if rtol is not None and atol is not None:
+        raise ValueError("Specify rtol or atol, not both")
+    elif rtol in None and atol is None:
+        raise ValueError("Specify rtol or atol")
+    elif atol is not None:
+        if atol > 0.0:
+            tolerance = atol
+    elif rtol is not None:
+        if rtol > 0.0:
+            tolerance = rtol
+            _rel = True
 
-    result, message = CompareWorkspaces(Workspace1, Workspace2, Tolerance=tolerance, ToleranceRelErr=_rel)
+    result, message = CompareWorkspaces(Workspace1, Workspace2, Tolerance=tolerance, ToleranceRelErr=_rel, **kwargs)
     msg_dict = message.toDict()
     if not result:
         msg = ", ".join(msg_dict["Message"]) + f", Workspaces are not within tolerance ({tolerance})"
