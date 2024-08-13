420685074bcf704861263134884125eb8a6890d4
Ross Whitfield <whitfieldre1@ornl.gov>
2024-08-13 13:55:45 +1000

    Add test for createRagged

--- Framework/DataObjects/inc/MantidDataObjects/WorkspaceCreation.h
+++ Framework/DataObjects/inc/MantidDataObjects/WorkspaceCreation.h
@@ -178,16 +178,6 @@ std::unique_ptr<T> create(const std::shared_ptr<const Geometry::Instrument> &ins
 }
 
 template <class T, class P, typename std::enable_if<std::is_base_of<API::MatrixWorkspace, P>::value>::type * = nullptr>
-std::unique_ptr<T> create(const P &parent) {
-  const auto numHistograms = parent.getNumberHistograms();
-  auto ws = create<T>(parent, numHistograms, detail::stripData(parent.histogram(0)));
-  for (size_t i = 0; i < numHistograms; ++i) {
-    ws->setSharedX(i, parent.sharedX(i));
-  }
-  return ws;
-}
-
-template <class T, class P, typename std::enable_if<std::is_base_of<API::MatrixWorkspace, P>::value>::type * = nullptr>
 std::unique_ptr<T> createRagged(const P &parent) {
   const auto numHistograms = parent.getNumberHistograms();
 
@@ -197,6 +187,19 @@ std::unique_ptr<T> createRagged(const P &parent) {
   auto ws = create<T>(parent, numHistograms, histArg);
   for (size_t i = 0; i < numHistograms; ++i) {
     ws->resizeHistogram(i, parent.histogramSize(i));
+    ws->setSharedX(i, parent.sharedX(i));
+  }
+  return ws;
+}
+
+template <class T, class P, typename std::enable_if<std::is_base_of<API::MatrixWorkspace, P>::value>::type * = nullptr>
+std::unique_ptr<T> create(const P &parent) {
+  if (parent.isRaggedWorkspace())
+    return createRagged<T>(parent);
+
+  const auto numHistograms = parent.getNumberHistograms();
+  auto ws = create<T>(parent, numHistograms, detail::stripData(parent.histogram(0)));
+  for (size_t i = 0; i < numHistograms; ++i) {
     ws->setSharedX(i, parent.sharedX(i));
   }
   return ws;
