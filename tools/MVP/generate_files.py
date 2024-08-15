ca39054957732b78ffc2b255d758b54409abf9e4
Applin <robert.applin@stfc.ac.uk>
2024-08-15 15:39:04 +0100

    Add code to python template files

--- tools/MVP/generate_files.py
+++ tools/MVP/generate_files.py
@@ -12,7 +12,7 @@ TEMPLATE_DIRECTORY = join(dirname(abspath(__file__)), "templates")
 
 def _python_filename(file_type: str, name: Union[str, None] = None) -> str:
     """Create a filename string with a Python filename convention."""
-    return f"{name}_{file_type.lower()}" if name is not None else file_type.lower()
+    return f"{name.lower()}_{file_type.lower()}" if name is not None else file_type.lower()
 
 
 def _cpp_filename(file_type: str, name: Union[str, None] = None) -> str:
@@ -20,7 +20,25 @@ def _cpp_filename(file_type: str, name: Union[str, None] = None) -> str:
     return f"{name}{file_type}" if name is not None else file_type
 
 
-def _generate_file_from(name: str, filename: Callable, file_type: str, extension: str, output_directory: str) -> None:
+def _generate_launch_file(name: str, filename: Callable, extension: str, output_directory: str) -> None:
+    """Generates a file which can be used to launch the generated MVP widget."""
+    template_filepath = join(TEMPLATE_DIRECTORY, f"launch.{extension}.in")
+    with open(template_filepath, mode="r") as file:
+        content = file.read()
+
+    content = content.replace("Model", f"{name}Model")
+    content = content.replace("View", f"{name}View")
+    content = content.replace("Presenter", f"{name}Presenter")
+    content = content.replace("model", filename("Model", name))
+    content = content.replace("view", filename("View", name))
+    content = content.replace("presenter", filename("Presenter", name))
+
+    output_filepath = join(output_directory, f"launch.{extension}")
+    with open(output_filepath, mode="w") as file:
+        file.write(content)
+
+
+def _generate_mvp_file(name: str, filename: Callable, file_type: str, extension: str, output_directory: str) -> None:
     """Generates a file using the corresponding template in the template directory."""
     template_filepath = join(TEMPLATE_DIRECTORY, f"{filename(file_type)}.{extension}.in")
     with open(template_filepath, mode="r") as file:
@@ -36,20 +54,29 @@ def _generate_file_from(name: str, filename: Callable, file_type: str, extension
 def _generate_python_files(name: str, output_directory: str) -> None:
     """Generate MVP files for a Python use case."""
     print("Generating Python files with an MVP pattern...")
-    _generate_file_from(name, _python_filename, "View", "py", output_directory)
-    _generate_file_from(name, _python_filename, "Presenter", "py", output_directory)
-    _generate_file_from(name, _python_filename, "Model", "py", output_directory)
+
+    _generate_mvp_file(name, _python_filename, "View", "py", output_directory)
+    _generate_mvp_file(name, _python_filename, "Presenter", "py", output_directory)
+    _generate_mvp_file(name, _python_filename, "Model", "py", output_directory)
+    _generate_launch_file(name, _python_filename, "py", output_directory)
+
+    print(f"Output directory: {output_directory}")
+    print("Done!")
 
 
 def _generate_cpp_files(name: str, output_directory: str) -> None:
     """Generate MVP files for a C++ use case."""
     print("Generating C++ files with an MVP pattern...")
-    _generate_file_from(name, _cpp_filename, "View", "cpp", output_directory)
-    _generate_file_from(name, _cpp_filename, "Presenter", "cpp", output_directory)
-    _generate_file_from(name, _cpp_filename, "Model", "cpp", output_directory)
-    _generate_file_from(name, _cpp_filename, "View", "h", output_directory)
-    _generate_file_from(name, _cpp_filename, "Presenter", "h", output_directory)
-    _generate_file_from(name, _cpp_filename, "Model", "h", output_directory)
+
+    _generate_mvp_file(name, _cpp_filename, "View", "cpp", output_directory)
+    _generate_mvp_file(name, _cpp_filename, "Presenter", "cpp", output_directory)
+    _generate_mvp_file(name, _cpp_filename, "Model", "cpp", output_directory)
+    _generate_mvp_file(name, _cpp_filename, "View", "h", output_directory)
+    _generate_mvp_file(name, _cpp_filename, "Presenter", "h", output_directory)
+    _generate_mvp_file(name, _cpp_filename, "Model", "h", output_directory)
+
+    print(f"Output directory: {output_directory}")
+    print("Done!")
 
 
 def _generate_files(name: str, language: str, output_directory: str) -> None:
