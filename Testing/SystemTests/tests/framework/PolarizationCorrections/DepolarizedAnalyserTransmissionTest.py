ac9e06d57c0e594c4e463311b0b76f9427612d76
Caila Finn <caila.finn@stfc.ac.uk>
2024-08-28 16:01:52 +0100

    Add Boilerplate system test code
    RE #37120

---
+++ Testing/SystemTests/tests/framework/PolarizationCorrections/DepolarizedAnalyserTransmissionTest.py
@@ -0,0 +1,19 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import systemtesting
+
+from mantid import *
+from mantid.simpleapi import *
+
+
+class DepolarizedAnalyzerTransmissionTest(systemtesting.MantidSystemTest):
+    def runTest(self):
+        pass
+
+    def validate(self):
+        pass
