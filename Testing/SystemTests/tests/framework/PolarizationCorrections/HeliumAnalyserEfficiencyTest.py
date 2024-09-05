5192ac9a6fcea4df5d6f9df09a49a78e73672bce
Caila Finn <caila.finn@stfc.ac.uk>
2024-09-05 16:17:44 +0100

    Add polarized test
    RE #37225

--- Testing/SystemTests/tests/framework/PolarizationCorrections/HeliumAnalyserEfficiencyTest.py
+++ Testing/SystemTests/tests/framework/PolarizationCorrections/HeliumAnalyserEfficiencyTest.py
@@ -12,9 +12,27 @@ from PolarizationCorrectionsBase import PolarizationCorrectionsBase
 class HeliumAnalyserEfficiencyTest(PolarizationCorrectionsBase):
     def __init__(self):
         PolarizationCorrectionsBase.__init__(self)
+        self.tolerance = 1e-5
 
     def _run_test(self):
-        pass
+        run = Load("/Users/caila.finn/MantidData/PolSANS/HeliumAnalyser/ZOOM00038249.nxs")
+
+        pre_processed = self._prepare_workspace(run)
+
+        HeliumAnalyserEfficiency(
+            pre_processed, "00,10,11,01", OutputWorkspace="efficiency", OutputFitCurves="curves", OutputFitParameters="params"
+        )
 
     def _validate(self):
-        pass
+        result_eff = "efficiency"
+        reference_eff = "/Users/caila.finn/MantidData/PolSANS/HeliumAnalyser/HeliumAnalyserEfficiencyReference.nxs"
+        result_curves = "curves"
+        reference_curves = "/Users/caila.finn/MantidData/PolSANS/HeliumAnalyser/HeliumAnalyserCurvesReference.nxs"
+        result_params = "params"
+        reference_params = "/Users/caila.finn/MantidData/PolSANS/HeliumAnalyser/HeliumAnalyserParamsReference.nxs"
+
+        is_efficiency_match = self._validate_workspace(result_eff, reference_eff)
+        is_curves_match = self._validate_workspace(result_curves, reference_curves)
+        is_params_match = self._validate_workspace(result_params, reference_params)
+
+        return is_efficiency_match and is_curves_match and is_params_match
