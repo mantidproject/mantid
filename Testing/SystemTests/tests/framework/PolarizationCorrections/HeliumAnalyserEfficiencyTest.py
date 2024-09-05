b45182b09f064e94b54e7c97279d48f2a6b45108
Caila Finn <caila.finn@stfc.ac.uk>
2024-09-05 11:50:23 +0100

    Implement boilerplate systest code
    RE #37225

---
+++ Testing/SystemTests/tests/framework/PolarizationCorrections/HeliumAnalyserEfficiencyTest.py
@@ -0,0 +1,23 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import systemtesting
+
+from mantid.api import AnalysisDataService
+from mantid.simpleapi import *
+
+
+class HeliumAnalyserEfficiencyTest(systemtesting.MantidSystemTest):
+
+    def runTest(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def cleanup(self):
+        AnalysisDataService.clear()
