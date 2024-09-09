adfaa86a88c5d5e209d33383673d3b64f2ed7555
rboston628 <bostonsr@ornl.gov>
2024-09-09 11:15:58 -0400

    fix IsisIndirectInelestic.OSIRISIqtAndIqtFitMulti to consistently pass

--- Testing/SystemTests/tests/framework/ISISIndirectInelastic.py
+++ Testing/SystemTests/tests/framework/ISISIndirectInelastic.py
@@ -1003,18 +1003,15 @@ class OSIRISIqtAndIqtFitMulti(ISISIndirectInelasticIqtAndIqtFitMulti):
         self.spec_max = 41
 
     def skipTests(self):
-        # In its current state this test is not fit for purpose. There is a large amount of randomness present such
-        # that a very very wide relative tolerance is required to pass reliably (50+). This was discovered after
-        # a change to CompareWorkspaces that made the comparison actually relative. Prior, this test was actually
-        # using an absolute tolerance of 5.0. A redesign is needed to remove some of the randomness or determine
-        # the actual required tolerance.
-        return True
+        # This test was once giving issues.  It seems the failures were caused by the large fluctuations in the
+        # uncertainties (i.e. delta delta y) due to the Monte Carlo calculation.  Turning off the check of uncertainity
+        # should fix this test, combined with a reasonable absolute tolerance of 0.05.
+        return False
 
     def get_reference_files(self):
-        # Relative tolerance is used because the calculation of Monte Carlo errors means the Iqt errors are randomized
-        # within a set amount
-        self.tolerance = 5.0
-        self.tolerance_is_rel_err = True
+        self.tolerance = 0.05
+        self.tolerance_is_rel_err = False
+        self.disableChecking = ["Uncertainty"]
         return ["II.OSIRISIqt.nxs", "II.OSIRISIqtFitMulti.nxs"]
 
 
