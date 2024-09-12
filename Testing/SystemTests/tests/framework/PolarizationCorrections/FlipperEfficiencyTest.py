599fd94001ab5bf46b9692adf80ba5e3724593be
Caila Finn <caila.finn@stfc.ac.uk>
2024-09-12 12:38:17 +0100

    Add unpolarized test
    Also adds some inheritance to avoid repeats.
    
    RE #37306
    
    Co-authored-by: Yusuf Jimoh <yusufjimoh@users.noreply.github.com>

--- Testing/SystemTests/tests/framework/PolarizationCorrections/FlipperEfficiencyTest.py
+++ Testing/SystemTests/tests/framework/PolarizationCorrections/FlipperEfficiencyTest.py
@@ -7,17 +7,52 @@
 
 from mantid.simpleapi import FlipperEfficiency
 from SANSPolarizationCorrectionsBase import SANSPolarizationCorrectionsBase
+from abc import abstractmethod
 
 
-class FlipperEfficiencyTest(SANSPolarizationCorrectionsBase):
+class FlipperEfficiencyTestBase(SANSPolarizationCorrectionsBase):
     def __init__(self):
         SANSPolarizationCorrectionsBase.__init__(self)
 
+    @property
+    @abstractmethod
+    def reference_basename(self):
+        """
+        The algorithm outputs one workspace. This value is the string that precedes the workspace's reference file.
+        :return: The prefix the reference file is preceded by for the given test.
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def input_filename(self):
+        """
+        The filename to use as the input to the FlipperEfficiency algorithm in this test.
+        :return: The input workspace's filename.
+        """
+        pass
+
     def _run_test(self):
-        pre_processed = self._prepare_workspace("ZOOM00038249.nxs")
+        pre_processed = self._prepare_workspace(self.input_filename)
         FlipperEfficiency(pre_processed, "00,10,11,01", OutputWorkspace="result")
 
     def _validate(self):
         result = "result"
-        reference = "FlipperEfficiencyReference.nxs"
+        reference = f"{self.reference_basename}Reference.nxs"
         return result, reference
+
+
+class FlipperEfficiencyPolarisedTest(FlipperEfficiencyTestBase):
+    reference_basename = "FlipperEfficiency"
+    input_filename = "ZOOM00038249.nxs"
+
+    def __init__(self):
+        FlipperEfficiencyTestBase.__init__(self)
+
+
+class FlipperEfficiencyUnpolarisedTest(FlipperEfficiencyTestBase):
+    reference_basename = "UnpolFlipperEfficiency"
+    input_filename = "ZOOM00038253.nxs"
+
+    def __init__(self):
+        FlipperEfficiencyTestBase.__init__(self)
