9683d81ebe9a79f503e01ac82763f58edf3ac7a2
Applin <robert.applin@stfc.ac.uk>
2024-09-19 13:17:49 +0100

    Refactor ElasticWindowMultiple system test to re-use code

--- Testing/SystemTests/tests/framework/ElasticWindowMultipleTest.py
+++ Testing/SystemTests/tests/framework/ElasticWindowMultipleTest.py
@@ -9,51 +9,44 @@ from systemtesting import MantidSystemTest
 from mantid.simpleapi import ElasticWindowMultiple, GroupWorkspaces, Load
 
 
-class ElasticWindowMultipleISISTest(MantidSystemTest):
-    def runTest(self):
-        Load(Filename="irs26176_graphite002_red.nxs", OutputWorkspace="irs26176_graphite002_red", LoadHistory=False)
-        GroupWorkspaces(InputWorkspaces="irs26176_graphite002_red", OutputWorkspace="Elwin_Input")
+class ElasticWindowMultipleBase:
+    def _run(self):
+        Load(Filename=f"{self._name}.nxs", OutputWorkspace=self._name, LoadHistory=False)
+        GroupWorkspaces(InputWorkspaces=self._name, OutputWorkspace="Elwin_Input")
         ElasticWindowMultiple(
             InputWorkspaces="Elwin_Input",
-            IntegrationRangeStart=-0.54760000000000009,
-            IntegrationRangeEnd=0.54410000000000014,
-            OutputInQ="irs26176_graphite002_red_elwin_eq",
-            OutputInQSquared="irs26176_graphite002_red_elwin_eq2",
-            OutputELF="irs26176_graphite002_red_elwin_elf",
+            IntegrationRangeStart=self._range_start,
+            IntegrationRangeEnd=self._range_end,
+            OutputInQ=f"{self._name}_elwin_eq",
+            OutputInQSquared=f"{self._name}_elwin_eq2",
+            OutputELF=f"{self._name}_elwin_elf",
         )
 
-    def validate(self):
+    def _validate(self):
         self.tolerance = 1e-10
         return (
-            "irs26176_graphite002_red_elwin_eq",
-            "irs26176_graphite002_red_elwin_eq.nxs",
-            "irs26176_graphite002_red_elwin_eq2",
-            "irs26176_graphite002_red_elwin_eq2.nxs",
-            "irs26176_graphite002_red_elwin_elf",
-            "irs26176_graphite002_red_elwin_elf.nxs",
+            f"{self._name}_elwin_eq",
+            f"{self._name}_elwin_eq.nxs",
+            f"{self._name}_elwin_eq2",
+            f"{self._name}_elwin_eq2.nxs",
+            f"{self._name}_elwin_elf",
+            f"{self._name}_elwin_elf.nxs",
         )
 
 
-class ElasticWindowMultipleILLTest(MantidSystemTest):
-    def runTest(self):
-        Load(Filename="243489-243506ElwinHeat_H2O-HSA-stw.nxs", OutputWorkspace="243489-243506ElwinHeat_H2O-HSA-stw", LoadHistory=False)
-        GroupWorkspaces(InputWorkspaces="243489-243506ElwinHeat_H2O-HSA-stw", OutputWorkspace="Elwin_Input")
-        ElasticWindowMultiple(
-            InputWorkspaces="Elwin_Input",
-            IntegrationRangeStart=-4.9999900000000004,
-            IntegrationRangeEnd=2.4999899999999999,
-            OutputInQ="243489-243506ElwinHeat_H2O-HSA-stw_elwin_eq",
-            OutputInQSquared="243489-243506ElwinHeat_H2O-HSA-stw_elwin_eq2",
-            OutputELF="243489-243506ElwinHeat_H2O-HSA-stw_elwin_elf",
-        )
+class ElasticWindowMultipleISISDataTest(MantidSystemTest, ElasticWindowMultipleBase):
+    _name = "irs26176_graphite002_red"
+    _range_start = -0.54760000000000009
+    _range_end = 0.54410000000000014
 
-    def validate(self):
-        self.tolerance = 1e-10
-        return (
-            "243489-243506ElwinHeat_H2O-HSA-stw_elwin_eq",
-            "243489-243506ElwinHeat_H2O-HSA-stw_elwin_eq.nxs",
-            "243489-243506ElwinHeat_H2O-HSA-stw_elwin_eq2",
-            "243489-243506ElwinHeat_H2O-HSA-stw_elwin_eq2.nxs",
-            "243489-243506ElwinHeat_H2O-HSA-stw_elwin_elf",
-            "243489-243506ElwinHeat_H2O-HSA-stw_elwin_elf.nxs",
-        )
+    runTest = ElasticWindowMultipleBase._run
+    validate = ElasticWindowMultipleBase._validate
+
+
+class ElasticWindowMultipleILLDataTest(MantidSystemTest, ElasticWindowMultipleBase):
+    _name = "243489-243506ElwinHeat_H2O-HSA-stw"
+    _range_start = -4.9999900000000004
+    _range_end = 2.4999899999999999
+
+    runTest = ElasticWindowMultipleBase._run
+    validate = ElasticWindowMultipleBase._validate
