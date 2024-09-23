33744bf98dd42b2dcaa1f75053f20344e6234f3e
Kevin A. Tactac <tactacka@ornl.gov>
2024-09-23 11:35:46 -0400

    Improve Unit Test

--- Framework/PythonInterface/test/python/mantid/dataobjects/WorkspaceValidatorsTest.py
+++ Framework/PythonInterface/test/python/mantid/dataobjects/WorkspaceValidatorsTest.py
@@ -13,7 +13,8 @@ import testhelpers
 from mantid.dataobjects import TableWorkspaceNotEmptyValidator
 
 from mantid.simpleapi import CreateEmptyTableWorkspace
-from mantid.api import ITableWorkspace
+from mantid.api import ITableWorkspace, PythonAlgorithm, WorkspaceProperty
+from mantid.kernel import Direction
 
 
 class WorkspaceValidatorsTest(unittest.TestCase):
@@ -21,24 +22,48 @@ class WorkspaceValidatorsTest(unittest.TestCase):
     def test_TableWorkspaceNotEmptyValidator_construction(self):
         """
         Test that the TableWorkspaceNotEmptyValidator can be constructed
-        and detect an empty TableWorkspace
         """
 
+        # test the validator can be constructed without error
+        testhelpers.assertRaisesNothing(self, TableWorkspaceNotEmptyValidator)
+
+    def test_TableWorkspaceNotEmptyValidator_usage(self):
+
+        class TestAlgorithm(PythonAlgorithm):
+            """Mock an extended algorithm that uses TableWorkspaceNotEmptyValidator"""
+
+            def PyInit(self):
+                self.declareProperty(
+                    WorkspaceProperty(
+                        name="InputWorkspace", defaultValue="", direction=Direction.Input, validator=TableWorkspaceNotEmptyValidator()
+                    )
+                )
+
+            def PyExec(self):
+                pass
+
+        # setup the algorithm
+        alg = TestAlgorithm()
+        alg.PyInit()
+
+        # setup the table workspace
         tableWS = CreateEmptyTableWorkspace()
 
-        # test the validator can be constructed
-        testhelpers.assertRaisesNothing(self, TableWorkspaceNotEmptyValidator)
+        # test the validator rejects an 0-column 0-row TableWorkspace
+        self.assertRaises(ValueError, alg.setProperty, "InputWorkspace", tableWS)
 
-        # test the validator rejects an empty TableWorkspace
-        self.assertRaises(Exception, TableWorkspaceNotEmptyValidator, tableWS)
+        # test the validator rejects a n-column, 0-row TableWorkspace
+        tableWS.addColumn("int", "values0")
+        self.assertRaises(ValueError, alg.setProperty, "InputWorkspace", tableWS)
 
-        # thest the validator accepts a filled TableWorkspace
-        tableWS.addColumn("int", "values")
-        tableWS.addRow([1])
-        tableWS.addRow([2])
-        tableWS.addRow([3])
+        tableWS.addColumn("int", "values1")
+        tableWS.addColumn("int", "values2")
+        self.assertRaises(ValueError, alg.setProperty, "InputWorkspace", tableWS)
 
-        testhelpers.assertRaisesNothing(self, TableWorkspaceNotEmptyValidator)
+        # test the validator accepts a filled TableWorkspace
+        tableWS.addRow([1, 2, 3])
+        tableWS.addRow([4, 5, 6])
+        testhelpers.assertRaisesNothing(self, alg.setProperty, "InputWorkspace", tableWS)
 
 
 if __name__ == "__main__":
