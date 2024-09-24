66bcc079257365bfbc3eb11dd2028e5f32e55eff
Dmitry Ganyushin <ganyushin@gmail.com>
2024-09-24 11:49:45 -0400

    Stubs

---
+++ Framework/Kernel/inc/MantidKernel/EnumStringProperty.h
@@ -0,0 +1,23 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidKernel/DllConfig.h"
+#include "MantidKernel/EnumeratedString.h"
+
+namespace Mantid {
+namespace Kernel {
+
+/** EnumStringProperty : TODO: DESCRIPTION
+ */
+class MANTID_KERNEL_DLL EnumStringProperty {
+public:
+  EnumString();
+};
+
+} // namespace Kernel
+} // namespace Mantid
