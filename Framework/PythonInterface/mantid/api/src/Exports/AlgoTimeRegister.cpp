4cf3bf63f699a552b1ae7f56df6ae2eee1659ccf
Patrou, Maria <patroum@ornl.gov>
2024-10-03 13:07:38 -0400

    changes in progress

--- Framework/PythonInterface/mantid/api/src/Exports/AlgoTimeRegister.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/AlgoTimeRegister.cpp
@@ -47,7 +47,7 @@ void addTimeWrapper(const std::string &name, long int begin, long int end) {
   std::chrono::time_point<std::chrono::high_resolution_clock> tp_begin_ns(begin_ns);
 
   // Print the time point
-  std::cout << "Begin Time point : " << tp_begin_ns.time_since_epoch().count() << " ns" << std::endl;
+  std::cout << "C++ Begin Time point : " << tp_begin_ns.time_since_epoch().count() << " ns" << std::endl;
 
   // Convert milliseconds to nanoseconds
   std::chrono::nanoseconds end_ns(end); // duration_cast<std::chrono::nanoseconds>(ms);
@@ -56,7 +56,7 @@ void addTimeWrapper(const std::string &name, long int begin, long int end) {
   std::chrono::time_point<std::chrono::high_resolution_clock> tp_end_ns(end_ns);
 
   // Print the time point
-  std::cout << "End Time point: " << tp_end_ns.time_since_epoch().count() << " ns" << std::endl;
+  std::cout << "C++ End Time point: " << tp_end_ns.time_since_epoch().count() << " ns" << std::endl;
 
   // Calculate the duration in nanoseconds
   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(tp_end_ns - tp_begin_ns);
