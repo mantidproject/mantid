646e4a1e1029579f0c802fb4702264464d850d3a
Patrou, Maria <patroum@ornl.gov>
2024-10-04 16:05:01 -0400

    threading test added

--- Framework/PythonInterface/test/python/mantid/api/AlgoRegisterTest.py
+++ Framework/PythonInterface/test/python/mantid/api/AlgoRegisterTest.py
@@ -12,7 +12,7 @@ import os
 from mantid.kernel import amend_config
 from mantid.api import AlgoTimeRegister
 from mantid.simpleapi import CreateSampleWorkspace
-
+import threading
 
 
 def get_recorded_timedata(filename):
@@ -20,6 +20,8 @@ def get_recorded_timedata(filename):
         timeentry = f.readlines()[-1].replace("\n","").split(",")
         recorded_start_time = recorded_end_time = -1
         entry_name = ""
+        print("time entry", timeentry)
+
         for value in timeentry:
             timedata = value.strip()
             if timedata.startswith("StartTime"):
@@ -28,9 +30,11 @@ def get_recorded_timedata(filename):
                 recorded_end_time = int(timedata.replace("EndTime=",""))
             if timedata.startswith("AlgorithmName"):
                 entry_name = timedata.replace("AlgorithmName=","")
-            if (recorded_start_time!= -1 and recorded_end_time !=-1 and entry_name!=""):
+            if timedata.startswith("ThreadID"):
+                thread_id = int(timedata.replace("ThreadID=",""))
+            if (recorded_start_time!= -1 and recorded_end_time !=-1 and entry_name!="" and thread_id!=""):
                 break
-    return recorded_start_time, recorded_end_time, entry_name
+    return recorded_start_time, recorded_end_time, entry_name, thread_id
 
 class AlgoTimeRegisterTest(unittest.TestCase):
 
@@ -46,13 +50,12 @@ class AlgoTimeRegisterTest(unittest.TestCase):
             time.sleep(3.009)
             end_time = time.time_ns()
             duration = end_time-start_time
-            print("durartion", duration)
             AlgoTimeRegister.addTime(entry_name,start_time,end_time)
-            rec_start_time,rec_end_time, rec_entry_name = get_recorded_timedata(self.PER_FILE.name)
+            rec_start_time,rec_end_time, rec_entry_name, rec_thread_id = get_recorded_timedata(self.PER_FILE.name)
             rec_duration = rec_end_time - rec_start_time
             self.assertEqual(duration, rec_duration)
             self.assertEqual(entry_name, rec_entry_name)
-
+            self.assertEqual(rec_thread_id,threading.get_ident())
 
     def test_addTime_timeentry2(self):
         performance_config = {"performancelog.write": "On", "performancelog.filename": self.PER_FILE.name}
@@ -65,10 +68,11 @@ class AlgoTimeRegisterTest(unittest.TestCase):
             end_time = time.time_ns()
             duration = end_time-start_time
             AlgoTimeRegister.addTime(entry_name,start_time,end_time)
-            rec_start_time,rec_end_time, rec_entry_name = get_recorded_timedata(self.PER_FILE.name)
+            rec_start_time,rec_end_time, rec_entry_name, rec_thread_id = get_recorded_timedata(self.PER_FILE.name)
             rec_duration = rec_end_time - rec_start_time
             self.assertEqual(duration, rec_duration)
             self.assertEqual(entry_name, rec_entry_name)
+            self.assertEqual(rec_thread_id,threading.get_ident())
 
     def ttest_addTime_timeentry3(self):
         performance_config = {"performancelog.write": "On", "performancelog.filename": self.PER_FILE.name}
@@ -81,12 +85,13 @@ class AlgoTimeRegisterTest(unittest.TestCase):
             end_time = time.time_ns()
             duration = end_time-start_time
             AlgoTimeRegister.addTime(entry_name,start_time,end_time)
-            rec_start_time,rec_end_time, rec_entry_name = get_recorded_timedata(self.PER_FILE.name)
+            rec_start_time,rec_end_time, rec_entry_name, rec_thread_id = get_recorded_timedata(self.PER_FILE.name)
             rec_duration = rec_end_time - rec_start_time
             self.assertEqual(duration, rec_duration)
             self.assertEqual(entry_name, rec_entry_name)
             self.assertEqual(rec_start_time, rec_start_time)
             self.assertEqual(end_time, end_time)
+            self.assertEqual(rec_thread_id,threading.get_ident())
 
     def test_addTime_timeentry4(self):
         performance_config = {"performancelog.write": "On", "performancelog.filename": self.PER_FILE.name}
@@ -99,12 +104,13 @@ class AlgoTimeRegisterTest(unittest.TestCase):
             end_time = time.time_ns()
             duration = end_time-start_time
             AlgoTimeRegister.addTime(entry_name,start_time,end_time)
-            rec_start_time,rec_end_time, rec_entry_name = get_recorded_timedata(self.PER_FILE.name)
+            rec_start_time,rec_end_time, rec_entry_name, rec_thread_id = get_recorded_timedata(self.PER_FILE.name)
             rec_duration = rec_end_time - rec_start_time
             self.assertEqual(duration, rec_duration)
             self.assertEqual(rec_start_time, rec_start_time)
             self.assertEqual(end_time, end_time)
             self.assertEqual(entry_name, rec_entry_name)
+            self.assertEqual(rec_thread_id,threading.get_ident())
 
     def test_addTime_algo(self):
         performance_config = {"performancelog.write": "On", "performancelog.filename": self.PER_FILE.name}
@@ -114,8 +120,19 @@ class AlgoTimeRegisterTest(unittest.TestCase):
             entry_name = "CreateSampleWorkspace"
             # algotimeregister is initialized through algorithm class
             ws = CreateSampleWorkspace()
-            rec_start_time,rec_end_time, rec_entry_name = get_recorded_timedata(self.PER_FILE.name)
+            rec_start_time,rec_end_time, rec_entry_name, rec_thread_id = get_recorded_timedata(self.PER_FILE.name)
             self.assertEqual(entry_name, rec_entry_name)
+            self.assertEqual(rec_thread_id,threading.get_ident())
+
+    def test_addTime_threading(self):
+        performance_config = {"performancelog.write": "On", "performancelog.filename": self.PER_FILE.name}
+        with amend_config(**performance_config):
+
+            # time entry
+            # call the above algorithm from another thread to verfy the thread entry is correct
+            x = threading.Thread(target=self.test_addTime_algo)
+            x.start()
+            x.join()
 
 
 if __name__ == "__main__":
