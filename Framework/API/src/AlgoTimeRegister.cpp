3f0ff582567bbffd633c40a95095b5d5143498e9
Carson Sears <searscr@ornl.gov>
2024-10-04 10:47:46 -0500

    update to write as we go

--- Framework/API/src/AlgoTimeRegister.cpp
+++ Framework/API/src/AlgoTimeRegister.cpp
@@ -27,16 +27,7 @@ AlgoTimeRegisterImpl::Dump::Dump(const std::string &nm)
 
 AlgoTimeRegisterImpl::Dump::~Dump() {
   const time_point_ns regFinish = std::chrono::high_resolution_clock::now();
-  {
-    // auto atr = ;
-    std::lock_guard<std::mutex> lock(AlgoTimeRegister::Instance().m_mutex);
-    AlgoTimeRegister::Instance().addTime(m_name, std::this_thread::get_id(), m_regStart_chrono, regFinish);
-  }
-}
-
-void AlgoTimeRegisterImpl::addTime(const std::string &name, const std::thread::id thread_id,
-                                   const Kernel::time_point_ns &begin, const Kernel::time_point_ns &end) {
-  m_info.emplace_back(name, thread_id, begin, end);
+  { AlgoTimeRegister::Instance().addTime(m_name, std::this_thread::get_id(), m_regStart_chrono, regFinish); }
 }
 
 void AlgoTimeRegisterImpl::addTime(const std::string &name, const Kernel::time_point_ns &begin,
@@ -44,30 +35,51 @@ void AlgoTimeRegisterImpl::addTime(const std::string &name, const Kernel::time_p
   AlgoTimeRegister::Instance().addTime(name, std::this_thread::get_id(), begin, end);
 }
 
-void AlgoTimeRegisterImpl::test(const int a, const int b) { printf("a + b = %d\n", a + b); }
+void AlgoTimeRegisterImpl::addTime(const std::string &name, const std::thread::id thread_id,
+                                   const Kernel::time_point_ns &begin, const Kernel::time_point_ns &end) {
+  if (m_writeToFile) {
+    std::lock_guard<std::mutex> lock(AlgoTimeRegister::Instance().m_mutex);
+    std::fstream fs;
+    fs.open(m_filename, std::ios::out | std::ios::app);
+    if (fs.is_open()) {
+      const std::chrono::nanoseconds st = begin - m_start;
+      const std::chrono::nanoseconds fi = end - m_start;
+      fs << "ThreadID=" << thread_id << ", AlgorithmName=" << name << ", StartTime=" << st.count()
+         << ", EndTime=" << fi.count() << "\n";
 
-AlgoTimeRegisterImpl::AlgoTimeRegisterImpl() : m_start(std::chrono::high_resolution_clock::now()) {}
-
-AlgoTimeRegisterImpl::~AlgoTimeRegisterImpl() {
-  std::fstream fs;
-  auto writeEnable = Kernel::ConfigService::Instance().getValue<bool>("performancelog.write").value();
-  auto filename = Kernel::ConfigService::Instance().getString("performancelog.filename");
-  if (!writeEnable || filename.empty()) {
-    LOGGER().information() << "performancelog.write is disabled (off/0/false) or performancelog.filename " << filename
-                           << " is an invalid filename\n";
-    return;
-  }
-  fs.open(filename, std::ios::out);
-  // c++20 has an implementation of operator<<
-  fs << "START_POINT: " << std::chrono::duration_cast<std::chrono::nanoseconds>(m_start.time_since_epoch()).count()
-     << " MAX_THREAD: " << PARALLEL_GET_MAX_THREADS << "\n";
-  for (auto &elem : m_info) {
-    const std::chrono::nanoseconds st = elem.m_begin - m_start;
-    const std::chrono::nanoseconds fi = elem.m_end - m_start;
-    fs << "ThreadID=" << elem.m_threadId << ", AlgorithmName=" << elem.m_name << ", StartTime=" << st.count()
-       << ", EndTime=" << fi.count() << "\n";
+      fs.close();
+    }
   }
 }
+
+AlgoTimeRegisterImpl::AlgoTimeRegisterImpl() : m_start(std::chrono::high_resolution_clock::now()) {
+  auto writeEnable = Kernel::ConfigService::Instance().getValue<bool>("performancelog.write").value();
+  if (!writeEnable) {
+    LOGGER().information() << "performancelog.write is disabled (off/0/false)\n";
+  }
+  auto filename = Kernel::ConfigService::Instance().getString("performancelog.filename");
+  if (filename.empty()) {
+    LOGGER().information() << "performancelog.filename is empty, please provide valid filename\n";
+  }
+  m_filename = filename;
+  m_writeToFile = true;
+  LOGGER().information() << "Performance log file: " << m_filename << '\n';
+  std::fstream fs;
+
+  // Open the file in truncation mode (ios::trunc)
+  fs.open(m_filename, std::ios::out);
+
+  if (fs.is_open()) {
+    fs << "START_POINT: " << std::chrono::duration_cast<std::chrono::nanoseconds>(m_start.time_since_epoch()).count()
+       << " MAX_THREAD: " << PARALLEL_GET_MAX_THREADS << "\n";
+    fs.close();
+  } else {
+    LOGGER().notice() << "Failed to open the file, timing will not write to file.\n";
+    m_writeToFile = false;
+  }
+}
+
+AlgoTimeRegisterImpl::~AlgoTimeRegisterImpl() {}
 
 } // namespace Instrumentation
 } // namespace Mantid
