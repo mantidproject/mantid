df09afe2562a98fc7600006972bcd4b710e17463
Applin <robert.applin@stfc.ac.uk>
2024-10-15 14:27:21 +0100

    Fix wild imports in algorithms

--- Framework/PythonInterface/plugins/algorithms/SANSWideAngleCorrection.py
+++ Framework/PythonInterface/plugins/algorithms/SANSWideAngleCorrection.py
@@ -5,8 +5,8 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 # pylint: disable=no-init,invalid-name
-from mantid.api import *
-from mantid.kernel import *
+from mantid.api import AlgorithmFactory, MatrixWorkspaceProperty, PythonAlgorithm, Progress, WorkspaceFactory
+from mantid.kernel import Direction
 import sys
 import numpy as np
 
