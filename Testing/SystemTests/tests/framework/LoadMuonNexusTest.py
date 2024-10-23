c79b738c0b755a9dc99062a2d0eec11124e37500
Applin <robert.applin@stfc.ac.uk>
2024-10-23 14:09:00 +0100

    Add test for _v1 muon nexus files

--- Testing/SystemTests/tests/framework/LoadMuonNexusTest.py
+++ Testing/SystemTests/tests/framework/LoadMuonNexusTest.py
@@ -48,7 +48,7 @@ class LoadHDF4NexusFromMUSR(MantidSystemTest):
         self.assertDelta(0.656, first_good_data, 0.0001)
 
 
-class LoadHDF4AndHDF5NexusFromMUSR(MantidSystemTest):
+class LoadHDF4AndHDF5NexusWhenUsingV2Suffix(MantidSystemTest):
     """
     In cycle 22_4, a HDF4 format file for each run was generated and named 'MUSR00087008.nxs',
     and a HDF5 format file was generated and named 'MUSR00087008.nxs_v2' (i.e. the HDF5 file
@@ -61,6 +61,34 @@ class LoadHDF4AndHDF5NexusFromMUSR(MantidSystemTest):
     def runTest(self):
         output_hdf4 = Load(Filename="MUSR00087008.nxs", StoreInADS=False)
         output_hdf5 = Load(Filename="MUSR00087008_v2.nxs", StoreInADS=False)
+
+        workspace_hdf4 = output_hdf4[0]
+        self.assertEquals("LoadMuonNexus", get_loader_name(workspace_hdf4))
+        workspace_hdf5 = output_hdf5[0]
+        self.assertEquals("LoadMuonNexusV2", get_loader_name(workspace_hdf5))
+
+        first_good_data_hdf4 = output_hdf4[3]
+        self.assertDelta(0.56, first_good_data_hdf4, 0.0001)
+        first_good_data_hdf5 = output_hdf5[3]
+        self.assertDelta(0.56, first_good_data_hdf5, 0.0001)
+
+        result, _ = CompareWorkspaces(Workspace1=workspace_hdf4, Workspace2=workspace_hdf5, Tolerance=1e-5)
+        self.assertTrue(result)
+
+
+class LoadHDF4AndHDF5NexusWhenUsingV1Suffix(MantidSystemTest):
+    """
+    In cycle 24_2, a HDF4 format file for each run was generated and named 'MUSR00093260.nxs_v1',
+    and a HDF5 format file was generated and named 'MUSR00093260.nxs' (i.e. the HDF4 file
+    was suffixed with _v1). The HDF5 file would be loaded by default when specifying a run
+    number. This test checks that both these files can be loaded, and that the loaded workspaces
+    are equivalent. Note that I have changed the position of '_v1' in this test so it is possible
+    to load the HDF5 file.
+    """
+
+    def runTest(self):
+        output_hdf4 = Load(Filename="MUSR00093260_v1.nxs", StoreInADS=False)
+        output_hdf5 = Load(Filename="MUSR00093260.nxs", StoreInADS=False)
 
         workspace_hdf4 = output_hdf4[0]
         self.assertEquals("LoadMuonNexus", get_loader_name(workspace_hdf4))
