a9c755d40d419c31ca766bc7e8625384494eeccd
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2024-10-24 10:34:34 +0100

    Refactor setOutputPlotOptionsPresenter to create model and pass it to the construtor of the OutputPlotOptionsPresenter

--- qt/scientific_interfaces/Indirect/Simulation/SimulationTab.cpp
+++ qt/scientific_interfaces/Indirect/Simulation/SimulationTab.cpp
@@ -14,8 +14,13 @@ SimulationTab::SimulationTab(QWidget *parent) : InelasticTab(parent) {}
 
 SimulationTab::~SimulationTab() = default;
 
-void SimulationTab::setOutputPlotOptionsPresenter(std::unique_ptr<OutputPlotOptionsPresenter> presenter) {
-  m_plotOptionsPresenter = std::move(presenter);
+void SimulationTab::setOutputPlotOptionsPresenter(
+    IOutputPlotOptionsView *view, PlotWidget const &plotType, std::string const &fixedIndices,
+    std::optional<std::map<std::string, std::string>> const &availableActions) {
+  auto OutputOptionsModel =
+      std::make_unique<OutputPlotOptionsModel>(std::make_unique<ExternalPlotter>(), availableActions);
+  m_plotOptionsPresenter =
+      std::make_unique<OutputPlotOptionsPresenter>(view, std::move(OutputOptionsModel), plotType, fixedIndices);
 }
 
 void SimulationTab::setOutputPlotOptionsWorkspaces(std::vector<std::string> const &outputWorkspaces) {
