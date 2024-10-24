b91adde2bd77b42e8be32212103e815d3c4f84c1
Rachel Baust <rachel.baust@stfc.ac.uk>
2024-10-24 16:52:09 +0100

    Add option for PolarizationCorrectionWildes to include output spin state in log
    This is a squashed version of #38322
    
    Add unit tests to check for spin state sample log
    
    Add ORSO polarizations constants and helper methods
    
    Add optional ORSO spin state log to corrected workspaces
    
    Use static const for polarisation constants
    
    We were previously using constexpr however it seems that this might not
    be ideal for string constants because they cannot be passed by
    reference.
    
    Refactor setting sample log on output ws
    
    Refactor the code for setting the spin state sample log on the output
    workspace to reduce code duplication.
    
    Use spin state log constant in Wildes tests
    
    Expose AddSpinStateToLog parameter in PolarizationEfficiencyCor
    
    Update algorithm documentation
    
    Add release notes for issue 38264
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Fix build errors and warnings
    
    Add a missing import and remove an unused variable.

--- Framework/Algorithms/test/PolarizationEfficiencyCorTest.h
+++ Framework/Algorithms/test/PolarizationEfficiencyCorTest.h
@@ -8,6 +8,7 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "MantidAlgorithms/PolarizationCorrections/PolarizationCorrectionsHelpers.h"
 #include "MantidAlgorithms/PolarizationEfficiencyCor.h"
 
 #include "MantidAPI/AlgorithmManager.h"
@@ -29,6 +30,7 @@
 
 using namespace Mantid::API;
 using namespace Mantid::Algorithms;
+using namespace Mantid::Algorithms::PolarizationCorrectionsHelpers;
 using namespace Mantid::DataObjects;
 using namespace Mantid::HistogramData;
 using namespace WorkspaceCreationHelper;
@@ -445,7 +447,13 @@ public:
     }
   }
 
+  void test_spin_state_log_not_added_by_default_Wildes() { runSpinStateLogTest(WILDES_METHOD, false); }
+
+  void test_spin_state_log_added_when_requested_Wildes() { runSpinStateLogTest(WILDES_METHOD, true); }
+
 private:
+  const std::string WILDES_METHOD{"Wildes"};
+
   std::vector<MatrixWorkspace_sptr> createWorkspaces(int n) {
     std::vector<MatrixWorkspace_sptr> workspaces;
     for (int i = 0; i < n; ++i) {
@@ -567,5 +575,26 @@ private:
     auto retVal = std::make_shared<Workspace2D>();
     retVal->initialize(1, Histogram(xVals, yVals));
     return retVal;
+  }
+
+  void runSpinStateLogTest(const std::string &correctionMethod, const bool expectLog) {
+    PolarizationEfficiencyCor alg;
+    alg.setRethrows(true);
+    alg.initialize();
+    alg.setProperty("OutputWorkspace", "out");
+    alg.setProperty("InputWorkspaces", createWorkspacesInADS(4));
+    alg.setProperty("CorrectionMethod", correctionMethod);
+    alg.setProperty("Efficiencies", createEfficiencies(correctionMethod));
+    if (expectLog) {
+      alg.setProperty("AddSpinStateToLog", true);
+    }
+    alg.execute();
+    WorkspaceGroup_sptr out = AnalysisDataService::Instance().retrieveWS<WorkspaceGroup>("out");
+    TS_ASSERT_EQUALS(out->size(), 4);
+    for (size_t i = 0; i != 4; ++i) {
+      MatrixWorkspace_sptr ws = std::dynamic_pointer_cast<MatrixWorkspace>(out->getItem(i));
+      TS_ASSERT(ws)
+      TS_ASSERT_EQUALS(ws->run().hasProperty(SpinStatesORSO::LOG_NAME), expectLog)
+    }
   }
 };
