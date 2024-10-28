08f3ffacc423f40b8299c4db72b20a3948136d80
Ross Whitfield <whitfieldre1@ornl.gov>
2024-10-28 12:13:43 +1100

    Add Fitler By Time

--- Framework/DataHandling/inc/MantidDataHandling/AlignAndFocusPowderSlim.h
+++ Framework/DataHandling/inc/MantidDataHandling/AlignAndFocusPowderSlim.h
@@ -46,8 +46,17 @@ private:
 
   void loadTOF(std::unique_ptr<std::vector<float>> &data, ::NeXus::File &h5file);
   void loadDetid(std::unique_ptr<std::vector<uint32_t>> &data, ::NeXus::File &h5file);
+  void loadPulseTimes(std::unique_ptr<std::vector<double>> &data, ::NeXus::File &h5file);
+  void loadEventIndex(std::unique_ptr<std::vector<uint64_t>> &data, ::NeXus::File &h5file);
 
   std::map<detid_t, double> m_calibration;
+  bool is_time_filtered{false};
+  size_t pulse_start_index{0};
+  size_t pulse_stop_index{std::numeric_limits<size_t>::max()};
+  /// Index to load start at in the file
+  std::vector<int64_t> loadStart;
+  /// How much to load in the file
+  std::vector<int64_t> loadSize;
 };
 
 } // namespace DataHandling
