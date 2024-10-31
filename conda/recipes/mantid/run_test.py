a4e2cbfb5b832d3709e265c18bda6d1c167848a1
Applin <robert.applin@stfc.ac.uk>
2024-10-31 11:44:28 +0000

    Fix E402 module level import in conda directory

--- conda/recipes/mantid/run_test.py
+++ conda/recipes/mantid/run_test.py
@@ -5,6 +5,6 @@ ConfigService["UpdateInstrumentDefinitions.OnStartup"] = "0"
 ConfigService["usagereports.enabled"] = "0"
 ConfigService["CheckMantidVersion.OnStartup"] = "0"
 
-import mantid.simpleapi
+import mantid.simpleapi  # noqa: E402
 
 assert "Rebin" in dir(mantid.simpleapi), "Rebin not found in simpleapi!"
