a12ee2748c66971fda457a12b60d15585c8e412a
Patrou, Maria <patroum@ornl.gov>
2024-11-14 12:32:30 -0500

    updateSelectorRange removed in Elwin

--- qt/scientific_interfaces/Inelastic/Processor/ElwinPresenter.cpp
+++ qt/scientific_interfaces/Inelastic/Processor/ElwinPresenter.cpp
@@ -159,7 +159,6 @@ void ElwinPresenter::newPreviewWorkspaceSelected(int index) {
   setInputWorkspace(workspace);
   updateAvailableSpectra();
   setSelectedSpectrum(m_view->getPreviewSpec());
-  // m_view->updateSelectorRange(workspace);
 
   m_view->plotInput(getInputWorkspace(), getSelectedSpectrum());
 }
