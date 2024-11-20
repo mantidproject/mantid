7be1adfcda5664c75d358b7fcc19d7cab3b2fcc7
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2024-11-20 11:19:39 +0000

    Refactor slightly ApplyFloodWorkspaceTest and add test to check that missing flood spectra are not modyfing the input spectra

--- Framework/Algorithms/test/ApplyFloodWorkspaceTest.h
+++ Framework/Algorithms/test/ApplyFloodWorkspaceTest.h
@@ -9,6 +9,7 @@
 #include <cxxtest/TestSuite.h>
 
 #include "MantidAlgorithms/ApplyFloodWorkspace.h"
+#include "MantidAlgorithms/CropWorkspace.h"
 
 #include "MantidAPI/AnalysisDataService.h"
 #include "MantidAPI/Axis.h"
@@ -22,24 +23,55 @@ using namespace Mantid::API;
 using namespace Mantid::Kernel;
 using namespace WorkspaceCreationHelper;
 
+namespace {
+constexpr double MAX_X_REFLECTOMETRY(100000);
+constexpr double DELTA(1e-9);
+
+std::vector<double> generateXRange(const int nBoundaries, double start) {
+  auto xvec = std::vector<double>(nBoundaries + 1);
+  double const interval = MAX_X_REFLECTOMETRY / nBoundaries; // increment
+  std::ranges::for_each(xvec.begin(), xvec.end(), [&start, &interval](double &item) {
+    item = start;
+    start += interval;
+  });
+  return xvec;
+}
+
+MatrixWorkspace_sptr cropWorkspaces(const MatrixWorkspace_sptr &inputWS, const int startIndex = 0) {
+  CropWorkspace alg;
+  alg.initialize();
+  alg.setChild(true);
+  alg.setProperty("InputWorkspace", inputWS);
+  alg.setProperty("StartWorkspaceIndex", startIndex);
+  alg.setProperty("OutputWorkspace", "dummy");
+  alg.execute();
+  MatrixWorkspace_sptr out = alg.getProperty("OutputWorkspace");
+  return out;
+}
+
+MatrixWorkspace_sptr applyFloodWorkspace(const MatrixWorkspace_sptr &inputWS, const MatrixWorkspace_sptr &floodWS) {
+  ApplyFloodWorkspace alg;
+  alg.initialize();
+  alg.setChild(true);
+  alg.setProperty("InputWorkspace", inputWS);
+  alg.setProperty("FloodWorkspace", floodWS);
+  alg.setProperty("OutputWorkspace", "dummy");
+  alg.execute();
+  return alg.getProperty("OutputWorkspace");
+}
+} // namespace
+
 class ApplyFloodWorkspaceTest : public CxxTest::TestSuite {
 public:
   void test_flood_same_x_units() {
     auto inputWS = create2DWorkspaceWithReflectometryInstrumentMultiDetector(0, 0.1);
     auto flood = createFloodWorkspace(inputWS->getInstrument());
 
-    ApplyFloodWorkspace alg;
-    alg.initialize();
-    alg.setChild(true);
-    alg.setProperty("InputWorkspace", inputWS);
-    alg.setProperty("FloodWorkspace", flood);
-    alg.setProperty("OutputWorkspace", "dummy");
-    alg.execute();
-    MatrixWorkspace_sptr out = alg.getProperty("OutputWorkspace");
-    TS_ASSERT_DELTA(out->y(0)[0], 2.8571428575, 1e-9);
-    TS_ASSERT_DELTA(out->y(1)[0], 2.0, 1e-9);
-    TS_ASSERT_DELTA(out->y(2)[0], 2.5, 1e-9);
-    TS_ASSERT_DELTA(out->y(3)[0], 2.2222222222, 1e-9);
+    auto const out = applyFloodWorkspace(inputWS, flood);
+    TS_ASSERT_DELTA(out->y(0)[0], 2.8571428575, DELTA);
+    TS_ASSERT_DELTA(out->y(1)[0], 2.0, DELTA);
+    TS_ASSERT_DELTA(out->y(2)[0], 2.5, DELTA);
+    TS_ASSERT_DELTA(out->y(3)[0], 2.2222222222, DELTA);
     AnalysisDataService::Instance().clear();
   }
 
@@ -47,29 +79,49 @@ public:
     auto inputWS = create2DWorkspaceWithReflectometryInstrumentMultiDetector(0, 0.1);
     auto flood = createFloodWorkspace(inputWS->getInstrument(), "Wavelength");
 
-    ApplyFloodWorkspace alg;
-    alg.initialize();
-    alg.setChild(true);
-    alg.setProperty("InputWorkspace", inputWS);
-    alg.setProperty("FloodWorkspace", flood);
-    alg.setProperty("OutputWorkspace", "dummy");
-    alg.execute();
-    MatrixWorkspace_sptr out = alg.getProperty("OutputWorkspace");
-    TS_ASSERT_DELTA(out->y(0)[0], 2.8571428575, 1e-9);
-    TS_ASSERT_DELTA(out->y(1)[0], 2.0, 1e-9);
-    TS_ASSERT_DELTA(out->y(2)[0], 2.5, 1e-9);
-    TS_ASSERT_DELTA(out->y(3)[0], 2.2222222222, 1e-9);
+    auto const out = applyFloodWorkspace(inputWS, flood);
+    TS_ASSERT_DELTA(out->y(0)[0], 2.8571428575, DELTA);
+    TS_ASSERT_DELTA(out->y(1)[0], 2.0, DELTA);
+    TS_ASSERT_DELTA(out->y(2)[0], 2.5, DELTA);
+    TS_ASSERT_DELTA(out->y(3)[0], 2.2222222222, DELTA);
+    AnalysisDataService::Instance().clear();
+  }
+
+  void test_flood_doesnt_transform_spectra_that_are_missing_in_flood_workspace_for_multiple_bin_file() {
+    auto inputWS = create2DWorkspaceWithReflectometryInstrumentMultiDetector(0, 0.1);
+    auto flood = createFloodWorkspace(inputWS->getInstrument(), "TOF", 4);
+
+    auto const cropped = cropWorkspaces(flood, 2);
+    auto const out = applyFloodWorkspace(inputWS, cropped);
+
+    // Histograms without Flood Spectra data are not modified
+    TS_ASSERT_DELTA(out->readY(0), inputWS->readY(0), DELTA);
+    TS_ASSERT_DELTA(out->readY(1), inputWS->readY(1), DELTA);
+
+    int const refactor = 5; // how bigger is bin size in input with respect to initial flood ws
+    // Histograms with Flood Spectra are rebinned prior to flood correction
+    TS_ASSERT_DELTA(out->readY(2), std::vector<double>(20, 2 / 0.8 * refactor), DELTA);
+    TS_ASSERT_DELTA(out->readY(3), std::vector<double>(20, 2 / 0.9 * refactor), DELTA);
     AnalysisDataService::Instance().clear();
   }
 
 private:
   MatrixWorkspace_sptr createFloodWorkspace(const Mantid::Geometry::Instrument_const_sptr &instrument,
-                                            std::string const &xUnit = "TOF") {
-    MatrixWorkspace_sptr flood = create2DWorkspace(4, 1);
-    flood->mutableY(0)[0] = 0.7;
-    flood->mutableY(1)[0] = 1.0;
-    flood->mutableY(2)[0] = 0.8;
-    flood->mutableY(3)[0] = 0.9;
+                                            std::string const &xUnit = "TOF", const int nBoundaries = 1) {
+    MatrixWorkspace_sptr flood = create2DWorkspace(4, nBoundaries);
+    flood->mutableY(0) = std::vector<double>(nBoundaries, 0.7);
+    flood->mutableY(1) = std::vector<double>(nBoundaries, 1.0);
+    flood->mutableY(2) = std::vector<double>(nBoundaries, 0.8);
+    flood->mutableY(3) = std::vector<double>(nBoundaries, 0.9);
+
+    // X values are added ad-hoc to match the bin X range of the default test Reflectometry Instrument workspaces
+    //  This doesn't affect tests with 1 bin per histogram
+    auto const &xvec = generateXRange(nBoundaries, 0);
+    flood->mutableX(0) = xvec;
+    flood->mutableX(1) = xvec;
+    flood->mutableX(2) = xvec;
+    flood->mutableX(3) = xvec;
+
     flood->setInstrument(instrument);
     for (size_t i = 0; i < flood->getNumberHistograms(); ++i) {
       flood->getSpectrum(i).setDetectorID(Mantid::detid_t(i + 1));
