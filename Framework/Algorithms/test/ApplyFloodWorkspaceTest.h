7338b240ad7b84cf167b01643d71ef04eb2cdd2a
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2024-11-21 09:57:43 +0000

    Add teardown method, move create flood workspace out of test class and add a test for testing single bin files when there is missing spectra

--- Framework/Algorithms/test/ApplyFloodWorkspaceTest.h
+++ Framework/Algorithms/test/ApplyFloodWorkspaceTest.h
@@ -59,10 +59,46 @@ MatrixWorkspace_sptr applyFloodWorkspace(const MatrixWorkspace_sptr &inputWS, co
   alg.execute();
   return alg.getProperty("OutputWorkspace");
 }
+
+MatrixWorkspace_sptr createFloodWorkspace(const Mantid::Geometry::Instrument_const_sptr &instrument,
+                                          std::string const &xUnit = "TOF", const int nBoundaries = 1) {
+  MatrixWorkspace_sptr flood = create2DWorkspace(4, nBoundaries);
+  flood->mutableY(0) = std::vector<double>(nBoundaries, 0.7);
+  flood->mutableY(1) = std::vector<double>(nBoundaries, 1.0);
+  flood->mutableY(2) = std::vector<double>(nBoundaries, 0.8);
+  flood->mutableY(3) = std::vector<double>(nBoundaries, 0.9);
+
+  // X values are added ad-hoc to match the bin X range of the default test Reflectometry Instrument workspaces
+  //  This doesn't affect tests with 1 bin per histogram
+  auto const &xvec = generateXRange(nBoundaries, 0);
+  flood->mutableX(0) = xvec;
+  flood->mutableX(1) = xvec;
+  flood->mutableX(2) = xvec;
+  flood->mutableX(3) = xvec;
+
+  flood->setInstrument(instrument);
+  for (size_t i = 0; i < flood->getNumberHistograms(); ++i) {
+    flood->getSpectrum(i).setDetectorID(Mantid::detid_t(i + 1));
+  }
+  flood->getAxis(0)->setUnit("TOF");
+  if (xUnit != "TOF") {
+    ConvertUnits convert;
+    convert.initialize();
+    convert.setChild(true);
+    convert.setProperty("InputWorkspace", flood);
+    convert.setProperty("Target", xUnit);
+    convert.setProperty("OutputWorkspace", "dummy");
+    convert.execute();
+    flood = convert.getProperty("OutputWorkspace");
+  }
+  return flood;
+}
 } // namespace
 
 class ApplyFloodWorkspaceTest : public CxxTest::TestSuite {
 public:
+  void tearDown() override { AnalysisDataService::Instance().clear(); }
+
   void test_flood_same_x_units() {
     auto inputWS = create2DWorkspaceWithReflectometryInstrumentMultiDetector(0, 0.1);
     auto flood = createFloodWorkspace(inputWS->getInstrument());
@@ -72,7 +108,6 @@ public:
     TS_ASSERT_DELTA(out->y(1)[0], 2.0, DELTA);
     TS_ASSERT_DELTA(out->y(2)[0], 2.5, DELTA);
     TS_ASSERT_DELTA(out->y(3)[0], 2.2222222222, DELTA);
-    AnalysisDataService::Instance().clear();
   }
 
   void test_flood_different_x_units() {
@@ -84,7 +119,6 @@ public:
     TS_ASSERT_DELTA(out->y(1)[0], 2.0, DELTA);
     TS_ASSERT_DELTA(out->y(2)[0], 2.5, DELTA);
     TS_ASSERT_DELTA(out->y(3)[0], 2.2222222222, DELTA);
-    AnalysisDataService::Instance().clear();
   }
 
   void test_flood_doesnt_transform_spectra_that_are_missing_in_flood_workspace_for_multiple_bin_file() {
@@ -102,41 +136,21 @@ public:
     // Histograms with Flood Spectra are rebinned prior to flood correction
     TS_ASSERT_DELTA(out->readY(2), std::vector<double>(20, 2 / 0.8 * refactor), DELTA);
     TS_ASSERT_DELTA(out->readY(3), std::vector<double>(20, 2 / 0.9 * refactor), DELTA);
-    AnalysisDataService::Instance().clear();
   }
 
-private:
-  MatrixWorkspace_sptr createFloodWorkspace(const Mantid::Geometry::Instrument_const_sptr &instrument,
-                                            std::string const &xUnit = "TOF", const int nBoundaries = 1) {
-    MatrixWorkspace_sptr flood = create2DWorkspace(4, nBoundaries);
-    flood->mutableY(0) = std::vector<double>(nBoundaries, 0.7);
-    flood->mutableY(1) = std::vector<double>(nBoundaries, 1.0);
-    flood->mutableY(2) = std::vector<double>(nBoundaries, 0.8);
-    flood->mutableY(3) = std::vector<double>(nBoundaries, 0.9);
+  void test_flood_doesnt_transform_spectra_that_are_missing_in_flood_workspace_for_single_bin_file() {
+    auto inputWS = create2DWorkspaceWithReflectometryInstrumentMultiDetector(0, 0.1);
+    auto flood = createFloodWorkspace(inputWS->getInstrument(), "TOF");
 
-    // X values are added ad-hoc to match the bin X range of the default test Reflectometry Instrument workspaces
-    //  This doesn't affect tests with 1 bin per histogram
-    auto const &xvec = generateXRange(nBoundaries, 0);
-    flood->mutableX(0) = xvec;
-    flood->mutableX(1) = xvec;
-    flood->mutableX(2) = xvec;
-    flood->mutableX(3) = xvec;
+    auto const cropped = cropWorkspaces(flood, 2);
+    auto const out = applyFloodWorkspace(inputWS, cropped);
 
-    flood->setInstrument(instrument);
-    for (size_t i = 0; i < flood->getNumberHistograms(); ++i) {
-      flood->getSpectrum(i).setDetectorID(Mantid::detid_t(i + 1));
-    }
-    flood->getAxis(0)->setUnit("TOF");
-    if (xUnit != "TOF") {
-      ConvertUnits convert;
-      convert.initialize();
-      convert.setChild(true);
-      convert.setProperty("InputWorkspace", flood);
-      convert.setProperty("Target", xUnit);
-      convert.setProperty("OutputWorkspace", "dummy");
-      convert.execute();
-      flood = convert.getProperty("OutputWorkspace");
-    }
-    return flood;
+    // Histograms without Flood Spectra data are not modified
+    TS_ASSERT_DELTA(out->readY(0)[0], 2.0, DELTA);
+    TS_ASSERT_DELTA(out->readY(1)[0], 2.0, DELTA);
+
+    // Histograms with Flood Spectra are divided by correction factor
+    TS_ASSERT_DELTA(out->readY(2)[0], 2 / 0.8, DELTA);
+    TS_ASSERT_DELTA(out->readY(3)[0], 2 / 0.9, DELTA);
   }
 };
