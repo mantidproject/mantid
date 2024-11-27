baa52e0906d381fa6eb96327f1e5ab5781a6ecd5
Pete Peterson <petersonpf@ornl.gov>
2024-11-27 13:12:43 -0500

    Add tests which compile but fail

---
+++ Framework/NexusCpp/test/leak_test1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+#include "MantidNexusCpp/napi.h"
+#include <stdlib.h>
+
+#define ON_ERROR(msgstr)                                                                                               \
+  {                                                                                                                    \
+    fprintf(stderr, msgstr);                                                                                           \
+    return 1;                                                                                                          \
+  }
+
+int main(int argc, char *argv[]) {
+  NXaccess access_mode = NXACC_CREATE5;
+  const int nReOpen = 1000;
+  printf("Running for %d iterations\n", nReOpen);
+  int iReOpen;
+  const char *szFile = "leak_test1.nxs";
+
+  NXhandle fileid;
+  unlink(szFile);
+  if (NXopen(szFile, access_mode, &fileid) != NX_OK)
+    ON_ERROR("NXopen failed!\n")
+
+  if (NXclose(&fileid) != NX_OK)
+    ON_ERROR("NXclose failed!\n")
+
+  for (iReOpen = 0; iReOpen < nReOpen; iReOpen++) {
+    if (0 == iReOpen % 100)
+      printf("loop count %d\n", iReOpen);
+
+    if (NXopen(szFile, NXACC_RDWR, &fileid) != NX_OK)
+      ON_ERROR("NXopen failed!\n");
+
+    if (NXclose(&fileid) != NX_OK)
+      ON_ERROR("NXclose failed!\n");
+  }
+
+  unlink(szFile);
+  fileid = NULL;
+  return 0;
+}
