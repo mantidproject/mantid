ff3c05314601dbc958a6ef66db229c379b3cfa2d
Pete Peterson <petersonpf@ornl.gov>
2024-12-02 14:50:36 -0500

    Move contents of nxconfig into napiconfig

--- Framework/NexusCpp/inc/MantidNexusCpp/napiconfig.h
+++ Framework/NexusCpp/inc/MantidNexusCpp/napiconfig.h
@@ -5,7 +5,28 @@
 #include <windows.h>
 #endif /* _WIN32 */
 
-#include "MantidNexusCpp/nxconfig.h"
+#define HAVE_HDF4
+#define WITH_HDF4
+
+#define HAVE_HDF5
+#define WITH_HDF5
+
+// ----- contents of nxconfig.h - START
+// not enabling HAVE_FTIME - not used?
+
+// not enabling HAVE_TZSET - not used?
+
+// not enabling HAVE_STRDUP - not used?
+
+// not enabling HAVE_LONG_LONG_INT - only in tests
+
+// not enabling HAVE_UNSIGNED_LONG_LONG_INT - not used?
+
+// should be standard everywhere
+#define HAVE_STDINT_H 1
+
+// not enabling HAVE_INTTYPES_H - stdint.h should be everywhere
+// ----- contents of nxconfig.h - END
 
 /*
  * Integer type definitions
