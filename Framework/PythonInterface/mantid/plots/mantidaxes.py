61f78e2bb22c0703f3477376714a4d1aee2e3a18
Michael Walsh <walshmm@ornl.gov>
2024-12-06 13:18:48 -0500

    account for lines sharing graphs with workspaces in mantidaxes

--- Framework/PythonInterface/mantid/plots/mantidaxes.py
+++ Framework/PythonInterface/mantid/plots/mantidaxes.py
@@ -7,6 +7,7 @@
 #  This file is part of the mantid package
 from collections.abc import Iterable
 import copy
+from enum import IntEnum
 from functools import wraps
 
 import numpy as np
@@ -37,9 +38,13 @@ from mantid.plots.utility import artists_hidden, autoscale_on_update, legend_set
 
 WATERFALL_XOFFSET_DEFAULT, WATERFALL_YOFFSET_DEFAULT = 10, 20
 
+
 # -----------------------------------------------------------------------------
 # Decorators
 # -----------------------------------------------------------------------------
+class AxisArgType(IntEnum):
+    WORKSPACE = 0
+    LINE = 1
 
 
 def plot_decorator(func):
@@ -50,6 +55,8 @@ def plot_decorator(func):
         # Saves saving it on array objects
         if datafunctions.validate_args(*args, **kwargs):
             # Fill out kwargs with the values of args
+            kwargs["argType"] = AxisArgType.WORKSPACE
+
             kwargs["workspaces"] = args[0].name()
             kwargs["function"] = func_name
 
@@ -58,8 +65,9 @@ def plot_decorator(func):
             if "cmap" in kwargs and isinstance(kwargs["cmap"], Colormap):
                 kwargs["cmap"] = kwargs["cmap"].name
             self.creation_args.append(kwargs)
-        elif func_name == "axhline" or func_name == "axvline":
-            self.creation_args.append({"function": func_name, "args": args, "kwargs": kwargs})
+        elif func_name in ["axhline", "axvline"]:
+            new_creation_args = {"function": func_name, "args": args, "kwargs": kwargs, "argType": AxisArgType.LINE}
+            self.creation_args.append(new_creation_args)
 
         return func_value
 
@@ -478,8 +486,11 @@ class MantidAxes(Axes):
         :param new_name : the new name of workspace
         :param old_name : the old name of workspace
         """
+
         for cargs in self.creation_args:
-            if cargs["workspaces"] == old_name:
+            argType = cargs["argType"]
+            # for workspace creation args, since lines dont have "workspaces"
+            if argType == AxisArgType.WORKSPACE and cargs["workspaces"] == old_name:
                 cargs["workspaces"] = new_name
         for ws_name, ws_artist_list in list(self.tracked_workspaces.items()):
             for ws_artist in ws_artist_list:
