1164887a710585ac171477ddf444f8c0da579417
darshdinger <ddarsh5544@gmail.com>
2024-12-10 12:28:12 -0500

    Most recent changes.

--- qt/widgets/common/inc/MantidQtWidgets/Common/HelpWindow.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/HelpWindow.h
@@ -18,7 +18,6 @@ namespace MantidQt {
 namespace API {
 
 /**
-<<<<<<< Updated upstream
  * HelpWindow provides a centralized interface for displaying
  * help pages, algorithm details, and concepts. It supports both
  * the C++ HelpWindow and launching a Python-based HelpWindow.
@@ -30,36 +29,40 @@ public:
   explicit HelpWindow(QWidget *parent = nullptr);
 
   /// Show a page by URL
-  *HelpWindow class manages the launching of help pages and the Python Help Window.*/
-      class EXPORT_OPT_MANTIDQT_COMMON HelpWindow {
-  public:
-    /// Show a help page by URL
+  static void showPage(const std::string &url = std::string());
+  static void showPage(const QString &url);
+  static void showPage(const QUrl &url);
 
-    static void showPage(const std::string &url = std::string());
-    static void showPage(const QString &url);
-    static void showPage(const QUrl &url);
+  /// Show help for an algorithm
+  static void showAlgorithm(const std::string &name = std::string(), const int version = -1);
+  static void showAlgorithm(const QString &name, const int version = -1);
 
-    /// Show help for an algorithm
-    static void showAlgorithm(const std::string &name = std::string(), const int version = -1);
-    static void showAlgorithm(const QString &name, const int version = -1);
+  /// Show help for a concept
+  static void showConcept(const std::string &name = std::string());
+  static void showConcept(const QString &name);
 
-    /// Show help for a concept
-    static void showConcept(const std::string &name = std::string());
-    static void showConcept(const QString &name);
+  /// Show help for a fit function
+  static void showFitFunction(const std::string &name = std::string());
+  static void showFitFunction(const QString &name);
 
-    /// Show help for a fit function
-    static void showFitFunction(const std::string &name = std::string());
-    static void showFitFunction(const QString &name);
+  /// Show help for a custom interface
+  static void showCustomInterface(const QString &name, const QString &area = QString(),
+                                  const QString &section = QString());
+  static void showCustomInterface(const std::string &name = std::string(), const std::string &area = std::string(),
+                                  const std::string &section = std::string());
 
-    /// Show help for a custom interface
-    static void showCustomInterface(const QString &name, const QString &area = QString(),
-                                    const QString &section = QString());
-    static void showCustomInterface(const std::string &name = std::string(), const std::string &area = std::string(),
-                                    const std::string &section = std::string());
+  /// Launch the Python-based Help Window
+  static void launchPythonHelpWindow();
 
-    /// Launch the Python-based Help Window
-    static void launchPythonHelpWindow();
-  };
+  /// Launch a specific Python help page
+  void launchPythonHelp();
+
+  /// Get the current page URL
+  QString currentPageUrl() const;
+
+private:
+  QString currentUrl; // Stores the current URL
+};
 
 } // namespace API
 } // namespace MantidQt
--- qt/widgets/common/src/HelpWindow.cpp
+++ qt/widgets/common/src/HelpWindow.cpp
@@ -13,194 +13,190 @@
 #include "MantidQtWidgets/Common/MantidDesktopServices.h"
 #include "MantidQtWidgets/Common/MantidHelpInterface.h"
 
-<<<<<<< Updated upstream
 #include <QPushButton>
-    == == ==
-    =
 #define PY_SSIZE_T_CLEAN
 #undef slots
 #include <Python.h>
 
-        >>>>>>> Stashed changes
 #include <QUrl>
 #include <QVBoxLayout>
 #include <QWidget>
 #include <boost/python.hpp>
 
-        namespace MantidQt::API {
-  namespace {
-  /// Static logger
-  Mantid::Kernel::Logger g_log("HelpWindow");
-  } // namespace
+namespace MantidQt::API {
+namespace {
+/// Static logger
+Mantid::Kernel::Logger g_log("HelpWindow");
+} // namespace
 
-  using std::string;
+using std::string;
 
-  void HelpWindow::showPage(const std::string &url) { showPage(QString::fromStdString(url)); }
+void HelpWindow::showPage(const std::string &url) { showPage(QString::fromStdString(url)); }
 
-  void HelpWindow::showPage(const QString &url) {
-    InterfaceManager interfaceManager;
-    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
-    if (gui) {
-      gui->showPage(url);
-    } else {
-      QUrl validUrl = QUrl::fromUserInput(url);
-      if (!validUrl.isValid()) {
-        g_log.error() << "Invalid URL: " << url.toStdString() << "\n";
-        return;
-      }
-      MantidDesktopServices::openUrl(validUrl);
-      g_log.debug() << "Opening URL in default browser: " << validUrl.toString().toStdString() << "\n";
+void HelpWindow::showPage(const QString &url) {
+  InterfaceManager interfaceManager;
+  MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+  if (gui) {
+    gui->showPage(url);
+  } else {
+    QUrl validUrl = QUrl::fromUserInput(url);
+    if (!validUrl.isValid()) {
+      g_log.error() << "Invalid URL: " << url.toStdString() << "\n";
+      return;
     }
+    MantidDesktopServices::openUrl(validUrl);
+    g_log.debug() << "Opening URL in default browser: " << validUrl.toString().toStdString() << "\n";
   }
+}
 
-  void HelpWindow::showPage(const QUrl &url) {
-    InterfaceManager interfaceManager;
-    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
-    if (gui) {
-      gui->showPage(url);
-    } else {
-      if (!url.isValid()) {
-        g_log.error() << "Invalid QUrl: " << url.toString().toStdString() << "\n";
-        return;
-      }
-      MantidDesktopServices::openUrl(url);
-      g_log.debug() << "Opening URL in default browser: " << url.toString().toStdString() << "\n";
+void HelpWindow::showPage(const QUrl &url) {
+  InterfaceManager interfaceManager;
+  MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+  if (gui) {
+    gui->showPage(url);
+  } else {
+    if (!url.isValid()) {
+      g_log.error() << "Invalid QUrl: " << url.toString().toStdString() << "\n";
+      return;
     }
+    MantidDesktopServices::openUrl(url);
+    g_log.debug() << "Opening URL in default browser: " << url.toString().toStdString() << "\n";
   }
+}
 
-  void HelpWindow::showAlgorithm(const std::string &name, const int version) {
-    showAlgorithm(QString::fromStdString(name), version);
-  }
+void HelpWindow::showAlgorithm(const std::string &name, const int version) {
+  showAlgorithm(QString::fromStdString(name), version);
+}
 
-  void HelpWindow::showAlgorithm(const QString &name, const int version) {
-    InterfaceManager interfaceManager;
-    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
-    if (gui) {
-      gui->showAlgorithm(name, version);
-    } else {
-      QString baseUrl = "https://docs.mantidproject.org/algorithms/";
-      QString url = baseUrl + name + "-v" + QString::number(version) + ".html";
-      QUrl validUrl = QUrl::fromUserInput(url);
-      if (!validUrl.isValid()) {
-        g_log.error() << "Invalid algorithm URL: " << url.toStdString() << "\n";
-        return;
-      }
-      MantidDesktopServices::openUrl(validUrl);
-      g_log.debug() << "Opening algorithm help URL: " << validUrl.toString().toStdString() << "\n";
+void HelpWindow::showAlgorithm(const QString &name, const int version) {
+  InterfaceManager interfaceManager;
+  MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+  if (gui) {
+    gui->showAlgorithm(name, version);
+  } else {
+    QString baseUrl = "https://docs.mantidproject.org/algorithms/";
+    QString url = baseUrl + name + "-v" + QString::number(version) + ".html";
+    QUrl validUrl = QUrl::fromUserInput(url);
+    if (!validUrl.isValid()) {
+      g_log.error() << "Invalid algorithm URL: " << url.toStdString() << "\n";
+      return;
     }
+    MantidDesktopServices::openUrl(validUrl);
+    g_log.debug() << "Opening algorithm help URL: " << validUrl.toString().toStdString() << "\n";
   }
+}
 
-  void HelpWindow::showConcept(const std::string &name) { showConcept(QString::fromStdString(name)); }
+void HelpWindow::showConcept(const std::string &name) { showConcept(QString::fromStdString(name)); }
 
-  void HelpWindow::showConcept(const QString &name) {
-    InterfaceManager interfaceManager;
-    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
-    if (gui) {
-      gui->showConcept(name);
-    } else {
-      QString baseUrl = "https://docs.mantidproject.org/concepts/";
-      QString url = baseUrl + name + ".html";
-      QUrl validUrl = QUrl::fromUserInput(url);
-      if (!validUrl.isValid()) {
-        g_log.error() << "Invalid concept URL: " << url.toStdString() << "\n";
-        return;
-      }
-      MantidDesktopServices::openUrl(validUrl);
-      g_log.debug() << "Opening concept help URL: " << validUrl.toString().toStdString() << "\n";
+void HelpWindow::showConcept(const QString &name) {
+  InterfaceManager interfaceManager;
+  MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+  if (gui) {
+    gui->showConcept(name);
+  } else {
+    QString baseUrl = "https://docs.mantidproject.org/concepts/";
+    QString url = baseUrl + name + ".html";
+    QUrl validUrl = QUrl::fromUserInput(url);
+    if (!validUrl.isValid()) {
+      g_log.error() << "Invalid concept URL: " << url.toStdString() << "\n";
+      return;
     }
+    MantidDesktopServices::openUrl(validUrl);
+    g_log.debug() << "Opening concept help URL: " << validUrl.toString().toStdString() << "\n";
   }
+}
 
-  void HelpWindow::showFitFunction(const std::string &name) { showFitFunction(QString::fromStdString(name)); }
+void HelpWindow::showFitFunction(const std::string &name) { showFitFunction(QString::fromStdString(name)); }
 
-  void HelpWindow::showFitFunction(const QString &name) {
-    InterfaceManager interfaceManager;
-    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
-    if (gui) {
-      gui->showFitFunction(name);
-    } else {
-      QString baseUrl = "https://docs.mantidproject.org/functions/";
-      QString url = baseUrl + name + ".html";
-      QUrl validUrl = QUrl::fromUserInput(url);
-      if (!validUrl.isValid()) {
-        g_log.error() << "Invalid function URL: " << url.toStdString() << "\n";
-        return;
-      }
-      MantidDesktopServices::openUrl(validUrl);
-      g_log.debug() << "Opening fit function help URL: " << validUrl.toString().toStdString() << "\n";
+void HelpWindow::showFitFunction(const QString &name) {
+  InterfaceManager interfaceManager;
+  MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+  if (gui) {
+    gui->showFitFunction(name);
+  } else {
+    QString baseUrl = "https://docs.mantidproject.org/functions/";
+    QString url = baseUrl + name + ".html";
+    QUrl validUrl = QUrl::fromUserInput(url);
+    if (!validUrl.isValid()) {
+      g_log.error() << "Invalid function URL: " << url.toStdString() << "\n";
+      return;
     }
+    MantidDesktopServices::openUrl(validUrl);
+    g_log.debug() << "Opening fit function help URL: " << validUrl.toString().toStdString() << "\n";
   }
+}
 
-  void HelpWindow::showCustomInterface(const std::string &name, const std::string &area, const std::string &section) {
-    showCustomInterface(QString::fromStdString(name), QString::fromStdString(area), QString::fromStdString(section));
-  }
+void HelpWindow::showCustomInterface(const std::string &name, const std::string &area, const std::string &section) {
+  showCustomInterface(QString::fromStdString(name), QString::fromStdString(area), QString::fromStdString(section));
+}
 
-  void HelpWindow::showCustomInterface(const QString &name, const QString &area, const QString &section) {
-    InterfaceManager interfaceManager;
-    MantidHelpInterface *gui = interfaceManager.createHelpWindow();
-    if (gui) {
-      gui->showCustomInterface(name, area, section);
-    } else {
-      QString baseUrl = "https://docs.mantidproject.org/interfaces/";
-      if (!area.isEmpty()) {
-        baseUrl += area + "/";
-      }
-      QString url = baseUrl + name + ".html";
-      QUrl validUrl = QUrl::fromUserInput(url);
-      if (!validUrl.isValid()) {
-        g_log.error() << "Invalid custom interface URL: " << url.toStdString() << "\n";
-        return;
-      }
-      MantidDesktopServices::openUrl(validUrl);
-      g_log.debug() << "Opening custom interface help URL: " << validUrl.toString().toStdString() << "\n";
+void HelpWindow::showCustomInterface(const QString &name, const QString &area, const QString &section) {
+  InterfaceManager interfaceManager;
+  MantidHelpInterface *gui = interfaceManager.createHelpWindow();
+  if (gui) {
+    gui->showCustomInterface(name, area, section);
+  } else {
+    QString baseUrl = "https://docs.mantidproject.org/interfaces/";
+    if (!area.isEmpty()) {
+      baseUrl += area + "/";
     }
+    QString url = baseUrl + name + ".html";
+    QUrl validUrl = QUrl::fromUserInput(url);
+    if (!validUrl.isValid()) {
+      g_log.error() << "Invalid custom interface URL: " << url.toStdString() << "\n";
+      return;
+    }
+    MantidDesktopServices::openUrl(validUrl);
+    g_log.debug() << "Opening custom interface help URL: " << validUrl.toString().toStdString() << "\n";
   }
+}
 
-  void HelpWindow::launchPythonHelpWindow() {
-    try {
-      // Initialize Python environment
-      Py_Initialize();
-      boost::python::object main = boost::python::import("__main__");
-      boost::python::object globals = main.attr("__dict__");
+void HelpWindow::launchPythonHelpWindow() {
+  try {
+    // Initialize Python environment
+    Py_Initialize();
+    boost::python::object main = boost::python::import("__main__");
+    boost::python::object globals = main.attr("__dict__");
 
-      // Update the Python script to include the correct module path
-      const std::string pythonScript = R"(
+    // Update the Python script to include the correct module path
+    const std::string pythonScript = R"(
 import sys
 sys.path.append('mantid/qt/python/mantidqt/mantidqt/widgets/helpwindow/')
 from helpwindowpresenter import HelpWindowPresenter
 presenter = HelpWindowPresenter()
 presenter.show_help_window()
 )";
-      boost::python::exec(pythonScript.c_str(), globals, globals);
-    } catch (boost::python::error_already_set &) {
-      PyErr_Print();
-      g_log.error("Error launching Python Help Window.");
-    }
+    boost::python::exec(pythonScript.c_str(), globals, globals);
+  } catch (boost::python::error_already_set &) {
+    PyErr_Print();
+    g_log.error("Error launching Python Help Window.");
   }
+}
 
-  void HelpWindow::launchPythonHelp() {
-    try {
-      boost::python::object mainNamespace = boost::python::import("__main__").attr("__dict__");
-      boost::python::exec("from helpwindowpresenter import HelpPresenter", mainNamespace);
+void HelpWindow::launchPythonHelp() {
+  try {
+    boost::python::object mainNamespace = boost::python::import("__main__").attr("__dict__");
+    boost::python::exec("from helpwindowpresenter import HelpPresenter", mainNamespace);
 
-      std::string currentPage = currentUrl.toStdString();
-      boost::python::exec(("presenter = HelpPresenter.instance(); presenter.show_page('" + currentPage + "')").c_str(),
-                          mainNamespace);
-    } catch (const boost::python::error_already_set &) {
-      PyErr_Print();
-      g_log.error("Failed to launch Python HelpWindow.");
-    }
+    std::string currentPage = currentUrl.toStdString();
+    boost::python::exec(("presenter = HelpPresenter.instance(); presenter.show_page('" + currentPage + "')").c_str(),
+                        mainNamespace);
+  } catch (const boost::python::error_already_set &) {
+    PyErr_Print();
+    g_log.error("Failed to launch Python HelpWindow.");
   }
+}
 
-  HelpWindow::HelpWindow(QWidget * parent) : QWidget(parent), currentUrl("about:blank") {
-    auto *layout = new QVBoxLayout(this);
+HelpWindow::HelpWindow(QWidget *parent) : QWidget(parent), currentUrl("about:blank") {
+  auto *layout = new QVBoxLayout(this);
 
-    QPushButton *pythonHelpButton = new QPushButton("Python Implementation", this);
-    connect(pythonHelpButton, &QPushButton::clicked, this, &HelpWindow::launchPythonHelp);
-    layout->addWidget(pythonHelpButton);
-  }
+  QPushButton *pythonHelpButton = new QPushButton("Python Implementation", this);
+  connect(pythonHelpButton, &QPushButton::clicked, this, &HelpWindow::launchPythonHelp);
+  layout->addWidget(pythonHelpButton);
+}
 
-  QString HelpWindow::currentPageUrl() const {
-    return this->currentUrl; // Provide the current URL
-  }
+QString HelpWindow::currentPageUrl() const {
+  return this->currentUrl; // Provide the current URL
+}
 
 } // namespace MantidQt::API
