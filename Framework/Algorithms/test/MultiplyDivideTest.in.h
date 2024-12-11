138ff056b138ff6e166868255d59a557d8e704fc
Kevin A. Tactac <tactacka@ornl.gov>
2024-12-11 14:08:47 -0500

    Add empty units check

--- Framework/Algorithms/test/MultiplyDivideTest.in.h
+++ Framework/Algorithms/test/MultiplyDivideTest.in.h
@@ -1051,6 +1051,7 @@ public:
     auto result = performTest(lhs, rhs, false, DO_DIVIDE ? 1.0 : 4.0, DO_DIVIDE ? 1.4142135625 : 5.6568542436);
     TS_ASSERT(result->isRaggedWorkspace());
     TS_ASSERT(result->isDistribution());
+    TS_ASSERT(result->YUnit().empty());
   }
 
   void test_RaggedWorkspace_and_single_value()
