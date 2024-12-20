1fc2fb3dcbfc055a5a5918a7e869dfe75a82eae2
Pete Peterson <petersonpf@ornl.gov>
2024-12-20 09:13:14 -0500

    Code cleanup

--- Framework/Algorithms/inc/MantidAlgorithms/GetDetectorOffsets.h
+++ Framework/Algorithms/inc/MantidAlgorithms/GetDetectorOffsets.h
@@ -34,7 +34,7 @@ public:
 
   /// Algorithm's version for identification overriding a virtual method
   int version() const override { return 1; }
-  const std::vector<std::string> seeAlso() const override { return {"AlignComponents"}; }
+  const std::vector<std::string> seeAlso() const override { return {"AlignComponents", "ConvertDiffCal"}; }
   /// Algorithm's category for identification overriding a virtual method
   const std::string category() const override { return "Diffraction\\Calibration"; }
 
@@ -44,20 +44,19 @@ private:
   std::map<std::string, std::string> validateInputs() override;
   void exec() override;
   /// Call Gaussian as a Child Algorithm to fit the peak in a spectrum
-  double fitSpectra(const int64_t s);
+  double fitSpectra(const size_t wksp_index);
   /// Create a function string from the given parameters and the algorithm
   /// inputs
   API::IFunction_sptr createFunction(const double peakHeight, const double peakLoc);
   /// Read in all the input parameters
   void retrieveProperties();
 
-  API::MatrixWorkspace_sptr inputW;           ///< A pointer to the input workspace
-  DataObjects::OffsetsWorkspace_sptr outputW; ///< A pointer to the output workspace
-  double m_Xmin = DBL_MAX;                    ///< The start of the X range for fitting
-  double m_Xmax = -DBL_MIN;                   ///< The end of the X range for fitting
-  double m_maxOffset = 0.0;                   ///< The maximum absolute value of offsets
-  double m_dreference = 0.0;                  ///< The expected peak position in d-spacing (?)
-  offset_mode mode;
+  API::MatrixWorkspace_sptr inputW; ///< A pointer to the input workspace
+  double m_Xmin = DBL_MAX;          ///< The start of the X range for fitting
+  double m_Xmax = -DBL_MIN;         ///< The end of the X range for fitting
+  double m_maxOffset = 0.0;         ///< The maximum absolute value of offsets
+  double m_dreference = 0.0;        ///< The expected peak position in d-spacing (?)
+  offset_mode m_mode = offset_mode::relative_offset;
   double m_dideal = 0.0; ///< The known peak centre value from the NIST standard
   /// information
   double m_step = 0.0;        ///< The step size
