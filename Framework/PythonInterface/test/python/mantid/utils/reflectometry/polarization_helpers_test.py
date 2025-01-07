859036f363660945dc27d1bac33ba09cba4eac9b
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2025-01-07 16:05:03 +0000

    Update date

--- Framework/PythonInterface/test/python/mantid/utils/reflectometry/polarization_helpers_test.py
+++ Framework/PythonInterface/test/python/mantid/utils/reflectometry/polarization_helpers_test.py
@@ -1,6 +1,6 @@
 # Mantid Repository : https://github.com/mantidproject/mantid
 #
-# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI,
+# Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
