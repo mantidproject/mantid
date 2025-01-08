f7cd3eed7fd2d60d04d30b940359c55a83bc2c94
Jimoh Yusuf <yusuf.jimoh@stfc.ac.uk>
2025-01-08 20:34:26 +0000

    - added helper methods to test methods in polAarization correction helper methods - added unit tests to splitSpinStateString - added unit tests to test indexOfWorkspaceForSpinState - added unit tests to test workSpaceForSpinState

--- Framework/Algorithms/test/PolarizationCorrections/PolarizationCorrectionsHelpersTest.h
+++ Framework/Algorithms/test/PolarizationCorrections/PolarizationCorrectionsHelpersTest.h
@@ -71,6 +71,48 @@ public:
     TS_ASSERT_THROWS(SpinStatesORSO::addORSOLogForSpinState(ws, "invalidSpinState"), std::invalid_argument &);
   }
 
+  void testSplitSpinStateStringSuccessful() { runTestSplitSpinStateString("01,11,10,00", {"01", "11", "10", "00"}); }
+
+  void testSplitSpinStateStringWithSpaces() {
+    runTestSplitSpinStateString(" 01 ,  11 , 10 ,  00 ", {"01", "11", "10", "00"});
+  }
+
+  void testSplitSpinStateStringEmptyString() { runTestSplitSpinStateString("", {}); }
+
+  void testSplitSpinStateStringSingleItem() { runTestSplitSpinStateString("01", {"01"}); }
+
+  void testIndexOfWorkspaceForSpinState_TargetStateExists() {
+    runTestIndexOfWorkspaceForSpinState({"00", "11", "10", "01"}, "10", true, 2);
+  }
+
+  void testIndexOfWorkspaceForSpinState_TargetStateDoesNotExist() {
+    runTestIndexOfWorkspaceForSpinState({"00", "11", "10", "01"}, "invalid_state", false);
+  }
+
+  void testIndexOfWorkspaceForSpinState_EmptySpinStateOrder() { runTestIndexOfWorkspaceForSpinState({}, "10", false); }
+
+  void testIndexOfWorkspaceForSpinState_DuplicateEntries() {
+    runTestIndexOfWorkspaceForSpinState({"10", "10", "11"}, "10", true, 0);
+  }
+
+  void testIndexOfWorkspaceForSpinState_TrimWhitespace() {
+    runTestIndexOfWorkspaceForSpinState({"00", "11", "10", "01"}, " 10 ", true, 2);
+  }
+
+  void testEmptySpinStateOrder() { runTestWorkspaceForSpinState({"01", "11"}, "", true); }
+
+  void testDuplicateSpinStates() { runTestWorkspaceForSpinState({"01", "01", "11"}, "01", false, "01"); }
+
+  void testWorkspaceForSpinStateUsingFredrikzeSpinStates() {
+    runTestWorkspaceForSpinState(FREDRIKZE_SPIN_STATES, SpinStateConfigurationsFredrikze::PARA_ANTI, false,
+                                 SpinStateConfigurationsFredrikze::PARA_ANTI);
+  }
+
+  void testWorkspaceForSpinStateUsingWildesSpinStates() {
+    runTestWorkspaceForSpinState(WILDES_SPIN_STATES, SpinStateConfigurationsWildes::PLUS_MINUS, false,
+                                 SpinStateConfigurationsWildes::PLUS_MINUS);
+  }
+
 private:
   const std::vector<std::string> WILDES_SPIN_STATES{
       SpinStateConfigurationsWildes::PLUS_PLUS,  SpinStateConfigurationsWildes::PLUS_MINUS,
@@ -138,6 +180,42 @@ private:
       const auto &run = ws->run();
       TS_ASSERT(run.hasProperty(SpinStatesORSO::LOG_NAME));
       TS_ASSERT_EQUALS(run.getPropertyValueAsType<std::string>(SpinStatesORSO::LOG_NAME), ORSO_SPIN_STATES[i]);
+    }
+  }
+
+  void runTestSplitSpinStateString(const std::string &spinStates, const std::vector<std::string> &expectedResult) {
+    std::vector<std::string> result = PolarizationCorrectionsHelpers::splitSpinStateString(spinStates);
+    TS_ASSERT_EQUALS(result.size(), expectedResult.size());
+
+    for (size_t i = 0; i < result.size(); ++i) {
+      TS_ASSERT_EQUALS(result[i], expectedResult[i]);
+    }
+  }
+
+  void runTestIndexOfWorkspaceForSpinState(const std::vector<std::string> &spinStateOrder,
+                                           const std::string &targetSpinState, bool expectedHasValue,
+                                           int expectedIndex = -1) {
+    auto index = PolarizationCorrectionsHelpers::indexOfWorkspaceForSpinState(spinStateOrder, targetSpinState);
+
+    if (expectedHasValue) {
+      TS_ASSERT(index.has_value());
+      TS_ASSERT_EQUALS(index.value(), expectedIndex);
+    } else {
+      ASSERT_FALSE(index.has_value());
+    }
+  }
+
+  void runTestWorkspaceForSpinState(const std::vector<std::string> &spinStateOrder, const std::string &targetSpinState,
+                                    bool expectedIsNull, const std::string &expectedWorkspaceName = "") {
+    auto grp = createGroupWorkspaceToMatchSpinStates(spinStateOrder);
+    auto ws =
+        PolarizationCorrectionsHelpers::workspaceForSpinState(grp, boost::join(spinStateOrder, ","), targetSpinState);
+
+    if (expectedIsNull) {
+      TS_ASSERT(ws == nullptr);
+    } else {
+      TS_ASSERT(ws != nullptr);
+      TS_ASSERT_EQUALS(ws->getName(), expectedWorkspaceName);
     }
   }
 };
