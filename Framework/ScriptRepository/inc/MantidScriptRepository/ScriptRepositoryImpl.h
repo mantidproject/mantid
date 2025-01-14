221dca923988aab04ba6fba17a9863655c9414a4
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2025-01-14 13:59:55 +0000

    fix cppcheck return by const ref

--- Framework/ScriptRepository/inc/MantidScriptRepository/ScriptRepositoryImpl.h
+++ Framework/ScriptRepository/inc/MantidScriptRepository/ScriptRepositoryImpl.h
@@ -111,7 +111,7 @@ public:
   int setAutoUpdate(const std::string &input_path, bool option = true) override;
 
   /// @deprecated Should avoid this, it is not in the design file.
-  std::string localRepository() const { return local_repository; }
+  const std::string &localRepository() const { return local_repository; }
 
   virtual void doDownloadFile(const std::string &url_file, const std::string &local_file_path = "");
   // convenient method to allow to perform the unit tests on remove files.
