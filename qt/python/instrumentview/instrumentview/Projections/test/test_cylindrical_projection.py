95e423ef71237691973a04efaf973fa8213f41bd
James Clarke <james.clarke@stfc.ac.uk>
2025-01-17 11:46:54 +0000

    Add tests for projections

---
+++ qt/python/instrumentview/instrumentview/Projections/test/test_cylindrical_projection.py
@@ -0,0 +1,13 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import unittest
+
+
+class TestCylindricalProjection(unittest.TestCase):
+    def test_calculate_detector_coordinates(self):
+        self.assertTrue(False)
