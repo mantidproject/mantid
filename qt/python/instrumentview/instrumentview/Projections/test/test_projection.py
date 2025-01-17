9df31cc099813ed4788efea940e906547ea71d2d
James Clarke <james.clarke@stfc.ac.uk>
2025-01-17 11:46:54 +0000

    Add tests for projections

---
+++ qt/python/instrumentview/instrumentview/Projections/test/test_projection.py
@@ -0,0 +1,17 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import unittest
+
+
+def create_mock_workspace_for_projections(self):
+    pass
+
+
+class TestProjection(unittest.TestCase):
+    def test_test(self):
+        pass
