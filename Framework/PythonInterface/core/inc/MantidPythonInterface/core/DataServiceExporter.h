abf9e4e8087ebb99b32c10b115a147e7f0c247bb
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-01-28 17:02:32 +0000

    update suppression file

--- Framework/PythonInterface/core/inc/MantidPythonInterface/core/DataServiceExporter.h
+++ Framework/PythonInterface/core/inc/MantidPythonInterface/core/DataServiceExporter.h
@@ -165,8 +165,8 @@ template <typename SvcType, typename SvcPtrType> struct DataServiceExporter {
       item = self.retrieve(name);
     } catch (Exception::NotFoundError &) {
       // Translate into a Python KeyError
-      std::string err = "'" + name + "' does not exist.";
-      PyErr_SetString(PyExc_KeyError, err.c_str());
+//      std::string err = "'" + name + "' does not exist.";
+//      PyErr_SetString(PyExc_KeyError, err.c_str());
       throw boost::python::error_already_set();
     }
     return WeakPtr(item);
