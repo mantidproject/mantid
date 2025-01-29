0f31aafbd91aa992172add12bdbb4b3cc57a9a8b
Richard Waite <richard.waite@stfc.ac.uk>
2025-01-29 15:33:27 +0000

    Not use macro to return

--- Framework/PythonInterface/mantid/api/src/Exports/IAlgorithm.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/IAlgorithm.cpp
@@ -315,7 +315,7 @@ PyObject *getAlgorithmID(const IAlgorithm &self) {
   AlgorithmID id = self.getAlgorithmID();
   if (id)
     return to_python_value<AlgorithmIDProxy>()(AlgorithmIDProxy(id));
-  return Py_NONE;
+  return Py_NewRef(Py_None)
 }
 
 //--------------------------------------------------------------------------------------
