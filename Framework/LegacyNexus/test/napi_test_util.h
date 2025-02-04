783992e8c8fb02e2a1d0c2d3bab317895e342e2c
Pete Peterson <petersonpf@ornl.gov>
2025-02-04 14:09:17 -0500

    Copy nexus functions into new subpackage for muons
    As part of #38332, this isolates the current nexus API which supports
    both the HDF4 and HDF5 backends so the muon algorithms can continue
    to use it until they change over to be hdf4 only.

---
+++ Framework/LegacyNexus/test/napi_test_util.h
@@ -0,0 +1,21 @@
+#include <iosfwd>
+#include <string>
+
+// return to system when any test failed
+constexpr int TEST_FAILED{1};
+// return to system when all tests succeed
+constexpr int TEST_SUCCEED{0};
+// error out and return failure
+#define ON_ERROR(msgstr)                                                                                               \
+  {                                                                                                                    \
+    std::cerr << msgstr << std::endl;                                                                                  \
+    return TEST_FAILED;                                                                                                \
+  }
+
+namespace LegacyNexusTest {
+void print_data(const std::string &prefix, std::ostream &stream, const void *data, const NXnumtype type, const int num);
+void write_dmc01(const std::string &filename);
+void write_dmc02(const std::string &filename);
+/// remove a file if it exists
+void removeFile(const std::string &filename);
+} // namespace LegacyNexusTest
