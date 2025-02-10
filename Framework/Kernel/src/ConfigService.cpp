811604541fb446dfa8448f6db4503a486b5d69da
James Clarke <james.clarke@stfc.ac.uk>
2025-02-10 15:26:30 +0000

    Change logic to match Poco::Path::isDirectory
    Poco::Path::isDirectory only checks if the Poco::Path object has something
    labelled as a filename, not necessarily if it exists. Hence to get the
    same functionality we need to check if the path exists or not, and also
    if it is a directory or not. If it's an existing file then it's no
    good.

--- Framework/Kernel/src/ConfigService.cpp
+++ Framework/Kernel/src/ConfigService.cpp
@@ -1441,7 +1441,8 @@ void ConfigServiceImpl::appendDataSearchSubDir(const std::string &subdir) {
     return;
   }
 
-  if (!std::filesystem::is_directory(subDirPath) || !subDirPath.is_relative()) {
+  if ((std::filesystem::exists(subDirPath) && !std::filesystem::is_directory(subDirPath)) ||
+      !subDirPath.is_relative()) {
     return;
   }
 
