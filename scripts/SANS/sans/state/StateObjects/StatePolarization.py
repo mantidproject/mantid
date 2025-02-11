524a0ff86ca8ad0dc0a2ddab9a88d7895d2f985a
Caila Finn <caila.finn@stfc.ac.uk>
2025-02-11 18:12:06 +0000

    Add StateComponent class
    RE #38524

--- scripts/SANS/sans/state/StateObjects/StatePolarization.py
+++ scripts/SANS/sans/state/StateObjects/StatePolarization.py
@@ -13,9 +13,42 @@ from sans.state.JsonSerializable import JsonSerializable
 # ----------------------------------------------------------------------------------------------------------------------
 
 
+class StateComponent(metaclass=JsonSerializable):
+    idf_component_name: None | str
+    device_name: None | str
+    device_type: None | str
+    location_x: None | int
+    location_y: None | int
+    location_z: None | int
+    transmission: None | str
+    efficiency: None | str
+
+    def __init__(self):
+        super(StateComponent, self).__init__()
+        # Name of the component in the IDF.
+        self.idf_component_name = None
+
+        # Only used if we want to use a name different from what is defined in the IDF.
+        self.device_name = None
+
+        # Used when overriding values in the IDF, or if the device is not in the IDF at all.
+        self.device_type = None
+        self.location_x = None
+        self.location_y = None
+        self.location_z = None
+
+        # Workspace names for correction workspaces
+        self.transmission = None
+        self.efficiency = None
+
+
 class StatePolarization(metaclass=JsonSerializable):
+    flipper_configuration: None | str
+    spin_configuration: None | str
+    flippers: list[StateComponent]
+
     def __init__(self):
         super(StatePolarization, self).__init__()
-
-        self.flipper_configuraiton = None  # : Str()
-        self.spin_configuration = None  # : Str()
+        self.flipper_configuration = None
+        self.spin_configuration = None
+        self.flippers = []
