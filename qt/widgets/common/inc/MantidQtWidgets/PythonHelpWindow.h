a9a8bdca01f39961e4df3f28736679f315d717b7
Darsh Dinger <106342815+darshdinger@users.noreply.github.com>
2025-02-11 15:03:47 -0500

    Update PythonHelpWindow.h

--- qt/widgets/common/inc/MantidQtWidgets/PythonHelpWindow.h
+++ qt/widgets/common/inc/MantidQtWidgets/PythonHelpWindow.h
@@ -1,3 +1,9 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
 #include "MantidQtWidgets/Common/DllOption.h"
