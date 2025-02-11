a5e63d8c3d9988f68a0b33b9f464049674f2e6e7
Darsh Dinger <106342815+darshdinger@users.noreply.github.com>
2025-02-11 15:02:43 -0500

    Update PythonHelpBridge.h

--- qt/widgets/common/inc/MantidQtWidgets/PythonHelpBridge.h
+++ qt/widgets/common/inc/MantidQtWidgets/PythonHelpBridge.h
@@ -1,10 +1,22 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2013 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
-#include "DllOption.h"
+#include "MantidQtWidgets/Common/DllOption.h"
 #include <string>
 
-class PythonHelpBridge {
+namespace MantidQt {
+namespace MantidWidgets {
+
+class EXPORT_OPT_MANTIDQT_COMMON PythonHelpBridge {
 public:
   PythonHelpBridge();
   void showHelpPage(const std::string &relative_url);
 };
+
+} // namespace MantidWidgets
+} // namespace MantidQt
