f113fa9fd85eaa29321332318f49fe034a9cc8e5
Darsh <ddarsh5544@gmail.com>
2025-02-14 14:59:41 -0500

    Updates to insure a better MVP structure and resolve issues

--- qt/python/mantidqt/mantidqt/widgets/helpwindow/helpwindowmodel.py
+++ qt/python/mantidqt/mantidqt/widgets/helpwindow/helpwindowmodel.py
@@ -4,10 +4,44 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
-class HelpWindowModel:
-    def __init__(self):
-        self.help_url = "https://docs.mantidproject.org/"
+import os
+from qtpy.QtCore import QUrl
 
-    def get_help_url(self):
-        """Get the help documentation URL."""
-        return self.help_url
+
+class HelpWindowModel:
+    def __init__(self, local_docs_base=None, online_base="https://docs.mantidproject.org/"):
+        self.local_docs_base = local_docs_base
+        self.online_base = online_base.rstrip("/")
+
+    def build_help_url(self, relative_url):
+        """
+        Returns a QUrl pointing to either local or online docs for the given relative URL.
+        """
+        if not relative_url or not relative_url.endswith(".html"):
+            relative_url = "index.html"
+
+        if self.local_docs_base and os.path.isdir(self.local_docs_base):
+            full_path = os.path.join(self.local_docs_base, relative_url)
+            return QUrl.fromLocalFile(full_path)
+        else:
+            return QUrl(self.online_base + "/" + relative_url)
+
+    def is_local_docs_enabled(self):
+        """
+        :return: True if local_docs_base is set and is a valid directory
+        """
+        return bool(self.local_docs_base and os.path.isdir(self.local_docs_base))
+
+    def get_home_url(self):
+        """
+        Return the 'home' page URL:
+          - Local 'index.html' if local docs are enabled
+          - Online docs homepage otherwise
+        """
+        if self.is_local_docs_enabled():
+            # local docs base + 'index.html'
+            full_path = os.path.join(self.local_docs_base, "index.html")
+            return QUrl.fromLocalFile(full_path)
+        else:
+            # online base
+            return QUrl(self.online_base + "/index.html")
