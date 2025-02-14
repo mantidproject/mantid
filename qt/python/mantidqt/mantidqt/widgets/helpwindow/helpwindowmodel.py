38e61f2e3958d1d30895f668d0b2926f1521a9fd
Darsh <ddarsh5544@gmail.com>
2025-02-14 09:30:44 -0500

    Added main

--- qt/python/mantidqt/mantidqt/widgets/helpwindow/helpwindowmodel.py
+++ qt/python/mantidqt/mantidqt/widgets/helpwindow/helpwindowmodel.py
@@ -1,3 +1,9 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2017 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
 class HelpWindowModel:
     def __init__(self):
         self.help_url = "https://docs.mantidproject.org/"
