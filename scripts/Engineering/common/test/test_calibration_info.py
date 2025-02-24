ce9d4ad55eb1a27f7e71da6ca150c7576210f0db
andy-bridger <71634246+andy-bridger@users.noreply.github.com>
2025-02-24 11:53:21 +0000

    Update scripts/Engineering/common/test/test_calibration_info.py
    Co-authored-by: Waruna Priyankara J A Wickramasingha <142818102+warunawickramasingha@users.noreply.github.com>

--- scripts/Engineering/common/test/test_calibration_info.py
+++ scripts/Engineering/common/test/test_calibration_info.py
@@ -1,6 +1,6 @@
 # Mantid Repository : https://github.com/mantidproject/mantid
 #
-# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
