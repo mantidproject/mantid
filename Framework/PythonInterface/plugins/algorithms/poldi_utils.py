869f52d33df504c40b6928a0b8c31edeb94eba70
Richard Waite <richard.waite@stfc.ac.uk>
2025-02-28 18:47:45 +0000

    Add release notes
    And update copyright date in headers

--- Framework/PythonInterface/plugins/algorithms/poldi_utils.py
+++ Framework/PythonInterface/plugins/algorithms/poldi_utils.py
@@ -1,4 +1,9 @@
-# import mantid algorithms, numpy and matplotlib
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
 from mantid.api import AlgorithmManager, AnalysisDataService as ADS
 import numpy as np
 from mantid.kernel import UnitConversion, DeltaEModeType, UnitParams, UnitParametersMap
