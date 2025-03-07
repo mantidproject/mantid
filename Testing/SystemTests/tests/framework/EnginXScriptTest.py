ed5a0970e31fe8575ab253122983838af1b1c4a7
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-03-07 15:12:40 +0000

    add system-test to check for bug-38879

--- Testing/SystemTests/tests/framework/EnginXScriptTest.py
+++ Testing/SystemTests/tests/framework/EnginXScriptTest.py
@@ -85,6 +85,42 @@ class FocusCroppedSpectraSameDiffConstsAsBank(systemtesting.MantidSystemTest):
         _try_delete_cal_and_focus_dirs(CWDIR)
 
 
+class TestSwappingCustomCroppingChangesFocussing(systemtesting.MantidSystemTest):
+    def runTest(self):
+        self._diff_consts = self.run_enginx_and_get_diff_consts("1-1200")
+
+        # run again with different cropping window and store the resulting diff consts
+        self._diff_consts2 = self.run_enginx_and_get_diff_consts("1-100")
+
+        # run the first test again and store the resulting diff consts
+        self._diff_consts3 = self.run_enginx_and_get_diff_consts("1-1200")
+
+    def validate(self):
+        for const, delta in ([UnitParams.difc, 5], [UnitParams.difa, 1], [UnitParams.tzero, 2]):
+            # check the second run is correctly using a different cropping than the first
+            self.assertNotAlmostEqual(self._diff_consts[const], self._diff_consts2[const], delta=delta)
+            # check the third run is unaffected by the second so is the same as the first
+            self.assertAlmostEqual(self._diff_consts[const], self._diff_consts3[const], delta=delta)
+
+    def run_enginx_and_get_diff_consts(self, specs: str):
+        enginx = EnginX(
+            vanadium_run="ENGINX236516",
+            focus_runs=["ENGINX299080"],
+            save_dir=CWDIR,
+            full_inst_calib_path=FULL_CALIB,
+            ceria_run="ENGINX193749",
+            group=GROUP.CROPPED,
+            spectrum_num=specs,
+        )
+        enginx.main(plot_cal=False, plot_foc=False)
+        ws_foc = ADS.retrieve("299080_engggui_focusing_output_ws_Cropped")
+        return ws_foc.spectrumInfo().diffractometerConstants(0)
+
+    def cleanup(self):
+        ADS.clear()
+        _try_delete_cal_and_focus_dirs(CWDIR)
+
+
 class FocusTexture(systemtesting.MantidSystemTest):
     def runTest(self):
         enginx = EnginX(
