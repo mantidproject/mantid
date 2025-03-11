e64f29c8e55200cbd916829416c7ce81b5e13d71
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2025-03-11 13:19:36 +0000

    Add polarization to SaveNXcanSAS base

--- Framework/DataHandling/inc/MantidDataHandling/SaveNXcanSASBase.h
+++ Framework/DataHandling/inc/MantidDataHandling/SaveNXcanSASBase.h
@@ -10,6 +10,7 @@
 #include "MantidAPI/MatrixWorkspace_fwd.h"
 #include "MantidDataHandling/DllConfig.h"
 #include <H5Cpp.h>
+#include <MantidAPI/WorkspaceGroup_fwd.h>
 #include <filesystem>
 
 namespace Mantid {
@@ -22,14 +23,23 @@ namespace DataHandling {
 class MANTID_DATAHANDLING_DLL SaveNXcanSASBase : public API::Algorithm {
 protected:
   void saveSingleWorkspaceFile(const API::MatrixWorkspace_sptr &workspace, const std::filesystem::path &path);
+  void savePolarizedGroup(const API::WorkspaceGroup_sptr &wsGroup, const std::filesystem::path &path);
   void addStandardMetadata(const Mantid::API::MatrixWorkspace_sptr &workspace, H5::Group &sasEntry);
+  void addPolarizedMetadata(const Mantid::API::MatrixWorkspace_sptr &workspace, H5::Group &sasEntry);
   void addData(H5::Group &group, const Mantid::API::MatrixWorkspace_sptr &workspace);
+  void addPolarizedData(H5::Group &group, const Mantid::API::WorkspaceGroup_sptr &wsGroup);
+
   H5::Group addSasEntry(H5::H5File &file, const Mantid::API::MatrixWorkspace_sptr &workspace,
                         const std::string &suffix);
   void initStandardProperties();
+  void initPolarizedProperties();
   std::map<std::string, std::string> validateStandardInputs();
+  std::map<std::string, std::string> validatePolarizedInputs(std::map<std::string, std::string> &result);
 
   std::unique_ptr<API::Progress> m_progress = nullptr;
+
+private:
+  std::map<std::string, std::string> createComponentMap();
 };
 
 } // namespace DataHandling
