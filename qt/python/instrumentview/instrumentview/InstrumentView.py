aa3ba1e9e2089fa8ca8f9ca663a99b3712bf946d
James Clarke <james.clarke@stfc.ac.uk>
2025-03-11 10:32:45 +0000

    Remove hard-coded file path from module
    The file to load is now an argument when launching the instrumentview
    module. It also now uses the more general Load algorithm, instead of
    the separate algorithm, e.g. LoadEventNexus

--- qt/python/instrumentview/instrumentview/InstrumentView.py
+++ qt/python/instrumentview/instrumentview/InstrumentView.py
@@ -5,34 +5,23 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 from instrumentview.FullInstrumentViewWindow import FullInstrumentViewWindow
-from mantid.simpleapi import LoadRaw, LoadNexus, LoadEventNexus
+from mantid.simpleapi import Load
 from pathlib import Path
 from qtpy.QtWidgets import QApplication
 import sys
 
 
 class InstrumentView:
-    def main():
-        sys.exit(InstrumentView.start_app_open_window())
+    def main(file_path: Path):
+        sys.exit(InstrumentView.start_app_open_window(file_path))
 
-    def start_app_open_window():
+    def start_app_open_window(file_path: Path):
         app = QApplication(sys.argv)
-        workspace_path = Path(r"C:\Tutorial\SampleData-ISIS\MAR11060.raw")
-        is_event = False
-        if is_event:
-            ws = LoadEventNexus(str(workspace_path), StoreInADS=False)
-        elif workspace_path.suffix == ".nxs":
-            ws = LoadNexus(str(workspace_path), StoreInADS=False)
-        else:
-            ws = LoadRaw(str(workspace_path), StoreInADS=False)
+        ws = Load(str(file_path), StoreInADS=False)
         window = FullInstrumentViewWindow(ws)
         window.show()
         app.exec_()
 
 
-def open_instrument_view():
-    InstrumentView.start_app_open_window()
-
-
-if __name__ == "__main__":
-    InstrumentView.main()
+def open_instrument_view(file_path: str):
+    InstrumentView.start_app_open_window(file_path)
