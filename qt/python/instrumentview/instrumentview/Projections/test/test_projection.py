81fda0f741eaa4cb6a46a25f2211200d19382b32
James Clarke <james.clarke@stfc.ac.uk>
2025-03-19 11:57:50 +0000

    Refactor projection classes and add tests

--- qt/python/instrumentview/instrumentview/Projections/test/test_projection.py
+++ qt/python/instrumentview/instrumentview/Projections/test/test_projection.py
@@ -5,13 +5,59 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 
+import unittest.mock
+from instrumentview.Projections.cylindrical_projection import cylindrical_projection
+
+import numpy as np
 import unittest
-
-
-def create_mock_workspace_for_projections(self):
-    pass
+from unittest.mock import MagicMock
 
 
 class TestProjection(unittest.TestCase):
-    def test_test(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_workspace = MagicMock()
+        cls.mock_componentInfo = MagicMock()
+        cls.mock_componentInfo.samplePosition.return_value = np.array([0, 0, 0])
+        cls.mock_componentInfo.position = cls.mock_position
+        cls.mock_componentInfo.hasValidShape.return_value = True
+        cls.mock_workspace.componentInfo.return_value = cls.mock_componentInfo
+        cls.detector_indices = [0, 1, 2]
+
+    def mock_position(index: int):
+        if index == 0:
+            return [0, 1, 0]
+        if index == 1:
+            return [2, 1, 0]
+        if index == 2:
+            return [-2, 1, 0]
+
+        raise ValueError(f"Unexpected component index: {index}")
+
+    def test_apply_x_correction_below_min(self):
+        proj = cylindrical_projection(self.mock_workspace, self.detector_indices, [0, 1, 0])
+        x_min = proj._x_range[0]
+        x_max = proj._x_range[1]
+        proj._detector_x_coordinates[0] = x_min - np.pi / 2
+        self.assertLess(proj._detector_x_coordinates[0], x_min)
+        proj._apply_x_correction(0)
+        self.assertGreaterEqual(proj._detector_x_coordinates[0], x_min)
+        self.assertLessEqual(proj._detector_x_coordinates[0], x_max)
+
+    def test_apply_x_correction_above_max(self):
+        proj = cylindrical_projection(self.mock_workspace, self.detector_indices, [0, 1, 0])
+        x_min = proj._x_range[0]
+        x_max = proj._x_range[1]
+        proj._detector_x_coordinates[0] = x_max + np.pi / 2
+        self.assertGreater(proj._detector_x_coordinates[0], x_max)
+        proj._apply_x_correction(0)
+        self.assertGreaterEqual(proj._detector_x_coordinates[0], x_min)
+        self.assertLessEqual(proj._detector_x_coordinates[0], x_max)
+
+    @unittest.mock.patch("instrumentview.Projections.projection.projection._apply_x_correction")
+    def test_find_and_correct_x_gap(self, mock_apply_x_correction):
+        proj = cylindrical_projection(self.mock_workspace, self.detector_indices, [0, 0, 1])
+        proj._u_period = (proj._x_range[1] - proj._x_range[0]) / 2
+        mock_apply_x_correction.reset_mock()
+        proj._find_and_correct_x_gap()
+        self.assertEqual(len(self.detector_indices), mock_apply_x_correction.call_count)
--- qt/python/instrumentview/instrumentview/Projections/test/test_spherical_projection.py
+++ qt/python/instrumentview/instrumentview/Projections/test/test_spherical_projection.py
@@ -4,3 +4,70 @@
 #   NScD Oak Ridge National Laboratory, European Spallation Source,
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
+import unittest.mock
+from instrumentview.Projections.spherical_projection import spherical_projection
+import unittest
+from unittest.mock import MagicMock
+import numpy as np
+
+
+class TestSphericalProjection(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_workspace = MagicMock()
+        cls.mock_componentInfo = MagicMock()
+        cls.mock_componentInfo.samplePosition.return_value = np.array([0, 0, 0])
+        cls.radius = 2.0
+        cls.mock_componentInfo.position = lambda x: cls.mock_position(x, cls.radius)
+        cls.mock_componentInfo.hasValidShape.return_value = True
+        cls.mock_workspace.componentInfo.return_value = cls.mock_componentInfo
+        cls.detector_indices = [0, 1, 2]
+        cls.abs_tol = 1e-9
+
+    def mock_position(index: int, radius: float):
+        # All points on a sphere
+        if index == 0:
+            return [0, radius, 0]
+        if index == 1:
+            return [0, 0, radius]
+        if index == 2:
+            return [radius, 0, 0]
+
+        raise ValueError(f"Unexpected component index: {index}")
+
+    def test_calculate_2d_coordinates_x(self):
+        # x-axis projection
+        self._run_test(
+            projection_axis=[1, 0, 0],
+            expected_x_axis=[0, 1, 0],
+            expected_y_axis=[0, 0, 1],
+            expected_projections=[(0, -np.pi / 2), (-np.pi / 2, -np.pi / 2), (0, 0)],
+        )
+
+    def test_calculate_2d_coordinates_y(self):
+        # y-axis projection
+        self._run_test(
+            projection_axis=[0, 1, 0],
+            expected_x_axis=[0, 0, 1],
+            expected_y_axis=[1, 0, 0],
+            expected_projections=[(0, 0), (0, -np.pi / 2), (-np.pi / 2, -np.pi / 2)],
+        )
+
+    def test_calculate_2d_coordinates_z(self):
+        # z-axis projection
+        self._run_test(
+            projection_axis=[0, 0, 1],
+            expected_x_axis=[1, 0, 0],
+            expected_y_axis=[0, 1, 0],
+            expected_projections=[(-np.pi / 2, -np.pi / 2), (0, 0), (0, -np.pi / 2)],
+        )
+
+    def _run_test(self, projection_axis, expected_x_axis, expected_y_axis, expected_projections):
+        sphere = spherical_projection(self.mock_workspace, self.detector_indices, projection_axis)
+        np.testing.assert_allclose(sphere._projection_axis, projection_axis, atol=self.abs_tol)
+        np.testing.assert_allclose(sphere._x_axis, expected_x_axis, atol=self.abs_tol)
+        np.testing.assert_allclose(sphere._y_axis, expected_y_axis, atol=self.abs_tol)
+        for i in self.detector_indices:
+            expected = expected_projections[i]
+            calculated = sphere.coordinate_for_detector(i)
+            np.testing.assert_allclose(calculated, expected, atol=self.abs_tol)
