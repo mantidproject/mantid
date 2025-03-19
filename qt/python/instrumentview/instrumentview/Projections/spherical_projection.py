eefb5c165dc91d710d35832928105b00e8ab5ca1
James Clarke <james.clarke@stfc.ac.uk>
2025-03-19 11:57:50 +0000

    Refactor projection classes and add tests

--- qt/python/instrumentview/instrumentview/Projections/spherical_projection.py
+++ qt/python/instrumentview/instrumentview/Projections/spherical_projection.py
@@ -10,23 +10,7 @@ import numpy as np
 
 class spherical_projection(projection):
     def __init__(self, workspace, detector_indices, axis: np.ndarray):
-        super().__init__(workspace, detector_indices)
-        self._projection_axis = np.array(axis)
-        self._calculate_axes()
-        self._calculate_detector_coordinates()
-        self._find_and_correct_x_gap()
-
-    def _calculate_detector_coordinates(self):
-        x_values = []
-        y_values = []
-        for det_id in self._detector_indices:
-            x, y = self._calculate_2d_coordinates(det_id)
-            x_values.append(x)
-            y_values.append(y)
-        self._detector_x_coordinates = np.array(x_values)
-        self._detector_y_coordinates = np.array(y_values)
-        self._x_range = (self._detector_x_coordinates.min(), self._detector_x_coordinates.max())
-        self._y_range = (self._detector_y_coordinates.min(), self._detector_y_coordinates.max())
+        super().__init__(workspace, detector_indices, axis)
 
     def _calculate_2d_coordinates(self, detector_index: int) -> tuple[float, float]:
         detector_relative_position = np.array(self._component_info.position(detector_index)) - self._sample_position
