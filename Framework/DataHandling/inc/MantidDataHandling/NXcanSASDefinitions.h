98b28a7623893c96af41c1c60ddd13ef865d30cb
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2025-03-20 13:22:31 +0000

    typo in definitions

--- Framework/DataHandling/inc/MantidDataHandling/NXcanSASDefinitions.h
+++ Framework/DataHandling/inc/MantidDataHandling/NXcanSASDefinitions.h
@@ -46,7 +46,7 @@ const std::string sasEntryDefinitionFormat = "NXcanSAS";
 const std::string sasEntryTitle = "title";
 const std::string sasEntryRun = "run";
 const std::string sasEntryRunInLogs = "run_number";
-const std::String sasEntryDefaultSuffix = "01";
+const std::string sasEntryDefaultSuffix = "01";
 
 // SASdata
 const std::string sasDataClassAttr = "SASdata";
