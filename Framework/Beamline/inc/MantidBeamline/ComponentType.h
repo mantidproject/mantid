d08a9a68c81dc14b1ee9d62f052248efa1a8ab05
James Clarke <james.clarke@stfc.ac.uk>
2025-03-20 13:28:30 +0000

    Inline function only used in one place.

--- Framework/Beamline/inc/MantidBeamline/ComponentType.h
+++ Framework/Beamline/inc/MantidBeamline/ComponentType.h
@@ -11,28 +11,5 @@
 namespace Mantid {
 namespace Beamline {
 enum class ComponentType { Generic, Infinite, Grid, Rectangular, Structured, Unstructured, Detector, OutlineComposite };
-
-static std::string componentTypeString(const ComponentType type) {
-  switch (type) {
-  case ComponentType::Generic:
-    return "Generic";
-  case ComponentType::Infinite:
-    return "Infinite";
-  case ComponentType::Grid:
-    return "Grid";
-  case ComponentType::Rectangular:
-    return "Rectangular";
-  case ComponentType::Structured:
-    return "Structured";
-  case ComponentType::Unstructured:
-    return "Unstructured";
-  case ComponentType::Detector:
-    return "Detector";
-  case ComponentType::OutlineComposite:
-    return "OutlineComposite";
-  default:
-    throw std::invalid_argument("Unknown ComponentType type");
-  }
-}
 } // namespace Beamline
 } // namespace Mantid
