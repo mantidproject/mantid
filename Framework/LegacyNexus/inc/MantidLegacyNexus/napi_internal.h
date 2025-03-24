e71fac06ee0f6b2a080c3a4eb099a3f1a5b00365
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-03-24 14:37:31 +0000

    remove unused napi5 code

--- Framework/LegacyNexus/inc/MantidLegacyNexus/napi_internal.h
+++ Framework/LegacyNexus/inc/MantidLegacyNexus/napi_internal.h
@@ -45,45 +45,24 @@ using Mantid::LegacyNexus::NXstatus;
 
 typedef struct {
   NXhandle pNexusData;
-  NXstatus (*nxreopen)(NXhandle pOrigHandle, NXhandle *pNewHandle);
   NXstatus (*nxclose)(NXhandle *pHandle);
-  NXstatus (*nxflush)(NXhandle *pHandle);
-  NXstatus (*nxmakegroup)(NXhandle handle, CONSTCHAR *name, CONSTCHAR *NXclass);
   NXstatus (*nxopengroup)(NXhandle handle, CONSTCHAR *name, CONSTCHAR *NXclass);
   NXstatus (*nxclosegroup)(NXhandle handle);
-  NXstatus (*nxmakedata64)(NXhandle handle, CONSTCHAR *label, NXnumtype datatype, int rank, int64_t dim[]);
-  NXstatus (*nxcompmakedata64)(NXhandle handle, CONSTCHAR *label, NXnumtype datatype, int rank, int64_t dim[],
-                               int comp_typ, int64_t const bufsize[]);
-  NXstatus (*nxcompress)(NXhandle handle, int compr_type);
   NXstatus (*nxopendata)(NXhandle handle, CONSTCHAR *label);
   NXstatus (*nxclosedata)(NXhandle handle);
   NXstatus (*nxputdata)(NXhandle handle, const void *data);
-  NXstatus (*nxputattr)(NXhandle handle, CONSTCHAR *name, const void *data, int iDataLen, NXnumtype iType);
-  NXstatus (*nxputattra)(NXhandle handle, CONSTCHAR *name, const void *data, const int rank, const int dim[],
-                         const NXnumtype iType);
-  NXstatus (*nxputslab64)(NXhandle handle, const void *data, const int64_t start[], const int64_t size[]);
   NXstatus (*nxgetdataID)(NXhandle handle, NXlink *pLink);
-  NXstatus (*nxmakelink)(NXhandle handle, NXlink *pLink);
-  NXstatus (*nxmakenamedlink)(NXhandle handle, CONSTCHAR *newname, NXlink *pLink);
   NXstatus (*nxgetdata)(NXhandle handle, void *data);
   NXstatus (*nxgetinfo64)(NXhandle handle, int *rank, int64_t dimension[], NXnumtype *datatype);
   NXstatus (*nxgetnextentry)(NXhandle handle, NXname name, NXname nxclass, NXnumtype *datatype);
-  NXstatus (*nxgetslab64)(NXhandle handle, void *data, const int64_t start[], const int64_t size[]);
   NXstatus (*nxgetnextattr)(NXhandle handle, NXname pName, int *iLength, NXnumtype *iType);
   NXstatus (*nxgetnextattra)(NXhandle handle, NXname pName, int *rank, int dim[], NXnumtype *iType);
   NXstatus (*nxgetattr)(NXhandle handle, const char *name, void *data, int *iDataLen, NXnumtype *iType);
-  NXstatus (*nxgetattra)(NXhandle handle, const char *name, void *data);
   NXstatus (*nxgetattrainfo)(NXhandle handle, NXname pName, int *rank, int dim[], NXnumtype *iType);
   NXstatus (*nxgetattrinfo)(NXhandle handle, int *no_items);
   NXstatus (*nxgetgroupID)(NXhandle handle, NXlink *pLink);
-  NXstatus (*nxgetgroupinfo)(NXhandle handle, int *no_items, NXname name, NXname nxclass);
-  NXstatus (*nxsameID)(NXhandle handle, NXlink const *pFirstID, NXlink const *pSecondID);
   NXstatus (*nxinitgroupdir)(NXhandle handle);
   NXstatus (*nxinitattrdir)(NXhandle handle);
-  NXstatus (*nxprintlink)(NXhandle handle, NXlink const *link);
-  NXstatus (*nxnativeexternallink)(NXhandle handle, CONSTCHAR *name, CONSTCHAR *externalfile, CONSTCHAR *remotetarget);
-  NXstatus (*nxnativeinquirefile)(NXhandle handle, char *externalfile, const int filenamelength);
-  NXstatus (*nxnativeisexternallink)(NXhandle handle, CONSTCHAR *name, char *url, int urllen);
   int stripFlag;
   int checkNameSyntax;
   NXaccess access_mode;
