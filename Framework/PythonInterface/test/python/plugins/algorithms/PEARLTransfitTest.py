54237e54856b65ccbb274510b0118e7e62c329cc
Richard Waite <richard.waite@stfc.ac.uk>
2025-03-25 14:34:58 +0000

    Add tests for non default resonance
    Default Ediv not suitable for this resonance, so have test with non default Ediv and another with RebinInEnergy=False

--- Framework/PythonInterface/test/python/plugins/algorithms/PEARLTransfitTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/PEARLTransfitTest.py
@@ -66,6 +66,20 @@ class PEARLTransfitTest(unittest.TestCase):
         PEARLTransfit(Files="PEARL00112777.nxs", Calibration=True)
         self._assert_output_workspaces_exist("S_fit")
 
+    def test_calibration_run_non_default_foil_no_rebin(self):
+        # Test with non default resonance, note default Ediv not suitable so using RebinInEnergy=False
+        PEARLTransfit(Files="PEARL00112777", Calibration=True, FoilType="Hf02", RebinInEnergy=False)
+        self._assert_output_workspaces_exist("S_fit")
+        self._assert_fit_parameters("S_fit", cost=0.89391, peak_cen=2380.03, peak_cen_err=0.31927)
+        self.assertEqual(ADS.retrieve("S_fit_Workspace").blocksize(), 96)
+
+    def test_calibration_run_non_default_foil_non_default_Ediv(self):
+        # Test with non default resonance, note default Ediv not suitable
+        PEARLTransfit(Files="PEARL00112777", Calibration=True, FoilType="Hf02", Ediv=0.01)
+        self._assert_output_workspaces_exist("S_fit")
+        self._assert_fit_parameters("S_fit", cost=0.74853, peak_cen=2380.03, peak_cen_err=0.32064)
+        self.assertEqual(ADS.retrieve("S_fit_Workspace").blocksize(), 70)
+
     def _assert_fit_parameters(self, ws_prefix, cost, peak_cen, peak_cen_err, frac_delta=1e-4):
         param_table = ADS.retrieve(f"{ws_prefix}_Parameters")
         self.assertAlmostEqual(param_table.column("Value")[-1], cost, delta=frac_delta * cost)
