083c76560df14acb2fb7f8f0731af576eb44df70
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2025-03-28 12:28:13 +0000

    - Refactor, reorganized and clean base algorithm - Improve validation for polarized data - Add definitions for standard properties - Improve polarized component map creation

--- Framework/DataHandling/inc/MantidDataHandling/SaveNXcanSASBase.h
+++ Framework/DataHandling/inc/MantidDataHandling/SaveNXcanSASBase.h
@@ -9,37 +9,39 @@
 #include "MantidAPI/Algorithm.h"
 #include "MantidAPI/MatrixWorkspace_fwd.h"
 #include "MantidDataHandling/DllConfig.h"
-#include <H5Cpp.h>
 #include <MantidAPI/WorkspaceGroup_fwd.h>
+
+#include <H5Cpp.h>
 #include <filesystem>
 
 namespace Mantid {
 namespace DataHandling {
 
-/** SaveNXcanSASBase : Base class to save a reduced workspace in the NXcanSAS format. Currently
- * only MatrixWorkspaces resulting from
- * 1D and 2D reductions are supported.
+/** SaveNXcanSASBase : Base class to save a reduced workspace in the NXcanSAS format. Depending on the derived
+ * algorithm, it contains member that store standard SANS reduced data in 1D or 2D from group or matrix workspaces or
+ * polarized SANS reduced data in 1D or 2D from group workspaces.
  */
 class MANTID_DATAHANDLING_DLL SaveNXcanSASBase : public API::Algorithm {
 protected:
-  void saveSingleWorkspaceFile(const API::MatrixWorkspace_sptr &workspace, const std::filesystem::path &path);
-  void savePolarizedGroup(const API::WorkspaceGroup_sptr &wsGroup, const std::filesystem::path &path);
-  void addStandardMetadata(const Mantid::API::MatrixWorkspace_sptr &workspace, H5::Group &sasEntry);
-  void addPolarizedMetadata(const Mantid::API::MatrixWorkspace_sptr &workspace, H5::Group &sasEntry);
-  void addData(H5::Group &group, const Mantid::API::MatrixWorkspace_sptr &workspace);
-  void addPolarizedData(H5::Group &group, const Mantid::API::WorkspaceGroup_sptr &wsGroup);
-
+  void addStandardMetadata(const Mantid::API::MatrixWorkspace_sptr &workspace, H5::Group &sasEntry) const;
+  void addPolarizedMetadata(const Mantid::API::MatrixWorkspace_sptr &workspace, H5::Group &sasEntry) const;
+  void addData(H5::Group &group, const Mantid::API::MatrixWorkspace_sptr &workspace) const;
+  void addPolarizedData(H5::Group &group, const Mantid::API::WorkspaceGroup_sptr &wsGroup) const;
   H5::Group addSasEntry(H5::H5File &file, const Mantid::API::MatrixWorkspace_sptr &workspace,
-                        const std::string &suffix);
+                        const std::string &suffix) const;
+
   void initStandardProperties();
   void initPolarizedProperties();
-  std::map<std::string, std::string> validateStandardInputs();
-  std::map<std::string, std::string> validatePolarizedInputs(std::map<std::string, std::string> &result);
+  std::map<std::string, std::string> validateStandardInputs() const;
+  std::map<std::string, std::string> validatePolarizedInputs() const;
+
+  void saveSingleWorkspaceFile(const API::MatrixWorkspace_sptr &workspace, const std::filesystem::path &path) const;
+  void savePolarizedGroup(const API::WorkspaceGroup_sptr &wsGroup, const std::filesystem::path &path) const;
 
   std::unique_ptr<API::Progress> m_progress = nullptr;
 
 private:
-  std::map<std::string, std::string> createComponentMap();
+  std::map<std::string, std::vector<std::string>> createPolarizedComponentMap() const;
 };
 
 } // namespace DataHandling
