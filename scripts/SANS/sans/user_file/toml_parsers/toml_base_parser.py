03f2e582af3dd0fc245b06def5b73481d3966109
Caila Finn <47181718+cailafinn@users.noreply.github.com>
2025-04-09 11:14:24 +0100

    SANS TOML V2: Polarization Options Added to User Files (#38940)
    * Create StatePolarization Class
    
    RE #38524
    
    * Add StateComponent class
    
    RE #38524
    
    * Add filter state
    
    RE #38524
    
    * Add StateField class
    
    RE #38524
    
    * Implement state validation
    
    RE #38524
    
    * Add polarization state tests
    
    RE #38524
    
    * Implement main polarization parsing
    
    RE #38524
    
    * Implement flipper parsing
    
    RE #38524
    
    * Parse polarizer and analyzer
    
    RE #38524
    
    * Implement field parsing
    
    Covers both the electric and magnetic field inputs.
    
    RE #38524
    
    * Add new state to the AllStates Object
    
    RE #38524
    
    * Validate the parsed state
    
    RE #38524
    
    * Make all parsers produce a polarization object
    
    The old txt format won't be able to edit the values like the toml
    parser, but it will return a blank state so that the behaviour of the
    two parsers is identical.
    
    RE #38524
    
    * Create TOML V2
    
    Given that the new things we're adding to the user file aren't
    compatible with older versions of mantid, we're bumping up to TOML V2 so
    that we can keep being backwards compatible. This is mainly to stop new
    user files containing this information from being loaded by older
    versions, making it clear that you need to upgrade to use these new
    features and toml files.
    
    RE #38524
    
    * Add TOML V2 information to docs
    
    RE #38524
    
    * Add a release note
    
    RE #38524
    
    * Improve dict naming
    
    RE #38524
    
    * Stop messing around with __class__
    
    RE #38524
    
    * Use match case
    
    RE #38524
    
    * Remove now unneeded typing types
    
    RE #38524
    
    * Validate substates using vars()
    
    RE #38524
    
    * Simplify sample direction validation
    
    RE #38524
    
    * Avoid setting implementation twice
    
    RE #38524
    
    * Remove TODO
    
    Created an issue instead here: https://github.com/mantidproject/mantid/issues/39103
    
    RE #38524

---
+++ scripts/SANS/sans/user_file/toml_parsers/toml_base_parser.py
@@ -0,0 +1,26 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+from abc import ABCMeta, abstractmethod
+
+from sans.state.IStateParser import IStateParser
+from sans.user_file.toml_parsers.toml_base_schema import TomlSchemaValidator
+
+
+class TomlParserBase(IStateParser, metaclass=ABCMeta):
+    def __init__(self, dict_to_parse, file_information, schema_validator: TomlSchemaValidator):
+        self._validator = schema_validator
+        self._validator.validate()
+
+        data_info = super().get_state_data(file_information)
+        self._implementation = self._get_impl(dict_to_parse, data_info)
+        self._implementation.parse_all()
+
+    @staticmethod
+    @abstractmethod
+    def _get_impl(*args):
+        pass
