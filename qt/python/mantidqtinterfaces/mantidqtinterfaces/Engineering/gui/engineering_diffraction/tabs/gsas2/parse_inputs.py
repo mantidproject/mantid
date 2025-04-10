9c0ce5e5107ca0f45efbfdae6f237d20964dd4b7
JackEAllen <jack.allen@stfc.ac.uk>
2025-04-10 17:53:35 +0100

    Refactor Gsas2InputParameters Structure to Collate Similar Attributes
    Refactor Gsas2InputParameters to collate similar attrbiutes, update GSAS2Model and call_G2sc.py references to new GSAS2Model structure

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/Engineering/gui/engineering_diffraction/tabs/gsas2/parse_inputs.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Engineering/gui/engineering_diffraction/tabs/gsas2/parse_inputs.py
@@ -8,7 +8,13 @@
 import json
 
 
-class Gsas2Inputs:
+class Gsas2InputParameters:
+    """
+    A container for GSAS-II input parameters used in the engineering diffraction interface.
+
+    This class organizes and stores all the necessary parameters for GSAS-II refinement tasks.
+    """
+
     def __init__(
         self,
         *,
@@ -34,15 +40,20 @@ class Gsas2Inputs:
         self.path_to_gsas2 = path_to_gsas2
         self.temporary_save_directory = temporary_save_directory
         self.project_name = project_name
-        self.refinement_method = refinement_method
-        self.refine_background = refine_background
-        self.refine_microstrain = refine_microstrain
-        self.refine_sigma_one = refine_sigma_one
-        self.refine_gamma = refine_gamma
-        self.refine_histogram_scale_factor = refine_histogram_scale_factor
-        self.data_files = data_files
-        self.phase_files = phase_files
-        self.instrument_files = instrument_files
+        self.refinement_settings = {
+            "method": refinement_method,
+            "background": refine_background,
+            "microstrain": refine_microstrain,
+            "sigma_one": refine_sigma_one,
+            "gamma": refine_gamma,
+            "histogram_scale_factor": refine_histogram_scale_factor,
+            "unit_cell": refine_unit_cell,
+        }
+        self.file_paths = {
+            "data_files": data_files,
+            "phase_files": phase_files,
+            "instrument_files": instrument_files,
+        }
         self.limits = limits  # x_min and x_max
         self.mantid_pawley_reflections = mantid_pawley_reflections
         self.override_cell_lengths = override_cell_lengths
@@ -55,15 +66,8 @@ class Gsas2Inputs:
             "path_to_gsas2": self.path_to_gsas2,
             "temporary_save_directory": self.temporary_save_directory,
             "project_name": self.project_name,
-            "refinement_method": self.refinement_method,
-            "refine_background": self.refine_background,
-            "refine_microstrain": self.refine_microstrain,
-            "refine_sigma_one": self.refine_sigma_one,
-            "refine_gamma": self.refine_gamma,
-            "refine_histogram_scale_factor": self.refine_histogram_scale_factor,
-            "data_files": self.data_files,
-            "phase_files": self.phase_files,
-            "instrument_files": self.instrument_files,
+            "refinement_settings": self.refinement_settings,
+            "file_paths": self.file_paths,
             "limits": self.limits,
             "mantid_pawley_reflections": self.mantid_pawley_reflections,
             "override_cell_lengths": self.override_cell_lengths,
@@ -73,6 +77,16 @@ class Gsas2Inputs:
         }
         return match_dict
 
+    def validate_inputs(self):
+        """
+        Validates the input parameters to ensure they meet the expected criteria.
+        Raises a ValueError if any parameter is invalid.
+        """
+        if not self.path_to_gsas2 or not isinstance(self.path_to_gsas2, str):
+            raise ValueError("Invalid path_to_gsas2: must be a non-empty string.")
+        if not self.file_paths["data_files"] or not isinstance(self.file_paths["data_files"], list):
+            raise ValueError("Invalid data_files: must be a non-empty list.")
 
-def Gsas2Inputs_to_json(gsas2_inputs):
+
+def Gsas2InputParameters_to_json(gsas2_inputs):
     return json.dumps(gsas2_inputs.matching_dict(), separators=(",", ":"))
