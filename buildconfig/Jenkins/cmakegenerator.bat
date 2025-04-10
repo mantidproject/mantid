555e72993b46aab8536468d80c5432e7e2e4593d
sf1919 <sarah.foxley@stfc.ac.uk>
2025-04-10 15:35:02 +0100

    Update cmakegenerator to Visual Studio 17 2022

--- buildconfig/Jenkins/cmakegenerator.bat
+++ buildconfig/Jenkins/cmakegenerator.bat
@@ -4,5 +4,5 @@
 :: when building on newer versions of Windows. This value must be supplied
 :: externally and cannot be supplied in the cmake configuration
 set SDK_VERS=8.1
-set CM_GENERATOR=Visual Studio 16 2019
+set CM_GENERATOR=Visual Studio 17 2022
 set CM_ARCH=x64
