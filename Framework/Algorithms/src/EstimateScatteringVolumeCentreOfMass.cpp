65c881908ec228b6d1194a4058867c6a56fe6a31
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-04-17 16:53:41 +0100

    implement alg, tests and docs

---
+++ Framework/Algorithms/src/EstimateScatteringVolumeCentreOfMass.cpp
@@ -0,0 +1,114 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidAlgorithms/EstimateScatteringVolumeCentreOfMass.h"
+#include "MantidAPI/InstrumentValidator.h"
+#include "MantidAPI/Run.h"
+#include "MantidAPI/Sample.h"
+#include "MantidGeometry/Instrument.h"
+#include "MantidGeometry/Objects/ShapeFactory.h"
+#include "MantidGeometry/Rasterize.h"
+#include "MantidKernel/BoundedValidator.h"
+#include "MantidKernel/CompositeValidator.h"
+#include "MantidKernel/V3D.h"
+
+namespace Mantid::Algorithms {
+
+using namespace API;
+using namespace Geometry;
+using namespace Kernel;
+
+EstimateScatteringVolumeCentreOfMass::EstimateScatteringVolumeCentreOfMass()
+    : API::Algorithm(), m_inputWS(), m_sampleObject(nullptr), m_elementPositions(), m_averagePos(), m_cubeSide(0.0) {}
+
+void EstimateScatteringVolumeCentreOfMass::init() {
+
+  // The input workspace must have an instrument and units of wavelength
+  auto wsValidator = std::make_shared<CompositeValidator>();
+  wsValidator->add<InstrumentValidator>();
+
+  declareProperty(std::make_unique<WorkspaceProperty<>>("InputWorkspace", "", Direction::Input, wsValidator),
+                  "Input Workspace");
+  declareProperty(std::make_unique<PropertyWithValue<std::vector<double>>>("CentreOfMass", V3D(), Direction::Output),
+                  "Estimated centre of mass of illuminated sample volume");
+
+  auto moreThanZero = std::make_shared<BoundedValidator<double>>();
+  moreThanZero->setLower(0.001);
+  declareProperty("ElementSize", 1.0, moreThanZero, "The size of one side of an integration element cube in mm");
+}
+
+void EstimateScatteringVolumeCentreOfMass::exec() {
+  // Retrieve the input workspace
+  m_inputWS = getProperty("InputWorkspace");
+  // Cache the beam direction
+  m_beamDirection = m_inputWS->getInstrument()->getBeamDirection();
+  m_cubeSide = getProperty("ElementSize"); // in mm
+  m_cubeSide *= 0.001;                     // now in m
+  // Get a reference to the parameter map (used for indirect instruments)
+  const ParameterMap &pmap = m_inputWS->constInstrumentParameters();
+
+  // Construct Sample
+  constructSample(m_inputWS->mutableSample());
+
+  const auto numHists = static_cast<int64_t>(m_inputWS->getNumberHistograms());
+  const auto specSize = static_cast<int64_t>(m_inputWS->blocksize());
+
+  // Calculate the cached values of L1, element volumes, and geometry size
+  auto integrationVolume = std::shared_ptr<const IObject>(m_sampleObject->clone());
+  if (m_inputWS->run().hasProperty("GaugeVolume")) {
+    integrationVolume = constructGaugeVolume();
+  }
+
+  auto raster = Geometry::Rasterize::calculate(m_beamDirection, *integrationVolume, *m_sampleObject, m_cubeSide);
+  if (raster.l1.size() == 0)
+    throw std::runtime_error("Failed to rasterize shape");
+  // move over the information
+  m_elementPositions = std::move(raster.position);
+  if (m_elementPositions.empty()) {
+    throw std::runtime_error("Failed to define any initial scattering gauge volume for geometry");
+  }
+  m_averagePos = calcAveragePosition(m_elementPositions);
+  setProperty("CentreOfMass", m_averagePos);
+}
+
+/// Create the sample object using the Geometry classes, or use the existing one
+void EstimateScatteringVolumeCentreOfMass::constructSample(API::Sample &sample) {
+  m_sampleObject = &sample.getShape();
+  // Check there is one, and fail if not
+  if (!m_sampleObject->hasValidShape()) {
+    const std::string mess("No shape has been defined for the sample in the input workspace");
+    g_log.error(mess);
+    throw std::invalid_argument(mess);
+  } else {
+    g_log.information("Successfully constructed the sample object");
+  }
+}
+
+std::shared_ptr<const Geometry::IObject> EstimateScatteringVolumeCentreOfMass::constructGaugeVolume() {
+  g_log.information("Calculating scattering within the gauge volume defined on "
+                    "the input workspace");
+
+  // Retrieve and create the gauge volume shape
+  std::shared_ptr<const Geometry::IObject> volume =
+      ShapeFactory().createShape(m_inputWS->run().getProperty("GaugeVolume")->value());
+
+  return volume;
+}
+
+std::vector<double> EstimateScatteringVolumeCentreOfMass::calcAveragePosition(const std::vector<V3D> &pos) {
+  V3D sum(0.0, 0.0, 0.0);
+  for (const auto &indiPos : pos) {
+    sum += indiPos;
+  }
+
+  if (!pos.empty()) {
+    sum /= static_cast<double>(pos.size());
+  }
+  std::vector<double> out = {sum[0], sum[1], sum[2]};
+  return out;
+}
+
+} // namespace Mantid::Algorithms
---
+++ Framework/Algorithms/test/EstimateScatteringVolumeCentreOfMassTest.h
@@ -0,0 +1,213 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidAlgorithms/EstimateScatteringVolumeCentreOfMass.h"
+#include "MantidDataHandling/SetSample.h"
+#include "MantidFrameworkTestHelpers/WorkspaceCreationHelper.h"
+#include "MantidKernel/PropertyManager.h"
+#include "MantidKernel/UnitFactory.h"
+#include "MantidKernel/V3D.h"
+#include <cxxtest/TestSuite.h>
+using Mantid::API::MatrixWorkspace_sptr;
+using Mantid::Kernel::V3D;
+class EstimateScatteringVolumeCentreOfMassTest : public CxxTest::TestSuite {
+public:
+  void testInit() {
+    Mantid::Algorithms::EstimateScatteringVolumeCentreOfMass centerOfMass;
+    TS_ASSERT_THROWS_NOTHING(centerOfMass.initialize());
+    TS_ASSERT(centerOfMass.isInitialized());
+  }
+  void testExecWithCylinderSample() {
+    // Create a test workspace with cylinder sample
+    MatrixWorkspace_sptr testWS = createWorkspaceWithCylinderSample();
+    // Run the algorithm
+    Mantid::Algorithms::EstimateScatteringVolumeCentreOfMass centerOfMass;
+    centerOfMass.initialize();
+    centerOfMass.setProperty("InputWorkspace", testWS);
+    centerOfMass.setProperty("ElementSize", 1.0); // 1mm cubes
+    TS_ASSERT_THROWS_NOTHING(centerOfMass.execute());
+    TS_ASSERT(centerOfMass.isExecuted());
+    // Check output
+    std::vector<double> resultVec = centerOfMass.getProperty("CentreOfMass");
+    V3D result(resultVec[0], resultVec[1], resultVec[2]);
+    // For a symmetric cylinder along y-axis centered at origin,
+    TS_ASSERT_DELTA(result.X(), 0.0, 0.00001);
+    TS_ASSERT_DELTA(result.Y(), 0.0, 0.00001);
+    TS_ASSERT_DELTA(result.Z(), 0.0, 0.00001);
+  }
+  void testExecWithOffsetCylinderSample() {
+    // Create a test workspace with offset cylinder sample
+    MatrixWorkspace_sptr testWS = createWorkspaceWithOffsetCylinderSample();
+    // Run the algorithm
+    Mantid::Algorithms::EstimateScatteringVolumeCentreOfMass centerOfMass;
+    centerOfMass.initialize();
+    centerOfMass.setProperty("InputWorkspace", testWS);
+    centerOfMass.setProperty("ElementSize", 1.0); // 1mm cubes
+    TS_ASSERT_THROWS_NOTHING(centerOfMass.execute());
+    TS_ASSERT(centerOfMass.isExecuted());
+    // Check output
+    std::vector<double> resultVec = centerOfMass.getProperty("CentreOfMass");
+    V3D result(resultVec[0], resultVec[1], resultVec[2]);
+    // For a cylinder with center offset to (0,-0.01,0),
+    // expect the center of mass to be close to this
+    TS_ASSERT_DELTA(result.X(), 0.0, 0.00002);
+    TS_ASSERT_DELTA(result.Y(), -0.01, 0.00002);
+    TS_ASSERT_DELTA(result.Z(), 0.0, 0.00002);
+  }
+  void testExecWithGaugeVolume() {
+    // Create a test workspace with cylinder sample and sphere gauge volume
+    MatrixWorkspace_sptr testWS = createWorkspaceWithOffsetCylinderSampleAndGaugeVolume();
+    // Run the algorithm
+    Mantid::Algorithms::EstimateScatteringVolumeCentreOfMass centerOfMass;
+    centerOfMass.initialize();
+    centerOfMass.setProperty("InputWorkspace", testWS);
+    centerOfMass.setProperty("ElementSize", 1.0); // 1mm cubes
+    TS_ASSERT_THROWS_NOTHING(centerOfMass.execute());
+    TS_ASSERT(centerOfMass.isExecuted());
+    // Check output
+    std::vector<double> resultVec = centerOfMass.getProperty("CentreOfMass");
+    V3D result(resultVec[0], resultVec[1], resultVec[2]);
+    // For a cubic gauge volume at (0,0,0), expect center of mass near that point
+    // despite the offset cylinder having a c.o.m at (0,-0.01,0)
+    TS_ASSERT_DELTA(result.X(), 0.0, 0.00002);
+    TS_ASSERT_DELTA(result.Y(), 0.0, 0.00002);
+    TS_ASSERT_DELTA(result.Z(), 0.0, 0.00002);
+  }
+  void testExecWithPartiallyIlluminatedSample() {
+    // Create a test workspace with cylinder sample and sphere gauge volume
+    MatrixWorkspace_sptr testWS = createWorkspaceWithPartiallyIlluminatedSample();
+    // Run the algorithm
+    Mantid::Algorithms::EstimateScatteringVolumeCentreOfMass centerOfMass;
+    centerOfMass.initialize();
+    centerOfMass.setProperty("InputWorkspace", testWS);
+    centerOfMass.setProperty("ElementSize", 1.0); // 1mm cubes
+    TS_ASSERT_THROWS_NOTHING(centerOfMass.execute());
+    TS_ASSERT(centerOfMass.isExecuted());
+    // Check output
+    std::vector<double> resultVec = centerOfMass.getProperty("CentreOfMass");
+    V3D result(resultVec[0], resultVec[1], resultVec[2]);
+    // cube is centre at (0.01,0.01,0.01) with sides 0.02
+    // For a cubic "gauge volume" at (0,0,0), with sides 0.02
+    // illuminated volume should be a cube centred on (0.005, 0.005, 0.005)
+    TS_ASSERT_DELTA(result.X(), 0.005, 0.00002);
+    TS_ASSERT_DELTA(result.Y(), 0.005, 0.00002);
+    TS_ASSERT_DELTA(result.Z(), 0.005, 0.00002);
+  }
+  void testWithoutSample() {
+    // Create a workspace without sample shape
+    MatrixWorkspace_sptr testWS = createTestWorkspace();
+    Mantid::Algorithms::EstimateScatteringVolumeCentreOfMass centerOfMass;
+    centerOfMass.setRethrows(true);
+    centerOfMass.initialize();
+    centerOfMass.setProperty("InputWorkspace", testWS);
+    // This should throw because no sample shape is defined
+    TS_ASSERT_THROWS(centerOfMass.execute(), const std::invalid_argument &);
+    TS_ASSERT(!centerOfMass.isExecuted());
+  }
+
+private:
+  MatrixWorkspace_sptr createTestWorkspace() {
+    // Create a basic test workspace
+    MatrixWorkspace_sptr testWS = WorkspaceCreationHelper::create2DWorkspaceWithFullInstrument(1, 10);
+    return testWS;
+  }
+  MatrixWorkspace_sptr createWorkspaceWithCylinderSample() {
+    // Create a workspace with a cylinder sample
+    MatrixWorkspace_sptr testWS = createTestWorkspace();
+    // Create the geometry (cylinder along y-axis)
+    auto geometry = std::make_shared<Mantid::Kernel::PropertyManager>();
+    geometry->declareProperty("Shape", "Cylinder");
+    geometry->declareProperty("Height", 4);
+    geometry->declareProperty("Radius", 1);
+    std::vector<double> center{0, 0, 0};
+    geometry->declareProperty("Center", center);
+    std::vector<double> axis{0, 1, 0}; // y-axis
+    geometry->declareProperty("Axis", axis);
+    // Set the sample information
+    Mantid::DataHandling::SetSample setsample;
+    setsample.initialize();
+    setsample.setProperty("InputWorkspace", testWS);
+    setsample.setProperty("Geometry", geometry);
+    setsample.execute();
+    return testWS;
+  }
+  MatrixWorkspace_sptr createWorkspaceWithOffsetCylinderSample() {
+    // Create a workspace with an offset cylinder sample
+    MatrixWorkspace_sptr testWS = createTestWorkspace();
+    // Create the geometry (cylinder with offset center)
+    auto geometry = std::make_shared<Mantid::Kernel::PropertyManager>();
+    geometry->declareProperty("Shape", "Cylinder");
+    geometry->declareProperty("Height", 4);
+    geometry->declareProperty("Radius", 1);
+    std::vector<double> center{0, -1, 0}; // Offset center
+    geometry->declareProperty("Center", center);
+    std::vector<double> axis{0, 1, 0}; // y-axis
+    geometry->declareProperty("Axis", axis);
+    // Set the sample information
+    Mantid::DataHandling::SetSample setsample;
+    setsample.initialize();
+    setsample.setProperty("InputWorkspace", testWS);
+    setsample.setProperty("Geometry", geometry);
+    setsample.execute();
+    return testWS;
+  }
+  MatrixWorkspace_sptr createWorkspaceWithOffsetCubeSample() {
+    // Create a workspace with an offset cylinder sample
+    MatrixWorkspace_sptr testWS = createTestWorkspace();
+    // Create the geometry (cylinder with offset center)
+    auto geometry = std::make_shared<Mantid::Kernel::PropertyManager>();
+    geometry->declareProperty("Shape", "FlatPlate");
+    geometry->declareProperty("Height", 2);
+    geometry->declareProperty("Width", 2);
+    geometry->declareProperty("Thick", 2);
+    std::vector<double> center{1, 1, 1}; // Offset center
+    geometry->declareProperty("Center", center);
+    geometry->declareProperty("Angle", 0);
+    // Set the sample information
+    Mantid::DataHandling::SetSample setsample;
+    setsample.initialize();
+    setsample.setProperty("InputWorkspace", testWS);
+    setsample.setProperty("Geometry", geometry);
+    setsample.execute();
+    return testWS;
+  }
+  MatrixWorkspace_sptr createWorkspaceWithOffsetCylinderSampleAndGaugeVolume() {
+    // Create workspace with cylinder sample and add gauge volume
+    MatrixWorkspace_sptr testWS = createWorkspaceWithOffsetCylinderSample();
+    // Define a spherical gauge volume
+    const std::string gaugeXML = " \
+        <cuboid id='some-cuboid'> \
+        <height val='0.01'  /> \
+        <width val='0.01' />  \
+        <depth  val='0.01' />  \
+        <centre x='0.0' y='0.0' z='0.0'  />  \
+        </cuboid>  \
+        <algebra val='some-cuboid' /> \
+        ";
+    // Add the gauge volume to the run properties
+    testWS->mutableRun().addProperty("GaugeVolume", gaugeXML);
+    return testWS;
+  }
+  MatrixWorkspace_sptr createWorkspaceWithPartiallyIlluminatedSample() {
+    // Create workspace with cylinder sample and add gauge volume
+    MatrixWorkspace_sptr testWS = createWorkspaceWithOffsetCubeSample();
+    // Define a spherical gauge volume
+    const std::string gaugeXML = " \
+        <cuboid id='some-cuboid'> \
+        <height val='0.02'  /> \
+        <width val='0.02' />  \
+        <depth  val='0.02' />  \
+        <centre x='0.0' y='0.0' z='0.0'  />  \
+        </cuboid>  \
+        <algebra val='some-cuboid' /> \
+        ";
+    // Add the gauge volume to the run properties
+    testWS->mutableRun().addProperty("GaugeVolume", gaugeXML);
+    return testWS;
+  }
+};
