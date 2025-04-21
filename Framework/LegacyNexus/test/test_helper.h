378189c0ca694991cfc2d4bba177d1ccf57fa1a5
Pete Peterson <petersonpf@ornl.gov>
2025-04-21 14:08:55 -0400

    Removed unused functionality from LegacyNexus - ornl-next (#39229)
    Removed unused functionality from LegacyNexus (#39092)
    
    * remove unused code from NeXusFile
    
    * readdd stream operators
    
    * remove unused code from napi/4 header and source
    
    * remove unused napi5 code
    
    * readd template override
    
    * readd getattr(info)
    
    * readd nx_group_info
    
    * replace nxstack with NexusFileID
    
    * remove nxstack code
    
    * change nxstack file names
    
    * fix compiler warnings
    
    * remove unused vars
    
    * fix and simplify nexus path build
    
    * change pLgcyFunc obj to unique_ptr
    
    * satify cppcheck
    
    * add const to NexusFileInfo
    
    * initial move and refactor of updated NexusFile tests
    
    * remove remove...
    
    * fix leak test 1
    
    * make legacy nexus tests work with current lnf functions
    
    * readd fns to fix legacynexus test
    
    * ensure new tests work read only
    
    * all tests running with unique filenames
    
    * refactor NexusFileTest for multiple hdf versions
    
    * refac rw test
    
    * refactor last 2 tests
    
    * add HDF4 tests to nexusfile
    
    * remove root ID from unique vars
    
    * add hdf4 to rw test
    
    * add hdf4 to two remaining tests
    
    * change remaining references to nexus in cmakelists
    
    * add hdf4 test files
    
    * add h5 data files
    
    * add missing h5 file
    
    * remove old napi tests
    
    * fix typos and change filenames for consistency
    
    * add missing files
    
    * remove debug come
    
    * remove unused var
    
    * correct char case typo in import
    
    * add macro to ensure env var can be set on all platforms
    
    * remove overcomplicated use of ranges
    
    * correct setenv typo
    
    * covert setenv input to c_str
    
    * covert second setenv input to c_str
    
    * change test file names for ctest
    
    * remove unused var
    
    * fix failing tests on linux
    
    * fix failing napi test due to lingering blank env var
    
    * use c_str in unsetenvvar
    
    * changes in response to review comments
    
    * add missing access mode to switch
    
    Co-authored-by: MialLewis <95620982+MialLewis@users.noreply.github.com>

---
+++ Framework/LegacyNexus/test/test_helper.h
@@ -0,0 +1,19 @@
+#include <string>
+
+namespace LegacyNexusTest {
+
+/**
+ * Let's face it, std::string is poorly designed,
+ * and this is the constructor that it needed to have.
+ * Initialize a string from a c-style formatting string.
+ */
+std::string strmakef(char const *const fmt, ...);
+
+enum NexusFormat { HDF4, HDF5 };
+
+struct FormatUniqueVars {
+  std::string relFilePath;
+};
+
+FormatUniqueVars getFormatUniqueVars(const NexusFormat fmt, const std::string &filename);
+} // namespace LegacyNexusTest
