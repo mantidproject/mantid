d54ebb6150504f6638631639d22ef14719bf5730
Daniel Caballero <dlcaballero16@gmail.com>
2025-04-22 11:25:19 -0400

    Undid change making raster object const ref

--- Framework/Algorithms/src/AnyShapeAbsorption.cpp
+++ Framework/Algorithms/src/AnyShapeAbsorption.cpp
@@ -62,7 +62,7 @@ void AnyShapeAbsorption::initialiseCachedDistances() {
     }
   }
 
-  const auto &raster = Geometry::Rasterize::calculate(m_beamDirection, *m_sampleObject, *integrationVolume, m_cubeSide);
+  auto raster = Geometry::Rasterize::calculate(m_beamDirection, *m_sampleObject, *integrationVolume, m_cubeSide);
   m_sampleVolume = raster.totalvolume;
   if (raster.l1.size() == 0)
     throw std::runtime_error("Failed to rasterize shape");
