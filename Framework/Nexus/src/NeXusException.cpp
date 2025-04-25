e5710c5c6b4645f8c8d6058b8abfbf934ecd81d4
Pete Peterson <petersonpf@ornl.gov>
2025-04-25 09:22:00 -0400

    Add function name as a variable in NexusException

--- Framework/Nexus/src/NeXusException.cpp
+++ Framework/Nexus/src/NeXusException.cpp
@@ -1,5 +1,6 @@
 #include "MantidNexus/NeXusException.hpp"
 #include "MantidNexus/NeXusFile_fwd.h"
+#include <ostream>
 
 /**
  * \file NeXusException.cpp
@@ -8,9 +9,22 @@
 
 namespace NeXus {
 
-Exception::Exception(const std::string &msg, const std::string &filename)
-    : std::runtime_error(msg), m_filename(filename) {}
+Exception::Exception(const std::string &msg, const std::string &functionname, const std::string &filename)
+    : std::runtime_error(msg), m_functionname(functionname), m_filename(filename) {}
+
+const std::string Exception::functionname() const throw() { return this->m_functionname; }
 
 const std::string Exception::filename() const throw() { return this->m_filename; }
+
+std::ostream &operator<<(std::ostream &os, const Exception &err) {
+  if (!err.functionname().empty()) {
+    os << "in function " << err.functionname() << " ";
+  }
+  os << err.what();
+  if (!err.filename().empty()) {
+    os << " in file " << err.filename();
+  }
+  return os;
+}
 
 } // namespace NeXus
