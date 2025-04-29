3475614b2ba87e1e2b082429a278ac8419e87263
rboston628 <bostonsr@ornl.gov>
2025-04-29 14:37:13 -0400

    fix small error

--- Framework/DataHandling/src/LoadBankFromDiskTask.cpp
+++ Framework/DataHandling/src/LoadBankFromDiskTask.cpp
@@ -231,7 +231,7 @@ std::unique_ptr<std::vector<float>> LoadBankFromDiskTask::loadTof(::NeXus::File 
 
   // Check that the required space is there in the file.
   ::NeXus::Info tof_info = file.getInfo();
-  int64_t tof_dim0 = recalculateDataSize(tof_info[0]);
+  int64_t tof_dim0 = recalculateDataSize(tof_info.dims[0]);
   if (tof_dim0 < m_loadSize[0] + m_loadStart[0]) {
     m_loader.alg->getLogger().warning() << "Entry " << entry_name
                                         << "'s event_time_offset field is too small "
