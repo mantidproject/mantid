03934dc47f978b15150087411369240930809ec6
Pete Peterson <petersonpf@ornl.gov>
2025-05-06 13:49:18 -0400

    Add meaningful tests

--- Framework/Nexus/test/NexusClassesTest.h
+++ Framework/Nexus/test/NexusClassesTest.h
@@ -6,11 +6,10 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
-#include <cxxtest/TestSuite.h>
-
 #include "MantidNexus/NexusClasses.h"
-
-using Mantid::Nexus::NexusClasses;
+#include "test_helper.h"
+#include <cxxtest/TestSuite.h>
+#include <filesystem>
 
 class NexusClassesTest : public CxxTest::TestSuite {
 public:
@@ -19,5 +18,47 @@ public:
   static NexusClassesTest *createSuite() { return new NexusClassesTest(); }
   static void destroySuite(NexusClassesTest *suite) { delete suite; }
 
-  void test_Something() { TS_FAIL("You forgot to write a test!"); }
+  void test_EQSANS_89157() {
+    const std::string filename = NexusTest::getFullPath("EQSANS_89157.nxs.h5");
+    Mantid::NeXus::NXRoot root(filename);
+
+    // groups don't load their attributes
+
+    auto entry = root.openFirstEntry();
+    TS_ASSERT_EQUALS(entry.name(), "entry");
+    // entry.NX_class() returns the type in "NexusClasses" (i.e. NXentry) rather than what is in the file
+    TS_ASSERT_EQUALS(entry.attributes.n(), 0); // groups don't load their attributes
+
+    // check NXChar
+    auto definition = root.openNXChar("entry/definition"); // relative path
+    definition.load();
+    TS_ASSERT_EQUALS(std::string(definition(), definition.dim0()), "NXsnsevent");
+    // and from getString
+    TS_ASSERT_EQUALS(root.getString("entry/definition"), "NXsnsevent");
+
+    TS_ASSERT(!entry.containsGroup("bank91_events")); // there aren't that many groups
+    TS_ASSERT(entry.containsGroup("bank19_events"));
+
+    auto bank19 = entry.openNXGroup("bank19_events");
+    TS_ASSERT_EQUALS(bank19.name(), "bank19_events");
+    // bank19.NX_class() returns the type in "NexusClasses" (i.e. NXClass) rather than what is in the file
+    TS_ASSERT_EQUALS(bank19.attributes.n(), 0); // groups don't load their attributes
+
+    // load time-of-flight as a float because NexusClasses doesn't autoconvert types
+    auto time_of_flight = bank19.openNXFloat("event_time_offset");
+    TS_ASSERT_EQUALS(time_of_flight.dim0(), 256); // from looking in the file
+    TS_ASSERT_EQUALS(time_of_flight.attributes.n(), 2);
+    TS_ASSERT_EQUALS(time_of_flight.attributes("units"), "microsecond");
+    TS_ASSERT_EQUALS(time_of_flight.attributes("target"), "/entry/instrument/bank19/event_time_offset");
+    time_of_flight.load();
+    TS_ASSERT_DELTA(time_of_flight[0], 16681.5, .01);
+    TS_ASSERT_DELTA(time_of_flight[255], 958.1, .01);
+    TS_ASSERT_THROWS_ANYTHING(time_of_flight[256]); // out of bounds
+
+    auto duration = root.openNXFloat("/entry/duration"); // absolute path
+    TS_ASSERT_EQUALS(duration.attributes.n(), 1);
+    TS_ASSERT_EQUALS(duration.attributes("units"), "second");
+    duration.load();
+    TS_ASSERT_DELTA(duration[0], 7200., .1);
+  }
 };
