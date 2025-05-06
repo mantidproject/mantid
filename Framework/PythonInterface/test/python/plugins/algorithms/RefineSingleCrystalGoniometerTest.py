d6a04802f1219f6b013a74c8e646c857383e8e99
Kevin A. Tactac <tactacka@ornl.gov>
2025-05-06 10:50:53 -0400

    test with mantid data

--- Framework/PythonInterface/test/python/plugins/algorithms/RefineSingleCrystalGoniometerTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/RefineSingleCrystalGoniometerTest.py
@@ -6,9 +6,7 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
 
-import numpy as np
-
-from mantid.simpleapi import mtd, RefineSingleCrystalGoniometer, LoadIsawPeaks, FindUBUsingIndexedPeaks, IndexPeaks
+from mantid.simpleapi import RefineSingleCrystalGoniometer, LoadIsawPeaks, FindUBUsingIndexedPeaks, IndexPeaks
 
 
 class RefineSingleCrystalGoniometerTest(unittest.TestCase):
@@ -19,39 +17,22 @@ class RefineSingleCrystalGoniometerTest(unittest.TestCase):
         return
 
     def testExample(self):
-        filename = "/SNS/TOPAZ/IPTS-33878/shared/RFMBA2PbI4/RFMBA2PbI4_mantid_295K_find_peaks/RFMBA2PbI4_Monoclinic_P_5sig.integrate"
+        filename = "TOPAZ_3007.peaks.nxs"
 
         LoadIsawPeaks(Filename=filename, OutputWorkspace="peaks")
-        FindUBUsingIndexedPeaks(PeaksWorkspace="peaks")
-        IndexPeaks(PeaksWorkspace="peaks", CommonUBForAll=True)
 
-        RefineSingleCrystalGoniometer("peaks", tol=0.25, cell="Monoclinic", n_iter=5)
-        IndexPeaks(PeaksWorkspace="peaks", CommonUBForAll=True)
+        FindUBUsingIndexedPeaks(PeaksWorkspace="peaks", Tolerance=0.12)
+        index_null = IndexPeaks(PeaksWorkspace="peaks", Tolerance=0.12)
 
-        assert mtd["peaks_#0"]
-        assert mtd["peaks_#0"].rowCount() == 20
-        assert mtd["peaks_#0"].columnCount() == 6
-        assert np.isclose(mtd["peaks_#0"].column(0)[0], -0.12999999523162842)
+        initial = index_null.NumIndexed
 
-        assert mtd["peaks_#1"]
-        assert mtd["peaks_#1"].rowCount() == 20
-        assert mtd["peaks_#1"].columnCount() == 6
-        assert np.isclose(mtd["peaks_#1"].column(0)[0], 0.6909528374671936)
+        RefineSingleCrystalGoniometer("peaks", 0.12, "Triclinic", 1)
 
-        assert mtd["peaks_#2"]
-        assert mtd["peaks_#2"].rowCount() == 20
-        assert mtd["peaks_#2"].columnCount() == 6
-        assert np.isclose(mtd["peaks_#2"].column(0)[0], 1.3717014789581299)
+        index_refine = IndexPeaks(PeaksWorkspace="peaks", Tolerance=0.12)
 
-        assert mtd["peaks_#3"]
-        assert mtd["peaks_#3"].rowCount() == 20
-        assert mtd["peaks_#3"].columnCount() == 6
-        assert np.isclose(mtd["peaks_#3"].column(0)[0], 1.9151899814605713)
+        final = index_refine.NumIndexed
 
-        assert mtd["peaks_#4"]
-        assert mtd["peaks_#4"].rowCount() == 20
-        assert mtd["peaks_#4"].columnCount() == 6
-        assert np.isclose(mtd["peaks_#4"].column(0)[0], 2.3677234649658203)
+        assert final > initial
 
 
 if __name__ == "__main__":
