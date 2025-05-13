7bd26224f18754a310d237987e9b219e39543767
rboston628 <bostonsr@ornl.gov>
2025-05-13 16:35:17 -0400

    all datahandling tests pass

--- Framework/DataHandling/test/SaveBankScatteringAnglesTest.h
+++ Framework/DataHandling/test/SaveBankScatteringAnglesTest.h
@@ -75,7 +75,6 @@ public:
     ADS.remove("group");
   }
 
-  // NEXUS REFACTOR FAILURE
   void test_savedDataIsCorrect() {
     auto load = API::AlgorithmManager::Instance().create("Load");
     load->setProperty("Filename", "ENGINX_277208_focused_bank_2.nxs");
