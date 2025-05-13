a8a295752b397ac03e7f14b91800ad197cfef883
Oleksandr Koshchii <o.koshchii@fz-juelich.de>
2025-05-13 17:30:54 +0200

    Add Plot Options menu to Plotting tab

---
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/dns_single_crystal_elastic/data_structures/dns_dialog.py
@@ -0,0 +1,26 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2023 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+"""
+Class for small input dialogs.
+"""
+
+from pathlib import Path
+
+from qtpy.QtWidgets import QDialog
+from qtpy.uic import loadUi
+
+
+class DNSDialog(QDialog):
+    """
+    Class for dialogs which can be opened by the GUI.
+    """
+
+    def __init__(self, files=None, ui=None):
+        super().__init__()
+        path = str(Path(files).parent.absolute())
+        self._content = loadUi(path + ui, self)
