mantidproject/mantid

commit 779ef5b835bbf45879227388f4a8c6131133d032

Take into account period filtering in getProtonCharge (#39029)
* take into account period filtering in getProtonCharge

* fix comment typo

* fix unit tests given new prop

* respond to PR review

* change comment

Framework/API/src/Run.cpp
@@ -261,7 +261,14 @@ double Run::getProtonCharge() const {
 
   if (!m_manager->existsProperty(PROTON_CHARGE_LOG_NAME)) {
     integrateProtonCharge();
+  } else if (m_manager->existsProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME) &&
+             m_manager->getProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME)) {
+    const std::vector<double> &protonChargeByPeriod = m_manager->getProperty("proton_charge_by_period");
+    const int currentPeriod = m_manager->getProperty("current_period");
+    m_manager->setProperty(PROTON_CHARGE_LOG_NAME, protonChargeByPeriod[currentPeriod - 1]);
+    m_manager->setProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME, false);
   }
+
   if (m_manager->existsProperty(PROTON_CHARGE_LOG_NAME)) {
     charge = m_manager->getProperty(PROTON_CHARGE_LOG_NAME);
   } else {
@@ -329,6 +336,11 @@ void Run::integrateProtonCharge(const std::string &logname) const {
                               "been left at the sum of the log values.");
     }
     const_cast<Run *>(this)->setProtonCharge(total);
+    // Mark gd_prtn_chrg as filtered as this method accounts for period filtering
+    if (m_manager->existsProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME)) {
+      m_manager->setProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME, false);
+    }
+
   } else {
     g_log.warning(logname + " log was not a time series property. The value of the total proton "
                             "charge has not been set");

Framework/DataHandling/src/LoadNexusLogs.cpp
@@ -605,9 +605,15 @@ void LoadNexusLogs::execLoader() {
     }
   }
 
-  if (!workspace->run().hasProperty("gd_prtn_chrg")) {
-    // Try pulling it from the main proton_charge entry first
+  if (!workspace->run().hasProperty(workspace->run().getProtonChargeLogName())) {
     try {
+      // For period data mark proton charge log value as unfiltered to enable subsequent filtering by period.
+      if (workspace->run().hasProperty("proton_charge_by_period")) {
+        Kernel::PropertyWithValue<bool> *pChargeUnfiltered =
+            new Kernel::PropertyWithValue<bool>(workspace->run().getProtonChargeUnfilteredName(), true);
+        workspace->mutableRun().addProperty(pChargeUnfiltered, true);
+      }
+      // Try pulling proton charge from the main proton_charge entry
       file.openData("proton_charge");
       std::vector<double> values;
       file.getDataCoerce(values);

Framework/DataHandling/test/LoadNexusLogsTest.h
@@ -88,8 +88,9 @@ public:
     const API::Run &run = testWS->run();
     const std::vector<Property *> &logs = run.getLogData();
     TS_ASSERT_EQUALS(logs.size(),
-                     36); // 34 logs in file + 1 synthetic nperiods log
+                     37); // 34 logs in file + 1 synthetic nperiods log
                           // + 1 proton_charge_by_period log
+                          // + 1 gd_prtn_chrg_unfiltered log
 
     TimeSeriesProperty<std::string> *slog =
         dynamic_cast<TimeSeriesProperty<std::string> *>(run.getLogData("icp_event"));
@@ -359,6 +360,7 @@ public:
     allowed.push_back("gd_prtn_chrg");
     allowed.push_back("proton_charge_by_period");
     allowed.push_back("nperiods");
+    allowed.push_back("gd_prtn_chrg_unfiltered");
 
     // The default logs that are always present:
     allowed.push_back("start_time");
@@ -406,7 +408,7 @@ public:
     auto properties = run.getProperties();
 
     // add 2 to account for selog_ versions of properties
-    TS_ASSERT_EQUALS(properties.size(), 94 - blocked.size() - 2);
+    TS_ASSERT_EQUALS(properties.size(), 95 - blocked.size() - 2);
 
     // Lookup each name in the workspace property list
     for (const auto &name : blocked) {
@@ -440,7 +442,7 @@ public:
     auto run = testWS->run();
     auto properties = run.getProperties();
 
-    TS_ASSERT_EQUALS(properties.size(), 94);
+    TS_ASSERT_EQUALS(properties.size(), 95);
   }
 
 private:

Framework/PythonInterface/test/python/mantid/api/RunTest.py
@@ -38,6 +38,19 @@ class RunTest(unittest.TestCase):
         self.assertEqual(type(charge), float)
         self.assertAlmostEqual(charge, 10.05)
 
+    def test_proton_charge_accounts_for_period_filtering(self):
+        run = self._run
+        run.addProperty("gd_prtn_chrg_unfiltered", True, True)
+        run.addProperty("proton_charge_by_period", [6.0, 4.05], True)
+        run.addProperty("current_period", 1, True)
+        charge = run.getProtonCharge()
+        self.assertAlmostEqual(charge, 6.0)
+
+        run.addProperty("gd_prtn_chrg_unfiltered", True, True)
+        run.addProperty("current_period", 2, True)
+        charge = run.getProtonCharge()
+        self.assertAlmostEqual(charge, 4.05)
+
     def test_get_time_roi(self):
         # there is intentionally no way create a TimeROI from python
         run = Run()

Framework/API/inc/MantidAPI/LogManager.h
@@ -206,6 +206,9 @@ public:
   bool operator==(const LogManager &other) const;
   bool operator!=(const LogManager &other) const;
 
+  const std::string &getProtonChargeLogName() const { return PROTON_CHARGE_LOG_NAME; }
+  const std::string &getProtonChargeUnfilteredName() const { return PROTON_CHARGE_UNFILTERED_LOG_NAME; }
+
 protected:
   bool hasStartTime() const;
   bool hasEndTime() const;
@@ -219,7 +222,9 @@ protected:
   std::unique_ptr<Kernel::TimeROI> m_timeroi;
   /// Name of the log entry containing the proton charge when retrieved using
   /// getProtonCharge
-  static const char *PROTON_CHARGE_LOG_NAME;
+  static const std::string PROTON_CHARGE_LOG_NAME;
+  /// Flag to signify if a filter has been applied to the proton charge log
+  static const std::string PROTON_CHARGE_UNFILTERED_LOG_NAME;
 
 private:
   /// Cache for the retrieved single values

Framework/API/src/LogManager.cpp
@@ -79,7 +79,9 @@ bool convertPropertyToDouble(const Property *property, double &value, const Math
 
 /// Name of the log entry containing the proton charge when retrieved using
 /// getProtonCharge
-const char *LogManager::PROTON_CHARGE_LOG_NAME = "gd_prtn_chrg";
+const std::string LogManager::PROTON_CHARGE_LOG_NAME = "gd_prtn_chrg";
+const std::string LogManager::PROTON_CHARGE_UNFILTERED_LOG_NAME = LogManager::PROTON_CHARGE_LOG_NAME + "_unfiltered";
+
 //----------------------------------------------------------------------
 // Public member functions
 //----------------------------------------------------------------------

commit 56ae02dbb935e373f668cc4f9a20156c7351d4d3

Ensure proton charge returned consistently across datasets with period data (#39322)
#### Summary of work

A bug was introduced in #39029. For any dataset that has period data,
the log `proton_charge_by_period` is used to return the proton charge
for the current period.

When runs are added. the `proton_charge_by_period` log is not summed; An
erroneous proton charge is therefore subsequently returned.

To resolve this, this PR enables the elementwise addition of the
`proton_charge_by_period` log during run addition.

Using a bool property was causing warnings to be raised during workspace
save, as bool properties are not supported. The unfiltered flag has
therefore been changed to an int.

Also, the check that flags for period data filtering has been moved
outside of ` if
(!workspace->run().hasProperty(workspace->run().getProtonChargeLogName()))`
to account for datasets that come pre-loaded with this log (vesuvio).

This PR fixes a number of the more prelevant issues, but leaves some
inconsistencies. An issue to fix these issues in an upcoming has been
raised:
https://github.com/mantidproject/mantid/issues/39336

Some release notes have been added which I owed to other PRs.

Framework/API/src/Run.cpp
@@ -6,6 +6,7 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #include "MantidAPI/Run.h"
 #include "MantidGeometry/Instrument/Goniometer.h"
+#include "MantidKernel/ArrayProperty.h"
 #include "MantidKernel/FilteredTimeSeriesProperty.h"
 #include "MantidKernel/Matrix.h"
 #include "MantidKernel/PropertyManager.h"
@@ -18,6 +19,7 @@
 #include <memory>
 
 #include <algorithm>
+#include <functional>
 #include <numeric>
 
 namespace Mantid::API {
@@ -26,12 +28,54 @@ using namespace Kernel;
 using Mantid::Types::Core::DateAndTime;
 
 namespace {
-/// The number of log entries summed when adding a run
-const int ADDABLES = 12;
-/// The names of the log entries summed when adding two runs together
-const std::string ADDABLE[ADDABLES] = {"tot_prtn_chrg",   "rawfrm",          "goodfrm",         "dur",
-                                       "gd_prtn_chrg",    "uA.hour",         "monitor0_counts", "monitor1_counts",
-                                       "monitor2_counts", "monitor3_counts", "monitor4_counts", "monitor5_counts"};
+/// static logger object
+Kernel::Logger g_log("Run");
+
+auto addPeriodSeries = [](Property *left, const Property *right) {
+  auto *leftAP = dynamic_cast<PropertyWithValue<std::vector<double>> *>(left);
+  const auto *rightAP = dynamic_cast<const PropertyWithValue<std::vector<double>> *>(right);
+  if (!leftAP || !rightAP) {
+    throw std::runtime_error("Expected ArrayProperty<double> for both inputs.");
+  }
+  const auto &leftVec = leftAP->operator()();
+  const auto &rightVec = rightAP->operator()();
+  if (leftVec.size() != rightVec.size()) {
+    g_log.warning(
+        "Summing runs with differing number of periods. Proton charge logs will retain values from LHS operand.");
+    return;
+  }
+  std::vector<double> resVec;
+  resVec.reserve(leftVec.size());
+  for (size_t i = 0; i < leftVec.size(); i++) {
+    resVec.push_back(leftVec[i] + rightVec[i]);
+  }
+  *leftAP = resVec;
+};
+
+/// helper struct for addable properties
+struct addableProperty {
+public:
+  using func = std::function<void(Property *, const Property *)>;
+  addableProperty(const std::string &name, func opFunc_in = nullptr) : name(name), opFunc(opFunc_in) {};
+  std::string name;
+  func opFunc;
+};
+
+/// The log entries that can be summed when adding two runs together
+const std::vector<addableProperty> ADDABLE{addableProperty("tot_prtn_chrg"),
+                                           addableProperty("rawfrm"),
+                                           addableProperty("goodfrm"),
+                                           addableProperty("dur"),
+                                           addableProperty("gd_prtn_chrg"),
+                                           addableProperty("uA.hour"),
+                                           addableProperty("monitor0_counts"),
+                                           addableProperty("monitor1_counts"),
+                                           addableProperty("monitor2_counts"),
+                                           addableProperty("monitor3_counts"),
+                                           addableProperty("monitor4_counts"),
+                                           addableProperty("monitor5_counts"),
+                                           addableProperty("proton_charge_by_period", addPeriodSeries)};
+
 /// Name of the goniometer log when saved to a NeXus file
 const char *GONIOMETER_LOG_NAME = "goniometer";
 const char *GONIOMETERS_LOG_NAME = "goniometers";
@@ -40,9 +84,6 @@ const char *HISTO_BINS_LOG_NAME = "processed_histogram_bins";
 const char *PEAK_RADIUS_GROUP = "peak_radius";
 const char *INNER_BKG_RADIUS_GROUP = "inner_bkg_radius";
 const char *OUTER_BKG_RADIUS_GROUP = "outer_bkg_radius";
-
-/// static logger object
-Kernel::Logger g_log("Run");
 } // namespace
 
 Run::Run() {
@@ -192,7 +233,8 @@ Run &Run::operator+=(const Run &rhs) {
   mergeMergables(*m_manager, *rhs.m_manager);
 
   // Other properties are added together if they are on the approved list
-  for (const auto &name : ADDABLE) {
+  for (const auto &addableProp : ADDABLE) {
+    const std::string name = addableProp.name;
     if (rhs.m_manager->existsProperty(name)) {
       // get a pointer to the property on the right-hand side workspace
       const Property *right = rhs.m_manager->getProperty(name);
@@ -200,7 +242,11 @@ Run &Run::operator+=(const Run &rhs) {
       // now deal with the left-hand side
       if (m_manager->existsProperty(name)) {
         Property *left = m_manager->getProperty(name);
-        left->operator+=(right);
+        if (!addableProp.opFunc) {
+          left->operator+=(right);
+        } else {
+          addableProp.opFunc(left, right);
+        }
       } else
         // no property on the left-hand side, create one and copy the
         // right-hand side across verbatim
@@ -262,11 +308,11 @@ double Run::getProtonCharge() const {
   if (!m_manager->existsProperty(PROTON_CHARGE_LOG_NAME)) {
     integrateProtonCharge();
   } else if (m_manager->existsProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME) &&
-             m_manager->getProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME)) {
+             m_manager->getProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME).operator int()) {
     const std::vector<double> &protonChargeByPeriod = m_manager->getProperty("proton_charge_by_period");
     const int currentPeriod = m_manager->getProperty("current_period");
     m_manager->setProperty(PROTON_CHARGE_LOG_NAME, protonChargeByPeriod[currentPeriod - 1]);
-    m_manager->setProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME, false);
+    m_manager->setProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME, 0);
   }
 
   if (m_manager->existsProperty(PROTON_CHARGE_LOG_NAME)) {
@@ -338,7 +384,7 @@ void Run::integrateProtonCharge(const std::string &logname) const {
     const_cast<Run *>(this)->setProtonCharge(total);
     // Mark gd_prtn_chrg as filtered as this method accounts for period filtering
     if (m_manager->existsProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME)) {
-      m_manager->setProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME, false);
+      m_manager->setProperty(PROTON_CHARGE_UNFILTERED_LOG_NAME, 0);
     }
 
   } else {
@@ -890,6 +936,12 @@ void Run::loadNexusCommon(::NeXus::File *file, const std::string &nameClass) {
     double charge;
     file->readData("proton_charge", charge);
     this->setProtonCharge(charge);
+  } else if (nameClass == "proton_charge_by_period") {
+    file->openGroup(nameClass, "NXlog");
+    std::vector<double> values;
+    file->readData("value", values);
+    file->closeGroup();
+    this->addProperty("proton_charge_by_period", values, true);
   }
 }
 

Framework/DataHandling/src/LoadNexusLogs.cpp
@@ -606,12 +606,6 @@ void LoadNexusLogs::execLoader() {
 
   if (!workspace->run().hasProperty(workspace->run().getProtonChargeLogName())) {
     try {
-      // For period data mark proton charge log value as unfiltered to enable subsequent filtering by period.
-      if (workspace->run().hasProperty("proton_charge_by_period")) {
-        Kernel::PropertyWithValue<bool> *pChargeUnfiltered =
-            new Kernel::PropertyWithValue<bool>(workspace->run().getProtonChargeUnfilteredName(), true);
-        workspace->mutableRun().addProperty(pChargeUnfiltered, true);
-      }
       // Try pulling proton charge from the main proton_charge entry
       file.openData("proton_charge");
       std::vector<double> values;
@@ -632,6 +626,12 @@ void LoadNexusLogs::execLoader() {
         // Ignore not found property error.
       }
     }
+  }
+  // For period data mark proton charge log value as unfiltered to enable subsequent filtering by period.
+  if (workspace->run().hasProperty("proton_charge_by_period")) {
+    Kernel::PropertyWithValue<int> *pChargeUnfiltered =
+        new Kernel::PropertyWithValue<int>(workspace->run().getProtonChargeUnfilteredName(), 1);
+    workspace->mutableRun().addProperty(pChargeUnfiltered, true);
   }
 
   if (!allow_list.empty()) {

Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/SANS/SANSTubeCalibration.py
@@ -96,7 +96,6 @@ class SANSTubeCalibration(DataProcessorAlgorithm):
     _MERGED_WS_NAME = "original"
     _CALIBRATED_WS_NAME = "result"
     _NEXUS_SUFFIX = ".nxs"
-    _PROTON_CHRG_LOG = "proton_charge_by_period"
     _REAR_DET_Z_LOG = "Rear_Det_Z"
     _FRONT_DET_Z_LOG = "Front_Det_Z"
     _BEAM_STOPPER_STRIP_POSITION = 260
@@ -396,8 +395,7 @@ class SANSTubeCalibration(DataProcessorAlgorithm):
         return ws_list
 
     def _get_proton_charge(self, ws) -> float:
-        proton_charge = ws.getRun()[self._PROTON_CHRG_LOG].value
-        return proton_charge[0] if type(proton_charge) is np.ndarray else proton_charge
+        return ws.getRun().getProtonCharge()
 
     def _crop_and_scale_workspace(self, ws, start_pixel: int, end_pixel: int, uamphr_to_rescale: float):
         scaled_ws_name = ws.name() + self._SCALED_WS_SUFFIX

Framework/DataHandling/test/LoadISISNexusTest.h
@@ -113,7 +113,7 @@ public:
     TS_ASSERT_EQUALS(mon_ws->y(0)[3], 0.);
 
     const std::vector<Property *> &logs = mon_ws->run().getLogData();
-    TS_ASSERT_EQUALS(logs.size(), 50);
+    TS_ASSERT_EQUALS(logs.size(), 51);
 
     TimeSeriesProperty<std::string> *slog =
         dynamic_cast<TimeSeriesProperty<std::string> *>(mon_ws->run().getLogData("icp_event"));
@@ -187,7 +187,7 @@ public:
     TS_ASSERT(ws->getSpectrum(1234).hasDetectorID(1235));
 
     const std::vector<Property *> &logs = ws->run().getLogData();
-    TS_ASSERT_EQUALS(logs.size(), 50);
+    TS_ASSERT_EQUALS(logs.size(), 51);
 
     TimeSeriesProperty<std::string> *slog =
         dynamic_cast<TimeSeriesProperty<std::string> *>(ws->run().getLogData("icp_event"));
@@ -402,7 +402,7 @@ public:
     ld.setPropertyValue("SpectrumMin", "10");
     ld.setPropertyValue("SpectrumMax", "20");
     //      ld.setPropertyValue("SpectrumList","29,30,31");
-    ld.setPropertyValue("EntryNumber", "5");
+    ld.setPropertyValue("EntryNumber", "1");
     TS_ASSERT_THROWS_NOTHING(ld.execute());
     TS_ASSERT(ld.isExecuted());
 
@@ -561,7 +561,7 @@ public:
     TS_ASSERT(ws->getSpectrum(1234 - 2).hasDetectorID(1235));
 
     const std::vector<Property *> &logs = ws->run().getLogData();
-    TS_ASSERT_EQUALS(logs.size(), 50);
+    TS_ASSERT_EQUALS(logs.size(), 51);
 
     TimeSeriesProperty<std::string> *slog =
         dynamic_cast<TimeSeriesProperty<std::string> *>(ws->run().getLogData("icp_event"));

Framework/DataHandling/test/LoadNexusLogsTest.h
@@ -189,6 +189,34 @@ public:
     TSM_ASSERT_EQUALS("Should have four proton charge entries", 4, protonChargeByPeriod.size());
   }
 
+  void test_gd_prtn_chrg_unfiltered_added_for_period_dataset() {
+
+    auto testWS = createTestWorkspace();
+    auto run = testWS->run();
+
+    TSM_ASSERT("Should not have gd_prtn_chrg_unfiltered until we run LoadNexusLogs",
+               !run.hasProperty("gd_prtn_chrg_unfiltered"));
+
+    LoadNexusLogs loader;
+    loader.setChild(true);
+    loader.initialize();
+    loader.setProperty("Workspace", testWS);
+    loader.setPropertyValue("Filename", "LARMOR00003368.nxs");
+    loader.execute();
+
+    run = testWS->run();
+    run.addProperty("current_period", 1, true);
+    const bool hasGdPrtnChrgUnfiltered = run.hasProperty("gd_prtn_chrg_unfiltered");
+    TSM_ASSERT("Should have period_log now we have run LoadNexusLogs", hasGdPrtnChrgUnfiltered);
+
+    int gdPrtnChrgUnfiltered = run.getPropertyValueAsType<int>("gd_prtn_chrg_unfiltered");
+    TSM_ASSERT("Value of gd_prtn_chrg_unfiltered true until getProtonCharge called", gdPrtnChrgUnfiltered);
+
+    run.getProtonCharge();
+    gdPrtnChrgUnfiltered = run.getPropertyValueAsType<int>("gd_prtn_chrg_unfiltered");
+    TSM_ASSERT("Value of gd_prtn_chrg_unfiltered false following getProtonCharge call", !gdPrtnChrgUnfiltered);
+  }
+
   void test_extract_run_title_from_event_nexus() {
 
     auto testWS = createTestWorkspace();

Framework/PythonInterface/test/python/mantid/api/RunTest.py
@@ -40,13 +40,13 @@ class RunTest(unittest.TestCase):
 
     def test_proton_charge_accounts_for_period_filtering(self):
         run = self._run
-        run.addProperty("gd_prtn_chrg_unfiltered", True, True)
+        run.addProperty("gd_prtn_chrg_unfiltered", 1, True)
         run.addProperty("proton_charge_by_period", [6.0, 4.05], True)
         run.addProperty("current_period", 1, True)
         charge = run.getProtonCharge()
         self.assertAlmostEqual(charge, 6.0)
 
-        run.addProperty("gd_prtn_chrg_unfiltered", True, True)
+        run.addProperty("gd_prtn_chrg_unfiltered", 1, True)
         run.addProperty("current_period", 2, True)
         charge = run.getProtonCharge()
         self.assertAlmostEqual(charge, 4.05)

qt/python/mantidqt/mantidqt/widgets/samplelogs/test/test_samplelogs_model.py
@@ -27,7 +27,7 @@ class SampleLogsModelTest(unittest.TestCase):
         self.assertEqual(log.size(), 4)
 
         log_names = model.get_log_names()
-        self.assertEqual(len(log_names), 46)
+        self.assertEqual(len(log_names), 47)
         self.assertIn("w", log_names)
 
         values = model.get_log_display_values("w")
@@ -49,7 +49,7 @@ class SampleLogsModelTest(unittest.TestCase):
         self.assertEqual(itemModel.horizontalHeaderItem(1).text(), "Type")
         self.assertEqual(itemModel.horizontalHeaderItem(2).text(), "Value")
         self.assertEqual(itemModel.horizontalHeaderItem(3).text(), "Units")
-        self.assertEqual(itemModel.rowCount(), 46)
+        self.assertEqual(itemModel.rowCount(), 47)
         self.assertEqual(itemModel.item(0, 0).text(), "C6_MASTER_FREQUENCY")
         self.assertEqual(itemModel.item(0, 1).text(), "float series")
         self.assertEqual(itemModel.item(0, 2).text(), "50.0 (2 entries)")
@@ -80,7 +80,7 @@ class SampleLogsModelTest(unittest.TestCase):
         model = SampleLogsModel(ws)
 
         log_names = model.get_log_names()
-        self.assertEqual(len(log_names), 140)
+        self.assertEqual(len(log_names), 141)
         self.assertIn("raw_uah_log", log_names)
         self.assertIn("current_period", log_names)
         self.assertIn("period", log_names)
@@ -138,7 +138,7 @@ class SampleLogsModelTest(unittest.TestCase):
 
         model = SampleLogsModel(ws)
         log_names = model.get_log_names()
-        self.assertEqual(len(log_names), 48)
+        self.assertEqual(len(log_names), 49)
         invalid_logs = model.get_logs_with_invalid_data()
         self.assertEqual(2, len(invalid_logs.keys()))
         self.assertIn("cryo_temp1", invalid_logs.keys())