3e28598fdb12734ed336c4c1921b4e0189b9900e
James Clarke <james.clarke@stfc.ac.uk>
2025-06-12 15:01:02 +0100

    Change to use Instrument 2.0
    Instead of looping through the components in the instrument, we now use
    the information on the DetectorInfo class on the workspace to get IDs
    and position, etc. This will hopefully make things simpler and
    easier to maintain in the future. This also eases the difficulties
    working out how to map components to spectra, since it's easy with
    detector IDs.

--- qt/python/instrumentview/instrumentview/Detectors.py
+++ qt/python/instrumentview/instrumentview/Detectors.py
@@ -7,6 +7,16 @@
 import numpy as np
 
 
+class DetectorPosition(np.ndarray):
+    """A numpy array that uses a tolerance on the data to check for equality"""
+
+    def __new__(cls, input_array):
+        return np.asarray(input_array).view(cls)
+
+    def __eq__(self, other):
+        return np.allclose(self, other)
+
+
 class DetectorInfo:
     """Class for wrapping up information relating to a detector. Used for transferring this info to the View to be displayed."""
 
@@ -27,3 +37,16 @@ class DetectorInfo:
         self.spherical_position = spherical_position
         self.component_path = component_path
         self.pixel_counts = pixel_counts
+
+
+class Detector:
+    """Class for wrapping up information relating to a detector. We need the detector ID,
+    position, whether it is a monitor, and the index in the whole list of detectors."""
+
+    def __init__(self, index: int, id: int, detector_info):
+        self.index_in_whole_list = index
+        self.id = id
+        position = detector_info.position(index)
+        self.position = DetectorPosition(position)
+        self.spherical_position = position.getSpherical()
+        self.is_monitor = detector_info.isMonitor(index)
