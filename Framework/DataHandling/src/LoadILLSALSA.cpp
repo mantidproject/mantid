f038b31f14053a732c50e9b558e4aafb080a8e49
Pete Peterson <petersonpf@ornl.gov>
2025-06-13 12:00:46 -0400

    Nexus changes and other assundries (#39480)
    This is a version of the following into `ornl-next`
    * #39441 
    * #39477 
    * #39455
    
    Co-authored-by: Reece Boston <52183986+rboston628@users.noreply.github.com>

--- Framework/DataHandling/src/LoadILLSALSA.cpp
+++ Framework/DataHandling/src/LoadILLSALSA.cpp
@@ -207,7 +207,7 @@ void LoadILLSALSA::loadNexusV2(const H5::H5File &h5file) {
   H5::DataSpace scanVarNamesSpace = scanVarNames.getSpace();
 
   nDims = scanVarNamesSpace.getSimpleExtentNdims();
-  dimsSize = std::vector<hsize_t>(nDims);
+  dimsSize.resize(nDims);
   scanVarNamesSpace.getSimpleExtentDims(dimsSize.data(), nullptr);
 
   std::vector<char *> rdata(dimsSize[0]);
@@ -229,17 +229,22 @@ void LoadILLSALSA::loadNexusV2(const H5::H5File &h5file) {
   H5::DataSpace scanVarSpace = scanVar.getSpace();
 
   nDims = scanVarSpace.getSimpleExtentNdims();
-  dimsSize = std::vector<hsize_t>(nDims);
-  scanVarSpace.getSimpleExtentDims(dimsSize.data(), nullptr);
-  if ((nDims != 2) || (dimsSize[1] != numberOfScans)) // cppcheck-suppress containerOutOfBounds
+  std::vector<double> monitorData;
+  dimsSize.resize(nDims);
+  if (dimsSize.size() < 2) {
     throw std::runtime_error("Scanned variables are not formatted properly. Check you nexus file.");
+  } else {
+    scanVarSpace.getSimpleExtentDims(dimsSize.data(), nullptr);
 
-  std::vector<double> scanVarData(dimsSize[0] * dimsSize[1]); // cppcheck-suppress containerOutOfBounds
-  scanVar.read(scanVarData.data(), scanVar.getDataType());
-  std::vector<double> monitorData(dimsSize[1]); // cppcheck-suppress containerOutOfBounds
-  for (size_t i = 0; i < monitorData.size(); i++)
-    monitorData[i] = scanVarData[monitorIndex * dimsSize[1] + i]; // cppcheck-suppress containerOutOfBounds
+    if (dimsSize[1] != numberOfScans)
+      throw std::runtime_error("Scanned variables are not formatted properly. Check you nexus file.");
 
+    std::vector<double> scanVarData(dimsSize[0] * dimsSize[1]);
+    scanVar.read(scanVarData.data(), scanVar.getDataType());
+    monitorData.resize(dimsSize[1]);
+    for (size_t i = 0; i < monitorData.size(); i++)
+      monitorData[i] = scanVarData[monitorIndex * dimsSize[1] + i];
+  }
   scanVar.close();
 
   // fill the workspace
