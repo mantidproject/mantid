1b7821c672bbd4c43722f964da805740df922cc8
Gui Pereira <guijose.pereira@gmail.com>
2025-06-16 14:14:59 +0100

    Remove detector info from projection inputs
    The projections depend only on sample, root and detector positions, so I
    made that clearer by setting these as the inputs to the class.
    Changed model getter method for projection positions to be consistent
    with main positions.

--- qt/python/instrumentview/instrumentview/Projections/cylindrical_projection.py
+++ qt/python/instrumentview/instrumentview/Projections/cylindrical_projection.py
@@ -6,13 +6,16 @@
 # SPDX - License - Identifier: GPL - 3.0 +
 from instrumentview.Projections.projection import projection
 import numpy as np
+from instrumentview.Detectors import DetectorPosition
 
 
 class cylindrical_projection(projection):
     """2D projection with a cylindrical coordinate system, see https://en.wikipedia.org/wiki/Cylindrical_coordinate_system"""
 
-    def __init__(self, workspace, detectors, axis: np.ndarray):
-        super().__init__(workspace, detectors, axis)
+    def __init__(
+        self, sample_position: np.ndarray, root_position: np.ndarray, detector_positions: list[DetectorPosition], axis: np.ndarray
+    ):
+        super().__init__(sample_position, root_position, detector_positions, axis)
 
     def _calculate_2d_coordinates(self, detector_position: np.ndarray) -> tuple[float, float]:
         detector_relative_position = detector_position - self._sample_position
