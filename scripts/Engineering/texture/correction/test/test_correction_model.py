b2478c02b103ccacb190aa91768ff58035c070e2
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-06-17 13:03:09 +0100

    update corr tests and add pf tests

--- scripts/Engineering/texture/correction/test/test_correction_model.py
+++ scripts/Engineering/texture/correction/test/test_correction_model.py
@@ -7,7 +7,7 @@
 import unittest
 from unittest import mock
 import numpy as np
-from unittest.mock import patch, MagicMock, mock_open
+from unittest.mock import patch, MagicMock, mock_open, call
 from mantid.api import AnalysisDataService as ADS
 from mantid.simpleapi import CreateSampleWorkspace, SetGoniometer, SetSample
 
@@ -65,6 +65,81 @@ class TextureCorrectionModelTest(unittest.TestCase):
         <centre x='0.0' y='0.0' z='0.0'  />  \
         </cuboid>  \
         <algebra val='{name}' /> \\ """
+
+    def run_euler_gonio_test(self, test_wss, txt_data, euler_scheme, euler_sense, target_calls, mock_set_gonio):
+        mock_set_gonio.reset_mock()
+        with patch("builtins.open", mock_open(read_data=txt_data)):
+            self.model.load_all_orientations(test_wss, "angles.txt", use_euler=True, euler_scheme=euler_scheme, euler_sense=euler_sense)
+
+        self.assertEqual(mock_set_gonio.call_count, 2)
+        expected_calls = [call(self.mock_ws, **target_call) for target_call in target_calls]
+        mock_set_gonio.assert_has_calls(expected_calls)
+
+    @patch(correction_model_path + ".SetGoniometer")
+    def test_load_all_orientations_using_euler_angles(self, mock_set_gonio):
+        test_wss = [self.mock_ws, self.mock_ws]
+        txt_data = "30,45,60\n15,30,45"
+        euler_schemes = [["x", "y", "z"], ["y", "x", "y"]]  # try two different rot schemes
+        euler_senses = ["1,1,-1", "-1,-1,1"]  # and two sets of sense
+        target_calls = [
+            (
+                {"Axis0": "30,1,0,0,1", "Axis1": "45,0,1,0,1", "Axis2": "60,0,0,1,-1"},
+                {"Axis0": "15,1,0,0,1", "Axis1": "30,0,1,0,1", "Axis2": "45,0,0,1,-1"},
+            ),
+            (
+                {"Axis0": "30,0,1,0,-1", "Axis1": "45,1,0,0,-1", "Axis2": "60,0,1,0,1"},
+                {"Axis0": "15,0,1,0,-1", "Axis1": "30,1,0,0,-1", "Axis2": "45,0,1,0,1"},
+            ),
+        ]  # expected rot axes
+        for i in range(2):
+            self.run_euler_gonio_test(test_wss, txt_data, euler_schemes[i], euler_senses[i], target_calls[i], mock_set_gonio)
+
+    @patch(correction_model_path + ".SetGoniometer")
+    def test_load_all_orientations_using_rot_mat(self, mock_set_gonio):
+        test_wss = [self.mock_ws, self.mock_ws]
+        txt_data = "1,0,0,0,1,0,0,0,1,2,2,2\n0,1,0,0,0,1,1,0,0,2,2,2"
+
+        target_calls = (
+            {"GoniometerMatrix": [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]},
+            {"GoniometerMatrix": [0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0]},
+        )
+
+        with patch("builtins.open", mock_open(read_data=txt_data)):
+            self.model.load_all_orientations(test_wss, "rot_mats.txt", use_euler=False)
+
+        self.assertEqual(mock_set_gonio.call_count, 2)
+        expected_calls = [call(self.mock_ws, **target_call) for target_call in target_calls]
+        mock_set_gonio.assert_has_calls(expected_calls)
+
+    @patch(correction_model_path + ".logger")
+    def test_load_all_orientations_error_handling_when_matrix_given_to_euler(self, mock_logger):
+        test_wss = [self.mock_ws, self.mock_ws]
+        txt_data = "1,0,0,0,1,0,0,0,1,2,2,2\n0,1,0,0,0,1,1,0,0,2,2,2"
+
+        with patch("builtins.open", mock_open(read_data=txt_data)):
+            self.model.load_all_orientations(test_wss, "rot_mats.txt", use_euler=True, euler_scheme=["x", "y", "z"], euler_sense=["1,1,1"])
+
+        mock_logger.error.assert_called_once_with(
+            "'list' object has no attribute 'split'. Failed to set goniometer, "
+            "are your settings for `use_euler_angles` correct? Currently: True"
+        )
+
+    @patch(correction_model_path + ".logger")
+    def test_load_all_orientations_error_handling_when_euler_given_as_matrix(self, mock_logger):
+        test_wss = [self.mock_ws, self.mock_ws]
+        txt_data = "30,45,60\n15,30,45"
+
+        with patch("builtins.open", mock_open(read_data=txt_data)):
+            self.model.load_all_orientations(test_wss, "rot_mats.txt", use_euler=False, euler_scheme=["x", "y", "z"], euler_sense=["1,1,1"])
+
+        mock_logger.error.assert_called_once_with(
+            'Problem setting "GoniometerMatrix" in SetGoniometer-v1: '
+            'When converting parameter "GoniometerMatrix": '
+            'When setting value of property "GoniometerMatrix": '
+            "Incorrect size. Failed to set goniometer, "
+            "are your settings for `use_euler_angles` correct? "
+            "Currently: False"
+        )
 
     @patch(correction_model_path + ".TextureCorrectionModel._has_no_orientation_set")
     @patch(correction_model_path + ".TextureCorrectionModel._has_no_valid_material")
@@ -211,8 +286,10 @@ class TextureCorrectionModelTest(unittest.TestCase):
     def test_define_gauge_vol_for_preset(self, mock_def_gauge_vol):
         preset = "4mmCube"
         expected_str = (
-            "\n<cuboid id='some-gv'> <height val='4.0'  /> <width val='4.0' />  <depth  val='4.0' />  "
-            "<centre x='0.0' y='0.0' z='0.0'  />  </cuboid>  <algebra val='some-gv' /> \\ "
+            "\n        <cuboid id='some-gv'>         <height val='0.04'  />         "
+            "<width val='0.04' />          <depth  val='0.04' />          "
+            "<centre x='0.0' y='0.0' z='0.0'  />          </cuboid>          "
+            "<algebra val='some-gv' /> \\ "
         )
         self.model.define_gauge_volume(self.ws_name, preset=preset, custom=None)
         mock_def_gauge_vol.assert_called_once_with(self.ws_name, expected_str)
@@ -379,6 +456,12 @@ class TextureCorrectionModelTest(unittest.TestCase):
         self.model._save_corrected_files("ws", "/tmp", "AbsorptionCorrection", "RB123", calib.group)
         mock_makedirs.assert_called()
         mock_save.assert_called()
+
+    @patch(correction_model_path + ".LoadEmptyInstrument")
+    def test_create_reference_ws_sets_correct_name_and_calls_loader(self, mock_load_instr):
+        self.model.create_reference_ws("RB123")
+        self.assertEqual(self.model.reference_ws, "RB123_reference_workspace")
+        mock_load_instr.assert_called_once_with(InstrumentName="ENGINX", OutputWorkspace="RB123_reference_workspace")
 
 
 if __name__ == "__main__":
