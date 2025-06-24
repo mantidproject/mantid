a6dfeebb55dcb5f871f65a5df84957a645b85bfa
James Clarke <james.clarke@stfc.ac.uk>
2025-06-24 09:53:53 +0100

    Fix projection tests after refactor

--- qt/python/instrumentview/instrumentview/test/test_model.py
+++ qt/python/instrumentview/instrumentview/test/test_model.py
@@ -9,13 +9,23 @@ from unittest import mock
 import numpy as np
 
 
+class MockPosition:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getSpherical(self):
+        return [self.x, self.y, self.z]
+
+    def __array__(self):
+        return np.array([self.x, self.y, self.z])
+
+
 class TestFullInstrumentViewModel(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls._ws = CreateSampleWorkspace(OutputWorkspace="TestFullInstrumentViewModel", XUnit="TOF")
-
-    def setUp(self):
-        self._model = FullInstrumentViewModel(self._ws)
 
     def _create_mock_workspace(self, detector_ids: list[int]):
         mock_workspace = mock.MagicMock()
@@ -24,30 +34,33 @@ class TestFullInstrumentViewModel(unittest.TestCase):
         mock_workspace.getNumberHistograms.return_value = len(detector_ids)
         mock_workspace.detectorInfo().detectorIDs.return_value = detector_ids
         mock_workspace.detectorInfo().isMonitor.return_value = False
+        mock_workspace.detectorInfo().position.side_effect = lambda i: MockPosition(i, i, i)
         mock_workspace.dataX.return_value = list(range(len(detector_ids)))
         mock_workspace.getDetectorIDToWorkspaceIndexMap.return_value = {id: i for i, id in enumerate(detector_ids)}
         mock_workspace.getIntegratedCountsForWorkspaceIndices.return_value = [100 * i for i in detector_ids]
         return mock_workspace
 
     def test_union_with_current_bin_min_max(self):
-        current_min = self._model._bin_min
-        current_max = self._model._bin_max
-        self._model._union_with_current_bin_min_max(current_min - 1)
-        self.assertEqual(self._model._bin_min, current_min - 1)
-        self._model._union_with_current_bin_min_max(current_min)
-        self.assertEqual(self._model._bin_min, current_min - 1)
-        self.assertEqual(self._model._bin_max, current_max)
-        self._model._union_with_current_bin_min_max(current_max + 1)
-        self.assertEqual(self._model._bin_max, current_max + 1)
+        model = FullInstrumentViewModel(self._ws)
+        current_min = model._bin_min
+        current_max = model._bin_max
+        model._union_with_current_bin_min_max(current_min - 1)
+        self.assertEqual(model._bin_min, current_min - 1)
+        model._union_with_current_bin_min_max(current_min)
+        self.assertEqual(model._bin_min, current_min - 1)
+        self.assertEqual(model._bin_max, current_max)
+        model._union_with_current_bin_min_max(current_max + 1)
+        self.assertEqual(model._bin_max, current_max + 1)
 
     def test_update_time_of_flight_range(self):
+        model = FullInstrumentViewModel(self._ws)
         integrated_spectra = list(range(len(self._ws.spectrumInfo())))
-        self._model._workspace = mock.MagicMock()
-        self._model._workspace.getIntegratedCountsForWorkspaceIndices.return_value = integrated_spectra
-        self._model.update_time_of_flight_range(200, 10000, False)
-        self._model._workspace.getIntegratedCountsForWorkspaceIndices.assert_called_once()
-        self.assertEqual(min(integrated_spectra), self._model._data_min)
-        self.assertEqual(max(integrated_spectra), self._model._data_max)
+        model._workspace = mock.MagicMock()
+        model._workspace.getIntegratedCountsForWorkspaceIndices.return_value = integrated_spectra
+        model.update_time_of_flight_range(200, 10000, False)
+        model._workspace.getIntegratedCountsForWorkspaceIndices.assert_called_once()
+        self.assertEqual(min(integrated_spectra), model._data_min)
+        self.assertEqual(max(integrated_spectra), model._data_max)
 
     # TODO: Add test for picked_detectors_info_text
     # TODO: Add tests for other picking methods
@@ -70,10 +83,12 @@ class TestFullInstrumentViewModel(unittest.TestCase):
         self._run_projection_test(mock_cylindrical_projection, False)
 
     def _run_projection_test(self, mock_projection_constructor, is_spherical):
+        mock_workspace = self._create_mock_workspace([1, 2, 3])
+        model = FullInstrumentViewModel(mock_workspace)
         mock_projection = mock.MagicMock()
-        mock_projection.positions.return_value = [[1, 2], [1, 2]]
+        mock_projection.positions.return_value = [[1, 2], [1, 2], [1, 2]]
         mock_projection_constructor.return_value = mock_projection
-        points = self._model.calculate_projection(is_spherical, axis=[0, 1, 0])
+        points = model.calculate_projection(is_spherical, axis=[0, 1, 0])
         mock_projection_constructor.assert_called_once()
         self.assertTrue(all(all(point == [1, 2, 0]) for point in points))
 
