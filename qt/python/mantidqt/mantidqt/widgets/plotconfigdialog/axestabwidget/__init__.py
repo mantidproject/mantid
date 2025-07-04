7739ad6debc2dcbb688c0f4eee9f03fad1e00990
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-07-04 09:24:17 +0100

    add linthresh to axestabwidget

--- qt/python/mantidqt/mantidqt/widgets/plotconfigdialog/axestabwidget/__init__.py
+++ qt/python/mantidqt/mantidqt/widgets/plotconfigdialog/axestabwidget/__init__.py
@@ -36,11 +36,15 @@ class AxProperties(dict):
         props["yautoscale"] = ax.get_autoscaley_on()
         props["canvas_color"] = ax.get_facecolor()
 
+        props["linthreshx"] = ax.get_linthreshx()
+        props["linthreshy"] = ax.get_linthreshy()
+
         if isinstance(ax, Axes3D):
             props["zlim"] = ax.get_zlim()
             props["zlabel"] = ax.get_zlabel()
             props["zscale"] = ax.get_zscale().title()
             props["zautoscale"] = ax.get_autoscalez_on()
+            # props["linthreshz"] = ax.get_linthreshz_bounds()
         else:
             props["minor_ticks"] = not isinstance(ax.xaxis.minor.locator, NullLocator)
             props["minor_gridlines"] = ax.show_minor_gridlines if hasattr(ax, "show_minor_gridlines") else False
@@ -59,5 +63,6 @@ class AxProperties(dict):
         props[f"{ax}lim"] = (view.get_lower_limit(), view.get_upper_limit())
         props[f"{ax}label"] = view.get_label()
         props[f"{ax}scale"] = view.get_scale()
+        props[f"linthresh{ax}"] = view.get_symlog_linear_threshold()
 
         return cls(props)
