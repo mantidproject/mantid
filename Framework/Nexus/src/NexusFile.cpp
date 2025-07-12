6f9e02cafe128c3c018f6847c16a375a531eb2e7
Carson Sears <searscr@ornl.gov>
2025-07-11 20:39:05 -0500

    Fix trim str len

--- Framework/Nexus/src/NexusFile.cpp
+++ Framework/Nexus/src/NexusFile.cpp
@@ -26,6 +26,7 @@ using std::vector;
 
 #define NXEXCEPTION(message) Exception((message), __func__, m_filename);
 #define NX_UNKNOWN_GROUP ""
+#define NUL '\0'
 
 #define NAPI_CALL(status, msg)                                                                                         \
   NXstatus tmp = (status);                                                                                             \
@@ -1491,7 +1492,7 @@ Info File::getInfo() {
   }
   // Trim 1D CHAR arrays to the actual string length
   if ((info.type == NXnumtype::CHAR) && (iRank == 1)) {
-    hsize_t len = myDim[0]; // original dataset length
+    hsize_t len = myDim[0];
     std::vector<char> buffer(len + 1, '\0');
 
     hid_t memType = H5Tcopy(H5T_C_S1);
@@ -1500,8 +1501,17 @@ Info File::getInfo() {
     H5Tclose(memType);
 
     if (ret >= 0) {
-      char *trimmed = nxitrim(buffer.data());
-      info.dims[0] = static_cast<int64_t>(strlen(trimmed));
+      char *start = buffer.data();
+      while (*start && isspace(*start)) {
+        ++start;
+      }
+      size_t trimmedLen = strlen(start);
+      while (trimmedLen > 0 && isspace(start[trimmedLen - 1])) {
+        --trimmedLen;
+      }
+      start[trimmedLen] = '\0';
+
+      info.dims[0] = static_cast<int64_t>(trimmedLen);
     }
   }
   return info;
