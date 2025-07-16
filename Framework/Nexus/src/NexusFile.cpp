ecc2d4ee16bc19b449b2e5c96161ec6b7d9fcc71
Carson Sears <searscr@ornl.gov>
2025-07-16 07:52:50 -0500

    fix clang issue

--- Framework/Nexus/src/NexusFile.cpp
+++ Framework/Nexus/src/NexusFile.cpp
@@ -1864,7 +1864,6 @@ template MANTID_NEXUS_DLL void File::getData(int32_t *data);
 template MANTID_NEXUS_DLL void File::getData(uint32_t *data);
 template MANTID_NEXUS_DLL void File::getData(int64_t *data);
 template MANTID_NEXUS_DLL void File::getData(uint64_t *data);
-template MANTID_NEXUS_DLL void File::getData(char *data);
 template MANTID_NEXUS_DLL void File::getData(bool *data);
 
 template MANTID_NEXUS_DLL void File::getData(vector<float> &data);
