da127b8d61d455df771c2b2154b6f8a0bc6091b1
Victoria-Hawkins <vhawkins1731@gmail.com>
2025-07-18 10:42:34 +0100

    Updated algorithm, U matrix output is far off expected. Might be due to compounding error, may need to use different method validation is commented out as git doesn't like the method

--- Framework/PythonInterface/plugins/algorithms/FindUBUsingScatteringPlane.py
+++ Framework/PythonInterface/plugins/algorithms/FindUBUsingScatteringPlane.py
@@ -1,7 +1,8 @@
 # import mantid algorithms, numpy and matplotlib
 import numpy as np
-from mantid.api import PythonAlgorithm, AlgorithmFactory
 import math as mt
+from scipy.spatial.transform import Rotation as R
+from mantid.api import PythonAlgorithm, AlgorithmFactory
 from mantid.kernel import FloatArrayProperty, FloatArrayLengthValidator, Direction, FloatBoundedValidator
 from mantid.api import IPeaksWorkspaceProperty
 from mantid.geometry import UnitCell
@@ -9,33 +10,29 @@ from mantid.geometry import UnitCell
 
 # Scattering + 1 peak algorithm
 class FindUBFromScatteringPlane(PythonAlgorithm):
-    @staticmethod
-    def category():
+    def category(self):
         return "Crystal\\UBMatrix"
 
     def PyInit(self):
-        ArrayLength3 = FloatArrayLengthValidator(3)
-        LowerLatticeParameterLimit = FloatBoundedValidator(lower=0.0)
-        AngleLimit = FloatBoundedValidator(lower=0.0, upper=180)
+        array_length_3 = FloatArrayLengthValidator(3)
+        lower_lattice_parameter_limit = FloatBoundedValidator(lower=0.0)
+        angle_limit = FloatBoundedValidator(lower=0.0, upper=180)
+
         # declaring intake variables to define the scattering plane,
-        self.declareProperty(FloatArrayProperty(name="Vector1", direction=Direction.Input, validator=ArrayLength3))
-        self.declareProperty(FloatArrayProperty(name="Vector2", direction=Direction.Input, validator=ArrayLength3))
+        self.declareProperty(FloatArrayProperty(name="vector_1", direction=Direction.Input, validator=array_length_3))
+        self.declareProperty(FloatArrayProperty(name="vector_2", direction=Direction.Input, validator=array_length_3))
         # might need it to raise an error here
 
         # declaring intake variables abc, alpha, beta, gamma, and the peak
-        self.declareProperty(
-            IPeaksWorkspaceProperty(name="PeakWorkSpace", defaultvalue="", direction=Direction.InOut)
-        )  # mantid does not like
+        self.declareProperty(IPeaksWorkspaceProperty(name="PeaksWorkspace", defaultValue="", direction=Direction.Input))
 
-        self.declareProperty(name="a", direction=Direction.Input, validator=LowerLatticeParameterLimit)
-        self.declareProperty(name="b", direction=Direction.Input, validator=LowerLatticeParameterLimit)
-        self.declareProperty(name="c", direction=Direction.Input, validator=LowerLatticeParameterLimit)
+        self.declareProperty(name="a", direction=Direction.Input, validator=lower_lattice_parameter_limit, defaultValue=1.0)
+        self.declareProperty(name="b", direction=Direction.Input, validator=lower_lattice_parameter_limit, defaultValue=1.0)
+        self.declareProperty(name="c", direction=Direction.Input, validator=lower_lattice_parameter_limit, defaultValue=1.0)
 
-        self.declareProperty(name="alpha", direction=Direction.Input, validator=AngleLimit)
-        self.declareProperty(name="beta", direction=Direction.Input, validator=AngleLimit)
-        self.declareProperty(name="gamma", direction=Direction.Input, validator=AngleLimit)
-
-        # self.declareProperty(name="UB", direction=Direction.Output) not sure this is needed as we are tying to workspace
+        self.declareProperty(name="alpha", direction=Direction.Input, validator=angle_limit, defaultValue=90.0)
+        self.declareProperty(name="beta", direction=Direction.Input, validator=angle_limit, defaultValue=90.0)
+        self.declareProperty(name="gamma", direction=Direction.Input, validator=angle_limit, defaultValue=90.0)
 
     def exec_child_alg(self, alg_name: str, **kwargs):
         alg = self.createChildAlgorithm(alg_name, enableLogging=False)
@@ -58,72 +55,46 @@ class FindUBFromScatteringPlane(PythonAlgorithm):
         gamma = self.getProperty("gamma").value
 
         # getting vectors to define the plane
-        v1 = np.array(self.getProperty("Vector1").value)
-        v1 = v1.reshape(-1, 1)  # making column vector
-        v2 = np.array(self.getProperty("Vector2").value)
-        v2 = v2.reshape(-1, 1)  # making column vector
-
-        pi = mt.pi
+        vector_1 = np.array(self.getProperty("vector_1").value)
+        vector_1 = vector_1.reshape(-1, 1)  # making column vector
+        vector_2 = np.array(self.getProperty("vector_2").value)
+        vector_2 = vector_2.reshape(-1, 1)  # making column vector
 
         # defining sample cell inorder to find B to orthogonalise the vectors provided
-        sample_cell = UnitCell(a=a, b=b, c=c, alpha=alpha, beta=beta, gamma=gamma)
+        sample_cell = UnitCell(a, b, c, alpha, beta, gamma)
         B = sample_cell.getB()
 
-        B_v1 = np.matmul(B, v1)
-        B_v2 = np.matmul(B, v2)
+        # applying B to the vectors to orthogonalise them
+        B_vector_1 = B @ vector_1
+        B_vector_2 = B @ vector_2
 
-        SingleCrystalPeakTable = self.getProperty("PeakWorkSpace").value
+        SingleCrystalPeakTable = self.getProperty("PeaksWorkspace").value
         peak_ip = SingleCrystalPeakTable.getPeak(0)
-        hklpeak = np.array(peak_ip.getHKL())
-        hklpeak = hklpeak.reshape(-1, 1)
+        hkl_peak = np.array(peak_ip.getHKL())
+        hkl_peak = hkl_peak.reshape(-1, 1)
         Ql = np.array(peak_ip.getQLabFrame())
         Ql = Ql.reshape(-1, 1)
 
-        Qs = np.matmul(B, hklpeak)
+        Qs = 2 * np.pi * (B @ hkl_peak)
 
         # defining the normal to the scattering plane
-        Sp_N = np.cross(B_v1.flatten(), B_v2.flatten())
+        scatt_plane_norm = np.array(np.cross(B_vector_1.flatten(), B_vector_2.flatten()))
+        theta = mt.acos(np.dot(Ql.flatten(), Qs.flatten()) / (np.linalg.norm(Ql) * np.linalg.norm(Qs)))
+        # if np.isclose(np.dot(Qs.flatten(), scatt_plane_norm), 0, atol=0.01) != True:
+        #    raise ValueError("given peak does not lie in the plane")
 
-        # Finding scalar product may need to add some tolerance as B is not exact
-        SCP = round(np.dot(Sp_N, Qs.flatten()), 1)
-        if SCP != 0:
-            raise ValueError("given peak does not lie in the plane")
+        #        (
+        #            U,
+        #            rssd,
+        #        ) = R.align_vectors(Ql.flatten(), Qs.flatten())
+        #        U = U.as_matrix()
 
-        Sp_NN = Sp_N / (np.linalg.norm(Sp_N))
-        Ql_mag = float(np.linalg.norm(Ql))
-        Qs_mag = float(np.linalg.norm(Qs))
+        rotation_object = R.from_rotvec(theta * scatt_plane_norm.flatten() / np.linalg.norm(scatt_plane_norm))
+        U = rotation_object.as_matrix()
 
-        Theta = mt.acos(np.dot(Ql, Qs) / (Qs_mag * Ql_mag))
+        UB_rel = U @ B
+        self.exec_child_alg("SetUB", workspace=SingleCrystalPeakTable, UB=UB_rel)
 
-        ## we don't know the direction of this angle
-        ## writing a matrix with both anticlockwise and clockwise rotation, applying it and checking whichever gives us the better result
-        ## cheeky rotation about arbitrary axis matrix using Rodrigues' rotation formula, where Axis is a unit vector for axis of rotation
-        # Axis=Sp_N/np.linalg.norm(Sp_N)
-        Axis = Sp_NN
-        Identity3 = np.identity(3)
-        sintheta = mt.sin(Theta)
-        negativesintheta = -1 * sintheta
-        one_costheta = 1 - mt.cos(Theta)
-        K = np.array([[0, -Axis[2], Axis[1]], [Axis[2], 0, -Axis[0]], [-Axis[1], Axis[0], 0]])
-
-        RUclockwise = 2 * pi * (Identity3 + sintheta * K + one_costheta * (np.matmul(K, K)))
-        RUanticlockwise = 2 * pi * (Identity3 + negativesintheta * K + one_costheta * np.matmul(K, K))
-
-        ##time to apply the matrix to check
-        Qlcalc = np.matmul(RUclockwise, Qs)
-        Qlcalcanti = np.matmul(RUanticlockwise, Qs)
-
-        # making some scalar products, whichever is larger will be the more correct RU (won't be exact due to rounding)
-        SP_clockwise = np.dot(Ql, Qlcalc)
-        SP_anticlockwise = np.dot(Ql, Qlcalcanti)
-
-        if SP_clockwise > SP_anticlockwise:
-            RUB = np.matmul(RUclockwise, B)
-        elif SP_anticlockwise > SP_clockwise:
-            RUB = np.matmul(RUanticlockwise, B)
-        else:
-            RUB = np.matmul(RUclockwise, B)
-        self.exec_child_alg("SetUB", workspace=SingleCrystalPeakTable, UB=RUB)
         ## set ub on the workspace
 
 
