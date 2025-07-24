d8be5e260f263cd1d9fc78db05e1a9f85e0ee0b6
rboston628 <bostonsr@ornl.gov>
2025-07-24 14:53:13 -0400

    replace file id with shared pointer

--- Framework/Nexus/inc/MantidNexus/NexusFile.h
+++ Framework/Nexus/inc/MantidNexus/NexusFile.h
@@ -4,6 +4,7 @@
 #include "MantidNexus/NexusAddress.h"
 #include "MantidNexus/NexusDescriptor.h"
 #include "MantidNexus/NexusFile_fwd.h"
+#include <H5Cpp.h>
 #include <map>
 #include <memory>
 #include <string>
@@ -53,12 +54,14 @@ private:
   /** should be close handle on exit */
   bool m_close_handle;
   /** Variables for use inside the C-API, formerly of NexusFile5
-   * \li m_fid -- the ID for open file object
+   * \li m_pfile -- shared ptr to a H5File object
    * \li m_current_group_id -- the ID for currently opened group (or 0 if none)
-   * \li m_current_data_id -- the ID forcurrently opened dataset (or 0 if none)
+   * \li m_current_data_id -- the ID for currently opened dataset (or 0 if none)
+   * \li m_current_type_id -- the ID of the type of the opened dataset
+   * \li m_current_soace_id -- the ID of the dataspace for the opened dataset
    * \li m_gid_stack -- a vector stack of opened group IDs
    */
-  hid_t m_fid;
+  std::shared_ptr<H5::H5File> m_pfile;
   hid_t m_current_group_id;
   hid_t m_current_data_id;
   hid_t m_current_type_id;
@@ -72,6 +75,8 @@ private:
   NexusDescriptor m_descriptor;
   /** NOTE: this is temporary until `napi` is fully deleted*/
   NexusAddress m_group_address;
+  /** NOTE: this is temporary until `napi` is fully deleted*/
+  hid_t m_fid;
 
   //------------------------------------------------------------------------------------------------------------------
   // CONSTRUCTORS / ASSIGNMENT / DECONSTRUCTOR
