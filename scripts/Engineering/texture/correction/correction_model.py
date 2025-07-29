17c27f211ec68167b162f6b0ad6887a358ad0698
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-07-29 11:22:01 +0100

    tidy up and improve type-hint cover

--- scripts/Engineering/texture/correction/correction_model.py
+++ scripts/Engineering/texture/correction/correction_model.py
@@ -1,3 +1,9 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
 from mantid.simpleapi import (
     LoadSampleShape,
     SetSample,
@@ -20,13 +26,122 @@ from os import path, makedirs
 from scipy import interpolate
 from Engineering.EnggUtils import GROUP
 from Engineering.common.texture_sample_viewer import has_no_valid_shape, plot_sample_directions, plot_gauge_vol
+from typing import Optional, Sequence, Union, Tuple
+from mantid.dataobjects import Workspace2D
+from matplotlib.figure import Figure
+from Engineering.common.calibration_info import CalibrationInfo
 
 
 class TextureCorrectionModel:
     def __init__(self):
         self.reference_ws = None
 
-    def load_all_orientations(self, wss, txt_file, use_euler, euler_scheme=None, euler_sense=None):
+    # ~~~~~ Correction Functions ~~~~~~~~~~~~~
+
+    def calc_absorption(self, ws: str, mc_param_str: str) -> None:
+        method_dict = self._param_str_to_dict(mc_param_str) if mc_param_str else {}  # allow no kwargs to be given
+        temp_ws = ConvertUnits(ws, Target="Wavelength")
+        method_dict["OutputWorkspace"] = "_abs_corr"
+        MonteCarloAbsorption(temp_ws, **method_dict)
+
+    @staticmethod
+    def calc_divergence(ws_name: str, horz: float, vert: float, det_horz: float) -> None:
+        # estimate of divergence taken from divergence component of equation 3 in
+        # J. Appl. Cryst. (2014). 47, 1337–1354 doi:10.1107/S1600576714012710
+
+        ws = ADS.retrieve(ws_name)
+        # extract thetas from detector table
+        det_tab = CreateDetectorTable(InputWorkspace=ws, DetectorTableWorkspace="det_tab", StoreInADS=False)
+        thetas = np.deg2rad(det_tab.column("Theta"))
+
+        # estimate per spectra divergence
+        div = vert * np.sqrt((horz**2) * (det_horz**2)) * np.sin(thetas) ** 2
+
+        # compile data in workspace
+        _div_corr = CloneWorkspace(InputWorkspace=ws, OutputWorkspace="_div_corr")
+        num_spec = ws.getNumberHistograms()
+        y_shape = ws.readY(0).shape
+        [_div_corr.setY(i, np.ones(y_shape) * div[i]) for i in range(num_spec)]
+
+    def apply_corrections(
+        self,
+        ws: str,
+        out_ws: str,
+        calibration_group: Optional[GROUP],
+        root_dir: str,
+        abs_corr: Union[float, str] = 1.0,
+        div_corr: Union[float, str] = 1.0,
+        rb_num: Optional[str] = None,
+        remove_ws_after_processing: bool = False,
+    ) -> None:
+        ws = ADS.retrieve(ws)
+        temp_ws = ConvertUnits(ws, Target="dSpacing", StoreInADS=False)
+
+        # if an absorption correction calculation has been run expect this to be "_abs_corr" (the output workspace)
+        # otherwise it will just be 1.0
+        if isinstance(abs_corr, str):
+            abs_ws = ConvertUnits(ADS.retrieve(abs_corr), Target="dSpacing", StoreInADS=False)
+            temp_ws = temp_ws / abs_ws
+            ADS.remove("abs_ws")
+        else:
+            temp_ws = temp_ws / abs_corr
+
+        # if a divergence correction calculation has been run expect this to be "_div_corr" (the output workspace)
+        # otherwise it will just be 1.0
+        if isinstance(div_corr, str):
+            div_ws = ConvertUnits(ADS.retrieve(div_corr), Target="dSpacing", StoreInADS=False)
+            temp_ws = temp_ws / div_ws
+            ADS.remove("div_ws")
+        else:
+            temp_ws = temp_ws / div_corr
+
+        # save files
+        CloneWorkspace(temp_ws, OutputWorkspace=out_ws, StoreInADS=True)
+        self._save_corrected_files(out_ws, root_dir, "AbsorptionCorrection", rb_num, calibration_group)
+
+        # optionally remove extra files
+        if remove_ws_after_processing:
+            logger.notice("removing saved and temporary workspaces from ADS")
+            # remove output ws from ADS to free up memory
+            ADS.remove(out_ws)
+            if isinstance(abs_corr, str):
+                ADS.remove(abs_corr)
+            if isinstance(div_corr, str):
+                ADS.remove(div_corr)
+
+    # ~~~~~ General Utility Functions ~~~~~~~~~~~~~
+
+    @staticmethod
+    def _validate_file(file: str, ext: str) -> bool:
+        valid = False
+        if file:
+            root, f_ext = path.splitext(file)
+            if f_ext == ext:
+                valid = True
+        return valid
+
+    @staticmethod
+    def _save_corrected_files(ws: str, root_dir: str, dir_name: str, rb_num: Optional[str], calibration_group: GROUP) -> None:
+        save_dirs = [path.join(root_dir, dir_name)]
+        if rb_num:
+            save_dirs.append(path.join(root_dir, "User", rb_num, dir_name))
+            if calibration_group == GROUP.TEXTURE20 or calibration_group == GROUP.TEXTURE30:
+                save_dirs.pop(0)  # only save to RB directory to limit number files saved
+        for save_dir in save_dirs:
+            if not path.exists(save_dir):
+                makedirs(save_dir)
+            SaveNexus(InputWorkspace=ws, Filename=path.join(save_dir, ws + ".nxs"))
+
+    # ~~~~~ Sample Definition and Orientation Functions ~~~~~~~~~~~~~
+
+    def load_all_orientations(
+        self,
+        wss: Sequence[Workspace2D],
+        txt_file: str,
+        use_euler: bool,
+        euler_scheme: Optional[str] = None,
+        euler_sense: Optional[str] = None,
+    ) -> None:
         if self._validate_file(txt_file, ".txt"):
             with open(txt_file, "r") as f:
                 goniometer_strings = [line.strip().replace("\t", ",") for line in f.readlines()]
@@ -51,14 +166,16 @@ class TextureCorrectionModel:
                     f"{str(e)}. Failed to set goniometer, are your settings for `use_euler_angles` correct? Currently: {use_euler}"
                 )
 
-    def set_sample_info(self, ws, shape, material):
+    @staticmethod
+    def set_sample_info(ws: Workspace2D, shape: str, material: str) -> None:
         if shape.endswith(".stl"):
             LoadSampleShape(InputWorkspace=ws, OutputWorkspace=ws, Filename=shape)
         else:
             SetSample(ws, Geometry={"Shape": "CSG", "Value": shape})
         SetSampleMaterial(ws, material)
 
-    def copy_sample_info(self, ref_ws_name, wss, is_ref=False):
+    @staticmethod
+    def copy_sample_info(ref_ws_name: str, wss: Sequence[Workspace2D], is_ref: bool = False) -> None:
         # currently copy shape bakes the orientation matrix into the sample shape
         if not is_ref:
             # need to create a ws with an unorientated sample to copy over
@@ -78,7 +195,7 @@ class TextureCorrectionModel:
             for ws in wss:
                 CopySample(InputWorkspace=ref_ws_name, OutputWorkspace=ws, CopyName=False, CopyEnvironment=False, CopyLattice=False)
 
-    def get_ws_info(self, ws_name, select=True):
+    def get_ws_info(self, ws_name: str, select: bool = True) -> dict:
         return {
             "shape": "Not set" if self._has_no_valid_shape(ws_name) else "set",
             "material": "Not set" if self._has_no_valid_material(ws_name) else self._get_material_name(ws_name),
@@ -86,24 +203,28 @@ class TextureCorrectionModel:
             "select": select,
         }
 
-    def _has_no_orientation_set(self, ws_name):
+    @staticmethod
+    def _has_no_orientation_set(ws_name) -> bool:
         try:
             return np.all(ADS.retrieve(ws_name).run().getGoniometer().getR() == np.identity(3))
         except RuntimeError:
             return True
 
-    def _get_material_name(self, ws_name):
+    @staticmethod
+    def _get_material_name(ws_name: str) -> str:
         return ADS.retrieve(ws_name).sample().getMaterial().name()
 
-    def _has_no_valid_material(self, ws_name):
+    def _has_no_valid_material(self, ws_name: str) -> bool:
         return self._get_material_name(ws_name) == ""
 
-    def define_gauge_volume(self, ws, preset, custom):
+    # ~~~~~ Gauge Volume Functions ~~~~~~~~~~~~~
+
+    def define_gauge_volume(self, ws: Workspace2D, preset: str, custom: Optional[str]) -> None:
         gauge_str = self.get_gauge_vol_str(preset, custom)
         if gauge_str:
             DefineGaugeVolume(ws, gauge_str)
 
-    def get_gauge_vol_str(self, preset, custom):
+    def get_gauge_vol_str(self, preset: str, custom: Optional[str]) -> str:
         if preset == "4mmCube":
             gauge_str = """
         <cuboid id='some-gv'> \
@@ -122,35 +243,24 @@ class TextureCorrectionModel:
                 gauge_str = None
         return gauge_str
 
-    def _read_xml(self, file):
+    def _read_xml(self, file: str) -> str:
         out = None
         if self._validate_file(file, ".xml"):
             with open(file, "r") as f:
                 out = f.read()
         return out
 
-    def _validate_file(self, file, ext):
-        valid = False
-        if file:
-            root, f_ext = path.splitext(file)
-            if f_ext == ext:
-                valid = True
-        return valid
+    # ~~~~~ `Parameter Dictionary as String` Functions ~~~~~~~~~~~~~
 
-    def calc_absorption(self, ws, mc_param_str):
-        method_dict = self._param_str_to_dict(mc_param_str) if mc_param_str else {}  # allow no kwargs to be given
-        temp_ws = ConvertUnits(ws, Target="Wavelength")
-        method_dict["OutputWorkspace"] = "_abs_corr"
-        MonteCarloAbsorption(temp_ws, **method_dict)
-
-    def _param_str_to_dict(self, param_string):
+    def _param_str_to_dict(self, param_string: str) -> dict:
         out_dict = {}
         for params in param_string.split(","):
             k, v = params.split(":")
             out_dict[k] = self._parse_param_values(v)
         return out_dict
 
-    def _parse_param_values(self, string):
+    @staticmethod
+    def _parse_param_values(string) -> Union[bool, int, str]:
         l_string = string.lower()
         if l_string == "true":
             return True
@@ -161,64 +271,10 @@ class TextureCorrectionModel:
         else:
             return string
 
-    def calc_divergence(self, ws_name, horz, vert, det_horz):
-        # estimate of divergence taken from divergence component of equation 3 in
-        # J. Appl. Cryst. (2014). 47, 1337–1354 doi:10.1107/S1600576714012710
+    # ~~~~~ Attenuation Table Functions ~~~~~~~~~~~~~
 
-        ws = ADS.retrieve(ws_name)
-        # extract thetas from detector table
-        det_tab = CreateDetectorTable(InputWorkspace=ws, DetectorTableWorkspace="det_tab", StoreInADS=False)
-        thetas = np.deg2rad(det_tab.column("Theta"))
-
-        # estimate per spectra divergence
-        div = vert * np.sqrt((horz**2) * (det_horz**2)) * np.sin(thetas) ** 2
-
-        # compile data in workspace
-        _div_corr = CloneWorkspace(InputWorkspace=ws, OutputWorkspace="_div_corr")
-        num_spec = ws.getNumberHistograms()
-        y_shape = ws.readY(0).shape
-        [_div_corr.setY(i, np.ones(y_shape) * div[i]) for i in range(num_spec)]
-
-    def apply_corrections(
-        self, ws, out_ws, calibration_group, root_dir, abs_corr=1.0, div_corr=1.0, rb_num=None, remove_ws_after_processing=False
-    ):
-        ws = ADS.retrieve(ws)
-        temp_ws = ConvertUnits(ws, Target="dSpacing", StoreInADS=False)
-        if isinstance(abs_corr, str):
-            abs_ws = ConvertUnits(ADS.retrieve(abs_corr), Target="dSpacing", StoreInADS=False)
-            temp_ws = temp_ws / abs_ws
-            ADS.remove("abs_ws")
-        else:
-            temp_ws = temp_ws / abs_corr
-        if isinstance(div_corr, str):
-            div_ws = ConvertUnits(ADS.retrieve(div_corr), Target="dSpacing", StoreInADS=False)
-            temp_ws = temp_ws / div_ws
-            ADS.remove("div_ws")
-        else:
-            temp_ws = temp_ws / div_corr
-        CloneWorkspace(temp_ws, OutputWorkspace=out_ws, StoreInADS=True)
-        self._save_corrected_files(out_ws, root_dir, "AbsorptionCorrection", rb_num, calibration_group)
-        if remove_ws_after_processing:
-            logger.notice("removing saved and temporary workspaces from ADS")
-            # remove output ws from ADS to free up memory
-            ADS.remove(out_ws)
-            if isinstance(abs_corr, str):
-                ADS.remove(abs_corr)
-            if isinstance(div_corr, str):
-                ADS.remove(div_corr)
-
-    def _save_corrected_files(self, ws, root_dir, dir_name, rb_num, calibration_group):
-        save_dirs = [path.join(root_dir, dir_name)]
-        if rb_num:
-            save_dirs.append(path.join(root_dir, "User", rb_num, dir_name))
-            if calibration_group == GROUP.TEXTURE20 or calibration_group == GROUP.TEXTURE30:
-                save_dirs.pop(0)  # only save to RB directory to limit number files saved
-        for save_dir in save_dirs:
-            if not path.exists(save_dir):
-                makedirs(save_dir)
-            SaveNexus(InputWorkspace=ws, Filename=path.join(save_dir, ws + ".nxs"))
-
-    def read_attenuation_coefficient_at_value(self, ws, val, unit):
+    @staticmethod
+    def read_attenuation_coefficient_at_value(ws: str, val: float, unit: str) -> Sequence[float]:
         conv_ws = ConvertUnits(ADS.retrieve(ws), Target=unit)
         coefs = []
         xbins = conv_ws.readX(0)
@@ -231,13 +287,23 @@ class TextureCorrectionModel:
         ADS.remove("conv_ws")
         return coefs
 
-    def get_atten_table_name(self, ws_str, eval_val, unit):
+    @staticmethod
+    def get_atten_table_name(ws_str: str, eval_val: float, unit: str) -> str:
         ws = ADS.retrieve(ws_str)
         run_num = str(ws.getRun().getProperty("run_number").value)
         instr = ws.getInstrument().getName()
         return f"{instr}_{run_num}_attenuation_coefficient_{eval_val}_{unit}"
 
-    def write_atten_val_table(self, ws, vals, eval_val, unit, rb_num, calibration, root_dir):
+    def write_atten_val_table(
+        self,
+        ws: str,
+        vals: Sequence[float],
+        eval_val: float,
+        unit: str,
+        rb_num: Optional[str],
+        calibration: Optional[CalibrationInfo],
+        root_dir: str,
+    ) -> None:
         out_ws = self.get_atten_table_name(ws, eval_val, unit)
         table = CreateEmptyTableWorkspace(OutputWorkspace=out_ws)
         table.addColumn("float", "mu")
@@ -250,18 +316,20 @@ class TextureCorrectionModel:
         group = calibration.group if calibration else None
         self._save_corrected_files(out_ws, root_dir, "AttenuationTables", rb_num, group)
 
-    def create_reference_ws(self, rb_num, instr="ENGINX"):
+    # ~~~~~ Reference Workspace Functions ~~~~~~~~~~~~~
+
+    def create_reference_ws(self, rb_num: str, instr: str = "ENGINX") -> None:
         self.set_reference_ws(f"{rb_num}_reference_workspace")
         LoadEmptyInstrument(InstrumentName=instr, OutputWorkspace=self.reference_ws)
 
-    def save_reference_file(self, rb_num, group, root_dir):
+    def save_reference_file(self, rb_num: str, group: Optional[GROUP], root_dir: Optional[str]) -> None:
         if self.reference_ws and ADS.doesExist(self.reference_ws):
             self._save_corrected_files(self.reference_ws, root_dir, "ReferenceWorkspaces", rb_num, group)
 
-    def set_reference_ws(self, ws_name):
+    def set_reference_ws(self, ws_name: str) -> None:
         self.reference_ws = ws_name
 
-    def get_reference_info(self):
+    def get_reference_info(self) -> Tuple[str, bool, str]:
         material = "Not set"
         shape_enabled = False
         if self.reference_ws:
@@ -269,12 +337,18 @@ class TextureCorrectionModel:
             shape_enabled = not self._has_no_valid_shape(self.reference_ws)
         return self.reference_ws, shape_enabled, material
 
-    def plot_gauge_vol(self, preset, custom_shape, fig):
+    # ~~~~~ Plotting Functions ~~~~~~~~~~~~~
+
+    def plot_gauge_vol(self, preset: str, custom_shape: Optional[str], fig: Figure) -> None:
         gauge_vol_str = self.get_gauge_vol_str(preset, custom_shape)
         plot_gauge_vol(gauge_vol_str, fig)
 
-    def plot_sample_directions(self, fig, ws_name, ax_transform, ax_labels, fix_axes_to_sample):
+    @staticmethod
+    def plot_sample_directions(
+        fig: Figure, ws_name: str, ax_transform: np.ndarray, ax_labels: Sequence[str], fix_axes_to_sample: bool
+    ) -> None:
         plot_sample_directions(fig, ws_name, ax_transform, ax_labels, fix_axes_to_sample)
 
-    def _has_no_valid_shape(self, ws_name):
-        has_no_valid_shape(ws_name)
+    @staticmethod
+    def _has_no_valid_shape(ws_name: str) -> bool:
+        return has_no_valid_shape(ws_name)
