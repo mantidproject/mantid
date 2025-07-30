393051df3eb7bf2bfbb570a6c7ba7260ab7ff2cd
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-07-30 15:30:25 +0100

    add repeat test

--- Testing/SystemTests/tests/framework/SampleWithShapeAndMaterialSaveNexusTest.py
+++ Testing/SystemTests/tests/framework/SampleWithShapeAndMaterialSaveNexusTest.py
@@ -1,3 +1,9 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
 import subprocess
 import sys
 import tempfile
@@ -17,21 +23,25 @@ class SetupMixin:
         self.fname_literal = json.dumps(self.fname)
         self.save_path_literal = json.dumps(self.save_fpath)
 
-    def execTest(self, test_script):
+    def execTest(self, test_script, runs=1):
         script_path = os.path.join(tempfile.gettempdir(), "subtest_script.py")
         with open(script_path, "w") as f:
             f.write(test_script)
 
-        result = subprocess.run(
-            [sys.executable, script_path],
-            capture_output=True,
-            text=True,
-        )
+        run_num = 0
+        self.result_code = 0
 
-        print("Subprocess STDOUT:\n", result.stdout)
-        print("Subprocess STDERR:\n", result.stderr)
+        while run_num < runs:
+            result = subprocess.run(
+                [sys.executable, script_path],
+                capture_output=True,
+                text=True,
+            )
 
-        self.result_code = result.returncode
+            print("Subprocess STDOUT:\n", result.stdout)
+            print("Subprocess STDERR:\n", result.stderr)
+            self.result_code = result.returncode
+            run_num += 1
 
     def validate(self):
         self.assertEqual(self.result_code, 0, "Subprocess crashed or failed assertion")
@@ -89,3 +99,32 @@ SaveNexus(Filename=save_fpath, InputWorkspace=fname)
 assert os.path.exists(save_fpath), "File was not saved."
 '''
         self.execTest(test_script)
+
+
+class TestRepeatedScriptMemoryAllocation(SetupMixin, MantidSystemTest):
+    def runTest(self):
+        test_script = r"""
+from mantid.simpleapi import *
+
+cuboid = " \
+<cuboid id='some-cuboid'> \
+<height val='2.0'  /> \
+<width val='2.0' />  \
+<depth  val='0.2' />  \
+<centre x='10.0' y='10.0' z='10.0'  />  \
+</cuboid>  \
+<algebra val='some-cuboid' /> \
+"
+
+ws = CreateSampleWorkspace()
+SetSample(ws, Geometry={'Shape': 'CSG', 'Value': cuboid})
+SetSampleMaterial(ws, "Fe")
+
+ws2 = CreateSampleWorkspace()
+SetSampleMaterial(ws2, "Fe")
+ws2.sample().setShape(ws.sample().getShape())
+    """
+        self.execTest(test_script, 2)
+
+    def validate(self):
+        self.assertEqual(self.result_code, 0, "Subprocess crashed or failed assertion")
