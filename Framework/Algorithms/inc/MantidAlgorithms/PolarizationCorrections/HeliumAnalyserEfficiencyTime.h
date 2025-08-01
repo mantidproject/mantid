ef03a82a9ca9df97d6fafeb7f624acb9bb884103
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2025-08-01 07:17:46 +0100

    Add new algorithm HeliumAnalyserEfficiencyTime

---
+++ Framework/Algorithms/inc/MantidAlgorithms/PolarizationCorrections/HeliumAnalyserEfficiencyTime.h
@@ -0,0 +1,55 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+
+#pragma once
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/WorkspaceFactory.h"
+#include "MantidAPI/WorkspaceGroup.h"
+#include "MantidAlgorithms/DllConfig.h"
+#include "MantidKernel/cow_ptr.h"
+
+namespace Mantid {
+namespace Algorithms {
+using namespace API;
+/*
+Docs
+*/
+class MANTID_ALGORITHMS_DLL HeliumAnalyserEfficiencyTime final : public API::Algorithm {
+public:
+  /// Algorithm's name for identification overriding a virtual method
+  const std::string name() const override { return "HeliumAnalyserEfficiencyTime"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Calculates the efficiency at the time of the run the using the lifetime, initial polarization and mean gas "
+           "length of the Helium analyzer.";
+  }
+  /// Algorithm's version for identification overriding a virtual method
+  int version() const override { return 1; }
+  /// Algorithm's category for identification overriding a virtual method
+  const std::string category() const override { return "SANS\\PolarizationCorrections"; }
+  /// Related algorithms
+  const std::vector<std::string> seeAlso() const override {
+    return {"HeliumAnalyserEfficiency", "DepolarizedAnalyserTransmission"};
+  };
+  /// Check that input params are valid
+  std::map<std::string, std::string> validateInputs() override;
+  bool checkGroups() override { return false; }
+
+private:
+  // Implement abstract Algorithm methods
+  void init() override;
+  void exec() override;
+  std::pair<double, double> getTimeDifference();
+  MatrixWorkspace_sptr calculateEfficiency();
+  MatrixWorkspace_sptr retrieveWorkspaceForWavelength() const;
+};
+} // namespace Algorithms
+} // namespace Mantid
