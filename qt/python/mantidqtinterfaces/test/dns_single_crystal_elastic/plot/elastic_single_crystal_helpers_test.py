6986d250dbf4353e145a4da1ca9ddb106b7acd96
Oleksandr Koshchii <o.koshchii@fz-juelich.de>
2025-08-07 15:35:14 +0200

    Upload legacy tests

---
+++ qt/python/mantidqtinterfaces/test/dns_single_crystal_elastic/plot/elastic_single_crystal_helpers_test.py
@@ -0,0 +1,12 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2024 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+import unittest
+
+
+if __name__ == "__main__":
+    unittest.main()
