def6a0d8bad373ac6a2bcd74d47713187748cf30
andy-bridger <71634246+andy-bridger@users.noreply.github.com>
2025-08-07 14:21:00 +0100

    Fix bug where mantid crashes after sample shape and material are set (#39757)
    This was super tricky for me, I think the issue is caused by a segfault
    when the owners of previously copied material go out of scope. I
    encountered it trying to set both the shape and material on a
    EmptyInstrumentWorkspace and couldn't get round it in the script so had
    a go at fixing it.
    
    I think the root of the issue was the shared pointer count not being
    correctly incremented when passing from cpp to python, so python garbage
    collection could end up clearing the shape while it is still referenced
    elsewhere.
    
    I think that this was perhaps caused by the exposure to python returning
    a reference to the the derived class (eg. in L66 shown below `getShape`
    method will give a `CSGObject` which is then returned by reference) and
    boost not correctly identifying the link between this and the underlying
    shared pointer managing the memory lifetime ? So I think the implicit
    cast to an `IObject_sptr` and returning of this, by value, from the
    `getShape` in the Sample Python Interface is the key fix.
    
    
    https://github.com/mantidproject/mantid/blob/b900fe695a75ebe177319233906a7ba1179bf436/Framework/PythonInterface/mantid/api/src/Exports/Sample.cpp#L66
    
    Actually only a few lines of code changed, mostly attempts at covering
    the issue with some system tests in the unlikely event a regression
    happens. There are also a few lines that aren't strictly necessary and
    are hang over from other attempts at fixing, which seem like they are
    still minor improvements so I have left them in.
    
    ### To test:
    At some points I had crashes from the following scripts (I've tried
    running them on idaas to compare behaviour):
    
    This one fairly consistently crashes when running:
    ```
    # import mantid algorithms, numpy and matplotlib
    from mantid.simpleapi import *
    import matplotlib.pyplot as plt
    import numpy as np
    
    ws = CreateSampleWorkspace()
    SetSampleMaterial(ws, "Fe")
    shape = ws.sample().getShape()
    ws.sample().setShape(shape)
    
    file_path = r"C:\Users\kcd17618\Documents\MantidScripts\set sample shape bug\save\save.nxs"
    
    SaveNexus(Filename = file_path, InputWorkspace = ws)
    
    print(ws.sample().getShape())
    ```
    
    this one can crash after repeatedly running the script:
    
    ```
    # import mantid algorithms, numpy and matplotlib
    from mantid.simpleapi import *
    import matplotlib.pyplot as plt
    import numpy as np
    
    cuboid = " \
    <cuboid id='some-cuboid'> \
    <height val='2.0'  /> \
    <width val='2.0' />  \
    <depth  val='0.2' />  \
    <centre x='10.0' y='10.0' z='10.0'  />  \
    </cuboid>  \
    <algebra val='some-cuboid' /> \
    "
    
    ws = CreateSampleWorkspace()
    SetSample(ws, Geometry={'Shape': 'CSG', 'Value': cuboid})
    SetSampleMaterial(ws, "Fe")
    
    ws2 = CreateSampleWorkspace()
    SetSampleMaterial(ws2, "Fe")
    ws2.sample().setShape(ws.sample().getShape())
    ```

---
+++ Testing/SystemTests/tests/framework/SampleWithShapeAndMaterialSaveNexusTest.py
@@ -0,0 +1,151 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+import subprocess
+import sys
+import tempfile
+import os
+import json
+from systemtesting import MantidSystemTest
+from mantid.simpleapi import Load
+
+
+class SetupMixin(object):
+    def setUp(self):
+        temp_dir = tempfile.gettempdir()
+        self.fname = "test_file.nxs"
+        self.save_fpath = os.path.join(temp_dir, self.fname)
+
+        self.temp_dir_literal = json.dumps(temp_dir)
+        self.fname_literal = json.dumps(self.fname)
+        self.save_path_literal = json.dumps(self.save_fpath)
+
+    def execTest(self, test_script):
+        script_path = os.path.join(tempfile.gettempdir(), "subtest_script.py")
+        with open(script_path, "w") as f:
+            f.write(test_script)
+
+        result = subprocess.run(
+            [sys.executable, script_path],
+            capture_output=True,
+            text=True,
+        )
+
+        print("Subprocess STDOUT:\n", result.stdout)
+        print("Subprocess STDERR:\n", result.stderr)
+        self.result_code = result.returncode
+
+    def validate(self):
+        self.assertEqual(self.result_code, 0, "Subprocess crashed or failed assertion")
+        self.assertTrue(os.path.exists(self.save_fpath), "Expected output file not created")
+        try:
+            Load(Filename=self.save_fpath, OutputWorkspace="loaded_ws")
+        except Exception as e:
+            self.fail(f"Failed to load saved file: {e}")
+        return True
+
+
+class TestSetMaterialThenShapeThenSave(SetupMixin, MantidSystemTest):
+    def runTest(self):
+        test_script = f"""
+from mantid.simpleapi import *
+import os
+
+TEMP_DIR = {self.temp_dir_literal}
+fname = {self.fname_literal}
+save_fpath = {self.save_path_literal}
+
+ws = CreateSampleWorkspace(OutputWorkspace=fname)
+SetSampleMaterial(ws, "Fe")
+ws.sample().setShape(ws.sample().getShape())
+
+SaveNexus(Filename=save_fpath, InputWorkspace=fname)
+assert os.path.exists(save_fpath), "File was not saved."
+"""
+        self.execTest(test_script)
+
+
+class TestSetShapeThenMaterialThenSave(SetupMixin, MantidSystemTest):
+    def runTest(self):
+        test_script = rf'''
+from mantid.simpleapi import *
+import os
+
+TEMP_DIR = {self.temp_dir_literal}
+fname = {self.fname_literal}
+save_fpath = {self.save_path_literal}
+
+ref_ws = LoadEmptyInstrument(InstrumentName="ENGINX", OutputWorkspace=fname)
+shape_info = """<cuboid id='cube'>     \
+<height val='0.02'  />     \
+<width val='0.02' />      \
+<depth  val='0.02' />      \
+<centre x='0.0' y='0.0' z='0.0'  />      \
+</cuboid>      \
+<algebra val='cube' /> \ """
+
+SetSampleShape(ref_ws, shape_info)
+SetSampleMaterial(ref_ws, "Fe")
+
+SaveNexus(Filename=save_fpath, InputWorkspace=fname)
+assert os.path.exists(save_fpath), "File was not saved."
+'''
+        self.execTest(test_script)
+
+
+class TestRepeatedExecutionSameProcess(SetupMixin, MantidSystemTest):
+    def runTest(self):
+        script_content = r"""
+from mantid.simpleapi import *
+
+cuboid = " \
+<cuboid id='some-cuboid'> \
+<height val='2.0'  /> \
+<width val='2.0' />  \
+<depth  val='0.2' />  \
+<centre x='10.0' y='10.0' z='10.0'  />  \
+</cuboid>  \
+<algebra val='some-cuboid' /> \
+"
+
+ws1 = CreateSampleWorkspace()
+SetSample(ws1, Geometry={'Shape': 'CSG', 'Value': cuboid})
+SetSampleMaterial(ws1, "Fe")
+
+ws2 = CreateSampleWorkspace()
+SetSampleMaterial(ws2, "Fe")
+shape = ws1.sample().getShape()
+ws2.sample().setShape(shape)
+"""
+        # write script to a temp file
+        temp_dir = tempfile.gettempdir()
+        test_script_path = os.path.join(temp_dir, "mantid_shape_test_script.py")
+        with open(test_script_path, "w") as f:
+            f.write(script_content)
+
+        # write wrapper script to execute initial script twice
+        wrapper_script_path = os.path.join(temp_dir, "wrapper_exec_twice.py")
+        with open(wrapper_script_path, "w") as f:
+            f.write(f'''
+exec(open(r"{test_script_path}").read())
+exec(open(r"{test_script_path}").read())
+''')
+
+        # run in subprocess
+        result = subprocess.run(
+            [sys.executable, wrapper_script_path],
+            capture_output=True,
+            text=True,
+        )
+
+        print("Wrapper STDOUT:\n", result.stdout)
+        print("Wrapper STDERR:\n", result.stderr)
+
+        self.result_code = result.returncode
+
+    def validate(self):
+        self.assertEqual(self.result_code, 0, "Script crashed on repeated execution in same process")
+        return True
