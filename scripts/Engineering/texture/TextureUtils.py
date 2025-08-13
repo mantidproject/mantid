24c5f38049ec46883364347db3f171a6f1b708b7
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-08-13 14:28:47 +0100

    update fitting

--- scripts/Engineering/texture/TextureUtils.py
+++ scripts/Engineering/texture/TextureUtils.py
@@ -17,7 +17,7 @@ from mantid.api import AnalysisDataService as ADS, MultiDomainFunction, Function
 from typing import Optional, Sequence, Union, Tuple
 from mantid.dataobjects import Workspace2D
 from mantid.fitfunctions import FunctionWrapper, CompositeFunctionWrapper
-from plugins.algorithms.IntegratePeaks1DProfile import PeakFunctionGenerator, calc_intens_and_sigma_arrays
+from plugins.algorithms.IntegratePeaks1DProfile import PeakFunctionGenerator, calc_sigma_from_summation
 # -------- Utility --------------------------------
 
 
@@ -400,6 +400,35 @@ def replace_nans(vals, method: str):
     return new_vals.T
 
 
+def get_eval_ws(out_ws_name, idom, ndom):
+    if ndom > 1:
+        return ADS.retrieve(f"{out_ws_name[:-1]}_{idom}")
+    else:
+        return ADS.retrieve(out_ws_name)
+
+
+def calc_intens_and_sigma_arrays(fit_result, error_strategy):
+    function = fit_result["Function"]
+    ndom = function.nDomains()
+    intens = np.zeros(ndom)
+    sigma = np.zeros(intens.shape)
+    intens_over_sig = np.zeros(intens.shape)
+    peak_func = FunctionFactory.Instance().createPeakFunction(function[0][0].name())
+    peak_limits = np.full(intens.shape, None)
+    for idom, comp_func in enumerate(function):
+        [peak_func.setParameter(iparam, comp_func.getParameterValue(iparam)) for iparam in range(peak_func.nParams())]
+        intens[idom] = peak_func.intensity()
+        if error_strategy == "Hessian":
+            [peak_func.setError(iparam, comp_func.getError(iparam)) for iparam in range(peak_func.nParams())]
+            sigma[idom] = peak_func.intensityError()
+        else:
+            ws_fit = get_eval_ws(fit_result["OutputWorkspace"], idom, ndom)
+            sigma[idom], peak_limits[idom] = calc_sigma_from_summation(ws_fit.readX(0), ws_fit.readE(0) ** 2, ws_fit.readY(0))
+    ivalid = ~np.isclose(sigma, 0)
+    intens_over_sig[ivalid] = intens[ivalid] / sigma[ivalid]
+    return intens, sigma, intens_over_sig, peak_limits
+
+
 def fit_all_peaks(
     wss: Sequence[str],
     peaks: Sequence[float],
@@ -412,6 +441,7 @@ def fit_all_peaks(
     no_fit_value_dict: Optional[dict] = None,
 ) -> None:
     """
+
     Fit all the peaks given in all the spectra of all the workspaces, for use in a texture analysis workflow
 
     wss: Workspace names of all the workspaces to fit
@@ -430,6 +460,7 @@ def fit_all_peaks(
         ws = ADS.retrieve(wsname)
         run, prefix = _get_run_and_prefix_from_ws_log(ws, wsname)
         grouping = _get_grouping_from_ws_log(ws)
+
         for peak in peaks:
             # change peak window to fraction
             out_ws = f"{prefix}{run}_{peak}_{grouping}_Fit_Parameters"
@@ -453,14 +484,14 @@ def fit_all_peaks(
                 Function=initial_function,
                 CostFunction="Least squares",
                 Output="first_fit",
-                MaxIterations=10,  # if it hasn't fit in 10 it is likely because the texture has the peak missing
+                MaxIterations=50,  # if it hasn't fit in 10 it is likely because the texture has the peak missing
                 **md_fit_kwargs,
             )
 
             fit_result = {"Function": fit_object.Function.function, "OutputWorkspace": fit_object.OutputWorkspace.name()}
 
             # update peak mask based on I/sig from fit
-            *_, i_over_sigma, _ = calc_intens_and_sigma_arrays(fit_result, "Hessian")
+            i, sig, i_over_sigma, _ = calc_intens_and_sigma_arrays(fit_result, "Summation")
             fit_mask = i_over_sigma > i_over_sigma_thresh
 
             # fit only peak pixels and let peak centers vary independently of DIFC ratio
@@ -494,20 +525,19 @@ def fit_all_peaks(
 
             table_vals = np.zeros((si.size(), 2 * len(u_params) + 1))  # intensity_est + param_1_val, param_1_err, +...
             for ispec in range(si.size()):
-                row = [intensity_estimates[ispec]]
                 if fit_mask[ispec]:
+                    row = [intensity_estimates[ispec]]
                     for p in u_params:
                         param_name = f"f{ispec}.f0.{p}"
                         pind = all_params.index(param_name)
                         row += [param_vals[pind], param_errs[pind]]
                 else:
+                    row = [default_vals.get("I_est", np.nan)]
                     for p in u_params:
                         row += [default_vals[p], np.nan]
                 table_vals[ispec] = row
-
             if nan_replacement:
                 table_vals = replace_nans(table_vals, nan_replacement)
-
             for i, row in enumerate(table_vals):
                 out_tab.addRow([i] + list(row))
             SaveNexus(InputWorkspace=out_ws, Filename=out_path)
