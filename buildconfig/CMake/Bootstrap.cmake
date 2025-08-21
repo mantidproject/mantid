c5408acbb25045c8856ab82b2baa1ee1fc58e035
Pete Peterson <petersonpf@ornl.gov>
2025-08-21 14:06:09 -0400

    Update minimum python version to match the rest of the project

--- buildconfig/CMake/Bootstrap.cmake
+++ buildconfig/CMake/Bootstrap.cmake
@@ -40,7 +40,7 @@ function(unset_cached_Python_variables)
 endfunction()
 
 # Find python interpreter
-set(MINIMUM_PYTHON_VERSION 3.6)
+set(MINIMUM_PYTHON_VERSION 3.11)
 # If we are not building the mantid framework we don't need the numpy developer env
 if(MANTID_FRAMEWORK_LIB STREQUAL "BUILD")
   find_package(Python ${MINIMUM_PYTHON_VERSION} REQUIRED COMPONENTS Interpreter Development NumPy)
