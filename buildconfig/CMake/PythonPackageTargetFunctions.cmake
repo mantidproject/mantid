b5368da7244895bc7c8d0ae93a1fa5d8e6f9b6f5
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-09-01 14:22:13 +0100

    remove redundant depends

--- buildconfig/CMake/PythonPackageTargetFunctions.cmake
+++ buildconfig/CMake/PythonPackageTargetFunctions.cmake
@@ -77,7 +77,7 @@ function(add_python_package pkg_name)
     set(_pkg_depends ${_stamp})
   endif()
 
-  add_custom_target(${pkg_name} ALL DEPENDS ${_pkg_depends} ${_egg_link_dir}/sitecustomize.py)
+  add_custom_target(${pkg_name} ALL DEPENDS ${_pkg_depends})
 
   # When running the install target, run the following code instead that defers to the `pip install` command. It assumes
   # the `${CMAKE_CURRENT_SOURCE_DIR}`, the directory where `add_python_package` was called from, contains either a
