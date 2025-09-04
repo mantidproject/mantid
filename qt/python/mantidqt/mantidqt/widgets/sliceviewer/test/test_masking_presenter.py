543039194f358b2d2f502e7fad503ad18d711fb9
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-09-04 14:17:57 +0100

    finish main presenter tests

--- qt/python/mantidqt/mantidqt/widgets/sliceviewer/test/test_masking_presenter.py
+++ qt/python/mantidqt/mantidqt/widgets/sliceviewer/test/test_masking_presenter.py
@@ -32,10 +32,9 @@ class SliceViewerBasePresenterTest(unittest.TestCase):
             "active_selector": active_selector,
         }
 
-    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel")
+    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel", autospec=True)
     def test_new_selector_no_active_selector(self, mock_masking_model_fn):
-        mock_model = Mock()
-        mock_masking_model_fn.return_value = mock_model
+        mock_model = mock_masking_model_fn.return_value
         presenter, mocks = self._setup_presenter_test()
         with patch(
             "mantidqt.widgets.sliceviewer.presenters.masking.TOOL_ITEM_TEXT_TO_SELECTOR", {ToolItemText.RECT_MASKING: mocks["selector_cls"]}
@@ -45,10 +44,9 @@ class SliceViewerBasePresenterTest(unittest.TestCase):
             mocks["selector_cls"].called_once_with(mocks["dataview"], mock_model)
             mocks["selector_obj"][0].set_active.assert_called_once_with(True)
 
-    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel")
+    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel", autospec=True)
     def test_new_selector_active_selector_reset_no_mask_drawn(self, mock_masking_model_fn):
-        mock_model = Mock()
-        mock_masking_model_fn.return_value = mock_model
+        mock_model = mock_masking_model_fn.return_value
         presenter, mocks = self._setup_presenter_test(active_selector=Mock(mask_drawn=False))
         with patch(
             "mantidqt.widgets.sliceviewer.presenters.masking.TOOL_ITEM_TEXT_TO_SELECTOR", {ToolItemText.RECT_MASKING: mocks["selector_cls"]}
@@ -62,10 +60,9 @@ class SliceViewerBasePresenterTest(unittest.TestCase):
             mock_model.clear_active_mask.assert_called_once()
             self.assertEqual(presenter._selectors, [])
 
-    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel")
+    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel", autospec=True)
     def test_new_selector_active_selector_reset_mask_drawn(self, mock_masking_model_fn):
-        mock_model = Mock()
-        mock_masking_model_fn.return_value = mock_model
+        mock_model = mock_masking_model_fn.return_value
         presenter, mocks = self._setup_presenter_test(active_selector=Mock(mask_drawn=True))
         with patch(
             "mantidqt.widgets.sliceviewer.presenters.masking.TOOL_ITEM_TEXT_TO_SELECTOR", {ToolItemText.RECT_MASKING: mocks["selector_cls"]}
@@ -78,30 +75,51 @@ class SliceViewerBasePresenterTest(unittest.TestCase):
             mock_model.store_active_mask.assert_called_once()
             self.assertEqual([mocks["active_selector"]], presenter._selectors)
 
-    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel")
+    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel", autospec=True)
     def test_export_selectors(self, mock_masking_model_fn):
-        mock_model = Mock()
-        mock_masking_model_fn.return_value = mock_model
-        presenter, mocks = self._setup_presenter_test(active_selector=Mock(mask_drawn=True))
-        with (
-            patch(
-                "mantidqt.widgets.sliceviewer.presenters.masking.TOOL_ITEM_TEXT_TO_SELECTOR",
-                {ToolItemText.RECT_MASKING: mocks["selector_cls"]},
-            ),
-            patch.object(Masking, "_reset_active_selector", autospec=True, wraps=Masking._reset_active_selector) as reset_spy,
-        ):
-            presenter = Masking(dataview=mocks["dataview"], ws_name=Mock())
+        mock_model = mock_masking_model_fn.return_value
+        presenter, mocks = self._setup_presenter_test()
+        with patch.object(Masking, "_reset_active_selector", autospec=True, wraps=Masking._reset_active_selector) as reset_spy:
             presenter.export_selectors()
             reset_spy.assert_called_once()
+            mock_model.export_selectors.assert_called_once()
 
-    def test_apply_selectors(self):
-        pass
+    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel", autospec=True)
+    def test_apply_selectors(self, mock_masking_model_fn):
+        mock_model = mock_masking_model_fn.return_value
+        presenter, mocks = self._setup_presenter_test(active_selector=Mock(mask_drawn=True))
+        with patch.object(Masking, "_reset_active_selector", autospec=True, wraps=Masking._reset_active_selector) as reset_spy:
+            presenter.apply_selectors()
+            reset_spy.assert_called_once()
+            mock_model.apply_selectors.assert_called_once()
 
-    def test_clear_and_disconnect(self):
-        pass
+    def test_clear_and_disconnect_no_active_selector(self):
+        presenter, mocks = self._setup_presenter_test()
+        mock_selectors = [Mock() for _ in range(3)]
+        presenter._selectors = mock_selectors
+        self.assertIsNone(presenter._active_selector)
+        presenter.clear_and_disconnect()
+        for mock_selector in mock_selectors:
+            mock_selector.clear.assert_called_once()
 
-    def test_clear_model(self):
-        pass
+    def test_clear_and_disconnect_active_selector_with_mask(self):
+        presenter, mocks = self._setup_presenter_test(active_selector=Mock(mask_drawn=True))
+        mock_selectors = [Mock() for _ in range(3)]
+        presenter._selectors = mock_selectors
+        presenter.clear_and_disconnect()
+        mocks["active_selector"].set_active.assert_called_once_with(False)
+        mocks["active_selector"].disconnect.assert_called_once()
+        mocks["active_selector"].clear.assert_called_once()
+        for mock_selector in mock_selectors:
+            mock_selector.clear.assert_called_once()
+
+    @patch("mantidqt.widgets.sliceviewer.presenters.masking.MaskingModel", autospec=True)
+    def test_clear_model(self, mock_masking_model_fn):
+        mock_model = mock_masking_model_fn.return_value
+        presenter, mocks = self._setup_presenter_test()
+        presenter.clear_model()
+        mock_model.clear_active_mask.assert_called_once()
+        mock_model.clear_stored_masks.assert_called_once()
 
 
 if __name__ == "__main__":
