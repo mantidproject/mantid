dd9a685f06973191c8bf9314bf5168c96ee60a25
Victoria-Hawkins <vhawkins1731@gmail.com>
2025-09-10 16:54:35 +0100

    New algorithm to find UB given peak and scattering plane (#39833)
    ### Description of work
    Algorithm to calculate UB matrix from a defined scattering plane + 1
    provided peak. This is done by optimising the rotation around the
    vertical to map the calculated Qsample to the real Qsample. This
    algorithm is intended to be used in alignments of crystals in
    instruments such as MAPS
    
    Closes #39663. 
    
    ### To test:
    ``` python
    inst_ws = LoadEmptyInstrument(Filename="SXD_Definition.xml", OutputWorkspace="empty_SXD")
    peaks1 = CreatePeaksWorkspace(InstrumentWorkspace=inst_ws, NumberOfPeaks=0, OutputWorkspace="peaks1")
    SetUB(peaks1, u=[1, -0.83, 0], v=[0.8, 1, 0], a=5.4, b=5.4, c=5.4, alpha=90, beta=90, gamma=90)
    AddPeakHKL(peaks1, [2, 2, 0])
    ClearUB(peaks1)
    FindUBFromScatteringPlane(
        vector_1=[1, -1, 0], vector_2=[1, 1, 0], a=5.4, b=5.4, c=5.4, alpha=90, beta=90, gamma=90, PeaksWorkspace=peaks1)
    print(ws.sample().getOrientedLattice().getUB())
    ```
    -->
    
    <!-- REMEMBER:
    - Add labels, milestones, etc.
    - Ensure the base of this PR is correct (e.g. release-next or main)
    - Add release notes in separate file as per
    ([guidelines](https://developer.mantidproject.org/Standards/ReleaseNotesGuide.html)),
    or justify their absence:
    *This does not require release notes* because <fill in an explanation of
    why>
    -->
    
    <!--  GATEKEEPER: When squashing, remove the section from HERE...  -->
    ---
    
    ### Reviewer
    
    **Your comments will be used as part of the gatekeeper process.**
    Comment clearly on what you have checked and tested during your review.
    Provide an audit trail for any changes requested.
    
    As per the [review
    guidelines](http://developer.mantidproject.org/ReviewingAPullRequest.html):
    
    - Is the code of an acceptable quality? ([Code
    standards](http://developer.mantidproject.org/Standards/)/[GUI
    standards](http://developer.mantidproject.org/Standards/GUIStandards.html))
    - Has a thorough functional test been performed? Do the changes handle
    unexpected input/situations?
    - Are appropriately scoped unit and/or system tests provided?
    - Do the release notes conform to the
    [guidelines](https://developer.mantidproject.org/Standards/ReleaseNotesGuide.html)
    and describe the changes appropriately?
    - Has the relevant (user and developer) documentation been
    added/updated?
    
    ### Gatekeeper
    
    As per the [gatekeeping
    guidelines](https://developer.mantidproject.org/Gatekeeping.html):
    
    - Has a thorough first line review been conducted, including functional
    testing?
    - At a high-level, is the code quality sufficient?
    - Are the base, milestone and labels correct?
    <!--  GATEKEEPER: ...To HERE  -->
    
    ---------
    
    Co-authored-by: RichardWaiteSTFC <55979119+RichardWaiteSTFC@users.noreply.github.com>

---
+++ Framework/PythonInterface/plugins/algorithms/FindUBFromScatteringPlane.py
@@ -0,0 +1,132 @@
+# import mantid algorithms, numpy and matplotlib
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+from mantid.api import PythonAlgorithm, AlgorithmFactory
+from mantid.kernel import FloatArrayProperty, FloatArrayLengthValidator, Direction, FloatBoundedValidator
+from mantid.api import IPeaksWorkspaceProperty
+from mantid.kernel import V3D, logger
+from scipy.optimize import minimize
+
+
+# Scattering + 1 peak algorithm
+class FindUBFromScatteringPlane(PythonAlgorithm):
+    def category(self):
+        return "Crystal\\UBMatrix"
+
+    def PyInit(self):
+        array_length_3 = FloatArrayLengthValidator(3)
+        lower_lattice_parameter_limit = FloatBoundedValidator(lower=0.0)
+        angle_limit = FloatBoundedValidator(lower=0.0, upper=180)
+
+        # declaring intake variables to define the scattering plane,
+        self.declareProperty(
+            FloatArrayProperty(name="Vector1", direction=Direction.Input, validator=array_length_3),
+            doc="First vector in the scattering plane in reciprocal lattice units - i.e. H,K,L",
+        )
+        self.declareProperty(
+            FloatArrayProperty(name="Vector2", direction=Direction.Input, validator=array_length_3),
+            doc="Second vector in the scattering plane in reciprocal lattice units - i.e. H,K,L",
+        )
+
+        # declaring intake variables abc, alpha, beta, gamma, and the peak
+        self.declareProperty(
+            IPeaksWorkspaceProperty(name="PeaksWorkspace", defaultValue="", direction=Direction.Input),
+            doc="PeaksWorkspace with 1 peak in the scattering plane, if more than 1 peak is provided"
+            "the first peak in the Workspace is chosen",
+        )
+
+        self.declareProperty(
+            name="a", direction=Direction.Input, validator=lower_lattice_parameter_limit, defaultValue=1.0, doc="Lattice Parameter a"
+        )
+        self.declareProperty(
+            name="b", direction=Direction.Input, validator=lower_lattice_parameter_limit, defaultValue=1.0, doc="Lattice Parameter b"
+        )
+        self.declareProperty(
+            name="c", direction=Direction.Input, validator=lower_lattice_parameter_limit, defaultValue=1.0, doc="Lattice Parameter c"
+        )
+
+        self.declareProperty(
+            name="alpha", direction=Direction.Input, validator=angle_limit, defaultValue=90.0, doc="Lattice Parameter alpha"
+        )
+        self.declareProperty(name="beta", direction=Direction.Input, validator=angle_limit, defaultValue=90.0, doc="Lattice Parameter beta")
+        self.declareProperty(
+            name="gamma", direction=Direction.Input, validator=angle_limit, defaultValue=90.0, doc="Lattice Parameter gamma"
+        )
+
+    def validateInputs(self):
+        issues = dict()
+        vecs = []
+        for ivec in range(1, 3):
+            param_name = f"Vector{ivec}"
+            vecs.append(V3D(*self.getProperty(param_name).value))
+            if np.isclose(vecs[-1].norm(), 0):
+                issues[param_name] = "Vector cannot be 0"
+        if len(issues) == 0 and np.isclose(np.sin(vecs[0].angle(vecs[1])), 0, atol=np.radians(1e-3)):
+            issues[param_name] = "Vectors cannot be collinear"
+        return issues
+
+    def exec_child_alg(self, alg_name: str, **kwargs):
+        alg = self.createChildAlgorithm(alg_name, enableLogging=False)
+        alg.setAlwaysStoreInADS(False)
+        alg.initialize()
+        alg.setProperties(kwargs)
+        alg.execute()
+        out_props = tuple(alg.getProperty(prop).value for prop in alg.outputProperties())
+        return out_props[0] if len(out_props) == 1 else out_props
+
+    # Can change these to be 2 arrays of abc, and alpha,beta,gamma if that is preferred
+    def PyExec(self):
+        # getting lattice parameters
+        a = self.getProperty("a").value
+        b = self.getProperty("b").value
+        c = self.getProperty("c").value
+
+        alpha = self.getProperty("alpha").value
+        beta = self.getProperty("beta").value
+        gamma = self.getProperty("gamma").value
+
+        # getting vectors to define the plane
+        vector_1 = V3D(*self.getProperty("Vector1").value)
+        vector_2 = V3D(*self.getProperty("Vector2").value)
+
+        # Find a UB with the correct scattering plane (even if `u` and `v` passed to `SetUB` are not orthogonal)
+
+        peak_table = self.getProperty("PeaksWorkspace").value
+        self.exec_child_alg("SetUB", Workspace=peak_table, u=vector_1, v=vector_2, a=a, b=b, c=c, alpha=alpha, beta=beta, gamma=gamma)
+        lattice = peak_table.sample().getOrientedLattice()
+        ub_arb = lattice.getUB()
+
+        vertical_dir = peak_table.getInstrument().getReferenceFrame().vecPointingUp()
+        vertical_dir = vertical_dir * (1 / vertical_dir.norm())
+
+        peak_ip = peak_table.getPeak(0)
+        hkl_peak = np.array(peak_ip.getHKL())
+        qsample_calc = np.array(np.pi * 2 * (ub_arb @ hkl_peak))
+        qsample_calc = V3D(*qsample_calc)
+        qsample_obs = np.array(peak_ip.getQSampleFrame())
+        qsample_obs = V3D(*qsample_obs)
+
+        if not abs(abs((np.degrees(qsample_calc.angle(vertical_dir)))) - 90) < 5:
+            logger.debug(f"{np.degrees(qsample_calc.angle(vertical_dir))}")
+            logger.warning("given peak hkl does not lie in the plane")
+
+        def objective(theta):
+            axis = np.array(vertical_dir)
+            rotation_object = R.from_rotvec(theta * axis)
+            rotated_vector = rotation_object.apply(np.array(qsample_calc))
+            res = qsample_obs - rotated_vector
+            cost = (np.linalg.norm(res)) ** 2
+            return cost
+
+        result = minimize(objective, 0, bounds=[(-np.pi, np.pi)], method="Nelder-Mead")
+        theta_new = result.x[0]
+        logger.debug(f"Angle of rotation applied to ub_arb {np.degrees(theta_new)}")
+
+        rotation_object = R.from_rotvec(theta_new * vertical_dir)
+        rotation_matrix = rotation_object.as_matrix()
+        ub_final = rotation_matrix @ ub_arb
+
+        self.exec_child_alg("SetUB", Workspace=peak_table, UB=ub_final)
+
+
+AlgorithmFactory.subscribe(FindUBFromScatteringPlane)
