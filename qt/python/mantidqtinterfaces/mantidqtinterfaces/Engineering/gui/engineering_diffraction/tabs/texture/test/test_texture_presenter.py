2b83cd8b05a10bcc9eb77d99b2fc0934e19db885
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-09-11 15:24:47 +0100

    add/fix copyright

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/Engineering/gui/engineering_diffraction/tabs/texture/test/test_texture_presenter.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/Engineering/gui/engineering_diffraction/tabs/texture/test/test_texture_presenter.py
@@ -1,3 +1,10 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+#
 import unittest
 from unittest.mock import patch, MagicMock, call
 from mantidqtinterfaces.Engineering.gui.engineering_diffraction.tabs.texture.presenter import TexturePresenter
