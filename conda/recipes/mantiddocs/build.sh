de8632c92c70e0019e5832f770f7f72aff43607d
Mohammed Almakki <mohammed.almakki@stfc.ac.uk>
2025-09-12 11:10:54 +0100

    Create minimal setup for test build

--- conda/recipes/mantiddocs/build.sh
+++ conda/recipes/mantiddocs/build.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-parent_dir="$(dirname "$RECIPE_DIR")"
-bash "${parent_dir}"/archive_env_logs.sh "$BUILD_PREFIX" "$PREFIX" 'mantiddocs'
+mkdir tbuild
+cd tbuild
 
-mkdir -p build
-cd build
-
-# unset LD_PRELOAD as this causes cmake to segfault
-LD_PRELOAD="" \
-  cmake \
+cmake \
   ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH=$PREFIX \
@@ -19,11 +14,12 @@ LD_PRELOAD="" \
   -DMANTID_QT_LIB=SYSTEM \
   -DENABLE_WORKBENCH=OFF \
   -DENABLE_DOCS=ON \
-  -DDOCS_DOTDIAGRAMS=ON \
-  -DDOCS_SCREENSHOTS=ON \
-  -DDOCS_MATH_EXT=sphinx.ext.mathjax \
-  -DDOCS_PLOTDIRECTIVE=ON \
-  -DPACKAGE_DOCS=ON \
+  -DENABLE_QTASSISTANT=OFF \
+  -DDOCS_DOTDIAGRAMS=OFF \
+  -DDOCS_SCREENSHOTS=OFF \
+  -DDOCS_MATH_EXT=sphinx.ext.imgmath \
+  -DDOCS_PLOTDIRECTIVE=OFF \
+  -DPACKAGE_DOCS=OFF \
   -DENABLE_PRECOMMIT=OFF \
   -DCONDA_BUILD=True \
   -DUSE_PYTHON_DYNAMIC_LIB=OFF \
@@ -36,12 +32,3 @@ cmake --build .
 # Build the StandardTestData target.
 # This might not be strictly necessary if docs-qthelp target is not built due to DOCS_QTHELP=OFF
 cmake --build . --target StandardTestData
-
-# Configure the 'datasearch.directories' in the Mantid.properties file so the test data is found
-export STANDARD_TEST_DATA_DIR=$SRC_DIR/build/ExternalData/Testing/Data
-echo 'datasearch.directories = '$STANDARD_TEST_DATA_DIR'/UnitTest/;'$STANDARD_TEST_DATA_DIR'/DocTest/' >> $PREFIX/bin/Mantid.properties
-
-# Use QT_QPA_PLATFORM instead of Xvfb because Xvfb hides a lot of the useful output
-QT_QPA_PLATFORM=offscreen cmake --build . --target docs-html
-
-cmake --build . --target install
