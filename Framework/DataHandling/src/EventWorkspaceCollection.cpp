316af1f15b0bda1fd4bb579335f91b134266a593
Silke Schomann <74243814+SilkeSchomann@users.noreply.github.com>
2025-09-15 16:05:09 +0100

    Catch runtime errors when adding period logs (#39870)
    Inconsistencies sometimes occur in period-related logs within Event
    NeXus files, such as duplicated entries with the same timestamps.
    Therefore, these repeated entries are reduced to a single entry. This
    can lead to runtime errors when loading the file because it is not
    possible to determine the ending value from a TimeSeriesProperty with a
    single time entry. In the past, this has resulted in Mantid crashes. In
    order to prevent this, the related event workspace is now created
    without the period logs if issues are detected.

--- Framework/DataHandling/src/EventWorkspaceCollection.cpp
+++ Framework/DataHandling/src/EventWorkspaceCollection.cpp
@@ -13,6 +13,7 @@
 #include "MantidDataObjects/WorkspaceCreation.h"
 #include "MantidGeometry/Instrument.h"
 #include "MantidIndexing/IndexInfo.h"
+#include "MantidKernel/Logger.h"
 #include "MantidKernel/UnitFactory.h"
 
 #include <algorithm>
@@ -28,6 +29,9 @@ using namespace Mantid::Kernel;
 using namespace Mantid::API;
 
 namespace {
+
+// Logger for this class
+auto g_log = Kernel::Logger("EventWorkspaceCollection");
 
 /**
  * Copy all logData properties from the 'from' workspace to the 'to'
@@ -91,7 +95,16 @@ void EventWorkspaceCollection::setNPeriods(size_t nPeriods, std::unique_ptr<cons
     m_WsVec[i] = createEmptyEventWorkspace();
     m_WsVec[i]->copyExperimentInfoFrom(temp.get());
     if (addBoolTimeSeries) {
-      logCreator.addPeriodLogs(periodNumber, m_WsVec[i]->mutableRun());
+      try {
+        logCreator.addPeriodLogs(periodNumber, m_WsVec[i]->mutableRun());
+      } catch (const std::runtime_error &e) {
+        // The addition of period related logs may result in runtime errors, for instance, if the log
+        // filter contains two entries with identical times as duplicated entries will be removed:
+        // "Cannot guess ending value from a TimeSeriesProperty that contains only a single time"
+        // Under these circumstances, it is advisable to omit the period-related logs.
+
+        g_log.warning(e.what());
+      }
     }
     copyLogs(temp,
              m_WsVec[i]); // Copy all logs from dummy workspace to period workspaces.
