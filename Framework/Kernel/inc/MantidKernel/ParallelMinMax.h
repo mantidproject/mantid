681a7537eb2c5a86087713cecaaca43d748ba49b
rboston628 <bostonsr@ornl.gov>
2025-09-22 11:02:53 -0400

    fix the helper methods

--- Framework/Kernel/inc/MantidKernel/ParallelMinMax.h
+++ Framework/Kernel/inc/MantidKernel/ParallelMinMax.h
@@ -42,7 +42,7 @@ template <typename T> std::pair<T, T> parallel_minmax(std::vector<T> const *cons
  * @return a std::pair<T,T> with the min (first) and max (second)
  */
 template <typename T>
-std::pair<T, T> parallel_minmax(std::shared_ptr<std::vector<T> const> const vec, size_t const grainsize = 1000);
+std::pair<T, T> parallel_minmax(std::shared_ptr<std::vector<T>> const &vec, size_t const grainsize = 1000);
 
 /** parallel_minmax
  * @param vec -- a unique pointer to a vector of values of type T, to search for a min and max
@@ -50,6 +50,6 @@ std::pair<T, T> parallel_minmax(std::shared_ptr<std::vector<T> const> const vec,
  * @return a std::pair<T,T> with the min (first) and max (second)
  */
 template <typename T>
-std::pair<T, T> parallel_minmax(std::unique_ptr<std::vector<T> const> const vec, size_t const grainsize = 1000);
+std::pair<T, T> parallel_minmax(std::unique_ptr<std::vector<T>> const &vec, size_t const grainsize = 1000);
 
 } // namespace Mantid::Kernel
