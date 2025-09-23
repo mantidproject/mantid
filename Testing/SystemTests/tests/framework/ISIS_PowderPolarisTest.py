d637ba8bc3ed110965ab4a8234b497cb8cc3694e
Richard Waite <richard.waite@stfc.ac.uk>
2025-09-23 17:02:01 +0100

    Add test for r limits

--- Testing/SystemTests/tests/framework/ISIS_PowderPolarisTest.py
+++ Testing/SystemTests/tests/framework/ISIS_PowderPolarisTest.py
@@ -555,6 +555,25 @@ class TotalScatteringTestWavelengthLimits(systemtesting.MantidSystemTest):
         self.assertAlmostEqual(self.pdf_output.dataY(0)[idx], 4.10094, places=3)  # less than in TotalScatteringLorchFilterTest
 
 
+class TotalScatteringTestRLimits(systemtesting.MantidSystemTest):
+    pdf_output = None
+    r_lims = [None, 10]
+
+    def runTest(self):
+        setup_mantid_paths()
+        # Load Focused ws
+        mantid.LoadNexus(Filename=total_scattering_input_file, OutputWorkspace="98533-ResultTOF")
+        q_lims = np.array([2.5, 3, 4, 6, 7, 3.5, 5, 7, 11, 40]).reshape((2, 5))
+        self.pdf_output = run_total_scattering("98533", True, q_lims=q_lims, lorch_filter=False, r_lims=self.r_lims)
+
+    def validate(self):
+        # Whilst total scattering is in development, the validation will avoid using reference files as they will have
+        # to be updated very frequently. In the meantime, the expected peak in the PDF at ~3.85 Angstrom will be checked.
+        idx = get_bin_number_at_given_r(self.pdf_output.dataX(0), 3.85)
+        self.assertAlmostEqual(self.pdf_output.dataY(0)[idx], 8.7571, places=3)
+        self.assertAlmostEqual(self.pdf_output.dataX(0)[-1], self.r_lims[-1], delta=2e-2)
+
+
 def run_total_scattering(
     run_number,
     merge_banks,
@@ -567,6 +586,7 @@ def run_total_scattering(
     per_detector=False,
     pdf_output_name=None,
     wavelength_lims=None,
+    r_lims=None,
 ):
     pdf_inst_obj = setup_inst_object(mode="PDF")
     return pdf_inst_obj.create_total_scattering_pdf(
@@ -581,6 +601,7 @@ def run_total_scattering(
         per_detector_vanadium=per_detector,
         pdf_output_name=pdf_output_name,
         wavelength_lims=wavelength_lims,
+        r_lims=r_lims,
     )
 
 
