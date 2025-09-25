Add miniplot toolbar and correct units (#39978)
Does two things in the new Instrument View:

- Adds a toolbar to the 'miniplot' 
- Displays the correct units for the integration slider

Closes #39748. <!-- One line per closed issue. -->

Several functions need to be renamed because they call say 'TOF' when
the unit of the workspace could be anything, but this can be done in
maintenance in a separate PR.

<!-- If issue raised by user. Do not leak email addresses.
**Report to:** [user name]
-->

### To test:

Check correct unit label is displayed by opening workspaces in different
units in the new Instrument View.

Test out the new buttons on the line plot showing the selected spectra
(or their sum).

<!-- Include sufficient instructions for someone unfamiliar with the
application to test.
Ok to refer back to instructions in the issue.
-->

<!-- REMEMBER:
- Add labels, milestones, etc.
- Ensure the base of this PR is correct (e.g. release-next or main)
- Add release notes in separate file as per
([guidelines](https://developer.mantidproject.org/Standards/ReleaseNotesGuide.html)),
or justify their absence:
*This does not require release notes* because <fill in an explanation of
why>
-->

qt/python/instrumentview/instrumentview/FullInstrumentViewModel.py
@@ -23,12 +23,15 @@ class FullInstrumentViewModel:
     _data_max = 0.0
     line_plot_workspace = None
     _workspace_x_unit: str
+    _workspace_x_unit_display: str
 
     def __init__(self, workspace: Workspace2D):
         """For the given workspace, calculate detector positions, the map from detector indices to workspace indices, and integrated
         counts. Optionally will draw detector geometry, e.g. rectangular bank or tube instead of points."""
         self._workspace = workspace
-        self._workspace_x_unit = workspace.getAxis(0).getUnit().unitID()
+        x_unit = workspace.getAxis(0).getUnit()
+        self._workspace_x_unit = x_unit.unitID()
+        self._workspace_x_unit_display = f"{str(x_unit.caption())} ({str(x_unit.symbol())})"
 
     def setup(self):
         component_info = self._workspace.componentInfo()
@@ -59,14 +62,14 @@ class FullInstrumentViewModel:
 
         # Get min and max tof values
         if self._workspace.isRaggedWorkspace():
-            first_last = np.array([self._workspace.readX(i)[[0, -1]] for i in range(self._workspace.getNumberHistograms())])
+            first_last = np.array([self._workspace.readX(i)[[0, -1]] for i in self._workspace_indices[self._is_valid]])
             self._tof_limits = (np.min(first_last[:, 0]), np.max(first_last[:, 1]))
 
         elif self._workspace.isCommonBins():
             self._tof_limits = tuple(self._workspace.dataX(0)[[0, -1]])
 
         else:
-            data_x = self._workspace.extractX()
+            data_x = self._workspace.extractX()[self._is_valid]
             self._tof_limits = (np.min(data_x[:, 0]), np.max(data_x[:, -1]))
 
         # Update counts with default total range
@@ -83,6 +86,10 @@ class FullInstrumentViewModel:
     @property
     def workspace_x_unit(self) -> str:
         return self._workspace_x_unit
+
+    @property
+    def workspace_x_unit_display(self) -> str:
+        return self._workspace_x_unit_display
 
     @property
     def default_projection(self) -> str:

qt/python/instrumentview/instrumentview/FullInstrumentViewWindow.py
@@ -20,7 +20,7 @@ from qtpy.QtWidgets import (
     QTextEdit,
     QPushButton,
 )
-from qtpy.QtGui import QPalette, QIntValidator, QMovie
+from qtpy.QtGui import QPalette, QDoubleValidator, QMovie
 from qtpy.QtCore import Qt, QEvent, QSize
 from superqt import QDoubleRangeSlider
 from pyvistaqt import BackgroundPlotter
@@ -29,6 +29,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from instrumentview.Detectors import DetectorInfo
 from typing import Callable
 from mantid.dataobjects import Workspace2D
+from mantidqt.plotting.mantid_navigation_toolbar import MantidNavigationToolbar
 import numpy as np
 import pyvista as pv
 import os
@@ -75,9 +76,11 @@ class FullInstrumentViewWindow(QMainWindow):
         self._detector_spectrum_fig, self._detector_spectrum_axes = plt.subplots(subplot_kw={"projection": "mantid"})
         self._detector_figure_canvas = FigureCanvas(self._detector_spectrum_fig)
         self._detector_figure_canvas.setMinimumSize(QSize(0, 0))
+        plot_toolbar = MantidNavigationToolbar(self._detector_figure_canvas, self)
         plot_widget = QWidget()
         plot_layout = QVBoxLayout(plot_widget)
         plot_layout.addWidget(self._detector_figure_canvas)
+        plot_layout.addWidget(plot_toolbar)
 
         vsplitter = QSplitter(Qt.Vertical)
         vsplitter.addWidget(self.main_plotter.app_window)
@@ -180,13 +183,13 @@ class FullInstrumentViewWindow(QMainWindow):
         min_hbox = QHBoxLayout()
         min_hbox.addWidget(QLabel("Min"))
         min_edit = QLineEdit()
-        max_int_32 = np.iinfo(np.int32).max
-        min_edit.setValidator(QIntValidator(0, max_int_32, self))
+        max_float_64 = np.finfo(np.float64).max
+        min_edit.setValidator(QDoubleValidator(0, max_float_64, 4, self))
         min_hbox.addWidget(min_edit)
         max_hbox = QHBoxLayout()
         max_hbox.addWidget(QLabel("Max"))
         max_edit = QLineEdit()
-        max_edit.setValidator(QIntValidator(0, max_int_32, self))
+        max_edit.setValidator(QDoubleValidator(0, max_float_64, 4, self))
         max_hbox.addWidget(max_edit)
 
         slider = QDoubleRangeSlider(Qt.Orientation.Horizontal, parent=parent_box)
@@ -206,15 +209,18 @@ class FullInstrumentViewWindow(QMainWindow):
         return (min_edit, max_edit, slider)
 
     def _add_connections_to_edits_and_slider(self, min_edit: QLineEdit, max_edit: QLineEdit, slider, presenter_callback: Callable):
+        def format_float(value):
+            return f"{value:.4f}".rstrip("0").rstrip(".") if "." in f"{value:.4f}" else f"{value:.4f}"
+
         def set_edits(limits):
             min, max = limits
-            min_edit.setText(f"{min:.0f}")
-            max_edit.setText(f"{max:.0f}")
+            min_edit.setText(format_float(min))
+            max_edit.setText(format_float(max))
 
         def set_slider(callled_from_min):
             def wrapped():
                 try:
-                    min, max = int(float(min_edit.text())), int(float(max_edit.text()))
+                    min, max = float(min_edit.text()), float(max_edit.text())
                 except ValueError:
                     return
                 if callled_from_min:
@@ -252,6 +258,7 @@ class FullInstrumentViewWindow(QMainWindow):
         self._presenter = presenter
         for unit in self._presenter.available_unit_options():
             self._units_combo_box.addItem(unit)
+        self._time_of_flight_group_box.setTitle(self._presenter.workspace_display_unit)
 
     def setup_connections_to_presenter(self) -> None:
         self._projection_combo_box.currentIndexChanged.connect(self._presenter.on_projection_option_selected)