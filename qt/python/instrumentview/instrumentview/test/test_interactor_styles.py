cd7bce53404d0b5c1b1bb8bdc8d94b7ab402f7a2
Gui Pereira <guijose.pereira@gmail.com>
2025-10-03 10:57:19 +0100

    Simplify unit tests to avoid library issues

--- qt/python/instrumentview/instrumentview/test/test_interactor_styles.py
+++ qt/python/instrumentview/instrumentview/test/test_interactor_styles.py
@@ -1,14 +1,9 @@
 from instrumentview.InteractorStyles import CustomInteractorStyleRubberBand3D, CustomInteractorStyleZoomAndSelect, on_selection_changed
-from pyvistaqt import BackgroundPlotter
 import unittest
 from unittest import mock
 
 
 class TestInteractorStyles(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls._plotter = BackgroundPlotter()
-
     def _make_mock_caller(self, start_position, end_position, window_size):
         mock_caller = mock.MagicMock()
         mock_caller.GetStartPosition.return_value = start_position
@@ -26,17 +21,33 @@ class TestInteractorStyles(unittest.TestCase):
 
     @mock.patch("instrumentview.InteractorStyles.on_selection_changed")
     def test_custom_3d_selection_changed(self, callback_mock):
-        self._plotter.iren.style = CustomInteractorStyleRubberBand3D()
-        self._plotter.iren.interactor.GetInteractorStyle().InvokeEvent("SelectionChangedEvent")
+        style = CustomInteractorStyleRubberBand3D()
+        style.InvokeEvent("SelectionChangedEvent")
         callback_mock.assert_called_once()
 
     @mock.patch("instrumentview.InteractorStyles.on_selection_changed")
     def test_custom_2d_selection_changed(self, callback_mock):
         style = CustomInteractorStyleZoomAndSelect()
-        style.set_interactor(self._plotter.iren.interactor)
-        self._plotter.iren.style = style
         style.rubber_band.InvokeEvent("SelectionChangedEvent")
         callback_mock.assert_called_once()
+
+    def test_set_interacor(self):
+        style = CustomInteractorStyleZoomAndSelect()
+        style.SetInteractor = mock.MagicMock()
+        style.rubber_band = mock.MagicMock()
+        style.rubber_band.SetInteractor = mock.MagicMock()
+        style.set_interactor(0)
+        style.rubber_band.SetInteractor.assert_called_with(0)
+        style.SetInteractor.assert_called_with(0)
+
+    def test_remove_interacor(self):
+        style = CustomInteractorStyleZoomAndSelect()
+        style.SetInteractor = mock.MagicMock()
+        style.rubber_band = mock.MagicMock()
+        style.rubber_band.SetInteractor = mock.MagicMock()
+        style.remove_interactor()
+        style.rubber_band.SetInteractor.assert_called_with(None)
+        style.SetInteractor.assert_called_with(None)
 
     def test_on_selection_changed_good_coords(self):
         mock_caller, mock_picker = self._make_mock_caller((5, 10), (4, 11), (15, 15))
