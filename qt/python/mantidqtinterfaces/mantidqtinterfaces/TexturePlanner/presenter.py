1a8c3f27e764939af141b5752d4b9e39ec99e2d7
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-10-06 17:14:43 +0100

    further slot connecting

--- qt/python/mantidqtinterfaces/mantidqtinterfaces/TexturePlanner/presenter.py
+++ qt/python/mantidqtinterfaces/mantidqtinterfaces/TexturePlanner/presenter.py
@@ -19,7 +19,6 @@ class TexturePlannerPresenter(object):
         self.set_model_texture_directions()
         self.set_model_group()
         self.on_goniometer_updated(0)
-        self.update_plots()
 
         # connect slots
 
@@ -30,6 +29,8 @@ class TexturePlannerPresenter(object):
         self.view.set_on_num_gonio_updated(self.on_num_gonio_updated)
         self.view.set_on_step_updated(self.update_angle_steps)
         self.view.set_on_group_changed(self.on_group_changed)
+        self.view.set_on_add_orientation_clicked(self.add_orientation)
+        self.view.set_on_current_index_changed(self.on_index_changed)
 
     def set_view_texture_directions(self, names, vecs):
         self.view.set_rd_name(names[0])
@@ -71,19 +72,25 @@ class TexturePlannerPresenter(object):
     def get_angles(self):
         return self.model.get_angles(self.view.get_angles(), self.view.get_num_gonios())
 
-    def on_goniometer_updated(self, index, *args):
-        self.model.set_gonio_index(index)
-        self.model.get_gRs(self.get_vecs(), self.get_senses(), self.get_angles())
+    def on_goniometer_updated(self, goniometer_index, *args):
+        self.model.set_gonio_index(goniometer_index)
+        orientation_index = self.view.get_current_index()
+        self.model.update_gRs(self.get_vecs(), self.get_senses(), self.get_angles(), orientation_index)
+        self.model.update_projected_data(orientation_index)
+        self.model.update_gonio_string(self.get_vecs(), self.get_senses(), self.get_angles(), orientation_index)
         self.update_plots()
+        self.update_table()
 
     def on_num_gonio_updated(self):
         num_gonios = self.view.get_num_gonios()
         self.update_enabled_gonios(num_gonios)
         self.on_goniometer_updated(self.model.update_gonio_index(num_gonios))
+        self.view.hide_axis_columns()
 
     def on_directions_updated(self):
         self.set_model_texture_directions()
         self.model.get_detQ_lab()
+        self.model.update_all_projected_data()
         self.update_plots()
 
     def on_group_changed(self):
@@ -92,8 +99,46 @@ class TexturePlannerPresenter(object):
 
     def update_plots(self):
         self.model.update_plot(
-            self.get_vecs(), self.get_senses(), self.get_angles(), self.view.get_lab_figure(), self.view.get_lab_ax(), self.view.get_pf_ax()
+            self.get_vecs(),
+            self.get_senses(),
+            self.get_angles(),
+            self.view.get_lab_figure(),
+            self.view.get_lab_ax(),
+            self.view.get_pf_ax(),
+            self.view.get_current_index(),
         )
+
+    def update_table(self):
+        self.view.tableWidget.setRowCount(self.model.get_num_orientations())
+        for row_index, row_info in enumerate(self.model.get_table_info()):
+            self.view.add_table_row(row_index, *row_info)
+
+    def add_orientation(self):
+        # add an orientation to the dictionary
+        self.model.add_orientation()
+        new_index = self.model.get_num_orientations()
+
+        # update the orientation selector
+        self.update_orientation_selector(new_index)
+        # update the goniometer bookkeeping
+        self.on_goniometer_updated(new_index)
+
+    def update_orientation_selector(self, new_index):
+        self.view.spnIndex.setMaximum(self.model.get_num_orientations())
+        self.view.spnIndex.setValue(new_index)
 
     def setup_group_options(self):
         self.view.setup_group_options(self.model.supported_groups)
+
+    def on_index_changed(self):
+        # need to save the state of the old orientation index, so we rely on the value that exists on the model
+        self.model.update_gonio_string(self.get_vecs(), self.get_senses(), self.get_angles(), self.model.get_orientation_index())
+        # we then update this value to match the new current index, so it is ready for the next call of this function
+        updated_index = self.view.get_current_index()
+        self.model.set_orientation_index(updated_index)
+
+        # we now update the goniometer fields with the saved values
+        vecs, senses, angles = self.model.get_goniometer_values(updated_index)
+        self.view.set_vecs(vecs)
+        self.view.set_senses(senses)
+        self.view.set_angles(angles)
