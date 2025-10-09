e968e323c359eaf04aa3805bf3eb18648e8af827
James Clarke <james.clarke@stfc.ac.uk>
2025-10-09 13:30:08 +0100

    Make Peak a dataclass

--- qt/python/instrumentview/instrumentview/Peaks/Peak.py
+++ qt/python/instrumentview/instrumentview/Peaks/Peak.py
@@ -5,20 +5,21 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 import numpy as np
+from dataclasses import dataclass
 
 
 def _format_hkl(value):
     return f"{value:.2f}".rstrip("0").rstrip(".") if "." in f"{value:.2f}" else f"{value:.2f}"
 
 
+@dataclass
 class Peak:
-    def __init__(self, detector_id: int, location: np.ndarray, hkl: tuple[float, float, float], tof: float, dspacing: float) -> None:
-        self.location = location
-        self._hkl = hkl
-        self.detector_id = detector_id
-        self.tof = tof
-        self.dspacing = dspacing
+    detector_id: int
+    location: np.ndarray
+    hkl: tuple[float, float, float]
+    tof: float
+    dspacing: float
 
     @property
     def label(self) -> str:
-        return f"[{_format_hkl(self._hkl[0])}, {_format_hkl(self._hkl[1])}, {_format_hkl(self._hkl[2])}]"
+        return f"[{_format_hkl(self.hkl[0])}, {_format_hkl(self.hkl[1])}, {_format_hkl(self.hkl[2])}]"
