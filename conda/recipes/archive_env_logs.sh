6e636bd16d01f9407da95c7e6de95326447c6a0b
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-10-10 15:55:41 +0100

    add missing if closure

--- conda/recipes/archive_env_logs.sh
+++ conda/recipes/archive_env_logs.sh
@@ -20,7 +20,7 @@ fi
 #Just for first package (mantid), archive the base environment
 if [ "$package_name" == mantid ] ; then
   conda list --explicit base > $log_directory/base_environment.txt 2>&1 || echo "Failed to write base conda list output to file"
-
+fi
 
 conda list --explicit --prefix "$build_prefix" > $log_directory/"${package_name}"_build_environment.txt 2>&1
 conda list --explicit --prefix "$prefix" > $log_directory/"${package_name}"_host_environment.txt 2>&1
