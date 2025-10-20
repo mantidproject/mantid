40e297f26e2f8dc5e454aa758ef7a5064a44e8e0
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2025-10-20 16:54:23 +0100

    update naming conventions and type hinting

--- tools/ReleaseNotes/release_editor.py
+++ tools/ReleaseNotes/release_editor.py
@@ -10,88 +10,79 @@ import pathlib
 import subprocess
 import requests
 
+from typing import List, Tuple
+
 
 DIRECTIVE = ".. amalgamate:: "
 GIT_REPO = "mantidproject/mantid"
 
 
-def getReleaseRoot() -> pathlib.Path:
+def get_release_root() -> pathlib.Path:
     program_path = pathlib.Path(__file__).resolve()
     script_dir = program_path / "../../../docs/source/release/"
     return script_dir.resolve()
 
 
-def fixReleaseName(name):
+def fix_release_name(name: str) -> str:
     if not name.startswith("v"):
         name = "v" + name
     return name
 
 
-def createFileLocation(releaseNo) -> pathlib.Path:
-    return getReleaseRoot() / releaseNo
+def create_file_location(release_version: str) -> pathlib.Path:
+    return get_release_root() / release_version
 
 
-def updateFile(originalFile, replaceText, textToReplace):
+def update_file(file: pathlib.Path, release_note_text: str, amalgamate_line: str) -> None:
     # opens the upper level release note e.g. diffraction.rst
-    with open(originalFile, mode="r") as mainRST:
-        rstToRead = mainRST.read()
-        mainRST.close()
+    with open(file, mode="r") as f:
+        rst_text = f.read()
 
     # replaces the amalgamate directive with the notes compiled for that heading
-    newdata = rstToRead.replace(textToReplace, replaceText)
+    text_with_replacements = rst_text.replace(amalgamate_line, release_note_text)
 
-    with open(originalFile, mode="w") as mainRST:
-        mainRST.write(newdata)
-        mainRST.close()
-
-
-def createFileName(fileName, pathDirectory, includeStatement) -> pathlib.Path:
-    newFileName = fileName.replace(includeStatement, "")
-    newFileName = newFileName.replace("\n", "")
-    newFileName = pathDirectory / newFileName
-    return newFileName
+    with open(file, mode="w") as f:
+        f.write(text_with_replacements)
 
 
-def addReleaseNotesToMain(pathDirectory, git_token):
+def create_release_note_directory_path(amalgamate_line: str, amalgamate_directive: str, release_notes_root: pathlib.Path) -> pathlib.Path:
+    release_note_dir = amalgamate_line.replace(amalgamate_directive, "").strip()
+    return release_notes_root / release_note_dir
+
+
+def add_release_notes_to_main_pages(release_notes_root: pathlib.Path, git_token: str) -> None:
     # iterates through files in a directory
-    for file in pathDirectory.glob("*.rst"):
-        with open(file) as fileToEdit:
+    for file in release_notes_root.glob("*.rst"):
+        with open(file) as f:
             # iterate through each line in the upper level release note file e.g. diffraction.rst
-            for line in fileToEdit:
+            for line in f:
                 # finds the amalgamate directive to replace
                 if line.startswith(DIRECTIVE):
-                    fileName = createFileName(line, pathDirectory, DIRECTIVE)
-                    releaseNotes = collateNotes(fileName, git_token)
-                    originalFile = mainDirectory / file
-                    updateFile(originalFile, releaseNotes, line)
+                    release_note_sub_dir = create_release_note_directory_path(line, DIRECTIVE, release_notes_root)
+                    release_notes_text = collate_release_notes(release_note_sub_dir, git_token)
+                    update_file(release_notes_root / file, release_notes_text, line)
 
 
-def getReleaseNoteDirectories(path, directoryList):
-    # return nothing if path is a file
-    if os.path.isfile(path):
-        return ()
+def append_release_note_directories(path: pathlib.Path, directory_list: List[pathlib.Path]):
+    if not os.path.isdir(path):
+        return
 
     # add dir to directorylist if it contains .rst files
     if len([f for f in os.listdir(path) if f.endswith(".rst")]) > 0:
-        path = pathlib.Path(path)
-        directoryList.append(path)
+        directory_list.append(path)
 
     for d in os.listdir(path):
-        new_path = os.path.join(path, d)
-        if os.path.isdir(new_path):
-            getReleaseNoteDirectories(new_path, directoryList)
-
-    return directoryList
+        append_release_note_directories(path / d, directory_list)
 
 
-def checkContainsReleaseNote(path):
-    releaseNoteList = []
-    getReleaseNoteDirectories(path, releaseNoteList)
-    releaseNoteList.pop(0)
-    return releaseNoteList
+def gather_release_note_directories(path: pathlib.Path) -> List[pathlib.Path]:
+    release_note_directories = []
+    append_release_note_directories(path, release_note_directories)
+    release_note_directories.pop(0)
+    return release_note_directories
 
 
-def get_pr_number_and_link(release_note_path, git_token):
+def get_pr_number_and_link(release_note_path: pathlib.Path, git_token: str) -> Tuple[str, str]:
     commit_hash = get_file_creation_commit(release_note_path)
     headers = {"Authorization": f"token {git_token}", "Accept": "application/vnd.github.groot-preview+json"}
     url = f"https://api.github.com/repos/{GIT_REPO}/commits/{commit_hash}/pulls"
@@ -105,7 +96,7 @@ def get_pr_number_and_link(release_note_path, git_token):
     return "", ""
 
 
-def get_file_creation_commit(release_note_path):
+def get_file_creation_commit(release_note_path: pathlib.Path) -> str:
     result = subprocess.run(
         ["git", "log", "--diff-filter=A", "--pretty=format:%H", "--", release_note_path], capture_output=True, text=True
     )
@@ -114,30 +105,29 @@ def get_file_creation_commit(release_note_path):
 
 
 # This is the method for iterating through release notes in a folder and collating the notes into one object
-def collateNotes(path, git_token):
-    combinedRst = ""
-    for file in path.glob("*.rst"):
-        pr_number, pr_link = get_pr_number_and_link(file, git_token)
+def collate_release_notes(release_note_dir: pathlib.Path, git_token: str) -> str:
+    combined_notes = ""
+    for release_note_file in release_note_dir.glob("*.rst"):
+        pr_number, pr_link = get_pr_number_and_link(release_note_file, git_token)
         pr_text = ""
         if pr_number and pr_link:
             pr_text = f"(`#{pr_number} <{pr_link}>`_)"
-        with open(file) as f:
+        with open(release_note_file) as f:
             for line in f:
                 if line.startswith("- "):
                     line = f"- {pr_text} {line[2:]}"
-                combinedRst += line
+                combined_notes += line
 
-    return combinedRst
+    return combined_notes
 
 
 # Moves the release note files that have been copied to top level file into 'Used' folders
-def moveFiles(listOfDirectories):
-    for path in listOfDirectories:
-        usedDir = path / "Used"
-        usedDir.mkdir(parents=True, exist_ok=True)
+def move_files_to_used(release_note_directories: List[pathlib.Path]) -> None:
+    for path in release_note_directories:
+        used_dir = path / "Used"
+        used_dir.mkdir(parents=True, exist_ok=True)
         for file in path.glob("*.rst"):
-            used_folder = "Used"
-            file.rename(path / used_folder / file.name)
+            file.rename(used_dir / file.name)
 
 
 if __name__ == "__main__":
@@ -148,9 +138,8 @@ if __name__ == "__main__":
     parser.add_argument("--git_token", required=True)
     args = parser.parse_args()
 
-    args.release = fixReleaseName(args.release)
-    mainDirectory = createFileLocation(args.release)
-    directoriesUsed = checkContainsReleaseNote(mainDirectory)
+    release_notes_root_dir = create_file_location(fix_release_name(args.release))
+    directories_with_release_notes = gather_release_note_directories(release_notes_root_dir)
 
-    addReleaseNotesToMain(mainDirectory, args.git_token)
-    moveFiles(directoriesUsed)
+    add_release_notes_to_main_pages(release_notes_root_dir, args.git_token)
+    move_files_to_used(directories_with_release_notes)
