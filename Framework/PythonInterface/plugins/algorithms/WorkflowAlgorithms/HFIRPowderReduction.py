55c633f2ca9ab7161669cf1e9522eded4876748b
Daniel Caballero <132241327+dlcaballero16@users.noreply.github.com>
2025-10-22 12:43:23 -0400

    Initial commit for HFIRPowderReduction (#40196)
    Sibling PR to #40107

---
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/HFIRPowderReduction.py
@@ -0,0 +1,91 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+from mantid.api import DataProcessorAlgorithm, AlgorithmFactory, MultipleFileProperty, FileAction
+from mantid.kernel import Property, IntArrayProperty, StringListValidator
+
+
+class HFIRPowderReduction(DataProcessorAlgorithm):
+    def name(self):
+        return "HFIRPowderReduction"
+
+    def category(self):
+        return "DataHandling\\Nexus"
+
+    def summary(self):
+        return "Powder reduction for HFIR instruments"
+
+    def PyInit(self):
+        self.declareProperty(
+            MultipleFileProperty(name="SampleFilename", action=FileAction.OptionalLoad, extensions=[".nxs.h5"]), "Sample files to Load"
+        )
+        self.declareProperty("SampleIPTS", Property.EMPTY_INT, "Sample IPTS number to load from")
+        self.declareProperty(IntArrayProperty("SampleRunNumbers"), "Sample run numbers to load")
+
+        self.declareProperty(
+            MultipleFileProperty(name="VanadiumFilename", action=FileAction.OptionalLoad, extensions=[".nxs.h5"]), "Vanadium files to Load"
+        )
+        self.declareProperty("VanadiumIPTS", Property.EMPTY_INT, "Vanadium IPTS number to load from")
+        self.declareProperty(IntArrayProperty("VanadiumRunNumbers", []), "Vanadium run numbers to load")
+
+        self.declareProperty(
+            MultipleFileProperty(name="VanadiumBackgroundFilename", action=FileAction.OptionalLoad, extensions=[".nxs.h5"]),
+            "Vanadium background files to Load",
+        )
+        self.declareProperty("VanadiumBackgroundIPTS", Property.EMPTY_INT, "Vanadium background IPTS number to load from")
+        self.declareProperty(IntArrayProperty("VanadiumBackgroundRunNumbers", []), "Vanadium background run numbers to load")
+
+        self.declareProperty(
+            MultipleFileProperty(name="SampleBackgroundFilename", action=FileAction.OptionalLoad, extensions=[".nxs.h5"]),
+            "Sample background files to Load",
+        )
+        self.declareProperty("SampleBackgroundIPTS", Property.EMPTY_INT, "Sample background IPTS number to load from")
+        self.declareProperty(IntArrayProperty("SampleBackgroundRunNumbers", []), "Sample background run numbers to load")
+
+        self.declareProperty("ApplyMask", True, "If True standard masking will be applied to the workspace")
+        self.declareProperty("Grouping", "None", StringListValidator(["None", "2x2", "4x4"]), "Group pixels")
+
+    def validateInputs(self):
+        issues = dict()
+
+        fileNameBool = self.getProperty("SampleFilename").value
+        IPTSBool = self.getProperty("SampleIPTS").value
+        runNumbersBool = len(self.getProperty("SampleRunNumbers").value)
+        if IPTSBool == Property.EMPTY_INT and runNumbersBool == 0 and not fileNameBool:
+            issues["SampleFilename"] = "Missing required field: Must specify either SampleFilename or SampleIPTS AND SampleRunNumbers"
+            issues["SampleIPTS"] = "Missing required field: Must specify either SampleFilename or SampleIPTS AND SampleRunNumbers"
+            issues["SampleRunNumbers"] = "Missing required field: Must specify either SampleFilename or SampleIPTS AND SampleRunNumbers"
+
+        allFields = ["Sample", "Vanadium", "VanadiumBackground", "SampleBackground"]
+        for field in allFields:
+            fileNameBool = self.getProperty(f"{field}Filename").value
+            IPTSBool = self.getProperty(f"{field}IPTS").value
+            runNumbersBool = len(self.getProperty(f"{field}RunNumbers").value)
+            if (IPTSBool != Property.EMPTY_INT or runNumbersBool != 0) and fileNameBool:
+                issues[f"{field}Filename"] = (
+                    f"Too many fields filled: Must specify either {field}Filename or {field}IPTS AND {field}RunNumbers"
+                )
+                if IPTSBool != Property.EMPTY_INT:
+                    issues[f"{field}IPTS"] = (
+                        f"Too many fields filled: Must specify either {field}Filename or {field}IPTS AND {field}RunNumbers"
+                    )
+                if runNumbersBool:
+                    issues[f"{field}RunNumbers"] = (
+                        f"Too many fields filled: Must specify either {field}Filename or {field}IPTS AND {field}RunNumbers"
+                    )
+            elif not (IPTSBool == Property.EMPTY_INT and runNumbersBool == 0):
+                if IPTSBool != Property.EMPTY_INT:
+                    issues[f"{field}RunNumbers"] = f"{field}RunNumbers must be provided if {field}IPTS is provided"
+                if runNumbersBool:
+                    issues[f"{field}IPTS"] = f"{field}IPTS must be provided if {field}RunNumbers is provided"
+
+        return issues
+
+    def PyExec(self):
+        raise NotImplementedError("HFIRPowderReduction algorithm is not yet implemented.")
+
+
+AlgorithmFactory.subscribe(HFIRPowderReduction)
