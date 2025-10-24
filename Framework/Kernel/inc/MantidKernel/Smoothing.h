6ed9de6bdd8754c200fda79b878c2c00636c7ee6
rboston628 <bostonsr@ornl.gov>
2025-10-24 14:11:10 -0400

    fix sum-square methods in light of error propagation formula being uses

--- Framework/Kernel/inc/MantidKernel/Smoothing.h
+++ Framework/Kernel/inc/MantidKernel/Smoothing.h
@@ -12,9 +12,38 @@ namespace Mantid::Kernel {
 
 namespace Smoothing {
 
+/** Performs boxcar (moving average) smoothing on the input data.
+ *
+ * @param input The input vector to be smoothed
+ * @param numPoints The width of the boxcar window (must be >= 3)
+ * @return A new vector containing the smoothed data
+ * @throw std::invalid_argument if numPoints is too small
+ * @tparam T Numeric type (e.g., int, float, double)
+ */
 template <typename T> std::vector<T> boxcarSmooth(std::vector<T> const &input, unsigned const numPoints);
 
-template <typename T> std::vector<T> boxcarSumSquareSmooth(std::vector<T> const &input, unsigned const numPoints);
+/** Performs boxcar (moving average) smoothing on the input data,
+ * using error propagation formula.  This is the correct method to use
+ * for smoothing histogram errors after their y-values have been smoothed.
+ *
+ * @param input The input vector to be smoothed
+ * @param numPoints The width of the boxcar window (must be >= 3)
+ * @return A new vector containing the smoothed data
+ * @throw std::invalid_argument if numPoints is too small
+ * @tparam T Numeric type (e.g., int, float, double)
+ */
+template <typename T> std::vector<T> boxcarErrorSmooth(std::vector<T> const &input, unsigned const numPoints);
+
+/** Performs boxcar (moving average) smoothing on the input data,
+ * using a RMSE average, as is appropriate for error averaging.
+ *
+ * @param input The input vector to be smoothed
+ * @param numPoints The width of the boxcar window (must be >= 3)
+ * @return A new vector containing the smoothed data
+ * @throw std::invalid_argument if numPoints is too small
+ * @tparam T Numeric type (e.g., int, float, double)
+ */
+template <typename T> std::vector<T> boxcarRMSESmooth(std::vector<T> const &input, unsigned const numPoints);
 
 } // namespace Smoothing
 } // namespace Mantid::Kernel
