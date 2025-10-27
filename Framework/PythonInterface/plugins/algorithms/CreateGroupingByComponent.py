ad6b87bf8e1e6c591b236fc6519b1e5ebbadbc9b
Andy Bridger <andy.bridger@stfc.ac.uk>
2025-10-27 11:36:53 +0000

    initial alg and tests

---
+++ Framework/PythonInterface/plugins/algorithms/CreateGroupingByComponent.py
@@ -0,0 +1,141 @@
+# import mantid algorithms, numpy and matplotlib
+import numpy as np
+from mantid.kernel import logger
+from mantid.api import AlgorithmFactory, WorkspaceProperty, PythonAlgorithm
+from mantid.kernel import (
+    Direction,
+    IntBoundedValidator,
+)
+
+
+class CreateGroupingByComponent(PythonAlgorithm):
+    def category(self):
+        return "Diffraction\\Engineering"
+
+    def name(self):
+        return "CreateGroupingByComponent"
+
+    def summary(self):
+        return (
+            "Creates a Grouping Workspace where all components matching the search criteria, "
+            "under a given parent component, are grouped together. "
+            "These groups are then subdivided by N"
+        )
+
+    def version(self):
+        return 1
+
+    def PyInit(self):
+        self.declareProperty(
+            "InstrumentName",
+            "",
+            direction=Direction.Input,
+            doc="Name of the instrument to base the GroupingWorkspace on",
+        )
+        self.declareProperty(
+            WorkspaceProperty("OutputWorkspace", "", direction=Direction.Output),
+            doc="The detector grouping workspace.",
+        )
+        self.declareProperty(
+            "IncludeComponents",
+            "",
+            direction=Direction.Input,
+            doc="Search String for determining which components should be grouped, such components MUST contain this string",
+        )
+        self.declareProperty(
+            "ExcludeComponents",
+            "",
+            direction=Direction.Input,
+            doc="Search String for determining which components should be grouped, such components MUST NOT contain this string",
+        )
+        positive_int_validator = IntBoundedValidator(lower=1)
+        self.declareProperty(
+            "GroupSubdivision",
+            defaultValue=1,
+            direction=Direction.Input,
+            validator=positive_int_validator,
+            doc="Number of subgroups to create for each collection of components. Minimum is 1.",
+        )
+
+    def validateInputs(self):
+        issues = dict()
+        instr = self.getProperty("InstrumentName").value
+
+        try:
+            self.exec_child_alg("LoadEmptyInstrument", InstrumentName=instr, OutputWorkspace="__instr")
+        except RuntimeError:
+            issues["InstrumentName"] = f"Failed to find a matching instrument to the provided input: '{instr}'"
+        return issues
+
+    def PyExec(self):
+        # get inputs
+        instr = self.getProperty("InstrumentName").value
+        ws = self.exec_child_alg("LoadEmptyInstrument", InstrumentName=instr, OutputWorkspace="__instr")
+
+        self.target = self.getProperty("IncludeComponents").value
+        self.null = self.getProperty("ExcludeComponents").value
+
+        self.is_target = self.is_target_include if self.null == "" else self.is_target_exclude
+
+        num_divisions = self.getProperty("GroupSubdivision").value
+
+        self.info = ws.componentInfo()
+        self.detinfo = ws.detectorInfo()
+        self.dets = self.detinfo.detectorIDs()
+        self.instr_dets = self.info.detectorsInSubtree(self.info.root())
+
+        idx = self.info.root()
+        subtree = list(self.info.componentsInSubtree(idx))
+
+        parents_with_any_target_descendant = [int(p) for p in subtree if self.has_target_descendant(int(p))]
+
+        direct_parents = [p for p in parents_with_any_target_descendant if np.any([self.is_target(int(c)) for c in self.info.children(p)])]
+
+        group_string = ",".join([self.get_component_group_string(p, num_divisions) for p in direct_parents])
+
+        logger.debug(group_string)
+
+        group_ws, *_ = self.exec_child_alg(
+            "CreateGroupingWorkspace",
+            InstrumentName=instr,
+            ComponentName=self.info.name(self.info.root()),
+            CustomGroupingString=group_string,
+            OutputWorkspace="_group_ws",
+        )
+
+        self.setProperty("OutputWorkspace", group_ws)
+
+    def exec_child_alg(self, alg_name: str, **kwargs):
+        alg = self.createChildAlgorithm(alg_name, enableLogging=False)
+        alg.setAlwaysStoreInADS(False)
+        alg.initialize()
+        alg.setProperties(kwargs)
+        alg.execute()
+        out_props = tuple(alg.getProperty(prop).value for prop in alg.outputProperties())
+        return out_props[0] if len(out_props) == 1 else out_props
+
+    def component_name_contains(self, idx, target):
+        return target in self.info.name(int(idx))
+
+    def is_target_include(self, idx):
+        return self.component_name_contains(idx, self.target)
+
+    def is_target_exclude(self, idx):
+        return self.is_target_include(idx) and not self.component_name_contains(idx, self.null)
+
+    def has_target_descendant(self, idx):
+        for c in self.info.children(idx):
+            c = int(c)
+            if self.is_target(c) or self.has_target_descendant(c):
+                return True
+        return False
+
+    def get_det_id(self, comp_ind):
+        return str(self.dets[np.where(self.instr_dets == comp_ind)][0])
+
+    def get_component_group_string(self, parent, n=1):
+        all_sub_arrays = np.array_split([self.get_det_id(det) for det in self.info.detectorsInSubtree(int(parent))], n)
+        return ",".join(["+".join(sub_array) for sub_array in all_sub_arrays])
+
+
+AlgorithmFactory.subscribe(CreateGroupingByComponent)
---
+++ Framework/PythonInterface/test/python/plugins/algorithms/CreateGroupingByComponentTest.py
@@ -0,0 +1,93 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2025 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+import unittest
+from mantid.simpleapi import (
+    AnalysisDataService as ADS,
+    AlgorithmManager,
+)
+from testhelpers import run_algorithm
+import numpy as np
+
+
+class CreateGroupingByComponenetTest(unittest.TestCase):
+    def setUp(self):
+        self.default_kwargs = {"InstrumentName": "ENGINX", "OutputWorkspace": "test_group"}
+
+    def tearDown(self):
+        ADS.clear()
+
+    def test_alg_with_default_parameters(self):
+        alg = run_algorithm("CreateGroupingByComponent", **self.default_kwargs)
+        self.assertTrue(alg.isExecuted())
+
+        # by default, include is "" which all components satisfy, so there should be 1 unique group
+        out_ws = ADS.retrieve(alg.getPropertyValue("OutputWorkspace"))
+        self.assertEqual(len(np.unique(out_ws.extractY())), 1)
+
+    def test_alg_with_no_search_term_splits_into_n_groups(self):
+        alg = run_algorithm("CreateGroupingByComponent", **self.default_kwargs, GroupSubdivision=5)
+        self.assertTrue(alg.isExecuted())
+
+        # by default, include is "" which all components satisfy, so sub-dividing this into 5 gives 5 unique groups
+        out_ws = ADS.retrieve(alg.getPropertyValue("OutputWorkspace"))
+        self.assertEqual(len(np.unique(out_ws.extractY())), 5)
+
+    def test_alg_with_include_term_splits_into_expected_groups(self):
+        alg = run_algorithm("CreateGroupingByComponent", **self.default_kwargs, IncludeComponents="detector-block")
+        self.assertTrue(alg.isExecuted())
+
+        # ENGINX has structure banks -> modules -> blocks -> pixels
+        # with 2 banks, each containing 5 modules (each module then made up of 9 blocks)
+
+        # here we have requested that we group the detector-blocks together.
+        # As this is done under each parent component we should have 10 groups + 1 for the null group
+
+        out_ws = ADS.retrieve(alg.getPropertyValue("OutputWorkspace"))
+        self.assertEqual(len(np.unique(out_ws.extractY())), 11)
+
+    def test_alg_with_exclude_term_splits_into_expected_groups(self):
+        alg = run_algorithm("CreateGroupingByComponent", **self.default_kwargs, IncludeComponents="pixel")
+        self.assertTrue(alg.isExecuted())
+
+        # ENGINX has diffraction structure: banks -> modules -> blocks -> pixels
+        # with 2 banks, each containing 5 modules (each module then made up of 9 blocks)
+
+        # AND forward transmission structure: bank -> columns -> pixels
+        # with 1 banks, containing 10 columns (each module then made up of 10 pixels)
+
+        # here we have requested that we group the pixel together.
+        # As this is done under each parent component we should have 2*5*9 groups for the diffraction
+        # and 10 for the transmission, so 100 total
+
+        out_ws = ADS.retrieve(alg.getPropertyValue("OutputWorkspace"))
+        self.assertEqual(len(np.unique(out_ws.extractY())), 100)
+
+        # Now we run again and exclude any component which has 'transmission' in the name
+
+        alg = run_algorithm("CreateGroupingByComponent", **self.default_kwargs, IncludeComponents="pixel", ExcludeComponents="transmission")
+        self.assertTrue(alg.isExecuted())
+
+        # Now only expect the 90 groups for the diffraction banks (+1 for the null group)
+
+        out_ws = ADS.retrieve(alg.getPropertyValue("OutputWorkspace"))
+        self.assertEqual(len(np.unique(out_ws.extractY())), 91)
+
+    def test_alg_with_bad_instrument_name_fails(self):
+        alg = _init_alg(InstrumentName="FakeInstrument", OutputWorkspace="test")
+        self.assertRaises(RuntimeError, alg.execute)
+
+
+def _init_alg(**kwargs):
+    alg = AlgorithmManager.create("CreateGroupingByComponent")
+    alg.initialize()
+    for prop, value in kwargs.items():
+        alg.setProperty(prop, value)
+    return alg
+
+
+if __name__ == "__main__":
+    unittest.main()
