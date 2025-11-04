df186021a1cd84f02b7599d08e02c27d5959e221
Marie Backman <backmanma@ornl.gov>
2025-11-04 13:33:22 -0500

    fix shadowFunction for Framework/Geometry/src/Instrument.cpp and remove cppcheck suppression

--- Framework/Geometry/src/Instrument.cpp
+++ Framework/Geometry/src/Instrument.cpp
@@ -626,8 +626,8 @@ void Instrument::markAsDetector(const IDetector *det) {
   if ((it != m_detectorCache.end()) && (std::get<0>(*it) == det->getID())) {
     raiseDuplicateDetectorError(det->getID());
   }
-  bool isMonitor = false;
-  m_detectorCache.emplace(it, det->getID(), det_sptr, isMonitor);
+  bool isMonitorFlag = false;
+  m_detectorCache.emplace(it, det->getID(), det_sptr, isMonitorFlag);
 }
 
 /// As markAsDetector but without the required sorting. Must call
@@ -639,8 +639,8 @@ void Instrument::markAsDetectorIncomplete(const IDetector *det) {
 
   // Create a (non-deleting) shared pointer to it
   IDetector_const_sptr det_sptr = IDetector_const_sptr(det, NoDeleting());
-  bool isMonitor = false;
-  m_detectorCache.emplace_back(det->getID(), det_sptr, isMonitor);
+  bool isMonitorFlag = false;
+  m_detectorCache.emplace_back(det->getID(), det_sptr, isMonitorFlag);
 }
 
 /// Sorts the detector cache. Called after all detectors have been marked via
