9a0068e820a13418863433fbc4890dba98f02b54
rboston628 <bostonsr@ornl.gov>
2025-11-11 09:27:39 -0500

    make fft smoothing functions

--- Framework/Kernel/src/Smoothing.cpp
+++ Framework/Kernel/src/Smoothing.cpp
@@ -6,6 +6,7 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #include "MantidKernel/Smoothing.h"
 #include "MantidKernel/DllConfig.h"
+#include "MantidKernel/GSL_FFT_Helpers.h"
 
 #include <algorithm>
 #include <cmath>
@@ -155,5 +156,107 @@ template <typename T> std::vector<T> boxcarRMSESmooth(std::vector<T> const &inpu
 template MANTID_KERNEL_DLL std::vector<double> boxcarSmooth(std::vector<double> const &, unsigned int const);
 template MANTID_KERNEL_DLL std::vector<double> boxcarRMSESmooth(std::vector<double> const &, unsigned int const);
 template MANTID_KERNEL_DLL std::vector<double> boxcarErrorSmooth(std::vector<double> const &, unsigned int const);
+
+// FFT SMOOTHING METHODS
+
+namespace fft {
+
+// filters
+template <typename Y> struct FFTFilter {
+  virtual Y operator()(std::size_t index) const = 0;
+  virtual ~FFTFilter() = default;
+};
+
+template <typename Y> struct ZeroFilter : public FFTFilter<Y> {
+  // remove the higher frequencies by setting to zero
+  // REF: see example code at
+  // - https://www.gnu.org/software/gsl/doc/html/fft.html#overview-of-real-data-f
+  ZeroFilter(std::size_t n) : cutoff(n) {}
+  Y operator()(std::size_t index) const override { return (index >= cutoff ? 0 : 1); }
+
+private:
+  std::size_t cutoff;
+};
+
+template <typename Y> struct ButterworthFilter : public FFTFilter<Y> {
+  // remove the higher frequencies by tapering with a Butterworth filter
+  // SOME REFS:
+  // - https://scikit-image.org/docs/0.25.x/api/skimage.filters.html#skimage.filters.butterworth
+  // - https://isbweb.org/software/sigproc/bogert/filter.pdf
+  // - https://users.cs.cf.ac.uk/dave/Vision_lecture/node22.html
+  ButterworthFilter(unsigned n, unsigned o) : two_order(2U * o), invcutoff(1.0 / static_cast<Y>(n)) {}
+  Y operator()(std::size_t index) const override {
+    return 1.0 / (1.0 + std::pow(invcutoff * static_cast<Y>(index), two_order));
+  }
+
+private:
+  unsigned two_order;
+  Y invcutoff;
+};
+
+template <typename Y> std::vector<Y> fftSmoothWithFilter(std::vector<Y> const &input, FFTFilter<Y> const &filter) {
+  std::size_t dn = input.size();
+  std::vector<Y> output(input.cbegin(), input.cend());
+
+  // obtain the FFT
+  Kernel::fft::real_ws_uptr real_ws = Kernel::fft::make_gsl_real_workspace(dn);
+  Kernel::fft::real_wt_uptr real_wt = Kernel::fft::make_gsl_real_wavetable(dn);
+  gsl_fft_real_transform(output.data(), 1, dn, real_wt.get(), real_ws.get());
+  real_wt.reset();
+
+  // NOTE: the halfcomplex storage requires special treatment of even/odd
+  // NOTE: we cannot use GSL's unpack because these values would have to be altered and then re-packed
+  // it is simpler to access them inplace.
+  bool even = (dn % 2 == 0);
+  std::size_t complex_size = (even ? dn / 2 : (dn - 1) / 2);
+  output[0] *= filter(0); // x[0] = z[0].real; z[0].imag = 0 and is not stored anywhere
+  for (std::size_t fn = 1; fn < complex_size + (even ? 0UL : 1UL); fn++) {
+    output[2 * fn - 1] *= filter(fn); // real parts
+    output[2 * fn] *= filter(fn);     // imaginary parts
+  }
+  // for even data, last point is an unmatched real value
+  if (even) {
+    output[dn - 1] *= filter(complex_size);
+  }
+
+  // transform back
+  Kernel::fft::hc_wt_uptr hc_wt = Kernel::fft::make_gsl_hc_wavetable(dn);
+  gsl_fft_halfcomplex_inverse(output.data(), 1, dn, hc_wt.get(), real_ws.get());
+  hc_wt.reset();
+  real_ws.reset();
+
+  // return the smoothed result
+  return output;
+}
+
+} // namespace fft
+
+template <typename Y> std::vector<Y> fftSmooth(std::vector<Y> const &input, unsigned const cutoff) {
+  if (cutoff == 0) {
+    throw std::invalid_argument("The cutoff frequency must be greater than zero");
+  } else if (cutoff > input.size()) {
+    throw std::invalid_argument("The cutoff frequency must be less than the array size");
+  }
+
+  fft::ZeroFilter<Y> filter(cutoff);
+  return fftSmoothWithFilter(input, filter);
+}
+
+template <typename Y>
+std::vector<Y> fftButterworthSmooth(std::vector<Y> const &input, unsigned const cutoff, unsigned const order) {
+  if (cutoff == 0) {
+    throw std::invalid_argument("The cutoff frequency must be greater than zero");
+  }
+  if (cutoff > input.size()) {
+    throw std::invalid_argument("The Butterworth cutoff frequency must be less than the array size");
+  }
+
+  fft::ButterworthFilter<Y> filter(cutoff, order);
+  return fft::fftSmoothWithFilter(input, filter);
+}
+
+template MANTID_KERNEL_DLL std::vector<double> fftSmooth(std::vector<double> const &, unsigned const);
+template MANTID_KERNEL_DLL std::vector<double> fftButterworthSmooth(std::vector<double> const &, unsigned const,
+                                                                    unsigned const);
 
 } // namespace Mantid::Kernel::Smoothing
