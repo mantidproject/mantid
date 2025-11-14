c91f05d6ee4a4c60fc552671bae075047ff1cb7e
James Clarke <139879523+jclarkeSTFC@users.noreply.github.com>
2025-11-14 10:20:13 +0000

    Log when the new Instrument View is opened (#40308)
    Log when the new Instrument View is opened
    
    I also changed the text in the spherical coordinates to show theta and
    phi instead of t and p, this was a user request.
    
    ### To test:
    
    With usage reports enabled, this entry in the usage database should be
    updated when you open the new Instrument View:
    
    <img width="731" height="70" alt="image"
    src="https://github.com/user-attachments/assets/5978b2f5-55d3-465c-9620-7595c8801534"
    />

--- qt/python/instrumentview/instrumentview/FullInstrumentViewWindow.py
+++ qt/python/instrumentview/instrumentview/FullInstrumentViewWindow.py
@@ -33,6 +33,8 @@ from instrumentview.Detectors import DetectorInfo
 from instrumentview.InteractorStyles import CustomInteractorStyleZoomAndSelect, CustomInteractorStyleRubberBand3D
 from typing import Callable
 from mantid.dataobjects import Workspace2D
+from mantid import UsageService
+from mantid.kernel import FeatureType
 from mantidqt.plotting.mantid_navigation_toolbar import MantidNavigationToolbar
 import numpy as np
 import pyvista as pv
@@ -199,6 +201,8 @@ class FullInstrumentViewWindow(QMainWindow):
         self.interactor_style = CustomInteractorStyleZoomAndSelect()
         self._overlay_meshes = []
         self._lineplot_overlays = []
+
+        UsageService.registerFeatureUsage(FeatureType.Interface, "InstrumentView2025", False)
 
     def check_sum_spectra_checkbox(self) -> None:
         self._sum_spectra_checkbox.setChecked(True)
@@ -539,7 +543,7 @@ class FullInstrumentViewWindow(QMainWindow):
         self._set_detector_edit_text(
             self._detector_spherical_position_edit,
             detector_infos,
-            lambda d: f"r: {d.spherical_position[0]:.3f}, t: {d.spherical_position[1]:.1f}, p: {d.spherical_position[2]:.1f}",
+            lambda d: f"r: {d.spherical_position[0]:.3f}, 2\u03b8: {d.spherical_position[1]:.1f}, \u03c6: {d.spherical_position[2]:.1f}",
         )
         self._set_detector_edit_text(self._detector_pixel_counts_edit, detector_infos, lambda d: str(d.pixel_counts))
 
