3994bd5bcedfe94cb20c0d192d40abc59b682e9c
Reece Boston <52183986+rboston628@users.noreply.github.com>
2025-11-17 09:22:27 -0500

    Create algorithm `AddLogSmoothed` (#40332)
    While Mantid currently provides functionality for smoothing data, there
    is no algorithm to smooth values stored in logs. Analysis for VULCAN
    requires splitting time series based on values of a time-changing log,
    and in this case sometimes noise in the log measurements can disrupt the
    splitting.
    
    This adds an algorithm to smooth log values.
    
    This is part of Roadmap Issue #40189, needed for VNext.
    [EWM13722](https://ornlrse.clm.ibmcloud.com/ccm/web/projects/Neutron%20Data%20Project%20(Change%20Management)#action=com.ibm.team.workitem.viewWorkItem&id=13722)
    
    ### To test:
    
    Check the unit tests.
    
    After building, try running this script:
    ``` python
    # import mantid algorithms, numpy and matplotlib
    from mantid.simpleapi import *
    import matplotlib.pyplot as plt
    import numpy as np
    
    instrument = "VULCAN"
    runnumber = 263929
    ipts = GetIPTS(runnumber, instrument)
    logName = "ls336.temp3"
    ws = Load(f"{ipts}nexus/{instrument}_{runnumber}.nxs.h5")
    
    AddLogSmoothed(
        InputWorkspace = ws,
        LogName = logName,
        SmoothingMethod="BoxCar",
        Params = 9,
        NewLogName = f"{logName}_boxcar",
    )
    
    AddLogSmoothed(
        InputWorkspace = ws,
        LogName = logName,
        SmoothingMethod="Zeroing",
        Params = 99,
        NewLogName = f"{logName}_fft",
    )
    
    AddLogSmoothed(
        InputWorkspace = ws,
        LogName = logName,
        SmoothingMethod="Butterworth",
        Params = [99, 2],
        NewLogName = f"{logName}_butterworth",
    )
    ```
    You will need `VULCAN_263929`. After running, view the logs and see the
    plots.
    
    <!--  GATEKEEPER: When squashing, remove the section from HERE...  -->
    ---
    
    ### Reviewer
    
    **Your comments will be used as part of the gatekeeper process.**
    Comment clearly on what you have checked and tested during your review.
    Provide an audit trail for any changes requested.
    
    As per the [review
    guidelines](http://developer.mantidproject.org/ReviewingAPullRequest.html):
    
    - Is the code of an acceptable quality? ([Code
    standards](http://developer.mantidproject.org/Standards/)/[GUI
    standards](http://developer.mantidproject.org/Standards/GUIStandards.html))
    - Has a thorough functional test been performed? Do the changes handle
    unexpected input/situations?
    - Are appropriately scoped unit and/or system tests provided?
    - Do the release notes conform to the
    [guidelines](https://developer.mantidproject.org/Standards/ReleaseNotesGuide.html)
    and describe the changes appropriately?
    - Has the relevant (user and developer) documentation been
    added/updated?
    - If the PR author isn’t in the `mantid-developers` or
    `mantid-contributors` teams, add a review comment `rerun ci` to
    authorize/rerun the CI
    
    ### Gatekeeper
    
    As per the [gatekeeping
    guidelines](https://developer.mantidproject.org/Gatekeeping.html):
    
    - Has a thorough first line review been conducted, including functional
    testing?
    - At a high-level, is the code quality sufficient?
    - Are the base, milestone and labels correct?
    <!--  GATEKEEPER: ...To HERE  -->

---
+++ Framework/Algorithms/inc/MantidAlgorithms/AddLogSmoothed.h
@@ -0,0 +1,45 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2011 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidAPI/Algorithm.h"
+#include "MantidAlgorithms/DllConfig.h"
+
+namespace Mantid {
+namespace Algorithms {
+
+/** Takes an existing sample log and smooths it using a selected method
+  @author Reece Boston
+  @date 2025-11-11
+*/
+
+class MANTID_ALGORITHMS_DLL AddLogSmoothed final : public API::Algorithm {
+public:
+  /// Algorithm's name for identification
+  const std::string name() const override { return "AddLogSmoothed"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Add a sample log that is the smoothed version of an existing log";
+  }
+
+  /// Algorithm's version for identification
+  int version() const override { return 1; }
+  const std::vector<std::string> seeAlso() const override { return {"AddSampleLog", "AddLogDerivative"}; }
+  /// Algorithm's category for identification
+  const std::string category() const override { return "DataHandling\\Logs"; }
+
+private:
+  /// Initialise the properties
+  void init() override;
+  /// Validate input parameters
+  std::map<std::string, std::string> validateInputs() override;
+  /// Run the algorithm
+  void exec() override;
+};
+
+} // namespace Algorithms
+} // namespace Mantid
