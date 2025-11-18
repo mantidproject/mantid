e574cdbb7a08ae85876956b856b039da1756663c
Caila Finn <caila.finn@stfc.ac.uk>
2025-11-18 17:26:00 +0000

    Fix odd pathing on SANS tests
    RE #34850

--- Testing/SystemTests/tests/framework/ISIS/SANS/LOQ/SANSLOQReductionGUI.py
+++ Testing/SystemTests/tests/framework/ISIS/SANS/LOQ/SANSLOQReductionGUI.py
@@ -7,7 +7,7 @@
 # pylint: disable=attribute-defined-outside-init
 
 import systemtesting
-from ISIS.SANS.isis_sans_system_test import ISISSansSystemTest
+from isis_sans_system_test import ISISSansSystemTest
 from mantid.api import FileFinder
 from mantid.kernel import config
 import ISISCommandInterface as i
