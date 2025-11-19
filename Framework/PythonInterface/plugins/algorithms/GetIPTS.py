d2e6aa61da649dbb167e2beb3b71b8ea7f9e0c2e
Pete Peterson <petersonpf@ornl.gov>
2025-11-19 16:25:57 -0500

    Fix typo in RuntimeError message formatting for IPTS directory

--- Framework/PythonInterface/plugins/algorithms/GetIPTS.py
+++ Framework/PythonInterface/plugins/algorithms/GetIPTS.py
@@ -56,7 +56,7 @@ class GetIPTS(PythonAlgorithm):
                 pass  # just keep looking
 
         # failed to find any is an error
-        raise RuntimeError(f"Cannot find IPTS directory for RunNumber'{runnumber}' at Instrument='{instrument}'")
+        raise RuntimeError(f"Cannot find IPTS directory for RunNumber='{runnumber}' at Instrument='{instrument}'")
 
     def getIPTSLocal(self, instrument, runnumber):
         # prepend non-empty instrument name for FileFinder
