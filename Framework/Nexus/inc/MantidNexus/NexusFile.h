b51843b0d967d9b116d67909bc464e3c2dec6dd3
rboston628 <bostonsr@ornl.gov>
2025-11-24 08:17:32 -0500

    add docstrings to UniqueID

--- Framework/Nexus/inc/MantidNexus/NexusFile.h
+++ Framework/Nexus/inc/MantidNexus/NexusFile.h
@@ -64,6 +64,14 @@ public:
 
 using Deleter = int (*const)(hid_t);
 
+/**
+ * \class UniqueID
+ * \brief A wrapper class for managing HDF5 object handles (hid_t).
+ *
+ * The UniqueID class is designed to manage the lifecycle of HDF5 object handles (hid_t),
+ * ensuring that the handle is properly closed when the UniqueID object is destroyed.
+ * This helps prevent resource leaks and ensures proper cleanup of HDF5 resources.
+ */
 template <Deleter const deleter> class UniqueID {
 private:
   hid_t m_id;
@@ -78,6 +86,8 @@ private:
 public:
   UniqueID(UniqueID<deleter> &uid) : m_id(uid.m_id) { uid.m_id = -1; };
   UniqueID(UniqueID<deleter> &&uid) noexcept : m_id(uid.m_id) { uid.m_id = -1; };
+  /// @brief Assign a HDF5 object ID to be managed
+  /// @param id : the ID to be managed
   UniqueID &operator=(hid_t const id) {
     if (id != m_id) {
       closeId();
@@ -85,6 +95,8 @@ public:
     }
     return *this;
   };
+  /// @brief Pass the HDF5 object ID from an existing UniqueID to another UniqueID
+  /// @param uid : the UniqueID previously managing the ID; it will lose ownership of the ID.
   UniqueID &operator=(UniqueID<deleter> &uid) {
     if (this != &uid) {
       closeId();
@@ -93,11 +105,12 @@ public:
     }
     return *this;
   }
-
   bool operator==(int const v) const { return static_cast<int>(m_id) == v; }
   bool operator<=(int const v) const { return static_cast<int>(m_id) <= v; }
   operator hid_t const &() const { return m_id; };
   hid_t getId() const { return m_id; }
+  /// @brief  Release hold on the managed ID; it will not be closed by this UniqueID
+  /// @return the managed ID
   hid_t releaseId() {
     hid_t tmp = m_id;
     m_id = -1;
