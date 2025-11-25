daa51c263e3b88f07d8db23d4c70246ee3ea448e
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-11-25 08:32:09 +0000

    Expose number of solid angle cylinder slices to SANS state (#40242)
    ### Description of work
    
    This PR adds the option to the SANS user file to specify a number of
    cylinder slices to be used when calculating the Solid Angle.
    
    This value is set to 11 by default, as opposed to the 10 used as default
    in the underlying algorithms. Because of this a large number of
    reference files for system tests have had to be updated.
    
    Plots comparing the results can be found here:
    `\\Olympic\babylon5\Public\miallewis`
    
    To make the changing of a large number of reference files easier, I
    added the ability to specify an environment variable
    `GENERATE_REFERENCE_FILES_ON_FAILURE=1`, that essentially reroutes the
    mismatch file into the reference data folder, ensuring it is named the
    same as the original reference file.
    
    Closes #40221 <!-- One line per closed issue. -->

--- Framework/Algorithms/src/Q1D2.cpp
+++ Framework/Algorithms/src/Q1D2.cpp
@@ -78,6 +78,8 @@ void Q1D2::init() {
                   Direction::Input);
   auto mustBePositive = std::make_shared<BoundedValidator<double>>();
   mustBePositive->setLower(0.0);
+  auto mustBeIntOver2 = std::make_shared<BoundedValidator<int>>();
+  mustBeIntOver2->setLower(3);
 
   declareProperty("RadiusCut", 0.0, mustBePositive,
                   "To increase resolution some wavelengths are excluded within "
@@ -101,7 +103,11 @@ void Q1D2::init() {
                   " returned by the property OutputWorkspace "
                   "(default is false).");
   declareProperty("ExtraLength", 0.0, mustBePositive, "Additional length for gravity correction.");
-
+  declareProperty(
+      "SolidAngleNumberOfCylinderSlices", 10, mustBeIntOver2,
+      "The number of angular slices used when triangulating a cylinder in order to calculate the solid "
+      "angle of a tube detector. The default is 10 to preserve legacy behaviour, but increased accuracy has "
+      "been observed when using values of 11+");
   declareProperty(
       std::make_unique<WorkspaceProperty<>>("QResolution", "", Direction::Input, PropertyMode::Optional, dataVal),
       "Workspace to calculate the Q resolution.\n");
@@ -378,11 +384,12 @@ void Q1D2::pixelWeight(const API::MatrixWorkspace_const_sptr &pixelAdj, const si
   const V3D samplePos = detectorInfo.samplePosition();
 
   if (m_doSolidAngle) {
+    const int numberOfCylinderSlices = getProperty("SolidAngleNumberOfCylinderSlices");
     weight = 0.0;
     for (const auto detID : m_dataWS->getSpectrum(wsIndex).getDetectorIDs()) {
       const auto index = detectorInfo.indexOf(detID);
       if (!detectorInfo.isMasked(index))
-        weight += detectorInfo.detector(index).solidAngle(samplePos);
+        weight += detectorInfo.detector(index).solidAngle({samplePos, numberOfCylinderSlices});
     }
   } else
     weight = 1.0;
