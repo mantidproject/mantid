89fc7c11f32ad46c8d07a83d406572b092cbed99
James Clarke <139879523+jclarkeSTFC@users.noreply.github.com>
2025-11-28 07:59:20 +0000

    Move legend to bottom, increase transparency of picked detectors (#40385)
    Legend now defaults to the bottom of the plot, and picked points are
    more transparent so you can still see the data of the picked points.
    
    This was a user request from Fabio
    
    ### To test:
    
    Open the new Instrument View and check both the legend and how picked
    detectors look.

--- qt/python/instrumentview/instrumentview/test/test_view.py
+++ qt/python/instrumentview/instrumentview/test/test_view.py
@@ -68,7 +68,7 @@ class TestFullInstrumentViewWindow(unittest.TestCase):
             scalars=mock_scalars,
             render_points_as_spheres=True,
             point_size=15,
-            scalar_bar_args={"interactive": True, "vertical": True, "title_font_size": 15, "label_font_size": 12},
+            scalar_bar_args={"interactive": True, "vertical": False, "title_font_size": 15, "label_font_size": 12},
         )
 
     def test_add_pickable_main_mesh(self):
@@ -79,7 +79,7 @@ class TestFullInstrumentViewWindow(unittest.TestCase):
         self._view.main_plotter.add_mesh.assert_called_once_with(
             mock_mesh,
             scalars=mock_scalars,
-            opacity=[0.0, 0.5],
+            opacity=[0.0, 0.3],
             show_scalar_bar=False,
             pickable=True,
             cmap="Oranges",
