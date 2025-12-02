bf9aef0f62e1f7a148f7a64dfcbda2bfa29cdc3a
RabiyaF <47083562+RabiyaF@users.noreply.github.com>
2025-12-02 23:21:50 +0000

    Added the FlatCell algorithm files

---
+++ Framework/Algorithms/inc/MantidAlgorithms/FlatCell.h
@@ -0,0 +1,46 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2009 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+#include "MantidAPI/Algorithm.h"
+#include "MantidAlgorithms/DllConfig.h"
+#include "MantidDataObjects/EventList.h"
+#include "MantidDataObjects/EventWorkspace.h"
+#include "MantidIndexing/SpectrumIndexSet.h"
+
+namespace Mantid {
+
+namespace Histogram {
+class BinEdges;
+}
+namespace Algorithms {
+/** Generates the FlatCell workspace for ISIS SANS reduction of LOQ */
+class MANTID_ALGORITHMS_DLL FlatCell : public API::Algorithm {
+public:
+  /// Algorithm's name
+  const std::string name() const override { return "FlatCell"; }
+  /// Summary of algorithms purpose
+  const std::string summary() const override {
+    return "Generates the FlatCell workspace for ISIS SANS reduction of LOQ.";
+  }
+
+  /// Algorithm's version
+  int version() const override { return (1); }
+
+  /// Algorithm's category for identification
+  const std::string category() const override { return "Place\\Holder"; }
+
+private:
+  /// Initialisation code
+  void init() override;
+  /// Execution code
+  void exec() override;
+  void execEvent();
+};
+
+} // namespace Algorithms
+} // namespace Mantid
---
+++ Testing/SystemTests/tests/framework/FlatCellTest.py
@@ -0,0 +1,37 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+# pylint: disable=no-init,attribute-defined-outside-init
+import systemtesting
+from mantid.simpleapi import CompareWorkspaces, FlatCell, LoadAscii
+
+
+class FlatCellTest(systemtesting.MantidSystemTest):
+    """Tests the FlatCell algorithm"""
+
+    def runTest(self):
+        # Load the input data and save into a workspace
+        LoadAscii(Filename="flatcell_input.csv", OutputWorkspace="FlatCellInput")
+
+        # Apply the algorithm to the input data
+        FlatCell(InputWorkspace="FlatCellInput", OutputWorkspace="FlatCellInput")
+
+        # Load the output data and save into a workspace
+        LoadAscii(Filename="flatcell_output.csv", OutputWorkspace="FlatCellOutput")
+
+        # Compare the results
+        (result, _messages) = CompareWorkspaces("FlatCellInput", "FlatCellOutput")
+        self.assertEqual(result, True)
+
+    # def requiredFiles(self):
+    #     return ['flatcell_input.csv', 'flatcell_output.csv']
+
+    # def validateMethod(self):
+    #     return 'ValidateAscii'
+
+    # def validate(self):
+    #     self.tolerance = 1e-8
+    #     return ("FlatCellInput", "flatcell_output.csv")
