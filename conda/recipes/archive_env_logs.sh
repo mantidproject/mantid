6e54f04783e6b9d34ff6ee4df3e6debe621e14b2
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2025-12-17 11:29:46 +0000

    avoid pixi exec and use package-standalone env when archive environment logs

--- conda/recipes/archive_env_logs.sh
+++ conda/recipes/archive_env_logs.sh
@@ -9,8 +9,8 @@ mkdir -p $log_directory
 
 #Just for first package (mantid), archive the base environment
 if [ "$package_name" == mantid ] ; then
-  pixi exec conda list --explicit base > $log_directory/base_environment.txt 2>&1 || echo "Failed to write base conda list output to file"
+  pixi run -e package-standalone conda list --explicit base > $log_directory/base_environment.txt 2>&1 || echo "Failed to write base conda list output to file"
 fi
 
-pixi exec conda list --explicit --prefix "$build_prefix" > $log_directory/"${package_name}"_build_environment.txt 2>&1
-pixi exec conda list --explicit --prefix "$prefix" > $log_directory/"${package_name}"_host_environment.txt 2>&1
+pixi run -e package-standalone conda list --explicit --prefix "$build_prefix" > $log_directory/"${package_name}"_build_environment.txt 2>&1
+pixi run -e package-standalone conda list --explicit --prefix "$prefix" > $log_directory/"${package_name}"_host_environment.txt 2>&1
