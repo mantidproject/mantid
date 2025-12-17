84e8436ddc0ed82bc9ba5b073fa938d23a71a116
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2025-12-17 10:23:14 +0000

    call pixi run on each command since function won't have been declared for the new pixi shell

--- buildconfig/Jenkins/Conda/delete-old-packages.sh
+++ buildconfig/Jenkins/Conda/delete-old-packages.sh
@@ -49,22 +49,24 @@ function delete_package() {
   package_name=$2
   label=$3
 
+  pixi_command="pixi run --manifest-path $REPO_ROOT_DIR/pixi.toml -e delete-old-packages --frozen"
+
   file_url="https://api.anaconda.org/package/$channel/$package_name/files"
   echo Get from url: $file_url
-  all_packages=$(curl -s -X GET $file_url)
+  all_packages=$($pixi_command curl -s -X GET $file_url)
 
-  latest_version=$(echo $all_packages | jq -r '.[-1].version')
+  latest_version=$(echo $all_packages | $pixi_command jq -r '.[-1].version')
   echo Latest Version: $latest_version
 
-  packages_to_delete=$(echo $all_packages | jq -r ".[] | select(.labels == [\"$label\"] and .version != \"$latest_version\") | .full_name")
+  packages_to_delete=$(echo $all_packages | $pixi_command jq -r ".[] | select(.labels == [\"$label\"] and .version != \"$latest_version\") | .full_name")
   echo Packages to delete: $packages_to_delete
 
-  packages_not_to_delete=$(echo $all_packages | jq -r ".[] | select(.labels == [\"$label\"] and .version == \"$latest_version\") | .full_name")
+  packages_not_to_delete=$(echo $all_packages | $pixi_command jq -r ".[] | select(.labels == [\"$label\"] and .version == \"$latest_version\") | .full_name")
   echo Packages not to delete: $packages_not_to_delete
 
   for package_file in $packages_to_delete; do
       echo Deleting package: $package_file
-      curl -s -X DELETE -H "Authorization: token $ANACONDA_TOKEN" "https://api.anaconda.org/dist/$package_file"
+      $pixi_command curl -s -X DELETE -H "Authorization: token $ANACONDA_TOKEN" "https://api.anaconda.org/dist/$package_file"
   done
 }
 ###
@@ -73,5 +75,5 @@ function delete_package() {
 install_pixi
 
 for name in "$@"; do
-  pixi run --manifest-path $REPO_ROOT_DIR/pixi.toml -e delete-old-packages --frozen delete_package $CHANNEL $name $LABEL
+  delete_package $CHANNEL $name $LABEL
 done
