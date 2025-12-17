a81849f64610a5c5181eead7b524a58e41016702
warunawickramasingha <waruna.wickramasingha@stfc.ac.uk>
2025-12-17 17:10:00 +0000

    removed px from injected docs

--- docs/sphinxext/mantiddoc/directives/plot_directive.py
+++ docs/sphinxext/mantiddoc/directives/plot_directive.py
@@ -76,7 +76,7 @@ def insert_placeholder_caption(state_machine):
     placeholder = [
         ".. figure:: /images/ImageNotFound.png",
         "   :class: screenshot",
-        "   :width: 200px",
+        "   :width: 200",
         "\n",
         "   Enable :plots: using DOCS_PLOTDIRECTIVE in CMake", ""
     ]
