787319cbb2e0a76b8418d693e9223bbf25d24f94
Ross Whitfield <whitfieldre1@ornl.gov>
2025-12-18 13:42:55 +1100

    IBackgroundFunction

--- Framework/API/inc/MantidAPI/IBackgroundFunction.h
+++ Framework/API/inc/MantidAPI/IBackgroundFunction.h
@@ -19,7 +19,7 @@ namespace API {
     @author Anders Markvardsen, ISIS, RAL
     @date 2/11/2009
 */
-class IBackgroundFunction : public IFunctionWithLocation {
+class MANTID_API_DLL IBackgroundFunction : public IFunctionWithLocation {
 public:
   /// Fits the function
   /// @param X :: a vector of x values
