e6e2e240dc4f2b3a2eb5a1958ed3a069866f302a
Pete Peterson <petersonpf@ornl.gov>
2025-12-31 12:03:11 -0500

    Undo changes to qt/ portion of the repository

---
+++ qt/scientific_interfaces/Muon/PrecompiledHeader.h
@@ -0,0 +1,23 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+// Mantid
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/MatrixWorkspace_fwd.h"
+#include "MantidKernel/System.h"
+
+// Qt
+#include <QDialog>
+#include <QString>
+#include <QWidget>
+
+// STL
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
---
+++ qt/widgets/common/inc/MantidQtWidgets/Common/PrecompiledHeader.h
@@ -0,0 +1,22 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+
+// Mantid
+#include "MantidAPI/Algorithm.h"
+#include "MantidAPI/AnalysisDataService.h"
+
+// STL
+
+// Qt
+#include <QApplication>
+#include <QDialog>
+#include <QObject>
+#include <QPushButton>
+#include <QString>
+#include <QStringList>
+#include <QWidget>
