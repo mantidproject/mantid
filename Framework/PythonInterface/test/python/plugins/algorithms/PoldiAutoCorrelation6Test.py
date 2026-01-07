fac8197ddc8a4e932a2209cddd9502a35741fbf1
Richard Waite <richard.waite@stfc.ac.uk>
2026-01-07 10:42:05 +0000

    Add unit tests for ngroups and detectors in PoldiAutoCorrelation

--- Framework/PythonInterface/test/python/plugins/algorithms/PoldiAutoCorrelation6Test.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/PoldiAutoCorrelation6Test.py
@@ -5,6 +5,8 @@
 #   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 # SPDX - License - Identifier: GPL - 3.0 +
 import unittest
+from numpy.testing import assert_array_almost_equal, assert_array_equal
+import numpy as np
 from mantid.simpleapi import PoldiAutoCorrelation, AnalysisDataService
 from mantid.api import FileFinder
 from plugins.algorithms.poldi_utils import load_poldi
@@ -41,6 +43,19 @@ class PoldiAutoCorrelation6Test(unittest.TestCase):
         # assert bin width/number bins
         self.assertEqual(ws_corr.blocksize(), 1748)
 
+    def test_exec_ngroups(self):
+        ngroups = 3
+        ws_corr = PoldiAutoCorrelation(InputWorkspace=self.ws, OutputWorkspace="ws_corr", NGroups=ngroups, Version=6)
+
+        # assert two-theta of spectra
+        self.assertEqual(ws_corr.getNumberHistograms(), ngroups)
+        si = ws_corr.spectrumInfo()
+        tths = np.degrees([si.twoTheta(ispec) for ispec in range(ws_corr.getNumberHistograms())])
+        assert_array_almost_equal(tths, np.array([79.19, 89.73, 100.26]), decimal=2)
+        # assert number fo detectors associated with each spectrum
+        ndets = [len(ws_corr.getSpectrum(ispec).getDetectorIDs()) for ispec in range(ws_corr.getNumberHistograms())]
+        assert_array_equal(ndets, np.array([150, 149, 149]))
+
     def _assert_auto_corr_workspace(self, ws_corr):
         # assert min/max Q
         self.assertAlmostEqual(ws_corr.readX(0)[0], 1.5144, delta=1e-3)
@@ -50,6 +65,8 @@ class PoldiAutoCorrelation6Test(unittest.TestCase):
         # assert max y-value at Bragg peak Q
         _, imax = ws_corr.findY(ws_corr.readY(0).max())
         self.assertAlmostEqual(ws_corr.readX(0)[imax], 3.272, delta=1e-2)  # (220) peak @ d = 1.920 Ang
+        # assert spectra have detectorIDs
+        self.assertTrue(ws_corr.spectrumInfo().hasDetectors(0))
 
 
 if __name__ == "__main__":
