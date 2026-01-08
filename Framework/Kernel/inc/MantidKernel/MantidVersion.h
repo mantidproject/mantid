18069cf6bfae41fc2162de948cf64683b21e177d
Pete Peterson <petersonpf@ornl.gov>
2026-01-08 18:02:16 -0500

    Fix cppcheck issues

--- Framework/Kernel/inc/MantidKernel/MantidVersion.h
+++ Framework/Kernel/inc/MantidKernel/MantidVersion.h
@@ -27,16 +27,16 @@ public:
     std::string tweak;
   };
 
-  static std::string version();                         ///< The full version number
-  static std::string versionShort();                    ///< The version number of the last full version
+  static const std::string &version();                  ///< The full version number
+  static const std::string &versionShort();             ///< The version number of the last full version
   static const VersionInfo versionInfo();               ///< A data structure containing the full version info.
   static std::string releaseNotes();                    ///< The url to the most applicable release notes
-  static std::string revision();                        ///< The abbreviated SHA-1 of the last commit
-  static std::string revisionFull();                    ///< The full SHA-1 of the last commit
+  static const std::string &revision();                 ///< The abbreviated SHA-1 of the last commit
+  static const std::string &revisionFull();             ///< The full SHA-1 of the last commit
   static Types::Core::DateAndTime releaseDateAndTime(); ///< The DateAndTime of the last commit
   static std::string releaseDate();                     ///< The date of the last commit
   static std::string doi();                             ///< The DOI for this release of Mantid.
-  static std::string paperCitation();                   ///< The citation for the Mantid paper
+  static const std::string &paperCitation();            ///< The citation for the Mantid paper
   static std::string versionForReleaseNotes(const VersionInfo &); ///< The version of mantid for the release notes url.
 
 private:
