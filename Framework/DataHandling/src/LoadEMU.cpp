b58cd4318c0ee482fb24190b978101c8a3545b34
rboston628 <bostonsr@ornl.gov>
2026-01-08 13:29:51 -0500

    small fixes

--- Framework/DataHandling/src/LoadEMU.cpp
+++ Framework/DataHandling/src/LoadEMU.cpp
@@ -1359,6 +1359,6 @@ void LoadEMUTar::exec() {
 
 // register the algorithms into the AlgorithmFactory
 DECLARE_FILELOADER_ALGORITHM(LoadEMUTar)
-// DECLARE_NEXUS_LAZY_FILELOADER_ALGORITHM(LoadEMUHdf)
+DECLARE_NEXUS_LAZY_FILELOADER_ALGORITHM(LoadEMUHdf)
 
 } // namespace Mantid::DataHandling
