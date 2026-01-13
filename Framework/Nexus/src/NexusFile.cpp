78379a842790cadf16810f3f4bf96033533e4b06
Reece Boston <52183986+rboston628@users.noreply.github.com>
2026-01-13 17:36:14 -0500

    Migrate `LoadMD` for lazy file descriptor (#40601)
    In PR #40570, a new file descriptor, `NexusDescriptorLazy`, was created
    for shallow and lazy checking of files as part of the `Load` algorithm's
    confidence check.
    
    Many algorithms could be migrated mechanically, as in PR #40583.
    
    This PR is migrating the algorithm `LoadMD` algorithm. This inherited
    from `NexusFileLoader`, which requires a `NexusDescriptor` for the
    confidence check. This object was being passed down multiple layers of
    loading objects. Most of the work of this PR was undoing the dependency
    on this object as a parameter.
    
    This adds a new method to `Nexus::File` which returns all of the entries
    with a given class name. That, plus the already-existing
    `Nexus::File::getEntries()` method are all that are needed at the
    various levels of this load process. Now the `NexusDecriptor` is no
    longer needed and this can be set to inherit from
    `IFileLoader<NexusDescriptorLazy>`.
    
    Related to Issue #37164 
    [EWM
    14485](https://ornlrse.clm.ibmcloud.com/ccm/web/projects/Neutron%20Data%20Project%20%28Change%20Management%29#action=com.ibm.team.workitem.viewWorkItem&id=14485)
    
    ### To test:
    
    :warning: This PR has the potential to destroy everything. **Review
    carefully** :warning:
    
    Firstly, this has the potential to make the `Run` object disappear.
    Build and run this script:
    ``` python
    from mantid.simpleapi import CreateMDWorkspace, SaveMD, LoadMD, AddSampleLog
    # create the file
    filename = '/tmp/test_loadmd_log.nxs'
    mdws = CreateMDWorkspace(Dimensions=3, Extents='-10,10,-10,10,-10,10', Names='A,B,C', Units='U,U,U')
    AddSampleLog(Workspace=mdws, LogName='x', LogText='hello world', LogType='String')
    # it has a log
    assert len(mdws.getExperimentInfo(0).run().keys()) != 0
    # save the file
    SaveMD(mdws, Filename=filename)
    # reload the file
    outws = LoadMD(Filename=filename)
    # check
    assert len(outws.getExperimentInfo(0).run().keys()) != 0
    ``` 
    That should ensure that the run is loaded. Or, run the reproduction
    script at Issue #39954 for a real file.
    
    Also, make sure there has been no performance regression. Run this
    script:
    ``` python
    from mantid.simpleapi import *
    import mantid
    import time
    import os
    import matplotlib.pyplot as plt
    
    sizes = ['4.64527e+05', '1.64527e+06', '4.64527e+06', '1.64527e+07', '4.64527e+07', '1.64527e+08']
    save_times = []
    load_times = []
    workspace_sizes = []
    memories = []
    
    kbToGB = 1024 * 1024
    
    for size in sizes:
        CreateMDWorkspace(
            Dimensions=4,
            Extents='-10,10,-10,10,-10,10,-10,10',
            Names='a,b,c,d', Units='rlu,rlu,rlu,meV',
            MaxRecursionDepth=1,
            OutputWorkspace='w_new_slice')
        FakeMDEventData(InputWorkspace='w_new_slice', UniformParams=size)
    
        output_filename = f'~/Documents/dm1_sliced_{mantid.__version__}.nxs'
        start_time = time.time()
        SaveMD(InputWorkspace='w_new_slice', Filename=output_filename)
        save_times.append(time.time() - start_time)
        workspace_sizes.append(mtd["w_new_slice"].getMemorySize()/kbToGB)
        start_time = time.time()
        LoadMD(Filename=output_filename, OutputWorkspace = "reloaded_ws")
        load_times.append(time.time() - start_time)
        memories.append(os.path.getsize(os.path.expanduser(output_filename))/kbToGB)
    
    plt.xscale('log')
    plt.yscale('log')
    plt.plot(workspace_sizes, save_times, "go-", label="SaveMD")
    plt.plot(workspace_sizes, load_times, "bo-", label="LoadMD")
    plt.xlabel("workspace size")
    plt.ylabel("time")
    plt.title("Time for SaveMD/LoadMD")
    x = np.linspace(workspace_sizes[0], workspace_sizes[-1], num=100)
    plt.plot(x, 3.e-3 * x, label="O(N)")
    plt.plot(x, 3.e-4 * x * x, label="O(N^2)")
    plt.legend()
    plt.show()
    
    ```
    If this worked correctly, then the load time line on the graph should be
    parallel to the $O(N)$ line.
    
    Next, build and run the `LoadLotsOfFile` system test. This does not run
    on Jenkins, so you will need to run it manually.
    ```
    ninja Framework && ./systemtest -R LoadLotsOfFiles
    ```

--- Framework/Nexus/src/NexusFile.cpp
+++ Framework/Nexus/src/NexusFile.cpp
@@ -47,7 +47,6 @@ const auto g_log = &Poco::Logger::get("NexusFile");
 namespace { // anonymous namespace to keep it in the file
 
 std::string const TARGET_ATTR_NAME("target");
-std::string const UNKNOWN_GROUP_SPEC("NX_UNKNOWN_GROUP");
 constexpr int DEFAULT_DEFLATE_LEVEL(6);
 
 template <typename NumT> static string toString(const vector<NumT> &data) {
