69402fc236dcb758d5ad548a286d651198eedfa4
Daniel Caballero <132241327+dlcaballero16@users.noreply.github.com>
2026-01-21 02:52:09 -0500

    Prepopulate fields for HFIRPowderReduction (#40580)
    ### Description of work
    
    This work modifies HFIRPowderReduction so that when a user updates a
    field, other fields will automatically get updated based on what the
    user input. This will also attempt to read the experiment run files to
    grab data to set the fields. For example, when the user fills out the
    SampleFileName field with a valid file, the algorithm will attempt to
    read the sample file and set the Instrument field. All cases will be
    described in the testing section.
    
    EWM
    [13209](https://ornlrse.clm.ibmcloud.com/ccm/web/projects/Neutron%20Data%20Project%20%28Change%20Management%29#action=com.ibm.team.workitem.viewWorkItem&id=13209)
    
    <!-- If issue raised by user. Do not leak email addresses.
    **Report to:** [user name]
    -->
    
    Note: there is currently an issue with prepopulating not overwriting
    fields if there is saved data already there. It will be fixed by [this
    EWM
    item](https://ornlrse.clm.ibmcloud.com/ccm/web/projects/Neutron%20Data%20Project%20%28Change%20Management%29#action=com.ibm.team.workitem.viewWorkItem&id=14694)
    
    ### To test:
    
    Open the HFIRPowderReduction algorithm in Mantid and set the
    SampleFileName to the file that gets generated with the attached script.
    You must generate the file with the attached script as github won't let
    me attach a .h5 file.
    
    [generate_test_h5.py](https://github.com/user-attachments/files/24675283/generate_test_h5.py)
    
     Make sure the following fields get set:
    
    Instrument
    Wavelength
    Vanadium diameter
    Vanadium IPTS and run numbers
    Vanadium bg. IPTS and run numbers
    
    Toggle the instrument and notice that the NormalizeBy field gets
    updated. Make sure that it matches like so:
    
    Wand^2 <-> Monitor
    MIDAS <-> Time
    
    Also make sure that you can overwrite any value that got prepopulated.
    
    The user should also be able to set the Sample Run and IPTS numbers to
    prepopulate the same fields, but this requires run files to exist in a
    specific location. Since there are no real sample run files yet, just
    check the unit tests and make sure they test for this and that they
    pass.

--- Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/HFIRPowderReduction.py
+++ Framework/PythonInterface/plugins/algorithms/WorkflowAlgorithms/HFIRPowderReduction.py
@@ -11,8 +11,14 @@ from mantid.kernel import (
     Property,
     FloatBoundedValidator,
     IntArrayProperty,
+    SetDefaultWhenProperty,
+    Logger,
 )
 from mantid.dataobjects import MaskWorkspaceProperty
+import h5py
+import numpy as np
+
+logger = Logger(__name__)
 
 
 class HFIRPowderReduction(DataProcessorAlgorithm):
@@ -25,7 +31,7 @@ class HFIRPowderReduction(DataProcessorAlgorithm):
     def summary(self):
         return "Powder reduction for HFIR instruments"
 
-    def PyInit(self):
+    def PyInit(self):  # noqa: C901
         # Load UI properties
         self.declareProperty(
             MultipleFileProperty(name="SampleFilename", action=FileAction.OptionalLoad, extensions=[".nxs.h5"]), "Sample files to Load"
@@ -125,7 +131,284 @@ class HFIRPowderReduction(DataProcessorAlgorithm):
             doc="Specifies either single output workspace or output group workspace containing several workspaces.",
         )
 
-    def validateInputs(self):
+        def readIntFromFile(filename, dataset):
+            intValue = 0
+            with h5py.File(filename, "r") as f:
+                intValue = f[dataset][0]
+                intValue = intValue.decode("utf-8")
+                intValue = int(intValue)
+            return intValue
+
+        def readFloatFromFile(filename, dataset):
+            floatValue = 0.0
+            with h5py.File(filename, "r") as f:
+                floatValue = f[dataset][0]
+                floatValue = floatValue.decode("utf-8")
+                floatValue = float(floatValue)
+            return floatValue
+
+        def readArrayFromFile(filename, dataset):
+            array = np.array([])
+            with h5py.File(filename, "r") as f:
+                array = f[dataset][()]
+            return array
+
+        def checkFilenameforInstrument(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            instrumentName = ""
+            with h5py.File(run[0], "r") as f:
+                instrumentName = f["/entry/instrument/name"][0]
+                instrumentName = instrumentName.decode("utf-8")
+            if "WAND" in instrumentName:
+                self.setProperty("Instrument", "WAND^2")
+            elif "MIDAS" in instrumentName:
+                self.setProperty("Instrument", "MIDAS")
+            return True
+
+        self.setPropertySettings("Instrument", SetDefaultWhenProperty("SampleFilename", checkFilenameforInstrument))
+
+        def checkFilenameforWavelength(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            if not run:
+                return False
+            wavelength = 0.0
+            wavelength = readFloatFromFile(run[0], "/entry/wavelength")
+            logger.information(f"Auto-populating Wavelength: {wavelength} from file: {run[0]}")
+            currentProp.value = wavelength
+            return True
+
+        self.setPropertySettings("Wavelength", SetDefaultWhenProperty("SampleFilename", checkFilenameforWavelength))
+
+        def checkFilenameforVanadiumDiameter(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            if not run:
+                return False
+            diameter = 0.0
+            diameter = readFloatFromFile(run[0], "/entry/vanadium_diameter")
+            logger.information(f"Auto-populating VanadiumDiameter: {diameter} from file: {run[0]}")
+            currentProp.value = diameter
+            return True
+
+        self.setPropertySettings("VanadiumDiameter", SetDefaultWhenProperty("SampleFilename", checkFilenameforVanadiumDiameter))
+
+        def checkInstrumentforNormaliseBy(algo, currentProp, watchedProp):
+            instrument = watchedProp.value
+            if instrument == "WAND^2":
+                currentProp.value = "Monitor"
+            elif instrument == "MIDAS":
+                currentProp.value = "Time"
+            return True
+
+        self.setPropertySettings("NormaliseBy", SetDefaultWhenProperty("Instrument", checkInstrumentforNormaliseBy))
+
+        def checkFilenameforVanadiumRunNumbers(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            if not run:
+                return False
+            vanadiumRunNumbers = readArrayFromFile(run[0], "/entry/vanadium_run_numbers")
+            logger.information(f"Auto-populating VanadiumRunNumbers: {vanadiumRunNumbers} from file: {run[0]}")
+            self.setProperty("VanadiumRunNumbers", vanadiumRunNumbers)
+            return True
+
+        self.setPropertySettings("VanadiumRunNumbers", SetDefaultWhenProperty("SampleFilename", checkFilenameforVanadiumRunNumbers))
+
+        def checkFilenameforVanadiumIPTS(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            if not run:
+                return False
+            vanadiumIPTS = readIntFromFile(run[0], "/entry/vanadium_ipts")
+            logger.information(f"Auto-populating VanadiumIPTS: {vanadiumIPTS} from file: {run[0]}")
+            self.setProperty("VanadiumIPTS", vanadiumIPTS)
+            return True
+
+        self.setPropertySettings("VanadiumIPTS", SetDefaultWhenProperty("SampleFilename", checkFilenameforVanadiumIPTS))
+
+        def checkFilenameforVanadiumBackgroundIPTS(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            if not run:
+                return False
+            vanadiumBGIPTS = readIntFromFile(run[0], "/entry/vanadium_background_ipts")
+            logger.information(f"Auto-populating VanadiumBackgroundIPTS: {vanadiumBGIPTS} from file: {run[0]}")
+            self.setProperty("VanadiumBackgroundIPTS", vanadiumBGIPTS)
+            return True
+
+        self.setPropertySettings("VanadiumBackgroundIPTS", SetDefaultWhenProperty("SampleFilename", checkFilenameforVanadiumBackgroundIPTS))
+
+        def checkFilenameforVanadiumBackgroundRunNumbers(algo, currentProp, watchedProp):
+            run = watchedProp.value
+            if not run:
+                return False
+            vanadiumBGRunNumbers = readArrayFromFile(run[0], "/entry/vanadium_background_run_numbers")
+            logger.information(f"Auto-populating VanadiumBackgroundRunNumbers: {vanadiumBGRunNumbers} from file: {run[0]}")
+            self.setProperty("VanadiumBackgroundRunNumbers", vanadiumBGRunNumbers)
+            return True
+
+        self.setPropertySettings(
+            "VanadiumBackgroundRunNumbers", SetDefaultWhenProperty("SampleFilename", checkFilenameforVanadiumBackgroundRunNumbers)
+        )
+
+        # Helper function to get sample runs based on IPTS and run numbers
+        # Note: this should never be refactored to use FileFinder as we want to force the user to set the IPTS number when using run numbers
+        def getRuns():
+            runs = []
+            sampleIPTS = self.getProperty("SampleIPTS").value
+            sampleRunNumbers = self.getProperty("SampleRunNumbers").value
+            instrument = self.getProperty("Instrument").value
+            if sampleIPTS == Property.EMPTY_INT or len(sampleRunNumbers) == 0 or instrument == "":
+                return runs
+            else:
+                if instrument == "WAND^2":
+                    runs = ["/HFIR/HB2C/IPTS-{}/nexus/HB2C_{}.nxs.h5".format(sampleIPTS, run) for run in sampleRunNumbers]
+                elif instrument == "MIDAS":
+                    runs = ["/HFIR/HB2A/IPTS-{}/nexus/HB2A_{}.nxs.h5".format(sampleIPTS, run) for run in sampleRunNumbers]
+            return runs
+
+        def checkRunNumbersforWavelength(algo, currentProp, watchedProp):
+            runs = getRuns()
+            if len(runs) == 0:
+                return False
+            else:
+                wavelength = 0.0
+                wavelength = readFloatFromFile(runs[0], "/entry/wavelength")
+                logger.information(f"Auto-populating Wavelength: {wavelength} from run numbers: {runs[0]}")
+                currentProp.value = wavelength
+            return True
+
+        self.setPropertySettings("Wavelength", SetDefaultWhenProperty("Instrument", checkRunNumbersforWavelength))
+        self.setPropertySettings("Wavelength", SetDefaultWhenProperty("SampleIPTS", checkRunNumbersforWavelength))
+        self.setPropertySettings("Wavelength", SetDefaultWhenProperty("SampleRunNumbers", checkRunNumbersforWavelength))
+
+        def checkRunNumbersforVanadiumDiameter(algo, currentProp, watchedProp):
+            runs = getRuns()
+            if len(runs) == 0:
+                return False
+            else:
+                diameter = 0.0
+                diameter = readFloatFromFile(runs[0], "/entry/vanadium_diameter")
+                logger.information(f"Auto-populating VanadiumDiameter: {diameter} from run numbers: {runs[0]}")
+                currentProp.value = diameter
+
+            return True
+
+        self.setPropertySettings("VanadiumDiameter", SetDefaultWhenProperty("Instrument", checkRunNumbersforVanadiumDiameter))
+        self.setPropertySettings("VanadiumDiameter", SetDefaultWhenProperty("SampleIPTS", checkRunNumbersforVanadiumDiameter))
+        self.setPropertySettings("VanadiumDiameter", SetDefaultWhenProperty("SampleRunNumbers", checkRunNumbersforVanadiumDiameter))
+
+        def checkRunNumbersforVanadiumRunNumbers(algo, currentProp, watchedProp):
+            runs = getRuns()
+            if len(runs) == 0:
+                return False
+            else:
+                vanadiumRunNumbers = readArrayFromFile(runs[0], "/entry/vanadium_run_numbers")
+                logger.information(f"Auto-populating VanadiumRunNumbers: {vanadiumRunNumbers} from run numbers: {runs[0]}")
+                self.setProperty("VanadiumRunNumbers", vanadiumRunNumbers)
+
+            return True
+
+        self.setPropertySettings("VanadiumRunNumbers", SetDefaultWhenProperty("Instrument", checkRunNumbersforVanadiumRunNumbers))
+        self.setPropertySettings("VanadiumRunNumbers", SetDefaultWhenProperty("SampleIPTS", checkRunNumbersforVanadiumRunNumbers))
+        self.setPropertySettings("VanadiumRunNumbers", SetDefaultWhenProperty("SampleRunNumbers", checkRunNumbersforVanadiumRunNumbers))
+
+        def checkRunNumbersforVanadiumIPTS(algo, currentProp, watchedProp):
+            runs = getRuns()
+            if len(runs) == 0:
+                return False
+            else:
+                vanadiumIPTS = readIntFromFile(runs[0], "/entry/vanadium_ipts")
+                logger.information(f"Auto-populating VanadiumIPTS: {vanadiumIPTS} from run numbers: {runs[0]}")
+                self.setProperty("VanadiumIPTS", vanadiumIPTS)
+
+            return True
+
+        self.setPropertySettings("VanadiumIPTS", SetDefaultWhenProperty("Instrument", checkRunNumbersforVanadiumIPTS))
+        self.setPropertySettings("VanadiumIPTS", SetDefaultWhenProperty("SampleIPTS", checkRunNumbersforVanadiumIPTS))
+        self.setPropertySettings("VanadiumIPTS", SetDefaultWhenProperty("SampleRunNumbers", checkRunNumbersforVanadiumIPTS))
+
+        def checkRunNumbersforVanadiumBackgroundRunNumbers(algo, currentProp, watchedProp):
+            runs = getRuns()
+            if len(runs) == 0:
+                return False
+            else:
+                vanadiumBGRunNumbers = readArrayFromFile(runs[0], "/entry/vanadium_background_run_numbers")
+                logger.information(f"Auto-populating VanadiumBackgroundRunNumbers: {vanadiumBGRunNumbers} from run numbers: {runs[0]}")
+                self.setProperty("VanadiumBackgroundRunNumbers", vanadiumBGRunNumbers)
+
+            return True
+
+        self.setPropertySettings(
+            "VanadiumBackgroundRunNumbers", SetDefaultWhenProperty("Instrument", checkRunNumbersforVanadiumBackgroundRunNumbers)
+        )
+        self.setPropertySettings(
+            "VanadiumBackgroundRunNumbers", SetDefaultWhenProperty("SampleIPTS", checkRunNumbersforVanadiumBackgroundRunNumbers)
+        )
+        self.setPropertySettings(
+            "VanadiumBackgroundRunNumbers", SetDefaultWhenProperty("SampleRunNumbers", checkRunNumbersforVanadiumBackgroundRunNumbers)
+        )
+
+        def checkRunNumbersforVanadiumBackgroundIPTS(algo, currentProp, watchedProp):
+            runs = getRuns()
+            if len(runs) == 0:
+                return False
+            else:
+                vanadiumBGIPTS = readIntFromFile(runs[0], "/entry/vanadium_background_ipts")
+                logger.information(f"Auto-populating VanadiumBackgroundIPTS: {vanadiumBGIPTS} from run numbers: {runs[0]}")
+                self.setProperty("VanadiumBackgroundIPTS", vanadiumBGIPTS)
+
+            return True
+
+        self.setPropertySettings("VanadiumBackgroundIPTS", SetDefaultWhenProperty("Instrument", checkRunNumbersforVanadiumBackgroundIPTS))
+        self.setPropertySettings("VanadiumBackgroundIPTS", SetDefaultWhenProperty("SampleIPTS", checkRunNumbersforVanadiumBackgroundIPTS))
+        self.setPropertySettings(
+            "VanadiumBackgroundIPTS", SetDefaultWhenProperty("SampleRunNumbers", checkRunNumbersforVanadiumBackgroundIPTS)
+        )
+
+    def _checkMetadataConsistency(self, files, field_name):
+        """Check that metadata is consistent across multiple run files."""
+        if len(files) <= 1:
+            return None
+
+        metadata_fields = [
+            "/entry/wavelength",
+            "/entry/vanadium_diameter",
+            "/entry/vanadium_run_numbers",
+            "/entry/vanadium_ipts",
+            "/entry/vanadium_background_ipts",
+            "/entry/vanadium_background_run_numbers",
+        ]
+
+        inconsistencies = []
+        for metadata_field in metadata_fields:
+            reference_value = None
+            try:
+                with h5py.File(files[0], "r") as f:
+                    if metadata_field in f:
+                        reference_value = f[metadata_field][()]
+            except (OSError, KeyError):
+                continue
+
+            if reference_value is None:
+                continue
+
+            for i, file in enumerate(files[1:], start=1):
+                try:
+                    with h5py.File(file, "r") as f:
+                        if metadata_field in f:
+                            current_value = f[metadata_field][()]
+                            # Compare values, handling numpy arrays
+                            if isinstance(reference_value, np.ndarray) and isinstance(current_value, np.ndarray):
+                                if not np.array_equal(reference_value, current_value):
+                                    inconsistencies.append(f"{metadata_field}: file 0 has {reference_value}, file {i} has {current_value}")
+                            elif reference_value != current_value:
+                                inconsistencies.append(f"{metadata_field}: file 0 has {reference_value}, file {i} has {current_value}")
+                except (OSError, KeyError):
+                    continue
+
+        if inconsistencies:
+            error_msg = f"Metadata mismatch in {field_name}: " + "; ".join(inconsistencies)
+            logger.warning(error_msg)
+        return None
+
+    def validateInputs(self):  # noqa: C901
         issues = dict()
 
         fileNameBool = self.getProperty("SampleFilename").value
@@ -190,6 +473,25 @@ class HFIRPowderReduction(DataProcessorAlgorithm):
         vanadiumDiameter = self.getProperty("VanadiumDiameter").value
         if vanadiumDiameter == Property.EMPTY_DBL:
             issues["VanadiumDiameter"] = "VanadiumDiameter must be provided"
+
+        # Check metadata consistency for multiple sample runs
+        files = []
+        filenames = self.getProperty("SampleFilename").value
+        if filenames:
+            files = list(filenames)
+        else:
+            # Build file list from IPTS and run numbers
+            ipts = self.getProperty("SampleIPTS").value
+            run_numbers = self.getProperty("SampleRunNumbers").value
+            instrument = self.getProperty("Instrument").value
+            if ipts != Property.EMPTY_INT and len(run_numbers) > 0 and instrument:
+                if instrument == "WAND^2":
+                    files = [f"/HFIR/HB2C/IPTS-{ipts}/nexus/HB2C_{run}.nxs.h5" for run in run_numbers]
+                elif instrument == "MIDAS":
+                    files = [f"/HFIR/HB2A/IPTS-{ipts}/nexus/HB2A_{run}.nxs.h5" for run in run_numbers]
+
+        if len(files) > 1:
+            self._checkMetadataConsistency(files, "Sample")
 
         return issues
 
