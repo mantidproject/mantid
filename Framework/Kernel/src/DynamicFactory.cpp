51c2d6a7150fd0a6eeaa0a1b32ab93a635b0cbb8
Ross Whitfield <whitfieldre1@ornl.gov>
2026-01-22 15:05:39 +1100

    Fix DynamicFactory

---
+++ Framework/Kernel/src/DynamicFactory.cpp
@@ -0,0 +1,15 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2018 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidKernel/DynamicFactory.h"
+
+namespace Mantid::Kernel {
+
+DynamicFactoryNotification::~DynamicFactoryNotification() = default;
+
+DynamicFactoryUpdateNotification::~DynamicFactoryUpdateNotification() = default;
+
+} // namespace Mantid::Kernel
