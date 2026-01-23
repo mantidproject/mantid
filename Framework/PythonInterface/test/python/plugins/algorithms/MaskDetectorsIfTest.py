8912a670608de7866e30716bceb3ba027aafc6ca
RabiyaF <47083562+RabiyaF@users.noreply.github.com>
2026-01-23 19:37:38 +0000

    Added the GenerateFlatCellWorkspaceLOQ algorithm for creating a workspace for ISIS SANS reduction of LOQ (#40429)
    ### Description of work
    
    <!-- Please provide an outline and reasoning for the work.
    If there is no linked issue provide context.
    -->
    
    Closes #40299 . <!-- One line per closed issue. -->
    
    Added the `GenerateFlatCellWorkspaceLOQ` algorithm that works with SANS
    event workspaces. The user can output the flat cell workspace and a mask
    workspace. The user also has the option to directly apply the mask to
    the output. An xml file can also created with the detector id to mask.
    
    Also updated the **MaskDetectorsIf** algorithm to provide two additional
    properties to specify the spectrum range in which to apply the
    conditional mask on.
    
    <!-- If issue raised by user. Do not leak email addresses.
    **Report to:** [user name]
    -->
    
    ### To test:
    1) LoadNexus(Filename="LOQ00113953.nxs", OutputWorkspace="input")
    2) GenerateFlatCellWorkspaceLOQ(InputWorkspace="input",
    OutputWorkspace="output")
    3) Verify that two new workspaces are created called **output** and
    **output_MASK**.
    
    <!-- Include sufficient instructions for someone unfamiliar with the
    application to test.
    Ok to refer back to instructions in the issue.
    -->
    
    <!-- REMEMBER:
    - Add labels, milestones, etc.
    - Ensure the base of this PR is correct (e.g. release-next or main)
    - Add release notes in separate file as per
    ([guidelines](https://developer.mantidproject.org/Standards/ReleaseNotesGuide.html)),
    or justify their absence:
    *This does not require release notes* because <fill in an explanation of
    why>
    -->

---
+++ Framework/PythonInterface/test/python/plugins/algorithms/MaskDetectorsIfTest.py
@@ -0,0 +1,54 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2026 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+import unittest
+from mantid.simpleapi import MaskDetectorsIf, CreateWorkspace
+
+
+class MaskDetectorsIfInputTest(unittest.TestCase):
+    def testValidateStartWorkspaceIndexInputs(self):
+        # StartWorkspaceIndex > Number of Histograms
+        ws = CreateWorkspace(DataX=[0, 1], DataY=[1] * 10, NSpec=10)
+        with self.assertRaises(ValueError) as context:
+            MaskDetectorsIf(
+                InputWorkspace=ws,
+                OutputWorkspace=ws,
+                StartWorkspaceIndex=80,
+            )
+        self.assertIn(
+            "StartWorkspaceIndex should be greater than or equal to 0 and less than 9. Value provided is invalid.", str(context.exception)
+        )
+
+    def testValidateEndWorkspaceIndexInputs(self):
+        # EndWorkspaceIndex > Number of Histograms
+        ws = CreateWorkspace(DataX=[0, 1], DataY=[1] * 10, NSpec=10)
+        with self.assertRaises(ValueError) as context:
+            MaskDetectorsIf(
+                InputWorkspace=ws,
+                OutputWorkspace=ws,
+                StartWorkspaceIndex=8,
+                EndWorkspaceIndex=50,
+            )
+        self.assertIn("EndWorkspaceIndex should be greater than 0 and less than 10. Value provided is invalid.", str(context.exception))
+
+    def testValidateInputs(self):
+        # EndWorkspaceIndex < StartWorkspaceIndex
+        ws = CreateWorkspace(DataX=[0, 1], DataY=[1] * 10, NSpec=10)
+        with self.assertRaises(RuntimeError) as context:
+            MaskDetectorsIf(
+                InputWorkspace=ws,
+                OutputWorkspace=ws,
+                StartWorkspaceIndex=8,
+                EndWorkspaceIndex=5,
+            )
+        self.assertIn(
+            "EndWorkspaceIndex: EndWorkspaceIndex should be more than StartWorkspaceIndex. Specify a value greater than 8.",
+            str(context.exception),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
