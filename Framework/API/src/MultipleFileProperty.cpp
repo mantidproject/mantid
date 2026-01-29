46670c33f7604365ced34e4bb8e52fa94de1b70e
Sam Tygier <sam.tygier@stfc.ac.uk>
2026-01-29 15:25:53 +0000

    MultipleFileProperty update regexs to handle spaces better

--- Framework/API/src/MultipleFileProperty.cpp
+++ Framework/API/src/MultipleFileProperty.cpp
@@ -44,14 +44,14 @@ const std::string INVALID = R"(\+\+|,,|\+,|,\+)";
 static const boost::regex REGEX_INVALID(INVALID);
 
 // Regular expressions that represent the allowed instances of , operators
-const std::string NUM_COMMA_ALPHA(R"((?<=\d)\s*,\s*(?=\D))");
-const std::string ALPHA_COMMA_ALPHA(R"((?<=\D)\s*,\s*(?=\D))");
+const std::string NUM_COMMA_ALPHA(R"((?<=\d)\s*,\s*(?=[a-zA-Z]))");
+const std::string ALPHA_COMMA_ALPHA(R"((?<=\D)\s*,\s*(?=[a-zA-Z]))");
 const std::string COMMA_OPERATORS = NUM_COMMA_ALPHA + "|" + ALPHA_COMMA_ALPHA;
 static const boost::regex REGEX_COMMA_OPERATORS(COMMA_OPERATORS);
 
 // Regular expressions that represent the allowed instances of + operators
-const std::string NUM_PLUS_ALPHA(R"((?<=\d)\s*\+\s*(?=\D))");
-const std::string ALPHA_PLUS_ALPHA(R"((?<=\D)\s*\+\s*(?=\D))");
+const std::string NUM_PLUS_ALPHA(R"((?<=\d)\s*\+\s*(?=[a-zA-Z]))");
+const std::string ALPHA_PLUS_ALPHA(R"((?<=\D)\s*\+\s*(?=[a-zA-Z]))");
 const std::string PLUS_OPERATORS = NUM_PLUS_ALPHA + "|" + ALPHA_PLUS_ALPHA;
 static const boost::regex REGEX_PLUS_OPERATORS(PLUS_OPERATORS, boost::regex_constants::perl);
 
