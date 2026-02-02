ce105bf8442c2dc4344f8dc9bf074a752a5dcf4b
adriazalvarez <adrian.diaz-alvarez@stfc.ac.uk>
2026-02-02 12:55:27 +0000

    Add tests

--- qt/scientific_interfaces/Inelastic/test/BayesFitting/StretchPresenterTest.h
+++ qt/scientific_interfaces/Inelastic/test/BayesFitting/StretchPresenterTest.h
@@ -91,7 +91,7 @@ public:
     configSvc.setString("defaultsave.directory", "/test/test");
     ON_CALL(*m_view, displaySaveDirectoryMessage()).WillByDefault(Return(false));
 
-    StretchRunData runData("sample_ws", "res_ws", -0.5, 0.5, 50, true, "flat", 30, 1, true);
+    StretchRunData runData("sample_ws", "res_ws", "flat", -0.5, 0.5, 50, 30, true);
 
     ON_CALL(*m_view, getRunData(false)).WillByDefault(Return(runData));
 
@@ -103,7 +103,7 @@ public:
   }
 
   void test_handleRun_with_valid_input_and_savedir() {
-    StretchRunData runData("sample_ws", "res_ws", -0.5, 0.5, 50, true, "flat", 30, 1, true);
+    StretchRunData runData("sample_ws", "res_ws", "flat", -0.5, 0.5, 50, 30, true);
 
     ON_CALL(*m_view, getRunData(false)).WillByDefault(Return(runData));
     EXPECT_CALL(*m_view, setPlotADSEnabled(false)).Times(1);
@@ -114,7 +114,7 @@ public:
   }
 
   void test_notifySaveClicked_with_output_workspaces() {
-    StretchRunData runData("sample_ws", "res_ws", -0.5, 0.5, 50, true, "flat", 30, 1, true);
+    StretchRunData runData("sample_ws", "res_ws", "flat", -0.5, 0.5, 50, 30, true);
 
     auto const cutIndex = runData.sampleName.find_last_of("_");
     auto const baseName = runData.sampleName.substr(0, cutIndex);
@@ -151,7 +151,7 @@ public:
   }
 
   void test_notifyBackendChanged_changes_stretchAlgorithm_call() {
-    StretchRunData runData("sample_ws", "res_ws", -0.5, 0.5, 50, true, "flat", 30, 1, true);
+    StretchRunData runData("sample_ws", "res_ws", "flat", -0.5, 0.5, 50, 30, true);
 
     EXPECT_CALL(*m_view, getRunData(true)).Times(1);
     ON_CALL(*m_view, getRunData(true)).WillByDefault(Return(runData));
