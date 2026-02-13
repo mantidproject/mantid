3dca10405192da08b22b35725876b047bcacb624
Mial Lewis <mial.lewis@stfc.ac.uk>
2026-02-13 14:45:11 +0000

    tweak warning messages

--- qt/scientific_interfaces/Inelastic/BayesFitting/BayesBackendType.h
+++ qt/scientific_interfaces/Inelastic/BayesFitting/BayesBackendType.h
@@ -5,11 +5,17 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
+#include <QString>
 
 namespace MantidQt {
 namespace CustomInterfaces {
 
 enum class BayesBackendType { QUASI_ELASTIC_BAYES, QUICK_BAYES };
+
+std::unordered_map<BayesBackendType, QString> const backendToQStr{
+    {BayesBackendType::QUASI_ELASTIC_BAYES, QString("quasielasticbayes")},
+    {BayesBackendType::QUICK_BAYES, QString("quickbayes")},
+};
 
 } // namespace CustomInterfaces
 } // namespace MantidQt
