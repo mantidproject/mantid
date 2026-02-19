aef9e2b07a77d62ed6cabe52196eeea947136200
Carson Sears <searscr@ornl.gov>
2026-02-19 09:28:10 -0500

    Documentation and create mantidmpi package

--- conda/recipes/mantid/build.bat
+++ conda/recipes/mantid/build.bat
@@ -6,6 +6,10 @@ CALL "%parent_dir%\archive_env_logs.bat" %BUILD_PREFIX% %PREFIX% mantid
 :: Specify MSVC version to avoid bug in 14.44
 CALL "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.38.17.8
 
+:: Determine MPI build flag (mantidmpi is skipped on Windows, but keep consistent)
+set "MPI_BUILD_FLAG=OFF"
+if "%PKG_NAME%"=="mantidmpi" set "MPI_BUILD_FLAG=ON"
+
 mkdir -p build && cd build
 
 cmake ^
@@ -20,6 +24,7 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DMPI_BUILD=%MPI_BUILD_FLAG% ^
     ..
 
 if errorlevel 1 exit 1
