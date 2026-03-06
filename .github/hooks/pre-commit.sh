97792ac61b0e3bee2654450d57a980781e9a84ad
Pete Peterson <petersonpf@ornl.gov>
2026-03-06 14:49:38 -0500

    Respond to copilot review comments

--- .github/hooks/pre-commit.sh
+++ .github/hooks/pre-commit.sh
@@ -1,15 +1,17 @@
-changed_files=$(git diff --name-only)
+# changed files that aren't deleted
+changed_files=$(git diff --diff-filter=d --name-only)
 if [ -n "${changed_files}" ]; then
   # run pre-commit and capture the return
-  pre-commit run --files ${changed_files}; rc=$?
+  pre-commit run --files -- "${changed_files[@]}"; rc=$?
   # do something special if nonzero return
   if [ ${rc} -ne 0 ]; then
-    if git diff --quiet; then
-      # return pre-commit's result if no files were changed
-      exit ${rc}
-    else
+    # this if is intentionally backwards
+    if ! git diff --quiet; then
       # return zero if files were changed - copilot will see those changes
       exit 0
+    else
+      # return pre-commit's result if no files were changed
+      exit ${rc}
     fi
   fi
 fi
