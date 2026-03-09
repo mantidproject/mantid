660d5a53779a714a2c65791aff1e52ec942a273e
RabiyaF <47083562+RabiyaF@users.noreply.github.com>
2026-03-09 17:21:54 +0000

    Updated SaveSESANS and SaveCanSAS1D to include the decimal point when the value written in the .ses file is an integer (#40975)
    ### Description of work
    
    Updated the`SaveSESANS` and `SaveCanSAS1D` algorithms to include the
    decimal point if the data written to file is an integer.
    
    **Note:** `SaveNXcanSAS` and `SaveRKH` don't need an update based on my
    testing.
    
    Co-authored-by: Waruna Priyankara J A Wickramasingha <142818102+warunawickramasingha@users.noreply.github.com>
    Co-authored-by: Caila Finn <47181718+cailafinn@users.noreply.github.com>

---
+++ Framework/DataHandling/inc/MantidDataHandling/FormattingHelpers.h
@@ -0,0 +1,17 @@
+// Mantid Repository : https://github.com/mantidproject/mantid
+//
+// Copyright &copy; 2026 ISIS Rutherford Appleton Laboratory UKRI,
+//   NScD Oak Ridge National Laboratory, European Spallation Source,
+//   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+// SPDX - License - Identifier: GPL - 3.0 +
+#pragma once
+#include "MantidDataHandling/DllConfig.h"
+#include <string>
+
+namespace Mantid {
+namespace DataHandling {
+
+MANTID_DATAHANDLING_DLL std::string formatDouble(double const value);
+
+} // namespace DataHandling
+} // namespace Mantid
