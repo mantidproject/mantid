6046a20b2089830f773af5188058ba871b8550ab
Mohammed Almakki <44416400+MohamedAlmaki@users.noreply.github.com>
2026-04-23 14:28:02 +0100

    Fix Windows conda-bld env_logs written to wrong directory (#41282)

--- conda/recipes/archive_env_logs.bat
+++ conda/recipes/archive_env_logs.bat
@@ -4,9 +4,9 @@ set build_env_prefix=%1
 set host_env_prefix=%2
 set package_name=%3
 
-if not exist ..\..\env_logs\win-64 mkdir ..\..\env_logs\win-64
+if not exist ..\..\..\env_logs\win-64 mkdir ..\..\..\env_logs\win-64
 :: Just for first package (mantid), archive the base environment
-if %package_name%==mantid call conda list --explicit base > ..\..\env_logs\win-64\base_environment.txt
+if %package_name%==mantid call pixi run -e package-standalone conda list --explicit base > ..\..\..\env_logs\win-64\base_environment.txt 2>&1
 
-call conda list --explicit --prefix %host_env_prefix% > ..\..\env_logs\win-64\%package_name%_host_environment.txt
-call conda list --explicit --prefix %build_env_prefix% > ..\..\env_logs\win-64\%package_name%_build_environment.txt
+call pixi run -e package-standalone conda list --explicit --prefix %host_env_prefix% > ..\..\..\env_logs\win-64\%package_name%_host_environment.txt 2>&1
+call pixi run -e package-standalone conda list --explicit --prefix %build_env_prefix% > ..\..\..\env_logs\win-64\%package_name%_build_environment.txt 2>&1
