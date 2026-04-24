f41747d62718c20a2eb19600bcb368b0dc4167d1
rboston628 <bostonsr@ornl.gov>
2026-04-24 14:45:52 -0400

    default mock state should be false; cleanup

--- Framework/TestHelpers/src/MockMemory.cpp
+++ Framework/TestHelpers/src/MockMemory.cpp
@@ -3,25 +3,24 @@
 
 #include <atomic>
 #include <dlfcn.h>
+#include <stdexcept>
 
 namespace {
 using AvailMemFunc = std::size_t (*)(Mantid::Kernel::MemoryStats const *);
-static std::atomic<bool> g_override_availMem{true}; // flag, whether to use the real or mock availMem
-static AvailMemFunc g_real_availMem(nullptr);       // the real function, looked up from symbol list
+static std::atomic<bool> g_override_availMem{false}; // flag, whether to use the real or mock availMem
+static AvailMemFunc g_real_availMem(nullptr);        // the real function, looked up from symbol list
 static std::atomic<std::size_t> g_value{Mantid::TestMemory::g_default_value}; // the return value of mocked availMem
 
 static void init_real_availMem() {
-  // prepare the real
-  if (g_real_availMem) {
-    // if the real function has been set, we are done
-    return;
-  } else {
-    // otherwise looked it up by its mangled name in the symbol list and set it
+  // if not set, set the real function for availMem by looking up its mangled name in the symbol list
+  if (!g_real_availMem) {
+    // NOTE: deep magic begins here
     char const *mangled = "_ZNK6Mantid6Kernel11MemoryStats8availMemEv";
     void *sym = dlsym(RTLD_NEXT, mangled);
     if (sym) {
       g_real_availMem = reinterpret_cast<AvailMemFunc>(sym);
     }
+    // end deep magic
   }
 }
 } // namespace
@@ -46,7 +45,8 @@ std::size_t Mantid::Kernel::MemoryStats::availMem() const {
     if (g_real_availMem) {
       return g_real_availMem(this);
     } else {
-      return 0;
+      // if it cannot be thrown, this can cause opaque testing errors; throw an error here instead
+      throw std::runtime_error("Failed to reset the MemoryStats patch by name lookup");
     }
   }
 }
