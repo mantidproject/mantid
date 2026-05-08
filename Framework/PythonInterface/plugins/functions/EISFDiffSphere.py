4944410ae8933edde011e3ccf9a6747392cb93a4
Sam Tygier <74248560+samtygier-stfc@users.noreply.github.com>
2026-05-08 08:21:30 +0100

    Fix typos in DiffSphere quasielastic functions documentation (#41323)

--- Framework/PythonInterface/plugins/functions/EISFDiffSphere.py
+++ Framework/PythonInterface/plugins/functions/EISFDiffSphere.py
@@ -11,36 +11,23 @@
 """
 
 import numpy as np
-
-try:
-    from scipy.special import spherical_jn
-
-    def j1(z):
-        return spherical_jn(1, z)
-
-    def j1d(z):
-        return spherical_jn(1, z, derivative=True)
-
-except ImportError:
-    # spherical_jn removed from scipy >= 1.0.0
-    from scipy.special import sph_jn
-
-    def j1(z):
-        return sph_jn(1, z)[0][1]
-
-    def j1d(z):
-        return sph_jn(1, z)[1][1]
-
+from scipy.special import spherical_jn
 
 from mantid.api import IFunction1D, FunctionFactory
+
+
+def j1(z):
+    return spherical_jn(1, z)
+
+
+def j1d(z):
+    return spherical_jn(1, z, derivative=True)
 
 
 class EISFDiffSphere(IFunction1D):
     r"""Models the elastic incoherent scattering intensity of a particle
     undergoing diffusion but confined to a spherical volume
     """
-
-    vecbessel = np.vectorize(lambda z: j1(z) / z)
 
     def category(self):
         return "QuasiElastic"
@@ -57,9 +44,6 @@ class EISFDiffSphere(IFunction1D):
         ----------
         xvals : sequence of floats
           The domain where to evaluate the function
-        jacobian: 2D array
-          partial derivative of the function with respect to the fitting
-          parameters evaluated at the domain.
 
         Returns
         -------
@@ -67,7 +51,7 @@ class EISFDiffSphere(IFunction1D):
             Function values
         """
         zs = self.getParameterValue("R") * np.asarray(xvals)
-        return self.getParameterValue("A") * np.square(3 * self.vecbessel(zs))
+        return self.getParameterValue("A") * np.square(3 * j1(zs) / zs)
 
     def functionDeriv1D(self, xvals, jacobian):
         r"""Calculate the partial derivatives
@@ -82,13 +66,12 @@ class EISFDiffSphere(IFunction1D):
         """
         amplitude = self.getParameterValue("A")
         radius = self.getParameterValue("R")
-        i = 0
-        for x in xvals:
+
+        for i, x in enumerate(xvals):
             z = radius * x
             j = j1(z) / z
             jacobian.set(i, 0, np.square(3 * j))
             jacobian.set(i, 1, amplitude * 2 * 9 * j * (j1d(z) - j) / radius)
-            i += 1
 
 
 # Required to have Mantid recognise the new function
