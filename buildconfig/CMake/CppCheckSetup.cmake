897b9be030c8a5c8d4801a339b3a41142307fa68
Waruna Wickramasingha <waruna.wickramasingha@stfc.ac.uk>
2026-05-12 15:32:19 +0100

    max config=3

--- buildconfig/CMake/CppCheckSetup.cmake
+++ buildconfig/CMake/CppCheckSetup.cmake
@@ -38,7 +38,7 @@ if(CPPCHECK_EXECUTABLE)
       --library=${_cppcheck_qt_library}
       --check-level=normal
       --inline-suppr
-      --max-configs=5
+      --max-configs=3
       --std=c++${CMAKE_CXX_STANDARD} # use the standard from cmake
       --cppcheck-build-dir="${CPPCHECK_BUILD_DIR}/cache"
       --suppressions-list="${CPPCHECK_BUILD_DIR}/CppCheck_Suppressions.txt"
