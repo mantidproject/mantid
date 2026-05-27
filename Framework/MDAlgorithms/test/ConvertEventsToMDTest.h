d744d5ca20d1d4f565697aacdc75d25f1d923ba7
Duc Le <duc.le@stfc.ac.uk>
2026-05-27 17:56:37 +0100

    Refactor ConvEvToMD tests in line with review comment

--- Framework/MDAlgorithms/test/ConvertEventsToMDTest.h
+++ Framework/MDAlgorithms/test/ConvertEventsToMDTest.h
@@ -34,23 +34,16 @@ public:
 //
 class ConvertEventsToMDTest : public CxxTest::TestSuite {
   std::unique_ptr<ConvertEvents2MDEvTestHelper> pAlg;
+  Mantid::API::MatrixWorkspace_sptr m_wsEv;
 
 public:
   static ConvertEventsToMDTest *createSuite() { return new ConvertEventsToMDTest(); }
   static void destroySuite(ConvertEventsToMDTest *suite) { delete suite; }
 
-  void testEventWS() {
-    // set up algorithm
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("InputWorkspace", "testEvWS"));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("OutputWorkspace", "testMDEvWorkspace"));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("OtherDimensions", ""));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("QDimensions", "Q3D"));
-    pAlg->setPropertyValue("PreprocDetectorsWS", "");
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("dEAnalysisMode", "Elastic"));
-    pAlg->setPropertyValue("MinValues", "-10,-10,-10");
-    pAlg->setPropertyValue("MaxValues", " 10, 10, 10");
+  void setUp() override { resetAlgorithm(); }
+  void tearDown() override { AnalysisDataService::Instance().remove("testMDEvWorkspace"); }
 
-    pAlg->setRethrows(false);
+  void testEventWS() {
     pAlg->execute();
     TSM_ASSERT("Should finish succesfully", pAlg->isExecuted());
     Mantid::API::Workspace_sptr spws;
@@ -66,28 +59,16 @@ public:
     } else {
       TS_FAIL("event workspace has not beed build");
     }
-    AnalysisDataService::Instance().remove("testMDEvWorkspace");
   }
 
   void testEventWSusingLogTimes() {
-    // Add Gonio here as otherwise the NaNs in the log will cause the other test to fail
-    Mantid::API::MatrixWorkspace_sptr wsEv =
-        std::dynamic_pointer_cast<MatrixWorkspace>(AnalysisDataService::Instance().retrieve("testEvWS"));
-    Goniometer gonio; // Add a vertical rotation axis
+    Goniometer gonio;
     gonio.pushAxis("Rot", 0., 1., 0., 0.);
-    wsEv->mutableRun().setGoniometer(gonio, true);
-
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("InputWorkspace", "testEvWS"));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("OutputWorkspace", "testMDEvWorkspace"));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("OtherDimensions", ""));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("QDimensions", "Q3D"));
-    TS_ASSERT_THROWS_NOTHING(pAlg->setProperty("UseLogTimes", true));
-    pAlg->setPropertyValue("PreprocDetectorsWS", "");
+    m_wsEv->mutableRun().setGoniometer(gonio, true);
     TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("dEAnalysisMode", "Direct"));
+    TS_ASSERT_THROWS_NOTHING(pAlg->setProperty("UseLogTimes", true));
     pAlg->setPropertyValue("MinValues", "-10,-10,-10,-100");
     pAlg->setPropertyValue("MaxValues", " 10, 10, 10, 100");
-
-    pAlg->setRethrows(false);
     pAlg->execute();
     TSM_ASSERT("Should finish succesfully", pAlg->isExecuted());
     Mantid::API::Workspace_sptr spws;
@@ -99,43 +80,82 @@ public:
     TSM_ASSERT("It should be 4D MD workspace", ws.get());
 
     if (ws.get()) {
+      TS_ASSERT_EQUALS(900, ws->getNPoints());
+    } else {
+      TS_FAIL("event workspace has not beed build");
+    }
+    m_wsEv->mutableRun().setGoniometer(gonio, false); // Change back to not using log
+  }
+
+  void testEventWSusingLogTimesHalf() {
+    // Add Half Gonio here as otherwise the NaNs in the log will cause the other tests to fail
+    Goniometer gonio;
+    gonio.pushAxis("Half", 0., 1., 0., 0.);
+    m_wsEv->mutableRun().setGoniometer(gonio, true);
+    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("dEAnalysisMode", "Direct"));
+    TS_ASSERT_THROWS_NOTHING(pAlg->setProperty("UseLogTimes", true));
+    pAlg->setPropertyValue("MinValues", "-10,-10,-10,-100");
+    pAlg->setPropertyValue("MaxValues", " 10, 10, 10, 100");
+    pAlg->execute();
+    std::shared_ptr<DataObjects::MDEventWorkspace<DataObjects::MDEvent<4>, 4>> ws =
+        std::dynamic_pointer_cast<DataObjects::MDEventWorkspace<DataObjects::MDEvent<4>, 4>>(
+            AnalysisDataService::Instance().retrieve("testMDEvWorkspace"));
+    TSM_ASSERT("It should be 4D MD workspace", ws.get());
+    if (ws.get()) {
       TS_ASSERT_EQUALS(450, ws->getNPoints()); // Expect half the events as for non-UseLogTimes
     } else {
       TS_FAIL("event workspace has not beed build");
     }
-    AnalysisDataService::Instance().remove("testMDEvWorkspace");
-    wsEv->mutableRun().setGoniometer(gonio, false); // Change back to not using log
+    m_wsEv->mutableRun().setGoniometer(gonio, false); // Change back to not using log
   }
 
   ConvertEventsToMDTest() {
     FrameworkManager::Instance();
-
+    createEventWorkspace();
     pAlg = std::make_unique<ConvertEvents2MDEvTestHelper>();
     pAlg->initialize();
+  }
 
+  void createEventWorkspace() {
     int numHist = 10;
     int numEvents = 100;
-    Mantid::API::MatrixWorkspace_sptr wsEv = std::dynamic_pointer_cast<MatrixWorkspace>(
+    m_wsEv = std::dynamic_pointer_cast<MatrixWorkspace>(
         WorkspaceCreationHelper::createEventWorkspace(numHist, numEvents, numEvents, 0.0, 0.1, 5, 0));
-    wsEv->setInstrument(ComponentCreationHelper::createTestInstrumentCylindrical(numHist));
-    wsEv->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
+    m_wsEv->setInstrument(ComponentCreationHelper::createTestInstrumentCylindrical(numHist));
+    m_wsEv->getAxis(0)->unit() = UnitFactory::Instance().create("TOF");
     // any inelastic units or unit conversion using TOF needs Ei to be present
     // among properties.
-    wsEv->mutableRun().addProperty("Ei", 13., "meV", true);
+    m_wsEv->mutableRun().addProperty("Ei", 13., "meV", true);
 
-    // Add fake log and goniometer for useLogTimes test
+    // Add fake logs for useLogTimes test
     auto rotlog = std::make_unique<Kernel::TimeSeriesProperty<double>>("Rot");
+    auto halflog = std::make_unique<Kernel::TimeSeriesProperty<double>>("Half");
     auto t0 = DateAndTime("2010-01-01T00:00:00");
     for (int i = 0; i < numEvents / 2; i++) {
       rotlog->addValue(t0 + double(i), double(i) * 0.5);
+      halflog->addValue(t0 + double(i), double(i) * 0.5);
     }
-    // Make half of the events have no rotation - so they should be ignored if UseLogTimes
     for (int i = numEvents / 2; i < numEvents; i++) {
-      rotlog->addValue(t0 + double(i), std::numeric_limits<double>::quiet_NaN());
+      rotlog->addValue(t0 + double(i), double(i) * 0.5);
+      // Make half of the events have no rotation - so they should be ignored if UseLogTimes
+      halflog->addValue(t0 + double(i), std::numeric_limits<double>::quiet_NaN());
     }
-    wsEv->mutableRun().addLogData(std::move(rotlog));
-    wsEv->mutableSample().setOrientedLattice(std::make_unique<OrientedLattice>(4., 4., 4., 90., 90., 90.));
+    m_wsEv->mutableRun().addLogData(std::move(rotlog));
+    m_wsEv->mutableRun().addLogData(std::move(halflog));
+    m_wsEv->mutableSample().setOrientedLattice(std::make_unique<OrientedLattice>(4., 4., 4., 90., 90., 90.));
 
-    AnalysisDataService::Instance().addOrReplace("testEvWS", wsEv);
+    AnalysisDataService::Instance().addOrReplace("testEvWS", m_wsEv);
+  }
+
+  void resetAlgorithm() {
+    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("InputWorkspace", "testEvWS"));
+    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("OutputWorkspace", "testMDEvWorkspace"));
+    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("OtherDimensions", ""));
+    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("QDimensions", "Q3D"));
+    pAlg->setPropertyValue("PreprocDetectorsWS", "");
+    TS_ASSERT_THROWS_NOTHING(pAlg->setPropertyValue("dEAnalysisMode", "Elastic"));
+    pAlg->setPropertyValue("MinValues", "-10,-10,-10");
+    pAlg->setPropertyValue("MaxValues", " 10, 10, 10");
+    pAlg->setRethrows(false);
   }
 };
