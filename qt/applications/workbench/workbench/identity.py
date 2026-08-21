78312fdc56518eadb646fcc565f0f06488e1d632
Pete Peterson <petersonpf@ornl.gov>
2026-08-21 14:30:58 -0400

    Move qsettings identifier stuff into a separate file (#42079)

---
+++ qt/applications/workbench/workbench/identity.py
@@ -0,0 +1,13 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2026 ISIS Rutherford Appleton Laboratory UKRI,
+#   NScD Oak Ridge National Laboratory, European Spallation Source,
+#   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
+# SPDX - License - Identifier: GPL - 3.0 +
+#  This file is part of the mantid workbench.
+
+"""Side-effect-free application identity shared by Workbench processes."""
+
+ORGANIZATION = "mantidproject"
+ORG_DOMAIN = "mantidproject.org"
+APPNAME = "mantidworkbench"
