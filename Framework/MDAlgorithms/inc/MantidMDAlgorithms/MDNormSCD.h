0d9dd0008c806bd23ae94bf282011627eba9db8d
Duc Le <duc.le@stfc.ac.uk>
2026-09-08 11:55:50 +0100

    Refactor MDNorm* algorithms to use common methods (#42126)

--- Framework/MDAlgorithms/inc/MantidMDAlgorithms/MDNormSCD.h
+++ Framework/MDAlgorithms/inc/MantidMDAlgorithms/MDNormSCD.h
@@ -6,20 +6,15 @@
 // SPDX - License - Identifier: GPL - 3.0 +
 #pragma once
 
-#include "MantidAPI/Algorithm.h"
-#include "MantidMDAlgorithms/SlicingAlgorithm.h"
+#include "MantidMDAlgorithms/MDNormBase.h"
 
-namespace Mantid {
-namespace DataObjects {
-class EventWorkspace;
-}
-namespace MDAlgorithms {
+namespace Mantid::MDAlgorithms {
 
 /** MDNormSCD : Generate MD normalization for single crystal diffraction
  */
-class MANTID_MDALGORITHMS_DLL MDNormSCD : public SlicingAlgorithm {
+class MANTID_MDALGORITHMS_DLL MDNormSCD : public MDNormBase {
 public:
-  MDNormSCD();
+  MDNormSCD() {};
 
   const std::string name() const override;
   int version() const override;
@@ -34,47 +29,6 @@ private:
   void exec() override;
 
   void cacheInputs();
-  std::string inputEnergyMode() const;
-
-  DataObjects::MDHistoWorkspace_sptr binInputWS();
-  void createNormalizationWS(const DataObjects::MDHistoWorkspace &dataWS);
-  std::vector<coord_t> getValuesFromOtherDimensions(bool &skipNormalization, uint16_t expInfoIndex = 0) const;
-  Kernel::Matrix<coord_t> findIntergratedDimensions(const std::vector<coord_t> &otherDimValues,
-                                                    bool &skipNormalization);
-  void cacheDimensionXValues();
-  void calculateNormalization(const std::vector<coord_t> &otherValues, const Kernel::Matrix<coord_t> &affineTrans,
-                              uint16_t expInfoIndex);
-  void calcIntegralsForIntersections(const std::vector<double> &xValues, const API::MatrixWorkspace &integrFlux,
-                                     size_t sp, std::vector<double> &yValues) const;
-  void calculateIntersections(std::vector<std::array<double, 4>> &intersections, const double theta, const double phi);
-
-  /// Normalization workspace
-  DataObjects::MDHistoWorkspace_sptr m_normWS;
-  /// Input workspace
-  API::IMDEventWorkspace_sptr m_inputWS;
-  /// limits for h,k,l dimensions
-  coord_t m_hmin, m_hmax, m_kmin, m_kmax, m_lmin, m_lmax;
-  /// flag for integrated h,k,l dimensions
-  bool m_hIntegrated, m_kIntegrated, m_lIntegrated;
-  /// (2*PiRUBW)^-1
-  Mantid::Kernel::DblMatrix m_rubw;
-  /// limits for momentum
-  double m_kiMin, m_kiMax;
-  /// index of h,k,l dimensions in the output workspaces
-  size_t m_hIdx, m_kIdx, m_lIdx;
-  /// cached X values along dimensions h,k,l
-  std::vector<double> m_hX, m_kX, m_lX;
-  /// Sample position
-  Kernel::V3D m_samplePos;
-  /// Beam direction
-  Kernel::V3D m_beamDir;
-  /// ki-kf for Inelastic convention; kf-ki for Crystallography convention
-  std::string convention;
-  /// internal flag to accumulate to an existing workspace
-  bool m_accumulate{false};
-  /// number of experiment infos
-  uint16_t m_numExptInfos;
 };
 
-} // namespace MDAlgorithms
-} // namespace Mantid
+} // namespace Mantid::MDAlgorithms
