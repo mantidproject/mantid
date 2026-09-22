8aa482585f870792b738d9de34fba60b05ef1264
Duc Le <duc.le@stfc.ac.uk>
2026-09-22 02:26:47 +0100

    Temporary fix for MDNorm out-of-bounds error #42274

--- Framework/MDAlgorithms/src/MDNormBase.cpp
+++ Framework/MDAlgorithms/src/MDNormBase.cpp
@@ -331,6 +331,13 @@ void MDNormBase::calculateNormContinuous(const std::vector<coord_t> &otherValues
     UBWSymm = calQTransform(DblMatrix(3, 3, true), *so, false);
     lowValues = getLogValues<VectorDoubleProperty>(currentExptInfo, "MDNorm_low");
     highValues = getLogValues<VectorDoubleProperty>(currentExptInfo, "MDNorm_high");
+    if (lowValues.size() != spectrumInfo.size()) {
+      g_log.warning() << "Incompatible detector and spectrum size. Using original data limits.\n";
+      double run_emin = currentExptInfo.run().getBinBoundaries().front();
+      double run_emax = currentExptInfo.run().getBinBoundaries().back();
+      lowValues.assign(spectrumInfo.size(), run_emin);
+      highValues.assign(spectrumInfo.size(), run_emax);
+    }
   }
   // MDEventWS was created with the "useLogTimes" option: should be only a single expInfo, but
   // gonios vary with time - we now coarse-bin it to compute the normalisation.
